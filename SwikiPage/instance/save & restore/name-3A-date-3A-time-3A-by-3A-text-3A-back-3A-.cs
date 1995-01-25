ab1449f28713ba6796f47077ee9f14f1bb46183c
tk <unknown>
1995-01-25 07:27:00 +0000

    no message

---
+++ SwikiPage/instance/save & restore/name-3A-date-3A-time-3A-by-3A-text-3A-back-3A-.cs
@@ -0,0 +1,5 @@
+name: pageName date: dateString time: timeString by: who text: theString back: bytes
+	"Record the name of this page during startup.  When reading the page in order to serve it, this is NOT executed.  The text is read from the file explicitly."
+	
+	name _ pageName.
+	date _ dateString asDate.
