6fe722bdcb79f7406966de2c96b50aa18dc8a35e
di <unknown>
1997-06-09 16:10:00 +0000

    no message

--- Form/instance/private/setExtent-3A-depth-3A-.cs
+++ Form/instance/private/setExtent-3A-depth-3A-.cs
@@ -1,9 +1,9 @@
 setExtent: extent depth: bitsPerPixel
 	"Create a virtual bit map with the given extent and bitsPerPixel."
 
-	width _ extent x.
+	width _ extent x asInteger.
 	width < 0 ifTrue: [width _ 0].
-	height _ extent y.
+	height _ extent y asInteger.
 	height < 0 ifTrue: [height _ 0].
 	depth _ bitsPerPixel.
 	bits _ Bitmap new: self bitsSize
