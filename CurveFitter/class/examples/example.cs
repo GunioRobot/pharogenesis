f1413d326f98d3b3249bf795958969054087e5b3
di <unknown>
1997-06-09 10:16:00 +0000

    no message

---
+++ CurveFitter/class/examples/example.cs
@@ -0,0 +1,20 @@
+example
+	"Designate three locations on the screen by clicking any button. The
+	curve determined by the points will be displayed with a long black form."
+
+	| aCurveFitter aForm |  
+	aForm _ Form extent: 1@30.			"make a long thin Form for display "
+	aForm fillBlack.							"turn it black"
+	aCurveFitter _ CurveFitter new.
+	aCurveFitter form: aForm.						"set the form for display"
+				"collect three Points and show them on the dispaly"
+	aCurveFitter firstPoint: Sensor waitButton. Sensor waitNoButton.
+	aForm displayOn: Display at: aCurveFitter firstPoint.
+	aCurveFitter secondPoint: Sensor waitButton. Sensor waitNoButton.
+	aForm displayOn: Display at: aCurveFitter secondPoint.
+	aCurveFitter thirdPoint: Sensor waitButton. Sensor waitNoButton.
+	aForm displayOn: Display at: aCurveFitter thirdPoint.
+
+	aCurveFitter displayOn: Display					"display the CurveFitter"
+
+	"CurveFitter example"
