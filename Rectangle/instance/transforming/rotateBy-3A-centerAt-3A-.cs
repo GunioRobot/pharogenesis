4962059078e1cdc6eb2b25c26ed48ce2f6a59e6f
di <unknown>
1997-06-11 15:11:00 +0000

    no message

--- Rectangle/instance/transforming/rotateBy-3A-centerAt-3A-.cs
+++ Rectangle/instance/transforming/rotateBy-3A-centerAt-3A-.cs
@@ -1,5 +1,4 @@
 rotateBy: direction centerAt: aPoint
-	"Return a copy rotated either #right or #left around aPoint"
-	^ Rectangle origin: ((origin rotateBy: direction centerAt: aPoint) - (direction == #right ifTrue: [self height @ 0] 
-	ifFalse: [0 @ self width])) extent: self extent transpose	
-		"origin becomes new topRight then offset to origin"
+	"Return a copy rotated #right, #left, or #pi about aPoint"
+	^ (origin rotateBy: direction centerAt: aPoint)
+		rect: (corner rotateBy: direction centerAt: aPoint)
