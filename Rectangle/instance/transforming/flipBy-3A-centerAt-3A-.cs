96156d9e71ced877dc2d00a47f621293fbae5b44
di <unknown>
1997-06-11 16:24:00 +0000

    no message

--- Rectangle/instance/transforming/flipBy-3A-centerAt-3A-.cs
+++ Rectangle/instance/transforming/flipBy-3A-centerAt-3A-.cs
@@ -1,8 +1,4 @@
 flipBy: direction centerAt: aPoint 
-	"Return a copy flipped according to the direction, either #vertical or #horizontal, around aPoint."
-	^ Rectangle
-		origin: ((direction == #vertical
-					ifTrue: [self bottomLeft]
-					ifFalse: [self topRight])
-				flipBy: direction centerAt: aPoint)
-		extent: self extent
+	"Return a copy flipped #vertical or #horizontal, about aPoint."
+	^ (origin flipBy: direction centerAt: aPoint)
+		rect: (corner flipBy: direction centerAt: aPoint)
