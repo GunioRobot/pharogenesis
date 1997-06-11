654e38defa597df2ab8183e9c15e58e1d4d54072
di <unknown>
1997-06-11 16:08:00 +0000

    no message

--- Point/instance/point functions/flipBy-3A-centerAt-3A-.cs
+++ Point/instance/point functions/flipBy-3A-centerAt-3A-.cs
@@ -1,5 +1,6 @@
 flipBy: direction centerAt: c
-	"Answer a Point which is receiver flipped according to the direction, either #vertical or #horizontal, center at point c"
-	^ direction == #vertical 
-		ifTrue: [x @ (c y * 2 - y)]
-		ifFalse: [(c x * 2 - x) @ y]
+	"Answer a Point which is flipped according to the direction about the point c.
+	Direction must be #vertical or #horizontal."
+	direction == #vertical ifTrue: [^ x @ (c y * 2 - y)].
+	direction == #horizontal ifTrue: [^ (c x * 2 - x) @ y].
+	self error: 'unrecognizable direction'
