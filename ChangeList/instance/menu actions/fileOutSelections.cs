9ee7709b4939ef5d00ff4d9e6b1f958b9b04ff1c
jm <unknown>
1997-06-12 10:54:00 +0000

    no message

--- ChangeList/instance/menu actions/fileOutSelections.cs
+++ ChangeList/instance/menu actions/fileOutSelections.cs
@@ -1,6 +1,7 @@
 fileOutSelections 
 	| f |
 	f _ FileStream newFileNamed: (FillInTheBlank request: 'Enter file name' initialAnswer: 'Filename.st').
+	f header; timeStamp.
 	listSelections with: changeList do: 
 		[:selected :item | selected ifTrue: [item fileOutOn: f]].
-	f close
+	f close.
