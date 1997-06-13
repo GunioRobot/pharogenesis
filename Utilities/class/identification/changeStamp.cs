aa2b562c17db36d8bf429764584058ea03fc95ba
di <unknown>
1997-06-13 13:00:00 +0000

    no message

--- Utilities/class/identification/changeStamp.cs
+++ Utilities/class/identification/changeStamp.cs
@@ -1,3 +1,4 @@
-changeStamp
-	"Answer a string to be pasted into source code to mark who changed it and when.  1/17/96 sw"
-	^ Date today mmddyy, ' ', self authorInitials
+changeStamp 
+	"Answer a string to be pasted into source code to mark who changed it and when."
+	^ self authorInitials , ' ' , Date today mmddyy, ' ',
+		((String streamContents: [:s | Time now print24: true on: s]) copyFrom: 1 to: 5)
