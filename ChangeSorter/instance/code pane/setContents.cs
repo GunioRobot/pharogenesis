cd4458c3fd727420295d466a3928923d9da43d7b
di <unknown>
1997-06-15 09:34:00 +0000

    no message

--- ChangeSorter/instance/code pane/setContents.cs
+++ ChangeSorter/instance/code pane/setContents.cs
@@ -1,21 +1,22 @@
 setContents
 	"return the source code that shows in the bottom pane"
-	| sel class strm what |
+	| sel class strm changeType |
 	self unlock.
 	(classList selection) == nil ifTrue: [^ contents _ ''].
 	class _ classList selectedClassOrMetaClass.
 	(sel _ messageList selection) == nil
-		ifFalse: [
-			what _ (myChangeSet atSelector: (sel _ sel asSymbol) class: class).
-			what == #remove ifFalse: [
-				(class includesSelector: sel) ifFalse: [
-					^ contents _ 'was added, but it''s gone! ******'].
-				^ contents _ (class sourceMethodAt: sel) copy]
-			  ifTrue: [^ contents _ 'remove the method ******']]
+		ifFalse: [changeType _ (myChangeSet atSelector: (sel _ sel asSymbol) class: class).
+			changeType == #remove
+				ifTrue: [^ contents _ 'Method has been removed (see versions)'].
+			changeType == #addedThenRemoved
+				ifTrue: [^ contents _ 'Added then removed (see versions)'].
+			(class includesSelector: sel)
+				ifFalse: [^ contents _ 'Method was added, but cannot be found!'].
+			^ contents _ (class sourceMethodAt: sel) copy]
 		ifTrue: [strm _ WriteStream on: (String new: 100).
 			(myChangeSet classChangeAt: class name) do: [:each |
-				each = #remove ifTrue: [strm nextPutAll: '**** entire class was removed ****'; cr].
-				each = #add ifTrue: [strm nextPutAll: '**** entire class was added ****'; cr].
-				each = #change ifTrue: [strm nextPutAll: '**** class definition was changed ****'; cr].
-				each = #comment ifTrue: [strm nextPutAll: '**** new class comment ****'; cr]].
+				each = #remove ifTrue: [strm nextPutAll: 'Entire class was removed.'; cr].
+				each = #add ifTrue: [strm nextPutAll: 'Entire class was added.'; cr].
+				each = #change ifTrue: [strm nextPutAll: 'Class definition was changed.'; cr].
+				each = #comment ifTrue: [strm nextPutAll: 'New class comment.'; cr]].
 			^ contents _ strm contents].
