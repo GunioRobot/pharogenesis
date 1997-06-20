da3d77074fe2ff63f6b69365a630731c85d85294
di <unknown>
1997-06-20 08:39:00 +0000

    no message

--- WriteStream/instance/private/pastEndPut-3A-.cs
+++ WriteStream/instance/private/pastEndPut-3A-.cs
@@ -1,5 +1,5 @@
 pastEndPut: anObject
 	collection _ collection ,
-		(collection class new: ((collection size max: 20) min: 5000)).
+		(collection class new: ((collection size max: 20) min: 20000)).
 	writeLimit _ collection size.
 	collection at: (position _ position + 1) put: anObject
