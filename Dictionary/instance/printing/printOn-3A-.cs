c928a80c3e53edf7df1f75c4aa1a62a78727d714
di <unknown>
1997-06-20 09:10:00 +0000

    no message

--- Dictionary/instance/printing/printOn-3A-.cs
+++ Dictionary/instance/printing/printOn-3A-.cs
@@ -1,13 +1,4 @@
 printOn: aStream
-	| tooMany |
-	tooMany _ self maxPrint.	
-		"Need absolute limit, or infinite recursion will never 
-		notice anything going wrong.  7/26/96 tk"
 	aStream nextPutAll: self class name, ' ('.
-	self associationsDo: 
-		[:element | 
-		aStream position > tooMany
-			ifTrue: [aStream nextPutAll: '...etc...)'. ^ self].
-		element printOn: aStream.
-		aStream space].
+	self associationsDo: [:element | element printOn: aStream. aStream space].
 	aStream nextPut: $)
