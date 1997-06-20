0c8769b243610988b7ee52811d6a361998315c92
di <unknown>
1997-06-20 09:09:00 +0000

    no message

--- Array/instance/printing/printOn-3A-.cs
+++ Array/instance/printing/printOn-3A-.cs
@@ -1,13 +1,4 @@
 printOn: aStream
-
-	| tooMany |
-	tooMany _ self maxPrint.	
-		"Need absolute limit, or infinite recursion will never 
-		notice anything going wrong.  7/26/96 tk"
 	aStream nextPut: $(.
-	self do: 
-		[:element | 
-		aStream position > tooMany ifTrue: [aStream nextPutAll: '...etc...)'. ^self].
-		element printOn: aStream.
-		aStream space].
+	self do: [:element | element printOn: aStream. aStream space].
 	aStream nextPut: $)
