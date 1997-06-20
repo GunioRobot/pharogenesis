2238ae0422ffde5bed6c206a5f9997076e3e5088
di <unknown>
1997-06-20 09:22:00 +0000

    no message

--- Object/instance/printing/printString.cs
+++ Object/instance/printing/printString.cs
@@ -1,7 +1,8 @@
 printString
-	"Answer a String whose characters are a description of the receiver."
-
-	| aStream |
-	aStream _ WriteStream on: (String new: 100).
-	self printOn: aStream.
-	^aStream contents
+	"Answer a String whose characters are a description of the receiver.
+	If you want to print without a character limit, use fullPrintString."
+	| limit limitedString |
+	limit _ 50000.
+	limitedString _ String streamContents: [:s | self printOn: s] limitedTo: limit.
+	limitedString size < limit ifTrue: [^ limitedString].
+	^ limitedString , '...etc...'
