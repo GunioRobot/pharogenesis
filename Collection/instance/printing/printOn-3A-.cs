c14e5eed7f64caf5a892172e6c0c38fd8d50b01c
di <unknown>
1997-06-20 09:09:00 +0000

    no message

--- Collection/instance/printing/printOn-3A-.cs
+++ Collection/instance/printing/printOn-3A-.cs
@@ -1,14 +1,5 @@
 printOn: aStream 
 	"Refer to the comment in Object|printOn:."
-
-	| tooMany |
-	tooMany _ self maxPrint.	
-		"Need absolute limit, or infinite recursion will never 
-		notice anything going wrong.  7/26/96 tk"
 	aStream nextPutAll: self class name, ' ('.
-	self do: 
-		[:element | 
-		aStream position > tooMany ifTrue: [aStream nextPutAll: '...etc...)'. ^self].
-		element printOn: aStream.
-		aStream space].
+	self do: [:element | element printOn: aStream. aStream space].
 	aStream nextPut: $)
