098374acf82b86f18ce5cc3723b038da0c5ce726
di <unknown>
1997-06-21 08:26:00 +0000

    no message

---
+++ Wavelets/instance/all/initFilters.cs
@@ -0,0 +1,125 @@
+initFilters    "FilterArray newFromTable"
+| h |
+h _ self new: 10.
+
+self hAt: 1 at: -1 put: 1.0 / 2.0 sqrt.
+self hAt: 1 at: 0 put: 1.0 / 2.0 sqrt.
+
+self hAt: 2 at: -2 put: 0.4829629131445341.
+self hAt: 2 at: -1 put: 0.8365163037378077.
+self hAt: 2 at: 0 put: 0.2241438680420134.
+self hAt: 2 at: 1 put: -0.1294095225512603.
+
+self hAt: 3 at: -3 put: 0.3326705529500825.
+self hAt: 3 at: -2 put: 0.8068915093110924.
+self hAt: 3 at: -1 put: 0.4598775021184914.
+self hAt: 3 at: 0 put: -0.1350110200102546.
+self hAt: 3 at: 1 put: -0.0854412738820267.
+self hAt: 3 at: 2 put: 0.0352262918857095.
+
+self hAt: 4 at: -4 put: 0.2303778133088964.
+self hAt: 4 at: -3 put: 0.7148465705529154.
+self hAt: 4 at: -2 put: 0.6308807679398587.
+self hAt: 4 at: -1 put: -0.0279837694168599.
+self hAt: 4 at: 0 put: -0.1870348117190931.
+self hAt: 4 at: 1 put: 0.0308413818355607.
+self hAt: 4 at: 2 put: 0.0328830116668852.
+self hAt: 4 at: 3 put: -0.0105974017850690.
+
+self hAt: 5 at: -5 put: 0.1601023979741929.
+self hAt: 5 at: -4 put: 0.6038292697971895.
+self hAt: 5 at: -3 put: 0.7243085284377726.
+self hAt: 5 at: -2 put: 0.1384281459013203.
+self hAt: 5 at: -1 put: -0.2422948870663823.
+self hAt: 5 at: 0 put: -0.0322448695846381.
+self hAt: 5 at: 1 put: 0.0775714938400459.
+self hAt: 5 at: 2 put: -0.0062414902127983.
+self hAt: 5 at: 3 put: -0.0125807519990820.
+self hAt: 5 at: 4 put: 0.0033357252854738.
+
+self hAt: 6 at: -6 put: 0.1115407433501095.
+self hAt: 6 at: -5 put: 0.4946238903984533.
+self hAt: 6 at: -4 put: 0.7511339080210959.
+self hAt: 6 at: -3 put: 0.3152503517091982.
+self hAt: 6 at: -2 put: -0.2262646939654400.
+self hAt: 6 at: -1 put: -0.1297668675672625.
+self hAt: 6 at: 0 put: 0.0975016055873225.
+self hAt: 6 at: 1 put: 0.0275228655303053.
+self hAt: 6 at: 2 put: -0.0315820393174862.
+self hAt: 6 at: 3 put: 0.0005538422011614.
+self hAt: 6 at: 4 put: 0.0047772575109455.
+self hAt: 6 at: 5 put: -0.0010773010853085.
+
+self hAt: 7 at: -7 put: 0.0778520540850037.
+self hAt: 7 at: -6 put: 0.3965393194818912.
+self hAt: 7 at: -5 put: 0.7291320908461957.
+self hAt: 7 at: -4 put: 0.4697822874051889.
+self hAt: 7 at: -3 put: -0.1439060039285212.
+self hAt: 7 at: -2 put: -0.2240361849938412.
+self hAt: 7 at: -1 put: 0.0713092192668272.
+self hAt: 7 at: 0 put: 0.0806126091510774.
+self hAt: 7 at: 1 put: -0.0380299369350104.
+self hAt: 7 at: 2 put: -0.0165745416306655.
+self hAt: 7 at: 3 put: 0.0125509985560986.
+self hAt: 7 at: 4 put: 0.0004295779729214.
+self hAt: 7 at: 5 put: -0.0018016407040473.
+self hAt: 7 at: 6 put: 0.0003537137999745.
+
+self hAt: 8 at: -8 put: 0.0544158422431072.
+self hAt: 8 at: -7 put: 0.3128715909143166.
+self hAt: 8 at: -6 put: 0.6756307362973195.
+self hAt: 8 at: -5 put: 0.5853546836542159.
+self hAt: 8 at: -4 put: -0.0158291052563823.
+self hAt: 8 at: -3 put: -0.2840155429615824.
+self hAt: 8 at: -2 put: 0.0004724845739124.
+self hAt: 8 at: -1 put: 0.1287474266204893.
+self hAt: 8 at: 0 put: -0.0173693010018090.
+self hAt: 8 at: 1 put: -0.0440882539307971.
+self hAt: 8 at: 2 put: 0.0139810279174001.
+self hAt: 8 at: 3 put: 0.0087460940474065.
+self hAt: 8 at: 4 put: -0.0048703529934520.
+self hAt: 8 at: 5 put: -0.0003917403733770.
+self hAt: 8 at: 6 put: 0.0006754494064506.
+self hAt: 8 at: 7 put: -0.0001174767841248.
+
+self hAt: 9 at: -9 put: 0.0380779473638778.
+self hAt: 9 at: -8 put: 0.2438346746125858.
+self hAt: 9 at: -7 put: 0.6048231236900955.
+self hAt: 9 at: -6 put: 0.6572880780512736.
+self hAt: 9 at: -5 put: 0.1331973858249883.
+self hAt: 9 at: -4 put: -0.2932737832791663.
+self hAt: 9 at: -3 put: -0.0968407832229492.
+self hAt: 9 at: -2 put: 0.1485407493381256.
+self hAt: 9 at: -1 put: 0.0307256814793385.
+self hAt: 9 at: 0 put: -0.0676328290613279.
+self hAt: 9 at: 1 put: 0.0002509471148340.
+self hAt: 9 at: 2 put: 0.0223616621236798.
+self hAt: 9 at: 3 put: -0.0047232047577518.
+self hAt: 9 at: 4 put: -0.0042815036824635.
+self hAt: 9 at: 5 put: 0.0018476468830563.
+self hAt: 9 at: 6 put: 0.0002303857635232.
+self hAt: 9 at: 7 put: -0.0002519631889427.
+self hAt: 9 at: 8 put: 0.0000393473203163.
+
+self hAt: 10 at: -10 put: 0.0266700579005473.
+self hAt: 10 at: -9 put: 0.1881768000776347.
+self hAt: 10 at: -8 put: 0.5272011889315757.
+self hAt: 10 at: -7 put: 0.6884590394534363.
+self hAt: 10 at: -6 put: 0.2811723436605715.
+self hAt: 10 at: -5 put: -0.2498464243271598.
+self hAt: 10 at: -4 put: -0.1959462743772862.
+self hAt: 10 at: -3 put: 0.1273693403357541.
+self hAt: 10 at: -2 put: 0.0930573646035547.
+self hAt: 10 at: -1 put: -0.0713941471663501.
+self hAt: 10 at: 0 put: -0.0294575368218399.
+self hAt: 10 at: 1 put: 0.0332126740593612.
+self hAt: 10 at: 2 put: 0.0036065535669870.
+self hAt: 10 at: 3 put: -0.0107331754833007.
+self hAt: 10 at: 4 put: 0.0013953517470688.
+self hAt: 10 at: 5 put: 0.0019924052951925.
+self hAt: 10 at: 6 put: -0.0006858566949564.
+self hAt: 10 at: 7 put: -0.0001164668551285.
+self hAt: 10 at: 8 put: 0.0000935886703202.
+self hAt: 10 at: 9 put: -0.0000132642028945.
+
+^ h
