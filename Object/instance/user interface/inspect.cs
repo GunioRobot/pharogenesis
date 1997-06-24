e1ec0bc3bced86d0450a0b66ac6c6783d148b61b
di <unknown>
1997-06-24 11:47:00 +0000

    no message

--- Object/instance/user interface/inspect.cs
+++ Object/instance/user interface/inspect.cs
@@ -1,4 +1,9 @@
 inspect
 	"Create and schedule an Inspector in which the user can examine the 
 	receiver's variables."
-	InspectorView open: (InspectorView inspectorWithTrash: (Inspector inspect: self))
+	| insp |
+	World ifNotNil:
+		["Written so that Morphic can still be removed."
+		insp _ (Smalltalk at: #ObjectInspector) on: self.
+		^ self world addMorph: insp; startStepping: insp].
+	Inspector openOn: self withEvalPane: true
