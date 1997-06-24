aa3eb6ab231e102e2950953ca29290e60b9030de
di <unknown>
1997-06-24 00:16:00 +0000

    no message

--- ScreenController/class/class initialization/initialize.cs
+++ ScreenController/class/class initialization/initialize.cs
@@ -1,5 +1,5 @@
 initialize
-	"Initialize the screen menus.  Call this method to reset everything back to nil, so that the various menu retrieval methods will duly reinitialize them.  7/24/96 sw"
+	"Initialize the screen menus.  Call this method to reset everything back to nil, so that the various menu retrieval methods will duly reinitialize them."
 
 	"ScreenController initialize"
-	ChangesMenu _ HelpMenu _ TopScreenMenu _ OpenMenu _ WindowMenu _ ProjectScreenMenu _ nil
+	ChangesMenu _ HelpMenu _ TopScreenMenu _ OpenMenu _ WindowMenu _ ProjectScreenMenu _ nil.
