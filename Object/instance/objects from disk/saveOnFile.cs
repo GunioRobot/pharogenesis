056c0a8a6e2db3b8a433a242d552d2f5ae0005de
tk <unknown>
1997-06-26 13:48:00 +0000

    no message

---
+++ Object/instance/objects from disk/saveOnFile.cs
@@ -0,0 +1,10 @@
+saveOnFile
+	"Ask the user for a filename and save myself on a SmartReferenceStream file.  Writes out the version and class structure.  The file is fileIn-able.  Does not file out the class of the object.  tk 6/26/97 13:48"
+
+	| aFileName fileStream |
+	aFileName _ self class name asFileName.	"do better?"
+	aFileName _ FillInTheBlank request: 'File name?' initialAnswer: aFileName.
+	aFileName size == 0 ifTrue: [^ self beep].
+
+	fileStream _ FileStream newFileNamed: aFileName.
+	fileStream fileOutClass: nil andObject: self.
