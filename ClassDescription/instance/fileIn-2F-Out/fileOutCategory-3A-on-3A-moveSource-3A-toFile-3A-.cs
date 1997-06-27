7a3d70192728abd959f30a8fbdd98c9d1decc7f9
di <unknown>
1997-06-27 05:27:00 +0000

    no message

--- ClassDescription/instance/fileIn-2F-Out/fileOutCategory-3A-on-3A-moveSource-3A-toFile-3A-.cs
+++ ClassDescription/instance/fileIn-2F-Out/fileOutCategory-3A-on-3A-moveSource-3A-toFile-3A-.cs
@@ -1,15 +1,14 @@
 fileOutCategory: aString on: aFileStream moveSource: moveSource toFile: fileIndex 
 	"File a description of the receiver's category, aString, onto aFileStream. If 
-	the boolean argument, moveSource, is true, then set the trailing bytes to 
-	the position of aFileStream and to fileIndex in order to indicate where to 
-	find the source code."
+	moveSource, is true, then set the method source pointer to the new file position.
+	Note when this method is called with moveSource=true, it is condensing the
+	.sources file, and should only write one preamble per method category."
 
 	aFileStream cr.
-	self printCategoryChunk: aString on: aFileStream.
+	moveSource ifTrue:
+		["Single header for condensing source files"
+		self printCategoryChunk: aString on: aFileStream].
 	(self organization listAtCategoryNamed: aString)
-		do: [:sel | self
-				printMethodChunk: sel
-				on: aFileStream
-				moveSource: moveSource
-				toFile: fileIndex].
-	aFileStream nextChunkPut: ' '
+		do: [:sel | self printMethodChunk: sel withPreamble: moveSource not
+						on: aFileStream moveSource: moveSource toFile: fileIndex].
+	moveSource ifTrue: [aFileStream nextChunkPut: ' ']
