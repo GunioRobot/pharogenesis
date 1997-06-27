3b6049fd59a09c988f5f00662757cd8837eac402
di <unknown>
1997-06-27 12:18:00 +0000

    no message

--- StandardFileStream/instance/access/peekFor-3A-.cs
+++ StandardFileStream/instance/access/peekFor-3A-.cs
@@ -1,13 +1,8 @@
 peekFor: item 
-	"Answer false and do not move over the next element if it is not equal to 
-	the argument, anObject, or if the receiver is at the end. Answer true 
-	and increment the position for accessing elements, if the next element is 
-	equal to anObject..  Copied over from HFS versino.  2/14/96 sw"
-
+	"Answer false and do not advance if the next element is not equal to item, or if this stream is at the end.  If the next element is equal to item, then advance over it and return true"
 	| next |
-
-	self atEnd ifTrue: [^ false].
-	next _ self next.
+	"self atEnd ifTrue: [^ false]. -- SFStream will give nil"
+	(next _ self next) == nil ifTrue: [^ false].
 	item = next ifTrue: [^ true].
 	self skip: -1.
 	^ false
