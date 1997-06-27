e718a6461d310522f005b34d2d6265ae4604b8fb
di <unknown>
1997-06-27 21:25:00 +0000

    no message

--- Browser/instance/message functions/fileOutMessage.cs
+++ Browser/instance/message functions/fileOutMessage.cs
@@ -1,15 +1,4 @@
 fileOutMessage
-	"Print a description of the selected message on the serial printer.
-	4/11/96 tk - header, trailer"
-	| fileStream |
+	"Print a description of the selected message"
 	messageListIndex = 0 ifTrue: [^ self].
-	fileStream _ FileStream newFileNamed: (self selectedClassOrMetaClass name , '-' , (self selectedMessageName copyReplaceAll: ':' with: '')) , '.st'.
-	fileStream header; timeStamp.
-	self selectedClassOrMetaClass printCategoryChunk: self selectedMessageCategoryName
-		on: fileStream.
-	self selectedClassOrMetaClass printMethodChunk: self selectedMessageName
-		on: fileStream
-		moveSource: false
-		toFile: 0.
-	fileStream nextChunkPut: ' '.
-	fileStream trailer; close
+	self selectedClassOrMetaClass fileOutMethod: self selectedMessageName
