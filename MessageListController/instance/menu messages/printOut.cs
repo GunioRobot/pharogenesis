7f328d9c88f12da848bbc58f40654619c38ca377
di <unknown>
1997-06-28 15:45:00 +0000

    no message

--- MessageListController/instance/menu messages/printOut.cs
+++ MessageListController/instance/menu messages/printOut.cs
@@ -1,2 +1,6 @@
 printOut
-	self fileOut
+	"Write a description of the selected message on an external file."
+	self controlTerminate.
+	Cursor write showWhile:
+		[model printOutMessage].
+	self controlInitialize
