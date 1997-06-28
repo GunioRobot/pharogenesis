b3ae7e12a7082d6135a562ad54902c7d6d358b5f
di <unknown>
1997-06-28 21:31:00 +0000

    no message

---
+++ HtmlFileStream/class/as yet unclassified/initialize.cs
@@ -0,0 +1,6 @@
+initialize   "HtmlFileStream initialize"
+	TabThing _ String
+		with: (Character value: 1)
+		with: (Character value: 32)
+		with: (Character value: 1)
+		with: (Character value: 32)
