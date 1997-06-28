9a27e19b197237cee148b7e80d1f55e121a27350
di <unknown>
1997-06-28 21:26:00 +0000

    no message

--- FileModel/instance/accessing/fileIntoNewChangeSet.cs
+++ FileModel/instance/accessing/fileIntoNewChangeSet.cs
@@ -1,4 +1,6 @@
 fileIntoNewChangeSet
-	"FileIn all of the contents from the external file, into a new change set.  7/12/96 sw"
-
-	ChangeSorter newChangesFromFileStream: (FileStream oldFileNamed: self fullName)
+	"FileIn all of the contents from the external file, into a new change set."
+	| f |
+	f _ FileStream oldFileNamed: self fullName.
+	(self fileNameSuffix sameAs: 'html') ifTrue: [f _ f asHtml].
+	ChangeSorter newChangesFromFileStream: f
