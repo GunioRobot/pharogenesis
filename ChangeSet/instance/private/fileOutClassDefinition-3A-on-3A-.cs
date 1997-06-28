84738fa371e1383a33e026cb432c3b2b0c6be10d
di <unknown>
1997-06-28 20:34:00 +0000

    no message

--- ChangeSet/instance/private/fileOutClassDefinition-3A-on-3A-.cs
+++ ChangeSet/instance/private/fileOutClassDefinition-3A-on-3A-.cs
@@ -3,4 +3,4 @@ fileOutClassDefinition: class on: stream
 
 	((self atClass: class includes: #add) or: [self atClass: class includes: #change])
 		ifTrue:
-			[stream emphasis: 5; nextChunkPut: class definition; cr; emphasis: 1]
+			[stream command: 'H3'; nextChunkPut: class definition; cr; command: '/H3']
