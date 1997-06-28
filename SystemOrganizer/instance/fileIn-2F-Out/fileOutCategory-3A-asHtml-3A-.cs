868030cf5f3d81d7ec74f952d482733bb28caedd
di <unknown>
1997-06-28 19:00:00 +0000

    no message

---
+++ SystemOrganizer/instance/fileIn-2F-Out/fileOutCategory-3A-asHtml-3A-.cs
@@ -0,0 +1,8 @@
+fileOutCategory: category asHtml: useHtml
+	"FileOut all the classes in the named system category."
+	| fileStream |
+	fileStream _ useHtml
+		ifTrue: [(FileStream newFileNamed: category , '.html') asHtml]
+		ifFalse: [FileStream newFileNamed: category , '.st'].
+	self fileOutCategory: category on: fileStream.
+	fileStream close
