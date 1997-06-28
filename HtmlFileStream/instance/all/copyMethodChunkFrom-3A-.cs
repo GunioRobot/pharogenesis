3d976a89b1fbaca244bd2af7a340d5993db2eabb
di <unknown>
1997-06-28 08:35:00 +0000

    no message

---
+++ HtmlFileStream/instance/all/copyMethodChunkFrom-3A-.cs
@@ -0,0 +1,16 @@
+copyMethodChunkFrom: aStream
+	"Overridden to bolden the first line (presumably a method header)"
+	| terminator code firstLine |
+	terminator _ $!.
+	aStream skipSeparators.
+	code _ aStream upTo: terminator.
+	firstLine _ code copyUpTo: Character cr.
+	firstLine size = code size
+		ifTrue: [self nextPutAll: code]
+		ifFalse: [self command: 'b'; nextPutAll: firstLine; command: '/b'.
+				self nextPutAll: (code copyFrom: firstLine size + 1 to: code size)].
+	self nextPut: terminator.
+	[aStream peekFor: terminator] whileTrue:   "case of imbedded (doubled) terminators"
+			[self nextPut: terminator;
+				nextPutAll: (aStream upTo: terminator);
+				nextPut: terminator]
