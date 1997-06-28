9fee1a9e461c9e7c1ba60f142e9c66a91bfaa3e5
di <unknown>
1997-06-28 20:28:00 +0000

    no message

--- ClassListController/instance/menu messages/fileOut.cs
+++ ClassListController/instance/menu messages/fileOut.cs
@@ -1,5 +1,5 @@
 fileOut
-	"Print a description of the selected class onto an external file."
+	"Print a description of the selected class onto an external file in .st format."
 
 	self controlTerminate.
 	Cursor write showWhile:
