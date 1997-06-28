0d4facb920b716558fd3d74a7e9ae46685c3aa6e
di <unknown>
1997-06-28 20:27:00 +0000

    no message

--- ClassListController/instance/menu messages/printOut.cs
+++ ClassListController/instance/menu messages/printOut.cs
@@ -1,6 +1,7 @@
 printOut
-	"Make a file with the description of the selected mesage category.
-	Defaults to the same file as fileOut, but could be changed in any given
-	implementation to have a prettier format."
+	"Print a description of the selected class onto an external file in HTML format."
 
-	self fileOut
+	self controlTerminate.
+	Cursor write showWhile:
+		[model printOutClass].
+	self controlInitialize
