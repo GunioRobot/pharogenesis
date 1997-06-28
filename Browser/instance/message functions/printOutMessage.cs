a8ed8b0c456e9192a5d817503f006e2a65ee7bba
di <unknown>
1997-06-28 15:53:00 +0000

    no message

---
+++ Browser/instance/message functions/printOutMessage.cs
@@ -0,0 +1,5 @@
+printOutMessage
+	"Print a description of the selected message"
+	messageListIndex = 0 ifTrue: [^ self].
+	self selectedClassOrMetaClass fileOutMethod: self selectedMessageName
+							asHtml: true
