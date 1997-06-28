f9c3a81d47edfb4c110e0f1fed7d53e9d04c9742
di <unknown>
1997-06-28 21:25:00 +0000

    no message

--- FileModel/instance/accessing/fileAllIn.cs
+++ FileModel/instance/accessing/fileAllIn.cs
@@ -2,4 +2,5 @@ fileAllIn
 	"FileIn all of the contents from the external file"
 	| f |
 	f _ FileStream oldFileNamed: self fullName.
+	(self fileNameSuffix sameAs: 'html') ifTrue: [f _ f asHtml].
 	f fileIn
