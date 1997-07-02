22adda8a2078a51c5b2ef48a2d303110b2af41c4
tk <unknown>
1997-07-02 08:02:00 +0000

    no message

---
+++ Morph/instance/event handling/mouseUpBalk-3A-.cs
@@ -0,0 +1,4 @@
+mouseUpBalk: evt
+	"May be sent to me when a user moves out of a button I own and then lets up the mouse.  For radion buttons, I need to select some button.  tk 7/2/97 08:02"
+
+	"do nothing as default"
