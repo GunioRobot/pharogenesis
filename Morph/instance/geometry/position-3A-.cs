02c00a42d15174df0576ed14b28d0e77dcdb6586
jm <unknown>
1997-07-05 13:35:00 +0000

    no message

---
+++ Morph/instance/geometry/position-3A-.cs
@@ -0,0 +1,9 @@
+position: aPoint
+	"Change the position of this morph and and all of its submorphs."
+
+	| delta |
+	delta _ aPoint truncated - bounds topLeft.
+	(delta x = 0 and: [delta y = 0]) ifTrue: [^ self].  "Null change"
+	self changed.
+	self privateFullMoveBy: delta.
+	self changed.
