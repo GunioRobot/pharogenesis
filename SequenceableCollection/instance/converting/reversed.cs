d45e368fba0656256563f9bd22729e68d89efcb9
di <unknown>
1997-07-07 09:51:00 +0000

    no message

--- SequenceableCollection/instance/converting/reversed.cs
+++ SequenceableCollection/instance/converting/reversed.cs
@@ -1,10 +1,8 @@
 reversed
-	"Answer a copy of the receiver with element order reversed.  1/26/96 sw"
-	| newCol |
-	newCol _ self species new.
-	self reverseDo:
-		[:elem | newCol add: elem].
-	^ newCol
-
-
-"#(2 3 4 'fred') reversed"
+	"Answer a copy of the receiver with element order reversed."
+	| reversal strm |
+	reversal _ self species new: self size.
+	strm _ WriteStream on: reversal.
+	self reverseDo: [:elem | strm nextPut: elem].
+	^ reversal
+" 'frog' reversed "
