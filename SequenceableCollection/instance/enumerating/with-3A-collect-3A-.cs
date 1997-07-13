0439fb4e52f6050ec4687d8d506d9b1b668a7ef6
di <unknown>
1997-07-13 09:30:00 +0000

    no message

---
+++ SequenceableCollection/instance/enumerating/with-3A-collect-3A-.cs
@@ -0,0 +1,10 @@
+with: otherCollection collect: twoArgBlock 
+	"Collect and return the result of evaluating twoArgBlock with corresponding elements from this collection and otherCollection."
+	| result |
+	result _ self species new: self size.
+	1 to: self size do:
+		[:index | result at: index put:
+		(twoArgBlock
+			value: (self at: index)
+			value: (otherCollection at: index))].
+	^ result
