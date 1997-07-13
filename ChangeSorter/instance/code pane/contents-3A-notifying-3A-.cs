cc1156c92cf516644ac5e850020c7016f8170bb6
di <unknown>
1997-07-13 11:15:00 +0000

    no message

--- ChangeSorter/instance/code pane/contents-3A-notifying-3A-.cs
+++ ChangeSorter/instance/code pane/contents-3A-notifying-3A-.cs
@@ -3,21 +3,14 @@ contents: aString notifying: aController
 	Create an error if the category of the selected message is unknown. 
 	Answer false if the compilation fails. Otherwise, if the compilation 
 	created a new method, deselect the current selection. Then answer true."
-	| category selector class oldSelector notice |
+	| category selector class oldSelector |
 	messageList listIndex = 0 ifTrue: [^ false].
 	class _ self selectedClassOrMetaClass.
 	oldSelector _ self selectedMessageName.
 	category _ class organization categoryOfElement: oldSelector.
-	selector _ class
-				compile: aString
+	selector _ class compile: aString
 				classified: category
 				notifying: aController.
 	selector == nil ifTrue: [^false].
 	selector == oldSelector ifFalse: [self changed: #message].
-	notice _ class checkForPerform: selector in: aController.
-	notice size = 0 ifFalse: ["insert the notice"
-			aController notify: notice
-				at: contents size + 1
-				in: nil.
-			self lock  "code is dirty"].
-	^true
+	^ true
