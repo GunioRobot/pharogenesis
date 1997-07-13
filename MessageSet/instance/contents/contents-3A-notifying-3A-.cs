2461da49ca86e0433b756a81413f88a915bbf3ea
di <unknown>
1997-07-13 11:15:00 +0000

    no message

--- MessageSet/instance/contents/contents-3A-notifying-3A-.cs
+++ MessageSet/instance/contents/contents-3A-notifying-3A-.cs
@@ -3,20 +3,13 @@ contents: aString notifying: aController
 	Create an error if the category of the selected message is unknown. 
 	Answer false if the compilation fails. Otherwise, if the compilation 
 	created a new method, deselect the current selection. Then answer true."
-	| category selector class oldSelector notice |
+	| category selector |
 	messageListIndex = 0 ifTrue: [^ false].
 	self setClassAndSelectorIn: [:class :oldSelector].
 	category _ class organization categoryOfElement: oldSelector.
-	selector _ class
-				compile: aString
+	selector _ class compile: aString
 				classified: category
 				notifying: aController.
 	selector == nil ifTrue: [^false].
 	selector == oldSelector ifFalse: [self messageListIndex: 0].
-	notice _ class checkForPerform: selector in: aController.
-	notice size = 0 ifFalse: ["insert the notice"
-			aController notify: notice
-				at: contents size + 1
-				in: nil.
-			self lock  "code is dirty"].
-	^true
+	^ true
