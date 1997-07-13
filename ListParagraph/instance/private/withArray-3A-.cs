86fbc1f3e955774732ea21fe7b128c46de76f29f
di <unknown>
1997-07-13 16:56:00 +0000

    no message

--- ListParagraph/instance/private/withArray-3A-.cs
+++ ListParagraph/instance/private/withArray-3A-.cs
@@ -1,6 +1,6 @@
 withArray: anArray 
 	"Modifies self to contain the list of strings in anArray"
-	| startOfLine endOfLine lineIndex aString item interval |
+	| startOfLine endOfLine lineIndex aString |
 	lines _ Array new: 20.
 	lastLine _ 0.
 	startOfLine _ 1.
@@ -11,11 +11,10 @@ withArray: anArray
 		endOfLine _ startOfLine + item size.		"this computation allows for a cr after each line..."
 												"...but later we will adjust for no cr after last line"
 		lineIndex _ lineIndex + 1.
-		self lineAt: lineIndex put: (TextLineInterval
-				start: startOfLine
-				stop: endOfLine
-				internalSpaces: 0
-				paddingWidth: 0).
+		self lineAt: lineIndex put:
+			((TextLineInterval start: startOfLine stop: endOfLine
+				internalSpaces: 0 paddingWidth: 0)
+				lineHeight: textStyle lineGrid baseline: textStyle baseline).
 		startOfLine _ endOfLine + 1].
 	endOfLine _ endOfLine - 1.		"endOfLine is now the total size of the text"
 	self trimLinesTo: lineIndex.
@@ -25,9 +24,12 @@ withArray: anArray
 		aString
 			replaceFrom: interval first
 			to: interval last - 1
-			with: item
+			with: item asString
 			startingAt: 1.
 		interval last <= endOfLine ifTrue: [aString at: interval last put: Character cr]].
 	lineIndex > 0 ifTrue: [(lines at: lineIndex) stop: endOfLine].	"adjust for no cr after last line"
 	self text: aString asText.
+	anArray with: lines do: 
+		[:item :interval |  item isText ifTrue:
+			[text replaceFrom: interval first to: interval last - 1 with: item]].
 	self updateCompositionHeight
