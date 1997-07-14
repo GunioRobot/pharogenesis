3f8ea0c86f55400c2269efcecc22263865802937
di <unknown>
1997-07-14 22:57:00 +0000

    no message

--- ReadWriteStream/instance/fileIn-2F-Out/timeStamp.cs
+++ ReadWriteStream/instance/fileIn-2F-Out/timeStamp.cs
@@ -1,10 +1,5 @@
 timeStamp
-	"Append the current time to the receiver."
-
-	| aStream |
-	aStream _ WriteStream on: (String new: 16).
-	Smalltalk timeStamp: aStream.
-	self command: 'H2'.
-	self nextChunkPut: aStream contents printString.	"double quotes and !s"
-	self command: '/H2'.
-	self cr; cr
+	"Append the current time to the receiver as a String."
+	self nextChunkPut:	"double string quotes and !s"
+		(String streamContents: [:s | Smalltalk timeStamp: s]) printString.
+	self cr
