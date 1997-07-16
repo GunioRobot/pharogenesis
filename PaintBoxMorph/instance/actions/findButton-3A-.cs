2f6c8eaae5f518219b6c0c6af1d9b7c39fc41ed2
tk <unknown>
1997-07-16 18:45:00 +0000

    no message

---
+++ PaintBoxMorph/instance/actions/findButton-3A-.cs
@@ -0,0 +1,12 @@
+findButton: aSelector
+	"Find this button in me"
+
+	submorphs do: [:button |
+		(button respondsTo: #arguments) 
+			ifTrue: [(button arguments at: 2) == aSelector ifTrue: [^ button]]
+			ifFalse: [(button isKindOf: LayoutMorph) ifTrue: [
+				button submorphsDo: [:sub |
+					(sub respondsTo: #arguments) 
+						ifTrue: [(sub arguments at: 2) == aSelector ifTrue: [^ sub]]]]].
+			].
+	^ nil
