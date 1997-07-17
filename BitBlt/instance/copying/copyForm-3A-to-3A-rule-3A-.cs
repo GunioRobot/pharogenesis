8fbe4394c20582d29fc706a16715d2114b2ee9ae
di <unknown>
1997-07-17 10:04:00 +0000

    no message

--- BitBlt/instance/copying/copyForm-3A-to-3A-rule-3A-.cs
+++ BitBlt/instance/copying/copyForm-3A-to-3A-rule-3A-.cs
@@ -1,11 +1,3 @@
 copyForm: srcForm to: destPt rule: rule
-	sourceForm _ srcForm.
-	halftoneForm _ nil.
-	combinationRule _ rule.
-	destX _ destPt x + sourceForm offset x.
-	destY _ destPt y + sourceForm offset y.
-	sourceX _ 0.
-	sourceY _ 0.
-	width _ sourceForm width.
-	height _ sourceForm height.
-	self copyBits
+	^ self copyForm: srcForm to: destPt rule: rule
+		colorMap: (srcForm colormapIfNeededForDepth: destForm depth)
