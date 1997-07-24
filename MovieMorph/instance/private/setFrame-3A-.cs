986bdb414439a9ac32f79a51920673c2841231ed
jm <unknown>
1997-07-24 15:05:00 +0000

    no message

---
+++ MovieMorph/instance/private/setFrame-3A-.cs
@@ -0,0 +1,21 @@
+setFrame: newFrameIndex
+
+	| oldFrame p newFrame |
+	oldFrame _ self currentFrame.
+	oldFrame ifNil: [^ self].
+
+	self changed.
+	p _ oldFrame referencePosition.
+	currentFrameIndex _ newFrameIndex.
+	currentFrameIndex > frameList size
+		ifTrue: [currentFrameIndex _ frameList size].
+	currentFrameIndex < 1
+		ifTrue: [currentFrameIndex _ 1].
+	newFrame _ frameList at: currentFrameIndex.
+	newFrame rotationDegrees: rotationDegrees scalePoint: scalePoint.
+	newFrame referencePosition: p.
+	oldFrame delete.
+	self addMorph: newFrame.
+	dwellCount _ newFrame framesToDwell.
+	self layoutChanged.
+	self changed.
