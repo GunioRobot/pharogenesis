e210b5c0012a3437e791e0022ee957ecabe43c90
jm <unknown>
1997-07-28 14:58:00 +0000

    no message

---
+++ SketchEditorMorph/instance/actions & preps/ellipse-3A-.cs
@@ -0,0 +1,17 @@
+ellipse: evt
+	"Draw an ellipse from the center. "
+
+	| rect oldRect ww ext oldExt |
+	ext _ (strokeOrigin - evt cursorPoint) abs * 2.
+	evt shiftPressed ifTrue: [ext _ ext r].
+	rect _ Rectangle center: strokeOrigin extent: ext.
+	ww _ palette getNib width.
+	lastEvent ifNotNil: [
+		oldExt _ (strokeOrigin - lastEvent cursorPoint) abs + ww * 2.
+		lastEvent shiftPressed ifTrue: [oldExt _ oldExt r].
+		(oldExt < ext) ifFalse: ["Last draw sticks out, must erase the area"
+			oldRect _ Rectangle center: strokeOrigin extent: oldExt.
+			self restoreRect: oldRect]].
+	formCanvas fillOval: rect color: currentColor 
+		borderWidth: ww borderColor: Color black.
+	self invalidRect: rect.
