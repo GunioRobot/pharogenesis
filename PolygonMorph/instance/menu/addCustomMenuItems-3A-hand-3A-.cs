58e9f025e4c1dbd63653a4a3e022af38d914364d
tk <unknown>
1997-07-28 23:04:00 +0000

    no message

---
+++ PolygonMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -0,0 +1,11 @@
+addCustomMenuItems: aCustomMenu hand: aHandMorph
+	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
+	handles == nil ifTrue: [aCustomMenu add: 'show handles' action: #addHandles]
+		ifFalse: [aCustomMenu add: 'hide handles' action: #removeHandles].
+	closed ifTrue: [aCustomMenu add: 'open polygon' action: #makeOpen]
+		ifFalse:
+		[aCustomMenu add: 'close polygon' action: #makeClosed.
+		arrows == #none ifFalse: [aCustomMenu add: '---' action: #makeNoArrows].
+		arrows == #forward ifFalse: [aCustomMenu add: '-->' action: #makeForwardArrow].
+		arrows == #back ifFalse: [aCustomMenu add: '<--' action: #makeBackArrow].
+		arrows == #both ifFalse: [aCustomMenu add: '<-->' action: #makeBothArrows]]
