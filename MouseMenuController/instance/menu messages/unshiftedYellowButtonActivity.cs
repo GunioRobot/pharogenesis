7275c883206abcba97fd13f3aacf7bd8755945da
sw <unknown>
1997-07-29 17:37:00 +0000

    no message

--- MouseMenuController/instance/menu messages/unshiftedYellowButtonActivity.cs
+++ MouseMenuController/instance/menu messages/unshiftedYellowButtonActivity.cs
@@ -1,11 +1,10 @@
 unshiftedYellowButtonActivity
-	"Put up the regular yellow-button menu and take action as appropriate.  1/24/96 sw"
+	"Put up the regular yellow-button menu and take action as appropriate"
 
 	| index  |
-
 	yellowButtonMenu ~~ nil
 		ifTrue: 
-			[index _ yellowButtonMenu startUpYellowButton.
+			[index _ self startUpYellowButtonMenu.
 			index ~= 0 
 				ifTrue: [self menuMessageReceiver performMenuMessage:
 							(yellowButtonMessages at: index)]]
