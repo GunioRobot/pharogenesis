1e9c5b603fc70aea7dacc6501fb8ab4f7044c286
sw <unknown>
1997-07-30 18:44:00 +0000

    no message

--- ClassDescription/instance/initialize-2D-release/subclassOf-3A-oldClass-3A-instanceVariableNames-3A-variable-3A-words-3A-pointers-3A-ifBad-3A-.cs
+++ ClassDescription/instance/initialize-2D-release/subclassOf-3A-oldClass-3A-instanceVariableNames-3A-variable-3A-words-3A-pointers-3A-ifBad-3A-.cs
@@ -2,15 +2,14 @@ subclassOf: newSuper oldClass: oldClass instanceVariableNames: newInstVarString 
 	"Basic initialization message for creating classes using the information 
 	provided as arguments. Answer whether old instances will be 
 	invalidated."
-
 	| oldNames newNames usedNames invalid oldSuperMeta newInstVarArray oldSpec |
 	oldNames _ self allInstVarNames.
 	usedNames _ #(self super thisContext true false nil ) asSet.
 	newInstVarArray _ Scanner new scanFieldNames: newInstVarString.
 	newNames _ newSuper allInstVarNames , newInstVarArray.
-	newNames size > 62
-		ifTrue: [self error: 'A class cannot have more than 62 instance variables'.
-				^ badBlock value].
+	newNames size > 254 ifTrue:
+		[self error: 'A class cannot have more than 254 instance variables'.
+		^ badBlock value].
 	newNames do: 
 		[:fieldName | 
 		(usedNames includes: fieldName)
@@ -22,19 +21,16 @@ subclassOf: newSuper oldClass: oldClass instanceVariableNames: newInstVarString 
 		ifTrue: 
 			["superclass changed"
 			oldSuperMeta _ superclass class.
-			superclass removeSubclass: self.
+			superclass ifNotNil: [superclass removeSubclass: self].
 			superclass _ newSuper.
 			superclass addSubclass: self.
 			self class superclass == oldSuperMeta 
 				ifTrue: ["Only false when self is a metaclass"
 						self class superclass: newSuper class]].
 	instanceVariables _ newInstVarArray size = 0 ifFalse: [newInstVarArray].
-	invalid _ invalid |   "field names changed"
-			(newNames size < oldNames size or:
-				[(newNames copyFrom: 1 to: oldNames size) ~= oldNames]).
+	invalid _ invalid | (newNames  ~= oldNames).   "field names changed"
 	oldSpec _ self instSpec.
-	self
-		format: newNames size
+	self format: newNames size
 		variable: v
 		words: w
 		pointers: p.
