51e4b5f2e78d76580acc4b5f57aad6db07daa139
jm <unknown>
1997-07-30 17:36:00 +0000

    no message

---
+++ SketchEditorMorph/instance/actions & preps/pickup-3A-.cs
@@ -0,0 +1,12 @@
+pickup: evt
+	"Grab a part of the picture (or screen) and store it in a known place.  Note where we started.  Use a rubberband rectangle to show what is being selected."
+
+	| rect oldRect |
+	rect _ strokeOrigin rect: evt cursorPoint + (14@14).
+	lastEvent == nil ifFalse: [
+			"Last draw will stick out, must erase the area"
+			oldRect _ strokeOrigin rect: lastEvent cursorPoint + (14@14).
+			self restoreRect: oldRect].
+	formCanvas frameAndFillRectangle: rect fillColor: Color transparent
+		borderWidth: 2 borderColor: Color gray.
+	self invalidRect: rect.
