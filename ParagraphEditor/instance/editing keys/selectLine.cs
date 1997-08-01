d0e393d072ecf5120d693b2f6f84723958e9211b
sw <unknown>
1997-08-01 16:14:00 +0000

    no message

--- ParagraphEditor/instance/editing keys/selectLine.cs
+++ ParagraphEditor/instance/editing keys/selectLine.cs
@@ -1,5 +1,5 @@
 selectLine
-	"Make the receiver's selection, if it currently consists of an insertion point only, encompass the current line. 2/29/96 sw"
+	"Make the receiver's selection, if it currently consists of an insertion point only, encompass the current line."
 
 	| string left right |
 
@@ -12,4 +12,4 @@ selectLine
 		[left _ left - 1].
 	[right < string size and: [(string at: (right + 1)) ~= Character cr]] whileTrue:
 		[right _ right + 1].
-	self selectFrom: left to: (right + 1 min: string size)
+	self selectFrom: left to: (right min: string size)
