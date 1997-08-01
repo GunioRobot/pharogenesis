ab6a77122be7d37d4655aab6bc5c0d9d809aa75c
sw <unknown>
1997-08-01 16:33:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/cut.cs
+++ ParagraphEditor/instance/menu messages/cut.cs
@@ -1,9 +1,7 @@
 cut
-	"Cut out the current selection and redisplay the paragraph if necessary.  Undoer & Redoer: undoCutCopy:.
-	2/29/96 sw: select line first, if selection was an insertion point"
+	"Cut out the current selection and redisplay the paragraph if necessary.  Undoer & Redoer: undoCutCopy:"
 
-	self selectLine.
-	startBlock = stopBlock ifTrue: [^ view flash].
+	self lineSelectAndEmptyCheck: [^ self].
 
 	self replaceSelectionWith: self nullText. 
 	self undoer: #undoCutCopy: with: self clipboardText.
