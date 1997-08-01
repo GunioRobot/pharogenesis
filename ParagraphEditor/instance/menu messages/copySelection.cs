bea39bc779ba6e60cb3c28d1ef3b367e8398b313
sw <unknown>
1997-08-01 15:09:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/copySelection.cs
+++ ParagraphEditor/instance/menu messages/copySelection.cs
@@ -1,9 +1,7 @@
 copySelection
-	"Copy the current selection and store it in the paste buffer, unless a caret.  Undoer & Redoer: undoCutCopy:.
-	 2/29/96 sw: select line first, if selection was an insertion point"
+	"Copy the current selection and store it in the paste buffer, unless a caret.  Undoer & Redoer: undoCutCopy"
 
-	self selectLine.
-	startBlock = stopBlock ifTrue: [^ view flash.].
+	self lineSelectAndEmptyCheck: [^ self].
 
 	"Simulate 'substitute: self selection' without locking the controller"
 	UndoSelection _ self selection.
