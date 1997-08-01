58f825eadadafa0323ff0cbe94ab4c4765ae183e
jm <unknown>
1997-08-01 17:04:00 +0000

    no message

---
+++ HandMorph/instance/meta menu/grabDrawingFromScreen.cs
@@ -0,0 +1,6 @@
+grabDrawingFromScreen
+	"Allow the user to specify a rectangular area of the Display, capture the pixels from that area, and use them to create a new drawing morph. Attach the result to the hand."
+
+	| m |
+	m _ self drawingClass new form: Form fromUser.
+	self attachMorph: m.
