22c9b710384a82dbb1266ab2de297cc26a52741b
sw <unknown>
1997-08-01 16:12:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/setSearchString.cs
+++ ParagraphEditor/instance/menu messages/setSearchString.cs
@@ -1,5 +1,4 @@
 setSearchString
-	"Make the current selection, if any, be the current search string.  2/29/96 sw"
-
+	"Make the current selection, if any, be the current search string."
 	startBlock = stopBlock ifTrue: [view flash. ^ self].
 	self setSearch:  self selection string
