a15efad50e4b498b93726f6a4e9842d9ad0100f3
sw <unknown>
1997-08-01 15:07:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/implementorsOfIt.cs
+++ ParagraphEditor/instance/menu messages/implementorsOfIt.cs
@@ -1,10 +1,7 @@
 implementorsOfIt
-	"Open an implementors browser on the selected selector.  1/8/96 sw.
-	1/18/96 sw: converted to use selectedSelector
-	2/29/96 sw: select current line if selection is insertion point"
+	"Open an implementors browser on the selected selector"
 
 	| aSelector |
-	self selectLine.
-	startBlock = stopBlock ifTrue: [view flash.  ^ self].
+	self lineSelectAndEmptyCheck: [^ self].
 	(aSelector _ self selectedSelector) == nil ifTrue: [^ view flash].
 	self terminateAndInitializeAround: [Smalltalk browseAllImplementorsOf: aSelector]
