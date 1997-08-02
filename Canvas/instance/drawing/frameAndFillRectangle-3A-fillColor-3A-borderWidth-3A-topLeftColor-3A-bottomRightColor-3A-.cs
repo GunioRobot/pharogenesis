c8d9b3b238a2fbb81ccd8c8b506b0f43a13cea57
jm <unknown>
1997-08-02 13:54:00 +0000

    no message

---
+++ Canvas/instance/drawing/frameAndFillRectangle-3A-fillColor-3A-borderWidth-3A-topLeftColor-3A-bottomRightColor-3A-.cs
@@ -0,0 +1,2 @@
+frameAndFillRectangle: r fillColor: fillColor borderWidth: borderWidth topLeftColor: topLeftColor bottomRightColor: bottomRightColor
+	"Noop here; overridden by non-trivial canvases."
