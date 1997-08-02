9b5f8807c4ba15aaba9e471475d7efa81a1e85f9
jm <unknown>
1997-08-02 14:21:00 +0000

    no message

---
+++ SketchEditorMorph/instance/actions & preps/star-3A-.cs
@@ -0,0 +1,28 @@
+star: evt
+	"Draw an star from the center. "
+
+	| poly ext ww rect oldExt oldRect oldR verts pt |
+	ww _ palette getNib width.
+	ext _ (pt _ strokeOrigin - evt cursorPoint) r + ww * 2.
+	rect _ Rectangle center: strokeOrigin extent: ext.
+	ww _ palette getNib width.
+	lastEvent ifNotNil: [
+		oldExt _ (strokeOrigin - lastEvent cursorPoint) r + ww * 2.
+		"Last draw sticks out, must erase the area"
+		oldRect _ Rectangle center: strokeOrigin extent: oldExt.
+		self restoreRect: oldRect].
+	ext _ pt r.
+	oldR _ ext.
+	verts _ (0 to: 350 by: 36) collect: [:angle |
+		(Point r: (oldR _ oldR = ext ifTrue: [ext*5//12] ifFalse: [ext]) degrees: angle + pt degrees)
+			+ strokeOrigin].
+	
+	poly _ PolygonMorph new addHandles.
+	poly color: currentColor; borderWidth: ww; borderColor: Color black.
+	"self addMorph: poly."
+	poly privateOwner: self.
+	poly bounds: (strokeOrigin extent: ext).
+	poly setVertices: verts.
+	poly drawOn: formCanvas.
+	"poly delete."
+	self invalidRect: rect.
