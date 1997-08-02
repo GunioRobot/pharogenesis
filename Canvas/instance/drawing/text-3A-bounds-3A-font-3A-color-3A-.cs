bdb725435f74b3e187d451df30da48694c1a6863
jm <unknown>
1997-08-02 13:54:00 +0000

    no message

---
+++ Canvas/instance/drawing/text-3A-bounds-3A-font-3A-color-3A-.cs
@@ -0,0 +1,2 @@
+text: s bounds: boundsRect font: fontOrNil color: c
+	"Draw the given string in the given font and color clipped to the given rectangle. If the font is nil, the default font is used. Noop here; overridden by non-trivial canvases."
