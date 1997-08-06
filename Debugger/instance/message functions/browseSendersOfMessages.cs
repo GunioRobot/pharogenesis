4b32d41820cf943085fe7329bfd978423669f7ec
sw <unknown>
1997-08-06 13:46:00 +0000

    no message

--- Debugger/instance/message functions/browseSendersOfMessages.cs
+++ Debugger/instance/message functions/browseSendersOfMessages.cs
@@ -2,9 +2,7 @@ browseSendersOfMessages
 	"Show a menu of all messages sent by the currently selected message. 
 	Create and schedule a message set browser of all senders of the 
 	message chosen. Do nothing if no message is chosen.  Derived from
-    browseMessages, 1/8/96 sw"
+    browseMessages,"
 
-	contextStackIndex = 0 ifTrue: [^self].
-	Smalltalk showMenuThenBrowseSendersOf:
-		(self selectedClassOrMetaClass compiledMethodAt: self selectedMessageName)
-			messages asSortedCollection
+	contextStackIndex = 0 ifTrue: [^ self].
+	Smalltalk showMenuThenBrowseSendersOf: self currentCompiledMethod messages asSortedCollection
