90ee7c88586bf76e0f91aea8295d0b2c3912cd9c
sw <unknown>
1997-08-12 22:15:00 +0000

    no message

---
+++ ParagraphEditor/instance/editing keys/setEmphasis-3A-.cs
@@ -0,0 +1,13 @@
+setEmphasis: emphasisSymbol
+	"Change the emphasis of the current selection."
+
+	| oldAttributes attribute |
+	oldAttributes _ paragraph text attributesAt: startBlock stringIndex.
+	(emphasisSymbol == #plain) 
+		ifTrue:
+			[attribute _ TextEmphasis normal]
+		ifFalse:
+			[attribute _ TextEmphasis perform: emphasisSymbol.
+			oldAttributes do:
+				[:att | (att dominates: attribute) ifTrue: [attribute turnOff]]].
+	self replaceSelectionWith: (self selection addAttribute: attribute)
