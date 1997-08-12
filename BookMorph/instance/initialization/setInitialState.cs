85ee8bb667c3cea11732384606ecb948b0511053
sw <unknown>
1997-08-12 21:31:00 +0000

    no message

---
+++ BookMorph/instance/initialization/setInitialState.cs
@@ -0,0 +1,10 @@
+setInitialState
+	orientation _ #vertical.
+	centering _ #topLeft.
+	hResizing _ #shrinkWrap.
+	vResizing _ #shrinkWrap.
+	inset _ 5.
+	color _ Color white.
+	pageSize _ 160@300.
+	openToDragNDrop _ true.
+	copyContents _ false.
