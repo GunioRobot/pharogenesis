b40944d2181fccce3543e11096238529f0f693aa
sw <unknown>
1997-08-15 17:18:00 +0000

    no message

--- StandardSystemView/instance/framing/fullScreen.cs
+++ StandardSystemView/instance/framing/fullScreen.cs
@@ -1,5 +1,5 @@
 fullScreen
-	"Expand the receiver to fill the screen (except for modest allowances).  6/6/96 sw"
+	"Expand the receiver to fill the screen.  Let the model decide how big is full -- allows for flop-out scrollbar on left if desired"
 
 	self isCollapsed ifFalse:
-		[self reframeTo: (Rectangle origin: (40@2) extent: (DisplayScreen actualScreenSize - (42@4)))]
+		[self reframeTo: model fullScreenSize]
