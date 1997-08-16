1ce8044c431fa127742d4c40eae12713f5c3f15c
sw <unknown>
1997-08-16 17:16:00 +0000

    no message

---
+++ Morph/instance/aug97 additions/unlockOneSubpart.cs
@@ -0,0 +1,9 @@
+unlockOneSubpart
+	| unlockables aMenu reply |
+	unlockables _ self submorphs select:
+		[:m | m isLocked].
+	unlockables size <= 1 ifTrue: [^ self unlockContents].
+	aMenu _ SelectionMenu labelList: (unlockables collect: [:m | m externalName]) selections: unlockables.
+	reply _ aMenu startUpWithCaption: 'Who should be be unlocked?'.
+	reply == nil ifTrue: [^ self].
+	reply unlock
