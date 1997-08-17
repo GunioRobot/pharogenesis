cc102bbe64b5c93664e882f39427b57f7d713030
sw <unknown>
1997-08-17 18:28:00 +0000

    no message

---
+++ WorldMorph/instance/painting support/paintAreaFor-3A-.cs
@@ -0,0 +1,4 @@
+paintAreaFor: aSketchMorph
+	"What rectangle should the the painting arena for painting aSketchMorph.  Overridden in EToyWorld; here, we just defer to the old way that takes no account of the sketch morh"
+
+	^ self paintArea
