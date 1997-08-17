d9d487043a1d13bc648eea74e7358acda7a0dfd7
ikp <unknown>
1997-08-17 13:28:00 +0000

    no message

---
+++ DynamicInterpreter/instance/message sending/executeForNormalReceiver.cs
@@ -0,0 +1,9 @@
+executeForNormalReceiver
+	self inline: true.
+
+	(primitiveIndex = 0 or: [self primitiveResponse not]) ifTrue: [
+		"if not primitive, or primitive failed, activate the method"
+		self activateNewMethod.
+		"check for possible interrupts at each real send"
+		self quickCheckForInterrupts.
+	].
