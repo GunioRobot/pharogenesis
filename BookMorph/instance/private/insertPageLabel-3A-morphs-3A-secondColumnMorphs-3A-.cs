b0a89db312f8b8e0bc5c92c84fa727f3cce3b8bc
jm <unknown>
1997-08-21 18:10:00 +0000

    no message

---
+++ BookMorph/instance/private/insertPageLabel-3A-morphs-3A-secondColumnMorphs-3A-.cs
@@ -0,0 +1,10 @@
+insertPageLabel: labelString morphs: firstColMorphs secondColumnMorphs: secondColMorphs
+
+	| c |
+	self insertPageLabel: labelString morphs: firstColMorphs.
+
+	"use a column to align the given morphs, then add them to the page"
+	c _ LayoutMorph newColumn centering: #center.
+	c addAllMorphs: secondColMorphs.
+	c position: currentPage position + (100@40).
+	currentPage addAllMorphs: secondColMorphs.
