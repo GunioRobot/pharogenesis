2c0886f5ced44ee8faf36ddd4ff7d70ba61777fe
sn <unknown>
1997-08-21 22:15:00 +0000

    no message

--- ContextPart/instance/instruction decoding/send-3A-super-3A-numArgs-3A-.cs
+++ ContextPart/instance/instruction decoding/send-3A-super-3A-numArgs-3A-.cs
@@ -5,11 +5,18 @@ send: selector super: superFlag numArgs: numArgs
 	of the message are found in the top numArgs locations on the stack and 
 	the receiver just below them."
 
-	| receiver arguments |
+	| receiver arguments answer |
 	arguments _ Array new: numArgs.
 	numArgs to: 1 by: -1 do: [ :i | arguments at: i put: self pop].
 	receiver _ self pop.
 	(selector == #halt or: [selector == #halt:]) ifTrue:
 		[self error: 'Cant simulate halt.  Proceed to bypass it.'.
 		self push: nil. ^self].
+	selector == #doPrimitive:receiver:args:
+		ifTrue: [answer _ receiver 
+					doPrimitive: (arguments at: 1)
+					receiver: (arguments at: 2)
+					args: (arguments at: 3).
+				self push: answer.
+				^self].
 	^self send: selector to: receiver with: arguments super: superFlag
