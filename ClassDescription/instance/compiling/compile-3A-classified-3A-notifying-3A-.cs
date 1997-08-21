04329996771cde673cb4cd257de6c11cf9d70b83
sw <unknown>
1997-08-21 00:26:00 +0000

    no message

--- ClassDescription/instance/compiling/compile-3A-classified-3A-notifying-3A-.cs
+++ ClassDescription/instance/compiling/compile-3A-classified-3A-notifying-3A-.cs
@@ -1,19 +1,7 @@
-compile: text classified: category notifying: requestor 
-	| selector dict priorMethod method |
-	method _ self
-		compile: text asString
-		notifying: requestor
-		trailer: #(0 0 0 )
-		ifFail: [^nil]
-		elseSetSelectorAndNode: 
-			[:sel :node | selector _ sel.
-			priorMethod _ methodDict at: selector ifAbsent: [nil]].
-	(SourceFiles isNil or: [(SourceFiles at: 2) == nil])
-		ifFalse: [self acceptsLoggingOfCompilation
-					ifTrue:
-						[method
-						putSource: text asString
-						class: self category: category
-						inFile: 2 priorMethod: priorMethod]].
-	self organization classify: selector under: category.
-	^selector
+compile: text classified: category notifying: requestor
+	| stamp |
+	stamp _ self acceptsLoggingOfCompilation ifTrue: [Utilities changeStamp] ifFalse: [nil].
+	^ self compile: text classified: category
+		withStamp: stamp notifying: requestor
+
+ 
