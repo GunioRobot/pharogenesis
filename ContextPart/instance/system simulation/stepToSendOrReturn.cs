b5a5cfcf70c0b9cc57800caa454366ebc0331cc5
sn <unknown>
1997-08-22 21:55:00 +0000

    no message

--- ContextPart/instance/system simulation/stepToSendOrReturn.cs
+++ ContextPart/instance/system simulation/stepToSendOrReturn.cs
@@ -2,5 +2,5 @@ stepToSendOrReturn
 	"Simulate the execution of bytecodes until either sending a message or 
 	returning a value to the receiver (that is, until switching contexts)."
 
-	[self willSend | self willReturn]
+	[self willReallySend | self willReturn]
 		whileFalse: [self step]
