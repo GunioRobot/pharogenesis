2981de608c1049aff80591a7c489372ecbe41610
tk <unknown>
1997-09-02 15:55:00 +0000

    no message

---
+++ SketchMorph/instance/accessing/forwardDirection-3A-.cs
@@ -0,0 +1,5 @@
+forwardDirection: degFromUp
+	"The direction I will go when issued a sent forward:.  Up is zero.
+Clockwise like a compass."
+
+self setProperty: #forwardDirection toValue: degFromUp
