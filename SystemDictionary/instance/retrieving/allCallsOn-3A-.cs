85f69945e392c1508dcd2d3002e1d03dd8fffcb5
di <unknown>
1997-09-05 16:22:00 +0000

    no message

--- SystemDictionary/instance/retrieving/allCallsOn-3A-.cs
+++ SystemDictionary/instance/retrieving/allCallsOn-3A-.cs
@@ -1,12 +1,21 @@
-allCallsOn: aLiteral 
+allCallsOn: aLiteral   "Smalltalk browseAllCallsOn: #open:label:."
 	"Answer a Collection of all the methods that call on aLiteral."
-	| aCollection special methods |
+	| aCollection special thorough aList |
+	#(23 48 'fred' (new open:label:)) size.    "For testing!"
 	aCollection _ OrderedCollection new.
 	special _ self hasSpecialSelector: aLiteral ifTrueSetByte: [:byte ].
+	thorough _ (aLiteral isMemberOf: Symbol)
+				and: ["Possibly search for symbols imbedded in literal arrays"
+					Preferences thoroughSenders].
 	Cursor wait showWhile: 
 		[self allBehaviorsDo: 
 			[:class |
-			 (class whichSelectorsReferTo: aLiteral special: special byte: byte) do: 
-				[:sel | sel ~~ #DoIt
-					ifTrue: [aCollection add: class name , ' ' , sel]]]].
-	^aCollection
+				aList _ thorough
+					ifTrue:
+			 			[(class thoroughWhichSelectorsReferTo: aLiteral special: special byte: byte)]
+					ifFalse:
+						[class whichSelectorsReferTo: aLiteral special: special byte: byte].
+				aList do: 
+					[:sel | sel ~~ #DoIt
+						ifTrue: [aCollection add: class name , ' ' , sel]]]].
+	^ aCollection
