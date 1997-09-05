cfdb1fd0e10b617bf8d335b3846431cfdf4a6ca5
sw <unknown>
1997-09-05 18:05:00 +0000

    no message

--- DebuggerView/class/instance creation/openNotifier-3A-contents-3A-label-3A-.cs
+++ DebuggerView/class/instance creation/openNotifier-3A-contents-3A-label-3A-.cs
@@ -1,20 +1,25 @@
 openNotifier: aDebugger contents: msgString label: label
 	"Create and schedule a simple view with a debugger which can be opened later."
-	| aStringHolderView topView displayPoint nLines |
-	self flag: #developmentNote.
+
+	| msg aStringHolderView topView nLines displayPoint |
 	Cursor normal show.
-	aStringHolderView _ StringHolderView container:
-		(StringHolder new contents: msgString).
+	Sensor flushKeyboard.
+	msg _ msgString.
+	(label beginsWith: 'Space is low')
+		ifTrue: [msg _ self lowSpaceChoices, msg].
+	aStringHolderView _
+		StringHolderView container: (StringHolder new contents: msg).
 	aStringHolderView controller: (NotifyStringHolderController debugger: aDebugger).
 	topView _ StandardSystemView new.
 	topView model: aStringHolderView model.
 	topView addSubView: aStringHolderView.
 	topView label: label.
-	nLines _ 1 + (msgString occurrencesOf: Character cr).
-	topView minimumSize: 350 @ (14*nLines + 6).
-	displayPoint _ 
+	nLines _ 1 + (msg occurrencesOf: Character cr).
+	topView minimumSize: 350 @ (14 * nLines + 6).
+	displayPoint _
 		ScheduledControllers activeController == nil
 			ifTrue: [Display boundingBox center]
 			ifFalse: [ScheduledControllers activeController view displayBox center].
+	displayPoint _ displayPoint max: (200@40).
 	topView controller openNoTerminateDisplayAt: displayPoint.
 	^ topView
