f07070032dde89eec759e026385bc4da88e311b6
jm <unknown>
1997-09-11 15:01:00 +0000

    no message

--- Delay/class/snapshotting/startUp.cs
+++ Delay/class/snapshotting/startUp.cs
@@ -1,5 +1,6 @@
 startUp
 	"Restart active delay, if any, when resuming a snapshot."
 
-	ActiveDelay == nil ifFalse: [ ActiveDelay continueAfterSnapshot ].
+	self restoreResumptionTimes.
+	ActiveDelay == nil ifFalse: [ActiveDelay activate].
 	AccessProtect signal.
