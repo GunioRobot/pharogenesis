c92e55b61306188453bf4dd94063fed64e1d9a1d
jm <unknown>
1997-09-11 13:31:00 +0000

    no message

--- Delay/instance/private/activate.cs
+++ Delay/instance/private/activate.cs
@@ -1,9 +1,7 @@
 activate
-	"Make the receiver the Delay to be signalled when the next timer
-	interrupt occurs. This method should only be called from a block
-	protected by the AccessProtect semaphore."
+	"Private! Make the receiver the Delay to be awoken when the next timer interrupt occurs. This method should only be called from a block protected by the AccessProtect semaphore."
 
 	ActiveDelay _ self.
 	ActiveDelayStartTime _ Time millisecondClockValue.
 	TimingSemaphore initSignals.
-	Processor signal: TimingSemaphore atTime: resumptionTime.
+	Delay primSignal: TimingSemaphore atMilliseconds: resumptionTime.
