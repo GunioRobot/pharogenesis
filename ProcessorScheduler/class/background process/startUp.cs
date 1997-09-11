0e84f6c104b36efb78c11278941281c4ecb73212
jm <unknown>
1997-09-11 10:42:00 +0000

    no message

---
+++ ProcessorScheduler/class/background process/startUp.cs
@@ -0,0 +1,8 @@
+startUp
+	"Install a background process of the lowest possible priority that is always runnable."
+	"Details: The virtual machine requires that there is aways some runnable process that can be scheduled; this background process ensures that this is the case."
+
+	BackgroundProcess == nil ifFalse: [BackgroundProcess terminate].
+	BackgroundProcess _ [self idleProcess] newProcess.
+	BackgroundProcess priority: SystemRockBottomPriority.
+	BackgroundProcess resume.
