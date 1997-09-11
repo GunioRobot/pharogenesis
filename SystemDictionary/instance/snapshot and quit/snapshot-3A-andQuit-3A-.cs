426a58b831a313da92b62f557deece2c62d38172
jm <unknown>
1997-09-11 11:42:00 +0000

    no message

--- SystemDictionary/instance/snapshot and quit/snapshot-3A-andQuit-3A-.cs
+++ SystemDictionary/instance/snapshot and quit/snapshot-3A-andQuit-3A-.cs
@@ -1,23 +1,26 @@
 snapshot: save andQuit: quit
-	"Mark the changes file and close all files.  If save is true, save the current state of this Smalltalk in the image file.  If quit is true, then exit to the outer shell.  Note: latter part of this method runs when resuming a previously saved image. 
-	1/17/96 sw: ripped out the disk-library maintenance stuff
-	5/8/96 sw: report snapshot/quit to transcript as well as chgs log"
+	"Mark the changes file and close all files. If save is true, save the current state of this Smalltalk in the image file. If quit is true, then exit to the outer shell. The latter part of this method runs when resuming a previously saved image. The resume logic checks for a document file to process when starting up."
 
-	| resuming msg |
+	| resuming msg sourceLink |
 	save & (SourceFiles at: 2) notNil ifTrue:
 		[msg _  (quit
 			ifTrue: ['----QUIT----']
-			ifFalse: ['----SNAPSHOT----']), Date dateAndTimeNow printString.
+			ifFalse: ['----SNAPSHOT----'])
+			, Date dateAndTimeNow printString.
+		sourceLink _ ' priorSource: ' , LastQuitLogPosition printString.
 
-		self logChange: msg.
-		Transcript cr; show: msg.
-		LastQuitLogPosition _ (SourceFiles at: 2) position].
+		LastQuitLogPosition _ (SourceFiles at: 2) setToEnd; position.
+		self logChange: msg , sourceLink.
+		Transcript cr; show: msg].
 
 	self processShutDownList.
 	Cursor write show.
-	save ifTrue: [resuming _ self snapshotPrimitive]  "<-- PC frozen here on image file"
+	save
+		ifTrue: [resuming _ self snapshotPrimitive]  "<-- PC frozen here on image file"
 		ifFalse: [resuming _ false].
 	quit & resuming not ifTrue: [self quitPrimitive].
 	Cursor normal show.
 	self processStartUpList.
-	
+	resuming ifTrue: [
+		self clearExternalObjects.
+		self readDocumentFile].
