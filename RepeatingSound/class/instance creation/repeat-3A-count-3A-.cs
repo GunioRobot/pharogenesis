a8bc980d7342337790d572f82870cb671f37adcb
jm <unknown>
1997-09-12 16:14:00 +0000

    no message

---
+++ RepeatingSound/class/instance creation/repeat-3A-count-3A-.cs
@@ -0,0 +1,4 @@
+repeat: aSound count: anInteger
+	"Return a RepeatingSound that will repeat the given sound for the given number of iterations."
+
+	^ self new setSound: aSound iterations: anInteger
