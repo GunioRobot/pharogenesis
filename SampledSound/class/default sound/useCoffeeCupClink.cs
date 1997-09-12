8baef92dfed132661c57af1826c185c74d65cac2
jm <unknown>
1997-09-12 19:38:00 +0000

    no message

---
+++ SampledSound/class/default sound/useCoffeeCupClink.cs
@@ -0,0 +1,6 @@
+useCoffeeCupClink
+	"Set the sample table to be used as the default waveform to the sound of a coffee cup being tapped with a spoon."
+	"SampledSound useCoffeeCupClink bachFugue play"
+
+	self defaultSampleTable: self coffeeCupClink.
+	NominalSamplePitch _ 400.
