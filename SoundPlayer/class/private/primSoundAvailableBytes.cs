47fb58d329dfec4657b420a94308dd4f8879ff76
jm <unknown>
1997-09-13 20:11:00 +0000

    no message

---
+++ SoundPlayer/class/private/primSoundAvailableBytes.cs
@@ -0,0 +1,6 @@
+primSoundAvailableBytes
+	"Return the number of bytes of available space in the sound output buffer."
+	"Note: Squeak always uses buffers containing 4-bytes per sample (2 channels at 2 bytes per channel) regardless of the state of the Stereo flag."
+
+	<primitive: 173>
+	^ self primitiveFailed
