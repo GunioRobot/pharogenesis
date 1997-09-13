906ad45a21863efdc5cd09e195f50d76dfe7d931
jm <unknown>
1997-09-13 20:07:00 +0000

    no message

---
+++ SoundPlayer/class/private/primSoundPlaySamples-3A-from-3A-startingAt-3A-.cs
@@ -0,0 +1,5 @@
+primSoundPlaySamples: count from: aSampleBuffer startingAt: index
+	"Copy count bytes into the current sound output buffer from the given sample buffer starting at the given index."
+
+	<primitive: 174>
+	^ self primitiveFailed
