a8dd23bbffb75c10493046143d404755e160723d
sw <unknown>
1997-09-14 18:22:00 +0000

    no message

---
+++ PolygonMorph/instance/geometry/flipHAroundX-3A-.cs
@@ -0,0 +1,12 @@
+flipHAroundX: centerX
+	"Flip me horizontally around the center.  If centerX is nil, compute my center of gravity."
+
+	| cent |
+	cent _ centerX 
+		ifNil: [bounds center x
+			"cent _ 0.
+			vertices do: [:each | cent _ cent + each x].
+			cent asFloat / vertices size"]		"average is the center"
+		ifNotNil: [centerX].
+	self setVertices: (vertices collect: [:vv |
+			(vv x - cent) * -1 + cent @ vv y]) reversed.
