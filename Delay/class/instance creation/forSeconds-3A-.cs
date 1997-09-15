41c711395237d72f4190c69e2ab0e1ad5e4c80a7
jm <unknown>
1997-09-15 17:09:00 +0000

    no message

--- Delay/class/instance creation/forSeconds-3A-.cs
+++ Delay/class/instance creation/forSeconds-3A-.cs
@@ -1,2 +1,7 @@
-forSeconds: t1 
-^ self new delay: t1 * 1000
+forSeconds: anInteger
+	"Return a new Delay for the given number of seconds. Sending 'wait' to this Delay will cause the sender's process to be suspended for approximately that length of time."
+
+	anInteger < 0 ifTrue: [self error: 'delay times cannot be negative'].
+	^ self new
+		setDelay: anInteger * 1000
+		forSemaphore: Semaphore new
