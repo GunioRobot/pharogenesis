26d346341bf7b6f54714b76c3b1581dffec295a4
jm <unknown>
1997-09-15 06:53:00 +0000

    no message

---
+++ Socket/class/network initialization/initializeNetwork-3A-.cs
@@ -0,0 +1,4 @@
+initializeNetwork: ignored
+	"Old message for backward compatibility. This method will be removed in some future release; clients should use 'Socket initializeNetwork' instead."
+
+	Socket initializeNetwork.
