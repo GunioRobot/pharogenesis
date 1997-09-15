171e4f01b49fa6dc4d997663e8d2f1ec302b9e76
jm <unknown>
1997-09-15 17:11:00 +0000

    no message

---
+++ Semaphore/instance/communication/waitTimeoutMSecs-3A-.cs
@@ -0,0 +1,7 @@
+waitTimeoutMSecs: anInteger
+	"Wait on this semaphore for up to the given number of milliseconds, then timeout. It is up to the sender to determine the difference between the expected event and a timeout."
+
+	| d |
+	d _ Delay timeoutSemaphore: self afterMSecs: (anInteger max: 0).
+	self wait.
+	d unschedule.
