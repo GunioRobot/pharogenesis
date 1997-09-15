b050062f5c6ab6e6510524bbcd74e57c57895714
jm <unknown>
1997-09-15 13:53:00 +0000

    no message

---
+++ SimpleClientSocket/class/POP mail example/simpleDateString-3A-.cs
@@ -0,0 +1,8 @@
+simpleDateString: dateString
+
+	| s |
+	s _ ReadStream on: dateString.
+	s skipTo: $,.  "scan thru first comma"
+	s atEnd ifTrue: [s reset].  "no comma found; reset s"
+	s skipSeparators.
+	^ (Date readFrom: s) mmddyy
