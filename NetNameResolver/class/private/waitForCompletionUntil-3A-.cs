11edeaa6a2a154716f6544d58465c8b6ec687783
jm <unknown>
1997-09-15 16:56:00 +0000

    no message

---
+++ NetNameResolver/class/private/waitForCompletionUntil-3A-.cs
@@ -0,0 +1,15 @@
+waitForCompletionUntil: deadline
+	"Wait up to the given number of seconds for the resolver to be ready to accept a new request. Return true if the resolver is ready, false if the network is not initialized or the resolver does not become free within the given time period."
+
+	| status |
+	status _ self primNameResolverStatus.
+	[(status = ResolverBusy) and:
+	 [Time millisecondClockValue < deadline]]
+		whileTrue: [
+			"wait for resolver to be available"
+			ResolverSemaphore waitTimeoutMSecs: (deadline - Time millisecondClockValue).
+			status _ self primNameResolverStatus].
+
+	status = ResolverReady
+		ifTrue: [^ true]
+		ifFalse: [self primAbortLookup. ^ false].
