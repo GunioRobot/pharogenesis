3938eb0a81c6bc9bd12174f46f1b26581bb37ece
jm <unknown>
1997-09-17 16:56:00 +0000

    no message

---
+++ SoundRecorder/instance/primitives/primGetActualRecordingSampleRate.cs
@@ -0,0 +1,5 @@
+primGetActualRecordingSampleRate
+	"Return the actual sample rate being used for recording. This primitive fails unless sound recording is currently in progress."
+
+	<primitive: 192>
+	self primitiveFailed
