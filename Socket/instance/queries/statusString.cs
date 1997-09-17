54fcf277e560c5a31dc14fa290e9cf2138ceae83
jm <unknown>
1997-09-17 16:10:00 +0000

    no message

---
+++ Socket/instance/queries/statusString.cs
@@ -0,0 +1,12 @@
+statusString
+	"Return a string describing the status of this socket."
+
+	| status |
+	status _ self primSocketConnectionStatus: socketHandle.
+	status = InvalidSocket ifTrue: [^ 'invalidSocketHandle'].
+	status = Unconnected ifTrue: [^ 'unconnected'].
+	status = WaitingForConnection ifTrue: [^ 'waitingForConnection'].
+	status = Connected ifTrue: [^ 'connected'].
+	status = OtherEndClosed ifTrue: [^ 'otherEndClosedButNotThisEnd'].
+	status = ThisEndClosed ifTrue: [^ 'thisEndClosedButNotOtherEnd'].
+	^ 'unknown socket status'
