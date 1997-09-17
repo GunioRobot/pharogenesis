0fb863675f353fea19ce57611321ca2cd1935239
jm <unknown>
1997-09-17 14:42:00 +0000

    no message

---
+++ Socket/class/class initialization/initialize.cs
@@ -0,0 +1,10 @@
+initialize
+	"Socket initialize"
+
+	"Socket Status Values"
+	InvalidSocket _ -1.
+	Unconnected _ 0.
+	WaitingForConnection _ 1.
+	Connected _ 2.
+	OtherEndClosed _ 3.
+	ThisEndClosed _ 4.
