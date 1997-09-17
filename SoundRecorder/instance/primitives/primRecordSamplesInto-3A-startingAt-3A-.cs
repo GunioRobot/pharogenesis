26d5bbca28a701254efd284e092882a32bd490ba
jm <unknown>
1997-09-17 16:56:00 +0000

    no message

---
+++ SoundRecorder/instance/primitives/primRecordSamplesInto-3A-startingAt-3A-.cs
@@ -0,0 +1,5 @@
+primRecordSamplesInto: aWordArray startingAt: index
+	"Record a sequence of 16-bit sound samples into the given array starting at the given sample index. Return the number of samples recorded, which may be zero if no samples are currently available."
+
+	<primitive: 193>
+	self primitiveFailed
