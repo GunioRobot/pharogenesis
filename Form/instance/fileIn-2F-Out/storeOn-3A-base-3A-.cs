889cb8372a2440b88527db29ce4409da37b2ee50
jm <unknown>
1997-09-18 23:13:00 +0000

    no message

---
+++ Form/instance/fileIn-2F-Out/storeOn-3A-base-3A-.cs
@@ -0,0 +1,23 @@
+storeOn: aStream base: anInteger 
+	"Store the receiver out as an expression that can be evaluated to recreate a Form with the same contents as the original."
+
+	aStream nextPut: $(.
+	aStream nextPutAll: self species name.
+	aStream crtab: 1.
+	aStream nextPutAll: 'extent: '.
+	self extent printOn: aStream.
+	aStream crtab: 1.
+	aStream nextPutAll: 'depth: '.
+	self depth printOn: aStream.
+	aStream crtab: 1.
+	aStream nextPutAll: 'fromArray: #('.
+	1 to: bits size do: [:index | 
+		anInteger = 10
+			ifTrue: [aStream space]
+			ifFalse: [aStream crtab: 2].
+		(self bits at: index) printOn: aStream base: anInteger].
+	aStream nextPut: $).
+	aStream crtab: 1.
+	aStream nextPutAll: 'offset: '.
+	self offset printOn: aStream.
+	aStream nextPut: $).
