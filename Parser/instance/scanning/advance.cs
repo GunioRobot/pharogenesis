66f39c35cac466ea1581429c2c266e6f52a7b9d6
sn <unknown>
1997-09-19 19:32:00 +0000

    no message

--- Parser/instance/scanning/advance.cs
+++ Parser/instance/scanning/advance.cs
@@ -1,14 +1,11 @@
 advance
-
-	| this |
-	prevMark _ hereMark.
-	prevToken _ "Now means prev size"
-		hereType == #number
-			ifTrue: [mark - prevMark]
-			ifFalse: [here size].
-	this _ here.
-	here _ token.
-	hereType _ tokenType.
-	hereMark _ mark.
-	self scanToken.
-	^this
+    | this |
+    prevMark _ hereMark.
+    prevToken _ "Now means prev size"
+        self previousTokenSize.
+    this _ here.
+    here _ token.
+    hereType _ tokenType.
+    hereMark _ mark.
+    self scanToken.
+    ^this
