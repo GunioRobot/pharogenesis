6c657c8d6a28099f94ca7304884b2bf498ef7d26
sw <unknown>
1997-09-21 01:14:00 +0000

    no message

---
+++ SystemDictionary/instance/housekeeping/makeInternalRelease.cs
@@ -0,0 +1,12 @@
+makeInternalRelease		"Smalltalk makeInternalRelease"
+	(self confirm: self version , '
+Is this the correct version designation?
+If not, choose no, and fix it.') ifFalse: [^ self].
+	(Object classPool at: #DependentsFields) size > 1 ifTrue: [self halt].
+	Browser initialize.
+	Undeclared isEmpty ifFalse: [self halt].
+	Smalltalk garbageCollect.
+	self obsoleteClasses isEmpty ifFalse: [self halt].
+	Symbol rehash.
+	self halt: 'Ready to condense changes'.
+	Smalltalk condenseChanges
