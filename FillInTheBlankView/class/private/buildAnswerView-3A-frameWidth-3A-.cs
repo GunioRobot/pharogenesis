221a052bbd6d8aefe99a0f222b11496f29150322
sw <unknown>
1997-09-21 11:47:00 +0000

    no message

--- FillInTheBlankView/class/private/buildAnswerView-3A-frameWidth-3A-.cs
+++ FillInTheBlankView/class/private/buildAnswerView-3A-frameWidth-3A-.cs
@@ -1,7 +1,3 @@
 buildAnswerView: aFillInTheBlank frameWidth: widthInteger
 
-	| answerView |
-	answerView _ self new model: aFillInTheBlank.
-	answerView window: (0@0 extent: widthInteger @ 40).
-	answerView borderWidth: 2.
-	^answerView
+	^ self buildAnswerView: aFillInTheBlank frameWidth: widthInteger height: 40
