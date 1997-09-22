462facb3018e74aef729bb2f5c79604a4d241406
di <unknown>
1997-09-22 13:19:00 +0000

    no message

--- ChangeSet/instance/change management/assimilateAllChangesFoundIn-3A-.cs
+++ ChangeSet/instance/change management/assimilateAllChangesFoundIn-3A-.cs
@@ -1,7 +1,7 @@
 assimilateAllChangesFoundIn: aChangeSet
 	"Make all changes in aChangeSet take effect on self as it they happened later.  *** classes renamed in aChangeSet may have have problems"
 
-	| cls info |
+	| cls info selector pair |
 	aChangeSet changedClassNames do: [:className |
 		(cls _ Smalltalk classNamed: className) notNil ifTrue:
 		[info _ aChangeSet classChangeAt: className.
@@ -10,8 +10,17 @@ assimilateAllChangesFoundIn: aChangeSet
 		info _ aChangeSet methodChanges at: className 
 			ifAbsent: [Dictionary new].
 		info associationsDo: [:assoc |
-			assoc value == #remove ifTrue:
-					[self removeSelector: assoc key class: cls]
+			assoc value == #remove
+				ifTrue:
+					[selector _ assoc key.
+					self removeSelector: selector class: cls.
+					pair _ aChangeSet methodRemoves
+							at: (Array with: cls name with: selector)
+							ifAbsent: [nil].
+					pair ifNotNil:
+						["Retain source code ref if stored"
+						methodRemoves at: (Array with: cls name with: selector)
+										put: pair]]
 				ifFalse: 
 					[self atSelector: assoc key class: cls put: assoc value]]]].
 	self flag: #developmentNote.  "the following cannot work, since the class will not exist; SW comments this out 8/91 because it thwarts integration!"
