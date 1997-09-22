ae6ecce12e4895e075eeafed988312e7f9941ac3
di <unknown>
1997-09-22 13:18:00 +0000

    no message

--- ChangeSet/instance/method changes/removeSelector-3A-class-3A-.cs
+++ ChangeSet/instance/method changes/removeSelector-3A-class-3A-.cs
@@ -2,8 +2,15 @@ removeSelector: selector class: class
 	"Include indication that a method has been forgotten."
 
 	(self atSelector: selector class: class) = #add
-		ifTrue: [self removeSelectorChanges: selector 
-					class: class]					"Forgot a new method, no-op"
+		ifTrue: [self atSelector: selector
+					class: class
+					put: #addedThenRemoved]
 		ifFalse: [self atSelector: selector
 					class: class
-					put: #remove]
+					put: #remove].
+
+	(class includesSelector: selector) ifTrue:
+		["Save the source code pointer and category so can still browse old versions"
+		methodRemoves at: (Array with: class name with: selector)
+			put: (Array with: (class compiledMethodAt: selector) sourcePointer
+						with: (class whichCategoryIncludesSelector: selector))]
