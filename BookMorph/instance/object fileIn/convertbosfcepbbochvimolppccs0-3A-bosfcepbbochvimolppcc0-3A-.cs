b455cae3f3e5fd822e1b8a175ba185612e947bec
jm <unknown>
1997-09-24 08:49:00 +0000

    no message

---
+++ BookMorph/instance/object fileIn/convertbosfcepbbochvimolppccs0-3A-bosfcepbbochvimolppcc0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcepbbochvimolppccs0: varDict bosfcepbbochvimolppcc0: smartRefStrm
+	"These variables are automatically stored into the new instance ('pageSize' 'pages' 'currentPage' 'copyContents' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"Be sure to to fill in () and deal with the information in ('saveBlock' )"
