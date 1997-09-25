9fdf2544bf94836845ec0ff86ab82924c42f2fec
di <unknown>
1997-09-25 16:13:00 +0000

    no message

---
+++ PolygonMorph/instance/editing/dropVertex-3A-fromHandle-3A-vertIndex-3A-.cs
@@ -0,0 +1,7 @@
+dropVertex: evt fromHandle: handle vertIndex: ix
+	| p |
+	p _ vertices at: ix.
+	(((vertices atWrap: ix-1) dist: p) < 3 or:
+		[((vertices atWrap: ix+1) dist: p) < 3])
+		ifTrue: ["Drag a vertex onto its neighbor means delete"
+				self setVertices: (vertices copyReplaceFrom: ix to: ix with: Array new)]
