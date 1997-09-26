f14dd457eb5e45355d8f99519012735290e388a8
ikp <unknown>
1997-09-26 14:45:00 +0000

    no message

---
+++ PseudoContext/class/filing out/definition.cs
@@ -0,0 +1,9 @@
+definition
+	"Our superclass is really nil, but this causes problems when we try to become compact
+	after filing in for the first time.  Fake the superclass as Object, and repair the situation
+	during class initialisation."
+	| defn |
+	defn _ super definition.
+	^(defn beginsWith: 'nil ')
+		ifTrue: ['Object' , (defn copyFrom: 4 to: defn size)]
+		ifFalse: [defn].
