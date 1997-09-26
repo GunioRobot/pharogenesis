08a4e6c43b1aa121c179cacce4288e80cb63771d
ikp <unknown>
1997-09-26 14:45:00 +0000

    no message

---
+++ PseudoContext/class/private/contextCacheDepth.cs
@@ -0,0 +1,5 @@
+contextCacheDepth
+	"Answer the number of entries in the context cache.  This requires a little insider
+	knowledge.  Not intended for casual use, which is why it's private protocol."
+
+	^self contextCacheDepth: thisContext yourself
