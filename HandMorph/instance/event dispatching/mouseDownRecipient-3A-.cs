56c5dc917d3000ccee3d84878f4eb997dbf3165b
di <unknown>
1997-09-26 10:39:00 +0000

    no message

---
+++ HandMorph/instance/event dispatching/mouseDownRecipient-3A-.cs
@@ -0,0 +1,5 @@
+mouseDownRecipient: aMorph
+	"Install a new recipient for mousedown events, namely stillDown and up,
+	and, in the process, unsubscribe any prior recipient"
+
+	mouseDownMorph _ aMorph
