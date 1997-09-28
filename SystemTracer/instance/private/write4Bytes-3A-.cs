2e5a3b1cf7cb2acc06b3b05f7dd13d8b71e1c0ba
di <unknown>
1997-09-28 14:20:00 +0000

    no message

---
+++ SystemTracer/instance/private/write4Bytes-3A-.cs
@@ -0,0 +1,13 @@
+write4Bytes: bits 
+	"Avoid overhead of large integers and nextWord:put:."
+	| posBits bytes |
+	bits positive
+		ifTrue: [posBits _ bits]
+		ifFalse: ["Change rep to twos complement."
+				posBits _ 16rFFFFFFFF+(bits+1)].
+	bytes _ ByteArray new: 4.
+	bytes at: 1 put: (posBits digitAt: 4).
+	bytes at: 2 put: (posBits digitAt: 3).
+	bytes at: 3 put: (posBits digitAt: 2).
+	bytes at: 4 put: (posBits digitAt: 1).
+	file nextPutAll: bytes
