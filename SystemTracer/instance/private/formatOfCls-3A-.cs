cec16afa6609f66f3df2dc5daf2a11c9ce41cc4b
di <unknown>
1997-09-28 14:46:00 +0000

    no message

---
+++ SystemTracer/instance/private/formatOfCls-3A-.cs
@@ -0,0 +1,4 @@
+formatOfCls: class
+	"Return the full word value that encodes instSize, bits, bytes, and variable."
+	"See the comment in Behavior format:variable:words:pointers:"
+	^ class format    "just use what's there"
