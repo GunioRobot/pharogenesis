bed4c480dbeda77bc89bf2cc8b5f358689fdcfa7
sw <unknown>
1997-09-29 17:14:00 +0000

    no message

--- InputSensor/instance/user interrupts/userInterruptWatcher.cs
+++ InputSensor/instance/user interrupts/userInterruptWatcher.cs
@@ -1,8 +1,7 @@
 userInterruptWatcher
 	"Wait for user interrupts and open a notifier on the active process when one occurs."
 
-	[true] whileTrue: [
-		InterruptSemaphore wait.
-		SoundPlayer shutDown.
-		[ScheduledControllers interruptName: 'User Interrupt'] fork.
-	].
+	[true] whileTrue:
+		[InterruptSemaphore wait.
+		Smalltalk shutDownSound.
+		ScheduledControllers maybeForkInterrupt]
