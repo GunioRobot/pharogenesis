b4e01f4c4b8778b904153aab7c0e75ade39f43d1
ikp <unknown>
1997-09-29 21:31:00 +0000

    no message

---
+++ DynamicInterpreterSimulator/instance/float primitives/primitiveFloatMultiply.cs
@@ -0,0 +1,9 @@
+primitiveFloatMultiply
+	"Use host Smalltalk's native function."
+
+	| rcvr arg |
+	arg _ self popFloatOnly.
+	rcvr _ self popFloatOnly.
+	successFlag
+		ifTrue: [self pushFloat: rcvr * arg]
+		ifFalse: [self unPop: 2].
