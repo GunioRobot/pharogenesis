65e916ac30e798c7af8fa6175a8277251a68439f
di <unknown>
1997-09-30 15:48:00 +0000

    no message

---
+++ TextMorph/instance/accessing/contentsAsIs-3A-.cs
@@ -0,0 +1,5 @@
+contentsAsIs: stringOrText
+	"Accept new text contents with line breaks only as in the text.
+	Fit my width and height to the result."
+	wrapFlag _ false.
+	self newContents: stringOrText
