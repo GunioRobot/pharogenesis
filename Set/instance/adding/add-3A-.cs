471eac8c079f872f276f6f12c2ec520f7b48e11e
go <unknown>
1997-10-01 09:33:00 +0000

    no message

--- Set/instance/adding/add-3A-.cs
+++ Set/instance/adding/add-3A-.cs
@@ -1,6 +1,8 @@
-add: newObject 
+add: newObject
+	"Add an element. User error instead of halt. go 10/1/97 09:33"
+
 	| index |
-	newObject == nil ifTrue: [self halt: 'Sets cannot meaningfully contain nil as an element'].
+	newObject == nil ifTrue: [self error: 'Sets cannot meaningfully contain nil as an element'].
 	index _ self findElementOrNil: newObject.
 	(array at: index) == nil ifTrue:
 		[self atNewIndex: index put: newObject].
