63718a5f872b38c9f4eac55d5b4a5bf373f990da
sw <unknown>
1997-10-02 16:12:00 +0000

    no message

--- ScreenController/instance/menu messages/indicateWindowsWithUnacceptedInput.cs
+++ ScreenController/instance/menu messages/indicateWindowsWithUnacceptedInput.cs
@@ -1,5 +1,5 @@
 indicateWindowsWithUnacceptedInput
-	"Put up a list of windows with unaccepted input, and let the user chose one to activate.  1/18/96 sw.  2/22/96 sw: use hasUnacceptedInput"
+	"Put up a list of windows with unaccepted input, and let the user chose one to activate"
 
 	ScheduledControllers findWindowSatisfying:
 		[:contr |  contr model hasUnacceptedInput]
