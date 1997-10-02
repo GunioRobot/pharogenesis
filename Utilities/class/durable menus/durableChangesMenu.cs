f4f5e00c8594d4c59ada0484306172825293336c
sw <unknown>
1997-10-02 20:12:00 +0000

    no message

---
+++ Utilities/class/durable menus/durableChangesMenu.cs
@@ -0,0 +1,3 @@
+durableChangesMenu
+	"Utilities durableChangesMenu"
+	self windowFromMenu: ScheduledControllers screenController changesMenu target: ScheduledControllers screenController title: 'Change controls'
