6d186dc63dce1c4bd23d1aac0bb3a1fe0d14d453
sw <unknown>
1997-10-02 19:02:00 +0000

    no message

---
+++ Morph/instance/properties/balloonText.cs
@@ -0,0 +1,3 @@
+balloonText
+	"NB: subclasses may override such that they programatically construct the text, for economy's sake, such as model phrases in a Viewer"
+	^ self valueOfProperty: #balloonText
