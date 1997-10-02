3a8acf769e380c980c27183feb5ea95d1fea61ed
di <unknown>
1997-10-02 11:34:00 +0000

    no message

---
+++ ParagraphEditor/instance/menu messages/mvcRedisplay.cs
@@ -0,0 +1,5 @@
+mvcRedisplay
+	"Overridable by subclasses that do their own display"
+	Display fill: paragraph clippingRectangle 
+			fillColor: view backgroundColor.	"very brute force"
+	self display
