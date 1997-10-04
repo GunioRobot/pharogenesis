45e9ccda720dbd4fcfc57b26a40e2eba145190fd
di <unknown>
1997-10-04 09:03:00 +0000

    no message

--- ControlManager/instance/displaying/restore.cs
+++ ControlManager/instance/displaying/restore.cs
@@ -1,9 +1,8 @@
 restore
-	"Clear the screen to gray and then redisplay all the scheduled views.  Try to be a bit intelligent about the view that wants control and not display it twice if possible..
-	1/24/96 sw: uncache bits of top view"
+	"Clear the screen to gray and then redisplay all the scheduled views.  Try to be a bit intelligent about the view that wants control and not display it twice if possible."
 
 	scheduledControllers first view uncacheBits.  "assure refresh"
 	self unschedule: screenController; scheduleOnBottom: screenController.
-	screenController view window: Display boundingBox.
-	scheduledControllers reverseDo:
+	screenController view window: Display boundingBox; displayDeEmphasized.
+	self scheduledWindowControllers reverseDo:
 		[:aController | aController view displayDeEmphasized].
