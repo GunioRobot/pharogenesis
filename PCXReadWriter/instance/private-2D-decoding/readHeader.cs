5f1b99ed53cbe1dccccdf67186e0d43f1b8d51f6
tao <unknown>
1997-10-06 09:42:00 +0000

    no message

---
+++ PCXReadWriter/instance/private-2D-decoding/readHeader.cs
@@ -0,0 +1,22 @@
+readHeader
+
+	| xMin xMax yMin yMax |
+	self next.	"skip over manufacturer field"
+	version _ self next.
+	encoding _ self next.
+	bitsPerPixel _ self next.
+	xMin _ self nextWord.
+	yMin _ self nextWord.
+	xMax _ self nextWord.
+	yMax _ self nextWord.
+	width _ xMax - xMin + 1.
+	height _ yMax - yMin + 1.
+	self next: 4. "skip over device resolution"
+	self next: 49. "skip over EGA color palette"
+	colorPlanes _ self next.
+	rowByteSize _ self nextWord.
+	isGrayScale _ (self next: 2) = 2.
+	self next: 58. "skip over filler".
+
+
+
