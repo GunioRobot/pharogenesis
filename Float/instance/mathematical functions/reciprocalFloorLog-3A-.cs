7bf9b8b7da64fca44d2fa28b701aeceafb8e9129
tao <unknown>
1997-10-10 17:38:00 +0000

    no message

---
+++ Float/instance/mathematical functions/reciprocalFloorLog-3A-.cs
@@ -0,0 +1,12 @@
+reciprocalFloorLog: radix 
+	"Quick computation of (self log: radix) floor, when self < 1.0.
+	Avoids infinite recursion problems with denormalized numbers"
+
+	| adjust scale n |
+	adjust _ 0.
+	scale _ 1.0.
+	[(n _ radix / (self * scale)) isInf]
+		whileTrue:
+			[scale _ scale * radix.
+			adjust _ adjust + 1].
+	^ ((n floorLog: radix) + adjust) negated
