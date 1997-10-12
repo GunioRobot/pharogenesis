1d1f0319c547ea92fc33f8f0b12ba079b01cc729
sw <unknown>
1997-10-12 21:48:00 +0000

    no message

--- ControlManager/instance/scheduling/findWindowSatisfying-3A-.cs
+++ ControlManager/instance/scheduling/findWindowSatisfying-3A-.cs
@@ -1,20 +1,22 @@
 findWindowSatisfying: aBlock
-	"Present a menu of window titles, and activate the one that gets chosen
-	 1/18/96 sw: Created this version with an argument for more general use, and also, as per Dan's request, modified so that windows whose topleft corners are beyond the lower-right screen corner get picked up by the window-rescue piece.
-	 5/8/96 sw: use activateController:"
+	"Present a menu of window titles, and activate the one that gets chosen"
 
-	| controllers labels index |
+	| controllers labels index listToUse sortAlphabetically |
+	sortAlphabetically _ Sensor optionKeyPressed.
 	controllers _ OrderedCollection new.
+	scheduledControllers do:
+		[:controller | controller == screenController ifFalse:
+			[(aBlock value: controller) ifTrue: [controllers addLast: controller]]].
+	controllers size == 0 ifTrue: [^ self].
+	listToUse _ sortAlphabetically
+		ifTrue:
+			[controllers asSortedCollection: [:a :b | a view label < b view label]]
+		ifFalse:
+			[controllers].
 	labels _ String streamContents:
-		[:strm |
-		scheduledControllers do:
-			[:controller | controller == screenController ifFalse:
-				[(aBlock value: controller) ifTrue:
-					[controllers addLast: controller.
-					strm nextPutAll: (controller view label contractTo: 40); cr]]].
-		strm position == 0 ifTrue: [^ self].  "Nothing satisfies"
+		[:strm | 
+			listToUse do: [:controller | strm nextPutAll: (controller view label contractTo: 40); cr].
 		strm skip: -1  "drop last cr"].
-
 	index _ (PopUpMenu labels: labels) startUp.
 	index > 0 ifTrue:
 		[self activateController: (controllers at: index)]
