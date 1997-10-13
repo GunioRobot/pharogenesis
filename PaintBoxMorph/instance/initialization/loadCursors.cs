328b43eeb269f1aa2be37aba9d4c43647140903b
di <unknown>
1997-10-13 18:49:00 +0000

    no message

---
+++ PaintBoxMorph/instance/initialization/loadCursors.cs
@@ -0,0 +1,17 @@
+loadCursors
+	"Display the form containing the cursors.  Transparent is (Color r: 1.0 g: 0 b: 1.0).  Grab the forms one at a time, and they are stored away.
+	self loadCursors.	"
+
+| button transp cursor map |
+transp _ Color r: 1.0 g: 0 b: 1.0.
+map _ Color indexedColors copy.	"just in case"
+1 to: 256 do: [:ind | (map at: ind) = transp ifTrue: [
+			map at: ind put: Color transparent]].
+
+#(erase: eyedropper: fill: paint: rect: ellipse: polygon: line: star: ) do: [:sel |
+	PopUpMenu notify: 'Rectangle for ',sel.
+	cursor _ ColorForm fromUser.
+	cursor colors: map.	"share it"
+	button _ self findButton: sel.
+	button arguments at: 3 put: cursor.
+	].
