ff6921bfc5375173cc0fb72a912c5cc1fb469090
di <unknown>
1997-10-14 06:23:00 +0000

    no message

---
+++ SketchEditorMorph/instance/start & finish/resumePainting.cs
@@ -0,0 +1,7 @@
+resumePainting
+	"Set everything up.  Amazingly, it's all handled by verifyState!"
+
+	"palette thumbnailOn: hostView.		not used in current paintbox"
+	"palette setRotation: hostView rotationStyle."
+	"stillScaling now will be indicated by action == #scaleOrRotate"
+	
