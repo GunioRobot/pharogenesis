2ffcaa8f0e57534975e865cdf50a8fbbe278f184
jhm <unknown>
1997-10-15 14:30:00 +0000

    no message

---
+++ SoundRecorder/instance/initialization/initializeRecordingState.cs
@@ -0,0 +1,10 @@
+initializeRecordingState
+
+	recordProcess _ nil.
+	bufferAvailableSema _ nil.
+	paused _ true.
+	meteringBuffer _ nil.
+	meterLevel _ 0.
+	soundPlaying _ nil.
+	currentBuffer _ nil.
+	nextIndex _ 1.
