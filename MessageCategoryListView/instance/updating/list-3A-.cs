d5df155ee22ffb4987f72fdae45028d67c22c1bd
jhm <unknown>
1997-10-15 17:16:00 +0000

    no message

--- MessageCategoryListView/instance/updating/list-3A-.cs
+++ MessageCategoryListView/instance/updating/list-3A-.cs
@@ -1,5 +1,6 @@
-list: anArray 
+list: anArray
+
 	super list: anArray.
-	list numberOfLines = 3 ifTrue: [
-		controller isNil ifFalse: [
-			controller changeModelSelection: 1]].
+	((Preferences valueOfFlag: #browserAutoSelect) and:
+	 [list numberOfLines = 3]) ifTrue: [
+		controller isNil ifFalse: [controller changeModelSelection: 1]].
