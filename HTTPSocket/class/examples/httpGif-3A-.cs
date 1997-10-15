45b40a1d30bae32587ffd2c10e53970c96f339ab
tk <unknown>
1997-10-15 12:07:00 +0000

    no message

---
+++ HTTPSocket/class/examples/httpGif-3A-.cs
@@ -0,0 +1,13 @@
+httpGif: url
+	"Fetch the given URL, parse it using the GIF reader, and return the resulting Form."
+	"	HTTPSocket httpShowGif: 'www.altavista.digital.com/av/pix/default/av-adv.gif'	 "
+	"	HTTPSocket httpShowGif: 'www.webPage.com/~kaehler2/ainslie.gif'	 "
+
+	| doc ggg |
+	doc _ self httpGet: url accept: 'image/gif'.
+	doc class == String ifTrue: [
+		self inform: 'The server with that GIF is not responding'.
+		^ ColorForm extent: 20@20 depth: 8].
+	doc binary; reset.
+	(ggg _ Smalltalk gifReaderClass new) setStream: doc.
+	^ ggg nextImage.
