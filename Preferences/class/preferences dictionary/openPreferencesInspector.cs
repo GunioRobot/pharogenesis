43886d0248f6a34647dfa9b86186f46a845b74a0
jhm <unknown>
1997-10-15 17:12:00 +0000

    no message

---
+++ Preferences/class/preferences dictionary/openPreferencesInspector.cs
@@ -0,0 +1,5 @@
+openPreferencesInspector
+	"Open a window on the current preferences dictionary, allowing the user to inspect and change the current preference settings."
+	"Preferences openPreferencesInspector"
+
+	FlagDictionary inspectWithLabel: 'Preferences'
