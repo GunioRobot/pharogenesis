ecfa44b28a5341f06a342d40b1ecc6a4c06ef00b
jm <unknown>
1997-10-16 15:20:00 +0000

    no message

--- Form/instance/transitions/zoomInTo-3A-at-3A-.cs
+++ Form/instance/transitions/zoomInTo-3A-at-3A-.cs
@@ -1,6 +1,6 @@
 zoomInTo: otherImage at: topLeft
 	"Display zoomInTo: (Form fromDisplay: (40@40 extent: 300@300)) reverse at: 40@40"
-	^ self wipeImage: otherImage at: topLeft rectForIndex:
+	^ self wipeImage: otherImage at: topLeft clippingBox: nil rectForIndex:
 		[:i | i <= 16
 			ifTrue: [otherImage center - (otherImage extent*i//32)
 						extent: otherImage extent*i//16]
