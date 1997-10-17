81e850c79f2625b73446d943ed43265026ad96cf
di <unknown>
1997-10-17 23:01:00 +0000

    no message

--- ChangeSet/instance/fileIn-2F-Out/fileOut.cs
+++ ChangeSet/instance/fileIn-2F-Out/fileOut.cs
@@ -1,11 +1,17 @@
 fileOut
-	"File out the receiver, to a file whose name is a function of the change-set name and of the date and the time.  1/18/96 sw
- 2/4/96 sw: show write cursor
-	5/30/96 sw: put a dot before the date/time stamp"
-
-	| file |
+	"File out the receiver, to a file whose name is a function of the change-set name and of the date and the time."
+	| file slips |
 	Cursor write showWhile:
 		[file _ FileStream newFileNamed: ((self name, '.', Utilities dateTimeSuffix, '.cs') truncateTo: 27).
 		file header; timeStamp.
+		self fileOutPreambleOn: file.
 		self fileOutOn: file.
-		file trailer; close]
+		self fileOutPostscriptOn: file.
+		file trailer; close].
+
+	slips _ self checkForSlips.
+	(slips size > 0 and: [self confirm: 'Methods in this fileOut have halts
+or references to the Transcript in them.
+Would you like to browse them?'])
+		ifTrue: [Smalltalk browseMessageList: slips
+							name: 'References to #halt or Transcript']
