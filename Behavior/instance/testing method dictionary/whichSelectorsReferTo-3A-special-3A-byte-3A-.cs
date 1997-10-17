593cc65adb39b5af54ebf863b5e898fe7bcb22b5
di <unknown>
1997-10-17 22:39:00 +0000

    no message

--- Behavior/instance/testing method dictionary/whichSelectorsReferTo-3A-special-3A-byte-3A-.cs
+++ Behavior/instance/testing method dictionary/whichSelectorsReferTo-3A-special-3A-byte-3A-.cs
@@ -1,17 +1,15 @@
 whichSelectorsReferTo: literal special: specialFlag byte: specialByte
 	"Answer a set of selectors whose methods access the argument as a literal."
-	| who method methodArray |
+	| who method |
 	who _ Set new.
 	methodDict associationsDo:
 		[:assn |
 		method _ assn value.
-		((method pointsTo: literal "faster than hasLiteral:") or:
+		((method hasLiteral: literal) or:
 				[specialFlag and: [method scanFor: specialByte]])
 			ifTrue:
-			[((literal isKindOf: Association) not
+			[((literal isMemberOf: Association) not
 				or: [method sendsToSuper not
-					or: [(method literals copyFrom: 1
-						to: method numLiterals-1)
-							includes: literal]])
+					or: [method literals allButLast includes: literal]])
 				ifTrue: [who add: assn key]]].
 	^who
