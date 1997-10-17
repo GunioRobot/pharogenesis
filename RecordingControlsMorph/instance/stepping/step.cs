78da929de514886e5682f6c5b1406e52c05c2ce1
jm <unknown>
1997-10-17 15:00:00 +0000

    no message

---
+++ RecordingControlsMorph/instance/stepping/step.cs
@@ -0,0 +1,6 @@
+step
+
+	recorder isPaused
+		ifTrue: [recordingStatusLight color: Color transparent]
+		ifFalse: [recordingStatusLight color: Color red].
+	recordMeter extent: (recorder meterLevel + 1) @ recordMeter height.
