3067522ebd1d4f076fa0b330bf14b069e5d6196a
di <unknown>
1997-10-17 22:38:00 +0000

    no message

--- CompiledMethod/instance/literals/hasLiteral-3A-.cs
+++ CompiledMethod/instance/literals/hasLiteral-3A-.cs
@@ -1,6 +1,9 @@
 hasLiteral: literal 
 	"Answer whether the receiver references the argument, literal."
+
+	<primitive: 132>  "a fast primitive operation equivalent to..."
+
 	2 to: self numLiterals + 1 do:
 		[:index |
 		literal == (self objectAt: index) ifTrue: [^ true]].
-	^false
+	^ false
