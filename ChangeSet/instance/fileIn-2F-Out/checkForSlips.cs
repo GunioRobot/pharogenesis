00fe61ee09efe4e8a2df553d0306e7084e64d2da
di <unknown>
1997-10-18 13:01:00 +0000

    no message

---
+++ ChangeSet/instance/fileIn-2F-Out/checkForSlips.cs
@@ -0,0 +1,16 @@
+checkForSlips
+	"Return a collection of method refs with possible debugging code."
+	| slips tsRef changes method |
+	slips _ OrderedCollection new.
+	tsRef _ Smalltalk associationAt: #Transcript.
+	self changedClasses do:
+		[:aClass |
+		changes _ methodChanges at: aClass name ifAbsent: [nil].
+		changes ifNotNil:
+			[changes associationsDo: 
+				[:mAssoc | (#(remove addedThenRemoved) includes: mAssoc value)
+					ifFalse:
+					[method _ aClass compiledMethodAt: mAssoc key.
+					((method hasLiteral: #halt) or: [method hasLiteral: tsRef])
+						ifTrue: [slips add: aClass name , ' ' , mAssoc key]]]]].
+	^ slips
