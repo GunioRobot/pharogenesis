c81881bdf4d717ae5e908d90a372fb9d15fb60d4
jm <unknown>
1997-10-18 12:34:00 +0000

    no message

--- InfiniteForm/instance/displaying/displayOn-3A-at-3A-clippingBox-3A-rule-3A-fillColor-3A-.cs
+++ InfiniteForm/instance/displaying/displayOn-3A-at-3A-clippingBox-3A-rule-3A-fillColor-3A-.cs
@@ -1,30 +1,22 @@
 displayOn: aDisplayMedium at: aDisplayPoint clippingBox: clipRectangle rule: ruleInteger fillColor: aForm
 	"This is the real display message, but it doesn't get used until the new
 	display protocol is installed."
-	| targetBox patternBox |
-	(patternForm class == Pattern)
-		ifTrue:
-			["Use patternForm as a mask for BitBlt"
-			aDisplayMedium fill: clipRectangle 
-				rule: ruleInteger fillColor: patternForm.
-			^ self].
-	(patternForm isKindOf: Form)
-		ifFalse:
-			["A Color-like thing.  Use patternForm as a mask for BitBlt"
-			aDisplayMedium fill: clipRectangle 
-				rule: ruleInteger fillColor: patternForm]
-		ifTrue:
-			["Do it iteratively"
-			targetBox _ aDisplayMedium boundingBox intersect: clipRectangle.
-			patternBox _ patternForm boundingBox.
-			(targetBox left truncateTo: patternBox width)
-				to: targetBox right - 1 by: patternBox width do:
-				[:x |
-				(targetBox top truncateTo: patternBox height)
-					to: targetBox bottom - 1 by: patternBox height do:
-					[:y |
-					patternForm displayOn: aDisplayMedium
-						at: x @ y
-						clippingBox: clipRectangle
-						rule: ruleInteger
-						fillColor: aForm]]]
+	| targetBox patternBox bb |
+	(patternForm isKindOf: Form) ifFalse:
+		[^ aDisplayMedium fill: clipRectangle rule: ruleInteger fillColor: patternForm].
+
+	"Do it iteratively"
+	targetBox _ aDisplayMedium boundingBox intersect: clipRectangle.
+	patternBox _ patternForm boundingBox.
+	bb _ BitBlt destForm: aDisplayMedium sourceForm: patternForm fillColor: aForm
+		combinationRule: ruleInteger destOrigin: 0@0 sourceOrigin: 0@0
+		extent: patternBox extent clipRect: clipRectangle.
+	bb colorMap:
+		(patternForm colormapIfNeededForDepth: aDisplayMedium depth).
+	(targetBox left truncateTo: patternBox width)
+		to: targetBox right - 1 by: patternBox width do:
+		[:x |
+		(targetBox top truncateTo: patternBox height)
+			to: targetBox bottom - 1 by: patternBox height do:
+			[:y |
+			bb destOrigin: x@y; copyBits]]
