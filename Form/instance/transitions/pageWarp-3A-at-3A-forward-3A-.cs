5595708e942d36d75c4b1ce1c28a2134914f9b98
jj <unknown>
1997-10-20 15:55:00 +0000

    no message

---
+++ Form/instance/transitions/pageWarp-3A-at-3A-forward-3A-.cs
@@ -0,0 +1,49 @@
+pageWarp: otherImage at: topLeft forward: forward
+	"Produce a page-turning illusion that gradually reveals otherImage
+	located at topLeft in this form.
+	forward == true means turn pages toward you, else away. [ignored for now]"
+	| pageRect oldPage nSteps buffer p leafRect sourceQuad warp oldBottom d |
+	pageRect _ otherImage boundingBox.
+	oldPage _ self copy: (pageRect translateBy: topLeft).
+	(forward ifTrue: [oldPage] ifFalse: [otherImage])
+		border: pageRect
+		widthRectangle: (Rectangle
+				left: 0
+				right: 2
+				top: 1
+				bottom: 1)
+		rule: Form over
+		fillColor: Color black.
+	oldBottom _ self copy: ((pageRect bottomLeft + topLeft) extent: (pageRect width@(pageRect height//4))).
+	nSteps _ 8.
+	buffer _ Form extent: otherImage extent + (0@(pageRect height//4)) depth: self depth.
+	d _ pageRect topLeft + (0@(pageRect height//4)) - pageRect topRight.
+	1 to: nSteps-1 do:
+		[:i | forward
+			ifTrue: [buffer copy: pageRect from: otherImage to: 0@0 rule: Form over.
+					p _ pageRect topRight + (d * i // nSteps)]
+			ifFalse: [buffer copy: pageRect from: oldPage to: 0@0 rule: Form over.
+					p _ pageRect topRight + (d * (nSteps-i) // nSteps)].
+		buffer copy: oldBottom boundingBox from: oldBottom to: pageRect bottomLeft rule: Form over.
+		leafRect _ pageRect topLeft corner: p x @ (pageRect bottom + p y).
+		sourceQuad _ Array with: pageRect topLeft
+			with: pageRect bottomLeft + (0@p y)
+			with: pageRect bottomRight
+			with: pageRect topRight - (0@p y).
+		warp _ (WarpBlt toForm: buffer)
+				clipRect: leafRect;
+				sourceForm: (forward ifTrue: [oldPage] ifFalse: [otherImage]);
+				combinationRule: Form paint.
+		warp copyQuad: sourceQuad toRect: leafRect.
+		self copy: buffer boundingBox from: buffer to: topLeft rule: Form over.
+		Smalltalk forceDisplayUpdate].
+
+	buffer copy: pageRect from: otherImage to: 0@0 rule: Form over.
+	buffer copy: oldBottom boundingBox from: oldBottom to: pageRect bottomLeft rule: Form over.
+	self copy: buffer boundingBox from: buffer to: topLeft rule: Form over.
+	Smalltalk forceDisplayUpdate.
+"
+1 to: 4 do: [:corner | Display pageWarp:
+				(Form fromDisplay: (10@10 extent: 200@300)) reverse
+			at: 10@10 forward: false]
+"
