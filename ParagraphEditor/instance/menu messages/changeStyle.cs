6c8313f627b73d7734f71ac1815311599b9e65f9
di <unknown>
1997-10-20 20:11:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/changeStyle.cs
+++ ParagraphEditor/instance/menu messages/changeStyle.cs
@@ -1,18 +1,15 @@
 changeStyle
-	"Let user change styles for the current text pane  8/20/96 tk
-	 Moved from experimentalCommand to its own method  8/20/96 sw"
+	"Let user change styles for the current text pane  
+	 Moved from experimentalCommand to its own method  "
 
 	| aList reply style |
-	aList _ (TextConstants at: #StyleNames).
+	aList _ (TextConstants select: [:thang | thang isKindOf: TextStyle])
+			keys asOrderedCollection.
 	reply _ (SelectionMenu labelList: aList selections: aList) startUp.
 	reply ~~ nil ifTrue:
-		[style _ TextConstants at: reply ifAbsent: [self beep. ^ true].
-		style class == TextStyle ifFalse: [self beep. ^ true].
-		paragraph textStyle: style.
+		[(style _ TextStyle named: reply) ifNil: [self beep. ^ true].
+		paragraph textStyle: style copy.
 		paragraph composeAll.
 		self recomputeSelection.
-		Display fill: paragraph clippingRectangle 
-			fillColor: view backgroundColor.	"very brute force"
-		self display.
-		"paragraph changed"].
+		self mvcRedisplay].
 	^ true
