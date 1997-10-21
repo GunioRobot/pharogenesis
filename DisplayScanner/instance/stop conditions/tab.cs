d34fe07f3e4c0d13c4ec09f559a1853fe948808c
di <unknown>
1997-10-21 13:53:00 +0000

    no message

--- DisplayScanner/instance/stop conditions/tab.cs
+++ DisplayScanner/instance/stop conditions/tab.cs
@@ -1,11 +1,14 @@
 tab
+	| oldX |
+	oldX _ destX.
 	destX _ (textStyle alignment == Justified and: [self leadingTab not])
 		ifTrue:		"imbedded tabs in justified text are weird"
 			[destX + (textStyle tabWidth - (line justifiedTabDeltaFor: spaceCount)) max: destX]
 		ifFalse: 
-			[textStyle
-				nextTabXFrom: destX
+			[textStyle nextTabXFrom: destX
 				leftMargin: leftMargin
 				rightMargin: rightMargin].
+	fillBlt == nil ifFalse:
+		[fillBlt destX: oldX destY: destY width: destX - oldX height: height; copyBits].
 	lastIndex _ lastIndex + 1.
 	^ false
