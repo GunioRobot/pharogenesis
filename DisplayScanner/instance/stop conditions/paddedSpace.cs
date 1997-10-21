354fa774e45a5307f1918a76a3448152c3a111b1
di <unknown>
1997-10-21 13:53:00 +0000

    no message

--- DisplayScanner/instance/stop conditions/paddedSpace.cs
+++ DisplayScanner/instance/stop conditions/paddedSpace.cs
@@ -3,8 +3,11 @@ paddedSpace
 	Padding must be added to the base width of the space according to 
 	which space in the line this space is and according to the amount of 
 	space that remained at the end of the line when it was composed."
-
+	| oldX |
 	spaceCount _ spaceCount + 1.
-	lastIndex _ lastIndex + 1.
+	oldX _ destX.
 	destX _ destX + spaceWidth + (line justifiedPadFor: spaceCount).
-	^false
+	fillBlt == nil ifFalse:
+		[fillBlt destX: oldX destY: destY width: destX - oldX height: height; copyBits].
+	lastIndex _ lastIndex + 1.
+	^ false
