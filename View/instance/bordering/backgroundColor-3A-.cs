7aa0e935d78ecaa0e5e93f5cc0820c6c93fb33a7
tk <unknown>
1997-10-21 12:31:00 +0000

    no message

--- View/instance/bordering/backgroundColor-3A-.cs
+++ View/instance/bordering/backgroundColor-3A-.cs
@@ -1,2 +1,6 @@
 backgroundColor: aColor
+	Display depth = 1 ifTrue:
+		[(aColor ~= nil and: [aColor isTransparent not]) ifTrue:
+			["Avoid stipple due to attempts to match non-whites"
+			^ insideColor _ Color white]].
 	insideColor _ aColor
