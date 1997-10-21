b69fc87e9be2233f24fde62dd96e00a087cf55a7
tk <unknown>
1997-10-21 12:31:00 +0000

    no message

---
+++ SketchEditorMorph/instance/actions & preps/rect-3A-.cs
@@ -0,0 +1,28 @@
+rect: evt
+	"While moving corner, just write on the canvas.  When done, write
+on the paintingForm"
+
+	| rect within oldRect now diff cor |
+	rect _ strokeOrigin rect: (now _ evt cursorPoint).
+	evt shiftPressed ifTrue:
+		[diff _ evt cursorPoint - strokeOrigin.
+		now _ strokeOrigin +
+			(Point r: (diff x abs min: diff y abs)*2 degrees:
+(diff degrees // 90 * 90 + 45)).
+		rect _ strokeOrigin rect: now].
+	lastEvent == nil ifFalse:
+		[oldRect _ strokeOrigin rect: lastEvent cursorPoint.
+		lastEvent shiftPressed ifTrue:
+			[diff _ lastEvent cursorPoint - strokeOrigin.
+			cor _ strokeOrigin + (Point r: (diff x abs min:
+diff y abs)*2
+						degrees: (diff degrees //
+90 * 90 + 45)).
+			oldRect _ strokeOrigin rect: cor].
+		within _ (rect containsRect: oldRect).
+		within & (currentColor isTransparent not) ifFalse:
+			["Last draw will stick out, must erase the area"
+			self restoreRect: oldRect]].
+	formCanvas frameAndFillRectangle: rect fillColor: currentColor
+		borderWidth: (palette getNib width) borderColor: Color black.
+	self invalidRect: rect.
