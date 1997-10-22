1d2b38beaa2feed6bf0e523a105389f7d2953a38
jm <unknown>
1997-10-22 07:45:00 +0000

    no message

---
+++ Morph/instance/aug97 additions/slideBackToFormerSituation-3A-.cs
@@ -0,0 +1,16 @@
+slideBackToFormerSituation: evt
+
+	| aForm formerOwner formerPosition aWorld origin startPoint endPoint |
+	aForm _ self imageForm offset: 0@0.
+	formerOwner _ evt hand oldOwner.
+	formerPosition _ evt hand oldPosition.
+	aWorld _ self world.
+	origin _ aWorld viewBox origin.
+	startPoint _ evt hand fullBounds origin + origin.
+	endPoint _ formerPosition + origin.
+	self delete.
+	aWorld displayWorld.
+	aForm slideFrom: startPoint to: endPoint nSteps: 12 delay: 15.
+	formerOwner addMorph: self.
+	self position: formerPosition.
+	self justDroppedInto: formerOwner event: evt.
