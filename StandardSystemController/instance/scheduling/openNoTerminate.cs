1c4106803b57947876ecfcbfb92b6a5f5f63ee34
jm <unknown>
1997-10-22 08:16:00 +0000

    no message

--- StandardSystemController/instance/scheduling/openNoTerminate.cs
+++ StandardSystemController/instance/scheduling/openNoTerminate.cs
@@ -2,6 +2,6 @@ openNoTerminate
 	"Create an area in which the receiver's scheduled view can be displayed. 
 	Make it the active view. Do not terminate the currently active process."
 
-	view resize.
+	view resizeInitially.
 	status _ #open.
 	ScheduledControllers scheduleActiveNoTerminate: self
