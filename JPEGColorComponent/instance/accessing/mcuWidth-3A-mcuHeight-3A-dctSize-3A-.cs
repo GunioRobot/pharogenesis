d27d9076f951111f5662e17178fcb2c0dca7202b
tao <unknown>
1997-10-23 12:29:00 +0000

    no message

---
+++ JPEGColorComponent/instance/accessing/mcuWidth-3A-mcuHeight-3A-dctSize-3A-.cs
@@ -0,0 +1,7 @@
+mcuWidth: mw mcuHeight: mh dctSize: ds
+
+	mcuWidth _ mw.
+	mcuHeight _ mh.
+	dctSize _ ds.
+	hSampleFactor _ mcuWidth // widthInBlocks.
+	vSampleFactor _ mcuHeight // heightInBlocks
