e63c59021cea550bf755a5911b7f629a040b0e5a
tao <unknown>
1997-10-23 11:49:00 +0000

    no message

---
+++ JPEGReadWriter/instance/marker parsing/parseHuffmanTable.cs
@@ -0,0 +1,25 @@
+parseHuffmanTable
+
+	| length markerStart index bits count huffVal isACTable hTable |
+	markerStart _ self position.
+	length _ self nextWord.
+	[self position - markerStart >= length] whileFalse:
+		[index _ self next.
+		isACTable _ (index bitAnd: 16r10) ~= 0.
+		index _ (index bitAnd: 16r0F) + 1.
+		index > HuffmanTableSize
+			ifTrue: [self error: 'image has more than ', HuffmanTableSize printString,
+				' quantization tables'].
+		bits _ self next: 16.
+		count _ bits sum.
+		(count > 256 or: [(count > (length - (self position - markerStart)))])
+			ifTrue: [self error: 'Huffman Table count is incorrect'].
+		huffVal _ self next: count.
+		(hTable _ JPEGHuffmanTable new)
+			bits: bits;
+			values: huffVal.
+		isACTable
+			ifTrue:
+				[self hACTable at: index put: hTable]
+			ifFalse:
+				[self hDCTable at: index put: hTable]].
