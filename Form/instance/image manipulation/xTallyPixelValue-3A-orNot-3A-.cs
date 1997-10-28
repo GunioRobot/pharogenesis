4715f2a82f34b27e2dfee8a9bcd2303ff75f3f50
di <unknown>
1997-10-28 07:49:00 +0000

    no message

--- Form/instance/image manipulation/xTallyPixelValue-3A-orNot-3A-.cs
+++ Form/instance/image manipulation/xTallyPixelValue-3A-orNot-3A-.cs
@@ -1,13 +1,20 @@
 xTallyPixelValue: pv orNot: not
 	"Return an array of the number of pixels with value pv by x-value.
 	Note that if not is true, then this will tally those different from pv."
-	| cm slice |
+	| cm slice countBlt copyBlt |
 	cm _ self newColorMap.		"Map all colors but pv to zero"
 	not ifTrue: [cm atAllPut: 1].		"... or all but pv to one"
 	cm at: pv+1 put: 1 - (cm at: pv+1).
 	slice _ Form extent: 1@height.
+	copyBlt _ (BitBlt destForm: slice sourceForm: self
+				halftoneForm: nil combinationRule: Form over
+				destOrigin: 0@0 sourceOrigin: 0@0 extent: 1 @ slice height
+				clipRect: slice boundingBox) colorMap: cm.
+	countBlt _ (BitBlt toForm: slice)
+				fillColor: (Bitmap with: 0);
+				destRect: (0@0 extent: slice extent);
+				combinationRule: 22.
 	^ (0 to: width-1) collect:
 		[:x |
-		slice copyBits: (x@0 extent: 1@height) from: self at: 0@0
-			colorMap: cm.
-		slice primCountBits]
+		copyBlt sourceOrigin: x@0; copyBits.
+		countBlt copyBits]
