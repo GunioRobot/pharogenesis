62f65fa955eb8310a25e1005768ca5523f07e81a
di <unknown>
1997-10-29 20:20:00 +0000

    no message

--- InputSensor/instance/keyboard/kbdTest.cs
+++ InputSensor/instance/keyboard/kbdTest.cs
@@ -1,6 +1,12 @@
 kbdTest    "Sensor kbdTest"
+	"This test routine will print the unmodified character, its keycode,
+	and the OR of all its modifier bits, until the character x is typed"
 	| char |
+	char _ nil.
 	[char = $x] whileFalse: 
 		[[self keyboardPressed] whileFalse: [].
 		char _ self characterForKeycode: self keyboard.
-		char asciiValue printString , '  ' displayAt: 10@10]
+		(String streamContents: 
+			[:s | s nextPut: char; space; print: char asciiValue;
+					space; print: self primMouseButtons; nextPutAll: '     '])
+			displayAt: 10@10]
