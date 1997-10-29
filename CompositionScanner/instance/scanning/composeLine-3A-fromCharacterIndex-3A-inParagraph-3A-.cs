6ffd59bf72694d58eb84a67c6fc5654f95310fe6
di <unknown>
1997-10-29 12:17:00 +0000

    no message

--- CompositionScanner/instance/scanning/composeLine-3A-fromCharacterIndex-3A-inParagraph-3A-.cs
+++ CompositionScanner/instance/scanning/composeLine-3A-fromCharacterIndex-3A-inParagraph-3A-.cs
@@ -1,15 +1,16 @@
 composeLine: lineIndex fromCharacterIndex: startIndex inParagraph: aParagraph 
-	"Answer an instance of TextLineInterval that represents the next line in the paragraph. "
-	
-	| runLengtrh done stopCondition |
+	"Answer an instance of TextLineInterval that represents the next line in the paragraph."
+	| runLength done stopCondition |
 	spaceX _ destX _ leftMargin _ aParagraph leftMarginForCompositionForLine: lineIndex.
 	destY _ 0.
 	rightMargin _ aParagraph rightMarginForComposition.
 	leftMargin >= rightMargin ifTrue: [self error: 'No room between margins to compose'].
 	lastIndex _ startIndex.	"scanning sets last index"
+	lineHeight _ textStyle lineGrid.  "may be increased by setFont:..."
+	baseline _ textStyle baseline.
 	self setStopConditions.	"also sets font"
-	runLengtrh _ text runLengthFor: startIndex.
-	runStopIndex _ (lastIndex _ startIndex) + (runLengtrh - 1).
+	runLength _ text runLengthFor: startIndex.
+	runStopIndex _ (lastIndex _ startIndex) + (runLength - 1).
 	line _ TextLineInterval
 		start: lastIndex
 		stop: 0
@@ -19,13 +20,10 @@ composeLine: lineIndex fromCharacterIndex: startIndex inParagraph: aParagraph
 	done _ false.
 	[done]
 		whileFalse: 
-			[stopCondition _ super
-				scanCharactersFrom: lastIndex
-				to: runStopIndex
-				in: text string
-				rightX: rightMargin
-				stopConditions: stopConditions
-				displaying: false.
+			[stopCondition _ self scanCharactersFrom: lastIndex to: runStopIndex
+				in: text string rightX: rightMargin stopConditions: stopConditions
+				kern: kern displaying: false.
 			"See setStopConditions for stopping conditions for composing."
 			(self perform: stopCondition)
-				ifTrue: [^line]]
+				ifTrue: [^line lineHeight: lineHeight + textStyle leading
+							baseline: baseline + textStyle leading]]
