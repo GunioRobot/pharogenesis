3839b896a56b9fb6eba06f3ec785319130f20f60
jm <unknown>
1997-10-30 05:47:00 +0000

    no message

---
+++ Interpreter/instance/other primitives/primitiveForceDisplayUpdate.cs
@@ -0,0 +1,4 @@
+primitiveForceDisplayUpdate
+	"On some platforms, this primitive forces enqueued display updates to be processed immediately. On others, it does nothing."
+
+	self ioForceDisplayUpdate.
