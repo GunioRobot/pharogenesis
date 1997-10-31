2447c603baaeda6e40b403d5f650381a6fac78c2
di <unknown>
1997-10-31 09:47:00 +0000

    no message

--- DisplayScanner/instance/scanning/displayLines-3A-in-3A-clippedBy-3A-.cs
+++ DisplayScanner/instance/scanning/displayLines-3A-in-3A-clippedBy-3A-.cs
@@ -1,51 +1,51 @@
-displayLines: linesInterval in: aParagraph clippedBy: visibleRectangle 
+displayLines: linesInterval in: aParagraph clippedBy: visibleRectangle
 	"The central display routine. The call on the primitive 
 	(scanCharactersFrom:to:in:rightX:) will be interrupted according to an 
 	array of stop conditions passed to the scanner at which time the code to 
 	handle the stop condition is run and the call on the primitive continued 
 	until a stop condition returns true (which means the line has 
 	terminated)."
-
-	| runLength done lineGrid lineIndex stopCondition leftInRun fore back |
-	"leftInRun is the # of characters left to scan in the current run; when 0,
-		it is time to call 'self setStopConditions'"
+	| runLength done stopCondition leftInRun |
+	"leftInRun is the # of characters left to scan in the current run;
+		when 0, it is time to call 'self setStopConditions'"
 	leftInRun _ 0.
 	super initializeFromParagraph: aParagraph clippedBy: visibleRectangle.
-	destForm depth > 1 ifTrue:
-		[fore _ aParagraph foregroundColor bitPatternForDepth: destForm depth.
-		back _ aParagraph backgroundColor bitPatternForDepth: destForm depth.
-		self colorMap: (Bitmap with: back first with: fore first)].
+	paragraph _ aParagraph.
+	foregroundColor _ paragraphColor _ aParagraph foregroundColor.
+	backgroundColor _ aParagraph backgroundColor.
+	aParagraph backgroundColor isTransparent
+		ifTrue: [fillBlt _ nil]
+		ifFalse: [fillBlt _ self copy.  "Blt to fill spaces, tabs, margins"
+				fillBlt sourceForm: nil; sourceOrigin: 0@0.
+				fillBlt fillColor: aParagraph backgroundColor].
 	rightMargin _ aParagraph rightMarginForDisplay.
-	lineGrid _ textStyle lineGrid.
-	lineY _ destY _ aParagraph topAtLineIndex: linesInterval first.
+	lineY _ aParagraph topAtLineIndex: linesInterval first.
 	linesInterval do: 
 		[:lineIndex | 
-		runX _ destX _ leftMargin _ 
-			aParagraph leftMarginForDisplayForLine: lineIndex.
+		leftMargin _ aParagraph leftMarginForDisplayForLine: lineIndex.
+		runX _ destX _ leftMargin.
 		line _ aParagraph lines at: lineIndex.
+		lineHeight _ line lineHeight.
+		fillBlt == nil ifFalse:
+			[fillBlt destX: visibleRectangle left destY: lineY
+				width: leftMargin - visibleRectangle left height: lineHeight; copyBits].
 		lastIndex _ line first.
-		leftInRun<= 0 
-			ifTrue:
-				[self setStopConditions.
-				"also sets the font"
-				leftInRun _ text runLengthFor: line first].
+		leftInRun <= 0
+			ifTrue: [self setStopConditions.  "also sets the font"
+					leftInRun _ text runLengthFor: line first].
+		destY _ lineY + line baseline - font ascent.  "Should have happened in setFont"
 		runLength _ leftInRun.
-		destY _ lineY + (textStyle baseline - font ascent).
-		"fontAscent delta"
-		(runStopIndex _ lastIndex + (runLength - 1)) > line last 
-			ifTrue: [runStopIndex _ line last].
+		runStopIndex _ lastIndex + (runLength - 1) min: line last.
 		leftInRun _ leftInRun - (runStopIndex - lastIndex + 1).
 		spaceCount _ 0.
 		done _ false.
 		[done]
 			whileFalse: 
-				[stopCondition _ 
-					self scanCharactersFrom: lastIndex
-						to: runStopIndex
-						in: text string
-						rightX: rightMargin
-						stopConditions: stopConditions
-						displaying: true.
-				"see setStopConditions for stopping conditions for displaying."
-				done _ self perform: stopCondition].
-		lineY _ lineY + lineGrid]
+			[stopCondition _ self scanCharactersFrom: lastIndex to: runStopIndex
+						in: text string rightX: rightMargin stopConditions: stopConditions
+						kern: kern displaying: true.
+		"see setStopConditions for stopping conditions for displaying."
+		done _ self perform: stopCondition].
+		fillBlt == nil ifFalse:
+			[fillBlt destX: destX destY: lineY width: visibleRectangle right-destX height: lineHeight; copyBits].
+		lineY _ lineY + lineHeight]
