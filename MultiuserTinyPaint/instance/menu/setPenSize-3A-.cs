3c37b7aed9e3c8a4896a296ce592734ee422c146
jm <unknown>
1997-11-04 07:46:00 +0000

    no message

---
+++ MultiuserTinyPaint/instance/menu/setPenSize-3A-.cs
@@ -0,0 +1,12 @@
+setPenSize: evt
+
+	| menu sizes |
+	menu _ MenuMorph new.
+	sizes _ (0 to: 5), (6 to: 12 by: 2), (15 to: 40 by: 5).
+	sizes do: [:w |
+		menu add: w printString
+			target: self
+			selector: #penSize:hand:
+			argumentList: (Array with: w with: evt hand)].
+
+	menu popUpAt: evt hand position event: evt.
