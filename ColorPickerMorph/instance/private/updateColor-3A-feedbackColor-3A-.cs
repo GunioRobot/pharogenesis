b2179eb9322e325013f1f2531fde85511fcaee3c
jm <unknown>
1997-11-04 07:46:00 +0000

    no message

---
+++ ColorPickerMorph/instance/private/updateColor-3A-feedbackColor-3A-.cs
@@ -0,0 +1,9 @@
+updateColor: aColor feedbackColor: feedbackColor
+	"Set my selected color to the given color if it is different. Give user feedback. Inform the target of the change if the target and selector are not nil." 
+
+	selectedColor = aColor ifTrue: [^ self].  "do nothing if color doesn't change"
+
+	originalForm fill: FeedbackBox fillColor: feedbackColor.
+	self form: originalForm.
+	selectedColor _ aColor.
+	updateContinuously ifTrue: [self updateTargetColor].
