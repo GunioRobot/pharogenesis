7b41eeeed97f4ad7ecbd0618c2dfa3a4c27787fc
di <unknown>
1997-11-06 19:33:00 +0000

    no message

--- SystemDictionary/instance/snapshot and quit/saveAs.cs
+++ SystemDictionary/instance/snapshot and quit/saveAs.cs
@@ -1,17 +1,20 @@
 saveAs
-	| dir newName |
+	| dir newName imageSuffix changesSuffix |
 	dir _ FileDirectory default.
+	imageSuffix _ self fileNameEnds first.
+	changesSuffix _ self fileNameEnds last.
 	newName _ (FillInTheBlank request: 'New File Name?' 					initialAnswer: 'NewImageName') asFileName.
-	(newName endsWith: '.image') ifTrue:
-		[newName _ newName copyFrom: 1 to: newName size - 6].
-	(dir includesKey: newName , '.image')
-		| (dir includesKey: newName , '.changes') ifTrue:
+	(newName endsWith: imageSuffix) ifTrue:
+		[newName _ newName copyFrom: 1 to: newName size - imageSuffix size].
+	(dir includesKey: newName , imageSuffix)
+		| (dir includesKey: newName , changesSuffix) ifTrue:
 		[^ self notify: newName , ' is already in use
 Please choose another name.'].
-	dir copyFileNamed: self changesName toFileNamed: newName , '.changes'.
+	dir copyFileNamed: self changesName toFileNamed: newName , changesSuffix.
 	self logChange: '----SAVEAS ' , newName , '----'
 		, Date dateAndTimeNow printString.
-	self imageName: newName , '.image'.
+	self imageName: newName , imageSuffix.
+	LastImageName _ self imageName.
 	self closeSourceFiles; openSourceFiles.
 	"Just so SNAPSHOT appears on the new file, and not the old"
 	self snapshot: true andQuit: false.
