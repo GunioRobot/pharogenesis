0cb70418bd69f5d5a0ecba2cb9619afc05690e69
dhhi <unknown>
1997-11-06 16:48:00 +0000

    no message

--- Utilities/class/identification/authorInitials.cs
+++ Utilities/class/identification/authorInitials.cs
@@ -1,4 +1,5 @@
 authorInitials
-	"Answer the initials to be used to identify the current code author.  1/18/96 sw"
+	"Answer the initials to be used to identify the current code author.  "
 
+	[AuthorInitials isEmpty] whileTrue: [self setAuthorInitials].
 	^ AuthorInitials
