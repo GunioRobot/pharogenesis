9b312ecbeffdaf4bf0ab90c96c7988a81675e941
di <unknown>
1997-11-12 09:30:00 +0000

    no message

---
+++ TextMorph/instance/private/text-3A-textStyle-3A-wrap-3A-color-3A-predecessor-3A-successor-3A-.cs
@@ -0,0 +1,10 @@
+text: t textStyle: s wrap: wrap color: c
+	predecessor: pred successor: succ
+	"Private -- for use only in morphic duplication"
+	text _ t.
+	textStyle _ s.
+	wrapFlag _ wrap.
+	color _ c.
+	paragraph _ editor _ container _ nil.
+	predecessor _ pred.
+	successor _ succ
