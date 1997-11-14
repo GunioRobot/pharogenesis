e34d6cc2179fde317389110a47d43a18148873e3
jm <unknown>
1997-11-14 11:21:00 +0000

    no message

---
+++ SoundMorph/instance/all/justDroppedInto-3A-event-3A-.cs
@@ -0,0 +1,6 @@
+justDroppedInto: aMorph event: anEvent
+	| relPosition |
+	relPosition _ self position - aMorph innerBounds topLeft.
+	relPosition _ (relPosition x roundTo: 8) @ relPosition y.
+	self position: aMorph innerBounds topLeft + relPosition.
+	sound copy play
