98ed80b86598067b5cb42440ad68cd24fd565c5c
jm <unknown>
1997-11-16 09:17:00 +0000

    no message

---
+++ HandMorph/class/all/initialize.cs
@@ -0,0 +1,8 @@
+initialize
+	"HandMorph initialize"
+
+	ColorChart _ nil.
+	DoubleClickTime _ 280.
+	NormalCursor _ ColorForm mappingWhiteToTransparentFrom: Cursor normal.
+	BlankCursor _ ColorForm mappingWhiteToTransparentFrom: Cursor blank.
+	UseHardwareCursor _ false.
