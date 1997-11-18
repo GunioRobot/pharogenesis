12524ded8c38560f95bbf39e92a7d5adb44c5ed6
mjg <unknown>
1997-11-18 09:38:00 +0000

    no message

---
+++ HTMLformatter/class/note support/showList-3A-prefix-3A-.cs
@@ -0,0 +1,14 @@
+showList: aCollection prefix: prefix
+	"Return an HTML list to reference an ordered collection of notes. prefix.index will be the reference URL for the note. Used in Comment application."
+	| ret aNote |
+	ret _ WriteStream on: String new.
+	ret nextPutAll: '<ul>' ; cr.
+	1 to: aCollection size do: [:each |
+		ret nextPutAll: '<li>'.
+		aNote := aCollection at: each.
+		ret nextPutAll: (self linkTo: (prefix , each printString) label: (aNote title)).
+		ret nextPutAll: ' ',aNote author,' ',aNote timestamp ; cr.
+		aNote children size > 0 ifTrue: 
+			[ret nextPutAll: (self showList: aNote children prefix: (prefix , each printString))].].
+	ret nextPutAll: '</ul>'.
+	^ret contents
