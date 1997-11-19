2c769918558efcee8e8228c75c8df72ac749a4d1
mjg <unknown>
1997-11-19 10:48:00 +0000

    no message

---
+++ HTMLformatter/class/note support/showListWithAuthors-3A-.cs
@@ -0,0 +1,20 @@
+showListWithAuthors: aCollection
+	"Return an HTML list to reference an ordered collection of notes. 
+	prefix.index will be the reference URL for the note. Special version for 
+	linking to author's homepage, if any."
+	| ret aNote |
+	ret _ WriteStream on: String new.
+	ret nextPutAll: '<ul>';
+	 cr.
+	1 to: aCollection size do: 
+		[:each | 
+		ret nextPutAll: '<li>'.
+		aNote _ aCollection at: each.
+		ret nextPutAll: (self linkTo: (aNote url) label: aNote title)
+				, '-'.
+		ret nextPutAll: (self linkTo: aNote author homepage label: aNote author name)
+				, '-' , aNote timestamp;
+		 cr.
+		aNote children size > 0 ifTrue: [ret nextPutAll: (self showListWithAuthors: aNote children)]].
+	ret nextPutAll: '</ul>'.
+	^ ret contents
