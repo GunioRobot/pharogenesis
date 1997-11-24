6fd6fb22c53aaa9b9ff223585cfafa1502716dac
jm <unknown>
1997-11-24 16:04:00 +0000

    no message

---
+++ RestSound/instance/sound generation/mixSampleCount-3A-into-3A-startingAt-3A-leftVol-3A-rightVol-3A-.cs
@@ -0,0 +1,5 @@
+mixSampleCount: n into: aSoundBuffer startingAt: startIndex leftVol: leftVol rightVol: rightVol
+	"Play silence for a given duration."
+	"(RestSound dur: 1.0) play"
+
+	count _ count - n.
