5345772824b8b9ab238fb6d10cf9b1484eab6c5b
tk <unknown>
1997-11-24 16:02:00 +0000

    no message

--- TextStyle/instance/fonts and font indexes/collectionFromFileNamed-3A-.cs
+++ TextStyle/instance/fonts and font indexes/collectionFromFileNamed-3A-.cs
@@ -1,22 +1,18 @@
 collectionFromFileNamed: fileName
-	"Read the file.  It is an Array of StrikeFonts.  File format is the ReferenceStream version 2 format.  For any fonts with new names, add them to DefaultTextStyle.fontArray.  
-	To write out fonts: (TextStyle default fontArray saveOnFile2).
+	"Read the file.  It is an Array of StrikeFonts.  File format is the ReferenceStream format.  (Do not use SmartRefStream, it is too smart.  It only writes a DiskProxy!)   For any fonts with new names, add them to DefaultTextStyle.fontArray.  
+	To write out fonts: 
+		| ff | ff _ ReferenceStream fileNamed: 'new fonts'.
+		ff nextPut: (TextStyle default fontArray).
+		ff close.
 	To read: (TextStyle default collectionFromFileNamed: 'new fonts')
-*** Do not remove this method *** 8/19/96 tk"
+*** Do not remove this method *** "
 
 	| ff this names |
 	ff _ ReferenceStream fileNamed: fileName.
-	[this _ ff next.
-		this class == SmallInteger ifTrue: ["version number"].
-		this class == Array ifTrue:
-			[(this at: 1) = 'class structure' ifTrue:
-				["Verify the shapes of all the classes"
-				(Smalltalk incomingObjectsClass acceptStructures: this) ifFalse:
-					[^ ff close]]].	"An error occurred"
-		this class == Array ifTrue:
+	this _ ff nextAndClose.
+		"Only works if file created by ReferenceStream, not SmartRefStream"
+	this class == Array ifTrue:
 			[names _ self fontNames.
 			this do: [:each | each class == StrikeFont ifTrue:
 				[(names includes: each name) ifFalse:
 					[fontArray _ fontArray copyWith: each]]]].
-		ff atEnd]  whileFalse.		 
-	ff close.
