70fd1b3b3decc81d17c5efcf9e360c3828df1da4
di <unknown>
1997-11-29 08:46:00 +0000

    no message

---
+++ CompositionScanner/instance/scanning/composeFrom-3A-inRectangle-3A-firstLine-3A-leftSide-3A-rightSide-3A-.cs
@@ -0,0 +1,33 @@
+composeFrom: startIndex inRectangle: lineRectangle
+	firstLine: firstLine leftSide: leftSide rightSide: rightSide
+	"Answer an instance of TextLineInterval that represents the next line in the paragraph."
+	| runLength done stopCondition |
+	"Set up margins"
+	leftMargin _ lineRectangle left.
+	leftSide ifTrue: [leftMargin _ leftMargin +
+						(firstLine ifTrue: [textStyle firstIndent]
+								ifFalse: [textStyle restIndent])].
+	spaceX _ destX _ leftMargin.
+	rightMargin _ lineRectangle right.
+	rightSide ifTrue: [rightMargin _ rightMargin - textStyle rightIndent].
+	lastIndex _ startIndex.	"scanning sets last index"
+	destY _ lineRectangle top.
+	lineHeight _ baseline _ 0.  "Will be increased by setFont"
+	self setStopConditions.	"also sets font"
+	runLength _ text runLengthFor: startIndex.
+	runStopIndex _ (lastIndex _ startIndex) + (runLength - 1).
+	line _ (TextLine start: lastIndex stop: 0 internalSpaces: 0 paddingWidth: 0)
+				rectangle: lineRectangle;
+				leftMargin: leftMargin.
+	
+	spaceCount _ 0.
+	done _ false.
+	[done]
+		whileFalse: 
+			[stopCondition _ self scanCharactersFrom: lastIndex to: runStopIndex
+				in: text string rightX: rightMargin stopConditions: stopConditions
+				kern: kern displaying: false.
+			"See setStopConditions for stopping conditions for composing."
+			(self perform: stopCondition)
+				ifTrue: [^ line lineHeight: lineHeight + textStyle leading
+							baseline: baseline + textStyle leading]]
