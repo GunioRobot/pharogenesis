c78a15d7b435dd5aa4dbf6444f6e8e36fa44223c
jm <unknown>
1997-12-01 19:22:00 +0000

    no message

--- Form/instance/image manipulation/tallyPixelValuesPrimitive-3A-into-3A-.cs
+++ Form/instance/image manipulation/tallyPixelValuesPrimitive-3A-into-3A-.cs
@@ -1,8 +1,5 @@
 tallyPixelValuesPrimitive: destRect into: valueTable
-	"Tally the selected pixels of this form into the valueTable, which is
-	a bitmap similar to a color map.  Since the underlying BitBlt function
-	that performs the tally does not do bit-boundary clipping, the
-	tallies for any word-boundary fringes must be subtracted."
+	"Tally the selected pixels of this form into valueTable, a Bitmap similar to a color map, and return valueTable. Since the BitBlt function that performs the tally does not do bit-boundary clipping, the tallies for any word-boundary fringes must be subtracted by the sender."
 
 	(BitBlt toForm: self)
 		sourceForm: self;  "src must be given for color map ops"
