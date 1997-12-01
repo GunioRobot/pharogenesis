f42f88a92a696fec427eea4aeda9d39a2e97afa2
ikp <unknown>
1997-12-01 22:21:00 +0000

    no message

---
+++ DynamicInterpreter/instance/assert-2D-verify/assertIsLegalTranslatedInstructionPointer-3A-in-3A-.cs
@@ -0,0 +1,4 @@
+assertIsLegalTranslatedInstructionPointer: t1 in: t2 
+	self inline: false.
+	t1 < (t2 + BaseHeaderSize + 4 - 1) ifTrue: [self error: 'ip points before first bytecode'].
+	t1 > (t2 + (self sizeBitsOf: t2) - 1) ifTrue: [self error: 'ip points after last bytecode']
