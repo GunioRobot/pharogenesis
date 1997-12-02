3eb86bd295ab89b8f34b10824dd3af2ae1160a49
di <unknown>
1997-12-02 14:30:00 +0000

    no message

--- CharacterBlockScanner/instance/private/buildCharacterBlockIn-3A-.cs
+++ CharacterBlockScanner/instance/private/buildCharacterBlockIn-3A-.cs
@@ -1,33 +1,29 @@
-buildCharacterBlockIn: aText
-
+buildCharacterBlockIn: para
 	| lineIndex runLength lineStop done stopCondition |
 	"handle nullText"
-	(aText numberOfLines = 0 or: [text size = 0])
-		ifTrue:	[^CharacterBlock
-					stringIndex: 1	"like being off end of string"
-					character: nil
-					topLeft: ((aText leftMarginForDisplayForLine: 1) @
-								(aText compositionRectangle) top)
-					extent: (0 @ textStyle lineGrid)].
-
+	(para numberOfLines = 0 or: [text size = 0])
+		ifTrue:	[^ CharacterBlock new stringIndex: 1  "like being off end of string"
+					text: para text
+					topLeft: (para leftMarginForDisplayForLine: 1)
+								@ para compositionRectangle top
+					extent: 0 @ textStyle lineGrid].
 	"find the line"
-	lineIndex _ aText lineIndexOfTop: characterPoint y.
-	destY _ (aText topAtLineIndex: lineIndex).
-	line _ aText lines at: lineIndex.
-	rightMargin _ aText rightMarginForDisplay.
+	lineIndex _ para lineIndexOfTop: characterPoint y.
+	destY _ para topAtLineIndex: lineIndex.
+	line _ para lines at: lineIndex.
+	rightMargin _ para rightMarginForDisplay.
 
-	(lineIndex = aText numberOfLines and:
-		[(destY + textStyle lineGrid) < characterPoint y])
+	(lineIndex = para numberOfLines and:
+		[(destY + line lineHeight) < characterPoint y])
 			ifTrue:	["if beyond lastLine, force search to last character"
-					characterPoint x: rightMargin]
-			ifFalse:	[characterPoint y < (aText compositionRectangle) top
+					self characterPointSetX: rightMargin]
+			ifFalse:	[characterPoint y < (para compositionRectangle) top
 						ifTrue: ["force search to first line"
-								characterPoint _
-								(aText compositionRectangle) topLeft].
+								characterPoint _ (para compositionRectangle) topLeft].
 					characterPoint x > rightMargin
-						ifTrue:	[characterPoint x: rightMargin]].
-	destX _ leftMargin _ aText leftMarginForDisplayForLine: lineIndex.
-	nextLeftMargin_ aText leftMarginForDisplayForLine: lineIndex+1.
+						ifTrue:	[self characterPointSetX: rightMargin]].
+	destX _ leftMargin _ para leftMarginForDisplayForLine: lineIndex.
+	nextLeftMargin_ para leftMarginForDisplayForLine: lineIndex+1.
 	lastIndex _ line first.
 
 	self setStopConditions.		"also sets font"
@@ -37,20 +33,17 @@ buildCharacterBlockIn: aText
 		ifFalse:	[lineStop _ line last].
 	(runStopIndex _ lastIndex + (runLength - 1)) > lineStop
 		ifTrue:	[runStopIndex _ lineStop].
-	lastCharacterExtent _ 0 @ textStyle lineGrid.
+	lastCharacterExtent _ 0 @ line lineHeight.
 	spaceCount _ 0. done  _ false.
 
 	[done]
 	whileFalse:
 	[stopCondition _ self scanCharactersFrom: lastIndex to: runStopIndex
 			in: text string rightX: characterPoint x
-			stopConditions: stopConditions displaying: false.
+			stopConditions: stopConditions kern: kern displaying: false.
 
 	"see setStopConditions for stopping conditions for character block 	operations."
-	lastCharacterExtent x: (font widthOf: (text at: lastIndex)).
+	self lastCharacterExtentSetX: (font widthOf: (text at: lastIndex)).
 	(self perform: stopCondition)
-		ifTrue:	[^CharacterBlock
-					stringIndex: lastIndex
-					character: lastCharacter
-					topLeft: characterPoint
-					extent: lastCharacterExtent]]
+		ifTrue:	[^ CharacterBlock new stringIndex: lastIndex text: text
+					topLeft: characterPoint extent: lastCharacterExtent]]
