eb92d96127f1d5e582e0b8d95f2c0d0ac0b4e26e
jm <unknown>
1997-12-03 22:15:00 +0000

    no message

--- Bitmap/instance/filing/writeOn-3A-.cs
+++ Bitmap/instance/filing/writeOn-3A-.cs
@@ -1,5 +1,16 @@
 writeOn: aStream 
-	"Store the array of bits onto the argument, aStream."
+	"Store the array of bits onto the argument, aStream.  Use a simple run-coded compression of the receiver into a byteArray:
+		First 4 bytes are the size of the original Bitmap *negated*.
+		This is followed by a number of runs...
+		[0 means end of runs]
+		[n = 1..127] [(n+3) copies of next byte]
+		[n = 128..191] [(n-127) next bytes as is]
+		[n = 192..255] [(n-190) copies of next 4 bytes].
+	Difference from compressToByteArray is that first word is size negated here."
+	"Optimization: compress in memory, then store on aStream; 30% faster for FileStreams"
 
-	aStream nextInt32Put: self size.
-	aStream nextPutAll: self
+	| s |
+	s _ WriteStream on: (ByteArray new: self size * 4).
+	self compressToStream: s.
+	aStream nextInt32Put: self size negated.
+	aStream nextPutAll: s contents.
