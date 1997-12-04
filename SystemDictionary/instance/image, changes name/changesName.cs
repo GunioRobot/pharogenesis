9a21bb97ac0a26de1a1fbd1dd3f100133a317875
jm <unknown>
1997-12-04 22:37:00 +0000

    no message

--- SystemDictionary/instance/image, changes name/changesName.cs
+++ SystemDictionary/instance/image, changes name/changesName.cs
@@ -1,9 +1,8 @@
-changesName  "Smalltalk changesName"
-	"Answer the current name for the changes file that matches the image file name"
-	| imName index |
-	FileDirectory splitName: self imageName
-		to: [:volName :fileName | imName _ fileName].
-	imName size > 5 ifTrue: 
-		[(index _ (imName findString: '.image' startingAt: imName size - 5)) > 0 ifTrue: 
-			[^(imName copyFrom: 1 to: index-1), '.changes']].
-	^imName, '.changes'
+changesName
+	"Answer the local name for the changes file corresponding to the image file name."
+	"Smalltalk changesName"
+
+	| imName ends |
+	imName _ FileDirectory localNameFor: self imageName.
+	ends _ self fileNameEnds.
+	^ (imName copyFrom: 1 to: imName size - ends first size), ends last
