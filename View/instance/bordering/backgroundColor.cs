a59b96291245256cdceee5fce41ea930c3df1eec
di <unknown>
1997-12-04 08:48:00 +0000

    no message

--- View/instance/bordering/backgroundColor.cs
+++ View/instance/bordering/backgroundColor.cs
@@ -1,7 +1,8 @@
 backgroundColor
+	Display depth <= 2 ifTrue: [^ Color white].
 	insideColor == nil ifFalse:
 		[(insideColor isMemberOf: Symbol) ifTrue:
 			[^ Color perform: insideColor].
 		^ insideColor].
 	superView == nil ifFalse: [^ superView backgroundColor].
-	^ Display white
+	^ Color white
