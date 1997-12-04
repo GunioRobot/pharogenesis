44e47086bda6902e4353d08e5a5fa555bf7f39a7
jm <unknown>
1997-12-04 22:25:00 +0000

    no message

--- SystemDictionary/instance/image, changes name/imagePath.cs
+++ SystemDictionary/instance/image, changes name/imagePath.cs
@@ -2,6 +2,4 @@ imagePath
 	"Answer the path for the directory containing the image file."
 	"Smalltalk imagePath"
 
-	^ FileDirectory
-		splitName: self imageName
-		to: [ :volName :fileName | ^ volName ]
+	^ FileDirectory dirPathFor: self imageName
