780ae99741e65ff02d104ec603c700da1d6c71e4
jm <unknown>
1997-12-05 11:45:00 +0000

    no message

--- FileList/instance/menu messages/copyName.cs
+++ FileList/instance/menu messages/copyName.cs
@@ -1,4 +1,4 @@
 copyName
+
 	listIndex = 0 ifTrue: [^ self].
-	ParagraphEditor new clipboardTextPut: 
-		(FileDirectory default localNameFor: self fullName) asText
+	ParagraphEditor new clipboardTextPut: self fullName asText.
