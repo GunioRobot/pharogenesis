5f6afe336386ce7bf499666d697972b5631067d5
jm <unknown>
1997-12-05 18:29:00 +0000

    no message

---
+++ ColorPickerMorph/class/all/initialize.cs
@@ -0,0 +1,5 @@
+initialize
+	"ColorPickerMorph initialize"
+	ColorChart _ Color colorPaletteForDepth: 16 extent: 190@60.
+	TransparentBox _ ColorChart boundingBox withHeight: 9.
+	FeedbackBox _ (ColorChart width-20)@0 extent: 20@9.
