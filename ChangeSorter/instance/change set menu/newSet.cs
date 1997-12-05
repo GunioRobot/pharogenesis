48fb1f027f890aa702e0ceff323163457eb7ffc9
di <unknown>
1997-12-05 09:25:00 +0000

    no message

--- ChangeSorter/instance/change set menu/newSet.cs
+++ ChangeSorter/instance/change set menu/newSet.cs
@@ -2,7 +2,7 @@ newSet
 	"Create a new changeSet and show it.  For splitting an existing one that is showing in the other pane..  1991-tck.
 	 3/9/96 sw: make the new guy the current one, corresponding to 99.5% of normal use.  Also, reject name if already in use."
 
-	| newName |
+	| newName newSet |
 	newName _ FillInTheBlank request: 'A name for the new change set'
 			initialAnswer: ChangeSet defaultName.
 
@@ -12,10 +12,7 @@ newSet
 			[self inform: 'Sorry that name is already used'.
 			^ self].
 
-	myChangeSet _ ChangeSet new initialize.
-	myChangeSet name: newName.
-	AllChangeSets add: myChangeSet.
-	buttonView label: myChangeSet name asParagraph.
-	buttonView display.
+	newSet _ ChangeSet new initialize name: newName.
+	AllChangeSets add: newSet.
+	self showChangeSet: newSet.
 	self newCurrent.
-	self changed: #set
