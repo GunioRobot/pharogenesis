4e219c88423cf8da505f6e6a9d896a5a0cb88d49
jm <unknown>
1997-12-05 12:54:00 +0000

    no message

--- StandardFileStream/class/file creation/readOnlyFileNamed-3A-.cs
+++ StandardFileStream/class/file creation/readOnlyFileNamed-3A-.cs
@@ -1,6 +1,16 @@
 readOnlyFileNamed: aFileName
 	"Open a file of the given name for read-only access.  1/31/96 sw"
-	| f |
-	f _ self new open: aFileName forWrite: false.
-	f == nil ifTrue: [self halt: 'Could not find ' , (self localNameFor: aFileName)].
-	^ f
+	| selection |
+	(self isAFileNamed: aFileName) ifTrue:
+		[^ self new open: aFileName forWrite: false].
+
+	"File does not exist..."
+	selection _ (PopUpMenu labels: 'choose another name
+cancel')
+			startUpWithCaption: (FileDirectory localNameFor: aFileName) , '
+does not exist.'.
+	selection = 1 ifTrue:
+		[^ self readOnlyFileNamed:
+			(FillInTheBlank request: 'Enter a new file name'
+						initialAnswer: (FileDirectory localNameFor: aFileName))].
+	self halt
