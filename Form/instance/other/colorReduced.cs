f32f5d853b46e4aed1cc555d958c123f188a775b
jm <unknown>
1997-12-05 19:44:00 +0000

    no message

---
+++ Form/instance/other/colorReduced.cs
@@ -0,0 +1,36 @@
+colorReduced
+	"Return a color-reduced ColorForm version of the receiver, if possible, or the receiver itself if not."
+
+	| tally tallyDepth colorCount newForm cm newFormColors nextColorIndex |
+	tally _ self tallyPixelValues asArray.
+	tallyDepth _ (tally size log: 2) asInteger.
+	colorCount _ 0.
+	tally do: [:n | n > 0 ifTrue: [colorCount _ colorCount + 1]].
+	(tally at: 1) = 0 ifTrue: [colorCount _ colorCount + 1].  "include transparent"
+	colorCount > 256 ifTrue: [^ self].  "cannot reduce"
+	newForm _ self formForColorCount: colorCount.
+
+	"build an array of just the colors used, and a color map to translate
+	 old pixel values to their indices into this color array"
+	cm _ Bitmap new: tally size.
+	newFormColors _ Array new: colorCount.
+	newFormColors at: 1 put: Color transparent.
+	nextColorIndex _ 2.
+	2 to: cm size do: [:i |
+		(tally at: i) > 0 ifTrue: [
+			newFormColors
+				at: nextColorIndex
+				put: (Color colorFromPixelValue: i - 1 depth: tallyDepth).
+			cm at: i put: nextColorIndex - 1.  "pixel values are zero-based indices"
+			nextColorIndex _ nextColorIndex + 1]].
+
+	"copy pixels into new ColorForm, mapping to new pixel values"
+	newForm copyBits: self boundingBox
+		from: self
+		at: 0@0
+		clippingBox: self boundingBox
+		rule: Form over
+		fillColor: nil
+		map: cm.
+	newForm colors: newFormColors.
+	^ newForm
