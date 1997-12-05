99e8e915a02783cb19aed9ec19ca3ddbc5a5abd1
jm <unknown>
1997-12-05 12:54:00 +0000

    no message

--- StandardFileStream/class/file creation/oldFileNamed-3A-.cs
+++ StandardFileStream/class/file creation/oldFileNamed-3A-.cs
@@ -2,9 +2,20 @@ oldFileNamed: aFileName
  	"Open a file in the default directory (or in the directory contained
 	in the input arg); by default, it's available for reading.  2/12/96 sw
 	Prior contents will be overwritten, but not truncated on close.  3/18 di"
+	| selection |
+	(self isAFileNamed: aFileName) ifTrue:
+		[^ self new open: aFileName forWrite: true].
 
-	(self isAFileNamed: aFileName) ifFalse:
-		[(self confirm: 'Could not find ' , (self localNameFor: aFileName) , '.
-Do you want to create it?')
-			ifFalse: [self halt]].
-	^ self new open: aFileName forWrite: true
+	"File does not exist..."
+	selection _ (PopUpMenu labels: 'create a new file
+choose another name
+cancel')
+			startUpWithCaption: (FileDirectory localNameFor: aFileName) , '
+does not exist.'.
+	selection = 1 ifTrue:
+		[^ self new open: aFileName forWrite: true].
+	selection = 2 ifTrue:
+		[^ self oldFileNamed:
+			(FillInTheBlank request: 'Enter a new file name'
+						initialAnswer: (FileDirectory localNameFor: aFileName))].
+	self halt
