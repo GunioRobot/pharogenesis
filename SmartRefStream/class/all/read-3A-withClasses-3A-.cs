f99f0ab84f9985e553b09f911a41d57fd2f6c8e0
tk <unknown>
1997-12-09 21:31:00 +0000

    no message

---
+++ SmartRefStream/class/all/read-3A-withClasses-3A-.cs
@@ -0,0 +1,10 @@
+read: aByteStream withClasses: structureArray
+	"Read an object off the stream, but first check structureArray against the current system."
+
+	| me |
+	me _ self on: aByteStream.
+	me noHeader.
+	me structures: (structureArray at: 2).
+	me superclasses: (structureArray at: 4).
+	(me verifyStructure = 'conversion method needed') ifTrue: [^ nil].
+	^ super next
