81f4b61ef2fc16e8ac838fa658f9e6940789bb20
tk <unknown>
1997-12-12 10:41:00 +0000

    no message

--- RemoteString/instance/private/string-3A-onFileNumber-3A-toFile-3A-.cs
+++ RemoteString/instance/private/string-3A-onFileNumber-3A-toFile-3A-.cs
@@ -1,8 +1,8 @@
-string: aString onFileNumber: anInteger toFile: aFileStream 
-	"Store this as the receiver's string if source files exist."
+string: aStringOrText onFileNumber: fileNumber toFile: aFileStream 
+	"Store this as the receiver's text if source files exist. If aStringOrText is a Text, store a marker with the string part, and then store the runs of TextAttributes in the next chunk."
 
 	| position |
-	sourceFileNumber _ anInteger.
-	filePositionHi _ (position _ aFileStream position) bitShift: -8.
-	filePositionLo _ position bitAnd: 255.
-	aFileStream nextChunkPut: aString
+	position _ aFileStream position.
+	self fileNumber: fileNumber position: position.
+	aFileStream nextChunkPutWithStyle: aStringOrText
+	"^ self		(important)"
