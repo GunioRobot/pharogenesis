26635062ba1a3f4fa3c962aa5c3027a34454c645
tk <unknown>
1997-12-12 13:01:00 +0000

    no message

--- Browser/instance/message list/selectedMessage.cs
+++ Browser/instance/message list/selectedMessage.cs
@@ -1,8 +1,29 @@
 selectedMessage
 	"Answer a copy of the source code for the selected message selector."
+	| class selector method tempNames |
+	contents == nil ifFalse: [^ contents copy].
+	class _ self selectedClassOrMetaClass.
+	selector _ self selectedMessageName.
+	method _ class compiledMethodAt: selector.
 
-	contents == nil 
-		ifTrue: [contents _ 
-					self selectedClassOrMetaClass 
-						sourceCodeAt: self selectedMessageName].
-	^contents copy
+	(Sensor controlKeyPressed
+		or: [method fileIndex > 0 and: [(SourceFiles at: method fileIndex) == nil]])
+		ifTrue:
+		["Emergency or no source file -- decompile without temp names"
+		contents _ (class decompilerClass new decompile: selector in: class method: method)
+			decompileString.
+		^ contents copy].
+
+	Sensor leftShiftDown ifTrue:
+		["Special request to decompile -- get temps from source file"
+		tempNames _ (class compilerClass new
+						parse: method getSourceFromFile asString in: class notifying: nil)
+						tempNames.
+		contents _ ((class decompilerClass new withTempNames: tempNames)
+				decompile: selector
+				in: class
+				method: method) decompileString.
+		^ contents copy].
+
+	contents _ class sourceCodeAt: selector.
+	^ contents copy
