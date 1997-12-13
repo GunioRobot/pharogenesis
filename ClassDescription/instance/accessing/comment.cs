8b8610165e7048d2b376c4c1d45342af67e28358
tk <unknown>
1997-12-13 14:33:00 +0000

    no message

--- ClassDescription/instance/accessing/comment.cs
+++ ClassDescription/instance/accessing/comment.cs
@@ -1,8 +1,11 @@
 comment
-	"Answer the receiver's comment."
+	"Answer the receiver's comment. (If old format, not a Text, unpack the old way.) "
 
 	| aString |
-	aString _ self organization classComment.
-	aString size = 0 ifTrue: [^''].
-	"get string only of classComment, undoubling quotes"
-	^ String readFromString: aString
+	aString _ self theNonMetaClass organization classComment.
+	(aString asString beginsWith: self name, ' comment:\''' withCRs) 
+		ifFalse: [^ self theNonMetaClass organization classComment]
+		ifTrue: ["old format"
+			aString size = 0 ifTrue: [^''].
+			"get string only of classComment, undoubling quotes"
+			^ String readFromString: aString]
