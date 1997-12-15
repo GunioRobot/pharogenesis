6881dcbf71b5e2c09800b37ca2b63a4a06149841
jm <unknown>
1997-12-15 14:29:00 +0000

    no message

---
+++ SampledSound/class/sound library/addLibrarySoundNamed-3A-samples-3A-samplingRate-3A-.cs
@@ -0,0 +1,6 @@
+addLibrarySoundNamed: aString samples: sampleData samplingRate: samplesPerSecond
+	"Add the given sound to the sound library. The sample data may be either a ByteArray or a SoundBuffer. If the former, it is take to be 8-bit unsigned samples (as from an AIFF file). If the latter, it is taken to be 16 bit signed samples."
+
+	SoundLibrary
+		at: aString
+		put: (Array with: sampleData with: samplesPerSecond).
