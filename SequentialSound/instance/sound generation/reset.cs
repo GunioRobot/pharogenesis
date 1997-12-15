31e9b8f8e3d97061d8b47ae34654e5c6bb02d260
jm <unknown>
1997-12-15 22:47:00 +0000

    no message

--- SequentialSound/instance/sound generation/reset.cs
+++ SequentialSound/instance/sound generation/reset.cs
@@ -1,5 +1,5 @@
 reset
 
 	super reset.
-	sounds do: [ :snd | snd reset ].
-	sounds size > 0 ifTrue: [ currentIndex _ 1 ].
+	sounds do: [:snd | snd reset].
+	sounds size > 0 ifTrue: [currentIndex _ 1].
