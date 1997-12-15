94a5fabddeeea6922a26e7f968df2438d8d23b3a
tk <unknown>
1997-12-15 16:26:00 +0000

    no message

--- ClassCategoryReader/instance/fileIn-2F-Out/scanFrom-3A-.cs
+++ ClassCategoryReader/instance/fileIn-2F-Out/scanFrom-3A-.cs
@@ -1,10 +1,9 @@
 scanFrom: aStream 
-	"File in methods from the stream, aStream. Print the name and category of 
-	the methods in the transcript view."
-
-	| string |
-	[string _ aStream nextChunk.
-	string size > 0]						"done when double terminators"
-		whileTrue: [class compile: string classified: category].
-	Transcript show: class name , '<' , category , '
-'
+	"File in methods from the stream, aStream."
+	| methodText |
+	[methodText _ aStream nextChunkText.
+	 methodText size > 0]
+		whileTrue:
+		[class compile: methodText classified: category
+			withStamp: changeStamp
+			notifying: (SyntaxError new category: category)]
