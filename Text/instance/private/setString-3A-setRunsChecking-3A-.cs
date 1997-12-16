2716a721d3705581d01751b38811f0dbfb588799
tk <unknown>
1997-12-16 14:14:00 +0000

    no message

---
+++ Text/instance/private/setString-3A-setRunsChecking-3A-.cs
@@ -0,0 +1,12 @@
+setString: aString setRunsChecking: aRunArray
+	"Check runs and do the best you can to make them fit..."
+
+	string _ aString.
+	"check the runs"
+	aRunArray ifNil: [^ aString asText].
+	(aRunArray isKindOf: RunArray) ifFalse: [^ aString asText].
+	aRunArray runs size = aRunArray values size ifFalse: [^ aString asText].
+	(aRunArray values includes: #()) ifTrue: [^ aString asText].	"not allowed?"
+	aRunArray size = aString size ifFalse: [^ aString asText].
+	
+	runs _ aRunArray.
