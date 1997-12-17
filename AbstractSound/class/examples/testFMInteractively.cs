59ae27f97d6b7705934983aec13022b89a06f473
jm <unknown>
1997-12-17 21:25:00 +0000

    no message

--- AbstractSound/class/examples/testFMInteractively.cs
+++ AbstractSound/class/examples/testFMInteractively.cs
@@ -2,21 +2,22 @@ testFMInteractively
 	"Experiment with different settings of the FM modulation and multiplier settings interactively by moving the mouse. The top-left corner of the screen is 0 for both parameters. Stop when the mouse is pressed."
 	"AbstractSound testFMInteractively"
 
-	| s mousePt lastVal status |
-	SoundPlayer startPlayerProcessBufferSize: 1100 rate: 22050 stereo: false.
-	s _ FMSound pitch: 440.0 dur: 200.0 loudness: 200.
-	s  decayRate: 1.0; modulationDecay: 1.0.
+	| s mousePt lastVal status mod mult |
+	SoundPlayer startPlayerProcessBufferSize: 1100 rate: 11025 stereo: false.
+	s _ FMSound pitch: 440.0 dur: 200.0 loudness: 0.2.
 
 	SoundPlayer playSound: s.
+	lastVal _ nil.
 	[Sensor anyButtonPressed] whileFalse: [
 		mousePt _ Sensor cursorPoint.
 		mousePt ~= lastVal ifTrue: [
-			s modulation: mousePt x * 3 multiplier: mousePt y asFloat / 100.0.
+			mod _ mousePt x asFloat / 20.0.
+			mult _ mousePt y asFloat / 20.0.
+			s modulation: mod multiplier: mult.
 			lastVal _ mousePt.
 			status _
-'mod: ', (mousePt x * 3) printString, '
-mult: ', (mousePt y asFloat / 100.0) printString.
-			status asParagraph displayOn: Display at: 10@10.
-		].
-	].
-	SoundPlayer pauseSound: s.
+'mod: ', mod printString, '
+mult: ', mult printString.
+			status asParagraph displayOn: Display at: 10@10]].
+
+	SoundPlayer shutDown.
