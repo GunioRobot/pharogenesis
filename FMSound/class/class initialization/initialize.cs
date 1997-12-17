926b633a86422975d470f5c2d4ae1de8b7a52c0b
jm <unknown>
1997-12-17 21:27:00 +0000

    no message

---
+++ FMSound/class/class initialization/initialize.cs
@@ -0,0 +1,11 @@
+initialize
+	"Build a sine wave table."
+	"FMSound initialize"
+
+	| tableSize radiansPerStep peak |
+	tableSize _ 4000.
+	SineTable _ SoundBuffer newMonoSampleCount: tableSize.
+	radiansPerStep _ (2.0 * Float pi) / tableSize asFloat.
+	peak _ ((1 bitShift: 15) - 1) asFloat.  "range is +/- (2^15 - 1)"
+	1 to: tableSize do: [:i |
+		SineTable at: i put: (peak * (radiansPerStep * i) sin) rounded].
