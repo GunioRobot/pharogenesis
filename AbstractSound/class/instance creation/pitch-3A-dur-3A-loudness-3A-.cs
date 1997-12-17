49c7da8aa50f26fe93c0164b21f0213831e56af9
jm <unknown>
1997-12-17 17:27:00 +0000

    no message

--- AbstractSound/class/instance creation/pitch-3A-dur-3A-loudness-3A-.cs
+++ AbstractSound/class/instance creation/pitch-3A-dur-3A-loudness-3A-.cs
@@ -1,4 +1,4 @@
 pitch: p dur: d loudness: l
-	"Return a new sound object to a note with the given parameters."
+	"Return a new sound object for a note with the given parameters."
 
-	^ self basicNew setPitch: p dur: d loudness: l
+	^ self new setPitch: p dur: d loudness: l
