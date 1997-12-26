9dfa4737e32f4a50d9d12d91ad1f0994ca55fb0b
tk <unknown>
1997-12-26 21:56:00 +0000

    no message

---
+++ FTPSocket/instance/as yet unclassified/getAllData.cs
@@ -0,0 +1,16 @@
+getAllData
+	"Reel in all data until the server closes the connection.  Return a RWBinaryOrTextStream.  Don't know how much is coming."
+
+	| buf response bytesRead |
+	buf _ String new: 4000.
+	response _ RWBinaryOrTextStream on: (String new: 4000).
+	[(self dataAvailable | self isConnected)] whileTrue: [
+		(self waitForDataUntil: (Socket deadlineSecs: 5)) ifFalse: [
+			Transcript show: 'data was late'; cr].
+		bytesRead _ self primSocket: socketHandle receiveDataInto: buf 
+			startingAt: 1 count: buf size.
+		1 to: bytesRead do: [:ii | response nextPut: (buf at: ii)].
+			"Any way to do this so we do not have to recopy?"
+		].
+	response reset.	"position: 0."
+	^ response
