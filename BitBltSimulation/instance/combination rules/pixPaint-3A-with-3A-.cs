0eca91f0fcaf1732e237c803a49e220bc5ebd0a8
di <unknown>
1997-12-27 10:39:00 +0000

    no message

---
+++ BitBltSimulation/instance/combination rules/pixPaint-3A-with-3A-.cs
@@ -0,0 +1,6 @@
+pixPaint: sourceWord with: destinationWord
+	self inline: false.
+	sourceWord = 0 ifTrue: [^ destinationWord].
+	^ sourceWord bitOr:
+		(self partitionedAND: sourceWord bitInvert32 to: destinationWord
+						nBits: destPixSize nPartitions: pixPerWord)
