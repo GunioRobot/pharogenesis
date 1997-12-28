99f463110d5c7fd058037bc823350936912ea0ec
tk <unknown>
1997-12-28 23:11:00 +0000

    no message

---
+++ TextLink/instance/as yet unclassified/actOnClickFor-3A-.cs
@@ -0,0 +1,5 @@
+actOnClickFor: anObject
+	"Open an new LinkedMessageSet and show this method in it.  If anObject is already a LinkedMessageSet, then just add to the end of the list.  (Class method) (Class Comment) (Class Definition) (Class Hierarchy)"
+
+	anObject linkMethod: classAndMethod.
+	^ true
