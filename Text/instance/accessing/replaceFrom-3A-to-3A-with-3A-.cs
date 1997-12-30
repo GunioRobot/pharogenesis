a8c4cb7e454370112951145ac7ed7982d509997f
tk <unknown>
1997-12-30 07:17:00 +0000

    no message

--- Text/instance/accessing/replaceFrom-3A-to-3A-with-3A-.cs
+++ Text/instance/accessing/replaceFrom-3A-to-3A-with-3A-.cs
@@ -1,4 +1,6 @@
 replaceFrom: start to: stop with: aText
 
-	string _ string copyReplaceFrom: start to: stop with: aText string.
-	runs _ runs copyReplaceFrom: start to: stop with: aText runs
+	| txt |
+	txt _ aText asText.	"might be a string"
+	string _ string copyReplaceFrom: start to: stop with: txt string.
+	runs _ runs copyReplaceFrom: start to: stop with: txt runs
