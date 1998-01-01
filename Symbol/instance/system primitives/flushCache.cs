42d685cabca7efdc481dccea05dc9bb39cb2b003
ikp <unknown>
1998-01-01 00:00:00 +0000

    no message

---
+++ Symbol/instance/system primitives/flushCache.cs
@@ -0,0 +1,8 @@
+flushCache
+	"Tell the interpreter to remove all entries with this symbol as a selector in its method lookup cache, if it has one.  This primitive must be called whenever a method is redefined.
+	Essential.  See Object documentation whatIsAPrimitive."
+
+	<primitive: 119>
+	Object flushCache.
+	"<primitive: 89>"
+	"self primitiveFailed"
