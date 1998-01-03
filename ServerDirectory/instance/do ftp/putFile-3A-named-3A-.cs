ac59ba6fb3d7a094f182e8601ffa1cd756c97dd5
tk <unknown>
1998-01-03 22:31:00 +0000

    no message

---
+++ ServerDirectory/instance/do ftp/putFile-3A-named-3A-.cs
@@ -0,0 +1,22 @@
+putFile: fileStream named: fileNameOnServer
+	"Just FTP a local fileStream to the server.  (Later -- Use a proxy server if one has been registered.)"
+
+	| so dd resp rr |
+	so _ self openFTP.	"Open passive.  Do everything up to RETR or STOR"
+	so sendCommand: 'STOR ', fileNameOnServer.
+	dd _ so dataSocket.
+
+	dd connectTo: so remoteAddress port: dd portNum.
+	dd waitForConnectionUntil: FTPSocket standardDeadline.
+	Transcript show: 'store via port ', dd portNum printString; cr.
+	dd sendData: fileStream contentsOfEntireFile.
+	dd close.
+
+	(rr _ so responseOK) == true ifFalse: [^ rr].	"150 Opening binary conn on foo (3113 bytes)"
+	(resp _ so responseOK) == true ifFalse: [^ rr].	"226 Transfer complete."
+	so sendCommand: 'QUIT'.
+	(rr _ so responseOK) == true ifFalse: [^ rr].	"221"
+	so destroy.	"Always OK to destroy"
+	dd destroy.
+
+	^ resp	"226 Transfer complete."
