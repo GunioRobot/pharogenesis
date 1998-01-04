1dd3ea9d7addb33f0bb478c04092ca853c30ffc9
di <unknown>
1998-01-04 23:16:00 +0000

    no message

--- ContextPart/instance/private/doPrimitive-3A-receiver-3A-args-3A-.cs
+++ ContextPart/instance/private/doPrimitive-3A-receiver-3A-args-3A-.cs
@@ -1,7 +1,13 @@
 doPrimitive: primitiveIndex receiver: receiver args: arguments 
 	"Simulate a primitive method whose index is primitiveIndex.  The
 	simulated receiver and arguments are given as arguments to this message."
-	| primitiveMethod value |
+"
+	NOTE: In order for perform:WithArguments: to work reliably here,
+	this method must be forced to invoke a large context.  This is done
+	by adding extra temps until the following expression evaluates as true:
+		(ContextPart compiledMethodAt: #doPrimitive:receiver:args:) frameSize > 20
+"
+	| value t1 t2 t3 |
 	"If successful, push result and return resuming context,
 		else ^ #simulatorFail"
 	(primitiveIndex = 80 and: [receiver isKindOf: ContextPart])
@@ -13,16 +19,11 @@ doPrimitive: primitiveIndex receiver: receiver args: arguments
 	(primitiveIndex = 81 and: [receiver isMemberOf: BlockContext])
 		ifTrue: [^receiver pushArgs: arguments from: self].
 	primitiveIndex = 83 
-		ifTrue: [^self send: (arguments at: 1) to: receiver
-					with: (arguments copyFrom: 2 to: arguments size)
+		ifTrue: [^ self send: arguments first to: receiver
+					with: arguments allButFirst
 					super: false].
 	arguments size > 6 ifTrue: [^#simulatorFail].
-	primitiveMethod _ TryPrimitiveMethods at: arguments size + 1.
-	"slam num into primitive instead of 100 such messages in Object"
-	primitiveMethod bePrimitive: primitiveIndex.
-	"Class flushCache."  "in case interp caches primitive #"
-	value _ receiver perform: (TryPrimitiveSelectors at: arguments size+1)
-				withArguments: arguments.
+	value _ receiver tryPrimitive: primitiveIndex withArgs: arguments.
 	value == #simulatorFail
 		ifTrue: [^ #simulatorFail]
 		ifFalse: [^ self push: value]
