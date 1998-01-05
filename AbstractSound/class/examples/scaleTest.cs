02e0b489481d239b75af4b8456ce6f840fc1c80f
jm <unknown>
1998-01-05 17:32:00 +0000

    no message

--- AbstractSound/class/examples/scaleTest.cs
+++ AbstractSound/class/examples/scaleTest.cs
@@ -3,4 +3,4 @@ scaleTest
 
 	^ MixedSound new
 		add: FMSound majorScale pan: 0;
-		add: (PluckedSound lowMajorScale delayedBy: 0.5) pan: 1000.
+		add: (PluckedSound lowMajorScale delayedBy: 0.5) pan: 1.0.
