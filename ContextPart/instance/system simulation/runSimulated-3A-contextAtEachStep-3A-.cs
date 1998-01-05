0fb7394578637f034a8f97da55daafe8135ff2e2
di <unknown>
1998-01-05 11:20:00 +0000

    no message

--- ContextPart/instance/system simulation/runSimulated-3A-contextAtEachStep-3A-.cs
+++ ContextPart/instance/system simulation/runSimulated-3A-contextAtEachStep-3A-.cs
@@ -2,11 +2,9 @@ runSimulated: aBlock contextAtEachStep: block2
 	"Simulate the execution of the argument, aBlock, until it ends. aBlock 
 	MUST NOT contain an '^'. Evaluate block2 with the current context 
 	prior to each instruction executed. Answer the simulated value of aBlock."
-
 	| current |
 	aBlock hasMethodReturn
 		ifTrue: [self error: 'simulation of blocks with ^ can run loose'].
-	self class initPrimitives.
 	current _ aBlock.
 	current pushArgs: Array new from: self.
 	[current == self]
