03569d0833365de8af61dc787be7b971de3dae06
ikp <unknown>
1998-01-05 14:22:00 +0000

    no message

---
+++ DynamicInterpreter/instance/primitive support/primitiveResponse.cs
@@ -0,0 +1,12 @@
+primitiveResponse
+	self inline: false.
+	primitiveIndex > MaxPrimitiveIndex ifTrue: [^ false].
+	successFlag _ true.
+	self dispatchOn: primitiveIndex in: PrimitiveTable.
+	"check for possible timer interrupts after each primitive"
+	(successFlag and:
+	 [(nextWakeupTick ~= 0) and:
+	 [self ioMSecs >= nextWakeupTick]]) ifTrue: [
+		interruptCheckCounter _ 1000.
+		self checkForInterrupts].
+	^ successFlag
