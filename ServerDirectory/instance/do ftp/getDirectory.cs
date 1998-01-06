da9e9d06935a48c7014895b84301d32145d90de9
tk <unknown>
1998-01-06 12:14:00 +0000

    no message

---
+++ ServerDirectory/instance/do ftp/getDirectory.cs
@@ -0,0 +1,23 @@
+getDirectory
+	"Return a stream with a listing of the current server directory.  (Later -- Use a proxy server if one has been registered.)"
+
+	| so dd resp rr |
+	so _ self openFTP.	"Open passive.  Do everything up to RETR or STOR"
+	so sendCommand: 'LIST'.
+	dd _ so dataSocket.
+
+	dd connectTo: so remoteAddress port: dd portNum.
+	dd waitForConnectionUntil: FTPSocket standardDeadline.
+	Transcript show: 'retrieve from port ', dd portNum printString; cr.
+	resp _ dd getAllDataWhileWatching: so.	"Later use the length to pre-allocate the buffer"
+	(resp == #error:) ifTrue: [^ resp].
+	dd close.
+
+	(rr _ so responseOK) == true ifFalse: [^ rr].	"150 Opening binary conn on foo (3113 bytes)"
+	(rr _ so responseOK) == true ifFalse: [^ rr].	"226 Transfer complete."
+	so sendCommand: 'QUIT'.
+	(rr _ so responseOK) == true ifFalse: [^ rr].	"221"
+	so destroy.	"Always OK to destroy"
+	dd destroy.
+
+	^ resp	"RWStream with just the data"
