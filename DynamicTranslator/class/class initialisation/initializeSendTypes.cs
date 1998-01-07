c73ef187d7e65d56e01e3d17f7b6c0e0d21268ba
ikp <unknown>
1998-01-07 02:23:00 +0000

    no message

---
+++ DynamicTranslator/class/class initialisation/initializeSendTypes.cs
@@ -0,0 +1,9 @@
+initializeSendTypes		"DynamicTranslator initialize"
+
+	ShortSendType _ 0.
+	ExtendedSendType _ 1.
+	DoubleExtendedSendType _ 2.
+
+	ImmediateSendType _ 1.
+	CompactSendType _ 2.
+	NormalSendType _ 3.
