ae32b71b99f1e97e1bc8057dfd236591085d82ec
di <unknown>
1998-01-08 15:08:00 +0000

    no message

--- FileListController/class/class initialization/initialize.cs
+++ FileListController/class/class initialization/initialize.cs
@@ -1,21 +1,24 @@
-initialize
+initialize   "FileListController initialize"
 	"Initialize the file list menu.  6/96 di; modified 7/12/96 sw to add the file-into-new-change-set feature"
 
 	FileListYellowButtonMenu _ PopUpMenu labels:
 'fileIn
 file into new change set
-import GIF into GIFImports
-import GIF into HyperSqueak
-load HyperSqueak stack
 browse changes
 spawn this file
+copy name to clipboard
+open image in a window
+read image into GIFImports
+play midi file
 sort by name
 sort by size
 sort by date
-copy name
 rename
 delete
-add new file' lines: # (3 5 7 10 ).
-	FileListYellowButtonMessages _ #(fileInSelection fileIntoNewChangeSet importGIF imporHyperSqueaktGIF loadIntoHyperSqueak browseChanges editFile sortByName sortBySize sortByDate copyName renameFile deleteFile addNewFile)
-
-	"FileListController initialize"
+add new file
+broadcast as update' lines: # (5 7 8 11).
+	FileListYellowButtonMessages _
+#(fileInSelection fileIntoNewChangeSet browseChanges editFile copyName
+ openImageInWindow importImage playMidiFile
+ sortByName sortBySize sortByDate
+ renameFile deleteFile addNewFile putUpdate)
