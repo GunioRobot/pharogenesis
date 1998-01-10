c02aea4000ec1007d330007c368adde321fe77e8
wod <unknown>
1998-01-10 00:43:00 +0000

    no message

--- Scanner/instance/multi-2D-character scans/xDoubleQuote.cs
+++ Scanner/instance/multi-2D-character scans/xDoubleQuote.cs
@@ -1,16 +1,19 @@
 xDoubleQuote
-	"Collect a comment."
-
-	| aStream stopChar |
-	stopChar _ 30 asCharacter.
-	aStream _ WriteStream on: (String new: 200).
-	self step.
-	[aStream nextPut: self step. hereChar == $"]
-		whileFalse: 
-			[(hereChar == stopChar and: [source atEnd])
-				ifTrue: [^self offEnd: 'Unmatched comment quote']].
-	self step.
-	currentComment == nil
-		ifTrue: [currentComment _ OrderedCollection with: aStream contents]
-		ifFalse: [currentComment add: aStream contents].
-	self scanToken
+    "Collect a comment."
+    "wod 1/10/98: Allow 'empty' comments by testing the first character
+for $"" rather than blindly adding it to the comment being collected."
+    | aStream stopChar |
+    stopChar _ 30 asCharacter.
+    aStream _ WriteStream on: (String new: 200).
+    self step.
+    [hereChar == $"]
+        whileFalse:
+            [(hereChar == stopChar and: [source atEnd])
+                ifTrue: [^self offEnd: 'Unmatched comment quote'].
+            aStream nextPut: self step.].
+    self step.
+    currentComment == nil
+        ifTrue: [currentComment _ OrderedCollection with: aStream
+contents]
+        ifFalse: [currentComment add: aStream contents].
+    self scanToken
