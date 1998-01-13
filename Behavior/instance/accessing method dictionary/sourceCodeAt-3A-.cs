984dd5333a3afb5f2edd72ff5e2306a7c2859c10
di <unknown>
1998-01-13 17:34:00 +0000

    no message

--- Behavior/instance/accessing method dictionary/sourceCodeAt-3A-.cs
+++ Behavior/instance/accessing method dictionary/sourceCodeAt-3A-.cs
@@ -1,17 +1,3 @@
 sourceCodeAt: selector
-	| code method dict |
-	method _ methodDict at: selector.
-	Sensor leftShiftDown
-		ifTrue: [code _ (self decompilerClass new
-						decompile: selector
-						in: self
-						method: method) decompileString]
-		ifFalse: 
-			[code _ method getSource.
-			code == nil
-				ifTrue: 
-					[code _ (self decompilerClass new
-									decompile: selector
-									in: self
-									method: method) decompileString]].
-	^code
+
+	^ (methodDict at: selector) getSourceFor: selector in: self
