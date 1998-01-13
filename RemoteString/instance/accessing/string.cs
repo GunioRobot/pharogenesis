0811a5c79415cc318c04772a6a1e43e5497d7410
di <unknown>
1998-01-13 16:57:00 +0000

    no message

--- RemoteString/instance/accessing/string.cs
+++ RemoteString/instance/accessing/string.cs
@@ -1,10 +1,7 @@
-string
+string 
 	"Answer the receiver's string if remote files are enabled."
-
 	| theFile |
-	(sourceFileNumber == nil or: [(SourceFiles at: sourceFileNumber) == nil])
-		ifTrue: [^'']
-		ifFalse: 
-			[theFile _ SourceFiles at: sourceFileNumber.
-			theFile position: (filePositionHi bitShift: 8) + filePositionLo.
-			^theFile nextChunk]
+	(sourceFileNumber == nil or: [(SourceFiles at: sourceFileNumber) == nil]) ifTrue: [^''].
+	theFile _ SourceFiles at: sourceFileNumber.
+	theFile position: filePositionHi.
+	^ theFile nextChunk
