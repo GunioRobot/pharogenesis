d03fb25d957bbc24d44e67f174edaff5ee2fc6d8
di <unknown>
1998-01-13 16:36:00 +0000

    no message

---
+++ RunArray/class/instance creation/scanFrom-3A-.cs
@@ -0,0 +1,31 @@
+scanFrom: strm
+	"Read the style section of a fileOut or sources file.  nextChunk has already been done.  We need to return a RunArray of TextAttributes of various kinds."
+	| rr vv aa this |
+	(strm peekFor: $( ) ifFalse: [^ nil].
+	rr _ OrderedCollection new.
+	[strm skipSeparators.
+	 strm peekFor: $)] whileFalse: 
+		[rr add: (Number readFrom: strm)].
+	vv _ OrderedCollection new.	"Value array"
+	aa _ OrderedCollection new.	"Attributes list"
+	[(this _ strm next) == nil] whileFalse: [
+		this == $, ifTrue: [vv add: aa asArray.  aa _ OrderedCollection new].
+		this == $f ifTrue: [aa add: (TextFontChange new fontNumber: (Number readFrom: strm))].
+		this == $b ifTrue: [aa add: (TextEmphasis bold)].
+		this == $i ifTrue: [aa add: (TextEmphasis italic)].
+		this == $u ifTrue: [aa add: (TextEmphasis underlined)].
+		this == $= ifTrue: [aa add: (TextEmphasis struckOut)].
+		this == $n ifTrue: [aa add: (TextEmphasis normal)].
+		this == $- ifTrue: [aa add: (TextKern kern: -1)].
+		this == $+ ifTrue: [aa add: (TextKern kern: 1)].
+		this == $c ifTrue: [aa add: (TextColor scanFrom: strm)]. "color"
+		this == $L ifTrue: [aa add: (TextLink scanFrom: strm)].	"L not look like 1"
+		this == $R ifTrue: [aa add: (TextURL scanFrom: strm)].	"R cap so it can follow a number"
+		this == $d ifTrue: [aa add: (TextDoIt scanFrom: strm)].
+		"space, cr do nothing"
+		].
+	aa size > 0 ifTrue: [vv add: aa asArray].
+	^ self runs: rr asArray values: vv asArray
+"
+RunArray scanFrom: (ReadStream on: '(14 50 312)f1,f1b,f1LInteger +;i')
+"
