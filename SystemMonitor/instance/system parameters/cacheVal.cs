7924cc4a38a85d5426239bc489989b13e5bf6ae2
ikp <unknown>
1998-01-13 01:02:00 +0000

    no message

---
+++ SystemMonitor/instance/system parameters/cacheVal.cs
@@ -0,0 +1,17 @@
+cacheVal
+	| icHit mcHit mcMiss total deltaMcMiss deltaMcHit deltaIcHit |
+	mcMiss _ vmParameters at: 15.
+	deltaMcMiss _ mcMiss - prevMcMiss.
+	prevMcMiss _ mcMiss.
+	mcHit _ vmParameters at: 16.
+	deltaMcHit _ mcHit - prevMcHit.
+	prevMcHit _ mcHit.
+	icHit _ vmParameters at: 17.
+	deltaIcHit _ icHit - prevIcHit.
+	prevIcHit _ icHit.
+	total _ deltaMcMiss + deltaMcHit + deltaIcHit.
+	deltaMcMiss _ (deltaMcMiss * 100 / total asInteger).
+	deltaMcHit _ (deltaMcHit * 100 / total asInteger).
+	^Array
+		with: deltaMcMiss
+		with: deltaMcMiss + deltaMcHit
