66fab679e10a5efd8ea623b2e038297ec26fb2ca
di <unknown>
1998-01-13 22:11:00 +0000

    no message

---
+++ BookMorph/instance/initialization/addDressing.cs
@@ -0,0 +1,12 @@
+addDressing
+	| controlColor pageControls |
+	self addMorph: (Morph new color: color; extent: 10@10).  "spacer"
+	controlColor _ (color saturation > 0.1)
+		ifTrue:
+			[color lighter]
+		ifFalse:
+			[color darker].
+	pageControls _ self makeAuthoringPageControlsColored: controlColor.
+	pageControls borderWidth: 1; inset: 4.
+			
+	self addMorph: pageControls
