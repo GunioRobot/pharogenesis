0acef7b07eb6a8f693dfe8406f8e57d9cd7fc8b3
di <unknown>
1998-01-14 10:26:00 +0000

    no message

--- String/class/instance creation/fromString-3A-.cs
+++ String/class/instance creation/fromString-3A-.cs
@@ -1,7 +1,4 @@
 fromString: aString 
 	"Answer an instance of me that is a copy of the argument, aString."
 	
-	| newString |
-	newString _ self new: aString size.
-	aString size do: [:i | newString at: i put: (aString at: i)].
-	^newString
+	^ aString copyFrom: 1 to: aString size
