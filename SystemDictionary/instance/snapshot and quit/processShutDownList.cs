98e71fefcaf329a2af9b7237123e18f1a6b5a4ea
di <unknown>
1998-01-14 08:42:00 +0000

    no message

--- SystemDictionary/instance/snapshot and quit/processShutDownList.cs
+++ SystemDictionary/instance/snapshot and quit/processShutDownList.cs
@@ -1,8 +1,11 @@
 processShutDownList
 	"Call the shutDown method on each object that needs to gracefully shut itself down before a snapshot."
 
-	SoundPlayer shutDown.
-	Smalltalk shutDown.
+	(self includesKey: #Password) ifTrue: [Password shutDown].
+	self shutDownSound.
 	Delay shutDown.
+	Smalltalk shutDown.
+	Color shutDown.
+	StrikeFont shutDown.
 	ControlManager shutDown.
 	DisplayScreen shutDown.
