e0c0a135027e92cfc6653cdc8c9653faf1796e1a
di <unknown>
1998-01-14 13:46:00 +0000

    no message

--- Browser/instance/accessing/contents.cs
+++ Browser/instance/accessing/contents.cs
@@ -2,20 +2,26 @@ contents
 	"Depending on the current selection, different information is retrieved.
 	Answer a string description of that information. This information is the
 	method of the currently selected class and message."
-
-	editSelection == #none ifTrue: [^''].
+	| comment theClass |
+	editSelection == #none ifTrue: [^ ''].
 	editSelection == #editSystemCategories 
-		ifTrue: [^systemOrganizer printString].
+		ifTrue: [^ systemOrganizer printString].
 	editSelection == #newClass 
-		ifTrue: [^Class template: self selectedSystemCategoryName].
+		ifTrue: [^ Class template: self selectedSystemCategoryName].
 	editSelection == #editClass 
-		ifTrue: [^self selectedClassOrMetaClass definition].
+		ifTrue: [^ self selectedClassOrMetaClass definition].
 	editSelection == #editComment 
-		ifTrue: [^self selectedClassOrMetaClass commentTemplate].
+		ifTrue: [(theClass _ self selectedClass) ifNil: [^ ''].
+				comment _ theClass comment.
+				comment size = 0
+				ifTrue: [ ^ 'This class has not yet been commented.']
+				ifFalse: [ ^ comment]].
 	editSelection == #hierarchy 
-		ifTrue: [^self selectedClassOrMetaClass printHierarchy].
+		ifTrue: [^ self selectedClassOrMetaClass printHierarchy].
 	editSelection == #editMessageCategories 
-		ifTrue: [^self classOrMetaClassOrganizer printString].
-	editSelection == #newMessage ifTrue: [^self selectedClassOrMetaClass sourceCodeTemplate].
-	editSelection == #editMessage ifTrue: [^self selectedMessage].
+		ifTrue: [^ self classOrMetaClassOrganizer printString].
+	editSelection == #newMessage
+		ifTrue: [^ self selectedClassOrMetaClass sourceCodeTemplate].
+	editSelection == #editMessage
+		ifTrue: [^ self selectedMessage].
 	self error: 'Browser internal error: unknown edit selection.'
