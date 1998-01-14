6468425fa58dd04c93f233762087f56abda5923d
di <unknown>
1998-01-14 13:40:00 +0000

    no message

--- Browser/instance/class list/classListIndex-3A-.cs
+++ Browser/instance/class list/classListIndex-3A-.cs
@@ -1,13 +1,24 @@
 classListIndex: anInteger 
 	"Set anInteger to be the index of the current class selection."
-	classListIndex _ anInteger.
+
+	| className |
+classListIndex _ anInteger.
 	self setClassOrganizer.
 	messageCategoryListIndex _ 0.
 	messageListIndex _ 0.
-	editSelection _ anInteger = 0
-			ifTrue: [metaClassIndicated
-				ifTrue: [#none]
-				ifFalse: [#newClass]]
-			ifFalse: [#editClass].
+	self classCommentIndicated
+		ifTrue: []
+		ifFalse: [editSelection _ anInteger = 0
+					ifTrue: [metaClassIndicated
+						ifTrue: [#none]
+						ifFalse: [#newClass]]
+					ifFalse: [#editClass]].
 	contents _ nil.
+	self selectedClass isNil
+		ifFalse: [className _ self selectedClass name.
+					(RecentClasses includes: className)
+				ifTrue: [RecentClasses remove: className].
+			RecentClasses addFirst: className.
+			RecentClasses size > 16
+				ifTrue: [RecentClasses removeLast]].
 	self changed: #classSelectionChanged
