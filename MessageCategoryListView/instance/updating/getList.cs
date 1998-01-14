efebbcbb62aa7e074efc8c9de68bd0dbf2fe6c63
di <unknown>
1998-01-14 14:04:00 +0000

    no message

--- MessageCategoryListView/instance/updating/getList.cs
+++ MessageCategoryListView/instance/updating/getList.cs
@@ -1,6 +1,7 @@
 getList 
 	"Refer to the comment in BrowserListView|getList."
 
+	model classCommentIndicated ifTrue: [^ Array new].
 	singleItemMode
 		ifTrue: [^Array with: model selectedMessageCategoryName asSymbol]
 		ifFalse: [^model messageCategoryList]
