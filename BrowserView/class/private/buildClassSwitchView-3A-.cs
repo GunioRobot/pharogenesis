9ea779fe09039c019ad0048bc081d89bde242902
di <unknown>
1998-01-14 13:29:00 +0000

    no message

--- BrowserView/class/private/buildClassSwitchView-3A-.cs
+++ BrowserView/class/private/buildClassSwitchView-3A-.cs
@@ -6,6 +6,6 @@ buildClassSwitchView: aBrowser
 	aSwitchView controller: LockedSwitchController new.
 	aSwitchView selector: #classMessagesIndicated.
 	aSwitchView controller selector: #indicateClassMessages.
-	aSwitchView window: (0 @ 0 extent: 25 @ 8).
+	aSwitchView window: (0 @ 0 extent: 15 @ 8).
 	aSwitchView label: 'class' asParagraph.
 	^aSwitchView
