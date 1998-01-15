27ad3526b97a67ae7c69d478336dd901f9601798
di <unknown>
1998-01-15 10:44:00 +0000

    no message

---
+++ SystemDictionary/instance/memory space/lowSpaceThreshold.cs
@@ -0,0 +1,6 @@
+lowSpaceThreshold
+	"Return the low space threshold. When the amount of free memory (after garbage collection) falls below this limit, the system is in serious danger of completely exhausting memory and crashing. This limit should be made high enough to allow the user open a debugger to diagnose a problem or to save the image."
+
+	thisContext isPseudoContext
+		ifTrue: [^ 300000  "Allow for translated methods"]
+		ifFalse: [^  80000  "Enough for interpreter"]
