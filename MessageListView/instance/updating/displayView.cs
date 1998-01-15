d29d46410564371d6e6bdad56496a1761ed4300a
sw <unknown>
1998-01-15 17:33:00 +0000

    no message

--- MessageListView/instance/updating/displayView.cs
+++ MessageListView/instance/updating/displayView.cs
@@ -2,24 +2,21 @@ displayView
 	"Refer to the comment in View|displayView."
 
 	| aClass sel index baseClass |
-	Browser postOpenSuggestion == nil ifFalse: [
-		"Set the class and message"
+	Browser postOpenSuggestion == nil ifFalse:
+		["Set the class and message"
 		aClass _ Browser postOpenSuggestion first.
 		sel _ Browser postOpenSuggestion last.
 		Browser postOpenSuggestion: nil.
 		baseClass _ aClass theNonMetaClass.
-		index _ SystemOrganization numberOfCategoryOfElement: baseClass name.
+		model systemCategoryListIndex:
+			(SystemOrganization numberOfCategoryOfElement: baseClass name).
+		model selectClass: baseClass.
 		model metaClassIndicated: aClass isMeta.
-		model systemCategoryListIndex: index.
-		model metaClassIndicated: aClass isMeta.
-		model classListIndex: ((SystemOrganization listAtCategoryNumber: index)
-				findFirst: [:each | each == baseClass name]).
-		sel notNil ifTrue: [
-			index _ aClass organization numberOfCategoryOfElement: sel.
-			model messageCategoryListIndex: index.
+		(sel notNil and: [aClass includesSelector: sel]) ifTrue:
+			[model messageCategoryListIndex:
+				(index _ aClass organization numberOfCategoryOfElement: sel).
 			model messageListIndex: 
-				((aClass organization listAtCategoryNumber: index) indexOf: sel)
-			].
-		^ self topView deEmphasize.   "a redisplay has already been done"
-		].
-	super displayView.
+				((aClass organization listAtCategoryNumber: index) indexOf: sel)].
+		self topView deEmphasize.
+		^ self   "a redisplay has already been done"].
+	super displayView
