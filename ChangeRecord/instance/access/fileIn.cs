6c706bd48c17a5de6b2d7873299c932707a23f02
sw <unknown>
1998-01-15 22:46:00 +0000

    no message

--- ChangeRecord/instance/access/fileIn.cs
+++ ChangeRecord/instance/access/fileIn.cs
@@ -2,6 +2,8 @@ fileIn
 	| methodClass |
 	Cursor read showWhile:
 		[(methodClass _ self methodClass) notNil ifTrue:
-			[methodClass compile: self string classified: category].
-		type = #doIt ifTrue:
-			[Compiler evaluate: self string]]
+			[methodClass compile: self text classified: category withStamp: stamp notifying: nil].
+		(type == #doIt) ifTrue:
+			[Compiler evaluate: self string].
+		(type == #classComment) ifTrue:
+			[(Smalltalk at: class asSymbol) comment: self text]]
