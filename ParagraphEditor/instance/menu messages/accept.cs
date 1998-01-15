236b4154b6417a02bda4ba42cf890f8b48246eae
sw <unknown>
1998-01-15 12:57:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/accept.cs
+++ ParagraphEditor/instance/menu messages/accept.cs
@@ -1,6 +1,5 @@
 accept
-	"Save the current text of the text being edited as the current acceptable version for purposes of canceling.  
-	7/16/96 sw: call view.accepted, giving the view a chance to take special action at this juncture."
+	"Save the current text of the text being edited as the current acceptable version for purposes of canceling."
 
 	initialText _ paragraph text copy.
-	view accepted
+	view ifNotNil: [view accepted]
