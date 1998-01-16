74a9b26548bc85f80b0767e918d7304341049ef3
di <unknown>
1998-01-16 16:40:00 +0000

    no message

--- SequenceableCollection/instance/copying/,.cs
+++ SequenceableCollection/instance/copying/,.cs
@@ -1,7 +1,10 @@
-, aSequenceableCollection 
-	"Answer a copy of the receiver with each element of the argument, 
-	aSequencableCollection, added, in order."
+, otherCollection 
+	"Concatenate two Strings or Collections."
 	
-	^self copyReplaceFrom: self size + 1
+	^ self copyReplaceFrom: self size + 1
 		  to: self size
-		  with: aSequenceableCollection
+		  with: otherCollection
+"
+#(2 4 6 8) , #(who do we appreciate)
+((2989 printStringBase: 16) copyFrom: 4 to: 6) , ' boy!'
+"
