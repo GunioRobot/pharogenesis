c73e4cb907ea040660aa1a12f9fb32199c4bf473
di <unknown>
1998-01-21 11:50:00 +0000

    no message

--- ProjectView/instance/displaying/displayView.cs
+++ ProjectView/instance/displaying/displayView.cs
@@ -1,16 +1,7 @@
 displayView
-	| scale rect topLeft |
 	super displayView.
 	self label = model name
 		ifFalse: [super relabel: model name].
 	self isCollapsed ifTrue: [^ self].
-	Display fill: self insetDisplayBox fillColor: Color lightGray.
-	scale _ self insetDisplayBox extent / Display extent.
-	topLeft _ self insetDisplayBox topLeft.
-	model views reverseDo:
-		[:v | rect _ (v displayBox scaleBy: scale) rounded
-				translateBy: topLeft.
-		Display fill: rect fillColor: v backgroundColor;
-			border: rect width: 1;
-			border: (rect topLeft extent: rect width@3) width: 1.
-		]
+	model thumbnail ifNotNil:
+		[model thumbnail displayAt: self insetDisplayBox topLeft]
