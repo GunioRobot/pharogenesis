779f6d8f64fa427158c5d8330279165e1daefa3a
di <unknown>
1998-01-21 11:45:00 +0000

    no message

--- Project/instance/menu messages/exit.cs
+++ Project/instance/menu messages/exit.cs
@@ -2,4 +2,17 @@ exit
 	"Leave the current project and return to the project
 	in which this one was created."
 
-	projectHolder enter
+	self isTopProject ifTrue: [^ PopUpMenu notify: 'Can''t exit the top project'].
+
+	"Cache a thumbnail image for the parent view."
+	viewSize ifNotNil:
+		[thumbnail _ Form extent: viewSize depth: Display depth.
+		(WarpBlt toForm: thumbnail)
+			sourceForm: Display;
+			cellSize: 2;    "installs a colormap"
+			combinationRule: Form over;
+			copyQuad: (Display boundingBox) innerCorners
+			toRect: (0@0 extent: viewSize)].
+
+	activeProcess _ nil.
+	parentProject enter.
