e37e496f55f71066782177f73d15a3eb01563183
di <unknown>
1998-01-21 13:23:00 +0000

    no message

---
+++ ColorSystemView/instance/as yet unclassified/displayDeEmphasized.cs
@@ -0,0 +1,7 @@
+displayDeEmphasized
+	"Display this view with emphasis off.
+	If windowBits is not nil, then simply BLT if possible."
+	bitsValid
+		ifTrue: [self lock.
+				windowBits displayAt: self windowOrigin]
+		ifFalse: [super displayDeEmphasized]
