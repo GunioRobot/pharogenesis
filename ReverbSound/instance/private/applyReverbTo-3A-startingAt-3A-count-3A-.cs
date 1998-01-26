ab3ec9eceaa2fe695d61fb3a9765632170246948
jm <unknown>
1998-01-26 20:34:00 +0000

    no message

---
+++ ReverbSound/instance/private/applyReverbTo-3A-startingAt-3A-count-3A-.cs
@@ -0,0 +1,36 @@
+applyReverbTo: aSoundBuffer startingAt: startIndex count: n
+
+	| delayedLeft delayedRight i tapGain j out |
+	<primitive: 183>
+	self var: #aSoundBuffer declareC: 'short int *aSoundBuffer'.
+	self var: #tapDelays declareC: 'int *tapDelays'.
+	self var: #tapGains declareC: 'int *tapGains'.
+	self var: #leftBuffer declareC: 'short int *leftBuffer'.
+	self var: #rightBuffer declareC: 'short int *rightBuffer'.
+
+	startIndex to: ((startIndex + n) - 1) do: [:sliceIndex |
+		delayedLeft _ delayedRight _ 0.
+		1 to: tapCount do: [:tapIndex |
+			i _ bufferIndex - (tapDelays at: tapIndex).
+			i < 1 ifTrue: [i _ i + bufferSize].  "wrap"
+			tapGain _ tapGains at: tapIndex.
+			delayedLeft _ delayedLeft + (tapGain * (leftBuffer at: i)).
+			delayedRight _ delayedRight + (tapGain * (rightBuffer at: i))].
+
+		"left channel"
+		j _ (2 * sliceIndex) - 1.
+		out _ (aSoundBuffer at: j) + (delayedLeft // ScaleFactor).
+		out >  32767 ifTrue: [out _  32767].  "clipping!"
+		out < -32767 ifTrue: [out _ -32767].  "clipping!"
+		aSoundBuffer at: j put: out.
+		leftBuffer at: bufferIndex put: out.
+
+		"right channel"
+		j _ j + 1.
+		out _ (aSoundBuffer at: j) + (delayedRight // ScaleFactor).
+		out >  32767 ifTrue: [out _  32767].  "clipping!"
+		out < -32767 ifTrue: [out _ -32767].  "clipping!"
+		aSoundBuffer at: j put: out.
+		rightBuffer at: bufferIndex put: out.
+
+		bufferIndex _ (bufferIndex \\ bufferSize) + 1].
