046c041c411d1e3e9e1d9f3b575d7f2858433e89
jm <unknown>
1998-01-26 22:05:00 +0000

    no message

--- AbstractSound/instance/playing/playSilently.cs
+++ AbstractSound/instance/playing/playSilently.cs
@@ -3,8 +3,7 @@ playSilently
 
 	| buf |
 	self reset.
-	buf _ SoundBuffer sampleCount: (self samplingRate // 10).
+	buf _ SoundBuffer newStereoSampleCount: (self samplingRate // 10).
 	[self samplesRemaining > 0] whileTrue: [
 		buf primFill: 0.
-		self playSampleCount: buf sampleCount into: buf startingAt: 1 stereo: true.
-	].
+		self playSampleCount: buf stereoSampleCount into: buf startingAt: 1].
