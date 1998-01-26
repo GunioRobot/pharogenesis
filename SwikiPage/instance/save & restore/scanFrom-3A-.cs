b3c51112b85c5982b0b6531f20ff8a2aba48e9cb
tk <unknown>
1998-01-26 12:03:00 +0000

    no message

---
+++ SwikiPage/instance/save & restore/scanFrom-3A-.cs
@@ -0,0 +1,9 @@
+scanFrom: aFile
+	"Read my name off the file for this page.  Other pages contain a ref to me like *myName* "
+
+	aFile setToEnd.
+	self backupAChunk: aFile.
+	"extract the name"
+	Compiler evaluate: (aFile nextChunk) for: self logged: false.
+	map at: name put: self.
+	aFile close.
