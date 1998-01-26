b1a96435963ee3b9ff96ad60fda2d4c093ccfd06
tk <unknown>
1998-01-26 22:57:00 +0000

    no message

---
+++ SwikiPage/instance/save & restore/fromFileNamed-3A-action-3A-.cs
@@ -0,0 +1,11 @@
+fromFileNamed: filePath action: aSwikiAction
+	"Fill in all parts of this page from the info in its file"
+
+	| theFile |
+	map _ aSwikiAction urlmap.
+	file _ filePath.
+	theFile _ FileStream oldFileNamed: file.
+	coreID _ theFile localName.
+	url _ aSwikiAction name, '.', coreID.
+	self scanFrom: theFile.	"name, date"
+	address _ ''.		"should be page that points at it, but how get that?"
