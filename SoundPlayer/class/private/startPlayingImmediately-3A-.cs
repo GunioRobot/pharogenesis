acf397189770c4fe394a4555dc7e61f5cf3d41d6
jm <unknown>
1998-01-26 22:29:00 +0000

    no message

---
+++ SoundPlayer/class/private/startPlayingImmediately-3A-.cs
@@ -0,0 +1,40 @@
+startPlayingImmediately: aSound
+	"Private! Start playing the given sound as soon as possible by mixing it into the sound output buffers of the underlying sound driver."
+
+	| dontInsertSamples totalSamples buf n leftover src rest |
+	dontInsertSamples _ false.  "temporary, until insert samples primitive is ported"
+	dontInsertSamples ifTrue: [
+		ActiveSounds add: aSound.
+		^ self].
+
+	"first, fill a double-size buffer with samples"
+	totalSamples _ Buffer stereoSampleCount * 2.  "two buffer's worth"
+	buf _ SoundBuffer newStereoSampleCount: totalSamples.
+	aSound playSampleCount: totalSamples into: buf startingAt: 1.
+	ReverbState == nil ifFalse: [
+		ReverbState applyReverbTo: buf startingAt: 1 count: totalSamples].
+
+	PlayerSemaphore critical: [
+		"insert as many samples as possible into the sound driver's buffers"
+		n _ self primSoundInsertSamples: totalSamples
+			from: buf
+			samplesOfLeadTime: 1024.
+		leftover _ totalSamples - n.
+
+		"copy the remainder of buf into Buffer"
+		"Note: the following loop iterates over 16-bit words, not two-word stereo slices"
+		"assert: 0 < leftover <= Buffer stereoSampleCount"
+		src _ 2 * n.
+		1 to: 2 * leftover do:
+			[:dst | Buffer at: dst put: (buf at: (src _ src + 1))].
+
+		"generate enough additional samples to finish filling Buffer"
+		rest _ Buffer stereoSampleCount - leftover.
+		aSound playSampleCount: rest into: Buffer startingAt: leftover + 1.
+		ReverbState == nil ifFalse: [
+			ReverbState applyReverbTo: Buffer startingAt: leftover + 1 count: rest].
+
+		"record the fact that this sound has already been played into Buffer so that we don't process it again this time around"
+		SoundJustStarted _ aSound.
+
+		ActiveSounds add: aSound].
