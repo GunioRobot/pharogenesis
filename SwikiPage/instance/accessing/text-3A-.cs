a4baa3305b4f67c6ed2d06f415b8d6504393c2cf
tk <unknown>
1998-01-27 08:50:00 +0000

    no message

---
+++ SwikiPage/instance/accessing/text-3A-.cs
@@ -0,0 +1,16 @@
+text: aString
+	"Add onto the end of the file"
+
+	| this aFile start end |
+	this _ String streamContents: [:ss | 
+		ss nextPutAll: self chunk1.
+		aString storeOn: ss.
+		ss nextPutAll: ' back: '].
+	(aFile _ FileStream fileNamed: file) setToEnd.
+	start _ aFile position.
+	aFile nextChunkPut: this; skip: -1.	"undo the ! at end"
+	end _ aFile position.
+	aFile nextPutAll: (end - start) printString; nextPut: $!; cr; close.
+
+
+		ss nextPutAll: ss position printString
