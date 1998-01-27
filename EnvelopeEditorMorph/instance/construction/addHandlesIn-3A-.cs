0dc7bd5ec25d8a4c7a2f08b5509eeb74f8cdd34b
di <unknown>
1998-01-27 14:40:00 +0000

    no message

---
+++ EnvelopeEditorMorph/instance/construction/addHandlesIn-3A-.cs
@@ -0,0 +1,17 @@
+addHandlesIn: frame
+	| handle |
+	handle := PolygonMorph
+		vertices: (Array with: 0@0 with: 8@0 with: 4@8)
+		color: Color orange borderWidth: 1 borderColor: Color black.
+	handle addMorph: ((RectangleMorph
+			newBounds: ((self handleOffset: handle)-(2@0) extent: 1@(graphArea height-2))
+			color: Color orange) borderWidth: 0).
+
+	limitHandles _ Array with: handle with: handle fullCopy with: handle fullCopy.
+	1 to: limitHandles size do:
+		[:i | handle _ limitHandles at: i.
+		handle on: #mouseStillDown
+				send: #limitHandleMoveEvent:from:index:
+				to: self withValue: i.
+		self addMorph: handle.
+		handle position: ((self xFromMs: (envelope points at: (limits at: i)) x) @ (graphArea top)) - (self handleOffset: handle)]
