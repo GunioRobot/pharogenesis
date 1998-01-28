2ab77ba1e4dbbd38ea01353d3ec4c864c523ae1d
tk <unknown>
1998-01-28 12:35:00 +0000

    no message

---
+++ SwikiPage/instance/accessing/timeOfAccept.cs
@@ -0,0 +1,14 @@
+timeOfAccept
+	"return the Time when the text was written on the file"
+
+	| theFile pp |
+	theFile _ FileStream oldFileNamed: file.
+	theFile setToEnd.
+	pp _ self backupAChunk: theFile.
+	pp _ pp - 5.		"before the back:"
+	theFile upTo: $'; skip: -1.		"name"
+	theFile nextDelimited: $'.
+	theFile upTo: $'; skip: -1.		"date"
+	theFile nextDelimited: $'.
+	theFile upTo: $'; skip: -1.		"time"
+	^ (theFile nextDelimited: $') asTime
