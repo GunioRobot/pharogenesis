93a39a11c75bfada2050340a82bb78357934cf03
jm <unknown>
1998-01-28 13:08:00 +0000

    no message

--- RestSound/instance/initialization/setDur-3A-.cs
+++ RestSound/instance/initialization/setDur-3A-.cs
@@ -1,5 +1,6 @@
 setDur: d
-	"Set duration in seconds."
+	"Set rest duration in seconds."
 
-	initialCount _ (d * self samplingRate asFloat) asInteger.
+	initialCount _ (d * self samplingRate asFloat) rounded.
 	count _ initialCount.
+	self reset.
