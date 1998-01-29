1a2c4878a83642c4925dd84d8e4931590c15446b
sw <unknown>
1998-01-29 13:08:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/experimentalCommand.cs
+++ ParagraphEditor/instance/menu messages/experimentalCommand.cs
@@ -1,7 +1,6 @@
 experimentalCommand
 	"Use for experimental command-key implementation.  using this, you can try things out without forever needing to reinitialize the ParagraphEditor.  2/7/96 sw"
 
-	self flag: #scottPrivate.
 	self inform: 
 'Cmd-t is not currently used.
 To get "ifTrue: [" inserted, 
