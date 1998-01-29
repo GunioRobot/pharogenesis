86e3b5eac0dcebc747620cde6b379999efc47f09
sw <unknown>
1998-01-29 13:08:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/presentSpecialMenu.cs
+++ ParagraphEditor/instance/menu messages/presentSpecialMenu.cs
@@ -1,11 +1,9 @@
 presentSpecialMenu
-	"Present a list of expressions, and if the user chooses one, evaluate it in the context of the receiver, a ParagraphEditor.  Primarily for debugging, this provides a convenient way to talk to the various views, controllers, and models associated with any text pane.  2/5/96 sw"
+	"Present a list of expressions, and if the user chooses one, evaluate it in the context of the receiver, a ParagraphEditor.  Primarily for debugging, this provides a convenient way to talk to the various views, controllers, and models associated with any text pane"
 
-	| aList reply items |
-	self flag: #scottPrivate.
+	| reply items |
 	self terminateAndInitializeAround:
-		[aList _ self specialMenuItems.
-		reply _ (PopUpMenu labelArray: (items _ self specialMenuItems) lines: #()) startUp.
+		[reply _ (PopUpMenu labelArray: (items _ self specialMenuItems) lines: #()) startUp.
 		reply = 0 ifTrue: [^ self].
 		Utilities evaluate: (items at: reply) in: [] to: self]
 	
