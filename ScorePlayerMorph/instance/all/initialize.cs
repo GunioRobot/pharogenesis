7569d3ba1df839c61b03f273b4c742e3843ff090
jm <unknown>
1998-01-30 14:55:00 +0000

    no message

---
+++ ScorePlayerMorph/instance/all/initialize.cs
@@ -0,0 +1,13 @@
+initialize
+
+	super initialize.
+	orientation _ #vertical.
+	centering _ #center.
+	hResizing _ #spaceFill.
+	vResizing _ #spaceFill.
+	inset _ 3.
+	color _ Color veryLightGray.
+	self borderWidth: 2.
+	self addMorph: self makeControls.
+	self onScorePlayer: (ScorePlayer new initialize) title: ' '.
+	self extent: 20@20.
