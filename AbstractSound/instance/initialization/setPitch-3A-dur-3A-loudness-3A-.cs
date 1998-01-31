f0e64113da2d2a42af61c167e5bed418d6505287
jm <unknown>
1998-01-31 15:26:00 +0000

    no message

--- AbstractSound/instance/initialization/setPitch-3A-dur-3A-loudness-3A-.cs
+++ AbstractSound/instance/initialization/setPitch-3A-dur-3A-loudness-3A-.cs
@@ -1,3 +1,10 @@
 setPitch: p dur: d loudness: l
+	"Initialize my envelopes for the given parameters. Subclasses overriding this method should include a resend to super."
 
-	self subclassResponsibility.
+	envelopes do: [:e |
+		(e isKindOf: VolumeEnvelope) ifTrue: [e scale: l].
+		(e isKindOf: PitchEnvelope) ifTrue: [e centerPitch: p].
+		e duration: d].
+	self initialVolume: l.
+	self duration: d.
+
