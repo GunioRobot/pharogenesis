3989635bd92702194ee89617893baf03ab928ca8
sw <unknown>
1998-02-01 16:55:00 +0000

    no message

---
+++ CngsMsgList/instance/menu messages/shiftedYellowButtonMenu.cs
@@ -0,0 +1,12 @@
+shiftedYellowButtonMenu
+	"Answer the menu to be put up when shift key is down.  "
+
+	^ PopUpMenu labels: 'browse method inheritance
+browse class hierarchy
+browse class
+browse method
+implementors of sent messages
+inspect instances
+inspect subinstances
+more...' 
+lines: #(5 7 )
