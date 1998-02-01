7ec04dd18936e4e4ce4a0cc8b2cc67f2a84ac9dd
di <unknown>
1998-02-01 21:37:00 +0000

    no message

--- ScreenController/instance/nested menus/openMenu.cs
+++ ScreenController/instance/nested menus/openMenu.cs
@@ -1,18 +1,20 @@
 openMenu
-	"Answer a menu for open-related items.  2/4/96 sw
-	 5/10/96 sw: useful expressions moved to help menu"
+	"Answer a menu for open-related items.  
+	 : useful expressions moved to help menu"
 
 	OpenMenu == nil ifTrue:
 		[OpenMenu _ SelectionMenu labelList:
 		#(	'open browser'
 			'open workspace'
 			'open file list'
+			'open change sorter'
 			'open project'
 			'open transcript'
-			'open system workspace')
-		selections: #(openBrowser openWorkspace openFileList openProject openTranscript  openSystemWorkspace)].
+			'open system workspace'
+			'open Morphic world')
+		lines: #()
+		selections: #(openBrowser openWorkspace openFileList openChangeManager openProject openTranscript openSystemWorkspace openMorphicWorld)].
 	^ OpenMenu
-
 "
 ScreenController new openMenu startUp
 "
