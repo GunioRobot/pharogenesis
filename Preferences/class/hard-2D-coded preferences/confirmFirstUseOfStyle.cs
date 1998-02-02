e7bd6f61a63f2f01f43eb88121d89d753468cee1
di <unknown>
1998-02-02 11:22:00 +0000

    no message

---
+++ Preferences/class/hard-2D-coded preferences/confirmFirstUseOfStyle.cs
@@ -0,0 +1,6 @@
+confirmFirstUseOfStyle
+	"Squeak will ask the user if this flag is true and a method is
+	being accepted for the first time with embellished text.
+	NOTE: ignoreStyleIfOnlyBold has precedence over this setting."
+
+	^ true
