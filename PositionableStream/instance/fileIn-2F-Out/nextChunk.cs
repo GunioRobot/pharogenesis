5650349e4dcb7aa3419c91c38f5372b95e903c1d
di <unknown>
1998-02-03 14:29:00 +0000

    no message

--- PositionableStream/instance/fileIn-2F-Out/nextChunk.cs
+++ PositionableStream/instance/fileIn-2F-Out/nextChunk.cs
@@ -1,10 +1,17 @@
 nextChunk
-	"Answer the contents of the receiver, up to the next terminator character.
-	Imbedded terminators are doubled."
-	| terminator segment |
+	"Answer the contents of the receiver, up to the next terminator character. Doubled terminators indicate an embedded terminator character."
+	| terminator out ch |
 	terminator _ $!.
+	out _ WriteStream on: (String new: 1000).
 	self skipSeparators.
-	segment _ self upTo: terminator.
-	[self peekFor: terminator] whileTrue:   "case of imbedded (doubled) terminator"
-			[segment _ (segment copyWith: terminator) , (self upTo: terminator)].
-	^ segment
+	[(ch _ self next) == nil] whileFalse: [
+		(ch == terminator) ifTrue: [
+			self peek == terminator ifTrue: [
+				self next.  "skip doubled terminator"
+			] ifFalse: [
+				^ out contents  "terminator is not doubled; we're done!"
+			].
+		].
+		out nextPut: ch.
+	].
+	^ out contents
