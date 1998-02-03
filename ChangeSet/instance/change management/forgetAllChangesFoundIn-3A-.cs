5cb684b75bb7cd609fc30b8fa98a332789aa66a4
sw <unknown>
1998-02-03 14:22:00 +0000

    no message

--- ChangeSet/instance/change management/forgetAllChangesFoundIn-3A-.cs
+++ ChangeSet/instance/change management/forgetAllChangesFoundIn-3A-.cs
@@ -1,5 +1,5 @@
 forgetAllChangesFoundIn: aChangeSet
-	"Remove from the receiver all method changes found in aChangeSet. The intention is facilitate the process of factoring a large set of changes into disjoint change sets.  3/13/96 sw.  Note that class-(re)-definition changes are not subtracted out, yet."
+	"Remove from the receiver all method changes found in aChangeSet. The intention is facilitate the process of factoring a large set of changes into disjoint change sets.  To use:  in a change sorter, copy over all the changes you want into some new change set, then use the subtract-other-side feature to subtract those changes from the larger change set, and continue in this manner"
 
 	| cls itsMethodChanges |
 
@@ -10,4 +10,6 @@ forgetAllChangesFoundIn: aChangeSet
 			ifTrue:
 				[itsMethodChanges _ aChangeSet methodChanges at: className  ifAbsent: [Dictionary new].
 				itsMethodChanges associationsDo:
-					[:assoc | self removeSelectorChanges:  assoc key class: cls]]]
+					[:assoc | self removeSelectorChanges:  assoc key class: cls].
+				(aChangeSet hasClassChangesFor: className) ifTrue:
+					[self removeClassChanges: cls]]]
