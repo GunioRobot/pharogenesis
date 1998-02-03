3a9db280a3b3b7901adfb5edbc0453ab88e97824
jm <unknown>
1998-02-03 11:18:00 +0000

    no message

---
+++ ObjectMemory/instance/garbage collection/incrementalGC.cs
@@ -0,0 +1,31 @@
+incrementalGC
+	"Do a mark/sweep garbage collection of just the young object area of object memory (i.e., objects above youngStart), using the root table to identify objects containing pointers to young objects from the old object area."
+
+	| survivorCount startTime |
+	self inline: false.
+	rootTableCount >= RootTableSize ifTrue: [
+		"root table overflow; cannot do an incremental GC (this should be very rare)"
+		statRootTableOverflows _ statRootTableOverflows + 1.
+		^ self fullGC
+	].
+
+	self preGCAction: false.
+	"incremental GC and compaction"
+	startTime _ self ioMicroMSecs.
+	self markPhase.
+	survivorCount _ self sweepPhase.
+	self incrementalCompaction.
+	allocationCount _ 0.
+	statIncrGCs _ statIncrGCs + 1.
+	statIncrGCMSecs _ statIncrGCMSecs + (self ioMicroMSecs - startTime).
+
+	survivorCount > tenuringThreshold ifTrue: [
+		"move up the young space boundary if there are too many survivors;
+		 this limits the number of objects that must be processed on future
+		 incremental GC's"
+
+		statTenures _ statTenures + 1.
+		self clearRootsTable.
+		youngStart _ freeBlock.  "reset the young object boundary"
+	].
+	self postGCAction.
