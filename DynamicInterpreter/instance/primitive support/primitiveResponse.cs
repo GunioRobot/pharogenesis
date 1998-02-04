c963298b425c70426136971b6685ad696f91f53c
di <unknown>
1998-02-04 23:54:00 +0000

    no message

--- DynamicInterpreter/instance/primitive support/primitiveResponse.cs
+++ DynamicInterpreter/instance/primitive support/primitiveResponse.cs
@@ -6,7 +6,7 @@ primitiveResponse
 	"check for possible timer interrupts after each primitive"
 	(successFlag and:
 	 [(nextWakeupTick ~= 0) and:
-	 [self ioMSecs >= nextWakeupTick]]) ifTrue: [
+	 [(self ioMSecs bitAnd: 16r1FFFFFFF) >= nextWakeupTick]]) ifTrue: [
 		interruptCheckCounter _ 1000.
 		self checkForInterrupts].
 	^ successFlag
