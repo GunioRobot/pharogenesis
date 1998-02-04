e32282750793067d66f4be539ca43f0285c52e3c
jm <unknown>
1998-02-04 08:58:00 +0000

    no message

--- FMSound/instance/initialization/setPitch-3A-dur-3A-loudness-3A-.cs
+++ FMSound/instance/initialization/setPitch-3A-dur-3A-loudness-3A-.cs
@@ -1,7 +1,10 @@
-setPitch: p dur: d loudness: l
+setPitch: p dur: d loudness: vol
+	"(FMSound pitch: 440.0 dur: 2.5 loudness: 0.4) play"
 
-	super setPitch: p dur: d loudness: l.
-	self modulation: 900 multiplier: 0.76.
-	self modulationDecay: 0.92.
-	self decayRate: 0.85.
-
+	super setPitch: p dur: d loudness: vol.
+	waveTable _ SineTable.
+	scaledWaveTableSize _ waveTable size * ScaleFactor.
+	modulation ifNil: [modulation _ 0.0].
+	multiplier ifNil: [multiplier _ 0.0].
+	self pitch: p.
+	self reset.
