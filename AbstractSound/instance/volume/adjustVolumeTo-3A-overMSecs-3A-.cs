1dd00a70e592a144457d9581a6adf49ff74b9478
jm <unknown>
1998-02-04 06:49:00 +0000

    no message

---
+++ AbstractSound/instance/volume/adjustVolumeTo-3A-overMSecs-3A-.cs
@@ -0,0 +1,16 @@
+adjustVolumeTo: vol overMSecs: mSecs
+	"Adjust the volume of this sound to the given volume, a number in the range [0.0..1.0], over the given number of milliseconds. The volume will be changed a little bit on each sample until the desired volume is reached."
+
+	| newScaledVol |
+	newScaledVol _ (32768.0 * vol) truncated.
+	newScaledVol = scaledVol ifTrue: [^ self].
+	scaledVolLimit _ newScaledVol.
+	scaledVolLimit > ScaleFactor ifTrue: [scaledVolLimit _ ScaleFactor].
+	scaledVolLimit < 0 ifTrue: [scaledVolLimit _ 0].
+	mSecs = 0
+		ifTrue: [  "change immediately"
+			scaledVol _ scaledVolLimit.
+			scaledVolIncr _ 0]
+		ifFalse: [
+			scaledVolIncr _
+				((scaledVolLimit - scaledVol) * 1000) // (self samplingRate * mSecs)].
