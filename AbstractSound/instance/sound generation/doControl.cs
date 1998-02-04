50da0cb72402d420c4e7de8eb57f1ca95b79e380
jm <unknown>
1998-02-04 09:56:00 +0000

    no message

--- AbstractSound/instance/sound generation/doControl.cs
+++ AbstractSound/instance/sound generation/doControl.cs
@@ -1,5 +1,12 @@
 doControl
-	"Update the control parameters of this sound (e.g., it's envelope)."
+	"Update the control parameters of this sound using its envelopes, if any."
 	"Note: This is only called at a small fraction of the sampling rate."
 
-	^ self
+	| pitchModOrRatioChange |
+	mSecsSinceStart _ mSecsSinceStart + (1000 // self controlRate).
+	envelopes size > 0 ifTrue: [
+		pitchModOrRatioChange _ false.
+		1 to: envelopes size do: [:i |
+			((envelopes at: i) updateTargetAt: mSecsSinceStart)
+				ifTrue: [pitchModOrRatioChange _ true]].
+		pitchModOrRatioChange ifTrue: [self internalizeModulationAndRatio]].
