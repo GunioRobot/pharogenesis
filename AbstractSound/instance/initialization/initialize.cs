90d023a0466ae6b30b511d4bb1920aee0f76eb52
jm <unknown>
1998-02-04 09:54:00 +0000

    no message

--- AbstractSound/instance/initialization/initialize.cs
+++ AbstractSound/instance/initialization/initialize.cs
@@ -1,3 +1,8 @@
 initialize
 
-	^ self
+	envelopes _ #().
+	mSecsSinceStart _ 0.
+	samplesUntilNextControl _ 0.
+	scaledVol _ (1.0 * ScaleFactor) rounded.
+	scaledVolIncr _ 0.
+	scaledVolLimit _ scaledVol.
