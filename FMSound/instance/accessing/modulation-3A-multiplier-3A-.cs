4ea3f8f9123462474f9e223be01c7d5433e2f873
jm <unknown>
1998-02-04 07:39:00 +0000

    no message

--- FMSound/instance/accessing/modulation-3A-multiplier-3A-.cs
+++ FMSound/instance/accessing/modulation-3A-multiplier-3A-.cs
@@ -1,9 +1,4 @@
-modulation: mod multiplier: mult
+modulation: mod multiplier: freqRatio
+	"For backward compatibility. Needed to read old .fmp files."
 
-	| modInRange multInRange |
-	modInRange _ mod rounded min: 1023 max: 0.
-	multInRange _ mult asFloat max: 0.0.
-	initialModulation _ (modInRange * increment) bitShift: -7.
-	modulation _ initialModulation.
-	offsetIncrement _ (increment * multInRange) rounded.
-	offsetIndex _ 1.
+	self modulation: mod ratio: freqRatio.
