8c1820bcadcf51046463eba98e680c591c0ab138
ar <unknown>
1998-03-12 22:42:00 +0000

    no message

---
+++ VRMLWriter/instance/multi field writing/writeMultiFieldRotation-3A-on-3A-indent-3A-.cs
@@ -0,0 +1,7 @@
+writeMultiFieldRotation: fields on: aVRMLStream indent: level 
+	"This method was automatically generated"
+	aVRMLStream nextPut: $[.
+	1 to: fields size do:[:i|
+		i > 1 ifTrue:[aVRMLStream crtab: level+1].
+		self storeSingleFieldRotation: (fields at: i) on: aVRMLStream indent: level+1].
+	aVRMLStream nextPut:$].
