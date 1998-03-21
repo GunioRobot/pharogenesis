c7628b6fe48e3834dfe9ccb5e88e1fe546100948
ar <unknown>
1998-03-21 02:36:00 +0000

    no message

--- Behavior/instance/testing class hierarchy/kindOfSubclass.cs
+++ Behavior/instance/testing class hierarchy/kindOfSubclass.cs
@@ -1,8 +1,8 @@
 kindOfSubclass 
 	"Answer a String that is the keyword that describes the receiver's kind of 
 	subclass, either a regular subclass, a variableSubclass, a 
-	variableByteSubclass, or a variableWordSubclass."
-
+	variableByteSubclass, a variableWordSubclass, or a weakSubclass"
+	self isWeak ifTrue:[^' weakSubclass: '].
 	self isVariable
 		ifTrue: [self isBits
 					ifTrue: [self isBytes
