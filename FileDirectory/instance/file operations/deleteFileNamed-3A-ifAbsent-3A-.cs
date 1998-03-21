3869750c86bd85ac5d893eceb596111b7f7a063a
ar <unknown>
1998-03-21 18:08:00 +0000

    no message

--- FileDirectory/instance/file operations/deleteFileNamed-3A-ifAbsent-3A-.cs
+++ FileDirectory/instance/file operations/deleteFileNamed-3A-ifAbsent-3A-.cs
@@ -1,5 +1,7 @@
 deleteFileNamed: localFileName ifAbsent: failBlock
-	"Delete the file of the given name if it exists, else evaluate failBlock."
-
-	(self primDeleteFileNamed: (self fullNameFor: localFileName)) == nil
-		ifTrue: [^ failBlock value].
+	"Delete the file of the given name if it exists, else evaluate failBlock.
+	If the first deletion attempt fails do a GC to force finalization of any lost references. ar 3/21/98 17:53"
+	(self 
+		retryWithGC:[self primDeleteFileNamed: (self fullNameFor: localFileName)]
+		until:[:result| result notNil]) == nil
+			ifTrue: [^failBlock value].
