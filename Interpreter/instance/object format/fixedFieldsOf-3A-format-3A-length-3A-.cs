bad1c21dc215e4b4270e5b630d6366dc7ad48f34
ar <unknown>
1998-03-21 02:37:00 +0000

    no message

--- Interpreter/instance/object format/fixedFieldsOf-3A-format-3A-length-3A-.cs
+++ Interpreter/instance/object format/fixedFieldsOf-3A-format-3A-length-3A-.cs
@@ -6,10 +6,10 @@ fixedFieldsOf: oop format: fmt length: wordLength
 "
 	| class classFormat |
 	self inline: true.
-	((fmt > 3) or: [fmt = 2]) ifTrue: [^ 0].  "indexable fields only"
+	((fmt > 4) or: [fmt = 2]) ifTrue: [^ 0].  "indexable fields only"
 	fmt < 2 ifTrue: [^ wordLength].  "fixed fields only (zero or more)"
 	
-	"fmt = 3: mixture of fixed and indexable fields, so must look at class format word"
+	"fmt = 3 or 4: mixture of fixed and indexable fields, so must look at class format word"
 	class _ self fetchClassOf: oop.
 	classFormat _ self formatOfClass: class.
 	^ (classFormat >> 11 bitAnd: 16rC0) + (classFormat >> 2 bitAnd: 16r3F) - 1
