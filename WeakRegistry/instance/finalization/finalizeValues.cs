b3844b9724f31581fc4ab9b125c12985df7b266f
ar <unknown>
1998-03-22 00:19:00 +0000

    no message

---
+++ WeakRegistry/instance/finalization/finalizeValues.cs
@@ -0,0 +1,17 @@
+finalizeValues
+	"Some of our elements may have gone away. Look for those and activate the associated executors."
+	| finiObjects |
+	finiObjects := nil.
+	"First collect the objects."
+	self protected:[
+		valueDictionary associationsDo:[:assoc|
+			assoc key isNil ifTrue:[
+				finiObjects isNil 
+					ifTrue:[finiObjects := OrderedCollection with: assoc value]
+					ifFalse:[finiObjects add: assoc value]]
+		].
+		finiObjects isNil ifFalse:[valueDictionary finalizeValues].
+	].
+	"Then do the finalization"
+	finiObjects isNil ifTrue:[^self].
+	finiObjects do:[:each| each finalize].
