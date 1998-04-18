7ab2c1fa1c98ea242409968703287a3f27e9dfdb
ls <unknown>
1998-04-18 16:23:00 +0000

    no message

--- HTMLformatter/class/translating/evalEmbedded-3A-with-3A-.cs
+++ HTMLformatter/class/translating/evalEmbedded-3A-with-3A-.cs
@@ -1,4 +1,5 @@
 evalEmbedded: stringOrStream with: request
 	| formatter |
-	formatter _ self forEvaluatingEmbedded: stringOrStream.
+	formatter _
+self forEvaluatingEmbedded: stringOrStream.
 	^formatter format: request
