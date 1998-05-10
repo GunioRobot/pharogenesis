bc3900ad111c5fa4dda61d01805517e5421d2864
TPR <unknown>
1998-05-10 21:47:00 +0000

    no message

--- FileDirectory/class/private/activeDirectoryClass.cs
+++ FileDirectory/class/private/activeDirectoryClass.cs
@@ -1,10 +1,8 @@
 activeDirectoryClass
 	"Return the concrete FileDirectory subclass for the platform on which we are currently running."
 
-	| platformDelimiter |
-	platformDelimiter _ self primPathNameDelimiter.
 	FileDirectory allSubclasses do: [:class |
-		class pathNameDelimiter = platformDelimiter ifTrue: [^ class]].
+		class isActiveDirectoryClass ifTrue: [^ class]].
 
 	"no responding subclass; use FileDirectory"
 	^ FileDirectory
