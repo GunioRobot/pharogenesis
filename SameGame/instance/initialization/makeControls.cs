29f67f4da0355c5efb9e989954533c8e98fd2ee6
tao <unknown>
1998-05-18 20:39:00 +0000

    no message

---
+++ SameGame/instance/initialization/makeControls.cs
@@ -0,0 +1,41 @@
+makeControls
+
+	| row |
+	row _ AlignmentMorph newRow
+		color: color;
+		borderWidth: 0;
+		inset: 3.
+	row hResizing: #spaceFill; vResizing: #shrinkWrap; centering: #center; extent: 5@5.
+	row addMorph:
+		(self
+			buildButton: SimpleSwitchMorph new
+			target: self
+			label: 'Help'
+			selector: #help:).
+	row addMorph:
+		(self
+			buildButton: SimpleButtonMorph new
+			target: self
+			label: 'Quit'
+			selector: #delete).
+	row addMorph:
+		(self
+			buildButton: SimpleButtonMorph new
+			target: self board
+			label: 'Hint'
+			selector: #hint).
+	row addMorph:
+		(self
+			buildButton: SimpleButtonMorph new
+			target: self
+			label: 'New game'
+			selector: #newGame).
+	selectionDisplay _ LedMorph new
+		digits: 2;
+		extent: (2*10@15).
+	row addMorph: (self wrapPanel: selectionDisplay label: 'Selection:').
+	scoreDisplay _ LedMorph new
+		digits: 4;
+		extent: (4*10@15).
+	row addMorph: (self wrapPanel: scoreDisplay label: 'Score:').
+	^ row
