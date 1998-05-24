09e455537b62fc35345c5d33a17084b3a96c8e61
di <unknown>
1998-05-24 15:50:00 +0000

    no message

---
+++ ServerDirectory/instance/server groups/group.cs
@@ -0,0 +1,6 @@
+group
+	"NOTE: This is an association whose key is the anme of the group, and
+	whose value is an array of serverDirectories, the first of which is
+	deemed to be the group directory."
+
+	^ group
