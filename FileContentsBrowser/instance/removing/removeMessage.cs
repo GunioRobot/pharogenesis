fda82e3c21dc68bc367146455f4a2af4c9293f01
wod <unknown>
1998-05-24 20:46:00 +0000

    no message

---
+++ FileContentsBrowser/instance/removing/removeMessage.cs
@@ -0,0 +1,10 @@
+removeMessage
+	| messageName |
+	messageListIndex = 0 ifTrue: [^ self].
+	self okToChange ifFalse: [^ self].
+	messageName _ self selectedMessageName.
+	(self selectedClassOrMetaClass confirmRemovalOf: messageName) ifFalse:[^false].
+	self selectedClassOrMetaClass removeMethod: self selectedMessageName.
+	self messageListIndex: 0.
+	self setClassOrganizer.  "In case organization not cached"
+	self changed: #messageList.
