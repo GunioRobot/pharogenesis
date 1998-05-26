934ced2759924e525fbbd49239eb05c6beea75cf
wod <unknown>
1998-05-26 17:03:00 +0000

    no message

--- PluggableTextMorph/instance/model access/setSelection-3A-.cs
+++ PluggableTextMorph/instance/model access/setSelection-3A-.cs
@@ -1,4 +1,4 @@
 setSelection: sel
 	selectionInterval _ sel.
 	textMorph editor selectFrom: sel first to: sel last.
-	self scrollSelectionIntoView
+	self scrollSelectionIntoView ifFalse: [scroller changed].
