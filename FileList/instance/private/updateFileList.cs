4fdbe12c3f35e1fa2d62957fed50083480e41b5b
wod <unknown>
1998-05-27 17:47:00 +0000

    no message

--- FileList/instance/private/updateFileList.cs
+++ FileList/instance/private/updateFileList.cs
@@ -1,15 +1,16 @@
 updateFileList
 	"Update my files list with file names in the current directory that match the pattern."
-
+	"wod 5/27/1998: nil out the fileName."
 	Cursor execute showWhile:
 		[list _ (pattern includes: $*) | (pattern includes: $#)
-			ifTrue: [directory listForPattern: pattern sortMode: sortMode]
+			ifTrue: [self listForPattern: pattern]
 			ifFalse: [
 				pattern isEmpty
-					ifTrue: [directory listForPattern: '*' sortMode: sortMode]
-					ifFalse: [directory listForPattern: '*', pattern, '*' sortMode: sortMode]].
+					ifTrue: [self listForPattern: '*']
+					ifFalse: [self listForPattern: '*', pattern, '*']].
 		listIndex _ 0.
 		volListIndex _ volList size.
+		fileName _ nil.
 		contents _ ''.
 		self changed: #volumeListIndex.
 		self changed: #fileList].
