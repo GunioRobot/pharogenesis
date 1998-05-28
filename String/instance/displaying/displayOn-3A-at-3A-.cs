c9d67fe6159af61d48135c96d0c0c4c17f86d863
di <unknown>
1998-05-28 13:25:00 +0000

    no message

--- String/instance/displaying/displayOn-3A-at-3A-.cs
+++ String/instance/displaying/displayOn-3A-at-3A-.cs
@@ -2,4 +2,5 @@ displayOn: aDisplayMedium at: aPoint
 	"Show a representation of the receiver as a DisplayText at location
 	aPoint on aDisplayMedium."
 
-	self asDisplayText displayOn: aDisplayMedium at: aPoint
+	(self asDisplayText foregroundColor: Color black backgroundColor: Color white)
+		displayOn: aDisplayMedium at: aPoint
