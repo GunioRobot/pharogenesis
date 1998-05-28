dd7f00855d1588b43f81b9e66c611e3f0c71c3b6
di <unknown>
1998-05-28 12:00:00 +0000

    no message

---
+++ ParagraphEditor/instance/editing keys/makeUppercase-3A-.cs
@@ -0,0 +1,6 @@
+makeUppercase: characterStream 
+	"Force the current selection to uppercase.  Triggered by Cmd-X."
+
+	sensor keyboard.		"flush the triggering cmd-key character"
+	self replaceSelectionWith: (Text fromString: (self selection string asUppercase)).
+	^ true
