2c07e37397e9fdc60479c05b4ef951c7f2a91680
jm <unknown>
1998-06-03 14:29:00 +0000

    no message

--- PolygonMorph/instance/editing/newVertex-3A-fromHandle-3A-afterVert-3A-.cs
+++ PolygonMorph/instance/editing/newVertex-3A-fromHandle-3A-afterVert-3A-.cs
@@ -1,7 +1,7 @@
 newVertex: evt fromHandle: handle afterVert: ix
-	"Insert a new vertex and fix everything up!
-	Install the drag-handle of the new vertex as recipient of further mouse events."
+	"Insert a new vertex and fix everything up! Install the drag-handle of the new vertex as recipient of further mouse events."
+
 	| pt |
 	pt _ evt cursorPoint.
-	self setVertices: (vertices copyReplaceFrom: ix+1 to: ix with: (Array with: pt)).
-	evt hand mouseDownRecipient: (handles at: ix+1*2-1)
+	self setVertices: (vertices copyReplaceFrom: ix + 1 to: ix with: (Array with: pt)).
+	evt hand newMouseFocus: (handles at: ((ix + 1) * 2) - 1).
