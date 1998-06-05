dae3845decfe757ecaec3ec2b7c5463ead528a57
ar <unknown>
1998-06-05 21:48:00 +0000

    no message

--- ControlManager/instance/scheduling/scheduleActive-3A-.cs
+++ ControlManager/instance/scheduling/scheduleActive-3A-.cs
@@ -3,6 +3,6 @@ scheduleActive: aController
 	active scheduling process asked to schedule this controller and that a 
 	new process associated this controller takes control. So this is the last act 
 	of the active scheduling process."
-
+	<primitive: 19> "Simulation guard"
 	self scheduleActiveNoTerminate: aController.
 	Processor terminateActive
