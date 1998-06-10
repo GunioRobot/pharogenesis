8eed32e222c8578afbafb64886f1d07b32b781c1
di <unknown>
1998-06-10 13:18:00 +0000

    no message

--- StandardSystemView/instance/label access/relabel-3A-.cs
+++ StandardSystemView/instance/label access/relabel-3A-.cs
@@ -2,6 +2,7 @@ relabel: aString
 	"A new string for the label.  Window is assumed to be active.
 	Window will redisplay only if label bar has to grow."
 	| oldRegion oldWidth |
+	(model windowReqNewLabel: aString) ifFalse: [^ self].
 	oldRegion _ self labelTextRegion.
 	oldWidth _ self insetDisplayBox width.
 	self label: aString.
