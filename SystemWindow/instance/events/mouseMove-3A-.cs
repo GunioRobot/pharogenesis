04037873d26a33782a673021c25552aab5d9221c
di <unknown>
1998-06-10 14:41:00 +0000

    no message

---
+++ SystemWindow/instance/events/mouseMove-3A-.cs
@@ -0,0 +1,7 @@
+mouseMove: evt
+	| cp |
+	model windowActiveOnFirstClick ifTrue:
+		["Normally window takes control on first click.
+		Need explicit transmission for first-click activity."
+		cp _ evt cursorPoint.
+		submorphs do: [:m | (m containsPoint: cp) ifTrue: [m mouseMove: evt]]]
