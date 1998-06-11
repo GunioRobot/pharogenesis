e33076ac94d0f288c41f77ae1df7e3daca940636
di <unknown>
1998-06-11 22:01:00 +0000

    no message

--- Pen/instance/geometric designs/dragon-3A-.cs
+++ Pen/instance/geometric designs/dragon-3A-.cs
@@ -1,5 +1,6 @@
-dragon: n  "Display restoreAfter: [Display fillWhite. 1 to: 4 do:
-	[:i | Pen new color: i; turn: 90*i; dragon: 10]]"
+dragon: n  "Display restoreAfter: [Display fillWhite. Pen new dragon: 10]."
+	"Display restoreAfter: [Display fillWhite. 1 to: 4 do:
+				[:i | Pen new color: i; turn: 90*i; dragon: 10]]"
 	"Draw a dragon curve of order n in the center of the screen."
 	n = 0
 		ifTrue: [self go: 5]
