c460f110ddeda8ea62cf756d804a33fa22d3c833
tk <unknown>
1998-06-13 14:25:00 +0000

    no message

--- UserScript/instance/initialization/donorActor-3A-ownActor-3A-.cs
+++ UserScript/instance/initialization/donorActor-3A-ownActor-3A-.cs
@@ -1,4 +1,6 @@
 donorActor: player1 ownActor: player2
 	player _ player2.
-	currentScriptEditor ifNotNil: [currentScriptEditor donorActor: player1 ownActor: player2].
+	currentScriptEditor ifNotNil: [
+		currentScriptEditor == #textuallyCoded ifFalse: [
+			currentScriptEditor donorActor: player1 ownActor: player2]].
 	self allScriptVersionsDo: [:anEditor | anEditor donorActor: player1 ownActor: player2]
