b22f4bf355975d5a3a015f079b0daa75f9a1c9c2
tk <unknown>
1998-06-14 15:18:00 +0000

    no message

--- PasteUpMorph/instance/scripting/scriptorForTextualScript-3A-ofPlayer-3A-.cs
+++ PasteUpMorph/instance/scripting/scriptorForTextualScript-3A-ofPlayer-3A-.cs
@@ -1,5 +1,6 @@
 scriptorForTextualScript: aSelector ofPlayer: aPlayer
 	| aScriptor |
+	self world ifNil: [^ nil].
 	aScriptor _ ScriptEditorMorph new setMorph: aPlayer costume scriptName: aSelector.
 	aScriptor position: (self primaryHand position - (10 @ 10)).
 	^ aScriptor
