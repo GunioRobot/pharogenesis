8fa572f8b33e1e504ed9ed2fca154177126e516d
wod <unknown>
1998-06-16 15:07:00 +0000

    no message

--- FileDirectory/instance/enumeration/statsForDirectoryTree-3A-.cs
+++ FileDirectory/instance/enumeration/statsForDirectoryTree-3A-.cs
@@ -1,20 +1,22 @@
 statsForDirectoryTree: rootedPathName
 	"Return the size statistics for the entire directory tree starting at the given root. The result is a three element array of the form: (<number of folders><number of files><total bytes in all files>). This method also serves as an example of how recursively enumerate a directory tree."
-	"FileDirectory default statsForDirectoryTree: 'Black Uhuru:System Folder'"
+	"wod 6/16/1998: add Cursor wait, and use 'self pathNameDelimiter asString' rather than hardwired ':' "
+	"FileDirectory default statsForDirectoryTree: '\smalltalk'"
 
 	| dirs files bytes todo p entries |
-	dirs _ files _ bytes _ 0.
-	todo _ OrderedCollection with: rootedPathName.
-	[todo isEmpty] whileFalse: [
-		p _ todo removeFirst.
-		entries _ self directoryContentsFor: p.
-		entries do: [:entry |
-			(entry at: 4)
-				ifTrue: [
-					todo addLast: (p, ':', (entry at: 1)).
-					dirs _ dirs + 1]
-				ifFalse: [
-					files _ files + 1.
-					bytes _ bytes + (entry at: 5)]]].
+	Cursor wait showWhile: [
+		dirs _ files _ bytes _ 0.
+		todo _ OrderedCollection with: rootedPathName.
+		[todo isEmpty] whileFalse: [
+			p _ todo removeFirst.
+			entries _ self directoryContentsFor: p.
+			entries do: [:entry |
+				(entry at: 4)
+					ifTrue: [
+						todo addLast: (p, self pathNameDelimiter asString, (entry at: 1)).
+						dirs _ dirs + 1]
+					ifFalse: [
+						files _ files + 1.
+						bytes _ bytes + (entry at: 5)]]]].
 
 	^ Array with: dirs with: files with: bytes
