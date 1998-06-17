11e51e6bb1087ff27058a8129973b50d2d1c556e
sw <unknown>
1998-06-17 16:05:00 +0000

    no message

--- Presenter/instance/standardPlayer etc/standardPlayer.cs
+++ Presenter/instance/standardPlayer etc/standardPlayer.cs
@@ -1,5 +1,5 @@
 standardPlayer
 	standardPlayer ifNil:
 		[self createStandardPlayer].
-	standardPlayer costume isInWorld ifFalse: [associatedMorph addMorphFront: standardPlayer costume].
+	standardPlayer costume isInWorld ifFalse: [associatedMorph addMorphNearBack: standardPlayer costume].
 	^ standardPlayer
