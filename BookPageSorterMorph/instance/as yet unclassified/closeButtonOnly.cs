2a9427dc1d6e2092f935b86ea5f949518b74b5f5
jm <unknown>
1998-06-17 21:42:00 +0000

    no message

---
+++ BookPageSorterMorph/instance/as yet unclassified/closeButtonOnly.cs
@@ -0,0 +1,12 @@
+closeButtonOnly
+	"Replace my default control panel with one that has only a close button."
+
+	| b r |
+	self firstSubmorph delete.  "remove old control panel"
+	b _ SimpleButtonMorph new target: self; borderColor: Color black.
+	r _ AlignmentMorph newRow.
+	r color: b color; borderWidth: 0; inset: 0.
+	r hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
+	r centering: #topLeft.
+	r addMorphBack: (b fullCopy label: 'Close'; actionSelector: #delete).
+	self addMorphFront: r.
