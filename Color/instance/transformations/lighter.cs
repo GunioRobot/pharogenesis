3747d8bceed300beadeecd85b406e964f441ca5a
jm <unknown>
1998-06-17 11:23:00 +0000

    no message

--- Color/instance/transformations/lighter.cs
+++ Color/instance/transformations/lighter.cs
@@ -1,3 +1,4 @@
 lighter
-	"Return a lighter shade of the same color.  1/6th towards white. 6/18/96 tk  Should this be an absolute step, instead of relative?"
-	^ self mixed: 5/6 with: Color white
+	"Answer a lighter shade of this color."
+
+	^ self mixed: 0.8333 with: Color white
