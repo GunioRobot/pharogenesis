3db56689f66da1a41e865f58a47202e7c8f7949b
di <unknown>
1998-06-21 13:02:00 +0000

    no message

--- ChangeSorter/instance/changeSet menu/update.cs
+++ ChangeSorter/instance/changeSet menu/update.cs
@@ -1,9 +1,5 @@
 update
 	"recompute all of my panes"
 
-	self okToChange ifFalse: [^ self].
-	self showChangeSet: myChangeSet.
-	parent ifNotNil: [
-		(parent other: self) okToChange ifTrue: [
-			(parent other: self) showChangeSet: 
-				(parent other: self) myChangeSet]].
+	self updateIfNecessary.
+	parent ifNotNil: [(parent other: self) updateIfNecessary]
