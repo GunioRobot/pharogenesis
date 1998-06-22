24990e791ab4d026682494f60f6558d5ce5ac692
di <unknown>
1998-06-22 10:55:00 +0000

    no message

--- TextMorph/instance/drawing/drawBoundsOn-3A-.cs
+++ TextMorph/instance/drawing/drawBoundsOn-3A-.cs
@@ -1,7 +1,4 @@
 drawBoundsOn: aCanvas
 	"Shows where line boundaries are"
-	container ifNil:
-			[aCanvas frameRectangle: bounds color: Color green]
-		ifNotNil:
-			[self paragraph lines do:
-				[:line | aCanvas frameRectangle: line rectangle color: Color green]].
+	self paragraph lines do:
+		[:line | aCanvas frameRectangle: line rectangle color: Color brown]
