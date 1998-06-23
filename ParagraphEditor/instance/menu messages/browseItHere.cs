8be9135792636b0cf97d36688cec5bab8e4ff495
di <unknown>
1998-06-23 11:08:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/browseItHere.cs
+++ ParagraphEditor/instance/menu messages/browseItHere.cs
@@ -1,12 +1,10 @@
 browseItHere
 	"Retarget the receiver's window to look at the selected class, if appropriate.  3/1/96 sw"
-
 	| aSymbol foundClass b |
-
-	(((b _ view topView model) isKindOf: Browser) and: [b couldBrowseAnyClass])
+	(((b _ model) isKindOf: Browser) and: [b couldBrowseAnyClass])
 		ifFalse: [^ view flash].
 	model okToChange ifFalse: [^ view flash].
-	self selectLine.
+	self selectionInterval isEmpty ifTrue: [self selectWord].
 	(aSymbol _ self selectedSymbol) isNil ifTrue: [^ view flash].
 
 	self terminateAndInitializeAround:
