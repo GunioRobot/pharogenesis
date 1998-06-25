89fcd9dc9b01f0bc21c3cae5e434985ce1781f26
ls <unknown>
1998-06-25 17:21:00 +0000

    no message

---
+++ HtmlFormatter/instance/formatting commands/ensureNewlines-3A-.cs
@@ -0,0 +1,4 @@
+ensureNewlines: number
+	"make sure there are at least number preceding newlines"
+	number > precedingNewlines ifTrue: [
+		(number - precedingNewlines) timesRepeat: [ self addChar: Character cr ] ].
