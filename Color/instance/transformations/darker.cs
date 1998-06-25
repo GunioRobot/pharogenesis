76802aa12767ff0eee281083028e22ad5c7f822b
jm <unknown>
1998-06-25 10:12:00 +0000

    no message

--- Color/instance/transformations/darker.cs
+++ Color/instance/transformations/darker.cs
@@ -1,3 +1,4 @@
 darker
-	"Return a lighter shade of the same color.  1/6th towards white. 6/18/96 tk  Should this be an absolute step, instead of relative?"
-	^ self mixed: 5/6 with: Color black
+	"Answer a darker shade of this color."
+
+	^ self mixed: 0.8333 with: Color black
