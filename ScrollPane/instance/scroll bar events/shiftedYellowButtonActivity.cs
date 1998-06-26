454f6e2d5756424fc5ebe33469d9d8bd40b45c99
di <unknown>
1998-06-26 13:31:00 +0000

    no message

--- ScrollPane/instance/scroll bar events/shiftedYellowButtonActivity.cs
+++ ScrollPane/instance/scroll bar events/shiftedYellowButtonActivity.cs
@@ -1,6 +1,2 @@
 shiftedYellowButtonActivity
-	| menu event |
-	(menu _ self getMenu: true) ifNotNil:
-		[event _ self primaryHand lastEvent.
-		menu setInvokingView: self.
-		menu popUpAt: event cursorPoint event: event]
+	^ self yellowButtonActivity: true
