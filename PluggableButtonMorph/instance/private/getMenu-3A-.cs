1fd4912f91720a6c261d7d483fbdfcb62d9e8e4d
di <unknown>
1998-06-26 09:40:00 +0000

    no message

--- PluggableButtonMorph/instance/private/getMenu-3A-.cs
+++ PluggableButtonMorph/instance/private/getMenu-3A-.cs
@@ -1,4 +1,4 @@
-getMenu: evt
+getMenu: shiftPressed
 	"Answer the menu for this button, supplying an empty menu to be filled in. If the menu selector takes an extra argument, pass in the current state of the shift key."
 
 	| menu |
@@ -7,5 +7,5 @@ getMenu: evt
 	getMenuSelector numArgs = 1 ifTrue:
 		[^ model perform: getMenuSelector with: menu].
 	getMenuSelector numArgs = 2 ifTrue:
-		[^ model perform: getMenuSelector with: menu with: evt shiftPressed].
+		[^ model perform: getMenuSelector with: menu with: shiftPressed].
 	^ self error: 'The getMenuSelector must be a 1- or 2-keyword symbol'
