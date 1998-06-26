7108a9967f2f604c5f4b85ec4a58091581ac9f3b
tk <unknown>
1998-06-26 15:58:00 +0000

    no message

--- TextStyle/instance/fonts and font indexes/collectionFromFileNamed-3A-.cs
+++ TextStyle/instance/fonts and font indexes/collectionFromFileNamed-3A-.cs
@@ -1,18 +1,24 @@
 collectionFromFileNamed: fileName
-	"Read the file.  It is an Array of StrikeFonts.  File format is the ReferenceStream format.  (Do not use SmartRefStream, it is too smart.  It only writes a DiskProxy!)   For any fonts with new names, add them to DefaultTextStyle.fontArray.  
+	"Read the file.  It is an TextStyle whose StrikeFonts are to be added to the system.  (Written by fooling SmartRefStream, so it won't write a DiskProxy!)  These fonts will be added to the master TextSytle for this font family.  
 	To write out fonts: 
 		| ff | ff _ ReferenceStream fileNamed: 'new fonts'.
-		ff nextPut: (TextStyle default fontArray).
+		TextConstants at: #forceFontWriting put: true.
+		ff nextPut: (TextConstants at: #AFontName).
+			'do not mix font families in the TextStyle written out'.
+		TextConstants at: #forceFontWriting put: false.
 		ff close.
 	To read: (TextStyle default collectionFromFileNamed: 'new fonts')
 *** Do not remove this method *** "
 
-	| ff this names |
+	| ff this newName style heights |
 	ff _ ReferenceStream fileNamed: fileName.
-	this _ ff nextAndClose.
-		"Only works if file created by ReferenceStream, not SmartRefStream"
-	this class == Array ifTrue:
-			[names _ self fontNames.
-			this do: [:each | each class == StrikeFont ifTrue:
-				[(names includes: each name) ifFalse:
-					[fontArray _ fontArray copyWith: each]]]].
+	this _ ff nextAndClose.	"Only works if file created by special code above"
+	newName _ this fontArray first name.
+	this fontArray do: [:aFont | aFont name = newName ifFalse: [
+		self error: 'All must be same family']].
+	style _ TextConstants at: newName asSymbol ifAbsent: [
+		^ TextConstants at: newName asSymbol put: this].		"new family"
+	this fontArray do: [:aFont | "add new fonts"
+		heights _ style fontArray collect: [:bFont | bFont height].
+		(heights includes: aFont height) ifFalse: [
+			style fontAt: style fontArray size + 1 put: aFont]].
