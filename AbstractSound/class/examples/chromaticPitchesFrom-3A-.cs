a2d5ecfcbfe87a49da2282b3dd429707c1245521
jm <unknown>
1998-06-30 18:40:00 +0000

    no message

--- AbstractSound/class/examples/chromaticPitchesFrom-3A-.cs
+++ AbstractSound/class/examples/chromaticPitchesFrom-3A-.cs
@@ -1,8 +1,9 @@
 chromaticPitchesFrom: aPitch
-	| pitch halfStep |
+
+	| halfStep pitch |
+	halfStep _ 2.0 raisedTo: (1.0 / 12.0).
 	pitch _ aPitch isNumber
 			ifTrue: [aPitch]
 			ifFalse: [self pitchForName: aPitch].
-	halfStep _ self halfStep.
 	pitch _ pitch / halfStep.
 	^ (0 to: 14) collect: [:i | pitch _ pitch * halfStep]
