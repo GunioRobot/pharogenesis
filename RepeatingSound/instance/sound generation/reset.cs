b88b1aabf283272bedf7543a5365cdd50fa052d9
jm <unknown>
1998-06-30 18:28:00 +0000

    no message

--- RepeatingSound/instance/sound generation/reset.cs
+++ RepeatingSound/instance/sound generation/reset.cs
@@ -2,6 +2,7 @@ reset
 
 	super reset.
 	sound reset.
+	samplesPerIteration _ sound samplesRemaining.
 	iterationCount == #forever
 		ifTrue: [iteration _ 1]
 		ifFalse: [iteration _ iterationCount].
