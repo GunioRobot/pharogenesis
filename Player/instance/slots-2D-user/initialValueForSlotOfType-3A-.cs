f8a81c88e863a120627a499a88805a58f94ba6f2
sw <unknown>
1998-07-04 18:06:00 +0000

    no message

--- Player/instance/slots-2D-user/initialValueForSlotOfType-3A-.cs
+++ Player/instance/slots-2D-user/initialValueForSlotOfType-3A-.cs
@@ -2,4 +2,8 @@ initialValueForSlotOfType: aType
 	aType == #number ifTrue: [^ (1 to: 9) atRandom].
 	aType == #boolean ifTrue: [^ true].
 	aType == #player ifTrue: [^ self costume presenter standardPlayer].
-	aType == #color ifTrue: [^ Color red]
+	aType == #color ifTrue: [^ Color random].
+	aType == #string ifTrue: [^ 'abc'].
+	aType == #sound ifTrue: [^ 'croak'].
+	aType == #point ifTrue: [^ 20 @ 30].
+	^ nil
