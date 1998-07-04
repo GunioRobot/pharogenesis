db47dcafa85285e40bfd8644ca4dd082950a59b0
jm <unknown>
1998-07-04 08:21:00 +0000

    no message

--- ScorePlayer/instance/sound generation/mixSampleCount-3A-into-3A-startingAt-3A-leftVol-3A-rightVol-3A-.cs
+++ ScorePlayer/instance/sound generation/mixSampleCount-3A-into-3A-startingAt-3A-leftVol-3A-rightVol-3A-.cs
@@ -1,14 +1,16 @@
 mixSampleCount: n into: aSoundBuffer startingAt: startIndex leftVol: leftVol rightVol: rightVol
 	"Play a number of sounds concurrently. The level of each sound can be set independently for the left and right channels."
 
-	| someSoundIsDone pair snd trk left right |
+	| myLeftVol myRightVol someSoundIsDone pair snd trk left right |
+	myLeftVol _ (leftVol * overallVolume) asInteger.
+	myRightVol _ (rightVol * overallVolume) asInteger.
 	someSoundIsDone _ false.
 	1 to: activeSounds size do: [:i |
 		pair _ activeSounds at: i.
 		snd _ pair at: 1.
 		trk _ pair at: 2.
-		left _ (leftVol * (leftVols at: trk)) // ScaleFactor.
-		right _ (rightVol * (rightVols at: trk)) // ScaleFactor.
+		left _ (myLeftVol * (leftVols at: trk)) // ScaleFactor.
+		right _ (myRightVol * (rightVols at: trk)) // ScaleFactor.
 		snd samplesRemaining > 0
 			ifTrue: [
 				snd mixSampleCount: n into: aSoundBuffer startingAt: startIndex leftVol: left rightVol: right]
