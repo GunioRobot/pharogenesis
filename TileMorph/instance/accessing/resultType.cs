06dad62fdb6fbffbc37482c421388285492da01f
sw <unknown>
1998-07-04 18:01:00 +0000

    no message

--- TileMorph/instance/accessing/resultType.cs
+++ TileMorph/instance/accessing/resultType.cs
@@ -1,8 +1,15 @@
 resultType
 
 	type == #literal ifTrue:
-		[(literal isKindOf: Number) ifTrue: [^ #number]].
+		[(literal isKindOf: Number) ifTrue: [^ #number].
+		(literal isKindOf: String) ifTrue: [^ #string].
+		(literal isKindOf: Point) ifTrue: [^ #point].
+		(literal isKindOf: String) ifTrue: [^ #string].
+		(literal isKindOf: Boolean) ifTrue: [^ #boolean].
+		(literal isKindOf: Morph) ifTrue: [^ #costume]  "This last one dubious!"].
+
 	type == #expression ifTrue:
 		[^ #number].
+
 	type == #objRef ifTrue: [^ #player].
 	^ #unknown
