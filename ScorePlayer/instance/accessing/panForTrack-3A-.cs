99d15d8771676d9360aee8916686bb94cc92ac6d
jm <unknown>
1998-07-04 09:32:00 +0000

    no message

--- ScorePlayer/instance/accessing/panForTrack-3A-.cs
+++ ScorePlayer/instance/accessing/panForTrack-3A-.cs
@@ -1,3 +1,12 @@
 panForTrack: i
 
-	^ ((leftVols at: i) asFloat / ((leftVols at: i) + (rightVols at: i))) roundTo: 0.001
+	| left right fullVol pan |
+	left _ leftVols at: i.
+	right _ rightVols at: i.
+	left = right ifTrue: [^ 0.5].  "centered"
+	fullVol _ left max: right.
+	left < fullVol
+		ifTrue: [pan _ left asFloat / (2.0 * fullVol)]
+		ifFalse: [pan _ 1.0 - (right asFloat / (2.0 * fullVol))].
+	^ pan roundTo: 0.001
+
