ae2023a60e01e7c31e55e2ced1d29e7313b39e41
jm <unknown>
1998-07-05 17:53:00 +0000

    no message

--- AbstractSound/instance/playing/playSilently.cs
+++ AbstractSound/instance/playing/playSilently.cs
@@ -1,9 +1,10 @@
 playSilently
 	"Compute the samples of this sound without outputting them. Used for performance analysis."
 
-	| buf |
+	| bufSize buf |
 	self reset.
-	buf _ SoundBuffer newStereoSampleCount: (self samplingRate // 10).
+	bufSize _ self samplingRate // 10.
+	buf _ SoundBuffer newStereoSampleCount: bufSize.
 	[self samplesRemaining > 0] whileTrue: [
 		buf primFill: 0.
-		self playSampleCount: buf stereoSampleCount into: buf startingAt: 1].
+		self playSampleCount: bufSize into: buf startingAt: 1].
