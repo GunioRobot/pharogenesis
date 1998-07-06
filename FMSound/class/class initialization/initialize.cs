9585aceee6950376052793a3d8ecf633892cb672
jm <unknown>
1998-07-06 10:26:00 +0000

    no message

--- FMSound/class/class initialization/initialize.cs
+++ FMSound/class/class initialization/initialize.cs
@@ -8,4 +8,4 @@ initialize
 	radiansPerStep _ (2.0 * Float pi) / tableSize asFloat.
 	peak _ ((1 bitShift: 15) - 1) asFloat.  "range is +/- (2^15 - 1)"
 	1 to: tableSize do: [:i |
-		SineTable at: i put: (peak * (radiansPerStep * i) sin) rounded].
+		SineTable at: i put: (peak * (radiansPerStep * (i - 1)) sin) rounded].
