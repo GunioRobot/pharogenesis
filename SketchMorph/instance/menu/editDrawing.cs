a4f9859950a36ddf7e3e1b11e49e685ed4ea65e7
tk <unknown>
1998-07-06 15:31:00 +0000

    no message

--- SketchMorph/instance/menu/editDrawing.cs
+++ SketchMorph/instance/menu/editDrawing.cs
@@ -1,3 +1,4 @@
 editDrawing
 	self flag: #deferred.  "Don't allow this if the user is already in paint mode, because it creates a very strange situation."
+	"costumee ifNotNil: [self forwardDirection: costumee direction]."  "how say this?"
 	self editDrawingIn: self pasteUpMorph forBackground: false
