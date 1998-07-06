c8047bc36f9fdead914c000152be9be9e32fe3d0
jm <unknown>
1998-07-06 17:08:00 +0000

    no message

--- FMSound/instance/initialization/setPitch-3A-dur-3A-loudness-3A-.cs
+++ FMSound/instance/initialization/setPitch-3A-dur-3A-loudness-3A-.cs
@@ -1,10 +1,8 @@
-setPitch: p dur: d loudness: vol
-	"(FMSound pitch: 440.0 dur: 2.5 loudness: 0.4) play"
+setPitch: pitchNameOrNumber dur: d loudness: vol
+	"(FMSound pitch: 'a4' dur: 2.5 loudness: 0.4) play"
 
-	super setPitch: p dur: d loudness: vol.
-	waveTable _ SineTable.
-	scaledWaveTableSize _ waveTable size * ScaleFactor.
+	super setPitch: pitchNameOrNumber dur: d loudness: vol.
 	modulation ifNil: [modulation _ 0.0].
 	multiplier ifNil: [multiplier _ 0.0].
-	self pitch: p.
+	self pitch: (self nameOrNumberToPitch: pitchNameOrNumber).
 	self reset.
