406be53699514318dc7db4a53eb3427af1a20fc4
tk <unknown>
1998-07-06 14:26:00 +0000

    no message

--- SketchMorph/instance/change reporting/layoutChanged.cs
+++ SketchMorph/instance/change reporting/layoutChanged.cs
@@ -4,10 +4,10 @@ layoutChanged
 	| unrotatedOrigin |
 	self changed.
 	unrotatedOrigin _ bounds origin - offsetWhenRotated.
-	((rotationDegrees = 0.0 or: [rotationStyle = #none]) and:
-	 [scalePoint = (1.0@1.0)])
+	(rotationStyle == #none and: [scalePoint = (1.0@1.0)])
 		ifTrue: [
-			"zero rotation and scale; use original Form"
+			"zero rotation and scale; use original Form.
+			All normal rotation handled by TransformMorph.rotationDegrees:"
 			rotatedForm _ originalForm.
 			offsetWhenRotated _ 0@0]
 		ifFalse: [self generateRotatedForm].	"changes offsetWhenRotated"
