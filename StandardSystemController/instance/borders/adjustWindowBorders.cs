ba1773e5fc7e9aa328a99b00544c0979aa652e3e
ls <unknown>
1998-07-11 07:42:00 +0000

    no message

--- StandardSystemController/instance/borders/adjustWindowBorders.cs
+++ StandardSystemController/instance/borders/adjustWindowBorders.cs
@@ -8,7 +8,8 @@ adjustWindowBorders
 				[side = #left ifTrue:
 					[view newFrame: [:f | f withLeft: sensor cursorPoint x]].
 				side = #right ifTrue:
-					[view newFrame: [:f | f withRight: sensor cursorPoint x]]]]].
+					[view newFrame: [:f | f withRight: sensor cursorPoint x]]].
+			self interActivityPause]. ].
 	HBorderCursor showWhile:
 		[ [side _ view displayBox sideNearestTo: sensor cursorPoint.
 		  self cursorOnBorder and: [(side = #top) | (side = #bottom)]]
@@ -17,5 +18,5 @@ adjustWindowBorders
 				[side = #top ifTrue:
 					[view newFrame: [:f | f withTop: sensor cursorPoint y]].
 				side = #bottom ifTrue:
-					[view newFrame: [:f | f withBottom: sensor cursorPoint y]].
-		]]]
+					[view newFrame: [:f | f withBottom: sensor cursorPoint y]]].
+		  self interActivityPause]]
