4656b3260ccd94046c109dd40264a50bc99b1521
TPR <unknown>
1998-07-21 18:15:00 +0000

    no message

--- CachedSwikiAction/instance/URL processing/generateRecent.cs
+++ CachedSwikiAction/instance/URL processing/generateRecent.cs
@@ -1,6 +1,8 @@
 generateRecent
 	| file |
-	file _ FileStream fileNamed: (self cacheDirectory),(self name),(ServerAction pathSeparator),'recent.html'.
-	file nextPutAll: (HTMLformatter evalEmbedded: (self fileContents: source, 'recent.html')
-					with: urlmap recentCache).
+	file _ FileStream fileNamed: (self cacheDirectory),(self
+name),(ServerAction pathSeparator),'recent.html'.
+	file nextPutAll: (HTMLformatter evalEmbedded: (self fileContents:
+source, 'recent.html')
+					with: urlmap recent).
 	file close.
