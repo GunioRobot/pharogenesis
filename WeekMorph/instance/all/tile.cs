abf4f7290c8fbc2f575ca467d02159848d856473
LC <unknown>
1998-07-25 22:02:00 +0000

    no message

---
+++ WeekMorph/instance/all/tile.cs
@@ -0,0 +1,10 @@
+tile
+	| onColor offColor |
+	offColor _ Color r: 0.4 g: 0.8 b: 0.6.
+	onColor _ offColor mixed: 1/2 with: Color white.
+	^ SimpleSwitchMorph new
+		offColor: offColor;
+		onColor: onColor;
+		borderWidth: 1;
+		useSquareCorners;
+		extent: 23 @ 19
