a7c61b086883181c8370d081109491035fed60dc
ls <unknown>
1998-07-28 20:35:00 +0000

    no message

---
+++ HtmlEntity/instance/testing/shouldContain-3A-.cs
@@ -0,0 +1,3 @@
+shouldContain: anEntity
+	"whether, according to the HTML DTD, this element should actually contain anEntity.  Used for checking the quality of a pages HTML"
+	^self mayContain: anEntity
