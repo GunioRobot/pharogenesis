3a1ee3ef7c56d200273eb24fca88b0a6fbab7ae4
tk <unknown>
1998-08-02 12:52:00 +0000

    no message

--- SketchMorph/instance/accessing/forwardDirection-3A-.cs
+++ SketchMorph/instance/accessing/forwardDirection-3A-.cs
@@ -1,6 +1,6 @@
 forwardDirection: degrees
 	"Set the forward direction of the original Form. Angles are in degrees, increasing clockwise like a compass. Up is zero degrees."
 
-	(0.0 - degrees) <= 0.0001
+	(0.0 - degrees) abs <= 0.0001
 		ifTrue: [self removeProperty: #forwardDirection]
 		ifFalse: [self setProperty: #forwardDirection toValue: degrees].
