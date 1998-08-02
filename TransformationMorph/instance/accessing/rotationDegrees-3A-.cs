05d09831d11b8fbb566656a7519eef8f76215899
tk <unknown>
1998-08-02 14:37:00 +0000

    no message

--- TransformationMorph/instance/accessing/rotationDegrees-3A-.cs
+++ TransformationMorph/instance/accessing/rotationDegrees-3A-.cs
@@ -1,4 +1,8 @@
 rotationDegrees: degrees
-
+	| m |
 	self adjustAfter:
-		[self angle: degrees degreesToRadians negated].
+		[(m _ self renderedMorph) doesOwnRotation
+			ifTrue: ["is SketchMorph and rotationStyle is not #normal"
+				m rotationDegrees: degrees.
+				"self angle: 0.0 or not flexed"]
+			ifFalse: [self angle: degrees degreesToRadians negated]]	"usual case"
