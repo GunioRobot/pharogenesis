88e537cf9514cfc02ee28961183abf450a09dc3e
sw <unknown>
1998-08-03 16:25:00 +0000

    no message

--- ChangeSet/instance/testing/okayToRemove.cs
+++ ChangeSet/instance/testing/okayToRemove.cs
@@ -1,16 +1,2 @@
 okayToRemove
-	| aName |
-	aName _ self name.
-	self == Smalltalk changes ifTrue:
-		[self inform: 'Cannot remove "', aName, '"
-because it is the 
-current change set.'.
-		^ false].
-
-	self belongsToAProject ifTrue:
-		[self inform: 'Cannot remove "', aName, '" 
-because it belongs to a 
-project.'.
-			^ false].
-
-	^ true
+	^ self okayToRemoveInforming: true
