e176bc68910837f59eefa5f904db3488715b7f15
sw <unknown>
1998-08-05 17:42:00 +0000

    no message

--- ChangedMessageSet/class/as yet unclassified/openFor-3A-.cs
+++ ChangedMessageSet/class/as yet unclassified/openFor-3A-.cs
@@ -1,9 +1,5 @@
 openFor: aChangeSet
 	"Open up a ChangedMessageSet browser on the given change set; this is a conventional message-list browser whose message list is the list of methods in aChangeSet.  After any method submission, the message list is refigured, making it plausibly dynamic.  "
 	| messageSet |
-
-	messageSet _ self messageList: aChangeSet changedMessageListAugmented.
-	messageSet changeSet: aChangeSet.
-	messageSet autoSelectString: nil.
-	ScheduledControllers scheduleActive: 
-					(self open: messageSet name:  'Methods in Change Set ', aChangeSet name)
+	messageSet _ aChangeSet changedMessageListAugmented.
+	self openMessageList: messageSet name: ('Methods in Change Set ', aChangeSet name) autoSelect: nil changeSet: aChangeSet
