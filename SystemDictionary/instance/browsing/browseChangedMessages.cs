3d78d68c7850af10940e10d7ca3ab263f2f14b00
sw <unknown>
1998-08-05 18:41:00 +0000

    no message

--- SystemDictionary/instance/browsing/browseChangedMessages.cs
+++ SystemDictionary/instance/browsing/browseChangedMessages.cs
@@ -2,4 +2,6 @@ browseChangedMessages
 	"Create and schedule a message browser on each method that has been 
 	changed."
 
+	SystemChanges isEmpty ifTrue: [^ self inform: 'There are no changed messages
+in the current change set.'].
 	ChangedMessageSet openFor: SystemChanges
