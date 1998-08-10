f2312eb9dfad286f5bda5818ca98e2dbb861b83d
tk <unknown>
1998-08-10 16:04:00 +0000

    no message

--- Morph/instance/event handling/mouseDown-3A-.cs
+++ Morph/instance/event handling/mouseDown-3A-.cs
@@ -1,5 +1,5 @@
 mouseDown: evt
 	"Handle a mouse down event. The default response is to let my eventHandler, if any, handle it."
 
-	eventHandler ifNotNil:
-		[eventHandler mouseDown: evt fromMorph: self].
+	self eventHandler ifNotNil:
+		[self eventHandler mouseDown: evt fromMorph: self].
