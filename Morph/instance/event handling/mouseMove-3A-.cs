33973a0de6239f2c130eb55558b25caf95d67012
tk <unknown>
1998-08-10 16:02:00 +0000

    no message

--- Morph/instance/event handling/mouseMove-3A-.cs
+++ Morph/instance/event handling/mouseMove-3A-.cs
@@ -1,5 +1,5 @@
 mouseMove: evt
 	"Handle a mouse move event. The default response is to let my eventHandler, if any, handle it."
 
-	eventHandler ifNotNil:
-		[eventHandler mouseStillDown: evt fromMorph: self].
+	self eventHandler ifNotNil:
+		[self eventHandler mouseStillDown: evt fromMorph: self].
