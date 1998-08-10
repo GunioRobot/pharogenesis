b55485f5c81a6d123b87d590b27417f2fa1299e6
tk <unknown>
1998-08-10 16:04:00 +0000

    no message

--- Morph/instance/event handling/on-3A-send-3A-to-3A-withValue-3A-.cs
+++ Morph/instance/event handling/on-3A-send-3A-to-3A-withValue-3A-.cs
@@ -1,4 +1,5 @@
 on: eventName send: selector to: recipient withValue: value
 	"NOTE: selector must take 3 arguments, of which value will be the 3rd"
-	eventHandler ifNil: [eventHandler _ EventHandler new].
-	eventHandler on: eventName send: selector to: recipient withValue: value
+
+	self eventHandler ifNil: [self eventHandler: EventHandler new].
+	self eventHandler on: eventName send: selector to: recipient withValue: value
