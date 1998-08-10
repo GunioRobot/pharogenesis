b44615abef850b334119103c83bd98154f109cd2
tk <unknown>
1998-08-10 16:00:00 +0000

    no message

--- Morph/instance/event handling/mouseEnter-3A-.cs
+++ Morph/instance/event handling/mouseEnter-3A-.cs
@@ -1,5 +1,5 @@
 mouseEnter: evt
 	"Handle a mouseEnter event, meaning the mouse just entered my bounds with no button pressed. The default response is to let my eventHandler, if any, handle it."
 
-	eventHandler ifNotNil:
-		[eventHandler mouseEnter: evt fromMorph: self].
+	self eventHandler ifNotNil:
+		[self eventHandler mouseEnter: evt fromMorph: self].
