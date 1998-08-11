d05ffb6c8a7f67e172debaf145ba05e993de56b7
sw <unknown>
1998-08-11 14:40:00 +0000

    no message

---
+++ ClassDescription/instance/compiling/compileUnlogged-3A-classified-3A-notifying-3A-.cs
@@ -0,0 +1,10 @@
+compileUnlogged: text classified: category notifying: requestor 
+	| selector  |
+	self compile: text asString
+		notifying: requestor
+		trailer: #(0 0 0 0)
+		ifFail: [^ nil]
+		elseSetSelectorAndNode: 
+			[:sel :node | selector _ sel].
+	self organization classify: selector under: category.
+	^ selector
