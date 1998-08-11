2bcab42a14ea7a08a99e4b365a94a5a6b5ea5aef
sw <unknown>
1998-08-11 12:45:00 +0000

    no message

--- Player/instance/scripts-2D-kernel/acceptScript-3A-for-3A-.cs
+++ Player/instance/scripts-2D-kernel/acceptScript-3A-for-3A-.cs
@@ -3,7 +3,7 @@ acceptScript: aScriptEditorMorph for: aSelector
 	| classif |
 	classif _ aScriptEditorMorph isAnonymous ifTrue: ['anonymous scripts'] ifFalse: ['named scripts'].
 	self class
-		compile: aScriptEditorMorph methodString
+		compileUnlogged: aScriptEditorMorph methodString
 		classified: classif
 		notifying: nil.
 	self class atSelector: aSelector putScriptEditor: aScriptEditorMorph
