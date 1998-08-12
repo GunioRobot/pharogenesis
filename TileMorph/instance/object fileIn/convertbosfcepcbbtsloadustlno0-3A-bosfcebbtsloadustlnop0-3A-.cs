d8f2ec63ebe9f23e36418e5666e179cc5948c2a8
tk <unknown>
1998-08-12 10:17:00 +0000

    no message

---
+++ TileMorph/instance/object fileIn/convertbosfcepcbbtsloadustlno0-3A-bosfcebbtsloadustlnop0-3A-.cs
@@ -0,0 +1,7 @@
+convertbosfcepcbbtsloadustlno0: varDict bosfcebbtsloadustlnop0: smartRefStrm
+	"These variables are automatically stored into the new instance ('type' 'slotName' 'literal' 'operatorOrExpression' 'actualObject' 'downArrow' 'upArrow' 'suffixArrow' 'typeColor' 'lastArrowTick' 'nArrowTicks' 'operatorReadoutString' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"Be sure to to fill in ('possessive' ) and deal with the information in ()"
+
+	"Morph's conversion method already deals with this, so no extra code here."
