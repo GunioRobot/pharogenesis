5872a6c3f1c2ff213a7c64614046a2ea33e31a40
tk <unknown>
1998-08-13 13:05:00 +0000

    no message

--- ScrollPane/instance/initialization/fullCopy.cs
+++ ScrollPane/instance/initialization/fullCopy.cs
@@ -1,6 +1,7 @@
 fullCopy
 	| copy |
-	self mouseEnter.          "Make sure scrollBar is in morphic structure"
-	copy _ super fullCopy.  "So that references are updated properly"
-	self mouseLeave.
-	^ copy mouseLeave
+	self mouseEnter: nil.		 "Make sure scrollBar is in morphic structure"
+	copy _ super fullCopy.		"So that references are updated properly"
+		"Will fail of any Players with scripts are in the ScrollPane"
+	self mouseLeave: nil.
+	^ copy mouseLeave: nil
