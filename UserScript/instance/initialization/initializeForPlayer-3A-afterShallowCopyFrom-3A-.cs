fb89d5bd70bb0b0784ed014691c133bf8e61c1af
tk <unknown>
1998-08-13 13:06:00 +0000

    no message

--- UserScript/instance/initialization/initializeForPlayer-3A-afterShallowCopyFrom-3A-.cs
+++ UserScript/instance/initialization/initializeForPlayer-3A-afterShallowCopyFrom-3A-.cs
@@ -4,6 +4,7 @@ initializeForPlayer: aPlayer afterShallowCopyFrom: aDonorUserScript
 	aDonorUserScript isTextuallyCoded
 		ifFalse:
 			[currentScriptEditor _ currentScriptEditor fullCopy.
+				"We have a rule that ScriptEditors can't have Players in them"
 			currentScriptEditor playerScripted: aPlayer.
 			currentScriptEditor donorActor: aDonorUserScript player ownActor: aPlayer]
 	
