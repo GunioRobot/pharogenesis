a1c6c1dce4905845e9c731f49e50c4557da3d8a5
di <unknown>
1998-08-13 10:09:00 +0000

    no message

--- Morph/instance/submorphs-2D-add-2F-remove/delete.cs
+++ Morph/instance/submorphs-2D-add-2F-remove/delete.cs
@@ -1,9 +1,13 @@
 delete
 	"Remove the receiver as a submorph of its owner and make its new owner be nil."
-
 	| aWorld |
 	owner ifNotNil:
-		[costumee ifNotNil: [aWorld _ self world].
-		owner privateRemoveMorph: self.
-		owner _ nil.
-		costumee ifNotNil: [costumee noteDeletionOf: self fromWorld: aWorld]].
+		[(extension == nil or: [self player == nil])
+		ifTrue: [owner privateRemoveMorph: self.
+				owner _ nil]
+		ifFalse: ["Player must be notified"
+				aWorld _ self world.
+				owner privateRemoveMorph: self.
+				owner _ nil.
+				self player noteDeletionOf: self fromWorld: aWorld]
+		].
