2dd32e70edea45d0454ed00c422b552c4708e8a2
jm <unknown>
1998-08-13 15:09:00 +0000

    no message

--- AbstractSound/instance/playing/playAndWaitUntilDone.cs
+++ AbstractSound/instance/playing/playAndWaitUntilDone.cs
@@ -1,5 +1,6 @@
 playAndWaitUntilDone
-	"Play this sound to the sound ouput port in real time."
+	"Play this sound to the sound ouput port and wait until it has finished playing before returning."
 
 	SoundPlayer playSound: self.
 	[self samplesRemaining > 0] whileTrue.
+	(Delay forMilliseconds: 2 * SoundPlayer bufferMSecs) wait.  "ensure last buffer has been output"
