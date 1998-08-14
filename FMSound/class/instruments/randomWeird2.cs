c154750204ab01b26b342f65a3c858ca977c6a5e
jm <unknown>
1998-08-14 12:57:00 +0000

    no message

--- FMSound/class/instruments/randomWeird2.cs
+++ FMSound/class/instruments/randomWeird2.cs
@@ -1,17 +1,8 @@
 randomWeird2
 	"FMSound randomWeird2 play"
 
-	| p snd env |
-	p _ 220.0 + 4000 atRandom.
-	snd _ FMSound pitch: p dur: 5.0 loudness: 0.5.
-
-	env _ VolumeEnvelope exponentialDecay: 0.96.
-	env target: snd; scale: 0.5.
-	snd addEnvelope: env.
-
-	env _ Envelope exponentialDecay: 0.98.
-	env target: snd; updateSelector: #pitch:; scale: p.
-	snd addEnvelope: env.
-
-	snd duration: 5.0.
-	^ snd
+	| snd |
+	snd _ FMSound new.
+	snd addEnvelope: (VolumeEnvelope exponentialDecay: 0.96).
+	snd addEnvelope: (PitchEnvelope exponentialDecay: 0.98).
+	^ snd setPitch: (150 + 2000 atRandom) dur: 2.0 loudness: 0.5
