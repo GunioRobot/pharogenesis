0fd9f34d4ec7da8d1e2b5224ab4376cdd2aff5d8
jm <unknown>
1998-08-14 13:02:00 +0000

    no message

--- FMSound/class/instruments/flute1.cs
+++ FMSound/class/instruments/flute1.cs
@@ -2,15 +2,9 @@ flute1
 	"FMSound flute1 play"
 	"(FMSound majorScaleOn: FMSound flute1) play"
 
-	| snd p env |
-	snd _ FMSound pitch: 220 dur: 1 loudness: 0.8.
-	snd modulation: 0 multiplier: 0.
-
+	| snd p |
+	snd _ FMSound new.
 	p _ OrderedCollection new.
 	p add: 0@0; add: 20@1.0; add: 100@1.0; add: 120@0.0.
-	env _ VolumeEnvelope points: p loopStart: 2 loopEnd: 3.
-	env target: snd; scale: 0.1.
-	snd addEnvelope: env.
-
-	snd duration: 1.0.
-	^ snd
+	snd addEnvelope: (VolumeEnvelope points: p loopStart: 2 loopEnd: 3).
+	^ snd setPitch: 440.0 dur: 1.0 loudness: 0.5
