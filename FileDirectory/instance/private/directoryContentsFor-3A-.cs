09c70f45909a4ea3d5cb0beeba4fd469bd487dd4
jm <unknown>
1998-08-14 16:44:00 +0000

    no message

--- FileDirectory/instance/private/directoryContentsFor-3A-.cs
+++ FileDirectory/instance/private/directoryContentsFor-3A-.cs
@@ -2,15 +2,16 @@ directoryContentsFor: fullPath
 	"Return a collection of directory entries for the files and directories in the directory with the given path. See primLookupEntryIn:index: for further details."
 	"FileDirectory default directoryContentsFor: ''"
 
-	| entries index done entry |
+	| entries index done entryArray |
 	entries _ OrderedCollection new: 200.
 	index _ 1.
 	done _ false.
 	[done] whileFalse: [
-		entry _ self primLookupEntryIn: fullPath index: index.
-		entry == nil
+		entryArray _ self primLookupEntryIn: fullPath index: index.
+		#badDirectoryPath = entryArray ifTrue: [^ OrderedCollection new].
+		entryArray == nil
 			ifTrue: [done _ true]
-			ifFalse: [entries addLast: entry].
+			ifFalse: [entries addLast: (DirectoryEntry fromArray: entryArray)].
 		index _ index + 1].
 
 	^ entries asArray
