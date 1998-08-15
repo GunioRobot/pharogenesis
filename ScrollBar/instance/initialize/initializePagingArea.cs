600e39020d858e0662a4ea0310e1dc65baa693a9
di <unknown>
1998-08-15 23:43:00 +0000

    no message

--- ScrollBar/instance/initialize/initializePagingArea.cs
+++ ScrollBar/instance/initialize/initializePagingArea.cs
@@ -2,5 +2,6 @@ initializePagingArea
 	pagingArea := RectangleMorph newBounds: self totalSliderArea
 								color: (Color r: 0.6 g: 0.6 b: 0.8).
 	pagingArea borderWidth: 0.
-	pagingArea on: #mouseDown send: #nextPage: to: self.
+	pagingArea on: #mouseDown send: #resetTimer to: self.
+	pagingArea on: #mouseStillDown send: #scrollByPage: to: self.
 	self addMorph: pagingArea
