c6d52c16a68c6ece98aa28c26772ba4cfc4a82ad
di <unknown>
1998-08-16 09:46:00 +0000

    no message

--- FileList/instance/private/put-3A-.cs
+++ FileList/instance/private/put-3A-.cs
@@ -1,17 +1,21 @@
 put: aText
+	| ff type |
+	brevityState == #fullFile ifTrue:
+		[ff _ directory newFileNamed: self fullName.
+		Cursor write showWhile: [ff nextPutAll: aText asString; close].
+		fileName = ff localName 
+			ifTrue: [contents _ aText asString]
+			ifFalse: [self updateFileList].		"user renamed the file"
+		^ true  "accepted"].
 
-	| ff |
-	listIndex = 0 ifTrue: [^ self].
-	((aText size >= 5) and:
-	 [#('File ' '16r0 ') includes: (aText copyFrom: 1 to: 5)])
-		ifTrue: [
-			(self confirm:
-'Abbreviated and hexadecimal file views
-cannot be meaningfully saved at present.
-Is this REALLY what you want to do?') ifFalse: [^ false]].
-	ff _ directory newFileNamed: self fullName.
-	Cursor write showWhile: [ff nextPutAll: aText asString; close].
-	fileName = ff localName 
-		ifTrue: [contents _ aText asString]
-		ifFalse: [self updateFileList].		"user renamed the file"
-	^ true  "accepted"
+	listIndex = 0 ifTrue:
+		[PopUpMenu notify: 'No fileName is selected'.
+		^ false  "failed"].
+	type _ 'These'.
+	brevityState = #briefFile ifTrue: [type _ 'Abbreviated'].
+	brevityState = #briefHex ifTrue: [type _ 'Abbreviated'].
+	brevityState = #fullHex ifTrue: [type _ 'Hexadecimal'].
+	brevityState = #FileList ifTrue: [type _ 'Directory'].
+	PopUpMenu notify: type , ' contents cannot
+meaningfully be saved at present.'.
+	^ false  "failed"
