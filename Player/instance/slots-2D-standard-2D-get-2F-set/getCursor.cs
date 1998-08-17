9fb2cc4fec822e6816a5a7235682d34ef972fafe
sw <unknown>
1998-08-17 15:41:00 +0000

    no message

--- Player/instance/slots-2D-standard-2D-get-2F-set/getCursor.cs
+++ Player/instance/slots-2D-standard-2D-get-2F-set/getCursor.cs
@@ -1,4 +1,2 @@
 getCursor
-	| aMorph |
-	((aMorph _ costume renderedMorph) respondsTo: #cursor) ifTrue: [^ aMorph cursor].
-	^ (self costumeNamed: #PasteUpMorph) cursor
+	^ self getValueFromCostume: #cursor
