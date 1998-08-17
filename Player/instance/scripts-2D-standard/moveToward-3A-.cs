d8dbcad9c2d913cf7224406e91f39e55d0de14c4
sw <unknown>
1998-08-17 17:24:00 +0000

    no message

--- Player/instance/scripts-2D-standard/moveToward-3A-.cs
+++ Player/instance/scripts-2D-standard/moveToward-3A-.cs
@@ -1,8 +1,8 @@
 moveToward: aPlayer
 
-	| angle |
+	| angle aCostume |
 	(aPlayer == nil or: [aPlayer == self]) ifTrue: [^ self].
-	(costume bounds intersects: aPlayer costume bounds) ifTrue: [^ self].
-	angle _ costume referencePosition bearingToPoint: aPlayer costume referencePosition.
+	((aCostume _ self costume) bounds intersects: aPlayer costume bounds) ifTrue: [^ self].
+	angle _ aCostume referencePosition bearingToPoint: aPlayer costume referencePosition.
 	self setHeading: angle.
 	self forward: self getSpeed
