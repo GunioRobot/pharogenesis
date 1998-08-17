6d00c2a99f66bf929e57ae95b3853bbefc1f3f5f
sw <unknown>
1998-08-17 15:41:00 +0000

    no message

--- Player/instance/slots-2D-standard-2D-get-2F-set/getBorderWidth.cs
+++ Player/instance/slots-2D-standard-2D-get-2F-set/getBorderWidth.cs
@@ -1,6 +1,2 @@
 getBorderWidth
-	| renderedMorph |
-	((renderedMorph _ costume renderedMorph) respondsTo: #borderWidth) ifTrue: [^ renderedMorph borderWidth].
-	self costumesDo: [:aCostume | (aCostume respondsTo: #borderWidth) ifTrue: [^ aCostume borderWidth]].
-	"No instantiated costume bears a border width.  Very unlikely"
-	^ (self costumeNamed: #RectangleMorph) borderWidth
+	^ self getValueFromCostume: #borderWidth
