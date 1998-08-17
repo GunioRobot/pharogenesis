57f88aa431809d034f13c8ea5fd50a453732affa
sw <unknown>
1998-08-17 15:41:00 +0000

    no message

--- Player/instance/slots-2D-standard-2D-get-2F-set/getBorderColor.cs
+++ Player/instance/slots-2D-standard-2D-get-2F-set/getBorderColor.cs
@@ -1,7 +1,2 @@
 getBorderColor
-	| renderedMorph |
-	((renderedMorph _ costume renderedMorph) respondsTo: #borderColor) ifTrue: [^ renderedMorph borderColor].
-
-	self costumesDo: [:aCostume | (aCostume respondsTo: #borderColor) ifTrue: [^ aCostume borderColor]].
-	"No instantiated costume bears a border color.  Very unlikely"
-	^ (self costumeNamed: #RectangleMorph) borderColor
+	^ self getValueFromCostume: #borderColor
