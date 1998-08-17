09a169099d075bfa3bd5df7bd168c608877b3e73
sw <unknown>
1998-08-17 17:22:00 +0000

    no message

--- Player/instance/scripts-2D-standard/bounce-3A-.cs
+++ Player/instance/scripts-2D-standard/bounce-3A-.cs
@@ -1,12 +1,12 @@
 bounce: soundName
 
-	| box bounced |
-	costume ifNil: [^ self].
-	(costume owner == nil or: [costume owner isHandMorph]) ifTrue: [^ self].
-	box _ costume owner bounds.
+	| box bounced aCostume |
+	(aCostume _ self costume) ifNil: [^ self].
+	(aCostume owner == nil or: [aCostume owner isHandMorph]) ifTrue: [^ self].
+	box _ aCostume owner bounds.
 	bounced _ false.
-	(costume left < box left)			ifTrue: [self headRight. bounced _ true].
-	(costume right > box right)		ifTrue: [self headLeft. bounced _ true].
-	(costume top < box top)			ifTrue: [self headDown. bounced _ true].
-	(costume bottom > box bottom)	ifTrue: [self headUp. bounced _ true].
+	(aCostume left < box left)			ifTrue: [self headRight. bounced _ true].
+	(aCostume right > box right)		ifTrue: [self headLeft. bounced _ true].
+	(aCostume top < box top)			ifTrue: [self headDown. bounced _ true].
+	(aCostume bottom > box bottom)	ifTrue: [self headUp. bounced _ true].
 	bounced ifTrue: [^ self makeBounceSound: soundName].
