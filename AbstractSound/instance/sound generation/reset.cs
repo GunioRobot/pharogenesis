c7b9474576228a123bea52d99b33c7d852cf41d7
jm <unknown>
1998-08-17 13:45:00 +0000

    no message

--- AbstractSound/instance/sound generation/reset.cs
+++ AbstractSound/instance/sound generation/reset.cs
@@ -2,6 +2,6 @@ reset
 	"Reset my internal state for a replay. Methods that override this method should do super reset."
 
 	mSecsSinceStart _ 0.
-	samplesUntilNextControl _ self samplingRate // self controlRate.
+	samplesUntilNextControl _ 0.
 	envelopes size > 0 ifTrue: [
 		1 to: envelopes size do: [:i | (envelopes at: i) reset]].
