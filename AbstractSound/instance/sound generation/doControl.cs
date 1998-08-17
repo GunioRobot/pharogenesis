7f3272f1c5ddfeb610b9b3eb1475b206980cc709
jm <unknown>
1998-08-17 13:34:00 +0000

    no message

--- AbstractSound/instance/sound generation/doControl.cs
+++ AbstractSound/instance/sound generation/doControl.cs
@@ -3,10 +3,10 @@ doControl
 	"Note: This is only called at a small fraction of the sampling rate."
 
 	| pitchModOrRatioChange |
-	mSecsSinceStart _ mSecsSinceStart + (1000 // self controlRate).
 	envelopes size > 0 ifTrue: [
 		pitchModOrRatioChange _ false.
 		1 to: envelopes size do: [:i |
 			((envelopes at: i) updateTargetAt: mSecsSinceStart)
 				ifTrue: [pitchModOrRatioChange _ true]].
 		pitchModOrRatioChange ifTrue: [self internalizeModulationAndRatio]].
+	mSecsSinceStart _ mSecsSinceStart + (1000 // self controlRate).
