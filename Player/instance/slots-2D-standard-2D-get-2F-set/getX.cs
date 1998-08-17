591a67c86ac517c12cb2b28368be34c941c45014
sw <unknown>
1998-08-17 17:23:00 +0000

    no message

--- Player/instance/slots-2D-standard-2D-get-2F-set/getX.cs
+++ Player/instance/slots-2D-standard-2D-get-2F-set/getX.cs
@@ -1,5 +1,6 @@
 getX
 		"emergency patch; unclear why not needed in getY; in any case, have
 		 removed the getX/getY retrievals from the viewer in 2.0-final anyway"
-	costume isInWorld ifFalse: [^ 100].
-	^ costume x
+	| aCostume |
+	(aCostume _ self costume) isInWorld ifFalse: [^ 100].
+	^ aCostume x
