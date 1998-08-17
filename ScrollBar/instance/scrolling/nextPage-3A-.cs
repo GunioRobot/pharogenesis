4d03c7b253d423948ba1c10a38479df5e8b35492
di <unknown>
1998-08-17 09:39:00 +0000

    no message

--- ScrollBar/instance/scrolling/nextPage-3A-.cs
+++ ScrollBar/instance/scrolling/nextPage-3A-.cs
@@ -1,4 +1,6 @@
 nextPage: event
-	event cursorPoint >= slider topLeft
-		ifTrue: [self setValue: value + pageDelta]
-		ifFalse: [self setValue: value - pageDelta]
+	"Note: this method is no longer called, except by old scrollBars"
+	(self waitForDelay1: 300 delay2: 100) ifFalse: [^ self].
+	event cursorPoint y >= slider center y
+		ifTrue: [self setValue: (value + pageDelta min: 1.0)]
+		ifFalse: [self setValue: (value - pageDelta max: 0.0)]
