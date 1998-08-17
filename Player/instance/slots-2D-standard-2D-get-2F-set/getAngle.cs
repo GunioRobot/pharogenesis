4a567559409a68fd8b666eaf4638660ec9165cc1
sw <unknown>
1998-08-17 15:40:00 +0000

    no message

--- Player/instance/slots-2D-standard-2D-get-2F-set/getAngle.cs
+++ Player/instance/slots-2D-standard-2D-get-2F-set/getAngle.cs
@@ -1,5 +1,2 @@
 getAngle
-	| renderedMorph |
-	((renderedMorph _ costume renderedMorph) respondsTo: #angle) ifTrue: [^ renderedMorph angle].
-
-	^ (self costumeNamed: #JoystickMorph) angle
+	^ self getValueFromCostume: #angle
