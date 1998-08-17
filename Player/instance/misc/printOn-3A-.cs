e1a8e846c8b4515090599ed7d8927586e7fbc389
sw <unknown>
1998-08-17 17:14:00 +0000

    no message

--- Player/instance/misc/printOn-3A-.cs
+++ Player/instance/misc/printOn-3A-.cs
@@ -1,5 +1,5 @@
 printOn: aStream
 	super printOn: aStream.
 	aStream nextPutAll: (' (', self asOop printString, ')').
-	costume ifNil: [aStream nextPutAll: ' (with nil costume)'.  ^ self].
+	self costume ifNil: [aStream nextPutAll: ' (with nil costume)'.  ^ self].
 	aStream nextPutAll: ' named ', self externalName
