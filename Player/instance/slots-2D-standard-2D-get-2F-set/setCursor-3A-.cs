c637d899592c977b2caa7843f277be02bae538a5
sw <unknown>
1998-08-17 15:47:00 +0000

    no message

--- Player/instance/slots-2D-standard-2D-get-2F-set/setCursor-3A-.cs
+++ Player/instance/slots-2D-standard-2D-get-2F-set/setCursor-3A-.cs
@@ -1,4 +1,2 @@
 setCursor: aNumber
-	| aMorph |
-	((aMorph _ costume renderedMorph) respondsTo: #cursor:) ifTrue: [^ aMorph cursor: aNumber].
-	^ (self costumeNamed: #PasteUpMorph) cursor: aNumber
+	self setCostumeSlot: #cursor: toValue: aNumber
