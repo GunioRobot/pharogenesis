bd204742d8494f6ce6f9207e4c39a38d433c4ced
jm <unknown>
1998-08-17 20:35:00 +0000

    no message

--- RecordingControlsMorph/instance/button commands/show.cs
+++ RecordingControlsMorph/instance/button commands/show.cs
@@ -1,20 +1,13 @@
 show
-	"Show my samples in a GraphMorph."
+	"Show my samples in a WaveEditor."
 
-	| g m |
+	| ed w |
 	recorder pause.
-	g _ GraphMorph new extent: 500@150; data: recorder condensedSamples.
+	ed _ WaveEditor new.
+	ed data: recorder condensedSamples.
+	ed samplingRate: recorder samplingRate.
+	w _ self world.
+	w activeHand
+		ifNil: [w addMorph: ed]
+		ifNotNil: [w activeHand attachMorph: ed].
 
-	m _ StringMorph new contents: 'Index:'.
-	g addMorph: (m position: 5@5).
-	m _ UpdatingStringMorph new
-		target: g; getSelector: #cursor; putSelector: #cursor:; step.
-	g addMorph: (m position: 45@5).
-
-	m _ StringMorph new contents: 'Value:'.
-	g addMorph: (m position: 5@20).
-	m _ UpdatingStringMorph new
-		target: g; getSelector: #valueAtCursor; putSelector: #valueAtCursor:; step.
-	g addMorph: (m position: 45@20).
-
-	self world hands first attachMorph: g.
