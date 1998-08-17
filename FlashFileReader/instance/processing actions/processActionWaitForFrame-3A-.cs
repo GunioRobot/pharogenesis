66ea1c89de564b84cea215379de1fac956198bc8
ar <unknown>
1998-08-17 10:10:00 +0000

    no message

---
+++ FlashFileReader/instance/processing actions/processActionWaitForFrame-3A-.cs
@@ -0,0 +1,13 @@
+processActionWaitForFrame: data
+	| length frame skip |
+	length _ data nextWord.
+	length = 3 ifFalse:["Something is wrong"
+		self halt.
+		data skip: -2.
+		^self processUnknownAction: data].
+	frame _ data nextWord.
+	skip _ data nextByte.
+	log ifNotNil:[
+		log nextPutAll:'frame = '; print: frame;
+			nextPutAll:', skip = '; print: skip].
+	^Message selector: #isFrameLoaded:elseSkip: arguments: (Array with: frame with: skip).
