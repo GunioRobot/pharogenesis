f4dfe3a1625d01b12a23efcbd4e4bda1069f9b6a
sw <unknown>
1998-08-17 15:46:00 +0000

    no message

--- Player/instance/slots-2D-standard-2D-get-2F-set/setBorderColor-3A-.cs
+++ Player/instance/slots-2D-standard-2D-get-2F-set/setBorderColor-3A-.cs
@@ -1,9 +1,2 @@
 setBorderColor: aColor
-	| renderedMorph |
-	((renderedMorph _ costume renderedMorph) respondsTo: #borderColor:) ifTrue: [^ renderedMorph borderColor: aColor].
-
-	"If doing invisibly, do it equally to any hidden costume that cares"
-	self costumesDo: [:aCostume | ((renderedMorph _ aCostume renderedMorph) respondsTo: #borderColor:) ifTrue: [renderedMorph borderColor: aColor]].
-
-	"No instantiated costume bears a border color.  Very unlikely"
-	^ (self costumeNamed: #RectangleMorph) borderColor: aColor
+	self setCostumeSlot: #borderColor: toValue: aColor
