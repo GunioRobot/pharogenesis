d363db01b1c529665bc86da85282507ed3000f73
jm <unknown>
1998-08-18 07:41:00 +0000

    no message

---
+++ LoopedSampledSound/class/instance creation/unloopedSamples-3A-pitch-3A-samplingRate-3A-.cs
@@ -0,0 +1,7 @@
+unloopedSamples: aSoundBuffer pitch: perceivedPitchInHz samplingRate: samplingRateInHz
+	"See the comment in the instance method of this name."
+
+	^ self basicNew
+		unloopedSamples: aSoundBuffer
+		pitch: perceivedPitchInHz
+		samplingRate: samplingRateInHz
