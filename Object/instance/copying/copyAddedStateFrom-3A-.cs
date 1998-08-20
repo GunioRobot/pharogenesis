219ceaa1c1f4a7d89a3a5173446c48e5187833e3
tk <unknown>
1998-08-20 16:01:00 +0000

    no message

--- Object/instance/copying/copyAddedStateFrom-3A-.cs
+++ Object/instance/copying/copyAddedStateFrom-3A-.cs
@@ -1,5 +1,5 @@
 copyAddedStateFrom: anotherObject
-	"Copy the values of instance variables added by the receiver's class from anotherObject to the receiver"
+	"Copy over the values of instance variables added by the receiver's class from anotherObject to the receiver.  These will be remapped in mapUniClasses, if needed."
 
 	self class superclass instSize + 1 to: self class instSize do:
 		[:index | self instVarAt: index put: (anotherObject instVarAt: index)]
