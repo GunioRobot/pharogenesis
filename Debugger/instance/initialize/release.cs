69255be9cae2675707cb8f2ffb5b4d4cc2fd1638
jm <unknown>
1998-08-20 18:31:00 +0000

    no message

--- Debugger/instance/initialize/release.cs
+++ Debugger/instance/initialize/release.cs
@@ -1,11 +1,4 @@
 release
 
-	interruptedProcess ~~ nil ifTrue: [interruptedProcess terminate].
-	interruptedProcess _ nil.
-	interruptedController _ nil.
-	contextStack _ nil.
-	contextStackTop _ nil.
-	receiverInspector _ nil.
-	contextVariablesInspector _ nil.
-	Smalltalk installLowSpaceWatcher.  "restart low space handler"
+	self windowIsClosing.
 	super release.
