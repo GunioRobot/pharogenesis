e936bd100d1f8a3d8a87b25078096eefc4b0e456
sw <unknown>
1998-08-20 17:35:00 +0000

    no message

---
+++ Player/instance/costume/rememberCostume-3A-.cs
@@ -0,0 +1,13 @@
+rememberCostume: aCostume
+	"Put aCostume in my remembered-costumes list, as the final element"
+	| costumeToRemember existing |
+	costumeToRemember _ aCostume renderedMorph.
+		"Remember real morphs, not their transformations"
+	costumes ifNil: [costumes _ OrderedCollection new].
+	existing _ (costumeToRemember isKindOf: SketchMorph)
+		ifTrue:
+			[self knownSketchCostumeWithSameFormAs: costumeToRemember]
+		ifFalse:
+			[costumes detect: [:c | c == costumeToRemember] ifNone: [nil]].
+	costumes _ costumes copyWithout: existing.
+	costumes addLast: costumeToRemember
