924d70c75b986e07d62b90688fc431d659d53584
sw <unknown>
1998-08-21 17:19:00 +0000

    no message

--- Player/instance/costume/costumesDo-3A-.cs
+++ Player/instance/costume/costumesDo-3A-.cs
@@ -1,2 +1,6 @@
 costumesDo: aBlock
-	self class costumesDo: aBlock
+	"Evaluate aBlock against every real (not flex) costume known to the receiver)"
+	aBlock value: costume renderedMorph.
+	costumes ifNotNil:
+		[(costumes copyWithout: costume) do:
+			[:aCostume | aBlock value: aCostume]]
