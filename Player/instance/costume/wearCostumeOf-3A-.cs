420cc61627b174b9b4a9892c58d7bba8d02f08fa
sw <unknown>
1998-08-22 21:51:00 +0000

    no message

--- Player/instance/costume/wearCostumeOf-3A-.cs
+++ Player/instance/costume/wearCostumeOf-3A-.cs
@@ -1,3 +1,13 @@
 wearCostumeOf: anotherPlayer
 	"Put on a costume similar to the one currently worn by anotherPlayer"
-	self costume: anotherPlayer costume fullCopy
+	| itsCostume |
+
+	itsCostume _ anotherPlayer costume renderedMorph.
+	(itsCostume isKindOf: SketchMorph)
+		ifTrue:
+			[self wearSketchCostumeResembling: itsCostume]
+		ifFalse:
+			[anotherPlayer costume player: nil.
+			self renderedCostume: itsCostume veryDeepCopy.
+			anotherPlayer costume player: anotherPlayer].
+	costume layoutChanged
