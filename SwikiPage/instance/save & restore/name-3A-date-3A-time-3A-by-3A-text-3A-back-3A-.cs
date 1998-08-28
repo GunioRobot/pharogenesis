10dd0cef1325013e2b58baf47aca563bf330a5e1
mjg <unknown>
1998-08-28 19:43:00 +0000

    no message

--- SwikiPage/instance/save & restore/name-3A-date-3A-time-3A-by-3A-text-3A-back-3A-.cs
+++ SwikiPage/instance/save & restore/name-3A-date-3A-time-3A-by-3A-text-3A-back-3A-.cs
@@ -1,5 +1,10 @@
-name: pageName date: dateString time: timeString by: who text: theString back: bytes
-	"Record the name of this page during startup.  When reading the page in order to serve it, this is NOT executed.  The text is read from the file explicitly."
-	
+name: pageName date: dateString time: timeString by: who text: theString
+back: bytes
+	"Record the name of this page during startup.  When reading the
+page in order to serve it, this is NOT executed.  The text is read from the
+file explicitly."
+
 	name _ pageName.
 	date _ dateString asDate.
+	time _ timeString asTime.
+	address _ who
