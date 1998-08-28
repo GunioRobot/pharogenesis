e1c0c9f54ccd890ac88e0f9c29d11fd7ad9801aa
mjg <unknown>
1998-08-28 20:59:00 +0000

    no message

--- SwikiAction/instance/URL processing/browse-3A-from-3A-.cs
+++ SwikiAction/instance/URL processing/browse-3A-from-3A-.cs
@@ -1,15 +1,19 @@
 browse: pageRef from: request
 	"Just reply with a page in HTML format"
 
-	| formattedPage |
+	| formattedPage htmlForUser |
 	formattedPage _ pageRef copy.
 
 	"Make a copy, then format the text."
-	formattedPage formatted: (HTMLformatter swikify: pageRef text
+	formattedPage formatted: (formatter swikify: pageRef text
 			linkhandler: [:link | urlmap
 					linkFor: link
 					from: request peerName
-					storingTo: OrderedCollection new]).
-		
+					storingTo: OrderedCollection new
+					page: formattedPage]).
+
 	"format using the cached formatter"
-	request reply: ((self formatterFor: 'page') format: formattedPage). 
+	htmlForUser _ ((self formatterFor: 'page') format: formattedPage).
+	htmlForUser size = 0 ifTrue: [self error: 'template file
+''page.html'' not found'].
+	request reply: htmlForUser.
