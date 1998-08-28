8cd6be3cc587f612b825b5005bbb84e7c419be8a
mjg <unknown>
1998-08-28 20:36:00 +0000

    no message

--- SwikiPage/instance/save & restore/backupAChunk-3A-.cs
+++ SwikiPage/instance/save & restore/backupAChunk-3A-.cs
@@ -1,9 +1,19 @@
 backupAChunk: aFile
-	"We are positioned at the end of a chunk.  Read the size of this chunk from its end.  Back up one chunk.  Return the position of the space before the back number."
+	"We are positioned at the end of a chunk.  Read the size of this
+chunk from its end.  Back up one chunk.  Return the position of the space
+before the back number."
 
-	| back pp |
+	| back pp aPiece isDigitFlag |
 	aFile skip: -2.
-	[aFile next isDigit] whileFalse: [aFile skip: -2].
+	isDigitFlag _ false.
+	[isDigitFlag] whileFalse:
+	[aPiece := aFile next. aPiece isNil ifTrue: [^0].
+	aPiece isDigit ifFalse: [aFile skip: -2] ifTrue: [isDigitFlag _
+true].].
+	"[aFile next isDigit] whileFalse: [aFile skip: -2]." "This was
+Ted's elegant version.
+									The
+uglier one above is to be able to test backing up too far."
 	aFile skip: -2.
 	[pp _ aFile position + 1. aFile next isDigit] whileTrue: [
 		aFile skip: -2].
