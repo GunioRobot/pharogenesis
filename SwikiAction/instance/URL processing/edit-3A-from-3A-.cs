d001250028026a06027353fc71d12598b8471802
mjg <unknown>
1998-08-31 15:32:00 +0000

    no message

--- SwikiAction/instance/URL processing/edit-3A-from-3A-.cs
+++ SwikiAction/instance/URL processing/edit-3A-from-3A-.cs
@@ -1,6 +1,7 @@
 edit: pageRef from: request
-	request reply: (HTMLformatter evalEmbedded: 
-							(self fileContents: source , 'edit.html')
+	request reply: (HTMLformatter evalEmbedded:
+							(self fileContents:
+source , 'edit.html')
 						with: pageRef).
 	pageRef noteEditRequest.
 	^ self
