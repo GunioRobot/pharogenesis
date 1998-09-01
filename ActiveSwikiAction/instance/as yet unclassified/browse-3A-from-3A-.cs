4b03aec134e0aecc01c56bf15b65ec8570a32ecc
mjg <unknown>
1998-09-01 12:44:00 +0000

    no message

--- ActiveSwikiAction/instance/as yet unclassified/browse-3A-from-3A-.cs
+++ ActiveSwikiAction/instance/as yet unclassified/browse-3A-from-3A-.cs
@@ -6,9 +6,10 @@ browse: pageRef from: request
 		with: request unlessContains: (self dangerSet).
 	formattedPage _ pageRef copy.
 	"Make a copy, then format the text."
-	formattedPage formatted: (HTMLformatter swikify: liveText
+	formattedPage formatted: (formatter swikify: liveText
 			linkhandler: [:link | urlmap
 					linkFor: link
 					from: request peerName
-					storingTo: OrderedCollection new]).
+					storingTo: OrderedCollection new
+					page: formattedPage]).
 	request reply: ((self formatterFor: 'page') format: formattedPage).
