d3eba9367fff9d1527f30ed2d39df806d59b3fe3
mjg <unknown>
1998-09-01 12:41:00 +0000

    no message

--- URLmap/instance/linking/linkFor-3A-from-3A-storingTo-3A-.cs
+++ URLmap/instance/linking/linkFor-3A-from-3A-storingTo-3A-.cs
@@ -1,17 +1,24 @@
 linkFor: string from: peer storingTo: aList
 	| uString newpage |
 	uString _ string asUppercase.
-	(((uString indexOfSubCollection: 'HTTP' startingAt: 1) = 1) or:
-	[((uString indexOfSubCollection: 'FTP' startingAt: 1) = 1) or:
-	[((uString indexOfSubCollection: 'MAILTO' startingAt: 1) = 1)]])
-	ifTrue:
-		[((uString endsWith: 'GIF') or: [(uString endsWith: 'JPEG') or:
-			[uString endsWith: 'JPG']])
-		ifTrue: [^'<image src="',string,'">']
-		ifFalse: [^'<a href="',string,'">',string,'</a>']]
-	ifFalse: "Serious! Gotta provide-a-link!"
-		[newpage _ pages at: string asLowercase ifAbsent: [nil].
-		newpage isNil ifTrue: [ "Create a new page"
-			newpage _ self newpage: string from: peer.].
-		(aList indexOf: newpage) ~= 0 ifFalse: [aList add: newpage]. "Add only if not there"
-		^self pageURL: newpage]
+	(self isStringRooted: uString)
+		ifTrue: ["an external URL"
+			(self isStringAnImage: uString)
+				ifTrue: ["Looks like an image URL"
+						^self imageURL: string]
+				ifFalse: [^self externalURL: string]]
+		ifFalse: [ "Serious! Gotta provide-a-link! But check for
+image or local action first"
+				(self isStringAnImage: uString)
+					ifTrue: [^self localImageURL: string].
+				(self isStringALocalAction: uString)
+					ifTrue:[^self localActionURL: string].
+			newpage _ pages at: string asLowercase ifAbsent: [nil].
+			newpage isNil
+				ifTrue: [ "Create a new page"
+					newpage _ self newpage: string
+from: peer.].
+			(aList indexOf: newpage) ~= 0
+				ifFalse: [aList add: newpage]. "Add only if
+not there"
+			^self pageURL: newpage]
