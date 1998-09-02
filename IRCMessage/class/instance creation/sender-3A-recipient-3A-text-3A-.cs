c5699338043d74300ee05fe20c13c4f422986bc0
ls <unknown>
1998-09-02 17:15:00 +0000

    no message

---
+++ IRCMessage/class/instance creation/sender-3A-recipient-3A-text-3A-.cs
@@ -0,0 +1,3 @@
+sender: sender  recipient: recipient  text: text
+	"create a new message with the given sender, recipient, and text"
+	^super new initializeSender: sender  recipient: recipient text: text
