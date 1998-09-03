0caed3618bf82fe678dc9f046d424cf1e19dfa09
di <unknown>
1998-09-03 09:19:00 +0000

    no message

--- PasteUpMorph/instance/misc/prepareToBeSaved.cs
+++ PasteUpMorph/instance/misc/prepareToBeSaved.cs
@@ -1,7 +1,4 @@
 prepareToBeSaved
 
 	super prepareToBeSaved.
-	self world ifNotNil:
-		[self setProperty: #worldSize toValue: self world extent].
-	turtlePen _ nil.
-	lastTurtlePositions _ IdentityDictionary new.
+	turtlePen _ nil
