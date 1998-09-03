24f83c2d4c6e2ebb0fffe1b14eb74dd470533494
ls <unknown>
1998-09-03 20:42:00 +0000

    no message

---
+++ IRCMorph/class/initialization/initialize.cs
@@ -0,0 +1,7 @@
+initialize 
+	"set all the defaults to something reasonable"
+	DefaultServer _ 'us.chatnet.org'.
+	DefaultPort _ 6667.
+	DefaultNick _ 'nick'.
+	DefaultUserName _ 'username'.
+	DefaultFullName _ 'J. Doe <jdoe@somewhere>'.
