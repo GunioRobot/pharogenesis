bbf8307ac6ee1a8d42fa9dceca7acd508000c8d6
BJP <unknown>
1998-09-03 13:55:00 +0000

    no message

---
+++ SwikiPage/instance/accessing/pageStatus-3A-.cs
@@ -0,0 +1,6 @@
+pageStatus: aSymbol
+	"While I'm trying to be careful about default behavior if the type is
+unknown, one should still be careful to check all the places that pageType
+is used."
+
+	pageStatus _ aSymbol.
