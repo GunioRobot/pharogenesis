ca7e3e192903ab71ae3352347dd19f792c660dc2
di <unknown>
1998-09-04 16:59:00 +0000

    no message

--- EnvelopeEditorMorph/instance/construction/addControls.cs
+++ EnvelopeEditorMorph/instance/construction/addControls.cs
@@ -1,26 +1,19 @@
 addControls
 	| chooser |
-	chooser _ PopUpChoiceMorph new extent: 90@14;
-		contentsClipped: 'edit ' , envelope name;
+	chooser _ PopUpChoiceMorph new extent: 110@14;
+		contentsClipped: 'editing ' , envelope name;
 		target: self;
 		actionSelector: #chooseFrom:envelopeItem:;
 		getItemsSelector: #curveChoices.
 	chooser arguments: (Array with: chooser).
 	self addMorph: chooser.
-	chooser position: bounds bottomCenter - (35@40).
+	chooser align: chooser bounds topLeft with: graphArea bounds bottomLeft + (0@5).
 
 	chooser _ PopUpChoiceMorph new extent: 110@14;
-		contentsClipped: whatToPlay;
+		contentsClipped: 'duration: ' , self durationName;
 		target: self;
-		actionSelector: #sampleToPlay:;
-		getItemsSelector: #sampleChoices.
+		actionSelector: #chooseFrom:durationItem:;
+		getItemsSelector: #durationChoices.
+	chooser arguments: (Array with: chooser).
 	self addMorph: chooser.
-	chooser position: bounds bottomCenter - (90@20).
-
-	chooser _ PopUpChoiceMorph new extent: 90@14;
-		contentsClipped: playMode;
-		target: self;
-		actionSelector: #setPlayMode:;
-		getItemsSelector: #playChoices.
-	self addMorph: chooser.
-	chooser position: bounds bottomCenter - (-20@20).
+	chooser align: chooser bounds topRight with: graphArea bounds bottomRight + (-50@5).
