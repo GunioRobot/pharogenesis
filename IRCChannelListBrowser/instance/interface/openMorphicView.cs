80a51429b1f296e1147b5d154a5ec1c1759ca290
ls <unknown>
1998-09-04 22:07:00 +0000

    no message

---
+++ IRCChannelListBrowser/instance/interface/openMorphicView.cs
@@ -0,0 +1,27 @@
+openMorphicView
+	| win descListView updateButton actionColumn openChannelButton createChannelButton |
+	win _ SystemWindow new.
+	win setLabel: 'Channel Listing'.
+	win model: self.
+
+	descListView _ PluggableListMorph on: self  list: #channelDescriptions selected: #channelIndex changeSelected: #channelIndex:.
+	win addMorph: descListView  frame: (0@0 extent: 0.8@0.9).
+
+	updateButton _ PluggableButtonMorph on: connection getState: nil action: #requestChannelList.
+	updateButton label: 'update'.
+	win addMorph: updateButton  frame: (0@0.9 extent: 1@0.1).
+
+
+	actionColumn _ AlignmentMorph newColumn.
+
+	openChannelButton _ PluggableButtonMorph on: self getState: nil action: #openSelectedChannel.
+	openChannelButton label: 'join channel'.
+	actionColumn addMorphBack: openChannelButton.
+
+	createChannelButton _ PluggableButtonMorph on: self getState: nil action: #createChannel.
+	createChannelButton label: 'create channel'.
+	actionColumn addMorphBack: createChannelButton.
+
+	win addMorph: actionColumn  frame: (0.8@0 extent: 0.2@0.9).
+
+	win openInWorld
