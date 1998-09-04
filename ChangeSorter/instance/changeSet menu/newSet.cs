dcf80112058d805de6a2315954e0af229ae805ea
sw <unknown>
1998-09-04 09:00:00 +0000

    no message

--- ChangeSorter/instance/changeSet menu/newSet.cs
+++ ChangeSorter/instance/changeSet menu/newSet.cs
@@ -1,15 +1,10 @@
 newSet
-	"Create a new changeSet and show it.  make the new guy the current one.  Also, reject name if already in use."
-	| newName newSet |
+	"Create a new changeSet and show it., making it the current one.  Reject name if already in use."
 
+	| aSet |
 	self okToChange ifFalse: [^ self].
-	newName _ FillInTheBlank request: 'A name for the new change set'
-			initialAnswer: ChangeSet defaultName.
-	newName isEmpty ifTrue: [^ self].
-	(self class changeSetNamed: newName) ifNotNil:
-			[^ self inform: 'Sorry that name is already used'].
-
-	newSet _ ChangeSet new initialize name: newName.
-	AllChangeSets add: newSet.
-	self showChangeSet: newSet.
-	self newCurrent.
+	aSet _ self class newChangeSet.
+	aSet ifNotNil:
+		[self update.
+		self showChangeSet: aSet.
+		self changed: #relabel]
