376f37835c3988ed3f7a32c4fb6e852c5ae446f7
BJP <unknown>
1998-09-07 00:52:00 +0000

    no message

--- URLmap/instance/creating/allPagesFrom-3A-for-3A-.cs
+++ URLmap/instance/creating/allPagesFrom-3A-for-3A-.cs
@@ -1,20 +1,34 @@
 allPagesFrom: pageRef for: request
 	| formattedPage refPages allPages peer |
-	(request isKindOf: PWS) ifTrue: [peer _ request peerName] ifFalse: [peer_''].
+	(request isKindOf: PWS) ifTrue: [peer _ request peerName] ifFalse:
+[peer_''].
 	formattedPage _ pageRef copy.  "Make a copy, then format the text."
 	refPages _ OrderedCollection new.
-	formattedPage formatted: (HTMLformatter swikify: (pageRef text)
-								linkhandler: [:link | self linkFor: link
-													from: peer
-													storingTo: refPages]).
+	formattedPage formatted: (action formatter swikify: (pageRef text)
+
+	linkhandler: [:link | self linkFor: link
+
+				from: peer
+
+				storingTo: refPages]).
+	"This filtering should be generalized a tad, methinks. Perhaps
+incorparated into the
+	linkhandler."
+	refPages _ refPages reject: [:page | page pageStatus = #new].
 	"Now, put all referenced pages into the page"
 	allPages _ WriteStream on: String new.
-	allPages nextPutAll: '<h2>',(pageRef name),'</h2>', formattedPage formatted.
+	allPages nextPutAll: '<h2>',(pageRef name),'</h2>', formattedPage
+formatted.
 	refPages do: [:page |
 		allPages nextPutAll: '<h2>',(page name),'</h2>'.
 		allPages nextPutAll: (HTMLformatter swikify: (page text)
-								linkhandler: [:link | self linkFor: link
-													from: peer
-													storingTo: (OrderedCollection new)]).].
-	formattedPage formatted: allPages contents. "Put all the pages into THE page"
+
+	linkhandler: [:link | self linkFor: link
+
+				from: peer
+
+				storingTo:
+(OrderedCollection new)]).].
+	formattedPage formatted: allPages contents. "Put all the pages into THE
+page"
 	^formattedPage
