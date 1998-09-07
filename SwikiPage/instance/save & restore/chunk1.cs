45e4ea97f9f88b4acddbf861cef983570629f651
BJP <unknown>
1998-09-07 11:30:00 +0000

    no message

--- SwikiPage/instance/save & restore/chunk1.cs
+++ SwikiPage/instance/save & restore/chunk1.cs
@@ -1,11 +1,11 @@
 chunk1
-	
-	| who |
-	^ String streamContents: [:ss | 
+	^self formatForOutput
+	"| who |
+	^ String streamContents: [:ss |
 		ss nextPutAll: 'self name: '; nextPutAll: name printString;
 		nextPutAll: ' date: '''; nextPutAll: date mmddyyyy.
-		ss nextPutAll: ''' time: '''.  Time now print24: true on: ss.
+		ss nextPutAll: ''' time: '''.  time print24: true on: ss.
 		who _ user class == String ifFalse: [address] ifTrue: [user].
-		who ifNil: [who _ 'unknown user'].	"must be a string!!"
+		who ifNil: [who _ 'unknown user'].
 		ss nextPutAll: ''' by: '; nextPutAll: who printString.
-		ss nextPutAll: ' text: ']
+		ss nextPutAll: ' text: ']"
