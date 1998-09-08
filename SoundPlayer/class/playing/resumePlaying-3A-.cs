f8c045ffa029b26df6ee4b89a75b667143acf731
jm <unknown>
1998-09-08 17:54:00 +0000

    no message

--- SoundPlayer/class/playing/resumePlaying-3A-.cs
+++ SoundPlayer/class/playing/resumePlaying-3A-.cs
@@ -1,18 +1,5 @@
 resumePlaying: aSound
 	"Start playing the given sound without resetting it; it will resume playing from where it last stopped."
+	"Implementation detail: On virtual machines that don't support the quickstart primitive, you may need to edit this method to pass false to resumePlaying:quickStart:."
 
-	| quickStart |
-	Preferences disableSounds ifTrue: [^ self].
-	quickStart _ true.
-	PlayerProcess == nil ifTrue: [
-		self canStartPlayer ifFalse: [^ self].
-		self startUp. quickStart _ false].
-
-	PlayerSemaphore critical: [
-		(ActiveSounds includes: aSound)
-			ifTrue: [quickStart _ false]
-			ifFalse: [
-				quickStart ifFalse: [ActiveSounds add: aSound]]].
-
-	"quick-start the given sound, unless the sound player has just started"
-	quickStart ifTrue: [self startPlayingImmediately: aSound].
+	self resumePlaying: aSound quickStart: true.
