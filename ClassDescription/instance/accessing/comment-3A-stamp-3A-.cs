054fe09bc28db73958f0a42ce8370eb1a425f0d8
sw <unknown>
1998-09-08 14:43:00 +0000

    no message

---
+++ ClassDescription/instance/accessing/comment-3A-stamp-3A-.cs
@@ -0,0 +1,5 @@
+comment: aStringOrText stamp: aStamp
+	"Set the receiver's comment to be the argument, aStringOrText."
+
+	self theNonMetaClass classComment: aStringOrText stamp: aStamp.
+	Smalltalk changes commentClass: self theNonMetaClass
