9cd48ea0f6fba7a89715f87c4999720b65d495cf
BJP <unknown>
1998-09-08 18:34:00 +0000

    no message

--- SwikiPage/instance/accessing/date-3A-.cs
+++ SwikiPage/instance/accessing/date-3A-.cs
@@ -1,4 +1,6 @@
 date: anObject
 	"Set the receiver's instance variable 'date' to be anObject."
 
-	date := anObject
+		(anObject isKindOf: String)
+			ifTrue: [date _ anObject asDate]
+			ifFalse: [date := anObject]
