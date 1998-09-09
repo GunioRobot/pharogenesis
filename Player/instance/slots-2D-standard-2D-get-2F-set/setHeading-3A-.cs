513d38ccc6be30e7a74d9ef48bd4f834c30d4f84
di <unknown>
1998-09-09 20:13:00 +0000

    no message

--- Player/instance/slots-2D-standard-2D-get-2F-set/setHeading-3A-.cs
+++ Player/instance/slots-2D-standard-2D-get-2F-set/setHeading-3A-.cs
@@ -1,10 +1,13 @@
-setHeading: aNumber
-
-	0.0 = aNumber
-		ifTrue: [
-			1.0 = self getScaleFactor ifTrue: [
-				costume isFlexMorph ifTrue: [costume removeFlexShell].
+setHeading: newHeading
+	| aCostume |
+	aCostume _ self costume.
+	newHeading = 0.0
+		ifTrue: 
+			[self getScaleFactor = 1.0 ifTrue:
+				[aCostume isFlexMorph ifTrue:
+					[aCostume rotationDegrees: newHeading.
+					aCostume removeFlexShell].
 				^ self]]
-		ifFalse: [
-			costume isFlexMorph ifFalse: [costume addFlexShell]].
-	costume rotationDegrees: aNumber.
+		ifFalse:
+			[aCostume isFlexMorph ifFalse: [aCostume addFlexShell]].
+	self costume rotationDegrees: newHeading.
