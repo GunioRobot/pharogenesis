8cc4965666c0516989bd9449fda6d699510bc10e
di <unknown>
1998-09-09 22:25:00 +0000

    no message

--- Morph/instance/drawing/imageFormForRectangle-3A-.cs
+++ Morph/instance/drawing/imageFormForRectangle-3A-.cs
@@ -1,5 +1,3 @@
 imageFormForRectangle: rect
-	| canvas |
-	canvas _ FormCanvas extent: rect extent.
-	self fullDrawOn: (canvas copyOffset: rect topLeft negated).
-	^ canvas form offset: rect topLeft
+
+	^ self imageForm: Display depth forRectangle: rect
