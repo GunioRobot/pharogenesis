0b3c489adfb392fe0444dcac5435752fab991b70
jm <unknown>
1998-09-10 16:58:00 +0000

    no message

--- ScorePlayer/instance/sound generation/isDone.cs
+++ ScorePlayer/instance/sound generation/isDone.cs
@@ -1,7 +1,8 @@
 isDone
 
 	| track |
-	activeSounds size > 0  ifTrue: [^ false].
+	activeSounds size > 0 ifTrue: [^ false].
+	activeMIDINotes size > 0 ifTrue: [^ false].
 	1 to: score tracks size do: [:i |
 		track _ score tracks at: i.
 		(trackEventIndex at: i) <= track size ifTrue: [^ false]].
