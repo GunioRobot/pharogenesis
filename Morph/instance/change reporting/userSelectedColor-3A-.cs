56f73e8643f21ec2c3e7b3c6ec513bf49306e955
sw <unknown>
1998-09-10 08:18:00 +0000

    no message

---
+++ Morph/instance/change reporting/userSelectedColor-3A-.cs
@@ -0,0 +1,4 @@
+userSelectedColor: aColor
+	"The user, via the UI, chose aColor to be the color for the receiver; set it, and tell my owner in case he wishes to react"
+	self color: aColor.
+	owner ifNotNil: [owner colorChangedForSubmorph: self]
