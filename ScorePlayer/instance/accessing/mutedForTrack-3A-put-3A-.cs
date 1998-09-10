b9a3ede82eba2a6a63859bf6047803a8f2faf421
jm <unknown>
1998-09-10 17:25:00 +0000

    no message

--- ScorePlayer/instance/accessing/mutedForTrack-3A-put-3A-.cs
+++ ScorePlayer/instance/accessing/mutedForTrack-3A-put-3A-.cs
@@ -1,6 +1,13 @@
 mutedForTrack: trackIndex put: aBoolean
 
 	muted at: trackIndex put: aBoolean.
-	aBoolean ifTrue: [
-		activeSounds do: [:pair |
-			pair last = trackIndex ifTrue: [activeSounds remove: pair ifAbsent: []]]].
+	aBoolean ifFalse: [^ self].
+
+	"silence any currently sounding notes for this track"
+	activeSounds do: [:pair |
+		pair last = trackIndex ifTrue: [activeSounds remove: pair ifAbsent: []]].
+	midiPort ifNotNil: [
+		activeMIDINotes do: [:pair |
+			pair last = trackIndex ifTrue: [
+				pair first endNoteOnMidiPort: midiPort.
+				activeMIDINotes remove: pair ifAbsent: []]]].
