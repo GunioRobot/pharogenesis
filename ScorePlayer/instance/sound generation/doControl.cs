26d30c38e6f904b6521db7d11cd658d1517e2ca7
jm <unknown>
1998-09-10 19:07:00 +0000

    no message

--- ScorePlayer/instance/sound generation/doControl.cs
+++ ScorePlayer/instance/sound generation/doControl.cs
@@ -1,13 +1,6 @@
 doControl
 
-	| t |
 	super doControl.
 	1 to: activeSounds size do: [:i | (activeSounds at: i) first doControl].
 	ticksSinceStart _ ticksSinceStart + ticksClockIncr.
-	t _ ticksSinceStart asInteger.
-	self processTempoMapAtTick: t.
-	self processEventsAtTick: t.
-	self isDone ifTrue: [
-		repeat
-			ifTrue: [self reset]
-			ifFalse: [done _ true]].
+	self processAllAtTick: ticksSinceStart asInteger.
