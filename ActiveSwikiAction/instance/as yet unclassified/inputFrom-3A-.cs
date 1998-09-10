d02a022f0b01bb22df4383ee50add15673516086
mjg <unknown>
1998-09-10 15:33:00 +0000

    no message

--- ActiveSwikiAction/instance/as yet unclassified/inputFrom-3A-.cs
+++ ActiveSwikiAction/instance/as yet unclassified/inputFrom-3A-.cs
@@ -1,23 +1,39 @@
 inputFrom: request
 	"Take user's input and respond with a searchresult or store the edit"
 
-	| coreRef page |
+	| coreRef page theText |
 	coreRef _ request message size < 2
 		ifTrue: ['1']
 		ifFalse: [request message at: 2].
 	coreRef = 'searchresult' ifTrue: [
 		"If contains search string, do search"
-		request reply: PWS crlf, 
-			(HTMLformatter evalEmbedded: (self fileContents: source, 'results.html')
-				with: (urlmap searchFor: (request fields at: 'searchFor' ifAbsent: ['nothing']))).
+		request reply: PWS crlf,
+			(HTMLformatter evalEmbedded: (self fileContents:
+source, 'results.html')
+				with: (urlmap searchFor: (request fields
+at: 'searchFor' ifAbsent: ['nothing']))).
 		^ #return].
-	(request fields includesKey: 'text') ifTrue: ["It's a response from an edit, so store the page"
+	(theText _ request fields at: 'text' ifAbsent: [nil]) ifNotNil: [
+		"It's a response from an edit, so store the page"
+		page _ urlmap atID: coreRef.
+		page user: request peerName.  "Address is machine, user only if
+logged in"
+		 page pageStatus = #new ifTrue: [page pageStatus: #standard].
 		page _ urlmap
 			storeID: coreRef
-			text: (request fields at: 'text' ifAbsent: ['blank text'])
+			text: theText withSqueakLineEndings
 			from: request peerName.
-		page user: request userID.
-		^ self].	"return self means do serve the edited page afterwards"
+		^ self].	"return self means do serve the edited page
+afterwards"
+	request fields keys do: [:aTag |
+		(aTag beginsWith: 'text-') ifTrue: [
+			urlmap
+				storeID: coreRef
+				text: (request fields at: aTag)
+withSqueakLineEndings
+				insertAt: (aTag copyFrom: 6 to: aTag size).
+	"string"
+			^ self]].
 	"oops, a new kind! -- but don't complain! Could be for ActivePage!"
 "	Transcript show: 'Unknown data from client. '; show: request fields
 printString; cr."
