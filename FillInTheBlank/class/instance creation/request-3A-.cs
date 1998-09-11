fa5b24d43e5ae5ab4bad9645c3fdfd2c02f2e32b
di <unknown>
1998-09-11 15:01:00 +0000

    no message

--- FillInTheBlank/class/instance creation/request-3A-.cs
+++ FillInTheBlank/class/instance creation/request-3A-.cs
@@ -2,13 +2,6 @@ request: queryString
 	"Create an instance of me whose question is queryString. Invoke it centered at the cursor, and answer the string the user accepts. Answer the empty string if the user cancels."
 	"FillInTheBlankMorph request: queryString"
 
-	World ifNotNil: [
-		^ FillInTheBlankMorph
-			request: queryString
-			initialAnswer: ''
-			centerAt: Sensor cursorPoint].
-
-	^ self
-		request: queryString
+	^ self request: queryString
 		initialAnswer: ''
 		centerAt: Sensor cursorPoint.
