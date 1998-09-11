b44c43924d45aefea16aef0c2880fab4acd3991c
TPR <unknown>
1998-09-11 14:42:00 +0000

    no message

--- SystemTracer/instance/mapping oops/initOopMap.cs
+++ SystemTracer/instance/mapping oops/initOopMap.cs
@@ -5,3 +5,6 @@ initOopMap
 	Access to an object causes it to be promoted in the subarray,
 		so that frequently accessed objects can be found quickly."
 	oopMap _ (1 to: 4096) collect: [:i | Array new].
+
+	"replacementClasses is a simple Dictionary to contain pairs of classes (well, globals should work actually) where the key is a class being replaced and the value is the replacement class"
+	replacementClasses _ Dictionary new
