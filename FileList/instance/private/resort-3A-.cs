99fbc5c382188fc4936700601cecd11dc01c937e
ls <unknown>
1998-09-11 04:15:00 +0000

    no message

--- FileList/instance/private/resort-3A-.cs
+++ FileList/instance/private/resort-3A-.cs
@@ -1,11 +1,12 @@
 resort: newMode
 	"Re-sort the list of files."
-
 	| name |
 	listIndex > 0
 		ifTrue: [name _ self fileNameFromFormattedItem: (list at: listIndex)].
 	sortMode _ newMode.
 	self pattern: pattern.
-	name ifNotNil:
-		[listIndex _ list findFirst: [:item | (self fileNameFromFormattedItem: item) = name].
+	name ifNotNil: [
+		fileName _ name.
+		listIndex _ list findFirst: [:item | (self fileNameFromFormattedItem: item) = name. ].
 		self changed: #fileListIndex].
+	listIndex = 0 ifTrue: [self changed: #contents]
