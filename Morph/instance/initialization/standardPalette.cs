a3d6c78431f7548f924e2f45498b4020581e2a39
sw <unknown>
1998-09-11 11:13:00 +0000

    no message

--- Morph/instance/initialization/standardPalette.cs
+++ Morph/instance/initialization/standardPalette.cs
@@ -1,7 +1,8 @@
 standardPalette
 	"Answer a standard palette forced by some level of enclosing presenter, or nil if none"
 	| pal aPresenter itsOwner |
-	^ (pal _ (aPresenter _ self presenter) ownStandardPalette)
+	(aPresenter _ self presenter) ifNil: [^ nil].
+	^ (pal _ aPresenter ownStandardPalette)
 		ifNotNil: [pal]
 		ifNil:	[(itsOwner _ aPresenter associatedMorph owner)
 					ifNotNil:
