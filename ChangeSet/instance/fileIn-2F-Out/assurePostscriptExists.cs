72fbc93f63d111e9eb5d010a298f52b59c22e10d
di <unknown>
1998-09-11 16:13:00 +0000

    no message

--- ChangeSet/instance/fileIn-2F-Out/assurePostscriptExists.cs
+++ ChangeSet/instance/fileIn-2F-Out/assurePostscriptExists.cs
@@ -1,4 +1,10 @@
 assurePostscriptExists
 	"Make sure there is a StringHolder holding the postscript.  "
 
-	postscript == nil ifTrue: [postscript _ StringHolder new contents: '']
+	"NOTE: FileIn recognizes the postscript by the line with Postscript: on it"
+	postscript == nil ifTrue: [postscript _ StringHolder new contents: '"Postscript:
+Leave the line above, and replace the rest of this comment by a useful one.
+Executable statements should follow this comment, and should
+be separated by periods, with no exclamation points (!).
+Be sure to put any further comments in double-quotes, like this one."
+']
