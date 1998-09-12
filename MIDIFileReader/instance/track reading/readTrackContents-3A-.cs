e34df3a3f6f2ccc837dc453fe78a354d87f24b51
jm <unknown>
1998-09-12 17:10:00 +0000

    no message

--- MIDIFileReader/instance/track reading/readTrackContents-3A-.cs
+++ MIDIFileReader/instance/track reading/readTrackContents-3A-.cs
@@ -9,9 +9,12 @@ readTrackContents: byteCount
 	(tracks isEmpty and: [self isTempoTrack: track])
 		ifTrue: [tempoMap _ track asArray]
 		ifFalse: [
-			track size > 0 ifTrue: [
-			tracks add: track asArray.
-			info _ WriteStream on: (String new: 100).
-			strings do: [:s | info nextPutAll: s; cr].
-			trackInfo add: info contents]].
+			"Note: Tracks without note events are currently not saved to
+			 eliminate clutter in the score player. In control applications,
+			 this can be easily changed by modifying the following test."
+			(self trackContainsNotes: track) ifTrue: [
+				tracks add: track asArray.
+				info _ WriteStream on: (String new: 100).
+				strings do: [:s | info nextPutAll: s; cr].
+				trackInfo add: info contents]].
 	strings _ track _ trackStream _ activeEvents _ nil.
