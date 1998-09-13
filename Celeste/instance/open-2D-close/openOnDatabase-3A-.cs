67c6254ee1d7fe1f1d7795784659a7360f521bfd
ls <unknown>
1998-09-13 21:15:00 +0000

    no message

---
+++ Celeste/instance/open-2D-close/openOnDatabase-3A-.cs
@@ -0,0 +1,11 @@
+openOnDatabase: aMailDB
+	"Initialize myself for the mail database with the given root filename."
+
+	mailDB _ aMailDB.
+	mailDB addDependent: self.
+	currentCategory _ 'new'.
+	lastCategory _ ''.
+	subjectFilter _ ''.
+	fromFilter _ ''.
+	dateFilter _ nil.
+	self setCategory: currentCategory.
