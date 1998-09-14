c096562a6b5c68403cc28e71105657fd868ecd0e
di <unknown>
1998-09-14 07:31:00 +0000

    no message

--- Morph/instance/event handling/handlesMouseOver-3A-.cs
+++ Morph/instance/event handling/handlesMouseOver-3A-.cs
@@ -1,4 +1,5 @@
 handlesMouseOver: evt
-	"Do I respond to mouseDown, mouseStillDown, or mouseUp?  "
-	eventHandler ifNotNil: [^ eventHandler handlesMouseOver: evt].
+	"Do I want to receive mouseEnter: and mouseLeave: when the button is up and the hand is empty?  The default response is false, except if you have added sensitivity to mouseEnter: or mouseLeave:, using the on:send:to: mechanism." 
+
+	self eventHandler ifNotNil: [^ self eventHandler handlesMouseOver: evt].
 	^ false
