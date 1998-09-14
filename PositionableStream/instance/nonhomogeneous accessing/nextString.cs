e896d44b0b21643014bddfd03d9968d9ad12e4c7
ls <unknown>
1998-09-14 22:46:00 +0000

    no message

--- PositionableStream/instance/nonhomogeneous accessing/nextString.cs
+++ PositionableStream/instance/nonhomogeneous accessing/nextString.cs
@@ -1,11 +1,16 @@
 nextString
-	"Read a string from the receiver. The first byte is the length of the 
-	string, unless it is greater than 192, in which case the first two bytes 
-	encode the length."
+	"Read a string from the receiver. The first byte is the length of the string, unless it is greater than 192, in which case the first four bytes encode the length.  I expect to be in ascii mode when called (caller puts back to binary)."
 
-	| aString char length|
+	| aString length |
+
+	"read the length in binary mode"
+	self binary.
 	length _ self next.		"first byte."
-	length >= 192 ifTrue: [length _ (length - 192) * 256 + self next].
+	length >= 192 ifTrue: [length _ length - 192.
+		1 to: 3 do: [:ii | length _ length * 256 + self next]].
 	aString _ String new: length.
-	1 to: length do: [:i | aString at: i put: self next asCharacter].
+
+	"read the characters in ASCII mode"
+	self ascii.
+	self nextInto: aString.
 	^aString
