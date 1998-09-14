91abedb7901717d87f1e89fbba203b5954436673
sw <unknown>
1998-09-14 14:41:00 +0000

    no message

--- StandardScriptingSystem/instance/font & color choices/statusColorSymbolFor-3A-.cs
+++ StandardScriptingSystem/instance/font & color choices/statusColorSymbolFor-3A-.cs
@@ -1,13 +1,15 @@
 statusColorSymbolFor: statusSymbol
-	#(	(normal				green)
-		(ticking				blue)
-		(paused				red)
-		(mouseDown			yellow)
-		(mouseStillDown		lightYellow)
-		(mouseUp			lightBlue)
-		(mouseEnter			lightBrown)
-		(mouseLeave		lightRed)
-		(keyStroke			lightGreen)) do:
+	#(	(normal					green)
+		(ticking					blue)
+		(paused					red)
+		(mouseDown				yellow)
+		(mouseStillDown			lightYellow)
+		(mouseUp				lightBlue)
+		(mouseEnter				lightBrown)
+		(mouseLeave			lightRed)
+		(mouseEnterDragging	lightGray)
+		(mouseLeaveDragging	darkGray)
+		(keyStroke				lightGreen)) do:
 
 			[:pair | statusSymbol == pair first ifTrue: [^ pair second]].
 
