c1afe86c87d97bd2a531057a5eef654b225893b3
di <unknown>
1998-09-15 16:35:00 +0000

    no message

---
+++ EventHandler/instance/object fileIn/convertmmmmmmmmmmkkv0-3A-mmmmmmmmmmmmmmkkv0-3A-.cs
@@ -0,0 +1,5 @@
+convertmmmmmmmmmmkkv0: varDict mmmmmmmmmmmmmmkkv0: smartRefStrm
+	"These variables are automatically stored into the new instance ('mouseDownRecipient' 'mouseDownSelector' 'mouseStillDownRecipient' 'mouseStillDownSelector' 'mouseUpRecipient' 'mouseUpSelector' 'mouseEnterRecipient' 'mouseEnterSelector' 'mouseLeaveRecipient' 'mouseLeaveSelector' 'keyStrokeRecipient' 'keyStrokeSelector' 'valueParameter' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"Be sure to to fill in ('mouseEnterDraggingRecipient' 'mouseEnterDraggingSelector' 'mouseLeaveDraggingRecipient' 'mouseLeaveDraggingSelector' ) and deal with the information in ()"
