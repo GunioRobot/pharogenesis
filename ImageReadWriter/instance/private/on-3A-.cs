50a78071fbfea820ae9d83c6c90567a43b17aed8
di <unknown>
1998-09-15 11:42:00 +0000

    no message

--- ImageReadWriter/instance/private/on-3A-.cs
+++ ImageReadWriter/instance/private/on-3A-.cs
@@ -1,6 +1,4 @@
 on: aStream
-
 	(stream _ aStream) reset.
 	(stream respondsTo: #binary) ifTrue: [stream binary].
-		"Note that 'reset' makes a file be text!  Must do this
-after!"
+	"Note that 'reset' makes a file be text.  Must do this after."
