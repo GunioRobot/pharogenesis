91eb2b5f8a29b5f53f3243ca781af50c7402baa5
tk <unknown>
1998-09-16 08:40:00 +0000

    no message

--- ServerDirectory/instance/file directory/oldFileNamed-3A-.cs
+++ ServerDirectory/instance/file directory/oldFileNamed-3A-.cs
@@ -1,13 +1,18 @@
-oldFileNamed: fullName
-	"If the file exists, answer a read-only RemoteFileStream on it.  fullName is directory path, and does include name of the server.  Or it can just be a fileName.  For now, pre-read the file."
+oldFileNamed: aName
+	"If the file exists, answer a read-only RemoteFileStream on it.  aName is directory path, and does include name of the server.  Or it can just be a fileName.  For now, pre-read the file."
 
-	| file remoteStrm |
-	file _ self as: ServerFile.
-	(fullName includes: self pathNameDelimiter)
-		ifTrue: [file fullPath: fullName]		"sets server, directory(path), fileName"
-		ifFalse: [file fileName: fullName].	"already have the rest"
-	file readOnly.
+	| rFile remoteStrm |
+	rFile _ self as: ServerFile.
+	(aName includes: self pathNameDelimiter)
+		ifTrue: [rFile fullPath: aName]
+			"sets server, directory(path), fileName.  If relative, merge with self."
+		ifFalse: [rFile fileName: aName].	"JUST a single NAME, already have the rest"
+			"Mac files that include / in name, must encode it as %2F "
+	rFile readOnly.
+	rFile type == #file ifTrue: [
+		^ FileStream oldFileNamed: (rFile fileNameRelativeTo: self)].
+
 	remoteStrm _ RemoteFileStream on: (String new: 2000).
-	remoteStrm remoteFile: file.
-	file getFileNamed: file fileName into: remoteStrm.
+	remoteStrm remoteFile: rFile.
+	rFile getFileNamed: rFile fileName into: remoteStrm.
 	^ remoteStrm
