e6f187492072af0343dbaa53e04f39ea765888f5
ls <unknown>
1998-09-17 05:49:00 +0000

    no message

---
+++ Celeste/class/user preferences/setUserName.cs
@@ -0,0 +1,7 @@
+setUserName
+	"Change the user's email name for use in composing messages."
+
+	(UserName isNil) ifTrue: [UserName _ ''].
+	UserName _ FillInTheBlank
+		request: 'What is your email address?\(This is the address other people will reply to you)' withCRs
+		initialAnswer: UserName.
