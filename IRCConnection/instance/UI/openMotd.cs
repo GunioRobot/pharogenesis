6785a725b05c5a397130316d61ba293e6a5994d0
ls <unknown>
1998-09-17 06:37:00 +0000

    no message

---
+++ IRCConnection/instance/UI/openMotd.cs
@@ -0,0 +1,14 @@
+openMotd
+	"open a view on the MOTD"
+	| win textView updateButton |
+
+	win _ SystemWindow labelled: 'MOTD'.
+
+	textView _ PluggableTextMorph on: self text: #motd accept: nil.
+	win addMorph: textView  frame: (0@0 extent: 1@0.9).
+
+	updateButton _ PluggableButtonMorph on: self getState: nil action: #requestMotd.
+	updateButton label: 'update'.
+	win addMorph: updateButton  frame: (0@0.9 extent: 1@0.1).
+
+	win openInWorld.
