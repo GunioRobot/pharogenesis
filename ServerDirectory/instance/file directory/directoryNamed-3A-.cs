dd4ddaeb8b0126b36b84a19e5a0f4ba1dd4b3dbd
tk <unknown>
1998-09-19 18:58:00 +0000

    no message

--- ServerDirectory/instance/file directory/directoryNamed-3A-.cs
+++ ServerDirectory/instance/file directory/directoryNamed-3A-.cs
@@ -3,5 +3,9 @@ directoryNamed: localFileName
 
 	| new |
 	new _ self copy.
+	urlObject ifNotNil: [
+		new urlObject path: (new urlObject path) copy.
+		(new urlObject path) removeLast; addLast: localFileName; addLast: ''.
+		^ new].
 	new directory: directory, self pathNameDelimiter asString, localFileName.
 	^ new
