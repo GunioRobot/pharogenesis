28720d4d857cd3ee0fa9b0e220bea2c51208dd60
jm <unknown>
1998-09-20 11:31:00 +0000

    no message

--- EnvelopeEditorMorph/instance/initialization/editSound-3A-.cs
+++ EnvelopeEditorMorph/instance/initialization/editSound-3A-.cs
@@ -1,3 +1,12 @@
 editSound: aSound
+
+	| p |
 	sound _ aSound.
+	sound envelopes isEmpty ifTrue: [
+		"provide a default volume envelope"
+		p _ OrderedCollection new.
+		p add: 0@0.0; add: 10@1.0; add: 100@1.0; add: 120@0.0.
+		sound addEnvelope: (VolumeEnvelope points: p loopStart: 2 loopEnd: 3)].
+
 	self editEnvelope: sound envelopes first.
+	keyboard soundPrototype: sound.
