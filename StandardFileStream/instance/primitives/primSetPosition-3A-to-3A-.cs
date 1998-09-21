daeab6bbd342cf6d6e50d94fc159e480cbb98b56
jm <unknown>
1998-09-21 14:09:00 +0000

    no message

--- StandardFileStream/instance/primitives/primSetPosition-3A-to-3A-.cs
+++ StandardFileStream/instance/primitives/primSetPosition-3A-to-3A-.cs
@@ -1,4 +1,5 @@
-primSetPosition: id to: aNumber
-	"Set the receiver's file position to be a Number.  2/12/96 sw"
+primSetPosition: id to: anInteger
+	"Set this file to the given position."
+
 	<primitive: 155>
-	^ self primitiveFailed
+	self primitiveFailed
