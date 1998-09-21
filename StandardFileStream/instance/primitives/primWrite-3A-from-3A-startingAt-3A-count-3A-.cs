a6f954e2378b0cba0d18c10c2d2daa43579c039c
jm <unknown>
1998-09-21 16:18:00 +0000

    no message

--- StandardFileStream/instance/primitives/primWrite-3A-from-3A-startingAt-3A-count-3A-.cs
+++ StandardFileStream/instance/primitives/primWrite-3A-from-3A-startingAt-3A-count-3A-.cs
@@ -1,8 +1,6 @@
-primWrite: id from: byteArray startingAt: startIndex count: count
-	"Write into the receiver's file from the given area of storage, starting at the given index, as many as count bytes; return the number of bytes actually written. 2/12/96 sw"
+primWrite: id from: stringOrByteArray startingAt: startIndex count: count
+	"Write count bytes onto this file from the given string or byte array starting at the given index. Answer the number of bytes written."
 
 	<primitive: 158>
-
-	closed ifTrue: [^ self halt: 'Write error: File not open'].
-	rwmode ifFalse: [^ self halt: 'Error-attempt to write to a read-only file.'].
-	self halt: 'File write error'
+	self closed ifTrue: [^ self error: 'File is closed'].
+	self error: 'File write failed'.
