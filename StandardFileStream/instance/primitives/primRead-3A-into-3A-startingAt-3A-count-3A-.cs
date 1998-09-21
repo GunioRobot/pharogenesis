0301860d0f48145fa143b3c6e1a65aae720993ae
jm <unknown>
1998-09-21 16:18:00 +0000

    no message

--- StandardFileStream/instance/primitives/primRead-3A-into-3A-startingAt-3A-count-3A-.cs
+++ StandardFileStream/instance/primitives/primRead-3A-into-3A-startingAt-3A-count-3A-.cs
@@ -1,6 +1,6 @@
 primRead: id into: byteArray startingAt: startIndex count: count
-	"read from the receiver's file into the given area of storage, starting at the given index, as many as count bytes; return the number of bytes actually read.  2/12/96 sw"
+	"Read up to count bytes of data from this file into the given string or byte array starting at the given index. Answer the number of bytes actually read."
 
 	<primitive: 154>
-
-	self halt: 'error reading file'
+	self closed ifTrue: [^ self error: 'File is closed'].
+	self error: 'File read failed'.
