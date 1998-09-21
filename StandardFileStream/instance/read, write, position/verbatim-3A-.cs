cb99dcc0fdfbf1c216cf628d882bfa7875b5dbdd
jm <unknown>
1998-09-21 13:56:00 +0000

    no message

--- StandardFileStream/instance/read, write, position/verbatim-3A-.cs
+++ StandardFileStream/instance/read, write, position/verbatim-3A-.cs
@@ -1,4 +1,4 @@
 verbatim: aString
-	"A copy of nextPutAll that can be called knowing it wont call nextPut: "
-	self primWrite: fileID from: aString startingAt: 1 count: aString size.
-	^ aString
+	"A version of nextPutAll that can be called knowing it won't call nextPut: "
+
+	^ self nextPutAll: aString
