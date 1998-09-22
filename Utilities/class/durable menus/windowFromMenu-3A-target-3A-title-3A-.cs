ec6b40761a761507463746700043b524ee7c1a55
di <unknown>
1998-09-22 14:49:00 +0000

    no message

--- Utilities/class/durable menus/windowFromMenu-3A-target-3A-title-3A-.cs
+++ Utilities/class/durable menus/windowFromMenu-3A-target-3A-title-3A-.cs
@@ -1,15 +1,4 @@
 windowFromMenu: aMenu target: aTarget title: aTitle
-	| n labelList colorPattern targetList selectionList |
-	"Utilities windowFromMenu: ScheduledControllers screenController helpMenu target: ScheduledControllers screenController title: 'Help'"
-
-	n _ aMenu selections size.
-
-	labelList _ (1 to: n) asArray  collect:
-		[:ind | aMenu labelString lineNumber: ind].
-
-	colorPattern _ #(lightRed lightGreen lightBlue lightYellow lightGray).
-			
-	targetList _  (1 to: n) asArray  collect: [:ind | aTarget].
-	selectionList _ aMenu selections.
-
-	self windowMenuWithLabels:  labelList colorPattern: colorPattern targets: targetList selections: selectionList title: aTitle
+	"Supply a default list of colors"
+	^ self windowFromMenu: aMenu target: aTarget title: aTitle
+		colorPattern: #(lightRed lightGreen lightBlue lightYellow lightGray lightCyan lightMagenta lightOrange)
