4e5d5af34a47bb85a114e01755cda71cd253bbdc
tk <unknown>
1998-09-22 20:26:00 +0000

    no message

---
+++ Scamper/instance/changing page/submitFormWithInputs-3A-url-3A-method-3A-.cs
@@ -0,0 +1,27 @@
+submitFormWithInputs: inputs  url: url  method: method
+	| newUrl newSource | 
+	self stopEverything.
+
+	(method asLowercase ~= 'get' and: [ method asLowercase ~= 'post' ]) ifTrue: [
+		self notify: 'unkown FORM method: ', method.
+		^false ].
+
+	newUrl _ url asUrlRelativeTo: currentUrl.	
+
+	newUrl schemeName ~= 'http' ifTrue: [
+		self notify: 'I can only submit forms via HTTP'.
+		^false ].
+
+	self status: 'submitting form...'.
+
+	downloadingProcess _ [
+			method asLowercase = 'get' 
+				ifTrue: [newSource _ newUrl retrieveContentsArgs: inputs] 
+				ifFalse: [newSource _ newUrl postFormArgs: inputs].
+			documentQueue nextPut:  newSource.
+
+			downloadingProcess _ nil.
+		] newProcess.
+
+		downloadingProcess resume.
+		^true
