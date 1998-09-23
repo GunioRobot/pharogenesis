944292c5dd49426937be0026bcdebd626a36ac40
di <unknown>
1998-09-23 02:02:00 +0000

    no message

--- Utilities/class/support windows/openStandardWorkspace.cs
+++ Utilities/class/support windows/openStandardWorkspace.cs
@@ -2,6 +2,5 @@ openStandardWorkspace
 	"Open up a throwaway workspace with useful expressions in it.  1/22/96 sw"
 	"Utilities openStandardWorkspace"
 
-	Utilities
-		openScratchWorkspaceLabeled: ('Useful Expressions ', Date today printString)
-		contents: self standardWorkspaceContents.
+	(StringHolder new contents: self standardWorkspaceContents)
+		openLabel: 'Useful Expressions ', Date today printString
