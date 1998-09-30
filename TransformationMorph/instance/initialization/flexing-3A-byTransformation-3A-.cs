afd25217d93aa9786dcc50175742b83359630476
di <unknown>
1998-09-30 23:12:00 +0000

    no message

---
+++ TransformationMorph/instance/initialization/flexing-3A-byTransformation-3A-.cs
@@ -0,0 +1,10 @@
+flexing: aMorph byTransformation: tfm
+	"Initialize me with position and bounds of aMorph,
+	and with an offset that provides centered rotation."
+
+	(aMorph isKindOf: TransformationMorph)
+		ifTrue: [aMorph submorphsDo: [:m | self addMorph: m clone]]
+		ifFalse: [self addMorph: aMorph].
+	transform _ tfm.
+	self chooseSmoothing.
+	self layoutChanged.
