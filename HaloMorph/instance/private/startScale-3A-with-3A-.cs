f60234f0aeeadfd251920303d5180be944dc07a8
di <unknown>
1998-10-02 15:26:00 +0000

    no message

--- HaloMorph/instance/private/startScale-3A-with-3A-.cs
+++ HaloMorph/instance/private/startScale-3A-with-3A-.cs
@@ -4,7 +4,7 @@ startScale: evt with: scaleHandle
 	target isFlexMorph ifFalse: [target addFlexShell].
 	growingOrRotating _ true.
 	self removeAllHandlesBut: scaleHandle.  "remove all other handles"
-	positionOffset _ scaleHandle center - target bottomRight.
+	positionOffset _ 0@0.
 	target isAlignmentMorph
 		ifTrue: [minExtent _ target minWidth@target minHeight]
-		ifFalse: [minExtent _ 1@1].
+		ifFalse: [minExtent _ 3@3].
