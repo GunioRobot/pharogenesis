82c003fb2569d4cc14631750b0bde2ae0795ca64
di <unknown>
1998-10-02 08:54:00 +0000

    no message

--- MorphicTransform/instance/transformations/invertRect-3A-.cs
+++ MorphicTransform/instance/transformations/invertRect-3A-.cs
@@ -1,14 +1,4 @@
 invertRect: aRectangle
-	"Return a rectangle whose coordinates have been transformed
-	from local back to global coordinates.  NOTE: if the transformation
-	is not just a translation, then it will compute the bounding box
-	in global coordinates."
-	| outerRect |
-	self isPureTranslation
-	ifTrue:
-		[^ (self invert: aRectangle topLeft)
-			corner: (self invert: aRectangle bottomRight)]
-	ifFalse:
-		[outerRect _ Rectangle encompassing:
-			(aRectangle innerCorners collect: [:p | self invert: p]).
-		^ outerRect topLeft corner: outerRect bottomRight + (1@1)]
+
+	self error: 'method name changed to emphasize enclosing bounds'.
+	^ self invertBoundsRect: aRectangle
