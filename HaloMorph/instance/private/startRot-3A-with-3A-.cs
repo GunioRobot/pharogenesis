d60eca24100d28a157a80624a97fc5c91b5da013
di <unknown>
1998-10-02 14:03:00 +0000

    no message

--- HaloMorph/instance/private/startRot-3A-with-3A-.cs
+++ HaloMorph/instance/private/startRot-3A-with-3A-.cs
@@ -4,8 +4,7 @@ startRot: evt with: rotHandle
 	target isFlexMorph ifFalse: [target addFlexShell].
 	growingOrRotating _ true.
 	self removeAllHandlesBut: rotHandle.  "remove all other handles"
-	angleOffset _ rotHandle center - target referencePosition.
-	angleOffset _
-		Point
+	angleOffset _ evt cursorPoint - (target pointInWorld: target referencePosition).
+	angleOffset _ Point
 			r: angleOffset r
 			degrees: angleOffset degrees - target rotationDegrees.
