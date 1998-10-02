ddb4407029dedcefdbd79b576def2a985604d830
jm <unknown>
1998-10-02 16:24:00 +0000

    no message

---
+++ Celeste/class/sending/addMessageToInbox-3A-.cs
@@ -0,0 +1,6 @@
+addMessageToInbox: msgString
+	"Append a copy of the given message to the inbox file."
+
+	self inform:
+'The "keep" feature is not yet implemented;
+please cc: yourself to retain a copy of your message.'
