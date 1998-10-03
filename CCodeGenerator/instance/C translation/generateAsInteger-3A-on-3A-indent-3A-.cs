ddf07900f66aae6e934e41cd5b40b19950025552
ar <unknown>
1998-10-03 13:45:00 +0000

    no message

---
+++ CCodeGenerator/instance/C translation/generateAsInteger-3A-on-3A-indent-3A-.cs
@@ -0,0 +1,6 @@
+generateAsInteger: msgNode on: aStream indent: level
+	"Generate the C code for this message onto the given stream."
+
+	aStream nextPutAll:'((int) '.
+	self emitCExpression: msgNode receiver on: aStream.
+	aStream nextPutAll: ' )'.
