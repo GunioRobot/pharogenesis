cf02d75b6374b38843dcca8fb95329c541b3ace0
jm <unknown>
1998-10-03 08:48:00 +0000

    no message

--- PWS/class/Serving/doBackupJobs.cs
+++ PWS/class/Serving/doBackupJobs.cs
@@ -1,7 +1,11 @@
 doBackupJobs
 	"This is just a wrapper so we don't have to restart the server loop when we add/remove jobs."
 
+	| problemReport |
 	BackupJobs do: [:block |
-		[block value] ifError: [:msg :rec | ^'*** ', rec asString, ': ', msg asString ]].
+		problemReport _ nil.
+		[block value] ifError: [:msg :rec |
+			problemReport _ '*** ', rec asString, ': ', msg asString].
+		problemReport ifNotNil: [^ problemReport]].
 
 	^ 'Backup jobs completed'
