2935092eb5252cc8f214325a6ed79c6896140f20
di <unknown>
1998-10-05 13:53:00 +0000

    no message

--- TextMorph/instance/private/installEditor.cs
+++ TextMorph/instance/private/installEditor.cs
@@ -1,7 +1,4 @@
 installEditor
 	"Install an editor for my paragraph.  This constitutes 'hasFocus'."
 	editor ifNotNil: [^ editor].
-	editor _ TextMorphEditor new morph: self.
-	editor changeParagraph: self paragraph.
-	self selectionChanged.
-	^ editor
+	^ self installEditorToReplace: nil
