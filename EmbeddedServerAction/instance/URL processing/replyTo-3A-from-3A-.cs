c99d8f8d30e98a890e1a6e74074331a30c89e0a0
mjg <unknown>
1998-10-05 16:44:00 +0000

    no message

--- EmbeddedServerAction/instance/URL processing/replyTo-3A-from-3A-.cs
+++ EmbeddedServerAction/instance/URL processing/replyTo-3A-from-3A-.cs
@@ -1,9 +1,14 @@
 replyTo: pieces from: request
-	(StandardFileStream isAFileNamed: pieces) 
-	ifTrue:
-		[request reply: PWS success; reply: PWS contentHTML, PWS crlf.
-		request reply: (HTMLformatter evalEmbedded:
-			(FileStream fileNamed: pieces) contentsOfEntireFile with: request)]
-	ifFalse:
-		[request error: PWS notFound].
-
+	| theLast |
+	(StandardFileStream isAFileNamed: pieces)
+		ifTrue:
+			[theLast _ request message last asLowercase.
+			theLast = 'gif' ifTrue: [^ self process: request MIMEtype: 'image/gif'].
+			theLast = 'jpeg' ifTrue: [^ self process: request MIMEtype: 'image/jpeg'].
+			theLast = 'jpg' ifTrue: [^ self process: request MIMEtype: 'image/jpeg'].
+			theLast = 'jpe' ifTrue: [^ self process: request MIMEtype: 'image/jpeg'].
+			request reply: PWS success;
+			 reply: PWS contentHTML , PWS crlf.
+			request reply: (HTMLformatter evalEmbedded: 
+				(FileStream fileNamed: pieces) contentsOfEntireFile with: request)]
+		ifFalse: [request error: PWS notFound]
