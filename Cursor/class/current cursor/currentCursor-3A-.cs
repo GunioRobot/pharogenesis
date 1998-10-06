f49e3b5c94c99f0813fa850cefa04d39cd3ce0b0
di <unknown>
1998-10-06 13:57:00 +0000

    no message

--- Cursor/class/current cursor/currentCursor-3A-.cs
+++ Cursor/class/current cursor/currentCursor-3A-.cs
@@ -2,8 +2,7 @@ currentCursor: aCursor
 	"Make the instance of cursor, aCursor, be the current cursor. Display it. 
 	Create an error if the argument is not a Cursor."
 
-	aCursor class == self
-		ifTrue: 
-			[CurrentCursor _ aCursor.
-			aCursor beCursor]
+	(aCursor isKindOf: self)
+		ifTrue: [CurrentCursor _ aCursor.
+				aCursor beCursor]
 		ifFalse: [self error: 'The new cursor must be an instance of class Cursor']
