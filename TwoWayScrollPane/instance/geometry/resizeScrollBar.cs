cef6aebf1c7d31709cc5adeaecb075b3348add2b
RAA <unknown>
1998-10-06 19:00:00 +0000

    no message

---
+++ TwoWayScrollPane/instance/geometry/resizeScrollBar.cs
@@ -0,0 +1,4 @@
+resizeScrollBar
+	"used to handle left vs right scrollbar"
+	yScrollBar bounds: (bounds topLeft extent: 16 @ (bounds height - 16)).
+	xScrollBar bounds: ((bounds left + 16) @ (bounds bottom - 16)  extent: (bounds width - 16) @ 16).
