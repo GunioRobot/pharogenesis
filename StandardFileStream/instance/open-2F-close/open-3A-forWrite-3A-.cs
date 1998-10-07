d984cb7caabb786853027aa6198b30c876a0c0af
ar <unknown>
1998-10-07 14:44:00 +0000

    no message

--- StandardFileStream/instance/open-2F-close/open-3A-forWrite-3A-.cs
+++ StandardFileStream/instance/open-2F-close/open-3A-forWrite-3A-.cs
@@ -1,8 +1,9 @@
-open: aFileName forWrite: writeMode 
-	"Open the receiver.  If writeMode is true, allow write, else access will be read-only.  2/12/96 sw"
-	fileID _ self primOpen: aFileName writable: writeMode.
-	fileID == nil ifTrue: [^ nil].
-	name _ aFileName.
+open: fileName forWrite: writeMode 
+	"Open the file with the given name. If writeMode is true, allow writing, otherwise open the file in read-only mode."
+	"Changed to do a GC and retry before failing ar 3/21/98 17:25"
+	fileID _ self retryWithGC:[self primOpen: fileName writable: writeMode] until:[:id| id notNil].
+	fileID ifNil: [^ nil].  "allows sender to detect failure"
+	self register.
+	name _ fileName.
 	rwmode _ writeMode.
 	buffer1 _ String new: 1.
-	closed _ false
