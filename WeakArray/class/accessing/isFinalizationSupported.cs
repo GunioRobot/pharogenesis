4ec8276f384f35bc72f59d31954966deb35bce6f
ar <unknown>
1998-10-07 15:30:00 +0000

    no message

---
+++ WeakArray/class/accessing/isFinalizationSupported.cs
@@ -0,0 +1,14 @@
+isFinalizationSupported
+	"Check if this VM supports the finalization mechanism"
+	| tempObject |
+	IsFinalizationSupported ifNotNil:[^IsFinalizationSupported].
+	tempObject _ WeakArray new: 1.
+	"Check if the class format 4 is correctly understood by the VM.
+	If the weak class support is not installed then the VM will report
+	any weak class as containing 32bit words - not pointers"
+	(tempObject at: 1) = nil 
+		ifFalse:[^IsFinalizationSupported _false].
+	"Check if objects are correctly freed"
+	self pvtCreateTemporaryObjectIn: tempObject.
+	Smalltalk garbageCollect.
+	^IsFinalizationSupported _ (tempObject at: 1) == nil
