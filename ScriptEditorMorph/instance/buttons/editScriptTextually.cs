8ad773dbcf2da6ac88bda204a7798d21459e84e5
sw <unknown>
1998-10-08 09:19:00 +0000

    no message

--- ScriptEditorMorph/instance/buttons/editScriptTextually.cs
+++ ScriptEditorMorph/instance/buttons/editScriptTextually.cs
@@ -2,5 +2,8 @@ editScriptTextually
 	| newBrowser |
 	self becomeTextuallyCoded.
 	(newBrowser _ Browser new) setClass: playerScripted class selector: scriptName.
-	^ Browser openBrowserView: (newBrowser openMessageEditString: nil)
-		label: ('textual script for "', scriptName, '" in ', playerScripted externalName)
+	(Preferences valueOfFlag: #editPlayerScriptsInPlace)
+		ifTrue:
+			[self addTextualScript: newBrowser]
+		ifFalse:
+			[Browser openBrowserView: (newBrowser openMessageEditString: nil) label: ('textual script for "', scriptName, '" in ', playerScripted externalName)]
