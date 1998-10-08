a2e5feb3aa7c684ad4e33a8ee6fff8ac59ba06f8
jm <unknown>
1998-10-08 18:34:00 +0000

    no message

---
+++ MIDIInputParser/instance/private-2D-state machine/endSysExclusive-3A-.cs
@@ -0,0 +1,4 @@
+endSysExclusive: cmdByte
+	"Error! Received 'end system exclusive' command when not receiving system exclusive data."
+
+	self error: 'unexpected ''End of System Exclusive'' command'.
