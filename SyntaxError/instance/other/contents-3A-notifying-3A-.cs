8b48077654eb51ec62911c479fc78ef85347ce7a
di <unknown>
1998-10-09 16:36:00 +0000

    no message

--- SyntaxError/instance/other/contents-3A-notifying-3A-.cs
+++ SyntaxError/instance/other/contents-3A-notifying-3A-.cs
@@ -1,6 +1,11 @@
 contents: aString notifying: aController
 	"Compile the code in aString and notify aController of any errors. If there are no errors, then automatically proceed."
 
-	(class compile: aString classified: category notifying: aController)
-		ifNil: [^ false]
-		ifNotNil: [self proceed].
+	doitFlag
+	ifTrue: [Compiler new evaluate: aString in: nil to: nil
+						notifying: aController ifFail: [^ false]]
+	ifFalse: [(class compile: aString classified: category
+						notifying: aController) ifNil: [^ false]].
+
+	aController hasUnacceptedEdits: false.
+	self proceed
