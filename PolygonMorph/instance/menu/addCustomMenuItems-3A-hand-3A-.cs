e1090680a14ac09868094d9607314d4916c6bdfa
di <unknown>
1998-10-09 14:30:00 +0000

    no message

--- PolygonMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ PolygonMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,8 +1,14 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
-	handles == nil ifTrue: [aCustomMenu add: 'show handles' action: #addHandles]
+	handles == nil
+		ifTrue: [aCustomMenu add: 'show handles' action: #addHandles]
 		ifFalse: [aCustomMenu add: 'hide handles' action: #removeHandles].
-	closed ifTrue: [aCustomMenu add: 'open polygon' action: #makeOpen]
+	closed
+		ifTrue:
+		[aCustomMenu add: 'open polygon' action: #makeOpen.
+		quickFill
+			ifTrue: [aCustomMenu add: 'proper fill' selector: #quickFill: argument: false]
+			ifFalse: [aCustomMenu add: 'quick fill' selector: #quickFill: argument: true]]
 		ifFalse:
 		[aCustomMenu add: 'close polygon' action: #makeClosed.
 		arrows == #none ifFalse: [aCustomMenu add: '---' action: #makeNoArrows].
