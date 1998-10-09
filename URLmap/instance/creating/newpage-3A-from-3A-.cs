e09286a68421be5a9eb0809787f517fe2ef2c04b
mjg <unknown>
1998-10-09 13:06:00 +0000

    no message

--- URLmap/instance/creating/newpage-3A-from-3A-.cs
+++ URLmap/instance/creating/newpage-3A-from-3A-.cs
@@ -1,16 +1,18 @@
 newpage: label from: peer
 	| newpage newfile |
-	newfile _ ((pages inject: 0 into: [:max :p | 
-		p coreID asNumber > max ifTrue: [p coreID asNumber] 
+	newfile _ ((pages inject: 0 into: [:max :p |
+		p coreID asNumber > max ifTrue: [p coreID asNumber]
 				ifFalse: [max]]) + 1) printString.
-	newpage _ SwikiPage new.
+	newpage _ action class pageClass new.
 	self at: label put: newpage.
 	newpage address: peer.
 	newpage date: (Date today).
+	newpage time: (Time now).
 	newpage coreID: newfile.
 	newpage name: label.
-	newpage file: ((ServerAction serverDirectory), 
+	newpage file: ((ServerAction serverDirectory),
 		directory, (ServerAction pathSeparator), newfile).
+	newpage pageStatus: #new.
 	newpage text: 'Describe ',label,' here'.
 	newpage map: self.
 	newpage url: (action name),'.',newfile.
