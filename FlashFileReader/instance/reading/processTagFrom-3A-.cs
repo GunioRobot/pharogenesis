4af8a474c097dbade76f4125a2d8dc5801577659
ar <unknown>
1998-10-12 23:57:00 +0000

    no message

---
+++ FlashFileReader/instance/reading/processTagFrom-3A-.cs
@@ -0,0 +1,18 @@
+processTagFrom: aStream
+	"Read and process the next tag from the input stream."
+	| tag data result |
+	tag _ aStream nextTag.
+	log ifNotNil:[
+		log cr; nextPutAll:'Tag #'; print: tag key.
+		log nextPutAll:' ('; nextPutAll: (TagTable at: tag key + 1); space; print: tag value size;
+			nextPutAll:' bytes)'.
+		self flushLog].
+	data _ FlashFileStream on: (ReadStream on: tag value).
+	result _ self dispatch: data on: tag key+1 in: TagTable ifNone:[self processUnknown: data].
+	(log isNil or:[data atEnd]) ifFalse:[
+		log 
+			nextPutAll:'*** ';
+			print: (data size - data position);
+			nextPutAll:' bytes skipped ***'.
+		self flushLog].
+	^result
