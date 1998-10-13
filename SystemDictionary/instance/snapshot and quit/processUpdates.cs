0aef231235dbea543147aa0366fce3fd2e2cacae
sw <unknown>
1998-10-13 16:06:00 +0000

    no message

--- SystemDictionary/instance/snapshot and quit/processUpdates.cs
+++ SystemDictionary/instance/snapshot and quit/processUpdates.cs
@@ -1,8 +1,8 @@
 processUpdates
-	"Process update files from a well-known update server."
+	"Process update files from a well-known update server.  This method is called at system startup time,   Only if the preference #updateFromServerAtStartup is true is the actual update processing undertaken automatically"
 
 	| choice |
-	(Preferences valueOfFlag: #updateFromServer) ifTrue: [
-		choice _ (PopUpMenu labels: 'Yes, Update\No, Not now' withCRs)
+	(Preferences valueOfFlag: #updateFromServerAtStartup) ifTrue:
+		[choice _ (PopUpMenu labels: 'Yes, Update\No, Not now' withCRs)
 			startUpWithCaption: 'Shall I look for new code\updates on the server?' withCRs.
-		choice = 1 ifTrue: [Utilities absorbUpdatesFromServer]].
+		choice = 1 ifTrue: [Utilities updateFromServer]].
