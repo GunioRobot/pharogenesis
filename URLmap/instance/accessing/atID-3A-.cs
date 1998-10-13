4370aae38427ef6ac0910a6dfd56a4ca9481943d
JZH <unknown>
1998-10-13 22:56:00 +0000

    no message

--- URLmap/instance/accessing/atID-3A-.cs
+++ URLmap/instance/accessing/atID-3A-.cs
@@ -1,6 +1,6 @@
 atID: id
 	"Return page of a given key."
-
 	| idString |
 	idString _ id isInteger ifTrue: [id printString] ifFalse: [id].
-	^pages detect: [:page | page coreID = idString] ifNone: [pages at: 'front page']
+	^ pages detect: [:page | page coreID = idString] ifNone: [self
+atID: '1']
