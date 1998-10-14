08b8704cbc2ae7e6db1ddce93895fab8dbb42012
jm <unknown>
1998-10-14 13:49:00 +0000

    no message

---
+++ MIDISynthChannel/instance/midi dispatching/keyUp-3A-vel-3A-.cs
@@ -0,0 +1,9 @@
+keyUp: key vel: vel
+	"Handle a key up event."
+
+	| snd |
+	activeSounds copy do: [:entry |
+		(entry at: 1) = key ifTrue: [
+			snd _ entry at: 2.
+			snd stopGracefully.
+			activeSounds remove: entry]].
