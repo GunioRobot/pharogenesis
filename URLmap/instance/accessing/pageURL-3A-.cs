4d5983fe7a8f682ee2466f6e681b30bc84a2fcc1
mjg <unknown>
1998-10-14 14:38:00 +0000

    no message

--- URLmap/instance/accessing/pageURL-3A-.cs
+++ URLmap/instance/accessing/pageURL-3A-.cs
@@ -1,2 +1,14 @@
 pageURL: aPage
-	^'<a href="',(action name),'.',aPage coreID,'">',(aPage name),'</a>'
+	(aPage pageStatus = #new)
+		ifTrue:
+			[(action isKindOf: CachedSwikiAction)
+			ifFalse: [^'<u>',(aPage name),'</u><a href="', 
+				(action pageURL: aPage),'.edit">?</a>']
+			ifTrue: [^'<u>',(aPage name),'</u><a href="',
+				(action pwsURL),(action name),'.',(aPage coreID),'.edit">?</a>']
+				"Underlines new but not yet edited pages"]
+		ifFalse:
+			[^'<a href="', (action pageURL: aPage),'">',(aPage name),'</a>']
+
+"Logically, there should be editPageUrl in SwikiAction as well. After all,
+we might want to define other URL schemes." 
