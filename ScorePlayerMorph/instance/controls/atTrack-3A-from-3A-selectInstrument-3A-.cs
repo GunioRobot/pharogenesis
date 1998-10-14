b60a839d6fcbf7f5a753ed4f3a1e0d94b4c80ee9
jm <unknown>
1998-10-14 15:53:00 +0000

    no message

---
+++ ScorePlayerMorph/instance/controls/atTrack-3A-from-3A-selectInstrument-3A-.cs
@@ -0,0 +1,29 @@
+atTrack: trackIndex from: aPopUpChoice selectInstrument: selection
+
+	| oldSnd name snd |
+	oldSnd _ scorePlayer instrumentForTrack: trackIndex.
+	(selection beginsWith: 'edit ') ifTrue: [
+		name _ selection copyFrom: 6 to: selection size.
+		aPopUpChoice contentsClipped: name.
+		(oldSnd isKindOf: FMSound) | (oldSnd isKindOf: LoopedSampledSound) ifTrue: [
+			EnvelopeEditorMorph openOn: oldSnd title: name].
+		(oldSnd isKindOf: SampledInstrument) ifTrue: [
+			EnvelopeEditorMorph openOn: oldSnd allNotes first title: name].
+		^ self].
+
+	snd _ nil.
+	1 to: instrumentSelector size do: [:i |
+		((trackIndex ~= i) and:
+		 [selection = (instrumentSelector at: i) contents])
+			ifTrue: [snd _ scorePlayer instrumentForTrack: i]].  "use existing instrument prototype"
+
+	snd ifNil: [
+		selection = 'clink'
+			ifTrue: [
+				snd _ (SampledSound
+					samples: SampledSound coffeeCupClink
+					samplingRate: 11025) copy]
+			ifFalse: [snd _ (AbstractSound soundNamed: selection) copy]].
+
+	scorePlayer instrumentForTrack: trackIndex put: snd.
+	(instrumentSelector at: trackIndex) contentsClipped: selection.
