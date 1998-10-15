54d3070aa445353ba6a136f584d978561d1769e8
ar <unknown>
1998-10-15 01:18:00 +0000

    no message

---
+++ FlashFileReader/class/class initialization/initializeIndexTables.cs
@@ -0,0 +1,10 @@
+initializeIndexTables
+	IndexTables _ Array new: 4.
+	IndexTables at: 1 put:
+		#(-1 2).
+	IndexTables at: 2 put:
+		#(-1 -1 2 4).
+	IndexTables at: 3 put:
+		#(-1 -1 -1 -1 2 4 6 8).
+	IndexTables at: 4 put:
+		#(-1 -1 -1 -1 -1 -1 -1 -1 1 2 4 6 8 10 13 16).
