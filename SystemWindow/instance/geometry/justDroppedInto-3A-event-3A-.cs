938a0c1689970f007d2dbcd9957248c7daa24cfc
sw <unknown>
1998-10-15 11:27:00 +0000

    no message

--- SystemWindow/instance/geometry/justDroppedInto-3A-event-3A-.cs
+++ SystemWindow/instance/geometry/justDroppedInto-3A-event-3A-.cs
@@ -2,4 +2,5 @@ justDroppedInto: aMorph event: anEvent
 	isCollapsed
 		ifTrue: [self position: ((self position max: 0@0) grid: 8@8).
 				collapsedFrame _ self bounds]
-		ifFalse: [fullFrame _ self bounds]
+		ifFalse: [fullFrame _ self bounds.
+				TopWindow ~~ self ifTrue: [self activate]]
