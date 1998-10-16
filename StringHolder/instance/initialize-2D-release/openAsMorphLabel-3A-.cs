bdb7ebbb57e3cc078560168273c4949a760b4008
sw <unknown>
1998-10-16 11:37:00 +0000

    no message

--- StringHolder/instance/initialize-2D-release/openAsMorphLabel-3A-.cs
+++ StringHolder/instance/initialize-2D-release/openAsMorphLabel-3A-.cs
@@ -1,10 +1,3 @@
 openAsMorphLabel: labelString 
 	"Workspace new openAsMorphLabel: 'Workspace'"
-	| window |
-	window _ (SystemWindow labelled: labelString) model: self.
-
-	window addMorph: (PluggableTextMorph on: self text: #contents accept: #acceptContents:
-			readSelection: nil menu: nil)
-		frame: (0@0 corner: 1@1).
-
-	window openInWorld
+	(self embeddedInMorphicWindowLabeled: labelString) openInWorld
