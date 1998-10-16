bcf77a26b7765f14643f4c7d34573c2edd12afc9
ar <unknown>
1998-10-16 01:26:00 +0000

    no message

---
+++ FlashFileWriter/instance/writing/writeHeader-3A-rate-3A-.cs
@@ -0,0 +1,9 @@
+writeHeader: bounds rate: frameRate
+	"Read header information from the source stream.
+	Return true if successful, false otherwise."
+	self halt.
+	self writeSignature.
+	stream nextBytePut: 3. "Always write flash3"
+	dataSize _ stream nextLongPut: 0.	"Place holder for data size"
+	stream nextRectPut: bounds.
+	stream nextWordPut: (frameRate * 256) truncated.
