ba55f6d0212c6968ec47a890860319909bd2cc13
sw <unknown>
1998-10-21 15:55:00 +0000

    no message

--- TileMorph/instance/accessing/playerBearingCode.cs
+++ TileMorph/instance/accessing/playerBearingCode.cs
@@ -2,4 +2,4 @@ playerBearingCode
 	"Answer the actual Player object who will be the 'self' when the receiver is being asked to generate code"
 	| anEditor |
 	(anEditor _ self topEditor) ifNotNil: [^ anEditor playerScripted].
-	^ (self nearestOwnerThat: [:m | m isKindOf: PartsViewer]) scriptedPlayer
+	^ (self nearestOwnerThat: [:m | m isAViewer]) scriptedPlayer
