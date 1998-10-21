19697899e801ce0a2b7bd9bafc505db940d8b33c
sw <unknown>
1998-10-21 15:27:00 +0000

    no message

---
+++ Player/instance/slots-2D-kernel/slotPhraseFor-3A-inViewer-3A-.cs
@@ -0,0 +1,2 @@
+slotPhraseFor: aSpec inViewer: aViewer
+	^ aViewer phraseForSlot: aSpec second type: aSpec third readOnly: aSpec fourth == #readOnly getSelector: aSpec fifth putSelector: aSpec sixth
