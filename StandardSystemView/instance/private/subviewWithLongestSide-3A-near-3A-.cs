8862affbad04b3c595329961c5ade4e471b7ee4a
di <unknown>
1998-10-21 16:12:00 +0000

    no message

--- StandardSystemView/instance/private/subviewWithLongestSide-3A-near-3A-.cs
+++ StandardSystemView/instance/private/subviewWithLongestSide-3A-near-3A-.cs
@@ -1,15 +1,13 @@
 subviewWithLongestSide: sideBlock near: aPoint 
-	| region subs max rect side len theSub theSide |
-	region _ aPoint - (4@4) corner: aPoint + (4@4).
-	subs _ subViews select: [:sub | sub insetDisplayBox intersects: region].
-	subs isEmpty ifTrue: [sideBlock value: #none.  ^ nil].
-	max _ 0.
-	subs do:
-		[:sub | rect _ sub insetDisplayBox.
-		side _ rect sideNearestTo: aPoint.
-		len _ (side = #left) | (side = #right)
-			ifTrue: [rect height]
-			ifFalse: [rect width].
-		len > max ifTrue: [max _ len.  theSub _ sub.  theSide _ side]].
+	| theSub theSide theLen box |
+	theLen _ 0.
+	subViews do:
+		[:sub | box _ sub insetDisplayBox.
+		box forPoint: aPoint closestSideDistLen:
+			[:side :dist :len |
+			(dist <= 5 and: [len > theLen]) ifTrue:
+				[theSub _ sub.
+				theSide _ side.
+				theLen _ len]]].
 	sideBlock value: theSide.
 	^ theSub
