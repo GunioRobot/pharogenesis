f13d734a16b4d190733a2c6c753a9323fde62356
di <unknown>
1998-10-22 16:15:00 +0000

    no message

--- StandardSystemView/instance/framing/reframePanesAdjoining-3A-along-3A-to-3A-.cs
+++ StandardSystemView/instance/framing/reframePanesAdjoining-3A-along-3A-to-3A-.cs
@@ -1,13 +1,14 @@
 reframePanesAdjoining: subView along: side to: aDisplayBox 
-	| newBox delta newRect minDim |
+	| newBox delta newRect minDim theMin |
 	newRect _ aDisplayBox.
-	"First check that this won't make any pane smaller than 8 screen dots"
+	theMin _ 16.
+	"First check that this won't make any pane smaller than theMin screen dots"
 	minDim _ ((subViews select: [:sub | sub displayBox bordersOn: subView displayBox along: side])
-		collect: [:sub | self newDisplayBoxFor: sub adjoining: newRect along: side])
+		collect: [:sub | sub displayBox adjustTo: newRect along: side])
 			inject: 999 into: [:was :rect | (was min: rect width) min: rect height].
 	"If so, amend newRect as required"
-	minDim < 8 ifTrue:
-		[delta _ minDim - 8.
+	minDim < theMin ifTrue:
+		[delta _ minDim - theMin.
 		newRect _ newRect withSide: side setTo: 
 				((newRect perform: side) > (subView displayBox perform: side)
 					ifTrue: [(newRect perform: side) + delta]
@@ -15,7 +16,7 @@ reframePanesAdjoining: subView along: side to: aDisplayBox
 	"Now adjust all adjoining panes for real"
 	subViews do:
 		[:sub | (sub displayBox bordersOn: subView displayBox along: side) ifTrue:
-			[newBox _ self newDisplayBoxFor: sub adjoining: newRect along: side.
+			[newBox _ sub displayBox adjustTo: newRect along: side.
 			sub window: sub window viewport:
 				(sub transform: (sub inverseDisplayTransform: newBox)) rounded]].
 	"And adjust the growing pane itself"
