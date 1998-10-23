42350b78874adc7c9a2a389de0c83f541dfd476f
sw <unknown>
1998-10-23 15:57:00 +0000

    no message

--- StringButtonMorph/instance/events/mouseMove-3A-.cs
+++ StringButtonMorph/instance/events/mouseMove-3A-.cs
@@ -1,7 +1,7 @@
 mouseMove: evt
-
+	actWhen == #buttonDown ifTrue: [^ self].
 	(self containsPoint: evt cursorPoint)
-		ifTrue: [
-			self color: (oldColor mixed: 1/2 with: Color white).
+		ifTrue:
+			[self color: (oldColor mixed: 1/2 with: Color white).
 			actWhen == #whilePressed ifTrue: [self doButtonAction]]
 		ifFalse: [self color: oldColor].
