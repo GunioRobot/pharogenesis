61f66325e479b9f525a08f494c180c8bbf2e5e24
sw <unknown>
1998-10-29 16:06:00 +0000

    no message

--- Presenter/instance/tile support/valueTiles.cs
+++ Presenter/instance/tile support/valueTiles.cs
@@ -1,16 +1,11 @@
 valueTiles
 	| tiles |
 	tiles _ OrderedCollection new.
-	tiles add: (5 newTileMorphRepresentative typeColor: (TilePadMorph colorForType: #number)).
-	tiles add: ((4 @ 9) newTileMorphRepresentative typeColor: (TilePadMorph colorForType: #point)).
-	tiles add: (ColorTileMorph new typeColor: (TilePadMorph colorForType: #color)).
-	tiles add: (TileMorph new typeColor: (TilePadMorph colorForType: #number);
+	tiles add: (5 newTileMorphRepresentative typeColor: (ScriptingSystem colorForType: #number)).
+	tiles add: ((4 @ 9) newTileMorphRepresentative typeColor: (ScriptingSystem colorForType: #point)).
+	tiles add: (ColorTileMorph new typeColor: (ScriptingSystem colorForType: #color)).
+	tiles add: (TileMorph new typeColor: (ScriptingSystem colorForType: #number);
 			setExpression: '(180 atRandom)'
 			label: 'random').
-	tiles add: ((TileMorph new typeColor: (TilePadMorph colorForType: #number))
-				setExpression: '(self getMouseX)' label: 'mouseX').
-
-	tiles add: ((TileMorph new typeColor: (TilePadMorph colorForType: #number))
-		setExpression: '(self getMouseY)' label: 'mouseY').
-
+	tiles add: RandomNumberTile new.
 	^ tiles
