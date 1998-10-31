7ca6d49ab26fa951fc2f5e0849287a72f6816f76
di <unknown>
1998-10-31 12:19:00 +0000

    no message

---
+++ FWT/instance/access/coeffs.cs
@@ -0,0 +1,13 @@
+coeffs
+	"Return all coefficients neede to reconstruct the original samples"
+	| header csize strm |
+	header _ Array with: nSamples with: nLevels with: alpha with: beta.
+	csize _ header size.
+	1 to: nLevels do: [:i | csize _ csize + (transform at: i*2) size].
+	csize _ csize + (transform at: nLevels*2-1) size.
+	coeffs _ Array new: csize.
+	strm _ WriteStream on: coeffs.
+	strm nextPutAll: header.
+	1 to: nLevels do: [:i | strm nextPutAll: (transform at: i*2)].
+	strm nextPutAll: (transform at: nLevels*2-1).
+	^ coeffs
