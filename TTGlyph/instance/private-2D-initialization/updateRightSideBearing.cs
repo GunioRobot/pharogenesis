7f4be5176b409031250c7f3aa93083d74eca3611
ar <unknown>
1998-11-01 22:27:00 +0000

    no message

---
+++ TTGlyph/instance/private-2D-initialization/updateRightSideBearing.cs
@@ -0,0 +1,4 @@
+updateRightSideBearing
+	"Update the right side bearing value"
+	"@@: Is the following really correct?!?!"
+	rightSideBearing _ advanceWidth - leftSideBearing - bounds corner x + bounds origin x
