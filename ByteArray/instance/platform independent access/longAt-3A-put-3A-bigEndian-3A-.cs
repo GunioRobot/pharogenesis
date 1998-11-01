513c9550aab82e02149962bf737aab7298320baf
ar <unknown>
1998-11-01 20:47:00 +0000

    no message

---
+++ ByteArray/instance/platform independent access/longAt-3A-put-3A-bigEndian-3A-.cs
@@ -0,0 +1,20 @@
+longAt: index put: value bigEndian: aBool
+	"Return a 32bit integer quantity starting from the given byte index"
+	| b0 b1 b2 b3 |
+	b0 _ value bitShift: -24.
+	b0 _ (b0 bitAnd: 16r7F) - (b0 bitAnd: 16r80).
+	b1 _ (value bitShift: -16) bitAnd: 255.
+	b2 _ (value bitShift: -8) bitAnd: 255.
+	b3 _ value bitAnd: 255.
+	aBool ifTrue:[
+		self at: index put: b0.
+		self at: index+1 put: b1.
+		self at: index+2 put: b2.
+		self at: index+3 put: b3.
+	] ifFalse:[
+		self at: index put: b3.
+		self at: index+1 put: b2.
+		self at: index+2 put: b1.
+		self at: index+3 put: b0.
+	].
+	^value
