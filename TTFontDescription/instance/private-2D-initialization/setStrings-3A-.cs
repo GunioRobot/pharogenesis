9aff5724315e961be865d33a4846976a49f4d88d
ar <unknown>
1998-11-02 00:46:00 +0000

    no message

---
+++ TTFontDescription/instance/private-2D-initialization/setStrings-3A-.cs
@@ -0,0 +1,9 @@
+setStrings: anArray
+	copyright _ anArray at: 1.
+	familyName _ anArray at: 2.
+	subfamilyName _ anArray at: 3.
+	uniqueName _ anArray at: 4.
+	fullName _ anArray at: 5.
+	versionName _ anArray at: 6.
+	postscriptName _ anArray at: 7.
+	trademark _ anArray at: 8.
