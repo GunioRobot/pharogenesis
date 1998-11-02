efc6934826028ba2a7a8819090dff194fcbfe914
sw <unknown>
1998-11-02 15:35:00 +0000

    no message

--- View/instance/bordering/foregroundColor.cs
+++ View/instance/bordering/foregroundColor.cs
@@ -1,7 +1,5 @@
 foregroundColor
-	borderColor == nil ifFalse:
-		[(borderColor isMemberOf: Symbol) ifTrue:
-			[^ Color perform: borderColor].
-		^ borderColor].
-	superView == nil ifFalse: [^ superView foregroundColor].
-	^ Color black
+	borderColor ifNotNil: [^ Color colorFrom: borderColor].
+	^ superView == nil
+		ifFalse: [superView foregroundColor]
+		ifTrue:	[Color black]
