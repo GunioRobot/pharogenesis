0d406c5919a8014735d7614785f407f9f325f87a
sw <unknown>
1998-11-02 15:34:00 +0000

    no message

--- View/instance/bordering/backgroundColor.cs
+++ View/instance/bordering/backgroundColor.cs
@@ -1,8 +1,6 @@
 backgroundColor
 	Display depth <= 2 ifTrue: [^ Color white].
-	insideColor == nil ifFalse:
-		[(insideColor isMemberOf: Symbol) ifTrue:
-			[^ Color perform: insideColor].
-		^ insideColor].
-	superView == nil ifFalse: [^ superView backgroundColor].
-	^ Color white
+	insideColor ifNotNil: [^ Color colorFrom: insideColor].
+	^ superView == nil
+		ifFalse: [superView backgroundColor]
+		ifTrue:	[Color white]
