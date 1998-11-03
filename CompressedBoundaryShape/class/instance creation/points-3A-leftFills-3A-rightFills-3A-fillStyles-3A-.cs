92df761f3a558669034d067bb8aba357cc15986b
ar <unknown>
1998-11-03 16:28:00 +0000

    no message

---
+++ CompressedBoundaryShape/class/instance creation/points-3A-leftFills-3A-rightFills-3A-fillStyles-3A-.cs
@@ -0,0 +1,2 @@
+points: pointList leftFills: leftFillList rightFills: rightFillList fillStyles: fillStyleList
+	^self new setPoints: pointList leftFills: leftFillList rightFills: rightFillList fillStyles: fillStyleList
