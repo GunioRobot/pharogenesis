182812f47db70d50ae0ffde6e8e7b24ae93e3177
ar <unknown>
1998-11-04 21:52:00 +0000

    no message

---
+++ BalloonEngineBase/class/class initialization/initializeEdgeInitTable.cs
@@ -0,0 +1,13 @@
+initializeEdgeInitTable
+	"BalloonEngineBase initialize"
+	^#(
+		errorWrongIndex
+		errorWrongIndex
+		errorWrongIndex
+		errorWrongIndex
+
+		stepToFirstLine
+		stepToFirstWideLine
+		stepToFirstBezier
+		stepToFirstWideBezier
+	)
