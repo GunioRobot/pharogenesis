f6d51bb86ecc0b4c5222682bbd0f735adf5b79d5
di <unknown>
1998-11-06 14:00:00 +0000

    no message

--- Integer/instance/arithmetic/quo-3A-.cs
+++ Integer/instance/arithmetic/quo-3A-.cs
@@ -1,9 +1,12 @@
 quo: aNumber 
 	"Refer to the comment in Number quo: "
 	| ng quo |
-	aNumber isInteger
-		ifTrue: 
-			[ng _ self negative == aNumber negative == false.
-			quo _ (self digitDiv: aNumber neg: ng) at: 1.
-			^ quo normalize]
-		ifFalse: [^ (aNumber adaptInteger: self) quo: aNumber adaptToInteger]
+	aNumber isInteger ifTrue: 
+		[ng _ self negative == aNumber negative == false.
+		quo _ (self digitDiv:
+			(aNumber class == SmallInteger
+				ifTrue: [aNumber abs]
+				ifFalse: [aNumber])
+			neg: ng) at: 1.
+		^ quo normalize].
+	^ aNumber adaptToInteger: self andSend: #quo:
