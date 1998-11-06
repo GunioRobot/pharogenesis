da7d3db67a7ab1ff38b9fe709fd161053b5f0476
di <unknown>
1998-11-06 13:58:00 +0000

    no message

--- Fraction/instance/arithmetic/+.cs
+++ Fraction/instance/arithmetic/+.cs
@@ -1,13 +1,11 @@
 + aNumber 
 	"Answer the sum of the receiver and aNumber."
 	| n d d1 d2 |
-	aNumber isFraction
-		ifTrue: 
-			[d _ denominator gcd: aNumber denominator.
-			n _ numerator * (d1 _ aNumber denominator // d) + (aNumber numerator * (d2 _ denominator // d)).
-			d1 _ d1 * d2.
-			n _ n // (d2 _ n gcd: d).
-			(d _ d1 * (d // d2)) = 1 ifTrue: [^ n].
-			^ Fraction numerator: n denominator: d]
-		ifFalse: [^ (aNumber adaptFraction: self)
-				+ aNumber adaptToFraction]
+	aNumber isFraction ifTrue: 
+		[d _ denominator gcd: aNumber denominator.
+		n _ numerator * (d1 _ aNumber denominator // d) + (aNumber numerator * (d2 _ denominator // d)).
+		d1 _ d1 * d2.
+		n _ n // (d2 _ n gcd: d).
+		(d _ d1 * (d // d2)) = 1 ifTrue: [^ n].
+		^ Fraction numerator: n denominator: d].
+	^ aNumber adaptToFraction: self andSend: #+
