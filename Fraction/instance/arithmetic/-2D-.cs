2636f51d9d278f22a4e9c38fff7fa8c919227cbd
di <unknown>
1998-11-06 13:58:00 +0000

    no message

--- Fraction/instance/arithmetic/-2D-.cs
+++ Fraction/instance/arithmetic/-2D-.cs
@@ -1,5 +1,5 @@
 - aNumber
 	"Answer the difference between the receiver and aNumber."
-	aNumber isFraction
-		ifTrue: [^ self + aNumber negated]
-		ifFalse: [^ (aNumber adaptFraction: self) - aNumber adaptToFraction]
+	aNumber isFraction ifTrue:
+		[^ self + aNumber negated].
+	^ aNumber adaptToFraction: self andSend: #-
