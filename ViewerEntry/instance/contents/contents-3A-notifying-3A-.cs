102d90de26fe279a10b97f9420bd4fad9e021cd1
sw <unknown>
1998-11-06 11:14:00 +0000

    no message

---
+++ ViewerEntry/instance/contents/contents-3A-notifying-3A-.cs
@@ -0,0 +1,9 @@
+contents: c notifying: k
+	"later, spruce this up so that it can accept input such as new method source"
+	| info |
+	(info _ self userSlotInformation)
+		ifNotNil:
+			[info documentation: c.
+			^ true].
+	self beep.
+	^ false
