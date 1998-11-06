5c2e344f5d2493801a18b40bd971af645408662e
di <unknown>
1998-11-06 14:00:00 +0000

    no message

--- Integer/instance/arithmetic/-2F-.cs
+++ Integer/instance/arithmetic/-2F-.cs
@@ -1,10 +1,10 @@
 / aNumber
 	"Refer to the comment in Number / "
 	| quoRem |
-	aNumber isInteger
-		ifTrue: [quoRem _ self digitDiv: aNumber 
-								neg: self negative ~~ aNumber negative.
-				(quoRem at: 2) = 0
-					ifTrue: [^(quoRem at: 1) normalize]
-					ifFalse: [^(Fraction numerator: self denominator: aNumber) reduced]]
-		ifFalse: [^ (aNumber adaptInteger: self) / aNumber adaptToInteger]
+	aNumber isInteger ifTrue:
+		[quoRem _ self digitDiv: aNumber abs	"*****I've added abs here*****"
+						neg: self negative ~~ aNumber negative.
+		(quoRem at: 2) = 0
+			ifTrue: [^ (quoRem at: 1) normalize]
+			ifFalse: [^ (Fraction numerator: self denominator: aNumber) reduced]].
+	^ aNumber adaptToInteger: self andSend: #/
