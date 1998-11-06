21fba0fec2e7ee347e4666dfd99f80000f0492b5
di <unknown>
1998-11-06 14:00:00 +0000

    no message

--- Integer/instance/comparing/<.cs
+++ Integer/instance/comparing/<.cs
@@ -1,8 +1,8 @@
 < aNumber
-	aNumber isInteger
-		ifTrue: [self negative == aNumber negative
-					ifTrue: [self negative
-								ifTrue: [^(self digitCompare: aNumber) > 0]
-								ifFalse: [^(self digitCompare: aNumber) < 0]]
-					ifFalse: [^self negative]]
-		ifFalse: [^ (aNumber adaptInteger: self) < aNumber adaptToInteger]
+	aNumber isInteger ifTrue:
+		[self negative == aNumber negative
+			ifTrue: [self negative
+						ifTrue: [^ (self digitCompare: aNumber) > 0]
+						ifFalse: [^ (self digitCompare: aNumber) < 0]]
+			ifFalse: [^ self negative]].
+	^ aNumber adaptToInteger: self andSend: #<
