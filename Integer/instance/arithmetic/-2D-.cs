13bb12dbda9d9784874fae4a6020b6d95c3f9cd8
di <unknown>
1998-11-06 13:59:00 +0000

    no message

--- Integer/instance/arithmetic/-2D-.cs
+++ Integer/instance/arithmetic/-2D-.cs
@@ -1,7 +1,7 @@
 - aNumber
 	"Refer to the comment in Number - "
-	aNumber isInteger
-		ifTrue: [self negative == aNumber negative
-					ifTrue: [^ self digitSubtract: aNumber]
-					ifFalse: [^ (self digitAdd: aNumber) normalize]]
-		ifFalse: [^ (aNumber adaptInteger: self) - aNumber adaptToInteger]
+	aNumber isInteger ifTrue:
+		[self negative == aNumber negative
+			ifTrue: [^ self digitSubtract: aNumber]
+			ifFalse: [^ (self digitAdd: aNumber) normalize]].
+	^ aNumber adaptToInteger: self andSend: #-
