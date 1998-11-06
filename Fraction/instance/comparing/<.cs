a600d1c303fbed0c684dbb5862a250e8abe52223
di <unknown>
1998-11-06 13:58:00 +0000

    no message

--- Fraction/instance/comparing/<.cs
+++ Fraction/instance/comparing/<.cs
@@ -1,5 +1,4 @@
 < aNumber
-	aNumber isFraction
-		ifTrue: [^ numerator * aNumber denominator < (aNumber numerator * denominator)]
-		ifFalse: [^ (aNumber adaptFraction: self)
-					< aNumber adaptToFraction]
+	aNumber isFraction ifTrue:
+		[^ numerator * aNumber denominator < (aNumber numerator * denominator)].
+	^ aNumber adaptToFraction: self andSend: #<
