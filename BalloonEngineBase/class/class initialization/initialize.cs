954abfbcb42e5fbc7740178ebd413c2e24740b49
ar <unknown>
1998-11-07 22:26:00 +0000

    no message

---
+++ BalloonEngineBase/class/class initialization/initialize.cs
@@ -0,0 +1,8 @@
+initialize
+	"BalloonEngineBase initialize"
+	"BalloonEnginePlugin translateDoInlining: true."
+	EdgeInitTable _ self initializeEdgeInitTable.
+	EdgeStepTable _ self initializeEdgeStepTable.
+	WideLineWidthTable _ self initializeWideLineWidthTable.
+	WideLineFillTable _ self initializeWideLineFillTable.
+	FillTable _ self initializeFillTable.
