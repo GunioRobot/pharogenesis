03d3d156c33d22748adf15e697a56ca056b3ebe9
jm <unknown>
1998-11-09 10:16:00 +0000

    no message

--- BlockContext/instance/scheduling/forkAt-3A-.cs
+++ BlockContext/instance/scheduling/forkAt-3A-.cs
@@ -1,8 +1,7 @@
 forkAt: priority 
-	"Create and schedule a Process running the code in the receiver. The 
-	priority of the process is the argument, priority."
+	"Create and schedule a Process running the code in the receiver at the given priority. Answer the newly created process."
 
 	| forkedProcess |
 	forkedProcess _ self newProcess.
 	forkedProcess priority: priority.
-	forkedProcess resume
+	^ forkedProcess resume
