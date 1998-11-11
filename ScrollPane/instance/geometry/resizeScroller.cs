dbc9cdecfe43485dd5a654719bc954f6d3db90dd
di <unknown>
1998-11-11 09:48:00 +0000

    no message

--- ScrollPane/instance/geometry/resizeScroller.cs
+++ ScrollPane/instance/geometry/resizeScroller.cs
@@ -1,7 +1,3 @@
 resizeScroller
-	| d inner |
-	d _ retractableScrollBar ifTrue: [16@0] ifFalse: [0@0].
-	inner _ self innerBounds.
-	scroller bounds: (scrollBarOnLeft
-		ifTrue: [inner topLeft + (16@0) - d corner: inner bottomRight]
-		ifFalse: [inner topLeft corner: inner bottomRight - (16@0) + d])
+
+	scroller bounds: self innerBounds
