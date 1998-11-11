f24e71147f69913403c2b7f00f1994fc6416b4c6
sw <unknown>
1998-11-11 11:40:00 +0000

    no message

--- Preferences/class/preferences dictionary/doesNotUnderstand-3A-.cs
+++ Preferences/class/preferences dictionary/doesNotUnderstand-3A-.cs
@@ -1,4 +1,4 @@
 doesNotUnderstand: aMessage
 	"Look up the message selector as a flag."
-
+	aMessage arguments size > 0 ifTrue: [^ super doesNotUnderstand: aMessage].
 	^ self valueOfFlag: aMessage selector
