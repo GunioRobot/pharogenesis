9855b8ed3c5dc70a578bdabcbcef5f8db6bdd76b
di <unknown>
1998-11-12 15:01:00 +0000

    no message

--- SequenceableCollection/instance/enumerating/pairsDo-3A-.cs
+++ SequenceableCollection/instance/enumerating/pairsDo-3A-.cs
@@ -1,11 +1,9 @@
 pairsDo: aBlock 
-	"March through the receiver two elements at a time.  If there's an odd number of items, ignore the last one.  Allows use of a flattened array for things that naturally group into pairs.  "
+	"Evaluate aBlock with my elements taken two at a time.  If there's an odd number of items, ignore the last one.  Allows use of a flattened array for things that naturally group into pairs.  See also pairsCollect:"
 
-	| i |
 	1 to: self size // 2 do:
-		[:index |
-			i _ 2 * index - 1.
-			 aBlock value: (self at: i) value: (self at: i + 1)]
-
-"#(1 'fred' 2 'charlie' 3 'elmer') pairsDo:
-	[:a :b | Transcript cr; show: b, ' is number ', a printString]"
+		[:index | aBlock value: (self at: 2 * index - 1) value: (self at: 2 * index)]
+"
+#(1 'fred' 2 'charlie' 3 'elmer') pairsDo:
+	[:a :b | Transcript cr; show: b, ' is number ', a printString]
+"
