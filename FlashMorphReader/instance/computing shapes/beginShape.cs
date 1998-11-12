138b8f2ceab73516e954694a828cda9cbdbe77a2
ar <unknown>
1998-11-12 21:43:00 +0000

    no message

---
+++ FlashMorphReader/instance/computing shapes/beginShape.cs
@@ -0,0 +1,10 @@
+beginShape
+
+	canCompressPoints _ true.
+	fillStyles _ Dictionary new.
+	lineStyles _ Dictionary new.
+	pointList resetToStart.
+	leftFillList resetToStart.
+	rightFillList resetToStart.
+	lineStyleList resetToStart.
+	fillIndex0 _ fillIndex1 _ lineStyleIndex _ 0.
