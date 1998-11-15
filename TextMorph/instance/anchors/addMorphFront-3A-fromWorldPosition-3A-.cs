dae0792f7112dfadd990511de3b34d103d4fce0b
ar <unknown>
1998-11-15 23:42:00 +0000

    no message

--- TextMorph/instance/anchors/addMorphFront-3A-fromWorldPosition-3A-.cs
+++ TextMorph/instance/anchors/addMorphFront-3A-fromWorldPosition-3A-.cs
@@ -2,7 +2,7 @@ addMorphFront: aMorph fromWorldPosition: wp
 	"Overridden for more specific re-layout and positioning"
 	| i |
 	self addMorphFront: aMorph.
-	i _ (self paragraph characterBlockAtPoint: (self transformFromWorld transform: wp))
+	i _ (self paragraph characterBlockAtPoint: (self transformFromWorld globalPointToLocal: wp))
 		stringIndex.
 	self paragraph replaceFrom: i to: i-1
 		with: (Text string: '*' attribute: (TextAnchor new anchoredMorph: aMorph))
