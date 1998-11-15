22af216df5bebad82a2f4f0e80a01d5ee23f4262
tk <unknown>
1998-11-15 19:59:00 +0000

    no message

---
+++ ObjectOut/instance/fetch from disk/xxxFixup.cs
@@ -0,0 +1,16 @@
+xxxFixup
+	"There is already an object in memory for my url.  All pointers to me need to be pointers to him.  Can't use become, because other pointers to him must stay valid."
+
+	| real temp list |
+	real _ page contentsMorph.
+	real == self ifTrue: [page error: 'should be converted by now'].
+	temp _ self.
+	list _ (Smalltalk pointersTo: temp) asOrderedCollection.
+	list add: thisContext.  list add: thisContext sender.
+	list do: [:holder |
+		1 to: holder class instSize do:
+			[:i | (holder instVarAt: i) == temp ifTrue: [holder instVarAt: i put: real]].
+		1 to: holder basicSize do:
+			[:i | (holder basicAt: i) == temp ifTrue: [holder basicAt: i put: real]].
+		].
+	^ real
