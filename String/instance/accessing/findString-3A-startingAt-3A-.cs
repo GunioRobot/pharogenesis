66358b857541d5bf9993da4f653681857f3729f1
di <unknown>
1998-11-15 16:43:00 +0000

    no message

--- String/instance/accessing/findString-3A-startingAt-3A-.cs
+++ String/instance/accessing/findString-3A-startingAt-3A-.cs
@@ -2,5 +2,4 @@ findString: subString startingAt: start
 	"Answer the index of subString within the receiver, starting at start. If 
 	the receiver does not contain subString, answer 0."
 
-	^ self indexOfSubCollection: subString startingAt: start
-		ifAbsent: [0]
+	^ self findSubstring: subString in: self startingAt: start matchTable: CaseSensitiveOrder
