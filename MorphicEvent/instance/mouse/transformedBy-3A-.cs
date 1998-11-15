2cd9a368a46518665c07824750a5eaf9c61e8d98
ar <unknown>
1998-11-15 23:42:00 +0000

    no message

--- MorphicEvent/instance/mouse/transformedBy-3A-.cs
+++ MorphicEvent/instance/mouse/transformedBy-3A-.cs
@@ -2,4 +2,4 @@ transformedBy: aMorphicTransform
 	"Return a copy of the receiver transformed by the given transformation."
 
 	aMorphicTransform isIdentity ifTrue: [^ self].  "no transformation needed"
-	^ self copy setCursorPoint: (aMorphicTransform transform: cursorPoint)
+	^ self copy setCursorPoint: (aMorphicTransform globalPointToLocal: cursorPoint)
