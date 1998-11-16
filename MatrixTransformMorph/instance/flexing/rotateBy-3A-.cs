738516ee2fcf38ddb2dddc6a9b4d107e7178f472
ar <unknown>
1998-11-16 01:14:00 +0000

    no message

---
+++ MatrixTransformMorph/instance/flexing/rotateBy-3A-.cs
@@ -0,0 +1,11 @@
+rotateBy: delta
+	| pt m |
+	delta = 0.0 ifTrue:[^self].
+	self changed.
+	pt _ self transformFromWorld globalPointToLocal: self referencePosition.
+	m _ MatrixTransform2x3 withOffset: pt.
+	m _ m composedWithLocal: (MatrixTransform2x3 withAngle: delta).
+	m _ m composedWithLocal: (MatrixTransform2x3 withOffset: pt negated).
+	transform _ self transform composedWithLocal: m.
+	self computeBounds.
+	self changed.
