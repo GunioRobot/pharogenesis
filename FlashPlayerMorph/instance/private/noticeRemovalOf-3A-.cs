3798c40b1911e43db0bc1590faf19a9ffc5b035d
ar <unknown>
1998-11-16 02:58:00 +0000

    no message

---
+++ FlashPlayerMorph/instance/private/noticeRemovalOf-3A-.cs
@@ -0,0 +1,10 @@
+noticeRemovalOf: aFlashMorph
+	"The flash morph is removed from the player.
+	Remove it's activation keys so that we don't have any problems."
+	| morphs |
+	aFlashMorph activationKeys do:[:key|
+		morphs _ activationKeys at: key.
+		activationKeys at: key put: (morphs copyWithout: aFlashMorph).
+	].
+	"And remove it from the activeMorphs"
+	activeMorphs remove: aFlashMorph ifAbsent:[]
