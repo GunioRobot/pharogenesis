2c83266ed1012cc03d4bf7f78fdd5486865baa29
sw <unknown>
1998-11-16 08:47:00 +0000

    no message

---
+++ StandardScriptingSystem/instance/parts bin/setCustomPartsBinFrom-3A-.cs
@@ -0,0 +1,6 @@
+setCustomPartsBinFrom: aMorph
+	"Establish aMorph as the custom parts bin.  Normally triggered by use menu action, but can also be used to reset the custom parts bin to undefined"
+
+	CustomPartsBin _ aMorph
+
+	"ScriptingSystem setCustomPartsBinFrom: nil"
