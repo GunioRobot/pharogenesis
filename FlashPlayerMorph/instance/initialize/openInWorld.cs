8dd41269254331999641cbd4f92eb39b7b56a888
ar <unknown>
1998-11-17 15:24:00 +0000

    no message

---
+++ FlashPlayerMorph/instance/initialize/openInWorld.cs
@@ -0,0 +1,8 @@
+openInWorld
+	| window extent |
+	self localBounds: localBounds.
+	extent _ bounds extent.
+	window _ FlashPlayerWindow labelled:'Flash Player'.
+	window model: (FlashPlayerModel player: self).
+	window addMorph: self frame:(0@0 corner: 1@1).
+	window openInWorldExtent: extent
