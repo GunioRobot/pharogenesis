c5eb7933f753b1ea72e0f945c038b7df6f819485
go <unknown>
1998-11-18 11:04:00 +0000

    no message

--- ObjectMemory/instance/header access/rightType-3A-.cs
+++ ObjectMemory/instance/header access/rightType-3A-.cs
@@ -1,9 +1,9 @@
 rightType: headerWord
 	"Computer the correct header type for an object based on the size and compact class fields of the given base header word, rather than its type bits. This is used during marking, when the header type bits are used to record the state of tracing."
 
-	(headerWord bitAnd: 16rFC) = 0  "zero size field in header word"
+	(headerWord bitAnd: SizeMask) = 0  "zero size field in header word"
 		ifTrue: [ ^ HeaderTypeSizeAndClass ]
 		ifFalse: [
-			(headerWord bitAnd: 16r1F000) = 0  "zero compact class field  in header word"
+			(headerWord bitAnd: CompactClassMask) = 0
 				ifTrue: [ ^ HeaderTypeClass ]
 				ifFalse: [ ^ HeaderTypeShort ]].
