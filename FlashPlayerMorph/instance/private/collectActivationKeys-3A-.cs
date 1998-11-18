9b9aa7a207226a5dcfe4f838a41d36093b8086b1
ar <unknown>
1998-11-18 23:21:00 +0000

    no message

---
+++ FlashPlayerMorph/instance/private/collectActivationKeys-3A-.cs
@@ -0,0 +1,17 @@
+collectActivationKeys: frame
+	"Note: Must only be called after a frame has been completed"
+	| vis lastKey |
+	vis _ Array new: frame.
+	vis atAllPut: #().
+	lastKey _ activationKeys size.
+	vis replaceFrom: 1 to: lastKey with: activationKeys startingAt: 1.
+	submorphs do:[:m|
+		(m isFlashMorph and:[m isFlashCharacter]) ifTrue:[
+			m activationKeys do:[:key|
+				key > lastKey ifTrue:[
+					vis at: key put: ((vis at: key) copyWith: m)
+				].
+			].
+		].
+	].
+	^vis
