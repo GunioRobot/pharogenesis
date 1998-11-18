e5f33b0976227215c43def09c44ab75f68bbb21d
ar <unknown>
1998-11-18 22:34:00 +0000

    no message

---
+++ FlashPlayerWindow/instance/as yet unclassified/addProgressIndicator.cs
@@ -0,0 +1,8 @@
+addProgressIndicator
+	progress _ FlashProgressMorph new.
+	progress borderWidth: 1.
+	progress color: Color transparent.
+	progress progressColor: Color gray.
+	progress extent: (100 @ (startButton extent y - 6)).
+	self addMorph: progress.
+
