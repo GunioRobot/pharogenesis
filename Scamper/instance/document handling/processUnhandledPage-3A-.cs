2e1f866d07d17aab26039c06390dfe8192dfbb0c
ar <unknown>
1998-11-19 22:01:00 +0000

    no message

---
+++ Scamper/instance/document handling/processUnhandledPage-3A-.cs
@@ -0,0 +1,21 @@
+processUnhandledPage: newSource
+	"offer to save it to a file"
+	| fileName file |
+	self status: 'sittin'.
+
+	(self confirm: 'unkown content-type ', newSource contentType,'--
+Would you like to save to a file?') ifFalse: [ ^false ].
+
+	fileName _ ''.
+	[
+		fileName _ FillInTheBlank request: 'file to save in' initialAnswer: fileName.
+		fileName isEmpty ifTrue: [ ^self ].
+		file _ FileStream fileNamed: fileName.
+		file == nil
+	] whileTrue.
+
+	file reset.
+	file binary.
+	file nextPutAll: newSource content.
+	file close.
+	^true
