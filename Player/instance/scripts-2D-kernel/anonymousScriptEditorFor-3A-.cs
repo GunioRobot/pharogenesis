a2a2e5aedfc0a9bfb5abb6dd88cbf37e1ba2b451
sw <unknown>
1998-11-19 12:33:00 +0000

    no message

--- Player/instance/scripts-2D-kernel/anonymousScriptEditorFor-3A-.cs
+++ Player/instance/scripts-2D-kernel/anonymousScriptEditorFor-3A-.cs
@@ -1,14 +1,7 @@
 anonymousScriptEditorFor: aPhraseTileMorph
 	| aScriptEditor aUserScript |
 	self isFlagshipForClass ifFalse:
-		[self flag: #deferred.
-		self inform: 'sorry, for the moment you cannot tear
-off a new anonymous script for
-any instance of a uniclass other
-than the flagship instance.  If this
-makes no sense to you,  you
-surely got here in error'.
-		self error: 'Please close this window'].
+		[^ aPhraseTileMorph].
 
 	aUserScript _ self class anonymousUserScriptFor: self unusedScriptName player: self.
 	aScriptEditor _ aUserScript instantiatedScriptEditor.
