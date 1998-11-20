f0b07068ed37904470df6867731068647f454a0e
djm <unknown>
1998-11-20 05:44:00 +0000

    no message

--- SequenceableCollection/instance/converting/asStringWithCr.cs
+++ SequenceableCollection/instance/converting/asStringWithCr.cs
@@ -1,11 +1,12 @@
 asStringWithCr
-	"Convert to a string with returns between items.  Elements are usually strings.
+	"Convert to a string with returns between items.  Elements are
+usually strings.
 	 Useful for labels for PopUpMenus."
 	| labelStream |
 	labelStream _ WriteStream on: (String new: 200).
 	self do: [:each |
 		(each isKindOf: String)
 			ifTrue: [labelStream nextPutAll: each; cr]
-			ifFalse: [each printOn: labelStream; cr]].
+			ifFalse: [each printOn: labelStream. labelStream cr]].
 	self size > 0 ifTrue: [labelStream skip: -1].
 	^ labelStream contents
