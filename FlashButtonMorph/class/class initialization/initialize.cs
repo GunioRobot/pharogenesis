c506940a09324ceb13b61d2baac6326fcd6af62a
ar <unknown>
1998-11-20 22:58:00 +0000

    no message

---
+++ FlashButtonMorph/class/class initialization/initialize.cs
@@ -0,0 +1,13 @@
+initialize
+	"FlashButtonMorph initialize"
+	ActionHelpText _ Dictionary new.
+	#(	(getURL:window: 'Jump to URL')
+		(gotoFrame: 'Continue playing')
+		(gotoLabel: 'Continue playing')
+		(gotoNextFrame 'Continue playing')
+		(gotoPrevFrame 'Continue playing')
+		(actionPlay 'Continue playing')
+		(actionStop 'Stop playing')
+		(stopSounds 'Stop all sounds')
+		(toggleQuality 'Toggle display quality')
+	) do:[:spec| ActionHelpText at: spec first put: spec last].
