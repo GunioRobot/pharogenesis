617cfc6a3758742f9a568cd73d29c4b60ccefb1a
ar <unknown>
1998-11-20 00:50:00 +0000

    no message

---
+++ FlashMorphReader/instance/defining text/recordTextEnd-3A-.cs
@@ -0,0 +1,9 @@
+recordTextEnd: id
+	textMorph submorphs isEmpty ifFalse:[
+		textMorph allMorphsDo:[:m| m color: textMorph color].
+		textMorph transform: nil.
+		textMorph id: id.
+		textMorph stepTime: stepTime.
+		textMorph lockChildren.
+		shapes at: id put: textMorph].
+	self doLog ifTrue:[log _ Transcript].
