d495adee425e2119466214d979c6574c978278fa
ar <unknown>
1998-11-20 18:17:00 +0000

    no message

---
+++ TextDiffBuilder/instance/initialize/destString-3A-.cs
@@ -0,0 +1,16 @@
+destString: aString
+	realDst := self split: aString asString.
+	dstLines := OrderedCollection new.
+	dstMap := OrderedCollection new.
+	realDst doWithIndex:[:line :realIndex|
+		"(line contains:[:anyChar| anyChar isSeparator not]) ifTrue:["
+			dstLines add: line.
+			dstMap add: realIndex.
+		"]."
+	].
+	dstPos := PluggableDictionary new: dstLines size.
+	dstPos hashBlock: self stringHashBlock.
+	dstLines doWithIndex:[:line :index|
+		(dstPos includesKey: line)
+			ifTrue:[(dstPos at: line) add: index. multipleMatches := true]
+			ifFalse:[dstPos at: line put: (OrderedCollection with: index)]].
