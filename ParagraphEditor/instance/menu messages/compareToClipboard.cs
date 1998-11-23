df5bcc73c8dec37bbc5082a9602f14ff3b139ba5
di <unknown>
1998-11-23 15:21:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/compareToClipboard.cs
+++ ParagraphEditor/instance/menu messages/compareToClipboard.cs
@@ -1,10 +1,10 @@
 compareToClipboard
-	"Check to see if whether the receiver's text is the same as the text currently on the clipboard, and inform the user.  4/29/96 sw"
-	| count s |
-	s _ self clipboardText string.
-	count _ paragraph text string charactersExactlyMatching: s.
-	count == (paragraph text string size max: s size) 
-		ifTrue:
-			[^ self inform: 'Exact match'].
+	"Check to see if whether the receiver's text is the same as the text currently on the clipboard, and inform the user."
+	| s1 s2 |
+	s1 _ self clipboardText string.
+	s2 _ paragraph text string.
+	s1 = s2 ifTrue: [^ self inform: 'Exact match'].
 
-	self selectFrom: 1 to: count
+	(StringHolder new textContents:
+		(TextDiffBuilder buildDisplayPatchFrom: s1 to: s2))
+		openLabel: 'Comparison to Clipboard Text'
