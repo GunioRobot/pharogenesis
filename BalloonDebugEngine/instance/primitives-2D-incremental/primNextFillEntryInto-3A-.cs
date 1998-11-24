534c51503db29f954206f822bb99bc9e45fabc47
ar <unknown>
1998-11-24 21:00:00 +0000

    no message

---
+++ BalloonDebugEngine/instance/primitives-2D-incremental/primNextFillEntryInto-3A-.cs
@@ -0,0 +1,4 @@
+primNextFillEntryInto: fillEntry
+	"Store the next fill entry of the active edge table in fillEntry.
+	Return false if there is no such entry, true otherwise"
+	^BalloonEnginePlugin doPrimitive: 'gePrimitiveNextFillEntry'
