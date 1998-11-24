93ef197ada3ce693b0c487f8c4756aee48bd84df
sw <unknown>
1998-11-24 14:53:00 +0000

    no message

--- Integer/instance/printing/asStringWithCommas.cs
+++ Integer/instance/printing/asStringWithCommas.cs
@@ -1,8 +1,12 @@
-asStringWithCommas  "123456789 asStringWithCommas"
+asStringWithCommas
+	"123456789 asStringWithCommas"
+	"-123456789 asStringWithCommas"
 	| digits |
 	digits _ self abs printString.
 	^ String streamContents:
-		[:strm | 1 to: digits size do: 
+		[:strm | 
+		self sign = -1 ifTrue: [strm nextPut: $-].
+		1 to: digits size do: 
 			[:i | strm nextPut: (digits at: i).
 			(i < digits size and: [(i - digits size) \\ 3 = 0])
 				ifTrue: [strm nextPut: $,]]]
