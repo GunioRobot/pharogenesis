5aeb16bd4b102886b0d249a07f63cc312835ffbe
ar <unknown>
1998-11-24 14:24:00 +0000

    no message

---
+++ FlashMorphReader/instance/defining buttons/recordButton-3A-character-3A-state-3A-layer-3A-matrix-3A-colorTransform-3A-.cs
@@ -0,0 +1,25 @@
+recordButton: buttonId character: characterId state: state layer: layer matrix: matrix colorTransform: cxForm
+	| button children shape |
+	button _ buttons at: buttonId ifAbsent:[^self halt].
+	button id: buttonId.
+	shape _ self oldMorphFromShape: characterId.
+	shape isNil ifTrue:[^nil].
+	children _ shape submorphs collect:[:m| m fullCopy].
+	shape _ FlashMorph withAll: children.
+	shape lockChildren.
+	shape depth: layer.
+	shape transform: matrix.
+	shape colorTransform: cxForm.
+	(state anyMask: 1) ifTrue:[
+		button defaultLook: shape.
+	].
+	(state anyMask: 2) ifTrue:[
+		button overLook: shape.
+	].
+	(state anyMask: 4) ifTrue:[
+		button pressLook: shape.
+	].
+	(state anyMask: 8) ifTrue:[
+		button sensitiveLook: shape.
+	].
+	button lockChildren.
