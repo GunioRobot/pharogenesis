f0b2771884e86fc8e7d926cfb639ec95460151c7
jm <unknown>
1998-11-25 19:59:00 +0000

    no message

--- ObjectMemory/instance/gc -2D--2D- compaction/objectAfterWhileForwarding-3A-.cs
+++ ObjectMemory/instance/gc -2D--2D- compaction/objectAfterWhileForwarding-3A-.cs
@@ -9,12 +9,12 @@ objectAfterWhileForwarding: oop
 	"Assume: mark bit cannot be set on a free chunk, so if we get here,
 	 oop is not free and it has a forwarding table entry"
 
-	fwdBlock _ header bitAnd: AllButMarkBitAndTypeMask.
-	checkAssertions ifTrue: [ self fwdBlockValidate: fwdBlock ].
+	fwdBlock _ (header bitAnd: AllButMarkBitAndTypeMask) << 1.
+	DoAssertionChecks ifTrue: [ self fwdBlockValidate: fwdBlock ].
 	realHeader _ self longAt: fwdBlock + 4.
 	"following code is like sizeBitsOf:"
 	(realHeader bitAnd: TypeMask) = HeaderTypeSizeAndClass
-		ifTrue: [ sz _ (self sizeHeader: oop) bitAnd: 16rFFFFFFC ]
-		ifFalse: [ sz _ realHeader bitAnd: 16rFC ].
+		ifTrue: [ sz _ (self sizeHeader: oop) bitAnd: AllButTypeMask ]
+		ifFalse: [ sz _ realHeader bitAnd: SizeMask ].
 
 	^ self oopFromChunk: (oop + sz)
