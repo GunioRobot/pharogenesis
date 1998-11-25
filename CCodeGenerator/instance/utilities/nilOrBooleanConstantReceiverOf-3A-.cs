bf01fd7c4bcc97a671ee55daf6d848dfdcd5a926
jm <unknown>
1998-11-25 19:02:00 +0000

    no message

---
+++ CCodeGenerator/instance/utilities/nilOrBooleanConstantReceiverOf-3A-.cs
@@ -0,0 +1,9 @@
+nilOrBooleanConstantReceiverOf: sendNode
+	"Answer nil or the boolean constant that is the receiver of the given message send. Used to suppress conditional code when the condition is a translation-time constant."
+
+	| rcvr val |
+	rcvr _ sendNode receiver.
+	rcvr isConstant ifTrue: [
+		val _ rcvr value.
+		((val == true) or: [val == false]) ifTrue: [^ val]].
+	^ nil
