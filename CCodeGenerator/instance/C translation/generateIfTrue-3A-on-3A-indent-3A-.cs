322d06cb99e66d2900323570e9b74122a32cb584
jm <unknown>
1998-11-25 19:04:00 +0000

    no message

--- CCodeGenerator/instance/C translation/generateIfTrue-3A-on-3A-indent-3A-.cs
+++ CCodeGenerator/instance/C translation/generateIfTrue-3A-on-3A-indent-3A-.cs
@@ -1,6 +1,12 @@
 generateIfTrue: msgNode on: aStream indent: level
 	"Generate the C code for this message onto the given stream."
 
+	| const |
+	const _ self nilOrBooleanConstantReceiverOf: msgNode.
+	const ifNotNil: [
+		const ifTrue: [msgNode args first emitCCodeOn: aStream level: level generator: self].
+		^ self].
+
 	aStream nextPutAll: 'if ('.
 	msgNode receiver emitCCodeOn: aStream level: level generator: self.
 	aStream nextPutAll: ') {'; cr.
