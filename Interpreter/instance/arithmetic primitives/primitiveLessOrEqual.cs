aad173352720c60dfbdbd51acb8e90a2e0f22e3b
di <unknown>
1998-11-27 15:43:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveLessOrEqual.cs
+++ Interpreter/instance/arithmetic primitives/primitiveLessOrEqual.cs
@@ -1,6 +1,5 @@
 primitiveLessOrEqual
 	| integerReceiver integerArgument |
-	successFlag _ true.
 	integerArgument _ self popInteger.
 	integerReceiver _ self popInteger.
-	self checkBooleanResult: integerReceiver <= integerArgument from: 5
+	self checkBooleanResult: integerReceiver <= integerArgument
