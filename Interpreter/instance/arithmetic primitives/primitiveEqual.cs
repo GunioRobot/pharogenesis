e71863a2cdca87a6c7676bdc09e474e6317b7793
di <unknown>
1998-11-27 15:43:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveEqual.cs
+++ Interpreter/instance/arithmetic primitives/primitiveEqual.cs
@@ -1,7 +1,6 @@
 primitiveEqual
 	| integerReceiver integerArgument result |
-	successFlag _ true.
 	integerArgument _ self popStack.
 	integerReceiver _ self popStack.
 	result _ self compare31or32Bits: integerReceiver equal: integerArgument.
-	self checkBooleanResult: result from: 7
+	self checkBooleanResult: result
