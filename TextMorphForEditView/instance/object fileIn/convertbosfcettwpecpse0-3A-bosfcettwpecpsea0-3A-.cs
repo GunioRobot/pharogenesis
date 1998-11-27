4dcf54c25e0a8f36c42a4c31db1adff6f4a4fa14
tk <unknown>
1998-11-27 20:33:00 +0000

    no message

---
+++ TextMorphForEditView/instance/object fileIn/convertbosfcettwpecpse0-3A-bosfcettwpecpsea0-3A-.cs
@@ -0,0 +1,7 @@
+convertbosfcettwpecpse0: varDict bosfcettwpecpsea0: smartRefStrm
+	"These variables are automatically stored into the new instance ('editView' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"Be sure to to fill in ('acceptOnCR' ) and deal with the information in ()"
+
+	acceptOnCR _ false.
