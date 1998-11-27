c17e9dfb70b463cfabbdf6f058ea07674b01617d
di <unknown>
1998-11-27 15:43:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveNotEqual.cs
+++ Interpreter/instance/arithmetic primitives/primitiveNotEqual.cs
@@ -1,7 +1,6 @@
 primitiveNotEqual
 	| integerReceiver integerArgument result |
-	successFlag _ true.
 	integerArgument _ self popStack.
 	integerReceiver _ self popStack.
 	result _ (self compare31or32Bits: integerReceiver equal: integerArgument) not.
-	self checkBooleanResult: result from: 8
+	self checkBooleanResult: result
