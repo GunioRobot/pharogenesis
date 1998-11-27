e350a856f981f44c26dc255654ea178407fea145
di <unknown>
1998-11-27 11:19:00 +0000

    no message

--- ObjectMemory/instance/interpreter access/fetchClassOf-3A-.cs
+++ ObjectMemory/instance/interpreter access/fetchClassOf-3A-.cs
@@ -1,15 +1,13 @@
 fetchClassOf: oop
-
 	| ccIndex |
 	self inline: true.
 	(self isIntegerObject: oop)
 		ifTrue: [ ^ self splObj: ClassInteger ].
 
-	ccIndex _ (((self baseHeader: oop) >> 12) bitAnd: 16r1F) - 1.
-	ccIndex < 0
-		ifTrue: [ ^ (self classHeader: oop) bitAnd: AllButTypeMask ]
-		ifFalse: [
-			"look up compact class"
-			^ self fetchPointer: ccIndex
-				ofObject: (self fetchPointer: CompactClasses ofObject: specialObjectsOop)
+	ccIndex _ ((self baseHeader: oop) >> 12) bitAnd: 16r1F.
+	ccIndex = 0
+		ifTrue: [^ (self classHeader: oop) bitAnd: AllButTypeMask ]
+		ifFalse: ["look up compact class"
+				^ self fetchPointer: ccIndex - 1
+					ofObject: (self fetchPointer: CompactClasses ofObject: specialObjectsOop)
 		].
