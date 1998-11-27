3e59b6936f7aa01041e2351606ccf691f5d3a020
di <unknown>
1998-11-27 14:49:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveFloatLessOrEqual.cs
+++ Interpreter/instance/float primitives/primitiveFloatLessOrEqual.cs
@@ -1,14 +1,4 @@
 primitiveFloatLessOrEqual
-
-	| rcvr rcvrOop arg argOop |
-	self var: #rcvr declareC: 'double rcvr'.
-	self var: #arg declareC: 'double arg'.
-
-	rcvrOop _ self stackValue: 1.
-	argOop _ self stackTop.
-	self assertFloat: rcvrOop and: argOop.
-	successFlag ifTrue: [
-		self fetchFloatAt: rcvrOop + BaseHeaderSize into: rcvr.
-		self fetchFloatAt: argOop + BaseHeaderSize into: arg.
-		self pop: 2.
-		self pushBool: rcvr <= arg].
+	| bool |
+	bool _ self primitiveFloatGreater: (self stackValue: 1) thanArg: self stackTop.
+	successFlag ifTrue: [self pop: 2. self pushBool: bool not].
