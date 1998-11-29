eebb5e35c9bd899cb16e32716835f1b6f1f1747a
di <unknown>
1998-11-29 12:02:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveBitOr.cs
+++ Interpreter/instance/arithmetic primitives/primitiveBitOr.cs
@@ -1,9 +1,8 @@
 primitiveBitOr
 	| integerReceiver integerArgument |
-	successFlag _ true.
 	integerArgument _ self popPos32BitInteger.
 	integerReceiver _ self popPos32BitInteger.
 	successFlag
 		ifTrue: [self push: (self positive32BitIntegerFor:
 					(integerReceiver bitOr: integerArgument))]
-		ifFalse: [self unPop: 2.  self failSpecialPrim: 15]
+		ifFalse: [self unPop: 2]
