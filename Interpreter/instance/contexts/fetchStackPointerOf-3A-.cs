498e0a83b6d956a6f64ae6d2dd27912817de1587
di <unknown>
1998-11-29 16:24:00 +0000

    no message

---
+++ Interpreter/instance/contexts/fetchStackPointerOf-3A-.cs
@@ -0,0 +1,7 @@
+fetchStackPointerOf: aContext
+	"Return the stackPointer of a Context or BlockContext."
+	| sp |
+	self inline: true.
+	sp _ self fetchPointer: StackPointerIndex ofObject: aContext.
+	(self isIntegerObject: sp) ifFalse: [^ 0].
+	^ self integerValueOf: sp
