51346a58ba32287bd83afc7b86b990d4a6eac2d8
di <unknown>
1998-11-30 09:03:00 +0000

    no message

--- Color/instance/conversions/pixelWordFor-3A-filledWith-3A-.cs
+++ Color/instance/conversions/pixelWordFor-3A-filledWith-3A-.cs
@@ -1,10 +1,12 @@
 pixelWordFor: depth filledWith: pixelValue
 	"Return to a 32-bit word that concatenates enough copies of the given pixel value to fill the word (i.e., 32/depth copies). Depth should be one of 1, 2, 4, 8, 16, or 32. The pixel value should be an integer in 0..2^depth-1."
-
+	| halfword |
 	depth = 32 ifTrue: [^ pixelValue].
-	^ (pixelValue bitAnd: (1 bitShift: depth) - 1) * 
-		(#(16rFFFFFFFF				"replicates at every bit"
-			16r55555555 -			"replicates every 2 bits"
-			16r11111111 - - -			"replicates every 4 bits"
-			16r01010101 - - - - - - -	"replicates every 8 bits"
-			16r00010001) at: depth)	"replicates every 16 bits"
+	depth = 16
+		ifTrue: [halfword _ pixelValue]
+		ifFalse: [halfword _ pixelValue * 
+					(#(16rFFFF				"replicates at every bit"
+						16r5555 -			"replicates every 2 bits"
+						16r1111 - - -			"replicates every 4 bits"
+						16r0101) at: depth)	"replicates every 8 bits"].
+	^ halfword bitOr: (halfword bitShift: 16)
