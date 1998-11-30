86d7cca7316fd246665721faabc7b0f1079e430b
di <unknown>
1998-11-30 13:18:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveBitXor.cs
+++ Interpreter/instance/arithmetic primitives/primitiveBitXor.cs
@@ -1,7 +1,4 @@
 primitiveBitXor
-	"Note: unlike all the other arithmetic primitives, this is called as
-	a real send, not as a special byte.  Thus successFlag has already
-	been set, and failure is normal, not through failSpecialPrim."
 	| integerReceiver integerArgument |
 	integerArgument _ self popPos32BitInteger.
 	integerReceiver _ self popPos32BitInteger.
