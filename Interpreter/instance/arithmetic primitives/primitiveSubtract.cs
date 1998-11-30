153d433aea640f363db330a2d818c6c816efe957
di <unknown>
1998-11-30 10:13:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveSubtract.cs
+++ Interpreter/instance/arithmetic primitives/primitiveSubtract.cs
@@ -1,6 +1,3 @@
 primitiveSubtract
-	| integerReceiver integerArgument |
-	successFlag _ true.
-	integerArgument _ self popInteger.
-	integerReceiver _ self popInteger.
-	self checkIntegerResult: integerReceiver - integerArgument from: 2
+
+	self pop2AndPushIntegerIfOK: (self stackIntegerValue: 1) - (self stackIntegerValue: 0)
