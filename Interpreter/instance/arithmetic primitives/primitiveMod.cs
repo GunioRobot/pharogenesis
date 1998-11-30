9893e2ceabc5362d2bb144cf6fe06044c34e35db
di <unknown>
1998-11-30 10:36:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveMod.cs
+++ Interpreter/instance/arithmetic primitives/primitiveMod.cs
@@ -1,19 +1,4 @@
 primitiveMod
-
-	| integerReceiver integerArgument integerResult |
-	successFlag _ true.
-	integerArgument _ self popInteger.
-	integerReceiver _ self popInteger.
-	self success: integerArgument ~= 0.
-	successFlag ifFalse: [integerArgument _ 1].  "fall through to fail"
-	integerResult _ integerReceiver \\ integerArgument.
-
-	"ensure that the result has the same sign as the argument"
-	integerArgument < 0 ifTrue: [
-		integerResult > 0
-			ifTrue: [integerResult _ integerResult + integerArgument].
-	] ifFalse: [
-		integerResult < 0
-			ifTrue: [integerResult _ integerResult + integerArgument].
-	].
-	self checkIntegerResult: integerResult from: 11.
+	| mod |
+	mod _ self doPrimitiveMod: (self stackValue: 1) by: (self stackValue: 0).
+	self pop2AndPushIntegerIfOK: mod
