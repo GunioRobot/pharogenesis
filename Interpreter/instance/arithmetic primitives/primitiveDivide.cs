280eb9735bbad012ac632108ad66c611aad934c0
di <unknown>
1998-11-30 10:25:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveDivide.cs
+++ Interpreter/instance/arithmetic primitives/primitiveDivide.cs
@@ -1,9 +1,7 @@
 primitiveDivide
 	| integerReceiver integerArgument |
-	successFlag _ true.
-	integerArgument _ self popInteger.
-	integerReceiver _ self popInteger.
-	self success: integerArgument ~= 0.
-	successFlag ifFalse: [integerArgument _ 1].  "fall through to fail"
-	self success: integerReceiver \\ integerArgument = 0.
-	self checkIntegerResult: integerReceiver // integerArgument from: 10
+	integerReceiver _ self stackIntegerValue: 1.
+	integerArgument _ self stackIntegerValue: 0.
+	(integerArgument ~= 0 and: [integerReceiver \\ integerArgument = 0])
+		ifTrue: [self pop2AndPushIntegerIfOK: integerReceiver // integerArgument]
+		ifFalse: [self primitiveFail]
