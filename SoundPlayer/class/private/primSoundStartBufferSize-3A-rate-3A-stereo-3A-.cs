21876e8c3d4c4096da488e2c8c6f13f1df25d662
ar <unknown>
1998-12-05 16:36:00 +0000

    no message

--- SoundPlayer/class/private/primSoundStartBufferSize-3A-rate-3A-stereo-3A-.cs
+++ SoundPlayer/class/private/primSoundStartBufferSize-3A-rate-3A-stereo-3A-.cs
@@ -1,5 +1,5 @@
 primSoundStartBufferSize: bufferSize rate: samplesPerSecond stereo: stereoFlag
 	"Start double-buffered sound output with the given buffer size and sampling rate. This version has been superceded by primitive 171 (primSoundStartBufferSize:rate:stereo:semaIndex:)."
-
+	"ar 12/5/1998 Turn off the sound if not supported"
 	<primitive: 170>
-	^ self primitiveFailed
+	SoundSupported _ false.
