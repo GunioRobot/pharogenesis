2890de768f31aa74ccaee34315bab981d37c150b
ar <unknown>
1998-12-05 22:22:00 +0000

    no message

---
+++ FlashMorphReader/instance/defining styles/recordBitmapFill-3A-matrix-3A-id-3A-clipped-3A-.cs
@@ -0,0 +1,9 @@
+recordBitmapFill: index matrix: bmMatrix id: bitmapID clipped: aBoolean
+	| fillStyle form |
+	form _ forms at: bitmapID ifAbsent:[^nil].
+	fillStyle _ BitmapFillStyle form: form.
+	fillStyle origin: (bmMatrix localPointToGlobal: 0@0).
+	fillStyle direction: (bmMatrix localPointToGlobal: form extent x @ 0) - fillStyle origin.
+	fillStyle normal: (bmMatrix localPointToGlobal: 0 @ form extent y) - fillStyle origin.
+	fillStyle tileFlag: aBoolean not.
+	fillStyles at: index put: fillStyle.
