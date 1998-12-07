7463eda1b4bfdfb66c5f4913cd7bf1988d39445e
ar <unknown>
1998-12-07 21:35:00 +0000

    no message

--- BitBltSimulation/instance/combination rules/partitionedMax-3A-with-3A-nBits-3A-nPartitions-3A-.cs
+++ BitBltSimulation/instance/combination rules/partitionedMax-3A-with-3A-nBits-3A-nPartitions-3A-.cs
@@ -1,7 +1,7 @@
 partitionedMax: word1 with: word2 nBits: nBits nPartitions: nParts
 	"Max word1 to word2 as nParts partitions of nBits each"
 	| mask result |
-	mask _ (1 << nBits) - 1.  "partition mask starts at the right"
+	mask _ maskTable at: nBits.  "partition mask starts at the right"
 	result _ 0.
 	1 to: nParts do:
 		[:i |
