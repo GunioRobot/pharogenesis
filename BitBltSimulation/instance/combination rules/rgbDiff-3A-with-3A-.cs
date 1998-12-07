9aac641f79bf141c9a578b4cbfe9b131e084f8b2
ar <unknown>
1998-12-07 21:12:00 +0000

    no message

--- BitBltSimulation/instance/combination rules/rgbDiff-3A-with-3A-.cs
+++ BitBltSimulation/instance/combination rules/rgbDiff-3A-with-3A-.cs
@@ -1,37 +1,33 @@
 rgbDiff: sourceWord with: destinationWord
 	"Subract the pixels in the source and destination, color by color,
 	and return the sum of the absolute value of all the differences.
-	For non-rgb, XOR the two and return the number of differing pixels.
-	Note that the region is not clipped to bit boundaries, but only to the
-	nearest (enclosing) word.  This is because copyLoop does not do
-	pre-merge masking.  For accurate results, you must subtract the
-	values obtained from the left and right fringes."
-	| diff pixMask |
+	For non-rgb, return the number of differing pixels."
+	| pixMask destShifted sourceShifted destPixVal bitsPerColor rgbMask sourcePixVal diff maskShifted |
 	self inline: false.
-	destPixSize < 16 ifTrue:
-		["Just xor and count differing bits if not RGB"
-		diff _ sourceWord bitXor: destinationWord.
-		pixMask _ (1 bitShift: destPixSize) - 1.
-		[diff = 0] whileFalse:
-			[(diff bitAnd: pixMask) ~= 0 ifTrue: [bitCount _ bitCount + 1].
-			diff _ diff >> destPixSize].
-		^ destinationWord "for no effect"].
- 	destPixSize = 16
-		ifTrue:
-		[diff _ (self partitionedSub: sourceWord from: destinationWord
-						nBits: 5 nPartitions: 3).
-		bitCount _ bitCount + (diff bitAnd: 16r1F)
-							+ (diff>>5 bitAnd: 16r1F)
-							+ (diff>>10 bitAnd: 16r1F).
-		diff _ (self partitionedSub: sourceWord>>16 from: destinationWord>>16
-						nBits: 5 nPartitions: 3).
-		bitCount _ bitCount + (diff bitAnd: 16r1F)
-							+ (diff>>5 bitAnd: 16r1F)
-							+ (diff>>10 bitAnd: 16r1F)]
-		ifFalse:
-		[diff _ (self partitionedSub: sourceWord from: destinationWord
-						nBits: 8 nPartitions: 3).
-		bitCount _ bitCount + (diff bitAnd: 16rFF)
-							+ (diff>>8 bitAnd: 16rFF)
-							+ (diff>>16 bitAnd: 16rFF)].
+	pixMask _ maskTable at: destPixSize.
+	destPixSize = 16
+		ifTrue: [bitsPerColor _ 5.  rgbMask _ 16r1F]
+		ifFalse: [bitsPerColor _ 8.  rgbMask _ 16rFF].
+	maskShifted _ destMask.
+	destShifted _ destinationWord.
+	sourceShifted _ sourceWord.
+	1 to: pixPerWord do:
+		[:i |
+		(maskShifted bitAnd: pixMask) > 0 ifTrue:
+			["Only tally pixels within the destination rectangle"
+			destPixVal _ destShifted bitAnd: pixMask.
+			sourcePixVal _ sourceShifted bitAnd: pixMask.
+			destPixSize < 16
+				ifTrue: [sourcePixVal = destPixVal
+							ifTrue: [diff _ 0]
+							ifFalse: [diff _ 1]]
+				ifFalse: [diff _ (self partitionedSub: sourcePixVal from: destPixVal
+								nBits: bitsPerColor nPartitions: 3).
+						diff _ (diff bitAnd: rgbMask)
+							+ (diff>>bitsPerColor bitAnd: rgbMask)
+							+ ((diff>>bitsPerColor)>>bitsPerColor bitAnd: rgbMask)].
+			bitCount _ bitCount + diff].
+		maskShifted _ maskShifted >> destPixSize.
+		sourceShifted _ sourceShifted >> destPixSize.
+		destShifted _ destShifted >> destPixSize].
 	^ destinationWord  "For no effect on dest"
