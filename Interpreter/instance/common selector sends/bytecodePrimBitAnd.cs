c750ce21eb09b4bb26092a79ad4e978bd0f087e0
jm <unknown>
1998-12-10 17:29:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimBitAnd.cs
+++ Interpreter/instance/common selector sends/bytecodePrimBitAnd.cs
@@ -1,5 +1,11 @@
 bytecodePrimBitAnd
 
+	successFlag _ true.
 	self externalizeIPandSP.
 	self primitiveBitAnd.
 	self internalizeIPandSP.
+	successFlag ifTrue: [^ self fetchNextBytecode "success"].
+
+	messageSelector _ self specialSelector: 14.
+	argumentCount _ 1.
+	self normalSend
