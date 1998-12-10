5fbda3ee8d9cb634203619da3b00579bcb142ae2
jm <unknown>
1998-12-10 17:34:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimPointY.cs
+++ Interpreter/instance/common selector sends/bytecodePrimPointY.cs
@@ -1,5 +1,11 @@
 bytecodePrimPointY
 
+	successFlag _ true.
 	self externalizeIPandSP.
 	self primitivePointY.
 	self internalizeIPandSP.
+	successFlag ifTrue: [^ self fetchNextBytecode "success"].
+
+	messageSelector _ self specialSelector: 31.
+	argumentCount _ 0.
+	self normalSend
