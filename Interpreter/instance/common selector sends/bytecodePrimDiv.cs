d8119762362ba4623570fca1a5c91547a4b1c844
jm <unknown>
1998-12-10 17:35:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimDiv.cs
+++ Interpreter/instance/common selector sends/bytecodePrimDiv.cs
@@ -1,5 +1,12 @@
 bytecodePrimDiv
+	| quotient |
+	successFlag _ true.
+	quotient _ self doPrimitiveDiv: (self internalStackValue: 1)
+							by: (self internalStackValue: 0).
+	successFlag ifTrue:
+		[self internalPop: 2 thenPush: (self integerObjectOf: quotient).
+		^ self fetchNextBytecode "success"].
 
-	self externalizeIPandSP.
-	self primitiveDiv.
-	self internalizeIPandSP.
+	messageSelector _ self specialSelector: 13.
+	argumentCount _ 1.
+	self normalSend
