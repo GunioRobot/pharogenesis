2a1d05a9b88978fa2ce31c9be88376305aad5481
jm <unknown>
1998-12-10 17:30:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimDivide.cs
+++ Interpreter/instance/common selector sends/bytecodePrimDivide.cs
@@ -1,26 +1,22 @@
 bytecodePrimDivide
-
 	| rcvr arg result |
 	rcvr _ self internalStackValue: 1.
 	arg _ self internalStackValue: 0.
-	(self areIntegers: rcvr and: arg) ifTrue: [
-		rcvr _ self integerValueOf: rcvr.
-		arg _ self integerValueOf: arg.
-		((arg ~= 0) and: [(rcvr \\ arg) = 0]) ifTrue: [
-			result _ rcvr // arg.  "generates C / operation"
-			(self isIntegerValue: result) ifTrue: [
-				self longAt: (localSP _ localSP - 4)
-						put: (self integerObjectOf: result).
-				^ nil
-			].
-		].
-	].
+	(self areIntegers: rcvr and: arg)
+		ifTrue: [rcvr _ self integerValueOf: rcvr.
+				arg _ self integerValueOf: arg.
+				((arg ~= 0) and: [(rcvr \\ arg) = 0]) ifTrue:
+					[result _ rcvr // arg.  "generates C / operation"
+					(self isIntegerValue: result) ifTrue:
+						[self internalPop: 2
+						thenPush: (self integerObjectOf: result).
+					^ self fetchNextBytecode "success"]]]
+		ifFalse: [successFlag _ true.
+				self externalizeIPandSP.
+				self primitiveFloatDivide: rcvr byArg: arg.
+				self internalizeIPandSP.
+				successFlag ifTrue: [^ self fetchNextBytecode "success"]].
 
-	self externalizeIPandSP.
-	successFlag _ true.
-	self primitiveFloatDivide.
-	successFlag ifFalse: [
-		successFlag _ true.
-		self primitiveDivide.
-	].
-	self internalizeIPandSP.
+	messageSelector _ self specialSelector: 9.
+	argumentCount _ 1.
+	self normalSend.
