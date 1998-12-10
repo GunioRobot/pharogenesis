5d3a771c48906ea0fabb32ff3164bbe3c51eb388
jm <unknown>
1998-12-10 17:29:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimBitShift.cs
+++ Interpreter/instance/common selector sends/bytecodePrimBitShift.cs
@@ -1,5 +1,11 @@
 bytecodePrimBitShift
 
+	successFlag _ true.
 	self externalizeIPandSP.
 	self primitiveBitShift.
 	self internalizeIPandSP.
+	successFlag ifTrue: [^ self fetchNextBytecode "success"].
+
+	messageSelector _ self specialSelector: 12.
+	argumentCount _ 1.
+	self normalSend
