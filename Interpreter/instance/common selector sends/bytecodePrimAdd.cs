981d2af561e52921631f1a8fb9a1c79c8d57757c
jm <unknown>
1998-12-10 17:28:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimAdd.cs
+++ Interpreter/instance/common selector sends/bytecodePrimAdd.cs
@@ -1,22 +1,19 @@
 bytecodePrimAdd
-
 	| rcvr arg result |
 	rcvr _ self internalStackValue: 1.
 	arg _ self internalStackValue: 0.
-	(self areIntegers: rcvr and: arg) ifTrue: [
-		result _ (self integerValueOf: rcvr) + (self integerValueOf: arg).
-		(self isIntegerValue: result) ifTrue: [
-			self longAt: (localSP _ localSP - 4)
-					put: (self integerObjectOf: result).
-			^ nil
-		].
-	].
+	(self areIntegers: rcvr and: arg)
+		ifTrue: [result _ (self integerValueOf: rcvr) + (self integerValueOf: arg).
+				(self isIntegerValue: result) ifTrue:
+					[self internalPop: 2
+						thenPush: (self integerObjectOf: result).
+					^ self fetchNextBytecode "success"]]
+		ifFalse: [successFlag _ true.
+				self externalizeIPandSP.
+				self primitiveFloatAdd: rcvr toArg: arg.
+				self internalizeIPandSP.
+				successFlag ifTrue: [^ self fetchNextBytecode "success"]].
 
-	self externalizeIPandSP.
-	successFlag _ true.
-	self primitiveFloatAdd.
-	successFlag ifFalse: [
-		successFlag _ true.
-		self primitiveAdd.
-	].
-	self internalizeIPandSP.
+	messageSelector _ self specialSelector: 0.
+	argumentCount _ 1.
+	self normalSend
