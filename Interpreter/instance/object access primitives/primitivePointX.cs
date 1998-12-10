f29e0fc1bbdf3f099c2acdc4eaf2d71ef8f08ffd
jm <unknown>
1998-12-10 18:49:00 +0000

    no message

--- Interpreter/instance/object access primitives/primitivePointX.cs
+++ Interpreter/instance/object access primitives/primitivePointX.cs
@@ -1,9 +1,8 @@
 primitivePointX
-
 	| rcvr | 
-	successFlag _ true.
+	self inline: false.
 	rcvr _ self popStack.
 	self assertClassOf: rcvr is: (self splObj: ClassPoint).
 	successFlag
 		ifTrue: [self push: (self fetchPointer: XIndex ofObject: rcvr)]
-		ifFalse: [self unPop: 1.  self failSpecialPrim: 0  "will fail"]
+		ifFalse: [self unPop: 1]
