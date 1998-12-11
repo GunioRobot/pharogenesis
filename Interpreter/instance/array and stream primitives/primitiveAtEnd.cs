164ef9a72d94c20b9e61221fc64ae41a77dc2b13
di <unknown>
1998-12-11 10:15:00 +0000

    no message

--- Interpreter/instance/array and stream primitives/primitiveAtEnd.cs
+++ Interpreter/instance/array and stream primitives/primitiveAtEnd.cs
@@ -1,15 +1,11 @@
 primitiveAtEnd
-	| stream array index limit arrayClass size |
+	| stream index limit |
 	stream _ self popStack.
 	successFlag _ ((self isPointers: stream)
 			and: [(self lengthOf: stream) >= (StreamReadLimitIndex+1)]).
  	successFlag ifTrue: [
-		array _ self fetchPointer: StreamArrayIndex ofObject: stream.
 		index _ self fetchInteger: StreamIndexIndex ofObject: stream.
-		limit _ self fetchInteger: StreamReadLimitIndex ofObject: stream.
-		arrayClass _ self fetchClassOf: array.
-		self success: (self okStreamArrayClass: arrayClass).
-		size _ self stSizeOf: array].
+		limit _ self fetchInteger: StreamReadLimitIndex ofObject: stream].
  	successFlag
-		ifTrue: [self pushBool: (index >= limit) | (index >= size)]
+		ifTrue: [self pushBool: (index >= limit)]
 		ifFalse: [self unPop: 1].
