f4fdb81249ab7fcb29f04b39ee7da7630d4418b6
jm <unknown>
1998-12-11 07:58:00 +0000

    no message

--- Interpreter/instance/stack bytecodes/storeAndPopReceiverVariableBytecode.cs
+++ Interpreter/instance/stack bytecodes/storeAndPopReceiverVariableBytecode.cs
@@ -2,6 +2,8 @@ storeAndPopReceiverVariableBytecode
 	"Note: This code uses storePointerUnchecked:ofObject:withValue: and does the store check explicitely in order to help the translator produce better code."
 
 	| rcvr top |
+	self fetchNextBytecode.
+	"this bytecode will be expanded so that refs to currentBytecode below will be constant"
 	rcvr _ receiver.
 	top _ self internalStackTop.
 	(rcvr < youngStart) ifTrue: [
