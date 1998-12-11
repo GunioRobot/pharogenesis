471b378255e7870c02b47d6b8b4d6dd37ad469ef
di <unknown>
1998-12-11 17:17:00 +0000

    no message

--- InterpreterSimulator/instance/testing/test.cs
+++ InterpreterSimulator/instance/testing/test.cs
@@ -1,11 +1,9 @@
 test
-
 	Transcript clear.
 	byteCount _ 0.
 	self internalizeIPandSP.
-	[true] whileTrue: [
-		currentBytecode _ self fetchByte.
-		self dispatchOn: currentBytecode in: BytecodeTable.
-		byteCount _ byteCount + 1.
-	].
+	self fetchNextBytecode.
+	[true] whileTrue:
+		[self dispatchOn: currentBytecode in: BytecodeTable.
+		byteCount _ byteCount + 1].
 	self externalizeIPandSP.
