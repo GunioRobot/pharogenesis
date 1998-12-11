51f51a53d678da3f796e1dccb475072ac3b6fe7a
jm <unknown>
1998-12-11 07:59:00 +0000

    no message

--- Interpreter/instance/stack bytecodes/storeAndPopTemporaryVariableBytecode.cs
+++ Interpreter/instance/stack bytecodes/storeAndPopTemporaryVariableBytecode.cs
@@ -1,6 +1,8 @@
 storeAndPopTemporaryVariableBytecode
 
+	self fetchNextBytecode.
+	"this bytecode will be expanded so that refs to currentBytecode below will be constant"
 	self storePointerUnchecked: (currentBytecode bitAnd: 7) + TempFrameStart
-		ofObject: theHomeContext
+		ofObject: localHomeContext
 		withValue: self internalStackTop.
 	self internalPop: 1.
