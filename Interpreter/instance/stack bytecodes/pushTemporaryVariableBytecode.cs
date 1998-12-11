338c1322c80284ff0707db7db627f23bbe7e740c
jm <unknown>
1998-12-11 07:57:00 +0000

    no message

--- Interpreter/instance/stack bytecodes/pushTemporaryVariableBytecode.cs
+++ Interpreter/instance/stack bytecodes/pushTemporaryVariableBytecode.cs
@@ -1,3 +1,5 @@
 pushTemporaryVariableBytecode
 
+	self fetchNextBytecode.
+	"this bytecode will be expanded so that refs to currentBytecode below will be constant"
 	self pushTemporaryVariable: (currentBytecode bitAnd: 16rF).
