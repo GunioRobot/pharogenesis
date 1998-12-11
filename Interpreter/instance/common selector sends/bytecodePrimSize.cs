505b69a03e4d5bfd8ac6d6330a0072fd57e4a6d4
di <unknown>
1998-12-11 10:22:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimSize.cs
+++ Interpreter/instance/common selector sends/bytecodePrimSize.cs
@@ -1,11 +1,4 @@
 bytecodePrimSize
-	"See the comment in bytePrimitiveAt"
-
-	| arrayClass |
-	self externalizeIPandSP.
-	successFlag _ true.
-	arrayClass _ self fetchClassOf: (self stackValue: 0).
-	(self okStreamArrayClass: arrayClass)
-		ifTrue: [self primitiveSize]
-		ifFalse: [self failSpecialPrim: 0].
-	self internalizeIPandSP.
+	messageSelector _ self specialSelector: 18.
+	argumentCount _ 0.
+	self normalSend
