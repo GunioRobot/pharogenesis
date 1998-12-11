6b9b58d73b22670b65b863a2e8894183b39da281
jm <unknown>
1998-12-11 07:56:00 +0000

    no message

--- Interpreter/instance/stack bytecodes/pushLiteralConstantBytecode.cs
+++ Interpreter/instance/stack bytecodes/pushLiteralConstantBytecode.cs
@@ -1,3 +1,5 @@
 pushLiteralConstantBytecode
 
+	self fetchNextBytecode.
+	"this bytecode will be expanded so that refs to currentBytecode below will be constant"
 	self pushLiteralConstant: (currentBytecode bitAnd: 16r1F).
