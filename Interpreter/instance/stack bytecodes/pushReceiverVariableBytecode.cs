1d40c12f61e8b332655655e296d85331b299d326
jm <unknown>
1998-12-11 07:57:00 +0000

    no message

--- Interpreter/instance/stack bytecodes/pushReceiverVariableBytecode.cs
+++ Interpreter/instance/stack bytecodes/pushReceiverVariableBytecode.cs
@@ -1,3 +1,5 @@
 pushReceiverVariableBytecode
 
+	self fetchNextBytecode.
+	"this bytecode will be expanded so that refs to currentBytecode below will be constant"
 	self pushReceiverVariable: (currentBytecode bitAnd: 16rF).
