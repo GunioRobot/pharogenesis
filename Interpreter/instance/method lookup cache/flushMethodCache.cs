e921c0aad877bf895e5f4ea458be5e10ecbbf31c
di <unknown>
1998-12-12 23:37:00 +0000

    no message

--- Interpreter/instance/method lookup cache/flushMethodCache.cs
+++ Interpreter/instance/method lookup cache/flushMethodCache.cs
@@ -2,4 +2,4 @@ flushMethodCache
 	"Flush the method cache. The method cache is flushed on every programming change and garbage collect."
 
 	1 to: MethodCacheSize do: [ :i | methodCache at: i put: 0 ].
-	mcProbe _ 0.
+	1 to: AtCacheTotalSize do: [ :i | atCache at: i put: 0 ].
