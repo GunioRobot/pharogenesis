641b8eddae7351e8e06af457fc7e4fe665ca2749
di <unknown>
1998-12-14 14:58:00 +0000

    no message

--- Interpreter/instance/array and stream primitives/primitiveNext.cs
+++ Interpreter/instance/array and stream primitives/primitiveNext.cs
@@ -1,31 +1,25 @@
 primitiveNext
+	"PrimitiveNext will succeed only if the stream's array is in the atCache.
+	Otherwise failure will lead to proper message lookup of at: and
+	subsequent installation in the cache if appropriate."
+	| stream array index limit result atIx |
+	stream _ self stackTop.
+	((self isPointers: stream)
+		and: [(self lengthOf: stream) >= (StreamReadLimitIndex + 1)])
+		ifFalse: [^ self primitiveFail].
 
-	| stream array index limit arrayClass stringy result |
-	stream _ self popStack.
-	successFlag _
-		((self isPointers: stream) and:
-		 [(self lengthOf: stream) >= (StreamReadLimitIndex + 1)]).
- 	successFlag ifTrue: [
-		array _ self fetchPointer: StreamArrayIndex ofObject: stream.
-		index _ self fetchInteger: StreamIndexIndex ofObject: stream.
-		limit _ self fetchInteger: StreamReadLimitIndex ofObject: stream.
-		arrayClass _ self fetchClassOf: array.
-		stringy _ arrayClass = (self splObj: ClassString).
-		stringy ifFalse: [
-			self success: (self okStreamArrayClass: arrayClass)].
-		self success: index < limit].
-	successFlag ifTrue: [
-		index _ index + 1.
-		self pushRemappableOop: stream.
-		result _ self stObject: array at: index.  "may cause GC!"
-		stream _ self popRemappableOop].
-	successFlag ifTrue: [
-		self storeInteger: StreamIndexIndex
-			ofObject: stream
-			withValue: index].
-	successFlag ifTrue: [
-		stringy
-			ifTrue: [self push: (self characterForAscii: result)]
-			ifFalse: [self push: result].
-	] ifFalse: [
-		self unPop: 1].
+	array _ self fetchPointer: StreamArrayIndex ofObject: stream.
+	index _ self fetchInteger: StreamIndexIndex ofObject: stream.
+	limit _ self fetchInteger: StreamReadLimitIndex ofObject: stream.
+	atIx _ array bitAnd: AtCacheMask.
+	(index < limit and: [(atCache at: atIx+AtCacheOop) = array])
+		ifFalse: [^ self primitiveFail].
+
+	"OK -- its not at end, and the array is in the cache"
+	index _ index + 1.
+	result _ self commonVariable: array at: index cacheIndex: atIx.
+	"Above may cause GC, so can't use stream, array etc. below it"
+	successFlag ifTrue:
+		[stream _ self stackTop.
+		self storeInteger: StreamIndexIndex ofObject: stream withValue: index.
+		^ self pop: 1 thenPush: result].
