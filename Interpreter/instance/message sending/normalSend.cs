2a6841ec0764c6211e8167f674035fa76447a0a4
jm <unknown>
1998-12-14 10:26:00 +0000

    no message

--- Interpreter/instance/message sending/normalSend.cs
+++ Interpreter/instance/message sending/normalSend.cs
@@ -3,12 +3,12 @@ normalSend
 	"Assume: messageSelector and argumentCount have been set, and that the receiver and arguments have been pushed onto the stack,"
 	"Note: This method is inlined into the interpreter dispatch loop."
 
-	| rcvrClass |
+	| rcvr |
 	self inline: true.
 	self sharedCodeNamed: 'commonSend' inCase: 131.
 
-	rcvrClass _ self fetchClassOf: (self internalStackValue: argumentCount).
-
-	self externalizeIPandSP.
-	self sendSelectorToClass: rcvrClass.
-	self internalizeIPandSP.
+	rcvr _ self internalStackValue: argumentCount.
+	lkupClass _ self fetchClassOf: rcvr.
+	self internalFindNewMethod.
+	self internalExecuteNewMethod.
+	self fetchNextBytecode.
