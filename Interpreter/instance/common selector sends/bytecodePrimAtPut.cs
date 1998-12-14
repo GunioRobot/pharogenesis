0435aa55fad9cda643a853baa7e1dd79744a34dd
di <unknown>
1998-12-14 15:10:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimAtPut.cs
+++ Interpreter/instance/common selector sends/bytecodePrimAtPut.cs
@@ -1,25 +1,22 @@
 bytecodePrimAtPut
-
-	| value valToStore index rcvr rcvrClass stringy |
-	value _ valToStore _ self internalStackTop.
+	"BytecodePrimAtPut will only succeed if the receiver is in the atCache.
+	Otherwise it will fail so that the more general primitiveAtPut will put it in the
+	cache after validating that message lookup results in a primitive response."
+	| index rcvr atIx value |
+	value _ self internalStackTop.
 	index _ self internalStackValue: 1.
 	rcvr _ self internalStackValue: 2.
-	successFlag _ self isIntegerObject: index.
-	successFlag ifTrue: [
-		rcvrClass _ self fetchClassOf: rcvr.
-		stringy _ rcvrClass = (self splObj: ClassString).
-		(stringy or: [self okArrayClass: rcvrClass])
-			ifFalse: [successFlag _ false]].
-	successFlag ifTrue: [
-		index _ self integerValueOf: index.
-		stringy ifTrue: [valToStore _ self asciiOfCharacter: value].
-		self stObject: rcvr at: index put: valToStore.
-	].
-	successFlag ifTrue: [
-		self internalPop: 3 thenPush: value.
-	] ifFalse: [
-		messageSelector _ self specialSelector: 17.
-		argumentCount _ 2.
-		self normalSend.
-	].
+	successFlag _ (self isIntegerObject: rcvr) not and: [self isIntegerObject: index].
+	successFlag ifTrue:
+		[atIx _ (rcvr bitAnd: AtCacheMask) + AtPutBase.  "Index into atPutCache"
+		(atCache at: atIx+AtCacheOop) = rcvr
+		ifTrue:
+			[self commonVariable: rcvr at: (self integerValueOf: index)
+							put: value cacheIndex: atIx.
+			successFlag ifTrue:
+				[self fetchNextBytecode.
+				^ self internalPop: 3 thenPush: value]]].
 
+	messageSelector _ self specialSelector: 17.
+	argumentCount _ 2.
+	self normalSend
