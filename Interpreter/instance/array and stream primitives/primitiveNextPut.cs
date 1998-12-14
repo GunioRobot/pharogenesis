aefcb7d7a2a6037fcde2709406b37ebeb2bc5af3
di <unknown>
1998-12-14 14:58:00 +0000

    no message

--- Interpreter/instance/array and stream primitives/primitiveNextPut.cs
+++ Interpreter/instance/array and stream primitives/primitiveNextPut.cs
@@ -1,25 +1,24 @@
 primitiveNextPut
-	| value stream index limit array arrayClass storeVal |
-	value _ self popStack.
-	stream _ self popStack.
-	successFlag _ ((self isPointers: stream)
-			and: [(self lengthOf: stream) >= (StreamWriteLimitIndex+1)]).
- 	successFlag ifTrue: [
-		array _ self fetchPointer: StreamArrayIndex ofObject: stream.
-		index _ self fetchInteger: StreamIndexIndex ofObject: stream.
-		limit _ self fetchInteger: StreamWriteLimitIndex ofObject: stream.
-		arrayClass _ self fetchClassOf: array.
-		self success: (self okStreamArrayClass: arrayClass).
-		self success: index < limit].
+	"PrimitiveNextPut will succeed only if the stream's array is in the atPutCache.
+	Otherwise failure will lead to proper message lookup of at:put: and
+	subsequent installation in the cache if appropriate."
+	| value stream index limit array atIx |
+	value _ self stackTop.
+	stream _ self stackValue: 1.
+	((self isPointers: stream)
+		and: [(self lengthOf: stream) >= (StreamReadLimitIndex + 1)])
+		ifFalse: [^ self primitiveFail].
+
+	array _ self fetchPointer: StreamArrayIndex ofObject: stream.
+	index _ self fetchInteger: StreamIndexIndex ofObject: stream.
+	limit _ self fetchInteger: StreamReadLimitIndex ofObject: stream.
+	atIx _ (array bitAnd: AtCacheMask) + AtPutBase.
+	(index < limit and: [(atCache at: atIx+AtCacheOop) = array])
+		ifFalse: [^ self primitiveFail].
+
+	"OK -- its not at end, and the array is in the cache"
+	index _ index + 1.
+	self commonVariable: array at: index put: value cacheIndex: atIx.
 	successFlag ifTrue:
-		[index _ index + 1.
-		arrayClass = (self splObj: ClassString)
-			ifTrue: [storeVal _ self asciiOfCharacter: value]
-			ifFalse: [storeVal _ value].
-		self stObject: array at: index put: storeVal].
-	successFlag ifTrue:
-		[self storeInteger: StreamIndexIndex ofObject: stream
-			withValue: index].
-	successFlag
-		ifTrue: [self push: value]
-		ifFalse: [self unPop: 2]
+		[self storeInteger: StreamIndexIndex ofObject: stream withValue: index.
+		^ self pop: 2 thenPush: value].
