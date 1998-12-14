15efae88cb6ae3f88bc8fbda40b602955ce7314e
jm <unknown>
1998-12-14 10:25:00 +0000

    no message

--- Interpreter/instance/message sending/superclassSend.cs
+++ Interpreter/instance/message sending/superclassSend.cs
@@ -3,12 +3,10 @@ superclassSend
 	"Assume: messageSelector and argumentCount have been set, and that the receiver and arguments have been pushed onto the stack,"
 	"Note: This method is inlined into the interpreter dispatch loop."
 
-	| superClass |
 	self inline: true.
 	self sharedCodeNamed: 'commonSupersend' inCase: 133.
 
-	superClass _ self superclassOf: (self methodClassOf: method).
-
-	self externalizeIPandSP.
-	self sendSelectorToClass: superClass.
-	self internalizeIPandSP.
+	lkupClass _ self superclassOf: (self methodClassOf: method).
+	self internalFindNewMethod.
+	self internalExecuteNewMethod.
+	self fetchNextBytecode.
