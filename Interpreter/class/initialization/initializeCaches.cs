18adec858a2c8da6ba3672371e54454789ad54e0
di <unknown>
1998-12-14 15:12:00 +0000

    no message

---
+++ Interpreter/class/initialization/initializeCaches.cs
@@ -0,0 +1,21 @@
+initializeCaches 
+	| atCacheEntrySize |
+	MethodCacheEntries _ 512. 
+	MethodCacheSelector _ 1.
+	MethodCacheClass _ 2.
+	MethodCacheMethod _ 3.
+	MethodCachePrim _ 4.
+	MethodCacheEntrySize _ 4.  "Must be power of two for masking scheme."
+	MethodCacheMask _ (MethodCacheEntries - 1) * MethodCacheEntrySize.
+	MethodCacheSize _ MethodCacheEntries * MethodCacheEntrySize.
+	CacheProbeMax _ 3.
+
+	AtCacheEntries _ 8.  "Must be power of two"
+	AtCacheOop _ 1.
+	AtCacheSize _ 2.
+	AtCacheFmt _ 3.
+	AtCacheFixedFields _ 4.
+	atCacheEntrySize _ 4.  "Must be power of two for masking scheme."
+	AtCacheMask _ (AtCacheEntries-1) * atCacheEntrySize.
+	AtPutBase _ AtCacheEntries * atCacheEntrySize.
+	AtCacheTotalSize _ AtCacheEntries * atCacheEntrySize * 2.
