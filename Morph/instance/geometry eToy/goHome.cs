030202c28570d4f7a17c59db81c082e9d8df5c17
tk <unknown>
1998-12-15 14:03:00 +0000

    no message

---
+++ Morph/instance/geometry eToy/goHome.cs
@@ -0,0 +1,11 @@
+goHome
+
+	| box |
+	(owner isInMemory and: [owner ~~ nil]) ifTrue: [
+		self visible ifTrue: [
+			box _ owner.
+			self left < box left ifTrue: [self position: box left@self position y].
+			self right > box right ifTrue: [self position: (box right - self width)@self position y].
+			self top < box top ifTrue: [self position: self position x@box top].
+			self bottom > box bottom ifTrue: [
+				self position: self position x@(box bottom - self height)]]].
