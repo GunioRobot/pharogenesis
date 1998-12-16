46f9155c7e0c42b7a5a1ec84e49a5355150de8af
sw <unknown>
1998-12-16 08:55:00 +0000

    no message

---
+++ TextMorphEditor/instance/events/recognizeCharacters.cs
@@ -0,0 +1,4 @@
+recognizeCharacters
+	"Recognize hand-written characters and put them into the receiving pane.  Invokes Alan's character recognizer."
+
+	self recognizeCharactersWhileMouseIn: morph recognizerArena
