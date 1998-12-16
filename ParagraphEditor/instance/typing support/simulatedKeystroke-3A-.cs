9d4cede9c37ccaf961bbcfbfd27fc40cd04727a6
sw <unknown>
1998-12-16 13:14:00 +0000

    no message

--- ParagraphEditor/instance/typing support/simulatedKeystroke-3A-.cs
+++ ParagraphEditor/instance/typing support/simulatedKeystroke-3A-.cs
@@ -1,5 +1,5 @@
 simulatedKeystroke: char
-	"Accept char as if it were struck on the keyboard. This version does not yet deal with command keys, and achieves update in the receiver's typically inactive window via the sledge-hammer of uncache-bits.  1/31/96 sw"
+	"Accept char as if it were struck on the keyboard.  This version does not yet deal with command keys, and achieves update in the receiver's typically inactive window via the sledge-hammer of uncache-bits."
 
 	self deselect.
 	self openTypeIn.
@@ -10,4 +10,6 @@ simulatedKeystroke: char
 	startBlock _ stopBlock copy.
 	self selectAndScroll.
 	self updateMarker.
-	view topView uncacheBits.
+	view ifNotNil:
+		[view topView uncacheBits
+		"in mvc, this makes sure the recognized character shows up in the pane right now; in morphic, a different mechanism is used for the same effect -- see TextMorphEditor method #recognizeCharactersWhileMouseIn:"]
