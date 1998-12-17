ddbfa48ef4ad459c278f3296c21ad09829803a3e
di <unknown>
1998-12-17 12:32:00 +0000

    no message

--- PolygonMorph/instance/geometry/containsPoint-3A-.cs
+++ PolygonMorph/instance/geometry/containsPoint-3A-.cs
@@ -1,15 +1,15 @@
 containsPoint: aPoint
 	(super containsPoint: aPoint) ifFalse: [^ false].
+
 	closed & color isTransparent not ifTrue:
-		[self filledForm colors: (Array with: Color white with:
-Color black).
-		^ (filledForm pixelValueAt: aPoint - bounds
-topLeft) = 1]
-	ifFalse:
-		[self lineSegmentsDo:
-			[:p1 :p2 | (aPoint onLineFrom: p1 to: p2 within: (2 max: borderWidth+1//2) asFloat)
-					ifTrue: [^ true]].
-		arrowForms ifNotNil: [arrowForms do:
-			[:f | (f pixelValueAt: aPoint - f offset) > 0
-					ifTrue: [^ true]]].
-		^ false]
+		[^ (self filledForm pixelValueAt: aPoint - bounds topLeft + 1) > 0].
+
+	self lineSegmentsDo:
+		[:p1 :p2 |
+		(aPoint onLineFrom: p1 to: p2 within: (2 max: borderWidth+1//2) asFloat)
+				ifTrue: [^ true]].
+
+	self arrowForms do:
+		[:f | (f pixelValueAt: aPoint - f offset) > 0 ifTrue: [^ true]].
+
+	^ false
