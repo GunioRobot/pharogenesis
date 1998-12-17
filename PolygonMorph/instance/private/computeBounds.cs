17b909b57c9ca1b1c49050c31138985ce18fadb7
di <unknown>
1998-12-17 13:09:00 +0000

    no message

--- PolygonMorph/instance/private/computeBounds.cs
+++ PolygonMorph/instance/private/computeBounds.cs
@@ -1,10 +1,9 @@
 computeBounds
 	self changed.
-	bounds _ self curveBounds.
 	self releaseCachedState.
-	self arrowForms ifNotNil:
-		[self arrowForms do:
-		[:f | bounds _ bounds merge: (f offset extent: f extent)]].
+	bounds _ self curveBounds.
+	self arrowForms do:
+		[:f | bounds _ bounds merge: (f offset extent: f extent)].
 	handles ifNotNil: [self updateHandles].
 	self layoutChanged.
 	self changed
