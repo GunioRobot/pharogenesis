ad29ea84a181c084dc58b1118375bcc645b5de9f
di <unknown>
1998-12-17 13:44:00 +0000

    no message

--- CurveMorph/instance/private/updateHandles.cs
+++ CurveMorph/instance/private/updateHandles.cs
@@ -1,12 +1,12 @@
 updateHandles
-	| midPts nextVertIx tweens newVert p2i |
+	| midPts nextVertIx tweens newVert |
 	midPts _ OrderedCollection new.
 	nextVertIx _ 2.
 	tweens _ OrderedCollection new.
 	self lineSegmentsDo:
-		[:p1 :p2 | p2i _ p2 asIntegerPoint.
-		tweens addLast: p2i.
-		p2i = (vertices atWrap: nextVertIx) ifTrue:
+		[:p1 :p2 | 
+		tweens addLast: p2 asIntegerPoint.
+		p2 = (vertices atWrap: nextVertIx) ifTrue:
 			["Found endPoint."
 			midPts addLast: (tweens at: tweens size // 2)
 						+ (tweens at: tweens size + 1 // 2) // 2.
