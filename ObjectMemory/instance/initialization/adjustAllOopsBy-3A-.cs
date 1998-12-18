be3ee0c1fa9a8606af95c07a8b0197c08e81ee2b
di <unknown>
1998-12-18 13:25:00 +0000

    no message

--- ObjectMemory/instance/initialization/adjustAllOopsBy-3A-.cs
+++ ObjectMemory/instance/initialization/adjustAllOopsBy-3A-.cs
@@ -1,14 +1,18 @@
 adjustAllOopsBy: bytesToShift
 	"Adjust all oop references by the given number of bytes. This is done just after reading in an image when the new base address of the object heap is different from the base address in the image."
+	"ar 10/7/1998 - Clear the RootBit of all objects"
 
-	| oop last |
-	bytesToShift = 0 ifTrue: [ ^ nil ].
+	| oop header |
+	"Note: Don't bypass this method even if bytesToShift is zero 
+		until the RootBit problem has been fixed in the appropriate places."
+	"bytesToShift = 0 ifTrue: [ ^ nil ]."
 
 	oop _ self firstObject.
 	[oop < endOfMemory] whileTrue: [
 		(self isFreeObject: oop) ifFalse: [
+			header _ self longAt: oop.
+			self longAt: oop put: (header bitAnd: AllButRootBit).
 			self adjustFieldsAndClassOf: oop by: bytesToShift.
  		].
-		last _ oop.
 		oop _ self objectAfter: oop.
 	].
