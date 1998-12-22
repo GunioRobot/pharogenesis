953b88fedf85a6eebbba9caa9db7273c87e8f357
sw <unknown>
1998-12-22 00:16:00 +0000

    no message

--- StringHolder/instance/initialize-2D-release/openLabel-3A-.cs
+++ StringHolder/instance/initialize-2D-release/openLabel-3A-.cs
@@ -1,19 +1,3 @@
 openLabel: aString 
-	"Create a standard system view of the model, me, a StringHolder and open it."
-	| topView codeView |
-
-	World ifNotNil: [^ self openAsMorphLabel: aString].
-
-	topView _ (StandardSystemView new) model: self.
-	topView borderWidth: 1.
-	topView label: aString.
-	topView minimumSize: 100 @ 50.
-
-	codeView _ PluggableTextView on: self 
-			text: #contents accept: #acceptContents:
-			readSelection: #contentsSelection menu: #codePaneMenu:shifted:.
-	codeView window: (0@0 extent: 200@200).
-	topView addSubView: codeView.
-	"self contents size > 0 ifTrue: [
-			codeView hasUnacceptedEdits: true].  Is it already saved or not??"
-	topView controller open
+	"Create a standard system view of the model, me, a StringHolder and open it.  If in mvc, terminate the active controller so that the new window will immediately be activated."
+	self openLabel: aString andTerminate: true
