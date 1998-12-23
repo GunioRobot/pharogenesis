6520f4b7e1301f1aa0e7537d4c8460e75704ab46
di <unknown>
1998-12-23 15:57:00 +0000

    no message

---
+++ BookPageThumbnailMorph/instance/as yet unclassified/encounteredAtTime-3A-inScorePlayer-3A-atIndex-3A-inEventTrack-3A-secsPerTick-3A-.cs
@@ -0,0 +1,3 @@
+encounteredAtTime: ticks inScorePlayer: scorePlayer atIndex: index inEventTrack: track secsPerTick: secsPerTick
+	"Flip to this page with no extra sound"
+	BookMorph turnOffSoundWhile: [self doPageFlip]
