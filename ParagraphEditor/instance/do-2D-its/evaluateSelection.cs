9c44a394fd1072307a9939f4e6fef3f659d0b2fc
sw <unknown>
1998-12-24 12:42:00 +0000

    no message

--- ParagraphEditor/instance/do-2D-its/evaluateSelection.cs
+++ ParagraphEditor/instance/do-2D-its/evaluateSelection.cs
@@ -2,7 +2,8 @@ evaluateSelection
 	"Treat the current selection as an expression; evaluate it and return the result"
 	| result rcvr ctxt |
 	self lineSelectAndEmptyCheck: [^ ''].
-	(model isKindOf: Model)
+
+	(model respondsTo: #doItReceiver) 
 		ifTrue: [FakeClassPool adopt: model selectedClass.  "Include model pool vars if any"
 				rcvr _ model doItReceiver.
 				ctxt _ model doItContext]
