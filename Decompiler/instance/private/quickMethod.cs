6cee988023b094cdfc08e1e787ca37344e9dc977
di <unknown>
1998-12-26 21:29:00 +0000

    no message

--- Decompiler/instance/private/quickMethod.cs
+++ Decompiler/instance/private/quickMethod.cs
@@ -1,8 +1,9 @@
 quickMethod
-	^ method isReturnSpecial
-		ifTrue: [constructor codeBlock:
-				(Array with: (constTable at: method primitive - 255)) returns: true]
-		ifFalse: [method isReturnField
-			ifTrue: [constructor codeBlock:
-				(Array with: (constructor codeInst: method returnField)) returns: true]
-			ifFalse: [self error: 'improper short method']]
+	| |
+	method isReturnSpecial
+		ifTrue: [^ constructor codeBlock:
+				(Array with: (constTable at: method primitive - 255)) returns: true].
+	method isReturnField
+		ifTrue: [^ constructor codeBlock:
+				(Array with: (constructor codeInst: method returnField)) returns: true].
+	self error: 'improper short method'
