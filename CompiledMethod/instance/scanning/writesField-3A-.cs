2c2275e5c414355fc997cb89fd6b4354ca82c756
di <unknown>
1998-12-26 21:30:00 +0000

    no message

--- CompiledMethod/instance/scanning/writesField-3A-.cs
+++ CompiledMethod/instance/scanning/writesField-3A-.cs
@@ -2,8 +2,9 @@ writesField: field
 	"Answer whether the receiver stores into the instance variable indexed 
 	by the argument."
 
-	self isQuick ifTrue: [^false].
-	field <= 8 ifTrue: [^ (self scanFor: 96 + field - 1)
-						or: [self scanLongStore: field - 1]].
-	field <= 64 ifTrue: [^ self scanLongStore: field - 1].
+	self isQuick ifTrue: [^ false].
+	field <= 8 ifTrue:
+		[^ (self scanFor: 96 + field - 1) or: [self scanLongStore: field - 1]].
+	field <= 64 ifTrue:
+		[^ self scanLongStore: field - 1].
 	^ self scanVeryLongStore: 160 offset: field - 1
