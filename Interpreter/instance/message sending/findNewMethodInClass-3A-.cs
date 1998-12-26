c8e6451c18f4eb7547617af2f2853a93e48a3ed4
di <unknown>
1998-12-26 14:40:00 +0000

    no message

--- Interpreter/instance/message sending/findNewMethodInClass-3A-.cs
+++ Interpreter/instance/message sending/findNewMethodInClass-3A-.cs
@@ -1,13 +1,11 @@
 findNewMethodInClass: class
 	"Find the compiled method to be run when the current messageSelector is sent to the given class, setting the values of 'newMethod' and 'primitiveIndex'."
-
 	| ok |
-	self inline: true.
+	self inline: false.
 	ok _ self lookupInMethodCacheSel: messageSelector class: class.
 	ok ifFalse: [
 		"entry was not found in the cache; look it up the hard way"
 		self lookupMethodInClass: class.
-		primitiveIndex _ self primitiveIndexOf: newMethod.
 		self addToMethodCacheSel: messageSelector
 			class: class
 			method: newMethod
