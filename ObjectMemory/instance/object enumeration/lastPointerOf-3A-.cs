0bd39db765383a709e03301eb7428bbb4bb0f573
di <unknown>
1998-12-27 23:17:00 +0000

    no message

--- ObjectMemory/instance/object enumeration/lastPointerOf-3A-.cs
+++ ObjectMemory/instance/object enumeration/lastPointerOf-3A-.cs
@@ -1,15 +1,17 @@
-lastPointerOf: objectPointer
+lastPointerOf: oop
 	"Return the byte offset of the last pointer field of the given object. Works with CompiledMethods, as well as ordinary objects. Can be used even when the type bits are not correct."
-
-	| fmt sz methodHeader |
+	| fmt sz methodHeader header |
 	self inline: true.
-	fmt _ self formatOf: objectPointer.
-	fmt < 4 ifTrue: [
-		sz _ self sizeBitsOfSafe: objectPointer.
-		^ sz - BaseHeaderSize  "all pointers"
-	].
+	header _ self baseHeader: oop.
+	fmt _ (header >> 8) bitAnd: 16rF.
+	fmt <= 4 ifTrue:
+		[(fmt = 3 and: [self isContextHeader: header]) ifTrue:
+			["contexts end at the stack pointer"
+			^ (CtxtTempFrameStart + (self fetchStackPointerOf: oop)) * 4].
+		sz _ self sizeBitsOfSafe: oop.
+		^ sz - BaseHeaderSize  "all pointers"].
 	fmt < 12 ifTrue: [ ^0 ].  "no pointers"
 
 	"CompiledMethod: contains both pointers and bytes:"
-	methodHeader _ self longAt: objectPointer + BaseHeaderSize.
+	methodHeader _ self longAt: oop + BaseHeaderSize.
 	^ ((methodHeader >> 10) bitAnd: 16rFF) * 4 + BaseHeaderSize
