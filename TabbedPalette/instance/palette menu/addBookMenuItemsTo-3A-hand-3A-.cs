c529639e2b47041a05665c1c21a1919e6b964220
sw <unknown>
1998-12-27 23:33:00 +0000

    no message

---
+++ TabbedPalette/instance/palette menu/addBookMenuItemsTo-3A-hand-3A-.cs
@@ -0,0 +1,3 @@
+addBookMenuItemsTo: aCustomMenu hand: aHandMorph
+	aCustomMenu add: 'add palette menu' action: #addMenuTab.
+	aCustomMenu add: 'become the Standard palette' action: #becomeStandardPalette
