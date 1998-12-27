d6a5755b1a5c5a702449d9d732bbe5c589dd639c
di <unknown>
1998-12-27 23:18:00 +0000

    no message

--- ObjectMemory/instance/gc -2D--2D- compaction/lastPointerWhileForwarding-3A-.cs
+++ ObjectMemory/instance/gc -2D--2D- compaction/lastPointerWhileForwarding-3A-.cs
@@ -6,19 +6,21 @@ lastPointerWhileForwarding: oop
 	header _ self longAt: oop.
 	(header bitAnd: MarkBit) ~= 0 ifTrue: [
 		"oop is forwarded; get its real header from its forwarding table entry"
-		fwdBlock _ header bitAnd: AllButMarkBitAndTypeMask.
-		checkAssertions ifTrue: [ self fwdBlockValidate: fwdBlock ].
+		fwdBlock _ (header bitAnd: AllButMarkBitAndTypeMask) << 1.
+		DoAssertionChecks ifTrue: [ self fwdBlockValidate: fwdBlock ].
 		header _ self longAt: fwdBlock + 4.
 	].
 
 	fmt _ (header >> 8) bitAnd: 16rF.
-	fmt < 4 ifTrue: [
+	fmt <= 4 ifTrue:
+		[(fmt = 3 and: [self isContextHeader: header]) ifTrue:
+			["contexts end at the stack pointer"
+			^ (CtxtTempFrameStart + (self fetchStackPointerOf: oop)) * 4].
 		"do sizeBitsOf: using the header we obtained"
 		(header bitAnd: TypeMask) = HeaderTypeSizeAndClass
-			ifTrue: [ size _ (self sizeHeader: oop) bitAnd: 16rFFFFFFC ]
-			ifFalse: [ size _ header bitAnd: 16rFC ].
-		^ size - BaseHeaderSize
-	].
+			ifTrue: [ size _ (self sizeHeader: oop) bitAnd: AllButTypeMask ]
+			ifFalse: [ size _ header bitAnd: SizeMask ].
+		^ size - BaseHeaderSize].
 	fmt < 12 ifTrue: [ ^ 0 ].  "no pointers"
 
 	methodHeader _ self longAt: oop + BaseHeaderSize.
