532f01fbb1cc34bc8a239835247de84888456b92
di <unknown>
1998-12-27 17:06:00 +0000

    no message

--- Interpreter/instance/array and stream primitives/commonAtPut-3A-.cs
+++ Interpreter/instance/array and stream primitives/commonAtPut-3A-.cs
@@ -1,22 +1,32 @@
 commonAtPut: stringy
-	"See the comment in commonAt:."
-
-	| value valToStore index rcvr |
-	self inline: true.
-	value _ valToStore _ self stackTop.
-	index _ self stackValue: 1.
+	"This code is called if the receiver responds primitively to at:Put:.
+	If this is so, it will be installed in the atPutCache so that subsequent calls of at:
+	or  next may be handled immediately in bytecode primitive routines."
+	| value index rcvr atIx |
+	value _ self stackValue: 0.
+	index _ self positive32BitValueOf: (self stackValue: 1).  "Sets successFlag"
 	rcvr _ self stackValue: 2.
-	(self isIntegerObject: index) & (self isIntegerObject: rcvr) not ifTrue: [
-		index _ self integerValueOf: index.
-		stringy ifTrue: [valToStore _ self asciiOfCharacter: value].
-		self stObject: rcvr at: index put: valToStore.
-	] ifFalse: [
-		successFlag _ false.
-	].
-	successFlag ifTrue: [
-		self pop: 3 thenPush: value.
-	] ifFalse: [
-		stringy
-			ifTrue: [self failSpecialPrim: 64]
-			ifFalse: [self failSpecialPrim: 61].
-	].
+	successFlag & (self isIntegerObject: rcvr) not
+		ifFalse: [^ self primitiveFail].
+
+	"NOTE:  The atPut-cache, since it is specific to the non-super response to #at:Put:.
+	Therefore we must determine that the message is #at:Put: (not, eg, #basicAt:Put:),
+	and that the send is not a super-send, before using the at-cache."
+	(messageSelector = (self specialSelector: 17)
+		and: [lkupClass = (self fetchClassOfNonInt: rcvr)])
+		ifTrue:
+		["OK -- look in the at-cache"
+		atIx _ (rcvr bitAnd: AtCacheMask) + AtPutBase.  "Index into atPutCache"
+		(atCache at: atIx+AtCacheOop) = rcvr ifFalse:
+			["Rcvr not in cache.  Install it..."
+			self install: rcvr inAtCache: atCache at: atIx string: stringy].
+		successFlag ifTrue:
+			[self commonVariable: rcvr at: index put: value cacheIndex: atIx].
+		successFlag ifTrue:
+			[^ self pop: 3 thenPush: value]].
+
+	"The slow but sure way..."
+	successFlag _ true.
+	stringy ifTrue: [self stObject: rcvr at: index put: (self asciiOfCharacter: value)]
+			ifFalse: [self stObject: rcvr at: index put: value].
+	successFlag ifTrue: [^ self pop: 3 thenPush: value].
