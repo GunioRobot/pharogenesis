6a5b8a00d06fdb82e08639bec922974b58a9da62
sw <unknown>
1998-12-28 10:57:00 +0000

    no message

---
+++ Player/class/scripts/permanentUserScriptFor-3A-player-3A-.cs
@@ -0,0 +1,7 @@
+permanentUserScriptFor: aSelector player: aPlayer
+	|  entry |
+	scripts ifNil: [scripts _ IdentityDictionary new].
+	self flag: #deferred.  "That anonymous script will of course contain refs to aPlayer who may well not be the class's prototype.  So if it is then saved, there's a problem"
+	entry _ UserScript new initializePermanentScriptFor: aPlayer.
+	scripts at: aSelector put: entry.
+	^ entry
