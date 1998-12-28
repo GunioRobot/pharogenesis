a771fd6a38d95483b01229138f97837faff5ecaf
di <unknown>
1998-12-28 15:35:00 +0000

    no message

--- InterpreterSimulator/instance/testing/profileSends-3A-.cs
+++ InterpreterSimulator/instance/testing/profileSends-3A-.cs
@@ -1,13 +1,13 @@
 profileSends: nBytecodes
 	"(InterpreterSimulator new openOn: 'clonex.image') profileSends: 5000"
-	Transcript clear.
 	byteCount _ 0.
 	MessageTally tallySendsTo: self inBlock: [
 		self internalizeIPandSP.
-		[byteCount < nBytecodes] whileTrue: [
-			currentBytecode _ self fetchByte.
-			self dispatchOn: currentBytecode in: BytecodeTable.
+		self fetchNextBytecode.
+		[byteCount < nBytecodes] whileTrue:
+			[self dispatchOn: currentBytecode in: BytecodeTable.
 			byteCount _ byteCount + 1.
-		].
+			byteCount \\ 100 = 0 ifTrue: [byteCount printString , '  ' displayAt: 0@0]].
 		self externalizeIPandSP.
 	] showTree: true.
+	self close
