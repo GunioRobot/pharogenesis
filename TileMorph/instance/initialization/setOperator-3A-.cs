be75e734da67324820bb2ee908ff0ee33f5a7597
sw <unknown>
1998-12-30 14:12:00 +0000

    no message

--- TileMorph/instance/initialization/setOperator-3A-.cs
+++ TileMorph/instance/initialization/setOperator-3A-.cs
@@ -3,6 +3,7 @@ setOperator: aString
 	type _ #operator.
 	operatorOrExpression _ aString asSymbol.
  	self line1: (ScriptingSystem wordingForOperator: aString).
-	operatorOrExpression isInfix ifTrue: [self addArrows; updateLiteralLabel].
+	(ScriptingSystem doesOperatorWantArrows: operatorOrExpression)
+		ifTrue: [self addArrows; updateLiteralLabel].
 
 	"operatorOrExpression == #heading ifTrue: [self halt]."
