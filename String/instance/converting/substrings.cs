4146c00fe4904202a8c6584a1124348e67fc3f94
ls <unknown>
1999-01-03 13:36:00 +0000

    no message

--- String/instance/converting/substrings.cs
+++ String/instance/converting/substrings.cs
@@ -1,11 +1,25 @@
 substrings
 	"Answer an array of the substrings that compose the receiver."
-	| result aStream char |
+	| result end beginning |
+
 	result _ WriteStream on: (Array new: 10).
-	aStream _ WriteStream on: (String new: 16).
-	1 to: self size do: [:i |
-		((char _ self at: i) isSeparator)
-		 ifTrue: [aStream isEmpty ifFalse: [result nextPut: aStream contents. aStream reset]]
-		 ifFalse: [aStream nextPut: char]].
-	aStream isEmpty ifFalse: [result nextPut: aStream contents].
-	^ result contents
+
+
+
+	end _ 0.
+	"find one substring each time through this loop"
+	[ 
+		"find the beginning of the next substring"
+		beginning _ self indexOfAnyOf: CSNonSeparators startingAt: end+1 ifAbsent: [ nil ].
+		beginning ~~ nil ] 
+	whileTrue: [
+		"find the end"
+		end _ self indexOfAnyOf: CSSeparators startingAt: beginning ifAbsent: [ self size + 1 ].
+		end _ end - 1.
+
+		result nextPut: (self copyFrom: beginning to: end).
+
+	].
+
+
+	^result contents
