da8bf847c53e18c431f79b46ba15933c79627d7f
ikp <unknown>
1999-01-03 17:29:00 +0000

    no message

--- Interpreter/instance/message sending/executeNewMethod.cs
+++ Interpreter/instance/message sending/executeNewMethod.cs
@@ -1,9 +1,11 @@
 executeNewMethod
 
-	(primitiveIndex = 0 or: [self primitiveResponse not]) ifTrue: [
-		"if not primitive, or primitive failed, activate the method"
-		self activateNewMethod.
+	primitiveIndex > 0 ifTrue: [
+		self primitiveResponse.
+		successFlag ifTrue: [^ nil]].
 
-		"check for possible interrupts at each real send"
-		self quickCheckForInterrupts.
-	].
+	"if not primitive, or primitive failed, activate the method"
+	self activateNewMethod.
+
+	"check for possible interrupts at each real send"
+	self quickCheckForInterrupts.
