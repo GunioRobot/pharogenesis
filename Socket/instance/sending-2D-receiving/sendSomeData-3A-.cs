4056b3602c1b62e36c72121931708a7f64b7fcd2
ls <unknown>
1999-01-05 15:05:00 +0000

    no message

--- Socket/instance/sending-2D-receiving/sendSomeData-3A-.cs
+++ Socket/instance/sending-2D-receiving/sendSomeData-3A-.cs
@@ -1,13 +1,8 @@
 sendSomeData: aStringOrByteArray
-	"Send as much of the given data as possible and return the number of bytes actually sent."
-	"Note: This operation may have to be repeated many times to send a large amount of data."
+	"Send as much of the given data as possible and answer the number of bytes actually sent."
+	"Note: This operation may have to be repeated multiple times to send a large amount of data."
 
-	| bytesSent |
-	(self waitForSendDoneUntil: (Socket deadlineSecs: 20))
-		ifTrue: [
-			bytesSent _ self primSocket: socketHandle
-				sendData: aStringOrByteArray
-				startIndex: 1
-				count: aStringOrByteArray size]
-		ifFalse: [self error: 'send data timeout; data not sent'].
-	^ bytesSent
+	^ self
+		sendSomeData: aStringOrByteArray
+		startIndex: 1
+		count: aStringOrByteArray size
