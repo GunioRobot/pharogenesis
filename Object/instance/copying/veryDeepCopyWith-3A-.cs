45208ae96c1206ae1479592e1efa76e862db4245
tk <unknown>
1999-01-06 16:30:00 +0000

    no message

---
+++ Object/instance/copying/veryDeepCopyWith-3A-.cs
@@ -0,0 +1,39 @@
+veryDeepCopyWith: deepCopier
+	"Copy me and the entire tree of objects I point to.  An object in the tree twice is copied once, and both references point to him.  deepCopier holds a dictionary of objects we have seen.  Some classes refuse to be copied.  Some classes are picky about which fields get deep copied."
+	| class index sub subAss new uc sup has mine |
+	deepCopier references at: self ifPresent: [:newer | ^ newer]. 	"already did him"
+	class _ self class.
+	class isMeta ifTrue: [^ self].		"a class"
+	new _ self clone.
+	class isSystemDefined ifFalse: [
+		uc _ deepCopier uniClasses at: class ifAbsent: [nil].
+		uc ifNil: [deepCopier uniClasses at: class put: (uc _ self copyUniClass)].
+		new _ uc new].
+	deepCopier references at: self put: new.	"remember"
+	(class isVariable and: [class isPointers]) ifTrue: 
+		[index _ self basicSize.
+		[index > 0] whileTrue: 
+			[sub _ self basicAt: index.
+			(subAss _ deepCopier references associationAt: sub ifAbsent: [nil])
+				ifNil: [new basicAt: index put: (sub veryDeepCopyWith: deepCopier)]
+				ifNotNil: [new basicAt: index put: subAss value].
+			index _ index - 1]].
+	"Ask each superclass if it wants to share (weak copy) any inst vars"
+	new veryDeepInner: deepCopier.		"does super a lot"
+
+	"other superclasses want all inst vars deep copied"
+	sup _ class.  index _ class instSize.
+	[has _ sup compiledMethodAt: #veryDeepInner: ifAbsent: [nil].
+	mine _ sup instVarNames.
+	has ifNotNil: [index _ index - mine size]
+		ifNil: [1 to: mine size do: [:xx |
+				sub _ self instVarAt: index.
+				(subAss _ deepCopier references associationAt: sub ifAbsent: [nil])
+						"use association, not value, so nil is an exceptional value"
+					ifNil: [new instVarAt: index put: 
+								(sub veryDeepCopyWith: deepCopier)]
+					ifNotNil: [new instVarAt: index put: subAss value].
+				index _ index - 1]].
+	(sup _ sup superclass) == nil] whileFalse.
+	new rehash.	"force Sets and Dictionaries to rehash"
+	^ new
