7792892975b0ba8b9c5c6c3ad7d6aa3bf179c6f0
jm <unknown>
1999-01-06 19:59:00 +0000

    no message

---
+++ MidiInputMorph/instance/as yet unclassified/addChannelControlsFor-3A-.cs
@@ -0,0 +1,22 @@
+addChannelControlsFor: channelIndex
+
+	| r divider col |
+	r _ self makeRow
+		hResizing: #shrinkWrap;
+		vResizing: #shrinkWrap.
+	r addMorphBack: (self channelNumAndMuteButtonFor: channelIndex).
+	r addMorphBack: (Morph new extent: 10@5; color: color).  "spacer"
+	r addMorphBack: (self panAndVolControlsFor: channelIndex).
+
+	divider _ AlignmentMorph new
+		extent: 10@1;
+		borderWidth: 1;
+		inset: 0;
+		borderColor: #raised;
+		color: color;
+		hResizing: #spaceFill;
+		vResizing: #rigid.
+
+	col _ self lastSubmorph.
+	col addMorphBack: divider.
+	col addMorphBack: r.
