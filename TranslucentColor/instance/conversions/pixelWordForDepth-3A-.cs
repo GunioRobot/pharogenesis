843a0d4acd8e4d3b9e0a43b89c166aefe3131f93
di <unknown>
1999-01-06 16:14:00 +0000

    no message

--- TranslucentColor/instance/conversions/pixelWordForDepth-3A-.cs
+++ TranslucentColor/instance/conversions/pixelWordForDepth-3A-.cs
@@ -1,8 +1,9 @@
 pixelWordForDepth: depth
-	"Return the pixel value for this color at the given depth. Translucency only works at a bit-depth of 32; this color will appear opaque at all other depths."
+	"Return the pixel value for this color at the given depth. Translucency only works in RGB; this color will appear either opaque or transparent at all other depths."
 
 	| basicPixelWord |
+	alpha = 0 ifTrue: [^ 0].
 	basicPixelWord _ super pixelWordForDepth: depth.
 	depth < 32
 		ifTrue: [^ basicPixelWord]
-		ifFalse: [^ basicPixelWord bitOr: (alpha bitShift: 24)].
+		ifFalse: [^ (basicPixelWord bitAnd: 16rFFFFFF) bitOr: (alpha bitShift: 24)].
