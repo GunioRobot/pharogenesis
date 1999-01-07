7221010ae6f36630a61bb5788a4e56773a5e14e0
tk <unknown>
1999-01-07 14:37:00 +0000

    no message

---
+++ MenuMorph/instance/copying/veryDeepFixupWith-3A-.cs
@@ -0,0 +1,6 @@
+veryDeepFixupWith: deepCopier
+	"If fields were weakly copied, fix them here.  If they were in the tree being copied, fix them up, otherwise point to the originals."
+
+super veryDeepFixupWith: deepCopier.
+defaultTarget _ deepCopier references at: defaultTarget ifAbsent: [defaultTarget].
+popUpOwner _ deepCopier references at: popUpOwner ifAbsent: [popUpOwner].
