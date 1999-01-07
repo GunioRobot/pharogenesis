cd5f21101dbc55221effcd8eb42fbdabf8fc7749
tk <unknown>
1999-01-07 15:40:00 +0000

    no message

---
+++ UpdatingStringMorphWithArgument/instance/copying/veryDeepFixupWith-3A-.cs
@@ -0,0 +1,6 @@
+veryDeepFixupWith: deepCopier
+	"If target and arguments fields were weakly copied, fix them here.  If they were in the tree being copied, fix them up, otherwise point to the originals!!"
+
+super veryDeepFixupWith: deepCopier.
+argumentTarget _ deepCopier references at: argumentTarget 
+			ifAbsent: [argumentTarget].
