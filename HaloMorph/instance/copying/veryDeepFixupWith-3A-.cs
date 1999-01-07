ad7eea205a296c3431231ebbb5a0def8cc0c7e75
tk <unknown>
1999-01-07 17:37:00 +0000

    no message

---
+++ HaloMorph/instance/copying/veryDeepFixupWith-3A-.cs
@@ -0,0 +1,6 @@
+veryDeepFixupWith: deepCopier
+	"If target and arguments fields were weakly copied, fix them here.  If they were in the tree being copied, fix them up, otherwise point to the originals!!"
+
+super veryDeepFixupWith: deepCopier.
+target _ deepCopier references at: target ifAbsent: [target].
+innerTarget _ deepCopier references at: innerTarget ifAbsent: [innerTarget].
