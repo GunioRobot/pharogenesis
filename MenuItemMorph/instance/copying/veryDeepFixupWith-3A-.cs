631eeffe67fed5af49a8a0723812b764a5f4082c
tk <unknown>
1999-01-07 14:54:00 +0000

    no message

---
+++ MenuItemMorph/instance/copying/veryDeepFixupWith-3A-.cs
@@ -0,0 +1,7 @@
+veryDeepFixupWith: deepCopier
+	"If target and arguments fields were weakly copied, fix them here.  If they were in the tree being copied, fix them up, otherwise point to the originals!!"
+
+super veryDeepFixupWith: deepCopier.
+target _ deepCopier references at: target ifAbsent: [target].
+arguments _ arguments collect: [:each |
+	deepCopier references at: each ifAbsent: [each]].
