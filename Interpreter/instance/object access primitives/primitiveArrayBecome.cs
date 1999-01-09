fe2c4c0fe9b3bd295dce1cb3cd00edcba1c163c4
di <unknown>
1999-01-09 15:30:00 +0000

    no message

--- Interpreter/instance/object access primitives/primitiveArrayBecome.cs
+++ Interpreter/instance/object access primitives/primitiveArrayBecome.cs
@@ -3,8 +3,8 @@ primitiveArrayBecome
 	to mutated classes and/or selectors."
 
 	| arg rcvr |
-	arg _ self popStack.
-	rcvr _ self stackTop.
-	self success: (self become: rcvr with: arg).
+	arg _ self stackTop.
+	rcvr _ self stackValue: 1.
+	self success: (self become: rcvr with: arg twoWay: true).
 	self flushMethodCache.
-	successFlag ifFalse: [ self unPop: 1 ].
+	successFlag ifTrue: [ self pop: 1 ].
