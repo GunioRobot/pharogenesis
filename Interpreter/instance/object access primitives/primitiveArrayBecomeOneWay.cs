1a27ff8d4f664103d45ed09c48ffc3a5fb35af71
di <unknown>
1999-01-09 15:30:00 +0000

    no message

---
+++ Interpreter/instance/object access primitives/primitiveArrayBecomeOneWay.cs
@@ -0,0 +1,10 @@
+primitiveArrayBecomeOneWay
+	"We must flush the method cache here, to eliminate stale references
+	to mutated classes and/or selectors."
+
+	| arg rcvr |
+	arg _ self stackTop.
+	rcvr _ self stackValue: 1.
+	self success: (self become: rcvr with: arg twoWay: false).
+	self flushMethodCache.
+	successFlag ifTrue: [ self pop: 1 ].
