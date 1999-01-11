5eea3d97b41dfdbe244cb5ca0c5b88e1a6c6ffab
di <unknown>
1999-01-11 21:48:00 +0000

    no message

---
+++ FlexMorph/instance/as yet unclassified/changeBorderWidth-3A-.cs
@@ -0,0 +1,12 @@
+changeBorderWidth: evt
+	| handle origin aHand |
+	aHand _ evt ifNil: [self primaryHand] ifNotNil: [evt hand].
+	origin _ aHand gridPointRaw.
+	handle _ HandleMorph new forEachPointDo:
+		[:newPoint | handle removeAllMorphs.
+		handle addMorph:
+			(PolygonMorph vertices: (Array with: origin with: newPoint)
+				color: Color black borderWidth: 1 borderColor: Color black).
+		self borderWidth: (newPoint - origin) abs // 5].
+	aHand attachMorph: handle.
+	handle startStepping
