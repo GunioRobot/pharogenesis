17ad1a2da3fb7d6f413c9457b71542f7b692fd86
di <unknown>
1999-01-11 10:24:00 +0000

    no message

--- MethodContext/instance/initialize-2D-release/restart.cs
+++ MethodContext/instance/initialize-2D-release/restart.cs
@@ -2,4 +2,4 @@ restart
 	"Reinitialize the receiver so that it is in the state it was at its creation."
 
 	pc _ method initialPC.
-	stackp _ method numArgs + method numTemps
+	self stackp: method numTemps
