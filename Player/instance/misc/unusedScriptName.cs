f3a28e42d441aa644da32d641ff12d7cedfa9a44
sw <unknown>
1999-01-12 16:02:00 +0000

    no message

--- Player/instance/misc/unusedScriptName.cs
+++ Player/instance/misc/unusedScriptName.cs
@@ -1,9 +1,9 @@
 unusedScriptName
-	"answer a name of the form 'sssScriptN', where N is one higher than the highest-numbered similarly-named script"
+	"answer a name of the form 'scriptN', where N is one higher than the highest-numbered similarly-named script"
 	| highestThus aPair |
 	highestThus _ 0.
 	self class tileScriptNames do:
 		[:aName |
 			aPair _ aName stemAndNumericSuffix.
-			aPair first = 'xxxScript' ifTrue: [highestThus _ highestThus max: aPair last]].
-	^ ('xxxScript', (highestThus + 1) printString) asSymbol
+			aPair first = 'script' ifTrue: [highestThus _ highestThus max: aPair last]].
+	^ ('script', (highestThus + 1) printString) asSymbol
