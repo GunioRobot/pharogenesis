83bd4e1246b4ac5c3a54e55cd536cb781ce5a536
sw <unknown>
1999-01-12 16:05:00 +0000

    no message

--- StandardScriptingSystem/instance/utilities/typeForSystemSlotNamed-3A-.cs
+++ StandardScriptingSystem/instance/utilities/typeForSystemSlotNamed-3A-.cs
@@ -1,2 +1,7 @@
 typeForSystemSlotNamed: aName
-	^ SystemSlotDictionary at: aName
+	^ SystemSlotDictionary at: aName ifAbsent: [nil]
+
+"
+ScriptingSystem typeForSystemSlotNamed: #colorSees
+ScriptingSystem typeForSystemSlotnamed: #frog
+"
