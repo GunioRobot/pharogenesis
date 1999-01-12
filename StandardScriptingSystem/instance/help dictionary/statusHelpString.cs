09d9c189270b35abce5ccabf36f2e832bd956e14
sw <unknown>
1999-01-12 16:05:00 +0000

    no message

---
+++ StandardScriptingSystem/instance/help dictionary/statusHelpString.cs
@@ -0,0 +1,14 @@
+statusHelpString
+	^	'normal -- run when called
+paused -- ready to run all the time
+ticking -- run all the time
+mouseDown -- run when mouse goes down on me
+mouseStillDown -- while mouse still down
+mouseUp -- when mouse comes back up
+mouseEnter -- when mouse enters my bounds, button up
+mouseLeave -- when mouse exits my bounds, button up
+mouseEnterDragging -- when mouse enters my bounds, button down
+mouseLeaveDragging -- when mouse exits my bounds, button down
+opening -- when I am being opened
+closing -- when I am being closed'
+"'keyStroke -- run when user hits a key' "
