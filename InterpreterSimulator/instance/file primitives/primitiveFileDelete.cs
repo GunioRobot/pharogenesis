49b81d851600a16b64d480b22b90c9c8e3a15668
di <unknown>
1999-01-12 15:38:00 +0000

    no message

--- InterpreterSimulator/instance/file primitives/primitiveFileDelete.cs
+++ InterpreterSimulator/instance/file primitives/primitiveFileDelete.cs
@@ -1,10 +1,8 @@
-primitiveFileDelete
+primitiveFileDelete 
 
 	| namePointer |
 	namePointer _ self stackTop.
 	self success: (self isBytes: namePointer).
 	self success: (StandardFileStream isAFileNamed: (self stringOf: namePointer)).
-	successFlag ifTrue: [
-		FileDirectory removeKey: (self stringOf: namePointer) ifAbsent: []].
-	successFlag ifTrue: [
-		self pop: 1].  "pop fileName; leave rcvr on stack"
+	successFlag ifTrue: [FileDirectory deleteFilePath: (self stringOf: namePointer)].
+	successFlag ifTrue: [self pop: 1].  "pop fileName; leave rcvr on stack"
