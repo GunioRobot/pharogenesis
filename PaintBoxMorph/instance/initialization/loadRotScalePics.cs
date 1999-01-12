0a8ce0f22f289c8de86df7205b67b8c1edd0784f
jm <unknown>
1999-01-12 12:01:00 +0000

    no message

--- PaintBoxMorph/instance/initialization/loadRotScalePics.cs
+++ PaintBoxMorph/instance/initialization/loadRotScalePics.cs
@@ -1,8 +1,5 @@
 loadRotScalePics
-	"Load up class vars with .bmp files for the images of the Rotation control button and the Scale control button.  File names do not change."
-	"	self loadRotScalePics		"
+	"Load up class vars with .bmp files for the images of the Rotation control button and the Scale control button."
 
-	rotationTabForm _ Form fromBMPFileNamed: 'Rotaball.bmp' depth: 16.
-	scaleTabForm _ Form fromBMPFileNamed: 'Scalball.bmp' depth: 16.
-	
-	
+	rotationTabForm _ (Form fromBMPFileNamed: 'Rotaball.bmp') asFormOfDepth: 16.
+	scaleTabForm _ (Form fromBMPFileNamed: 'Scalball.bmp') asFormOfDepth: 16.
