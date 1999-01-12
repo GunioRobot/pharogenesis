61cd9ec473d200468dcd22fdee2d7a7b13d72a25
sw <unknown>
1999-01-12 16:01:00 +0000

    no message

--- Player/instance/slots-2D-user/renameSlot-3A-.cs
+++ Player/instance/slots-2D-user/renameSlot-3A-.cs
@@ -1,2 +1,13 @@
-renameSlot: aSlotName
-	self notYetImplemented
+renameSlot: oldSlotName
+	| reply newSlotName |
+	reply _   FillInTheBlank request: 'New name for "', oldSlotName, '":' initialAnswer: oldSlotName.
+ 	reply size == 0 ifTrue: [^ self].
+	reply first isUppercase ifTrue: [^ self inform: 'Must start with lower case letter.'].
+	(Scanner isLiteralSymbol: reply) ifFalse: [^ self inform: 'Bad script name, please try again'].
+	
+	((newSlotName _ reply asSymbol) == oldSlotName) ifTrue: [^ self].
+	(self class slotInfo includesKey: newSlotName) ifTrue: [^ self inform: 'Sorry, that name is already in use'].
+	(ScriptingSystem isAcceptablePlayerSlotName: newSlotName)
+		ifFalse:
+			[^ self inform: 'Sorry, that name is reserved.'].
+	self renameSlot: oldSlotName newSlotName: newSlotName
