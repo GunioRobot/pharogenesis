96227990df4506f640db2be32c0a8be8fabd4bd0
tk <unknown>
1999-01-13 11:22:00 +0000

    no message

---
+++ TranslucentColor/instance/object fileIn/convertrcc0-3A-rcca0-3A-.cs
@@ -0,0 +1,7 @@
+convertrcc0: varDict rcca0: smartRefStrm
+	"These variables are automatically stored into the new instance ().
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"Be sure to to fill in ('alpha' ) and deal with the information in ()"
+
+	^ Color transparent
