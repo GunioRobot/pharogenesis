bb1a94cd3351cba9e135ce652f4c8a4bd25a4a23
di <unknown>
1999-01-13 14:11:00 +0000

    no message

--- TextMorph/instance/geometry/containsPoint-3A-.cs
+++ TextMorph/instance/geometry/containsPoint-3A-.cs
@@ -1,5 +1,6 @@
 containsPoint: aPoint
+	(super containsPoint: aPoint) ifFalse: [^false].
 	self startingIndex > text size ifTrue:
 		["make null text frame visible"
-		^ true].
+		^ super containsPoint: aPoint].
 	^ self paragraph containsPoint: aPoint
