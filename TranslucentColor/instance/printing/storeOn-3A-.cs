52b58b5e0513fc5cc7fea4223ff3d488125a8889
di <unknown>
1999-01-14 14:31:00 +0000

    no message

--- TranslucentColor/instance/printing/storeOn-3A-.cs
+++ TranslucentColor/instance/printing/storeOn-3A-.cs
@@ -1,8 +1,9 @@
 storeOn: aStream
 
+	self isTransparent ifTrue: [^ aStream nextPutAll: '(Color transparent)'].
 	super storeOn: aStream.
 	aStream
 		skip: -1;	  "get rid of trailing )"
 		nextPutAll: ' alpha: ';
-		nextPutAll: alpha printString;
+		nextPutAll: self alpha printString;
 		nextPutAll: ')'.
