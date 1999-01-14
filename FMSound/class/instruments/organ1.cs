6434cb7a87364d5f879ab252e1ea5a82a0d00ba5
jm <unknown>
1999-01-14 12:56:00 +0000

    no message

--- FMSound/class/instruments/organ1.cs
+++ FMSound/class/instruments/organ1.cs
@@ -2,15 +2,9 @@ organ1
 	"FMSound organ1 play"
 	"(FMSound majorScaleOn: FMSound organ1) play"
 
-	| snd p env |
-	snd _ FMSound pitch: 220 dur: 1 loudness: 0.8.
-	snd modulation: 0 multiplier: 0.
-
+	| snd p |
+	snd _ FMSound new.
 	p _ OrderedCollection new.
-	p add: 0@0; add: 60@1.0; add: 125@0.6; add: 200@1.0; add: 250@0.0.
-	env _ VolumeEnvelope points: p loopStart: 2 loopEnd: 4.
-	env target: snd; scale: 0.2.
-	snd addEnvelope: env.
-
-	snd duration: 1.0.
-	^ snd
+	p add: 0@0; add: 60@1.0; add: 110@0.8; add: 200@1.0; add: 250@0.0.
+	snd addEnvelope: (VolumeEnvelope points: p loopStart: 2 loopEnd: 4).
+	^ snd setPitch: 440.0 dur: 1.0 loudness: 0.5
