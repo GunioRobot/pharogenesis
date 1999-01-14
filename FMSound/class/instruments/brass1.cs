83fb3489167a976e1ceeaff077d0cdf93755582f
jm <unknown>
1999-01-14 12:00:00 +0000

    no message

--- FMSound/class/instruments/brass1.cs
+++ FMSound/class/instruments/brass1.cs
@@ -3,20 +3,15 @@ brass1
 	"(FMSound lowMajorScaleOn: FMSound brass1) play"
 
 	| snd p env |
-	snd _ FMSound pitch: 220 dur: 1 loudness: 0.8.
-	snd modulation: 0 multiplier: 1.
+	snd _ FMSound new modulation: 0 ratio: 1.
+	p _ OrderedCollection new.
+	p add: 0@0.0; add: 30@0.8; add: 90@1.0; add: 120@0.9; add: 220@0.7; add: 320@0.9; add: 360@0.0.
+	snd addEnvelope: (VolumeEnvelope points: p loopStart: 4 loopEnd: 6).
 
 	p _ OrderedCollection new.
-	p add: 0@0.1; add: 70@1.0; add: 120@0.8; add: 220@0.7; add: 320@0.8; add: 380@0.0.
+	p add: 0@0.5; add: 60@1.0; add: 120@0.8; add: 220@0.65; add: 320@0.8; add: 360@0.0.
 	env _ Envelope points: p loopStart: 3 loopEnd: 5.
 	env target: snd; updateSelector: #modulation:; scale: 5.0.
 	snd addEnvelope: env.
 
-	p _ OrderedCollection new.
-	p add: 0@0.0; add: 90@1.0; add: 120@0.9; add: 220@0.7; add: 320@0.9; add: 380@0.0.
-	env _ VolumeEnvelope points: p loopStart: 3 loopEnd: 5.
-	env target: snd; scale: 0.2.
-	snd addEnvelope: env.
-
-	snd duration: 1.0.
-	^ snd
+	^ snd setPitch: 220.0 dur: 1.0 loudness: 0.5
