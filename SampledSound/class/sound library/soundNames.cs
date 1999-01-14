786da92717efec6a68d6b26219954091dc0f3b27
jm <unknown>
1999-01-14 11:08:00 +0000

    no message

--- SampledSound/class/sound library/soundNames.cs
+++ SampledSound/class/sound library/soundNames.cs
@@ -1,5 +1,9 @@
 soundNames
-	"Return a list of sound names for the sounds stored in the sound library."
-	"SampledSound soundNames do: [:n | SampledSound playSoundNamed: n]"
+	"Answer a list of sound names for the sounds stored in the sound library."
+	"| s |
+	 SampledSound soundNames asSortedCollection do: [:n |
+		n asParagraph display.
+		s _ SampledSound soundNamed: n.
+		s ifNotNil: [s playAndWaitUntilDone]]"
 
 	^ SoundLibrary keys asArray
