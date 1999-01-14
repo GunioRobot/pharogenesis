d89c58253bbc885d5c1259917fbc6e05eb1d4e74
jm <unknown>
1999-01-14 13:13:00 +0000

    no message

--- SoundPlayer/class/initialization/initialize.cs
+++ SoundPlayer/class/initialization/initialize.cs
@@ -1,9 +1,8 @@
 initialize
-	"SoundPlayer initialize"
-	"Details: BufferMSecs represents a tradeoff between latency and quality. If BufferMSecs is too low, the sound will not play smoothing, especially during other activities. If it is too high, there will be an overly long time lag between when a sound buffer is submitted to be played and when that sound is actually heard. It is typically in the range 50-200."
 	"SoundPlayer initialize; shutDown; startUp"
+	"Details: BufferMSecs represents a tradeoff between latency and quality. If BufferMSecs is too low, the sound will not play smoothly, especially during long-running primitives such as large BitBlts. If BufferMSecs is too high, there will be a long time lag between when a sound buffer is submitted to be played and when that sound is actually heard. BufferMSecs is typically in the range 50-200."
 
 	SamplingRate _ 22050.
-	BufferMSecs _ 200.
+	BufferMSecs _ 120.
 	Stereo _ true.
 	UseReverb ifNil: [UseReverb _ true].
