0c07947c13f3413fb4d9b2998e1115a1adf7907d
jm <unknown>
1999-01-14 12:00:00 +0000

    no message

--- FMSound/class/instruments/bass1.cs
+++ FMSound/class/instruments/bass1.cs
@@ -3,8 +3,6 @@ bass1
 	"(FMSound lowMajorScaleOn: FMSound bass1) play"
 
 	| snd |
-	snd _ FMSound pitch: 220 dur: 1 loudness: 0.8.
-	snd modulation: 0 multiplier: 0.
-	snd addEnvelope:
-		(VolumeEnvelope exponentialDecay: 0.95).
+	snd _ FMSound new modulation: 0 ratio: 0.
+	snd addEnvelope: (VolumeEnvelope exponentialDecay: 0.95).
 	^ snd setPitch: 220 dur: 1.0 loudness: 0.3
