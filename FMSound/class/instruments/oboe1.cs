b14204661d4c307b77f9dfd5089349e06daa8edd
jm <unknown>
1999-01-14 12:00:00 +0000

    no message

--- FMSound/class/instruments/oboe1.cs
+++ FMSound/class/instruments/oboe1.cs
@@ -2,15 +2,9 @@ oboe1
 	"FMSound oboe1 play"
 	"(FMSound majorScaleOn: FMSound oboe1) play"
 
-	| snd p env |
-	snd _ FMSound pitch: 220 dur: 1 loudness: 0.8.
-	snd modulation: 1 multiplier: 1.
-
+	| snd p |
+	snd _ FMSound new modulation: 1 ratio: 1.
 	p _ OrderedCollection new.
-	p add: 0@0; add: 10@1.0; add: 100@1.0; add: 120@0.0.
-	env _ VolumeEnvelope points: p loopStart: 2 loopEnd: 3.
-	env target: snd; scale: 0.2.
-	snd addEnvelope: env.
-
-	snd duration: 1.0.
-	^ snd
+	p add: 0@0.0; add: 10@1.0; add: 100@1.0; add: 120@0.0.
+	snd addEnvelope: (VolumeEnvelope points: p loopStart: 2 loopEnd: 3).
+	^ snd setPitch: 440.0 dur: 1.0 loudness: 0.5
