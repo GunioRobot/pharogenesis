182e58fd825c312efdb9be79970e66b9be779d91
jm <unknown>
1999-01-14 11:04:00 +0000

    no message

--- SampledSound/class/sound library/playSoundNamed-3A-.cs
+++ SampledSound/class/sound library/playSoundNamed-3A-.cs
@@ -1,4 +1,8 @@
 playSoundNamed: aString
+	"Play the sound with given name. Do nothing if there is no sound of that name in the library."
 	"SampledSound playSoundNamed: 'croak'"
-	| aSound |
-	(aSound _ self soundNamed: aString) ifNotNil: [aSound play]
+
+	| snd |
+	snd _ self soundNamed: aString.
+	snd ifNotNil: [snd play].
+	^ snd
