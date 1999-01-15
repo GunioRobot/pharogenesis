35651f4dfc721120bb66e144375ca4f2012399ed
tk <unknown>
1999-01-15 08:02:00 +0000

    no message

---
+++ BookMorph/instance/initialization/fromURL-3A-.cs
@@ -0,0 +1,10 @@
+fromURL: url
+	"Make a book from an index and a bunch of pages on a server.  NOT showing any page!"
+
+	| strm |
+	Cursor wait showWhile: [
+		strm _ (ServerFile new fullPath: url) asStream].
+	strm class == String ifTrue: [self inform: 'Sorry, ',strm. ^ nil].
+	self setProperty: #url toValue: url.
+	self fromRemoteStream: strm.
+	^ self
