ae14e5130032b773930ea691276114d32190cf6c
sw <unknown>
1999-01-26 09:18:00 +0000

    no message

---
+++ StringHolder/instance/message list menu/systemCatListKey-3A-from-3A-.cs
@@ -0,0 +1,4 @@
+systemCatListKey: aChar from: view
+	"Respond to a Command key.  I am a model with a code pane, and I also have a listView that has a list of methods.  The view knows how to get the list and selection."
+	aChar == $f ifTrue: [^ self findClass].
+	^ self classListKey: aChar from: view
