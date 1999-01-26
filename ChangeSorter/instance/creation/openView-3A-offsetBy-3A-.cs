c29fff57e3b843937f6c9fa79588655782908949
sw <unknown>
1999-01-26 09:15:00 +0000

    no message

--- ChangeSorter/instance/creation/openView-3A-offsetBy-3A-.cs
+++ ChangeSorter/instance/creation/openView-3A-offsetBy-3A-.cs
@@ -1,27 +1,24 @@
 openView: topView offsetBy: offset
 	"Add a set of change sorter views to the given top view offset by the given amount. To create a single change sorter, call this once with an offset of 0@0. To create a dual change sorter, call it twice with offsets of 0@0 and 360@0."
-	| classView messageView codeView buttonView |
+	| classView messageView codeView cngSetListView |
 	contents _ ''.
 	self addDependent: topView.		"so it will get changed: #relabel"
-	buttonView _ PluggableButtonView
-		on: self
-		getState: #mainButtonState
-		action: #changeSetMenuStart
-		label: #mainButtonName
-		menu: #changeSetMenu:.
-	buttonView
-		label: myChangeSet name;
-		triggerOnMouseDown: true; borderWidth: 1; 
-		window: ((0 @ 0 extent: 360 @ 20) translateBy: offset).
-	topView addSubView: buttonView.
+	cngSetListView _ PluggableListViewByItem on: self
+		list: #changeSetList
+		selected: #currentCngSet
+		changeSelected: #showChangeSetNamed:
+		menu: #changeSetMenu:shifted:.
+	cngSetListView window: ((0 @ 0 extent: 180 @ 100) translateBy: offset).
+	topView addSubView: cngSetListView.
 
 	classView _ PluggableListViewByItem on: self
 		list: #classList
 		selected: #currentClassName
 		changeSelected: #currentClassName:
-		menu: #classMenu:.
-	classView window: (0 @ 0 extent: 180 @ 160).
-	topView addSubView: classView below: buttonView.
+		menu: #classMenu:
+		keystroke: #classListKey:from:.
+	classView window: (180 @ 0 extent: 180 @ 100).
+	topView addSubView: classView toRightOf: cngSetListView.
 
 	messageView _ PluggableListViewByItem on: self
 		list: #messageList
@@ -29,11 +26,12 @@ openView: topView offsetBy: offset
 		changeSelected: #currentSelector:
 		menu: #messageMenu:shifted:
 		keystroke: #messageListKey:from:.
-	messageView window: (0 @ 0 extent: 180 @ 160).
-	topView addSubView: messageView toRightOf: classView.
+	messageView menuTitleSelector: #messageListSelectorTitle.
+	messageView window: (0 @ 100 extent: 360 @ 100).
+	topView addSubView: messageView below: cngSetListView.
 
 	codeView _ PluggableTextView on: self 
 			text: #contents accept: #contents:notifying:
 			readSelection: #contentsSelection menu: #codePaneMenu:shifted:.
 	codeView window: (0 @ 0 extent: 360 @ 180).
-	topView addSubView: codeView below: classView.
+	topView addSubView: codeView below: messageView.
