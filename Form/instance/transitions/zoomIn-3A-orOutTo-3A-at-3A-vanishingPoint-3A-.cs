edc00b87fa993c5b1eb3b326b678210a98616f2b
di <unknown>
1999-01-28 09:20:00 +0000

    no message

--- Form/instance/transitions/zoomIn-3A-orOutTo-3A-at-3A-vanishingPoint-3A-.cs
+++ Form/instance/transitions/zoomIn-3A-orOutTo-3A-at-3A-vanishingPoint-3A-.cs
@@ -1,13 +1,21 @@
-zoomIn: goingIn orOutTo: otherImage at: topLeft vanishingPoint: vp
+zoomIn: goingIn orOutTo: otherImage at: topLeft vanishingPoint: vp 
 	"Display zoomInTo: (Form fromDisplay: (40@40 extent: 300@300)) reverse at: 40@40.
 	Display zoomOutTo: (Form fromDisplay: (40@40 extent: 300@300)) reverse at: 40@40."
-	| nSteps j bigR lilR |
+	| nSteps j bigR lilR minTime startTime lead |
 	nSteps _ 16.
+	minTime _ 500.  "milliseconds"
+	startTime _ Time millisecondClockValue.
 	^ self wipeImage: otherImage at: topLeft clippingBox: nil rectForIndex:
 		[:i | "i runs from 1 to nsteps"
-		i > nSteps ifTrue: [nil "indicates all done"]
+		i > nSteps
+			ifTrue: [nil "indicates all done"]
 			ifFalse:
-			[j _ goingIn ifTrue: [i] ifFalse: [nSteps+1-i].
+			["If we are going too fast, delay for a bit"
+			lead _ startTime + (i-1*minTime//nSteps) - Time millisecondClockValue.
+			lead > 10 ifTrue: [(Delay forMilliseconds: lead) wait].
+
+			"Return an array with the difference rectangles for this step."
+			j _ goingIn ifTrue: [i] ifFalse: [nSteps+1-i].
 			bigR _ vp - (vp*(j)//nSteps) corner:
 				vp + (otherImage extent-vp*(j)//nSteps).
 			lilR _ vp - (vp*(j-1)//nSteps) corner:
