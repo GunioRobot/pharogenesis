3ffaa58d04901cd382e5fb5293b802450346fdd9
sw <unknown>
1999-01-29 16:34:00 +0000

    no message

--- ScriptEditorMorph/instance/other/hasScriptReferencing-3A-ofPlayer-3A-.cs
+++ ScriptEditorMorph/instance/other/hasScriptReferencing-3A-ofPlayer-3A-.cs
@@ -1,4 +1,5 @@
 hasScriptReferencing: aSlotName ofPlayer: aPlayer
 	"Answer whether the receiver has any tiles in it which reference the given slot of the given player.  Place-holder for now, needs to be implemented"
 
+	self flag: #deferred.  "a feature whose time has now just about come?!"
 	^ false
