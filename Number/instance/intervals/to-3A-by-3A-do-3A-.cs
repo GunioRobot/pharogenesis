26c2bf6c9415cd9b8b32eae444f3435faf35f2f8
tao <unknown>
1999-01-30 08:58:00 +0000

    no message

--- Number/instance/intervals/to-3A-by-3A-do-3A-.cs
+++ Number/instance/intervals/to-3A-by-3A-do-3A-.cs
@@ -1,8 +1,10 @@
 to: stop by: step do: aBlock 
 	"Normally compiled in-line, and therefore not overridable.
-	Evaluate aBlock for each element of the interval (self to: stop by: step)."
+	Evaluate aBlock for each element of the interval (self to: stop by: 
+step)."
 	| nextValue |
 	nextValue _ self.
+	step = 0 ifTrue: [self error: 'step must be non-zero'].
 	step < 0
 		ifTrue: [[stop <= nextValue]
 				whileTrue: 
