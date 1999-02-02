2f19dc92a2eb2e26f1411de7622ffa46b9638f58
TPR <unknown>
1999-02-02 15:56:00 +0000

    no message

--- SystemTracer/instance/private/sizeInWordsOf-3A-.cs
+++ SystemTracer/instance/private/sizeInWordsOf-3A-.cs
@@ -5,5 +5,9 @@ sizeInWordsOf: anObject
 	class _ anObject class.
 	class isBytes ifTrue: [^ anObject basicSize+3 // 4].
 	class isBits ifTrue: [^ anObject basicSize].	"in two byte chunks"
-	class isVariable ifTrue: [^ class instSize + anObject basicSize].
+	class isVariable ifTrue: [
+		"terrible hack to get around contexts beleiving they are only sized up to the SP"
+		(class = BlockContext or: [class = MethodContext])
+			ifTrue:[^class instSize + CompiledMethod fullFrameSize].
+		 ^ class instSize + anObject basicSize].
 	^ class instSize
