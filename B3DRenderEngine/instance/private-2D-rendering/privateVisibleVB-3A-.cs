15466e67f33cee3ddae2135f4bf5779bddcc1236
ar <unknown>
1999-02-02 19:31:00 +0000

    no message

---
+++ B3DRenderEngine/instance/private-2D-rendering/privateVisibleVB-3A-.cs
@@ -0,0 +1,7 @@
+privateVisibleVB: vb
+	"Return the visibility of the objects in the vertex buffer.
+	Return:
+		true - if completely inside view frustum
+		false - if completely outside view frustum
+		nil - if partly inside/outside view frustum
+	"
