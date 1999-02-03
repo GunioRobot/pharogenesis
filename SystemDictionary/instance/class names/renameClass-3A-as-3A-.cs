3c52c8f9b31c7983a49c888541c280d80999aacf
di <unknown>
1999-02-03 22:33:00 +0000

    no message

--- SystemDictionary/instance/class names/renameClass-3A-as-3A-.cs
+++ SystemDictionary/instance/class names/renameClass-3A-as-3A-.cs
@@ -1,6 +1,6 @@
 renameClass: aClass as: newName 
 	"Rename the class, aClass, to have the title newName."
-	| oldref |
+	| oldref i |
 	SystemOrganization classify: newName under: aClass category.
 	SystemOrganization removeElement: aClass name.
 	SystemChanges renameClass: aClass as: newName.
@@ -8,4 +8,7 @@ renameClass: aClass as: newName
 	self removeKey: aClass name.
 	oldref key: newName.
 	self add: oldref.  "Old association preserves old refs"
+	(Array with: StartUpList with: ShutDownList) do:
+		[:list |  i _ list indexOf: aClass name ifAbsent: [0].
+		i > 0 ifTrue: [list at: i put: newName]].
 	self flushClassNameCache
