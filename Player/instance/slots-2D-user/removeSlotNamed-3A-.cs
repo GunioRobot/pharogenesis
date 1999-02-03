22cf4a55fa7fb889669214aa8897086a1590fd96
sw <unknown>
1999-02-03 09:52:00 +0000

    no message

--- Player/instance/slots-2D-user/removeSlotNamed-3A-.cs
+++ Player/instance/slots-2D-user/removeSlotNamed-3A-.cs
@@ -2,5 +2,13 @@ removeSlotNamed: aSlotName
 	(self okayToRemoveSlotNamed: aSlotName) ifFalse:
 		[^ self inform: 'Sorry, ', aSlotName, ' is in
 use in a script.'].
+	self inform: 
+'Caution!  Any scripts that may reference
+this instance variable may now be broken.
+You may need to fix them up manually.  
+In some future release, we''ll automatically
+fix those up, hopefully.'.
 
-	self notYetImplemented
+	self class removeInstVarName: aSlotName asString.
+
+	self updateAllViewers
