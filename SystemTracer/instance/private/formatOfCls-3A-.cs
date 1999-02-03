784b52f58981fe7ea89dfdf524679234062e0590
TPR <unknown>
1999-02-03 21:40:00 +0000

    no message

--- SystemTracer/instance/private/formatOfCls-3A-.cs
+++ SystemTracer/instance/private/formatOfCls-3A-.cs
@@ -1,4 +1,7 @@
 formatOfCls: class
 	"Return the full word value that encodes instSize, bits, bytes, and variable."
-	"See the comment in Behavior format:variable:words:pointers:"
-	^ class format    "just use what's there"
+	"See the comment in Behavior format:variable:words:pointers:
+	NB: we must clear the compact class related field and replace it with the value from the cloner's list of compact classes"
+	| ccField |
+	ccField _ compactClasses indexOf: class.	"0 means need full word for class "
+	^ (class format bitClear: (2r11111 bitShift:11)) bitOr: (ccField bitShift:11)
