3b9d4f6a7a1c00c77d0eb693bed240a373fecdd8
jm <unknown>
1999-02-04 11:33:00 +0000

    no message

---
+++ GSMCodec/instance/primitives/primEncode-3A-frames-3A-from-3A-at-3A-into-3A-at-3A-.cs
@@ -0,0 +1,4 @@
+primEncode: state frames: frameCount from: srcSoundBuffer at: srcIndex into: dstByteArray at: dstIndex
+
+	<primitive: 'primitiveGSMEncode' module: 'SoundCodecPrims'>
+	self primitiveFailed.
