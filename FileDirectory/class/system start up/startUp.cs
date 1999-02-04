233db4ab56a78a33d4b3ff93b93e3a84234d075f
di <unknown>
1999-02-04 08:50:00 +0000

    no message

--- FileDirectory/class/system start up/startUp.cs
+++ FileDirectory/class/system start up/startUp.cs
@@ -2,4 +2,4 @@ startUp
 	"Establish the platform-specific FileDirectory subclass. Do any platform-specific startup."
 
 	self setDefaultDirectoryFrom: Smalltalk imageName.
-	DirectoryClass platformSpecificStartup.
+	Smalltalk openSourceFiles.
