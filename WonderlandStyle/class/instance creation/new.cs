62eac2687d8f383e2c5b600e260f508682f59511
jsp <unknown>
1999-02-04 14:58:00 +0000

    no message

---
+++ WonderlandStyle/class/instance creation/new.cs
@@ -0,0 +1,4 @@
+new
+	"Users shouldn't create any instances of this class, so this method throws an error if they try."
+
+	self error: 'This class exists to generate style parameters, so it should not be instatiated.'
