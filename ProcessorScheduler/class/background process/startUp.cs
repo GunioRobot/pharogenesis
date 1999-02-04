67fe604bfdc98e0bb867ce14f800dfabb1702b3a
di <unknown>
1999-02-04 08:45:00 +0000

    no message

--- ProcessorScheduler/class/background process/startUp.cs
+++ ProcessorScheduler/class/background process/startUp.cs
@@ -2,6 +2,7 @@ startUp
 	"Install a background process of the lowest possible priority that is always runnable."
 	"Details: The virtual machine requires that there is aways some runnable process that can be scheduled; this background process ensures that this is the case."
 
+	Smalltalk installLowSpaceWatcher.
 	BackgroundProcess == nil ifFalse: [BackgroundProcess terminate].
 	BackgroundProcess _ [self idleProcess] newProcess.
 	BackgroundProcess priority: SystemRockBottomPriority.
