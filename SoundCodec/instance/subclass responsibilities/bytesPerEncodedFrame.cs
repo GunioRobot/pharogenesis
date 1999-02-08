00f44a17f409fbf8e60b0499d8c438626f73f95a
di <unknown>
1999-02-08 14:23:00 +0000

    no message

---
+++ SoundCodec/instance/subclass responsibilities/bytesPerEncodedFrame.cs
@@ -0,0 +1,4 @@
+bytesPerEncodedFrame
+	"Answer the number of bytes required to hold one frame of compressed sound data. Answer zero if this codec produces encoded frames of variable size."
+
+	self subclassResponsibility.
