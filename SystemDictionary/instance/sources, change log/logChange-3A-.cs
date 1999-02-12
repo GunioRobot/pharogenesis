00223cd092213fb5d3be28ad89d6a0e4f681fd39
sw <unknown>
1999-02-12 17:18:00 +0000

    no message

--- SystemDictionary/instance/sources, change log/logChange-3A-.cs
+++ SystemDictionary/instance/sources, change log/logChange-3A-.cs
@@ -1,15 +1,17 @@
-logChange: aStringOrText
+logChange: aStringOrText 
 	"Write the argument, aString, onto the changes file."
-	| aString |
-	(SourceFiles isNil or: [(SourceFiles at: 2) == nil]) ifTrue: [^self].
+	| aString changesFile |
+	(SourceFiles isNil or: [(SourceFiles at: 2) == nil]) ifTrue: [^ self].
+	self assureStartupStampLogged.
+
 	aStringOrText isText
 		ifTrue: [aString _ aStringOrText string]
 		ifFalse: [aString _ aStringOrText].
 	(aString isMemberOf: String)
-		ifFalse: [self error: 'cant log this change'].
+		ifFalse: [self error: 'can''t log this change'].
 	(aString findFirst: [:char | char isSeparator not]) = 0
-		ifTrue: [^self].  "null doits confuse replay"
-	(SourceFiles at: 2) setToEnd;
-			cr; cr; nextChunkPut: aString.
+		ifTrue: [^ self].  "null doits confuse replay"
+	(changesFile _ SourceFiles at: 2) setToEnd; cr; cr.
+	changesFile nextChunkPut: aString.
 		"If want style changes in DoIt, use nextChunkPutWithStyle:, and allow Texts to get here"
 	self forceChangesToDisk.
