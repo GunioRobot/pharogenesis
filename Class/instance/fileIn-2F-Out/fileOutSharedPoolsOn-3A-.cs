5f964eb4c2c291be8cf6e6a87ead614d9064cdc1
ar <unknown>
1999-02-13 21:17:00 +0000

    no message

--- Class/instance/fileIn-2F-Out/fileOutSharedPoolsOn-3A-.cs
+++ Class/instance/fileIn-2F-Out/fileOutSharedPoolsOn-3A-.cs
@@ -2,6 +2,6 @@ fileOutSharedPoolsOn: aFileStream
 	"file out the shared pools of this class after prompting the user about each pool"
 	| poolsToFileOut |
 	poolsToFileOut _ self sharedPools select: 
-		[:aPool | (self shouldFileOutPool: (Smalltalk keyAtValue: aPool))].
+		[:aPool | (self shouldFileOutPool: (Smalltalk keyAtIdentityValue: aPool))].
 	poolsToFileOut do: [:aPool | self fileOutPool: aPool onFileStream: aFileStream].
 	
