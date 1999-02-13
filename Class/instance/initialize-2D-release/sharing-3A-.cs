0d844f9b6c8a04d56db5418658a73da43ebd7bca
ar <unknown>
1999-02-13 21:04:00 +0000

    no message

--- Class/instance/initialize-2D-release/sharing-3A-.cs
+++ Class/instance/initialize-2D-release/sharing-3A-.cs
@@ -5,7 +5,11 @@ sharing: poolString
 	sharedPools _ OrderedCollection new.
 	(Scanner new scanFieldNames: poolString) do: 
 		[:poolName | 
-		sharedPools add: (Smalltalk at: poolName asSymbol)].
+		sharedPools add: (Smalltalk at: poolName asSymbol ifAbsent:[
+			(self confirm: 'The pool dictionary ', poolName,' does not exist.',
+						'\Do you want it automatically created?' withCRs)
+				ifTrue:[Smalltalk at: poolName asSymbol put: Dictionary new]
+				ifFalse:[^self error: poolName,' does not exist']])].
 	sharedPools isEmpty ifTrue: [sharedPools _ nil].
 	oldPools do: [:pool | found _ false.
 				self sharedPools do: [:p | p == pool ifTrue: [found _ true]].
