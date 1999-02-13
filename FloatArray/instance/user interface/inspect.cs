830771ff2630c6cc42beef2adf0c1cb34d311f8e
ar <unknown>
1999-02-13 21:33:00 +0000

    no message

---
+++ FloatArray/instance/user interface/inspect.cs
@@ -0,0 +1,4 @@
+inspect
+	"Open a OrderedCollectionInspector on the receiver.  Use basicInspect to get a normal (less useful) type of inspector."
+
+	OrderedCollectionInspector openOn: self withEvalPane: true
