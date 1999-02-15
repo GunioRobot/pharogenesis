1e60758a55d9eb5e9e38bd5732f0f6405b5674df
tk <unknown>
1999-02-15 14:49:00 +0000

    no message

---
+++ DocLibrary/instance/database of updates/updateMethodVersions.cs
@@ -0,0 +1,15 @@
+updateMethodVersions
+	"See if any new updates have occurred, and put their methods into the database."
+
+	| indexFile list result |
+	self haveNetwork ifFalse: [^ self].
+	indexFile _ 'latest.ix'.
+	list _ OrderedCollection new.
+	[result _ self absorbAfter: lastUpdate from: indexFile.
+	"boolean if succeeded, or we are up to date, or server not available"
+	result class == String] whileTrue: [
+		"result is the prev file name"
+		list addFirst: indexFile.
+		indexFile _ result].
+	list do: [:aFile | self absorbAfter: lastUpdate from: aFile].
+		"should always work this time"
