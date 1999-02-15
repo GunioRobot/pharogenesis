8494093f91df40baca76a47f74aeeb25844f8300
sw <unknown>
1999-02-15 20:36:00 +0000

    no message

---
+++ PasteUpMorph/instance/flaps/removeAccommodationForFlap-3A-.cs
@@ -0,0 +1,6 @@
+removeAccommodationForFlap: aFlapTab
+	"Shift submorphs over, if appropriate"
+	| offset |
+	aFlapTab slidesOtherObjects ifTrue:
+		[offset _ self offsetForAccommodating: aFlapTab referent extent onEdge: aFlapTab edgeToAdhereTo.
+		self shiftSubmorphsBy: offset negated]
