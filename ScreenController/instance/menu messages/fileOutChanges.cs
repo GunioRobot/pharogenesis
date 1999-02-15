c492694746457872ed558077796b243e4bea7b53
sw <unknown>
1999-02-15 12:25:00 +0000

    no message

--- ScreenController/instance/menu messages/fileOutChanges.cs
+++ ScreenController/instance/menu messages/fileOutChanges.cs
@@ -1,5 +1,2 @@
 fileOutChanges
-	"File out changes to a file whose name is a function of the current date and time."
-
-	Smalltalk changes fileOut.
-	Transcript show: 'Changes filed out ', Date dateAndTimeNow printString; cr.
+	Utilities fileOutChanges
