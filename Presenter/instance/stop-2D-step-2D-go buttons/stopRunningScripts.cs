b0fd24ff1ed02f407a44fbf190b3a9a8f36fff9a
sw <unknown>
1999-02-15 19:46:00 +0000

    no message

--- Presenter/instance/stop-2D-step-2D-go buttons/stopRunningScripts.cs
+++ Presenter/instance/stop-2D-step-2D-go buttons/stopRunningScripts.cs
@@ -2,4 +2,5 @@ stopRunningScripts
 	self stopButtonState: true.
 	self stepButtonState: false.
 	self goButtonState: false.
-	self stopButton world stopRunningAll
+	associatedMorph stopRunningAll.
+	associatedMorph borderColor: Preferences borderColorWhenStopped
