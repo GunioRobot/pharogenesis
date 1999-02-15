b32626f1b2ec1da280660af10ce7440a16569c22
sw <unknown>
1999-02-15 16:18:00 +0000

    no message

--- ScreenController/instance/menu messages/aboutThisSystem.cs
+++ ScreenController/instance/menu messages/aboutThisSystem.cs
@@ -1,13 +1,2 @@
 aboutThisSystem 
-	"Identify software version"
-
-	| aString eToySystem aNumber |
-	aString _ Smalltalk version.
-	(eToySystem _ Smalltalk at: #EToySystem ifAbsent: [nil]) ifNotNil:
-		[aString _ aString, '
-EToy System: ', eToySystem version, ' of ', eToySystem versionDate].
-	aNumber _ ChangeSorter highestNumberedChangeSet.
-	aNumber > 0 ifTrue:
-		[aString _ aString, '
-Highest-numbered Change Set: ', aNumber printString].
-	^ self inform: aString
+	Smalltalk aboutThisSystem
