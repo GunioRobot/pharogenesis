c2b34b5df98d481d18eb1607bb42c17032fb4e98
sw <unknown>
1999-02-15 19:38:00 +0000

    no message

--- Player/instance/misc/tileReferringToSelf.cs
+++ Player/instance/misc/tileReferringToSelf.cs
@@ -2,6 +2,6 @@ tileReferringToSelf
 	| aTile |
 	aTile _ TileMorph new
 		setObjectRef: nil "disused parm" actualObject: self;
-		typeColor: (TilePadMorph colorForType: #player).
-	self costume presenter coloredTilesEnabled ifFalse: [aTile useUniformTileColor].
+		typeColor: (ScriptingSystem colorForType: #player).
+	aTile enforceTileColorPolicy.
 	^ aTile
