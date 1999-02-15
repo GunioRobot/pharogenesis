4411d677a3d45b4ce9eab490b3ae30665ce629b1
sw <unknown>
1999-02-15 19:24:00 +0000

    no message

--- Player/instance/scripts-2D-standard/forward-3A-.cs
+++ Player/instance/scripts-2D-standard/forward-3A-.cs
@@ -1,36 +1,39 @@
-forward: dist
+forward: dist 
 
-	| rho radians delta didStray p fractionalP newP |
-	costume isInWorld ifFalse: [^ self].
-	costume isWorldOrHandMorph ifTrue: [^ self].
+	| rho radians delta didStray p fractionalP newP aCostume |
+	(aCostume _ self costume) isInWorld ifFalse: [^ self].
+	aCostume isWorldOrHandMorph ifTrue: [^ self].
 
-	rho _ (costume asNumber: dist) asFloat.
+	rho _ (aCostume asNumber: dist) asFloat.
 	radians _ (self getHeadingUnrounded asFloat - 90.0) degreesToRadians.
 	delta _ (radians cos @ radians sin) * rho.
 
-	((costume owner isHandMorph not) and:
-	 [costume presenter fenceEnabled]) ifTrue:
-		[(costume owner bounds containsRect: costume bounds) ifFalse:
+	((aCostume owner isHandMorph not) and:
+	 [Preferences fenceEnabled]) ifTrue:
+		[(aCostume owner bounds containsRect: aCostume bounds) ifFalse:
 			["If I stray out of the bounds of my owner, pull me back, but
 			 without changing my heading as bounce would. Do nothing if
 			 bounce has already corrected the direction."
 			didStray _ false.
-			((costume left < costume owner left and: [delta x < 0]) or:
-			 [costume right > costume owner right and: [delta x > 0]]) ifTrue: [
+			((aCostume left < aCostume owner left and: [delta x < 0]) or:
+			 [aCostume right > aCostume owner right and: [delta x > 0]]) ifTrue: [
 				delta _ delta x negated @ delta y.
 				didStray _ true].
-			((costume top < costume owner top and: [delta y < 0]) or:
-			 [costume bottom > costume owner bottom and: [delta y > 0]]) ifTrue: [
+			((aCostume top < aCostume owner top and: [delta y < 0]) or:
+			 [aCostume bottom > aCostume owner bottom and: [delta y > 0]]) ifTrue: [
 				delta _ delta x @ delta y negated.
 				didStray _ true].
-			didStray ifTrue: [costume makeFenceSound]]].
+			didStray ifTrue: [aCostume makeFenceSound]]].
 
 	"use and record the fractional position"
-	p _ costume referencePosition asFloatPoint.
-	fractionalP _ costume actorState fractionalPosition.
-	(fractionalP == nil or: [(fractionalP - p) abs >= (1@1)])
-		ifTrue: [newP _ p + delta]
-		ifFalse: [newP _ p + (fractionalP - p) + delta].
-
-	costume actorState fractionalPosition: newP.
-	costume referencePosition: newP rounded.
+	p _ aCostume referencePosition.
+	fractionalP _ aCostume actorState fractionalPosition.
+	(fractionalP == nil or: [fractionalP asIntegerPoint ~= p])
+		ifTrue: [newP _ p asFloatPoint + delta]
+		ifFalse: [newP _ fractionalP + delta].
+"
+Transcript cr; print: p; space; print: fractionalP;
+cr; cr; print: newP asIntegerPoint; space; print: newP; show: ''.
+"
+	aCostume actorState fractionalPosition: newP.
+	aCostume referencePosition: newP asIntegerPoint.
