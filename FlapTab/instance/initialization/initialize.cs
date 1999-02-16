dd6fba7b98f039a85d35cd884562207eed30e55e
sw <unknown>
1999-02-16 23:44:00 +0000

    no message

---
+++ FlapTab/instance/initialization/initialize.cs
@@ -0,0 +1,10 @@
+initialize
+	super initialize.
+
+	edgeToAdhereTo _ #left.
+	flapShowing _ false.
+	slidesOtherObjects _ false.
+	popOutOnDragOver _ false.
+	popOutOnMouseOver _ false.
+	inboard _ false.
+	dragged _ false
