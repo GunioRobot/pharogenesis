6a8bec3648a1ccda955a931c2bccb14573918696
ar <unknown>
1999-02-16 19:08:00 +0000

    no message

---
+++ B3DIndexedMesh/instance/private/computeBoundingBox.cs
@@ -0,0 +1,8 @@
+computeBoundingBox
+	| min max |
+	min _ max _ nil.
+	vertices do:[:vtx|
+		min ifNil:[min _ vtx] ifNotNil:[min _ min min: vtx].
+		max ifNil:[max _ vtx] ifNotNil:[max _ max max: vtx].
+	].
+	^Rectangle origin: min corner: max
