3b90083de217fcfa30129e28df36a899888957b9
ar <unknown>
1999-02-17 04:20:00 +0000

    no message

---
+++ B3DPrimitiveTransformer/instance/private-2D-transforming/privateTransformPrimitiveNormal-3A-byMatrix-3A-rescale-3A-.cs
@@ -0,0 +1,4 @@
+privateTransformPrimitiveNormal: primitiveVertex byMatrix: aMatrix rescale: scaleNeeded
+	"Use the primitive operation"
+	<primitive: 'b3dTransformPrimitiveNormal' module:'Squeak3D'>
+	^super privateTransformPrimitiveNormal: primitiveVertex byMatrix: aMatrix rescale: scaleNeeded
