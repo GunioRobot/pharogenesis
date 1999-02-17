37643df6fce8c0581a542159c1b69aea671ccaa3
ar <unknown>
1999-02-17 15:59:00 +0000

    no message

---
+++ B3DSTriangleMesh/instance/private/computeFunkyVertexNormals.cs
@@ -0,0 +1,4 @@
+computeFunkyVertexNormals
+	"Compute the vertex normals for the receiver.
+	Don't split the faces so we'll get some funky lighting effects."
+	vtxNormals _ super computeVertexNormals
