850de5bebb144930529937870aefd501c1869eed
di <unknown>
1999-02-17 10:54:00 +0000

    no message

--- SoundRecorder/instance/recording controls/clearRecordedSound.cs
+++ SoundRecorder/instance/recording controls/clearRecordedSound.cs
@@ -2,5 +2,5 @@ clearRecordedSound
 	"Clear the sound recorded thus far. Go into pause mode if currently recording."
 
 	paused _ true.
-	recordedBuffers _ OrderedCollection new: 1000.
+	recordedSound _ SequentialSound new.
 	self allocateBuffer.
