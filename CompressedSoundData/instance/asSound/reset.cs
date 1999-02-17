b9f10dbab5ae3985917661829ae58112f149f2a6
di <unknown>
1999-02-17 20:49:00 +0000

    no message

---
+++ CompressedSoundData/instance/asSound/reset.cs
@@ -0,0 +1,7 @@
+reset
+	"This message is the cue to start behaving like a real sound in order to be played.
+	We do this by caching a decompressed version of this sound.
+	See also samplesRemaining."
+
+	cachedSound == nil ifTrue: [cachedSound _ self asSound].
+	cachedSound reset
