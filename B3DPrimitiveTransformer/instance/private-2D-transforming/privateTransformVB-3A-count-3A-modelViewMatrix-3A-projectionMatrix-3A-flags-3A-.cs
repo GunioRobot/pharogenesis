24c4c54c8f4dba44fea2b51ec2da702027128648
ar <unknown>
1999-02-17 04:21:00 +0000

    no message

---
+++ B3DPrimitiveTransformer/instance/private-2D-transforming/privateTransformVB-3A-count-3A-modelViewMatrix-3A-projectionMatrix-3A-flags-3A-.cs
@@ -0,0 +1,4 @@
+privateTransformVB: vertexArray count: vertexCount modelViewMatrix: modelViewMatrix projectionMatrix: projectionMatrix flags: flags
+	"Use the primitive operation"
+	<primitive: 'b3dTransformVertexBuffer' module:'Squeak3D'>
+	^super privateTransformVB: vertexArray count: vertexCount modelViewMatrix: modelViewMatrix projectionMatrix: projectionMatrix flags: flags
