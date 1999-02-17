cfb4b77b459e8dc11820548169b2aa36470b1ca7
di <unknown>
1999-02-17 08:49:00 +0000

    no message

---
+++ CompressedSoundData/instance/asSound/mixSampleCount-3A-into-3A-startingAt-3A-leftVol-3A-rightVol-3A-.cs
@@ -0,0 +1,3 @@
+mixSampleCount: n into: aSoundBuffer startingAt: startIndex leftVol: leftVol rightVol: rightVol
+
+	cachedSound mixSampleCount: n into: aSoundBuffer startingAt: startIndex leftVol: leftVol rightVol: rightVol
