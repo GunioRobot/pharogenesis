b7fbef79a1587ef6969a337c063c291d9ab085c4
ar <unknown>
1999-02-17 04:19:00 +0000

    no message

---
+++ B3DPrimitiveTransformer/instance/private-2D-transforming/privateTransformMatrix-3A-with-3A-into-3A-.cs
@@ -0,0 +1,4 @@
+privateTransformMatrix: m1 with: m2 into: m3
+	"Use the primitive operation"
+	<primitive: 'b3dTransformMatrixWithInto' module:'Squeak3D'>
+	^super privateTransformMatrix: m1 with: m2 into: m3
