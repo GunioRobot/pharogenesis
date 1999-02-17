973d87b40a2d38949568875acbb3fb6f57d0bdd2
tk <unknown>
1999-02-17 12:38:00 +0000

    no message

---
+++ URLMorph/instance/accessing/book-3A-.cs
@@ -0,0 +1,4 @@
+book: aUrl
+	"A notation about what book this page is in.  true means page is in same book as url strm says.  Set to the url of the Book if the book has a different stem url.  nil or false if not for a book page at all."
+
+	book _ aUrl
