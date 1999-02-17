6b94f872695e9dcf02479ce92f0e7af942676505
di <unknown>
1999-02-17 11:13:00 +0000

    no message

--- SoundRecorder/instance/private/allocateBuffer.cs
+++ SoundRecorder/instance/private/allocateBuffer.cs
@@ -1,5 +1,11 @@
 allocateBuffer
 	"Allocate a new buffer and reset nextIndex."
 
-	currentBuffer _ SoundBuffer newMonoSampleCount: 20000.
+	| bufferTime |
+	bufferTime _ stereo  "Buffer time = 1/2 second"
+		ifTrue: [self samplingRate asInteger]
+		ifFalse: [self samplingRate asInteger // 2].
+	currentBuffer _ SoundBuffer newMonoSampleCount:
+		"Multiple of samplesPerFrame that is approx. bufferTime long"
+		(bufferTime truncateTo: self samplesPerFrame).
 	nextIndex _ 1.
