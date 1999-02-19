fa5338ad754fc3f9b85eb3b1ec0dcd5801c1a96b
dew <unknown>
1999-02-19 18:49:00 +0000

    no message

--- ScrollPane/instance/geometry/scrollBy-3A-.cs
+++ ScrollPane/instance/geometry/scrollBy-3A-.cs
@@ -1,7 +1,9 @@
 scrollBy: delta
 	"Move the contents in the direction delta."
 	"For now, delta is assumed to have a zero x-component"
-	| newYoffset |
+	| newYoffset r |
 	newYoffset _ scroller offset y - delta y max: 0.
 	scroller offset: scroller offset x @ newYoffset.
-	scrollBar value: (newYoffset asFloat / self totalScrollRange)
+	(r _ self leftoverScrollRange) = 0
+		ifTrue: [scrollBar value: 0.0]
+		ifFalse: [scrollBar value: newYoffset asFloat / r]
