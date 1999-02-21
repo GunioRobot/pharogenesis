871e2760517f732ae062f1c46549f0a1585639db
dew <unknown>
1999-02-21 03:08:00 +0000

    no message

--- ScrollBar/instance/access/interval-3A-.cs
+++ ScrollBar/instance/access/interval-3A-.cs
@@ -1,4 +1,5 @@
 interval: d
 	"Supply an optional floating fraction so slider can expand to indicate range"
-	interval _ d.
-	self computeSlider
+	interval _ d min: 1.0.
+	self expandSlider.
+	self computeSlider.
