15366c67c0716d55c72d95c7383483448fc2ceab
dew <unknown>
1999-02-21 03:08:00 +0000

    no message

--- ScrollBar/instance/scrolling/setValue-3A-.cs
+++ ScrollBar/instance/scrolling/setValue-3A-.cs
@@ -1,2 +1,3 @@
 setValue: newValue
-	^ super setValue: (newValue + 0.0001 truncateTo: scrollDelta)
+	"Using roundTo: instead of truncateTo: ensures that scrollUp will scroll the same distance as scrollDown."
+	^ super setValue: (newValue roundTo: scrollDelta)
