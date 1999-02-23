7cc0998a4098d0b4fcac5a3180b20a85f31e501d
ar <unknown>
1999-02-23 00:10:00 +0000

    no message

---
+++ FlashFileReader/instance/processing tags/processDefineBitsJPEG2-3A-.cs
@@ -0,0 +1,10 @@
+processDefineBitsJPEG2: data
+	| id image decoder |
+	id _ data nextWord.
+	decoder _ FlashJPEGDecoder new.
+	decoder isStreaming: self isStreaming.
+	decoder decodeJPEGTables: data.
+	image _ decoder decodeNextImageFrom: data.
+	Preferences compressFlashImages ifTrue:[image _ image asFormOfDepth: 8].
+	self recordBitmap: id data: image.
+	^true
