20f0541be07fe1e01961e8b17c8313ebca0c986f
tk <unknown>
1999-02-24 11:17:00 +0000

    no message

---
+++ TranscriptStream/instance/file in-2F-out/objectForDataStream-3A-.cs
@@ -0,0 +1,4 @@
+objectForDataStream: refStrm
+	"I am about to be written on an object file.  Write a path to me in the other system instead."
+
+	^ DiskProxy global: #Transcript selector: #yourself args: #()
