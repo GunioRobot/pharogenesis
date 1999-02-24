83afffff8c5282d95c4c0ab6f31ee19e75754079
tk <unknown>
1999-02-24 11:10:00 +0000

    no message

---
+++ DisplayScreen/instance/disk I-2F-O/objectForDataStream-3A-.cs
@@ -0,0 +1,5 @@
+objectForDataStream: refStrm
+	"I am about to be written on an object file.  Write a reference to the Display in the other system instead.  "
+
+	"A path to me"
+	^ DiskProxy global: #Display selector: #yourself args: #()
