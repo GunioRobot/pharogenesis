5efa0fcda89799bff4ef6ba11eb3be7ad73ee6f9
tk <unknown>
1999-02-24 11:10:00 +0000

    no message

---
+++ Class/instance/fileIn-2F-Out/objectForDataStream-3A-.cs
@@ -0,0 +1,5 @@
+objectForDataStream: refStrm
+	"I am about to be written on an object file.  Write a reference to a class in Smalltalk instead."
+
+	^ DiskProxy global: self theNonMetaClass name selector: #yourself
+			args: #()
