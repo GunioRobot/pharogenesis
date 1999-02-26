3718877fb80fd51c5d70c284104ea94ce51498c0
di <unknown>
1999-02-26 17:22:00 +0000

    no message

--- ObjectMemory/instance/gc -2D--2D- compaction/beRootWhileForwarding-3A-.cs
+++ ObjectMemory/instance/gc -2D--2D- compaction/beRootWhileForwarding-3A-.cs
@@ -1,27 +1,13 @@
 beRootWhileForwarding: oop
-	"Record that the given oop in the old object area points to an object in the young area when oop may be forwarded. Like beRoot:"
+	"Record that the given oop in the old object area points to an object in the young area when oop may be forwarded."
 	"Warning: No young objects should be recorded as roots. Callers are responsible for ensuring this constraint is not violated."
 
-	| header forwarding fwdBlock newHeader |
+	| header fwdBlock |
 	header _ self longAt: oop.
-	(header bitAnd: MarkBit) ~= 0 ifTrue: [
-		forwarding _ true.
-		fwdBlock _ header bitAnd: AllButMarkBitAndTypeMask.
-		checkAssertions ifTrue: [ self fwdBlockValidate: fwdBlock ].
-		header _ self longAt: fwdBlock + 4.
-	] ifFalse: [
-		forwarding _ false.
-	].
-
-	(header bitAnd: RootBit) = 0 ifTrue: [
-		"record oop as root only if not already recorded"
-		rootTableCount < RootTableSize ifTrue: [
-			"record root only if there is room in the roots table"
-			rootTableCount _ rootTableCount + 1.
-			rootTable at: rootTableCount put: oop.
-			newHeader _ header bitOr: RootBit.
-			forwarding
-				ifTrue: [ self longAt: (fwdBlock + 4) put: newHeader ]
-				ifFalse: [ self longAt: oop put: newHeader ].
-		].
-	].
+	(header bitAnd: MarkBit) ~= 0
+		ifTrue: ["This oop is forwarded"
+				fwdBlock _ (header bitAnd: AllButMarkBitAndTypeMask) << 1.
+				DoAssertionChecks ifTrue: [ self fwdBlockValidate: fwdBlock ].
+				self noteAsRoot: oop headerLoc: fwdBlock + 4]
+		ifFalse: ["Normal -- no forwarding"
+				self noteAsRoot: oop headerLoc: oop]
