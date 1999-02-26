0a3b424cfbf0d464af855131460f38054f4e7f36
di <unknown>
1999-02-26 17:17:00 +0000

    no message

--- ObjectMemory/instance/garbage collection/possibleRootStoreInto-3A-value-3A-.cs
+++ ObjectMemory/instance/garbage collection/possibleRootStoreInto-3A-value-3A-.cs
@@ -1,19 +1,7 @@
 possibleRootStoreInto: oop value: valueObj
-	"Called when storing the given value object into the given old object. If valueObj is young, record the fact that oldObj is now a root for incremental garbage collection."
-	"Warning: No young objects should be recorded as roots."
+	"oop is an old object.  If valueObj is young, mark the object as a root."
 
-	| header |
 	self inline: false.
-	((valueObj >= youngStart) and:
-	 [(self isIntegerObject: valueObj) not]) ifTrue: [
-		header _ self longAt: oop.
-		(header bitAnd: RootBit) = 0 ifTrue: [
-			"record oop as root only if not already recorded"
-			rootTableCount < RootTableSize ifTrue: [
-				"record root only if there is room in the roots table"
-				rootTableCount _ rootTableCount + 1.
-				rootTable at: rootTableCount put: oop.
-				self longAt: oop put: (header bitOr: RootBit).
-			].
-		].
-	].
+	((valueObj >= youngStart) and: [(self isIntegerObject: valueObj) not]) ifTrue:
+		["Yes, valueObj is a young object"
+		self noteAsRoot: oop headerLoc: oop].
