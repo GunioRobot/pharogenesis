2d0d4cced2747dc172a033132b13aece0c945399
crl <unknown>
1999-02-26 15:34:00 +0000

    no message

---
+++ ContextPart/instance/controlling/terminateTo-3A-.cs
@@ -0,0 +1,12 @@
+terminateTo: previousContext
+	"Terminate all the Contexts between me and previousContext, if previousContext is on my Context stack. Make previousContext my sender."
+
+	| currentContext sendingContext |
+
+	(self hasSender: previousContext) ifTrue: [
+		currentContext _ sender.
+		[currentContext == previousContext] whileFalse: [
+			sendingContext _ currentContext sender.
+			currentContext terminate.
+			currentContext _ sendingContext]].
+	sender _ previousContext
