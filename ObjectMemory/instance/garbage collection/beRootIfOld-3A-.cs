9eb9f0e9adf2672fb46a2e834f64311f72ca4e8a
di <unknown>
1999-02-26 17:17:00 +0000

    no message

--- ObjectMemory/instance/garbage collection/beRootIfOld-3A-.cs
+++ ObjectMemory/instance/garbage collection/beRootIfOld-3A-.cs
@@ -1,18 +1,7 @@
 beRootIfOld: oop
-	"Record that the given oop in the old object area may point to an object in the young area."
+	"If this object is old, mark it as a root (because a new object may be stored into it)"
 
-	| header |
 	self inline: false.
-	((oop < youngStart) and: [(self isIntegerObject: oop) not]) ifTrue: [
-		"oop is in the old object area"
-		header _ self longAt: oop.
-		(header bitAnd: RootBit) = 0 ifTrue: [
-			"record oop as root only if not already recorded"
-			rootTableCount < RootTableSize ifTrue: [
-				"record root only if there is room in the roots table"
-				rootTableCount _ rootTableCount + 1.
-				rootTable at: rootTableCount put: oop.
-				self longAt: oop put: (header bitOr: RootBit).
-			].
-		].
-	].
+	((oop < youngStart) and: [(self isIntegerObject: oop) not]) ifTrue:
+		["Yes, oop is an old object"
+		self noteAsRoot: oop headerLoc: oop].
