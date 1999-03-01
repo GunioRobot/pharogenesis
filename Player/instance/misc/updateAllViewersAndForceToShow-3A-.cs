93cebfd23d227b30af2dab4b2d6aa9c08455ac6f
sw <unknown>
1999-03-01 11:52:00 +0000

    no message

---
+++ Player/instance/misc/updateAllViewersAndForceToShow-3A-.cs
@@ -0,0 +1,6 @@
+updateAllViewersAndForceToShow: aCategory
+	"The receiver's structure has changed, so viewers on it need to be reconstituted."
+	| aPresenter |
+	(aPresenter _ self costume presenter) ifNil: [^ self].
+	self allOpenViewers do:
+		[:aViewer | aPresenter updateViewer: aViewer forceToShow: aCategory]
