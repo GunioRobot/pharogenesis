3771bc7b3e8fde18d9e4fbb5bf5c896bb723c335
sw <unknown>
1999-03-01 11:52:00 +0000

    no message

--- Player/instance/misc/updateAllViewers.cs
+++ Player/instance/misc/updateAllViewers.cs
@@ -1,11 +1,6 @@
 updateAllViewers
 	"The receiver's structure has changed, so viewers on it need to be reconstituted."
-
-	| aPresenter viewers |
+	| aPresenter |
 	(aPresenter _ self costume presenter) ifNil: [^ self].
-
-	viewers _ self costume world allMorphs select:
-		[:m | (m isKindOf: PartsViewer) and: [m scriptedPlayer == self]].
-
-	viewers do: [:m | aPresenter updatePartsViewer: m]
-	"overkill, removes player from viewer cache repeatedly"
+	self allOpenViewers do:
+		[:aViewer | aPresenter updateViewer: aViewer]
