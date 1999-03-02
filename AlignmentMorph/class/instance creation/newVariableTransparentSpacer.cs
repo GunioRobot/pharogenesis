d4125d330c8685f9e55e1a11bc7928c58ae274d9
sw <unknown>
1999-03-02 14:46:00 +0000

    no message

---
+++ AlignmentMorph/class/instance creation/newVariableTransparentSpacer.cs
@@ -0,0 +1,9 @@
+newVariableTransparentSpacer
+	"Answer a space-filling instance of me of the given color."
+
+	^ self new
+		hResizing: #spaceFill;
+		vResizing: #spaceFill;
+		inset: 0;
+		borderWidth: 0;
+		color: Color transparent
