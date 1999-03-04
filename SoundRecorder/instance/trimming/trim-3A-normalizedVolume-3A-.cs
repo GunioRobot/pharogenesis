42f6910b72d158de241de9854d70e2b3dac63352
di <unknown>
1999-03-04 22:52:00 +0000

    no message

--- SoundRecorder/instance/trimming/trim-3A-normalizedVolume-3A-.cs
+++ SoundRecorder/instance/trimming/trim-3A-normalizedVolume-3A-.cs
@@ -4,6 +4,7 @@ trim: threshold normalizedVolume: percentOfMaxVolume
 	| max min sum totalSamples bufSize s dcOffset startPlace endPlace resultBuf nFactor |
 	stereo ifTrue: [self error: 'stereo trimming is not yet supported'].
 	paused ifFalse: [self error: 'must stop recording before trimming'].
+	recordedBuffers _ recordedSound sounds collect: [:snd | snd samples].
 	recordedBuffers isEmpty ifTrue: [^ self].
 
 	max _ min _ sum _ totalSamples _ 0.
@@ -18,14 +19,19 @@ trim: threshold normalizedVolume: percentOfMaxVolume
 	dcOffset _ sum // totalSamples.
 
 	"a place is an array of <buffer index><index of sample in buffer>"
-	startPlace _ self scanForStartThreshold: threshold dcOffset: dcOffset.
-	(((startPlace at: 1) = recordedBuffers size) and:
-	 [(startPlace at: 2) = recordedBuffers last size])
-		ifTrue: [  "no samples above threshold"
-			recordedBuffers _ recordedBuffers species new.
-			^ self].
-	endPlace _ self scanForEndThreshold: threshold dcOffset: dcOffset.
+	startPlace _ self scanForStartThreshold: threshold
+					dcOffset: dcOffset
+					minDur: (samplingRate/60.0) asInteger "at least 1/60th of a second"
+					startingAt: #(1 1).
+	startPlace = self endPlace ifTrue:
+		["no samples above threshold"
+		recordedBuffers _ nil.  ^ self].
 
+	endPlace _ self scanForEndThreshold: threshold
+					dcOffset: dcOffset
+					minLull: (samplingRate/5) asInteger
+					startingAt: startPlace.
 	nFactor _ self normalizeFactorFor: percentOfMaxVolume min: min max: max dcOffset: dcOffset.
 	resultBuf _ self copyFrom: startPlace to: endPlace normalize: nFactor dcOffset: dcOffset.
-	recordedBuffers _ recordedBuffers species with: resultBuf.
+	recordedSound _ SampledSound new setSamples: resultBuf samplingRate: samplingRate.
+	recordedBuffers _ nil
