50cf007191f5e7e25863c9a422f7085b61e9f77c
sw <unknown>
1999-03-06 02:08:00 +0000

    no message

---
+++ FlapTab/instance/show & hide/hideFlap.cs
@@ -0,0 +1,10 @@
+hideFlap
+	| aWorld |
+	aWorld _ self currentWorld.
+	referent privateDelete.
+	aWorld removeAccommodationForFlap: self.
+	flapShowing _ false.
+	self isInWorld ifFalse: [aWorld addMorphFront: self].
+	self adjustPositionAfterHidingFlap.
+	aWorld haloMorphs do:
+		[:m | m target isInWorld ifFalse: [m delete]]
