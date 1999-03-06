6e614de0fd3e95f9c38c54e64d0d9ac9a8cb8e32
sw <unknown>
1999-03-06 09:34:00 +0000

    no message

--- ChangeSorter/instance/creation/openAsMorph.cs
+++ ChangeSorter/instance/creation/openAsMorph.cs
@@ -1,9 +1,3 @@
 openAsMorph
 	"ChangeSorter new openAsMorph"
-	|  window |
-
-	myChangeSet ifNil: [self myChangeSet: Smalltalk changes]. 
-	window _ (SystemWindow labelled: self labelString) model: self.
-	self openAsMorphIn: window rect: (0@0 extent: 1@1).
-	World ifNil: [^ window openInMVC].  "test"
-	window openInWorld
+	^ self morphicWindow openInWorld.
