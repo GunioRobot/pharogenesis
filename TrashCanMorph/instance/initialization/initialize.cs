ace3c6fac0141bee7dfbb90e4c4f653c183ee0b1
sw <unknown>
1999-03-07 00:53:00 +0000

    no message

--- TrashCanMorph/instance/initialization/initialize.cs
+++ TrashCanMorph/instance/initialization/initialize.cs
@@ -1,14 +1,9 @@
 initialize
 
 	super initialize.
-	handsOverMe _ IdentitySet new.
 	self image: TrashPicOn;
 		offImage: TrashPic;
 		pressedImage: TrashPicOn.
 	self setNameTo: 'Trash'.
-	self setProperty: #scriptingControl toValue: true.
 	self setBalloonText:
-'The Trash Can
-To remove an object, drag it
-over the Trash, and drop it,
-and it will disappear.'.
+'To remove an object, drop it on this trash can.'.
