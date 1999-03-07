1ffb3c8b15fbd8122268f56431830764612ae9b2
sw <unknown>
1999-03-07 00:53:00 +0000

    no message

---
+++ StandardScriptingSystem/instance/script-2D-control/stopButton.cs
@@ -0,0 +1,15 @@
+stopButton
+	"Answer a new button that can serve as a stop button"
+	| aButton |
+	aButton _ ThreePhaseButtonMorph new.
+	aButton
+		image:  (ScriptingSystem formAtKey: 'StopPic');
+		offImage: (ScriptingSystem formAtKey: 'StopPic');
+		pressedImage:  (ScriptingSystem formAtKey: 'StopPicOn').
+		aButton actionSelector: #stopUp:with:; 
+		arguments: (Array with: nil with: aButton);
+		actWhen: #buttonUp;
+		target: self;
+		setNameTo: 'Stop Button'; 
+		setBalloonText: 'Pause all ticking scripts.'.
+	^ aButton
