15437645474b804513f55cdadae9e3278d109cba
di <unknown>
1999-03-08 13:50:00 +0000

    no message

---
+++ EventRecorderMorph/instance/fileIn-2F-Out/writeTape.cs
@@ -0,0 +1,15 @@
+writeTape
+	| args b |
+	args _ (b _ self button: 'writeTape') isNil
+				ifTrue: [#()]
+				ifFalse: [b arguments].
+	(args size > 0 and: [args first size > 0])
+		ifTrue: [args first.
+				self writeTape: args first]
+		ifFalse: [(Smalltalk at: #RequestBoxMorph
+					ifAbsent: [^ self writeTape: (FillInTheBlankMorph
+											request: 'Tape to write'
+											initialAnswer: 'tapeName.tape')])
+						request: 'Tape to write'
+						respondTo: self
+						selector: #writeTape:]
