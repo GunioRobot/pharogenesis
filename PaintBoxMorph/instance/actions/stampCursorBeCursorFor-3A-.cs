7fe2a57ec365538224defd316554813268061bd7
tk <unknown>
1999-03-08 22:35:00 +0000

    no message

--- PaintBoxMorph/instance/actions/stampCursorBeCursorFor-3A-.cs
+++ PaintBoxMorph/instance/actions/stampCursorBeCursorFor-3A-.cs
@@ -1,7 +1,7 @@
 stampCursorBeCursorFor: anAction
 	"User just chose a stamp.  Take that stamp picture and make it be the cursor for the tool named."
 	"self stampCursorBeCursorFor: #star:.
-	currentCursor offset: 9@3.			Has side effect on the saved cursor."
+	currentCursor offset: -9@-3.			Has side effect on the saved cursor."
 
 	(self findButton: anAction) arguments at: 3 put: currentCursor.
 		"Already converted to 8 bits and in the right form"
