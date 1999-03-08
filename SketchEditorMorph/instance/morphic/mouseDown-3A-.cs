6da3a7eb419ff799d3e3c3782fd29678509087b8
tk <unknown>
1999-03-08 23:04:00 +0000

    no message

--- SketchEditorMorph/instance/morphic/mouseDown-3A-.cs
+++ SketchEditorMorph/instance/morphic/mouseDown-3A-.cs
@@ -1,6 +1,6 @@
 mouseDown: evt
 	"Start a new stroke.  Check if any palette setting have changed.  6/11/97 20:30 tk"
-
+	| cur |
 	"verify that we are in a good state"
 	self verifyState.		"includes prepareToPaint and #scalingOrRotate"
 	undoBuffer _ paintingForm deepCopy.	"know we will draw something"
@@ -8,5 +8,7 @@ mouseDown: evt
 	strokeOrigin _ evt cursorPoint.
 		"origin point for pickup: rect: ellispe: polygon: line: star:.  Always take it."
 	action == #pickup: ifTrue: [
-		evt hand showTemporaryCursor: Cursor corner].
+		cur _ Cursor corner clone.
+		cur offset: 0@0  "cur offset abs".
+		evt hand showTemporaryCursor: cur].
 	action == #polygon: ifTrue: [self polyNew: evt].	"a mode lets you drag vertices"
