667060cdc02a423e33e6ec6a69dbb2165d850446
jsp <unknown>
1999-03-08 17:02:00 +0000

    no message

---
+++ WonderlandActor/instance/primitive behaviors/alignWith-3A-duration-3A-.cs
@@ -0,0 +1,4 @@
+alignWith: anObject duration: aDuration
+	"This method sets the orientation of this instance to be the same as that of the specified object."
+
+	^ self turnTo: anObject duration: aDuration.
