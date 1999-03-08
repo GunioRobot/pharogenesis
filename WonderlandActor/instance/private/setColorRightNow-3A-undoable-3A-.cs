538931b7ff35fffba56a245d795eedfd8b942f8d
jsp <unknown>
1999-03-08 16:33:00 +0000

    no message

---
+++ WonderlandActor/instance/private/setColorRightNow-3A-undoable-3A-.cs
@@ -0,0 +1,16 @@
+setColorRightNow: newColor undoable: isUndoable
+	"Change this instance's color instantaneously"
+
+	| aColor |
+
+	(isUndoable) ifTrue: [
+		myWonderland getUndoStack push: (UndoColorChange for: self from: (self getColor)) ].
+
+	((newColor at: 1) isNumber)
+				ifTrue: [ aColor _ B3DColor4 red: (newColor at: 1) green: (newColor at: 2)
+										blue: (newColor at: 3) alpha: (myColor alpha) ]
+				ifFalse: [ aColor _ Color perform: newColor.
+						  aColor _ B3DColor4 red: (aColor red) green: (aColor green)
+										blue: (aColor blue) alpha: (myColor alpha) ].
+
+	self setColorVector: aColor.
