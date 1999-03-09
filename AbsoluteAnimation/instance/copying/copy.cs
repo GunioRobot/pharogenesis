b43338c5c167bb935dc9ebe1451081b708cd4aeb
jsp <unknown>
1999-03-09 15:49:00 +0000

    no message

---
+++ AbsoluteAnimation/instance/copying/copy.cs
@@ -0,0 +1,19 @@
+copy
+	"Creates a copy of the animation"
+
+	| anim |
+
+	anim _ AbsoluteAnimation new.
+
+	anim object: animatedObject
+		update: updateFunction
+		getStartState: getStartStateFunction
+		getEndState: getEndStateFunction
+		style: styleFunction
+		duration: duration
+		undoable: undoable
+		inWonderland: myWonderland.
+
+	(direction = Forward) ifFalse: [ anim reverseDirection ].
+
+	^ anim.
