4c82ee297194f12d45220fc54ba5af6b13c26090
jsp <unknown>
1999-03-09 15:49:00 +0000

    no message

---
+++ AbsoluteAnimation/instance/copying/makeUndoVersion.cs
@@ -0,0 +1,20 @@
+makeUndoVersion
+	"Creates the undo version of an animation"
+
+	| anim |
+
+	anim _ AbsoluteAnimation new.
+
+	anim object: animatedObject
+		update: updateFunction
+		getStartState: getStartStateFunction
+		getEndState: getEndStateFunction
+		style: styleFunction
+		duration: 0.5
+		undoable: false
+		inWonderland: myWonderland.
+
+	anim stop.
+	(direction = Forward) ifTrue: [ anim reverseDirection ].
+
+	^ anim.
