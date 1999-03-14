35e847718712f0058d21ac042c4c5ac014764ab9
di <unknown>
1999-03-14 23:27:00 +0000

    no message

--- SimpleButtonMorph/instance/events/mouseMove-3A-.cs
+++ SimpleButtonMorph/instance/events/mouseMove-3A-.cs
@@ -1,7 +1,8 @@
 mouseMove: evt
-
+	actWhen == #buttonDown ifTrue: [^ self].
 	(self containsPoint: evt cursorPoint)
-		ifTrue: [self color: (oldColor mixed: 1/2 with: Color white).
+		ifTrue: [oldColor ifNotNil: [self color: (oldColor mixed: 1/2 with: Color white)].
 				(actWhen == #whilePressed and: [evt anyButtonPressed])
-					 ifTrue: [self doButtonAction]]
-		ifFalse: [self color: oldColor].
+					 ifTrue: [self doButtonAction.
+							evt hand noteSignificantEvent: evt]]
+		ifFalse: [oldColor ifNotNil: [self color: oldColor]].
