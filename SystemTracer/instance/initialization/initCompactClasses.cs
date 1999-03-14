0424ef54e678b4ab1b36be2b1f8e0ae403280791
di <unknown>
1999-03-14 10:00:00 +0000

    no message

--- SystemTracer/instance/initialization/initCompactClasses.cs
+++ SystemTracer/instance/initialization/initCompactClasses.cs
@@ -1,4 +1,4 @@
-initCompactClasses
+initCompactClasses 
 	| c |
 	c _ Array new: 31.	
 	"These classes have a short name (their index in this table.  It is not their oop.)
@@ -7,9 +7,12 @@ initCompactClasses
 	c at: 4 put: Float.  c at: 5 put: LargePositiveInteger.  c at: 6 put: String.
 	c at: 7 put: MethodDictionary.  c at: 8 put: Association.  c at: 9 put: Point.
 	c at: 10 put: Rectangle.  c at: 11 put: ClassOrganizer.  c at: 12 put: TextLineInterval.
-	c at: 13 put: BlockContext.  c at: 14 put: MethodContext.  c at: 15 put: PseudoContext.
-	compactClasses _ c.
-	"Leave 16 to 31 for user defined compact classes."
 
-	"Attempt to correctly write contextCace image.."
-	compactClasses _ Smalltalk compactClassesArray
+	"**NOTE** at present the Squeak VM relies on BlockContext=13 and MethodContext=14"
+	c at: 13 put: BlockContext.  c at: 14 put: MethodContext.  c at: 15 put: PseudoContext.
+
+	compactClasses _ c.
+	"16 to 31 are available for user defined compact classes."
+
+	"Attempt to correctly write contextCache image.."
+	"compactClasses _ Smalltalk compactClassesArray"
