f0bba9100512ddadadfeaa14006dd009bdc1b11f
RJ <unknown>
1999-03-15 19:35:00 +0000

    no message

--- Float/instance/mathematical functions/raisedTo-3A-.cs
+++ Float/instance/mathematical functions/raisedTo-3A-.cs
@@ -1,5 +1,11 @@
 raisedTo: aNumber
 	"Answer the receiver raised to aNumber."
 
-	0.0 = aNumber ifTrue: [^ 1.0].  "special case for 0.0 raisedTo: 0.0"
-	^ (self ln * aNumber asFloat) exp
+	aNumber isInteger ifTrue:
+		["Do the special case of integer power"
+		^ self raisedToInteger: aNumber].
+	self < 0.0 ifTrue:
+		[ self error: self printString, ' raised to a non-integer power' ].
+	0.0 = aNumber ifTrue: [^ 1.0].				"special case for exponent = 0.0"
+	(self= 0.0) | (aNumber = 1.0) ifTrue: [^ self].	"special case for self = 1.0"
+	^ (self ln * aNumber asFloat) exp			"otherwise use logarithms"
