177db84fb9e4226de0688942ffc6b0900d5cb8e9
jsp <unknown>
1999-03-17 16:57:00 +0000

    no message

---
+++ WonderlandActor/instance/primitive behaviors/moveTo-3A-eachFrameUntil-3A-.cs
@@ -0,0 +1,20 @@
+moveTo: aVector eachFrameUntil: aCondition
+	"Move the actor to a specified point each frame until the condition returns true."
+
+	"Check our arguments to make sure they're valid"
+	[ WonderlandVerifier VerifyTarget: aVector ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine where to move ' , myName , ' to because ', msg.
+			^ nil ].
+
+	[ WonderlandVerifier VerifyCondition: aCondition ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine for how long ' , myName , ' should move to this position because ', msg.
+			^ nil ].
+
+	myWonderland getUndoStack push: (UndoPOVChange for: self from: (self getPointOfView)).
+
+	^ self doEachFrame: [ self moveToRightNow: aVector undoable: false ]
+			until: aCondition.
+
+
