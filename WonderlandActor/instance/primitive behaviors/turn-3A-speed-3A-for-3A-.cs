af7a80cbc210c4e69c5f940e8b94c63805765262
jsp <unknown>
1999-03-18 11:10:00 +0000

    no message

---
+++ WonderlandActor/instance/primitive behaviors/turn-3A-speed-3A-for-3A-.cs
@@ -0,0 +1,28 @@
+turn: aDirection speed: aSpeed for: aLifetime
+	"Creates and returns an action that turn the object in the specified direction at the specified speed."
+
+	"Check our arguments to make sure they're valid"
+	[ WonderlandVerifier VerifyDirection: aDirection ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine the direction to turn ' , myName , ' because ', msg.
+			^ nil ].
+
+	[ WonderlandVerifier VerifyPositiveNumber: aSpeed ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine the speed to turn ' , myName , ' at because ', msg.
+			^ nil ].
+
+	[ WonderlandVerifier VerifyNonNegativeNumber: aLifetime ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine how long it should turn ' , myName , ' because ', msg.
+			^ nil ].
+
+	myWonderland getUndoStack push: (UndoPOVChange for: self from: (self getPointOfView)).
+
+	^ self doEachFrame: [
+				self turnRightNow: aDirection
+				numberOfTurns: ((myWonderland getScheduler getElapsedTime) * aSpeed)
+				undoable: false
+						]
+		   for: aLifetime.
+
