2521e4d1a9602be68f842001dd8b185585643b61
jsp <unknown>
1999-03-18 15:46:00 +0000

    no message

---
+++ WonderlandActor/instance/set property/setPointOfView-3A-asSeenBy-3A-eachFrameUntil-3A-.cs
@@ -0,0 +1,44 @@
+setPointOfView: aVector asSeenBy: reference eachFrameUntil: aCondition
+	"Sets the object's position and orientation"
+
+	| positionVector rotationVector |
+
+	[ WonderlandVerifier VerifyPointOfView: aVector ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine what point of view to assign ' , myName , ' because ', msg.
+			^ nil ].
+
+	( aVector isKindOf: WonderlandActor) ifTrue: [
+			myWonderland reportErrorToUser: 'Squeak does not allow you to use an Actor to specify the point of view when using asSeenBy.'.
+			^ nil ].
+
+	[ WonderlandVerifier VerifyReferenceFrame: reference ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser:
+				'Squeak can not set the point of view for ' , myName , ' relative to ' , (reference asString) , ' because ', msg.
+			^ nil ].
+
+	[ WonderlandVerifier VerifyCondition: aCondition ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine for how long ' , myName , ' should keep this point of view because ', msg.
+			^ nil ].
+
+	positionVector _ Array new: 3.
+	positionVector at: 1 put: (aVector at: 1).
+	positionVector at: 2 put: (aVector at: 2).
+	positionVector at: 3 put: (aVector at: 3).
+
+	rotationVector _ Array new: 3.
+	rotationVector at: 1 put: (aVector at: 4).
+	rotationVector at: 2 put: (aVector at: 5).
+	rotationVector at: 3 put: (aVector at: 6).
+
+	myWonderland getUndoStack push: (UndoPOVChange for: self
+														from: (self getPointOfView)).
+
+	^ self doEachFrame: [ 
+							self moveToRightNow: positionVector undoable: false.
+							self turnToRightNow: rotationVector undoable: false.
+						]
+			until: aCondition.
+
