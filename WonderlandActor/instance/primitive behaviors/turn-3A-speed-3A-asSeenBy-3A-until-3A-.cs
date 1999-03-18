cc8e1ffc873e5efdb0f4af7f6f4a87d25c9857dc
jsp <unknown>
1999-03-18 11:11:00 +0000

    no message

---
+++ WonderlandActor/instance/primitive behaviors/turn-3A-speed-3A-asSeenBy-3A-until-3A-.cs
@@ -0,0 +1,34 @@
+turn: aDirection speed: aSpeed asSeenBy: reference until: aCondition
+	"Creates and returns an action that turn the object in the specified direction at the specified speed."
+
+	"Check our arguments to make sure they're valid"
+	[ WonderlandVerifier VerifyDirection: aDirection ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine the direction to turn ' , myName , ' because ', msg.
+			^ nil ].
+
+	[ WonderlandVerifier VerifyPositiveNumber: aSpeed ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine the speed to turn ' , myName , ' at because ', msg.
+			^ nil ].
+
+	[ WonderlandVerifier VerifyReferenceFrame: reference ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak can not turn ' , myName , ' relative to ' , (reference asString) , ' because ', msg.
+			^ nil ].
+
+	[ WonderlandVerifier VerifyCondition: aCondition ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine how long it should turn ' , myName , ' because ', msg.
+			^ nil ].
+
+	myWonderland getUndoStack push: (UndoPOVChange for: self from: (self getPointOfView)).
+
+	^ self doEachFrame: [
+				self turnRightNow: aDirection
+				numberOfTurns: ((myWonderland getScheduler getElapsedTime) * aSpeed)
+				asSeenBy: reference
+				undoable: false
+						]
+		   until: aCondition.
+
