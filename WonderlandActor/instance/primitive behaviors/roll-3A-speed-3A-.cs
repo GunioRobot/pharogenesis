55e429e7f542366973721076ede44928857a9c9f
jsp <unknown>
1999-03-18 10:43:00 +0000

    no message

---
+++ WonderlandActor/instance/primitive behaviors/roll-3A-speed-3A-.cs
@@ -0,0 +1,21 @@
+roll: aDirection speed: aSpeed
+	"Creates and returns an action that rolls the object in the specified direction at the specified speed."
+
+	"Check our arguments to make sure they're valid"
+	[ WonderlandVerifier VerifyDirection: aDirection ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine the direction to roll ' , myName , ' because ', msg.
+			^ nil ].
+
+	[ WonderlandVerifier VerifyPositiveNumber: aSpeed ]
+		ifError: [ :msg :rcvr |
+			myWonderland reportErrorToUser: 'Squeak could not determine the speed to roll ' , myName , ' at because ', msg.
+			^ nil ].
+
+	myWonderland getUndoStack push: (UndoPOVChange for: self from: (self getPointOfView)).
+
+	^ self doEachFrame: [
+				self rollRightNow: aDirection
+				numberOfTurns: ((myWonderland getScheduler getElapsedTime) * aSpeed)
+				undoable: false
+						].
