7d65625cdb54ef9519b20a1a1fa06fe8f4c25eee
di <unknown>
1999-03-18 19:38:00 +0000

    no message

--- ObjectMemory/instance/header access/formatOf-3A-.cs
+++ ObjectMemory/instance/header access/formatOf-3A-.cs
@@ -1,10 +1,10 @@
 formatOf: oop
 "       0      no fields
-        1      fixed fields only (possibly containing pointers)
-        2      indexable fields only (possibly containing pointers)
-        3      both fixed and indexable fields (possibly containing pointers)
- 
-        4      unused  -- may use for contexts (size = stackPointer for scanning purposes)
+        1      fixed fields only (all containing pointers)
+        2      indexable fields only (all containing pointers)
+        3      both fixed and indexable fields (all containing pointers)
+        4      both fixed and indexable weak fields (all containing pointers).
+
         5      unused
         6      indexable word fields only (no pointers)
         7      unused
