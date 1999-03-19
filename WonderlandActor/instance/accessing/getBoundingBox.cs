32d52a45e3152d06ae9a036361a9480ee367d3a3
jsp <unknown>
1999-03-19 14:33:00 +0000

    no message

---
+++ WonderlandActor/instance/accessing/getBoundingBox.cs
@@ -0,0 +1,36 @@
+getBoundingBox
+	"Returns the actor's bounding box"
+
+	| size x y z nOrigin nCorner childBBox cOrigin cCorner meshBBox mOrigin mCorner |
+
+	(myMesh notNil)
+		ifTrue: [ meshBBox _ myMesh boundingBox.
+				 size _ self getSizeVector.
+				 mOrigin _ meshBBox origin.
+				 mCorner _ meshBBox corner.
+
+				 x _ (mOrigin x) * (size x).
+				 y _ (mOrigin y) * (size y).
+				 z _ (mOrigin z) * (size z).
+				 nOrigin _ B3DVector3 x: x y: y z: z.
+
+				 x _ (mCorner x) * (size x).
+				 y _ (mCorner y) * (size y).
+				 z _ (mCorner z) * (size z).
+				 nCorner _ B3DVector3 x: x y: y z: z.
+				]
+		ifFalse: [ nOrigin _ (B3DVector3 x:0 y:0 z:0).
+				  nCorner _ (B3DVector3 x:0 y:0 z:0) ].
+
+
+	myChildren do: [:child | 
+			(child isPart) ifTrue: [
+									childBBox _ child getBoundingBox: self.
+				 					cOrigin _ childBBox origin.
+				 					cCorner _ childBBox corner.
+									nOrigin _ nOrigin min: cOrigin.
+									nCorner _ nCorner max: cCorner.
+								 ].
+					].
+
+	^ Rectangle origin: nOrigin corner: nCorner.
