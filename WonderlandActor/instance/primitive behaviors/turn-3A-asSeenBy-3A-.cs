25469d67a1c8d293da7490c212c2aea57e62756c
jsp <unknown>
1999-03-19 10:59:00 +0000

    no message

---
+++ WonderlandActor/instance/primitive behaviors/turn-3A-asSeenBy-3A-.cs
@@ -0,0 +1,6 @@
+turn: aDirection asSeenBy: reference
+	"Turn the actor once around in the specified direction, taking 1 second and using the Gently animation style."
+
+	^ (self turn: aDirection turns: 0.25 duration: 1.0 asSeenBy: reference
+			style: gently).
+
