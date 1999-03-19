1862ff44077c7a116e42e84d5711cc3a68af4339
jsp <unknown>
1999-03-19 11:50:00 +0000

    no message

---
+++ WonderlandActor/instance/private/scaleByVector-3A-.cs
@@ -0,0 +1,16 @@
+scaleByVector: aVector
+	"Scale the object by the given vector."
+
+	| aMatrix |
+	aMatrix _ B3DMatrix4x4 identity.
+
+	"Scale my matrix"
+	aMatrix scalingX: (aVector x) y: (aVector y) z: (aVector z).
+	scaleMatrix _ scaleMatrix composeWith: aMatrix.
+
+	"Now scale my parts"
+	myChildren do: [:child | (child isPart) ifTrue: [
+								child setPositionVector: (child getPositionVector) * aVector.
+								child scaleByVector: aVector
+												].
+					].
