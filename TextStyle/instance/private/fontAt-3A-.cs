8da95f98c868e43e82c3022de92cd40a630ff779
di <unknown>
1999-03-20 22:31:00 +0000

    no message

--- TextStyle/instance/private/fontAt-3A-.cs
+++ TextStyle/instance/private/fontAt-3A-.cs
@@ -2,8 +2,4 @@ fontAt: index
 	"This is private because no object outside TextStyle should depend on the 
 	representation of the font family in fontArray."
 
-	((fontArray atPin: index) isMemberOf: StrikeFont)
-				ifTrue: [^fontArray atPin: index].
-	((fontArray at: 1) isMemberOf: StrikeFont)
-				ifTrue: [^fontArray at: 1].
-	self error: 'No valid fonts in font array'
+	^ fontArray atPin: index
