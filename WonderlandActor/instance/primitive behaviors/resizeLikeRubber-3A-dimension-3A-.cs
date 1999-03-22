f31c00ef35b9ad5768dfde72dbaf6a10c8b010f9
jsp <unknown>
1999-03-22 12:42:00 +0000

    no message

---
+++ WonderlandActor/instance/primitive behaviors/resizeLikeRubber-3A-dimension-3A-.cs
@@ -0,0 +1,4 @@
+resizeLikeRubber: amount dimension: aDimension
+	"Resizes the object using a volume preserving resize"
+
+	^ self resizeLikeRubber: amount dimension: aDimension duration: 1.0 style: gently.
