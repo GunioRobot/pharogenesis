792a6f8420c2d0c67812c13bbc13fa8fa349c920
sw <unknown>
1999-03-23 15:58:00 +0000

    no message

--- ClassOrganizer/instance/accessing/listAtCategoryNumber-3A-.cs
+++ ClassOrganizer/instance/accessing/listAtCategoryNumber-3A-.cs
@@ -1,6 +1,5 @@
 listAtCategoryNumber: anInteger 
-	"Answer the array of elements stored at the position indexed by 
-	anInteger."
+	"Answer the array of elements stored at the position indexed by anInteger.  Answer nil if anInteger is larger than the number of categories."
 
 	| firstIndex lastIndex |
 	firstIndex _ 
@@ -8,5 +7,7 @@ listAtCategoryNumber: anInteger
 			ifTrue: [categoryStops at: anInteger - 1]
 			ifFalse: [0])
 		+ 1.
+	(categoryStops size < anInteger) ifTrue:
+		[^ nil].  "It can happen, if Default category got aggressively removed by some automatic operation"
 	lastIndex _ categoryStops at: anInteger.
 	^elementArray copyFrom: firstIndex to: lastIndex
