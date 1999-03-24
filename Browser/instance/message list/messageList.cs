f71e37908dc6c4944689936a328f33b90ec16c36
ccn <unknown>
1999-03-24 10:48:00 +0000

    no message

--- Browser/instance/message list/messageList.cs
+++ Browser/instance/message list/messageList.cs
@@ -1,8 +1,12 @@
 messageList
-	"Answer an Array of the message selectors of the currently selected 
-	message category. Otherwise, answer a new empty Array."
-
-	messageCategoryListIndex = 0
-		ifTrue: [^Array new]
-		ifFalse: [^self classOrMetaClassOrganizer 
-					listAtCategoryNumber: messageCategoryListIndex]
+	"Answer an Array of the message selectors of the currently selected message category, provided that the messageCategoryListIndex is in proper range.  Otherwise, answer an empty Array  If messageCategoryListIndex is found to be larger than the number of categories (it happens!), it is reset to zero."
+	| sel |
+	(sel _ self messageCategoryListSelection) ifNil: [^ Array new].
+	^ sel = ClassOrganizer allCategory
+		ifTrue: 
+			[self classOrMetaClassOrganizer
+				ifNil:		[Array new]
+				ifNotNil:	[self classOrMetaClassOrganizer allMethodSelectors]]
+		ifFalse:
+			[(self classOrMetaClassOrganizer listAtCategoryNumber: messageCategoryListIndex - 1)
+				ifNil: [messageCategoryListIndex _ 0.  Array new]]
