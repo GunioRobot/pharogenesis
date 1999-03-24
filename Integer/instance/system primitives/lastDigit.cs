844ead67865a2f4fee5a636708ab7de1b65035d6
tk <unknown>
1999-03-24 20:26:00 +0000

    no message

--- Integer/instance/system primitives/lastDigit.cs
+++ Integer/instance/system primitives/lastDigit.cs
@@ -1,4 +1,4 @@
 lastDigit
-	"Answer the last digit of the integer."
+	"Answer the last digit of the integer base 256.  LargePositiveInteger uses bytes of base two number, and each is a 'digit'."
 
 	^self digitAt: self digitLength
