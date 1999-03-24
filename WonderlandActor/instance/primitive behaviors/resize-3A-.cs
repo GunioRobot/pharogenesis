11d2ccc3d25d5e90a1d5c26b7038085424f4751a
jsp <unknown>
1999-03-24 16:46:00 +0000

    no message

---
+++ WonderlandActor/instance/primitive behaviors/resize-3A-.cs
@@ -0,0 +1,20 @@
+resize: amount
+	"Resize the object by the specified amount over 1 second using the Gently animation style."
+
+	"Check our arguments to make sure they're valid"
+		[ WonderlandVerifier VerifyNumberOrTriple: amount ]
+			ifError: [ :msg :rcvr |
+				myWonderland reportErrorToUser: 'Squeak could not determine how much to resize ' , myName , '  because ', msg.
+				^ nil ].
+
+	(amount isNumber)
+		ifTrue: [ ^ (self resizeTopToBottom: amount leftToRight: amount frontToBack: amount
+						duration: 1.0
+						style: gently). ]
+		ifFalse: [  ^ (self resizeTopToBottom: (amount at: 2)
+						leftToRight: (amount at: 1)
+						frontToBack: (amount at: 3)
+						duration: 1.0
+						style: gently). ].
+
+
