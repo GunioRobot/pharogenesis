8b8735c3c3c1dec745eebb7468c6f19181ed5b0f
jm <unknown>
1999-03-24 12:01:00 +0000

    no message

--- SampledSound/instance/initialization/setPitch-3A-dur-3A-loudness-3A-.cs
+++ SampledSound/instance/initialization/setPitch-3A-dur-3A-loudness-3A-.cs
@@ -1,12 +1,14 @@
-setPitch: p dur: d loudness: vol
+setPitch: pitchNameOrNumber dur: d loudness: vol
 	"Used to play scores using the default sample table."
 	"(SampledSound pitch: 880.0 dur: 1.5 loudness: 0.6) play"
 
-	super setPitch: p dur: d loudness: vol.
+	| p |
+	super setPitch: pitchNameOrNumber dur: d loudness: vol.
+	p _ self nameOrNumberToPitch: pitchNameOrNumber.
 	samples _ DefaultSampleTable.
 	samplesSize _ samples size.
 	initialCount _ (d * self samplingRate asFloat) rounded.
 	originalSamplingRate _
 		((self samplingRate asFloat * p asFloat) / NominalSamplePitch asFloat) asInteger.
-	self setLoudness: vol.
+	self loudness: vol.
 	self reset.
