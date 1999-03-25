8cf897ac10b8b0f5f631d986dfad8c56c0ea8dfd
jm <unknown>
1999-03-25 19:48:00 +0000

    no message

--- Color/class/colormaps/computeColormapFrom-3A-to-3A-.cs
+++ Color/class/colormaps/computeColormapFrom-3A-to-3A-.cs
@@ -1,15 +1,18 @@
 computeColormapFrom: sourceDepth to: destDepth
 	"Compute a colorMap for translating between the given depths. A colormap is a Bitmap whose entries contain the pixel values for the destination depth. Typical clients use cachedColormapFrom:to: instead."
 
-	| map |
+	| map bitsPerColor |
 	sourceDepth < 16 ifTrue: [
 		"source is 1-, 2-, 4-, or 8-bit indexed color"
 		map _ (IndexedColors copyFrom: 1 to: (1 bitShift: sourceDepth))
 					collect: [:c | c pixelValueForDepth: destDepth].
 		map _ map as: Bitmap.
 	] ifFalse: [
-		"source is 16-bit or 32-bit RGB; use colormap with 4 bits per color component"
-		map _ self computeRGBColormapFor: destDepth bitsPerColor: 4].
+		"source is 16-bit or 32-bit RGB"
+		destDepth > 8
+			ifTrue: [bitsPerColor _ 5]  "retain maximum color resolution"
+			ifFalse: [bitsPerColor _ 4].
+		map _ self computeRGBColormapFor: destDepth bitsPerColor: bitsPerColor].
 
 	"Note: zero is transparent except when source depth is one-bit deep"
 	sourceDepth > 1 ifTrue: [map at: 1 put: 0].
