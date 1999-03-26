34108e1662cf7ae1d78d7e3f2076ca4c3d45bda5
di <unknown>
1999-03-26 07:51:00 +0000

    no message

--- Object/instance/message handling/perform-3A-with-3A-with-3A-with-3A-.cs
+++ Object/instance/message handling/perform-3A-with-3A-with-3A-with-3A-.cs
@@ -1,13 +1,8 @@
 perform: aSymbol with: firstObject with: secondObject with: thirdObject 
-	"Primitive. Send the receiver the keyword message indicated by the 
-	arguments. The first argument is the selector of the message. The other 
-	arguments are the arguments of the message to be sent. Invoke 
-	messageNotUnderstood: if the number of arguments expected by the 
-	selector is not three. Optional. See Object documentation 
-	whatIsAPrimitive."
+	"Send the selector, aSymbol, to the receiver with the given arguments.
+	Fail if the number of arguments expected by the selector is not three.
+	Primitive. Optional. See Object documentation whatIsAPrimitive."
 
 	<primitive: 83>
-	^self perform: aSymbol withArguments: (Array
-			with: firstObject
-			with: secondObject
-			with: thirdObject)
+	^ self perform: aSymbol
+		withArguments: (Array with: firstObject with: secondObject with: thirdObject)
