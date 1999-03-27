4e46ddd282c2cd898db3e239810422e36274ec9c
di <unknown>
1999-03-27 23:20:00 +0000

    no message

--- Behavior/instance/testing method dictionary/includesSelector-3A-.cs
+++ Behavior/instance/testing method dictionary/includesSelector-3A-.cs
@@ -2,4 +2,4 @@ includesSelector: aSymbol
 	"Answer whether the message whose selector is the argument is in the 
 	method dictionary of the receiver's class."
 
-	^methodDict includesKey: aSymbol
+	^ self methodDict includesKey: aSymbol
