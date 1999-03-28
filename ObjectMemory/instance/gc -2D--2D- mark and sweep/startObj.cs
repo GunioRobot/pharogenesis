b1e74bbaa10bb1c05d60556ef91a8cd943467454
di <unknown>
1999-03-28 14:27:00 +0000

    no message

--- ObjectMemory/instance/gc -2D--2D- mark and sweep/startObj.cs
+++ ObjectMemory/instance/gc -2D--2D- mark and sweep/startObj.cs
@@ -2,6 +2,8 @@ startObj
 	"Start tracing the object 'child' and answer the next action. The object may be anywhere in the middle of being swept itself. See comment in markAndTrace for explanation of tracer state variables."
 
 	| oop header lastFieldOffset |
+	self inline: true.
+
 	oop _ child.
 	oop < youngStart ifTrue: [
 		"old object; skip it"
@@ -12,10 +14,17 @@ startObj
 	header _ self longAt: oop.
 	(header bitAnd: MarkBit) = 0 ifTrue: [
 		"unmarked; mark and trace"
+		"<-- Finalization support: Do not trace the object's indexed fields if it's a weak class -->"
+		(self isWeak: oop) ifTrue:[
+			"Set lastFieldOffset before the weak fields in the receiver"
+			lastFieldOffset := (self nonWeakFieldsOf: oop) << 2.
+		] ifFalse:[
+			"Do it the usual way"
+			lastFieldOffset _ self lastPointerOf: oop.
+		].
 		header _ header bitAnd: AllButTypeMask.
 		header _ (header bitOr: MarkBit) bitOr: HeaderTypeGC.
 		self longAt: oop put: header.
-		lastFieldOffset _ self lastPointerOf: oop.
 		field _ oop + lastFieldOffset.
 		^ StartField	"trace its fields and class"
 	] ifFalse: [
