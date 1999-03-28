f4b31ece3a0459c178cf4e7ea78664ee1c9f62d2
di <unknown>
1999-03-28 10:22:00 +0000

    no message

--- Array/instance/converting/elementsExchangeIdentityWith-3A-.cs
+++ Array/instance/converting/elementsExchangeIdentityWith-3A-.cs
@@ -1,3 +1,4 @@
 elementsExchangeIdentityWith: otherArray
+	"This primitive performs a bulk mutation, causing all pointers to the elements of this array to be replaced by pointers to the corresponding elements of otherArray.  At the same time, all pointers to the elements of otherArray are replaced by pointers to the corresponding elements of this array.  The identityHashes remain with the pointers rather than with the objects so that objects in hashed structures should still be properly indexed after the mutation."
 	<primitive: 128>
 	self primitiveFailed
