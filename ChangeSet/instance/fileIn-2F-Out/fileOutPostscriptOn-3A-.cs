8c188eb294be8c1f9bc6184fb453c708651fa5dc
di <unknown>
1999-03-29 13:35:00 +0000

    no message

--- ChangeSet/instance/fileIn-2F-Out/fileOutPostscriptOn-3A-.cs
+++ ChangeSet/instance/fileIn-2F-Out/fileOutPostscriptOn-3A-.cs
@@ -2,7 +2,8 @@ fileOutPostscriptOn: stream
 	"If the receiver has a postscript, put it out onto the stream.  "
 
 	| aString |
-	((aString _ self postscriptString) size > 0)
+	aString _ self postscriptString.
+	(aString ~~ nil and: [aString size > 0])
 		ifTrue:
 			[stream nextChunkPut: aString "surroundedBySingleQuotes".
 			stream cr; cr]
