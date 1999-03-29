806773a96a8cdb7d5a025bc060a2a6f9025fab43
di <unknown>
1999-03-29 11:28:00 +0000

    no message

--- Interpreter/instance/array and stream primitives/primitiveSize.cs
+++ Interpreter/instance/array and stream primitives/primitiveSize.cs
@@ -1,10 +1,8 @@
 primitiveSize
-
 	| rcvr sz |
 	rcvr _ self stackTop.
-	(self isIntegerObject: rcvr)
-		ifTrue: [sz _ 0]  "integers have no indexable fields"
-		ifFalse: [sz _ self stSizeOf: rcvr].
-	successFlag
-		ifTrue: [self pop: 1. self pushInteger: sz]
-		ifFalse: [self failSpecialPrim: 62].
+	(self isIntegerObject: rcvr) ifTrue: [^ self primitiveFail].  "Integers are not indexable"
+	(self formatOf: rcvr) < 2 ifTrue: [^ self primitiveFail].  "This is not an indexable object"
+	sz _ self stSizeOf: rcvr.
+	successFlag ifTrue:
+		[self pop: 1 thenPush: (self positive32BitIntegerFor: sz)]
