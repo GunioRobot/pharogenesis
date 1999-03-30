d6c02a5fbedfcc1eaccede82211417acfa8dafbf
jm <unknown>
1999-03-30 09:08:00 +0000

    no message

---
+++ FlashFileReader/instance/processing sounds/createSoundFrom-3A-stereo-3A-samplingRate-3A-.cs
@@ -0,0 +1,13 @@
+createSoundFrom: soundBuffers stereo: stereo samplingRate: samplingRate
+
+	| snds |
+	snds _ soundBuffers collect: [:buf |
+		(SampledSound samples: buf samplingRate: samplingRate) loudness: 1.0].
+	stereo
+		ifTrue:[
+			^ MixedSound new
+				add: (snds at: 1) pan: 0.0;
+				add: (snds at: 2) pan: 1.0;
+				yourself]
+		ifFalse: [
+			^ snds at: 1].
