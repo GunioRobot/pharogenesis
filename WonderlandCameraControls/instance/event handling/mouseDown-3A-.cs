904ec0141bbde0b6912d766bb2f7e71cd7ce3a3e
jsp <unknown>
1999-03-30 11:28:00 +0000

    no message

---
+++ WonderlandCameraControls/instance/event handling/mouseDown-3A-.cs
@@ -0,0 +1,6 @@
+mouseDown: evt
+	"Handle a mouse down event."
+
+	myUndoStack push: (UndoPOVChange for: myCamera from: (myCamera getPointOfView)).
+
+	moveAction _ myCamera doEachFrame: [ self moveCamera ].
