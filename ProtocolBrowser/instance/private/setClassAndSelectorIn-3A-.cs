a9031a1752522d9c83da283a681748633c395496
di <unknown>
1999-03-31 14:33:00 +0000

    no message

--- ProtocolBrowser/instance/private/setClassAndSelectorIn-3A-.cs
+++ ProtocolBrowser/instance/private/setClassAndSelectorIn-3A-.cs
@@ -1,3 +1,10 @@
 setClassAndSelectorIn: csBlock
-	"Decode strings of the form <selectorName>   (<className> [class])  "
-	^ self parse: self selection toClassAndSelector: csBlock
+	"Decode strings of the form    <selectorName> (<className> [class])  "
+
+	| i classAndSelString sel |
+	sel _ self selection asString.
+	i _ sel indexOf: $(.
+	"Rearrange to  <className> [class] <selectorName> , and use MessageSet"
+	classAndSelString _ (sel copyFrom: i + 1 to: sel size - 1) , ' ' ,
+					(sel copyFrom: 1 to: i - 1) withoutTrailingBlanks.
+	MessageSet parse: classAndSelString toClassAndSelector: csBlock
