e7042fce54fcbb659d0828b2ddf0dcf366e6f09b
di <unknown>
1999-04-03 16:35:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveAsFloat.cs
+++ Interpreter/instance/float primitives/primitiveAsFloat.cs
@@ -2,5 +2,6 @@ primitiveAsFloat
 	| arg |
 	arg _ self popInteger.
 	successFlag
-		ifTrue: [ self pushFloat: (self cCode: '((double) arg)') ]
+		ifTrue: [ self pushFloat: (self cCode: '((double) arg)'
+									inSmalltalk: [arg asFloat]) ]
 		ifFalse: [ self unPop: 1 ].
