7c35c8ed9900d90320c4205592382db828fddfc8
di <unknown>
1999-04-03 21:24:00 +0000

    no message

--- Interpreter/instance/float primitives/popFloat.cs
+++ Interpreter/instance/float primitives/popFloat.cs
@@ -6,6 +6,7 @@ popFloat
 	self var: #result declareC: 'double result'.
 	top _ self popStack.
 	self assertClassOf: top is: (self splObj: ClassFloat).
-	successFlag
-		ifTrue: [self fetchFloatAt: top + BaseHeaderSize into: result].
+	successFlag ifTrue:
+		[self cCode: '' inSmalltalk: [result _ Float new: 2].
+		self fetchFloatAt: top + BaseHeaderSize into: result].
 	^ result
