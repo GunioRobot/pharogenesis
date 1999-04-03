621820b6c5507379704211409aeae5ac89ea918c
di <unknown>
1999-04-03 16:52:00 +0000

    no message

--- Interpreter/instance/float primitives/pushFloat-3A-.cs
+++ Interpreter/instance/float primitives/pushFloat-3A-.cs
@@ -1,7 +1,4 @@
 pushFloat: f
 
-	| newFloatObj |
 	self var: #f declareC: 'double f'.
-	newFloatObj _ self instantiateSmallClass: (self splObj: ClassFloat) sizeInBytes: 12 fill: 0.
-	self storeFloatAt: newFloatObj + BaseHeaderSize from: f.
-	self push: newFloatObj.
+	self push: (self floatObjectOf: f).
