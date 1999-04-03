bd26969359bb1b208bc4ff33fb87712fc059b10f
di <unknown>
1999-04-03 00:59:00 +0000

    no message

--- HaloMorph/instance/private/doScale-3A-with-3A-.cs
+++ HaloMorph/instance/private/doScale-3A-with-3A-.cs
@@ -1,12 +1,12 @@
 doScale: evt with: scaleHandle
 	"Update the scale of my target if it is scalable."
-
-	| newExtent |
-	newExtent _ (evt cursorPoint - positionOffset) - target topLeft.
-	newExtent _ (newExtent max: minExtent) min: Display extent.
-	target scaleToFit: newExtent.
+	| newExtent newHandlePos |
+	newHandlePos _ evt cursorPoint - (scaleHandle extent // 2).
+	newExtent _ (target pointFromWorld: newHandlePos) - target center * 2.
+	evt shiftPressed ifTrue: [newExtent _ (newExtent x max: newExtent y) asPoint].
+	target scaleToFit: ((newExtent max: minExtent) min: Display extent).
 	target scale = 1.0
 		ifTrue: [scaleHandle color: Color yellow]
 		ifFalse: [scaleHandle color: Color orange].
-	scaleHandle position: evt cursorPoint - (scaleHandle extent // 2).
+	scaleHandle position: newHandlePos.
 	self layoutChanged.
