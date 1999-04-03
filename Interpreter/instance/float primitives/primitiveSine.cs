44d7a321f66d2f3b86818a4b9ead4fd3d37c1b9c
di <unknown>
1999-04-03 16:45:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveSine.cs
+++ Interpreter/instance/float primitives/primitiveSine.cs
@@ -4,5 +4,6 @@ primitiveSine
 	self var: #rcvr declareC: 'double rcvr'.
 	rcvr _ self popFloat.
 	successFlag
-		ifTrue: [self pushFloat: (self cCode: 'sin(rcvr)')]
+		ifTrue: [self pushFloat: (self cCode: 'sin(rcvr)'
+									inSmalltalk: [rcvr sin])]
 		ifFalse: [self unPop: 1]
