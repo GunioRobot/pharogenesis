dd3e5eb8afee79952a7f948a9719e75de32b8f95
tk <unknown>
1999-04-04 17:42:00 +0000

    no message

---
+++ StringMorph/instance/object fileIn/convertbosfcefch0-3A-bosfcefech0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcefch0: varDict bosfcefech0: smartRefStrm
+	"These variables are automatically stored into the new instance ('font' 'contents' 'hasFocus' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"Be sure to to fill in ('emphasis' ) and deal with the information in ()"
