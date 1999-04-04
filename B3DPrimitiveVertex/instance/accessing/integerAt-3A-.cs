1e232ba50f67512e8515bcdcd29f0aab11eec119
ar <unknown>
1999-04-04 00:29:00 +0000

    no message

---
+++ B3DPrimitiveVertex/instance/accessing/integerAt-3A-.cs
@@ -0,0 +1,8 @@
+integerAt: index
+	| word |
+	word _ self basicAt: index.
+	word < 16r3FFFFFFF ifTrue:[^word]. "Avoid LargeInteger computations"
+	^word >= 16r80000000	"Negative?!"
+		ifTrue:["word - 16r100000000"
+				(word bitInvert32 + 1) negated]
+		ifFalse:[word]
