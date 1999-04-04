734599a11542c17624a9752a7e2ac96887a43a19
ar <unknown>
1999-04-04 00:29:00 +0000

    no message

---
+++ B3DPrimitiveVertex/instance/accessing/integerAt-3A-put-3A-.cs
@@ -0,0 +1,8 @@
+integerAt: index put: anInteger
+	| word |
+	anInteger < 0
+		ifTrue:["word _ 16r100000000 + anInteger"
+				word _ (anInteger + 1) negated bitInvert32]
+		ifFalse:[word _ anInteger].
+	self  basicAt: index put: word.
+	^anInteger
