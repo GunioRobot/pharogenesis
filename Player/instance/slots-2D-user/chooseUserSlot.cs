d05d30842baaa747cad924c49f5e52ab4da89634
sw <unknown>
1999-04-05 13:55:00 +0000

    no message

--- Player/instance/slots-2D-user/chooseUserSlot.cs
+++ Player/instance/slots-2D-user/chooseUserSlot.cs
@@ -4,5 +4,5 @@ chooseUserSlot
 		ifTrue: [^ names first].
 	aMenu _ SelectionMenu selections: names.
 	result _ aMenu startUpWithCaption: 'Please choose a variable'.
-	result size == 0 ifTrue: [^ nil].
+	result isEmptyOrNil ifTrue: [^ nil].
 	^ result
