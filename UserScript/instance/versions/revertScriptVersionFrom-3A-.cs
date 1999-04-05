3a2972509b29c40621155da1d2dfd598fea36ff2
sw <unknown>
1999-04-05 13:50:00 +0000

    no message

--- UserScript/instance/versions/revertScriptVersionFrom-3A-.cs
+++ UserScript/instance/versions/revertScriptVersionFrom-3A-.cs
@@ -1,11 +1,12 @@
 revertScriptVersionFrom: anEditor
-	| aMenu result aPosition |
-	formerScriptEditors size == 0 ifTrue: [^ self beep].
+	| aMenu result aPosition oldOwner |
+	formerScriptEditors isEmptyOrNil ifTrue: [^ self beep].
 	aMenu _ SelectionMenu labelList: (formerScriptEditors collect: [:e | e timeStamp])
 		selections: formerScriptEditors.
 	result _ aMenu startUp.
 	result ifNotNil:
 		[aPosition _ anEditor position.
+		oldOwner _ anEditor topRendererOrSelf owner.
 		anEditor delete.
 		currentScriptEditor _ result bringUpToDate install.
-		player costume viewAfreshShowingScript: selector at: aPosition]
+		player costume viewAfreshIn: oldOwner showingScript: selector at: aPosition]
