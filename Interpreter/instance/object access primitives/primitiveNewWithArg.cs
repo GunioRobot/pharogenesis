62a8a68ba2983b087adbdf7ac9fc5acbf9e681a9
ar <unknown>
1999-04-06 13:08:00 +0000

    no message

--- Interpreter/instance/object access primitives/primitiveNewWithArg.cs
+++ Interpreter/instance/object access primitives/primitiveNewWithArg.cs
@@ -2,13 +2,14 @@ primitiveNewWithArg
 	"Allocate a new indexable instance. Fail if the allocation would leave less than lowSpaceThreshold bytes free."
 
 	| size class spaceOkay |
-	size _ self popInteger.
-	class _ self popStack.
+	size _ self positive32BitValueOf: (self stackValue: 0).
+	class _ self stackValue: 1.
 	self success: size >= 0.
 	successFlag ifTrue: [
+		"The following may cause GC!"
 		spaceOkay _ self sufficientSpaceToInstantiate: class indexableSize: size.
 		self success: spaceOkay.
+		class _ self stackValue: 1. "Reload class in case of GC"
 	].
 	successFlag
-		ifTrue: [ self push: (self instantiateClass: class indexableSize: size) ]
-		ifFalse: [ self unPop: 2 ].
+		ifTrue: [ self pop: 2 thenPush: (self instantiateClass: class indexableSize: size) ]
