aa75852963d3fa002242f5c9fd1e2163b7a6156d
sw <unknown>
1999-04-07 17:45:00 +0000

    no message

--- ChangeSet/instance/fileIn-2F-Out/assurePreambleExists.cs
+++ ChangeSet/instance/fileIn-2F-Out/assurePreambleExists.cs
@@ -1,6 +1,5 @@
 assurePreambleExists
-	"Make sure there is a StringHolder holding the preamble.  
-	 : if it's found to have reverted to empty contents, put up the template"
+	"Make sure there is a StringHolder holding the preamble; if it's found to have reverted to empty contents, put up the template"
 
-	(preamble == nil or: [preamble contents size == 0])
+	(preamble == nil or: [preamble contents isEmptyOrNil])
 		ifTrue: [preamble _ StringHolder new contents: self preambleTemplate]
