2eb150ae6072b5e0a7ccddca0985c749d8eaa1f2
ar <unknown>
1999-04-08 18:30:00 +0000

    no message

---
+++ B3DScanner/class/class initialization/initialize.cs
@@ -0,0 +1,8 @@
+initialize
+	"B3DScanner initialize"
+	FlagContinueLeftEdge _ 1.
+	FlagContinueRightEdge _ 2.
+	FlagEdgeLeftMajor _ 4.
+	FlagEdgeRightMajor _ 8.
+	FlagFaceActive _ 1.
+	FlagFaceInitialized _ 2.
