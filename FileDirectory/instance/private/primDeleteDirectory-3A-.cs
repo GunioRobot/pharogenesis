57e086a78cbba3906a40a59a281dfa3d89630387
jm <unknown>
1999-04-09 17:59:00 +0000

    no message

---
+++ FileDirectory/instance/private/primDeleteDirectory-3A-.cs
@@ -0,0 +1,5 @@
+primDeleteDirectory: fullPath
+	"Delete the directory named by the given path. Fail if the path is bad or if a directory by that name does not exist."
+
+ 	<primitive: 163>
+	self primitiveFailed
