29ec2a4b7bec43dafb260110cba7d4d9610ecfb1
tk <unknown>
1999-04-09 10:22:00 +0000

    no message

---
+++ Dictionary/instance/accessing/associationDeclareAt-3A-.cs
@@ -0,0 +1,12 @@
+associationDeclareAt: aKey
+	"Return an existing association, or create and return a new one.  Needed as a single message by ImageSegment.prepareToBeSaved."
+
+	| existing |
+	^ self associationAt: aKey ifAbsent: [
+		(Undeclared includesKey: aKey)
+			ifTrue: 
+				[existing _ Undeclared associationAt: aKey.
+				Undeclared removeKey: aKey.
+				self add: existing]
+			ifFalse: 
+				[self add: aKey -> false]]
