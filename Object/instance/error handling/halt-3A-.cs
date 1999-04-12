d8f10e115dc2177279c32b6ec243ae4d5a4b5552
tfei <unknown>
1999-04-12 12:59:00 +0000

    no message

--- Object/instance/error handling/halt-3A-.cs
+++ Object/instance/error handling/halt-3A-.cs
@@ -2,10 +2,5 @@ halt: aString
 	"This is the typical message to use for inserting breakpoints during 
 	debugging. It creates and schedules a Notifier with the argument, 
 	aString, as the label."
-
-	Debugger
-		openContext: thisContext
-		label: aString
-		contents: thisContext shortStack
-
-	"nil halt: 'Test of halt:.'."
+	
+	Halt new signal: aString
