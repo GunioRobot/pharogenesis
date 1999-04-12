4c32da0c6c0ec3360ff28a7cb7a33a11b52089ce
di <unknown>
1999-04-12 11:38:00 +0000

    no message

--- Interpreter/instance/control primitives/primitivePerform.cs
+++ Interpreter/instance/control primitives/primitivePerform.cs
@@ -1,22 +1,36 @@
 primitivePerform
-	| performSelector newReceiver selectorIndex |
+	| performSelector newReceiver selectorIndex lookupClass performMethod |
 	performSelector _ messageSelector.
+	performMethod _ newMethod.
 	messageSelector _ self stackValue: argumentCount - 1.
 	newReceiver _ self stackValue: argumentCount.
 	"NOTE: the following lookup may fail and be converted to #doesNotUnderstand:,
-		so we must adjust argument count now, so that would work."
+		so we must adjust argumentCount and slide args now, so that would work."
+
+	"Slide arguments down over selector"
 	argumentCount _ argumentCount - 1.
-	self lookupMethodInClass: (self fetchClassOf: newReceiver).
-	self success: (self argumentCountOf: newMethod) = argumentCount.
+	selectorIndex _ self stackPointerIndex - argumentCount.
+	self transfer: argumentCount
+		fromIndex: selectorIndex + 1
+		ofObject: activeContext
+		toIndex: selectorIndex
+		ofObject: activeContext.
+	self pop: 1.
+
+	lookupClass _ self fetchClassOf: newReceiver.
+	self findNewMethodInClass: lookupClass.
+	self success: ((self argumentCountOf: newMethod) = argumentCount).
 	successFlag
-		ifTrue: [selectorIndex _ self stackPointerIndex - argumentCount.
-				self transfer: argumentCount
-					fromIndex: selectorIndex + 1
-					ofObject: activeContext
-					toIndex: selectorIndex
-					ofObject: activeContext.
-				self pop: 1.
-				self executeNewMethod.  "Recursive xeq affects successFlag"
+		ifTrue: [self executeNewMethod.  "Recursive xeq affects successFlag"
 				successFlag _ true]
-		ifFalse: [argumentCount _ argumentCount + 1.
+		ifFalse: ["Slide the args back up (sigh) and re-insert the selector."
+				1 to: argumentCount do:
+					[:i |
+					self storePointer: argumentCount-i+1+selectorIndex ofObject: activeContext
+						withValue:
+						(self fetchPointer: argumentCount-i+selectorIndex ofObject: activeContext)].
+				self unPop: 1.
+				self storePointer: selectorIndex ofObject: activeContext withValue: messageSelector.
+				argumentCount _ argumentCount + 1.
+				newMethod _ performMethod.
 				messageSelector _ performSelector]
