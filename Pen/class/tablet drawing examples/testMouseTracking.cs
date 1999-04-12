db71e9a9bd35161799404b192232971d9ef65b7a
jm <unknown>
1999-04-12 12:51:00 +0000

    no message

---
+++ Pen/class/tablet drawing examples/testMouseTracking.cs
@@ -0,0 +1,17 @@
+testMouseTracking
+	"A very simple example of drawing using the mouse. Compare the tracking speed of this example with that of testTabletTracking. Mouse down to draw a stroke, shift-mouse to exit." 
+	"Pen testMouseTracking"
+
+	| pen p |
+	pen _ Pen newOnForm: Display.
+	pen roundNib: 8.
+	pen color: Color black.
+	Display fillColor: Color white.
+	Display restoreAfter: [
+		[Sensor shiftPressed and: [Sensor anyButtonPressed]] whileFalse: [
+			p _ Sensor cursorPoint.
+		     Sensor anyButtonPressed
+				ifTrue: [pen goto: p]
+				ifFalse: [
+					pen color: Color random.
+					pen place: p]]].
