8e04a07b9cd32d533bfdfb4a9f62ca6629520670
tfei <unknown>
1999-04-12 12:55:00 +0000

    no message

--- Object/instance/error handling/error-3A-.cs
+++ Object/instance/error handling/error-3A-.cs
@@ -1,17 +1,4 @@
 error: aString 
-	"The default behavior for error: is the same as halt:. The code is 
-	replicated in order to avoid showing an extra level of message sending 
-	in the Debugger. This additional message is the one a subclass should 
-	override in order to change the error handling behavior."
+	"Throw a generic Error exception."
 
-	| handler |
-	(handler _ Processor activeProcess errorHandler) notNil
-		ifTrue:
-			[handler  value: aString value: self]
-		ifFalse: 
-			[Debugger
-				openContext: thisContext
-				label: aString
-				contents: thisContext shortStack]
-
-	"nil error: 'error message'."
+	^Error new signal: aString
