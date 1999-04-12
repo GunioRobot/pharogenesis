012407a9a409254eb349ca170ee868d6a6441dac
tfei <unknown>
1999-04-12 10:20:00 +0000

    no message

--- Object/instance/error handling/doesNotUnderstand-3A-.cs
+++ Object/instance/error handling/doesNotUnderstand-3A-.cs
@@ -1,15 +1,9 @@
 doesNotUnderstand: aMessage 
 	 "Handle the fact that there was an attempt to send the given message to the receiver but the receiver does not understand this message (typically sent from the machine when a message is sent to the receiver and no method is defined for that selector)."
-	"Unless the receiver has an error handler defined for the active process, report to the user that the receiver does not understand the argument, aMessage, as a message."
 	"Testing: (3 activeProcess)"
 
-	| handler errorString |
 	(Preferences autoAccessors and: [self tryToDefineVariableAccess: aMessage])
 		ifTrue: [^ aMessage sentTo: self].
-	errorString _ 'Message not understood: ', aMessage selector.
-	(handler _ Processor activeProcess errorHandler) notNil
-		ifTrue: [handler value: errorString value: self]
-		ifFalse: [Debugger openContext: thisContext
-					label: errorString
-					contents: thisContext shortStack].
-	^ aMessage sentTo: self
+	MessageNotUnderstood new message: aMessage;
+		signal.
+	^aMessage sentTo: self
