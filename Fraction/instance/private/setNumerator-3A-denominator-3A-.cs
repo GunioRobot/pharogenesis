4f3e5f20e54aaff7b8fbfb0cd3a941dbdd239210
tfei <unknown>
1999-04-12 12:45:00 +0000

    no message

--- Fraction/instance/private/setNumerator-3A-denominator-3A-.cs
+++ Fraction/instance/private/setNumerator-3A-denominator-3A-.cs
@@ -1,7 +1,7 @@
 setNumerator: n denominator: d
 
 	d = 0
-		ifTrue: [self error: 'denominator cannot be zero']
+		ifTrue: [^(ZeroDivide dividend: n) signal]
 		ifFalse: 
 			[numerator _ n asInteger.
 			denominator _ d asInteger abs. "keep sign in numerator"
