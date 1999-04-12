c70e65979b95c5ef1e429e6df7d2cc826a040745
jsp <unknown>
1999-04-12 14:56:00 +0000

    no message

---
+++ WonderlandMorph/instance/primitive behaviors/move-3A-distance-3A-duration-3A-.cs
@@ -0,0 +1,4 @@
+move: aDirection distance: aDistance duration: aDuration
+	"Move the morph the specified distance in the specified direction"
+
+	^ self move: aDirection distance: aDistance duration: aDuration style: gently.
