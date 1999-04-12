7412d3a782bd716bb7d0822c48d31cc1bd0f2f0a
tfei <unknown>
1999-04-12 12:45:00 +0000

    no message

--- Float/instance/arithmetic/-2F-.cs
+++ Float/instance/arithmetic/-2F-.cs
@@ -4,6 +4,5 @@
 	whatIsAPrimitive."
 
 	<primitive: 50>
-	aNumber = 0
-		ifTrue: [self error: 'attempt to divide by zero']
-		ifFalse: [^ (aNumber adaptFloat: self) / aNumber adaptToFloat]
+	aNumber = 0 ifTrue: [^(ZeroDivide dividend: self) signal].
+	^ aNumber adaptToFloat: self andSend: #/
