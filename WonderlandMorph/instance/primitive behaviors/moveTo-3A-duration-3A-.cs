ec943a1d1659c1a96993ab5f33ab39f5d5c2f205
jsp <unknown>
1999-04-12 14:57:00 +0000

    no message

---
+++ WonderlandMorph/instance/primitive behaviors/moveTo-3A-duration-3A-.cs
@@ -0,0 +1,4 @@
+moveTo: aPoint duration: aDuration
+	"Moves the object to the specified position in its parent's coordinate system using the specified style over the specified duration."
+
+	^ self moveTo: aPoint duration: aDuration style: gently.
