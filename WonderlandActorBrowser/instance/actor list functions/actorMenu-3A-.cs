e42dd366e8c2f3f18f9fb1ddd1227b210e895a4a
jsp <unknown>
1999-04-13 12:13:00 +0000

    no message

---
+++ WonderlandActorBrowser/instance/actor list functions/actorMenu-3A-.cs
@@ -0,0 +1,32 @@
+actorMenu: aMenu
+	"Builds the menu to display when the user right clicks on an actor"
+
+	selectedActor ifNil: [ ^ aMenu ].
+
+	(selectedActor isFirstClass)
+		ifTrue: [
+			^ aMenu addList: {
+								{'Point camera at'. #pointAt}.
+								{'Turn around once'. #turnAround}.
+								{'Become part'. #becomePart }.
+								{'Stand up'. #standUp}.
+								{'Grow'. #grow}.
+								{'Shrink'. #shrink}.
+								{'Squash'. #squash}.
+								{'Stretch'. #stretch}.
+								{'Destroy'. #destroy}
+							  }.
+				]
+		ifFalse: [
+			^ aMenu addList: {
+								{'Point camera at'. #pointAt}.
+								{'Turn around once'. #turnAround}.
+								{'Become first class'. #becomeFirstClass }.
+								{'Stand up'. #standUp}.
+								{'Grow'. #grow}.
+								{'Shrink'. #shrink}.
+								{'Squash'. #squash}.
+								{'Stretch'. #stretch}.
+								{'Destroy'. #destroy}
+							  }.
+				].
