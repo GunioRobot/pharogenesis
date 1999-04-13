f9849ff447209d4267d99d7b376905de1b43f096
di <unknown>
1999-04-13 13:53:00 +0000

    no message

--- AbstractSound/class/examples/testFMInteractively.cs
+++ AbstractSound/class/examples/testFMInteractively.cs
@@ -2,7 +2,7 @@ testFMInteractively
 	"Experiment with different settings of the FM modulation and multiplier settings interactively by moving the mouse. The top-left corner of the screen is 0 for both parameters. Stop when the mouse is pressed."
 	"AbstractSound testFMInteractively"
 
-	| s mousePt lastVal status mod mult |
+	| s mousePt lastVal status mod ratio |
 	SoundPlayer startPlayerProcessBufferSize: 1100 rate: 11025 stereo: false.
 	s _ FMSound pitch: 440.0 dur: 200.0 loudness: 0.2.
 
@@ -12,12 +12,12 @@ testFMInteractively
 		mousePt _ Sensor cursorPoint.
 		mousePt ~= lastVal ifTrue: [
 			mod _ mousePt x asFloat / 20.0.
-			mult _ mousePt y asFloat / 20.0.
-			s modulation: mod multiplier: mult.
+			ratio _ mousePt y asFloat / 20.0.
+			s modulation: mod ratio: ratio.
 			lastVal _ mousePt.
 			status _
 'mod: ', mod printString, '
-mult: ', mult printString.
-			status asParagraph displayOn: Display at: 10@10]].
+ratio: ', ratio printString.
+			status displayOn: Display at: 10@10]].
 
 	SoundPlayer shutDown.
