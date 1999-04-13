a379481db54c64d66d2222bb87445e019d26650b
di <unknown>
1999-04-13 17:51:00 +0000

    no message

--- PopUpMenu/instance/private/menuForm.cs
+++ PopUpMenu/instance/private/menuForm.cs
@@ -1,8 +1,5 @@
 menuForm
 	"Answer a Form to be displayed for this menu."
-	"Details: On slower systems, cache the menu Form for speed."
 
-	form == nil ifFalse: [^ form].
-	CacheMenuForms
-		ifTrue: [^ form _ self computeForm]
-		ifFalse: [^ self computeForm]
+	form == nil ifTrue: [self computeForm].
+	^ form
