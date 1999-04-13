8ae33b03e2078af05c8fc811426776c2170f9c8b
di <unknown>
1999-04-13 17:42:00 +0000

    no message

--- PopUpMenu/instance/marker adjustment/manageMarker.cs
+++ PopUpMenu/instance/marker adjustment/manageMarker.cs
@@ -7,8 +7,6 @@ manageMarker
 	(frame inside containsPoint: pt)
 		ifTrue: ["Need to cache the form for reasonable scrolling performance"
 				((Display boundingBox insetBy: 0@3) containsPoint: pt)
-					ifTrue: [CacheMenuForms ifFalse: [form _ nil]]
-					ifFalse: [form == nil ifTrue: [form _ self computeForm].
-							pt _ pt - (self scrollIntoView: pt)].
+					ifFalse: [pt _ pt - (self scrollIntoView: pt)].
 				self markerOn: pt]
 		ifFalse: [self markerOff]
