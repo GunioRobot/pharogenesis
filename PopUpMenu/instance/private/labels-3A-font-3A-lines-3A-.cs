c6d22eabc5f2bb0ee027e1124223d43eaa9574ff
di <unknown>
1999-04-13 16:21:00 +0000

    no message

--- PopUpMenu/instance/private/labels-3A-font-3A-lines-3A-.cs
+++ PopUpMenu/instance/private/labels-3A-font-3A-lines-3A-.cs
@@ -1,12 +1,5 @@
 labels: aString font: aFont lines: anArray
 
-	| style inside |
 	labelString _ aString.
 	font _ aFont.
 	lineArray _ anArray.
-	frame _ Quadrangle new.
-	frame region: self menuForm boundingBox.
-	frame borderWidth: 2.
-	inside _ frame inside.
-	marker _ inside topLeft extent: (inside width @ self computeLabelParagraph lineGrid).
-	selection _ 1.
