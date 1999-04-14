c699f536175b83ccc54af756e5df74040473ec9b
di <unknown>
1999-04-14 21:34:00 +0000

    no message

--- Project/class/class initialization/initialize.cs
+++ Project/class/class initialization/initialize.cs
@@ -1,6 +1,8 @@
 initialize
 	"This is the Top Project."   
 
-	CurrentProject _ super new initialProject
+	CurrentProject ifNil:
+		[CurrentProject _ super new initialProject.
+		CurrentProject spawnNewProcessAndTerminateOld: true].
 
 	"Project initialize"
