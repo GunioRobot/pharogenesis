df7dc6c26766dc2548466778ca017fea234b24af
ar <unknown>
1999-04-14 05:12:00 +0000

    no message

---
+++ B3DPrimitiveRasterizer/instance/primitives/primAddObject-3A-primitive-3A-vertexArray-3A-size-3A-indexArray-3A-size-3A-viewport-3A-textureIndex-3A-.cs
@@ -0,0 +1,3 @@
+primAddObject: obj primitive: primitive vertexArray: vertexArray size: vertexCount indexArray: indexArray size: indexCount viewport: vp textureIndex: txIndex
+	<primitive:'b3dInitPrimitiveObject' module:'Squeak3D'>
+	^self primitiveFailed
