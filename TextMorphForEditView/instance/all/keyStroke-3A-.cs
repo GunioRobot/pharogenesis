114cd5db49754080c14bb89b0eece5fd07de7a37
bf <unknown>
1999-04-14 12:39:00 +0000

    no message

--- TextMorphForEditView/instance/all/keyStroke-3A-.cs
+++ TextMorphForEditView/instance/all/keyStroke-3A-.cs
@@ -1,4 +1,9 @@
 keyStroke: evt
+	| view |
+	(editView scrollByKeyboard: evt) ifTrue: [^self].
 	self editor model: editView model.  "For evaluateSelection"
+	view _ editView.  "Copy into temp for case of a self-mutating doit"
+	(acceptOnCR and: [evt keyCharacter = Character cr])
+		ifTrue: [^ self editor accept].
 	super keyStroke: evt.
-	editView scrollSelectionIntoView
+	view scrollSelectionIntoView
