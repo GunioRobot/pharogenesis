50fb424cd308f321cc8b92d76e51bd70022d6e5c
ar <unknown>
1999-04-14 05:18:00 +0000

    no message

---
+++ B3DPrimitiveRasterizer/instance/primitives/primStartRasterizer-3A-objects-3A-textures-3A-.cs
@@ -0,0 +1,4 @@
+primStartRasterizer: primState objects: primitiveObjects textures: textureArray
+	"Primitive. Start the rasterizer. Return an error code."
+	<primitive:'b3dStartRasterizer' module:'Squeak3D'>
+	^self primitiveFailed
