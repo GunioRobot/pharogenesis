b2d7984ef94adb804a4cb85a2ae2641ac03968ff
ar <unknown>
1999-04-18 02:07:00 +0000

    no message

---
+++ B3DPrimitiveClipper/instance/private/primNextClippedTriangleAfter-3A-vertices-3A-count-3A-indexes-3A-count-3A-.cs
@@ -0,0 +1,3 @@
+primNextClippedTriangleAfter: firstIndex vertices: vtxArray count: vtxCount indexes: idxArray count: idxCount
+	<primitive: 'b3dPrimitiveNextClippedTriangle' module:'Squeak3D'>
+	^super primNextClippedTriangleAfter: firstIndex vertices: vtxArray count: vtxCount indexes: idxArray count: idxCount
