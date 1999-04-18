7e94dab2973ec9719f975bb065c52008ed39fa81
ar <unknown>
1999-04-18 02:08:00 +0000

    no message

---
+++ B3DPrimitiveClipper/instance/clipping polygons/clipPolygon-3A-count-3A-with-3A-mask-3A-.cs
@@ -0,0 +1,3 @@
+clipPolygon: vtxArray count: vtxCount with: tempVtxArray mask: outMask
+	<primitive:'b3dClipPolygon' module:'Squeak3D'>
+	^super clipPolygon: vtxArray count: vtxCount with: tempVtxArray mask: outMask
