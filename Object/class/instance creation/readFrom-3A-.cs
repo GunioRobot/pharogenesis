64a2f681fe5d6fd841278232318a961351f4248d
tk <unknown>
1999-04-18 07:05:00 +0000

    no message

--- Object/class/instance creation/readFrom-3A-.cs
+++ Object/class/instance creation/readFrom-3A-.cs
@@ -1,7 +1,9 @@
 readFrom: aStream
 	"Create an object based on the contents of aStream."
 
-	| object |
+	| object ok |
+	ok _ (aStream isKindOf: Stream) or: [aStream isKindOf: String].
+	(ok or: [aStream isKindOf: Text]) ifFalse: [^ self error: 'expected String or Text'].
 	object _ Compiler evaluate: aStream.
 	(object isKindOf: self) ifFalse: [self error: self name, ' expected'].
 	^object
