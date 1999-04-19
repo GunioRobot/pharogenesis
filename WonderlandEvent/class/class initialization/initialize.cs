4b107ff62facde6dddc291a89bab604cef20df32
jsp <unknown>
1999-04-19 17:47:00 +0000

    no message

---
+++ WonderlandEvent/class/class initialization/initialize.cs
@@ -0,0 +1,13 @@
+initialize
+	"Add some new constants to the WonderlandConstants shared pool"
+
+	WonderlandConstants at: 'leftMouseDown' put: 'leftMouseDown'.
+	WonderlandConstants at: 'leftMouseUp' put: 'leftMouseUp'.
+	WonderlandConstants at: 'rightMouseDown' put: 'rightMouseDown'.
+	WonderlandConstants at: 'rightMouseUp' put: 'rightMouseUp'.
+	WonderlandConstants at: 'leftMouseClick' put: 'leftMouseUp'.
+	WonderlandConstants at: 'rightMouseClick' put: 'rightMouseUp'.
+	WonderlandConstants at: 'mouseMove' put: 'mouseMove'.
+	WonderlandConstants at: 'keyPress' put: 'keyPress'.
+	
+ 
