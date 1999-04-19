7325f1cd2d2925eabacfdee2a10c9b949051018c
jsp <unknown>
1999-04-19 14:21:00 +0000

    no message

---
+++ WonderlandReaction/class/instance creation/new-3A-.cs
@@ -0,0 +1,4 @@
+new: aReaction
+	"Create a new WonderlandReaction instance.  This instance basically wraps a block that specifies what the given object should do"
+
+	^ self new initializeWith: aReaction.
