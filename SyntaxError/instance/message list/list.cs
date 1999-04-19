6dd663741947448a76df94144cdf72b038cb9079
tk <unknown>
1999-04-19 08:08:00 +0000

    no message

--- SyntaxError/instance/message list/list.cs
+++ SyntaxError/instance/message list/list.cs
@@ -1,4 +1,5 @@
 list
 	"Answer an array of one element made up of the class name, message category, and message selector in which the syntax error was found. This is the single item in the message list of a view/browser on the receiver."
 
+	selector ifNil: [^ Array with: (class name, '  ', category, '  ', '<none>')].
 	^ Array with: (class name, '  ', category, '  ', selector)
