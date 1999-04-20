5aa37d4305e52b4dcfbe053d41cecb6ccc27fc16
jsp <unknown>
1999-04-20 14:29:00 +0000

    no message

---
+++ WonderlandMorph/instance/primitive behaviors/place-3A-morph-3A-duration-3A-.cs
@@ -0,0 +1,4 @@
+place: aLocation morph: aMorph duration: aDuration
+	"Place this WonderlandMorph in the specified location relative to the specified morph over 1 second using the gently animation style."
+
+	^ self place: aLocation morph: aMorph duration: aDuration style: gently.
