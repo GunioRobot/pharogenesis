d526a5f0c1bee3b5ab62cb46e68d09946b301b16
di <unknown>
1999-04-20 12:03:00 +0000

    no message

---
+++ Wonderland/class/instance creation/new.cs
@@ -0,0 +1,11 @@
+new   "Wonderland new" 
+	"Create and initialize a new Wonderland."
+	B3DPrimitiveEngine isAvailable ifFalse: [
+	(self confirm: 'WARNING: This Squeak does not have real 3D support.
+Opening a Wonderland will EXTREMELY time consuming.
+Are you sure you want to do this?
+(NO is probably the right answer :-)') ifFalse: [^ self]].
+	Display depth < 8 ifTrue:
+		[(self confirm: 'The display depth should be set to at least 8 bit.
+Shall I do this now for you?') ifTrue: [Display newDepth: 8]].
+	^ super new initialize
