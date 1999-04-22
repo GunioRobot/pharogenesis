a17b0174a64aeddb69d1bbc563376f392d27f002
sw <unknown>
1999-04-22 14:13:00 +0000

    no message

--- TileMorph/instance/initialization/setLiteral-3A-.cs
+++ TileMorph/instance/initialization/setLiteral-3A-.cs
@@ -1,8 +1,11 @@
 setLiteral: anObject
 
-	| m |
+	| m already |
+	already _ submorphs detect: [:aSubMorph  | aSubMorph isKindOf: UpdatingStringMorph] ifNone: [nil].
+	already ifNotNil: [already delete].
 	type _ #literal.
-	m _ UpdatingStringMorph contents: ' ' font: ScriptingSystem fontForTiles.  "BUT this doesn't do it, damnit"
+	m _ UpdatingStringMorph contents: ' ' font: ScriptingSystem fontForTiles.
 	m target: self; getSelector: #literal; putSelector: #literal:.
+	(anObject isKindOf: String) ifTrue: [m useStringFormat].
 	self addMorph: m.
 	self literal: anObject.
