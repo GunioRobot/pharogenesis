f7dd966d3f74595cef4d5a842df7f07416294f42
jm <unknown>
1999-04-22 16:45:00 +0000

    no message

---
+++ SpeakerMorph/instance/speaker/appendSample-3A-.cs
@@ -0,0 +1,8 @@
+appendSample: aFloat
+	"Append the given sample, a number between -100.0 and 100.0, to my buffer. Flush the buffer if it is full."
+
+	lastConePosition _ aFloat.
+	lastConePosition > 100.0 ifTrue: [lastConePosition _ 100.0].
+	lastConePosition < -100.0 ifTrue: [lastConePosition _ -100.0].
+	buffer nextPut: (327.67 * lastConePosition) truncated.
+	buffer position >= bufferSize ifTrue: [self flushBuffer].
