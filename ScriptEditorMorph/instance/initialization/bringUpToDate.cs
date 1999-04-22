91aca5a9be3e724881f645578820beb3699c6657
sw <unknown>
1999-04-22 18:33:00 +0000

    no message

--- ScriptEditorMorph/instance/initialization/bringUpToDate.cs
+++ ScriptEditorMorph/instance/initialization/bringUpToDate.cs
@@ -2,6 +2,8 @@ bringUpToDate
 	"Make certain that the script name and the names of actors within are up to date"
 
 	| currentName titleMorph newName |
+	playerScripted ifNil: ["likely a naked test/yes/no fragment!" ^ self].
+
 	currentName _ playerScripted externalName.
 	titleMorph _ self findDeepSubmorphThat: [:m | m externalName = 'title'] ifAbsent: [nil].
 	titleMorph ifNotNil:
