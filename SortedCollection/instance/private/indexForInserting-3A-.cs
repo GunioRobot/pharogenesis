1e52942aeaebfe2c1852efa3f218f5463e9a7eea
stp <unknown>
1999-04-23 05:36:00 +0000

    no message

--- SortedCollection/instance/private/indexForInserting-3A-.cs
+++ SortedCollection/instance/private/indexForInserting-3A-.cs
@@ -3,10 +3,15 @@ indexForInserting: newObject
 	| index low high |
 	low _ firstIndex.
 	high _ lastIndex.
-	[index _ high + low // 2.
-	low > high]
-		whileFalse: 
-			[(sortBlock value: (array at: index) value: newObject)
-				ifTrue: [low _ index + 1]
-				ifFalse: [high _ index - 1]].
+	sortBlock isNil
+		ifTrue: [[index _ high + low // 2.  low > high]
+			whileFalse: 
+				[((array at: index) <= newObject)
+					ifTrue: [low _ index + 1]
+					ifFalse: [high _ index - 1]]]
+		ifFalse: [[index _ high + low // 2.  low > high]
+			whileFalse: 
+				[(sortBlock value: (array at: index) value: newObject)
+					ifTrue: [low _ index + 1]
+					ifFalse: [high _ index - 1]]].
 	^low
