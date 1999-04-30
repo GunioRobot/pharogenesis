abc2a33c0d2b384d34de705d19489aad7e075bb7
jsp <unknown>
1999-04-30 11:33:00 +0000

    no message

---
+++ WonderlandLightSource/instance/set property/setBrightness-3A-.cs
@@ -0,0 +1,5 @@
+setBrightness: aNumber
+	"Sets the brightness of the light between 0 and 1 by varying the color of the light."
+
+	"The parameter checks out, so run the animation"
+	self setBrightness: aNumber duration: 1.0.
