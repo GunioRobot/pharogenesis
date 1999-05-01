51ca0a90637b361a75fa62164a7b6be3254d6f56
ar <unknown>
1999-05-01 09:25:00 +0000

    no message

--- Debugger/class/opening/openInterrupt-3A-onProcess-3A-.cs
+++ Debugger/class/opening/openInterrupt-3A-onProcess-3A-.cs
@@ -1,14 +1,21 @@
 openInterrupt: aString onProcess: interruptedProcess
 	"Open a notifier in response to an interrupt. An interrupt occurs when the user types the interrupt key (cmd-. on Macs, ctrl-c or alt-. on other systems) or when the low-space watcher detects that memory is low."
-
 	| debugger |
+	<primitive: 19> "Simulation guard"
 	debugger _ self new.
 	debugger
 		process: interruptedProcess
-		controller: (ScheduledControllers activeControllerProcess == interruptedProcess
+		controller: ((Smalltalk isMorphic not
+					and: [ScheduledControllers activeControllerProcess == interruptedProcess])
 						ifTrue: [ScheduledControllers activeController])
 		context: interruptedProcess suspendedContext.
 	debugger externalInterrupt: true.
+
+Preferences logDebuggerStackToFile ifTrue:
+	[(aString includesSubString: 'Space') & 
+		(aString includesSubString: 'low') ifTrue: [
+			Smalltalk logError: aString inContext: debugger interruptedContext to:'LowSpaceDebug.log']].
+
 	^ debugger
 		openNotifierContents: debugger interruptedContext shortStack
 		label: aString
