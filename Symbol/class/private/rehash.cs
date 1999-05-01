da03a357b53cb712cb24c6f7c4735b0b18409ce7
ar <unknown>
1999-05-01 05:21:00 +0000

    no message

--- Symbol/class/private/rehash.cs
+++ Symbol/class/private/rehash.cs
@@ -1,18 +1,5 @@
 rehash		"Symbol rehash"
 	"Rebuild the hash table, reclaiming unreferenced Symbols."
-	| count oldCount |
-	SelectorTables _ (1 to: 6) collect: [ :i | (1 to: 26) collect: [ :j | Array new: 0 ] ].
-	OtherTable _ (1 to: 51) collect: [:k | Array new: 0].
-	oldCount _ Symbol instanceCount.
-	count _ 0.
-	'Rebuilding Symbol Tables...'
-		displayProgressAt: Sensor cursorPoint
-		from: 0 to: oldCount
-		during:
-			[:bar |
-			Smalltalk garbageCollect.
-			Symbol allInstancesDo:
-				[ :sym |
-				self intern: sym.
-				bar value: (count _ count + 1)]].
-	^ (oldCount - count) printString , ' reclaimed'
+	^SelectorTables first first class == (Smalltalk at: #WeakArray ifAbsent:[nil])
+		ifTrue:[self fastRehash]
+		ifFalse:[self slowRehash].
