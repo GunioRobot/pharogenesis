2cc5c7012e32529e914a30026256ed7f725d0e56
sw <unknown>
1999-05-03 22:40:00 +0000

    no message

---
+++ StandardScriptingSystem/instance/parts bin/prototypicalHolder.cs
@@ -0,0 +1,5 @@
+prototypicalHolder
+	| aHolder |
+	aHolder _ PasteUpMorph authoringPrototype color: Color orange muchLighter; borderColor: Color orange lighter.
+	aHolder setNameTo: 'holder'; extent: 160 @ 110.
+	^ aHolder behaveLikeHolder.
