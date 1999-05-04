bba050e1075e502d9d23a97cae3584b22b9019a8
di <unknown>
1999-05-04 13:52:00 +0000

    no message

--- SystemDictionary/instance/browsing/browseAllObjectReferencesTo-3A-except-3A-ifNone-3A-.cs
+++ SystemDictionary/instance/browsing/browseAllObjectReferencesTo-3A-except-3A-ifNone-3A-.cs
@@ -1,12 +1,10 @@
 browseAllObjectReferencesTo: anObject except: objectsToExclude ifNone: aBlock
-	"Bring up a list inspector on the objects that point to anObject. If there are none, then evaluate aBlock on anObject.  "
+	"Bring up a list inspector on the objects that point to anObject.
+	If there are none, then evaluate aBlock on anObject.  "
 
 	| aList shortName |
 	aList _ Smalltalk pointersTo: anObject except: objectsToExclude.
 	aList size > 0 ifFalse: [^ aBlock value: anObject].
-	shortName _ anObject name.
-	shortName size > 12 ifTrue: [
-		shortName _ (shortName truncateTo: 12), '...'.
-	].
+	shortName _ (anObject name ifNil: [anObject printString]) contractTo: 20.
 	OrderedCollectionInspector openOn: aList withEvalPane: false
 		withLabel: 'Objects pointing to ', shortName.
