cb45255fde27ca5957c6d6de249ad4b23ffbbb8d
stp <unknown>
1999-05-07 15:31:00 +0000

    no message

--- MessageTally/instance/initialize-2D-release/spyEvery-3A-on-3A-.cs
+++ MessageTally/instance/initialize-2D-release/spyEvery-3A-on-3A-.cs
@@ -1,16 +1,17 @@
 spyEvery: millisecs on: aBlock 
 	"Create a spy and spy on the given block at the specified rate."
 
-	| myDelay value startTime |
+	| myDelay value startTime time0 |
 	(aBlock isMemberOf: BlockContext)
 		ifFalse: [self error: 'spy needs a block here'].
 	self class: aBlock receiver class method: aBlock method.
 		"set up the probe"
 	ObservedProcess _ Processor activeProcess.
-	myDelay _ Delay forMilliseconds: millisecs.
-	Timer _
+	myDelay := Delay forMilliseconds: millisecs.
+	time0 := Time millisecondClockValue.
+	Timer :=
 		[[true] whileTrue: 
-			[startTime _ Time millisecondClockValue.
+			[startTime := Time millisecondClockValue.
 			myDelay wait.
 			self tally: ObservedProcess suspendedContext
 				"tally can be > 1 if ran a long primitive"
@@ -19,7 +20,8 @@ spyEvery: millisecs on: aBlock
 	Timer priority: Processor userInterruptPriority.
 		"activate the probe and evaluate the block"
 	Timer resume.
-	value _ aBlock value.
+	value := aBlock value.
 		"cancel the probe and return the value"
 	Timer terminate.
+	time := Time millisecondClockValue - time0.
 	^value
