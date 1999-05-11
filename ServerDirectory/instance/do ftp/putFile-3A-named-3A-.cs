61e610ee3f519da9b24ea71bfaa5cf37c4e2a496
tk <unknown>
1999-05-11 16:45:00 +0000

    no message

--- ServerDirectory/instance/do ftp/putFile-3A-named-3A-.cs
+++ ServerDirectory/instance/do ftp/putFile-3A-named-3A-.cs
@@ -9,15 +9,21 @@ putFile: fileStream named: fileNameOnServer
 
 	dd connectTo: so remoteAddress port: dd portNum.
 	dd waitForConnectionUntil: FTPSocket standardDeadline.
-	Transcript show: 'store via port ', dd portNum printString; cr.
+	Transcript show: 'storing file ', fileNameOnServer; cr.
+	"Transcript show: 'store via port ', dd portNum printString; cr."
 	dd sendData: fileStream contentsOfEntireFile.
 	dd close.
 
-	(rr _ so responseOK) == true ifFalse: [^ rr].	"150 Opening binary conn on foo (3113 bytes)"
-	(resp _ so responseOK) == true ifFalse: [^ rr].	"226 Transfer complete."
-	so sendCommand: 'QUIT'.
-	(rr _ so responseOK) == true ifFalse: [^ rr].	"221"
-	so destroy.	"Always OK to destroy"
+	(rr _ so responseOK) == true ifFalse: [
+		(rr beginsWith: '426 ') ifTrue: [socket _ nil.  ^ rr, ' (Server may be full.)'].
+			"Connection closed, transfer aborted"
+		socket _ nil.  ^ rr].	"150 Opening BINARY mode data connection"
+	(resp _ so responseOK) == true ifFalse: [
+		socket _ nil.  ^ rr].	"226 Transfer complete."
+	socket ifNil: [
+		so sendCommand: 'QUIT'.
+		(rr _ so responseOK) == true ifFalse: [^ rr].	"221"
+		so destroy].	"Always OK to destroy"
 	dd destroy.
 
 	^ resp	"226 Transfer complete."
