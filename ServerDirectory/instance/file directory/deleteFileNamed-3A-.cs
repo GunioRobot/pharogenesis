a0a19b24a83080d3083bc867b6ba063ddc2035f0
tk <unknown>
1999-05-11 16:43:00 +0000

    no message

--- ServerDirectory/instance/file directory/deleteFileNamed-3A-.cs
+++ ServerDirectory/instance/file directory/deleteFileNamed-3A-.cs
@@ -1,3 +1,20 @@
-deleteFileNamed: fileName
-
-	self inform: 'Sorry, not implemented yet.'.
+deleteFileNamed: fullName
+	"Detete a remote file.  fullName is directory path, and does include name of the server.  Or it can just be a fileName."
+	| file so rr |
+	file _ self as: ServerFile.
+	(fullName includes: self pathNameDelimiter)
+		ifTrue: [file fullPath: fullName]		"sets server, directory(path), fileName"
+		ifFalse: [file fileName: fullName].	"JUST a single NAME, rest is here"
+			"Mac files that include / in name, must encode it as %2F "
+	file type == #file ifTrue: [
+		^ (FileDirectory forFileName: (file fileNameRelativeTo: self)) 
+			deleteFileNamed: file fileName].
+	
+	so _ file openNoDataFTP.
+	so class == String ifTrue: ["error, was reported" ^ so].
+	so sendCommand: 'DELE ', file fileName.
+	(rr _ so responseOK) == true ifFalse: [socket _ nil.  ^ rr].	""
+	socket ifNil: [
+		so sendCommand: 'QUIT'.
+		(rr _ so responseOK) == true ifFalse: [^ rr].	"221"
+		so destroy].	"Always OK to destroy"
