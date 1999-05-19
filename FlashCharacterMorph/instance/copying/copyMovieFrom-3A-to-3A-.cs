70ce4843635f7b06d41306e2c928321e454828e1
ar <unknown>
1999-05-19 19:07:00 +0000

    no message

---
+++ FlashCharacterMorph/instance/copying/copyMovieFrom-3A-to-3A-.cs
@@ -0,0 +1,13 @@
+copyMovieFrom: firstFrame to: lastFrame
+	| copy newFrame |
+	copy _ super copyMovieFrom: firstFrame to: lastFrame.
+	copy reset.
+	copy visible: false atFrame: 0.
+	firstFrame to: lastFrame do:[:i|
+		newFrame _ i - firstFrame + 1.
+		copy visible: (self visibleAtFrame: i) atFrame: newFrame.
+		copy matrix: (self matrixAtFrame: i) atFrame: newFrame.
+		copy depth: (self depthAtFrame: i) atFrame: newFrame.
+		copy colorTransform: (self colorTransformAtFrame: i) atFrame: newFrame.
+	].
+	^copy
