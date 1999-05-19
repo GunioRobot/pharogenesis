35042b47402e91b6dde86a2ab189728d4ba7de04
ar <unknown>
1999-05-19 19:39:00 +0000

    no message

---
+++ FlashSorterMorph/instance/interaction/mouseStillDown-3A-onItem-3A-.cs
@@ -0,0 +1,28 @@
+mouseStillDown: evt onItem: aMorph
+	| pt index m yOffset |
+	submorphs do:[:mm|
+		mm == aMorph ifFalse:[mm isSelected: false]].
+	pt _ evt cursorPoint.
+	yOffset _ self offset y.
+	index _ aMorph frameNumber. "What a fake hack@!"
+	pt y - yOffset < 0 ifTrue:[
+		owner scrollBy: 0@owner scrollDeltaHeight.
+		evt hand eventTransform: (aMorph transformFrom: evt hand)].
+	pt y - yOffset > self extent y ifTrue:[
+		owner scrollBy: 0@owner scrollDeltaHeight negated.
+		evt hand eventTransform: (aMorph transformFrom: evt hand)].
+	(aMorph bounds containsPoint: pt) ifTrue:[^self].
+	(pt y > aMorph bottom or:[pt x > aMorph right]) ifTrue:[
+		"Select all morphs forward."
+		index+1 to: submorphs size do:[:i|
+			m _ submorphs at: i.
+			m isSelected: aMorph isSelected.
+			(m bounds containsPoint: pt) ifTrue:[^self]. "Done"
+		].
+		^self].
+	"Select morphs backwards"
+	index-1 to: 1 by: -1 do:[:i|
+		m _ submorphs at: i.
+		m isSelected: aMorph isSelected.
+		(m bounds containsPoint: pt) ifTrue:[^self].
+	].
