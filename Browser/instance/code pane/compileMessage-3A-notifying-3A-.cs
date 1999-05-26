6e67fb97e6d53b93305f8939116b36b3f41d9a40
sw <unknown>
1999-05-26 23:43:00 +0000

    no message

---
+++ Browser/instance/code pane/compileMessage-3A-notifying-3A-.cs
@@ -0,0 +1,37 @@
+compileMessage: aText notifying: aController
+	"Compile the code that was accepted by the user, placing the compiled method into an appropriate message category.  Return true if the compilation succeeded, else false."
+
+	| fallBackCategoryIndex fallBackMethodIndex originalSelectorName result |
+	self selectedMessageCategoryName asSymbol = ClassOrganizer allCategory
+		ifTrue:
+			[ "User tried to save a method while the ALL category was selected"
+			fallBackCategoryIndex _ messageCategoryListIndex.
+			fallBackMethodIndex _ messageListIndex.
+			editSelection == #newMessage
+				ifTrue:
+					[ "Select the 'as yet unclassified' category"
+					messageCategoryListIndex _ 0.
+					(result _ self defineMessageFrom: aText notifying: aController)
+						ifNil:
+							["Compilation failure:  reselect the original category & method"
+							messageCategoryListIndex _ fallBackCategoryIndex.
+							messageListIndex _ fallBackMethodIndex]
+						ifNotNil:
+							[self setSelector: result]]
+				ifFalse:
+					[originalSelectorName _ self selectedMessageName.
+					self setOriginalCategoryIndexForCurrentMethod.
+					messageListIndex _ fallBackMethodIndex _ self messageList indexOf: originalSelectorName.			
+					(result _ self defineMessageFrom: aText notifying: aController)
+						ifNotNil:
+							[self setSelector: result]
+						ifNil:
+							[ "Compilation failure:  reselect the original category & method"
+							messageCategoryListIndex _ fallBackCategoryIndex.
+							messageListIndex _ fallBackMethodIndex.
+							^ result notNil]].
+			self changed: #messageCategoryList.
+			^ result notNil]
+		ifFalse:
+			[ "User tried to save a method while the ALL category was NOT selected"
+			^ (self defineMessageFrom: aText notifying: aController) notNil]
