2569b746e93f78a0b1aa23c0089f7f012f03e5da
tk <unknown>
1999-05-27 16:16:00 +0000

    no message

---
+++ MethodFinder/instance/search/simpleSearch.cs
@@ -0,0 +1,31 @@
+simpleSearch
+	"Run through first arg's class' selectors, looking for one that works."
+
+| class supers listOfLists |
+self exceptions.
+class _ thisData first first class.
+"Cache the selectors for the receiver class"
+(class == cachedClass and: [cachedArgNum = ((argMap size) - 1)]) 
+	ifTrue: [listOfLists _ cachedSelectorLists]
+	ifFalse: [supers _ class withAllSuperclasses.
+		listOfLists _ OrderedCollection new.
+		supers do: [:cls |
+			listOfLists add: (cls selectorsWithArgs: (argMap size) - 1)].
+		cachedClass _ class.
+		cachedArgNum _ (argMap size) - 1.
+		cachedSelectorLists _ listOfLists].
+listOfLists do: [:selectorList |
+	selectorList do: [:aSel |
+		(selector includes: aSel) ifFalse: [
+			((Approved includes: aSel) or: [AddAndRemove includes: aSel]) ifTrue: [
+				(self testPerfect: aSel) ifTrue: [
+					selector add: aSel.
+					expressions nextPut: $(.
+					expressions nextPutAll: 'data', argMap first printString.
+					aSel keywords doWithIndex: [:key :ind |
+						expressions nextPutAll: ' ',key.
+						(key last == $:) | (key first isLetter not)
+							ifTrue: [expressions nextPutAll: ' data', 
+								(argMap at: ind+1) printString]].
+					expressions nextPut: $); space.
+					]]]]].
