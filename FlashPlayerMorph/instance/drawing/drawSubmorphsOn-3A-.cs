47ecdafdc3c91d519d2962fc413c59c03871e0b3
ar <unknown>
1999-05-29 09:49:00 +0000

    no message

---
+++ FlashPlayerMorph/instance/drawing/drawSubmorphsOn-3A-.cs
@@ -0,0 +1,9 @@
+drawSubmorphsOn: aCanvas
+	| myCanvas |
+	aCanvas clipBy: self bounds during:[:tempCanvas|
+		myCanvas _ tempCanvas asBalloonCanvas.
+		myCanvas aaLevel: (self defaultAALevel ifNil:[1]).
+		myCanvas deferred: self deferred.
+		myCanvas transformBy: self transform during:[:childCanvas| 
+			activeMorphs reverseDo:[:m| childCanvas fullDrawMorph: m]].
+		myCanvas flush].
