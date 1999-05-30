a5e66c8c26c0ae23267290a2812a893e612ab6f3
di <unknown>
1999-05-30 12:40:00 +0000

    no message

---
+++ MoviePlayerMorph/instance/menu/openMovieFile.cs
@@ -0,0 +1,5 @@
+openMovieFile
+	| fileName |
+	fileName _ Utilities chooseFileWithSuffixFromList: #('.movie')
+					withCaption: 'Choose a movie file to open'.
+	fileName ifNotNil: [self openFileNamed: fileName]
