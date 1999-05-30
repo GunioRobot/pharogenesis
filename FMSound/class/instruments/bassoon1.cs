d97ca5f5c11cd48bf86dd7c6f93b6a851b4056a0
jm <unknown>
1999-05-30 20:37:00 +0000

    no message

---
+++ FMSound/class/instruments/bassoon1.cs
@@ -0,0 +1,18 @@
+bassoon1
+	"FMSound bassoon1 play"
+	"(FMSound lowMajorScaleOn: FMSound bassoon1) play"
+
+	| snd p env |
+	snd _ FMBassoonSound new ratio: 1.
+
+	p _ OrderedCollection new.
+	p add: 0@0.0; add: 40@0.45; add: 90@1.0; add: 180@0.9; add: 270@1.0; add: 320@0.0.
+	snd addEnvelope: (VolumeEnvelope points: p loopStart: 3 loopEnd: 5).
+
+	p _ OrderedCollection new.
+	p add: 0@0.2; add: 40@0.9; add: 90@0.6; add: 270@0.6; add: 320@0.5.
+	env _ Envelope points: p loopStart: 3 loopEnd: 4.
+	env updateSelector: #modulation:; scale: 5.05.
+	snd addEnvelope: env.
+
+	^ snd setPitch: 220.0 dur: 1.0 loudness: 0.5
