83ff368558f16c674f1c14d4b9a14f3cca8c0675
len <unknown>
1999-05-31 00:14:00 +0000

    no message

---
+++ PhoneticRule/class/examples-2D-english/english.cs
@@ -0,0 +1,12 @@
+english
+	"Answer the english phonetic rules."
+	| answer |
+	answer _ OrderedCollection new.
+	#(englishPunctuationRules
+		englishARules englishBRules englishCRules englishDRules englishERules
+		englishFRules englishGRules englishHRules englishIRules englishJRules
+		englishKRules englishLRules englishMRules englishNRules englishORules
+		englishPRules englishQRules englishRRules englishSRules englishTRules
+		englishURules englishVRules englishWRules englishXRules englishYRules
+		englishZRules) do: [ :each | answer addAll: (self perform: each)].
+	^ answer asArray
