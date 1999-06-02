cb6beb557b8bf248bc60fda28cb6a248b8a0ba7a
ar <unknown>
1999-06-02 04:56:00 +0000

    no message

---
+++ FlashPlayerMorph/instance/project transition/addFillForProjectTarget-3A-.cs
@@ -0,0 +1,8 @@
+addFillForProjectTarget: aFillStyle
+	| fillStyles |
+	fillStyles _ self valueOfProperty: #projectTargetFills ifAbsent:[IdentityDictionary new].
+	(fillStyles includesKey: aFillStyle) ifTrue:[^self].
+	fillStyles at: aFillStyle put: aFillStyle form.
+	self setProperty: #projectTargetFills toValue: fillStyles.
+	self updateProjectFillsFrom: Project current.
+	self changed.
