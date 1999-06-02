bc8db24f2a7bc832091b9de1a6ccff52c4e22fff
ar <unknown>
1999-06-02 11:54:00 +0000

    no message

---
+++ B3DPickerEngine/instance/private-2D-rendering/primComputeMinIndexZ-3A-vtxArray-3A-vtxSize-3A-idxArray-3A-idxSize-3A-.cs
@@ -0,0 +1,3 @@
+primComputeMinIndexZ: primType vtxArray: vtxArray vtxSize: vtxSize idxArray: idxArray idxSize: idxSize
+	"<primitive: 'b3dComputeMinIndexZ' module: 'Squeak3D'>"
+	^nil "Indicates failure"
