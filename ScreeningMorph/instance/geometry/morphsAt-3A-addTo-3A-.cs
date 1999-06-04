689874d865a152d13628333bc1a55c75b35cad10
di <unknown>
1999-06-04 09:28:00 +0000

    no message

---
+++ ScreeningMorph/instance/geometry/morphsAt-3A-addTo-3A-.cs
@@ -0,0 +1,7 @@
+morphsAt: aPoint addTo: mList
+	"Impose screenMorph clipping when screening is in effect"
+
+	(submorphs size = 2
+		and: [displayMode == #showScreened
+		and: [(self containsPoint: aPoint) not]]) ifTrue: [^ mList].
+	^ super morphsAt: aPoint addTo: mList
