bfea4c0eb2de60cafe478c85bb5269d7b20e2e15
di <unknown>
1999-06-07 15:37:00 +0000

    no message

--- EnvelopeEditorMorph/instance/stepping/step.cs
+++ EnvelopeEditorMorph/instance/stepping/step.cs
@@ -1,18 +1,11 @@
 step
 	| mouseDown hand |
-	playRemaining > 0 ifTrue:
-		["Finish playing the last sample"
-		playRemaining _ playRemaining - self stepTime max: 0.
-		^ self].
-	hand _ self world hands first.
+	hand _ self world firstHand.
 	(bounds containsPoint: hand position) ifFalse: [^ self].
 
 	mouseDown _ hand lastEvent redButtonPressed.
-	(mouseDown and: [playMode = #duringEdits]) ifTrue:
-		[self playSample].
 	mouseDown not & prevMouseDown ifTrue:
 		["Mouse just went up"
-		playMode = #afterEdits ifTrue: [self playSample].
 		limitXs = (limits collect: [:i | (envelope points at: i) x]) ifFalse:
 			["Redisplay after changing limits"
 			self editEnvelope: envelope]].
