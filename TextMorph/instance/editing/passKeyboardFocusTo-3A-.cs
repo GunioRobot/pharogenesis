a831bc8642302316b628119bf922a2cef08d5d32
di <unknown>
1999-06-07 15:56:00 +0000

    no message

--- TextMorph/instance/editing/passKeyboardFocusTo-3A-.cs
+++ TextMorph/instance/editing/passKeyboardFocusTo-3A-.cs
@@ -1,9 +1,6 @@
 passKeyboardFocusTo: otherMorph
 	| w |
-	w _ self world.
-	w notNil ifTrue: 
-		[w hands do:
+	(w _ self world) == nil ifFalse:
+		[w handsDo:
 			[:h | h keyboardFocus == self
 				ifTrue: [h newKeyboardFocus: otherMorph]]].
-	"Release control unless some hand is still holding on"
-	self releaseEditor.
