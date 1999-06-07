8ab29fc803fc1d3e19ba6a9de20e6b2f61b4aaf3
di <unknown>
1999-06-07 15:45:00 +0000

    no message

--- MVCMenuMorph/instance/invoking/invokeAt-3A-in-3A-.cs
+++ MVCMenuMorph/instance/invoking/invokeAt-3A-in-3A-.cs
@@ -1,11 +1,9 @@
 invokeAt: aPoint in: aWorld
 	"Add this menu to the given world centered at the given point. Wait for the user to make a selection and answer it. The selection value returned is an integer in keeping with PopUpMenu, if the menu is converted from an MVC-style menu."
-	"Details: This is invoked synchronously from the caller. In order to keep processing inputs and updating the screen while waiting for the user to respond, this method has its own version of the World's event loop."
-	| h |
-	h _ aWorld activeHand.
-	h ifNil: [h _ aWorld hands first].
-	self popUpAt: aPoint forHand: h.
+	"Details: This is invoked synchronously from the caller. In order to keep processing inputs and updating the screen while waiting for the user to respond, this method has its own version of the World's event loop." 
+
+	self popUpAt: aPoint forHand: aWorld primaryHand.
 	done _ false.
-	[self isInWorld & done not] whileTrue: [aWorld doOneCycle].
+	[self isInWorld & done not] whileTrue: [aWorld doOneSubCycle].
 	self delete.
 	^ selectedItem
