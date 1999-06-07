99f2f0cf9c94ff1b55735de81879e1896e01877d
jm <unknown>
1999-06-07 10:40:00 +0000

    no message

--- SoundPlayer/class/player process/startReverb.cs
+++ SoundPlayer/class/player process/startReverb.cs
@@ -1,6 +1,7 @@
 startReverb
+	"Start a delay-line style reverb with the given tap delays and gains. Tap delays are given in samples and should be prime integers; the following comment gives an expression that generates primes."
+	"Integer primesUpTo: 22050"
 
 	UseReverb _ true.
 	ReverbState _ ReverbSound new
-		tapDelays: #(1601 7919) gains: #(0.15 0.09).
-
+		tapDelays: #(1601 7919) gains: #(0.12 0.07).
