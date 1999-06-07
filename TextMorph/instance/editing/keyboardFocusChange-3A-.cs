b4ec38fa1ce23bb42daef897dee815879acc662a
di <unknown>
1999-06-07 15:51:00 +0000

    no message

--- TextMorph/instance/editing/keyboardFocusChange-3A-.cs
+++ TextMorph/instance/editing/keyboardFocusChange-3A-.cs
@@ -2,10 +2,9 @@ keyboardFocusChange: aBoolean
 	| w |
 	aBoolean
 		ifTrue: ["A hand is wanting to send us characters..."
-				self hasFocus ifFalse: [self installEditor]]
+				self hasFocus ifFalse: [self editor "Forces install"]]
 		ifFalse: ["A hand has clicked elsewhere..."
-				w _ self world.
-				(w notNil and: 
-					[(w hands collect: [:h | h keyboardFocus]) includes: self])
-					ifFalse: ["Release control unless some hand is still holding on"
-							self releaseEditor]].
+				(w _ self world) == nil ifFalse:
+					[w handsDo: [:h | h keyboardFocus == self ifTrue: [^ self]].
+					"Release control unless some hand is still holding on"
+					self releaseEditor]].
