f0e1701d56274b4f9aa0ecf58f02ac1d8cb9de4a
di <unknown>
1999-06-08 15:47:00 +0000

    no message

--- SystemDictionary/instance/housekeeping/removeEmptyMessageCategories.cs
+++ SystemDictionary/instance/housekeeping/removeEmptyMessageCategories.cs
@@ -1,7 +1,5 @@
 removeEmptyMessageCategories
 	"Smalltalk removeEmptyMessageCategories"
 	Smalltalk garbageCollect.
-	ClassOrganizer allInstances , (Array with: SystemOrganization) do:
-		[:org | org categories do: 
-			[:cat | (org listAtCategoryNamed: cat) isEmpty
-				ifTrue: [org removeCategory: cat]]]
+	(ClassOrganizer allInstances copyWith: SystemOrganization) do:
+		[:org | org removeEmptyCategories]
