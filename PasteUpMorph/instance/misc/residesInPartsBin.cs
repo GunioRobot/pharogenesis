f742d218c337293c67d932c2228e741df8c0da33
di <unknown>
1999-06-08 14:22:00 +0000

    no message

--- PasteUpMorph/instance/misc/residesInPartsBin.cs
+++ PasteUpMorph/instance/misc/residesInPartsBin.cs
@@ -1,3 +1,6 @@
 residesInPartsBin
 	"Answer true if the receiver is, or has some ancestor owner who is, a parts bin"
-	^ self isPartsBin ifTrue: [true] ifFalse: [super residesInPartsBin]
+
+	self isWorldMorph
+		ifTrue: [^ self isPartsBin]
+		ifFalse: [^ self isPartsBin or: [super residesInPartsBin]]
