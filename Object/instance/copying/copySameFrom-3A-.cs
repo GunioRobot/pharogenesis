e6a4e403a4f43bcdfcd0785de1d662a2c5c91baf
di <unknown>
1999-06-09 14:44:00 +0000

    no message

---
+++ Object/instance/copying/copySameFrom-3A-.cs
@@ -0,0 +1,10 @@
+copySameFrom: otherObject
+	"Copy to myself all instance variables named the same in otherObject.
+	This ignores otherObject's control over its own inst vars."
+
+	| myInstVars otherInstVars match |
+	myInstVars _ self class allInstVarNames.
+	otherInstVars _ otherObject class allInstVarNames.
+	myInstVars doWithIndex: [:each :index |
+		(match _ otherInstVars indexOf: each) > 0 ifTrue:
+			[self instVarAt: index put: (otherObject instVarAt: match)]]
