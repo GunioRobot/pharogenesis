e1a2f754f249d478707cc572d6b4d0f0954c7f9e
sw <unknown>
1999-06-11 20:25:00 +0000

    no message

--- ScreenController/instance/nested menus/windowMenu.cs
+++ ScreenController/instance/nested menus/windowMenu.cs
@@ -2,25 +2,23 @@ windowMenu
 	"Answer a menu for windows-related items.  "
 
 	^ SelectionMenu labelList:
-		#(	'find window...'
+		#(	'keep this menu up'
+
+			'find window...'
 			'find changed browsers...'
 			'find changed windows...'
+
 			'collapse all windows'
 			'expand all windows'
-			'close unchanged windows') ,
-			(Array with: (StandardSystemView cachingBits
-							ifTrue: ['dont save bits (compact)']
-							ifFalse: ['save bits (fast)'])
-				with: ((Preferences valueOfFlag: #reverseWindowStagger)
-							ifTrue: ['tile windows']
-							ifFalse: ['stagger windows']))
-		lines: #(3 5 6)
-		selections: #(findWindow chooseDirtyBrowser chooseDirtyWindow
-				collapseAll expandAll
-				closeUnchangedWindows
-				fastWindows changeWindowPolicy)
-
+			'close unchanged windows' ) , 
+			(Array
+				with: self bitCachingString
+				with: self staggerPolicyString)
+		lines: #(1 4 7)
+		selections: #(durableWindowMenu
+findWindow chooseDirtyBrowser chooseDirtyWindow
+collapseAll expandAll closeUnchangedWindows
+fastWindows changeWindowPolicy)
 "
 ScreenController new windowMenu startUp
-ScreenController initialize
 "
