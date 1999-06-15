a9a724fca6b6450818196ce50782b077dc29da2a
di <unknown>
1999-06-15 11:17:00 +0000

    no message

--- ScorePlayer/instance/initialization/initialize.cs
+++ ScorePlayer/instance/initialization/initialize.cs
@@ -3,8 +3,10 @@ initialize
 	super initialize.
 	score _ MIDIScore new initialize.
 	instruments _ Array new.
+	overallVolume _ 0.5.
 	leftVols _ Array new.
 	rightVols _ Array new.
 	muted _ Array new.
 	rate _ 1.0.
 	repeat _ false.
+	durationInTicks _ 100.
