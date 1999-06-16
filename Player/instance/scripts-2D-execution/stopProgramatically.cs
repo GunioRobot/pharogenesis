110fa02f9da5154ff9e6f74a673b887416cc9f81
jm <unknown>
1999-06-16 14:37:00 +0000

    no message

--- Player/instance/scripts-2D-execution/stopProgramatically.cs
+++ Player/instance/scripts-2D-execution/stopProgramatically.cs
@@ -1,4 +1,6 @@
 stopProgramatically
 	"stop running my ticking scripts -- called from running code"
 	self instantiatedUserScriptsDo:
-		[:aUserScript | aUserScript stopTicking]
+		[:aUserScript | aUserScript stopTicking].
+	(costume renderedMorph isKindOf: SpeakerMorph)
+		ifTrue: [costume renderedMorph stopSound].  "turn off buffered speaker sound"
