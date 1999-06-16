ba01a52e17b176be5e4436dd558ec03bbd4c05dc
jm <unknown>
1999-06-16 21:04:00 +0000

    no message

--- ScorePlayer/instance/sound generation/processTempoMapAtTick-3A-.cs
+++ ScorePlayer/instance/sound generation/processTempoMapAtTick-3A-.cs
@@ -1,10 +1,16 @@
-processTempoMapAtTick: scoreTicks
+processTempoMapAtTick: scoreTick
+	"Process tempo changes through the given score tick."
 
-	| map |
+	| map tempoChanged |
 	map _ score tempoMap.
 	map ifNil: [^ self].
+	tempoChanged _ false.
 	[(tempoMapIndex <= map size) and:
-	 [(map at: tempoMapIndex) time <= scoreTicks]] whileTrue: [
-		tempo _ (120.0 * (500000.0 / (map at: tempoMapIndex) tempo)) roundTo: 0.01.
-		self tempoOrRateChanged.
+	 [(map at: tempoMapIndex) time <= scoreTick]] whileTrue: [
+		tempoChanged _ true.
 		tempoMapIndex _ tempoMapIndex + 1].
+
+	tempoChanged ifTrue: [
+		tempo _ (120.0 * (500000.0 / (map at: tempoMapIndex - 1) tempo)) roundTo: 0.01.
+		self tempoOrRateChanged].
+
