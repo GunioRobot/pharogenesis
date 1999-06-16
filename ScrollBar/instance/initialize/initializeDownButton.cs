33d9856d9adff290a4573801a12e32b7f0372409
dew <unknown>
1999-06-16 01:59:00 +0000

    no message

--- ScrollBar/instance/initialize/initializeDownButton.cs
+++ ScrollBar/instance/initialize/initializeDownButton.cs
@@ -2,10 +2,10 @@ initializeDownButton
 	downButton := RectangleMorph
 		newBounds: (self innerBounds bottomRight - self buttonExtent extent: self buttonExtent)
 		color: Color lightGray.
-	downButton on: #mouseDown send: #borderInset to: downButton.
+	downButton on: #mouseDown send: #scrollDownInit to: self.
 	downButton on: #mouseStillDown send: #scrollDown to: self.
 	downButton on: #mouseUp send: #borderRaised to: downButton.
 	downButton addMorphCentered: (ImageMorph new image: 
 		(UpArrow rotateBy: (bounds isWide ifTrue: [#right] ifFalse: [#pi]) centerAt: 0@0)).
-	downButton setBorderWidth: 2 borderColor: #raised.
+	downButton setBorderWidth: 1 borderColor: #raised.
 	self addMorph: downButton
