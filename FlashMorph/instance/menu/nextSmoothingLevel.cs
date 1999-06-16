57574f53bbee4d34c5c6351014e0c77f4c29617d
ar <unknown>
1999-06-16 07:17:00 +0000

    no message

---
+++ FlashMorph/instance/menu/nextSmoothingLevel.cs
@@ -0,0 +1,7 @@
+nextSmoothingLevel
+	| aaLevel |
+	aaLevel _ self defaultAALevel ifNil:[1].
+	aaLevel = 1 ifTrue:[self defaultAALevel: 2].
+	aaLevel = 2 ifTrue:[self defaultAALevel: 4].
+	aaLevel = 4 ifTrue:[self defaultAALevel: nil].
+	self changed.
