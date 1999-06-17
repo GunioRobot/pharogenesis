6634b1f069faf4a6994fd3cf0b7200f51de5f541
sw <unknown>
1999-06-17 14:21:00 +0000

    no message

---
+++ Morph/instance/submorphs-2D-add-2F-remove/dismissViaHalo.cs
@@ -0,0 +1,4 @@
+dismissViaHalo
+	"The user has clicked in the delete halo-handle.  This provides a hook in case some concomitant action should be taken, or if the paticular morph is not one which should be put in the trash can, for example."
+
+	TrashCanMorph moveToTrash: self
