25828bc4bdea55e7b371e1dd67b82c05ce27cd51
sw <unknown>
1999-06-17 16:58:00 +0000

    no message

--- HaloMorph/instance/private/dismiss.cs
+++ HaloMorph/instance/private/dismiss.cs
@@ -2,7 +2,8 @@ dismiss
 	"Remove my target from the world."
 
 	| w |
+	self isThisEverCalled. "Seemingly no longer enfranchised"
 	w _ self world.
 	w ifNotNil: [w stopStepping: target].
 	self delete.
-	target delete.
+	target dismissViaHalo
