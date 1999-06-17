2cbfc7f1ec28ed3f48a963486570b109f1e54d7d
jm <unknown>
1999-06-17 21:55:00 +0000

    no message

---
+++ GraphMorph/instance/object fileIn/convertbosfcebbddcccsmmc0-3A-bosfcebbddcccsmmch0-3A-.cs
@@ -0,0 +1,4 @@
+convertbosfcebbddcccsmmc0: varDict bosfcebbddcccsmmch0: smartRefStrm
+	"Ignore the deleted variables lastAcceptedScript and lastScriptEditor and initialize the new variable hasChanged."
+
+	hasChanged _ false.
