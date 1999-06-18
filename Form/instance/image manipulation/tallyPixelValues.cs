a2cec60025051feedc6dd72cdc20d8fa4b597ce4
jm <unknown>
1999-06-18 18:41:00 +0000

    no message

--- Form/instance/image manipulation/tallyPixelValues.cs
+++ Form/instance/image manipulation/tallyPixelValues.cs
@@ -1,12 +1,18 @@
 tallyPixelValues
-	"Return a Bitmap with tallies in it of the number of pixels in this Form that have each pixel value. Note that several Forms may be tallied into the same table by tallyPixelValuesPrimitive:into: with the same table. Also Forms of depth 16 or 32 can be tallied into a tables of size 512, 4096, or 32768 entries by making a direct call with a Bitmap of the given size."
-	| tallies pixPerWord extraZeros |
-	tallies _ Bitmap new: (1 bitShift: (self depth min: 15)).
-	self tallyPixelValuesPrimitive: self boundingBox into: tallies.
-	pixPerWord _ 32 // depth.
-	(self width \\ pixPerWord) = 0
-		ifFalse: [
-			"subtract extra zeros counts due to word-boundary"
-			extraZeros _ self height * (pixPerWord - (self width \\ pixPerWord)).
-			tallies at: 1 put: (tallies at: 1) - extraZeros].
-	^ tallies
+	"Answer a Bitmap whose elements contain the number of pixels in this Form with the pixel value corresponding to their index. Note that the pixels of multiple Forms can be tallied together using tallyPixelValuesInRect:into:."
+
+	^ self tallyPixelValuesInRect: self boundingBox
+		into: (Bitmap new: (1 bitShift: (self depth min: 15)))
+"
+Move a little rectangle around the screen and print its tallies...
+ | r tallies nonZero |
+Cursor blank showWhile: [
+[Sensor anyButtonPressed] whileFalse:
+	[r _ Sensor cursorPoint extent: 10@10.
+	Display border: (r expandBy: 2) width: 2 rule: Form reverse fillColor: nil.
+	tallies _ (Display copy: r) tallyPixelValues.
+	nonZero _ (1 to: tallies size) select: [:i | (tallies at: i) > 0]
+			thenCollect: [:i | (tallies at: i) -> (i-1)].
+	nonZero printString , '          ' displayAt: 0@0.
+	Display border: (r expandBy: 2) width: 2 rule: Form reverse fillColor: nil]]
+"
