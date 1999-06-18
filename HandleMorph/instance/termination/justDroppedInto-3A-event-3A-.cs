a85509adca75d4b0b4cad444102f4457ea95d329
ar <unknown>
1999-06-18 07:51:00 +0000

    no message

--- HandleMorph/instance/termination/justDroppedInto-3A-event-3A-.cs
+++ HandleMorph/instance/termination/justDroppedInto-3A-event-3A-.cs
@@ -1,3 +1,6 @@
 justDroppedInto: aMorph event: anEvent
 	"So that when the hand drops me (into the world) I go away"
+	anEvent hand keyboardFocus == self
+		ifTrue:[anEvent hand newKeyboardFocus: nil].
+	self changed.
 	self delete.
