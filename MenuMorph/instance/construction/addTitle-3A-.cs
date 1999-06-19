c491ff7e242fb65cafae76f394cccf0d33fd720f
sw <unknown>
1999-06-19 23:09:00 +0000

    no message

--- MenuMorph/instance/construction/addTitle-3A-.cs
+++ MenuMorph/instance/construction/addTitle-3A-.cs
@@ -1,11 +1,4 @@
 addTitle: aString
 	"Add a title line at the top of this menu."
 
-	| title |
-	title _ AlignmentMorph new setColor: (Color r: 0.5 g: 1 b: 0.75) borderWidth: 1 borderColor: #inset.
-	title vResizing: #shrinkWrap.
-	title orientation: #vertical.
-	title centering: #center.
-	(aString asString findTokens: String cr) do:
-		[:line | title addMorphBack: (StringMorph new contents: line)].
-	self addMorphFront: title.
+	self addTitle: aString updatingSelector: nil updateTarget: nil
