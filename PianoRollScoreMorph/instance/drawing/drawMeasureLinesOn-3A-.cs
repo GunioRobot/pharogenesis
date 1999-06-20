965b80fb4f93d56657e70c1d6fac6a5287fe69d8
di <unknown>
1999-06-20 16:35:00 +0000

    no message

---
+++ PianoRollScoreMorph/instance/drawing/drawMeasureLinesOn-3A-.cs
@@ -0,0 +1,27 @@
+drawMeasureLinesOn: aCanvas
+
+	| ticksPerMeas x measureLineColor inner |
+	showBeatLines ifNil: [showBeatLines _ false].
+	showMeasureLines ifNil: [showMeasureLines _ true].
+	notePerBeat ifNil: [self timeSignature: 4 over: 4].
+	showBeatLines ifTrue:
+		[measureLineColor _ Color gray: 0.8.
+		ticksPerMeas _ score ticksPerQuarterNote.
+		inner _ self innerBounds.
+		(leftEdgeTime + ticksPerMeas truncateTo: ticksPerMeas)
+			to: ((self timeForX: self right - borderWidth) truncateTo: ticksPerMeas)
+			by: ticksPerMeas
+			do: [:tickTime | x _ self xForTime: tickTime.
+				aCanvas fillRectangle: (x @ inner top extent: 1 @ inner height)
+						color: measureLineColor]].
+
+	showMeasureLines ifTrue:
+		[measureLineColor _ Color gray: 0.7.
+		ticksPerMeas _ beatsPerMeasure*score ticksPerQuarterNote*4//notePerBeat.
+		inner _ self innerBounds.
+		(leftEdgeTime + ticksPerMeas truncateTo: ticksPerMeas)
+			to: ((self timeForX: self right - borderWidth) truncateTo: ticksPerMeas)
+			by: ticksPerMeas
+			do: [:tickTime | x _ self xForTime: tickTime.
+				aCanvas fillRectangle: (x @ inner top extent: 1 @ inner height)
+						color: measureLineColor]].
