45f6becf8ff2004af9b9fc4732ccbccd874a2f87
sw <unknown>
1999-06-20 20:55:00 +0000

    no message

---
+++ FlapTab/instance/solid tabs/useSolidTab.cs
@@ -0,0 +1,21 @@
+useSolidTab
+	| thickness colorToUse |
+	self preserveDetails.
+
+	thickness _ self valueOfProperty: #priorThickness ifAbsent: [20].
+	colorToUse _ self valueOfProperty: #priorColor ifAbsent: [Color red muchLighter].
+	self color: colorToUse.
+	self removeAllMorphs.
+	
+	(self orientation == #vertical)
+		ifTrue:
+			[self width: thickness.
+			self height: self currentWorld height.
+			self position: (self position x @ 0)]
+		ifFalse:
+			[self height: thickness.
+			self width: self currentWorld width.
+			self position: (0 @ self position y)].
+
+	self borderWidth: 0.
+	self layoutChanged.
