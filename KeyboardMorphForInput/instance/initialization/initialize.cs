d0a176d54e3c7a3da9e6a023990dacba70e3d34a
di <unknown>
1999-06-20 12:54:00 +0000

    no message

---
+++ KeyboardMorphForInput/instance/initialization/initialize.cs
@@ -0,0 +1,8 @@
+initialize
+	super initialize.
+	buildingChord _ false.
+	self addRecordingControls.
+	self duration: 4 onOff: true.
+	self durMod: #normal onOff: true.
+	self articulation: #normal onOff: true.
+	insertMode _ false
