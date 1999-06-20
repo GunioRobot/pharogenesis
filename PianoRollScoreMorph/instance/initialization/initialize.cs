79b6ea515ab24fade8d8335b2aaffd3ee553207b
di <unknown>
1999-06-20 16:20:00 +0000

    no message

---
+++ PianoRollScoreMorph/instance/initialization/initialize.cs
@@ -0,0 +1,9 @@
+initialize
+
+	super initialize.
+	borderWidth _ 1.
+	color _ Color white.
+	self extent: 400@300.
+	showMeasureLines _ true.
+	showBeatLines _ false.
+	self timeSignature: 4 over: 4.
