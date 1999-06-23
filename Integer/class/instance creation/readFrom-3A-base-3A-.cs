80ef688afb9ead1f45b6614e6d928d5610717f45
ls <unknown>
1999-06-23 20:37:00 +0000

    no message

--- Integer/class/instance creation/readFrom-3A-base-3A-.cs
+++ Integer/class/instance creation/readFrom-3A-base-3A-.cs
@@ -4,15 +4,18 @@ readFrom: aStream base: base
 	allowed--use Number readFrom: for that. Answer zero (not an error) if 
 	there are no digits."
 
-	| digit value neg |
+	| digit value neg startPos |
 	neg _ aStream peekFor: $-.
+	neg ifFalse: [aStream peekFor: $+].
 	value _ 0.
+	startPos _ aStream position.
 	[aStream atEnd]
 		whileFalse: 
 			[digit _ aStream next digitValue.
 			(digit < 0 or: [digit >= base])
 				ifTrue: 
 					[aStream skip: -1.
+					aStream position = startPos ifTrue: [self error: 'At least one digit expected here'].
 					neg ifTrue: [^ value negated].
 					^ value]
 				ifFalse: [value _ value * base + digit]].
