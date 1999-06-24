db4313308599606b2954b1a6f7196b99d77251aa
tk <unknown>
1999-06-24 11:31:00 +0000

    no message

---
+++ AbstractSound/class/sound library/soundNamed-3A-put-3A-.cs
@@ -0,0 +1,6 @@
+soundNamed: soundName put: aSound
+
+	Sounds at: soundName put: aSound.
+	Smalltalk at: #ScorePlayerMorph ifPresent: [:playerClass |
+		playerClass allSubInstancesDo:
+			[:player | player updateInstrumentsFromLibrary]].
