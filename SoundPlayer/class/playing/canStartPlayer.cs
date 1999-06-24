ac229cd16601f35e91a959d59c27d462ce34369b
tk <unknown>
1999-06-24 11:42:00 +0000

    no message

--- SoundPlayer/class/playing/canStartPlayer.cs
+++ SoundPlayer/class/playing/canStartPlayer.cs
@@ -2,5 +2,5 @@ canStartPlayer
 	"Some platforms do no support simultaneous record and play. If this is one of those platforms, return false if there is a running SoundRecorder."
 
 	SoundRecorder canRecordWhilePlaying ifTrue: [^ true].
-	SoundRecorder allInstancesDo: [:rec | rec isActive ifTrue: [^ false]].
+	SoundRecorder allSubInstancesDo: [:rec | rec isActive ifTrue: [^ false]].
 	^ true
