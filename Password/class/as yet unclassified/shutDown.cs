d3639dbb6612de18df311e383bc4dcc39e2c80ad
tk <unknown>
1999-06-24 11:36:00 +0000

    no message

--- Password/class/as yet unclassified/shutDown.cs
+++ Password/class/as yet unclassified/shutDown.cs
@@ -1,5 +1,4 @@
 shutDown
 	"Forget all cached passwords, so they won't stay in the image"
 
-	self withAllSubclasses do: [:cls |
-		cls allInstancesDo: [:each | each cache: nil]].
+	self allSubInstancesDo: [:each | each cache: nil].
