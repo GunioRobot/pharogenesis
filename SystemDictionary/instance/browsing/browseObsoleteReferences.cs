d25de1f0a140bb38281aaac11ccf4010c55dbdaa
tk <unknown>
1999-06-24 11:28:00 +0000

    no message

--- SystemDictionary/instance/browsing/browseObsoleteReferences.cs
+++ SystemDictionary/instance/browsing/browseObsoleteReferences.cs
@@ -1,7 +1,7 @@
 browseObsoleteReferences   "Smalltalk browseObsoleteReferences"
 	| references |
 	references _ OrderedCollection new.
-	(Association allInstances select:
+	(Association allSubInstances select:
 		[:x | ((x value isKindOf: Behavior) and: ['AnOb*' match: x value name]) or:
 		['AnOb*' match: x value class name]]) 
 		do: [:x | references addAll: (Smalltalk allCallsOn: x)].
