49b2e02f7795153968ef3b04b8eb773b0be6b858
ar <unknown>
1999-06-28 16:33:00 +0000

    no message

---
+++ FlashFileReader/instance/reading/processHeader.cs
@@ -0,0 +1,28 @@
+processHeader
+	"Read header information from the source stream.
+	Return true if successful, false otherwise."
+	| twipsFrameSize frameRate frameCount |
+	self processSignature ifFalse:[^false].
+	version _ stream nextByte.
+	"Check for the version supported"
+	version > self maximumSupportedVersion ifTrue:[^false].
+	dataSize _ stream nextLong.
+	"Check for the minimal file size"
+	dataSize < 21 ifTrue:[^false].
+	twipsFrameSize _ stream nextRect.
+	self recordGlobalBounds: twipsFrameSize.
+	frameRate _ stream nextWord / 256.0.
+	self recordFrameRate: frameRate.
+	frameCount _ stream nextWord.
+	self recordFrameCount: frameCount.
+	log ifNotNil:[
+		log cr; nextPutAll:'------------- Header information --------------'.
+		log cr; nextPutAll:'File version		'; print: version.
+		log cr; nextPutAll:'File size			'; print: dataSize.
+		log cr; nextPutAll:'Movie width		'; print: twipsFrameSize extent x // 20.
+		log cr; nextPutAll:'Movie height	'; print: twipsFrameSize extent y // 20.
+		log cr; nextPutAll:'Frame rate		'; print: frameRate.
+		log cr; nextPutAll:'Frame count	'; print: frameCount.
+		log cr; cr.
+		self flushLog].
+	^true
