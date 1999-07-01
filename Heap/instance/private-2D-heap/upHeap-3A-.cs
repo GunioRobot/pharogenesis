29bfd3db865bbf5541b147d7fd7cb562ea163f89
ar <unknown>
1999-07-01 04:34:00 +0000

    no message

---
+++ Heap/instance/private-2D-heap/upHeap-3A-.cs
@@ -0,0 +1,12 @@
+upHeap: anIndex
+	"Check the heap upwards for correctness starting at anIndex.
+	 Everything below anIndex is ok."
+	| value k kDiv2 tmp |
+	anIndex = 0 ifTrue:[^self].
+	k _ anIndex.
+	value _ array at: anIndex.
+	[ (k > 1) and:[self sorts: value before: (tmp _ array at: (kDiv2 _ k bitShift: -1))] ] 
+		whileTrue:[
+			array at: k put: tmp.
+			k _ kDiv2].
+	array at: k put: value.
