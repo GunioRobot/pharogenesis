af84b718fc6f097fd2e82e6ca6e897088793e2e3
tk <unknown>
1999-07-05 19:23:00 +0000

    no message

--- ServerDirectory/instance/updates/checkServers.cs
+++ ServerDirectory/instance/updates/checkServers.cs
@@ -1,9 +1,10 @@
 checkServers
-	"Check that all servers are up and have the latest Updates.list.  Return the servers that ready to receiver this update"
+	"Check that all servers are up and have the latest Updates.list.
+Warn user when can't write to a server that can still be read."
 
-	| final fileSize this serverList theUpdates decided myUpdates abort strm res |
-	serverList _ group 
-			ifNil: [Array with: self] 
+	| final fileSize this serverList theUpdates decided myUpdates abort |
+	serverList _ group
+			ifNil: [Array with: self]
 			ifNotNil: [group value].
 	final _ OrderedCollection new.
 	fileSize _ 0.  theUpdates _ ''.	"list of updates"
@@ -11,28 +12,23 @@ checkServers
 	serverList do: [:aServer |
 		decided _ false.
 		this _ aServer getFileNamed: 'updates.list'.
-		(this class == String) & (url size > 0) ifTrue: [
-			strm _ HTTPSocket httpGet: url accept: 'application/octet-stream'.
-			strm class == String 
-				ifTrue: [res _ (PopUpMenu  
-						labels: 'Install even though not visible\Cancel entire update' withCRs)
-						startUpWithCaption: 'Server ', aServer moniker, 
-						' is a weird state.\You cannot store, but users can get updates.\If you store on other servers, the file will not be visible.\Strongly advise that you Cancel.' withCRs]
-				ifFalse: [
-					res _ (PopUpMenu labels: 'Install on others\Cancel entire update' withCRs)
-							startUpWithCaption: 'Server ', aServer moniker, 
-							' is unavailable\' withCRs, this].
-			abort _ res ~= 1.
+		(this = #error:) ifTrue: [^'' "Not found"].
+		this class == String ifTrue: ["no ftp"
+			(PopUpMenu labels: 'Cancel entire update' withCRs)
+				startUpWithCaption: 'Server ', aServer moniker,
+				' is unavailable.\Please consider phoning the administator.\' withCRs, this.
+			abort _ true.
 			decided _ true].
 		decided not & (this size > fileSize) ifTrue: ["new has a longer update.list"
 			fileSize _ this size.
 			final do: [:each | abort _ self outOfDate: each].
 			(final _ OrderedCollection new) add: aServer.
-			theUpdates _ this contentsOfEntireFile. 
+			theUpdates _ this contentsOfEntireFile.
 			decided _ true].
-		decided not & (this size < fileSize) ifTrue: [abort _ self outOfDate: aServer.  decided _ true].
+		decided not & (this size < fileSize) ifTrue: [
+			abort _ self outOfDate: aServer.  decided _ true].
 		decided not ifTrue: [myUpdates _ this contentsOfEntireFile.
-			myUpdates = theUpdates 
+			myUpdates = theUpdates
 				ifTrue: [final add: aServer]
 				ifFalse: [abort _ self outOfDate: this]].
 		abort ifTrue: [^ Array new].
