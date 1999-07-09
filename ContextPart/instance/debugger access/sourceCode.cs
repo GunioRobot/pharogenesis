37c18b8608d5cd4d01aab4e121d567b963a1ac4c
ar <unknown>
1999-07-09 19:01:00 +0000

    no message

--- ContextPart/instance/debugger access/sourceCode.cs
+++ ContextPart/instance/debugger access/sourceCode.cs
@@ -2,4 +2,10 @@ sourceCode
 	| selector methodClass |
 	selector _ self receiver class selectorAtMethod: self method
 		setClass: [:mclass | methodClass _ mclass].
-	^ methodClass sourceCodeAt: selector
+	^self method getSourceFor: selector in: methodClass
+	"Note: The above is a bit safer than
+		^ methodClass sourceCodeAt: selector
+	which may fail if the receiver's method has been changed in
+	the debugger (e.g., the method is no longer in the methodDict
+	and thus the above selector is something like #Doit:with:with:with:)
+	but the source code is still available."
