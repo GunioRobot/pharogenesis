80df5e47a622f56d832e0b5edbaf187d80d0bd23
jm <unknown>
1999-07-09 19:07:00 +0000

    no message

--- SampledSound/class/class initialization/initialize.cs
+++ SampledSound/class/class initialization/initialize.cs
@@ -1,5 +1,8 @@
 initialize
 	"SampledSound initialize"
 
+	IncrementFractionBits _ 16.
+	IncrementScaleFactor _ 2 raisedTo: IncrementFractionBits.
+	ScaledIndexOverflow _ 2 raisedTo: 29.  "handle overflow before needing LargePositiveIntegers"
 	self useCoffeeCupClink.
-	SoundLibrary _ Dictionary new.
+	SoundLibrary ifNil: [SoundLibrary _ Dictionary new].
