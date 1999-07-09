a19aca22b86c1ae226827e4e5e79d4f92e9fa2e4
jm <unknown>
1999-07-09 19:23:00 +0000

    no message

--- SampledSound/instance/initialization/setSamples-3A-samplingRate-3A-.cs
+++ SampledSound/instance/initialization/setSamples-3A-samplingRate-3A-.cs
@@ -1,5 +1,6 @@
 setSamples: anArray samplingRate: rate
 	"Set my samples array to the given array with the given nominal sampling rate. Altering the rate parameter allows the sampled sound to be played back at different pitches."
+	"Note: There are two ways to use sampled sound: (a) you can play them through once (supported by this method) or (b) you can make them the default waveform with which to play a musical score (supported by the class method defaultSampleTable:)."
 	"Assume: anArray is either a SoundBuffer or a collection of signed 16-bit sample values."
 	"(SampledSound
 		samples: SampledSound coffeeCupClink
@@ -11,7 +12,9 @@ setSamples: anArray samplingRate: rate
 		ifFalse: [samples _ SoundBuffer fromArray: anArray].
 
 	samplesSize _ samples size.
+	samplesSize >= SmallInteger maxVal ifTrue: [  "this is unlikely..."
+		self error: 'sample count must be under ',  SmallInteger maxVal printString].
 	originalSamplingRate _ rate.
 	initialCount _ (samplesSize * self samplingRate) // originalSamplingRate.
-	self setLoudness: 0.25.
+	self loudness: 1.0.
 	self reset.
