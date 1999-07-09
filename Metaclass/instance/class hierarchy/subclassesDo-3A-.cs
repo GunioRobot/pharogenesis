a9e8e261e93e5f067339d4513a677fb0920092c1
ar <unknown>
1999-07-09 14:11:00 +0000

    no message

--- Metaclass/instance/class hierarchy/subclassesDo-3A-.cs
+++ Metaclass/instance/class hierarchy/subclassesDo-3A-.cs
@@ -1,8 +1,6 @@
 subclassesDo: aBlock
 	"Evaluate aBlock for each of the receiver's immediate subclasses."
-	self == Class class
-		ifTrue: ["Don't include Object class class in Class class's subclasses (heh heh)"
-				thisClass subclassesDo: [:aSubclass | aSubclass == Object class
-											ifFalse: [aBlock value: aSubclass class]]]
-		ifFalse: [thisClass == nil
-				ifFalse: [thisClass subclassesDo: [:aSubclass | aBlock value: aSubclass class]]]
+	thisClass subclassesDo:[:aSubclass|
+		"The following test is for Class class which has to exclude
+		the Metaclasses being subclasses of Class."
+		aSubclass isMeta ifFalse:[aBlock value: aSubclass class]].
