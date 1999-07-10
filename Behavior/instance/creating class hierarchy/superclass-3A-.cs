1a883dea0eecb6d7f8f7d82fc508dca3345b711c
ar <unknown>
1999-07-10 12:10:00 +0000

    no message

--- Behavior/instance/creating class hierarchy/superclass-3A-.cs
+++ Behavior/instance/creating class hierarchy/superclass-3A-.cs
@@ -1,6 +1,8 @@
 superclass: aClass 
 	"Change the receiver's superclass to be aClass."
-
-	(aClass == nil or: [aClass isKindOf: Behavior])
-		ifTrue: [superclass _ aClass]
+	"Note: Do not use 'aClass isKindOf: Behavior' here
+		in case we recompile from Behavior itself."
+	(aClass == nil or: [aClass isBehavior])
+		ifTrue: [superclass _ aClass.
+				Object flushCache]
 		ifFalse: [self error: 'superclass must be a class-describing object']
