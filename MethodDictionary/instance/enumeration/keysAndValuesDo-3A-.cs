3207bf8adad2064ef709f4d6bdc15e03e4d826f9
ar <unknown>
1999-07-11 08:05:00 +0000

    no message

---
+++ MethodDictionary/instance/enumeration/keysAndValuesDo-3A-.cs
@@ -0,0 +1,8 @@
+keysAndValuesDo: aBlock 
+	"Enumerate the receiver with all the keys and values passed to the block"
+	| key |
+	tally = 0 ifTrue: [^ self].
+	1 to: self basicSize do:
+		[:i | (key _ self basicAt: i) == nil ifFalse:
+			[aBlock value: key value: (array at: i)]
+		]
