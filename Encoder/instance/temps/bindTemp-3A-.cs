1fdf92f05c89f70b6097177d93145c97d457130d
ar <unknown>
1999-07-12 00:24:00 +0000

    no message

--- Encoder/instance/temps/bindTemp-3A-.cs
+++ Encoder/instance/temps/bindTemp-3A-.cs
@@ -1,6 +1,8 @@
 bindTemp: name 
 	"Declare a temporary; error not if a field or class variable."
-
-	(scopeTable includesKey: name)
-		ifTrue: [^self notify: 'Name is already defined'].
+	scopeTable at: name ifPresent:[:node|
+		"When non-interactive raise the error only if its a duplicate"
+		(node isTemp or:[requestor interactive])
+			ifTrue:[^self notify:'Name is already defined']
+			ifFalse:[Transcript show: '(', name, ' is shadowed)']].
 	^self reallyBind: name
