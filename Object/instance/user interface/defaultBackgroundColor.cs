6cb57a4d5c2758b8c69a69a10af5caa2cd148885
sw <unknown>
1999-07-13 15:53:00 +0000

    no message

--- Object/instance/user interface/defaultBackgroundColor.cs
+++ Object/instance/user interface/defaultBackgroundColor.cs
@@ -1,4 +1,4 @@
 defaultBackgroundColor
-	"Answer the symbol representing the default background color to use if the receiver is used as the model for a StandardSystemView.  4/30/96 sw"
-
-	^ #white
+	"Answer the color to be used as the base window color for a window whose model is an object of the receiver's class"
+	
+	^ Preferences windowColorFor: self class name
