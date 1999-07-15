2fd5b45e2c8b6a55ad495809c13a4397d6737dd7
ar <unknown>
1999-07-15 16:39:00 +0000

    no message

---
+++ Behavior/instance/initialize-2D-release/superclass-3A-methodDictionary-3A-format-3A-.cs
@@ -0,0 +1,6 @@
+superclass: aClass methodDictionary: mDict format: fmt
+	"Basic initialization of the receiver.
+	Must only be sent to a new instance; else we would need Object flushCache."
+	superclass _ aClass.
+	format _ fmt.
+	methodDict _ mDict.
