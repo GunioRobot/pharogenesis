870c1fa6cd5ed027425070ebedea9c11dce80c42
sw <unknown>
1999-07-15 07:28:00 +0000

    no message

--- UpdatingStringMorph/instance/stepping/step.cs
+++ UpdatingStringMorph/instance/stepping/step.cs
@@ -1,7 +1,8 @@
 step
-
 	| s |
+	super step.
 	hasFocus ifFalse:
 		["update contents, but only if user isn't editing this string"
 		s _ self readFromTarget.
-		s = contents ifFalse: [self contentsClipped: s]].
+		s = contents ifFalse:
+			[self updateContentsFrom: s]]
