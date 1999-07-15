913a62f319db366e71e96fe7c6e5632f7467b48d
ar <unknown>
1999-07-15 13:49:00 +0000

    no message

---
+++ ClassBuilder/instance/validation/validateInstvars-3A-from-3A-forSuper-3A-.cs
@@ -0,0 +1,34 @@
+validateInstvars: instVarArray from: oldClass forSuper: newSuper
+	"Check if any of the instVars of oldClass conflict with the new superclass"
+	| instVars usedNames temp |
+	instVarArray isEmpty ifTrue:[^true]. "Okay"
+
+	"Validate the inst var names"
+	usedNames _ instVarArray asSet.
+	usedNames size = instVarArray size 
+		ifFalse:[	instVarArray do:[:var|
+					usedNames remove: var ifAbsent:[temp _ var]].
+				self error: temp,' is multiply defined'. ^false].
+	(usedNames includesAnyOf: self reservedNames) 
+		ifTrue:[	self reservedNames do:[:var|
+					(usedNames includes: var) ifTrue:[temp _ var]].
+				self error: temp,' is a reserved name'. ^false].
+
+	newSuper == nil ifFalse:[
+		usedNames _ newSuper allInstVarNames asSet.
+		instVarArray do:[:iv|
+			(usedNames includes: iv) ifTrue:[
+				newSuper withAllSuperclassesDo:[:cl|
+					(cl instVarNames includes: iv) ifTrue:[temp _ cl]].
+				self error: iv,' is already defined in ', temp name.
+				^false]]].
+	oldClass == nil ifFalse:[
+		usedNames _ Set new: 20.
+		oldClass allSubclassesDo:[:cl| usedNames addAll: cl instVarNames].
+		instVars _ instVarArray.
+		newSuper == nil ifFalse:[instVars _ instVars, newSuper allInstVarNames].
+		instVars do:[:iv|
+			(usedNames includes: iv) ifTrue:[
+				self error: iv, ' is already defined in a subclass of ', oldClass name.
+				^false]]].
+	^true
