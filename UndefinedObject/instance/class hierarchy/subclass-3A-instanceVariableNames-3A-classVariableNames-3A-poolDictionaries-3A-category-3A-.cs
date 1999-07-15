1382aad73075d5dfe073cfb8b10b9a566f97e680
ar <unknown>
1999-07-15 18:56:00 +0000

    no message

--- UndefinedObject/instance/class hierarchy/subclass-3A-instanceVariableNames-3A-classVariableNames-3A-poolDictionaries-3A-category-3A-.cs
+++ UndefinedObject/instance/class hierarchy/subclass-3A-instanceVariableNames-3A-classVariableNames-3A-poolDictionaries-3A-category-3A-.cs
@@ -3,13 +3,10 @@ subclass: nameOfClass  "Define root (superclass = nil) of a class hierarchy"
 	classVariableNames: classVarNames
 	poolDictionaries: poolDictnames
 	category: category
-	| newClass |
-	newClass _ Object subclass: nameOfClass  "First, define as a normal class"
-	instanceVariableNames: instVarNames
-	classVariableNames: classVarNames
-	poolDictionaries: poolDictnames
-	category: category.
-
-	Object removeSubclass: newClass.   "Then remove it from the old hierarchy"
-	newClass superclass: nil.
-	^ newClass
+	^(ClassBuilder new)
+		superclass: self
+		subclass: nameOfClass
+		instanceVariableNames: instVarNames
+		classVariableNames: classVarNames
+		poolDictionaries: poolDictnames
+		category: category
