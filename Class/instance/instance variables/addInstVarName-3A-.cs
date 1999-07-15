23dfede7f54a4d5abfa823db41f7880962a494a6
ar <unknown>
1999-07-15 18:56:00 +0000

    no message

--- Class/instance/instance variables/addInstVarName-3A-.cs
+++ Class/instance/instance variables/addInstVarName-3A-.cs
@@ -1,16 +1,11 @@
 addInstVarName: aString
 	"Add the argument, aString, as one of the receiver's instance variables."
-
-	superclass class
+	^(ClassBuilder new)
 		name: self name
-		inEnvironment: Smalltalk
+		inEnvironment: self environment
 		subclassOf: superclass
+		type: self typeOfClass
 		instanceVariableNames: self instanceVariablesString , aString
-		variable: self isVariable
-		words: self isWords
-		pointers: self isPointers
 		classVariableNames: self classVariablesString
 		poolDictionaries: self sharedPoolsString
 		category: self category
-		comment: nil
-		changed: false
