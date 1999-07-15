3223fe89215b7c5314f75c3d55969f926abafd8f
ar <unknown>
1999-07-15 18:56:00 +0000

    no message

--- Metaclass/instance/initialize-2D-release/instanceVariableNames-3A-.cs
+++ Metaclass/instance/initialize-2D-release/instanceVariableNames-3A-.cs
@@ -1,18 +1,5 @@
 instanceVariableNames: instVarString 
 	"Declare additional named variables for my instance."
-	| newMeta invalid |
-	newMeta _ self copyForValidation.
-	invalid _ newMeta
-				subclassOf: superclass
-				oldClass: self
-				instanceVariableNames: instVarString
-				variable: false
-				words: true
-				pointers: true
-				ifBad: [^false].
-	(invalid or: [instVarString ~= self instanceVariablesString])
-		ifTrue: [newMeta validateFrom: self
-					in: Smalltalk
-					instanceVariableNames: true
-					methods: true.
-				Smalltalk changes changeClass: self]
+	^(ClassBuilder new)
+		class: self
+		instanceVariableNames: instVarString
