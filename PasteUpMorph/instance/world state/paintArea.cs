6c3fc4abb0cebd0cd004cceb6e015242bee7ad4e
di <unknown>
1999-07-15 09:51:00 +0000

    no message

---
+++ PasteUpMorph/instance/world state/paintArea.cs
@@ -0,0 +1,11 @@
+paintArea
+	"What rectangle should the user be allowed to create a new painting in??  An area beside the paintBox.  Allow playArea to override with its own bounds!  "
+
+	| playfield paintBoxBounds |
+	playfield _ self submorphNamed: 'playfield' ifNone: [nil].
+	playfield ifNotNil: [^ playfield bounds].
+
+	paintBoxBounds _ self paintBox bounds.
+	self firstHand targetOffset x < paintBoxBounds center x
+		ifTrue: [^ bounds topLeft corner: paintBoxBounds left@bounds bottom]   "paint on left side"
+		ifFalse: [^ paintBoxBounds right@bounds top corner: bounds bottomRight].  "paint on right side"
