42e9cb96189e5715fdb15d9cf9eb06bfbf2713a0
sw <unknown>
1999-07-16 16:43:00 +0000

    no message

--- ScriptEditorMorph/instance/buttons/dismiss.cs
+++ ScriptEditorMorph/instance/buttons/dismiss.cs
@@ -2,13 +2,14 @@ dismiss
 	| aMenu reply |
 	owner ifNil: [^ self].
 	scriptName ifNil: [^ self delete].  "ad hoc fixup for bkwrd compat"
-	(self isAnonymous and:
-			[(submorphs size > 3) or: [self scriptInstantiation status ~~ #normal]]) ifTrue:
-
-		[aMenu _ SelectionMenu selections: #('yes, name it' 'no, discard it' 'cancel').
-		reply _ aMenu startUpWithCaption: 'Do you want to give this
+	self isAnonymous
+		ifTrue:
+			[((submorphs size > 3) or: [self scriptInstantiation status ~~ #normal]) ifTrue:
+				[aMenu _ SelectionMenu selections: #('yes, name it' 'no, discard it' 'cancel').
+				reply _ aMenu startUpWithCaption: 'Do you want to give this
 script a name and save it? '.
-		(reply size == 0 or: [reply = 'cancel']) ifTrue: [^ self].
-		(reply = 'yes, name it') ifTrue: [^ self renameScript].
+				(reply isEmptyOrNil or: [reply = 'cancel']) ifTrue: [^ self].
+				(reply = 'yes, name it') ifTrue: [^ self renameScript]].
 		self actuallyDestroyScript].
+	(playerScripted isExpendableScript: scriptName) ifTrue: [playerScripted removeScript: scriptName].
 	self delete
