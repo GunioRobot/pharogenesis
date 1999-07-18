0937524765c16b016b8c220f685f1768f4448c5c
sw <unknown>
1999-07-18 21:12:00 +0000

    no message

---
+++ WordNet/instance/as yet unclassified/definition-3A-.cs
@@ -0,0 +1,15 @@
+definition: theWord
+	"look this word up in the basic way.  Return nil if there is trouble accessing the web site."
+	| doc |
+	word _ theWord.
+	doc _ HTTPSocket 
+		httpGetDocument: 'http://www.cogsci.princeton.edu/cgi-bin/webwn/' 
+		args: 'stage=1&word=', word.
+	replyHTML _ (doc isKindOf: MIMEDocument)
+		ifTrue:
+			[doc content]
+		ifFalse:
+			[nil].
+	"self parseReply."
+
+	^ replyHTML
