646b073a30529021908a704b49fc6c5a1055deb6
jsp <unknown>
1999-07-19 22:29:00 +0000

    no message

---
+++ AliceAction/instance/management/setLifetime-3A-andCondition-3A-.cs
@@ -0,0 +1,5 @@
+setLifetime: howlong andCondition: condition
+	"Sets how long the action should run, or the condition under which it should stop"
+
+	lifetime _ howlong.
+	stopCondition _ condition.
