f88e2e34fa7ec72acc03ce778520af7ad199ca8e
ar <unknown>
1999-07-20 16:03:00 +0000

    no message

---
+++ FlashCodec/instance/compressing fills/storeColor-3A-on-3A-.cs
@@ -0,0 +1,9 @@
+storeColor: color on: aStream
+	| pv |
+	pv _ color pixelValueForDepth: 32.
+	aStream 
+		nextPut: (pv digitAt: 1) asCharacter;
+		nextPut: (pv digitAt: 2) asCharacter;
+		nextPut: (pv digitAt: 3) asCharacter;
+		nextPut: (pv digitAt: 4) asCharacter.
+
