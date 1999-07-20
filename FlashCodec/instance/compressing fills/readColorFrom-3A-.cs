4e48c31ef63a643f274a2e57cc64d239d1049ad1
ar <unknown>
1999-07-20 16:05:00 +0000

    no message

---
+++ FlashCodec/instance/compressing fills/readColorFrom-3A-.cs
@@ -0,0 +1,7 @@
+readColorFrom: aStream
+	| pv |
+	pv _ stream next asciiValue +
+			(stream next asciiValue bitShift: 8) +
+				(stream next asciiValue bitShift: 16) +
+					(stream next asciiValue bitShift: 24).
+	^Color colorFromPixelValue: pv depth: 32
