20460a0c749594b83fa8b977ec597b4d9bedac9b
ar <unknown>
1999-07-30 16:57:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveMakePoint.cs
+++ Interpreter/instance/arithmetic primitives/primitiveMakePoint.cs
@@ -1,8 +1,21 @@
 primitiveMakePoint
-	| integerReceiver integerArgument |
-	successFlag _ true.
-	integerArgument _ self popInteger.
-	integerReceiver _ self popInteger.
-	successFlag
-		ifTrue: [self push: (self makePointwithxValue: integerReceiver yValue: integerArgument)]
-		ifFalse: [self checkIntegerResult: 0 from: 18  "will fail"]
+	| rcvr argument pt |
+	argument _ self stackValue: 0.
+	rcvr _ self stackValue: 1.
+	(self isIntegerObject: rcvr) ifTrue:[
+		(self isIntegerObject: argument)
+			ifTrue:[pt _ self makePointwithxValue: (self integerValueOf: rcvr)
+							yValue: (self integerValueOf: argument)]
+			ifFalse:[pt _ self makePointwithxValue: (self integerValueOf: rcvr)
+							yValue: 0.
+					"Above may cause GC!"
+					self storePointer: 1 ofObject: pt withValue: (self stackValue: 0)].
+	] ifFalse:[
+		(self isFloatObject: rcvr) ifFalse:[^self success: false].
+		pt _ self makePointwithxValue: 0 yValue: 0.
+		"Above may cause GC!"
+		self storePointer: 0 ofObject: pt withValue: (self stackValue: 1).
+		self storePointer: 1 ofObject: pt withValue: (self stackValue: 0).
+	].
+	self pop: 2.
+	self push: pt.
