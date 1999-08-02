cd5092e091adc364581bf10ac92377e8ea25e595
mir <unknown>
1999-08-02 10:34:00 +0000

    no message

--- TextMorph/instance/initialization/string-3A-fontName-3A-size-3A-.cs
+++ TextMorph/instance/initialization/string-3A-fontName-3A-size-3A-.cs
@@ -1,6 +1,3 @@
 string: aString fontName: aName size: aSize
 
-	self contentsWrapped: aString.
-	textStyle _ (TextStyle named: aName asSymbol) copy.
-	textStyle ifNil: [self halt: 'Error: font ', aName, ' not found.'].
-	text addAttribute: (TextFontChange fontNumber: (textStyle fontIndexOfSize: aSize))
+	self string: aString fontName: aName size: aSize wrap: true
