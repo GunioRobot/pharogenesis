e4e8d452971de4abb83b3b3b60d1b31d344b68f5
tk <unknown>
1999-08-03 13:25:00 +0000

    no message

---
+++ TilePadMorph/instance/miscellaneous/isOutsideRef-3A-.cs
@@ -0,0 +1,11 @@
+isOutsideRef: aParseNode
+	"Is this a reference to an outside Player, of the form (self class refUnscriptedPlayer1)?
+(MessageNode (VariableNode 'self') (SelectorNode 'class')) (SelectorNode 'refUnscriptedPlayer1')"
+
+	| rec |
+	((rec _ aParseNode receiver) isKindOf: MessageNode) ifFalse: [^ false].
+	rec receiver isSelfPseudoVariable ifFalse: [^ false].
+	rec selector key == #class ifFalse: [^ false].
+	aParseNode selector key numArgs = 0 ifFalse: [^ false].
+	(aParseNode selector key beginsWith: 'ref') ifFalse: [^ false].
+	^ true
