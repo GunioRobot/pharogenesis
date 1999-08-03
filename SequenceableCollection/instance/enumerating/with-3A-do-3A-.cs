d5abee98ef9d920f92b6b61c4c1dd2db592831ca
di <unknown>
1999-08-03 15:26:00 +0000

    no message

--- SequenceableCollection/instance/enumerating/with-3A-do-3A-.cs
+++ SequenceableCollection/instance/enumerating/with-3A-do-3A-.cs
@@ -1,5 +1,6 @@
 with: otherCollection do: twoArgBlock 
 	"Evaluate twoArgBlock with corresponding elements from this collection and otherCollection."
+	otherCollection size = self size ifFalse: [self error: 'otherCollection must be the same size'].
 	1 to: self size do:
 		[:index |
 		twoArgBlock value: (self at: index)
