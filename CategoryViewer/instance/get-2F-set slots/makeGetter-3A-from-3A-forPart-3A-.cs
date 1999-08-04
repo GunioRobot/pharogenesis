f2aacede904ffc99e177d6fc8d15b21fb1e3da18
tk <unknown>
1999-08-04 10:56:00 +0000

    no message

---
+++ CategoryViewer/instance/get-2F-set slots/makeGetter-3A-from-3A-forPart-3A-.cs
@@ -0,0 +1,25 @@
+makeGetter: evt from: aMorph forPart: args
+
+	| m selfTile selector aType firstArg |
+	(aType _ args last) == #unknown ifTrue: [^ self beep].
+
+	(#(colorSees isOverColor) includes: (firstArg _ args first))
+		ifFalse:
+			[m _ PhraseTileMorph new setSlotRefOperator: args first asSymbol type: aType]
+		ifTrue:
+			[(firstArg == #colorSees) ifTrue: [m _ self colorSeesPhrase].
+			(firstArg == #isOverColor) ifTrue: [m _ self seesColorPhrase]].
+
+	selfTile _ self tileForSelf bePossessive.
+	selfTile position: m firstSubmorph position.
+	m firstSubmorph addMorph: selfTile.
+
+	selector _ m submorphs at: 2.
+	(aType == #number) ifTrue:
+		[selector addSuffixArrow].
+	selector updateLiteralLabel.
+
+	m enforceTileColorPolicy.
+
+	owner ifNotNil: [self primaryHand attachMorph: m]
+		ifNil: [^ m].
