351cc74909b8ac3c2dd1262cbf65653b2ef41fd8
tk <unknown>
1999-08-13 17:50:00 +0000

    no message

--- SystemWindow/instance/panes/paneColor.cs
+++ SystemWindow/instance/panes/paneColor.cs
@@ -1,2 +1,7 @@
 paneColor
-	^ Color perform: model defaultBackgroundColor
+	Display depth > 2 ifTrue:
+		[model ifNotNil: [
+			model isInMemory ifTrue: [
+				^ Color colorFrom: model defaultBackgroundColor]].
+		paneMorphs isEmptyOrNil ifFalse: [^ paneMorphs first color]].
+	^ Color white
