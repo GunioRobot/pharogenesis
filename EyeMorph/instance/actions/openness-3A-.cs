66e65619ba2fdac6ea68bf4c54a8f207b5f0235a
len <unknown>
1999-08-24 01:18:00 +0000

    no message

---
+++ EyeMorph/instance/actions/openness-3A-.cs
@@ -0,0 +1,6 @@
+openness: aNumber
+	| previousCenter |
+	previousCenter _ self center.
+	self extent: self extent x @ (self extent x * 37.0 / 30.0 * aNumber) rounded.
+	self align: self center with: previousCenter.
+	(self containsPoint: self iris center) ifFalse: [self lookAtFront]
