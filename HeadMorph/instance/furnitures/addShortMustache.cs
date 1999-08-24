c38406986201018c871c47b43837c80345c5c18a
len <unknown>
1999-08-24 01:39:00 +0000

    no message

---
+++ HeadMorph/instance/furnitures/addShortMustache.cs
@@ -0,0 +1,10 @@
+addShortMustache
+	| mustache |
+	mustache _ CurveMorph
+		vertices: {29@1. 54@14. 30@11. 1@15}
+		color: self randomHairColor
+		borderWidth: 1
+		borderColor: Color black.
+	mustache extent: (mustache extent * (self width / mustache width * 0.5)) rounded.
+	mustache align: mustache center with: self face mustachePosition.
+	self addMorphFront: mustache
