fb5cc2ce19f0e4ae35e0724c3f2bb90e3f6074ef
len <unknown>
1999-08-24 01:40:00 +0000

    no message

---
+++ HeadMorph/instance/furnitures/addLargeMustache.cs
@@ -0,0 +1,10 @@
+addLargeMustache
+	| mustache |
+	mustache _ CurveMorph
+		vertices: {48@4. 75@3. 93@15. 48@9. 3@19. 17@5}
+		color: self randomHairColor
+		borderWidth: 1
+		borderColor: Color black.
+	mustache extent: (mustache extent * (self width / mustache width)) rounded.
+	mustache align: mustache center with: self face mustachePosition.
+	self addMorphFront: mustache
