bb2b86fbc9ab141de8fcdddd2204c8bf899d0c21
tk <unknown>
1999-09-01 13:42:00 +0000

    no message

--- Utilities/class/fetching updates/serverUrls.cs
+++ Utilities/class/fetching updates/serverUrls.cs
@@ -3,4 +3,14 @@ serverUrls
 #( ('squeak updates' ('url1' 'url2'))
     ('some other updates' ('url3' 'url4')))"
 
-	^ UpdateUrlLists first last
+	| list |
+	list _ UpdateUrlLists first last.
+
+	"If there is a dead server, return a copy with that server last" 
+	list clone withIndexDo: [:aName :ind |
+	(aName beginsWith: Socket deadServer) ifTrue: [
+		list _ list asOrderedCollection.	"and it's a copy"
+		list removeAt: ind.
+		list addLast: aName]].
+
+	^ list asArray
