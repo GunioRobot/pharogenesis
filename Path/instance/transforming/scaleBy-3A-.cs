0f5fca79a7e0066d32d0bbad15ad9245be1c88ff
jrm <unknown>
1999-09-01 21:26:00 +0000

    no message

--- Path/instance/transforming/scaleBy-3A-.cs
+++ Path/instance/transforming/scaleBy-3A-.cs
@@ -2,11 +2,8 @@ scaleBy: aPoint
 	"Answers a new Path scaled by aPoint. Does not affect the current data in 
 	this Path."
 
-	| newPath |
-	newPath _ Path new: self size.
+	| newPath | 
+	newPath _ self species new: self size. 
 	newPath form: self form.
-	collectionOfPoints do: 
-		[:element | 
-		newPath add: 
-				(aPoint x * element x) asInteger @ (aPoint y * element y) asInteger].
+	collectionOfPoints do: [:element | newPath add: (element scaleBy: aPoint)].
 	^newPath
