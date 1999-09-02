96adfa2d985c562c05064f0a33186ac9f8a37df9
di <unknown>
1999-09-02 08:42:00 +0000

    no message

--- String/instance/converting/asUnHtml.cs
+++ String/instance/converting/asUnHtml.cs
@@ -1,20 +1,22 @@
 asUnHtml
-	"Strip out all Html stuff (commands in angle brackets <>) and convert the characters &<> back to their real value.  Leave actual cr and tab as they were in text.  4/12/96 tk"
+	"Strip out all Html stuff (commands in angle brackets <>) and convert
+the characters &<> back to their real value.  Leave actual cr and tab as
+they were in text."
 	| in out char rest did |
 	in _ ReadStream on: self.
 	out _ WriteStream on: (String new: self size).
-	[in atEnd] whileFalse: [
-		in peek = $< ifTrue: [in unCommand].	"Absorb <...><...>"
-		(char _ in next) = $&
-			ifTrue: [
-				rest _ in upTo: $;.
-				did _ out position.
-				rest = 'lt' ifTrue: [out nextPut: $<].
-				rest = 'gt' ifTrue: [out nextPut: $>].
-				rest = 'amp' ifTrue: [out nextPut: $&].
-				did = out position ifTrue: [
-					self error: 'new HTML char encoding'.
-					"Please add it to this code"]]
-			ifFalse: [out nextPut: char].
+	[in atEnd] whileFalse:
+		[in peek = $<
+			ifTrue: [in unCommand] 	"Absorb <...><...>"
+			ifFalse: [(char _ in next) = $&
+						ifTrue: [rest _ in upTo: $;.
+								did _ out position.
+								rest = 'lt' ifTrue: [out nextPut: $<].
+								rest = 'gt' ifTrue: [out nextPut: $>].
+								rest = 'amp' ifTrue: [out nextPut: $&].
+								did = out position ifTrue: [
+									self error: 'new HTML char encoding'.
+									"Please add it to this code"]]
+						ifFalse: [out nextPut: char]].
 		].
 	^ out contents
