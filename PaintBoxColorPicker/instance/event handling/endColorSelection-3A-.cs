99ae811ba6a267fc092d8b00abbe7c4bbfd67b69
di <unknown>
1999-09-02 09:19:00 +0000

    no message

--- PaintBoxColorPicker/instance/event handling/endColorSelection-3A-.cs
+++ PaintBoxColorPicker/instance/event handling/endColorSelection-3A-.cs
@@ -2,8 +2,6 @@ endColorSelection: evt
 	"Update current color and report it to paint box."
 
 	self selectColor: evt.
-	(owner isKindOf: PaintBoxMorph)
-		ifTrue: [owner takeColorEvt: evt from: self].
 
 	"restore mouseLeave handling"
 	self on: #mouseLeave send: #delete to: self.
