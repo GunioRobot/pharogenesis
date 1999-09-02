637f46f55d94d99f8e4b94484c2534ac4f239fa5
di <unknown>
1999-09-02 09:25:00 +0000

    no message

--- PaintBoxMorph/instance/actions/takeColorEvt-3A-from-3A-.cs
+++ PaintBoxMorph/instance/actions/takeColorEvt-3A-from-3A-.cs
@@ -1,6 +1,3 @@
 takeColorEvt: evt from: colorPicker
-	"Accept a new color from the colorMemory.  Programs use currentColor: instead.  Do not do this before the picker has a chance to set its own color!"
 
-	currentColor _ colorPicker currentColor.
-	self showColor.
-	self colorable ifFalse: [self setAction: #paint:].	"User now thinking of painting"
+	^ self takeColorFrom: colorPicker
