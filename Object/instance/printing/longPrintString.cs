cad51f250779556d2626fb39754dd11dd5c43ced
sw <unknown>
1999-09-02 15:18:00 +0000

    no message

--- Object/instance/printing/longPrintString.cs
+++ Object/instance/printing/longPrintString.cs
@@ -1,7 +1,3 @@
 longPrintString
 	"Answer a String whose characters are a description of the receiver."
-
-	| aStream |
-	aStream _ WriteStream on: (String new: 100).
-	self longPrintOn: aStream.
-	^aStream contents
+	^ String streamContents: [:aStream | self longPrintOn: aStream]
