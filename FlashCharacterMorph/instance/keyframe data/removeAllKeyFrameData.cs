2cec7ec331bd7381e869789c8c2b12779f14f1c9
ar <unknown>
1999-09-03 15:20:00 +0000

    no message

---
+++ FlashCharacterMorph/instance/keyframe data/removeAllKeyFrameData.cs
@@ -0,0 +1,7 @@
+removeAllKeyFrameData
+	"Remove all of the keyframe data associated with this morph"
+	self removeColorMatrixData.
+	self removeDepthData.
+	self removeMatrixData.
+	self removeVisibleData.
+	self removeRatioData.
