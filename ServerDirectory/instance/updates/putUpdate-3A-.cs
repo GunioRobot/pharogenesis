d61a7e4f6d2b6dcdc30380f93b7b76abfd4381e3
ar <unknown>
1999-09-03 13:03:00 +0000

    no message

--- ServerDirectory/instance/updates/putUpdate-3A-.cs
+++ ServerDirectory/instance/updates/putUpdate-3A-.cs
@@ -1,27 +1,29 @@
-putUpdate: fileStrm
-	"Put this file out as an Update on the servers of my group.  Each version of the system has its own set of update files.  'updates.list' holds the master list.  Each update is a fileIn whose name begins with a number.  See Utilities class absorbUpdatesFromServer."
+putUpdate: fileStrm 
+	"Put this file out as an Update on the servers of my group.  Each version of the system has its own set of update files.  'updates.list' holds the master list.  Each update is a fileIn whose name begins with a number.  See Utilities class readServerUpdatesThrough:saveLocally:updateImage:."
 
-	| myServers updateStrm sequence newName myName response local restOfText seq |
+	| myServers updateStrm sequence newName myName response local restOfText seq fileContents |
 "	(ScheduledControllers scheduledControllers detect: [:each |
 		each model == Transcript] ifNone: [nil]) ifNil: [
 			^ self inform: 'Please open a Transcript window, and 
 then start putting out this update again.'].
 "
 	local _ fileStrm localName.
-	(local count: [:char | char == $.]) > 1 ifTrue: [
-		^ self inform: 'File name cannot have more than one period'].
-	(local at: 1) isDigit ifTrue: [
-		^ self inform: 'File name cannot begin with a number'].
-		"later offer to strip it off and put on new number"
-	(local findDelimiters: '%/* ' startingAt: 1) <= local size ifTrue: [
-		^ self inform: 'File name cannot contain % / * or space'].
+	fileStrm size = 0
+		ifTrue: [^ self inform: 'That file has zero bytes!  May have a new name.'].
+	fileContents _ fileStrm contentsOfEntireFile.
+	(fileContents includes: Character linefeed)
+		ifTrue: [self notify: 'That file contains linefeeds.
+Proceed if you know that this is okay (e.g. the file contains raw binary data).'].
+	fileStrm reset.
+	(self checkNames: (Array with: local)) ifFalse: [^ nil].	"illegal characters"
 	myName _ group ifNil: [self moniker] ifNotNil: [group key].
 	response _ (PopUpMenu labels: 'Install update\Cancel update' withCRs)
 		startUpWithCaption: 'Do you really want to broadcast the file ', local, 
 			'\to every Squeak user who updates from ' withCRs, myName, '?'.
 	response = 1 ifFalse: [^ nil].	"abort"
 
-	(myServers _ self checkServers) size = 0 ifTrue: [^ self].
+	self openGroup.
+	(myServers _ self checkServers) size = 0 ifTrue: [self closeGroup.  ^ self].
 	updateStrm _ myServers first getFileNamed: 'updates.list'.
 	"get last number and add 1"
 	sequence _ Utilities lastUpdateNum: updateStrm.
@@ -35,7 +37,7 @@ then start putting out this update again.'].
 		response _ (PopUpMenu labels: 'Make update for my older version\Cancel update' withCRs)
 			startUpWithCaption: 'This system, ', (Smalltalk at: #EToySystem) version,
 				' is not the latest version'.
-		response = 1 ifFalse: [^ nil].	"abort"
+		response = 1 ifFalse: [self closeGroup.  ^ nil].	"abort"
 		].
 	"append name to updates"
 	(updateStrm skip: -1; next) == Character cr ifFalse: [
@@ -44,10 +46,11 @@ then start putting out this update again.'].
 
 	myServers do: [:aServer |
 		fileStrm reset.	"reopen"
-		aServer putFile: fileStrm named: newName.
+		aServer putFile: fileStrm named: newName retry: true.
 		updateStrm reset.
-		aServer putFile: updateStrm named: 'updates.list'.
+		aServer putFile: updateStrm named: 'updates.list' retry: true.
 		Transcript cr; show: 'Update succeeded on server ', aServer moniker].
+	self closeGroup.
 		
 	Transcript cr; show: 'Be sure to test your new update!'; cr.
 	"rename the file locally (may fail)"
