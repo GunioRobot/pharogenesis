6dfc04a23a868ae4dd5c2b6b0100959bcf6f2233
tk <unknown>
1999-09-03 13:55:00 +0000

    no message

---
+++ ImageSegment/instance/testing/findOwnerMap-3A-.cs
@@ -0,0 +1,10 @@
+findOwnerMap: morphs
+	| st |
+	"Construct a string that has a printout of the owner chain for every morph in the list.  Need it as a string so not hold onto them."
+
+st _ ''.
+morphs do: [:mm |
+	(st includesSubString: mm printString) ifFalse: [
+		st _ st, '
+', mm withAllContainers printString]].
+Smalltalk at: #Owners put: st.
