b6af2eea04b374928aa1d1488832d737a7e37597
di <unknown>
1999-09-06 22:43:00 +0000

    no message

--- StringMorph/instance/drawing/drawOn-3A-.cs
+++ StringMorph/instance/drawing/drawOn-3A-.cs
@@ -1,4 +1,3 @@
 drawOn: aCanvas
 
-	hasFocus ifTrue: [aCanvas fillRectangle: self bounds color: Color yellow].
-	aCanvas text: contents bounds: bounds font: font color: color.
+	aCanvas text: contents bounds: bounds font: self fontToUse color: color.
