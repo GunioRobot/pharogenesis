564e288ad41ee3585ff180d034dfb1a0187e0157
di <unknown>
1999-09-07 08:44:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/spawnWorkspace.cs
+++ ParagraphEditor/instance/menu messages/spawnWorkspace.cs
@@ -6,6 +6,5 @@ spawnWorkspace
 		[toUse _ paragraph text string.
 		toUse size > 0 ifFalse: [^ self flash]].
 	"NB: BrowserCodeController's version does a cancel here"
-	self controlTerminate.
-	Utilities openScratchWorkspaceLabeled: 'Untitled' contents: toUse.
-	self controlInitialize.
+	self terminateAndInitializeAround:
+		[Utilities openScratchWorkspaceLabeled: 'Untitled' contents: toUse]
