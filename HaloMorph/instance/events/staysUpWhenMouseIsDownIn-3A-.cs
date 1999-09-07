c537b5df09713654c5fc84d576cf8cec554ac5e7
sw <unknown>
1999-09-07 15:43:00 +0000

    no message

--- HaloMorph/instance/events/staysUpWhenMouseIsDownIn-3A-.cs
+++ HaloMorph/instance/events/staysUpWhenMouseIsDownIn-3A-.cs
@@ -1,2 +1,4 @@
 staysUpWhenMouseIsDownIn: aMorph
-	^ (aMorph == target) or: [submorphs includes: aMorph]
+	^ ((aMorph == target) or: [submorphs includes: aMorph]) or:
+		["name under edit, special case"
+		(aMorph isKindOf: StringMorphEditor) and: [submorphs includes: aMorph owner]]
