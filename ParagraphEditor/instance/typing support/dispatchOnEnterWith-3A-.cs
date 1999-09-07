0cc789d1ed52dd53b9ded839f797d4a3357c7cc7
di <unknown>
1999-09-07 11:26:00 +0000

    no message

--- ParagraphEditor/instance/typing support/dispatchOnEnterWith-3A-.cs
+++ ParagraphEditor/instance/typing support/dispatchOnEnterWith-3A-.cs
@@ -2,12 +2,12 @@ dispatchOnEnterWith: typeAheadStream
 	"Enter key hit.  Treat is as an 'accept', viz a synonym for cmd-s.  If cmd key is down, treat is as a synonym for print-it. "
 
 	sensor keyboard.  "consume enter key"
+	self terminateAndInitializeAround: [
 	sensor commandKeyPressed
 		ifTrue:
 			[self printIt.]
 		ifFalse: 
 			[self closeTypeIn: typeAheadStream.
-			self controlTerminate.
-			self accept.
-			self controlInitialize].
+			self accept].
+	].
 	^ true
