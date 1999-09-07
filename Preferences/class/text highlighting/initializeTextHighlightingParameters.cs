db707c112a254afa3bf5b6465498c12182b0bf3a
sw <unknown>
1999-09-07 13:07:00 +0000

    no message

---
+++ Preferences/class/text highlighting/initializeTextHighlightingParameters.cs
@@ -0,0 +1,4 @@
+initializeTextHighlightingParameters
+	"Preferences initializeTextHighlightingParameters"
+	Parameters at: #insertionPointColor put: (Color r: 0.4 g: 1.0 b: 0).
+	Parameters at: #textHighlightColor put: (Color r: 0.4 g: 1.0 b: 0).
