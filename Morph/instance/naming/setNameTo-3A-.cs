cf63fc39489d43d02e14bdd116e4264aaa47ecc6
sw <unknown>
1999-09-07 16:31:00 +0000

    no message

--- Morph/instance/naming/setNameTo-3A-.cs
+++ Morph/instance/naming/setNameTo-3A-.cs
@@ -1,2 +1,5 @@
 setNameTo: aName
-	self setNamePropertyTo: aName
+	| nameToUse nameString |
+	nameToUse _ aName ifNotNil:
+		[(nameString _ aName asString) size > 0 ifTrue: [nameString] ifFalse: ['É']].
+	self setNamePropertyTo: nameToUse "no Texts here!"
