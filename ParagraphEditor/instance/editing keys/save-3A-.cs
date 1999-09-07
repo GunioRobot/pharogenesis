20b55b1f61fbf791b73b93820776b9b3a1d7e120
di <unknown>
1999-09-07 08:43:00 +0000

    no message

--- ParagraphEditor/instance/editing keys/save-3A-.cs
+++ ParagraphEditor/instance/editing keys/save-3A-.cs
@@ -1,10 +1,8 @@
-save: characterStream 
+save: characterStream
 	"Submit the current text.  Equivalent to 'accept' 1/18/96 sw
 	 Keeps typeahead."
 
 	sensor keyboard.		"flush character"
 	self closeTypeIn: characterStream.
-	self controlTerminate.
-	self accept.
-	self controlInitialize.
+	self terminateAndInitializeAround: [self accept].
 	^ true
