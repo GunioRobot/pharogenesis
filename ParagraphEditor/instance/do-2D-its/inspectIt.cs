efadd79fc3fd33100a74c408593e43fbd71451c2
di <unknown>
1999-09-07 11:25:00 +0000

    no message

--- ParagraphEditor/instance/do-2D-its/inspectIt.cs
+++ ParagraphEditor/instance/do-2D-its/inspectIt.cs
@@ -1,9 +1,7 @@
 inspectIt
 	"1/13/96 sw: minor fixup"
 	| result |
-	self controlTerminate.
 	result _ self evaluateSelection.
 	((result isKindOf: FakeClassPool) or: [result == #failedDoit])
 			ifTrue: [view flash]
 			ifFalse: [result inspect].
-	self controlInitialize
