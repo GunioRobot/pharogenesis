a28a95c7e1ec445cb24c8caa4ac31112d62f23b6
sw <unknown>
1999-09-08 18:18:00 +0000

    no message

--- TileMorph/instance/private/updateLiteralLabel.cs
+++ TileMorph/instance/private/updateLiteralLabel.cs
@@ -1,12 +1,12 @@
 updateLiteralLabel
-	"now works for operator tiles also"
 
 	|  desiredW leader myLabel |
 	(myLabel _ self labelMorph) ifNil: [^ self].
-	myLabel contents:
+
+	myLabel acceptValue:
 		(type == #literal
 			ifTrue:
-				[literal stringForReadout] 
+				[literal] 
 			ifFalse: [operatorReadoutString 
 				ifNil:		[ScriptingSystem wordingForOperator: operatorOrExpression]
 				ifNotNil:  	[operatorReadoutString]]).
@@ -15,7 +15,8 @@ updateLiteralLabel
 	desiredW _ leader + myLabel width.
 	suffixArrow ifNotNil: [desiredW _ desiredW + SuffixArrowAllowance].
 	self extent: (desiredW max: self minimumWidth) @ self class defaultH.
-	myLabel position: (self left + (leader + 0)) @ (bounds top + 5); fullBounds.
+
+	myLabel position: (self left + (leader + 0)) @ (bounds top + 3); fullBounds.
 	suffixArrow ifNotNil: [suffixArrow align: suffixArrow topRight with:
 				bounds topRight + (-2 @ (self height // 2)) - (0 @ (suffixArrow height // 2))].
 	self changed
