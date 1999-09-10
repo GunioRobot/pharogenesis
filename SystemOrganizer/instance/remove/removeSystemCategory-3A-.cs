0584032d6668ee5a6e85dcc59459dc41eac02508
di <unknown>
1999-09-10 09:37:00 +0000

    no message

--- SystemOrganizer/instance/remove/removeSystemCategory-3A-.cs
+++ SystemOrganizer/instance/remove/removeSystemCategory-3A-.cs
@@ -1,5 +1,5 @@
 removeSystemCategory: category
 	"remove all the classes associated with the category"
 
-	(self superclassOrder: category) do: [:class | class removeFromSystem].
+	(self superclassOrder: category) reverseDo: [:class | class removeFromSystem].
 	self removeEmptyCategories
