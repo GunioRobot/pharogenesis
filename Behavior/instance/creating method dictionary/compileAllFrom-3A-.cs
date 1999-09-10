b1f8726039101faf30822429868b34d488db8d64
di <unknown>
1999-09-10 15:53:00 +0000

    no message

--- Behavior/instance/creating method dictionary/compileAllFrom-3A-.cs
+++ Behavior/instance/creating method dictionary/compileAllFrom-3A-.cs
@@ -2,5 +2,6 @@ compileAllFrom: oldClass
 	"Compile all the methods in the receiver's method dictionary.
 	This validates sourceCode and variable references and forces
 	all methods to use the current bytecode set"
-
-	self selectorsDo: [:sel | self recompile: sel from: oldClass]
+	"ar 7/10/1999: Use oldClass selectors not self selectors"
+	oldClass selectorsDo: [:sel | self recompile: sel from: oldClass].
+	Smalltalk currentProjectDo: [:proj | proj compileAllIsolated: self from: oldClass].
