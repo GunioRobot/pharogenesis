4fae3ffc7fe29fb9566adbd679c045d27d4b8422
ar <unknown>
1999-09-10 14:58:00 +0000

    no message

---
+++ B3DPrimitiveRasterizer/instance/processing/processIndexedQuads-3A-.cs
@@ -0,0 +1,5 @@
+processIndexedQuads: vb
+	"Process an indexed quad set"
+	| objSize |
+	objSize _ self primObjectSize + (vb vertexCount + 1 * PrimVertexSize) + (vb indexCount).
+	self addPrimitiveObject: vb ofSize: objSize.
