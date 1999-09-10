64dc24624f8fd0fba5852240f31dca885a1bc2df
ar <unknown>
1999-09-10 17:34:00 +0000

    no message

--- Class/instance/initialize-2D-release/obsolete.cs
+++ Class/instance/initialize-2D-release/obsolete.cs
@@ -1,8 +1,11 @@
 obsolete
-	"Change the receiver to an obsolete class by changing its name to have
-	the prefix -AnObsolete-."
-
+	"Change the receiver and all of its subclasses to an obsolete class."
+	self == Object 
+		ifTrue:[^self error:'Object is NOT obsolete'].
 	name _ 'AnObsolete' , name.
-	classPool _ Dictionary new.
+	Object class instSize + 1 to: self class instSize do:
+		[:i | self instVarAt: i put: nil]. "Store nil over class instVars."
+	classPool _ nil.
+	sharedPools _ nil.
 	self class obsolete.
-	super obsolete
+	super obsolete.
