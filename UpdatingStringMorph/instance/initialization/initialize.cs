6a1c43b7d78d0fb8eb47e30d34a87af18fc88167
sw <unknown>
1999-09-10 09:59:00 +0000

    no message

--- UpdatingStringMorph/instance/initialization/initialize.cs
+++ UpdatingStringMorph/instance/initialization/initialize.cs
@@ -2,4 +2,10 @@ initialize
 
 	super initialize.
 	format _ #default.  "formats: #string, #default"
-	target _ getSelector _ putSelector _ nil.
+	target _ getSelector _ putSelector _  nil.
+	floatPrecision _ 1.
+	growable _ true.
+	stepTime _ 50.
+	autoAcceptOnFocusLoss _ true.
+	minimumWidth _ 8.
+	maximumWidth _ 300.
