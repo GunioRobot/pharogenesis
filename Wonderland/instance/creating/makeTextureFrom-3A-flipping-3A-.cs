ca717db4a802ff275af4846991b463357fe31edc
ar <unknown>
1999-09-14 20:01:00 +0000

    no message

---
+++ Wonderland/instance/creating/makeTextureFrom-3A-flipping-3A-.cs
@@ -0,0 +1,11 @@
+makeTextureFrom: filename flipping: aBool
+	"Create a texture from the given file name and flip it in y if requested"
+	| tex |
+	filename == nil ifTrue:[^nil].
+	^self getSharedTextureDict at: filename ifAbsentPut:[
+		tex _ (Form fromBMPFileNamed: filename) asTexture.
+		aBool ifTrue:[tex flipVertically].
+		tex wrap: true.
+		tex interpolate: true.
+		tex envMode: 0.
+	].
