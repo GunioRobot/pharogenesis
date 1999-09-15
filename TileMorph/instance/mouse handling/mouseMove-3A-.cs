1aeb1faed1c96f3f17b47c77bed25a647cb77db7
sw <unknown>
1999-09-15 14:21:00 +0000

    no message

--- TileMorph/instance/mouse handling/mouseMove-3A-.cs
+++ TileMorph/instance/mouse handling/mouseMove-3A-.cs
@@ -1,10 +1,22 @@
 mouseMove: evt
+	| aPoint label |
 	"See if arrows are being pressed and call arrowAction:..."
 
 	upArrow ifNotNil:
-		[(upArrow containsPoint: evt cursorPoint) ifTrue:
-			[self readyForAnotherTick ifTrue: [^ self arrowAction: 1]].
-		(downArrow containsPoint: evt cursorPoint) ifTrue:
-			[self readyForAnotherTick ifTrue: [^ self arrowAction: -1]]].
+		[aPoint _ evt cursorPoint.
+		(label _ self labelMorph) ifNotNil:
+			[label step. literal _ label valueFromContents].
+		(upArrow containsPoint: aPoint) ifTrue:
+			[self abandonLabelFocus.
+			self variableDelay:
+				[self arrowAction: 1.
+				self resizeToFitLabel].
+			^ evt hand noteSignificantEvent: evt].
+		(downArrow containsPoint: aPoint) ifTrue:
+			[self abandonLabelFocus.
+			self variableDelay:
+				[self arrowAction: -1.
+				self resizeToFitLabel].
+			^ evt hand noteSignificantEvent: evt]].
 
 	super mouseMove: evt.
