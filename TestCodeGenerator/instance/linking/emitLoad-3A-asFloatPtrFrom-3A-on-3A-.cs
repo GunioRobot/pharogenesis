b4f8bdc48cc5be4af977beba3189b629aafa38d6
acg <unknown>
1999-09-17 01:43:00 +0000

    no message

---
+++ TestCodeGenerator/instance/linking/emitLoad-3A-asFloatPtrFrom-3A-on-3A-.cs
@@ -0,0 +1,9 @@
+emitLoad: aString asFloatPtrFrom: anInteger on: aStream
+
+	aStream
+		nextPutAll: aString;
+		nextPutAll: 	' = (float *) interpreterProxy->firstIndexableField(';
+		crtab: 2;
+		nextPutAll: 	'interpreterProxy->stackValueOf(';
+		nextPutAll: anInteger asString;
+		nextPutAll: '))'
