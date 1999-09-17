dfce73b6b9a22541bf8da39434efcfd4443f7160
mpw <unknown>
1999-09-17 21:36:00 +0000

    no message

---
+++ PostscriptCanvas/instance/drawing/text-3A-at-3A-font-3A-color-3A-justified-3A-parwidth-3A-.cs
@@ -0,0 +1,14 @@
+text: s at:point font: fontOrNil color: c justified:justify parwidth:parwidth
+	self setFont:(fontOrNil ifNil:[self defaultFont]).
+	self comment:' text color: ',c printString.
+	self setColor:c.
+	self comment:'  origin ',  origin printString.
+     self moveto: point.
+	target print:' (';
+     	 print:s asPostscript; print:') '.
+		justify ifTrue:[
+			target write:parwidth; print:' jshow'; cr.
+		] ifFalse:[
+			target print:'show'.
+		].
+		target cr.
