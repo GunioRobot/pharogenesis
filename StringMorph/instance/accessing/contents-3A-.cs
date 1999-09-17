b2dda427c974e0e35bff625a7aa739e1aae173cc
sw <unknown>
1999-09-17 16:29:00 +0000

    no message

--- StringMorph/instance/accessing/contents-3A-.cs
+++ StringMorph/instance/accessing/contents-3A-.cs
@@ -1,5 +1,10 @@
-contents: aString
+contents: newContents
 
-	contents = aString ifTrue: [^ self].  "no substantive change"
-	contents _ aString.
+	newContents isText
+		ifTrue: [emphasis _ newContents emphasisAt: 1.
+				contents _ newContents string]
+		ifFalse: [contents = newContents ifTrue: [^ self].  "no substantive change"
+				contents _ newContents].
 	self fitContents.
+	self changed
+
