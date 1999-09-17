327e6a0ca976a8682e2c5be610883217b8892cb7
acg <unknown>
1999-09-17 18:18:00 +0000

    no message

---
+++ TestTMethod/instance/transforming/removeFinalSelfReturn.cs
@@ -0,0 +1,13 @@
+removeFinalSelfReturn
+	"The Smalltalk parser automatically adds the statement '^self' to the end of methods without explicit returns. This method removes such statements, since the generated code has no notion of 'self' anyway."
+
+	| stmtList lastStmt |
+	stmtList _ parseTree statements asOrderedCollection.
+	lastStmt _ stmtList last.
+
+	((lastStmt isReturn) and:
+	 [(lastStmt expression isVariable) and:
+	 ['self' = lastStmt expression name]]) ifTrue: [
+		stmtList removeLast.
+		parseTree setStatements: stmtList.
+	].
