a632144d7b2d5124ce19ae3c97009a36243a5ab5
bf <unknown>
1999-09-18 20:01:00 +0000

    no message

--- ScreenController/instance/menu messages/setDisplayDepth.cs
+++ ScreenController/instance/menu messages/setDisplayDepth.cs
@@ -1,6 +1,6 @@
 setDisplayDepth
 	"Let the user choose a new depth for the display. "
 	| result |
-	(result _ (SelectionMenu selections: #('1' '2' '4' '8' '16' '32')) startUpWithCaption: 'Choose a display depth
+	(result _ (SelectionMenu selections: Display supportedDisplayDepths) startUpWithCaption: 'Choose a display depth
 (it is currently ' , Display depth printString , ')') == nil ifFalse:
-		[Display newDepth: result asNumber]
+		[Display newDepth: result]
