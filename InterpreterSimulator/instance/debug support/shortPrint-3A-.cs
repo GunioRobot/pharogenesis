879f4d982391974dab6257b3bf1db427ad85fa30
tk <unknown>
1999-09-18 16:33:00 +0000

    no message

--- InterpreterSimulator/instance/debug support/shortPrint-3A-.cs
+++ InterpreterSimulator/instance/debug support/shortPrint-3A-.cs
@@ -1,14 +1,16 @@
 shortPrint: oop
 	| name classOop |
-	(self isIntegerObject: oop) ifTrue: [^ '=' , (self integerValueOf: oop) printString , ' (' , (self integerValueOf: oop) hex , ')'].
+	(self isIntegerObject: oop) ifTrue: [^ '=' , (self integerValueOf: oop) printString , 
+		' (' , (self integerValueOf: oop) hex , ')'].
 	classOop _ self fetchClassOf: oop.
-	(self sizeBitsOf: classOop) =16r20 ifTrue: [^ 'class ' , (self nameOfClass: oop)].
+	(self sizeBitsOf: classOop) = (Metaclass instSize +1*4) ifTrue: [
+		^ 'class ' , (self nameOfClass: oop)].
 	name _ self nameOfClass: classOop.
 	name size = 0 ifTrue: [name _ '??'].
 	name = 'String' ifTrue: [^ (self stringOf: oop) printString].
 	name = 'Symbol' ifTrue: [^ '#' , (self stringOf: oop)].
-	name = 'Character' ifTrue: [^ '=' ,
-			(Character value: (self integerValueOf: (self fetchPointer: 0 ofObject: oop))) printString].
+	name = 'Character' ifTrue: [^ '=' , (Character value: (self integerValueOf: 
+				(self fetchPointer: 0 ofObject: oop))) printString].
 	name = 'UndefinedObject' ifTrue: [^ 'nil'].
 	name = 'False' ifTrue: [^ 'false'].
 	name = 'True' ifTrue: [^ 'true'].
