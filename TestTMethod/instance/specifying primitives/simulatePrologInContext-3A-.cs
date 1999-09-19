133b74d3ff39142c9f5a736d2a4aaaeb8ba9fab0
acg <unknown>
1999-09-19 14:06:00 +0000

    no message

---
+++ TestTMethod/instance/specifying primitives/simulatePrologInContext-3A-.cs
@@ -0,0 +1,28 @@
+simulatePrologInContext: aContext
+
+	|cg instructions |
+	cg _ TestCodeGenerator new.
+	parmSpecs keysAndValuesDo: 
+		[:index :each |
+		 instructions _ ((parmSpecs at: index)
+			ccg: cg 
+			prolog: (cg ccgTVarBlock: index) 
+			expr: '<foo>' 
+			index: args size - index).
+		 Compiler new 
+			evaluate: instructions
+			in: aContext 
+			to: aContext receiver
+			notifying: nil
+			ifFail: nil].
+	instructions _ (rcvrSpec
+		ccg: cg 
+		prolog: [:expr | '^', expr]
+		expr: '<foo>' 
+		index: args size).
+	 ^Compiler new 
+		evaluate: instructions
+		in: aContext 
+		to: aContext receiver
+		notifying: nil
+		ifFail: nil
