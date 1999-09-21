ebbb3dd4e8aae0bbc850a94df25fc938bb167a7e
acg <unknown>
1999-09-21 01:29:00 +0000

    no message

---
+++ TestOSAPlugin/class/as yet unclassified/declareCVarsIn-3A-.cs
@@ -0,0 +1,7 @@
+declareCVarsIn: cg
+
+	cg addHeaderFile: '<AppleEvents.h>'.
+	cg addHeaderFile: '<AppleScript.h>'.
+	cg addHeaderFile: '<OSA.h>'.
+	cg addHeaderFile: '<OSAGeneric.h>'.
+	cg addHeaderFile: '<Script.h>'.
