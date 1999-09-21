d2ec1ae0f2beb999c8cf9a52fcf1140badf3e6a6
bf <unknown>
1999-09-21 12:09:00 +0000

    no message

---
+++ MagnifierMorph/instance/menu/chooseMagnification-3A-.cs
@@ -0,0 +1,10 @@
+chooseMagnification: evt
+	| handle origin aHand currentMag |
+	currentMag _ magnification.
+	aHand _ evt ifNil: [self currentHand] ifNotNil: [evt hand].
+	origin _ aHand gridPointRaw y.
+	handle _ HandleMorph new forEachPointDo:
+		[:newPoint | self magnification: (newPoint y - origin) / 8.0 + currentMag].
+	aHand attachMorph: handle.
+	handle startStepping.
+	self changed. "Magnify handle"
