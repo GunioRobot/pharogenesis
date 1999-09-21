e154ba67afc9e21234c26801face4c547d16c7da
sw <unknown>
1999-09-21 12:13:00 +0000

    no message

--- Inspector/instance/menu commands/inspectorKey-3A-from-3A-.cs
+++ Inspector/instance/menu commands/inspectorKey-3A-from-3A-.cs
@@ -2,5 +2,7 @@ inspectorKey: aChar from: view
 	"respond to a Command key.  Got here from a list of fields being inspected"
 
 	aChar == $i ifTrue: [self selection inspect].
+	aChar == $I ifTrue: [self selection explore].
+	aChar == $b ifTrue:	[self browseMethodFull].
 	aChar == $c ifTrue: [self copyName].
 	^ self arrowKey: aChar from: view
