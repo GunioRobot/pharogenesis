9d7625f7056799f59df5474a1d8152ee75b76410
tk <unknown>
1999-09-21 22:31:00 +0000

    no message

---
+++ Project/class/utilities/storeAllInSegments.cs
@@ -0,0 +1,13 @@
+storeAllInSegments
+	"Write out all Projects in this Image.
+	Project storeAllInSegments.		"
+
+	| all ff ll |
+all _ Project allInstances.
+Transcript show: 'Initial Space Left: ', (ff _ Smalltalk garbageCollect) printString; cr.
+all do: [:proj |
+	Transcript show: proj name; cr.
+	proj storeSegment  "storeSegmentNoFile"].
+Transcript show: 'After writing all: ', (ll _ Smalltalk garbageCollect) printString; cr.
+Transcript show: 'Space gained: ', (ll - ff) printString; cr.
+"some will come back in"
