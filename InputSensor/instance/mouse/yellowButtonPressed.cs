42922c67dd53d4b106f0c1a22b455f85fa8db3e4
bf <unknown>
1999-09-22 12:43:00 +0000

    no message

--- InputSensor/instance/mouse/yellowButtonPressed.cs
+++ InputSensor/instance/mouse/yellowButtonPressed.cs
@@ -1,4 +1,4 @@
 yellowButtonPressed
-	"Answer whether only the yellow mouse button is being pressed."
+	"Answer whether only the yellow mouse button is being pressed. This is the second mouse button or option+click on the Mac."
 
 	^self buttons = 2
