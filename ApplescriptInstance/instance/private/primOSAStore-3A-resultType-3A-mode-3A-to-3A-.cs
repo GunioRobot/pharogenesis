6612408cc22946411b72af5f9b71e39abac90470
acg <unknown>
1999-09-22 04:22:00 +0000

    no message

---
+++ ApplescriptInstance/instance/private/primOSAStore-3A-resultType-3A-mode-3A-to-3A-.cs
@@ -0,0 +1,6 @@
+primOSAStore: a resultType: b mode: c to: d
+
+	<primitive: 'primOSAStore' module: 'TestOSAPlugin'>
+	^TestOSAPlugin 
+		doPrimitive: 'primOSAStore:resultType:mode:to:'
+		withArguments: {a. b. c. d}
