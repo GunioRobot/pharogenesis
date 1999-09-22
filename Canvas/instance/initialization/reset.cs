282161c1adb3249d74fb6f39cd073dcf398ce4ba
di <unknown>
1999-09-22 19:21:00 +0000

    no message

--- Canvas/instance/initialization/reset.cs
+++ Canvas/instance/initialization/reset.cs
@@ -1,5 +1,4 @@
 reset
+	"Reset the canvas."
 
-	origin _ 0@0.							"origin of the top-left corner of this cavas"
-	clipRect _ (0@0 corner: 10000@10000).		"default clipping rectangle"
-	shadowDrawing _ false.					"draw translucent shadows when true"
+	super initWithTarget:self class defaultTarget.
