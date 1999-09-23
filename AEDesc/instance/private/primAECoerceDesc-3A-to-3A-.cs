5686af35087cadbde73223d63704676251cbada9
acg <unknown>
1999-09-23 22:51:00 +0000

    no message

---
+++ AEDesc/instance/private/primAECoerceDesc-3A-to-3A-.cs
@@ -0,0 +1,6 @@
+primAECoerceDesc: typeCode to: result
+
+	<primitive: 'primAECoerceDesc' module: 'TestOSAPlugin'>
+	^TestOSAPlugin 
+		doPrimitive: 'primAECoerceDesc:to:'
+		withArguments: {typeCode. result}
