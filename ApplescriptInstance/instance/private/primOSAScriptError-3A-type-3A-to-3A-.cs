3e2c1e5f5a346302f3c8d693b9e366d0d330a2f4
acg <unknown>
1999-09-23 20:43:00 +0000

    no message

---
+++ ApplescriptInstance/instance/private/primOSAScriptError-3A-type-3A-to-3A-.cs
@@ -0,0 +1,6 @@
+primOSAScriptError: anOSType type: aDescType to: anAEDesc
+
+	<primitive: 'primOSAScriptError' module: 'TestOSAPlugin'>
+	^TestOSAPlugin 
+		doPrimitive: 'primOSAScriptError:type:to:'
+		withArguments: {anOSType. aDescType. anAEDesc}
