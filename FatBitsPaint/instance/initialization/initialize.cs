95c0a07fe7f40c98d7b281a732ce97a055401fb8
RAA <unknown>
1999-09-25 22:27:00 +0000

    no message

--- FatBitsPaint/instance/initialization/initialize.cs
+++ FatBitsPaint/instance/initialization/initialize.cs
@@ -1,10 +1,11 @@
 initialize
 
-	super initialize.
-	formToEdit _ Form extent: 50@40 depth: 8.
-	formToEdit fill: formToEdit boundingBox fillColor: Color veryVeryLightGray.
-	magnification _ 4.
-	color _ Color veryVeryLightGray.
-	brushColor _ Color red.
-	brushSize _ 3.
-	self revert.
+        super initialize.
+        self setCurrentToolTo: self toolsForPaintBrush.
+        formToEdit _ Form extent: 50@40 depth: 8.
+        formToEdit fill: formToEdit boundingBox fillColor: Color veryVeryLightGray.
+        brushSize _ magnification _ 4.
+        color _ Color veryVeryLightGray.
+        brushColor _ Color red.
+        backgroundColor _ Color white.
+        self revert
