6104ac1ef33bab513d8812caec35242494e87b2e
RAA <unknown>
1999-09-25 11:42:00 +0000

    no message

--- ColorPickerMorph/instance/initialization/initialize.cs
+++ ColorPickerMorph/instance/initialization/initialize.cs
@@ -1,10 +1,21 @@
 initialize
-
-	super initialize.
-	self form: ColorChart deepCopy.
-	selectedColor _ Color white.
-	sourceHand _ nil.
-	deleteOnMouseUp _ true.
-	updateContinuously _ true.
-	selector _ nil.
-	target _ nil.
+        super initialize.
+        theSelectorDisplayMorph _ AlignmentMorph newRow
+                color: Color white;
+                borderWidth: 1;
+                borderColor: Color red;
+                hResizing: #shrinkWrap;
+                vResizing: #shrinkWrap;
+                addMorph: (StringMorph contents: 'theSelector').
+        self addMorph: theSelectorDisplayMorph.
+        self buildChartForm.
+        self addMorph: (SimpleButtonMorph new borderWidth: 0;
+                        label: 'x' font: nil; color: Color transparent;
+                        actionSelector: #delete; target: self;
+                        position: 1@0; extent: 10@9).
+        selectedColor _ Color white.
+        sourceHand _ nil.
+        deleteOnMouseUp _ false.
+        updateContinuously _ true.
+        selector _ nil.
+        target _ nil
