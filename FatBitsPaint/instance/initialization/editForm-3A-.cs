103aeee07a3c86775492f9c82959896993f3f1ce
RAA <unknown>
1999-09-25 15:54:00 +0000

    no message

--- FatBitsPaint/instance/initialization/editForm-3A-.cs
+++ FatBitsPaint/instance/initialization/editForm-3A-.cs
@@ -1,4 +1,5 @@
 editForm: aForm
 
-	formToEdit _ aForm.
-	self revert.
+        formToEdit _ aForm.
+        brushSize _ magnification _ 64 // (aForm height min: aForm width) max: 4.
+        self revert
