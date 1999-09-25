e2126c9fec75f60aad9882660ea148bba3a1c4a5
RAA <unknown>
1999-09-25 15:49:00 +0000

    no message

--- FatBitsPaint/instance/menu/magnification-3A-.cs
+++ FatBitsPaint/instance/menu/magnification-3A-.cs
@@ -1,10 +1,10 @@
 magnification: aNumber
 
-	| oldPenSize oldForm |
-	oldPenSize _ brushSize / magnification.
-	oldForm _ self form shrink: self form boundingBox by: magnification.
-	magnification _ aNumber asInteger max: 1.
-	self form: (oldForm magnify: oldForm boundingBox by: magnification).
-	brush _ Pen newOnForm: originalForm.
-	self penSize: oldPenSize.
-	brush color: brushColor.
+        | oldPenSize oldForm |
+        oldPenSize _ brushSize / magnification.
+        oldForm _ self unmagnifiedForm.
+        magnification _ aNumber asInteger max: 1.
+        self form: (oldForm magnify: oldForm boundingBox by: magnification).
+        brush _ Pen newOnForm: originalForm.
+        self penSize: oldPenSize.
+        brush color: brushColor
