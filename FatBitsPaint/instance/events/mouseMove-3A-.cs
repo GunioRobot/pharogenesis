ed0022bc08cb38f167b38fe227cf98c31e4417fd
RAA <unknown>
1999-09-25 15:24:00 +0000

    no message

--- FatBitsPaint/instance/events/mouseMove-3A-.cs
+++ FatBitsPaint/instance/events/mouseMove-3A-.cs
@@ -1,16 +1,5 @@
 mouseMove: evt
 
-	| p p2 damageRect |
-	p _ self griddedPoint: evt.
-	lastMouse = p ifTrue: [^ self].
-	lastMouse ifNil: [lastMouse _ p].  "first point in a stroke"
-	"draw etch-a-sketch style-first horizontal, then vertical"
-	p2 _ p x@lastMouse y.
-	brush drawFrom: lastMouse to: p2.
-	brush drawFrom: p2 to: p.
-			
-	damageRect _ 
-		((lastMouse min: p) - brush sourceForm extent) corner:
-		((lastMouse max: p) + brush sourceForm extent).
-	self invalidRect: (damageRect translateBy: self position).
-	lastMouse _ p.
+        ^ self
+                perform: (currentTools at: #mouseMove: ifAbsent: [^nil])
+                with: evt
