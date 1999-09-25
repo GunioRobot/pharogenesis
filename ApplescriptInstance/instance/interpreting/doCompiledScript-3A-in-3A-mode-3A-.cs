5a0ef5f7f479dbccf3a9b24ab20376e7f337c63a
acg <unknown>
1999-09-25 14:14:00 +0000

    no message

---
+++ ApplescriptInstance/instance/interpreting/doCompiledScript-3A-in-3A-mode-3A-.cs
@@ -0,0 +1,4 @@
+doCompiledScript: aCompiledApplescriptData in: contextOSAID mode: anInteger
+	"Answer text result of executing Applescript aString in context contexOSAID in mode: anInteger"
+
+	^self valueOf: aCompiledApplescriptData in: contextOSAID mode: anInteger
