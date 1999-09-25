94712d963446f06497339be77a0f1b4053257b95
RAA <unknown>
1999-09-25 21:17:00 +0000

    no message

--- FatBitsPaint/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ FatBitsPaint/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,9 +1,16 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph
 
-	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
-	aCustomMenu add: 'pen color' action: #setPenColor:.
-	aCustomMenu add: 'pen size' action: #setPenSize:.
-	aCustomMenu add: 'fill' action: #fill.
-	aCustomMenu add: 'magnification' action: #setMagnification:.
-	aCustomMenu add: 'accept' action: #accept.
-	aCustomMenu add: 'revert' action: #revert.
+        super addCustomMenuItems: aCustomMenu hand: aHandMorph.
+        aCustomMenu 
+                add: 'background color' action: #setBackgroundColor:;
+                add: 'pen color' action: #setPenColor:;
+                add: 'pen size' action: #setPenSize:;
+                add: 'fill' action: #fill;
+                add: 'magnification' action: #setMagnification:;
+                add: 'accept' action: #accept;
+                add: 'revert' action: #revert;
+                add: 'inspect' action: #inspectForm;
+                add: 'file out' action: #fileOut;
+                add: 'selection...' action: #selectionMenu:;
+                add: 'tools...' action: #toolMenu:;
+                yourself
