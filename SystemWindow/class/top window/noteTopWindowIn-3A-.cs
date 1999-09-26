b62faa64cc13ee9f615359cebc72be5ba8708b01
di <unknown>
1999-09-26 22:48:00 +0000

    no message

--- SystemWindow/class/top window/noteTopWindowIn-3A-.cs
+++ SystemWindow/class/top window/noteTopWindowIn-3A-.cs
@@ -1,7 +1,14 @@
 noteTopWindowIn: aWorld
+	| newTop |
 	"TopWindow must be nil or point to the top window in this project."
 	TopWindow _ nil.
 	aWorld ifNil: [^ self].
+	newTop _ nil.
 	aWorld submorphsDo:
-		[:m | ((m isKindOf: SystemWindow) and: [m activeOnlyOnTop])
-			ifTrue: [^ m activate]]
+		[:m | (m isKindOf: SystemWindow) ifTrue:
+			[(newTop == nil and: [m activeOnlyOnTop])
+				ifTrue: [newTop _ m].
+			(m model isKindOf: Project)
+				ifTrue: ["This really belongs in a special ProjWindow class"
+						m label ~= m model name ifTrue: [m setLabel: m model name]]]].
+	newTop == nil ifFalse: [newTop activate]
