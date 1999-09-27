0722030d601b9e87fbfda9a602173019ee33a064
sw <unknown>
1999-09-27 11:42:00 +0000

    no message

---
+++ ParagraphEditor/instance/menu messages/changeEmphasisOrAlignment.cs
@@ -0,0 +1,15 @@
+changeEmphasisOrAlignment
+	| aList reply  |
+	aList _ #(plain bold italic narrow underlined struckOut leftFlush centered rightFlush justified).
+	reply _ (SelectionMenu labelList: aList lines: #(6) selections: aList) startUp.
+	reply ~~ nil ifTrue:
+		[(#(leftFlush centered rightFlush justified) includes: reply)
+			ifTrue:
+				[paragraph perform: reply.
+				self recomputeInterval]
+			ifFalse:
+				[self setEmphasis: reply.
+				paragraph composeAll.
+				self recomputeSelection.
+				self mvcRedisplay]].
+	^ true
