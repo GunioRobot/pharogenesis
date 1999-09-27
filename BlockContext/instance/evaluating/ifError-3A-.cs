e170050a394d04da572fe5d78b64feff99a5254a
bf <unknown>
1999-09-27 16:50:00 +0000

    no message

--- BlockContext/instance/evaluating/ifError-3A-.cs
+++ BlockContext/instance/evaluating/ifError-3A-.cs
@@ -3,17 +3,20 @@ ifError: errorHandlerBlock
 	"Examples:
 		[1 whatsUpDoc] ifError: [:err :rcvr | ^ 'huh?'].
 		[1 / 0] ifError: [:err :rcvr |
-			'division by 0' = err
-				ifTrue: [^ Float inf]
+			'ZeroDivide' = err
+				ifTrue: [^ Float infinity]
 				ifFalse: [self error: err]]
 "
-
 	| lastHandler val activeProcess |
 	activeProcess _ Processor activeProcess.
 	lastHandler _ activeProcess errorHandler.
 	activeProcess errorHandler: [:aString :aReceiver |
 		activeProcess errorHandler: lastHandler.
 		^ errorHandlerBlock value: aString value: aReceiver].
-	val _ self value.
+	val _ self on: Error do: [:ex |
+		activeProcess errorHandler: lastHandler.
+		^errorHandlerBlock value: ex description value: ex receiver].
 	activeProcess errorHandler: lastHandler.
 	^ val
+
+
