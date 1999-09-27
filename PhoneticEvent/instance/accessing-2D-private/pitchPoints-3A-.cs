af8cd717de991690ce6f0cf8564e5e512874f5c7
len <unknown>
1999-09-27 23:06:00 +0000

    no message

---
+++ PhoneticEvent/instance/accessing-2D-private/pitchPoints-3A-.cs
@@ -0,0 +1,4 @@
+pitchPoints: p
+	pitchPoints _ p isNumber
+		ifTrue: [((0.0 to: duration by: 0.035) collect: [ :time | time @ p])]
+		ifFalse: [p first isPoint ifTrue: [p] ifFalse: [(p collect: [ :each | each first @ each last])]]
