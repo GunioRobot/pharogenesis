4016f546fd5874d265b7846d972cc76c981f6be6
sw <unknown>
1999-09-28 17:04:00 +0000

    no message

---
+++ ClassDescription/instance/instance variables/chooseInstVarAlphabeticallyThenDo-3A-.cs
@@ -0,0 +1,12 @@
+chooseInstVarAlphabeticallyThenDo: aBlock
+	| allVars index |
+	"Put up a menu of all the instance variables in the receiver, presented in alphabetical order, and when the user chooses one, evaluate aBlock with the chosen variable as its parameter."
+
+	allVars _ self allInstVarNames asSortedArray.
+	allVars isEmpty ifTrue: [^ self inform: 'There are no
+instance variables'].
+
+	index _ (PopUpMenu labelArray: allVars lines: #()) startUpWithCaption: 'Instance variables in
+', self name.
+	index = 0 ifTrue: [^ self].
+	aBlock value: (allVars at: index)
