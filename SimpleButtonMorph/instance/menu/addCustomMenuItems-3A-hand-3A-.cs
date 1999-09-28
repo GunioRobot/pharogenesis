e53c83bdf0175a36353856cdb3b161fef82b55a2
sw <unknown>
1999-09-28 20:42:00 +0000

    no message

--- SimpleButtonMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ SimpleButtonMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,9 +1,14 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph
 
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
-	aCustomMenu add: 'change label' action: #setLabel.
-	aCustomMenu add: 'change action selector' action: #setActionSelector.
-	aCustomMenu add: 'change arguments' action: #setArguments.
-	aCustomMenu add: 'change when to act' action: #setActWhen.
-	((self world rootMorphsAt: aHandMorph targetOffset) size > 1) ifTrue: [
-		aCustomMenu add: 'set target' action: #setTarget:].
+	self addLabelItemsTo: aCustomMenu hand: aHandMorph.
+	(target isKindOf: BookMorph)
+		ifTrue:
+			[aCustomMenu add: 'set page sound' action: #setPageSound:.
+			aCustomMenu add: 'set page visual' action: #setPageVisual:]
+		ifFalse:
+			[aCustomMenu add: 'change action selector' action: #setActionSelector.
+			aCustomMenu add: 'change arguments' action: #setArguments.
+			aCustomMenu add: 'change when to act' action: #setActWhen.
+			((self world rootMorphsAt: aHandMorph targetOffset) size > 1) ifTrue:
+				[aCustomMenu add: 'set target' action: #setTarget:]].
