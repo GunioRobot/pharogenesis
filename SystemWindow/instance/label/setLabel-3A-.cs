cbcd66d6f6670f3fb91ad49eef40a37f67df0713
sw <unknown>
1999-09-29 07:25:00 +0000

    no message

--- SystemWindow/instance/label/setLabel-3A-.cs
+++ SystemWindow/instance/label/setLabel-3A-.cs
@@ -1,3 +1,11 @@
 setLabel: aString
 	labelString _ aString.
-	label ifNotNil: [label contents: aString]
+	label ifNil: [^ self].
+	label contents: aString.
+	self labelWidgetAllowance.  "Sets it if not already"
+	self isCollapsed
+		ifTrue: [self extent: (label width + labelWidgetAllowance) @ (self labelHeight + 2)]
+		ifFalse: [label fitContents; setWidth: (label width min: bounds width - labelWidgetAllowance).
+				label align: label bounds topCenter with: bounds topCenter + (0@borderWidth).
+				collapsedFrame ifNotNil:
+					[collapsedFrame _ collapsedFrame withWidth: label width + labelWidgetAllowance]]
