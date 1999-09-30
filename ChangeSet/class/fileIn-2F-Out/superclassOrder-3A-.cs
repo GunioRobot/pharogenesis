d25a22ff394c3d64abc84480173ab03a3ccb0662
RAA <unknown>
1999-09-30 14:34:00 +0000

    no message

--- ChangeSet/class/fileIn-2F-Out/superclassOrder-3A-.cs
+++ ChangeSet/class/fileIn-2F-Out/superclassOrder-3A-.cs
@@ -2,25 +2,23 @@ superclassOrder: classes
 	"Arrange the classes in the collection, classes, in superclass order so the 
 	classes can be properly filed in."
 
-	| all list i aClass superClass |
+	| all list i aClass |
 	list _ classes copy. 			"list is indexable"
 	all _ OrderedCollection new: list size.
-	[list size > 0]
-		whileTrue: 
-			[aClass _ list first.
-			superClass _ aClass superclass.
-			"Make sure it doesn't have an as yet uncollected superclass"
-			[superClass == nil or: [list includes: superClass]]
-				whileFalse: [superClass _ superClass superclass].
-			i _ 1.
-			[superClass == nil]
-				whileFalse: 
-					[i _ i + 1.
-					aClass _ list at: i.
-					superClass _ aClass superclass.
-					"check as yet uncollected superclass"
-					[superClass == nil or: [list includes: superClass]]
-						whileFalse: [superClass _ superClass superclass]].
-			all addLast: aClass.
-			list _ list copyWithout: aClass].
+	[list size > 0] whileTrue: [
+		i _ 0.
+		[
+			i _ i + 1.
+			aClass _ list at: i.
+			(list includesAnyOf: aClass allSuperclasses) or: [
+				aClass isMeta and: [
+					(list includes: aClass soleInstance) or: [
+						list includesAnyOf: aClass soleInstance allSuperclasses
+					] 
+				].
+			].
+		] whileTrue.
+		all addLast: aClass.
+		list _ list copyWithout: aClass
+	].
 	^all
