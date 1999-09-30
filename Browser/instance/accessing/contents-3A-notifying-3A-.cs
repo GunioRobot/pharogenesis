a1c53528d8ff0b27e37272eb7f526da4b40ec0cb
sw <unknown>
1999-09-30 13:19:00 +0000

    no message

--- Browser/instance/accessing/contents-3A-notifying-3A-.cs
+++ Browser/instance/accessing/contents-3A-notifying-3A-.cs
@@ -1,30 +1,38 @@
 contents: input notifying: aController 
-	"The retrieved information has changed and its source must now be 
-	updated. The information can be a variety of things, depending on the 
-	list selections (such as templates for class or message definition, methods) 
-	or the user menu commands (such as definition, comment, hierarchy). 
-	Answer the result of updating the source."
+	"The retrieved information has changed and its source must now be
+	 updated. The information can be a variety of things, depending on
+	 the list selections (such as templates for class or message definition,
+	 methods) or the user menu commands (such as definition, comment,
+	 hierarchy).  Answer the result of updating the source."
+
 	| aString aText theClass |
+	self changed: #annotation.
 	aString _ input asString.
 	aText _ input asText.
-
-	editSelection == #editSystemCategories 
-		ifTrue: [^ self changeSystemCategories: aString].
-	editSelection == #editClass | (editSelection == #newClass) 
-		ifTrue: [^ self defineClass: aString notifying: aController].
-	editSelection == #editComment 
-		ifTrue: [theClass _ self selectedClass.
-				theClass ifNil: [PopUpMenu notify: 'You must select a class
+	editSelection == #editSystemCategories ifTrue: [^ self changeSystemCategories: aString].
+	editSelection == #editClass | (editSelection == #newClass) ifTrue: [^ self defineClass: aString notifying: aController].
+	editSelection == #editComment
+		ifTrue: 
+			[theClass _ self selectedClass.
+			theClass
+				ifNil: 
+					[PopUpMenu notify: 'You must select a class
 before giving it a comment.'.
-				^ false].
-				theClass comment: aText. ^ true].
+					^ false].
+			theClass comment: aText stamp: Utilities changeStamp.
+			^ true].
 	editSelection == #hierarchy ifTrue: [^ true].
-	editSelection == #editMessageCategories 
-		ifTrue: [^ self changeMessageCategories: aString].
-	editSelection == #editMessage | (editSelection == #newMessage) 
-		ifTrue: [^ self defineMessage: aText notifying: aController].
+	editSelection == #editMessageCategories ifTrue: [^ self changeMessageCategories: aString].
+	editSelection == #editMessage | (editSelection == #newMessage)
+		ifTrue:
+			[^ self okayToAccept
+				ifFalse:
+					[false]
+				ifTrue:
+					[self compileMessage: aText notifying: aController]].
 	editSelection == #none
-		ifTrue: [PopUpMenu notify: 'This text cannot be accepted
+		ifTrue: 
+			[PopUpMenu notify: 'This text cannot be accepted
 in this part of the browser.'.
-				^ false].
+			^ false].
 	self error: 'unacceptable accept'
