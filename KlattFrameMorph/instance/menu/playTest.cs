156e06f6a1b561466dade1d16f6a396f2d517ec3
len <unknown>
1999-10-01 01:31:00 +0000

    no message

---
+++ KlattFrameMorph/instance/menu/playTest.cs
@@ -0,0 +1,5 @@
+playTest
+	| synth |
+	synth _ KlattSynthesizer new.
+	synth millisecondsPerFrame: 1000; cascade: 8.
+	(SampledSound samples: (synth samplesFromFrames: {frame}) samplingRate: synth samplingRate) play
