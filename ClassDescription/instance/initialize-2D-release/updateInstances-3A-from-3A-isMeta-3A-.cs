5b562bf63cff1c048df3cd8e5057a4409aa3d3c1
tk <unknown>
1999-10-04 09:43:00 +0000

    no message

---
+++ ClassDescription/instance/initialize-2D-release/updateInstances-3A-from-3A-isMeta-3A-.cs
@@ -0,0 +1,19 @@
+updateInstances: oldInstances from: oldClass isMeta: isMeta
+	"Recreate any existing instances of the argument, oldClass, as instances of the receiver, which is a newly changed class. Permute variables as necessary."
+	"If there are any contexts having an old instance as receiver it might crash the system because the layout has changed, and the method only knows about the old layout."
+	| map variable instSize newInstances |
+
+	oldInstances isEmpty ifTrue:[^self]. "no instances to convert"
+	isMeta ifTrue: [
+		oldInstances size = 1 ifFalse:[^self error:'Metaclasses can only have one instance'].
+		self soleInstance class == self ifTrue:[
+			^self error:'Metaclasses can only have one instance']].
+	map _ self instVarMappingFrom: oldClass.
+	variable _ self isVariable.
+	instSize _ self instSize.
+	newInstances _ Array new: oldInstances size.
+	1 to: oldInstances size do:[:i|
+		newInstances at: i put: (
+			self newInstanceFrom: (oldInstances at: i) variable: variable size: instSize map: map)].
+	"Now perform a bulk mutation of old instances into new ones"
+	oldInstances elementsExchangeIdentityWith: newInstances.
