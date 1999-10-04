a4693bab8d20f0f48c190e3318f365e6231e59e0
bf <unknown>
1999-10-04 16:47:00 +0000

    no message

--- TrashCanMorph/instance/event handling/mouseDown-3A-.cs
+++ TrashCanMorph/instance/event handling/mouseDown-3A-.cs
@@ -1,4 +1,15 @@
 mouseDown: evt
-
-	self state: #pressed.
-	self openTrash.
+	| paintBox palette |
+	self currentHand endDisplaySuppression.
+	"See if a stamp is being dropped into the trash. It is not held by the hand."
+	(paintBox _ self findActivePaintBox) ifNotNil: [
+		paintBox getSpecial == #stamp: ifTrue: [
+			paintBox deleteCurrentStamp.  "throw away stamp..."
+			self primaryHand showTemporaryCursor: nil.
+			^ self]].	  "... and don't open trash"
+	palette _ self standardPalette.
+	((palette notNil and: [palette isInWorld]) and: [palette hasScrapsTab])
+		ifTrue:
+			[palette showScrapsTab]
+		ifFalse:
+			[self currentHand openScrapsBook]
