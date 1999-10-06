1e755bf5283139b5a7fea691006953437187ed21
sw <unknown>
1999-10-06 20:29:00 +0000

    no message

---
+++ HaloMorph/instance/private/addGraphicalHandleFrom-3A-at-3A-.cs
@@ -0,0 +1,8 @@
+addGraphicalHandleFrom: formKey at: aPoint
+	"Add the supplied form as a graphical handle centered at the given point.  Return the handle."
+	| handle aForm |
+	aForm _ (ScriptingSystem formAtKey: formKey) ifNil: [ScriptingSystem formAtKey: #SolidMenu].
+	handle _ ImageMorph new image: aForm; bounds: (Rectangle center: aPoint extent: aForm extent).
+	self addMorph: handle.
+	handle on: #mouseUp send: #endInteraction to: self.
+	^ handle
