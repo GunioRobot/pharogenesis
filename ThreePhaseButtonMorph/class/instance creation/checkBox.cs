76b49549d09ac07ea0911934234f4159bb463b6a
bf <unknown>
1999-10-08 15:23:00 +0000

    no message

---
+++ ThreePhaseButtonMorph/class/instance creation/checkBox.cs
@@ -0,0 +1,9 @@
+checkBox
+	"Answer a button pre-initialized with checkbox images."
+	| f |
+	^self new
+		onImage: (f _ ScriptingSystem formAtKey: 'CheckBoxOn');
+		pressedImage: (ScriptingSystem formAtKey: 'CheckBoxPressed');
+		offImage: (ScriptingSystem formAtKey: 'CheckBoxOff');
+		extent: f extent + (2@0);
+		yourself
