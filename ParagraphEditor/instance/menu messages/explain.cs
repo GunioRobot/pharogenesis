a4fc8c8c04e8d51bb98e80b9006c231066e0b256
ls <unknown>
1999-10-10 11:36:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/explain.cs
+++ ParagraphEditor/instance/menu messages/explain.cs
@@ -1,12 +1,16 @@
 explain
-	"Try to shed some light on what kind of entity the current selection is. 
-	The selection must be a single token or construct. Insert the answer after 
+	"Try to shed some light on what kind of entity the current selection
+is. 
+	The selection must be a single token or construct. Insert the answer
+after 
 	the selection. Send private messages whose names begin with 'explain' 
 	that return a string if they recognize the selection, else nil."
 
-	| string tiVars cgVars selectors delimitors numbers sorry reply |
+	| string tiVars cgVars selectors delimitors numbers sorry reply symbol
+|
 Cursor execute showWhile: 
-			[sorry _ '"Sorry, I can''t explain that.  Please select a single token, construct, or special character.'.
+			[sorry _ '"Sorry, I can''t explain that.  Please select a single
+token, construct, or special character.'.
 			sorry _ sorry , (view canDiscardEdits
 							ifFalse: ['  Also, please cancel or accept."']
 							ifTrue: ['"']).
@@ -14,7 +18,8 @@ Cursor execute showWhile:
 				ifTrue: [reply _ '']
 				ifFalse: [string _ self explainScan: string.
 					"Remove space, tab, cr"
-					"Temps and Instance vars need only test strings that are all letters"
+					"Temps and Instance vars need only test strings that are all
+letters"
 					(string detect: [:char | (char isLetter or: [char isDigit]) not]
 						ifNone: []) ifNil: 
 							[tiVars _ self explainTemp: string.
@@ -26,7 +31,7 @@ Cursor execute showWhile:
 						ifFalse: [tiVars _ tiVars , '\' withCRs].
 					"Context, Class, Pool, and Global vars, and Selectors need 
 					only test symbols"
-					(Symbol hasInterned: string ifTrue: [:symbol | symbol])
+					(Symbol hasInterned: string ifTrue: [:s | symbol _ s])
 						ifTrue: [cgVars _ self explainCtxt: symbol.
 							cgVars == nil
 								ifTrue: [cgVars _ self explainClass: symbol.
