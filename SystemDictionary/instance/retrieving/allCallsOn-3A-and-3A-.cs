57fc7d71af2b8a24258731e0c5d86fed25ac5c6b
ls <unknown>
1999-10-10 13:22:00 +0000

    no message

--- SystemDictionary/instance/retrieving/allCallsOn-3A-and-3A-.cs
+++ SystemDictionary/instance/retrieving/allCallsOn-3A-and-3A-.cs
@@ -1,16 +1,22 @@
 allCallsOn: firstLiteral and: secondLiteral
-	"Answer a SortedCollection of all the methods that call on both aLiteral 
+	"Answer a SortedCollection of all the methods that call on both
+aLiteral 
 	and secondLiteral."
 
-	| aCollection secondArray firstSpecial secondSpecial |
+	| aCollection secondArray firstSpecial secondSpecial firstByte
+secondByte |
 	aCollection _ SortedCollection new.
-	firstSpecial _ self hasSpecialSelector: firstLiteral ifTrueSetByte: [:firstByte].
-	secondSpecial _ self hasSpecialSelector: secondLiteral ifTrueSetByte: [:secondByte].
+	firstSpecial _ self hasSpecialSelector: firstLiteral ifTrueSetByte: [:b
+| firstByte _ b].
+	secondSpecial _ self hasSpecialSelector: secondLiteral ifTrueSetByte:
+[:b | secondByte _ b].
 	Cursor wait showWhile:
 		[self allBehaviorsDo:
 			[:class |
-			secondArray _ class whichSelectorsReferTo: secondLiteral special: secondSpecial byte: secondByte.
-			((class whichSelectorsReferTo: firstLiteral special: firstSpecial byte: firstByte) select:
+			secondArray _ class whichSelectorsReferTo: secondLiteral special:
+secondSpecial byte: secondByte.
+			((class whichSelectorsReferTo: firstLiteral special: firstSpecial
+byte: firstByte) select:
 				[:aSel | (secondArray includes: aSel)]) do:
 						[:sel | aCollection add: class name , ' ' , sel]]].
 	^aCollection
