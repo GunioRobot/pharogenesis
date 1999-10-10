d1bb1f35bab8d3b155cefc574da0c16775e280e3
sw <unknown>
1999-10-10 23:26:00 +0000

    no message

--- PluggableTextMorph/instance/drawing/drawOn-3A-.cs
+++ PluggableTextMorph/instance/drawing/drawOn-3A-.cs
@@ -1,6 +1,10 @@
 drawOn: aCanvas 
-	"Include a thin red inset border for unaccepted edits"
+	"Include a thin red inset border for unaccepted edits, or, if the unaccepted edits are known to conflict with a change made somewhere else to the same method (typically), put a thick red frame"
 
-	super drawOn: aCanvas.
-	self hasUnacceptedEdits ifTrue:
-		[aCanvas frameRectangle: self innerBounds width: 1 color: Color red]
+	super drawOn: aCanvas. 
+	self hasEditingConflicts
+		ifTrue:
+			[aCanvas frameRectangle: self innerBounds width: 3 color: Color red] 
+		ifFalse:
+			[self hasUnacceptedEdits ifTrue:
+				[aCanvas frameRectangle: self innerBounds width: 1 color: Color red]]
