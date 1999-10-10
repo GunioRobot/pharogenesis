e9ed3987c33530f63aeb82510458c453ee3ab317
ls <unknown>
1999-10-10 13:22:00 +0000

    no message

--- Behavior/instance/testing method dictionary/whichSelectorsReferTo-3A-.cs
+++ Behavior/instance/testing method dictionary/whichSelectorsReferTo-3A-.cs
@@ -1,8 +1,10 @@
 whichSelectorsReferTo: literal 
-	"Answer a Set of selectors whose methods access the argument as a literal."
+	"Answer a Set of selectors whose methods access the argument as a
+literal."
 
-	| special |
-	special _ Smalltalk hasSpecialSelector: literal ifTrueSetByte: [:byte ].
+	| special byte |
+	special _ Smalltalk hasSpecialSelector: literal ifTrueSetByte: [:b |
+byte _ b].
 	^self whichSelectorsReferTo: literal special: special byte: byte
 
 	"Rectangle whichSelectorsReferTo: #+."
