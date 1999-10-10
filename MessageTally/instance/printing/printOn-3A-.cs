b5e479e645dd7f92809f0a10a5fa5ed280f3c6bd
ls <unknown>
1999-10-10 11:56:00 +0000

    no message

--- MessageTally/instance/printing/printOn-3A-.cs
+++ MessageTally/instance/printing/printOn-3A-.cs
@@ -1,6 +1,6 @@
 printOn: aStream
-	| aSelector className |
-	aSelector _ class selectorAtMethod: method setClass: [:aClass].
+	| aSelector className aClass |
+	aSelector _ class selectorAtMethod: method setClass: [:c | aClass _ c].
 	className _ aClass name contractTo: 30.
 	aStream nextPutAll: className; nextPutAll: ' >> ';
 			nextPutAll: (aSelector contractTo: 60-className size)
