d28ce96b62ce47e00e6f755490e922bef5ea4d6d
ls <unknown>
1999-10-10 13:22:00 +0000

    no message

--- SystemDictionary/instance/retrieving/allCallsOn-3A-.cs
+++ SystemDictionary/instance/retrieving/allCallsOn-3A-.cs
@@ -1,9 +1,13 @@
 allCallsOn: aLiteral   "Smalltalk browseAllCallsOn: #open:label:."
 	"Answer a Collection of all the methods that call on aLiteral."
-	| aCollection special thorough aList |
-	#(23 48 'fred' (new open:label:)) size.    "For testing!"
+	| aCollection special thorough aList byte |
+
+	#(23 48 'fred' (new open:label:)) size.
+		"Example above should find #open:label:, though it is deeply embedded
+here."
 	aCollection _ OrderedCollection new.
-	special _ self hasSpecialSelector: aLiteral ifTrueSetByte: [:byte ].
+	special _ self hasSpecialSelector: aLiteral ifTrueSetByte: [:b | byte _
+b ].
 	thorough _ (aLiteral isMemberOf: Symbol)
 				and: ["Possibly search for symbols imbedded in literal arrays"
 					Preferences thoroughSenders].
@@ -12,9 +16,11 @@ allCallsOn: aLiteral   "Smalltalk browseAllCallsOn: #open:label:."
 			[:class |
 				aList _ thorough
 					ifTrue:
-			 			[(class thoroughWhichSelectorsReferTo: aLiteral special: special byte: byte)]
+			 			[(class thoroughWhichSelectorsReferTo: aLiteral special: special
+byte: byte)]
 					ifFalse:
-						[class whichSelectorsReferTo: aLiteral special: special byte: byte].
+						[class whichSelectorsReferTo: aLiteral special: special byte:
+byte].
 				aList do: 
 					[:sel | sel ~~ #DoIt
 						ifTrue: [aCollection add: class name , ' ' , sel]]]].
