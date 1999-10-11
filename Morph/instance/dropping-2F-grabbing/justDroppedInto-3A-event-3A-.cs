30d7909cd88d5856dd4d7532c5f8fa8ac717ecda
sw <unknown>
1999-10-11 13:20:00 +0000

    no message

--- Morph/instance/dropping-2F-grabbing/justDroppedInto-3A-event-3A-.cs
+++ Morph/instance/dropping-2F-grabbing/justDroppedInto-3A-event-3A-.cs
@@ -1,4 +1,12 @@
 justDroppedInto: aMorph event: anEvent
 	"This message is sent to a dropped morph after it has been dropped on--and been accepted by--a drop-sensitive morph"
 
-	self removeProperty: #partsDonor
+	| aWindow partsBinCase |
+	(partsBinCase _ aMorph isPartsBin) ifFalse:
+		[self isPartsDonor: false].
+	(aWindow _ aMorph ownerThatIsA: SystemWindow) ifNotNil:
+		[aWindow isActive ifFalse:
+			[aWindow activate]].
+	(self isInWorld and: [partsBinCase not]) ifTrue:
+		[self world startSteppingSubmorphsOf: self]  
+	"Note an unhappy inefficiency here:  the startStepping... call will often have already been called in the sequence leading up to entry to this method, but unfortunately the isPartsDonor: call often will not have already happened, with the result that the startStepping... call will not have resulted in the startage of the steppage."
