f04eb7cd1ac17cc795b729a3d968b7dfc094165f
bf <unknown>
1999-10-12 18:07:00 +0000

    no message

--- Form/instance/image manipulation/dominantColor.cs
+++ Form/instance/image manipulation/dominantColor.cs
@@ -1,6 +1,8 @@
 dominantColor
 	| tally max maxi |
+	depth > 16 ifTrue:
+		[^(self asFormOfDepth: 16) dominantColor].
 	tally _ self tallyPixelValues.
 	max _ maxi _ 0.
-	tally withIndexDo: [:n :i | n > max ifTrue: [maxi _ i]].
+	tally withIndexDo: [:n :i | n > max ifTrue: [max _ n. maxi _ i]].
 	^ Color colorFromPixelValue: maxi - 1 depth: depth
