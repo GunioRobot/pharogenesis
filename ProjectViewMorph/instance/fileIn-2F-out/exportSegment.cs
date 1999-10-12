624b0e604b670b2dd5e4e2f7452c3fb8191c4747
tk <unknown>
1999-10-12 19:38:00 +0000

    no message

---
+++ ProjectViewMorph/instance/fileIn-2F-out/exportSegment.cs
@@ -0,0 +1,5 @@
+exportSegment
+	"Store my project out on the disk as an ImageSegment that can be brought into other images.  Write the outPointers on the file also.  Name it <project name>.extSeg"
+
+	project exportSegment.
+
