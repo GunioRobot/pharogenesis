8e589e0656623f8bb76909e2f85ac543c7273324
sw <unknown>
1999-10-12 17:42:00 +0000

    no message

---
+++ FileContentsBrowser/instance/other/browseVersions.cs
@@ -0,0 +1,13 @@
+browseVersions
+	"Create and schedule a message set browser on all versions of the 
+	currently selected message selector."
+	| class selector |
+	(selector _ self selectedMessageName) ifNotNil:
+		[class _ self selectedClassOrMetaClass.
+		(class exists and: [class realClass includesSelector: selector]) ifTrue:
+			[VersionsBrowser
+				browseVersionsOf: (class realClass compiledMethodAt: selector)
+				class: class realClass
+				meta: class realClass isMeta
+				category: self selectedMessageCategoryName
+				selector: selector]]
