f937e5390eb15c087eb60de06e90ce3ee6e9b063
sbw <unknown>
1999-10-12 16:51:00 +0000

    no message

---
+++ ParagraphEditor/instance/current selection/selectAndScrollToTop.cs
@@ -0,0 +1,10 @@
+selectAndScrollToTop
+	"Scroll until the selection is in the view and then highlight it."
+	| lineHeight deltaY clippingRectangle |
+	self select.
+	lineHeight _ paragraph textStyle lineGrid.
+	clippingRectangle _ paragraph clippingRectangle.
+	deltaY _ stopBlock top - clippingRectangle top.
+	deltaY ~= 0 
+		ifTrue: [self scrollBy: (deltaY abs + lineHeight - 1 truncateTo: lineHeight)
+									* deltaY sign]
