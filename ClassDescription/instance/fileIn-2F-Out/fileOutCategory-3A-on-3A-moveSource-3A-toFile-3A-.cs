c59921ae92d701ebd88542de8a63b60de4839084
di <unknown>
1999-10-15 14:45:00 +0000

    no message

--- ClassDescription/instance/fileIn-2F-Out/fileOutCategory-3A-on-3A-moveSource-3A-toFile-3A-.cs
+++ ClassDescription/instance/fileIn-2F-Out/fileOutCategory-3A-on-3A-moveSource-3A-toFile-3A-.cs
@@ -1,22 +1,21 @@
-fileOutCategory: aString on: aFileStream moveSource: moveSource toFile: fileIndex 
+fileOutCategory: aSymbol on: aFileStream moveSource: moveSource toFile: fileIndex 
 	"File a description of the receiver's category, aString, onto aFileStream. If 
 	moveSource, is true, then set the method source pointer to the new file position.
 	Note when this method is called with moveSource=true, it is condensing the
 	.sources file, and should only write one preamble per method category."
 
+	| selectors |
+
 	aFileStream cr.
+	selectors := (aSymbol asString = ClassOrganizer allCategory)
+				ifTrue: [ self organization allMethodSelectors ]
+				ifFalse: [ self organization listAtCategoryNamed: aSymbol ].
 
-true ifTrue:
-	["Overridden to preserve author stamps in sources file regardless"
-	(self organization listAtCategoryNamed: aString)
-		do: [:sel | self printMethodChunk: sel withPreamble: true
-						on: aFileStream moveSource: moveSource toFile: fileIndex].
-	^ self].
-
-	moveSource ifTrue:
-		["Single header for condensing source files"
-		self printCategoryChunk: aString on: aFileStream].
-	(self organization listAtCategoryNamed: aString)
-		do: [:sel | self printMethodChunk: sel withPreamble: moveSource not
-						on: aFileStream moveSource: moveSource toFile: fileIndex].
-	moveSource ifTrue: [aFileStream nextChunkPut: ' ']
+	"Overridden to preserve author stamps in sources file regardless"
+	selectors do: [:sel |
+		self printMethodChunk: sel 
+			withPreamble: true
+			on: aFileStream 
+			moveSource: moveSource 
+			toFile: fileIndex].
+	^ self
