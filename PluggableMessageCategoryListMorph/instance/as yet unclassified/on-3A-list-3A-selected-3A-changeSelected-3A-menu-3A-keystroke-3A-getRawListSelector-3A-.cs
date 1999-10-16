a8f8f28dd59f9d5a0b1783506ea373d474c347c9
sw <unknown>
1999-10-16 22:32:00 +0000

    no message

---
+++ PluggableMessageCategoryListMorph/instance/as yet unclassified/on-3A-list-3A-selected-3A-changeSelected-3A-menu-3A-keystroke-3A-getRawListSelector-3A-.cs
@@ -0,0 +1,13 @@
+on: anObject list: getListSel selected: getSelectionSel changeSelected: setSelectionSel menu: getMenuSel keystroke: keyActionSel getRawListSelector: getRawSel.
+	self model: anObject.
+	getListSelector _ getListSel.
+	getIndexSelector _ getSelectionSel.
+	setIndexSelector _ setSelectionSel.
+	getMenuSelector _ getMenuSel.
+	keystrokeActionSelector _ keyActionSel.
+	autoDeselect _ true.
+	self borderWidth: 1.
+	getRawListSelector _ getRawSel.
+	self list: self getList.
+	self selectionIndex: self getCurrentSelectionIndex.
+	self initForKeystrokes
