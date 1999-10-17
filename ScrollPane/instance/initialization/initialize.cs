f490a070531cf79ca2fc0842107022fb144a5f3c
dew <unknown>
1999-10-17 19:41:00 +0000

    no message

--- ScrollPane/instance/initialization/initialize.cs
+++ ScrollPane/instance/initialization/initialize.cs
@@ -1,16 +1,19 @@
 initialize
-	| |
 	super initialize.
-	borderWidth _ 2.  borderColor _ #inset.
-	retractableScrollBar _ scrollBarOnLeft _ true.
+	hasFocus _ false.
+	borderWidth _ 2.
+	borderColor _ Color black.
+	retractableScrollBar _ (Preferences valueOfFlag: #inboardScrollbars) not.
+	scrollBarOnLeft _ (Preferences valueOfFlag: #scrollBarsOnRight) not.
 
 	scrollBar := ScrollBar new model: self slotName: 'scrollBar'.
-	scrollBar borderWidth: 2; borderColor: #inset.
-	retractableScrollBar ifFalse: [self addMorph: scrollBar].
+	scrollBar borderWidth: 1; borderColor: Color black.
 
 	scroller := TransformMorph new color: Color transparent.
 	scroller offset: -3@0.
 	self addMorph: scroller.
+
+	retractableScrollBar ifFalse: [self addMorph: scrollBar].
 
 	self on: #mouseEnter send: #mouseEnter: to: self.
 	self on: #mouseLeave send: #mouseLeave: to: self.
