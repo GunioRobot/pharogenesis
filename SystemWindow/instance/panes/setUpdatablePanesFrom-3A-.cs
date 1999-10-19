5640b07e2f318b10fa266b42eb648115e6d5536a
sw <unknown>
1999-10-19 09:50:00 +0000

    no message

---
+++ SystemWindow/instance/panes/setUpdatablePanesFrom-3A-.cs
@@ -0,0 +1,14 @@
+setUpdatablePanesFrom: getSelectors
+	| aList aPane |
+	"Set my updatablePanes inst var to the list of panes which are list panes with the given get-list selectors.  Order is important here!  Note that the method is robust in the face of panes not found, but a warning is printed in the transcript in each such case"
+
+	aList _ OrderedCollection new.
+	getSelectors do:
+		[:sel | aPane _ self paneMorphSatisfying:
+				[:pane | (pane isKindOf: PluggableListMorph) and: [pane getListSelector == sel]].
+			aPane
+				ifNotNil:
+					[aList add: aPane]
+				ifNil:
+					[Transcript cr; show: 'Warning: pane ', sel, ' not found.']].
+	updatablePanes _ aList asArray
