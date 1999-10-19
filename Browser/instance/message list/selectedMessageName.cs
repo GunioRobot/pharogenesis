b967152cc88d352c321b18753a9b4cd27d3ef8a6
sw <unknown>
1999-10-19 17:39:00 +0000

    no message

--- Browser/instance/message list/selectedMessageName.cs
+++ Browser/instance/message list/selectedMessageName.cs
@@ -1,6 +1,11 @@
 selectedMessageName
+	| aList |
 	"Answer the message selector of the currently selected message, if any. 
 	Answer nil otherwise."
 
-	messageListIndex = 0 ifTrue: [^nil].
-	^self messageList at: messageListIndex
+	messageListIndex = 0 ifTrue: [^ nil].
+	^ (aList _ self messageList) size >= messageListIndex
+		ifTrue:
+			[aList at: messageListIndex]
+		ifFalse:
+			[nil]
