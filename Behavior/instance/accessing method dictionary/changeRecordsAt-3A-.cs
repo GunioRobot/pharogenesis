ea2d9eb87eee36497f6f84ec2135413bbc96e295
sw <unknown>
1999-10-19 15:12:00 +0000

    no message

--- Behavior/instance/accessing method dictionary/changeRecordsAt-3A-.cs
+++ Behavior/instance/accessing method dictionary/changeRecordsAt-3A-.cs
@@ -1,10 +1,11 @@
 changeRecordsAt: selector
-	"Return a list of ChangeRecords for all versions of the method at selector.
-	Source code can be retrieved by sending string to any one"
+	"Return a list of ChangeRecords for all versions of the method at selector. Source code can be retrieved by sending string to any one.  Return nil if the method is absent."
+
 	"(Pen changeRecordsAt: #go:) collect: [:cRec | cRec string]"
-	^ (ChangeList new
-			scanVersionsOf: (self compiledMethodAt: selector)
+	| aList |
+	aList _ VersionsBrowser new
+			scanVersionsOf: (self compiledMethodAt: selector ifAbsent: [^ nil])
 			class: self meta: self isMeta
 			category: (self whichCategoryIncludesSelector: selector)
-			selector: selector)
-		changeList
+			selector: selector.
+	^ aList ifNotNil: [aList changeList]
