e58dedf14c1f350e3184ee54da719ecd95a63aac
sw <unknown>
1999-10-19 22:39:00 +0000

    no message

---
+++ Preferences/class/parameters/annotationInfo.cs
@@ -0,0 +1,11 @@
+annotationInfo 
+	^ #(
+
+		(timeStamp			'The time stamp of the last submission of the method.')
+		(messageCategory	'Which method category the method lies in')
+		(sendersCount		'A report of how many senders there of the message.')
+		(implementorsCount	'A report of how many implementors there are of the message.')
+		(recentChangeSet	'The most recent change set bearing the method.')
+		(allChangeSets		'A list of all change sets bearing the method.')
+		(priorVersionsCount	'A report of how many previous versions there are of the method' )
+		(priorTimeStamp		'The time stamp of the penultimate submission of the method, if any'))
