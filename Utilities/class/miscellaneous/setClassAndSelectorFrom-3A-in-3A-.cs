103f2e3a072232f27ff3295437acd504f83e62fc
sw <unknown>
1999-10-20 13:48:00 +0000

    no message

--- Utilities/class/miscellaneous/setClassAndSelectorFrom-3A-in-3A-.cs
+++ Utilities/class/miscellaneous/setClassAndSelectorFrom-3A-in-3A-.cs
@@ -1,14 +1,21 @@
 setClassAndSelectorFrom: messageIDString in: csBlock
-	"Decode strings of the form <className> [class] <selectorName>.  Derived from method setClassAndSelectorIn: of class MessageSet.  6/28/96 sw"
+	"Decode strings of the form <className> [class] <selectorName>.   If <className> does not exist as a class, use nil for the class in the block"
 
 	| aStream aClass maybeClass sel |
 	aStream _ ReadStream on: messageIDString.
-	aClass _ Smalltalk at: (aStream upTo: $ ) asSymbol.
+	aClass _ Smalltalk at: (aStream upTo: $ ) asSymbol ifAbsent: [nil].
 	maybeClass _ aStream upTo: $ .
 	sel _ aStream upTo: $ .
-	(maybeClass = 'class') & (sel size ~= 0)
-		ifFalse: [csBlock value: aClass value: maybeClass asSymbol]
-		ifTrue: [csBlock value: aClass class value: sel asSymbol]
+	((maybeClass = 'class') & (sel size ~= 0))
+		ifTrue:
+			[aClass
+				ifNil:
+					[csBlock value: nil value: sel asSymbol]
+				ifNotNil:
+					[csBlock value: aClass class value: sel asSymbol]]
+		ifFalse:
+			[csBlock value: aClass value: maybeClass asSymbol]
+
 
 "
 Utilities setClassAndSelectorFrom: 'Utilities class oppositeModeTo:' in: [:aClass :aSelector | Transcript cr; show: 'Class = ', aClass name printString, ' selector = ', aSelector printString].
