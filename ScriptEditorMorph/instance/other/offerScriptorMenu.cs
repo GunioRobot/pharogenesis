5605aa3c8666c08395d9e631f6b3f11713947651
sw <unknown>
1999-10-21 13:18:00 +0000

    no message

--- ScriptEditorMorph/instance/other/offerScriptorMenu.cs
+++ ScriptEditorMorph/instance/other/offerScriptorMenu.cs
@@ -2,11 +2,8 @@ offerScriptorMenu
 	| aMenu result  aList lines title |
 	self isAnonymous
 		ifTrue:
-			[aList _ #(
-				('name and save this script'	renameScript)
-				('destroy this script'			destroyScript)).
-			title _ 'temporary script'.
-			lines _ #()]
+			[^ self inform: 'ancient structure!
+please modernize!']
 		ifFalse:
 			[title _ scriptName asString.
 			self isTextuallyCoded
@@ -15,17 +12,27 @@ offerScriptorMenu
 					aList _ #(
 						('revert to tile version...'		revertScriptVersion)
 						('modify textual script'			editScriptTextually)
+						('stand-alone script pane'		makeIsolatedCodePane)
+						('fires per tick...'				chooseFrequency)
 				"		('view all scripts'				browseScripts)"
-						('destroy this script'				destroyScript)).
-					lines _ #(2)]
+						('destroy this script'				destroyScript)
+						('rename this script'				renameScript)
+						('explain status alternatives' 	explainStatusAlternatives)
+						('button to fire this script'		tearOfButtonToFireScript)).
+					lines _ #(3 4 6)]
 				ifFalse:
 					[aList _ #(
 						('save this version'				saveScriptVersion)
 						('revert to prior version...'		revertScriptVersion)
 						('edit this script textually'		editScriptTextually)
+						('stand-alone script pane'		makeIsolatedCodePane)
+						('fires per tick...'				chooseFrequency)
 					"	('view all scripts'				browseScripts)"
-						('destroy this script'				destroyScript)).
-					lines _ #(2 3)]].
+						('destroy this script'				destroyScript)
+						('rename this script'				renameScript)
+						('explain status alternatives' 	explainStatusAlternatives)
+						('button to fire this script'		tearOfButtonToFireScript)).
+					lines _ #(4 5 7)]].
 
 	aMenu _ SelectionMenu labelList: (aList collect: [:pair | pair first]) lines: lines selections: (aList collect: [:pair | pair second]).
 	result _ aMenu startUpWithCaption: title.
