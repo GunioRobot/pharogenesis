85d277bcf929163331e1b1ab25ff4fc214dd9a4e
hmm <unknown>
1999-10-22 21:18:00 +0000

    no message

--- PositionableStream/instance/positioning/match-3A-.cs
+++ PositionableStream/instance/positioning/match-3A-.cs
@@ -5,13 +5,12 @@ match: subCollection
 	pattern _ ReadStream on: subCollection.
 	startMatch _ nil.
 	[pattern atEnd] whileFalse: 
-		[self atEnd ifTrue: [^false].
+		[self atEnd ifTrue: [^ false].
 		(self next) = (pattern next) 
 			ifTrue: [pattern position = 1 ifTrue: [startMatch _ self position]]
 			ifFalse: [pattern position: 0.
 					startMatch ifNotNil: [
-						self position: startMatch + 1.
+						self position: startMatch.
 						startMatch _ nil]]].
-
-	^self atEnd
+	^ true
 
