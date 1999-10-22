4578b3fcbd9a69af73fead1c3dc7edb5f3c78217
di <unknown>
1999-10-22 11:52:00 +0000

    no message

--- ObjectMemory/instance/allocation/allocateOrRecycleContext-3A-.cs
+++ ObjectMemory/instance/allocation/allocateOrRecycleContext-3A-.cs
@@ -1,25 +1,22 @@
-allocateOrRecycleContext: smallContextWanted
-	"Return a recycled context or a newly allocated one if none is available for recycling. The argument indicates that a small context is wanted."
-
+allocateOrRecycleContext: needsLarge
+	"Return a recycled context or a newly allocated one if none is available for recycling."
 	| cntxt |
-	self inline: true.
-	smallContextWanted ifTrue: [
-		freeSmallContexts ~= NilContext ifTrue: [
-			cntxt _ freeSmallContexts.
-			freeSmallContexts _ self fetchPointer: 0 ofObject: cntxt.
-		] ifFalse: [
-			cntxt _ self instantiateSmallClass: (self splObj: ClassMethodContext)
-								 sizeInBytes: SmallContextSize
-										 fill: nilObj.
-		].
-	] ifFalse: [
-		freeLargeContexts ~= NilContext ifTrue: [
-			cntxt _ freeLargeContexts.
-			freeLargeContexts _ self fetchPointer: 0 ofObject: cntxt.
-		] ifFalse: [
-			cntxt _ self instantiateSmallClass: (self splObj: ClassMethodContext)
-								sizeInBytes: LargeContextSize
-										fill: nilObj.
-		].
-	].
+	needsLarge = 0
+	ifTrue: [freeContexts ~= NilContext ifTrue:
+				[cntxt _ freeContexts.
+				freeContexts _ self fetchPointer: 0 ofObject: cntxt.
+				^ cntxt]]
+	ifFalse: [freeLargeContexts ~= NilContext ifTrue:
+				[cntxt _ freeLargeContexts.
+				freeLargeContexts _ self fetchPointer: 0 ofObject: cntxt.
+				^ cntxt]].
+	
+	needsLarge = 0
+		ifTrue: [cntxt _ self instantiateContext: (self splObj: ClassMethodContext)
+				sizeInBytes: SmallContextSize]
+		ifFalse: [cntxt _ self instantiateContext: (self splObj: ClassMethodContext)
+				sizeInBytes: LargeContextSize].
+	"Required init -- above does not fill w/nil.  All others get written."
+	self storePointerUnchecked: 4 "InitialIPIndex" ofObject: cntxt
+					withValue: nilObj.
 	^ cntxt
