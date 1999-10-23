63da2ffda4c3210bb913711d2716d038517dfddb
ar <unknown>
1999-10-23 20:46:00 +0000

    no message

--- BitBltSimulation/instance/combination rules/tallyIntoMap-3A-with-3A-.cs
+++ BitBltSimulation/instance/combination rules/tallyIntoMap-3A-with-3A-.cs
@@ -1,37 +1,26 @@
 tallyIntoMap: sourceWord with: destinationWord
-	"Tally pixels into the color map.  Note that the source should be 
-	specified = destination, in order for the proper color map checks 
-	to be performed at setup.
-	Note that the region is not clipped to bit boundaries, but only to the
-	nearest (enclosing) word.  This is because copyLoop does not do
-	pre-merge masking.  For accurate results, you must subtract the
-	values obtained from the left and right fringes."
-	| mapIndex pixMask shiftWord |
-	colorMap = interpreterProxy nilObject
+	"Tally pixels into the color map.  Those tallied are exactly those
+	in the destination rectangle.  Note that the source should be 
+	specified == destination, in order for the proper color map checks 
+	to be performed at setup."
+	| mapIndex pixMask destShifted maskShifted pixVal |
+	self inline: false.
+	colorMap = nil
 		ifTrue: [^ destinationWord "no op"].
-	destPixSize < 16 ifTrue:
-		["loop through all packed pixels."
-		pixMask _ (1<<destPixSize) - 1.
-		shiftWord _ destinationWord.
-		1 to: pixPerWord do:
-			[:i |
-			mapIndex _ shiftWord bitAnd: pixMask.
-			interpreterProxy storeWord: mapIndex ofObject: colorMap
-				withValue: (interpreterProxy fetchWord: mapIndex ofObject: colorMap) + 1.
-			shiftWord _ shiftWord >> destPixSize].
-		^ destinationWord].
-	destPixSize = 16 ifTrue:
-		["Two pixels  Tally the right half..."
-		mapIndex _ self rgbMap: (destinationWord bitAnd: 16rFFFF) from: 5 to: cmBitsPerColor.
-		interpreterProxy storeWord: mapIndex ofObject: colorMap
-			withValue: (interpreterProxy fetchWord: mapIndex ofObject: colorMap) + 1.
-		"... and then left half"
-		mapIndex _ self rgbMap: destinationWord>>16 from: 5 to: cmBitsPerColor.
-		interpreterProxy storeWord: mapIndex ofObject: colorMap
-			withValue: (interpreterProxy fetchWord: mapIndex ofObject: colorMap) + 1]
-	ifFalse:
-		["Just one pixel."
-		mapIndex _ self rgbMap: destinationWord from: 8 to: cmBitsPerColor.
-		interpreterProxy storeWord: mapIndex ofObject: colorMap
-			withValue: (interpreterProxy fetchWord: mapIndex ofObject: colorMap) + 1].
+	pixMask _ maskTable at: destPixSize.
+	destShifted _ destinationWord.
+	maskShifted _ destMask.
+	1 to: pixPerWord do:
+		[:i |
+		(maskShifted bitAnd: pixMask) = 0 ifFalse:
+			["Only tally pixels within the destination rectangle"
+			pixVal _ destShifted bitAnd: pixMask.
+			destPixSize < 16
+				ifTrue: [mapIndex _ pixVal]
+				ifFalse: [destPixSize = 16
+					ifTrue: [mapIndex _ self rgbMap: pixVal from: 5 to: cmBitsPerColor]
+					ifFalse: [mapIndex _ self rgbMap: pixVal from: 8 to: cmBitsPerColor]].
+			self colormapAt: mapIndex put: (self colormapAt: mapIndex) + 1].
+		maskShifted _ maskShifted >> destPixSize.
+		destShifted _ destShifted >> destPixSize].
 	^ destinationWord  "For no effect on dest"
