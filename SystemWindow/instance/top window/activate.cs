ec4caf2e848966daa2ebf5e308fe77ec90c08b3f
sw <unknown>
1999-10-25 23:50:00 +0000

    no message

--- SystemWindow/instance/top window/activate.cs
+++ SystemWindow/instance/top window/activate.cs
@@ -1,10 +1,21 @@
 activate
 	"Bring me to the front and make me able to respond to mouse and keyboard"
-	| oldTop |
+	| oldTop outerMorph sketchEditor pal |
+	outerMorph _ self topRendererOrSelf.
+	outerMorph owner ifNil: [^ self "avoid spurious activate when drop in trash"].
 	oldTop _ TopWindow.
 	TopWindow _ self.
 	oldTop ifNotNil: [oldTop passivate].
-	owner firstSubmorph == self ifFalse: [owner addMorphFront: self].
+	outerMorph owner firstSubmorph == outerMorph
+		ifFalse: ["Bring me (with any flex) to the top if not already"
+				outerMorph owner addMorphFront: outerMorph].
 	self submorphsDo: [:m | m unlock].
-	self setStripeColorsFrom: self paneColor.
-	self addHandles
+	self setStripeColorsFrom: self paneColorToUse.
+	self isCollapsed ifFalse:
+		[model modelWakeUpIn: self.
+		self positionSubmorphs].
+
+	(sketchEditor _ self extantSketchEditor) ifNotNil:
+		[sketchEditor comeToFront.
+		(pal _ self world findA: PaintBoxMorph) ifNotNil:
+			[pal comeToFront]]
