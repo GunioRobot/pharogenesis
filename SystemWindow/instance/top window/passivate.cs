be8ace8609ccdd646e0788ca22f2b8c4f5e42841
sw <unknown>
1999-10-25 23:51:00 +0000

    no message

--- SystemWindow/instance/top window/passivate.cs
+++ SystemWindow/instance/top window/passivate.cs
@@ -1,5 +1,11 @@
 passivate
 	"Make me unable to respond to mouse and keyboard"
-	self submorphsDo: [:m | m lock].
-	self setStripeColorsFrom: self paneColor.
-	self removeHandles
+	self setStripeColorsFrom: self paneColorToUse.
+	model modelSleep.
+	self submorphsDo:
+		[:m | (m == closeBox or: [m == collapseBox])
+				ifTrue: ["Control boxes remain active, except in novice mode"
+						Preferences noviceMode ifTrue: [m lock]]
+				ifFalse: [m lock]].
+	self world ifNotNil:  "clean damage now, so dont merge this rect with new top window"
+		[self world == World ifTrue: [self world displayWorld]]
