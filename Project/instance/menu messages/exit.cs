48f26eb86f176eb5e7cd6f087aa6a5bd1b4bf768
tk <unknown>
1999-10-26 14:25:00 +0000

    no message

--- Project/instance/menu messages/exit.cs
+++ Project/instance/menu messages/exit.cs
@@ -2,7 +2,4 @@ exit
 	"Leave the current project and return to the project in which this one was created."
 
 	self isTopProject ifTrue: [^ PopUpMenu notify: 'Can''t exit the top project'].
-	Smalltalk at: #ScorePlayer ifPresent: [:playerClass |
-		playerClass allInstancesDo: [:player | player pause]].
-	activeProcess _ nil.
-	parentProject enter: false.
+	parentProject enter: false revert: false saveForRevert: false.
