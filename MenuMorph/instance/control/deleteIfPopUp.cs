46a0a1be456ed74c61cf4ec25d15f742a6ef600b
di <unknown>
1999-10-28 09:50:00 +0000

    no message

--- MenuMorph/instance/control/deleteIfPopUp.cs
+++ MenuMorph/instance/control/deleteIfPopUp.cs
@@ -1,7 +1,7 @@
 deleteIfPopUp
 	"Remove this menu from the screen if stayUp is not true. If it is a submenu, also remove its owning menu."
 
-	stayUp ifFalse: [self delete].
+	stayUp ifFalse: [self topRendererOrSelf delete].
 	(popUpOwner notNil and: [popUpOwner isKindOf: MenuItemMorph]) ifTrue: [
 		popUpOwner isSelected: false.
 		(popUpOwner owner isKindOf: MenuMorph)
