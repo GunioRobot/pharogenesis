4a9f7893624b25c041cee709b82d743e8ab12702
di <unknown>
1999-10-28 15:49:00 +0000

    no message

--- SystemWindow/class/top window/windowsIn-3A-satisfying-3A-.cs
+++ SystemWindow/class/top window/windowsIn-3A-satisfying-3A-.cs
@@ -1,3 +1,12 @@
 windowsIn: aWorld satisfying: windowBlock
-	^ aWorld submorphs select:
-		[:m | (m isKindOf: SystemWindow) and: [windowBlock value: m]]
+	| windows s |
+	windows _ OrderedCollection new.
+	aWorld submorphs do:
+		[:m |
+		((m isKindOf: SystemWindow) and: [windowBlock value: m])
+			ifTrue: [windows addLast: m]
+			ifFalse: [((m isKindOf: TransformationMorph) and: [m submorphs size = 1])
+					ifTrue: [s _ m firstSubmorph.
+							((s isKindOf: SystemWindow) and: [windowBlock value: s])
+								ifTrue: [windows addLast: s]]]].
+	^ windows
