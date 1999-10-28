4ee5ee9ab9f29cf95fc6694971f7f9997e1d5240
di <unknown>
1999-10-28 09:50:00 +0000

    no message

--- MenuMorph/instance/menu/toggleStayUp-3A-.cs
+++ MenuMorph/instance/menu/toggleStayUp-3A-.cs
@@ -1,10 +1,10 @@
 toggleStayUp: evt
 	"Toggle my 'stayUp' flag and adjust the menu item to reflect its new state."
 
-	stayUp _ stayUp not.
 	self items do: [:item |
-		item selector = #toggleStayUp: ifTrue: [
-			stayUp
+		item selector = #toggleStayUp: ifTrue:
+			[stayUp _ stayUp not.	
+			 stayUp
 				ifTrue: [item contents: 'dismiss this menu']
-				ifFalse: [item contents: 'stay up']]].
-	stayUp ifFalse: [self delete].
+				ifFalse: [item contents: 'keep this menu up']]].
+	stayUp ifFalse: [self topRendererOrSelf delete].
