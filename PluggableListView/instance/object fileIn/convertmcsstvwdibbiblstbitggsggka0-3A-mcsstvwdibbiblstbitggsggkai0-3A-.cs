07eb869162e065f097df1e801b28330cdc92cef9
sw <unknown>
1999-10-29 20:25:00 +0000

    no message

---
+++ PluggableListView/instance/object fileIn/convertmcsstvwdibbiblstbitggsggka0-3A-mcsstvwdibbiblstbitggsggkai0-3A-.cs
@@ -0,0 +1,5 @@
+convertmcsstvwdibbiblstbitggsggka0: varDict mcsstvwdibbiblstbitggsggkai0: smartRefStrm
+	"These variables are automatically stored into the new instance ('getListSelector' 'getSelectionSelector' 'setSelectionSelector' 'getMenuSelector' 'getMenuTitleSelector' 'keystrokeActionSelector' 'autoDeselect' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('items' )  If a non-nil value is needed, please assign it."
