c3c1a04f1722bb962ad3c5efa886b934e6d838a3
sw <unknown>
1999-10-29 20:28:00 +0000

    no message

---
+++ StandardSystemView/instance/object fileIn/convertmcsstvwdibbibllismmcelwb0-3A-mcsstvwdibbibllismmcelwbu0-3A-.cs
@@ -0,0 +1,6 @@
+convertmcsstvwdibbibllismmcelwb0: varDict mcsstvwdibbibllismmcelwbu0: smartRefStrm
+	"These variables are automatically stored into the new instance ('labelFrame' 'labelText' 'isLabelComplemented' 'savedSubViews' 'minimumSize' 'maximumSize' 'collapsedViewport' 'expandedViewport' 'labelBits' 'windowBits' 'bitsValid' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('updatablePanes' )  If a non-nil value is needed, please assign it."
+	updatablePanes _ #()
