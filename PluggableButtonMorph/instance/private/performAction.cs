4da71e4c2ac76e640e32caaf99610d3a1704ac7e
tk <unknown>
1999-10-30 08:31:00 +0000

    no message

--- PluggableButtonMorph/instance/private/performAction.cs
+++ PluggableButtonMorph/instance/private/performAction.cs
@@ -1,4 +1,5 @@
 performAction
 	"Inform the model that this button has been pressed. Sent by the controller when this button is pressed."
 
+	askBeforeChanging ifTrue: [model okToChange ifFalse: [^ self]].
 	actionSelector ifNotNil: [model perform: actionSelector].
