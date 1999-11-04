e2c34bab9c6aa21d53f58741399c22d7a332c897
di <unknown>
1999-11-04 13:55:00 +0000

    no message

---
+++ SelectorBrowser/instance/as yet unclassified/openAsMorph.cs
@@ -0,0 +1,4 @@
+openAsMorph
+	"Create a Browser that lets you type part of a selector, shows a list of selectors, shows the classes of the one you chose, and spwns a full browser on it.
+	SelectorBrowser new open   "
+	^ self morphicWindow openInWorld
