51f2c9a1f9e15e5e9ee6143c38c9987270842039
sw <unknown>
1999-11-06 22:58:00 +0000

    no message

--- Debugger/instance/context stack (message list)/selectedMessage.cs
+++ Debugger/instance/context stack (message list)/selectedMessage.cs
@@ -1,5 +1,12 @@
 selectedMessage
 	"Answer the source code of the currently selected context."
-
-	contents == nil ifTrue: [contents _ self selectedContext sourceCode].
-	^contents
+	contents _ [self selectedContext sourceCode] ifError: [ :err :rcvr |
+		'ERROR
+"',(err reject: [ :each | each == $"]),'"'
+	].
+	Preferences browseWithPrettyPrint ifTrue: [contents _ self selectedClass compilerClass new
+					format: contents
+					in: self selectedClass
+					notifying: nil
+					decorated: Preferences colorWhenPrettyPrinting].
+	^ contents _ contents asText makeSelectorBoldIn: self selectedClass
