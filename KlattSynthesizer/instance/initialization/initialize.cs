d7b22d905f9eb3cb5cde3832c08663623a5df850
len <unknown>
1999-11-08 01:25:00 +0000

    no message

---
+++ KlattSynthesizer/instance/initialization/initialize.cs
@@ -0,0 +1,10 @@
+initialize
+	"Initialize the Klatt synthesizer."
+
+	"Seed for noise generation:"
+	seed _ 17.
+	"Sampling rate and millseconds per frame:"
+	self samplingRate: 22025.
+	self millisecondsPerFrame: self defaultMillisecondsPerFrame.
+	"Number of formants in the cascade branch: (0 to 8)"
+	self cascade: 0
