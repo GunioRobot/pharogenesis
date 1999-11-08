0c55ff036ecab014c7ba7c48a2bca1fc0c80a56b
len <unknown>
1999-11-08 00:56:00 +0000

    no message

---
+++ KlattFrame/class/examples/example2.cs
@@ -0,0 +1,10 @@
+example2
+	"
+	KlattFrame example2
+	"
+	| frame |
+	frame _ self default
+		voicing: 62;
+		anv: 0; a1v: 62; a2v: 62; a3v: 62; a4v: 62;
+		yourself.
+	(KlattSynthesizer new cascade: 6; millisecondsPerFrame: 1000; soundFromFrames: {frame}) play
