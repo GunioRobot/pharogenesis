95a39ed17a67d09e2a08172ffa468fe1a7bf0601
ar <unknown>
1999-11-08 14:02:00 +0000

    no message

---
+++ WonderlandActor/instance/morphic reactions/morphicMouseMove-3A-.cs
@@ -0,0 +1,8 @@
+morphicMouseMove: event
+	"Handle the given event"
+	(event getMorphicEvent shiftPressed and:[self getUserPointOfView notNil])
+		ifTrue:[^self]. "Reserved for navigation"
+	(self getProperty: #userTransition) == true ifTrue:[^true]. "Shift key went up"
+	myTexture isMorph ifTrue:[
+		self dispatchEvent: event using:[:evt :hand| hand handleMouseMove: evt].
+	].
