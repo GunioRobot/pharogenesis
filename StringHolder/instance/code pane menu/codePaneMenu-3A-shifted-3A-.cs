59339de82db3bcb2474844e14534444de6b7f042
sw <unknown>
1999-11-08 17:56:00 +0000

    no message

--- StringHolder/instance/code pane menu/codePaneMenu-3A-shifted-3A-.cs
+++ StringHolder/instance/code pane menu/codePaneMenu-3A-shifted-3A-.cs
@@ -1,15 +1,9 @@
 codePaneMenu: aMenu shifted: shifted
-	"Note that unless we override doMenuItem:, PluggableTextController will respond to all menu items in a text pane"
-
-| shiftMenu |
-^ shifted 
-	ifFalse: [aMenu 
-		labels: PluggableTextController yellowButtonMenu labelString 
-		lines: PluggableTextController yellowButtonMenu lineArray
-		selections: PluggableTextController yellowButtonMessages]
-
-	ifTrue: [shiftMenu _ PluggableTextController shiftedYellowButtonMenu.
-		aMenu 
-			labels: shiftMenu labelString 
-			lines: shiftMenu lineArray
-			selections: PluggableTextController shiftedYellowButtonMessages]
+	"Note that unless we override perform:orSendTo:, PluggableTextController will respond to all menu items in a text pane"
+	| donorMenu |
+	donorMenu _ shifted
+		ifTrue:
+			[ParagraphEditor shiftedYellowButtonMenu]
+		ifFalse:
+			[ParagraphEditor yellowButtonMenu].
+	^ aMenu labels: donorMenu labelString lines: donorMenu lineArray selections: donorMenu selections
