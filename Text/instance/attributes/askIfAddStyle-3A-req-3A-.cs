e0f42abc396caaaf829f156d1f9d56a00b106c8b
sw <unknown>
1999-11-09 17:24:00 +0000

    no message

--- Text/instance/attributes/askIfAddStyle-3A-req-3A-.cs
+++ Text/instance/attributes/askIfAddStyle-3A-req-3A-.cs
@@ -1,17 +1,16 @@
 askIfAddStyle: priorMethod req: requestor
 	"Ask the user if we have a complex style (i.e. bold) for the first time"
-
 	| tell answ old |
+	(Preferences browseWithPrettyPrint and: [Preferences colorWhenPrettyPrinting])
+		ifTrue: [self couldDeriveFromPrettyPrinting ifTrue: [^ self asString]].
 	self runs coalesce.
 	self unembellished ifTrue: [^ self asString].
-
 	priorMethod ifNotNil: [old _ priorMethod getSourceFromFile].
-	(old == nil or: [old unembellished]) ifTrue: [
-		tell _ 'This method contains style (i.e. bold) for the first time.
+	(old == nil or: [old unembellished])
+		ifTrue:
+			[tell _ 'This method contains style (e.g. bold) for the first time.
 Do you really want to save the style info?'.
-		answ _ (PopUpMenu labels: 'Save method with style
-Save method simply') startUpWithCaption: tell. 
-		answ = 2 ifTrue: [ 
-			"Dan, here is where to tell it to display self asString" "requestor model changed." 
-			^ self asString]].
-	"^ self		keep my style"
+			answ _ (PopUpMenu labels: 'Save method with style
+Save method simply')
+						startUpWithCaption: tell.
+			answ = 2 ifTrue: [^ self asString]]
