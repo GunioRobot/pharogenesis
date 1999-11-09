d5ebccf43e17c345b08f185ac22ecb214a0d15b1
sw <unknown>
1999-11-09 19:26:00 +0000

    no message

---
+++ FileContentsBrowser/instance/edit pane/selectedMessage.cs
@@ -0,0 +1,18 @@
+selectedMessage
+	"Answer a copy of the source code for the selected message selector."
+	| class selector |
+	class _ self selectedClassOrMetaClass.
+	selector _ self selectedMessageName.
+	contents _ class sourceCodeAt: selector.
+	Preferences browseWithPrettyPrint ifTrue: [contents _ Compiler new
+					format: contents
+					in: class
+					notifying: nil
+					decorated: Preferences colorWhenPrettyPrinting].
+	self showDiffs ifTrue:
+		[contents _ self
+			methodDiffFor: contents
+			class: self selectedClass
+			selector: self selectedMessageName
+			meta: self metaClassIndicated].
+	^ contents asText makeSelectorBoldIn: class
