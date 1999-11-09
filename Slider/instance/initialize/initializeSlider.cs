efd477480401db81cb6a16cb660ac254cba0fe4a
di <unknown>
1999-11-09 13:31:00 +0000

    no message

--- Slider/instance/initialize/initializeSlider.cs
+++ Slider/instance/initialize/initializeSlider.cs
@@ -1,6 +1,14 @@
 initializeSlider
-	slider := RectangleMorph newBounds: self totalSliderArea color: Color veryLightGray.
+	slider := RectangleMorph newBounds: self totalSliderArea color: self sliderColor.
+	sliderShadow := RectangleMorph newBounds: self totalSliderArea
+						color: self pagingArea color.
 	slider on: #mouseStillDown send: #scrollAbsolute: to: self.
-	slider setBorderWidth: 2 borderColor: #raised.
+	slider on: #mouseDown send: #mouseDownInSlider: to: self.
+	slider on: #mouseUp send: #mouseUpInSlider: to: self.
+	slider setBorderWidth: 1 borderColor: #raised.
+	sliderShadow setBorderWidth: 1 borderColor: #inset.
+	"(the shadow must have the pagingArea as its owner to highlight properly)"
+	self pagingArea addMorph: sliderShadow.
+	sliderShadow hide.
 	self addMorph: slider.
 	self computeSlider.
