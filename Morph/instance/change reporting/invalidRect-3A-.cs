f1bb8d4adf599344ae4a874d8d2f1079ce4eaf2f
ar <unknown>
1999-11-10 20:13:00 +0000

    no message

--- Morph/instance/change reporting/invalidRect-3A-.cs
+++ Morph/instance/change reporting/invalidRect-3A-.cs
@@ -1,3 +1,5 @@
 invalidRect: damageRect
 
 	owner ifNotNil: [owner invalidRect: damageRect].
+	(self hasProperty:#wonderlandTexture) ifTrue:["If I am a wonderland texture"
+		self setProperty: #textureIsDirty toValue: true].
