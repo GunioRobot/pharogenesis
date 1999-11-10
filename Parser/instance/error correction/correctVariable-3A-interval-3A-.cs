c2c17926f82737d6942a7cdb8c7272ac2d8694b8
sw <unknown>
1999-11-10 13:26:00 +0000

    no message

--- Parser/instance/error correction/correctVariable-3A-interval-3A-.cs
+++ Parser/instance/error correction/correctVariable-3A-interval-3A-.cs
@@ -7,12 +7,12 @@ correctVariable: proposedVariable interval: spot
 
 	| alternatives aStream choice userSelection temp binding globalToo |
 	"If we can't ask the user for correction, make it undeclared"
-	self interactive ifFalse: [ ^ encoder undeclared: proposedVariable ].
+	self interactive ifFalse: [^ encoder undeclared: proposedVariable].
 
 	temp _ proposedVariable first isLowercase.
 	"First check to see if the requestor knows anything about the variable"
 	(temp and: [(binding _ requestor bindingOf: proposedVariable) notNil])
-		ifTrue: [^encoder global: binding name: proposedVariable].
+		ifTrue: [^ encoder global: binding name: proposedVariable].
 	userSelection _ requestor selectionInterval.
 	requestor selectFrom: spot first to: spot last.
 	requestor select.
@@ -26,19 +26,19 @@ correctVariable: proposedVariable interval: spot
 			ifFalse: [encoder classEncoding == UndefinedObject
 					ifTrue: ['Global']
 					ifFalse: [globalToo _ 1.  'Class Variable']]); cr.
-	globalToo = 1 ifTrue: [aStream nextPutAll: 'Global'; cr].
+	globalToo = 1 ifTrue: [aStream nextPutAll: 'declare Global'; cr].
 	alternatives do:
 		[:sel | aStream nextPutAll: sel; cr].
 	aStream nextPutAll: 'cancel'.
 
 	choice _ (PopUpMenu
 				labels: aStream contents
-				lines: (Array with: 1 with: alternatives size+1) )
+				lines: (Array with: (globalToo + 1) with: (globalToo + alternatives size + 1)))
 		startUpWithCaption:
-(('Unknown variable: ' , proposedVariable , '
-please correct, or cancel:') asText makeBoldFrom: 19 to: 19+proposedVariable size).
-	(choice = 0) | (choice > (alternatives size+1))
-		ifTrue: [ ^ self fail ].
+(('Unknown variable: ', proposedVariable, '
+please correct, or cancel:') asText makeBoldFrom: 19 to: 19 + proposedVariable size).
+	(choice = 0) | (choice > (globalToo + alternatives size + 1))
+		ifTrue: [^ self fail].
 
 	requestor deselect.
 	requestor selectInvisiblyFrom: userSelection first to: userSelection last.
