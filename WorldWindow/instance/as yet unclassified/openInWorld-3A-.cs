cdae830d53e955850f65c7b521613630cbfb0ec4
RAA <unknown>
1999-11-11 20:03:00 +0000

    no message

---
+++ WorldWindow/instance/as yet unclassified/openInWorld-3A-.cs
@@ -0,0 +1,7 @@
+openInWorld: aWorld
+	"This msg and its callees result in the window being activeOnlyOnTop"
+	self bounds: (RealEstateAgent initialFrameFor: self).
+	self firstSubmorph position: (self left + 1) @ (self top + self labelHeight).
+	aWorld addMorph: self.
+	self activate.
+	aWorld startSteppingSubmorphsOf: self
