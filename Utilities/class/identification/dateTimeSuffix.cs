a85bbf78337a6a7c976683b57cf4fceb3de6b705
sw <unknown>
1999-11-13 22:45:00 +0000

    no message

--- Utilities/class/identification/dateTimeSuffix.cs
+++ Utilities/class/identification/dateTimeSuffix.cs
@@ -1,11 +1,9 @@
 dateTimeSuffix
-	"Answer a string which indicates the date and time, intended for use in building fileout filenames, etc.  1/18/96 sw"
+	"Answer a string which indicates the date and time, intended for use in building fileout filenames, etc."
 
 	"Utilities dateTimeSuffix"
-
-	| dateTime headString tailString |
-	dateTime _ Time dateAndTimeNow.
-	headString _ dateTime first printString copyFrom: 1 to: 6.
-	headString _ headString copyWithout: $ .
-	tailString _ dateTime last printString copyWithout: $:.
-	^ headString, (tailString copyFrom: 1 to: tailString size - 5), (tailString copyFrom: tailString size -1 to: tailString size)
+	^ Preferences twentyFourHourFileStamps
+		ifFalse:
+			[self monthDayTimeStringFrom: Time primSecondsClock]
+		ifTrue:
+			[self monthDayTime24StringFrom: Time primSecondsClock]
