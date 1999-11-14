0f5c316354d6cd49e511d7cadf00d6ba77117b7e
RAA <unknown>
1999-11-14 11:56:00 +0000

    no message

---
+++ WiWPasteUpMorph/instance/update cycle/doDeferredUpdating.cs
@@ -0,0 +1,9 @@
+doDeferredUpdating
+	"If this platform supports deferred updates, then make my canvas be the Display (or a rectangular portion of it), set the Display to deferred update mode, and answer true. Otherwise, do nothing and answer false. One can set the class variable DisableDeferredUpdates to true to completely disable the deferred updating feature."
+
+	DisableDeferredUpdates ifNil: [DisableDeferredUpdates _ false].
+	DisableDeferredUpdates ifTrue: [^ false].
+	(Display deferUpdates: true) ifNil: [^ false].  "deferred updates not supported"
+
+	self resetViewBox.
+	^ true
