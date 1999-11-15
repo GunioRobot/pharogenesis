d0a01c38fcc72a3627c0e7ecd17f642db17f802b
sw <unknown>
1999-11-15 22:23:00 +0000

    no message

--- ParseNode/instance/printing/printCommentOn-3A-indent-3A-.cs
+++ ParseNode/instance/printing/printCommentOn-3A-indent-3A-.cs
@@ -1,15 +1,14 @@
 printCommentOn: aStream indent: indent 
 	| thisComment |
-	comment == nil ifTrue: [^self].
-	"Show comments in green"
-	aStream withAttribute: (TextColor color: Color blue) do: 
-	[1 to: comment size do: 
-		[:index | 
-		index > 1 ifTrue: [aStream crtab: indent].
-		aStream nextPut: $".
-		thisComment _ comment at: index.
-		self printSingleComment: thisComment
-			on: aStream
-			indent: indent.
-		aStream nextPut: $"]].
+	comment == nil ifTrue: [^ self].
+	aStream withAttributes: (Preferences syntaxAttributesFor: #comment)
+		do: [1 to: comment size do: 
+				[:index | 
+				index > 1 ifTrue: [aStream crtab: indent].
+				aStream nextPut: $".
+				thisComment _ comment at: index.
+				self printSingleComment: thisComment
+					on: aStream
+					indent: indent.
+				aStream nextPut: $"]].
 	comment _ nil
