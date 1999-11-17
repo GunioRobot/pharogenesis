4b9f43769473a57af0e03560213084afebbd8d70
sw <unknown>
1999-11-17 15:03:00 +0000

    no message

--- SelectorNode/instance/printing/printOn-3A-indent-3A-.cs
+++ SelectorNode/instance/printing/printOn-3A-indent-3A-.cs
@@ -1,3 +1,4 @@
-printOn: aStream indent: level
-
-	aStream nextPutAll: key
+printOn: aStream indent: level 
+	"nb: this method is seemingly never reached"
+	aStream withAttributes: (Preferences syntaxAttributesFor: #keyword)
+		do: [aStream nextPutAll: key]
