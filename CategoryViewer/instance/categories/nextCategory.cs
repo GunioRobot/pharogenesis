2d8da18aecedd85460e777ec33382b6d7d141da4
sw <unknown>
1999-11-18 16:07:00 +0000

    no message

---
+++ CategoryViewer/instance/categories/nextCategory.cs
@@ -0,0 +1,11 @@
+nextCategory
+	| aList anIndex newIndex already aChoice |
+	aList _ scriptedPlayer categories.
+	already _ self outerViewer ifNil: [#()] ifNotNil: [self outerViewer categoriesCurrentlyShowing].
+	anIndex _ aList indexOf: self currentCategory ifAbsent: [0].
+	newIndex _ anIndex = aList size
+		ifTrue:		[1]
+		ifFalse:		[anIndex + 1].
+	[already includes: (aChoice _ aList at: newIndex)] whileTrue:
+		[newIndex _ (newIndex \\ aList size) + 1].
+	self categoryChoice: aChoice
