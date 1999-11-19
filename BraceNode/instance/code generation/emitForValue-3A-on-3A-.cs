f95716441c62bb83177d7daa06ec0d97f07c41ac
di <unknown>
1999-11-19 08:58:00 +0000

    no message

--- BraceNode/instance/code generation/emitForValue-3A-on-3A-.cs
+++ BraceNode/instance/code generation/emitForValue-3A-on-3A-.cs
@@ -1,8 +1,3 @@
 emitForValue: stack on: aStream
-	"elem1, ..., elemN, collectionClass, N, fromBraceStack:"
 
-	elements do: [:element | element emitForValue: stack on: aStream].
-	collClassNode emitForValue: stack on: aStream.
-	nElementsNode emitForValue: stack on: aStream.
-	fromBraceStackNode emit: stack args: 1 on: aStream.
-	stack pop: elements size
+	^ emitNode emitForValue: stack on: aStream
