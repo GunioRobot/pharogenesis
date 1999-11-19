9b4a71aa4d11e445c8db78215e92dd43af0d0197
di <unknown>
1999-11-19 11:13:00 +0000

    no message

--- BraceNode/instance/code generation/sizeForValue-3A-.cs
+++ BraceNode/instance/code generation/sizeForValue-3A-.cs
@@ -1,14 +1,24 @@
 sizeForValue: encoder
-	"elem1, ..., elemN, collectionClass, N, fromBraceStack:"
 
-	nElementsNode _ encoder encodeLiteral: elements size.
-	collClassNode isNil ifTrue:
-		[collClassNode _ encoder encodeVariable: #Array].
-	fromBraceStackNode _ encoder encodeSelector: #fromBraceStack:.
-	^elements inject:
-		(nElementsNode sizeForValue: encoder) +
-		(collClassNode sizeForValue: encoder) +
-		(fromBraceStackNode size: encoder args: 1 super: false)
-	 into:
-		[:subTotal :element |
-		 subTotal + (element sizeForValue: encoder)]
+	emitNode _ elements size <= 4
+		ifTrue: ["Short form: Array braceWith: a with: b ... "
+				MessageNode new
+					receiver: (encoder encodeVariable: #Array)
+					selector: (self selectorForShortForm: elements size)
+					arguments: elements precedence: 3 from: encoder]
+		ifFalse: ["Long form: (Array braceStream: N) nextPut: a; nextPut: b; ...; braceArray"
+				CascadeNode new
+					receiver: (MessageNode new
+								receiver: (encoder encodeVariable: #Array)
+								selector: #braceStream:
+								arguments: (Array with: (encoder encodeLiteral: elements size))
+								precedence: 3 from: encoder)
+					messages: ((elements collect: [:elt | MessageNode new receiver: nil
+														selector: #nextPut:
+														arguments: (Array with: elt)
+														precedence: 3 from: encoder])
+								copyWith: (MessageNode new receiver: nil
+														selector: #braceArray
+														arguments: (Array new)
+														precedence: 1 from: encoder))].
+	^ emitNode sizeForValue: encoder
