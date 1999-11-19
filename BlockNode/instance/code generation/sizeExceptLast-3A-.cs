80c08fa03e995cbb73195dc0c7d0330994aa7d09
di <unknown>
1999-11-19 19:33:00 +0000

    no message

--- BlockNode/instance/code generation/sizeExceptLast-3A-.cs
+++ BlockNode/instance/code generation/sizeExceptLast-3A-.cs
@@ -3,9 +3,6 @@ sizeExceptLast: encoder
 	nextToLast _ statements size - 1.
 	nextToLast < 1 ifTrue: [^ 0]. "Only one statement"
 	codeSize _ 0.
-	1 to: nextToLast - 1 do: 
+	1 to: nextToLast do: 
 		[:i | codeSize _ codeSize + ((statements at: i) sizeForEffect: encoder)].
-	^ (returns  "Don't pop before a return"
-			and: [(statements at: nextToLast) prefersValue])
-		ifTrue: [codeSize + ((statements at: nextToLast) sizeForValue: encoder)]
-		ifFalse: [codeSize + ((statements at: nextToLast) sizeForEffect: encoder)]
+	^ codeSize
