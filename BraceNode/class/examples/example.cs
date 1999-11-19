3f63bd74ddc97e70be219219cda02c9888daa918
di <unknown>
1999-11-19 09:05:00 +0000

    no message

--- BraceNode/class/examples/example.cs
+++ BraceNode/class/examples/example.cs
@@ -1,13 +1,8 @@
 example
 	"Test the {a. b. c} syntax."
 
-	| a b c d e x y |
+	| x |
 	x _ {1. {2. 3}. 4}.
-	{a. {b. c}. d. e} _ x, {5}, {}.
-	y _ {a} _ {0}.
-	{} _ {}.
-	^{e. d. c. b. a + 1. y first} as: Set
+	^ {x first. x second first. x second last. x last. 5} as: Set
 
-"BraceNode example"
-"Smalltalk garbageCollect.
- Time millisecondsToRun: [20 timesRepeat: [BraceNode example]] 1097 2452"
+"BraceNode example Set (0 1 2 3 4 5 )"
