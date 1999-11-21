729dc856d2ed98b571fe0fae04cdbf60742401be
RAA <unknown>
1999-11-21 22:55:00 +0000

    no message

--- RealEstateAgent/class/as yet unclassified/standardWindowExtent.cs
+++ RealEstateAgent/class/as yet unclassified/standardWindowExtent.cs
@@ -2,16 +2,18 @@ standardWindowExtent
 	"Answer the standard default extent for new windows.  "
 
 	| effectiveExtent width strips height grid allowedArea maxLevel |
-	effectiveExtent _ Display usableArea extent - (ScrollBarSetback @ ScreenTopSetback).
+	effectiveExtent _ self maximumUsableArea extent
+					- (self scrollBarSetback @ self screenTopSetback).
 	Preferences reverseWindowStagger ifTrue:
 		["NOTE: following copied from strictlyStaggeredInitialFrameFor:"
-		allowedArea _ ScrollBarSetback @ ScreenTopSetback
-						corner: Display usableArea bottomRight.
+		allowedArea _ self maximumUsableArea insetBy: (
+			self scrollBarSetback @ self screenTopSetback extent: 0@0
+		).
 		"Number to be staggered at each corner (less on small screens)"
 		maxLevel _ allowedArea area > 300000 ifTrue: [3] ifFalse: [2].
 		"Amount by which to stagger (less on small screens)"
 		grid _ allowedArea area > 500000 ifTrue: [40] ifFalse: [20].
-		^ (allowedArea extent - (grid*(maxLevel+1*2) + (grid//2))) min: 600@400].
+		^ (allowedArea extent - (grid*(maxLevel+1*2) + (grid//2))) min: StandardSize "600@400"].
 	width _ (strips _ self windowColumnsDesired) > 1
 		ifTrue:
 			[effectiveExtent x // strips]
