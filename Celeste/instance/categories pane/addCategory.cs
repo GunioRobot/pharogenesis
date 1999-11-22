56499065b546acf4eed2e2513b625ed173080911
mdr <unknown>
1999-11-22 14:05:00 +0000

    no message

---
+++ Celeste/instance/categories pane/addCategory.cs
@@ -0,0 +1,9 @@
+addCategory
+	"Create a new category with the user-specified name. This does nothing if the category already exists."
+
+	| newCatName |
+	newCatName _ FillInTheBlank request: 'Name for new category?'.
+	(newCatName isEmpty) ifTrue: [^self].	"user aborted"
+
+	self requiredCategory: newCatName.
+	self setCategory: newCatName.
