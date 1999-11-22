321f29cdb54d954ed46e6c56526e4c31fcb44035
mdr <unknown>
1999-11-22 13:14:00 +0000

    no message

---
+++ Celeste/instance/table of contents pane/search.cs
@@ -0,0 +1,24 @@
+search
+	"Search the text of all messages in the present category"
+
+	| destCat matchString msgText |
+
+	destCat _ FillInTheBlank
+		request: 'In what category should the search results be filed?'
+		initialAnswer: '.search results.'.
+	(destCat isEmpty) ifTrue: [^self].
+	matchString _ FillInTheBlank
+		request: 'String sought in message text?'
+		initialAnswer: ''.
+	(matchString isEmpty) ifTrue: [^self].
+
+	self requiredCategory: destCat.
+
+	(self filteredMessagesIn: currentCategory) do:
+		[: msgID |
+		 msgText _ mailDB getText: msgID.
+		 ((msgText findString: matchString startingAt: 1) > 0) ifTrue:
+			[mailDB file: msgID inCategory: destCat]].
+
+	self setCategory: destCat.
+
