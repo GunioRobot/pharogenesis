53320a448c8897c206765dc786ed5e9d5f50a034
sw <unknown>
1999-11-24 14:48:00 +0000

    no message

--- Browser/instance/class list/selectedClassName.cs
+++ Browser/instance/class list/selectedClassName.cs
@@ -1,5 +1,6 @@
 selectedClassName
+	| aClassList |
 	"Answer the name of the current class. Answer nil if no selection exists."
 
-	classListIndex = 0 ifTrue: [^nil].
-	^self classList at: classListIndex
+	(classListIndex = 0 or: [classListIndex > (aClassList _ self classList) size]) ifTrue: [^ nil].
+	^ aClassList at: classListIndex
