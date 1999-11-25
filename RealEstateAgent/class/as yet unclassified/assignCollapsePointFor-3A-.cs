fd0a78c59c7ab08406268415dbbed3a80853b186
di <unknown>
1999-11-25 23:51:00 +0000

    no message

--- RealEstateAgent/class/as yet unclassified/assignCollapsePointFor-3A-.cs
+++ RealEstateAgent/class/as yet unclassified/assignCollapsePointFor-3A-.cs
@@ -1,19 +1,21 @@
 assignCollapsePointFor: aSSView
 	"Offer up a location along the left edge of the screen for a collapsed SSView.
 	Make sure it doesn't overlap any other collapsed frames."
-	| grid otherFrames y free topLeft |
+	| grid otherFrames y free topLeft viewBox |
 	grid _ 24.  "should be mult of 8, since manual move is gridded by 8"
-	otherFrames _ Smalltalk isMorphic
-		ifTrue: [(SystemWindow windowsIn: World satisfying: [:w | true])
+	Smalltalk isMorphic
+		ifTrue: [otherFrames _ (SystemWindow windowsIn: World satisfying: [:w | true])
 					collect: [:w | w collapsedFrame]
-					thenSelect: [:rect | rect notNil]]
-		ifFalse: [ScheduledControllers scheduledWindowControllers
+					thenSelect: [:rect | rect notNil].
+				viewBox _ World viewBox]
+		ifFalse: [otherFrames _ ScheduledControllers scheduledWindowControllers
 					collect: [:aController | aController view collapsedFrame]
-					thenSelect: [:rect | rect notNil]].
-	y _ 0.
-	[(y _ y + grid) <= (Display height - grid)]
+					thenSelect: [:rect | rect notNil].
+				viewBox _ Display boundingBox].
+	y _ viewBox top.
+	[(y _ y + grid) <= (viewBox height - grid)]
 		whileTrue:
-		[topLeft _ 0@y.
+		[topLeft _ viewBox left@y.
 		free _ true.
 		otherFrames do: [:w | free _ free & (topLeft ~= w topLeft)].
 		free ifTrue: [^ topLeft]].
