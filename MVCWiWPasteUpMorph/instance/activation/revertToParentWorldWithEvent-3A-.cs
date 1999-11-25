ba0d92467d4295cde90bcbc369d04d2bf4fa4562
RAA <unknown>
1999-11-25 10:09:00 +0000

    no message

---
+++ MVCWiWPasteUpMorph/instance/activation/revertToParentWorldWithEvent-3A-.cs
@@ -0,0 +1,12 @@
+revertToParentWorldWithEvent: evt
+
+">>unused, but we may want some of this later
+	self damageRecorder reset.
+	World _ parentWorld.
+	World assuredCanvas.
+	World installFlaps.
+	owner changed.
+	hostWindow setStripeColorsFrom: Color red.
+	World restartWorldCycleWithEvent: evt.
+<<<"
+
