c52f654d639517f9f563a83657057679bd3e30d5
sw <unknown>
1999-11-26 15:44:00 +0000

    no message

---
+++ StandardScriptingSystem/instance/utilities/stripGraphicsForExternalRelease.cs
@@ -0,0 +1,9 @@
+stripGraphicsForExternalRelease
+	"ScriptingSystem stripGraphicsForExternalRelease"
+
+	|  replacement |
+	replacement _ FormDictionary at: #Gets.
+
+	#('BadgeMiniPic' 'BadgePic' 'Broom' 'CedarPic' 'CollagePic' 'CoverMain' 'CoverSpiral' 'CoverTexture' 'Fred' 'ImagiPic' 'KayaPic' 'StudioPic')
+		do:
+			[:aKey | FormDictionary at: aKey asSymbol put: replacement]
