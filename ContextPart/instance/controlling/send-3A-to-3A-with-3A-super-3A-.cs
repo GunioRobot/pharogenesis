f6279ba8fc2cd76ba2f91df568182115eec290e4
di <unknown>
1999-11-26 19:34:00 +0000

    no message

--- ContextPart/instance/controlling/send-3A-to-3A-with-3A-super-3A-.cs
+++ ContextPart/instance/controlling/send-3A-to-3A-with-3A-super-3A-.cs
@@ -4,29 +4,23 @@ send: selector to: rcvr with: args super: superFlag
 	receiver of the message was specified with 'super' in the source method."
 
 	| class meth val |
-	class _ 
-		superFlag
+	class _ superFlag
 			ifTrue: [(self method literalAt: self method numLiterals) value superclass]
 			ifFalse: [rcvr class].
-	[class == nil]
-		whileFalse: 
-			[(class includesSelector: selector)
-				ifTrue: 
-					[meth _ class compiledMethodAt: selector.
-					val _ 
-						self tryPrimitiveFor: meth
-							receiver: rcvr
-							args: args.
-					val == #simulatorFail ifFalse: [^val].
-					(selector == #doesNotUnderstand: and: [class == Object]) ifTrue:
-						[ ^ self error: 'Simulated message ' , (args at: 1) selector , ' not understood' ].
-					^self
-						activateMethod: meth
-						withArgs: args
+	meth _ class lookupSelector: selector.
+	meth == nil
+		ifTrue: [^ self send: #doesNotUnderstand:
+					to: rcvr
+					with: (Array with: (Message selector: selector arguments: args))
+					super: superFlag]
+		ifFalse: [val _ self tryPrimitiveFor: meth
 						receiver: rcvr
-						class: class].
-			class _ class superclass].
-	^self send: #doesNotUnderstand:
-		to: rcvr
-		with: (Array with: (Message selector: selector arguments: args))
-		super: superFlag
+						args: args.
+				val == PrimitiveFailToken ifFalse: [^ val].
+				(selector == #doesNotUnderstand: and: [class == ProtoObject]) ifTrue:
+					[^ self error: 'Simulated message ' , (args at: 1) selector
+									, ' not understood'].
+				^ self activateMethod: meth
+					withArgs: args
+					receiver: rcvr
+					class: class]
