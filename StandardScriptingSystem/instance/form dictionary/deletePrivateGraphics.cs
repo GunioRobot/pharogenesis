4a236fb8fae4914407001d5eb50d1210875bfaa3
sw <unknown>
1999-11-26 15:45:00 +0000

    no message

---
+++ StandardScriptingSystem/instance/form dictionary/deletePrivateGraphics.cs
@@ -0,0 +1,5 @@
+deletePrivateGraphics
+	"ScriptingSystem deletePrivateGraphics"
+	self deletePrivateGraphics:
+			#(#BadgeMiniPic #BadgePic #Broom #CedarPic #CollagePic #CoverMain #CoverSpiral #CoverTexture #Fred #ImagiPic #KayaPic #StudioPic)
+		afterStoringToFileNamed: 'disGraphics'
