fcc72d0fdb0dc299031fdf5ce32f47c76ee5e677
di <unknown>
1999-11-26 10:00:00 +0000

    no message

--- MorphWorldController/instance/control sequence/controlInitialize.cs
+++ MorphWorldController/instance/control sequence/controlInitialize.cs
@@ -1,10 +1,12 @@
 controlInitialize
 	"This window is becoming active."
 
+	true ifTrue: [model becomeTheActiveWorldWith: nil].
+
 	model canvas ifNil: [  "i.e., only on first entry"
 		"In case of, eg, inspect during balloon help..."
 		model submorphsDo: [:m |  "delete any existing balloons"
 			(m isKindOf: BalloonMorph) ifTrue: [m delete]].
 
-		model hands do: [:h | h initForEvents].
+		model handsDo: [:h | h initForEvents].
 		view displayView].  "initializes the WorldMorph's canvas"
