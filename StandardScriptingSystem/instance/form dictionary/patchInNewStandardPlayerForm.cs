df8477aa372d238524acda99648b6ba070a5ef48
sw <unknown>
1999-11-26 15:43:00 +0000

    no message

---
+++ StandardScriptingSystem/instance/form dictionary/patchInNewStandardPlayerForm.cs
@@ -0,0 +1,5 @@
+patchInNewStandardPlayerForm
+	"ScriptingSystem patchInNewStandardPlayerForm"
+	FormDictionary at: #standardPlayer put:
+		(Form extent: 3@3
+	depth: 16 fromArray: #( 17969 0 1177634353 1177616384 17969 0) offset: 0@0)
