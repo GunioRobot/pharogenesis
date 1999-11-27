a7e529bb2724be1bda73df5cf1cb21b09d07625d
RAA <unknown>
1999-11-27 19:16:00 +0000

    no message

--- Morph/instance/geometry/setConstrainedPositionFrom-3A-.cs
+++ Morph/instance/geometry/setConstrainedPositionFrom-3A-.cs
@@ -1,7 +1,11 @@
 setConstrainedPositionFrom: aPoint
 	"Change the position of this morph and and all of its submorphs to aPoint, but don't let me go outside my owner's bounds."
 
-	| trialRect delta |
+	| trialRect delta boundingMorph |
 	trialRect _ aPoint extent: self bounds extent.
-	delta _ trialRect amountToTranslateWithin: owner bounds.
-	self position: aPoint + delta
+	boundingMorph _ self owner topRendererOrSelf.
+	delta _ boundingMorph
+			ifNil:    [0@0]
+			ifNotNil: [trialRect amountToTranslateWithin: boundingMorph bounds].
+	self position: aPoint + delta.
+	self layoutChanged  "So that, eg, surrounding text will readjust"
