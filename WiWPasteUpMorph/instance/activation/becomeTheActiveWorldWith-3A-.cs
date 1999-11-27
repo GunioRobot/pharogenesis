fcaa29e1f7da478442b836723964e315e5ad7186
RAA <unknown>
1999-11-27 15:48:00 +0000

    no message

---
+++ WiWPasteUpMorph/instance/activation/becomeTheActiveWorldWith-3A-.cs
@@ -0,0 +1,23 @@
+becomeTheActiveWorldWith: evt
+	| outerWorld |
+	World == self ifTrue: [^ self].
+	self damageRecorder reset.	"since we may have moved, old data no longer valid"
+	hostWindow setStripeColorsFrom: Color green.
+	self canvas: nil.	"safer to start from scratch"
+	displayChangeSignatureOnEntry _ Display displayChangeSignature.
+
+	"Messy stuff to clear flaps from outer world"
+	Utilities globalFlapTabs do: [:f | f changed].
+	outerWorld _ World.
+	World _ self.
+		self installFlaps.
+	World _ outerWorld.
+		outerWorld displayWorld.
+	World _ self.
+
+	self viewBox: hostWindow panelRect.
+	self startSteppingSubmorphsOf: self.
+	self changed.
+	pendingEvent _ nil.
+	evt ifNotNil: [self primaryHand handleEvent: (evt setHand: self primaryHand)].
+
