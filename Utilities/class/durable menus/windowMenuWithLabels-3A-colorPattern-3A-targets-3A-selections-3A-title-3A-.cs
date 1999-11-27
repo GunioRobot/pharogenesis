ca376529f160df71c99948c7928009cd2357e1e1
di <unknown>
1999-11-27 08:23:00 +0000

    no message

--- Utilities/class/durable menus/windowMenuWithLabels-3A-colorPattern-3A-targets-3A-selections-3A-title-3A-.cs
+++ Utilities/class/durable menus/windowMenuWithLabels-3A-colorPattern-3A-targets-3A-selections-3A-title-3A-.cs
@@ -1,13 +1,11 @@
 windowMenuWithLabels: labelList colorPattern: colorPattern  targets: targetList selections: selectionList title: aTitle
-	| aWorld n   colorList  pos delta aButton  rightmost |
-
-	aWorld _ WorldMorph new.
-	n _ labelList size.
-
-	colorList _  (1 to: n) asArray  collect:
-		[:ind | Color perform: (colorPattern at: (ind \\ colorPattern size + 1))].
+	| aWorld colorList  pos delta aButton  rightmost widthToUse |
+	Smalltalk verifyMorphicAvailability ifFalse: [^ self].
+	aWorld _ MVCWiWPasteUpMorph newWorldForProject: nil.
+	colorList _  (1 to: labelList size) collect:
+		[:ind | Color colorFrom: (colorPattern at: (ind \\ colorPattern size + 1))].
 			
-	pos _ 4 @ 2.
+	pos _ 4 @ 6.
 	delta _ 0 @ 30.
 	rightmost _ 0.
 
@@ -22,5 +20,8 @@ windowMenuWithLabels: labelList colorPattern: colorPattern  targets: targetList 
 			rightmost _ rightmost max: aButton right.
 			pos _ pos + delta.
 			aWorld addMorphBack: aButton].
-	aWorld setProperty: #initialExtent toValue: ((rightmost @ aButton bottom) + (10@10)).
-	aWorld openWithTitle: aTitle
+	widthToUse _ rightmost + 10.
+	aWorld submorphs do:
+		[:m | m position: (((widthToUse - m width) // 2) @ m position y)].
+	aWorld setProperty: #initialExtent toValue: (widthToUse @ (aButton bottom + 10)).
+	aWorld openWithTitle: aTitle cautionOnClose: false
