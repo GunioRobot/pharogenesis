7d487859fed7fb38dcab0f76355b42bbfc09ab48
ar <unknown>
1999-11-28 19:38:00 +0000

    no message

--- MethodNode/instance/printing/printPrimitiveOn-3A-.cs
+++ MethodNode/instance/printing/printPrimitiveOn-3A-.cs
@@ -1,3 +1,23 @@
 printPrimitiveOn: aStream
-	aStream nextPutAll: '<primitive: '; print: primitive.
-	aStream nextPutAll: '>'
+	"Print the primitive on aStream"
+	| primIndex primDecl |
+	primIndex _ primitive.
+	primIndex = 0 ifTrue:[^self].
+	primIndex = 120 "External call spec"
+		ifTrue:[^aStream print: encoder literals first].
+	aStream nextPutAll: '<primitive: '.
+	primIndex = 117 ifTrue:[
+		primDecl _ encoder literals at: 1.
+		aStream 
+			nextPut: $';
+			nextPutAll: (primDecl at: 2);
+			nextPut:$'.
+		(primDecl at: 1) notNil ifTrue:[
+			aStream 
+				nextPutAll:' module:';
+				nextPut:$';
+				nextPutAll: (primDecl at: 1);
+				nextPut:$'.
+		].
+	] ifFalse:[aStream print: primIndex].
+	aStream nextPut: $>.
