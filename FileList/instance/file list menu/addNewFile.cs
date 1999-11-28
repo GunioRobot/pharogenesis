5e0e029346eb816ad617d80aac0100b8c8198890
sge <unknown>
1999-11-28 09:04:00 +0000

    no message

--- FileList/instance/file list menu/addNewFile.cs
+++ FileList/instance/file list menu/addNewFile.cs
@@ -1,13 +1,4 @@
 addNewFile
-	"Add a new file and update the list"
-	| newName index ending |
-	self okToChange ifFalse: [^ self].
-	newName _ (FillInTheBlank request: 'New File Name?'
- 					initialAnswer: 'FileName') asFileName.
-	Cursor wait showWhile: [
-		(directory newFileNamed: newName) close].
-	self updateFileList.
-	index _ list indexOf: newName.
-	index = 0 ifTrue: [ending _ ') ',newName.
-		index _ list findFirst: [:line | line endsWith: ending]].
-	self fileListIndex: index.
+	self 
+		addNew: 'File'
+		byEvaluating: [:newName | (directory newFileNamed: newName) close]
