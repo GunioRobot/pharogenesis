a7760b2650b62cf0a87f98814beb369f8167ef38
sw <unknown>
1999-11-29 18:27:00 +0000

    no message

--- BorderedMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ BorderedMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,7 +1,11 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
-	aCustomMenu addList: #(('border color...' changeBorderColor:)
-						('border width...' changeBorderWidth:)).
+	aCustomMenu addList: 
+		#(('border color...' changeBorderColor:)
+		('border width...' changeBorderWidth:)).
+	self couldHaveRoundedCorners ifTrue:
+		[aCustomMenu addUpdating: #roundedCornersString target: self action: #toggleCornerRounding].
+
 	self doesBevels ifTrue:
 		[borderColor == #raised ifFalse: [aCustomMenu add: 'raised bevel' action: #borderRaised].
 		borderColor == #inset ifFalse: [aCustomMenu add: 'inset bevel' action: #borderInset]]
