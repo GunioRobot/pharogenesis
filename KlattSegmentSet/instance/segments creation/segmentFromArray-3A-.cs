c3431198b20926ac2a84906b7c3ac649007729b3
len <unknown>
1999-11-29 01:40:00 +0000

    no message

---
+++ KlattSegmentSet/instance/segments creation/segmentFromArray-3A-.cs
@@ -0,0 +1,7 @@
+segmentFromArray: anArray
+	| answer |
+	answer _ KlattSegment new.
+	#(name: rank: duration: features:)
+		doWithIndex: [ :each :index | answer perform: each with: (anArray at: index)].
+	5 to: anArray size do: [ :each | answer addParameter: (self parameterFromArray: (anArray at: each))].
+	^ answer
