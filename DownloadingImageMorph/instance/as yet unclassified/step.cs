e89ed8835a94397253f596848b12bf7e9b13ddb5
bolot <unknown>
1999-11-30 23:22:00 +0000

    no message

---
+++ DownloadingImageMorph/instance/as yet unclassified/step.cs
@@ -0,0 +1,9 @@
+step
+	| doc |
+	downloadQueue size > 0 ifTrue: [
+		doc _ downloadQueue next.
+		doc mainType = 'image' 
+		ifTrue: [
+			[image _ ImageReadWriter  formFromStream: doc contentStream binary]
+				ifError: [:err :rcvr | "ignore" image _ nil].
+			self setContents ] ].
