f52030cdb6708429cb9c821e19e105225b59962b
stp <unknown>
1999-12-01 12:19:00 +0000

    no message

--- ScrollBar/instance/initialize/initializeUpButton.cs
+++ ScrollBar/instance/initialize/initializeUpButton.cs
@@ -1,13 +1,14 @@
 initializeUpButton
 	upButton := RectangleMorph
-		newBounds: ((bounds isWide ifTrue: [menuButton bounds topRight]
-									ifFalse: [menuButton bounds bottomLeft])
+		newBounds: ((menuButton ifNil: [self innerBounds topLeft]
+				ifNotNil: [bounds isWide ifTrue: [menuButton bounds topRight]
+									ifFalse: [menuButton bounds bottomLeft]])
 					extent: self buttonExtent)
 		color: Color lightGray.
-	upButton on: #mouseDown send: #borderInset to: upButton.
+	upButton on: #mouseDown send: #scrollUpInit to: self.
 	upButton on: #mouseStillDown send: #scrollUp to: self.
 	upButton on: #mouseUp send: #borderRaised to: upButton.
 	upButton addMorphCentered: (ImageMorph new image: 
 		(bounds isWide ifTrue: [UpArrow rotateBy: #left centerAt: 0@0] ifFalse: [UpArrow])).
-	upButton setBorderWidth: 2 borderColor: #raised.
+	upButton setBorderWidth: 1 borderColor: #raised.
 	self addMorph: upButton
