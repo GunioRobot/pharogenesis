5a55a1ce56303c4d2e7dc5951ed933734712a371
sw <unknown>
1999-12-01 13:13:00 +0000

    no message

---
+++ MenuTile/instance/as yet unclassified/mouseDown-3A-.cs
@@ -0,0 +1,10 @@
+mouseDown: evt
+	| aPoint aMenu reply |
+	aPoint _ evt cursorPoint.
+	nArrowTicks _ 0.
+	upArrow ifNotNil:
+		[(upArrow boundsInWorld containsPoint: aPoint) ifTrue: [^ self].
+		(downArrow boundsInWorld containsPoint: aPoint) ifTrue: [^ self]].
+	aMenu _ SelectionMenu selections: (self ownerThatIsA: PhraseTileMorph) associatedPlayer costume allMenuWordings.
+	reply _ aMenu startUp.
+	reply ifNotNil: [self literal: reply; layoutChanged]
