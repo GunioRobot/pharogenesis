59eed400888544f03f009b6328335cd027f07372
ar <unknown>
1999-12-02 22:19:00 +0000

    no message

---
+++ FFIPlugin/class/accessing/moduleName.cs
@@ -0,0 +1,5 @@
+moduleName "FFIPlugin translate"
+	"IMPORTANT: IF YOU CHANGE THE NAME OF THIS PLUGIN YOU MUST CHANGE
+		Interpreter>>primitiveCalloutToFFI
+	TO REFLECT THE CHANGE."
+	^'SqueakFFIPrims'
