8dd303db1db74d8814442f6c58c33522d1771140
len <unknown>
1999-12-02 03:39:00 +0000

    no message

---
+++ KlattFrameMorph/instance/initialization/newSliderForParameter-3A-target-3A-min-3A-max-3A-description-3A-.cs
@@ -0,0 +1,24 @@
+newSliderForParameter: parameter target: target min: min max: max description: description
+	| r slider m |
+	r _ AlignmentMorph newRow.
+	r color: self color; borderWidth: 0; inset: 0.
+	r hResizing: #spaceFill; vResizing: #rigid; extent: 5@20; centering: #center.
+
+	slider _ SimpleSliderMorph new
+		color: (Color r: 0.065 g: 0.548 b: 0.645);
+		extent: 120@2;
+		target: target;
+		actionSelector: (parameter, ':') asSymbol;
+		minVal: min;
+		maxVal: max;
+		adjustToValue: (target perform: parameter asSymbol).
+	r addMorphBack: slider.
+
+	m _ StringMorph new contents: parameter, ': '.
+	r addMorphBack: m.
+	m _ UpdatingStringMorph new
+		target: target; getSelector: parameter asSymbol; putSelector: (parameter, ':') asSymbol;
+		width: 30; growable: false; floatPrecision: (max - min / 100.0 min: 1.0); step.
+	r addMorphBack: m.
+	r setBalloonText: description.
+	^ r
