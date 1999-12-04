3a7852a16d15d7c58b6fe931610cb4ec72d1a636
di <unknown>
1999-12-04 16:51:00 +0000

    no message

--- Class/instance/compiling/scopeHas-3A-ifTrue-3A-.cs
+++ Class/instance/compiling/scopeHas-3A-ifTrue-3A-.cs
@@ -1,30 +1,30 @@
-scopeHas: varName ifTrue: assocBlock 
+scopeHas: varName ifTrue: assocBlock
 	"Look up the first argument, varName, in the context of the receiver. If it is there,
-	pass the association to the second argument, assocBlock, and answer true.
-	Else answer false.
-	: Allow key in shared pools to be a string for HyperSqueak"
+	pass the association to the second argument, assocBlock, and answer true."
 
 	| assoc |
-	assoc _ self classPool associationAt: varName ifAbsent: [].
-	assoc == nil
-		ifFalse: 
-			[assocBlock value: assoc.
-			^true].
+	"First look in classVar dictionary."
+	(assoc _ self classPool associationAt: varName ifAbsent: []) == nil
+		ifFalse: [assocBlock value: assoc.
+				^ true].
+
+	"Next look in shared pools."
 	self sharedPools do: 
 		[:pool | 
-		varName = #Textual ifTrue: [self halt].
 		assoc _ pool associationAt: varName ifAbsent: [
+			"String key hack from Hypersqueak now used in Wonderland  **Eliminate this**"
 			pool associationAt: varName asString ifAbsent: []].
-		assoc == nil
-			ifFalse: 
+		assoc == nil ifFalse: 
 				[assocBlock value: assoc.
 				^true]].
+
+	"Next look in declared environment."
+	(assoc _ self environment associationAtOrAbove: varName ifAbsent: [nil]) == nil
+		ifFalse: [assocBlock value: assoc.
+				^ true].
+
+	"Finally look higher up the superclass chain and fail at the end."
 	superclass == nil
-		ifTrue: 
-			[assoc _ Smalltalk associationAt: varName ifAbsent: [].
-			assoc == nil
-				ifFalse: 
-					[assocBlock value: assoc.
-					^true].
-			^false].
-	^superclass scopeHas: varName ifTrue: assocBlock
+		ifTrue: [^ false]
+		ifFalse: [^ superclass scopeHas: varName ifTrue: assocBlock].
+
