e261a234cdd6463ba3ec285b90e79df3e01d10c8
di <unknown>
1999-12-04 21:04:00 +0000

    no message

--- Parser/instance/expression types/variable.cs
+++ Parser/instance/expression types/variable.cs
@@ -4,5 +4,6 @@ variable
 	varStart _ self startOfNextToken + requestorOffset.
 	varName _ self advance.
 	varEnd _ self endOfLastToken + requestorOffset.
-	^encoder encodeVariable: varName ifUnknown:
-		[self correctVariable: varName interval: (varStart to: varEnd)]
+	^ encoder encodeVariable: varName
+		sourceRange: (varStart to: varEnd)
+		ifUnknown: [self correctVariable: varName interval: (varStart to: varEnd)]
