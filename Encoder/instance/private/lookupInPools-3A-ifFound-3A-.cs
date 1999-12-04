d541eb8480fb0dd093f589f4ec79b3d45a8f70bc
di <unknown>
1999-12-04 16:51:00 +0000

    no message

--- Encoder/instance/private/lookupInPools-3A-ifFound-3A-.cs
+++ Encoder/instance/private/lookupInPools-3A-ifFound-3A-.cs
@@ -1,6 +1,8 @@
-lookupInPools: name ifFound: assocBlock
-	| |
-	Symbol 
-		hasInterned: name 
-		ifTrue: [:sym | ^class scopeHas: sym ifTrue: assocBlock].
-	^ class scopeHas: name ifTrue: assocBlock.  "Its a string in the pool"
+lookupInPools: varName ifFound: assocBlock
+
+	Symbol hasInterned: varName ifTrue:
+		[:sym | (class scopeHas: sym ifTrue: assocBlock) ifTrue: [^ true].
+		(Preferences valueOfFlag: #lenientScopeForGlobals)  "**Temporary**"
+			ifTrue: [^ Smalltalk lenientScopeHas: sym ifTrue: assocBlock]
+			ifFalse: [^ false]].
+	^ class scopeHas: varName ifTrue: assocBlock.  "Maybe a string in a pool  **Eliminate this**"
