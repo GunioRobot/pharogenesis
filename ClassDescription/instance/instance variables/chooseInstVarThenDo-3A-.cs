a9aefa77dbffa408e74a7fbf42f337697cb44109
ls <unknown>
1999-12-05 13:40:00 +0000

    no message

--- ClassDescription/instance/instance variables/chooseInstVarThenDo-3A-.cs
+++ ClassDescription/instance/instance variables/chooseInstVarThenDo-3A-.cs
@@ -1,11 +1,24 @@
 chooseInstVarThenDo: aBlock 
-	"Put up a menu of all the instance variables in the receiver, and when the user chooses one, evaluate aBlock with the chosen variable as its parameter.  7/30/96 sw"
+	"Put up a menu of all the instance variables in the receiver, and when
+the user chooses one, evaluate aBlock with the chosen variable as its
+parameter.  If the list is 6 or larger, then offer an alphabetical
+formulation as an alternative. triggered by a 'show alphabetically' item
+at the top of the list."
 
-	| lines labelStream vars allVars index |
+	| lines labelStream vars allVars index count offerAlpha |
+	(count _ self allInstVarNames size) = 0 ifTrue: 
+		[^ self inform: 'There are no
+instance variables.'].
 
-	lines _ OrderedCollection new.
 	allVars _ OrderedCollection new.
+	lines _ OrderedCollection new.
 	labelStream _ WriteStream on: (String new: 200).
+
+	(offerAlpha _ count > 5)
+		ifTrue:
+			[lines add: 1.
+			allVars add: 'show alphabetically'.
+			labelStream nextPutAll: allVars first; cr].
 	self withAllSuperclasses reverseDo:
 		[:class |
 		vars _ class instVarNames.
@@ -14,12 +27,13 @@ chooseInstVarThenDo: aBlock
 			labelStream nextPutAll: var; cr.
 			allVars add: var].
 		vars isEmpty ifFalse: [lines add: allVars size]].
-	labelStream isEmpty ifTrue:
-		[^ (PopUpMenu labels: ' OK ')
-			startUpWithCaption: 'There are no
-instance variables.'].
 	labelStream skip: -1 "cut last CR".
-	index _ (PopUpMenu labels: labelStream contents lines: lines) startUpWithCaption: 'Instance variables in
-', class name.
+	(lines size > 0 and: [lines last = allVars size]) ifTrue:
+		[lines removeLast].  "dispense with inelegant line beneath last item"
+	index _ (PopUpMenu labels: labelStream contents lines: lines)
+startUpWithCaption: 'Instance variables in
+', self name.
 	index = 0 ifTrue: [^ self].
+	(index = 1 and: [offerAlpha]) ifTrue: [^ self
+chooseInstVarAlphabeticallyThenDo: aBlock].
 	aBlock value: (allVars at: index)
