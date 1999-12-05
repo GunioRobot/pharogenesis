4cb2934948773c5e53121b7fac146d11d09b9aad
ls <unknown>
1999-12-05 13:43:00 +0000

    no message

--- ContextPart/instance/debugger access/mclass.cs
+++ ContextPart/instance/debugger access/mclass.cs
@@ -1,5 +1,6 @@
 mclass 
 	"Answer the class in which the receiver's method was found."
-
-	self receiver class selectorAtMethod: self method setClass: [:mclass].
+	| mclass |
+	self receiver class selectorAtMethod: self method setClass: [:mc |
+mclass _ mc ].
 	^mclass
