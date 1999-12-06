2e6c0b26ea14e43bbdac114bcbf67a6f8668d3e2
sw <unknown>
1999-12-06 13:08:00 +0000

    no message

--- PopUpMenu/class/class initialization/initialize.cs
+++ PopUpMenu/class/class initialization/initialize.cs
@@ -1,11 +1,5 @@
 initialize  "PopUpMenu initialize"
-	"Change CacheMenuForms to true to get faster popup menus on slower systems."
-	"CacheMenuForms _ true"
-
-	CacheMenuForms _ false.
 	(MenuStyle _ TextStyle default copy)
-		gridForFont: 1 withLead: 0;
+		gridForFont: TextStyle default defaultFontIndex withLead: 0;
 		centered.
-	PopUpMenu withAllSubclasses do:
-		[:menuClass | menuClass allInstancesDo:
-			[:m | m rescan]]
+	PopUpMenu allSubInstancesDo: [:m | m rescan]
