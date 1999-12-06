b7db5221d8c26a0e2a6ad2b397f17a3fa0c2a5cd
di <unknown>
1999-12-06 12:40:00 +0000

    no message

---
+++ InterpreterProxy/class/private/validateProxyImplementation-3A-.cs
@@ -0,0 +1,12 @@
+validateProxyImplementation: anInterpreter 
+	"InterpreterProxy validateProxyImplementation: Interpreter"
+	"InterpreterProxy validateProxyImplementation: DynamicInterpreter"
+	| proxyClass catList |
+	proxyClass _ InterpreterProxy.
+	catList _ proxyClass organization categories copy asOrderedCollection.
+	catList remove: 'initialize' ifAbsent:[].
+	catList remove: 'private' ifAbsent:[].
+	catList do:[:categ|
+		(proxyClass organization listAtCategoryNamed: categ) do:[:selector|
+			(anInterpreter canUnderstand: selector) 
+				ifFalse:[self notify: selector, ' is not implemented in ', anInterpreter name]]].
