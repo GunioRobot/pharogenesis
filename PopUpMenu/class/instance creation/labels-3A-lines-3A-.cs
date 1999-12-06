784ec79cd5806de908eb6db03b860a3982cfee93
sw <unknown>
1999-12-06 17:55:00 +0000

    no message

--- PopUpMenu/class/instance creation/labels-3A-lines-3A-.cs
+++ PopUpMenu/class/instance creation/labels-3A-lines-3A-.cs
@@ -2,7 +2,7 @@ labels: aString lines: anArray
 	"Answer an instance of me whose items are in aString, with lines drawn 
 	after each item indexed by anArray."
 
-	^self new
+	^ self new
 		labels: aString
-		font: (MenuStyle fontAt: 1)
+		font: MenuStyle defaultFont
 		lines: anArray
