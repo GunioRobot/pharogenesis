afb99e2ca6675fef11193f14cc9a4826eaee58a3
di <unknown>
1999-12-06 20:41:00 +0000

    no message

--- Browser/instance/class list/selectedClass.cs
+++ Browser/instance/class list/selectedClass.cs
@@ -2,6 +2,7 @@ selectedClass
 	"Answer the class that is currently selected. Answer nil if no selection 
 	exists."
 
-	| name |
+	| name envt |
 	(name _ self selectedClassName) ifNil: [^ nil].
-	^ Smalltalk at: name
+	(envt _ self selectedEnvironment) ifNil: [^ nil].
+	^ envt at: name
