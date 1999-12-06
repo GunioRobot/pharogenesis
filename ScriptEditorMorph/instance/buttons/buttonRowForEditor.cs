9aadfed90e9aa68928043b359d7558b3b1392559
sw <unknown>
1999-12-06 18:02:00 +0000

    no message

--- ScriptEditorMorph/instance/buttons/buttonRowForEditor.cs
+++ ScriptEditorMorph/instance/buttons/buttonRowForEditor.cs
@@ -1,6 +1,6 @@
 buttonRowForEditor
 	| r aString aButtonMorph buttonFont aStatusMorph aStatus |
-	buttonFont _ ScriptingSystem fontForScriptorButtons.
+	buttonFont _ Preferences standardButtonFont.
 	r _ AlignmentMorph newRow color: Color transparent; inset: 0.
 
 	r addMorphFront:
@@ -8,6 +8,7 @@ buttonRowForEditor
 			label: '!' font: (StrikeFont familyName: #ComicBold size: 16);
 			target: self;
 			color: Color yellow;
+			borderWidth: 0;
 			actWhen: #whilePressed;
 			actionSelector: #tryMe;
 			balloonTextSelector: #tryMe).
@@ -15,7 +16,7 @@ buttonRowForEditor
 
 	aString _ playerScripted externalName, ' ', self scriptTitle.
 	r addMorphBack:
-		(aButtonMorph _ SimpleButtonMorph new label: aString font: buttonFont; target: self; setNameTo: 'title').
+		(aButtonMorph _ SimpleButtonMorph new useSquareCorners label: aString font: buttonFont; target: self; setNameTo: 'title').
 	aButtonMorph actWhen: #buttonDown; actionSelector: #offerScriptorMenu.
 	aButtonMorph borderColor: (Color fromRgbTriplet: #(0.065 0.258 1.0)).
 	aButtonMorph color: (self isAnonymous ifTrue: [Color blue muchLighter] ifFalse: [ScriptingSystem uniformTileInteriorColor]).
@@ -24,20 +25,22 @@ buttonRowForEditor
 
 	aStatus _ self scriptInstantiation status.
 	r addMorphBack:
-			(aStatusMorph _ SimpleButtonMorph new label: aStatus font: buttonFont;
+			(aStatusMorph _ SimpleButtonMorph new useSquareCorners label: aStatus font: buttonFont;
 				setNameTo: 'trigger';
 				target: self;
 				actWhen: #buttonDown;
 				actionSelector: #chooseTrigger;
+				setBalloonText: (ScriptingSystem helpStringFor: #chooseTrigger) maxLineLength: 65;
 				balloonTextSelector: #chooseTrigger).
 
 	r addTransparentSpacerOfSize: 10@10.
 	r addMorphBack:
-		((SimpleButtonMorph new label: '¥' font: buttonFont)
-			target: self;
-			color: Color veryLightGray;
+		(IconicButton new borderWidth: 0;
+			labelGraphic: (ScriptingSystem formAtKey: 'AddTest'); color: Color transparent; 
 			actWhen: #buttonDown;
+			target: self;
 			actionSelector: #addYesNoToHand;
+			shedSelvedge;
 			balloonTextSelector: #addYesNoToHand).
 	r addTransparentSpacerOfSize: 12@10.
 	self addDismissButtonTo: r.
