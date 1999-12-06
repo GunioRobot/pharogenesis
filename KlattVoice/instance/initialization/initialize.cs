d4be58b45c6e5c2aaf977f36dc8098090fe13218
len <unknown>
1999-12-06 00:00:00 +0000

    no message

---
+++ KlattVoice/instance/initialization/initialize.cs
@@ -0,0 +1,8 @@
+initialize
+	super initialize.
+	synthesizer _ KlattSynthesizer new cascade: 0.
+	self segments: KlattSegmentSet arpabet.
+	self patternFrame: self defaultPatternFrame.
+	self breathiness: 0.0.
+	self tract: 17.3. "Set a male vocal tract."
+	self reset
