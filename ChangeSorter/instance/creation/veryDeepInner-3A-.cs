15a27c3ccdf4b7d56473384015b38ac037e0739f
tk <unknown>
1999-12-07 12:51:00 +0000

    no message

---
+++ ChangeSorter/instance/creation/veryDeepInner-3A-.cs
@@ -0,0 +1,9 @@
+veryDeepInner: deepCopier
+	"Copy all of my instance variables.  Some need to be not copied at all, but shared."
+
+super veryDeepInner: deepCopier.
+"parent _ parent.		Weakly copied"
+"myChangeSet _ myChangeSet.		Weakly copied"
+currentClassName _ currentClassName veryDeepCopyWith: deepCopier.
+"currentSelector _ currentSelector.		Symbol"
+priorChangeSetList _ priorChangeSetList veryDeepCopyWith: deepCopier.
