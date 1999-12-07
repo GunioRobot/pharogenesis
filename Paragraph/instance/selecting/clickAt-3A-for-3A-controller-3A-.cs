453de7efad217d1d39e43446e6883ec9ace47b63
sw <unknown>
1999-12-07 12:26:00 +0000

    no message

--- Paragraph/instance/selecting/clickAt-3A-for-3A-controller-3A-.cs
+++ Paragraph/instance/selecting/clickAt-3A-for-3A-controller-3A-.cs
@@ -3,17 +3,14 @@ clickAt: clickPoint for: model controller: aController
 	| startBlock action range box boxes |
 	action _ false.
 	startBlock _ self characterBlockAtPoint: clickPoint.
-	(text attributesAt: startBlock stringIndex) 
+	(text attributesAt: startBlock stringIndex forStyle: textStyle) 
 		do: [:att | att mayActOnClick ifTrue:
-				[range _ text rangeOf: att startingAt: startBlock stringIndex.
+				[range _ text rangeOf: att startingAt: startBlock stringIndex forStyle: textStyle.
 				boxes _ self selectionRectsFrom: (self characterBlockForIndex: range first) 
 							to: (self characterBlockForIndex: range last).
 				box _ boxes detect: [:each | each containsPoint: clickPoint]
 							ifNone: [^ action].
 				Utilities awaitMouseUpIn: box repeating: []
-						ifSucceed: [
-							aController controlTerminate.
-							(att actOnClickFor: model) ifTrue: [action _ true].
-							aController controlInitialize].
-				]].
+					ifSucceed: [aController terminateAndInitializeAround:
+								[(att actOnClickFor: model) ifTrue: [action _ true]]]]].
 	^ action
