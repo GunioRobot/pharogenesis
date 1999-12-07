85757cedd1092f231999b435db2498b406a27d9a
sw <unknown>
1999-12-07 11:39:00 +0000

    no message

--- ParagraphEditor/instance/typing support/setEmphasisHere.cs
+++ ParagraphEditor/instance/typing support/setEmphasisHere.cs
@@ -1,4 +1,4 @@
 setEmphasisHere
 
-	emphasisHere _ (paragraph text attributesAt: (startBlock stringIndex-1 max: 1))
+	emphasisHere _ (paragraph text attributesAt: (startBlock stringIndex-1 max: 1) forStyle: paragraph textStyle)
 					select: [:att | att mayBeExtended]
