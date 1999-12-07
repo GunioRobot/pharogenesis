a2cd7f8f0094d7d5bc51f61a4e3880a99cb75d6c
sw <unknown>
1999-12-07 18:11:00 +0000

    no message

--- SimpleButtonMorph/instance/accessing/label-3A-.cs
+++ SimpleButtonMorph/instance/accessing/label-3A-.cs
@@ -3,7 +3,7 @@ label: aString
 	| oldLabel m |
 	(oldLabel _ self findA: StringMorph)
 		ifNotNil: [oldLabel delete].
-	m _ StringMorph new contents: aString.
+	m _ StringMorph contents: aString font: TextStyle defaultFont.
 	self extent: m extent + (borderWidth + 6).
 	m position: self center - (m extent // 2).
 	self addMorph: m.
