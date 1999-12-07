79aaefa3fc9d34f15aec9095551cb8f66f6aa072
sw <unknown>
1999-12-07 11:34:00 +0000

    no message

--- NewParagraph/instance/editing/clickAt-3A-for-3A-controller-3A-.cs
+++ NewParagraph/instance/editing/clickAt-3A-for-3A-controller-3A-.cs
@@ -1,24 +1,23 @@
 clickAt: clickPoint for: model controller: editor
 	"Give sensitive text a chance to fire.  Display flash: (100@100 extent: 100@100)."
-	| startBlock action range boxes box |
+	| startBlock action target |
 	action _ false.
 	startBlock _ self characterBlockAtPoint: clickPoint.
-	(text attributesAt: startBlock stringIndex) 
+	(text attributesAt: startBlock stringIndex forStyle: textStyle) 
 		do: [:att | att mayActOnClick ifTrue:
-				[range _ text rangeOf: att startingAt: startBlock stringIndex.
+				[
+	"			range _ text rangeOf: att startingAt: startBlock stringIndex.
 				boxes _ self selectionRectsFrom: (self characterBlockForIndex: range first) 
 							to: (self characterBlockForIndex: range last).
 				box _ boxes detect: [:each | each containsPoint: clickPoint].
-"				*This doesn't work in morphic*
+					*This doesn't work in morphic*
 				Need to replace by a highlighting morph that waits for moueUp.
 				Utilities awaitMouseUpIn: (editor transform invertRect: box)
 						repeating: []
 						ifSucceed: [(att actOnClickFor: model) ifTrue: [action _ true]].
-"
-				
-				(att actOnClickFor: model)
+	"
+				(target _ model) ifNil: [target _ editor morph].
+				(att actOnClickFor: target)
 					ifTrue: [Sensor waitNoButton.  "FIX THIS"
-							action _ true]
-				].
-			].
+							action _ true]]].
 	^ action
