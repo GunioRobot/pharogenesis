b3ac75fb4f474abdc5b0ac165463c65a3470273d
sw <unknown>
1999-12-07 12:27:00 +0000

    no message

--- TextMorphEditor/instance/mvc compatibility/zapSelectionWith-3A-.cs
+++ TextMorphEditor/instance/mvc compatibility/zapSelectionWith-3A-.cs
@@ -4,6 +4,10 @@ zapSelectionWith: aText
 	self deselect.
 	start _ startBlock stringIndex.
 	stop _ stopBlock stringIndex.
+	(aText isEmpty and: [stop > start]) ifTrue:
+		["If deleting, then set emphasisHere from 1st character of the deletion"
+		emphasisHere _ (paragraph text attributesAt: start forStyle: paragraph textStyle)
+					select: [:att | att mayBeExtended]].
 	(start = stop and: [aText size = 0]) ifFalse:
 		[paragraph replaceFrom: start to: stop - 1
 			with: aText displaying: false.  "** was true in super"
