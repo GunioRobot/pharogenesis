4297dab14ea94db908b7baa6c1d55907ba89b414
ar <unknown>
1999-12-07 21:37:00 +0000

    no message

--- BitBltSimulation/instance/inner loop/copyLoopNoSource.cs
+++ BitBltSimulation/instance/inner loop/copyLoopNoSource.cs
@@ -1,5 +1,5 @@
 copyLoopNoSource
-	| halftoneWord mergeWord mergeFnwith |
+	| halftoneWord mergeWord mergeFnwith destWord |
 	"Faster copyLoop when source not used.  hDir and vDir are both
 	positive, and perload and skew are unused"
 
@@ -12,40 +12,43 @@ copyLoopNoSource
 		[ :i |
 		noHalftone
 			ifTrue: [halftoneWord _ AllOnes]
-			ifFalse: [halftoneWord _ interpreterProxy longAt: (halftoneBase + (dy+i-1 \\ halftoneHeight * 4))].
+			ifFalse: [halftoneWord _ self halftoneAt: dy+i-1].
 
 	"Note: the horizontal loop has been expanded into three parts for speed:"
 
 			"This first section requires masking of the destination store..."
+			destMask _ mask1.
+			destWord _ self dstLongAt: destIndex.
 			mergeWord _ self mergeFn: halftoneWord
-							with: (interpreterProxy longAt: destIndex).
-			interpreterProxy longAt: destIndex
-				put: ((mask1 bitAnd: mergeWord)
-					bitOr: (mask1 bitInvert32 bitAnd: (interpreterProxy longAt: destIndex))).
+							with: destWord.
+			destWord _ (destMask bitAnd: mergeWord) bitOr: 
+							(destWord bitAnd: destMask bitInvert32).
+			self dstLongAt: destIndex put: destWord.
 			destIndex _ destIndex + 4.
 
 		"This central horizontal loop requires no store masking"
-combinationRule = 3
-ifTrue: [2 to: nWords-1 do: "Special inner loop for STORE"
-			[ :word |
-			interpreterProxy longAt: destIndex put: halftoneWord.
-			destIndex _ destIndex + 4].
-] ifFalse: [2 to: nWords-1 do: "Normal inner loop does merge"
-			[ :word |
-			mergeWord _ self mergeFn: halftoneWord
-							with: (interpreterProxy longAt: destIndex).
-			interpreterProxy longAt: destIndex put: mergeWord.
-			destIndex _ destIndex + 4].
-
-].
+			destMask _ AllOnes.
+			combinationRule = 3 ifTrue: ["Special inner loop for STORE"
+				destWord _ halftoneWord.
+				2 to: nWords-1 do:[ :word |
+					self dstLongAt: destIndex put: destWord.
+					destIndex _ destIndex + 4].
+			] ifFalse:[ "Normal inner loop does merge"
+				2 to: nWords-1 do:[ :word | "Normal inner loop does merge"
+					destWord _ self dstLongAt: destIndex.
+					mergeWord _ self mergeFn: halftoneWord with: destWord.
+					self dstLongAt: destIndex put: mergeWord.
+					destIndex _ destIndex + 4].
+			].
 
 		"This last section, if used, requires masking of the destination store..."
 		nWords > 1 ifTrue:
-			[mergeWord _ self mergeFn: halftoneWord
-							with: (interpreterProxy longAt: destIndex).
-			interpreterProxy longAt: destIndex
-				put: ((mask2 bitAnd: mergeWord)
-					bitOr: (mask2 bitInvert32 bitAnd: (interpreterProxy longAt: destIndex))).
+			[destMask _ mask2.
+			destWord _ self dstLongAt: destIndex.
+			mergeWord _ self mergeFn: halftoneWord with: destWord.
+			destWord _ (destMask bitAnd: mergeWord) bitOr:
+							(destWord bitAnd: destMask bitInvert32).
+			self dstLongAt: destIndex put: destWord.
 			destIndex _ destIndex + 4].
 
 	destIndex _ destIndex + destDelta]
