7c2f057f67e37aa5dd7ea0b1c82ef7fa2fbf1754
sw <unknown>
1999-12-07 10:58:00 +0000

    no message

--- Text/instance/emphasis/fontAt-3A-withStyle-3A-.cs
+++ Text/instance/emphasis/fontAt-3A-withStyle-3A-.cs
@@ -1,9 +1,9 @@
 fontAt: characterIndex withStyle: aTextStyle
 	"Answer the fontfor characters in the run beginning at characterIndex."
 	| attributes font |
-	self size = 0 ifTrue: [^ aTextStyle fontAt: 1].	"null text tolerates access"
+	self size = 0 ifTrue: [^ aTextStyle defaultFont].	"null text tolerates access"
 	attributes _ runs at: characterIndex.
-	attributes do:
+	font _ aTextStyle defaultFont.  "default"
+	attributes do: 
 		[:att | att forFontInStyle: aTextStyle do: [:f | font _ f]].
 	^ font
-	
