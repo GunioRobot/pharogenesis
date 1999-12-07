1086ad60b9f74ccb0bc3ec32e2b7ae0a982aa9cc
tk <unknown>
1999-12-07 10:54:00 +0000

    no message

---
+++ ProjectViewMorph/instance/copying/veryDeepInner-3A-.cs
@@ -0,0 +1,8 @@
+veryDeepInner: deepCopier
+	"Copy all of my instance variables.  Some need to be not copied at all, but shared.  See DeepCopier class comment."
+
+	super veryDeepInner: deepCopier.
+	project _ project.		"Weakly copied"
+	lastProjectThumbnail _ lastProjectThumbnail veryDeepCopyWith: deepCopier.
+	currentBorderColor _ currentBorderColor veryDeepCopyWith: deepCopier.
+	mouseDownTime _ nil.
