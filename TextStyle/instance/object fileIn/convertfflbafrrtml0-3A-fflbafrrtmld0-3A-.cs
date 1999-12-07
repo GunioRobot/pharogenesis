759c7a32de32964a34eef1151e64a54855ae1359
sw <unknown>
1999-12-07 11:58:00 +0000

    no message

---
+++ TextStyle/instance/object fileIn/convertfflbafrrtml0-3A-fflbafrrtmld0-3A-.cs
@@ -0,0 +1,5 @@
+convertfflbafrrtml0: varDict fflbafrrtmld0: smartRefStrm
+	"These variables are automatically stored into the new instance ('fontArray' 'fontFamilySize' 'lineGrid' 'baseline' 'alignment' 'firstIndent' 'restIndent' 'rightIndent' 'tabsArray' 'marginTabsArray' 'leading' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('defaultFontIndex' )  If a non-nil value is needed, please assign it."
