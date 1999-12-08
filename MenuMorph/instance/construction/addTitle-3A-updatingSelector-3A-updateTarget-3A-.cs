1ccd34bd92ef335305af34006cf0adf686ef1cf6
sw <unknown>
1999-12-08 17:53:00 +0000

    no message

---
+++ MenuMorph/instance/construction/addTitle-3A-updatingSelector-3A-updateTarget-3A-.cs
@@ -0,0 +1,17 @@
+addTitle: aString updatingSelector: aSelector updateTarget: aTarget
+	"Add a title line at the top of this menu  Make aString its initial contents.  If aSelector is not nil, then periodically obtain fresh values for its contents by sending aSelector to aTarget.."
+
+	| title |
+	title _ AlignmentMorph new.
+	self setTitleParametersFor: title.
+	title vResizing: #shrinkWrap.
+	title orientation: #vertical.
+	title centering: #center.
+	aSelector
+		ifNotNil:
+			[title addMorphBack: (UpdatingStringMorph new lock; useStringFormat; target: aTarget; getSelector: aSelector)]
+		ifNil:
+			[(aString asString findTokens: String cr) do:
+				[:line | title addMorphBack: (StringMorph contents: line font: Preferences standardMenuFont)]].
+	
+	self addMorphFront: title.
