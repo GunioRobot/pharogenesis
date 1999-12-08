6f9fbd17ed9a24f5a6230403f37bde2b18e67fad
sbw <unknown>
1999-12-08 12:32:00 +0000

    no message

---
+++ Browser/instance/initialize-2D-release/buildOptionalButtonsView.cs
@@ -0,0 +1,27 @@
+buildOptionalButtonsView
+
+	| aView buttonView offset bWidth bHeight first previousView |
+	aView _ View new model: self.
+	bHeight _ self optionalButtonHeight.
+	aView window: (0 @ 0 extent: 200 @ bHeight).
+	offset _ 0.
+	first _ true.
+	previousView _ nil.
+	self optionalButtonPairs do: [:pair |
+		buttonView _ PluggableButtonView on: self
+			getState: nil
+			action: pair last.
+		buttonView
+			label: pair first asParagraph.
+		bWidth _ buttonView label boundingBox width // 2.  "Need something more deterministic."
+		buttonView window: (offset@0 extent: bWidth@bHeight).
+		offset _ offset + bWidth + 0.
+		first
+			ifTrue:
+				[aView addSubView: buttonView.
+				first _ false]
+			ifFalse:
+				[buttonView borderWidthLeft: 1 right: 0 top: 0 bottom: 0.
+				aView addSubView: buttonView toRightOf: previousView]. 
+		previousView _ buttonView].
+	^ aView
