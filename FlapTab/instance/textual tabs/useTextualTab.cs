ded644340d20091074969740ebcab4bdb7634e40
sw <unknown>
1999-12-08 15:01:00 +0000

    no message

---
+++ FlapTab/instance/textual tabs/useTextualTab.cs
@@ -0,0 +1,7 @@
+useTextualTab
+	| stringToUse colorToUse |
+	self preserveDetails.
+	colorToUse _ self valueOfProperty: #priorColor ifAbsent: [Color green muchLighter].
+	submorphs size > 0 ifTrue: [self removeAllMorphs].
+	stringToUse _ self valueOfProperty: #priorWording ifAbsent: ['Unnamed Flap'].
+	self assumeString: stringToUse font:  Preferences standardFlapFont orientation: self orientation color: colorToUse
