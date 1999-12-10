ef88e89ae1a327fca1576132a4645cbcf8b95461
sw <unknown>
1999-12-10 08:58:00 +0000

    no message

---
+++ Preferences/class/fonts/chooseCodeFont.cs
@@ -0,0 +1,4 @@
+chooseCodeFont
+	"Not currently sent, but once protocols are sorted out so that we can disriminate on whether a text object being launched is for code or not, will be reincorporated"
+
+	self chooseFontWithPrompt: 'standard code font' andSendTo: self withSelector: #setCodeFontTo:.
