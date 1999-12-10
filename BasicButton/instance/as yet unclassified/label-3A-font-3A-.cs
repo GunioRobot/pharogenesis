b7b1ad243c1a4883dd1374125e53e0de6f5c4477
sw <unknown>
1999-12-10 09:07:00 +0000

    no message

---
+++ BasicButton/instance/as yet unclassified/label-3A-font-3A-.cs
@@ -0,0 +1,11 @@
+label: aString font: aFontOrNil
+
+	| oldLabel m aFont |
+	(oldLabel _ self findA: StringMorph)
+		ifNotNil: [oldLabel delete].
+	aFont _ aFontOrNil ifNil: [Preferences standardButtonFont].
+	m _ StringMorph contents: aString font: aFont.
+	self extent: (m width + 6) @ (m height + 6).
+	m position: self center - (m extent // 2).
+	self addMorph: m.
+	m lock
