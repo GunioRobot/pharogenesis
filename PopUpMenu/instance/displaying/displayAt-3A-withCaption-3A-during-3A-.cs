3f013ee73bc74b6137be762932a53f4c672f6b79
sw <unknown>
1999-12-10 09:55:00 +0000

    no message

--- PopUpMenu/instance/displaying/displayAt-3A-withCaption-3A-during-3A-.cs
+++ PopUpMenu/instance/displaying/displayAt-3A-withCaption-3A-during-3A-.cs
@@ -1,25 +1,31 @@
 displayAt: aPoint withCaption: captionOrNil during: aBlock
 	"Display the receiver just to the right of aPoint while aBlock is evaluated.  If the receiver is forced off screen, display it just to the right."
-	| delta savedArea captionView captionSave outerFrame captionText tFrame frameSaveLoc |
+	| delta savedArea captionForm captionSave outerFrame captionText tFrame frameSaveLoc captionBox |
+	marker ifNil: [self computeForm].
 	frame _ frame align: marker leftCenter with: aPoint + (2@0).
 	outerFrame _ frame.
 	captionOrNil notNil ifTrue:
-		[captionText _ DisplayText
+		[captionText _ (DisplayText
 				text: captionOrNil asText
-				textStyle: TextStyle default copy centered.
+				textStyle: MenuStyle copy centered)
+					foregroundColor: Color black
+					backgroundColor: Color white.
 		tFrame _ captionText boundingBox insetBy: -2.
 		outerFrame _ frame merge: (tFrame align: tFrame bottomCenter
 					with: frame topCenter + (0@2))].
 	delta _ outerFrame amountToTranslateWithin: Display boundingBox.
+	frame right > Display boundingBox right
+		ifTrue: [delta _ 0 - frame width @ delta y].
 	frame _ frame translateBy: delta.
 	captionOrNil notNil ifTrue:
-		[captionView _ DisplayTextView new model: captionText.
-		captionView align: captionView boundingBox bottomCenter
-					with: frame topCenter + (0@2).
-		captionView insideColor: Color white.
-		captionView borderWidth: 2.
-		captionSave _ Form fromDisplay: captionView displayBox.
-		captionView unlock; display; release].
+		[captionForm _ captionText form.
+		captionBox _ captionForm boundingBox expandBy: 4.
+		captionBox _ captionBox align: captionBox bottomCenter
+								with: frame topCenter + (0@2).
+		captionSave _ Form fromDisplay: captionBox.
+		Display border: captionBox width: 4 fillColor: Color white.
+		Display border: captionBox width: 2 fillColor: Color black.
+		captionForm displayAt: captionBox topLeft + 4].
 	marker _ marker align: marker leftCenter with: aPoint + delta +  (2@0).
 	savedArea _ Form fromDisplay: frame.
 	self menuForm displayOn: Display at: (frameSaveLoc _ frame topLeft).
@@ -27,4 +33,4 @@ displayAt: aPoint withCaption: captionOrNil during: aBlock
 	Cursor normal showWhile: [aBlock value].
 	savedArea displayOn: Display at: frameSaveLoc.
 	captionOrNil notNil ifTrue:
-		[captionSave displayOn: Display at: captionView displayBox topLeft]
+		[captionSave displayOn: Display at: captionBox topLeft]
