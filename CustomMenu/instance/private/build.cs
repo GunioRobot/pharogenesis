9daa17321b657a92c9707f1bd3b5aad35ca2d644
sw <unknown>
1999-12-10 11:21:00 +0000

    no message

--- CustomMenu/instance/private/build.cs
+++ CustomMenu/instance/private/build.cs
@@ -3,8 +3,8 @@ build
 
 	| stream |
 	stream _ WriteStream on: (String new).
-	labels do: [: label | stream nextPutAll: label; cr].
+	labels do: [:label | stream nextPutAll: label; cr].
 	(labels isEmpty) ifFalse: [stream skip: -1].  "remove final cr"
-	self labels: stream contents
-		font: (TextStyle default fontAt: 1)
-		lines: dividers.
+	super labels: stream contents
+		font: MenuStyle defaultFont
+		lines: dividers
