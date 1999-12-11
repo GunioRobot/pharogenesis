6f6483947a65041744226ee42d192b47c4a01460
stp <unknown>
1999-12-11 19:37:00 +0000

    no message

--- FileList/instance/volume list and pattern/volumeListIndex-3A-.cs
+++ FileList/instance/volume list and pattern/volumeListIndex-3A-.cs
@@ -2,13 +2,16 @@ volumeListIndex: index
 	"Select the volume name having the given index."
 
 	| delim path |
-	volListIndex _ index.
-	delim _ directory pathNameDelimiter.
-	path _ String streamContents: [:strm |
-		2 to: index do: [:i |
-			strm nextPutAll: (volList at: i) withBlanksTrimmed.
-			i < index ifTrue: [strm nextPut: delim]]].
-	self directory: (directory on: path).
-	brevityState _ #FileList.
+	volListIndex := index.
+	index = 1 
+		ifTrue: [self directory: (FileDirectory on: '')]
+		ifFalse: [delim := directory pathNameDelimiter.
+				path := String streamContents: [:strm |
+					2 to: index do: [:i |
+						strm nextPutAll: (volList at: i) withBlanksTrimmed.
+						i < index ifTrue: [strm nextPut: delim]]].
+				self directory: (directory on: path)].
+	brevityState := #FileList.
+	self addPath: path.
 	self changed: #fileList.
-	self changed: #contents.
+	self changed: #contents
