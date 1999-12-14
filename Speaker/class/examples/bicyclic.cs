37ee265d2bfcf18ebc2ae076398d5a91d989d15c
len <unknown>
1999-12-14 03:17:00 +0000

    no message

---
+++ Speaker/class/examples/bicyclic.cs
@@ -0,0 +1,8 @@
+bicyclic
+	"
+	Speaker bicyclic say: 'This is my voice. I am a woman with bicyclic voice.'
+	"
+
+	^ self new
+		pitch: 200.0;
+		voice: (KlattVoice new diplophonia: 0.4; tract: 14.4)
