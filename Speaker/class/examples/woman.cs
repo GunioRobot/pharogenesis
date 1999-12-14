05506397ea121a4cb7976228dd031eb5bddfb61b
len <unknown>
1999-12-14 04:10:00 +0000

    no message

---
+++ Speaker/class/examples/woman.cs
@@ -0,0 +1,10 @@
+woman
+	"
+	Speaker woman say: 'Do you listen? I am a woman speaking.'
+	"
+
+	^ self new
+		pitch: 230.0;
+		range: 0.5;
+		speed: 0.7;
+		voice: (KlattVoice new flutter: 0.5; ro: 0.3; ra: 0.003; tract: 14.4)
