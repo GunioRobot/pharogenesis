021a3a428a7d4e998182ecb4b9f5e8e2b58fe8f8
len <unknown>
1999-12-14 04:05:00 +0000

    no message

---
+++ PhoneticTranscriber/class/examples/englishLexicon.cs
@@ -0,0 +1,31 @@
+englishLexicon
+	^ Dictionary new
+		add: 'HOW' -> 'HH AW1';
+		add: 'YOU' -> 'Y UW1';
+		add: 'ARE' -> 'AA1 R';
+		add: 'DOING' -> 'D UW1 IH0 NG';
+		add: 'THIS' -> 'DH IH1 S';
+		add: 'IS' -> 'IH1 Z';
+		add: 'MY' -> 'M AY1';
+		add: 'VOICE' -> 'V OY1 S';
+		add: 'FAST' -> 'F AE1 S T';
+		add: 'SLOW' -> 'S L OW1';
+		add: 'I' -> 'AY1';
+		add: 'AM' -> 'AE1 M';
+		add: 'A' -> 'AH0';
+		add: 'AN' ->  'AE1 N';
+		add: 'LOW' -> 'L OW1';
+		add: 'SPEAKER' -> 'S P IY1 K ER0';
+		add: 'ANSWER' -> 'AE1 N S ER0';
+		add: 'RECEIVER' -> 'R AH0 S IY1 V ER0';
+		add: 'OBJECT' -> 'AA1 B JH EH0 K T';
+		add: 'OBJECTS' -> 'AA1 B JH EH0 K T S';
+		add: 'READ' -> 'R IY1 D';
+		add: 'WRITE' -> 'R AY1 T';
+		add: 'SQUEAK' -> 'S K W IY1 K';
+		add: 'SMALLTALK' -> ' S M AO1 L T AO2 K';
+		add: 'CLASS' -> 'K L AE1 S';
+		add: 'WOMAN' -> 'W UH1 M AH0 N';
+		add: 'BICYCLIC' ->  'B AY1 S IH0 K L IH0 K';
+		add: 'LISTEN' -> 'L IH1 S AH0 N';
+		yourself
