f62296b2bac4c44dd4328f9d9f6d0570c7f49c98
md <unknown>
1999-12-15 12:44:00 +0000

    no message

--- MenuMorph/instance/construction/addList-3A-.cs
+++ MenuMorph/instance/construction/addList-3A-.cs
@@ -1,5 +1,7 @@
 addList: listOfPairs
-	"Add the given items to this menu, where each item is a pair (<string> <actionSelector>)."
+	"Add the given items to this menu, where each item is a pair (<string> <actionSelector>)..  ILf an element of the list is simply the symobl $-, add a line to the receiver."
 
-	listOfPairs do:
-		[:aPair | self add: aPair first action: aPair last].
+	listOfPairs do: [:pair |
+		#- = pair
+			ifTrue: [self addLine]
+			ifFalse: [self add: pair first action: pair last]]
