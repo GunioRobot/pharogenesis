9036a01eec4ac8836996516af79c7d7238a0d218
ar <unknown>
1999-12-17 13:56:00 +0000

    no message

--- StandardFileStream/class/file creation/readOnlyFileNamed-3A-.cs
+++ StandardFileStream/class/file creation/readOnlyFileNamed-3A-.cs
@@ -1,16 +1,24 @@
-readOnlyFileNamed: aFileName
-	"Open a file of the given name for read-only access.  1/31/96 sw"
-	| selection |
-	(self isAFileNamed: aFileName) ifTrue:
-		[^ self new open: aFileName forWrite: false].
-
+readOnlyFileNamed: fileName 
+	"Open an existing file with the given name for reading."
+	"Changed to open a more usefull popup menu.  It now also includes the most likely choices.  jaf"
+	| selection dir files choices newName fullName |
+	fullName _ self fullName: fileName.
+	(self isAFileNamed: fullName)
+		ifTrue: [^ self new open: fullName forWrite: false].
 	"File does not exist..."
-	selection _ (PopUpMenu labels: 'choose another name
-cancel')
-			startUpWithCaption: (FileDirectory localNameFor: aFileName) , '
+	dir _ FileDirectory forFileName: fullName.
+	files _ dir fileNames.
+	choices _ (FileDirectory localNameFor: fullName) correctAgainst: files.
+	choices add: 'Choose another name'.
+	choices add: 'Cancel'.
+	selection _ (PopUpMenu labelArray: choices lines: (Array with: 5) )
+		startUpWithCaption: (FileDirectory localNameFor: fullName), '
 does not exist.'.
-	selection = 1 ifTrue:
-		[^ self readOnlyFileNamed:
-			(FillInTheBlank request: 'Enter a new file name'
-						initialAnswer: (FileDirectory localNameFor: aFileName))].
-	self halt
+	selection < (choices size - 1) ifTrue: [
+		newName _ (dir pathName , FileDirectory slash , (choices at: selection))].
+	selection = (choices size - 1) ifTrue: [
+		newName _ FillInTheBlank 
+							request: 'Enter a new file name' 
+							initialAnswer: fileName].
+	newName = '' ifFalse: [^ self readOnlyFileNamed: (self fullName: newName)].
+	^ self error: 'Could not open a file'
