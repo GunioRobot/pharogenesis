7fbe94b52d2b2e889c3f463c327604baf20cd379
sw <unknown>
1999-12-20 17:49:00 +0000

    no message

--- HaloMorph/instance/private/startDrag-3A-with-3A-.cs
+++ HaloMorph/instance/private/startDrag-3A-with-3A-.cs
@@ -1,5 +1,11 @@
 startDrag: evt with: dragHandle
+	| itsOwner |
 	"Drag my target without removing it from its owner."
 
 	self removeAllHandlesBut: dragHandle.
-	positionOffset _ dragHandle center - self position.
+	positionOffset _ dragHandle center - target positionInWorld.
+
+	 ((itsOwner _ target topRendererOrSelf owner) notNil and:
+			[itsOwner automaticViewing]) ifTrue:
+				[target openViewerForArgument]
+
