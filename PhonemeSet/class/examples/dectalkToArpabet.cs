53f1de070ea883c624754c64aae93e5caf4b744f
len <unknown>
1999-12-20 04:34:00 +0000

    no message

---
+++ PhonemeSet/class/examples/dectalkToArpabet.cs
@@ -0,0 +1,17 @@
+dectalkToArpabet
+	"Answer a dictionary with DECTalk phoneme names
+	as keys and ARPAbet phonemes as values."
+
+	| answer |
+	answer _ Dictionary new.
+	self arpabet do: [ :each | answer at: each name put: each].
+	#(
+		('nx'	'ng')
+		('yx'	'jh')
+		('lx'		'l')
+		('rr'	'r')
+		('u'		'uw')
+		('hx'	'hh')
+		('h'		'hh')
+		('_'		'sil')) do: [ :each | answer at: each first put: (self arpabet at: each last)].
+	^ answer
