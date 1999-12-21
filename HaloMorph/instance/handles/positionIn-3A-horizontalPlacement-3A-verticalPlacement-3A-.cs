200423da7e3a6ceef9e4da5886152331325092d5
sw <unknown>
1999-12-21 18:22:00 +0000

    no message

---
+++ HaloMorph/instance/handles/positionIn-3A-horizontalPlacement-3A-verticalPlacement-3A-.cs
@@ -0,0 +1,26 @@
+positionIn: aBox horizontalPlacement: horiz verticalPlacement: vert
+	| xCoord yCoord |
+
+	horiz == #left
+		ifTrue:	[xCoord _ aBox left].
+	horiz == #leftCenter
+		ifTrue:	[xCoord _ aBox left + (aBox width // 4)].
+	horiz == #center
+		ifTrue:	[xCoord _ (aBox left + aBox right) // 2].
+	horiz == #rightCenter
+		ifTrue:	[xCoord _ aBox left + ((3 * aBox width) // 4)].
+	horiz == #right
+		ifTrue:	[xCoord _ aBox right].
+
+	vert == #top
+		ifTrue:	[yCoord _ aBox top].
+	vert == #topCenter
+		ifTrue:	[yCoord _ aBox top + (aBox height // 4)].
+	vert == #center
+		ifTrue:	[yCoord _ (aBox top + aBox bottom) // 2].
+	vert == #bottomCenter
+		ifTrue:	[yCoord _ aBox top + ((3 * aBox height) // 4)].
+	vert == #bottom
+		ifTrue:	[yCoord _ aBox bottom].
+
+	^ xCoord @ yCoord
