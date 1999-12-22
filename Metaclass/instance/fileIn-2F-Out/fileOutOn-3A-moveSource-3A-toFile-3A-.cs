e30dca2e858eea7dfa6b26aa7ea701d571e106ba
ar <unknown>
1999-12-22 17:31:00 +0000

    no message

--- Metaclass/instance/fileIn-2F-Out/fileOutOn-3A-moveSource-3A-toFile-3A-.cs
+++ Metaclass/instance/fileIn-2F-Out/fileOutOn-3A-moveSource-3A-toFile-3A-.cs
@@ -1,9 +1,2 @@
 fileOutOn: aFileStream moveSource: moveSource toFile: fileIndex
-	super fileOutOn: aFileStream
-		moveSource: moveSource
-		toFile: fileIndex.
-	(moveSource not and: [methodDict includesKey: #initialize]) ifTrue: 
-		[aFileStream cr.
-		aFileStream cr.
-		aFileStream nextChunkPut: thisClass name , ' initialize'.
-		aFileStream cr]
+	^self fileOutOn: aFileStream moveSource: moveSource toFile: fileIndex initializing: true
