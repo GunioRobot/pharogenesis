8b36fadce57c748e07cb78c66dff3cd529cf5381
len <unknown>
1999-12-22 03:52:00 +0000

    no message

---
+++ KlattVoice/instance/playing/flush.cs
@@ -0,0 +1,8 @@
+flush
+	"Play all the events in the queue, and then reset."
+	| lastEventSegments |
+	lastEvent isNil
+		ifFalse: [lastEventSegments _ self segments at: lastEvent phoneme ifAbsent: [self segments silence].
+				self playEvent: lastEvent segments: lastEventSegments boundary: self segments end at: lastEventTime].
+	super flush.
+	self reset
