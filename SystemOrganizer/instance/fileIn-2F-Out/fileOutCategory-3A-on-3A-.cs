38e456ee5753bbfdd30d84e514730c8714f66928
ar <unknown>
1999-12-22 17:28:00 +0000

    no message

--- SystemOrganizer/instance/fileIn-2F-Out/fileOutCategory-3A-on-3A-.cs
+++ SystemOrganizer/instance/fileIn-2F-Out/fileOutCategory-3A-on-3A-.cs
@@ -1,24 +1,4 @@
 fileOutCategory: category on: aFileStream 
 	"Store on the file associated with aFileStream, all the classes associated 
 	with the category and any requested shared pools."
-
-	| first poolSet tempClass classes |
-	classes _ (self superclassOrder: category).
-	poolSet _ Set new.
-	classes do: 
-		[:class | class sharedPools do: [:eachPool | poolSet add: eachPool]].
-	poolSet size > 0 ifTrue:
-		[tempClass _ Class new.
-		tempClass shouldFileOutPools ifTrue:
-			[poolSet _ poolSet select: [:aPool | tempClass shouldFileOutPool: (Smalltalk keyAtValue: aPool)].
-			poolSet do: [:aPool | tempClass fileOutPool: aPool onFileStream: aFileStream]]].
-	first _ true.
-	classes do: 
-		[:class | 
-		first
-			ifTrue: [first _ false]
-			ifFalse: [aFileStream cr; nextPut: Character newPage; cr].
-		class
-			fileOutOn: aFileStream
-			moveSource: false
-			toFile: 0]
+	^self fileOutCategory: category on: aFileStream initializing: true
