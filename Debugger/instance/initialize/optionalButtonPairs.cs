f1af70f73f473d431b83cdbb176f9bb3f8dbe22a
sbw <unknown>
1999-12-23 09:28:00 +0000

    no message

---
+++ Debugger/instance/initialize/optionalButtonPairs.cs
@@ -0,0 +1,9 @@
+optionalButtonPairs
+
+	^ #(('Proceed'		proceed)
+		('Restart'		restart)
+		('Send'			send)
+		('Step'			doStep)
+		('Full Stack'		fullStack)
+		('Where'		where)
+		('Browse'		browseMethodFull))
