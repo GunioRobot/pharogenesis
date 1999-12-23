b8b084ea91dae4b5a7006d554a1680b1e8c190af
sr <unknown>
1999-12-23 18:46:00 +0000

    no message

---
+++ LargeIntegersPlugin/instance/C core/cCoreBytesRshiftCount-3A-n-3A-m-3A-f-3A-bytes-3A-from-3A-len-3A-to-3A-len-3A-.cs
@@ -0,0 +1,22 @@
+cCoreBytesRshiftCount: count n: n m: m f: f bytes: b from: pFrom len: fromLen to: pTo len: toLen 
+	| x digit |
+	self var: #pTo declareC: 'unsigned char * pTo'.
+	self var: #pFrom declareC: 'unsigned char * pFrom'.
+	self assert: b < fromLen.
+	x _ (pFrom at: b)
+				bitShift: n.
+	self assert: count - 1 < fromLen.
+	b + 1 to: count - 1 do: 
+		[:j | 
+		digit _ pFrom at: j.
+		pTo at: j - b - 1 put: (((digit bitAnd: m)
+				bitShift: f)
+				bitOr: x).
+		"Avoid values > 8 bits"
+		x _ digit bitShift: n].
+	count = fromLen
+				ifTrue: [digit _ 0]
+				ifFalse: [digit _ pFrom at: count].
+	pTo at: count - b - 1 put: (((digit bitAnd: m)
+			bitShift: f)
+			bitOr: x)
