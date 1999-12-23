ee71c20aacd5e4ae0d7e8aeed192fcea86973af9
sbw <unknown>
1999-12-23 08:36:00 +0000

    no message

--- Debugger/instance/initialize/buildMVCNotifierViewLabel-3A-message-3A-minSize-3A-.cs
+++ Debugger/instance/initialize/buildMVCNotifierViewLabel-3A-message-3A-minSize-3A-.cs
@@ -1,8 +1,10 @@
 buildMVCNotifierViewLabel: aString message: messageString minSize: aPoint
 
-	| topView aStringHolderView |
+	| topView aStringHolderView buttonView x y bHeight |
 	topView _ StandardSystemView new model: self.
 	topView borderWidth: 1.
+	buttonView _ self buildMVCNotifierButtonView.
+	topView addSubView: buttonView.
 	aStringHolderView _ PluggableTextView on: self
 		text: #contents
 		accept: #doNothing:
@@ -11,8 +13,13 @@ buildMVCNotifierViewLabel: aString message: messageString minSize: aPoint
 	aStringHolderView
 		editString: messageString;
 		askBeforeDiscardingEdits: false.
+	x _ 350 max: (aPoint x).
+	y _ ((4 * 15) + 16) max: (aPoint y - 16 - self optionalButtonHeight).
+	bHeight _ self optionalButtonHeight.
+	y _ y - bHeight.
+	aStringHolderView window: (0@0 extent: x@y).
 	topView
-		addSubView: aStringHolderView;
+		addSubView: aStringHolderView below: buttonView;
 		label: aString;
 		minimumSize: aPoint.
 	^ topView
