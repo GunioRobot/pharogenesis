ab5f43769ee530d725bfccb017c267f579a33cae
len <unknown>
1999-12-24 02:45:00 +0000

    no message

---
+++ PhoneticEvent/instance/converting/asPHOString.cs
@@ -0,0 +1,8 @@
+asPHOString
+	| stream |
+	stream _ WriteStream on: String new.
+	stream
+		nextPutAll: (PhonemeSet arpabetToSampa at: self phoneme); space;
+		print: (self duration * 1000) rounded.
+	self pitchPoints do: [ :each | stream space; print: (each x * 1000) rounded; space; print: each y rounded].
+	^ stream contents
