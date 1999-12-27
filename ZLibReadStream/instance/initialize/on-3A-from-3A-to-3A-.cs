7d58c2e5b09b78e568d56c521c35074c2b301782
ar <unknown>
1999-12-27 15:38:00 +0000

    no message

---
+++ ZLibReadStream/instance/initialize/on-3A-from-3A-to-3A-.cs
@@ -0,0 +1,10 @@
+on: aCollection from: firstIndex to: lastIndex
+	"Check the header of the ZLib stream."
+	| method byte |
+	super on: aCollection from: firstIndex to: lastIndex.
+	method _ self nextBits: 8.
+	(method bitAnd: 15) = 8 ifFalse:[^self error:'Unknown compression method'].
+	(method bitShift: -4) + 8 > 15 ifTrue:[^self error:'Invalid window size'].
+	byte _ self nextBits: 8.
+	(method bitShift: 8) + byte \\ 31 = 0 ifFalse:[^self error:'Incorrect header'].
+	(byte anyMask: 32) ifTrue:[^self error:'Need preset dictionary'].
