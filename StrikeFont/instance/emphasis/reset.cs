f05bd67dc2a5dddd6a8b899e3169628ad6dba60e
sma <unknown>
1999-12-30 15:02:00 +0000

    no message

--- StrikeFont/instance/emphasis/reset.cs
+++ StrikeFont/instance/emphasis/reset.cs
@@ -1,3 +1,11 @@
 reset
 	"Reset the cache of derivative emphasized fonts"
-	derivativeFonts _ Array new: 32
+
+	| style font |
+	derivativeFonts _ Array new: 32.
+	#('B' 'I' 'BI') doWithIndex:
+		[:tag :index | 
+		(style _ TextStyle named: self familyName) ifNotNil:
+			[(font _ style fontArray
+				detect: [:each | each name = (self name , tag)]
+				ifNone: [nil]) ifNotNil: [derivativeFonts at: index put: font]]]
