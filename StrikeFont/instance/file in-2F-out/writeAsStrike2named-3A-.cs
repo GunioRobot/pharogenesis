c9b86a9ec54ecf5b730deb943b8fd867eba9b0b8
sma <unknown>
1999-12-30 14:25:00 +0000

    no message

--- StrikeFont/instance/file in-2F-out/writeAsStrike2named-3A-.cs
+++ StrikeFont/instance/file in-2F-out/writeAsStrike2named-3A-.cs
@@ -1,10 +1,11 @@
 writeAsStrike2named: fileName
 	"Write me onto a file in strike2 format.
 	fileName should be of the form: <family name><pointSize>.sf2"
+
 	| file |
 	file _ FileStream fileNamed: fileName.
 	file binary.
-	file nextInt32Put: 2.
+	[file nextInt32Put: 2. "type"
 	file nextInt32Put: minAscii.
 	file nextInt32Put: maxAscii.
 	file nextInt32Put: maxWidth.
@@ -14,7 +15,5 @@ writeAsStrike2named: fileName
 	superscript _ ascent - descent // 3.	
 	subscript _ descent - ascent // 3.	
 	file nextInt32Put: emphasis.
-	(minAscii + 1 to: maxAscii + 3) do:
-		[:index | file nextInt32Put: (xTable at: index)].
-	glyphs writeOn: file.
-	file close.
+	(minAscii + 1 to: maxAscii + 3) do: [:index | file nextInt32Put: (xTable at: index)].
+	glyphs writeOn: file] ensure: [file close]
