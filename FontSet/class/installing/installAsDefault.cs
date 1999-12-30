176e43d5c74629880e881a2102fa7e694f3a4da7
sma <unknown>
1999-12-30 15:07:00 +0000

    no message

---
+++ FontSet/class/installing/installAsDefault.cs
@@ -0,0 +1,12 @@
+installAsDefault  "FontSetNewYork installAsDefault"
+	(SelectionMenu confirm: 'Do you want to install
+''' , self fontName , ''' as default font?')
+		ifFalse: [^ self].
+	self installAsTextStyle.
+	"TextConstants at: #OldDefaultTextStyle put: TextStyle default."
+	TextConstants at: #DefaultTextStyle put: (TextStyle named: self fontName).
+	ListParagraph initialize.
+	PopUpMenu initialize.
+	StandardSystemView initialize.
+	"SelectionMenu notify: 'The old text style has been saved
+as ''OldDefaultTextStyle''.'"
