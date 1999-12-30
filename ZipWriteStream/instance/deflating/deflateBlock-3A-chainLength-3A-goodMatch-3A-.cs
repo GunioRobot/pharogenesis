9d41e4547bd8afccabdc80b5d6a041c8851c1a5a
ar <unknown>
1999-12-30 14:27:00 +0000

    no message

---
+++ ZipWriteStream/instance/deflating/deflateBlock-3A-chainLength-3A-goodMatch-3A-.cs
@@ -0,0 +1,4 @@
+deflateBlock: lastIndex chainLength: chainLength goodMatch: goodMatch
+	"^DeflatePlugin doPrimitive:#primitiveDeflateBlock"
+	<primitive:'primitiveDeflateBlock'>
+	^super deflateBlock: lastIndex chainLength: chainLength goodMatch: goodMatch
