c248ebb9fb7e0eec108b3b55c74a57c3d42106e3
sw <unknown>
1999-12-30 19:43:00 +0000

    no message

--- HaloMorph/instance/private/addNameBeneath-3A-string-3A-.cs
+++ HaloMorph/instance/private/addNameBeneath-3A-string-3A-.cs
@@ -1,10 +1,13 @@
 addNameBeneath: outerRectangle string: aString
 	"Add a name display centered beneath the bottom of the outer rectangle. Return the handle."
 
-	| nameMorph |
-	nameMorph _ UpdatingStringMorph contents: aString.
+	| nameMorph namePosition |
+	nameMorph _ NameStringInHalo contents: aString.
+	nameMorph color: Color magenta.
 	nameMorph useStringFormat; target: innerTarget; putSelector: #renameTo:.
-	nameMorph position: outerRectangle bottomCenter - (((nameMorph width + 6) // 2) @ 8 negated).
+	namePosition _ outerRectangle bottomCenter -
+		((nameMorph width // 2) @ (self handleSize negated // 2 - 1)).
+	nameMorph position: (namePosition min: self world viewBox bottomRight - nameMorph extent y + 2).
 	nameMorph balloonTextSelector: #objectNameInHalo.
 	self addMorph: nameMorph.
 	^ nameMorph
