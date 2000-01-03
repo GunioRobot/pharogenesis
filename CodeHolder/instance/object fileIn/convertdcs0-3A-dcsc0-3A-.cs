971136c30140cf119e56d6b52cf2369c98d7ba3b
tk <unknown>
2000-01-03 15:59:00 +0000

    no message

---
+++ CodeHolder/instance/object fileIn/convertdcs0-3A-dcsc0-3A-.cs
@@ -0,0 +1,5 @@
+convertdcs0: varDict dcsc0: smartRefStrm
+	"These variables are automatically stored into the new instance ('showDiffs' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('currentCompiledMethod' )  If a non-nil value is needed, please assign it."
