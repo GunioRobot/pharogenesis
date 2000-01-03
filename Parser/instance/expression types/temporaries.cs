eee2421bbb2f35a4855e0caa4c820f7d0f3d3d48
tk <unknown>
2000-01-03 13:56:00 +0000

    no message

--- Parser/instance/expression types/temporaries.cs
+++ Parser/instance/expression types/temporaries.cs
@@ -1,13 +1,22 @@
 temporaries
-	" [ '|' (variable)* '|' ] "
-	| vars |
-	(self match: #verticalBar) ifFalse:	"no temps"
-		[tempsMark _ hereMark.
-		^ #()].
+	" [ '|' (variable)* '|' ]"
+	| vars theActualText |
+	(self match: #verticalBar) ifFalse: 
+		["no temps"
+		doitFlag ifTrue: [requestor
+				ifNil: [tempsMark _ 1]
+				ifNotNil: [tempsMark _ requestor selectionInterval first].
+			^ #()].
+		tempsMark _ prevMark + prevToken.
+		tempsMark > 0 ifTrue:
+			[theActualText _ source contents.
+			[tempsMark < theActualText size and: [(theActualText at: tempsMark) isSeparator]]
+				whileTrue: [tempsMark _ tempsMark + 1]].
+			^ #()].
 	vars _ OrderedCollection new.
 	[hereType == #word]
 		whileTrue: [vars addLast: (encoder bindTemp: self advance)].
-	(self match: #verticalBar) ifTrue:
+	(self match: #verticalBar) ifTrue: 
 		[tempsMark _ prevMark.
 		^ vars].
-	^self expected: 'Vertical bar'
+	^ self expected: 'Vertical bar'
