fd2d760a86e28632f17f9bea1430d27931c2b622
tk <unknown>
2000-01-03 12:57:00 +0000

    no message

--- Debugger/instance/initialize/buildMorphicNotifierLabelled-3A-message-3A-.cs
+++ Debugger/instance/initialize/buildMorphicNotifierLabelled-3A-message-3A-.cs
@@ -1,11 +1,19 @@
 buildMorphicNotifierLabelled: label message: messageString
-	| notifyPane window |
-	window _ (SystemWindow labelled: label) model: self.
+	| notifyPane window contentTop extentV |
+	window _ (PreDebugWindow labelled: label) model: self.
 
-	notifyPane _ PluggableTextMorph on: self text: #contents accept: #doNothing:
-		readSelection: #contentsSelection menu: #debugProceedMenu:.
-	notifyPane editString: messageString;
-		askBeforeDiscardingEdits: false.
-	window addMorph: notifyPane frame: (0@0 corner: 1@1).
+	"Preferences optionalMorphicButtons" true
+		ifTrue:
+			[contentTop _ 0.2.
+			window addMorph: (self buttonRowForPreDebugWindow: window)
+				frame: (0@0 corner: 1 @ contentTop).
+			extentV _ 156]
+		ifFalse:
+			[extentV _ 116.
+			contentTop _ 0].
+	notifyPane _ PluggableTextMorph on: self text: nil accept: nil
+		readSelection: nil menu: #debugProceedMenu:.
+	notifyPane editString: messageString; askBeforeDiscardingEdits: false.
+	window addMorph: notifyPane frame: (0@contentTop corner: 1@1).
 
-	^ window openInWorldExtent: 350@116
+	^ window openInWorldExtent: 450 @ extentV
