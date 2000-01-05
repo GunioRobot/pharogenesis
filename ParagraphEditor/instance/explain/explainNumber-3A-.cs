a1a171b7f240b740394016ba137c33813e8c144f
apb <unknown>
2000-01-05 16:56:00 +0000

    no message

--- ParagraphEditor/instance/explain/explainNumber-3A-.cs
+++ ParagraphEditor/instance/explain/explainNumber-3A-.cs
@@ -2,7 +2,7 @@ explainNumber: string
 	"Is string a Number?"
 
 	| strm c |
-	(c _ string at: 1) isDigit ifFalse: [(c = $- and: [string size > 1])
+	(c _ string at: 1) isDigit ifFalse: [(c = $- and: [string size > 1 and: [(string at: 2) isDigit]])
 			ifFalse: [^nil]].
 	strm _ ReadStream on: string.
 	c _ Number readFrom: strm.
