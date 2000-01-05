7ac77e763539ca66197b5c357f9d479e4d5afb8c
laza <unknown>
2000-01-05 12:00:00 +0000

    no message

---
+++ POSimplePolygon/instance/converting/asSortedCollection.cs
@@ -0,0 +1,5 @@
+asSortedCollection
+	"Returns a SortedCollection of my vertices. The sort order is      
+	lexicographically.    
+	This can be used as an eventqueue for sweepline algorithms."
+	^ self assorts copy
