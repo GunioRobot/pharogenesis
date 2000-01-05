d100aee49a2f3c0a2f4daa11efe1829b445f6057
tk <unknown>
2000-01-05 13:06:00 +0000

    no message

---
+++ ReferenceStream/instance/writing/setStream-3A-reading-3A-.cs
@@ -0,0 +1,11 @@
+setStream: aStream reading: isReading
+	"PRIVATE -- Initialization method."
+
+	super setStream: aStream reading: isReading.
+	"isReading ifFalse: [  when we are sure"
+	references _ IdentityDictionary new: 4096 * 5.
+	isReading ifTrue: [
+		objects _ IdentityDictionary new: 4096 * 5.
+		skipping _ false.
+		fwdRefEnds _ IdentityDictionary new].
+	blockers ifNil: [blockers _ IdentityDictionary new].	"keep blockers we just passed in"
