85d2a7a1a33a33a6047cb9282ad7585dd328aed2
bf <unknown>
2000-01-05 19:10:00 +0000

    no message

---
+++ Morph/instance/dropping-2F-grabbing/slideBackToFormerSituation-3A-.cs
@@ -0,0 +1,21 @@
+slideBackToFormerSituation: evt
+
+	| slideForm formerOwner formerPosition aWorld startPoint endPoint trans |
+	formerOwner _ evt hand formerOwner.
+	formerPosition _ evt hand formerPosition.
+	aWorld _ self world.
+
+	trans _ formerOwner transformFromWorld.
+	trans isPureTranslation 
+		ifTrue: [slideForm _ self imageForm offset: 0@0]
+		ifFalse: [slideForm _ ((TransformationMorph new asFlexOf: self) transform: trans)
+								imageForm offset: 0@0].
+
+	startPoint _ evt hand fullBounds origin.
+	endPoint _ trans localPointToGlobal: formerPosition.
+	owner privateRemoveMorph: self.
+	aWorld displayWorld.
+	slideForm slideFrom: startPoint to: endPoint nSteps: 12 delay: 15.
+	formerOwner addMorph: self.
+	self position: formerPosition.
+	self justDroppedInto: formerOwner event: evt.
