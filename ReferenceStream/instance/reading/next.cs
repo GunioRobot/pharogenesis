587764991b73559d6dcd34fe62bf068e216970bc
tk <unknown>
2000-01-05 13:07:00 +0000

    no message

---
+++ ReferenceStream/instance/reading/next.cs
@@ -0,0 +1,20 @@
+next
+	"Answer the next object in the stream.  If this object was already read, don't re-read it.  File is positioned just before the object."
+	| curPosn skipToPosn haveIt theObject fake wasSkipping |
+
+	haveIt _ true.
+	curPosn _ byteStream position - basePos.
+	theObject _ objects at: curPosn ifAbsent: [haveIt _ false].
+		"probe in objects is done twice when coming from objectAt:.  This is OK."
+	skipToPosn _ fwdRefEnds at: curPosn ifAbsent: [nil].
+	haveIt 
+		ifFalse: [^ super next]
+		ifTrue: [skipToPosn 
+				ifNotNil: ["Skip over the object and return the already-read-in value."
+					byteStream position: skipToPosn + basePos]	"make absolute"
+				ifNil: ["File is not positioned correctly.  Read object and throw it away."
+					wasSkipping _ skipping.
+					skipping _ true.
+					fake _ super next.
+					skipping _ wasSkipping].
+			^ theObject].
