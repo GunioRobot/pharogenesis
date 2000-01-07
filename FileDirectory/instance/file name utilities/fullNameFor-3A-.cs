a96bf28cf133d567242859e6132c518e7be9ba15
acg <unknown>
2000-01-07 08:00:00 +0000

    no message

--- FileDirectory/instance/file name utilities/fullNameFor-3A-.cs
+++ FileDirectory/instance/file name utilities/fullNameFor-3A-.cs
@@ -1,15 +1,15 @@
 fullNameFor: fileName
 	"Return a corrected, fully-qualified name for the given file name. If the given name is already a full path (i.e., it contains a delimiter character), assume it is already a fully-qualified name. Otherwise, prefix it with the path to this directory. In either case, correct the local part of the file name."
 	"Details: Note that path relative to a directory, such as '../../foo' are disallowed by this algorithm."
-
 	| correctedLocalName prefix |
-	FileDirectory splitName: fileName to:
+	self class splitName: fileName to:
 		[:filePath :localName |
-			correctedLocalName _ self checkName: localName fixErrors: true.
-			filePath isEmpty
-				ifTrue: [prefix _ pathName]  "path to this directory"
-				ifFalse: [prefix _ filePath]].  "path supplied with fileName"
-
+			correctedLocalName _ localName isEmpty 
+				ifFalse: [self checkName: localName fixErrors: true]
+				ifTrue: [localName].
+			prefix _ self fullPathFor: filePath].
 	prefix isEmpty
-		ifTrue: [^ correctedLocalName]
-		ifFalse: [^ prefix, self pathNameDelimiter asString, correctedLocalName].
+		ifTrue: [^correctedLocalName].
+	prefix last = self pathNameDelimiter
+		ifTrue:[^ prefix, correctedLocalName]
+		ifFalse:[^ prefix, self slash, correctedLocalName]
