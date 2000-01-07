8112ad3aea0df181a6456ae93b3ff5037c1d342e
sw <unknown>
2000-01-07 12:42:00 +0000

    no message

---
+++ ChangeList/instance/initialization-2D-release/changeListButtonSpecs.cs
@@ -0,0 +1,8 @@
+changeListButtonSpecs
+
+	^#(
+		('select all' 			selectAll				'select all entries')
+		('deselect all'		deselectAll			'deselect all entries')
+		('select conflicts'	selectAllConflicts	'select all methods that occur in any change set')
+		('file in selections' 	fileInSelections		'file in all selected entries')
+		)
