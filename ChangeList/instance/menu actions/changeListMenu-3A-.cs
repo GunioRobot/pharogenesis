3170b89562d7be25cfe12e162ce6364b533e835d
sw <unknown>
2000-01-07 13:25:00 +0000

    no message

--- ChangeList/instance/menu actions/changeListMenu-3A-.cs
+++ ChangeList/instance/menu actions/changeListMenu-3A-.cs
@@ -3,13 +3,23 @@ changeListMenu: aMenu
 ^ aMenu labels:
 'fileIn selections
 fileOut selections...
-select conflicts
-select conflicts with
+compare to current
+toggle diffing
+select conflicts with any changeset
+select conflicts with current changeset
+select conflicts with...
 select unchanged methods
+select methods for this class
 select all
 deselect all
+browse current versions of selections
 remove doIts
 remove older versions
 remove selections'
-	lines: #(2 6)
-	selections: #(fileInSelections fileOutSelections selectConflicts selectConflictsWith selectUnchangedMethods selectAll deselectAll removeDoIts removeOlderMethodVersions removeSelections)
+	lines: #(2 4 6 9 11 12)
+	selections: #(fileInSelections fileOutSelections
+compareToCurrentVersion toggleDiffing selectAllConflicts  selectConflicts selectConflictsWith selectUnchangedMethods selectMethodsForThisClass  selectAll  deselectAll browseCurrentVersionsOfSelections
+removeDoIts removeOlderMethodVersions removeSelections)
+
+"select such that...   selectSuchThat"
+
