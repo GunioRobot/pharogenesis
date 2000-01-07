2dc38dae029540254fe99f8d60c327560cd2fdb7
sw <unknown>
2000-01-07 12:59:00 +0000

    no message

---
+++ ChangeList/instance/menu actions/browseCurrentVersionsOfSelections.cs
@@ -0,0 +1,16 @@
+browseCurrentVersionsOfSelections
+	"Opens a message-list browser on the current in-memory versions of all methods that are currently seleted"
+	|  aClass aChange aList |
+	aList _ OrderedCollection new.
+	Cursor read showWhile: 
+	[1 to: changeList size do:
+		[:i | (listSelections at: i) ifTrue:
+			 [aChange _ changeList at: i.
+			(aChange type = #method
+				and: [(aClass _ aChange methodClass) notNil
+				and: [aClass includesSelector: aChange methodSelector]])
+					ifTrue:
+						[aList add: aClass name, ' ', aChange methodSelector]]]].
+
+	aList size == 0 ifTrue: [^ self inform: 'no selected methods have in-memory counterparts'].
+	MessageSet openMessageList: aList name: 'Current versions of selected methods in ', file localName
