8a3ab6ce6859e819d7690960395956f2edde1059
RAA <unknown>
2000-01-07 23:19:00 +0000

    no message

---
+++ Tetris/class/as yet unclassified/colors.cs
@@ -0,0 +1,10 @@
+colors
+
+	^{
+		Color r: 0.5 g: 0 b: 0.
+		Color r: 0 g: 0.5 b: 0.
+		Color r: 0 g: 0 b: 0.5.
+		Color r: 0.5 g: 0.5 b: 0.
+		Color r: 0.5 g: 0 b: 0.5.
+		Color r: 0 g: 0.5 b: 0.5
+	}
