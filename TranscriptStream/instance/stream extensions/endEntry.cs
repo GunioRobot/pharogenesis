fe5791b1ff19c6cba9c5e7eaca162cf9429b0786
mir <unknown>
2000-01-11 11:41:00 +0000

    no message

--- TranscriptStream/instance/stream extensions/endEntry.cs
+++ TranscriptStream/instance/stream extensions/endEntry.cs
@@ -1,4 +1,6 @@
 endEntry
 	"Display all the characters since the last endEntry, and reset the stream"
-	self changed: #appendEntry.
-	self reset
+	self semaphore critical:[
+		self changed: #appendEntry.
+		self reset.
+	].
