6b19d385cf6dfbc7774cc79d0a0d5112b8ca012d
sw <unknown>
2000-01-12 16:51:00 +0000

    no message

---
+++ PluggableListMorph/instance/object fileIn/convertbosfcebbmsopssrsggslssggskafll0-3A-bosfcebbmsopssrsggslssggskafllld0-3A-.cs
@@ -0,0 +1,3 @@
+convertbosfcebbmsopssrsggslssggskafll0: varDict bosfcebbmsopssrsggslssggskafllld0: smartRefStrm
+
+	"New variables: ('lastClickTime' 'doubleClickSelector' )  If a non-nil value is needed, please assign it."
