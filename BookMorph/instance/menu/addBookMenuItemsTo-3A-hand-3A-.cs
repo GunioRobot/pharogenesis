440e447f33deb01c340c709681bfec5982af5244
mjg <unknown>
2000-01-12 20:56:00 +0000

    no message

--- BookMorph/instance/menu/addBookMenuItemsTo-3A-hand-3A-.cs
+++ BookMorph/instance/menu/addBookMenuItemsTo-3A-hand-3A-.cs
@@ -1,18 +1,45 @@
-addBookMenuItemsTo: aCustomMenu hand: aHandMorph
-	aCustomMenu
-		add: (copyContents
-				ifTrue: ['don''t be parts bin when closed']
-				ifFalse: ['be parts bin when closed'])
-		action: #toggleCopyContents.
-	aCustomMenu add: 'previous page' action: #previousPage.
-	aCustomMenu add: 'next page' action: #nextPage.
-	aCustomMenu add: 'insert a page' action: #insertPage.
-	aCustomMenu add: 'delete this page' action: #deletePage.
-	aCustomMenu add: 'page controls' action: #pageControls:.
-	aCustomMenu add: 'sort pages' action: #sortPages:.
-	aCustomMenu add: 'save as new-page prototype' action: #setNewPagePrototype.
-	newPagePrototype ifNotNil: [
-		aCustomMenu add: 'clear new-page prototype' action: #clearNewPagePrototype].
+addBookMenuItemsTo: aMenu hand: aHandMorph
+	| controlsShowing subMenu |
+	subMenu _ MenuMorph new defaultTarget: self.
+	subMenu add: 'previous page' action: #previousPage.
+	subMenu add: 'next page' action: #nextPage.
+	subMenu add: 'goto page' action: #goToPage.
+	subMenu add: 'insert a page' action: #insertPage.
+	subMenu add: 'delete this page' action: #deletePage.
 
+	controlsShowing _ self hasSubmorphWithProperty: #pageControl.
+	controlsShowing
+		ifTrue:
+			[subMenu add: 'hide page controls' action: #hidePageControls.
+			subMenu add: 'fewer page controls' action: #fewerPageControls]
+		ifFalse:
+			[subMenu add: 'show page controls' action: #showPageControls].
+
+	subMenu addLine.
+	subMenu add: 'sound effect for all pages' action: #menuPageSoundForAll:.
+	subMenu add: 'sound effect this page only' action: #menuPageSoundForThisPage:.
+	subMenu add: 'visual effect for all pages' action: #menuPageVisualForAll:.
+	subMenu add: 'visual effect this page only' action: #menuPageVisualForThisPage:.
+
+	subMenu addLine.
+	subMenu add: 'sort pages' action: #sortPages:.
+	subMenu add: 'uncache page sorter' action: #uncachePageSorter.
+	(self hasProperty: #dontWrapAtEnd)
+		ifTrue: [subMenu add: 'wrap after last page' selector: #setWrapPages: argument: true]
+		ifFalse: [subMenu add: 'stop at last page' selector: #setWrapPages: argument: false].
+
+	subMenu addLine.
+	subMenu add: 'search for text' action: #textSearch.
 	(aHandMorph classOfPasteBuffer isKindOf: PasteUpMorph class) ifTrue:
-		[aCustomMenu add: 'paste book page'	action: #pasteBookPage]
+		[subMenu add: 'paste book page'	action: #pasteBookPage].
+
+	subMenu add: 'send all pages to server' action: #savePagesOnURL.
+	subMenu add: 'send this page to server' action: #saveOneOnURL.
+	subMenu add: 'reload all from server' action: #reload.
+	subMenu add: 'copy page url to clipboard' action: #copyUrl.
+	subMenu add: 'keep in one file' action: #keepTogether.
+	subMenu add: 'save as new-page prototype' action: #setNewPagePrototype.
+	newPagePrototype ifNotNil:
+		[subMenu add: 'clear new-page prototype' action: #clearNewPagePrototype].
+
+	aMenu add: 'book...' subMenu: subMenu
