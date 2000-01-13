62cf8afb247dbf932530e2540561123db18b1cad
sw <unknown>
2000-01-13 14:17:00 +0000

    no message

--- Preferences/class/initialization/chooseInitialSettings.cs
+++ Preferences/class/initialization/chooseInitialSettings.cs
@@ -2,9 +2,12 @@ chooseInitialSettings
 	"Restore the default choices for Preferences."
 	"Preferences chooseInitialSettings"
 
-	self setPreference: #reverseWindowStagger toValue: true.
-	self setPreference: #thoroughSenders toValue: true.
-	self setPreference: #uniformWindowColors toValue: false.
-	self setPreference: #warnIfNoChangesFile toValue: true.
-	self setPreference: #warnIfNoSourcesFile toValue: true.
-	self setPreference: #disableSounds toValue: false.
+	self allPreferenceInitializationSpecs do:
+		[:aSpec |
+			aSpec second == #true
+				ifTrue:
+					[self enable: aSpec first]
+				ifFalse:
+					[self disable: aSpec first]].
+	self resetCategoryInfo
+
