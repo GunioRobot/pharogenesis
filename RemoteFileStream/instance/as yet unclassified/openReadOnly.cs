dc269be68783f2484e2d885d6bbc65c5bc6ab70c
tk <unknown>
2000-01-13 21:48:00 +0000

    no message

--- RemoteFileStream/instance/as yet unclassified/openReadOnly.cs
+++ RemoteFileStream/instance/as yet unclassified/openReadOnly.cs
@@ -1,6 +1,7 @@
 openReadOnly
-	"If we have data, don't reread.  Is this OK????"
+	"If we have data, don't reread."
 
 	self readOnly.
 	readLimit _ readLimit max: position.
-	readLimit > 0 ifFalse: [remoteFile getFileNamed: remoteFile fileName into: self]. 
+	localDataValid ifFalse: [remoteFile getFileNamed: remoteFile fileName into: self].
+		"sets localDataValid _ true"
