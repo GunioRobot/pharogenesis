80d307eda2f9293406d89c9f2a55c987acb3efc3
tk <unknown>
2000-01-13 21:47:00 +0000

    no message

--- RemoteFileStream/instance/as yet unclassified/contentsOfEntireFile.cs
+++ RemoteFileStream/instance/as yet unclassified/contentsOfEntireFile.cs
@@ -2,7 +2,7 @@ contentsOfEntireFile
 	"Fetch the data off the server and store it in me.  But not if I already have it."
 
 	readLimit _ readLimit max: position.
-	readLimit > 0 ifTrue: [^ super contentsOfEntireFile].
+	localDataValid ifTrue: [^ super contentsOfEntireFile].
 	collection size = 0 ifTrue: [self on: (String new: 2000)].
-	remoteFile getFileNamed: remoteFile fileName into: self.
+	remoteFile getFileNamed: remoteFile fileName into: self.	"sets localDataValid _ true"
 	^ super contentsOfEntireFile
