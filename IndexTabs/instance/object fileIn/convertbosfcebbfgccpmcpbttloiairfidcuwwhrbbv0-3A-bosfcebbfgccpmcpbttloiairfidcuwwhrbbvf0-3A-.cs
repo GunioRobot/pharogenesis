a8e98a1aaa947513498c330ac4fe56bcb26594c3
sw <unknown>
2000-01-13 15:27:00 +0000

    no message

---
+++ IndexTabs/instance/object fileIn/convertbosfcebbfgccpmcpbttloiairfidcuwwhrbbv0-3A-bosfcebbfgccpmcpbttloiairfidcuwwhrbbvf0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcebbfgccpmcpbttloiairfidcuwwhrbbv0: varDict bosfcebbfgccpmcpbttloiairfidcuwwhrbbvf0: smartRefStrm
+	"These variables are automatically stored into the new instance ('highlightColor' 'regularColor' 'basicHeight' 'basicWidth' 'verticalPadding' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('fixedWidth' )  If a non-nil value is needed, please assign it."
