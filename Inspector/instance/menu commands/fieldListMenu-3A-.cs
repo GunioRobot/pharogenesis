a0f0ebb553114f48d5f041d408d9080d9c6b49f3
sw <unknown>
2000-01-14 11:55:00 +0000

    no message

--- Inspector/instance/menu commands/fieldListMenu-3A-.cs
+++ Inspector/instance/menu commands/fieldListMenu-3A-.cs
@@ -1,16 +1,24 @@
 fieldListMenu: aMenu
+	| sel |
+	((((sel _ self selection) isMemberOf: Array) or: [sel isMemberOf: OrderedCollection])
+			and: [sel size >= 1])
+		ifTrue:
+			[^ self fieldListMenuForCollection: aMenu].
 
-	^ aMenu labels: 'inspect
+	^ aMenu labels: 'inspect (i)
+explore (I)
 method refs to this inst var
+methods storing into this inst var
 objects pointing to this value
 copy name
-browse full
+browse full (b)
 browse class
+browse hierarchy
 inst var refs...
 inst var defs...
 class var refs...
 class variables
 class refs
 basic inspect'
-	lines: #(1 3 4  6 8 11 )
-	selections: #(inspectSelection referencesToSelection objectReferencesToSelection copyName  browseMethodFull browseClass browseInstVarRefs browseInstVarDefs classVarRefs browseClassVariables browseClassRefs inspectBasic).
+	lines: #(2 5 6 9 11 14)
+	selections: #(inspectSelection exploreSelection referencesToSelection defsOfSelection objectReferencesToSelection copyName  browseMethodFull browseClass classHierarchy browseInstVarRefs browseInstVarDefs classVarRefs browseClassVariables browseClassRefs inspectBasic).
