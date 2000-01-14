70af8886f144862a188055903af3d989cc04c0df
sr <unknown>
2000-01-14 03:59:00 +0000

    no message

--- SystemWindow/instance/geometry/extent-3A-.cs
+++ SystemWindow/instance/geometry/extent-3A-.cs
@@ -1,25 +1,18 @@
-extent: newExtent
-	| inner panelRect labelRect paneColor |
-	self removeHandles.
+extent: newExtent 
+	| inner labelRect |
 	isCollapsed
 		ifTrue: [super extent: newExtent x @ (self labelHeight + 2)]
 		ifFalse: [super extent: newExtent].
 	inner _ self innerBounds.
-	labelRect _ inner withHeight: self labelHeight.
-	panelRect _ self panelRect.
-	paneColor _ Color perform: model defaultBackgroundColor.
-
+	labelRect _ self labelRect.
 	stripes first bounds: (labelRect insetBy: 1).
 	stripes second bounds: (labelRect insetBy: 3).
-	self setStripeColorsFrom: paneColor.
-	closeBox align: closeBox topLeft with: inner topLeft + (4@0).
-	collapseBox align: collapseBox topRight with: inner topRight - (4@0).
-	label fitContents; setWidth: (label width min: bounds width - 50).
+	self setStripeColorsFrom: self paneColorToUse.
+	closeBox ifNotNil: [closeBox align: closeBox topLeft with: inner topLeft + (4 @ 1)].
+	menuBox ifNotNil: [menuBox align: menuBox topLeft with: inner topLeft + (19 @ 1)].
+	collapseBox align: collapseBox topRight with: inner topRight - (4 @ -1).
+	label fitContents; setWidth: (label width min: bounds width - self labelWidgetAllowance).
 	label align: label bounds topCenter with: inner topCenter.
-	paneMorphs with: paneRects do:
-		[:m :frame |  m color: paneColor.
-		m bounds: (((frame scaleBy: panelRect extent) translateBy: panelRect topLeft)) truncated].
 	isCollapsed
 		ifTrue: [collapsedFrame _ self bounds]
-		ifFalse: [fullFrame _ self bounds].
-	self isActive ifTrue: [self addHandles]
+		ifFalse: [self setBoundsOfPaneMorphs. fullFrame _ self bounds]
