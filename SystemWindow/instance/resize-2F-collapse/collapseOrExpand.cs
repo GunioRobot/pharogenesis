72645c2f03b4fcaaf5e6ad6f4483f8ddeff0b49d
sr <unknown>
2000-01-14 02:26:00 +0000

    no message

--- SystemWindow/instance/resize-2F-collapse/collapseOrExpand.cs
+++ SystemWindow/instance/resize-2F-collapse/collapseOrExpand.cs
@@ -1,16 +1,25 @@
 collapseOrExpand
 	isCollapsed
-	ifTrue:
-		["Expand -- restore panes to morphics structure"
-		isCollapsed _ false.
-		paneMorphs reverseDo: [:m | self addMorph: m].
-		super bounds: fullFrame.
-		self activate "-- mainly for findWindow"]
-	ifFalse:
-		["Collapse -- remove panes from morphics structure"
-		isCollapsed _ true.
-		paneMorphs do: [:m | m delete; releaseCachedState].
-		collapsedFrame
-			ifNil:	[self extent: (label width + 50) @ (self labelHeight + 2).
-					self position: (RealEstateAgent assignCollapsePointFor: self)]
-			ifNotNil: [super bounds: collapsedFrame]]
+		ifTrue: 
+			["Expand -- restore panes to morphics structure"
+			isCollapsed _ false.
+			collapsedFrame _ self getBoundsWithFlex.
+			"First save latest collapsedFrame"
+			self setBoundsWithFlex: fullFrame.
+			paneMorphs
+				reverseDo: 
+					[:m | 
+					self addMorph: m.
+					self world startSteppingSubmorphsOf: m].
+			self activate "-- mainly for findWindow"]
+		ifFalse: 
+			["Collapse -- remove panes from morphics structure"
+			isCollapsed _ true.
+			fullFrame _ self getBoundsWithFlex.
+			"First save latest fullFrame"
+			paneMorphs do: [:m | m delete; releaseCachedState].
+			model modelSleep.
+			collapsedFrame _ (RealEstateAgent assignCollapseFrameFor: self).
+						
+			self setBoundsWithFlex: collapsedFrame].
+	self layoutChanged
