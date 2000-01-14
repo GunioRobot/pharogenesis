a7b4307f4ba30211d24c29ba3f1e2f6d88bb8a7c
sw <unknown>
2000-01-14 13:39:00 +0000

    no message

--- Inspector/instance/menu commands/inspectSelection.cs
+++ Inspector/instance/menu commands/inspectSelection.cs
@@ -1,6 +1,5 @@
 inspectSelection
-	"Create and schedule an Inspector on the receiver's model's currently 
-	selected object."
+	"Create and schedule an Inspector on the receiver's model's currently selected object."
 
 	self selectionIndex = 0 ifTrue: [^ self changed: #flash].
-	^ self selection smartInspect  "eliminate annoyance of 1-element collections"
+	^ self selection inspect
