add9ceb8e5311a0dba3f436877d849a78ff86182
dtl <unknown>
2000-01-15 17:54:00 +0000

    no message

--- Class/instance/fileIn-2F-Out/fileOutPool-3A-onFileStream-3A-.cs
+++ Class/instance/fileIn-2F-Out/fileOutPool-3A-onFileStream-3A-.cs
@@ -1,12 +1,12 @@
 fileOutPool: aPool onFileStream: aFileStream 
 	| aPoolName aValue |
-	aPoolName _ Smalltalk keyAtValue: aPool.
+	aPoolName _ Smalltalk keyAtIdentityValue: aPool.
 	Transcript cr; show: aPoolName.
 	aFileStream nextPutAll: 'Transcript show: ''' , aPoolName , '''; cr!'; cr.
 	aFileStream nextPutAll: 'Smalltalk at: #' , aPoolName , ' put: Dictionary new!'; cr.
 	aPool keys asSortedCollection do: [ :aKey |
 		aValue _ aPool at: aKey.
-		aFileStream nextPutAll: aPoolName , ' at: #' , aKey asString , ' put:  '.
+		aFileStream nextPutAll: aPoolName , ' at: #''' , aKey asString , '''', ' put:  '.
 		(aValue isKindOf: Number)
 			ifTrue: [aValue printOn: aFileStream]
 			ifFalse: [aFileStream nextPutAll: '('.
