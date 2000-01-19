c13b232c14cb75f1d9d76afd07a449a50e77ba35
di <unknown>
2000-01-19 15:47:00 +0000

    no message

--- TextMorph/instance/private/updateFromParagraph.cs
+++ TextMorph/instance/private/updateFromParagraph.cs
@@ -1,39 +1,41 @@
 updateFromParagraph
-	| newStyle sel oldLast |
+	"A change has taken place in my paragraph, as a result of editing and I must be updated.  If a line break causes recomposition of the current paragraph, or it the selection has entered a different paragraph, then the current editor will be release, and must be reinstalled with the resulting new paragraph, while retaining any editor state, such as selection, undo state, and current typing emphasis."
+	| newStyle sel oldLast oldEditor |
 	paragraph ifNil: [^ self].
 	wrapFlag ifNil: [wrapFlag _ true].
-	editor ifNotNil: [sel _ editor selectionInterval.
+	editor ifNotNil: [oldEditor _ editor.
+					sel _ editor selectionInterval.
 					editor storeSelectionInParagraph].
+	text _ paragraph text.
 	paragraph textStyle = textStyle
 		ifTrue: [self fit]
 		ifFalse: ["Broadcast style changes to all morphs"
 				newStyle _ paragraph textStyle.
 				(self firstInChain text: text textStyle: newStyle) recomposeChain.
-				sel ifNotNil: [self installEditor selectFrom: sel first to: sel last]].
+				editor ifNotNil: [self installEditorToReplace: editor]].
+	super layoutChanged.
 	sel ifNil: [^ self].
 
 	"If selection is in top line, then recompose predecessor for possible ripple-back"
 	predecessor ifNotNil:
-		[sel first <= (paragraph lines first last+1) ifTrue:
+		[sel first <= (self paragraph lines first last+1) ifTrue:
 			[oldLast _ predecessor lastCharacterIndex.
-			predecessor paragraph recomposeFrom: oldLast orLineAbove: false.
+			predecessor paragraph recomposeFrom: oldLast to: text size delta: 0.
 			oldLast = predecessor lastCharacterIndex
 				ifFalse: [predecessor changed. "really only last line"
 						self predecessorChanged]]].
 
-	((predecessor~~nil and: [sel first <= paragraph firstCharacterIndex])
-		or: [successor~~nil and: [sel first > (paragraph lastCharacterIndex+1)]])
+	((predecessor~~nil and: [sel first <= self paragraph firstCharacterIndex])
+		or: [successor~~nil and: [sel first > (self paragraph lastCharacterIndex+1)]])
 		ifTrue:
 		["The selection is no longer inside this paragraph.
 		Pass focus to the paragraph that should be in control."
-		self releaseEditor.
 		self firstInChain withSuccessorsDo:
 			[:m |  (sel first between: m firstCharacterIndex
 								and: m lastCharacterIndex+1)
-					ifTrue: [m installEditor selectFrom: sel first to: sel last.
-							m selectionChanged.
+					ifTrue: [m installEditorToReplace: editor.
 							^ self passKeyboardFocusTo: m]].
-		^ self].
+		self error: 'Inconsistency in text editor' "Must be somewhere in the successor chain"].
 	editor ifNil:
 		["Reinstate selection after, eg, style change"
-		self installEditor selectFrom: sel first to: sel last]
+		self installEditorToReplace: oldEditor]
