f475053a2f62f346500c18dda15961b62560599e
sw <unknown>
2000-01-19 14:16:00 +0000

    no message

---
+++ Preferences/class/factored pref panel/factoredCategories.cs
@@ -0,0 +1,15 @@
+factoredCategories
+	| prefsWithoutInits extraItem |
+	"Preferences factoredCategories"
+	"CategoryInfo _ nil"
+
+	CategoryInfo ifNil:
+		[CategoryInfo _ self initialCategoryInfo].
+	((prefsWithoutInits _ self preferencesLackingInitializers) size > 0)
+		ifTrue:
+			[extraItem _ (Array with: 'uncategorized' with: prefsWithoutInits asSortedArray)].
+	^ extraItem
+		ifNil:
+			[CategoryInfo]
+		ifNotNil:
+			[CategoryInfo, (Array with: extraItem)]
