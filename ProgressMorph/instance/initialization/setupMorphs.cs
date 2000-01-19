19a8cbbc4082e33c4c12977367b238d73c72f1eb
mir <unknown>
2000-01-19 13:33:00 +0000

    no message

---
+++ ProgressMorph/instance/initialization/setupMorphs.cs
@@ -0,0 +1,21 @@
+setupMorphs
+	| container  |
+	self initProgressMorph.
+	container _ AlignmentMorph newColumn.
+	container
+		centering: #center;
+		hResizing: #shrinkWrap;
+		vResizing: #shrinkWrap;
+		color: Color transparent.
+
+	container addMorphBack: self labelMorph.
+	container addMorphBack: self subLabelMorph.
+	container addMorphBack: self progress.
+
+	self addMorph: container.
+	self borderWidth: 2.
+	self borderColor: Color black.
+
+	self extent: container extent.
+	self color: Color veryLightGray.
+	self align: self fullBounds center with: Display boundingBox center
