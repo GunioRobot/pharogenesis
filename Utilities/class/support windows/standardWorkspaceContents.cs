bcefbb3c2db21a887fd77cc4fa7c785bf0d1e6a3
sw <unknown>
2000-01-19 13:59:00 +0000

    no message

--- Utilities/class/support windows/standardWorkspaceContents.cs
+++ Utilities/class/support windows/standardWorkspaceContents.cs
@@ -1,38 +1,50 @@
 standardWorkspaceContents
 	^ self class firstCommentAt: #standardWorkspaceContents
 
-	"Smalltalk recover: 5000.
-(FileStream oldFileNamed: 'DryRot.cs') edit.
-(FileStream oldFileNamed: 'change.cs') fileIn
-ChangeList browseFile: 'Elvis.st'
+	"Smalltalk recover: 10000.
+ChangeList browseRecentLog.
+ChangeList browseRecent: 2000.
+
+Preferences editAnnotations.
+Utilities reinstateDefaultFlaps. 
+Preferences resetCategoryInfo
+
+(FileStream oldFileNamed: 'Lives of the Wolves') edit.
+(FileStream oldFileNamed: 'tuesdayFixes.cs') fileIn
+ChangeList browseFile: 'myChanges.st'
 
 TextStyle default fontAt: 7 put: (StrikeFont new readMacFontHex: 'Cairo 18')
 
-InputState browseAllAccessesTo: 'deltaTime'.
-StandardSystemView doCacheBits  ""restore fast windows mode""
+StandardSystemView browseAllAccessesTo: 'maximumSize'.
+StandardSystemView doCacheBits  ""restore fast windows mode in mvc""
 
 Symbol selectorsContaining: 'rsCon'.
 Smalltalk browseMethodsWhoseNamesContain: 'screen'.
 
 Browser newOnClass: Utilities.
-BrowserView browseFullForClass: ControlManager.
+Browser fullOnClass: SystemDictionary.
 
 FormView allInstances inspect.
-ScrollController someInstance inspect
+StandardSystemView someInstance inspect.
+
+Utilities storeTextWindowContentsToFileNamed: 'TextWindows'
+Utilities reconstructTextWindowsFromFileNamed: 'TextWindows'
+
+ScriptingSystem resetStandardPartsBin.
+ScheduledControllers screenController openMorphicConstructionWorld.
+ScheduledControllers screenController openMorphicWorld.
 
 SystemOrganization categoryOfElement: #Controller. 
-Component organization categoryOfElement: #contentView .
+ParagraphEditor organization categoryOfElement: #changeEmphasis.
 
-ChangeList browseRecentLog.
-ChangeList browseRecent: 2000.
-
-StringHolderView openSystemWorkspace. ""edit shared sys workspace""
 Cursor wait showWhile: [Sensor waitButton].
 
-Smalltalk spaceLeft.
+Smalltalk bytesLeft asStringWithCommas.
 Symbol instanceCount. 
 Time millisecondsToRun:
 	[Smalltalk allCallsOn: #asOop]
 MessageTally spyOn: [Smalltalk allCallsOn: #asOop].
 
 "
+
+"Utilities openStandardWorkspace"
