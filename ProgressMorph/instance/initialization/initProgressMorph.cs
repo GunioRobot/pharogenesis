2fea416afaa418106c5f8d85116bb54e25d39fd4
mir <unknown>
2000-01-19 13:16:00 +0000

    no message

---
+++ ProgressMorph/instance/initialization/initProgressMorph.cs
@@ -0,0 +1,6 @@
+initProgressMorph
+	progress _ FlashProgressMorph new.
+	progress borderWidth: 1.
+	progress color: Color transparent.
+	progress progressColor: Color gray.
+	progress extent: (200 @ 15).
