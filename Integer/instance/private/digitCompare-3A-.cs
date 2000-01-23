829ca1371e9e207f3a509cdc3580edc320800059
sr <unknown>
2000-01-23 05:43:00 +0000

    no message

--- Integer/instance/private/digitCompare-3A-.cs
+++ Integer/instance/private/digitCompare-3A-.cs
@@ -1,17 +1,18 @@
-digitCompare: arg
-	"Compare the magnitude of self with that of arg.
+digitCompare: arg 
+	"Compare the magnitude of self with that of arg.   
 	Return a code of 1, 0, -1 for self >, = , < arg"
 	| len arglen argDigit selfDigit |
+	<primitive: 'primDigitCompare' module:'LargeIntegers'>
 	len _ self digitLength.
-	(arglen _ arg digitLength) ~= len 
+	(arglen _ arg digitLength) ~= len
 		ifTrue: [arglen > len
-					ifTrue: [^-1]
-					ifFalse: [^1]].
+				ifTrue: [^ -1]
+				ifFalse: [^ 1]].
 	[len > 0]
 		whileTrue: 
-			[(argDigit _ arg digitAt: len) ~= (selfDigit _ self digitAt: len) 
+			[(argDigit _ arg digitAt: len) ~= (selfDigit _ self digitAt: len)
 				ifTrue: [argDigit < selfDigit
-							ifTrue: [^1]
-							ifFalse: [^-1]].
+						ifTrue: [^ 1]
+						ifFalse: [^ -1]].
 			len _ len - 1].
-	^0
+	^ 0
