49581070ea19b6e9d4b9a1679e2223aca9791cf8
sr <unknown>
2000-01-23 05:41:00 +0000

    no message

--- Integer/instance/private/digitAdd-3A-.cs
+++ Integer/instance/private/digitAdd-3A-.cs
@@ -1,15 +1,17 @@
-digitAdd: arg
+digitAdd: arg 
 	| len arglen accum sum |
+	<primitive: 'primDigitAdd' module:'LargeIntegers'>
 	accum _ 0.
-	(len _ self digitLength) < (arglen _ arg digitLength)
-		ifTrue: [len _ arglen].
+	(len _ self digitLength) < (arglen _ arg digitLength) ifTrue: [len _ arglen].
 	"Open code max: for speed"
 	sum _ Integer new: len neg: self negative.
 	1 to: len do: 
-		[:i |
-		accum _ (accum bitShift: -8) + (self digitAt: i) + (arg digitAt: i).
+		[:i | 
+		accum _ (accum bitShift: -8)
+					+ (self digitAt: i) + (arg digitAt: i).
 		sum digitAt: i put: (accum bitAnd: 255)].
-	accum > 255 ifTrue: 
+	accum > 255
+		ifTrue: 
 			[sum _ sum growby: 1.
 			sum at: sum digitLength put: (accum bitShift: -8)].
-	^sum
+	^ sum
