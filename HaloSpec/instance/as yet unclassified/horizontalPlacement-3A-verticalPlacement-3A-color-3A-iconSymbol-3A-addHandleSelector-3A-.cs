aa157f4b0df6cc8bce75222fea8e06c1756ffe9f
sw <unknown>
2000-01-25 19:54:00 +0000

    no message

---
+++ HaloSpec/instance/as yet unclassified/horizontalPlacement-3A-verticalPlacement-3A-color-3A-iconSymbol-3A-addHandleSelector-3A-.cs
@@ -0,0 +1,6 @@
+horizontalPlacement: hp verticalPlacement: vp color: col iconSymbol: is addHandleSelector: sel
+	horizontalPlacement _ hp.
+	verticalPlacement _ vp.
+	color_ col.
+	iconSymbol _ is asSymbol.
+	addHandleSelector _ sel
