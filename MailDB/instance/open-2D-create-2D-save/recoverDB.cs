5829bc7701aab0842a6f3a65cc7db48a60ef321f
mdr <unknown>
2000-01-25 12:06:00 +0000

    no message

---
+++ MailDB/instance/open-2D-create-2D-save/recoverDB.cs
@@ -0,0 +1,12 @@
+recoverDB
+	"Open a mail database with the given root file name."
+
+	(self confirm:
+'The mail database named:
+    ', rootFilename, '
+appears to be damaged.
+Shall I fix it?  (This might take some time)')
+		ifFalse: [self release. ^nil].
+
+	self openDB.
+	self compact.
