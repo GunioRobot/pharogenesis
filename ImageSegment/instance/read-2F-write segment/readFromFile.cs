e62f08f60cbd4792f516c698d66b05e1a871d3f8
tk <unknown>
2000-01-25 21:49:00 +0000

    no message

---
+++ ImageSegment/instance/read-2F-write segment/readFromFile.cs
@@ -0,0 +1,11 @@
+readFromFile
+	"Read in a simple segment.  Use folder of this image, even if remembered as previous location of this image"
+
+	| ff realName |
+	realName _ self class folder, FileDirectory slash, self localName.
+	ff _ FileStream oldFileNamed: realName.
+	segment _ ff nextWordsInto: (WordArrayForSegment new: ff size//4).
+	endMarker _ segment nextObject. 	"for enumeration of objects"
+	endMarker == 0 ifTrue: [endMarker _ 'End' clone].
+	ff close.
+	state _ #active
