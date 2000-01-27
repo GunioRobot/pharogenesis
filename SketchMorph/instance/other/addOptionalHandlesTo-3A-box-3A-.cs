f36d067fef4aedddc5f6bb63ca57bb10b8d2b3ec
sw <unknown>
2000-01-27 17:39:00 +0000

    no message

--- SketchMorph/instance/other/addOptionalHandlesTo-3A-box-3A-.cs
+++ SketchMorph/instance/other/addOptionalHandlesTo-3A-box-3A-.cs
@@ -1,5 +1,2 @@
 addOptionalHandlesTo: aHalo box: box
-
-	| aHandle |
-	aHandle _ aHalo addHandleAt: box rightCenter color: Color lightGray.
-	aHandle on: #mouseDown send: #editDrawing to: aHalo target renderedMorph
+	aHalo addDirectionHandles
