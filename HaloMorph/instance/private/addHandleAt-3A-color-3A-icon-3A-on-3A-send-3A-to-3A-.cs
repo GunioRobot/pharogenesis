e33bf7f9bdfeb4d26ae3f2a19a84550776079973
sw <unknown>
2000-01-27 16:29:00 +0000

    no message

---
+++ HaloMorph/instance/private/addHandleAt-3A-color-3A-icon-3A-on-3A-send-3A-to-3A-.cs
@@ -0,0 +1,19 @@
+addHandleAt: aPoint color: aColor icon: iconName on: eventName send: selector to: recipient
+	"Add a handle centered at the given point with the given color, and set it up to respond to the given event by sending the given selector to the given recipient.  Return the handle."
+	| handle |
+	handle _ EllipseMorph
+		newBounds: (Rectangle center: aPoint extent: self handleSize asPoint)
+		color: aColor.
+	self addMorph: handle.
+	iconName ifNotNil:
+		[ | form |
+		form _ ScriptingSystem formAtKey: iconName.
+		form ifNotNil:
+			[handle addMorphCentered: (ImageMorph new
+				image: form; 
+				color: aColor makeForegroundColor;
+				lock)]].
+	handle on: #mouseUp send: #endInteraction to: self.
+	handle on: eventName send: selector to: recipient.
+	handle setBalloonText: (target balloonHelpTextForHandle: handle).
+	^ handle
