ed18f68b13a6ddb53e01f0fdf56450aebbd2818b
sw <unknown>
2000-01-28 09:43:00 +0000

    no message

---
+++ HaloMorph/instance/copying/veryDeepInner-3A-.cs
@@ -0,0 +1,13 @@
+veryDeepInner: deepCopier
+	"Copy all of my instance variables.  Some need to be not copied at all, but shared.  	Warning!!  Every instance variable defined in this class must be handled.  We must also implement veryDeepFixupWith:.  See DeepCopier class comment."
+
+	super veryDeepInner: deepCopier.
+	"target _ target.		Weakly copied"
+	"innerTarget _ innerTarget.		Weakly copied"
+	positionOffset _ positionOffset veryDeepCopyWith: deepCopier.
+	angleOffset _ angleOffset veryDeepCopyWith: deepCopier.
+	minExtent _ minExtent veryDeepCopyWith: deepCopier.
+	growingOrRotating _ growingOrRotating veryDeepCopyWith: deepCopier.
+	directionArrowAnchor _ directionArrowAnchor.
+	simpleMode _ simpleMode.
+	haloBox _ haloBox
