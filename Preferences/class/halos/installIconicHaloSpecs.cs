e2357a6172cd87769f419102bcfe53a3c8ff07b2
sw <unknown>
2000-01-28 10:36:00 +0000

    no message

---
+++ Preferences/class/halos/installIconicHaloSpecs.cs
@@ -0,0 +1,4 @@
+installIconicHaloSpecs
+	"Install an alternative set of halos,  rather more based on the old placements, and without icons, , and lacking the scripting-relating handles.."
+	"Preferences installIconicHaloSpecs"
+	self installHaloTheme: #iconicHaloSpecifications
