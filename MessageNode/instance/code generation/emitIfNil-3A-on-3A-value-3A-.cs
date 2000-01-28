f58decf19483b357d46f612796fcaa35e143489e
acg <unknown>
2000-01-28 21:59:00 +0000

    no message

---
+++ MessageNode/instance/code generation/emitIfNil-3A-on-3A-value-3A-.cs
@@ -0,0 +1,20 @@
+emitIfNil: stack on: strm value: forValue
+
+	| theNode theSize theSelector |
+	theNode _ arguments first.
+	theSize _ sizes at: 1.
+	theSelector _ #ifNotNil:.
+	receiver emitForValue: stack on: strm.
+	forValue ifTrue: [strm nextPut: Dup. stack push: 1].
+	strm nextPut: LdNil. stack push: 1.
+	equalNode emit: stack args: 1 on: strm.
+	self 
+		emitBranchOn: (selector key == theSelector)
+		dist: theSize 
+		pop: stack 
+		on: strm.
+	forValue 
+		ifTrue: 
+			[strm nextPut: Pop. stack pop: 1.
+			theNode emitForEvaluatedValue: stack on: strm]	
+		ifFalse: [theNode emitForEvaluatedEffect: stack on: strm].
