bd066e38dfa8bd6e3f2216a85a0909c495b49843
di <unknown>
2000-01-28 22:21:00 +0000

    no message

--- Decompiler/class/class initialization/initialize.cs
+++ Decompiler/class/class initialization/initialize.cs
@@ -3,5 +3,6 @@ initialize
 	CascadeFlag _ 'cascade'.  "A unique object"
 	CaseFlag _ 'case'. "Ditto"
 	ArgumentFlag _ 'argument'.  "Ditto"
+	IfNilFlag _ 'ifNil'.  "Ditto"
 
 	"Decompiler initialize"
