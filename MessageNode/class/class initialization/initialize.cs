88176025e1702fba347f2abdc098ebb34365130d
acg <unknown>
2000-01-28 21:58:00 +0000

    no message

--- MessageNode/class/class initialization/initialize.cs
+++ MessageNode/class/class initialization/initialize.cs
@@ -4,28 +4,33 @@ initialize		"MessageNode initialize"
 			and: or:
 			whileFalse: whileTrue: whileFalse whileTrue
 			to:do: to:by:do:
-			caseOf: caseOf:otherwise: as: ).
+			caseOf: caseOf:otherwise:
+			ifNil: ifNotNil:  ifNil:ifNotNil: ifNotNil:ifNil:).
 	MacroTransformers _ 
 		#(transformIfTrue: transformIfFalse: transformIfTrueIfFalse: transformIfFalseIfTrue:
 			transformAnd: transformOr:
 			transformWhile: transformWhile: transformWhile: transformWhile:
 			transformToDo: transformToDo:
-			transformCase: transformCase: transformAs: ).
+			transformCase: transformCase:
+			transformIfNil: transformIfNil:  transformIfNilIfNotNil: transformIfNotNilIfNil:).
 	MacroEmitters _ 
 		#(emitIf:on:value: emitIf:on:value: emitIf:on:value: emitIf:on:value:
 			emitIf:on:value: emitIf:on:value:
 			emitWhile:on:value: emitWhile:on:value: emitWhile:on:value: emitWhile:on:value:
 			emitToDo:on:value: emitToDo:on:value:
-			emitCase:on:value: emitCase:on:value: emitAs:on:value: ).
+			emitCase:on:value: emitCase:on:value:
+			emitIfNil:on:value: emitIfNil:on:value: emitIf:on:value: emitIf:on:value:).
 	MacroSizers _ 
 		#(sizeIf:value: sizeIf:value: sizeIf:value: sizeIf:value:
 			sizeIf:value: sizeIf:value:
 			sizeWhile:value: sizeWhile:value: sizeWhile:value: sizeWhile:value:
 			sizeToDo:value: sizeToDo:value:
-			sizeCase:value: sizeCase:value: sizeAs:value: ).
+			sizeCase:value: sizeCase:value:
+			sizeIfNil:value: sizeIfNil:value: sizeIf:value: sizeIf:value: ).
 	MacroPrinters _ 
 		#(printIfOn:indent: printIfOn:indent: printIfOn:indent: printIfOn:indent:
 			printIfOn:indent: printIfOn:indent:
 			printWhileOn:indent: printWhileOn:indent: printWhileOn:indent: printWhileOn:indent:
 			printToDoOn:indent: printToDoOn:indent:
-			printCaseOn:indent: printCaseOn:indent: printAsOn:indent: )
+			printCaseOn:indent: printCaseOn:indent:
+			printIfNil:indent: printIfNil:indent: printIfNilNotNil:indent: printIfNilNotNil:indent:)
