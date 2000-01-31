a0d9092893689918481fab5c6a9dfb6329b55596
sw <unknown>
2000-01-31 15:17:00 +0000

    no message

---
+++ FillInTheBlank/instance/object fileIn/convertdcad0-3A-dcadr0-3A-.cs
@@ -0,0 +1,6 @@
+convertdcad0: varDict dcadr0: smartRefStrm
+	"These variables are automatically stored into the new instance ('acceptOnCR' 'done' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('responseUponCancel' )  If a non-nil value is needed, please assign it."
+	responseUponCancel _ ''
