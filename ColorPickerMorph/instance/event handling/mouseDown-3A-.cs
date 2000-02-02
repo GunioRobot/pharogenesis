bd743e320f74e4f6ac1c91f1cd51dc120f32e415
LC <unknown>
2000-02-02 03:34:00 +0000

    no message

--- ColorPickerMorph/instance/event handling/mouseDown-3A-.cs
+++ ColorPickerMorph/instance/event handling/mouseDown-3A-.cs
@@ -1,4 +1,10 @@
 mouseDown: evt
-
+	| localPt |
+	localPt _ evt cursorPoint - self topLeft.
+	(DragBox containsPoint: localPt)
+		ifTrue: [^ evt hand grabMorph: self].
+	(RevertBox containsPoint: localPt)
+		ifTrue: [^ self updateColor: originalColor feedbackColor: originalColor].
+	self comeToFront.
 	sourceHand _ evt hand.
 	self startStepping.
