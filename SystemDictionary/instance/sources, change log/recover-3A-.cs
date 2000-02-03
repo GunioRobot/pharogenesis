0f9abd633a34fa8cf6a8185f06f7689792e1d164
sw <unknown>
2000-02-03 15:59:00 +0000

    no message

--- SystemDictionary/instance/sources, change log/recover-3A-.cs
+++ SystemDictionary/instance/sources, change log/recover-3A-.cs
@@ -1,6 +1,3 @@
 recover: nCharacters
 	"Schedule an editable text view on the last n characters of changes."
-	| changes |
-	changes _ SourceFiles at: 2.
-	changes setToEnd; skip: nCharacters negated.
-	(FileStream newFileNamed: 'st80.recent') nextPutAll: (changes next: nCharacters); close; open; edit
+	self writeRecentCharacters: nCharacters toFileNamed: 'st80.recent'
