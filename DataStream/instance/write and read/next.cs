ab2106d005d6c0b5fd064c04fe783ebced0fd232
tk <unknown>
2000-02-03 21:16:00 +0000

    no message

---
+++ DataStream/instance/write and read/next.cs
@@ -0,0 +1,41 @@
+next
+	"Answer the next object in the stream."
+	| type selector anObject isARefType pos internalObject |
+
+	type _ byteStream next.
+	type ifNil: [pos _ byteStream position.	"absolute!!"
+		byteStream close.	"clean up"
+		byteStream position = 0 
+			ifTrue: [self error: 'The file did not exist in this directory'] 
+			ifFalse: [self error: 'Unexpected end of object file'].
+		pos.	"so can see it in debugger"
+		^ nil].
+	type = 0 ifTrue: [pos _ byteStream position.	"absolute!!"
+		byteStream close.	"clean up"
+		self error: 'Expected start of object, but found 0'.
+		^ nil].
+	isARefType _ self noteCurrentReference: type.
+	selector _ #(readNil readTrue readFalse readInteger
+			readStringOld readSymbol readByteArray
+			readArray readInstance readReference readBitmap
+			readClass readUser readFloat readRectangle readShortInst 
+			readString readWordArray readWordArrayForSegment 
+			readWordLike) at: type.
+	selector == 0 ifTrue: [pos _ byteStream position.	"absolute!!"
+			byteStream close. 
+			self error: 'file is more recent than this system'. ^ nil].
+	anObject _ self perform: selector. "A method that recursively
+		calls next (readArray, readInstance, objectAt:) must save &
+		restore the current reference position."
+	isARefType ifTrue: [self beginReference: anObject].
+
+		"After reading the externalObject, internalize it.
+		 #readReference is a special case. Either:
+		   (1) We actually have to read the object, recursively calling
+			   next, which internalizes the object.
+		   (2) We just read a reference to an object already read and
+			   thus already interalized.
+		 Either way, we must not re-internalize the object here."
+	selector == #readReference ifTrue: [^ anObject].
+	internalObject _ anObject comeFullyUpOnReload: self.
+	^ self maybeBeginReference: internalObject
