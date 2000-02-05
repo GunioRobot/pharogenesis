a1d6782a9c5af4194f934230c127a7881d652599
sma <unknown>
2000-02-05 12:03:00 +0000

    no message

--- Text/instance/emphasis/makeSelectorBoldIn-3A-.cs
+++ Text/instance/emphasis/makeSelectorBoldIn-3A-.cs
@@ -1,8 +1,8 @@
 makeSelectorBoldIn: aClass
 	"For formatting Smalltalk source code, set the emphasis of that portion of 
 	the receiver's string that parses as a message selector to be bold."
+
 	| parser |
 	string size = 0 ifTrue: [^self].
-	string first isLetter ifFalse: [^ self].
 	(parser _ aClass parserClass new) parseSelector: string.
 	self makeBoldFrom: 1 to: (parser endOfLastToken min: string size)
