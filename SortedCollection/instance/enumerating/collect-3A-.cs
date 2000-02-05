8d25f87aa4858f2c70d1f6fb6f9b2436d13d0d38
sma <unknown>
2000-02-05 15:22:00 +0000

    no message

--- SortedCollection/instance/enumerating/collect-3A-.cs
+++ SortedCollection/instance/enumerating/collect-3A-.cs
@@ -5,6 +5,6 @@ collect: aBlock
 	of a SortedCollection."
 
 	| newCollection | 
-	newCollection _ OrderedCollection new.
-	self do: [:each | newCollection add: (aBlock value: each)].
-	^newCollection
+	newCollection _ OrderedCollection new: self size.
+	self do: [:each | newCollection addLast: (aBlock value: each)].
+	^ newCollection
