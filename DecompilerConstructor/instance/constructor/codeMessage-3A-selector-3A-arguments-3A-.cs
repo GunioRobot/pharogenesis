7bfae0baf83755eca109bb2da234cbd5640340ed
sma <unknown>
2000-02-05 12:37:00 +0000

    no message

--- DecompilerConstructor/instance/constructor/codeMessage-3A-selector-3A-arguments-3A-.cs
+++ DecompilerConstructor/instance/constructor/codeMessage-3A-selector-3A-arguments-3A-.cs
@@ -1,12 +1,14 @@
 codeMessage: receiver selector: selector arguments: arguments
-
-	| symbol |
+	| symbol node |
 	symbol _ selector key.
-	^MessageNode new
-		receiver: receiver
-		selector: selector
-		arguments: arguments
-		precedence:
-			(symbol isInfix
-				ifTrue: [2]
-				ifFalse: [symbol isKeyword ifTrue: [3] ifFalse: [1]])
+	(node _ BraceNode new
+			matchBraceWithReceiver: receiver
+			selector: symbol
+			arguments: arguments) ifNotNil: [^ node].
+	(node _ self decodeIfNilWithReceiver: receiver
+			selector: symbol
+			arguments: arguments) ifNotNil: [^ node].
+	^ MessageNode new
+			receiver: receiver selector: selector
+			arguments: arguments
+			precedence: symbol precedence
