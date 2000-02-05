b467a389261867c2b3b60075af9486d2cf24fcee
sma <unknown>
2000-02-05 15:13:00 +0000

    no message

--- OrderedCollection/instance/enumerating/select-3A-.cs
+++ OrderedCollection/instance/enumerating/select-3A-.cs
@@ -1,8 +1,12 @@
 select: aBlock 
 	"Evaluate aBlock with each of my elements as the argument. Collect into
 	a new collection like the receiver, only those elements for which aBlock
-	evaluates to true. Override the superclass in order to use add:, not at:put:."
-	| newCollection |
+	evaluates to true."
+
+	| newCollection element |
 	newCollection _ self copyEmpty.
-	self do: [:each | (aBlock value: each) ifTrue: [newCollection add: each]].
+	firstIndex to: lastIndex do:
+		[:index |
+		(aBlock value: (element _ array at: index))
+			ifTrue: [newCollection addLast: element]].
 	^ newCollection
