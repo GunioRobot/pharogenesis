4af99ec6319e301c68808eac99dbe5c9fb78987b
di <unknown>
2000-02-06 08:46:00 +0000

    no message

--- Decompiler/instance/instruction decoding/jump-3A-if-3A-.cs
+++ Decompiler/instance/instruction decoding/jump-3A-if-3A-.cs
@@ -1,8 +1,8 @@
 jump: dist if: condition
 
 	| savePc elseDist sign elsePc elseStart end cond ifExpr thenBlock elseBlock thenJump
-		elseJump condHasValue b |
-	stack last == CascadeFlag ifTrue: [^self case: dist].
+		elseJump condHasValue b isIfNil saveStack |
+	stack last == CascadeFlag ifTrue: [^ self case: dist].
 	elsePc _ lastPc.
 	elseStart _ pc + dist.
 	end _ limit.
@@ -11,20 +11,27 @@ jump: dist if: condition
 	sign _ condition.
 	savePc _ pc.
 	((elseDist _ self interpretJump) notNil and: [elseDist >= 0 and: [elseStart = pc]])
-		ifTrue: [sign _ sign not.  elseStart _ pc + elseDist]
-		ifFalse: [pc _ savePc].
+		ifTrue: [sign _ sign not.  elseStart _ pc + elseDist].
+	pc _ savePc.
 	ifExpr _ stack removeLast.
+	(stack size > 0 and: [stack last == IfNilFlag])
+		ifTrue: [stack removeLast.  isIfNil _ true]
+		ifFalse: [isIfNil _ false].
+	saveStack _ stack.
+	stack _ OrderedCollection new.
 	thenBlock _ self blockTo: elseStart.
-	condHasValue _ hasValue.
+	condHasValue _ hasValue or: [isIfNil].
 	"ensure jump is within block (in case thenExpr returns)"
 	thenJump _ exit <= end ifTrue: [exit] ifFalse: [elseStart].
 	"if jump goes back, then it's a loop"
 	thenJump < elseStart
 		ifTrue:
-			["thenJump will jump to the beginning of the while expr.  In the case of
+			["Must be a while loop...
+			thenJump will jump to the beginning of the while expr.  In the case of
 			while's with a block in the condition, the while expr
 			should include more than just the last expression: find all the
 			statements needed by re-decompiling."
+			stack _ saveStack.
 			pc _ thenJump.
 			b _ self statementsTo: elsePc.
 			"discard unwanted statements from block"
@@ -36,18 +43,27 @@ jump: dist if: condition
 			pc _ elseStart.
 			self convertToDoLoop]
 		ifFalse:
-			[elseBlock _ self blockTo: thenJump.
+			["Must be a conditional..."
+			elseBlock _ self blockTo: thenJump.
 			elseJump _ exit.
 			"if elseJump is backwards, it is not part of the elseExpr"
 			elseJump < elsePc
 				ifTrue: [pc _ lastPc].
-			cond _ constructor
+			isIfNil
+			ifTrue: [cond _ constructor
+						codeMessage: ifExpr ifNilReceiver
+						selector: (sign
+							ifTrue: [constructor codeSelector: #ifNotNil: code: #macro]
+							ifFalse: [constructor codeSelector: #ifNil: code: #macro])
+						arguments: (Array with: thenBlock)]
+			ifFalse: [cond _ constructor
 						codeMessage: ifExpr
 						selector: (constructor codeSelector: #ifTrue:ifFalse: code: #macro)
 						arguments:
 							(sign
 								ifTrue: [Array with: elseBlock with: thenBlock]
-								ifFalse: [Array with: thenBlock with: elseBlock]).
+								ifFalse: [Array with: thenBlock with: elseBlock])].
+			stack _ saveStack.
 			condHasValue
 				ifTrue: [stack addLast: cond]
 				ifFalse: [statements addLast: cond]]
