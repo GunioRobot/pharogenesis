1ae1b99e0f7b4ed42d656d455727f2a678d10680
sma <unknown>
2000-02-06 18:36:00 +0000

    no message

---
+++ BlobMorph/instance/stepping/mergeBlobs.cs
@@ -0,0 +1,16 @@
+mergeBlobs
+	"See if we need to merge by checking our bounds against all other Blob
+	bounds, then all our vertices against any Blob with overlapping bounds.
+	If we find a need to merge, then someone else does all the work."
+
+	AllBlobs size < 2 ifTrue: [^ self].
+	AllBlobs
+		do:
+			[:aBlob |
+			aBlob owner == self owner ifTrue:
+				[(self bounds intersects: aBlob bounds) ifTrue:
+					[vertices do:
+						[:aPoint |
+						(aBlob containsPoint: aPoint) ifTrue:
+							[^ self mergeSelfWithBlob: aBlob atPoint: aPoint]]]]]
+		without: self
