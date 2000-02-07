b90a0dead643560453235342bdf641861a52e1bc
sw <unknown>
2000-02-07 11:28:00 +0000

    no message

--- Morph/instance/halos and balloon help/showBalloon-3A-.cs
+++ Morph/instance/halos and balloon help/showBalloon-3A-.cs
@@ -1,11 +1,16 @@
 showBalloon: msgString
-	"Pop up a balloon containing the given string, first removing any existing BalloonMorphs in the world."
+	"Pop up a balloon containing the given string,
+	first removing any existing BalloonMorphs in the world."
 
-	| w balloon |
-	w _ self world.
-	w ifNil: [^ self].
-	balloon _ BalloonMorph string: msgString for: self corner: #bottomRight.
-	w submorphsDo: [:m |  "delete any existing balloons"
-		(m isKindOf: BalloonMorph) ifTrue: [m delete]].
+	| w balloon worldBounds |
+	(w _ self world) ifNil: [^ self].
+	w submorphsDo: [:m | (m isKindOf: BalloonMorph) ifTrue: [m delete]].
+	balloon _ BalloonMorph string: msgString for: self balloonHelpAligner.
+	balloon lock.
 	w addMorphFront: balloon.
-	self setProperty: #balloon toValue: balloon.
+	"So that if the translation below makes it overlap the receiver, it won't
+	interfere with the rootMorphsAt: logic and hence cause flashing.  Without
+	this, flashing happens, believe me!"
+	((worldBounds _ w bounds) containsRect: balloon bounds) ifFalse:
+		[balloon bounds: (balloon bounds translatedToBeWithin: worldBounds)].
+	self setProperty: #balloon toValue: balloon
