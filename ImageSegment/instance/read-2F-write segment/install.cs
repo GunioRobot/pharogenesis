27495181dd6b5e73a2b2c2b53b981ef1eeedcd67
di <unknown>
2000-02-11 13:06:00 +0000

    no message

---
+++ ImageSegment/instance/read-2F-write segment/install.cs
@@ -0,0 +1,17 @@
+install
+	"This operation retrieves the segment if necessary from file storage, installs it in memory, and replaces (using become:) all the root stubs with the reconstructed roots of the segment."
+
+	| newRoots |
+	state = #onFile ifTrue: [self readFromFile].
+	state = #onFileWithSymbols ifTrue: [self readFromFileWithSymbols.
+		endMarker _ segment nextObject. 	"for enumeration of objects"
+		endMarker == 0 ifTrue: [endMarker _ 'End' clone]].
+	(state = #active) | (state = #imported) ifFalse: [self errorWrongState].
+	newRoots _ self loadSegmentFrom: segment outPointers: outPointers.
+	state = #imported 
+		ifTrue: ["just came in from exported file"
+			arrayOfRoots _ newRoots]
+		ifFalse: [
+			arrayOfRoots elementsForwardIdentityTo: newRoots].
+	state _ #inactive.
+	self beep.	"Don't use Squeak sound here."
