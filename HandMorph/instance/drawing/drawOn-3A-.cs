3e0587a3f4392ad6dc4cbc2cd5c087d070467d84
ar <unknown>
2000-02-12 18:05:00 +0000

    no message

--- HandMorph/instance/drawing/drawOn-3A-.cs
+++ HandMorph/instance/drawing/drawOn-3A-.cs
@@ -2,7 +2,9 @@ drawOn: aCanvas
 	"Draw the hand itself (i.e., the cursor)."
 
 	temporaryCursor == nil
-		ifTrue: [aCanvas image: NormalCursor at: bounds topLeft]
-		ifFalse: [aCanvas image: temporaryCursor at: bounds topLeft].
+		ifTrue: [aCanvas paintImage: NormalCursor at: bounds topLeft]
+		ifFalse: [aCanvas paintImage: temporaryCursor at: bounds topLeft].
 	userInitials size > 0 ifTrue:
-		[aCanvas text: userInitials at: (self position + (16@4)) font: nil color: color].
+		[aCanvas text: userInitials
+					at: (self cursorBounds topRight + (0@4))
+					font: nil color: color].
