8f633f9842c2924aad5ccf697be6508368de908c
ar <unknown>
2000-02-12 18:07:00 +0000

    no message

--- RemoteHandMorph/instance/other/drawOn-3A-.cs
+++ RemoteHandMorph/instance/other/drawOn-3A-.cs
@@ -2,4 +2,4 @@ drawOn: aCanvas
 	"For remote cursors, always draw the hand itself (i.e., the cursor)."
 
 	super drawOn: aCanvas.
-	aCanvas image: NormalCursor at: self position.
+	aCanvas paintImage: NormalCursor at: self position.
