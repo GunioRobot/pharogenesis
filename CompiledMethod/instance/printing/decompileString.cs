688daf4c2f0b7d918e7dfb0b5e229289a9121c91
sma <unknown>
2000-02-12 14:01:00 +0000

    no message

--- CompiledMethod/instance/printing/decompileString.cs
+++ CompiledMethod/instance/printing/decompileString.cs
@@ -1,7 +1,17 @@
 decompileString
 	| clAndSel cl sel |
 	clAndSel _ self who.
-	cl _ clAndSel first.
-	sel _ clAndSel last.
+	clAndSel = #(unknown unknown)
+		ifTrue:
+			[cl _ Object.
+			sel _ #xxxUnknown.
+			self numArgs >= 1
+				ifTrue:
+					[sel _ sel , ':'.
+					2 to: self numArgs do: [:i | sel _ sel , 'with:'].
+					sel _ sel asSymbol]]
+		ifFalse:
+			[cl _ clAndSel first.
+			sel _ clAndSel last].
 	^ (cl decompilerClass new
 			decompile: sel in: cl method: self) decompileString
