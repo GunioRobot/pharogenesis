1b0336b468df771be5ad5a50632ea78705508420
tk <unknown>
2000-02-14 15:03:00 +0000

    no message

---
+++ StandardFileMenu/instance/object fileIn/convertlflffmssc0-3A-lflffmsscp0-3A-.cs
@@ -0,0 +1,5 @@
+convertlflffmssc0: varDict lflffmsscp0: smartRefStrm
+	"These variables are automatically stored into the new instance ('canTypeFileName' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('pattern' )  If a non-nil value is needed, please assign it."
