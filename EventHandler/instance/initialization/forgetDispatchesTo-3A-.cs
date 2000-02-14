db5c97aefd86a0b4b2a2437b0d1974923d22118f
LC <unknown>
2000-02-14 08:13:00 +0000

    no message

--- EventHandler/instance/initialization/forgetDispatchesTo-3A-.cs
+++ EventHandler/instance/initialization/forgetDispatchesTo-3A-.cs
@@ -1,20 +1,20 @@
 forgetDispatchesTo: aSelector
 	"aSelector is no longer implemented by my corresponding Player, so don't call it any more"
 	mouseDownSelector == aSelector
-		ifTrue:
-			[mouseDownRecipient _ mouseDownSelector _ nil].
+		ifTrue: [mouseDownRecipient _ mouseDownSelector _ nil].
 	mouseStillDownSelector == aSelector
-		ifTrue:
-			[mouseStillDownRecipient _ mouseStillDownSelector _ nil].
+		ifTrue: [mouseStillDownRecipient _ mouseStillDownSelector _ nil].
 	mouseUpSelector == aSelector
-		ifTrue:
-			[mouseUpRecipient _ mouseUpSelector _ nil].
+		ifTrue: [mouseUpRecipient _ mouseUpSelector _ nil].
 	mouseEnterSelector == aSelector
-		ifTrue:
-			[mouseEnterRecipient _ mouseEnterSelector _ nil].
+		ifTrue: [mouseEnterRecipient _ mouseEnterSelector _ nil].
 	mouseLeaveSelector == aSelector
-		ifTrue:
-			[mouseLeaveRecipient _ mouseLeaveSelector _ nil].
+		ifTrue: [mouseLeaveRecipient _ mouseLeaveSelector _ nil].
+	mouseEnterDraggingSelector == aSelector
+		ifTrue: [mouseEnterDraggingRecipient _ mouseEnterDraggingSelector _ nil].
+	mouseLeaveDraggingSelector == aSelector
+		ifTrue: [mouseLeaveDraggingRecipient _ mouseLeaveDraggingSelector _ nil].
+	doubleClickSelector == aSelector
+		ifTrue: [doubleClickRecipient _ doubleClickSelector _ nil].
 	keyStrokeSelector == aSelector
-		ifTrue:
-			[keyStrokeRecipient _ keyStrokeSelector _ nil].
+		ifTrue: [keyStrokeRecipient _ keyStrokeSelector _ nil].
