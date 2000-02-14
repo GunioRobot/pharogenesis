75bbb4e21edcc546b95d2079173d6aebf6e58877
ssa <unknown>
2000-02-14 13:14:00 +0000

    no message

--- FillInTheBlankMorph/instance/initialization/initialize.cs
+++ FillInTheBlankMorph/instance/initialization/initialize.cs
@@ -1,6 +1,7 @@
 initialize
-
 	super initialize.
+	Preferences roundedWindowCorners ifTrue: [self useRoundedCorners].
 	color _ Color white.
 	borderWidth _ 2.
 	self extent: 200@70.
+	responseUponCancel _ ''.  "Caller can reset this to return something else, e.g. nil, upon cancel"
