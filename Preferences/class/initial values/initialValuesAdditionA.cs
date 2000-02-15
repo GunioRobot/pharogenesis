05ecdadd0030d0d760595e88dcadb4d3feae6bca
sw <unknown>
2000-02-15 18:37:00 +0000

    no message

---
+++ Preferences/class/initial values/initialValuesAdditionA.cs
@@ -0,0 +1,9 @@
+initialValuesAdditionA
+	"Another lot of initial values for prefs.  Simply placing methods of this sort in the 'initial values' category will assure that the initial values provided are ascribed to the preference when prefs are initialized"
+
+	^ #(	
+		(simpleMenus						false	(menus))
+)
+	"Preferences resetCategoryInfo"
+
+
