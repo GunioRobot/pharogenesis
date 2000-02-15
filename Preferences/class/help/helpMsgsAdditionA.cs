ca8eaca03e0f983053d82860bfca6ca9def9efdd
sw <unknown>
2000-02-15 18:39:00 +0000

    no message

---
+++ Preferences/class/help/helpMsgsAdditionA.cs
@@ -0,0 +1,15 @@
+helpMsgsAdditionA
+	"This, and the other methodds in this category, are automatically called whenever you call
+
+   	Preferences initializeHelpMessages
+		or
+	Preferences callHelpMessageInitializers
+"
+
+	^ #( 
+(simpleMenus
+	'If true, simpler menus are presented.')
+)
+	"Preferences resetCategoryInfo"
+
+
