27a4de2ec4b0c2d90f8471d1fd180198e9d1ad4d
ar <unknown>
2000-02-16 22:07:00 +0000

    no message

--- FormCanvas/instance/drawing/line-3A-to-3A-width-3A-color-3A-.cs
+++ FormCanvas/instance/drawing/line-3A-to-3A-width-3A-color-3A-.cs
@@ -1,8 +1,6 @@
 line: pt1 to: pt2 width: w color: c
 	| offset |
 	offset _ origin - (w // 2) asPoint.
-	port sourceForm: nil;
-		fillColor: (self drawColor: c);
-		combinationRule: (self drawRule: Form over);
-		width: w; height: w;
+	self setFillColor: c.
+	port width: w; height: w;
 		drawFrom: (pt1 + offset) to: (pt2 + offset)
