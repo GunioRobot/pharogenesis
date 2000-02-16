50e294de3241158e31ebfcc7650102ad57e85dcb
di <unknown>
2000-02-16 10:52:00 +0000

    no message

--- Inspector/class/instance creation/openOn-3A-withEvalPane-3A-withLabel-3A-.cs
+++ Inspector/class/instance creation/openOn-3A-withEvalPane-3A-withLabel-3A-.cs
@@ -1,4 +1,7 @@
 openOn: anObject withEvalPane: withEval withLabel: label
+	Smalltalk isMorphic ifTrue:
+		[^ self openAsMorphOn: anObject withEvalPane: withEval
+			withLabel: label valueViewClass: nil].
 	^ self openOn: anObject 
 		withEvalPane: withEval 
 		withLabel: label 
