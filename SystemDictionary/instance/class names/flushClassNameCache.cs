6ba68f464a6be11553c861bb399fd900d1fecdaf
di <unknown>
2000-02-16 10:28:00 +0000

    no message

--- SystemDictionary/instance/class names/flushClassNameCache.cs
+++ SystemDictionary/instance/class names/flushClassNameCache.cs
@@ -1,7 +1,5 @@
 flushClassNameCache
-	"This is an implementation efficiency: the collection of class names is 
-	saved as a class variable and recomputed whenever the collection is 
-	needed but has been previously flushed (set to nil).  Last touched sw 8/91"
 	"Smalltalk flushClassNameCache"
+	"Forse recomputation of the cached list of class names."
 
-	CachedClassNames _ nil
+	cachedClassNames _ nil
