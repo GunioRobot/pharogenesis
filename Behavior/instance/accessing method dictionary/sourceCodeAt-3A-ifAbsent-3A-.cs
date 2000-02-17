eb48577944cfe9e71bbed09dd68029f74ab39abb
di <unknown>
2000-02-17 22:40:00 +0000

    no message

--- Behavior/instance/accessing method dictionary/sourceCodeAt-3A-ifAbsent-3A-.cs
+++ Behavior/instance/accessing method dictionary/sourceCodeAt-3A-ifAbsent-3A-.cs
@@ -1,3 +1,3 @@
 sourceCodeAt: selector ifAbsent: aBlock
 
-	^ (methodDict at: selector ifAbsent: [^ aBlock value]) getSourceFor: selector in: self
+	^ (self methodDict at: selector ifAbsent: [^ aBlock value]) getSourceFor: selector in: self
