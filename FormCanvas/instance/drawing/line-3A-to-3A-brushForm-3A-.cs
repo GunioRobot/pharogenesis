d951fdb569d1e331d8a93d71f5d3e59bb0edd673
ar <unknown>
2000-02-17 00:12:00 +0000

    no message

--- FormCanvas/instance/drawing/line-3A-to-3A-brushForm-3A-.cs
+++ FormCanvas/instance/drawing/line-3A-to-3A-brushForm-3A-.cs
@@ -1,8 +1,8 @@
 line: pt1 to: pt2 brushForm: brush
 	| offset |
 	offset _ origin.
+	self setPaintColor: Color black.
 	port sourceForm: brush; fillColor: nil;
-		combinationRule: (self drawRule: Form paint);
 		sourceRect: brush boundingBox;
 		colorMap: (brush colormapIfNeededForDepth: self depth);
 		drawFrom: (pt1 + offset) to: (pt2 + offset)
