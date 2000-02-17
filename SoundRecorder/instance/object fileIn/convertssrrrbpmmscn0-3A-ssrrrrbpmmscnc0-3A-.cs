587da2ad97bc125917b3c63ff8400454e1759514
tk <unknown>
2000-02-17 21:48:00 +0000

    no message

---
+++ SoundRecorder/instance/object fileIn/convertssrrrbpmmscn0-3A-ssrrrrbpmmscnc0-3A-.cs
@@ -0,0 +1,5 @@
+convertssrrrbpmmscn0: varDict ssrrrrbpmmscnc0: smartRefStrm
+	"These variables are automatically stored into the new instance ('stereo' 'samplingRate' 'recordLevel' 'recordedBuffers' 'recordProcess' 'bufferAvailableSema' 'paused' 'meteringBuffer' 'meterLevel' 'soundPlaying' 'currentBuffer' 'nextIndex' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('recordedSound' 'codec' )  If a non-nil value is needed, please assign it."
