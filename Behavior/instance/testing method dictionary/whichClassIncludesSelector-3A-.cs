b087f5a829fb7bfaf5fb4af0907e4d2463ec1b96
di <unknown>
2000-02-17 22:39:00 +0000

    no message

--- Behavior/instance/testing method dictionary/whichClassIncludesSelector-3A-.cs
+++ Behavior/instance/testing method dictionary/whichClassIncludesSelector-3A-.cs
@@ -2,8 +2,8 @@ whichClassIncludesSelector: aSymbol
 	"Answer the class on the receiver's superclass chain where the argument, 
 	aSymbol (a message selector), will be found. Answer nil if none found."
 
-	(methodDict includesKey: aSymbol) ifTrue: [^self].
-	superclass == nil ifTrue: [^nil].
-	^superclass whichClassIncludesSelector: aSymbol
+	(self methodDict includesKey: aSymbol) ifTrue: [^ self].
+	superclass == nil ifTrue: [^ nil].
+	^ superclass whichClassIncludesSelector: aSymbol
 
 	"Rectangle whichClassIncludesSelector: #inspect."
