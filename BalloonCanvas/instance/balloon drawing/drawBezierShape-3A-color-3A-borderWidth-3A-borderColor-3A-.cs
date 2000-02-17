5123bf61032c341cf54f1bc222c2efe8c853df6f
ar <unknown>
2000-02-17 00:25:00 +0000

    no message

---
+++ BalloonCanvas/instance/balloon drawing/drawBezierShape-3A-color-3A-borderWidth-3A-borderColor-3A-.cs
@@ -0,0 +1,14 @@
+drawBezierShape: vertices color: c borderWidth: borderWidth borderColor: borderColor
+	"Draw a boundary shape that is defined by a list of vertices.
+	Each three subsequent vertices define a quadratic bezier segment.
+	For lines, the control point should be set to either the start or the end
+	of the bezier curve."
+	| fillC borderC |
+	fillC _ self shadowColor ifNil:[c].
+	borderC _ self shadowColor ifNil:[borderColor].
+	self ensuredEngine
+		drawBezierShape: vertices
+		fill: fillC
+		borderWidth: borderWidth
+		borderColor: borderC
+		transform: transform.
