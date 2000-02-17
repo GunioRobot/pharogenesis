2b14e5caaca2296961828a8180aa17bd9bc7aea5
di <unknown>
2000-02-17 22:37:00 +0000

    no message

--- Behavior/instance/accessing method dictionary/compiledMethodAt-3A-.cs
+++ Behavior/instance/accessing method dictionary/compiledMethodAt-3A-.cs
@@ -3,4 +3,4 @@ compiledMethodAt: selector
 	Symbol), a message selector in the receiver's method dictionary. If the 
 	selector is not in the dictionary, create an error notification."
 
-	^methodDict at: selector
+	^ self methodDict at: selector
