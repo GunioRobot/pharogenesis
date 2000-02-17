b3837412cb09286d7a79abc5ebf62c695516b3bd
di <unknown>
2000-02-17 20:31:00 +0000

    no message

---
+++ Morph/instance/fileIn-2F-out/updateFromResource.cs
@@ -0,0 +1,14 @@
+updateFromResource
+
+	| pathName newMorph f |
+	(pathName _ self valueOfProperty: #resourceFilePath) ifNil: [^ self].
+	(pathName asLowercase endsWith: '.morph')
+		ifTrue:
+			[newMorph _ (FileStream oldFileNamed: pathName) fileInObjectAndCode.
+			(newMorph isKindOf: Morph) ifFalse: [^ self error: 'Resource not a single morph']]
+		ifFalse:
+			[f _ Form fromFileNamed: pathName.
+			f ifNil: [^ self error: 'unrecognized image file format'].
+			newMorph _ SketchMorph withForm: f].
+	newMorph setProperty: #resourceFilePath toValue: pathName.
+	self owner replaceSubmorph: self by: newMorph
