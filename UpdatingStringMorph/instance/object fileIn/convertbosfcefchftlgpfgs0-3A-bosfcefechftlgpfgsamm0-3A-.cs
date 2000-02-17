a7e9760fd274ec75c4c46f9252fc02bc70865f47
tk <unknown>
2000-02-17 21:34:00 +0000

    no message

---
+++ UpdatingStringMorph/instance/object fileIn/convertbosfcefchftlgpfgs0-3A-bosfcefechftlgpfgsamm0-3A-.cs
@@ -0,0 +1,6 @@
+convertbosfcefchftlgpfgs0: varDict bosfcefechftlgpfgsamm0: smartRefStrm
+	"These variables are automatically stored into the new instance ('format' 'target' 'lastValue' 'getSelector' 'putSelector' 'floatPrecision' 'growable' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('stepTime' 'autoAcceptOnFocusLoss' 'minimumWidth' 'maximumWidth' )  If a non-nil value is needed, please assign it."
+	"These are going away ('stepFrequency' ).  Possibly store their info in another variable?"
