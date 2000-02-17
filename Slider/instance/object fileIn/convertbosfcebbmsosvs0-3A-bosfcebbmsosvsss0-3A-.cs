5ef39204ac57906c8844d09ea08edb42e76be29d
tk <unknown>
2000-02-17 21:44:00 +0000

    no message

---
+++ Slider/instance/object fileIn/convertbosfcebbmsosvs0-3A-bosfcebbmsosvsss0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcebbmsosvs0: varDict bosfcebbmsosvsss0: smartRefStrm
+	"These variables are automatically stored into the new instance ('slider' 'value' 'setValueSelector' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('sliderShadow' 'sliderColor' )  If a non-nil value is needed, please assign it."
