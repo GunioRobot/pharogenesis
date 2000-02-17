bc239cec4f332111536d67d9b6b61c0d579023cd
di <unknown>
2000-02-17 22:41:00 +0000

    no message

--- Behavior/instance/creating method dictionary/addSelector-3A-withMethod-3A-.cs
+++ Behavior/instance/creating method dictionary/addSelector-3A-withMethod-3A-.cs
@@ -2,5 +2,10 @@ addSelector: selector withMethod: compiledMethod
 	"Add the message selector with the corresponding compiled method to the 
 	receiver's method dictionary."
 
-	methodDict at: selector put: compiledMethod.
+	| oldMethod |
+	oldMethod _ self lookupSelector: selector.
+	self methodDict at: selector put: compiledMethod.
+
+	"Now flush Squeak's method cache, either by selector or by method"
+	oldMethod == nil ifFalse: [oldMethod flushCache].
 	selector flushCache
