a437891a781c978769defca5a504759203380b43
di <unknown>
2000-02-17 22:41:00 +0000

    no message

--- Behavior/instance/accessing method dictionary/compiledMethodAt-3A-ifAbsent-3A-.cs
+++ Behavior/instance/accessing method dictionary/compiledMethodAt-3A-ifAbsent-3A-.cs
@@ -1,4 +1,4 @@
 compiledMethodAt: selector ifAbsent: aBlock
 	"Answer the compiled method associated with the argument, selector (a Symbol), a message selector in the receiver's method dictionary. If the selector is not in the dictionary, return the value of aBlock"
 
-	^ methodDict at: selector ifAbsent: [aBlock value]
+	^ self methodDict at: selector ifAbsent: [aBlock value]
