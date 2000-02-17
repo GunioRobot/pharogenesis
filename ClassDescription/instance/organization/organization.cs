6102e9f795d1d1a7ac49901cd729335e08d204a8
di <unknown>
2000-02-17 22:36:00 +0000

    no message

--- ClassDescription/instance/organization/organization.cs
+++ ClassDescription/instance/organization/organization.cs
@@ -2,8 +2,9 @@ organization
 	"Answer the instance of ClassOrganizer that represents the organization 
 	of the messages of the receiver."
 
-	organization==nil
-		ifTrue: [organization _ 
-				 ClassOrganizer defaultList: 
-						methodDict keys asSortedCollection asArray].
-	^organization
+	organization ifNil:
+		[organization _ ClassOrganizer defaultList: 
+						self methodDict keys asSortedCollection asArray].
+	(organization isMemberOf: Array) ifTrue:
+		[self recoverFromMDFault].
+	^ organization
