6601aafeaeee32a3161041c78f3d60f5cdf50391
tk <unknown>
2000-02-17 21:34:00 +0000

    no message

---
+++ ScriptEditorMorph/instance/object fileIn/convertbosfcebbochvimolsftp0-3A-bosfcebbochvimolpsftph0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcebbochvimolsftp0: varDict bosfcebbochvimolpsftph0: smartRefStrm
+	"These variables are automatically stored into the new instance ('scriptName' 'firstTileRow' 'timeStamp' 'playerScripted' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('handWithTile' )  If a non-nil value is needed, please assign it."
