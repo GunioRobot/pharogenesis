1da9e5f24562be6082a61a06abf82fab78f985d2
tk <unknown>
2000-02-17 21:44:00 +0000

    no message

---
+++ SystemWindow/instance/object fileIn/convertbosfcebbmsolslccappcfi0-3A-bosfcebbmsolslccappcfimmlu0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcebbmsolslccappcfi0: varDict bosfcebbmsolslccappcfimmlu0: smartRefStrm
+	"These variables are automatically stored into the new instance ('labelString' 'stripes' 'label' 'closeBox' 'collapseBox' 'activeOnlyOnTop' 'paneMorphs' 'paneRects' 'collapsedFrame' 'fullFrame' 'isCollapsed' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('menuBox' 'mustNotClose' 'labelWidgetAllowance' 'updatablePanes' )  If a non-nil value is needed, please assign it."
