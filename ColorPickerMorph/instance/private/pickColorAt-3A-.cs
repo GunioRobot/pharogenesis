ef2f77d078fff7866659ea5e26e32f9716ee8348
ar <unknown>
2000-02-17 01:12:00 +0000

    no message

--- ColorPickerMorph/instance/private/pickColorAt-3A-.cs
+++ ColorPickerMorph/instance/private/pickColorAt-3A-.cs
@@ -1,16 +1,31 @@
-pickColorAt: aPoint
+pickColorAt: aPoint 
 
-	| worldBox globalP c |
-	(FeedbackBox containsPoint: aPoint - self topLeft) ifTrue: [^ self].  "do nothing"
+	"RAA 27 Nov 99 - aPoint is global, so no need to add viewbox topleft"
+
+	| worldBox globalP c alpha localPt |
+
+	localPt _ aPoint - self topLeft.
+	(FeedbackBox containsPoint: localPt) ifTrue: [^ self].
+	(RevertBox containsPoint: localPt)
+		ifTrue: [^ self updateColor: originalColor feedbackColor: originalColor].
 
 	"pick up color, either inside or outside this world"
 	worldBox _ self world viewBox.
-	globalP _ aPoint + worldBox topLeft.  "get point in screen coordinates"
+	globalP _ aPoint 		"+ worldBox topLeft".
+	"get point in screen coordinates"
 	(worldBox containsPoint: globalP)
 		ifTrue: [c _ self world colorAt: aPoint belowMorph: Morph new]
 		ifFalse: [c _ Display colorAt: globalP].
 
-	"check for transparent color and update using appropriate feedback color"
-	(TransparentBox containsPoint: aPoint - self topLeft)
-		ifTrue: [self updateColor: Color transparent feedbackColor: Color white]
-		ifFalse: [self updateColor: c feedbackColor: c].
+	"check for transparent color and update using appropriate feedback color "
+	(TransparentBox containsPoint: localPt)
+		ifTrue: [alpha _ (aPoint x - bounds left - TransparentBox left - 10) asFloat /
+							(TransparentBox width - 20)
+							min: 1.0 max: 0.0.
+					"(alpha roundTo: 0.01) printString , '   ' displayAt: 0@0." " -- debug"
+					self updateColor: (selectedColor alpha: alpha)
+						feedbackColor: (selectedColor alpha: alpha)]
+		ifFalse: [self updateColor: ((selectedColor isColor and: [selectedColor isTranslucentColor])
+					ifTrue: [c alpha: selectedColor alpha]
+					ifFalse: [c])
+				feedbackColor: c]
