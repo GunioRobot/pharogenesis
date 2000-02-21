ce356e78a8635884e66d454c44cabf8d64d233fb
ar <unknown>
2000-02-21 19:53:00 +0000

    no message

--- CCodeGenerator/instance/C code generator/emitCCodeOn-3A-doInlining-3A-doAssertions-3A-.cs
+++ CCodeGenerator/instance/C code generator/emitCCodeOn-3A-doInlining-3A-doAssertions-3A-.cs
@@ -1,7 +1,7 @@
 emitCCodeOn: aStream doInlining: inlineFlag doAssertions: assertionFlag
 	"Emit C code for all methods in the code base onto the given stream. All inlined method calls should already have been expanded."
 
-	| verbose |
+	| verbose methodList |
 	"method preparation"
 	verbose _ false.
 	self prepareMethods.
@@ -14,14 +14,14 @@ emitCCodeOn: aStream doInlining: inlineFlag doAssertions: assertionFlag
 	self doInlining: inlineFlag.
 
 	"code generation"
-	methods _ methods asSortedCollection: [ :m1 :m2 | m1 selector < m2 selector ].
+	methodList _ methods asSortedCollection: [ :m1 :m2 | m1 selector < m2 selector ].
 	self emitCHeaderOn: aStream.
 	self emitCVariablesOn: aStream.
-	self emitCFunctionPrototypesOn: aStream.
+	self emitCFunctionPrototypes: methodList on: aStream.
 'Writing Translated Code...'
 displayProgressAt: Sensor cursorPoint
 from: 0 to: methods size
 during: [:bar |
-	methods doWithIndex: [ :m :i | bar value: i.
+	methodList doWithIndex: [ :m :i | bar value: i.
 		m emitCCodeOn: aStream generator: self.
 ]].
