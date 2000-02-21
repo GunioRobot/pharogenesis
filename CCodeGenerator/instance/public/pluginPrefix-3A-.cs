c3f1f261f0392ae0a922718b9aab5898edf0d537
ar <unknown>
2000-02-21 14:58:00 +0000

    no message

---
+++ CCodeGenerator/instance/public/pluginPrefix-3A-.cs
@@ -0,0 +1,5 @@
+pluginPrefix: aString
+	"Set the plugin prefix when generating local plugins.
+	Local plugins are plugins compiled with the main interpreter
+	source but are not included (nor inlined into) interp.c"
+	pluginPrefix _ aString.
