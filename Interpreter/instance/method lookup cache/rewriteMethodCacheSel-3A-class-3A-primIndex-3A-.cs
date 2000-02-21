d5862f4375dd39923cbb7d174929a74589fa18ef
sr <unknown>
2000-02-21 19:50:00 +0000

    no message

---
+++ Interpreter/instance/method lookup cache/rewriteMethodCacheSel-3A-class-3A-primIndex-3A-.cs
@@ -0,0 +1,15 @@
+rewriteMethodCacheSel: selector class: class primIndex: primIndex 
+	"Rewrite an existing entry in the method cache with a new primitive   
+	index."
+	| probe hash |
+	self inline: false.
+	hash _ selector bitXor: class.
+	0 to: CacheProbeMax - 1 do: 
+		[:p | 
+		probe _ hash >> p bitAnd: MethodCacheMask.
+		((methodCache at: probe + MethodCacheSelector)
+			= selector and: [(methodCache at: probe + MethodCacheClass)
+				= class])
+			ifTrue: 
+				[methodCache at: probe + MethodCachePrim put: primIndex.
+				^ nil]]
