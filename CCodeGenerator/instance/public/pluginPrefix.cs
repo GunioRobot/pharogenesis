e206be10fdd969715a1ae0288898fcb5484cd539
ar <unknown>
2000-02-21 14:58:00 +0000

    no message

---
+++ CCodeGenerator/instance/public/pluginPrefix.cs
@@ -0,0 +1,5 @@
+pluginPrefix
+	"Return the plugin prefix when generating local plugins.
+	Local plugins are plugins compiled with the main interpreter
+	source but are not included (nor inlined into) interp.c"
+	^pluginPrefix
