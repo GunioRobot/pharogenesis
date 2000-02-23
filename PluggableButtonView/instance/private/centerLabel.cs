47d3faffacf83e66f31d3fe5e00493ce486d9e93
acg <unknown>
2000-02-23 00:10:00 +0000

    no message

--- PluggableButtonView/instance/private/centerLabel.cs
+++ PluggableButtonView/instance/private/centerLabel.cs
@@ -1,9 +1,5 @@
 centerLabel
-	"Align the center of the label with the center of the receiver's window."
+	"If there is a label, align its center with the center of the insetDisplayBox"
 
-	| alignPt |
-	label ifNotNil: [
-		alignPt _ label boundingBox center.
-		(label isKindOf: Paragraph) ifTrue: [
-			alignPt _ alignPt + (0@1)].  "compensate for leading in default style"
-		label align: alignPt with: self getWindow center].
+	label ifNotNil: 
+		[self centerAlignLabelWith: self insetDisplayBox center].
