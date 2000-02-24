c06aaf363328c233dbf6498bc34b1d7950603320
tk <unknown>
2000-02-24 13:47:00 +0000

    no message

---
+++ ImageSegment/instance/read-2F-write segment/rootsIncludingPlayers.cs
@@ -0,0 +1,37 @@
+rootsIncludingPlayers
+	"Return a new roots array with more objects.  (Caller should store into rootArray.) Player (non-systemDefined) gets its class and metaclass put into the Roots array.  Then ask for the segment again."
+
+| extras havePresenter players morphs env |
+userRootCnt ifNil: [userRootCnt _ arrayOfRoots size].
+extras _ OrderedCollection new.
+arrayOfRoots do: [:root | 
+	(root isKindOf: Presenter) ifTrue: [havePresenter _ root].
+	(root isKindOf: PasteUpMorph) ifTrue: [
+			root isWorldMorph ifTrue: [havePresenter _ root presenter]].
+	(root isKindOf: Project) ifTrue: [havePresenter _ root world presenter]].
+havePresenter ifNotNil: [
+	havePresenter flushPlayerListCache.		"old and outside guys"
+	morphs _ IdentitySet new: 400.
+	havePresenter associatedMorph allMorphsAndBookPagesInto: morphs.
+	players _ (morphs select: [:m | m player ~~ nil] 
+				thenCollect: [:m | m player]) asArray.
+	players _ players select: [:ap | (arrayOfRoots includes: ap class) not
+		& (ap class isSystemDefined not)].
+	extras addAll: (players collect: [:each | each class]).
+	(env _ havePresenter world project environment) ifNil: [
+		extras addAll: (players collect: [:each | each class class])].
+	].
+extras isEmpty ifTrue: [^ nil].	"no change"
+env 
+	ifNil: ["old pre-environment"
+		havePresenter _ players _ morphs _ nil.
+		^ arrayOfRoots, extras]	"will contain multiples of some, but reduced later"
+	ifNotNil: [
+		(env includesKey: #Object) ifTrue: [self error: 'only look in local env, not up chain'].
+			"If get error, use a message other than includesKey:"
+		extras do: [:cls | 
+			(env includesKey: cls name) ifFalse: [
+				env declare: cls name from: Smalltalk]].
+		havePresenter _ players _ morphs _ env _ nil.
+		^ arrayOfRoots, extras	"still need in roots in case outside pointers"
+		]
