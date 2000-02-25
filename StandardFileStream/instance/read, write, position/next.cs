1c506919b77e90645e570bdfe5d387c703dcca85
mir <unknown>
2000-02-25 12:37:00 +0000

    no message

--- StandardFileStream/instance/read, write, position/next.cs
+++ StandardFileStream/instance/read, write, position/next.cs
@@ -1,8 +1,4 @@
 next
-	"Read the next object from the file. "
+	"Answer the next byte from this file, or nil if at the end of the file."
 
-	| count |
-	count _ self primRead: fileID into: buffer1 startingAt: 1 count: 1.
-	count = 1
-		ifTrue: [ ^ buffer1 at: 1 ]
-		ifFalse: [ ^ nil ].
+	^ self basicNext
