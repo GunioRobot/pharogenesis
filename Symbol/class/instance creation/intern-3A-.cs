a3feaaf5cd7bf1c7142d988619ee95e1c4869e11
sma <unknown>
2000-02-26 20:17:00 +0000

    no message

--- Symbol/class/instance creation/intern-3A-.cs
+++ Symbol/class/instance creation/intern-3A-.cs
@@ -1,11 +1,14 @@
 intern: aString 
 	"Answer a unique Symbol whose characters are those of aString."
 
-	| ascii table mainTable index sym numArgs |
+	| ascii table mainTable index sym numArgs symbol lastNilIndex |
 
-	ascii _ (aString at: 1) asciiValue.
-	aString size = 1 ifTrue: [ascii < 128 ifTrue: 
-		[^ SingleCharSymbols at: ascii + 1]].
+	aString size = 0
+		ifTrue: [ascii _ 0]
+		ifFalse:
+			[ascii _ (aString at: 1) asciiValue.
+			aString size = 1 ifTrue: [ascii < 128 ifTrue: 
+				[^ SingleCharSymbols at: ascii + 1]]].
 
 	table _ ((ascii >= "$a asciiValue" 97) and:
 		[(ascii <= "$z asciiValue" 122) and:
@@ -17,12 +20,18 @@ intern: aString
 						at: (index _ aString stringhash \\ OtherTable size + 1)].
 
 	1 to: table size do: [:i |
-		aString size = (table at: i) size ifTrue: [aString = (table at: i) ifTrue: [^ table at: i]]
+		symbol _ table at: i.
+		symbol isNil 
+			ifTrue:[lastNilIndex _ i]
+			ifFalse:[(aString size = symbol size and:[aString = symbol])
+						ifTrue:[^symbol]]
 	].
 
 	sym _ (aString isMemberOf: Symbol)
 		ifTrue: [aString]	"putting old symbol in new table"
 		ifFalse: [(Symbol new: aString size) string: aString]. "create a new one"
 
-	mainTable at: index put: (table copyWith: sym).
+	lastNilIndex isNil
+		ifTrue:[mainTable at: index put: (table copyWith: sym)]
+		ifFalse:[table at: lastNilIndex put: sym].
 	^sym
