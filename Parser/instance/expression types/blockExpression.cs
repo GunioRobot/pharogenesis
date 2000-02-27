4fee20ef17b314f0e35a56e64f0762e0fefdd063
sma <unknown>
2000-02-27 22:38:00 +0000

    no message

--- Parser/instance/expression types/blockExpression.cs
+++ Parser/instance/expression types/blockExpression.cs
@@ -1,15 +1,20 @@
 blockExpression
-	" [ {:var} ( | statements) ] => BlockNode."
+	"[ ({:var} |) (| {temps} |) (statements) ] => BlockNode."
 
-	| argNodes |
-	argNodes _ OrderedCollection new.
-	[self match: #colon
-	"gather any arguments"]
-		whileTrue: 
-			[argNodes addLast: (encoder autoBind: self argumentName)].
-	(argNodes size > 0 & (hereType ~~ #rightBracket) and: [(self match: #verticalBar) not])
-		ifTrue: [^self expected: 'Vertical bar'].
-	self statements: argNodes innerBlock: true.
-	(self match: #rightBracket)
-		ifFalse: [^self expected: 'Period or right bracket'].
-	argNodes do: [:arg | arg scope: -1] "Scope no longer active"
+	| variableNodes temporaryBlockVariables |
+
+	variableNodes _ OrderedCollection new.
+
+	"Gather parameters."
+	[self match: #colon] whileTrue: [variableNodes addLast: (encoder autoBind: self argumentName)].
+	(variableNodes size > 0 & (hereType ~~ #rightBracket) and: [(self match: #verticalBar) not]) ifTrue: [^self expected: 'Vertical bar'].
+
+	temporaryBlockVariables _ self temporaryBlockVariables.
+	self statements: variableNodes innerBlock: true.
+	parseNode temporaries: temporaryBlockVariables.
+
+	(self match: #rightBracket) ifFalse: [^self expected: 'Period or right bracket'].
+
+	"The scope of the parameters and temporary block variables is no longer active."
+	temporaryBlockVariables do: [:variable | variable scope: -1].
+	variableNodes do: [:variable | variable scope: -1]
