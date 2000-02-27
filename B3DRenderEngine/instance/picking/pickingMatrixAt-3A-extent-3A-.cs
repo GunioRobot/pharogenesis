05ccd6061e509195b415c9cf04dee251747b4f32
ar <unknown>
2000-02-27 20:12:00 +0000

    no message

---
+++ B3DRenderEngine/instance/picking/pickingMatrixAt-3A-extent-3A-.cs
@@ -0,0 +1,3 @@
+pickingMatrixAt: aPoint extent: extentPoint
+	"Return a matrix for picking at the given point using the given extent."
+	^self pickingMatrixFor: self viewport at: aPoint extent: extentPoint
