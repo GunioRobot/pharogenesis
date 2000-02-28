9a5b6b86085e19d42fe5b74c3e13218cfc334a0a
bolot <unknown>
2000-02-28 00:00:00 +0000

    no message

---
+++ HtmlInput/instance/formatting/addImageButtonToFormatter-3A-.cs
@@ -0,0 +1,20 @@
+addImageButtonToFormatter: formatter
+	"is it a submit button?"
+	| formData imageUrl morph |
+	(imageUrl _ self getAttribute: 'src') ifNil: [^self].
+	formatter baseUrl
+		ifNotNil: [imageUrl _ imageUrl asUrlRelativeTo: formatter baseUrl].
+
+	morph _ DownloadingImageMorph new.
+	morph defaultExtent: self imageExtent.
+	morph altText: self alt.
+	morph url: imageUrl.
+
+	value _ self getAttribute: 'name' default: 'Submit'.
+	formData _ formatter currentFormData.
+	morph
+		on: #mouseUp
+		send: #mouseUpEvent:linkMorph:formData:
+		to: self
+		withValue: formData.
+	formatter addIncompleteMorph: morph
