43c0170fdebcb503d3370ba027c3e6212b74aacb
sma <unknown>
2000-02-29 19:59:00 +0000

    no message

--- Object/instance/dependents access/hasUnacceptedEdits.cs
+++ Object/instance/dependents access/hasUnacceptedEdits.cs
@@ -1,7 +1,7 @@
 hasUnacceptedEdits
 	"Answer true if any of the views on this object has unaccepted edits."
 
-	self dependents do: [:v |
-		v == self ifFalse: [
-			v hasUnacceptedEdits ifTrue: [^ true]]].
-	^ false 
+	self dependents
+		do: [:each | each hasUnacceptedEdits ifTrue: [^ true]]
+		without: self.
+	^ false
