2a2187cca036cc27719b752d34309e9107e9bf0b
sma <unknown>
2000-02-29 20:23:00 +0000

    no message

--- Object/instance/dependents access/removeDependent-3A-.cs
+++ Object/instance/dependents access/removeDependent-3A-.cs
@@ -1,9 +1,7 @@
 removeDependent: anObject
 	"Remove the given object as one of the receiver's dependents."
 
-	| dependents newDependents |
-	dependents _ self dependents.
-	newDependents _ dependents select: [ :d | (d == anObject) not].
-	newDependents isEmpty
-		ifTrue: [DependentsFields removeKey: self ifAbsent: []]
-		ifFalse: [DependentsFields at: self put: newDependents].
+	| dependents |
+	dependents _ self dependents reject: [:each | each == anObject].
+	self myDependents: (dependents isEmpty ifFalse: [dependents]).
+	^ anObject
