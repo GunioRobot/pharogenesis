370b5fc0f809baeff5faba4667b9948e13c4a79f
sma <unknown>
2000-02-29 19:26:00 +0000

    no message

--- Model/instance/dependents/canDiscardEdits.cs
+++ Model/instance/dependents/canDiscardEdits.cs
@@ -1,5 +1,5 @@
 canDiscardEdits
 	"Answer true if none of the views on this model has unaccepted edits that matter."
 
-	dependents == nil ifTrue: [^ true].
+	dependents ifNil: [^ true].
 	^ super canDiscardEdits
