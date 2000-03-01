34933d1edc9d36cabc012c3557c3483c3c341d20
sw <unknown>
2000-03-01 10:57:00 +0000

    no message

---
+++ MessageSet/instance/contents/selectedMessage.cs
@@ -0,0 +1,15 @@
+selectedMessage
+	"Answer the source method for the currently selected message."
+	| source |
+	self setClassAndSelectorIn: [:class :selector | 
+		class ifNil: [^ 'Class vanished'].
+		source _ class sourceMethodAt: selector ifAbsent:
+			[currentCompiledMethod _ nil.
+			^ 'Missing'].
+		currentCompiledMethod _ class compiledMethodAt: selector ifAbsent: [nil].
+		Preferences browseWithPrettyPrint ifTrue:
+			[source _ class compilerClass new
+				format: source in: class notifying: nil decorated: Preferences colorWhenPrettyPrinting].
+		self showDiffs ifTrue:
+			[source _ self diffFromPriorSourceFor: source].
+		^ source asText makeSelectorBoldIn: class]
