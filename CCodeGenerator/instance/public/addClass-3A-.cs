8198bcbc1488621c7e58b46922f2c8aa08ca2e5d
TPR <unknown>
2000-03-02 11:26:00 +0000

    no message

--- CCodeGenerator/instance/public/addClass-3A-.cs
+++ CCodeGenerator/instance/public/addClass-3A-.cs
@@ -1,22 +1,22 @@
-addClass: aClass
+addClass: aClass 
 	"Add the variables and methods of the given class to the code base."
 	| source |
 	self checkClassForNameConflicts: aClass.
-	aClass classPool associationsDo: [ :assoc |
-		constants at: assoc key put: (TConstantNode new setValue: assoc value).
-	].
+	self addClassVarsFor: aClass.
 	"ikp..."
-	aClass sharedPools do: [:pool |
-		pool associationsDo: [ :assoc |
-			constants at: assoc key put: (TConstantNode new setValue: assoc value).
-		].
-	].
+	self addPoolVarsFor: aClass.
 	variables addAll: aClass instVarNames.
-'Adding Class ' , aClass name , '...'
-displayProgressAt: Sensor cursorPoint
-from: 0 to: aClass selectors size
-during: [:bar |
-	aClass selectors doWithIndex: [ :sel :i | bar value: i.
-		source _ aClass sourceCodeAt: sel.
-		self addMethod: ((Compiler new parse: source in: aClass notifying: nil) asTMethodFromClass: aClass).
-	]].
+	'Adding Class ' , aClass name , '...'
+		displayProgressAt: Sensor cursorPoint
+		from: 0
+		to: aClass selectors size
+		during: [:bar | aClass selectors
+				doWithIndex: 
+					[:sel :i | 
+					bar value: i.
+					source _ aClass sourceCodeAt: sel.
+					self addMethod: ((Compiler new
+							parse: source
+							in: aClass
+							notifying: nil)
+							asTranslationMethodOfClass: self translationMethodClass)]]
