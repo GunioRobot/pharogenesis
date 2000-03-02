1fb1acae9f20911eab9bbc8b4d4055073bf92a75
TPR <unknown>
2000-03-02 16:13:00 +0000

    no message

--- TMethod/instance/primitive compilation/popArgsExpr-3A-.cs
+++ TMethod/instance/primitive compilation/popArgsExpr-3A-.cs
@@ -2,5 +2,5 @@ popArgsExpr: argCount
 	"Return the parse tree for an expression that pops the given number of arguments from the stack."
 
 	| expr |
-	expr _ 'self pop: ', argCount printString.
+	expr _ '', self vmNameString, ' pop: ', argCount printString.
 	^ self statementsFor: expr varName: ''
