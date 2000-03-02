271eaf065435803014fbe99dd7952ca60482a55e
TPR <unknown>
2000-03-02 16:12:00 +0000

    no message

--- TMethod/instance/primitive compilation/instVarGetExprFor-3A-offset-3A-.cs
+++ TMethod/instance/primitive compilation/instVarGetExprFor-3A-offset-3A-.cs
@@ -7,17 +7,17 @@ instVarGetExprFor: varName offset: instIndex
 		decl _ declarations at: varName.
 		(decl includes: $*) ifTrue: [  "array"
 			exprList add:
-				(varName, ' _ self fetchArray: ', instIndex printString, ' ofObject: rcvr').
+				(varName, ' _ ', self vmNameString, ' fetchArray: ', instIndex printString, ' ofObject: rcvr').
 			exprList add: (varName, ' _ ', varName, ' - 1').
 		] ifFalse: [  "must be a double"
 			((decl findString: 'double' startingAt: 1) = 0)
 				ifTrue: [ self error: 'unsupported type declaration in a primitive method' ].
 			exprList add:
-				(varName, ' _ self fetchFloat: ', instIndex printString, ' ofObject: rcvr').
+				(varName, ' _ ', self vmNameString, ' fetchFloat: ', instIndex printString, ' ofObject: rcvr').
 		].
 	] ifFalse: [  "undeclared variables are taken to be integer"
 		exprList add:
-			(varName, ' _ self fetchInteger: ', instIndex printString, ' ofObject: rcvr').
+			(varName, ' _ ', self vmNameString, ' fetchInteger: ', instIndex printString, ' ofObject: rcvr').
 	].
 	stmtList _ OrderedCollection new.
 	exprList do: [:e | stmtList addAll: (self statementsFor: e varName: varName)].
