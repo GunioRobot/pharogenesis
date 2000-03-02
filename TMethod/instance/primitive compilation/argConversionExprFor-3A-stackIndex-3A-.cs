d70edea3579300d7e3493ec8d7988157966efbb7
TPR <unknown>
2000-03-02 16:12:00 +0000

    no message

--- TMethod/instance/primitive compilation/argConversionExprFor-3A-stackIndex-3A-.cs
+++ TMethod/instance/primitive compilation/argConversionExprFor-3A-stackIndex-3A-.cs
@@ -1,22 +1,23 @@
-argConversionExprFor: varName stackIndex: stackIndex
-	"Return the parse tree for an expression that fetches and converts the primitive argument at the given stack offset."
-
-	| exprList expr decl stmtList |
+argConversionExprFor: varName stackIndex: stackIndex 
+	"Return the parse tree for an expression that fetches and converts the 
+	primitive argument at the given stack offset."
+	| exprList decl stmtList |
 	exprList _ OrderedCollection new.
-	expr _ '(self stackValue: ( ', stackIndex printString, '))'.
-	(declarations includesKey: varName) ifTrue: [  "array"
-		decl _ declarations at: varName.
-		(decl includes: $*) ifTrue: [
-			exprList add: (varName, ' _ self arrayValueOf: ', expr).
-			exprList add: (varName, ' _ ', varName, ' - 1').
-		] ifFalse: [  "must be a double"
-			((decl findString: 'double' startingAt: 1) = 0)
-				ifTrue: [ self error: 'unsupported type declaration in a primitive method' ].
-			exprList add: (varName, ' _ self floatValueOf: ', expr).
-		].
-	] ifFalse: [  "undeclared variables are taken to be integer"
-		exprList add: (varName, ' _ self checkedIntegerValueOf: ', expr).
-	].
+	(declarations includesKey: varName)
+		ifTrue: 
+			[decl _ declarations at: varName.
+			(decl includes: $*)
+				ifTrue: 
+					["array"
+					exprList add: varName , ' _ ', self vmNameString, ' arrayValueOf: (', self vmNameString, ' stackValue: (' , stackIndex printString , '))'.
+					exprList add: varName , ' _ ' , varName , ' - 1']
+				ifFalse: 
+					["must be a double"
+					(decl findString: 'double' startingAt: 1)
+						= 0 ifTrue: [self error: 'unsupported type declaration in a primitive method'].
+					exprList add: varName , ' _ ', self vmNameString, ' stackFloatValue: ' , stackIndex printString]]
+		ifFalse: ["undeclared variables are taken to be integer"
+			exprList add: varName , ' _ ', self vmNameString, ' stackIntegerValue: ' , stackIndex printString].
 	stmtList _ OrderedCollection new.
 	exprList do: [:e | stmtList addAll: (self statementsFor: e varName: varName)].
 	^ stmtList
