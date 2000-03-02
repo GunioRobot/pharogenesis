acaaa323edeac72506740fa4847733196e66e6d8
TPR <unknown>
2000-03-02 16:12:00 +0000

    no message

--- TMethod/instance/primitive compilation/fetchRcvrExpr.cs
+++ TMethod/instance/primitive compilation/fetchRcvrExpr.cs
@@ -2,5 +2,5 @@ fetchRcvrExpr
 	"Return the parse tree for an expression that fetches the receiver from the stack."
 
 	| expr |
-	expr _ 'rcvr _ self stackValue: (', args size printString, ')'.
+	expr _ 'rcvr _ ', self vmNameString, ' stackValue: (', args size printString, ')'.
 	^ self statementsFor: expr varName: ''
