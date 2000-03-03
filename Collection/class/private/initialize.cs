206b275475e2293347d58b865979a9c0d989256e
sma <unknown>
2000-03-03 10:45:00 +0000

    no message

--- Collection/class/private/initialize.cs
+++ Collection/class/private/initialize.cs
@@ -1,4 +1,5 @@
 initialize
-	"Set up a Random number generator to be used by pickOne when the user does not feel like creating his own Random generator."
-	RandomForPicking _ Random new.
-	
+	"Set up a Random number generator to be used by atRandom when the 
+	user does not feel like creating his own Random generator."
+
+	RandomForPicking _ Random new
