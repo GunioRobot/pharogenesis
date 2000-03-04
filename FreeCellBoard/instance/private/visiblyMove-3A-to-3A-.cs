efdc5155f918cb2a77bed6b639d2f4cad73418ca
RAA <unknown>
2000-03-04 10:43:00 +0000

    no message

---
+++ FreeCellBoard/instance/private/visiblyMove-3A-to-3A-.cs
@@ -0,0 +1,11 @@
+visiblyMove: aCard to: aCell
+	| p1 p2 nSteps |
+	"Should be tweaked with graduated motion"
+	owner owner addMorphFront: aCard.
+	p1 _ aCard position.
+	p2 _ aCell position.
+	nSteps _ 10.
+	1 to: nSteps-1 do: "Note final step happens with actual drop"
+		[:i | aCard position: ((p2*i) + (p1*(nSteps-i))) // nSteps.
+		self world displayWorld].
+	aCell acceptDroppingMorph: aCard event: nil
