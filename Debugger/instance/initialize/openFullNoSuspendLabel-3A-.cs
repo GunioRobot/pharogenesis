ad5bfec9d1c10acb5c7e535963fdc2ddd7c59816
ls <unknown>
2000-03-05 14:20:00 +0000

    no message

--- Debugger/instance/initialize/openFullNoSuspendLabel-3A-.cs
+++ Debugger/instance/initialize/openFullNoSuspendLabel-3A-.cs
@@ -2,6 +2,9 @@ openFullNoSuspendLabel: aString
 	"Create and schedule a full debugger with the given label. Do not terminate the current active process."
 
 	| topView |
+	Smalltalk isMorphic ifTrue:
+		[self openFullMorphicLabel: aString.
+		^ Project current spawnNewProcessIfThisIsUI: interruptedProcess].
 	topView _ self buildMVCDebuggerViewLabel: aString minSize: 300@200.
 	topView controller openNoTerminate.
 	^ topView
