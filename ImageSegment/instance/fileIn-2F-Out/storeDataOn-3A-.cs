ab7d0887968857fd5e460c70704a2c0272b0fbd2
tk <unknown>
2000-03-06 17:22:00 +0000

    no message

---
+++ ImageSegment/instance/fileIn-2F-Out/storeDataOn-3A-.cs
@@ -0,0 +1,26 @@
+storeDataOn: aDataStream
+	"Don't wrote the array of Roots.  Also remember the structures of the classes of objects inside the segment."
+
+	| tempRoots tempOutP list |
+	state = #activeCopy ifFalse: [self error: 'wrong state'].
+		"real state is activeCopy, but we changed it will be right when coming in"
+	tempRoots _ arrayOfRoots.
+	tempOutP _ outPointers.
+	outPointers _ outPointers clone.
+	self prepareToBeSaved.
+	arrayOfRoots _ nil.
+	state _ #imported.
+	super storeDataOn: aDataStream.		"record my inst vars"
+	arrayOfRoots _ tempRoots.
+	outPointers _ tempOutP.
+	state _ #activeCopy.
+	aDataStream references at: #AnImageSegment put: false.
+		"flag that there is one in this write"
+
+	"Find the receivers of blocks in the segment.  Need to get the structure of their classes into structures.  Put the receivers into references."
+	(aDataStream byteStream isKindOf: DummyStream) ifTrue: [
+		list _ Set new.
+		arrayOfRoots do: [:ea | 
+			(ea class == BlockContext) | (ea class == MethodContext) ifTrue: [ 
+				list add: ea receiver class ]].
+		aDataStream references at: #BlockReceiverClasses put: list].
