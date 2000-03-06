e44569ba0adbafc51bb59d9e8d3847b93ff4a35a
tk <unknown>
2000-03-06 13:09:00 +0000

    no message

---
+++ ImageSegment/instance/read-2F-write segment/copyFromRootsForExport-3A-.cs
@@ -0,0 +1,28 @@
+copyFromRootsForExport: rootArray 
+	"Add to roots:  Any methods pointed to from the outside by blocks."
+	| newRoots list segSize symbolHolder |
+	arrayOfRoots _ rootArray.
+	Smalltalk forgetDoIts.  
+	symbolHolder _ Symbol allInstances.	"Hold onto Symbols with strong pointers, 
+		so they will be in outPointers"
+	(newRoots _ self rootsIncludingPlayers) ifNotNil: [
+		arrayOfRoots _ newRoots].		"world, presenter, and all Player classes"
+	"Creation of the segment happens here"
+	self copyFromRoots: arrayOfRoots sizeHint: 0.
+	segSize _ segment size.
+	[(newRoots _ self rootsIncludingBlockMethods) == nil] whileFalse: [
+		arrayOfRoots _ newRoots.
+		self copyFromRoots: arrayOfRoots sizeHint: segSize].
+		"with methods pointed at from outside"
+	[(newRoots _ self rootsIncludingBlocks) == nil] whileFalse: [
+		arrayOfRoots _ newRoots.
+		self copyFromRoots: arrayOfRoots sizeHint: segSize].
+		"with methods, blocks from outPointers"
+	"classes of receivers of blocks"
+	list _ self compactClassesArray.
+	outPointers _ outPointers, ((list select: [:cls | cls ~~ nil]), (Array with: 1717 with: list)).
+	"Zap sender of a homeContext. Can't send live stacks out."
+	1 to: outPointers size do: [:ii | 
+		(outPointers at: ii) class == BlockContext ifTrue: [outPointers at: ii put: nil].
+		(outPointers at: ii) class == MethodContext ifTrue: [outPointers at: ii put: nil]].
+	symbolHolder.
