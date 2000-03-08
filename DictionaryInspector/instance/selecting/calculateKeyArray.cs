c88fcbb27855b0187d6726146f75e8ec5da252dc
di <unknown>
2000-03-08 09:14:00 +0000

    no message

--- DictionaryInspector/instance/selecting/calculateKeyArray.cs
+++ DictionaryInspector/instance/selecting/calculateKeyArray.cs
@@ -1,16 +1,5 @@
 calculateKeyArray
 	"Recalculate the KeyArray from the object being inspected"
 
-	| sortedKeys |
-	sortedKeys _ SortedCollection new: object size.
-	sortedKeys sortBlock: [ :x :y |
-		(((x isKindOf: String) & (y isKindOf: String))
-		or: [(x isKindOf: Number) & (y isKindOf: Number)])
-			ifTrue: [ x < y]
-			ifFalse: [ (x class = y class)
-				ifTrue: [ x printString < y printString]
-				ifFalse: [ x class name < y class name ] ] ].
-	object keysDo:
-		[ :aKey | sortedKeys add: aKey. ].
-	keyArray _ sortedKeys asArray.
+	keyArray _ object keysSortedSafely asArray.
 	selectionIndex _ 0.
