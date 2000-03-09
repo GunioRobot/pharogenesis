aa092a30f59992b86df1bb03ea1659e33eb28796
dew <unknown>
2000-03-09 00:13:00 +0000

    no message

---
+++ Workspace/instance/as yet unclassified/saveContentsInFile.cs
@@ -0,0 +1,9 @@
+saveContentsInFile
+	"A bit of a hack to pass along this message to the controller or morph.  (Possibly this Workspace menu item could be deleted, since it's now in the text menu.)"
+	| textMorph textView |
+
+	textMorph _ self dependents detect: [:dep | dep isKindOf: PluggableTextMorph] ifNone: [nil].
+	textMorph notNil ifTrue: [^ textMorph saveContentsInFile].
+
+	textView _ self dependents detect: [:dep | dep isKindOf: PluggableTextView] ifNone: [nil].
+	textView notNil ifTrue: [^ textView controller saveContentsInFile].
