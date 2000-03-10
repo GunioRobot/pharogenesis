9cf5307094f620949ef86546acd161f0196fc171
sw <unknown>
2000-03-10 13:43:00 +0000

    no message

---
+++ Slider/instance/object fileIn/convertbosfcebbmsosvsss0-3A-bosfcebbmsosvsssd0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcebbmsosvsss0: varDict bosfcebbmsosvsssd0: smartRefStrm
+	"These variables are automatically stored into the new instance ('slider' 'value' 'setValueSelector' 'sliderShadow' 'sliderColor' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('descending' )  If a non-nil value is needed, please assign it."
