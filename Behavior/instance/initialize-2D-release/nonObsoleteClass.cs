0a239ea00bf2729694f1406f37e071120cf50f8e
di <unknown>
2000-03-10 08:40:00 +0000

    no message

---
+++ Behavior/instance/initialize-2D-release/nonObsoleteClass.cs
@@ -0,0 +1,8 @@
+nonObsoleteClass
+	"Attempt to find and return the current version of this obsolete class"
+
+	| obsName |
+	obsName _ self name.
+	[obsName beginsWith: 'AnObsolete']
+		whileTrue: [obsName _ obsName copyFrom: 'AnObsolete' size + 1 to: obsName size].
+	^ Smalltalk at: obsName asSymbol
