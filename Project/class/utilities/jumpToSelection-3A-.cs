8cb6b2d2542ec8d507e9fd1fa979b392e28bdc87
tk <unknown>
2000-03-10 21:09:00 +0000

    no message

---
+++ Project/class/utilities/jumpToSelection-3A-.cs
@@ -0,0 +1,17 @@
+jumpToSelection: selection
+	"Enter the project corresponding to this menu selection."
+	"Project jumpToProject"
+	| nBack prev pr |
+	selection ifNil: [^ self].
+	(selection beginsWith: '%back') ifTrue:
+		[nBack _ (selection copyFrom: 6 to: selection size) asNumber.
+		prev _ CurrentProject previousProject.
+		1 to: nBack-1 do:
+			[:i | prev ifNotNil: [prev _ prev previousProject]].
+		prev ifNotNil: [prev enter: true revert: false saveForRevert: false]].
+	selection = #parent ifTrue: [
+		CurrentProject parent enter: false revert: false saveForRevert: false.
+		^ self].
+	(pr _ Project named: selection) ifNil: [^ self].
+	pr enter: false revert: false saveForRevert: false
+
