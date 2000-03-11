605ce9ae829ed516a1b69709d1cbcdffbb2b1003
sma <unknown>
2000-03-11 14:59:00 +0000

    no message

--- MouseMenuController/instance/menu messages/yellowButtonActivity.cs
+++ MouseMenuController/instance/menu messages/yellowButtonActivity.cs
@@ -1,12 +1,6 @@
 yellowButtonActivity
-	"Determine which item in the yellow button pop-up menu is selected. If 
-	one is selected, then send the corresponding message to the object 
-	designated as the menu message receiver.
-	1/18/96 sw: added the escape to shifted variant
-	1/24/96 sw: separate methods for shifted and unshifted variant.
-	1/25/96 sw: speeded up by passing shifted menu along"
+	"This normally opens a popup menu. Determine the selected
+	item and, if one is selected, then send the corresponding message
+	to either the model or the receiver."
 
-	| shiftMenu |
-	^ (Sensor leftShiftDown and: [(shiftMenu _ self shiftedYellowButtonMenu) notNil])
-		ifTrue: [self shiftedYellowButtonActivity: shiftMenu]
-		ifFalse:	[self unshiftedYellowButtonActivity]
+	^ self pluggableYellowButtonActivity: sensor leftShiftDown
