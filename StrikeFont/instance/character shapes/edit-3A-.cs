32815f50529a0d8d00117eba6b429e423db0e7ed
sma <unknown>
2000-03-11 11:28:00 +0000

    no message

--- StrikeFont/instance/character shapes/edit-3A-.cs
+++ StrikeFont/instance/character shapes/edit-3A-.cs
@@ -3,13 +3,12 @@ edit: character		"(TextStyle default fontAt: 1) edit: $_"
 	(in the option menu of the bit editor) if you want this work.
 	Accepted edits will not take effect in the font until you leave or close the bit editor.
 	Also note that unaccepted edits will be lost when you leave or close."
-	| charForm smallRect editRect scaleFactor bitEditor savedForm r |
+	| charForm editRect scaleFactor bitEditor savedForm r |
 	charForm _ self characterFormAt: character.
 	editRect _ BitEditor locateMagnifiedView: charForm
 	                                        scale: (scaleFactor _ 8@8).
 	bitEditor _ BitEditor bitEdit: charForm at: editRect topLeft
 			scale: scaleFactor remoteView: nil.
-	bitEditor controller blueButtonMenu: nil blueButtonMessages: nil.
 	savedForm _ Form fromDisplay: (r _ bitEditor displayBox expandBy: (0@23 corner: 0@0)).
 	bitEditor controller startUp.
 	bitEditor release.
