ae06b32ffa9bc185a71d596995b1dfe7ffad8d56
sma <unknown>
2000-03-11 15:06:00 +0000

    no message

--- FormEditor/class/class initialization/initialize.cs
+++ FormEditor/class/class initialization/initialize.cs
@@ -2,10 +2,13 @@ initialize
 
 	FlashCursor _ false.
 	self setKeyboardMap.
-	YellowButtonMenu _ PopUpMenu labels: 'accept
+	YellowButtonMenu _ SelectionMenu 
+		labels:
+'accept
 cancel
 edit
-file out' lines: #(2).
-	YellowButtonMessages _ #(accept cancel edit fileOut)
+file out'
+		lines: #(2)
+		selections: #(accept cancel edit fileOut).
 
 	"FormEditor initialize"
