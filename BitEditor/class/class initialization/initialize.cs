a9f4dc69edc51d430471b5e4820504c37b23486c
sma <unknown>
2000-03-11 14:48:00 +0000

    no message

--- BitEditor/class/class initialization/initialize.cs
+++ BitEditor/class/class initialization/initialize.cs
@@ -2,11 +2,13 @@ initialize
 	"The Bit Editor is the only controller to override the use of the blue
 	button with a different pop-up menu. Initialize this menu."
 
-	YellowButtonMenu _ PopUpMenu labels:
+	YellowButtonMenu _ SelectionMenu
+		labels:
 'cancel
 accept
 file out
-test' lines: #(2 3).
-	YellowButtonMessages _ #(cancel accept fileOut test)	
+test'
+		lines: #(2 3)
+		selections: #(cancel accept fileOut test)
 
 	"BitEditor initialize"
