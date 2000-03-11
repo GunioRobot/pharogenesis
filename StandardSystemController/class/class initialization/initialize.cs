a9e2f3ae6ea5fa9b4c86bc7bae34457c7db0f9fb
sma <unknown>
2000-03-11 11:57:00 +0000

    no message

--- StandardSystemController/class/class initialization/initialize.cs
+++ StandardSystemController/class/class initialization/initialize.cs
@@ -1,8 +1,9 @@
-initialize   "StandardSystemController initialize"
-	"Set up the menus for standard windows.  
-	 6/6/96 sw: added fullScreen"
+initialize
+	"StandardSystemController initialize"
 
-	ScheduledBlueButtonMenu _ PopUpMenu labels: 'edit label
+	ScheduledBlueButtonMenu _ SelectionMenu
+		labels:
+'edit label
 choose color...
 two-tone/full color
 move
@@ -10,13 +11,9 @@ frame
 full screen
 collapse
 close'
-	lines: #(3 7).
-	ScheduledBlueButtonMessages _ #(label chooseColor toggleTwoTone move reframe fullScreen collapse close).
-"
-StandardSystemController initialize.
-ScheduledControllers scheduledWindowControllers
-		do: [:c | c initializeBlueButtonMenu]
-"
+	lines: #(3 7)
+	selections: #(label chooseColor toggleTwoTone move reframe fullScreen collapse close).
+
 	VBorderCursor _ Cursor extent: 16@16 fromArray: #(
 		2r1010000000000000
 		2r1010000000000000
