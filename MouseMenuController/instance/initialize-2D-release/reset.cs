716db2763ad36350befd064a2c8eb39e55bffffd
sma <unknown>
2000-03-11 15:54:00 +0000

    no message

--- MouseMenuController/instance/initialize-2D-release/reset.cs
+++ MouseMenuController/instance/initialize-2D-release/reset.cs
@@ -2,8 +2,4 @@ reset
 	"Eliminate references to all mouse button menus."
 
 	redButtonMenu _ nil.
-	redButtonMessages _ nil.
-	yellowButtonMenu _ nil.
-	yellowButtonMessages _ nil.
-	blueButtonMenu _ nil.
-	blueButtonMessages _ nil
+	redButtonMessages _ nil
