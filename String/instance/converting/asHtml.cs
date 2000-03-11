125a595e01b47e3e115fc6591b435eec9eaa9c49
sma <unknown>
2000-03-11 17:25:00 +0000

    no message

--- String/instance/converting/asHtml.cs
+++ String/instance/converting/asHtml.cs
@@ -1,15 +1,19 @@
 asHtml
-	"Do the basic character conversion for HTML.  Leave all original return and tabs in place, so can conver back by simply removing bracked things.
-	4/4/96 tk"
+	"Do the basic character conversion for HTML.  Leave all original return 
+	and tabs in place, so can conver back by simply removing bracked 
+	things. 4/4/96 tk"
 	| temp |
 	temp _ self copyReplaceAll: '&' with: '&amp;'.
-	temp _ temp copyReplaceAll: '<' with: '&lt;'.
-	temp _ temp copyReplaceAll: '>' with: '&gt;'.
-
-	temp _ temp copyReplaceAll: '	' 
-			with: '	<IMG SRC="tab.gif" ALT="    ">'.
+	HtmlEntities keysAndValuesDo:
+		[:entity :char |
+		char = $& ifFalse:
+			[temp _ temp copyReplaceAll: char asString with: '&' , entity , ';']].
+	temp _ temp copyReplaceAll: '	' with: '	<IMG SRC="tab.gif" ALT="    ">'.
 	temp _ temp copyReplaceAll: '
-' 
-			with: '
+' with: '
 <BR>'.
 	^ temp
+
+"
+	'A<&>B' asHtml
+"
