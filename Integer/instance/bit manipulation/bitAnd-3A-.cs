02b3832c4bcedcfcdb6bdd358df1dc1000aa5311
sr <unknown>
2000-03-13 17:47:00 +0000

    no message

--- Integer/instance/bit manipulation/bitAnd-3A-.cs
+++ Integer/instance/bit manipulation/bitAnd-3A-.cs
@@ -1,8 +1,10 @@
 bitAnd: n 
-	"Answer an Integer whose bits are the logical AND of the receiver's bits 
+	"Answer an Integer whose bits are the logical AND of the receiver's bits  
 	and those of the argument, n."
 	| norm |
+	<primitive: 'primDigitBitAnd' module:'LargeIntegers'>
 	norm _ n normalize.
-	^ self digitLogic: norm
+	^ self
+		digitLogic: norm
 		op: #bitAnd:
 		length: (self digitLength max: norm digitLength)
