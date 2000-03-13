f928c7c138b8b2e517cf290f874b2be9a2c77838
acg <unknown>
2000-03-13 08:44:00 +0000

    no message

--- PluggableButtonController/instance/basic control sequence/controlTerminate.cs
+++ PluggableButtonController/instance/basic control sequence/controlTerminate.cs
@@ -1,6 +1,8 @@
 controlTerminate 
 	"Reverse the feedback displayed by controlInitialize, if any. Perform the button action if necessary."
 
-	view triggerOnMouseDown ifFalse: [
-		view toggleMouseOverFeedback.
-		self viewHasCursor ifTrue: [view performAction]].
+	view ifNotNil:
+		[view triggerOnMouseDown
+			ifFalse:
+				[shownAsComplemented ifTrue: [view toggleMouseOverFeedback].
+				self viewHasCursor ifTrue: [view performAction]]]
