58e6645f0ee984fd69771fbf19e46130541f4dbd
sr <unknown>
2000-03-13 17:47:00 +0000

    no message

--- Integer/instance/bit manipulation/bitOr-3A-.cs
+++ Integer/instance/bit manipulation/bitOr-3A-.cs
@@ -1,8 +1,10 @@
 bitOr: n 
-	"Answer an Integer whose bits are the logical OR of the receiver's bits 
+	"Answer an Integer whose bits are the logical OR of the receiver's bits  
 	and those of the argument, n."
 	| norm |
+	<primitive: 'primDigitBitOr' module:'LargeIntegers'>
 	norm _ n normalize.
-	^self digitLogic: norm
+	^ self
+		digitLogic: norm
 		op: #bitOr:
 		length: (self digitLength max: norm digitLength)
