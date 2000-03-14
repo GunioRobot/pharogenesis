c9a1f22ca8e72789e0670b40397513192dda4f63
tk <unknown>
2000-03-14 23:05:00 +0000

    no message

---
+++ SystemWindow/instance/object fileIn/convertbosfcebbmsolslccappcfimmlu0-3A-bosfcebbmsolslccappcfimmlua0-3A-.cs
@@ -0,0 +1,7 @@
+convertbosfcebbmsolslccappcfimmlu0: varDict bosfcebbmsolslccappcfimmlua0: smartRefStrm
+	"These variables are automatically stored into the new instance ('labelString' 'stripes' 'label' 'closeBox' 'collapseBox' 'activeOnlyOnTop' 'paneMorphs' 'paneRects' 'collapsedFrame' 'fullFrame' 'isCollapsed' 'menuBox' 'mustNotClose' 'labelWidgetAllowance' 'updatablePanes' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('allowReframeHandles' )  If a non-nil value is needed, please assign it."
+
+	allowReframeHandles := true.
