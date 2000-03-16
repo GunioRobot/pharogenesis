fc0f11cf9f3fe8533c36ac67c149941bcb922d68
bf <unknown>
2000-03-16 18:57:00 +0000

    no message

--- MouseOverMorph/instance/event handling/mouseMove-3A-.cs
+++ MouseOverMorph/instance/event handling/mouseMove-3A-.cs
@@ -2,10 +2,10 @@ mouseMove: event
 	"Relay a mouseMove event to my model."
 
 	mouseMoveSelector ifNotNil:
-		[mouseMoveSelector argumentCount = 0
+		[mouseMoveSelector numArgs = 0
 			ifTrue: [^ model perform: mouseMoveSelector].
-		mouseMoveSelector argumentCount = 1
+		mouseMoveSelector numArgs = 1
 			ifTrue: [^ model perform: mouseMoveSelector with: event cursorPoint].
-		mouseMoveSelector argumentCount = 2
+		mouseMoveSelector numArgs = 2
 			ifTrue: [^ model perform: mouseMoveSelector with: event cursorPoint with: event].
 		^ self error: 'mouseMoveSelector must take 0, 1, or 2 arguments']
