8c33f8650fbcb45c97944126a85d6e259677a501
bf <unknown>
2000-03-16 18:25:00 +0000

    no message

--- ParagraphEditor/instance/editing keys/copyHiddenInfo.cs
+++ ParagraphEditor/instance/editing keys/copyHiddenInfo.cs
@@ -1,18 +1,16 @@
 copyHiddenInfo
-	"In TextLinks, TextDoits, TextColor, and TextURLs, there is hidden info.  Copy that to the clipboard.  You can paste it and see what it is.  Usually enclosed in <>."
+	"In TextLinks, TextDoits, TextColor, and TextURLs, there is hidden
+info.  Copy that to the clipboard.  You can paste it and see what it is.
+Usually enclosed in <>."
 
 	| attrList |
-	attrList _ paragraph text attributesAt: (startBlock stringIndex + stopBlock stringIndex)//2.
+	attrList _ paragraph text attributesAt: (startBlock stringIndex +
+stopBlock stringIndex)//2 forStyle: paragraph textStyle.
 	attrList do: [:attr |
-		attr class == TextLink ifTrue: [
-			^ self clipboardTextPut: ('<', attr info, '>') asText].
-		attr class == TextURL ifTrue: [
-			^ self clipboardTextPut: ('<', attr info, '>') asText].
-		attr class == TextAction ifTrue: [
-			^ self clipboardTextPut: ('<', attr info, '>') asText].
-		].
+		(attr isKindOf: TextAction) ifTrue:
+			[^ self clipboardTextPut: ('<', attr info, '>') asText]].
 	"If none of the above"
 	attrList do: [:attr |
-		attr class == TextColor ifTrue: [
-			^ self clipboardTextPut: attr color printString asText]].
+		attr class == TextColor ifTrue:
+			[^ self clipboardTextPut: attr color printString asText]].
 	^ self clipboardTextPut: '[No hidden info]' asText
