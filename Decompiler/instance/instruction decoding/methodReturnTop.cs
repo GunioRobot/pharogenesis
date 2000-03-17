32d27e2c753de7dd91ec33ceb2801044cee600d5
th <unknown>
2000-03-17 20:48:00 +0000

    no message

--- Decompiler/instance/instruction decoding/methodReturnTop.cs
+++ Decompiler/instance/instruction decoding/methodReturnTop.cs
@@ -1,6 +1,6 @@
 methodReturnTop
 	| last |
-	last _ stack removeLast.
+	last _ stack removeLast "test test" asReturnNode.
 	stack size > blockStackBase  "get effect of elided pop before return"
 		ifTrue: [statements addLast: stack removeLast].
 	exit _ method size + 1.
