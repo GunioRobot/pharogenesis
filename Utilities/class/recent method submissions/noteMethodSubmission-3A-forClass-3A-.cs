5dda3f151f3234715fb701f801fcadf2f04a096a
di <unknown>
2000-03-19 08:28:00 +0000

    no message

--- Utilities/class/recent method submissions/noteMethodSubmission-3A-forClass-3A-.cs
+++ Utilities/class/recent method submissions/noteMethodSubmission-3A-forClass-3A-.cs
@@ -1,10 +1,11 @@
-noteMethodSubmission: selectorName forClass: className
-	| aSubmission |
-	aSubmission _ className asString, ' ', selectorName.
-	(self recentMethodSubmissions includes: aSubmission)
-		ifTrue:
-			[RecentSubmissions remove: aSubmission]
-		ifFalse:
-			[(RecentSubmissions size >= self numberOfRecentSubmissionsToStore) 
-				ifTrue: [RecentSubmissions removeFirst]].
-	RecentSubmissions addLast: aSubmission
+noteMethodSubmission: selectorName forClass: class
+	| submission className |
+	class wantsChangeSetLogging ifFalse: [^ self].
+	className _ class name.
+	self purgeRecentSubmissionsOfMissingMethods.
+	submission _ className asString, ' ', selectorName.
+	(self recentMethodSubmissions includes: submission)
+		ifTrue: [RecentSubmissions remove: submission]
+		ifFalse: [(RecentSubmissions size >= self numberOfRecentSubmissionsToStore) 
+					ifTrue: [RecentSubmissions removeFirst]].
+	RecentSubmissions addLast: submission
