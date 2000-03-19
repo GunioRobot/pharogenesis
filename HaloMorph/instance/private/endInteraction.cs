117d812079f48b951df03539f1e56730caaa6fc3
di <unknown>
2000-03-19 13:42:00 +0000

    no message

--- HaloMorph/instance/private/endInteraction.cs
+++ HaloMorph/instance/private/endInteraction.cs
@@ -3,8 +3,11 @@ endInteraction
 	| m |
 	(target isInWorld not or: [owner == nil]) ifTrue: [^ self].
 	[target isFlexMorph and: [target hasNoScaleOrRotation]]
-		whileTrue: [
-			m _ target firstSubmorph.
+		whileTrue:
+			[m _ target firstSubmorph.
 			target removeFlexShell.
 			target _ m].
-	self isInWorld ifTrue: [self addHandles].
+	self isInWorld ifTrue:
+		["make sure handles show in front, even if flex shell added"
+		self comeToFront.
+		self addHandles].
