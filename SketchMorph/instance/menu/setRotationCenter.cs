69b6341b4c23c704731efd91ee42b0903587f96c
di <unknown>
2000-03-19 13:13:00 +0000

    no message

--- SketchMorph/instance/menu/setRotationCenter.cs
+++ SketchMorph/instance/menu/setRotationCenter.cs
@@ -1,13 +1,8 @@
 setRotationCenter
-
-	| oldRotation p oldScale |
-	oldRotation _ rotationDegrees.
-	oldScale _ scalePoint.
-	scalePoint _ 1.0@1.0.
-	self rotationDegrees: 0.0.
+	| p |
 	self world displayWorld.
 	Cursor crossHair showWhile:
-		[p _ Sensor waitButton - self world viewBox origin].
-	self rotationCenter: p - bounds origin.
-	scalePoint _ oldScale.
-	self rotationDegrees: oldRotation.
+		[p _ Sensor waitButton].
+	Sensor waitNoButton.
+	self setRotationCenterFromGlobalPosition: p.
+
