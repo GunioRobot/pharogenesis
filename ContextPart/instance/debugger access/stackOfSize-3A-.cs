793c7993d5d0d1a15fff019f1ed6108262e56d98
tfei <unknown>
2000-03-20 00:51:00 +0000

    no message

--- ContextPart/instance/debugger access/stackOfSize-3A-.cs
+++ ContextPart/instance/debugger access/stackOfSize-3A-.cs
@@ -2,9 +2,17 @@ stackOfSize: limit
 	"Answer an OrderedCollection of the top 'limit' contexts
 		on the receiver's sender chain."
 
-	| a stack |
+	| a stack cachedStackTop newLimit |
 	stack _ OrderedCollection new.
 	stack addLast: (a _ self).
 	[(a _ a sender) ~~ nil and: [stack size < limit]]
-		whileTrue: [stack addLast: a].
-	^ stack
+		whileTrue:
+			[a hideFromDebugger ifFalse: [stack addLast: a].
+			a cachesStack ifTrue: [cachedStackTop := a cachedStackTop]].
+	^cachedStackTop == nil 
+		ifTrue: [stack]
+		ifFalse:
+			[newLimit := limit - stack size.
+			newLimit > 0
+				ifTrue: [stack addAllLast: (cachedStackTop stackOfSize: newLimit); yourself]
+				ifFalse: [stack]]
