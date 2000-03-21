007e1a9ec7bcb375dc8cbb1e5fa434e1dbc90093
di <unknown>
2000-03-21 09:11:00 +0000

    no message

--- BitBltSimulation/instance/inner loop/copyLoop.cs
+++ BitBltSimulation/instance/inner loop/copyLoop.cs
@@ -1,11 +1,10 @@
 copyLoop
-	| prevWord thisWord skewWord halftoneWord mergeWord hInc y unskew skewMask notSkewMask mergeFnwith |
+	| prevWord thisWord skewWord halftoneWord mergeWord hInc y unskew skewMask notSkewMask mergeFnwith destWord |
 	"This version of the inner loop assumes noSource = false."
 	self inline: false.
 	self var: #mergeFnwith declareC: 'int (*mergeFnwith)(int, int)'.
 	mergeFnwith _ self cCoerce: (opTable at: combinationRule+1) to: 'int (*)(int, int)'.
 	mergeFnwith.  "null ref for compiler"
-
 	hInc _ hDir*4.  "Byte delta"
 	"degenerate skew fixed for Sparc. 10/20/96 ikp"
 	skew == -32
@@ -15,7 +14,7 @@ copyLoop
 				[unskew _ skew+32.
 				skewMask _ AllOnes << (0-skew)]
 			ifFalse:
-				[skew == 0
+				[skew = 0
 					ifTrue:
 						[unskew _ 0.
 						skewMask _ AllOnes]
@@ -25,17 +24,17 @@ copyLoop
 	notSkewMask _ skewMask bitInvert32.
 	noHalftone
 		ifTrue: [halftoneWord _ AllOnes.  halftoneHeight _ 0]
-		ifFalse: [halftoneWord _ interpreterProxy longAt: halftoneBase].
+		ifFalse: [halftoneWord _ self halftoneAt: 0].
+
 	y _ dy.
 	1 to: bbH do: "here is the vertical loop"
 		[ :i |
 		halftoneHeight > 1 ifTrue:  "Otherwise, its always the same"
-			[halftoneWord _ interpreterProxy longAt:
-						(halftoneBase + (y \\ halftoneHeight * 4)).
+			[halftoneWord _ self halftoneAt: y.
 			y _ y + vDir].
 		preload ifTrue:
 			["load the 64-bit shifter"
-			prevWord _ interpreterProxy longAt: sourceIndex.
+			prevWord _ self srcLongAt: sourceIndex.
 			sourceIndex _ sourceIndex + hInc]
 			ifFalse:
 			[prevWord _ 0].
@@ -43,58 +42,71 @@ copyLoop
 	"Note: the horizontal loop has been expanded into three parts for speed:"
 
 			"This first section requires masking of the destination store..."
-			thisWord _ interpreterProxy longAt: sourceIndex.  "pick up next word"
+			destMask _ mask1.
+			thisWord _ self srcLongAt: sourceIndex.  "pick up next word"
+			sourceIndex _ sourceIndex + hInc.
 			skewWord _ ((prevWord bitAnd: notSkewMask) bitShift: unskew)
 							bitOr:  "32-bit rotate"
 						((thisWord bitAnd: skewMask) bitShift: skew).
 			prevWord _ thisWord.
-			sourceIndex _ sourceIndex + hInc.
-			mergeWord _ self mergeFn: (skewWord bitAnd: halftoneWord)
-							with: (interpreterProxy longAt: destIndex).
-			interpreterProxy longAt: destIndex
-				put: ((mask1 bitAnd: mergeWord)
-					bitOr: (mask1 bitInvert32 bitAnd: (interpreterProxy longAt: destIndex))).
+			destWord _ self dstLongAt: destIndex.
+			mergeWord _ self mergeFn: (skewWord bitAnd: halftoneWord) with: destWord.
+			destWord _ (destMask bitAnd: mergeWord) bitOr:
+							(destWord bitAnd: destMask bitInvert32).
+			self dstLongAt: destIndex put: destWord.
 			destIndex _ destIndex + hInc.
 
 		"This central horizontal loop requires no store masking"
+		destMask _ AllOnes.
 combinationRule = 3
-ifTrue: [2 to: nWords-1 do: "Special inner loop for STORE"
+ifTrue: [(skew = 0) & (halftoneWord = AllOnes)
+		ifTrue:  
+		["Very special inner loop for STORE mode with no skew -- just move words"
+		2 to: nWords-1 do: 
+			[ :word |  "Note loop starts with prevWord loaded (due to preload)"
+			self dstLongAt: destIndex put: prevWord.
+			destIndex _ destIndex + hInc.
+			prevWord _ self srcLongAt: sourceIndex.
+			sourceIndex _ sourceIndex + hInc]]
+		ifFalse:
+		["Special inner loop for STORE mode -- no need to call merge"
+		2 to: nWords-1 do: 
 			[ :word |
-			thisWord _ interpreterProxy longAt: sourceIndex.  "pick up next word"
+			thisWord _ self srcLongAt: sourceIndex.
+			sourceIndex _ sourceIndex + hInc.
 			skewWord _ ((prevWord bitAnd: notSkewMask) bitShift: unskew)
 							bitOr:  "32-bit rotate"
 						((thisWord bitAnd: skewMask) bitShift: skew).
 			prevWord _ thisWord.
-			sourceIndex _ sourceIndex + hInc.
-			interpreterProxy longAt: destIndex put: (skewWord bitAnd: halftoneWord).
-			destIndex _ destIndex + hInc]
+			self dstLongAt: destIndex put: (skewWord bitAnd: halftoneWord).
+			destIndex _ destIndex + hInc]]
 ] ifFalse: [2 to: nWords-1 do: "Normal inner loop does merge:"
 			[ :word |
-			thisWord _ interpreterProxy longAt: sourceIndex.  "pick up next word"
+			thisWord _ self srcLongAt: sourceIndex.  "pick up next word"
+			sourceIndex _ sourceIndex + hInc.
 			skewWord _ ((prevWord bitAnd: notSkewMask) bitShift: unskew)
 							bitOr:  "32-bit rotate"
 						((thisWord bitAnd: skewMask) bitShift: skew).
 			prevWord _ thisWord.
-			sourceIndex _ sourceIndex + hInc.
 			mergeWord _ self mergeFn: (skewWord bitAnd: halftoneWord)
-							with: (interpreterProxy longAt: destIndex).
-			interpreterProxy longAt: destIndex put: mergeWord.
+							with: (self dstLongAt: destIndex).
+			self dstLongAt: destIndex put: mergeWord.
 			destIndex _ destIndex + hInc]
 ].
 
 		"This last section, if used, requires masking of the destination store..."
 		nWords > 1 ifTrue:
-			[thisWord _ interpreterProxy longAt: sourceIndex.  "pick up next word"
+			[destMask _ mask2.
+			thisWord _ self srcLongAt: sourceIndex.  "pick up next word"
+			sourceIndex _ sourceIndex + hInc.
 			skewWord _ ((prevWord bitAnd: notSkewMask) bitShift: unskew)
 							bitOr:  "32-bit rotate"
 						((thisWord bitAnd: skewMask) bitShift: skew).
-			prevWord _ thisWord.
-			sourceIndex _ sourceIndex + hInc.
-			mergeWord _ self mergeFn: (skewWord bitAnd: halftoneWord)
-							with: (interpreterProxy longAt: destIndex).
-			interpreterProxy longAt: destIndex
-				put: ((mask2 bitAnd: mergeWord)
-					bitOr: (mask2 bitInvert32 bitAnd: (interpreterProxy longAt: destIndex))).
+			destWord _ self dstLongAt: destIndex.
+			mergeWord _ self mergeFn: (skewWord bitAnd: halftoneWord) with: destWord.
+			destWord _ (destMask bitAnd: mergeWord) bitOr:
+							(destWord bitAnd: destMask bitInvert32).
+			self dstLongAt: destIndex put: destWord.
 			destIndex _ destIndex + hInc].
 
 	sourceIndex _ sourceIndex + sourceDelta.
