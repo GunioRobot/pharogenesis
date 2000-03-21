3e8c1e33f16fc24765c46e064d7a16804f17fc0d
ti <unknown>
2000-03-21 15:11:00 +0000

    no message

---
+++ B3DSceneExplorerMorph/instance/actions/selectNewCamera.cs
@@ -0,0 +1,13 @@
+selectNewCamera
+	| menu sel |
+	((self scene cameras isNil) or: [self scene cameras size = 0]) ifTrue: [
+		(SelectionMenu selections: #('OK'))
+			startUpWithCaption: 'No cameras defined!'.
+		^self].
+	menu _ SelectionMenu
+		selections: self scene cameras keys asArray.
+	sel := menu startUp.
+	sel ifNotNil: [
+		self scene defaultCamera: (self scene cameras at: sel) copy.
+		self updateUpVectorForCamera: self scene defaultCamera.
+		self changed.]
