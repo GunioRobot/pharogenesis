6e4aea6cf03e5600a606b1d6110337d8a2b3d5d0
sw <unknown>
2000-03-22 14:17:00 +0000

    no message

--- Player/instance/costume/costume-3A-.cs
+++ Player/instance/costume/costume-3A-.cs
@@ -2,19 +2,13 @@ costume: aMorph
 	"Make aMorph be the receiver's current costume"
 	| itsBounds |
 	costume == aMorph ifTrue: [^ self].
-	((costume isKindOf: SketchMorph) and: [(aMorph isKindOf: SketchMorph)])
-		ifTrue:
-			[^ costume wearCostume: aMorph].
-
-	self costumeDictionary
-		at: aMorph formalCostumeName
-		put: (aMorph fullCopy costumee: nil).
 	costume ifNotNil:
-		[itsBounds _ costume bounds.
-		costume pasteUpMorph replaceSubmorph: costume topRendererOrSelf by: aMorph.
+		[self rememberCostume: costume renderedMorph.
+		itsBounds _ costume bounds.
+		(costume ownerThatIsA: HandMorph orA: PasteUpMorph) replaceSubmorph: costume topRendererOrSelf by: aMorph.
 		aMorph position: itsBounds origin.
 		aMorph actorState: costume actorState.
 		aMorph setNameTo: costume externalName].
-	aMorph costumee: self.
+	aMorph player: self.
 	costume _ aMorph.
 	aMorph arrangeToStartStepping
