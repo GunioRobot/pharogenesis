b057edeeabc394b268b09e5273de3f7369e47e0f
tk <unknown>
2000-03-22 14:23:00 +0000

    no message

---
+++ DiskProxy/instance/as yet unclassified/printOn-3A-.cs
@@ -0,0 +1,7 @@
+printOn: aStream
+	"Try to report the name of the project"
+
+	globalObjectName == #Project ifFalse: [^ super printOn: aStream].
+	constructorArgs size > 0 ifFalse: [^ super printOn: aStream].
+	constructorArgs first class == String ifFalse: [^ super printOn: aStream].
+	aStream nextPutAll: constructorArgs first, ' (on server)'
