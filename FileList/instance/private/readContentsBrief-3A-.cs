78540edff3e5894371361dbc766128bdd0535c11
tk <unknown>
2000-03-23 16:21:00 +0000

    no message

--- FileList/instance/private/readContentsBrief-3A-.cs
+++ FileList/instance/private/readContentsBrief-3A-.cs
@@ -1,21 +1,26 @@
 readContentsBrief: brevityFlag
-	"Read the contents of the receiver's selected file, unless it is too long, in which case show just the first and last 1000 characters. Don't create a file if it doesn't already exist."
+	"Read the contents of the receiver's selected file, unless it is too long, in which case show just the first 5000 characters. Don't create a file if it doesn't already exist."
+	| f fileSize first5000 |
 
-	| f fileSize first2000 |
-	listIndex = 0 ifTrue: [^ self defaultContents].
+	brevityFlag ifTrue: [
+		(directory isKindOf: ServerDirectory) ifTrue: [^ self readServerBrief]].
 	f _ directory oldFileOrNoneNamed: self fullName.
 	f ifNil: [^ 'For some reason, this file cannot be read'].
-	(brevityFlag not or: [(fileSize _ f size) <= 30000])
-		ifTrue: [^ f contentsOfEntireFile].
+	(brevityFlag not or: [(fileSize _ f size) <= 100000]) ifTrue:
+		[contents _ f contentsOfEntireFile.
+		brevityState _ #fullFile.   "don't change till actually read"
+		^ contents].
 
 	"if brevityFlag is true, don't display long files when first selected"
-	first2000 _ f next: 2000.
+	first5000 _ f next: 5000.
 	f close.
-	^ 'File ''', fileName, ''' is ', fileSize printString, ' bytes long.
+	contents _ 'File ''', fileName, ''' is ', fileSize printString, ' bytes long.
 You may use the ''get'' command to read the entire file.
 
-Here are the first 2000 characters...
+Here are the first 5000 characters...
 ------------------------------------------
-', first2000 , '
+', first5000 , '
 ------------------------------------------
-... end of the first 2000 characters.'
+... end of the first 5000 characters.'.
+	brevityState _ #briefFile.   "don't change till actually read"
+	^ contents.
