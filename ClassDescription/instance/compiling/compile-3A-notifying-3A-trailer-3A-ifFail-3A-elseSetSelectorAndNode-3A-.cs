8a7c3940f8d25515bd198a2dc272a0462a3d1323
di <unknown>
2000-03-23 20:47:00 +0000

    no message

--- ClassDescription/instance/compiling/compile-3A-notifying-3A-trailer-3A-ifFail-3A-elseSetSelectorAndNode-3A-.cs
+++ ClassDescription/instance/compiling/compile-3A-notifying-3A-trailer-3A-ifFail-3A-elseSetSelectorAndNode-3A-.cs
@@ -1,11 +1,11 @@
-compile: code notifying: requestor trailer: bytes
+compile: code notifying: requestor trailer: bytes 
 		ifFail: failBlock
 		elseSetSelectorAndNode: selAndNodeBlock
 	"Intercept this message in order to remember system changes.
 	 5/15/96 sw: modified so that if the class does not wish its methods logged in the changes file, then they also won't be accumulated in the current change set.
 	7/12/96 sw: use wantsChangeSetLogging to determine whether to put in change set"
 
-	| methodNode selector method |
+	| methodNode selector newMethod priorMethodOrNil |
 	methodNode _ self compilerClass new
 				compile: code
 				in: self
@@ -13,11 +13,15 @@ compile: code notifying: requestor trailer: bytes
 				ifFail: failBlock.
 	selector _ methodNode selector.
 	selAndNodeBlock value: selector value: methodNode.
-	self wantsChangeSetLogging ifTrue:
-		[(methodDict includesKey: selector)
-			ifTrue: [Smalltalk changes changeSelector: selector class: self]
-			ifFalse: [Smalltalk changes addSelector: selector class: self]].
+	requestor ifNotNil:
+		["Note this change for recent submissions list"
+		Utilities noteMethodSubmission: selector forClass: self].
 	methodNode encoder requestor: requestor.  "Why was this not preserved?"
-	method _ methodNode generate: bytes.
-	self addSelector: selector withMethod: method.
-	^ method
+	newMethod _ methodNode generate: bytes.
+	priorMethodOrNil _ (methodDict includesKey: selector)
+		ifTrue: [self compiledMethodAt: selector]
+		ifFalse: [nil].
+	Smalltalk changes noteNewMethod: newMethod forClass: self
+		selector: selector priorMethod: priorMethodOrNil.
+	self addSelector: selector withMethod: newMethod.
+	^ newMethod
