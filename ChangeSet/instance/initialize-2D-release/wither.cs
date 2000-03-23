0f762fc3f2525ccfa51689027d2fad614822e838
di <unknown>
2000-03-23 12:14:00 +0000

    no message

--- ChangeSet/instance/initialize-2D-release/wither.cs
+++ ChangeSet/instance/initialize-2D-release/wither.cs
@@ -1,8 +1,5 @@
 wither
 	"The receiver is to be clobbered.  Clear it out.  2/7/96 sw"
 
-	classChanges _ Dictionary new.
-	methodChanges _ Dictionary new.
-	classRemoves _ Set new.
-	methodRemoves _ Dictionary new.
+	self clear.
 	name _ nil
