dad8d364e6f26800f1b3742dd207e2df351cc443
ar <unknown>
2000-03-23 14:35:00 +0000

    no message

---
+++ PaintBoxMorph/instance/object fileIn/convertbosfceiatctccccsrscb0-3A-bosfceiatctccccsrscbf0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfceiatctccccsrscb0: varDict bosfceiatctccccsrscbf0: smartRefStrm
+	"These variables are automatically stored into the new instance ('action' 'tool' 'currentCursor' 'thumbnail' 'currentColor' 'currentBrush' 'colorMemory' 'colorPatch' 'stampHolder' 'rotationTabForm' 'scaleTabForm' 'colorMemoryThin' 'brushes' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('focusMorph' )  If a non-nil value is needed, please assign it."
