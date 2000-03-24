01920a1dd7ef0436ace11bf0946ae5ae414860a4
laza <unknown>
2000-03-24 13:52:00 +0000

    no message

---
+++ Preferences/class/halos/classicHaloSpecs.cs
@@ -0,0 +1,26 @@
+classicHaloSpecs
+	"Non-iconic halos with traditional placements"
+
+	"Preferences installClassicHaloSpecs"
+	"Preferences resetHaloSpecifications"  "  <-  will result in the standard default halos being reinstalled"
+	"NB: listed below in clockwise order"
+
+		^ #(
+	"  	selector				horiz		vert			color info						icon key
+		---------				------		-----------		-------------------------------		---------------"
+	(addMenuHandle:		left			top				(red)							none)
+	(addDismissHandle:		leftCenter	top				(red		muchLighter)			'Halo-Dismiss')
+	(addGrabHandle:			center		top				(black)							none)
+	(addDragHandle:			rightCenter	top				(brown)							none)
+	(addDupHandle:			right		top				(green)							none)	
+	(addDebugHandle:		right		topCenter		(blue	veryMuchLighter)		none)
+	(addPoohHandle:			right		center			(white)							none)
+	(addPaintBgdHandle:		right		center			(lightGray)						none)
+	(addRepaintHandle:		right		center			(lightGray)						none)
+	(addGrowHandle:		right		bottom			(yellow)						none)
+	(addScaleHandle:		right		bottom			(lightOrange)					none)
+	(addFontEmphHandle:	rightCenter	bottom			(lightBrown darker)				none)
+	(addFontStyleHandle:		center		bottom			(lightRed)						none)
+	(addFontSizeHandle:		leftCenter	bottom			(lightGreen)						none)
+	(addRotateHandle:		left			bottom			(blue)							none))
+
