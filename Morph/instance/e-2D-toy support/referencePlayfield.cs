9492d971c211c86e4114df8d0d046f7756e3c0fe
sma <unknown>
2000-03-24 11:48:00 +0000

    no message

--- Morph/instance/e-2D-toy support/referencePlayfield.cs
+++ Morph/instance/e-2D-toy support/referencePlayfield.cs
@@ -1,8 +1,11 @@
 referencePlayfield
-	| pf |
-	owner ifNil: [self halt: 'no owner for morph'].
-	owner isPlayfieldLike ifTrue: [^ owner].
-	(owner isKindOf: HandMorph) ifTrue:
-		[((pf _ owner formerOwner) ~~ nil and: [pf isPlayfieldLike]) ifTrue: [^ owner formerOwner]].
+	| former |
+	owner ifNotNil:
+		[owner isPlayfieldLike ifTrue: [^ owner].
+		(owner isHandMorph and: [(former _ owner formerOwner) notNil])
+			ifTrue:
+				[^ former isPlayfieldLike 
+					ifTrue: [former]
+					ifFalse: [former referencePlayfield]]].
 	self isInWorld ifFalse: [^ nil].
 	^ self world submorphNamed: 'playfield'
