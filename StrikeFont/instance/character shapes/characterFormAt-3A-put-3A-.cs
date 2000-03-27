e26f9f20f4220ca476073eb04a0f09677cc1e94e
di <unknown>
2000-03-27 16:15:00 +0000

    no message

--- StrikeFont/instance/character shapes/characterFormAt-3A-put-3A-.cs
+++ StrikeFont/instance/character shapes/characterFormAt-3A-put-3A-.cs
@@ -2,6 +2,13 @@ characterFormAt: character put: characterForm
 	"Copy characterForm over the glyph for the argument, character."
 	| ascii leftX rightX widthDif newGlyphs |
 	ascii _ character asciiValue.
+	ascii < minAscii ifTrue: [^ self error: 'Cant store characters below min ascii'].
+	ascii > maxAscii ifTrue:
+		[(self confirm:
+'This font does not accomodate ascii values higher than ' , maxAscii printString , '.
+Do you wish to extend it permanently to handle values up to ' , ascii printString)
+			ifTrue: [self extendMaxAsciiTo: ascii]
+			ifFalse: [^ self error: 'No change made']].
 	leftX _ xTable at: ascii + 1.
 	rightX _ xTable at: ascii + 2.
 	widthDif _ characterForm width - (rightX - leftX).
@@ -16,9 +23,9 @@ characterFormAt: character put: characterForm
 		"adjust further entries on xTable"
 		ascii+2 to: xTable size
 			do: [:i | xTable at: i put: (xTable at: i) + widthDif]].
-	glyphs copy: (leftX @ 0 corner: rightX @ self height)
+	glyphs copy: (leftX @ 0 extent: characterForm extent)
 		from: 0@0 in: characterForm rule: Form over
 "
-| f |  f _ TextStyle default fontAt: 1.
+| f |  f _ TextStyle defaultFont.
 f characterFormAt: $  put: (Form extent: (f widthOf: $ )+10@f height)
 "
