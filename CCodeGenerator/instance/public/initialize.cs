62ff1b873cf5a60ed78fd209b4d768fc4d021fec
RMF <unknown>
2000-03-27 09:53:00 +0000

    no message

--- CCodeGenerator/instance/public/initialize.cs
+++ CCodeGenerator/instance/public/initialize.cs
@@ -1,9 +1,10 @@
 initialize
-
 	translationDict _ Dictionary new.
 	inlineList _ Array new.
-	constants _ Dictionary new.
-	variables _ OrderedCollection new.
-	variableDeclarations _ Dictionary new.
-	methods _ Dictionary new.
+	constants _ Dictionary new: 100.
+	variables _ OrderedCollection new: 100.
+	variableDeclarations _ Dictionary new: 100.
+	methods _ Dictionary new: 500.
 	self initializeCTranslationDictionary.
+	headerFiles _ OrderedCollection new.
+	isCPP _ false
