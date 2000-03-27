cab67ff0e92df2f589a62cf0190f9b722efeb735
di <unknown>
2000-03-27 14:25:00 +0000

    no message

--- StrikeFont/instance/character shapes/characterFormAt-3A-.cs
+++ StrikeFont/instance/character shapes/characterFormAt-3A-.cs
@@ -2,6 +2,7 @@ characterFormAt: character
 	"Answer a Form copied out of the glyphs for the argument, character."
 	| ascii leftX rightX |
 	ascii _ character asciiValue.
+	(ascii between: minAscii and: maxAscii) ifFalse: [ascii _ maxAscii + 1].
 	leftX _ xTable at: ascii + 1.
 	rightX _ xTable at: ascii + 2.
 	^ glyphs copy: (leftX @ 0 corner: rightX @ self height)
