b6afd656d9fe693104ad1c366eef64e4eebcfa27
di <unknown>
2000-03-27 14:49:00 +0000

    no message

--- StrikeFont/instance/accessing/widthOf-3A-.cs
+++ StrikeFont/instance/accessing/widthOf-3A-.cs
@@ -2,5 +2,6 @@ widthOf: aCharacter
 	"Answer the width of the argument as a character in the receiver."
 
 	| ascii |
-	ascii _ (aCharacter asciiValue min: maxAscii) max: minAscii.
-	^(xTable at: ascii + 2) - (xTable at: ascii + 1)
+	ascii _ aCharacter asciiValue.
+	(ascii between: minAscii and: maxAscii) ifFalse: [ascii _ maxAscii + 1].
+	^ (xTable at: ascii + 2) - (xTable at: ascii + 1)
