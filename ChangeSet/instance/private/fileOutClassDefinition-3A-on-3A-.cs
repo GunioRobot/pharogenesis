a5a80c5a7a40d38cc6b40274b7c1d85881611871
di <unknown>
2000-03-28 09:23:00 +0000

    no message

--- ChangeSet/instance/private/fileOutClassDefinition-3A-on-3A-.cs
+++ ChangeSet/instance/private/fileOutClassDefinition-3A-on-3A-.cs
@@ -1,6 +1,12 @@
 fileOutClassDefinition: class on: stream 
-	"Write out class definition for the given class on the given stream, if the class definition was added or changed.  5/15/96 sw"
+	"Write out class definition for the given class on the given stream, if the class definition was added or changed."
 
-	((self atClass: class includes: #add) or: [self atClass: class includes: #change])
-		ifTrue:
-			[stream command: 'H3'; nextChunkPut: class definition; cr; command: '/H3']
+	(self atClass: class includes: #rename) ifTrue:
+		[stream nextChunkPut: 'Smalltalk renameClassNamed: #', (self oldNameFor: class), ' as: #', class name; cr].
+
+	((self atClass: class includes: #change) or: [self atClass: class includes: #add]) ifTrue:
+		[stream command: 'H3'; nextChunkPut: (self fatDefForClass: class); cr; command: '/H3'].
+
+	(self atClass: class includes: #comment) ifTrue:
+		[class theNonMetaClass organization putCommentOnFile: stream numbered: 0 moveSource: false forClass: class theNonMetaClass].
+
