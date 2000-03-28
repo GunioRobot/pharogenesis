27df02b34a0372da6534ec031d19d9c7c0db6f80
tk <unknown>
2000-03-28 10:33:00 +0000

    no message

---
+++ BookPageThumbnailMorph/instance/fileIn-2F-Out/objectForDataStream-3A-.cs
@@ -0,0 +1,33 @@
+objectForDataStream: refStrm
+	"I am about to be written on an object file.  It would be bad to write a whole BookMorph out.  Store a string that is the url of the book or page in my inst var."
+
+	| clone bookUrl bb stem ind |
+	(bookMorph class == String) & (page class == String) ifTrue: [
+		^ super objectForDataStream: refStrm].
+	(bookMorph == nil) & (page class == String) ifTrue: [
+		^ super objectForDataStream: refStrm].
+	(bookMorph == nil) & (page url ~~ nil) ifTrue: [
+		^ super objectForDataStream: refStrm].
+	(bookMorph == nil) & (page url == nil) ifTrue: [
+		self error: 'page should already have a url'.
+		"find page's book, and remember it"
+		"bookMorph _ "].
+	
+	clone _ self clone.
+	(bookUrl _ bookMorph url)
+		ifNil: [bookUrl _ self valueOfProperty: #futureUrl].
+	bookUrl 
+		ifNil: [	bb _ RectangleMorph new.	"write out a dummy"
+			bb bounds: bounds.
+			^ bb]
+		ifNotNil: [clone instVarNamed: 'bookMorph' put: bookUrl].
+
+	page url ifNil: [
+			"Need to assign a url to a page that will be written later.
+			It might have bookmarks too.  Don't want to recurse deeply.  
+			Have that page write out a dummy morph to save its url on the server."
+		stem _ SqueakPage stemUrl: bookUrl.
+		ind _ bookMorph pages identityIndexOf: page.
+		page reserveUrl: stem,(ind printString),'.sp'].
+	clone instVarNamed: 'page' put: page url.
+	^ clone
