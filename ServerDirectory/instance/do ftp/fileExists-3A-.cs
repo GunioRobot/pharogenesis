a38445179829600b9592f07ac39079d3aa196096
tk <unknown>
2000-03-28 21:38:00 +0000

    no message

--- ServerDirectory/instance/do ftp/fileExists-3A-.cs
+++ ServerDirectory/instance/do ftp/fileExists-3A-.cs
@@ -1,4 +1,7 @@
 fileExists: fileName
 	"Does the file exist on this server directory?  fileName must be simple with no / or references to other directories."
 
+	type == #file ifTrue: [^ self fileNames includes: fileName].
+	type == #http ifTrue: [^ (self readOnlyFileNamed: fileName) class ~~ String].
+	"ftp"
 	^ (self getFileList contentsOfEntireFile findTokens: FTPSocket crLf) includes: fileName
