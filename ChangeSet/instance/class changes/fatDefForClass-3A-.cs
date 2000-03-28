1c01118e09eefd374339a5775bae2ee67dc75677
di <unknown>
2000-03-28 11:44:00 +0000

    no message

---
+++ ChangeSet/instance/class changes/fatDefForClass-3A-.cs
@@ -0,0 +1,35 @@
+fatDefForClass: class
+
+	| newDef oldDef oldStrm newStrm outStrm oldVars newVars addedVars |
+	newDef _ class definition.
+	oldDef _ (self changeRecorderFor: class) priorDefinition.
+	oldDef ifNil: [^ newDef].
+	oldDef = newDef ifTrue: [^ newDef].
+
+	oldStrm _ ReadStream on: oldDef.
+	newStrm _ ReadStream on: newDef.
+	outStrm _ WriteStream on: (String new: newDef size * 2).
+
+	"Merge inst vars from old and new defs..."
+	oldStrm upToAll: 'instanceVariableNames:'; upTo: $'.
+	outStrm nextPutAll: (newStrm upToAll: 'instanceVariableNames:'); nextPutAll: 'instanceVariableNames:';
+		nextPutAll: (newStrm upTo: $'); nextPut: $'.
+	oldVars _ (oldStrm upTo: $') findTokens: Character separators.
+	newVars _ (newStrm upTo: $') findTokens: Character separators.
+	addedVars _ oldVars asSet addAll: newVars; removeAll: oldVars; asOrderedCollection.
+	oldVars , addedVars do: [:var | outStrm nextPutAll: var; space].
+	outStrm nextPut: $'.
+
+	class isMeta ifFalse:
+		["Merge class vars from old and new defs..."
+		oldStrm upToAll: 'classVariableNames:'; upTo: $'.
+		outStrm nextPutAll: (newStrm upToAll: 'classVariableNames:'); nextPutAll: 'classVariableNames:';
+			nextPutAll: (newStrm upTo: $'); nextPut: $'.
+		oldVars _ (oldStrm upTo: $') findTokens: Character separators.
+		newVars _ (newStrm upTo: $') findTokens: Character separators.
+		addedVars _ oldVars asSet addAll: newVars; removeAll: oldVars; asOrderedCollection.
+		oldVars , addedVars do: [:var | outStrm nextPutAll: var; space].
+		outStrm nextPut: $'].
+
+	outStrm nextPutAll: newStrm upToEnd.
+	^ outStrm contents
