a389dbc59dd081ae51a7c7fc7b82de1876a1b29e
di <unknown>
2000-03-29 15:49:00 +0000

    no message

---
+++ Project/instance/isolation layers/invokeFrom-3A-.cs
@@ -0,0 +1,16 @@
+invokeFrom: otherProject
+	"Revoke the changes in force for this project, and then invoke those in force for otherProject.  This method shortens the process to the shortest path up then down through the isolation layers."
+
+	| pathUp pathDown |
+	pathUp _ otherProject layersToTop.  "Full paths to top"
+	pathDown _ self layersToTop.
+
+	"Shorten paths to nearest common ancestor"
+	[pathUp isEmpty not
+		and: [pathDown isEmpty not
+		and: [pathUp last == pathDown last]]]
+		whileTrue: [pathUp removeLast.  pathDown removeLast].
+
+	"Now revoke changes up from otherProject and invoke down to self."
+	pathUp do: [:p | p revoke].
+	pathDown reverseDo: [:p | p invoke].
