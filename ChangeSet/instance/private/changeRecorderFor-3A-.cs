506eb5ae8aebf22249673663ee072911d092701f
di <unknown>
2000-03-29 20:46:00 +0000

    no message

---
+++ ChangeSet/instance/private/changeRecorderFor-3A-.cs
@@ -0,0 +1,11 @@
+changeRecorderFor: class
+
+	| cname |
+	(class isKindOf: String)
+		ifTrue: [ cname _ class ]
+		ifFalse: [ cname _ class name ].
+
+	"Later this will init the changeRecords so according to whether they should be revertable."
+	^ changeRecords at: cname
+			ifAbsent: [^ changeRecords at: cname
+							put: (ClassChangeRecord new initFor: cname revertable: revertable)]
