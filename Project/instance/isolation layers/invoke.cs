2e0386b0e6bf31f4d4b3b7195d927d812e0c1a7b
di <unknown>
2000-03-29 16:04:00 +0000

    no message

---
+++ Project/instance/isolation layers/invoke.cs
@@ -0,0 +1,8 @@
+invoke
+	"Install all methods changed here into method dictionaries.
+	Make my versions be the ones that will be called."
+
+	isolatedHead ifFalse: [^ self error: 'This isnt an isolation layer.'].
+	inForce ifTrue: [^ self error: 'This layer is already in force.'].
+	changeSet invoke.	
+	inForce _ true.
