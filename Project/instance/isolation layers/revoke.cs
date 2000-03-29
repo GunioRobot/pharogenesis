88d0f36afea16595c29b1255670c88e0b2853e74
di <unknown>
2000-03-29 16:06:00 +0000

    no message

---
+++ Project/instance/isolation layers/revoke.cs
@@ -0,0 +1,9 @@
+revoke
+	"Take back all methods changed here.
+	Install the original method dictionaries and organizations.
+	The orignal method versions will now be the ones used."
+
+	isolatedHead ifFalse: [^ self error: 'This isnt an isolation layer.'].
+	inForce ifFalse: [^ self error: 'This layer should have been in force.'].
+	changeSet revoke.	
+	inForce _ false.
