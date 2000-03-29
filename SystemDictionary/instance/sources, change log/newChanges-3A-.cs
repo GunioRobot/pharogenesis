efef551d2731c88217a261284b13031c2c16c4af
di <unknown>
2000-03-29 14:23:00 +0000

    no message

--- SystemDictionary/instance/sources, change log/newChanges-3A-.cs
+++ SystemDictionary/instance/sources, change log/newChanges-3A-.cs
@@ -1,4 +1,9 @@
 newChanges: aChangeSet 
-	"Set the system ChangeSet to be the argument, aChangeSet."
+	"Set the system ChangeSet to be the argument, aChangeSet.  Tell the current project that aChangeSet is now its change set.  When called from Project enter:, the setChangeSet: call is redundant but harmless; when called from code that changes the current-change-set from within a project, it's vital"
 
-	SystemChanges _ aChangeSet
+	SystemChanges isolationSet: nil.
+	SystemChanges _ aChangeSet.
+	Smalltalk currentProjectDo:
+		[:proj |
+		proj setChangeSet: aChangeSet.
+		aChangeSet isolationSet: proj isolationSet]
