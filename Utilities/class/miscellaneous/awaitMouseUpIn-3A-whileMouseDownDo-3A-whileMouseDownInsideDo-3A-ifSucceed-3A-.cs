20be9c2461bf83550ba68a3a3a7f3a8d3c39d54f
JMM <unknown>
2000-03-31 20:41:00 +0000

    no message

--- Utilities/class/miscellaneous/awaitMouseUpIn-3A-whileMouseDownDo-3A-whileMouseDownInsideDo-3A-ifSucceed-3A-.cs
+++ Utilities/class/miscellaneous/awaitMouseUpIn-3A-whileMouseDownDo-3A-whileMouseDownInsideDo-3A-ifSucceed-3A-.cs
@@ -1,18 +1,24 @@
 awaitMouseUpIn: box whileMouseDownDo: doBlock1 whileMouseDownInsideDo: doBlock2 ifSucceed: succBlock
-	"The mouse has gone down in box; track the mouse, inverting the box while it's within, and if, on mouse up, the cursor was still within the box, execute succBlock.  While waiting for the mouse to come up, repeatedly execute doBlock1, and also, if the cursor is within the box, execute doBlock2.  6/10/96 sw"
+	"The mouse has gone down in box; track the mouse, inverting the box while it's within, and if, on mouse up, the cursor was still within the box, execute succBlock.  While waiting for the mouse to come up, repeatedly execute doBlock1, and also, if the cursor is within the box, execute doBlock2.  6/10/96 sw
+3/31/00 JMM added logic to stop multiple redraws"
 
-	| p inside lightForm darkForm |
+	| p inside lightForm darkForm isLight |
 
 	p _ Sensor cursorPoint.
 	inside _ box insetBy: 1.
+	isLight _ true.
 	lightForm _ Form fromDisplay: inside.
 	darkForm _ lightForm deepCopy reverse.
 	[Sensor anyButtonPressed] whileTrue:
 		[doBlock1 value.
 		(box containsPoint: (p _ Sensor cursorPoint))
-			ifTrue: [doBlock2 value..
-					darkForm displayAt: inside origin]
-			ifFalse: [lightForm displayAt: inside origin]].
+			ifTrue: [doBlock2 value.
+					isLight ifTrue: 
+						[isLight _ false.
+						darkForm displayAt: inside origin]]
+			ifFalse: [isLight ifFalse:
+						[isLight _ true.
+						lightForm displayAt: inside origin]]].
 	(box containsPoint: p)
 		ifTrue: [lightForm displayAt: inside origin.
 				^ succBlock value]
