073a015fa5757e69f50f9097948a4a25fb60a1f1
tk <unknown>
2000-03-31 21:09:00 +0000

    no message

--- FileDirectory/instance/file operations/rename-3A-toBe-3A-.cs
+++ FileDirectory/instance/file operations/rename-3A-toBe-3A-.cs
@@ -1,5 +1,22 @@
 rename: oldFileName toBe: newFileName
+	| selection |
 	"Rename the file of the given name to the new name. Fail if there is no file of the old name or if there is an existing file with the new name."
+	"Modified for retry after GC ar 3/21/98 18:09"
 
-	^ self primRename: (self fullNameFor: oldFileName)
-				     to: (self fullNameFor: newFileName) 
+	(self retryWithGC:[self primRename: (self fullNameFor: oldFileName)
+						to: (self fullNameFor: newFileName)]
+		until:[:result| result notNil]) ~~ nil ifTrue:[^self].
+	(self fileExists: oldFileName) ifFalse:[
+		^self error:'Attempt to rename a non-existent file'.
+	].
+	(self fileExists: newFileName) ifTrue:[
+		selection _ (PopUpMenu labels:
+'delete old version
+cancel')
+				startUpWithCaption: 'Trying to rename a file to be
+', newFileName , '
+and it already exists.'.
+		selection = 1 ifTrue:
+			[self deleteFileNamed: newFileName.
+			^ self rename: oldFileName toBe: newFileName]].
+	^self error:'Failed to rename file'.
