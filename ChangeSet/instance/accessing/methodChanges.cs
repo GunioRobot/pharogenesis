c0d591aaab7aa141171efd99537893513e66a071
di <unknown>
2000-04-01 12:00:00 +0000

    no message

--- ChangeSet/instance/accessing/methodChanges.cs
+++ ChangeSet/instance/accessing/methodChanges.cs
@@ -1,2 +1,9 @@
 methodChanges
-	^methodChanges
+
+	| methodChangeDict changeTypes |
+	methodChangeDict _ Dictionary new.
+	changeRecords associationsDo:
+		[:assn |
+		changeTypes _ assn value methodChangeTypes.
+		changeTypes isEmpty ifFalse: [methodChangeDict at: assn key put: changeTypes]].
+	^ methodChangeDict
