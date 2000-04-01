caa226f5c57e09a5e4d7c103a2f6f2ad970b95f0
di <unknown>
2000-04-01 12:00:00 +0000

    no message

--- ChangeSet/instance/private/oldNameFor-3A-.cs
+++ ChangeSet/instance/private/oldNameFor-3A-.cs
@@ -1,5 +1,3 @@
 oldNameFor: class
-	| cName |
-	cName _ (classChanges at: class name) asOrderedCollection
-				detect: [:x | 'oldName: *' match: x].
-	^ (Scanner new scanTokens: cName) last
+
+	^ (changeRecords at: class name) priorName
