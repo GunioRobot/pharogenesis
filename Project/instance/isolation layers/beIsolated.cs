feb6f46854ddea2e8a10d52ea35124f342f58f3c
di <unknown>
2000-04-03 14:48:00 +0000

    no message

---
+++ Project/instance/isolation layers/beIsolated.cs
@@ -0,0 +1,13 @@
+beIsolated
+	"Establish an isolation layer at this project.
+	This requires clearing the current changeSet or installing a new one."
+
+	isolatedHead ifTrue: [^ self error: 'Already isolated'].
+	Project current == self ifFalse:
+		[^ self inform: 'Must be in this project to isolate it'.].
+	changeSet isEmpty ifFalse: [changeSet _ ChangeSorter newChangeSet].
+	changeSet beIsolationSetFor: self.
+	isolatedHead _ true.
+	inForce _ true.
+	environment _ Environment new setName: self name outerEnvt: Smalltalk.
+
