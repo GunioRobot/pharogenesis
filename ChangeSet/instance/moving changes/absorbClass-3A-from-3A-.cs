1dd4c1ad839cac3c7ce298ed426188bf3097920c
di <unknown>
2000-04-04 09:37:00 +0000

    no message

---
+++ ChangeSet/instance/moving changes/absorbClass-3A-from-3A-.cs
@@ -0,0 +1,10 @@
+absorbClass: className from: otherChangeSet
+	"Absorb into the receiver all the changes found in the class in the other change set.
+	*** Classes renamed in otherChangeSet may have problems"
+
+	| cls |
+	(self changeRecorderFor: className)
+			assimilateAllChangesIn: (otherChangeSet changeRecorderFor: className).
+
+	(cls _ Smalltalk classNamed: className) ifNotNil:
+		[self absorbStructureOfClass: cls from: otherChangeSet].
