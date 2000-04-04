ef93140cf33e719548023f6572424e7c8a5b9e0c
di <unknown>
2000-04-04 14:35:00 +0000

    no message

---
+++ Project/instance/object fileIn/convertdwctppdaevtngpiicmovue0-3A-dwctppdaevtngpiivue0-3A-.cs
@@ -0,0 +1,6 @@
+convertdwctppdaevtngpiicmovue0: varDict dwctppdaevtngpiivue0: smartRefStrm
+	"These variables are automatically stored into the new instance ('world' 'changeSet' 'transcript' 'parentProject' 'previousProject' 'displayDepth' 'activeProcess' 'exitFlag' 'viewSize' 'thumbnail' 'nextProject' 'guards' 'projectParameters' 'isolatedHead' 'inForce' 'version' 'urlList' 'environment' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	isolatedHead _ false.
+	inForce _ false.
