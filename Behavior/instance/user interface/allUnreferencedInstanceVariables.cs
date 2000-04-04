b37b98bd57cb4ec655285846c480befc800cac0d
sw <unknown>
2000-04-04 11:22:00 +0000

    no message

---
+++ Behavior/instance/user interface/allUnreferencedInstanceVariables.cs
@@ -0,0 +1,13 @@
+allUnreferencedInstanceVariables
+	"Return a list of the instance variables known to the receiver which are not referenced in the receiver or any of its subclasses OR superclasses"
+
+	| any definingClass |
+
+	^ self allInstVarNames copy reject:
+		[:ivn | any _ false.
+		definingClass _ self classThatDefinesInstanceVariable: ivn.
+		definingClass withAllSubclasses do:
+			[:class |  any ifFalse:
+				[(class whichSelectorsAccess: ivn asSymbol) do: 
+					[:sel | sel ~~ #DoIt ifTrue: [any _ true]]]].
+			any]
