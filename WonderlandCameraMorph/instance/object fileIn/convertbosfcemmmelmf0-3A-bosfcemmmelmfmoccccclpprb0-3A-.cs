373d26ced7c35423f04607a76eba614491ebedf5
ar <unknown>
2000-04-06 18:25:00 +0000

    no message

---
+++ WonderlandCameraMorph/instance/object fileIn/convertbosfcemmmelmf0-3A-bosfcemmmelmfmoccccclpprb0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcemmmelmf0: varDict bosfcemmmelmfmoccccclpprb0: smartRefStrm
+	"These variables are automatically stored into the new instance ('myCamera' 'myControls' 'eventFocus' 'lastCursorPoint' 'mouseUpButton' 'firstPersonControls' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('mode' 'outline' 'currentActor' 'currentPosition' 'currentCanvas' 'currentColor' 'currentNib' 'lastPosition' 'paintMode' 'palette' 'restore' 'backup' )  If a non-nil value is needed, please assign it."
