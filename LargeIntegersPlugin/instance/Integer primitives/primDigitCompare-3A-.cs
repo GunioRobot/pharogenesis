3a5a72334bfbca83d9cc8b31bb721927c9e95408
sr <unknown>
2000-04-08 02:08:00 +0000

    no message

---
+++ LargeIntegersPlugin/instance/Integer primitives/primDigitCompare-3A-.cs
@@ -0,0 +1,25 @@
+primDigitCompare: secondInteger 
+	| firstVal secondVal firstInteger |
+	self debugCode: [self msg: 'primDigitCompare: secondInteger'].
+	firstInteger _ self
+		primitive: 'primDigitCompare'
+		parameters: #(Integer  )
+		receiver: #Integer.
+	"shortcut: aSmallInteger has to be smaller in Magnitude as aLargeInteger"
+	(interpreterProxy isIntegerObject: firstInteger)
+		ifTrue: ["first"
+			(interpreterProxy isIntegerObject: secondInteger)
+				ifTrue: ["second"
+					(firstVal _ interpreterProxy integerValueOf: firstInteger) > (secondVal _ interpreterProxy integerValueOf: secondInteger)
+						ifTrue: [^ 1 asOop: SmallInteger"first > second"]
+						ifFalse: [firstVal < secondVal
+								ifTrue: [^ -1 asOop: SmallInteger"first < second"]
+								ifFalse: [^ 0 asOop: SmallInteger"first = second"]]]
+				ifFalse: ["SECOND"
+					^ -1 asOop: SmallInteger"first < SECOND"]]
+		ifFalse: ["FIRST"
+			(interpreterProxy isIntegerObject: secondInteger)
+				ifTrue: ["second"
+					^ 1 asOop: SmallInteger"FIRST > second"]
+				ifFalse: ["SECOND"
+					^ self digitCompareLarge: firstInteger with: secondInteger]]
