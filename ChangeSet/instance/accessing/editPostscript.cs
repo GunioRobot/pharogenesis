1c5ab3848f16d6503b46c6e098bef903f9272706
di <unknown>
2000-04-08 23:16:00 +0000

    no message

---
+++ ChangeSet/instance/accessing/editPostscript.cs
@@ -0,0 +1,22 @@
+editPostscript
+	"edit the receiver's postscript, in a separate window.  "
+
+	| deps found |
+	self assurePostscriptExists.
+	deps _ postscript dependents select:
+		[:m | (m isKindOf: SystemWindow) or:
+			[m isKindOf: StandardSystemView]].
+	deps size > 0 ifTrue:
+		[Smalltalk isMorphic
+			ifTrue:
+				[found _ deps detect: [:obj | (obj isKindOf: SystemWindow) and: [obj world == self currentWorld]] ifNone: [nil].
+				found ifNotNil:  [^ found activate]]
+			ifFalse:
+				[found _ deps detect: [:obj | (obj isKindOf: StandardSystemView) and: [ScheduledControllers scheduledControllers includes: obj controller]] ifNone: [nil].
+				found ifNotNil: [^ ScheduledControllers activateController: found controller]].
+.
+		self inform:
+'Caution -- there', (deps size isOrAreStringWith: 'other window'), '
+already open on this postscript elsewhere'].
+
+	postscript openLabel: 'Postscript for ChangeSet named ', name
