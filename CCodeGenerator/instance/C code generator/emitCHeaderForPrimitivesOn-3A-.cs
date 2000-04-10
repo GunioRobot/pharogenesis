80e6b11d91a3c66ab8cd97534a78e98b281d4773
TPR <unknown>
2000-04-10 10:56:00 +0000

    no message

--- CCodeGenerator/instance/C code generator/emitCHeaderForPrimitivesOn-3A-.cs
+++ CCodeGenerator/instance/C code generator/emitCHeaderForPrimitivesOn-3A-.cs
@@ -7,6 +7,10 @@ emitCHeaderForPrimitivesOn: aStream
 
 	aStream nextPutAll: '#include "sq.h"'; cr; cr.
 
+	"Additional header files"
+	headerFiles do:[:hdr|
+		aStream nextPutAll:'#include '; nextPutAll: hdr; cr].
+
 	aStream nextPutAll: '
 /* Memory Access Macros */
 #define byteAt(i) (*((unsigned char *) (i)))
@@ -16,6 +20,10 @@ emitCHeaderForPrimitivesOn: aStream
 
 /*** Imported Functions/Variables ***/
 extern int stackValue(int);
+extern int stackIntegerValue(int);
 extern int successFlag;
+
+/* allows accessing Strings in both C and Smalltalk */
+#define asciiValue(c) c
 '.
 	aStream cr.
