d10eaafa0cf7455603cbe877739c9f0da3ab0946
di <unknown>
2000-04-14 16:29:00 +0000

    no message

--- Debugger/class/instance creation/context-3A-.cs
+++ Debugger/class/instance creation/context-3A-.cs
@@ -1,10 +1,4 @@
 context: aContext 
 	"Answer an instance of me for debugging the active process starting with the given context."
 
-	^ self new
-		process: Processor activeProcess
-		controller:
-			(ScheduledControllers inActiveControllerProcess
-				ifTrue: [ScheduledControllers activeController]
-				ifFalse: [nil])
-		context: aContext
+	^ self context: aContext isolationHead: nil
