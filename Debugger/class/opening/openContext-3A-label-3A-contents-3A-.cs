646cab60c9b8dd0426be888c17cc04b0f9c42fb6
di <unknown>
2000-04-14 16:30:00 +0000

    no message

--- Debugger/class/opening/openContext-3A-label-3A-contents-3A-.cs
+++ Debugger/class/opening/openContext-3A-label-3A-contents-3A-.cs
@@ -1,11 +1,16 @@
 openContext: aContext label: aString contents: contentsString
+	| isolationHead |
 	"Open a notifier in response to an error, halt, or notify. A notifier view just shows a short view of the sender stack and provides a menu that lets the user open a full debugger."
-
+	<primitive: 19> "Simulation guard"
+	ErrorRecursion not & Preferences logDebuggerStackToFile ifTrue:
+		[Smalltalk logError: aString inContext: aContext to: 'SqueakDebug.log'].
 	ErrorRecursion ifTrue:
 		[ErrorRecursion _ false.
-		self primitiveError: aString].
+		(isolationHead _ Project current isolationHead)
+			ifNil: [self primitiveError: aString]
+			ifNotNil: [isolationHead revoke]].
 	ErrorRecursion _ true.
-	(Debugger context: aContext)
+	(Debugger context: aContext isolationHead: isolationHead)
 		openNotifierContents: contentsString
 		label: aString.
 	ErrorRecursion _ false.
