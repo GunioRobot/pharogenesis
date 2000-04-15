3dff5724a9601f9a5f018c3027b1e2e93a72598a
th <unknown>
2000-04-15 17:21:00 +0000

    no message

--- InterpreterSimulator/instance/memory access/byteAt-3A-.cs
+++ InterpreterSimulator/instance/memory access/byteAt-3A-.cs
@@ -1,6 +1,2 @@
 byteAt: byteAddress
-	| lowBits |
-	lowBits _ byteAddress bitAnd: 3.
-	^((self longAt: byteAddress - lowBits)
-		bitShift: (lowBits - 3) * 8)
-		bitAnd: 16rFF
+	^self subclassResponsibility
