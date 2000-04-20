77ccfe987c289c91ef27e7cd24e0b635cf03eace
sw <unknown>
2000-04-20 12:18:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/browseIt.cs
+++ ParagraphEditor/instance/menu messages/browseIt.cs
@@ -2,21 +2,27 @@ browseIt
 	"Launch a browser for the current selection, if appropriate"
 
 	| aSymbol anEntry brow |
+
+	Preferences alternativeBrowseIt ifTrue: [^ self browseClassFromIt].
+
 	self lineSelectAndEmptyCheck: [^ self].
 	(aSymbol _ self selectedSymbol) isNil ifTrue: [^ view flash].
 
 	self terminateAndInitializeAround:
 		[aSymbol first isUppercase
 			ifTrue:
-				[anEntry _ (Smalltalk at: aSymbol ifAbsent: [nil]).
+				[anEntry _ (Smalltalk
+					at: aSymbol
+					ifAbsent:
+						[Smalltalk browseAllImplementorsOf: aSymbol.
+						^ nil]).
 				anEntry isNil ifTrue: [^ view flash].
 				(anEntry isKindOf: Class)
-					ifTrue:
-						[brow _ Browser new.
-						brow setClass: anEntry selector: nil.
-						Browser openBrowserView: (brow openEditString: nil)
-							label: 'System Browser']
-					ifFalse:
-						[anEntry inspect]]
+					ifFalse:	[anEntry _ anEntry class].
+				brow _ Preferences browseToolClass new.
+				brow setClass: anEntry selector: nil.
+				brow class
+					openBrowserView: (brow openEditString: nil)
+					label: 'System Browser']
 			ifFalse:
 				[Smalltalk browseAllImplementorsOf: aSymbol]]
