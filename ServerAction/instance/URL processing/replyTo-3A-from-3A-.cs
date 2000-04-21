3d87776689e789a87efd9846dabf472dda1fba9a
mjg <unknown>
2000-04-21 14:10:00 +0000

    no message

--- ServerAction/instance/URL processing/replyTo-3A-from-3A-.cs
+++ ServerAction/instance/URL processing/replyTo-3A-from-3A-.cs
@@ -3,7 +3,9 @@ replyTo: pieces from: request
 	"Transcript show: 'Reply: ', pieces; cr."
 	(StandardFileStream isAFileNamed: pieces) 
 		ifTrue: [
-			request reply: PWS success; reply: PWS contentHTML, PWS crlf.
+			request reply: PWS success; 
+				reply: (PWS content: (MIMEDocument guessTypeFromName: pieces)),
+					 PWS crlf.
 			request reply: (FileStream fileNamed: pieces) contentsOfEntireFile]
 		ifFalse: [
 			request error: PWS notFound].
