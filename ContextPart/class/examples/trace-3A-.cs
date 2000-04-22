c5166d1680488035ee17d32c4ef71a512029f48d
sma <unknown>
2000-04-22 17:03:00 +0000

    no message

--- ContextPart/class/examples/trace-3A-.cs
+++ ContextPart/class/examples/trace-3A-.cs
@@ -1,18 +1,5 @@
 trace: aBlock		"ContextPart trace: [3 factorial]"
 	"This method uses the simulator to print calls and returned values in the Transcript."
-	| prev |
+
 	Transcript clear.
-	prev _ aBlock.
-	^ thisContext sender
-		runSimulated: aBlock
-		contextAtEachStep:
-			[:current |
-			Sensor anyButtonPressed ifTrue: [^ nil].
-			current == prev
-				ifFalse:
-					[prev sender == nil ifTrue:  "returning"
-						[Transcript space; nextPut: $^; print: current top].
-					Transcript cr;
-						nextPutAll: (String new: (current depthBelow: aBlock) withAll: $ );
-						print: current receiver; space; nextPutAll: current selector; endEntry.
-					prev _ current]]
+	^ self trace: aBlock on: Transcript
