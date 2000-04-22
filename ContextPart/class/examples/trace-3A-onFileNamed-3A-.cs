fd6a0f85d90d913bc6a154afaacb3c6214a4570c
sma <unknown>
2000-04-22 17:05:00 +0000

    no message

--- ContextPart/class/examples/trace-3A-onFileNamed-3A-.cs
+++ ContextPart/class/examples/trace-3A-onFileNamed-3A-.cs
@@ -1,18 +1,6 @@
-trace: aBlock onFileNamed: fileName		"ContextPart trace: [3 factorial]"
+trace: aBlock onFileNamed: fileName		"ContextPart trace: [3 factorial] onFileNamed: 'trace'"
 	"This method uses the simulator to print calls to a file."
-	| prev f sel |
-	f _ FileStream fileNamed: fileName.
-	prev _ aBlock.
-	thisContext sender
-		runSimulated: aBlock
-		contextAtEachStep:
-			[:current |
-			Sensor anyButtonPressed ifTrue: [^ nil].
-			current == prev
-				ifFalse:
-					[f cr;
-						nextPutAll: (String new: (current depthBelow: aBlock) withAll: $ );
-						print: current receiver class; space; nextPutAll: (sel _ current selector); flush.
-					prev _ current.
-					sel == #error: ifTrue: [self halt]]].
-	f close
+
+	| aStream |
+	^ [aStream _ FileStream fileNamed: fileName.
+		self trace: aBlock on: aStream] ensure: [aStream close]
