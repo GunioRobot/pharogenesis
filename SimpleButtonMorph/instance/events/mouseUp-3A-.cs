1ac0b434843a4b589a1346c3b8eef3058148d102
sma <unknown>
2000-04-22 17:29:00 +0000

    no message

--- SimpleButtonMorph/instance/events/mouseUp-3A-.cs
+++ SimpleButtonMorph/instance/events/mouseUp-3A-.cs
@@ -1,5 +1,9 @@
 mouseUp: evt
-
-	self color: oldColor.
-	(actWhen == #buttonUp and: [self containsPoint: evt cursorPoint])
-		ifTrue: [self doButtonAction].
+	oldColor ifNotNil:
+		["if oldColor nil, it signals that mouse had not gone DOWN
+		inside me, e.g. because of a cmd-drag; in this case we want
+		to avoid triggering the action!"
+		self color: oldColor.
+		oldColor _ nil.
+		(actWhen == #buttonUp and: [self containsPoint: evt cursorPoint])
+			ifTrue: [self doButtonAction]]
