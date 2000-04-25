91b138af1471e60dd27fdc04adb39a1342daf14c
hmm <unknown>
2000-04-25 21:19:00 +0000

    no message

---
+++ StandardSourceFileArray/class/initialize-2D-release/install.cs
@@ -0,0 +1,5 @@
+install
+	"Replace SourceFiles by an instance of me with the standard sources and changes files.
+	This only works if SourceFiles is either an Array or an instance of this class"
+	"StandardSourceFileArray install"
+	SourceFiles _ self new initialize
