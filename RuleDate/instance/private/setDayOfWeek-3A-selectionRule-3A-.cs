00bca73de39a948057fbaee697438f8ce29c7550
RAH <unknown>
2000-04-25 19:49:00 +0000

    no message

---
+++ RuleDate/instance/private/setDayOfWeek-3A-selectionRule-3A-.cs
@@ -0,0 +1,9 @@
+setDayOfWeek: dayName selectionRule: positionName 
+	"Private - Set dayOfWeek to dayName, and selectionRule to positionName. 
+	 
+	Parameters 
+	dayName		<Symbol>		captured 
+	positionName	<Symbol>		captured
+	"
+	dayOfWeek := dayName.
+	selectionRule := positionName
