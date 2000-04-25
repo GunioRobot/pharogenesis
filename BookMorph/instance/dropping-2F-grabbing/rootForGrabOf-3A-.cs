cb9e9aa066168279f91f385eaf2f5f14ba10f24a
panda <unknown>
2000-04-25 15:47:00 +0000

    no message

--- BookMorph/instance/dropping-2F-grabbing/rootForGrabOf-3A-.cs
+++ BookMorph/instance/dropping-2F-grabbing/rootForGrabOf-3A-.cs
@@ -1,16 +1,12 @@
 rootForGrabOf: aMorph
 
 	| root |
-	(openToDragNDrop or: [copyContents])
-		ifFalse: [^ super rootForGrabOf: aMorph].
+	self dragNDropEnabled ifFalse: [^ super rootForGrabOf: aMorph].
 	(aMorph = currentPage or: [aMorph owner = self])
 		ifTrue: [^ self rootForGrabOf: self].
 
 	root _ aMorph.
 	[root = self] whileFalse:
-		[root owner == currentPage ifTrue:
-			[(copyContents and: [openToDragNDrop not])
-				ifTrue: [^ root fullCopy]
-				ifFalse: [^ root]].
+		[root owner == currentPage ifTrue: [^ root].
 		root _ root owner].
 	^ super rootForGrabOf: aMorph
