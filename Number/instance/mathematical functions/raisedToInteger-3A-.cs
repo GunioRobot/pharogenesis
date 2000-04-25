8b9726e68d353b9ede468d3451be23e804369025
RAH <unknown>
2000-04-25 19:49:00 +0000

    no message

--- Number/instance/mathematical functions/raisedToInteger-3A-.cs
+++ Number/instance/mathematical functions/raisedToInteger-3A-.cs
@@ -1,12 +1,19 @@
-raisedToInteger: anInteger 
-	"Answer the receiver raised to the power anInteger where the argument 
-	must be a kind of Integer. This is a special case of raisedTo:."
-
-	anInteger isInteger ifFalse:
-		[^self error: 'raisedToInteger: only works for integral arguments'].
-	anInteger = 0 ifTrue: [^ 1].
-	(self = 0) | (anInteger = 1) ifTrue: [^ self].
-	anInteger > 1 ifTrue:
-		[^ (self * self raisedToInteger: anInteger // 2)
-					* (self raisedToInteger: anInteger \\ 2)].
-	^ (self raisedToInteger: anInteger negated) reciprocal
+raisedToInteger: operand 
+	"Answer the receiver raised to the power operand, an Integer."
+	| count result |
+	#Numeric.
+	"Changed 200/01/19 For ANSI <number> support."
+	operand isInteger ifFalse: [^ ArithmeticError signal: 'parameter is not an Integer'"<- Chg"].
+	operand = 0 ifTrue: [^ self class one].
+	operand = 1 ifTrue: [^ self].
+	operand < 0 ifTrue: [^ (self raisedToInteger: operand negated) reciprocal].
+	count := 1.
+	[(count := count + count) < operand] whileTrue.
+	result := self class one.
+	[count > 0]
+		whileTrue: 
+			[result := result * result.
+			(operand bitAnd: count)
+				= 0 ifFalse: [result := result * self].
+			count := count bitShift: -1].
+	^ result
