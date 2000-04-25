d90f08ba869636b935278a11991d63df4772d34d
RAH <unknown>
2000-04-25 19:49:00 +0000

    no message

---
+++ RuleSelectionCodeDate/instance/private/applyRuleTo-3A-.cs
@@ -0,0 +1,4 @@
+applyRuleTo: daysNamedInMonthList 
+	"Private - Answer the day of the month selected from dayOfMonth list 
+	by applying the receiver's rule."
+	^ selectionRule value: daysNamedInMonthList
