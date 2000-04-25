aed006e7cbf5564b1ed64c02ba65a2599d95fdb6
panda <unknown>
2000-04-25 15:47:00 +0000

    no message

--- AlignmentMorph/instance/dropping-2F-grabbing/rootForGrabOf-3A-.cs
+++ AlignmentMorph/instance/dropping-2F-grabbing/rootForGrabOf-3A-.cs
@@ -1,7 +1,7 @@
 rootForGrabOf: aMorph
 
 	| root |
-	openToDragNDrop ifFalse: [^ super rootForGrabOf: aMorph].
+	self dragNDropEnabled ifFalse: [^ super rootForGrabOf: aMorph].
 	root _ aMorph.
 	[root == self] whileFalse:
 		[root owner = self ifTrue: [^ root].
