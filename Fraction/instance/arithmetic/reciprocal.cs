6729fc9fed9e04a17cfffe9d77c494155e4f235b
RAH <unknown>
2000-04-25 19:49:00 +0000

    no message

--- Fraction/instance/arithmetic/reciprocal.cs
+++ Fraction/instance/arithmetic/reciprocal.cs
@@ -1,7 +1,8 @@
-reciprocal 
+reciprocal
 	"Refer to the comment in Number|reciprocal."
-
-	numerator = 0 ifTrue: [self error: '0 has no reciprocal'].
-	numerator = 1 ifTrue: [^denominator].
-	numerator = -1 ifTrue: [^denominator negated].
-	^Fraction numerator: denominator denominator: numerator
+	#Numeric.
+	"Changed 200/01/19 For ANSI <number> support."
+	numerator = 0 ifTrue: [^ (ZeroDivide dividend: self) signal"<- Chg"].
+	numerator = 1 ifTrue: [^ denominator].
+	numerator = -1 ifTrue: [^ denominator negated].
+	^ Fraction numerator: denominator denominator: numerator
