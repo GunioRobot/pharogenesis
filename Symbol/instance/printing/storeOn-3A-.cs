56e6cedb230cfec4b01d271bf7e2556f6ae3caca
di <unknown>
2000-04-25 12:32:00 +0000

    no message

--- Symbol/instance/printing/storeOn-3A-.cs
+++ Symbol/instance/printing/storeOn-3A-.cs
@@ -1,9 +1,6 @@
-storeOn: aStream
+storeOn: aStream 
 
-	self isLiteral
-		ifTrue:
-			[aStream nextPut: $#.
-			aStream nextPutAll: self]
-		ifFalse:
-			[super storeOn: aStream.
-			aStream nextPutAll: ' asSymbol']
+	aStream nextPut: $#.
+	(Scanner isLiteralSymbol: self)
+		ifTrue: [aStream nextPutAll: self]
+		ifFalse: [super storeOn: aStream]
