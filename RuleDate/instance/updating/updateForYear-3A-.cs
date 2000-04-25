9bebe53a1c3270a64a6fe4ee1e4eac15bf2c801e
RAH <unknown>
2000-04-25 19:49:00 +0000

    no message

---
+++ RuleDate/instance/updating/updateForYear-3A-.cs
@@ -0,0 +1,9 @@
+updateForYear: yyyy 
+	"Update the receiver by applying the rule for its month number of 
+	year, yyyy. 
+	 
+	Definition: <RuleDate> 
+	Parameters 
+	yyyy	<Integer>	captured
+	"
+	self basicUpdateForMonth: self monthIndex year: yyyy
