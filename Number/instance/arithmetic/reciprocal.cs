9d8f0e2be76ef9c885c9d90f2243ff303e5c4736
RAH <unknown>
2000-04-25 19:49:00 +0000

    no message

--- Number/instance/arithmetic/reciprocal.cs
+++ Number/instance/arithmetic/reciprocal.cs
@@ -1,7 +1,7 @@
 reciprocal
 	"Answer 1 divided by the receiver. Create an error notification if the 
 	receiver is 0."
-
-	self = 0
-		ifTrue: [^self error: 'zero has no reciprocal']
-		ifFalse: [^1 / self]
+	#Numeric.
+	"Changed 200/01/19 For ANSI <number> support."
+	self = 0 ifTrue: [^ (ZeroDivide dividend: self) signal"<- Chg"].
+	^ 1 / self
