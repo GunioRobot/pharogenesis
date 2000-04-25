017b24dace8b4fb3b387062196054c5e24f2b26d
sw <unknown>
2000-04-25 14:07:00 +0000

    no message

---
+++ Preferences/class/hard-2D-coded prefs/isFlagship.cs
@@ -0,0 +1,4 @@
+isFlagship
+	"Manually change this to return true if you wish your system to be marked as a 'flagship'.  The intent here is to allow an update to query this flag before undertaking some radical do-it that might clobber important content is such an image."
+
+	^ false
