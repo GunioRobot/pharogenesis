76007a7afa3b01168fb76a4d7c0114cfcac4369f
hmm <unknown>
2000-04-26 20:44:00 +0000

    no message

--- CompiledMethod/instance/source code management/sourcePointer.cs
+++ CompiledMethod/instance/source code management/sourcePointer.cs
@@ -1,2 +1,10 @@
 sourcePointer
-	^ (self fileIndex * 16r1000000) + self filePosition
+	"Answer the integer which can be used to find the source file and position for this method.
+	The returned value is either 0 (if no source is stored) or a number between 16r1000000 and 16r4FFFFFF.
+	The actual interpretation of this number is up to the SourceFileArray stored in the global variable SourceFiles."
+
+	| pos |
+	self last < 252 ifTrue: [^ 0  "no source"].
+	pos _ self last - 251.
+	self size - 1 to: self size - 3 by: -1 do: [:i | pos _ pos * 256 + (self at: i)].
+	^pos
