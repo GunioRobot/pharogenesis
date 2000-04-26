c64322c03fd30b245290998b780ca8dba3e03bcb
di <unknown>
2000-04-26 20:22:00 +0000

    no message

--- ClassOrganizer/instance/printing/printOn-3A-.cs
+++ ClassOrganizer/instance/printing/printOn-3A-.cs
@@ -1,17 +1,14 @@
 printOn: aStream 
 	"Refer to the comment in Object|printOn:."
 
-	| elementIndex lastStop |
+	| elementIndex |
 	elementIndex _ 1.
-	lastStop _ 1.
 	1 to: categoryArray size do: 
 		[:i | 
 		aStream nextPut: $(.
 		(categoryArray at: i) asString printOn: aStream.
 		[elementIndex <= (categoryStops at: i)]
 			whileTrue: 
-				[aStream space.
-				(elementArray at: elementIndex) printOn: aStream.
+				[aStream space; nextPutAll: (elementArray at: elementIndex).
 				elementIndex _ elementIndex + 1].
-		aStream nextPut: $).
-		aStream cr]
+		aStream nextPut: $); cr]
