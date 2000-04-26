dc2818e3af05dae198b79b296bf3c493cefeb474
hmm <unknown>
2000-04-26 20:44:00 +0000

    no message

--- CompiledMethod/instance/source code management/fileIndex.cs
+++ CompiledMethod/instance/source code management/fileIndex.cs
@@ -1,10 +1,2 @@
 fileIndex
-	"Answer the index of the sources file on which this method is stored, as follows:
-		1:	.sources file
-		2:	.changes file
-		3 and 4 are also available for future extension of source code management"
-
-	self last < 252 ifTrue: [^ 0  "no source"].
-	^ self last - 251
-
-	
+	^SourceFiles fileIndexFromSourcePointer: self sourcePointer
