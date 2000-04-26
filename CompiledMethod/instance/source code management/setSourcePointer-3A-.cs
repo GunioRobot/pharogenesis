af55a78de3721338de8199b7398ded6f09daf2b1
hmm <unknown>
2000-04-26 21:00:00 +0000

    no message

--- CompiledMethod/instance/source code management/setSourcePointer-3A-.cs
+++ CompiledMethod/instance/source code management/setSourcePointer-3A-.cs
@@ -1,2 +1,8 @@
 setSourcePointer: srcPointer
-	self setSourcePosition: srcPointer \\ 16r1000000 inFile: srcPointer // 16r1000000
+	srcPointer = 0 ifTrue: [
+		self at: self size put: 0.
+		^self].
+	(srcPointer between: 16r1000000 and: 16r4FFFFFF) ifFalse: [self error: 'Source pointer out of range'].
+	self at: self size put: (srcPointer bitShift: -24) + 251.
+	1 to: 3 do: [:i |
+		self at: self size-i put: ((srcPointer bitShift: (i-3)*8) bitAnd: 16rFF)]
