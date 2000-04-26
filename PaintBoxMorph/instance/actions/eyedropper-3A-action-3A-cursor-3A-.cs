214ef7c5fc7e9b9374d5ff7c914949cbbdc5b7b1
kfr <unknown>
2000-04-26 22:25:00 +0000

    no message

--- PaintBoxMorph/instance/actions/eyedropper-3A-action-3A-cursor-3A-.cs
+++ PaintBoxMorph/instance/actions/eyedropper-3A-action-3A-cursor-3A-.cs
@@ -1,25 +1,39 @@
-eyedropper: aButton action: aSelector cursor: aCursor
-	"Take total control and pick up a color!!"
+eyedropper: aButton action: aSelector cursor: aCursor 
+        "Take total control and pick up a color!!"
+        | pt feedbackColor |
+        aButton state: #on.
+        tool ifNotNil: [tool state: #off].
+        currentCursor _ aCursor.
+        self activeHand
+                showTemporaryCursor: currentCursor 
+                hotSpotOffset: 6 negated @ 4 negated.    "<<<< the form was changed a bit??"
+        feedbackColor _ Display colorAt: Sensor cursorPoint.
+        self addMorphFront: colorMemory.
+        "Full color picker"
+        [Sensor anyButtonPressed]
+                whileFalse: 
+                        [pt _ Sensor cursorPoint.
 
-	| pt feedbackColor |
-	aButton state: #on.
-	tool ifNotNil: [tool state: #off].
-	currentCursor _ aCursor.
-"	self world hands first showTemporaryCursor: aCursor 
-		hotSpotOffset: (aCursor ifNil: [0@0] ifNotNil: [aCursor offset])."
-	feedbackColor _ Display colorAt: Sensor cursorPoint.
-	self addMorphFront: colorMemory.	"Full color picker"
-	self world displayWorld.
-		[Sensor anyButtonPressed] whileFalse: [
-			pt _ Sensor cursorPoint.
-			feedbackColor _ Display colorAt: pt.
-			Display fill: (colorPatch bounds translateBy: self world viewBox origin) 
-					fillColor: feedbackColor].
-		Sensor waitNoButton.
-"	self world hands first showTemporaryCursor: nil 
-		hotSpotOffset: 0@0."
-	self currentColor: feedbackColor.
-	colorMemory delete.
-	tool ifNotNil: [tool state: #on.
-		currentCursor _ tool arguments at: 3].
-	aButton state: #off.
+                        "deal with the fact that 32 bit displays may have garbage in the alpha bits"
+                        feedbackColor _ Display depth = 32 ifTrue: [
+                                Color colorFromPixelValue: ((Display pixelValueAt: pt)
+														bitOr: 16rFF000000) depth: 32
+                        ] ifFalse: [
+                                Display colorAt: pt
+                        ].
+
+                        "the hand needs to be drawn"
+                        self activeHand position: pt.
+                        self world displayWorldSafely.
+                        Display fill: colorPatch bounds fillColor: feedbackColor].
+        Sensor waitNoButton.
+        self activeHand showTemporaryCursor: nil hotSpotOffset: 0 @ 0.
+        self currentColor: feedbackColor.
+        colorMemory delete.
+		 
+        tool
+                ifNotNil: 
+                        [tool state: #on.
+                        currentCursor _ tool arguments at: 3].
+
+        aButton state: #off
