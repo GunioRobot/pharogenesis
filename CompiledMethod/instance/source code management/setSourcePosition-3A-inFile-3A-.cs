43311a50fe8d9a6df5fade2a5e505e7a2193b602
hmm <unknown>
2000-04-26 21:02:00 +0000

    no message

--- CompiledMethod/instance/source code management/setSourcePosition-3A-inFile-3A-.cs
+++ CompiledMethod/instance/source code management/setSourcePosition-3A-inFile-3A-.cs
@@ -1,9 +1,2 @@
 setSourcePosition: position inFile: fileIndex 
-	"Store the location of the source code for the receiver in the receiver. The 
-	location consists of which source file (*.sources or *.changes) and the 
-	position in that file."
-
-	fileIndex > 4 ifTrue: [^ self error: 'invalid file number'].
-	self at: self size put: 251 + fileIndex.
-	1 to: 3 do: 
-		[:i | self at: self size - i put: ((position bitShift: (i-3)*8) bitAnd: 16rFF)].
+	self setSourcePointer: (SourceFiles sourcePointerFromFileIndex: fileIndex andPosition: position)
