2909acc211c5beed2d2b496536cf11ef38be0701
hmm <unknown>
2000-04-26 20:45:00 +0000

    no message

--- CompiledMethod/instance/source code management/filePosition.cs
+++ CompiledMethod/instance/source code management/filePosition.cs
@@ -1,7 +1,2 @@
 filePosition
-	"Answer the file position of this method's source code."
-	| pos |
-	self last < 252 ifTrue: [^ 0  "no source"].
-	pos _ 0.
-	self size - 1 to: self size - 3 by: -1 do: [:i | pos _ pos * 256 + (self at: i)].
-	^ pos
+	^SourceFiles filePositionFromSourcePointer: self sourcePointer
