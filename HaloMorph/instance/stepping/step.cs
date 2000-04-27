6aece163ff46859a517badd44fadefdc8103933f
sw <unknown>
2000-04-27 13:40:00 +0000

    no message

--- HaloMorph/instance/stepping/step.cs
+++ HaloMorph/instance/stepping/step.cs
@@ -1,9 +1,12 @@
 step
+
 	| newBounds |
 	target ifNil: [^ self].
-	(newBounds _ target fullBoundsInWorld) = self bounds ifTrue: [^ self].
-	growingOrRotating ifFalse:
-		["adjust halo bounds if appropriate"
-		submorphs size > 1 ifTrue:
-			[self addHandles "recreates full set with new bounds"].
-		self bounds: newBounds]
+	target isWorldMorph
+		ifTrue: [newBounds _ target bounds]
+		ifFalse: [newBounds _ target renderedMorph worldBoundsForHalo].
+	newBounds = self bounds ifTrue: [^ self].
+	growingOrRotating ifFalse: [  "adjust halo bounds if appropriate"
+		submorphs size > 1
+			ifTrue: [self addHandles].  "recreates full set with new bounds"
+		self bounds: newBounds].
