50981814c6b785941e3a8266bb6ba1f4659b4435
kfr <unknown>
2000-04-27 20:57:00 +0000

    no message

--- SystemWindow/instance/initialization/initialize.cs
+++ SystemWindow/instance/initialization/initialize.cs
@@ -1,6 +1,8 @@
 initialize
 	| aFont |
 	super initialize.
+	allowReframeHandles := true.
+	labelString ifNil: [labelString _ 'Untitled Window'].
 	isCollapsed _ false.
 	activeOnlyOnTop _ true.
 	paneMorphs _ Array new.
@@ -8,17 +10,26 @@ initialize
 	borderColor _ #raised.
 	borderWidth _ 1.
 	color _ Color black.
-	aFont _ Preferences fontForScriptorButtons.
+	aFont _ Preferences standardButtonFont.
 	stripes _ Array with: (RectangleMorph newBounds: bounds)  "see extent:"
 				with: (RectangleMorph newBounds: bounds).
 	self addMorph: (stripes first borderWidth: 1).
 	self addMorph: (stripes second borderWidth: 2).
 	self addMorph: (label _ StringMorph new contents: labelString;
-			font: ((TextStyle default fontAt: 2) emphasized: 1)).
-	self addMorph: (closeBox _ SimpleButtonMorph new borderWidth: 0;
-			label: 'X' font: aFont; color: Color transparent;
-			actionSelector: #delete; target: self; extent: 16@16).
+			font: Preferences windowTitleFont emphasis: 1).
+	self setLabelWidgetAllowance.
+	self addCloseBox.
+	self addMenuControl.
 	self addMorph: (collapseBox _ SimpleButtonMorph new borderWidth: 0;
 			label: 'O' font: aFont; color: Color transparent;
-			actionSelector: #collapseOrExpand; target: self; extent: 16@16).
-	self extent: 300@200
+			actionSelector: #collapseOrExpand; target: self; extent: 14@14).
+	Preferences noviceMode ifTrue:
+		[closeBox ifNotNil: [closeBox setBalloonText: 'close window'].
+		menuBox ifNotNil: [menuBox setBalloonText: 'window menu'].
+		collapseBox ifNotNil: [collapseBox setBalloonText: 'collapse/expand window']].
+	self on: #mouseEnter send: #spawnReframeHandle: to: self.
+	self on: #mouseLeave send: #spawnReframeHandle: to: self.
+	label on: #mouseDown send: #relabelEvent: to: self.
+	self extent: 300@200.
+	mustNotClose _ false.
+	updatablePanes _ Array new.
