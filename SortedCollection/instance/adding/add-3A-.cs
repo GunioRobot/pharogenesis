f61f5487ee565784077bfedffa158cf6a1b0af6a
go <unknown>
2000-04-27 13:19:00 +0000

    no message

--- SortedCollection/instance/adding/add-3A-.cs
+++ SortedCollection/instance/adding/add-3A-.cs
@@ -1,7 +1,2 @@
 add: newObject
-
-	| nextIndex |
-	self isEmpty ifTrue: [^self addLast: newObject].
-	nextIndex _ self indexForInserting: newObject.
-	self insert: newObject before: nextIndex.
-	^newObject
+	^ super insert: newObject before: (self indexForInserting: newObject)
