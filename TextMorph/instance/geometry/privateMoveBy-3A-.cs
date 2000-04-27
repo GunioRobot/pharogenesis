6856eaded625a65222692ad8fa026d12f7d86674
di <unknown>
2000-04-27 10:37:00 +0000

    no message

--- TextMorph/instance/geometry/privateMoveBy-3A-.cs
+++ TextMorph/instance/geometry/privateMoveBy-3A-.cs
@@ -1,5 +1,9 @@
 privateMoveBy: delta
-	self releaseEditor.
-	super privateMoveBy: delta.
-	paragraph ifNotNil: [paragraph moveBy: delta].
-	container ifNotNil: [container releaseCachedState]
+
+	editor == nil
+		ifTrue: [super privateMoveBy: delta.
+				paragraph ifNotNil: [paragraph moveBy: delta]]
+		ifFalse: ["When moving text with an active editor, save and restore all state."
+				super privateMoveBy: delta.
+				paragraph moveBy: delta.
+				self installEditorToReplace: editor]
