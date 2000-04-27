cc4cd4697db0f9d292f5ecba0f65487502009836
di <unknown>
2000-04-27 17:45:00 +0000

    no message

--- HaloMorph/instance/private/doGrow-3A-with-3A-.cs
+++ HaloMorph/instance/private/doGrow-3A-with-3A-.cs
@@ -1,7 +1,8 @@
 doGrow: evt with: growHandle
 
 	| newExtent |
-	newExtent _ (evt cursorPoint - positionOffset) - target topLeft.
-	target extent: (newExtent max: minExtent).
+	newExtent _ (target pointFromWorld: (evt cursorPoint - positionOffset)) - target topLeft.
+	evt shiftPressed ifTrue: [newExtent _ (newExtent x max: newExtent y) asPoint].
+	target renderedMorph extent: (newExtent max: minExtent).
 	growHandle position: evt cursorPoint - (growHandle extent // 2).
 	self layoutChanged.
