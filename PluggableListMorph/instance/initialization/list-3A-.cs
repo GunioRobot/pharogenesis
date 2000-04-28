4a820b6cbe36a162d1e59fbc7f9e84c8fb3058a1
panda <unknown>
2000-04-28 18:14:00 +0000

    no message

--- PluggableListMorph/instance/initialization/list-3A-.cs
+++ PluggableListMorph/instance/initialization/list-3A-.cs
@@ -1,23 +1,25 @@
-list: listOfStrings
-	| morphList handler h loc index |
+list: listOfStrings 
+	| morphList h loc index |
 	scroller removeAllMorphs.
 	list _ listOfStrings ifNil: [Array new].
-	list isEmpty ifTrue: [^ self selectedMorph: nil].
+	list isEmpty ifTrue: [self setScrollDeltas.  ^ self selectedMorph: nil].
 	"NOTE: we will want a quick StringMorph init message, possibly even
 		combined with event install and positioning"
-	morphList _ list collect: [:item | StringMorph contents: item].
+	font ifNil: [font _ Preferences standardListFont].
+	morphList _ list collect:
+		[:item | item isText
+			ifTrue: [StringMorph contents: item font: font emphasis: (item emphasisAt: 1)]
+			ifFalse: [StringMorph contents: item font: font]].
 
-	"Sensitize first morph and copy handler to all the rest"
-	morphList first on: #mouseDown send: #mouseDown:onItem: to: self.
-	handler _ morphList first eventHandler.
-	morphList do: [:m | m eventHandler: handler].
 
 	"Lay items out vertically and install them in the scroller"
 	h _ morphList first height "self listItemHeight".
 	loc _ 0@0.
 	morphList do: [:m | m bounds: (loc extent: 9999@h).  loc _ loc + (0@h)].
 	scroller addAllMorphs: morphList.
+	self installEventHandlerOn: morphList.
+
 	index _ self getCurrentSelectionIndex.
-	self selectedMorph: (index = 0 ifTrue: [nil] ifFalse: [morphList at: index]).
+	self selectedMorph: ((index = 0 or: [index > morphList size]) ifTrue: [nil] ifFalse: [morphList at: index]).
 	self setScrollDeltas.
-	scrollBar setValue: 0.0.
+	scrollBar setValue: 0.0
