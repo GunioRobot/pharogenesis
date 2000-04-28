c1339036de9af8a068cde4630e68711d725f869d
dvf <unknown>
2000-04-28 02:50:00 +0000

    no message

---
+++ MIMEPart/instance/as yet unclassified/fieldsAsMimeHeader.cs
@@ -0,0 +1,8 @@
+fieldsAsMimeHeader
+	| strm |
+	strm _ WriteStream on: (String new: 100).
+	self fields associationsDo: [:e | strm nextPutAll: e key;
+		 nextPutAll: ': ';
+		 nextPutAll: e value asHeaderValue;
+		 cr]. 
+	^ strm contents
