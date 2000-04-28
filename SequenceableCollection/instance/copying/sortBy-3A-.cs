7e25070bdfd1df84e237ed6086df2e9c4dab8545
sma <unknown>
2000-04-28 18:34:00 +0000

    no message

--- SequenceableCollection/instance/copying/sortBy-3A-.cs
+++ SequenceableCollection/instance/copying/sortBy-3A-.cs
@@ -1,8 +1,6 @@
 sortBy: aBlock
-	"Create a copy that is sorted.  Sort criteria is the block that accepts two arguments.  When the block is true, the first arg goes first ([:a :b | a > b] sorts in descending order)."
-	| sorted other |
-	sorted _ (SortedCollection sortBlock: aBlock) addAll: self.
-	other _ self copy.
-	1 to: self size do: [:index |  other at: index put: 
-		(sorted at: index)].
-	^ other
+	"Create a copy that is sorted.  Sort criteria is the block that accepts two arguments.
+	When the block is true, the first arg goes first ([:a :b | a > b] sorts in descending
+	order)."
+
+	^ (self asSortedCollection: aBlock) asOrderedCollection
