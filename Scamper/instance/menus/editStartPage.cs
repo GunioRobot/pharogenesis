104e0cea9c70a60214aa265860c287952bf9f3e7
sma <unknown>
2000-04-30 10:12:00 +0000

    no message

---
+++ Scamper/instance/menus/editStartPage.cs
@@ -0,0 +1,9 @@
+editStartPage
+	| win textMorph |
+	Smalltalk isMorphic ifFalse: [^ self inform: 'only works for morphic currently'].
+
+	win _ SystemWindow labelled: 'edit Bookmark page'.
+	textMorph _ PluggableTextMorph on: self text: #startPage  accept: #startPage:.
+	win addMorph: textMorph frame: (0@0 extent: 1@1).
+	win openInWorld.
+	^ true
