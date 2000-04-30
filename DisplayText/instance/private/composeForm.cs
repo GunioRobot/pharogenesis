1c808931f00d4499227c76f8a7609c5f93db40de
sma <unknown>
2000-04-30 09:28:00 +0000

    no message

--- DisplayText/instance/private/composeForm.cs
+++ DisplayText/instance/private/composeForm.cs
@@ -1,3 +1,12 @@
 composeForm
-
-	form _ self asParagraph asForm
+	| form1 |
+	Smalltalk isMorphic
+		ifTrue:
+			[form1 _ (TextMorph new contentsAsIs: text) imageFormDepth: 1.
+			form _ (ColorForm extent: form1 extent)
+				offset: offset;
+				colors: (Array
+					with: (backColor == nil ifTrue: [Color transparent] ifFalse: [backColor])
+					with: (foreColor == nil ifTrue: [Color black] ifFalse: [foreColor])).
+			form1 displayOn: form]
+		ifFalse: [form _ self asParagraph asForm]
