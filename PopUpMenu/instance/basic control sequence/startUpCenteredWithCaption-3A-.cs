fb806a49263bcb51217e034ef11a95484da6ac7f
sma <unknown>
2000-04-30 10:04:00 +0000

    no message

--- PopUpMenu/instance/basic control sequence/startUpCenteredWithCaption-3A-.cs
+++ PopUpMenu/instance/basic control sequence/startUpCenteredWithCaption-3A-.cs
@@ -2,4 +2,8 @@ startUpCenteredWithCaption: captionOrNil
 	"Differs from startUpWithCaption: by appearing with cursor in the menu,
 	and thus ready to act on mouseUp, without requiring user tweak to confirm"
 
-	^ self startUpWithCaption: captionOrNil at: Sensor cursorPoint - (frame width//2@0)
+	| cursorPoint |
+	cursorPoint _ Smalltalk isMorphic 
+			ifTrue: [World cursorPoint]
+			ifFalse: [Sensor cursorPoint].
+	^ self startUpWithCaption: captionOrNil at: cursorPoint - (20@0)
