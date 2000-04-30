8de8f1bee5dc17c3935e09287db71561c31a5661
sma <unknown>
2000-04-30 10:43:00 +0000

    no message

--- Morph/instance/initialization/openInWorld.cs
+++ Morph/instance/initialization/openInWorld.cs
@@ -1,4 +1,6 @@
 openInWorld
 	"Add this morph to the world.  If in MVC, then provide a Morphic window for it."
-	Smalltalk isMorphic ifFalse: [^ self openInMVC].
-	World addMorph: self
+
+	Smalltalk isMorphic
+		ifTrue: [self openInWorld: self currentWorld]
+		ifFalse: [self openInMVC]
