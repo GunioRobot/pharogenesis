cdce4e3f31dccd8335a813efe1f4cfb39bd2a960
sma <unknown>
2000-04-30 10:15:00 +0000

    no message

--- SyntaxError/class/instance creation/open-3A-.cs
+++ SyntaxError/class/instance creation/open-3A-.cs
@@ -1,7 +1,12 @@
 open: aSyntaxError
 	"Answer a standard system view whose model is an instance of me."
-
 	| topView |
+	<primitive: 19> "Simulation guard"
+	Smalltalk isMorphic
+		ifTrue:
+			[self buildMorphicViewOn: aSyntaxError.
+			Project current spawnNewProcessIfThisIsUI: Processor activeProcess.
+			^ Processor activeProcess suspend].
 	topView _ self buildMVCViewOn: aSyntaxError.
 	topView controller openNoTerminateDisplayAt: Display extent // 2.
 	Cursor normal show.
