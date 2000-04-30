aeb5ccb99a67d8275154f48fbfc6022759ffd486
sma <unknown>
2000-04-30 10:17:00 +0000

    no message

--- Utilities/class/user interface/informUser-3A-during-3A-.cs
+++ Utilities/class/user interface/informUser-3A-during-3A-.cs
@@ -2,12 +2,13 @@ informUser: aString during: aBlock
 	"Display a message above (or below if insufficient room) the cursor during execution of the given block."
 	"Utilities informUser: 'Just a sec!' during: [(Delay forSeconds: 1) wait]"
 
-	World ifNotNil: [
-		(MVCMenuMorph from: (SelectionMenu labels: '') title: aString)
-			displayAt: Sensor cursorPoint during: [aBlock value].
-		^ self].
+	Smalltalk isMorphic
+		ifTrue:
+			[(MVCMenuMorph from: (SelectionMenu labels: '') title: aString)
+				displayAt: Sensor cursorPoint during: [aBlock value].
+			^ self].
 
 	(SelectionMenu labels: '')
 		displayAt: Sensor cursorPoint
 		withCaption: aString
-		during: [aBlock value].
+		during: [aBlock value]
