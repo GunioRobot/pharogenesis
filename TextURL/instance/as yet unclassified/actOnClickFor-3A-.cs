1096a73f537ab898577b76ae61416204b3800ceb
sma <unknown>
2000-04-30 11:50:00 +0000

    no message

--- TextURL/instance/as yet unclassified/actOnClickFor-3A-.cs
+++ TextURL/instance/as yet unclassified/actOnClickFor-3A-.cs
@@ -1,10 +1,36 @@
 actOnClickFor: anObject
 	"Do what you can with this URL.  Later a web browser."
 
-	| response |
-	(url asLowercase endsWith: '.gif') ifTrue: [
-		HTTPSocket httpShowGif: url].	"opens a new window"
+	| response m |
+
+	"if it's a web browser, tell it to jump"
+	anObject isWebBrowser
+		ifTrue: [anObject jumpToUrl: url. ^ true]
+		ifFalse: [((anObject respondsTo: #model) and: [anObject model isWebBrowser])
+				ifTrue: [anObject model jumpToUrl: url. ^ true]].
+
+		"if it's a morph, see if it is contained in a web browser"
+		(anObject isKindOf: Morph) ifTrue: [
+			m _ anObject.
+			[ m ~= nil ] whileTrue: [
+				(m isWebBrowser) ifTrue: [
+					m  jumpToUrl: url.
+					^true ].
+				(m hasProperty: #webBrowserView) ifTrue: [
+					m model jumpToUrl: url.
+					^true ].
+				m _ m owner. ]
+		].
+
+	"no browser in sight.  ask if we should start a new browser"
+	(self confirm: 'open a browser to view this URL?') ifTrue: [
+		Scamper openOnUrl: url.
+		^ true ].
+
+	"couldn't display in a browser.  Offer to put up just the source"
+
 	response _ (PopUpMenu labels: 'View web page as source\Cancel' withCRs)
-		startUpWithCaption: 'Sorry, we don''t have a web browser yet'.
+		startUpWithCaption: 'Couldn''t find a web browser.  View
+page as source?'.
 	response = 1 ifTrue: [HTTPSocket httpShowPage: url].
 	^ true
