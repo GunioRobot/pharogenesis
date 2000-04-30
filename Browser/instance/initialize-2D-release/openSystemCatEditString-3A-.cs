0a3c29d90342ab690a88c73af39a3e6880000f4a
sma <unknown>
2000-04-30 09:21:00 +0000

    no message

--- Browser/instance/initialize-2D-release/openSystemCatEditString-3A-.cs
+++ Browser/instance/initialize-2D-release/openSystemCatEditString-3A-.cs
@@ -1,8 +1,8 @@
 openSystemCatEditString: aString
 	"Create a pluggable version of all the views for a Browser, including views and controllers.  The top list view is of the currently selected system class category--a single item list."
-	| systemCategoryListView classListView messageCategoryListView messageListView browserCodeView topView switchView |
+	| systemCategoryListView classListView messageCategoryListView messageListView browserCodeView topView switchView y annotationPane underPane optionalButtonsView |
 
-	World ifNotNil: [^ self openAsMorphSysCatEditing: aString].
+	Smalltalk isMorphic ifTrue: [^ self openAsMorphSysCatEditing: aString].
 
 	topView _ (StandardSystemView new) model: self.
 	topView borderWidth: 1.
@@ -12,7 +12,8 @@ openSystemCatEditString: aString
 		list: #systemCategorySingleton
 		selected: #indexIsOne 
 		changeSelected: #indexIsOne:
-		menu: #systemCategoryMenu:.
+		menu: #systemCatSingletonMenu:
+		keystroke: #systemCatSingletonKey:from:.
 	systemCategoryListView window: (0 @ 0 extent: 200 @ 12).
 	topView addSubView: systemCategoryListView.
 
@@ -20,7 +21,8 @@ openSystemCatEditString: aString
 		list: #classList
 		selected: #classListIndex
 		changeSelected: #classListIndex:
-		menu: #classListMenu:.
+		menu: #classListMenu:
+		keystroke: #classListKey:from:.
 	classListView window: (0 @ 0 extent: 67 @ 62).
 	topView addSubView: classListView below: systemCategoryListView.
 
@@ -46,14 +48,36 @@ openSystemCatEditString: aString
 		changeSelected: #messageListIndex:
 		menu: #messageListMenu:shifted:
 		keystroke: #messageListKey:from:.
+	messageListView menuTitleSelector: #messageListSelectorTitle.
 	messageListView window: (0 @ 0 extent: 67 @ 70).
 	topView addSubView: messageListView toRightOf: messageCategoryListView.
+
+	Preferences useAnnotationPanes
+		ifTrue: [
+			annotationPane _ PluggableTextView on: self
+				text: #annotation accept: nil
+				readSelection: nil menu: nil.
+			annotationPane window: (0@0 extent: 200@self optionalAnnotationHeight).
+			topView addSubView: annotationPane below: switchView.
+			y _ 110 - 12 - self optionalAnnotationHeight.
+			underPane _ annotationPane]
+		ifFalse: [
+			y _ 110 - 12.
+			underPane _ switchView].
+
+	Preferences optionalButtons ifTrue:
+		[optionalButtonsView _ self buildOptionalButtonsView.
+		optionalButtonsView borderWidth: 1.
+		topView addSubView: optionalButtonsView below: underPane.
+		underPane _ optionalButtonsView.
+		y _ y - self optionalButtonHeight].
 
 	browserCodeView _ PluggableTextView on: self 
 			text: #contents accept: #contents:notifying:
 			readSelection: #contentsSelection menu: #codePaneMenu:shifted:.
-	browserCodeView window: (0@0 extent: 200@(110-12)).
-	topView addSubView: browserCodeView below: switchView.
+	browserCodeView window: (0@0 extent: 200@y).
+	topView addSubView: browserCodeView below: underPane.
 	aString ifNotNil: [browserCodeView editString: aString.
 			browserCodeView hasUnacceptedEdits: true].
+	topView setUpdatablePanesFrom: #(classList messageCategoryList messageList).
 	^ topView
