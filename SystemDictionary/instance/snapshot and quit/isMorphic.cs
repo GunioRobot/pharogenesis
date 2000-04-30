2fe96bf8bda0f0dc3f4647488a85f5e4f0265a6d
sma <unknown>
2000-04-30 09:17:00 +0000

    no message

--- SystemDictionary/instance/snapshot and quit/isMorphic.cs
+++ SystemDictionary/instance/snapshot and quit/isMorphic.cs
@@ -1,7 +1,7 @@
 isMorphic
-	"Return true if the user interface is running in Morphic rathern than MVC.
-	By convention the gloabl variable World is set to nil when MVC is running.
-	ScheduledControllers could be set to nil when Morphic is running, but
-	this symmetry is not yet in effect."
+	"Answer true if the user interface is running in Morphic rathern than 
+	MVC.  By convention the gloabl variable World is set to nil when MVC is 
+	running.  ScheduledControllers could be set to nil when Morphic is 
+	running, but this symmetry is not yet in effect."
 
 	^ World ~~ nil
