0ba5e52530a355b1f815b3ece931cec4bbffa42b
sma <unknown>
2000-04-30 09:20:00 +0000

    no message

--- Browser/instance/initialize-2D-release/openMessageCatEditString-3A-.cs
+++ Browser/instance/initialize-2D-release/openMessageCatEditString-3A-.cs
@@ -1,8 +1,8 @@
 openMessageCatEditString: aString
 	"Create a pluggable version of the views for a Browser that just shows one message category."
-	| messageCategoryListView messageListView browserCodeView topView |
+	| messageCategoryListView messageListView browserCodeView topView annotationPane underPane y optionalButtonsView |
 
-	World ifNotNil: [^ self openAsMorphMsgCatEditing: aString].
+	Smalltalk isMorphic ifTrue: [^ self openAsMorphMsgCatEditing: aString].
 
 	topView _ (StandardSystemView new) model: self.
 	topView borderWidth: 1.
@@ -22,14 +22,36 @@ openMessageCatEditString: aString
 		changeSelected: #messageListIndex:
 		menu: #messageListMenu:shifted:
 		keystroke: #messageListKey:from:.
+	messageListView menuTitleSelector: #messageListSelectorTitle.
 	messageListView window: (0 @ 0 extent: 200 @ 70).
 	topView addSubView: messageListView below: messageCategoryListView.
+
+	Preferences useAnnotationPanes
+		ifTrue:
+			[annotationPane _ PluggableTextView on: self
+				text: #annotation accept: nil
+				readSelection: nil menu: nil.
+			annotationPane window: (0@0 extent: 200@self optionalAnnotationHeight).
+			topView addSubView: annotationPane below: messageListView.
+			underPane _ annotationPane.
+			y _ (200 - 12 - 70) - self optionalAnnotationHeight]
+		ifFalse:
+			[underPane _ messageListView.
+			y _ (200 - 12 - 70)].
+
+	Preferences optionalButtons ifTrue:
+		[optionalButtonsView _ self buildOptionalButtonsView.
+		optionalButtonsView borderWidth: 1.
+		topView addSubView: optionalButtonsView below: underPane.
+		underPane _ optionalButtonsView.
+		y _ y - self optionalButtonHeight].
 
 	browserCodeView _ PluggableTextView on: self 
 			text: #contents accept: #contents:notifying:
 			readSelection: #contentsSelection menu: #codePaneMenu:shifted:.
-	browserCodeView window: (0@0 extent: 200@(200-12-70)).
-	topView addSubView: browserCodeView below: messageListView.
+	browserCodeView window: (0@0 extent: 200@y).
+	topView addSubView: browserCodeView below: underPane.
 	aString ifNotNil: [browserCodeView editString: aString.
 			browserCodeView hasUnacceptedEdits: true].
+	topView setUpdatablePanesFrom: #(messageCatListSingleton messageList).
 	^ topView
