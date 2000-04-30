d6088be54e6b793bfd7404b47e5bed1468aa994c
sma <unknown>
2000-04-30 10:05:00 +0000

    no message

--- PopUpMenu/instance/basic control sequence/startUpWithCaption-3A-.cs
+++ PopUpMenu/instance/basic control sequence/startUpWithCaption-3A-.cs
@@ -1,4 +1,8 @@
 startUpWithCaption: captionOrNil
-	"Display the menu, slightly offset from the cursor, so that a slight tweak is required to confirm any action."
+	"Display the menu, slightly offset from the cursor,
+	so that a slight tweak is required to confirm any action."
 
-	^ self startUpWithCaption: captionOrNil at: Sensor cursorPoint
+	^ self startUpWithCaption: captionOrNil
+		at: (Smalltalk isMorphic 
+			ifTrue: [World cursorPoint]
+			ifFalse: [Sensor cursorPoint])
