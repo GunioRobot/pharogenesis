0bbf0dfec8b4158ab40557bb03c33141a2a7f121
sma <unknown>
2000-04-30 09:38:00 +0000

    no message

--- FileList/instance/file list menu/openImageInWindow.cs
+++ FileList/instance/file list menu/openImageInWindow.cs
@@ -1,6 +1,10 @@
 openImageInWindow
-	"Handle three file formats: GIF, Form stoteOn: (run coded), and BMP. Fail if file format is not recognized."
- 
-	| ff |
-	ff _ Form fromFile: (directory oldFileNamed: fileName).
-	FormView open: ff named: fileName.
+	"Handle five file formats: GIF, JPG, PNG, Form stoteOn: (run coded), and BMP.
+	Fail if file format is not recognized."
+
+	| image |
+	image _ Form fromFileNamed: self fullName.
+
+	Smalltalk isMorphic
+		ifTrue: [World addMorph: (SketchMorph withForm: image)]
+		ifFalse: [FormView open: image named: fileName]
