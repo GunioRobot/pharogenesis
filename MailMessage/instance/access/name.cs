d95806702722741f46841d9eb1595ffaff0e285c
ls <unknown>
2000-04-30 18:21:00 +0000

    no message

---
+++ MailMessage/instance/access/name.cs
@@ -0,0 +1,16 @@
+name
+	"return a default name for this part, if any was specified.  If not, return nil"
+	| type nameField disposition |
+
+	"try in the content-type: header"
+	type _ self fields at: 'content-type' ifAbsent: [nil].
+	(type notNil and: [(nameField _ type parameters at: 'name' ifAbsent: [nil]) notNil])
+		ifTrue: [^ nameField].
+
+	"try in content-disposition:"
+	disposition _ self fields at: 'content-disposition' ifAbsent: [nil].
+	(disposition notNil and: [(nameField _ disposition parameters at: 'filename' ifAbsent: [nil]) notNil])
+		ifTrue: [^ nameField].
+
+	"give up"
+	^ nil
