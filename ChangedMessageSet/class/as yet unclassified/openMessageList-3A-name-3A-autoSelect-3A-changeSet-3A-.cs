478443d60b17a0f56339b223d97a16715c8cafc0
sma <unknown>
2000-04-30 09:22:00 +0000

    no message

---
+++ ChangedMessageSet/class/as yet unclassified/openMessageList-3A-name-3A-autoSelect-3A-changeSet-3A-.cs
@@ -0,0 +1,8 @@
+openMessageList: messageList name: labelString autoSelect: autoSelectString changeSet: aChangeSet
+	| messageSet |
+	messageSet _ self messageList: messageList.
+	messageSet changeSet: aChangeSet.
+	messageSet autoSelectString: autoSelectString.
+	Smalltalk isMorphic
+		ifTrue: [self openAsMorph: messageSet name: labelString]
+		ifFalse: [ScheduledControllers scheduleActive:  (self open: messageSet name: labelString)]
