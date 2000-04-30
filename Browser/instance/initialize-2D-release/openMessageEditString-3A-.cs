0d39cb0524271b29c59f86195b67e57da9c866eb
sma <unknown>
2000-04-30 09:21:00 +0000

    no message

--- Browser/instance/initialize-2D-release/openMessageEditString-3A-.cs
+++ Browser/instance/initialize-2D-release/openMessageEditString-3A-.cs
@@ -1,8 +1,8 @@
 openMessageEditString: aString
 	"Create a pluggable version of the views for a Browser that just shows one message."
-	| messageListView browserCodeView topView |
+	| messageListView browserCodeView topView annotationPane underPane y |
 
-	World ifNotNil: [^ self openAsMorphMessageEditing: aString].
+	Smalltalk isMorphic ifTrue: [^ self openAsMorphMessageEditing: aString].
 
 	topView _ (StandardSystemView new) model: self.
 	topView borderWidth: 1.
@@ -16,11 +16,24 @@ openMessageEditString: aString
 	messageListView window: (0 @ 0 extent: 200 @ 12).
 	topView addSubView: messageListView.
 
+	Preferences useAnnotationPanes
+		ifTrue:
+			[annotationPane _ PluggableTextView on: self
+				text: #annotation accept: nil
+				readSelection: nil menu: nil.
+			annotationPane window: (0@0 extent: 200@self optionalAnnotationHeight).
+			topView addSubView: annotationPane below: messageListView.
+			underPane _ annotationPane.
+			y _ (200 - 12) - self optionalAnnotationHeight]
+		ifFalse:
+			[underPane _ messageListView.
+			y _ 200 - 12].
+
 	browserCodeView _ PluggableTextView on: self 
 			text: #contents accept: #contents:notifying:
 			readSelection: #contentsSelection menu: #codePaneMenu:shifted:.
-	browserCodeView window: (0@0 extent: 200@(200-12)).
-	topView addSubView: browserCodeView below: messageListView.
+	browserCodeView window: (0@0 extent: 200@y).
+	topView addSubView: browserCodeView below: underPane.
 	aString ifNotNil: [browserCodeView editString: aString.
 			browserCodeView hasUnacceptedEdits: true].
 	^ topView
