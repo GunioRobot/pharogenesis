75f2836244296adec3a0dcfa18e686ca9c4e2edf
sma <unknown>
2000-04-30 09:24:00 +0000

    no message

--- Debugger/instance/initialize/openNotifierContents-3A-label-3A-.cs
+++ Debugger/instance/initialize/openNotifierContents-3A-label-3A-.cs
@@ -1,21 +1,25 @@
 openNotifierContents: msgString label: label
-	"Create and schedule a notifier view with the given label and message. A notifier view shows just the message or the first several lines of the stack, with a menu that allows the user to open a full debugger if so desired. Do not terminate the current active process."
-	| msg topView p newActiveProcess |
+	"Create and schedule a notifier view with the given label and message. A notifier view shows just the message or the first several lines of the stack, with a menu that allows the user to open a full debugger if so desired."
+	"NOTE: When this method returns, a new process has been scheduled to run the windows, and thus this notifier, but the previous active porcess has not been suspended.  The sender will do this."
+	| msg topView p |
 	Sensor flushKeyboard.
-	(label beginsWith: 'Space is low')
-		ifTrue: [msg _ self lowSpaceChoices, msgString]
-		ifFalse: [msg _ msgString].
+	savedCursor _ Sensor currentCursor.
+	Sensor currentCursor: Cursor normal.
+	msg _ msgString.
+	(label beginsWith: 'Space is low') ifTrue: [msg _ self lowSpaceChoices, msgString].
+	isolationHead ifNotNil:
+		["We have already revoked the isolation layer -- now jump to the parent project."
+		msg _ self isolationRecoveryAdvice, msgString.
+		failedProject _ Project current.
+		isolationHead parent enterForEmergencyRecovery].
 
-	World ifNotNil:
-		[self buildMorphicNotifierLabelled: label message: msg.
-		newActiveProcess _
-			[[true] whileTrue: [World doOneCycle.  Processor yield]] newProcess
-					priority: Processor userSchedulingPriority.
-		^ newActiveProcess resume].
+	Smalltalk isMorphic
+		ifTrue:
+			[self buildMorphicNotifierLabelled: label message: msg.
+			^ Project current spawnNewProcessIfThisIsUI: interruptedProcess].
 
 	Display fullScreen.
-	Cursor normal show.
-	topView _ self buildMVCNotifierViewLabel: label message: msg minSize: 350@((14 * 5) + 16).
+	topView _ self buildMVCNotifierViewLabel: label message: msg minSize: 350@((14 * 5) + 16 + self optionalButtonHeight).
 	ScheduledControllers activeController
 		ifNil: [p _ Display boundingBox center]
 		ifNotNil: [p _ ScheduledControllers activeController view displayBox center].
