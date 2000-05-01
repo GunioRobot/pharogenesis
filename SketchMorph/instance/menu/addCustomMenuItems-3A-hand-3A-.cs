85d420264bc639ebe7a35ebf6044da320a4aefbc
jcg <unknown>
2000-05-01 23:12:00 +0000

    no message

--- SketchMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ SketchMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,4 +1,5 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph
-
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
+	aCustomMenu add: 'choose new graphic...' target: self action: #chooseNewGraphic.
+	aCustomMenu add: 'set as background' target: rotatedForm action: #setAsBackground.
 	self addPaintingItemsTo: aCustomMenu hand: aHandMorph
