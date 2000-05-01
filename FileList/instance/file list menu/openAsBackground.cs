d8ad0aa58e2ea46fec742b99a16e9c643d900132
jcg <unknown>
2000-05-01 23:09:00 +0000

    no message

---
+++ FileList/instance/file list menu/openAsBackground.cs
@@ -0,0 +1,5 @@
+openAsBackground
+	"Set an image as a background image.  Support Squeak's common file format 
+	(GIF, JPG, PNG, 'Form stoteOn: (run coded)' and BMP)"
+
+	(Form fromFileNamed: self fullName) setAsBackground
