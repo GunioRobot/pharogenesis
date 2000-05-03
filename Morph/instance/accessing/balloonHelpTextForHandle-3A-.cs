290fb219a7d4419c6cd9be20b6559558f7768562
sma <unknown>
2000-05-03 23:14:00 +0000

    no message

--- Morph/instance/accessing/balloonHelpTextForHandle-3A-.cs
+++ Morph/instance/accessing/balloonHelpTextForHandle-3A-.cs
@@ -1,26 +1,37 @@
 balloonHelpTextForHandle: aHandle
-	| colorName |
-	colorName _ aHandle color name.
-	#(	(blue					'Rotate')
-		(yellow					'Change size') 
-		(green					'Make another')
-		(black					'Pick up')
-		(red						'Menu')
-		(lightBlue				'Help')
-		(lightBrown				'Make a Tile')
-		(lightGray				'Repaint')
-		(cyan					'View me')
-		(transparent			'Remove')
-		(lightGray				'Repaint')
-		(lightOrange			'Change scale') 
-		(veryVeryLightGray		'Paint new object'))
+	|  itsSelector |
+	itsSelector _ aHandle eventHandler firstMouseSelector.
+	#(	(addFullHandles							'More halo handles')
+		(addSimpleHandles						'Fewer halo handles')
+		(chooseEmphasisOrAlignment				'Emphasis & alignment')
+		(chooseFont								'Change font')
+		(chooseStyle								'Change style')
+		(dismiss									'Remove')
+		(doDebug:with:							'Debug')
+		(doDirection:with:						'Choose forward direction')
+		(doDup:with:							'Duplicate')
+		(doMenu:with:							'Menu')
+		(doGrab:with:							'Pick up')
+		(doRecolor:with:							'Change color')
+		(editDrawing							'Repaint')
+		(maybeDoDup:with:						'Duplicate')
+		(makeNascentScript						'Make a scratch script')
+		(makeNewDrawingWithin				'Paint new object')
+		(mouseDownInDimissHandle:with:			'Move to trash')
+		(mouseDownInCollapseHandle:with:		'Collapse morph')
+		(mouseDownOnHelpHandle:				'Help')
+		(openViewerForArgument				'Open a Viewer for me')
+		(paintBackground						'Paint background')
+		(prepareToTrackCenterOfRotation:with:	'Set center of rotation')
+		(startDrag:with:							'Move')
+		(startGrow:with:							'Change size') 
+		(startRot:with:							'Rotate')
+		(startScale:with:							'Change scale') 
+		(tearOffTile								'Make a tile representing this object')
+		(trackCenterOfRotation:with:				'Set center of rotation')) 
+
 
 	do:
-		[:pair | colorName == pair first ifTrue: [^ pair last]].
+		[:pair | itsSelector == pair first ifTrue: [^ pair last]].
 
-	^ 'If you click on the ' , aHandle color name , ' handle,
-it will ' , (#('probably not do anything.'
-		'let you change the font'
-		'let you change the style'
-		'let you change the emphasis')
-	at: (#(none  lightGreen lightRed lightBrown ) indexOf: colorName ifAbsent: [1]))
+	^ 'unknown halo handle'
