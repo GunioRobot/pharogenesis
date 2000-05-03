a6cbb87bd2d584cc28a78f396393f9e34706f164
sma <unknown>
2000-05-03 22:55:00 +0000

    no message

---
+++ Preferences/class/halos/iconicHaloSpecifications.cs
@@ -0,0 +1,28 @@
+iconicHaloSpecifications
+
+	"Preferences resetHaloSpecifications"
+
+	^ #(
+	"  	selector				horiz		vert			color info						icon key
+		---------				------		-----------		-------------------------------		---------------"
+	(addCollapseHandle:		left			topCenter		(tan)							'Halo-Collapse')
+	(addPoohHandle:			right		center			(white)							'Halo-Pooh')
+	(addDebugHandle:		right		topCenter		(blue	veryMuchLighter)		'Halo-Debug')
+	(addDismissHandle:		left			top				(red		muchLighter)			'Halo-Dismiss')
+	(addRotateHandle:		left			bottom			(blue)							'Halo-Rot')
+	(addMenuHandle:		leftCenter	top				(red)							'Halo-Menu')
+	(addTileHandle:			left			bottomCenter	(lightBrown)					'Halo-Tile')
+	(addViewHandle:			left			center			(cyan)							'Halo-View')
+	(addGrabHandle:			center		top				(black)							'Halo-Grab')
+	(addDragHandle:			rightCenter	top				(brown)							'Halo-Drag')
+	(addDupHandle:			right		top				(green)							'Halo-Dup')	
+	(addHelpHandle:			center		bottom			(lightBlue)						'Halo-Help')
+	(addGrowHandle:		right		bottom			(yellow)						'Halo-Scale')
+	(addScaleHandle:		right		bottom			(lightOrange)					'Halo-Scale')
+	(addScriptHandle:		right		bottomCenter	(green muchLighter)			'Halo-Script')
+	(addPaintBgdHandle:		right		center			(lightGray)						'Halo-Paint')
+	(addRepaintHandle:		right		center			(lightGray)						'Halo-Paint')
+	(addFontSizeHandle:		leftCenter	bottom			(lightGreen)						'Halo-FontSize')
+	(addFontStyleHandle:		center		bottom			(lightRed)						'Halo-FontStyle')
+	(addFontEmphHandle:	rightCenter	bottom			(lightBrown darker)				'Halo-FontEmph')
+		) 
