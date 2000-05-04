d5780279083985506e0811725bb55e4fcab4ab74
sw <unknown>
2000-05-04 20:22:00 +0000

    no message

--- Browser/instance/accessing/contents.cs
+++ Browser/instance/accessing/contents.cs
@@ -2,20 +2,27 @@ contents
 	"Depending on the current selection, different information is retrieved.
 	Answer a string description of that information. This information is the
 	method of the currently selected class and message."
-	| comment theClass |
+	| comment theClass latestCompiledMethod |
+	latestCompiledMethod _ currentCompiledMethod.
+	currentCompiledMethod _ nil.
+
 	editSelection == #none ifTrue: [^ ''].
 	editSelection == #editSystemCategories 
 		ifTrue: [^ systemOrganizer printString].
 	editSelection == #newClass 
-		ifTrue: [^ Class template: self selectedSystemCategoryName].
+		ifTrue: [^ (theClass _ self selectedClassOrMetaClass)
+			ifNil:
+				[Class template: self selectedSystemCategoryName]
+			ifNotNil:
+				[Class templateForSubclassOf: theClass category: self selectedSystemCategoryName]].
 	editSelection == #editClass 
 		ifTrue: [^ self selectedClassOrMetaClass definition].
 	editSelection == #editComment 
 		ifTrue: [(theClass _ self selectedClass) ifNil: [^ ''].
 				comment _ theClass comment.
 				comment size = 0
-				ifTrue: [ ^ 'This class has not yet been commented.']
-				ifFalse: [ ^ comment]].
+				ifTrue: [^ 'This class has not yet been commented.']
+				ifFalse: [^ comment]].
 	editSelection == #hierarchy 
 		ifTrue: [^ self selectedClassOrMetaClass printHierarchy].
 	editSelection == #editMessageCategories 
@@ -23,9 +30,11 @@ contents
 	editSelection == #newMessage
 		ifTrue: [^ self selectedClassOrMetaClass sourceCodeTemplate].
 	editSelection == #editMessage
-		ifTrue: [^ self selectedMessage].
-	editSelection == #byteCodes ifTrue: [
-		^ (self selectedClassOrMetaClass compiledMethodAt: 
-				self selectedMessageName) symbolic asText].
+		ifTrue:
+			[currentCompiledMethod _ latestCompiledMethod.
+			^ self selectedMessage].
+	editSelection == #byteCodes ifTrue:
+		[^ (self selectedClassOrMetaClass compiledMethodAt: self selectedMessageName)
+			symbolic asText].
 
 	self error: 'Browser internal error: unknown edit selection.'
