2fb31f864c0d1807eb87f7ee41cc98414d0fc96a
sw <unknown>
2000-05-04 13:45:00 +0000

    no message

---
+++ ParagraphEditor/instance/menu messages/browseClassFromIt.cs
@@ -0,0 +1,11 @@
+browseClassFromIt
+	"Launch a hierarchy browser for the class indicated by the current selection.  If multiple classes matching the selection exist, let the user choose among them."
+
+	| aClass |
+	self lineSelectAndEmptyCheck: [^ self].
+
+	aClass _ Utilities classFromPattern: (self selection string copyWithout: Character cr) withCaption: 'choose a class to browse...'.
+	aClass ifNil: [^ view flash].
+
+	self terminateAndInitializeAround:
+		[Utilities spawnHierarchyForClass: aClass selector: nil]
