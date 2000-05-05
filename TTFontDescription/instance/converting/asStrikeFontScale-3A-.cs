33d6992d3c9d3f62de6ff445ccdf131ba0ecca9a
sma <unknown>
2000-05-05 13:46:00 +0000

    no message

---
+++ TTFontDescription/instance/converting/asStrikeFontScale-3A-.cs
@@ -0,0 +1,14 @@
+asStrikeFontScale: scale
+	"Generate a StrikeFont (actually a FormSetFont) for this TTF font at a given scale."
+
+	| forms |
+	forms _ (0 to: 255) collect:
+		[:i |
+		(self at: i)
+			asFormWithScale: scale
+			ascender: ascender
+			descender: descender].
+	^ FormSetFont new
+		fromFormArray: forms
+		asciiStart: 0
+		ascent: (ascender * scale) rounded
