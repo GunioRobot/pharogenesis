3b9fed32d7fdb789e5b7bbd0a5bd40a74cd54a0b
sma <unknown>
2000-05-05 13:48:00 +0000

    no message

---
+++ TTGlyph/instance/converting/asFormWithScale-3A-ascender-3A-descender-3A-.cs
@@ -0,0 +1,7 @@
+asFormWithScale: scale ascender: ascender descender: descender
+	^ self
+		asFormWithScale: scale
+		ascender: ascender
+		descender: descender
+		fgColor: Color black
+		bgColor: Color white
