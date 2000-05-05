38a5ef3c85143aba1383802554f27e3b03e90cbd
mir <unknown>
2000-05-05 15:26:00 +0000

    no message

--- Browser/instance/initialize-2D-release/openAsMorphMessageEditing-3A-.cs
+++ Browser/instance/initialize-2D-release/openAsMorphMessageEditing-3A-.cs
@@ -1,18 +1,36 @@
 openAsMorphMessageEditing: editString
-	"Create a pluggable version a Browser on just a messageCategory."
-	| window codePane |
+	"Create a pluggable version a Browser that shows just one message"
+	| window codePane baseline aTextMorph |
 	window _ (SystemWindow labelled: 'later') model: self.
 
-	window addMorph: (PluggableListMorph on: self list: #messageListSingleton
+	window addMorph: ((PluggableListMorph on: self list: #messageListSingleton
 			selected: #indexIsOne changeSelected: #indexIsOne:
-			menu: #messageListMenu:shifted:)
+			menu: #messageListMenu:shifted:
+			keystroke: #messageListKey:from:) enableDragNDrop: Preferences browseWithDragNDrop)
 		frame: (0@0 extent: 1.0@0.06).
+
+	Preferences useAnnotationPanes
+		ifFalse:
+			[baseline _ 0.06]
+		ifTrue:
+			[aTextMorph _ PluggableTextMorph on: self
+					text: #annotation accept: nil
+					readSelection: nil menu: nil.
+			aTextMorph askBeforeDiscardingEdits: false.
+			window addMorph: aTextMorph
+				frame: (0@0.06 corner: 1@0.11).
+			baseline _ 0.11].
+
+	Preferences optionalButtons
+		ifTrue:
+			[window addMorph: self optionalButtonRow frame: ((0@baseline corner: 1 @ (baseline + 0.08))).
+			baseline _ baseline + 0.08].
 
 	codePane _ PluggableTextMorph on: self text: #contents accept: #contents:notifying:
 			readSelection: #contentsSelection menu: #codePaneMenu:shifted:.
 	editString ifNotNil: [codePane editString: editString.
 					codePane hasUnacceptedEdits: true].
 	window addMorph: codePane
-		frame: (0@0.06 corner: 1@1).
+		frame: (0@baseline corner: 1@1).
 
 	^ window
