6eb2e357001a742d3edaee2332197e55b14260d2
sma <unknown>
2000-05-06 18:08:00 +0000

    no message

---
+++ FileContentsBrowser/instance/diffs/methodDiffFor-3A-class-3A-selector-3A-meta-3A-.cs
@@ -0,0 +1,8 @@
+methodDiffFor: aString class: aPseudoClass selector: selector meta: meta 
+	| theClass source |
+	theClass _ Smalltalk at: aPseudoClass name ifAbsent: [^ aString copy].
+	meta ifTrue: [theClass _ theClass class].
+	(theClass includesSelector: selector)
+		ifFalse: [^ aString copy].
+	source _ theClass sourceCodeAt: selector.
+	^ Cursor wait showWhile: [TextDiffBuilder buildDisplayPatchFrom: source to: aString]
