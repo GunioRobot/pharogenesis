5c9925c4aac28285bfeca20259c7542fe79f57af
sw <unknown>
2000-05-06 02:53:00 +0000

    no message

--- AlignmentMorph/instance/private/resizeIfNeeded.cs
+++ AlignmentMorph/instance/private/resizeIfNeeded.cs
@@ -5,16 +5,20 @@ resizeIfNeeded
 	newWidth _ bounds width.
 	newHeight _ bounds height.
 
-	(owner == nil or: [owner isAlignmentMorph not]) ifTrue: [
+	(owner == nil or: [owner isAlignmentMorph not]) ifTrue:
 		"if spaceFill and not in a LayoutMorph, grow to enclose submorphs"
-		hResizing = #spaceFill ifTrue: [newWidth _ self minWidth max: self bounds width].
-		vResizing = #spaceFill ifTrue: [newHeight _ self minHeight max: self bounds height]].
+		[hResizing = #spaceFill ifTrue:
+			[newWidth _ self minWidth max: self bounds width.
+			owner ifNotNil:
+				[(self hasProperty: #clipToOwnerWidth) ifTrue:
+					[newWidth _ newWidth min: (owner right - bounds left)]]].
+		vResizing = #spaceFill ifTrue:
+			[newHeight _ self minHeight max: self bounds height]].
 
 	"if shrinkWrap, adjust size to just fit around submorphs"
 	hResizing = #shrinkWrap ifTrue: [newWidth _ self minWidth].
 	vResizing = #shrinkWrap ifTrue: [newHeight _ self minHeight].
 
-	((newWidth ~= bounds width) or: [newHeight ~= bounds height]) ifTrue: [
-		"bounds really changed; flush fullBounds cache and fix submorph layouts"
-		bounds _ bounds origin extent: newWidth@newHeight.
-		fullBounds _ nil].
+	((newWidth ~= bounds width) or: [newHeight ~= bounds height])
+		ifTrue: ["bounds really changed"
+				bounds _ bounds origin extent: newWidth@newHeight].
