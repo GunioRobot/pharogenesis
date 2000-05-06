1657bb777e106c0dc545da060a54ec809a757f84
sma <unknown>
2000-05-06 19:58:00 +0000

    no message

--- Collection/class/instance creation/with-3A-.cs
+++ Collection/class/instance creation/with-3A-.cs
@@ -1,7 +1,6 @@
 with: anObject 
 	"Answer an instance of me containing anObject."
 
-	| newCollection |
-	newCollection _ self new.
-	newCollection add: anObject.
-	^newCollection
+	^ self new
+		add: anObject;
+		yourself
