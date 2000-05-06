476251ae7c08d43d33c622798245e79fa33c48ae
bf <unknown>
2000-05-06 12:27:00 +0000

    no message

---
+++ PasteUpMorph/instance/display/setGradientColor-3A-.cs
@@ -0,0 +1,8 @@
+setGradientColor: evt
+	"For backwards compatibility with GradientFillMorph"
+
+	self flag: #fixThis.
+	evt hand changeColorTarget: self selector: #gradientFillColor:
+		originalColor: (self fillStyle isGradientFill
+			ifTrue: [self fillStyle colorRamp last value]
+			ifFalse: [color]).
