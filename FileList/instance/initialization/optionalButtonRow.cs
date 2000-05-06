48795a4670468ef48c44b4bbd68b36cb0e581851
sw <unknown>
2000-05-06 02:54:00 +0000

    no message

---
+++ FileList/instance/initialization/optionalButtonRow.cs
@@ -0,0 +1,24 @@
+optionalButtonRow
+	| aRow aButton |
+	aRow _ AlignmentMorph newRow beSticky.
+	aRow setProperty: #clipToOwnerWidth toValue: true.
+	aRow addTransparentSpacerOfSize: (5@0).
+	self optionalButtonSpecs do:
+			[:spec |
+				aButton _ PluggableButtonMorph
+					on: self
+					getState: nil
+					action: spec second.
+				aButton useRoundedCorners;
+					label: spec first asString;
+					askBeforeChanging: true;
+					onColor: Color transparent offColor: Color transparent.
+				aRow addMorphBack: aButton.
+				aRow addTransparentSpacerOfSize: (3 @ 0).
+				aButton setBalloonText: spec fourth.
+				aRow addTransparentSpacerOfSize: (3 @ 0).
+
+				(spec second == #sortBySize)
+					ifTrue:
+						[aRow addTransparentSpacerOfSize: (4@0)]].
+	^ aRow
