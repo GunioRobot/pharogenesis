83a6ca3be5a320769ed6c4e72a2d65935a5d917a
sma <unknown>
2000-05-06 20:01:00 +0000

    no message

--- Collection/class/instance creation/with-3A-with-3A-.cs
+++ Collection/class/instance creation/with-3A-with-3A-.cs
@@ -1,8 +1,7 @@
 with: firstObject with: secondObject 
 	"Answer an instance of me containing the two arguments as elements."
 
-	| newCollection |
-	newCollection _ self new.
-	newCollection add: firstObject.
-	newCollection add: secondObject.
-	^newCollection
+	^ self new
+		add: firstObject;
+		add: secondObject;
+		yourself
