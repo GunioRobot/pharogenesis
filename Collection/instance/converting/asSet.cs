24e50a1a91a1f5e5bc9b9bfbc3a9097f401096e8
sma <unknown>
2000-05-06 20:29:00 +0000

    no message

--- Collection/instance/converting/asSet.cs
+++ Collection/instance/converting/asSet.cs
@@ -1,7 +1,4 @@
 asSet
 	"Answer a Set whose elements are the unique elements of the receiver."
 
-	| aSet |
-	aSet _ Set new: self size.
-	self do: [:each | aSet add: each].
-	^aSet
+	^ Set withAll: self
