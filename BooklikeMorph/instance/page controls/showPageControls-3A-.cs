9c651fd91ae117b0100d91a061bfe734fe444849
bf <unknown>
2000-05-07 11:35:00 +0000

    no message

---
+++ BooklikeMorph/instance/page controls/showPageControls-3A-.cs
@@ -0,0 +1,16 @@
+showPageControls: controlSpecs 
+	| spacer pageControls anIndex |
+	self hidePageControls.
+	anIndex _ (submorphs size > 0 and: [submorphs first hasProperty: #header])
+		ifTrue:	[2]
+		ifFalse:	[1].
+	spacer _ Morph new color: color; extent: 0@10.
+	spacer  setProperty: #pageControl toValue: true.
+	self privateAddMorph: spacer atIndex: anIndex.
+
+	pageControls _ self makePageControlsFrom: controlSpecs.
+	pageControls borderWidth: 0; inset: 4.
+	pageControls  setProperty: #pageControl toValue: true.
+	pageControls setNameTo: 'Page Controls'.
+	pageControls eventHandler: (EventHandler new on: #mouseDown send: #move to: self).
+	self privateAddMorph:  pageControls beSticky atIndex: anIndex
