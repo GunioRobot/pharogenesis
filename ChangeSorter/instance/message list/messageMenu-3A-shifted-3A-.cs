5c464b338b711e9e4a5bfcb3d1fa91cedad746a6
sw <unknown>
2000-05-08 13:01:00 +0000

    no message

--- ChangeSorter/instance/message list/messageMenu-3A-shifted-3A-.cs
+++ ChangeSorter/instance/message list/messageMenu-3A-shifted-3A-.cs
@@ -1,30 +1,28 @@
 messageMenu: aMenu shifted: shifted
 	"Could be for a single or double changeSorter"
 	shifted ifTrue: [^ self shiftedMessageMenu: aMenu].
-	parent ifNotNil: [
-		^ aMenu labels: 
-'copy method to other side
-delete method from change set
-remove method from system
-browse full
-fileOut
-printOut
-senders of...
-implementors of...
-versions
-more...'
-		lines: #(1 3 6 9 )
-		selections: #(copyMethodToOther forget removeMessage browseMethodFull fileOutMessage printOutMessage browseSendersOfMessages browseMessages browseVersions shiftedYellowButtonActivity )]
 
-	ifNil: [^ aMenu labels: 
-'delete method from change set
-remove method from system
-browse full
-fileOut
-printOut
-senders of...
-implementors of...
-versions
-more...'
-		lines: #(2 5 8 )
-		selections: #( forget removeMessage browseMethodFull fileOutMessage printOutMessage browseSendersOfMessages browseMessages browseVersions shiftedYellowButtonActivity )]
+	parent ifNotNil:
+		[aMenu addList: #(
+			('copy method to other side'			copyMethodToOther)
+			('move method to other side'			moveMethodToOther))].
+
+	aMenu addList: #(
+			('delete method from change set'		forget)
+			-
+			('remove method from system (x)'	removeMessage)
+				-
+			('browse full (b)'					browseMethodFull)
+			('browse hierarchy (h)'				spawnHierarchy)
+			('browse method (O)'				openSingleMessageBrowser)
+			-
+			('fileOut'							fileOutMessage)
+			('printOut'							printOutMessage)
+			-
+			('senders of... (n)'					browseSendersOfMessages)
+			('implementors of... (m)'				browseMessages)
+			('inheritance (i)'					methodHierarchy)
+			('versions (v)'						browseVersions)
+			-
+			('more...'							shiftedYellowButtonActivity)).
+	^ aMenu
