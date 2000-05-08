9dabd47b590ad8ef2d7d625ec56377154b321297
di <unknown>
2000-05-08 10:02:00 +0000

    no message

--- SystemWindow/instance/open-2F-close/openInWorldExtent-3A-.cs
+++ SystemWindow/instance/open-2F-close/openInWorldExtent-3A-.cs
@@ -1,6 +1,4 @@
 openInWorldExtent: extent
 	"This msg and its callees result in the window being activeOnlyOnTop"
 	Smalltalk isMorphic ifFalse: [^ self openInMVCExtent: extent].
-	self position: (RealEstateAgent initialFrameFor: self) topLeft; extent: extent.
-	World addMorph: self.
-	self activate
+	self openInWorld: World extent: extent
