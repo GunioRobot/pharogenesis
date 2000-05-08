28f5877b0aaeaf75842aec6613f17808afa4efdf
sw <unknown>
2000-05-08 03:02:00 +0000

    no message

--- Debugger/instance/context stack menu/contextStackMenu-3A-shifted-3A-.cs
+++ Debugger/instance/context stack menu/contextStackMenu-3A-shifted-3A-.cs
@@ -6,39 +6,44 @@ proceed (p)
 step (t)
 send (e)
 where (w)
-senders of...
-implementors of...
-method inheritance
-versions
+peel to first like this
+senders of... (n)
+implementors of... (m)
+inheritance (i)
+versions (v)
 inst var refs...
 inst var defs...
 class var refs...
 class variables
-class refs
-browse full
+class refs (N)
+browse full (b)
+file out 
 more...'
-	lines: #(6 10 12 15)
-	selections: #(fullStack restart proceed step send where
+	lines: #(7 11 13 16 18)
+	selections: #(fullStack restart proceed doStep send where peelToFirst
 browseSendersOfMessages browseMessages methodHierarchy browseVersions
 browseInstVarRefs browseInstVarDefs
 browseClassVarRefs browseClassVariables browseClassRefs
-browseMethodFull
+browseMethodFull fileOutMessage
 shiftedYellowButtonActivity)]
 
 	ifTrue: [aMenu labels: 
 'browse class hierarchy
 browse class
-browse method
+browse method (O)
 implementors of sent messages
 change sets with this method
 inspect instances
 inspect subinstances
+revert to previous version
 remove from current change set
+revert and forget
 more...' 
-	lines: #(5 7 9)
+	lines: #(5 7 10)
 	selections: #(classHierarchy browseClass 
-		buildMessageBrowser browseAllMessages findMethodInChangeSets 
+		openSingleMessageBrowser browseAllMessages findMethodInChangeSets 
 		inspectInstances inspectSubInstances
-		removeFromCurrentChanges
+		revertToPreviousVersion 
+		removeFromCurrentChanges revertAndForget
 		unshiftedYellowButtonActivity)]
 
