28e6e13ac5a0e34fea908e937b944aa35c15427d
sw <unknown>
2000-05-08 02:59:00 +0000

    no message

--- StringHolder/instance/message list menu/messageListKey-3A-from-3A-.cs
+++ StringHolder/instance/message list menu/messageListKey-3A-from-3A-.cs
@@ -1,11 +1,21 @@
 messageListKey: aChar from: view
-	"Respond to a Command key.  I am a model with a code pane, and I also have a listView that has a list of methods.  The view knows how to get the list and selection."
+	"Respond to a Command key.  I am a model with a code pane, and I also
+	have a listView that has a list of methods.  The view knows how to get
+	the list and selection."
 
 	| sel class |
-	(sel _ self selectedMessageName) ifNil: [^ self arrowKey: aChar from: view].
-	aChar == $m ifTrue: [Smalltalk browseAllImplementorsOf: sel].
-	aChar == $n ifTrue: [Smalltalk browseAllCallsOn: sel].
-	aChar == $b ifTrue: [
-		(class _ self selectedClass) ifNotNil: [
-			Browser fullOnClass: class selector: sel]].
+	(class _ self selectedClassOrMetaClass) ifNil: [^ self arrowKey: aChar from: view].
+	sel _ self selectedMessageName.
+	aChar == $b ifTrue: [^ Browser fullOnClass: class selector: sel].
+	aChar == $N ifTrue: [^ self browseClassRefs].
+	aChar == $i ifTrue: [^ self methodHierarchy].
+	aChar == $h ifTrue: [^ self classHierarchy].
+
+	sel ifNotNil: 
+		[aChar == $m ifTrue: [^ Smalltalk browseAllImplementorsOf: sel].
+		aChar == $n ifTrue: [^ Smalltalk browseAllCallsOn: sel].
+		aChar == $v ifTrue: [^ self browseVersions].
+		aChar == $O ifTrue: [^ self openSingleMessageBrowser].
+		aChar == $x ifTrue: [^ self removeMessage]].
+
 	^ self arrowKey: aChar from: view
