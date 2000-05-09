fefba094cd598637ce3078eb6b92820ae6c4f2c6
ar <unknown>
2000-05-09 14:50:00 +0000

    no message

--- CCodeGenerator/instance/C code generator/emitCVariablesOn-3A-.cs
+++ CCodeGenerator/instance/C code generator/emitCVariablesOn-3A-.cs
@@ -1,13 +1,16 @@
 emitCVariablesOn: aStream
 	"Store the global variable declarations on the given stream."
-
+	| varString |
 	aStream nextPutAll: '/*** Variables ***/'; cr.
 	variables asSortedCollection do: [ :var |
-		(variableDeclarations includesKey: var) ifTrue: [
-			aStream nextPutAll: (variableDeclarations at: var), ';'; cr.
+		(self isGeneratingPluginCode and:[self isTranslatingLocally])
+			ifTrue:[aStream nextPutAll:'static '].
+		varString _ var asString.
+		(variableDeclarations includesKey: varString) ifTrue: [
+			aStream nextPutAll: (variableDeclarations at: varString), ';'; cr.
 		] ifFalse: [
 			"default variable declaration"
-			aStream nextPutAll: 'int ', var, ';'; cr.
+			aStream nextPutAll: 'int ', varString, ';'; cr.
 		].
 	].
 	aStream cr.
