534f5f8954755a2fdf540df2541c8c39ffd5788c
sw <unknown>
2000-05-09 02:56:00 +0000

    no message

---
+++ Morph/instance/dropping-2F-grabbing/willingToBeEmbeddedUponLanding.cs
@@ -0,0 +1,4 @@
+willingToBeEmbeddedUponLanding
+	"Answer whether the receiver, when dropped over a container that is open to drag-and-drop, is eager to be embedded in the it.  SystemWindows and MenuMorphs are examples of morphs that are reluctant in this regard."
+	
+	^ true
