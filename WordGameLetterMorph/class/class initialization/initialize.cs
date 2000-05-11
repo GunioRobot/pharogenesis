6802c5503add58298c7d23fd8292869d61af345b
di <unknown>
2000-05-11 07:15:00 +0000

    no message

---
+++ WordGameLetterMorph/class/class initialization/initialize.cs
@@ -0,0 +1,7 @@
+initialize  "WordGameLetterMorph initialize"
+
+	IDFont _ StrikeFont familyName: 'ComicPlain' size: 13.
+	IDHeight _ IDFont height.
+	LetterFont _ StrikeFont familyName: 'ComicBold' size: 19.
+	LetterHeight _ LetterFont height.
+
