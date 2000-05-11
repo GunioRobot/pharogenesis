fa7efc29929928c369d9105149ddaf0e60035984
ar <unknown>
2000-05-11 22:11:00 +0000

    no message

---
+++ FilePluginSimulator/instance/as yet unclassified/makeDirEntryName-3A-size-3A-createDate-3A-modDate-3A-isDir-3A-fileSize-3A-.cs
@@ -0,0 +1,8 @@
+makeDirEntryName: entryName size: entryNameSize
+	createDate: createDate modDate: modifiedDate
+	isDir: dirFlag fileSize: fileSize
+
+	^interpreterProxy
+		makeDirEntryName: entryName size: entryNameSize
+		createDate: createDate modDate: modifiedDate
+		isDir: dirFlag fileSize: fileSize
