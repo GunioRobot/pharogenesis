e4ba50655afc3df6d95e724d17b90c79cd4b8f0a
ar <unknown>
2000-05-11 22:23:00 +0000

    no message

---
+++ SoundPlugin/class/translation/headerFile.cs
@@ -0,0 +1,22 @@
+headerFile
+^'/* SoundPlugin.h - header for Sound Plugins :-) tim@sumeru.stanford.edu */
+
+/* module initialization/shutdown */
+int soundInit(void);
+int soundShutdown(void);
+
+/* sound output */
+int snd_AvailableSpace(void);
+int snd_InsertSamplesFromLeadTime(int frameCount, int srcBufPtr, int samplesOfLeadTime);
+int snd_PlaySamplesFromAtLength(int frameCount, int arrayIndex, int startIndex);
+int snd_PlaySilence(void);
+int snd_Start(int frameCount, int samplesPerSec, int stereo, int semaIndex);
+int snd_Stop(void);
+
+/* sound input */
+int snd_SetRecordLevel(int level);
+int snd_StartRecording(int desiredSamplesPerSec, int stereo, int semaIndex);
+int snd_StopRecording(void);
+double snd_GetRecordingSampleRate(void);
+int snd_RecordSamplesIntoAtLength(int buf, int startSliceIndex, int bufferSizeInBytes);
+'
