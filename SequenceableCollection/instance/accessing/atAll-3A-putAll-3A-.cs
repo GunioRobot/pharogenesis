db3e1ee6ef6af6a6a6d37e90a5e12e1c1fec608c
sma <unknown>
2000-05-12 12:18:00 +0000

    no message

--- SequenceableCollection/instance/accessing/atAll-3A-putAll-3A-.cs
+++ SequenceableCollection/instance/accessing/atAll-3A-putAll-3A-.cs
@@ -1,5 +1,6 @@
 atAll: indexArray putAll: valueArray
 	"Store the elements of valueArray into the slots
 	of this collection selected by indexArray."
-	indexArray with: valueArray do:
-		[:i :x | self at: i put: x]
+
+	indexArray with: valueArray do: [:index :value | self at: index put: value].
+	^ valueArray
