fceb033f2964c970c74d3574ba26efb2c8026f62
sma <unknown>
2000-05-12 12:36:00 +0000

    no message

--- SequenceableCollection/instance/accessing/atRandom.cs
+++ SequenceableCollection/instance/accessing/atRandom.cs
@@ -1,11 +1,13 @@
 atRandom
-	"Return a random element of myself.  Uses a shared random number generator owned by class Collection.  If you use this a lot, define your own instance of Random and use atRandom:.  Causes an error if self has no elements."
+	"Answer a random element of the receiver.  Uses a shared random 
+	number generator owned by class Collection.  If you use this a lot, 
+	define your own instance of Random and use #atRandom:.  Causes 
+	an error if self has no elements."
 
-	| index |
-	index _ (RandomForPicking next * self size) asInteger + 1.
-	^ self at: index
+	^ self atRandom: Collection randomForPicking
 
-"  #('one' 'or' 'the' 'other') atRandom
-   (1 to: 10) atRandom
-   'Just pick one of these letters at random' atRandom
+"Examples:
+	#('one' 'or' 'the' 'other') atRandom
+	(1 to: 10) atRandom
+	'Just pick one of these letters at random' atRandom
 "
