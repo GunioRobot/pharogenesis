04f8d20afc641e99fff2a50eb210bec4556e04c8
sma <unknown>
2000-05-12 12:50:00 +0000

    no message

--- SequenceableCollection/instance/accessing/atRandom-3A-.cs
+++ SequenceableCollection/instance/accessing/atRandom-3A-.cs
@@ -1,12 +1,8 @@
 atRandom: aGenerator
-	"Return a random element of myself.  Uses the instance of class Random supplied by the caller.  Caller should keep the generator in a variable and use the same one every time.  Use this instead of atRandom for better uniformity of random numbers because only you use the generator.  Causes an error if self has no elements."
+	"Answer a random element of the receiver.  Uses aGenerator which
+	should be kept by the user in a variable and used every time. Use
+	this instead of #atRandom for better uniformity of random numbers 
+	because only you use the generator.  Causes an error if self has no 
+	elements."
 
-	| index |
-	index _ (aGenerator next * self size) asInteger + 1.
-	^ self at: index
-
-"	| aGen |
-	aGen _ Random new.   
-	(1 to: 10) atRandom: aGen
-  
-"
+	^ self at: (aGenerator nextInt: self size)
