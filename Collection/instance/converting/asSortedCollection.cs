74862799cac518952a6a2117c5ec0d783904d0e1
sma <unknown>
2000-05-12 17:44:00 +0000

    no message

--- Collection/instance/converting/asSortedCollection.cs
+++ Collection/instance/converting/asSortedCollection.cs
@@ -2,7 +2,4 @@ asSortedCollection
 	"Answer a SortedCollection whose elements are the elements of the 
 	receiver. The sort order is the default less than or equal."
 
-	| aSortedCollection |
-	aSortedCollection _ SortedCollection new: self size.
-	aSortedCollection addAll: self.
-	^aSortedCollection
+	^ self as: SortedCollection
