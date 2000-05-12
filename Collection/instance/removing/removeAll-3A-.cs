6bac87332c2c93b91803798e21d047ad82d644c9
sma <unknown>
2000-05-12 11:14:00 +0000

    no message

--- Collection/instance/removing/removeAll-3A-.cs
+++ Collection/instance/removing/removeAll-3A-.cs
@@ -1,6 +1,7 @@
 removeAll: aCollection 
 	"Remove each element of aCollection from the receiver. If successful for 
-	each, answer aCollection. Otherwise create an error notification."
+	each, answer aCollection. Otherwise create an error notification.
+	ArrayedCollections cannot respond to this message."
 
 	aCollection do: [:each | self remove: each].
-	^aCollection
+	^ aCollection
