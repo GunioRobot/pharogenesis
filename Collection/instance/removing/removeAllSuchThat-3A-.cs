cdd462485c0148d66296a7a860ea26fcc3300bdb
sma <unknown>
2000-05-12 11:19:00 +0000

    no message

--- Collection/instance/removing/removeAllSuchThat-3A-.cs
+++ Collection/instance/removing/removeAllSuchThat-3A-.cs
@@ -1,10 +1,6 @@
-removeAllSuchThat: aBlock
-	"Apply the condition to each element and remove it if the condition is true.  Use a copy to enumerate collections whose order changes when an element is removed (Set)."
-	| copy newCollection |
-	newCollection _ self species new.
-	copy _ self copy.
-	copy do: [:element |
-		(aBlock value: element) ifTrue: [
-			self remove: element.
-			newCollection add: element]].
-	^ newCollection
+removeAllSuchThat: aBlock 
+	"Evaluate aBlock for each element and remove all that elements from
+	the receiver for that aBlock evaluates to true.  Use a copy to enumerate 
+	collections whose order changes when an element is removed (i.e. Sets)."
+
+	self copy do: [:each | (aBlock value: each) ifTrue: [self remove: each]]
