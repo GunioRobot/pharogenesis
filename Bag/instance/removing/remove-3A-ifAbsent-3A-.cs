b694d6fa804fbc22d80882dce6e679d7589e1e76
sma <unknown>
2000-05-12 14:32:00 +0000

    no message

--- Bag/instance/removing/remove-3A-ifAbsent-3A-.cs
+++ Bag/instance/removing/remove-3A-ifAbsent-3A-.cs
@@ -2,9 +2,8 @@ remove: oldObject ifAbsent: exceptionBlock
 	"Refer to the comment in Collection|remove:ifAbsent:."
 
 	| count |
-	(self includes: oldObject)
-		ifTrue: [(count _ contents at: oldObject) = 1
-				ifTrue: [contents removeKey: oldObject]
-				ifFalse: [contents at: oldObject put: count - 1]]
-		ifFalse: [^exceptionBlock value].
-	^oldObject
+	count _ contents at: oldObject ifAbsent: [^ exceptionBlock value].
+	count = 1
+		ifTrue: [contents removeKey: oldObject]
+		ifFalse: [contents at: oldObject put: count - 1].
+	^ oldObject
