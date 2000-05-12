972e6a435d9f982d9e4a1fc5ff6306a363423303
sma <unknown>
2000-05-12 17:26:00 +0000

    no message

--- Collection/instance/adding/addAll-3A-.cs
+++ Collection/instance/adding/addAll-3A-.cs
@@ -1,6 +1,6 @@
 addAll: aCollection 
 	"Include all the elements of aCollection as the receiver's elements. Answer 
-	aCollection."
+	aCollection. Actually, any object responding to #do: can be used as argument."
 
 	aCollection do: [:each | self add: each].
-	^aCollection
+	^ aCollection
