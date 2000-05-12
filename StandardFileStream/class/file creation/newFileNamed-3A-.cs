2f84f78ac0c284f0eba6a0735ef1c7b09cf85bfe
sma <unknown>
2000-05-12 10:36:00 +0000

    no message

--- StandardFileStream/class/file creation/newFileNamed-3A-.cs
+++ StandardFileStream/class/file creation/newFileNamed-3A-.cs
@@ -1,24 +1,31 @@
-newFileNamed: aFileName
- 	"create a file in the default directory (or in the directory contained in the input arg), set for write access."
-	| selection |
-	(self isAFileNamed: aFileName) ifFalse:
-		[^ self new open: aFileName forWrite: true].
+newFileNamed: fileName
+ 	"Create a new file with the given name, and answer a stream opened for writing on that file. If the file already exists, ask the user what to do."
 
-	"File already exists..."
-	selection _ (PopUpMenu labels: 'overwrite that file
+	| dir localName choice newName fullName |
+	fullName _ self fullName: fileName.
+	(self isAFileNamed: fullName)
+		ifFalse: [^ self new open: fullName forWrite: true].
+
+	"file already exists:"
+	dir _ FileDirectory forFileName: fullName.
+	localName _ FileDirectory localNameFor: fullName.
+	choice _ (PopUpMenu
+		labels:
+'overwrite that file
 choose another name
 cancel')
-			startUpWithCaption: (FileDirectory localNameFor: aFileName) , '
+		startUpWithCaption: localName, '
 already exists.'.
-	selection = 1 ifTrue:
-		[FileDirectory default
-			deleteFileNamed: aFileName
-			ifAbsent: [self error: 'Sorry, deletion failed'].
-		^ self new open: aFileName forWrite: true].
-	selection = 2 ifTrue: [
-		^ self newFileNamed:
-			(FillInTheBlank request: 'Enter a new file name'
-				initialAnswer: aFileName)].
-	"Return a dummy stream that will absorb all file messages and do nothing"
-	"^ RWBinaryOrTextStream on: (DummyStream on: nil)"
+
+	choice = 1 ifTrue: [
+		dir deleteFileNamed: localName
+			ifAbsent: [self error: 'Could not delete the old version of that file'].
+		^ self new open: fullName forWrite: true].
+
+	choice = 2 ifTrue: [
+		newName _ FillInTheBlank request: 'Enter a new file name' initialAnswer: 'fullName'.
+		newName isEmpty ifFalse: [
+			fullName _ self fullName: newName.
+			^ self newFileNamed: fullName]].
+
 	self error: 'Please close this to abort file opening'
