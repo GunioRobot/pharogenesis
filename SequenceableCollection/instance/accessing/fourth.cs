225d5aba40bdd9f038848ce2ff57bd7b6ea5af18
sma <unknown>
2000-05-12 13:58:00 +0000

    no message

--- SequenceableCollection/instance/accessing/fourth.cs
+++ SequenceableCollection/instance/accessing/fourth.cs
@@ -1,4 +1,5 @@
 fourth
-	"Answer the fourth element of the receiver. Create an error notification if 
-	the receiver contains fewer than four elements."
-	^ self at: 4 ifAbsent: [self error: 'element not found']
+	"Answer the fourth element of the receiver.
+	Raise an error if there are not enough elements."
+
+	^ self checkedAt: 4
