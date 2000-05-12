774de9bb1cb86ebaaa2e500b2ba7b0a03e7be62e
di <unknown>
2000-05-12 10:32:00 +0000

    no message

---
+++ CrosticPanel/instance/menu/openFile.cs
@@ -0,0 +1,14 @@
+openFile
+
+	| stdFileMenuResult crostic file |
+	stdFileMenuResult _ ((StandardFileMenu new) pattern: '*.crostic'; 
+		oldFileFrom: FileDirectory default ) 
+			startUpWithCaption: 'Select a Crostic File...'.
+	stdFileMenuResult ifNil: [^ nil].
+	file _ stdFileMenuResult directory oldFileNamed: stdFileMenuResult name.
+	crostic _ CrosticPanel newFromFile: file.  file close.
+	(self isClean or: [self confirm: 'Is it OK to discard this crostic?'])
+		ifTrue: [self world addMorphFront: (crostic position: self position).
+				self delete]
+		ifFalse: [self world addMorphFront: crostic].
+
