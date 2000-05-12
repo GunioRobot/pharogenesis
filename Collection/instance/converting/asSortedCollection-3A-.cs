6b94792b46071b1eed5ec45d89291dceda2c58ef
sma <unknown>
2000-05-12 17:46:00 +0000

    no message

--- Collection/instance/converting/asSortedCollection-3A-.cs
+++ Collection/instance/converting/asSortedCollection-3A-.cs
@@ -1,9 +1,9 @@
-asSortedCollection: aBlock 
+asSortedCollection: aSortBlock 
 	"Answer a SortedCollection whose elements are the elements of the 
-	receiver. The sort order is defined by the argument, aBlock."
+	receiver. The sort order is defined by the argument, aSortBlock."
 
 	| aSortedCollection |
 	aSortedCollection _ SortedCollection new: self size.
-	aSortedCollection sortBlock: aBlock.
+	aSortedCollection sortBlock: aSortBlock.
 	aSortedCollection addAll: self.
-	^aSortedCollection
+	^ aSortedCollection
