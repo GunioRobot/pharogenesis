fb7abe480dfe6b2fff8d39b560c0584ce0c74357
sma <unknown>
2000-05-12 11:36:00 +0000

    no message

--- ArrayedCollection/instance/accessing/size.cs
+++ ArrayedCollection/instance/accessing/size.cs
@@ -1,8 +1,5 @@
 size
-	"Primitive. Answer the number of indexable fields in the receiver. This
-	value is the same as the largest legal subscript. Primitive is specified
-	here to override SequenceableCollection size. Essential. See Object
-	documentation whatIsAPrimitive. "
+	"Answer how many elements the receiver contains."
 
 	<primitive: 62>
-	^self basicSize
+	^ self basicSize
