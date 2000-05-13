8154d44d070f5f6ee2743b40a770156874f705d1
dvf <unknown>
2000-05-13 12:31:00 +0000

    no message

---
+++ CelesteComposition/instance/interface/addAttachment.cs
@@ -0,0 +1,13 @@
+addAttachment
+	| file fileResult fileName |
+	textEditor
+		ifNotNil: [self hasUnacceptedEdits ifTrue: [textEditor accept]].
+	"transform into multipart if needed"
+	self hasAttachments ifFalse: [self transformToMultipart].
+	"then simply append another attachment section"
+	(fileResult _ StandardFileMenu oldFile)
+		ifNotNil: 
+			[fileName _ fileResult directory fullNameFor: fileResult name.
+			file _ FileStream oldFileNamed: fileName.
+			file ifNotNil: [self messageText: ((MailMessage from: self messageText)
+						asTextEncodingNewPart: file named: fileResult name)]] 
