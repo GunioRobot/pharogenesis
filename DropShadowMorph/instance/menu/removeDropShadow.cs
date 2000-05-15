506993c199c23e96ea2309768f8f91a155a61033
sw <unknown>
2000-05-15 00:15:00 +0000

    no message

--- DropShadowMorph/instance/menu/removeDropShadow.cs
+++ DropShadowMorph/instance/menu/removeDropShadow.cs
@@ -3,5 +3,6 @@ removeDropShadow
 	(hadHalo _ self hasHalo) ifTrue: [self halo delete].
 	first _ self firstSubmorph.
 	owner addAllMorphs: self submorphs.
+	first absorbStateFromRenderer: self.
 	hadHalo ifTrue: [first addHalo].
 	self delete
