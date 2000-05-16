5b5f97ce322cb67d545c9d7288c6cafe1140dfa6
ar <unknown>
2000-05-16 19:56:00 +0000

    no message

---
+++ BalloonEngineBase/instance/initialize-2D-release/initialiseModule.cs
@@ -0,0 +1,5 @@
+initialiseModule
+	self export: true.
+	loadBBFn _ interpreterProxy ioLoadFunction: 'loadBitBltFrom' From: bbPluginName.
+	copyBitsFn _ interpreterProxy ioLoadFunction: 'copyBitsFromtoat' From: bbPluginName.
+	^(loadBBFn ~= 0 and:[copyBitsFn ~= 0])
