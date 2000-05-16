cba42f4acd3ee10e4985dbde5cd971998ea02f76
di <unknown>
2000-05-16 22:05:00 +0000

    no message

--- Point/instance/private/setR-3A-degrees-3A-.cs
+++ Point/instance/private/setR-3A-degrees-3A-.cs
@@ -1,6 +1,6 @@
-setR: rho degrees: theta
+setR: rho degrees: theta 
 
 	| radians |
 	radians _ theta asFloat degreesToRadians.
-	x _ (rho asFloat * radians cos) asInteger.
-	y _ (rho asFloat * radians sin) asInteger.
+	x _ rho asFloat * radians cos.
+	y _ rho asFloat * radians sin.
