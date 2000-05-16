baae50bfdee1d98346941f8212a71c8f5032050c
di <unknown>
2000-05-16 09:42:00 +0000

    no message

--- FileList/instance/initialization/directory-3A-.cs
+++ FileList/instance/initialization/directory-3A-.cs
@@ -1,12 +1,15 @@
 directory: dir
 	"Set the path of the volume to be displayed."
 
-	sortMode == nil ifTrue: [sortMode _ #date].
 	self okToChange ifFalse: [^ self].
+
+	self modelSleep.
 	directory _ dir.
+	self modelWakeUp.
+
+	sortMode == nil ifTrue: [sortMode _ #date].
 	volList _ ((Array with: '[]'), directory pathParts)  "Nesting suggestion from RvL"
 			withIndexCollect: [:each :i | ( String new: i-1 withAll: $ ), each].
 	self changed: #relabel.
 	self changed: #volumeList.
-	self pattern: pattern.
-	self changed: #contents.
+	self pattern: pattern
