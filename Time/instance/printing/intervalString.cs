963564688bd64c262344fbc2d6e5e5b34627f12f
BP <unknown>
2000-05-17 19:44:00 +0000

    no message

--- Time/instance/printing/intervalString.cs
+++ Time/instance/printing/intervalString.cs
@@ -2,8 +2,8 @@ intervalString
 	"Treat the time as a difference.  Give it in hours and minutes with two digits of accuracy."
 
 	| hh mm ss |
-	hh _ hours = 0 ifTrue: [''] ifFalse: [' ', hours printString, ' hours'].
-	mm _ minutes = 0 ifTrue: [''] ifFalse: [' ', minutes printString, ' minutes'].
-	ss _ seconds = 0 ifTrue: [''] ifFalse: [' ', seconds printString, ' seconds'].
+	hh _ self hours = 0 ifTrue: [''] ifFalse: [' ', self hours printString, ' hours'].
+	mm _ self minutes = 0 ifTrue: [''] ifFalse: [' ', self minutes printString, ' minutes'].
+	ss _ self seconds = 0 ifTrue: [''] ifFalse: [' ', self seconds printString, ' seconds'].
 	hh size > 0 ifTrue: [ss _ ''].
 	^ hh, mm, ss
