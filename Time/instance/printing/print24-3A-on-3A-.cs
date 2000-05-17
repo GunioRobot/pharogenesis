b08f82af3a7c724946f325e6a03719330b97e778
BP <unknown>
2000-05-17 19:41:00 +0000

    no message

--- Time/instance/printing/print24-3A-on-3A-.cs
+++ Time/instance/printing/print24-3A-on-3A-.cs
@@ -1,21 +1,4 @@
 print24: hr24 on: aStream 
 	"Format is 'hh:mm:ss' or 'h:mm:ss am' "
-	hr24
-	ifTrue:
-		[hours < 10 ifTrue: [aStream nextPutAll: '0'].
-		hours printOn: aStream]
-	ifFalse:
-		[hours > 12
-		ifTrue: [hours - 12 printOn: aStream]
-		ifFalse: [hours < 1
-				ifTrue: [12 printOn: aStream]
-				ifFalse: [hours printOn: aStream]]].
-	aStream nextPutAll: (minutes < 10 ifTrue: [':0']
-									  ifFalse: [':']).
-	minutes printOn: aStream.
-	aStream nextPutAll: (seconds < 10 ifTrue: [':0']
-									  ifFalse: [':']).
-	seconds printOn: aStream.
-	hr24 ifFalse:
-		[ aStream nextPutAll: (hours < 12 ifTrue: [' am']
-										ifFalse: [' pm'])]
+
+	self print24: hr24 showSeconds: true on: aStream 
