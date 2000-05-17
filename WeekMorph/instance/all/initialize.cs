afe1c2e3ca9f2f8752cbbd522e0591823edfe698
ar <unknown>
2000-05-17 00:06:00 +0000

    no message

---
+++ WeekMorph/instance/all/initialize.cs
@@ -0,0 +1,9 @@
+initialize
+	super initialize.
+	self
+		inset: 0;
+		color: Color transparent;
+		orientation: #horizontal;
+		disableDragNDrop;
+		height: 19;
+		week: Date today week month: Date today month
