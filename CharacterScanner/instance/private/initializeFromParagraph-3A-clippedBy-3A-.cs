e3878c7afdefcb5f5b23ef418f7c1fdcb5814e1f
ar <unknown>
2000-05-17 17:13:00 +0000

    no message

--- CharacterScanner/instance/private/initializeFromParagraph-3A-clippedBy-3A-.cs
+++ CharacterScanner/instance/private/initializeFromParagraph-3A-clippedBy-3A-.cs
@@ -2,8 +2,3 @@ initializeFromParagraph: aParagraph clippedBy: clippingRectangle
 
 	text _ aParagraph text.
 	textStyle _ aParagraph textStyle. 
-	destForm _ aParagraph destinationForm.
-	self fillColor: aParagraph fillColor.	"sets halftoneForm"
-	self combinationRule: aParagraph rule.
-	self clipRect: clippingRectangle.
-	sourceY _ 0
