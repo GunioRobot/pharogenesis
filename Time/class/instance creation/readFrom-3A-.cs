0f19efa71762e5b4b73036ddf4e0a6f503689beb
BP <unknown>
2000-05-17 19:53:00 +0000

    no message

--- Time/class/instance creation/readFrom-3A-.cs
+++ Time/class/instance creation/readFrom-3A-.cs
@@ -4,19 +4,24 @@ readFrom: aStream
 
 	<minute>, <second> or <am/pm> may be omitted.  e.g. 1:59:30 pm; 8AM; 15:30"
 
-	| hour minute second |
+	| hour minute second ampm |
 	hour _ Integer readFrom: aStream.
 	minute _ 0.
 	second _ 0.
 	(aStream peekFor: $:) ifTrue:
-		[minute _ Integer readFrom: aStream.
-		(aStream peekFor: $:) ifTrue:
-			[second _ Integer readFrom: aStream]].
+	[
+		minute _ Integer readFrom: aStream.
+		(aStream peekFor: $:) ifTrue: [ second _ Integer readFrom: aStream ]
+	].
 	aStream skipSeparators.
 	(aStream atEnd not and: [aStream peek isLetter])
-		ifTrue:
-			[aStream next asLowercase = $p ifTrue: [hour _ hour + 12].
-			(aStream peekFor: $m) ifFalse: [aStream peekFor: $M]].
+	ifTrue:
+	[
+		ampm _ aStream next asLowercase.
+		(ampm = $p and: [hour < 12]) ifTrue: [hour _ hour + 12].
+		(ampm = $a and: [hour = 12]) ifTrue: [hour _ 0].
+		(aStream peekFor: $m) ifFalse: [aStream peekFor: $M]
+	].
 	^self fromSeconds: 60*(60*hour+minute)+second
 
 	"Time readFrom: (ReadStream on: '2:23:09 pm')"
