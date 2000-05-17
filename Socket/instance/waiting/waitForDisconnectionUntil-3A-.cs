4955bc5dd1a318ad432461d9b04a099af8d7f0ec
JMM <unknown>
2000-05-17 14:52:00 +0000

    no message

--- Socket/instance/waiting/waitForDisconnectionUntil-3A-.cs
+++ Socket/instance/waiting/waitForDisconnectionUntil-3A-.cs
@@ -1,11 +1,12 @@
 waitForDisconnectionUntil: deadline
 	"Wait up until the given deadline for the the connection to be broken. Return true if it is broken by the deadline, false if not."
-	"Note: The client should know the the connect is really going to be closed (e.g., because he has called 'close' to send a close request to the other end) before calling this method."
+	"Note: The client should know the the connect is really going to be closed (e.g., because he has called 'close' to send a close request to the other end) before calling this method.
+JMM 00/5/17 note that other end can close which will terminate wait"
 
 	| extraBytes status |
 	extraBytes _ 0.
 	status _ self primSocketConnectionStatus: socketHandle.
-	[((status = Connected) or: [status = ThisEndClosed]) and:
+	[((status = Connected) or: [(status = ThisEndClosed)]) and:
 	 [Time millisecondClockValue < deadline]] whileTrue: [
 		self dataAvailable
 			ifTrue: [extraBytes _ extraBytes + self discardReceivedData].
