bf2c71a53ff48081762a5a5caa0ffd1c5bd7205c
dvf <unknown>
2000-05-17 20:48:00 +0000

    no message

---
+++ Celeste/instance/sending mail/queueMessageWithText-3A-.cs
@@ -0,0 +1,11 @@
+queueMessageWithText: aStringOrText 
+	"Queue a message to be sent later. The message is added to the database 
+	and filed in the '.tosend.' category."
+	| messageText id |
+	messageText _ 'X-Mailer: ' , Celeste versionString , String cr , 'Date: ' , MailMessage dateStampNow, ' ' , self timeZoneString, ' ', String cr.
+	messageText _ messageText , aStringOrText asString.
+	self requiredCategory: '.tosend.'.
+	"queue the message"
+	id _ mailDB addNewMessage: (MailMessage from: messageText).
+	mailDB file: id inCategory: '.tosend.'.
+	self category = '.tosend.' ifTrue: [self updateTOC]
