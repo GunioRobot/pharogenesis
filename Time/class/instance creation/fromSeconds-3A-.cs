d43e7892a771a0681f1a7492cbdd78f47cbdcea2
BP <unknown>
2000-05-17 19:53:00 +0000

    no message

--- Time/class/instance creation/fromSeconds-3A-.cs
+++ Time/class/instance creation/fromSeconds-3A-.cs
@@ -1,10 +1,6 @@
 fromSeconds: secondCount 
 	"Answer an instance of me that is secondCount number of seconds since midnight."
 
-	| secondsInHour hours secs |
-	secs _ secondCount asInteger.
-	hours _ secs // 3600.
-	secondsInHour _ secs \\ 3600.
-	^self new hours: hours
-			   minutes: secondsInHour // 60
-			   seconds: secondsInHour \\ 60
+	^self new 
+		setSeconds: secondCount;
+		yourself.
