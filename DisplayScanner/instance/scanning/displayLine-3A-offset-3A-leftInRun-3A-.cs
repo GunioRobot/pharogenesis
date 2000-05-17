2060041689c0cd18d47b0f83558f956920db1919
ar <unknown>
2000-05-17 20:46:00 +0000

    no message

--- DisplayScanner/instance/scanning/displayLine-3A-offset-3A-leftInRun-3A-.cs
+++ DisplayScanner/instance/scanning/displayLine-3A-offset-3A-leftInRun-3A-.cs
@@ -1,10 +1,10 @@
 displayLine: textLine offset: offset leftInRun: leftInRun
 	"The call on the primitive (scanCharactersFrom:to:in:rightX:) will be interrupted according to an array of stop conditions passed to the scanner at which time the code to handle the stop condition is run and the call on the primitive continued until a stop condition returns true (which means the line has terminated).  leftInRun is the # of characters left to scan in the current run; when 0, it is time to call setStopConditions."
-	| done stopCondition nowLeftInRun |
+	| done stopCondition nowLeftInRun startIndex string lastPos |
 	line _ textLine.
 	morphicOffset _ offset.
 	leftMargin _ (line leftMarginForAlignment: textStyle alignment) + offset x.
-	runX _ destX _ leftMargin.
+	destX _ runX _ leftMargin.
 	rightMargin _ line rightMargin + offset x.
 	lineY _ line top + offset y.
 	lineHeight _ line lineHeight.
@@ -21,10 +21,16 @@ displayLine: textLine offset: offset leftInRun: leftInRun
 	runStopIndex _ lastIndex + (nowLeftInRun - 1) min: line last.
 	spaceCount _ 0.
 	done _ false.
-	[done] whileFalse: 
-		[stopCondition _ self scanCharactersFrom: lastIndex to: runStopIndex
-						in: text string rightX: rightMargin stopConditions: stopConditions
-						kern: kern displaying: true.
+	string _ text string.
+	[done] whileFalse:[
+		startIndex _ lastIndex.
+		lastPos _ destX@destY.
+		stopCondition _ self scanCharactersFrom: lastIndex to: runStopIndex
+						in: string rightX: rightMargin stopConditions: stopConditions
+						kern: kern.
+		lastIndex >= startIndex ifTrue:[
+			font displayString: string on: bitBlt 
+				from: startIndex to: lastIndex at: lastPos kern: kern].
 		"see setStopConditions for stopping conditions for displaying."
 		done _ self perform: stopCondition].
 	fillBlt == nil ifFalse:
