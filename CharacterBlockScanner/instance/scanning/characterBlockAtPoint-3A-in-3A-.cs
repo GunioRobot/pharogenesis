4a51de7c98eadd4bde7cd458a2dcd4381e8f912f
ar <unknown>
2000-05-17 19:14:00 +0000

    no message

--- CharacterBlockScanner/instance/scanning/characterBlockAtPoint-3A-in-3A-.cs
+++ CharacterBlockScanner/instance/scanning/characterBlockAtPoint-3A-in-3A-.cs
@@ -3,6 +3,6 @@ characterBlockAtPoint: aPoint in: aParagraph
 	is assumed that aPoint has been transformed into coordinates appropriate 
 	to the text's destination form rectangle and the composition rectangle."
 
-	super initializeFromParagraph: aParagraph clippedBy: aParagraph clippingRectangle.
+	self initializeFromParagraph: aParagraph clippedBy: aParagraph clippingRectangle.
 	characterPoint _ aPoint.
 	^self buildCharacterBlockIn: aParagraph
