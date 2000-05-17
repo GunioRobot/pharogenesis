1b4027b448bcc56f7dfc7ce4ba807ec754d8eece
ar <unknown>
2000-05-17 18:20:00 +0000

    no message

--- CharacterScanner/instance/scanning/placeEmbeddedObject-3A-.cs
+++ CharacterScanner/instance/scanning/placeEmbeddedObject-3A-.cs
@@ -1,11 +1,11 @@
 placeEmbeddedObject: anchoredMorph
 	"Place the anchoredMorph or return false if it cannot be placed.
 	In any event, advance destX by its width."
-
-	destX _ destX + (width _ anchoredMorph width).
-	(destX > rightMargin and: [(leftMargin + width) <= rightMargin])
+	| w |
+	destX _ destX + (w _ anchoredMorph width).
+	(destX > rightMargin and: [(leftMargin + w) <= rightMargin])
 		ifTrue: ["Won't fit, but would on next line"
 				^ false].
-	runStopIndex _ lastIndex.  "Force new calc of emphasis"
 	lastIndex _ lastIndex + 1.
+	self setFont.  "Force recalculation of emphasis for next run"
 	^ true
