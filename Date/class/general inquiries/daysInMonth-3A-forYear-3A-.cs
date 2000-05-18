8bc300ab5cd51acfdb6c027cea2c49f085eaaa4d
BP <unknown>
2000-05-18 19:00:00 +0000

    no message

--- Date/class/general inquiries/daysInMonth-3A-forYear-3A-.cs
+++ Date/class/general inquiries/daysInMonth-3A-forYear-3A-.cs
@@ -1,7 +1,4 @@
 daysInMonth: monthName forYear: yearInteger 
-	"Answer the number of days in the month named monthName in the 
-	year yearInteger."
+	"Answer the number of days in the month named monthName in the year yearInteger."
 
-	^(self newDay: 1
-		  month: monthName
-		  year: yearInteger) daysInMonth
+	^(self newDay: 1 month: monthName year: yearInteger) daysInMonth
