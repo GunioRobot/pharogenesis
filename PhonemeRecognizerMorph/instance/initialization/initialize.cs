ab57e7cf7a58614c213cd02f71b74cb4dc71e477
jm <unknown>
2000-05-18 18:29:00 +0000

    no message

---
+++ PhonemeRecognizerMorph/instance/initialization/initialize.cs
@@ -0,0 +1,18 @@
+initialize
+
+	| r |
+	super initialize.
+	borderWidth _ 2.
+	orientation _ #vertical.
+	soundInput _ SoundInputStream new samplingRate: 22050.
+	phonemeRecords _ OrderedCollection new.
+	silentPhoneme _ PhonemeRecord new initialize name: '...'.
+	currentPhoneme _ silentPhoneme.  "the PhonemeRecord of the current match"
+	self addTitle.
+	self addButtonRows.
+	self addLevelSlider.
+	r _ AlignmentMorph newRow vResizing: #shrinkWrap.
+	r addMorphBack: self makeLevelMeter.
+	self addMorphBack: r.
+	self addPhonemeDisplay.
+	self extent: 10@10.  "make minimum size"
