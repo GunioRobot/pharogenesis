a4b66f3cb7c10996c3e4d278fdcd95f825a874f9
ar <unknown>
2000-05-18 18:00:00 +0000

    no message

---
+++ DisplayScanner/instance/quick print/stringWidth-3A-.cs
@@ -0,0 +1,12 @@
+stringWidth: aString
+	"Answer the width of the given string."
+	destX _ destY _ 0.
+	aString ifNil: [^ 0].
+	self primScanCharactersFrom: 1 to: aString size in: aString
+		rightX: 99999	"virtual infinity"
+		stopConditions: stopConditions
+		kern: kern.
+	^ destX
+"
+	(1 to: 10) collect: [:i | QuickPrint new stringWidth: (String new: i withAll: $A)]
+"
