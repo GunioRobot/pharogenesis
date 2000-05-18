d7115d27e390d9f7ff7421be52849bd158e117df
BP <unknown>
2000-05-18 19:10:00 +0000

    no message

--- Date/instance/inquiries/previous-3A-.cs
+++ Date/instance/inquiries/previous-3A-.cs
@@ -1,4 +1,4 @@
 previous: dayName 
 	"Answer the previous date whose weekday name is dayName."
 
-	^self subtractDays: 7 + self weekdayIndex - (Date dayOfWeek: dayName) \\ 7
+	^self subtractDays: 7 + self weekdayIndex - (self class dayOfWeek: dayName) \\ 7
