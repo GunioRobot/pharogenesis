cbc92a6ec3ed5b3ab1c7ad41dd34f50af955dea5
bolot <unknown>
2000-05-18 13:11:00 +0000

    no message

---
+++ HttpUrl/instance/downloading/checkAuthorization-3A-retry-3A-.cs
@@ -0,0 +1,20 @@
+checkAuthorization: webDocument retry: retryBlock
+	"authorization failed if webDocument is a String"
+	| oldRealm i end encoded |
+	((webDocument isKindOf: String)
+		and: [(webDocument beginsWith: 'HTTP/1.0 401')
+			or: [webDocument beginsWith: 'HTTP/1.1 401']])
+	ifFalse: [^self].
+
+	oldRealm _ realm.
+	i _ webDocument findString: 'realm="'.
+	i = 0 ifTrue: [^self].
+	end _ webDocument indexOf: $" startingAt: i.
+	realm _ webDocument copyFrom: i+7 to: end.
+	"realm _ (webDocument findTokens: '""') at: 2."
+	Passwords ifNil: [Passwords _ Dictionary new].
+	encoded _ Passwords at: realm ifAbsent: [nil].
+	(oldRealm ~= realm) & (encoded ~~ nil) 
+		ifTrue: [^ retryBlock value]
+		ifFalse: ["ask the user"
+			self askNamePassword ifTrue: [^ retryBlock value]]
