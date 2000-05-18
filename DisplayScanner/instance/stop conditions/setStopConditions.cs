4469e54b4df9a09449de8a8e1cef5af181ddc5c6
ar <unknown>
2000-05-18 16:47:00 +0000

    no message

--- DisplayScanner/instance/stop conditions/setStopConditions.cs
+++ DisplayScanner/instance/stop conditions/setStopConditions.cs
@@ -2,6 +2,8 @@ setStopConditions
 	"Set the font and the stop conditions for the current run."
 	
 	self setFont.
-	stopConditions 
-		at: Space asciiValue + 1 
-		put: (textStyle alignment = Justified ifTrue: [#paddedSpace])
+	textStyle alignment = Justified ifTrue:[
+		"Make a local copy of stop conditions so we don't modify the default"
+		stopConditions == DefaultStopConditions 
+			ifTrue:[stopConditions _ stopConditions copy].
+		stopConditions at: Space asciiValue + 1 put: #paddedSpace]
