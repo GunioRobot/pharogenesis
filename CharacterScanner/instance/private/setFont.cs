8eec068550a7d512a692f93744d0b6d64810b2e6
ar <unknown>
2000-05-18 18:02:00 +0000

    no message

--- CharacterScanner/instance/private/setFont.cs
+++ CharacterScanner/instance/private/setFont.cs
@@ -1,20 +1,19 @@
 setFont
 	"Set the font and other emphasis."
-	self setFont: 1.
-	emphasisCode _ 0.
-	kern _ 0.
-	(text attributesAt: lastIndex) do: 
-		[:att | att emphasizeScanner: self].
+	text == nil ifFalse:[
+		emphasisCode _ 0.
+		kern _ 0.
+		indentationLevel _ 0.
+		alignment _ textStyle alignment.
+		font _ nil.
+		(text attributesAt: lastIndex forStyle: textStyle)
+			do: [:att | att emphasizeScanner: self]].
+	font == nil ifTrue:
+		[self setFont: textStyle defaultFontIndex].
 	font _ font emphasized: emphasisCode.
 
 	"Install various parameters from the font."
-	spaceWidth _ font widthOf: Space. 
-	sourceForm _ font glyphs.  "Should only be needed in DisplayScanner"
-	height _ font height.			" ditto "
+	spaceWidth _ font widthOf: Space.
 	xTable _ font xTable.
-	stopConditions _ font stopConditions.
-	stopConditions at: Space asciiValue + 1 put: #space.
-	stopConditions at: Tab asciiValue + 1 put: #tab.
-	stopConditions at: CR asciiValue + 1 put: #cr.
-	stopConditions at: EndOfRun put: #endOfRun.
-	stopConditions at: CrossedX put: #crossedX
+	map _ font characterToGlyphMap.
+	stopConditions _ DefaultStopConditions.
