53170c873855b74fbc15f56f036093cb1511ed43
ar <unknown>
2000-05-18 18:35:00 +0000

    no message

--- FormCanvas/instance/drawing/text-3A-bounds-3A-font-3A-color-3A-.cs
+++ FormCanvas/instance/drawing/text-3A-bounds-3A-font-3A-color-3A-.cs
@@ -1,7 +1,7 @@
 text: s bounds: boundsRect font: fontOrNil color: c
 	| scanner |
-	scanner _ QuickPrint newOn: form
+	scanner _ DisplayScanner quickPrintOn: form
 				box: ((boundsRect translateBy: origin) intersect: clipRect) truncated
 				font: fontOrNil
-				color: (shadowDrawing ifTrue: [shadowStipple] ifFalse: [c]).
+				color: (self shadowColor ifNil:[c]).
 	scanner drawString: s at: boundsRect topLeft + origin
