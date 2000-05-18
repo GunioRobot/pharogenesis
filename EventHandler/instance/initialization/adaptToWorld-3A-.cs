09d906528866fc9c3831af58ad0cdefc211109e0
LC <unknown>
2000-05-18 09:37:00 +0000

    no message

---
+++ EventHandler/instance/initialization/adaptToWorld-3A-.cs
@@ -0,0 +1,20 @@
+adaptToWorld: aWorld
+	"If any of my recipients refer to a world or a hand, make them now refer
+	to the corresponding items in the new world.  (instVarNamed: is slow, later
+	use perform of two selectors.)"
+
+	| value newValue |
+	#(mouseDownRecipient mouseStillDownRecipient mouseUpRecipient
+	mouseEnterRecipient mouseLeaveRecipient mouseEnterDraggingRecipient
+	mouseLeaveDraggingRecipient doubleClickRecipient startDragRecipient keyStrokeRecipient valueParameter) do:
+		[:aName |
+		(value _ self instVarNamed: aName asString) ifNotNil:
+			[newValue _ nil.
+			value isMorph
+				ifTrue:
+					[value isWorldMorph ifTrue: [newValue _ aWorld].
+					value isHandMorph ifTrue: [newValue _ aWorld primaryHand]]
+				ifFalse: [(value isKindOf: Presenter) ifTrue: [newValue _ aWorld presenter]].
+			(newValue notNil and: [newValue ~~ value])
+				ifTrue:
+					[self instVarNamed: aName asString put: newValue]]]
