ade218d381512dbda1a2d76273d48ca2c397670a
BP <unknown>
2000-05-18 18:05:00 +0000

    no message

--- Date/class/general inquiries/leapYear-3A-.cs
+++ Date/class/general inquiries/leapYear-3A-.cs
@@ -1,6 +1,10 @@
-leapYear: yearInteger 
-	"Answer 1 if the year yearInteger is a leap year; answer 0 if it is not."
+leapYear: yearInteger
+ "Answer 1 if the year yearInteger is a leap year; answer 0 if it is not."
 
-	(yearInteger \\ 4 ~= 0 or: [yearInteger \\ 100 = 0 and: [yearInteger \\ 400 ~= 0]])
-		ifTrue: [^0]
-		ifFalse: [^1]
+    | adjustedYear |
+    adjustedYear := yearInteger > 0
+        ifTrue: [yearInteger]
+        ifFalse: [(yearInteger + 1) negated "There is no year 0!!"].
+     (adjustedYear \\ 4 ~= 0 or: [adjustedYear \\ 100 = 0 and: [adjustedYear \\ 400 ~= 0]])
+        ifTrue: [^0]
+        ifFalse: [^1]
