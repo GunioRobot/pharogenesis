242b0cf8836c14ac5861582e30b85eb2cfad8bed
bf <unknown>
2000-05-18 15:10:00 +0000

    no message

---
+++ X11Display/instance/accessing/queryPointer-3A-.cs
@@ -0,0 +1,12 @@
+queryPointer: aX11Window
+	| root child rootX rootY winX winY mask |
+	root _ X11Window new.
+	child _ X11Window new.
+	rootX _ WordArray new: 1.
+	rootY _ WordArray new: 1.
+	winX _ WordArray new: 1.
+	winY _ WordArray new: 1.
+	mask _ WordArray new: 1.
+	self XQueryPointer: self window: aX11Window returnRoot: root child: child
+		rootX: rootX rootY: rootY winX: winX winY: winY mask: mask.
+	^{root. child. rootX first @ rootY first. winX first @ winY first. mask first}
