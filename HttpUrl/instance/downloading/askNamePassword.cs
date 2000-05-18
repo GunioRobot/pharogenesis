136fb719343dbfe6a35fb1e75a1c836e62ae8488
bolot <unknown>
2000-05-18 13:55:00 +0000

    no message

---
+++ HttpUrl/instance/downloading/askNamePassword.cs
@@ -0,0 +1,13 @@
+askNamePassword
+	"Authorization is required by the host site.  Ask the user for a userName and password.  Encode them and store under this realm.  Return false if the user wants to give up."
+
+	| user password |
+	(self confirm: 'Host ', self toText, '
+wants a different user and password.  Type them now?' orCancel: [false])
+		ifFalse: [^ false].
+	"Note: When Scamper is converted to run under MVC, we'll have to pass in topView in order to decide which FillInTheBlank to call."
+	user _ FillInTheBlank request: 'User account name?' initialAnswer: '' 
+				centerAt: Sensor cursorPoint - (50@0).
+	password _ FillInTheBlank requestPassword: 'Password?'.
+	Passwords at: realm put: (Authorizer new encode: user password: password).
+	^ true
