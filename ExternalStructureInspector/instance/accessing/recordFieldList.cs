c7a38f77b4b5ede081098509aad70ce9a52484f1
bf <unknown>
2000-05-18 16:33:00 +0000

    no message

---
+++ ExternalStructureInspector/instance/accessing/recordFieldList.cs
@@ -0,0 +1,5 @@
+recordFieldList
+	| fields |
+	fields _ object class fields.
+	(fields first isKindOf: Array) ifFalse: [fields _ Array with: fields].
+	^fields collect: [ :field | field first ] thenSelect: [:name | name ~~ #nil]
