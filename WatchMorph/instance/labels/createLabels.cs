f37ba69a3c3ebbbad960b6cfb04bb6e6981ef969
ak <unknown>
2000-05-19 21:47:00 +0000

    no message

---
+++ WatchMorph/instance/labels/createLabels.cs
@@ -0,0 +1,12 @@
+createLabels
+	| numeral font h r |
+	self removeAllMorphs.
+	font _ StrikeFont familyName: fontName size: (h _ self height min: self width)//8.
+	r _ 1.0 - (1.4 * font height / h).
+	1 to: 12 do:
+		[:hour |
+		numeral _ romanNumerals
+			ifTrue: [hour romanString]
+			ifFalse: [hour asString].
+		self addMorphBack: ((StringMorph contents: numeral font: font emphasis: 1)
+			center: (self radius: r hourAngle: hour)) lock]
