4aead383bba56614e35809f1d7b68136da82dcea
ar <unknown>
2000-05-19 14:45:00 +0000

    no message

--- CharacterScanner/instance/scanning/characterNotInFont.cs
+++ CharacterScanner/instance/scanning/characterNotInFont.cs
@@ -1,5 +1,11 @@
 characterNotInFont
-	"All fonts have an illegal character to be used when a character is not 
+	"Note: All fonts should have some sort of a character to glyph mapping.
+	If a character is not in the font it should be mapped to the appropriate
+	glyph (that is the glyph describing a non-existing character). If done
+	correctly, this method should never be called. It is mainly provided
+	for backward compatibility (and I'd really like to get rid of it - ar).
+
+	All fonts have an illegal character to be used when a character is not 
 	within the font's legal range. When characters out of ranged are 
 	encountered in scanning text, then this special character indicates the 
 	appropriate behavior. The character is usually treated as a unary 
@@ -11,7 +17,7 @@ characterNotInFont
 	stopCondition _ self scanCharactersFrom: 1 to: 1
 			in: illegalAsciiString
 			rightX: rightMargin stopConditions: stopConditions
-			kern: kern displaying: self doesDisplaying.
+			kern: kern.
 	lastIndex _ saveIndex + 1.
 	stopCondition ~= (stopConditions at: EndOfRun)
 		ifTrue:	[^self perform: stopCondition]
