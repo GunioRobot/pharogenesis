c5c76840baf335aa07de30f7ec283448bb992a81
jm <unknown>
2000-05-19 23:11:00 +0000

    no message

---
+++ PhonemeRecord/instance/access/mouthPosition.cs
@@ -0,0 +1,4 @@
+mouthPosition
+	"Answer the mouth position associated with this phoneme, a positive integer that can be used to index into a collection of frames for an animation."
+
+	^ mouthPosition
