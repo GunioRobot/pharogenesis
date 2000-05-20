6242d4dfaaac9523954114f230f726527f6c2923
sma <unknown>
2000-05-20 18:29:00 +0000

    no message

--- FileList/instance/file list menu/fileNameSuffix.cs
+++ FileList/instance/file list menu/fileNameSuffix.cs
@@ -1,7 +1,2 @@
 fileNameSuffix
-
-	| name i |
-	name _ self fullName.
-	i _ name findLast: [:c | c = $.].
-	i = 0 ifTrue: [^ ''].
-	^ name copyFrom: i + 1 to: name size
+	^ self getSuffix: self fullName
