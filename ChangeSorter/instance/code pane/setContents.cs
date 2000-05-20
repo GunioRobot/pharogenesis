96d86505659918dca066f100f8e95e553d13736e
mas <unknown>
2000-05-20 22:03:00 +0000

    no message

--- ChangeSorter/instance/code pane/setContents.cs
+++ ChangeSorter/instance/code pane/setContents.cs
@@ -2,7 +2,7 @@ setContents
 	"return the source code that shows in the bottom pane"
 	| sel class strm changeType |
 	self clearUserEditFlag.
-	currentClassName ifNil: [^ contents _ ''].
+	currentClassName ifNil: [^ contents _ myChangeSet preambleString ifNil: ['']].
 	class _ self selectedClassOrMetaClass.
 	(sel _ currentSelector) == nil
 		ifFalse: [changeType _ (myChangeSet atSelector: (sel _ sel asSymbol) class: class).
@@ -13,11 +13,22 @@ setContents
 			class ifNil: [^ contents _ 'Method was added, but cannot be found!'].
 			(class includesSelector: sel)
 				ifFalse: [^ contents _ 'Method was added, but cannot be found!'].
-			^ contents _ (class sourceMethodAt: sel) copy]
+			contents _ class sourceCodeAt: sel.
+			Preferences browseWithPrettyPrint ifTrue:
+				[contents _ class compilerClass new
+					format: contents in: class notifying: nil decorated: Preferences colorWhenPrettyPrinting].
+			self showDiffs
+				ifTrue: [contents _ self diffFromPriorSourceFor: contents].
+			^ contents _ contents asText makeSelectorBoldIn: class]
 		ifTrue: [strm _ WriteStream on: (String new: 100).
-			(myChangeSet classChangeAt: currentClassName) do: [:each |
+			(myChangeSet classChangeAt: currentClassName) do:
+				[:each |
 				each = #remove ifTrue: [strm nextPutAll: 'Entire class was removed.'; cr].
-				each = #add ifTrue: [strm nextPutAll: 'Entire class was added.'; cr].
+				each = #addedThenRemoved ifTrue: [strm nextPutAll: 'Class was added then removed.'].
+				each = #rename ifTrue: [strm nextPutAll: 'Class name was changed.'; cr].
+				each = #add ifTrue: [strm nextPutAll: 'Class definition was added.'; cr].
 				each = #change ifTrue: [strm nextPutAll: 'Class definition was changed.'; cr].
-				each = #comment ifTrue: [strm nextPutAll: 'New class comment.'; cr]].
+				each = #reorganize ifTrue: [strm nextPutAll: 'Class organization was changed.'; cr].
+				each = #comment ifTrue: [strm nextPutAll: 'New class comment.'; cr.
+				]].
 			^ contents _ strm contents].
