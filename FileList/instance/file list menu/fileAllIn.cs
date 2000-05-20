76813ac5e9bca39f6ce990f02ec8ff4de036f21c
sma <unknown>
2000-05-20 18:30:00 +0000

    no message

--- FileList/instance/file list menu/fileAllIn.cs
+++ FileList/instance/file list menu/fileAllIn.cs
@@ -1,8 +1,9 @@
 fileAllIn
 	"File in all of the currently selected file, if any."
+	"wod 5/24/1998: open the file read only."
 
-	| ff |
+	| fn ff |
 	listIndex = 0 ifTrue: [^ self].
-	ff _ directory oldFileNamed: self fullName.
-	(self fileNameSuffix sameAs: 'html') ifTrue: [ff _ ff asHtml].
-	ff fileIn.
+	ff _ directory readOnlyFileNamed: (fn _ self uncompressedFileName).
+	((self getSuffix: fn) sameAs: 'html') ifTrue: [ff _ ff asHtml].
+	ff fileIn
