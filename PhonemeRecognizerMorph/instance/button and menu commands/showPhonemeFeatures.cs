5fb885f87c68bb2e29055c4fc85ddfce26c4c5b5
jm <unknown>
2000-05-22 17:40:00 +0000

    no message

---
+++ PhonemeRecognizerMorph/instance/button and menu commands/showPhonemeFeatures.cs
@@ -0,0 +1,8 @@
+showPhonemeFeatures
+	"Show a graph of the features array for the phoneme selected by the user."
+
+	| phoneme m |
+	phoneme _ self selectPhonemeFromMenu.
+	phoneme ifNotNil: [
+		m _ ImageMorph new image: phoneme featuresGraph.
+		self world firstHand attachMorph: m].
