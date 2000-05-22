17ebfeaf233e5610a99755a32997a911fdcc394c
jm <unknown>
2000-05-22 17:50:00 +0000

    no message

---
+++ PhonemeRecognizerMorph/instance/private/makeLevelMeter.cs
@@ -0,0 +1,10 @@
+makeLevelMeter
+	"Create a recording level meter."
+
+	| outerBox |
+	outerBox _ RectangleMorph new extent: 125@14; color: Color lightGray.
+	levelMeter _ Morph new extent: 2@10; color: Color yellow.
+	levelMeter position: outerBox topLeft + (2@2).
+	outerBox addMorph: levelMeter.
+	^ outerBox
+
