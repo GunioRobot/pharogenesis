827adadc9965ebb0178bd7d75d61be209b1a0b6f
jm <unknown>
2000-05-22 17:47:00 +0000

    no message

---
+++ PhonemeRecognizerMorph/instance/private/addPhonemeDisplay.cs
@@ -0,0 +1,16 @@
+addPhonemeDisplay
+	"Add a display to show the currently matching phoneme."
+
+	| font r |
+	font _ StrikeFont familyName: 'Helvetica' size: 36.
+	phonemeDisplay _ StringMorph contents: '...' font: font.
+	r _ AlignmentMorph newColumn
+		color: color;
+		inset: 0;
+		centering: #center;
+		hResizing: #spaceFill;
+		vResizing: #rigid;
+		height: 20.
+	r addMorphBack: phonemeDisplay.
+	self addMorphBack: (Morph new extent: 5@8; color: Color transparent).  "spacer"
+	self addMorphBack: r.
