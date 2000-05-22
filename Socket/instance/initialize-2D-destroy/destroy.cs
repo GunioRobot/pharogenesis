5e8b7a693526e0b93e19e016067a6cf60b29a88b
JMM <unknown>
2000-05-22 22:54:00 +0000

    no message

--- Socket/instance/initialize-2D-destroy/destroy.cs
+++ Socket/instance/initialize-2D-destroy/destroy.cs
@@ -1,9 +1,11 @@
 destroy
 	"Destroy this socket. Its connection, if any, is aborted and its resources are freed. Do nothing if the socket has already been destroyed (i.e., if its socketHandle is nil)."
 
-	socketHandle = nil
-		ifFalse: [
-			self isValid ifTrue: [self primSocketDestroy: socketHandle].
-			Smalltalk unregisterExternalObject: semaphore.
-			socketHandle _ nil.
-			semaphore _ nil].
+	socketHandle = nil ifFalse: 
+		[self isValid ifTrue: [self primSocketDestroy: socketHandle].
+		Smalltalk unregisterExternalObject: semaphore.
+		Smalltalk unregisterExternalObject: readSemaphore.
+		Smalltalk unregisterExternalObject: writeSemaphore.
+		socketHandle _ nil.
+		readSemaphore _ writeSemaphore _ semaphore _ nil.
+		self unregister].
