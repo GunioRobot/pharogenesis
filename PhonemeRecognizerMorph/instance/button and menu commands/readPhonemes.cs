ae498f5b83f1c1f980a766ab88eadeefb4f1f513
jm <unknown>
2000-05-22 17:40:00 +0000

    no message

---
+++ PhonemeRecognizerMorph/instance/button and menu commands/readPhonemes.cs
@@ -0,0 +1,14 @@
+readPhonemes
+	"Read a previously saved phoneme set from a file."
+
+	| fname s newPhonemes |
+	fname _ Utilities
+		chooseFileWithSuffixFromList: #('.pho' '.phonemes')
+		withCaption: 'Phoneme file?'.
+	fname == nil ifTrue: [^ self].
+	fname ifNil: [^ self].
+
+	s _ FileStream oldFileNamed: fname.
+	newPhonemes _ s fileInObjectAndCode.
+	s close.
+	phonemeRecords _ newPhonemes.
