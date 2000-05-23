557a6c927909bef5599fff30ae2855086f96ff74
jm <unknown>
2000-05-23 06:05:00 +0000

    no message

---
+++ PhonemeRecord/instance/other/recordWithLevel-3A-.cs
@@ -0,0 +1,23 @@
+recordWithLevel: recordLevel
+	"Initialize my sound samples by recording a snippet of sound while the mouse is held down. Trim off leading and trailing silence, and normalize the level of the recording."
+
+	| recorder |
+	"record the sound"
+	recorder _ SoundRecorder new
+		samplingRate: samplingRate;
+		recordLevel: recordLevel;
+		clearRecordedSound.
+	Utilities
+		informUser: 'Recording a phoneme. Release the mouse button when done.'
+		during: [
+			recorder resumeRecording.
+			Sensor waitNoButton.
+			recorder stopRecording].
+	Utilities
+		informUser: 'Removing leading/trailing silence...'
+		during: [
+			samples _ recorder condensedSamples.
+			samples size > 0 ifTrue: [
+				samples _ self trimAndNormalize: samples]].
+
+	self clearFeatures.
