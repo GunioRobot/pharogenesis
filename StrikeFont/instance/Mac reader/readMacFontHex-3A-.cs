c5ff914056926e534c886ae3982a9dc6d90f29d8
ar <unknown>
2000-05-23 12:49:00 +0000

    no message

--- StrikeFont/instance/Mac reader/readMacFontHex-3A-.cs
+++ StrikeFont/instance/Mac reader/readMacFontHex-3A-.cs
@@ -31,10 +31,5 @@ readMacFontHex: fileName
 	"Insert one pixel between each character.  And add space character."
 	self fixKerning: (fRectWidth - maxWidth).	
 
-	"This has to do with scanning characters, not with the font"
-	stopConditions _ Array new: 258.
-	stopConditions atAllPut: nil.
-	1 to: (minAscii - 1) do:
-		[:index | stopConditions at: index put: #characterNotInFont].
-	(maxAscii + 3) to: stopConditions size do:
-		[:index | stopConditions at: index put: #characterNotInFont]
+	"Recompute character to glyph mapping"
+	characterToGlyphMap _ nil.
