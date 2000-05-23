b559f47cbb0b8d026c3cc6da2ebdb69ed6b5e7a9
ar <unknown>
2000-05-23 12:50:00 +0000

    no message

--- StrikeFont/instance/file in-2F-out/newFromStrike-3A-.cs
+++ StrikeFont/instance/file in-2F-out/newFromStrike-3A-.cs
@@ -13,18 +13,18 @@ newFromStrike: fileName
 		whileTrue: [startName _ startName - 1].
 	name _ name copyFrom: startName+1 to: name size.
 
-	type			_		strike nextWord16.		"type is ignored now -- simplest
+	type			_		strike nextWord.		"type is ignored now -- simplest
 												assumed.  Kept here to make
 												writing and consistency more
 												straightforward."
-	minAscii		_		strike nextWord16.
-	maxAscii		_		strike nextWord16.
-	maxWidth		_		strike nextWord16.
-	strikeLength	_		strike nextWord16.
-	ascent			_		strike nextWord16.
-	descent			_		strike nextWord16.
-	"xOffset			_"		strike nextWord16. 	
-	raster16			_		strike nextWord16.	
+	minAscii		_		strike nextWord.
+	maxAscii		_		strike nextWord.
+	maxWidth		_		strike nextWord.
+	strikeLength	_		strike nextWord.
+	ascent			_		strike nextWord.
+	descent			_		strike nextWord.
+	"xOffset			_"		strike nextWord. 	
+	raster16			_		strike nextWord.	
 	superscript		_		ascent - descent // 3.	
 	subscript		_		descent - ascent // 3.	
 	emphasis		_		0.
@@ -34,7 +34,7 @@ newFromStrike: fileName
 
 	xTable _ (Array new: maxAscii + 3) atAllPut: 0.
 	(minAscii + 1 to: maxAscii + 3) do:
-		[:index | xTable at: index put: strike nextWord16].
+		[:index | xTable at: index put: strike nextWord].
 
 	"Set up space character"
 	((xTable at: (Space asciiValue + 2))  = 0 or:
@@ -42,5 +42,4 @@ newFromStrike: fileName
 		ifTrue:	[(Space asciiValue + 2) to: xTable size do:
 					[:index | xTable at: index put: ((xTable at: index) + DefaultSpace)]].
 	strike close.
-
-	self setStopConditions 
+	characterToGlyphMap _ nil.
