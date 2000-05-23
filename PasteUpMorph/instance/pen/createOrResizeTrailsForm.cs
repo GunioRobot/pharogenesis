e907289ff15775e836d729942a6cd7a5d93111be
di <unknown>
2000-05-23 16:54:00 +0000

    no message

--- PasteUpMorph/instance/pen/createOrResizeTrailsForm.cs
+++ PasteUpMorph/instance/pen/createOrResizeTrailsForm.cs
@@ -2,19 +2,18 @@ createOrResizeTrailsForm
 	"If necessary, create a new turtleTrailsForm or resize the existing one to fill my bounds. On return, turtleTrailsForm exists and is the correct size."
 
 	| newForm |
-	turtleTrailsForm ifNil: [
-		"create new turtleTrailsForm if needed"
+	turtleTrailsForm ifNil:
+		["create new TrailsForm if needed"
 		turtleTrailsForm _ Form extent: self extent depth: 8.
-		turtlePen _ nil.
-		^ self].
+		turtlePen _ nil].
 
-	turtleTrailsForm extent = self extent ifFalse: [
-		"resize turtleTrailsForm if my size has changed"
+	turtleTrailsForm extent = self extent ifFalse:
+		["resize TrailsForm if my size has changed"
 		newForm _ Form extent: self extent depth: 8.
-		newForm
-			copy: self bounds
-			from: turtleTrailsForm
-			to: 0@0
-			rule: Form paint.
+		newForm copy: self bounds from: turtleTrailsForm
+					to: 0@0 rule: Form paint.
 		turtleTrailsForm _ newForm.
 		turtlePen _ nil].
+
+	"Recreate Pen for this form"
+	turtlePen ifNil: [turtlePen _ Pen newOnForm: turtleTrailsForm].
