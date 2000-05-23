23d4dd4be500131044c50de1f44fb1e7047064a2
ar <unknown>
2000-05-23 12:51:00 +0000

    no message

---
+++ StrikeFont/instance/object fileIn/convertxgnstmmmsadxrssedp0-3A-cxgntmmmsadxrssedp0-3A-.cs
@@ -0,0 +1,6 @@
+convertxgnstmmmsadxrssedp0: varDict cxgntmmmsadxrssedp0: smartRefStrm
+	"These variables are automatically stored into the new instance ('xTable' 'glyphs' 'name' 'type' 'minAscii' 'maxAscii' 'maxWidth' 'strikeLength' 'ascent' 'descent' 'xOffset' 'raster' 'subscript' 'superscript' 'emphasis' 'derivativeFonts' 'pointSize' ).
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: ('characterToGlyphMap' )  If a non-nil value is needed, please assign it."
+	"These are going away ('stopConditions' ).  Possibly store their info in another variable?"
