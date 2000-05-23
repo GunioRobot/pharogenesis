4faca051dbac3424259bc1d47cf8a8ef9dfaf54f
di <unknown>
2000-05-23 16:17:00 +0000

    no message

--- SimpleButtonMorph/instance/events/mouseDown-3A-.cs
+++ SimpleButtonMorph/instance/events/mouseDown-3A-.cs
@@ -1,5 +1,9 @@
 mouseDown: evt
 
+	| now dt |
 	oldColor _ color.
+	now _ Time millisecondClockValue.
 	actWhen == #buttonDown
 		ifTrue: [self doButtonAction].
+	dt _ Time millisecondClockValue - now max: 0.  "Time it took to do"
+	dt < 200 ifTrue: [(Delay forMilliseconds: 200-dt) wait]
