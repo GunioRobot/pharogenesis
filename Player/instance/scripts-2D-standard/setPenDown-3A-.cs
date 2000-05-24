d7d08fc0e8f7b7d2c837fc38d034eab805928b75
di <unknown>
2000-05-24 12:44:00 +0000

    no message

--- Player/instance/scripts-2D-standard/setPenDown-3A-.cs
+++ Player/instance/scripts-2D-standard/setPenDown-3A-.cs
@@ -1,2 +1,9 @@
-setPenDown: aBoolean
-	self actorState setPenDown: aBoolean
+setPenDown: penDown
+
+	| morph trailMorph tfm |
+	self actorState setPenDown: penDown.
+	((morph _ self costume) notNil and: [(trailMorph _ morph trailMorph) notNil])
+		ifTrue:
+		[tfm _ morph owner transformFrom: trailMorph.
+		trailMorph notePenDown: penDown forPlayer: self
+					at: (tfm localPointToGlobal: morph referencePosition)]
