d4d905a63ec0667fc459e0614a98fb692deca235
di <unknown>
2000-05-24 09:25:00 +0000

    no message

--- PasteUpMorph/instance/display/drawOn-3A-.cs
+++ PasteUpMorph/instance/display/drawOn-3A-.cs
@@ -2,15 +2,9 @@ drawOn: aCanvas
 	"Update and draw the turtleTrails form. See the comment in updateTrailsForm."
 
 	super drawOn: aCanvas.
-	(lastTurtlePositions ~~ nil and:
-	 [lastTurtlePositions size > 0]) ifTrue:
-		["Note: trails form is created when first pen goes down"
-		self createOrResizeTrailsForm.
-		self updateTrailsForm].
 
-	turtleTrailsForm ifNotNil:
-		[aCanvas image: turtleTrailsForm at: self position].
-	self recordTurtlePositions.
+	self updateTrailsForm.
+	turtleTrailsForm ifNotNil: [aCanvas paintImage: turtleTrailsForm at: self position].
 
 	(submorphs size > 0 and: [self indicateCursor]) ifTrue:
 		[aCanvas
