4b751c84944550abcd232a5f1125645f7b1d78bf
di <unknown>
2000-05-24 16:05:00 +0000

    no message

--- Behavior/instance/creating method dictionary/recompile-3A-from-3A-.cs
+++ Behavior/instance/creating method dictionary/recompile-3A-from-3A-.cs
@@ -1,8 +1,9 @@
 recompile: selector from: oldClass
 	"Compile the method associated with selector in the receiver's method dictionary."
+	"ar 7/10/1999: Use oldClass compiledMethodAt: not self compiledMethodAt:"
 	| method trailer methodNode |
-	method _ self compiledMethodAt: selector.
-	trailer _ (method size - 3 to: method size) collect: [:i | method at: i].
+	method _ oldClass compiledMethodAt: selector.
+	trailer _ (method endPC + 1 to: method size) collect: [:i | method at: i].
 	methodNode _ self compilerClass new
 				compile: (oldClass sourceCodeAt: selector)
 				in: self
