fa65c1bd28e2a429d7b500aa1bda7dd65b496fe1
ar <unknown>
2000-05-25 16:36:00 +0000

    no message

--- CCodeGenerator/instance/C translation/initializeCTranslationDictionary.cs
+++ CCodeGenerator/instance/C translation/initializeCTranslationDictionary.cs
@@ -13,6 +13,7 @@ initializeCTranslationDictionary
 	#+				#generatePlus:on:indent:
 	#-				#generateMinus:on:indent:
 	#*				#generateTimes:on:indent:
+	#/				#generateDivide:on:indent:
 	#//				#generateDivide:on:indent:
 	#\\				#generateModulo:on:indent:
 	#<<				#generateShiftLeft:on:indent:
@@ -33,11 +34,14 @@ initializeCTranslationDictionary
 	#>=				#generateGreaterThanOrEqual:on:indent:
 	#~=				#generateNotEqual:on:indent:
 	#==				#generateEqual:on:indent:
+	#~~				#generateNotEqual:on:indent:
 	#isNil			#generateIsNil:on:indent:
 	#notNil			#generateNotNil:on:indent:
 
 	#whileTrue: 	#generateWhileTrue:on:indent:
 	#whileFalse:	#generateWhileFalse:on:indent:
+	#whileTrue 		#generateDoWhileTrue:on:indent:
+	#whileFalse		#generateDoWhileFalse:on:indent:
 	#to:do:			#generateToDo:on:indent:
 	#to:by:do:		#generateToByDo:on:indent:
 
@@ -55,13 +59,24 @@ initializeCTranslationDictionary
 	#integerObjectOf:	#generateIntegerObjectOf:on:indent:
 	#isIntegerObject: 	#generateIsIntegerObject:on:indent:
 	#cCode:				#generateInlineCCode:on:indent:
+	#cCode:inSmalltalk:	#generateInlineCCode:on:indent:
 	#cCoerce:to:			#generateCCoercion:on:indent:
 	#preIncrement		#generatePreIncrement:on:indent:
 	#preDecrement		#generatePreDecrement:on:indent:
 	#inline:				#generateInlineDirective:on:indent:
 	#sharedCodeNamed:inCase:	#generateSharedCodeDirective:on:indent:
+	#asFloat				#generateAsFloat:on:indent:
+	#asInteger			#generateAsInteger:on:indent:
+	#anyMask:			#generateBitAnd:on:indent:
+	#raisedTo:			#generateRaisedTo:on:indent:
+
+	#perform:						#generatePerform:on:indent:
+	#perform:with:					#generatePerform:on:indent:
+	#perform:with:with:				#generatePerform:on:indent:
+	#perform:with:with:with:		#generatePerform:on:indent:
+	#perform:with:with:with:with:	#generatePerform:on:indent:
+
 	).
 
-	1 to: pairs size by: 2 do: [ :i |
-		translationDict at: (pairs at: i) put: (pairs at: i + 1).
-	].
+	1 to: pairs size by: 2 do: [:i |
+		translationDict at: (pairs at: i) put: (pairs at: i + 1)].
