11378e8eb89177d77c822eae0e092ae26d5f552e
ar <unknown>
2000-05-25 20:41:00 +0000

    no message

---
+++ ContextPart/instance/private/tryNamedPrimitiveIn-3A-for-3A-withArgs-3A-.cs
@@ -0,0 +1,19 @@
+tryNamedPrimitiveIn: aCompiledMethod for: aReceiver withArgs: arguments
+	"Hack. Attempt to execute the named primitive from the given compiled method"
+	| selector theMethod spec |
+	arguments size > 8 ifTrue:[^PrimitiveFailToken].
+	selector _ #(
+		tryNamedPrimitive 
+		tryNamedPrimitive: 
+		tryNamedPrimitive:with: 
+		tryNamedPrimitive:with:with: 
+		tryNamedPrimitive:with:with:with:
+		tryNamedPrimitive:with:with:with:with:
+		tryNamedPrimitive:with:with:with:with:with:
+		tryNamedPrimitive:with:with:with:with:with:with:
+		tryNamedPrimitive:with:with:with:with:with:with:with:) at: arguments size+1.
+	theMethod _ aReceiver class lookupSelector: selector.
+	theMethod == nil ifTrue:[^PrimitiveFailToken].
+	spec _ theMethod literalAt: 1.
+	spec replaceFrom: 1 to: spec size with: (aCompiledMethod literalAt: 1) startingAt: 1.
+	^aReceiver perform: selector withArguments: arguments
