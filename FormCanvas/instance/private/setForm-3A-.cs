70ce8512f0bc3207a657b2e4a2858dcd327ac0dc
ar <unknown>
2000-05-25 17:25:00 +0000

    no message

--- FormCanvas/instance/private/setForm-3A-.cs
+++ FormCanvas/instance/private/setForm-3A-.cs
@@ -1,13 +1,5 @@
 setForm: aForm
 
-	| screen blackWord |
 	self reset.
 	form _ aForm.
-	port _ GrafPort toForm: form.
-	shadowDrawing _ false.
-
-	"Build a 50% stipple of black for the given depth."
-	screen _ Color pixelScreenForDepth: form depth.
-	blackWord _ Color black pixelWordForDepth: form depth.
-	shadowStipple _
-		(screen collect: [:maskWord | maskWord bitAnd: blackWord]).
+	port _ self portClass toForm: form.
