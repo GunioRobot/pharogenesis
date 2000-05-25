4ea2c916f6ce7fe61a4a8339a000263368de6460
mir <unknown>
2000-05-25 13:02:00 +0000

    no message

--- EventHandler/instance/events/mouseDown-3A-fromMorph-3A-.cs
+++ EventHandler/instance/events/mouseDown-3A-fromMorph-3A-.cs
@@ -1,2 +1,8 @@
-mouseDown: event fromMorph: sourceMorph
-	^ self send: mouseDownSelector to: mouseDownRecipient withEvent: event fromMorph: sourceMorph
+mouseDown: event fromMorph: sourceMorph 
+	"Take double-clicks into account."
+	((self handlesDoubleClick: event)
+		and: [event redButtonPressed])
+		ifTrue: 
+			[event hand waitForClicksOrDrag: sourceMorph event: event.
+			^ nil]
+		ifFalse: [^ self click: event fromMorph: sourceMorph]
