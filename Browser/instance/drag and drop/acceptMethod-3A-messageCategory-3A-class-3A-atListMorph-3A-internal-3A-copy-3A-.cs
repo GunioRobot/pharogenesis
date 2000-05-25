d07d2f48f7f2b23a2b143d055febdf2bb56f84a2
mir <unknown>
2000-05-25 13:27:00 +0000

    no message

---
+++ Browser/instance/drag and drop/acceptMethod-3A-messageCategory-3A-class-3A-atListMorph-3A-internal-3A-copy-3A-.cs
@@ -0,0 +1,29 @@
+acceptMethod: methodSel messageCategory: srcMessageCategorySel class: srcClassOrMeta atListMorph: dstListMorph internal: internal copy: copyFlag 
+	| success dstClassOrMeta dstClass dstMessageCategorySel |
+	dstClass _ self dstClassDstListMorph: dstListMorph.
+	dstClassOrMeta _ dstClass
+				ifNotNil: [self metaClassIndicated
+						ifTrue: [dstClass class]
+						ifFalse: [dstClass]].
+	dstMessageCategorySel _ self dstMessageCategoryDstListMorph: dstListMorph.
+	success _ (dstClassOrMeta notNil
+				and: [dstClassOrMeta == srcClassOrMeta])
+						ifTrue: ["one class"
+							self
+								changeMessageCategoryForMethod: methodSel
+								dstMessageCategory: dstMessageCategorySel
+								srcMessageCategory: srcMessageCategorySel
+								insideClassOrMeta: dstClassOrMeta
+								internal: internal
+								copySemantic: copyFlag]
+						ifFalse: ["different classes"
+							self
+								acceptMethod: methodSel
+								dstMessageCategory: dstMessageCategorySel
+								srcMessageCategory: srcMessageCategorySel
+								dstClass: dstClass
+								dstClassOrMeta: dstClassOrMeta
+								srcClassOrMeta: srcClassOrMeta
+								internal: internal
+								copySemantic: copyFlag].
+	^ success
