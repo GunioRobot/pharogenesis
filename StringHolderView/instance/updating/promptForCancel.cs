147e2d77405f6e784cbac46d66021443e672e36e
ar <unknown>
2000-05-25 19:52:00 +0000

    no message

--- StringHolderView/instance/updating/promptForCancel.cs
+++ StringHolderView/instance/updating/promptForCancel.cs
@@ -5,7 +5,7 @@ promptForCancel
 		[(self confirm: 'Changes have not been saved.
 Is it OK to cancel those changes?') ifTrue: [model clearUserEditFlag].
 		^ self].
-	stripes _ Form extent: 16@16 fromStipple: 16r36C9.
+	stripes _ (Form extent: 16@16 fromStipple: 16r36C9) bits.
 	Display border: self insetDisplayBox width: 4
 			rule: Form reverse fillColor: stripes.
 	okToCancel _ self confirm: 'Changes have not been saved.
