1776a9377dc3524a6391aa5737a3aee7de15940d
di <unknown>
2000-05-25 06:51:00 +0000

    no message

--- CompiledMethod/class/instance creation/toReturnSelf.cs
+++ CompiledMethod/class/instance creation/toReturnSelf.cs
@@ -1,4 +1,4 @@
 toReturnSelf
 	"Answer an instance of me that is a quick return of the instance (^self)."
 
-	^ self newBytes: 0 nArgs: 0 nTemps: 0 nStack: 0 nLits: 0 primitive: 256
+	^ self toReturnSelfTrailerBytes: #(0 0 0 0)
