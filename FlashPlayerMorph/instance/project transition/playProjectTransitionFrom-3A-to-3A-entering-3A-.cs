99279123225f3ce14da70088e0051b37952defd0
ar <unknown>
2000-05-25 17:59:00 +0000

    no message

---
+++ FlashPlayerMorph/instance/project transition/playProjectTransitionFrom-3A-to-3A-entering-3A-.cs
@@ -0,0 +1,31 @@
+playProjectTransitionFrom: oldProject to: newProject entering: aBoolean
+	"Play the transition from the old to the new project."
+
+	Smalltalk isMorphic ifFalse: [^ self]. "Not in MVC"
+	self stopPlaying.
+	owner ifNotNil:[
+		self stopStepping.
+		owner privateRemoveMorph: self.
+		owner _ nil].
+	aBoolean ifTrue:[
+		self updateProjectFillsFrom: newProject.
+	] ifFalse:[
+		self updateProjectFillsFrom: oldProject.
+		self setProperty: #transitionBackground toValue: newProject imageForm.
+	].
+	self frameNumber: 1.
+	self loopFrames: false.
+	(self valueOfProperty: #fullScreenTransition ifAbsent:[false])
+		ifTrue:[self bounds: self world bounds].
+	self comeToFront.
+	self startStepping.
+	self startPlaying.
+	[playing] whileTrue:[World doOneCycleNow].
+	self stopPlaying.
+	self stopStepping.
+	owner privateRemoveMorph: self.
+	owner _ nil.
+	self removeProperty: #transitionBackground.
+	Display deferUpdates: true.
+	World fullDrawOn: (Display getCanvas).
+	Display deferUpdates: false.
