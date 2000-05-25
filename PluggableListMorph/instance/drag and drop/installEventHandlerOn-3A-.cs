ac473ba9c63dad2834cd19a63711c78bb5bebe40
mir <unknown>
2000-05-25 12:54:00 +0000

    no message

---
+++ PluggableListMorph/instance/drag and drop/installEventHandlerOn-3A-.cs
@@ -0,0 +1,24 @@
+installEventHandlerOn: morphList
+	| handler |
+	handler _ EventHandler new.
+	handler
+		on: #mouseDown send: #mouseDown:onItem: to: self;
+		on: #mouseUp send: #mouseUp:onItem: to: self;
+		on: #doubleClick send: #doubleClick:onItem: to: self.
+	self dragEnabled
+		ifTrue: [handler
+				on: #startDrag
+				send: #startDrag:onItem:
+				to: self].
+	self dropEnabled
+		ifTrue: 
+			[handler
+				on: #mouseEnterDragging
+				send: #mouseEnterDragging:onItem:
+				to: self.
+			handler
+				on: #mouseLeaveDragging
+				send: #mouseLeaveDragging:onItem:
+				to: self].
+
+	morphList do: [:m | m eventHandler: handler].
