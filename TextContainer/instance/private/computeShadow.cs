750a0e542932386c60d8243c9783125eb9e3fea0
ar <unknown>
2000-05-25 18:04:00 +0000

    no message

--- TextContainer/instance/private/computeShadow.cs
+++ TextContainer/instance/private/computeShadow.cs
@@ -1,20 +1,22 @@
 computeShadow
-	| canvas back bounds |
+	| canvas back bounds theText |
 	bounds _ self bounds.
-	canvas _ (FormCanvas extent: bounds extent depth: 1)
-			setShadowDrawing; stipple: Color black;
-			copyOffset: bounds topLeft negated.
-	self fillsOwner
-		ifTrue: [(textMorph owner copyWithoutSubmorph: textMorph) fullDrawOn: canvas]
-		ifFalse: [canvas fillRectangle: textMorph bounds color: Color black].
-	self avoidsOcclusions ifTrue:
-		[back _ canvas form deepCopy.
-		canvas form fillWhite.
-		textMorph owner submorphsInFrontOf: textMorph do:
-			[:m | (textMorph isLinkedTo: m)
-				ifTrue: []
-				ifFalse: [m fullDrawOn: canvas]].
-		back displayOn: canvas form at: 0@0 rule: Form reverse].
+	theText _ textMorph meOrMyDropShadow.
+	canvas _ (Display defaultCanvasClass extent: bounds extent depth: 1)
+			shadowColor: Color black.
+	canvas translateBy: bounds topLeft negated during:[:tempCanvas|
+		self fillsOwner
+			ifTrue: [(theText owner copyWithoutSubmorph: theText) fullDrawOn: tempCanvas]
+			ifFalse: [tempCanvas fillRectangle: textMorph bounds color: Color black].
+		self avoidsOcclusions ifTrue:
+			[back _ tempCanvas form deepCopy.
+			tempCanvas form fillWhite.
+			theText owner submorphsInFrontOf: theText do:
+				[:m | (textMorph isLinkedTo: m)
+					ifTrue: []
+					ifFalse: [m fullDrawOn: tempCanvas]].
+			back displayOn: tempCanvas form at: 0@0 rule: Form reverse].
+	].
 	shadowForm _ canvas form offset: bounds topLeft.
 	vertProfile _ shadowForm  yTallyPixelValue: 1 orNot: false.
 	rectangleCache _ Dictionary new.
