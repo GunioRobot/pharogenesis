e46492a75793f745c57d2d8adecabe953d9156e0
ar <unknown>
2000-05-25 20:27:00 +0000

    no message

---
+++ Object/instance/system primitives/tryNamedPrimitive-3A-.cs
@@ -0,0 +1,5 @@
+tryNamedPrimitive: arg1
+	"This method is a template that the Smalltalk simulator uses to 
+	execute primitives. See Object documentation whatIsAPrimitive."
+	<primitive:'' module:''>
+	^ ContextPart primitiveFailToken
