7e2fed0cf4c844b0118ca9d4b74b5035d66bdb67
ar <unknown>
2000-05-25 18:00:00 +0000

    no message

--- PaintBoxMorph/instance/actions/actionCursor.cs
+++ PaintBoxMorph/instance/actions/actionCursor.cs
@@ -1,28 +1,36 @@
 actionCursor
 	"Return the cursor to use with this painting action/tool. Offset of the form must be set."
 
-	| ff c old map width co larger box |
-	action == #paint: ifTrue: ["Make a cursor from the brush and the color"
-		old _ self getNib.
-		ff _ ColorForm extent: old extent depth: 1.
-		old displayOn: ff at: old offset negated.
-		c _ self getColor.
-		c = Color white ifTrue: [c _ Color black].
-		c isTransparent ifTrue: [c _ Color black].
-		map _ ff colors.
-		map at: (Color white indexInMap: map) put: Color transparent.
-		map at: (Color black indexInMap: map) put: c.
-		ff colors: map.
-		ff offset: ff extent // 2.
-		^ ff].
+	| ff width co larger c box |
+
+action == #paint: ifTrue: ["Make a cursor from the brush and the color"
+		width _ self getNib width.
+		c _ self ringColor.
+		co _ (currentCursor offset - ((width//4)@34-(width//6))) min: (0@0).
+		
+		larger _ width negated + 10@0 extent: currentCursor extent + (width@width).
+		ff _ currentCursor copy: larger.
+		ff colors at: 1 put: Color transparent.
+         	ff colors at: 2 put: Color transparent.
+	
+          ff offset: co - (width@width //2).
+            (ff getCanvas) fillOval: (Rectangle center: ff offset negated
+						extent: width@width)
+                        color: Color transparent
+                        borderWidth: 1
+                        borderColor: c.
+	 	 ^ ff].
+	
 	action == #erase: ifTrue: ["Make a cursor from the cursor and the color"
 		width _ self getNib width.
-		co _ (currentCursor offset - (width//2@4)) max: (0@0).
+		co _ (currentCursor offset + (width//2@4)) min: (0@0).
 		larger _ 0@0 extent: currentCursor extent + (width@width).
 		ff _ currentCursor copy: larger.
-		ff fill: (box _ co extent: (width@width)) fillColor: (Color r: 0.5 g: 0.5 b: 1.0).
+		ff offset: co - (width@width //2).
+		ff fill: (box _ co negated extent: (width@width)) 
+					fillColor: (Color r: 0.5 g: 0.5 b: 1.0).
 		ff fill: (box insetBy: 1@1) fillColor: Color transparent.
-		ff offset: co + (width@width //2).
+		
 		^ ff].
 
 	^ currentCursor
