446d06761c4cbebf20f9ed14a4b965090248d3c0
sw <unknown>
2000-05-26 13:42:00 +0000

    no message

--- TransformationMorph/instance/menu/removeFlexShell.cs
+++ TransformationMorph/instance/menu/removeFlexShell.cs
@@ -1,21 +1,23 @@
 removeFlexShell
 	"Remove the shell used to make a morph rotatable and scalable."
 
-	| oldHalo unflexed anActorState aName |
+	| oldHalo unflexed pensDown player |
 	oldHalo _ self halo.
 	submorphs isEmpty ifTrue: [^ self delete].
 	unflexed _ self firstSubmorph.
-	(anActorState _ self valueOfProperty: #actorState) ifNotNil:
-		[unflexed setProperty: #actorState toValue: anActorState].
-	(aName _ self valueOfProperty: #name) ifNotNil:
-		[unflexed setProperty: #name toValue: aName].
-
-	unflexed costumee: costumee.
-	costumee ifNotNil:
-		[costumee rawCostume: unflexed.
-		costumee _ nil].
-	self submorphs copy do: [:m |
+	pensDown _ OrderedCollection new.
+	self allMorphsDo:  "Note any pens down -- must not be down during the move"
+		[:m | ((player _ m player) notNil and: [player getPenDown]) ifTrue:
+			[m == player costume ifTrue:
+				[pensDown add: player.
+				player setPenDown: false]]].
+	self submorphs do: [:m |
 		m position: self center - (m extent // 2).
 		owner addMorph: m].
+	unflexed absorbStateFromRenderer: self.
+	pensDown do: [:p | p setPenDown: true].
 	oldHalo ifNotNil: [oldHalo setTarget: unflexed].
+	self currentWorld startSteppingSubmorphsOf: unflexed.
 	self delete.
+
+	^ unflexed
