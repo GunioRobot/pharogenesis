fa0179792fef9d080b5449960c23be7fc2105786
ar <unknown>
2000-05-26 12:49:00 +0000

    no message

---
+++ B3DHardwareRasterizer/instance/primitives/primProcessVB-3A-texture-3A-vertices-3A-vertexCount-3A-faces-3A-faceCount-3A-.cs
@@ -0,0 +1,3 @@
+primProcessVB: primitiveType texture: textureHandle vertices: vtxArray vertexCount: vtxCount faces: idxArray faceCount: idxCount
+	<primitive:'primitiveProcessVertexBuffer' module:'Squeak3DX'>
+	^self primitiveFailed
