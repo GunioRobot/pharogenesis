5d04cb40ccbe15a8d04cea3530c87943c84cec95
sw <unknown>
2000-05-26 08:57:00 +0000

    no message

--- HaloMorph/instance/private/doRot-3A-with-3A-.cs
+++ HaloMorph/instance/private/doRot-3A-with-3A-.cs
@@ -2,12 +2,19 @@ doRot: evt with: rotHandle
 	"Update the rotation of my target if it is rotatable."
 
 	| degrees |
-	degrees _ (evt cursorPoint - target referencePosition) degrees.
+	degrees _ (evt cursorPoint - (target pointInWorld: target referencePosition)) degrees.
 	degrees _ degrees - angleOffset degrees.
-	(degrees abs < 1.0) ifTrue: [degrees _ 0.0].
+	degrees _ degrees detentBy: 10.0 atMultiplesOf: 90.0 snap: false.
 	degrees = 0.0
 		ifTrue: [rotHandle color: Color lightBlue]
 		ifFalse: [rotHandle color: Color blue].
+	rotHandle submorphsDo:
+		[:m | m color: rotHandle color makeForegroundColor].
+	((innerTarget isKindOf: SketchMorph) and: [innerTarget rotationStyle == #normal]) ifTrue:
+		[self removeAllHandlesBut: rotHandle.
+		target player ifNotNil: [self addDirectionShaft]].
+
 	target rotationDegrees: degrees.
+
 	rotHandle position: evt cursorPoint - (rotHandle extent // 2).
-	self layoutChanged.
+	self layoutChanged
