5d15822712a7c78711f3ebe333cf59002c9bcf58
ar <unknown>
2000-05-27 17:23:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-textures/primGetTextureDepth-3A-.cs
@@ -0,0 +1,4 @@
+primGetTextureDepth: aHandle
+	"Primitive. Return the actual depth of the texture with the given handle"
+	<primitive:'primitiveTextureDepth' module:'Squeak3DX'>
+	^self primitiveFailed
