fc5dedece153f16043fa879363182e534269a84c
ar <unknown>
2000-05-27 17:23:00 +0000

    no message

---
+++ ExternalScreen/instance/primitives-2D-textures/primAllocateTexture-3A-width-3A-height-3A-.cs
@@ -0,0 +1,5 @@
+primAllocateTexture: d width: w height: h
+	"Primitive. Allocate a texture with the given dimensions.
+	Note: The texture allocated may *not* match the specified
+	values here."
+	^nil
