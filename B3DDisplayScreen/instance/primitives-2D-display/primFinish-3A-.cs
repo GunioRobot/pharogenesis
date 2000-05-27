7a617b20d6e36ef0a797978b021b0eeafdb86c83
ar <unknown>
2000-05-27 17:20:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-display/primFinish-3A-.cs
@@ -0,0 +1,5 @@
+primFinish: aHandle
+	"Primitive. Finish all rendering operations on the receiver.
+	Do not return before all rendering operations have taken effect."
+	<primitive: 'primitiveFinishDisplaySurface' module:'Squeak3DX'>
+	^nil
