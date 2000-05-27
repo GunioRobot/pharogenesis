26ec21f8c43fc4b451a09943c418a99f06d943de
ar <unknown>
2000-05-27 17:18:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-display/primDestroyDisplaySurface-3A-.cs
@@ -0,0 +1,4 @@
+primDestroyDisplaySurface: aHandle
+	"Primitive. Destroy the display surface associated with the given handle."
+	<primitive: 'primitiveDestroyDisplaySurface' module:'Squeak3DX'>
+	^nil
