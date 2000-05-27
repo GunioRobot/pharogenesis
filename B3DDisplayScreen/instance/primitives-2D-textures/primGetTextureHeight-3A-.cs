3d9fbbc3c67e139b7ce6e9532448f2b7936dc1eb
ar <unknown>
2000-05-27 17:24:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-textures/primGetTextureHeight-3A-.cs
@@ -0,0 +1,4 @@
+primGetTextureHeight: aHandle
+	"Primitive. Return the actual height of the texture with the given handle"
+	<primitive:'primitiveTextureHeight' module:'Squeak3DX'>
+	^self primitiveFailed
