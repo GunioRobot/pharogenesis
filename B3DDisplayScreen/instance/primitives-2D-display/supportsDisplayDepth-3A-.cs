fe9c1215d998762896492c96f9cdb6032a899b03
ar <unknown>
2000-05-27 17:21:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-display/supportsDisplayDepth-3A-.cs
@@ -0,0 +1,4 @@
+supportsDisplayDepth: pixelDepth
+	"Return true if this pixel depth is supported on the current host platform."
+	<primitive: 'primitiveSupportsDisplayDepth' module:'Squeak3DX'>
+	^false
