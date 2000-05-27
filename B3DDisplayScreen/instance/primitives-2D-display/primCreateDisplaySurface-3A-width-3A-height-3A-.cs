9f0a0eaa1fdd37198a85e2af0daf8760c9eb783b
ar <unknown>
2000-05-27 17:18:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-display/primCreateDisplaySurface-3A-width-3A-height-3A-.cs
@@ -0,0 +1,4 @@
+primCreateDisplaySurface: d width: w height: h
+	"Primitive. Create a new external display surface. Return the handle used to identify the receiver. Fail if the surface cannot be created."
+	<primitive:'primitiveCreateDisplaySurface' module: 'Squeak3DX'>
+	^nil
