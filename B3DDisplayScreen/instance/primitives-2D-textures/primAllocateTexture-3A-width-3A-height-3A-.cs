17b0b7d673b4dd573f1e25cac8d4aec2ac66a975
ar <unknown>
2000-05-27 20:21:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-textures/primAllocateTexture-3A-width-3A-height-3A-.cs
@@ -0,0 +1,5 @@
+primAllocateTexture: d width: w height: h
+	"Primitive. Allocate a texture with the given dimensions.
+	Note: The texture allocated may *not* match the specified values here."
+	<primitive:'primitiveAllocateTexture' module:'Squeak3DX'>
+	^nil
