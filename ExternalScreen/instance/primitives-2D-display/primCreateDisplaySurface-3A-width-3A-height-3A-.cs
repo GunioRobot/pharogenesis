6a83529af65f1cf8ec5d0fbc3147cfc3a0f0d902
ar <unknown>
2000-05-27 17:18:00 +0000

    no message

---
+++ ExternalScreen/instance/primitives-2D-display/primCreateDisplaySurface-3A-width-3A-height-3A-.cs
@@ -0,0 +1,3 @@
+primCreateDisplaySurface: d width: w height: h
+	"Primitive. Create a new external display surface. Return the handle used to identify the receiver. Fail if the surface cannot be created."
+	^nil
