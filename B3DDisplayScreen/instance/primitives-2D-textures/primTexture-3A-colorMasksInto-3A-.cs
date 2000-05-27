75fba1f11afd40c8e97b6db29f2b40f21153b948
ar <unknown>
2000-05-27 17:24:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-textures/primTexture-3A-colorMasksInto-3A-.cs
@@ -0,0 +1,4 @@
+primTexture: aHandle colorMasksInto: anArray
+	"Primitive. Store the bit masks for each color into the given array."
+	<primitive: 'primitiveTextureGetColorMasks' module:'Squeak3DX'>
+	^nil
