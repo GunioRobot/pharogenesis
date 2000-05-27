b63c818e163f5f5bf6567eb47f654b5f566928f8
ar <unknown>
2000-05-27 17:22:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-forms/primForm-3A-colorMasksInto-3A-.cs
@@ -0,0 +1,4 @@
+primForm: aHandle colorMasksInto: anArray
+	"Primitive. Store the bit masks for each color into the given array."
+	<primitive: 'primitiveFormGetColorMasks' module:'Squeak3DX'>
+	^nil
