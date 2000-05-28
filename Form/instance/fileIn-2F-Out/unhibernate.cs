12f3d4cdad28839d228d364bacd6477343514d43
ar <unknown>
2000-05-28 00:52:00 +0000

    no message

--- Form/instance/fileIn-2F-Out/unhibernate.cs
+++ Form/instance/fileIn-2F-Out/unhibernate.cs
@@ -1,7 +1,7 @@
 unhibernate
 	"If my bitmap has been compressed into a ByteArray,
 	then expand it now, and return true."
-
+	bits == nil ifTrue:[bits _ Bitmap new: self bitsSize. ^true].
 	(bits isMemberOf: ByteArray)
 		ifTrue: [bits _ Bitmap decompressFromByteArray: bits. ^ true].
 	^ false
