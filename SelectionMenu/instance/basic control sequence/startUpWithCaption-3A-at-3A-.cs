b4b674fed2a41b6b21cdf2595e7f1cda877dce72
sma <unknown>
2000-05-28 15:28:00 +0000

    no message

--- SelectionMenu/instance/basic control sequence/startUpWithCaption-3A-at-3A-.cs
+++ SelectionMenu/instance/basic control sequence/startUpWithCaption-3A-at-3A-.cs
@@ -1,6 +1,8 @@
 startUpWithCaption: captionOrNil at: location
 	"Overridden to return value returned by manageMarker."
+
 	| index |
 	index _ super startUpWithCaption: captionOrNil at: location.
-	(selections = nil or: [(index between: 1 and: selections size) not]) ifTrue: [^ nil].
+	(selections = nil or: [(index between: 1 and: selections size) not])
+		ifTrue: [^ nil].
 	^ selections at: index
