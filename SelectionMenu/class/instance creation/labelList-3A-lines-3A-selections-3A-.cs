ebec913f4f3b9dcdb6e55222e79db6307c53b3e9
sma <unknown>
2000-05-28 16:04:00 +0000

    no message

--- SelectionMenu/class/instance creation/labelList-3A-lines-3A-selections-3A-.cs
+++ SelectionMenu/class/instance creation/labelList-3A-lines-3A-selections-3A-.cs
@@ -1,6 +1,2 @@
 labelList: labelList lines: lines selections: selections
-	^ self labels: (String streamContents:
-			[:strm |  "Concatenate labels with CRs"
-			labelList do: [:each | strm nextPutAll: each; cr].
-			strm skip: -1])  "No CR at end"
-		lines: lines selections: selections
+	^ (self labelArray: labelList lines: lines) selections: selections
