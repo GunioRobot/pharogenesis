792079bf8c9776c701384882a6c8bc4a13f3e451
ar <unknown>
2000-05-28 12:08:00 +0000

    no message

--- Form/instance/copying/copy-3A-from-3A-in-3A-rule-3A-.cs
+++ Form/instance/copying/copy-3A-from-3A-in-3A-rule-3A-.cs
@@ -1,6 +1,6 @@
 copy: destRectangle from: sourcePt in: sourceForm rule: rule 
 	"Make up a BitBlt table and copy the bits."
-	(BitBlt toForm: self)
+	(BitBlt current toForm: self)
 		copy: destRectangle
 		from: sourcePt in: sourceForm
 		fillColor: nil rule: rule
