c0bab8ec8a3d2d0382b05d4c6be019c61011732b
sma <unknown>
2000-05-28 11:09:00 +0000

    no message

--- MessageSet/instance/contents/contents.cs
+++ MessageSet/instance/contents/contents.cs
@@ -1,4 +1,9 @@
 contents
-	contents == nil
-		ifTrue: [^ '']
-		ifFalse: [^ contents]
+	^ contents == nil
+		ifTrue: [currentCompiledMethod _ nil. '']
+		ifFalse: [messageListIndex = 0 
+			ifTrue: [currentCompiledMethod _ nil. contents]
+			ifFalse: [editSelection == #byteCodes
+				ifTrue: [(self selectedClassOrMetaClass
+							compiledMethodAt: self selectedMessageName) symbolic]
+				ifFalse: [self selectedMessage]]]
