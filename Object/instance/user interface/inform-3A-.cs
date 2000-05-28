0b557b60e19f5de9e4bebd0dfa9eda43df6b469f
sma <unknown>
2000-05-28 15:59:00 +0000

    no message

--- Object/instance/user interface/inform-3A-.cs
+++ Object/instance/user interface/inform-3A-.cs
@@ -1,4 +1,4 @@
 inform: aString
-	"Display a message for the user to read and then dismiss.  6/9/96 sw"
+	"Display a message for the user to read and then dismiss. 6/9/96 sw"
 
-	aString size > 0 ifTrue: [(PopUpMenu labels: '  OK  ') startUpWithCaption: aString]
+	aString isEmptyOrNil ifFalse: [PopUpMenu inform: aString]
