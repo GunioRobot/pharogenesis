d380a91a025a3e8fa2c9526e6fc4236b06173dff
ar <unknown>
2000-05-28 12:09:00 +0000

    no message

--- Form/instance/image manipulation/smear-3A-distance-3A-.cs
+++ Form/instance/image manipulation/smear-3A-distance-3A-.cs
@@ -1,7 +1,7 @@
 smear: dir distance: dist
 	"Smear any black pixels in this form in the direction dir in Log N steps"
 	| skew bb |
-	bb _ BitBlt destForm: self sourceForm: self fillColor: nil
+	bb _ BitBlt current destForm: self sourceForm: self fillColor: nil
 		combinationRule: Form under destOrigin: 0@0 sourceOrigin: 0@0
 		extent: self extent clipRect: self boundingBox.
 	skew _ 1.
