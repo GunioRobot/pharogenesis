7e2e4caa4ca5b0cdd5584e70fe1a7b3cedd99343
sma <unknown>
2000-05-28 16:17:00 +0000

    no message

---
+++ EmphasizedMenu/class/examples/example3.cs
@@ -0,0 +1,6 @@
+example3
+	"EmphasizedMenu example3"
+
+	^ (self
+		selectionAndEmphasisPairs: #('how' #bold 'well' #plain 'does' #italic 'this' #struckOut 'work' #plain))
+		startUpWithCaption: 'A Menu with Emphases'
