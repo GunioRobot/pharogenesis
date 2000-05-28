063fb84dd5af2accba0bfff516af5af2a9fb8e98
sma <unknown>
2000-05-28 16:14:00 +0000

    no message

--- EmphasizedMenu/class/instance creation/selections-3A-emphases-3A-.cs
+++ EmphasizedMenu/class/instance creation/selections-3A-emphases-3A-.cs
@@ -1,6 +1,10 @@
 selections: selList emphases: emphList
-	"Answer an instance of the receiver with the given selections and emphases.  "
+	"Answer an instance of the receiver with the given selections and 
+	emphases."
 
 	^ (self selections: selList) emphases: emphList
 
-"(EmphasizedMenu selections: #('how' 'well' 'does' 'this' 'work?') emphases: #(bold 0 italic struckOut plain)) startUp" 
+"Example:
+	(EmphasizedMenu
+		selections: #('how' 'well' 'does' 'this' 'work?') 
+		emphases: #(bold plain italic struckOut plain)) startUp"
