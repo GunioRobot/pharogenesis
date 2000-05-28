42f0902a573b5fd998128309bbc97de505a23c26
ar <unknown>
2000-05-28 12:08:00 +0000

    no message

--- Form/instance/image manipulation/primCountBits.cs
+++ Form/instance/image manipulation/primCountBits.cs
@@ -1,7 +1,9 @@
 primCountBits
 	"Count the non-zero pixels of this form."
-	^ (BitBlt toForm: self)
+	depth > 8 ifTrue:
+		[^(self asFormOfDepth: 8) primCountBits].
+	^ (BitBlt current toForm: self)
 		fillColor: (Bitmap with: 0);
 		destRect: (0@0 extent: width@height);
-		combinationRule: 22;
+		combinationRule: 32;
 		copyBits
