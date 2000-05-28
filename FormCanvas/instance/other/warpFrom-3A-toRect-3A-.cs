707dcc24da91526e61514131b70d7faf97277d59
ar <unknown>
2000-05-28 12:12:00 +0000

    no message

--- FormCanvas/instance/other/warpFrom-3A-toRect-3A-.cs
+++ FormCanvas/instance/other/warpFrom-3A-toRect-3A-.cs
@@ -1,5 +1,5 @@
 warpFrom: sourceQuad toRect: destRect
-	^ (WarpBlt toForm: port destForm)
-		combinationRule: Form paint;
-		sourceQuad: sourceQuad destRect: (destRect translateBy: origin);
-		clipRect: clipRect
+        ^ (WarpBlt current toForm: port destForm)
+                combinationRule: Form paint;
+                sourceQuad: sourceQuad destRect: (destRect translateBy: origin);
+                clipRect: clipRect
