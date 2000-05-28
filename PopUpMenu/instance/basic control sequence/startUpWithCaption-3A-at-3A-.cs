75ff07bdbf444cc4c4ef04ea4554c4d2b2d2f878
sma <unknown>
2000-05-28 12:11:00 +0000

    no message

--- PopUpMenu/instance/basic control sequence/startUpWithCaption-3A-at-3A-.cs
+++ PopUpMenu/instance/basic control sequence/startUpWithCaption-3A-at-3A-.cs
@@ -1,18 +1,27 @@
 startUpWithCaption: captionOrNil at: location
-	"Display the menu, with caption if supplied. Wait for the mouse button to go down, then track the selection as long as the button is pressed. When the button is released, answer the index of the current selection, or zero if the mouse is not released over any menu item. Location specifies the desired topLeft of the menu body rectangle."
+	"Display the menu, with caption if supplied. Wait for the mouse button to go down,
+	then track the selection as long as the button is pressed. When the button is released, 
+	answer the index of the current selection, or zero if the mouse is not released over 
+	any menu item. Location specifies the desired topLeft of the menu body rectangle."
+
 	| maxHeight |
 	maxHeight _ Display height*3//4.
-	frame height > maxHeight ifTrue:
-		[^ self startUpSegmented: maxHeight withCaption: captionOrNil at: location].
-
-	World ifNotNil: [
-		^ (MVCMenuMorph from: self title: captionOrNil)
-			invokeAt: location in: World].
-
-	Cursor normal showWhile:
-		[self displayAt: location
+	self frameHeight > maxHeight ifTrue:
+		[^ self
+			startUpSegmented: maxHeight
 			withCaption: captionOrNil
-			during: [[Sensor anyButtonPressed] whileFalse: [].
-					[Sensor anyButtonPressed] whileTrue: [self manageMarker]]].
+			at: location].
 
+	Smalltalk isMorphic
+		ifTrue:
+			[selection _ Cursor normal showWhile:
+				[(MVCMenuMorph from: self title: captionOrNil) invokeAt: location in: World].
+			^ selection].
+
+	frame ifNil: [self computeForm].
+	Cursor normal showWhile:
+		[self
+			displayAt: location
+			withCaption: captionOrNil
+			during: [self controlActivity]].
 	^ selection
