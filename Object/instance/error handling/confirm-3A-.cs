e8ffea105c82733b1894a50cea2966ff088c2dc8
sma <unknown>
2000-05-28 15:48:00 +0000

    no message

--- Object/instance/error handling/confirm-3A-.cs
+++ Object/instance/error handling/confirm-3A-.cs
@@ -1,5 +1,8 @@
-confirm: queryString 
-	"Put up a yes/no menu with caption aString. Answer true if the response is yes, false if no. This is a modal question--the user must respond yes or no."
+confirm: queryString
+	"Put up a yes/no menu with caption queryString. Answer true if the 
+	response is yes, false if no. This is a modal question--the user must 
+	respond yes or no."
+
 	"nil confirm: 'Are you hungry?'"
 
-	^ SelectionMenu confirm: queryString
+	^ PopUpMenu confirm: queryString
