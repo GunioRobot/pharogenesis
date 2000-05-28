4049d3a81b41409a4d2c1d97e4dc805d9d8f14e7
ar <unknown>
2000-05-28 12:12:00 +0000

    no message

---
+++ BalloonCanvas/instance/private/image-3A-at-3A-sourceRect-3A-rule-3A-.cs
@@ -0,0 +1,19 @@
+image: aForm at: aPoint sourceRect: sourceRect rule: rule
+	| warp dstRect srcQuad dstOffset center |
+	(self ifNoTransformWithIn: sourceRect) & false
+		ifTrue:[^super image: aForm at: aPoint sourceRect: sourceRect rule: rule].
+	dstRect _ (transform localBoundsToGlobal: (aForm boundingBox translateBy: aPoint)).
+	dstOffset _ 0@0. "dstRect origin."
+	"dstRect _ 0@0 corner: dstRect extent."
+	center _ 0@0."transform globalPointToLocal: dstRect origin."
+	srcQuad _ transform globalPointsToLocal: (dstRect innerCorners).
+	srcQuad _ srcQuad collect:[:pt| pt - aPoint].
+	warp _ (WarpBlt current toForm: Display)
+			sourceForm: aForm;
+			cellSize: 2;  "installs a new colormap if cellSize > 1"
+			combinationRule: Form over.
+	warp copyQuad: srcQuad toRect: (dstRect translateBy: dstOffset).
+
+	self frameRectangle: (aForm boundingBox translateBy: aPoint) color: Color green.
+
+	"... TODO ... create a bitmap fill style from the form and use it for a simple rectangle."
