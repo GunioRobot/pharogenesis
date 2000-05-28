6d12e659309c09d6f70518ba916c43206f77135d
sma <unknown>
2000-05-28 15:28:00 +0000

    no message

--- SelectionMenu/instance/basic control sequence/invokeOn-3A-.cs
+++ SelectionMenu/instance/basic control sequence/invokeOn-3A-.cs
@@ -1,14 +1,14 @@
 invokeOn: targetObject
-	"Pop up this menu and return the result of sending to the target object
-	 the selector corresponding to the menu item selected by the user. Return
-	 nil if no item is selected."
+	"Pop up this menu and return the result of sending to the target object 
+	the selector corresponding to the menu item selected by the user. Return 
+	nil if no item is selected."
 
 	| sel |
 	sel _ self startUp.
 	sel = nil ifFalse: [^ targetObject perform: sel].
 	^ nil
 
-"(SelectionMenu labels:
-'sin
+"Example:
+	(SelectionMenu labels: 'sin
 cos
 neg' lines: #() selections: #(sin cos negated)) invokeOn: 0.7"
