b409d37f2c0841c43f445cceca57a55ab2cd6868
ar <unknown>
2000-05-28 01:47:00 +0000

    no message

---
+++ B3DDisplayScreen/instance/primitives-2D-display/primBltFast-3A-to-3A-at-3A-from-3A-extent-3A-.cs
@@ -0,0 +1,4 @@
+primBltFast: displayHandle to: dstHandle at: destOrigin from: sourceOrigin extent: extent
+	"Primitive. Perform a fast blt operation. Return the receiver if successful."
+	<primitive:'primitiveBltFromDisplay' module:'Squeak3DX'>
+	^nil
