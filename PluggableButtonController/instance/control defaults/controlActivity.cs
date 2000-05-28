d428489695894188aab240fb931c07c616724f08
sma <unknown>
2000-05-28 16:29:00 +0000

    no message

--- PluggableButtonController/instance/control defaults/controlActivity.cs
+++ PluggableButtonController/instance/control defaults/controlActivity.cs
@@ -1,3 +1,7 @@
 controlActivity 
 
-	^ self
+	shownAsComplemented ifNil: [^ self].
+	shownAsComplemented = self viewHasCursor
+		ifFalse:
+			[view ifNotNil: [view toggleMouseOverFeedback]. 
+			shownAsComplemented _ shownAsComplemented not]
