735c74886ef16773ef7b1a352f36dfe858ee15ec
sma <unknown>
2000-05-28 16:09:00 +0000

    no message

--- SelectionMenu/class/instance creation/labels-3A-lines-3A-selections-3A-.cs
+++ SelectionMenu/class/instance creation/labels-3A-lines-3A-selections-3A-.cs
@@ -1,6 +1,7 @@
-labels: aString lines: linesArray selections: selectionsArray
-	"Answer an instance of me whose items are in aString, with lines drawn 
-	after each item indexed by anArray. Record the given array of selections
-	corresponding to the items in labelsArray."
+labels: labels lines: linesArray selections: selectionsArray
+	"Answer an instance of me whose items are in labels, with lines drawn  
+	after each item indexed by linesArray. Labels can be either a string  
+	with embedded CRs, or a collection of strings. Record the given array of 
+	selections corresponding to the items in labels."
 
-	^ (self labels: aString lines: linesArray) selections: selectionsArray
+	^ (self labels: labels lines: linesArray) selections: selectionsArray
