ed24d59dc7d7da7b09ed87cfef944e440a46fc77
sma <unknown>
2000-05-28 15:55:00 +0000

    no message

--- Object/instance/error handling/confirm-3A-orCancel-3A-.cs
+++ Object/instance/error handling/confirm-3A-orCancel-3A-.cs
@@ -1,14 +1,6 @@
 confirm: aString orCancel: cancelBlock
-	"Put up a yes/no/cancel menu with caption aString.
-	Answer true if the response is yes, false if no.
-	If cancel is chosen, evaluate cancelBlock."
+	"Put up a yes/no/cancel menu with caption aString. Answer true if  
+	the response is yes, false if no. If cancel is chosen, evaluate  
+	cancelBlock. This is a modal question--the user must respond yes or no."
 
-	| choice |
-	[true] whileTrue:
-	[choice _ (PopUpMenu labels:
-'yes
-no
-cancel') startUpWithCaption: aString.
-	choice = 1 ifTrue: [^ true].
-	choice = 2 ifTrue: [^ false].
-	choice = 3 ifTrue: [^ cancelBlock value]]
+	^ PopUpMenu confirm: aString orCancel: cancelBlock
