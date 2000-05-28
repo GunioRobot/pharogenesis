d3807ceab474c44b772b8aba02ceb91bceb14c7a
ar <unknown>
2000-05-28 12:13:00 +0000

    no message

--- Project/instance/menu messages/makeThumbnail.cs
+++ Project/instance/menu messages/makeThumbnail.cs
@@ -1,11 +1,11 @@
 makeThumbnail
 	"Make a thumbnail image of this project from the Display."
 
-	viewSize ifNotNil: [
-		thumbnail _ Form extent: viewSize depth: Display depth.
-		(WarpBlt toForm: thumbnail)
+	viewSize ifNil: [viewSize _ Display extent // 8].
+	thumbnail _ Form extent: viewSize depth: Display depth.
+	(WarpBlt current toForm: thumbnail)
 			sourceForm: Display;
 			cellSize: 2;  "installs a colormap"
 			combinationRule: Form over;
 			copyQuad: (Display boundingBox) innerCorners
-			toRect: (0@0 extent: viewSize)].
+			toRect: (0@0 extent: viewSize).
