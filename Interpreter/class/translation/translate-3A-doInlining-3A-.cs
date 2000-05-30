67ccddedb033e0b7f7ef40262d522daa0254f5d4
jm <unknown>
2000-05-30 08:53:00 +0000

    no message

--- Interpreter/class/translation/translate-3A-doInlining-3A-.cs
+++ Interpreter/class/translation/translate-3A-doInlining-3A-.cs
@@ -2,15 +2,6 @@ translate: fileName doInlining: inlineFlag
 	"Time millisecondsToRun: [
 		Interpreter translate: 'interp.c' doInlining: true.
 		Smalltalk beep]"
-	| cg |
-	BitBltSimulation initialize.
-	Interpreter initialize.
-	ObjectMemory initialize.
-	cg _ CCodeGenerator new initialize.
-	cg addClass: BitBltSimulation.
-	cg addClass: Interpreter.
-	cg addClass: ObjectMemory.
-	BitBltSimulation declareCVarsIn: cg.
-	Interpreter declareCVarsIn: cg.
-	ObjectMemory declareCVarsIn: cg.
-	cg storeCodeOnFile: fileName doInlining: inlineFlag.
+	"Interpreter patchInterp: 'Squeak VM PPC'"
+
+	self translate: fileName doInlining: inlineFlag forBrowserPlugin: false.
