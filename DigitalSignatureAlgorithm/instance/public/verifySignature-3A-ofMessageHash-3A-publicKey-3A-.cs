db351820a50fa7afb845f6532e4829088cfe2cdc
raa <unknown>
2000-05-30 15:49:00 +0000

    no message

---
+++ DigitalSignatureAlgorithm/instance/public/verifySignature-3A-ofMessageHash-3A-publicKey-3A-.cs
@@ -0,0 +1,19 @@
+verifySignature: aSignature ofMessageHash: hash publicKey: publicKey
+	"Answer true if the given signature is the authentic signature of the given message hash. That is, if the signature must have been computed using the private key set corresponding to the given public key. The public key is an array of four large integers: (p, q, g, y)."
+
+	| p q g y r s w u1 u2 v0 v |
+	p _ publicKey first.
+	q _ publicKey second.
+	g _ publicKey third.
+	y _ publicKey fourth.
+	r _ aSignature first.
+	s _ aSignature last.
+	((r > 0) and: [r < q]) ifFalse: [^ false].  "reject"
+	((s > 0) and: [s < q]) ifFalse: [^ false].  "reject"
+
+	w _ self inverseOf: s mod: q.
+	u1 _ (hash * w) \\ q.
+	u2 _ (r * w) \\ q.
+	v0 _ (self raise: g to: u1 mod: p) * (self raise: y to: u2 mod: p).
+	v _ ( v0 \\ p) \\ q.
+	^ v = r
