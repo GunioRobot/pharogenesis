ef71bbff76157b76f5e54eab2e58b5c980c4187e
RAA <unknown>
2000-05-31 08:46:00 +0000

    no message

---
+++ DigitalSignatureAlgorithm/class/public/sign-3A-privateKey-3A-dsa-3A-.cs
@@ -0,0 +1,12 @@
+sign: aStringOrStream privateKey: privateKey dsa: dsa
+	"Sign the given message (a stream or string) and answer a signature string."
+	"Note: Unguessable random numbers are needed for message signing. The user will be prompted to type a really long random string (two or three lines) to initialize the random number generator before signing a message. A different random string should be typed for every session; it is not a password and we wish to produce different random number streams."
+
+	| hasher h sig |
+
+	hasher _ SecureHashAlgorithm new.
+	(aStringOrStream class isBytes)
+		ifTrue: [h _ hasher hashMessage: aStringOrStream]
+		ifFalse: [h _ hasher hashStream: aStringOrStream].
+	sig _ dsa computeSignatureForMessageHash: h privateKey: privateKey.
+	^ dsa signatureToString: sig
