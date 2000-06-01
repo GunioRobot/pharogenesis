8257207764ff4825c425f0409ae23e0190cf6c17
sma <unknown>
2000-06-01 09:50:00 +0000

    no message

--- Interval/instance/printing/printOn-3A-.cs
+++ Interval/instance/printing/printOn-3A-.cs
@@ -1,11 +1,7 @@
 printOn: aStream
-
-	aStream nextPut: $(.
-	start printOn: aStream.
-	aStream nextPutAll: ' to: '.
-	stop printOn: aStream.
-	step ~= 1
-		ifTrue: 
-			[aStream nextPutAll: ' by: '.
-			step printOn: aStream].
+	aStream nextPut: $(;
+	 print: start;
+	 nextPutAll: ' to: ';
+	 print: stop.
+	step ~= 1 ifTrue: [aStream nextPutAll: ' by: '; print: step].
 	aStream nextPut: $)
