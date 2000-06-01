fb18d5b141b336413370681e996b0658947e87d5
sma <unknown>
2000-06-01 10:00:00 +0000

    no message

--- Message/instance/printing/printOn-3A-.cs
+++ Message/instance/printing/printOn-3A-.cs
@@ -1,8 +1,8 @@
 printOn: aStream 
 	"Refer to the comment in Object|printOn:."
  
-	aStream nextPutAll: 'a Message with selector: '.
-	selector printOn: aStream.
-	aStream nextPutAll: ' and arguments: '.
-	args printOn: aStream.
-	^aStream
+	super printOn: aStream.
+	aStream nextPutAll: ' with selector: ';
+	 print: selector;
+	 nextPutAll: ' and arguments: ';
+	 print: args
