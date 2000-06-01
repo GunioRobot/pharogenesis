f2eec32f174f22448287e580c648f807a7303eff
sma <unknown>
2000-06-01 09:31:00 +0000

    no message

--- Object/instance/printing/printOn-3A-.cs
+++ Object/instance/printing/printOn-3A-.cs
@@ -1,10 +1,9 @@
-printOn: aStream 
-	"Append to the argument, aStream, a sequence of characters that 
+printOn: aStream
+	"Append to the argument, aStream, a sequence of characters that  
 	identifies the receiver."
 
 	| title |
 	title _ self class name.
-	aStream nextPutAll: ((title at: 1) isVowel
-							ifTrue: ['an ']
-							ifFalse: ['a '])
-						, title
+	aStream
+		nextPutAll: (title first isVowel ifTrue: ['an '] ifFalse: ['a ']);
+		nextPutAll: title
