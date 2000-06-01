2ff863505f5504f299297ee0a920e4d6fc6928b2
sw <unknown>
2000-06-01 22:30:00 +0000

    no message

--- PasteUpMorph/instance/dropping-2F-grabbing/morphToDropFrom-3A-.cs
+++ PasteUpMorph/instance/dropping-2F-grabbing/morphToDropFrom-3A-.cs
@@ -1,15 +1,32 @@
 morphToDropFrom: aMorph
-	"Presently only in service for features in the tile-scripting system that allow a different object from the one being carried to be dropped"
+	| itsSelector aScriptor adjustment anEditor actualObject aUserScript ownersChoice aNail representee |
 
-	| itsSelector aScriptor adjustment anEditor actualObject aUserScript |
+	owner ifNotNil:
+		[(ownersChoice _ owner substituteForMorph: aMorph beingDroppedOn: self)
+			ifNotNil:	[^ ownersChoice]].
+
+	self alwaysShowThumbnail ifTrue:
+		[aNail _ aMorph representativeNoTallerThan: self maxHeightToAvoidThumbnailing norWiderThan: self maximumThumbnailWidth thumbnailHeight: self heightForThumbnails.
+		aNail == aMorph ifFalse:
+			[aNail position: (self primaryHand position - ((self primaryHand targetOffset - self primaryHand formerPosition) * (aNail extent / aMorph extent)) rounded)].
+		^ aNail].
+
+	((aMorph isKindOf: MorphThumbnail) and: [(representee _ aMorph morphRepresented) owner == nil])
+		ifTrue:
+			[representee position: (self primaryHand position - ((self primaryHand targetOffset - self primaryHand formerPosition) * (representee extent / aMorph extent)) rounded).
+			^ representee].
+
 	self expandPhrasesToScripts ifFalse: [^ aMorph].
 
-	(aMorph hasProperty: #newAnonymousScript) ifTrue: [^ self emptyAnonymousScriptorFrom: aMorph].
+	(aMorph hasProperty: #newPermanentScript) ifTrue: [^ self emptyPermanentScriptorFrom: aMorph].
+
 	(aMorph isKindOf: PhraseTileMorph) ifFalse: [^ aMorph].
-
+	aMorph isCommand ifFalse: [^ aMorph].
 	(actualObject _ aMorph actualObject) ifNil: [^ aMorph].
+	actualObject assureUniClass.
 
-	aScriptor _ (itsSelector _ aMorph userScriptSelector) size > 0
+	itsSelector _ aMorph userScriptSelector.
+	aScriptor _ (itsSelector ~~ nil and: [itsSelector size > 0])
 		ifTrue:
 			[actualObject isFlagshipForClass
 				ifFalse:
@@ -29,11 +46,13 @@ morphToDropFrom: aMorph
 							anEditor]
 						ifTrue:
 							[adjustment _ 60 @ 20.
-							actualObject anonymousScriptEditorFor: aMorph]]]
+							actualObject permanentScriptEditorFor: aMorph]]]
 
 		ifFalse:   "It's a system-defined selector; construct an anonymous scriptor around it"
 			[adjustment _ 60 @ 20.
-			actualObject anonymousScriptEditorFor: aMorph].
+			actualObject permanentScriptEditorFor: aMorph].
 
 	aScriptor position: (self primaryHand position - adjustment).
+	(aScriptor isMemberOf: ScriptEditorMorph) ifTrue:
+		[aScriptor playerScripted expungeEmptyUnRenamedScripts].
 	^ aScriptor
