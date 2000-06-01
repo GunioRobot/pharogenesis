61db5a4eda495e3b89b00d7a849d43a83f548e40
sma <unknown>
2000-06-01 16:43:00 +0000

    no message

---
+++ ScrollPane/instance/object fileIn/convertbosfcebbmsopssrsggs0-3A-bosfcebbmsopssrsggsh0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcebbmsopssrsggs0: varDict bosfcebbmsopssrsggsh0: smartRefStrm
+	"These variables are automatically stored into the new instance #('bounds' 'owner' 'submorphs' 'fullBounds' 'color' 'extension' 'borderWidth' 'borderColor' 'model' 'slotName' 'open' 'pinSpecs' 'scrollBar' 'scroller' 'retractableScrollBar' 'scrollBarOnLeft' 'getMenuSelector' 'getMenuTitleSelector' 'scrollBarHidden').
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+	"New variables: #('hasFocus')  If a non-nil value is needed, please assign it."
