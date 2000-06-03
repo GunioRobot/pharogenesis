c3f25f6b7fdfdd0088fcb0e3806a6de6748e8e3f
sma <unknown>
2000-06-03 22:03:00 +0000

    no message

--- Behavior/instance/testing method dictionary/thoroughWhichSelectorsReferTo-3A-special-3A-byte-3A-.cs
+++ Behavior/instance/testing method dictionary/thoroughWhichSelectorsReferTo-3A-special-3A-byte-3A-.cs
@@ -1,15 +1,16 @@
 thoroughWhichSelectorsReferTo: literal special: specialFlag byte: specialByte
-	"Answer a set of selectors whose methods access the argument as a literal.  Dives into the compact literal notation, making it slow but thorough"
-	| who method |
+	"Answer a set of selectors whose methods access the argument as a 
+	literal. Dives into the compact literal notation, making it slow but 
+	thorough "
+
+	| who |
 	who _ Set new.
-	methodDict associationsDo:
-		[:assn |
-		method _ assn value.
-		((method hasLiteralSuchThat: [:lit | lit == literal]) or:
-				[specialFlag and: [method scanFor: specialByte]])
+	self selectorsAndMethodsDo:
+		[:sel :method |
+		((method hasLiteralThorough: literal) or: [specialFlag and: [method scanFor: specialByte]])
 			ifTrue:
-			[((literal isMemberOf: Association) not
-				or: [method sendsToSuper not
+				[((literal isMemberOf: Association) not
+					or: [method sendsToSuper not
 					or: [method literals allButLast includes: literal]])
-				ifTrue: [who add: assn key]]].
+						ifTrue: [who add: sel]]].
 	^ who
