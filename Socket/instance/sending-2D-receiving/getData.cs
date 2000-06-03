bdd639756e77c28080a577fe1eb56bbe518c997c
JMM <unknown>
2000-06-03 21:48:00 +0000

    no message

--- Socket/instance/sending-2D-receiving/getData.cs
+++ Socket/instance/sending-2D-receiving/getData.cs
@@ -3,6 +3,6 @@ getData
 
 	| buf bytesRead |
 	(self waitForDataUntil: Socket standardDeadline) ifFalse: [self error: 'getData timeout'].
-	buf _ String new: 2000.
+	buf _ String new: 4000.
 	bytesRead _ self primSocket: socketHandle receiveDataInto: buf startingAt: 1 count: buf size.
 	^ buf copyFrom: 1 to: bytesRead
