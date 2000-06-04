8f3d6bfd62a17e920ebf6e670d00d8c84888cd9b
sw <unknown>
2000-06-04 23:58:00 +0000

    no message

--- PasteUpMorph/instance/layout/fixLayout.cs
+++ PasteUpMorph/instance/layout/fixLayout.cs
@@ -1,10 +1,12 @@
 fixLayout
-	"Pack my submorphs into rows that fit within my width."
+	"Pack my submorphs into rows that fit within my width, if autoLineLayout is true."
 
 	| nextY i morphsForThisRow |
-	nextY _ bounds top + borderWidth.
-	i _ 1.
-	[i <= submorphs size] whileTrue: [
-		morphsForThisRow _ self rowMorphsStartingAt: i.
-		nextY _ self layoutRow: morphsForThisRow lastRowBase: nextY.
-		i _ i + morphsForThisRow size].
+	self invalidRect: bounds.
+	self autoLineLayout ifTrue:
+		[nextY _ bounds top + borderWidth.
+		i _ 1.
+		[i <= submorphs size] whileTrue:
+			[morphsForThisRow _ self rowMorphsStartingAt: i.
+			nextY _ self layoutRow: morphsForThisRow lastRowBase: nextY.
+			i _ i + morphsForThisRow size]]
