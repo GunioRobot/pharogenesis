0f8dfbaaa97807e6e850e8ffcf849077bc473bd3
sma <unknown>
2000-06-05 13:33:00 +0000

    no message

---
+++ PluggableTextMorph/instance/menu commands/yellowButtonActivity.cs
@@ -0,0 +1,7 @@
+yellowButtonActivity
+	"Called when the shifted-menu's 'more' item is chosen"
+	| menu |
+	(menu _ self getMenu: false) ifNotNil:
+		["Set up to use perform:orSendTo: for model/view dispatch"
+		menu setInvokingView: self.
+		menu popUpEvent: self currentEvent]
