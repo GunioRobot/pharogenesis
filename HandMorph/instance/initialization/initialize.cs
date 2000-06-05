a65bbf1266716f30352b987cfd4f69f55da4b988
jwh <unknown>
2000-06-05 07:39:00 +0000

    no message

--- HandMorph/instance/initialization/initialize.cs
+++ HandMorph/instance/initialization/initialize.cs
@@ -3,13 +3,13 @@ initialize
 	self initForEvents.
 	keyboardFocus _ nil.
 	mouseOverMorphs _ OrderedCollection new.
+	dragOverMorphs _ OrderedCollection new.
 	bounds _ 0@0 extent: Cursor normal extent.
 	userInitials _ ''.
 	damageRecorder _ DamageRecorder new.
+	cachedCanvasHasHoles _ false.
 	grid _ 4@4.
 	gridOn _ false.
 	remoteConnections _ OrderedCollection new.
 	lastEventTransmitted _ MorphicEvent new.
-	mouseOverTimes _ Dictionary new.
-	temporaryCursor _ nil.
-	temporaryCursorOffset _ 0@0.
+	temporaryCursor _ temporaryCursorOffset _ nil
