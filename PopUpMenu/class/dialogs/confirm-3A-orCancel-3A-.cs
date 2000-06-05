f01fa3dff7c511932a0e800cc139706c94eaee63
sma <unknown>
2000-06-05 09:11:00 +0000

    no message

---
+++ PopUpMenu/class/dialogs/confirm-3A-orCancel-3A-.cs
@@ -0,0 +1,13 @@
+confirm: queryString orCancel: cancelBlock
+	"Put up a yes/no/cancel menu with caption aString. Answer true if  
+	the response is yes, false if no. If cancel is chosen, evaluate  
+	cancelBlock. This is a modal question--the user must respond yes or no."
+
+	"PopUpMenu confirm: 'Reboot universe' orCancel: [^'Nevermind']"
+
+	| menu choice |
+	menu _ PopUpMenu labelArray: {'Yes'. 'No'. 'Cancel'}.
+	choice _ menu startUpWithCaption: queryString.
+	choice = 1 ifTrue: [^ true].
+	choice = 2 ifTrue: [^ false].
+	^ cancelBlock value
