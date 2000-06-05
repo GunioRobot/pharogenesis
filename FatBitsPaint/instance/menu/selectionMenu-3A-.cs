a87411fa25107804b9b257a9cb0a69ae164ecc81
sma <unknown>
2000-06-05 13:24:00 +0000

    no message

---
+++ FatBitsPaint/instance/menu/selectionMenu-3A-.cs
@@ -0,0 +1,20 @@
+selectionMenu: evt
+
+        | menu |
+ 
+        (menu _ MenuMorph new)
+                addTitle: 'Edit';
+                addStayUpItem.
+
+        {
+                {'edit separately'. #editSelection}.
+                {'copy'. #copySelection}.
+                {'cut'. #cutSelection}.
+                {'paste'. #pasteSelection}
+        } do: [:each |
+                menu add: each first
+                        target: self
+                        selector: each second
+                        argumentList: #()].
+        menu toggleStayUp: nil.
+        menu popUpEvent: evt
