7910510e0daca08b12c3bbac1f371101d08b12d3
di <unknown>
2000-06-05 14:39:00 +0000

    no message

--- Morph/instance/private/privateMoveBy-3A-.cs
+++ Morph/instance/private/privateMoveBy-3A-.cs
@@ -1,9 +1,15 @@
 privateMoveBy: delta
 	"Private! Use 'position:' instead."
-
-	fullBounds == bounds ifTrue: [
-		"optimization: avoids recomputing fullBounds"
-		fullBounds _ bounds _ bounds translateBy: delta.
-	] ifFalse: [
-		bounds _ bounds translateBy: delta.
-		fullBounds _ nil].
+	| fill |
+	(extension == nil or: [extension player == nil]) ifFalse:
+		["Most cases eliminated fast by above test"
+		self getPenDown ifTrue:
+			["If this is a costume for a player with its pen down, draw a line."
+			self moveWithPenDownBy: delta]].
+	fullBounds == bounds
+		ifTrue: ["optimization: avoids recomputing fullBounds"
+				fullBounds _ bounds _ bounds translateBy: delta]
+		ifFalse: [bounds _ bounds translateBy: delta.
+				fullBounds _ nil].
+	fill _ self fillStyle.
+	fill isOrientedFill ifTrue:[fill origin: fill origin + delta].
