57b49a725e6899b18ed74d4343a16d6bc19cc513
sma <unknown>
2000-06-05 13:33:00 +0000

    no message

--- PluggableTextMorph/instance/menu commands/scrollBarMenuButtonPressed-3A-.cs
+++ PluggableTextMorph/instance/menu commands/scrollBarMenuButtonPressed-3A-.cs
@@ -1,6 +1,6 @@
 scrollBarMenuButtonPressed: event
 	| menu |
-	(menu _ self getMenu) ifNotNil:
+	(menu _ self getMenu: event shiftPressed) ifNotNil:
 		["Set up to use perform:orSendTo: for model/view dispatch"
 		menu setInvokingView: self.
-		menu popUpAt: event cursorPoint event: event]
+		menu popUpEvent: event]
