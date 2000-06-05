9fbe1ba3f04d6423763bf98d53855f538088ec6c
sma <unknown>
2000-06-05 13:55:00 +0000

    no message

--- MenuMorph/instance/control/popUpAt-3A-event-3A-.cs
+++ MenuMorph/instance/control/popUpAt-3A-event-3A-.cs
@@ -1,5 +1,4 @@
 popUpAt: aPoint event: evt
 	"Present this menu at the given point in response to the given event."
 
-	originalEvent _ evt.
-	self popUpAt: aPoint forHand: evt hand.
+	self popUpAt: aPoint forHand: evt hand
