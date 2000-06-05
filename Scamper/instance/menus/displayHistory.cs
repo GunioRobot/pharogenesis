9fac39aa62401502fe80a6ba30e1ee59b27e8bec
sma <unknown>
2000-06-05 14:00:00 +0000

    no message

---
+++ Scamper/instance/menus/displayHistory.cs
@@ -0,0 +1,12 @@
+displayHistory
+	"Let the user selecet a previous page to view."
+
+	| menu |
+	menu _ MenuMorph entitled: 'Recent URLs'.
+	menu defaultTarget: self.
+	menu addStayUpItem.
+	menu addLine.
+	recentDocuments reverseDo:
+		[:doc |
+		menu add: doc url toText selector: #displayDocument: argument: doc].
+	menu popUpForHand: self currentHand
