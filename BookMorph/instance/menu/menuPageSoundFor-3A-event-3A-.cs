0c5062fb802c0c208f868b5b2b79d4ce5e5398ca
sma <unknown>
2000-06-05 13:44:00 +0000

    no message

---
+++ BookMorph/instance/menu/menuPageSoundFor-3A-event-3A-.cs
@@ -0,0 +1,13 @@
+menuPageSoundFor: target event: evt
+	| tSpec menu |
+	tSpec _ self transitionSpecFor: target.
+	menu _ (MenuMorph entitled: 'Choose a sound
+(it is now ' , tSpec first , ')') defaultTarget: target.
+	SampledSound soundNames do:
+		[:soundName |
+		menu add: soundName target: target
+			selector: #setProperty:toValue:
+			argumentList: (Array with: #transitionSpec
+								with: (tSpec copy at: 1 put: soundName; yourself))].
+
+	menu popUpEvent: evt
