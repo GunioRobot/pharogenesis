cc13a7b1fb09fa6c19582eb650a430c00bd5b41e
jwh <unknown>
2000-06-05 08:48:00 +0000

    no message

---
+++ HandMorph/instance/cursor/showTemporaryCursor-3A-hotSpotOffset-3A-.cs
@@ -0,0 +1,14 @@
+showTemporaryCursor: cursorOrNil hotSpotOffset: hotSpotOffset
+	"Set the temporary cursor to the given Form.
+	If the argument is nil, revert to the normal hardware cursor."
+
+	self changed.
+	cursorOrNil == nil
+		ifTrue: [temporaryCursor _ temporaryCursorOffset _ nil]
+		ifFalse: [temporaryCursor _ cursorOrNil asCursorForm.
+				temporaryCursorOffset _ temporaryCursor offset - hotSpotOffset].
+	bounds _ self cursorBounds.
+	self 
+		userInitials: userInitials;
+		layoutChanged;
+		changed
