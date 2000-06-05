db2e2611a8036eb31a3210cf856ba88a5dc854cb
sw <unknown>
2000-06-05 14:22:00 +0000

    no message

--- BookMorph/instance/menu/invokeBookMenu.cs
+++ BookMorph/instance/menu/invokeBookMenu.cs
@@ -1,23 +1,48 @@
 invokeBookMenu
-	"Answer a menu to be popped up from the book-control panel"
+	"Invoke the book's control panel menu."
 	| aMenu |
-	aMenu _ CustomMenu new.
-	aMenu addList:	#(
-		"	('border color...' 		changeBorderColor:)
-			('border width...' 		changeBorderWidth:)
-			('lock'					lock)"
-			('make bookmark'		bookmarkForThisPage)
-			('sort pages'				sortPages:)
-			('remove control panel'	deleteControls)
-		).
+	aMenu _ MenuMorph new defaultTarget: self.
+	aMenu addStayUpItem.
+	aMenu add: 'find...' action: #textSearch.
+	aMenu add: 'go to page...' action: #goToPage.
+	aMenu addLine.
+
+	aMenu addList:
+		#(('sort pages'			sortPages)
+		('uncache page sorter'	uncachePageSorter)).
+	(self hasProperty: #dontWrapAtEnd)
+		ifTrue: [aMenu add: 'wrap after last page' selector: #setWrapPages: argument: true]
+		ifFalse: [aMenu add: 'stop at last page' selector: #setWrapPages: argument: false].
+	aMenu addList:
+		#(('make bookmark'		bookmarkForThisPage)
+		('make thumbnail'		thumbnailForThisPage)).
+	aMenu addUpdating: #showingPageControlsString action: #toggleShowingOfPageControls.
+
+	aMenu addLine.
+	aMenu add: 'sound effect for all pages' action: #menuPageSoundForAll:.
+	aMenu add: 'sound effect this page only' action: #menuPageSoundForThisPage:.
+	aMenu add: 'visual effect for all pages' action: #menuPageVisualForAll:.
+	aMenu add: 'visual effect this page only' action: #menuPageVisualForThisPage:.
+
+	aMenu addLine.
 	(self primaryHand classOfPasteBuffer isKindOf: PasteUpMorph class) ifTrue:
-		[aMenu add: 'paste book page'	action: #pasteBookPage].
+		[aMenu add: 'paste book page'   action: #pasteBookPage].
 
 	aMenu add: 'save as new-page prototype' action: #setNewPagePrototype.
 	newPagePrototype ifNotNil: [
 		aMenu add: 'clear new-page prototype' action: #clearNewPagePrototype].
 
-	aMenu add: (openToDragNDrop ifTrue: ['close'] ifFalse: ['open']) , ' dragNdrop'
-			action: #openCloseDragNDrop.
+	aMenu add: (self dragNDropEnabled ifTrue: ['close'] ifFalse: ['open']) , ' dragNdrop'
+			action: #toggleDragNDrop.
+	aMenu add: 'make all pages this size' action: #makeUniformPageSize.
+	aMenu add: 'send all pages to server' action: #savePagesOnURL.
+	aMenu add: 'send this page to server' action: #saveOneOnURL.
+	aMenu add: 'reload all from server' action: #reload.
+	aMenu add: 'copy page url to clipboard' action: #copyUrl.
+	aMenu add: 'keep in one file' action: #keepTogether.
 
-	aMenu invokeOn: self defaultSelection: nil
+	aMenu addLine.
+	aMenu add: 'load PPT images from slide #1' action: #loadImagesIntoBook.
+	aMenu add: 'background color for all pages...' action: #setPageColor.
+
+	aMenu popUpEvent: self world activeHand lastEvent
