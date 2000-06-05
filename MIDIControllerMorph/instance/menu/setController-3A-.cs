b0912b9d12f5ebbde9f99da008424a8d3143f74e
sma <unknown>
2000-06-05 13:29:00 +0000

    no message

---
+++ MIDIControllerMorph/instance/menu/setController-3A-.cs
@@ -0,0 +1,10 @@
+setController: evt
+	| menu |
+	menu _ MenuMorph new.
+	self controllerList do: [:pair |
+		menu add: (pair last)
+			target: self
+			selector: #controller:
+			argumentList: (Array with: pair first)].
+
+	menu popUpEvent: evt
