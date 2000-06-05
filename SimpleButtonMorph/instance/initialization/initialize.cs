2e3f9c8f26824607ab29c1fafc380ea6a1a644e1
di <unknown>
2000-06-05 09:08:00 +0000

    no message

--- SimpleButtonMorph/instance/initialization/initialize.cs
+++ SimpleButtonMorph/instance/initialization/initialize.cs
@@ -1,11 +1,3 @@
 initialize
 
-	super initialize.
-	self borderWidth: 1.
-	self borderColor: #raised.
-	self color: (Color r: 0.4 g: 0.8 b: 0.6).
-	target _ nil.
-	actionSelector _ #flash.
-	arguments _ EmptyArray.
-	actWhen _ #buttonUp.
-	self label: 'Flash'.
+	self initializeAllButLabel; setDefaultLabel
