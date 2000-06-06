f43ff74f87e6b63ab3ab79564b2a820d41bbb85d
JMM <unknown>
2000-06-06 00:41:00 +0000

    no message

--- Interpreter/instance/processes/signalSemaphoreWithIndex-3A-.cs
+++ Interpreter/instance/processes/signalSemaphoreWithIndex-3A-.cs
@@ -1,12 +1,14 @@
 signalSemaphoreWithIndex: index
-	"If it is not there already, record the given semaphore index in the list of semaphores to be signaled at the next convenient moment. Set the interruptCheckCounter to zero to force a real interrupt check as soon as possible."
+	"Record the given semaphore index in the double buffer semaphores array to be signaled at the next convenient moment. Set the interruptCheckCounter to zero to force a real interrupt check as soon as possible."
 
 	index <= 0 ifTrue: [^ nil].  "bad index; ignore it"
 
+	semaphoresUseBufferA ifTrue: 
+		[semaphoresToSignalCountA < SemaphoresToSignalSize ifTrue: [
+			semaphoresToSignalCountA _ semaphoresToSignalCountA + 1.
+			semaphoresToSignalA at: semaphoresToSignalCountA put: index]]
+	ifFalse:
+		[semaphoresToSignalCountB < SemaphoresToSignalSize ifTrue: [
+			semaphoresToSignalCountB _ semaphoresToSignalCountB + 1.
+			semaphoresToSignalB at: semaphoresToSignalCountB put: index]].
 	interruptCheckCounter _ 0.
-	1 to: semaphoresToSignalCount do: [:i |
-		(semaphoresToSignal at: i) = index ifTrue: [^ nil]].
-
-	semaphoresToSignalCount < SemaphoresToSignalSize ifTrue: [
-		semaphoresToSignalCount _ semaphoresToSignalCount + 1.
-		semaphoresToSignal at: semaphoresToSignalCount put: index].
