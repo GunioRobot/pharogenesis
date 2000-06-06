67a70ab73f4600b82c8179a9617327574f6096e8
JMM <unknown>
2000-06-06 20:36:00 +0000

    no message

--- SystemDictionary/instance/special objects/clearExternalObjects.cs
+++ SystemDictionary/instance/special objects/clearExternalObjects.cs
@@ -2,4 +2,4 @@ clearExternalObjects
 	"Clear the array of objects that have been registered for use in non-Smalltalk code."
 	"Smalltalk clearExternalObjects"
 
-	Smalltalk specialObjectsArray at: 39 put: Array new.
+	ExternalSemaphoreTable clearExternalObjects
