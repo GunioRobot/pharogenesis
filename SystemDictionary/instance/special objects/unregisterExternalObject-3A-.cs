34c41da75cb420612b1e8049e22a96c6328805d8
JMM <unknown>
2000-06-06 20:40:00 +0000

    no message

--- SystemDictionary/instance/special objects/unregisterExternalObject-3A-.cs
+++ SystemDictionary/instance/special objects/unregisterExternalObject-3A-.cs
@@ -1,8 +1,4 @@
 unregisterExternalObject: anObject
 	"Unregister the given object in the external objects array. Do nothing if it isn't registered."
 
-	| objects |
-	anObject ifNil: [^ self].
-	objects _ self specialObjectsArray at: 39.
-	1 to: objects size do: [:i |
-		(objects at: i) == anObject ifTrue: [objects at: i put: nil]].
+	ExternalSemaphoreTable unregisterExternalObject: anObject
