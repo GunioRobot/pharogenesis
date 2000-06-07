f92ed5a4217d78c111d27bc1a332941ce3b28360
JMM <unknown>
2000-06-07 11:47:00 +0000

    no message

--- Interpreter/class/translation/declareCVarsIn-3A-.cs
+++ Interpreter/class/translation/declareCVarsIn-3A-.cs
@@ -1,8 +1,28 @@
 declareCVarsIn: aCCodeGenerator
-
-	aCCodeGenerator var: 'methodCache'
+	aCCodeGenerator 
+		var: #interpreterProxy 
+		type: #'struct VirtualMachine*'.
+	aCCodeGenerator
+		var: #methodCache
 		declareC: 'int methodCache[', (MethodCacheSize + 1) printString, ']'.
-	aCCodeGenerator var: 'localIP' declareC: 'char * localIP'.
-	aCCodeGenerator var: 'localSP' declareC: 'char * localSP'.
-	aCCodeGenerator var: 'semaphoresToSignal'
-		declareC: 'int semaphoresToSignal[', (SemaphoresToSignalSize + 1) printString, ']'.
+	aCCodeGenerator
+		var: #atCache
+		declareC: 'int atCache[', (AtCacheTotalSize + 1) printString, ']'.
+	aCCodeGenerator var: #localIP type: #'char*'.
+	aCCodeGenerator var: #localSP type: #'char*'.
+	aCCodeGenerator var: 'semaphoresToSignalA'
+		declareC: 'int semaphoresToSignalA[', (SemaphoresToSignalSize + 1) printString, ']'.
+	aCCodeGenerator var: 'semaphoresToSignalB'
+		declareC: 'int semaphoresToSignalB[', (SemaphoresToSignalSize + 1) printString, ']'.
+	aCCodeGenerator
+		var: #compilerHooks
+		declareC: 'int (*compilerHooks[', (CompilerHooksSize + 1) printString, '])()'.
+	aCCodeGenerator
+		var: #interpreterVersion
+		declareC: 'const char *interpreterVersion = "', Smalltalk version, ' [', Smalltalk lastUpdateString,']"'.
+	aCCodeGenerator
+		var: #obsoleteIndexedPrimitiveTable
+		declareC: 'char* obsoleteIndexedPrimitiveTable[][3] = ', self obsoleteIndexedPrimitiveTableString.
+	aCCodeGenerator
+		var: #obsoleteNamedPrimitiveTable
+		declareC: 'const char* obsoleteNamedPrimitiveTable[][3] = ', self obsoleteNamedPrimitiveTableString.
