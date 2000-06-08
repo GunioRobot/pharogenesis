82ef36b1cd13b6aa1fcd7d0cb43f819dd976567a
sr <unknown>
2000-06-08 02:11:00 +0000

    no message

--- LargePositiveInteger/instance/bit manipulation/highBit.cs
+++ LargePositiveInteger/instance/bit manipulation/highBit.cs
@@ -1,8 +1,7 @@
 highBit
-	"Answer the index of the high order bit of the receiver, or zero if the receiver is zero. This method is allowed (and needed) for LargeNegativeIntegers as well, since Squeak's LargeIntegers are sign/magnitude."
-	| realLength lastDigit |
-	realLength _ self digitLength.
-	[(lastDigit _ self digitAt: realLength) = 0]
-		whileTrue:
-		[(realLength _ realLength - 1) = 0 ifTrue: [^ 0]].
-	^ lastDigit highBit + (8 * (realLength - 1))
+	"Answer the index of the high order bit of the receiver, or zero if the  
+	receiver is zero. Raise an error if the receiver is negative, since  
+	negative integers are defined to have an infinite number of leading 1's 
+	in 2's-complement arithmetic. Use >>highBitOfMagnitude if you want to  
+	get the highest bit of the magnitude."
+	^ self highBitOfMagnitude
