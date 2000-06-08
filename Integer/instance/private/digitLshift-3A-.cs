dfabc134e5375729470ab588320d2a7c0a6996e6
sr <unknown>
2000-06-08 01:30:00 +0000

    no message

--- Integer/instance/private/digitLshift-3A-.cs
+++ Integer/instance/private/digitLshift-3A-.cs
@@ -1,14 +1,16 @@
-digitLshift: shiftCount
+digitLshift: shiftCount 
 	| carry rShift mask len result digit byteShift bitShift highBit |
-	(highBit _ self highBit) = 0 ifTrue: [^ 0].
+	(highBit _ self highBitOfMagnitude) = 0 ifTrue: [^ 0].
 	len _ highBit + shiftCount + 7 // 8.
 	result _ Integer new: len neg: self negative.
 	byteShift _ shiftCount // 8.
 	bitShift _ shiftCount \\ 8.
-	bitShift = 0 ifTrue:  
-		["Fast version for byte-aligned shifts"
-		^ result replaceFrom: byteShift+1 to: len
-				with: self startingAt: 1].
+	bitShift = 0 ifTrue: ["Fast version for byte-aligned shifts"
+		^ result
+			replaceFrom: byteShift + 1
+			to: len
+			with: self
+			startingAt: 1].
 	carry _ 0.
 	rShift _ bitShift - 8.
 	mask _ 255 bitShift: 0 - bitShift.
@@ -16,7 +18,8 @@ digitLshift: shiftCount
 	1 to: len - byteShift do: 
 		[:i | 
 		digit _ self digitAt: i.
-		result digitAt: i + byteShift 
-			put: (((digit bitAnd: mask) bitShift: bitShift) bitOr: carry).
+		result digitAt: i + byteShift put: (((digit bitAnd: mask)
+				bitShift: bitShift)
+				bitOr: carry).
 		carry _ digit bitShift: rShift].
 	^ result
