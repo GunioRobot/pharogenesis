f48899f99e6869e438042fe92847bd34a97d13e5
sr <unknown>
2000-06-08 02:07:00 +0000

    no message

--- SmallInteger/instance/bit manipulation/highBit.cs
+++ SmallInteger/instance/bit manipulation/highBit.cs
@@ -1,14 +1,8 @@
 highBit
-	"Answer the index of the high order bit of the receiver, or zero if the receiver is zero. Raise an error if the receiver is negative, since negative integers are defined to have an infinite number of leading 1's in 2's-complement arithmetic."
-
-	| shifted bitNo |
-	self < 0 ifTrue: [self error: 'highBit is not defined for negative integers'].
-	shifted _ self.
-	bitNo _ 0.
-	[shifted < 16] whileFalse:
-		[shifted _ shifted bitShift: -4.
-		bitNo _ bitNo + 4].
-	[shifted = 0] whileFalse:
-		[shifted _ shifted bitShift: -1.
-		bitNo _ bitNo + 1].
-	^ bitNo
+	"Answer the index of the high order bit of the receiver, or zero if the  
+	receiver is zero. Raise an error if the receiver is negative, since  
+	negative integers are defined to have an infinite number of leading 1's 
+	in 2's-complement arithmetic. Use >>highBitOfMagnitude if you want to 
+	get the highest bit of the magnitude."
+	self < 0 ifTrue: [^ self error: 'highBit is not defined for negative integers'].
+	^ self highBitOfPositiveReceiver
