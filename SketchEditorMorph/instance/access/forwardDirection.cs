8edf1df4226936efb0d0bd6efc9aba5e7fab76d0
sw <unknown>
2000-06-09 18:48:00 +0000

    no message

--- SketchEditorMorph/instance/access/forwardDirection.cs
+++ SketchEditorMorph/instance/access/forwardDirection.cs
@@ -2,6 +2,9 @@ forwardDirection
 	"The direction object will go when issued a sent forward:.  Up is
 zero.  Clockwise like a compass.  From the arrow control."
 
-| bb |
-bb _ (self valueOfProperty: #fwdButton).
-^ (self center - bb vertices first) degrees - 90.0
+	| bb result |
+	bb _ (self valueOfProperty: #fwdButton).
+	result _ (self center - bb vertices first) degrees - 90.0.
+	result abs < 1.0e-10 ifTrue: [result _ 0].  
+	"Workaround because the above can yield spurious miscroscopic but nonzero values"
+	^ result
