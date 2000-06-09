375332dd3a09f6193e87b4f8778c39133b279b70
sr <unknown>
2000-06-09 10:09:00 +0000

    no message

--- Integer/instance/bit manipulation/bitShift-3A-.cs
+++ Integer/instance/bit manipulation/bitShift-3A-.cs
@@ -1,11 +1,11 @@
 bitShift: shiftCount 
-	"Answer an Integer whose value (in twos-complement representation) is 
+	"Answer an Integer whose value (in twos-complement representation) is  
 	the receiver's value (in twos-complement representation) shifted left by 
-	the number of bits indicated by the argument. Negative arguments shift 
-	right. Zeros are shifted in from the right in left shifts."
-	| rShift |
-	shiftCount >= 0 ifTrue: [^ self digitLshift: shiftCount].
-	rShift _ 0 - shiftCount.
-	^ (self digitRshift: (rShift bitAnd: 7)
-				bytes: (rShift bitShift: -3)
-				lookfirst: self digitLength) normalize
+	the number of bits indicated by the argument. Negative arguments  
+	shift right. Zeros are shifted in from the right in left shifts."
+	| magnitudeShift |
+	magnitudeShift _ self bitShiftMagnitude: shiftCount.
+	^ ((self negative and: [shiftCount negative])
+		and: [self anyBitOfMagnitudeFrom: 1 to: shiftCount negated])
+		ifTrue: [magnitudeShift - 1]
+		ifFalse: [magnitudeShift]
