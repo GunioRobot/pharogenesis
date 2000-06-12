2f87768bdfb230f6cab069572e78a54991b99d82
BJP <unknown>
2000-06-12 18:53:00 +0000

    no message

--- String/instance/accessing/skipAnySubStr-3A-startingAt-3A-.cs
+++ String/instance/accessing/skipAnySubStr-3A-startingAt-3A-.cs
@@ -1,6 +1,5 @@
 skipAnySubStr: delimiters startingAt: start 
 	"Answer the index of the last character within the receiver, starting at start, that does NOT match one of the delimiters. delimiters is a Array of substrings (Characters also allowed).  If the receiver is all delimiters, answer size + 1."
-
 	| any this ind ii |
 	ii _ start-1.
 	[(ii _ ii + 1) <= self size] whileTrue: [ "look for char that does not match"
@@ -9,11 +8,13 @@ skipAnySubStr: delimiters startingAt: start
 			delim class == Character 
 				ifTrue: [(self at: ii) == delim ifTrue: [any _ true]]
 				ifFalse: ["a substring"
-					ind _ 0.
-					this _ true.
-					delim do: [:dd | 
-						dd == (self at: ii+ind) ifFalse: [this _ false].
-						ind _ ind + 1].
-					this ifTrue: [ii _ ii + delim size - 1.  any _ true]]].
+					delim size > (self size - ii + 1) ifFalse: "Here's where the one-off error was."
+						[ind _ 0.
+						this _ true.
+						delim do: [:dd | 
+							dd == (self at: ii+ind) ifFalse: [this _ false].
+							ind _ ind + 1].
+						this ifTrue: [ii _ ii + delim size - 1.  any _ true]]
+							ifTrue: [any _ false] "if the delim is too big, it can't match"]].
 		any ifFalse: [^ ii]].
 	^ self size + 1
