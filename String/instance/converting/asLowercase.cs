c2285895fc8056bdc30b640589d76120578e6001
sma <unknown>
2000-06-12 11:40:00 +0000

    no message

--- String/instance/converting/asLowercase.cs
+++ String/instance/converting/asLowercase.cs
@@ -2,7 +2,4 @@ asLowercase
 	"Answer a String made up from the receiver whose characters are all 
 	lowercase."
 
-	| aStream |
-	aStream _ WriteStream on: (String new: self size).
-	self do: [:aCharacter | aStream nextPut: aCharacter asLowercase].
-	^aStream contents
+	^ self copy asString translateToLowercase
