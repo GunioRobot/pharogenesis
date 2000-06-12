d28ccdcd828866e7f5a9a6e1ac1c7d2642ea5233
sma <unknown>
2000-06-12 11:41:00 +0000

    no message

--- String/instance/converting/asUppercase.cs
+++ String/instance/converting/asUppercase.cs
@@ -2,7 +2,4 @@ asUppercase
 	"Answer a String made up from the receiver whose characters are all 
 	uppercase."
 
-	| aStream |
-	aStream _ WriteStream on: (String new: self size).
-	self do: [:aCharacter | aStream nextPut: aCharacter asUppercase].
-	^aStream contents
+	^ self collect: [:each | each asUppercase]
