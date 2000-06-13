719f08f3460a66233fafa3c74d35ea3f6747915e
rca <unknown>
2000-06-13 12:31:00 +0000

    no message

--- Date/instance/printing/printOn-3A-format-3A-.cs
+++ Date/instance/printing/printOn-3A-format-3A-.cs
@@ -1,6 +1,5 @@
 printOn: aStream format: formatArray 
-	"Print a description of the receiver on aStream using the format denoted 
-	by the argument, formatArray:
+	"Print a description of the receiver on aStream using the format denoted the argument, formatArray:
 		#(item item item sep monthfmt yearfmt twoDigits)
 		items:  1=day  2=month  3=year  will appear in the order given,
 		separated by sep which is eaither an ascii code or character.
@@ -8,32 +7,47 @@ printOn: aStream format: formatArray
 		yearFmt:  1=1996  2=96
 		digits:  (missing or)1=9  2=09.
 	See the examples in printOn: and mmddyy"
-	| monthIndex element monthFormat twoDigits monthDay |
-	twoDigits _ formatArray size > 6 and: [(formatArray at: 7) > 1].
-	monthIndex _ self monthIndex.
+
+	| gregorian twoDigits element monthFormat  |
+	gregorian _ self asGregorian.
+	twoDigits _ formatArray size > 6 and: [ (formatArray at: 7) > 1 ].
+
 	1 to: 3 do: 
-		[:elementIndex | 
-		element _ formatArray at: elementIndex.
+	[ :i | 
+		element _ formatArray at: i.
 		element = 1 ifTrue:
-			[monthDay _ day - self firstDayOfMonth + 1.
-			twoDigits & (monthDay < 10) ifTrue: [aStream nextPutAll: '0'].
-				monthDay printOn: aStream].
+		[ 
+			twoDigits 
+				ifTrue: [ aStream nextPutAll: (gregorian first asString padded: #left to: 2 with: $0) ]
+				ifFalse: [ gregorian first printOn: aStream ]
+		].
 		element = 2 ifTrue: 
-			[monthFormat _ formatArray at: 5.
+		[
+			monthFormat _ formatArray at: 5.
 			monthFormat = 1 ifTrue:
-				[twoDigits & (monthIndex < 10) ifTrue: [aStream nextPutAll: '0'].
-				monthIndex printOn: aStream].
+			[
+				twoDigits 
+					ifTrue: [ aStream nextPutAll: (gregorian middle asString padded: #left to: 2 with: $0) ]
+					ifFalse: [ gregorian middle printOn: aStream ].
+			].
 			monthFormat = 2 ifTrue:
-				[aStream nextPutAll: ((MonthNames at: monthIndex)
-												copyFrom: 1 to: 3)].
+			[
+				aStream nextPutAll: ((MonthNames at: gregorian middle) copyFrom: 1 to: 3)
+			].
 			monthFormat = 3 ifTrue:
-				[aStream nextPutAll: (MonthNames at: monthIndex)]].
+			[
+				aStream nextPutAll: (MonthNames at: gregorian middle)
+			].
+		].
 		element = 3 ifTrue: 
-			[(formatArray at: 6) = 1
-				ifTrue: [year printOn: aStream]
-				ifFalse: [twoDigits & ((year \\ 100) < 10)
-							ifTrue: [aStream nextPutAll: '0'].
-						(year \\ 100) printOn: aStream]].
-		elementIndex < 3 ifTrue: 
-			[(formatArray at: 4) ~= 0 
-				ifTrue: [aStream nextPut: (formatArray at: 4) asCharacter]]]
+		[
+			(formatArray at: 6) = 1
+				ifTrue: [ gregorian last printOn: aStream ]
+				ifFalse: [ aStream nextPutAll: ((gregorian last \\ 100) asString padded: #left to: 2 with: $0) ].
+		].
+		i < 3 ifTrue: 
+		[
+			(formatArray at: 4) ~= 0 
+				ifTrue: [ aStream nextPut: (formatArray at: 4) asCharacter ] 
+		].
+	].
