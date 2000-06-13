5b31f2d979f5350b9fbcc839e00ee02a6ba16675
RAA <unknown>
2000-06-13 10:55:00 +0000

    no message

--- PluggableTextMorph/instance/menu commands/printIt.cs
+++ PluggableTextMorph/instance/menu commands/printIt.cs
@@ -1,7 +1,15 @@
 printIt
-	| result |
-	self handleEdit:
-		[result _ textMorph editor evaluateSelection.
-		((result isKindOf: FakeClassPool) or: [result == #failedDoit])
-			ifTrue: [self flash]
-			ifFalse: [textMorph editor afterSelectionInsertAndSelect: result printString]]
+	| result oldEditor |
+
+	textMorph editor selectFrom: selectionInterval first to: selectionInterval last;
+						model: model.  "For, eg, evaluateSelection"
+	textMorph handleEdit: [result _ (oldEditor _ textMorph editor) evaluateSelection].
+	((result isKindOf: FakeClassPool) or: [result == #failedDoit]) ifTrue: [^self flash].
+	selectionInterval _ oldEditor selectionInterval.
+	textMorph installEditorToReplace: oldEditor.
+	textMorph handleEdit: [oldEditor afterSelectionInsertAndSelect: result printString].
+	selectionInterval _ oldEditor selectionInterval.
+	
+	textMorph editor selectFrom: selectionInterval first to: selectionInterval last.
+	self scrollSelectionIntoView.
+
