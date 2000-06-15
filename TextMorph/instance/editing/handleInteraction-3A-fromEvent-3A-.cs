754dfe7d7a8998c81b516550f9a1fc4b02eda044
RAA <unknown>
2000-06-15 13:43:00 +0000

    no message

--- TextMorph/instance/editing/handleInteraction-3A-fromEvent-3A-.cs
+++ TextMorph/instance/editing/handleInteraction-3A-fromEvent-3A-.cs
@@ -1,8 +1,16 @@
-handleInteraction: interActionBlock fromEvent: evt
-	self installEditor.  "Make sure editor is installed"
-	editor sensor: (KeyboardBuffer new startingEvent: evt).  "Fool MVC"
-	self selectionChanged.  "Note old selection"
+handleInteraction: interactionBlock fromEvent: evt
+        | oldEditor oldParagraph |
+        "Perform the changes in interactionBlock, noting any change in selection"
 
-		interActionBlock value.
+        "Also couple ParagraphEditor to Morphic keyboard events"
+        self editor sensor: (KeyboardBuffer new startingEvent: evt).
+        oldEditor _ editor.
+        oldParagraph _ paragraph.
+        self selectionChanged.  "Note old selection"
 
-	self selectionChanged.  "Note new selection"
+                interactionBlock value.
+
+        oldParagraph == paragraph ifTrue: [     "this will not work if the paragraph changed"
+                editor _ oldEditor.     "since it may have been changed while in block"
+        ].
+        self selectionChanged.  "Note new selection"
