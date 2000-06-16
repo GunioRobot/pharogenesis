8fe2d358a9f30d5cf432c9e2169162a6153d8371
dvf <unknown>
2000-06-16 17:40:00 +0000

    no message

--- TextMorph/instance/accessing/newContents-3A-.cs
+++ TextMorph/instance/accessing/newContents-3A-.cs
@@ -1,8 +1,26 @@
-newContents: stringOrText
+newContents: stringOrText 
 	"Accept new text contents."
-	| newText |
+	| newText embeddedMorphs |
 	newText _ stringOrText asText.
-	text = newText ifTrue: [^ self].  "No substantive change"
+	text = newText ifTrue: [^ self].
+	"No substantive change"
+
+	text ifNotNil: [(embeddedMorphs _ text embeddedMorphs)
+			ifNotNil: 
+				[self removeAllMorphsIn: embeddedMorphs.
+				embeddedMorphs do: [:m | m delete]]].
+
 	text _ newText.
-	self releaseParagraph.  "update the paragraph cache"
-	self paragraph.  "re-instantiate to set bounds"
+
+	"add all morphs off the visible region; they'll be moved into the right 
+	place when they become visible. (this can make the scrollable area too 
+	large, though)"
+	stringOrText asText embeddedMorphs do: 
+		[:m | 
+		self addMorph: m.
+		m position: -1000 @ 0].
+	self releaseParagraph.
+	"update the paragraph cache"
+	self paragraph.
+	"re-instantiate to set bounds"
+	self world ifNotNil: [self world startSteppingSubmorphsOf: self]
