e3637dbe516b4b9a9f7143f1e5eaf1ad2d60be67
hmm <unknown>
2000-06-18 19:16:00 +0000

    no message

--- DisplayScreen/instance/other/deferUpdates-3A-.cs
+++ DisplayScreen/instance/other/deferUpdates-3A-.cs
@@ -1,5 +1,7 @@
 deferUpdates: aBoolean
-	"Set the deferUpdates flag in the virtual machine. When this flag is true, BitBlt operations on the Display are not automatically propagated to the screen. If this underlying platform does not support deferred updates, this primitive will fail. Answer the receiver if the primitive succeeds, nil if it fails."
+	| wasDeferred |
+	"Set the deferUpdates flag in the virtual machine. When this flag is true, BitBlt operations on the Display are not automatically propagated to the screen. If this underlying platform does not support deferred updates, this primitive will fail. Answer whether updates were deferred before if the primitive succeeds, nil if it fails."
 
-	<primitive: 126>
-	^ nil  "answer nil if primitive fails"
+	wasDeferred _ DeferringUpdates == true.
+	DeferringUpdates _ aBoolean.
+	^(self primitiveDeferUpdates: aBoolean) ifNotNil: [wasDeferred]
