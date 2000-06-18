ba2d24790846c2b4ca173b54457ad97949532f2c
hmm <unknown>
2000-06-18 19:24:00 +0000

    no message

--- StringHolderView/instance/displaying/displayView.cs
+++ StringHolderView/instance/displaying/displayView.cs
@@ -1,7 +1,8 @@
 displayView 
 	"Refer to the comment in View|displayView."
 
-	self clearInside.
-	(self controller isKindOf: ParagraphEditor)
-		ifTrue: [controller display]
-		ifFalse: [displayContents display]
+	Display deferUpdatesIn: self displayBox while: [
+		self clearInside.
+		(self controller isKindOf: ParagraphEditor)
+			ifTrue: [controller display]
+			ifFalse: [displayContents display]]
