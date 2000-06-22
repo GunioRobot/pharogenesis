4f65afd4190a105224d2c1d049e182e26ff05b12
ls <unknown>
2000-06-22 14:35:00 +0000

    no message

--- CompiledMethod/instance/accessing/primitive.cs
+++ CompiledMethod/instance/accessing/primitive.cs
@@ -1,11 +1,10 @@
 primitive
 	"Answer the primitive index associated with the receiver.
 	Zero indicates that this is not a primitive method.
-	We currently allow 11 bits of primitive index, but they are in two places
+	We currently allow 10 bits of primitive index, but they are in two places
 	for  backward compatibility.  The time to unpack is negligible,
 	since the reconstituted full index is stored in the method cache."
 	| primBits |
-	primBits _ self header bitAnd: 16r300001FF.
-	primBits > 16r1FF
-		ifTrue: [^ (primBits bitAnd: 16r1FF) + (primBits bitShift: -19)]
-		ifFalse: [^ primBits]
+	primBits _ self header bitAnd: 16r100001FF.
+	
+	^ (primBits bitAnd: 16r1FF) + (primBits bitShift: -19)
