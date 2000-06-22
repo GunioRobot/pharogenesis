c20d0f6206fd82c2ae6292f5f34198404f8c6487
ls <unknown>
2000-06-22 14:35:00 +0000

    no message

--- Interpreter/instance/compiled methods/primitiveIndexOf-3A-.cs
+++ Interpreter/instance/compiled methods/primitiveIndexOf-3A-.cs
@@ -1,9 +1,8 @@
 primitiveIndexOf: methodPointer
-	"Note: We now have 11 bits of primitive index, but they are in two places
+	"Note: We now have 10 bits of primitive index, but they are in two places
 	for temporary backward compatibility.  The time to unpack is negligible,
 	since the reconstituted full index is stored in the method cache."
 	| primBits |
-	primBits _ ((self headerOf: methodPointer) >> 1) bitAnd: 16r300001FF.
-	primBits > 16r1FF
-		ifTrue: [^ (primBits bitAnd: 16r1FF) + (primBits >> 19)]
-		ifFalse: [^ primBits]
+	primBits _ ((self headerOf: methodPointer) >> 1) bitAnd: 16r100001FF.
+	
+	^ (primBits bitAnd: 16r1FF) + (primBits >> 19)
