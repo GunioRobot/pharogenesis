a140b1a496a47667d08e475df4356e9ac2d63e84
SqR <unknown>
2000-07-02 16:52:00 +0000

    no message

---
+++ XMLParser/instance/callbacks/text-3A-.cs
@@ -0,0 +1,5 @@
+text: aString
+	"This method is called for the blocks of text between tags.
+	It preserves whitespace, but has all of the enclosed entities expanded"
+
+	^self subclassResponsibility
