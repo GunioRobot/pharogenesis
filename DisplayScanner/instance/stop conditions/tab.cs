a6fa74608ad0d157e09d663ad9ec68404b0e2867
hmm <unknown>
2000-07-16 08:23:00 +0000

    no message

--- DisplayScanner/instance/stop conditions/tab.cs
+++ DisplayScanner/instance/stop conditions/tab.cs
@@ -1,10 +1,4 @@
 tab
-	destX _ (textStyle alignment == Justified and: [self leadingTab not])
-		ifTrue:		"imbedded tabs in justified text are weird"
-			[destX + (textStyle tabWidth - (line justifiedTabDeltaFor: spaceCount)) max: destX]
-		ifFalse: 
-			[textStyle nextTabXFrom: destX
-				leftMargin: leftMargin
-				rightMargin: rightMargin].
+	self plainTab.
 	lastIndex _ lastIndex + 1.
 	^ false
