82e58b703b054508a496adf67e90e2303e9fb7db
TPR <unknown>
2000-08-03 19:21:00 +0000

    no message

--- SystemDictionary/instance/shrinking/discardSoundSynthesis.cs
+++ SystemDictionary/instance/shrinking/discardSoundSynthesis.cs
@@ -2,20 +2,14 @@ discardSoundSynthesis
 	"Discard the sound synthesis facilities, and the methods and classes that use it. This also discards MIDI."
 
 	Smalltalk discardMIDI.
-	Smalltalk removeClassNamed: #EnvelopeLineMorph.
-	Smalltalk removeClassNamed: #EnvelopeEditorMorph.
-	Smalltalk removeClassNamed: #SoundSequencerMorph.
-	Smalltalk removeClassNamed: #SoundMorph.
-	Smalltalk removeClassNamed: #SoundLoopMorph.
-	Smalltalk removeClassNamed: #InterimSoundMorph.
-	Smalltalk removeClassNamed: #RecordingControlsMorph.
-	Smalltalk removeClassNamed: #SoundDemoMorph.
+	Smalltalk discardSpeech.
+	SystemOrganization removeCategoriesMatching: 'Sound-Interface'.
 	Smalltalk at: #GraphMorph ifPresent: [:graphMorph |
-		#(loadCoffeeCupClink play playBach playOnce
-		  readDataFromFile registerWaveform stopPlaying)
+		#(playOnce readDataFromFile)
 			do: [:sel | graphMorph removeSelector: sel]].
 	Smalltalk at: #TrashCanMorph ifPresent: [:trashMorph |
 		trashMorph class removeSelector: #samplesForDelete.
 		trashMorph class removeSelector: #samplesForMouseEnter.
 		trashMorph class removeSelector: #samplesForMouseLeave].
-	SystemOrganization removeCategoriesMatching: 'System-Sound'.
+	SystemOrganization removeCategoriesMatching: 'Sound-Synthesis'.
+	SystemOrganization removeCategoriesMatching: 'Sound-Scores'.
