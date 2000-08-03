94c1b8aefe63d410879c4585919d5e94d7dda7da
TPR <unknown>
2000-08-03 19:48:00 +0000

    no message

--- SystemDictionary/instance/shrinking/discardNetworking.cs
+++ SystemDictionary/instance/shrinking/discardNetworking.cs
@@ -2,4 +2,5 @@ discardNetworking
 	"Discard the support for TCP/IP networking."
 
 	Smalltalk discardPluggableWebServer.
-	SystemOrganization removeCategoriesMatching: 'System-Network'.
+	SystemOrganization removeCategoriesMatching: 'Network-*'.
+
