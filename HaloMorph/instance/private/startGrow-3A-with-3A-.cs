c42672b41dee70eb3ff293f68857e28da075ecc4
di <unknown>
2000-08-07 23:48:00 +0000

    no message

--- HaloMorph/instance/private/startGrow-3A-with-3A-.cs
+++ HaloMorph/instance/private/startGrow-3A-with-3A-.cs
@@ -1,9 +1,13 @@
 startGrow: evt with: growHandle
+	| botRt |
 	"Initialize resizing of my target."
 
 	growingOrRotating _ true.
 	self removeAllHandlesBut: growHandle.  "remove all other handles"
-	positionOffset _ growHandle center - target bottomRight.
+	botRt _ target pointInWorld: target bottomRight.
+	(self world viewBox containsPoint: botRt)
+		ifTrue: [positionOffset _ evt cursorPoint - botRt]
+		ifFalse: [positionOffset _ 0@0].
 	target isAlignmentMorph
 		ifTrue: [minExtent _ target minWidth@target minHeight]
 		ifFalse: [minExtent _ 1@1].
