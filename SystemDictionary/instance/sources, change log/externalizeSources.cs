e13cdd270d30dc60a332ca259d8433c8f7182873
JMM <unknown>
2000-08-10 15:15:00 +0000

    no message

--- SystemDictionary/instance/sources, change log/externalizeSources.cs
+++ SystemDictionary/instance/sources, change log/externalizeSources.cs
@@ -17,11 +17,21 @@ file named ', changesName].
 	aFile _  FileStream newFileNamed: sourcesName.
 	aFile nextPutAll: SourceFiles first originalContents.
 	aFile close.
+	"On Mac, set the file type and creator (noop on other platforms)"
+	FileDirectory default
+		setMacFileNamed: sourcesName
+		type: 'STch'
+		creator: 'FAST'.
 	SourceFiles at: 1 put: (FileStream readOnlyFileNamed: sourcesName).
 
 	aFile _ FileStream newFileNamed: self changesName.
 	aFile nextPutAll: SourceFiles last contents.
 	aFile close.
+	"On Mac, set the file type and creator (noop on other platforms)"
+	FileDirectory default
+		setMacFileNamed: self changesName
+		type: 'STch'
+		creator: 'FAST'.
 	SourceFiles at: 2 put: (FileStream oldFileNamed: changesName).
 
 	self inform: 'Sources successfully externalized'.
