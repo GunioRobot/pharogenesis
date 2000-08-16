cf3b2568649a7ca2ab4eef17a1dbac4895b3e761
di <unknown>
2000-08-16 14:22:00 +0000

    no message

--- SystemDictionary/instance/shrinking/majorShrink.cs
+++ SystemDictionary/instance/shrinking/majorShrink.cs
@@ -1,7 +1,32 @@
 majorShrink    "Smalltalk majorShrink; abandonSources; lastRemoval"
-	"This method throws out lots of the system that is not needed for, eg, operation in a hand-held PC.  The shrink process is being improved and, in conjunction with removeAllUnSentMessages, yields an image around 600k in size."
+	"This method throws out lots of the system that is not needed for, eg, operation in a hand-held PC.  majorShrink produces a 999k image in Squeak 2.8"
 
-	self minorShrink.
+	Smalltalk isMorphic ifTrue: [^ self error: 'You can only run majorShrink in MVC'].
+	Project current isTopProject ifFalse: [^ self error: 'You can only run majorShrink in the top project'].
+	(Smalltalk confirm: 'All sub-projects will be deleted from this image.
+You should already have made a backup copy,
+or you must save with a different name after shrinking.
+Shall we proceed to discard most of the content in this image?')
+		ifFalse: [^ PopUpMenu notify: 'No changes have been made.'].
+
+	"Remove all projects but the current one.  - saves 522k"
+	ProjectView allInstancesDo: [:pv | pv controller closeAndUnscheduleNoTerminate].
+	Project current setParent: Project current.
+	MorphWorldView allInstancesDo: [:pv | pv topView controller closeAndUnscheduleNoTerminate].
+	Wonderland removeActorPrototypesFromSystem.
+	Player freeUnreferencedSubclasses.
+	MorphicModel removeUninstantiatedModels.
+	Utilities classPool at: #ScrapsBook put: nil.
+
+	Smalltalk discardVMConstruction.  "755k"
+	Smalltalk discardSoundSynthesis.  "544k"
+	Smalltalk discardOddsAndEnds.  "227k"
+	Smalltalk discardNetworking.  "234k"
+	Smalltalk discard3D.  "407k"
+	Smalltalk discardFFI.  "33k"
+	Smalltalk discardMorphic.  "1372k"
+	Symbol rehash.  "40k"
+	"Above by itself saves about 4,238k"
 
 	"Remove references to a few classes to be deleted, so that they won't leave obsolete versions around."
 	FileList removeSelector: #fileIntoNewChangeSet.
@@ -10,21 +35,17 @@ majorShrink    "Smalltalk majorShrink; abandonSources; lastRemoval"
 	ScreenController removeSelector: #openChangeManager.
 	ScreenController removeSelector: #exitProject.
 	ScreenController removeSelector: #openProject.
-	ScreenController removeSelector: #viewGIFImports.
+	ScreenController removeSelector: #viewImageImports.
 
-	"Now delete lots of classes.."
-	(SystemOrganization categories select: [:c | 'Morphic*' match: c]) reverseDo:
-		[:c | SystemOrganization removeSystemCategory: c].
-	SystemOrganization removeSystemCategory: 'System-Network'.
-	SystemOrganization removeSystemCategory: 'System-Monitoring'.
-	SystemOrganization removeSystemCategory: 'Graphics-Symbols'.
+	"Now delete various other classes.."
 	SystemOrganization removeSystemCategory: 'Graphics-Files'.
-	SystemOrganization removeSystemCategory: 'Interface-Projects'.
-	SystemOrganization removeSystemCategory: 'Object Storage'.
-	SystemOrganization removeSystemCategory: 'System-Sound'.
+	SystemOrganization removeSystemCategory: 'System-Object Storage'.
+	Smalltalk removeClassNamed: #ProjectController.
+	Smalltalk removeClassNamed: #ProjectView.
+	"Smalltalk removeClassNamed: #Project."
+	Smalltalk removeClassNamed: #Environment.
+	Smalltalk removeClassNamed: #Component1.
 
-	Smalltalk removeClassNamed: #Circle.
-	Smalltalk removeClassNamed: #Arc.
 	Smalltalk removeClassNamed: #FormSetFont.
 	Smalltalk removeClassNamed: #FontSet.
 	Smalltalk removeClassNamed: #InstructionPrinter.
@@ -32,15 +53,23 @@ majorShrink    "Smalltalk majorShrink; abandonSources; lastRemoval"
 	Smalltalk removeClassNamed: #DualChangeSorter.
 	Smalltalk removeClassNamed: #EmphasizedMenu.
 	Smalltalk removeClassNamed: #MessageTally.
-	Smalltalk removeClassNamed: #BitEditor.
 
 	StringHolder class removeSelector: #originalWorkspaceContents.
 	CompiledMethod removeSelector: #symbolic.
-	TextConstants removeKey: #ClairVaux ifAbsent: [].  "deletes a couple of fonts"
 
-	TextStyle allInstancesDo:
-		[:ts | (ts instVarAt: 1) size > 2 ifTrue:  "Only need two fonts"
-			[ts instVarAt: 1 put: ((ts instVarAt: 1) copyFrom: 1 to: 2)]].
+	RemoteString removeSelector: #makeNewTextAttVersion.
+	Utilities class removeSelector: #absorbUpdatesFromServer.
+	Smalltalk removeClassNamed: #PenPointRecorder.
+	Smalltalk removeClassNamed: #Path.
+	Smalltalk removeClassNamed: #Base64MimeConverter.
+	"Smalltalk removeClassNamed: #EToySystem. Dont bother - its very small and used for timestamps etc"
+	Smalltalk removeClassNamed: #RWBinaryOrTextStream.
+	Smalltalk removeClassNamed: #AttributedTextStream.
+	Smalltalk removeClassNamed: #WordNet.
+	Smalltalk removeClassNamed: #SelectorBrowser.
+
+	TextStyle allSubInstancesDo:
+		[:ts | ts newFontArray: (ts fontArray copyFrom: 1 to: (2 min: ts fontArray size))].
 	ListParagraph initialize.
 	PopUpMenu initialize.
 	StandardSystemView initialize.
@@ -48,7 +77,16 @@ majorShrink    "Smalltalk majorShrink; abandonSources; lastRemoval"
 	Smalltalk noChanges.
 	ChangeSorter classPool at: #AllChangeSets 
 		put: (OrderedCollection with: Smalltalk changes).
+	(Smalltalk includesKey: #Morph) "only remove if Morphic has been removed"
+		ifTrue:[Smalltalk removeClassNamed: #CornerRounder.
+			ScriptingSystem _ nil].
+	SystemDictionary removeSelector: #majorShrink.
 
-	[self removeAllUnSentMessages > 0] whileTrue.
+	[Smalltalk removeAllUnSentMessages > 0]
+		whileTrue:
+		[Smalltalk unusedClasses do: [:c | (Smalltalk at: c) removeFromSystem]].
+	SystemOrganization removeEmptyCategories.
 	Smalltalk allClassesDo: [:c | c zapOrganization].
+	MethodDictionary allInstances do: [:d | d rehash].
+	Smalltalk changes initialize.
 	Symbol rehash.
