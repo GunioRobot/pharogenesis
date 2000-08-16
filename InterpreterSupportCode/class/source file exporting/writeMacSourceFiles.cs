f40aa0a1abbb34a4bbf619ac1c90dc0490da8d0e
JMM <unknown>
2000-08-16 13:43:00 +0000

    no message

--- InterpreterSupportCode/class/source file exporting/writeMacSourceFiles.cs
+++ InterpreterSupportCode/class/source file exporting/writeMacSourceFiles.cs
@@ -1,15 +1,15 @@
 writeMacSourceFiles
-	"Store into this image's folder the C sources files required to support the interpreter on the Macintosh. It also generates the code for the sound synthesis primitives. However, because generating code for the interpreter itself takes several minutes, that is not done automatically by this method. To generate that code, use the method 'translate:doInlining:' in Interpreter class."
+	"Store into this image's folder the C sources files required to support the interpreter on the Macintosh. It also generates the code for the sound synthesis primitives. However, because generating code for the interpreter itself takes several minutes, that is not done automatically by this method. To generate the interpreter code, use the method 'translate:doInlining:' in Interpreter class."
 	"InterpreterSupportCode writeMacSourceFiles"
 
-	self storeString: self readmeFile			onFileNamed: 'readme'.
-	self storeString: self squeakHeaderFile	onFileNamed: 'sq.h'.
-	self storeString: self squeakConfigFile	onFileNamed: 'sqConfig.h'.
-	self storeString: self squeakMachDepFile	onFileNamed: 'sqMachDep.h'.
-	self storeString: self squeakPlatSpecFile	onFileNamed: 'sqPlatformSpecific.h'.
-	self storeString: self squeakFilePrimsFile	onFileNamed: 'sqFilePrims.c'.
+	self writeSupportFiles.
+	FFIPlugin writeSupportFiles.
+
+	self storeString: self macAsyncFilePrimsFile	onFileNamed: 'sqMacAsyncFilePrims.c'.
+	self storeString: self macBrowserPluginFile	onFileNamed: 'sqMacNSPlugin.c'.
 	self storeString: self macDirectoryFile	onFileNamed: 'sqMacDirectory.c'.
-	self storeString: self macJoystickFile		onFileNamed: 'sqMacJoystick.c'.
+	self storeString: self macJoystickAndTabletFile  onFileNamed: 'sqMacJoystickAndTablet.c'.
+	self storeString: self macMinimal		onFileNamed: 'sqMacMinimal.c'.
 	self storeString: self macNetworkFile		onFileNamed: 'sqMacNetwork.c'.
 	self storeString: self macSerialAndMIDIPortFile	onFileNamed: 'sqMacSerialAndMIDIPort.c'.
 	self storeString: self macSoundFile		onFileNamed: 'sqMacSound.c'.
@@ -17,10 +17,6 @@ writeMacSourceFiles
 	self storeString: self macTCPFile			onFileNamed: 'MacTCP.h'.
 	self storeString: self macAddressXlationFile		onFileNamed: 'AddressXlation.h'.
 	self storeString: self macDNRFile					onFileNamed: 'dnr.c'.
-	Smalltalk at: #AbstractSound ifPresent: [:abstractSound |
-		self storeString: abstractSound cCodeForSoundPrimitives
-													onFileNamed: 'sqSoundPrims.c'].
-	self storeString: self cCodeForMiscPrimitives
-													onFileNamed: 'sqMiscPrims.c'.
-	self storeString: self squeakOldSoundPrimsFile	onFileNamed: 'sqOldSoundPrims.c'.
-	self storeProjectArchiveOnFileNamed: 'projectArchive.sit'.
+
+	self storeStuffitArchive: self	 macArchiveBinaryFile
+		onFileNamed: 'projectArchive.sit'.
