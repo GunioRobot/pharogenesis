101a3cfeea12b492bfd84199efc67dd70b399634
pnm <unknown>
2000-08-16 15:23:00 +0000

    no message

--- Exception/instance/exceptionBuilder/tag-3A-.cs
+++ Exception/instance/exceptionBuilder/tag-3A-.cs
@@ -1,4 +1,4 @@
 tag: t
-	"TFEI - This message is not specified in the ANSI protocol, but that looks like an oversight because #tag is specified, and states that the signaler may store the tag value."
+	"This message is not specified in the ANSI protocol, but that looks like an oversight because #tag is specified, and the spec states that the signaler may store the tag value."
 
 	tag := t
