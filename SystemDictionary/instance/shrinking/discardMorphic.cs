2b05bd7ef57333cd8ef216729158d47c984fc3c8
di <unknown>
2000-08-16 13:17:00 +0000

    no message

--- SystemDictionary/instance/shrinking/discardMorphic.cs
+++ SystemDictionary/instance/shrinking/discardMorphic.cs
@@ -1,8 +1,22 @@
 discardMorphic
-	"Discard Morphic."
+   "Smalltalk discardMorphic"
+	"Discard Morphic.
+Updated for 2.8 TPR"
 
-	self halt: 'not yet complete; leaves many obsolete classes'.
-	SystemOrganization removeCategoriesMatching: 'User Objects'.
-	SystemOrganization removeCategoriesMatching: '*EToy*'.
-	SystemOrganization removeCategoriesMatching: 'Morphic*'.
+	| subs |
+	"Check that we are in an MVC Project and that there are no Morphic Projects
+		or WorldMorphViews."
+	Utilities clobberFlapTabList.
+	Smalltalk discardFlash.
+	Smalltalk discardTrueType.
+	subs _ OrderedCollection new.
+	Morph allSubclassesWithLevelDo: [:c :i | subs addFirst: c]
+		startingLevel: 0.
+	subs do: [:c | c removeFromSystem].
+	Smalltalk removeClassNamed: #CornerRounder.
+	Smalltalk removeKey: #BalloonEngineConstants ifAbsent: [].
+	SystemOrganization removeCategoriesMatching: 'Balloon-*'.
+	SystemOrganization removeCategoriesMatching: 'Morphic-*'.
+	SystemOrganization removeSystemCategory: 'Graphics-Transformations'.
+	SystemOrganization removeSystemCategory: 'ST80-Morphic'.
 
