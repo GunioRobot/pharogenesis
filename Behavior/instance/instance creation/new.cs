ba32010f90fbf6237a7d785bece90b33f0347336
di <unknown>
2000-08-18 20:27:00 +0000

    no message

--- Behavior/instance/instance creation/new.cs
+++ Behavior/instance/instance creation/new.cs
@@ -1,9 +1,5 @@
 new
 	"Answer a new instance of the receiver (which is a class) with no indexable variables. Fail if the class is indexable."
-	"Essential Primitive. See Object documentation whatIsAPrimitive."
 
-	<primitive: 70>
-	self isVariable ifTrue: [^ self basicNew: 0].
-	"space must be low"
-	Smalltalk signalLowSpace.
-	^ self basicNew  "retry if user proceeds"
+	<primitive: 70>  "This method runs primitively if successful"
+	^ self basicNew  "Exceptional conditions will be handled in basicNew"
