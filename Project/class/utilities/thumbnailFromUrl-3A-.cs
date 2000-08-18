27896e24da53fd4945f603ab430a0fdd8b074923
di <unknown>
2000-08-18 12:57:00 +0000

    no message

--- Project/class/utilities/thumbnailFromUrl-3A-.cs
+++ Project/class/utilities/thumbnailFromUrl-3A-.cs
@@ -4,8 +4,5 @@ thumbnailFromUrl: urlString
 Project thumbnailFromUrl: 'http://www.squeak.org/Squeak2.0/2.7segments/SqueakEasy.extSeg'.
 "
 
-	Smalltalk verifyMorphicAvailability ifFalse: [^ self].
-	Smalltalk isMorphic ifFalse: [^ self inform: 
-			'Later, allow jumping from MVC to Morphic Projects.'].
 	preStream _ (ServerFile new fullPath: urlString) asStream.
 	ProjectViewMorph openFromFile: preStream
