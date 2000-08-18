7a280052bc1a442972b66e99b24d56335b6a7214
di <unknown>
2000-08-18 12:59:00 +0000

    no message

---
+++ PasteUpMorph/instance/object fileIn/convertbosfcebbfgccpmcpbttloiairfidcuww0-3A-bosfcebbpmcpbttliairfidcuww0-3A-.cs
@@ -0,0 +1,5 @@
+convertbosfcebbfgccpmcpbttloiairfidcuww0: varDict bosfcebbpmcpbttliairfidcuww0: smartRefStrm
+	"These variables are automatically stored into the new instance #('bounds' 'owner' 'submorphs' 'fullBounds' 'color' 'extension' 'borderWidth' 'borderColor' 'presenter' 'model' 'cursor' 'padding' 'backgroundMorph' 'turtleTrailsForm' 'turtlePen' 'lastTurtlePositions' 'isPartsBin' 'autoLineLayout' 'indicateCursor' 'resizeToFit' 'fileName' 'isStackLike' 'dataInstances' 'currentDataInstance' 'userFrameRectangle' 'wantsMouseOverHalos' 'worldState').
+	This method is for additional changes. Use statements like (foo _ varDict at: 'foo')."
+
+		"These are going away #('fillColor2' 'gradientDirection' 'colorArray' 'colorDepth' 'openToDragNDrop').  Possibly store their info in another variable?"
