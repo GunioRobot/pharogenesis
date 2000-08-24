d168614b246ed99da3bed7d82ee808ff7aa6c2c8
ka <unknown>
2000-08-24 18:56:00 +0000

    no message

---
+++ FileList/instance/private/readContentsEUCKR.cs
@@ -0,0 +1,10 @@
+readContentsEUCKR
+	| f writeStream |
+	f _ directory oldFileOrNoneNamed: self fullName.
+	f ifNil: [^ 'For some reason, this file cannot be read'].
+	writeStream _ WriteStream on: String new.
+	f converter: EUCKRTextConverter new.
+	[f atEnd]
+		whileFalse: [writeStream nextPut: f next].
+	f close.
+	^ writeStream contents
