96b681b82a1e071c24b321922bb48b1242f122fd
ar <unknown>
2000-08-28 17:27:00 +0000

    no message

---
+++ HostFont/class/accessing/textStyleFromUser.cs
@@ -0,0 +1,23 @@
+textStyleFromUser
+	"HostFont textStyleFromUser"
+	| styleName fonts |
+	styleName _ self fontNameFromUser ifNil:[^self].
+	styleName _ styleName asSymbol.
+	(TextConstants includesKey: styleName)
+		ifTrue:[(self confirm: 
+styleName , ' is already defined in TextConstants.
+Do you want to replace that definition?')
+			ifFalse: [^ self]].
+	fonts _ #(10 12 14 16 18 20 22 24 26 28 30 36 48 60 72 90).
+	('Rendering ', styleName) displayProgressAt: Sensor cursorPoint
+		from: 1 to: fonts size during:[:bar|
+			fonts _ fonts
+				collect:[:ptSize| bar value: (fonts indexOf: ptSize).
+							   self fontName: styleName 
+									size: ptSize
+									emphasis: 0]
+				thenSelect:[:font| font notNil]]. "reject those that failed"
+	fonts size = 0 ifTrue:[^self error:'Could not create font style', styleName].
+	TextConstants
+		at: styleName
+		put: (TextStyle fontArray: fonts).
