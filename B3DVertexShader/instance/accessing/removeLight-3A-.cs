71b0869f8676938f10ce5be82728386e2be9baa6
ar <unknown>
2000-09-06 23:37:00 +0000

    no message

--- B3DVertexShader/instance/accessing/removeLight-3A-.cs
+++ B3DVertexShader/instance/accessing/removeLight-3A-.cs
@@ -2,3 +2,4 @@ removeLight: lightIndex
 	"Remove the light with the given index"
 	(lightIndex < 1 or:[lightIndex > lights size]) ifTrue:[^nil].
 	lights at: lightIndex put: nil. "So we don't change the indexes"
+	needsLightUpdate _ true.
