23527d214a366d934586379f513f140a6fdfd4f1
ar <unknown>
2000-09-06 23:34:00 +0000

    no message

--- B3DVertexTransformer/instance/accessing/popMatrix.cs
+++ B3DVertexTransformer/instance/accessing/popMatrix.cs
@@ -2,3 +2,4 @@ popMatrix
 	"Pop the current matrix from the stack"
 	matrixStack isEmpty ifTrue:[^self error:'Empty matrix stack'].
 	currentMatrix loadFrom: matrixStack removeLast.
+	needsUpdate _ true.
