3a6f8188b4e03a9b3e3dc173659f097427d66ef4
ar <unknown>
2000-09-08 23:04:00 +0000

    no message

--- B3DRenderEngine/instance/initialize/destroy.cs
+++ B3DRenderEngine/instance/initialize/destroy.cs
@@ -1,6 +1,7 @@
 destroy
 	"Utility - destroy all resources associated with any part of the engine"
-	transformer destroy.
-	shader destroy.
-	clipper destroy.
-	rasterizer destroy.
+	transformer ifNotNil:[transformer destroy].
+	shader ifNotNil:[shader destroy].
+	clipper ifNotNil:[clipper destroy].
+	rasterizer ifNotNil:[rasterizer destroy].
+	transformer _ shader _ clipper _ rasterizer _ nil.
