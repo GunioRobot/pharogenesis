b17506f66060de9594185989fe5c09275566fe2e
ar <unknown>
2000-09-09 15:50:00 +0000

    no message

---
+++ B3DAcceleratorPlugin/instance/primitives-2D-textures/primitiveTextureByteSex.cs
@@ -0,0 +1,12 @@
+primitiveTextureByteSex
+	| handle result renderer |
+	self export: true.
+	interpreterProxy methodArgumentCount = 2
+		ifFalse:[^interpreterProxy primitiveFail].
+	handle _ interpreterProxy stackIntegerValue: 0.
+	renderer _ interpreterProxy stackIntegerValue: 1.
+	interpreterProxy failed ifTrue:[^nil].
+	result _ self cCode:'b3dxTextureByteSex(renderer, handle)' inSmalltalk:[-1].
+	result < 0 ifTrue:[^interpreterProxy primitiveFail].
+	interpreterProxy pop: 3.
+	^interpreterProxy pushBool: result.
