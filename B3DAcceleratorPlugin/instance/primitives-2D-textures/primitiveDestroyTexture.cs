a70d67379f24067facb98197d5f835804bcba61c
ar <unknown>
2000-09-09 15:50:00 +0000

    no message

--- B3DAcceleratorPlugin/instance/primitives-2D-textures/primitiveDestroyTexture.cs
+++ B3DAcceleratorPlugin/instance/primitives-2D-textures/primitiveDestroyTexture.cs
@@ -1,10 +1,11 @@
 primitiveDestroyTexture
-	| handle result |
+	| handle result renderer |
 	self export: true.
-	interpreterProxy methodArgumentCount = 1
+	interpreterProxy methodArgumentCount = 2
 		ifFalse:[^interpreterProxy primitiveFail].
 	handle _ interpreterProxy stackIntegerValue: 0.
+	renderer _ interpreterProxy stackIntegerValue: 1.
 	interpreterProxy failed ifTrue:[^nil].
-	result _ self cCode:'b3dxDestroyTexture(handle)' inSmalltalk:[false].
+	result _ self cCode:'b3dxDestroyTexture(renderer, handle)' inSmalltalk:[false].
 	result ifFalse:[^interpreterProxy primitiveFail].
-	interpreterProxy pop: 1. "pop arg; return rcvr"
+	^interpreterProxy pop: 2. "pop arg; return rcvr"
