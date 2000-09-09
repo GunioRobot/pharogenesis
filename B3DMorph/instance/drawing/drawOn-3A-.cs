dca6c3f455cdb46a7cb512d105f7559269f5ea00
ar <unknown>
2000-09-09 22:06:00 +0000

    no message

--- B3DMorph/instance/drawing/drawOn-3A-.cs
+++ B3DMorph/instance/drawing/drawOn-3A-.cs
@@ -1,4 +1,4 @@
 drawOn: aCanvas
 	color ifNotNil:["aCanvas frameAndFillRectangle: self bounds fillColor: color borderWidth: 1 borderColor: Color black."
 		aCanvas frameRectangle: self bounds color: self color].
-	aCanvas asBalloonCanvas render: self.
+	aCanvas render: self.
