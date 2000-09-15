e1fc7459eee610f047acaca0468fcb24befb9782
sw <unknown>
2000-09-15 06:14:00 +0000

    no message

--- TextMorph/instance/scripting access/getAllButFirstCharacter.cs
+++ TextMorph/instance/scripting access/getAllButFirstCharacter.cs
@@ -2,4 +2,4 @@ getAllButFirstCharacter
 	"Obtain all but the first character from the receiver; if that would be empty, return a black dot"
 
 	| aString |
-	^ (aString _ text string) size > 1 ifTrue: [aString copyFrom: 2 to: aString size] ifFalse: ['¥']
+	^ (aString _ text string) size > 1 ifTrue: [aString copyFrom: 2 to: aString size] ifFalse: ['Â·']
