53f6bc597c6fbca0a0c233ad582de12ab7c57aba
JMM <unknown>
2000-09-19 13:35:00 +0000

    no message

---
+++ MPEGFile/instance/primitives/primAudioReReadBuffer-3A-buffer-3A-channel-3A-samples-3A-stream-3A-.cs
@@ -0,0 +1,3 @@
+primAudioReReadBuffer: aFileHandle  buffer: aBuffer channel: aChannel samples: aSampleNumber stream: aStreamNumber
+	<primitive: 'primitiveMPEG3ReReadAudio' module: 'Mpeg3Plugin'>
+	self primitiveFailed
