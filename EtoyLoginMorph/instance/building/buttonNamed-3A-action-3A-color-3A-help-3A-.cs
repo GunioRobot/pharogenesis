87e78d9746f60a34a766e2d5a961d2d807ed99b4
ar <unknown>
2000-09-23 13:48:00 +0000

    no message

---
+++ EtoyLoginMorph/instance/building/buttonNamed-3A-action-3A-color-3A-help-3A-.cs
@@ -0,0 +1,11 @@
+buttonNamed: aString action: aSymbol color: aColor help: helpString
+
+	| f col |
+	f _ SimpleButtonMorph new
+		target: self;
+		label: aString font: self myFont;
+		color: aColor;
+		actionSelector: aSymbol;
+		setBalloonText: helpString.
+	col _ (self inAColumn: {f}) hResizing: #spaceFill.
+	^col
