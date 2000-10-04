0e0d1d4b494445e021643403fc8aec1fe349f383
di <unknown>
2000-10-04 10:45:00 +0000

    no message

--- CipherPanel/class/as yet unclassified/tedsHack.cs
+++ CipherPanel/class/as yet unclassified/tedsHack.cs
@@ -1,10 +1,5 @@
 tedsHack  
 	"Generate cryptic puzzles from method comments in the system"
-	| c s | s _ 'none'.
-	[s = 'none'] whileTrue:
-		[s _ ((c _ Smalltalk allClasses atRandom) selectors
-					collect: [:sel | (c firstCommentAt: sel) asString])
-		 			detect: [:str | str size between: 100 and: 200] ifNone: ['none']].
-	(CipherPanel quoteToScramble: s) openInWorld
+	(self newFromQuote: (self encode: (self randomComment))) openInWorld
 
 "CipherPanel tedsHack"
