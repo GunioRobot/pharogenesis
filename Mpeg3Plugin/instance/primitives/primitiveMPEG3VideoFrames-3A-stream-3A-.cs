7997db7b6ad4293d59fcf91377ffbc0b6ce8321a
ls <unknown>
2000-11-04 14:03:00 +0000

    no message

---
+++ Mpeg3Plugin/instance/primitives/primitiveMPEG3VideoFrames-3A-stream-3A-.cs
@@ -0,0 +1,17 @@
+primitiveMPEG3VideoFrames: fileHandle stream: aNumber
+	| file result |
+
+	"long mpeg3_video_frames(mpeg3_t *file, int stream)"
+	self var: #file declareC: 'mpeg3_t * file'.
+	self primitive: 'primitiveMPEG3VideoFrames'
+		parameters: #(Oop SmallInteger).
+
+	file _ self mpeg3tValueOf: fileHandle.
+	file = nil ifTrue: [^0].
+	aNumber < 0 ifTrue: [interpreterProxy success: false. ^nil].
+	aNumber >= (self cCode: 'result = mpeg3_total_vstreams(file)') ifTrue: [
+		interpreterProxy success: false.  ^0 ].
+
+
+	self cCode: 'result = mpeg3_video_frames(file,aNumber)'.
+	^result asOop: Float.
