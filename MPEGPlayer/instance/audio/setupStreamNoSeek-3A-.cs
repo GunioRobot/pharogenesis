e9d2b13b122339457008937e9a25043878038ad9
JMM <unknown>
2000-11-08 10:33:00 +0000

    no message

---
+++ MPEGPlayer/instance/audio/setupStreamNoSeek-3A-.cs
@@ -0,0 +1,4 @@
+setupStreamNoSeek: aStream
+	self sampleRate: (self audioSampleRate: aStream).
+	SoundPlayer startPlayerProcessBufferSize:  8192 "(SoundPlayer bufferMSecs * self sampleRate) // 1000"
+		rate: self sampleRate stereo: ((self  audioChannels: aStream) > 1).
