9946d94c63af8834c9446dbdfb94aaf0fb846256
sw <unknown>
2000-12-13 10:54:00 +0000

    no message

---
+++ Lexicon/class/visible category names/sendersCategoryName.cs
@@ -0,0 +1,6 @@
+sendersCategoryName
+	"Answer the name to be used for the senders-results category"
+
+	true ifTrue: [^ #'-- "senders" results --'].
+
+	^ '-- "senders" results --'.  "so methods-strings-containing will find this"
