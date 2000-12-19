401aea40b66feecb1abec61c78fd08b886b63818
ar <unknown>
2000-12-19 19:16:00 +0000

    no message

--- PaintBoxMorph/instance/actions/showColorPalette-3A-.cs
+++ PaintBoxMorph/instance/actions/showColorPalette-3A-.cs
@@ -1,6 +1,13 @@
 showColorPalette: evt
 
+	| w box |
 	self comeToFront.
 	colorMemory align: colorMemory bounds topRight 
 			with: colorMemoryThin bounds topRight.
+	"make sure color memory fits or else align with left"
+	w _ self world.
+	box _ self bounds: colorMemory fullBounds in: w.
+	box left < 0 ifTrue:[
+		colorMemory align: colorMemory bounds topLeft
+			with: colorMemoryThin bounds topLeft].
 	self addMorphFront: colorMemory.
