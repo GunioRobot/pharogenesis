ccb83efb44f1fb499a8bc644740a6fc294fee722
mdr <unknown>
2000-12-24 01:14:00 +0000

    no message

--- MessageFile/instance/message operations/deleteMessageAt-3A-id-3A-.cs
+++ MessageFile/instance/message operations/deleteMessageAt-3A-id-3A-.cs
@@ -2,7 +2,9 @@ deleteMessageAt: filePosition id: msgID
 	"Mark as deleted the message with the given ID located at the given file position."
 
 	self ensureFileIsOpen.
-	self assertValidMessageAt: filePosition id: msgID.
+	(self assertValidMessageAt: filePosition id: msgID)
+		ifFalse: [^false].		"Don't delete if it looks like we have a problem"
+
 	file position: filePosition.
 	file nextPutAll: '&&&&&XXXXX'.		"delimiter for deleted messages"
 	file flush.
