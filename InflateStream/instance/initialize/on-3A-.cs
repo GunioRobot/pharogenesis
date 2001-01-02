c7a6fd1ab51d9154478579b6d8643d8e3359c8fd
ls <unknown>
2001-01-02 11:44:00 +0000

    no message

--- InflateStream/instance/initialize/on-3A-.cs
+++ InflateStream/instance/initialize/on-3A-.cs
@@ -1,6 +1,7 @@
 on: aCollectionOrStream
 	aCollectionOrStream isStream 
-		ifTrue:[	sourceStream _ aCollectionOrStream.
+		ifTrue:[	aCollectionOrStream binary.
+				sourceStream _ aCollectionOrStream.
 				self getFirstBuffer]
 		ifFalse:[source _ aCollectionOrStream].
 	^self on: source from: 1 to: source size.
