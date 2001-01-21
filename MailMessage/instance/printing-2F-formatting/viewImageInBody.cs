e2515535146957da7effdf4a5ac4c96704dc0fe6
sbw <unknown>
2001-01-21 11:10:00 +0000

    no message

--- MailMessage/instance/printing-2F-formatting/viewImageInBody.cs
+++ MailMessage/instance/printing-2F-formatting/viewImageInBody.cs
@@ -2,4 +2,4 @@ viewImageInBody
 	| stream image |
 	stream _ self body contentStream.
 	image _ Form fromBinaryStream: stream.
-	(World drawingClass withForm: image) openInWorld
+	(SketchMorph withForm: image) openInWorld
