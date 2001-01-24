0490aa20786f29a67eb510ac92358e873c732839
jm <unknown>
2001-01-24 12:42:00 +0000

    no message

---
+++ StarSqueakMorph/instance/setup/displayPatchVariable-3A-.cs
@@ -0,0 +1,4 @@
+displayPatchVariable: patchVarName
+	"Make this StarSqueak world display the patch variable of the given name. Only one patch variable can be displayed at any given time."
+
+	self displayPatchVariable: patchVarName logScale: -2.
