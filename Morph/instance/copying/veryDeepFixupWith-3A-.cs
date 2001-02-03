bf40b584ef5fd3c2df3dcc497cead3eedfcebe27
tk <unknown>
2001-02-03 14:29:00 +0000

    no message

--- Morph/instance/copying/veryDeepFixupWith-3A-.cs
+++ Morph/instance/copying/veryDeepFixupWith-3A-.cs
@@ -1,5 +1,9 @@
 veryDeepFixupWith: deepCopier
 	"If some fields were weakly copied, fix new copy here."
 
-"super veryDeepFixupWith: deepCopier."
-"My owner should already have been fixed up."
+	"super veryDeepFixupWith: deepCopier.	Object has no fixups, so don't call it"
+
+	"If my owner is being duplicated too, then store his duplicate.
+	 If I am owned outside the duplicated tree, then I am no longer owned!"
+	owner _ deepCopier references at: owner ifAbsent: [nil].
+
