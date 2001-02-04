f202d87a916c01ada826da2f5402edc89de23430
ar <unknown>
2001-02-04 18:01:00 +0000

    no message

--- SoundPlayer/class/player process/startPlayerProcessBufferSize-3A-rate-3A-stereo-3A-.cs
+++ SoundPlayer/class/player process/startPlayerProcessBufferSize-3A-rate-3A-stereo-3A-.cs
@@ -1,26 +1,7 @@
 startPlayerProcessBufferSize: bufferSize rate: samplesPerSecond stereo: stereoFlag
 	"Start the sound player process. Terminate the old process, if any."
 	"SoundPlayer startPlayerProcessBufferSize: 1000 rate: 11025 stereo: false"
-
-	self stopPlayerProcess.
-	ActiveSounds _ OrderedCollection new.
-	Buffer _ SoundBuffer newStereoSampleCount: (bufferSize // 4) * 4.
-	PlayerSemaphore _ Semaphore forMutualExclusion.
-	SamplingRate _ samplesPerSecond.
-	Stereo _ stereoFlag.
-	ReadyForBuffer _ Semaphore new.
-	SoundSupported _ true. "Assume so"
-	UseReadySemaphore _ true.  "set to false if ready semaphore not supported by VM"
-	self primSoundStartBufferSize: Buffer stereoSampleCount
-		rate: samplesPerSecond
-		stereo: Stereo
-		semaIndex: (Smalltalk registerExternalObject: ReadyForBuffer).
-	"Check if sound start prim was successful"
-	SoundSupported ifFalse:[^self].
-	UseReadySemaphore
-		ifTrue: [PlayerProcess _ [SoundPlayer playLoop] newProcess]
-		ifFalse: [PlayerProcess _ [SoundPlayer oldStylePlayLoop] newProcess].
-	UseReverb ifTrue: [self startReverb].
-
-	PlayerProcess priority: Processor userInterruptPriority.
-	PlayerProcess resume.
+	^self startPlayerProcessBufferSize: bufferSize 
+			rate: samplesPerSecond 
+			stereo: stereoFlag 
+			sound: nil
