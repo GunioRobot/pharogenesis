e7526df0260797154c7f0528affff1caa0f9ceeb
jm <unknown>
2001-02-06 17:42:00 +0000

    no message

---
+++ StarSqueakMorph/instance/private-2D-primitives/testDiffusePrim.cs
@@ -0,0 +1,10 @@
+testDiffusePrim
+	"This test should diffuse the initial value in the center cell so that each cell has 1000."
+	"StarSqueakMorph new testDiffusePrim"
+
+	| src dst |
+	src _ Bitmap new: 49.
+	src at: 25 put: 49000.
+	dst _ Bitmap new: 49.
+	self primDiffuseFrom: src to: dst width: 7 height: 7 delta: 3.
+	^ dst asArray
