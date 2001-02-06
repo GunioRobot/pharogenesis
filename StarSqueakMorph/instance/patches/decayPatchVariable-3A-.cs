59972b8fb25568b2981cee65ac74ac0041883c76
jm <unknown>
2001-02-06 17:54:00 +0000

    no message

---
+++ StarSqueakMorph/instance/patches/decayPatchVariable-3A-.cs
@@ -0,0 +1,7 @@
+decayPatchVariable: patchVarName
+	"Decay the values of the patch variable of the given name. That is, the value of each patch is replaced by a fraction of its former value, resulting in an expontial decay each patch's value over time. This can be used to model evaporation of a pheromone."
+
+	| patchVar |
+	patchVar _ patchVariables at: patchVarName ifAbsent: [^ self].
+	self primEvaporate: patchVar rate: scaledEvaporationRate.
+
