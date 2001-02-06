e1b3f9a1c4018f167175c6f687cfdedfb2670d1c
ar <unknown>
2001-02-06 16:16:00 +0000

    no message

---
+++ SecurityManager/instance/testing/hasFileAccess.cs
@@ -0,0 +1,5 @@
+hasFileAccess
+	"SecurityManager default hasFileAccess"
+	"Return true if the right to access arbitrary files hasn't been revoked"
+	<primitive: 'primitiveHasFileAccess' module: 'FilePlugin'>
+	^true "assume so unless otherwise proven"
