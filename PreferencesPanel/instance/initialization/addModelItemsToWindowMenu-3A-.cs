358fc3d5de98b8c1cb149cc2d31fe8aec1518b09
sw <unknown>
2001-02-06 02:13:00 +0000

    no message

--- PreferencesPanel/instance/initialization/addModelItemsToWindowMenu-3A-.cs
+++ PreferencesPanel/instance/initialization/addModelItemsToWindowMenu-3A-.cs
@@ -1,6 +1,9 @@
 addModelItemsToWindowMenu: aMenu
 	"aMenu is being constructed to be presented to the user in response to the user's pressing on the menu widget in the title bar of a morphic SystemWindow.  Here, the model is given the opportunity to add any model-specific items to the menu, whose default target is the SystemWindow itself."
 
+	true ifTrue: [^ self].  
+
+	"The below are provisionally disenfranchised, because their function is now directly available in the ? category"
 	aMenu addLine.
 	aMenu add: 'find preference... (f)' target: self action: #findPreference:.
 	aMenu add: 'inspect parameters' target: Preferences action: #inspectParameters
