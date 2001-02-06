42f5c35bbaf6f3d5a03150fdacde3da91726ac4c
ar <unknown>
2001-02-06 16:14:00 +0000

    no message

---
+++ SecurityManager/instance/security operations/disableFileAccess.cs
@@ -0,0 +1,6 @@
+disableFileAccess
+	"SecurityManager default disableFileAccess"
+	"Primitive. Disable unlimited access to files.
+	Cannot be revoked from the image."
+	<primitive: 'primitiveDisableFileAccess' module: 'FilePlugin'>
+	^self primitiveFailed
