67a062864dfd890eea65f399a051489d67531f48
ar <unknown>
2001-02-06 16:15:00 +0000

    no message

---
+++ SecurityManager/instance/security operations/disableImageWrite.cs
@@ -0,0 +1,6 @@
+disableImageWrite
+	"SecurityManager default disableImageWrite"
+	"Primitive. Disable writing to an image file.
+	Cannot be revoked from the image."
+	<primitive: 'primitiveDisableImageWrite' module: 'SecurityPlugin'>
+	^self primitiveFailed
