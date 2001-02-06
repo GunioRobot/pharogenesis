dae9717ea22ba4e83699c6e4c3732a9056d3480b
RAA <unknown>
2001-02-06 14:00:00 +0000

    no message

--- FormCanvas/instance/drawing-2D-rectangles/infiniteFillRectangle-3A-fillStyle-3A-.cs
+++ FormCanvas/instance/drawing-2D-rectangles/infiniteFillRectangle-3A-fillStyle-3A-.cs
@@ -1,5 +1,17 @@
 infiniteFillRectangle: aRectangle fillStyle: aFillStyle
 
+	| additionalOffset rInPortTerms clippedPort targetTopLeft clipOffset ex |
+
+	"this is a bit of a kludge to get the form to be aligned where I *think* it should be.
+	something better is needed, but not now"
+
+	additionalOffset _ 0@0.
+	ex _ aFillStyle form extent.
+	rInPortTerms _ aRectangle translateBy: origin.
+	clippedPort _ port clippedBy: rInPortTerms.
+	targetTopLeft _ clippedPort clipRect topLeft truncateTo: ex.
+	clipOffset _ rInPortTerms topLeft - targetTopLeft.
+	additionalOffset _ (clipOffset \\ ex) - ex.
 	^aFillStyle
-		displayOnPort: (port clippedBy: (aRectangle translateBy: origin)) 
-		at: aRectangle origin + origin
+		displayOnPort: clippedPort
+		offsetBy: additionalOffset
