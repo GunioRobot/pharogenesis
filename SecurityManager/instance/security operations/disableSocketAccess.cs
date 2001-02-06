0e4a099392a5ddd07751af1e37114f9fcdd38888
ar <unknown>
2001-02-06 16:15:00 +0000

    no message

---
+++ SecurityManager/instance/security operations/disableSocketAccess.cs
@@ -0,0 +1,6 @@
+disableSocketAccess
+	"SecurityManage default disableSocketAccess"
+	"Primitive. Disable access to sockets.
+	Cannot be revoked from the image."
+	<primitive: 'primitiveDisableSocketAccess' module: 'SocketPlugin'>
+	^self primitiveFailed
