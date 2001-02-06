358af94b200b0b5b51faef62ade41e3f54d93ba6
ar <unknown>
2001-02-06 16:14:00 +0000

    no message

---
+++ SecurityManager/instance/testing/canWriteImage.cs
@@ -0,0 +1,5 @@
+canWriteImage
+	"SecurityManager default canWriteImage"
+	"Primitive. Return true if the right to write an image hasn't been revoked."
+	<primitive: 'primitiveCanWriteImage' module: 'SecurityPlugin'>
+	^true "assume so unless otherwise proven"
