c411c68312a0f4a8209c0b6fa6b28b0a1a152540
sw <unknown>
2001-02-06 04:13:00 +0000

    no message

--- Model/instance/dependents/containingWindow.cs
+++ Model/instance/dependents/containingWindow.cs
@@ -2,4 +2,4 @@ containingWindow
 	"Answer the window that holds the receiver.  The dependents technique is odious and may not be airtight, if multiple windows have the same model."
 
 	^ self dependents detect:
-		[:d | (d isKindOf: SystemWindow orOf: StandardSystemView) and: [d model == self]] ifNone: [nil]
+		[:d | ((d isKindOf: SystemWindow orOf: StandardSystemView) or: [d isKindOf: MVCWiWPasteUpMorph]) and: [d model == self]] ifNone: [nil]
