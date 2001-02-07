8c71c9747bca4e6af8876d28190415735fe6ee4e
jm <unknown>
2001-02-07 19:20:00 +0000

    no message

---
+++ AntColonyTurtle/instance/demons/returnToNest.cs
@@ -0,0 +1,9 @@
+returnToNest
+
+	isCarryingFood ifTrue: [
+		"decrease size of pheromone drops to create a gradient back to food"
+		pheromoneDropSize > 0 ifTrue: [
+			self increment: 'pheromone' by: pheromoneDropSize.
+			pheromoneDropSize _ pheromoneDropSize - 20].
+		self turnTowardsStrongest: 'nestScent'.
+		self forward: 1].
