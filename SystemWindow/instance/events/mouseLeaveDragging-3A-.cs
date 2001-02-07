1a00ece3022e7b2754b1efc7c55857640528b7f3
RAA <unknown>
2001-02-07 07:12:00 +0000

    no message

--- SystemWindow/instance/events/mouseLeaveDragging-3A-.cs
+++ SystemWindow/instance/events/mouseLeaveDragging-3A-.cs
@@ -1,6 +1,6 @@
 mouseLeaveDragging: evt
 	"lock children after drop operations"
 	(self ~~ TopWindow and:[evt hand hasSubmorphs]) ifTrue:[
-		self submorphsDo:[:m| m lock].
+		self lockInactivePortions.
 		evt hand removeMouseListener: self.
 	].
