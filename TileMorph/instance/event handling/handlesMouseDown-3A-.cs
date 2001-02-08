2552a1ce9ec24ff38bf9273da673e5b74a38ccaa
ar <unknown>
2001-02-08 20:07:00 +0000

    no message

---
+++ TileMorph/instance/event handling/handlesMouseDown-3A-.cs
@@ -0,0 +1,12 @@
+handlesMouseDown: evt
+	"Answer whether the receiver would handle the mouseDown represented by evt"
+
+	| aPoint |
+	aPoint _ evt cursorPoint.
+	(operatorOrExpression notNil and: [upArrow notNil]) ifTrue: [^ true].
+		"Click on the operator presents list of alternatives"
+
+	upArrow ifNotNil: [^true].
+	suffixArrow ifNotNil: [^true].
+	retractArrow ifNotNil: [^true].
+	^ super handlesMouseDown: evt
