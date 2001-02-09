e891704f640575e6cd37254a342501641263d200
RAA <unknown>
2001-02-09 16:34:00 +0000

    no message

--- Browser/instance/initialize-2D-release/buildMorphicSystemCatList.cs
+++ Browser/instance/initialize-2D-release/buildMorphicSystemCatList.cs
@@ -1,9 +1,11 @@
 buildMorphicSystemCatList
 	| dragNDropFlag myCatList |
 	dragNDropFlag _ Preferences browseWithDragNDrop.
-	myCatList _ PluggableListMorph on: self list: #systemCategoryList
+	(myCatList _ PluggableListMorph new) 
+			setProperty: #highlightSelector toValue: #highlightSystemCategoryList:with:;
+
+			on: self list: #systemCategoryList
 			selected: #systemCategoryListIndex changeSelected: #systemCategoryListIndex:
 			menu: #systemCategoryMenu: keystroke: #systemCatListKey:from:.
 	myCatList enableDragNDrop: dragNDropFlag.
-	myCatList highlightSelector: #highlightSystemCategoryList:with:.
 	^myCatList
