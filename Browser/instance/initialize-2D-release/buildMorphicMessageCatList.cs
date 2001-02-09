6875d4d96f5d52ccaa0436ea8c2fa45ef2720e9c
RAA <unknown>
2001-02-09 16:37:00 +0000

    no message

--- Browser/instance/initialize-2D-release/buildMorphicMessageCatList.cs
+++ Browser/instance/initialize-2D-release/buildMorphicMessageCatList.cs
@@ -1,10 +1,13 @@
 buildMorphicMessageCatList
 
 	| myMessageCatList |
-	myMessageCatList _ PluggableMessageCategoryListMorph on: self list: #messageCategoryList
+
+	(myMessageCatList _ PluggableMessageCategoryListMorph new) 
+			setProperty: #highlightSelector toValue: #highlightMessageCategoryList:with:;
+
+			on: self list: #messageCategoryList
 			selected: #messageCategoryListIndex changeSelected: #messageCategoryListIndex:
 			menu: #messageCategoryMenu: 
 			keystroke: #arrowKey:from: getRawListSelector: #rawMessageCategoryList.
 	myMessageCatList enableDragNDrop: Preferences browseWithDragNDrop.
-	myMessageCatList highlightSelector: #highlightMessageCategoryList:with:.
 	^myMessageCatList
