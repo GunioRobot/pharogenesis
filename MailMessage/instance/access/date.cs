791a759c385c22cb25b8d4e644319b071000e7d2
ls <unknown>
2001-02-10 12:19:00 +0000

    no message

--- MailMessage/instance/access/date.cs
+++ MailMessage/instance/access/date.cs
@@ -1,5 +1,5 @@
 date
 	"Answer a date string for this message."
 
-	^(Date fromSeconds: time + (Date newDay: 1 year: 1980) asSeconds) 
+	^(Date fromSeconds: self time + (Date newDay: 1 year: 1980) asSeconds) 
 		printFormat: #(2 1 3 47 1 2)
