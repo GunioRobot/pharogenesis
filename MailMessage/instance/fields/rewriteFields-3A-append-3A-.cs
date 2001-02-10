18fb85dac08944eeeedcd8abacfc8b1cf734ae94
ls <unknown>
2001-02-10 13:47:00 +0000

    no message

--- MailMessage/instance/fields/rewriteFields-3A-append-3A-.cs
+++ MailMessage/instance/fields/rewriteFields-3A-append-3A-.cs
@@ -3,6 +3,7 @@ rewriteFields: aBlock append: appendBlock
 	Each field's key and value is reported to aBlock. The block's return value is the replacement for the entire header line. Nil means don't change the line, empty means delete it. After all fields are processed, evaluate appendBlock and append the result to the header."
 
 	| old new result appendString |
+	self halt: 'this method is out of date.  it needs to update body, at the very least.  do we really need this now that we have setField:to: and setField:toString: ?!'.
 	old _ ReadStream on: text.
 	new _ WriteStream on: (String new: text size).
 	self fieldsFrom: old do: [ :fName :fValue |
@@ -17,4 +18,3 @@ rewriteFields: aBlock append: appendBlock
 		appendString last = Character cr ifFalse: [new cr]].
 	new cr. "End of header"
 	text _ new contents, old upToEnd.
-	self cacheFieldsFrom: (ReadStream on: text) andDo: [:f :n | "Just chache it"]
