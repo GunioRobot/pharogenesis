d27402bd70189fa8220c45d508d770729a6ed524
sr <unknown>
2001-02-12 03:38:00 +0000

    no message

--- Utilities/class/fetching updates/applyUpdatesFromDisk.cs
+++ Utilities/class/fetching updates/applyUpdatesFromDisk.cs
@@ -1,3 +1,15 @@
 applyUpdatesFromDisk
 	"Utilities applyUpdatesFromDisk"
-	self applyUpdatesFromDiskToUpdateNumber: 1000000 "one million should be sufficient ;-)"
+	"compute highest update number"
+	| updateDirectory updateNumbers |
+	updateDirectory _ self getUpdateDirectoryOrNil.
+	updateDirectory
+		ifNil: [^ self].
+	updateNumbers _ updateDirectory fileNames
+				collect: [:fn | fn initialIntegerOrNil]
+				thenSelect: [:fn | fn notNil].
+	self
+		applyUpdatesFromDiskToUpdateNumber: (updateNumbers
+				inject: 0
+				into: [:max :num | max max: num])
+		stopIfGap: false
