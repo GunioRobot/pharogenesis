18a73633853d31f59fabf7c226bfce386770b59c
ar <unknown>
2001-02-12 13:55:00 +0000

    no message

--- TileMorph/instance/arrows/variableDelay-3A-.cs
+++ TileMorph/instance/arrows/variableDelay-3A-.cs
@@ -1,10 +1,13 @@
 variableDelay: aBlock
 
 	| now delay dt |
+	(self hasProperty: #inVariableDelay) ifTrue:[^self].
 	nArrowTicks ifNil: [nArrowTicks _ 1].
 	now _ Time millisecondClockValue.
 	aBlock value.
 	delay _ nArrowTicks > 5 ifTrue: [100] ifFalse: [300].
 	nArrowTicks _ nArrowTicks + 1.
 	dt _ Time millisecondClockValue - now max: 0.  "Time it took to do."
-	dt < delay ifTrue: [(Delay forMilliseconds: delay - dt) wait].
+	dt < delay ifTrue: [
+		self setProperty: #inVariableDelay toValue: true.
+		self addAlarm: #removeProperty: withArguments: #(inVariableDelay) after: (delay - dt)].
