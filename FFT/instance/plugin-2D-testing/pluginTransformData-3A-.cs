9987047dac8a12d39cc38f55b9511f717a26cad7
ar <unknown>
2001-02-13 21:10:00 +0000

    no message

--- FFT/instance/plugin-2D-testing/pluginTransformData-3A-.cs
+++ FFT/instance/plugin-2D-testing/pluginTransformData-3A-.cs
@@ -2,4 +2,5 @@ pluginTransformData: forward
 	"Plugin testing -- if the primitive is not implemented 
 	or cannot be found run the simulation. See also: FFTPlugin"
 	<primitive: 'primitiveFFTTransformData' module: 'FFTPlugin'>
-	^FFTPlugin doPrimitive: 'primitiveFFTTransformData'.
+	^(Smalltalk at: #FFTPlugin ifAbsent:[^self primitiveFailed])
+		doPrimitive: 'primitiveFFTTransformData'.
