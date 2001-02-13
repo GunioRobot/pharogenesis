ad224eb5eeb1cb6348abd43d126ab67d319669dc
RAA <unknown>
2001-02-13 17:24:00 +0000

    no message

--- HandMorph/instance/halo handling/removeHaloFromClick-3A-on-3A-.cs
+++ HandMorph/instance/halo handling/removeHaloFromClick-3A-on-3A-.cs
@@ -2,4 +2,7 @@ removeHaloFromClick: anEvent on: aMorph
 	| halo |
 	halo _ self halo ifNil:[^self].
 	(halo target hasOwner: self) ifTrue:[^self].
-	(halo staysUpWhenMouseIsDownIn: aMorph) ifFalse:[halo delete].
+	(halo staysUpWhenMouseIsDownIn: aMorph) ifFalse:[
+		halo delete.
+		self removeProperty: #halo.
+	].
