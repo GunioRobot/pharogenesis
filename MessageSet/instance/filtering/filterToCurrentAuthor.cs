63e66f79e2fc65c92e318fed6d3df86288726449
sw <unknown>
2001-02-14 18:30:00 +0000

    no message

--- MessageSet/instance/filtering/filterToCurrentAuthor.cs
+++ MessageSet/instance/filtering/filterToCurrentAuthor.cs
@@ -1,4 +1,12 @@
 filterToCurrentAuthor
 	"Filter down only to messages with my initials as most recent author"
 
-	self notYetImplemented
+	| myInitials aMethod aTimeStamp |
+	(myInitials _ Utilities authorInitialsPerSe) ifNil: [^ self inform: 'No author initials set in this image'].
+	self filterFrom:
+		[:aClass :aSelector |
+			(aClass notNil and: [aSelector notNil]) and:			
+				[aMethod _ aClass compiledMethodAt: aSelector ifAbsent: [nil].
+				aMethod notNil and:
+					[(aTimeStamp _ Utilities timeStampForMethod: aMethod) notNil and:
+						[aTimeStamp beginsWith: myInitials]]]]
