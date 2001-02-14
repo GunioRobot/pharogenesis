055d6fb7489763fac0d8e5de2c53057434f24b7a
RAA <unknown>
2001-02-14 15:40:00 +0000

    no message

--- SyntaxMorph/instance/menus/acceptIgnoring-3A-.cs
+++ SyntaxMorph/instance/menus/acceptIgnoring-3A-.cs
@@ -1,4 +1,5 @@
 acceptIgnoring: aString
 	"If I am inside a ScriptEditorMorph, tell my root to accept the new changes.  Ignore the argument, which is the string whose conents just changed."
 
+	thisContext sender receiver removeProperty: #syntacticallyCorrectContents.
 	self acceptIfInScriptor
