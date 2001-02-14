bed49a8e48c3b55d4352dc3b533a66cbdb4b05b9
sw <unknown>
2001-02-14 18:24:00 +0000

    no message

--- MessageSet/instance/filtering/filterToNotCurrentAuthor.cs
+++ MessageSet/instance/filtering/filterToNotCurrentAuthor.cs
@@ -1,4 +1,12 @@
 filterToNotCurrentAuthor
 	"Filter down only to messages not stamped with my initials"
 
-	self notYetImplemented
+	| myInitials aMethod aTimeStamp |
+	(myInitials _ Utilities authorInitialsPerSe) ifNil: [^ self inform: 'No author initials set in this image'].
+	self filterFrom:
+		[:aClass :aSelector |
+			(aClass notNil and: [aSelector notNil]) and:			
+				[aMethod _ aClass compiledMethodAt: aSelector ifAbsent: [nil].
+				aMethod notNil and:
+					[(aTimeStamp _ Utilities timeStampForMethod: aMethod) isNil or:
+						[(aTimeStamp beginsWith: myInitials) not]]]]
