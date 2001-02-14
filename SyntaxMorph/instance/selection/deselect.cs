9fb74868a851d6a03372bd0dd49597351907e5ed
RAA <unknown>
2001-02-14 11:33:00 +0000

    no message

--- SyntaxMorph/instance/selection/deselect.cs
+++ SyntaxMorph/instance/selection/deselect.cs
@@ -1,6 +1,6 @@
 deselect
 	self allMorphsDo:
-		[:m | m isSyntaxMorph ifTrue: [m color: Color transparent; borderColor: Color transparent]].
+		[:m | m isSyntaxMorph ifTrue: [m setDeselectedColor]].
 
 	"Note following is wasteful because we do a deselect before each select, and it is often the same morph."
 	self deletePopup
