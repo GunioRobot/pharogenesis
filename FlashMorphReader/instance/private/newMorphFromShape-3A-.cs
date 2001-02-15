779f0e276f289da13dba2009f556f9910b94b505
tk <unknown>
2001-02-15 16:33:00 +0000

    no message

--- FlashMorphReader/instance/private/newMorphFromShape-3A-.cs
+++ FlashMorphReader/instance/private/newMorphFromShape-3A-.cs
@@ -2,8 +2,9 @@ newMorphFromShape: objectIndex
 	"Return a new character morph from the given object index.
 	If the character morph at objectIndex is already used, then create and return a full copy of it"
 	| prototype |
+
 	prototype _ self oldMorphFromShape: objectIndex.
 	prototype isNil ifTrue:[^nil].
 	^(prototype owner notNil) 
-		ifTrue:[prototype fullCopy]
+		ifTrue:[prototype veryDeepCopy]
 		ifFalse:[prototype].
