0d8dc8534d403cdbf84f366aac92eb3bfa921fa7
tk <unknown>
2001-02-15 16:36:00 +0000

    no message

--- CompoundTileMorph/instance/miscellaneous/tileRows.cs
+++ CompoundTileMorph/instance/miscellaneous/tileRows.cs
@@ -1,4 +1,4 @@
 tileRows
 	"Answer a list of tile rows, in this case just one though it's compound"
 
-	^ Array with: (Array with: self fullCopyWithoutFormerOwner)
+	^ Array with: (Array with: self veryDeepCopy)
