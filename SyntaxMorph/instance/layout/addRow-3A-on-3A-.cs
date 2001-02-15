fe43971c6be5b96fe8c82c448af34c19e443b5da
RAA <unknown>
2001-02-15 19:22:00 +0000

    no message

--- SyntaxMorph/instance/layout/addRow-3A-on-3A-.cs
+++ SyntaxMorph/instance/layout/addRow-3A-on-3A-.cs
@@ -2,4 +2,7 @@ addRow: aColorOrSymbol on: aNode
 
 	| row |
 	self addMorphBack: (row _ self class row: aColorOrSymbol on: aNode).
+
+"row setProperty: #howCreated toValue: thisContext longStack."
+
 	^row
