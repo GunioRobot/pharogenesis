9523ae1e0f0f3c1a32f27b12ef02c796b1ec0593
RAA <unknown>
2001-02-15 19:34:00 +0000

    no message

--- MessageNode/instance/tiles/asMorphicSyntaxIn-3A-.cs
+++ MessageNode/instance/tiles/asMorphicSyntaxIn-3A-.cs
@@ -1,23 +1,7 @@
 asMorphicSyntaxIn: parent
 
-	| printer substitute row sel |
-	sel _ #message.
-	((parent nodeClassIs: CascadeNode) and: [parent parseNode receiver ~~ self]) ifTrue: [
-		sel _ #keyword2.
-		receiver ifNotNil: [self inform: 'receiver should be nil']].
-	row _ parent addRow: sel on: self.
-	special > 0 ifTrue: [printer _ MacroPrinters at: special].
-	substitute _ self as: TileMessageNode.
-	(printer == #printCaseOn:indent:) ifTrue: [
-		self asMorphicCaseOn: row indent: nil.
-		^ parent].
-	(special > 0)
-		ifTrue: 
-			[substitute perform: printer with: row with: nil]
-		ifFalse: 
-			[substitute 
-				printKeywords: selector key
-				arguments: arguments
-				on: row
-				indent: nil].
-	^ row addTransparentSpacerOfSize: 3@10.
+	^parent 
+		vanillaMessageNode: self 
+		receiver: receiver 
+		selector: selector 
+		arguments: arguments
