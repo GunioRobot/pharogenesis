761bf47a20c3cb4b2f4d4b68aa1f3e9117f81a2f
sw <unknown>
2001-02-16 00:46:00 +0000

    no message

---
+++ UniclassScript/instance/updating/revertToLastSavedTileVersionFor-3A-.cs
@@ -0,0 +1,12 @@
+revertToLastSavedTileVersionFor: anEditor
+	"revert to the last saved tile version"
+
+	Preferences universalTiles
+		ifFalse:
+			[formerScriptingTiles isEmptyOrNil ifFalse:
+				[anEditor reinsertSavedTiles: formerScriptingTiles last second]]
+		ifTrue:
+			[anEditor removeAllButFirstSubmorph.
+			anEditor insertUniversalTiles].
+	anEditor showingMethodPane: false.
+	isTextuallyCoded _ false
