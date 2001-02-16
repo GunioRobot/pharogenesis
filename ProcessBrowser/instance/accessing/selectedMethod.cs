1efc62fa90ca50fbb55ba208ab90976ab7ec3261
nk <unknown>
2001-02-16 13:39:00 +0000

    no message

--- ProcessBrowser/instance/accessing/selectedMethod.cs
+++ ProcessBrowser/instance/accessing/selectedMethod.cs
@@ -2,7 +2,8 @@ selectedMethod
 	^ methodText ifNil: [methodText _ selectedContext
 						ifNil: ['']
 						ifNotNil: [| pcRange | 
-							methodText _ selectedContext sourceCode.
+							methodText _ [ selectedContext sourceCode ]
+								ifError: [ :err :rcvr | 'error getting method text' ].
 							pcRange _ self pcRange.
 							methodText asText
 								addAttribute: TextColor red
