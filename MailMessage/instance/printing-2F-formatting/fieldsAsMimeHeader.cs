3994643be6c962656e0a6b519eb72a02b3374e6f
RAA <unknown>
2001-02-16 07:40:00 +0000

    no message

--- MailMessage/instance/printing-2F-formatting/fieldsAsMimeHeader.cs
+++ MailMessage/instance/printing-2F-formatting/fieldsAsMimeHeader.cs
@@ -1,10 +1,14 @@
 fieldsAsMimeHeader
 	"return the entire header in proper MIME format"
+
+	self halt.		"This no longer appears to be used and since, as a result of recent changes, it references an undeclared variable <subject>, I have commented out the code to clean up the inspection of undeclared vars"
+
+"---
 	| strm |
 	strm _ WriteStream on: (String new: 100).
 	self fields associationsDo: [:e | strm nextPutAll: e key;
 		 nextPutAll: ': ';
-		 "circumvent the forced lowercasing of the subject MimeHeader"
 		 nextPutAll: (e key = 'subject' ifTrue: [subject] ifFalse: [e value asHeaderValue]);
 		 cr]. 
 	^ strm contents
+---"
