99e70bd8c6d760df041aaab250528fac9549ca31
RAA <unknown>
2001-02-16 08:15:00 +0000

    no message

--- TextMorph/instance/private/installEditor.cs
+++ TextMorph/instance/private/installEditor.cs
@@ -1,4 +1,9 @@
 installEditor
+
+
+	self flag: #bob.		"I don't see any senders (16 Feb 2001)"
+
+
 	"Install an editor for my paragraph.  This constitutes 'hasFocus'."
 	editor ifNotNil: [^ editor].
 	^ self installEditorToReplace: nil
