153d0286498c413ec0ad2bcd8e8cabfb55bd2044
sw <unknown>
2001-02-17 01:05:00 +0000

    no message

--- Player/instance/scripts-2D-kernel/scriptEditorFor-3A-.cs
+++ Player/instance/scripts-2D-kernel/scriptEditorFor-3A-.cs
@@ -2,7 +2,7 @@ scriptEditorFor: aSelector
 	"Answer the receiver's script editor for aSelector"
 
 	| aScriptEditor |
-	aScriptEditor _ (self class userScriptForPlayer: self selector: aSelector) instantiatedScriptEditor.
+	aScriptEditor _ (self class userScriptForPlayer: self selector: aSelector) instantiatedScriptEditorForPlayer: self.
 	aScriptEditor updateToPlayer: self.
 	aScriptEditor bringUpToDate.
 	^ aScriptEditor
