8a6db7862a9ed27a4999a2d69bbdcf4d6549425c
sw <unknown>
2001-02-17 03:28:00 +0000

    no message

---
+++ MethodWithInterface/instance/script editor/currentScriptEditor-3A-.cs
@@ -0,0 +1,2 @@
+currentScriptEditor: anEditor
+	"Set the receiver's currentScriptEditor as indicated, if I care.  MethodWithInterface does not care, since it does not hold on to a ScriptEditor.  A subclass of mine, however does, or did, care"
