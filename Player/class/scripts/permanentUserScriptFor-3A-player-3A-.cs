bef53bf50bfb374e2179fe2eabc049f11b71c478
sw <unknown>
2001-02-17 00:59:00 +0000

    no message

--- Player/class/scripts/permanentUserScriptFor-3A-player-3A-.cs
+++ Player/class/scripts/permanentUserScriptFor-3A-player-3A-.cs
@@ -1,8 +1,8 @@
 permanentUserScriptFor: aSelector player: aPlayer
-	"Create and answer a UserScript object for the given player (an instance of the receiver) and selector.  Save that UniclassScript in my (i.e. the class's) directory of scripts"
+	"Create and answer a suitable script  object for the given player (who will be an instance of the receiver) and selector.  Save that script-interface object in my (i.e. the class's) directory of scripts"
 
 	|  entry |
 	scripts ifNil: [scripts _ IdentityDictionary new].
-	entry _ UniclassScript new playerClass: aPlayer class selector: aSelector.
+	entry _ self nascentUserScriptInstance playerClass: aPlayer class selector: aSelector.
 	scripts at: aSelector put: entry.
 	^ entry
