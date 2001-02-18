8212b6e40ab2562b2aa575e8f851bd6133575be8
sw <unknown>
2001-02-18 17:50:00 +0000

    no message

--- StandardScriptingSystem/instance/utilities/newScriptingSpace.cs
+++ StandardScriptingSystem/instance/utilities/newScriptingSpace.cs
@@ -1,46 +1,4 @@
 newScriptingSpace
-	"Answer a complete scripting space"
+	"Answer a complete scripting space - raa 19 sept 2000 - experiment for Alan, a variant *not* in a window, now adopted as the only true scripting space"
 
-	| aTemplate  palette aPlayfield aWindow itsModel |
-
-	"19 sept 2000 - experiment for Alan"
-	1 = 1 ifTrue: [^self newScriptingSpace2].
-
-	aWindow _ (SystemWindow labelled: 'scripting area') model: (itsModel _ ScriptingDomain new).
-	aWindow setStripeColorsFrom: itsModel defaultBackgroundColor.
-	aWindow extent: 640 @ 480.
-	aTemplate _ PasteUpMorph new setNameTo: 'etoy'.
-	aTemplate extent: 638 @ 470.
-	aWindow addMorph: aTemplate frame: (0@0 corner: 1@1).
-	aTemplate setStandardTexture.
-	aTemplate impartPrivatePresenter; setProperty: #automaticPhraseExpansion toValue: true; beSticky.
-	aTemplate presenter addStopStepGoButtons; addTrashCan.
-
-	palette _ TabbedPalette new pageSize: 200@320.
-	palette beSticky; useRoundedCorners.
-	aTemplate addMorph: palette.
-	palette addMenuTab.
-	palette addPartsTab.
-	palette addControlsTab.
-	palette addTilesTab.
-	palette selectTabNamed: 'supplies'.
-	palette becomeStandardPalette.
-	aPlayfield _ PasteUpMorph new setNameTo: 'playfield'.
-	aPlayfield useRoundedCorners.
-	(Preferences valueOfFlag: #eToyScheme)  "Not widely advertised"
-		ifFalse:	
-			[aPlayfield setToAdhereToEdge: #topRight.
-			palette setToAdhereToEdge: #topLeft]
-		ifTrue:
-			[aPlayfield setToAdhereToEdge: #topLeft.
-			palette setToAdhereToEdge: #topRight].
-
-	aTemplate addMorph: aPlayfield.
-	aPlayfield extent: 340@300; position: aTemplate topRight - (400@0).
-	aPlayfield beSticky.
-	aPlayfield automaticViewing: true.
-	aPlayfield wantsMouseOverHalos: true.
-	aTemplate presenter standardPlayfield: aPlayfield.
-	
-	^ aWindow
-
+	^ self newScriptingSpace2
