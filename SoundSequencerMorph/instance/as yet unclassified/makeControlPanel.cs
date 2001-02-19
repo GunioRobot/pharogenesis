ce45ada881db30bfba5afd5e0fca8c1478886363
tk <unknown>
2001-02-19 18:14:00 +0000

    no message

--- SoundSequencerMorph/instance/as yet unclassified/makeControlPanel.cs
+++ SoundSequencerMorph/instance/as yet unclassified/makeControlPanel.cs
@@ -1,9 +1,14 @@
 makeControlPanel
-	| b |
-	b _ SimpleButtonMorph new target: self; borderColor: Color black.
+	| bb cc |
+	cc _ Color black.
+	bb _ SimpleButtonMorph new target: self; borderColor: cc.
 	controlPanel _ AlignmentMorph newRow.
-	controlPanel color: b color; borderWidth: 0; layoutInset: 0.
+	bb _ SimpleButtonMorph new target: self; borderColor: cc.
+	controlPanel color: bb color; borderWidth: 0; layoutInset: 0.
 	controlPanel hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
-	controlPanel addMorphBack: (b fullCopy label: 'reset';	actionSelector: #reset).
-	controlPanel addMorphBack: (b fullCopy label: 'stop';		actionSelector: #stop).
-	controlPanel addMorphBack: (b fullCopy label: 'play';	actionSelector: #play).
+	bb _ SimpleButtonMorph new target: self; borderColor: cc.
+	controlPanel addMorphBack: (bb label: 'reset';	actionSelector: #reset).
+	bb _ SimpleButtonMorph new target: self; borderColor: cc.
+	controlPanel addMorphBack: (bb label: 'stop';		actionSelector: #stop).
+	bb _ SimpleButtonMorph new target: self; borderColor: cc.
+	controlPanel addMorphBack: (bb label: 'play';	actionSelector: #play).
