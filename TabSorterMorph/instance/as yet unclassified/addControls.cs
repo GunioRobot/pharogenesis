6fd099d347c2dd008fa15548abbc02c5c7d36982
tk <unknown>
2001-02-19 18:28:00 +0000

    no message

--- TabSorterMorph/instance/as yet unclassified/addControls.cs
+++ TabSorterMorph/instance/as yet unclassified/addControls.cs
@@ -6,6 +6,7 @@ addControls
 	r color: b color; borderWidth: 0; layoutInset: 0.
 	r hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
 	r wrapCentering: #topLeft.
-	r addMorphBack: (b fullCopy label: 'Okay';	actionSelector: #acceptSort).
-	r addMorphBack: (b fullCopy label: 'Cancel';	actionSelector: #cancelSort).
+	r addMorphBack: (b label: 'Okay';	actionSelector: #acceptSort).
+	b _ SimpleButtonMorph new target: self; borderColor: Color black.
+	r addMorphBack: (b label: 'Cancel';	actionSelector: #cancelSort).
 	self addMorphFront: r.
