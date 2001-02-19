4b5a63633482d7ff50ac965e51a3fa26760f32fa
tk <unknown>
2001-02-19 17:48:00 +0000

    no message

--- FlashSorterMorph/instance/initialization/makeControls.cs
+++ FlashSorterMorph/instance/initialization/makeControls.cs
@@ -1,11 +1,11 @@
 makeControls
 
-	| b r |
-	b _ SimpleButtonMorph new
+	| bb r |
+	bb _ SimpleButtonMorph new
 		target: self;
 		borderColor: #raised;
 		borderWidth: 2.
 	r _ AlignmentMorph newRow.
 	r hResizing: #spaceFill; vResizing: #spaceFill; layoutInset: 2.
-	r addMorphBack: (b fullCopy label: 'Make movie';		actionSelector: #makeMovie).
+	r addMorphBack: (bb label: 'Make movie';		actionSelector: #makeMovie).
 	^r
