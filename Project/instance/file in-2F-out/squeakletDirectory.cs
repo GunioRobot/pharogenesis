daeca8e6a4ba2608ce4df130b1d3d1b32f0a0a09
RAA <unknown>
2001-02-19 07:37:00 +0000

    no message

--- Project/instance/file in-2F-out/squeakletDirectory.cs
+++ Project/instance/file in-2F-out/squeakletDirectory.cs
@@ -1,8 +1,3 @@
 squeakletDirectory
 
-	| squeakletDirectoryName |
-	squeakletDirectoryName _ 'Squeaklets'.
-	(FileDirectory default directoryExists: squeakletDirectoryName) ifFalse: [
-		FileDirectory default createDirectory: squeakletDirectoryName
-	].
-	^FileDirectory default directoryNamed: squeakletDirectoryName
+	^self class squeakletDirectory
