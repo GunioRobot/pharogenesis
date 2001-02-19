d036bee2173944e6aa6a5127fa6050d3ab845998
tk <unknown>
2001-02-19 18:35:00 +0000

    no message

--- BookMorph/instance/other/makeMinimalControlsWithColor-3A-title-3A-.cs
+++ BookMorph/instance/other/makeMinimalControlsWithColor-3A-title-3A-.cs
@@ -1,7 +1,8 @@
 makeMinimalControlsWithColor: aColor title: aString
 
 	| aButton aColumn aRow but |
-	aButton _ SimpleButtonMorph new target: self; borderColor: Color black; color: aColor; borderWidth: 0.
+	aButton _ SimpleButtonMorph new target: self; borderColor: Color black; 
+				color: aColor; borderWidth: 0.
 	aColumn _ AlignmentMorph newColumn.
 	aColumn color: aButton color; borderWidth: 0; layoutInset: 0.
 	aColumn hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
@@ -10,13 +11,15 @@ makeMinimalControlsWithColor: aColor title: aString
 	aRow color: aButton color; borderWidth: 0; layoutInset: 0.
 	aRow hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
 	aRow addTransparentSpacerOfSize: 40@0.
-	aRow addMorphBack: (but _ aButton fullCopy label: ' < ' ; actionSelector: #previousPage).
-		"fullCopy is OK, since we just made it and it can't own any Players"
+	aRow addMorphBack: (but _ aButton label: ' < ' ; actionSelector: #previousPage).
+		"copy is OK, since we just made it and it can't own any Players"
 	but setBalloonText: 'Go to previous page'.
 	aRow addTransparentSpacerOfSize: 82@0.
 	aRow addMorphBack: (StringMorph contents: aString) lock.
 	aRow addTransparentSpacerOfSize: 82@0.
-	aRow addMorphBack: (but _ aButton fullCopy label: ' > ' ; actionSelector: #nextPage).
+	aButton _ SimpleButtonMorph new target: self; borderColor: Color black; 
+				color: aColor; borderWidth: 0.
+	aRow addMorphBack: (but _ aButton label: ' > ' ; actionSelector: #nextPage).
 	but setBalloonText: 'Go to next page'.
 	aRow addTransparentSpacerOfSize: 40@0.
 
