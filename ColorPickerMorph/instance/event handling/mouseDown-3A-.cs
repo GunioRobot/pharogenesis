2a301c42b2ad6c3d99126258cf828c9381bf318e
RAA <unknown>
2001-02-19 13:17:00 +0000

    no message

--- ColorPickerMorph/instance/event handling/mouseDown-3A-.cs
+++ ColorPickerMorph/instance/event handling/mouseDown-3A-.cs
@@ -3,10 +3,12 @@ mouseDown: evt
 	localPt _ evt cursorPoint - self topLeft.
 	self deleteAllBalloons.
 	clickedTranslucency _ TransparentBox containsPoint: localPt.
-	(DragBox containsPoint: localPt)
-		ifTrue: [^ evt hand grabMorph: self].
+	self inhibitDragging ifFalse: [
+		(DragBox containsPoint: localPt)
+			ifTrue: [^ evt hand grabMorph: self].
+	].
 	(RevertBox containsPoint: localPt)
 		ifTrue: [^ self updateColor: originalColor feedbackColor: originalColor].
-	self comeToFront.
+	self inhibitDragging ifFalse: [self comeToFront].
 	sourceHand _ evt hand.
 	self startStepping.
