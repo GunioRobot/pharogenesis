d56a424455340fec2892594812bae6b85ec0288a
RAA <unknown>
2001-02-20 20:25:00 +0000

    no message

--- ProjectLoading/class/as yet unclassified/openFromDirectory-3A-andFileName-3A-.cs
+++ ProjectLoading/class/as yet unclassified/openFromDirectory-3A-andFileName-3A-.cs
@@ -1,11 +1,16 @@
 openFromDirectory: aDirectory andFileName: aFileName
 
+	| fileAndDir |
+
 	ComplexProgressIndicator new 
 		targetMorph: nil;
 		historyCategory: 'project loading';
 		withProgressDo: [
-			ProjectLoading 
-				openFromFile: (aDirectory oldFileNamed: aFileName) 
-				fromDirectory: aDirectory
+			ProgressNotification signal: '1:foundMostRecent'.
+			fileAndDir _ self bestAccessToFileName: aFileName andDirectory: aDirectory.
+			self 
+				openName: aFileName 
+				stream: fileAndDir first 
+				fromDirectory: fileAndDir second
 				withProjectView: nil.
 		]
