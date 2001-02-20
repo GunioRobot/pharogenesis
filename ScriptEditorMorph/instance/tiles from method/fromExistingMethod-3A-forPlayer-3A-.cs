46dcbd15c07438c490cf7bd41568535f86f2afdb
sw <unknown>
2001-02-20 02:03:00 +0000

    no message

--- ScriptEditorMorph/instance/tiles from method/fromExistingMethod-3A-forPlayer-3A-.cs
+++ ScriptEditorMorph/instance/tiles from method/fromExistingMethod-3A-forPlayer-3A-.cs
@@ -1,12 +1,7 @@
 fromExistingMethod: aSelector forPlayer: aPlayer 
 	"Create tiles for this method.  "
 
-	| parseTree |
 	self initialize.
 	playerScripted _ aPlayer.
 	self setMorph: aPlayer costume scriptName: aSelector.
-	parseTree _ (Decompiler new) decompile: aSelector
-		in: aPlayer class 
-		method: (aPlayer class compiledMethodAt: aSelector).
-	self tilesFrom: parseTree.
-	self install.
+	self insertUniversalTiles
