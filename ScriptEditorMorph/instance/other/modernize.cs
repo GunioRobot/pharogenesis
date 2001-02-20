1f1b1e63c4ba18bdbcfe669a2e22a5586f6f34a2
sw <unknown>
2001-02-20 03:30:00 +0000

    no message

--- ScriptEditorMorph/instance/other/modernize.cs
+++ ScriptEditorMorph/instance/other/modernize.cs
@@ -1,6 +1,7 @@
 modernize
 	"If the receiver appears to date from the past, try to fix it up"
 	
-	(self isTextuallyCoded and: [self showingMethodPane not]) ifTrue:
-		["Fix up old guys that  are not showing the code in place"
-		self showSourceInScriptor]
+	Preferences universalTiles ifFalse:
+		[(self isTextuallyCoded and: [self showingMethodPane not]) ifTrue:
+			["Fix up old guys that  are not showing the code in place"
+			self showSourceInScriptor]]
