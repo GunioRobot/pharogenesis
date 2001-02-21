2c104a9634c3598f9fcb30dd9f7a7fa27e61e756
tk <unknown>
2001-02-21 20:07:00 +0000

    no message

---
+++ TileMorph/instance/change reporting/colorChangedForSubmorph-3A-.cs
@@ -0,0 +1,5 @@
+colorChangedForSubmorph: aSubmorph
+	"Invoked when the user selects a new color on a colorTile or a color-seer-tile; need to recompile the script."
+	self acceptNewLiteral
+	owner ifNil: [^ self].
+	owner isTileLike ifFalse: [owner colorChangedForSubmorph: aSubmorph].
