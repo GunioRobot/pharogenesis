ac5aefa150953a5e1a45efc3cf5cac86046bbbec
sw <unknown>
2001-02-22 06:37:00 +0000

    no message

---
+++ CodeHolder/instance/misc/suggestCategoryToSpawnedBrowser-3A-.cs
@@ -0,0 +1,4 @@
+suggestCategoryToSpawnedBrowser: aBrowser
+	"aBrowser is a message-category browser being spawned from the receiver.  Tell it what it needs to know to get its category info properly set up."
+
+	aBrowser setOriginalCategoryIndexForCurrentMethod
