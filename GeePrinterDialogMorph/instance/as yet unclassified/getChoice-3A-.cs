0f887f5fbbd26e39bd57a3885412ea89e555dbe0
RAA <unknown>
2001-02-22 07:36:00 +0000

    no message

--- GeePrinterDialogMorph/instance/as yet unclassified/getChoice-3A-.cs
+++ GeePrinterDialogMorph/instance/as yet unclassified/getChoice-3A-.cs
@@ -2,3 +2,4 @@ getChoice: aSymbol
 
 	aSymbol == #landscapeFlag ifTrue: [^printSpecs landscapeFlag].
 	aSymbol == #drawAsBitmapFlag ifTrue: [^printSpecs drawAsBitmapFlag].
+	aSymbol == #scaleToFitPage ifTrue: [^printSpecs scaleToFitPage].
