d4b80267fcac4221b98e78cc3060d6898adb3529
RAA <unknown>
2001-02-22 13:56:00 +0000

    no message

--- CascadeNode/instance/tiles/asMorphicSyntaxIn-3A-.cs
+++ CascadeNode/instance/tiles/asMorphicSyntaxIn-3A-.cs
@@ -1,13 +1,6 @@
 asMorphicSyntaxIn: parent
-	| row |
 
-	row _ parent addRow: #cascade on: self.
-	receiver asMorphicSyntaxIn: row.
-	messages do: [:m | m asMorphicSyntaxIn: row].
-	^ row
-
-"	(node2 _ self copy) receiver: nil messages: messages.
-	cascadeMorph _ row addColumn: #cascade2 on: node2.
-	messages do: [ :m | m asMorphicSyntaxIn: cascadeMorph].
-	^row
-"
+	^parent
+		cascadeNode: self 
+		receiver: receiver 
+		messages: messages
