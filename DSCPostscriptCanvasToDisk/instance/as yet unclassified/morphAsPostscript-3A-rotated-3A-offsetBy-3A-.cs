a4bedf6bfdb55625232b93c0a6399b382d410fac
RAA <unknown>
2001-02-22 07:41:00 +0000

    no message

--- DSCPostscriptCanvasToDisk/instance/as yet unclassified/morphAsPostscript-3A-rotated-3A-offsetBy-3A-.cs
+++ DSCPostscriptCanvasToDisk/instance/as yet unclassified/morphAsPostscript-3A-rotated-3A-offsetBy-3A-.cs
@@ -1,10 +1,3 @@
 morphAsPostscript: aMorph rotated: rotateFlag offsetBy: offset
 
-	self reset.
-	psBounds _ offset extent: aMorph bounds extent.
-	topLevelMorph _ aMorph.
-	self writeHeaderRotated: rotateFlag.
-	self fullDrawMorph: aMorph.
-
-	^self close
-
+	^self morphAsPostscript: aMorph rotated: rotateFlag offsetBy: offset specs: nil
