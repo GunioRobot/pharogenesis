9863012b5290399677fa8ca52c31d941ee5629c7
ar <unknown>
2001-02-23 21:14:00 +0000

    no message

--- WorldState/instance/stepping/cleanseStepListForWorld-3A-.cs
+++ WorldState/instance/stepping/cleanseStepListForWorld-3A-.cs
@@ -2,6 +2,7 @@ cleanseStepListForWorld: aWorld
 	"Remove morphs from the step list that are not in this World.  Often were in a flap that has moved on to another world."
 
 	| deletions morphToStep |
+	deletions _ nil.
 	stepList do: [:entry |
 		morphToStep _ entry receiver.
 		morphToStep world == aWorld ifFalse:[
@@ -10,5 +11,9 @@ cleanseStepListForWorld: aWorld
 
 	deletions ifNotNil:[
 		deletions do: [:entry|
-			entry receiver stopStepping]].
+			self stopStepping: entry receiver]].
 
+	self alarms copy do:[:entry|
+		morphToStep _ entry receiver.
+		(morphToStep isMorph and:[morphToStep world == aWorld]) 
+			ifFalse:[self removeAlarm: entry selector for: entry receiver]].
