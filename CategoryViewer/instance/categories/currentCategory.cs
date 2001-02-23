b25b7deb903483d8f404553aef15c7ca3ec3e1d2
sw <unknown>
2001-02-23 22:29:00 +0000

    no message

--- CategoryViewer/instance/categories/currentCategory.cs
+++ CategoryViewer/instance/categories/currentCategory.cs
@@ -1,9 +1,6 @@
 currentCategory
 	"Answer the symbol representing the receiver's currently-selected category"
 
-	| current actualPane |
-	actualPane _ namePane renderedMorph.
-	current _ (actualPane isKindOf: StringMorph)
-				ifTrue:	[actualPane contents]
-				ifFalse:	[actualPane firstSubmorph contents].
+	| current |
+	current _ namePane renderedMorph firstSubmorph contents.
 	^ current ifNotNil: [current asSymbol] ifNil: [#basic]
