279f14e7d2a8f161fa628fce2b74ce7ccdda1ee8
nk <unknown>
2001-02-24 13:50:00 +0000

    no message

---
+++ ArchiveViewer/instance/member operations/commentMember.cs
@@ -0,0 +1,10 @@
+commentMember
+	| newName |
+	newName _ FillInTheBlankMorph
+			request: 'New comment for member:'
+			initialAnswer: self selectedMember fileComment
+			centerAt: Sensor cursorPoint
+			inWorld: self world
+			onCancelReturn: self selectedMember fileComment
+			acceptOnCR: true.
+	self selectedMember fileComment: newName.
