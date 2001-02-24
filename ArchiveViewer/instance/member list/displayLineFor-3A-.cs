c9625ad75bd4fe3ded86e281d3b081ce08e23380
nk <unknown>
2001-02-24 22:32:00 +0000

    no message

---
+++ ArchiveViewer/instance/member list/displayLineFor-3A-.cs
@@ -0,0 +1,17 @@
+displayLineFor: aMember
+	| stream dateTime |
+	stream _ WriteStream on: (String new: 60).
+	dateTime _ Time dateAndTimeFromSeconds: aMember lastModTime. 
+	stream
+		nextPutAll: (aMember uncompressedSize printString padded: #left to: 8 with: $  );
+		space;
+		nextPutAll: (aMember compressedSize printString padded: #left to: 8 with: $  );
+		space; space;
+		nextPutAll: (aMember crc32String );
+		space; space.
+	dateTime first printOn: stream format: #(3 2 1 $- 2 1 2).
+	stream space.
+	dateTime second print24: true showSeconds: false on: stream.
+	stream space; space;
+		nextPutAll: (aMember fileName ).
+	^stream contents
