d3e3a2b6d949544e37ca656294f27f976e36787b
ar <unknown>
2001-02-24 19:43:00 +0000

    no message

--- ZipWriteStream/instance/initialize-2D-release/on-3A-.cs
+++ ZipWriteStream/instance/initialize-2D-release/on-3A-.cs
@@ -1,5 +1,9 @@
 on: aCollectionOrStream
+	crc _ 16rFFFFFFFF.
+	crcPosition _ 1.
+	bytesWritten _ 0.
 	encoder _ ZipEncoder on: aCollectionOrStream.
 	encoder isBinary
 		ifTrue:[super on: ByteArray new]
-		ifFalse:[super on: String new]
+		ifFalse:[super on: String new].
+	self writeHeader.
