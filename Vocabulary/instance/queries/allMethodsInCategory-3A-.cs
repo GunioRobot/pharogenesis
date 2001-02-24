ac65afd479c21c8b61728e5a0840aa9a35b59df5
sw <unknown>
2001-02-24 01:02:00 +0000

    no message

---
+++ Vocabulary/instance/queries/allMethodsInCategory-3A-.cs
@@ -0,0 +1,4 @@
+allMethodsInCategory: categoryName 
+	"Answer a list of methods in the category of the given name"
+
+	^ self allMethodsInCategory: categoryName forInstance: object ofClass: object class
