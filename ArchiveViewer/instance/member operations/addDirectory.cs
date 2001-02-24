b725b12256badf3ac47b1b59f8214140e103a673
nk <unknown>
2001-02-24 23:28:00 +0000

    no message

---
+++ ArchiveViewer/instance/member operations/addDirectory.cs
@@ -0,0 +1,9 @@
+addDirectory
+	| directory |
+	self canAddMember ifFalse: [ ^self ].
+	directory _ FileList2 modalFolderSelector.
+	directory
+		ifNil: [^ self].
+	archive addTree: directory removingFirstCharacters: directory pathName size + 1.
+	self memberIndex: 0.
+	self changed: #memberList.
