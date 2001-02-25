68645fdb363ace96bc6af5625bf641396502e748
tk <unknown>
2001-02-25 17:53:00 +0000

    no message

---
+++ Command/instance/copying/veryDeepInner-3A-.cs
@@ -0,0 +1,6 @@
+veryDeepInner: deepCopier
+	"ALL fields are weakly copied!  Can't duplicate an object by duplicating a Command that involves it.  See DeepCopier."
+
+	super veryDeepInner: deepCopier.
+	"just keep old pointers to all fields"
+	parameters _ parameters.
