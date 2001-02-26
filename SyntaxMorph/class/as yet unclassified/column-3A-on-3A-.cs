1417c3aca98a9015c4587821405c2aee9229f6eb
RAA <unknown>
2001-02-26 22:59:00 +0000

    no message

--- SyntaxMorph/class/as yet unclassified/column-3A-on-3A-.cs
+++ SyntaxMorph/class/as yet unclassified/column-3A-on-3A-.cs
@@ -4,12 +4,12 @@ column: aColor on: aParseNode
 	color _ self translateColor: aColor.
 	(c _ self newColumn)
 		parseNode: aParseNode;
-		layoutInset: self standardInset;
+		layoutInset: c standardInset;
 		hResizing: #shrinkWrap;
 		vResizing: #shrinkWrap;
 		color: color;
 		borderWidth: 1;
 		borderColor: c stdBorderColor;
 		wrapCentering: #topLeft;
-		cellPositioning: #topLeft.
+		cellPositioning: c standardCellPositioning.
 	^c
