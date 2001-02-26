cad1ff2ef067c2d7208cc5bb3e5779fb2c1239fa
ar <unknown>
2001-02-26 15:59:00 +0000

    no message

--- StandardFileStream/instance/browser requests/waitBrowserReadyFor-3A-ifFail-3A-.cs
+++ StandardFileStream/instance/browser requests/waitBrowserReadyFor-3A-ifFail-3A-.cs
@@ -1,12 +1,14 @@
 waitBrowserReadyFor: timeout ifFail: errorBlock
-	| startTime delay |
-	self primBrowserReady
-		ifTrue: [^true].
+	| startTime delay okay |
+	okay _ self primBrowserReady.
+	okay ifNil:[^errorBlock value].
+	okay ifTrue: [^true].
 	startTime _ Time millisecondClockValue.
 	delay _ Delay forMilliseconds: 100.
 	[(Time millisecondsSince: startTime) < timeout]
 		whileTrue: [
 			delay wait.
-			self primBrowserReady
-				ifTrue: [^true]].
+			okay _ self primBrowserReady.
+			okay ifNil:[^errorBlock value].
+			okay ifTrue: [^true]].
 	^errorBlock value
