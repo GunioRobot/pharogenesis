d41e6f79079ae17fe8237ff2fe0887e46da98211
RAA <unknown>
2001-02-26 13:45:00 +0000

    no message

---
+++ SyntaxMorph/instance/alans styles/translateToWordySelfVariant-3A-.cs
@@ -0,0 +1,11 @@
+translateToWordySelfVariant: aString
+
+	| lc |
+	lc _ aString asLowercase.
+	lc = 'me' ifTrue: [^#selfWrittenAsMe].
+	lc = 'my' ifTrue: [^#selfWrittenAsMy].
+	lc = 'i''ll' ifTrue: [^#selfWrittenAsIll].
+	lc = 'i''m' ifTrue: [^#selfWrittenAsIm].
+	lc = 'this' ifTrue: [^#selfWrittenAsThis].
+	^nil
+
