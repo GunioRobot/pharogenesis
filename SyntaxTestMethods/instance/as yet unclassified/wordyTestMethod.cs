7120c82acca9a8d14d6e7aadb39bf814ae273ab3
RAA <unknown>
2001-02-26 08:23:00 +0000

    no message

---
+++ SyntaxTestMethods/instance/as yet unclassified/wordyTestMethod.cs
@@ -0,0 +1,8 @@
+wordyTestMethod
+
+	self selfWrittenAsMe = 1 ifTrue: [
+		self selfWrittenAsMy size.
+		self selfWrittenAsIll stop.
+		self selfWrittenAsIm large.
+		self selfWrittenAsThis helps.
+	].
