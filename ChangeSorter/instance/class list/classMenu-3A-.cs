a4730cce81b755ff9cd1bf601468b4338726bb28
sw <unknown>
2001-02-26 12:00:00 +0000

    no message

--- ChangeSorter/instance/class list/classMenu-3A-.cs
+++ ChangeSorter/instance/class list/classMenu-3A-.cs
@@ -1,4 +1,4 @@
 classMenu: aMenu
 	"Set up aMenu for the class-list.  Retained for backward compatibility with old change sorters in image segments"
 
-	^ self classMenu: aMenu shifted: false
+	^ self classListMenu: aMenu shifted: false
