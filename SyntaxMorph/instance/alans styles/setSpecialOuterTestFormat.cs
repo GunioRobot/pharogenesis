21d2331d9512a6e50dca6ac1ce910ab08c2c2352
RAA <unknown>
2001-02-27 07:34:00 +0000

    no message

---
+++ SyntaxMorph/instance/alans styles/setSpecialOuterTestFormat.cs
@@ -0,0 +1,8 @@
+setSpecialOuterTestFormat
+
+	self 
+		specialColor: self darkerColor 
+		andBorder: self lighterColor.
+	self useRoundedCorners.
+	self layoutInset: 1.
+	"self setProperty: #variableInsetSize toValue: 6."
