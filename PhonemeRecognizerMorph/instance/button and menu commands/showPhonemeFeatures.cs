c80628cad93690c306a3425778b58d1208d1deb0
dns <unknown>
2001-02-28 14:04:00 +0000

    no message

--- PhonemeRecognizerMorph/instance/button and menu commands/showPhonemeFeatures.cs
+++ PhonemeRecognizerMorph/instance/button and menu commands/showPhonemeFeatures.cs
@@ -2,7 +2,7 @@ showPhonemeFeatures
 	"Show a graph of the features array for the phoneme selected by the user."
 
 	| phoneme m |
-	phoneme _ self selectPhonemeFromMenu.
+	phoneme _ self selectPhonemeFromMenu: 'Show Features'.
 	phoneme ifNotNil: [
 		m _ ImageMorph new image: phoneme featuresGraph.
 		self world firstHand attachMorph: m].
