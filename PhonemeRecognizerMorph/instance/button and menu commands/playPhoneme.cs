1e655588079da5c4290deb1cc7de12b76afc07ad
dns <unknown>
2001-02-28 14:03:00 +0000

    no message

--- PhonemeRecognizerMorph/instance/button and menu commands/playPhoneme.cs
+++ PhonemeRecognizerMorph/instance/button and menu commands/playPhoneme.cs
@@ -2,5 +2,5 @@ playPhoneme
 	"Play a phoneme specified by the user."
 
 	| phoneme |
-	phoneme _ self selectPhonemeFromMenu.
+	phoneme _ self selectPhonemeFromMenu: 'Phoneme to play'.
 	phoneme ifNotNil: [phoneme play].
