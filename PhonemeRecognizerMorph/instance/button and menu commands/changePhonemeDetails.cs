8d2bdf87f32720674c9f3335f7b03543162b8387
dns <unknown>
2001-02-28 14:02:00 +0000

    no message

--- PhonemeRecognizerMorph/instance/button and menu commands/changePhonemeDetails.cs
+++ PhonemeRecognizerMorph/instance/button and menu commands/changePhonemeDetails.cs
@@ -2,5 +2,5 @@ changePhonemeDetails
 	"Change the name and mouth position index of a phoneme specified by the user."
 
 	| phoneme |
-	phoneme _ self selectPhonemeFromMenu.
+	phoneme _ self selectPhonemeFromMenu: 'Phoneme to rename'.
 	phoneme ifNotNil: [self promptForDetailsOfPhoneme: phoneme].
