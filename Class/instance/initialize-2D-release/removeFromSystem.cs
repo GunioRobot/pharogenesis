eb1abac06ef445813aa3c2e5576fd225da994f76
ar <unknown>
2001-03-01 22:28:00 +0000

    no message

--- Class/instance/initialize-2D-release/removeFromSystem.cs
+++ Class/instance/initialize-2D-release/removeFromSystem.cs
@@ -1,6 +1,4 @@
 removeFromSystem
 	"Forget the receiver from the Smalltalk global dictionary. Any existing 
 	instances will refer to an obsolete version of the receiver."
-
-	self environment removeClassFromSystem: self.
-	self obsolete
+	self removeFromSystem: true.
