7042dd3d0430c6dc7f010492f20cbb9b7a2cc626
ar <unknown>
2001-03-02 18:16:00 +0000

    no message

---
+++ ResourceManager/instance/loading/prioritizedUnloadedResources.cs
@@ -0,0 +1,7 @@
+prioritizedUnloadedResources
+	"Return an array of unloaded resource locators prioritized by some means"
+	| list |
+	list _ unloaded asArray.
+	^list sort:[:l1 :l2|
+		(l1 resourceFileSize ifNil:[SmallInteger maxVal]) <=
+			(l2 resourceFileSize ifNil:[SmallInteger maxVal])]
