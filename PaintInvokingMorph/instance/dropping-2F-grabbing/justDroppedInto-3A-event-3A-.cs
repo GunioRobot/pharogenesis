69999768dce3edce518ef2317f9209e844c2a479
ar <unknown>
2001-03-03 20:41:00 +0000

    no message

---
+++ PaintInvokingMorph/instance/dropping-2F-grabbing/justDroppedInto-3A-event-3A-.cs
@@ -0,0 +1,6 @@
+justDroppedInto: aPasteUpMorph event: anEvent
+	"This message is sent to a dropped morph after it has been dropped on--and been accepted by--a drop-sensitive morph"
+	aPasteUpMorph isPartsBin ifFalse:[
+		self delete.
+		^aPasteUpMorph makeNewDrawing: anEvent].
+	^super justDroppedInto: aPasteUpMorph event: anEvent
