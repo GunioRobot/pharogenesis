1e589c08e2f432e25554551e61fa8ec9910553d8
ar <unknown>
2001-03-04 22:18:00 +0000

    no message

--- JPEGReadWriter/instance/colorspace conversion/colorConvertIntYCbCrMCU.cs
+++ JPEGReadWriter/instance/colorspace conversion/colorConvertIntYCbCrMCU.cs
@@ -13,27 +13,27 @@ colorConvertIntYCbCrMCU
 		y _ ySampleStream nextSample.
 		cb _ cbSampleStream nextSample - SampleOffset.
 		cr _ crSampleStream nextSample - SampleOffset.
-		red _ y + ((FIXn1n40200 * cr) // 65536) + redResidual.
+		red _ y + ((FIXn1n40200 * cr) // 65536) + (residuals at: 1).
 		red > MaxSample
 			ifTrue: [red _ MaxSample]
 			ifFalse: [red < 0 ifTrue: [red _ 0]].
-		redResidual _ red bitAnd: ditherMask.
+		residuals at: 1 put: (red bitAnd: ditherMask).
 		red _ red bitAnd: MaxSample - ditherMask.
 		red < 1 ifTrue: [red _ 1].
 		green _ y - ((FIXn0n34414 * cb) // 65536) -
-			((FIXn0n71414 * cr) // 65536) + greenResidual.
+			((FIXn0n71414 * cr) // 65536) + (residuals at: 2).
 		green > MaxSample
 			ifTrue: [green _ MaxSample]
 			ifFalse: [green < 0 ifTrue: [green _ 0]].
-		greenResidual _ green bitAnd: ditherMask.
+		residuals at: 2 put: (green bitAnd: ditherMask).
 		green _ green bitAnd: MaxSample - ditherMask.
 		green < 1 ifTrue: [green _ 1].
-		blue _ y + ((FIXn1n77200 * cb) // 65536) + blueResidual.
+		blue _ y + ((FIXn1n77200 * cb) // 65536) + (residuals at: 3).
 		blue > MaxSample
 			ifTrue: [blue _ MaxSample]
 			ifFalse: [blue < 0 ifTrue: [blue _ 0]].
-		blueResidual _ blue bitAnd: ditherMask.
+		residuals at: 3 put: (blue bitAnd: ditherMask).
 		blue _ blue bitAnd: MaxSample - ditherMask.
 		blue < 1 ifTrue: [blue _ 1].
-		bits at: i put: (red bitShift: 16) + (green bitShift: 8) + blue].
+		bits at: i put: 16rFF000000 + (red bitShift: 16) + (green bitShift: 8) + blue].
 	
