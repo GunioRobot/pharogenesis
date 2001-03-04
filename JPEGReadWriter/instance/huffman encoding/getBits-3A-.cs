afa19a06043a346d559580e81c3e2d75a1285e60
ar <unknown>
2001-03-04 17:27:00 +0000

    no message

--- JPEGReadWriter/instance/huffman encoding/getBits-3A-.cs
+++ JPEGReadWriter/instance/huffman encoding/getBits-3A-.cs
@@ -1,14 +1,2 @@
 getBits: requestedBits
-
-	| value |
-	requestedBits > bitsInBuffer
-		ifTrue:
-			[self fillBuffer.
-			requestedBits > bitsInBuffer
-				ifTrue:
-					[self error: 'not enough bits available to decode']].
-	value _ bitBuffer >> (bitsInBuffer - requestedBits).
-	bitBuffer _ bitBuffer bitAnd: (1 << (bitsInBuffer - requestedBits) -1).
-	bitsInBuffer _ bitsInBuffer - requestedBits.
-	^ value
-		
+	^stream getBits: requestedBits
