b3ef5978802c196d78aa847e432a456ea2fa50b6
di <unknown>
2001-03-04 22:45:00 +0000

    no message

--- ImageSegmentRootStub/instance/fetch from disk/doesNotUnderstand-3A-.cs
+++ ImageSegmentRootStub/instance/fetch from disk/doesNotUnderstand-3A-.cs
@@ -1,11 +1,14 @@
 doesNotUnderstand: aMessage 
-	 "Any normal message sent to this object is really intended for another object that is in a non-resident imageSegment.  Reinstall the segment and resend the message."
+	 | segmentName |
+"Any normal message sent to this object is really intended for another object that is in a non-resident imageSegment.  Reinstall the segment and resend the message."
 
+	segmentName _ imageSegment segmentName.
 	imageSegment install.
 	LoggingFaults ifTrue:		"Save the stack printout to show who caused the fault"
 		[FaultLogs at: Time millisecondClockValue printString
 			put: (String streamContents:
 				[:strm | 
+				strm nextPutAll: segmentName; cr.
 				strm print: self class; space; print: aMessage selector; cr.
 				(thisContext sender stackOfSize: 30)
 					do: [:item | strm print: item; cr]])].
