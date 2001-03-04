73e0c74a80ed465fdd65a8132b298c21555ca752
ar <unknown>
2001-03-04 18:38:00 +0000

    no message

--- JPEGReadWriter/instance/marker parsing/initialSOSSetup.cs
+++ JPEGReadWriter/instance/marker parsing/initialSOSSetup.cs
@@ -2,10 +2,6 @@ initialSOSSetup
 
 	mcuWidth _ (components detectMax: [:c | c widthInBlocks]) widthInBlocks.
 	mcuHeight _ (components detectMax: [:c | c heightInBlocks]) heightInBlocks.
-	components do:
-		[:c |
+	components do:[:c |
 		c mcuWidth: mcuWidth mcuHeight: mcuHeight dctSize: DCTSize].
-	hACTable , hDCTable do: [:t | t ifNotNil: [t makeDerivedTables]].
-	bitBuffer _ 0.
-	bitsInBuffer _ 0.
-	lookahead _ JPEGHuffmanTable lookahead.
+	stream resetBitBuffer.
