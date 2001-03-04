f63f43f33a8da35d92e828add0af7b4986c7365a
RAA <unknown>
2001-03-04 12:19:00 +0000

    no message

---
+++ SyntaxMorph/instance/printing/printBlockArgsNodeOn-3A-indent-3A-.cs
@@ -0,0 +1,18 @@
+printBlockArgsNodeOn: strm indent: level
+
+	| argString |
+
+	self
+		submorphsDoIfSyntax: [ :sub |
+			(argString _ sub decompile) isEmpty ifFalse: [
+				strm 
+					nextPut: $:;
+					nextPutAll: argString;
+					space
+			].
+		] 
+		ifString: [ :sub |
+			"self printSimpleStringMorph: sub on: strm	<<<< do we need this??"
+		].
+	strm nextPut: $|; crtab: level.
+
