258aa71f60799eafdbab1eea3d04be066ad0012c
sw <unknown>
2001-03-05 18:04:00 +0000

    no message

--- Browser/instance/class functions/removeClass.cs
+++ Browser/instance/class functions/removeClass.cs
@@ -1,17 +1,5 @@
 removeClass
-	"The selected class should be removed from the system. Use a Confirmer 
-	to make certain the user intends this irrevocable command to be carried 
-	out."
-	| message class className |
-	classListIndex = 0 ifTrue: [^ self].
-	self okToChange ifFalse: [^ self].
-	class _ self selectedClass.
-	className _ class name.
-	message _ 'Are you certain that you
-want to delete the class ', className, '?'.
-	(self confirm: message) ifTrue: 
-			[class subclasses size > 0
-				ifTrue: [self notify: 'class has subclasses: ' , message].
-			class removeFromSystem.
-			self classListIndex: 0].
-	self changed: #classList.
+	"If the user confirms the wish to delete the class, do so"
+
+	super removeClass ifTrue:
+		[self classListIndex: 0]
