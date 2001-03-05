5e1f8759e60042655e1fc498cb70c053f094dfce
ar <unknown>
2001-03-05 12:00:00 +0000

    no message

--- ClassBuilder/instance/private/showProgressFor-3A-.cs
+++ ClassBuilder/instance/private/showProgressFor-3A-.cs
@@ -1,6 +1,7 @@
 showProgressFor: aClass
 	"Announce that we're processing aClass"
 	progress == nil ifTrue:[^self].
+	aClass isObsolete ifTrue:[^self].
 	currentClassIndex _ currentClassIndex + 1.
 	(aClass hasMethods and: [aClass wantsRecompilationProgressReported]) ifTrue:
 		[progress value: ('Recompiling ', aClass name,' (', currentClassIndex printString,'/', maxClassIndex printString,')')]
