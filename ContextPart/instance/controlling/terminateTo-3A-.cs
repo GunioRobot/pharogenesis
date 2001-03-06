fc74753da951ba433513472981cbb53fc47a24d2
ar <unknown>
2001-03-06 14:26:00 +0000

    no message

--- ContextPart/instance/controlling/terminateTo-3A-.cs
+++ ContextPart/instance/controlling/terminateTo-3A-.cs
@@ -2,7 +2,7 @@ terminateTo: previousContext
 	"Terminate all the Contexts between me and previousContext, if previousContext is on my Context stack. Make previousContext my sender."
 
 	| currentContext sendingContext |
-
+	<primitive: 196>
 	(self hasSender: previousContext) ifTrue: [
 		currentContext _ sender.
 		[currentContext == previousContext] whileFalse: [
