1915f99b6889fbf882c4d1710a7e35f6e90d0117
ar <unknown>
2001-03-06 15:02:00 +0000

    no message

--- MethodContext/instance/controlling/answer-3A-.cs
+++ MethodContext/instance/controlling/answer-3A-.cs
@@ -1,6 +1,6 @@
 answer: anObject
+	"ar 3/6/2001: OBSOLETE. Must not be used. Will be removed VERY SOON."
 	"Modify my code, from the current program counter value, to answer anObject."
-
 	self push: anObject.
 	(method at: pc) = 124 ifFalse: [
 		method _ (
