f98d9db957147a0b5b82353e0cc90992f57bf7c4
RAA <unknown>
2001-03-07 22:31:00 +0000

    no message

---
+++ EToySenderMorph/instance/layout/acceptDroppingMorph-3A-event-3A-.cs
@@ -0,0 +1,18 @@
+acceptDroppingMorph: morphToDrop event: evt
+
+	| myCopy outData |
+
+	(morphToDrop isKindOf: NewHandleMorph) ifTrue: [			"don't send these"
+		^morphToDrop rejectDropMorphEvent: evt.
+	].
+	self eToyRejectDropMorph: morphToDrop event: evt.		"we don't really want it"
+
+	"7 mar 2001 - remove #veryDeepCopy"
+	myCopy _ morphToDrop.	"gradient fills require doing this second"
+	myCopy setProperty: #positionInOriginatingWorld toValue: morphToDrop position.
+	self stopFlashing.
+
+	outData _ myCopy eToyStreamedRepresentationNotifying: self.
+	self resetIndicator: #working.
+	self transmitStreamedObject: outData to: self ipAddress.
+
