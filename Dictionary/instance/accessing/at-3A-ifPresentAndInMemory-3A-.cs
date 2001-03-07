84d3b8b04e103cb51edc34943d48cae32a8b11a9
di <unknown>
2001-03-07 15:29:00 +0000

    no message

---
+++ Dictionary/instance/accessing/at-3A-ifPresentAndInMemory-3A-.cs
@@ -0,0 +1,7 @@
+at: key ifPresentAndInMemory: aBlock
+	"Lookup the given key in the receiver. If it is present, answer the value of evaluating the given block with the value associated with the key. Otherwise, answer nil."
+
+	| v |
+	v _ self at: key ifAbsent: [^ nil].
+	v isInMemory ifFalse: [^ nil].
+	^ aBlock value: v
