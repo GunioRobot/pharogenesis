e9a08d79d7730ed9ec23f81811195f52a49b14b8
tk <unknown>
2001-03-07 15:46:00 +0000

    no message

--- DeepCopier/instance/like fullCopy/warnIverNotCopiedIn-3A-sel-3A-.cs
+++ DeepCopier/instance/like fullCopy/warnIverNotCopiedIn-3A-sel-3A-.cs
@@ -1,5 +1,5 @@
 warnIverNotCopiedIn: aClass sel: sel
 	"Warn the user to update veryDeepCopyWith: or veryDeepInner:"
 
-	self inform: ('Your file operation succeeded.\\An instance variable was added to to class ', aClass name, ',\and it is not copied in the method ', sel, '.\Please rewrite it to handle all instance variables.\See DeepCopier class comment.') withCRs.
+	self inform: ('An instance variable was added to to class ', aClass name, ',\and it is not copied in the method ', sel, '.\Please rewrite it to handle all instance variables.\See DeepCopier class comment.') withCRs.
 	Browser openMessageBrowserForClass: aClass selector: sel editString: nil.
