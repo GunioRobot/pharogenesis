be7b8482f2f94fd1bc1d3b5c6f43eb2a7793f28c
nk <unknown>
2001-03-08 13:25:00 +0000

    no message

--- ProcessBrowser/instance/process actions/terminateProcess.cs
+++ ProcessBrowser/instance/process actions/terminateProcess.cs
@@ -4,9 +4,5 @@ terminateProcess
 	nameAndRules second
 		ifFalse: [PopUpMenu inform: 'Nope, won''t kill ' , nameAndRules first.
 			^ self].
-	selectedProcess
-		ifNotNil: [self class suspendedProcesses
-		removeKey: selectedProcess
-		ifAbsent: [].selectedProcess terminate].
-	
+	self class terminateProcess: selectedProcess.	
 	self updateProcessList
