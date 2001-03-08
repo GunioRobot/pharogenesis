75f0ca77bbe01f18644c586450e3d525e34d3b83
nk <unknown>
2001-03-08 17:23:00 +0000

    no message

---
+++ CPUWatcher/instance/porcine capture/openMorphicWindowForSuspendedProcess-3A-.cs
@@ -0,0 +1,27 @@
+openMorphicWindowForSuspendedProcess: aProcess
+	| menu rules |
+	menu _ MenuMorph new.
+	"nickname  allow-stop  allow-debug"
+	rules _ ProcessBrowser nameAndRulesFor: aProcess.
+	menu add: 'Dismiss this menu' target: menu selector: #delete; addLine.
+	menu add: 'Open Process Browser' target: ProcessBrowser selector: #open.
+	menu add: 'Resume'
+		target: self
+		selector: #resumeProcess:fromMenu:
+		argumentList: { aProcess . menu }.
+	menu add: 'Terminate'
+		target: self
+		selector: #terminateProcess:fromMenu:
+		argumentList: { aProcess . menu }.
+	rules third ifTrue: [
+		menu add: 'Debug at a lower priority'
+			target: self
+			selector: #debugProcess:fromMenu:
+			argumentList: { aProcess . menu }.
+	].
+	menu addTitle: aProcess identityHash asString,
+		' ', rules first,
+		' is taking too much time and has been suspended.
+What do you want to do with it?'.
+	menu stayUp: true.
+	menu popUpInWorld
