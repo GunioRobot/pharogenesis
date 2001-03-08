05ea2a7a09c57f22d843c457f91f6c1ed956df61
nk <unknown>
2001-03-08 13:23:00 +0000

    no message

--- ProcessBrowser/instance/process actions/resumeProcess.cs
+++ ProcessBrowser/instance/process actions/resumeProcess.cs
@@ -1,10 +1,5 @@
 resumeProcess
-	| priority |
 	selectedProcess
 		ifNil: [^ self].
-	priority _ self class suspendedProcesses
-				removeKey: selectedProcess
-				ifAbsent: [selectedProcess priority].
-	selectedProcess priority: priority.
-	selectedProcess resume.
+	self class resumeProcess: selectedProcess.
 	self updateProcessList
