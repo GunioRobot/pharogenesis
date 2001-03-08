17e2a228bd8ae32c0d36a787dc96a614f1923b0b
nk <unknown>
2001-03-08 13:21:00 +0000

    no message

--- ProcessBrowser/instance/process actions/suspendProcess.cs
+++ ProcessBrowser/instance/process actions/suspendProcess.cs
@@ -1,19 +1,10 @@
 suspendProcess
-	| nameAndRules priority |
+	| nameAndRules |
 	selectedProcess isSuspended
 		ifTrue: [^ self].
 	nameAndRules _ self nameAndRulesForSelectedProcess.
 	nameAndRules second
 		ifFalse: [PopUpMenu inform: 'Nope, won''t suspend ' , nameAndRules first.
 			^ self].
-	priority _ selectedProcess priority.
-	self class suspendedProcesses at: selectedProcess put: priority.
-	"Need to take the priority down below mine so that I can keep control after signaling the Semaphore"
-	(selectedProcess suspendingList isKindOf: Semaphore)
-		ifTrue: [selectedProcess priority: 1.
-			selectedProcess suspendingList signal].
-	[selectedProcess suspend]
-		on: Error
-		do: [:ex | self class suspendedProcesses removeKey: selectedProcess].
-	selectedProcess priority: priority.
+	self class suspendProcess: selectedProcess.
 	self updateProcessList
