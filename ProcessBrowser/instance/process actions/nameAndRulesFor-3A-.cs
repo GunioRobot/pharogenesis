e20b61858da6b7048cc18f2dc40584180b927945
nk <unknown>
2001-03-08 13:35:00 +0000

    no message

--- ProcessBrowser/instance/process actions/nameAndRulesFor-3A-.cs
+++ ProcessBrowser/instance/process actions/nameAndRulesFor-3A-.cs
@@ -1,17 +1,4 @@
 nameAndRulesFor: aProcess 
 	"Answer a nickname and two flags: allow-stop, and allow-debug"
-	(aProcess priority = Processor timingPriority
-			and: [aProcess suspendedContext receiver == Delay])
-		ifTrue: [^ {'the timer interrupt watcher'. false. false}].
-	^ aProcess caseOf: {
-		[] -> [{'no process'. false. false}].
-		[autoUpdateProcess] -> [{'my auto-update process'. true. true}].
-		[Smalltalk lowSpaceWatcherProcess] -> [{'the low space watcher'. false. false}].
-		[WeakArray runningFinalizationProcess] -> [{'the WeakArray finalization process'. false. false}].
-		[Processor activeProcess] -> [{'the UI process'. false. true}].
-		[Processor backgroundProcess] -> [{'the idle process'. false. false}].
-		[Sensor inputProcess] -> [{'the I/O process'. false. false}].
-		[Sensor interruptWatcherProcess] -> [{'the user interrupt watcher'. false. false}].
-		[Project uiProcess] -> [{'the inactive Morphic UI process'. false. false}].
-		[ScheduledControllers activeControllerProcess] -> [{'the inactive MVC controller process'. false. true}]}
-		 otherwise: [{aProcess suspendedContext asString. true. true}]
+	aProcess == autoUpdateProcess ifTrue: [ ^{'my auto-update process'. true. true} ].
+	^self class nameAndRulesFor: aProcess 
