73ace030065594bb9573fba772fadffce873fdf1
tk <unknown>
2001-03-09 10:42:00 +0000

    no message

--- ScriptEditorMorph/instance/buttons/scriptTitle.cs
+++ ScriptEditorMorph/instance/buttons/scriptTitle.cs
@@ -1,2 +1,8 @@
 scriptTitle
-	^ scriptName
+
+	^ Preferences universalTiles 
+		ifTrue: [SyntaxMorph new substituteKeywordFor: scriptName] 
+				"spaces instead of capitals, no colons"
+				"Don't use property #syntacticallyCorrectContents.  
+				  scriptName here holds the truth"
+		ifFalse: [scriptName].
