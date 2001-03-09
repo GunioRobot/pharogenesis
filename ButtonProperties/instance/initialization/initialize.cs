c1a80b030e058b96aac61b40d42018302e51645f
RAA <unknown>
2001-03-09 09:47:00 +0000

    no message

---
+++ ButtonProperties/instance/initialization/initialize.cs
@@ -0,0 +1,9 @@
+initialize
+
+	wantsRolloverIndicator _ false.
+	delayBetweenFirings _ nil.
+	mouseOverHaloWidth _ 10.
+	mouseOverHaloColor _ Color blue alpha: 0.3.
+	mouseDownHaloWidth _ 15.
+	mouseDownHaloColor _ Color blue alpha: 0.7.
+	arguments _ #().
