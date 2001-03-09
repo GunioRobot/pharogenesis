37b55afe02a111bba1db89b56702f2db4f7bd0e8
di <unknown>
2001-03-09 01:25:00 +0000

    no message

--- ServerDirectory/instance/file directory/oldFileOrNoneNamed-3A-.cs
+++ ServerDirectory/instance/file directory/oldFileOrNoneNamed-3A-.cs
@@ -1,14 +1,11 @@
 oldFileOrNoneNamed: fullName
 	"If the file exists, answer a read-only RemoteFileStream on it. If it doesn't, answer nil.  fullName is directory path, and does include name of the server.  Or just a simple fileName.  Do prefetch the data."
-
+ 
 	| file |
-
-	Cursor wait showWhile: [
-		file _ self asServerFileNamed: fullName.
+	^ Cursor wait showWhile:
+		[file _ self asServerFileNamed: fullName.
 		file readOnly.
-		file isTypeFile ifTrue: [
-			^ FileStream oldFileOrNoneNamed: (file fileNameRelativeTo: self)].
 		"file exists ifFalse: [^ nil]."		"on the server"
-
-		^self streamOnBeginningOf: file
-	].
+		file isTypeFile
+			ifTrue: [FileStream oldFileOrNoneNamed: (file fileNameRelativeTo: self)]
+			ifFalse: [self streamOnBeginningOf: file]]
