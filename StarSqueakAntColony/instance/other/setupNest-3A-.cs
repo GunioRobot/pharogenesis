ad913dcb322662c498b8c9705b025c5283a12a7d
jm <unknown>
2001-03-11 17:11:00 +0000

    no message

---
+++ StarSqueakAntColony/instance/other/setupNest-3A-.cs
@@ -0,0 +1,15 @@
+setupNest: aPatch
+	"Create a nest of radius 5 centered at 50@50."
+
+	| distanceToNest |
+	distanceToNest _ aPatch distanceTo: 50@50.
+	distanceToNest <= 4
+		ifTrue: [
+			aPatch set: 'isNest' to: 1.
+			aPatch color: Color brown lighter]
+		ifFalse: [aPatch set: 'isNest' to: 0].
+
+	"create a 'hill' of nest scent centered on the nest"
+	distanceToNest > 0 ifTrue: [
+		aPatch set: 'nestScent' to: 10000.0 // distanceToNest].
+
