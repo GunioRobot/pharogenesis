1c65a20fc2d130508fdd2f7c301d082d0ff2fea5
nk <unknown>
2001-03-14 07:53:00 +0000

    no message

--- ProcessBrowser/class/instance creation/open.cs
+++ ProcessBrowser/class/instance creation/open.cs
@@ -1,6 +1,7 @@
 open
 	"ProcessBrowser open"
 	"Create and schedule a ProcessBrowser."
+	Smalltalk garbageCollect.
 	^ Smalltalk isMorphic
 		ifTrue: [ self new openAsMorph ]
 		ifFalse: [ self new openAsMVC ]
