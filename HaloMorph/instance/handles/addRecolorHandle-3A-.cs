60d080ec82f6eafbca284e36256f265061da7f2f
RAA <unknown>
2001-03-15 11:24:00 +0000

    no message

--- HaloMorph/instance/handles/addRecolorHandle-3A-.cs
+++ HaloMorph/instance/handles/addRecolorHandle-3A-.cs
@@ -2,8 +2,10 @@ addRecolorHandle: haloSpec
 	"Add a recolor handle to the receiver, if appropriate"
 
 	| recolorHandle |
-	 innerTarget canSetColor ifTrue:
-		[recolorHandle _ self addHandle: haloSpec on: #mouseUp send: #doRecolor:with:  to: self.
-		recolorHandle on: #mouseUp send: #doRecolor:with: to: self]
 
+	"since this halo now opens a more general properties panel, allow it in all cases"
+	"innerTarget canSetColor ifTrue:"
+
+	recolorHandle _ self addHandle: haloSpec on: #mouseUp send: #doRecolor:with: to: self.
+	recolorHandle on: #mouseUp send: #doRecolor:with: to: self
 
