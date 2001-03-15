cbfded70d5ccfdfa01150e9c9979239ac8e82a0b
ar <unknown>
2001-03-15 22:42:00 +0000

    no message

---
+++ Project/instance/file in-2F-out/htmlPagePrototype.cs
@@ -0,0 +1,22 @@
+htmlPagePrototype
+	"Return the HTML page prototype"
+^'<html>
+<head>
+<title>Squeak Project</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF">
+<EMBED 
+	type="application/x-squeak-source"
+	ALIGN="CENTER"
+	WIDTH="$$WIDTH$$"
+	HEIGHT="$$HEIGHT$$"
+	src="$$PROJECT$$"
+	pluginspage="http://www.squeakland.org/plugin/detect/detectinstaller.html">
+
+</EMBED>
+
+</body>
+</html>
+'
