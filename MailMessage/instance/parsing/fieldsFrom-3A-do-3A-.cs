cb1b71e62670d46d70f494bdac6ef530f03e83de
mdr <unknown>
2001-03-15 18:46:00 +0000

    no message

--- MailMessage/instance/parsing/fieldsFrom-3A-do-3A-.cs
+++ MailMessage/instance/parsing/fieldsFrom-3A-do-3A-.cs
@@ -2,11 +2,11 @@ fieldsFrom: aStream do: aBlock
 	"Invoke the given block with each of the header fields from the given stream. The block arguments are the field name and value. The streams position is left right after the empty line separating header and body."
 
 	| savedLine line s |
-	savedLine _ self readStringLineFrom: aStream.
+	savedLine _ MailDB readStringLineFrom: aStream.
 	[aStream atEnd] whileFalse: [
 		line _ savedLine.
 		(line isEmpty) ifTrue: [^self].  "quit when we hit a blank line"
-		[savedLine _ self readStringLineFrom: aStream.
+		[savedLine _ MailDB readStringLineFrom: aStream.
 		 (savedLine size > 0) and: [savedLine first isSeparator]] whileTrue: [
 			"lines starting with white space are continuation lines"
 			s _ ReadStream on: savedLine.
