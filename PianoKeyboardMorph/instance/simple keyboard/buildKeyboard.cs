5c23be2720f4c2e29cff13905645e950a6714e5c
ar <unknown>
2001-03-17 14:30:00 +0000

    no message

--- PianoKeyboardMorph/instance/simple keyboard/buildKeyboard.cs
+++ PianoKeyboardMorph/instance/simple keyboard/buildKeyboard.cs
@@ -11,16 +11,16 @@ buildKeyboard
 			[:j | keyRect _ octavePt + (j-1*wtWid@0) extent: (wtWid+1)@36.
 			self addMorph: ((RectangleMorph newBounds: keyRect color: whiteKeyColor)
 								borderWidth: 1;
-				on: #mouseDown send: #mouseDownEvent:noteMorph:pitch: to: self
+				on: #mouseDown send: #mouseDownPitch:event:noteMorph: to: self
 								withValue: i-1*12 + (#(1 3 5 6 8 10 12) at: j))].
 		1 to: nBlack do:
 			[:j | keyRect _ octavePt + ((#(6 15 29 38 47) at: j)@1) extent: bkWid@21.
 			self addMorph: ((Morph newBounds: keyRect color: blackKeyColor)
-				on: #mouseDown send: #mouseDownEvent:noteMorph:pitch: to: self
+				on: #mouseDown send: #mouseDownPitch:event:noteMorph: to: self
 								withValue: i-1*12 + (#(2 4 7 9 11) at: j))]].
 	self submorphsDo:
-		[:m | m on: #mouseMove send: #mouseMoveEvent:noteMorph:pitch: to: self;
-				on: #mouseUp send: #mouseUpEvent:noteMorph:pitch: to: self;
-				on: #mouseEnterDragging send: #mouseDownEvent:noteMorph:pitch: to: self;
-				on: #mouseLeaveDragging send: #mouseUpEvent:noteMorph:pitch: to: self].
+		[:m | m on: #mouseMove send: #mouseMovePitch:event:noteMorph: to: self;
+				on: #mouseUp send: #mouseUpPitch:event:noteMorph: to: self;
+				on: #mouseEnterDragging send: #mouseDownPitch:event:noteMorph: to: self;
+				on: #mouseLeaveDragging send: #mouseUpPitch:event:noteMorph: to: self].
 	self extent: (self fullBounds extent + borderWidth - 1)
