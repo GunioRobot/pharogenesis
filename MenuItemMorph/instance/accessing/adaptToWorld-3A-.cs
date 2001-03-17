fe446f28415483430aa2b771e1d4600980c94e26
ar <unknown>
2001-03-17 20:16:00 +0000

    no message

--- MenuItemMorph/instance/accessing/adaptToWorld-3A-.cs
+++ MenuItemMorph/instance/accessing/adaptToWorld-3A-.cs
@@ -1,9 +1,4 @@
 adaptToWorld: aWorld
 
-        super adaptToWorld: aWorld.
-        (target isKindOf: Presenter) ifTrue: [^self target: aWorld presenter].
-        (target isKindOf: TheWorldMenu) ifTrue: [^target adaptToWorld: aWorld].
-        target isMorph ifTrue: [
-                target isWorldMorph ifTrue: [self target: aWorld].
-                target isHandMorph ifTrue: [self target: aWorld primaryHand]
-        ].
+	super adaptToWorld: aWorld.
+	target _ target adaptedToWorld: aWorld.
