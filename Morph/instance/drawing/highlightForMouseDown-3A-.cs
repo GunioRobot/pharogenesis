534ea45168bb90aff80cb1d30b9666d2f0669e28
ar <unknown>
2001-03-17 15:56:00 +0000

    no message

--- Morph/instance/drawing/highlightForMouseDown-3A-.cs
+++ Morph/instance/drawing/highlightForMouseDown-3A-.cs
@@ -1,3 +1,5 @@
 highlightForMouseDown: aBoolean
-	self setProperty: #highlightedForMouseDown toValue: aBoolean.
+	aBoolean 
+		ifTrue:[self setProperty: #highlightedForMouseDown toValue: aBoolean]
+		ifFalse:[self removeProperty: #highlightedForMouseDown. self resetExtension].
 	self changed
