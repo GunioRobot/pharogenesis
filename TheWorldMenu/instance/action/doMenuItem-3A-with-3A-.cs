7578792cd816afad1f5cd1c3d6fc07d788926c24
ar <unknown>
2001-03-17 23:46:00 +0000

    no message

--- TheWorldMenu/instance/action/doMenuItem-3A-with-3A-.cs
+++ TheWorldMenu/instance/action/doMenuItem-3A-with-3A-.cs
@@ -2,12 +2,10 @@ doMenuItem: aCollection with: event
 	| realTarget selector nArgs |
 	selector _ aCollection second.
 	nArgs _ selector numArgs.
-	^[
-		realTarget _ aCollection first.
-		realTarget == #myWorld ifTrue: [realTarget _ myWorld].
-		realTarget == #myHand ifTrue: [realTarget _ myHand].
-		realTarget == #myProject ifTrue: [realTarget _ self projectForMyWorld].
-		nArgs = 0 
-			ifTrue:[realTarget perform: selector]
-			ifFalse:[realTarget perform: selector with: event].
-	] on: RequestCurrentWorldNotification do: [ :ex | ex resume: myWorld ]
+	realTarget _ aCollection first.
+	realTarget == #myWorld ifTrue: [realTarget _ myWorld].
+	realTarget == #myHand ifTrue: [realTarget _ myHand].
+	realTarget == #myProject ifTrue: [realTarget _ self projectForMyWorld].
+	^nArgs = 0 
+		ifTrue:[realTarget perform: selector]
+		ifFalse:[realTarget perform: selector with: event].
