f000c3d4612be6fd8f23ee7d7c860cce9fde0bde
ar <unknown>
2001-03-18 17:27:00 +0000

    no message

--- CategoryViewer/instance/get-2F-set slots/newMakeGetter-3A-from-3A-forPart-3A-.cs
+++ CategoryViewer/instance/get-2F-set slots/newMakeGetter-3A-from-3A-forPart-3A-.cs
@@ -1,8 +1,4 @@
-newMakeGetter: evt from: aMorph forPart: aSpec
-	"Button in viewer performs this to make a new style tile and attach to hand."
-
-	| m |
-	m _ self newTilesFor: scriptedPlayer getter: aSpec.
-	owner ifNotNil: [self primaryHand attachMorph: m.
-			m align: m topLeft with: evt hand position + (7@14)]
-		ifNil: [^ m].
+newMakeGetter: arg1 from: arg2 forPart: arg3
+	"Reorder the arguments for existing event handlers"
+	(arg3 isMorph and:[arg3 eventHandler notNil]) ifTrue:[arg3 eventHandler fixReversedValueMessages].
+	^self newMakeGetter: arg1 event: arg2 from: arg3
