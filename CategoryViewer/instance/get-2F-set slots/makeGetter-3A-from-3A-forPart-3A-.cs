b6e948cf1991cb14a116400a0ea94b117bb6487f
ar <unknown>
2001-03-18 17:26:00 +0000

    no message

--- CategoryViewer/instance/get-2F-set slots/makeGetter-3A-from-3A-forPart-3A-.cs
+++ CategoryViewer/instance/get-2F-set slots/makeGetter-3A-from-3A-forPart-3A-.cs
@@ -1,27 +1,4 @@
-makeGetter: evt from: aMorph forPart: args
-
-	| m selfTile selector aType firstArg |
-	(aType _ args last) == #unknown ifTrue: [^ self beep].
-
-	(#(colorSees isOverColor touchesA) includes: (firstArg _ args first))
-		ifFalse:
-			[m _ PhraseTileMorph new setSlotRefOperator: args first asSymbol type: aType]
-		ifTrue:
-			[(firstArg == #colorSees) ifTrue: [m _ self colorSeesPhrase].
-			(firstArg == #isOverColor) ifTrue: [m _ self seesColorPhrase].
-			(firstArg == #touchesA) ifTrue: [m _ self touchesAPhrase].
-		].
-
-	selfTile _ self tileForSelf bePossessive.
-	selfTile position: m firstSubmorph position.
-	m firstSubmorph addMorph: selfTile.
-
-	selector _ m submorphs at: 2.
-	(aType == #number) ifTrue:
-		[selector addSuffixArrow].
-	selector updateLiteralLabel.
-
-	m enforceTileColorPolicy.
-
-	owner ifNotNil: [self primaryHand attachMorph: m]
-		ifNil: [^ m].
+makeGetter: arg1 from: arg2 forPart: arg3
+	"Reorder the arguments for existing event handlers"
+	(arg3 isMorph and:[arg3 eventHandler notNil]) ifTrue:[arg3 eventHandler fixReversedValueMessages].
+	^self makeGetter: arg1 event: arg2 from: arg3
