d19a8f97562f51b710034a27deac37cc36aea73e
ls <unknown>
2001-03-18 16:26:00 +0000

    no message

--- MailMessage/instance/multipart/decoderClass.cs
+++ MailMessage/instance/multipart/decoderClass.cs
@@ -1,6 +1,6 @@
 decoderClass
 	| encoding |
-	encoding _ self fields at: 'content-transfer-encoding' ifAbsent: [^ nil].
+	encoding _ self fieldNamed: 'content-transfer-encoding' ifAbsent: [^ nil].
 	encoding _ encoding mainValue.
 	encoding asLowercase = 'base64' ifTrue: [^ Base64MimeConverter].
 	encoding asLowercase = 'quoted-printable' ifTrue: [^ QuotedPrintableMimeConverter].
