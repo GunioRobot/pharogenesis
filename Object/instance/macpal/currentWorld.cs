7774fc49c17ef9182ec80bdf51ad66ca2d1bbc7e
ar <unknown>
2001-03-18 00:08:00 +0000

    no message

--- Object/instance/macpal/currentWorld.cs
+++ Object/instance/macpal/currentWorld.cs
@@ -1,5 +1,6 @@
 currentWorld
 	"Answer a morphic world that is the current UI focus.
+		If in an embedded world, it's that world.
 		If in a morphic project, it's that project's world.  
 		If in an mvc project, it is the topmost morphic-mvc-window's worldMorph. 
 		If in an mvc project that has no morphic-mvc-windows, then it's just some existing worldmorph instance.
@@ -8,14 +9,13 @@ currentWorld
 	This method will never return nil, it will always return its best effort at returning a relevant world morph, but if need be -- if there are no worlds anywhere, it will create a new one."
 
 	| aView aSubview |
-	
-	^Display getCurrentMorphicWorld ifNil: [
-		aView _ ScheduledControllers controllerSatisfying:
-			[:ctrl | (aSubview _ ctrl view firstSubView) notNil and:
-				[aSubview model isMorph and: [aSubview model isWorldMorph]]].
-		aView
-			ifNotNil:
-				[aSubview model]
-			ifNil:
-				[MVCWiWPasteUpMorph newWorldForProject: nil]
-	].
+	ActiveWorld ifNotNil:[^ActiveWorld].
+	World ifNotNil:[^World].
+	aView _ ScheduledControllers controllerSatisfying:
+		[:ctrl | (aSubview _ ctrl view firstSubView) notNil and:
+			[aSubview model isMorph and: [aSubview model isWorldMorph]]].
+	^aView
+		ifNotNil:
+			[aSubview model]
+		ifNil:
+			[MVCWiWPasteUpMorph newWorldForProject: nil].
