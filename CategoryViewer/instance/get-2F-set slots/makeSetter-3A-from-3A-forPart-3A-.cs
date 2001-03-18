6c00557df7664549bb7aee365d60f0fb065e2dfd
ar <unknown>
2001-03-18 17:27:00 +0000

    no message

--- CategoryViewer/instance/get-2F-set slots/makeSetter-3A-from-3A-forPart-3A-.cs
+++ CategoryViewer/instance/get-2F-set slots/makeSetter-3A-from-3A-forPart-3A-.cs
@@ -1,27 +1,4 @@
-makeSetter: evt from: aMorph forPart: args
-
-	| argType m argTile selfTile argValue |
-	argType _ args last.
-	m _ PhraseTileMorph new setAssignmentRoot: args first asSymbol
-		type: #command
-		rcvrType: #player
-		argType: argType.
-
-	argValue _ self scriptedPlayer perform: (ScriptingSystem getterSelectorFor: args first asSymbol).
-	(argValue isKindOf: Player)
-		ifTrue:
-			[argTile _ argValue tileReferringToSelf]
-		ifFalse:
-			[argTile _ scriptedPlayer tileForArgType: argType inViewer: self.
-			argTile setLiteral: argValue; updateLiteralLabel.].
-	argTile position: m lastSubmorph position.
-
-	m lastSubmorph addMorph: argTile.
-
-	selfTile _ self tileForSelf bePossessive.
-	selfTile position: m firstSubmorph position.
-	m firstSubmorph addMorph: selfTile.
-
-	m enforceTileColorPolicy.
-	owner ifNotNil: [self primaryHand attachMorph: m]
-		ifNil: [^ m].
+makeSetter: arg1 from: arg2 forPart: arg3
+	"Reorder the arguments for existing event handlers"
+	(arg3 isMorph and:[arg3 eventHandler notNil]) ifTrue:[arg3 eventHandler fixReversedValueMessages].	
+	^self makeSetter: arg1 event: arg2 from: arg3
