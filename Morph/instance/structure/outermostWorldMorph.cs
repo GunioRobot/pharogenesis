3323265798da4808cd491b67c49aa38650641a49
ar <unknown>
2001-03-18 00:12:00 +0000

    no message

--- Morph/instance/structure/outermostWorldMorph.cs
+++ Morph/instance/structure/outermostWorldMorph.cs
@@ -1,6 +1,8 @@
 outermostWorldMorph
 
 	| outer |
+	World ifNotNil:[^World].
+	self flag: #arNote. "stuff below is really only for MVC"
 	outer _ self outermostMorphThat: [ :x | x isWorldMorph].
 	outer ifNotNil: [^outer].
 	self isWorldMorph ifTrue: [^self].
