7612e051e4a4e83c55341f8efec1d186b1c65655
ar <unknown>
2001-03-18 01:43:00 +0000

    no message

--- HandMorph/instance/private events/sendEvent-3A-focus-3A-.cs
+++ HandMorph/instance/private events/sendEvent-3A-focus-3A-.cs
@@ -1,4 +1,3 @@
 sendEvent: anEvent focus: focusHolder
 	"Send the event to the morph currently holding the focus, or if none to the owner of the hand."
-	focusHolder ifNil:[^owner processEvent: anEvent].
-	^self sendFocusEvent: anEvent to: focusHolder.
+	^self sendEvent: anEvent focus: focusHolder clear:[nil]
