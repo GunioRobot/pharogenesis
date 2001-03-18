cbecbb3963457a38aab1a94d43b25c47bced5914
ar <unknown>
2001-03-18 00:54:00 +0000

    no message

--- HttpUrl/instance/downloading/askNamePassword.cs
+++ HttpUrl/instance/downloading/askNamePassword.cs
@@ -7,7 +7,7 @@ wants a different user and password.  Type them now?' orCancel: [false])
 		ifFalse: [^ false].
 	"Note: When Scamper is converted to run under MVC, we'll have to pass in topView in order to decide which FillInTheBlank to call."
 	user _ FillInTheBlank request: 'User account name?' initialAnswer: '' 
-				centerAt: Sensor cursorPoint - (50@0).
+				centerAt: (ActiveHand ifNil:[Sensor]) cursorPoint - (50@0).
 	password _ FillInTheBlank requestPassword: 'Password?'.
 	Passwords at: realm put: (Authorizer new encode: user password: password).
 	^ true
