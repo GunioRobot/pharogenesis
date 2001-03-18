4f977105ecd64b9e5ff339049fd8d2bc41bd8d88
ar <unknown>
2001-03-18 00:55:00 +0000

    no message

--- PopUpMenu/instance/basic control sequence/startUpCenteredWithCaption-3A-.cs
+++ PopUpMenu/instance/basic control sequence/startUpCenteredWithCaption-3A-.cs
@@ -1,7 +1,4 @@
 startUpCenteredWithCaption: captionOrNil
 	"Differs from startUpWithCaption: by appearing with cursor in the menu,
 	and thus ready to act on mouseUp, without requiring user tweak to confirm"
-
-	| cursorPoint |
-	cursorPoint _ Display lastKnownCursorPoint.
-	^ self startUpWithCaption: captionOrNil at: cursorPoint - (20@0)
+	^ self startUpWithCaption: captionOrNil at: (ActiveHand ifNil:[Sensor]) cursorPoint - (20@0)
