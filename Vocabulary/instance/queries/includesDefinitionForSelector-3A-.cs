eabd81361f6240d7e94d7083f8179d1f11e0ab2b
sw <unknown>
2001-03-20 15:42:00 +0000

    no message

---
+++ Vocabulary/instance/queries/includesDefinitionForSelector-3A-.cs
@@ -0,0 +1,4 @@
+includesDefinitionForSelector: aSelector
+	"Answer whether the given selector is known to the vocabulary.  This is independent of whether its definition lies within the range specified by my limitClass.  Answer whether the given selector is known to the vocabulary.  Unsent at the moment, may disappear."
+
+	^ methodInterfaces includesKey: aSelector
