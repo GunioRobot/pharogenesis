d13dddd0b4625ab44cecf469b1fd750d78b05820
ajh <unknown>
2001-03-20 11:41:00 +0000

    no message

--- CompiledMethod/instance/printing/symbolic.cs
+++ CompiledMethod/instance/printing/symbolic.cs
@@ -1,13 +1,8 @@
 symbolic
 	"Answer a String that contains a list of all the byte codes in a method 
-	with a short description of each." 
+	with a short description of each."
+
 	| aStream |
-	self isQuick ifTrue: 
-		[self isReturnSpecial ifTrue:
-			[^ 'Quick return ' , (#('self' 'true' 'false' 'nil' '-1' '0' '1' '2')
-									at: self primitive - 255)].
-		^ 'Quick return field ' , self returnField printString , ' (0-based)'].
 	aStream _ WriteStream on: (String new: 1000).
-	self printPrimitiveOn: aStream.
-	(InstructionPrinter on: self) printInstructionsOn: aStream.
+	self longPrintOn: aStream.
 	^aStream contents
