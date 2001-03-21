6fc9b481fd0abcdb34bd8ab43851b2522d291302
mdr <unknown>
2001-03-21 17:28:00 +0000

    no message

--- CelesteComposition/instance/access/submit.cs
+++ CelesteComposition/instance/access/submit.cs
@@ -1,7 +1,13 @@
 submit
+	| message msgID |
 	"submit the message"
 	textEditor
 		ifNotNil: [self hasUnacceptedEdits ifTrue: [textEditor accept]].
-	celeste queueMessageWithText: (MailMessage from: messageText asString) asSendableText.
+	message := MailMessage from: messageText asString.
+	self breakLinesInMessage: message.
+	msgID _ (celeste isActive ifTrue: [celeste] ifFalse: [Celeste current])
+				queueMessageWithText: message text.
+	msgID ifNil: [^self].		"There was an error, so do not close"
+
 	morphicWindow ifNotNil: [morphicWindow delete].
 	mvcWindow ifNotNil: [mvcWindow controller close]
