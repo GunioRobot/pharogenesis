be1bb9a763b9c3d7eca7822e034608f6be2f7848
sw <unknown>
2001-03-23 23:26:00 +0000

    no message

--- UpdatingRectangleMorph/instance/setting/setTargetColor-3A-.cs
+++ UpdatingRectangleMorph/instance/setting/setTargetColor-3A-.cs
@@ -1,6 +1,7 @@
 setTargetColor: aColor
+	"Set my target's color as indicated"
 
 	putSelector ifNotNil:
 		[self color: aColor.
 		contents _ aColor.
-		target scriptPerformer perform: self putSelector withArguments: (Array with: aColor)]
+		self valueProvider perform: self putSelector withArguments: (Array with: aColor)]
