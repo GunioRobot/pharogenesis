274017a8b2530b247ab1e31de1155ade84267e47
sw <unknown>
2001-03-24 23:58:00 +0000

    no message

--- PasteUpMorph/instance/painting/makeNewDrawingWithin.cs
+++ PasteUpMorph/instance/painting/makeNewDrawingWithin.cs
@@ -1,4 +1,6 @@
 makeNewDrawingWithin
+	"Start a painting session in my interior which will result in a new SketchMorph being created as one of my submorphs"
+
 	| evt |
 	evt _ MouseEvent new setType: nil position: self center buttons: 0 hand: self world activeHand.
-	self primaryHand makeNewDrawing: evt
+	self makeNewDrawing: evt
