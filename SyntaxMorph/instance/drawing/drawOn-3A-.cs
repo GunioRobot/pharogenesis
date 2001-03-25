c11f6f79688a56fc88e525218ea2b88be0bbe6a6
RAA <unknown>
2001-03-25 16:16:00 +0000

    no message

--- SyntaxMorph/instance/drawing/drawOn-3A-.cs
+++ SyntaxMorph/instance/drawing/drawOn-3A-.cs
@@ -1,12 +1,13 @@
 drawOn: aCanvas
 
 	super drawOn: aCanvas.
-	self isBlockNode ifTrue:
-		[((owner respondsTo: #isMethodNode) and: [owner isMethodNode])
-		ifTrue:
-			[aCanvas fillRectangle: (self topLeft + (0@-1) extent: self width@1) color: Color gray]
-		ifFalse:
-			[aCanvas fillRectangle: (self topLeft + (1@1) extent: 2@(self height-2)) color: Color gray.
-			aCanvas fillRectangle: (self topLeft + (1@1) extent: 4@1) color: Color gray.
-			aCanvas fillRectangle: (self bottomLeft + (1@-1) extent: 4@1) color: Color gray].
-		]
+	self isBlockNode ifFalse: [^self].
+	self alansTest1 ifTrue: [^self].
+
+	self immediatelyBelowTheMethodNode ifTrue: [
+		aCanvas fillRectangle: (self topLeft + (0@-1) extent: self width@1) color: Color gray
+	] ifFalse: [
+		aCanvas fillRectangle: (self topLeft + (1@1) extent: 2@(self height-2)) color: Color gray.
+		aCanvas fillRectangle: (self topLeft + (1@1) extent: 4@1) color: Color gray.
+		aCanvas fillRectangle: (self bottomLeft + (1@-1) extent: 4@1) color: Color gray
+	].
