6ed00f849d31a4f74649b8691bac935c1cb87f85
sw <unknown>
2001-03-27 11:52:00 +0000

    no message

---
+++ Morph/instance/dropping-2F-grabbing/nameForUndoWording.cs
@@ -0,0 +1,6 @@
+nameForUndoWording
+	"Return wording appropriate to the receiver for use in an undo-related menu item (and perhaps elsewhere)"
+
+	| aName |
+	aName _ self knownName ifNil: [self renderedMorph class name].
+	^ aName truncateTo: 24
