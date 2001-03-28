0f2a0d5e050389d365c8553506a10562bfa998b3
bf <unknown>
2001-03-28 16:22:00 +0000

    no message

--- X11Display/instance/accessing/getInputFocus.cs
+++ X11Display/instance/accessing/getInputFocus.cs
@@ -1,7 +1,6 @@
 getInputFocus
 	| focus revert |
-	focus _ X11Window new.
-	focus display: self.
+	focus _ X11Window display: self.
 	revert _ WordArray new: 1.
 	self XGetInputFocus: self with: focus with: revert.
 	^focus
