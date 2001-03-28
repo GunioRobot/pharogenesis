23cbd4ce3618bffa607d5bf3271193f726c21b7e
sw <unknown>
2001-03-28 16:26:00 +0000

    no message

---
+++ MethodWithInterface/instance/script editor/instantiatedScriptEditorForPlayer-3A-.cs
@@ -0,0 +1,15 @@
+instantiatedScriptEditorForPlayer: aPlayer
+	"Return a new script editor for the player and selector"
+
+	| aScriptEditor |
+	aScriptEditor _ (self playerClass includesSelector: selector) 
+			ifTrue: [ScriptEditorMorph new 
+				fromExistingMethod: selector 
+				forPlayer: aPlayer]
+			ifFalse: [ScriptEditorMorph new
+				setMorph: aPlayer costume
+				scriptName: selector].
+		defaultStatus == #ticking ifTrue:
+			[aPlayer costume arrangeToStartStepping].
+	
+	^ aScriptEditor
