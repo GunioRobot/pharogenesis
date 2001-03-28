52ef3f6ebbf3a0f234c55f1e3d3e58b6b03fa239
sw <unknown>
2001-03-28 19:47:00 +0000

    no message

--- ElementCategory/instance/initialization/initialize.cs
+++ ElementCategory/instance/initialization/initialize.cs
@@ -2,5 +2,4 @@ initialize
 	"Initialize the receiver (automatically called when instances are created via 'new')"
 
 	super initialize.
-	keysInOrder _ OrderedCollection new.
-	elementDictionary _ IdentityDictionary new
+	self clear
