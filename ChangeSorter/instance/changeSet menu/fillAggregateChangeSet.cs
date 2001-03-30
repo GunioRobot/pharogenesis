ffb307d8240f8d3efed9394cfc9c9758da513bdd
sw <unknown>
2001-03-30 01:26:00 +0000

    no message

---
+++ ChangeSorter/instance/changeSet menu/fillAggregateChangeSet.cs
@@ -0,0 +1,4 @@
+fillAggregateChangeSet
+	"Create a change-set named Aggregate and pour into it all the changes in all the change-sets of the currently-selected category"
+
+	self changeSetCategory fillAggregateChangeSet
