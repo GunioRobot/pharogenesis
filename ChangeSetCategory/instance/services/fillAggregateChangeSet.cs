80d3683cfc3260be3e05431924f9543789c0b80f
sw <unknown>
2001-03-30 13:55:00 +0000

    no message

---
+++ ChangeSetCategory/instance/services/fillAggregateChangeSet.cs
@@ -0,0 +1,12 @@
+fillAggregateChangeSet
+	"Create a change-set named Aggregate and pour into it all the changes in all the change-sets of the currently-selected category"
+
+	| aggChangeSet |
+	aggChangeSet _  ChangeSorter assuredChangeSetNamed: #Aggregate.
+	aggChangeSet clear.
+	aggChangeSet setPreambleToSay: '"Change Set:		Aggregate
+Created at ', Time now printString, ' on ', Date today printString, ' by combining all the changes in all the change sets in the category ', categoryName printString, '"'.
+
+	(self elementsInOrder copyWithout: aggChangeSet) do:
+		[:aChangeSet  | aggChangeSet assimilateAllChangesFoundIn: aChangeSet].
+	Smalltalk isMorphic ifTrue: [SystemWindow wakeUpTopWindowUponStartup] 
