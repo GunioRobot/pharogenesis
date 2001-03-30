e3cd7f3c650cbdcf0bd0a3412b81a04f3d8fd86d
sw <unknown>
2001-03-30 14:39:00 +0000

    no message

---
+++ ChangeSetCategory/instance/queries/changeSetList.cs
@@ -0,0 +1,10 @@
+changeSetList
+	"Answer the list of change-set names in the category"
+
+	| aChangeSet |
+	self reconstituteList.
+	keysInOrder size == 0 ifTrue:
+		["don't tolerate emptiness, because ChangeSorters gag when they have no change-set selected"
+		aChangeSet _ ChangeSorter assuredChangeSetNamed: 'New Changes'.
+		self elementAt: aChangeSet name put: aChangeSet].
+	^ keysInOrder reversed
