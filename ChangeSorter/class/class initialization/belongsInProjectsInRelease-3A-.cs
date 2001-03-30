a40bb81b45ed30f90870f312c4feeee63d03d32e
sw <unknown>
2001-03-30 12:44:00 +0000

    no message

---
+++ ChangeSorter/class/class initialization/belongsInProjectsInRelease-3A-.cs
@@ -0,0 +1,5 @@
+belongsInProjectsInRelease:  aChangeSet
+	"Answer whether a change set belongs in the ProjectsInRelease category.  You can hand-tweak this to suit your working style.  This just covers the space of project names in the 2.9, 3.0, and 3.1a systems"
+
+	| aString |
+	^ ((aString _ aChangeSet name) beginsWith: 'Play With Me') or: [self changeSetNamesInReleaseImage includes: aString]
