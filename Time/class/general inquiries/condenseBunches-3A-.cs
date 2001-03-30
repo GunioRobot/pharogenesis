d8dbe459d24b7df325f8009ccc557feaac837e2d
BP <unknown>
2001-03-30 15:25:00 +0000

    no message

--- Time/class/general inquiries/condenseBunches-3A-.cs
+++ Time/class/general inquiries/condenseBunches-3A-.cs
@@ -1,13 +1,19 @@
 condenseBunches: aCollectionOfSeconds
 	| secArray pause now out prev bunchEnd ago |
-	"Identify the major intervals in a bunch of numbers.  Each number is a seconds since 1901 that represents a date and time.  We want the last event in a bunch.  Return array of seconds for:
+	"Identify the major intervals in a bunch of numbers.  
+	Each number is a seconds since 1901 that represents a date and time.
+	We want the last event in a bunch.  Return array of seconds for:
+	
 	Every event in the last half hour.
-	Every bunch separated by 30 min in the last 24 hours.
+		Every bunch separated by 30 min in the last 24 hours.
+	
 	Every bunch separated by two hours before that."
-"Time condenseBunches: 
-	(#(20 400 401  20000 20200 20300   40000 45000  200000 201000 202000) collect: [:tt | 
-		self totalSeconds - tt]) 
+
+	"Time condenseBunches: 
+		(#(20 400 401  20000 20200 20300 40000 45000  200000 201000 202000) 
+			collect: [ :tt | self totalSeconds - tt])
 "
+
 	secArray _ aCollectionOfSeconds asSortedCollection.
 	pause _ 1.
 	now _ self totalSeconds.
