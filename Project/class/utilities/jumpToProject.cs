02410907fdd3a9c8575b145dc5450d995dbd12c0
jla <unknown>
2001-04-02 20:34:00 +0000

    no message

--- Project/class/utilities/jumpToProject.cs
+++ Project/class/utilities/jumpToProject.cs
@@ -1,4 +1,4 @@
 jumpToProject		"Project jumpToProject"
 	"Present a list of potential projects and enter the one selected."
 
-	self jumpToSelection: (self buildJumpToMenu: CustomMenu new) startUp
+	self jumpToSelection: (self buildJumpToMenu: CustomMenu new) startUpLeftFlush
