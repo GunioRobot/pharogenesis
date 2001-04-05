e25c3f93398a2098fdbb183a538c3f67abe6740d
di <unknown>
2001-04-05 19:42:00 +0000

    no message

--- ChangeSorter/class/enumerating/changeSetsNamedSuchThat-3A-.cs
+++ ChangeSorter/class/enumerating/changeSetsNamedSuchThat-3A-.cs
@@ -3,5 +3,4 @@ changeSetsNamedSuchThat: nameBlock
 		[:name | name first isDigit and: [name initialInteger >= 373]])
 		do: [:cs | AllChangeSets remove: cs wither]"
 
-	self gatherChangeSets.
 	^ AllChangeSets select: [:aChangeSet | nameBlock value: aChangeSet name]
