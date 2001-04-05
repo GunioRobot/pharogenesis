db352919e9ba08f1e4a96d086ae81586001d37a3
di <unknown>
2001-04-05 19:42:00 +0000

    no message

--- ChangeSorter/class/enumerating/changeSetNamed-3A-.cs
+++ ChangeSorter/class/enumerating/changeSetNamed-3A-.cs
@@ -1,8 +1,6 @@
 changeSetNamed: aName
 	"Return the change set of the given name, or nil if none found.  1/22/96 sw"
 
-	self gatherChangeSets.
-	AllChangeSets do:
-		[:aChangeSet | aChangeSet name = aName ifTrue:
-			[^ aChangeSet]].
-	^ nil
+	^ AllChangeSets
+			detect: [:aChangeSet | aChangeSet name = aName]
+			ifNone: [nil]
