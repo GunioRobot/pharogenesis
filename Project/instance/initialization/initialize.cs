4925c2605a62e0f92a6466701b175ce0578f8618
di <unknown>
2001-04-06 10:30:00 +0000

    no message

--- Project/instance/initialization/initialize.cs
+++ Project/instance/initialization/initialize.cs
@@ -1,7 +1,9 @@
 initialize
+	"Initialize the project, seting the CurrentProject as my parentProject and initializing my project preferences from those of the CurrentProject"
 
-	changeSet _ ChangeSet new initialize.
+	changeSet _ ChangeSet new.
 	transcript _ TranscriptStream new.
 	displayDepth _ Display depth.
 	parentProject _ CurrentProject.
 	isolatedHead _ false.
+	self initializeProjectPreferences
