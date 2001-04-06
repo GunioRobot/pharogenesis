055fa4323123d7b1de2d5f69997f8a4b1cf35107
di <unknown>
2001-04-06 10:37:00 +0000

    no message

---
+++ ChangeSetCategory/instance/miscellaneous/reconstituteList.cs
@@ -0,0 +1,13 @@
+reconstituteList
+	"Clear out the receiver's elements and rebuild them"
+
+	| newMembers |
+	"First determine newMembers and check if they have not changed..."
+	newMembers _ ChangeSorter allChangeSets select:
+		[:aChangeSet | ChangeSorter perform: membershipSelector with: aChangeSet].
+	(newMembers collect: [:cs | cs name]) = keysInOrder ifTrue: [^ self  "all current"].
+
+	"Things have changed.  Need to recompute the whole category"
+	self clear.
+	newMembers do:
+		[:aChangeSet | self fasterElementAt: aChangeSet name asSymbol put: aChangeSet] 
