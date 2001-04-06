53ea6d40e30f2575a332c61a681d939b5ef1f782
di <unknown>
2001-04-06 09:46:00 +0000

    no message

--- ChangeSorter/class/adding/basicNewChangeSet-3A-.cs
+++ ChangeSorter/class/adding/basicNewChangeSet-3A-.cs
@@ -1,8 +1,9 @@
 basicNewChangeSet: newName
 	| newSet |
+	newName ifNil: [^ nil].
 	(self changeSetNamed: newName) ifNotNil:
 		[self inform: 'Sorry that name is already used'.
 		^ nil].
-	newSet _ ChangeSet new initialize name: newName.
+	newSet _ ChangeSet basicNewNamed: newName.
 	AllChangeSets add: newSet.
 	^ newSet
