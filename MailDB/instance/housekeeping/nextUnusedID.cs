82c1ef5edfb3a1bf8fba96043002dc49a1e10354
mdr <unknown>
2001-04-09 13:05:00 +0000

    no message

--- MailDB/instance/housekeeping/nextUnusedID.cs
+++ MailDB/instance/housekeeping/nextUnusedID.cs
@@ -1,13 +1,19 @@
 nextUnusedID
-	"Answer the next unused message identifier. Message ID's are a monotonically increasing series roughly related to the time that they were requested. We use a block of message ID's based on the starting ID computed here. The last ID used is kept in LastID, to be sure that we don't reuse an already allocated ID."
+	"Answer the next unused message identifier number."
+
+	"Each message needs to have a unique ID.  Message ID's are a monotonically increasing integers roughly related to the time that they were requested.  The last ID used is kept in lastIssuedMsgID, to guard against reuse (e.g. if the clock changes)."
 
 	| id |
-	"initialize LastID the first time it is used"
-	(LastID isNil) ifTrue: [LastID _ 0].
+	(lastIssuedMsgID isNil) ifTrue: [ 		"find the largest msgID currently in this database"
+		lastIssuedMsgID _ 0.
+		indexFile keys do: [ :msgID | lastIssuedMsgID _ lastIssuedMsgID max: msgID].
+		].
 
  	"message ID's are roughly the number of seconds since the beginning of 1980"
 	id _ Date today asSeconds + Time now asSeconds -
 		(Date newDay: 1 year: 1980) asSeconds.
-	id _ id max: (LastID + 1).  "never go backwards!"
-	LastID _ id.
+	id _ id max: (lastIssuedMsgID + 1).  "never go backwards!"
+	lastIssuedMsgID _ id.
 	^ id
+
+	"MailDB someInstance nextUnusedID"
