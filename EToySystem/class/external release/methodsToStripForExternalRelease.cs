3d5742dbe96aa5eadf8ca1c370353d7da4d6cd3f
tk <unknown>
2001-04-10 13:08:00 +0000

    no message

--- EToySystem/class/external release/methodsToStripForExternalRelease.cs
+++ EToySystem/class/external release/methodsToStripForExternalRelease.cs
@@ -1,8 +1,6 @@
 methodsToStripForExternalRelease
 	"Answer a list of triplets #(className, class/instance, methodName) of methods to be stripped in an external release."
 	^ #(
-
-		(EToySystem			class		serverUrls)
 		(EToySystem			class		prepareRelease)	
 		(EToySystem			class		previewEToysOn:)
 		)
