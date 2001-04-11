cad91db2addfbb3c1e1dc9294316d0e6171a14df
sw <unknown>
2001-04-11 23:33:00 +0000

    no message

--- Preferences/class/initialization/addPreferenceForOptionalCelesteStatusPane.cs
+++ Preferences/class/initialization/addPreferenceForOptionalCelesteStatusPane.cs
@@ -1,9 +1,11 @@
 addPreferenceForOptionalCelesteStatusPane
+	"Assure existence of a preference that governs the optional celeste status pane"
+
 	"Preferences addPreferenceForOptionalCelesteStatusPane"
-	(FlagDictionary includesKey: #celesteHasStatusPane)
-		ifFalse: [self
-				addPreference: #celesteHasStatusPane
-				category: #general
-				default: false
-				balloonHelp: 'If true, Celeste (e-mail reader) includes a status pane.'
-			"Because Lex doesn't like it the default is false :)"]
+	self preferenceAt: #celesteHasStatusPane ifAbsent:
+		[self
+			addPreference: #celesteHasStatusPane
+			category: #general
+			default: false
+			balloonHelp: 'If true, Celeste (e-mail reader) includes a status pane.'
+		"Because Lex doesn't like it the default is false :)"]
