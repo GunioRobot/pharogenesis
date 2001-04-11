1f355d48a44c3e6018ec59ae6c75d3a41e017c3e
sw <unknown>
2001-04-11 16:31:00 +0000

    no message

---
+++ ChangeSorter/instance/changeSet menu/removeFromCategory.cs
@@ -0,0 +1,14 @@
+removeFromCategory
+	"Add the current change set to the category viewed on the opposite side, if it's of the sort to accept things like that"
+
+	| aCategory |
+	(aCategory _ self changeSetCategory) acceptsManualAdditions
+		ifTrue:
+			[aCategory removeElementAt: myChangeSet name.
+			aCategory reconstituteList.
+			self update]
+		ifFalse:
+			[self inform: 
+'sorry, this command only makes
+sense for static categories whose
+members are manually maintained']
