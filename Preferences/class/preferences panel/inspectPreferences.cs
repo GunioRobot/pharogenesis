a984714621919bab9cbc931aa21d8165f27de9ca
sw <unknown>
2001-04-11 23:55:00 +0000

    no message

---
+++ Preferences/class/preferences panel/inspectPreferences.cs
@@ -0,0 +1,5 @@
+inspectPreferences
+	"Open a window on the current preferences dictionary, allowing the user to inspect and change the current preference settings.  This is fallen back upon if Morphic is not present"
+	"Preferences inspectPreferences"
+
+	DictionaryOfPreferences inspectWithLabel: 'Preferences'
