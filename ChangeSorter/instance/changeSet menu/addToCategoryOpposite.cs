4f7fd0f88db47169285a4e9fd5d631f7b41e8aaa
sw <unknown>
2001-04-11 16:30:00 +0000

    no message

---
+++ ChangeSorter/instance/changeSet menu/addToCategoryOpposite.cs
@@ -0,0 +1,16 @@
+addToCategoryOpposite
+	"Add the current change set to the category viewed on the opposite side, if it's of the sort to accept things like that"
+
+	| categoryOpposite |
+	categoryOpposite _ (parent other: self) changeSetCategory.
+	categoryOpposite acceptsManualAdditions
+		ifTrue:
+			[categoryOpposite addChangeSet: myChangeSet.
+			categoryOpposite reconstituteList.
+			self update]
+		ifFalse:
+			[self inform: 
+'sorry, this command only makes sense
+if the category showing on the opposite
+side is a static category whose
+members are manually maintained']
