07e514f9ac033ae409f18bd8454a7e2a48d71112
nk <unknown>
2001-04-11 18:28:00 +0000

    no message

--- EventSensor/instance/private-2D-I-2F-O/processKeyboardEvent-3A-.cs
+++ EventSensor/instance/private-2D-I-2F-O/processKeyboardEvent-3A-.cs
@@ -2,6 +2,7 @@ processKeyboardEvent: evt
 	"process a keyboard event, updating InputSensor state"
 	| charCode pressCode |
 	"Never update keyboardBuffer if we have an eventQueue active"
+	mouseButtons _ (mouseButtons bitAnd: 7) bitOr: ((evt at: 5) bitShift: 3).
 	eventQueue ifNotNil:[^self]. 
 	charCode _ evt at: 3.
 	charCode = nil ifTrue:[^self]. "extra characters not handled in MVC"
@@ -9,5 +10,4 @@ processKeyboardEvent: evt
 	pressCode = EventKeyChar ifFalse:[^self]. "key down/up not handled in MVC"
 	"mix in modifiers"
 	charCode _ charCode bitOr: ((evt at: 5) bitShift: 8).
-	mouseButtons _ (mouseButtons bitAnd: 7) bitOr: ((evt at: 5) bitShift: 3).
 	keyboardBuffer nextPut: charCode.
