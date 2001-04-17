e1710ab70d20c15fe31b7c8421e0b647f99dcd2e
sw <unknown>
2001-04-17 11:34:00 +0000

    no message

--- Preferences/class/fonts/setSystemFontTo-3A-.cs
+++ Preferences/class/fonts/setSystemFontTo-3A-.cs
@@ -1,10 +1,11 @@
 setSystemFontTo: aFont
 	"Establish the default text font and style"
+
 	| aStyle newDefaultStyle |
 	aFont ifNil: [^ self].
 	aStyle _ aFont textStyle ifNil: [^ self].
 	newDefaultStyle _ aStyle copy.
 	newDefaultStyle defaultFontIndex: (aStyle fontIndexOf: aFont).
 	TextConstants at: #DefaultTextStyle put: newDefaultStyle.
-	Utilities replaceToolsFlap.
+	Flaps replaceToolsFlap.
 	ScriptingSystem resetStandardPartsBin
