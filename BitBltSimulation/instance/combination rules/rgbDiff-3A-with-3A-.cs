1d5b0a33e61bc6dd8ffeeefe6487fd6a8ff073ef
ar <unknown>
2001-04-18 20:38:00 +0000

    no message

--- BitBltSimulation/instance/combination rules/rgbDiff-3A-with-3A-.cs
+++ BitBltSimulation/instance/combination rules/rgbDiff-3A-with-3A-.cs
@@ -4,20 +4,20 @@ rgbDiff: sourceWord with: destinationWord
 	For non-rgb, return the number of differing pixels."
 	| pixMask destShifted sourceShifted destPixVal bitsPerColor rgbMask sourcePixVal diff maskShifted |
 	self inline: false.
-	pixMask _ maskTable at: destPixSize.
-	destPixSize = 16
+	pixMask _ maskTable at: destDepth.
+	destDepth = 16
 		ifTrue: [bitsPerColor _ 5.  rgbMask _ 16r1F]
 		ifFalse: [bitsPerColor _ 8.  rgbMask _ 16rFF].
 	maskShifted _ destMask.
 	destShifted _ destinationWord.
 	sourceShifted _ sourceWord.
-	1 to: pixPerWord do:
+	1 to: destPPW do:
 		[:i |
 		(maskShifted bitAnd: pixMask) > 0 ifTrue:
 			["Only tally pixels within the destination rectangle"
 			destPixVal _ destShifted bitAnd: pixMask.
 			sourcePixVal _ sourceShifted bitAnd: pixMask.
-			destPixSize < 16
+			destDepth < 16
 				ifTrue: [sourcePixVal = destPixVal
 							ifTrue: [diff _ 0]
 							ifFalse: [diff _ 1]]
@@ -27,7 +27,7 @@ rgbDiff: sourceWord with: destinationWord
 							+ (diff>>bitsPerColor bitAnd: rgbMask)
 							+ ((diff>>bitsPerColor)>>bitsPerColor bitAnd: rgbMask)].
 			bitCount _ bitCount + diff].
-		maskShifted _ maskShifted >> destPixSize.
-		sourceShifted _ sourceShifted >> destPixSize.
-		destShifted _ destShifted >> destPixSize].
+		maskShifted _ maskShifted >> destDepth.
+		sourceShifted _ sourceShifted >> destDepth.
+		destShifted _ destShifted >> destDepth].
 	^ destinationWord  "For no effect on dest"
