b633d89bb239fb8110a42dd453f0d644cdbad943
ar <unknown>
2001-04-18 20:38:00 +0000

    no message

--- BitBltSimulation/instance/combination rules/rgbMinInvert-3A-with-3A-.cs
+++ BitBltSimulation/instance/combination rules/rgbMinInvert-3A-with-3A-.cs
@@ -2,11 +2,11 @@ rgbMinInvert: wordToInvert with: destinationWord
 	| sourceWord |
 	self inline: false.
 	sourceWord _ wordToInvert bitInvert32.
-	destPixSize < 16 ifTrue:
+	destDepth < 16 ifTrue:
 		["Min each pixel separately"
 		^ self partitionedMin: sourceWord with: destinationWord
-						nBits: destPixSize nPartitions: pixPerWord].
-	destPixSize = 16 ifTrue:
+						nBits: destDepth nPartitions: destPPW].
+	destDepth = 16 ifTrue:
 		["Min RGB components of each pixel separately"
 		^ (self partitionedMin: sourceWord with: destinationWord
 						nBits: 5 nPartitions: 3)
