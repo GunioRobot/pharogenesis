86b8352c882c8516a4196a109e190dbf0fcf9093
ar <unknown>
2001-04-18 21:16:00 +0000

    no message

--- BitBltSimulator/instance/debug support/srcLongAt-3A-.cs
+++ BitBltSimulator/instance/debug support/srcLongAt-3A-.cs
@@ -2,6 +2,6 @@ srcLongAt: srcIndex
 	interpreterProxy isInterpreterProxy
 		ifTrue:[^srcIndex longAt: 0].
 	((srcIndex anyMask: 3) or:[srcIndex + 4 < sourceBits or:[
-		srcIndex > (sourceBits + (sourcePitch * srcHeight))]])
+		srcIndex > (sourceBits + (sourcePitch * sourceHeight))]])
 			ifTrue:[self error:'Out of bounds'].
 	^interpreterProxy longAt: srcIndex
