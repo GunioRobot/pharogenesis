44771e9f8c38d249d033e61cb7b800e2c0dcd3c8
ar <unknown>
2001-04-18 20:39:00 +0000

    no message

--- BitBltSimulation/instance/combination rules/pixMask-3A-with-3A-.cs
+++ BitBltSimulation/instance/combination rules/pixMask-3A-with-3A-.cs
@@ -1,4 +1,4 @@
 pixMask: sourceWord with: destinationWord
 	self inline: false.
 	^ self partitionedAND: sourceWord bitInvert32 to: destinationWord
-					nBits: destPixSize nPartitions: pixPerWord
+					nBits: destDepth nPartitions: destPPW
