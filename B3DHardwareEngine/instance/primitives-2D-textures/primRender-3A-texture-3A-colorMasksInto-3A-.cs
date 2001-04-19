44862052bb42257e41b303181fd18ca29ebcfa56
ar <unknown>
2001-04-19 03:37:00 +0000

    no message

---
+++ B3DHardwareEngine/instance/primitives-2D-textures/primRender-3A-texture-3A-colorMasksInto-3A-.cs
@@ -0,0 +1,4 @@
+primRender: rendererHandle texture: textureHandle colorMasksInto: anArray
+	"Primitive. Store the bit masks for each color into the given array."
+	<primitive: 'primitiveTextureGetColorMasks' module:'B3DAcceleratorPlugin'>
+	^nil
