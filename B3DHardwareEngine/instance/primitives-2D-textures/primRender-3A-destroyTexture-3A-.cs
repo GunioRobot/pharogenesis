2050b0ed6f8b4986da8c07a1ed6c7d7295ac3a3d
ar <unknown>
2001-04-19 03:37:00 +0000

    no message

---
+++ B3DHardwareEngine/instance/primitives-2D-textures/primRender-3A-destroyTexture-3A-.cs
@@ -0,0 +1,4 @@
+primRender: rendererHandle destroyTexture: textureHandle
+	"Primitive. Destroy the texture associated with the given handle."
+	<primitive:'primitiveDestroyTexture' module:'B3DAcceleratorPlugin'>
+	^nil
