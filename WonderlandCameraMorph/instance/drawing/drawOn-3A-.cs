288ed4773202902e3d87494787162b87f02ad947
ar <unknown>
2001-04-19 05:51:00 +0000

    no message

--- WonderlandCameraMorph/instance/drawing/drawOn-3A-.cs
+++ WonderlandCameraMorph/instance/drawing/drawOn-3A-.cs
@@ -1,15 +1,4 @@
 drawOn: aCanvas 
-	| bg |
-	bg _ self valueOfProperty: #backgroundSnapshot.
-	bg == nil ifFalse:[
-		(self bgCacheValid) ifFalse:[
-			bg _ Display allocateForm: self extent.
-			Display displayOn: bg at: bounds origin negated.
-			self setProperty: #backgroundSnapshot toValue: bg.
-		].
-		aCanvas drawImage: bg at: bounds origin.
-	].
-	aCanvas asBalloonCanvas render: self.
-	self outline ifNotNil: [self sketchOn: aCanvas].
-	"*** hack ***"
-	pickedPoint _ nil.
+	(aCanvas form == Display and:[self accelerationEnabled and:[self isFlexed not]])
+		ifTrue:[self drawAcceleratedOn: aCanvas]
+		ifFalse:[self drawSimulatedOn: aCanvas].
