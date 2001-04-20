024dc5494afe393dd69f14790cbef5a572476f8a
sw <unknown>
2001-04-20 20:11:00 +0000

    no message

--- Player/class/scripts/scripts.cs
+++ Player/class/scripts/scripts.cs
@@ -1,3 +1,9 @@
 scripts
-	scripts ifNil: [scripts _ IdentityDictionary new].
+	"Answer the receiver's scripts -- an IdentityDictionary"
+
+	scripts
+		ifNil:
+			[scripts _ IdentityDictionary new]
+		ifNotNil:
+			[self cleanseScriptsOfNilKeys].
 	^ scripts
