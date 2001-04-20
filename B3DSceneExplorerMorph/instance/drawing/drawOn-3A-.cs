75c7abd7326f7a37aa04643f148016e31277d13b
ar <unknown>
2001-04-20 04:34:00 +0000

    no message

--- B3DSceneExplorerMorph/instance/drawing/drawOn-3A-.cs
+++ B3DSceneExplorerMorph/instance/drawing/drawOn-3A-.cs
@@ -1,3 +1,7 @@
 drawOn: aCanvas
-	super drawOn: aCanvas.
-	aCanvas fillRectangle: (self bounds insetBy: frameWidth) color: Color black.
+	"Don't fill if my b3dScene does it"
+	(b3DSceneMorph notNil and:[b3DSceneMorph color isOpaque]) ifTrue:[
+		(aCanvas clipRect areasOutside: b3DSceneMorph bounds) do:[:r|
+			aCanvas clipBy: r during:[:cc| super drawOn: cc].
+		].
+	] ifFalse:[super drawOn: aCanvas].
