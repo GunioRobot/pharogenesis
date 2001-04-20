fe2500b974457cc1df1275d637adbc1dbad055f1
ar <unknown>
2001-04-20 03:17:00 +0000

    no message

--- B3DRenderEngine/class/instance creation/defaultForPlatformOn-3A-.cs
+++ B3DRenderEngine/class/instance creation/defaultForPlatformOn-3A-.cs
@@ -1,7 +1,5 @@
 defaultForPlatformOn: aForm
 	"Return the render engine that is most appropriate for the current host platform."
-	(B3DHardwareEngine isAvailableFor: aForm) 
-		ifTrue:[^B3DHardwareEngine newOn: aForm].
 	(B3DPrimitiveEngine isAvailableFor: aForm) 
 		ifTrue:[^B3DPrimitiveEngine newOn: aForm].
 	^B3DRenderEngine newOn: aForm
