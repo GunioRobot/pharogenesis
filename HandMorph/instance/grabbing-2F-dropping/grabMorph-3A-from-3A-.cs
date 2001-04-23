46020feacbba3d0bf544d3a0797fed481ce92a7b
ar <unknown>
2001-04-23 15:17:00 +0000

    no message

--- HandMorph/instance/grabbing-2F-dropping/grabMorph-3A-from-3A-.cs
+++ HandMorph/instance/grabbing-2F-dropping/grabMorph-3A-from-3A-.cs
@@ -1,16 +1,21 @@
 grabMorph: aMorph from: formerOwner
 	"Grab the given morph (i.e., add it to this hand and remove it from its current owner) without changing its position. This is used to pick up a morph under the hand's current position, versus attachMorph: which is used to pick up a morph that may not be near this hand."
+
 	| grabbed offset targetPoint grabTransform fullTransform |
 	self releaseMouseFocus. "Break focus"
 	grabbed _ aMorph.
-	"Compute the transform to apply to the grabbed morph"
-	grabTransform _ formerOwner 
-			ifNil:[IdentityTransform new] 
-			ifNotNil:[formerOwner grabTransform].
-	"Compute the full transform for the grabbed morph"
-	fullTransform _ formerOwner 
-			ifNil:[IdentityTransform new] 
-			ifNotNil:[formerOwner transformFrom: owner].
+	aMorph keepsTransform ifTrue:[
+		grabTransform _ fullTransform _ IdentityTransform new.
+	] ifFalse:[
+		"Compute the transform to apply to the grabbed morph"
+		grabTransform _ formerOwner 
+			ifNil:		[IdentityTransform new] 
+			ifNotNil:	[formerOwner grabTransform].
+		"Compute the full transform for the grabbed morph"
+		fullTransform _ formerOwner 
+			ifNil:		[IdentityTransform new] 
+			ifNotNil:	[formerOwner transformFrom: owner].
+	].
 	"targetPoint is point in aMorphs reference frame"
 	targetPoint _ fullTransform globalPointToLocal: self position.
 	"but current position will be determined by grabTransform, so compute offset"
@@ -18,9 +23,10 @@ grabMorph: aMorph from: formerOwner
 	"apply the transform that should be used after grabbing"
 	grabbed _ grabbed transformedBy: grabTransform.
 	grabbed == aMorph 
-		ifFalse:[grabbed setProperty: #addedFlexAtGrab toValue: true].
+		ifFalse:	[grabbed setProperty: #addedFlexAtGrab toValue: true].
 	"offset target to compensate for differences in transforms"
 	grabbed position: grabbed position - offset asIntegerPoint.
 	"And compute distance from hand's position"
 	targetOffset _ grabbed position - self position.
 	self addMorphBack: grabbed.
+	grabbed justGrabbedFrom: formerOwner.
