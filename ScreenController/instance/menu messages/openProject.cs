a77866ceda9d63722c4991b0fc9954b44dfcc3a0
sw <unknown>
2001-04-24 12:00:00 +0000

    no message

--- ScreenController/instance/menu messages/openProject.cs
+++ ScreenController/instance/menu messages/openProject.cs
@@ -1,7 +1,7 @@
 openProject 
 	"Create and schedule a Project."
+
 	| proj |
 	Smalltalk at: #ProjectView ifPresent:
 		[:c | proj _ Project new.
-		proj projectParameters at: #globalFlapsEnabledInProject put: false.
 		c open: proj].
