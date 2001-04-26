d5c064bbdcb45c98c93e9bdd9042aa01ce3cdb54
ar <unknown>
2001-04-26 19:43:00 +0000

    no message

--- BitBltSimulation/instance/interpreter interface/setupColorMasks.cs
+++ BitBltSimulation/instance/interpreter interface/setupColorMasks.cs
@@ -1,15 +1,16 @@
 setupColorMasks
+	"WARNING: For WarpBlt w/ smoothing the source depth is wrong here!"
 	| bits targetBits |
 	bits _ targetBits _ 0.
-	sourcePixSize <= 8 ifTrue:[^nil].
-	sourcePixSize = 16 ifTrue:[bits _ 5].
-	sourcePixSize = 32 ifTrue:[bits _ 8].
+	sourceDepth <= 8 ifTrue:[^nil].
+	sourceDepth = 16 ifTrue:[bits _ 5].
+	sourceDepth = 32 ifTrue:[bits _ 8].
 
-	colorMap == nil
-		ifTrue:["Convert between RGB values"
-				destPixSize <= 8 ifTrue:[^nil].
-				destPixSize = 16 ifTrue:[targetBits _ 5].
-				destPixSize = 32 ifTrue:[targetBits _ 8]]
+	cmBitsPerColor = 0
+		ifTrue:["Convert to destDepth"
+				destDepth <= 8 ifTrue:[^nil].
+				destDepth = 16 ifTrue:[targetBits _ 5].
+				destDepth = 32 ifTrue:[targetBits _ 8]]
 		ifFalse:[targetBits _ cmBitsPerColor].
 
 	self setupColorMasksFrom: bits to: targetBits
