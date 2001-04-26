e9801f0ce44f0a34d6824e2e39700ed721d12eab
ar <unknown>
2001-04-26 19:42:00 +0000

    no message

--- BitBltSimulation/instance/setup/performCopyLoop.cs
+++ BitBltSimulation/instance/setup/performCopyLoop.cs
@@ -7,9 +7,9 @@ performCopyLoop
 		self copyLoopNoSource.
 	] ifFalse: ["Loop using source and dest"
 		self checkSourceOverlap.
-		(sourcePixSize ~= destPixSize or: [colorMap ~= nil]) ifTrue: [
+		(sourceDepth ~= destDepth or: [(cmFlags ~= 0) or:[sourceMSB ~= destMSB]]) ifTrue: [
 			"If we must convert between pixel depths or use
-			color lookups use the general version"
+			color lookups or swap pixels use the general version"
 			self copyLoopPixMap.
 		] ifFalse: [
 			"Otherwise we simple copy pixels and can use a faster version"
