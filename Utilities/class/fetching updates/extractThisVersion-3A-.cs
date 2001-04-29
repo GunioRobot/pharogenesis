dcc82b387f80a7202275aaf3e5c9f26f1383b295
di <unknown>
2001-04-29 22:26:00 +0000

    no message

--- Utilities/class/fetching updates/extractThisVersion-3A-.cs
+++ Utilities/class/fetching updates/extractThisVersion-3A-.cs
@@ -1,14 +1,9 @@
 extractThisVersion: list
 	"Pull out the part of the list that applies to this version."
 
-	| delims lines ii out eToySystem |
-	(eToySystem _ Smalltalk at: #EToySystem ifAbsent: [nil]) ifNil: [^ #()].
-	delims _ String with: Character cr with: Character linefeed.
-	lines _ list findTokens: delims.
-	ii _ lines indexOf: '#', eToySystem version.
-	ii = 0 ifTrue: [^ #()].
-	out _ OrderedCollection new.
-	[(ii _ ii + 1) <= lines size] whileTrue:
-		[(lines at: ii) first == $# ifTrue: [^ out "next version"].
-		(lines at: ii) first == $* ifFalse: [out addLast: (lines at: ii)]].	"keep, except comments"
-	^ out
+	| listContents version versIndex |
+	listContents _ self parseListContents: list.
+	version _ SystemVersion current version.
+	versIndex _ (listContents collect: [:pair | pair first]) indexOf: version.
+	versIndex = 0 ifTrue: [^ Array new].		"abort"
+	^ (listContents at: versIndex) last
