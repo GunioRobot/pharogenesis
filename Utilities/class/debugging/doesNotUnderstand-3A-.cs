70939d6d4b1796c0ca53208f6ea10decd44748e9
sw <unknown>
2001-04-29 23:42:00 +0000

    no message

---
+++ Utilities/class/debugging/doesNotUnderstand-3A-.cs
@@ -0,0 +1,12 @@
+doesNotUnderstand: aMessage
+	"A temporary expedient for revectoring various messages formerly sent to Utilities that now are instead implemented by Flaps; this is only for the benefit of pre-existing buttons and menu items that were set up to call the old interface"
+
+	| aSelector |
+	aSelector _ aMessage selector.
+	(#(addLocalFlap explainFlaps addMenuFlap addPaintingFlap addStackToolsFlap addGlobalFlap offerGlobalFlapsMenu toggleWhetherToUseGlobalFlaps ) includes: aSelector)
+		ifTrue:
+			[^ self inform: 
+'Sorry, this is an obsolete menu.  Please
+dismiss it and get a fresh one.  Thank you'].
+
+	^ super doesNotUnderstand: aMessage
