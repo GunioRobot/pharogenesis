27efd14d511a892dec34808749090f8d9be9ac5d
sw <unknown>
2001-04-30 20:31:00 +0000

    no message

--- PasteUpMorph/instance/flaps/addGlobalFlaps.cs
+++ PasteUpMorph/instance/flaps/addGlobalFlaps.cs
@@ -2,31 +2,22 @@ addGlobalFlaps
 	"Must make global flaps adapt to world.  Do this even if not shown, so the old world will not be pointed at by the flaps."
 
 	| use thisWorld |
-
-	use _ true.
-	(Preferences valueOfFlag: #useGlobalFlaps) ifFalse: [use _ false].
+	use _ Flaps sharedFlapsAllowed.
 	CurrentProjectRefactoring currentFlapsSuppressed ifTrue: [use _ false].
 	"Smalltalk isMorphic ifFalse: [use _ false]."
 	thisWorld _ use 
 		ifTrue: [self]
-		ifFalse: [(PasteUpMorph new) initForProject:  "fake to be flap owner"
+		ifFalse: [PasteUpMorph new initForProject:  "fake to be flap owner"
 						WorldState new;
 					bounds: (0@0 extent: 4000@4000);
 					viewBox: (0@0 extent: 4000@4000)].
 	
-	Utilities globalFlapTabs do: [:aFlapTab |
+	Flaps globalFlapTabsIfAny do: [:aFlapTab |
 		(CurrentProjectRefactoring isFlapEnabled: aFlapTab) ifTrue:
 			[(aFlapTab world == thisWorld) ifFalse:
 				[thisWorld addMorphFront: aFlapTab.
 				aFlapTab adaptToWorld: thisWorld].	"always do"
-			use ifTrue: [
-				aFlapTab spanWorld.
+			use ifTrue:
+				[aFlapTab spanWorld.
 				aFlapTab adjustPositionAfterHidingFlap.
-				aFlapTab flapShowing ifTrue: [aFlapTab showFlap]]]].
-
-"
-	Utilities clobberFlapTabList.
-	Utilities initializeStandardFlaps.
-	self currentWorld deleteAllFlapArtifacts.
-	self currentWorld addGlobalFlaps.
-"
+				aFlapTab flapShowing ifTrue: [aFlapTab showFlap]]]]
