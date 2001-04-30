5e2a3ce300a638d8ab399b08f4bf64403c05567e
di <unknown>
2001-04-30 11:28:00 +0000

    no message

---
+++ Utilities/class/fetching updates/updateComment.cs
@@ -0,0 +1,19 @@
+updateComment
+"The following used to be at the beginning of the update file.
+	Now it is here to simplify parsing the file...
+
+* To add a new update:  Name it starting with a new four-digit code.  
+* Do not use %, /, *, space, or more than one period in the name of an update file.
+* The update name does not need to have any relation to the version name.
+* Figure out which versions of the system the update makes sense for.
+* Add the name of the file to each version's category below.
+* Put this file and the update file on all of the servers.
+*
+* To make a new version of the system:  Pick a name for it (no restrictions)
+* Put # and exactly that name on a new line at the end of this file.
+* During the release process, fill in exactly that name in the dialog box.
+* Put a copy of updates.list on the server.
+*
+* Special file with a different name for Disney Internal Updates.  
+* No need to move or rename files to release them to external updates.
+"
