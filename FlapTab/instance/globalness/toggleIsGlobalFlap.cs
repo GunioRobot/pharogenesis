8b54217f039b836f46e1812fa0c48bff4dc74ca6
sw <unknown>
2001-04-30 18:52:00 +0000

    no message

--- FlapTab/instance/globalness/toggleIsGlobalFlap.cs
+++ FlapTab/instance/globalness/toggleIsGlobalFlap.cs
@@ -2,14 +2,15 @@ toggleIsGlobalFlap
 	"Toggle whether the receiver is currently a global flap or not"
 
 	| oldWorld |
-	self hideFlap.  "In case showing"
+	self hideFlap.
 	oldWorld _ self currentWorld.
 	self isGlobalFlap
 		ifTrue:
-			[Utilities removeFromGlobalFlapTabList: self.
+			[Flaps removeFromGlobalFlapTabList: self.
 			oldWorld addMorphFront: self]
 		ifFalse:
 			[self delete.
-			Utilities addGlobalFlap: self.
-			self currentWorld addGlobalFlaps]
+			Flaps addGlobalFlap: self.
+			self currentWorld addGlobalFlaps].
+	ActiveWorld reformulateUpdatingMenus
 		
