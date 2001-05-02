ec4fc175423e55e5a2d028fda85fe55c45d995ae
RAA <unknown>
2001-05-02 14:13:00 +0000

    no message

--- TextPlusMorph/instance/as yet unclassified/jumpToDocumentTop.cs
+++ TextPlusMorph/instance/as yet unclassified/jumpToDocumentTop.cs
@@ -2,5 +2,4 @@ jumpToDocumentTop
 
 	self editor selectFrom: 1 to: 0.
 	self changed.
-	scrollerOwner ifNil: [^self].
-	scrollerOwner scrollSelectionIntoView: nil alignTop: true.
+	self scrollSelectionToTop.
