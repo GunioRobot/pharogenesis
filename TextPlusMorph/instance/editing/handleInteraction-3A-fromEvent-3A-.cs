7e47dd9d23badc7437c4ff7713e976492eb1861a
RAA <unknown>
2001-05-03 17:35:00 +0000

    no message

---
+++ TextPlusMorph/instance/editing/handleInteraction-3A-fromEvent-3A-.cs
@@ -0,0 +1,7 @@
+handleInteraction: interactionBlock fromEvent: evt
+	
+	super handleInteraction: interactionBlock fromEvent: evt.
+	(self parentGeeMail ifNil: [^self])
+		scrollSelectionIntoView: evt 
+		alignTop: false 
+		inTextMorph: self.
