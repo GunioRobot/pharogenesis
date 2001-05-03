2a0522143da802cbe4b0863a9700b1032668ae4b
ar <unknown>
2001-05-03 13:02:00 +0000

    no message

--- FloatArray/instance/comparing/hash.cs
+++ FloatArray/instance/comparing/hash.cs
@@ -1,7 +1,6 @@
 hash
 	| result |
-"WARNING: THE PRIMITIVE SEEMS TO BE BROKEN"
-	"<primitive:'primitiveFloatArrayHash'>"
+	<primitive:'primitiveHashArray' module: 'FloatArrayPlugin'>
 	result _ 0.
 	1 to: self size do:[:i| result _ result + (self basicAt: i) ].
 	^result bitAnd: 16r1FFFFFFF
