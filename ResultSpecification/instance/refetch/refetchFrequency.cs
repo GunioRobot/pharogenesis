309f88e2c406c67a762583cc8f1e4403ba016e7e
sw <unknown>
2001-05-03 00:07:00 +0000

    no message

---
+++ ResultSpecification/instance/refetch/refetchFrequency.cs
@@ -0,0 +1,4 @@
+refetchFrequency
+	"Answer the frequency with which the receiver should be refetched by a readout polling values from it, as in a Viewer.  Answer nil if not ever to be refetched automatically"
+
+	^ refetchFrequency
