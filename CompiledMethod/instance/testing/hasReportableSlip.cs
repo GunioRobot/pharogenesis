48b9163d3c4b2e8cf060a127707c421f4c97c8d4
sw <unknown>
2001-05-03 15:06:00 +0000

    no message

--- CompiledMethod/instance/testing/hasReportableSlip.cs
+++ CompiledMethod/instance/testing/hasReportableSlip.cs
@@ -1,8 +1,8 @@
 hasReportableSlip
-	"Answer whether the receiver contains anything that might be brought to the attention of the author when filing out.   Customize the lists to suit your preferences.  If slips do not get reported in spite of your best efforts here, make certain that the Preference 'suppressCheckForSlips' has not been hard-coded to true."
+	"Answer whether the receiver contains anything that should be brought to the attention of the author when filing out.   Customize the lists here to suit your preferences.  If slips do not get reported in spite of your best efforts here, make certain that the Preference 'checkForSlips' is set to true."
 
 	| assoc | 
-	#(halt halt: urgent hottest) do:
+	#(doOnlyOnce: halt halt: hottest printDirectlyToDisplay toRemove personal urgent) do:
 		[:aLit | (self hasLiteral: aLit) ifTrue: [^ true]].
 
 	#(Transcript AA BB CC DD EE) do:
