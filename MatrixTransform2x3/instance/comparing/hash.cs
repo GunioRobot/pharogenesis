7008087d778f8d10b98daedf399f6aea662501c1
ar <unknown>
2001-05-03 13:02:00 +0000

    no message

--- MatrixTransform2x3/instance/comparing/hash.cs
+++ MatrixTransform2x3/instance/comparing/hash.cs
@@ -1,6 +1,6 @@
 hash
 	| result |
-	<primitive: 'primitiveHash' module: 'FloatArrayPlugin'>
+	<primitive: 'primitiveHashArray' module: 'FloatArrayPlugin'>
 	result _ 0.
 	1 to: self size do:[:i| result _ result + (self basicAt: i) ].
 	^result bitAnd: 16r1FFFFFFF
