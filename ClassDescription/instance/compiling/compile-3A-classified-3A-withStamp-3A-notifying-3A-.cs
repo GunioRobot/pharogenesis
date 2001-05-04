e636f01199148949ceddc018db52622256b2e446
di <unknown>
2001-05-04 11:35:00 +0000

    no message

--- ClassDescription/instance/compiling/compile-3A-classified-3A-withStamp-3A-notifying-3A-.cs
+++ ClassDescription/instance/compiling/compile-3A-classified-3A-withStamp-3A-notifying-3A-.cs
@@ -1,22 +1,2 @@
-compile: text classified: category withStamp: changeStamp notifying: requestor 
-	| selector priorMethod method methodNode newText |
-	method _ self
-		compile: text asString
-		notifying: requestor
-		trailer: #(0 0 0 0)
-		ifFail: [^nil]
-		elseSetSelectorAndNode: 
-			[:sel :node | selector _ sel.
-			priorMethod _ self methodDict at: selector ifAbsent: [nil].
-			methodNode _ node].
-	self acceptsLoggingOfCompilation ifTrue:
-		[newText _ ((requestor == nil or: [requestor isKindOf: SyntaxError]) not
-						and: [Preferences confirmFirstUseOfStyle])
-			ifTrue: [text askIfAddStyle: priorMethod req: requestor]
-			ifFalse: [text].
-		 method putSource: newText
-				fromParseNode: methodNode
-				class: self category: category withStamp: changeStamp 
-				inFile: 2 priorMethod: priorMethod].
-	self organization classify: selector under: category.
-	^selector
+compile: text classified: category withStamp: changeStamp notifying: requestor
+	^ self compile: text classified: category withStamp: changeStamp notifying: requestor logSource: self acceptsLoggingOfCompilation
