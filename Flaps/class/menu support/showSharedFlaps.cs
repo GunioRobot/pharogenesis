6d2eb47e40e25bcb2e94db91a191472dabe02063
sw <unknown>
2001-05-04 23:14:00 +0000

    no message

---
+++ Flaps/class/menu support/showSharedFlaps.cs
@@ -0,0 +1,4 @@
+showSharedFlaps
+	"Answer whether shared flaps are currently showing.  Presumably it is in service of Alan's wishes to have flaps show sometimes on interior subprojects and sometomes on outer projects that Bob's CurrentProjectRefactoring is threaded into the logic here."
+
+	^ CurrentProjectRefactoring showSharedFlaps
