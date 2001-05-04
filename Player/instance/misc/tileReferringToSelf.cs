2b53a4518a3a9bee31c6859a31cea8456d79c81d
sw <unknown>
2001-05-04 07:12:00 +0000

    no message

--- Player/instance/misc/tileReferringToSelf.cs
+++ Player/instance/misc/tileReferringToSelf.cs
@@ -1,21 +1,17 @@
 tileReferringToSelf
-	| aTile ww nn tile |
+	"answer a tile that refers to the receiver"
 
-	"universal tiles"
-	ww _ costume bestGuessOfCurrentWorld.
-	(ww valueOfProperty: #universalTiles ifAbsent: [false]) ifTrue: [
-		nn _ self externalName. 	"name it, if necessary, and put in References"
+	| aTile  nn tile |
+	Preferences universalTiles ifTrue:
+		[nn _ self externalName. 	"name it, if necessary, and put in References"
 		(References includesKey: nn asSymbol) ifFalse: [
 			 References at: nn asSymbol put: self].
 		tile _ SyntaxMorph new parseNode: 
 			(VariableNode new name: nn key: nn code: nil).
-		tile layoutInset: 1; addMorph: (tile addString: nn).
+		tile layoutInset: 1; addMorph: (tile addString: nn special: false).
 		tile color: (SyntaxMorph translateColor: #variable).
 		tile extent: tile firstSubmorph extent + (2@2).
 		^ tile].
 
-	aTile _ TileMorph new
-		setObjectRef: nil "disused parm" actualObject: self;
-		typeColor: (ScriptingSystem colorForType: #player).
-	aTile enforceTileColorPolicy.
+	aTile _ TileMorph new setToReferTo: self.
 	^ aTile
