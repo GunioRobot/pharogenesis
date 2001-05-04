da0c23e2bfc4f8c123fb10bdcaeec4a3809b0b8a
sw <unknown>
2001-05-04 23:22:00 +0000

    no message

---
+++ CurrentProjectRefactoring/class/flaps/showSharedFlaps.cs
@@ -0,0 +1,4 @@
+showSharedFlaps
+	"Answer whether shared flaps are currently showing (true) or suppressed (false).  The CurrentProjectRefactoring circumlocution is in service of making it possible for shared flaps to appear on the edges of an interior subworld, I believe."
+
+	^ self xxxCurrent showSharedFlaps
