1c8e6aeb1e00a5c37f1e87a141c2f4bd205c64df
sw <unknown>
2001-05-05 00:27:00 +0000

    no message

--- PasteUpMorph/instance/flaps/deleteGlobalFlapArtifacts.cs
+++ PasteUpMorph/instance/flaps/deleteGlobalFlapArtifacts.cs
@@ -1,9 +1,12 @@
 deleteGlobalFlapArtifacts
-	"self currentWorld deleteGlobalFlapArtifacts"
+	"Delete all flap-related detritus from the world"
 
 	| localFlaps |
 	localFlaps _ self localFlapTabs collect: [:m | m referent].
 	self submorphs do:
 		[:m | 
-			((m isFlapTab) and: [m isGlobal]) ifTrue: [m delete].
+			((m isFlapTab) and: [m isGlobalFlap]) ifTrue: [m delete].
 			m isFlap ifTrue:[(localFlaps includes: m) ifFalse: [m delete]]]
+
+"ActiveWorld deleteGlobalFlapArtifacts"
+
