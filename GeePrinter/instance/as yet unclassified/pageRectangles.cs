a078bbd7f0954a21431f73ca9f49f19cef3b1a0a
RAA <unknown>
2001-05-07 13:32:00 +0000

    no message

--- GeePrinter/instance/as yet unclassified/pageRectangles.cs
+++ GeePrinter/instance/as yet unclassified/pageRectangles.cs
@@ -1,7 +1,17 @@
 pageRectangles
 
-	| pageBounds allPageRects |
+	| pageBounds allPageRects maxExtent |
 
+	geeMail ifNotNil: [
+		allPageRects _ geeMail pageRectanglesForPrinting.
+		allPageRects ifNotNil: [
+			maxExtent _ allPageRects inject: 0@0 into: [ :max :each |
+				max max: each extent
+			].
+			computedBounds _ 0@0 extent: maxExtent.
+			^allPageRects
+		].
+	].
 	pageBounds _ self bounds.
 	allPageRects _ OrderedCollection new.
 	[pageBounds top <= pasteUp bottom] whileTrue: [
