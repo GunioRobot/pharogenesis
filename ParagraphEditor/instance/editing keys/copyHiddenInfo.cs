fdbbd3cbfac82e5eb054150019e8a9eb86be99d9
tk <unknown>
2001-05-07 08:47:00 +0000

    no message

--- ParagraphEditor/instance/editing keys/copyHiddenInfo.cs
+++ ParagraphEditor/instance/editing keys/copyHiddenInfo.cs
@@ -3,14 +3,4 @@ copyHiddenInfo
 info.  Copy that to the clipboard.  You can paste it and see what it is.
 Usually enclosed in <>."
 
-	| attrList |
-	attrList _ paragraph text attributesAt: (startBlock stringIndex +
-stopBlock stringIndex)//2 forStyle: paragraph textStyle.
-	attrList do: [:attr |
-		(attr isKindOf: TextAction) ifTrue:
-			[^ self clipboardTextPut: ('<', attr info, '>') asText]].
-	"If none of the above"
-	attrList do: [:attr |
-		attr class == TextColor ifTrue:
-			[^ self clipboardTextPut: attr color printString asText]].
-	^ self clipboardTextPut: '[No hidden info]' asText
+	^ self clipboardTextPut: self hiddenInfo asText
