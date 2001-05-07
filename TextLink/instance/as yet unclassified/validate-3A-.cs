fc1460a12189087caf1aa8e549a5f9ba68bf9bd9
tk <unknown>
2001-05-07 09:30:00 +0000

    no message

--- TextLink/instance/as yet unclassified/validate-3A-.cs
+++ TextLink/instance/as yet unclassified/validate-3A-.cs
@@ -3,6 +3,11 @@ validate: specString
 
 	| list first mid last |
 	list _ specString findTokens: ' 	.|'.
+	last _ list last.
+	last first isUppercase ifTrue: [
+		(#('Comment' 'Definition' 'Hierarchy') includes: last) ifFalse: [^ nil].
+		"Check for 'Rectangle Comment Comment' and remove last one"
+		(list at: list size - 1) = last ifTrue: [list _ list allButLast]].
 	list size > 3 ifTrue: [^ nil].
 	list size < 2 ifTrue: [^ nil].
 	Symbol hasInterned: list first ifTrue: [:sym | first _ sym].
@@ -12,7 +17,4 @@ validate: specString
 		ifTrue: [(list at: 2) = 'class' ifTrue: ['class '] ifFalse: [^ nil]]
 		ifFalse: [''].
 	"OK if method name is not interned -- may not be defined yet"
-	last _ list last.
-	last first isUppercase ifTrue: [
-		(#('Comment' 'Definition' 'Hierarchy') includes: last) ifFalse: [^ nil]].
 	^ first, ' ', mid, last
