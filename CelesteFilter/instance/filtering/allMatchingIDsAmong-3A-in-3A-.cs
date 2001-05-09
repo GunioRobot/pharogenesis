35b998c8aff57567c246c7d6b1d1f3b2ff8b6ffe
ls <unknown>
2001-05-09 19:09:00 +0000

    no message

---
+++ CelesteFilter/instance/filtering/allMatchingIDsAmong-3A-in-3A-.cs
@@ -0,0 +1,3 @@
+allMatchingIDsAmong: messageIDs in: aMailDB
+	"return a list of all message ID's in messageIDs that match the filter; the message ID's are for messages in aMailDB"
+	^self subclassResponsibility
