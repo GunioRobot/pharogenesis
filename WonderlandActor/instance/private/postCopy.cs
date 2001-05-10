b8e49c4913a1b3cfd322f547f0651e220fae47fe
ar <unknown>
2001-05-10 20:22:00 +0000

    no message

--- WonderlandActor/instance/private/postCopy.cs
+++ WonderlandActor/instance/private/postCopy.cs
@@ -1,5 +1,10 @@
 postCopy
 	| props |
+	myName _ myName copy.
+	myMaterial _ myMaterial copy.
+	myColor _ myColor copy.
+	myReactions _ myReactions copy.
+	myMesh _ myMesh shallowCopy.
 	composite _ composite copy.
 	scaleMatrix _ scaleMatrix copy.
 	props _ myProperties.
