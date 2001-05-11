09bfaa11b99b76764c341e30089d09450a1b29f2
ar <unknown>
2001-05-11 23:47:00 +0000

    no message

--- SystemWindow/instance/open-2F-close/openInWorld-3A-.cs
+++ SystemWindow/instance/open-2F-close/openInWorld-3A-.cs
@@ -1,6 +1,4 @@
 openInWorld: aWorld
 	"This msg and its callees result in the window being activeOnlyOnTop"
 	self bounds: (RealEstateAgent initialFrameFor: self world: aWorld).
-	aWorld addMorph: self.
-	self activate.
-	aWorld startSteppingSubmorphsOf: self.
+	^self openAsIsIn: aWorld
