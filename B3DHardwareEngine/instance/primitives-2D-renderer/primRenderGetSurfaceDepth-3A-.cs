ab9a032414dc61203b0db987280b70edae2ae2e5
ar <unknown>
2001-05-14 21:34:00 +0000

    no message

---
+++ B3DHardwareEngine/instance/primitives-2D-renderer/primRenderGetSurfaceDepth-3A-.cs
@@ -0,0 +1,4 @@
+primRenderGetSurfaceDepth: aHandle
+	"Primitive. If this renderer is associated with a surface that we can blt directly to, return the surface height for it."
+	<primitive:'primitiveGetRendererSurfaceDepth' module: 'B3DAcceleratorPlugin'>
+	^nil
