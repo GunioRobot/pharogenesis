93902b0c33582c70b4c6969193cfd20be10da586
tpr <unknown>
2001-05-14 12:14:00 +0000

    no message

---
+++ BalloonEngineBase/class/translation/shouldBeTranslated.cs
@@ -0,0 +1,3 @@
+shouldBeTranslated
+"BalloonEnginePlugin should be translated but its superclasse should not since it is incorporated within this class's translation process. Nor should the simulation subclass be translated"
+	^self == BalloonEnginePlugin
