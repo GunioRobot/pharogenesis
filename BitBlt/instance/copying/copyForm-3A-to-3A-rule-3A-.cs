417bc00efdbda420c4cdd8735ee7495ea71ab294
ar <unknown>
2001-05-14 23:32:00 +0000

    no message

--- BitBlt/instance/copying/copyForm-3A-to-3A-rule-3A-.cs
+++ BitBlt/instance/copying/copyForm-3A-to-3A-rule-3A-.cs
@@ -1,3 +1,3 @@
 copyForm: srcForm to: destPt rule: rule
 	^ self copyForm: srcForm to: destPt rule: rule
-		colorMap: (srcForm colormapIfNeededForDepth: destForm depth)
+		colorMap: (srcForm colormapIfNeededFor: destForm)
