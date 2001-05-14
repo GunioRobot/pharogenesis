db57d783660142a14ff0a4bdaeb3e5fd56636b69
ar <unknown>
2001-05-14 21:09:00 +0000

    no message

---
+++ B3DAcceleratorPlugin/instance/primitives-2D-renderer/primitiveGetRendererSurfaceHandle.cs
@@ -0,0 +1,11 @@
+primitiveGetRendererSurfaceHandle
+	| handle result |
+	self export: true.
+	interpreterProxy methodArgumentCount = 1
+		ifFalse:[^interpreterProxy primitiveFail].
+	handle _ interpreterProxy stackIntegerValue: 0.
+	interpreterProxy failed ifTrue:[^nil].
+	result _ self cCode:'b3dxGetRendererSurfaceHandle(handle)' inSmalltalk:[-1].
+	result < 0 ifTrue:[^interpreterProxy primitiveFail].
+	interpreterProxy pop: 2. "args+rcvr"
+	^interpreterProxy pushInteger: result
