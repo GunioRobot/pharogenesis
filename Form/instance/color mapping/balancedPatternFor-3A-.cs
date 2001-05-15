594fce9b8ea01a70c7cbc5113ca06e4ea6575d96
ar <unknown>
2001-05-15 16:16:00 +0000

    no message

--- Form/instance/color mapping/balancedPatternFor-3A-.cs
+++ Form/instance/color mapping/balancedPatternFor-3A-.cs
@@ -1,5 +1,5 @@
 balancedPatternFor: aColor
 	"Return the pixel word for representing the given color on the receiver"
-	self isExternalForm
+	self hasNonStandardPalette
 		ifTrue:[^self bitPatternFor: aColor]
 		ifFalse:[^aColor balancedPatternForDepth: self depth]
