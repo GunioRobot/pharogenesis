05bf6b663b45e7d34d3f4ff1680922936159117c
ar <unknown>
2001-05-15 18:39:00 +0000

    no message

---
+++ B3DHardwareEngine/instance/primitives-2D-rendering/primFinish-3A-.cs
@@ -0,0 +1,5 @@
+primFinish: aHandle
+	"Primitive. Finish all rendering operations on the receiver.
+	Do not return before all rendering operations have taken effect."
+	<primitive: 'primitiveFinishRenderer' module:'B3DAcceleratorPlugin'>
+	^nil
