a22ff0e1b4f71040dee5df0c1e3c9212c3cdd74b
ls <unknown>
2001-05-15 22:31:00 +0000

    no message

--- PluggableListMorph/instance/updating/update-3A-.cs
+++ PluggableListMorph/instance/updating/update-3A-.cs
@@ -2,7 +2,7 @@ update: aSymbol
 	"Refer to the comment in View|update:."
 
 	aSymbol == getListSelector ifTrue: 
-		[self list: self getList.
+		[self updateList.
 		^ self].
 	aSymbol == getIndexSelector ifTrue:
 		[self selectionIndex: self getCurrentSelectionIndex.
