ab540b9d8ee9a01867f8ebe41bff7a80c0c9cde3
ar <unknown>
2001-05-15 16:12:00 +0000

    no message

--- ColorMap/instance/pixel mapping/pixelMap-3A-.cs
+++ ColorMap/instance/pixel mapping/pixelMap-3A-.cs
@@ -5,8 +5,10 @@ pixelMap: pixelValue
 		ifTrue:[pv _ pixelValue]
 		ifFalse:[pv _ colors at: pixelValue].
 	(shifts == nil and:[masks == nil]) 
-		ifTrue:[^pv]
-		ifFalse:[^(((pv bitAnd: self redMask) bitShift: self redShift) bitOr: 
+		ifFalse:[pv _ (((pv bitAnd: self redMask) bitShift: self redShift) bitOr: 
 				((pv bitAnd: self greenMask) bitShift: self greenShift)) bitOr:
 					(((pv bitAnd: self blueMask) bitShift: self blueShift) bitOr: 
-						((pv bitAnd: self alphaMask) bitShift: self alphaShift))]
+						((pv bitAnd: self alphaMask) bitShift: self alphaShift))].
+	"Need to check for translucency else Form>>paint goes gaga"
+	pv = 0 ifTrue:[pixelValue = 0 ifFalse:[pv _ 1]].
+	^pv
