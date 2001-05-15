e62e6c3386c420d9e62245f3456b4efb7a44a9d1
ls <unknown>
2001-05-15 22:22:00 +0000

    no message

--- PluggableMultiColumnListMorph/instance/initialization/createMorphicListsFrom-3A-.cs
+++ PluggableMultiColumnListMorph/instance/initialization/createMorphicListsFrom-3A-.cs
@@ -1,15 +1,14 @@
 createMorphicListsFrom: arrayOfLists 
 	| array |
-	font
-		ifNil: [font _ Preferences standardListFont].
+
 	array _ Array new: arrayOfLists size.
 	1 to: arrayOfLists size do: [:arrayIndex |
 		array at: arrayIndex put: (
 			(arrayOfLists at: arrayIndex) collect: [:item | item isText
 						ifTrue: [StringMorph
 								contents: item
-								font: font
+								font: self font
 								emphasis: (item emphasisAt: 1)]
-						ifFalse: [StringMorph contents: item font: font]])
+						ifFalse: [StringMorph contents: item font: self font]])
 		].
 	^array
