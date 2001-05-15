96a64d9936a4311f0922342cf1feaca1e56572c7
ar <unknown>
2001-05-15 18:38:00 +0000

    no message

---
+++ B3DHardwareEngine/instance/primitives-2D-rendering/primRenderVB-3A-primitive-3A-flags-3A-texture-3A-vertices-3A-vertexCount-3A-faces-3A-faceCount-3A-.cs
@@ -0,0 +1,3 @@
+primRenderVB: aHandle primitive: primitiveType flags: flags texture: textureHandle vertices: vtxArray vertexCount: vtxCount faces: idxArray faceCount: idxCount
+	<primitive:'primitiveRenderVertexBuffer' module:'B3DAcceleratorPlugin'>
+	^nil
