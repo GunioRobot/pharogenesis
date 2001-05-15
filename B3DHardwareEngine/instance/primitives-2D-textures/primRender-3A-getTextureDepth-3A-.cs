95d710a77560e1871fd353dee83b009786df6833
ar <unknown>
2001-05-15 18:40:00 +0000

    no message

---
+++ B3DHardwareEngine/instance/primitives-2D-textures/primRender-3A-getTextureDepth-3A-.cs
@@ -0,0 +1,4 @@
+primRender: rendererHandle getTextureDepth: textureHandle
+	"Primitive. Return the actual depth of the texture with the given handle"
+	<primitive:'primitiveTextureDepth' module:'B3DAcceleratorPlugin'>
+	^nil
