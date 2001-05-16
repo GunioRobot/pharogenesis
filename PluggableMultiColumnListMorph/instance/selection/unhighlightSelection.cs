c31d711809a67bd0be7abcd36b1300fdf9cd6ce6
ls <unknown>
2001-05-16 22:23:00 +0000

    no message

--- PluggableMultiColumnListMorph/instance/selection/unhighlightSelection.cs
+++ PluggableMultiColumnListMorph/instance/selection/unhighlightSelection.cs
@@ -1,17 +1,2 @@
 unhighlightSelection
-	| unhighlightColor columnLength localIndex m previousSelectedIndex |
-	unhighlightColor _ Color black.
-	selectedMorph
-		ifNotNil: [selectedMorph color: unhighlightColor;
-				 changed.
-			previousSelectedIndex _ scroller submorphs
-						findFirst: [:mp | mp contents = selectedMorph contents].
-			columnLength _ lists first size.
-			2
-				to: lists size
-				do: [:columnIndex | 
-					localIndex _ previousSelectedIndex + (columnLength * (columnIndex - 1)).
-					localIndex = 0 ifFalse: [
-						m _ scroller submorphs asArray at: localIndex.
-						m color: unhighlightColor;
-						 	changed]]]
+^self
