fba4668b03ad5bcc04fb3ec694bda685b07efc32
ls <unknown>
2001-05-16 22:24:00 +0000

    no message

--- PluggableMultiColumnListMorph/instance/selection/highlightSelection.cs
+++ PluggableMultiColumnListMorph/instance/selection/highlightSelection.cs
@@ -1,18 +1,2 @@
 highlightSelection
-	| highlightColor columnLength localIndex m properSelectedIndex |
-	highlightColor _ Color red.
-	"<-- Change the highlight color here."
-	selectedMorph
-		ifNotNil: [selectedMorph color: highlightColor;
-				 changed.
-			properSelectedIndex _ scroller submorphs
-						findFirst: [:mp | mp contents = selectedMorph contents].
-			selectedIndex = properSelectedIndex ifFalse: [selectedIndex _ properSelectedIndex].
-			columnLength _ lists first size.
-			2
-				to: lists size
-				do: [:columnIndex | 
-					localIndex _ selectedIndex + (columnLength * (columnIndex - 1)).
-					m _ scroller submorphs asArray at: localIndex.
-					m color: highlightColor;
-						 changed]]
+^self
