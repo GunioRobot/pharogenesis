fce286095fb6925e382b1f57ab85d2aa5ab61ebb
JW <unknown>
2001-05-17 07:54:00 +0000

    no message

--- ScorePlayerMorph/instance/layout/showResumeButtonInTheWorld.cs
+++ ScorePlayerMorph/instance/layout/showResumeButtonInTheWorld.cs
@@ -3,7 +3,10 @@ showResumeButtonInTheWorld
 	| w |
 
 	WorldState addDeferredUIMessage: [
-		w _ self world ifNil: [^self].
-		w addMorphFront: (self standaloneResumeButton position: (w right - 100) @ (w top + 10)).
-		scorePlayer pause.
+		w _ self world.
+		w ifNotNil: [
+			w addMorphFront:
+				(self standaloneResumeButton position: (w right - 100) @ (w top + 10)).
+			scorePlayer pause.
+			].
 	]
