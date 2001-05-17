7a96ce2b96e80d0415abcb4ef20436a7c7a1c16c
ar <unknown>
2001-05-17 13:58:00 +0000

    no message

--- BitBltSimulation/instance/interpreter interface/loadBitBltDestForm.cs
+++ BitBltSimulation/instance/interpreter interface/loadBitBltDestForm.cs
@@ -7,18 +7,20 @@ loadBitBltDestForm
 	destHeight _ interpreterProxy fetchInteger: FormHeightIndex ofObject: destForm.
 	(destWidth >= 0 and: [destHeight >= 0])
 		ifFalse: [^ false].
-	destPixSize _ interpreterProxy fetchInteger: FormDepthIndex ofObject: destForm.
+	destDepth _ interpreterProxy fetchInteger: FormDepthIndex ofObject: destForm.
+	destMSB _ destDepth > 0.
+	destDepth < 0 ifTrue:[destDepth _ 0 - destDepth].
 	"Ignore an integer bits handle for Display in which case 
 	the appropriate values will be obtained by calling ioLockSurfaceBits()."
 	(interpreterProxy isIntegerObject: destBits) ifTrue:[
 		"Query for actual surface dimensions"
 		(self queryDestSurface: (interpreterProxy integerValueOf: destBits))
 			ifFalse:[^false].
-		pixPerWord _ 32 // destPixSize.
+		destPPW _ 32 // destDepth.
 		destBits _ destPitch _ 0.
 	] ifFalse:[
-		pixPerWord _ 32 // destPixSize.
-		destPitch _ destWidth + (pixPerWord-1) // pixPerWord * 4.
+		destPPW _ 32 // destDepth.
+		destPitch _ destWidth + (destPPW-1) // destPPW * 4.
 		destBitsSize _ interpreterProxy byteSizeOf: destBits.
 		((interpreterProxy isWordsOrBytes: destBits)
 			and: [destBitsSize = (destPitch * destHeight)])
