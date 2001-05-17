87a948a97e142d1cf72126bb3a1cc53898cbb199
ar <unknown>
2001-05-17 15:40:00 +0000

    no message

--- Form/instance/image manipulation/replaceColor-3A-withColor-3A-.cs
+++ Form/instance/image manipulation/replaceColor-3A-withColor-3A-.cs
@@ -2,12 +2,12 @@ replaceColor: oldColor withColor: newColor
 	"Replace one color with another everywhere is this form"
 
 	| cm newInd target ff |
-	depth = 32
+	self depth = 32
 		ifTrue: [cm _ (Color  cachedColormapFrom: 16 to: 32) copy]
-		ifFalse: [cm _ Bitmap new: (1 bitShift: (depth min: 15)).
+		ifFalse: [cm _ Bitmap new: (1 bitShift: (self depth min: 15)).
 				1 to: cm size do: [:i | cm at: i put: i - 1]].
-	newInd _ newColor pixelValueForDepth: depth.
-	cm at: (oldColor pixelValueForDepth: (depth min: 16))+1 put: newInd.
+	newInd _ newColor pixelValueForDepth: self depth.
+	cm at: (oldColor pixelValueForDepth: (self depth min: 16))+1 put: newInd.
 	target _ newColor isTransparent 
 		ifTrue: [ff _ Form extent: self extent depth: depth.
 			ff fillWithColor: newColor.  ff]
