cd8704e759ef3922730e3f9f77011af03062e4ae
ar <unknown>
2001-05-17 13:56:00 +0000

    no message

--- BitBltSimulation/instance/interpreter interface/loadBitBltSourceForm.cs
+++ BitBltSimulation/instance/interpreter interface/loadBitBltSourceForm.cs
@@ -1,27 +1,29 @@
 loadBitBltSourceForm
 	"Load the source form for BitBlt. Return false if anything is wrong, true otherwise."
-	| sourcePixPerWord sourceBitsSize |
+	| sourceBitsSize |
 	self inline: true.
 	sourceBits _ interpreterProxy fetchPointer: FormBitsIndex ofObject: sourceForm.
-	srcWidth _ self fetchIntOrFloat: FormWidthIndex ofObject: sourceForm.
-	srcHeight _ self fetchIntOrFloat: FormHeightIndex ofObject: sourceForm.
-	(srcWidth >= 0 and: [srcHeight >= 0])
+	sourceWidth _ self fetchIntOrFloat: FormWidthIndex ofObject: sourceForm.
+	sourceHeight _ self fetchIntOrFloat: FormHeightIndex ofObject: sourceForm.
+	(sourceWidth >= 0 and: [sourceHeight >= 0])
 		ifFalse: [^ false].
-	sourcePixSize _ interpreterProxy fetchInteger: FormDepthIndex ofObject: sourceForm.
+	sourceDepth _ interpreterProxy fetchInteger: FormDepthIndex ofObject: sourceForm.
+	sourceMSB _ sourceDepth > 0.
+	sourceDepth < 0 ifTrue:[sourceDepth _ 0 - sourceDepth].
 	"Ignore an integer bits handle for Display in which case 
 	the appropriate values will be obtained by calling ioLockSurfaceBits()."
 	(interpreterProxy isIntegerObject: sourceBits) ifTrue:[
 		"Query for actual surface dimensions"
 		(self querySourceSurface: (interpreterProxy integerValueOf: sourceBits))
 			ifFalse:[^false].
-		sourcePixPerWord _ 32 // sourcePixSize.
+		sourcePPW _ 32 // sourceDepth.
 		sourceBits _ sourcePitch _ 0.
 	] ifFalse:[
-		sourcePixPerWord _ 32 // sourcePixSize.
-		sourcePitch _ srcWidth + (sourcePixPerWord-1) // sourcePixPerWord * 4.
+		sourcePPW _ 32 // sourceDepth.
+		sourcePitch _ sourceWidth + (sourcePPW-1) // sourcePPW * 4.
 		sourceBitsSize _ interpreterProxy byteSizeOf: sourceBits.
 		((interpreterProxy isWordsOrBytes: sourceBits)
-			and: [sourceBitsSize = (sourcePitch * srcHeight)])
+			and: [sourceBitsSize = (sourcePitch * sourceHeight)])
 			ifFalse: [^ false].
 		"Skip header since external bits don't have one"
 		sourceBits _ self cCoerce: (interpreterProxy firstIndexableField: sourceBits) to:'int'.
