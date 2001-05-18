1c83d6e9e5a09de550d8dddd3bb04142faec528f
RAA <unknown>
2001-05-18 13:25:00 +0000

    no message

---
+++ UniclassScript/instance/initialize-2D-release/releaseCachedState.cs
@@ -0,0 +1,11 @@
+releaseCachedState
+	"release all non-showing scriptors.  What do we do about versions????"
+	"18 May 2001 - get more aggressive in dropping stuff"
+
+	formerScriptingTiles _ OrderedCollection new.
+
+	currentScriptEditor ifNil: [^ self].
+
+	true ifTrue: [^ self].	"<<< to test the reconstruction of scripts, change to false"
+	currentScriptEditor world ifNil: ["not showing"
+		currentScriptEditor _ nil]
