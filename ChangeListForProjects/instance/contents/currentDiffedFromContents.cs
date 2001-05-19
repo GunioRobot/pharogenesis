2db3c6bd2f714aeff99ce50d3a1515d66f2d5900
sw <unknown>
2001-05-19 11:06:00 +0000

    no message

---
+++ ChangeListForProjects/instance/contents/currentDiffedFromContents.cs
@@ -0,0 +1,16 @@
+currentDiffedFromContents
+	"Answer the current in-memory method diffed from the current contents"
+
+	| aChange aClass |
+	listIndex = 0
+		ifTrue: [^ ''].
+	aChange _ changeList at: listIndex.
+	^ ((aChange type == #method
+				and: [(aClass _ aChange methodClass) notNil])
+			and: [aClass includesSelector: aChange methodSelector])
+		ifTrue: [TextDiffBuilder
+				buildDisplayPatchFrom: aChange text
+				to: (aClass sourceCodeAt: aChange methodSelector)
+				inClass: aClass
+				prettyDiffs: self showingPrettyDiffs]
+		ifFalse: [(changeList at: listIndex) text]
