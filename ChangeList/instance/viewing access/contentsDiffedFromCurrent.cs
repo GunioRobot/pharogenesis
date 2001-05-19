58789ae678287531f3825748a855e50c145f61cc
sw <unknown>
2001-05-19 10:59:00 +0000

    no message

--- ChangeList/instance/viewing access/contentsDiffedFromCurrent.cs
+++ ChangeList/instance/viewing access/contentsDiffedFromCurrent.cs
@@ -1,10 +1,12 @@
 contentsDiffedFromCurrent
+	"Answer the contents diffed forward from current (in-memory) method version"
+
 	| aChange aClass |
 	listIndex = 0
 		ifTrue: [^ ''].
 	aChange _ changeList at: listIndex.
 	^ ((aChange type == #method and: [(aClass _ aChange methodClass) notNil]) and: [aClass includesSelector: aChange methodSelector])
 		ifTrue:
-			 [Utilities methodDiffFor: aChange text class: aClass selector: aChange methodSelector]
+			 [Utilities methodDiffFor: aChange text class: aClass selector: aChange methodSelector prettyDiffs: self showingPrettyDiffs]
 		ifFalse:
 			[(changeList at: listIndex) text]
