6e7b5cdd2ec7794de2da842726fd2cd6b2ede16c
ar <unknown>
2001-05-19 14:28:00 +0000

    no message

---
+++ Subdivision/instance/accessing/outlineThreshold.cs
@@ -0,0 +1,6 @@
+outlineThreshold
+	"Return the current outline threshold.
+	The outline threshold determines when to stop recursive
+	subdivision of outline edges in the case of non-simple
+	(that is self-intersecting) polygons."
+	^outlineThreshold
