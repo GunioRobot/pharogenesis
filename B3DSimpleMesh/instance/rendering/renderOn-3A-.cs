5e2fb4e37a1bd5127de309f6df58630dc23cea63
ar <unknown>
2001-05-20 00:08:00 +0000

    no message

--- B3DSimpleMesh/instance/rendering/renderOn-3A-.cs
+++ B3DSimpleMesh/instance/rendering/renderOn-3A-.cs
@@ -1,8 +1,4 @@
 renderOn: aRenderer
-	| box bounds |
-	box _ nil.
 	1 to: self size do:[:i|
-		bounds _ (self at: i) renderOn: aRenderer.
-		box == nil ifTrue:[box _ bounds] ifFalse:[box _ box quickMerge: bounds].
+		(self at: i) renderOn: aRenderer.
 	].
-	^box
