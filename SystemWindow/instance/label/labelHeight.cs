e9c167d1c3358c10159998374d7d9c996ff9ebfd
sw <unknown>
2001-05-20 22:34:00 +0000

    no message

--- SystemWindow/instance/label/labelHeight.cs
+++ SystemWindow/instance/label/labelHeight.cs
@@ -1,2 +1,10 @@
 labelHeight
-	^ label height + 1 max: collapseBox height
+	"Answer the height for the window label.  The standard behavior is at bottom; a hook is provided so that models can stipulate other heights, in support of various less-window-looking demos."
+
+	| aHeight |
+	(model notNil and: [model respondsTo: #desiredWindowLabelHeightIn:]) ifTrue:
+		[(aHeight _ model desiredWindowLabelHeightIn: self) ifNotNil: [^ aHeight]].
+
+	^ label ifNil: [0] ifNotNil:
+		 [(label height + 1) max:
+			(collapseBox ifNotNil: [collapseBox height] ifNil: [10])]
