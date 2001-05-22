a56a628577c73f106a1b321dde6e9e55f50e4953
tk <unknown>
2001-05-22 21:37:00 +0000

    no message

--- SelectorBrowser/instance/as yet unclassified/contents-3A-notifying-3A-.cs
+++ SelectorBrowser/instance/as yet unclassified/contents-3A-notifying-3A-.cs
@@ -1,14 +1,17 @@
 contents: aString notifying: aController
 	"Take what the user typed and find all selectors containing it"
 
-	| tokens |
+	| tokens raw sorted |
 	contents _ aString.
 	classList _ #().  classListIndex _ 0.
 	selectorIndex _ 0.
 	tokens _ contents asString findTokens: ' .'.
 	selectorList _ Cursor wait showWhile: [
 		tokens size = 1 
-			ifTrue: [(Symbol selectorsContaining: contents asString) asSortedArray]
+			ifTrue: [raw _ (Symbol selectorsContaining: contents asString).
+				sorted _ raw as: SortedCollection.
+				sorted sortBlock: [:x :y | x asLowercase <= y asLowercase].
+				sorted asArray]
 			ifFalse: [self quickList]].	"find selectors from a single example of data"
 	self changed: #messageList.
 	self changed: #classList.
