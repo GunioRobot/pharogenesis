57537005cd764e18cbb8ea108e3302ca18f350eb
sw <unknown>
2001-05-23 14:24:00 +0000

    no message

---
+++ WiWPasteUpMorph/instance/world state/jumpToProject.cs
@@ -0,0 +1,5 @@
+jumpToProject
+	"Jump directly to another project.  However, this is not currently allowed for inner worlds"
+
+	self inform: 'Project changes are not yet allowed
+from inner worlds.'
