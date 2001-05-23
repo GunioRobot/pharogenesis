81e0f066f5d57f339ff7b207ea61130a7efc91f5
sw <unknown>
2001-05-23 14:13:00 +0000

    no message

--- UserInputEvent/instance/modifier state/macOptionKeyPressed.cs
+++ UserInputEvent/instance/modifier state/macOptionKeyPressed.cs
@@ -1,7 +1,7 @@
 macOptionKeyPressed
 	"Answer whether the option key on the Macintosh keyboard was being held down when this event occurred. Macintosh specific."
 
-	Preferences macOptionKeyAllowed ifFalse: [self notify: 'Portability note:
+	Preferences macOptionKeyAllowed ifFalse: [self notifyWithLabel: 'Portability note:
 MorphicEvent>>macOptionKeyPressed is not portable.
 Please use MorphicEvent>>yellowButtonPressed instead!'].
 	^ buttons anyMask: 32
