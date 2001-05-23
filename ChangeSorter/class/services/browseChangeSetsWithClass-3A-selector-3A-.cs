6ead617a3e21abf30aa6e9b8e6c40a7e9ec929c6
sw <unknown>
2001-05-23 13:30:00 +0000

    no message

--- ChangeSorter/class/services/browseChangeSetsWithClass-3A-selector-3A-.cs
+++ ChangeSorter/class/services/browseChangeSetsWithClass-3A-selector-3A-.cs
@@ -1,8 +1,10 @@
 browseChangeSetsWithClass: class selector: selector
+	"Put up a menu comprising a list of change sets that hold changes for the given class and selector.  If the user selects one, open a single change-sorter onto it"
+
 	| hits index |
-	hits _ self gatherChangeSets select: 
+	hits _ self allChangeSets select: 
 		[:cs | (cs atSelector: selector class: class) ~~ #none].
-	hits isEmpty ifTrue: [^ PopUpMenu notify: class name,'.',selector , '
+	hits isEmpty ifTrue: [^ self inform: class name, '.', selector , '
 is not in any change set'].
 	index _ hits size == 1
 		ifTrue:	[1]
