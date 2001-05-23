772187139583b552fea53df22da0ce06ec9f43d5
sw <unknown>
2001-05-23 13:51:00 +0000

    no message

--- MorphicModel/class/compilation/chooseNewName.cs
+++ MorphicModel/class/compilation/chooseNewName.cs
@@ -1,5 +1,6 @@
 chooseNewName
-	"Rename this class."
+	"Choose a new name for the receiver, persisting until an acceptable name is provided or until the existing name is resubmitted"
+
 	| oldName newName |
 	oldName _ self name.
 		[newName _ (FillInTheBlank request: 'Please give this Model a name'
@@ -7,5 +8,5 @@ chooseNewName
 		newName = oldName ifTrue: [^ self].
 		Smalltalk includesKey: newName]
 		whileTrue:
-		[PopUpMenu notify: 'Sorry, that name is already in use.'].
+		[self inform: 'Sorry, that name is already in use.'].
 	self rename: newName.
