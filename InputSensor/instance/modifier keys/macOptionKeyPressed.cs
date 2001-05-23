b9e5e2c896a5537c61c884ee8903d1af11ab9425
sw <unknown>
2001-05-23 13:46:00 +0000

    no message

--- InputSensor/instance/modifier keys/macOptionKeyPressed.cs
+++ InputSensor/instance/modifier keys/macOptionKeyPressed.cs
@@ -1,7 +1,7 @@
 macOptionKeyPressed
 	"Answer whether the option key on the Macintosh keyboard is being held down. Macintosh specific."
 
-	Preferences macOptionKeyAllowed ifFalse: [self notify: 'Portability note:
+	Preferences macOptionKeyAllowed ifFalse: [self notifyWithLabel: 'Portability note:
 InputSensor>>macOptionKeyPressed is not portable.
 Please use InputSensor>>yellowButtonPressed instead!'].
 	^ self primMouseButtons anyMask: 32
