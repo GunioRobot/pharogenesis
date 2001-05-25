c5471b3e978e713fe802096de334c6d584cb0af4
sw <unknown>
2001-05-25 10:47:00 +0000

    no message

---
+++ InstanceBrowser/instance/initialization/desiredWindowLabelHeightIn-3A-.cs
@@ -0,0 +1,5 @@
+desiredWindowLabelHeightIn: aSystemWindow
+	"Answer the desired window label height.  To be exploited in due course"
+
+	self flag: #deferred.  "For tweaking appearance in due course"
+	^ nil
