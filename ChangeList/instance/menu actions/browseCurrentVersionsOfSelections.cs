b9d9d4f18fe7dd1c2495f1d142a7ba6e879926eb
RAA <unknown>
2001-05-28 11:37:00 +0000

    no message

--- ChangeList/instance/menu actions/browseCurrentVersionsOfSelections.cs
+++ ChangeList/instance/menu actions/browseCurrentVersionsOfSelections.cs
@@ -1,16 +1,24 @@
 browseCurrentVersionsOfSelections
 	"Opens a message-list browser on the current in-memory versions of all methods that are currently seleted"
 	|  aClass aChange aList |
+
 	aList _ OrderedCollection new.
-	Cursor read showWhile: 
-	[1 to: changeList size do:
-		[:i | (listSelections at: i) ifTrue:
-			 [aChange _ changeList at: i.
-			(aChange type = #method
-				and: [(aClass _ aChange methodClass) notNil
-				and: [aClass includesSelector: aChange methodSelector]])
-					ifTrue:
-						[aList add: aClass name, ' ', aChange methodSelector]]]].
+	Cursor read showWhile: [
+		1 to: changeList size do: [:i |
+			(listSelections at: i) ifTrue: [
+				aChange _ changeList at: i.
+				(aChange type = #method
+					and: [(aClass _ aChange methodClass) notNil
+					and: [aClass includesSelector: aChange methodSelector]])
+						ifTrue: [
+							aList add: (
+								MethodReference new
+									setStandardClass: aClass  
+									methodSymbol: aChange methodSelector
+							)
+						]]]].
 
 	aList size == 0 ifTrue: [^ self inform: 'no selected methods have in-memory counterparts'].
-	MessageSet openMessageList: aList name: 'Current versions of selected methods in ', file localName
+	MessageSet 
+		openMessageList: aList 
+		name: 'Current versions of selected methods in ', file localName
