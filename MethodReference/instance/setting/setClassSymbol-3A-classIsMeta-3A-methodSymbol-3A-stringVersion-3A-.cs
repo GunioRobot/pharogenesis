f2bb38f8015f9294453f41dd1516d010c1beb0ce
RAA <unknown>
2001-05-28 06:04:00 +0000

    no message

---
+++ MethodReference/instance/setting/setClassSymbol-3A-classIsMeta-3A-methodSymbol-3A-stringVersion-3A-.cs
@@ -0,0 +1,6 @@
+setClassSymbol: classSym classIsMeta: isMeta methodSymbol: methodSym stringVersion: aString 
+
+	classSymbol _ classSym.
+	classIsMeta _ isMeta.
+	methodSymbol _ methodSym.
+	stringVersion _ aString.
