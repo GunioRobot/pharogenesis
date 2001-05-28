e7e82393c70e846ca4adef86cb8909b166989b1b
RAA <unknown>
2001-05-28 11:07:00 +0000

    no message

--- ProtocolBrowser/instance/private/setClassAndSelectorIn-3A-.cs
+++ ProtocolBrowser/instance/private/setClassAndSelectorIn-3A-.cs
@@ -1,10 +1,16 @@
 setClassAndSelectorIn: csBlock
-	"Decode strings of the form    <selectorName> (<className> [class])  "
+	"Decode strings of the form    <selectorName> (<className> [class])"
 
-	| i classAndSelString sel |
-	sel _ self selection asString.
-	i _ sel indexOf: $(.
-	"Rearrange to  <className> [class] <selectorName> , and use MessageSet"
-	classAndSelString _ (sel copyFrom: i + 1 to: sel size - 1) , ' ' ,
-					(sel copyFrom: 1 to: i - 1) withoutTrailingBlanks.
-	MessageSet parse: classAndSelString toClassAndSelector: csBlock
+	| i classAndSelString selString sel |
+
+	sel _ self selection ifNil: [^ csBlock value: nil value: nil].
+	(sel isKindOf: MethodReference) ifTrue: [
+		sel setClassAndSelectorIn: csBlock
+	] ifFalse: [
+		selString _ sel asString.
+		i _ selString indexOf: $(.
+		"Rearrange to  <className> [class] <selectorName> , and use MessageSet"
+		classAndSelString _ (selString copyFrom: i + 1 to: selString size - 1) , ' ' ,
+							(selString copyFrom: 1 to: i - 1) withoutTrailingBlanks.
+		MessageSet parse: classAndSelString toClassAndSelector: csBlock.
+	].
