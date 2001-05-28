d82d48d2169a737c35f69ceb5a9772c2fe3ad056
ar <unknown>
2001-05-28 17:12:00 +0000

    no message

--- WonderlandCameraMorph/instance/pooh-2D-palette/clear.cs
+++ WonderlandCameraMorph/instance/pooh-2D-palette/clear.cs
@@ -1,3 +1,4 @@
 clear
 	currentActor ifNotNil: [
-		currentActor setTexturePointer: ((Form extent: 256@128 depth: Display depth) fillColor: Color white) asTexture]
+		currentActor getTexturePointer fillColor: Color white.
+	].
