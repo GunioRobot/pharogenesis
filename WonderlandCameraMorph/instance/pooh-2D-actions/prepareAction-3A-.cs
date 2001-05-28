f7d0579bd1d9f6f14ffe99febd81468a3984586d
ar <unknown>
2001-05-28 17:17:00 +0000

    no message

--- WonderlandCameraMorph/instance/pooh-2D-actions/prepareAction-3A-.cs
+++ WonderlandCameraMorph/instance/pooh-2D-actions/prepareAction-3A-.cs
@@ -1,7 +1,17 @@
 prepareAction: newEvent
-	|  range  |
+	|  range  palNib |
 	currentColor _ palette getColor.
-	currentNib _ palette getNib.
+	palNib _ palette getNib.
+	(currentNib notNil 
+		and:[currentNib extent = palNib extent 
+		and:[currentNib isColorForm 
+		and:[currentNib depth = 8 
+		and:[currentNib colors last = currentColor]]]]) ifFalse:[
+			currentNib _ self roundNibOfSize: (palette getNib extent).
+			self aaPaintingEnabled
+				ifTrue:[currentNib colors:((0 to: 255) collect:[:i| currentColor alpha: i / 255.0])]
+				ifFalse:[currentNib colors: ((Array new: 256) at: 1 put: Color transparent; from: 2 to: 256 put: currentColor; yourself)].
+		].
 	currentActor _ newEvent getActor.
 	currentCanvas _ (currentActor getTexturePointer getCanvas).
 	range _ currentCanvas extent.
