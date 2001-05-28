a95de6900cd3adcf624919686f120b627a3be3c0
ar <unknown>
2001-05-28 17:20:00 +0000

    no message

--- WonderlandCameraMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ WonderlandCameraMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,10 +1,11 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph 
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
-	aCustomMenu addUpdating: #snapshotBackgroundString action: #toggleSnapshotBackground.
-	aCustomMenu addUpdating: #getDragAndDropState action: #toggleDragAndDropState.
-	(myControls == nil)
-		
-		ifTrue:[aCustomMenu add: 'showCameraControls' action: #showCameraControls]
-ifFalse:[aCustomMenu add: 'hideCameraControls' action: #deleteCameraControls]
-		
-	
+	aCustomMenu addLine.
+	aCustomMenu addUpdating: #firstPersonControlString action: #toggleFirstPersonControl.
+	aCustomMenu addUpdating: #showControlsString action: #toggleCameraControls.
+	aCustomMenu addUpdating: #getAcceleratorState action: #toggleAcceleratorState.
+	aCustomMenu addLine.
+	aCustomMenu addUpdating: #getAAPaintState action: #toggleAAPaintState.
+	aCustomMenu addLine.
+	aCustomMenu add: 'Stop/Go button' action: #attachStopGoButtons.
+	aCustomMenu add: 'Load actor' action: #loadActor.
