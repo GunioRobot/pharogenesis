4cd26d7bf3c200bda84e835fbeb8272621a835c7
RAA <unknown>
2001-05-28 07:39:00 +0000

    no message

--- Utilities/class/recent method submissions/recentMethodSubmissions.cs
+++ Utilities/class/recent method submissions/recentMethodSubmissions.cs
@@ -1,5 +1,8 @@
 recentMethodSubmissions
 	"Answer the list of recent method submissions, in order.  5/16/96 sw"
 
+
+	self flag: #mref.	"fix for faster references to methods"
+
 	RecentSubmissions == nil ifTrue: [RecentSubmissions _ OrderedCollection new].
 	^ RecentSubmissions
