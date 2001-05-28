447663f4bbd1a6e0cc68d5c0df0ed197bcd2d923
RAA <unknown>
2001-05-28 08:06:00 +0000

    no message

---
+++ MethodReference/instance/setting/setClass-3A-methodSymbol-3A-stringVersion-3A-.cs
@@ -0,0 +1,6 @@
+setClass: aClass methodSymbol: methodSym stringVersion: aString 
+
+	classSymbol _ aClass theNonMetaClass name.
+	classIsMeta _ aClass isMeta.
+	methodSymbol _ methodSym.
+	stringVersion _ aString.
