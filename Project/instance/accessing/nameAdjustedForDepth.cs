a6ed032bd527d8b3c2b56b55099f2d1db468077e
jla <unknown>
2001-05-28 20:01:00 +0000

    no message

---
+++ Project/instance/accessing/nameAdjustedForDepth.cs
@@ -0,0 +1,10 @@
+nameAdjustedForDepth
+	"Answer the name of the project, prepended with spaces reflecting the receiver's depth from the top project"
+	"	Project current nameAdjustedForDepth	"
+
+	| stream |
+	stream _ WriteStream on: String new.
+	self depth timesRepeat: 
+	  [2 timesRepeat: [stream nextPut: $ ]].
+	stream nextPutAll: self name.
+	^ stream contents
