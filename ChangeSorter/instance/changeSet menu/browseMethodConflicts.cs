4da173cd25f61ffba85c0c0486a36d1181b6caf7
RAA <unknown>
2001-05-28 12:06:00 +0000

    no message

--- ChangeSorter/instance/changeSet menu/browseMethodConflicts.cs
+++ ChangeSorter/instance/changeSet menu/browseMethodConflicts.cs
@@ -1,14 +1,16 @@
 browseMethodConflicts
 	"Check to see if any other change set also holds changes to any methods in the selected change set; if so, open a browser on all such."
 
-	| aList aClass aSelector |
-	aList _ myChangeSet changedMessageListAugmented select:
-		[:aChange |
-			MessageSet parse: aChange toClassAndSelector: [:cls :sel | aClass _ cls.  aSelector _ sel].
-			(ChangeSorter allChangeSetsWithClass: aClass selector: aSelector) size > 1].
+	| aList |
 
-	aList size == 0 ifTrue: [^ self inform: 'No other change set has changes
+	aList _ myChangeSet 
+		messageListForChangesWhich: [ :aClass :aSelector |
+			(ChangeSorter allChangeSetsWithClass: aClass selector: aSelector) size > 1
+		]
+		ifNone: [^ self inform: 'No other change set has changes
 for any method in this change set.'].
 	
-	MessageSet openMessageList: aList name: 'Methods in "', myChangeSet name, '" that are also in other change sets (', aList size printString, ')'
+	MessageSet 
+		openMessageList: aList 
+		name: 'Methods in "', myChangeSet name, '" that are also in other change sets (', aList size printString, ')'
 	
