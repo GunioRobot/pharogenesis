8c608543dfd87c2d1eeed6b12e9fe6f0d2f3e991
RAA <unknown>
2001-05-28 12:07:00 +0000

    no message

--- ChangeSorter/instance/changeSet menu/methodConflictsWithOtherSide.cs
+++ ChangeSorter/instance/changeSet menu/methodConflictsWithOtherSide.cs
@@ -1,17 +1,19 @@
 methodConflictsWithOtherSide
 	"Check to see if the change set on the other side shares any methods with the selected change set; if so, open a browser on all such."
 
-	| aList aClass aSelector other |
+	| aList other |
+
 	self checkThatSidesDiffer: [^ self].
 	other _ (parent other: self) changeSet.
-	aList _ myChangeSet changedMessageListAugmented select:
-		[:aChange |
-			MessageSet parse: aChange toClassAndSelector: [:cls :sel | aClass _ cls.  aSelector _ sel].
-			aClass notNil and: [(other methodChangesAtClass: aClass name) includesKey: aSelector]].
-
-	aList size == 0 ifTrue: [^ self inform: 'There are no methods that appear
+	aList _ myChangeSet 
+		messageListForChangesWhich: [ :aClass :aSelector |
+			aClass notNil and: [(other methodChangesAtClass: aClass name) includesKey: aSelector]
+		]
+		ifNone:  [^ self inform: 'There are no methods that appear
 both in this change set and
 in the one on the other side.'].
 	
-	MessageSet openMessageList: aList name: 'Methods in "', myChangeSet name, '" that are also in ', other name,' (', aList size printString, ')'
+	MessageSet 
+		openMessageList: aList 
+		name: 'Methods in "', myChangeSet name, '" that are also in ', other name,' (', aList size printString, ')'
 	
