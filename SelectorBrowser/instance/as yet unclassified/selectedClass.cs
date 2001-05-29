b9063daf44d1b54ed61b95b27fc0806a99d164ea
RAA <unknown>
2001-05-29 14:39:00 +0000

    no message

--- SelectorBrowser/instance/as yet unclassified/selectedClass.cs
+++ SelectorBrowser/instance/as yet unclassified/selectedClass.cs
@@ -2,8 +2,15 @@ selectedClass
 	"Answer the currently selected class."
 
 	| pairString |
+
+	self flag: #mref.	"allows for old-fashioned style"
+
 	classListIndex = 0 ifTrue: [^nil].
 	pairString _ classList at: classListIndex.
+	(pairString isKindOf: MethodReference) ifTrue: [
+		^pairString actualClass
+	].
 	(pairString includes: $*) ifTrue: [pairString _ pairString allButFirst].
-	MessageSet parse: pairString
+	MessageSet 
+		parse: pairString
 		toClassAndSelector: [:cls :sel | ^ cls].
