a3696b10f7affaacc7726906ee78f7b1d4a821e9
sw <unknown>
2001-05-29 13:57:00 +0000

    no message

--- Player/instance/slots-2D-kernel/typeForSlot-3A-.cs
+++ Player/instance/slots-2D-kernel/typeForSlot-3A-.cs
@@ -1,7 +1,11 @@
 typeForSlot: aSlotName
 	"Answer the data type for values of the instance variable of the given name"
 
+	| getter |
 	(self slotInfo includesKey: aSlotName) ifTrue: [^ (self slotInfoAt: aSlotName) type].
-	^ (ScriptingSystem typeForSystemSlotNamed: aSlotName)
-		ifNil:
-			[self error: 'no type for slot named ', aSlotName]
+	getter _ (aSlotName beginsWith: 'get')
+		ifTrue:
+			[aSlotName]
+		ifFalse:
+			[Utilities getterSelectorFor: aSlotName].
+	^ (self currentVocabulary methodInterfaceAt: getter ifAbsent: [self error: 'Unknown slot name: ', aSlotName]) resultType
