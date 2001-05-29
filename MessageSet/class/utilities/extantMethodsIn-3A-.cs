cb4cb6569371462c7d20f6ae2d0cc7ca876192f5
RAA <unknown>
2001-05-29 10:19:00 +0000

    no message

--- MessageSet/class/utilities/extantMethodsIn-3A-.cs
+++ MessageSet/class/utilities/extantMethodsIn-3A-.cs
@@ -1,7 +1,14 @@
-extantMethodsIn: aList
+extantMethodsIn: aListOfMethodRefs
 	"Answer the subset of the incoming list consisting only of those message markers that refer to methods actually in the current image"
 
-	^ aList select:
-		[:aToken |
-			self parse: aToken toClassAndSelector:
-				[:aClass :aSelector | aClass notNil and: [aClass includesSelector: aSelector]]]
+
+	self flag: #mref.	"may be removed in second round"
+
+
+	^ aListOfMethodRefs select: [:aToken |
+		self 
+			parse: aToken 
+			toClassAndSelector: [ :aClass :aSelector |
+				aClass notNil and: [aClass includesSelector: aSelector]
+			]
+	]
