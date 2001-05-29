74c0fb7bcf9e3aec3478a07d4cd83410cbbd736d
RAA <unknown>
2001-05-29 10:19:00 +0000

    no message

--- ChangedMessageSet/class/as yet unclassified/openFor-3A-.cs
+++ ChangedMessageSet/class/as yet unclassified/openFor-3A-.cs
@@ -2,5 +2,10 @@ openFor: aChangeSet
 	"Open up a ChangedMessageSet browser on the given change set; this is a conventional message-list browser whose message-list consists of all the methods in aChangeSet.  After any method submission, the message list is refigured, making it plausibly dynamic"
 
 	| messageSet |
-	messageSet _ MessageSet extantMethodsIn: aChangeSet changedMessageListAugmented.
-	self openMessageList: messageSet name: ('Methods in Change Set ', aChangeSet name) autoSelect: nil changeSet: aChangeSet
+
+	messageSet _ aChangeSet changedMessageListAugmented select: [ :each | each isValid].
+	self 
+		openMessageList: messageSet 
+		name: 'Methods in Change Set ', aChangeSet name
+		autoSelect: nil
+		changeSet: aChangeSet
