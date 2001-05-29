7adb8141b57258cadbab5ffd4296056d02926712
RAA <unknown>
2001-05-29 10:19:00 +0000

    no message

--- RecentMessageSet/instance/update/reformulateList.cs
+++ RecentMessageSet/instance/update/reformulateList.cs
@@ -1,7 +1,9 @@
 reformulateList
+	| myList |
 	"Reformulate the receiver's list.  Exclude methods now deleted"
 
-	self initializeMessageList: (MessageSet extantMethodsIn: Utilities recentMethodSubmissions reversed).
+	myList _ Utilities recentMethodSubmissions reversed select: [ :each | each isValid].
+	self initializeMessageList: myList.
 	self messageListIndex: (messageList size min: 1).	"0 or 1"
 	self changed: #messageList.
 	self changed: #messageListIndex
