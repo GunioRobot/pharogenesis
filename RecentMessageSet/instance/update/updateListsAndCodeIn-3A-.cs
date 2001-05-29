d7658de4feb3c98f6d5ec9bbcd126765acb7054b
RAA <unknown>
2001-05-29 10:42:00 +0000

    no message

--- RecentMessageSet/instance/update/updateListsAndCodeIn-3A-.cs
+++ RecentMessageSet/instance/update/updateListsAndCodeIn-3A-.cs
@@ -3,9 +3,11 @@ updateListsAndCodeIn: aWindow
 	| recentFromUtilities |
 	"RAA 20 june 2000 - a recent change to how messages were displayed in the list caused them not to match what was stored in Utilities. This caused the recent submissions to be continuously updated. The hack below fixed that problem"
 
+	self flag: #mref.	"in second pass, use simpler test"
+
 	self canDiscardEdits ifFalse: [^ self].
-	recentFromUtilities _ Utilities mostRecentlySubmittedMessage asString,' '.
-	(messageList first asString beginsWith: recentFromUtilities)
+	recentFromUtilities _ Utilities mostRecentlySubmittedMessage,' '.
+	(messageList first asStringOrText asString beginsWith: recentFromUtilities)
 		ifFalse:
 			[self reformulateList]
 		ifTrue:
