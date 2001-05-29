e12e4a98beb9c870d507c13246a5bc9d1e5c6c3c
RAA <unknown>
2001-05-29 09:04:00 +0000

    no message

--- Symbol/class/class initialization/initialize.cs
+++ Symbol/class/class initialization/initialize.cs
@@ -1,4 +1,8 @@
 initialize
+
 	"Symbol initialize"
 
-	SymbolTable _ WeakSet withAll: self allInstances
+	Symbol rehash.
+	OneCharacterSymbols _ nil.
+	OneCharacterSymbols _ (1 to: 256) collect: [ :i | (i - 1) asCharacter asSymbol].
+	Smalltalk addToShutDownList: self.
