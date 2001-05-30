9588d657c52f75913590bdfbc6b29d78e29a6b53
ar <unknown>
2001-05-30 23:11:00 +0000

    no message

---
+++ ResourceManager/instance/loading/installResource-3A-from-3A-locator-3A-.cs
@@ -0,0 +1,12 @@
+installResource: aResource from: aStream locator: loc
+	| repl |
+	aResource ifNil:[^false]. "it went away, so somebody might have deleted it"
+	(aStream == nil or:[aStream size = 0]) ifTrue:[^false]. "error?!"
+	repl _ aResource clone readResourceFrom: aStream asUnZippedStream.
+	repl ifNotNil:[
+		aResource replaceByResource: repl.
+		unloaded remove: loc.
+		loaded add: loc.
+		^true
+	].
+	^false
