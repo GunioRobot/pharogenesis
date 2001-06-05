dbcf1d8acc6f5a30f61db30608ad1c75e89a0909
RAA <unknown>
2001-06-05 11:57:00 +0000

    no message

--- Parser/instance/error correction/declareTempAndPaste-3A-.cs
+++ Parser/instance/error correction/declareTempAndPaste-3A-.cs
@@ -1,24 +1,24 @@
 declareTempAndPaste: name
-	| insertion tabbed |
-	(requestor text string at: tempsMark) = $|
-				ifTrue:  "Paste it before the second vertical bar"
-					[insertion _ name, ' '.
-					(requestor text string at: tempsMark-1) isSeparator
-						ifFalse: [insertion _ ' ', insertion].
-					tempsMark _ tempsMark +
-						(self substituteWord: insertion
-							wordInterval: (tempsMark to: tempsMark-1)
-							offset: 0)]
-				ifFalse:  "No bars - insert some with CR, tab"
-					[insertion _ '| ' , name , ' |
-'.
-					tabbed _ tempsMark > 1
-						and: [(requestor text string at: tempsMark-1) = Character tab].
-					tabbed
-						ifTrue: [insertion _ insertion , (String with: Character tab)].
-					tempsMark _ tempsMark +
-						(self substituteWord: insertion
-							wordInterval: (tempsMark to: tempsMark-1)
-							offset: 0)
-						- (tabbed ifTrue: [3] ifFalse: [2])].
-			^ encoder bindAndJuggle: name
+	| insertion delta theTextString characterBeforeMark |
+
+	theTextString _ requestor text string.
+	characterBeforeMark _ theTextString at: tempsMark-1 ifAbsent: [$ ].
+	(theTextString at: tempsMark) = $| ifTrue: [
+  		"Paste it before the second vertical bar"
+		insertion _ name, ' '.
+		characterBeforeMark isSeparator ifFalse: [insertion _ ' ', insertion].
+		delta _ 0.
+	] ifFalse: [
+		"No bars - insert some with CR, tab"
+		insertion _ '| ' , name , ' |',String cr.
+		delta _ 2.	"the bar and CR"
+		characterBeforeMark = Character tab ifTrue: [
+			insertion _ insertion , String tab.
+			delta _ delta + 1.	"the tab"
+		].
+	].
+	tempsMark _ tempsMark +
+		(self substituteWord: insertion
+			wordInterval: (tempsMark to: tempsMark-1)
+			offset: 0) - delta.
+	^ encoder bindAndJuggle: name
