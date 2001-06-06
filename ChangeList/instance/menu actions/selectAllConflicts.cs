d765f6c586cad2ad9eeba7dea981e8a17b46fa7e
sw <unknown>
2001-06-06 12:54:00 +0000

    no message

--- ChangeList/instance/menu actions/selectAllConflicts.cs
+++ ChangeList/instance/menu actions/selectAllConflicts.cs
@@ -1,12 +1,12 @@
 selectAllConflicts
-	"Selects all method definitions in the receiver which are also in any existing change set in the system.  This makes no statement about whether the content of the methods differ, only whether there is a change represented.
-"
+	"Selects all method definitions in the receiver which are also in any existing change set in the system.  This makes no statement about whether the content of the methods differ, only whether there is a change represented."
+
 	|  aClass aChange |
 	Cursor read showWhile: 
-	[1 to: changeList size do:
-		[:i | aChange _ changeList at: i.
-		listSelections at: i put:
-			(aChange type = #method
-			and: [(aClass _ aChange methodClass) notNil
-			and: [(ChangeSorter allChangeSetsWithClass: aClass selector: aChange methodSelector) size > 0]])]].
+		[1 to: changeList size do:
+			[:i | aChange _ changeList at: i.
+			listSelections at: i put:
+				(aChange type = #method
+				and: [(aClass _ aChange methodClass) notNil
+				and: [ChangeSorter doesAnyChangeSetHaveClass: aClass andSelector:  aChange methodSelector]])]].
 	self changed: #allSelections
