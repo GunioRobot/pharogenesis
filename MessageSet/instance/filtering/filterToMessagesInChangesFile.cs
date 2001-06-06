176f79965c6d14fea3fbfa6648c1f3a5ea25eb3d
sw <unknown>
2001-06-06 15:14:00 +0000

    no message

--- MessageSet/instance/filtering/filterToMessagesInChangesFile.cs
+++ MessageSet/instance/filtering/filterToMessagesInChangesFile.cs
@@ -1,4 +1,10 @@
 filterToMessagesInChangesFile
 	"Filter down only to messages whose source code risides in the Changes file.  This allows one to ignore long-standing methods that live in the .sources file."
 
-	self notYetImplemented
+	| cm |
+	self filterFrom:
+		[:aClass :aSelector |
+			aClass notNil and: [aSelector notNil and:
+				[(self class isPseudoSelector: aSelector) not and:
+					[(cm _ aClass compiledMethodAt: aSelector ifAbsent: [nil]) notNil and:
+					[cm fileIndex ~~ 1]]]]]
