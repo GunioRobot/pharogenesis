6b4c3106c669d9f4a60f38bd8bcb789141cc1079
sw <unknown>
2001-06-07 23:54:00 +0000

    no message

--- ChangeList/instance/viewing access/diffedVersionContents.cs
+++ ChangeList/instance/viewing access/diffedVersionContents.cs
@@ -1,11 +1,16 @@
 diffedVersionContents
-	| thisText change class |
+	"Answer diffed version contents, maybe pretty maybe not"
+
+	| change class earlier later |
 	(listIndex = 0
 			or: [changeList size < listIndex])
 		ifTrue: [^ ''].
 	change _ changeList at: listIndex.
-	thisText _ change text.
+	later _ change text.
 	class _ change methodClass.
-	^ listIndex == changeList size
-		ifTrue: [thisText]
-		ifFalse: [TextDiffBuilder buildDisplayPatchFrom: (changeList at: listIndex + 1) text to: thisText inClass: class]
+	(listIndex == changeList size or: [class == nil])
+		ifTrue: [^ later].
+
+	earlier _ (changeList at: listIndex + 1) text.
+
+	^ TextDiffBuilder buildDisplayPatchFrom: earlier to: later inClass: class prettyDiffs: self showingPrettyDiffs
