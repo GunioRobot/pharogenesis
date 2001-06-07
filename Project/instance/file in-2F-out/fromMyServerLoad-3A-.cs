8edfcaf87aafd7791b776da6623079977d4166cc
mir <unknown>
2001-06-07 14:41:00 +0000

    no message

--- Project/instance/file in-2F-out/fromMyServerLoad-3A-.cs
+++ Project/instance/file in-2F-out/fromMyServerLoad-3A-.cs
@@ -1,19 +1,16 @@
 fromMyServerLoad: otherProjectName
-	| servers pair pr dirToUse |
+	| pair pr dirToUse |
 	"If a newer version of me is on the server, load it."
 
 	(pr _ Project named: otherProjectName) ifNotNil: ["it appeared"
 		^ pr enter
 	].
-	(servers _ self serverList) ifNil: [
+	dirToUse _ self primaryServerIfNil: [
 		lastDirectory ifNil: [
 			self inform: 'Current project does not know a server either.'.
-			^nil
-		].
-		dirToUse _ lastDirectory.
-	] ifNotNil: [
-		dirToUse _ servers first.
-	].
+			^nil].
+		lastDirectory].
+
 	pair _ self class mostRecent: otherProjectName onServer: dirToUse.
 	pair first ifNil: [^self decideAboutCreatingBlank: otherProjectName].	"nothing to load"
 	^ProjectLoading
