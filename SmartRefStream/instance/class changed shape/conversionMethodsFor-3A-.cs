92ac38e5ae266760a321c222abe015f2800255ca
tk <unknown>
2001-06-08 10:53:00 +0000

    no message

---
+++ SmartRefStream/instance/class changed shape/conversionMethodsFor-3A-.cs
@@ -0,0 +1,14 @@
+conversionMethodsFor: classList
+	| oldStruct newStruct list |
+	"Each of these needs a conversion method.  Hard part is the comment in it.  Return a MessageSet."
+
+	list _ OrderedCollection new.
+	classList do: [:cls |
+		oldStruct _ structures at: cls name ifAbsent: [#()].
+		newStruct _ (Array with: cls classVersion), (cls allInstVarNames).
+		self writeConversionMethodIn: cls fromInstVars: oldStruct to: newStruct 
+				renamedFrom: nil.
+		list add: cls name, ' convertToCurrentVersion:refStream:'.
+		].
+
+	^ MessageSet new initializeMessageList: list.
