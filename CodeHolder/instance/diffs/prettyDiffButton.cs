cf2a0556b25f846d6dc311fb454a4605464892ed
sw <unknown>
2001-06-08 00:37:00 +0000

    no message

---
+++ CodeHolder/instance/diffs/prettyDiffButton.cs
@@ -0,0 +1,21 @@
+prettyDiffButton
+	"Return a checkbox that lets the user decide whether prettyDiffs should be shown or not"
+
+	|  outerButton aButton |
+	outerButton _ AlignmentMorph newRow.
+	outerButton wrapCentering: #center; cellPositioning: #leftCenter.
+	outerButton color:  Color transparent.
+	outerButton hResizing: #shrinkWrap; vResizing: #shrinkWrap.
+	outerButton addMorph: (aButton _ UpdatingThreePhaseButtonMorph checkBox).
+	aButton
+		target: self;
+		actionSelector: #togglePrettyDiffing;
+		getSelector: #showingPrettyDiffs.
+	outerButton addMorphBack: (StringMorph contents: 'prettyDiffs') lock.
+	(self isKindOf: VersionsBrowser)
+		ifTrue:
+			[outerButton setBalloonText: 'If checked, then pretty-printed code differences from the previous version, if any, will be shown.']
+		ifFalse:
+			[outerButton setBalloonText: 'If checked, then pretty-printed code differences between the file-based method and the in-memory version, if any, will be shown.'].
+
+	^ outerButton
