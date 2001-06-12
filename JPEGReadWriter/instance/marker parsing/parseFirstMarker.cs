aa2344f580fa36daf85e4f3c20c93548ab35ee55
mir <unknown>
2001-06-12 11:28:00 +0000

    no message

--- JPEGReadWriter/instance/marker parsing/parseFirstMarker.cs
+++ JPEGReadWriter/instance/marker parsing/parseFirstMarker.cs
@@ -1,5 +1,9 @@
 parseFirstMarker
 
+	| marker |
 	self next = 16rFF ifFalse: [self error: 'JFIF marker expected'].
-	self next = 16rD8 ifFalse: [self error: 'SOI marker expected'].
+	marker _ self next.
+	marker = 16rD9
+		ifTrue: [^self "halt: 'EOI encountered.'"].
+	marker = 16rD8 ifFalse: [self error: 'SOI marker expected'].
 	self parseStartOfInput.
