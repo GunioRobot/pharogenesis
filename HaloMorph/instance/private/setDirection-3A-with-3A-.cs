b50f59de4770588c9a692922f321e89f4ba6cbab
ar <unknown>
2001-06-12 05:24:00 +0000

    no message

--- HaloMorph/instance/private/setDirection-3A-with-3A-.cs
+++ HaloMorph/instance/private/setDirection-3A-with-3A-.cs
@@ -1,8 +1,5 @@
 setDirection: anEvent with: directionHandle
 	"The user has let up after having dragged the direction arrow; now set the forward direction of the actual SketchMorph accordingly"
-	| delta degrees |
 	anEvent hand obtainHalo: self.
-	delta _ (innerTarget transformFromWorld globalPointToLocal: directionHandle center) - innerTarget referencePosition.
-	degrees _ delta degrees + 90.0.
-	target forwardDirection: (degrees \\ 360) rounded.
+	target setDirectionFrom: directionHandle center.
 	self endInteraction
