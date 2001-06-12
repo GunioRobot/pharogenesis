a8f75a9df143f596987d88beb2170137ba9c98d4
mir <unknown>
2001-06-12 11:28:00 +0000

    no message

--- JPEGReadWriter/instance/marker parsing/parseNextMarker.cs
+++ JPEGReadWriter/instance/marker parsing/parseNextMarker.cs
@@ -1,11 +1,12 @@
 parseNextMarker
+	"Parse the next marker of the stream"
 
 	| byte discardedBytes |
 	discardedBytes _ 0.
-	[(byte _ self next) = 16rFF] whileFalse: [ self debug. discardedBytes _ discardedBytes + 1].	
+	[(byte _ self next) = 16rFF] whileFalse: [discardedBytes _ discardedBytes + 1].	
 	[[(byte _ self next) = 16rFF] whileTrue. byte = 16r00] whileTrue:
 		[discardedBytes _ discardedBytes + 2].
-	discardedBytes > 0 ifTrue: [self notify: 'warning: extraneous data discarded'].
+	discardedBytes > 0 ifTrue: [self "notifyWithLabel: 'warning: extraneous data discarded'"].
 	self perform:
 		(JFIFMarkerParser
 			at: byte
