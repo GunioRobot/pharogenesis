5869ba5c4ddd58b9aa8d6e47182ebf426fe3b805
sw <unknown>
2001-06-13 19:42:00 +0000

    no message

--- Morph/instance/dropping-2F-grabbing/justDroppedInto-3A-event-3A-.cs
+++ Morph/instance/dropping-2F-grabbing/justDroppedInto-3A-event-3A-.cs
@@ -1,5 +1,5 @@
 justDroppedInto: aMorph event: anEvent
-	"This message is sent to a dropped morph after it has been dropped on--and been accepted by--a drop-sensitive morph"
+	"This message is sent to a dropped morph after it has been dropped on -- and been accepted by -- a drop-sensitive morph"
 
 	| aWindow partsBinCase cmd aStack |
 	self formerOwner: nil.
@@ -15,6 +15,12 @@ justDroppedInto: aMorph event: anEvent
 	(self isInWorld and: [partsBinCase not]) ifTrue:
 		[self world startSteppingSubmorphsOf: self].
 	"Note an unhappy inefficiency here:  the startStepping... call will often have already been called in the sequence leading up to entry to this method, but unfortunately the isPartsDonor: call often will not have already happened, with the result that the startStepping... call will not have resulted in the startage of the steppage."
+
+	"An object launched by certain parts-launcher mechanisms should end up fully visible..."
+	(self hasProperty: #beFullyVisibleAfterDrop) ifTrue:
+		[aMorph == ActiveWorld ifTrue:
+			[self goHome].
+		self removeProperty: #beFullyVisibleAfterDrop].
 
 	(self holdsSeparateDataForEachInstance and: [(aStack _ self stack) notNil])
 		ifTrue:
