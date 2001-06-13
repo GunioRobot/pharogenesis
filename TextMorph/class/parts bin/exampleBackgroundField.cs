21f488d29e02a81d44494b261f92567830339a89
sw <unknown>
2001-06-13 22:46:00 +0000

    no message

---
+++ TextMorph/class/parts bin/exampleBackgroundField.cs
@@ -0,0 +1,10 @@
+exampleBackgroundField
+	"Answer a background field for a parts bin"
+
+	| aMorph |
+	aMorph _ TextMorph authoringPrototype.
+	aMorph contents: 'background field' asText allBold.
+	aMorph setProperty: #shared toValue: true.
+	aMorph setNameTo: 'field1'.
+	aMorph setProperty: #holdsSeparateDataForEachInstance toValue: true.	
+	^ aMorph
