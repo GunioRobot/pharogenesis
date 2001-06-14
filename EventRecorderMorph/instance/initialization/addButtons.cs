8913da655f677e79064b25b21baa9f9d973e63c3
RAA <unknown>
2001-06-14 16:52:00 +0000

    no message

--- EventRecorderMorph/instance/initialization/addButtons.cs
+++ EventRecorderMorph/instance/initialization/addButtons.cs
@@ -1,55 +1,35 @@
 addButtons
-	| r b w |
+	| r b |
 
-	caption ifNotNil:
-		["Special setup for play-only interface"
-		r _ AlignmentMorph newRow vResizing: #shrinkWrap;
-			 wrapCentering: #center; cellPositioning: #leftCenter;
-			 minCellSize: 4;
-			 color: Color blue.
-		r addMorphBack: (SimpleButtonMorph new target: self;
-	 							label: caption; actionSelector: #play).
-		r addMorphBack: (Morph new extent: 4@4; color: Color transparent).
-		r addMorphBack: (statusLight _ EllipseMorph new extent: 11 @ 11;
-								color: Color green; borderWidth: 0).
-		r addMorphBack: (Morph new extent: 4@4; color: Color transparent).
-		^ self addMorphBack: r].
+	caption ifNotNil: ["Special setup for play-only interface"
+		(r _ self makeARowForButtons)
+			addMorphBack: (SimpleButtonMorph new target: self;
+	 							label: caption; actionSelector: #play);
+			addMorphBack: self makeASpacer;
+			addMorphBack: self makeStatusLight;
+			addMorphBack: self makeASpacer.
+		^ self addMorphBack: r
+	].
 
-	"record - stop - play"
-	r _ AlignmentMorph newRow vResizing: #shrinkWrap;
-			 minCellSize: 4;
-			 color: Color blue.
-	r addMorphBack: (b _ self buttonFor: #record).
-	w _ b width.
-	r addMorphBack: (AlignmentMorph newSpacer: Color transparent).
-	r addMorphBack: ((self buttonFor: #stop) width: w).
-	r addMorphBack: (AlignmentMorph newSpacer: Color transparent).
-	r addMorphBack: ((self buttonFor: #play) width: w).
+	(r _ self makeARowForButtons)
+		addMorphBack: (b _ self buttonFor: {#record. nil. 'Begin recording'});
+		addMorphBack: self makeASpacer;
+		addMorphBack: (self buttonFor: {#stop. b width. 'Stop recording - you can also use the ESC key to stop it'});
+		addMorphBack: self makeASpacer;
+		addMorphBack: (self buttonFor: {#play. b width. 'Play current recording'}).
 	self addMorphBack: r.
 
-	"read file - write file"
-	r _ AlignmentMorph newRow vResizing: #shrinkWrap;
-			 minCellSize: 4;
-			 color: Color blue.
-	r addMorphBack: (b _ self buttonFor: #writeTape).
-	w _ b width.
-	r addMorphBack: (AlignmentMorph newSpacer: Color transparent).
-	r addMorphBack: ((self buttonFor: #readTape)
-			width: w).
+	(r _ self makeARowForButtons)
+		addMorphBack: (b _ self buttonFor: {#writeTape. nil. 'Save current recording on disk'});
+		addMorphBack: self makeASpacer;
+		addMorphBack: (self buttonFor: {#readTape. b width. 'Get a new recording from disk'}).
 	self addMorphBack: r.
 
-	"rewind - light - reset"
-	r _ AlignmentMorph newRow vResizing: #shrinkWrap;
-			 wrapCentering: #center; cellPositioning: #leftCenter;
-			 minCellSize: 4;
-			 color: Color blue.
-	r addMorphBack: (b _ self buttonFor: #shrink).
-	w _ b width.
-	r addMorphBack: (AlignmentMorph newSpacer: Color transparent).
-	r addMorphBack: (statusLight _ EllipseMorph new extent: 11 @ 11;
-			color: Color green;
-		 	borderWidth: 0).
-	r addMorphBack: (AlignmentMorph newSpacer: Color transparent).
-	r addMorphBack: ((self buttonFor: #button)
-			width: w).
-	self addMorph: r
+	(r _ self makeARowForButtons)
+		addMorphBack: (b _ self buttonFor: {#shrink. nil. 'Make recording shorter by removing unneeded events'});
+		addMorphBack: self makeASpacer;
+		addMorphBack: self makeStatusLight;
+		addMorphBack: self makeASpacer;
+		addMorphBack: (self buttonFor: {#button. b width. 'Make a simple button to play this recording'}).
+	self addMorph: r.
+	self setStatusLight: #ready.
