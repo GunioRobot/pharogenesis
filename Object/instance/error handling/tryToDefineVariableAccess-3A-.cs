4d1d023efc439e372b97a1c9d6eea11b5aefb4b3
tk <unknown>
2001-06-18 15:04:00 +0000

    no message

--- Object/instance/error handling/tryToDefineVariableAccess-3A-.cs
+++ Object/instance/error handling/tryToDefineVariableAccess-3A-.cs
@@ -1,18 +1,25 @@
 tryToDefineVariableAccess: aMessage
 	"See if the message just wants to get at an instance variable of this class.  Ask the user if its OK.  If so, define the message to read or write that instance or class variable and retry."
-	| ask newMessage sel |
+	| ask newMessage sel canDo classOrSuper |
 	aMessage arguments size > 1 ifTrue: [^ false].
 	sel _ aMessage selector asString.	"works for 0 args"
 	aMessage arguments size = 1 ifTrue: [
 		sel last = $: ifFalse: [^ false].
 		sel _ sel copyWithout: $:].
-	(self class instVarNames includes: sel) ifFalse: [
-		(self class classVarNames includes: sel asSymbol) ifFalse: [
-			^ false]].
+	canDo _ false.  classOrSuper _ self class.
+	[((classOrSuper instVarNames includes: sel) 	
+		ifTrue: [canDo _ true. nil]
+		ifFalse: [classOrSuper _ classOrSuper superclass]) == nil] whileFalse.
+	canDo ifFalse: [classOrSuper _ self class.
+		[((classOrSuper classVarNames includes: sel) 	
+			ifTrue: [canDo _ true. nil]
+			ifFalse: [classOrSuper _ classOrSuper superclass]) == nil] whileFalse].
+	canDo ifFalse: [^ false].
+
 	ask _ self confirm: 'A ', thisContext sender sender receiver 
 		class printString, ' wants to ', 
 		(aMessage arguments size = 1 ifTrue: ['write into'] ifFalse: ['read from']), '
-', sel ,' in class ', self class printString, '.
+', sel ,' in class ', classOrSuper printString, '.
 Define a this access message?'.
 	ask ifTrue: [
 		aMessage arguments size = 1 
@@ -20,5 +27,5 @@ Define a this access message?'.
 	', sel, ' _ anObject']
 			ifFalse: [newMessage _ aMessage selector, '
 	^', aMessage selector].
-		self class compile: newMessage classified: 'accessing' notifying: nil].
+		classOrSuper compile: newMessage classified: 'accessing' notifying: nil].
 	^ ask
