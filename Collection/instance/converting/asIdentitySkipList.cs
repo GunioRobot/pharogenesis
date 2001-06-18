04adbcb2a788fa2e8ed090a43ee04c7e93718cef
LC <unknown>
2001-06-18 20:30:00 +0000

    no message

---
+++ Collection/instance/converting/asIdentitySkipList.cs
@@ -0,0 +1,5 @@
+asIdentitySkipList
+	"Answer a IdentitySkipList whose elements are the elements of the 
+	receiver. The sort order is the default less than or equal."
+
+	^ self as: IdentitySkipList
