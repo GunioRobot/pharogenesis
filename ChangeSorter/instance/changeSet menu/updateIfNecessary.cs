5fee632f2db135242ee6aeb90646a2c813b7dea7
di <unknown>
2001-06-20 09:37:00 +0000

    no message

--- ChangeSorter/instance/changeSet menu/updateIfNecessary.cs
+++ ChangeSorter/instance/changeSet menu/updateIfNecessary.cs
@@ -3,8 +3,14 @@ updateIfNecessary
 
 	| newList |
 	self okToChange ifFalse: [^ self].
-	myChangeSet isMoribund ifTrue: [^ self showChangeSet: Smalltalk changes].
+
+	myChangeSet ifNil: [^ self].  "Has been known to happen though shouldn't"
+	(myChangeSet isMoribund or: [(changeSetCategory notNil and: [changeSetCategory includesChangeSet: myChangeSet]) not]) ifTrue:
+		[self changed: #changeSetList.
+		^ self showChangeSet: self changeSetCategory defaultChangeSetToShow].
+
 	newList _ self changeSetList.
+
 	(priorChangeSetList == nil or: [priorChangeSetList ~= newList])
 		ifTrue:
 			[priorChangeSetList _ newList.
