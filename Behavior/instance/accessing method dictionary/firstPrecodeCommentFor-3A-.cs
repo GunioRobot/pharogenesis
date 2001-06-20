6ad327ee1f8d54f3d757bf035ea8aab4d8dd7bc7
sw <unknown>
2001-06-20 15:46:00 +0000

    no message

--- Behavior/instance/accessing method dictionary/firstPrecodeCommentFor-3A-.cs
+++ Behavior/instance/accessing method dictionary/firstPrecodeCommentFor-3A-.cs
@@ -1,14 +1,20 @@
 firstPrecodeCommentFor:  selector
 	"If there is a comment in the source code at the given selector that preceeds the body of the method, return it here, else return nil"
 
-	| parser lastHeaderChar firstCommentPosition |
+	| parser source tree |
 	"Behavior firstPrecodeCommentFor: #firstPrecodeCommentFor:"
-
-	(parser _ self parserClass new) parseSelector: (self sourceCodeAt: selector).
-	lastHeaderChar _ parser endOfLastToken.
-	firstCommentPosition _ self positionOfFirstCommentAt: selector.
-	^ (firstCommentPosition == nil or: [firstCommentPosition <= (lastHeaderChar + 4)])
-		ifFalse:
-			[nil]
+	(MessageSet isPseudoSelector: selector)
 		ifTrue:
-			[self firstCommentAt: selector]
+			["Not really a selector"
+			^ nil].
+	source _ self sourceCodeAt: selector asSymbol ifAbsent: [^ nil].
+	parser _ self parserClass new.
+	tree _ 
+		parser
+			parse: (ReadStream on: source)
+			class: self
+			noPattern: false
+			context: nil
+			notifying: nil
+			ifFail: [^ nil].
+	^ (tree comment ifNil: [^ nil]) first
