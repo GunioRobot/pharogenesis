e4294b352815348c073b766dc9ac836da7cc16ea
sw <unknown>
2001-06-20 12:37:00 +0000

    no message

--- Presenter/instance/viewer/viewObject-3A-.cs
+++ Presenter/instance/viewer/viewObject-3A-.cs
@@ -1,39 +1,8 @@
 viewObject: anObject
-	"Open up a viewer on the given object"
+	"Open up and return a viewer on the given object.  If the object is a Morph, open a viewer on its associated Player"
 
-	|  aViewer aRect aPoint nominalHeight aFlapTab flapLoc |
-	anObject isMorph ifTrue: [^ self viewMorph: anObject].  "historic morph/player implementation"
-
-	associatedMorph addMorph: (aViewer _ self nascentPartsViewer).
-
-	"19 sept 2000 - allow flaps in any paste up"
-	flapLoc _ associatedMorph "world".
-	Preferences viewersInFlaps ifTrue:
-		[aViewer setProperty: #noInteriorThumbnail toValue: true.
-		aViewer initializeFor: anObject barHeight: 0.
-		aViewer enforceTileColorPolicy.
-		flapLoc hideViewerFlapsOtherThanFor: anObject.
-		aFlapTab _ flapLoc viewerFlapTabFor: anObject.
-		aFlapTab referent submorphs do: 
-			[:m | (m isKindOf: Viewer) ifTrue: [m delete]].
-		aFlapTab referent addMorph: aViewer beSticky.
-		aViewer visible: true.
-		aFlapTab applyThickness: aViewer width + 25.
-		aFlapTab spanWorld.
-		aFlapTab showFlap. 
-		aViewer position: aFlapTab referent position.
-		flapLoc startSteppingSubmorphsOf: aFlapTab.
-		^ flapLoc startSteppingSubmorphsOf: aViewer].
-		
-	aViewer initializeFor: anObject barHeight: 6.
-	aViewer enforceTileColorPolicy.
-	Preferences automaticViewerPlacement ifTrue:
-		[aPoint _ anObject bounds right @ 
-			(anObject center y - ((nominalHeight _ aViewer initialHeightToAllow) // 2)).
-		aRect _ (aPoint extent: (aViewer width @ nominalHeight)) translatedToBeWithin: flapLoc bounds.
-		aViewer position: aRect topLeft.
-		aViewer visible: true.
-		flapLoc startSteppingSubmorphsOf: aViewer.
-		"it's already in the world, somewhat coincidentally"
-		^ self].
-	anObject primaryHand attachMorph: (aViewer visible: true)
+	anObject isMorph
+		ifTrue:
+			[self viewMorph: anObject]  "historic morph/player implementation"
+		ifFalse:
+			[self viewObjectDirectly: anObject]
