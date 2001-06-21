dba20f089f5c682c76fe059debaa55d7e3832728
ls <unknown>
2001-06-21 00:05:00 +0000

    no message

--- MailDB/instance/open-2D-create-2D-save/dbStatus.cs
+++ MailDB/instance/open-2D-create-2D-save/dbStatus.cs
@@ -1,15 +1,2 @@
 dbStatus
-	"See if my database exists. Since the database has several components, the answer is one of:
-	#exists				all files exist
-	#partialDatabase	only some of the files exist
-	#doesNotExist		none of the files exist"
-
-	| dir localName |
-	dir _ FileDirectory forFileName: rootFilename.
-	localName _ FileDirectory localNameFor: rootFilename.
-	messageFile _ dir includesKey: localName, '.messages'.
-	indexFile _ dir includesKey: localName, '.index'.
-	categoriesFile _ dir includesKey: localName, '.categories'.
-	(messageFile & indexFile & categoriesFile) ifTrue: [^ #exists].
-	(messageFile | indexFile | categoriesFile) ifFalse: [^ #doesNotExist].
-	^ #partialDatabase
+	^self class dbStatusFor: rootFilename
