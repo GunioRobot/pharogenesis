b5f710b59d1704a43ba47813fc7cdeef4a067cd3
mir <unknown>
2001-06-21 15:43:00 +0000

    no message

--- Project/instance/file in-2F-out/versionedFileName.cs
+++ Project/instance/file in-2F-out/versionedFileName.cs
@@ -1,13 +1,9 @@
 versionedFileName
 	"Project current versionedFileName"
-	| v |
 	^String streamContents:[:s|
 		s nextPutAll: self name.
 		s nextPutAll: FileDirectory dot.
-		v _ self currentVersionNumber printString.
-		v size < 3 ifTrue:[v _ '0', v].
-		v size < 3 ifTrue:[v _ '0', v].
-		s nextPutAll: v.
+		s nextPutAll: self versionForFileName.
 		s nextPutAll: FileDirectory dot.
 		s nextPutAll: self projectExtension.
 	]
