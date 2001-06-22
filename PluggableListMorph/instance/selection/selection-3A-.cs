e0af3e1186fdefd64af9cc2fcd94efc79b5cc389
ls <unknown>
2001-06-22 22:49:00 +0000

    no message

--- PluggableListMorph/instance/selection/selection-3A-.cs
+++ PluggableListMorph/instance/selection/selection-3A-.cs
@@ -1,6 +1,4 @@
 selection: item
-	"Called from outside to request setting a new selection.
-	Assumes scroller submorphs is exactly our list.
-	Note: won't work right if list includes repeated items"
+	"Called from outside to request setting a new selection."
 
-	self selectionIndex: (scroller submorphs findFirst: [:m | m contents = item])
+	self selectionIndex: (self getList indexOf: item)
