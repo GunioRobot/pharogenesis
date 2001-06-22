dcfb7158defd495ca364bd31987b52bc0736f706
ls <unknown>
2001-06-22 22:49:00 +0000

    no message

--- PluggableListMorph/instance/events/mouseUp-3A-.cs
+++ PluggableListMorph/instance/events/mouseUp-3A-.cs
@@ -1,2 +1,14 @@
-mouseUp: evt
-	"do nothing but fix the nobody land bug"
+mouseUp: event
+	"The mouse came up within the list; take appropriate action"
+
+	| row |
+	row _ self rowAtLocation: event position.
+	"aMorph ifNotNil: [aMorph highlightForMouseDown: false]."
+	model okToChange ifFalse:
+		[^ self].
+	(autoDeselect == false and: [row == 0]) ifTrue: [^ self].  "work-around the no-mans-land bug"
+	"No change if model is locked"
+	((autoDeselect == nil or: [autoDeselect]) and: [row == self selectionIndex])
+		ifTrue: [self changeModelSelection: 0]
+		ifFalse: [self changeModelSelection: row].
+	Cursor normal show.
