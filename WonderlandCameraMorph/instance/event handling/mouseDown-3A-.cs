3861da20bb8ab55b3a1be2eca8721e06a7a7bec8
bf <unknown>
2001-06-25 12:24:00 +0000

    no message

--- WonderlandCameraMorph/instance/event handling/mouseDown-3A-.cs
+++ WonderlandCameraMorph/instance/event handling/mouseDown-3A-.cs
@@ -2,29 +2,48 @@ mouseDown: evt
 	"When the user clicks in a camera window, determine which actor the    
 	         user clicked on and have that actor respond to the event"
 	| newEvent reactions |
-	firstPersonControls == true ifTrue:[
-		myControls setCenter: evt position.
-		myControls mouseDown: evt.
-		evt hand needsToBeDrawn ifFalse:[Cursor crossHair show].
-		^self
+	mode == #view ifTrue:[mode _ nil].
+	"ar 5/28/2001 - remove the above at some point; it's compensating for old wonderlands"
+
+	self on: #mouseMove send: #mouseMoveDefault: to: self.
+	self on: #mouseUp send: #mouseUpDefault: to: self.
+	(evt redButtonPressed and:[evt anyModifierKeyPressed not]) ifTrue:[
+		(mode == nil and:[firstPersonControls == true]) ifTrue:[
+			self getCameraControls. 		"create if necessary"
+			myControls setCenter: evt position.
+			myControls mouseDown: evt.
+			evt hand needsToBeDrawn ifFalse:[Cursor crossHair show].
+			self on: #mouseMove send: #mouseMoveFirstPersonControl: to: self.
+			self on: #mouseUp send: #mouseUpFirstPersonControl: to: self.
+			^self
+		].
+
+		mode == #stroke ifTrue:[
+			self on: #mouseMove send: #mouseMoveStroke: to: self.
+			self on: #mouseUp send: #mouseUpStroke: to: self.
+			^self recordStroke: evt cursorPoint].
+
+		newEvent _ self convertEvent: evt.
+		newEvent ifNil:[^self].
+
+		mode == #paint ifTrue:[
+			newEvent getVertex ifNil:[^self].
+			self prepareAction: newEvent.
+			self on: #mouseMove send: #mouseMovePaint: to: self.
+			self on: #mouseUp send: #mouseUpPaint: to: self.
+			^self perform: palette action with: newEvent].
+	] ifFalse:[
+		newEvent _ self convertEvent: evt.
+		newEvent ifNil:[^self].
 	].
-	newEvent _ self convertEvent: evt.
-	newEvent
-		ifNotNil: 
-			[((mode = #paint and: [newEvent getVertex ~= nil]) and: [evt commandKeyPressed not])
-				ifTrue:
-					[self prepareAction: newEvent.
-					^self perform: palette action with: newEvent]
-				ifFalse:
-					[newEvent getActor hasActiveTexture
-						ifTrue: [^ newEvent getActor morphicMouseDown: newEvent].
-					evt redButtonPressed
-						ifTrue: 
-							[reactions _ newEvent getActor getReactionsTo: leftMouseDown.
-							mouseUpButton _ leftMouseUp]
-						ifFalse: [evt yellowButtonPressed
-								ifTrue: 
-									[reactions _ newEvent getActor getReactionsTo: rightMouseDown.
-									mouseUpButton _ rightMouseUp]
-								ifFalse: [reactions _ nil]].
-					reactions ifNotNil: [reactions do: [:aReaction | aReaction reactTo: newEvent]]]]
+	newEvent getActor hasActiveTexture
+		ifTrue: [^ newEvent getActor morphicMouseDown: newEvent].
+
+	(evt redButtonPressed or:[evt controlKeyPressed]) ifTrue:[
+		reactions _ newEvent getActor getReactionsTo: leftMouseDown.
+		mouseUpButton _ leftMouseUp.
+	] ifFalse: [evt yellowButtonPressed ifTrue:[ 
+		reactions _ newEvent getActor getReactionsTo: rightMouseDown.
+		mouseUpButton _ rightMouseUp.
+	] ifFalse: [reactions _ nil]].
+	reactions ifNotNil: [reactions do: [:aReaction | aReaction reactTo: newEvent]].
