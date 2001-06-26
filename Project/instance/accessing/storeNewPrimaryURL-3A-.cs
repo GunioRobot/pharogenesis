3dd4a509290c127189d43c61ae0293a643b30ca6
mir <unknown>
2001-06-26 17:09:00 +0000

    no message

--- Project/instance/accessing/storeNewPrimaryURL-3A-.cs
+++ Project/instance/accessing/storeNewPrimaryURL-3A-.cs
@@ -1,4 +1,7 @@
 storeNewPrimaryURL: aURLString
-
+	| oldResourceUrl |
+	oldResourceUrl _ self resourceUrl.
 	urlList isEmptyOrNil ifTrue: [urlList _ Array new: 1].
-	urlList at: 1 put: aURLString
+	urlList at: 1 put: aURLString.
+	self lastDirectory: nil.
+	self resourceManager adjustToNewServer: self resourceUrl from: oldResourceUrl
