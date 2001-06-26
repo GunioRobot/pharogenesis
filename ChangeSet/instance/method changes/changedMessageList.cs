cacf0d32b3394e6356e102d5e57e16a51c2d5472
sw <unknown>
2001-06-26 12:15:00 +0000

    no message

--- ChangeSet/instance/method changes/changedMessageList.cs
+++ ChangeSet/instance/method changes/changedMessageList.cs
@@ -1,11 +1,26 @@
 changedMessageList
 	"Used by a message set browser to access the list view information."
 
-	| messageList |
-	messageList _ SortedCollection new.
-	changeRecords associationsDo: 
-		[:clAssoc | 
-		clAssoc value methodChangeTypes associationsDo: 
-			[:mAssoc | (#(remove addedThenRemoved) includes: mAssoc value) ifFalse:
-				[messageList add: clAssoc key asString, ' ' , mAssoc key]]].
-	^ messageList asArray
+	| messageList classNameInFull classNameInParts |
+	messageList _ OrderedCollection new.
+	changeRecords associationsDo: [:clAssoc |
+		classNameInFull _ clAssoc key asString.
+		classNameInParts _ classNameInFull findTokens: ' '.
+
+		(clAssoc value allChangeTypes includes: #comment) ifTrue:
+			[messageList add:
+				(MethodReference new
+					setClassSymbol: classNameInParts first asSymbol
+					classIsMeta: false 
+					methodSymbol: #Comment 
+					stringVersion: classNameInFull, ' Comment')].
+
+		clAssoc value methodChangeTypes associationsDo: [:mAssoc |
+			(#(remove addedThenRemoved) includes: mAssoc value) ifFalse:
+				[messageList add:
+					(MethodReference new
+						setClassSymbol: classNameInParts first asSymbol
+						classIsMeta: classNameInParts size > 1 
+						methodSymbol: mAssoc key 
+						stringVersion: classNameInFull, ' ' , mAssoc key)]]].
+	^ messageList asSortedArray
