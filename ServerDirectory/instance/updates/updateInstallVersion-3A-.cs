71cfe37e9791c911cc9f0893b808a46ad077d282
mir <unknown>
2001-06-26 12:07:00 +0000

    no message

--- ServerDirectory/instance/updates/updateInstallVersion-3A-.cs
+++ ServerDirectory/instance/updates/updateInstallVersion-3A-.cs
@@ -1,27 +1,32 @@
 updateInstallVersion: newVersion
-	"For each server group, ask whether we want to put the new version marker (#Squeak2.3) at the end of the file.  Current version of Squeak must be the old one when this is done.
-	ServerDirectory new updateInstallVersion: 'Squeak2.3'      "
+	"For each server group, ask whether we want to put the new version marker (eg 'Squeak2.3') at the end of the file.  Current version of Squeak must be the old one when this is done.
+		ServerDirectory new updateInstallVersion: 'Squeak9.9test'
+"
+	| myServers updateStrm names choice indexPrefix listContents version versIndex |
+	[names _ ServerDirectory groupNames asSortedArray.
+	choice _ (SelectionMenu labelList: names selections: names) startUp.
+	choice == nil]
+		whileFalse:
+		[indexPrefix _ (choice endsWith: '*') 
+			ifTrue: [(choice findTokens: ' ') first]	"special for internal updates"
+			ifFalse: ['']. 	"normal"
+		myServers _ (ServerDirectory serverInGroupNamed: choice)
+						checkServersWithPrefix: indexPrefix
+						andParseListInto: [:x | listContents _ x].
+		myServers size = 0 ifTrue: [^ self].
 
-	| myServers updateStrm names choice |
-[names _ ServerDirectory groupNames asSortedArray.
-choice _ (SelectionMenu labelList: names selections: names) startUp.
-choice == nil] whileFalse: [
-	myServers _ (ServerDirectory groupNamed: choice) checkServers.
-	myServers size = 0 ifTrue: [self inform: 'checkServers failed on one of those'].
-	updateStrm _ myServers first getFileNamed: 'updates.list'.
+		version _ SystemVersion current version.
+		versIndex _ (listContents collect: [:pair | pair first]) indexOf: version.
+		versIndex = 0 ifTrue:
+			[^ self inform: 'There is no section in updates.list for your version'].  "abort"
 
-	Utilities position: updateStrm 	"checks for current OLD version"
-			atVersion: (Smalltalk at: #EToySystem) version.	
-	
-	"append name to updates"
-	updateStrm setToEnd.
-	(updateStrm skip: -1; next) == Character cr ifFalse: [
-		updateStrm nextPut: Character cr].
-	updateStrm nextPutAll: '#', newVersion; nextPut: Character cr.
+		"Append new version to updates following my version"
+		listContents _ listContents copyReplaceFrom: versIndex+1 to: versIndex with: {{newVersion. {}}}.
+		updateStrm _ ReadStream on:
+			(String streamContents: [:s | Utilities writeList: listContents toStream: s]).
 
-	myServers do: [:aServer |
-		updateStrm reset.
-		aServer putFile: updateStrm named: 'updates.list' retry: true.
-		Transcript cr; show: 'Update.list written on server ', aServer moniker].
-		
-	]
+		myServers do:
+			[:aServer | updateStrm reset.
+			aServer putFile: updateStrm named: indexPrefix ,'updates.list'.
+			Transcript cr; show: indexPrefix ,'updates.list written on server ', aServer moniker].
+		self closeGroup]
