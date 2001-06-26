6a1365e484ff8f13b8ac5ce3053ed3348dedac88
mir <unknown>
2001-06-26 12:06:00 +0000

    no message

---
+++ ServerDirectory/class/server groups/serverInGroupNamed-3A-.cs
@@ -0,0 +1,8 @@
+serverInGroupNamed: groupName
+	"Return the first (available) server in the group of this name."
+
+	| servers |
+	servers _ self serversInGroupNamed: groupName.
+	servers isEmpty
+		ifTrue: [self error: 'No server found in group "' , groupName asString , '".'].
+	^servers first
