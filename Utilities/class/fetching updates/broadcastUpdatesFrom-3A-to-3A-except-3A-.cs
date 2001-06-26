4e7f7d822fae6e4eb1bbff5fb9d387987d095b76
mir <unknown>
2001-06-26 12:08:00 +0000

    no message

--- Utilities/class/fetching updates/broadcastUpdatesFrom-3A-to-3A-except-3A-.cs
+++ Utilities/class/fetching updates/broadcastUpdatesFrom-3A-to-3A-except-3A-.cs
@@ -1,36 +1,28 @@
 broadcastUpdatesFrom: n1 to: n2 except: skipList
 "
-	ChangeSorter removeChangeSetsNamedSuchThat:
-		[:name | name first isDigit and: [name initialIntegerOrNil > 412]].
-	Utilities readServerUpdatesSaveLocally: true updateImage: true.
-	Utilities broadcastUpdatesFrom: 413 to: 999 except: #().
-
-	Utilities readServerUpdatesSaveLocally: true updateImage: false
-       The expression above ftps all updates not in the current image
-over to the local
-       hard disk, but does NOT absorb them into the current image
+	Note:  This method takes its list of files from the directory named 'updates',
+	which will have been created and filled by, eg,
+		Utilities readServerUpdatesSaveLocally: true updateImage: true.
+	These can then be rebroadcast to any server using, eg,
+		Utilities broadcastUpdatesFrom: 1 to: 9999 except: #(223 224).
+	If the files are already on the server, and it is only a matter
+	of copying them to the index for a different version, then use...
+		(ServerDirectory serverInGroupNamed: 'SqC Internal Updates*')
+			exportUpdatesExcept: #().
 "
-	| fileNames fileNamesInOrder fileNamesUnnumbered names choice file
-csname updateDirectory |
+	| fileNames fileNamesInOrder names choice file updateDirectory |
 	updateDirectory _ FileDirectory default directoryNamed: 'updates'.
 	fileNames _ updateDirectory fileNames select:
 		[:n | n first isDigit
 			and: [(n initialIntegerOrNil between: n1 and: n2)
 			and: [(skipList includes: n initialIntegerOrNil) not]]].
-	fileNamesInOrder _ fileNames asSortedCollection: [:a :b | a
-initialIntegerOrNil < b initialIntegerOrNil].
-	fileNamesUnnumbered _ fileNamesInOrder collect:
-		[:n | n copyFrom: (n findFirst: [:c | c isDigit not]) to: n size].
-	(csname _ fileNamesUnnumbered asBag sortedCounts first) key > 1
-		ifTrue: [self halt: 'Repeated name: ' , csname value].
-	(file _ fileNamesUnnumbered select: [:n | (n occurrencesOf: $.) > 1])
-size > 0
+	(file _ fileNames select: [:n | (n occurrencesOf: $.) > 1]) size > 0
 		ifTrue: [self halt: file first , ' has multiple periods'].
-	fileNamesInOrder with: fileNamesUnnumbered do:
-		[:n :nu | updateDirectory rename: n toBe: nu].
+	fileNamesInOrder _ fileNames asSortedCollection:
+		[:a :b | a initialIntegerOrNil < b initialIntegerOrNil].
 
 	names _ ServerDirectory groupNames asSortedArray.
 	choice _ (SelectionMenu labelList: names selections: names) startUp.
 	choice == nil ifTrue: [^ self].
-	(ServerDirectory groupNamed: choice) putUpdateMulti:
-fileNamesUnnumbered fromDirectory: updateDirectory
+	(ServerDirectory serverInGroupNamed: choice)
+		putUpdateMulti: fileNamesInOrder fromDirectory: updateDirectory
