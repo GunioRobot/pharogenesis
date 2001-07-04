cba1bfe789eaff98c249b9cb0140d12c1f72de95
sw <unknown>
2001-07-04 12:07:00 +0000

    no message

---
+++ Utilities/class/recent method submissions/openRecentSubmissionsBrowser.cs
@@ -0,0 +1,4 @@
+openRecentSubmissionsBrowser
+	"Open up a browser on the most recent methods submitted in the image; reuse any existing one found in the world."
+
+	self currentWorld openRecentSubmissionsBrowser: nil
