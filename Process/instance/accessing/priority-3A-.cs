bb9b8764f047879a18bf5a9608154c5651303834
ar <unknown>
2001-07-08 17:04:00 +0000

    no message

--- Process/instance/accessing/priority-3A-.cs
+++ Process/instance/accessing/priority-3A-.cs
@@ -1,6 +1,5 @@
 priority: anInteger 
 	"Set the receiver's priority to anInteger."
-
-	anInteger<=Processor highestPriority
+	(anInteger >= Processor lowestPriority and:[anInteger <= Processor highestPriority])
 		ifTrue: [priority _ anInteger]
-		ifFalse: [self error: 'priority too high']
+		ifFalse: [self error: 'Invalid priority: ', anInteger printString]
