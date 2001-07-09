03b925beab78cafd7440828915b50345dc7b9c0e
mdr <unknown>
2001-07-09 13:23:00 +0000

    no message

--- MailMessage/class/preferences/omittedHeaderFields.cs
+++ MailMessage/class/preferences/omittedHeaderFields.cs
@@ -1,53 +1,31 @@
 omittedHeaderFields
 	"Reply a list of fields to omit when displaying a nice simple message"
 
+	"Note that heads of the form
+		X-something: value
+	are filtered programatically.  This is done since we don't want any of them
+	and it is impossible to predict them in advance."
+
 	^ #(
 			'comments'
 			'priority'
 			'disposition-notification-to'
-			'x-envelope-info'
-			'x-mime-autoconverted'
 			'content-id'
 			'received'
-			'x-accept-language'
-			'x-mailer-version'
-			'x-unityuser'
 			'return-path'
 			'newsgroups'
 			'message-id'
 			'path'
 			'in-reply-to'
 			'sender'
-			'x-sender'
 			'fonts'
-			'x-mailer'
 			'mime-version'
-			'x-uidl'
 			'status'
 			'content-type'
 			'content-transfer-encoding'
 			'errors-to'
 			'keywords'
 			'references'
-			'x-vms-to'
-			'x-delivery-notice'
-			'x-reply-of'
-			'x-newsreader'
-			'x-icon'
-			'x-mailing-list'
-			'x-loop'
-			'x-priority'
-			'x-msmail-priority'
-			'x-mimeole'
-			'x-organization'
-			'x-originating-ip'
-			'x-sun-charset'
-			'x-url'
-			'x-nextstep-mailer'
-			'x-authentication-warning'
-			'x-lotus-fromdomain'
-			'x-blackmail'
-			'x-authenticated-timestamp'
 			'nntp-posting-host'
 			'lines'
 			'return-receipt-to'
@@ -62,4 +40,15 @@ omittedHeaderFields
 			'resent-date'
 			'resent-sender'
 			'resent-from'
+			'delivered-to'
+			'user-agent'
+			'content-class'
+			'thread-topic'
+			'thread-index'
+			'list-help',
+			'list-post',
+			'list-subscribe',
+			'list-id',
+			'list-unsubscribe',
+			'list-archive'
 		)
