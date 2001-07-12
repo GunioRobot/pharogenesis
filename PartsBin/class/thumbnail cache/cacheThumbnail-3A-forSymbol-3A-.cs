2de1bf5aa64f882838705265df3e35776522aefc
sw <unknown>
2001-07-12 19:06:00 +0000

    no message

---
+++ PartsBin/class/thumbnail cache/cacheThumbnail-3A-forSymbol-3A-.cs
@@ -0,0 +1,4 @@
+cacheThumbnail: aThumbnail forSymbol: aSymbol
+	"Cache the thumbnail provided as the graphic representing a parts-bin denizen whose name is the given symbol"
+
+	Thumbnails at: aSymbol put: aThumbnail
