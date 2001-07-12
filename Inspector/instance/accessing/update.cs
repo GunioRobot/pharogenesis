56251aee53d324a1d32ca6f3a681bb48b7a58afc
hmm <unknown>
2001-07-12 20:35:00 +0000

    no message

--- Inspector/instance/accessing/update.cs
+++ Inspector/instance/accessing/update.cs
@@ -3,6 +3,9 @@ update
 
 	selectionIndex = 0
 		ifFalse:
-			[contents _ self selectionPrintString.
+			[self contentsIsString
+				ifTrue: [contents _ self selection]
+				ifFalse: [contents _ self selectionPrintString].
+			self changed: #contents.
 			self changed: #selection.
 			self changed: #selectionIndex]
