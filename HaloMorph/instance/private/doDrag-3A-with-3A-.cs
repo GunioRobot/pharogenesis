32320cb6c22f5d3e06e8ab44987a1ccbffb92849
tk <unknown>
2001-07-14 11:04:00 +0000

    no message

--- HaloMorph/instance/private/doDrag-3A-with-3A-.cs
+++ HaloMorph/instance/private/doDrag-3A-with-3A-.cs
@@ -2,4 +2,4 @@ doDrag: evt with: dragHandle
 	| thePoint |
 	evt hand obtainHalo: self.
 	thePoint _ target point: evt position - positionOffset from: owner.
-	target setConstrainedPositionFrom:(target griddedPoint: thePoint).
+	target setConstrainedPosition:(target griddedPoint: thePoint) hangOut: true.
