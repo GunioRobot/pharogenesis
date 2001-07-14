0be6e5f0e70b592391b01baa2db62362cf8fde4d
tk <unknown>
2001-07-14 11:04:00 +0000

    no message

--- HaloMorph/instance/events/dragTarget-3A-.cs
+++ HaloMorph/instance/events/dragTarget-3A-.cs
@@ -2,5 +2,5 @@ dragTarget: event
 	"Begin dragging the target"
 	| thePoint |
 	thePoint _ target point: event position - positionOffset from: owner.
-	target setConstrainedPositionFrom: thePoint.
+	target setConstrainedPosition: thePoint hangOut: true.
 	event hand newMouseFocus: self.
