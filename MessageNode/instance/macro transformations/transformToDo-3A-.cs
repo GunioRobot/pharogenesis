733dfe86b6070f40d6a5d8f9f0d1aac893861532
hmm <unknown>
2001-07-15 22:22:00 +0000

    no message

--- MessageNode/instance/macro transformations/transformToDo-3A-.cs
+++ MessageNode/instance/macro transformations/transformToDo-3A-.cs
@@ -1,7 +1,7 @@
 transformToDo: encoder
 	" var _ rcvr. L1: [var <= arg1] Bfp(L2) [block body. var _ var + inc] 
 Jmp(L1) L2: "
-	| limit increment block initStmt test incStmt limitInit blockVar |
+	| limit increment block initStmt test incStmt limitInit blockVar myRange blockRange |
 	"First check for valid arguments"
 	((arguments last isMemberOf: BlockNode)
 			and: [arguments last numberOfArguments = 1])
@@ -17,7 +17,9 @@ Jmp(L1) L2: "
 		[selector _ SelectorNode new key: #to:by:do: code: #macro].
 
 	"Now generate auxiliary structures"
+	myRange _ encoder rawSourceRanges at: self ifAbsent: [1 to: 0].
 	block _ arguments last.
+	blockRange _ encoder rawSourceRanges at: block ifAbsent: [1 to: 0].
 	blockVar _ block firstArgument.
 	initStmt _ AssignmentNode new variable: blockVar value: receiver.
 	limit _ arguments at: 1.
@@ -32,13 +34,16 @@ Jmp(L1) L2: "
 	test _ MessageNode new receiver: blockVar
 			selector: (increment key > 0 ifTrue: [#<=] ifFalse: [#>=])
 			arguments: (Array with: limit)
-			precedence: precedence from: encoder.
+			precedence: precedence from: encoder
+			sourceRange: (myRange first to: blockRange first).
 	incStmt _ AssignmentNode new
 			variable: blockVar
 			value: (MessageNode new
 				receiver: blockVar selector: #+
 				arguments: (Array with: increment)
-				precedence: precedence from: encoder).
+				precedence: precedence from: encoder)
+			from: encoder
+			sourceRange: (myRange last to: myRange last).
 	arguments _ (Array with: limit with: increment with: block)
 		, (Array with: initStmt with: test with: incStmt with: limitInit).
 	^ true
