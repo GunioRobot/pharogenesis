c56d33ae9299b8e8d2773cccc39fb5d7f6896b7c
hmm <unknown>
2001-07-16 20:12:00 +0000

    no message

--- Parser/instance/scanning/advance.cs
+++ Parser/instance/scanning/advance.cs
@@ -1,11 +1,12 @@
 advance
-    | this |
-    prevMark _ hereMark.
-    prevToken _ "Now means prev size"
-        self previousTokenSize.
-    this _ here.
-    here _ token.
-    hereType _ tokenType.
-    hereMark _ mark.
-    self scanToken.
-    ^this
+	| this |
+	prevMark _ hereMark.
+	prevEnd _ hereEnd.
+	this _ here.
+	here _ token.
+	hereType _ tokenType.
+	hereMark _ mark.
+	hereEnd _ source position - (source atEnd ifTrue: [hereChar == 30 asCharacter ifTrue: [0] ifFalse: [1]] ifFalse: [2]).
+	self scanToken.
+	"Transcript show: 'here: ', here printString, ' mark: ', hereMark printString, ' end: ', hereEnd printString; cr."
+	^this
