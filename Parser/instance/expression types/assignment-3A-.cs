e1be3089b3f3490d060297bf47c179e0b06dc1ec
hmm <unknown>
2001-07-16 18:47:00 +0000

    no message

--- Parser/instance/expression types/assignment-3A-.cs
+++ Parser/instance/expression types/assignment-3A-.cs
@@ -1,13 +1,15 @@
 assignment: varNode
 	" var '_' expression => AssignmentNode."
-	| loc |
+	| loc start |
 	(loc _ varNode assignmentCheck: encoder at: prevMark + requestorOffset) >= 0
 		ifTrue: [^self notify: 'Cannot store into' at: loc].
+	start _ self startOfNextToken.
 	varNode nowHasDef.
 	self advance.
 	self expression ifFalse: [^self expected: 'Expression'].
 	parseNode _ AssignmentNode new
 				variable: varNode
 				value: parseNode
-				from: encoder.
+				from: encoder
+				sourceRange: (start to: self endOfLastToken).
 	^true
