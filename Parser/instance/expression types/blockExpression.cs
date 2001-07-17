dacc4eefeaaa69abfbb57a6e10fe535d144d4c4c
hmm <unknown>
2001-07-17 21:03:00 +0000

    no message

--- Parser/instance/expression types/blockExpression.cs
+++ Parser/instance/expression types/blockExpression.cs
@@ -1,10 +1,10 @@
 blockExpression
 	"[ ({:var} |) (| {temps} |) (statements) ] => BlockNode."
 
-	| variableNodes temporaryBlockVariables |
+	| variableNodes temporaryBlockVariables start |
 
 	variableNodes _ OrderedCollection new.
-
+	start _ prevMark + requestorOffset.
 	"Gather parameters."
 	[self match: #colon] whileTrue: [variableNodes addLast: (encoder autoBind: self argumentName)].
 	(variableNodes size > 0 & (hereType ~~ #rightBracket) and: [(self match: #verticalBar) not]) ifTrue: [^self expected: 'Vertical bar'].
@@ -14,6 +14,8 @@ blockExpression
 	parseNode temporaries: temporaryBlockVariables.
 
 	(self match: #rightBracket) ifFalse: [^self expected: 'Period or right bracket'].
+
+	encoder noteSourceRange: (self endOfLastToken to: self endOfLastToken) forNode: parseNode.
 
 	"The scope of the parameters and temporary block variables is no longer active."
 	temporaryBlockVariables do: [:variable | variable scope: -1].
