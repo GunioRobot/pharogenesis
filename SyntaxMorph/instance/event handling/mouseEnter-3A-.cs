5fcc52d1f1d7d8404b7103d46d7567a48f004d7c
tk <unknown>
2001-07-19 20:21:00 +0000

    no message

--- SyntaxMorph/instance/event handling/mouseEnter-3A-.cs
+++ SyntaxMorph/instance/event handling/mouseEnter-3A-.cs
@@ -2,6 +2,7 @@ mouseEnter: evt
 	"Highlight this level as a potential grab target"
 
 "Transcript cr; print: self; show: ' enter'."
-	self unhighlightOwner.
+	self rootTile isMethodNode ifFalse: [^ self]. 	"not in a script"
+	self unhighlightOwnerBorder.
 	self highlightForGrab: evt.
 	evt hand newKeyboardFocus: self.
