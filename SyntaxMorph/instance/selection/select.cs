e3433a8fe41558d121da2926728a0218218e6a17
tk <unknown>
2001-07-23 18:31:00 +0000

    no message

--- SyntaxMorph/instance/selection/select.cs
+++ SyntaxMorph/instance/selection/select.cs
@@ -1,5 +1,8 @@
 select
 	self deselect.
-	self color: Color lightBrown.
+	"Outer block is not colored and has no popup"
+	(owner isSyntaxMorph and: [owner nodeClassIs: MethodNode]) 
+		ifTrue: [self setDeselectedColor "normal"]
+		ifFalse: [self color: Color lightBrown].
 	self borderColor: #raised.
 	self offerPopUp.
