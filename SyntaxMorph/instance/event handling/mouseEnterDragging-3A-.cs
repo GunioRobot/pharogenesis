cfa3061ce8d9eb07ece7a6df2b78a62687f62fde
tk <unknown>
2001-07-25 10:09:00 +0000

    no message

--- SyntaxMorph/instance/event handling/mouseEnterDragging-3A-.cs
+++ SyntaxMorph/instance/event handling/mouseEnterDragging-3A-.cs
@@ -1,10 +1,14 @@
 mouseEnterDragging: evt
 	"Highlight this level as a potential drop target"
 
-"Transcript cr; print: self; show: ' enterDragging'."
+"self isBlockNode ifTrue: [Transcript cr; print: self; show: ' enterDragging']."
+	self rootTile isMethodNode ifFalse: [^ self]. 	"not in a script"
+
 	evt hand hasSubmorphs ifFalse: [^ self].  "Don't react to empty hand"
-	self unhighlightOwner.
-	self highlightForDrop: evt.
+	self unhighlightOwnerBorder.
+	self isBlockNode ifFalse: [self highlightForDrop: evt.
+		(self firstOwnerSuchThat: [:m | m isSyntaxMorph and: [m color = self dropColor]])
+			ifNotNilDo: [:m | m unhighlight]].
 
 	self isBlockNode ifTrue:
 		[(self firstOwnerSuchThat: [:m | m isSyntaxMorph and: [m isBlockNode]])
