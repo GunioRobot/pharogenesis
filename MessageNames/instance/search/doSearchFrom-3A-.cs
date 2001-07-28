4b95e4807a058a72e96af2f66f830499c2d719d6
sw <unknown>
2001-07-28 00:32:00 +0000

    no message

---
+++ MessageNames/instance/search/doSearchFrom-3A-.cs
@@ -0,0 +1,5 @@
+doSearchFrom: aPane
+	"The user hit the Search button -- treat it as a synonym for the user having hit the Return or Enter (or cmd-s) in the type-in pane"
+
+	aPane accept.
+	aPane selectAll
