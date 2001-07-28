95d622e1de482e029058ff51b2e15e99a22879b8
sw <unknown>
2001-07-28 02:16:00 +0000

    no message

---
+++ MessageNames/instance/initialization/inMorphicWindowLabeled-3A-.cs
@@ -0,0 +1,5 @@
+inMorphicWindowLabeled: labelString
+	"Answer a morphic window with the given label that can display the receiver"
+"MessageNames openMessageNames"
+
+	^ self inMorphicWindowWithInitialSearchString: nil
