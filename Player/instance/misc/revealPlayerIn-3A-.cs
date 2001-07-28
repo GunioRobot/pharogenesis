352023d6ee8e39cf232e1606a649eb92a9b6ef61
sw <unknown>
2001-07-28 01:05:00 +0000

    no message

--- Player/instance/misc/revealPlayerIn-3A-.cs
+++ Player/instance/misc/revealPlayerIn-3A-.cs
@@ -1,15 +1,24 @@
 revealPlayerIn: aWorld
+	"Reveal the receiver if at all possible in the world; once it's visible, flash its image for a bit, and leave it with its halo showing"
+
 	| aMorph |
-	(aMorph _ self costume) isInWorld ifTrue: [^ aMorph goHome].
+	(aMorph _ self costume) isInWorld ifTrue:
+		[aMorph goHome.
+		self indicateLocationOnScreen.
+		aMorph addHalo.
+		^ self].
 
 	"It's hidden somewhere; search for it"
 	aWorld submorphs do:
-		[:m | (m succeededInRevealing: self) ifTrue: [^ self]].
+		[:m | (m succeededInRevealing: self) ifTrue:  "will have obtained halo already"
+			[aWorld doOneCycle.
+			self indicateLocationOnScreen.
+			^ self]].
 
-	self inform: 
-'Sorry.  Unaccountably, this object
-seems to be irretrievably lost'
-
-	
+	"The morph is truly unreachable in this world at present.  So extract it from hyperspace, and place it at center of screen, wearing a halo."
+	aMorph isWorldMorph ifFalse:
+		[aWorld addMorphFront: aMorph.
+		aMorph position: aWorld bounds center.
+		aMorph addHalo]
 	
 	
