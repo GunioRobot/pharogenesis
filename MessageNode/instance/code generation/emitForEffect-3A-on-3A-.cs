07b9ee014f9f8b6c1ea52f238ef66d7e62b7a774
hmm <unknown>
2001-07-28 14:39:00 +0000

    no message

--- MessageNode/instance/code generation/emitForEffect-3A-on-3A-.cs
+++ MessageNode/instance/code generation/emitForEffect-3A-on-3A-.cs
@@ -1,8 +1,8 @@
 emitForEffect: stack on: strm
-
+	"For #ifTrue:ifFalse: and #whileTrue: / #whileFalse: style messages, the pc is set to the jump instruction, so that mustBeBoolean exceptions can be shown correctly."
 	special > 0
 		ifTrue: 
-			[self perform: (MacroEmitters at: special) with: stack with: strm with: false.
-			pc _ 0]
+			[pc _ 0.
+			self perform: (MacroEmitters at: special) with: stack with: strm with: false]
 		ifFalse: 
 			[super emitForEffect: stack on: strm]
