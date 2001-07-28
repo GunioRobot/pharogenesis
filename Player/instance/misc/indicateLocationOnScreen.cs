70aba820b3040c4f32a6ef8b0558f7917bdf3342
sw <unknown>
2001-07-28 01:03:00 +0000

    no message

---
+++ Player/instance/misc/indicateLocationOnScreen.cs
@@ -0,0 +1,11 @@
+indicateLocationOnScreen
+	"Give momentary feedback on screen until mouse button is clicked"
+
+	| bds |
+	bds _ self costume boundsInWorld.
+	5 timesRepeat:
+		[Display reverse: bds.
+		(Delay forMilliseconds: 80) wait.
+		Display reverse: bds.
+		(Delay forMilliseconds: 200) wait.].
+	costume changed
