277d034f39c35f884efab8bb65d513fa1c8b5b65
di <unknown>
2001-07-28 10:34:00 +0000

    no message

--- TextMorph/instance/linked frames/addPredecessor-3A-.cs
+++ TextMorph/instance/linked frames/addPredecessor-3A-.cs
@@ -1,7 +1,6 @@
 addPredecessor: evt
 	| newMorph |
-	newMorph _ TextMorph new text: text textStyle: textStyle wrap: wrapFlag
-			color: color predecessor: predecessor successor: self.
+	newMorph _ self copy predecessor: predecessor successor: self.
 	newMorph extent: self width @ 100.
 	predecessor ifNotNil: [predecessor setSuccessor: newMorph].
 	self setPredecessor: newMorph.
