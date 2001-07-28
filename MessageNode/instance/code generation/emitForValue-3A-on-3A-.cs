46ccc1e75992a8eecc7291eb2bbe100bfb12c516
hmm <unknown>
2001-07-28 14:40:00 +0000

    no message

--- MessageNode/instance/code generation/emitForValue-3A-on-3A-.cs
+++ MessageNode/instance/code generation/emitForValue-3A-on-3A-.cs
@@ -1,9 +1,9 @@
 emitForValue: stack on: strm
-
+	"For #ifTrue:ifFalse: and #whileTrue: / #whileFalse: style messages, the pc is set to the jump instruction, so that mustBeBoolean exceptions can be shown correctly."
 	special > 0
 		ifTrue: 
-			[self perform: (MacroEmitters at: special) with: stack with: strm with: true.
-			pc _ 0]
+			[pc _ 0.
+			self perform: (MacroEmitters at: special) with: stack with: strm with: true]
 		ifFalse: 
 			[receiver ~~ nil ifTrue: [receiver emitForValue: stack on: strm].
 			arguments do: [:argument | argument emitForValue: stack on: strm].
