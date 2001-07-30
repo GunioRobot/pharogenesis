7aeb4a9149f576f1e30a21ad6085006e6daebe67
hmm <unknown>
2001-07-30 20:43:00 +0000

    no message

--- ContextPart/instance/system simulation/completeCallee-3A-.cs
+++ ContextPart/instance/system simulation/completeCallee-3A-.cs
@@ -1,9 +1,11 @@
 completeCallee: aContext
 	"Simulate the execution of bytecodes until a return to the receiver."
-	| ctxt current |
+	| ctxt current ctxt1 |
 	ctxt _ aContext.
 	[ctxt == current or: [ctxt hasSender: self]]
 		whileTrue: 
 			[current _ ctxt.
-			ctxt _ ctxt step].
-	self stepToSendOrReturn
+			ctxt1 _ ctxt quickStep.
+			ctxt1 ifNil: [self halt].
+			ctxt _ ctxt1].
+	^self stepToSendOrReturn
