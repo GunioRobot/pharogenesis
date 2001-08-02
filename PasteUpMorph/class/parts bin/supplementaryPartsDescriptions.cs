8cb40b8e6d49db2656d2b2236ecbcd017c2cbe19
sw <unknown>
2001-08-02 16:29:00 +0000

    no message

---
+++ PasteUpMorph/class/parts bin/supplementaryPartsDescriptions.cs
@@ -0,0 +1,7 @@
+supplementaryPartsDescriptions
+	^ {DescriptionForPartsBin
+		formalName: 'Holder'
+		categoryList: #(Scripting)
+		documentation: 'A place for storing alternative pictures in an animation, ec.'
+		globalReceiverSymbol: #ScriptingSystem
+		nativitySelector: #prototypicalHolder}
