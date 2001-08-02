c8c5b6211659add227b868fd1e2793daab768199
sw <unknown>
2001-08-02 12:50:00 +0000

    no message

---
+++ GraphMorph/class/parts bin/descriptionForPartsBin.cs
@@ -0,0 +1,4 @@
+descriptionForPartsBin
+	^ self partName:	'Graph'
+		categories:		#('Useful')
+		documentation:	'A graph of numbers, normalized so the full range of values just fits my height.  I support a movable cursor that can be dragged with the mouse.'
