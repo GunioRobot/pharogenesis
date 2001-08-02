acc53aebac9012f1180c8670afdf28afce90dbad
sw <unknown>
2001-08-02 01:04:00 +0000

    no message

---
+++ DescriptionForPartsBin/instance/initialization/formalName-3A-categoryList-3A-documentation-3A-globalReceiverSymbol-3A-nativitySelector-3A-.cs
@@ -0,0 +1,8 @@
+formalName: aName categoryList: aList documentation: aDoc globalReceiverSymbol: aSym nativitySelector: aSel
+	"Set all of the receiver's instance variables from the parameters provided"
+
+	formalName _ aName.
+	categoryList _ aList.
+	documentation _ aDoc.
+	globalReceiverSymbol _ aSym.
+	nativitySelector  _ aSel
