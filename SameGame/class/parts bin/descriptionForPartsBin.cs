bedaaa9d515e4ca5bb468e879590a8001f66cdd1
sw <unknown>
2001-08-02 12:52:00 +0000

    no message

---
+++ SameGame/class/parts bin/descriptionForPartsBin.cs
@@ -0,0 +1,4 @@
+descriptionForPartsBin
+	^ self partName:	'Same'
+		categories:		#('Games')
+		documentation:	'A board game implementedby Tim Olson, based on a game originally written for UNIX by Eiji Fukumoto.'
