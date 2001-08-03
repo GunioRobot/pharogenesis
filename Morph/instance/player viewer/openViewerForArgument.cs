d2f2bd236984e6ce1769b42d1de89cf986a66b00
sw <unknown>
2001-08-03 18:40:00 +0000

    no message

--- Morph/instance/player viewer/openViewerForArgument.cs
+++ Morph/instance/player viewer/openViewerForArgument.cs
@@ -1,2 +1,7 @@
 openViewerForArgument
+	"Open up a viewer for a player associated with the morph in question.  Temporarily, if shift key is down, open up an instance browser on the morph itself, not the player, with tiles showing, instead"
+
+	ActiveEvent shiftPressed ifTrue:
+		[ActiveWorld abandonAllHalos.
+		^ self openInstanceBrowserWithTiles].
 	self presenter viewMorph: self
