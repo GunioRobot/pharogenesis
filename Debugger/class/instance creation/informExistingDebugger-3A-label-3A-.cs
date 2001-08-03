3b93d6d896b0d5ae229ce91b1006d55ceed13601
hmm <unknown>
2001-08-03 13:05:00 +0000

    no message

---
+++ Debugger/class/instance creation/informExistingDebugger-3A-label-3A-.cs
@@ -0,0 +1,22 @@
+informExistingDebugger: aContext label: aString
+	"Walking the context chain, we try to find out if we're in a debugger stepping situation.
+	If we find the relevant contexts, we must rearrange them so they look just like they would
+	if the methods were excuted outside of the debugger."
+	| ctx quickStepMethod oldSender baseContext |
+	ctx _ thisContext.
+	quickStepMethod _ ContextPart compiledMethodAt: #quickSend:to:with:super:.
+	[ctx sender == nil or: [ctx sender method == quickStepMethod]] whileFalse: [ctx _ ctx sender].
+	ctx sender == nil ifTrue: [^self].
+	baseContext _ ctx.
+	"baseContext is now the context created by the #quickSend... method."
+	oldSender _ ctx _ ctx sender home sender.
+	"oldSender is the context which originally sent the #quickSend... method"
+	[ctx == nil or: [ctx receiver isKindOf: self]] whileFalse: [ctx _ ctx sender].
+	ctx == nil ifTrue: [^self].
+	"ctx is the context of the Debugger method #doStep"
+	ctx receiver labelString: aString.
+	ctx receiver externalInterrupt: false; proceedValue: aContext receiver.
+	baseContext swapSender: baseContext sender sender sender.	"remove intervening contexts"
+	thisContext swapSender: oldSender.	"make myself return to debugger"
+	ErrorRecursion _ false.
+	^aContext
