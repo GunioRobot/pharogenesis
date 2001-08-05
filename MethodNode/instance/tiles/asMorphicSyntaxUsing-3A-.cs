990c4e2460adde08048da358456f553658a9ceff
tk <unknown>
2001-08-05 11:40:00 +0000

    no message

--- MethodNode/instance/tiles/asMorphicSyntaxUsing-3A-.cs
+++ MethodNode/instance/tiles/asMorphicSyntaxUsing-3A-.cs
@@ -1,13 +1,5 @@
 asMorphicSyntaxUsing: aClass
 	
-	| morph |
-	(morph _ aClass 
-		column: #method 
-		on: self)
-		borderWidth: 0.
-	self asMorphicSyntaxIn: morph.
-	block _ morph submorphs last.
-	block submorphs size = 1 ifTrue: [^ morph].	"keep '^ self' if that is the only thing in method"
-	block submorphs last decompile string = '^  self ' ifTrue: [
-		block submorphs last delete].
-	^ morph
+	^ Cursor wait showWhile: [
+		(aClass methodNodeOuter: self) finalAppearanceTweaks]
+		
