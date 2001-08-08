3986bfc7ec97ea51f3333364f7ecaec3988ae3ad
mir <unknown>
2001-08-08 17:58:00 +0000

    no message

--- Project/instance/file in-2F-out/urlForLoading.cs
+++ Project/instance/file in-2F-out/urlForLoading.cs
@@ -1,10 +1,15 @@
 urlForLoading
-	| aDir |
 	"compose a url that will load me in someone's browser"
-
-	aDir _ (self serverList ifNil: [^nil]) first.
-	aDir loaderUrl isEmptyOrNil ifTrue: [^nil].
-	^
-		aDir loaderUrl,
-		'?',
-		self versionedFileName encodeForHTTP
+	| myServer serverList myUrl downloadUrl |
+	serverList _ self serverList.
+	serverList isEmptyOrNil
+		ifTrue: [
+			urlList isEmptyOrNil ifTrue: [^nil].
+			downloadUrl _ urlList first asUrl downloadUrl]
+		ifFalse: [
+			myServer _ serverList first.
+			myUrl _ myServer altUrl.
+			myUrl last == $/
+				ifFalse: [myUrl _ myUrl , '/'].
+			downloadUrl _ myUrl].
+	^downloadUrl , (self name, FileDirectory dot,'html') encodeForHTTP
