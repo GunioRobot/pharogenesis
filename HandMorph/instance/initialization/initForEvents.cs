74c0eb1547156ff111ceff70d3724356129126a5
tk <unknown>
2001-08-09 16:55:00 +0000

    no message

--- HandMorph/instance/initialization/initForEvents.cs
+++ HandMorph/instance/initialization/initForEvents.cs
@@ -1,4 +1,5 @@
 initForEvents
+	mouseOverHandler _ nil.
 	lastMouseEvent _ MouseEvent new setType: #mouseMove position: 0@0 buttons: 0 hand: self.
 	lastEventBuffer _ {1. 0. 0. 0. 0. 0. nil. nil}.
 	self resetClickState.
