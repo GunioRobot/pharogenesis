c49b857873eb7b087e3890b487d75b184c3375b8
ar <unknown>
2001-08-09 15:38:00 +0000

    no message

---
+++ ChessMove/instance/printing/printOn-3A-.cs
@@ -0,0 +1,13 @@
+printOn: aStream
+	super printOn: aStream.
+	aStream nextPutAll:'('.
+	aStream nextPutAll: (#('' 'N' 'B' 'R' 'Q' 'K') at: movingPiece).
+	aStream nextPutAll: (String with: ($a asInteger + (sourceSquare - 1 bitAnd: 7)) asCharacter with: ($1 asInteger + (sourceSquare -1 bitShift: -3)) asCharacter).
+	capturedPiece = 0 ifTrue:[
+		aStream nextPutAll: '-'.
+	] ifFalse:[
+		aStream nextPutAll: 'x'.
+		aStream nextPutAll: (#('' 'N' 'B' 'R' 'Q' 'K') at: capturedPiece).
+	].
+	aStream nextPutAll: (String with: ($a asInteger + (destinationSquare - 1 bitAnd: 7)) asCharacter with: ($1 asInteger + (destinationSquare -1 bitShift: -3)) asCharacter).
+	aStream nextPutAll:')'.
