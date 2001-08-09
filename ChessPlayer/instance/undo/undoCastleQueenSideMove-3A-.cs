5daab8b0ff03860b089a676669758d3983ac40a6
ar <unknown>
2001-08-09 22:22:00 +0000

    no message

---
+++ ChessPlayer/instance/undo/undoCastleQueenSideMove-3A-.cs
@@ -0,0 +1,4 @@
+undoCastleQueenSideMove: move
+	self prepareNextMove. "in other words, remove extra kings"
+	self movePiece: move movingPiece from: move destinationSquare to: move sourceSquare.
+	self movePiece: Rook from: move sourceSquare-1 to: move sourceSquare-4.
