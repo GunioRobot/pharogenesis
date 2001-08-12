e253b3ab32f335f2412f327c50771c76175b4033
sw <unknown>
2001-08-12 22:25:00 +0000

    no message

--- MessageSet/instance/filtering/filterToMessagesWithPriorVersions.cs
+++ MessageSet/instance/filtering/filterToMessagesWithPriorVersions.cs
@@ -1,4 +1,8 @@
 filterToMessagesWithPriorVersions
 	"Filter down only to messages which have at least one prior version"
 
-	self notYetImplemented
+	self filterFrom:
+		[:aClass :aSelector |
+			(aClass notNil and: [aSelector notNil]) and:
+				[(self class isPseudoSelector: aSelector) not and:
+					[(VersionsBrowser versionCountForSelector: aSelector class: aClass) > 1]]]
