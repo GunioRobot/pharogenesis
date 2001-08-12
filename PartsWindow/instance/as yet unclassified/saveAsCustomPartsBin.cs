1ae850c0ae261476d52ea107e32fe29c3d0d5515
sw <unknown>
2001-08-12 17:16:00 +0000

    no message

--- PartsWindow/instance/as yet unclassified/saveAsCustomPartsBin.cs
+++ PartsWindow/instance/as yet unclassified/saveAsCustomPartsBin.cs
@@ -1,11 +1,2 @@
 saveAsCustomPartsBin
-	| aBin |
-	self closeEditing.
-	labelString = 'Standard Parts' ifTrue: [self setLabel: 'Custom Parts'].
-	aBin _ self veryDeepCopy.
-
-	ScriptingSystem setCustomPartsBinFrom: aBin.
-	self inform: 
-'Okay; from now on, when you request
-''custom parts bin'' from the ''authoring tools...''
-menu, you will obtain a copy of this parts bin'
+	self inform: 'this feature is obsolete, as, indeed, is this entire tool'
