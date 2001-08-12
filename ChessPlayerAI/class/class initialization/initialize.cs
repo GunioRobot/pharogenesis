153b11459fab79aae98834ee6b44eaab105894e6
ar <unknown>
2001-08-12 14:39:00 +0000

    no message

---
+++ ChessPlayerAI/class/class initialization/initialize.cs
@@ -0,0 +1,11 @@
+initialize
+	"ChessPlayerAI initialize"
+	AlphaBetaGiveUp _ -29990.
+	AlphaBetaIllegal _ -31000.
+	AlphaBetaMaxVal _ 30000.
+	AlphaBetaMinVal _ -30000.
+	ValueAccurate _ 2.
+	ValueBoundary _ 4.
+	ValueLowerBound _ 4.
+	ValueUpperBound _ 5.
+	ValueThreshold _ 200.
