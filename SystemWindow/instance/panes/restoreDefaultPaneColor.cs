8663e4b3a38cdfb1b8a6dbd70b6163942d13e1c5
ar <unknown>
2001-08-15 23:44:00 +0000

    no message

--- SystemWindow/instance/panes/restoreDefaultPaneColor.cs
+++ SystemWindow/instance/panes/restoreDefaultPaneColor.cs
@@ -2,4 +2,5 @@ restoreDefaultPaneColor
 	"Useful when changing from monochrome to color display"
 
 	self setStripeColorsFrom: self paneColor.
-	paneMorphs do: [:p | p color: self paneColor].
+	Preferences alternativeWindowLook ifFalse:[
+		paneMorphs do: [:p | p color: self paneColor]].
