1001f5306a9cddd0523a15498b7c490499465ccc
ar <unknown>
2001-08-15 23:43:00 +0000

    no message

--- SystemWindow/instance/panes/replacePane-3A-with-3A-.cs
+++ SystemWindow/instance/panes/replacePane-3A-with-3A-.cs
@@ -1,13 +1,22 @@
 replacePane: oldPane with: newPane
+	"Make newPane exactly occupy the position and extent of oldPane"
 
+	| aLayoutFrame hadDep |
+	hadDep _ model dependents includes: oldPane.
+	oldPane owner replaceSubmorph: oldPane by: newPane.
 	newPane
 		position: oldPane position;
 		extent: oldPane extent.
-	oldPane visible: false; lock: true; extent: 10@10.
-	newPane visible: true; lock: false.
-	(submorphs includes: newPane) ifFalse: [self addMorph: newPane].
-	paneMorphs _ paneMorphs collect: [ :each |
-		each == oldPane ifTrue: [newPane] ifFalse: [each]
-	].
-	self changed.
+	aLayoutFrame _ oldPane layoutFrame.
+	paneMorphs _ paneMorphs collect:
+		[:each |
+		each == oldPane ifTrue: [newPane] ifFalse: [each]].
+	aLayoutFrame ifNotNil: [newPane layoutFrame: aLayoutFrame].
+	Preferences alternativeWindowLook
+		ifTrue:[newPane color: Color transparent]
+		ifFalse:[oldPane color = self paneColor ifTrue: [newPane color: self paneColor]].
+	
+	hadDep ifTrue: [model removeDependent: oldPane. model addDependent: newPane].
+
+	self changed
 
