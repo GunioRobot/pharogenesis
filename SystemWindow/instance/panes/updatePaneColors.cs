c518a72094ee0c5690f2ed7f33a3f3e8c40e9d5e
ar <unknown>
2001-08-15 23:42:00 +0000

    no message

--- SystemWindow/instance/panes/updatePaneColors.cs
+++ SystemWindow/instance/panes/updatePaneColors.cs
@@ -2,4 +2,5 @@ updatePaneColors
 	"Useful when changing from monochrome to color display"
 
 	self setStripeColorsFrom: self paneColorToUse.
-	paneMorphs do: [:p | p color: self paneColorToUse].
+	Preferences alternativeWindowLook ifFalse:[
+		paneMorphs do: [:p | p color: self paneColorToUse]].
