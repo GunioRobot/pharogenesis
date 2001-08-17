5e14445345ef7ebad9bedf39b56a722c04e8c53d
ar <unknown>
2001-08-17 16:52:00 +0000

    no message

--- BorderedMorph/instance/accessing/borderColor-3A-.cs
+++ BorderedMorph/instance/accessing/borderColor-3A-.cs
@@ -1,5 +1,6 @@
 borderColor: colorOrSymbolOrNil
-
+	self doesBevels ifFalse:[
+		colorOrSymbolOrNil isColor ifFalse:[^self]].
 	borderColor = colorOrSymbolOrNil ifFalse: [
 		borderColor _ colorOrSymbolOrNil.
 		self changed].
