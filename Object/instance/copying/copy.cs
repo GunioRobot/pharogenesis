17b8336462aaf4efdccd8a93ffa34c608e4c47a4
ajh <unknown>
2001-08-18 21:25:00 +0000

    no message

--- Object/instance/copying/copy.cs
+++ Object/instance/copying/copy.cs
@@ -1,5 +1,4 @@
 copy
-	"Answer another instance just like the receiver. Subclasses typically 
-	override this method; they typically do not override shallowCopy."
+	"Answer another instance just like the receiver. Subclasses typically override postCopy; they typically do not override shallowCopy."
 
-	^self shallowCopy
+	^self shallowCopy postCopy
