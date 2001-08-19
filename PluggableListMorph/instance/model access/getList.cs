364cdbccd6e4c25f1248c6061a21d71b9d960fa3
ls <unknown>
2001-08-19 14:16:00 +0000

    no message

--- PluggableListMorph/instance/model access/getList.cs
+++ PluggableListMorph/instance/model access/getList.cs
@@ -1,7 +1,7 @@
 getList
-	"Answer the list to be displayed."
-	| lst |
+	"Answer the list to be displayed.  Caches the returned list in the 'list' ivar"
 	getListSelector == nil ifTrue: [^ #()].
-	lst _ model perform: getListSelector.
-	lst == nil ifTrue: [^ #()].
-	^ lst
+	list _ model perform: getListSelector.
+	list == nil ifTrue: [^ #()].
+	list _ list collect: [ :item | item asStringOrText ].
+	^ list
