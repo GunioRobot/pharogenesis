8ee10e9df50ddc19dc9d3b8ecaf04be684d39aca
ls <unknown>
2001-08-19 14:36:00 +0000

    no message

--- PluggableListMorph/instance/updating/verifyContents.cs
+++ PluggableListMorph/instance/updating/verifyContents.cs
@@ -1,14 +1,15 @@
 verifyContents
-	| newList existingSelection anIndex |
-	"Called periodically, or at least on window reactivation to react to possible structural changes.  Update contents if necessary."
+	"Verify the contents of the receiver, reconstituting if necessary.  Called whenever window is reactivated, to react to possible structural changes.  Also called periodically in morphic if the smartUpdating preference is true"
+	| newList existingSelection anIndex oldList |
+	oldList _ list ifNil: [ #() ].
 	newList _ self getList.
-	((list == newList) "fastest" or: [list = newList]) ifTrue: [^ self].
-	self flash.  "list has changed beneath us; could get annoying, but hell"
-	existingSelection _ selection.
-	self list: newList.
-	(anIndex _ newList indexOf: existingSelection ifAbsent: [nil])
-		ifNotNil:
+	((oldList == newList) "fastest" or: [oldList = newList]) ifTrue: [^ self].
+	self flash.  "list has changed beneath us; give the user a little visual feedback that the contents of the pane are being updated."
+	existingSelection _ self selectionIndex > 0 ifTrue: [ oldList at: self selectionIndex ] ifFalse: [ nil ].
+	self updateList.
+	(existingSelection notNil and: [(anIndex _ list indexOf: existingSelection asStringOrText ifAbsent: [nil]) notNil])
+		ifTrue:
 			[model noteSelectionIndex: anIndex for: getListSelector.
 			self selectionIndex: anIndex]
-		ifNil:
+		ifFalse:
 			[self changeModelSelection: 0]
