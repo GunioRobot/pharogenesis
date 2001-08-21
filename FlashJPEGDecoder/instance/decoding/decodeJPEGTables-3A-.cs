77c368d32f37ce3866b976edf2207dc3ff6656a8
RAA <unknown>
2001-08-21 23:15:00 +0000

    no message

--- FlashJPEGDecoder/instance/decoding/decodeJPEGTables-3A-.cs
+++ FlashJPEGDecoder/instance/decoding/decodeJPEGTables-3A-.cs
@@ -1,5 +1,10 @@
 decodeJPEGTables: aStream
+"
+fixing the #atEnd allows the following to work:
+
+(FlashMorphReader on: (HTTPSocket httpGet: 'http://www.audi.co.uk/flash/intro1.swf' accept:'application/x-shockwave-flash')) processFile startPlaying openInWorld. 
+"
 	self setStream: aStream.
 	eoiSeen _ false.
 	self parseFirstMarker.
-	[eoiSeen] whileFalse:[self parseNextMarker].
+	[eoiSeen or: [stream atEnd]] whileFalse:[self parseNextMarker].
