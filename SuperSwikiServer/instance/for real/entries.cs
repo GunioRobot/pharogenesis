1559ac02bf4941715c30488d929c66ad5640c189
mir <unknown>
2001-08-23 22:16:00 +0000

    no message

--- SuperSwikiServer/instance/for real/entries.cs
+++ SuperSwikiServer/instance/for real/entries.cs
@@ -1,15 +1,3 @@
 entries
 
-	| answer c first |
-
-	answer _ self sendToSwikiProjectServer: {
-		'action: listallprojects'.
-	}.
-	(answer beginsWith: 'OK') ifFalse: [^#()].
-	c _ OrderedCollection new.
-	first _ true.
-	answer linesDo: [ :x |
-		first ifFalse: [c add: (Compiler evaluate: x)].
-		first _ false.
-	].
-	^c
+	^self allEntries
