67ed2fd1268a0a4cea6a0a64849eb7580de0ea99
ar <unknown>
2001-08-24 21:30:00 +0000

    no message

---
+++ ChessMoveGenerator/instance/moves-2D-general/moveWhiteKingAt-3A-.cs
@@ -0,0 +1,22 @@
+moveWhiteKingAt: square
+	| capture |
+	(KingMoves at: square) do:[:destSquare|
+		(myPieces at: destSquare) = 0 ifTrue:[
+			capture _ itsPieces at: destSquare.
+			(forceCaptures and:[capture = 0]) ifFalse:[
+				(moveList at: (lastMoveIndex _ lastMoveIndex + 1))
+					move: King from: square to: destSquare capture: capture.
+				capture = King ifTrue:[kingAttack _ moveList at: lastMoveIndex].
+			].
+		].
+	].
+	forceCaptures ifTrue:[^self].
+	"now consider castling"
+	self canCastleWhiteKingSide ifTrue:[
+		(moveList at: (lastMoveIndex _ lastMoveIndex + 1))
+			moveCastlingKingSide: King from: square to: square+2.
+	].
+	self canCastleWhiteQueenSide ifTrue:[
+		(moveList at: (lastMoveIndex _ lastMoveIndex + 1))
+			moveCastlingQueenSide: King from: square to: square-2.
+	].
