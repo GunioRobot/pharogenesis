006ddc6dfa7f8fd9c52687e687df402ae6e2439a
ar <unknown>
2001-08-24 16:06:00 +0000

    no message

---
+++ ChessPlayer/instance/moves-2D-general/findPossibleMovesAt-3A-.cs
@@ -0,0 +1,8 @@
+findPossibleMovesAt: square
+	"Find all possible moves at the given square. This method does not check if the move is legal, e.g., if the king of the player is under attack after the move. If the opponent is check mate (e.g., the king could be taken in the next move) the method returns nil. If the game is stale mate (e.g., the receiver has no move left) this method returns an empty array."
+	| moveList moves |
+	moveList _ board generator findPossibleMovesFor: self at: square.
+	moveList ifNil:[^nil].
+	moves _ moveList contents collect:[:move| move copy].
+	board generator recycleMoveList: moveList.
+	^moves
