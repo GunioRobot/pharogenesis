c4fec0ac82e7d09ef80fdbb6b68e613335c82616
ar <unknown>
2001-08-24 15:58:00 +0000

    no message

---
+++ ChessMoveGenerator/instance/moves-2D-pawns/blackPawnCaptureAt-3A-direction-3A-.cs
@@ -0,0 +1,16 @@
+blackPawnCaptureAt: square direction: dir
+	| destSquare move piece |
+	destSquare _ square-8-dir.
+	piece _ itsPieces at: destSquare.
+	piece = 0 ifFalse:[
+		(move _ moveList at: (lastMoveIndex _ lastMoveIndex + 1))
+			move: Pawn from: square to: destSquare capture: piece.
+		piece = King ifTrue:[kingAttack _ move].
+		destSquare <= 8 "a promotion"
+			ifTrue:[self promotePawn: move].
+	].
+	"attempt an en-passant capture"
+	enpassantSquare = destSquare ifTrue:[
+		(moveList at: (lastMoveIndex _ lastMoveIndex + 1))
+			captureEnPassant: Pawn from: square to: destSquare.
+	].
