e6441fe58976fbd2f79236609f7a34ed6d93b8f7
tk <unknown>
2001-08-24 13:14:00 +0000

    no message

--- SyntaxMorph/instance/pop ups/extendArrow.cs
+++ SyntaxMorph/instance/pop ups/extendArrow.cs
@@ -1,20 +1,10 @@
 extendArrow
 	"Return the extend arrow button.  It replaces the argument with a new message.
 	I am a number or getter messageNode."
-	| patch ok sel ss |
+	| patch |
 	
-	ok _ false.
-	(self nodeClassIs: LiteralNode) ifTrue: [
-		[self decompile asString asNumber] ifError: [:msg :rcvr | ^ nil].
-		ok _ true].
-	(self nodeClassIs: MessageNode) ifTrue: [
-		ss _ submorphs select: [:mm | mm isSyntaxMorph].
-		ss size > 1 ifTrue: [
-			(ss second nodeClassIs: SelectorNode) ifTrue: [
-				((sel _ ss second decompile asString) beginsWith: 'get') ifTrue: [
-					ok _ (sel atWrap: 4) isUppercase ]]]].	"a getter -- hope it is a number"
-
-	ok ifFalse: [^ nil].
+	self isNoun ifFalse: [^ nil].
+	self isDeclaration ifTrue: [^ nil].
 	patch _ (ImageMorph new image: (TileMorph classPool at: #SuffixPicture)).
 	patch on: #mouseDown send: #extend to: self.
 	^ patch
