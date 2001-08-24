8b359d4e066aab7314f6b71ae995d6bf3b09e8b0
ar <unknown>
2001-08-24 22:06:00 +0000

    no message

---
+++ ChessMoveGenerator/instance/moves-2D-pawns/whitePawnPushAt-3A-.cs
@@ -0,0 +1,20 @@
+whitePawnPushAt: square
+	"Pawns only move in one direction so check for which direction to use"
+	| destSquare move |
+	"Try to push this pawn"
+	destSquare _ square+8.
+
+	(myPieces at: destSquare) = 0 ifFalse:[^self].
+	(itsPieces at: destSquare) = 0 ifFalse:[^self].
+	(move _ moveList at: (lastMoveIndex _ lastMoveIndex + 1))
+		move: Pawn from: square to: destSquare.
+	destSquare > 56 "a promotion (can't be double-push so get out)"
+		ifTrue:[^self promotePawn: move].
+
+	"Try to double-push if possible"
+	square <= 16 ifFalse:[^self].
+	destSquare _ square+16.
+	(myPieces at: destSquare) = 0 ifFalse:[^self].
+	(itsPieces at: destSquare) = 0 ifFalse:[^self].
+	(moveList at: (lastMoveIndex _ lastMoveIndex + 1))
+		doublePush: Pawn from: square to: destSquare.
