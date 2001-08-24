47f674c2eec744927f669ddc583a062be0708768
tk <unknown>
2001-08-24 15:27:00 +0000

    no message

---
+++ SyntaxMorph/instance/node to morph/alanKwdSetter2-3A-isAConditional-3A-key-3A-args-3A-.cs
@@ -0,0 +1,22 @@
+alanKwdSetter2: aNode isAConditional: template key: key args: args
+	"translates
+		foo setHeading: 0
+	to
+		foo's heading _ 0
+	"
+	| kwdHolder wordy |
+	kwdHolder _ self
+		addToken: key
+		type: #keywordSetter
+		on: (SelectorNode new key: key code: nil "fill this in?").
+	wordy _ self translateToWordySetter: key.
+	kwdHolder firstSubmorph 
+		setProperty: #syntacticReformatting toValue: #keywordSetter;
+		contents: wordy;
+		emphasis: 1.
+	wordy = key asString ifFalse: [
+		kwdHolder firstSubmorph 
+			setProperty: #syntacticallyCorrectContents toValue: key asString].
+
+	(args first asMorphicSyntaxIn: self) setConditionalPartStyle
+			
