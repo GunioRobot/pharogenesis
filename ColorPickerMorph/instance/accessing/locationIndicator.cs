e329488245f31cbc6097e9584ddb4f3c2d53152c
ar <unknown>
2001-08-25 20:44:00 +0000

    no message

---
+++ ColorPickerMorph/instance/accessing/locationIndicator.cs
@@ -0,0 +1,11 @@
+locationIndicator
+	| loc |
+	^self valueOfProperty: #locationIndicator ifAbsent:[
+		loc _ EllipseMorph new.
+		loc color: Color transparent; 
+			borderWidth: 1; 
+			borderColor: Color red; 
+			extent: 6@6.
+		self setProperty: #locationIndicator toValue: loc.
+		self addMorphFront: loc.
+		loc]
