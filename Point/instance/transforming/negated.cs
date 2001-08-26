0021168e83559b7a40fd0748e52899d87adb557f
ar <unknown>
2001-08-26 22:14:00 +0000

    no message

--- Point/instance/transforming/negated.cs
+++ Point/instance/transforming/negated.cs
@@ -1,4 +1,4 @@
 negated
 	"Answer a point whose x and y coordinates are the negatives of those of the receiver.  6/6/96 sw"
-
-	^ x negated @ y negated
+	"Optimized for speed -- ar 8/26/2001"
+	^ (0 - x) @ (0 - y)
