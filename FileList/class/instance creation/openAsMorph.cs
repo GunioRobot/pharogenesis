726703c71bbe6d971027502783d3baed7a6e07ef
sbw <unknown>
2001-08-29 19:37:00 +0000

    no message

--- FileList/class/instance creation/openAsMorph.cs
+++ FileList/class/instance creation/openAsMorph.cs
@@ -1,26 +1,28 @@
 openAsMorph
 	"Open a morphic view of a FileList on the default directory."
-	| dir aFileList window fileListTop |
+	| dir aFileList window upperFraction offset |
 	dir _ FileDirectory default.
 	aFileList _ self new directory: dir.
-	window _ (SystemWindow labelled: dir pathName) model: aFileList.
-
-	window addMorph: ((PluggableListMorph on: aFileList list: #volumeList selected: #volumeListIndex
-				changeSelected: #volumeListIndex: menu: #volumeMenu:) autoDeselect: false)
-		frame: (0@0 corner: 0.3@0.2).
-	window addMorph: (PluggableTextMorph on: aFileList text: #pattern accept: #pattern:)
-		frame: (0@0.2 corner: 0.3@0.3).
-	aFileList wantsOptionalButtons
-		ifTrue:
-			[window addMorph: aFileList optionalButtonRow frame: (0.3 @ 0 corner: 1 @ 0.08).
-			fileListTop _ 0.08]
-		ifFalse:
-			[fileListTop _ 0].
-
-	window addMorph: (PluggableListMorph on: aFileList list: #fileList selected: #fileListIndex
-				changeSelected: #fileListIndex: menu: #fileListMenu:)
-		frame: (0.3 @ fileListTop corner: 1@0.3).
-	window addMorph: (PluggableTextMorph on: aFileList text: #contents accept: #put:
-			readSelection: #contentsSelection menu: #fileContentsMenu:shifted:)
-		frame: (0@0.3 corner: 1@1).
+	window _ (SystemWindow labelled: dir pathName)
+				model: aFileList.
+	upperFraction _ 0.3.
+	offset _ 0.
+	self
+		addVolumesAndPatternPanesTo: window
+		at: (0 @ 0 corner: 0.3 @ upperFraction)
+		plus: offset
+		forFileList: aFileList.
+	self
+		addButtonsAndFileListPanesTo: window
+		at: (0.3 @ 0 corner: 1.0 @ upperFraction)
+		plus: offset
+		forFileList: aFileList.
+	window
+		addMorph: (PluggableTextMorph
+				on: aFileList
+				text: #contents
+				accept: #put:
+				readSelection: #contentsSelection
+				menu: #fileContentsMenu:shifted:)
+		frame: (0 @ 0.3 corner: 1 @ 1).
 	^ window
