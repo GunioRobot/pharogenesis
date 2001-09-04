118f689947b09cc7a67daf475f247b05999d611b
ar <unknown>
2001-09-04 13:26:00 +0000

    no message

--- ColorPickerMorph/instance/initialization/initialize.cs
+++ ColorPickerMorph/instance/initialization/initialize.cs
@@ -2,6 +2,7 @@ initialize
 	"Initialize the receiver.  Obey the modalColorPickers preference when deciding how to configure myself.  This is not quite satisfactory -- we'd like to have explicit calls tell us things like whether whether to be modal, whether to allow transparency, but for the moment, in grand Morphic fashion, this is rather inflexibly all housed right here"
 
 	super initialize.
+	self clipSubmorphs: true.
 	self buildChartForm.
 	
 	selectedColor _ Color white.
