179ed0ff759fca58a77651e225efc477cb2360ac
di <unknown>
2001-09-05 21:26:00 +0000

    no message

--- AssignmentNode/instance/code generation/emitForValue-3A-on-3A-.cs
+++ AssignmentNode/instance/code generation/emitForValue-3A-on-3A-.cs
@@ -1,4 +1,6 @@
 emitForValue: stack on: aStream
 
+	variable emitLoad: stack on: aStream.
 	value emitForValue: stack on: aStream.
-	variable emitStore: stack on: aStream
+	variable emitStore: stack on: aStream.
+	pc _ aStream position
