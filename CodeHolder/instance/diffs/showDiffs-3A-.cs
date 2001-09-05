701c2899020f2ab483d35ab58b257149a7e68857
sw <unknown>
2001-09-05 13:36:00 +0000

    no message

--- CodeHolder/instance/diffs/showDiffs-3A-.cs
+++ CodeHolder/instance/diffs/showDiffs-3A-.cs
@@ -1,2 +1,12 @@
 showDiffs: aBoolean
-	showDiffs _ aBoolean
+	"Set whether I'm showing diffs as indicated; use the global preference to determine which kind of diffs to institute."
+
+	self showingAnyKindOfDiffs
+		ifFalse:
+			[aBoolean ifTrue:
+				[contentsSymbol _ self defaultDiffsSymbol]]
+		ifTrue:
+			[aBoolean ifFalse:
+				[contentsSymbol _ #source]].
+	self setContentsToForceRefetch.
+	self contentsChanged
