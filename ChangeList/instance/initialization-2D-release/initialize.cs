476abbf9163ec8172b88bbfc45f5c981a3a1949f
sw <unknown>
2001-09-05 13:53:00 +0000

    no message

--- ChangeList/instance/initialization-2D-release/initialize.cs
+++ ChangeList/instance/initialization-2D-release/initialize.cs
@@ -1,5 +1,11 @@
 initialize
-	showDiffs _ Preferences diffsInChangeList.
+	"Initialize a blank ChangeList.  Set the contentsSymbol to reflect whether diffs will initally be shown or not"
+
+	contentsSymbol _ Preferences diffsInChangeList
+		ifTrue:
+			[self defaultDiffsSymbol]
+		ifFalse:
+			[#source].
 	changeList _ OrderedCollection new.
 	list _ OrderedCollection new.
 	listIndex _ 0.
