1f8abf1835dfc3de6647fceb1192f1115b1e8838
di <unknown>
2001-09-05 18:46:00 +0000

    no message

--- AssignmentNode/instance/code generation/emitForEffect-3A-on-3A-.cs
+++ AssignmentNode/instance/code generation/emitForEffect-3A-on-3A-.cs
@@ -1,4 +1,6 @@
 emitForEffect: stack on: aStream
 
+	variable emitLoad: stack on: aStream.
 	value emitForValue: stack on: aStream.
-	variable emitStorePop: stack on: aStream
+	variable emitStorePop: stack on: aStream.
+	pc _ aStream position
