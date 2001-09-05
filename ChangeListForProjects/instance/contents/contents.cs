c7c294278131fdbebb89fc1e4e648a7489fdbc04
sw <unknown>
2001-09-05 15:25:00 +0000

    no message

---
+++ ChangeListForProjects/instance/contents/contents.cs
@@ -0,0 +1,5 @@
+contents
+	^ self showingAnyKindOfDiffs
+		ifFalse: [self undiffedContents]
+		ifTrue: [self currentDiffedFromContents]
+			"Current is writing over one in list.  Show how I would change it"
