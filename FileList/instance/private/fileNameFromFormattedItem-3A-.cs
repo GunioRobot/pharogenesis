2949aef8a0a8471fbc5bc83b6f378209349e9792
rhi <unknown>
2001-09-08 02:17:00 +0000

    no message

--- FileList/instance/private/fileNameFromFormattedItem-3A-.cs
+++ FileList/instance/private/fileNameFromFormattedItem-3A-.cs
@@ -1,5 +1,14 @@
 fileNameFromFormattedItem: item
 	"Extract fileName and folderString from a formatted fileList item string"
-	| i |
-	(i _ item indexOf: $( ifAbsent: [0]) = 0 ifTrue: [^ item withBlanksTrimmed].
-	^ (item copyReplaceFrom: i to: (item findFirst: [:c | c = $)]) with: '') withBlanksTrimmed
+
+	| from to |
+	self sortingByName
+		ifTrue: [
+			from _ item lastIndexOf: $( ifAbsent: [0].
+			to _ item lastIndexOf: $) ifAbsent: [0]]
+		ifFalse: [
+			from _ item indexOf: $( ifAbsent: [0].
+			to _ item indexOf: $) ifAbsent: [0]].
+	^ (from * to = 0
+		ifTrue: [item]
+		ifFalse: [item copyReplaceFrom: from to: to with: '']) withBlanksTrimmed
