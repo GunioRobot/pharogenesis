fc39cb1a6bbe03197dfa99c11e8526689fe31ab8
sw <unknown>
2001-09-13 14:29:00 +0000

    no message

--- Vocabulary/instance/queries/atKey-3A-putMethodInterface-3A-.cs
+++ Vocabulary/instance/queries/atKey-3A-putMethodInterface-3A-.cs
@@ -1,4 +1,4 @@
 atKey: aKey putMethodInterface: anInterface
-	"Place the given interface at the given key"
+	"Place the given interface at the given key."
 
 	methodInterfaces at: aKey put: anInterface
