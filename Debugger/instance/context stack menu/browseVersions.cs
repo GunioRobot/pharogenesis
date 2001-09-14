26c4631ddaa4a3e72e180076dd2c744b571932a5
sw <unknown>
2001-09-14 00:26:00 +0000

    no message

--- Debugger/instance/context stack menu/browseVersions.cs
+++ Debugger/instance/context stack menu/browseVersions.cs
@@ -1,12 +1,13 @@
 browseVersions
 	"Create and schedule a message set browser on all versions of the 
 	currently selected message selector."
+
 	| class selector |
 	class _ self selectedClassOrMetaClass.
 	selector _ self selectedMessageName.
 	VersionsBrowser
 		browseVersionsOf: (class compiledMethodAt: selector)
-		class: self selectedClass
-		meta: self selectedClass isMeta
+		class: self selectedClass theNonMetaClass
+		meta: class isMeta
 		category: self selectedMessageCategoryName
 		selector: selector
