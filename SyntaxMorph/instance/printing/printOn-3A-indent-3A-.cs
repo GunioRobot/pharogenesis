d8a4cf61d175c8aa4b610a74ae544f2ee7e3d90c
tk <unknown>
2001-09-14 13:52:00 +0000

    no message

--- SyntaxMorph/instance/printing/printOn-3A-indent-3A-.cs
+++ SyntaxMorph/instance/printing/printOn-3A-indent-3A-.cs
@@ -1,43 +1,24 @@
 printOn: strm indent: level
-	| lev nodeClass parens cnt |
-	"Tree walk and produce text of the code.  #ST80.  Just do it in one big ugly method."
 
-	lev _ level.
-	(nodeClass _ parseNode class) == BlockNode ifTrue: [
-		owner isSyntaxMorph ifTrue: [
-			owner isMethodNode ifFalse: [strm nextPut: $[.  lev _ lev+1]]].
-				"normal block has []"
-	nodeClass == VariableNode ifTrue: ["nil out any old association"
-		parseNode key class == Association ifTrue: [
-			parseNode name: (parseNode name) key: nil code: (parseNode code)]].
-	(nodeClass == MessageNode or: [nodeClass == CascadeNode]) ifTrue: [
-		parseNode receiver ifNotNil: [parens _ true.  strm nextPut: $( ]].
-	"has a receiver"
-	nodeClass == MethodTempsNode ifTrue: [strm nextPut: $|; space].
-	cnt _ 0.
-	submorphs do: [:sub |
-		sub isSyntaxMorph ifTrue: [cnt _ cnt + 1.
-			(nodeClass == CascadeNode "and: [sub isCascadePart]") ifTrue:
-				[cnt > 2 ifTrue: [strm nextPutAll: '; ']].
-			nodeClass == BlockArgsNode ifTrue: [strm nextPut: $:].
-			sub printOn: strm indent: lev.	"<<<<### install the subnode"
-			(nodeClass == BlockNode) & (sub parseNode class == BlockArgsNode) not	&
-				(sub parseNode class == ReturnNode) not
-					ifTrue: [strm nextPut: $.].
-			(nodeClass == BlockNode) & (sub parseNode class == BlockArgsNode) not
-				ifTrue: [strm crtab: lev]
-				ifFalse: [self isMethodNode ifTrue: [strm crtab: lev] ifFalse: [strm space]].
-			].
-		(sub isKindOf: StringMorph) ifTrue: [strm nextPutAll: sub contents].
-		"return indent for ifTrue: ifFalse:"].
-	parens == true ifTrue: [strm nextPut: $) ].
-	"has a receiver"
-	nodeClass  == BlockNode ifTrue: [
-		owner isSyntaxMorph ifTrue: [
-			owner isMethodNode ifFalse: [strm nextPut: $] ]]].
-				"normal block has []"
-	nodeClass == BlockArgsNode ifTrue: [strm nextPut: $|; crtab: lev].
-	nodeClass == MethodTempsNode ifTrue: [strm nextPut: $|; crtab: lev].
-	nodeClass == MethodNode ifTrue: [
-		strm contents last "ugh!" == $. ifTrue: [strm skip: -1]].
-	"erase last period"
+	| nodeClass |
+
+	(self hasProperty: #ignoreNodeWhenPrinting) ifFalse: [
+		nodeClass _ parseNode class.
+		nodeClass == VariableNode ifTrue: [^self printVariableNodeOn: strm indent: level].
+		nodeClass == LiteralVariableNode ifTrue: [^self printVariableNodeOn: strm indent: level].
+		nodeClass == MessageNode ifTrue: [^self printMessageNodeOn: strm indent: level].
+		nodeClass == BlockNode ifTrue: [^self printBlockNodeOn: strm indent: level].
+		nodeClass == BlockArgsNode ifTrue: [^self printBlockArgsNodeOn: strm indent: level].
+		nodeClass == MethodNode ifTrue: [^self printMethodNodeOn: strm indent: level].
+		nodeClass == MethodTempsNode ifTrue: [^self printMethodTempsNodeOn: strm indent: level].
+		nodeClass == CascadeNode ifTrue: [^self printCascadeNodeOn: strm indent: level].
+		nodeClass == AssignmentNode ifTrue: [^self printAssignmentNodeOn: strm indent: level].
+	].
+	self
+		submorphsDoIfSyntax: [ :sub |
+			sub printOn: strm indent: level.
+			strm ensureASpace.
+		]
+		ifString: [ :sub |
+			self printSimpleStringMorph: sub on: strm
+		].
