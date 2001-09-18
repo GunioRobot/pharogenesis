6dee6696a266baeb973eebf37d2aadc50248c802
dew <unknown>
2001-09-18 02:30:00 +0000

    no message

--- BitBlt/class/examples/exampleOne.cs
+++ BitBlt/class/examples/exampleOne.cs
@@ -1,6 +1,10 @@
 exampleOne
-	"This tests BitBlt by displaying the result of all sixteen combination rules that BitBlt is capable of using. (Please see the comment in BitBlt for the meaning of the combination rules)."
-	| path |
+	"This tests BitBlt by displaying the result of all sixteen combination rules that BitBlt is capable of using. (Please see the comment in BitBlt for the meaning of the combination rules). This only works at Display depth of 1. (Rule 15 does not work?)"
+	| path displayDepth |
+
+	displayDepth _ Display depth.
+	Display newDepth: 1.
+
 	path _ Path new.
 	0 to: 3 do: [:i | 0 to: 3 do: [:j | path add: j * 100 @ (i * 75)]].
 	Display fillWhite.
@@ -8,6 +12,9 @@ exampleOne
 	1 to: 16 do: [:index | BitBlt
 			exampleAt: (path at: index)
 			rule: index - 1
-			fillColor: Color black]
+			fillColor: nil].
+
+	[Sensor anyButtonPressed] whileFalse: [].
+	Display newDepth: displayDepth.
 
 	"BitBlt exampleOne"
