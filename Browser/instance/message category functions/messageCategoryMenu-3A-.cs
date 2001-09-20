b1cd81baa4d397b2460eebaaa32a127d83450e30
dew <unknown>
2001-09-20 00:21:00 +0000

    no message

--- Browser/instance/message category functions/messageCategoryMenu-3A-.cs
+++ Browser/instance/message category functions/messageCategoryMenu-3A-.cs
@@ -7,11 +7,12 @@ fileOut
 reorganize
 alphabetize
 remove empty categories
+categorize all uncategorized
 new category...
 rename...
 remove'
-	lines: #(3 7)
+	lines: #(3 8)
 	selections:
 		#(buildMessageCategoryBrowser printOutMessageCategories fileOutMessageCategories
 		editMessageCategories alphabetizeMessageCategories removeEmptyCategories
-		addCategory renameCategory removeMessageCategory)
+		categorizeAllUncategorizedMethods addCategory renameCategory removeMessageCategory)
