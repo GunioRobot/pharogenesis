4e8ea1d2f0d5440c6db7cd0fa7f4a251100046c4
sw <unknown>
2001-09-20 00:16:00 +0000

    no message

--- HaloMorph/instance/private/doRecolor-3A-with-3A-.cs
+++ HaloMorph/instance/private/doRecolor-3A-with-3A-.cs
@@ -1,11 +1,13 @@
 doRecolor: evt with: aHandle
-	"Change the color of the target, if appropriate"
+	"The mouse went down in the 'recolor' halo handle.  Allow the user to change the color of the innerTarget"
+
 	evt hand obtainHalo: self.
 	(aHandle containsPoint: evt cursorPoint)
 		ifFalse:  "only do it if mouse still in handle on mouse up"
 			[self delete.
 			target addHalo: evt]
 		ifTrue:
-			[innerTarget changeColor.
+			[(Preferences propertySheetFromHalo == evt shiftPressed)
+				ifFalse:	[innerTarget openAPropertySheet]
+				ifTrue:	[innerTarget changeColor].
 			self showingDirectionHandles ifTrue: [self addHandles]]
-	
