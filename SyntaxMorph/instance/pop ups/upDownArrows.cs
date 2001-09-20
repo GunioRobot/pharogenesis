4bab7047bdc5021db8f1842c769ee19aa9f6759d
tk <unknown>
2001-09-20 16:33:00 +0000

    no message

--- SyntaxMorph/instance/pop ups/upDownArrows.cs
+++ SyntaxMorph/instance/pop ups/upDownArrows.cs
@@ -2,23 +2,30 @@ upDownArrows
 	"Return an array of two up/down arrow buttons.
 	It replaces the selector or arg with a new one.
 	I am a number or boolean or a selector (beep:, +,-,*,//,\\, or setX: incX: decX: for any X."
-	| patch sel any ok |
+	| patch any noMenu |
 	any _ (self nodeClassIs: LiteralNode) and: [parseNode key isNumber].
 	any _ any or: [(self nodeClassIs: VariableNode) and:
 						[(#('true' 'false') includes: self decompile asString)]].
-	any _ any or: [(self nodeClassIs: SelectorNode) and:
-				[ok _ #(beep: + - * // \\) includes: (sel _ parseNode key).
-				ok _ ok or: [(sel beginsWith: 'set') and: [(sel atWrap: 4) isUppercase]].
-				ok _ ok or: [sel size > 11 and:
-						[#('IncreaseBy:' 'DecreaseBy:' 'MultiplyBy:') includes: (sel last: 11)]].
-				ok]].
-	any _ any or: [(self nodeClassIs: SelectorNode) and:
-				[ok _ #(= ~= == ~~) includes: (sel _ parseNode key).
-				ok]].
+	noMenu _ any.
+
+	any _ any or: [self nodeClassIs: SelectorNode].	"arrows and menu of selectors"
+	any _ any or: [self nodeClassIs: KeyWordNode].
 	any ifFalse: [^ nil].
 
 	patch _ {(ImageMorph new image: TileMorph upPicture)
-				on: #mouseDown send: #event:arrow:upDown: to: self withValue: 1.
+				on: #mouseDown send: #upDown:event:arrow: to: self withValue: 1;
+				on: #mouseStillDown send: #upDownMore:event:arrow: 
+					to: self withValue: 1;
+				on: #mouseUp send: #upDownDone to: self.
 			(ImageMorph new image: TileMorph downPicture)
-				on: #mouseDown send: #event:arrow:upDown: to: self withValue: -1}.
+				on: #mouseDown send: #upDown:event:arrow: to: self withValue: -1;
+				on: #mouseStillDown send: #upDownMore:event:arrow: 
+					to: self withValue: -1;
+				on: #mouseUp send: #upDownDone to: self}.
+	noMenu ifFalse: [patch _ patch, {(RectangleMorph new)
+						extent: 6@10; borderWidth: 1;
+						borderColor: Color gray;
+						on: #mouseUp send: #selectorMenu to: self}.
+					patch last color: ((self nodeClassIs: SelectorNode) 
+						ifTrue: [Color lightGreen] ifFalse: [Color red darker])].
 	^ patch
