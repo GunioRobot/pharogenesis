54d5789deca3432566a5e3b74634f978412b558b
ar <unknown>
2001-09-21 23:06:00 +0000

    no message

--- Bitmap/instance/accessing/byteAt-3A-put-3A-.cs
+++ Bitmap/instance/accessing/byteAt-3A-put-3A-.cs
@@ -1,7 +1,7 @@
 byteAt: byteAddress put: byte
 	"Insert a byte into a Bitmap.  Note that this is a byte address and it is one-order.  For repeated use, create an instance of BitBlt and use pixelAt:put:.  See Form pixelAt:put:  7/1/96 tk"
-
 	| longWord shift lowBits longAddr |
+	(byte < 0 or:[byte > 255]) ifTrue:[^self errorImproperStore].
 	lowBits _ byteAddress - 1 bitAnd: 3.
 	longWord _ self at: (longAddr _ (byteAddress - 1 - lowBits) // 4 + 1).
 	shift _ (3 - lowBits) * 8.
