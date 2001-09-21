b9681cc98c406b63132ae88a9e220a15319ed92f
jcg <unknown>
2001-09-21 13:18:00 +0000

    no message

---
+++ HaloMorph/instance/meta-2D-actions/blueButtonDown-3A-.cs
@@ -0,0 +1,14 @@
+blueButtonDown: event
+	"Transfer the halo to the next likely recipient"
+	target ifNil:[^self delete].
+	event hand obtainHalo: self.
+	positionOffset _ event position - (target point: target position in: owner).
+	self isMagicHalo ifTrue:[
+		self isMagicHalo: false.
+		^self magicAlpha: 1.0].
+	"wait for drags or transfer"
+	event hand 
+		waitForClicksOrDrag: self 
+		event: event
+		selectors: { #transferHalo:. nil. nil. #dragTarget:. }
+		threshold: 5.
