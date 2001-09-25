7b87cd2803eeeda112660fdb8e386a04dad9c467
sw <unknown>
2001-09-25 21:52:00 +0000

    no message

--- FullVocabulary/instance/initialization/initialize.cs
+++ FullVocabulary/instance/initialization/initialize.cs
@@ -4,6 +4,6 @@ Vocabulary initialize
 "
 
 	super initialize.
-	vocabularyName _ #Full.
-	self documentation: '"Full" is all-encompassing vocabulary that embraces all methods understood by an object'.
+	vocabularyName _ #Object.
+	self documentation: '"Object" is all-encompassing vocabulary that embraces all methods understood by an object'.
 	self rigAFewCategories
