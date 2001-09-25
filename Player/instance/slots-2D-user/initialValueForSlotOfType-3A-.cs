19874fa2da127568926e80b623c42e817d5c9a09
sw <unknown>
2001-09-25 22:28:00 +0000

    no message

--- Player/instance/slots-2D-user/initialValueForSlotOfType-3A-.cs
+++ Player/instance/slots-2D-user/initialValueForSlotOfType-3A-.cs
@@ -1,12 +1,5 @@
 initialValueForSlotOfType: aType
 	"Answer the default initial value to ascribe to a slot of the given type"
 
-	aType == #number ifTrue: [^ (1 to: 9) atRandom].
-	aType == #boolean ifTrue: [^ true].
-	aType == #player ifTrue: [^ self costume presenter standardPlayer].
-	aType == #color ifTrue: [^ Color random].
-	aType == #string ifTrue: [^ 'abc'].
-	aType == #sound ifTrue: [^ 'croak'].
-	aType == #point ifTrue: [^ 20 @ 30].
-	aType == #buttonPhase ifTrue: [^ #buttonUp].
-	^ nil
+	^ (Vocabulary vocabularyForType: aType)
+		initialValueForASlotFor: self
