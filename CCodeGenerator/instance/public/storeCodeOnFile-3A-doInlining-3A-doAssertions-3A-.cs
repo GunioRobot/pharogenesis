fe6437b36209e4faeb441beb2b8e4c3ae65b536c
tpr <unknown>
2001-09-26 07:28:00 +0000

    no message

--- CCodeGenerator/instance/public/storeCodeOnFile-3A-doInlining-3A-doAssertions-3A-.cs
+++ CCodeGenerator/instance/public/storeCodeOnFile-3A-doInlining-3A-doAssertions-3A-.cs
@@ -1,11 +1,8 @@
 storeCodeOnFile: fileName doInlining: inlineFlag doAssertions: assertionFlag
 	"Store C code for this code base on the given file."
 
-	| stream realName |
-	"(self isTranslatingLocally and:[(fileName beginsWith: 'sq') not])
-		ifTrue:[realName _ 'sq', fileName]
-		ifFalse:[realName _ fileName]."
-	stream _ CrLfFileStream newFileNamed: fileName.
-	stream ifNil: [Error signal: 'Could not open C code file: ', realName].
+	| stream |
+	stream _ CrLfFileStream forceNewFileNamed: fileName.
+	stream ifNil: [Error signal: 'Could not open C code file: ', fileName].
 	self emitCCodeOn: stream doInlining: inlineFlag doAssertions: assertionFlag.
 	stream close
