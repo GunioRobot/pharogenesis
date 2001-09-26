0344f2d2acdb6054d47f14ba51da5e1db8fcf5df
tpr <unknown>
2001-09-26 07:27:00 +0000

    no message

--- InterpreterPlugin/class/translation/storeString-3A-onFileNamed-3A-.cs
+++ InterpreterPlugin/class/translation/storeString-3A-onFileNamed-3A-.cs
@@ -2,6 +2,6 @@ storeString: s onFileNamed: fileName
 	"Store the given string in a file of the given name."
 
 	| f |
-	f _ CrLfFileStream newFileNamed: fileName.
+	f _ CrLfFileStream forceNewFileNamed: fileName.
 	f nextPutAll: s.
 	f close.
