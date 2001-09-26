784cacec0b20a8c5bfb687f5025da38390aa57a5
sw <unknown>
2001-09-26 11:58:00 +0000

    no message

--- PhraseTileMorph/instance/initialization/setOperator-3A-type-3A-rcvrType-3A-argType-3A-.cs
+++ PhraseTileMorph/instance/initialization/setOperator-3A-type-3A-rcvrType-3A-argType-3A-.cs
@@ -1,9 +1,13 @@
 setOperator: opSymbol type: opType rcvrType: rcvrType argType: argType
+	"Set the operator, type, receiver type, and argument type for the phrase"
+
+	| aTileMorph |
 
 	resultType _ opType.
-	self color: (ScriptingSystem colorForType: opType).
+	opType ifNotNil: [self color: (ScriptingSystem colorForType: opType)].
 	self removeAllMorphs.
 	self addMorph: (TilePadMorph new setType: rcvrType).
-	self addMorphBack: ((TileMorph new setOperator: opSymbol asString) typeColor: color).
+	aTileMorph _ TileMorph new adoptVocabulary: self currentVocabulary.
+	self addMorphBack: ((aTileMorph setOperator: opSymbol asString) typeColor: color).
 	opSymbol numArgs = 1 ifTrue:
-		[self addMorphBack: (TilePadMorph new setType: (argType ifNil: [#object]))]
+		[self addMorphBack: (TilePadMorph new setType: (argType ifNil: [#Object]))]
