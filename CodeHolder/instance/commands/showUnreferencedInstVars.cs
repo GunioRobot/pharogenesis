c73f8d6ddd95fc506f1284048c34baff2fc2b3f0
sw <unknown>
2001-09-26 01:55:00 +0000

    no message

--- CodeHolder/instance/commands/showUnreferencedInstVars.cs
+++ CodeHolder/instance/commands/showUnreferencedInstVars.cs
@@ -2,7 +2,7 @@ showUnreferencedInstVars
 	"Search for all instance variables known to the selected class, and put up a list of those that have no references anywhere in the system.  The search includes superclasses, so that you don't need to navigate your way to the class that defines each inst variable in order to determine whether it is unreferenced"
 
 	| cls aList aReport |
-	(cls _ self selectedClass) ifNil: [^ self].
+	(cls _ self selectedClassOrMetaClass) ifNil: [^ self].
 	aList _ cls allUnreferencedInstanceVariables.
 	aList size == 0 ifTrue: [^ self inform: 'There are no unreferenced
 instance variables in
