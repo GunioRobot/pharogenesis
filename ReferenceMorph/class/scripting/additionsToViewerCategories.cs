11fc6f3e9f07196b0e607638b35daec376da635b
sw <unknown>
2001-09-26 04:04:00 +0000

    no message

--- ReferenceMorph/class/scripting/additionsToViewerCategories.cs
+++ ReferenceMorph/class/scripting/additionsToViewerCategories.cs
@@ -1,7 +1,7 @@
 additionsToViewerCategories
 	"Answer a list of (<categoryName> <list of category specs>) pairs that characterize the phrases this kind of morph wishes to add to various Viewer categories."
 
-	^ #((paintbox ((command makeNewDrawingIn: 'make a new drawing in the specified playfield' player))))
+	^ #((paintbox ((command makeNewDrawingIn: 'make a new drawing in the specified playfield' Player))))
 
 
 
