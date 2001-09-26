dfaba6a0b5bf11ade222db70250b04bb9516c9b2
tk <unknown>
2001-09-26 06:01:00 +0000

    no message

---
+++ SyntaxMorph/instance/scripting/tearOffTile.cs
@@ -0,0 +1,9 @@
+tearOffTile
+	"For a SyntaxMorph, this means give a copy of me"
+
+	| dup |
+	dup _ self duplicate.
+	ActiveHand attachMorph: dup.
+	Preferences tileTranslucentDrag
+		ifTrue: [^ dup lookTranslucent]
+		ifFalse: [^ dup align: dup topLeft with: ActiveHand position + self cursorBaseOffset]
