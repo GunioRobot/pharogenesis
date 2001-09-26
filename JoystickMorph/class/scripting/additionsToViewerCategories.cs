d64dca0da5cdc40902cd255944dde5f33189d704
sw <unknown>
2001-09-26 04:20:00 +0000

    no message

--- JoystickMorph/class/scripting/additionsToViewerCategories.cs
+++ JoystickMorph/class/scripting/additionsToViewerCategories.cs
@@ -2,9 +2,9 @@ additionsToViewerCategories
 	"Answer a list of (<categoryName> <list of category specs>) pairs that characterize the phrases this kind of morph wishes to add to various Viewer categories."
 
 	^ #((joystick (
-(slot amount 'The amount of displacement' number readOnly player getAmount unused unused)
-(slot angle 'The angular displacement' number readOnly player getAngle  unused  unused)
-(slot leftRight  'The horizontal displacement' number  readOnly player getLeftRight  unused  unused)
-(slot upDown 'The vertical displacement' number  readOnly player getUpDown unused unused))))
+(slot amount 'The amount of displacement' Number readOnly Player getAmount unused unused)
+(slot angle 'The angular displacement' Number readOnly Player getAngle  unused  unused)
+(slot leftRight  'The horizontal displacement' Number  readOnly Player getLeftRight  unused  unused)
+(slot upDown 'The vertical displacement' Number  readOnly Player getUpDown unused unused))))
 
 
