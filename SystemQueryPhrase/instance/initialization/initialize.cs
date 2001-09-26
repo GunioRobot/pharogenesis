d0b085723372eaaf749410a9c10d437501f50b1e
sw <unknown>
2001-09-26 03:04:00 +0000

    no message

--- SystemQueryPhrase/instance/initialization/initialize.cs
+++ SystemQueryPhrase/instance/initialization/initialize.cs
@@ -1,4 +1,7 @@
 initialize
+	"Initialize the receiver.  In this case we primarily seek to undo the damage done by inherited implementors of #initialize"
+
 	super initialize.
 	self removeAllMorphs.
-	resultType _ #boolean
+	resultType _ #Boolean.
+	self vResizing: #shrinkWrap
