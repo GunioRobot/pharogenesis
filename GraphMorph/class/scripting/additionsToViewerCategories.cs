a64fa9ea2fbc388a7bbbec7c3abc0d96790063bb
sw <unknown>
2001-09-26 04:19:00 +0000

    no message

--- GraphMorph/class/scripting/additionsToViewerCategories.cs
+++ GraphMorph/class/scripting/additionsToViewerCategories.cs
@@ -4,16 +4,16 @@ additionsToViewerCategories
 	^ #(
 
 	(basic (
-(slot cursor 	'The current cursor location, wrapped back to the beginning if appropriate' number	 readWrite player getCursor player setCursorWrapped:)
+(slot cursor 	'The current cursor location, wrapped back to the beginning if appropriate' Number	 readWrite Player getCursor Player setCursorWrapped:)
 
-(slot sampleAtCursor	'The sample value at the current cursor location' number readWrite player getSampleAtCursor player setSampleAtCursor:)))
+(slot sampleAtCursor	'The sample value at the current cursor location' Number readWrite Player getSampleAtCursor Player setSampleAtCursor:)))
 
 	(sampling (
-(slot cursor 	'The current cursor location, wrapped back to the beginning if appropriate' number	 readWrite player getCursor player setCursorWrapped:)
-(slot sampleAtCursor	'The sample value at the current cursor location' number readWrite player getSampleAtCursor player setSampleAtCursor:)
-(slot lastValue 'The last value obtained' number readWrite	player getLastValue player setLastValue:)
+(slot cursor 	'The current cursor location, wrapped back to the beginning if appropriate' Number	 readWrite Player getCursor Player setCursorWrapped:)
+(slot sampleAtCursor	'The sample value at the current cursor location' Number readWrite Player getSampleAtCursor Player setSampleAtCursor:)
+(slot lastValue 'The last value obtained' Number readWrite	Player getLastValue Player setLastValue:)
 (command clear 'Clear the graph of current contents')
 (command loadSineWave 'Load a sine wave as the current graph')
-(command loadSound: 'Load the specified sound into the current graph' sound)
+(command loadSound: 'Load the specified sound into the current graph' Sound)
 (command reverse 'Reverse the graph')
 (command play 'Play the current graph as a sound'))))
