ab7ea80b670fc3b54854d2342e72117ba1cb806c
sw <unknown>
2001-09-27 13:28:00 +0000

    no message

--- CategoryViewer/instance/support/booleanPhraseForRetrieverOfType-3A-retrieverOp-3A-player-3A-.cs
+++ CategoryViewer/instance/support/booleanPhraseForRetrieverOfType-3A-retrieverOp-3A-player-3A-.cs
@@ -2,18 +2,17 @@ booleanPhraseForRetrieverOfType: retrieverType retrieverOp: retrieverOp player: 
 	"Answer a boolean-valued phrase derived from a retriever (e.g. 'car's heading'); this is in order to assure that tiles laid down in a TEST area will indeed produce a boolean result"
 
 	| outerPhrase getterPhrase receiverTile  rel finalTile |
-	rel _ (retrieverType == #number)
-		ifTrue:		[#<]
-		ifFalse:		[#=].
-	outerPhrase _ PhraseTileMorph new setOperator: rel type: #boolean rcvrType: retrieverType argType: retrieverType.
-	getterPhrase _  PhraseTileMorph new setOperator: retrieverOp type: retrieverType rcvrType: #player.
-	getterPhrase submorphs last setSlotRefOperator: (Utilities inherentSelectorForGetter: retrieverOp).
-	receiverTile _ (self tileForPlayer: aPlayer) bePossessive.
+	rel _ (Vocabulary vocabularyForType:  retrieverType) comparatorForSampleBoolean.
+	outerPhrase _ PhraseTileMorph new setOperator: rel type: #Boolean rcvrType: retrieverType argType: retrieverType.
+	getterPhrase _  PhraseTileMorph new setOperator: retrieverOp type: retrieverType rcvrType: #Player.
+	getterPhrase submorphs last setSlotRefOperator: retrieverOp.
+	getterPhrase submorphs first changeTableLayout.
+	receiverTile _ aPlayer tileToRefer bePossessive.
 	receiverTile position: getterPhrase firstSubmorph position.
 	getterPhrase firstSubmorph addMorph: receiverTile.
 
 	outerPhrase firstSubmorph addMorph: getterPhrase.
-	finalTile _ aPlayer tileForArgType: retrieverType.	"comes with arrows"
+	finalTile _ ScriptingSystem tileForArgType: retrieverType.	"comes with arrows"
 	outerPhrase submorphs last addMorph: finalTile.
 	outerPhrase submorphs second submorphs last setBalloonText: (ScriptingSystem helpStringForOperator: rel).    
 	^ outerPhrase
