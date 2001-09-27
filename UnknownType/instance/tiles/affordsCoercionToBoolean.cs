7e902865962f52cae531b7cb72fac4245168c6db
sw <unknown>
2001-09-27 13:33:00 +0000

    no message

---
+++ UnknownType/instance/tiles/affordsCoercionToBoolean.cs
@@ -0,0 +1,4 @@
+affordsCoercionToBoolean
+	"Answer true if a tile of this data type, when dropped into a pane that demands a boolean, could plausibly be expanded into a comparison (of the form  frog < toad   or frog = toad) to provide a boolean expression"
+
+	^ false
