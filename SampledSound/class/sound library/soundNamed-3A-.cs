f26c2a0831d90b766d427521d19f3dd5f3f90bf2
sw <unknown>
2001-09-27 14:46:00 +0000

    no message

--- SampledSound/class/sound library/soundNamed-3A-.cs
+++ SampledSound/class/sound library/soundNamed-3A-.cs
@@ -1,14 +1,8 @@
 soundNamed: aString
-	"Answer the sound of the given name, or nil if there is no sound of that name."
+	"Answer the sound of the given name, or, if there is no sound of that name, put up an informer so stating, and answer nil"
+
 	"(SampledSound soundNamed: 'shutterClick') play"
 
-	| entry samples |
-	entry _ SoundLibrary
-		at: aString
-		ifAbsent:
-			[self inform: aString, ' not found in the Sound Library'.
-			^ nil].
-	entry ifNil: [^ nil].
-	samples _ entry at: 1.
-	samples class isBytes ifTrue: [samples _ self convert8bitSignedTo16Bit: samples].
-	^ self samples: samples samplingRate: (entry at: 2)
+	^ self soundNamed: aString ifAbsent:
+		[self inform: aString, ' not found in the Sound Library'.
+		nil]
