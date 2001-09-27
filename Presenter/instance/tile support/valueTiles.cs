da8ae3195074a73f73aa464182c15120cf23bc85
sw <unknown>
2001-09-27 17:43:00 +0000

    no message

--- Presenter/instance/tile support/valueTiles.cs
+++ Presenter/instance/tile support/valueTiles.cs
@@ -1,10 +1,11 @@
 valueTiles
+	"Answer some constant-valued tiles.  This dates back to very early etoy work in 1997, and presently has no senders"
+
 	| tiles |
 	tiles _ OrderedCollection new.
-	tiles add: (5 newTileMorphRepresentative typeColor: (ScriptingSystem colorForType: #number)).
-	tiles add: ((4 @ 9) newTileMorphRepresentative typeColor: (ScriptingSystem colorForType: #point)).
-	tiles add: (ColorTileMorph new typeColor: (ScriptingSystem colorForType: #color)).
-	tiles add: (TileMorph new typeColor: (ScriptingSystem colorForType: #number);
+	tiles add: (5 newTileMorphRepresentative typeColor: (ScriptingSystem colorForType: #Number)).
+	tiles add: (ColorTileMorph new typeColor: (ScriptingSystem colorForType: #Color)).
+	tiles add: (TileMorph new typeColor: (ScriptingSystem colorForType: #Number);
 			setExpression: '(180 atRandom)'
 			label: 'random').
 	tiles add: RandomNumberTile new.
