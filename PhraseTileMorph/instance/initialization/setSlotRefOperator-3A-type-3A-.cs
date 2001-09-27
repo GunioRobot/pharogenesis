634a2476526094e16e211de815769c6ef84dd9eb
sw <unknown>
2001-09-27 17:41:00 +0000

    no message

--- PhraseTileMorph/instance/initialization/setSlotRefOperator-3A-type-3A-.cs
+++ PhraseTileMorph/instance/initialization/setSlotRefOperator-3A-type-3A-.cs
@@ -1,7 +1,8 @@
 setSlotRefOperator: opSymbol type: opType
+	"Set the given symbol as the receiver's slot-reference operator, adding tiles to the receiver appropriately"
 
 	resultType _ opType.
 	self color: (ScriptingSystem colorForType: opType).
 	self removeAllMorphs.
-	self addMorph: (TilePadMorph new setType: #player).
+	self addMorph: (TilePadMorph new setType: #Player).
 	self addMorphBack: ((TileMorph new setSlotRefOperator: opSymbol asString) typeColor: color)
