7944c81b94d4840106a239074f0ff2b87b76854e
tk <unknown>
2001-09-28 10:06:00 +0000

    no message

--- SyntaxMorph/instance/pop ups/retract.cs
+++ SyntaxMorph/instance/pop ups/retract.cs
@@ -1,15 +1,18 @@
 retract
 	"replace this message with its receiver.  I am the message node."
-	| rec cascade |
-	self isCascadePart ifTrue:
+	| rec cascade msg |
+	(self nodeClassIs: CascadeNode) ifTrue:
 		["This is a piece of a cascaded message -- just delete it"
 		self deletePopup.
 		cascade _ owner.
 		self delete.
 		cascade setSelection: {cascade. nil. cascade}.
 		^ cascade acceptIfInScriptor].
-	rec _ submorphs detect: [:m | m isSyntaxMorph and: [m parseNode == parseNode receiver]].
 	self deletePopup.
-	owner replaceSubmorph: self by: rec.
+	(rec _ self receiverNode)
+		ifNil: [msg _ owner.
+			rec _ owner receiverNode.
+			msg owner replaceSubmorph: msg by: rec]
+		ifNotNil: [owner replaceSubmorph: self by: rec].
 	rec setSelection: {rec. nil. rec}.
 	rec acceptIfInScriptor.
