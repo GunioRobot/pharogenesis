602a61ebebe47629436bc34cece075c79cb762ae
tk <unknown>
2001-09-28 11:43:00 +0000

    no message

--- Player/class/other/wantsChangeSetLogging.cs
+++ Player/class/other/wantsChangeSetLogging.cs
@@ -1,5 +1,6 @@
 wantsChangeSetLogging
 	"Log changes for Player itself, but not for automatically-created subclasses like Player1, Player2, but *do* log it for uniclasses that have been manually renamed."
 
-	^ self == Player or:
-		[(self name beginsWith: 'Player') not]
+	^ (self == Player or:
+		[(self name beginsWith: 'Player') not]) or:
+			[Preferences universalTiles]
