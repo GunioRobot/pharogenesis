36e33c418b85b016c73d060c3ca1df73cc7e3f46
sw <unknown>
2001-09-28 08:52:00 +0000

    no message

--- Preferences/class/parameters/parameterAt-3A-default-3A-.cs
+++ Preferences/class/parameters/parameterAt-3A-default-3A-.cs
@@ -1,4 +1,4 @@
 parameterAt: aKey default: defaultValueBlock
-	"Return the Parameter setting at the given key.  If there is no entry for this key in the Parameters dictionary, create one with the value of defaultValueBlock as its value"
+	"Deprecated interface; no surviving senders in the released image, but clients probably still use"
 
-	^ Parameters at: aKey ifAbsent: [Parameters at: aKey put: defaultValueBlock value]
+	^ self parameterAt: aKey ifAbsentPut: defaultValueBlock
