773536962da3ad92733055cf4527a719c99157b8
tk <unknown>
2001-09-30 11:09:00 +0000

    no message

--- SyntaxMorph/instance/dropping-2F-grabbing/morphToDropInPasteUp-3A-.cs
+++ SyntaxMorph/instance/dropping-2F-grabbing/morphToDropInPasteUp-3A-.cs
@@ -2,12 +2,12 @@ morphToDropInPasteUp: aPasteUp
 	"If property #beScript is true, create a scriptor around me."
 
 	| actualObject itsSelector aScriptor adjustment handy tw blk |
-	self flag: #noteToTed.  "I changed PhraseTileMorph's version of this method to eliminate the flagshipInstance annoyance, and the below needs to be changed accordingly -- look at my method and notice diffs.  Besides eliminating the flagshipInstance test, one needs to be sure that the thing dropped is adjusted to reflect which instance of the uniclass is at hand.  sw 1/19/2001 02:30"
-
 	(self valueOfProperty: #beScript ifAbsent: [false]) ifFalse: [^ self].
 	self removeProperty: #beScript.
-
-	(actualObject _ self actualObject) ifNil: [^ self].
+	actualObject _ self actualObject ifNil: [
+					self valueOfProperty: #scriptedPlayer ifAbsent: [nil]].
+	actualObject ifNil: [^ self].
+	self removeProperty: #scriptedPlayer.
 	actualObject assureUniClass.
 
 	itsSelector _ self userScriptSelector.
@@ -16,25 +16,20 @@ morphToDropInPasteUp: aPasteUp
 			[adjustment _ 0@0.
 			actualObject scriptEditorFor: itsSelector]
 		ifTrue:
-			["It's a system-defined selector; construct an anonymous scriptor around it"
-			adjustment _ 60 @ 20.
+			[adjustment _ 60 @ 20.
 			actualObject newScriptorAround: self].
-
+	aScriptor ifNil: [^self].
 	handy _ aPasteUp primaryHand.
-	aScriptor ifNotNil: [
-		aScriptor position: handy position - adjustment.
-		aPasteUp addMorphFront: aScriptor.	"do this early so can find World"
-		(tw _ aScriptor findA: TwoWayScrollPane) ifNil: [
-			aScriptor useNewTiles.
-			itsSelector ifNil: ["blank script"
+
+	aScriptor position: handy position - adjustment.
+	aPasteUp addMorphFront: aScriptor.	"do this early so can find World"
+	aScriptor showingMethodPane ifFalse: [
+		"(tw _ aScriptor findA: TwoWayScrollPane) ifNil:
+			[itsSelector ifNil: ['blank script'.
 				tw _ aScriptor findA: TwoWayScrollPane.
-				blk _ tw scroller firstSubmorph "MethodNode" lastSubmorph "BlockNode".
-				blk addMorphFront: self.
-				"self position: self topLeft + (7@14)"]]].
-	aScriptor showSourceInScriptor.  "**This destroys most of the work done before**"
-	aScriptor hResizing: #shrinkWrap;
-		vResizing: #shrinkWrap;
-		cellPositioning: #topLeft.
-	(aScriptor isKindOf: ScriptEditorMorph) ifTrue:
-		[aScriptor playerScripted expungeEmptyUnRenamedScripts].
-	^ aScriptor ifNil: [self]
+				blk _ (tw scroller findA:  SyntaxMorph ""MethodNode"") findA: BlockNode.
+				blk addMorphFront: self]].
+		"
+		SyntaxMorph setSize: nil andMakeResizable: aScriptor.
+		].
+	^ aScriptor
