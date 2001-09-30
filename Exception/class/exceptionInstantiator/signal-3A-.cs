a2f1e21551d302b54bc782e3dfe676ed8894d421
ajh <unknown>
2001-09-30 21:54:00 +0000

    no message

--- Exception/class/exceptionInstantiator/signal-3A-.cs
+++ Exception/class/exceptionInstantiator/signal-3A-.cs
@@ -1,7 +1,4 @@
 signal: signalerText
-	"TFEI - Signal the occurrence of an exceptional condition with a specified textual description."
+	"Signal the occurrence of an exceptional condition with a specified textual description."
 
-	| ex |
-	ex := self new.
-	ex initialContext: thisContext sender.
-	^ex signal: signalerText
+	^ self new signal: signalerText
