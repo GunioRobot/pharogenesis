59f18e4f3bcc8e18df22e004ba65db2ef28e1b78
di <unknown>
2001-10-01 22:26:00 +0000

    no message

--- MessageSet/instance/contents/contents.cs
+++ MessageSet/instance/contents/contents.cs
@@ -1,9 +1,10 @@
 contents
+	"Answer the contents of the receiver"
+
 	^ contents == nil
 		ifTrue: [currentCompiledMethod _ nil. '']
 		ifFalse: [messageListIndex = 0 
 			ifTrue: [currentCompiledMethod _ nil. contents]
-			ifFalse: [editSelection == #byteCodes
-				ifTrue: [(self selectedClassOrMetaClass
-							compiledMethodAt: self selectedMessageName) symbolic]
+			ifFalse: [self showingByteCodes
+				ifTrue: [self selectedBytecodes]
 				ifFalse: [self selectedMessage]]]
