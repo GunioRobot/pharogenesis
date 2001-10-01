cc92136a968b447ba1c4f929b57b2a1b8d78bbd8
ajh <unknown>
2001-10-01 17:10:00 +0000

    no message

--- MailMessage/instance/parsing/timeFrom-3A-.cs
+++ MailMessage/instance/parsing/timeFrom-3A-.cs
@@ -1,18 +1,33 @@
 timeFrom: aString 
-	"Parse the date and time and answer the result as the number of seconds 
+	"Parse the date and time (rfc822) and answer the result as the number of seconds 
 	since the start of 1980."
-	| s t rawDelta delta |
+
+	| s t rawDelta delta plusOrMinus |
 	s _ ReadStream on: aString.
-	t _ (self readDateFrom: s) asSeconds.
+
 	"date part"
+	t _ ((self readDateFrom: s) ifNil: [Date today]) asSeconds.
+
 	[s atEnd or: [s peek isAlphaNumeric]]
 		whileFalse: [s next].
+
+	"time part"
 	s atEnd ifFalse: ["read time part (interpreted as local, regardless of sender's timezone)"
-		t _ t + (Time readFrom: s) asSeconds].
+		(s peek isDigit) ifTrue: [t _ t + (Time readFrom: s) asSeconds].
+		].
 	s skipSeparators.
-	('+-' includes: s peek)
-		ifTrue: 
-			[s peekFor: $+. rawDelta _ Integer readFrom: s.
-			delta _ rawDelta // 100 * 60 + (rawDelta \\ 100).
-			t _ t - (delta * 60)].
-	^ t - (Date newDay: 1 year: 1980) asSeconds"time started with 1980..."
+
+	"Check for a numeric time zone offset"
+	('+-' includes: s peek) ifTrue: 
+		[plusOrMinus _ s next.
+		rawDelta _ (s peek isDigit) ifTrue: [Integer readFrom: s] ifFalse: [0].
+		delta _ (rawDelta // 100 * 60 + (rawDelta \\ 100)) * 60.
+		t _ plusOrMinus = $+ ifTrue: [t - delta] ifFalse: [t + delta]].
+
+	"We ignore text time zone offsets like EST, GMT, etc..."
+
+	^ t - (Date newDay: 1 year: 1980) asSeconds
+
+"MailMessage new timeFrom: 'Thu, 22 Jun 2000 14:17:47 -500'"
+"MailMessage new timeFrom: 'Thu, 22 Jun 2000 14:17:47 --500'"
+"MailMessage new timeFrom: 'on, 04 apr 2001 14:57:32'"
