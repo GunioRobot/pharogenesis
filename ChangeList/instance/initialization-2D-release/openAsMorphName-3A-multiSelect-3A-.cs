1f99901059142ab03997fc700c1336046783c0b0
tpr <unknown>
2001-10-04 21:58:00 +0000

    no message

--- ChangeList/instance/initialization-2D-release/openAsMorphName-3A-multiSelect-3A-.cs
+++ ChangeList/instance/initialization-2D-release/openAsMorphName-3A-multiSelect-3A-.cs
@@ -1,30 +1,35 @@
-openAsMorphName: labelString multiSelect: multiSelect
-	"Open a morphic view for the messageSet, whose label is labelString.
+openAsMorphName: labelString multiSelect: multiSelect 
+	"Open a morphic view for the messageSet, whose label is labelString. 
 	The listView may be either single or multiple selection type"
-
 	| window listHeight listPane |
 	listHeight _ 0.4.
-
 	window _ (SystemWindow labelled: labelString)
-		model: self.
-
-	listPane _ (multiSelect
-				ifTrue: [PluggableListMorphOfMany]
-				ifFalse: [PluggableListMorph])
-			on: self
-			list: #list
-			selected: #listIndex
-			changeSelected: #toggleListIndex:
-			menu: (self showsVersions
-				ifTrue: [#versionsMenu:]
-				ifFalse: [#changeListMenu:]).
+				model: self.
+	listPane _ multiSelect
+				ifTrue: [PluggableListMorphOfMany
+						on: self
+						list: #list
+						primarySelection: #listIndex
+						changePrimarySelection: #toggleListIndex:
+						listSelection: #listSelectionAt:
+						changeListSelection: #listSelectionAt:put:
+						menu: (self showsVersions
+								ifTrue: [#versionsMenu:]
+								ifFalse: [#changeListMenu:])]
+				ifFalse: [PluggableListMorph
+						on: self
+						list: #list
+						selected: #listIndex
+						changeSelected: #toggleListIndex:
+						menu: (self showsVersions
+								ifTrue: [#versionsMenu:]
+								ifFalse: [#changeListMenu:])].
 	listPane keystrokeActionSelector: #changeListKey:from:.
-	window addMorph: listPane
+	window
+		addMorph: listPane
 		frame: (0 @ 0 extent: 1 @ listHeight).
-
-	self 
-		addLowerPanesTo: window 
-		at: (0@listHeight corner: 1@1) 
+	self
+		addLowerPanesTo: window
+		at: (0 @ listHeight corner: 1 @ 1)
 		with: nil.
-
 	^ window openInWorld
