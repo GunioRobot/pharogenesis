0995228743aa1a9194a626d11e97441b5fc66f09
tpr <unknown>
2001-10-04 21:34:00 +0000

    no message

--- PluggableListViewOfMany/instance/displaying/highlightFrom-3A-to-3A-.cs
+++ PluggableListViewOfMany/instance/displaying/highlightFrom-3A-to-3A-.cs
@@ -2,5 +2,5 @@ highlightFrom: start to: stop
 	(start == nil or: [stop == nil]) ifTrue: [^ self displayView].
 	start to: stop do:
 		[:i | selection _ i.
-		(model listSelectionAt: selection) ifTrue: [self displaySelectionBox]].
+		(self listSelectionAt: selection) ifTrue: [self displaySelectionBox]].
 	selection _ 0
