0d2cadbfbd24e253d880e39920a93f4d457270c4
tk <unknown>
2001-10-04 16:17:00 +0000

    no message

--- Point/class/instance creation/r-3A-degrees-3A-.cs
+++ Point/class/instance creation/r-3A-degrees-3A-.cs
@@ -1,4 +1,4 @@
-r: rho degrees: theta
+r: rho degrees: degrees
 	"Answer an instance of me with polar coordinates rho and theta."
 
-	^self new setR: rho degrees: theta
+	^self new setR: rho degrees: degrees
