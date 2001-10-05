5138a455a9e18cd9d6b9b7b92cb1bcb54c0bf920
rhi <unknown>
2001-10-05 20:49:00 +0000

    no message

--- Morph/instance/halos and balloon help/wantsBalloon.cs
+++ Morph/instance/halos and balloon help/wantsBalloon.cs
@@ -1,4 +1,4 @@
 wantsBalloon
-	"Answer true if receiver wants to show a balloon help text is a few moments." 
+	"Answer true if receiver wants to show a balloon help text is a few moments."
 
-	^ self balloonText notNil
+	^ (self balloonText notNil) and: [Preferences balloonHelpEnabled]
