b4a7575ed152cdf715fe872310c602605588ec92
LC <unknown>
2001-10-08 04:46:00 +0000

    no message

--- PseudoClass/instance/class/definition.cs
+++ PseudoClass/instance/class/definition.cs
@@ -1,2 +1,8 @@
 definition
-	^definition ifNil:['There is no class definition for this class'].
+	| link linkText defText |
+	^definition ifNil:
+		[defText _ Text fromString: 'There is no class definition for '.
+		link _ TextLink new.
+		linkText _ link analyze: self name with: 'Definition'.
+		linkText _ Text string: (linkText ifNil: ['']) attribute: link.
+		defText append: linkText; append: ' in this file'].
