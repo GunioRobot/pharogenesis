64f53c6f42b67e6f03de0781911b8422278cce6f
sw <unknown>
2001-10-08 14:34:00 +0000

    no message

---
+++ Lexicon/instance/menu commands/showMainCategory.cs
@@ -0,0 +1,4 @@
+showMainCategory
+	"Continue to show the current selector, but show it within the context of its primary category.  Preserved for backward compatibility with pre-existing buttons."
+
+	^ self showHomeCategory
