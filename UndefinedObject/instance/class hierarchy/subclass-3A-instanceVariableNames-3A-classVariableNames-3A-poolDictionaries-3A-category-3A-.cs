dbcf3719ad54f7229fddadb6d5363cf8c4cccf7d
ls <unknown>
2001-10-09 00:11:00 +0000

    no message

--- UndefinedObject/instance/class hierarchy/subclass-3A-instanceVariableNames-3A-classVariableNames-3A-poolDictionaries-3A-category-3A-.cs
+++ UndefinedObject/instance/class hierarchy/subclass-3A-instanceVariableNames-3A-classVariableNames-3A-poolDictionaries-3A-category-3A-.cs
@@ -1,10 +1,11 @@
-subclass: nameOfClass  "Define root (superclass = nil) of a class hierarchy"
+subclass: nameOfClass  
 	instanceVariableNames: instVarNames
 	classVariableNames: classVarNames
 	poolDictionaries: poolDictnames
 	category: category
-	^(ClassBuilder new)
-		superclass: self
+	"Calling this method is now considered an accident.  If you really want to create a class with a nil superclass, then create the class and then set the superclass using #superclass:"
+	Transcript show: ('Attempt to create ', nameOfClass, ' as a subclass of nil.  Possibly a class is being loaded before its superclass.'); cr.
+	^ProtoObject
 		subclass: nameOfClass
 		instanceVariableNames: instVarNames
 		classVariableNames: classVarNames
