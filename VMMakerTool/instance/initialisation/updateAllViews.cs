d39dffb6f349018fe0abe6e16e2f5569c35903cd
tpr <unknown>
2001-10-12 18:06:00 +0000

    no message

---
+++ VMMakerTool/instance/initialisation/updateAllViews.cs
@@ -0,0 +1,7 @@
+updateAllViews
+	self changed: #platformsPathText;
+				 changed: #platformNameText;
+				 changed: #sourcePathText;
+				 changed: #availableModules;
+				 changed: #internalModules;
+				 changed: #externalModules
