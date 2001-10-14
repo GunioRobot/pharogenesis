326fdbfbbefcb4e109aee28dfdbc6edf3e88abdb
hg <unknown>
2001-10-14 16:20:00 +0000

    no message

--- Inspector/instance/accessing/object-3A-.cs
+++ Inspector/instance/accessing/object-3A-.cs
@@ -1,10 +1,15 @@
 object: anObject 
 	"Set anObject to be the object being inspected by the receiver."
 
+	| oldIndex |
 	anObject == object
 		ifTrue: [self update]
 		ifFalse:
-			[self inspect: anObject.
+			[oldIndex _ selectionIndex <= 2 ifTrue: [selectionIndex] ifFalse: [0].
+			self inspect: anObject.
+			oldIndex _ oldIndex min: self fieldList size.
 			self changed: #inspectObject.
+			oldIndex > 0
+				ifTrue: [self toggleIndex: oldIndex].
 			self changed: #fieldList.
 			self changed: #contents]
