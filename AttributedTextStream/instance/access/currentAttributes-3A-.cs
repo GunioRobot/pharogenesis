a60bf12b85637721334846a456a58d2bcb3dec43
ar <unknown>
2001-10-16 22:57:00 +0000

    no message

--- AttributedTextStream/instance/access/currentAttributes-3A-.cs
+++ AttributedTextStream/instance/access/currentAttributes-3A-.cs
@@ -1,4 +1,8 @@
 currentAttributes: newAttributes
 	"set the current attributes"
-	attributesChanged _ currentAttributes ~= newAttributes.
+	(currentRun > 0 and:[currentAttributes ~= newAttributes]) ifTrue:[
+		attributeRuns nextPut: currentRun.
+		attributeValues nextPut: currentAttributes.
+		currentRun _ 0.
+	].
 	currentAttributes _ newAttributes.
