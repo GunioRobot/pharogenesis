2011397b8e28d5280330f917428a6478290eff02
ar <unknown>
2001-10-16 22:39:00 +0000

    no message

--- AttributedTextStream/instance/retrieving the text/contents.cs
+++ AttributedTextStream/instance/retrieving the text/contents.cs
@@ -1,5 +1,11 @@
 contents
 	| ans |
+	currentRun > 0 ifTrue:[
+		attributeValues nextPut: currentAttributes.
+		attributeRuns nextPut: currentRun.
+		currentRun _ 0].
 	ans _ Text new: characters size.
-	ans setString: characters contents  setRuns: attributeRuns.   "this is declared private, but it's exactly what I need, and it's declared as exactly what I want it to do...."
+	"this is declared private, but it's exactly what I need, and it's declared as exactly what I want it to do...."
+	ans setString: characters contents  setRuns: 
+		(RunArray runs: attributeRuns contents values: attributeValues contents).
 	^ans
