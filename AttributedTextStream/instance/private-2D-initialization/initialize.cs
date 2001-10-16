20ccb053db6ebb282d2663d1e78d23a0dcebfdb9
ar <unknown>
2001-10-16 22:40:00 +0000

    no message

--- AttributedTextStream/instance/private-2D-initialization/initialize.cs
+++ AttributedTextStream/instance/private-2D-initialization/initialize.cs
@@ -1,6 +1,6 @@
 initialize
 	characters _ WriteStream on: String new.
 	currentAttributes _ OrderedCollection new.
-	attributesChanged _ true.
-	attributeRuns _ RunArray new.
-	
+	currentRun _ 0.
+	attributeValues _ WriteStream on: (Array new: 50).
+	attributeRuns _ WriteStream on: (Array new: 50).	
