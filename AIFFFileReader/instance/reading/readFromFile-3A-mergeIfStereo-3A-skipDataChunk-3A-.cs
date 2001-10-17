f3972710920a4991b08527ee340813c5cbbcc1b7
jm <unknown>
2001-10-17 17:20:00 +0000

    no message

--- AIFFFileReader/instance/reading/readFromFile-3A-mergeIfStereo-3A-skipDataChunk-3A-.cs
+++ AIFFFileReader/instance/reading/readFromFile-3A-mergeIfStereo-3A-skipDataChunk-3A-.cs
@@ -1,7 +1,8 @@
 readFromFile: fileName mergeIfStereo: mergeFlag skipDataChunk: skipDataFlag
-	"Read the AIFF file of the given name. If mergeFlag is true and the file contains stereo data, then the left and right channels will be mixed together as the samples are read in. If skipDataFlag is true, then the data chunk to be skipped; this allows the other chunks of a file to be processed in order to extract format information quickly without reading the data."
+	"Read the AIFF file of the given name. See comment in readFromStream:mergeIfStereo:skipDataChunk:."
 	"AIFFFileReader new readFromFile: 'test.aiff' mergeIfStereo: false skipDataChunk: true"
 
-	| binaryStream |
-	binaryStream _ (FileStream readOnlyFileNamed: fileName) binary.
-	self readFrom: binaryStream mergeIfStereo: mergeFlag skipDataChunk: skipDataFlag
+	| f |
+	f _ (FileStream readOnlyFileNamed: fileName) binary.
+	self readFromStream: f mergeIfStereo: mergeFlag skipDataChunk: skipDataFlag.
+	f close.
