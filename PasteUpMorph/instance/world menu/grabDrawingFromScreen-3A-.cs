60e68bd5045964b53b0606cc60c3bd3828d795e9
di <unknown>
2001-10-18 03:33:00 +0000

    no message

--- PasteUpMorph/instance/world menu/grabDrawingFromScreen-3A-.cs
+++ PasteUpMorph/instance/world menu/grabDrawingFromScreen-3A-.cs
@@ -2,4 +2,5 @@ grabDrawingFromScreen: evt
 	"Allow the user to specify a rectangular area of the Display, capture the pixels from that area, and use them to create a new drawing morph. Attach the result to the hand."
 	| m |
 	m _ self drawingClass new form: Form fromUser.
+	evt hand position: Sensor cursorPoint.  "update hand pos after Sensor loop in fromUser"
 	evt hand attachMorph: m.
