366fedcac4ad9d3f83f851360960de21c5e42310
sw <unknown>
2001-10-18 15:09:00 +0000

    no message

--- Preferences/class/halos/haloSpecificationsForWorld.cs
+++ Preferences/class/halos/haloSpecificationsForWorld.cs
@@ -3,6 +3,6 @@ haloSpecificationsForWorld
 	"Answer a list of HaloSpecs that describe which halos are to be used on a world halo, what they should look like, and where they should be situated"
 	"Preferences resetHaloSpecifications"
 
-	desired _ #(addDebugHandle: addMenuHandle: addTileHandle: addViewHandle: addHelpHandle: addScriptHandle:).
+	desired _ #(addDebugHandle: addMenuHandle: addTileHandle: addViewHandle: addHelpHandle: addScriptHandle: addPaintBgdHandle:).
 	^ self haloSpecifications select:
 		[:spec | desired includes: spec addHandleSelector]
