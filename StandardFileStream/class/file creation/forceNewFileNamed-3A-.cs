e9dae4f7b66eaa10f55f85cb0746876f299648a3
tpr <unknown>
2001-10-21 11:11:00 +0000

    no message

--- StandardFileStream/class/file creation/forceNewFileNamed-3A-.cs
+++ StandardFileStream/class/file creation/forceNewFileNamed-3A-.cs
@@ -1,13 +1,23 @@
-forceNewFileNamed: fileName
- 	"Create a new file with the given name, and answer a stream opened for writing on that file. If the file already exists, delete it without asking before creating the new file."
-
-	| dir localName fullName |
+forceNewFileNamed: fileName 
+	"Create a new file with the given name, and answer a stream opened 
+	for writing on that file. If the file already exists, delete it without 
+	asking before creating the new file."
+	| dir localName fullName f |
 	fullName _ self fullName: fileName.
 	(self isAFileNamed: fullName)
-		ifFalse: [^ self new open: fullName forWrite: true].
-
+		ifFalse: [f _ self new open: fullName forWrite: true.
+			^ f isNil
+				ifTrue: ["Failed to open the file"
+					(FileDoesNotExistException fileName: fullName) signal]
+				ifFalse: [f]].
 	dir _ FileDirectory forFileName: fullName.
 	localName _ FileDirectory localNameFor: fullName.
-	dir deleteFileNamed: localName
-		ifAbsent: [self error: 'Could not delete the old version of that file'].
-	^ self new open: fullName forWrite: true.
+	dir
+		deleteFileNamed: localName
+		ifAbsent: [(CannotDeleteFileException new
+			messageText: 'Could not delete the old version of file ' , fullName) signal].
+	f _ self new open: fullName forWrite: true.
+	^ f isNil
+		ifTrue: ["Failed to open the file"
+			(FileDoesNotExistException fileName: fullName) signal]
+		ifFalse: [f]
