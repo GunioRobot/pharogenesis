1e241dc4a112ee81189db73d8fe23a5f5a9d092f
sw <unknown>
2001-10-23 05:42:00 +0000

    no message

---
+++ MethodInterface/instance/initialization/resultType-3A-.cs
@@ -0,0 +1,5 @@
+resultType: aType
+	"Set the receiver's resultSpecification to be a ResultType of the given type"
+
+	resultSpecification _ ResultSpecification new.
+	resultSpecification resultType: aType
