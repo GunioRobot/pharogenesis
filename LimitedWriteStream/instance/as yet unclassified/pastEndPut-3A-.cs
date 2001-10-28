846510b6679ca2793a2bba0f0e80be49c1940a44
di <unknown>
2001-10-28 12:49:00 +0000

    no message

--- LimitedWriteStream/instance/as yet unclassified/pastEndPut-3A-.cs
+++ LimitedWriteStream/instance/as yet unclassified/pastEndPut-3A-.cs
@@ -1,6 +1,3 @@
 pastEndPut: anObject
 	collection size >= limit ifTrue: [limitBlock value].  "Exceptional return"
-	collection _ collection ,
-		(collection class new: ((collection size max: 20) min: limit)).
-	writeLimit _ collection size.
-	collection at: (position _ position + 1) put: anObject
+	^ super pastEndPut: anObject
