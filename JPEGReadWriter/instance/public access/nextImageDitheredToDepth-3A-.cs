812954d70c114ead1d893417ee7ae86d1d8ee5c5
ar <unknown>
2001-10-28 16:25:00 +0000

    no message

--- JPEGReadWriter/instance/public access/nextImageDitheredToDepth-3A-.cs
+++ JPEGReadWriter/instance/public access/nextImageDitheredToDepth-3A-.cs
@@ -1,14 +1,19 @@
 nextImageDitheredToDepth: depth
 
-	| form xStep yStep x y |
+	| form xStep yStep x y bb |
 	ditherMask _ DitherMasks
 		at: depth
 		ifAbsent: [self error: 'can only dither to display depths'].
-	redResidual _ greenResidual _ blueResidual _ 0.
+	residuals _ WordArray new: 3.
 	sosSeen _ false.
 	self parseFirstMarker.
 	[sosSeen] whileFalse: [self parseNextMarker].
-	form _ Form extent: (width @ height) depth: 32.
+	form _ Form extent: (width @ height) depth: depth.
+	bb _ BitBlt current toForm: form.
+	bb sourceForm: mcuImageBuffer.
+	bb colorMap: (mcuImageBuffer colormapIfNeededFor: form).
+	bb sourceRect: mcuImageBuffer boundingBox.
+	bb combinationRule: Form over.
 	xStep _ mcuWidth * DCTSize.
 	yStep _ mcuHeight * DCTSize.
 	y _ 0.
@@ -20,14 +25,7 @@ nextImageDitheredToDepth: depth
 			self decodeMCU.
 			self idctMCU.
 			self colorConvertMCU.
-			mcuImageBuffer displayOn: form at: (x @ y).
+			bb destX: x; destY: y; copyBits.
 			x _ x + xStep].
 		y _ y + yStep].
-	"Fixup the alpha channel in the 32bit form"
-	(BitBlt current toForm: form)
-		destRect: form boundingBox;
-		fillColor: (Bitmap with: 16rFF000000);
-		combinationRule: 7; "bitOr:with:"
-		copyBits.
-		
 	^ form
