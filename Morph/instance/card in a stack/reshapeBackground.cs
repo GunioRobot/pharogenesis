e598db81edb6f1b5a936dea7a5e3f499284e8d44
tk <unknown>
2001-10-30 13:47:00 +0000

    no message

---
+++ Morph/instance/card in a stack/reshapeBackground.cs
@@ -0,0 +1,5 @@
+reshapeBackground
+	"Abandon any memory of variable-name preferences, and reassess the shape of the background"
+
+	self relaxGripOnVariableNames.
+	"self reassessBackgroundShape.	already done there"
