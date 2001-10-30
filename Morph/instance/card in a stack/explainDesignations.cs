b429f79d4c7c0247b3d813f475cb9b220bc344fe
tk <unknown>
2001-10-30 13:33:00 +0000

    no message

---
+++ Morph/instance/card in a stack/explainDesignations.cs
@@ -0,0 +1,6 @@
+explainDesignations
+	"Hand the user an object that contains explanations for the designation feedback used"
+
+	StackMorph designationsExplainer openInHand
+
+	"self currentWorld explainDesignations"
