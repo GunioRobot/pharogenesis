4ab7c82ebde5b99b460babe6c4fe7b12fc23e342
sw <unknown>
2001-10-31 07:18:00 +0000

    no message

--- DisplayScreen/class/display box access/checkForNewScreenSize.cs
+++ DisplayScreen/class/display box access/checkForNewScreenSize.cs
@@ -1,6 +1,11 @@
 checkForNewScreenSize
+	"Check whether the screen size has changed and if so take appropriate actions"
+
 	Display extent = DisplayScreen actualScreenSize ifTrue: [^ self].
 	DisplayScreen startUp.
 	Smalltalk isMorphic
-		ifTrue: [Display restoreMorphicDisplay]
-		ifFalse: [ScheduledControllers restore; searchForActiveController]
+		ifTrue:
+			[World restoreMorphicDisplay.
+			World repositionFlapsAfterScreenSizeChange]
+		ifFalse:
+			[ScheduledControllers restore; searchForActiveController]
