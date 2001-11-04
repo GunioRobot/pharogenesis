3daf8b31a942708f10dcd567a8c8f1f273c6cb1d
tk <unknown>
2001-11-04 21:47:00 +0000

    no message

---
+++ SyntaxMorph/instance/card & stack/setTarget-3A-.cs
@@ -0,0 +1,8 @@
+setTarget: aPlayer
+	"Find my UpdatingStringMorph and set its getSelector, putSelector, and target"
+
+	| updatingString |
+	(updatingString _ self readOut) ifNil: [^ self].
+	updatingString putSelector: (Utilities setterSelectorFor: self knownName).
+	updatingString getSelector: (Utilities getterSelectorFor: self knownName).
+	updatingString target: aPlayer. 
