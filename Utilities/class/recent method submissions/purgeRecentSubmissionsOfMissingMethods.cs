69fdd410ea2d64943ea840d6c867971934349bbd
sw <unknown>
2001-11-05 12:04:00 +0000

    no message

--- Utilities/class/recent method submissions/purgeRecentSubmissionsOfMissingMethods.cs
+++ Utilities/class/recent method submissions/purgeRecentSubmissionsOfMissingMethods.cs
@@ -1,10 +1,13 @@
 purgeRecentSubmissionsOfMissingMethods
 	"Utilities purgeRecentSubmissionsOfMissingMethods"
 
-	| result |
+	| keep |
+	self flag: #mref.	"fix for faster references to methods"
 	RecentSubmissions _ RecentSubmissions select:
-		[:aSubmission |
-			Utilities setClassAndSelectorFrom: aSubmission in: 
+		[:aSubmission | 
+			Utilities setClassAndSelectorFrom: aSubmission in:
 				[:aClass :aSelector |
-					result _ aClass notNil and: [(aClass compiledMethodAt: aSelector ifAbsent: [nil]) notNil]].
-			result]
+					keep _ (aClass == nil) not
+						and: [aClass isInMemory
+						and: [aSelector == #Comment or: [(aClass compiledMethodAt: aSelector ifAbsent: [nil]) notNil]]]].
+			keep]
