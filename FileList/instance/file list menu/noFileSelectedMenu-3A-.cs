17e58ba0d53f0a8b7a72bc9b50f5405d9cca7a4b
SD <unknown>
2001-11-08 20:34:00 +0000

    no message

--- FileList/instance/file list menu/noFileSelectedMenu-3A-.cs
+++ FileList/instance/file list menu/noFileSelectedMenu-3A-.cs
@@ -1,7 +1,7 @@
 noFileSelectedMenu: aMenu
-	| items |
-	items _ self itemsForNoFile.
+
 	^ aMenu
-		labels: items first
-		lines: items second
-		selections: items third
+		addServices: self itemsForNoFile 
+		for: self
+		extraLines: #()
+		
