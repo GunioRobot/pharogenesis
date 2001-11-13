70c947c8ea76f9b42d4eaec811e4542801ff5f0e
sw <unknown>
2001-11-13 14:10:00 +0000

    no message

--- Presenter/instance/stop-2D-step-2D-go buttons/stepButtonState-3A-.cs
+++ Presenter/instance/stop-2D-step-2D-go buttons/stepButtonState-3A-.cs
@@ -1,5 +1,5 @@
 stepButtonState: newState
-	stepButton ifNotNil:
-		[newState
-			ifTrue: [stepButton state: #on]
-			ifFalse: [stepButton state: #off]]
+	"Get all step buttons in my scope to show the correct state"
+
+	self allStepButtons do:
+		[:aButton | aButton state: newState]
