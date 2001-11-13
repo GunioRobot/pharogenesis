f273cbf88a06b06520d3187218330b84233b714c
sw <unknown>
2001-11-13 18:42:00 +0000

    no message

--- Presenter/instance/stop-2D-step-2D-go buttons/stopRunningScripts.cs
+++ Presenter/instance/stop-2D-step-2D-go buttons/stopRunningScripts.cs
@@ -1,6 +1,9 @@
 stopRunningScripts
-	self stopButtonState: true.
-	self stepButtonState: false.
-	self goButtonState: false.
+	"Put all ticking scripts within my scope into paused mode.  Get any scripting-control buttons to show the correct state"
+
+	self stopButtonState: #on.
+	self stepButtonState: #off.
+	self goButtonState: #off.
 	associatedMorph stopRunningAll.
-	associatedMorph borderColor: Preferences borderColorWhenStopped
+
+	"associatedMorph borderColor: Preferences borderColorWhenStopped"
