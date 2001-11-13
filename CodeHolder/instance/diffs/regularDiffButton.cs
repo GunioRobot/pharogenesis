e8d19109e56a559f87d56fcd3171e458996821c0
sw <unknown>
2001-11-13 07:37:00 +0000

    no message

---
+++ CodeHolder/instance/diffs/regularDiffButton.cs
@@ -0,0 +1,17 @@
+regularDiffButton
+	"Return a checkbox that lets the user decide whether regular diffs should be shown or not"
+
+	|  outerButton aButton |
+	outerButton _ AlignmentMorph newRow.
+	outerButton wrapCentering: #center; cellPositioning: #leftCenter.
+	outerButton color:  Color transparent.
+	outerButton hResizing: #shrinkWrap; vResizing: #shrinkWrap.
+	outerButton addMorph: (aButton _ UpdatingThreePhaseButtonMorph checkBox).
+	aButton
+		target: self;
+		actionSelector: #toggleRegularDiffing;
+		getSelector: #showingRegularDiffs.
+	outerButton addMorphBack: (StringMorph contents: 'diffs') lock.
+	outerButton setBalloonText: 'If checked, then code differences from the previous version, if any, will be shown.'.
+
+	^ outerButton
