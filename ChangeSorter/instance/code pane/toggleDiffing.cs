c2ff1a49a2460b8e8524688e56cdd1d924f9b475
sw <unknown>
2001-11-13 07:34:00 +0000

    no message

--- ChangeSorter/instance/code pane/toggleDiffing.cs
+++ ChangeSorter/instance/code pane/toggleDiffing.cs
@@ -2,7 +2,7 @@ toggleDiffing
 	"Toggle whether diffs should be shown in the code pane"
 
 	self okToChange ifTrue:
-		[self showDiffs: self showDiffs not.
+		[super toggleDiffing.
 		self changed: #contents.
 		self update]
 
