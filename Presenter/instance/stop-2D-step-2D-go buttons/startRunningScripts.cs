9f341711a48b35cbeada3ef0d88b771038ca3eff
sw <unknown>
2001-11-13 18:43:00 +0000

    no message

--- Presenter/instance/stop-2D-step-2D-go buttons/startRunningScripts.cs
+++ Presenter/instance/stop-2D-step-2D-go buttons/startRunningScripts.cs
@@ -1,9 +1,11 @@
 startRunningScripts
+	"Start running scripts; get stop-step-go buttons to show the right thing"
 
-	self stopButtonState: false.
-	self stepButtonState: false.
-	self goButtonState: true.
+	self stopButtonState: #off.
+	self stepButtonState: #off.
+	self goButtonState: #on.
 	associatedMorph startRunningAll.
-	associatedMorph borderColor: Preferences borderColorWhenRunning.
 
-	ThumbnailMorph recursionReset.  "needs to be done once in a while"
+	"associatedMorph borderColor: Preferences borderColorWhenRunning."
+
+	ThumbnailMorph recursionReset.  "needs to be done once in a while (<- tk note from 1997)"
