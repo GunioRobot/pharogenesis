907a8ffde1163138f9c09857d2678937ceff1338
sw <unknown>
2001-11-13 18:42:00 +0000

    no message

--- Presenter/instance/stop-2D-step-2D-go buttons/stopRunningScriptsFrom-3A-.cs
+++ Presenter/instance/stop-2D-step-2D-go buttons/stopRunningScriptsFrom-3A-.cs
@@ -1,7 +1,4 @@
-stopRunningScriptsFrom: aStopButton
-	(stopButton == nil or: [stopButton isInWorld not]) ifTrue: [stopButton _ aStopButton].
-	self stopButtonState: true.
-	self stepButtonState: false.
-	self goButtonState: false.
-	associatedMorph stopRunningAll.
-	associatedMorph borderColor: Preferences borderColorWhenStopped
+stopRunningScriptsFrom: ignored
+	"Stop running scripts; get all script-control buttons to reflect this"
+
+	self stopRunningScripts
