4b4df9bc807963abdd88e68563e5a0c801194b41
sw <unknown>
2001-11-13 09:34:00 +0000

    no message

---
+++ Vocabulary/instance/method list/phraseSymbolsToSuppress.cs
@@ -0,0 +1,4 @@
+phraseSymbolsToSuppress
+	"Answer a dictatorially-imposed list of phrase-symbols that are to be suppressed from viewers, even if they otherwise show up.  Note that EToyVocabulary reimplements"
+
+	^ #()
