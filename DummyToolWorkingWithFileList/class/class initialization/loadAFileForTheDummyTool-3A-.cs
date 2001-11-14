cbc2d3f80bb426bec7e6a390f69fdd428ae31160
SD <unknown>
2001-11-14 22:12:00 +0000

    no message

---
+++ DummyToolWorkingWithFileList/class/class initialization/loadAFileForTheDummyTool-3A-.cs
@@ -0,0 +1,3 @@
+loadAFileForTheDummyTool: aFileListOrAPath
+	
+	"attention. if the file list selects a file the argument will be a fullpath of the selected file else it will pass the filelist itself"
