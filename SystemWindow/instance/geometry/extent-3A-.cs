58438cfd4aa0f534c81addda579f247cebb536c0
di <unknown>
2001-11-14 22:00:00 +0000

    no message

--- SystemWindow/instance/geometry/extent-3A-.cs
+++ SystemWindow/instance/geometry/extent-3A-.cs
@@ -1,10 +1,19 @@
-extent: newExtent 
+extent: aPoint 
+	"Set the receiver's extent to value provided. Respect my minimumExtent."
+
+	| newExtent |
+	newExtent _ self isCollapsed
+		ifTrue: [aPoint]
+		ifFalse: [aPoint max: self minimumExtent].
+	newExtent = self extent ifTrue: [^ self].
+
 	isCollapsed
 		ifTrue: [super extent: newExtent x @ (self labelHeight + 2)]
 		ifFalse: [super extent: newExtent].
-	self setStripeColorsFrom: self paneColorToUse.
-	label fitContents; setWidth: (label width min: bounds width - self labelWidgetAllowance).
-	label layoutFrame leftOffset: label width negated // 2.
+	labelArea ifNotNil:
+		[self setStripeColorsFrom: self paneColorToUse.
+		label fitContents; setWidth: (label width min: bounds width - self labelWidgetAllowance).
+		label layoutFrame leftOffset: label width negated // 2].
 	isCollapsed
 		ifTrue: [collapsedFrame _ self bounds]
 		ifFalse: [fullFrame _ self bounds]
