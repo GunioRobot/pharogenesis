58ad71a9ffbf17f3329a8e8dd3efb3c53b378b15
sw <unknown>
2001-11-14 00:32:00 +0000

    no message

--- AllScriptsTool/instance/toggles/toggleWhetherShowingAllInstances.cs
+++ AllScriptsTool/instance/toggles/toggleWhetherShowingAllInstances.cs
@@ -2,4 +2,4 @@ toggleWhetherShowingAllInstances
 	"Toggle whether the receiver is showing all instances or only one exemplar per uniclass"
 
 	showingAllInstances _ showingAllInstances not.
-	presenter updateContentsFor: self
+	self presenter reinvigorateAllScriptsTool: self
