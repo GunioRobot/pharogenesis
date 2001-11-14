502f2ab8a5462f55b95ecaf06d5353a8fa6c36cc
di <unknown>
2001-11-14 00:01:00 +0000

    no message

--- ServerDirectory/instance/updates/putUpdateMulti-3A-fromDirectory-3A-.cs
+++ ServerDirectory/instance/updates/putUpdateMulti-3A-fromDirectory-3A-.cs
@@ -1,49 +1,65 @@
-putUpdateMulti: list fromDirectory: updateDirectory
-	"Put these files out as an Update on the servers of my group.  List is an array of local file names without number prefixes.  Each version of the system has its own set of update files.  'updates.list' holds the master list.  Each update is a fileIn whose name begins with a number.  See Utilities class absorbUpdatesFromServer."
+putUpdateMulti: list fromDirectory: updateDirectory 
+	"Put these files out as an Update on the servers of my group.  List is an array of local file names with or without number prefixes.  Each version of the system has its own set of update files.  'updates.list' holds the master list.  Each update is a fileIn whose name begins with a number.  See Utilities class absorbUpdatesFromServer."
 
-	| myServers updateStrm sequence myName response restOfText seq add newNames file |
-	(self checkNames: list) ifFalse: [^ nil].
-	myName _ group ifNil: [self moniker] ifNotNil: [group key].
+	| myServers updateStrm lastNum response newNames file numStr indexPrefix listContents version versIndex seq stripped |
+	(self checkNames: (list collect: "Check the names without their numbers"
+		[:each | each copyFrom: (each findFirst: [:c | c isDigit not]) to: each size]))
+		ifFalse: [^ nil].
 	response _ (PopUpMenu labels: 'Install update\Cancel update' withCRs)
 		startUpWithCaption: 'Do you really want to broadcast ', list size printString, ' updates',
-			'\to every Squeak user who updates from ' withCRs, myName, '?'.
+			'\to every Squeak user who updates from ' withCRs, self groupName, '?'.
 	response = 1 ifFalse: [^ nil].	"abort"
 
 	self openGroup.
-	(myServers _ self checkServers) size = 0 ifTrue: [self closeGroup.  ^ self].
-	updateStrm _ myServers first getFileNamed: 'updates.list'.
-	"get last number and add 1"
-	sequence _ Utilities lastUpdateNum: updateStrm.
-	add _ WriteStream on: (String new: 200).
-	newNames _ list collect: [:each | 
-		seq _ (sequence _ sequence+1) printString.
-		seq size = 1 ifTrue: [seq _ '00', seq].
-		seq size = 2 ifTrue: [seq _ '0', seq].
-		add nextPutAll: seq; nextPutAll: each; cr.
-		seq, each].
-	restOfText _ Utilities position: updateStrm 	"sets the postion!!"
-			atVersion: (Smalltalk at: #EToySystem) version.
-	restOfText size > 0 ifTrue: [
-		response _ (PopUpMenu labels: 'Make update for my older version\Cancel update' withCRs)
-			startUpWithCaption: 'This system, ', (Smalltalk at: #EToySystem) version,
-				' is not the latest version'.
-		response = 1 ifFalse: [self closeGroup.  ^ nil].	"abort"
-		].
-	"append name to updates"
-	(updateStrm skip: -1; next) == Character cr ifFalse: [
-		updateStrm nextPut: Character cr].
-	updateStrm nextPutAll: add contents; nextPutAll: restOfText.
+	indexPrefix _ (self groupName includes: $*) 
+		ifTrue: [(self groupName findTokens: ' ') first]	"special for internal updates"
+		ifFalse: ['']. 	"normal"
+	myServers _ self checkServersWithPrefix: indexPrefix
+					andParseListInto: [:x | listContents _ x].
+	myServers size = 0 ifTrue: [self closeGroup.  ^ self].
 
-	myServers do: [:aServer |
+	version _ SystemVersion current version.
+	versIndex _ (listContents collect: [:pair | pair first]) indexOf: version.
+	versIndex = 0 ifTrue:
+		[self inform: 'There is no section in updates.list for your version'.
+		self closeGroup.  ^ nil].	"abort"
+	lastNum _ (listContents at: versIndex) last last initialIntegerOrNil.
+	versIndex < listContents size ifTrue:
+		[response _ (PopUpMenu labels: 'Make update for an older version\Cancel update' withCRs)
+			startUpWithCaption: 'This system, ', SystemVersion current version,
+				' is not the latest version'.
+		response = 1 ifFalse: [self closeGroup.  ^ nil].
+		numStr _ FillInTheBlank 
+			request: 'Please confirm or change the starting update number' 
+			initialAnswer: (lastNum+1) printString.
+		lastNum _ numStr asNumber - 1].	"abort"
+	"Save old copy of updates.list on local disk"
+	FileDirectory default deleteFileNamed: indexPrefix , 'updates.list.bk'.
+	Utilities writeList: listContents toStream: (FileStream fileNamed: indexPrefix , 'updates.list.bk').
+
+	"Append names to updates with new sequence numbers"
+	newNames _ list with: (lastNum+1 to: lastNum+list size) collect:
+		[:each :num | seq _ num printString padded: #left to: 4 with: $0.
+		"strip off any old seq number"
+		stripped _ each copyFrom: (each  findFirst: [:c | c isDigit not]) to: each size.
+		seq , stripped].
+	listContents at: versIndex put:
+		{version. (listContents at: versIndex) second , newNames}.
+
+	"Write a new copy on all servers..."
+	updateStrm _ ReadStream on:
+		(String streamContents: [:s | Utilities writeList: listContents toStream: s]).
+	myServers do:
+		[:aServer |
 		list doWithIndex: [:local :ind |
 			file _ updateDirectory oldFileNamed: local.
 			aServer putFile: file named: (newNames at: ind) retry: true.
 			file close].
 		updateStrm reset.
-		aServer putFile: updateStrm named: 'updates.list' retry: true.
-		Transcript cr; show: 'Updates succeeded on server ', aServer moniker].
+		aServer putFile: updateStrm named: indexPrefix , 'updates.list' retry: true.
+		Transcript show: 'Update succeeded on server ', aServer moniker; cr].
 	self closeGroup.
-	
+
 	Transcript cr; show: 'Be sure to test your new update!'; cr.
 	"rename the file locally"
 	list with: newNames do:
