1938c42d64b079ac3c7c048874c76a50c9ba3dd7
sw <unknown>
2001-11-14 00:32:00 +0000

    no message

--- ScriptInstantiation/instance/misc/assignStatusToAllSiblingsIn-3A-.cs
+++ ScriptInstantiation/instance/misc/assignStatusToAllSiblingsIn-3A-.cs
@@ -2,4 +2,5 @@ assignStatusToAllSiblingsIn: aStatusViewer
 	"Let all sibling instances of my player have the same status that I do"
 
 	self assignStatusToAllSiblings.
-	self currentWorld presenter updateContentsFor: aStatusViewer 
+	self updateAllStatusMorphs.
+	aStatusViewer presenter reinvigorateAllScriptsTool: aStatusViewer 
