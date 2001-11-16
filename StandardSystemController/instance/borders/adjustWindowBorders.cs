6ead5a98a69d73e2962b980ffe736c255ba82448
di <unknown>
2001-11-16 22:22:00 +0000

    no message

--- StandardSystemController/instance/borders/adjustWindowBorders.cs
+++ StandardSystemController/instance/borders/adjustWindowBorders.cs
@@ -1,21 +1,28 @@
 adjustWindowBorders 
-	| side |
+	| side noClickYet |
+	noClickYet _ true.
 	VBorderCursor showWhile:
 		[ [side _ view displayBox sideNearestTo: sensor cursorPoint.
-		  self cursorOnBorder and: [(side = #left) | (side = #right)]]
+		  self cursorOnBorder
+			and: [(side = #left) | (side = #right)
+			and: [noClickYet or: [sensor redButtonPressed]]]]
 			whileTrue:
-			[(sensor redButtonPressed and: [self cursorOnBorder]) ifTrue:
-				[side = #left ifTrue:
+			[sensor redButtonPressed ifTrue:
+				[noClickYet _ false.
+				side = #left ifTrue:
 					[view newFrame: [:f | f withLeft: sensor cursorPoint x]].
 				side = #right ifTrue:
 					[view newFrame: [:f | f withRight: sensor cursorPoint x]]].
-			self interActivityPause]. ].
+			self interActivityPause]].
 	HBorderCursor showWhile:
 		[ [side _ view displayBox sideNearestTo: sensor cursorPoint.
-		  self cursorOnBorder and: [(side = #top) | (side = #bottom)]]
+		  self cursorOnBorder
+			and: [(side = #top) | (side = #bottom)
+			and: [noClickYet or: [sensor redButtonPressed]]]]
 			whileTrue:
-			[(sensor redButtonPressed and: [self cursorOnBorder]) ifTrue:
-				[side = #top ifTrue:
+			[sensor redButtonPressed ifTrue:
+				[noClickYet _ false.
+				side = #top ifTrue:
 					[view newFrame: [:f | f withTop: sensor cursorPoint y]].
 				side = #bottom ifTrue:
 					[view newFrame: [:f | f withBottom: sensor cursorPoint y]]].
