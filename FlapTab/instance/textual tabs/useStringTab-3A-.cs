6c641ced0fb4155ceba4401289a0c04f52dffbde
di <unknown>
2001-11-17 20:22:00 +0000

    no message

--- FlapTab/instance/textual tabs/useStringTab-3A-.cs
+++ FlapTab/instance/textual tabs/useStringTab-3A-.cs
@@ -1,6 +1,7 @@
 useStringTab: aString
 	| aLabel |
-	aLabel _ StringMorph  new contents: aString asString.
+	labelString _ aString asString.
+	aLabel _ StringMorph  new contents: labelString.
 	self addMorph: aLabel.
 	aLabel position: self position.
 	aLabel highlightColor: self highlightColor; regularColor: self regularColor.
