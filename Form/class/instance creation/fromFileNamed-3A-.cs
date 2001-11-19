bb8d116f8a164f6453865c1d88fb98efcb68dcaf
mir <unknown>
2001-11-19 14:13:00 +0000

    no message

--- Form/class/instance creation/fromFileNamed-3A-.cs
+++ Form/class/instance creation/fromFileNamed-3A-.cs
@@ -4,5 +4,9 @@ fromFileNamed: fileName
 	| file form |
 	file _ (FileStream readOnlyFileNamed: fileName) binary.
 	form _ self fromBinaryStream: file.
+	Smalltalk isMorphic ifTrue:[
+		Project current resourceManager
+			addResource: form
+			url: (FileDirectory urlForFileNamed: file name) asString].
 	file close.
 	^ form
