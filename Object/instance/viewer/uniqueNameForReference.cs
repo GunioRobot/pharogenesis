0c1bedd1a3be924746b7c10ccaccf8a1acd0202d
sw <unknown>
2001-11-21 15:16:00 +0000

    no message

--- Object/instance/viewer/uniqueNameForReference.cs
+++ Object/instance/viewer/uniqueNameForReference.cs
@@ -2,9 +2,14 @@ uniqueNameForReference
 	"Answer a nice name by which the receiver can be referred to by other objects.  At present this uses a global References dictionary to hold the database of references, but in due course this will need to acquire some locality"
 
 	| aName nameSym stem knownClassVars |
-	(References includesIdentity: self) ifTrue: [^ References keyAtValue: self].
-
-	stem _ (self knownName ifNil: [self defaultNameStemForInstances asString]) capitalized.
+	(aName _ self uniqueNameForReferenceOrNil) ifNotNil: [^ aName].
+	(stem _ self knownName) ifNil:
+		[stem _ self defaultNameStemForInstances asString].
+	stem _ stem select: [:ch | ch isLetter or: [ch isDigit]].
+	stem size == 0 ifTrue: [stem _ 'A'].
+	stem first isLetter ifFalse:
+		[stem _ 'A', stem].
+	stem _ stem capitalized.
 	knownClassVars _ ScriptingSystem allKnownClassVariableNames.
 	aName _ Utilities keyLike:  stem satisfying:
 		[:jinaLake |
