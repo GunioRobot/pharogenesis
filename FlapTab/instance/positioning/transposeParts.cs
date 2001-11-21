e651cecf7819ae747088fd6f0e9e315fae984935
di <unknown>
2001-11-21 16:02:00 +0000

    no message

--- FlapTab/instance/positioning/transposeParts.cs
+++ FlapTab/instance/positioning/transposeParts.cs
@@ -1,4 +1,27 @@
 transposeParts
-	"The receiver's orientation has just been changed from vertical to horizontal or vice-versa.  One could imagine trying to be smart about transposition, though the variety of possibilities is daunting."
-	self flag: #deferred.
-	"edgeToAdhereTo == #vertical ifTrue: ..."
+	"The receiver's orientation has just been changed from vertical to horizontal or vice-versa."
+	"First expand the flap to screen size, letting the submorphs lay out to fit,
+	and then shrink the minor dimension back to the last row."
+
+	self isCurrentlyTextual ifTrue:  "First recreate the tab with proper orientation"
+		[self assumeString: self existingWording font: Preferences standardFlapFont
+			orientation: self orientation color: self color].
+	self orientation == #vertical
+		ifTrue:	"changed from horizontal"
+			[referent listDirection: #topToBottom; wrapDirection: #leftToRight.
+			referent hasSubmorphs ifTrue:
+				[referent extent: self currentWorld extent.
+				referent fullBounds.  "Needed to trigger layout"
+				referent width: (referent submorphs collect: [:m | m right]) max
+									- referent left + self width]]
+		ifFalse:
+			[referent listDirection: #leftToRight; wrapDirection: #topToBottom.
+			referent hasSubmorphs ifTrue:
+				[referent extent: self currentWorld extent.
+				referent fullBounds.  "Needed to trigger layout"
+				referent height: (referent submorphs collect: [:m | m bottom]) max
+									- referent top + self height]].
+	referent hasSubmorphs ifFalse: [referent extent: 100@100].
+
+	self spanWorld.
+	flapShowing ifTrue: [self showFlap]
