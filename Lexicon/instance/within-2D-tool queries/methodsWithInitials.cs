b056ca4cc87d173ef4edfb4fa80286bae4c7f668
sw <unknown>
2001-11-21 10:48:00 +0000

    no message

---
+++ Lexicon/instance/within-2D-tool queries/methodsWithInitials.cs
@@ -0,0 +1,4 @@
+methodsWithInitials
+	"Answer the list of method selectors within the scope of this tool whose time stamps begin with the initials designated by my currentQueryParameter"
+
+	^ self methodsWithInitials: currentQueryParameter
