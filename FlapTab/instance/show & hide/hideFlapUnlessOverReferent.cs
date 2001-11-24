c9d06f69f7e70d0bc41a808d5ad0a724227d96e4
sw <unknown>
2001-11-24 21:50:00 +0000

    no message

--- FlapTab/instance/show & hide/hideFlapUnlessOverReferent.cs
+++ FlapTab/instance/show & hide/hideFlapUnlessOverReferent.cs
@@ -1,14 +1,12 @@
 hideFlapUnlessOverReferent
-	| aWorld where |
+	"Hide the flap unless the mouse is over my referent."
 
-	
+	| aWorld where |
 	(referent isInWorld and: 
-		[
-			where _ self outermostWorldMorph activeHand lastEvent cursorPoint.
-			referent bounds containsPoint: (referent globalPointToLocal: where)
-		]
-	) ifTrue: [^self].
-	aWorld _ self world.
+		[where _ self outermostWorldMorph activeHand lastEvent cursorPoint.
+			referent bounds containsPoint: (referent globalPointToLocal: where)])
+				ifTrue: [^ self].
+	(aWorld _ self world) ifNil: [^ self].  "In case flap tabs just got hidden"
 	self referent delete.
 	aWorld removeAccommodationForFlap: self.
 	flapShowing _ false.
