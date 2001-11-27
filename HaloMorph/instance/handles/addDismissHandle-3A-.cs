4ae37e3db6a249a6aaddd322503350891a95f6b2
sw <unknown>
2001-11-27 11:18:00 +0000

    no message

--- HaloMorph/instance/handles/addDismissHandle-3A-.cs
+++ HaloMorph/instance/handles/addDismissHandle-3A-.cs
@@ -1,7 +1,10 @@
 addDismissHandle: handleSpec
+	"Add the dismiss handle according to the spec, unless selectiveHalos is on and my target resists dismissal"
+
 	| dismissHandle |
-	dismissHandle _ self addHandle: handleSpec
-		on: #mouseDown send: #mouseDownInDimissHandle:with: to: self.
-	dismissHandle on: #mouseUp send: #maybeDismiss:with: to: self.
-	dismissHandle on: #mouseDown send: #setDismissColor:with: to: self.
-	dismissHandle on: #mouseMove send: #setDismissColor:with: to: self.
+	(target okayToAddDismissHandle or: [Preferences selectiveHalos not]) ifTrue:
+		[dismissHandle _ self addHandle: handleSpec
+			on: #mouseDown send: #mouseDownInDimissHandle:with: to: self.
+		dismissHandle on: #mouseUp send: #maybeDismiss:with: to: self.
+		dismissHandle on: #mouseDown send: #setDismissColor:with: to: self.
+		dismissHandle on: #mouseMove send: #setDismissColor:with: to: self]
