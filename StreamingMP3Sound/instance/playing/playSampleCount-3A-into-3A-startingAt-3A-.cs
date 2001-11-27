10b55a1d07b45a53105a649d325b69d6a4c68781
jm <unknown>
2001-11-27 10:09:00 +0000

    no message

---
+++ StreamingMP3Sound/instance/playing/playSampleCount-3A-into-3A-startingAt-3A-.cs
@@ -0,0 +1,13 @@
+playSampleCount: n into: aSoundBuffer startingAt: startIndex
+	"Mix the next n samples of this sound into the given buffer starting at the given index"
+
+	| current |
+	self repeat ifTrue: [  "loop if necessary"
+		current _ mpegFile audioGetSample: mpegStreamIndex.
+		(totalSamples - current) < n ifTrue: [
+			mpegFile audioSetSample: 0 stream: mpegStreamIndex]].
+
+	mutex critical: [
+		lastBufferMSecs _ Time millisecondClockValue.
+		self loadBuffersForSampleCount: (n * streamSamplingRate) // SoundPlayer samplingRate.
+		mixer playSampleCount: n into: aSoundBuffer startingAt: startIndex].
