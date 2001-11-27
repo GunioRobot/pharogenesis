9c25470c2363570b91fe8f8bfd025e41c7e16231
sw <unknown>
2001-11-27 15:25:00 +0000

    no message

---
+++ SketchMorph/instance/menus/addToggleItemsToHaloMenu-3A-.cs
@@ -0,0 +1,5 @@
+addToggleItemsToHaloMenu: aCustomMenu 
+	"Add  toggle-items to the halo menu"
+
+	super addToggleItemsToHaloMenu: aCustomMenu.
+	aCustomMenu addUpdating: #useInterpolationString target: self action: #toggleInterpolation.
