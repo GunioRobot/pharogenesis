7525b1a85b7944ae8ddb76c5794d1a0342833e39
ar <unknown>
2001-11-29 20:01:00 +0000

    no message

--- HaloMorph/instance/private/doMenu-3A-with-3A-.cs
+++ HaloMorph/instance/private/doMenu-3A-with-3A-.cs
@@ -2,13 +2,8 @@ doMenu: evt with: menuHandle
 	"Ask hand to invoke the halo menu for my inner target."
 
 	| menu |
-	evt hand obtainHalo: self.
-	self removeAllHandlesBut: nil.  "remove all handles"
+	self obtainHaloForEvent: evt andRemoveAllHandlesBut: nil.
 	self world displayWorld.
 	menu _ innerTarget buildHandleMenu: evt hand.
-	innerTarget mightEntertainDirectionHandles ifTrue:
-		[self showingDirectionHandles
-			ifTrue: [menu add: 'hide direction handles' target: self selector: #showDirectionHandles: argument: false]
-			ifFalse: [menu add: 'show direction handles' target: self selector: #showDirectionHandles: argument: true]].
 	innerTarget addTitleForHaloMenu: menu.
 	menu popUpEvent: evt in: self world.
