ca2fb6f033814039854ff06e12bef53c2e266964
di <unknown>
2001-12-01 22:54:00 +0000

    no message

--- SystemWindow/instance/events/handlesMouseDown-3A-.cs
+++ SystemWindow/instance/events/handlesMouseDown-3A-.cs
@@ -1,9 +1,7 @@
-handlesMouseDown: evt
+handlesMouseDown: evt 
 	"If I am not the topWindow, then I will only respond to dragging by the title bar.
 	Any other click will only bring me to the top"
 
-	self flag: #bob.		"#handlesMouseDown: is in global coords, #mouseDown: is in local"
-	(self fastFramingOn 
-		and: [self labelRect containsPoint: (self globalPointToLocal: evt cursorPoint)])
+	(self labelRect containsPoint: evt cursorPoint)
 		ifTrue: [^ true].
 	^ self activeOnlyOnTop and: [self ~~ TopWindow]
