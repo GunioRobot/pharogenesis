5a4f932fdbd919ec4f0c0117829b11aaf119a4e3
RAA <unknown>
2001-12-05 11:20:00 +0000

    no message

--- TextMorph/instance/private/releaseParagraph.cs
+++ TextMorph/instance/private/releaseParagraph.cs
@@ -1,7 +1,7 @@
 releaseParagraph
-	"Paragraph instantiation is lazy -- it will be created only when needed"
-	self releaseEditor.
-	paragraph ifNotNil:
-		[paragraph _ nil].
-	container ifNotNil:
-		[container releaseCachedState]
+
+	"a slight kludge so subclasses can have a bit more control over whether the paragraph really 
+	gets released. important for GeeMail since the selection needs to be accessible even if the 
+	hand is outside me"
+
+	self releaseParagraphReally.
