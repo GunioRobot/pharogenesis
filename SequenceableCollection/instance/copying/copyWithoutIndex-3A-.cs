75b246f0f4a5f25aaf79250163a11cfa1492f808
rhi <unknown>
2001-12-06 14:04:00 +0000

    no message

--- SequenceableCollection/instance/copying/copyWithoutIndex-3A-.cs
+++ SequenceableCollection/instance/copying/copyWithoutIndex-3A-.cs
@@ -1,7 +1,8 @@
 copyWithoutIndex: index
+	"Return a copy containing all elements except the index-th."
+
 	| copy |
-	"return a copy containing all elements except the index-th"
-	copy := self species new: self size - 1.
+	copy := self species ofSize: self size - 1.
 	copy replaceFrom: 1 to: index-1 with: self startingAt: 1.
 	copy replaceFrom: index to: copy size with: self startingAt: index+1.
-	^copy
+	^ copy
