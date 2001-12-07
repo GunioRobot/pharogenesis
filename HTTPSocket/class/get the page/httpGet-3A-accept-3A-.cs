a66fedad60b480a72c5778ddbd74608bc9041054
tk <unknown>
2001-12-07 17:37:00 +0000

    no message

--- HTTPSocket/class/get the page/httpGet-3A-accept-3A-.cs
+++ HTTPSocket/class/get the page/httpGet-3A-accept-3A-.cs
@@ -1,5 +1,5 @@
 httpGet: url accept: mimeType
-	"Return the exact contents of a web object. Asks for the given MIME type. If mimeType is nil, use 'text/html'. The parsed header is saved. Use a proxy server if one has been registered.  tk 7/23/97 17:12"
-	"Note: To fetch raw data, you can use the MIMI type 'application/octet-stream'."
+	"Return the exact contents of a web object. Asks for the given MIME type. If mimeType is nil, use 'text/html'. The parsed header is saved. Use a proxy server if one has been registered.
+	Note: To fetch raw data, you can use the MIME type 'application/octet-stream'.  To accept anything, use '*/*'."
 
 	^self httpGet: url  args: nil accept: mimeType
