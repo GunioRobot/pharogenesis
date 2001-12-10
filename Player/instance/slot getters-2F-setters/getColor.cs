1c399f3427ef47a75ce3fb5f22ab257904c587a5
sw <unknown>
2001-12-10 00:34:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/getColor.cs
+++ Player/instance/slot getters-2F-setters/getColor.cs
@@ -1,4 +1,9 @@
 getColor
-	"Answer the color of my costume"
+	"Answer the color of my costume.  If it uses a gradient fill, answer the first color."
 
-	^ self costume renderedMorph color
+	| aFillStyle aMorph |
+	^ (aFillStyle _ (aMorph _ self costume renderedMorph) fillStyle) isGradientFill
+		ifTrue:
+			[aFillStyle colorRamp first value]
+		ifFalse:
+			[aMorph color]
