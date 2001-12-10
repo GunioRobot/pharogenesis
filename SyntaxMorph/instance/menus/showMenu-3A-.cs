06f71f756624fa55d551a4eaef6711edfe5fdb32
tk <unknown>
2001-12-10 17:48:00 +0000

    no message

--- SyntaxMorph/instance/menus/showMenu-3A-.cs
+++ SyntaxMorph/instance/menus/showMenu-3A-.cs
@@ -1,12 +1,21 @@
 showMenu: evt
 	| menu |
 	menu _ MenuMorph new.
-	menu add: 'show code' target: self selector: #showCode.
-	menu add: 'try out' target: self selector: #try.
 	self rootTile isMethodNode ifTrue:
-		[menu addLine.
-		menu add: 'accept method' target: self selector: #accept].
-	menu popUpAt: evt hand position forHand: evt hand in: World.
+		[menu add: 'accept method' target: self selector: #accept.
+		menu addLine.
+
+		menu add: 'new temp variable' target: self selector: #attachTileForCode:nodeType: 
+					argumentList: {'| temp | temp'. TempVariableNode}.
+		menu addLine.
+
+		self parsedInClass allInstVarNames do: [:nn |
+			menu add: nn,' tile' target: self selector: #instVarTile: argument: nn].
+		menu addLine.
+
+		menu add: 'show code' target: self selector: #showCode.
+		menu add: 'try out' target: self selector: #try.
+		menu popUpAt: evt hand position forHand: evt hand in: World].
 
 
 
