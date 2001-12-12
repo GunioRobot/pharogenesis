68d597d4d7b92b605db899898aad1806aa8c970b
ar <unknown>
2001-12-12 02:44:00 +0000

    no message

--- StringMorph/instance/accessing/fitContents.cs
+++ StringMorph/instance/accessing/fitContents.cs
@@ -1,6 +1,7 @@
 fitContents
 
-	| scanner |
-	scanner _ DisplayScanner quickPrintOn: Display box: Display boundingBox font: self fontToUse.
-	self extent: (((scanner stringWidth: contents) max: self minimumWidth)  @ scanner lineHeight).
-	self changed
+	| newBounds boundsChanged |
+	newBounds _ self measureContents.
+	boundsChanged _ bounds extent ~= newBounds.
+	self extent: newBounds.		"default short-circuits if bounds not changed"
+	boundsChanged ifFalse: [self changed]
