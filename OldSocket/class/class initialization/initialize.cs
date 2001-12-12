5b5c902c8ff36d288b9c04c00f80979265f899e7
ar <unknown>
2001-12-12 19:12:00 +0000

    no message

---
+++ OldSocket/class/class initialization/initialize.cs
@@ -0,0 +1,16 @@
+initialize
+	"Socket initialize"
+
+	"Socket Types"
+	TCPSocketType _ 0.
+	UDPSocketType _ 1.
+
+	"Socket Status Values"
+	InvalidSocket _ -1.
+	Unconnected _ 0.
+	WaitingForConnection _ 1.
+	Connected _ 2.
+	OtherEndClosed _ 3.
+	ThisEndClosed _ 4.
+
+	RegistryThreshold _ 100. "# of sockets"
