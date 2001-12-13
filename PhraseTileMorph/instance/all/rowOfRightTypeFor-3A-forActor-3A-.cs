3c963539b213b775c44f46b54e50705f69009eee
sw <unknown>
2001-12-13 17:41:00 +0000

    no message

--- PhraseTileMorph/instance/all/rowOfRightTypeFor-3A-forActor-3A-.cs
+++ PhraseTileMorph/instance/all/rowOfRightTypeFor-3A-forActor-3A-.cs
@@ -1,8 +1,11 @@
 rowOfRightTypeFor: aLayoutMorph forActor: aPlayer
+	"Answer a phrase of the right type for the putative container"
+
 	| aTemporaryViewer aPhrase |
 	aLayoutMorph demandsBoolean ifTrue:
 		[self isBoolean ifTrue: [^ self].
 		aTemporaryViewer _ CategoryViewer new invisiblySetPlayer: aPlayer.
 		aPhrase _ aTemporaryViewer booleanPhraseFromPhrase: self.
+		aPhrase justGrabbedFromViewer: false.
 		^ aPhrase].
 	^ self
