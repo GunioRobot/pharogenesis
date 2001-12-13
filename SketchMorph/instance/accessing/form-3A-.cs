50bee522751a7d7622fa79e9bee48f016661464e
sw <unknown>
2001-12-13 12:10:00 +0000

    no message

--- SketchMorph/instance/accessing/form-3A-.cs
+++ SketchMorph/instance/accessing/form-3A-.cs
@@ -1,5 +1,12 @@
 form: aForm
+	"Set the receiver's form"
 
+	| oldForm topRenderer |
+	oldForm _ originalForm.
+	(self hasProperty: #baseGraphic) ifFalse: [self setProperty: #baseGraphic toValue: aForm].
 	originalForm _ aForm.
 	self rotationCenter: 0.5@0.5.
 	self layoutChanged.
+	topRenderer _ self topRendererOrSelf.
+
+	oldForm ifNotNil: [topRenderer position: topRenderer position + (oldForm extent - aForm extent // 2)].
