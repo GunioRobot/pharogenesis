4ee7eb53990f03b82d624e59d67e930910bd6bf7
sw <unknown>
2001-12-13 14:34:00 +0000

    no message

--- Player/instance/misc/color-3A-sees-3A-.cs
+++ Player/instance/misc/color-3A-sees-3A-.cs
@@ -1,2 +1,5 @@
 color: myColor sees: externalColor
+	"Answer whether any pixel of one color on my costume is coincident with any pixel of a second color in its surround.  Returns false if the costume is not currently in the world"
+
+	self costume isInWorld ifFalse: [^ false].
 	^ self costume color: myColor sees: externalColor
