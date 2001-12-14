3686ea77345047138d4a435eed3315c0164a2280
ar <unknown>
2001-12-14 20:02:00 +0000

    no message

---
+++ BorderedStringMorph/instance/initialization/initWithContents-3A-font-3A-emphasis-3A-.cs
@@ -0,0 +1,3 @@
+initWithContents: aString font: aFont emphasis: emphasisCode
+	super initWithContents: aString font: aFont emphasis: emphasisCode.
+	self borderStyle: (SimpleBorder width: 1 color: Color white).
