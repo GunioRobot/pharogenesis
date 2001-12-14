e2155598938416eeaa9cd89c2bc1afc06a392535
sw <unknown>
2001-12-14 01:29:00 +0000

    no message

--- Debugger/instance/initialize/optionalButtonRow.cs
+++ Debugger/instance/initialize/optionalButtonRow.cs
@@ -1,7 +1,7 @@
 optionalButtonRow
 	"Answer a button pane affording the user one-touch access to certain functions; the pane is given the formal name 'buttonPane' by which it can be retrieved by code wishing to send messages to widgets residing on the pane"
 
-	| aRow aButton |
+	| aRow aButton aLabel |
 	aRow _ AlignmentMorph newRow beSticky.
 	aRow setNameTo: 'buttonPane'.
 	aRow clipSubmorphs: true.
@@ -9,18 +9,28 @@ optionalButtonRow
 	aButton color: Color lightRed; borderWidth: 1; borderColor: Color red darker.
 	aRow addTransparentSpacerOfSize: (5@0).
 	self optionalButtonPairs do:
-		[:pair |
+		[:tuple |
 				aButton _ PluggableButtonMorph
 					on: self
 					getState: nil
-					action: pair second.
+					action: tuple second.
 				aButton
 					hResizing: #spaceFill;
 					vResizing: #spaceFill;
 					useRoundedCorners;
-					label: pair first asString;
-					askBeforeChanging: true;
 					onColor: Color transparent offColor: Color transparent.
+				(#(proceed restart send doStep stepIntoBlock fullStack where) includes: tuple second)
+					ifTrue:
+						[aButton askBeforeChanging: true].
+
+				aLabel _ Preferences abbreviatedBrowserButtons 
+					ifTrue: [self abbreviatedWordingFor: tuple second]
+					ifFalse: [nil].
+				aButton label: (aLabel ifNil: [tuple first asString]).
+
+				tuple size > 2 ifTrue: [aButton setBalloonText: tuple third].
+				Preferences alternativeWindowLook 
+					ifTrue:[aButton borderWidth: 2; borderColor: #raised].
 				aRow addMorphBack: aButton.
 				aRow addTransparentSpacerOfSize: (3 @ 0)].
 	^ aRow
