3b61e81fb4a4b353c13f336907d6573b5c1da6cc
ar <unknown>
2001-12-16 19:27:00 +0000

    no message

--- CharacterScanner/instance/scanning/placeEmbeddedObject-3A-.cs
+++ CharacterScanner/instance/scanning/placeEmbeddedObject-3A-.cs
@@ -2,6 +2,8 @@ placeEmbeddedObject: anchoredMorph
 	"Place the anchoredMorph or return false if it cannot be placed.
 	In any event, advance destX by its width."
 	| w |
+	"Workaround: The following should really use #textAnchorType"
+	anchoredMorph relativeTextAnchorPosition ifNotNil:[^true].
 	destX _ destX + (w _ anchoredMorph width).
 	(destX > rightMargin and: [(leftMargin + w) <= rightMargin])
 		ifTrue: ["Won't fit, but would on next line"
