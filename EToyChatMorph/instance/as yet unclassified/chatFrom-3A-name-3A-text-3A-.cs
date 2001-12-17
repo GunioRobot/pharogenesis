e001b9cb495f374a0750b4fe596e6e8efdb43c83
ar <unknown>
2001-12-17 02:18:00 +0000

    no message

--- EToyChatMorph/instance/as yet unclassified/chatFrom-3A-name-3A-text-3A-.cs
+++ EToyChatMorph/instance/as yet unclassified/chatFrom-3A-name-3A-text-3A-.cs
@@ -6,7 +6,7 @@ chatFrom: ipAddress name: senderName text: text
 		initialText _ senderName asText allBold.
 	] ifNotNil: [
 		attrib _ TextAnchor new anchoredMorph: recipientForm "asMorph".
-		initialText _ '*' asText.
+		initialText _ (String value: 1) asText.
 		initialText addAttribute: attrib from: 1 to: 1.
 	].
 	self appendMessage: initialText,' - ',text,String cr.
