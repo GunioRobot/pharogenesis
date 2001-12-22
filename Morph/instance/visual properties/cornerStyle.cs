8045638b617159f0ad16cc9760d4fd2895dc5170
ar <unknown>
2001-12-22 22:44:00 +0000

    no message

--- Morph/instance/visual properties/cornerStyle.cs
+++ Morph/instance/visual properties/cornerStyle.cs
@@ -1,4 +1,2 @@
 cornerStyle
-	"Answer the style, #square or #rounded, of corners.  This is a backstop implementation for morphs that don't do rounded corners."
-
-	^ #square
+	^ self valueOfProperty: #cornerStyle ifAbsent: [#square]
