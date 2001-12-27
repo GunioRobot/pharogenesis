64a861e7beb4796ba06b8565e48686b1936a9136
ar <unknown>
2001-12-27 22:29:00 +0000

    no message

--- Behavior/instance/accessing method dictionary/methodHeaderFor-3A-.cs
+++ Behavior/instance/accessing method dictionary/methodHeaderFor-3A-.cs
@@ -2,7 +2,7 @@ methodHeaderFor: selector
 	"Answer the string corresponding to the method header for the given selector"
 
 	| sourceString parser |
-	sourceString _ self ultimateSourceCodeAt: selector ifAbsent: [self error: 'not found'].
+	sourceString _ self ultimateSourceCodeAt: selector ifAbsent: [self standardMethodHeaderFor: selector].
 	(parser _ self parserClass new) parseSelector: sourceString.
 	^ sourceString asString copyFrom: 1 to: (parser endOfLastToken min: sourceString size)
 
