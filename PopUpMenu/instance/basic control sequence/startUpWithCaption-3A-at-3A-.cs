e22b239f22c0bb6847066378fc70502bf43b0c6e
ar <unknown>
2001-12-27 22:47:00 +0000

    no message

--- PopUpMenu/instance/basic control sequence/startUpWithCaption-3A-at-3A-.cs
+++ PopUpMenu/instance/basic control sequence/startUpWithCaption-3A-at-3A-.cs
@@ -4,26 +4,4 @@ startUpWithCaption: captionOrNil at: location
 	answer the index of the current selection, or zero if the mouse is not released over 
 	any menu item. Location specifies the desired topLeft of the menu body rectangle."
 
-	| maxHeight |
-	maxHeight _ Display height*3//4.
-	self frameHeight > maxHeight ifTrue:
-		[^ self
-			startUpSegmented: maxHeight
-			withCaption: captionOrNil
-			at: location].
-
-	Smalltalk isMorphic
-		ifTrue:
-			[selection _ Cursor normal showWhile:
-				[(MVCMenuMorph from: self title: captionOrNil) 
-					invokeAt: location 
-					in: (Display morphicWorldAt: location)].
-			^ selection].
-
-	frame ifNil: [self computeForm].
-	Cursor normal showWhile:
-		[self
-			displayAt: location
-			withCaption: captionOrNil
-			during: [self controlActivity]].
-	^ selection
+		^ self startUpWithCaption: captionOrNil at: location allowKeyboard: Preferences menuKeyboardControl
