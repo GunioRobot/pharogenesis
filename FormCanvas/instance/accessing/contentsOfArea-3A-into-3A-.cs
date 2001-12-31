dc923edaac40dfe1b87952b107ae3816cb507dcf
ar <unknown>
2001-12-31 03:26:00 +0000

    no message

--- FormCanvas/instance/accessing/contentsOfArea-3A-into-3A-.cs
+++ FormCanvas/instance/accessing/contentsOfArea-3A-into-3A-.cs
@@ -1,5 +1,9 @@
 contentsOfArea: aRectangle into: aForm
+	| bb |
 	self flush.
-	port contentsOfArea: ((aRectangle origin + origin) negated extent: aRectangle extent)
-		into: aForm.
+	bb _ BitBlt toForm: aForm.
+	bb sourceForm: form; combinationRule: Form over;
+		sourceX: (aRectangle left + origin x); sourceY: (aRectangle top + origin y);
+		width: aRectangle width; height: aRectangle height;
+		copyBits.
 	^aForm
