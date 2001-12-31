d81877fff5922eee4b0a9daa20f6b3a76586e659
ar <unknown>
2001-12-31 02:51:00 +0000

    no message

--- DropDownChoiceMorph/instance/drawing/maxExtent-3A-.cs
+++ DropDownChoiceMorph/instance/drawing/maxExtent-3A-.cs
@@ -1,11 +1,11 @@
 maxExtent: listOfStrings
 
-	| scanner h w maxW |
+	| h w maxW f |
 	maxW _ 0.
 	listOfStrings do: [:str |
-		scanner _ DisplayScanner quickPrintOn: Display box: Display boundingBox font: self fontToUse.
-		w _ (scanner stringWidth: str).
-		h _ scanner lineHeight.
+		f _ self fontToUse.
+		w _ f widthOfString: str.
+		h _ f height.
 		maxW _ maxW max: w].
 	self extent: (maxW + 4 + h) @ (h + 4).
 	self changed
