64629059d4fb3a44ce4750a1881a9e43ea3e4532
sd <unknown>
2002-01-05 21:11:00 +0000

    no message

--- Browser/instance/system category functions/browseAllClasses.cs
+++ Browser/instance/system category functions/browseAllClasses.cs
@@ -2,5 +2,5 @@ browseAllClasses
 	"Create and schedule a new browser on all classes alphabetically."
 	| newBrowser |
 	newBrowser _ HierarchyBrowser new initAlphabeticListing.
-	Browser openBrowserView: (newBrowser openSystemCatEditString: nil)
+	self class openBrowserView: (newBrowser openSystemCatEditString: nil)
 		label: 'All Classes Alphabetically'
