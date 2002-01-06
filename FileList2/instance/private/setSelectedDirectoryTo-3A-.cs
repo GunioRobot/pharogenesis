628c86e12bb0e582dbf8cb3b1348400648d3e67b
LC <unknown>
2002-01-06 09:03:00 +0000

    no message

---
+++ FileList2/instance/private/setSelectedDirectoryTo-3A-.cs
@@ -0,0 +1,8 @@
+setSelectedDirectoryTo: aFileDirectoryWrapper
+	currentDirectorySelected _ aFileDirectoryWrapper.
+	self directory: aFileDirectoryWrapper withoutListWrapper.
+	brevityState := #FileList.
+	"self addPath: path."
+	self changed: #fileList.
+	self changed: #contents.
+	self changed: #currentDirectorySelected.
