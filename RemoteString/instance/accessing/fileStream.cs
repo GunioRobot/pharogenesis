9b14221162d0f5f675b1f554fb234564573c465b
ajh <unknown>
2002-01-18 01:04:00 +0000

    no message

---
+++ RemoteString/instance/accessing/fileStream.cs
@@ -0,0 +1,8 @@
+fileStream 
+	"Answer the file stream with position set at the beginning of my string"
+
+	| theFile |
+	(sourceFileNumber == nil or: [(SourceFiles at: sourceFileNumber) == nil]) ifTrue: [^ nil].
+	theFile _ SourceFiles at: sourceFileNumber.
+	theFile position: filePositionHi.
+	^ theFile
