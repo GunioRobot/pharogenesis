dffed377d8b0c27871fbfe94ee725a15b2cd52f6
JMM <unknown>
2002-01-20 21:35:00 +0000

    no message

--- FilePlugin/instance/file primitives/primitiveFileSize.cs
+++ FilePlugin/instance/file primitives/primitiveFileSize.cs
@@ -1,9 +1,10 @@
 primitiveFileSize
 	| file size |
 	self var: 'file' declareC: 'SQFile *file'.
+	self var: 'size' type: 'squeakFileOffsetType'.
 	self export: true.
 	file _ self fileValueOf: (interpreterProxy stackValue: 0).
 	interpreterProxy failed ifFalse:[size _ self sqFileSize: file].
 	interpreterProxy failed ifFalse: [
 		interpreterProxy pop: 2.
-		interpreterProxy push: (interpreterProxy positive32BitIntegerFor: size)].
+		interpreterProxy push: (interpreterProxy positive64BitIntegerFor: size)].
