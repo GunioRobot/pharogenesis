9bf7b7f74bde592467c5aa861800bf3db131e56a
JMM <unknown>
2002-01-20 21:35:00 +0000

    no message

--- FilePlugin/instance/file primitives/primitiveFileGetPosition.cs
+++ FilePlugin/instance/file primitives/primitiveFileGetPosition.cs
@@ -1,9 +1,10 @@
 primitiveFileGetPosition
 	| file position |
 	self var: 'file' declareC: 'SQFile *file'.
+	self var: 'position' type: 'squeakFileOffsetType'.
 	self export: true.
 	file _ self fileValueOf: (interpreterProxy stackValue: 0).
 	interpreterProxy failed ifFalse: [position _ self sqFileGetPosition: file].
 	interpreterProxy failed ifFalse: [
 		interpreterProxy pop: 2.
-		interpreterProxy push: (interpreterProxy positive32BitIntegerFor: position)].
+		interpreterProxy push: (interpreterProxy positive64BitIntegerFor: position)].
