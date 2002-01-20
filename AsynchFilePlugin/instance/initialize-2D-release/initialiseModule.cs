992ec4f0e4f5e77209620a30864ec63ec79ef1fa
JMM <unknown>
2002-01-20 22:00:00 +0000

    no message

--- AsynchFilePlugin/instance/initialize-2D-release/initialiseModule.cs
+++ AsynchFilePlugin/instance/initialize-2D-release/initialiseModule.cs
@@ -1,4 +1,5 @@
 initialiseModule
 	"Initialise the module"
 	self export: true.
+	sCOAFfn _ interpreterProxy ioLoadFunction: 'secCanOpenAsyncFileOfSizeWritable' From: 'SecurityPlugin'.
 	^self cCode: 'asyncFileInit()' inSmalltalk:[true]
