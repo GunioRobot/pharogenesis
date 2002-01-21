7d0abfe61b041baf78368dbeb981e7c8daff616a
JMM <unknown>
2002-01-21 11:07:00 +0000

    no message

--- SocketPlugin/instance/primitives/primitiveSocket-3A-connectTo-3A-port-3A-.cs
+++ SocketPlugin/instance/primitives/primitiveSocket-3A-connectTo-3A-port-3A-.cs
@@ -1,11 +1,19 @@
-primitiveSocket: socket connectTo: address port: port
-
-	| addr s |
+primitiveSocket: socket connectTo: address port: port 
+	| addr s okToConnect  |
 	self var: #s declareC: 'SocketPtr s'.
-	self primitive: 'primitiveSocketConnectToPort'
-		parameters: #(Oop ByteArray SmallInteger).
-
-	addr _ self netAddressToInt: (self cCoerce: address to: 'unsigned char *').
+	self primitive: 'primitiveSocketConnectToPort' parameters: #(#Oop #ByteArray #SmallInteger ).
+	addr _ self
+				netAddressToInt: (self cCoerce: address to: 'unsigned char *').
+	"If the security plugin can be loaded, use it to check for permission.
+	If 
+	not, assume it's ok"
+	sCCTPfn ~= 0
+		ifTrue: [okToConnect _ self cCode: ' ((int (*) (int, int)) sCCTPfn)(addr, port)'.
+			okToConnect
+				ifFalse: [^ interpreterProxy primitiveFail]].
 	s _ self socketValueOf: socket.
-	interpreterProxy failed ifFalse: [
-		self sqSocket: s ConnectTo: addr Port: port]
+	interpreterProxy failed
+		ifFalse: [self
+				sqSocket: s
+				ConnectTo: addr
+				Port: port]
