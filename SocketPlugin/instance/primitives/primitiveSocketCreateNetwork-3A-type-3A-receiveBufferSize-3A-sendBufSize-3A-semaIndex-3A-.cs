352d9e2dc6c698c40bea86b77f84aa38797af76d
JMM <unknown>
2002-01-21 11:09:00 +0000

    no message

--- SocketPlugin/instance/primitives/primitiveSocketCreateNetwork-3A-type-3A-receiveBufferSize-3A-sendBufSize-3A-semaIndex-3A-.cs
+++ SocketPlugin/instance/primitives/primitiveSocketCreateNetwork-3A-type-3A-receiveBufferSize-3A-sendBufSize-3A-semaIndex-3A-.cs
@@ -1,14 +1,21 @@
-primitiveSocketCreateNetwork: netType type: socketType receiveBufferSize: recvBufSize sendBufSize: sendBufSize semaIndex: semaIndex
-
-
-	| socketOop s |
+primitiveSocketCreateNetwork: netType type: socketType receiveBufferSize: recvBufSize sendBufSize: sendBufSize semaIndex: semaIndex 
+	| socketOop s okToCreate |
 	self var: #s declareC: 'SocketPtr s'.
-	self primitive: 'primitiveSocketCreate'
-		parameters: #(SmallInteger SmallInteger SmallInteger SmallInteger SmallInteger).
-
-	socketOop _ interpreterProxy instantiateClass: interpreterProxy classByteArray
-					indexableSize: self socketRecordSize.
+	self primitive: 'primitiveSocketCreate' parameters: #(#SmallInteger #SmallInteger #SmallInteger #SmallInteger #SmallInteger ).
+	"If the security plugin can be loaded, use it to check for permission.
+	If 
+	not, assume it's ok"
+	sCCSOTfn ~= 0
+		ifTrue: [okToCreate _ self cCode: ' ((int (*) (int, int)) sCCSOTfn)(netType, socketType)'.
+			okToCreate
+				ifFalse: [^ interpreterProxy primitiveFail]].
+	socketOop _ interpreterProxy instantiateClass: interpreterProxy classByteArray indexableSize: self socketRecordSize.
 	s _ self socketValueOf: socketOop.
-	self sqSocket: s CreateNetType: netType SocketType: socketType
-		RecvBytes: recvBufSize SendBytes: sendBufSize SemaID: semaIndex.
-	^socketOop
+	self
+		sqSocket: s
+		CreateNetType: netType
+		SocketType: socketType
+		RecvBytes: recvBufSize
+		SendBytes: sendBufSize
+		SemaID: semaIndex.
+	^ socketOop
