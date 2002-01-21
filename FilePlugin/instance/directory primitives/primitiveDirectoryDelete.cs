34a5e71614051f9c7725941c29074d00c611f912
JMM <unknown>
2002-01-21 10:38:00 +0000

    no message

--- FilePlugin/instance/directory primitives/primitiveDirectoryDelete.cs
+++ FilePlugin/instance/directory primitives/primitiveDirectoryDelete.cs
@@ -1,13 +1,21 @@
 primitiveDirectoryDelete
-
-	| dirName dirNameIndex dirNameSize |
+	| dirName dirNameIndex dirNameSize okToDelete |
 	self var: #dirNameIndex type: 'char *'.
 	self export: true.
 	dirName _ interpreterProxy stackValue: 0.
-	(interpreterProxy isBytes: dirName) ifFalse:[^interpreterProxy primitiveFail].
+	(interpreterProxy isBytes: dirName)
+		ifFalse: [^ interpreterProxy primitiveFail].
 	dirNameIndex _ interpreterProxy firstIndexableField: dirName.
 	dirNameSize _ interpreterProxy byteSizeOf: dirName.
-	(self cCode: 'dir_Delete((char *) dirNameIndex, dirNameSize)'
-		inSmalltalk:[false])
-		ifFalse:[^interpreterProxy primitiveFail].
-	interpreterProxy pop: 1.  "pop dirName; leave rcvr on stack"
+	"If the security plugin can be loaded, use it to check for permission.
+	If 
+	not, assume it's ok"
+	sCDPfn ~= 0
+		ifTrue: [okToDelete _ self cCode: ' ((int (*) (char *, int)) sCDPfn)(dirNameIndex, dirNameSize)'.
+			okToDelete
+				ifFalse: [^ interpreterProxy primitiveFail]].
+	(self
+			cCode: 'dir_Delete((char *) dirNameIndex, dirNameSize)'
+			inSmalltalk: [false])
+		ifFalse: [^ interpreterProxy primitiveFail].
+	interpreterProxy pop: 1
