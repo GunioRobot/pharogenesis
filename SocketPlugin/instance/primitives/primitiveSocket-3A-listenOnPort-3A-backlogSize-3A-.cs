31e7ed0be9b66ab873dc8b9483cecf3fa1111a2a
JMM <unknown>
2002-01-21 11:08:00 +0000

    no message

--- SocketPlugin/instance/primitives/primitiveSocket-3A-listenOnPort-3A-backlogSize-3A-.cs
+++ SocketPlugin/instance/primitives/primitiveSocket-3A-listenOnPort-3A-backlogSize-3A-.cs
@@ -1,8 +1,19 @@
-primitiveSocket: socket listenOnPort: port backlogSize: backlog
-"second part of the wierdass dual prim primitiveSocketListenOnPort which was warped by some demented evil person determined to twist the very nature of reality"
-	| s |
+primitiveSocket: socket listenOnPort: port backlogSize: backlog 
+	"second part of the wierdass dual prim primitiveSocketListenOnPort 
+	which was warped by some demented evil person determined to twist the 
+	very nature of reality"
+	| s okToListen |
 	self var: #s declareC: 'SocketPtr s'.
-	self primitive: 'primitiveSocketListenOnPortBacklog'
-		parameters: #(Oop SmallInteger SmallInteger).
+	self primitive: 'primitiveSocketListenOnPortBacklog' parameters: #(#Oop #SmallInteger #SmallInteger ).
 	s _ self socketValueOf: socket.
-	self sqSocket: s ListenOnPort: port BacklogSize: backlog.
+	"If the security plugin can be loaded, use it to check for permission.
+	If 
+	not, assume it's ok"
+	sCCLOPfn ~= 0
+		ifTrue: [okToListen _ self cCode: ' ((int (*) (SocketPtr, int)) sCCLOPfn)(s, port)'.
+			okToListen
+				ifFalse: [^ interpreterProxy primitiveFail]].
+	self
+		sqSocket: s
+		ListenOnPort: port
+		BacklogSize: backlog
