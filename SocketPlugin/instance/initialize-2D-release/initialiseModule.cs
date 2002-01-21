1e96495afe877833d2a90d4d881b2f99658f0f03
JMM <unknown>
2002-01-21 11:09:00 +0000

    no message

--- SocketPlugin/instance/initialize-2D-release/initialiseModule.cs
+++ SocketPlugin/instance/initialize-2D-release/initialiseModule.cs
@@ -1,3 +1,8 @@
 initialiseModule
 	self export: true.
+	sDSAfn _ interpreterProxy ioLoadFunction: 'secDisableSocketAccess' From: 'SecurityPlugin'.
+	sHSAfn _ interpreterProxy ioLoadFunction: 'secHasSocketAccess' From: 'SecurityPlugin'.
+	sCCTPfn _ interpreterProxy ioLoadFunction: 'secCanConnectToPort' From: 'SecurityPlugin'.
+	sCCLOPfn _ interpreterProxy ioLoadFunction: 'secCanListenOnPort' From: 'SecurityPlugin'.
+	sCCSOTfn _ interpreterProxy ioLoadFunction: 'secCanCreateSocketOfType' From: 'SecurityPlugin'.
 	^self cCode: 'socketInit()' inSmalltalk:[true]
