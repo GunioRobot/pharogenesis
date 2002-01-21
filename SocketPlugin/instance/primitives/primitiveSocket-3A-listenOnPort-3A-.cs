f6164a0128dba6f18e097821d4447be1bed6fa71
JMM <unknown>
2002-01-21 11:07:00 +0000

    no message

--- SocketPlugin/instance/primitives/primitiveSocket-3A-listenOnPort-3A-.cs
+++ SocketPlugin/instance/primitives/primitiveSocket-3A-listenOnPort-3A-.cs
@@ -1,8 +1,16 @@
-primitiveSocket: socket listenOnPort: port
-"one part of the wierdass dual prim primitiveSocketListenOnPort which was warped by some demented evil person determined to twist the very nature of reality"
-	| s |
+primitiveSocket: socket listenOnPort: port 
+	"one part of the wierdass dual prim primitiveSocketListenOnPort which 
+	was warped by some demented evil person determined to twist the very 
+	nature of reality"
+	| s  okToListen |
 	self var: #s declareC: 'SocketPtr s'.
-	self primitive: 'primitiveSocketListenOnPort'
-		parameters: #(Oop SmallInteger).
+	self primitive: 'primitiveSocketListenOnPort' parameters: #(#Oop #SmallInteger ).
 	s _ self socketValueOf: socket.
-	self sqSocket: s ListenOnPort: port.
+	"If the security plugin can be loaded, use it to check for permission.
+	If 
+	not, assume it's ok"
+	sCCLOPfn ~= 0
+		ifTrue: [okToListen _ self cCode: ' ((int (*) (SocketPtr, int)) sCCLOPfn)(s, port)'.
+			okToListen
+				ifFalse: [^ interpreterProxy primitiveFail]].
+	self sqSocket: s ListenOnPort: port
