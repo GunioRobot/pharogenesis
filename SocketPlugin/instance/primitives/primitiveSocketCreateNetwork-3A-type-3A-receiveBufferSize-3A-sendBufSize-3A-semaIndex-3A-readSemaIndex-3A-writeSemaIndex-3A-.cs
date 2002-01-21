492bc9d56361f4c220450be1781a4b0461b14092
JMM <unknown>
2002-01-21 11:10:00 +0000

    no message

--- SocketPlugin/instance/primitives/primitiveSocketCreateNetwork-3A-type-3A-receiveBufferSize-3A-sendBufSize-3A-semaIndex-3A-readSemaIndex-3A-writeSemaIndex-3A-.cs
+++ SocketPlugin/instance/primitives/primitiveSocketCreateNetwork-3A-type-3A-receiveBufferSize-3A-sendBufSize-3A-semaIndex-3A-readSemaIndex-3A-writeSemaIndex-3A-.cs
@@ -1,14 +1,23 @@
-primitiveSocketCreateNetwork: netType type: socketType receiveBufferSize: recvBufSize sendBufSize: sendBufSize semaIndex: semaIndex readSemaIndex: aReadSema writeSemaIndex: aWriteSema
-
-
-	| socketOop s |
+primitiveSocketCreateNetwork: netType type: socketType receiveBufferSize: recvBufSize sendBufSize: sendBufSize semaIndex: semaIndex readSemaIndex: aReadSema writeSemaIndex: aWriteSema 
+	| socketOop s okToCreate |
 	self var: #s declareC: 'SocketPtr s'.
-	self primitive: 'primitiveSocketCreate3Semaphores'
-		parameters: #(SmallInteger SmallInteger SmallInteger SmallInteger SmallInteger SmallInteger SmallInteger).
-
-	socketOop _ interpreterProxy instantiateClass: interpreterProxy classByteArray
-					indexableSize: self socketRecordSize.
+	self primitive: 'primitiveSocketCreate3Semaphores' parameters: #(#SmallInteger #SmallInteger #SmallInteger #SmallInteger #SmallInteger #SmallInteger #SmallInteger ).
+	"If the security plugin can be loaded, use it to check for permission.
+	If 
+	not, assume it's ok"
+	sCCSOTfn ~= 0
+		ifTrue: [okToCreate _ self cCode: ' ((int (*) (int, int)) sCCSOTfn)(netType, socketType)'.
+			okToCreate
+				ifFalse: [^ interpreterProxy primitiveFail]].
+	socketOop _ interpreterProxy instantiateClass: interpreterProxy classByteArray indexableSize: self socketRecordSize.
 	s _ self socketValueOf: socketOop.
-	self sqSocket: s CreateNetType: netType SocketType: socketType
-		RecvBytes: recvBufSize SendBytes: sendBufSize SemaID: semaIndex ReadSemaID: aReadSema WriteSemaID: aWriteSema.
-	^socketOop
+	self
+		sqSocket: s
+		CreateNetType: netType
+		SocketType: socketType
+		RecvBytes: recvBufSize
+		SendBytes: sendBufSize
+		SemaID: semaIndex
+		ReadSemaID: aReadSema
+		WriteSemaID: aWriteSema.
+	^ socketOop
