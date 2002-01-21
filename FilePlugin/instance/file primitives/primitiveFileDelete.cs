975b1ddf70ff22b15f5937e637562eea0d461158
JMM <unknown>
2002-01-21 10:59:00 +0000

    no message

--- FilePlugin/instance/file primitives/primitiveFileDelete.cs
+++ FilePlugin/instance/file primitives/primitiveFileDelete.cs
@@ -1,13 +1,21 @@
 primitiveFileDelete
-
-	| namePointer nameIndex nameSize |
+	| namePointer nameIndex nameSize  okToDelete |
 	self var: 'nameIndex' type: 'char *'.
 	self export: true.
 	namePointer _ interpreterProxy stackValue: 0.
-	(interpreterProxy isBytes: namePointer) 
-		ifFalse:[^interpreterProxy primitiveFail].
+	(interpreterProxy isBytes: namePointer)
+		ifFalse: [^ interpreterProxy primitiveFail].
 	nameIndex _ interpreterProxy firstIndexableField: namePointer.
 	nameSize _ interpreterProxy byteSizeOf: namePointer.
-	self sqFileDeleteName: (self cCoerce: nameIndex to: 'int') Size: nameSize.
-	interpreterProxy failed 
-		ifFalse:[interpreterProxy pop: 1. "pop name, leave rcvr on stack" ].
+	"If the security plugin can be loaded, use it to check for permission.
+	If 
+	not, assume it's ok"
+	sCDFfn ~= 0
+		ifTrue: [okToDelete _ self cCode: ' ((int (*) (char *, int)) sCDFfn)(nameIndex, nameSize)'.
+			okToDelete
+				ifFalse: [^ interpreterProxy primitiveFail]].
+	self
+		sqFileDeleteName: (self cCoerce: nameIndex to: 'int')
+		Size: nameSize.
+	interpreterProxy failed
+		ifFalse: [interpreterProxy pop: 1]
