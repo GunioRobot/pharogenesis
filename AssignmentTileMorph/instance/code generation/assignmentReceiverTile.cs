78f7a45afad6b15f2126cdb6b093584b11728b4d
sw <unknown>
2002-02-06 01:17:00 +0000

    no message

---
+++ AssignmentTileMorph/instance/code generation/assignmentReceiverTile.cs
@@ -0,0 +1,4 @@
+assignmentReceiverTile
+	"Answer the TilePadMorph that should be sent storeCodeOn:indent: to get the receiver of the assignment properly stored on the code stream"
+
+	^ owner submorphs first
