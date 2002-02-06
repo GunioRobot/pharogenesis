d59539fa3c2a51c18b29affffa448316af117b25
ar <unknown>
2002-02-06 12:18:00 +0000

    no message

--- PolygonMorph/instance/private/computeBounds.cs
+++ PolygonMorph/instance/private/computeBounds.cs
@@ -1,10 +1,24 @@
 computeBounds
+	| oldBounds delta excludeHandles |
 	vertices ifNil: [^ self].
+
 	self changed.
+	oldBounds _ bounds.
 	self releaseCachedState.
-	bounds _ self curveBounds.
+	bounds _ self curveBounds truncated.
 	self arrowForms do:
 		[:f | bounds _ bounds merge: (f offset extent: f extent)].
 	handles ifNotNil: [self updateHandles].
+
+	"since we are directly updating bounds, see if any ordinary submorphs exist and move them accordingly"
+	(oldBounds notNil and: [(delta _ bounds origin - oldBounds origin) ~= (0@0)]) ifTrue: [
+		excludeHandles _ IdentitySet new.
+		handles ifNotNil: [excludeHandles addAll: handles].
+		self submorphsDo: [ :each |
+			(excludeHandles includes: each) ifFalse: [
+				each position: each position + delta
+			].
+		].
+	].
 	self layoutChanged.
-	self changed
+	self changed.
