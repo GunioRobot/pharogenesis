a2cc914ff3afcdd64bbfaf257ba6421066923d14
ls <unknown>
2002-02-08 22:23:00 +0000

    no message

--- IndexFile/instance/dictionary access/at-3A-put-3A-.cs
+++ IndexFile/instance/dictionary access/at-3A-put-3A-.cs
@@ -1,10 +1,14 @@
 at: msgID put: anIndexFileEntry 
 	"Associate the given IndexFileEntry with the given message ID."
-	timeSortedEntries removeAllSuchThat: [:assoc | assoc key = msgID].
-	"don't duplicate the entry!"
+	| fileStream |
 	self privateAt: msgID put: anIndexFileEntry.
-	self logStream print: msgID;
-	 cr.
-	"message ID"
-	anIndexFileEntry writeOn: self logStream.
-	self logStream close
+	
+	"save the entry immediately to disk"
+	fileStream := self fileStream .
+	fileStream setToEnd.
+	fileStream
+		print: msgID;
+	 	cr.
+	anIndexFileEntry writeOn: fileStream.
+	fileStream close.
+	self updateSizeAndModTime.
