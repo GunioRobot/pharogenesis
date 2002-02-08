131da6889739ad1a9b8d01a9157d548e3f15d48d
ls <unknown>
2002-02-08 22:07:00 +0000

    no message

--- IndexFile/instance/dictionary access/privateRemove-3A-.cs
+++ IndexFile/instance/dictionary access/privateRemove-3A-.cs
@@ -2,4 +2,4 @@ privateRemove: msgID
 	"Remove the entry with the given ID from my Dictionary."
 	timeSortedEntries removeAllSuchThat: [:assoc | assoc key = msgID].
 	msgDictionary removeKey: msgID ifAbsent: [].
-	
+	anyRemovalsSinceLastSave := true.
