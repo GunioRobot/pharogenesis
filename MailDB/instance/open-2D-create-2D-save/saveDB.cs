2ee9e5645df52255399a92367ee6c95a9dfcd03b
ls <unknown>
2002-02-08 21:49:00 +0000

    no message

--- MailDB/instance/open-2D-create-2D-save/saveDB.cs
+++ MailDB/instance/open-2D-create-2D-save/saveDB.cs
@@ -1,10 +1,14 @@
 saveDB
-	"Write all database files to disk."
-	rootFilename notNil
-		ifTrue: [Transcript show: 'Saving mail database ''' , rootFilename , '''...']
-		ifFalse: [Transcript show: 'Saving mail database...'].
+	"Write all database files to disk.  noRemovals specifies whether any messages were removed from the database -- if so, then the index file doesn't need to be re-saved"
+
+	"Return quietly if the database is no longer in use"
+	rootFilename isNil
+		ifTrue: [^self].
+
+	Transcript show: 'Saving mail database ''' , (rootFilename ifNil: ['']) , '''...'.
+
 	messageFile notNil ifTrue: [messageFile save].
-	"indexFile notNil ifTrue: [indexFile save]."
+	indexFile notNil ifTrue: [indexFile save].
 	categoriesFile notNil ifTrue: [categoriesFile save].
-	Transcript show: 'Done.';
-	 cr
+
+	Transcript show: 'Done.'; cr
