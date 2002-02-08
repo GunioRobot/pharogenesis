8a8f475fd7a76c18b6c683728f5618faa20a8fad
ls <unknown>
2002-02-08 21:31:00 +0000

    no message

--- MailDBFile/instance/file operations/save.cs
+++ MailDBFile/instance/file operations/save.cs
@@ -1,16 +1,18 @@
 save
-	"Atomically save a representation of this object to its file. The old file is 
-	renamed to '<oldname>.bak' before the new file is written. If the write 
-	operation fails, the old file may be restored by renaming it. If it 
-	succeeds, the .bak file is deleted."
-	"create the file if it doesn't already exist"
+	"Atomically save a representation of this object to its file.  The new file is written to <name>.new, and on success, renamed to simply <name>.  If the write fails, then the old version will still exist"
+
 	| f dir shortName |
 
 	(StandardFileStream fileNamed: filename) close.	"ensure it exists"
 	shortName _ FileDirectory localNameFor: filename.
 	dir _ FileDirectory forFileName: filename.
-	dir rename: shortName toBe: shortName , '.bak'.
-	Cursor write showWhile: [f _ FileStream fileNamed: filename.
-	self writeOn: f.
-	f setToEnd; close.].
-	dir deleteFileNamed: shortName , '.bak' ifAbsent: []
+
+	Cursor write showWhile: [
+		f _ FileStream fileNamed: filename, '.new'.
+		self writeOn: f.
+		f setToEnd; close
+		].
+	dir deleteFileNamed: shortName ifAbsent: [].
+	dir rename: shortName, '.new' toBe: shortName.
+
+	self updateSizeAndModTime.
