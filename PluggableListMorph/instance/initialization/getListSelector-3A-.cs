27a29a4088eba078a0f86c49ddc124aba1962e8c
ls <unknown>
2002-02-09 01:03:00 +0000

    no message

--- PluggableListMorph/instance/initialization/getListSelector-3A-.cs
+++ PluggableListMorph/instance/initialization/getListSelector-3A-.cs
@@ -2,4 +2,5 @@ getListSelector: sel
 	"Set the receiver's getListSelector as indicated, and trigger a recomputation of the list"
 
 	getListSelector _ sel.
-	self changed
+	self changed.
+	self updateList.
