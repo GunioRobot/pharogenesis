57344b3adac46d656e9dd9bba7fe60837106f41f
nk <unknown>
2002-02-11 12:18:00 +0000

    no message

--- EventSensor/instance/private-2D-I-2F-O/processEvent-3A-.cs
+++ EventSensor/instance/private-2D-I-2F-O/processEvent-3A-.cs
@@ -10,8 +10,17 @@ processEvent: evt
 					^interruptSemaphore signal].
 
 	"Store the event in the queue if there's any"
+	type = EventTypeMouse ifTrue:
+		[evt at: 5 put: (ButtonDecodeTable at: (evt at: 5) + 1)].
+
+	type = EventTypeKeyboard ifTrue:
+		["swap ctrl/alt keys"
+		KeyDecodeTable at: { evt at: 3 . evt at: 5 } ifPresent: [:a |
+			evt at: 3 put: a first;
+				at: 5 put: a second]].
+
 	self queueEvent: evt.
 
 	"Update state for InputSensor."
 	EventTypeMouse = type ifTrue:[self processMouseEvent: evt].
-	EventTypeKeyboard = type ifTrue:[self processKeyboardEvent: evt].
+	EventTypeKeyboard = type ifTrue:[self processKeyboardEvent: evt]
