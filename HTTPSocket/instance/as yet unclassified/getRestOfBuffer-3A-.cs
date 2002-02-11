fb7ef17287cf65ad1f1625044e3425ecea7466ca
hg <unknown>
2002-02-11 20:13:00 +0000

    no message

--- HTTPSocket/instance/as yet unclassified/getRestOfBuffer-3A-.cs
+++ HTTPSocket/instance/as yet unclassified/getRestOfBuffer-3A-.cs
@@ -8,12 +8,13 @@ getRestOfBuffer: beginning
 
 	[self isConnected | self dataAvailable] 
 	whileTrue: [
-		(self waitForDataUntil: (Socket deadlineSecs: 5)) ifTrue: [
+		(self waitForDataUntil: (Socket deadlineSecs: 5)) ifFalse: [
 	 		Transcript show: 'data was slow'; cr].
 		bytesRead _ self primSocket: socketHandle receiveDataInto: buf 
 				startingAt: 1 count: buf size. 
 		bytesRead > 0 ifTrue: [  
 			response nextPutAll: (buf copyFrom: 1 to: bytesRead)] ].
-	Transcript cr; show: 'data byte count: ', response position printString.
+	self logToTranscript ifTrue: [
+		Transcript cr; show: 'data byte count: ', response position printString].
 	response reset.	"position: 0."
 	^ response
