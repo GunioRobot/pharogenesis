8633327baed27aacdd717dce671bdefbbf7b233e
hg <unknown>
2002-02-12 11:39:00 +0000

    no message

--- HTTPSocket/class/get the page/httpGet-3A-args-3A-accept-3A-.cs
+++ HTTPSocket/class/get the page/httpGet-3A-args-3A-accept-3A-.cs
@@ -1,11 +1,3 @@
 httpGet: url args: args accept: mimeType
-	"Return the exact contents of a web object. Asks for the given MIME type. If mimeType is nil, use 'text/html'. The parsed header is saved. Use a proxy server if one has been registered.  tk 7/23/97 17:12"
-	"Note: To fetch raw data, you can use the MIMI type 'application/octet-stream'."
 
-	| document |
-	document _ self httpGetDocument: url  args: args  accept: mimeType.
-	(document isKindOf: String) ifTrue: [
-		"strings indicate errors"
-		^ document ].
-
-	^ (RWBinaryOrTextStream with: document content) reset
+	^self httpGet: url args: args accept: mimeType request: ''
