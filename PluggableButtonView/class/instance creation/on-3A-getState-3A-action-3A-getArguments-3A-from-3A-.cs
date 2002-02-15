c0f186c0754b711e35918436657d39c4dc8df071
sumim <unknown>
2002-02-15 17:18:00 +0000

    no message

---
+++ PluggableButtonView/class/instance creation/on-3A-getState-3A-action-3A-getArguments-3A-from-3A-.cs
@@ -0,0 +1,10 @@
+on: anObject getState: getStateSel action: actionSel getArguments: getArgumentsSel from: argsProvidor
+
+	^ self new
+		on: anObject
+		getState: getStateSel
+		action: actionSel
+		getArguments: getArgumentsSel
+		from: argsProvidor
+		label: nil
+		menu: nil
