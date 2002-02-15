85f7c0b9923c01506ec443448ab11543712412fa
sw <unknown>
2002-02-15 02:37:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/getScaleFactor.cs
+++ Player/instance/slot getters-2F-setters/getScaleFactor.cs
@@ -1,7 +1,4 @@
 getScaleFactor
 	"Answer the scale factor of the object"
 
-	| aCostume |
-	^ (aCostume _ self costume) isFlexMorph
-		ifTrue: [aCostume scale]
-		ifFalse: [1.0]
+	^ self costume scaleFactor
