06f4444a4d600c83bac53d2b1a32532e0404c74f
sw <unknown>
2002-02-17 05:32:00 +0000

    no message

--- PluggableButtonView/instance/other/performAction.cs
+++ PluggableButtonView/instance/other/performAction.cs
@@ -1,4 +1,11 @@
 performAction
 	"Inform the model that this button has been pressed. Sent by the controller when this button is pressed."
 
-	actionSelector ifNotNil: [model perform: actionSelector].
+	argumentsSelector
+		ifNil:
+			[actionSelector ifNotNil:
+				[model perform: actionSelector]]
+		ifNotNil:
+			[model perform: actionSelector
+				withArguments:
+					(Array with: (argumentsProvider perform: argumentsSelector))]
