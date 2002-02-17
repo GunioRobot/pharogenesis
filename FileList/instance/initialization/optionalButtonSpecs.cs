6b5e62745a0f1ada63fa141e8bdd901c3528040b
sw <unknown>
2002-02-17 00:07:00 +0000

    no message

--- FileList/instance/initialization/optionalButtonSpecs.cs
+++ FileList/instance/initialization/optionalButtonSpecs.cs
@@ -1,9 +1,4 @@
 optionalButtonSpecs
-	^ #(	
-		('Name' 		sortByName				sortingByName	'sort entries by name')
-		('Date'			sortByDate				sortingByDate	'sort entries by date')
-		('Size'			sortBySize				sortingBySize	'sort entries by size')
-		('Changes'		browseChanges			none			'open a changelist browser on selected file')
-		('File-in'		fileInSelection			none			'fileIn the selected file')
-		('File-in to New'	fileIntoNewChangeSet	none			'fileIn the selected file into a new change set')
-		('Delete'			deleteFile				none			'delete the seleted item'))
+	"Answer a list of services underlying the optional buttons in their initial inception."
+
+	^ {self serviceSortByName. self serviceSortByDate. self serviceSortBySize}
