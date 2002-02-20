afde8cb3adf7a02a9e38dcbbc732451587fe38e7
sw <unknown>
2002-02-20 01:09:00 +0000

    no message

--- StandardScriptingSystem/instance/form dictionary/patchInNewStandardPlayerForm.cs
+++ StandardScriptingSystem/instance/form dictionary/patchInNewStandardPlayerForm.cs
@@ -1,5 +1,11 @@
 patchInNewStandardPlayerForm
+	"Patch in a darker and larger representation of a Dot.  No senders -- called from the postscript of an update"
+
 	"ScriptingSystem patchInNewStandardPlayerForm"
+
 	FormDictionary at: #standardPlayer put:
-		(Form extent: 3@3
-	depth: 16 fromArray: #( 17969 0 1177634353 1177616384 17969 0) offset: 0@0)
+		(Form
+	extent: 13@13
+	depth: 16
+	fromArray: #( 0 0 0 65536 0 0 0 0 0 65537 65537 65536 0 0 0 65537 65537 65537 65537 65536 0 0 65537 65537 65537 65537 65536 0 1 65537 65537 65537 65537 65537 0 1 65537 65537 65537 65537 65537 0 65537 65537 65537 65537 65537 65537 65536 1 65537 65537 65537 65537 65537 0 1 65537 65537 65537 65537 65537 0 0 65537 65537 65537 65537 65536 0 0 65537 65537 65537 65537 65536 0 0 0 65537 65537 65536 0 0 0 0 0 65536 0 0 0)
+	offset: 0@0)
