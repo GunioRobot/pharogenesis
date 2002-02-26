728870756a79c20f769781aa385f1356e1bfcced
sw <unknown>
2002-02-26 14:35:00 +0000

    no message

---
+++ Lexicon/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name wording: 'Lexicon' brightColor: #(0.878 1.000 0.878) pastelColor: #(0.925 1.000 0.925) helpMessage: 'A tool for browsing the full protocol of a class.'
