6b814b0eeeed7cf20a5b752b1fddb860411a59ec
sw <unknown>
2002-02-26 14:43:00 +0000

    no message

---
+++ SelectorBrowser/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name wording: 'Method Finder' brightColor: #lightCyan	pastelColor: #palePeach helpMessage: 'A tool for finding methods by giving sample arguments and values.'
