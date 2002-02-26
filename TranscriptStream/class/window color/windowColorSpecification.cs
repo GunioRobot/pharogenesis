63995d286e8d1ec162704171aafa625784499f06
sw <unknown>
2002-02-26 14:46:00 +0000

    no message

---
+++ TranscriptStream/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name wording: 'Transcript' brightColor: #lightOrange pastelColor: #paleOrange helpMessage: 'The system transcript'
