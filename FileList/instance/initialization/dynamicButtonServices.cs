09614eda57a3090a42f5fc278f5635f246d43aba
sw <unknown>
2002-02-26 00:37:00 +0000

    no message

---
+++ FileList/instance/initialization/dynamicButtonServices.cs
@@ -0,0 +1,11 @@
+dynamicButtonServices
+	"Answer services for buttons that may come and go in the button pane, depending on selection"
+
+	^ fileName isEmptyOrNil
+		ifTrue:
+			[#()]
+		ifFalse:
+			[ | toReject |
+				toReject _ self buttonSelectorsToSuppress.
+				(self itemsForFile: self fullName) reject:
+					[:svc | toReject includes: svc selector]]
