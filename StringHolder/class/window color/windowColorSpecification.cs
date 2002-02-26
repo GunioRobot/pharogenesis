9c88032f2bf95172853565426db091eb1a6c0ec6
sw <unknown>
2002-02-26 14:44:00 +0000

    no message

---
+++ StringHolder/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name wording: 'Workspace' brightColor: #lightYellow pastelColor: #paleYellow helpMessage: 'A place for text in a window.'
