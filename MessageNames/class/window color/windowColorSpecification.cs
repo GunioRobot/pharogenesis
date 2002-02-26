ffe4a35c99ef7208c3803d0dc9b0b7b767085595
sw <unknown>
2002-02-26 14:35:00 +0000

    no message

---
+++ MessageNames/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name wording: 'Message Names' brightColor: #(0.645 1.0 0.452) pastelColor: #(0.843 0.976 0.843) helpMessage: 'A tool finding, viewing, and editing all methods whose names contiane a given character sequence.'
