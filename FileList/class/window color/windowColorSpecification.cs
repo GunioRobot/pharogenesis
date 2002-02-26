5830b94a83583efea9c9d9894bfff59766d6d16e
sw <unknown>
2002-02-26 14:04:00 +0000

    no message

---
+++ FileList/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name  wording: 'File List' brightColor: #lightMagenta pastelColor: #paleMagenta helpMessage: 'A tool for looking at files'
