2ea5a3b9f45cf8b637665c4e603219245e6b116c
sw <unknown>
2002-02-26 12:55:00 +0000

    no message

--- Preferences/class/window colors/installUniformWindowColors.cs
+++ Preferences/class/window colors/installUniformWindowColors.cs
@@ -1,23 +1,5 @@
 installUniformWindowColors
+	"Install the factory-provided uniform window colors for all tools"
+
 	"Preferences installUniformWindowColors"
-	| windowColorDict |
-	(Parameters includesKey: #windowColors) ifFalse:
-		[Parameters at: #windowColors put: IdentityDictionary new].
-	windowColorDict _ Parameters at: #windowColors.
-
-	#(	(Browser				white)
-		(ChangeList				white)
-		(ChangeSorter			white)
-		(Debugger				white)
-		(DualChangeSorter		white)
-		(FileContentsBrowser		white)
-		(FileList					white)
-		(MessageSet				white)
-		(Object					white)
-		(SelectorBrowser			white)
-		(StringHolder			white)
-		(TranscriptStream		white))
-
-	do:
-			[:pair |
-				windowColorDict at: pair first put: (Color perform: pair last)]
+	self installWindowColorsVia: [:aQuad | #white]
