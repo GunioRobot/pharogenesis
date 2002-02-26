09cb6b7bf93efa805420f3bf6c401d2387db5bd7
sw <unknown>
2002-02-26 14:37:00 +0000

    no message

---
+++ MessageSet/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name wording: 'Message List' brightColor: #lightBlue pastelColor: #paleBlue helpMessage: 'A list of messages (e.g. senders, implementors)'
