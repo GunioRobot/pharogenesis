b8ad20a5ffe49fc1d799b848738b1dbb1e6a79fa
sw <unknown>
2002-02-26 14:09:00 +0000

    no message

---
+++ ChangeSorter/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name  wording: 'Change Sorter' brightColor: #lightBlue pastelColor: #paleBlue helpMessage: 'A tool that lets you see the code for one change set at a time.'
