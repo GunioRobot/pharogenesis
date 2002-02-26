a2348bfe15f4a1962bd1b1c8717f98b225cb9721
sw <unknown>
2002-02-26 13:39:00 +0000

    no message

---
+++ WindowColorSpec/instance/initialization/classSymbol-3A-wording-3A-brightColor-3A-pastelColor-3A-helpMessage-3A-.cs
@@ -0,0 +1,8 @@
+classSymbol: sym wording: wrd brightColor: brCol pastelColor: paCol helpMessage: hlpMsg
+	"Initialize the receiver's instance variables"
+
+	classSymbol _ sym.
+	wording _ wrd.
+	brightColor _ brCol.
+	pastelColor _ paCol.
+	helpMessage _ hlpMsg
