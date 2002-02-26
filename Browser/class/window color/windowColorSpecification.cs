5610adcaf2a9c38cc0d361dca580d48c36033f79
sw <unknown>
2002-02-26 13:46:00 +0000

    no message

---
+++ Browser/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name  wording: 'Browser' brightColor: #lightGreen pastelColor: #paleGreen helpMessage: 'The standard "system browser" tool that allows you to browse through all the code in the system'
