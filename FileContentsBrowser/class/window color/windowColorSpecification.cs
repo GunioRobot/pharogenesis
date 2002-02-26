d39366cc6d0197a7d5937d09fb76ce2c55bae92d
sw <unknown>
2002-02-26 14:25:00 +0000

    no message

---
+++ FileContentsBrowser/class/window color/windowColorSpecification.cs
@@ -0,0 +1,4 @@
+windowColorSpecification
+	"Answer a WindowColorSpec object that declares my preference"
+
+	^ WindowColorSpec classSymbol: self name wording: 'File Contents Browser' brightColor: #tan pastelColor: #paleTan helpMessage: 'Lets you view the contents of a file as code, in a browser-like tool.'
