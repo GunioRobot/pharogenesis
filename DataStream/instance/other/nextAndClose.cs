696bd0b7fbd24f9715145eaf7328111d13b0ad91
tk <unknown>
2002-03-05 09:51:00 +0000

    no message

--- DataStream/instance/other/nextAndClose.cs
+++ DataStream/instance/other/nextAndClose.cs
@@ -2,14 +2,6 @@ nextAndClose
 	"Speedy way to grab one object.  Only use when we are inside an object binary file.  Do not use for the start of a SmartRefStream mixed code-and-object file."
 
 	| obj |
-	byteStream peek = 4 ifFalse: ["Try to fix the user's sins..."
-		self inform: 'Should be using fileInObjectAndCode'.
-		byteStream ascii.
-		byteStream fileIn.
-		obj _ SmartRefStream scannedObject.
-		SmartRefStream scannedObject: nil.
-		^ obj].
-
 	obj _ self next.
 	self close.
 	^ obj
