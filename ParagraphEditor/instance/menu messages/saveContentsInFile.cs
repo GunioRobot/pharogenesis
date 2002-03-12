c85a43cc744db2e8cc7bae33e135fda5910f16e2
sw <unknown>
2002-03-12 20:59:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/saveContentsInFile.cs
+++ ParagraphEditor/instance/menu messages/saveContentsInFile.cs
@@ -1,18 +1,32 @@
 saveContentsInFile
-	| fileName stringToSave parentWindow labelToUse |
+	"Save the receiver's contents string to a file, prompting the user for a file-name.  Suggest a reasonable file-name."
+
+	| fileName stringToSave parentWindow labelToUse suggestedName lastIndex |
 	stringToSave _ paragraph text string.
 	stringToSave size == 0 ifTrue: [^ self inform: 'nothing to save.'].
 	parentWindow _ self model dependents
-						detect: [:dep | dep isKindOf: SystemWindow]
+						detect: [:dep | dep isKindOf: SystemWindow orOf: StandardSystemView]
 						ifNone: [nil].
-	parentWindow isNil
-		ifTrue: [labelToUse _ 'Untitled']
-		ifFalse: [labelToUse _ parentWindow label].
+	labelToUse _ parentWindow
+		ifNil: 		['Untitled']
+		ifNotNil: 	[parentWindow label].
+	suggestedName _ nil.
+	#(('Decompressed contents of: '		'.gz')) do:  "can add more here..."
+		[:leaderTrailer |
+			(labelToUse beginsWith: leaderTrailer first) ifTrue:
+				[suggestedName _ labelToUse copyFrom: leaderTrailer first size + 1 to: labelToUse size.
+				(labelToUse endsWith: leaderTrailer last)
+					ifTrue:
+						[suggestedName _ suggestedName copyFrom: 1 to: suggestedName size - leaderTrailer last size]
+					ifFalse:
+						[lastIndex _ suggestedName lastIndexOf: $. ifAbsent: [0].
+						(lastIndex = 0 or: [lastIndex = 1]) ifFalse:
+							[suggestedName _ suggestedName copyFrom: 1 to: lastIndex - 1]]]].
 
-	fileName _ FillInTheBlank request: 'File name? (".text" will be added to end)' 
-			initialAnswer: labelToUse.
-	fileName size == 0 ifTrue: [^ self beep].
-	(fileName asLowercase endsWith: '.text') ifFalse: [fileName _ fileName,'.text'].
-
-	(FileStream newFileNamed: fileName)
-		nextPutAll: stringToSave; close
+	suggestedName ifNil:
+		[suggestedName _ labelToUse, '.text'].
+			
+	fileName _ FillInTheBlank request: 'File name?'
+			initialAnswer: suggestedName.
+	fileName isEmptyOrNil ifFalse:
+		[(FileStream newFileNamed: fileName) nextPutAll: stringToSave; close]
