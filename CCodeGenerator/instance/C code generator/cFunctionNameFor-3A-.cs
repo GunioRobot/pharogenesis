a48a0e630d3c6b2204efdf37ce05b20bf29c53e9
ar <unknown>
2002-03-16 15:33:00 +0000

    no message

--- CCodeGenerator/instance/C code generator/cFunctionNameFor-3A-.cs
+++ CCodeGenerator/instance/C code generator/cFunctionNameFor-3A-.cs
@@ -1,9 +1,4 @@
 cFunctionNameFor: aSelector
 	"Create a C function name from the given selector by omitting colons
 	and prefixing with the plugin name if the method is exported."
-	| meth |
-	pluginPrefix == nil ifTrue:[^aSelector copyWithout: $:].
-	meth _ methods at: aSelector ifAbsent:[nil].
-	(meth notNil and:[meth export])
-		ifTrue:[^pluginPrefix,'_', (aSelector copyWithout: $:)]
-		ifFalse:[^aSelector copyWithout: $:].
+	^aSelector copyWithout: $:
