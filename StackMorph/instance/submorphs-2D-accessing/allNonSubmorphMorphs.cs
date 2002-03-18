759c2022963afd3e67b616de44279d913d40ac89
sw <unknown>
2002-03-18 02:20:00 +0000

    no message

--- StackMorph/instance/submorphs-2D-accessing/allNonSubmorphMorphs.cs
+++ StackMorph/instance/submorphs-2D-accessing/allNonSubmorphMorphs.cs
@@ -1,8 +1,8 @@
 allNonSubmorphMorphs
 	"Return a collection containing all morphs in this morph which are not currently in the submorph containment hierarchy.  Especially the non-showing pages in BookMorphs."
-	| coll |
 
+	| coll |
 	coll _ OrderedCollection new.
-	cards do: [:cd | 
+	self privateCards do: [:cd | 
 		cd privateMorphs ifNotNil: [coll addAll: cd privateMorphs]].
 	^ coll
