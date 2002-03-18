a09ff2d36ffcff94bc28ffea0f55c3f933f1185c
sw <unknown>
2002-03-18 02:12:00 +0000

    no message

--- StackMorph/instance/initialization/initialize.cs
+++ StackMorph/instance/initialization/initialize.cs
@@ -3,12 +3,11 @@ initialize
 
 	| initialBackground |
 	super initialize.
-	initialBackground _ self findA: PasteUpMorph.  "yuk"
+	initialBackground _ pages first.
 	initialBackground extent: (640@480); beSticky.
 	initialBackground beAStackBackground.
 	self beUnsticky.
 	self setProperty: #controlsAtBottom toValue: true.
-
-	cards _ OrderedCollection with: initialBackground currentDataInstance.
+	self privateCards: (OrderedCollection with: initialBackground currentDataInstance).
 
 "self currentHand attachMorph: StackMorph authoringPrototype"
