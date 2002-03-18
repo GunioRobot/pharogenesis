d7953f1186b0b3b5a3b30526a56d53c162d12d16
sw <unknown>
2002-03-18 02:03:00 +0000

    no message

---
+++ StackMorph/instance/card access/insertCardOfBackground-3A-withDataFrom-3A-forInstanceVariables-3A-.cs
@@ -0,0 +1,8 @@
+insertCardOfBackground: aBackground withDataFrom: aLine forInstanceVariables: slotNames
+	"Insert a new card of the given background and have it become the current card. "
+
+	| newCard |
+	newCard _  aBackground newCard.
+	self privateCards add: newCard after: self currentCard.
+	newCard absorbBackgroundDataFrom: aLine forInstanceVariables: slotNames.
+	self goToCard: newCard
