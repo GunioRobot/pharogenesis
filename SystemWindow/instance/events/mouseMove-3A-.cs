f1fe76255cde94fff5e4b241d9aba24f78746644
bmk <unknown>
2002-03-19 02:11:00 +0000

    no message

--- SystemWindow/instance/events/mouseMove-3A-.cs
+++ SystemWindow/instance/events/mouseMove-3A-.cs
@@ -1,7 +1,17 @@
 mouseMove: evt
+	"Handle a mouse-move event"
+
 	| cp |
+	cp _ evt cursorPoint.
+	self valueOfProperty: #clickPoint ifPresentDo: 
+		[:firstClick |
+		((self labelRect containsPoint: firstClick) and: [(cp dist: firstClick) > 3]) ifTrue:
+		["If this is a drag that started in the title bar, then pick me up"
+		^ self isSticky ifFalse:
+			[self fastFramingOn 
+				ifTrue: [self doFastFrameDrag: firstClick]
+				ifFalse: [evt hand grabMorph: self topRendererOrSelf]]]].
 	model windowActiveOnFirstClick ifTrue:
 		["Normally window takes control on first click.
 		Need explicit transmission for first-click activity."
-		cp _ evt cursorPoint.
 		submorphs do: [:m | (m containsPoint: cp) ifTrue: [m mouseMove: evt]]]
