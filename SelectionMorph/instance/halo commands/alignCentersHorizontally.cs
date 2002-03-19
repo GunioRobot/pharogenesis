6bceda5a41f8a73f168f96ba4233a2e2bf7b8c41
sw <unknown>
2002-03-19 22:50:00 +0000

    no message

---
+++ SelectionMorph/instance/halo commands/alignCentersHorizontally.cs
@@ -0,0 +1,9 @@
+alignCentersHorizontally
+	"Make every morph in the selection have the same vertical center as the topmost item."
+
+	| minLeft leftMost |
+	selectedItems size > 1 ifFalse: [^ self].
+	minLeft _ (selectedItems collect: [:itm | itm left]) min.
+	leftMost _ selectedItems detect: [:m | m left = minLeft].
+	selectedItems do:
+		[:itm | itm center: (itm center x @ leftMost center y)]
