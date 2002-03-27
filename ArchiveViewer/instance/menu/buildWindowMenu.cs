35d33b42653f0dc9b9e935c7e5502052f0f3b30f
nk <unknown>
2002-03-27 12:48:00 +0000

    no message

---
+++ ArchiveViewer/instance/menu/buildWindowMenu.cs
@@ -0,0 +1,7 @@
+buildWindowMenu
+	| menu |
+	menu _ super buildWindowMenu.
+	menu addLine.
+	menu add: 'inspect archive' target: archive action: #inspect.
+	menu add: 'write prepending file...' target: self action: #writePrependingFile.
+	^menu.
