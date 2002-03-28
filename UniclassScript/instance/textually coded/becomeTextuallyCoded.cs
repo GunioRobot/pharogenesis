96dc2261fbe9aa27630abc2429988ecfe3ab77ab
sw <unknown>
2002-03-28 00:39:00 +0000

    no message

--- UniclassScript/instance/textually coded/becomeTextuallyCoded.cs
+++ UniclassScript/instance/textually coded/becomeTextuallyCoded.cs
@@ -1,4 +1,5 @@
 becomeTextuallyCoded
 	"Transform the receiver into one which is textually coded"
 
-	isTextuallyCoded _ true
+	isTextuallyCoded _ true.
+	lastSourceString _ (playerClass compiledMethodAt: selector) decompileString 		"Save this to compare when going back to tiles"
