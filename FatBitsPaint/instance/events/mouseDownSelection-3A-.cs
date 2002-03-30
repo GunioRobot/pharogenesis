3389c3521fb94f78485e781e5d6679536dc0da8a
sw <unknown>
2002-03-30 16:47:00 +0000

    no message

--- FatBitsPaint/instance/events/mouseDownSelection-3A-.cs
+++ FatBitsPaint/instance/events/mouseDownSelection-3A-.cs
@@ -2,4 +2,4 @@ mouseDownSelection: evt
 
         lastMouse _ nil.
         currentSelectionMorph ifNotNil: [currentSelectionMorph delete. currentSelectionMorph _ nil].
-        selectionAnchor _ self griddedPoint: evt
+        selectionAnchor _ self pointGriddedFromEvent: evt
