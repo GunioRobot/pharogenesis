04c666704b35640101d9ac1efe3aa119c1e21b4f
nk <unknown>
2002-04-12 14:06:00 +0000

    no message

--- TwoWayScrollPane/instance/geometry/totalScrollRange.cs
+++ TwoWayScrollPane/instance/geometry/totalScrollRange.cs
@@ -1,5 +1,5 @@
 totalScrollRange
 
 	"Return the entire scrolling range."
-	^ ((scroller submorphBounds ifNil: [^nil]) encompass: 0@0) extent
+	^ ((scroller localVisibleSubmorphBounds ifNil: [^nil]) encompass: 0@0) extent
 
