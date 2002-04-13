f509fe38aca4f661773869ed01da32f38e0ff0e0
nk <unknown>
2002-04-13 13:00:00 +0000

    no message

--- HTTPSocket/class/utilities/retry-3A-asking-3A-ifGiveUp-3A-.cs
+++ HTTPSocket/class/utilities/retry-3A-asking-3A-ifGiveUp-3A-.cs
@@ -1,9 +1,15 @@
 retry: tryBlock asking: troubleString ifGiveUp: abortActionBlock
 	"Execute the given block. If it evaluates to true, return true. If it evaluates to false, prompt the user with the given string to see if he wants to try again. If not, evaluate the abortActionBlock and return false."
 
-	| response |
+	| response  |
 	[tryBlock value] whileFalse: [
-		response _ (PopUpMenu labels: 'Retry\Give Up' withCRs)
-			startUpWithCaption: troubleString.
+		| sema |
+		sema _ Semaphore new.
+		WorldState addDeferredUIMessage: [
+			response _ (PopUpMenu labels: 'Retry\Give Up' withCRs)
+				startUpWithCaption: troubleString.
+			sema signal.
+		].
+		sema wait.
 		response = 2 ifTrue: [abortActionBlock value. ^ false]].
 	^ true
