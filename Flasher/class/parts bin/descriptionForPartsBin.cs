f79ac1623ddab67cce376cd6746a02e421b832de
sw <unknown>
2002-04-17 11:37:00 +0000

    no message

---
+++ Flasher/class/parts bin/descriptionForPartsBin.cs
@@ -0,0 +1,6 @@
+descriptionForPartsBin
+	"Answer a description of the receiver for use in a parts bin"
+
+	^ self partName:	'Flasher'
+		categories:		#('Demo')
+		documentation:	'A circle that flashes'
