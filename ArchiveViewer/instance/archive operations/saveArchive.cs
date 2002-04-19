fbd0dbe81b8828b7d133b007e5daed43eebdc5d9
nk <unknown>
2002-04-19 09:08:00 +0000

    no message

---
+++ ArchiveViewer/instance/archive operations/saveArchive.cs
@@ -0,0 +1,14 @@
+saveArchive
+	| result name |
+	self canSaveArchive ifFalse: [ ^self ].
+	result _ StandardFileMenu newFile.
+	result ifNil: [ ^self ].
+	name _ result directory fullNameFor: result name.
+	(archive canWriteToFileNamed: name)
+		ifFalse: [ self inform: name, ' is used by one or more members
+in your archive, and cannot be overwritten.
+Try writing to another file name'.
+			^self ].
+	[ archive writeToFileNamed: name ] on: Error do: [ :ex | self inform: ex description. ].
+	self setLabel: name asString.
+	self changed: #memberList	"in case CRC's and compressed sizes got set"
