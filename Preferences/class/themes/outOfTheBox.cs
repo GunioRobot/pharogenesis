ddd6cafdfd7a5c3040239d3123da6304b367965f
sw <unknown>
2002-04-21 07:37:00 +0000

    no message

---
+++ Preferences/class/themes/outOfTheBox.cs
@@ -0,0 +1,4 @@
+outOfTheBox
+	"The default out-of-the-box preference settings for Squeak 3.2.  The 'alternative' window-look and scrollbar-look are used.  Button panes are used but not annotation panes.  Scrollbars are on the right and do not flop out."
+
+	self setPreferencesFrom: self defaultValueTableForCurrentRelease
