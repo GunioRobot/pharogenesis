32401a56beeedbf0514e9c9f5c78285ba557e129
tpr <unknown>
2002-04-24 13:53:00 +0000

    no message

--- FilePlugin/instance/file primitives/primitiveFileOpen.cs
+++ FilePlugin/instance/file primitives/primitiveFileOpen.cs
@@ -1,23 +1,16 @@
 primitiveFileOpen
-
-	| writeFlag namePointer filePointer file nameIndex nameSize |
-	self var: 'file' declareC: 'SQFile *file'.
-	self var: 'nameIndex' type:'char *'.
+	| writeFlag namePointer filePointer nameIndex nameSize |
+	self var: 'nameIndex' type: 'char *'.
 	self export: true.
-	writeFlag _ interpreterProxy booleanValueOf: (interpreterProxy stackValue: 0).
+	writeFlag _ interpreterProxy
+				booleanValueOf: (interpreterProxy stackValue: 0).
 	namePointer _ interpreterProxy stackValue: 1.
-	(interpreterProxy isBytes: namePointer) 
-		ifFalse:[^interpreterProxy primitiveFail].
-	filePointer _ interpreterProxy 
-					instantiateClass: (interpreterProxy classByteArray)
-					indexableSize: self fileRecordSize.
-	file _ self fileValueOf: filePointer.
+	(interpreterProxy isBytes: namePointer)
+		ifFalse: [^ interpreterProxy primitiveFail].
 	nameIndex _ interpreterProxy firstIndexableField: namePointer.
 	nameSize _ interpreterProxy byteSizeOf: namePointer.
-	interpreterProxy failed ifFalse:[
-		self cCode: 'sqFileOpen(file, (int)nameIndex, nameSize, writeFlag)'.
-	].
-	interpreterProxy failed ifFalse:[
-		interpreterProxy pop: 3.  "rcvr, name, writeFlag"
-		interpreterProxy push: filePointer.
-	].
+	filePointer _ self fileOpenName: nameIndex size: nameSize write: writeFlag secure: true.
+	interpreterProxy failed
+		ifFalse: [interpreterProxy pop: 3.
+			"rcvr, name, writeFlag"
+			interpreterProxy push: filePointer]
