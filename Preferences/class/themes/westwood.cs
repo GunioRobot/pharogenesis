28b275dbcc01b555936bb5be6d8a3eab8a02cee8
sw <unknown>
2002-05-02 11:03:00 +0000

    no message

---
+++ Preferences/class/themes/westwood.cs
@@ -0,0 +1,15 @@
+westwood
+	"Settings generally similar to those found in a standard browser-plug-in-based Squeak image"
+
+	self setPreferencesFrom: #(
+		(alternativeScrollbarLook true)
+		(alternativeWindowLook true)
+		(classicNavigatorEnabled true)
+		(eToyFriendly true)
+		(haloTransitions true)
+		(honorDesktopCmdKeys false)
+		(magicHalos true)
+		(menuKeyboardControl false)
+		(preserveTrash true)
+		(propertySheetFromHalo true)
+		(unlimitedPaintArea true))
