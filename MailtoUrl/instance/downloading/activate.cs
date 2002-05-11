46a4930c49fcabccc4fcdea2131075c2c2d65d0a
dvf <unknown>
2002-05-11 00:47:00 +0000

    no message

--- MailtoUrl/instance/downloading/activate.cs
+++ MailtoUrl/instance/downloading/activate.cs
@@ -1,6 +1,4 @@
 activate
-	(Smalltalk includesKey: #Celeste)
-		ifFalse: [^ self notify: 'no mail reader present'].
-	^ CelesteComposition
-		openForCeleste: Celeste current
-		initialText: self composeText
+	"Activate a Celeste window for the receiver"
+
+	MailSender sendMessage: (MailMessage from: self composeText)
