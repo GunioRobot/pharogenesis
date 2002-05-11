f39d217a0aca8095617f69fa27cca548a23e6eb7
dvf <unknown>
2002-05-11 01:00:00 +0000

    no message

--- MailtoUrl/instance/downloading/composeText.cs
+++ MailtoUrl/instance/downloading/composeText.cs
@@ -3,13 +3,10 @@ composeText
 
 	^ String streamContents: [:str |
 		str nextPutAll: 'From: '.
-		str nextPutAll: Celeste userName; cr.
+		str nextPutAll: MailSender userName; cr.
 		str nextPutAll: 'To: '.
 		str nextPutAll: locator asString; cr.
 
 		str nextPutAll: 'Subject: '; cr.
 
-		Celeste ccList isEmpty ifFalse: [
-			str nextPutAll: 'Cc: '.
-			str nextPutAll: Celeste ccList; cr].
 		str cr].
