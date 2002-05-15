3afd75dff30d2c9a5e0736b5c67f1f995208a65e
gh <unknown>
2002-05-15 09:55:00 +0000

    no message

--- WriteStream/instance/private/pastEndPut-3A-.cs
+++ WriteStream/instance/private/pastEndPut-3A-.cs
@@ -1,5 +1,12 @@
 pastEndPut: anObject
-	collection _ collection ,
-		(collection class new: ((collection size max: 20) min: 20000)).
+	"Grow the collection by creating a new bigger collection and then
+	copy over the contents from the old one. We grow by doubling the size
+	but the growth is kept between 20 and 1000000.
+	Finally we put <anObject> at the current write position."
+
+	| oldSize grownCollection |
+	oldSize _ collection size.
+	grownCollection _ collection class new: oldSize + ((oldSize max: 20) min: 1000000).
+	collection _ grownCollection replaceFrom: 1 to: oldSize with: collection startingAt: 1.
 	writeLimit _ collection size.
 	collection at: (position _ position + 1) put: anObject
