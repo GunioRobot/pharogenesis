580fc8721bf59bdf697f18d912b87096f4a8d8b4
emm <unknown>
2002-05-30 10:25:00 +0000

    no message

--- Browser/instance/message functions/messageListMenu-3A-shifted-3A-.cs
+++ Browser/instance/message functions/messageListMenu-3A-shifted-3A-.cs
@@ -1,30 +1,34 @@
 messageListMenu: aMenu shifted: shifted
 	"Answer the message-list menu"
+	"Changed by emm to include menu-item for breakpoints"
 
 	shifted ifTrue: [^ self shiftedMessageListMenu: aMenu].
 
 	aMenu addList:#(
-			('browse full (b)' 						browseMethodFull)
-			('browse hierarchy (h)'					classHierarchy)
-			('browse method (O)'					openSingleMessageBrowser)
-			('browse protocol (p)'					browseFullProtocol)
+			('what to show...'			offerWhatToShowMenu)
+                	('toggle break on entry'		toggleBreakOnEntry)
+            		-
+			('browse full (b)' 			browseMethodFull)
+			('browse hierarchy (h)'			classHierarchy)
+			('browse method (O)'			openSingleMessageBrowser)
+			('browse protocol (p)'			browseFullProtocol)
 			-
-			('fileOut'								fileOutMessage)
-			('printOut'								printOutMessage)
+			('fileOut'				fileOutMessage)
+			('printOut'				printOutMessage)
 			-
-			('senders of... (n)'						browseSendersOfMessages)
-			('implementors of... (m)'					browseMessages)
-			('inheritance (i)'						methodHierarchy)
-			('tile scriptor'							openSyntaxView)
-			('versions (v)'							browseVersions)
+			('senders of... (n)'			browseSendersOfMessages)
+			('implementors of... (m)'		browseMessages)
+			('inheritance (i)'			methodHierarchy)
+			('tile scriptor'			openSyntaxView)
+			('versions (v)'				browseVersions)
 			-
-			('inst var refs...'						browseInstVarRefs)
-			('inst var defs...'						browseInstVarDefs)
-			('class var refs...'						browseClassVarRefs)
-			('class variables'						browseClassVariables)
-			('class refs (N)'							browseClassRefs)
+			('inst var refs...'			browseInstVarRefs)
+			('inst var defs...'			browseInstVarDefs)
+			('class var refs...'			browseClassVarRefs)
+			('class variables'			browseClassVariables)
+			('class refs (N)'			browseClassRefs)
 			-
-			('remove method (x)'					removeMessage)
+			('remove method (x)'			removeMessage)
 			-
-			('more...'								shiftedYellowButtonActivity)).
+			('more...'				shiftedYellowButtonActivity)).
 	^ aMenu
