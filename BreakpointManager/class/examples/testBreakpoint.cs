c2eb58f1ca49fbc82698c1fdc57f97241c187ac9
emm <unknown>
2002-05-30 14:12:00 +0000

    no message

---
+++ BreakpointManager/class/examples/testBreakpoint.cs
@@ -0,0 +1,7 @@
+testBreakpoint
+	"In the menu of the methodList, click on -toggle break on entry-
+	and evaluate the following:"
+
+	"BreakpointManager testBreakpoint"
+
+	Transcript cr; show: 'Breakpoint test'
