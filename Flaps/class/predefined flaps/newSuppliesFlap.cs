64707f7124ccbb1f629c19ea9c5174ab89e3a574
sw <unknown>
2002-06-11 14:00:00 +0000

    no message

---
+++ Flaps/class/predefined flaps/newSuppliesFlap.cs
@@ -0,0 +1,4 @@
+newSuppliesFlap
+	"Answer a fully-instantiated flap named 'Supplies' to be placed at the bottom of the screen; this is for the non-plug-in-version"
+
+	^ self newSuppliesFlapFromQuads: self quadsDefiningSuppliesFlap positioning: #right
