abec45939e3591112bc6f46a2ae3d4139b3d1a8d
svp <unknown>
2002-06-18 14:51:00 +0000

    no message

---
+++ VersionNumber/instance/accessing/numbers.cs
@@ -0,0 +1,5 @@
+numbers
+	"Answer a copy (to discourage people from directly changing a version number).
+	VersionNumbers should never change, instead, instantiate a new instance."
+
+	^numbers copy
