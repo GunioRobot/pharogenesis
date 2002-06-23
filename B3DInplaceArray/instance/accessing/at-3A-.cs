ade492d396cbf6ec6a309ed12cfc38ef82838ffd
ar <unknown>
2002-06-23 01:16:00 +0000

    no message

--- B3DInplaceArray/instance/accessing/at-3A-.cs
+++ B3DInplaceArray/instance/accessing/at-3A-.cs
@@ -1,7 +1,6 @@
 at: index
 	"Return the primitive vertex at the given index"
 	| vtx |
-	(index < 1 or:[index > self size]) ifTrue:[^self errorSubscriptBounds: index].
 	vtx _ self contentsClass new.
 	vtx replaceFrom: 1 to: vtx size with: self startingAt: index - 1 * self contentsSize + 1.
 	^vtx
