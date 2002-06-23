f6ec8cc81c71b4f25a1085997b51be50816816a7
ar <unknown>
2002-06-23 01:16:00 +0000

    no message

--- B3DInplaceArray/instance/accessing/at-3A-put-3A-.cs
+++ B3DInplaceArray/instance/accessing/at-3A-put-3A-.cs
@@ -1,7 +1,6 @@
 at: index put: anObject
 	"Store the object at the given index in the receiver"
 	| idx |
-	(index < 1 or:[index > self size]) ifTrue:[^self errorSubscriptBounds: index].
 	idx _ index - 1 * self contentsSize.
 	self privateReplaceFrom: idx+1 to: idx + self contentsSize with: anObject startingAt: 1.
 	^anObject
