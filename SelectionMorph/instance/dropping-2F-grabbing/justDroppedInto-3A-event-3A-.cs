5b63455657a317abb04eb212046d64da233b4acf
nk <unknown>
2002-06-27 10:11:00 +0000

    no message

---
+++ SelectionMorph/instance/dropping-2F-grabbing/justDroppedInto-3A-event-3A-.cs
@@ -0,0 +1,14 @@
+justDroppedInto: newOwner event: evt
+
+	selectedItems isEmpty ifTrue:
+		["Hand just clicked down to draw out a new selection"
+		^ self extendByHand: evt hand].
+	dupLoc ifNotNil: [dupDelta _ self position - dupLoc].
+	selectedItems reverseDo: [:m | 
+		WorldState addDeferredUIMessage:
+			[m referencePosition: (newOwner localPointToGlobal: m referencePosition).
+			newOwner handleDropMorph:
+				(DropEvent new setPosition: evt cursorPoint contents: m hand: evt hand)] fixTemps].
+	selectedItems _ nil.
+	self removeHalo; delete.
+	evt wasHandled: true
