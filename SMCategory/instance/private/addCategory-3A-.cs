e971c0f5a6ad7e94901211f80d331a12a9ddaa93
gh <unknown>
2002-06-28 12:46:00 +0000

    no message

---
+++ SMCategory/instance/private/addCategory-3A-.cs
@@ -0,0 +1,9 @@
+addCategory: cat
+	"Add a category as a subcategory to self.
+	The collection of subcategories is lazily instantiated."
+
+	subCategories ifNil: [subCategories _ OrderedCollection new].
+	cat parent ifNotNil: [cat parent removeCategory: cat ].
+	subCategories add: cat.
+	cat parent: self.
+	^cat
