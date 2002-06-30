cab8a27eba2f21117dad13434534ca0aa3227645
ka <unknown>
2002-06-30 13:53:00 +0000

    no message

---
+++ Preferences/class/themes/keihanna.cs
@@ -0,0 +1,22 @@
+keihanna
+	"Settings more similar to those found in a standard browser-plug-in-based Squeak image than westwood"
+
+	self setPreferencesFrom: #(
+		(alternativeScrollbarLook true)
+		(alternativeWindowLook true)
+		(classicNavigatorEnabled true)
+		(eToyFriendly true)
+		(haloTransitions true)
+		(honorDesktopCmdKeys false)
+		(includeSoundControlInNavigator true)
+		(magicHalos true)
+		(menuKeyboardControl false)
+		(mouseOverHalos true)
+		(preserveTrash true)
+		(projectViewsInWindows false)
+		(propertySheetFromHalo true)
+		(showDirectionHandles true)
+		(soundStopWhenDone true)
+		(unlimitedPaintArea true)
+		(uniqueNamesInHalos true)
+		(uniTilesClassic false))
