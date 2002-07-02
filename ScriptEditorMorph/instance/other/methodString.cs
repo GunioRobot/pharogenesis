27e3767d0027b43d2328f909dcd9d8f25e6b44e1
sw <unknown>
2002-07-02 14:07:00 +0000

    no message

--- ScriptEditorMorph/instance/other/methodString.cs
+++ ScriptEditorMorph/instance/other/methodString.cs
@@ -1,5 +1,10 @@
 methodString
+	"Answer the source-code string for the receiver.  This is for use by classic tiles, but is also used in universal tiles to formulate an initial method declaration for a nascent user-defined script; in universalTiles mode, the codeString (at present anyway) is empty -- the actual code derives from the SyntaxMorph in that case"
+
 	^ String streamContents:
 		[:aStream |
-			aStream nextPutAll: scriptName; cr; cr; tab.
+			aStream nextPutAll: scriptName.
+			scriptName numArgs > 0 ifTrue:
+				[aStream nextPutAll: ' parameter'].
+			aStream cr; cr; tab.
 			aStream nextPutAll: self codeString]
