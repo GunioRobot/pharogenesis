913f03b4d1c6908e9719578576febae028d44152
sw <unknown>
2002-07-04 00:22:00 +0000

    no message

--- WatchMorph/instance/drawing/drawOn-3A-.cs
+++ WatchMorph/instance/drawing/drawOn-3A-.cs
@@ -1,19 +1,25 @@
 drawOn: aCanvas
-	| pHour pMin pSec time |
+	"Draw the watch on the given canvas"
+
+	| pHour pMin pSec time centerColor |
 	time _ Time now.
 	pHour _ self radius: 0.6 hourAngle: time hours + (time minutes/60.0).
 	pMin _ self radius: 0.72 hourAngle: (time minutes / 5.0).
 	pSec _ self radius: 0.8 hourAngle: (time seconds / 5.0).
-	time hours < 12
-		ifTrue: [self centerColor: Color veryLightGray]
-		ifFalse: [self centerColor: Color darkGray].
+	centerColor _ cColor
+		ifNil:
+			[Color transparent]
+		ifNotNil:
+			[time hours < 12
+				ifTrue: [cColor muchLighter]
+				ifFalse: [cColor]].
 
-	antialias ifTrue: [
-		aCanvas asBalloonCanvas
+	antialias ifTrue:
+		[aCanvas asBalloonCanvas
 			aaLevel: 4;
 			drawOval: (bounds insetBy: borderWidth // 2 + 1) color: self fillStyle
 				borderWidth: borderWidth borderColor: borderColor;
-			drawOval: (bounds insetBy: self extent*0.35) color: cColor
+			drawOval: (bounds insetBy: self extent*0.35) color: centerColor
 				borderWidth: 0 borderColor: Color black;
 			drawPolygon: {self center. pHour}
 				color: Color transparent borderWidth: 3 borderColor: handsColor;
@@ -21,10 +27,10 @@ drawOn: aCanvas
 				color: Color transparent borderWidth: 2 borderColor: handsColor;
 			drawPolygon: {self center. pSec}
 				color: Color transparent borderWidth: 1 borderColor: handsColor]
-		ifFalse: [
-			super drawOn: aCanvas.
+		ifFalse:
+			[super drawOn: aCanvas.
 			aCanvas
-				fillOval: (bounds insetBy: self extent*0.35) color: cColor;
+				fillOval: (bounds insetBy: self extent*0.35) color: centerColor;
 				line: self center to: pHour width: 3 color: handsColor;
 				line: self center to: pMin width: 2 color: handsColor;
-				line: self center to: pSec width: 1 color: handsColor.]
+				line: self center to: pSec width: 1 color: handsColor]
