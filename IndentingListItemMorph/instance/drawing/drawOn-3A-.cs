f056c6c31a15dbb9a4ba63fb672139590bebade2
nk <unknown>
2002-07-10 11:53:00 +0000

    no message

--- IndentingListItemMorph/instance/drawing/drawOn-3A-.cs
+++ IndentingListItemMorph/instance/drawing/drawOn-3A-.cs
@@ -3,11 +3,11 @@ drawOn: aCanvas
 	| tRect sRect columnRect columnScanner columnData columnLeft colorToUse |
 
 	tRect := self toggleRectangle.
-	sRect := bounds withLeft: tRect right + 3.
+	sRect := bounds withLeft: tRect right + 4.
 	self drawToggleOn: aCanvas in: tRect.
 	colorToUse _ complexContents preferredColor ifNil: [color].
 	(container columns isNil or: [(contents asString indexOf: Character tab) = 0]) ifTrue: [
-		aCanvas text: contents asString bounds: sRect font: font color: colorToUse.
+		aCanvas drawString: contents asString in: sRect font: self fontToUse color: colorToUse.
 	] ifFalse: [
 		columnLeft _ sRect left.
 		columnScanner _ ReadStream on: contents asString.
@@ -15,7 +15,7 @@ drawOn: aCanvas
 			columnRect _ columnLeft @ sRect top extent: width @ sRect height.
 			columnData _ columnScanner upTo: Character tab.
 			columnData isEmpty ifFalse: [
-				aCanvas text: columnData bounds: columnRect font: font color: colorToUse.
+				aCanvas drawString: columnData in: columnRect font: self fontToUse color: colorToUse.
 			].
 			columnLeft _ columnRect right + 5.
 		].
