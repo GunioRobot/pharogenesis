da58871a980c256ee7fcf12e9f6a4b4042b6f823
sw <unknown>
2002-07-17 11:37:00 +0000

    no message

--- ChangeSorter/instance/changeSet menu/changeSetListKey-3A-from-3A-.cs
+++ ChangeSorter/instance/changeSet menu/changeSetListKey-3A-from-3A-.cs
@@ -2,14 +2,17 @@ changeSetListKey: aChar from: view
 	"Respond to a Command key.  I am a model with a listView that has a list of changeSets."
 
 	aChar == $b ifTrue: [^ self browseChangeSet].
+	aChar == $B ifTrue: [^ self openChangeSetBrowser].
+	aChar == $c ifTrue: [^ self copyAllToOther].
+	aChar == $D ifTrue: [^ self toggleDiffing]. 
 	aChar == $f ifTrue: [^ self findCngSet].
 	aChar == $m ifTrue: [^ self newCurrent].
 	aChar == $n ifTrue: [^ self newSet].
 	aChar == $o ifTrue: [^ self fileOut].
-	aChar == $r ifTrue: [^ self rename].
-	aChar == $x ifTrue: [^ self remove].
 	aChar == $p ifTrue: [^ self addPreamble].
-	aChar == $c ifTrue: [^ self copyAllToOther].
-	aChar == $D ifTrue: [^ self toggleDiffing]. 
+	aChar == $r ifTrue: [^ self rename].
+	aChar == $s ifTrue: [^ self chooseChangeSetCategory].
+	aChar == $x ifTrue: [^ self remove].
+	aChar == $- ifTrue: [^ self subtractOtherSide].
 
 	^ self messageListKey: aChar from: view
