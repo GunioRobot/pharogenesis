41a6e871f92f5d8eef5a75aa35340e41dafae919
sw <unknown>
2002-07-20 14:27:00 +0000

    no message

--- UniclassScript/instance/script editor/instantiatedScriptEditorForPlayer-3A-.cs
+++ UniclassScript/instance/script editor/instantiatedScriptEditorForPlayer-3A-.cs
@@ -2,14 +2,16 @@ instantiatedScriptEditorForPlayer: aPlayer
 	"Return the current script editor, creating it if necessary"
 
 	currentScriptEditor ifNil:
-		[currentScriptEditor _ (playerClass includesSelector: selector) 
-			ifTrue: [ScriptEditorMorph new 
-				fromExistingMethod: selector 
-				forPlayer: aPlayer]
-			ifFalse: [ScriptEditorMorph new
-				setMorph: aPlayer costume
-				scriptName: selector].
-		defaultStatus == #ticking ifTrue:
+		[currentScriptEditor _ (self playerClass includesSelector: selector) 
+			ifTrue:
+				[Preferences universalTiles
+					ifFalse:
+						[self error: 'duplicate selector'].
+				ScriptEditorMorph new fromExistingMethod: selector forPlayer: aPlayer]
+			ifFalse:
+				[ScriptEditorMorph new setMorph: aPlayer costume scriptName: selector].
+
+		(defaultStatus == #ticking and: [selector numArgs == 0]) ifTrue:
 			[aPlayer costume arrangeToStartStepping]].
 	
 	^ currentScriptEditor
