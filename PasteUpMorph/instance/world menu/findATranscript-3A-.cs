2d4991a5d93d1c75c675970cb2611e3f8e5e2296
sw <unknown>
2002-07-22 08:54:00 +0000

    no message

--- PasteUpMorph/instance/world menu/findATranscript-3A-.cs
+++ PasteUpMorph/instance/world menu/findATranscript-3A-.cs
@@ -1,13 +1,5 @@
 findATranscript: evt
 	"Locate a transcript, open it, and bring it to the front.  Create one if necessary"
 
-	| aWindow |
-	submorphs do:
-		[:aMorph | (((aWindow _ aMorph renderedMorph) isKindOf: SystemWindow) and:
-			[aWindow model == Transcript])
-				ifTrue:
-					[aWindow isCollapsed ifTrue: [aWindow expand].
-					aWindow activateAndForceLabelToShow.
-					^ self]].
-	"None found, so create one"
-	(Transcript openAsMorphLabel: 'Transcript') openInWorld: evt hand world
+	self findAWindowSatisfying:
+		[:aWindow | aWindow model == Transcript] orMakeOneUsing: [Transcript openAsMorphLabel: 'Transcript']
