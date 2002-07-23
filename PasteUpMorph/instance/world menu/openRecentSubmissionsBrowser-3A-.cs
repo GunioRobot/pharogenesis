31a9b9545c1b9e5091091cc23924bbd14976e0a1
sw <unknown>
2002-07-23 14:01:00 +0000

    no message

--- PasteUpMorph/instance/world menu/openRecentSubmissionsBrowser-3A-.cs
+++ PasteUpMorph/instance/world menu/openRecentSubmissionsBrowser-3A-.cs
@@ -1,13 +1,5 @@
 openRecentSubmissionsBrowser: evt
 	"Locate a recent-submissions browser, open it, and bring it to the front.  Create one if necessary.  Only works in morphic"
 
-	| aWindow |
-	submorphs do:
-		[:aMorph | (((aWindow _ aMorph renderedMorph) isKindOf: SystemWindow) and:
-			[aWindow model isKindOf: RecentMessageSet])
-				ifTrue:
-					[aWindow isCollapsed ifTrue: [aWindow expand].
-					aWindow activateAndForceLabelToShow.
-					^ self]].
-	"None found, so create one"
-	Utilities browseRecentSubmissions
+	self findAWindowSatisfying:
+		[:aWindow | aWindow model isKindOf: RecentMessageSet] orMakeOneUsing: [Utilities recentSubmissionsWindow]
