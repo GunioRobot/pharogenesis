0f57d295cbeacba4018d0cc3bc94ce821a884da8
sw <unknown>
2002-07-29 02:12:00 +0000

    no message

--- Utilities/class/recent method submissions/mostRecentlySubmittedMessage.cs
+++ Utilities/class/recent method submissions/mostRecentlySubmittedMessage.cs
@@ -1,2 +1,7 @@
 mostRecentlySubmittedMessage
-	^ RecentSubmissions last
+	"Answer a string indicating the most recently submitted method that is still extant"
+
+	self flag: #mref.	"fix for faster references to methods"
+
+	self assureMostRecentSubmissionExists.
+	^ RecentSubmissions last asStringOrText asString
