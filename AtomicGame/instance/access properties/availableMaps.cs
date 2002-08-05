0a44dfdf84f0d64adfd9c33e01597ef700347e7a
grp <unknown>
2002-08-05 13:29:00 +0000

    no message

---
+++ AtomicGame/instance/access properties/availableMaps.cs
@@ -0,0 +1,33 @@
+availableMaps
+	| maps |
+	maps _ OrderedCollection new.
+	maps add: AtomicMap01;
+		 add: AtomicMap02;
+		 add: AtomicMap03;
+		 add: AtomicMap04;
+		 add: AtomicMap05;
+		 add: AtomicMap06;
+		 add: AtomicMap07;
+		 add: AtomicMap08;
+		 add: AtomicMap09;
+		 add: AtomicMap10.
+	maps add: AtomicMap11;
+		 add: AtomicMap12;
+		 add: AtomicMap13;
+		 add: AtomicMap14;
+		 add: AtomicMap15;
+		 add: AtomicMap16;
+		 add: AtomicMap17;
+		 add: AtomicMap18;
+		 add: AtomicMap19;
+		 add: AtomicMap20.
+	maps add: AtomicMap21;
+		 add: AtomicMap22;
+		 add: AtomicMap23;
+		 add: AtomicMap24;
+		 add: AtomicMap25;
+		 add: AtomicMap26;
+		 add: AtomicMap27;
+		 add: AtomicMap28;
+		 add: AtomicMap29; add: AtomicMap30.
+	^ maps
