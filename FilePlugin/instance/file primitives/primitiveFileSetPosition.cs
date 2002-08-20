de3f9dbf4db112e6bb03259e2314682ea7a83e29
ajh <unknown>
2002-08-20 02:05:00 +0000

    no message

--- FilePlugin/instance/file primitives/primitiveFileSetPosition.cs
+++ FilePlugin/instance/file primitives/primitiveFileSetPosition.cs
@@ -1,8 +1,13 @@
 primitiveFileSetPosition
-	| newPosition file |
+	| newPosition file sz |
 	self var: 'file' declareC: 'SQFile *file'.
+	self var: 'newPosition' type: 'squeakFileOffsetType'.
 	self export: true.
-	newPosition _ interpreterProxy positive32BitValueOf: (interpreterProxy stackValue: 0).
+	(interpreterProxy isIntegerObject: (interpreterProxy stackValue: 0)) ifFalse:
+		[sz _ self cCode: 'sizeof(squeakFileOffsetType)'.
+		(interpreterProxy byteSizeOf: (interpreterProxy stackValue: 0)) > sz 
+			ifTrue: [^interpreterProxy primitiveFail]].
+	newPosition _ interpreterProxy positive64BitValueOf: (interpreterProxy stackValue: 0).
 	file _ self fileValueOf: (interpreterProxy stackValue: 1).
 	interpreterProxy failed ifFalse:[
 		self sqFile: file SetPosition: newPosition ].
