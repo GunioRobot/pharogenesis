3c6f9fee7ddb18b2c8ceb28c05979510798dc99e
ajh <unknown>
2002-08-20 00:30:00 +0000

    no message

---
+++ CArray/instance/private/interpreter-3A-address-3A-unitSize-3A-.cs
@@ -0,0 +1,6 @@
+interpreter: interpreterSimulator address: arrayAddress unitSize: numBytes
+
+	interpreter _ interpreterSimulator.
+	arrayBaseAddress _ arrayAddress.
+	unitSize _ numBytes.
+	ptrOffset _ 0.
