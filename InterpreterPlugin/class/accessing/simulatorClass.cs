4a0121453a694e46daa155980f29df0540f30732
ajh <unknown>
2002-08-21 21:43:00 +0000

    no message

--- InterpreterPlugin/class/accessing/simulatorClass.cs
+++ InterpreterPlugin/class/accessing/simulatorClass.cs
@@ -1,5 +1,4 @@
 simulatorClass
-	"For running from Smalltalk - answer a class that can be used to 
-	simulate the receiver. By default every plugin can simulate itself."
+	"For running from Smalltalk - answer a class that can be used to simulate the receiver, or nil if you want the primitives in this module to always fail, causing simulation to fall through to the Smalltalk code.  By default every non-TestInterpreterPlugin can simulate itself."
 
 	^ self
