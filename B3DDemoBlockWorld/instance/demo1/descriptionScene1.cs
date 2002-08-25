f2d6339fca09dbd5543adf5f4a7d9301a35f7a71
BG <unknown>
2002-08-25 17:52:00 +0000

    no message

---
+++ B3DDemoBlockWorld/instance/demo1/descriptionScene1.cs
@@ -0,0 +1,3 @@
+descriptionScene1
+
+   ^'This example displays three colored blocks of different height in green ambient light. Note that the edges are not visible in ambient light. The reason is that all surfaces are illuminated with light of the same intensity. '
