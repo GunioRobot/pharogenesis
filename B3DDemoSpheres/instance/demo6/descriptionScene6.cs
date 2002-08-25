8672eecbf031b049ee79ed29d0202761cacdc53e
BG <unknown>
2002-08-25 14:23:00 +0000

    no message

---
+++ B3DDemoSpheres/instance/demo6/descriptionScene6.cs
@@ -0,0 +1,3 @@
+descriptionScene6
+
+  ^'Two white spheres without texture are shown in the light of three directed lights of different color. This example shows the use of scaling and translation to create a scene.'
