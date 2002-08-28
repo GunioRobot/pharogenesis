16e7343325c1666397fed9b411cde2d9b4240f4f
yo <unknown>
2002-08-28 14:46:00 +0000

    no message

---
+++ MultiString/instance/converting/asFourCode.cs
@@ -0,0 +1,9 @@
+asFourCode
+
+	| result |
+	self size = 1 ifFalse: [^self error: 'must be exactly four octets'].
+	result _ self basicAt: 1.
+	(result bitAnd: 16r80000000) = 0 
+		ifFalse: [self error: 'cannot resolve fourcode'].
+	(result bitAnd: 16r40000000) = 0 ifFalse: [^result - 16r80000000].
+	^ result
