6d336d281035bbcc7c7fa1980ed699dcfc79de7d
yo <unknown>
2002-08-28 17:33:00 +0000

    no message

--- Scanner/instance/multi-2D-character scans/xLetter.cs
+++ Scanner/instance/multi-2D-character scans/xLetter.cs
@@ -1,23 +1,24 @@
 xLetter
 	"Form a word or keyword."
 
-	| type |
+	| type c |
 	buffer reset.
-	[(type _ typeTable at: hereChar asciiValue) == #xLetter or: [type == #xDigit]]
-		whileTrue:
-			["open code step for speed"
+	[c _ hereChar asciiValue.
+	(type _ typeTable at: c ifAbsent: [#xLetter]) == #xLetter or: [type == #xDigit]]
+		whileTrue: ["open code step for speed"
 			buffer nextPut: hereChar.
 			hereChar _ aheadChar.
 			source atEnd
-				ifTrue: [aheadChar _ 30 asCharacter "doit"]
+				ifTrue: [aheadChar _ 30 asCharacter
+					"doit"]
 				ifFalse: [aheadChar _ source next]].
 	(type == #colon or: [type == #xColon and: [aheadChar ~= $=]])
-		ifTrue: 
-			[buffer nextPut: self step.
+		ifTrue: [buffer nextPut: self step.
 			["Allow any number of embedded colons in literal symbols"
-			(typeTable at: hereChar asciiValue) == #xColon]
+			(typeTable at: hereChar asciiValue ifAbsent: [#xLetter])
+				== #xColon]
 				whileTrue: [buffer nextPut: self step].
 			tokenType _ #keyword]
-		ifFalse: 
-			[tokenType _ #word].
-	token _ buffer contents
+		ifFalse: [tokenType _ #word].
+	token _ buffer contents.
+	token isOctetString ifTrue: [token _ token asOctetString].
