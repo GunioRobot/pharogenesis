e433104dbd75ef808155d1ddb7419d8b7d3c41e8
yo <unknown>
2002-08-28 16:42:00 +0000

    no message

--- String/instance/accessing/at-3A-put-3A-.cs
+++ String/instance/accessing/at-3A-put-3A-.cs
@@ -1,13 +1,20 @@
-at: index put: aCharacter 
+at: index put: aCharacter
 	"Primitive. Store the Character in the field of the receiver indicated by
 	the index. Fail if the index is not an Integer or is out of bounds, or if
 	the argument is not a Character. Essential. See Object documentation
 	whatIsAPrimitive."
 
 	<primitive: 64>
-	(aCharacter isKindOf: Character)
-		ifTrue: [	
-			index isInteger
-				ifTrue: [self errorSubscriptBounds: index]
-				ifFalse: [self errorNonIntegerIndex]]
-		ifFalse: [self error: 'Strings only store Characters']
+	(aCharacter isMemberOf: Character) ifTrue: [	
+		index isInteger
+			ifTrue: [self errorSubscriptBounds: index]
+			ifFalse: [self errorNonIntegerIndex]
+	] ifFalse: [
+		(aCharacter isMemberOf: MultiCharacter) 
+			ifTrue: [
+				self becomeForward: (MultiString from: self).
+				self at: index put: aCharacter.
+			] ifFalse: [	
+				self error: 'Strings only store Characters'
+			]
+	].
