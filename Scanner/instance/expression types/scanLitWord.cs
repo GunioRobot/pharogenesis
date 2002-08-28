7d3e88e867ce32fa8ff056d3d2971085036dfa68
yo <unknown>
2002-08-28 17:52:00 +0000

    no message

--- Scanner/instance/expression types/scanLitWord.cs
+++ Scanner/instance/expression types/scanLitWord.cs
@@ -2,9 +2,9 @@ scanLitWord
 	"Accumulate keywords and asSymbol the result."
 
 	| t |
-	[(typeTable at: hereChar asciiValue) = #xLetter]
-		whileTrue: 
-			[t _ token.
-			self xLetter.
-			token _ t , token].
-	token _ token asSymbol
+	[(typeTable at: hereChar asciiValue ifAbsent: [#xLetter]) = #xLetter] whileTrue: [
+		t _ token.
+		self xLetter.
+		token _ t , token
+	].
+	token _ token asSymbol.
