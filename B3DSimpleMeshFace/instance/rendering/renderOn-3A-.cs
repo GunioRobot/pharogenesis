9e3e66dc7fc1745e61ad965ca49caeef4c1ead0d
BG <unknown>
2002-08-28 22:06:00 +0000

    no message

--- B3DSimpleMeshFace/instance/rendering/renderOn-3A-.cs
+++ B3DSimpleMeshFace/instance/rendering/renderOn-3A-.cs
@@ -1,5 +1,6 @@
 renderOn: aRenderer
-	^aRenderer drawPolygonAfter:[
+	^aRenderer trackEmissionColor: true;		"Turn on pre-lit colors"
+			drawPolygonAfter:[
 		aRenderer normal: self normal.
 		1 to: self size do:[:i| (self at: i) renderOn: aRenderer].
 	].
