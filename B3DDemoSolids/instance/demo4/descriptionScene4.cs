a8a46417246ace2bd6da38aff708c74b1a4db49d
BG <unknown>
2002-08-29 15:18:00 +0000

    no message

---
+++ B3DDemoSolids/instance/demo4/descriptionScene4.cs
@@ -0,0 +1,7 @@
+descriptionScene4
+
+  ^'Two dodecahedra with colored faces.
+
+ No lights, no texture and no materials are needed to show that solid with colored faces.
+
+The scene was build with geometric transformations. Note that B3DSimpleMesh does not implement in place transformation: The method #transformBy:  returns a new instance.'
