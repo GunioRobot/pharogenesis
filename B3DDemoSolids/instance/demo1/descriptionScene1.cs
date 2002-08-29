0b0906e2b008cdd0fb04b93ec5811af8bbd32d2d
BG <unknown>
2002-08-29 09:20:00 +0000

    no message

---
+++ B3DDemoSolids/instance/demo1/descriptionScene1.cs
@@ -0,0 +1,3 @@
+descriptionScene1
+
+  ^'A dodecahedron with a reflecting white surface in the light of three colored directed lights. The pentangles are represented by instances of B3DSimpleMeshFace. An instance of B3DSimpleMesh is used to put the faces together to form a solid.'
