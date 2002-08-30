8c01e7166b10a0877f45c4b9998591abea961c15
acg <unknown>
2002-08-30 17:31:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveFloatLessOrEqual.cs
+++ Interpreter/instance/float primitives/primitiveFloatLessOrEqual.cs
@@ -1,4 +1,4 @@
 primitiveFloatLessOrEqual
-	| bool |
-	bool _ self primitiveFloatGreater: (self stackValue: 1) thanArg: self stackTop.
-	successFlag ifTrue: [self pop: 2. self pushBool: bool not].
+	| aBool |
+	aBool _ self primitiveFloatGreater: (self stackValue: 1) thanArg: self stackTop.
+	successFlag ifTrue: [self pop: 2. self pushBool: aBool not].
