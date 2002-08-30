76c9e84e443eae72f015b18f1dc16c9521a4fb30
acg <unknown>
2002-08-30 17:31:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveFloatNotEqual.cs
+++ Interpreter/instance/float primitives/primitiveFloatNotEqual.cs
@@ -1,4 +1,4 @@
 primitiveFloatNotEqual
-	| bool |
-	bool _ self primitiveFloatEqual: (self stackValue: 1) toArg: self stackTop.
-	successFlag ifTrue: [self pop: 2. self pushBool: bool not].
+	| aBool |
+	aBool _ self primitiveFloatEqual: (self stackValue: 1) toArg: self stackTop.
+	successFlag ifTrue: [self pop: 2. self pushBool: aBool not].
