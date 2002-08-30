bc42a75550bbf501ff3769ffe1b2001c1ee3bcfb
acg <unknown>
2002-08-30 17:30:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveFloatEqual.cs
+++ Interpreter/instance/float primitives/primitiveFloatEqual.cs
@@ -1,4 +1,4 @@
 primitiveFloatEqual
-	| bool |
-	bool _ self primitiveFloatEqual: (self stackValue: 1) toArg: self stackTop.
-	successFlag ifTrue: [self pop: 2. self pushBool: bool].
+	| aBool |
+	aBool _ self primitiveFloatEqual: (self stackValue: 1) toArg: self stackTop.
+	successFlag ifTrue: [self pop: 2. self pushBool: aBool].
