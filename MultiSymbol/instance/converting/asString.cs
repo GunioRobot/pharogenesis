3a0a8c2c50643a6580c6b54bd134d528c9984f37
yo <unknown>
2002-08-30 14:14:00 +0000

    no message

---
+++ MultiSymbol/instance/converting/asString.cs
@@ -0,0 +1,7 @@
+asString 
+	"Refer to the comment in String|asString."
+
+	| newString |
+	newString _ String new: self size.
+	1 to: self size do: [:index | newString at: index put: (self at: index)].
+	^ newString
