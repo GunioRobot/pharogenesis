7584c59ca6fe1dea8b5a19a1693f32e691add49d
acg <unknown>
2002-08-30 17:31:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveFloatGreaterThan.cs
+++ Interpreter/instance/float primitives/primitiveFloatGreaterThan.cs
@@ -1,4 +1,4 @@
 primitiveFloatGreaterThan
-	| bool |
-	bool _ self primitiveFloatGreater: (self stackValue: 1) thanArg: self stackTop.
-	successFlag ifTrue: [self pop: 2. self pushBool: bool].
+	| aBool |
+	aBool _ self primitiveFloatGreater: (self stackValue: 1) thanArg: self stackTop.
+	successFlag ifTrue: [self pop: 2. self pushBool: aBool].
