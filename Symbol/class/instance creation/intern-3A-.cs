a38b671f48afea2c33fac77aad2fb4d1c586d30f
yo <unknown>
2002-08-30 14:52:00 +0000

    no message

--- Symbol/class/instance creation/intern-3A-.cs
+++ Symbol/class/instance creation/intern-3A-.cs
@@ -1,9 +1,11 @@
-intern: aStringOrSymbol
-	^(SymbolTable like: aStringOrSymbol)
-		ifNil:
-			[
-				SymbolTable add:
-					((aStringOrSymbol isKindOf: Symbol)
+intern: aStringOrSymbol 
+
+	(aStringOrSymbol class == MultiString
+			and: [aStringOrSymbol isOctetString not])
+		ifTrue: [^ MultiSymbol intern: aStringOrSymbol].
+	^ (self lookup: aStringOrSymbol)
+		ifNil: [NewSymbols
+				add: ((aStringOrSymbol isKindOf: Symbol)
 						ifTrue: [aStringOrSymbol]
-						ifFalse: [(self new: aStringOrSymbol size) string: aStringOrSymbol])
-			]
+						ifFalse: [(self new: aStringOrSymbol size)
+								string: aStringOrSymbol])].
