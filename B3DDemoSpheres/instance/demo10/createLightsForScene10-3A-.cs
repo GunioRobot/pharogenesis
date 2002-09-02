519a5305e0a8ff0936f916065311d931e34d2ee9
BG <unknown>
2002-09-02 18:36:00 +0000

    no message

---
+++ B3DDemoSpheres/instance/demo10/createLightsForScene10-3A-.cs
@@ -0,0 +1,8 @@
+createLightsForScene10: aScene
+
+	| ambientLight |
+
+     ambientLight :=B3DAmbientLight new.	
+	ambientLight lightColor: (B3DMaterialColor color: Color green).
+	
+	scene lights add: ambientLight.
