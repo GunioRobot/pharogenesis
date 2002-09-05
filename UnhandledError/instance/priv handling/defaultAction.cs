92b5f0b758ed226641f3875697daf50bdd99d36e
ajh <unknown>
2002-09-05 15:11:00 +0000

    no message

---
+++ UnhandledError/instance/priv handling/defaultAction.cs
@@ -0,0 +1,6 @@
+defaultAction
+	"The current computation is terminated. The cause of the error should be logged or reported to the user. If the program is operating in an interactive debugging environment the computation should be suspended and the debugger activated."
+
+	self isDevelopmentEnvironmentPresent
+		ifTrue: [self devDefaultAction]
+		ifFalse: [self runtimeDefaultAction].
