7c15ec9e5bcbda84a4f423a6abc683566a47466a
sw <unknown>
2002-09-06 13:11:00 +0000

    no message

--- Player/instance/misc/touchesA-3A-.cs
+++ Player/instance/misc/touchesA-3A-.cs
@@ -1,18 +1,16 @@
 touchesA: aPrototypicalPlayer
+	"Answer whether the receiver overlaps any player who wears a Sketch costume and who is of the same class as the prototypicalPlayer and who is wearing the same bitmap, but who is *not that player itself*!  This is an extreme case of a function highly customized (by Bob Arning) to suit a single, idiosycratic, and narrow demo need of Alan's.  Consult:
+http://groups.yahoo.com/group/squeak/message/40560"
 
-	| envelope myBounds trueNeighbor trueGoal trueSelf |
-
-	aPrototypicalPlayer ifNil: [^false].
-	envelope _ costume owner ifNil: [^false].
-	myBounds _ costume bounds.
+	| envelope trueNeighbor trueGoal trueSelf itsPlayer |
+	aPrototypicalPlayer ifNil: [^ false].
+	envelope _ costume owner ifNil: [^ false].
 	trueSelf _ costume renderedMorph.
 	trueGoal _ aPrototypicalPlayer costume renderedMorph.
-	envelope submorphs do: [ :each |
+	envelope submorphs do: [:each |
 		trueNeighbor _ each renderedMorph.
-		(trueNeighbor == trueGoal or: [trueNeighbor == trueSelf]) ifFalse: [
-			(myBounds intersects: each bounds) ifTrue: [
-				(trueGoal appearsToBeSameCostumeAs: trueNeighbor) ifTrue: [ ^true].
-			].
-		].
-	].
-	^false
+		(trueNeighbor == trueGoal or: [trueNeighbor == trueSelf]) ifFalse:
+			[(itsPlayer _ each player) ifNotNil:
+				[(itsPlayer overlaps: self) ifTrue:
+					[(trueGoal appearsToBeSameCostumeAs: trueNeighbor) ifTrue: [^ true]]]]].
+	^ false
