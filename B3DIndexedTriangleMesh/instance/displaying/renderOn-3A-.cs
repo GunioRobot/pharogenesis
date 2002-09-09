40cbd771f1b517d80d605b8e1e29740bff60a6ef
ar <unknown>
2002-09-09 15:10:00 +0000

    no message

--- B3DIndexedTriangleMesh/instance/displaying/renderOn-3A-.cs
+++ B3DIndexedTriangleMesh/instance/displaying/renderOn-3A-.cs
@@ -2,9 +2,4 @@ renderOn: aRenderer
 	self hasVertexColors ifTrue:[
 		aRenderer trackAmbientColor: true.
 		aRenderer trackDiffuseColor: true].
-	^aRenderer
-		drawIndexedTriangles: faces
-			vertices: vertices
-			normals: vtxNormals
-			colors: vtxColors
-			texCoords: vtxTexCoords.
+	^aRenderer drawIndexedTriangleMesh: self
