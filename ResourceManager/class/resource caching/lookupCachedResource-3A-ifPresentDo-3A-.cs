5ea5d0c7b064e5b3d879e784cb550c3953c8edce
mir <unknown>
2002-09-15 15:59:00 +0000

    no message

---
+++ ResourceManager/class/resource caching/lookupCachedResource-3A-ifPresentDo-3A-.cs
@@ -0,0 +1,21 @@
+lookupCachedResource: cachedUrlString ifPresentDo: streamBlock
+	"See if we have cached the resource described by the given url and if so, evaluate streamBlock with the cached resource."
+	|  urlString candidates url stream |
+	CachedResources ifNil:[^self].
+
+	candidates _ CachedResources at: cachedUrlString ifAbsent:[nil].
+	(self lookupCachedResource: cachedUrlString in: candidates ifPresentDo: streamBlock)
+		ifTrue: [^self].
+
+	urlString _ self relocatedExternalResource: cachedUrlString.
+	urlString ifNil: [^self].
+	candidates _ CachedResources at: urlString ifAbsent:[nil].
+	candidates
+		ifNil: [
+			(url _ urlString asUrl) schemeName = 'file'
+				ifTrue: [
+					stream _ [FileStream readOnlyFileNamed: url pathForFile] 
+							on: FileDoesNotExistException do:[:ex| ex return: nil].
+					stream
+						ifNotNil: [[streamBlock value: stream] ensure: [stream close]]]]
+		ifNotNil: [self lookupCachedResource: urlString in: candidates ifPresentDo: streamBlock]
