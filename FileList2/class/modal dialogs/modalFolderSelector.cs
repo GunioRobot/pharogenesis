683a6df58632c8126acb3d43aadb8623c07fc4b4
gh <unknown>
2002-09-16 10:33:00 +0000

    no message

--- FileList2/class/modal dialogs/modalFolderSelector.cs
+++ FileList2/class/modal dialogs/modalFolderSelector.cs
@@ -1,10 +1,3 @@
 modalFolderSelector
 
-	| window fileModel |
-	window _ self morphicViewFolderSelector.
-	fileModel _ window model.
-	window openInWorld: self currentWorld extent: 300@400.
-	[window world notNil] whileTrue: [
-		window outermostWorldMorph doOneCycleNow.
-	].
-	^fileModel getSelectedDirectory withoutListWrapper
+	^self modalFolderSelector: FileDirectory default
