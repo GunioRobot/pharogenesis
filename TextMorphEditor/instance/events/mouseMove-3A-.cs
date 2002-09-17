473db2f36f8e04b1eea06c6224a0a62375d950a8
th <unknown>
2002-09-17 16:45:00 +0000

    no message

--- TextMorphEditor/instance/events/mouseMove-3A-.cs
+++ TextMorphEditor/instance/events/mouseMove-3A-.cs
@@ -1,9 +1,6 @@
 mouseMove: evt 
 	"Change the selection in response to moue-down drag"
-	| dragBlock |
+
 	pivotBlock ifNil: [^ self].  "Patched during clickAt: repair"
-	dragBlock _ paragraph characterBlockAtPoint: (evt cursorPoint).
-	dragBlock > pivotBlock
-		ifTrue: [stopBlock _ dragBlock.  startBlock _ pivotBlock]
-		ifFalse: [startBlock _ dragBlock.  stopBlock _ pivotBlock].
+	self pointBlock: (paragraph characterBlockAtPoint: (evt cursorPoint)).
 	self storeSelectionInParagraph
