6552bf92ca024270c17a675098a173b4062b53af
th <unknown>
2002-09-17 16:23:00 +0000

    no message

--- ParagraphEditor/instance/typing support/insertTypeAhead-3A-.cs
+++ ParagraphEditor/instance/typing support/insertTypeAhead-3A-.cs
@@ -2,4 +2,4 @@ insertTypeAhead: typeAhead
 	typeAhead position = 0 ifFalse:
 		[self zapSelectionWith: (Text string: typeAhead contents emphasis: emphasisHere).
 		typeAhead reset.
-		startBlock _ stopBlock copy]
+		self unselect]
