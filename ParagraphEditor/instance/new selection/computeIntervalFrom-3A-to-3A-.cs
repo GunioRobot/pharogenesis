37dc23f301380a126c1bf53986924c7ffb5fd1dd
th <unknown>
2002-09-17 16:11:00 +0000

    no message

--- ParagraphEditor/instance/new selection/computeIntervalFrom-3A-to-3A-.cs
+++ ParagraphEditor/instance/new selection/computeIntervalFrom-3A-to-3A-.cs
@@ -1,7 +1,5 @@
 computeIntervalFrom: start to: stop
 	"Select the designated characters, inclusive.  Make no visual changes."
 
-	startBlock _ paragraph characterBlockForIndex: start.
-	stopBlock _ start > stop
-		ifTrue: [startBlock copy]
-		ifFalse: [paragraph characterBlockForIndex: stop + 1]
+	self setMark: start.
+	self setPoint: stop + 1.
