659d0a0733a991216c0e3a0fcb682a911fedab43
th <unknown>
2002-09-18 16:50:00 +0000

    no message

--- ParagraphEditor/instance/new selection/selectFrom-3A-to-3A-.cs
+++ ParagraphEditor/instance/new selection/selectFrom-3A-to-3A-.cs
@@ -2,7 +2,7 @@ selectFrom: start to: stop
 	"Deselect, then select the specified characters inclusive.
 	 Be sure the selection is in view."
 
-	(start = startBlock stringIndex and: [stop + 1 = stopBlock stringIndex]) ifFalse:
+	(start = self startIndex and: [stop + 1 = self stopIndex]) ifFalse:
 		[self deselect.
 		self selectInvisiblyFrom: start to: stop].
 	self selectAndScroll
