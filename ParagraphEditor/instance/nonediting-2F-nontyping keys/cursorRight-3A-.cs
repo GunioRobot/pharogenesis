61c957bde535088b1d558b4efffe91d2cbcf47c5
th <unknown>
2002-09-19 20:01:00 +0000

    no message

--- ParagraphEditor/instance/nonediting-2F-nontyping keys/cursorRight-3A-.cs
+++ ParagraphEditor/instance/nonediting-2F-nontyping keys/cursorRight-3A-.cs
@@ -3,15 +3,10 @@ cursorRight: characterStream
 	otherwise move cursor to end of selection. If the shift key is down, 
 	start selecting characters or extending already selected characters. 
 	Don't allow cursor past end of text"
-	| shift right |
+
 	self closeTypeIn: characterStream.
-	shift _ sensor leftShiftDown.
-	right _ stopBlock stringIndex + 1.
-	sensor controlKeyPressed ifTrue: [right _ self nextWord: right].
-	sensor keyboard.
-	shift
-		ifTrue: [self selectFrom: startBlock stringIndex to: right - 1]
-		ifFalse: [startBlock stringIndex == stopBlock stringIndex
-				ifTrue: [self selectAt: right]
-				ifFalse: [self selectAt: stopBlock stringIndex]].
+	self
+		moveCursor: [:position | position + 1]
+		forward: true
+		specialBlock:[:position | self nextWord: position].
 	^ true
