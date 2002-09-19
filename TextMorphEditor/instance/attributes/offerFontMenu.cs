8c75b918e5e27b935e0324388b1ab5233c1a7bea
th <unknown>
2002-09-19 18:30:00 +0000

    no message

--- TextMorphEditor/instance/attributes/offerFontMenu.cs
+++ TextMorphEditor/instance/attributes/offerFontMenu.cs
@@ -2,8 +2,9 @@ offerFontMenu
 	"Present a menu of available fonts, and if one is chosen, apply it to the current selection.  
 	Use only names of Fonts of this paragraph  "
 	| aList reply curFont menuList |
+true ifTrue:[^self changeTextFont].
 	self flag: #arNote. "Move this up once we get rid of MVC"
-	curFont _ (paragraph text fontAt: startBlock stringIndex withStyle: paragraph textStyle) fontNameWithPointSize.
+	curFont _ (paragraph text fontAt: self startIndex withStyle: paragraph textStyle) fontNameWithPointSize.
 	aList _ paragraph textStyle fontNamesWithPointSizes.
 	menuList _ aList collect:[:fntName|
 		fntName = curFont ifTrue:['<on>',fntName] ifFalse:['<off>',fntName]].
