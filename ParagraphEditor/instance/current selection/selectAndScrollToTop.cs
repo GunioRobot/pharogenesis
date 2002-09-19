6182006b8ba9648e22f1aa5b3dec244d5ea18478
th <unknown>
2002-09-19 18:48:00 +0000

    no message

--- ParagraphEditor/instance/current selection/selectAndScrollToTop.cs
+++ ParagraphEditor/instance/current selection/selectAndScrollToTop.cs
@@ -4,7 +4,7 @@ selectAndScrollToTop
 	self select.
 	lineHeight _ paragraph textStyle lineGrid.
 	clippingRectangle _ paragraph clippingRectangle.
-	deltaY _ stopBlock top - clippingRectangle top.
+	deltaY _ self stopBlock top - clippingRectangle top.
 	deltaY ~= 0 
 		ifTrue: [self scrollBy: (deltaY abs + lineHeight - 1 truncateTo: lineHeight)
 									* deltaY sign]
