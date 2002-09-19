8e21cb94decd573acebed6b801711164ecd6c1ee
th <unknown>
2002-09-19 18:47:00 +0000

    no message

--- ParagraphEditor/instance/current selection/selectAndScroll.cs
+++ ParagraphEditor/instance/current selection/selectAndScroll.cs
@@ -1,12 +1,13 @@
 selectAndScroll
 	"Scroll until the selection is in the view and then highlight it."
-	| lineHeight deltaY clippingRectangle |
+	| lineHeight deltaY clippingRectangle endBlock |
 	self select.
+	endBlock _ self stopBlock.
 	lineHeight _ paragraph textStyle lineGrid.
 	clippingRectangle _ paragraph clippingRectangle.
-	deltaY _ stopBlock top - clippingRectangle top.
+	deltaY _ endBlock top - clippingRectangle top.
 	deltaY >= 0 
-		ifTrue: [deltaY _ stopBlock bottom - clippingRectangle bottom max: 0].
+		ifTrue: [deltaY _ endBlock bottom - clippingRectangle bottom max: 0].
 						"check if stopIndex below bottom of clippingRectangle"
 	deltaY ~= 0 
 		ifTrue: [self scrollBy: (deltaY abs + lineHeight - 1 truncateTo: lineHeight)
