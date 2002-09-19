59e453bad9214d62aa410cdc53c0843b62c10e74
th <unknown>
2002-09-19 17:34:00 +0000

    no message

--- ParagraphEditor/instance/typing-2F-selecting keys/simulatedBackspace.cs
+++ ParagraphEditor/instance/typing-2F-selecting keys/simulatedBackspace.cs
@@ -2,7 +2,7 @@ simulatedBackspace
 	"Backspace over the last character, derived from hand-char recognition.  2/5/96 sw"
 
 	| startIndex |
-	startIndex _ startBlock stringIndex + (startBlock = stopBlock ifTrue: [0] ifFalse: [1]).
+	startIndex _ self markIndex + (self hasSelection ifTrue: [1] ifFalse: [0]).
 
 	startIndex _ 1 max: startIndex - 1.
 	self backTo: startIndex.
