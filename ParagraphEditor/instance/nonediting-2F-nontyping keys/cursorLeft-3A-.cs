53799bb43f5a3e7b78dbd0424e7a6baf79d140f1
th <unknown>
2002-09-19 20:07:00 +0000

    no message

--- ParagraphEditor/instance/nonediting-2F-nontyping keys/cursorLeft-3A-.cs
+++ ParagraphEditor/instance/nonediting-2F-nontyping keys/cursorLeft-3A-.cs
@@ -3,16 +3,10 @@ cursorLeft: characterStream
 	move cursor to beginning of selection. If the shift key is down, start 
 	selecting or extending current selection. Don't allow cursor past 
 	beginning of text"
-	| shift left |
+
 	self closeTypeIn: characterStream.
-	shift _ sensor leftShiftDown.
-	left _ startBlock stringIndex - 1.
-	sensor controlKeyPressed ifTrue: [left _ self previousWord: left].
-	sensor keyboard.
-	shift
-		ifTrue: [startBlock stringIndex > 1
-			ifTrue: [self selectFrom: left to: stopBlock stringIndex - 1]]
-		ifFalse: [(startBlock stringIndex == stopBlock stringIndex and: [startBlock stringIndex > 1])
-			ifTrue: [self selectAt: left]
-			ifFalse: [self selectAt: startBlock stringIndex]].
+	self
+		moveCursor:[:position | position - 1 max: 1]
+		forward: false
+		specialBlock:[:position | self previousWord: position].
 	^ true
