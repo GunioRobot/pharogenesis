63bc1772c9ae8329efeb7c6231706cc611f8b203
th <unknown>
2002-09-20 12:14:00 +0000

    no message

--- ParagraphEditor/instance/nonediting-2F-nontyping keys/cursorHome-3A-.cs
+++ ParagraphEditor/instance/nonediting-2F-nontyping keys/cursorHome-3A-.cs
@@ -1,23 +1,20 @@
 cursorHome: characterStream 
 
-"Private - Move cursor from position in current line to beginning of
-current line. If cursor already at beginning of line, put cursor at
-beginning of text"
+	"Private - Move cursor from position in current line to beginning of
+	current line. If control key is pressed put cursor at beginning of text"
 
-	| string left |
-
-	self closeTypeIn: characterStream.
-	sensor keyboard.
+	| string |
 
 	string _ paragraph text string.
-	left _ startBlock stringIndex.
-	[left > 1 and: [(string at: (left - 1)) ~= Character cr]]
-		whileTrue: [left _ left - 1].
-
-	sensor commandKeyPressed ifTrue: [left _ 1].
-
-	sensor leftShiftDown
-		ifTrue: [self selectFrom: left to: stopBlock stringIndex - 1]
-		ifFalse: [self selectAt: left].
-
+	self
+		moveCursor: [ :position | Preferences wordStyleCursorMovement
+				ifTrue:[
+					(paragraph lines at:(paragraph lineIndexOfCharacterIndex: position)) first]
+				ifFalse:[
+					(string
+						lastIndexOf: Character cr
+						startingAt: position - 1
+						ifAbsent:[0]) + 1]]
+		forward: false
+		specialBlock: [:dummy | 1].
 	^true
