b5ae7e8ff3c891b522c995dcc360d81651ede59f
th <unknown>
2002-09-20 11:41:00 +0000

    no message

--- ParagraphEditor/instance/current selection/recomputeInterval.cs
+++ ParagraphEditor/instance/current selection/recomputeInterval.cs
@@ -1,4 +1,4 @@
 recomputeInterval
 	"The same characters are selected but their coordinates may have changed."
 
-	self computeIntervalFrom: startBlock stringIndex to: stopBlock stringIndex - 1
+	self computeIntervalFrom: self mark to: self pointIndex - 1
