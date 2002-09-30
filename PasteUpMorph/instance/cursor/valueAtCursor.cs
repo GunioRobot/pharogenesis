612de76d52615f58ac6ed5ffd501764d3315550a
bf <unknown>
2002-09-30 23:37:00 +0000

    no message

--- PasteUpMorph/instance/cursor/valueAtCursor.cs
+++ PasteUpMorph/instance/cursor/valueAtCursor.cs
@@ -2,4 +2,4 @@ valueAtCursor
 	"Answer the submorph of mine indexed by the value of my 'cursor' slot"
 
 	submorphs isEmpty ifTrue: [^ self presenter standardPlayer costume].
-	^ submorphs at: ((cursor truncated max: 1) min: submorphs size)
+	^ (submorphs at: ((cursor truncated max: 1) min: submorphs size)) morphRepresented
