3ec80dac5f7f284e7e75085c8c2c0647f7cf22f2
bf <unknown>
2002-10-02 17:07:00 +0000

    no message

--- PasteUpMorph/instance/painting/backgroundSketch.cs
+++ PasteUpMorph/instance/painting/backgroundSketch.cs
@@ -1,6 +1,6 @@
 backgroundSketch
 
 	backgroundMorph ifNil: [^ nil].
-	backgroundMorph owner ifNil:
+	backgroundMorph owner == self ifFalse:
 		[backgroundMorph _ nil].	"has been deleted"
 	^ backgroundMorph
