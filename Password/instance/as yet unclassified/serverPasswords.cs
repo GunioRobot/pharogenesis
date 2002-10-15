4c8e3723d64d3e8b6c9ffe1aee7afe11b9a465bf
tk <unknown>
2002-10-15 14:39:00 +0000

    no message

--- Password/instance/as yet unclassified/serverPasswords.cs
+++ Password/instance/as yet unclassified/serverPasswords.cs
@@ -1,10 +1,9 @@
 serverPasswords
-	"Get the server passwords off the disk and decode them. The file 'sqk.info' must be in the same folder as the Squeak application (VM) that is running this image. (Note: This code works even if you are running with no system sources file.)"
+	"Get the server passwords off the disk and decode them. The file 'sqk.info' must be in some folder that Squeak thinks is special (vm folder, or default directory).  (Note: This code works even if you are running with no system sources file.)"
 
-	| dir encodedPasswords |
-	dir _ FileDirectory on: Smalltalk vmPath.
-	(dir fileExists: 'sqk.info') ifFalse: [^ nil].	"Caller will ask user for password"
-	encodedPasswords _ (dir readOnlyFileNamed: 'sqk.info') contentsOfEntireFile.
-		"If you don't have this file, and you really do want to release 
-		an update, contact Ted Kaehler."
-	^ (self decode: encodedPasswords) findTokens: String cr
+	| sfile |
+	(sfile _ FileDirectory lookInUsualPlaces: 'sqk.info') ifNil: [^ nil].
+		"If not there, Caller will ask user for password"
+		"If you don't have this file, and you really do want to release an update, 
+		 contact Ted Kaehler."
+	^ (self decode: (sfile contentsOfEntireFile)) findTokens: String cr
