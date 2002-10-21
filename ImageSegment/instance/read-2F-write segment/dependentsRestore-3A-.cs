34bb7e53216fdbba1e38837e7da77194e6b1eba8
tk <unknown>
2002-10-21 16:17:00 +0000

    no message

---
+++ ImageSegment/instance/read-2F-write segment/dependentsRestore-3A-.cs
@@ -0,0 +1,10 @@
+dependentsRestore: aProject
+	"Retrieve the list of dependents from the exporting system, hook them up, and erase the place we stored them."
+
+	| dict |
+	dict _ aProject projectParameterAt: #GlobalDependentsInProject.
+	dict ifNil: [^ self].
+	dict associationsDo: [:assoc |
+		assoc value do: [:dd | assoc key addDependent: dd]].
+
+	self dependentsCancel: aProject.
