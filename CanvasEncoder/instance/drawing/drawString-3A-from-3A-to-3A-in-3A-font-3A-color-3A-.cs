fe0cdefd82c50104925a45241a0834858af6e231
yo <unknown>
2002-10-23 23:40:00 +0000

    no message

---
+++ CanvasEncoder/instance/drawing/drawString-3A-from-3A-to-3A-in-3A-font-3A-color-3A-.cs
@@ -0,0 +1,21 @@
+drawString: s from: firstIndex to: lastIndex in: boundsRect font: fontOrNil color: c
+	| fontIndex str |
+	fontIndex := self establishFont: (fontOrNil ifNil: [ TextStyle defaultFont ]).
+	str _ s asString.
+	str class = MultiString ifTrue: [
+		self sendCommand: {
+			String with: CanvasEncoder codeMultiText.
+			(str copyFrom: firstIndex to: lastIndex) asByteArray asString.
+			self class encodeRectangle: boundsRect.
+			self class encodeInteger: fontIndex.
+			self class encodeColor: c
+		}
+	] ifFalse: [
+		self sendCommand: {
+			String with: CanvasEncoder codeText.
+			s asString copyFrom: firstIndex to: lastIndex.
+			self class encodeRectangle: boundsRect.
+			self class encodeInteger: fontIndex.
+			self class encodeColor: c
+		}
+	].
