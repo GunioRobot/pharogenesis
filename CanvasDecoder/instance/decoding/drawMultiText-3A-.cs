1acd4ddbd5fbd08153aa8d2a8aad6a05a97750de
yo <unknown>
2002-10-23 23:37:00 +0000

    no message

---
+++ CanvasDecoder/instance/decoding/drawMultiText-3A-.cs
@@ -0,0 +1,18 @@
+drawMultiText: command
+
+	| boundsEnc colorEnc  text bounds color fontIndexEnc fontIndex |
+
+	text := MultiString fromByteArray: (command at: 2) asByteArray.
+	"text asByteArray printString displayAt: 800@0."
+	"self halt."
+	boundsEnc := command at: 3.
+	fontIndexEnc := command at: 4.
+	colorEnc := command at: 5.
+
+
+	bounds _ self class decodeRectangle: boundsEnc.
+	fontIndex := self class decodeInteger: fontIndexEnc.
+	color _ self class decodeColor: colorEnc.
+
+	self drawCommand: [ :c |
+		c drawString: text in: bounds font: (fonts at: fontIndex) color: color ]
