1fcb0ef0d683ffc54f861175b8453f78b74f2a99
sw <unknown>
2002-10-27 09:27:00 +0000

    no message

--- HaloMorph/instance/private/doDebug-3A-with-3A-.cs
+++ HaloMorph/instance/private/doDebug-3A-with-3A-.cs
@@ -2,13 +2,12 @@ doDebug: evt with: menuHandle
 	"Ask hand to invoke the a debugging menu for my inner target.  If shift key is down, immediately put up an inspector on the inner target"
 
 	| menu |
-	evt hand obtainHalo: self.
-	self removeAllHandlesBut: nil.  "remove all handles"
+	self obtainHaloForEvent: evt andRemoveAllHandlesBut: nil.
 	self world displayWorld.
 	evt shiftPressed ifTrue: 
 		[self delete.
 		^ innerTarget inspectInMorphic: evt].
 
 	menu _ innerTarget buildDebugMenu: evt hand.
-	menu addTitle: innerTarget externalName.
-	menu popUpEvent: evt in: self world.
+	menu addTitle: (innerTarget externalName truncateWithElipsisTo: 40).
+	menu popUpEvent: evt in: self world
