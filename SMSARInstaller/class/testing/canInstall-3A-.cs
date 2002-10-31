ca1eb0e0800eec3fc3da83a4c645f297d9d3640f
gh <unknown>
2002-10-31 10:03:00 +0000

    no message

---
+++ SMSARInstaller/class/testing/canInstall-3A-.cs
@@ -0,0 +1,12 @@
+canInstall: aPackage
+	"Answer if this class can install the package.
+	We handle it if the filename has the extension
+	.sar (upper and lowercase) and SARInstaller is
+	present in the image to handle the install."
+
+	| fileName |
+	fileName _ aPackage downloadFileName.
+	fileName ifNil: [^false].
+	Smalltalk at: #SARInstaller ifPresentAndInMemory: [ :installer |
+			^'sar' = (FileDirectory extensionFor: fileName) asLowercase].
+	^false
