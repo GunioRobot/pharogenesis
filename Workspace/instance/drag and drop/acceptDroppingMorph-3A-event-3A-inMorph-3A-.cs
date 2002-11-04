a8984397fca4e71700a3ce502273d5641da12bb7
yo <unknown>
2002-11-04 22:50:00 +0000

    no message

--- Workspace/instance/drag and drop/acceptDroppingMorph-3A-event-3A-inMorph-3A-.cs
+++ Workspace/instance/drag and drop/acceptDroppingMorph-3A-event-3A-inMorph-3A-.cs
@@ -1,8 +1,11 @@
 acceptDroppingMorph: dropee event: evt inMorph: targetMorph 
 	"Return the dropee to its old position, and add a reference to it at the cursor point."
-	| bindingName |
 
-	bindingName _  dropee externalName translateToLowercase, dropee identityHash printString..
+	| bindingName externalName |
+	externalName _ dropee externalName.
+	externalName _ externalName isOctetString
+		ifTrue: [externalName] ifFalse: ['a' , externalName].
+	bindingName _ externalName translateToLowercase, dropee identityHash printString.
 	targetMorph correctSelectionWithString: bindingName, ' '.
 	(self bindingOf: bindingName) value: dropee.
 	dropee rejectDropMorphEvent: evt.
