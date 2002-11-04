5a60768cf1bb0b0f74b463560839125648869415
yo <unknown>
2002-11-04 20:50:00 +0000

    no message

--- FlapTab/instance/edge/setEdge-3A-.cs
+++ FlapTab/instance/edge/setEdge-3A-.cs
@@ -1,26 +1,16 @@
 setEdge: anEdge
-	| changedOrientation |
-	changedOrientation _ nil.
+	"Set the edge as indicated, if possible"
+
+	| newOrientation |
+	self edgeToAdhereTo = anEdge ifTrue: [^ self].
+	newOrientation _ nil.
 	self orientation == #vertical
-			ifTrue:
-				[(#(top bottom) includes: anEdge) ifTrue:
-					[changedOrientation _ #horizontal]]
-			ifFalse:
-				[(#(top bottom) includes: anEdge) ifFalse:
-					[changedOrientation _ #vertical]].
-	changedOrientation ifNotNil:
-		[^ self inform:
-'SORRY -- this sort of switch from horizontal to vertical
-or vice-versa is not yet permitted.  For now, to achieve
-the desired effect, create a new flap and copy over
-the elements you want.'].
-
-	self isCurrentlyTextual ifTrue:
-		[changedOrientation ifNotNil:
-			[self assumeString: (submorphs first contents copyWithout: Character cr) font:  Preferences standardFlapFont orientation: changedOrientation color: self color]].
-
+		ifTrue: [(#(top bottom) includes: anEdge) ifTrue:
+					[newOrientation _ #horizontal]]
+		ifFalse: [(#(top bottom) includes: anEdge) ifFalse:
+					[newOrientation _ #vertical]].
 	self edgeToAdhereTo: anEdge.
-	changedOrientation ifNotNil:
-		[self transposeParts].
+	newOrientation ifNotNil: [self transposeParts].
 	referent isInWorld ifTrue: [self positionReferent].
+	self changeTabText: self existingWording.
 	self adjustPositionVisAVisFlap
