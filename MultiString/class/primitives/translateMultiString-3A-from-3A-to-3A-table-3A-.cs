c2b82dcd03b7d1d8f4a777882201f6fd485f3f87
yo <unknown>
2002-11-04 22:58:00 +0000

    no message

---
+++ MultiString/class/primitives/translateMultiString-3A-from-3A-to-3A-table-3A-.cs
@@ -0,0 +1,13 @@
+translateMultiString: aString from: start  to: stop  table: table
+	"translate the characters in the string by the given table, in place"
+
+	| char |
+	self var: #table  declareC: 'unsigned char *table'.
+	self var: #aString  declareC: 'unsigned int *aString'.
+
+	start to: stop do: [:i |
+		char _ aString basicAt: i.
+		char < 256 ifTrue: [
+			aString basicAt: i put: (table at: char+1) asciiValue
+		].
+	].
