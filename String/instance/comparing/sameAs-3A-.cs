e26409936d150a8dc6c1250f2b9948a1551a0036
yo <unknown>
2002-11-05 15:31:00 +0000

    no message

--- String/instance/comparing/sameAs-3A-.cs
+++ String/instance/comparing/sameAs-3A-.cs
@@ -2,4 +2,9 @@ sameAs: aString
 	"Answer whether the receiver sorts equal to aString. The 
 	collation sequence is ascii with case differences ignored."
 
-	^ (self compare: self with: aString collated: CaseInsensitiveOrder) = 2
+	| m |
+	aString isOctetString ifTrue: [
+		^ (self compare: self with: aString asOctetString collated: CaseInsensitiveOrder) = 2
+	].
+	m _ self asMultiString.
+	^ (m compare: m with: aString collated: CaseInsensitiveOrder) = 2
