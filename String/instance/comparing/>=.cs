a03a5c3d464cdc4fcf11b1f25f1dfcae67e2a62c
yo <unknown>
2002-11-05 15:30:00 +0000

    no message

--- String/instance/comparing/>=.cs
+++ String/instance/comparing/>=.cs
@@ -2,4 +2,10 @@
 	"Answer whether the receiver sorts after or equal to aString.
 	The collation order is simple ascii (with case differences)."
 
-	^ (self compare: self with: aString collated: AsciiOrder) >= 2
+
+	| m |
+	aString isOctetString ifTrue: [
+		^ (self compare: self with: aString asOctetString collated: AsciiOrder) >= 2
+	].
+	m _ self asMultiString.
+	^ (m compare: m with: aString collated: nil) >= 2
