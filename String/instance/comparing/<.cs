b09433b6ed8cf118741b4e869ea1db180816049b
yo <unknown>
2002-11-05 15:29:00 +0000

    no message

--- String/instance/comparing/<.cs
+++ String/instance/comparing/<.cs
@@ -2,4 +2,9 @@
 	"Answer whether the receiver sorts before aString.
 	The collation order is simple ascii (with case differences)."
 
-	^ (self compare: self with: aString collated: AsciiOrder) = 1
+	| m |
+	aString isOctetString ifTrue: [
+		^ (self compare: self with: aString asOctetString collated: AsciiOrder) = 1
+	].
+	m _ self asMultiString.
+	^ (m compare: m with: aString collated: nil) = 1
