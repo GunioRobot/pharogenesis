5b4f256214e739f82b6aadc9fa0f5f0b95e19652
yo <unknown>
2002-11-05 15:29:00 +0000

    no message

--- String/instance/comparing/=.cs
+++ String/instance/comparing/=.cs
@@ -2,6 +2,10 @@
 	"Answer whether the receiver sorts equally as aString.
 	The collation order is simple ascii (with case differences)."
 
-	aString species == String ifFalse: [^ false].
+	(aString class == String or: [aString class == Symbol]) ifFalse: [
+		aString class == MultiString ifTrue: [^ aString = self].
+		aString isText ifTrue: [^ self = aString string].
+		^ false
+	].
 
-	^ (self compare: self with: aString collated: AsciiOrder) = 2
+	^ (self compare: self with: aString collated: AsciiOrder) = 2.
