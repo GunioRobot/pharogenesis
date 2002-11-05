3c99811e8eb7df947a044e7aa0b8c518c3862ca2
yo <unknown>
2002-11-05 15:30:00 +0000

    no message

--- String/instance/comparing/caseInsensitiveLessOrEqual-3A-.cs
+++ String/instance/comparing/caseInsensitiveLessOrEqual-3A-.cs
@@ -2,4 +2,9 @@ caseInsensitiveLessOrEqual: aString
 	"Answer whether the receiver sorts before or equal to aString.
 	The collation order is case insensitive."
 
-	^ (self compare: self with: aString collated: CaseInsensitiveOrder) <= 2
+	| m |
+	aString isOctetString ifTrue: [
+		^ (self compare: self with: aString asOctetString collated: CaseInsensitiveOrder) <= 2
+	].
+	m _ self asMultiString.
+	^ (m compare: m with: aString collated: CaseInsensitiveOrder) <= 2
