aa6c62a318ec99cbc1644d7275339c7a37d1fb88
sw <unknown>
2002-11-08 15:15:00 +0000

    no message

--- StackMorph/instance/card access/goToCard-3A-.cs
+++ StackMorph/instance/card access/goToCard-3A-.cs
@@ -2,14 +2,20 @@ goToCard: destinationCard
 	"Install the indicated destinationCard as the current card in the receiver.  Any viewer currently open on the current card will get retargeted to look at the new one."
 
 	| aBackground existingCard oldViewers |
+	destinationCard == self currentCard ifTrue: [^ self].
+	self currentPlayerDo:
+		[:aPlayer | aPlayer runAllClosingScripts].   "Like HyperCard 'on closeCard'"
+
 	aBackground _ self backgroundWithCard: destinationCard.
 	existingCard _ aBackground currentDataInstance.
 	oldViewers _ existingCard ifNil: [#()] ifNotNil: [existingCard allOpenViewers].
 
 	aBackground installAsCurrent: destinationCard.
+	aBackground setProperty: #myStack toValue: self.	"pointer cardMorph -> stack"
 
 	aBackground ~~ currentPage ifTrue:
-		["concern that need to run opening/closing scripts even when we're not going to a different background"
-		self goToPageMorph: aBackground].
+		[self goToPageMorph: aBackground runTransitionScripts: false].
+	self currentPlayerDo:
+		[:aPlayer | aPlayer runAllOpeningScripts] .  "Like HyperCard 'on opencard'"
 
 	oldViewers do: [:aViewer | aViewer retargetFrom: existingCard to: destinationCard]
