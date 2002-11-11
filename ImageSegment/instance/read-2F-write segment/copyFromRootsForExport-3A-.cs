9167efd0fcf8fc85f89cc12c38d4be2b1a942071
yo <unknown>
2002-11-11 17:30:00 +0000

    no message

--- ImageSegment/instance/read-2F-write segment/copyFromRootsForExport-3A-.cs
+++ ImageSegment/instance/read-2F-write segment/copyFromRootsForExport-3A-.cs
@@ -2,8 +2,9 @@ copyFromRootsForExport: rootArray
 	"When possible, use copySmartRootsExport:.  This way may not copy a complete tree of objects.  Add to roots: all of the methods pointed to from the outside by blocks."
 	| newRoots list segSize symbolHolder |
 	arrayOfRoots _ rootArray.
-	Smalltalk forgetDoIts.  
-	symbolHolder _ Symbol allInstances.	"Hold onto Symbols with strong pointers, 
+	Smalltalk forgetDoIts.
+	"self halt."
+	symbolHolder _ Symbol allInstances, MultiSymbol allInstances.	"Hold onto Symbols with strong pointers, 
 		so they will be in outPointers"
 	(newRoots _ self rootsIncludingPlayers) ifNotNil: [
 		arrayOfRoots _ newRoots].		"world, presenter, and all Player classes"
