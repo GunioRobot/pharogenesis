0eb16d03e843a6c69f6974fd870abf74e0bdf036
sw <unknown>
2002-11-12 13:41:00 +0000

    no message

--- CodeHolder/instance/commands/spawnHierarchy.cs
+++ CodeHolder/instance/commands/spawnHierarchy.cs
@@ -1,14 +1,20 @@
 spawnHierarchy
-	"Create and schedule a new class hierarchy browser on the currently selected class or meta."
+	"Create and schedule a new hierarchy browser on the currently selected class or meta."
+
 	| newBrowser aSymbol aBehavior messageCatIndex selectedClassOrMetaClass |
-	(selectedClassOrMetaClass _ self selectedClassOrMetaClass) ifNil: [^ self].
+	(selectedClassOrMetaClass _ self selectedClassOrMetaClass)
+		ifNil: [^ self].
 	newBrowser _ HierarchyBrowser new initHierarchyForClass: selectedClassOrMetaClass.
-	(aSymbol _ self selectedMessageName) ifNotNil:
-		[aBehavior _ selectedClassOrMetaClass.
-		messageCatIndex _ aBehavior organization numberOfCategoryOfElement: aSymbol.
-		newBrowser messageCategoryListIndex: messageCatIndex + 1.
-		newBrowser messageListIndex:
-			((aBehavior organization listAtCategoryNumber: messageCatIndex)
-						indexOf: aSymbol)].
-	Browser openBrowserView: (newBrowser openSystemCatEditString: nil)
-		label: self selectedClassName , ' hierarchy'
+	((aSymbol _ self selectedMessageName) notNil and: [(MessageSet isPseudoSelector: aSymbol) not])
+		ifTrue:
+			[aBehavior _ selectedClassOrMetaClass.
+			messageCatIndex _ aBehavior organization numberOfCategoryOfElement: aSymbol.
+			newBrowser messageCategoryListIndex: messageCatIndex + 1.
+			newBrowser messageListIndex:
+				((aBehavior organization listAtCategoryNumber: messageCatIndex) indexOf: aSymbol)].
+	Browser
+		openBrowserView: (newBrowser openSystemCatEditString: nil)
+		label: newBrowser labelString.
+	Smalltalk isMorphic
+		ifTrue: ["this workaround only needed in morphic"
+			newBrowser assureSelectionsShow]
