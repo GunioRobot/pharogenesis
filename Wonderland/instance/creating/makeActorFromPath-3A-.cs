27d44d108a4eb44b650e69f2f05d4805ffa08021
ar <unknown>
2002-11-16 22:44:00 +0000

    no message

---
+++ Wonderland/instance/creating/makeActorFromPath-3A-.cs
@@ -0,0 +1,119 @@
+makeActorFromPath: filePath
+	"Creates a new actor using the specification from the given file"
+	| aFile words line startSubstr index parent name texture meshFile matrix baseActor newActor protoClass actorClass fileVersion pos filename |
+	filename _ filePath inject: FileDirectory default pathName into:[:fp :p| fp, FileDirectory slash, p].
+	aFile _ (CrLfFileStream readOnlyFileNamed: filename) ascii.
+
+	"First see if we need to create a prototype class for this model"
+	protoClass _ WonderlandActor.
+
+	"Check what version this mdl file is"
+	line _ aFile upTo: (Character cr).
+	line _ aFile upTo: (Character cr).
+	line _ aFile upTo: (Character cr).
+
+	((line truncateTo: 7) = 'version')
+		ifTrue: [ fileVersion _ 1 ]
+		ifFalse: [ fileVersion _ 0 ].
+
+	[ line _ aFile upTo: (Character cr).
+	aFile atEnd] whileFalse:[
+		words _ line findTokens: '='.
+		"See if we're creating a new object"
+		(((words size) > 1) and: [ ((words at: 2) beginsWith: ' _MakeObject')
+			or: [ (words at: 2) beginsWith: ' Alice.MakeObject' ] ]) ifTrue: [
+			(fileVersion = 0) ifTrue: [
+				words _ line findTokens: #,.
+				parent _ (words at: 2) withBlanksTrimmed.
+				name _ (((words at: 3) withBlanksTrimmed) findBetweenSubStrs: '"') at: 1.
+			] ifFalse: [
+				name _ (words at: 1) truncateTo: (((words at: 1) size) - 1).
+				parent _ ((words at: 3) findTokens: #,) at: 1.
+			].
+
+			"Now pull in the texture to use"
+			startSubstr _ name , '.SetTexture'.
+			pos _ aFile position.
+			texture := nil.
+			[aFile atEnd or:[texture notNil]] whileFalse:[
+				line _ aFile upTo: (Character cr).
+				(line beginsWith: startSubstr) ifTrue:[
+					texture _ (line findBetweenSubStrs: '"') at: 2.
+					texture _ (aFile directory pathName), FileDirectory slash, texture]].
+			texture ifNil:[aFile position: pos].
+
+			"Read the composite matrix to use"
+			startSubstr _ name , '._SetLocalTransformation'.
+			pos _ aFile position.
+			matrix := nil.
+			[aFile atEnd or:[matrix notNil]] whileFalse:[
+				line _ aFile upTo: (Character cr).
+				(line beginsWith: startSubstr) ifTrue:[
+					matrix _ B3DMatrix4x4 new.
+					words _ line findBetweenSubStrs: ',()'.
+					words removeAllSuchThat: [:str | str = ' '].
+					index _ words size.
+					4 to: 1 by: -1 do: [:i | 
+						4 to: 1 by: -1 do: [:j | matrix at: i at: j put: 
+									((words at: index) withBlanksTrimmed) asNumber.
+									   		index _ index - 1. ]. ].
+
+					1 to: 4 do: [:i | index _ matrix at: i at: 4.
+								   matrix at: i at: 4 put: (matrix at: 4 at: i).
+								   matrix at: 4 at: i put: index. ].
+
+					matrix a14: (matrix a14 negated).
+				].
+			].
+			matrix ifNil:[aFile position: pos].
+
+			"Read the mesh file to use"
+			startSubstr _ 'LoadGeometry'.
+			pos _ aFile position.
+			meshFile := nil.
+			[aFile atEnd or:[meshFile notNil]] whileFalse:[
+				line _ aFile upTo: (Character cr).
+				(line beginsWith: startSubstr) ifTrue:[
+					meshFile _ (line findBetweenSubStrs: '"') at: 2.
+					meshFile _ (aFile directory pathName), FileDirectory slash, meshFile.
+				].
+			].
+			meshFile ifNil:[aFile position: pos].
+
+			"Now build the actor name"
+			words _ name findTokens: '.'.
+			name _ words last.
+			name at: 1 put: ((name at: 1) asLowercase).
+
+			"Now build the parent name"
+			parent _ parent copyReplaceAll: '.' with: ' '.
+
+			"Now create the object"
+			(parent = 'None') ifTrue: [
+				actorClass _ protoClass newUniqueClassInstVars: '' classInstVars: ''.
+				baseActor _ actorClass createFor: self.
+				baseActor setName: name.
+				texture ifNotNil:[baseActor setTexture: texture].
+				meshFile ifNotNil:[baseActor loadMeshFromFile: meshFile].
+				matrix ifNotNil:[baseActor setComposite: matrix].
+				"end base actor creation"
+			] ifFalse: [
+				actorClass _ WonderlandActor newUniqueClassInstVars: ''
+											classInstVars: ''.
+				newActor _ actorClass createFor: self.
+				newActor setName: name.
+				parent _ (baseActor getChildNamed: parent).
+				newActor reparentTo: parent.
+				newActor becomePart.
+				texture ifNotNil:[newActor setTexture: texture].
+				meshFile ifNotNil:[newActor loadMeshFromFile: meshFile].
+				matrix ifNotNil:[newActor setComposite: matrix].
+				"end new actor with parent"
+			].
+		"end MakeObject parsing"
+		].
+
+	]. "end file parsing"
+
+	aFile close.
+	^ baseActor.
