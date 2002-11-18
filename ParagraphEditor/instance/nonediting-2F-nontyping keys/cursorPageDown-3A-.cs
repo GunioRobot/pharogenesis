1115d7ec13e31e0b9a9da086a1eb151abef0551a
th <unknown>
2002-11-18 17:09:00 +0000

    no message

--- ParagraphEditor/instance/nonediting-2F-nontyping keys/cursorPageDown-3A-.cs
+++ ParagraphEditor/instance/nonediting-2F-nontyping keys/cursorPageDown-3A-.cs
@@ -1,3 +1,12 @@
 cursorPageDown: characterStream 
 
-	^self cursorPageJump: characterStream down: true
+	self closeTypeIn: characterStream.
+	self 
+		moveCursor: [:position |
+			self
+				sameColumn: position
+				newLine:[:lineNo | lineNo + self pageHeight]
+				forward: true]
+		forward: true
+		specialBlock:[:dummy | dummy].
+	^true
