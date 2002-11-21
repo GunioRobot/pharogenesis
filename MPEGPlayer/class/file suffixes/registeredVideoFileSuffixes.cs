291b330c62ef202050e0c9c7c285febf45d3a9ac
bkv <unknown>
2002-11-21 11:14:00 +0000

    no message

---
+++ MPEGPlayer/class/file suffixes/registeredVideoFileSuffixes.cs
@@ -0,0 +1,7 @@
+registeredVideoFileSuffixes
+    "Answer the file extensions for which MPEGPlayer registers video services with FileList."
+     "MPEGPlayer registeredVideoFileSuffixes"
+
+     ^{ 'mpg'. 'mpeg'. 'jmv'. }
+
+	
