d07672d39a1472ad33857339f4c2940e48f65032
bkv <unknown>
2002-11-21 15:28:00 +0000

    no message

---
+++ MPEGPlayer/class/file suffixes/registeredAudioFileSuffixes.cs
@@ -0,0 +1,7 @@
+registeredAudioFileSuffixes
+    "Answer the file extensions for which MPEGPlayer registers audio services with FileList."
+     "MPEGPlayer registeredAudioFileSuffixes"
+
+     ^{ 'mp3'.  }
+
+	
