e1dc823c0d9b71c38f7977694e2f9ad47bc0c851
AFi <unknown>
2002-11-23 21:06:00 +0000

    no message

--- Float/instance/mathematical functions/raisedTo-3A-.cs
+++ Float/instance/mathematical functions/raisedTo-3A-.cs
@@ -5,7 +5,7 @@ raisedTo: aNumber
 		["Do the special case of integer power"
 		^ self raisedToInteger: aNumber].
 	self < 0.0 ifTrue:
-		[ self error: self printString, ' raised to a non-integer power' ].
+		[ ArithmeticError signal: ' raised to a non-integer power' ].
 	0.0 = aNumber ifTrue: [^ 1.0].				"special case for exponent = 0.0"
 	(self= 0.0) | (aNumber = 1.0) ifTrue: [^ self].	"special case for self = 1.0"
 	^ (self ln * aNumber asFloat) exp			"otherwise use logarithms"
