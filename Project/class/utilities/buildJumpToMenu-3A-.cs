7843f4a55c0a24f2b80f740b83cb3b10cbaa2776
sw <unknown>
2002-11-24 10:57:00 +0000

    no message

--- Project/class/utilities/buildJumpToMenu-3A-.cs
+++ Project/class/utilities/buildJumpToMenu-3A-.cs
@@ -3,34 +3,56 @@ buildJumpToMenu: menu
 		*	The previous-project chain
 		*	The next project, if any
 		*	The parent project, if any
-		*	All projects, alphabetically"
+		*	All projects, alphabetically or hierarchically"
 
 	| prev listed i next  toAdd |
 	listed _ OrderedCollection with: CurrentProject.
 	i _ 0.
+
+	"The previous Project chain"
 	prev _ CurrentProject previousProject.
 	[(prev ~~ nil and: [(listed includes: prev) not])] whileTrue:
-		[i _ i + 1.
+	  [i _ i + 1.
 		listed add: prev.
-		self addItem: prev name , ' (back ' , i printString , ')'
-					toMenu: menu selection: ('%back' , i printString) project: prev.
-				prev _ prev previousProject].
+		self 	addItem: prev name , ' (back ' , i printString , ')'
+				toMenu: menu 
+				selection: ('%back' , i printString) 
+				project: prev.
+		prev _ prev previousProject].
+	i > 0 ifTrue: [menu addLine].
+
+
+	"Then the next Project"
 	(((next _ CurrentProject nextProject) ~~ nil) and: [(listed includes: next) not]) ifTrue:
-		[self addItem: (next name, ' (forward 1)') toMenu: menu selection: next name project: next]. 
-	(i > 0 or: [next ~~ nil]) ifTrue: [menu addLine].
+		[self	addItem: (next name, ' (forward 1)') 
+				toMenu: menu 
+				selection: next name 
+				project: next]. 
+	next ~~ nil ifTrue: [menu addLine].
 
 	"Then the parent"
 	CurrentProject isTopProject ifFalse: 
-		[self addItem: CurrentProject parent name , ' (parent)' toMenu: menu selection: #parent project: CurrentProject parent.
-		menu addLine].
+		[self	addItem: CurrentProject parent name , ' (parent)' 
+				toMenu: menu 
+				selection: #parent 
+				project: CurrentProject parent.
+		  menu addLine].
 
-	"Finally all the projects, in alphabetical order"
-	Project allNamesAndProjects do:
+	"Finally all the projects, in hierarchical or alphabetical order:"
+	(Preferences alphabeticalProjectMenu
+			ifTrue:
+				[Project allNamesAndProjects]
+			ifFalse:
+				[Project hierarchyOfNamesAndProjects]) do:
+
 		[:aPair | 
 			toAdd _ aPair last isCurrentProject
 				ifTrue:
-					[aPair first, ' (current)']
+				  [aPair first, ' (current)']
 				ifFalse:
-					[aPair first].
-			self addItem: toAdd toMenu: menu selection: aPair first project: aPair last].
+				  [aPair first].
+			self	addItem: toAdd 
+				toMenu: menu 
+				selection: aPair first 
+				project: aPair last].
 	^ menu
