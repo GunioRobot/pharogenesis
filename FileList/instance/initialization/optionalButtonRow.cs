522ac018d80b3e5b8b793742812b4a3ffc6395c8
sw <unknown>
2002-11-30 14:36:00 +0000

    no message

--- FileList/instance/initialization/optionalButtonRow.cs
+++ FileList/instance/initialization/optionalButtonRow.cs
@@ -1,27 +1,17 @@
 optionalButtonRow
-	| aRow aButton |
-	aRow _ AlignmentMorph newRow beSticky.
-	aRow clipSubmorphs: true.
-	aRow addTransparentSpacerOfSize: (5@0).
-	self optionalButtonSpecs do:
-			[:spec |
-				aButton _ PluggableButtonMorph
-					on: self
-					getState: nil
-					action: spec second.
-				aButton
-					hResizing: #spaceFill;
-					vResizing: #spaceFill;
-					useRoundedCorners;
-					label: spec first asString;
-					askBeforeChanging: true;
-					onColor: Color transparent offColor: Color transparent.
-				aRow addMorphBack: aButton.
-				aRow addTransparentSpacerOfSize: (3 @ 0).
-				aButton setBalloonText: spec fourth.
-				aRow addTransparentSpacerOfSize: (3 @ 0).
+	"Answer the button row associated with a file list"
 
-				(spec second == #sortBySize)
+	| aRow |
+	aRow _ AlignmentMorph newRow beSticky.
+	aRow color: Color transparent.
+	aRow clipSubmorphs: true.
+	aRow layoutInset: 5@1; cellInset: 6.
+	self universalButtonServices do:  "just the three sort-by items"
+			[:service |
+				aRow addMorphBack: (service buttonToTriggerIn: self).
+				(service selector  == #sortBySize)
 					ifTrue:
 						[aRow addTransparentSpacerOfSize: (4@0)]].
+	aRow setNameTo: 'buttons'.
+	aRow setProperty: #buttonRow toValue: true.  "Used for dynamic retrieval later on"
 	^ aRow
