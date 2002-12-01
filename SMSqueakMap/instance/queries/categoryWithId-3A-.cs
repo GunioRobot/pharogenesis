9a88852a37b8fd7c8d68cfb372fbf61f9f5346e2
gh <unknown>
2002-12-01 20:10:00 +0000

    no message

---
+++ SMSqueakMap/instance/queries/categoryWithId-3A-.cs
@@ -0,0 +1,9 @@
+categoryWithId: anIdString 
+	"Look up a category. Return nil if missing.
+	Raise error if it is not a category."
+
+	| cat |
+	cat _ self objectWithId: anIdString.
+	cat ifNil: [^nil].
+	cat isCategory ifTrue:[^cat].
+	self error: 'UUID did not map to a category.'
