71f8e9ec67c34131bf03c4a99658801531b62703
JMM <unknown>
2002-12-09 22:16:00 +0000

    no message

--- ObjectMemory/instance/gc -2D--2D- compaction/mapPointersInObjectsFrom-3A-to-3A-.cs
+++ ObjectMemory/instance/gc -2D--2D- compaction/mapPointersInObjectsFrom-3A-to-3A-.cs
@@ -1,27 +1,10 @@
 mapPointersInObjectsFrom: memStart to: memEnd
-	"Use the forwarding table to update the pointers of all non-free objects in the given range of memory. Also remap pointers in root objects which may contains pointers into the given memory range."
+	"Use the forwarding table to update the pointers of all non-free objects in the given range of memory. Also remap pointers in root objects which may contains pointers into the given memory range, and don't forget to flush the method cache based on the range"
 
-	| oop |
 	self inline: false.
 	self compilerMapHookFrom: memStart to: memEnd.
 	"update interpreter variables"
 	self mapInterpreterOops.
-
-	"update pointers in root objects"
-	1 to: rootTableCount do: [ :i | 
-		oop _ rootTable at: i.
-		((oop < memStart) or: [oop >= memEnd]) ifTrue: [
-			"Note: must not remap the fields of any object twice!"
-			"remap this oop only if not in the memory range covered below"
-			self remapFieldsAndClassOf: oop.
-		].
-	].
-
-	"update pointers in the given memory range"
-	oop _ self oopFromChunk: memStart.
-	[oop < memEnd] whileTrue: [
-		(self isFreeObject: oop) ifFalse: [
-			self remapFieldsAndClassOf: oop.
-		].
-		oop _ self objectAfterWhileForwarding: oop.
-	].
+	self flushMethodCacheFrom: memStart to: memEnd.
+	self updatePointersInRootObjectsFrom: memStart to: memEnd.
+	self updatePointersInRangeFrom: memStart to: memEnd.
