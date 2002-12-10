d21f3eb947378259eae8c94fd65f3b482813c5de
yo <unknown>
2002-12-10 18:15:00 +0000

    no message

---
+++ TTCFont/instance/friend/installOn-3A-foregroundColor-3A-backgroundColor-3A-.cs
@@ -0,0 +1,5 @@
+installOn: aDisplayContext foregroundColor: fgColor backgroundColor: bgColor
+
+	foregroundColor _ fgColor.
+
+	aDisplayContext installTTCFont: self foregroundColor: foregroundColor backgroundColor: bgColor
