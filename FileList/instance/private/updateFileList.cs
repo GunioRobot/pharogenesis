8c91092c90222ea6b2ff2efee0259fbfec4de953
nk <unknown>
2002-12-10 07:57:00 +0000

    no message

--- FileList/instance/private/updateFileList.cs
+++ FileList/instance/private/updateFileList.cs
@@ -1,16 +1,23 @@
 updateFileList
-	"Update my files list with file names in the current directory that match the pattern."
-	"wod 5/27/1998: nil out the fileName."
-	Cursor execute showWhile:
-		[list _ (pattern includes: $*) | (pattern includes: $#)
-			ifTrue: [self listForPattern: pattern]
-			ifFalse: [
-				pattern isEmpty
-					ifTrue: [self listForPattern: '*']
-					ifFalse: [self listForPattern: '*', pattern, '*']].
-		listIndex _ 0.
-		volListIndex _ volList size.
-		fileName _ nil.
-		contents _ ''.
-		self changed: #volumeListIndex.
-		self changed: #fileList].
+	"Update my files list with file names in the current directory  
+	that match the pattern.
+	The pattern string may have embedded newlines or semicolons; these separate different patterns."
+	| patterns |
+	patterns _ OrderedCollection new.
+	Cursor wait showWhile: [
+	(pattern findTokens: (String with: Character cr with: Character lf with: $;))
+		do: [ :each |
+			(each includes: $*) | (each includes: $#)
+					ifTrue: [ patterns add: each]
+					ifFalse: [each isEmpty
+										ifTrue: [ patterns add: '*']
+										ifFalse: [ patterns add: '*' , each , '*']]].
+
+	list _ self listForPatterns: patterns.
+	listIndex _ 0.
+	volListIndex _ volList size.
+	fileName _ nil.
+	contents _ ''.
+	self changed: #volumeListIndex.
+	self changed: #fileList.
+	self updateButtonRow]
