8bb71ddb58c40fa8ea4445ec3071ce17f1933653
BG <unknown>
2002-12-13 15:32:00 +0000

    no message

--- FileList/instance/initialization/directory-3A-.cs
+++ FileList/instance/initialization/directory-3A-.cs
@@ -10,6 +10,7 @@ directory: dir
 	sortMode == nil ifTrue: [sortMode _ #date].
 	volList _ ((Array with: '[]'), directory pathParts)  "Nesting suggestion from RvL"
 			withIndexCollect: [:each :i | ( String new: i-1 withAll: $ ), each].
+	volListIndex := volList size.
 	self changed: #relabel.
 	self changed: #volumeList.
 	self pattern: pattern
