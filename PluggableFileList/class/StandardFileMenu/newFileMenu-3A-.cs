40188c639bdd93e9c42ae41ddabe2537def89d23
BG <unknown>
2002-12-13 15:31:00 +0000

    no message

--- PluggableFileList/class/StandardFileMenu/newFileMenu-3A-.cs
+++ PluggableFileList/class/StandardFileMenu/newFileMenu-3A-.cs
@@ -3,4 +3,5 @@ newFileMenu: aDirectory
 	"For compatibility with StandardFileMenu for now, answer a StandardFileMenuResult"
 	^(self getFilePathNameDialogWithExistenceCheck)
 		resultBlock: self sfmResultBlock;
+		directory: aDirectory;
 		yourself
