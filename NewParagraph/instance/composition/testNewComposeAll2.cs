b8d2083dd14d126ce3634a7af080668d7c6b8393
yo <unknown>
2002-12-17 14:48:00 +0000

    no message

---
+++ NewParagraph/instance/composition/testNewComposeAll2.cs
@@ -0,0 +1,14 @@
+testNewComposeAll2
+	| newResult |
+	newResult _ TextComposer new
+		composeLinesFrom: firstCharacterIndex 
+		to: text size 
+		delta: 0
+		into: OrderedCollection new 
+		priorLines: Array new 
+		atY: container top
+		textStyle: textStyle 
+		text: text 
+		container: container
+		wantsColumnBreaks: false.
+	^{newResult. {lines. maxRightX}}
