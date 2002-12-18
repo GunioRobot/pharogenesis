ac0623360c53f201fe96700ebdd602a2fb507e91
yo <unknown>
2002-12-18 15:00:00 +0000

    no message

---
+++ NewParagraph/instance/composition/testNewComposeAll3.cs
@@ -0,0 +1,14 @@
+testNewComposeAll3
+	| newResult |
+	newResult _ TextComposer new
+		multiComposeLinesFrom: firstCharacterIndex 
+		to: text size 
+		delta: 0
+		into: OrderedCollection new 
+		priorLines: Array new 
+		atY: container top
+		textStyle: textStyle 
+		text: text 
+		container: (0@0 extent: 31@60)
+		wantsColumnBreaks: false.
+	^{newResult. {lines. maxRightX}}
