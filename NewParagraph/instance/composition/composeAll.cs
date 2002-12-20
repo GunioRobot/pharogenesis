f4e9e80ec737e0891421b9db4e8c852c69de87c4
yo <unknown>
2002-12-20 16:18:00 +0000

    no message

--- NewParagraph/instance/composition/composeAll.cs
+++ NewParagraph/instance/composition/composeAll.cs
@@ -1,3 +1,8 @@
 composeAll
-	self composeLinesFrom: firstCharacterIndex to: text size delta: 0
-		into: OrderedCollection new priorLines: Array new atY: container top
+	text string isOctetString ifTrue: [
+		^ self composeLinesFrom: firstCharacterIndex to: text size delta: 0
+			into: OrderedCollection new priorLines: Array new atY: container top.
+	].
+
+	^ self multiComposeLinesFrom: firstCharacterIndex to: text size delta: 0
+		into: OrderedCollection new priorLines: Array new atY: container top.
