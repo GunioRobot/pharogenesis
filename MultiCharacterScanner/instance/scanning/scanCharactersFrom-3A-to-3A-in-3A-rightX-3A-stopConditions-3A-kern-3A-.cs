3d64e5ad25819a8a0462839dbe9205b55e656f98
yo <unknown>
2002-12-27 04:33:00 +0000

    no message

---
+++ MultiCharacterScanner/instance/scanning/scanCharactersFrom-3A-to-3A-in-3A-rightX-3A-stopConditions-3A-kern-3A-.cs
@@ -0,0 +1,13 @@
+scanCharactersFrom: startIndex to: stopIndex in: sourceString rightX: rightX stopConditions: stops kern: kernDelta
+
+	| startEncoding selector |
+	(sourceString isKindOf: String) ifTrue: [^ self basicScanCharactersFrom: startIndex to: stopIndex in: sourceString rightX: rightX stopConditions: stops kern: kernDelta.].
+
+	(sourceString isKindOf: MultiString) ifTrue: [
+		startIndex > stopIndex ifTrue: [lastIndex _ stopIndex. ^ stops at: EndOfRun].
+		startEncoding _  (sourceString at: startIndex) leadingChar.
+		selector _ (EncodedCharSet charsetAt: startEncoding) scanSelector.
+		^ self perform: selector withArguments: (Array with: startIndex with: stopIndex with: sourceString with: rightX with: stopConditions with: kernDelta).
+	].
+	
+	^ stops at: EndOfRun
