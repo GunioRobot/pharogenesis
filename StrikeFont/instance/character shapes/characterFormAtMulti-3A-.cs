53d8568393473cfb4ced826fcc43a909a4c17cd3
yo <unknown>
2002-12-28 22:37:00 +0000

    no message

---
+++ StrikeFont/instance/character shapes/characterFormAtMulti-3A-.cs
@@ -0,0 +1,8 @@
+characterFormAtMulti: character 
+	"Answer a Form copied out of the glyphs for the argument, character."
+	| ascii leftX rightX |
+	ascii _ character charCode.
+	(ascii between: minAscii and: maxAscii) ifFalse: [ascii _ maxAscii + 1].
+	leftX _ xTable at: ascii + 1.
+	rightX _ xTable at: ascii + 2.
+	^ glyphs copy: (leftX @ 0 corner: rightX @ self height)
