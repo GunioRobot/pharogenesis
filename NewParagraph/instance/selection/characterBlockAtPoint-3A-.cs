0d6bd67609db97cfdffa9d5abf9f6f5ce26623c1
yo <unknown>
2003-01-01 15:09:00 +0000

    no message

--- NewParagraph/instance/selection/characterBlockAtPoint-3A-.cs
+++ NewParagraph/instance/selection/characterBlockAtPoint-3A-.cs
@@ -2,6 +2,8 @@ characterBlockAtPoint: aPoint
 	"Answer a CharacterBlock for the character in the text at aPoint."
 	| line |
 	line _ lines at: (self lineIndexForPoint: aPoint).
-	^ (CharacterBlockScanner new text: text textStyle: textStyle)
+	^ ((text string isKindOf: MultiString) ifTrue: [
+		MultiCharacterBlockScanner new text: text textStyle: textStyle
+	] ifFalse: [CharacterBlockScanner new text: text textStyle: textStyle])
 		characterBlockAtPoint: aPoint index: nil
 		in: line
