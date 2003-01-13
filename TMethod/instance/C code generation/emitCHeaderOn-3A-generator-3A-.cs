49558ee2ab0d1ef6775d1c456ffc178b9f1bbaba
tpr <unknown>
2003-01-13 11:26:00 +0000

    no message

--- TMethod/instance/C code generation/emitCHeaderOn-3A-generator-3A-.cs
+++ TMethod/instance/C code generation/emitCHeaderOn-3A-generator-3A-.cs
@@ -1,9 +1,10 @@
 emitCHeaderOn: aStream generator: aCodeGen
 	"Emit a C function header for this method onto the given stream."
 
-	aStream cr.
+	aStream cr. 
 	self emitCFunctionPrototype: aStream generator: aCodeGen.
 	aStream nextPutAll: ' {'; cr.
+	self emitGlobalStructReferenceOn: aStream.
 	locals do: [ :var |
 		aStream nextPutAll: '    '.
 		aStream nextPutAll: (declarations at: var ifAbsent: [ 'int ', var]), ';'; cr.
