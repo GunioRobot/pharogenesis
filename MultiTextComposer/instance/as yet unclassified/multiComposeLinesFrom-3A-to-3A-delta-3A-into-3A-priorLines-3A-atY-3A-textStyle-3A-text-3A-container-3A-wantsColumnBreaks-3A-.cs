90ff6b413e8e0f1279ff6bf6edf2ec06402929cf
yo <unknown>
2003-01-16 17:30:00 +0000

    no message

---
+++ MultiTextComposer/instance/as yet unclassified/multiComposeLinesFrom-3A-to-3A-delta-3A-into-3A-priorLines-3A-atY-3A-textStyle-3A-text-3A-container-3A-wantsColumnBreaks-3A-.cs
@@ -0,0 +1,31 @@
+multiComposeLinesFrom: argStart to: argStop delta: argDelta into: argLinesCollection priorLines: argPriorLines atY: argStartY textStyle: argTextStyle text: argText container: argContainer wantsColumnBreaks: argWantsColumnBreaks
+
+	wantsColumnBreaks _ argWantsColumnBreaks.
+	lines _ argLinesCollection.
+	presentationLines _ argLinesCollection copy.
+	theTextStyle _ argTextStyle.
+	theText _ argText.
+	theContainer _ argContainer.
+	deltaCharIndex _ argDelta.
+	currCharIndex _ startCharIndex _ argStart.
+	stopCharIndex _ argStop.
+	prevLines _ argPriorLines.
+	currentY _ argStartY.
+	defaultLineHeight _ theTextStyle lineGrid.
+	maxRightX _ theContainer left.
+	possibleSlide _ stopCharIndex < theText size and: [theContainer isMemberOf: Rectangle].
+	nowSliding _ false.
+	prevIndex _ 1.
+	scanner _ MultiCompositionScanner new text: theText textStyle: theTextStyle.
+	scanner wantsColumnBreaks: wantsColumnBreaks.
+	isFirstLine _ true.
+	self composeAllLines.
+	isFirstLine ifTrue: ["No space in container or empty text"
+		self 
+			addNullLineWithIndex: startCharIndex
+			andRectangle: (theContainer topLeft extent: 0@defaultLineHeight)
+	] ifFalse: [
+		self fixupLastLineIfCR
+	].
+	^{lines asArray. maxRightX}
+
