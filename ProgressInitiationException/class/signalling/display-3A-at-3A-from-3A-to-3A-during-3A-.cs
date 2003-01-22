2f77c304b47831523e2880523cae01402a29071a
ajh <unknown>
2003-01-22 23:51:00 +0000

    no message

--- ProgressInitiationException/class/signalling/display-3A-at-3A-from-3A-to-3A-during-3A-.cs
+++ ProgressInitiationException/class/signalling/display-3A-at-3A-from-3A-to-3A-during-3A-.cs
@@ -1,5 +1,4 @@
 display: aString at: aPoint from: minVal to: maxVal during: workBlock
 
-	^self new
-		initialContext: thisContext sender;
+	^ self new
 		display: aString at: aPoint from: minVal to: maxVal during: workBlock
