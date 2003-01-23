864eb5bb7da29a3afa474a5debbb8f18e69199d5
yo <unknown>
2003-01-23 17:33:00 +0000

    no message

--- GrafPort/instance/accessing/displayScannerFor-3A-foreground-3A-background-3A-ignoreColorChanges-3A-.cs
+++ GrafPort/instance/accessing/displayScannerFor-3A-foreground-3A-background-3A-ignoreColorChanges-3A-.cs
@@ -1,5 +1,11 @@
 displayScannerFor: para foreground: foreColor background: backColor ignoreColorChanges: shadowMode
 
+	((para isMemberOf: MultiNewParagraph) or: [para text string class == String]) ifTrue: [
+		^ (MultiDisplayScanner new text: para text textStyle: para textStyle
+				foreground: foreColor background: backColor fillBlt: self
+				ignoreColorChanges: shadowMode)
+			setPort: self clone
+	].
 	^ (DisplayScanner new text: para text textStyle: para textStyle
 			foreground: foreColor background: backColor fillBlt: self
 			ignoreColorChanges: shadowMode)
