18828118f8f7f62bfb17b2eff9da6851099aa634
ajh <unknown>
2003-01-24 16:34:00 +0000

    no message

--- ContextPart/instance/instruction decoding/methodReturnTop.cs
+++ ContextPart/instance/instruction decoding/methodReturnTop.cs
@@ -2,4 +2,4 @@ methodReturnTop
 	"Simulate the action of a 'return top of stack' bytecode. This corresponds 
 	to source expressions like '^something'."
 
-	^self return: self pop to: self home sender
+	^ self return: self pop from: self home
