b18dc68733ed16f7fca8791def9e5f5eb21c5d5e
ajh <unknown>
2003-01-24 12:29:00 +0000

    no message

--- Debugger/instance/context stack menu/send.cs
+++ Debugger/instance/context stack menu/send.cs
@@ -1,20 +1,8 @@
 send
 	"Send the selected message in the accessed method, and take control in 
-	the method invoked to allow further step or 
-send."
+	the method invoked to allow further step or send."
 
-	| currentContext |
-	"Sensor leftShiftDown ifTrue: [self halt]."
 	self okToChange ifFalse: [^ self].
 	self checkContextSelection.
-	externalInterrupt ifFalse: [contextStackTop push: proceedValue].
-	externalInterrupt _ true. "simulation leaves same state as interrupting"
-	currentContext _ self selectedContext.
-	currentContext stepToSendOrReturn.
-	self contextStackIndex > 1 | currentContext willReturn
-		ifTrue: 
-			[self changed: #notChanged]
-		ifFalse: 
-			[currentContext _ currentContext step.
-			currentContext stepToSendOrReturn.
-			self resetContext: currentContext]
+	interruptedProcess step: self selectedContext.
+	self resetContext: interruptedProcess stepToSendOrReturn.
