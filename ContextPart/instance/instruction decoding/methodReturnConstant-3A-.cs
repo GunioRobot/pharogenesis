6d7da6abc00931f07a19447b267b5035a6a61d28
ajh <unknown>
2003-01-24 16:35:00 +0000

    no message

--- ContextPart/instance/instruction decoding/methodReturnConstant-3A-.cs
+++ ContextPart/instance/instruction decoding/methodReturnConstant-3A-.cs
@@ -2,4 +2,4 @@ methodReturnConstant: value
 	"Simulate the action of a 'return constant' bytecode whose value is the 
 	argument, value. This corresponds to a source expression like '^0'."
 
-	^self return: value to: self home sender
+	^ self return: value from: self home
