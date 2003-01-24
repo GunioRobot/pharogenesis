924da496bbab69c0eed50a262928501f0af00ce9
ajh <unknown>
2003-01-24 18:46:00 +0000

    no message

--- Encoder/instance/initialize-2D-release/nTemps-3A-literals-3A-class-3A-.cs
+++ Encoder/instance/initialize-2D-release/nTemps-3A-literals-3A-class-3A-.cs
@@ -1,7 +1,10 @@
 nTemps: n literals: lits class: cl 
 	"Decompile."
 
+	supered _ false.
 	class _ cl.
 	nTemps _ n.
 	literalStream _ ReadStream on: lits.
-	literalStream position: lits size
+	literalStream position: lits size.
+	sourceRanges _ Dictionary new: 32.
+	globalSourceRanges _ OrderedCollection new: 32.
