dce5423b04ae6c43b329416e867902c4ced3d89c
ajh <unknown>
2003-01-28 12:33:00 +0000

    no message

--- CompiledMethod/instance/evaluating/valueWithReceiver-3A-arguments-3A-.cs
+++ CompiledMethod/instance/evaluating/valueWithReceiver-3A-arguments-3A-.cs
@@ -1,6 +1,3 @@
 valueWithReceiver: aReceiver arguments: anArray 
-	| selector |
-	selector _ Symbol new.
-	aReceiver class addSelector: selector withMethod: self.
-	^ [aReceiver perform: selector withArguments: anArray]
-		ensure: [aReceiver class removeSelectorSimply: selector]
+
+	^ aReceiver withArgs: anArray executeMethod: self
