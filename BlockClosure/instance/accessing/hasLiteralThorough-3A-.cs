ba3d87684f28cbdb7cb20dc0e3a37b7f4e61ef1f
ajh <unknown>
2003-01-31 16:59:00 +0000

    no message

---
+++ BlockClosure/instance/accessing/hasLiteralThorough-3A-.cs
@@ -0,0 +1,5 @@
+hasLiteralThorough: literal
+	"Answer true if literal is identical to any literal imbedded in my method"
+
+	method == literal ifTrue: [^ true].
+	^ method hasLiteralThorough: literal
