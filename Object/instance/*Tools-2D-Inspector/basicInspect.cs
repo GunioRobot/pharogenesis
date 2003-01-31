a8266390dc52c2f3c25e0b7f8e87e5943c78e6ff
ajh <unknown>
2003-01-31 15:49:00 +0000

    no message

---
+++ Object/instance/*Tools-2D-Inspector/basicInspect.cs
@@ -0,0 +1,5 @@
+basicInspect
+	"Create and schedule an Inspector in which the user can examine the 
+	receiver's variables. This method should not be overriden."
+
+	BasicInspector openOn: self withEvalPane: false
