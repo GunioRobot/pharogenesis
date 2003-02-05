0c2799e080d9e471c29891473d74e8be2c337ecc
tpr <unknown>
2003-02-05 15:56:00 +0000

    no message

--- CCodeGenerator/instance/C code generator/emitCVariablesOn-3A-.cs
+++ CCodeGenerator/instance/C code generator/emitCVariablesOn-3A-.cs
@@ -1,16 +1,20 @@
-emitCVariablesOn: aStream
+emitCVariablesOn: aStream 
 	"Store the global variable declarations on the given stream."
 	| varString |
-	aStream nextPutAll: '/*** Variables ***/'; cr.
-	variables asSortedCollection do: [ :var |
-		(self isGeneratingPluginCode and:[self isTranslatingLocally])
-			ifTrue:[aStream nextPutAll:'static '].
-		varString _ var asString.
-		(variableDeclarations includesKey: varString) ifTrue: [
-			aStream nextPutAll: (variableDeclarations at: varString), ';'; cr.
-		] ifFalse: [
-			"default variable declaration"
-			aStream nextPutAll: 'int ', varString, ';'; cr.
-		].
-	].
-	aStream cr.
+	aStream nextPutAll: '/*** Variables ***/';
+		 cr.
+	variables asSortedCollection
+		do: [:var | 
+			varString _ var asString.
+			self isGeneratingPluginCode
+				ifTrue: [varString = 'interpreterProxy'
+						ifTrue: ["quite special..."
+							aStream cr; nextPutAll: '#ifdef SQUEAK_BUILTIN_PLUGIN'.
+							aStream cr; nextPutAll: 'extern'.
+							aStream cr; nextPutAll: '#endif'; cr]
+						ifFalse: [aStream nextPutAll: 'static ']].
+			(variableDeclarations includesKey: varString)
+				ifTrue: [aStream nextPutAll: (variableDeclarations at: varString) , ';'; cr]
+				ifFalse: ["default variable declaration"
+					aStream nextPutAll: 'int ' , varString , ';'; cr]].
+	aStream cr
