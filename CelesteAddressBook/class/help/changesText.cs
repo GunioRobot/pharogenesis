55ba5048f82fcaec960f758b6337c017f81691cb
gg <unknown>
2003-02-09 17:33:00 +0000

    no message

---
+++ CelesteAddressBook/class/help/changesText.cs
@@ -0,0 +1,27 @@
+changesText
+  ^'===== Release Notes =====
+===========================
+Version 1.3 -- Ticket To Ride
+	Reverted to DVS file format for a bug in the SAR Builder
+	Fixed a nasty bug on Windows: the db now is opend and 
+	compacted at every collection, so 
+	will always work and no spammers will go in your contatacs!
+	Button bar with a more rational ordering of buttons.
+	Added minimal size to the window (resolved resize bug of 1.2).
+	Added a nice "more info"	 feature on a email contacts and
+	auto-load of contacts if saved.
+	TODO: More rational use of CABContact. 
+	Try to add Minnestore Support?
+Version 1.2 -- Red Rock
+	First SAR-enabled version (thanks squeakers!).
+	Known Bugs: Resizing the window do not work well.
+Version 1.1 -- 
+	New search box and frequency filters (very userful)
+	Fixed the installation preamble (now we use the initialize method of CelesteAddressBook class for installing).
+	Best Parsing.
+	GUI Fixes
+Version 1.0 -- 16 Dec 2001
+	This is a first shot version. I have fixed some annoing bug of the alpha version.
+	The search box is unimplemented at the moment.
+	The CABContact class will model a Contact.
+gg 2/9/2003 17:19'		
