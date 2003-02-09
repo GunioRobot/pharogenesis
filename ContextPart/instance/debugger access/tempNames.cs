410094ed929c0db6c7171842907838912e463aad
ajh <unknown>
2003-02-09 12:25:00 +0000

    no message

--- ContextPart/instance/debugger access/tempNames.cs
+++ ContextPart/instance/debugger access/tempNames.cs
@@ -1,11 +1,5 @@
 tempNames
 	"Answer an OrderedCollection of the names of the receiver's temporary 
 	variables, which are strings."
-	| names |
-	self method setTempNamesIfCached: [:n | ^n].
-	names _ (self mclass compilerClass new
-			parse: self sourceCode
-			in: self mclass
-			notifying: nil) tempNames.
-	self method cacheTempNames: names.
-	^names
+
+	^ self methodNode tempNames
