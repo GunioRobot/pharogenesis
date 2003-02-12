0fd8ebe64cdce38041b611b0145ae0ac0e0511f9
nk <unknown>
2003-02-12 22:56:00 +0000

    no message

--- Debugger/instance/initialize/buttonRowForPreDebugWindow-3A-.cs
+++ Debugger/instance/initialize/buttonRowForPreDebugWindow-3A-.cs
@@ -1,17 +1,22 @@
 buttonRowForPreDebugWindow: aDebugWindow
-	| aRow aButton |
+	| aRow aButton quads |
 	aRow _ AlignmentMorph newRow hResizing: #spaceFill.
 	aRow beSticky.
-	aButton _ SimpleButtonMorph new target: aDebugWindow.
-	aButton color: Color transparent; borderWidth: 1.
 	aRow addMorphBack: AlignmentMorph newVariableTransparentSpacer.
-	self preDebugButtonQuads do:
+	quads _ OrderedCollection withAll: self preDebugButtonQuads.
+	(self interruptedContext selector == #doesNotUnderstand:) ifTrue: [
+		quads add: { 'Create'. #createMethod. #magenta. 'create the missing method' }
+	].
+	quads do:
 			[:quad |
-				aButton _ aButton fullCopy.
+				aButton _ SimpleButtonMorph new target: aDebugWindow.
+				aButton color: Color transparent; borderWidth: 1.
 				aButton actionSelector: quad second.
 				aButton label: quad first.
 				aButton submorphs first color: (Color colorFrom: quad third).
 				aButton setBalloonText: quad fourth.
+				Preferences alternativeWindowLook 
+					ifTrue:[aButton borderWidth: 2; borderColor: #raised].
 				aRow addMorphBack: aButton.
 				aRow addMorphBack: AlignmentMorph newVariableTransparentSpacer].
 	^ aRow
