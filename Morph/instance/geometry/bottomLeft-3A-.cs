3e4446b19608b3446f643d08913a43b730932e96
efc <unknown>
2003-02-13 18:08:00 +0000

    no message

--- Morph/instance/geometry/bottomLeft-3A-.cs
+++ Morph/instance/geometry/bottomLeft-3A-.cs
@@ -1,3 +1,4 @@
 bottomLeft: aPoint
-	self bottom: aPoint y.
-	self left: aPoint x
+	" Move me so that my bottom left corner is at aPoint. My extent (width & height) are unchanged "
+
+	self position: ((aPoint x) @ (aPoint y - self height)).
