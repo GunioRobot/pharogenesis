aafb990eae3012d66f57103c287aa67d8ce69569
dgd <unknown>
2003-02-14 20:17:00 +0000

    no message

--- EnvelopeLineMorph/instance/as yet unclassified/vertices-3A-borderWidth-3A-borderColor-3A-.cs
+++ EnvelopeLineMorph/instance/as yet unclassified/vertices-3A-borderWidth-3A-borderColor-3A-.cs
@@ -1,9 +1,9 @@
-vertices: verts borderWidth: bw borderColor: bc
+vertices: verts borderWidth: bw borderColor: bc 
 	super initialize.
 	vertices _ verts.
-	color _ Color transparent.
+	
 	borderWidth _ bw.
 	borderColor _ bc.
 	closed _ false.
 	arrows _ #none.
-	self computeBounds.
+	self computeBounds
