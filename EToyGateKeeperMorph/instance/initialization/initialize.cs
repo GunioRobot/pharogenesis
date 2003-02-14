06dcef9d413c27b5ecc1ff55efa8fed089661787
dgd <unknown>
2003-02-14 19:42:00 +0000

    no message

---
+++ EToyGateKeeperMorph/instance/initialization/initialize.cs
@@ -0,0 +1,10 @@
+initialize
+	"initialize the state of the receiver"
+	super initialize.
+	""
+	self listDirection: #topToBottom;
+		 layoutInset: 4;
+		 hResizing: #spaceFill;
+		 vResizing: #spaceFill;
+		 useRoundedCorners;
+		 rebuild 
