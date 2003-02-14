f90452d7816ccd754ace80d2010a21df1ffb3884
dgd <unknown>
2003-02-14 19:12:00 +0000

    no message

--- SameGameBoard/instance/initialization/initialize.cs
+++ SameGameBoard/instance/initialization/initialize.cs
@@ -1,4 +1,5 @@
 initialize
+	"initialize the state of the receiver"
 	super initialize.
 	target _ nil.
 	actionSelector _ #selection.
@@ -6,13 +7,11 @@ initialize
 	self layoutPolicy: nil.
 	self hResizing: #rigid.
 	self vResizing: #rigid.
-	borderWidth _ 2.
-	borderColor _ Color black.
 	rows _ self preferredRows.
 	columns _ self preferredColumns.
-	color _ Color gray.
+
 	palette _ (Color wheel: self preferredTileTypes + 1) asOrderedCollection.
 	flashColor _ palette removeLast.
 	flash _ false.
 	self extent: self protoTile extent * (columns @ rows).
-	self resetBoard.
+	self resetBoard
