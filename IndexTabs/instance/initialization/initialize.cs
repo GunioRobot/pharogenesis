cdbf5b117262ef0d37a1dfca24ff88f526b64a5a
dgd <unknown>
2003-02-14 21:43:00 +0000

    no message

--- IndexTabs/instance/initialization/initialize.cs
+++ IndexTabs/instance/initialization/initialize.cs
@@ -1,11 +1,11 @@
 initialize
-	"Initialize the receiver.  Make sure it is not open to drag and drop"
-
+	"Initialize the receiver. Make sure it is not open to drag and  
+	drop"
 	super initialize.
+	""
 	padding _ 10.
 	verticalPadding _ 4.
 	basicHeight _ 14.
 	basicWidth _ 200.
-	color _ Color r: 0.0 g: 0.6 b: 0.6.
-	borderWidth _ 0.
+	
 	self enableDragNDrop: false
