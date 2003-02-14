63ce889315cebef22b9bb3f84e4d4202fb82e732
dgd <unknown>
2003-02-14 21:39:00 +0000

    no message

--- FishEyeMorph/instance/initialization/initialize.cs
+++ FishEyeMorph/instance/initialization/initialize.cs
@@ -1,6 +1,8 @@
 initialize
+"initialize the state of the receiver"
 	super initialize.
+""
 	"magnification should be always 1"
 	magnification _ 1.
 	d _ 1.3.
-	self extent: 130@130.
+	self extent: 130 @ 130
