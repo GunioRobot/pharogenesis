8831a33548673a84a7da5444af5695023a5c22c3
dgd <unknown>
2003-02-14 19:54:00 +0000

    no message

---
+++ EToyProjectRenamerMorph/instance/initialization/initialize.cs
@@ -0,0 +1,9 @@
+initialize
+	"initialize the state of the receiver"
+	super initialize.
+	""
+	self vResizing: #shrinkWrap;
+	  hResizing: #shrinkWrap;
+	  layoutInset: 4;
+	  useRoundedCorners;
+	  rebuild
