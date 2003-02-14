a05a78a3f509ca785c4c3b4e9ef2280df7db6d75
dgd <unknown>
2003-02-14 19:48:00 +0000

    no message

--- AudioChatGUI/instance/initialization/initialize.cs
+++ AudioChatGUI/instance/initialization/initialize.cs
@@ -1,15 +1,12 @@
 initialize
-
+	"initialize the state of the receiver"
 	super initialize.
-
+	""
 	transmitWhileRecording _ false.
-	handsFreeTalking _ false.	
+	handsFreeTalking _ false.
 	mycodec _ GSMCodec new.
 	myrecorder _ ChatNotes new.
 	mytargetip _ ''.
-	color _ Color yellow.
-	borderWidth _ 4.
-	borderColor _ Color black.
-	self start2.
-	self changeTalkButtonLabel.
 	
+	self start2.
+	self changeTalkButtonLabel
