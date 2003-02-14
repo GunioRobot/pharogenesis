1ef3d1f15c297e9262ac73750890c6dc4bbbb4b4
dgd <unknown>
2003-02-14 20:18:00 +0000

    no message

--- LipsMorph/instance/initialization/initialize.cs
+++ LipsMorph/instance/initialization/initialize.cs
@@ -1,9 +1,9 @@
 initialize
+	"initialize the state of the receiver"
 	super initialize.
- 	self beSmoothCurve.
-	vertices _ {11@3. 35@1. 60@5. 67@17. 34@24. 3@17}.
-	color _ Color black. "red darker."
-	borderColor _ Color black.
-	borderWidth _ 1.
+	""
+	self beSmoothCurve.
+	vertices _ {11 @ 3. 35 @ 1. 60 @ 5. 67 @ 17. 34 @ 24. 3 @ 17}.
+	
 	closed _ true.
 	self neutral; updateShape
