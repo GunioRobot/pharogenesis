57e68c3569e377498772b4aea7f960c2d4ca7185
dgd <unknown>
2003-02-14 21:54:00 +0000

    no message

--- FatBitsPaint/instance/initialization/initialize.cs
+++ FatBitsPaint/instance/initialization/initialize.cs
@@ -1,11 +1,12 @@
 initialize
-
-        super initialize.
-        self setCurrentToolTo: self toolsForPaintBrush.
-        formToEdit _ Form extent: 50@40 depth: 8.
-        formToEdit fill: formToEdit boundingBox fillColor: Color veryVeryLightGray.
-        brushSize _ magnification _ 4.
-        color _ Color veryVeryLightGray.
-        brushColor _ Color red.
-        backgroundColor _ Color white.
-        self revert
+	"initialize the state of the receiver"
+	super initialize.
+	""
+	self setCurrentToolTo: self toolsForPaintBrush.
+	formToEdit _ Form extent: 50 @ 40 depth: 8.
+	formToEdit fill: formToEdit boundingBox fillColor: Color veryVeryLightGray.
+	brushSize _ magnification _ 4.
+	
+	brushColor _ Color red.
+	backgroundColor _ Color white.
+	self revert
