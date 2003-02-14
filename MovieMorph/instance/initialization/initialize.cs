a0c350f6c6201ae95beb16c4f238ba2b5908e8b4
dgd <unknown>
2003-02-14 21:47:00 +0000

    no message

--- MovieMorph/instance/initialization/initialize.cs
+++ MovieMorph/instance/initialization/initialize.cs
@@ -1,11 +1,13 @@
 initialize
-
+	"initialize the state of the receiver"
 	super initialize.
-	color _ (Color r: 1 g: 0 b: 1).
-	playMode _ #stop.  "#stop, #playOnce, or #loop"
+	""
+	
+	playMode _ #stop.
+	"#stop, #playOnce, or #loop"
 	msecsPerFrame _ 200.
 	rotationDegrees _ 0.
-	scalePoint _ 1.0@1.0.
+	scalePoint _ 1.0 @ 1.0.
 	frameList _ EmptyArray.
 	currentFrameIndex _ 1.
-	dwellCount _ 0.
+	dwellCount _ 0
