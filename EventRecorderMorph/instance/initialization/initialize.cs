b47fc0bf1b078d5ad485ed7e9bc787e9324fe651
dgd <unknown>
2003-02-14 19:21:00 +0000

    no message

--- EventRecorderMorph/instance/initialization/initialize.cs
+++ EventRecorderMorph/instance/initialization/initialize.cs
@@ -1,13 +1,13 @@
 initialize
+	"initialize the state of the receiver"
 	super initialize.
+	""
 	saved _ true.
-	borderWidth _ 2.
-	borderColor _ #raised.
-	color _ Color red.
-	self listDirection: #topToBottom.
-	self wrapCentering: #center; cellPositioning: #topCenter.
-	self hResizing: #shrinkWrap.
-	self vResizing: #shrinkWrap.
-	self layoutInset: 2.
-	self minCellSize: 4.
-	self addButtons.
+	self listDirection: #topToBottom;
+		 wrapCentering: #center;
+		 cellPositioning: #topCenter;
+		 hResizing: #shrinkWrap;
+		 vResizing: #shrinkWrap;
+		 layoutInset: 2;
+		 minCellSize: 4;
+		 addButtons
