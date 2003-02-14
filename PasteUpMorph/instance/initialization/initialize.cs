7f347b88ba9675ca951503389c26c91e4f9545b4
dgd <unknown>
2003-02-14 20:31:00 +0000

    no message

--- PasteUpMorph/instance/initialization/initialize.cs
+++ PasteUpMorph/instance/initialization/initialize.cs
@@ -1,12 +1,10 @@
 initialize
-	owner _ nil.
-	submorphs _ EmptyArray.
-	borderWidth _ 1.
-	borderColor _ Color r: 0.861 g: 1.0 b: 0.722.
-	color _ Color r: 0.8 g: 1.0 b: 0.6.
-	bounds _ 0@0 corner: 50@40.
+"initialize the state of the receiver"
+	super initialize.
+""
 	cursor _ 1.
 	padding _ 3.
 	self enableDragNDrop.
-	self isWorldMorph ifTrue: [self setProperty: #automaticPhraseExpansion toValue: true].
-	self clipSubmorphs: true.
+	self isWorldMorph
+		ifTrue: [self setProperty: #automaticPhraseExpansion toValue: true].
+	self clipSubmorphs: true
