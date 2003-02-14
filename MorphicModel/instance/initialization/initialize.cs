fd5449f56d8252917caa48189e8d0362defcb158
dgd <unknown>
2003-02-14 20:47:00 +0000

    no message

--- MorphicModel/instance/initialization/initialize.cs
+++ MorphicModel/instance/initialization/initialize.cs
@@ -1,6 +1,5 @@
 initialize
+	"initialize the state of the receiver"
 	super initialize.
-	open _ false.
-	bounds _ 0@0 corner: 200@100.
-	self color: Color transparent;
-		setBorderWidth: 2 borderColor: Color yellow
+""
+	open _ false
