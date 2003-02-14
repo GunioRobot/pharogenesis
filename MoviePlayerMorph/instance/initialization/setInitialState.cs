d7979e41cf422c8b839d955d939522895fcc524b
dgd <unknown>
2003-02-14 22:07:00 +0000

    no message

--- MoviePlayerMorph/instance/initialization/setInitialState.cs
+++ MoviePlayerMorph/instance/initialization/setInitialState.cs
@@ -1,8 +1,7 @@
 setInitialState
 	super setInitialState.
+""
 	self layoutInset: 3.
-	self borderWidth: 2.
-	self color: Color veryLightGray.
-	pageSize _ frameSize _ 200@200.
+	pageSize _ frameSize _ 200 @ 200.
 	frameDepth _ 8.
 	self disableDragNDrop
