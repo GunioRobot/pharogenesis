24e691f734490d44cc6bb5afe076ba1f7deb85f9
dgd <unknown>
2003-02-14 18:03:00 +0000

    no message

--- TwoWayScrollPane/instance/initialization/initialize.cs
+++ TwoWayScrollPane/instance/initialization/initialize.cs
@@ -1,20 +1,9 @@
 initialize
-	| |
+	"initialize the state of the receiver"
 	super initialize.
-	borderWidth _ 2.  borderColor _ #inset.
-
-	yScrollBar := ScrollBar new model: self slotName: 'yScrollBar'.
-	yScrollBar borderWidth: 2; borderColor: #inset.
-	self addMorph: yScrollBar.
-
-	xScrollBar := ScrollBar new model: self slotName: 'xScrollBar'.
-	xScrollBar borderWidth: 2; borderColor: #inset.
-	self addMorph: xScrollBar.
-
-	scroller := TransformMorph new color: Color transparent.
-	scroller offset: 0@0.
-	self addMorph: scroller.
-
-	self on: #mouseEnter send: #mouseEnter: to: self.
-	self on: #mouseLeave send: #mouseLeave: to: self.
-	self extent: 150@120
+	""
+	self addMorph: (yScrollBar _ self createScrollBarNamed: 'yScrollBar');
+		 addMorph: (xScrollBar _ self createScrollBarNamed: 'xScrollBar');
+		 addMorph: (scroller _ self createScroller).
+	""
+	self extent: 150 @ 120
