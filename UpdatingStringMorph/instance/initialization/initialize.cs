88efd147e91f9cdb6d69e7833c20d4d4600ec969
dgd <unknown>
2003-02-14 20:44:00 +0000

    no message

--- UpdatingStringMorph/instance/initialization/initialize.cs
+++ UpdatingStringMorph/instance/initialization/initialize.cs
@@ -1,11 +1,14 @@
 initialize
-
+	"Initialie the receiver to have default values in its instance 
+	variables "
 	super initialize.
-	format _ #default.  "formats: #string, #default"
-	target _ getSelector _ putSelector _  nil.
+""
+	format _ #default.
+	"formats: #string, #default"
+	target _ getSelector _ putSelector _ nil.
 	floatPrecision _ 1.
 	growable _ true.
 	stepTime _ 50.
 	autoAcceptOnFocusLoss _ true.
 	minimumWidth _ 8.
-	maximumWidth _ 300.
+	maximumWidth _ 300
