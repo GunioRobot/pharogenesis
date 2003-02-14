68e961c96cbebfa94e9c1d708322e941b1f5e6bb
dgd <unknown>
2003-02-14 21:49:00 +0000

    no message

--- FaceMorph/instance/initialization/initialize.cs
+++ FaceMorph/instance/initialization/initialize.cs
@@ -1,10 +1,11 @@
 initialize
+	"initialize the state of the receiver"
 	super initialize.
-	color _ Color transparent.
-	self addMorph: (leftEye _ EyeMorph new).
-	self addMorph: (rightEye _ EyeMorph new).
-	self addMorph: (lips _ LipsMorph new).
+	""
+	self addMorph: (leftEye _ EyeMorph new);
+	  addMorph: (rightEye _ EyeMorph new);
+	  addMorph: (lips _ LipsMorph new).
 	leftEye position: self position.
 	rightEye position: leftEye extent x @ 0 + leftEye position.
-	lips position: (0 @ 20 + (leftEye bottomRight + rightEye bottomLeft - lips extent // 2)).
+	lips position: 0 @ 20 + (leftEye bottomRight + rightEye bottomLeft - lips extent // 2).
 	self bounds: self fullBounds
