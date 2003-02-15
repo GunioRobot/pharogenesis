8ff160d932c1e0ef1931e018f1c2ae6696c7698b
aoy <unknown>
2003-02-15 21:38:00 +0000

    no message

--- BouncingAtomsMorph/instance/other/collisionPairs.cs
+++ BouncingAtomsMorph/instance/other/collisionPairs.cs
@@ -3,30 +3,28 @@ collisionPairs
 circles of known radius. This version uses the morph's positions--i.e.
 the top-left of their bounds rectangles--rather than their centers."
 
-	| count sortedAtoms radius twoRadii radiiSquared collisions p1 continue
-j p2 distSquared m1 m2 |
-	count _ submorphs size.
-	sortedAtoms _ submorphs asSortedCollection:
-		[ :mt1 :mt2 | mt1 position x < mt2 position x].
-	radius _ 8.
-	twoRadii _ 2 * radius.
-	radiiSquared _ radius squared * 2.
-	collisions _ OrderedCollection new.
-	1 to: count - 1 do: [ :i |
-		m1 _ sortedAtoms at: i.
-		p1 _ m1 position.
-		continue _ (j _ i + 1) <= count.
-		[continue] whileTrue: [
-			m2 _ sortedAtoms at: j.
-			p2 _ m2 position.
-			(p2 x - p1 x) <= twoRadii  ifTrue: [
-				distSquared _ (p1 x - p2 x) squared + (p1 y - p2 y) squared.
-				distSquared < radiiSquared ifTrue: [
-					collisions add: (Array with: m1 with: m2)].
-				continue _ (j _ j + 1) <= count.
-			] ifFalse: [
-				continue _ false.
-			].
-		].
-	].
-	^ collisions
+	| count sortedAtoms radius twoRadii radiiSquared collisions p1 continue j p2 distSquared m1 m2 |
+	count := submorphs size.
+	sortedAtoms := submorphs 
+				asSortedCollection: [:mt1 :mt2 | mt1 position x < mt2 position x].
+	radius := 8.
+	twoRadii := 2 * radius.
+	radiiSquared := radius squared * 2.
+	collisions := OrderedCollection new.
+	1 to: count - 1
+		do: 
+			[:i | 
+			m1 := sortedAtoms at: i.
+			p1 := m1 position.
+			continue := (j := i + 1) <= count.
+			[continue] whileTrue: 
+					[m2 := sortedAtoms at: j.
+					p2 := m2 position.
+					continue := p2 x - p1 x <= twoRadii  
+								ifTrue: 
+									[distSquared := (p1 x - p2 x) squared + (p1 y - p2 y) squared.
+									distSquared < radiiSquared 
+										ifTrue: [collisions add: (Array with: m1 with: m2)].
+									(j := j + 1) <= count]
+								ifFalse: [false]]].
+	^collisions
