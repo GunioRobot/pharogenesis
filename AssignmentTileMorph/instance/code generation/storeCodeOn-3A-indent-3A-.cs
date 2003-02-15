8fc49ddedea2cb39432bd0d4d5209f6fd0e6f065
aoy <unknown>
2003-02-15 21:09:00 +0000

    no message

--- AssignmentTileMorph/instance/code generation/storeCodeOn-3A-indent-3A-.cs
+++ AssignmentTileMorph/instance/code generation/storeCodeOn-3A-indent-3A-.cs
@@ -1,13 +1,16 @@
-storeCodeOn: aStream indent: tabCount
+storeCodeOn: aStream indent: tabCount 
 	"Generate code for an assignment statement.  The code generated looks presentable in the case of simple assignment, though the code generated for the increment/decrement/multiply cases is still the same old assignGetter... sort for now"
+aStream nextPutAll: (Utilities setterSelectorFor: assignmentRoot).
+			aStream space."Simple assignment, don't need existing value"
+	assignmentSuffix = ':' 
 
-	assignmentSuffix = ':'
-		ifTrue:   "Simple assignment, don't need existing value"
-			[aStream nextPutAll: (ScriptingSystem setterSelectorFor: assignmentRoot).
+		ifFalse: 
+			["Assignments that require that old values be retrieved"
+
+			
+			self assignmentReceiverTile storeCodeOn: aStream indent: tabCount.
+			aStream space.
+			aStream nextPutAll: (Utilities getterSelectorFor: assignmentRoot).
+			aStream space.
+			aStream nextPutAll: (self operatorForAssignmentSuffix: assignmentSuffix).
 			aStream space]
-		ifFalse:  "Assignments that require that old values be retrieved"
-			[aStream nextPutAll: ' assign', (assignmentSuffix copyWithout: $:), 'Getter: #'.
-			aStream nextPutAll: (ScriptingSystem getterSelectorFor: assignmentRoot).
-			aStream nextPutAll: ' setter: #'.
-			aStream nextPutAll: (ScriptingSystem setterSelectorFor: assignmentRoot).
-			aStream nextPutAll: ' amt: ']
