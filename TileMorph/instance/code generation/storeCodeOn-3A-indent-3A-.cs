a4714505b7cbef32caae8210fda34574d1293bf0
aoy <unknown>
2003-02-15 21:29:00 +0000

    no message

--- TileMorph/instance/code generation/storeCodeOn-3A-indent-3A-.cs
+++ TileMorph/instance/code generation/storeCodeOn-3A-indent-3A-.cs
@@ -1,47 +1,52 @@
-storeCodeOn: aStream indent: tabCount
+storeCodeOn: aStream indent: tabCount 
 	"Store code representing the receiver onto the stream, with the given amount of indentation"
 
 	| op playerBearingCode |
-	playerBearingCode _ self playerBearingCode.  "Must determine whom is scripted for what follows to work; if it's ever nil, we've got trouble"
-	type = #expression ifTrue:
-		[^ aStream
-			nextPut: $(;
-			nextPutAll: operatorOrExpression;
-			nextPut: $)].
+	playerBearingCode := self playerBearingCode.	"Must determine whom is scripted for what follows to work; if it's ever nil, we've got trouble"
+	type = #expression 
+		ifTrue: 
+			[^aStream
+				nextPut: $(;
+				nextPutAll: operatorOrExpression;
+				nextPut: $)].
+	type = #literal 
+		ifTrue: 
+			[^aStream
+				nextPut: $(;
+				nextPutAll: literal printString;
+				nextPut: $)].
+	type == #objRef 
+		ifTrue: 
+			[^playerBearingCode == actualObject 
+				ifTrue: 
+					["If the object is the method's own 'self' then we MUST, rather than just MAY, put out 'self' rather than the referencer call, though the latter will temporarily work if only one instance of the uniclass exists."
 
-	type = #literal ifTrue:
-		[^ aStream
-			nextPut: $(;
-			nextPutAll: literal printString;
-			nextPut: $)].
+					aStream nextPutAll: 'self']
+				ifFalse: 
+					[Preferences capitalizedReferences 
+						ifTrue: 
+							["Global dictionary References"
 
-	type == #objRef ifTrue:
-		[^ (playerBearingCode == actualObject)
-			ifTrue:
-				["If the object is the method's own 'self' then we MUST, rather than just MAY, put out 'self' rather than the referencer call, though the latter will temporarily work if only one instance of the uniclass exists."
-				aStream nextPutAll: 'self']
-			ifFalse:
-				[Preferences capitalizedReferences "Global dictionary References"
-					ifTrue:
-						[self flag: #deferred.  "Start deploying the meesage-receiver hints soon"
-						aStream nextPutAll: actualObject uniqueNameForReference]
-
-					ifFalse:
-						"old class-inst-var-based scheme used  Feb 1998 to Oct 2000, and indeed
+							self flag: #deferred.	"Start deploying the meesage-receiver hints soon"
+							aStream nextPutAll: actualObject uniqueNameForReference]
+						ifFalse: 
+							["old class-inst-var-based scheme used  Feb 1998 to Oct 2000, and indeed
 						ongoing in school year 2000-01 at the open school"
-						[aStream nextPutAll: 'self class '.
-						aStream nextPutAll: (playerBearingCode class referenceSelectorFor: actualObject)]]].
 
-	type = #operator ifTrue:
-		[((UpdatingOperators includesKey: operatorOrExpression) and:
-		 [self precedingTileType = #slotRef])
-			ifTrue: [op _ UpdatingOperators at: operatorOrExpression]
-			ifFalse: [op _ operatorOrExpression].
-		^ op isEmpty
-			ifTrue: [aStream position: aStream position - 1]
-			ifFalse: [aStream nextPutAll: op]].
+							aStream nextPutAll: 'self class '.
+							aStream 
+								nextPutAll: (playerBearingCode class referenceSelectorFor: actualObject)]]].
+	type = #operator 
+		ifTrue: 
+			[op := ((UpdatingOperators includesKey: operatorOrExpression) 
+				and: [self precedingTileType = #slotRef]) 
+					ifTrue: [UpdatingOperators at: operatorOrExpression]
+					ifFalse: [operatorOrExpression].
+			^op isEmpty 
+				ifTrue: [aStream position: aStream position - 1]
+				ifFalse: [aStream nextPutAll: op]]
 
-"The following branch has long been disused
+	"The following branch has long been disused
 	type = #slotRef ifTrue:
 		[self isThisEverCalled.
 		refType _ self slotRefType.
