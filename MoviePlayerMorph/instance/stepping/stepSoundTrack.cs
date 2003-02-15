4bdbe3840ea16c233cd3634fec429285191ed58f
aoy <unknown>
2003-02-15 21:45:00 +0000

    no message

--- MoviePlayerMorph/instance/stepping/stepSoundTrack.cs
+++ MoviePlayerMorph/instance/stepping/stepSoundTrack.cs
@@ -1,15 +1,21 @@
 stepSoundTrack
-
 	| x image timeInMillisecs |
-	scorePlayer ifNil: [^ self].
-	soundTrackForm ifNil: [^ self].
-	playDirection = 0
-		ifTrue: ["Stepping forward or back"
-				timeInMillisecs _ frameNumber - 1 * msPerFrame]
-		ifFalse: ["Driven by sound track"
-				timeInMillisecs _ scorePlayer millisecondsSinceStart].
-	x _ timeInMillisecs / 1000.0 * scorePlayer originalSamplingRate // 250.
-	image _ soundTrackMorph image.
-	image copy: (image boundingBox translateBy: (x - (image width//2)) @ 0)
-			from: soundTrackForm to: 0@0 rule: Form over.
-	soundTrackMorph changed.
+	scorePlayer ifNil: [^self].
+	soundTrackForm ifNil: [^self].
+	timeInMillisecs := playDirection = 0 
+		ifTrue: 
+			["Stepping forward or back"
+
+			 (frameNumber - 1) * msPerFrame]
+		ifFalse: 
+			["Driven by sound track"
+
+			 scorePlayer millisecondsSinceStart].
+	x := timeInMillisecs / 1000.0 * scorePlayer originalSamplingRate // 250.
+	image := soundTrackMorph image.
+	image 
+		copy: (image boundingBox translateBy: (x - (image width // 2)) @ 0)
+		from: soundTrackForm
+		to: 0 @ 0
+		rule: Form over.
+	soundTrackMorph changed
