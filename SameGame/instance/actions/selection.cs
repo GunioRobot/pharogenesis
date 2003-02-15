f7cce542f6eb7589f73bf54b6dfb47509a000340
aoy <unknown>
2003-02-15 21:40:00 +0000

    no message

--- SameGame/instance/actions/selection.cs
+++ SameGame/instance/actions/selection.cs
@@ -2,14 +2,15 @@ selection
 	"a selection was made on the board; get its count and update the displays"
 
 	| count score |
-	count _ self board selectionCount.
-	count = 0 ifTrue:
-		[score _ scoreDisplay value + (selectionDisplay value - 2) squared.
-		board findSelection ifNil:
-			[count _ board tilesRemaining.
-			count = 0
-				ifTrue: [score _ score + (5 * board rows * board columns)]
-				ifFalse: [score _ score - count].
-			scoreDisplay flash: true].
-		scoreDisplay value: score].
-	selectionDisplay value: count.
+	count := self board selectionCount.
+	count = 0 
+		ifTrue: 
+			[score := scoreDisplay value + (selectionDisplay value - 2) squared.
+			board findSelection ifNil: 
+					[count := board tilesRemaining.
+					score := count = 0 
+						ifTrue: [score + (5 * board rows * board columns)]
+						ifFalse: [score - count].
+					scoreDisplay flash: true].
+			scoreDisplay value: score].
+	selectionDisplay value: count
