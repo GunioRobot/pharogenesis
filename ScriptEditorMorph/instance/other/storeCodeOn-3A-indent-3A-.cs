6c1344b69e35bb9c2c78a8e3b0e5ca2fa09fabbf
aoy <unknown>
2003-02-15 21:13:00 +0000

    no message

--- ScriptEditorMorph/instance/other/storeCodeOn-3A-indent-3A-.cs
+++ ScriptEditorMorph/instance/other/storeCodeOn-3A-indent-3A-.cs
@@ -1,18 +1,23 @@
-storeCodeOn: aStream indent: tabCount
-
+storeCodeOn: aStream indent: tabCount 
 	| lastOwner |
-	lastOwner _ nil.
-	self tileRows do: [:r |
-		r do: [:m |
-			((m isKindOf: TileMorph) or:
-			 [(m isKindOf: CompoundTileMorph) or:
-			 [m isKindOf: PhraseTileMorph]]) ifTrue: [
-				tabCount timesRepeat: [aStream tab].
-				((m owner ~= lastOwner) and: [lastOwner ~= nil])
-					ifTrue:
-						[aStream nextPut: $.; cr; tab.
-						m storeCodeOn: aStream indent: tabCount]
-					ifFalse:
-						[(lastOwner ~= nil) ifTrue: [aStream space].
-						m storeCodeOn: aStream indent: tabCount].
-				lastOwner _ m owner]]].
+	lastOwner := nil.
+	self tileRows do: 
+			[:r | 
+			r do: 
+					[:m | 
+					((m isKindOf: TileMorph) 
+						or: [(m isKindOf: CompoundTileMorph) or: [m isKindOf: PhraseTileMorph]]) 
+							ifTrue: 
+								[tabCount timesRepeat: [aStream tab].
+								(m owner ~= lastOwner and: [lastOwner ~= nil]) 
+									ifTrue: 
+										[aStream
+											nextPut: $.;
+											cr;
+											tab.
+										]
+									ifFalse: 
+										[lastOwner ~= nil ifTrue: [aStream space].
+										].
+								m storeCodeOn: aStream indent: tabCount.
+								lastOwner := m owner]]]
