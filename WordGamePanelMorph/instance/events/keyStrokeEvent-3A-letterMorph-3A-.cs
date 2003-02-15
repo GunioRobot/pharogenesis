8401ca0cefde6a998b59fc9c018ef4d363455112
aoy <unknown>
2003-02-15 21:25:00 +0000

    no message

--- WordGamePanelMorph/instance/events/keyStrokeEvent-3A-letterMorph-3A-.cs
+++ WordGamePanelMorph/instance/events/keyStrokeEvent-3A-letterMorph-3A-.cs
@@ -1,27 +1,33 @@
-keyStrokeEvent: evt letterMorph: morph
+keyStrokeEvent: evt letterMorph: morph 
 	"Handle typing.  Calls keyCharacter:atIndex:nextFocus: for further behavior."
 
 	| affectedMorph keyCharacter nextFocus |
-	evt keyCharacter = Character backspace
-		ifTrue: ["<delete> zaps the current selection if there has been no typing,
+	evt keyCharacter = Character backspace 
+		ifTrue: 
+			["<delete> zaps the current selection if there has been no typing,
 				but it zaps the previous selection if there has been prior typing."
-				haveTypedHere
-					ifTrue: [affectedMorph _ morph previousTypeableLetter]
-					ifFalse: [affectedMorph _ morph].
-				keyCharacter _ Character space.
-				nextFocus _ morph previousTypeableLetter]
-		ifFalse: [affectedMorph _ morph.
-				keyCharacter _ evt keyCharacter asUppercase.
-				(keyCharacter isLetter or: [keyCharacter = Character space])
-					ifFalse: [^ self].
-				haveTypedHere _ true.
-				nextFocus _ morph nextTypeableLetter.
-				nextFocus == morph ifTrue:
+
+			affectedMorph := haveTypedHere 
+						ifTrue: [morph previousTypeableLetter]
+						ifFalse: [morph]. 
+			keyCharacter := Character space.
+			nextFocus := morph previousTypeableLetter]
+		ifFalse: 
+			[affectedMorph := morph.
+			keyCharacter := evt keyCharacter asUppercase.
+			(keyCharacter isLetter or: [keyCharacter = Character space]) 
+				ifFalse: [^self].
+			haveTypedHere := true.
+			nextFocus := morph nextTypeableLetter.
+			nextFocus == morph 
+				ifTrue: 
 					["If hit end of a word, change backspace mode"
-					haveTypedHere _ false]].
+
+					haveTypedHere := false]].
 	evt hand newKeyboardFocus: nextFocus.
 	self unhighlight.
 	nextFocus color: Color green.
-
-	self keyCharacter: keyCharacter atIndex: affectedMorph indexInQuote nextFocus: nextFocus
-
+	self 
+		keyCharacter: keyCharacter
+		atIndex: affectedMorph indexInQuote
+		nextFocus: nextFocus
