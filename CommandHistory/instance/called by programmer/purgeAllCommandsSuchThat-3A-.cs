ca05f04712d9c0025e9aa46854f107e63a1ada8f
aoy <unknown>
2003-02-15 21:14:00 +0000

    no message

--- CommandHistory/instance/called by programmer/purgeAllCommandsSuchThat-3A-.cs
+++ CommandHistory/instance/called by programmer/purgeAllCommandsSuchThat-3A-.cs
@@ -1,8 +1,6 @@
-purgeAllCommandsSuchThat: cmdBlock
+purgeAllCommandsSuchThat: cmdBlock 
 	"Remove a bunch of commands, as in [:cmd | cmd undoTarget == zort]"
 
-	Preferences useUndo ifFalse: [^ self].
-	history _ history reject: cmdBlock.
-	history isEmpty
-		ifTrue: [lastCommand _ nil]
-		ifFalse: [lastCommand _ history last]
+	Preferences useUndo ifFalse: [^self].
+	history := history reject: cmdBlock.
+	lastCommand := history isEmpty ifTrue: [nil] ifFalse: [history last] 
