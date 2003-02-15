cec70bec0c73bbb4bf8b0a3496784c05d0512f0e
aoy <unknown>
2003-02-15 21:16:00 +0000

    no message

--- ScaleMorph/instance/drawing/drawMinorTicksOn-3A-.cs
+++ ScaleMorph/instance/drawing/drawMinorTicksOn-3A-.cs
@@ -1,32 +1,30 @@
 drawMinorTicksOn: aCanvas 
 	| scale x1 y1 y2 x loopStart yoffset randomLabel |
-	scale _ self innerBounds width - 1 / (stop - start) asFloat.
-	majorTickLength < 0
-		ifTrue: [yoffset _ majorTickLength abs + 1]
-		ifFalse: [yoffset _ 1].
-	caption
-		ifNotNil: [captionAbove
+	scale := (self innerBounds width - 1) / (stop - start) asFloat.
+	yoffset := majorTickLength < 0 
+				ifTrue: [majorTickLength abs + 1]
+				ifFalse: [1]. 
+	caption ifNotNil: 
+			[captionAbove 
 				ifFalse: 
-					[randomLabel _ StringMorph contents: 'Foo'.
-					yoffset _ yoffset + randomLabel height + 2]].
-	tickPrintBlock
-		ifNotNil: [labelsAbove
+					[randomLabel := StringMorph contents: 'Foo'.
+					yoffset := yoffset + randomLabel height + 2]].
+	tickPrintBlock ifNotNil: 
+			[labelsAbove 
 				ifFalse: 
-					[randomLabel _ StringMorph contents: '50'.
-					yoffset _ yoffset + randomLabel height + 2]].
-	x1 _ self innerBounds left.
-	y1 _ self innerBounds bottom - yoffset.
-	y2 _ y1 - minorTickLength.
-	loopStart _ (start / minorTick) ceiling * minorTick.
-	loopStart
-		to: stop
+					[randomLabel := StringMorph contents: '50'.
+					yoffset := yoffset + randomLabel height + 2]].
+	x1 := self innerBounds left.
+	y1 := self innerBounds bottom - yoffset.
+	y2 := y1 - minorTickLength.
+	loopStart := (start / minorTick) ceiling * minorTick.
+	loopStart to: stop
 		by: minorTick
 		do: 
 			[:v | 
-			x _ x1 + (scale * (v - start)).
-			aCanvas
+			x := x1 + (scale * (v - start)).
+			aCanvas 
 				line: x @ y1
 				to: x @ y2
 				width: 1
-				color: Color black].
-	
+				color: Color black]
