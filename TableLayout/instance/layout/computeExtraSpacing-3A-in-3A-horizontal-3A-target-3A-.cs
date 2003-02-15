1d42563d3f57a2fca2d3f382957cbf4c8dac69af
aoy <unknown>
2003-02-15 20:49:00 +0000

    no message

--- TableLayout/instance/layout/computeExtraSpacing-3A-in-3A-horizontal-3A-target-3A-.cs
+++ TableLayout/instance/layout/computeExtraSpacing-3A-in-3A-horizontal-3A-target-3A-.cs
@@ -1,95 +1,136 @@
-computeExtraSpacing: arrangement in: newBounds horizontal: aBool target: aMorph
+computeExtraSpacing: arrangement in: newBounds horizontal: aBool target: aMorph 
 	"Compute the required extra spacing for laying out the cells"
-	| extent extra centering n extraPerCell cell last hFill vFill max amount allow |
+
 	"match newBounds extent with arrangement's orientation"
-	extent _ newBounds extent.
-	aBool ifFalse:[extent _ extent transposed].
+
+	| extent extra centering n extraPerCell cell last hFill vFill max amount allow |
+	extent := newBounds extent.
+	aBool ifFalse: [extent := extent transposed].
 
 	"figure out if we have any horizontal or vertical space fillers"
-	hFill _ vFill _ false.
-	max _ 0@0.
-	arrangement do:[:c|
-		max _ (max x max: c cellSize x) @ (max y + c cellSize y).
-		max _ max max: c cellSize.
-		hFill _ hFill or:[c hSpaceFill].
-		vFill _ vFill or:[c vSpaceFill]].
+	hFill := vFill := false.
+	max := 0 @ 0.
+	arrangement do: 
+			[:c | 
+			max := (max x max: c cellSize x) @ (max y + c cellSize y).
+			max := max max: c cellSize.
+			hFill := hFill or: [c hSpaceFill].
+			vFill := vFill or: [c vSpaceFill]].
 
 	"Take client's shrink wrap constraints into account.
 	Note: these are only honored when there are no #spaceFill children,
 	or when #rubberBandCells is set."
-	allow _ properties rubberBandCells not.
-	aMorph hResizing == #shrinkWrap ifTrue:[
-		aBool 
-			ifTrue:[allow & hFill ifFalse:[extent _ max x @ (max y max: extent y)]]
-			ifFalse:[allow & vFill ifFalse:[extent _ (max x max: extent x) @ max y]]].
-	aMorph vResizing == #shrinkWrap ifTrue:[
-		aBool 
-			ifFalse:[allow & hFill ifFalse:[extent _ max x @ (max y max: extent y)]]
-			ifTrue:[allow & vFill ifFalse:[extent _ (max x max: extent x) @ max y]]].
+	allow := properties rubberBandCells not.
+	aMorph hResizing == #shrinkWrap 
+		ifTrue: 
+			[aBool 
+				ifTrue: [allow & hFill ifFalse: [extent := max x @ (max y max: extent y)]]
+				ifFalse: [allow & vFill ifFalse: [extent := (max x max: extent x) @ max y]]].
+	aMorph vResizing == #shrinkWrap 
+		ifTrue: 
+			[aBool 
+				ifFalse: [allow & hFill ifFalse: [extent := max x @ (max y max: extent y)]]
+				ifTrue: [allow & vFill ifFalse: [extent := (max x max: extent x) @ max y]]].
 
 	"Now compute the extra v space"
-	extra _ extent y - (arrangement inject: 0 into:[:sum :c| sum + c cellSize y]).
-	extra > 0 ifTrue:[
-		"Check if we have any #spaceFillers"
-		vFill ifTrue:[ "use only #spaceFillers"
-			n _ arrangement inject: 0 into:[:sum :c| 
-				c vSpaceFill ifTrue:[sum + 1] ifFalse:[sum]].
-			n isZero ifFalse:[extraPerCell _ extra asFloat / n asFloat].
-			extra _ last _ 0.
-			arrangement do:[:c|
-				c vSpaceFill ifTrue:[
-					extra _ (last _ extra) + extraPerCell.
-					amount _ 0 @ (extra truncated - last truncated).
-					c do:[:cc| cc cellSize: cc cellSize + amount]]].
-		] ifFalse:[ "no #spaceFillers; distribute regularly"
-			centering _ properties wrapCentering.
-			"centering == #topLeft ifTrue:[]." "add all extra space to the last cell; e.g., do nothing"
-			centering == #bottomRight "add all extra space to the first cell"
-				ifTrue:[arrangement first addExtraSpace: 0@extra].
-			centering == #center "add 1/2 extra space to the first and last cell"
-				ifTrue:[arrangement first addExtraSpace: 0@ (extra // 2)].
-			centering == #justified "add extra space equally distributed to each cell"
-				ifTrue:[	n _ (arrangement size - 1) max: 1.
-						extraPerCell _ extra asFloat / n asFloat.
-						extra _ last _ 0.
-						arrangement do:[:c|
-							c addExtraSpace: 0@(extra truncated - last truncated).
-							extra _ (last _ extra) + extraPerCell]].
-		].
-	].
+	extra := extent y 
+				- (arrangement inject: 0 into: [:sum :c | sum + c cellSize y]).
+	extra > 0 
+		ifTrue: 
+			["Check if we have any #spaceFillers"
+
+			vFill 
+				ifTrue: 
+					["use only #spaceFillers"
+
+					n := arrangement inject: 0
+								into: [:sum :c | c vSpaceFill ifTrue: [sum + 1] ifFalse: [sum]].
+					n isZero ifFalse: [extraPerCell := extra asFloat / n asFloat].
+					extra := last := 0.
+					arrangement do: 
+							[:c | 
+							c vSpaceFill 
+								ifTrue: 
+									[extra := (last := extra) + extraPerCell.
+									amount := 0 @ (extra truncated - last truncated).
+									c do: [:cc | cc cellSize: cc cellSize + amount]]]]
+				ifFalse: 
+					["no #spaceFillers; distribute regularly"
+
+					centering := properties wrapCentering.
+					"centering == #topLeft ifTrue:[]."	"add all extra space to the last cell; e.g., do nothing"
+					centering == #bottomRight 
+						ifTrue: 
+							["add all extra space to the first cell"
+
+							arrangement first addExtraSpace: 0 @ extra].
+					centering == #center 
+						ifTrue: 
+							["add 1/2 extra space to the first and last cell"
+
+							arrangement first addExtraSpace: 0 @ (extra // 2)].
+					centering == #justified 
+						ifTrue: 
+							["add extra space equally distributed to each cell"
+
+							n := arrangement size - 1 max: 1.
+							extraPerCell := extra asFloat / n asFloat.
+							extra := last := 0.
+							arrangement do: 
+									[:c | 
+									c addExtraSpace: 0 @ (extra truncated - last truncated).
+									extra := (last := extra) + extraPerCell]]]].
 
 	"Now compute the extra space for the primary direction"
-	centering _ properties listCentering.
-	1 to: arrangement size do:[:i|
-		cell _ (arrangement at: i).
-		extra _ extent x - cell cellSize x.
-		extra > 0 ifTrue:[
-			"Check if we have any #spaceFillers"
-			cell hSpaceFill ifTrue:[ "use only #spaceFillers"
-				cell _ cell nextCell.
-				n _ cell inject: 0 into:[:sum :c| 
-					c hSpaceFill ifTrue:[sum + c target spaceFillWeight] ifFalse:[sum]].
-				n isZero ifFalse:[extraPerCell _ extra asFloat / n asFloat].
-				extra _ last _ 0.
-				cell do:[:c|
-					c hSpaceFill ifTrue:[
-						extra _ (last _ extra) + (extraPerCell * c target spaceFillWeight).
-						amount _ extra truncated - last truncated.
-						c cellSize: c cellSize + (amount@0)]].
-			] ifFalse:[ "no #spaceFiller; distribute regularly"
-				cell _ cell nextCell.
-				"centering == #topLeft ifTrue:[]" "add all extra space to the last cell; e.g., do nothing"
-				centering == #bottomRight "add all extra space to the first cell"
-					ifTrue:[cell addExtraSpace: extra @ 0].
-				centering == #center "add 1/2 extra space to the first and last cell"
-					ifTrue:[cell addExtraSpace: (extra // 2) @ 0].
-				centering == #justified "add extra space equally distributed to each cell"
-					ifTrue:[	n _ (cell size - 1) max: 1.
-							extraPerCell _ extra asFloat / n asFloat.
-							extra _ last _ 0.
-							cell do:[:c|
-								c addExtraSpace: (extra truncated - last truncated) @ 0.
-								extra _ (last _ extra) + extraPerCell]].
-			].
-		].
-	].
+	centering := properties listCentering.
+	1 to: arrangement size
+		do: 
+			[:i | 
+			cell := arrangement at: i.
+			extra := extent x - cell cellSize x.
+			extra > 0 
+				ifTrue: 
+					["Check if we have any #spaceFillers"
+					cell := cell nextCell.
+					cell hSpaceFill 
+						ifTrue: 
+							["use only #spaceFillers"
+
+							
+							n := cell inject: 0
+										into: [:sum :c | c hSpaceFill ifTrue: [sum + c target spaceFillWeight] ifFalse: [sum]].
+							n isZero ifFalse: [extraPerCell := extra asFloat / n asFloat].
+							extra := last := 0.
+							cell do: 
+									[:c | 
+									c hSpaceFill 
+										ifTrue: 
+											[extra := (last := extra) + (extraPerCell * c target spaceFillWeight).
+											amount := extra truncated - last truncated.
+											c cellSize: c cellSize + (amount @ 0)]]]
+						ifFalse: 
+							["no #spaceFiller; distribute regularly"
+
+						
+							"centering == #topLeft ifTrue:[]"	"add all extra space to the last cell; e.g., do nothing"
+							centering == #bottomRight 
+								ifTrue: 
+									["add all extra space to the first cell"
+
+									cell addExtraSpace: extra @ 0].
+							centering == #center 
+								ifTrue: 
+									["add 1/2 extra space to the first and last cell"
+
+									cell addExtraSpace: (extra // 2) @ 0].
+							centering == #justified 
+								ifTrue: 
+									["add extra space equally distributed to each cell"
+
+									n := cell size - 1 max: 1.
+									extraPerCell := extra asFloat / n asFloat.
+									extra := last := 0.
+									cell do: 
+											[:c | 
+											c addExtraSpace: (extra truncated - last truncated) @ 0.
+											extra := (last := extra) + extraPerCell]]]]]
