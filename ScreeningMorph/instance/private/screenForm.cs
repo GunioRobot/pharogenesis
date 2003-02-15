6fd189fcc7e2b4f55b8101ab51b86d66087df61c
aoy <unknown>
2003-02-15 21:40:00 +0000

    no message

--- ScreeningMorph/instance/private/screenForm.cs
+++ ScreeningMorph/instance/private/screenForm.cs
@@ -1,16 +1,23 @@
 screenForm
-
 	| screenImage colorMap pickValue elseValue |
-	screenForm ifNotNil: [^ screenForm].
-	passElseBlock ifNil: [passElseBlock _ true].
-	passingColor ifNil: [passingColor _ Color black].
-	passElseBlock
-		ifTrue: [pickValue _ 16rFFFFFFFF.  elseValue _ 0]
-		ifFalse: [pickValue _ 0.  elseValue _ 16rFFFFFFFF].
-	screenImage _ self screenMorph imageFormForRectangle: self screenMorph bounds.
-	colorMap _ screenImage newColorMap atAllPut: elseValue.
+	screenForm ifNotNil: [^screenForm].
+	passElseBlock ifNil: [passElseBlock := true].
+	passingColor ifNil: [passingColor := Color black].
+	elseValue := passElseBlock 
+		ifTrue: 
+			[pickValue := 4294967295.
+			 0]
+		ifFalse: 
+			[pickValue := 0.
+			 4294967295].
+	screenImage := self screenMorph 
+				imageFormForRectangle: self screenMorph bounds.
+	colorMap := screenImage newColorMap atAllPut: elseValue.
 	colorMap at: (passingColor indexInMap: colorMap) put: pickValue.
-	screenForm _ Form extent: screenImage extent.
-	screenForm copyBits: screenForm boundingBox
-			from: screenImage at: 0@0 colorMap: colorMap.
-	^ screenForm
+	screenForm := Form extent: screenImage extent.
+	screenForm 
+		copyBits: screenForm boundingBox
+		from: screenImage
+		at: 0 @ 0
+		colorMap: colorMap.
+	^screenForm
