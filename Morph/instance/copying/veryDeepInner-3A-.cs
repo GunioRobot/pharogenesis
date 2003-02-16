677fd2b647fc60beb7f945d1ccea13e209af26bf
dgd <unknown>
2003-02-16 19:59:00 +0000

    no message

--- Morph/instance/copying/veryDeepInner-3A-.cs
+++ Morph/instance/copying/veryDeepInner-3A-.cs
@@ -1,17 +1,14 @@
-veryDeepInner: deepCopier
-	"The inner loop, so it can be overridden when a field should not be traced."
-
+veryDeepInner: deepCopier 
+	"The inner loop, so it can be overridden when a field should not  
+	be traced."
 	"super veryDeepInner: deepCopier.	know Object has no inst vars"
-	bounds _ bounds clone.	"Points are shared with original"
+	bounds _ bounds clone.
+	"Points are shared with original"
 	"owner _ owner.	special, see veryDeepFixupWith:"
 	submorphs _ submorphs veryDeepCopyWith: deepCopier.
-	self submorphsDo: [:mySub | mySub privateOwner: self].		"I am the owner!"
-		"My owner field:  If owner is in tree being copied, he will set it.  
-		If not, if I am top object, caller will addMorph: me to another morph.
-			if I am not in the submorph tree, I should not be copied.  The field I
-			am in needs to be weakly copied."
+	"each submorph's fixup will install me as the owner"
 	"fullBounds _ fullBounds.	fullBounds is shared with original!"
 	color _ color veryDeepCopyWith: deepCopier.
-		"color, if simple, will return self. may be complex"
-	extension _ extension veryDeepCopyWith: deepCopier.
-		"extension is treated like any generic inst var"
+	"color, if simple, will return self. may be complex"
+	self
+		privateExtension: (self extension veryDeepCopyWith: deepCopier)
