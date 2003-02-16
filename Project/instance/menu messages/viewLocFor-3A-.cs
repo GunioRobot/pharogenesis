e508043789483cb535b1ff1d09f50263e068122a
gm <unknown>
2003-02-16 20:37:00 +0000

    no message

--- Project/instance/menu messages/viewLocFor-3A-.cs
+++ Project/instance/menu messages/viewLocFor-3A-.cs
@@ -1,13 +1,14 @@
-viewLocFor: exitedProject
+viewLocFor: exitedProject 
 	"Look for a view of the exitedProject, and return its center"
+
 	| ctlr |
-	world isMorph
-		ifTrue: [world submorphsDo:
-					[:v | ((v isKindOf: SystemWindow) and: [v model == exitedProject])
-						ifTrue: [^ v center]]]
-		ifFalse: [ctlr _ world controllerWhoseModelSatisfies:
-							[:p | p == exitedProject].
-				ctlr ifNotNil: [^ ctlr view windowBox center]].
-
-	^ Sensor cursorPoint.  "default result"
-
+	world isMorph 
+		ifTrue: 
+			[world submorphsDo: 
+					[:v | 
+					((v isSystemWindow) and: [v model == exitedProject]) 
+						ifTrue: [^v center]]]
+		ifFalse: 
+			[ctlr := world controllerWhoseModelSatisfies: [:p | p == exitedProject].
+			ctlr ifNotNil: [^ctlr view windowBox center]].
+	^Sensor cursorPoint	"default result"
