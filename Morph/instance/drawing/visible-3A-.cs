6fd1a05beae9f58045ba5df0c6c9216f8b1d0650
dgd <unknown>
2003-02-16 20:24:00 +0000

    no message

--- Morph/instance/drawing/visible-3A-.cs
+++ Morph/instance/drawing/visible-3A-.cs
@@ -1,5 +1,8 @@
-visible: aBoolean
-	extension ifNil: [aBoolean ifTrue: [^ self]].
-	self visible == aBoolean ifTrue: [^ self].
+visible: aBoolean 
+	"set the 'visible' attribute of the receiver to aBoolean"
+	(self hasExtension not and:[aBoolean])
+				ifTrue: [^ self].
+	self visible == aBoolean
+		ifTrue: [^ self].
 	self assureExtension visible: aBoolean.
 	self changed
