b013f1e0a9d3f4c844c6bf7af17d4a48a37cbf59
dgd <unknown>
2003-02-16 19:30:00 +0000

    no message

--- Morph/instance/halos and balloon help/setBalloonText-3A-maxLineLength-3A-.cs
+++ Morph/instance/halos and balloon help/setBalloonText-3A-maxLineLength-3A-.cs
@@ -1,7 +1,8 @@
-setBalloonText: stringOrText maxLineLength: aLength
+setBalloonText: stringOrText maxLineLength: aLength 
 	"Set receiver's balloon help text. Pass nil to remove the help."
-
-	(extension isNil and: [stringOrText isNil]) ifTrue: [^ self].
-	self assureExtension balloonText: (stringOrText
-		ifNil: [nil]
-		ifNotNil: [stringOrText asString withNoLineLongerThan: aLength])
+	(self hasExtension not
+			and: [stringOrText isNil])
+		ifTrue: [^ self].
+	self assureExtension
+		balloonText: (stringOrText
+				ifNotNil: [stringOrText asString withNoLineLongerThan: aLength])
