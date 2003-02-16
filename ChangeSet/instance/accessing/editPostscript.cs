253de5c08d545b0406df265949537bbaba592dba
gm <unknown>
2003-02-16 20:39:00 +0000

    no message

--- ChangeSet/instance/accessing/editPostscript.cs
+++ ChangeSet/instance/accessing/editPostscript.cs
@@ -3,20 +3,26 @@ editPostscript
 
 	| deps found |
 	self assurePostscriptExists.
-	deps _ postscript dependents select:
-		[:m | (m isKindOf: SystemWindow) or:
-			[m isKindOf: StandardSystemView]].
-	deps size > 0 ifTrue:
-		[Smalltalk isMorphic
-			ifTrue:
-				[found _ deps detect: [:obj | (obj isKindOf: SystemWindow) and: [obj world == self currentWorld]] ifNone: [nil].
-				found ifNotNil:  [^ found activate]]
-			ifFalse:
-				[found _ deps detect: [:obj | (obj isKindOf: StandardSystemView) and: [ScheduledControllers scheduledControllers includes: obj controller]] ifNone: [nil].
-				found ifNotNil: [^ ScheduledControllers activateController: found controller]].
-.
-		self inform:
-'Caution -- there', (deps size isOrAreStringWith: 'other window'), '
+	deps := postscript dependents 
+				select: [:m | (m isSystemWindow) or: [m isKindOf: StandardSystemView]].
+	deps size > 0 
+		ifTrue: 
+			[Smalltalk isMorphic 
+				ifTrue: 
+					[found := deps 
+								detect: [:obj | obj isSystemWindow and: [obj world == self currentWorld]]
+								ifNone: [nil].
+					found ifNotNil: [^found activate]]
+				ifFalse: 
+					[found := deps detect: 
+									[:obj | 
+									(obj isKindOf: StandardSystemView) 
+										and: [ScheduledControllers scheduledControllers includes: obj controller]]
+								ifNone: [nil].
+					found 
+						ifNotNil: [^ScheduledControllers activateController: found controller]].
+			self 
+				inform: 'Caution -- there' , (deps size isOrAreStringWith: 'other window') 
+						, '
 already open on this postscript elsewhere'].
-
-	postscript openLabel: 'Postscript for ChangeSet named ', name
+	postscript openLabel: 'Postscript for ChangeSet named ' , name
