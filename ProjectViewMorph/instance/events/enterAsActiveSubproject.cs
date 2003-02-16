3510cff928c9096473612609079c39656d8aadac
gm <unknown>
2003-02-16 20:34:00 +0000

    no message

--- ProjectViewMorph/instance/events/enterAsActiveSubproject.cs
+++ ProjectViewMorph/instance/events/enterAsActiveSubproject.cs
@@ -1,14 +1,13 @@
 enterAsActiveSubproject
 	"Enter my project."
 
-	project class == DiskProxy ifTrue: ["When target is not in yet"
-		[self enterWhenNotPresent]	"will bring it in"
-			on: ProjectEntryNotification
-			do: [ :ex | 
-				^ex projectToEnter enterAsActiveSubprojectWithin: self world
-			].
-		project class == DiskProxy ifTrue: [self error: 'Could not find view']].
-	(owner isKindOf: SystemWindow)
-		ifTrue: [project setViewSize: self extent].
+	project class == DiskProxy 
+		ifTrue: 
+			["When target is not in yet"
+
+			[self enterWhenNotPresent	"will bring it in"] on: ProjectEntryNotification
+				do: [:ex | ^ex projectToEnter enterAsActiveSubprojectWithin: self world].
+			project class == DiskProxy ifTrue: [self error: 'Could not find view']].
+	(owner isSystemWindow) ifTrue: [project setViewSize: self extent].
 	self showMouseState: 3.
-	project enterAsActiveSubprojectWithin: self world.
+	project enterAsActiveSubprojectWithin: self world
