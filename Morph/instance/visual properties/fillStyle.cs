df7c46c7a9e28fbf52ee809321946d5ad2aafc1f
dgd <unknown>
2003-02-16 20:02:00 +0000

    no message

--- Morph/instance/visual properties/fillStyle.cs
+++ Morph/instance/visual properties/fillStyle.cs
@@ -1,6 +1,7 @@
 fillStyle
 	"Return the current fillStyle of the receiver."
-	extension ifNil:[^color ifNil:[self defaultColor]].
-	^self valueOfProperty: #fillStyle ifAbsent:[
-		"Workaround already converted morphs"
-		color ifNil:[self defaultColor]].
+	^ self
+		valueOfProperty: #fillStyle
+		ifAbsent: ["Workaround already converted morphs"
+			color
+				ifNil: [self defaultColor]]
