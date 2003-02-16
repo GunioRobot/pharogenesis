240ba3639369f8424a3499927e1f10623fd9b887
dgd <unknown>
2003-02-16 19:53:00 +0000

    no message

--- Morph/instance/private/privateMoveBy-3A-.cs
+++ Morph/instance/private/privateMoveBy-3A-.cs
@@ -1,13 +1,16 @@
-privateMoveBy: delta
+privateMoveBy: delta 
 	"Private! Use 'position:' instead."
 	| fill |
-	extension ifNotNil:[
-		extension player ifNotNil:[
-			"Most cases eliminated fast by above test"
-			self getPenDown ifTrue:[
-				"If this is a costume for a player with its pen down, draw a line."
-				self moveWithPenDownBy: delta]]].
+	self hasExtension
+		ifTrue: [self extension player
+				ifNotNil: ["Most cases eliminated fast by above test"
+					self getPenDown
+						ifTrue: ["If this is a costume for a player with its 
+							pen down, draw a line."
+							self moveWithPenDownBy: delta]]].
 	bounds _ bounds translateBy: delta.
-	fullBounds ifNotNil:[fullBounds _ fullBounds translateBy: delta].
+	fullBounds
+		ifNotNil: [fullBounds _ fullBounds translateBy: delta].
 	fill _ self fillStyle.
-	fill isOrientedFill ifTrue:[fill origin: fill origin + delta].
+	fill isOrientedFill
+		ifTrue: [fill origin: fill origin + delta]
