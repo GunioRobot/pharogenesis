0676c4faf288db8d4fd8ab838606646b51c57b1f
dgd <unknown>
2003-02-16 21:14:00 +0000

    no message

--- MorphExtension/instance/other/isDefault.cs
+++ MorphExtension/instance/other/isDefault.cs
@@ -1,14 +1,26 @@
 isDefault
 	"Return true if the receiver is a default and can be omitted"
-	locked == true ifTrue:[^false].
-	visible == false ifTrue:[^false].
-	sticky == true ifTrue:[^false].
-	balloonText == nil ifFalse:[^false].
-	balloonTextSelector == nil ifFalse:[^false].
-	externalName == nil ifFalse:[^false].
-	isPartsDonor == true ifTrue:[^false].
-	actorState == nil ifFalse:[^false].
-	player == nil ifFalse:[^false].
-	eventHandler == nil ifFalse:[^false].
-	otherProperties == nil ifFalse:[otherProperties size > 0 ifTrue:[^false]].
-	^true
+	locked == true
+		ifTrue: [^ false].
+	visible == false
+		ifTrue: [^ false].
+	sticky == true
+		ifTrue: [^ false].
+	balloonText isNil
+		ifFalse: [^ false].
+	balloonTextSelector isNil
+		ifFalse: [^ false].
+	externalName isNil
+		ifFalse: [^ false].
+	isPartsDonor == true
+		ifTrue: [^ false].
+	actorState isNil
+		ifFalse: [^ false].
+	player isNil
+		ifFalse: [^ false].
+	eventHandler isNil
+		ifFalse: [^ false].
+	self hasOtherProperties
+		ifTrue: [self otherProperties isEmpty
+				ifFalse: [^ false]].
+	^ true
