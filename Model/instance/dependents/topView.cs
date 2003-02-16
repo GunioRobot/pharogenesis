dd81ed5e4b4a20da404b1df98d643872c422d50f
gm <unknown>
2003-02-16 20:37:00 +0000

    no message

--- Model/instance/dependents/topView.cs
+++ Model/instance/dependents/topView.cs
@@ -1,10 +1,11 @@
 topView
 	"Find the first top view on me. Is there any danger of their being two with the same model?  Any danger from ungarbage collected old views?  Ask if schedulled?"
 
-	dependents ifNil: [^ nil].
-	Smalltalk isMorphic ifTrue: [
-		dependents do:
-			[:v | ((v isKindOf: SystemWindow) and: [v isInWorld]) ifTrue: [^ v]].
-		^ nil].
-	dependents do: [:v | v superView ifNil: [v model == self ifTrue: [^ v]]].
-	^ nil
+	dependents ifNil: [^nil].
+	Smalltalk isMorphic 
+		ifTrue: 
+			[dependents 
+				do: [:v | ((v isSystemWindow) and: [v isInWorld]) ifTrue: [^v]].
+			^nil].
+	dependents do: [:v | v superView ifNil: [v model == self ifTrue: [^v]]].
+	^nil
