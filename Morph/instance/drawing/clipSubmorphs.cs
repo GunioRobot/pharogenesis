f062123bf8293264f61b0cbc11d62065831474eb
dgd <unknown>
2003-02-16 20:02:00 +0000

    no message

--- Morph/instance/drawing/clipSubmorphs.cs
+++ Morph/instance/drawing/clipSubmorphs.cs
@@ -1,4 +1,6 @@
 clipSubmorphs
-	"Drawing specific. If this property is set, clip the receiver's submorphs to the receiver's clipping bounds."
-	extension == nil ifTrue:[^false]. "get out quickly"
-	^self valueOfProperty: #clipSubmorphs ifAbsent:[false].
+	"Drawing specific. If this property is set, clip the receiver's  
+	submorphs to the receiver's clipping bounds."
+	^ self
+		valueOfProperty: #clipSubmorphs
+		ifAbsent: [false]
