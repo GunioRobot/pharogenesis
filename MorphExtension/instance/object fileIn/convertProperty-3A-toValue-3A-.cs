bcadda9373f7949de9afb7b9adae795f6bd7c2cd
dgd <unknown>
2003-02-16 21:06:00 +0000

    no message

--- MorphExtension/instance/object fileIn/convertProperty-3A-toValue-3A-.cs
+++ MorphExtension/instance/object fileIn/convertProperty-3A-toValue-3A-.cs
@@ -1,13 +1,24 @@
-convertProperty: propName toValue: aValue
-	"These special cases move old properties into named fields of the extension"
-	propName == #locked ifTrue: [^ locked _ aValue].
-	propName == #visible ifTrue: [^ visible _ aValue].
-	propName == #sticky ifTrue: [^ sticky _ aValue].
-	propName == #balloonText ifTrue: [^ balloonText _ aValue].
-	propName == #balloonTextSelector ifTrue: [^ balloonTextSelector _ aValue].
-	propName == #actorState ifTrue: [^ actorState _ aValue].
-	propName == #player ifTrue: [^ player _ aValue].
-	propName == #name ifTrue: [^ externalName _ aValue].  "*renamed*"
-	propName == #partsDonor ifTrue: [^ isPartsDonor _ aValue].  "*renamed*"
-	otherProperties == nil ifTrue: [otherProperties _ IdentityDictionary new].
-	otherProperties at: propName put: aValue.
+convertProperty: aSymbol toValue: anObject 
+	"These special cases move old properties into named fields of the 
+	extension"
+	aSymbol == #locked
+		ifTrue: [^ locked _ anObject].
+	aSymbol == #visible
+		ifTrue: [^ visible _ anObject].
+	aSymbol == #sticky
+		ifTrue: [^ sticky _ anObject].
+	aSymbol == #balloonText
+		ifTrue: [^ balloonText _ anObject].
+	aSymbol == #balloonTextSelector
+		ifTrue: [^ balloonTextSelector _ anObject].
+	aSymbol == #actorState
+		ifTrue: [^ actorState _ anObject].
+	aSymbol == #player
+		ifTrue: [^ player _ anObject].
+	aSymbol == #name
+		ifTrue: [^ externalName _ anObject].
+	"*renamed*"
+	aSymbol == #partsDonor
+		ifTrue: [^ isPartsDonor _ anObject].
+	"*renamed*"
+	self assureOtherProperties at: aSymbol put: anObject
