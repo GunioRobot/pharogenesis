a8f534137541252c1f3bc75d0b705c809064ec1d
gm <unknown>
2003-02-16 20:37:00 +0000

    no message

--- Project/instance/release/subProjects.cs
+++ Project/instance/release/subProjects.cs
@@ -1,16 +1,11 @@
 subProjects
 	"Answer a list of all the subprojects  of the receiver.  This is nastily idiosyncratic."
 
-	^ self isMorphic
+	^self isMorphic 
 		ifTrue: 
-			[world submorphs
-				select:
-					[:m | (m isKindOf: SystemWindow)
-						and: [m model isKindOf: Project]]
-				thenCollect:
-					[:m | m model]]
-		ifFalse:
-			[(world controllersSatisfying:
-					[:m | m model isKindOf: Project])
-				collect:
-					[:c | c model]]
+			[world submorphs 
+				select: [:m | (m isSystemWindow) and: [m model isKindOf: Project]]
+				thenCollect: [:m | m model]]
+		ifFalse: 
+			[(world controllersSatisfying: [:m | m model isKindOf: Project]) 
+				collect: [:c | c model]]
