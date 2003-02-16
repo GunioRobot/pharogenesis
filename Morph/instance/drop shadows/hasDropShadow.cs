508fbebdc19c06196151df12b7478f2196fa59ed
dgd <unknown>
2003-02-16 21:42:00 +0000

    no message

--- Morph/instance/drop shadows/hasDropShadow.cs
+++ Morph/instance/drop shadows/hasDropShadow.cs
@@ -1,3 +1,5 @@
 hasDropShadow
-	extension == nil ifTrue:[^false].
-	^self valueOfProperty: #hasDropShadow ifAbsent:[false]
+	"answer whether the receiver has DropShadow"
+	^ self
+		valueOfProperty: #hasDropShadow
+		ifAbsent: [false]
