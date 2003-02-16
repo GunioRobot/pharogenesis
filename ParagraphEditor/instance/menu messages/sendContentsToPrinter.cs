5f3454623b09a206f0a6595283b275f5d6c43cdc
gm <unknown>
2003-02-16 20:38:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/sendContentsToPrinter.cs
+++ ParagraphEditor/instance/menu messages/sendContentsToPrinter.cs
@@ -1,13 +1,12 @@
 sendContentsToPrinter
 	| textToPrint printer parentWindow |
-	textToPrint _ paragraph text.
-	textToPrint size == 0 ifTrue: [^ self inform: 'nothing to print.'].
-	printer _ TextPrinter defaultTextPrinter.
-	parentWindow _ self model dependents
-						detect: [:dep | dep isKindOf: SystemWindow]
-						ifNone: [nil].
-	parentWindow isNil
+	textToPrint := paragraph text.
+	textToPrint size == 0 ifTrue: [^self inform: 'nothing to print.'].
+	printer := TextPrinter defaultTextPrinter.
+	parentWindow := self model dependents 
+				detect: [:dep | dep isSystemWindow]
+				ifNone: [nil].
+	parentWindow isNil 
 		ifTrue: [printer documentTitle: 'Untitled']
 		ifFalse: [printer documentTitle: parentWindow label].
-	printer printText: textToPrint.
-
+	printer printText: textToPrint
