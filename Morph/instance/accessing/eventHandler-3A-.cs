ee25469f7f5e3bd29c2fc3cba2fcde14622a468f
dgd <unknown>
2003-02-16 19:25:00 +0000

    no message

--- Morph/instance/accessing/eventHandler-3A-.cs
+++ Morph/instance/accessing/eventHandler-3A-.cs
@@ -1,4 +1,3 @@
-eventHandler: anEventHandler
-	"Note that morphs can share eventHandlers and all is OK.  "
-	extension == nil ifTrue: [self assureExtension].
-	extension eventHandler: anEventHandler
+eventHandler: anEventHandler 
+	"Note that morphs can share eventHandlers and all is OK. "
+	self assureExtension eventHandler: anEventHandler
