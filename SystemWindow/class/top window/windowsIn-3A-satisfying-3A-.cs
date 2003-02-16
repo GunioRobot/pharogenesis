b2288ad64afabb83c706465e4ce620153f0b26a8
gm <unknown>
2003-02-16 20:55:00 +0000

    no message

--- SystemWindow/class/top window/windowsIn-3A-satisfying-3A-.cs
+++ SystemWindow/class/top window/windowsIn-3A-satisfying-3A-.cs
@@ -5,10 +5,10 @@ windowsIn: aWorld satisfying: windowBlock
 	aWorld ifNil: [^windows].	"opening MVC in Morphic - WOW!"
 	aWorld submorphs do:
 		[:m |
-		((m isKindOf: SystemWindow) and: [windowBlock value: m])
+		((m isSystemWindow) and: [windowBlock value: m])
 			ifTrue: [windows addLast: m]
 			ifFalse: [((m isKindOf: TransformationMorph) and: [m submorphs size = 1])
 					ifTrue: [s _ m firstSubmorph.
-							((s isKindOf: SystemWindow) and: [windowBlock value: s])
+							((s isSystemWindow) and: [windowBlock value: s])
 								ifTrue: [windows addLast: s]]]].
 	^ windows
