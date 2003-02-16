87847e09a9dfc6e5d517eb90f0c2fe67b677cb9b
dgd <unknown>
2003-02-16 20:02:00 +0000

    no message

--- Morph/instance/texture support/isValidWonderlandTexture.cs
+++ Morph/instance/texture support/isValidWonderlandTexture.cs
@@ -1,4 +1,5 @@
 isValidWonderlandTexture
 	"Return true if the receiver is a valid wonderland texture"
-	extension == nil ifTrue:[^false].
-	^self valueOfProperty: #isValidWonderlandTexture ifAbsent:[true]
+	^ self
+		valueOfProperty: #isValidWonderlandTexture
+		ifAbsent: [true]
