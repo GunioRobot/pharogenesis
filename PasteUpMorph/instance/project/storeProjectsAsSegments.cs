e5e0165d09667be12f3405f60ffb5dd71afddfee
gm <unknown>
2003-02-16 20:35:00 +0000

    no message

--- PasteUpMorph/instance/project/storeProjectsAsSegments.cs
+++ PasteUpMorph/instance/project/storeProjectsAsSegments.cs
@@ -1,7 +1,7 @@
 storeProjectsAsSegments
 	"Force my sub-projects out to disk"
 
-submorphs do: [:sub |
-	(sub isKindOf: SystemWindow) ifTrue: [
-		(sub model isKindOf: Project) ifTrue: [
-			sub model storeSegment]]].	"OK if was already out"
+	submorphs do: 
+			[:sub | 
+			(sub isSystemWindow) 
+				ifTrue: [(sub model isKindOf: Project) ifTrue: [sub model storeSegment]]]	"OK if was already out"
