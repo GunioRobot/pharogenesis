fec5e8a9a260625ff116bdc39df9b15d79dd17cc
aoy <unknown>
2003-02-17 01:01:00 +0000

    no message

--- AudioChatGUI/instance/stuff/changeTalkButtonLabel.cs
+++ AudioChatGUI/instance/stuff/changeTalkButtonLabel.cs
@@ -1,23 +1,22 @@
 changeTalkButtonLabel
-
 	| bText |
 	self transmitWhileRecording.
-	handsFreeTalking ifTrue: [
-		theTalkButton
-			labelUp: 'Talk';
-			labelDown: 'Release';
-			label: 'Talk'.
-		bText _ 'Click once to begin a message. Click again to end the message.'
-	] ifFalse: [
-		theTalkButton
-			labelUp: 'Talk';
-			labelDown: (transmitWhileRecording ifTrue: ['TALKING'] ifFalse: ['RECORDING']);
-			label: 'Talk'.
-		bText _ 'Press and hold to record a message.'
-	].
-	transmitWhileRecording ifTrue: [
-		bText _ bText , ' The message will be sent while you are speaking.'
-	] ifFalse: [
-		bText _ bText , ' The message will be sent when you are finished.'
-	].
-	theTalkButton setBalloonText: bText.
+	handsFreeTalking 
+		ifTrue: 
+			[theTalkButton
+				labelUp: 'Talk';
+				labelDown: 'Release';
+				label: 'Talk'.
+			bText := 'Click once to begin a message. Click again to end the message.']
+		ifFalse: 
+			[theTalkButton
+				labelUp: 'Talk';
+				labelDown: (transmitWhileRecording 
+							ifTrue: ['TALKING']
+							ifFalse: ['RECORDING']);
+				label: 'Talk'.
+			bText := 'Press and hold to record a message.'].
+	bText := transmitWhileRecording 
+		ifTrue: [bText , ' The message will be sent while you are speaking.']
+		ifFalse: [bText , ' The message will be sent when you are finished.'].
+	theTalkButton setBalloonText: bText
