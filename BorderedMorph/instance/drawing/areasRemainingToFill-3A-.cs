168598d32aaf004110219f092832e249a1d26e78
dgd <unknown>
2003-02-17 19:57:00 +0000

    no message

--- BorderedMorph/instance/drawing/areasRemainingToFill-3A-.cs
+++ BorderedMorph/instance/drawing/areasRemainingToFill-3A-.cs
@@ -1,9 +1,16 @@
-areasRemainingToFill: aRectangle
-	(color isColor and: [color isTranslucent]) ifTrue: [^ Array with: aRectangle].
+areasRemainingToFill: aRectangle 
+	(color isColor
+			and: [color isTranslucent])
+		ifTrue: [^ Array with: aRectangle].
 	self wantsRoundedCorners
-	ifTrue: [(borderWidth > 0 and: [borderColor isColor and: [borderColor isTranslucent]])
-				ifTrue: [^ aRectangle areasOutside: (self innerBounds intersect: self boundsWithinCorners)]
+		ifTrue: [(self borderWidth > 0
+					and: [self borderColor isColor
+							and: [self borderColor isTranslucent]])
+				ifTrue: [^ aRectangle
+						areasOutside: (self innerBounds intersect: self boundsWithinCorners)]
 				ifFalse: [^ aRectangle areasOutside: self boundsWithinCorners]]
-	ifFalse: [(borderWidth > 0 and: [borderColor isColor and: [borderColor isTranslucent]])
+		ifFalse: [(self borderWidth > 0
+					and: [self borderColor isColor
+							and: [self borderColor isTranslucent]])
 				ifTrue: [^ aRectangle areasOutside: self innerBounds]
 				ifFalse: [^ aRectangle areasOutside: self bounds]]
