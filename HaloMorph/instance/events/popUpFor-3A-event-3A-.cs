5494da6db8a5a84dc54ce1c738ba01713f58f506
aoy <unknown>
2003-02-17 01:27:00 +0000

    no message

--- HaloMorph/instance/events/popUpFor-3A-event-3A-.cs
+++ HaloMorph/instance/events/popUpFor-3A-event-3A-.cs
@@ -1,15 +1,23 @@
-popUpFor: aMorph event: evt
+popUpFor: aMorph event: evt 
 	"This message is sent by morphs that explicitly request the halo on a button click. Note: anEvent is in aMorphs coordinate frame."
+
 	| hand anEvent |
-	self flag: #workAround. "We should really have some event/hand here..."
-	evt isNil
-		ifTrue:[	hand _ aMorph world activeHand.
-				hand ifNil:[hand _ aMorph world primaryHand].
-				anEvent _ hand lastEvent transformedBy: (aMorph transformedFrom: nil)]
-		 ifFalse:[hand _ evt hand.
-				anEvent _ evt].
+	self flag: #workAround.	"We should really have some event/hand here..."
+	anEvent := evt isNil 
+				ifTrue: 
+					[hand := aMorph world activeHand.
+					hand ifNil: [hand := aMorph world primaryHand]. 
+					hand lastEvent transformedBy: (aMorph transformedFrom: nil)]
+				ifFalse: 
+					[hand := evt hand.
+					evt].
 	self target: aMorph.
 	hand halo: self.
 	hand world addMorphFront: self.
-	positionOffset _ anEvent position - (aMorph point: aMorph position in: owner).
+	positionOffset := anEvent position 
+				- (aMorph point: aMorph position in: owner).
 	self startStepping.
+	(Preferences haloTransitions or: [self isMagicHalo]) 
+		ifTrue: 
+			[self magicAlpha: 0.0.
+			self startSteppingSelector: #fadeInInitially]
