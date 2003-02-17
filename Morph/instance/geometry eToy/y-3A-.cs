5101c43e25e88abb2596daecb4cb1ae74b0660c3
aoy <unknown>
2003-02-17 01:00:00 +0000

    no message

--- Morph/instance/geometry eToy/y-3A-.cs
+++ Morph/instance/geometry eToy/y-3A-.cs
@@ -1,13 +1,12 @@
-y: aNumber
+y: aNumber 
 	"Set my vertical position relative to the cartesian origin of the playfield or the world. Note that larger y values are closer to the top of the screen."
 
 	| w offset newY aPlayfield |
-
-	w _ self world.
-	w ifNil: [^ self position: bounds left @ aNumber].
-	aPlayfield _ self referencePlayfield.
-	offset _ self top - self referencePosition y.
-	aPlayfield == nil
-		ifTrue: [newY _ (w bottom - aNumber) + offset]
-		ifFalse: [newY _ (aPlayfield cartesianOrigin y - aNumber) + offset].
-	self position: bounds left @ newY.
+	w := self world.
+	w ifNil: [^self position: bounds left @ aNumber].
+	aPlayfield := self referencePlayfield.
+	offset := self top - self referencePosition y.
+	newY := aPlayfield isNil
+				ifTrue: [w bottom - aNumber + offset]
+				ifFalse: [aPlayfield cartesianOrigin y - aNumber + offset].
+	self position: bounds left @ newY
