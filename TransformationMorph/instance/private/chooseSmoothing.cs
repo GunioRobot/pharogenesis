c2f1171ad08586153d7d518a59b3cc633224750a
aoy <unknown>
2003-02-17 01:02:00 +0000

    no message

--- TransformationMorph/instance/private/chooseSmoothing.cs
+++ TransformationMorph/instance/private/chooseSmoothing.cs
@@ -1,6 +1,6 @@
 chooseSmoothing
 	"Choose appropriate smoothing, after a change of scale or rotation."
 
-	(self scale < 1.0 or: [self angle ~= (self angle roundTo: Float pi / 2.0)])
-		ifTrue: [smoothing _ 2]
-		ifFalse: [smoothing _ 1]
+	smoothing := (self scale < 1.0 or: [self angle ~= (self angle roundTo: Float pi / 2.0)]) 
+		ifTrue: [ 2]
+		ifFalse: [1]
