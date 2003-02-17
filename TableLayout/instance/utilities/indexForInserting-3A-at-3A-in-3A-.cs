2e89dc4327e1b45bf7a63e7a57daa5fcda8ffe9d
aoy <unknown>
2003-02-17 01:22:00 +0000

    no message

--- TableLayout/instance/utilities/indexForInserting-3A-at-3A-in-3A-.cs
+++ TableLayout/instance/utilities/indexForInserting-3A-at-3A-in-3A-.cs
@@ -1,13 +1,17 @@
-indexForInserting: aMorph at: aPoint in: owner
+indexForInserting: aMorph at: aPoint in: owner 
 	"Return the insertion index based on the layout strategy defined for some morph. Used for drop insertion."
+
 	| horizontal morphList index |
-	owner hasSubmorphs ifFalse:[^1].
-	aMorph disableTableLayout ifTrue:[^1].
-	(owner listDirection == #topToBottom or:[owner listDirection == #bottomToTop])
-		ifTrue:[	horizontal _ false]
-		ifFalse:[	horizontal _ true].
-	morphList _ owner submorphs.
-	owner reverseTableCells ifTrue:[morphList _ morphList reversed].
-	index _ self indexForInserting: aPoint inList: morphList horizontal: horizontal target: owner.
-	owner reverseTableCells ifTrue:[index _ morphList size - index + 2].
-	^index ifNil:[1].			
+	owner hasSubmorphs ifFalse: [^1].
+	aMorph disableTableLayout ifTrue: [^1].
+	horizontal := (owner listDirection == #topToBottom 
+				or: [owner listDirection == #bottomToTop]) not .
+	morphList := owner submorphs.
+	owner reverseTableCells ifTrue: [morphList := morphList reversed].
+	index := self 
+				indexForInserting: aPoint
+				inList: morphList
+				horizontal: horizontal
+				target: owner.
+	owner reverseTableCells ifTrue: [index := morphList size - index + 2].
+	^index ifNil: [1]
