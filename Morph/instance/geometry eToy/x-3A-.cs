1f1171e11a50cbd44824e9f2615a757e9140fb87
aoy <unknown>
2003-02-17 01:00:00 +0000

    no message

--- Morph/instance/geometry eToy/x-3A-.cs
+++ Morph/instance/geometry eToy/x-3A-.cs
@@ -1,12 +1,10 @@
-x: aNumber
+x: aNumber 
 	"Set my horizontal position relative to the cartesian origin of the playfield or the world."
 
-	|  offset  aPlayfield newX |
-
-	aPlayfield _ self referencePlayfield.
-	offset _ self left - self referencePosition x.
-	aPlayfield == nil
-		ifTrue: [newX _ aNumber + offset]
-		ifFalse: [newX _ aPlayfield cartesianOrigin x + aNumber + offset].
-	self position: newX@bounds top.
-
+	| offset aPlayfield newX |
+	aPlayfield := self referencePlayfield.
+	offset := self left - self referencePosition x.
+	newX := aPlayfield isNil
+				ifTrue: [aNumber + offset]
+				ifFalse: [aPlayfield cartesianOrigin x + aNumber + offset].
+	self position: newX @ bounds top
