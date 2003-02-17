5f91ade36fc7bc637fbef11cc9772247a0f8300c
aoy <unknown>
2003-02-17 01:27:00 +0000

    no message

--- HaloMorph/instance/private/showDirectionHandles-3A-addHandles-3A-.cs
+++ HaloMorph/instance/private/showDirectionHandles-3A-addHandles-3A-.cs
@@ -1,6 +1,5 @@
-showDirectionHandles: wantToShow addHandles: needHandles
-
-	wantToShow
-		ifTrue: [directionArrowAnchor _ target referencePositionInWorld]  "not nil means show"
-		ifFalse: [directionArrowAnchor _ nil].
-	needHandles ifTrue: [self addHandles]
+showDirectionHandles: wantToShow addHandles: needHandles 
+	directionArrowAnchor := wantToShow 
+				ifTrue: [target referencePositionInWorld	"not nil means show"]
+				ifFalse: [nil].
+	needHandles ifTrue: [self addHandles] 
