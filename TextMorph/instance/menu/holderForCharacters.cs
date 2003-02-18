327cabc9b9717f63c90579c06f66204dfc3822c7
sw <unknown>
2003-02-18 03:20:00 +0000

    no message

---
+++ TextMorph/instance/menu/holderForCharacters.cs
@@ -0,0 +1,12 @@
+holderForCharacters
+	"Hand the user a Holder that is populated with individual text morphs representing my characters"
+
+	| aHolder |
+	aHolder _ ScriptingSystem prototypicalHolder.
+	aHolder setNameTo: 'H', self externalName.
+	text string do:
+		[:aChar |
+			aHolder addMorphBack: (TextMorph new contents: aChar asText)].
+	aHolder setProperty: #donorTextMorph toValue: self.
+	aHolder fullBounds.
+	aHolder openInHand
