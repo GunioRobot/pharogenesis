11ac033120bce8a46e739faef37a6d724934ce9e
dgd <unknown>
2003-02-21 22:56:00 +0000

    no message

--- PianoRollScoreMorph/instance/editing/deleteSelection.cs
+++ PianoRollScoreMorph/instance/editing/deleteSelection.cs
@@ -1,17 +1,17 @@
 deleteSelection
-
 	| selMorphs priorEvent x |
-	(selection == nil or: [selection second = 0]) ifTrue: [^ self].
+	(selection isNil or: [selection second = 0]) ifTrue: [^self].
 	score cutSelection: selection.
-	selection second > 1
-		ifTrue: [selection at: 2 put: selection second - 1.
-				selection at: 3 put: selection second.
-				priorEvent _ (score tracks at: selection first) at: selection second.
-				(x _ self xForTime: priorEvent time) < (self left + 30) ifTrue:
-					[self autoScrollForX: x - (30 + self width // 4)]]
-		ifFalse: [selection _ nil].
+	selection second > 1 
+		ifTrue: 
+			[selection at: 2 put: selection second - 1.
+			selection at: 3 put: selection second.
+			priorEvent := (score tracks at: selection first) at: selection second.
+			(x := self xForTime: priorEvent time) < (self left + 30) 
+				ifTrue: [self autoScrollForX: x - ((30 + self width) // 4)]]
+		ifFalse: [selection := nil].
 	scorePlayer updateDuration.
 	self rebuildFromScore.
-	selMorphs _ self submorphsSatisfying:
-						[:m | (m isKindOf: PianoRollNoteMorph) and: [m selected]].
+	selMorphs := self 
+				submorphsSatisfying: [:m | (m isKindOf: PianoRollNoteMorph) and: [m selected]].
 	selMorphs isEmpty ifFalse: [(selMorphs last noteOfDuration: 0.3) play]
