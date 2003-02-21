c85ac2ab14089672ecf66e8e679a7ffff9750e52
dgd <unknown>
2003-02-21 23:17:00 +0000

    no message

--- PaintBoxMorph/instance/actions/grabFromScreen-3A-.cs
+++ PaintBoxMorph/instance/actions/grabFromScreen-3A-.cs
@@ -1,14 +1,16 @@
-grabFromScreen: evt
+grabFromScreen: evt 
 	"Allow the user to grab a picture from the screen OUTSIDE THE PAINTING AREA and install it in a blank stamp.  To get a stamp in the painting area, click on the stamp tool in a blank stamp."
 
-	| stampButton form |
 	"scroll to blank stamp"
-	stampButton _ stampHolder stampButtons first.
-	[(stampHolder stampFormFor: stampButton) == nil] whileFalse: [stampHolder scroll: 1].
-	form _ Form fromUser.
+
+	| stampButton form |
+	stampButton := stampHolder stampButtons first.
+	[(stampHolder stampFormFor: stampButton) isNil] 
+		whileFalse: [stampHolder scroll: 1].
+	form := Form fromUser.
 	tool state: #off.
-	tool _ stampHolder otherButtonFor: stampButton.
+	tool := stampHolder otherButtonFor: stampButton.
 	stampHolder stampForm: form for: tool.	"install it"
 	stampButton state: #on.
 	stampButton doButtonAction: evt.
-	evt hand showTemporaryCursor: (focusMorph getCursorFor: evt).
+	evt hand showTemporaryCursor: (focusMorph getCursorFor: evt)
