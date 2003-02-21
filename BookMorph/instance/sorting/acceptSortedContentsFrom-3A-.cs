a90ed6a82c17c738614ab8c7079588dd6f9562b1
dgd <unknown>
2003-02-21 23:09:00 +0000

    no message

--- BookMorph/instance/sorting/acceptSortedContentsFrom-3A-.cs
+++ BookMorph/instance/sorting/acceptSortedContentsFrom-3A-.cs
@@ -1,33 +1,40 @@
-acceptSortedContentsFrom: aHolder
+acceptSortedContentsFrom: aHolder 
 	"Update my page list from the given page sorter."
 
 	| goodPages rejects toAdd sqPage |
-	goodPages _ OrderedCollection new.
-	rejects _ OrderedCollection new.
-	aHolder submorphs doWithIndex: [:m :i |
-		toAdd _ nil.
-		(m isKindOf: PasteUpMorph) ifTrue: [toAdd _ m].
-		(m isKindOf: BookPageThumbnailMorph) ifTrue: [
-			toAdd _ m page.
-			m bookMorph == self ifFalse: [
-				"borrowed from another book. preserve the original"
-				toAdd _ toAdd veryDeepCopy.	
+	goodPages := OrderedCollection new.
+	rejects := OrderedCollection new.
+	aHolder submorphs doWithIndex: 
+			[:m :i | 
+			toAdd := nil.
+			(m isKindOf: PasteUpMorph) ifTrue: [toAdd := m].
+			(m isKindOf: BookPageThumbnailMorph) 
+				ifTrue: 
+					[toAdd := m page.
+					m bookMorph == self 
+						ifFalse: 
+							["borrowed from another book. preserve the original"
 
-				"since we came from elsewhere, cached strings are wrong"
-				self removeProperty: #allTextUrls.
-				self removeProperty: #allText.
-			].
-		].
-		toAdd class == String ifTrue: ["a url"
-			toAdd _ pages detect: [:aPage | aPage url = toAdd] ifNone: [toAdd]].
-		toAdd class == String ifTrue: [
-			sqPage _ SqueakPageCache atURL: toAdd.
-			toAdd _ sqPage contentsMorph 
-				ifNil: [sqPage copyForSaving]	"a MorphObjectOut"
-				ifNotNil: [sqPage contentsMorph]].
-		toAdd ifNil: [rejects add: m]
-			ifNotNil: [goodPages add: toAdd]].
+							toAdd := toAdd veryDeepCopy.
 
+							"since we came from elsewhere, cached strings are wrong"
+							self removeProperty: #allTextUrls.
+							self removeProperty: #allText]].
+			toAdd class == String 
+				ifTrue: 
+					["a url"
+
+					toAdd := pages detect: [:aPage | aPage url = toAdd] ifNone: [toAdd]].
+			toAdd class == String 
+				ifTrue: 
+					[sqPage := SqueakPageCache atURL: toAdd.
+					toAdd := sqPage contentsMorph 
+								ifNil: [sqPage copyForSaving	"a MorphObjectOut"]
+								ifNotNil: [sqPage contentsMorph]].
+			toAdd ifNil: [rejects add: m] ifNotNil: [goodPages add: toAdd]].
 	self newPages: goodPages.
-	goodPages size = 0 ifTrue: [self insertPage].
-	rejects size > 0 ifTrue: [self inform: rejects size printString, ' objects vanished in this process.']
+	goodPages isEmpty ifTrue: [self insertPage].
+	rejects notEmpty 
+		ifTrue: 
+			[self 
+				inform: rejects size printString , ' objects vanished in this process.']
