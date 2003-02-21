4a8d84a58712f9f0d933068500d0d9d5a88d60e8
dgd <unknown>
2003-02-21 23:10:00 +0000

    no message

--- BookMorph/instance/navigation/goToPageUrl-3A-.cs
+++ BookMorph/instance/navigation/goToPageUrl-3A-.cs
@@ -1,9 +1,10 @@
-goToPageUrl: aUrl
-
+goToPageUrl: aUrl 
 	| pp short |
-	pp _ pages detect: [:pg | pg url = aUrl] ifNone: [nil].
-	pp ifNil: [short _ (aUrl findTokens: '/') last.
-			pp _ pages detect: [:pg | pg url ifNil: [false]
-						ifNotNil: [(pg url findTokens: '/') last = short]] "it moved"
-					ifNone: [pages at: 1]].
-	self goToPageMorph: pp.
+	pp := pages detect: [:pg | pg url = aUrl] ifNone: [nil].
+	pp ifNil: 
+			[short := (aUrl findTokens: '/') last.
+			pp := pages detect: 
+							[:pg | 
+							pg url ifNil: [false] ifNotNil: [(pg url findTokens: '/') last = short]	"it moved"]
+						ifNone: [pages first]].
+	self goToPageMorph: pp
