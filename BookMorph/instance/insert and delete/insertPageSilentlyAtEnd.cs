649c8894ea215bed6e3c9b41357461abce1da7d2
dgd <unknown>
2003-02-21 23:11:00 +0000

    no message

--- BookMorph/instance/insert and delete/insertPageSilentlyAtEnd.cs
+++ BookMorph/instance/insert and delete/insertPageSilentlyAtEnd.cs
@@ -1,24 +1,29 @@
 insertPageSilentlyAtEnd
 	"Create a new page at the end of the book.  Do not turn to it."
 
-	| sz newPage bw bc cc | 
-	currentPage == nil
-		ifTrue: [sz _ pageSize.
-			bw _ 0.
-			bc _ Color blue muchLighter.
-			cc _ color]
-		ifFalse: [sz _ currentPage extent.
-			bw _ currentPage borderWidth.
-			bc _ currentPage borderColor.
-			cc _ currentPage color].
-	newPagePrototype
-		ifNil: [newPage _ PasteUpMorph new extent: sz; color: cc.
-			newPage borderWidth: bw; borderColor: bc]
-		ifNotNil: [Cursor wait showWhile: 
-				[newPage _ newPagePrototype veryDeepCopy]].
+	| sz newPage bw bc cc |
+	cc := currentPage isNil 
+				ifTrue: 
+					[sz := pageSize.
+					bw := 0.
+					bc := Color blue muchLighter.
+					color]
+				ifFalse: 
+					[sz := currentPage extent.
+					bw := currentPage borderWidth.
+					bc := currentPage borderColor.
+					currentPage color].
+	newPagePrototype ifNil: 
+			[newPage := (PasteUpMorph new)
+						extent: sz;
+						color: cc.
+			newPage
+				borderWidth: bw;
+				borderColor: bc]
+		ifNotNil: [Cursor wait showWhile: [newPage := newPagePrototype veryDeepCopy]].
 	newPage setNameTo: self defaultNameStemForNewPages.
-	newPage resizeToFit: false.
-	pages isEmpty
-		ifTrue: [pages add: (currentPage _ newPage)]	"had been none"
+	newPage vResizeToFit: false.
+	pages isEmpty 
+		ifTrue: [pages add: (currentPage := newPage)	"had been none"]
 		ifFalse: [pages add: newPage after: pages last].
-	^ newPage
+	^newPage
