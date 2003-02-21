2c0700a674f24fb9ea45ce7852c3cc9550e2eff8
dgd <unknown>
2003-02-21 22:39:00 +0000

    no message

--- UserScript/instance/versions/saveScriptVersion-3A-.cs
+++ UserScript/instance/versions/saveScriptVersion-3A-.cs
@@ -1,5 +1,9 @@
-saveScriptVersion: timeStampCurrentlyIgnored
-	self isTextuallyCoded ifFalse:
-		[formerScriptEditors == nil ifTrue: [formerScriptEditors _ OrderedCollection new].
-		currentScriptEditor ifNotNil: [formerScriptEditors add: currentScriptEditor fullCopy].
-		formerScriptEditors size > 100 ifTrue: [^ self halt: 'apparent runaway versions']]
+saveScriptVersion: timeStampCurrentlyIgnored 
+	self isTextuallyCoded 
+		ifFalse: 
+			[formerScriptEditors isNil 
+				ifTrue: [formerScriptEditors := OrderedCollection new].
+			currentScriptEditor 
+				ifNotNil: [formerScriptEditors add: currentScriptEditor veryDeepCopy].
+			formerScriptEditors size > 100 
+				ifTrue: [^self error: 'apparent runaway versions']]
