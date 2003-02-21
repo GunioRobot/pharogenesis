715d32ef37ca5ddfff152768f16bdf444bccf783
dgd <unknown>
2003-02-21 23:14:00 +0000

    no message

--- BookMorph/instance/menu/textSearch-3A-.cs
+++ BookMorph/instance/menu/textSearch-3A-.cs
@@ -1,8 +1,8 @@
-textSearch: stringWithKeys
+textSearch: stringWithKeys 
 	"search the text on all pages of this book"
 
 	| wants |
-	wants _ stringWithKeys findTokens: Character separators.
-	wants size = 0 ifTrue: [^ self].
-	self getAllText.		"save in allText, allTextUrls"
-	^ self findText: wants	"goes to the page and highlights the text"
+	wants := stringWithKeys findTokens: Character separators.
+	wants isEmpty ifTrue: [^self].
+	self getAllText.	"save in allText, allTextUrls"
+	^self findText: wants	"goes to the page and highlights the text"
