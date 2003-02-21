9048319c99d1f6c04710b0774728ae9e6106feac
dgd <unknown>
2003-02-21 23:11:00 +0000

    no message

--- BookMorph/instance/navigation/nextPage.cs
+++ BookMorph/instance/navigation/nextPage.cs
@@ -1,4 +1,3 @@
 nextPage
-
-	currentPage == nil ifTrue: [^ self goToPage: 1].
-	self goToPage: (self pageNumberOf: currentPage) + 1.
+	currentPage isNil ifTrue: [^self goToPage: 1].
+	self goToPage: (self pageNumberOf: currentPage) + 1
