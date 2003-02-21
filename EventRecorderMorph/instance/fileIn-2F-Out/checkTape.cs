e0e6726b2d774cb20019463f9dcfb24e5a539894
dgd <unknown>
2003-02-21 23:15:00 +0000

    no message

--- EventRecorderMorph/instance/fileIn-2F-Out/checkTape.cs
+++ EventRecorderMorph/instance/fileIn-2F-Out/checkTape.cs
@@ -1,5 +1,7 @@
 checkTape
 	"See if this tape was already converted to the new format"
-	tape ifNil:[^self].
-	tape size = 0 ifTrue:[^self].
-	(tape first isKindOf: Association) ifTrue:[tape _ self convertV0Tape: tape].
+
+	tape ifNil: [^self].
+	tape isEmpty ifTrue: [^self].
+	(tape first isKindOf: Association) 
+		ifTrue: [tape := self convertV0Tape: tape]
