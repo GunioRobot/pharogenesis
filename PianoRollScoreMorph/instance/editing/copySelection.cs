8aceec2b074396308da53e57121a32fa673ffe50
dgd <unknown>
2003-02-21 22:56:00 +0000

    no message

--- PianoRollScoreMorph/instance/editing/copySelection.cs
+++ PianoRollScoreMorph/instance/editing/copySelection.cs
@@ -1,5 +1,5 @@
 copySelection
-
-	selection == nil ifTrue: [^ self].
-	NotePasteBuffer _ (score tracks at: selection first)
-		copyFrom: selection second to: selection third
+	selection isNil ifTrue: [^self].
+	NotePasteBuffer := (score tracks at: selection first) 
+				copyFrom: selection second
+				to: selection third
