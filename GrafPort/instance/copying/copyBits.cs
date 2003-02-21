905dd0876f016a8c4d66815b7fd137cc923ac6a6
dgd <unknown>
2003-02-21 22:38:00 +0000

    no message

--- GrafPort/instance/copying/copyBits.cs
+++ GrafPort/instance/copying/copyBits.cs
@@ -1,8 +1,9 @@
 copyBits
 	"Override copybits to do translucency if desired"
 
-	(combinationRule >= 30 and: [combinationRule <= 31])
-		ifTrue: [alpha == nil
-					ifTrue: [self copyBitsTranslucent: 255]
-					ifFalse: [self copyBitsTranslucent: alpha]]
+	(combinationRule >= 30 and: [combinationRule <= 31]) 
+		ifTrue: 
+			[alpha isNil 
+				ifTrue: [self copyBitsTranslucent: 255]
+				ifFalse: [self copyBitsTranslucent: alpha]]
 		ifFalse: [super copyBits]
