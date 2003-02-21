d0302600a451efb4abcd5e7d313ed741f931bdf6
dgd <unknown>
2003-02-21 23:03:00 +0000

    no message

--- ScriptEditorMorph/instance/other/hibernate.cs
+++ ScriptEditorMorph/instance/other/hibernate.cs
@@ -1,10 +1,9 @@
 hibernate
-	"Possibly delete the tiles."
+	"Possibly delete the tiles, but only if using universal tiles."
 
-	| ww |
-	(ww _ self world) 
-		ifNil: [(playerScripted == nil or: [playerScripted isUniversalTiles]) ifFalse: [^ self]]
-		ifNotNil:
-			[(ww valueOfProperty: #universalTiles ifAbsent: [false]) ifFalse: [^ self]].
-
-	submorphs size > 1 ifTrue: [submorphs second delete].
+	| tw |
+	Preferences universalTiles ifFalse: [^self].
+	(tw := self findA: TwoWayScrollPane) isNil 
+		ifFalse: 
+			[self setProperty: #sizeAtHibernate toValue: self extent.	"+ tw xScrollerHeight"
+			submorphs size > 1 ifTrue: [tw delete]]
