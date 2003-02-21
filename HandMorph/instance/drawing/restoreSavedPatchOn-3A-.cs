eb804903c925b8e7c620f4679bbbb1c3407c1be3
dgd <unknown>
2003-02-21 22:49:00 +0000

    no message

--- HandMorph/instance/drawing/restoreSavedPatchOn-3A-.cs
+++ HandMorph/instance/drawing/restoreSavedPatchOn-3A-.cs
@@ -1,16 +1,17 @@
-restoreSavedPatchOn: aCanvas
+restoreSavedPatchOn: aCanvas 
 	"Clear the changed flag and restore the part of the given canvas under this hand from the previously saved patch. If necessary, handle the transition to using the hardware cursor."
 
-	hasChanged _ false.
-	savedPatch ifNotNil: [
-		aCanvas drawImage: savedPatch at: savedPatch offset.
-		self hasUserInformation ifTrue: [^self].	"cannot use hw cursor if so"
-		submorphs size > 0 ifTrue: [^self].
-		temporaryCursor ifNotNil: [^self].
+	hasChanged := false.
+	savedPatch ifNotNil: 
+			[aCanvas drawImage: savedPatch at: savedPatch offset.
+			self hasUserInformation ifTrue: [^self].	"cannot use hw cursor if so"
+			submorphs notEmpty ifTrue: [^self].
+			temporaryCursor ifNotNil: [^self].
 
-		"Make the transition to using hardware cursor. Clear savedPatch and
+			"Make the transition to using hardware cursor. Clear savedPatch and
 		 report one final damage rectangle to erase the image of the software cursor."
-		super invalidRect: (savedPatch offset extent: savedPatch extent + self shadowOffset) from: self.
-		Sensor currentCursor == Cursor normal ifFalse: [Cursor normal show].  "show hardware cursor"
-		savedPatch _ nil.
-	].
+			super invalidRect: (savedPatch offset 
+						extent: savedPatch extent + self shadowOffset)
+				from: self.
+			Sensor currentCursor == Cursor normal ifFalse: [Cursor normal show].	"show hardware cursor"
+			savedPatch := nil]
