1f7c90ddcd4098704663042ddaf156aa95e5c552
dgd <unknown>
2003-02-21 22:49:00 +0000

    no message

--- HandMorph/instance/drawing/savePatchFrom-3A-.cs
+++ HandMorph/instance/drawing/savePatchFrom-3A-.cs
@@ -1,16 +1,18 @@
-savePatchFrom: aCanvas
+savePatchFrom: aCanvas 
 	"Save the part of the given canvas under this hand as a Form and return its bounding rectangle."
+
 	"Details: The previously used patch Form is recycled when possible to reduce the burden on storage management."
 
 	| damageRect myBnds |
-	damageRect _ myBnds _ self fullBounds.
-	savedPatch ifNotNil: [
-		damageRect _ myBnds merge: (savedPatch offset extent: savedPatch extent)].
-	(savedPatch == nil or: [savedPatch extent ~= myBnds extent])
-		ifTrue: [  "allocate new patch form if needed"
-			savedPatch _ aCanvas form allocateForm: myBnds extent].
-	aCanvas
-		contentsOfArea: (myBnds translateBy: aCanvas origin)
+	damageRect := myBnds := self fullBounds.
+	savedPatch ifNotNil: 
+			[damageRect := myBnds merge: (savedPatch offset extent: savedPatch extent)].
+	(savedPatch isNil or: [savedPatch extent ~= myBnds extent]) 
+		ifTrue: 
+			["allocate new patch form if needed"
+
+			savedPatch := aCanvas form allocateForm: myBnds extent].
+	aCanvas contentsOfArea: (myBnds translateBy: aCanvas origin)
 		into: savedPatch.
 	savedPatch offset: myBnds topLeft.
-	^ damageRect
+	^damageRect
