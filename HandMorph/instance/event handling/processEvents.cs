a007badc7a9c4f40b9d1d178f1240fef52a73ca5
dgd <unknown>
2003-02-21 22:48:00 +0000

    no message

--- HandMorph/instance/event handling/processEvents.cs
+++ HandMorph/instance/event handling/processEvents.cs
@@ -1,32 +1,40 @@
 processEvents
-	"Process user input events from the local input devices.
-	An interesting problem. Morphic is always supposed to run in an event driven environment but for now it is possible that a Morphic window is run from MVC so that we have to synthesize events in the sensor. That's quite a bit of a problem because it means that we'll get endless mouse events. To compensate for this, EventSensor will always return keyboard events first if synthesized so if we're running into a mouse event AND Sensor doesn't have an event queue we can safely break out of the event processing loop. See also EventSensor>>nextEventSynthesized."
-	| evt evtBuf type hadMouse hadAny |
-	hadMouse _ hadAny _ false.
-	[(evtBuf _ Sensor nextEvent) == nil] whileFalse:[
-		evt _ nil. "for unknown event types"
-		type _ evtBuf at: 1.
-		(type = EventTypeMouse)
-			ifTrue:[evt _ self generateMouseEvent: evtBuf. hadMouse _ true].
-		(type = EventTypeKeyboard) 
-			ifTrue:[evt _ self generateKeyboardEvent: evtBuf].
-		(type = EventTypeDragDropFiles)
-			ifTrue:[evt _ self generateDropFilesEvent: evtBuf].
-		"All other events are ignored"
+	"Process user input events from the local input devices."
 
-		evt == nil ifFalse:[
-			"Finally, handle it"
-			self handleEvent: evt.
-			hadAny _ true.
+	| evt evtBuf type hadAny |
+	ActiveEvent ifNotNil: 
+			["Meaning that we were invoked from within an event response.
+		Make sure z-order is up to date"
 
-			"See the note on running a World in MVC in the method comment"
-			(evt isMouse and:[Sensor eventQueue == nil]) ifTrue:[^self].
-		].
-	].
-	(mouseClickState notNil and:[hadMouse not]) ifTrue:[
-		"No mouse events during this cycle. Make sure click states time out accordingly"
-		mouseClickState handleEvent: lastMouseEvent asMouseMove from: self].
-	hadAny ifFalse:[
-		"No pending events. Make sure z-order is up to date"
-		self mouseOverHandler processMouseOver: lastMouseEvent.
-	].
+			self mouseOverHandler processMouseOver: lastMouseEvent].
+	hadAny := false.
+	[(evtBuf := Sensor nextEvent) isNil] whileFalse: 
+			[evt := nil.	"for unknown event types"
+			type := evtBuf first.
+			type = EventTypeMouse ifTrue: [evt := self generateMouseEvent: evtBuf].
+			type = EventTypeKeyboard 
+				ifTrue: [evt := self generateKeyboardEvent: evtBuf].
+			type = EventTypeDragDropFiles 
+				ifTrue: [evt := self generateDropFilesEvent: evtBuf].
+			"All other events are ignored"
+			(type ~= EventTypeDragDropFiles and: [evt isNil]) ifTrue: [^self].
+			evt isNil 
+				ifFalse: 
+					["Finally, handle it"
+
+					self handleEvent: evt.
+					hadAny := true.
+
+					"For better user feedback, return immediately after a mouse event has been processed."
+					evt isMouse ifTrue: [^self]]].
+	"note: if we come here we didn't have any mouse events"
+	mouseClickState notNil 
+		ifTrue: 
+			["No mouse events during this cycle. Make sure click states time out accordingly"
+
+			mouseClickState handleEvent: lastMouseEvent asMouseMove from: self].
+	hadAny 
+		ifFalse: 
+			["No pending events. Make sure z-order is up to date"
+
+			self mouseOverHandler processMouseOver: lastMouseEvent]
