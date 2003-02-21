4b7ce0327697f64b062cd8f34a623d2e3d439bcc
dgd <unknown>
2003-02-21 23:03:00 +0000

    no message

--- CachingMorph/instance/as yet unclassified/updateCacheCanvas-3A-.cs
+++ CachingMorph/instance/as yet unclassified/updateCacheCanvas-3A-.cs
@@ -1,18 +1,25 @@
-updateCacheCanvas: aCanvas
+updateCacheCanvas: aCanvas 
 	"Update the cached image of the morphs being held by this hand."
 
 	| myBnds rectList |
-	myBnds _ self fullBounds.
-	(cacheCanvas == nil or: [cacheCanvas extent ~= myBnds extent]) ifTrue: [
-		cacheCanvas _ (aCanvas allocateForm: myBnds extent) getCanvas.
-		cacheCanvas translateBy: myBnds origin negated
-			during:[:tempCanvas| super fullDrawOn: tempCanvas].
-		^self].
+	myBnds := self fullBounds.
+	(cacheCanvas isNil or: [cacheCanvas extent ~= myBnds extent]) 
+		ifTrue: 
+			[cacheCanvas := (aCanvas allocateForm: myBnds extent) getCanvas.
+			cacheCanvas translateBy: myBnds origin negated
+				during: [:tempCanvas | super fullDrawOn: tempCanvas].
+			^self].
 
 	"incrementally update the cache canvas"
-	rectList _ damageRecorder invalidRectsFullBounds: (0@0 extent: myBnds extent).
+	rectList := damageRecorder 
+				invalidRectsFullBounds: (0 @ 0 extent: myBnds extent).
 	damageRecorder reset.
-	rectList do: [:r |
-		cacheCanvas translateTo: myBnds origin negated clippingTo: r during:[:c|
-			c fillColor: Color transparent.  "clear to transparent"
-			super fullDrawOn: c]].
+	rectList do: 
+			[:r | 
+			cacheCanvas 
+				translateTo: myBnds origin negated
+				clippingTo: r
+				during: 
+					[:c | 
+					c fillColor: Color transparent.	"clear to transparent"
+					super fullDrawOn: c]]
