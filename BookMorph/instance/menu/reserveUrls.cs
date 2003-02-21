55628b34e4f938bf578fdaabd0d14e2dcc06e603
dgd <unknown>
2003-02-21 23:12:00 +0000

    no message

--- BookMorph/instance/menu/reserveUrls.cs
+++ BookMorph/instance/menu/reserveUrls.cs
@@ -1,9 +1,12 @@
 reserveUrls
 	"Save a dummy version of the book first, assign all pages URLs, write dummy files to reserve the url, and write the index.  Good when I have pages with interpointing bookmarks."
 
-| stem |
-(stem _ self getStemUrl) size = 0 ifTrue: [^ self].
-pages doWithIndex: [:pg :ind | 	"does write the current page too"
-	pg url ifNil: [pg reserveUrl: stem,(ind printString),'.sp']].
+	| stem |
+	(stem := self getStemUrl) isEmpty ifTrue: [^self].
+	pages doWithIndex: 
+			[:pg :ind | 
+			"does write the current page too"
 
-"self saveIndexOnURL."
+			pg url ifNil: [pg reserveUrl: stem , ind printString , '.sp']]
+
+	"self saveIndexOnURL."
