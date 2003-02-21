b87034b169031d63a31bc1348e50e1be80964491
dgd <unknown>
2003-02-21 23:15:00 +0000

    no message

--- WordGameLetterMorph/instance/linking/morphsInWordDo-3A-.cs
+++ WordGameLetterMorph/instance/linking/morphsInWordDo-3A-.cs
@@ -1,5 +1,4 @@
-morphsInWordDo: aBlock
-
+morphsInWordDo: aBlock 
 	aBlock value: self.
-	(successor == nil or: [successor isBlank]) ifTrue: [^ self].
+	(successor isNil or: [successor isBlank]) ifTrue: [^self].
 	successor morphsInWordDo: aBlock
