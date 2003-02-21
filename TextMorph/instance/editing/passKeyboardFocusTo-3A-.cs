63d9f1626f5da8a17443303078453bbb4c4c1c03
dgd <unknown>
2003-02-21 22:29:00 +0000

    no message

--- TextMorph/instance/editing/passKeyboardFocusTo-3A-.cs
+++ TextMorph/instance/editing/passKeyboardFocusTo-3A-.cs
@@ -1,7 +1,7 @@
-passKeyboardFocusTo: otherMorph
+passKeyboardFocusTo: otherMorph 
 	| w |
-self flag: #arNote. "Do we need this?!"
-	(w _ self world) == nil ifFalse:
-		[w handsDo:
-			[:h | h keyboardFocus == self
-				ifTrue: [h newKeyboardFocus: otherMorph]]].
+	self flag: #arNote.	"Do we need this?!"
+	(w := self world) isNil 
+		ifFalse: 
+			[w 
+				handsDo: [:h | h keyboardFocus == self ifTrue: [h newKeyboardFocus: otherMorph]]]
