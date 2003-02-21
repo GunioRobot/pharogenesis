4d5226e25b1ccab7437294e5be71a164fc969ea2
dgd <unknown>
2003-02-21 23:15:00 +0000

    no message

--- WordGameLetterMorph/instance/initialization/setLetter-3A-color-3A-.cs
+++ WordGameLetterMorph/instance/initialization/setLetter-3A-color-3A-.cs
@@ -1,19 +1,19 @@
-setLetter: aLetter color: aColor
-
+setLetter: aLetter color: aColor 
 	letterMorph ifNotNil: [letterMorph delete].
-	letter _ aLetter.
-	letter ifNil: [^ letterMorph _ nil].
-	letterMorph _ StringMorph contents: aLetter asString font: LetterFont.
+	letter := aLetter.
+	letter ifNil: [^letterMorph := nil].
+	letterMorph := StringMorph contents: aLetter asString font: LetterFont.
 	letterMorph color: aColor.
-	style == #boxed
-		ifTrue:
-		[letterMorph align: letterMorph bounds bottomCenter
-					with: self bounds bottomCenter + (0@(LetterFont descent-2))]
-		ifFalse:
-		[lineMorph == nil
-		ifTrue: [letterMorph align: letterMorph bounds bottomCenter
-						with: self bounds bottomCenter + (0@(LetterFont descent-4))]
-		ifFalse: [letterMorph align: letterMorph bounds bottomCenter
-						with: self center x@(lineMorph top + LetterFont descent)]].
-	self addMorphBack: letterMorph.
-
+	style == #boxed 
+		ifTrue: 
+			[letterMorph align: letterMorph bounds bottomCenter
+				with: self bounds bottomCenter + (0 @ (LetterFont descent - 2))]
+		ifFalse: 
+			[lineMorph isNil 
+				ifTrue: 
+					[letterMorph align: letterMorph bounds bottomCenter
+						with: self bounds bottomCenter + (0 @ (LetterFont descent - 4))]
+				ifFalse: 
+					[letterMorph align: letterMorph bounds bottomCenter
+						with: self center x @ (lineMorph top + LetterFont descent)]].
+	self addMorphBack: letterMorph
