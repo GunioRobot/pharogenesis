0f4d43c95509a9040731cf64d7a3ce6e52be5842
dgd <unknown>
2003-02-21 23:14:00 +0000

    no message

--- ChineseCheckers/instance/game sequence/step.cs
+++ ChineseCheckers/instance/game sequence/step.cs
@@ -1,25 +1,28 @@
 step
+	whoseMove = 0 ifTrue: [^self].	"Game over."
+	plannedMove isNil 
+		ifTrue: 
+			[(autoPlay at: whoseMove) ifFalse: [^self].	"Waiting for a human."
+			plannedMove := (self endGameFor: whoseMove) 
+						ifTrue: 
+							["Look deeper at the end."
 
-	whoseMove = 0 ifTrue: [^ self].  "Game over."
+							self bestMove: 2 forTeam: whoseMove]
+						ifFalse: [self bestMove: 1 forTeam: whoseMove].
+			movePhase := 1	"Start the animated move"].
+	animateMoves 
+		ifTrue: 
+			["Display the move in phases..."
 
-	plannedMove == nil ifTrue:
-		[(autoPlay at: whoseMove) ifFalse: [^ self].  "Waiting for a human."
-		(self endGameFor: whoseMove)  "Look deeper at the end."
-			ifTrue: [plannedMove _ self bestMove: 2 forTeam: whoseMove]
-			ifFalse: [plannedMove _ self bestMove: 1 forTeam: whoseMove].
-		movePhase _ 1.  "Start the animated move"].
+			movePhase > 0 ifTrue: [^self showNextMoveSegment]]
+		ifFalse: 
+			["... or skip the entire animated move if requested."
 
-	animateMoves
-		ifTrue: ["Display the move in phases..."
-				movePhase > 0 ifTrue: [^ self showNextMoveSegment]]
-		ifFalse: ["... or skip the entire animated move if requested."
-				self makeMove: plannedMove.
-				(self pieceAt: plannedMove first)
-						position: (self cellPointAt: plannedMove last);
-						setBoard: self loc: plannedMove last.
-				self changed.
-				movePhase _ 0].
-
-	plannedMove _ nil.  "End the animated move"
-
+			self makeMove: plannedMove.
+			(self pieceAt: plannedMove first)
+				position: (self cellPointAt: plannedMove last);
+				setBoard: self loc: plannedMove last.
+			self changed.
+			movePhase := 0].
+	plannedMove := nil.	"End the animated move"
 	self nextTurn
