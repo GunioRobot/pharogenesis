fd1256515a32959b9d0cde6f9c8acfcb63b7af27
dgd <unknown>
2003-02-21 22:49:00 +0000

    no message

--- HandMorph/instance/cursor/showTemporaryCursor-3A-hotSpotOffset-3A-.cs
+++ HandMorph/instance/cursor/showTemporaryCursor-3A-hotSpotOffset-3A-.cs
@@ -1,14 +1,17 @@
-showTemporaryCursor: cursorOrNil hotSpotOffset: hotSpotOffset
+showTemporaryCursor: cursorOrNil hotSpotOffset: hotSpotOffset 
 	"Set the temporary cursor to the given Form.
 	If the argument is nil, revert to the normal hardware cursor."
 
 	self changed.
-	cursorOrNil == nil
-		ifTrue: [temporaryCursor _ temporaryCursorOffset _ nil]
-		ifFalse: [temporaryCursor _ cursorOrNil asCursorForm.
-				temporaryCursorOffset _ temporaryCursor offset - hotSpotOffset].
-	bounds _ self cursorBounds.
-	self 
-		userInitials: userInitials andPicture: (self userPicture);
+	temporaryCursorOffset 
+		ifNotNil: [bounds := bounds translateBy: temporaryCursorOffset negated].
+	cursorOrNil isNil 
+		ifTrue: [temporaryCursor := temporaryCursorOffset := nil]
+		ifFalse: 
+			[temporaryCursor := cursorOrNil asCursorForm.
+			temporaryCursorOffset := temporaryCursor offset - hotSpotOffset].
+	bounds := self cursorBounds.
+	self
+		userInitials: userInitials andPicture: self userPicture;
 		layoutChanged;
 		changed
