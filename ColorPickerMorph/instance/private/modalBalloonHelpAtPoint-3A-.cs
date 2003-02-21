546506f6e75cfe792b4de668e02f3b1c31d3fe6b
dgd <unknown>
2003-02-21 22:59:00 +0000

    no message

--- ColorPickerMorph/instance/private/modalBalloonHelpAtPoint-3A-.cs
+++ ColorPickerMorph/instance/private/modalBalloonHelpAtPoint-3A-.cs
@@ -1,7 +1,10 @@
-modalBalloonHelpAtPoint: cursorPoint
-	self flag: #arNote. "Throw this away. There needs to be another way."
-	self submorphsDo:
-		[:m | m wantsBalloon ifTrue:
-			[(m valueOfProperty: #balloon) == nil
-				ifTrue: [(m containsPoint: cursorPoint) ifTrue: [m showBalloon: m balloonText]]
-				ifFalse: [(m containsPoint: cursorPoint) ifFalse: [m deleteBalloon]]]]
+modalBalloonHelpAtPoint: cursorPoint 
+	self flag: #arNote.	"Throw this away. There needs to be another way."
+	self submorphsDo: 
+			[:m | 
+			m wantsBalloon 
+				ifTrue: 
+					[(m valueOfProperty: #balloon) isNil
+						ifTrue: 
+							[(m containsPoint: cursorPoint) ifTrue: [m showBalloon: m balloonText]]
+						ifFalse: [(m containsPoint: cursorPoint) ifFalse: [m deleteBalloon]]]]
