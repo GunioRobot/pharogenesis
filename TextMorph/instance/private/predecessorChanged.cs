780d44749f0ba5a502767a70af06a4e7de16ed17
dgd <unknown>
2003-02-21 22:29:00 +0000

    no message

--- TextMorph/instance/private/predecessorChanged.cs
+++ TextMorph/instance/private/predecessorChanged.cs
@@ -1,12 +1,16 @@
 predecessorChanged
 	| newStart oldStart |
-	newStart _ predecessor == nil
-		ifTrue: [1]
-		ifFalse: [predecessor lastCharacterIndex + 1].
-	(self paragraph adjustedFirstCharacterIndex ~= newStart or: [newStart >= text size])
-		ifTrue: [paragraph composeAllStartingAt: newStart.
+	(self hasProperty: #CreatingParagraph) ifTrue: [^self].
+	newStart := predecessor isNil 
+				ifTrue: [1]
+				ifFalse: [predecessor lastCharacterIndex + 1].
+	(self paragraph adjustedFirstCharacterIndex ~= newStart 
+		or: [newStart >= text size]) 
+			ifTrue: 
+				[paragraph composeAllStartingAt: newStart.
 				self fit]
-		ifFalse: ["If the offset to end of text has not changed, just slide"
-				oldStart _ self firstCharacterIndex.
-				self withSuccessorsDo:
-					[:m | m adjustLineIndicesBy: newStart - oldStart]].
+			ifFalse: 
+				["If the offset to end of text has not changed, just slide"
+
+				oldStart := self firstCharacterIndex.
+				self withSuccessorsDo: [:m | m adjustLineIndicesBy: newStart - oldStart]]
