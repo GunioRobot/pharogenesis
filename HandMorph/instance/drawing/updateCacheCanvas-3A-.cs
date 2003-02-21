dd4ce84949a2a90fad2b22a8fcbcc60b5032c885
dgd <unknown>
2003-02-21 22:49:00 +0000

    no message

--- HandMorph/instance/drawing/updateCacheCanvas-3A-.cs
+++ HandMorph/instance/drawing/updateCacheCanvas-3A-.cs
@@ -1,33 +1,50 @@
-updateCacheCanvas: aCanvas
+updateCacheCanvas: aCanvas 
 	"Update the cached image of the morphs being held by this hand."
-	| subBnds rectList |
+
 	"Note: The following is an attempt to quickly get out if there's no change"
-	subBnds _ Rectangle merging: (submorphs collect: [:m | m fullBounds]).
-	rectList _ damageRecorder invalidRectsFullBounds: (0@0 extent: subBnds extent).
-	(rectList isEmpty and:[cacheCanvas notNil and:[cacheCanvas extent = subBnds extent]])
-		ifTrue:[^self].
+
+	| subBnds rectList nPix |
+	subBnds := Rectangle merging: (submorphs collect: [:m | m fullBounds]).
+	rectList := damageRecorder invalidRectsFullBounds: subBnds.
+	damageRecorder reset.
+	(rectList isEmpty 
+		and: [cacheCanvas notNil and: [cacheCanvas extent = subBnds extent]]) 
+			ifTrue: [^self].
 
 	"Always check for real translucency -- can't be cached in a form"
-	self allMorphsDo: [:m |
-		m hasTranslucentColor ifTrue: [
-			cacheCanvas _ nil.
-			cachedCanvasHasHoles _ true.
-			^ self]].
-
-	(cacheCanvas == nil or: [cacheCanvas extent ~= subBnds extent]) ifTrue: [
-		cacheCanvas _ (aCanvas allocateForm: subBnds extent) getCanvas.
-		cacheCanvas translateBy: subBnds origin negated
-			during:[:tempCanvas| self drawSubmorphsOn: tempCanvas].
-		self submorphsDo:
-			[:m | (m areasRemainingToFill: subBnds) isEmpty
-				ifTrue: [^ cachedCanvasHasHoles _ false]].
-		cachedCanvasHasHoles _ (cacheCanvas form tallyPixelValues at: 1) > 0.
-		^ self].
+	self submorphsDo: 
+			[:m | 
+			m wantsToBeCachedByHand 
+				ifFalse: 
+					[cacheCanvas := nil.
+					cachedCanvasHasHoles := true.
+					^self]].
+	(cacheCanvas isNil or: [cacheCanvas extent ~= subBnds extent]) 
+		ifTrue: 
+			[cacheCanvas := (aCanvas allocateForm: subBnds extent) getCanvas.
+			cacheCanvas translateBy: subBnds origin negated
+				during: [:tempCanvas | self drawSubmorphsOn: tempCanvas].
+			self submorphsDo: 
+					[:m | 
+					(m areasRemainingToFill: subBnds) isEmpty 
+						ifTrue: [^cachedCanvasHasHoles := false]].
+			nPix := cacheCanvas form tallyPixelValues first.
+			"--> begin rounded corners hack <---"
+			cachedCanvasHasHoles := (nPix = 48 
+						and: [submorphs size = 1 and: [submorphs first wantsRoundedCorners]]) 
+							ifTrue: [false]
+							ifFalse: [nPix > 0].
+			"--> end rounded corners hack <---"
+			^self].
 
 	"incrementally update the cache canvas"
-	rectList _ damageRecorder invalidRectsFullBounds: (0@0 extent: subBnds extent).
-	damageRecorder reset.
-	rectList do: [:r |
-		cacheCanvas translateTo: subBnds origin negated clippingTo: r during:[:c|
-			c fillColor: Color transparent.  "clear to transparent"
-			self drawSubmorphsOn: c]].
+	cacheCanvas translateBy: subBnds origin negated
+		during: 
+			[:cc | 
+			rectList do: 
+					[:r | 
+					cc clipBy: r
+						during: 
+							[:c | 
+							c fillColor: Color transparent.
+							self drawSubmorphsOn: c]]]
