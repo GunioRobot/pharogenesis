de79d0ffd82a2a8e090566004ed4f1d80347c5f5
dgd <unknown>
2003-02-21 23:02:00 +0000

    no message

--- PluggableTextMorph/instance/model access/getSelection.cs
+++ PluggableTextMorph/instance/model access/getSelection.cs
@@ -1,5 +1,5 @@
 getSelection
 	"Answer the model's selection interval."
 
-	getSelectionSelector == nil ifTrue: [^ 1 to: 0].  "null selection"
-	^ model perform: getSelectionSelector
+	getSelectionSelector isNil ifTrue: [^1 to: 0].	"null selection"
+	^model perform: getSelectionSelector
