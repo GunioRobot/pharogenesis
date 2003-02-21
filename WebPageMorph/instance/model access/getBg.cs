f55263203a0bad53bcba225ee675692ef0096cb3
dgd <unknown>
2003-02-21 23:18:00 +0000

    no message

--- WebPageMorph/instance/model access/getBg.cs
+++ WebPageMorph/instance/model access/getBg.cs
@@ -1,6 +1,5 @@
 getBg
 	"Retrieve the current model background color/image"
 
-	getBgSelector == nil ifTrue: [^Color white].
-	^ (model perform: getBgSelector) ifNil: [Color white]
-	
+	getBgSelector isNil ifTrue: [^Color white].
+	^(model perform: getBgSelector) ifNil: [Color white]
