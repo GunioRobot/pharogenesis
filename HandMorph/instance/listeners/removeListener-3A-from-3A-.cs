bdb355f8f69f369ecba05ae9a5417e58f235d6ef
dgd <unknown>
2003-02-21 22:48:00 +0000

    no message

--- HandMorph/instance/listeners/removeListener-3A-from-3A-.cs
+++ HandMorph/instance/listeners/removeListener-3A-from-3A-.cs
@@ -1,9 +1,10 @@
-removeListener: anObject from: aListenerGroup
+removeListener: anObject from: aListenerGroup 
 	"Remove anObject from the given listener group. Return the new group."
+
 	| listeners |
-	aListenerGroup ifNil:[^nil].
-	listeners _ aListenerGroup.
-	listeners _ listeners copyWithout: anObject.
-	listeners _ listeners copyWithout: nil. "obsolete entries"
-	listeners size = 0 ifTrue:[listeners _ nil].
+	aListenerGroup ifNil: [^nil].
+	listeners := aListenerGroup.
+	listeners := listeners copyWithout: anObject.
+	listeners := listeners copyWithout: nil.	"obsolete entries"
+	listeners isEmpty ifTrue: [listeners := nil].
 	^listeners
