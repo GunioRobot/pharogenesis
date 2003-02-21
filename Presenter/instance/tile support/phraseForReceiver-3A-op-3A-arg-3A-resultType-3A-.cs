17dbc6991648ea35bd022a567b3a0f3b52299716
dgd <unknown>
2003-02-21 22:35:00 +0000

    no message

--- Presenter/instance/tile support/phraseForReceiver-3A-op-3A-arg-3A-resultType-3A-.cs
+++ Presenter/instance/tile support/phraseForReceiver-3A-op-3A-arg-3A-resultType-3A-.cs
@@ -1,21 +1,25 @@
-phraseForReceiver: rcvr op: op arg: arg resultType: resultType
+phraseForReceiver: rcvr op: op arg: arg resultType: resultType 
+	"Answer a PhraseTileMorph affiliated with the given receiver, initialized to hold the given operator, argument, and result type"
 
 	| m argTile rcvrTile |
-	arg == nil
-		ifTrue:
-			[m _ PhraseTileMorph new setOperator: op
-				type: resultType
-				rcvrType: (self typeForConstant: rcvr)]
-		ifFalse:
-			[m _ PhraseTileMorph new setOperator: op
-				type: resultType
-				rcvrType: (self typeForConstant: rcvr)
-				argType: (self typeForConstant: arg).
-			argTile _ self constantTile: arg.
+	arg isNil 
+		ifTrue: 
+			[m := PhraseTileMorph new 
+						setOperator: op
+						type: resultType
+						rcvrType: (self typeForConstant: rcvr)]
+		ifFalse: 
+			[m := PhraseTileMorph new 
+						setOperator: op
+						type: resultType
+						rcvrType: (self typeForConstant: rcvr)
+						argType: (self typeForConstant: arg).
+			argTile := self constantTile: arg.
 			argTile position: m lastSubmorph position.
 			m lastSubmorph addMorph: argTile].
-	rcvrTile _ self constantTile: rcvr.
-"	TilePadMorph makeReceiverColorOfResultType ifTrue: [rcvrTile color: m color]."
+	rcvrTile := self constantTile: rcvr.
+	"	TilePadMorph makeReceiverColorOfResultType ifTrue: [rcvrTile color: m color]."
 	rcvrTile position: m firstSubmorph position.
 	m firstSubmorph addMorph: rcvrTile.
-	^ m
+	m vResizing: #shrinkWrap.
+	^m
