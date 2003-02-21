25ac587d17e9345e1e1ceddbc993fa9ae9a2226b
dgd <unknown>
2003-02-21 23:05:00 +0000

    no message

--- PluggableListMorph/instance/model access/modifierKeyPressed-3A-.cs
+++ PluggableListMorph/instance/model access/modifierKeyPressed-3A-.cs
@@ -1,10 +1,12 @@
 modifierKeyPressed: aChar 
 	| args |
-	keystrokeActionSelector == nil ifTrue: [^ nil].
-	args _ keystrokeActionSelector numArgs.
-	args = 1 ifTrue: [^ model perform: keystrokeActionSelector with: aChar].
-	args = 2 ifTrue: [^ model
-			perform: keystrokeActionSelector
-			with: aChar
-			with: self].
-	^ self error: 'keystrokeActionSelector must be a 1- or 2-keyword symbol'
+	keystrokeActionSelector isNil ifTrue: [^nil].
+	args := keystrokeActionSelector numArgs.
+	args = 1 ifTrue: [^model perform: keystrokeActionSelector with: aChar].
+	args = 2 
+		ifTrue: 
+			[^model 
+				perform: keystrokeActionSelector
+				with: aChar
+				with: self].
+	^self error: 'keystrokeActionSelector must be a 1- or 2-keyword symbol'
