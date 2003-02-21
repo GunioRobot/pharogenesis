497c64b3a905e6529efbd46546ec891c73b93fac
dgd <unknown>
2003-02-21 22:41:00 +0000

    no message

--- PluggableButtonMorph/instance/private/getModelState.cs
+++ PluggableButtonMorph/instance/private/getModelState.cs
@@ -1,6 +1,6 @@
 getModelState
 	"Answer the result of sending the receiver's model the getStateSelector message."
 
-	getStateSelector == nil
-		ifTrue: [^ false]
-		ifFalse: [^ model perform: getStateSelector].
+	^ getStateSelector isNil 
+		ifTrue: [false]
+		ifFalse: [model perform: getStateSelector]
