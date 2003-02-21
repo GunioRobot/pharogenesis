519539b13dce82186c880ae0d537d06ff7e03bdd
dgd <unknown>
2003-02-21 23:04:00 +0000

    no message

--- FishEyeMorph/instance/initialization/transformY-3A-.cs
+++ FishEyeMorph/instance/initialization/transformY-3A-.cs
@@ -1,15 +1,15 @@
-transformY: aFloatArray
+transformY: aFloatArray 
 	| focus subArray dMaxY |
-
-	focus _ srcExtent y asFloat / 2.
-
-	(aFloatArray at: 1) <= focus ifTrue: [
-		dMaxY _ 0.0 - focus.
-	] ifFalse: [
-		dMaxY _ focus.    " = (size - focus)".
-	].
-		
-	subArray _ self g: (aFloatArray copyFrom: 1 to: gridNum x + 1) max: dMaxY focus: focus.
-
-	aFloatArray replaceFrom: 1 to: gridNum x + 1 with: subArray startingAt: 1.
-
+	focus := srcExtent y asFloat / 2.
+	dMaxY := (aFloatArray first) <= focus 
+				ifTrue: [0.0 - focus]
+				ifFalse: [focus].
+	subArray := self 
+				g: (aFloatArray copyFrom: 1 to: gridNum x + 1)
+				max: dMaxY
+				focus: focus.
+	aFloatArray 
+		replaceFrom: 1
+		to: gridNum x + 1
+		with: subArray
+		startingAt: 1
