48d462c1961fb5158302e4146edf5946bdb175cf
dgd <unknown>
2003-02-21 22:31:00 +0000

    no message

--- TextMorph/instance/scripting access/setAllButFirstCharacter-3A-.cs
+++ TextMorph/instance/scripting access/setAllButFirstCharacter-3A-.cs
@@ -1,14 +1,13 @@
-setAllButFirstCharacter: source
+setAllButFirstCharacter: source 
 	"Set all but the first char of the receiver to the source"
-
 	| aChar chars |
 	aChar _ source asCharacter.
-	(chars _ self getCharacters) size > 0 
-		ifFalse:
-			[self newContents: ('¥', source asString)]
-		ifTrue:
-			[(chars first = aChar) ifFalse:
-				[self newContents: (String streamContents:
-					[:aStream |
-						aStream nextPut: chars first.
-						aStream nextPutAll: source])]]
+	(chars _ self getCharacters) isEmpty
+		ifTrue: [self newContents: 'Â·' , source asString]
+		ifFalse: [chars first = aChar
+				ifFalse: [""
+					self
+						newContents: (String
+								streamContents: [:aStream | 
+									aStream nextPut: chars first.
+									aStream nextPutAll: source])]] 
