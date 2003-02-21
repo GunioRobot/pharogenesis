15eae8b348ceda71bbf08f1f46cdd94869516c62
dgd <unknown>
2003-02-21 23:05:00 +0000

    no message

--- PluggableListMorph/instance/model access/getCurrentSelectionIndex.cs
+++ PluggableListMorph/instance/model access/getCurrentSelectionIndex.cs
@@ -1,5 +1,5 @@
 getCurrentSelectionIndex
 	"Answer the index of the current selection."
 
-	getIndexSelector == nil ifTrue: [^ 0].
-	^ model perform: getIndexSelector
+	getIndexSelector isNil ifTrue: [^0].
+	^model perform: getIndexSelector
