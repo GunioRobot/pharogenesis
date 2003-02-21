2bc2359bc85183f3416c31ed8f0cdf65cc9639e0
dgd <unknown>
2003-02-21 22:26:00 +0000

    no message

--- TextMorph/instance/linked frames/firstInChain.cs
+++ TextMorph/instance/linked frames/firstInChain.cs
@@ -1,7 +1,7 @@
 firstInChain
 	"Return the first morph in a chain of textMorphs"
+
 	| first |
-	first _ self.  
-	[first predecessor == nil]
-		whileFalse: [first _ first predecessor].
-	^ first
+	first := self.
+	[first predecessor isNil] whileFalse: [first := first predecessor].
+	^first
