96b5566257bcdb0d23064c77a2c733770df4babd
dgd <unknown>
2003-02-22 18:49:00 +0000

    no message

--- PlayingCardDeck/instance/events/doubleClickOnCard-3A-.cs
+++ PlayingCardDeck/instance/events/doubleClickOnCard-3A-.cs
@@ -1,4 +1,7 @@
-doubleClickOnCard: aCard
-
-	(target ~~ nil and: [cardDoubleClickSelector ~~ nil]) 
-		ifTrue: [^ target perform: cardDoubleClickSelector with: self with: aCard]
+doubleClickOnCard: aCard 
+	(target notNil and: [cardDoubleClickSelector notNil]) 
+		ifTrue: 
+			[^target 
+				perform: cardDoubleClickSelector
+				with: self
+				with: aCard]
