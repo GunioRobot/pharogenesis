27bdb1c11265d68966f3d42b678c4e3581e56811
dgd <unknown>
2003-02-22 18:56:00 +0000

    no message

--- PolygonMorph/instance/drawing/drawArrowOn-3A-at-3A-from-3A-.cs
+++ PolygonMorph/instance/drawing/drawArrowOn-3A-at-3A-from-3A-.cs
@@ -1,18 +1,19 @@
 drawArrowOn: aCanvas at: endPoint from: priorPoint 
 	"Draw a triangle oriented along the line from priorPoint to  
 	endPoint. Answer the wingBase."
+
 	| pts spec wingBase |
-	pts _ self arrowBoundsAt: endPoint from: priorPoint.
-	wingBase _ pts size = 4 ifTrue: [ pts at: 3 ]
-		ifFalse: [ (pts copyFrom: 2 to: 3) average ].
-	spec _ self
-				valueOfProperty: #arrowSpec
-				ifAbsent: [5 @ 4].
-	spec x sign = spec y sign
+	pts := self arrowBoundsAt: endPoint from: priorPoint.
+	wingBase := pts size = 4 
+				ifTrue: [pts third]
+				ifFalse: [(pts copyFrom: 2 to: 3) average].
+	spec := self valueOfProperty: #arrowSpec ifAbsent: [5 @ 4].
+	spec x sign = spec y sign 
 		ifTrue: [aCanvas drawPolygon: pts fillStyle: borderColor]
-		ifFalse: [aCanvas
+		ifFalse: 
+			[aCanvas 
 				drawPolygon: pts
 				fillStyle: Color transparent
-				borderWidth: borderWidth + 1 // 2
+				borderWidth: (borderWidth + 1) // 2
 				borderColor: borderColor].
 	^wingBase
