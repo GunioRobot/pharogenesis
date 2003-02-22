084fd9137919b6d46c04a4904eb36950bbf41df3
dgd <unknown>
2003-02-22 13:19:00 +0000

    no message

--- CanvasDecoder/instance/decoding/drawRect-3A-.cs
+++ CanvasDecoder/instance/decoding/drawRect-3A-.cs
@@ -1,15 +1,19 @@
-drawRect: command
+drawRect: command 
 	| verb rectEnc fillColorEnc borderWidthEnc borderColorEnc rect fillColor borderWidth borderColor |
-	verb := command at: 1.
-	rectEnc := command at: 2.
-	fillColorEnc := command at: 3.
-	borderWidthEnc := command at: 4.
-	borderColorEnc := command at: 5.
-
+	verb _ command first.
+	rectEnc _ command second.
+	fillColorEnc _ command third.
+	borderWidthEnc _ command fourth.
+	borderColorEnc _ command fifth.
+	""
 	rect _ self class decodeRectangle: rectEnc.
 	fillColor _ self class decodeColor: fillColorEnc.
 	borderWidth _ self class decodeInteger: borderWidthEnc.
 	borderColor _ self class decodeColor: borderColorEnc.
-
-	self drawCommand: [ :c |
-		c frameAndFillRectangle: rect  fillColor: fillColor  borderWidth: borderWidth  borderColor: borderColor ]
+	""
+	self
+		drawCommand: [:c | c
+				frameAndFillRectangle: rect
+				fillColor: fillColor
+				borderWidth: borderWidth
+				borderColor: borderColor]
