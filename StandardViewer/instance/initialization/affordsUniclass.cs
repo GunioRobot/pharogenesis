351639e86588e17a51d8a23b3ddcda631e682411
gm <unknown>
2003-02-22 13:44:00 +0000

    no message

--- StandardViewer/instance/initialization/affordsUniclass.cs
+++ StandardViewer/instance/initialization/affordsUniclass.cs
@@ -2,7 +2,6 @@ affordsUniclass
 	"Answer true iff the receiver operates on behalf of an object that is, or could become, a member of a Uniclass"
 
 	| viewee |
-	^ (viewee _ self objectViewed) belongsToUniClass or: 
-			[((viewee isKindOf: Integer) not and: 
-				[(viewee isKindOf: Behavior) not]) and:
-					[self userLevel > 0]]
+	^(viewee := self objectViewed) belongsToUniClass or: 
+			[((viewee isInteger) not and: [viewee isBehavior not]) 
+				and: [self userLevel > 0]]
