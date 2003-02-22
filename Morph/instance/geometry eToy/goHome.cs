adce488a814acb0b4256383ab10868345d3f5713
dgd <unknown>
2003-02-22 19:05:00 +0000

    no message

--- Morph/instance/geometry eToy/goHome.cs
+++ Morph/instance/geometry eToy/goHome.cs
@@ -1,11 +1,13 @@
 goHome
-
 	| box |
-	(owner isInMemory and: [owner ~~ nil]) ifTrue: [
-		self visible ifTrue: [
-			box _ owner.
-			self left < box left ifTrue: [self position: box left@self position y].
-			self right > box right ifTrue: [self position: (box right - self width)@self position y].
-			self top < box top ifTrue: [self position: self position x@box top].
-			self bottom > box bottom ifTrue: [
-				self position: self position x@(box bottom - self height)]]].
+	(owner isInMemory and: [owner notNil]) 
+		ifTrue: 
+			[self visible 
+				ifTrue: 
+					[box := owner.
+					self left < box left ifTrue: [self position: box left @ self position y].
+					self right > box right 
+						ifTrue: [self position: (box right - self width) @ self position y].
+					self top < box top ifTrue: [self position: self position x @ box top].
+					self bottom > box bottom 
+						ifTrue: [self position: self position x @ (box bottom - self height)]]]
