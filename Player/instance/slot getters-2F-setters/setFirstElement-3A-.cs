2e2e820e4f1161fa712195d671bfbee8049480e4
dgd <unknown>
2003-02-22 13:45:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/setFirstElement-3A-.cs
+++ Player/instance/slot getters-2F-setters/setFirstElement-3A-.cs
@@ -1,6 +1,7 @@
-setFirstElement: aPlayer
+setFirstElement: aPlayer 
 	"Caution - this is a replacement operation!  Replace the receiver's costume's first element with the morph represented by aPlayer"
 
 	| aCostume |
-	(aPlayer == self or: [(aCostume _ self costume) submorphs size == 0]) ifTrue: [^ self].
+	(aPlayer == self or: [(aCostume := self costume) submorphs isEmpty]) 
+		ifTrue: [^self].
 	costume replaceSubmorph: aCostume submorphs first by: aPlayer costume
