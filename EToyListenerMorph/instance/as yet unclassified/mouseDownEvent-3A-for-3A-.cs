34bf02f540ac08999cea5d657525f299af4001da
dgd <unknown>
2003-02-22 18:59:00 +0000

    no message

--- EToyListenerMorph/instance/as yet unclassified/mouseDownEvent-3A-for-3A-.cs
+++ EToyListenerMorph/instance/as yet unclassified/mouseDownEvent-3A-for-3A-.cs
@@ -1,20 +1,14 @@
-mouseDownEvent: event for: aMorph
-
+mouseDownEvent: event for: aMorph 
 	| menu selection depictedObject |
-
-	depictedObject _ aMorph firstSubmorph valueOfProperty: #depictedObject.
-	menu _ CustomMenu new.
-	menu 
-		add: 'Grab' 
-		action: [
-			event hand attachMorph: depictedObject veryDeepCopy
-		];
-		add: 'Delete' 
-		action: [
-			self class removeFromGlobalIncomingQueue: depictedObject.
-			self rebuild.
-		].
-	selection _ menu build startUpCenteredWithCaption: 'Morph from ',
-					(aMorph submorphs at: 2) firstSubmorph contents.
+	depictedObject := aMorph firstSubmorph valueOfProperty: #depictedObject.
+	menu := CustomMenu new.
+	menu
+		add: 'Grab' action: [event hand attachMorph: depictedObject veryDeepCopy];
+		add: 'Delete'
+			action: 
+				[self class removeFromGlobalIncomingQueue: depictedObject.
+				self rebuild].
+	selection := menu build startUpCenteredWithCaption: 'Morph from ' 
+						, (aMorph submorphs second) firstSubmorph contents.
 	selection ifNil: [^self].
-	selection value.
+	selection value
