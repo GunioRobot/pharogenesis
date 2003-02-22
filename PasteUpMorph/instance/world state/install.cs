779dfb87f75321906974771c592de5802f2c862d
dgd <unknown>
2003-02-22 14:10:00 +0000

    no message

--- PasteUpMorph/instance/world state/install.cs
+++ PasteUpMorph/instance/world state/install.cs
@@ -1,14 +1,21 @@
 install
-
-	owner _ nil.	"since we may have been inside another world previously"
-
-	submorphs do: [:ss | ss owner == nil ifTrue: [ss privateOwner: self]].
-		"Transcript that was in outPointers and then got deleted."
+	owner := nil.	"since we may have been inside another world previously"
+	ActiveWorld := self.
+	ActiveHand := self hands first.	"default"
+	ActiveEvent := nil.
+	submorphs do: [:ss | ss owner isNil ifTrue: [ss privateOwner: self]].
+	"Transcript that was in outPointers and then got deleted."
 	self viewBox: Display boundingBox.
-	Sensor eventQueue: SharedQueue new.
+	Sensor flushAllButDandDEvents.
 	worldState handsDo: [:h | h initForEvents].
-
 	self installFlaps.
-
+	self borderWidth: 0.	"default"
+	(Preferences showSecurityStatus 
+		and: [SecurityManager default isInRestrictedMode]) 
+			ifTrue: 
+				[self
+					borderWidth: 2;
+					borderColor: Color red].
+	self presenter allExtantPlayers do: [:player | player prepareToBeRunning].
 	SystemWindow noteTopWindowIn: self.
-	self displayWorldSafely.
+	self displayWorldSafely
