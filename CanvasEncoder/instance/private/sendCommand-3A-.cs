f42f356decc5e0534d0a36b04a43cdf4f15cd062
dgd <unknown>
2003-02-22 14:41:00 +0000

    no message

--- CanvasEncoder/instance/private/sendCommand-3A-.cs
+++ CanvasEncoder/instance/private/sendCommand-3A-.cs
@@ -1,16 +1,16 @@
-sendCommand: stringArray
-
+sendCommand: stringArray 
 	| bucket |
-	connection ifNil: [ ^self ].
-	connection isConnected ifFalse: [ ^self ].
+	connection ifNil: [^self].
+	connection isConnected ifFalse: [^self].
 	connection nextPut: stringArray.
-	
 	SentTypesAndSizes ifNil: [^self].
-	bucket _ SentTypesAndSizes at: stringArray first ifAbsentPut: [{0. 0. 0}].
-	bucket at: 1 put: (bucket at: 1) + 1.
-	bucket at: 2 put: (bucket at: 2) + (
-		stringArray inject: 4 into: [ :sum :array | sum + (array size + 4) ]
-	).
-
-
-	
+	bucket := SentTypesAndSizes at: stringArray first
+				ifAbsentPut: 
+					[{ 
+						0.
+						0.
+						0}].
+	bucket at: 1 put: bucket first + 1.
+	bucket at: 2
+		put: (bucket second) 
+				+ (stringArray inject: 4 into: [:sum :array | sum + (array size + 4)])
