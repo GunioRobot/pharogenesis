6633d6dddbe89587f9b750242dd070360346a403
dgd <unknown>
2003-02-22 14:39:00 +0000

    no message

--- SystemWindow/instance/layout/convertAlignment.cs
+++ SystemWindow/instance/layout/convertAlignment.cs
@@ -1,56 +1,91 @@
 convertAlignment
+	"Primarily Jesse Welton's code to convert old system windows to ones with modern layout scheme"
+
 	| frame |
 	self layoutPolicy: ProportionalLayout new.
-	(paneMorphs == nil or:[paneRects == nil or:[paneMorphs size ~= paneRects size]]) ifFalse:[
-		self addLabelArea.
-		self putLabelItemsInLabelArea.
-		self setFramesForLabelArea.
-		paneMorphs with: paneRects do:[:m :r|
-			frame _ LayoutFrame new.
-			frame leftFraction: r left; rightFraction: r right; topFraction: r top; bottomFraction: r bottom.
-			m layoutFrame: frame.
-			m hResizing: #spaceFill; vResizing: #spaceFill.
-		].
-	].
-	labelArea ifNil: [
-		self addLabelArea.
-		self putLabelItemsInLabelArea.
-		self setFramesForLabelArea.
-		paneMorphs ifNotNil: [
-			paneMorphs do: [:m |
-				frame := m layoutFrame ifNil: [LayoutFrame new].
-				frame topOffset: (frame topOffset ifNil: [0]) - self labelHeight.
-				(frame bottomFraction ~= 1.0) ifTrue:
-					[ frame bottomOffset: (frame bottomOffset ifNil: [0]) - self labelHeight ].
-			].
-		].
-	].
-	label ifNotNil:[
-		frame _ LayoutFrame new.
-		frame leftFraction: 0.5; topFraction: 0; leftOffset: label width negated // 2.
-		label layoutFrame: frame].
-	collapseBox ifNotNil:[
-		frame _ LayoutFrame new.
-		frame rightFraction: 1; topFraction: 0; rightOffset: -1; topOffset: 1.
-		collapseBox layoutFrame: frame].
-	stripes ifNotNil:[
-		frame _ LayoutFrame new.
-		frame leftFraction: 0; topFraction: 0; rightFraction: 1;
-				leftOffset: 1; topOffset: 1; rightOffset: -1.
-		stripes first layoutFrame: frame.
-		stripes first height: self labelHeight - 2.
-		stripes first hResizing: #spaceFill.
-		frame _ LayoutFrame new.
-		frame leftFraction: 0; topFraction: 0; rightFraction: 1;
-				leftOffset: 3; topOffset: 3; rightOffset: -3.
-		stripes last layoutFrame: frame.
-		stripes last height: self labelHeight - 6.
-		stripes last hResizing: #spaceFill].
-	menuBox ifNotNil:[
-		frame _ LayoutFrame new.
-		frame leftFraction: 0; leftOffset: 19; topFraction: 0; topOffset: 1.
-		menuBox layoutFrame: frame].
-	closeBox ifNotNil:[
-		frame _ LayoutFrame new.
-		frame leftFraction: 0; leftOffset: 4; topFraction: 0; topOffset: 1.
-		closeBox layoutFrame: frame].
+	(paneMorphs isNil 
+		or: [paneRects isNil or: [paneMorphs size ~= paneRects size]]) 
+			ifFalse: 
+				[self addLabelArea.
+				self putLabelItemsInLabelArea.
+				self setFramesForLabelArea.
+				paneMorphs with: paneRects
+					do: 
+						[:m :r | 
+						frame := LayoutFrame new.
+						frame
+							leftFraction: r left;
+							rightFraction: r right;
+							topFraction: r top;
+							bottomFraction: r bottom.
+						m layoutFrame: frame.
+						m
+							hResizing: #spaceFill;
+							vResizing: #spaceFill]].
+	(labelArea isNil and: [self wantsLabel]) 
+		ifTrue: 
+			[self addLabelArea.
+			self putLabelItemsInLabelArea.
+			self setFramesForLabelArea.
+			paneMorphs ifNotNil: 
+					[paneMorphs do: 
+							[:m | 
+							frame := m layoutFrame ifNil: [LayoutFrame new].
+							frame topOffset: (frame topOffset ifNil: [0]) - self labelHeight.
+							frame bottomFraction ~= 1.0 
+								ifTrue: 
+									[frame bottomOffset: (frame bottomOffset ifNil: [0]) - self labelHeight]]]].
+	label ifNotNil: 
+			[frame := LayoutFrame new.
+			frame
+				leftFraction: 0.5;
+				topFraction: 0;
+				leftOffset: label width negated // 2.
+			label layoutFrame: frame].
+	collapseBox ifNotNil: 
+			[frame := LayoutFrame new.
+			frame
+				rightFraction: 1;
+				topFraction: 0;
+				rightOffset: -1;
+				topOffset: 1.
+			collapseBox layoutFrame: frame].
+	stripes ifNotNil: 
+			[frame := LayoutFrame new.
+			frame
+				leftFraction: 0;
+				topFraction: 0;
+				rightFraction: 1;
+				leftOffset: 1;
+				topOffset: 1;
+				rightOffset: -1.
+			stripes first layoutFrame: frame.
+			stripes first height: self labelHeight - 2.
+			stripes first hResizing: #spaceFill.
+			frame := LayoutFrame new.
+			frame
+				leftFraction: 0;
+				topFraction: 0;
+				rightFraction: 1;
+				leftOffset: 3;
+				topOffset: 3;
+				rightOffset: -3.
+			stripes last layoutFrame: frame.
+			stripes last height: self labelHeight - 6.
+			stripes last hResizing: #spaceFill].
+	menuBox ifNotNil: 
+			[frame := LayoutFrame new.
+			frame
+				leftFraction: 0;
+				leftOffset: 19;
+				topFraction: 0;
+				topOffset: 1.
+			menuBox layoutFrame: frame].
+	closeBox ifNotNil: 
+			[frame := LayoutFrame new.
+			frame
+				leftFraction: 0;
+				leftOffset: 4;
+				topFraction: 0;
+				topOffset: 1.
+			closeBox layoutFrame: frame]
