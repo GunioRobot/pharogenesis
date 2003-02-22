2431382ec598586ae95304bbfc378fcfa53ffbbc
dgd <unknown>
2003-02-22 18:44:00 +0000

    no message

--- CanvasDecoder/instance/decoding/drawText-3A-.cs
+++ CanvasDecoder/instance/decoding/drawText-3A-.cs
@@ -1,15 +1,16 @@
-drawText: command
-	| boundsEnc colorEnc  text bounds color fontIndexEnc fontIndex |
-
-	text := command at: 2.
-	boundsEnc := command at: 3.
-	fontIndexEnc := command at: 4.
-	colorEnc := command at: 5.
-
-
-	bounds _ self class decodeRectangle: boundsEnc.
+drawText: command 
+	| boundsEnc colorEnc text bounds color fontIndexEnc fontIndex |
+	text := command second.
+	boundsEnc := command third.
+	fontIndexEnc := command fourth.
+	colorEnc := command fifth.
+	bounds := self class decodeRectangle: boundsEnc.
 	fontIndex := self class decodeInteger: fontIndexEnc.
-	color _ self class decodeColor: colorEnc.
-
-	self drawCommand: [ :c |
-		c text: text bounds: bounds font: (fonts at: fontIndex) color: color ]
+	color := self class decodeColor: colorEnc.
+	self drawCommand: 
+			[:c | 
+			c 
+				drawString: text
+				in: bounds
+				font: (fonts at: fontIndex)
+				color: color]
