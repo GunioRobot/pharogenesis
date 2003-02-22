4ab20a101afb655a5ae954df5e63b7d0229c5220
dgd <unknown>
2003-02-22 18:54:00 +0000

    no message

--- SimpleButtonMorph/instance/menu/setTarget-3A-.cs
+++ SimpleButtonMorph/instance/menu/setTarget-3A-.cs
@@ -1,7 +1,6 @@
-setTarget: evt
-
+setTarget: evt 
 	| rootMorphs |
-	rootMorphs _ self world rootMorphsAt: evt hand targetOffset.
-	rootMorphs size > 1
-		ifTrue: [target _ rootMorphs at: 2]
-		ifFalse: [target _ nil. ^ self].
+	rootMorphs := self world rootMorphsAt: evt hand targetOffset.
+	target := rootMorphs size > 1 
+		ifTrue: [rootMorphs second]
+		ifFalse: [nil]
