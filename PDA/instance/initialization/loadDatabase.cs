632516e92589053f78c3bd0607d00be59fb1d92c
dgd <unknown>
2003-02-22 13:27:00 +0000

    no message

--- PDA/instance/initialization/loadDatabase.cs
+++ PDA/instance/initialization/loadDatabase.cs
@@ -1,21 +1,19 @@
 loadDatabase
-
 	| aName aFileStream list |
-	aName _ Utilities chooseFileWithSuffixFromList: #('.pda' '.pda.gz')
-		withCaption: 'Choose a file to load'.
-	aName ifNil: [^ self].  "User made no choice"
-	aName == #none ifTrue: [^ self inform: 
-'Sorry, no suitable files found
+	aName _ Utilities chooseFileWithSuffixFromList: #('.pda' '.pda.gz' ) withCaption: 'Choose a file to load'.
+	aName
+		ifNil: [^ self].
+	"User made no choice"
+	aName == #none
+		ifTrue: [^ self inform: 'Sorry, no suitable files found
 (names should end with .data or .data.gz)'].
-
 	aFileStream _ FileStream oldFileNamed: aName.
-
 	list _ aFileStream fileInObjectAndCode.
-	userCategories _ list at: 1.
-	allPeople _ list at: 2.
-	allEvents _ list at: 3.
-	recurringEvents _ list at: 4.
-	allToDoItems _ list at: 5.
-	allNotes _ list at: 6.
+	userCategories _ list first.
+	allPeople _ list second.
+	allEvents _ list third.
+	recurringEvents _ list fourth.
+	allToDoItems _ list fifth.
+	allNotes _ list sixth.
 	date _ Date today.
-	self selectCategory: 'all'.
+	self selectCategory: 'all'
