1b1ef168e36ac04092932977e8dc9cc0bc0324e2
dgd <unknown>
2003-02-22 14:23:00 +0000

    no message

--- EnvelopeEditorMorph/instance/editing/acceptGraphPoint-3A-at-3A-.cs
+++ EnvelopeEditorMorph/instance/editing/acceptGraphPoint-3A-at-3A-.cs
@@ -1,20 +1,30 @@
-acceptGraphPoint: p at: index
+acceptGraphPoint: p at: index 
 	| ms val points whichLim linePoint other boundedP |
-	boundedP _ p adhereTo: graphArea bounds.
-	ms _ self msFromX: boundedP x.
-	points _ envelope points.
-	ms _ self constrain: ms adjacentTo: index in: points.
-	(index = 1 or: [(whichLim _ limits indexOf: index) > 0]) ifTrue:
-		["Limit points must not move laterally"
-		ms _ (points at: index) x].
-	val _ self valueFromY: boundedP y.
-	points at: index put: ms@val.
-	linePoint _ (self xFromMs: ms) @ (self yFromValue: val).
-	(whichLim notNil and: [whichLim between: 1 and: 2]) ifTrue:
-		["Loop start and loop end must be tied together"
-		other _ limits at: (3 - whichLim).  " 1 <--> 2 "
-		points at: other put: (points at: other) x @ val.
-		line verticesAt: other put: (line vertices at: other) x @ linePoint y].
+	boundedP := p adhereTo: graphArea bounds.
+	ms := self msFromX: boundedP x.
+	points := envelope points.
+	ms := self 
+				constrain: ms
+				adjacentTo: index
+				in: points.
+	(index = 1 or: [(whichLim := limits indexOf: index) > 0]) 
+		ifTrue: 
+			["Limit points must not move laterally"
+
+			ms := (points at: index) x].
+	val := self valueFromY: boundedP y.
+	points at: index put: ms @ val.
+	linePoint := (self xFromMs: ms) @ (self yFromValue: val).
+	(whichLim notNil and: [whichLim between: 1 and: 2]) 
+		ifTrue: 
+			["Loop start and loop end must be tied together"
+
+			other := limits at: 3 - whichLim.	" 1 <--> 2 "
+			points at: other put: (points at: other) x @ val.
+			line verticesAt: other put: (line vertices at: other) x @ linePoint y].
 	"Make sure envelope feels the change in points array..."
-	envelope setPoints: points loopStart: (limits at: 1) loopEnd: (limits at: 2).
-	^ linePoint
+	envelope 
+		setPoints: points
+		loopStart: limits first
+		loopEnd: (limits second).
+	^linePoint
