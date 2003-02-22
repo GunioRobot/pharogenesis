a82c817dc4a7aa60255a1275d6e19c8bbaf94d41
dgd <unknown>
2003-02-22 13:38:00 +0000

    no message

--- PlayingCardDeck/instance/printing/printOn-3A-.cs
+++ PlayingCardDeck/instance/printing/printOn-3A-.cs
@@ -1,12 +1,13 @@
-printOn: aStream
+printOn: aStream 
 	| cards |
-	cards _ self cards.
+	cards := self cards.
 	aStream nextPutAll: 'aCardDeck('.
 	cards size > 1 
-		ifTrue: [
-			cards allButLast do: [:card |
-				aStream
-					print: card;
-					nextPutAll: ', ']].
-	cards size > 0 ifTrue: [aStream print: cards last].
-	aStream nextPut: $).
+		ifTrue: 
+			[cards allButLast do: 
+					[:card | 
+					aStream
+						print: card;
+						nextPutAll: ', ']].
+	cards notEmpty ifTrue: [aStream print: cards last].
+	aStream nextPut: $)
