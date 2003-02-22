95f9553600e47d6ef29a2d19e5016276ccd51d4f
dgd <unknown>
2003-02-22 14:10:00 +0000

    no message

--- PasteUpMorph/instance/world menu/findDirtyWindows-3A-.cs
+++ PasteUpMorph/instance/world menu/findDirtyWindows-3A-.cs
@@ -1,11 +1,14 @@
-findDirtyWindows: evt
+findDirtyWindows: evt 
 	"Present a menu of window titles for all windows with changes,
 	and activate the one that gets chosen."
-	| menu |
-	menu _ MenuMorph new.
-	(SystemWindow windowsIn: self
-		satisfying: [:w | w model canDiscardEdits not])
-		do: [:w | menu add: w label target: w action: #activate].
 
-	menu submorphs size > 0 ifTrue:
-		[menu popUpEvent: evt in: self]
+	| menu |
+	menu := MenuMorph new.
+	(SystemWindow windowsIn: self
+		satisfying: [:w | w model canDiscardEdits not]) do: 
+				[:w | 
+				menu 
+					add: w label
+					target: w
+					action: #activate].
+	menu submorphs notEmpty ifTrue: [menu popUpEvent: evt in: self]
