bc37002026349108eb1e79a88e6de069a3e07413
dgd <unknown>
2003-02-22 18:42:00 +0000

    no message

--- CanvasDecoder/instance/decoding/drawBalloonOval-3A-.cs
+++ CanvasDecoder/instance/decoding/drawBalloonOval-3A-.cs
@@ -1,16 +1,13 @@
-drawBalloonOval: command
-
+drawBalloonOval: command 
 	| aRectangle aFillStyle borderWidth borderColor |
-
-	aRectangle _ self class decodeRectangle: (command at: 2).
-	aFillStyle _ self class decodeFillStyle: (command at: 3).
-	borderWidth _ self class decodeInteger: (command at: 4).
-	borderColor _ self class decodeColor: (command at: 5).
-
-	self drawCommand: [ :c |
-		c asBalloonCanvas 
-			fillOval: aRectangle
-			fillStyle: aFillStyle
-			borderWidth: borderWidth
-			borderColor: borderColor
-	].
+	aRectangle := self class decodeRectangle: command second.
+	aFillStyle := self class decodeFillStyle: command third.
+	borderWidth := self class decodeInteger: command fourth.
+	borderColor := self class decodeColor: (command fifth).
+	self drawCommand: 
+			[:c | 
+			c asBalloonCanvas 
+				fillOval: aRectangle
+				fillStyle: aFillStyle
+				borderWidth: borderWidth
+				borderColor: borderColor]
