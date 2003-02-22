883ac657f47ac149dc9a9f12464be294102d362f
dgd <unknown>
2003-02-22 14:31:00 +0000

    no message

--- Morph/instance/layout/doLayoutIn-3A-.cs
+++ Morph/instance/layout/doLayoutIn-3A-.cs
@@ -1,17 +1,19 @@
-doLayoutIn: layoutBounds
+doLayoutIn: layoutBounds 
 	"Compute a new layout based on the given layout bounds."
-	| layout box priorBounds |
+
 	"Note: Testing for #bounds or #layoutBounds would be sufficient to
 	figure out if we need an invalidation afterwards but #outerBounds
 	is what we need for all leaf nodes so we use that."
-	priorBounds _ self outerBounds.
-	submorphs size = 0 ifTrue:[^fullBounds _ priorBounds].
+
+	| layout box priorBounds |
+	priorBounds := self outerBounds.
+	submorphs isEmpty ifTrue: [^fullBounds := priorBounds].
 	"Send #ownerChanged to our children"
-	submorphs do:[:m| m ownerChanged].
-	layout _ self layoutPolicy.
-	layout ifNotNil:[layout layout: self in: layoutBounds].
+	submorphs do: [:m | m ownerChanged].
+	layout := self layoutPolicy.
+	layout ifNotNil: [layout layout: self in: layoutBounds].
 	self adjustLayoutBounds.
-	fullBounds _ self privateFullBounds.
-	box _ self outerBounds.
-	box = priorBounds
-		ifFalse:[self invalidRect: (priorBounds quickMerge: box)].
+	fullBounds := self privateFullBounds.
+	box := self outerBounds.
+	box = priorBounds 
+		ifFalse: [self invalidRect: (priorBounds quickMerge: box)]
