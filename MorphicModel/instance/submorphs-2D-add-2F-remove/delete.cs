168adeb6b6abb35d8f8daace2c4eba05b8842634
gm <unknown>
2003-02-22 12:51:00 +0000

    no message

---
+++ MorphicModel/instance/submorphs-2D-add-2F-remove/delete.cs
@@ -0,0 +1,17 @@
+delete
+	(model isMorphicModel) ifFalse: [^super delete].
+	slotName ifNotNil: 
+			[(PopUpMenu confirm: 'Shall I remove the slot ' , slotName 
+						, '
+along with all associated methods?') 
+				ifTrue: 
+					[(model class selectors select: [:s | s beginsWith: slotName]) 
+						do: [:s | model class removeSelector: s].
+					(model class instVarNames includes: slotName) 
+						ifTrue: [model class removeInstVarName: slotName]]
+				ifFalse: 
+					[(PopUpMenu 
+						confirm: '...but should I at least dismiss this morph?
+[choose no to leave everything unchanged]') 
+							ifFalse: [^self]]].
+	super delete
