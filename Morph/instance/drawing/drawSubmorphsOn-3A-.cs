370e63cfb2ddfd694efd2b725e41200f03b91b55
dgd <unknown>
2003-02-22 14:31:00 +0000

    no message

--- Morph/instance/drawing/drawSubmorphsOn-3A-.cs
+++ Morph/instance/drawing/drawSubmorphsOn-3A-.cs
@@ -1,10 +1,9 @@
-drawSubmorphsOn: aCanvas
+drawSubmorphsOn: aCanvas 
 	"Display submorphs back to front"
+
 	| drawBlock |
-	submorphs size = 0 ifTrue:[^self].
-	drawBlock _ [:canvas| submorphs reverseDo:[:m| canvas fullDrawMorph: m]].
-	self clipSubmorphs ifTrue:[
-		aCanvas clipBy: self clippingBounds during: drawBlock.
-	] ifFalse:[
-		drawBlock value: aCanvas.
-	].
+	submorphs isEmpty ifTrue: [^self].
+	drawBlock := [:canvas | submorphs reverseDo: [:m | canvas fullDrawMorph: m]].
+	self clipSubmorphs 
+		ifTrue: [aCanvas clipBy: self clippingBounds during: drawBlock]
+		ifFalse: [drawBlock value: aCanvas]
