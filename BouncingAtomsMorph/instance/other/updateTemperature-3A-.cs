2f09c5b831ba973f60a173e8c4eba46832faa8bc
dgd <unknown>
2003-02-22 13:36:00 +0000

    no message

--- BouncingAtomsMorph/instance/other/updateTemperature-3A-.cs
+++ BouncingAtomsMorph/instance/other/updateTemperature-3A-.cs
@@ -1,10 +1,10 @@
-updateTemperature: currentTemperature
+updateTemperature: currentTemperature 
 	"Record the current temperature, which is taken to be the number of atoms that have bounced in the last cycle. To avoid too much jitter in the reading, the last several readings are averaged."
 
-	recentTemperatures == nil ifTrue: [
-		recentTemperatures _ OrderedCollection new.
-		20 timesRepeat: [recentTemperatures add: 0]].
-
+	recentTemperatures isNil 
+		ifTrue: 
+			[recentTemperatures := OrderedCollection new.
+			20 timesRepeat: [recentTemperatures add: 0]].
 	recentTemperatures removeLast.
 	recentTemperatures addFirst: currentTemperature.
-	temperature _ recentTemperatures sum asFloat / recentTemperatures size.
+	temperature := recentTemperatures sum asFloat / recentTemperatures size
