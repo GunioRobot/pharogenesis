086225e4f2413d034cd0b3fec0b1d28f8f513fac
dgd <unknown>
2003-02-22 13:22:00 +0000

    no message

--- MoviePlayerMorph/instance/stepping/startRunning.cs
+++ MoviePlayerMorph/instance/stepping/startRunning.cs
@@ -1,21 +1,28 @@
 startRunning
-
 	| ms |
 	(frameBufferIfScaled ifNil: [currentPage image]) unhibernate.
-	movieFile _ AsyncFile new open: movieFileName forWrite: false.
-	movieFile primReadStart: movieFile fileHandle
-			fPosition: (self filePosForFrameNo: frameNumber)
-			count: self fileByteCountPerFrame.
-	scorePlayer == nil
-		ifTrue: [ms _ Time millisecondClockValue.
-				msAtStart _ ms - ((frameNumber-1) * msPerFrame).
-				msAtLastSync _ ms - msAtStart.
-				frameAtLastSync _ frameNumber]
-		ifFalse: [(playDirection > 0 and: [scorePlayer isKindOf: SampledSound]) ifTrue:
-					[scorePlayer reset;
-							playSilentlyUntil: (frameNumber - 1 * msPerFrame / 1000.0);						initialVolume: 1.0.
+	movieFile := AsyncFile new 
+				open: (FileDirectory default fullNameFor: movieFileName)
+				forWrite: false.
+	movieFile 
+		primReadStart: movieFile fileHandle
+		fPosition: (self filePosForFrameNo: frameNumber)
+		count: self fileByteCountPerFrame.
+	scorePlayer isNil 
+		ifTrue: 
+			[ms := Time millisecondClockValue.
+			msAtStart := ms - ((frameNumber - 1) * msPerFrame).
+			msAtLastSync := ms - msAtStart]
+		ifFalse: 
+			[(playDirection > 0 and: [scorePlayer isKindOf: SampledSound]) 
+				ifTrue: 
+					[scorePlayer
+						reset;
+						playSilentlyUntil: (frameNumber - 1) * msPerFrame / 1000.0;
+						initialVolume: 1.0.
+					
 					[scorePlayer resumePlaying.
-					msAtLastSync _ scorePlayer millisecondsSinceStart]
-						forkAt: Processor userInterruptPriority].
-				msAtLastSync _ scorePlayer millisecondsSinceStart.
-				frameAtLastSync _ frameNumber]
+					msAtLastSync := scorePlayer millisecondsSinceStart] 
+							forkAt: Processor userInterruptPriority].
+			msAtLastSync := scorePlayer millisecondsSinceStart].
+	frameAtLastSync := frameNumber
