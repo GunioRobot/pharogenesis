53cbd750edff10fc860e99805cd778dee218ede3
dgd <unknown>
2003-02-22 13:43:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/getFirstElement.cs
+++ Player/instance/slot getters-2F-setters/getFirstElement.cs
@@ -2,8 +2,6 @@ getFirstElement
 	"Answer a player representing the receiver's costume's first submorph"
 
 	| itsMorphs |
-	^ (itsMorphs _ costume submorphs) size > 0
-		ifFalse:
-			[self standardPlayer]
-		ifTrue:
-			[itsMorphs first assuredPlayer]
+	^(itsMorphs := costume submorphs) notEmpty 
+		ifFalse: [costume presenter standardPlayer]
+		ifTrue: [itsMorphs first assuredPlayer]
