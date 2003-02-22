2cbf1890bf47faa8349c14d09dd7d24c792519dd
dgd <unknown>
2003-02-22 19:01:00 +0000

    no message

--- PasteUpMorph/instance/world state/paintAreaFor-3A-.cs
+++ PasteUpMorph/instance/world state/paintAreaFor-3A-.cs
@@ -1,7 +1,7 @@
-paintAreaFor: aSketchMorph
+paintAreaFor: aSketchMorph 
 	"Answer the area to comprise the onion-skinned canvas for painting/repainting aSketchMorph"
-	| itsOwner |
-	((itsOwner _ aSketchMorph owner) ~~ nil and: [itsOwner isPlayfieldLike])
-		ifTrue: [^ itsOwner bounds].  "handles every plausible situation"
 
-	^ self paintArea
+	| itsOwner |
+	((itsOwner := aSketchMorph owner) notNil and: [itsOwner isPlayfieldLike]) 
+		ifTrue: [^itsOwner bounds].	"handles every plausible situation"
+	^self paintArea
