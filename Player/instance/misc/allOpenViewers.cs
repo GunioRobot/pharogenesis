9fe89fcc096dc721b44b72b8b2bf3a7464181792
gm <unknown>
2003-02-22 14:53:00 +0000

    no message

--- Player/instance/misc/allOpenViewers.cs
+++ Player/instance/misc/allOpenViewers.cs
@@ -2,9 +2,9 @@ allOpenViewers
 	"Answer a list of all the viewers open on the receiver.  Include viewers in closed flaps"
 
 	| aWorld all |
-	(aWorld _ self costume world) ifNil: [^ #()].
-	all _ aWorld allMorphs.
-	aWorld closedViewerFlapTabs do:
-		[:aTab | all addAll: aTab referent allMorphs].
-	^ all select:
-		[:m | (m isKindOf: StandardViewer) and: [m scriptedPlayer == self]]
+	(aWorld := self costume world) ifNil: [^#()].
+	all := aWorld allMorphs.
+	aWorld closedViewerFlapTabs 
+		do: [:aTab | all addAll: aTab referent allMorphs].
+	^all 
+		select: [:m | (m isStandardViewer) and: [m scriptedPlayer == self]]
