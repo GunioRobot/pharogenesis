aef4d5898e0a5ac43ddaec470e4f0a4233ac6f2f
dgd <unknown>
2003-02-22 14:49:00 +0000

    no message

--- PianoRollNoteMorph/instance/event handling/mouseMove-3A-.cs
+++ PianoRollNoteMorph/instance/event handling/mouseMove-3A-.cs
@@ -1,17 +1,22 @@
-mouseMove: evt
-
+mouseMove: evt 
 	| delta offsetEvt |
-	editMode == nil ifTrue:
-		["First movement determines edit mode"
-		((delta _ evt cursorPoint - hitLoc) dist: 0@0) <= 2 ifTrue:
-			[^ self  "No significant movement yet."].
-		delta x abs > delta y abs
-			ifTrue: [delta x > 0  "Horizontal drag"
-						ifTrue: [editMode _ #selectNotes]
-						ifFalse: [self playSound: nil.
-								offsetEvt _ evt copy setCursorPoint: evt cursorPoint + (20@0).
-								self invokeNoteMenu: offsetEvt]]
-			ifFalse: [editMode _ #editPitch  "Vertical drag"]].
-	editMode == #editPitch ifTrue: [self editPitch: evt].
-	editMode == #selectNotes ifTrue: [self selectNotes: evt].
+	editMode isNil 
+		ifTrue: 
+			["First movement determines edit mode"
 
+			((delta := evt cursorPoint - hitLoc) dist: 0 @ 0) <= 2 
+				ifTrue: [^self	"No significant movement yet."].
+			delta x abs > delta y abs 
+				ifTrue: 
+					[delta x > 0 
+						ifTrue: 
+							["Horizontal drag"
+
+							editMode := #selectNotes]
+						ifFalse: 
+							[self playSound: nil.
+							offsetEvt := evt copy setCursorPoint: evt cursorPoint + (20 @ 0).
+							self invokeNoteMenu: offsetEvt]]
+				ifFalse: [editMode := #editPitch	"Vertical drag"]].
+	editMode == #editPitch ifTrue: [self editPitch: evt].
+	editMode == #selectNotes ifTrue: [self selectNotes: evt]
