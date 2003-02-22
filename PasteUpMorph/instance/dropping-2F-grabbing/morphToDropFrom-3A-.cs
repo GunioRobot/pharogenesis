7919f25f591084d33e4eca020b124f63437f412e
gm <unknown>
2003-02-22 13:08:00 +0000

    no message

--- PasteUpMorph/instance/dropping-2F-grabbing/morphToDropFrom-3A-.cs
+++ PasteUpMorph/instance/dropping-2F-grabbing/morphToDropFrom-3A-.cs
@@ -1,33 +1,41 @@
-morphToDropFrom: aMorph
+morphToDropFrom: aMorph 
 	"Given a morph being carried by the hand, which the hand is about to drop, answer the actual morph to be deposited.  Normally this would be just the morph itself, but several unusual cases arise, which this method is designed to service."
 
 	| aNail representee handy posBlock tempPos |
-	handy _ self primaryHand.
-	posBlock _ [:z | 
-			tempPos _ handy position - (handy targetOffset - aMorph formerPosition * (z extent / aMorph extent)) rounded.
+	handy := self primaryHand.
+	posBlock := 
+			[:z | 
+			tempPos := handy position 
+						- ((handy targetOffset - aMorph formerPosition) 
+								* (z extent / aMorph extent)) rounded.
 			self pointFromWorld: tempPos].
-	self alwaysShowThumbnail
-		ifTrue: [aNail _ aMorph
+	self alwaysShowThumbnail 
+		ifTrue: 
+			[aNail := aMorph 
 						representativeNoTallerThan: self maxHeightToAvoidThumbnailing
 						norWiderThan: self maximumThumbnailWidth
 						thumbnailHeight: self heightForThumbnails.
-			aNail == aMorph
-				ifFalse: [aNail
-						position: (posBlock value: aNail)].
-			^ aNail].
-	((aMorph isKindOf: MorphThumbnail)
-			and: [(representee _ aMorph morphRepresented) owner == nil])
-		ifTrue: [representee
-				position: (posBlock value: representee).
-			^ representee].
+			aNail == aMorph 
+				ifFalse: 
+					[aMorph formerPosition: aMorph position.
+					aNail position: (posBlock value: aNail)].
+			^aNail].
+	((aMorph isKindOf: MorphThumbnail) 
+		and: [(representee := aMorph morphRepresented) owner isNil]) 
+			ifTrue: 
+				[representee position: (posBlock value: representee).
+				^representee].
+	self showingListView 
+		ifTrue: 
+			[^aMorph 
+				listViewLineForFieldList: (self valueOfProperty: #fieldListSelectors)].
+	(aMorph hasProperty: #newPermanentScript) 
+		ifTrue: [^aMorph asEmptyPermanentScriptor].
+	((aMorph isKindOf: PhraseTileMorph) or: [aMorph isSyntaxMorph]) 
+		ifFalse: [^aMorph].
+	aMorph userScriptSelector isEmptyOrNil 
+		ifTrue: 
+			["non-user"
 
-	self showingListView ifTrue:
-		[^ aMorph listViewLineForFieldList: (self valueOfProperty: #fieldListSelectors)].
-
-	(aMorph hasProperty: #newPermanentScript)
-		ifTrue: [^ aMorph asEmptyPermanentScriptor].
-	self automaticPhraseExpansion
-		ifFalse: [^ aMorph].
-	((aMorph isKindOf: PhraseTileMorph) or: [aMorph isKindOf: SyntaxMorph])
-		ifFalse: [^ aMorph].
-	^ aMorph morphToDropInPasteUp: self
+			self automaticPhraseExpansion ifFalse: [^aMorph]].
+	^aMorph morphToDropInPasteUp: self
