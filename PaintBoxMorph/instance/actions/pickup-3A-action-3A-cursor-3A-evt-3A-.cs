6ee0ee511f3ed1d166f71e2e8a3e0fc0165c1b17
dgd <unknown>
2003-02-22 19:04:00 +0000

    no message

--- PaintBoxMorph/instance/actions/pickup-3A-action-3A-cursor-3A-evt-3A-.cs
+++ PaintBoxMorph/instance/actions/pickup-3A-action-3A-cursor-3A-evt-3A-.cs
@@ -1,35 +1,49 @@
-pickup: actionButton action: aSelector cursor: aCursor evt: evt
+pickup: actionButton action: aSelector cursor: aCursor evt: evt 
 	"Special version for pickup: and stamp:, because of these tests"
 
 	| ss picker old map stamper |
+	self 
+		tool: actionButton
+		action: aSelector
+		cursor: aCursor
+		evt: evt.
+	aSelector == #stamp: 
+		ifTrue: 
+			[(stampHolder pickupButtons includes: actionButton) 
+				ifTrue: 
+					[stamper := stampHolder otherButtonFor: actionButton.
+					^self 
+						pickup: stamper
+						action: #stamp:
+						cursor: (stamper arguments third)
+						evt: evt].
+			(stampHolder stampFormFor: actionButton) ifNil: 
+					["If not stamp there, go to pickup mode"
 
-	self tool: actionButton action: aSelector cursor: aCursor evt: evt.
-
-	aSelector == #stamp: ifTrue: [
-		(stampHolder pickupButtons includes: actionButton) ifTrue: [
-				stamper _ stampHolder otherButtonFor: actionButton.
-				^ self pickup: stamper action: #stamp: cursor: (stamper arguments at: 3) evt: evt].
-		(stampHolder stampFormFor: actionButton) 
-			ifNil: [
-				"If not stamp there, go to pickup mode"
-				picker _ stampHolder otherButtonFor: actionButton.
-				picker state: #on.
-				^ self pickup: picker action: #pickup: cursor: (picker arguments at: 3) evt: evt]
-			ifNotNil: [
-				old _ stampHolder stampFormFor: actionButton.
-				currentCursor _ ColorForm extent: old extent depth: 8.
-				old displayOn: currentCursor.
-				map _ Color indexedColors copy.
-				map at: 1 put: Color transparent.
-				currentCursor colors: map.
-				currentCursor offset: currentCursor extent // -2.
-				"Emphisize the stamp button"
-				actionButton owner "layoutMorph" "color: (Color r: 1.0 g: 0.645 b: 0.419);"
-					borderColor: (Color r: 0.65 g: 0.599 b: 0.8).
-				]].
-
-	aSelector == #pickup: ifTrue: [
-		ss _ self focusMorph.
-		ss ifNotNil: [currentCursor _ aCursor]	 
-			ifNil: [self notCurrentlyPainting.
-				self setAction: #paint: evt: evt]].
+					picker := stampHolder otherButtonFor: actionButton.
+					picker state: #on.
+					^self 
+						pickup: picker
+						action: #pickup:
+						cursor: (picker arguments third)
+						evt: evt]
+				ifNotNil: 
+					[old := stampHolder stampFormFor: actionButton.
+					currentCursor := ColorForm extent: old extent depth: 8.
+					old displayOn: currentCursor.
+					map := Color indexedColors copy.
+					map at: 1 put: Color transparent.
+					currentCursor colors: map.
+					currentCursor offset: currentCursor extent // -2.
+					"Emphisize the stamp button"
+					actionButton owner borderColor: (Color 
+								r: 0.65
+								g: 0.599
+								b: 0.8)	"layoutMorph"	"color: (Color r: 1.0 g: 0.645 b: 0.419);"]].
+	aSelector == #pickup: 
+		ifTrue: 
+			[ss := self focusMorph.
+			ss ifNotNil: [currentCursor := aCursor]
+				ifNil: 
+					[self notCurrentlyPainting.
+					self setAction: #paint: evt: evt]]
