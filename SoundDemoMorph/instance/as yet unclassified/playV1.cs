d0302babea5824ea09692fefd26f52e90d76bc60
dgd <unknown>
2003-02-22 13:35:00 +0000

    no message

--- SoundDemoMorph/instance/as yet unclassified/playV1.cs
+++ SoundDemoMorph/instance/as yet unclassified/playV1.cs
@@ -1,5 +1,6 @@
 playV1
-
-	soundColumn submorphs size < 1 ifTrue: [^ self].
-	self setTimbreFromTile: (soundColumn submorphs at: 1).
-	(SampledSound bachFugueVoice1On: SampledSound new) play.
+	soundColumn submorphs isEmpty
+		ifTrue: [^ self].
+	self
+		setTimbreFromTile: (soundColumn submorphs first).
+	(SampledSound bachFugueVoice1On: SampledSound new) play
