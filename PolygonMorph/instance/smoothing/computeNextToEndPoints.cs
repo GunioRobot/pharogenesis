4d42b99cd81ffc9de9c407d73c877ab633408f95
dgd <unknown>
2003-02-22 14:15:00 +0000

    no message

--- PolygonMorph/instance/smoothing/computeNextToEndPoints.cs
+++ PolygonMorph/instance/smoothing/computeNextToEndPoints.cs
@@ -1,9 +1,9 @@
 computeNextToEndPoints
-
 	| pointAfterFirst pointBeforeLast |
-	pointAfterFirst _ nil.
-	self lineSegmentsDo:
-		[:p1 :p2 | pointAfterFirst == nil ifTrue: [pointAfterFirst _ p2 asIntegerPoint].
-		pointBeforeLast _ p1 asIntegerPoint].
+	pointAfterFirst := nil.
+	self lineSegmentsDo: 
+			[:p1 :p2 | 
+			pointAfterFirst isNil ifTrue: [pointAfterFirst := p2 asIntegerPoint].
+			pointBeforeLast := p1 asIntegerPoint].
 	curveState at: 2 put: pointAfterFirst.
-	curveState at: 3 put: pointBeforeLast.
+	curveState at: 3 put: pointBeforeLast
