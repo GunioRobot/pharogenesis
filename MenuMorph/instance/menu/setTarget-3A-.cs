6ef20c35655f07186d90c7e8effc8a8d8f106873
dgd <unknown>
2003-02-22 18:55:00 +0000

    no message

--- MenuMorph/instance/menu/setTarget-3A-.cs
+++ MenuMorph/instance/menu/setTarget-3A-.cs
@@ -1,14 +1,15 @@
-setTarget: evt
+setTarget: evt 
 	"Set the default target object to be used for add item commands, and re-target all existing items to the new target or the the invoking hand."
 
 	| rootMorphs old |
-	rootMorphs _ self world rootMorphsAt: evt hand targetOffset.
-	rootMorphs size > 1
-		ifTrue: [defaultTarget _ rootMorphs at: 2]
-		ifFalse: [^ self].
+	rootMorphs := self world rootMorphsAt: evt hand targetOffset.
+	rootMorphs size > 1 
+		ifTrue: [defaultTarget := rootMorphs second]
+		ifFalse: [^self].
 	"re-target all existing items"
-	self items do: [:item |
-		old _ item target.
-		old isHandMorph
-			ifTrue: [item target: evt hand]
-			ifFalse: [item target: defaultTarget]].
+	self items do: 
+			[:item | 
+			old := item target.
+			old isHandMorph 
+				ifTrue: [item target: evt hand]
+				ifFalse: [item target: defaultTarget]]
