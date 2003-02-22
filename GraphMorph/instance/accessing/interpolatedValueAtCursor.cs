73fb2aec5dd8c831ed437f17b0b82db16a67b1dd
dgd <unknown>
2003-02-22 14:40:00 +0000

    no message

--- GraphMorph/instance/accessing/interpolatedValueAtCursor.cs
+++ GraphMorph/instance/accessing/interpolatedValueAtCursor.cs
@@ -1,16 +1,15 @@
 interpolatedValueAtCursor
-
 	| sz prev frac next |
-	data isEmpty ifTrue: [^ 0].
-	sz _ data size.
-	cursor < 0 ifTrue: [^ data at: 1].  "just to be safe, though cursor shouldn't be negative"
-	prev _ cursor truncated.
-	frac _ cursor - prev.
-	prev < 1 ifTrue: [prev _ sz].
-	prev > sz ifTrue: [prev _ 1].
+	data isEmpty ifTrue: [^0].
+	sz := data size.
+	cursor < 0 ifTrue: [^data first].	"just to be safe, though cursor shouldn't be negative"
+	prev := cursor truncated.
+	frac := cursor - prev.
+	prev < 1 ifTrue: [prev := sz].
+	prev > sz ifTrue: [prev := 1].
 	"assert: 1 <= prev <= sz"
-	frac = 0 ifTrue: [^ data at: prev].  "no interpolation needed"
+	frac = 0 ifTrue: [^data at: prev].	"no interpolation needed"
 
 	"interpolate"
-	next _ prev = sz ifTrue: [1] ifFalse: [prev + 1].
-	^ ((1.0 - frac) * (data at: prev)) + (frac * (data at: next))
+	next := prev = sz ifTrue: [1] ifFalse: [prev + 1].
+	^(1.0 - frac) * (data at: prev) + (frac * (data at: next))
