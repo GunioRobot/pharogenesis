332a5c149b1dc3bf0a7652933fb9a325e8ae1491
dgd <unknown>
2003-02-22 13:39:00 +0000

    no message

--- SyntaxMorph/instance/event handling/handlesMouseDown-3A-.cs
+++ SyntaxMorph/instance/event handling/handlesMouseDown-3A-.cs
@@ -1,8 +1,6 @@
-handlesMouseDown: evt
-
-	evt yellowButtonPressed ifTrue: [^ true].
-	parseNode == nil ifTrue: [^ false].
-	owner isSyntaxMorph ifTrue:
-		[(owner isMethodNode and: [self isBlockNode not])
-			ifTrue: [^ false]].	"Can only take block out of a MethodNode"
-	^ true
+handlesMouseDown: evt 
+	evt yellowButtonPressed ifTrue: [^true].
+	parseNode isNil ifTrue: [^false].
+	owner isSyntaxMorph 
+		ifTrue: [(owner isMethodNode and: [self isBlockNode not]) ifTrue: [^false]].	"Can only take block out of a MethodNode"
+	^true
