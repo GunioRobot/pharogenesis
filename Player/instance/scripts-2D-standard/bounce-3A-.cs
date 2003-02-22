ffe8b93e6a9870eaf0fbb678858968da220380fa
dgd <unknown>
2003-02-22 13:42:00 +0000

    no message

--- Player/instance/scripts-2D-standard/bounce-3A-.cs
+++ Player/instance/scripts-2D-standard/bounce-3A-.cs
@@ -1,13 +1,25 @@
-bounce: soundName
+bounce: soundName 
 	"If the receiver's current bounds obtrude beyond the bounds of its container, then 'bounce' it back within the container, and make the indicated sound while doing so"
 
 	| box bounced aCostume |
-	(aCostume _ self costume) ifNil: [^ self].
-	(aCostume owner == nil or: [aCostume owner isHandMorph]) ifTrue: [^ self].
-	box _ aCostume owner bounds.
-	bounced _ false.
-	(aCostume left < box left)			ifTrue: [self headRight. bounced _ true].
-	(aCostume right > box right)		ifTrue: [self headLeft. bounced _ true].
-	(aCostume top < box top)			ifTrue: [self headDown. bounced _ true].
-	(aCostume bottom > box bottom)	ifTrue: [self headUp. bounced _ true].
-	bounced ifTrue: [^ self makeBounceSound: soundName].
+	(aCostume := self costume) ifNil: [^self].
+	(aCostume owner isNil or: [aCostume owner isHandMorph]) ifTrue: [^self].
+	box := aCostume owner bounds.
+	bounced := false.
+	aCostume left < box left 
+		ifTrue: 
+			[self headRight.
+			bounced := true].
+	aCostume right > box right 
+		ifTrue: 
+			[self headLeft.
+			bounced := true].
+	aCostume top < box top 
+		ifTrue: 
+			[self headDown.
+			bounced := true].
+	aCostume bottom > box bottom 
+		ifTrue: 
+			[self headUp.
+			bounced := true].
+	bounced ifTrue: [^self makeBounceSound: soundName]
