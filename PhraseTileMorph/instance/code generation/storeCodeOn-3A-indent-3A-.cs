71d2182854101a1bca78cdad42e16b41da001885
dgd <unknown>
2003-02-22 19:08:00 +0000

    no message

--- PhraseTileMorph/instance/code generation/storeCodeOn-3A-indent-3A-.cs
+++ PhraseTileMorph/instance/code generation/storeCodeOn-3A-indent-3A-.cs
@@ -1,12 +1,14 @@
-storeCodeOn: aStream indent: tabCount
+storeCodeOn: aStream indent: tabCount 
 	"Add in some smarts for division by zero."
 
 	aStream nextPut: $(.
-	(submorphs at: 1) storeCodeOn: aStream indent: tabCount.
+	submorphs first storeCodeOn: aStream indent: tabCount.
 	aStream space.
-	(submorphs at: 2) storeCodeOn: aStream indent: tabCount.
-	submorphs size > 2 ifTrue: [
-		(self catchDivideByZero: aStream indent: tabCount) ifFalse: [
-			aStream space.
-			(submorphs at: 3) storeCodeOn: aStream indent: tabCount]].
-	aStream nextPut: $).
+	submorphs second storeCodeOn: aStream indent: tabCount.
+	submorphs size > 2 
+		ifTrue: 
+			[(self catchDivideByZero: aStream indent: tabCount) 
+				ifFalse: 
+					[aStream space.
+					(submorphs third) storeCodeOn: aStream indent: tabCount]].
+	aStream nextPut: $)
