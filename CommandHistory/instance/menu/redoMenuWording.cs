b6d522504c0adb04571d29b9db795bb929feee8f
dgd <unknown>
2003-02-22 14:40:00 +0000

    no message

--- CommandHistory/instance/menu/redoMenuWording.cs
+++ CommandHistory/instance/menu/redoMenuWording.cs
@@ -2,12 +2,11 @@ redoMenuWording
 	"Answer the wording to be used in a menu offering the current Redo command"
 
 	| nextCommand |
-
-	((nextCommand _ self nextCommand) == nil or: [Preferences useUndo not]) ifTrue:  [^ 'can''t redo'].
-	^ String streamContents:
-		[:aStream | 
+	((nextCommand := self nextCommand) isNil or: [Preferences useUndo not]) 
+		ifTrue: [^'can''t redo'].
+	^String streamContents: 
+			[:aStream | 
 			aStream nextPutAll: 'redo "'.
-			aStream nextPutAll: (nextCommand cmdWording truncateTo: 12).
+			aStream nextPutAll: (nextCommand cmdWording truncateWithElipsisTo: 20).
 			aStream nextPut: $".
-			lastCommand phase == #done ifFalse:
-				[aStream nextPutAll: ' (z)']]
+			lastCommand phase == #done ifFalse: [aStream nextPutAll: ' (z)']]
