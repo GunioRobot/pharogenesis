5a6781b48ed446275bb1d007013d6fb959f89d3f
dgd <unknown>
2003-02-22 18:55:00 +0000

    no message

--- SimpleSliderMorph/instance/menu/setTarget-3A-.cs
+++ SimpleSliderMorph/instance/menu/setTarget-3A-.cs
@@ -1,7 +1,5 @@
-setTarget: evt
-
+setTarget: evt 
 	| rootMorphs |
 	rootMorphs _ self world rootMorphsAt: evt hand targetOffset.
-	rootMorphs size > 1
-		ifTrue: [target _ rootMorphs at: 2]
-		ifFalse: [target _ nil].
+	target _ rootMorphs size > 1
+				ifTrue: [rootMorphs second]
