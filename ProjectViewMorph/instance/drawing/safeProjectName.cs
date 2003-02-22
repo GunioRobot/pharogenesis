e143e485550328731ad3d19cdd45fbd0b7392700
gm <unknown>
2003-02-22 13:14:00 +0000

    no message

--- ProjectViewMorph/instance/drawing/safeProjectName.cs
+++ ProjectViewMorph/instance/drawing/safeProjectName.cs
@@ -1,7 +1,14 @@
 safeProjectName
-
-	| projectName |
-	projectName _ self valueOfProperty: #SafeProjectName ifAbsent: ['???'].
-	self isTheRealProjectPresent ifFalse: [^ projectName].
+	| projectName args |
+	projectName := self valueOfProperty: #SafeProjectName ifAbsent: ['???'].
+	self isTheRealProjectPresent 
+		ifFalse: 
+			[project class == DiskProxy 
+				ifTrue: 
+					[args := project constructorArgs.
+					((args isKindOf: Array) 
+						and: [args size = 1 and: [args first isString]]) 
+							ifTrue: [^args first]]
+				ifFalse: [^projectName]].
 	self setProperty: #SafeProjectName toValue: project name.
 	^project name
