7947d598d545a3e919dc1bc56b552d391d826f8f
dgd <unknown>
2003-02-22 18:58:00 +0000

    no message

--- PluggableTextMorph/instance/updating/update-3A-.cs
+++ PluggableTextMorph/instance/updating/update-3A-.cs
@@ -1,27 +1,37 @@
-update: aSymbol
-	aSymbol ifNil: [^ self].
-	aSymbol == #flash ifTrue: [^ self flash].
-	aSymbol == getTextSelector ifTrue:
+update: aSymbol 
+	aSymbol ifNil: [^self].
+	aSymbol == #flash ifTrue: [^self flash].
+	aSymbol == getTextSelector 
+		ifTrue: 
 			[self setText: self getText.
-			^ self setSelection: self getSelection].
-	aSymbol == getSelectionSelector ifTrue: [^ self setSelection: self getSelection].
-	(aSymbol == #autoSelect and: [getSelectionSelector ~~ nil]) ifTrue:
-			[self handleEdit:
-				[textMorph editor setSearch: model autoSelectString;
-							againOrSame: true]].
-	aSymbol == #clearUserEdits ifTrue: [^ self hasUnacceptedEdits: false].
-	aSymbol == #wantToChange ifTrue:
-			[self canDiscardEdits ifFalse: [^ self promptForCancel].
-			^ self].
-	aSymbol == #appendEntry ifTrue:
+			^self setSelection: self getSelection].
+	aSymbol == getSelectionSelector 
+		ifTrue: [^self setSelection: self getSelection].
+	(aSymbol == #autoSelect and: [getSelectionSelector notNil]) 
+		ifTrue: 
+			[self handleEdit: 
+					[ParagraphEditor abandonChangeText.	"no replacement!"
+					(textMorph editor)
+						setSearch: model autoSelectString;
+						againOrSame: true]].
+	aSymbol == #clearUserEdits ifTrue: [^self hasUnacceptedEdits: false].
+	aSymbol == #wantToChange 
+		ifTrue: 
+			[self canDiscardEdits ifFalse: [^self promptForCancel].
+			^self].
+	aSymbol == #appendEntry 
+		ifTrue: 
 			[self handleEdit: [self appendEntry].
-			^ self refreshWorld].
-	aSymbol == #clearText ifTrue:
+			^self refreshWorld].
+	aSymbol == #clearText 
+		ifTrue: 
 			[self handleEdit: [self changeText: Text new].
-			^ self refreshWorld].
-	aSymbol == #bs ifTrue:
+			^self refreshWorld].
+	aSymbol == #bs 
+		ifTrue: 
 			[self handleEdit: [self bsText].
-			^ self refreshWorld].
-	aSymbol == #codeChangedElsewhere ifTrue:
+			^self refreshWorld].
+	aSymbol == #codeChangedElsewhere 
+		ifTrue: 
 			[self hasEditingConflicts: true.
-			^ self changed]
+			^self changed]
