3481e1a0b3be6235c0d1202943de0c6df0e551d2
dgd <unknown>
2003-02-22 13:43:00 +0000

    no message

--- Player/instance/scripts-2D-standard/makeNewDrawingIn-3A-.cs
+++ Player/instance/scripts-2D-standard/makeNewDrawingIn-3A-.cs
@@ -1,11 +1,11 @@
-makeNewDrawingIn: paintPlacePlayer
+makeNewDrawingIn: paintPlacePlayer 
 	| paintPlace |
-	((paintPlacePlayer == nil or: [((paintPlace _ paintPlacePlayer costume) isKindOf: PasteUpMorph) not])
-		or: [paintPlace isInWorld not])
-			ifTrue:
-				[^ self inform: 'Error: not a plausible
+	((paintPlacePlayer isNil 
+		or: [((paintPlace := paintPlacePlayer costume) isKindOf: PasteUpMorph) not]) 
+			or: [paintPlace isInWorld not]) 
+			ifTrue: 
+				[^self 
+					inform: 'Error: not a plausible
 place in which to make
 a new drawing'].
 	paintPlace makeNewDrawingWithin
-	
-	
