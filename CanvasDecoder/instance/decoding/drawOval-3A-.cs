de481e78deed12f7c5afc9a305e20d2be90383be
dgd <unknown>
2003-02-22 13:19:00 +0000

    no message

--- CanvasDecoder/instance/decoding/drawOval-3A-.cs
+++ CanvasDecoder/instance/decoding/drawOval-3A-.cs
@@ -1,15 +1,19 @@
-drawOval: command
+drawOval: command 
 	| verb rectEnc colorEnc borderWidthEnc borderColorEnc rect color borderWidth borderColor |
-	verb := command at: 1.
-	rectEnc := command at: 2.
-	colorEnc := command at: 3.
-	borderWidthEnc := command at: 4.
-	borderColorEnc := command at: 5.
-
+	verb _ command first.
+	rectEnc _ command second.
+	colorEnc _ command third.
+	borderWidthEnc _ command fourth.
+	borderColorEnc _ command fifth.
+	""
 	rect _ self class decodeRectangle: rectEnc.
 	color _ self class decodeColor: colorEnc.
 	borderWidth _ self class decodeInteger: borderWidthEnc.
 	borderColor _ self class decodeColor: borderColorEnc.
-
-	self drawCommand: [ :c |
-		c fillOval: rect color: color borderWidth: borderWidth borderColor: borderColor ]
+	""
+	self
+		drawCommand: [:c | c
+				fillOval: rect
+				color: color
+				borderWidth: borderWidth
+				borderColor: borderColor]
