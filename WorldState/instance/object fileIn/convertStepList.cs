d40c8ea696e8fd454ed5406f86648e1cacf84fe8
dgd <unknown>
2003-02-22 13:30:00 +0000

    no message

--- WorldState/instance/object fileIn/convertStepList.cs
+++ WorldState/instance/object fileIn/convertStepList.cs
@@ -1,20 +1,19 @@
 convertStepList
 	"Convert the old-style step list (an Array of Arrays) into the new-style StepMessage heap"
-	| newList wakeupTime morphToStep |
 
-	(stepList isKindOf: Heap) ifTrue:[
-		^stepList sortBlock: self stepListSortBlock	"ensure that we have a cleaner block"
-	].
-	newList _ Heap sortBlock: self stepListSortBlock.
-	stepList do:[:entry|
-		wakeupTime _ entry at: 2.
-		morphToStep _ entry at: 1.
-		newList add: (
-			StepMessage
-				scheduledAt: wakeupTime
-				stepTime: nil
-				receiver: morphToStep
-				selector: #stepAt:
-				arguments: nil).
-	].
-	stepList _ newList.
+	| newList wakeupTime morphToStep |
+	(stepList isKindOf: Heap) 
+		ifTrue: 
+			[^stepList sortBlock: self stepListSortBlock	"ensure that we have a cleaner block"].
+	newList := Heap sortBlock: self stepListSortBlock.
+	stepList do: 
+			[:entry | 
+			wakeupTime := entry second.
+			morphToStep := entry first.
+			newList add: (StepMessage 
+						scheduledAt: wakeupTime
+						stepTime: nil
+						receiver: morphToStep
+						selector: #stepAt:
+						arguments: nil)].
+	stepList := newList
