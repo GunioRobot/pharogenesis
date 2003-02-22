44c362483dd73c9a8e768e173f44439f1649d4f7
dgd <unknown>
2003-02-22 14:25:00 +0000

    no message

--- ColorSeerTile/instance/code generation/storeCodeOn-3A-indent-3A-.cs
+++ ColorSeerTile/instance/code generation/storeCodeOn-3A-indent-3A-.cs
@@ -1,8 +1,11 @@
-storeCodeOn: aStream indent: tabCount
+storeCodeOn: aStream indent: tabCount 
 	"We have a hidden arg. Output two keywords with interspersed arguments."
 
 	| parts |
-	parts _ operatorOrExpression keywords.	"color:sees:"
-	^ aStream nextPutAll: (parts at: 1); space;
-		nextPutAll: colorSwatch color printString; space;
-		nextPutAll: (parts at: 2).
+	parts := operatorOrExpression keywords.	"color:sees:"
+	^aStream
+		nextPutAll: (parts first);
+		space;
+		nextPutAll: colorSwatch color printString;
+		space;
+		nextPutAll: (parts second)
