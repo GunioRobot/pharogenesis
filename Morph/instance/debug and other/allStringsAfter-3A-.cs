07d1b03714eba1f4eacf53be9438b6a2cc1ae5cb
dgd <unknown>
2003-02-22 14:27:00 +0000

    no message

--- Morph/instance/debug and other/allStringsAfter-3A-.cs
+++ Morph/instance/debug and other/allStringsAfter-3A-.cs
@@ -1,14 +1,14 @@
-allStringsAfter: aSubmorph
+allStringsAfter: aSubmorph 
 	"return an OrderedCollection of strings of text in my submorphs.  If aSubmorph is non-nil, begin with that container."
 
 	| list string ok |
-	list _ OrderedCollection new.
-	ok _ aSubmorph == nil.
-	self allMorphsDo: [:sub | 
-		ok ifFalse: [ok _ sub == aSubmorph].		"and do this one too"
-		ok ifTrue: [
-			(string _ sub userString) ifNotNil: [
-				(string isKindOf: String) 
-					ifTrue: [list add: string]
-					ifFalse: [list addAll: string]]]].
-	^ list
+	list := OrderedCollection new.
+	ok := aSubmorph isNil.
+	self allMorphsDo: 
+			[:sub | 
+			ok ifFalse: [ok := sub == aSubmorph].	"and do this one too"
+			ok 
+				ifTrue: 
+					[(string := sub userString) ifNotNil: 
+							[string isString ifTrue: [list add: string] ifFalse: [list addAll: string]]]].
+	^list
