622958044b8673834ddd6397564909eb327e90e6
dgd <unknown>
2003-02-22 13:41:00 +0000

    no message

--- SyntaxMorph/instance/selection/setSelection-3A-.cs
+++ SyntaxMorph/instance/selection/setSelection-3A-.cs
@@ -1,19 +1,21 @@
-setSelection: newSpec
+setSelection: newSpec 
 	"A selectionSpec is {Inner morph.  Where clicked.  Outer morph}.
-	First mouseDown starts a selection (with outerMorph == nil).
+	First mouseDown starts a selection (with outerMorph isNil).
 	Dragging more than 4 pixels means to grab a copy of the current outer selection.
 		The current selection is the outerMorph, or the inner if it is nil.
 	Each mouseUp extends the selection to the next outer morph that is selectable.
 		Except if this is the first click."
+
 	| rootTile |
-	(rootTile _ self rootTile) valueOfProperty: #selectionSpec ifPresentDo:
-		[:oldSpec | oldSpec third ifNotNilDo: [:m | m deselect]].
-	(newSpec == nil or: [newSpec third == nil and: [self isMethodNode]])
-		ifTrue: [self deselect.
-			^ rootTile removeProperty: #selectionSpec].
+	(rootTile := self rootTile) valueOfProperty: #selectionSpec
+		ifPresentDo: [:oldSpec | oldSpec third ifNotNilDo: [:m | m deselect]].
+	(newSpec isNil or: [newSpec third isNil and: [self isMethodNode]]) 
+		ifTrue: 
+			[self deselect.
+			^rootTile removeProperty: #selectionSpec].
 
 	"Select outer morph of the new selection"
-	newSpec third == nil
-		ifTrue: [self select  "first click down"]
-		ifFalse: [newSpec third select  "subsequent clicks"].
+	newSpec third isNil 
+		ifTrue: [self select	"first click down"]
+		ifFalse: [newSpec third select	"subsequent clicks"].
 	rootTile setProperty: #selectionSpec toValue: newSpec
