60a1e6920447609b94695967a95b10a2f46f3913
dgd <unknown>
2003-02-22 14:09:00 +0000

    no message

--- PasteUpMorph/instance/model/createCustomModel.cs
+++ PasteUpMorph/instance/model/createCustomModel.cs
@@ -1,5 +1,5 @@
 createCustomModel
 	"Create a model object for this world if it does not yet have one. A model object is an initially empty subclass of MorphicModel. As the user names parts and adds behavior, instance variables and methods are added to this class."
 
-	model == nil ifFalse: [^ self].
-	model _ MorphicModel newSubclass new.
+	model isNil ifFalse: [^self].
+	model := MorphicModel newSubclass new
