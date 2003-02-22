f837ac88c81fa970c82cbb0bc1b9cb4b2b3e0740
dgd <unknown>
2003-02-22 18:43:00 +0000

    no message

--- CanvasDecoder/instance/decoding/drawInfiniteFill-3A-.cs
+++ CanvasDecoder/instance/decoding/drawInfiniteFill-3A-.cs
@@ -1,12 +1,7 @@
-drawInfiniteFill: command
-
+drawInfiniteFill: command 
 	| aRectangle aFillStyle |
-
-	aRectangle _ self class decodeRectangle: (command at: 2).
-	aFillStyle _ InfiniteForm with: (self class decodeImage: (command at: 3)).
-
-	self drawCommand: [ :c |
-		c asBalloonCanvas 
-			fillRectangle: aRectangle 
-			fillStyle: aFillStyle.
-	].
+	aRectangle := self class decodeRectangle: (command second).
+	aFillStyle := InfiniteForm with: (self class decodeImage: command third).
+	self drawCommand: 
+			[:c | 
+			c asBalloonCanvas fillRectangle: aRectangle fillStyle: aFillStyle]
