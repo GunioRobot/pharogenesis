08c2fce243095e3f0a31a7a47f1fce2330452f2b
gm <unknown>
2003-02-22 13:14:00 +0000

    no message

--- SystemWindow/instance/panes/existingPaneColor.cs
+++ SystemWindow/instance/panes/existingPaneColor.cs
@@ -1,10 +1,14 @@
 existingPaneColor
-	| aColor |
 	"Answer the existing pane color for the window, obtaining it from the first paneMorph if any, and fall back on using the second stripe color if necessary."
 
-	paneMorphs isEmptyOrNil
-		ifFalse:
-			[((aColor _ paneMorphs first color) isKindOf: Color)
-				ifTrue:
-					[^ aColor]].
-	^ stripes second color
+	| aColor |
+	Preferences alternativeWindowLook 
+		ifTrue: 
+			[aColor := self valueOfProperty: #paneColor.
+			aColor 
+				ifNil: [self setProperty: #paneColor toValue: (aColor := self paneColor)].
+			^aColor].
+	paneMorphs isEmptyOrNil 
+		ifFalse: 
+			[((aColor := paneMorphs first color) isColor) ifTrue: [^aColor]].
+	^stripes ifNotNil: [stripes second color] ifNil: [Color blue lighter]
