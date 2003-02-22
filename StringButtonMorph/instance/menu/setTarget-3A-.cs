79b1c9e70e7ac51425430a1f5b159f144e07035e
dgd <unknown>
2003-02-22 18:55:00 +0000

    no message

--- StringButtonMorph/instance/menu/setTarget-3A-.cs
+++ StringButtonMorph/instance/menu/setTarget-3A-.cs
@@ -1,7 +1,6 @@
-setTarget: evt
-
+setTarget: evt 
 	| rootMorphs |
 	rootMorphs _ self world rootMorphsAt: evt hand targetOffset.
-	rootMorphs size > 1
-		ifTrue: [target _ rootMorphs at: 2]
-		ifFalse: [target _ nil. ^ self].
+	target _ rootMorphs size > 1
+		ifTrue: [rootMorphs second]
+		ifFalse: [nil]
