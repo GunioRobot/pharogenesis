7be0bdafac4481910aba515dceb0f7dc5f2cdc3d
dgd <unknown>
2003-02-22 18:51:00 +0000

    no message

--- RemoteHandMorph/instance/event handling/processEvents.cs
+++ RemoteHandMorph/instance/event handling/processEvents.cs
@@ -2,10 +2,11 @@ processEvents
 	"Process user input events from the remote input devices."
 
 	| evt |
-	evt _ self getNextRemoteEvent.
-	[evt ~~ nil] whileTrue: [
-		evt type == #worldExtent ifTrue: [
-			remoteWorldExtent _ evt argument.
-			^ self].
-		self handleEvent: evt.
-		evt _ self getNextRemoteEvent]
+	evt := self getNextRemoteEvent.
+	[evt notNil] whileTrue: 
+			[evt type == #worldExtent 
+				ifTrue: 
+					[remoteWorldExtent := evt argument.
+					^self].
+			self handleEvent: evt.
+			evt := self getNextRemoteEvent]
