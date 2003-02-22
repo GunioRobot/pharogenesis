0612290259b79bd3397662dd8f0d30dc44430f73
dgd <unknown>
2003-02-22 14:39:00 +0000

    no message

--- GraphMorph/instance/drawing/drawOn-3A-.cs
+++ GraphMorph/instance/drawing/drawOn-3A-.cs
@@ -1,12 +1,13 @@
-drawOn: aCanvas
-
+drawOn: aCanvas 
 	| c |
-	cachedForm = nil ifTrue:  [
-		c _ Display defaultCanvasClass extent: bounds extent.
-		c translateBy: bounds origin negated
-			during:[:tempCanvas| self drawDataOn: tempCanvas].
-		cachedForm _ c form].
-	aCanvas cache: bounds
-			using: cachedForm
-			during:[:cachingCanvas| self drawDataOn: cachingCanvas].
-	self drawCursorOn: aCanvas.
+	cachedForm isNil 
+		ifTrue: 
+			[c := Display defaultCanvasClass extent: bounds extent.
+			c translateBy: bounds origin negated
+				during: [:tempCanvas | self drawDataOn: tempCanvas].
+			cachedForm := c form].
+	aCanvas 
+		cache: bounds
+		using: cachedForm
+		during: [:cachingCanvas | self drawDataOn: cachingCanvas].
+	self drawCursorOn: aCanvas
