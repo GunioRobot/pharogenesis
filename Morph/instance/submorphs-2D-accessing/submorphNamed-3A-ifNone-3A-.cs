895b89da655718f8a245683465853a3ef1b50aa2
gm <unknown>
2003-02-22 13:16:00 +0000

    no message

--- Morph/instance/submorphs-2D-accessing/submorphNamed-3A-ifNone-3A-.cs
+++ Morph/instance/submorphs-2D-accessing/submorphNamed-3A-ifNone-3A-.cs
@@ -1,13 +1,14 @@
-submorphNamed: aName ifNone: aBlock
+submorphNamed: aName ifNone: aBlock 
 	"Find the first submorph with this name, or a button with an action selector of that name"
 
 	| sub args |
-	self submorphs do: [:p | p knownName = aName ifTrue: [^ p]].
-	self submorphs do: [:button |
-		(button respondsTo: #actionSelector) ifTrue:
-			[button actionSelector == aName ifTrue: [^ button]].
-		((button respondsTo: #arguments) and: [(args _ button arguments) notNil])
-			ifTrue: [(args at: 2 ifAbsent: [nil]) == aName ifTrue: [^ button]].
-		(button isKindOf: AlignmentMorph) ifTrue:
-			[(sub _ button submorphNamed: aName ifNone: [nil]) ifNotNil: [^ sub]]].
-	^ aBlock value
+	self submorphs do: [:p | p knownName = aName ifTrue: [^p]].
+	self submorphs do: 
+			[:button | 
+			(button respondsTo: #actionSelector) 
+				ifTrue: [button actionSelector == aName ifTrue: [^button]].
+			((button respondsTo: #arguments) and: [(args := button arguments) notNil]) 
+				ifTrue: [(args at: 2 ifAbsent: [nil]) == aName ifTrue: [^button]].
+			(button isAlignmentMorph) 
+				ifTrue: [(sub := button submorphNamed: aName ifNone: [nil]) ifNotNil: [^sub]]].
+	^aBlock value
