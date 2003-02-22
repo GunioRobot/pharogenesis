145354ac7055e177321a80fdd749724b79c288ad
dgd <unknown>
2003-02-22 18:45:00 +0000

    no message

--- ThreePhaseButtonMorph/instance/printing/printOn-3A-.cs
+++ ThreePhaseButtonMorph/instance/printing/printOn-3A-.cs
@@ -1,14 +1,18 @@
-printOn: aStream
-
+printOn: aStream 
 	| string |
-
 	aStream nextPutAll: '3PButton'.
-	arguments size > 0 ifTrue: [string _ arguments at: (2 min: arguments size)].
+	arguments notEmpty 
+		ifTrue: [string := arguments at: (2 min: arguments size)].
 	aStream nextPutAll: '('.
-	(string ~~ nil and: [string ~~ self])
-		ifTrue:
-			[aStream print: string; space]
-		ifFalse:
-			[aStream print: actionSelector; space].
-	aStream print: self identityHash;
-			nextPutAll: ')'.
+	(string notNil and: [string ~~ self]) 
+		ifTrue: 
+			[aStream
+				print: string;
+				space]
+		ifFalse: 
+			[aStream
+				print: actionSelector;
+				space].
+	aStream
+		print: self identityHash;
+		nextPutAll: ')'
