e5609e729ae0481714856a39405aee6ae2851039
dgd <unknown>
2003-02-22 13:45:00 +0000

    no message

--- Player/instance/misc/revertToUnscriptedPlayerIfAppropriate.cs
+++ Player/instance/misc/revertToUnscriptedPlayerIfAppropriate.cs
@@ -1,6 +1,7 @@
 revertToUnscriptedPlayerIfAppropriate
 	| anInstance |
-	((self class selectors size > 0) or: [self class instVarNames size > 0]) ifTrue: [^ self].
-	anInstance _ UnscriptedPlayer new.
+	(self class selectors notEmpty or: [self class instVarNames notEmpty]) 
+		ifTrue: [^self].
+	anInstance := UnscriptedPlayer new.
 	anInstance initializeCostumesFrom: self.
 	self become: anInstance
