6b864f6ac439d4810231df59eebb2e663763c034
dgd <unknown>
2003-02-22 13:18:00 +0000

    no message

--- CanvasDecoder/instance/decoding/drawLine-3A-.cs
+++ CanvasDecoder/instance/decoding/drawLine-3A-.cs
@@ -1,15 +1,19 @@
-drawLine: command
+drawLine: command 
 	| verb pt1Enc pt2Enc widthEnc colorEnc pt1 pt2 width color |
-	verb := command at: 1.
-	pt1Enc := command at: 2.
-	pt2Enc := command at: 3.
-	widthEnc := command at: 4.
-	colorEnc := command at: 5.
-
+	verb _ command first.
+	pt1Enc _ command second.
+	pt2Enc _ command third.
+	widthEnc _ command fourth.
+	colorEnc _ command fifth.
+""
 	pt1 _ self class decodePoint: pt1Enc.
 	pt2 _ self class decodePoint: pt2Enc.
 	width _ self class decodeInteger: widthEnc.
 	color _ self class decodeColor: colorEnc.
-
-	self drawCommand: [ :c |
-		c line: pt1 to: pt2 width: width color: color ]
+""
+	self
+		drawCommand: [:c | c
+				line: pt1
+				to: pt2
+				width: width
+				color: color]
