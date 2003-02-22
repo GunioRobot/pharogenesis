5dbe762672912f95a657b65284ae74e0c5998fbf
dgd <unknown>
2003-02-22 18:44:00 +0000

    no message

--- CanvasDecoder/instance/decoding/drawStencil-3A-.cs
+++ CanvasDecoder/instance/decoding/drawStencil-3A-.cs
@@ -1,14 +1,17 @@
-drawStencil: command
+drawStencil: command 
 	| stencilFormEnc locationEnc sourceRectEnc colorEnc stencilForm location sourceRect color |
-	stencilFormEnc := command at: 2.
-	locationEnc := command at: 3.
-	sourceRectEnc := command at: 4.
-	colorEnc := command at: 5.
-
+	stencilFormEnc := command second.
+	locationEnc := command third.
+	sourceRectEnc := command fourth.
+	colorEnc := command fifth.
 	stencilForm := self class decodeImage: stencilFormEnc.
 	location := self class decodePoint: locationEnc.
 	sourceRect := self class decodeRectangle: sourceRectEnc.
 	color := self class decodeColor: colorEnc.
-
-	self drawCommand: [ :executor |
-		executor stencil: stencilForm at: location sourceRect: sourceRect  color: color ]
+	self drawCommand: 
+			[:executor | 
+			executor 
+				stencil: stencilForm
+				at: location
+				sourceRect: sourceRect
+				color: color]
