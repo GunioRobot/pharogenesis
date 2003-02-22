8cd7ed08520cb131ad228c652d5102b7367bc060
dgd <unknown>
2003-02-22 14:38:00 +0000

    no message

--- MVCWiWPasteUpMorph/instance/geometry/resetViewBox.cs
+++ MVCWiWPasteUpMorph/instance/geometry/resetViewBox.cs
@@ -1,8 +1,7 @@
 resetViewBox
-
 	| c |
-	(c _ worldState canvas) == nil ifTrue: [^self resetViewBoxForReal].
+	(c := worldState canvas) isNil ifTrue: [^self resetViewBoxForReal].
 	c form == Display ifFalse: [^self resetViewBoxForReal].
-	c origin = (0@0) ifFalse: [^self resetViewBoxForReal].
-	c clipRect extent = self viewBox extent ifFalse: [^self resetViewBoxForReal].
-			
+	c origin = (0 @ 0) ifFalse: [^self resetViewBoxForReal].
+	c clipRect extent = self viewBox extent 
+		ifFalse: [^self resetViewBoxForReal]
