d39039394e9895aa8e61071b394ef7abbe04bca9
dgd <unknown>
2003-02-22 14:40:00 +0000

    no message

--- CommandHistory/instance/menu/undoMenuWording.cs
+++ CommandHistory/instance/menu/undoMenuWording.cs
@@ -1,14 +1,13 @@
 undoMenuWording
 	"Answer the wording to be used in an 'undo' menu item"
 
-	(((lastCommand == nil or: [Preferences useUndo not]) or:
-		[Preferences infiniteUndo not and: [lastCommand phase == #undone]])  or: 
-			[self nextCommandToUndo == nil]) ifTrue: [^ 'can''t undo'].
-
-	^ String streamContents:
-		[:aStream | 
+	(((lastCommand isNil or: [Preferences useUndo not]) 
+		or: [Preferences infiniteUndo not and: [lastCommand phase == #undone]]) 
+			or: [self nextCommandToUndo isNil]) ifTrue: [^'can''t undo'].
+	^String streamContents: 
+			[:aStream | 
 			aStream nextPutAll: 'undo "'.
-			aStream nextPutAll: (self nextCommandToUndo cmdWording truncateTo: 12).
+			aStream 
+				nextPutAll: (self nextCommandToUndo cmdWording truncateWithElipsisTo: 20).
 			aStream nextPut: $".
-			lastCommand phase == #done ifTrue:
-				[aStream nextPutAll: ' (z)']]
+			lastCommand phase == #done ifTrue: [aStream nextPutAll: ' (z)']]
