0c99d912fae25ce6455d3caa348f80e211704f21
gm <unknown>
2003-02-22 13:17:00 +0000

    no message

--- BookMorph/instance/menu/highlightText-3A-at-3A-in-3A-.cs
+++ BookMorph/instance/menu/highlightText-3A-at-3A-in-3A-.cs
@@ -1,17 +1,19 @@
-highlightText: stringToHilite at: index in: insideOf
+highlightText: stringToHilite at: index in: insideOf 
 	"Find the container with this text and highlight it.  May not be able to do it for stringMorphs."
 
 	"Find the container with that text"
+
 	| container |
-	self allMorphsDo: [:sub | 
-		insideOf == sub userString ifTrue: [container _ sub]].
-	container ifNil: [
-		self allMorphsDo: [:sub | 
-			insideOf = sub userString ifTrue: [container _ sub]]].	"any match"
-	container ifNil: [^ nil].
+	self 
+		allMorphsDo: [:sub | insideOf == sub userString ifTrue: [container := sub]].
+	container ifNil: 
+			[self 
+				allMorphsDo: [:sub | insideOf = sub userString ifTrue: [container := sub]]].	"any match"
+	container ifNil: [^nil].
 
 	"Order it highlighted"
-	(container isKindOf: TextMorph) ifTrue: [
-		container editor selectFrom: index to: stringToHilite size - 1 + index].
+	(container isTextMorph) 
+		ifTrue: 
+			[container editor selectFrom: index to: stringToHilite size - 1 + index].
 	container changed.
-	^ container
+	^container
