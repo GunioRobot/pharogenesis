57d89566c56bc1856205b984c60cddae75cf72b0
dgd <unknown>
2003-02-22 14:23:00 +0000

    no message

--- EnvelopeEditorMorph/instance/menu/adjustScale-3A-.cs
+++ EnvelopeEditorMorph/instance/menu/adjustScale-3A-.cs
@@ -1,14 +1,16 @@
-adjustScale: evt
+adjustScale: evt 
 	| scaleString oldScale baseValue |
-	oldScale _ envelope scale.
-	scaleString _ FillInTheBlank request: 'Enter the new full-scale value...'
-						initialAnswer: oldScale printString.
-	scaleString isEmpty ifTrue: [^ self].
+	oldScale := envelope scale.
+	scaleString := FillInTheBlank request: 'Enter the new full-scale value...'
+				initialAnswer: oldScale printString.
+	scaleString isEmpty ifTrue: [^self].
 	envelope scale: (Number readFrom: scaleString) asFloat.
-	baseValue _ envelope updateSelector = #pitch: ifTrue: [0.5] ifFalse: [0.0].
-	envelope setPoints: (envelope points collect:
-				[:p |
-				p x @ (p y - baseValue * oldScale / envelope scale + baseValue
-					min: 1.0 max: 0.0)])
-			 loopStart: (limits at: 1) loopEnd: (limits at: 2).
+	baseValue := envelope updateSelector = #pitch: ifTrue: [0.5] ifFalse: [0.0].
+	envelope 
+		setPoints: (envelope points collect: 
+					[:p | 
+					p x @ ((p y - baseValue) * oldScale / envelope scale + baseValue min: 1.0
+								max: 0.0)])
+		loopStart: (limits first)
+		loopEnd: (limits second).
 	self buildView
