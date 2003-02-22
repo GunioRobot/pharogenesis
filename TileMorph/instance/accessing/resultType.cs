d3f2a17f1e3dac49ce09bee13bfa635cf1e466c7
gm <unknown>
2003-02-22 13:11:00 +0000

    no message

--- TileMorph/instance/accessing/resultType.cs
+++ TileMorph/instance/accessing/resultType.cs
@@ -1,15 +1,11 @@
 resultType
+	"Answer the result type of the receiver"
 
-	type == #literal ifTrue:
-		[(literal isKindOf: Number) ifTrue: [^ #number].
-		(literal isKindOf: String) ifTrue: [^ #string].
-		(literal isKindOf: Point) ifTrue: [^ #point].
-		(literal isKindOf: String) ifTrue: [^ #string].
-		(literal isKindOf: Boolean) ifTrue: [^ #boolean].
-		(literal isKindOf: Morph) ifTrue: [^ #costume]  "This last one dubious!"].
-
-	type == #expression ifTrue:
-		[^ #number].
-
-	type == #objRef ifTrue: [^ #player].
-	^ #unknown
+	type == #literal 
+		ifTrue: 
+			[(literal isNumber) ifTrue: [^#Number].
+			(literal isString) ifTrue: [^#String].
+			(literal isKindOf: Boolean) ifTrue: [^#Boolean]].
+	type == #expression ifTrue: [^#Number].
+	type == #objRef ifTrue: [^#Player].
+	^#unknown
