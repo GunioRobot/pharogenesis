55e96fe07a7753a09acb7abeaa3715b0bae8db64
gm <unknown>
2003-02-22 12:35:00 +0000

    no message

--- SyntaxMorph/instance/tests/testForNode-3A-andDo-3A-.cs
+++ SyntaxMorph/instance/tests/testForNode-3A-andDo-3A-.cs
@@ -1,8 +1,6 @@
-testForNode: targetNode andDo: aBlock
-
+testForNode: targetNode andDo: aBlock 
 	targetNode == parseNode ifTrue: [aBlock value: self].
-	self submorphsDo: [ :each | 
-		(each isKindOf: SyntaxMorph) ifTrue: [
-			each testForNode: targetNode andDo: aBlock
-		].
-	].
+	self submorphsDo: 
+			[:each | 
+			(each isSyntaxMorph) 
+				ifTrue: [each testForNode: targetNode andDo: aBlock]]
