7b9ebe479fc865732d682b3b1207c2e26ff6c3c2
dgd <unknown>
2003-02-22 14:30:00 +0000

    no message

--- Morph/instance/submorphs-2D-add-2F-remove/comeToFront.cs
+++ Morph/instance/submorphs-2D-add-2F-remove/comeToFront.cs
@@ -1,6 +1,7 @@
 comeToFront
 	| outerMorph |
-	outerMorph _ self topRendererOrSelf.
-	(outerMorph owner == nil or: [outerMorph owner hasSubmorphs not]) ifTrue: [^ self].
-	outerMorph owner firstSubmorph == outerMorph ifFalse:
-		[outerMorph owner addMorphFront: outerMorph]
+	outerMorph := self topRendererOrSelf.
+	(outerMorph owner isNil or: [outerMorph owner hasSubmorphs not]) 
+		ifTrue: [^self].
+	outerMorph owner firstSubmorph == outerMorph 
+		ifFalse: [outerMorph owner addMorphFront: outerMorph]
