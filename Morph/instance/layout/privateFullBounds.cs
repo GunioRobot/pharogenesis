52502908f63020f8b9a1ff47f1c751a24d7e3bad
dgd <unknown>
2003-02-22 14:33:00 +0000

    no message

--- Morph/instance/layout/privateFullBounds.cs
+++ Morph/instance/layout/privateFullBounds.cs
@@ -1,9 +1,10 @@
 privateFullBounds
 	"Private. Compute the actual full bounds of the receiver"
+
 	| box |
-	submorphs size = 0 ifTrue: [^self outerBounds].
-	box _ self outerBounds copy.
-	self clipSubmorphs
-		ifTrue:[box _ box quickMerge: (self submorphBounds intersect: self clippingBounds)]
-		ifFalse:[box _ box quickMerge: self submorphBounds].
-	^ box origin asIntegerPoint corner: box corner asIntegerPoint
+	submorphs isEmpty ifTrue: [^self outerBounds].
+	box := self outerBounds copy.
+	box := box quickMerge: (self clipSubmorphs 
+						ifTrue: [self submorphBounds intersect: self clippingBounds]
+						ifFalse: [self submorphBounds]).
+	^box origin asIntegerPoint corner: box corner asIntegerPoint
