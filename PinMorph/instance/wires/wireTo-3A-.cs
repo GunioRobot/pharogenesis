b77aac016238e15ddbe302112d8d1d3b7b11e8ac
dgd <unknown>
2003-02-22 14:38:00 +0000

    no message

--- PinMorph/instance/wires/wireTo-3A-.cs
+++ PinMorph/instance/wires/wireTo-3A-.cs
@@ -1,13 +1,17 @@
-wireTo: otherPin
+wireTo: otherPin 
 	"Note must return true or false indicating success"
-	(otherPin == nil or: [otherPin == self]) ifTrue: [^ false].
-	self hasVariable
-	ifTrue: [otherPin hasVariable
-			ifTrue: [self mergeVariableWith: otherPin]
-			ifFalse: [otherPin shareVariableOf: self]]
-	ifFalse: [otherPin hasVariable
-			ifTrue: [self shareVariableOf: otherPin]
-			ifFalse: [self addModelVariable.
+
+	(otherPin isNil or: [otherPin == self]) ifTrue: [^false].
+	self hasVariable 
+		ifTrue: 
+			[otherPin hasVariable 
+				ifTrue: [self mergeVariableWith: otherPin]
+				ifFalse: [otherPin shareVariableOf: self]]
+		ifFalse: 
+			[otherPin hasVariable 
+				ifTrue: [self shareVariableOf: otherPin]
+				ifFalse: 
+					[self addModelVariable.
 					otherPin shareVariableOf: self]].
 	component model changed: pinSpec modelReadSelector.
-	^ true
+	^true
