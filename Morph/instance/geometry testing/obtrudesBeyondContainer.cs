6b9ffd89fa570c3cc66e841417cd4a2824d8c4cc
dgd <unknown>
2003-02-22 14:33:00 +0000

    no message

--- Morph/instance/geometry testing/obtrudesBeyondContainer.cs
+++ Morph/instance/geometry testing/obtrudesBeyondContainer.cs
@@ -2,7 +2,6 @@ obtrudesBeyondContainer
 	"Answer whether the receiver obtrudes beyond the bounds of its container"
 
 	| top |
-	top _ self topRendererOrSelf.
-	(top owner == nil or: [top owner isHandMorph]) ifTrue: [^ false].
-	^ (top owner bounds containsRect: top bounds) not
-	
+	top := self topRendererOrSelf.
+	(top owner isNil or: [top owner isHandMorph]) ifTrue: [^false].
+	^(top owner bounds containsRect: top bounds) not
