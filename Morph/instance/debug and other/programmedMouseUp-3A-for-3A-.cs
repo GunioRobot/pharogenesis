2114b1c9b4c18516942b6c71a293787abbc53ecf
gm <unknown>
2003-02-22 13:41:00 +0000

    no message

--- Morph/instance/debug and other/programmedMouseUp-3A-for-3A-.cs
+++ Morph/instance/debug and other/programmedMouseUp-3A-for-3A-.cs
@@ -1,20 +1,16 @@
-programmedMouseUp: anEvent for: aMorph
-
+programmedMouseUp: anEvent for: aMorph 
 	| aCodeString |
-
 	self deleteAnyMouseActionIndicators.
-	aCodeString _ self valueOfProperty: #mouseUpCodeToRun ifAbsent: [^self].
+	aCodeString := self valueOfProperty: #mouseUpCodeToRun ifAbsent: [^self].
 	(self fullBounds containsPoint: anEvent cursorPoint) ifFalse: [^self].
-	[
-		(aCodeString isKindOf: MessageSend) ifTrue: [
-			aCodeString value
-		] ifFalse: [
-			Compiler
+	
+	[(aCodeString isMessageSend) 
+		ifTrue: [aCodeString value]
+		ifFalse: 
+			[Compiler 
 				evaluate: aCodeString
 				for: self
 				notifying: nil
-				logged: false
-		].
-	]
-		on: ProgressTargetRequestNotification
-		do: [ :ex | ex resume: self].		"in case a save/load progress display needs a home"
+				logged: false]] 
+			on: ProgressTargetRequestNotification
+			do: [:ex | ex resume: self]	"in case a save/load progress display needs a home"
