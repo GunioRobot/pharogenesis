b0c6f5a7f2b5011ed3b16d32e541b521b611194f
dgd <unknown>
2003-02-22 19:05:00 +0000

    no message

--- Morph/instance/debug and other/ownerChain.cs
+++ Morph/instance/debug and other/ownerChain.cs
@@ -1,7 +1,8 @@
 ownerChain
 	"Answer a list of objects representing the receiver and all of its owners.   The first element is the receiver, and the last one is typically the world in which the receiver resides"
+
 	| c next |
-	c _ OrderedCollection with: self.
-	next _ self.
-	[(next _ next owner) ~~ nil] whileTrue: [c add: next].
-	^ c asArray
+	c := OrderedCollection with: self.
+	next := self.
+	[(next := next owner) notNil] whileTrue: [c add: next].
+	^c asArray
