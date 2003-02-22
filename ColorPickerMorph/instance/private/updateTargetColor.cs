18eb5d929999ec663dcc6d436942e0473fc2906f
dgd <unknown>
2003-02-22 18:41:00 +0000

    no message

--- ColorPickerMorph/instance/private/updateTargetColor.cs
+++ ColorPickerMorph/instance/private/updateTargetColor.cs
@@ -1,8 +1,20 @@
 updateTargetColor
 	| nArgs |
-	(target ~~ nil and: [selector ~~ nil]) ifTrue:
-		[self updateSelectorDisplay.
-		nArgs _ selector numArgs.
-		nArgs = 1 ifTrue:[^target perform: selector with: selectedColor].
-		nArgs = 2 ifTrue:[^target perform: selector with: selectedColor with: sourceHand].
-		nArgs = 3 ifTrue:[^target perform: selector with: selectedColor with: argument with: sourceHand]].
+	(target notNil and: [selector notNil]) 
+		ifTrue: 
+			[self updateSelectorDisplay.
+			nArgs := selector numArgs.
+			nArgs = 1 ifTrue: [^target perform: selector with: selectedColor].
+			nArgs = 2 
+				ifTrue: 
+					[^target 
+						perform: selector
+						with: selectedColor
+						with: sourceHand].
+			nArgs = 3 
+				ifTrue: 
+					[^target 
+						perform: selector
+						with: selectedColor
+						with: argument
+						with: sourceHand]]
