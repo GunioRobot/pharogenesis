d6eb932e935944d8504fb3acc077b1a234cdbd57
dgd <unknown>
2003-02-22 13:22:00 +0000

    no message

--- MoviePlayerMorph/instance/stepping/step.cs
+++ MoviePlayerMorph/instance/stepping/step.cs
@@ -6,45 +6,61 @@ step
 	"This code may not work right for playing backwards right now.
 	Single-step and backwards (dir <= 0) should just run open-loop."
 
-	|  byteCount simTime ms nextFrameNumber |
-	movieFile == nil ifTrue: [^ self].
-	scorePlayer == nil
-		ifTrue: [(ms _ Time millisecondClockValue) < msAtStart ifTrue:  "clock rollover"
-					[msAtStart _ ms - (frameNumber * msPerFrame)].
-				simTime _ ms - msAtStart]
-		ifFalse: [simTime _ scorePlayer millisecondsSinceStart].
-	playDirection > 0
-		ifTrue: [nextFrameNumber _ frameAtLastSync + ((simTime - msAtLastSync)//msPerFrame).
-				nextFrameNumber = frameNumber ifTrue:
-					[((scorePlayer isKindOf: AbstractSound) and: [scorePlayer isPlaying not])
-						ifTrue: [^ self stopRunning].
-					^ self]]
-		ifFalse: [playDirection < 0 ifTrue: [nextFrameNumber _ frameNumber - 1]
-								ifFalse: [nextFrameNumber _ frameNumber]].
+	| byteCount simTime ms nextFrameNumber |
+	movieFile isNil ifTrue: [^self].
+	scorePlayer isNil 
+		ifTrue: 
+			[(ms := Time millisecondClockValue) < msAtStart 
+				ifTrue: 
+					["clock rollover"
 
-	byteCount _ self fileByteCountPerFrame.
+					msAtStart := ms - (frameNumber * msPerFrame)].
+			simTime := ms - msAtStart]
+		ifFalse: [simTime := scorePlayer millisecondsSinceStart].
+	playDirection > 0 
+		ifTrue: 
+			[nextFrameNumber := frameAtLastSync 
+						+ ((simTime - msAtLastSync) // msPerFrame).
+			nextFrameNumber = frameNumber 
+				ifTrue: 
+					[((scorePlayer isKindOf: AbstractSound) and: [scorePlayer isPlaying not]) 
+						ifTrue: [^self stopRunning].
+					^self]]
+		ifFalse: 
+			[nextFrameNumber := playDirection < 0 
+						ifTrue: [frameNumber - 1]
+						ifFalse: [frameNumber]].
+	byteCount := self fileByteCountPerFrame.
 	self stepSoundTrack.
 	movieFile waitForCompletion.
-	movieFile primReadResult: movieFile fileHandle
-			intoBuffer: (frameBufferIfScaled ifNil: [currentPage image]) bits
-			at: 1 count: byteCount//4.
-	frameBufferIfScaled ifNotNil:
-		["If this player has been shrunk, then we have to warp to the current page."
-		(WarpBlt current toForm: currentPage image) sourceForm: frameBufferIfScaled;
+	movieFile 
+		primReadResult: movieFile fileHandle
+		intoBuffer: (frameBufferIfScaled ifNil: [currentPage image]) bits
+		at: 1
+		count: byteCount // 4.
+	frameBufferIfScaled ifNotNil: 
+			["If this player has been shrunk, then we have to warp to the current page."
+
+			(WarpBlt current toForm: currentPage image)
+				sourceForm: frameBufferIfScaled;
 				combinationRule: 3;
-						"Use smoothing if just stepping"
-				cellSize: (playDirection = 0 ifTrue: [2] ifFalse: [1]);
+				cellSize: (playDirection = 0 
+							ifTrue: 
+								["Use smoothing if just stepping"
+
+								2]
+							ifFalse: [1]);
 				copyQuad: frameBufferIfScaled boundingBox innerCorners
 					toRect: currentPage image boundingBox].
 	currentPage changed.
-	frameNumber _ nextFrameNumber.	
-
-	(playDirection = 0
-		or: [(playDirection > 0 and: [frameNumber >= frameCount])
-		or: [playDirection < 0 and: [frameNumber <= 1]]])
-		ifTrue: [^ self stopRunning].
+	frameNumber := nextFrameNumber.
+	(playDirection = 0 or: 
+			[(playDirection > 0 and: [frameNumber >= frameCount]) 
+				or: [playDirection < 0 and: [frameNumber <= 1]]]) 
+		ifTrue: [^self stopRunning].
 
 	"Start the read operation for the next frame..."
-	movieFile primReadStart: movieFile fileHandle
-			fPosition: (self filePosForFrameNo: frameNumber)
-			count: byteCount.
+	movieFile 
+		primReadStart: movieFile fileHandle
+		fPosition: (self filePosForFrameNo: frameNumber)
+		count: byteCount
