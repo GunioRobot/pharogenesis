782afc102097af7f594f4bf714e33eaed7471eb8
dgd <unknown>
2003-02-22 14:10:00 +0000

    no message

--- PasteUpMorph/instance/world menu/findDirtyBrowsers-3A-.cs
+++ PasteUpMorph/instance/world menu/findDirtyBrowsers-3A-.cs
@@ -1,10 +1,15 @@
-findDirtyBrowsers: evt
+findDirtyBrowsers: evt 
 	"Present a menu of window titles for browsers with changes,
 	and activate the one that gets chosen."
+
 	| menu |
-	menu _ MenuMorph new.
+	menu := MenuMorph new.
 	(SystemWindow windowsIn: self
-		satisfying: [:w | (w model isKindOf: Browser) and: [w model canDiscardEdits not]])
-		do: [:w | menu add: w label target: w action: #activate].
-	menu submorphs size > 0 ifTrue:
-		[menu popUpEvent: evt in: self]
+		satisfying: [:w | (w model isKindOf: Browser) and: [w model canDiscardEdits not]]) 
+			do: 
+				[:w | 
+				menu 
+					add: w label
+					target: w
+					action: #activate].
+	menu submorphs notEmpty ifTrue: [menu popUpEvent: evt in: self]
