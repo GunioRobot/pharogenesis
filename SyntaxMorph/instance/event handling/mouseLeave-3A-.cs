a4e0adb7060d5ec2ae55a7ac0679356f7ac9011a
dgd <unknown>
2003-02-22 18:48:00 +0000

    no message

--- SyntaxMorph/instance/event handling/mouseLeave-3A-.cs
+++ SyntaxMorph/instance/event handling/mouseLeave-3A-.cs
@@ -1,8 +1,9 @@
-mouseLeave: evt
+mouseLeave: evt 
 	"Move grab highlight back out a level"
 
-"Transcript cr; print: self; show: ' leave'."
-	self unhighlight.
-	(owner ~~ nil and: [owner isSyntaxMorph])
-		ifTrue: [owner highlightForGrab: evt].
+	"Transcript cr; print: self; show: ' leave'."
 
+	self rootTile isMethodNode ifFalse: [^self].	"not in a script"
+	self unhighlightBorder.
+	(owner notNil and: [owner isSyntaxMorph]) 
+		ifTrue: [owner highlightForGrab: evt]
