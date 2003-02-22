c68676af4a43d7e564a4cf668153125f8548f91b
dgd <unknown>
2003-02-22 14:27:00 +0000

    no message

--- Morph/instance/submorphs-2D-accessing/allMorphsDo-3A-.cs
+++ Morph/instance/submorphs-2D-accessing/allMorphsDo-3A-.cs
@@ -1,7 +1,5 @@
-allMorphsDo: aBlock
+allMorphsDo: aBlock 
 	"Evaluate the given block for all morphs in this composite morph (including the receiver)."
 
-	submorphs size > 0 ifTrue: [
-		submorphs do: [:m | m allMorphsDo: aBlock].
-	].
-	aBlock value: self.
+	submorphs do: [:m | m allMorphsDo: aBlock].
+	aBlock value: self
