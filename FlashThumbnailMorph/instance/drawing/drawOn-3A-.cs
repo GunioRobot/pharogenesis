b85ab3c1ed4e66be4355166567d9ed442781dfa4
dgd <unknown>
2003-02-22 13:29:00 +0000

    no message

--- FlashThumbnailMorph/instance/drawing/drawOn-3A-.cs
+++ FlashThumbnailMorph/instance/drawing/drawOn-3A-.cs
@@ -1,11 +1,20 @@
-drawOn: aCanvas
-	(player == nil or:[frameNumber == nil]) ifTrue:[^super drawOn: aCanvas].
-false ifTrue:[super drawOn: aCanvas.
-			^aCanvas text: frameNumber printString bounds: self innerBounds font: nil color: Color red].
-	image ifNil:[
-		Cursor wait showWhile:[
-			image _ player imageFormOfSize: (self extent - (self borderWidth * 2))
-					 forFrame: frameNumber.
-			frameNumber printString displayOn: image]].
-	aCanvas frameRectangle: self bounds width: self borderWidth color: self borderColor.
-	aCanvas paintImage: image at: self topLeft + self borderWidth.
+drawOn: aCanvas 
+	(player isNil or: [frameNumber isNil]) ifTrue: [^super drawOn: aCanvas].
+	false 
+		ifTrue: 
+			[super drawOn: aCanvas.
+			^aCanvas 
+				drawString: frameNumber printString
+				in: self innerBounds
+				font: nil
+				color: Color red].
+	image ifNil: 
+			[Cursor wait showWhile: 
+					[image := player imageFormOfSize: self extent - (self borderWidth * 2)
+								forFrame: frameNumber.
+					frameNumber printString displayOn: image]].
+	aCanvas 
+		frameRectangle: self bounds
+		width: self borderWidth
+		color: self borderColor.
+	aCanvas paintImage: image at: self topLeft + self borderWidth
