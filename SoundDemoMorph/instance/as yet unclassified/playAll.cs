f33d93181dfb48328ba44e290068535e16e036e9
dgd <unknown>
2003-02-22 13:34:00 +0000

    no message

--- SoundDemoMorph/instance/as yet unclassified/playAll.cs
+++ SoundDemoMorph/instance/as yet unclassified/playAll.cs
@@ -1,16 +1,15 @@
 playAll
-
 	| snd |
-	soundColumn submorphs size < 1 ifTrue: [^ self].
-	self setTimbreFromTile: (soundColumn submorphs at: 1).
+	soundColumn submorphs isEmpty
+		ifTrue: [^ self].
+	self setTimbreFromTile: soundColumn submorphs first.
 	snd _ SampledSound bachFugueVoice1On: SampledSound new.
-
-	soundColumn submorphs size >= 2 ifTrue: [
-		self setTimbreFromTile: (soundColumn submorphs at: 2).
-		snd _ snd + (AbstractSound bachFugueVoice2On: SampledSound new)].
-
-	soundColumn submorphs size >= 3 ifTrue: [
-		self setTimbreFromTile: (soundColumn submorphs at: 3).
-		snd _ snd + (AbstractSound bachFugueVoice3On: SampledSound new)].
-
-	snd play.
+	soundColumn submorphs size >= 2
+		ifTrue: [""self setTimbreFromTile: soundColumn submorphs second.
+			snd _ snd
+						+ (AbstractSound bachFugueVoice2On: SampledSound new)].
+	soundColumn submorphs size >= 3
+		ifTrue: [""self setTimbreFromTile: soundColumn submorphs third.
+			snd _ snd
+						+ (AbstractSound bachFugueVoice3On: SampledSound new)].
+	snd play
