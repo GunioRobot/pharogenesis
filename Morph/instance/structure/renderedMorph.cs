ca4733530fb5a122f3282a59783a9b73d2af017a
dgd <unknown>
2003-02-22 14:34:00 +0000

    no message

--- Morph/instance/structure/renderedMorph.cs
+++ Morph/instance/structure/renderedMorph.cs
@@ -1,6 +1,6 @@
 renderedMorph
 	"If the receiver is a renderer morph, answer the rendered morph. Otherwise, answer the receiver. A renderer morph with no submorphs answers itself. See the comment in Morph>isRenderer."
 
-	self isRenderer ifFalse: [^ self].
-	submorphs size = 0 ifTrue: [^ self].
-	^ self firstSubmorph renderedMorph.
+	self isRenderer ifFalse: [^self].
+	submorphs isEmpty ifTrue: [^self].
+	^self firstSubmorph renderedMorph
