d23f4d812ea31ab55c4da7088130030108859992
dgd <unknown>
2003-02-22 13:26:00 +0000

    no message

--- PDA/instance/to do/declareItemDone.cs
+++ PDA/instance/to do/declareItemDone.cs
@@ -1,11 +1,13 @@
 declareItemDone
 	| report |
-	report _ FillInTheBlank request:
-'This item will be declared done as of
-' , date printString , '.
+	report := FillInTheBlank 
+				request: 'This item will be declared done as of
+' , date printString 
+						, '.
 Please give a short summary of status'
-		initialAnswer: 'Completed.'.
-	(report == nil or: [report isEmpty]) ifTrue: [^ self].
-	currentItem dayDone: date;
-			result: report.
+				initialAnswer: 'Completed.'.
+	(report isNil or: [report isEmpty]) ifTrue: [^self].
+	currentItem
+		dayDone: date;
+		result: report.
 	self currentItem: currentItem
