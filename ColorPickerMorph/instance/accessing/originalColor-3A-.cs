f3b107a0c27ea4edfaccd5e1ce144488a92ba34f
gm <unknown>
2003-02-22 13:12:00 +0000

    no message

--- ColorPickerMorph/instance/accessing/originalColor-3A-.cs
+++ ColorPickerMorph/instance/accessing/originalColor-3A-.cs
@@ -1,8 +1,10 @@
-originalColor: colorOrSymbol
+originalColor: colorOrSymbol 
 	"Set the receiver's original color.  It is at this point that a command is launched to represent the action of the picker, in support of Undo."
 
-	originalColor _ (colorOrSymbol isKindOf: Color)
-		ifTrue: [colorOrSymbol]
-		ifFalse: [Color lightGreen].
+	originalColor := (colorOrSymbol isColor) 
+				ifTrue: [colorOrSymbol]
+				ifFalse: [Color lightGreen].
 	originalForm fill: RevertBox fillColor: originalColor.
-	selectedColor _ originalColor.
+	selectedColor := originalColor.
+	self locationIndicator 
+		center: self topLeft + (self positionOfColor: originalColor)
