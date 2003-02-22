e319862f6d98788096a7d2e3d03f5350eb59d3c7
dgd <unknown>
2003-02-22 14:30:00 +0000

    no message

--- Morph/instance/object fileIn/convertNovember2000DropShadow-3A-using-3A-.cs
+++ Morph/instance/object fileIn/convertNovember2000DropShadow-3A-using-3A-.cs
@@ -1,22 +1,31 @@
-convertNovember2000DropShadow: varDict using: smartRefStrm
-	| rend |
-	"Work hard to eliminate the DropShadow.  Inst vars are already stored into."
+convertNovember2000DropShadow: varDict using: smartRefStrm 
+	"Work hard to eliminate the DropShadow. Inst vars are already  
+	stored into."
 
-	submorphs size > 0 ifTrue: [
-		rend _ submorphs first renderedMorph.	"a text?"
-		rend setProperty: #hasDropShadow toValue: true.
-		rend setProperty: #shadowColor toValue: (varDict at: 'color').
-		rend setProperty: #shadowOffset toValue: (varDict at: 'shadowOffset').
-		"ds owner ifNotNil: [ds owner addAllMorphs: ds submorphs].   ^rend does this"
-		rend privateOwner: owner.
-		extension ifNotNil: [
-			extension actorState ifNotNil: [
-				rend actorState: extension actorState].
-			extension externalName ifNotNil: [
-				rend setNameTo: extension externalName].
-			extension player ifNotNil: [
-				rend player: extension player.
-				extension player rawCostume: rend]].
-		^ rend].
-	(rend _ Morph new) color: Color transparent.
-	^ rend
+	| rend |
+	submorphs notEmpty 
+		ifTrue: 
+			[rend := submorphs first renderedMorph.
+			"a text?"
+			rend setProperty: #hasDropShadow toValue: true.
+			rend setProperty: #shadowColor toValue: (varDict at: 'color').
+			rend setProperty: #shadowOffset toValue: (varDict at: 'shadowOffset').
+			"ds owner ifNotNil: [ds owner addAllMorphs: ds  
+			submorphs]. ^rend does this"
+			rend privateOwner: owner.
+			self hasExtension 
+				ifTrue: 
+					[""
+
+					self extension actorState 
+						ifNotNil: [rend actorState: self extension actorState].
+					self extension externalName 
+						ifNotNil: [rend setNameTo: self extension externalName].
+					self extension player ifNotNil: 
+							[""
+
+							rend player: self extension player.
+							self extension player rawCostume: rend]].
+			^rend].
+	(rend := Morph new) color: Color transparent.
+	^rend
