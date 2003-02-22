ba7bf3049d91015967d2555fb74715a216053168
dgd <unknown>
2003-02-22 13:21:00 +0000

    no message

--- MoviePlayerMorph/instance/menu/addSoundTrack.cs
+++ MoviePlayerMorph/instance/menu/addSoundTrack.cs
@@ -1,16 +1,18 @@
 addSoundTrack
-
 	| fileName |
-	fileName _ Utilities chooseFileWithSuffixFromList: #('.aif' '.wav')
-					withCaption: 'Choose a sound track file'.
-	fileName == nil ifTrue: [^ self].
-	soundTrackFileName _ fileName.
+	fileName := Utilities chooseFileWithSuffixFromList: #('.aif' '.wav')
+				withCaption: 'Choose a sound track file'.
+	fileName isNil ifTrue: [^self].
+	soundTrackFileName := fileName.
 	self tryToShareScoreFor: soundTrackFileName.
-	scorePlayer ifNil:
-		[('*aif' match: fileName) ifTrue:
-			[scorePlayer _ SampledSound fromAIFFfileNamed: fileName].
-		('*wav' match: fileName) ifTrue:
-			[scorePlayer _ SampledSound fromWaveFileNamed: fileName]].
-	soundTrackForm ifNotNil:
-		["Compute new soundTrack if we're showing it."
-		self showHideSoundTrack; showHideSoundTrack]
+	scorePlayer ifNil: 
+			[('*aif' match: fileName) 
+				ifTrue: [scorePlayer := SampledSound fromAIFFfileNamed: fileName].
+			('*wav' match: fileName) 
+				ifTrue: [scorePlayer := SampledSound fromWaveFileNamed: fileName]].
+	soundTrackForm ifNotNil: 
+			["Compute new soundTrack if we're showing it."
+
+			self
+				showHideSoundTrack;
+				showHideSoundTrack]
