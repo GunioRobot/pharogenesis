ecdd3b70d7ef8f60e73f3f8e7c0b995b8da681f9
jm <unknown>
2003-02-25 16:27:00 +0000

    no message

--- SelectionMorph/instance/undo/undoMove-3A-redo-3A-owner-3A-bounds-3A-predecessor-3A-.cs
+++ SelectionMorph/instance/undo/undoMove-3A-redo-3A-owner-3A-bounds-3A-predecessor-3A-.cs
@@ -1,20 +1,25 @@
-undoMove: cmd redo: redo owner: oldOwners bounds: oldBoundses predecessor: oldPredecessors
-	| item |
+undoMove: cmd redo: redo owner: oldOwners bounds: oldBoundses predecessor: oldPredecessors 
 	"Handle undo and redo of move commands in morphic"
-	redo ifFalse:
-		["undo sets up the redo state first"
-		cmd redoTarget: self
-			selector: #undoMove:redo:owner:bounds:predecessor:
-			arguments: {cmd. true.
+
+	| item |
+	redo 
+		ifFalse: 
+			["undo sets up the redo state first"
+
+			cmd 
+				redoTarget: self
+				selector: #undoMove:redo:owner:bounds:predecessor:
+				arguments: { 
+						cmd.
+						true.
 						selectedItems collect: [:m | m owner].
 						selectedItems collect: [:m | m bounds].
 						selectedItems collect: [:m | m owner morphPreceding: m]}].
-	(redo ifTrue: [1 to: selectedItems size]
-			ifFalse: [1 to: selectedItems size]) do:
-		[:i | item _ selectedItems at: i.
-		(oldOwners at: i) ifNotNil:
-			[(oldPredecessors at: i)
-				ifNil: [(oldOwners at: i) addMorphFront: item]
-				ifNotNil: [(oldOwners at: i) addMorph: item after: (oldPredecessors at: i)]].
-		item bounds: (oldBoundses at: i).
-		(item isKindOf: SystemWindow) ifTrue: [item activate]]
+	1 to: selectedItems size do: 
+				[:i | 
+				item := selectedItems at: i.
+				(oldOwners at: i) ifNotNil: 
+						[(oldPredecessors at: i) ifNil: [(oldOwners at: i) addMorphFront: item]
+							ifNotNil: [(oldOwners at: i) addMorph: item after: (oldPredecessors at: i)]].
+				item bounds: (oldBoundses at: i).
+				item isSystemWindow ifTrue: [item activate]]
