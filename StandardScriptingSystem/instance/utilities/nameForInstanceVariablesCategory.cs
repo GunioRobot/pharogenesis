d8d3f3ab75c2b2c5ddd6c2e71f6e16a1173a41b5
sw <unknown>
2003-02-26 22:44:00 +0000

    no message

---
+++ StandardScriptingSystem/instance/utilities/nameForInstanceVariablesCategory.cs
@@ -0,0 +1,7 @@
+nameForInstanceVariablesCategory
+	"Answer the name to use for the viewer category that contains instance variables"
+
+	^ #variables    
+	"^ #'instance variables'"
+
+"ScriptingSystem nameForInstanceVariablesCategory"
