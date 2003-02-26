9af3483f08faf20f8615281e551d2bb4ce126c77
tpr <unknown>
2003-02-26 14:05:00 +0000

    no message

--- Interpreter/instance/processes/internalQuickCheckForInterrupts.cs
+++ Interpreter/instance/processes/internalQuickCheckForInterrupts.cs
@@ -6,8 +6,6 @@ internalQuickCheckForInterrupts
 		self externalizeIPandSP.
 		self checkForInterrupts.
 
-		"When Squeak runs as a browser plugin, it must return control to the browser periodically. This should done only when the state of the currently running Squeak thread is safely stored in the object heap, as is the case at this point. Since this method is inlined, the message 'ReturnFromInterpret' is actually a C macro that returns control from the interpret() method to the browser that called it."
-		GenerateBrowserPlugin ifTrue: [
-			self plugInTimeToReturn ifTrue: [self ReturnFromInterpret]].
+		self browserPluginReturnIfNeeded.
 
 		self internalizeIPandSP].
