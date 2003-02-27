b0d7bf0a2bddd274a9b57fae3bbc669355a9cb4f
nk <unknown>
2003-02-27 11:48:00 +0000

    no message

--- Morph/instance/visual properties/useGradientFill.cs
+++ Morph/instance/visual properties/useGradientFill.cs
@@ -2,11 +2,11 @@ useGradientFill
 	"Make receiver use a solid fill style (e.g., a simple color)"
 	| fill color1 color2 |
 	self fillStyle isGradientFill ifTrue:[^self]. "Already done"
-	color1 _ self color dominantColor.
+	color1 _ self color asColor.
 	color2 _ color1 negated.
 	fill _ GradientFillStyle ramp: {0.0 -> color1. 1.0 -> color2}.
 	fill origin: self topLeft.
 	fill direction: 0 @ self bounds extent y.
 	fill normal: self bounds extent x @ 0.
 	fill radial: false.
-	self fillStyle: fill.
+	self fillStyle: fill
