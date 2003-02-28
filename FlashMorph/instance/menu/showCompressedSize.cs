3062df13d85a09029949f225ed3727273a054f92
gm <unknown>
2003-02-28 00:16:00 +0000

    no message

--- FlashMorph/instance/menu/showCompressedSize.cs
+++ FlashMorph/instance/menu/showCompressedSize.cs
@@ -1,8 +1,9 @@
 showCompressedSize
 	| size string |
-	size _ self originalFileSize.
-	size = 0 
-		ifTrue:[string _ 'Compressed size: not available']
-		ifFalse:[string _ 'Compressed size: ', size asStringWithCommas, ' bytes'].
-	self world primaryHand attachMorph:
-		(TextMorph new contents: string; beAllFont: ScriptingSystem fontForTiles).
+	size := self originalFileSize.
+	string := size = 0 
+		ifTrue: ['Compressed size: not available']
+		ifFalse: ['Compressed size: ' , size asStringWithCommas , ' bytes'].
+	self world primaryHand attachMorph: ((TextMorph new)
+				contents: string;
+				beAllFont: ScriptingSystem fontForTiles)
