7efb2559e9da81898c440206f31955a5987f03dd
gm <unknown>
2003-02-28 01:43:00 +0000

    no message

--- TableLayout/instance/layout/minExtentOf-3A-in-3A-.cs
+++ TableLayout/instance/layout/minExtentOf-3A-in-3A-.cs
@@ -1,37 +1,38 @@
-minExtentOf: aMorph in: box
+minExtentOf: aMorph in: box 
 	"Return the minimal size aMorph's children would require given the new bounds"
-	| cells arrangement horizontal newBounds minX minY dir |
-	minExtentCache == nil ifFalse:[^minExtentCache].
-	aMorph hasSubmorphs ifFalse:[^0@0].
-	properties _ aMorph assureTableProperties.
 
-	(properties wrapDirection == #none and:[properties cellSpacing == #none]) ifTrue:[
-		"Get into the fast lane"
-		dir _ properties listDirection.
-		(dir == #leftToRight or:[dir == #rightToLeft])
-			ifTrue:[^self minExtentHorizontal: aMorph].
-		(dir == #topToBottom or:[dir == #bottomToTop])
-			ifTrue:[^self minExtentVertical: aMorph].
-	].
-	newBounds _ box origin asIntegerPoint corner: (box corner asIntegerPoint).
-	(properties listDirection == #topToBottom or:[properties listDirection == #bottomToTop])
-		ifTrue:[	horizontal _ false]
-		ifFalse:[	horizontal _ true].
+	| cells arrangement horizontal newBounds minX minY dir |
+	minExtentCache isNil ifFalse: [^minExtentCache].
+	aMorph hasSubmorphs ifFalse: [^0 @ 0].
+	properties := aMorph assureTableProperties.
+	(properties wrapDirection == #none and: [properties cellSpacing == #none]) 
+		ifTrue: 
+			["Get into the fast lane"
+
+			dir := properties listDirection.
+			(dir == #leftToRight or: [dir == #rightToLeft]) 
+				ifTrue: [^self minExtentHorizontal: aMorph].
+			(dir == #topToBottom or: [dir == #bottomToTop]) 
+				ifTrue: [^self minExtentVertical: aMorph]].
+	newBounds := box origin asIntegerPoint corner: box corner asIntegerPoint.
+	horizontal := (properties listDirection == #topToBottom 
+				or: [properties listDirection == #bottomToTop]) not.
 	"Step 1: Compute the minimum extent for all the children of aMorph"
-	cells _ self computeCellSizes: aMorph 
-				in: (0@0 corner: newBounds extent) 
+	cells := self 
+				computeCellSizes: aMorph
+				in: (0 @ 0 corner: newBounds extent)
 				horizontal: horizontal.
 	"Step 2: Compute the arrangement of the cells for each row and column"
-	arrangement _ self computeCellArrangement: cells 
-						in: newBounds 
-						horizontal: horizontal
-						target: aMorph.
+	arrangement := self 
+				computeCellArrangement: cells
+				in: newBounds
+				horizontal: horizontal
+				target: aMorph.
 	"Step 3: Extract the minimum size out of the arrangement"
-	minX _ minY _ 0.
-	arrangement do:[:cell|
-		minX _ minX max: cell cellSize x + cell extraSpace x.
-		minY _ minY + cell cellSize y + cell extraSpace y].
-	horizontal 
-		ifTrue:[minExtentCache _ minX@minY]
-		ifFalse:[minExtentCache _ minY@minX].
+	minX := minY := 0.
+	arrangement do: 
+			[:cell | 
+			minX := minX max: cell cellSize x + cell extraSpace x.
+			minY := minY + cell cellSize y + cell extraSpace y].
+	minExtentCache := horizontal ifTrue: [minX @ minY] ifFalse: [minY @ minX].
 	^minExtentCache
