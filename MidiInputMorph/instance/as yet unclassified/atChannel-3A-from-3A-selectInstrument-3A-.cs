feac633ed593eeb3704f832fbe49e1914a56751b
gm <unknown>
2003-02-28 00:00:00 +0000

    no message

--- MidiInputMorph/instance/as yet unclassified/atChannel-3A-from-3A-selectInstrument-3A-.cs
+++ MidiInputMorph/instance/as yet unclassified/atChannel-3A-from-3A-selectInstrument-3A-.cs
@@ -1,30 +1,27 @@
-atChannel: channelIndex from: aPopUpChoice selectInstrument: selection
-
+atChannel: channelIndex from: aPopUpChoice selectInstrument: selection 
 	| oldSnd name snd instSelector |
-	oldSnd _ midiSynth instrumentForChannel: channelIndex.
-	(selection beginsWith: 'edit ') ifTrue: [
-		name _ selection copyFrom: 6 to: selection size.
-		aPopUpChoice contentsClipped: name.
-		(oldSnd isKindOf: FMSound) | (oldSnd isKindOf: LoopedSampledSound) ifTrue: [
-			EnvelopeEditorMorph openOn: oldSnd title: name].
-		(oldSnd isKindOf: SampledInstrument) ifTrue: [
-			EnvelopeEditorMorph openOn: oldSnd allNotes first title: name].
-		^ self].
-
-	snd _ nil.
-	1 to: instrumentSelector size do: [:i |
-		((channelIndex ~= i) and:
-		 [(instSelector _ instrumentSelector at: i) notNil and:
-		 [selection = instSelector contents]])
-			ifTrue: [snd _ midiSynth instrumentForChannel: i]].  "use existing instrument prototype"
-
-	snd ifNil: [
-		selection = 'clink'
-			ifTrue: [
-				snd _ (SampledSound
-					samples: SampledSound coffeeCupClink
-					samplingRate: 11025) copy]
-			ifFalse: [snd _ (AbstractSound soundNamed: selection) copy]].
-
+	oldSnd := midiSynth instrumentForChannel: channelIndex.
+	(selection beginsWith: 'edit ') 
+		ifTrue: 
+			[name := selection copyFrom: 6 to: selection size.
+			aPopUpChoice contentsClipped: name.
+			(oldSnd isKindOf: FMSound) | (oldSnd isKindOf: LoopedSampledSound) 
+				ifTrue: [EnvelopeEditorMorph openOn: oldSnd title: name].
+			(oldSnd isKindOf: SampledInstrument) 
+				ifTrue: [EnvelopeEditorMorph openOn: oldSnd allNotes first title: name].
+			^self].
+	snd := nil.
+	1 to: instrumentSelector size
+		do: 
+			[:i | 
+			(channelIndex ~= i and: 
+					[(instSelector := instrumentSelector at: i) notNil 
+						and: [selection = instSelector contents]]) 
+				ifTrue: [snd := midiSynth instrumentForChannel: i]].	"use existing instrument prototype"
+	snd ifNil: 
+			[snd := (selection = 'clink' 
+						ifTrue: 
+							[(SampledSound samples: SampledSound coffeeCupClink samplingRate: 11025)]
+						ifFalse: [(AbstractSound soundNamed: selection) ])copy ].
 	midiSynth instrumentForChannel: channelIndex put: snd.
-	(instrumentSelector at: channelIndex) contentsClipped: selection.
+	(instrumentSelector at: channelIndex) contentsClipped: selection
