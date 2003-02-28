a2e237b35288890016998e662d2bebc92ba2406f
gm <unknown>
2003-02-28 01:42:00 +0000

    no message

--- TheWorldMenu/instance/construction/alphabeticalMorphMenu.cs
+++ TheWorldMenu/instance/construction/alphabeticalMorphMenu.cs
@@ -1,22 +1,27 @@
 alphabeticalMorphMenu
-
 	| list splitLists menu firstChar lastChar subMenu |
-	list _ Morph withAllSubclasses select:[:m | m includeInNewMorphMenu].
-	list _ list asArray sortBy:[:c1 :c2| c1 name < c2 name].
-	splitLists _ self splitNewMorphList: list depth: 3.
-	menu _ MenuMorph new defaultTarget: self.
-	1 to: splitLists size do:[:i|
-		i = 1 ifTrue:[firstChar _ $A]
-			ifFalse:[firstChar _ ((splitLists at: i-1) last name first asInteger + 1) asCharacter].
-		i = splitLists size
-			ifTrue:[lastChar _ $Z]
-			ifFalse:[lastChar _ (splitLists at: i) last name first].
-		subMenu _ MenuMorph new.
-		(splitLists at: i) do:[:cl|
-			subMenu add: cl name
-					target: self
-					selector: #newMorphOfClass:event:
-					argument: cl].
-		menu add: firstChar asString,' - ', lastChar asString subMenu: subMenu.
-	].
+	list := Morph withAllSubclasses select: [:m | m includeInNewMorphMenu].
+	list := list asArray sortBy: [:c1 :c2 | c1 name < c2 name].
+	splitLists := self splitNewMorphList: list depth: 3.
+	menu := MenuMorph new defaultTarget: self.
+	1 to: splitLists size
+		do: 
+			[:i | 
+			firstChar := i = 1 
+				ifTrue: [$A]
+				ifFalse: 
+					[((splitLists at: i - 1) last name first asInteger + 1) 
+								asCharacter].
+			lastChar := i = splitLists size 
+						ifTrue: [$Z]
+						ifFalse: [(splitLists at: i) last name first].
+			subMenu := MenuMorph new.
+			(splitLists at: i) do: 
+					[:cl | 
+					subMenu 
+						add: cl name
+						target: self
+						selector: #newMorphOfClass:event:
+						argument: cl].
+			menu add: firstChar asString , ' - ' , lastChar asString subMenu: subMenu].
 	^menu
