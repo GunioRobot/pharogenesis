5578fabde17d22d363e2489886922a350838fa7a
gm <unknown>
2003-03-02 18:35:00 +0000

    no message

--- Component/instance/variables/addVariableNamed-3A-.cs
+++ Component/instance/variables/addVariableNamed-3A-.cs
@@ -1,25 +1,42 @@
-addVariableNamed: varName
-	| otherNames i partName |
+addVariableNamed: varName 
 	"Adjust name if necessary and add it"
-	otherNames _ self class allInstVarNames.
-	i _ nil.
-	[i == nil ifTrue: [partName _ varName] ifFalse: [partName _ varName, i printString].
-	otherNames includes: partName]
-		whileTrue: [i == nil ifTrue: [i _ 1] ifFalse: [i _ i + 1]].
+
+	| otherNames i partName |
+	otherNames := self class allInstVarNames.
+	i := nil.
+	
+	[partName := i isNil 
+		ifTrue: [varName]
+		ifFalse: [varName , i printString].
+	otherNames includes: partName] 
+			whileTrue: [i := i isNil ifTrue: [1] ifFalse: [i + 1]].
 	self class addInstVarName: partName.
 
 	"Now compile read method and write-with-change method"
-	self class compile: (String streamContents:
-			[:s | s nextPutAll: partName; cr;
-			tab; nextPutAll: '^', partName])
+	self class 
+		compile: (String streamContents: 
+					[:s | 
+					s
+						nextPutAll: partName;
+						cr;
+						tab;
+						nextPutAll: '^' , partName])
 		classified: 'view access'
 		notifying: nil.
-	self class compile: (String streamContents:
-			[:s | s nextPutAll: partName, 'Set: newValue'; cr;
-				tab; nextPutAll: partName, ' _ newValue.'; cr;
-				tab; nextPutAll: 'self changed: #', partName, '.'; cr;
-				tab; nextPutAll: '^ true' "for components that expect a boolean for accept"])
+	self class 
+		compile: (String streamContents: 
+					[:s | 
+					s
+						nextPutAll: partName , 'Set: newValue';
+						cr;
+						tab;
+						nextPutAll: partName , ' _ newValue.';
+						cr;
+						tab;
+						nextPutAll: 'self changed: #' , partName , '.';
+						cr;
+						tab;
+						nextPutAll: '^ true'	"for components that expect a boolean for accept"])
 		classified: 'view access'
 		notifying: nil.
-
-	^ Array with: partName asSymbol with: (partName , 'Set:') asSymbol
+	^Array with: partName asSymbol with: (partName , 'Set:') asSymbol
