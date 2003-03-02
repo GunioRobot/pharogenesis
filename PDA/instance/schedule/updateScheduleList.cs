ddb3a6032c5cd49ca5943c1d8ec9184f0c862f18
gm <unknown>
2003-03-02 18:26:00 +0000

    no message

--- PDA/instance/schedule/updateScheduleList.cs
+++ PDA/instance/schedule/updateScheduleList.cs
@@ -1,15 +1,19 @@
 updateScheduleList
-
-	(date == nil and: [category ~= 'recurring']) ifTrue:
-		[scheduleList _ Array new.  scheduleListIndex _ 0.
-		^ self changed: #scheduleListItems].
-	category = 'recurring'
-	ifTrue: ["When 'recurring' is selected, edit actual masters"
-			scheduleList _ (recurringEvents select:
-				[:c | c matchesKey: category andMatchesDate: date]) sort]
-	ifFalse: ["Otherwise, recurring events just spawn copies."
-			scheduleList _ ((allEvents select: [:c | c matchesKey: category andMatchesDate: date])
-			, ((recurringEvents select: [:c | c matchesKey: category andMatchesDate: date])
-					collect: [:re | (re as: PDAEvent) date: date])) sort].
+	(date isNil
+			and: [category ~= 'recurring'])
+		ifTrue: [scheduleList _ Array new.
+			scheduleListIndex _ 0.
+			^ self changed: #scheduleListItems].
+	scheduleList _ (category = 'recurring'
+				ifTrue: ["When 'recurring' is selected, edit actual masters"
+					(recurringEvents
+						select: [:c | c matchesKey: category andMatchesDate: date]) ]
+				ifFalse: ["Otherwise, recurring events just spawn copies."
+					((allEvents
+						select: [:c | c matchesKey: category andMatchesDate: date])
+						, ((recurringEvents
+								select: [:c | c matchesKey: category andMatchesDate: date])
+								collect: [:re | (re as: PDAEvent)
+										date: date])) ])sort.
 	scheduleListIndex _ scheduleList indexOf: currentItem.
 	self changed: #scheduleListItems
