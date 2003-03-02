e014faba509bce97f20720992d243c07968f44df
ls <unknown>
2003-03-02 11:34:00 +0000

    no message

--- CelesteComposition/instance/interface/addAttachment.cs
+++ CelesteComposition/instance/interface/addAttachment.cs
@@ -2,12 +2,14 @@ addAttachment
 	| file fileResult fileName |
 	textEditor
 		ifNotNil: [self hasUnacceptedEdits ifTrue: [textEditor accept]].
-	"transform into multipart if needed"
-	self hasAttachments ifFalse: [self transformToMultipart].
-	"then simply append another attachment section"
-	(fileResult _ StandardFileMenu oldFile)
+
+	(fileResult _ FileList2 modalFileSelector)
 		ifNotNil: 
 			[fileName _ fileResult directory fullNameFor: fileResult name.
 			file _ FileStream readOnlyFileNamed: fileName.
-			file ifNotNil: [self messageText: ((MailMessage from: self messageText)
-						asTextEncodingNewPart: file named: fileResult name)]] 
+			file ifNotNil:
+				[file binary.
+				self messageText:
+						((MailMessage from: self messageText asString)
+							addAttachmentFrom: file withName: fileResult name; text).
+				file close]] 
