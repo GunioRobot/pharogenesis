cfdd79b50976bc95264a549a3c289df7b640b164
dgd <unknown>
2003-03-07 14:34:00 +0000

    no message

--- HeadingMorph/instance/initialization/initialize.cs
+++ HeadingMorph/instance/initialization/initialize.cs
@@ -1,8 +1,8 @@
 initialize
-
+	"initialize the state of the receiver"
 	super initialize.
-	self color: (Color r:0.6 g: 1.0 b: 1.0).
+	""
 	degrees _ 90.0.
 	magnitude _ 1.0.
-	self borderWidth: 1.
-	self extent: 160@160.
+	
+	self extent: 160 @ 160
