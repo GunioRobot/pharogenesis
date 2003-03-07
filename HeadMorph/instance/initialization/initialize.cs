d2d75bb6d81d15137910fd5d6b770bb6491e3a27
dgd <unknown>
2003-03-07 14:32:00 +0000

    no message

--- HeadMorph/instance/initialization/initialize.cs
+++ HeadMorph/instance/initialization/initialize.cs
@@ -1,6 +1,8 @@
 initialize
+	"initialize the state of the receiver"
 	super initialize.
-	self color: self randomSkinColor.
+	""
+
 	self face: FaceMorph new.
 	self extent: self face extent * (1.5 @ 1.7).
 	self face align: self face center with: self center + (0 @ self height // 10).
