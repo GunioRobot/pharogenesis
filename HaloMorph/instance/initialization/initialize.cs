bebc60040eed84f7f7e8cea8b01fb930d9b1f704
dgd <unknown>
2003-03-07 14:29:00 +0000

    no message

--- HaloMorph/instance/initialization/initialize.cs
+++ HaloMorph/instance/initialization/initialize.cs
@@ -1,6 +1,6 @@
 initialize
-
+	"initialize the state of the receiver"
 	super initialize.
-	self color: (Color r: 0.6 g: 0.8 b: 1.0).
+	""
 	growingOrRotating _ false.
-	simpleMode _ Preferences  simpleHalosInForce
+	simpleMode _ Preferences simpleHalosInForce 
