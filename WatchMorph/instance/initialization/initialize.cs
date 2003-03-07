416615f128824e7b44a324c9d69e95d11722b92d
dgd <unknown>
2003-03-07 15:01:00 +0000

    no message

--- WatchMorph/instance/initialization/initialize.cs
+++ WatchMorph/instance/initialization/initialize.cs
@@ -1,10 +1,12 @@
-initialize  "Establish all default parameters..."
+initialize
+	"initialize the state of the receiver"
 	super initialize.
-	self color: Color green.
+	""
+
 	self handsColor: Color red.
 	self centerColor: Color gray.
 	romanNumerals _ false.
 	antialias _ false.
 	fontName _ 'NewYork'.
-	self extent: 130@130.
-	self start.
+	self extent: 130 @ 130.
+	self start
