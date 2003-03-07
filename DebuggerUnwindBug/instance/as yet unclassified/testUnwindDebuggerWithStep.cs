554bf61774394cb649aa7813d3c734a8c0b7001a
ar <unknown>
2003-03-07 01:40:00 +0000

    no message

---
+++ DebuggerUnwindBug/instance/as yet unclassified/testUnwindDebuggerWithStep.cs
@@ -0,0 +1,22 @@
+testUnwindDebuggerWithStep
+	"test if unwind blocks work properly when a debugger is closed"
+	| sema process debugger top |
+	sema := Semaphore forMutualExclusion.
+	self assert: sema isSignaled.
+	process := [sema critical:[sema wait]] forkAt: Processor userInterruptPriority.
+	self deny: sema isSignaled.
+
+	"everything set up here - open a debug notifier"
+	debugger := Debugger openInterrupt: 'test' onProcess: process.
+	"get into the debugger"
+	debugger debug.
+	top := debugger topView.
+	"set top context"
+	debugger toggleContextStackIndex: 1.
+	"do single step"
+	debugger doStep.
+	"close debugger"
+	top delete.
+
+	"and see if unwind protection worked"
+	self assert: sema isSignaled.
