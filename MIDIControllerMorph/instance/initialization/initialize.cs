3c13ba1eb44032a051925f8b21742be50215564f
dgd <unknown>
2003-03-07 14:37:00 +0000

    no message

--- MIDIControllerMorph/instance/initialization/initialize.cs
+++ MIDIControllerMorph/instance/initialization/initialize.cs
@@ -1,22 +1,22 @@
 initialize
-
+"initialize the state of the receiver"
 	| slider |
 	super initialize.
+""
 	self listDirection: #topToBottom.
-	self wrapCentering: #center; cellPositioning: #topCenter.
+	self wrapCentering: #center;
+		 cellPositioning: #topCenter.
 	self hResizing: #shrinkWrap.
 	self vResizing: #shrinkWrap.
-	self color: (Color r: 0.484 g: 0.613 b: 0.0).
-	self borderWidth: 1.
 	channel _ 0.
-	controller _ 7.  "channel volume"
-
-	slider _ SimpleSliderMorph new
-		target: self;
-		actionSelector: #newSliderValue:;
-		minVal: 0;
-		maxVal: 127;
-		extent: 128@10.
+	controller _ 7.
+	"channel volume"
+	slider _ SimpleSliderMorph new target: self;
+				 actionSelector: #newSliderValue:;
+				 minVal: 0;
+				 maxVal: 127;
+				 extent: 128 @ 10.
 	self addMorphBack: slider.
-	self addMorphBack: (StringMorph contents: 'Midi Controller').
-	self updateLabel.
+	self
+		addMorphBack: (StringMorph contents: 'Midi Controller').
+	self updateLabel
