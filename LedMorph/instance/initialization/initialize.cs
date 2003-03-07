847b78adfcbee3aa6f79aba4171789afba39c1da
dgd <unknown>
2003-03-07 14:36:00 +0000

    no message

--- LedMorph/instance/initialization/initialize.cs
+++ LedMorph/instance/initialization/initialize.cs
@@ -1,8 +1,9 @@
 initialize
-
+"initialize the state of the receiver"
 	super initialize.
+""
 	flashing _ false.
 	flash _ false.
+	self scrollInit.
 	self digits: 2.
-	self value: 0.
-	self color: Color green.
+	self value: 0
