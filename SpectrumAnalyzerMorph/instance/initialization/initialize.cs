0eedebd3a17103a8f510447c259224c146f51947
dgd <unknown>
2003-03-07 14:52:00 +0000

    no message

--- SpectrumAnalyzerMorph/instance/initialization/initialize.cs
+++ SpectrumAnalyzerMorph/instance/initialization/initialize.cs
@@ -1,14 +1,17 @@
 initialize
-
+"initialize the state of the receiver"
 	super initialize.
-	borderWidth _ 2.
+""
 	self listDirection: #topToBottom.
 	soundInput _ SoundInputStream new samplingRate: 22050.
 	fft _ FFT new: 512.
 	displayType _ 'sonogram'.
-
+	self hResizing: #shrinkWrap.
+	self vResizing: #shrinkWrap.
 	self addButtonRow.
 	self addLevelSlider.
 	self addMorphBack: self makeLevelMeter.
-	self addMorphBack: (Morph new extent: 10@10; color: Color transparent).  "spacer"
-	self resetDisplay.  "adds the display morph"
+	self addMorphBack: (Morph new extent: 10 @ 10;
+			 color: Color transparent).
+	"spacer"
+	self resetDisplay
