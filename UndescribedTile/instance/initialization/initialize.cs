e140b7b592e4e03deab1cd868e1af0eee229461e
dgd <unknown>
2003-03-07 15:45:00 +0000

    no message

--- UndescribedTile/instance/initialization/initialize.cs
+++ UndescribedTile/instance/initialization/initialize.cs
@@ -1,3 +1,7 @@
 initialize
+	"Initialize the receiver. This is in its infancy -- just a place-holder 
+	at present"
 	super initialize.
-	self extent: (20 @ 22)
+""
+	self extent: 20 @ 22.
+	self setLiteral: 'arg'
