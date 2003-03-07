189c3747489cd1c3baed5388100e208f70cde7b1
dgd <unknown>
2003-03-07 14:28:00 +0000

    no message

--- GraphMorph/instance/initialization/initialize.cs
+++ GraphMorph/instance/initialization/initialize.cs
@@ -1,15 +1,16 @@
 initialize
-
+	"initialize the state of the receiver"
 	super initialize.
-	self color: (Color r: 0.8 g: 0.8 b: 0.6).
-	self extent: 365@80.
-	self borderWidth: 2.
+	""
+	self extent: 365 @ 80.
+
 	dataColor _ Color darkGray.
-	cursor _ 1.0.  "may be fractional"
+	cursor _ 1.0.
+	"may be fractional"
 	cursorColor _ Color red.
 	cursorColorAtZeroCrossings _ Color red.
 	startIndex _ 1.
 	hasChanged _ false.
-	self data:
-		((0 to: 360 - 1) collect:
-			[:x | (100.0 * (x degreesToRadians sin)) asInteger]).
+	self
+		data: ((0 to: 360 - 1)
+				collect: [:x | (100.0 * x degreesToRadians sin) asInteger])
