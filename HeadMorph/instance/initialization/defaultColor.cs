3f24fa8244f579dd506ed7dac6bdcd37fd67632f
dgd <unknown>
2003-03-07 14:32:00 +0000

    no message

---
+++ HeadMorph/instance/initialization/defaultColor.cs
@@ -0,0 +1,19 @@
+defaultColor
+	"answer the default color/fill style for the receiver"
+
+	^ {Color
+		r: 0.258
+		g: 0.161
+		b: 0.0. Color
+		r: 0.452
+		g: 0.258
+		b: 0.0. Color
+		r: 0.516
+		g: 0.323
+		b: 0.0. Color
+		r: 1.0
+		g: 0.935
+		b: 0.645. Color
+		r: 1.0
+		g: 0.806
+		b: 0.548} atRandom
