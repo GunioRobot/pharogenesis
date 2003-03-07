a0e4463c433756bcdc2eb1efc35f226d6f0355ef
dgd <unknown>
2003-03-07 15:44:00 +0000

    no message

--- TileMorph/instance/initialization/initialize.cs
+++ TileMorph/instance/initialization/initialize.cs
@@ -1,17 +1,22 @@
 initialize
-
+	"initialize the state of the receiver"
 	super initialize.
-	self extent: 1@1.
-	self typeColor: (Color r: 0.8 g: 1.0 b: 0.6).
-	self borderWidth: 1.
-	type _ #literal.  "#literal, #slotRef, #objRef, #operator, #expression"
+	""
+	self extent: 1 @ 1.
+	self
+		typeColor: (Color
+				r: 0.8
+				g: 1.0
+				b: 0.6).
+
+	type _ #literal.
+	"#literal, #slotRef, #objRef, #operator, #expression"
 	slotName _ ''.
 	literal _ 1.
-
 	self layoutPolicy: TableLayout new.
-	self cellInset: 2@0.
-	self layoutInset: 1@0.
+	self cellInset: 2 @ 0.
+	self layoutInset: 1 @ 0.
 	self listDirection: #leftToRight.
 	self wrapCentering: #center.
 	self hResizing: #shrinkWrap.
-	self vResizing: #spaceFill.
+	self vResizing: #spaceFill
