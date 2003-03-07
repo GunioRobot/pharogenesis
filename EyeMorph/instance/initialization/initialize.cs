9e3e15e0ebcd4394da287cbd8a2e228ed0ede8b6
dgd <unknown>
2003-03-07 14:25:00 +0000

    no message

--- EyeMorph/instance/initialization/initialize.cs
+++ EyeMorph/instance/initialization/initialize.cs
@@ -1,7 +1,10 @@
 initialize
+	"initialize the state of the receiver"
 	super initialize.
-	self color: (Color r: 1.0 g: 0.968 b: 0.935).
-	self borderColor: Color black; borderWidth: 1.
+	""
+	
 	self extent: 30 @ 37.
-	self addMorphFront: (iris _ EllipseMorph new extent: 6 @ 6; borderWidth: 0; color: Color black).
+	self addMorphFront: (iris _ EllipseMorph new extent: 6 @ 6;
+					 borderWidth: 0;
+					 color: Color black).
 	self lookAtFront
