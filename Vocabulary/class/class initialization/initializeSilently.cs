6eb9cdf1c7248fc2ac35a363b8b67b0a585001de
sw <unknown>
2003-03-11 12:36:00 +0000

    no message

---
+++ Vocabulary/class/class initialization/initializeSilently.cs
@@ -0,0 +1,8 @@
+initializeSilently
+	"Initialize a few standard vocabularies and place them in the AllVocabularies list."
+
+	self initializeStandardVocabularies.
+	self embraceAddedTypeVocabularies.
+
+	"Vocabulary initializeSilently"
+
