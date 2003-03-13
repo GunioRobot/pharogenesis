83ae42d5798f865a51aa3e628157ed61ded57ecb
asm <unknown>
2003-03-13 10:25:00 +0000

    no message

---
+++ Flaps/class/predefined flaps/quadsDefiningStackToolsFlap.cs
@@ -0,0 +1,6 @@
+quadsDefiningStackToolsFlap
+	"Answer a structure defining the items on the default system Stack Tools flap"
+
+	^ self registeredFlapsQuadsAt: 'Stack Tools'
+
+	"Flaps replaceGlobalFlapwithID: 'Stack Tools'"
