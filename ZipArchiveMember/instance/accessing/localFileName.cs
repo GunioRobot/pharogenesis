1e11bae06c9d72a827be7e1d5b3404133adf4e4b
nk <unknown>
2003-03-13 09:23:00 +0000

    no message

---
+++ ZipArchiveMember/instance/accessing/localFileName.cs
@@ -0,0 +1,7 @@
+localFileName
+	"Answer my fileName in terms of the local directory naming convention"
+	| localName |
+	localName _ fileName copyReplaceAll: '/' with: FileDirectory slash.
+	^(fileName first = $/)
+		ifTrue: [ FileDirectory default class makeAbsolute: localName ]
+		ifFalse: [ FileDirectory default class makeRelative: localName ]
