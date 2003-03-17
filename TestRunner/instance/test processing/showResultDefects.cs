f2a4cc593d0ce1f98101a6705364d877d2f1dfb2
jp <unknown>
2003-03-17 14:00:00 +0000

    no message

--- TestRunner/instance/test processing/showResultDefects.cs
+++ TestRunner/instance/test processing/showResultDefects.cs
@@ -2,11 +2,11 @@ showResultDefects
 
 	(self result failureCount > 0)
 		ifTrue: [
-			Transcript cr; show: '---- SUnit ----- Failures ----'.
+			self errorLog cr; show: '---- SUnit ----- Failures ----'.
 			self result failures do: [:failure |
-				Transcript crtab; show: failure printString]].
+				self errorLog crtab; show: failure printString]].
 	(self result errorCount > 0)
 		ifTrue: [
-			Transcript cr; show: '---- SUnit ------- Errors ----'.
+			self errorLog cr; show: '---- SUnit ------- Errors ----'.
 			self result errors do: [:error |
-				Transcript crtab; show: error printString]].
+				self errorLog crtab; show: error printString]].
