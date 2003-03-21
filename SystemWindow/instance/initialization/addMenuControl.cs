9f403503aa49e7ba9aa033f755c8860977ec6123
dgd <unknown>
2003-03-21 18:52:00 +0000

    no message

--- SystemWindow/instance/initialization/addMenuControl.cs
+++ SystemWindow/instance/initialization/addMenuControl.cs
@@ -1,13 +1,16 @@
 addMenuControl
-"NB: for the moment, we always supply balloon help for this control, until people get used to it; eventually, we mays switch to showing this balloon help only in novice mode, as we do for the other standard window controls."
+	"If I have a label area, add a menu control to it."
 	| frame |
-	menuBox _ IconicButton new borderWidth: 0;
-			labelGraphic: (ScriptingSystem formAtKey: 'TinyMenu'); color: Color transparent; 
-			actWhen: #buttonDown;
-			actionSelector: #offerWindowMenu; target: self;
-			setBalloonText: 'window menu'.
-	frame _ LayoutFrame new.
-	frame leftFraction: 0; leftOffset: 19; topFraction: 0; topOffset: 1.
+	labelArea
+		ifNil: [^ self].
+	"No menu if no label area"
+	menuBox
+		ifNotNil: [menuBox delete].
+	menuBox := self createMenuBox.
+	frame := LayoutFrame new.
+	frame leftFraction: 0;
+		 leftOffset: closeBox right + 3;
+		 topFraction: 0;
+		 topOffset: 0.
 	menuBox layoutFrame: frame.
-	labelArea addMorph: menuBox.
-
+	labelArea addMorph: menuBox
