58c801c2a7b6f227afa249b2c3fa9e810d5a6cf9
dgd <unknown>
2003-03-22 19:25:00 +0000

    no message

--- MenuMorph/instance/construction/addTitle-3A-updatingSelector-3A-updateTarget-3A-.cs
+++ MenuMorph/instance/construction/addTitle-3A-updatingSelector-3A-updateTarget-3A-.cs
@@ -1,17 +1,23 @@
-addTitle: aString updatingSelector: aSelector updateTarget: aTarget
-	"Add a title line at the top of this menu  Make aString its initial contents.  If aSelector is not nil, then periodically obtain fresh values for its contents by sending aSelector to aTarget.."
-
+addTitle: aString updatingSelector: aSelector updateTarget: aTarget 
+	"Add a title line at the top of this menu Make aString its initial 
+	contents.  
+	If aSelector is not nil, then periodically obtain fresh values for its 
+	contents by sending aSelector to aTarget.."
 	| title |
-	title _ AlignmentMorph new.
+	title := AlignmentMorph new.
 	self setTitleParametersFor: title.
 	title vResizing: #shrinkWrap.
 	title listDirection: #topToBottom.
-	title wrapCentering: #center; cellPositioning: #topCenter.
+	title wrapCentering: #center;
+		 cellPositioning: #topCenter;
+
+		 layoutInset: 0.
 	aSelector
-		ifNotNil:
-			[title addMorphBack: (UpdatingStringMorph new lock; useStringFormat; target: aTarget; getSelector: aSelector)]
-		ifNil:
-			[(aString asString findTokens: String cr) do:
-				[:line | title addMorphBack: (StringMorph contents: line font: Preferences standardMenuFont)]].
-	
+		ifNil: [(aString asString findTokens: String cr)
+				do: [:line | title addMorphBack: (StringMorph new contents: line;
+							 font: Preferences standardMenuFont)]]
+		ifNotNil: [title addMorphBack: (UpdatingStringMorph new lock; font: Preferences standardMenuFont; useStringFormat; target: aTarget; getSelector: aSelector)].
+	title setProperty: #titleString toValue: aString.
 	self addMorphFront: title.
+	(self hasProperty: #needsTitlebarWidgets)
+		ifTrue: [self addStayUpIcons]
