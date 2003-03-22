9dd414751265d17d2e9a6f482aa919a85956e485
dgd <unknown>
2003-03-22 18:46:00 +0000

    no message

--- MenuMorph/instance/initialization/setDefaultParameters.cs
+++ MenuMorph/instance/initialization/setDefaultParameters.cs
@@ -1,14 +1,24 @@
 setDefaultParameters
-
-	| worldColor |
-	((Preferences menuColorFromWorld and: [Display depth > 4]) and: [(worldColor _ self currentWorld color) isColor])
-		ifTrue: [self setColor: (worldColor luminance > 0.7
-						ifTrue: [worldColor mixed: 0.8 with: Color black]
-						ifFalse: [worldColor mixed: 0.4 with: Color white])
-						"Think about whether alpha should be included."
-					borderWidth: Preferences menuBorderWidth
-					borderColor: #raised]
-		ifFalse: [self setColor: Preferences menuColor
-					borderWidth: Preferences menuBorderWidth
-					borderColor: Preferences menuBorderColor].
-	self layoutInset: 3.
+	| colorFromMenu worldColor menuColor menuBorderColor |
+	colorFromMenu := Preferences menuColorFromWorld
+				and: [Display depth > 4]
+				and: [(worldColor := self currentWorld color) isColor].
+	""
+	menuColor := colorFromMenu
+				ifTrue: [worldColor luminance > 0.7
+						ifTrue: [worldColor mixed: 0.85 with: Color black]
+						ifFalse: [worldColor mixed: 0.4 with: Color white]]
+				ifFalse: [Preferences menuColor].
+	""
+	menuBorderColor := Preferences menuAppearance3d
+				ifTrue: [#raised]
+				ifFalse: [colorFromMenu
+						ifTrue: [worldColor muchDarker]
+						ifFalse: [Preferences menuBorderColor]].
+	""
+	self
+		setColor: menuColor
+		borderWidth: Preferences menuBorderWidth
+		borderColor: menuBorderColor.
+	""
+	self layoutInset: 3
