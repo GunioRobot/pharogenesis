89c094d01a648646df49160374fbab78eb37014a
dgd <unknown>
2003-03-23 11:11:00 +0000

    no message

--- Preferences/class/menu parameters/restoreDefaultMenuParameters.cs
+++ Preferences/class/menu parameters/restoreDefaultMenuParameters.cs
@@ -1,14 +1,33 @@
 restoreDefaultMenuParameters
-	"Preferences restoreDefaultMenuParameters"
-	"Restore the four color choices of the original implementors of MorphicMenus"
-
-	Parameters at: #menuColor put: (Color r: 0.8 g: 0.8 b: 0.8).
-	Parameters at: #menuBorderColor put: #raised.
+	"Restore the four color choices of the original implementors of  
+	MorphicMenus"
+	" 
+	Preferences restoreDefaultMenuParameters
+	"
+	Parameters
+		at: #menuColor
+		put: (Color
+				r: 0.97
+				g: 0.97
+				b: 0.97).
+	Parameters
+		at: #menuBorderColor
+		put: (Color
+				r: 0.167
+				g: 0.167
+				b: 1.0).
 	Parameters at: #menuBorderWidth put: 2.
-
-	Parameters at: #menuTitleColor put: (Color r: 0.5 g: 1 b: 0.75).
-	Parameters at: #menuTitleBorderColor put: #inset.
+	Parameters at: #menuTitleColor put: (Color
+			r: 0.4
+			g: 0.8
+			b: 0.9) twiceDarker.
+	Parameters
+		at: #menuTitleBorderColor
+		put: (Color
+				r: 0.333
+				g: 0.667
+				b: 0.751).
 	Parameters at: #menuTitleBorderWidth put: 1.
-
-	Parameters at: #menuLineUpperColor put: (Color r: 0.667 g: 0.667 b: 0.667).
-	Parameters at: #menuLineLowerColor put: (Color r: 0.833 g: 0.833 b: 0.833).
+	Parameters
+		at: #menuLineColor
+		put: (Preferences menuBorderColor lighter)
