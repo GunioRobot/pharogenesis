ccf3638a420b2dc4dd5ecbb4b43c756fae40ed79
ads <unknown>
2003-03-25 17:34:00 +0000

    no message

--- MessageFile/instance/message operations/assertValidMessageAt-3A-id-3A-.cs
+++ MessageFile/instance/message operations/assertValidMessageAt-3A-id-3A-.cs
@@ -1,13 +1,17 @@
 assertValidMessageAt: filePosition id: msgID
-	"Verify that the given filePosition is, indeed, the start of a message (possibly deleted) with the given ID and raise an error if this assertion is false."
+	"Verify that the given filePosition is, indeed, the start of a valid undeleted message with the given ID and raise an error if this assertion is false."
 
 	| delimiter fileMsgID |
 	"assume file is open"
+
 	file position: filePosition.
-	delimiter _ file next: 10.
-	((delimiter = '&&&&&start') or: [delimiter = '&&&&&XXXXX']) ifFalse:
-		[^self reportInconsistency].
-	file next. "skip cr"
+
+	delimiter _ file next: 11.
+	(delimiter = ('&&&&&start', String cr)) ifFalse:
+		[self reportInconsistency. ^false].
+
 	fileMsgID _ MailDB readIntegerLineFrom: file.
 	(msgID = fileMsgID) ifFalse:
-		[^self reportInconsistency].
+		[self reportInconsistency. ^false].
+
+	^true.
