98b65176ba796fe4101f097e3364ded90b201f93
dgd <unknown>
2003-03-26 09:13:00 +0000

    no message

--- ScrollBar/instance/geometry/extent-3A-.cs
+++ ScrollBar/instance/geometry/extent-3A-.cs
@@ -1,4 +1,7 @@
-extent: p
-        p x > p y
-                ifTrue: [super extent: (p max: 42@8)]
-                ifFalse: [super extent: (p max: 8@42)]
+extent: p 
+	p x > p y
+		ifTrue: [super
+				extent: (p max: 42 @ 8)]
+		ifFalse: [super
+				extent: (p max: 8 @ 42)].
+	
