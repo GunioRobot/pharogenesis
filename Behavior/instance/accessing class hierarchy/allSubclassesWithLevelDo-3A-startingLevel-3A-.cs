2d8f210aabe6d7123f57db5fbe9439dd5816e062
sd <unknown>
2003-03-28 15:06:00 +0000

    no message

--- Behavior/instance/accessing class hierarchy/allSubclassesWithLevelDo-3A-startingLevel-3A-.cs
+++ Behavior/instance/accessing class hierarchy/allSubclassesWithLevelDo-3A-startingLevel-3A-.cs
@@ -1,12 +1,12 @@
 allSubclassesWithLevelDo: classAndLevelBlock startingLevel: level 
 	"Walk the tree of subclasses, giving the class and its level"
-	| subclassNames subclass |
+	| subclassNames |
 	classAndLevelBlock value: self value: level.
 	self == Class ifTrue:  [^ self].  "Don't visit all the metaclasses"
 	"Visit subclasses in alphabetical order"
 	subclassNames _ SortedCollection new.
 	self subclassesDo: [:subC | subclassNames add: subC name].
 	subclassNames do:
-		[:name | (Smalltalk at: name)
+		[:name | (self environment at: name)
 			allSubclassesWithLevelDo: classAndLevelBlock
 			startingLevel: level+1]
