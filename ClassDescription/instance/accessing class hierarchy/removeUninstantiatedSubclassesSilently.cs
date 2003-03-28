ce874daafba7c635efd90a020e7a1e71aee2168d
sd <unknown>
2003-03-28 15:32:00 +0000

    no message

--- ClassDescription/instance/accessing class hierarchy/removeUninstantiatedSubclassesSilently.cs
+++ ClassDescription/instance/accessing class hierarchy/removeUninstantiatedSubclassesSilently.cs
@@ -4,9 +4,9 @@ removeUninstantiatedSubclassesSilently
 
 	| candidatesForRemoval  oldFree |
 
-	oldFree _ Smalltalk garbageCollect.
+	oldFree _ self environment garbageCollect.
 	candidatesForRemoval _
 		self subclasses select: [:c |
 			(c instanceCount = 0) and: [c subclasses size = 0]].
 	candidatesForRemoval do: [:c | c removeFromSystem].
-	^ Smalltalk garbageCollect - oldFree
+	^ self environment garbageCollect - oldFree
