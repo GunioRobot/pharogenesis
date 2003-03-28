0136de34884c380c9b4f5ccc7cc2f945529bddd9
sd <unknown>
2003-03-28 15:24:00 +0000

    no message

--- Class/instance/subclass creation/newSubclass.cs
+++ Class/instance/subclass creation/newSubclass.cs
@@ -2,7 +2,7 @@ newSubclass
 	| i className |
 	i _ 1.
 	[className _ (self name , i printString) asSymbol.
-	 Smalltalk includesKey: className]
+	 self environment includesKey: className]
 		whileTrue: [i _ i + 1].
 
 	^ self subclass: className
