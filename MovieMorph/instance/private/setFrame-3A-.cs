1b8c5f6d5d687197d7f73ad61d8701c4a0702dad
jdl <unknown>
2003-03-28 08:08:00 +0000

    no message

--- MovieMorph/instance/private/setFrame-3A-.cs
+++ MovieMorph/instance/private/setFrame-3A-.cs
@@ -1,20 +1,16 @@
-setFrame: newFrameIndex
-
+setFrame: newFrameIndex 
 	| oldFrame p newFrame |
-	oldFrame _ self currentFrame.
-	oldFrame ifNil: [^ self].
-
+	oldFrame := self currentFrame.
+	oldFrame ifNil: [^self].
 	self changed.
-	p _ oldFrame referencePosition.
-	currentFrameIndex _ newFrameIndex.
-	currentFrameIndex > frameList size
-		ifTrue: [currentFrameIndex _ frameList size].
-	currentFrameIndex < 1
-		ifTrue: [currentFrameIndex _ 1].
-	newFrame _ frameList at: currentFrameIndex.
+	p := oldFrame referencePosition.
+	currentFrameIndex := newFrameIndex.
+     currentFrameIndex :=  currentFrameIndex min: (frameList size). 
+	currentFrameIndex := currentFrameIndex max: 1.
+	newFrame := frameList at: currentFrameIndex.
 	newFrame referencePosition: p.
 	oldFrame delete.
 	self addMorph: newFrame.
-	dwellCount _ newFrame framesToDwell.
+	dwellCount := newFrame framesToDwell.
 	self layoutChanged.
-	self changed.
+	self changed
