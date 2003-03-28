11644e60588777b9d565d32187a4b6cb3245e4d4
jdl <unknown>
2003-03-28 08:50:00 +0000

    no message

--- TableLayout/instance/optimized/layoutTopToBottom-3A-in-3A-.cs
+++ TableLayout/instance/optimized/layoutTopToBottom-3A-in-3A-.cs
@@ -1,91 +1,96 @@
-layoutTopToBottom: aMorph in: newBounds
+layoutTopToBottom: aMorph in: newBounds 
 	"An optimized top-to-bottom list layout"
+
 	| inset n size extent width height block sum vFill posX posY extra centering extraPerCell last amount minX minY maxX maxY sizeX sizeY first cell props |
-	size _ properties minCellSize asPoint. minX _ size x. minY _ size y.
-	size _ properties maxCellSize asPoint. maxX _ size x. maxY _ size y.
-	inset _ properties cellInset asPoint y.
-	extent _ newBounds extent.
-	n _ 0. vFill _ false. sum _ 0.
-	width _ height _ 0.
-	first _ last _ nil.
-	block _ [:m|
-		props _ m layoutProperties ifNil:[m].
-		props disableTableLayout ifFalse:[
-			n _ n + 1.
-			cell _ LayoutCell new target: m.
-			(props vResizing == #spaceFill) ifTrue:[
-				cell vSpaceFill: true.
-				extra _ m spaceFillWeight.
-				cell extraSpace: extra.
-				sum _ sum + extra.
-			] ifFalse:[cell vSpaceFill: false].
-			(props hResizing == #spaceFill) ifTrue:[vFill _ true].
-			size _ m minExtent. sizeX _ size x. sizeY _ size y.
-			sizeX < minX
-				ifTrue:[sizeX _ minX]
-				ifFalse:[sizeX > maxX ifTrue:[sizeX _ maxX]].
-			sizeY < minY
-				ifTrue:[sizeY _ minY]
-				ifFalse:[sizeY > maxY ifTrue:[sizeY _ maxY]].
-			cell cellSize: sizeY.
-			first ifNil:[first _ cell] ifNotNil:[last nextCell: cell].
-			last _ cell.
-			height _ height + sizeY.
-			sizeX > width ifTrue:[width _ sizeX].
-		].
-	].
-	properties reverseTableCells
-		ifTrue:[aMorph submorphsReverseDo: block]
-		ifFalse:[aMorph submorphsDo: block].
-
-	n > 1 ifTrue:[height _ height + (n-1 * inset)].
-
-	(properties vResizing == #shrinkWrap and:[properties rubberBandCells or:[sum isZero]])
-		ifTrue:[extent _ (extent x max: width) @ height].
-	(properties hResizing == #shrinkWrap and:[properties rubberBandCells or:[vFill not]])
-		ifTrue:[extent _ width @ (extent y max: height)].
-
-	posX _ newBounds left.
-	posY _ newBounds top.
+	size := properties minCellSize asPoint.
+	minX := size x.
+	minY := size y.
+	size := properties maxCellSize asPoint.
+	maxX := size x.
+	maxY := size y.
+	inset := properties cellInset asPoint y.
+	extent := newBounds extent.
+	n := 0.
+	vFill := false.
+	sum := 0.
+	width := height := 0.
+	first := last := nil.
+	block := 
+			[:m | 
+			props := m layoutProperties ifNil: [m].
+			props disableTableLayout 
+				ifFalse: 
+					[n := n + 1.
+					cell := LayoutCell new target: m.
+					props vResizing == #spaceFill 
+						ifTrue: 
+							[cell vSpaceFill: true.
+							extra := m spaceFillWeight.
+							cell extraSpace: extra.
+							sum := sum + extra]
+						ifFalse: [cell vSpaceFill: false].
+					props hResizing == #spaceFill ifTrue: [vFill := true].
+					size := m minExtent.
+					sizeX := size x.
+					sizeY := size y.
+					sizeX < minX ifTrue: [sizeX := minX] ifFalse: [sizeX := sizeX min: maxX].
+					sizeY < minY ifTrue: [sizeY := minY] ifFalse: [sizeY := sizeY min: maxY].
+					cell cellSize: sizeY.
+					first ifNil: [first := cell] ifNotNil: [last nextCell: cell].
+					last := cell.
+					height := height + sizeY.
+					sizeX > width ifTrue: [width := sizeX]]].
+	properties reverseTableCells 
+		ifTrue: [aMorph submorphsReverseDo: block]
+		ifFalse: [aMorph submorphsDo: block].
+	n > 1 ifTrue: [height := height + ((n - 1) * inset)].
+	(properties vResizing == #shrinkWrap 
+		and: [properties rubberBandCells or: [sum isZero]]) 
+			ifTrue: [extent := (extent x max: width) @ height].
+	(properties hResizing == #shrinkWrap 
+		and: [properties rubberBandCells or: [vFill not]]) 
+			ifTrue: [extent := width @ (extent y max: height)].
+	posX := newBounds left.
+	posY := newBounds top.
 
 	"Compute extra horizontal space"
-	extra _ extent x - width.
-	extra < 0 ifTrue:[extra _ 0].
-	extra > 0 ifTrue:[
-		vFill ifTrue:[
-			width _ extent x.
-		] ifFalse:[
-			centering _ properties wrapCentering.
-			centering == #bottomRight ifTrue:[posX _ posX + extra].
-			centering == #center ifTrue:[posX _ posX + (extra // 2)]
-		].
-	].
+	extra := extent x - width.
+	extra := extra max: 0.
+	extra > 0 
+		ifTrue: 
+			[vFill 
+				ifTrue: [width := extent x]
+				ifFalse: 
+					[centering := properties wrapCentering.
+					centering == #bottomRight ifTrue: [posX := posX + extra].
+					centering == #center ifTrue: [posX := posX + (extra // 2)]]].
 
 
 	"Compute extra vertical space"
-	extra _ extent y - height.
-	extra < 0 ifTrue:[extra _ 0].
-	extraPerCell _ 0.
-	extra > 0 ifTrue:[
-		sum isZero ifTrue:["extra space but no #spaceFillers"
-			centering _ properties listCentering.
-			centering == #bottomRight ifTrue:[posY _ posY + extra].
-			centering == #center ifTrue:[posY _ posY + (extra // 2)].
-		] ifFalse:[extraPerCell _ extra asFloat / sum asFloat].
-	].
+	extra := extent y - height.
+	extra := extra max: 0.
+	extraPerCell := 0.
+	extra > 0 
+		ifTrue: 
+			[sum isZero 
+				ifTrue: 
+					["extra space but no #spaceFillers"
 
-	n _ 0.
-	extra _ last _ 0.
-	cell _ first.
-	[cell == nil] whileFalse:[
-		n _ n + 1.
-		height _ cell cellSize.
-		(extraPerCell > 0 and:[cell vSpaceFill]) ifTrue:[
-			extra _ (last _ extra) + (extraPerCell * cell extraSpace).
-			amount _ extra truncated - last truncated.
-			height _ height + amount.
-		].
-		cell target layoutInBounds: (posX @ posY extent: width @ height).
-		posY _ posY + height + inset.
-		cell _ cell nextCell.
-	].
+					centering := properties listCentering.
+					centering == #bottomRight ifTrue: [posY := posY + extra].
+					centering == #center ifTrue: [posY := posY + (extra // 2)]]
+				ifFalse: [extraPerCell := extra asFloat / sum asFloat]].
+	n := 0.
+	extra := last := 0.
+	cell := first.
+	[cell isNil] whileFalse: 
+			[n := n + 1.
+			height := cell cellSize.
+			(extraPerCell > 0 and: [cell vSpaceFill]) 
+				ifTrue: 
+					[extra := (last := extra) + (extraPerCell * cell extraSpace).
+					amount := extra truncated - last truncated.
+					height := height + amount].
+			cell target layoutInBounds: (posX @ posY extent: width @ height).
+			posY := posY + height + inset.
+			cell := cell nextCell]
