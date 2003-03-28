7a2401138d492b5df934bc1181720257900a9153
dgd <unknown>
2003-03-28 19:44:00 +0000

    no message

--- ScrollBar/instance/initialize/initializePagingArea.cs
+++ ScrollBar/instance/initialize/initializePagingArea.cs
@@ -1,7 +1,21 @@
 initializePagingArea
-	pagingArea := RectangleMorph newBounds: self totalSliderArea
-								color: (Color r: 0.6 g: 0.6 b: 0.8).
+"initialize the receiver's pagingArea"
+	pagingArea := RectangleMorph
+				newBounds: self totalSliderArea
+				color: (Color
+						r: 0.6
+						g: 0.6
+						b: 0.8).
 	pagingArea borderWidth: 0.
-	pagingArea on: #mouseDown send: #scrollPageInit: to: self.
-	pagingArea on: #mouseUp send: #finishedScrolling to: self.
-	self addMorph: pagingArea
+	pagingArea
+		on: #mouseDown
+		send: #scrollPageInit:
+		to: self.
+	pagingArea
+		on: #mouseUp
+		send: #finishedScrolling
+		to: self.
+	self addMorph: pagingArea.
+	self roundedScrollbarLook
+		ifTrue: [pagingArea
+				color: (Color gray: 0.9)]
