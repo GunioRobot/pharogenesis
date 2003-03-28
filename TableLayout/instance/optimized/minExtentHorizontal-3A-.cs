fcc9b1ecacf3ecfbb8a82d9157c80f3fc58e8c40
jdl <unknown>
2003-03-28 08:37:00 +0000

    no message

--- TableLayout/instance/optimized/minExtentHorizontal-3A-.cs
+++ TableLayout/instance/optimized/minExtentHorizontal-3A-.cs
@@ -1,24 +1,31 @@
-minExtentHorizontal: aMorph
+minExtentHorizontal: aMorph 
 	"Return the minimal size aMorph's children would require given the new bounds"
+
 	| inset n size width height minX minY maxX maxY sizeX sizeY |
-	size _ properties minCellSize asPoint. minX _ size x. minY _ size y.
-	size _ properties maxCellSize asPoint. maxX _ size x. maxY _ size y.
-	inset _ properties cellInset asPoint.
-	n _ 0.
-	width _ height _ 0.
-	aMorph submorphsDo:[:m|
-		m disableTableLayout ifFalse:[
-			n _ n + 1.
-			size _ m minExtent. sizeX _ size x. sizeY _ size y.
-			sizeX < minX
-				ifTrue:[sizeX _ minX]
-				ifFalse:[sizeX > maxX ifTrue:[sizeX _ maxX]].
-			sizeY < minY
-				ifTrue:[sizeY _ minY]
-				ifFalse:[sizeY > maxY ifTrue:[sizeY _ maxY]].
-			width _ width + sizeX.
-			sizeY > height ifTrue:[height _ sizeY].
-		].
-	].
-	n > 1 ifTrue:[width _ width + (n-1 * inset x)].
-	^minExtentCache _ width @ height
+	size := properties minCellSize asPoint.
+	minX := size x.
+	minY := size y.
+	size := properties maxCellSize asPoint.
+	maxX := size x.
+	maxY := size y.
+	inset := properties cellInset asPoint.
+	n := 0.
+	width := height := 0.
+	aMorph submorphsDo: 
+			[:m | 
+			m disableTableLayout 
+				ifFalse: 
+					[n := n + 1.
+					size := m minExtent.
+					sizeX := size x.
+					sizeY := size y.
+					sizeX < minX 
+						ifTrue: [sizeX := minX]
+						ifFalse: [sizeX := sizeX min: maxX].
+					sizeY < minY 
+						ifTrue: [sizeY := minY]
+						ifFalse: [sizeY := sizeY min: maxY].
+					width := width + sizeX.
+					sizeY > height ifTrue: [height := sizeY]]].
+	n > 1 ifTrue: [width := width + ((n - 1) * inset x)].
+	^minExtentCache := width @ height
