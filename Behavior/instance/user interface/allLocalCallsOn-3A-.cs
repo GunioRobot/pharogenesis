6a127c38047cf3f7bea2f9fba76a38d4fcade464
sd <unknown>
2003-03-28 15:05:00 +0000

    no message

---
+++ Behavior/instance/user interface/allLocalCallsOn-3A-.cs
@@ -0,0 +1,17 @@
+allLocalCallsOn: aSymbol
+	"Answer a SortedCollection of all the methods that call on aSymbol, anywhere in my class hierarchy."
+
+	| aSet special byte cls |
+	aSet _ Set new.
+	cls _ self theNonMetaClass.
+	special _ self environment hasSpecialSelector: aSymbol
+					ifTrueSetByte: [:b | byte _ b ].
+	cls withAllSuperAndSubclassesDoGently: [ :class |
+		(class whichSelectorsReferTo: aSymbol special: special byte: byte)
+			do: [:sel |
+				sel ~~ #DoIt ifTrue: [aSet add: class name , ' ', sel]]].
+	cls class withAllSuperAndSubclassesDoGently: [ :class |
+		(class whichSelectorsReferTo: aSymbol special: special byte: byte)
+			do: [:sel |
+				sel ~~ #DoIt ifTrue: [aSet add: class name , ' ', sel]]].
+	^aSet
