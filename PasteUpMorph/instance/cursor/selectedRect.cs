a6828b80b90e6348a22e66d1aab8099259722597
jdl <unknown>
2003-03-28 08:17:00 +0000

    no message

--- PasteUpMorph/instance/cursor/selectedRect.cs
+++ PasteUpMorph/instance/cursor/selectedRect.cs
@@ -2,7 +2,7 @@ selectedRect
 	"Return a rectangle enclosing the morph at the current cursor. Note that the cursor may be a float and may be out of range, so pick the nearest morph. Assume there is at least one submorph."
 
 	| p |
-	p _ cursor asInteger.
-	p > submorphs size ifTrue: [p _ submorphs size].
-	p < 1 ifTrue: [p _ 1].
-	^ (submorphs at: p) fullBounds expandBy: 2.
+	p := cursor asInteger.
+	p := p min: submorphs size.
+	p := p max: 1.
+	^(submorphs at: p) fullBounds expandBy: 2
