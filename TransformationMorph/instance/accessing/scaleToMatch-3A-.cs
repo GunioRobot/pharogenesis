64a53c6a778bc99a93ed3daf3f121a1ce6d51139
jdl <unknown>
2003-03-28 08:13:00 +0000

    no message

--- TransformationMorph/instance/accessing/scaleToMatch-3A-.cs
+++ TransformationMorph/instance/accessing/scaleToMatch-3A-.cs
@@ -1,12 +1,21 @@
-scaleToMatch: aPoint
+scaleToMatch: aPoint 
 	| scaleFactor tfm originalScale |
-	tfm _ transform withScale: 1.0.
-	originalScale _ ((tfm localBoundsToGlobal: self renderedMorph fullBounds) corner -
-		(tfm localPointToGlobal: self renderedMorph referencePosition)) r.
+	tfm := transform withScale: 1.0.
+	originalScale := ((tfm localBoundsToGlobal: self renderedMorph fullBounds) 
+				corner - (tfm localPointToGlobal: self renderedMorph referencePosition)) 
+				r.
 	"Catch cases where the reference point is on fullBounds corner"
-	originalScale < 1.0 ifTrue:[originalScale _ 1.0].
-	scaleFactor _ (aPoint - self referencePosition) r / originalScale.
-	scaleFactor _ scaleFactor < 1.0
-		ifTrue: [scaleFactor detentBy: 0.05 atMultiplesOf: 0.25 snap: false]
-		ifFalse: [scaleFactor detentBy: 0.1 atMultiplesOf: 0.5 snap: false].
-	self adjustAfter:[self scale: ((scaleFactor min: 8.0) max: 0.1)].
+	originalScale := originalScale max: 1.0.
+	scaleFactor := (aPoint - self referencePosition) r / originalScale.
+	scaleFactor := scaleFactor < 1.0 
+				ifTrue: 
+					[scaleFactor 
+						detentBy: 0.05
+						atMultiplesOf: 0.25
+						snap: false]
+				ifFalse: 
+					[scaleFactor 
+						detentBy: 0.1
+						atMultiplesOf: 0.5
+						snap: false].
+	self adjustAfter: [self scale: ((scaleFactor min: 8.0) max: 0.1)]
