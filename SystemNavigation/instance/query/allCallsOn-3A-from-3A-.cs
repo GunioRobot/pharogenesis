347a4162c1a7a6fe7970181ea71be76f75f07e3b
sd <unknown>
2003-03-28 17:44:00 +0000

    no message

---
+++ SystemNavigation/instance/query/allCallsOn-3A-from-3A-.cs
@@ -0,0 +1,18 @@
+allCallsOn: aSymbol from: aClass
+	"Answer a SortedCollection of all the methods that call on aSymbol."
+
+	| aSortedCollection special byte |
+	aSortedCollection _ SortedCollection new.
+	special _ aClass environment hasSpecialSelector: aSymbol ifTrueSetByte: [:b | byte _ b ].
+	aClass withAllSubclassesDo: [ :class |
+		(class whichSelectorsReferTo: aSymbol special: special byte: byte) do: [:sel |
+			sel == #DoIt ifFalse: [
+				aSortedCollection add: (
+					MethodReference new
+						setStandardClass: class 
+						methodSymbol: sel
+				)
+			]
+		]
+	].
+	^aSortedCollection
