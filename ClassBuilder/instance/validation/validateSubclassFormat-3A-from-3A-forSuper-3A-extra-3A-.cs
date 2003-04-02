3faf4cf585c35139c8da58d1fe2db513fdce2e6d
bkv <unknown>
2003-04-02 17:19:00 +0000

    no message

--- ClassBuilder/instance/validation/validateSubclassFormat-3A-from-3A-forSuper-3A-extra-3A-.cs
+++ ClassBuilder/instance/validation/validateSubclassFormat-3A-from-3A-forSuper-3A-extra-3A-.cs
@@ -1,6 +1,6 @@
 validateSubclassFormat: newType from: oldClass forSuper: newSuper extra: newInstSize
 	"Validate the # of instVars and the format of the subclasses"
-	| deltaSize oldType |
+	| deltaSize |
 	oldClass == nil ifTrue: [^ true]. "No subclasses"
 	"Compute the # of instvars needed for all subclasses"
 	deltaSize _ newInstSize.
@@ -10,20 +10,18 @@ validateSubclassFormat: newType from: oldClass forSuper: newSuper extra: newInst
 		ifTrue: [deltaSize _ deltaSize + newSuper instSize].
 	(oldClass notNil and: [oldClass superclass notNil]) 
 		ifTrue: [deltaSize _ deltaSize - oldClass superclass instSize].
-	oldClass == nil ifTrue: [
-		deltaSize > 254 ifTrue: [
-			self error: 'More than 254 instance variables'.
-			^ false].
-		^ true].
-	oldClass withAllSubclassesDo: [:sub |
-		sub instSize + deltaSize > 254 ifTrue: [
-			self error: sub name,' has more than 254 instance variables'.
-			^ false]].
-	newType ~~ #normal ifTrue:
-		["And check if the immediate subclasses of oldClass can keep its layout"
-		oldClass subclassesDo:[:sub|
-			oldType _ sub typeOfClass.
-			oldType == newType ifFalse: [
-				self error: sub name,' cannot be recompiled'.
-				^ false]]].
+	(oldClass == nil)
+		 ifTrue: [ (deltaSize > 254)
+					ifTrue: [ self error: 'More than 254 instance variables'.
+							^ false].
+				  ^ true].
+
+	oldClass withAllSubclassesDo: [:sub |  ( sub instSize + deltaSize > 254 )
+											ifTrue: [ self error: sub name,' has more than 254 instance variables'.
+					 								^ false].
+
+										"If we get this far, check whether the immediate subclasses of oldClass can keep its layout."
+               							(newType ~~ #normal) 
+											ifTrue: [ self validateSubclass: sub canKeepLayoutFrom: oldClass forSubclassFormat: newType ]].
+
 	^ true
