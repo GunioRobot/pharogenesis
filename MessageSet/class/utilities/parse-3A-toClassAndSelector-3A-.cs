09d0361f9c3092262eca8aed1efbde7d5fcff5fe
bkv <unknown>
2003-04-02 11:33:00 +0000

    no message

--- MessageSet/class/utilities/parse-3A-toClassAndSelector-3A-.cs
+++ MessageSet/class/utilities/parse-3A-toClassAndSelector-3A-.cs
@@ -1,9 +1,17 @@
-parse: messageString toClassAndSelector: csBlock
+parse: methodRef toClassAndSelector: csBlock
 	"Decode strings of the form <className> [class] <selectorName>."
 
 	| tuple cl |
-	messageString ifNil: [^ csBlock value: nil value: nil].
-	tuple _ messageString findTokens: ' .'.
+
+
+	self flag: #mref.	"compatibility with pre-MethodReference lists"
+
+	methodRef ifNil: [^ csBlock value: nil value: nil].
+	(methodRef isKindOf: MethodReference) ifTrue: [
+		^methodRef setClassAndSelectorIn: csBlock
+	].
+	methodRef isEmpty ifTrue: [^ csBlock value: nil value: nil].
+	tuple _ methodRef asString findTokens: ' .'.
 	cl _ Smalltalk atOrBelow: tuple first asSymbol ifAbsent: [^ csBlock value: nil value: nil].
 	(tuple size = 2 or: [tuple size > 2 and: [(tuple at: 2) ~= 'class']])
 		ifTrue: [^ csBlock value: cl value: (tuple at: 2) asSymbol]
