412b52d70497eeaa0833128272ec7752fe301c73
hpt <unknown>
2003-04-03 19:18:00 +0000

    no message

--- ScrollBar/instance/geometry/totalSliderArea.cs
+++ ScrollBar/instance/geometry/totalSliderArea.cs
@@ -1,4 +1,12 @@
 totalSliderArea
-	^ bounds isWide
-		ifTrue: [upButton bounds topRight corner: downButton bounds bottomLeft]
-		ifFalse: [upButton bounds bottomLeft corner: downButton bounds topRight]
+	| upperBoundsButton |
+	upperBoundsButton _ menuButton ifNil: [upButton].
+	bounds isWide
+		ifTrue: [
+			upButton right > upperBoundsButton right
+				ifTrue: [upperBoundsButton _ upButton].
+			^upperBoundsButton bounds topRight corner: downButton bounds bottomLeft]
+		ifFalse:[
+			upButton bottom > upperBoundsButton bottom
+				ifTrue: [upperBoundsButton _ upButton].
+			^upperBoundsButton bounds bottomLeft corner: downButton bounds topRight].
