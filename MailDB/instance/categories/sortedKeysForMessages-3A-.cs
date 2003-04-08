c0365ebf3abef6b762715b66a51bfa32ba6cd2cf
ads <unknown>
2003-04-08 20:57:00 +0000

    no message

--- MailDB/instance/categories/sortedKeysForMessages-3A-.cs
+++ MailDB/instance/categories/sortedKeysForMessages-3A-.cs
@@ -1,9 +1,2 @@
 sortedKeysForMessages: aSet 
-	"use a heuristic to choose method of obtaining sorted message list"
-	^ aSet size * (aSet size log: 2) * 3 > indexFile keysCount
-		ifTrue: [indexFile keys select: [:msgID | "indexFile keys is sorted"
-				aSet includes: msgID]]
-		ifFalse: [(aSet asArray
-				mergeSortFrom: 1
-				to: aSet size
-				by: [:a :b | (self getTOCentry: a) time < (self getTOCentry: b) time]) asOrderedCollection]
+	^ self keysForMessages: aSet sortedBy: #time
