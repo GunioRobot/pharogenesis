db7299740c0fcdd482e8ae3c8651f33f299df74c
asm <unknown>
2003-04-10 12:41:00 +0000

    no message

--- ChangeSorter/class/class initialization/initialize.cs
+++ ChangeSorter/class/class initialization/initialize.cs
@@ -1,4 +1,15 @@
 initialize
+	"Initialize the class variables"
+
 	AllChangeSets == nil ifTrue:
 		[AllChangeSets _ OrderedCollection new].
 	self gatherChangeSets.
+	ChangeSetCategories ifNil:
+		[self initializeChangeSetCategories].
+	RecentUpdateMarker _ 0.
+
+	"ChangeSorter initialize"
+
+	FileList registerFileReader: self.
+
+	self registerInFlapsRegistry.
