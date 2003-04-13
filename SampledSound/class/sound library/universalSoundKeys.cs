4e2ec064ff726ccd41ff71963e57d0ee2497ac19
sw <unknown>
2003-04-13 20:58:00 +0000

    no message

---
+++ SampledSound/class/sound library/universalSoundKeys.cs
@@ -0,0 +1,5 @@
+universalSoundKeys
+	"Answer a list of the sound-names that are expected to be found in the SoundLibrary of every image."
+
+	^ #('splash' 'peaks' 'clink' 'croak' 'scratch' 'chirp' 'scritch' 'warble' 'scrape' 'camera' 'coyote' 'silence' 'motor')
+
