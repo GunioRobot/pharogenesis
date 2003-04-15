860ced69e9211e8d9a7a9ff76bc52e7116461883
sw <unknown>
2003-04-15 23:42:00 +0000

    no message

--- EToyVocabulary/instance/method list/allMethodsInCategory-3A-forInstance-3A-ofClass-3A-.cs
+++ EToyVocabulary/instance/method list/allMethodsInCategory-3A-forInstance-3A-ofClass-3A-.cs
@@ -1,19 +1,20 @@
-allMethodsInCategory: categoryName forInstance: anObject ofClass: aClass
+allMethodsInCategory: aCategoryName forInstance: anObject ofClass: aClass
 	"Answer a list of all methods in the etoy interface which are in the given category, on behalf of anObject, or if it is nil, aClass"
 
 	| aCategory unfiltered suitableSelectors isAll |
-	categoryName ifNil: [^ OrderedCollection new].
+
+	aCategoryName ifNil: [^ OrderedCollection new].
 	aClass isUniClass ifTrue:
-		[categoryName == #scripts ifTrue:
+		[aCategoryName = ScriptingSystem nameForScriptsCategory ifTrue:
 			[^ aClass namedTileScriptSelectors].
-		categoryName == #'instance variables' ifTrue:
+		aCategoryName = ScriptingSystem nameForInstanceVariablesCategory ifTrue:
 			[^ aClass slotInfo keys asSortedArray collect:
 				[:anInstVarName | Utilities getterSelectorFor: anInstVarName]]].
-	unfiltered _ (isAll _ categoryName = self allCategoryName)
+	unfiltered _ (isAll _ aCategoryName = self allCategoryName)
 		ifTrue:
 			[methodInterfaces collect: [:anInterface | anInterface selector]]
 		ifFalse:
-			[aCategory _ categories detect: [:cat | cat categoryName == categoryName asSymbol] 
+			[aCategory _ categories detect: [:cat | cat categoryName == aCategoryName] 
 							ifNone: [^ OrderedCollection new].
 			aCategory elementsInOrder collect: [:anElement | anElement selector]].
 
