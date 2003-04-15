166f45c2d01ef7ec0f5892f93d74637cce266bc4
sd <unknown>
2003-04-15 16:11:00 +0000

    no message

--- StringHolder/instance/message list menu/browseClassVarRefs.cs
+++ StringHolder/instance/message list menu/browseClassVarRefs.cs
@@ -2,4 +2,5 @@ browseClassVarRefs
 	"1/17/96 sw: devolve responsibility to the class, so that the code that does the real work can be shared"
 
 	| cls |
-	(cls _ self selectedClass) ifNotNil: [cls browseClassVarRefs]
+	cls _ self selectedClass.
+	cls ifNotNil: [self systemNavigation  browseClassVarRefs: cls]
