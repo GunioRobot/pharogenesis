77cea0119c4fab24da4741c4849c82149ea9f30a
sd <unknown>
2003-04-15 20:32:00 +0000

    no message

---
+++ SystemNavigation/instance/browse/browseObsoleteReferences.cs
@@ -0,0 +1,12 @@
+browseObsoleteReferences  
+	"self new browseObsoleteReferences"
+
+	| references |
+	references _ OrderedCollection new.
+	(LookupKey allSubInstances select:
+		[:x | ((x value isKindOf: Behavior) and: ['AnOb*' match: x value name]) or:
+		['AnOb*' match: x value class name]]) 
+		do: [:x | references addAll: (self allCallsOn: x)].
+	self  
+		browseMessageList: references 
+		name: 'References to Obsolete Classes'
