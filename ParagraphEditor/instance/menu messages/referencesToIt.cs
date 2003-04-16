edede42ca83ce45a3da01652b57bb5cae69fe93e
sd <unknown>
2003-04-16 11:47:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/referencesToIt.cs
+++ ParagraphEditor/instance/menu messages/referencesToIt.cs
@@ -1,9 +1,10 @@
 referencesToIt
 	"Open a references browser on the selected symbol"
+
 	| aSymbol |
-	self selectionInterval isEmpty ifTrue: [self selectWord].
+	self selectLine.
 	((aSymbol _ self selectedSymbol) == nil or:
 		[(Smalltalk includesKey: aSymbol) not])
 			ifTrue: [^ view flash].
 
-	self terminateAndInitializeAround: [Smalltalk browseAllCallsOn: (Smalltalk associationAt: self selectedSymbol)]
+	self terminateAndInitializeAround: [self systemNavigation browseAllCallsOn: (Smalltalk associationAt: self selectedSymbol)]
