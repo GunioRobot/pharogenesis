d4b7a64ac48b844b4d3529587d13b75a09dc4b94
sd <unknown>
2003-04-16 08:53:00 +0000

    no message

---
+++ SystemNavigation/instance/browse/browseAllStoresInto-3A-from-3A-.cs
@@ -0,0 +1,25 @@
+browseAllStoresInto: instVarName from: aClass
+	"Create and schedule a Message Set browser for all the receiver's methods 
+	or any methods of a subclass/superclass that refer to the instance variable name."
+	
+	"self new browseAllStoresInto: 'contents' from: Collection."
+
+	| coll |
+	coll _ OrderedCollection new.
+	Cursor wait showWhile: [
+		aClass withAllSubAndSuperclassesDo: [:class | 
+			(class whichSelectorsStoreInto: instVarName) do: [:sel |
+				sel == #DoIt ifFalse: [
+					coll add: (
+						MethodReference new
+							setStandardClass: class 
+							methodSymbol: sel
+					)
+				]
+			]
+		].
+	].
+	^ self
+		browseMessageList: coll 
+		name: 'Stores into ' , instVarName 
+		autoSelect: instVarName
