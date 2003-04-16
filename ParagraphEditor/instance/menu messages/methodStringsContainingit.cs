85b46d1749e5c68b6465165d1948861cf5d08e4c
sd <unknown>
2003-04-16 19:28:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/methodStringsContainingit.cs
+++ ParagraphEditor/instance/menu messages/methodStringsContainingit.cs
@@ -2,4 +2,4 @@ methodStringsContainingit
 	"Open a browser on methods which contain the current selection as part of a string constant."
 
 	self lineSelectAndEmptyCheck: [^ self].
-	self terminateAndInitializeAround: [Smalltalk browseMethodsWithString: self selection string]
+	self terminateAndInitializeAround: [self systemNavigation browseMethodsWithString: self selection string]
