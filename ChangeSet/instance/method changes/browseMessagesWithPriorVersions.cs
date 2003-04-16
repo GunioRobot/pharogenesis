91c3268dc672b74773096572f5c40282ba73f3b0
sd <unknown>
2003-04-16 09:15:00 +0000

    no message

--- ChangeSet/instance/method changes/browseMessagesWithPriorVersions.cs
+++ ChangeSet/instance/method changes/browseMessagesWithPriorVersions.cs
@@ -1,11 +1,13 @@
 browseMessagesWithPriorVersions
 	"Open a message list browser on the new and changed methods in the receiver which have at least one prior version.  6/28/96 sw"
 
-	| aList aSelector aClass |
+	| aList |
 
-	aList _ self changedMessageListAugmented select:
-		[:msg |  Utilities setClassAndSelectorFrom: msg in: 
-				[:cl :sl | aClass _ cl.  aSelector _ sl].
-			(VersionsBrowser versionCountForSelector: aSelector class: aClass) > 1].
-	aList size > 0 ifFalse: [self inform: 'None!'.  ^ nil].
-	Smalltalk browseMessageList: aList name: (self name, ' methods that have prior versions')
+	aList _ self 
+		messageListForChangesWhich: [ :aClass :aSelector |
+			(VersionsBrowser versionCountForSelector: aSelector class: aClass) > 1
+		]
+		ifNone: [^self inform: 'None!'].
+	self systemNavigation 
+		browseMessageList: aList 
+		name: self name, ' methods that have prior versions'
