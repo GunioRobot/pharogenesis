dc20457978d417c67f7b2825efc22d25c3429382
sd <unknown>
2003-04-16 08:52:00 +0000

    no message

---
+++ ClassCommentVersionsBrowser/instance/menu/openSingleMessageBrowser.cs
@@ -0,0 +1,12 @@
+openSingleMessageBrowser
+	| mr |
+	"Create and schedule a message list browser populated only by the currently selected message"
+
+	mr _ MethodReference new
+				setStandardClass: self selectedClass
+				methodSymbol: #Comment.
+
+	self systemNavigation 
+		browseMessageList: (Array with: mr)
+		name: mr asStringOrText
+		autoSelect: nil
