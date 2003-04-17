8c20d61b5a451ab0795091c0d3d9e30550d82433
sd <unknown>
2003-04-17 21:01:00 +0000

    no message

--- SystemDictionary/instance/housekeeping/verifyChanges.cs
+++ SystemDictionary/instance/housekeeping/verifyChanges.cs
@@ -1,3 +1,3 @@
 verifyChanges		"Smalltalk verifyChanges"
 	"Recompile all methods in the changes file."
-	Smalltalk allBehaviorsDo: [:class | class recompileChanges].
+	self systemNavigation allBehaviorsDo: [:class | class recompileChanges].
