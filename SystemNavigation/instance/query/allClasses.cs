e7f98ccfec313d51bfd12a95d65dd96b8af50e9b
sd <unknown>
2003-04-17 21:31:00 +0000

    no message

---
+++ SystemNavigation/instance/query/allClasses.cs
@@ -0,0 +1,7 @@
+allClasses
+	"currently returns all the classes defined in Smalltalk but could be customized 
+	for dealing with environments and in such a case would return on really all the classes"
+
+	^ Smalltalk allClasses
+
+	
