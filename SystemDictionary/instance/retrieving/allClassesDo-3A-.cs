f4655de829d7ecc49bb340b5a07875c3900b3f01
sd <unknown>
2003-04-17 21:18:00 +0000

    no message

--- SystemDictionary/instance/retrieving/allClassesDo-3A-.cs
+++ SystemDictionary/instance/retrieving/allClassesDo-3A-.cs
@@ -1,4 +1,4 @@
 allClassesDo: aBlock
 	"Evaluate the argument, aBlock, for each class in the system."
 
-	(self classNames collect: [:name | Smalltalk at: name]) do: aBlock
+	(self classNames collect: [:name | self at: name]) do: aBlock
