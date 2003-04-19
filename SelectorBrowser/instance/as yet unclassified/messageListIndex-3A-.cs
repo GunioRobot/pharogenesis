1f0c4b26afcc1d1b213d73854e8f53c8e67a3046
sd <unknown>
2003-04-19 12:13:00 +0000

    no message

--- SelectorBrowser/instance/as yet unclassified/messageListIndex-3A-.cs
+++ SelectorBrowser/instance/as yet unclassified/messageListIndex-3A-.cs
@@ -1,11 +1,12 @@
 messageListIndex: anInteger 
-	"Set the selected message selector to be the one indexed by anInteger.  Find all classes it is in."
-
+	"Set the selected message selector to be the one indexed by anInteger. 
+	Find all classes it is in."
 	selectorIndex _ anInteger.
-	selectorIndex = 0 ifTrue: [^ self].
-
-	classList _ Smalltalk allImplementorsOf: self selectedMessageName.
+	selectorIndex = 0
+		ifTrue: [^ self].
+	classList _ self systemNavigation allImplementorsOf: self selectedMessageName.
 	self markMatchingClasses.
 	classListIndex _ 0.
-	self changed: #messageListIndex.		"update my selection"
-	self changed: #classList.
+	self changed: #messageListIndex.
+	"update my selection"
+	self changed: #classList
