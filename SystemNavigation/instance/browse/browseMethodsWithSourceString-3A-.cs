45bce1b35e7020c52ae279cc39b5a86a5fab2f8e
sd <unknown>
2003-04-20 14:11:00 +0000

    no message

---
+++ SystemNavigation/instance/browse/browseMethodsWithSourceString-3A-.cs
@@ -0,0 +1,12 @@
+browseMethodsWithSourceString: aString 
+	"SystemNavigation new browseMethodsWithSourceString: 'SourceString'"
+	"Launch a browser on all methods whose source code contains aString as 
+	a substring."
+	| caseSensitive suffix |
+	suffix _ (caseSensitive _ Sensor shiftPressed)
+				ifTrue: [' (case-sensitive)']
+				ifFalse: [' (use shift for case-sensitive)'].
+	^ self
+		browseMessageList: (self allMethodsWithSourceString: aString matchCase: caseSensitive)
+		name: 'Methods containing ' , aString printString , suffix
+		autoSelect: aString
