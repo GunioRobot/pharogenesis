bac165bfb14ae4375eb3a5ddbe469f399f5860a2
sd <unknown>
2003-04-29 19:10:00 +0000

    no message

---
+++ SystemNavigation/instance/query/allSentMessagesWithout-3A-.cs
@@ -0,0 +1,38 @@
+allSentMessagesWithout: classesAndMessagesPair 
+	"Answer the set of selectors which are sent somewhere in the system,  
+	computed in the absence of the supplied classes and messages."
+	| sent absentClasses absentSelectors |
+	sent _ IdentitySet new: CompiledMethod instanceCount.
+	absentClasses _ classesAndMessagesPair first.
+	absentSelectors _ classesAndMessagesPair second.
+	self flag: #shouldBeRewrittenUsingSmalltalkAllClassesDo:.
+	"sd 29/04/03"
+	Cursor execute
+		showWhile: [Smalltalk classNames
+				do: [:cName | ((absentClasses includes: cName)
+						ifTrue: [{}]
+						ifFalse: [{Smalltalk at: cName. (Smalltalk at: cName) class}])
+						do: [:cl | (absentSelectors isEmpty
+								ifTrue: [cl selectors]
+								ifFalse: [cl selectors copyWithoutAll: absentSelectors])
+								do: [:sel | "Include all sels, but not if sent by self"
+									(cl compiledMethodAt: sel) literals
+										do: [:m | 
+											(m isMemberOf: Symbol)
+												ifTrue: ["might be sent"
+													m == sel
+														ifFalse: [sent add: m]].
+											(m isMemberOf: Array)
+												ifTrue: ["might be performed"
+													m
+														do: [:x | (x isMemberOf: Symbol)
+																ifTrue: [x == sel
+																		ifFalse: [sent add: x]]]]]]]].
+			"The following may be sent without being in any literal frame"
+			1
+				to: Smalltalk specialSelectorSize
+				do: [:index | sent
+						add: (Smalltalk specialSelectorAt: index)]].
+	Smalltalk presumedSentMessages
+		do: [:sel | sent add: sel].
+	^ sent
