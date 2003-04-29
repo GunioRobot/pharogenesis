c3066857748d80bb7405059391b83cf8b92ad0d0
sd <unknown>
2003-04-29 12:23:00 +0000

    no message

---
+++ SystemNavigation/instance/browse/browseAllCallsOn-3A-.cs
@@ -0,0 +1,12 @@
+browseAllCallsOn: aLiteral 
+	"Create and schedule a message browser on each method that refers to 
+	aLiteral. For example, SystemNavigation new browseAllCallsOn: #open:label:."
+	(aLiteral isKindOf: LookupKey)
+		ifTrue: [^ self
+				browseMessageList: (self allCallsOn: aLiteral) asSortedCollection
+				name: 'Users of ' , aLiteral key
+				autoSelect: aLiteral key].
+	self
+		browseMessageList: (self allCallsOn: aLiteral) asSortedCollection
+		name: 'Senders of ' , aLiteral
+		autoSelect: aLiteral keywords first
