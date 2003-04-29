043fd34b0599a5263ed9ce8358370b7282754b9f
sd <unknown>
2003-04-29 20:41:00 +0000

    no message

---
+++ SystemNavigation/instance/query/allMethodsSelect-3A-.cs
@@ -0,0 +1,13 @@
+allMethodsSelect: aBlock 
+	"Answer a SortedCollection of each method that, when used as the block  
+	argument to aBlock, gives a true result."
+	| aCollection |
+	aCollection _ SortedCollection new.
+	Cursor execute
+		showWhile: [self
+				allBehaviorsDo: [:class | class
+						selectorsDo: [:sel | (aBlock
+									value: (class compiledMethodAt: sel))
+								ifTrue: [aCollection
+										add: (MethodReference new setStandardClass: class methodSymbol: sel)]]]].
+	^ aCollection
