6c5ea99074e5afdd238f65f130f791bebf329e4f
sd <unknown>
2003-04-29 20:19:00 +0000

    no message

--- ChangeSet/instance/fileIn-2F-Out/checkForUnsentMessages.cs
+++ ChangeSet/instance/fileIn-2F-Out/checkForUnsentMessages.cs
@@ -1,24 +1,18 @@
 checkForUnsentMessages
-	"Check the change set for unsent messages, and if any are found, open up a message-list browser on them"
-
-	| nameLine allChangedSelectors augList unsent messageList |
-	nameLine _ '"', self name, '"'.
+	"Check the change set for unsent messages, and if any are found, open 
+	up a message-list browser on them"
+	| nameLine allChangedSelectors augList unsent |
+	nameLine _ '"' , self name , '"'.
 	allChangedSelectors _ Set new.
-	(augList _ self changedMessageListAugmented) do:
-		[:aChange |
-			MessageSet parse: aChange toClassAndSelector: [:cls :sel | (cls notNil and:
-				[cls includesSelector: sel]) ifTrue: [allChangedSelectors add: sel]]].
-
-	unsent _ Smalltalk allUnSentMessagesIn: allChangedSelectors.
+	(augList _ self changedMessageListAugmented)
+		do: [:each | each isValid
+				ifTrue: [allChangedSelectors add: each methodSymbol]].
+	unsent _ self systemNavigation allUnSentMessagesIn: allChangedSelectors.
 	unsent size = 0
-		ifTrue:
-			[self inform: 'There are no unsent 
+		ifTrue: [^ self inform: 'There are no unsent 
 messages in change set
-', nameLine]
-		ifFalse:
-			[messageList _ augList select:
-				[:aChange |
-					MessageSet parse: aChange toClassAndSelector:
-						[:cls :sel | unsent includes: sel]].
-
-			Smalltalk browseMessageList: messageList name: 'Unsent messages in ', nameLine]
+' , nameLine].
+	self systemNavigation
+		browseMessageList: (augList
+				select: [:each | unsent includes: each methodSymbol])
+		name: 'Unsent messages in ' , nameLine
