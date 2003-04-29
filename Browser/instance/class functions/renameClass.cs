fa8d8e97a955a798d35e1fd2efa2c3603539247c
sd <unknown>
2003-04-29 11:49:00 +0000

    no message

--- Browser/instance/class functions/renameClass.cs
+++ Browser/instance/class functions/renameClass.cs
@@ -1,19 +1,28 @@
 renameClass
 	| oldName newName obs |
-	classListIndex = 0 ifTrue: [^ self].
-	self okToChange ifFalse: [^ self].
+	classListIndex = 0
+		ifTrue: [^ self].
+	self okToChange
+		ifFalse: [^ self].
 	oldName _ self selectedClass name.
 	newName _ self request: 'Please type new class name' initialAnswer: oldName.
-	newName = '' ifTrue: [^ self].  " Cancel returns '' "
+	newName = ''
+		ifTrue: [^ self].
+	"Cancel returns ''"
 	newName _ newName asSymbol.
-	newName = oldName ifTrue: [^ self].
+	newName = oldName
+		ifTrue: [^ self].
 	(Smalltalk includesKey: newName)
 		ifTrue: [^ self error: newName , ' already exists'].
 	self selectedClass rename: newName.
 	self changed: #classList.
-	self classListIndex: ((systemOrganizer listAtCategoryNamed: self selectedSystemCategoryName) indexOf: newName).
-	obs _ Smalltalk allCallsOn: (Smalltalk associationAt: newName).
-	obs isEmpty ifFalse:
-		[Smalltalk browseMessageList: obs
-			name: 'Obsolete References to ' , oldName
-			autoSelect: oldName].
+	self
+		classListIndex: ((systemOrganizer listAtCategoryNamed: self selectedSystemCategoryName)
+				indexOf: newName).
+	obs _ self systemNavigation
+				allCallsOn: (Smalltalk associationAt: newName).
+	obs isEmpty
+		ifFalse: [self systemNavigation
+				browseMessageList: obs
+				name: 'Obsolete References to ' , oldName
+				autoSelect: oldName]
