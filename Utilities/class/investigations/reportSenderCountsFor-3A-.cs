662c62b644b316409ab9c60a907905b0957d852e
sd <unknown>
2003-04-29 11:59:00 +0000

    no message

--- Utilities/class/investigations/reportSenderCountsFor-3A-.cs
+++ Utilities/class/investigations/reportSenderCountsFor-3A-.cs
@@ -1,16 +1,17 @@
-reportSenderCountsFor: selectorList
-	"Produce a report on the number of senders of each of the selectors in the list.  1/27/96 sw"
-
+reportSenderCountsFor: selectorList 
+	"Produce a report on the number of senders of each of the selectors in 
+	the list. 1/27/96 sw"
 	| total report thisSize |
 	total _ 0.
 	report _ '
 '.
-	selectorList do:
-		[:selector | thisSize _ (Smalltalk allCallsOn: selector) size.
-		report _ report, thisSize printString, String tab, selector printString, String cr.
-		total _ total + thisSize].
-	report _ report, '--- ------------------
+	selectorList
+		do: [:selector | 
+			thisSize _ (self systemNavigation allCallsOn: selector) size.
+			report _ report , thisSize printString , String tab , selector printString , String cr.
+			total _ total + thisSize].
+	report _ report , '--- ------------------
 '.
-	report _ report, total printString, String tab, 'TOTAL
+	report _ report , total printString , String tab , 'TOTAL
 '.
 	^ report
