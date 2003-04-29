3a5b19fa84d31ec913ec6235c50ee5b03bc94444
sd <unknown>
2003-04-29 15:17:00 +0000

    no message

---
+++ SystemNavigation/instance/query/selectAllMethodsNoDoits-3A-.cs
@@ -0,0 +1,13 @@
+selectAllMethodsNoDoits: aBlock 
+	"Like allSelect:, but strip out Doits"
+	| aCollection |
+	aCollection _ SortedCollection new.
+	Cursor execute
+		showWhile: [self
+				allBehaviorsDo: [:class | class
+						selectorsDo: [:sel | (sel ~~ #DoIt
+									and: [aBlock
+											value: (class compiledMethodAt: sel)])
+								ifTrue: [aCollection
+										add: (MethodReference new setStandardClass: class methodSymbol: sel)]]]].
+	^ aCollection
