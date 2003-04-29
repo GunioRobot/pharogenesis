f23d13c9dc450c764ec171dea79c78eb6f6bcbfe
sd <unknown>
2003-04-29 20:20:00 +0000

    no message

--- StringHolder/instance/message list menu/browseUnusedMethods.cs
+++ StringHolder/instance/message list menu/browseUnusedMethods.cs
@@ -1,12 +1,15 @@
 browseUnusedMethods
 	| classes unsent messageList cls |
-
-	(cls _ self selectedClass) ifNil: [^ self].
+	(cls _ self selectedClass)
+		ifNil: [^ self].
 	classes _ Array with: cls with: cls class.
 	unsent _ Set new.
-	classes do: [:c | unsent addAll: c selectors].
-	unsent _ Smalltalk allUnSentMessagesIn: unsent.
+	classes
+		do: [:c | unsent addAll: c selectors].
+	unsent _ self systemNavigation allUnSentMessagesIn: unsent.
 	messageList _ OrderedCollection new.
-	classes do: [:c | (c selectors select: [:s | unsent includes: s]) asSortedCollection
-					do: [:sel | messageList add: c name , ' ' , sel]].
-	Smalltalk browseMessageList: messageList name: 'Unsent Methods in ', cls name
+	classes
+		do: [:c | (c selectors
+				select: [:s | unsent includes: s]) asSortedCollection
+				do: [:sel | messageList add: c name , ' ' , sel]].
+	self systemNavigation browseMessageList: messageList name: 'Unsent Methods in ' , cls name
