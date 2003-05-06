c9d4d51ce8fe8facc2f07d385c34768b0e876793
tpr <unknown>
2003-05-06 14:05:00 +0000

    no message

--- Browser/instance/message list/selectedMessageName.cs
+++ Browser/instance/message list/selectedMessageName.cs
@@ -1,8 +1,8 @@
 selectedMessageName
-	| aList |
 	"Answer the message selector of the currently selected message, if any. 
 	Answer nil otherwise."
 
+	| aList |
 	messageListIndex = 0 ifTrue: [^ nil].
 	^ (aList _ self messageList) size >= messageListIndex
 		ifTrue:
