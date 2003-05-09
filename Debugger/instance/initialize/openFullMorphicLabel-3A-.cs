b8d963e73f2ea906b0b4f47fc9379a9fc8d2a3dc
tk <unknown>
2003-05-09 11:07:00 +0000

    no message

--- Debugger/instance/initialize/openFullMorphicLabel-3A-.cs
+++ Debugger/instance/initialize/openFullMorphicLabel-3A-.cs
@@ -1,18 +1,19 @@
-openFullMorphicLabel: labelString
-	| window aListMorph oldContextStackIndex |
+openFullMorphicLabel: aLabelString
+	"Open a full morphic debugger with the given label"
 
+	| window aListMorph oldContextStackIndex |
 	oldContextStackIndex _ contextStackIndex.
 	self expandStack. "Sets contextStackIndex to zero."
 
-	window _ (SystemWindow labelled: labelString) model: self.
+	window _ (SystemWindow labelled: aLabelString) model: self.
 	aListMorph _ PluggableListMorph on: self list: #contextStackList
 			selected: #contextStackIndex changeSelected: #toggleContextStackIndex:
 			menu: #contextStackMenu:shifted: keystroke: #contextStackKey:from:.
 	aListMorph menuTitleSelector: #messageListSelectorTitle.
 	window addMorph: aListMorph
-		frame: (0@0 corner: 1@0.3).
+		frame: (0@0 corner: 1@0.25).
 
-	self addLowerPanesTo: window at: (0@0.3 corner: 1@0.7) with: nil.
+	self addLowerPanesTo: window at: (0@0.25 corner: 1@0.8) with: nil.
 
 	window addMorph: (
 		PluggableListMorph new
@@ -21,11 +22,11 @@ openFullMorphicLabel: labelString
 			on: self receiverInspector list: #fieldList
 			selected: #selectionIndex changeSelected: #toggleIndex:
 			menu: #fieldListMenu: keystroke: #inspectorKey:from:)
-		frame: (0@0.7 corner: 0.2@1).
+		frame: (0@0.8 corner: 0.2@1).
 	window addMorph: (PluggableTextMorph on: self receiverInspector
 			text: #contents accept: #accept:
 			readSelection: #contentsSelection menu: #codePaneMenu:shifted:)
-		frame: (0.2@0.7 corner: 0.5@1).
+		frame: (0.2@0.8 corner: 0.5@1).
 	window addMorph: (
 		PluggableListMorph new
 			doubleClickSelector: #inspectSelection;
@@ -33,11 +34,11 @@ openFullMorphicLabel: labelString
 			on: self contextVariablesInspector list: #fieldList
 			selected: #selectionIndex changeSelected: #toggleIndex:
 			menu: #fieldListMenu: keystroke: #inspectorKey:from:)
-		frame: (0.5@0.7 corner: 0.7@1).
+		frame: (0.5@0.8 corner: 0.7@1).
 	window addMorph: (PluggableTextMorph on: self contextVariablesInspector
 			text: #contents accept: #accept:
 			readSelection: #contentsSelection menu: #codePaneMenu:shifted:)
-		frame: (0.7@0.7 corner: 1@1).
+		frame: (0.7@0.8 corner: 1@1).
 	window openInWorld.
 	self toggleContextStackIndex: oldContextStackIndex.
 	^ window 
