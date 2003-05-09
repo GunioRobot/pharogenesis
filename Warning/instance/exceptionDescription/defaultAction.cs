d1cf5eecb5f40651161ee2f8b27bed2d1e4cdd2f
brp <unknown>
2003-05-09 12:54:00 +0000

    no message

--- Warning/instance/exceptionDescription/defaultAction.cs
+++ Warning/instance/exceptionDescription/defaultAction.cs
@@ -1,6 +1,8 @@
 defaultAction
 	"The user should be notified of the occurrence of an exceptional occurrence and given an option of continuing or aborting the computation. The description of the occurrence should include any text specified as the argument of the #signal: message."
 
-	| continue |
-	continue := self confirm: 'Warning: ', self messageText, ' Continue?'.
-	continue ifTrue: [self resume]
+	Debugger
+		openContext: thisContext
+		label: 'Warning'
+		contents: self messageText, '\\Select Proceed to continue, or close this window to cancel the operation.' withCRs.
+	self resume
