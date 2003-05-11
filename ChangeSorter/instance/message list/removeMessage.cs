e59d8489c44d434ac425c1d9cee407670a8e98be
sd <unknown>
2003-05-11 18:38:00 +0000

    no message

--- ChangeSorter/instance/message list/removeMessage.cs
+++ ChangeSorter/instance/message list/removeMessage.cs
@@ -1,13 +1,14 @@
 removeMessage
-	"Remove the selected msg from the system.  Real work done by the parent, a ChangeSorter"
-
+	"Remove the selected msg from the system. Real work done by the 
+	parent, a ChangeSorter"
 	| confirmation sel |
-	self okToChange ifFalse: [^ self].
-	currentSelector ifNotNil: [
-		confirmation _ self selectedClassOrMetaClass 
-			confirmRemovalOf: (sel _ self selectedMessageName).
-		confirmation == 3 ifTrue: [^ self].
-		self selectedClassOrMetaClass removeSelector: sel.
-		self update.
-		confirmation == 2 ifTrue:
-			[Smalltalk browseAllCallsOn: sel]]
+	self okToChange
+		ifFalse: [^ self].
+	currentSelector
+		ifNotNil: [confirmation _ self systemNavigation   confirmRemovalOf: (sel _ self selectedMessageName) on: self selectedClassOrMetaClass.
+			confirmation == 3
+				ifTrue: [^ self].
+			self selectedClassOrMetaClass removeSelector: sel.
+			self update.
+			confirmation == 2
+				ifTrue: [self systemNavigation browseAllCallsOn: sel]]
