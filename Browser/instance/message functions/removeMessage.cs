a12bb7c6581737b93e7db37c9a4f6c0fa12e4640
sd <unknown>
2003-05-11 21:01:00 +0000

    no message

--- Browser/instance/message functions/removeMessage.cs
+++ Browser/instance/message functions/removeMessage.cs
@@ -1,20 +1,22 @@
 removeMessage
-	"If a message is selected, create a Confirmer so the user can verify that 
-	the currently selected message should be removed from the system. If so, 
-	remove it.  If the Preference 'confirmMethodRemoves' is set to false, the 
+	"If a message is selected, create a Confirmer so the user can verify that  
+	the currently selected message should be removed from the system. If 
+	so,  
+	remove it. If the Preference 'confirmMethodRemoves' is set to false, the 
 	confirmer is bypassed."
 	| messageName confirmation |
-
-	messageListIndex = 0 ifTrue: [^ self].
-	self okToChange ifFalse: [^ self].
+	messageListIndex = 0
+		ifTrue: [^ self].
+	self okToChange
+		ifFalse: [^ self].
 	messageName _ self selectedMessageName.
-	confirmation _ self selectedClassOrMetaClass confirmRemovalOf: messageName.
-	confirmation == 3 ifTrue: [^ self].
-
+	confirmation _ self systemNavigation   confirmRemovalOf: messageName on: self selectedClassOrMetaClass.
+	confirmation == 3
+		ifTrue: [^ self].
 	self selectedClassOrMetaClass removeSelector: self selectedMessageName.
 	self messageListIndex: 0.
 	self changed: #messageList.
-	self setClassOrganizer.  "In case organization not cached"
-
-	confirmation == 2 ifTrue:
-		[Smalltalk browseAllCallsOn: messageName]
+	self setClassOrganizer.
+	"In case organization not cached"
+	confirmation == 2
+		ifTrue: [self systemNavigation browseAllCallsOn: messageName]
