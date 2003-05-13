72b39745f3b1eb0a672d5e2680b3647c706f2c7d
mir <unknown>
2003-05-13 10:43:00 +0000

    no message

---
+++ HTTPClient/class/private/pluginHttpPostMultipart-3A-args-3A-.cs
@@ -0,0 +1,37 @@
+pluginHttpPostMultipart: url args: argsDict
+	| mimeBorder argsStream crLf fieldValue resultStream result |
+	" do multipart/form-data encoding rather than x-www-urlencoded "
+
+	crLf _ String crlf.
+	mimeBorder _ '----squeak-', Time millisecondClockValue printString, '-stuff-----'.
+	"encode the arguments dictionary"
+	argsStream _ WriteStream on: String new.
+	argsDict associationsDo: [:assoc |
+		assoc value do: [ :value |
+		"print the boundary"
+		argsStream nextPutAll: '--', mimeBorder, crLf.
+		" check if it's a non-text field "
+		argsStream nextPutAll: 'Content-disposition: form-data; name="', assoc key, '"'.
+		(value isKindOf: MIMEDocument)
+			ifFalse: [fieldValue _ value]
+			ifTrue: [argsStream nextPutAll: ' filename="', value url pathForFile, '"', crLf, 'Content-Type: ', value contentType.
+				fieldValue _ (value content
+					ifNil: [(FileStream fileNamed: value url pathForFile) contentsOfEntireFile]
+					ifNotNil: [value content]) asString].
+" Transcript show: 'field=', key, '; value=', fieldValue; cr. "
+		argsStream nextPutAll: crLf, crLf, fieldValue, crLf.
+	]].
+	argsStream nextPutAll: '--', mimeBorder, '--'.
+	resultStream _ FileStream
+		post: 
+			('ACCEPT: text/html', crLf,
+			'User-Agent: Squeak 3.1', crLf,
+			'Content-type: multipart/form-data; boundary=', mimeBorder, crLf,
+			'Content-length: ', argsStream contents size printString, crLf, crLf, 
+			argsStream contents)
+		url: url ifError: [^'Error in post ' url toText].
+	"get the header of the reply"
+	result _ resultStream
+		ifNil: ['']
+		ifNotNil: [resultStream upToEnd].
+	^MIMEDocument content: result
