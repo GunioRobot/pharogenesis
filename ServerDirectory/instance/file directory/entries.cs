0ccb6393bc4493b81097d9129f7540976efe8f0c
mir <unknown>
2003-05-13 10:44:00 +0000

    no message

--- ServerDirectory/instance/file directory/entries.cs
+++ ServerDirectory/instance/file directory/entries.cs
@@ -1,6 +1,6 @@
 entries 
 	"Return a collection of directory entries for the files and directories in this directory. Each entry is a five-element array: (<name> <creationTime> <modificationTime> <dirFlag> <fileSize>)."
-	| dir ftpEntries thisYear tokens dateInSeconds longy thisMonth |
+	| dir ftpEntries |
 	"We start with ftp directory entries of the form...
 d---------   1 owner    group               0 Apr 27 22:01 blasttest
 ----------   1 owner    group           93812 Jul 21  1997 COMMAND.COM
@@ -12,29 +12,8 @@ d---------   1 owner    group               0 Apr 27 22:01 blasttest
 
 	dir _ self getDirectory.
 	(dir respondsTo: #contentsOfEntireFile) ifFalse: [^ #()].
-	ftpEntries _ dir contentsOfEntireFile findTokens: FTPSocket crLf.
+	ftpEntries _ dir contentsOfEntireFile findTokens: String crlf.
 "ftpEntries inspect."
-	thisYear _ Date today year.
-	thisMonth _ Date today monthIndex.
-	^ ftpEntries collect:
-		[:ftpEntry | tokens _ ftpEntry findTokens: ' '.
-		tokens size >= 9
-		ifTrue:
-		[tokens size > 9 ifTrue:
-			[longy _ tokens at: 9.
-			10 to: tokens size do: [:i | longy _ longy , ' ' , (tokens at: i)].
-			tokens at: 9 put: longy].
-		dateInSeconds _ self
-			secondsForDay: (tokens at: 7) 
-			month: (tokens at: 6) 
-			yearOrTime: (tokens at: 8) 
-			thisMonth: thisMonth 
-			thisYear: thisYear.
-		DirectoryEntry name: (tokens at: 9)  "file name"
-			creationTime: dateInSeconds "creation date"
-			modificationTime: dateInSeconds "modification time"
-			isDirectory: tokens first first = $d "is-a-directory flag"
-			fileSize: tokens fifth asNumber "file size"]
-		ifFalse:
-		[nil]]
+	^ ftpEntries 
+		collect:[:ftpEntry | self class parseFTPEntry: ftpEntry]
 		thenSelect: [:entry | entry notNil]
