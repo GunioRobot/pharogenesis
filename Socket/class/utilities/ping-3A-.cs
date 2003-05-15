e12da441b2d4b3e7439859a172abad63de44aecf
mir <unknown>
2003-05-15 18:30:00 +0000

    no message

--- Socket/class/utilities/ping-3A-.cs
+++ Socket/class/utilities/ping-3A-.cs
@@ -4,26 +4,28 @@ ping: hostName
 
 	| tcpPort sock serverAddr startTime echoTime |
 	tcpPort _ 7.  "7 = echo port, 13 = time port, 19 = character generator port"
-	Socket initializeNetwork.
+
 	serverAddr _ NetNameResolver addressForName: hostName timeout: 10.
 	serverAddr = nil ifTrue: [
 		^ self inform: 'Could not find an address for ', hostName].
 
 	sock _ Socket new.
-	sock connectTo: serverAddr port: tcpPort.
-	[sock waitForConnectionUntil: (Socket deadlineSecs: 10).
-	 sock isConnected] whileFalse: [
-		(self confirm: 'Continue to wait for connection to ', hostName, '?') ifFalse: [
-			sock destroy.
-			^ self]].
+	sock connectNonBlockingTo: serverAddr port: tcpPort.
+	[sock waitForConnectionFor: 10]
+		on: ConnectionTimedOut
+		do: [:ex |
+			(self confirm: 'Continue to wait for connection to ', hostName, '?')
+				ifTrue: [ex retry]
+				ifFalse: [
+					sock destroy.
+					^ self]].
 
 	sock sendData: 'echo!'.
 	startTime _ Time millisecondClockValue.
-	[sock waitForDataUntil: (Socket deadlineSecs: 15).
-	 sock dataAvailable] whileFalse: [
-		(self confirm: 'Packet sent but no echo yet; keep waiting?') ifFalse: [
-			sock destroy.
-			^ self]].
+	[sock waitForDataFor: 15]
+		on: ConnectionTimedOut
+		do: [:ex | (self confirm: 'Packet sent but no echo yet; keep waiting?')
+			ifTrue: [ex retry]].
 	echoTime _ Time millisecondClockValue - startTime.
 
 	sock destroy.
