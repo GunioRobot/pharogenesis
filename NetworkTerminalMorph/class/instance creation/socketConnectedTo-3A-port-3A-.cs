10ac3701651323267ebb7f37b0f55feacb7610cf
mir <unknown>
2003-05-15 18:06:00 +0000

    no message

--- NetworkTerminalMorph/class/instance creation/socketConnectedTo-3A-port-3A-.cs
+++ NetworkTerminalMorph/class/instance creation/socketConnectedTo-3A-port-3A-.cs
@@ -4,8 +4,8 @@ socketConnectedTo: serverHost  port: serverPort
 
 	Socket initializeNetwork.
 	sock _ Socket new.
-	sock connectTo: (NetNameResolver addressForName: serverHost) port: serverPort.
-	sock waitForConnectionUntil: Socket standardDeadline.
-	sock isConnected ifFalse: [ self error: 'could not connect to server' ].
+	[sock connectTo: (NetNameResolver addressForName: serverHost) port: serverPort]
+		on: ConnectionTimedOut
+		do: [:ex | self error: 'could not connect to server' ].
 	^StringSocket on: sock
 
