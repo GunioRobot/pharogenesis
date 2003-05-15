a4e3b46d874b78acadd132d474f83afeaa74e03f
mir <unknown>
2003-05-15 18:29:00 +0000

    no message

--- Socket/instance/connection open-2F-close/connectTo-3A-port-3A-.cs
+++ Socket/instance/connection open-2F-close/connectTo-3A-port-3A-.cs
@@ -1,9 +1,5 @@
 connectTo: hostAddress port: port
-	"Initiate a connection to the given port at the given host address. This operation will return immediately; follow it with waitForConnectionUntil: to wait until the connection is established."
+	"Initiate a connection to the given port at the given host address.
+	Waits until the connection is established or time outs."
 
-	| status |
-	status _ self primSocketConnectionStatus: socketHandle.
-	(status == Unconnected)
-		ifFalse: [self error: 'Socket status must Unconnected before opening a new connection'].
-
-	self primSocket: socketHandle connectTo: hostAddress port: port.
+	self connectTo: hostAddress port: port waitForConnectionFor: Socket standardTimeout
