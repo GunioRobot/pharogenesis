a2701e9a51c50edafc77721480016084c2a2260a
mir <unknown>
2003-05-15 15:40:00 +0000

    no message

--- EToyPeerToPeer/instance/receiving/doReceiveOneMessage.cs
+++ EToyPeerToPeer/instance/receiving/doReceiveOneMessage.cs
@@ -5,7 +5,7 @@ doReceiveOneMessage
 	awaitingLength _ true.
 	answer _ WriteStream on: String new.
 	[awaitingLength] whileTrue: [
-		leftOverData _ leftOverData,socket getData.
+		leftOverData _ leftOverData , socket receiveData.
 		(i _ leftOverData indexOf: $ ) > 0 ifTrue: [
 			awaitingLength _ false.
 			length _ (leftOverData first: i - 1) asNumber.
@@ -14,7 +14,7 @@ doReceiveOneMessage
 	].
 	leftOverData _ ''.
 	[answer size < length] whileTrue: [
-		answer nextPutAll: socket getData.
+		answer nextPutAll: socket receiveData.
 		communicatorMorph commResult: {#commFlash -> true}.
 	].
 	answer _ answer contents.
