deacb409db699db0fc5e7ac2fe6a5c6e9d57a66d
mir <unknown>
2003-05-15 18:28:00 +0000

    no message

--- Socket/class/utilities/deadlineSecs-3A-.cs
+++ Socket/class/utilities/deadlineSecs-3A-.cs
@@ -1,4 +1,4 @@
 deadlineSecs: secs
 	"Return a deadline time the given number of seconds from now."
 
-	^ Time millisecondClockValue + (secs * 1000)
+	^ Time millisecondClockValue + (secs * 1000) truncated
