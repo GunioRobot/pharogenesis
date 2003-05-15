0ba32c21ed558848d89be7e8acf743183c0b61ca
mir <unknown>
2003-05-15 18:33:00 +0000

    no message

---
+++ Socket/instance/waiting/waitForSendDoneFor-3A-.cs
@@ -0,0 +1,11 @@
+waitForSendDoneFor: timeout
+	"Wait up until the given deadline for the current send operation to complete. Return true if it completes by the deadline, false if not."
+
+	| sendDone deadline |
+	deadline := Socket deadlineSecs: timeout.
+	[self isConnected & (sendDone _ self primSocketSendDone: socketHandle) not
+			"Connection end and final data can happen fast, so test in this order"
+		and: [Time millisecondClockValue < deadline]] whileTrue: [
+			self writeSemaphore waitTimeoutMSecs: (deadline - Time millisecondClockValue)].
+
+	^ sendDone
