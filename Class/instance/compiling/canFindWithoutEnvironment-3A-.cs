7844c91ff7b7f9f61ea89d49ff7f9e04f6c3779a
ar <unknown>
2003-05-17 14:13:00 +0000

    no message

--- Class/instance/compiling/canFindWithoutEnvironment-3A-.cs
+++ Class/instance/compiling/canFindWithoutEnvironment-3A-.cs
@@ -2,22 +2,16 @@ canFindWithoutEnvironment: varName
 	"This method is used for analysis of system structure -- see senders."
 	"Look up varName, in the context of the receiver. Return true if it can be found without using the declared environment."
 
-	| assoc |
 	"First look in classVar dictionary."
-	(assoc _ self classPool associationAt: varName ifAbsent: []) == nil
-		ifFalse: [^ true].
+	(self classPool bindingOf: varName) ifNotNil:[^true].
 
 	"Next look in shared pools."
-	self sharedPools do: 
-		[:pool | 
-		assoc _ pool associationAt: varName ifAbsent: [
-			"Hideous string key hack from Hypersqueak now used in Wonderland"
-			pool associationAt: varName asString ifAbsent: []].
-		assoc == nil ifFalse: 
-				[^ true]].
+	self sharedPools do:[:pool | 
+		(pool bindingOf: varName) ifNotNil:[^true].
+	].
 
 	"Finally look higher up the superclass chain and fail at the end."
 	superclass == nil
 		ifTrue: [^ false]
-		ifFalse: [^ superclass scopeHas: varName ifTrue: [:ignored]].
+		ifFalse: [^ (superclass bindingOf: varName) notNil].
 
