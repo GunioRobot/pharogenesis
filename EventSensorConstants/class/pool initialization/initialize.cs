477984b42531b684635f88d8ea9f700257925a29
ar <unknown>
2003-05-18 18:26:00 +0000

    no message

---
+++ EventSensorConstants/class/pool initialization/initialize.cs
@@ -0,0 +1,21 @@
+initialize
+	"EventSensorConstants initialize"
+	RedButtonBit := 4.
+	BlueButtonBit := 2.
+	YellowButtonBit := 1.
+
+	ShiftKeyBit := 1.
+	CtrlKeyBit := 2.
+	OptionKeyBit := 4.
+	CommandKeyBit := 8.
+
+	"Types of events"
+	EventTypeNone := 0.
+	EventTypeMouse := 1.
+	EventTypeKeyboard := 2.
+	EventTypeDragDropFiles := 3.
+
+	"Press codes for keyboard events"
+	EventKeyChar := 0.
+	EventKeyDown := 1.
+	EventKeyUp := 2.
