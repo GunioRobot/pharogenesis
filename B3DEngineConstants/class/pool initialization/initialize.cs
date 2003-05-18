2dafaa775b8b1f29e17137dd9a5783ff859ae097
ar <unknown>
2003-05-18 19:46:00 +0000

    no message

---
+++ B3DEngineConstants/class/pool initialization/initialize.cs
@@ -0,0 +1,8 @@
+initialize
+	"B3DEngineConstants initialize"
+	self defineVBConstants.
+	self definePrimitiveVertexIndexes.
+	self defineMatrixFlags.
+	self defineClipConstants.
+	self definePrimitiveTypes.
+	self defineMaterialAndLights.
