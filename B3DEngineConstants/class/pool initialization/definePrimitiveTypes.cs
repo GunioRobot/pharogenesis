dad83539054a8dccf92049e117412a77b2622b35
ar <unknown>
2003-05-18 19:42:00 +0000

    no message

---
+++ B3DEngineConstants/class/pool initialization/definePrimitiveTypes.cs
@@ -0,0 +1,9 @@
+definePrimitiveTypes
+	"Initialize the types of Primitives"
+	PrimTypePoints := 1.
+	PrimTypeLines := 2.
+	PrimTypePolygon := 3.
+	PrimTypeIndexedLines := 4.
+	PrimTypeIndexedTriangles := 5.
+	PrimTypeIndexedQuads := 6.
+	PrimTypeMax := 6.	"Max used primitive type"
