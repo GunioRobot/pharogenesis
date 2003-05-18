8abee822bfd498e66bd6b7343f4d03f7f7728eb8
ar <unknown>
2003-05-18 18:14:00 +0000

    no message

---
+++ ChessConstants/class/pool initialization/initializePieceValues.cs
@@ -0,0 +1,8 @@
+initializePieceValues
+	PieceValues _ Array new: 6.
+	PieceValues at: Pawn put: 100.
+	PieceValues at: Knight put: 300.
+	PieceValues at: Bishop put: 350.
+	PieceValues at: Rook put: 500.
+	PieceValues at: Queen put: 900.
+	PieceValues at: King put: 2000.
