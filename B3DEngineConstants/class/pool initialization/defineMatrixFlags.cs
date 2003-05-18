b854c88b0775475453e6fc43d718d492348f1fc0
ar <unknown>
2003-05-18 19:41:00 +0000

    no message

---
+++ B3DEngineConstants/class/pool initialization/defineMatrixFlags.cs
@@ -0,0 +1,5 @@
+defineMatrixFlags
+	"Define the flags for analyzing vertices"
+	FlagM44Identity := 1.		"Matrix is identity"
+	FlagM44NoPerspective := 2. "Matrix has no perspective part"
+	FlagM44NoTranslation := 4. "Matrix has no translation"
