a6d755cd3a5f731e6e39dc744f5e629b4c03fbe7
sw <unknown>
2003-05-22 04:30:00 +0000

    no message

---
+++ ViewerLine/instance/slot/addSetterFeedback.cs
@@ -0,0 +1,8 @@
+addSetterFeedback
+	"Add screen feedback showing what would be torn off to make a setter"
+
+	| aMorph |
+	aMorph _ RectangleMorph new bounds: ((submorphs fourth topLeft - (2@1)) corner: (submorphs last bottomRight) + (2@0)).
+	aMorph useRoundedCorners; beTransparent; borderWidth: 2; borderColor: (Color r: 1.0 g: 0.548 b: 0.452); lock.
+	aMorph setProperty: #highlight toValue: true.
+	ActiveWorld addMorphFront: aMorph
