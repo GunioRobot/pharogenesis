844cd11567c0c1ee42a53478043adc9a683ee873
sd <unknown>
2003-05-23 14:52:00 +0000

    no message

---
+++ CleanKernelTest/instance/Running/tearDown.cs
@@ -0,0 +1,9 @@
+tearDown
+
+	| name |
+	self classesCreated
+		do: [:cls | 
+			name _ cls name.
+			self removeClassNamedIfExists: name.
+			ChangeSet current removeClassChanges: name].
+	classesCreated _ nil
