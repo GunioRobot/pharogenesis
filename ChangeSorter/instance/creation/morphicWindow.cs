805292433bd7afb20355d9a8e35f131cdef764ba
sd <unknown>
2003-05-23 14:25:00 +0000

    no message

--- ChangeSorter/instance/creation/morphicWindow.cs
+++ ChangeSorter/instance/creation/morphicWindow.cs
@@ -2,7 +2,7 @@ morphicWindow
 	"ChangeSorter new openAsMorph"
 	|  window |
 
-	myChangeSet ifNil: [self myChangeSet: Smalltalk changes]. 
+	myChangeSet ifNil: [self myChangeSet: ChangeSet current]. 
 	window _ (SystemWindow labelled: self labelString) model: self.
 	self openAsMorphIn: window rect: (0@0 extent: 1@1).
 	^ window
