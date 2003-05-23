2d985a174b8b9346d07a7f4a036a7d3bee7656eb
sd <unknown>
2003-05-23 14:24:00 +0000

    no message

--- ChangeList/instance/menu actions/selectConflicts.cs
+++ ChangeList/instance/menu actions/selectConflicts.cs
@@ -1,12 +1,12 @@
 selectConflicts
 	"Selects all method definitions for which there is ALSO an entry in changes"
-	| change class systemChanges |
+	| change class  |
 	Cursor read showWhile: 
 	[1 to: changeList size do:
 		[:i | change _ changeList at: i.
 		listSelections at: i put:
 			(change type = #method
 			and: [(class _ change methodClass) notNil
-			and: [(Smalltalk changes atSelector: change methodSelector
+			and: [(ChangeSet current atSelector: change methodSelector
 						class: class) ~~ #none]])]].
 	self changed: #allSelections
