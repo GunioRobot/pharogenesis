d0825107f59a33e819d863c8753d6e9a6901f6bb
sd <unknown>
2003-05-23 14:33:00 +0000

    no message

--- Class/instance/fileIn-2F-Out/removeFromChanges.cs
+++ Class/instance/fileIn-2F-Out/removeFromChanges.cs
@@ -2,4 +2,4 @@ removeFromChanges
 	"References to the receiver, a class, and its metaclass should no longer be included in the system ChangeSet.
 	7/18/96 sw: call removeClassAndMetaClassChanges:"
 
-	Smalltalk changes removeClassAndMetaClassChanges: self
+	ChangeSet current removeClassAndMetaClassChanges: self
