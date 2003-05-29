4d8a8805b116a828455af12ff8ddb0ea88d7ba0a
bf <unknown>
2003-05-29 01:38:00 +0000

    no message

--- GIFReadWriter/instance/private-2D-encoding/writeHeader.cs
+++ GIFReadWriter/instance/private-2D-encoding/writeHeader.cs
@@ -1,26 +1,34 @@
 writeHeader
 
 	| byte |
-	self nextPutAll: 'GIF87a' asByteArray.
-	self writeWord: width.	"Screen Width"
-	self writeWord: height.	"Screen Height"
-	byte _ 16r80.  "has color map"
-	byte _ byte bitOr: ((bitsPerPixel - 1) bitShift: 5).  "color resolution"
-	byte _ byte bitOr: bitsPerPixel - 1.  "bits per pixel"
-	self nextPut: byte.
-	self nextPut: 0.		"background color."
-	self nextPut: 0.		"reserved"
-	colorPalette do: [:pixelValue |
-		self	nextPut: ((pixelValue bitShift: -16) bitAnd: 255);
-			nextPut: ((pixelValue bitShift: -8) bitAnd: 255);
-			nextPut: (pixelValue bitAnd: 255)].
+	stream position = 0 ifTrue: [
+		"For first image only"
+		self nextPutAll: 'GIF89a' asByteArray.
+		self writeWord: width.	"Screen Width"
+		self writeWord: height.	"Screen Height"
+		byte _ 16r80.  "has color map"
+		byte _ byte bitOr: ((bitsPerPixel - 1) bitShift: 5).  "color resolution"
+		byte _ byte bitOr: bitsPerPixel - 1.  "bits per pixel"
+		self nextPut: byte.
+		self nextPut: 0.		"background color."
+		self nextPut: 0.		"reserved"
+		colorPalette do: [:pixelValue |
+			self	nextPut: ((pixelValue bitShift: -16) bitAnd: 255);
+				nextPut: ((pixelValue bitShift: -8) bitAnd: 255);
+				nextPut: (pixelValue bitAnd: 255)].
+		loopCount notNil ifTrue: [
+			"Write a Netscape loop chunk"
+			self nextPut: Extension.
+			self nextPutAll: #(255 11 78 69 84 83 67 65 80 69 50 46 48 3 1) asByteArray.
+			self writeWord: loopCount.
+			self nextPut: 0]].
 
-	transparentIndex ifNotNil: [
-		"write graphics control block to record transparent color index"
-		self
-			nextPut: Extension;
-			nextPutAll: (#(16rF9 4 1 0 0) as: ByteArray);
-			nextPut: transparentIndex;
+	delay notNil | transparentIndex notNil ifTrue: [
+		self nextPut: Extension;
+			nextPutAll: #(16rF9 4) asByteArray;
+			nextPut: (transparentIndex isNil ifTrue: [0] ifFalse: [9]);
+			writeWord: (delay isNil ifTrue: [0] ifFalse: [delay]);
+			nextPut: (transparentIndex isNil ifTrue: [0] ifFalse: [transparentIndex]);
 			nextPut: 0].
 
 	self nextPut: ImageSeparator.
