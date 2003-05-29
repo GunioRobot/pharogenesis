b2b6d23f06550edf8757364470d10d7f9d13d36c
tpr <unknown>
2003-05-29 20:07:00 +0000

    no message

--- ParagraphEditor/instance/explain/explainGlobal-3A-.cs
+++ ParagraphEditor/instance/explain/explainGlobal-3A-.cs
@@ -2,16 +2,16 @@ explainGlobal: symbol
 	"Is symbol a global variable?"
 	| reply classes |
 	reply _ Smalltalk at: symbol ifAbsent: [^nil].
+	(reply class == Dictionary or:[reply isKindOf: SharedPool class])
+		ifTrue: 
+			[classes _ Set new.
+			self systemNavigation allBehaviorsDo: [:each | (each sharedPools detect: [:pool | pool == reply]
+					ifNone: [])
+					~~ nil ifTrue: [classes add: each]].
+			classes _ classes printString.
+			^'"is a global variable.  It is a pool which is used by the following classes ' , (classes allButFirst: 5) , '"'].
 	(reply isKindOf: Behavior)
 		ifTrue: [^'"is a global variable.  ' , symbol , ' is a class in category ', reply category,
 			'."', '\' withCRs, 'Browser newOnClass: ' , symbol , '.'].
 	symbol == #Smalltalk ifTrue: [^'"is a global.  Smalltalk is the only instance of SystemDictionary and holds all global variables."'].
-	reply class == Dictionary
-		ifTrue: 
-			[classes _ Set new.
-			Smalltalk allBehaviorsDo: [:each | (each sharedPools detect: [:pool | pool == reply]
-					ifNone: [])
-					~~ nil ifTrue: [classes add: each]].
-			classes _ classes printString.
-			^'"is a global variable.  ' , symbol , ' is a Dictionary.  It is a pool which is used by the following classes' , (classes copyFrom: 4 to: classes size) , '"'].
 	^'"is a global variable.  ' , symbol , ' is ' , reply printString , '"'
