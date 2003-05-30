22531749ef1d78f32ec9ae067474f8e107cd078c
tpr <unknown>
2003-05-30 13:04:00 +0000

    no message

--- Behavior/instance/accessing instances and variables/allSharedPools.cs
+++ Behavior/instance/accessing instances and variables/allSharedPools.cs
@@ -1,5 +1,4 @@
 allSharedPools
-	"Answer a Set of the names of the pools (Dictionaries) that the receiver 
-	and the receiver's ancestors share."
+	"Answer a Set of the names of the pools (Dictionaries or SharedPool subclasses) that the receiver and the receiver's ancestors share."
 
 	^superclass allSharedPools
