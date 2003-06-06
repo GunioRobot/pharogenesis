6229c60f294cf2eed9a374a5f9308770cfa3a959
ls <unknown>
2003-06-06 13:22:00 +0000

    no message

--- Celeste/class/instance creation/openOn-3A-.cs
+++ Celeste/class/instance creation/openOn-3A-.cs
@@ -2,5 +2,8 @@ openOn: rootFilename
 	"Open a MailReader on the mail database with the given root filename."
 	|database |
 
-	database _  MailDB openOn: rootFilename.
-	database ifNotNil: [ ^ self openOnDatabase: database ].
+	(MailDB isADBNamed: rootFilename)
+		ifTrue: [ database _  MailDB openOn: rootFilename. ]
+		ifFalse: [ database _ nil.  "open an empty Celeste with a welcome message" ].
+
+	^self openOnDatabase: database
