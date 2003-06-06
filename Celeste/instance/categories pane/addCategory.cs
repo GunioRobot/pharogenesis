80d973c35defef9ccd8354cb142b2ce755c026f9
ls <unknown>
2003-06-06 12:47:00 +0000

    no message

--- Celeste/instance/categories pane/addCategory.cs
+++ Celeste/instance/categories pane/addCategory.cs
@@ -2,6 +2,7 @@ addCategory
 	"Create a new category with the user-specified name. This does nothing if the category already exists."
 
 	| newCatName |
+	mailDB ifNil: [ ^self ].
 	newCatName _ FillInTheBlank request: 'Name for new category?'.
 	(newCatName isEmpty) ifTrue: [^self].	"user aborted"
 
