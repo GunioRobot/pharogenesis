fe2d86ec2521c4647e0201c91b61b04f4740bafe
ls <unknown>
2003-06-06 15:41:00 +0000

    no message

--- Celeste/class/common build/openOnDatabase-3A-.cs
+++ Celeste/class/common build/openOnDatabase-3A-.cs
@@ -1,7 +1,11 @@
 openOnDatabase: aMailDB 
 	"Open a MailReader on the given mail database."
 	| model topWindow title |
-	model _ self new openOnDatabase: aMailDB.
+	self == Celeste ifTrue: [
+		"don't open the abstract class"
+		^self interfaceClass openOnDatabase: aMailDB ].
+
+	model _ self onDatabase: aMailDB.
 	title _ self defaultWindowTitle.
 	Smalltalk isMorphic
 		ifTrue: [topWindow _ self
