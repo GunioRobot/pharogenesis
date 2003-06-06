3542f84979de13a8ca0756db9521ea14d765d8d0
ls <unknown>
2003-06-06 12:43:00 +0000

    no message

--- Celeste/instance/table of contents pane/otherCategories.cs
+++ Celeste/instance/table of contents pane/otherCategories.cs
@@ -2,9 +2,10 @@ otherCategories
 	"Prompt the user with a menu of all other categories in which the currently selected message appears. If the user chooses a category from this menu, go to that category."
 
 	| otherCategories choice |
+	mailDB ifNil: [ ^self ].
 	otherCategories _
 		(mailDB categoriesThatInclude: currentMsgID) asOrderedCollection.
-	otherCategories remove: currentCategory ifAbsent: [].
+	otherCategories remove: self category ifAbsent: [].
 	(otherCategories isEmpty) ifTrue: [^self].
 	choice _ (CustomMenu selections: otherCategories) startUp.
 	choice = nil ifFalse: [self setCategory: choice].
