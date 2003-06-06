8988edf66332aed27c77c63f8cc54c17957f46f5
ls <unknown>
2003-06-06 12:48:00 +0000

    no message

---
+++ Celeste/instance/categories pane/currentCategory.cs
@@ -0,0 +1,4 @@
+currentCategory
+	"return a notion of the current category, or nil if there is no reasonable choice.  This method doesn't make a lot of sense in GeneralCeleste, but it is here for transition to a filtery future"
+	
+	self subclassResponsibility
