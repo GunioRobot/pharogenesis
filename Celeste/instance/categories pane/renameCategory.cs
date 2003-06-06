0e598cce2eb31579cb913bba66939a570477d991
ls <unknown>
2003-06-06 16:52:00 +0000

    no message

--- Celeste/instance/categories pane/renameCategory.cs
+++ Celeste/instance/categories pane/renameCategory.cs
@@ -2,12 +2,17 @@ renameCategory
 	"Rename the category with the user-specified name."
 
 	| newCatName |
-	currentCategory ifNil: [ ^self ].
+	mailDB ifNil: [ ^self ].
+	self category ifNil: [ ^self ].
 
 	newCatName _ FillInTheBlank
 		request: 'New name?'
-		initialAnswer: currentCategory.
+		initialAnswer: self category.
 	(newCatName isEmpty) ifTrue: [^self].	"user aborted"
-	mailDB renameCategory: currentCategory to: newCatName.
-	currentCategory _ newCatName.
+	mailDB renameCategory: self category to: newCatName.
+
+	"currentCategory _ newCatName."   
 	self changed: #categoryList.
+
+	self setCategory: newCatName.
+	self flag: #xxx.  "this is suboptimal for FilteringCeleste; the old category filter, if there is one, should be modified in place..."
