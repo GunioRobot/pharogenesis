b742b028ad53a51a6c6c925760566adefa474ddc
ls <unknown>
2003-06-06 12:53:00 +0000

    no message

--- Celeste/instance/message text pane/message.cs
+++ Celeste/instance/message text pane/message.cs
@@ -1,6 +1,6 @@
 message
 	"Answer the text of the currently selected message or nil if there isn't one."
-
+self isThisEverCalled .
 	(currentMsgID isNil)
 		ifTrue: [^'']
 		ifFalse: [^(mailDB getText: currentMsgID) asText]
