57310a3046638c6d73dee0321936906b08a9cd8c
ls <unknown>
2003-06-06 12:43:00 +0000

    no message

--- Celeste/instance/table of contents pane/moveAgain.cs
+++ Celeste/instance/table of contents pane/moveAgain.cs
@@ -2,12 +2,12 @@ moveAgain
 	"Move the current message to the same category as last time."
 
 	| newCatName |
-
 	currentMsgID ifNil: [ ^self ].
 	(lastCategory isEmpty not)
 		ifTrue: [newCatName _ lastCategory]
 		ifFalse: [newCatName _ self getCategoryNameIfNone: [^self]].
-	newCatName = currentCategory ifTrue: [ ^self ].
+	newCatName = self category ifTrue: [ ^self ].
 
 	mailDB file: currentMsgID inCategory: newCatName.
+
 	self removeMessage.
