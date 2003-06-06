283dc59fa13539705da02d0252d9383392c628b1
ls <unknown>
2003-06-06 12:43:00 +0000

    no message

--- Celeste/instance/table of contents pane/moveMessage.cs
+++ Celeste/instance/table of contents pane/moveMessage.cs
@@ -2,8 +2,9 @@ moveMessage
 	"Move the current message to another category; this consists of filing it in the new category, and then removing it from the current category"
 
 	| newCatName |
+	mailDB ifNil: [ ^self ].
 	newCatName _ self getCategoryNameIfNone: [^self].
-	newCatName = currentCategory ifTrue: [ ^self ].
+	newCatName = self category ifTrue: [ ^self ].
 	mailDB file: currentMsgID inCategory: newCatName.
 
 	self removeMessage.
