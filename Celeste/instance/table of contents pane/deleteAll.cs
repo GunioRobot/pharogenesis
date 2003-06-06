795653d432e5785336eab57e17bd4cff169aa2e2
ls <unknown>
2003-06-06 12:43:00 +0000

    no message

--- Celeste/instance/table of contents pane/deleteAll.cs
+++ Celeste/instance/table of contents pane/deleteAll.cs
@@ -2,9 +2,8 @@ deleteAll
 	"Move all visible messages in the current category to '.trash.'."
 
 	| |
+	mailDB ifNil: [ ^self ].
 
 	self requiredCategory: '.trash.'.
-
 	mailDB fileAll: currentMessages inCategory: '.trash.'.
-
 	self removeAll.
