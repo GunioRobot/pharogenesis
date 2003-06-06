ebb5a2b6e58fea1acc946c805b9ec8b0baa96aa2
ls <unknown>
2003-06-06 13:51:00 +0000

    no message

--- Celeste/class/common build/buildButtonsFor-3A-.cs
+++ Celeste/class/common build/buildButtonsFor-3A-.cs
@@ -1,39 +1,4 @@
 buildButtonsFor: model 
 	"Answer a collection of handy buttons for the Celeste user interface."
-	| buttons b |
-	buttons _ OrderedCollection new.
-	b _ self buildButtonFromSpec: self specForSubjectFilterButton forModel: model.
-	buttons add: b.
-	true
-		ifFalse: ["Skip these buttons..."
-			b _ self buildFromFilterButtonForModel: model.
-			buttons add: b].
-	b _ self buildButtonFromSpec: self specForParticipantFilterButton forModel: model.
-	buttons add: b.
-	b _ self buildButtonFromSpec: self specForCustomFilterButton forModel: model.
-	buttons add: b.
-	true
-		ifFalse: ["Skip these buttons..."
-	b _ self buildButtonFromSpec: self specForCustomFilterMoveButton forModel: model.
-	buttons add: b].
-	b _ self
-				buildButtonFromSpec: self specForComposeButton
-				withBlock: [model compose].
-	buttons add: b.
-	b _ self
-				buildButtonFromSpec: self specForReplyButton
-				withBlock: [model reply].
-	buttons add: b.
-	b _ self
-				buildButtonFromSpec: self specForForwardButton
-				withBlock: [model forward].
-	buttons add: b.
-	b _ self
-				buildButtonFromSpec: self specForMoveAgainButton
-				withBlock: [model moveAgain].
-	buttons add: b.
-	b _ self
-				buildButtonFromSpec: self specForDeleteButton
-				withBlock: [model deleteMessage].
-	buttons add: b.
-	^ buttons
+	^ self buttonSpecs collect: [ :spec |
+		self buildButtonFromSpec: spec forModel: model ].
