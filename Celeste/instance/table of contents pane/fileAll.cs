c6b218a07a72d18fee822c914f3e136ffc02d33e
ls <unknown>
2003-06-06 12:43:00 +0000

    no message

--- Celeste/instance/table of contents pane/fileAll.cs
+++ Celeste/instance/table of contents pane/fileAll.cs
@@ -2,7 +2,8 @@ fileAll
 	"File all visible messages in the current category in some other category as well."
 
 	| newCatName msgList |
+	mailDB ifNil: [ ^self ].
 	newCatName _ self getCategoryNameIfNone: [^self].
-	msgList _ self filteredMessagesIn: currentCategory.
+	msgList _ self filteredMessages.
 	mailDB fileAll: msgList inCategory: newCatName.
 	self updateTOC.
