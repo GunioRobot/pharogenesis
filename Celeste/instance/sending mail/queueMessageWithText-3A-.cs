e0b68ac3a1449d66eea5c9149e25a72f938b77a7
ls <unknown>
2003-06-06 12:54:00 +0000

    no message

--- Celeste/instance/sending mail/queueMessageWithText-3A-.cs
+++ Celeste/instance/sending mail/queueMessageWithText-3A-.cs
@@ -1,11 +1,28 @@
 queueMessageWithText: aStringOrText 
-	"Queue a message to be sent later. The message is added to the database 
+	"Queue a message to be sent later. The message is added to the database  
 	and filed in the '.tosend.' category."
-	| messageText id |
-	messageText _ 'X-Mailer: ' , Celeste versionString , String cr , 'Date: ' , MailMessage dateStampNow, ' ' , self timeZoneString, ' ', String cr.
+
+	| messageText id msg |
+
+	messageText _ 'X-Mailer: ' , Celeste versionString , String cr , 'Date: ' , MailMessage dateStampNow , ' ' , self timeZoneString , ' ' , String cr.
 	messageText _ messageText , aStringOrText asString.
-	self requiredCategory: '.tosend.'.
+
+	msg _ MailMessage from: messageText.
+
+	"Check now that the addresses are well formed email addresses.  This prevents
+	runtime errors when actually transmitting the mail"
+	[MailAddressParser addressesIn: msg from] ifError:
+		[ :err :rcvr | self inform: 'From: in message header', String cr, err. ^nil].
+	[MailAddressParser addressesIn: msg to] ifError:
+		[ :err :rcvr | self inform: 'To: in message header', String cr, err. ^nil].
+	[MailAddressParser addressesIn: msg cc] ifError:
+		[:err :rcvr | self inform: 'CC: in message header', String cr, err. ^nil].
+
 	"queue the message"
-	id _ mailDB addNewMessage: (MailMessage from: messageText).
+	self requiredCategory: '.tosend.'.
+	id _ mailDB addNewMessage: msg.
 	mailDB file: id inCategory: '.tosend.'.
-	self category = '.tosend.' ifTrue: [self updateTOC]
+	self category = '.tosend.'
+		ifTrue: [self updateTOC].
+	self changed: #outBoxStatus.
+	^ id
