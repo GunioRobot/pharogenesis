ea24b5e516df200b3ca0f72c73e02f85caf20f06
ls <unknown>
2003-06-06 13:28:00 +0000

    no message

--- Celeste/class/build-2D-morphic/addMorphicStatusPaneTo-3A-from-3A-at-3A-plus-3A-.cs
+++ Celeste/class/build-2D-morphic/addMorphicStatusPaneTo-3A-from-3A-at-3A-plus-3A-.cs
@@ -1,9 +1,17 @@
 addMorphicStatusPaneTo: row from: views at: innerFractions plus: verticalOffset 
-	| delta |
+	| delta statusFractions outputBoxFractions |
 	delta _ 20.
+	statusFractions _ innerFractions withRight: 0.5.
+	outputBoxFractions _ (statusFractions withLeft: 0.5)
+				withRight: 1.
 	row
 		addMorph: (views at: #status)
 		fullFrame: (LayoutFrame
-				fractions: innerFractions
+				fractions: statusFractions
+				offsets: (0 @ verticalOffset corner: 0 @ (verticalOffset + delta))).
+	row
+		addMorph: (views at: #outBoxStatus)
+		fullFrame: (LayoutFrame
+				fractions: outputBoxFractions
 				offsets: (0 @ verticalOffset corner: 0 @ (verticalOffset + delta))).
 	^ verticalOffset + delta
