b5dedf73293c9bf56bbf04960215a2da99aa2e4e
ls <unknown>
2003-06-06 12:48:00 +0000

    no message

--- Celeste/instance/categories pane/exportCategory.cs
+++ Celeste/instance/categories pane/exportCategory.cs
@@ -2,7 +2,7 @@ exportCategory
 	"Store the filtered message list of the current category to another mail database. The user is prompted for the name of the other database."
 
 	| destDBName destDB |
-	currentCategory ifNil: [ ^self ].
+	mailDB ifNil: [ ^self ].
 
 	destDBName _ FillInTheBlank
 		request: 'Destination mail database?'
@@ -10,5 +10,5 @@ exportCategory
 	(destDBName isEmpty) ifTrue: [^self].
 	destDB _ MailDB openOn: destDBName.
 	(destDB isNil) ifTrue: [^self].
-	destDB mergeMessages: (self filteredMessagesIn: currentCategory) from: mailDB.
+	destDB mergeMessages: (self filteredMessages) from: mailDB.
 	destDB saveDB.
