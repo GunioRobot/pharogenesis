a8e2b63c19485a81d210004fb64ddb0afaa4bf16
ls <unknown>
2003-06-06 16:47:00 +0000

    no message

--- Celeste/instance/other/requiredCategory-3A-.cs
+++ Celeste/instance/other/requiredCategory-3A-.cs
@@ -1,7 +1,7 @@
 requiredCategory: catName
 	"catName is a required category.  If it does not exist in the database, then create it, and update the category list to reflect that it now exists."
 
-	(self categoryList includes: catName)
+	(mailDB hasCategory: catName)
 		ifFalse:
 			[mailDB addCategory: catName.
 			self changed: #categoryList.]
