3dc946d14ce735b18cb530b4f75c1b7f38d88192
ls <unknown>
2003-06-06 12:43:00 +0000

    no message

--- Celeste/instance/table of contents pane/fileAgain.cs
+++ Celeste/instance/table of contents pane/fileAgain.cs
@@ -2,6 +2,7 @@ fileAgain
 	"File the current message in the same category as last time."
 
 	| newCatName |
+	mailDB ifNil: [ ^self ].
 	(lastCategory isEmpty not)
 		ifTrue: [newCatName _ lastCategory]
 		ifFalse: [newCatName _ self getCategoryNameIfNone: [^self]].
