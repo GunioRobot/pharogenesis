f61b1d6ea29c5855443a17cabe97e9743a2e33a4
ls <unknown>
2003-06-06 15:30:00 +0000

    no message

---
+++ Celeste/instance/custom filters/aaaREADMEaboutFiltering.cs
@@ -0,0 +1,2 @@
+aaaREADMEaboutFiltering
+	"custom filtering has been replaced by named filters.  The ScaffoldingCeleste subclass emulates custom filters on top of the combination of named filters and code filters.  The methods in this category need to be either rewritten or deleted."
