ea72c6b335fd418726d593358dc44cbc209b241c
ls <unknown>
2003-06-06 13:56:00 +0000

    no message

---
+++ GeneralCeleste/class/build-2D-morphic/buildMorphicViewsFor-3A-.cs
@@ -0,0 +1,15 @@
+buildMorphicViewsFor: model 
+	"Answer a dictionary of window panes for the Celeste user interface."
+	|  views v |
+	views _ Dictionary new.
+	views at: #tocEntryList put: (self buildMorphicTocEntryListFor: model).
+
+	v _ self buildMorphicMessageTextPaneFor: model.
+	model messageTextView: v.
+	views at: #messageText put: v.
+
+
+	views at: #status put: (self buildMorphicStatusPaneFor: model).
+	views at: #outBoxStatus put: (self buildMorphicOutBoxStatusPaneFor: model).
+	views at: #filterList put: (self buildMorphicFilterListFor: model).
+	^ views
