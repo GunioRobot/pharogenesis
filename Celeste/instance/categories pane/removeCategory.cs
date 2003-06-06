573994832b48b86574f6b8a79af87d0b69ecaec0
ls <unknown>
2003-06-06 14:32:00 +0000

    no message

--- Celeste/instance/categories pane/removeCategory.cs
+++ Celeste/instance/categories pane/removeCategory.cs
@@ -2,16 +2,17 @@ removeCategory
 	"Remove the existing category with the user-specified name."
 
 	| msgList |
-	currentCategory ifNil: [ ^self ].
+	mailDB ifNil: [ ^self ].
+	self category ifNil: [ ^self ].
 
-	msgList _ mailDB messagesIn: currentCategory.
+	msgList _ mailDB messagesIn: self category.
 	(mailDB messagesIn: '.trash.') do: [: id |
 		msgList remove: id ifAbsent: []].
 	msgList isEmpty ifFalse: [
 		(self confirm:
 'This category is not empty. Are
 you sure you wish to remove it?') ifFalse: [^self]].
-	mailDB removeCategory: currentCategory.
+	mailDB removeCategory: self category.
 
+	self updateTOC.
 	self changed: #categoryList.
-	self setCategory: nil.
