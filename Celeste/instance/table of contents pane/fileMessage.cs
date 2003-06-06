f0464574e5877cd666efe64ec8a1ea0e3620b78d
ls <unknown>
2003-06-06 12:43:00 +0000

    no message

--- Celeste/instance/table of contents pane/fileMessage.cs
+++ Celeste/instance/table of contents pane/fileMessage.cs
@@ -2,5 +2,6 @@ fileMessage
 	"File the current message in another category."
 
 	| newCatName |
+	mailDB ifNil: [ ^self ].
 	newCatName _ self getCategoryNameIfNone: [^self].
 	mailDB file: currentMsgID inCategory: newCatName.
