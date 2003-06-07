e2434c2151ae8165e4741f49502ca42ed468dd4b
sd <unknown>
2003-06-07 19:46:00 +0000

    no message

---
+++ DisplayScreen/instance/other/fullScreenMode-3A-.cs
@@ -0,0 +1,6 @@
+fullScreenMode: aBoolean
+	"On platforms that support it, set full-screen mode to the value of the argument. (Note: you'll need to restore the Display after calling this primitive."
+	"Display fullScreenMode: true. Display newDepth: Display depth"
+
+	<primitive: 233>
+	self primitiveFailed
