8fee5ea7cf452e89f551457543ff8a900f714d0a
nk <unknown>
2003-06-09 06:05:00 +0000

    no message

--- SimpleSliderMorph/class/scripting/additionsToViewerCategories.cs
+++ SimpleSliderMorph/class/scripting/additionsToViewerCategories.cs
@@ -4,22 +4,22 @@ additionsToViewerCategories
 	^ #(
 (slider (
 
-(slot numericValue 'A number representing the current position of the knob.' number readWrite player getNumericValue player setNumericValue:)
+(slot numericValue 'A number representing the current position of the knob.' Number readWrite Player getNumericValue Player setNumericValue:)
 
-(slot minVal	 'The number represented when the knob is at the left or top of the slider; the smallest value returned by the slider.' number readWrite	player getMinVal player setMinVal:)
+(slot minVal	 'The number represented when the knob is at the left or top of the slider; the smallest value returned by the slider.' Number readWrite	Player getMinVal Player setMinVal:)
 
-(slot maxVal 'The number represented when the knob is at the right or bottom of the slider; the largest value returned by the slider.' number readWrite	player getMaxVal player setMaxVal:)
+(slot maxVal 'The number represented when the knob is at the right or bottom of the slider; the largest value returned by the slider.' Number readWrite	Player getMaxVal Player setMaxVal:)
 
-(slot descending 'Tells whether the smallest value is at the top/left (descending = false) or at the bottom/right (descending = true)' boolean readWrite player getDescending player setDescending:)
+(slot descending 'Tells whether the smallest value is at the top/left (descending = false) or at the bottom/right (descending = true)' Boolean readWrite Player getDescending Player setDescending:)
 
-(slot truncate 'If true, only whole numbers are used as values; if false, fractional values are allowed.' boolean readWrite	player getTruncate player setTruncate:)
+(slot truncate 'If true, only whole numbers are used as values; if false, fractional values are allowed.' Boolean readWrite	Player getTruncate Player setTruncate:)
 
-(slot color 'The color of the slider' color readWrite player getColor  player  setColor:)
+(slot color 'The color of the slider' Color readWrite Player getColor  Player  setColor:)
 
-(slot knobColor 'The color of the slider' color readWrite player getKnobColor player setKnobColor:)
-(slot  width  'The width' number readWrite player getWidth  player  setWidth:)
-(slot  height  'The height' number readWrite player getRight  player  setHeight:)))
+(slot knobColor 'The color of the slider' Color readWrite Player getKnobColor Player setKnobColor:)
+(slot  width  'The width' Number readWrite Player getWidth  Player  setWidth:)
+(slot  height  'The height' Number readWrite Player getHeight  Player  setHeight:)))
 
 (basic	(
-(slot numericValue 'A number representing the current position of the knob.' number readWrite player getNumericValue player setNumericValue:))))
+(slot numericValue 'A number representing the current position of the knob.' Number readWrite Player getNumericValue Player setNumericValue:))))
 
