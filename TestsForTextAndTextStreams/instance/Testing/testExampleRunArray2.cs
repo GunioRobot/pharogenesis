647448bfc73566bd256f138ae80015da00289a66
BG <unknown>
2003-06-10 20:47:00 +0000

    no message

---
+++ TestsForTextAndTextStreams/instance/Testing/testExampleRunArray2.cs
@@ -0,0 +1,12 @@
+testExampleRunArray2
+
+  " this demonstrates that different runs are not merged "
+ | runArray |
+
+   runArray := RunArray new.
+   runArray 
+     addLast: TextEmphasis normal times: 5;
+     addLast: TextEmphasis bold times: 5;
+     addLast: TextEmphasis normal times: 5.
+   self assert:
+       (runArray runs size = 3). 
