52fb719279b64156c2a76308bdb9b97c934b80cb
sw <unknown>
2003-06-10 17:31:00 +0000

    no message

--- PseudoClass/instance/methods/stampAt-3A-.cs
+++ PseudoClass/instance/methods/stampAt-3A-.cs
@@ -1,2 +1,9 @@
 stampAt: selector
-	^(self sourceCode at: selector) stamp
+	"Answer the authoring time-stamp of the change"
+
+	| code |
+	^ ((code _ self sourceCode at: selector) isKindOf: ChangeRecord)
+		ifTrue:
+			[code stamp]
+		ifFalse:
+			[code string]
