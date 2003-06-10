b40141dcc3a78e81d52282de6e4b5b2111003d85
BG <unknown>
2003-06-10 20:47:00 +0000

    no message

---
+++ TestsForTextAndTextStreams/instance/Testing/testExampleRunArray4.cs
@@ -0,0 +1,13 @@
+testExampleRunArray4
+
+  " this tests the reversal of a  RunArray "
+
+ | runArray |
+
+   runArray := RunArray new.
+   runArray 
+     addLast: TextEmphasis normal times: 5;
+     addLast: TextEmphasis bold times: 5;
+     addLast: TextEmphasis normal times: 5.
+   self assert:
+       (runArray reversed runs size = 3). 
