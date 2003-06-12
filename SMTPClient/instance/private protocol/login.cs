6d53fd8f1c66234b99948976fdbc52e23523aea5
mir <unknown>
2003-06-12 15:39:00 +0000

    no message

---
+++ SMTPClient/instance/private protocol/login.cs
@@ -0,0 +1,8 @@
+login
+	self user ifNil: [^self].
+	self sendCommand: 'AUTH LOGIN ' , (self encodeString: self user).
+	[self checkResponse]
+		on: TelnetProtocolError
+		do: [ :ex | ex isCommandUnrecognized ifTrue: [^ self] ifFalse: [ex pass]].
+	self sendCommand: (self encodeString: self password).
+	self checkResponse
