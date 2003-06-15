3f40cbd10245c03fea43ae7184987d797a8e08d3
ls <unknown>
2003-06-15 13:40:00 +0000

    no message

--- HierarchicalUrl/instance/parsing/privateInitializeFromText-3A-relativeTo-3A-.cs
+++ HierarchicalUrl/instance/parsing/privateInitializeFromText-3A-relativeTo-3A-.cs
@@ -1,40 +1,28 @@
 privateInitializeFromText: aString relativeTo: aUrl 
-	| remainder ind nextTok s |
+	| remainder ind basePath |
 	remainder _ aString.
 	"set the scheme"
 	schemeName _ aUrl schemeName.
+
 	"a leading // means the authority is specified, meaning it is absolute"
 	(remainder beginsWith: '//')
 		ifTrue: [^ self privateInitializeFromText: aString].
+
 	"otherwise, use the same authority"
 	authority _ aUrl authority.
 	port _ aUrl port.
+	username _ aUrl username.
+	password _ aUrl password.
+
 	"get the query"
 	ind _ remainder indexOf: $?.
 	ind > 0
 		ifTrue: [query _ remainder copyFrom: ind + 1 to: remainder size.
 			remainder _ remainder copyFrom: 1 to: ind - 1].
+
 	"get the path"
-	remainder isEmpty
-		ifTrue: [path _ aUrl path]
-		ifFalse: [(remainder beginsWith: '/')
-				ifTrue: [path _ OrderedCollection new]
-				ifFalse: [path _ aUrl path shallowCopy.
-					path size > 0
-						ifTrue: [path removeLast]].
-			s _ ReadStream on: remainder.
-			[s peek = $/
-				ifTrue: [s next].
-			nextTok _ WriteStream on: String new.
-			[s atEnd
-				or: [s peek = $/]]
-				whileFalse: [nextTok nextPut: s next].
-			nextTok _ nextTok contents unescapePercents.
-			nextTok = '..'
-				ifTrue: [path size > 0
-						ifTrue: [path removeLast]]
-				ifFalse: [nextTok ~= '.'
-						ifTrue: [path add: nextTok]].
-			s atEnd] whileFalse.
-			path isEmpty
-				ifTrue: [path add: '']]
+	(remainder beginsWith: '/')
+		ifTrue: [ basePath := #() ]
+		ifFalse: [ basePath := aUrl path ].
+	path := self privateParsePath: remainder  relativeTo: basePath.
+
