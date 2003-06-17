c168f81088870521e3185e90d080f437a95dfd1d
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- StringSocket/class/as yet unclassified/showRatesSeen.cs
+++ StringSocket/class/as yet unclassified/showRatesSeen.cs
@@ -4,7 +4,7 @@ StringSocket showRatesSeen
 "
 	| answer |
 
-	MaxRatesSeen ifNil: [^1 beep].
+	MaxRatesSeen ifNil: [^Beeper beep].
 	answer _ WriteStream on: String new.
 	MaxRatesSeen keys asSortedCollection do: [ :key |
 		answer nextPutAll: key printString,'  ',((MaxRatesSeen at: key) // 10000) printString; cr
