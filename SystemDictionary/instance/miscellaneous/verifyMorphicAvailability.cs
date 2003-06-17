0a345ce2dc9bfafb3542acdb2d34924d0293984a
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- SystemDictionary/instance/miscellaneous/verifyMorphicAvailability.cs
+++ SystemDictionary/instance/miscellaneous/verifyMorphicAvailability.cs
@@ -1,7 +1,7 @@
 verifyMorphicAvailability
 	"If Morphic is available, return true; if not, put up an informer and return false"
 	self hasMorphic ifFalse:
-		[self beep.
+		[Beeper beep.
 		self inform: 'Sorry, Morphic must
 be present to use this feature'.
 		^ false].
