4d45d1a4cbef6c9e762c15bb8129c2d4ee85d3a9
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

---
+++ StackMorph/instance/background/addCardsFromAFile.cs
@@ -0,0 +1,7 @@
+addCardsFromAFile
+	"Using the current background, create new cards by reading in data from a fileThe data are in each record are expected to be tab-delimited, and to occur in the same order as the instance variables of the current-background's cards "
+
+	| aFileStream |
+	(aFileStream _ FileList2 modalFileSelector) ifNil: [^ Beeper beep].
+	self addCardsFromString: aFileStream contentsOfEntireFile.
+	aFileStream close
