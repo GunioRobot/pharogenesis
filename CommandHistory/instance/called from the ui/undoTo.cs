2c34376b5180d284a7e6cd844c161d075f427ccb
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- CommandHistory/instance/called from the ui/undoTo.cs
+++ CommandHistory/instance/called from the ui/undoTo.cs
@@ -1,13 +1,13 @@
 undoTo
-	"Incomplete.  Allow the user to choose a point somewhere in the undo/redo tape, and undo his way to there.   Applicable only if infiniteUndo is set.  Not yet functional."
+	"Not yet functional, and not yet sent.  Allow the user to choose a point somewhere in the undo/redo tape, and undo his way to there.   Applicable only if infiniteUndo is set. "
 
 	| anIndex commandList aMenu reply |
-	(anIndex _ self historyIndexOfLastCommand) ifNil: [^ self beep].
+	(anIndex _ self historyIndexOfLastCommand) == 0 ifTrue: [^ Beeper beep].
 	commandList _ history
 		copyFrom:	((anIndex - 10) max: 1)
 		to:			((anIndex + 10) min: history size).
-	aMenu _ SelectionMenu labels:  (commandList collect: [:cmd | cmd cmdWording]) selections: commandList.
+	aMenu _ SelectionMenu labels:  (commandList collect: [:cmd | cmd cmdWording truncateWithElipsisTo: 20]) selections: commandList.
 	reply _ aMenu startUpWithCaption: 'undo or redo to...'.
-	reply ifNotNil: [self halt: 'now for the rest...!']
+	reply ifNotNil: [self inform: #deferred]
 
-	"Command undoTo"
+	"ActiveWorld commandHistory undoTo"
