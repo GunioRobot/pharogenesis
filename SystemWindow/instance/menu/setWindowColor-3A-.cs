36f8b1cbade11fe32bc2309094d2752d263e7e64
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- SystemWindow/instance/menu/setWindowColor-3A-.cs
+++ SystemWindow/instance/menu/setWindowColor-3A-.cs
@@ -5,14 +5,15 @@ setWindowColor: incomingColor
 	(aColor = ColorPickerMorph perniciousBorderColor 
 		or: [aColor = Color black]) ifTrue: [^ self].
 	existingColor _ self paneColorToUse.
-	existingColor ifNil: [^ self beep].
-	(self allMorphs copyWithout: self) do:
-		[:aMorph |
+	existingColor ifNil: [^ Beeper beep].
+	Preferences alternativeWindowLook ifFalse:[
+		(self allMorphs copyWithout: self) do:[:aMorph |
 			((aMorph isKindOf: PluggableButtonMorph) and: [aMorph offColor = existingColor])
 				ifTrue:
 					[aMorph onColor: aColor darker offColor: aColor].
 			aMorph color = existingColor
 				ifTrue:
-					[aMorph color: aColor]].
-	self setStripeColorsFrom: aColor
-		
+					[aMorph color: aColor]]].
+	self paneColor: aColor.
+	self setStripeColorsFrom: aColor.
+	self changed.
