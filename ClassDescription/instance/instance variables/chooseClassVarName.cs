6952efbebdcd74b46650f3f4bffaed025d209b78
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

---
+++ ClassDescription/instance/instance variables/chooseClassVarName.cs
@@ -0,0 +1,20 @@
+chooseClassVarName 
+	"Present the user with a list of class variable names and answer the one selected, or nil if none"
+
+	| lines labelStream vars allVars index |
+	lines _ OrderedCollection new.
+	allVars _ OrderedCollection new.
+	labelStream _ WriteStream on: (String new: 200).
+	self withAllSuperclasses reverseDo:
+		[:class |
+		vars _ class classVarNames asSortedCollection.
+		vars do:
+			[:var |
+			labelStream nextPutAll: var; cr.
+			allVars add: var].
+		vars isEmpty ifFalse: [lines add: allVars size]].
+	labelStream contents isEmpty ifTrue: [^Beeper beep]. "handle nil superclass better"
+	labelStream skip: -1 "cut last CR".
+	index _ (PopUpMenu labels: labelStream contents lines: lines) startUp.
+	index = 0 ifTrue: [^ nil].
+	^ allVars at: index
