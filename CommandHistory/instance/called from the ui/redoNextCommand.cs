6c6a1f650cc5d2268cc1f08e4a20f5f74084bfd2
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- CommandHistory/instance/called from the ui/redoNextCommand.cs
+++ CommandHistory/instance/called from the ui/redoNextCommand.cs
@@ -2,7 +2,7 @@ redoNextCommand
 	"If there is a way to 'redo' (move FORWARD) in the undo/redo history tape, do it."
 
 	| anIndex |
-	lastCommand ifNil: [^ self beep].
+	lastCommand ifNil: [^ Beeper beep].
 	lastCommand phase == #undone
 		ifFalse:
 			[anIndex _ history indexOf: lastCommand.
@@ -10,7 +10,7 @@ redoNextCommand
 				ifTrue:
 					[lastCommand _ history at: anIndex + 1]
 				ifFalse:
-					[^ self beep]].
+					[^ Beeper beep]].
 
 	lastCommand redoCommand.
 	lastCommand phase: #done
