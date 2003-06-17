b705a003ef0e27228e28e998bd69a55558873760
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- PasteUpMorph/instance/world state/nextPage.cs
+++ PasteUpMorph/instance/world state/nextPage.cs
@@ -1,4 +1,4 @@
 nextPage
 	"backstop for smart next-page buttons that look up the containment hierarchy until they find somone who is willing to field this command.  If we get here, the 'next' button was not embedded in a book, so we can do nothing useful"
 
-	self beep
+	Beeper beep
