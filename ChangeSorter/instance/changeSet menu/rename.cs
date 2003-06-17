ef5605efc64f7c2ee19b0300749d0aa1f0b72b2b
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- ChangeSorter/instance/changeSet menu/rename.cs
+++ ChangeSorter/instance/changeSet menu/rename.cs
@@ -5,7 +5,7 @@ rename
 	newName _ FillInTheBlank request: 'New name for this change set'
 						initialAnswer: myChangeSet name.
 	(newName = myChangeSet name or: [newName size == 0]) ifTrue:
-			[^ self inform: 'No change made'].
+			[^ Beeper beep].
 
 	(self class changeSetNamed: newName) ifNotNil:
 			[^ Utilities inform: 'Sorry that name is already used'].
