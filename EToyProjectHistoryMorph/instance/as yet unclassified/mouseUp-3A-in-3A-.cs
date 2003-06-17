b80af9fbdde84c7d6472d6eb58ee3bd241202def
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- EToyProjectHistoryMorph/instance/as yet unclassified/mouseUp-3A-in-3A-.cs
+++ EToyProjectHistoryMorph/instance/as yet unclassified/mouseUp-3A-in-3A-.cs
@@ -3,11 +3,11 @@ mouseUp: evt in: aMorph
 	| tuple project url |
 
 	(aMorph boundsInWorld containsPoint: evt cursorPoint) ifFalse: [^self].
-	tuple _ aMorph valueOfProperty: #projectParametersTuple ifAbsent: [^1 beep].
+	tuple _ aMorph valueOfProperty: #projectParametersTuple ifAbsent: [^Beeper beep].
 	project _ tuple fourth first.
 	(project notNil and: [project world notNil]) ifTrue: [self closeMyFlapIfAny. ^project enter].
 	url _ tuple third.
-	url isEmptyOrNil ifTrue: [^1 beep].
+	url isEmptyOrNil ifTrue: [^Beeper beep].
 	self closeMyFlapIfAny.
 	ProjectLoading thumbnailFromUrl: url.
 
