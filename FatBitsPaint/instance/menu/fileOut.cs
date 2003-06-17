f7ab235c3bc8998cd61a0e9d411a734fe2a8a889
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- FatBitsPaint/instance/menu/fileOut.cs
+++ FatBitsPaint/instance/menu/fileOut.cs
@@ -2,7 +2,7 @@ fileOut
 
         | fileName result |
 
-        result _ StandardFileMenu newFile ifNil: [^ 1 beep].
+        result _ StandardFileMenu newFile ifNil: [^Beeper beep].
         fileName _ result directory fullNameFor: result name.
         Cursor normal showWhile:
                 [self unmagnifiedForm writeOnFileNamed: fileName]
