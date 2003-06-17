e178709ab48a721d39f222e8b11633554eb2167a
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- ChangeSorter/instance/changeSet menu/findCngSet.cs
+++ ChangeSorter/instance/changeSet menu/findCngSet.cs
@@ -1,13 +1,14 @@
 findCngSet 
 	"Search for a changeSet by name.  Pop up a menu of all changeSets whose name contains the string entered by the user.  If only one matches, then the pop-up menu is bypassed"
-	| index pattern candidates |
+	| index pattern candidates nameList |
 	self okToChange ifFalse: [^ self].
-	ChangeSet instanceCount > AllChangeSets size ifTrue: [self class gatherChangeSets].
 	pattern _ FillInTheBlank request: 'ChangeSet name or fragment?'.
 	pattern isEmpty ifTrue: [^ self].
+	nameList _ self changeSetList asSet.
 	candidates _ AllChangeSets select:
-			[:c | c name includesSubstring: pattern caseSensitive: false].
-	candidates size = 0 ifTrue: [^ self beep].
+			[:c | (nameList includes: c name) and: 
+				[c name includesSubstring: pattern caseSensitive: false]].
+	candidates size = 0 ifTrue: [^ Beeper beep].
 	candidates size = 1 ifTrue:
 		[^ self showChangeSet: candidates first].
 	index _ (PopUpMenu labels: 
