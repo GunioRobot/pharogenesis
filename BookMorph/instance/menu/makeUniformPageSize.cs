950f865a3a99a2b4e92c6b10b4f58a35555c8d64
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- BookMorph/instance/menu/makeUniformPageSize.cs
+++ BookMorph/instance/menu/makeUniformPageSize.cs
@@ -1,6 +1,6 @@
 makeUniformPageSize
 	"Make all pages be of the same size as the current page."
-	currentPage ifNil: [^ self beep].
+	currentPage ifNil: [^ Beeper beep].
 	self resizePagesTo: currentPage extent.
 	newPagePrototype ifNotNil:
 		[newPagePrototype extent: currentPage extent]
