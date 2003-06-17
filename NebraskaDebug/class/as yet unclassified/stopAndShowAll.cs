ac27a24b87265f730db790aac8edbe11e5f12306
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- NebraskaDebug/class/as yet unclassified/stopAndShowAll.cs
+++ NebraskaDebug/class/as yet unclassified/stopAndShowAll.cs
@@ -6,7 +6,7 @@ self halt.	"not updated to new format"
 
 	prev _ DEBUG.
 	DEBUG _ nil.
-	prev ifNil: [^1 beep].
+	prev ifNil: [^Beeper beep].
 	prev keysAndValuesDo: [ :k :v |
 		self showStats: k from: v
 	].
