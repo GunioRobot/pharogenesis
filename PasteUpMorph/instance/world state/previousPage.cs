63061be9023d1720231b3fa4be1ddcf247540188
nb <unknown>
2003-06-17 12:25:00 +0000

    no message

--- PasteUpMorph/instance/world state/previousPage.cs
+++ PasteUpMorph/instance/world state/previousPage.cs
@@ -1,4 +1,4 @@
 previousPage
 	"backstop for smartprev-page buttons that look up the containment hierarchy until they find somone who is willing to field this command.  If we get here, the button was not embedded in a book, so we can do nothing useful"
 
-	self beep
+	Beeper beep
