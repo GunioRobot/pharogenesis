2a72f4498e5522af5b0d1a11caf068c711b121fc
ls <unknown>
2003-06-20 16:22:00 +0000

    no message

--- Celeste/class/instance creation/current.cs
+++ Celeste/class/instance creation/current.cs
@@ -2,4 +2,5 @@ current
 	"Answer the currently active Celeste (assuming that there's
 	only one Celeste open at a given time) or open a new one."
 
-	^Celeste allInstances detect: [:e | e isActive] ifNone: [self open].
+	Smalltalk garbageCollect.
+	^Celeste allSubInstances detect: [:e | e isActive] ifNone: [self open].
