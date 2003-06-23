bbf194b24b7139ab4580010b7c65bd308ac24e98
yo <unknown>
2003-06-23 20:05:00 +0000

    no message

--- DisplayText/instance/displaying/displayOn-3A-at-3A-clippingBox-3A-rule-3A-fillColor-3A-.cs
+++ DisplayText/instance/displaying/displayOn-3A-at-3A-clippingBox-3A-rule-3A-fillColor-3A-.cs
@@ -1,12 +1,17 @@
-displayOn: aDisplayMedium at: aDisplayPoint clippingBox: clipRectangle rule: ruleInteger fillColor: aForm 
+displayOn: aDisplayMedium at: aDisplayPoint clippingBox: clipRectangle rule: ruleInteger fillColor: aForm
+	"For TT font, rule 34 is used if possible."
 	"Refer to the comment in 
 	DisplayObject|displayOn:at:clippingBox:rule:mask:."
 
-	self form
+	| form1 rule |
+	form1 _ self form.
+	rule _ (ruleInteger = Form over and: [backColor isTransparent])
+				ifTrue: [form1 depth = 32 ifTrue: [rule _ 34] ifFalse: [Form paint]]
+				ifFalse: [ruleInteger].
+	form1 depth = 32 ifTrue: [rule _ 34].
+	form1
 		displayOn: aDisplayMedium
 		at: aDisplayPoint + offset
 		clippingBox: clipRectangle
-		rule: ((ruleInteger = Form over and: [backColor isTransparent])
-				ifTrue: [Form paint]
-				ifFalse: [ruleInteger])
+		rule: rule
 		fillColor: aForm
