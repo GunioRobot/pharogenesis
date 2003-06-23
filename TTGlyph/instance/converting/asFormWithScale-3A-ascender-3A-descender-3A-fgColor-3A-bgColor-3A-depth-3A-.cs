8311066f63328b15b1c5f3f3799f7f52eb281341
yo <unknown>
2003-06-23 18:27:00 +0000

    no message

---
+++ TTGlyph/instance/converting/asFormWithScale-3A-ascender-3A-descender-3A-fgColor-3A-bgColor-3A-depth-3A-.cs
@@ -0,0 +1,10 @@
+asFormWithScale: scale ascender: ascender descender: descender fgColor: fgColor bgColor: bgColor depth: depth
+
+	^ self
+		asFormWithScale: scale
+		ascender: ascender
+		descender: descender
+		fgColor: fgColor
+		bgColor: bgColor
+		depth: depth
+		replaceColor: false.
