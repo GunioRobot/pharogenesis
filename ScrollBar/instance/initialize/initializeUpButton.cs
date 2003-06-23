94252aada28e2a295d41a46e061577f6571d8da1
dgd <unknown>
2003-06-23 20:12:00 +0000

    no message

--- ScrollBar/instance/initialize/initializeUpButton.cs
+++ ScrollBar/instance/initialize/initializeUpButton.cs
@@ -1,23 +1,28 @@
 initializeUpButton
-	upButton := RectangleMorph
-		newBounds: ((menuButton ifNil: [self innerBounds topLeft]
-				ifNotNil: [bounds isWide ifTrue: [menuButton bounds topRight]
-									ifFalse: [menuButton bounds bottomLeft]])
-					extent: self buttonExtent)
-		color: Color lightGray.
-	upButton on: #mouseDown send: #scrollUpInit to: self.
-	upButton on: #mouseUp send: #finishedScrolling to: self.
-	upButton addMorphCentered: (ImageMorph new image:
-		(self 
-			cachedImageAt: (bounds isWide ifTrue: ['left'] ifFalse: ['up'])
-			ifAbsentPut: [
-				bounds isWide ifTrue: [
-					self upArrow8Bit rotateBy: #left centerAt: 0@0
-				] ifFalse: [
-					self upArrow8Bit
-				]
-			]
-		)
-	).
-	upButton setBorderWidth: 1 borderColor: #raised.
+"initialize the receiver's upButton"
+	upButton := self roundedScrollbarLook
+		ifTrue: [RectangleMorph
+						newBounds: (self innerBounds topLeft extent: self buttonExtent)]
+		ifFalse: [RectangleMorph
+						newBounds: ((menuButton
+								ifNil: [self innerBounds topLeft]
+								ifNotNil: [bounds isWide
+										ifTrue: [menuButton bounds topRight]
+										ifFalse: [menuButton bounds bottomLeft]])
+								extent: self buttonExtent)].
+	upButton color: self thumbColor.
+	upButton
+		on: #mouseDown
+		send: #scrollUpInit
+		to: self.
+	upButton
+		on: #mouseUp
+		send: #finishedScrolling
+		to: self.
+	self updateUpButtonImage.
+	self roundedScrollbarLook
+		ifTrue: [upButton color: Color veryLightGray.
+			upButton
+				borderStyle: (BorderStyle complexRaised width: 3)]
+		ifFalse: [upButton setBorderWidth: 1 borderColor: #raised].
 	self addMorph: upButton
