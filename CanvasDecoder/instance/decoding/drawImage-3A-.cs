5639bd83d402b4c9c48926ae36f37368d8f28337
nk <unknown>
2003-06-25 12:24:00 +0000

    no message

--- CanvasDecoder/instance/decoding/drawImage-3A-.cs
+++ CanvasDecoder/instance/decoding/drawImage-3A-.cs
@@ -1,31 +1,30 @@
-drawImage: command
+drawImage: command 
 	| image point sourceRect rule cacheID cacheNew previousImage |
-
-	image := self class decodeImage: (command at: 2).
-	point := self class decodePoint: (command at: 3).
-	sourceRect := self class decodeRectangle: (command at: 4).
-	rule := self class decodeInteger: (command at: 5).
-	command size >= 7 ifTrue: [
-		false ifTrue: [self showSpaceUsed].		"debugging"
-		cacheID _ self class decodeInteger: (command at: 6).
-		cacheNew _ (self class decodeInteger: (command at: 7)) = 1.
-		cacheID > 0 ifTrue: [
-			CachedForms ifNil: [CachedForms _ Array new: 100].
-			cacheNew ifTrue: [
-				CachedForms at: cacheID put: image
-			] ifFalse: [
-				previousImage _ CachedForms at: cacheID.
-				image ifNil: [
-					image _ previousImage
-				] ifNotNil: [
-					(previousImage notNil and: [image depth > 8]) ifTrue: [
-						image _ previousImage addDeltasFrom: image.
-					].
-					CachedForms at: cacheID put: image
-				].
-			].
-		].
-	].
-	self drawCommand: [ :c |
-		c image: image  at: point  sourceRect: sourceRect rule: rule
-	]
+	image := self class decodeImage: command second.
+	point := self class decodePoint: command third.
+	sourceRect := self class decodeRectangle: command fourth.
+	rule := self class decodeInteger: command fifth.
+	command size >= 7 
+		ifTrue: 
+			[false ifTrue: [self showSpaceUsed].	"debugging"
+			cacheID := self class decodeInteger: (command sixth).
+			cacheNew := (self class decodeInteger: command seventh) = 1.
+			cacheID > 0 
+				ifTrue: 
+					[
+					cacheNew 
+						ifTrue: [CachedForms at: cacheID put: image]
+						ifFalse: 
+							[previousImage := CachedForms at: cacheID.
+							image ifNil: [image := previousImage]
+								ifNotNil: 
+									[(previousImage notNil and: [image depth > 8]) 
+										ifTrue: [image := previousImage addDeltasFrom: image].
+									CachedForms at: cacheID put: image]]]].
+	self drawCommand: 
+			[:c | 
+			c 
+				image: image
+				at: point
+				sourceRect: sourceRect
+				rule: rule]
