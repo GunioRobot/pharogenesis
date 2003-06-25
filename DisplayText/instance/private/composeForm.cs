408eb0b0aee43b13de2b6c7da73035363b899161
nk <unknown>
2003-06-25 12:51:00 +0000

    no message

--- DisplayText/instance/private/composeForm.cs
+++ DisplayText/instance/private/composeForm.cs
@@ -1,12 +1,14 @@
 composeForm
-	| form1 |
+	"For the TT strings in MVC widgets in a Morphic world such as a progress bar, the form is created by Morphic machinery."
+	| canvas tmpText |
 	Smalltalk isMorphic
 		ifTrue:
-			[form1 _ (TextMorph new contentsAsIs: text) imageFormDepth: 1.
-			form _ (ColorForm extent: form1 extent)
-				offset: offset;
-				colors: (Array
-					with: (backColor == nil ifTrue: [Color transparent] ifFalse: [backColor])
-					with: (foreColor == nil ifTrue: [Color black] ifFalse: [foreColor])).
-			form1 displayOn: form]
+			[tmpText _ TextMorph new contentsAsIs: text deepCopy.
+			foreColor ifNotNil: [tmpText text addAttribute: (TextColor color: foreColor)].
+			backColor ifNotNil: [tmpText backgroundColor: backColor].
+			tmpText setTextStyle: textStyle.
+			canvas _ FormCanvas on: (Form extent: tmpText extent depth: 32).
+			tmpText drawOn: canvas.
+			form _ canvas form.
+		]
 		ifFalse: [form _ self asParagraph asForm]
