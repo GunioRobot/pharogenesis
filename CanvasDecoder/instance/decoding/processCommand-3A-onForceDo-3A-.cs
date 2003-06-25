1830004a590c381ce408de53e33d3e6c324f2963
nk <unknown>
2003-06-25 12:42:00 +0000

    no message

--- CanvasDecoder/instance/decoding/processCommand-3A-onForceDo-3A-.cs
+++ CanvasDecoder/instance/decoding/processCommand-3A-onForceDo-3A-.cs
@@ -1,26 +1,18 @@
 processCommand: command  onForceDo: forceBlock
-	| verb verbCode |
+	"Decode the given string command and perform the required action.
+	If the command is a forceToScreen command, also pass the forceBlock.
+	The previous chained equality tests and conditionals have been replaced by a lookup table in my class variable DecodeTable, which is set in the class-side initialize method."
+	| verb verbCode selector |
 	command isEmpty ifTrue: [ ^self ].
 
 	verb _ command first.
-	verbCode := verb at: 1.
+	verbCode := verb first.
 
-	verbCode = CanvasEncoder codeClip ifTrue: [ ^self setClip: command ].
-	verbCode = CanvasEncoder codeTransform ifTrue: [ ^self setTransform: command ].
-	verbCode = CanvasEncoder codeText ifTrue: [ ^self drawText: command ].
-	verbCode = CanvasEncoder codeLine ifTrue: [ ^self drawLine: command ].
-	verbCode = CanvasEncoder codeRect ifTrue: [ ^self drawRect: command ].
-	verbCode = CanvasEncoder codeBalloonRect ifTrue: [ ^self drawBalloonRect: command ].
-	verbCode = CanvasEncoder codeBalloonOval ifTrue: [ ^self drawBalloonOval: command ].
-	verbCode = CanvasEncoder codeInfiniteFill ifTrue: [ ^self drawInfiniteFill: command ].
+	selector _ DecodeTable
+		at: (verbCode asciiValue + 1)
+		ifAbsent: [ self error: 'unknown command: ', verb ].
 
-	verbCode = CanvasEncoder codeOval ifTrue: [ ^self drawOval: command ].
-	verbCode = CanvasEncoder codeImage ifTrue: [ ^self drawImage: command ].
-	verbCode = CanvasEncoder codeReleaseCache ifTrue: [ ^self releaseImage: command ].
-	verbCode = CanvasEncoder codePoly ifTrue: [ ^self drawPoly: command ].
-	verbCode = CanvasEncoder codeStencil ifTrue: [ ^self drawStencil: command ].
-	verbCode = CanvasEncoder codeForce ifTrue: [ ^self forceToScreen: command withBlock: forceBlock ].
-	verbCode = CanvasEncoder codeFont ifTrue: [ ^self addFontToCache: command ].
-	verbCode = CanvasEncoder codeExtentDepth ifTrue: [ ^self extentDepth: command ].
-
-self error: 'unknown command: ', command first.
+	"note: codeForce is the only odd one"
+	^(selector == #forceToScreen:)
+		ifTrue: [ self forceToScreen: command withBlock: forceBlock ]
+		ifFalse: [ self perform: selector withArguments: { command } ]
