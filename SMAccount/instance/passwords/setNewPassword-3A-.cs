eabf6c74b30c7786cb9826dfa951993bac4ab025
gk <unknown>
2003-06-26 14:02:00 +0000

    no message

---
+++ SMAccount/instance/passwords/setNewPassword-3A-.cs
@@ -0,0 +1,8 @@
+setNewPassword: aString
+	"Set a new parallell password the user can use to get in
+	if the old password is forgotten. We don't delete the old
+	password since the request for this new password is made
+	anonymously. Note that the password is stored as a secured
+	hash large integer."
+
+	newPassword _ SecureHashAlgorithm new hashMessage: aString
