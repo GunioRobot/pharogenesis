a72340dd06b8082c432341037b7ff2b97b97dfec
nk <unknown>
2003-06-26 21:12:00 +0000

    no message

--- Utilities/class/fetching updates/serverUrls.cs
+++ Utilities/class/fetching updates/serverUrls.cs
@@ -7,10 +7,12 @@ serverUrls
 	list _ UpdateUrlLists first last.
 
 	"If there is a dead server, return a copy with that server last" 
-	list clone withIndexDo: [:aName :ind |
-	(aName beginsWith: Socket deadServer) ifTrue: [
-		list _ list asOrderedCollection.	"and it's a copy"
-		list removeAt: ind.
-		list addLast: aName]].
+	Socket deadServer ifNotNil: [
+		list clone withIndexDo: [:aName :ind |
+		(aName beginsWith: Socket deadServer) ifTrue: [
+			list _ list asOrderedCollection.	"and it's a copy"
+			list removeAt: ind.
+			list addLast: aName]]
+	].
 
 	^ list asArray
