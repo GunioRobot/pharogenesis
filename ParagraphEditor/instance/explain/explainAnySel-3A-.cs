fa6e791e2ab3c8b18438e6a90d5638b36f1a4674
nk <unknown>
2003-06-26 22:02:00 +0000

    no message

--- ParagraphEditor/instance/explain/explainAnySel-3A-.cs
+++ ParagraphEditor/instance/explain/explainAnySel-3A-.cs
@@ -2,9 +2,9 @@ explainAnySel: symbol
 	"Is this any message selector?"
 
 	| list reply |
-	list _ Smalltalk allClassesImplementing: symbol.
+	list _ self systemNavigation allClassesImplementing: symbol.
 	list size = 0 ifTrue: [^nil].
 	list size < 12
 		ifTrue: [reply _ ' is a message selector which is defined in these classes ' , list printString]
 		ifFalse: [reply _ ' is a message selector which is defined in many classes'].
-	^'"' , symbol , reply , '."' , '\' withCRs, 'Smalltalk browseAllImplementorsOf: #' , symbol
+	^'"' , symbol , reply , '."' , '\' withCRs, 'SystemNavigation new browseAllImplementorsOf: #' , symbol
