fd5d560e39b293dd5541f1a8cb13a3d5c93c51a1
nk <unknown>
2003-06-27 10:51:00 +0000

    no message

--- NetNameResolver/class/lookups/nameForAddress-3A-timeout-3A-.cs
+++ NetNameResolver/class/lookups/nameForAddress-3A-timeout-3A-.cs
@@ -1,16 +1,20 @@
 nameForAddress: hostAddress timeout: secs
 	"Look up the given host address and return its name. Return nil if the lookup fails or is not completed in the given number of seconds. Depends on the given host address being known to the gateway, which may not be the case for dynamically allocated addresses."
 	"NetNameResolver
-		nameForAddress: (NetNameResolver addressFromString: '128.111.92.40')
+		nameForAddress: (NetNameResolver addressFromString: '128.111.92.2')
 		timeout: 30"
 
-	| deadline ready success |
+	| deadline result |
+	self initializeNetwork.
 	deadline _ Time millisecondClockValue + (secs * 1000).
-	ready _ self waitForResolverReadyUntil: deadline.
-	ready ifFalse: [^ nil].
-
-	self primStartLookupOfAddress: hostAddress.
-	success _ self waitForCompletionUntil: deadline.
-	success
-		ifTrue: [^ self primAddressLookupResult]
-		ifFalse: [^ nil].
+	"Protect the execution of this block, as the ResolverSemaphore is used for both parts of the transaction."
+	self resolverMutex
+		critical: [
+			result _ (self waitForResolverReadyUntil: deadline)
+				ifTrue: [
+					self primStartLookupOfAddress: hostAddress.
+					(self waitForCompletionUntil: deadline)
+						ifTrue: [self primAddressLookupResult]
+						ifFalse: [nil]]
+				ifFalse: [nil]].
+	^result
