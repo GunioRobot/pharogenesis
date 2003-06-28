901ba5c8a49d5f7fea6e7417b75f5c60aa508022
ar <unknown>
2003-06-28 01:07:00 +0000

    no message

--- WorldState/instance/update cycle/displayWorldSafely-3A-.cs
+++ WorldState/instance/update cycle/displayWorldSafely-3A-.cs
@@ -1,19 +1,17 @@
 displayWorldSafely: aWorld
 	"Update this world's display and keep track of errors during draw methods."
 
-	| oldHandler errCtx errMorph |
-
-	oldHandler _ Processor activeProcess errorHandler.
 	[aWorld displayWorld] ifError: [:err :rcvr |
 		"Handle a drawing error"
+		| errCtx errMorph |
 		errCtx _ thisContext.
 		[
 			errCtx _ errCtx sender.
 			"Search the sender chain to find the morph causing the problem"
-			[errCtx notNil and:[(errCtx receiver isKindOf: Morph) not]] 
+			[errCtx notNil and:[(errCtx receiver isMorph) not]] 
 				whileTrue:[errCtx _ errCtx sender].
 			"If we're at the root of the context chain then we have a fatal drawing problem"
-			errCtx == nil ifTrue:[^self handleFatalDrawingError: err].
+			errCtx ifNil:[^self handleFatalDrawingError: err].
 			errMorph _ errCtx receiver.
 			"If the morph causing the problem has already the #drawError flag set,
 			then search for the next morph above in the caller chain."
@@ -21,6 +19,5 @@ displayWorldSafely: aWorld
 		] whileTrue.
 		errMorph setProperty: #errorOnDraw toValue: true.
 		"Install the old error handler, so we can re-raise the error"
-		Processor activeProcess errorHandler: oldHandler.
 		rcvr error: err.
 	].
