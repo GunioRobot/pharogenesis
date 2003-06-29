fa1058fb47a4dbc7cc6f4c2b7f7460fd155b6759
asm <unknown>
2003-06-29 22:36:00 +0000

    no message

--- SystemWindow/instance/panes/paneColor.cs
+++ SystemWindow/instance/panes/paneColor.cs
@@ -1,7 +1,19 @@
 paneColor
-	Display depth > 2 ifTrue:
-		[model ifNotNil: [
-			model isInMemory ifTrue: [
-				^ Color colorFrom: model defaultBackgroundColor]].
-		paneMorphs isEmptyOrNil ifFalse: [^ paneMorphs first color]].
-	^ Color white
+	| cc |
+	(cc := self valueOfProperty: #paneColor) ifNotNil: [^cc].
+	Display depth > 2 
+		ifTrue: 
+			[model ifNotNil: 
+					[model isInMemory 
+						ifTrue: 
+							[cc := Color colorFrom: model defaultBackgroundColor.
+							Preferences alternativeWindowLook 
+								ifTrue: 
+									[cc := (cc = Color lightYellow or: [cc = Color white]) 
+										ifTrue: [Color gray: 0.67]
+										ifFalse: [cc duller]]]].
+			cc 
+				ifNil: [cc := paneMorphs isEmptyOrNil ifFalse: [paneMorphs first color]]].
+	cc ifNil: [cc := self defaultBackgroundColor].
+	self paneColor: cc.
+	^cc
