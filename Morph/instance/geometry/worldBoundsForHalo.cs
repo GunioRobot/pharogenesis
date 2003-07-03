18b3682f2d0d390a716bcdede8409e95d573c566
nk <unknown>
2003-07-03 19:39:00 +0000

    no message

--- Morph/instance/geometry/worldBoundsForHalo.cs
+++ Morph/instance/geometry/worldBoundsForHalo.cs
@@ -1,4 +1,10 @@
 worldBoundsForHalo
-	"To restore older behavior, change this to return  self fullBoundsInWorld"
+	"Answer the rectangle to be used as the inner dimension of my halos.
+	Allow for showing either bounds or fullBounds, and compensate for the optional bounds rectangle."
 
-	^ self boundsIn: nil
+	| r |
+	r _ (Preferences haloEnclosesFullBounds)
+		ifFalse: [ self boundsIn: nil ]
+		ifTrue: [ self fullBoundsInWorld ].
+	Preferences showBoundsInHalo ifTrue: [ ^r outsetBy: 1 ].
+	^r
