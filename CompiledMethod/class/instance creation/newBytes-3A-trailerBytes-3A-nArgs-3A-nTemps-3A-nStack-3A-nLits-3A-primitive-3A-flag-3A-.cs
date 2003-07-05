ed343b7dca47a287feb0084c8d13fcb0f9094ca9
ls <unknown>
2003-07-05 13:49:00 +0000

    no message

---
+++ CompiledMethod/class/instance creation/newBytes-3A-trailerBytes-3A-nArgs-3A-nTemps-3A-nStack-3A-nLits-3A-primitive-3A-flag-3A-.cs
@@ -0,0 +1,26 @@
+newBytes: numberOfBytes trailerBytes: trailer nArgs: nArgs nTemps: nTemps nStack: stackSize nLits: nLits primitive: primitiveIndex flag: flag
+	"Answer an instance of me. The header is specified by the message 
+	arguments. The remaining parts are not as yet determined."
+	| largeBit primBits method flagBit |
+	nTemps > 64 ifTrue:
+		[^ self error: 'Cannot compile -- too many temporary variables'].	
+	largeBit _ (nTemps + stackSize) > SmallFrame ifTrue: [1] ifFalse: [0].
+
+	"For now the high bit of the primitive no. is in a high bit of the header"
+	primBits _ (primitiveIndex bitAnd: 16r1FF) + ((primitiveIndex bitAnd: 16r200) bitShift: 19).
+
+	flagBit := flag ifTrue: [ 1 ] ifFalse: [ 0 ].
+
+	method _ self newMethod: numberOfBytes + trailer size
+		header: (nArgs bitShift: 24) +
+				(nTemps bitShift: 18) +
+				(largeBit bitShift: 17) +
+				(nLits bitShift: 9) +
+				primBits +
+				(flagBit bitShift: 29).
+
+	"Copy the source code trailer to the end"
+	1 to: trailer size do:
+		[:i | method at: method size - trailer size + i put: (trailer at: i)].
+
+	^ method
