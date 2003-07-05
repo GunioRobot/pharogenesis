602e4a94980c39a3b02346f829fc73249c2c6e7e
nk <unknown>
2003-07-05 22:28:00 +0000

    no message

---
+++ SARInstaller/instance/client services/openTextFile-3A-.cs
@@ -0,0 +1,9 @@
+openTextFile: memberOrName
+	"Open a text window on the given member"
+	| member |
+	member _ self memberNamed: memberOrName.
+	member ifNil: [ ^self errorNoSuchMember: memberOrName ].
+	StringHolder new
+		acceptContents: member contents;
+		openLabel: member fileName.
+	self installed: member.
