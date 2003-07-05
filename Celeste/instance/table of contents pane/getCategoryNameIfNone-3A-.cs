4b10a4b5ce517b93ab692319ce5d64c2a4fc8e7c
ls <unknown>
2003-07-05 13:18:00 +0000

    no message

--- Celeste/instance/table of contents pane/getCategoryNameIfNone-3A-.cs
+++ Celeste/instance/table of contents pane/getCategoryNameIfNone-3A-.cs
@@ -1,24 +1,3 @@
 getCategoryNameIfNone: aBlock
-	"Prompt the user for a category name, remembering it for the next time."
-
-	| catList categoryName |
-	catList _ self categoryList.
-	catList remove: '.all.' ifAbsent: [].
-	catList remove: '.trash.' ifAbsent: [].
-	catList remove: '.unclassified.' ifAbsent: [].
-	catList add: '<new category>'.
-	((lastCategoryList ~= catList) or: [lastCategoryMenu = nil])
-		ifTrue: [lastCategoryMenu _ CustomMenu selections: catList].
-	categoryName _ lastCategoryMenu startUp.
-	categoryName = nil ifTrue: [^aBlock value].
-	categoryName = '<new category>' ifTrue: [
-		categoryName _ FillInTheBlank
-			request: 'New category name?'
-			initialAnswer: ''.
-		(categoryName isEmpty) ifTrue: [^aBlock value].
-		lastCategoryMenu _ nil.
-		mailDB addCategory: categoryName.
-		self changed: #categoryList.
-	].
-	lastCategoryList _ catList.
-	^lastCategory _ categoryName
+	"Prompt the user for a category name"
+	^self getCategoryNameAllowingAny: false  ifNone: aBlock
