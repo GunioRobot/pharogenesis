aea7674a829273d4921c61e25a2e10ba8cc44595
nk <unknown>
2003-07-06 07:49:00 +0000

    no message

--- CodeHolder/instance/misc/isThereAnOverride.cs
+++ CodeHolder/instance/misc/isThereAnOverride.cs
@@ -1,12 +1,9 @@
 isThereAnOverride
-	"Answer whether any subclass of my selected class implements my selected selector"
-
-	| aName aList aClass |
-	(aName _ self selectedMessageName) ifNil: [^ false].
-
-	aList _ Smalltalk allImplementorsOf: aName.
+	"Answer whether any subclass of my selected class implements my 
+	selected selector"
+	| aName aClass |
+	aName _ self selectedMessageName
+				ifNil: [^ false].
 	aClass _ self selectedClassOrMetaClass.
-	aList do:
-		[:element | MessageSet parse: element toClassAndSelector:
-			[:cl :sel | (cl inheritsFrom: aClass) ifTrue: [^ true]]].
+	aClass allSubclassesDo: [ :cls | (cls includesSelector: aName) ifTrue: [ ^true ]].
 	^ false
