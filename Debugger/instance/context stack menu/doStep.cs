6bca08fa430f55c10cc800728d79729aabfc91cf
ajh <unknown>
2003-07-06 21:06:00 +0000

    no message

--- Debugger/instance/context stack menu/doStep.cs
+++ Debugger/instance/context stack menu/doStep.cs
@@ -2,27 +2,16 @@ doStep
 	"Send the selected message in the accessed method, and regain control 
 	after the invoked method returns."
 	
-	| currentContext oldMethod |
+	| currentContext newContext |
 	self okToChange ifFalse: [^ self].
 	self checkContextSelection.
-	externalInterrupt ifFalse: [contextStackTop push: proceedValue].
-	externalInterrupt _ true. "simulation leaves same state as interrupting"
 	currentContext _ self selectedContext.
+	newContext _ interruptedProcess completeStep: currentContext.
+	newContext == currentContext ifTrue: [
+		newContext _ interruptedProcess stepToSendOrReturn].
 	self contextStackIndex > 1
-		ifTrue: 
-			[currentContext completeCallee: contextStackTop.
-			self resetContext: currentContext]
-		ifFalse: 
-			[currentContext stepToSendOrReturn.
-			currentContext willReturn
-				ifTrue: 
-					[oldMethod _ currentContext method.
-					currentContext _ currentContext step.
-					currentContext stepToSendOrReturn.
-					self resetContext: currentContext.
-					oldMethod == currentContext method "didnt used to update pc here"
-						ifTrue: [self changed: #contentsSelection]]
-				ifFalse: 
-					[currentContext completeCallee: currentContext step.
-					self changed: #contentsSelection.
-					self updateInspectors]]
+		ifTrue: [self resetContext: newContext]
+		ifFalse: [newContext == currentContext
+				ifTrue: [self changed: #contentsSelection.
+						self updateInspectors]
+				ifFalse: [self resetContext: newContext]].
