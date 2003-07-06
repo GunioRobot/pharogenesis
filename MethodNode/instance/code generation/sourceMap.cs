4b75257edbd8c5ac19fd2645e76fe76bbbeada33
ajh <unknown>
2003-07-06 15:26:00 +0000

    no message

--- MethodNode/instance/code generation/sourceMap.cs
+++ MethodNode/instance/code generation/sourceMap.cs
@@ -1,6 +1,13 @@
 sourceMap
-	"Answer a SortedCollection of associations of the form: pc (byte offset in 
-	me) -> sourceRange (an Interval) in source text."
+	"Answer a SortedCollection of associations of the form: pc (byte offset in me) -> sourceRange (an Interval) in source text."
 
-	self generate: #(0 0 0 0).
-	^encoder sourceMap
+	| methNode |
+	methNode _ self.
+	sourceText ifNil: [
+		"No source, use decompile string as source to map from"
+		methNode _ self parserClass new
+			parse: self decompileString
+			class: self methodClass
+	].
+	methNode generateNative: #(0 0 0 0).  "set bytecodes to map to"
+	^ methNode encoder sourceMap
