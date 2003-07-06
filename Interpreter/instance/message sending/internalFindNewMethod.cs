1a047ad72706b428b7f3927ea13f0d2c0de45303
ar <unknown>
2003-07-06 23:55:00 +0000

    no message

--- Interpreter/instance/message sending/internalFindNewMethod.cs
+++ Interpreter/instance/message sending/internalFindNewMethod.cs
@@ -2,8 +2,6 @@ internalFindNewMethod
 	"Find the compiled method to be run when the current messageSelector is sent to the class 'lkupClass', setting the values of 'newMethod' and 'primitiveIndex'."
 	| ok | 
 	self inline: true.
-	self sharedCodeNamed: 'commonLookup' inCase: 131.
-
 	ok _ self lookupInMethodCacheSel: messageSelector class: lkupClass.
 	ok ifFalse: [
 		"entry was not found in the cache; look it up the hard way"
