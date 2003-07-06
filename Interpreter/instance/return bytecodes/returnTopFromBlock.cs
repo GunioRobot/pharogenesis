ca51d9f4166166dafe2f9c85c15b24ca52aace9e
ar <unknown>
2003-07-06 21:55:00 +0000

    no message

--- Interpreter/instance/return bytecodes/returnTopFromBlock.cs
+++ Interpreter/instance/return bytecodes/returnTopFromBlock.cs
@@ -1,7 +1,5 @@
 returnTopFromBlock
 	"Return to the caller of the method containing the block."
-
-	| cntx val |
-	cntx _ self caller.  "Note: caller, not sender!"
-	val _ self internalStackTop.
-	self returnValue: val to: cntx.
+	localReturnContext _ self caller.  "Note: caller, not sender!"
+	localReturnValue _ self internalStackTop.
+	self commonReturn.
