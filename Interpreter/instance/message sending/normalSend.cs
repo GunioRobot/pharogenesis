a55defbed0ba1750c138895c4410960d3174808a
ar <unknown>
2003-07-06 22:19:00 +0000

    no message

--- Interpreter/instance/message sending/normalSend.cs
+++ Interpreter/instance/message sending/normalSend.cs
@@ -1,14 +1,12 @@
 normalSend
 	"Send a message, starting lookup with the receiver's class."
-	"Assume: messageSelector and argumentCount have been set, and that the receiver and arguments have been pushed onto the stack,"
+	"Assume: messageSelector and argumentCount have been set, and that 
+	the receiver and arguments have been pushed onto the stack,"
 	"Note: This method is inlined into the interpreter dispatch loop."
-
 	| rcvr |
 	self inline: true.
-	self sharedCodeNamed: 'commonSend' inCase: 131.
-
+	self sharedCodeNamed: 'normalSend' inCase: 131.
 	rcvr _ self internalStackValue: argumentCount.
-	receiverClass _ lkupClass _ self fetchClassOf: rcvr.
-	self internalFindNewMethod.
-	self internalExecuteNewMethod.
-	self fetchNextBytecode.
+	lkupClass _ self fetchClassOf: rcvr.
+	receiverClass _ lkupClass.
+	self commonSend.
