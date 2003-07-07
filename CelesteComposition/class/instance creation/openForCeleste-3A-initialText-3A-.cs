97f584f3d54e388f4812fc3e48527c09bdd44a3c
nk <unknown>
2003-07-07 11:49:00 +0000

    no message

--- CelesteComposition/class/instance creation/openForCeleste-3A-initialText-3A-.cs
+++ CelesteComposition/class/instance creation/openForCeleste-3A-initialText-3A-.cs
@@ -1,3 +1,3 @@
 openForCeleste: aCeleste  initialText: initialText
 	"open a composition window for the given mail reader, editting the given text"
-	(self celeste: aCeleste  initialText: initialText) open
+	^(self celeste: aCeleste  initialText: initialText) open; yourself
