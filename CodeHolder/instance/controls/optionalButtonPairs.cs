ee3d76bdfdb6db5470ac67620c8692c0de5acec4
nk <unknown>
2003-07-07 11:39:00 +0000

    no message

---
+++ CodeHolder/instance/controls/optionalButtonPairs.cs
@@ -0,0 +1,31 @@
+optionalButtonPairs
+	"Answer a tuple (formerly pairs) defining buttons, in the format:
+			button label
+			selector to send
+			help message"
+
+	| aList |
+
+	aList _ #(
+	('browse'			browseMethodFull			'view this method in a browser')
+	('senders' 			browseSendersOfMessages	'browse senders of...')
+	('implementors'		browseMessages				'browse implementors of...')
+	('versions'			browseVersions				'browse versions')), 
+
+	(Preferences decorateBrowserButtons
+		ifTrue:
+			[{#('inheritance'		methodHierarchy 'browse method inheritance
+green: sends to super
+tan: has override(s)
+mauve: both of the above
+pink: is an override but doesn''t call super
+pinkish tan: has override(s), also is an override but doesn''t call super' )}]
+		ifFalse:
+			[{#('inheritance'		methodHierarchy			'browse method inheritance')}]),
+
+	#(
+	('hierarchy'		classHierarchy				'browse class hierarchy')
+	('inst vars'			browseInstVarRefs			'inst var refs...')
+	('class vars'			browseClassVarRefs			'class var refs...')).
+
+	^ aList
