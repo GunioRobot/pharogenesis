5fa063337255f4414e46baabf05a145e6b4396cf
nk <unknown>
2003-07-07 17:18:00 +0000

    no message

--- Morph/instance/geometry eToy/color-3A-sees-3A-.cs
+++ Morph/instance/geometry eToy/color-3A-sees-3A-.cs
@@ -1,34 +1,46 @@
-color: sensitiveColor sees: soughtColor
+color: sensitiveColor sees: soughtColor 
 	"Return true if any of my pixels of sensitiveColor intersect with pixels of soughtColor."
-	| myImage sensitivePixelMask map patchBelowMe tfm morphAsFlexed i1 |
+
 	"Make a mask with black where sensitiveColor is, white elsewhere"
-	tfm _ self transformFrom: self world.
-	morphAsFlexed _ tfm isIdentity
-		ifTrue: [self]
-		ifFalse: [TransformationMorph new
-					flexing: self clone
-					byTransformation: tfm].
-	myImage _ morphAsFlexed imageForm offset: 0@0.
-	sensitivePixelMask _ Form extent: myImage extent depth: 1.
-	map _ Bitmap new: (1 bitShift: (myImage depth min: 15)).
-	map at: (i1 _ sensitiveColor indexInMap: map) put: 1.
-	sensitivePixelMask copyBits: sensitivePixelMask boundingBox
-		from: myImage form at: 0@0 colorMap: map.
+
+	| myImage sensitivePixelMask map patchBelowMe tfm morphAsFlexed i1 pasteUp |
+	pasteUp _ self world ifNil: [ ^false ].
+	tfm := self transformFrom: pasteUp.
+	morphAsFlexed := tfm isIdentity 
+				ifTrue: [self]
+				ifFalse: [TransformationMorph new flexing: self clone byTransformation: tfm].
+	myImage := morphAsFlexed imageForm offset: 0 @ 0.
+	sensitivePixelMask := Form extent: myImage extent depth: 1.
+	"ensure at most a 16-bit map"
+	map := Bitmap new: (1 bitShift: (myImage depth - 1 min: 15)).
+	map at: (i1 := sensitiveColor indexInMap: map) put: 1.
+	sensitivePixelMask 
+		copyBits: sensitivePixelMask boundingBox
+		from: myImage form
+		at: 0 @ 0
+		colorMap: map.
 
 	"get an image of the world below me"
-	patchBelowMe _ self world patchAt: morphAsFlexed fullBounds
-								without: self andNothingAbove: false.
-"
+	patchBelowMe := pasteUp 
+				patchAt: morphAsFlexed fullBounds
+				without: self
+				andNothingAbove: false.
+	"
 sensitivePixelMask displayAt: 0@0.
 patchBelowMe displayAt: 100@0.
 "
 	"intersect world pixels of the color we're looking for with the sensitive pixels"
-	map at: i1 put: 0.  "clear map and reuse it"
+	map at: i1 put: 0.	"clear map and reuse it"
 	map at: (soughtColor indexInMap: map) put: 1.
-	sensitivePixelMask copyBits: patchBelowMe boundingBox
-		from: patchBelowMe at: 0@0 clippingBox: patchBelowMe boundingBox
-		rule: Form and fillColor: nil map: map.
-"
+	sensitivePixelMask 
+		copyBits: patchBelowMe boundingBox
+		from: patchBelowMe
+		at: 0 @ 0
+		clippingBox: patchBelowMe boundingBox
+		rule: Form and
+		fillColor: nil
+		map: map.
+	"
 sensitivePixelMask displayAt: 200@0.
 "
-	^ (sensitivePixelMask tallyPixelValues at: 2) > 0
+	^(sensitivePixelMask tallyPixelValues second) > 0
