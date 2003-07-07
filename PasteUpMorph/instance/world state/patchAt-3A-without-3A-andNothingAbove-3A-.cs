a0fbcc16009e208d328e40cdc12848593fd83a01
nk <unknown>
2003-07-07 11:15:00 +0000

    no message

--- PasteUpMorph/instance/world state/patchAt-3A-without-3A-andNothingAbove-3A-.cs
+++ PasteUpMorph/instance/world state/patchAt-3A-without-3A-andNothingAbove-3A-.cs
@@ -1,18 +1,22 @@
 patchAt: patchRect without: stopMorph andNothingAbove: stopThere
 	"Return a complete rendering of this patch of the display screen
 	without stopMorph, and possibly without anything above it."
+
 	| c |
-	c _ ColorPatchCanvas extent: patchRect extent depth: Display depth.
+	c _ ColorPatchCanvas
+		extent: patchRect extent
+		depth: Display depth
+		origin: patchRect topLeft negated
+		clipRect: (0@0 extent: patchRect extent).
 	c stopMorph: stopMorph.
 	c doStop: stopThere.
-	c _ c copyOrigin: patchRect topLeft negated clipRect: (0@0 extent: patchRect extent).
+
 	(self bounds containsRect: patchRect) ifFalse:
 		["Need to fill area outside bounds with black."
 		c form fillColor: Color black].
 	(self bounds intersects: patchRect) ifFalse:
 		["Nothing within bounds to show."
 		^ c form].
-	c fillRectangle: self bounds color: color.  "Fill bounds with world color."
-	self drawSubmorphsOn: c.
-	worldState handsReverseDo: [:h | h drawSubmorphsOn: c].
+	self fullDrawOn: c.
+	stopThere ifFalse: [ self world handsReverseDo: [:h | h drawSubmorphsOn: c]].
 	^c form
