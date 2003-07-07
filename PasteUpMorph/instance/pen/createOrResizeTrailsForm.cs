3e8e00640d5cd8614d2732f96291a59d7b262812
nk <unknown>
2003-07-07 11:17:00 +0000

    no message

--- PasteUpMorph/instance/pen/createOrResizeTrailsForm.cs
+++ PasteUpMorph/instance/pen/createOrResizeTrailsForm.cs
@@ -1,17 +1,15 @@
 createOrResizeTrailsForm
-	"If necessary, create a new turtleTrailsForm or resize the existing one to fill my bounds. On return, turtleTrailsForm exists and is the correct size."
+	"If necessary, create a new turtleTrailsForm or resize the existing one to fill my bounds.
+	On return, turtleTrailsForm exists and is the correct size.
+	Use the Display depth so that color comparisons (#color:sees: and #touchesColor:) will work right."
 
 	| newForm |
-	turtleTrailsForm ifNil:
-		["create new TrailsForm if needed"
-		turtleTrailsForm _ Form extent: self extent depth: 8.
-		turtlePen _ nil].
-
-	turtleTrailsForm extent = self extent ifFalse:
+	(turtleTrailsForm isNil or: [ turtleTrailsForm extent ~= self extent ]) ifTrue:
 		["resize TrailsForm if my size has changed"
-		newForm _ Form extent: self extent depth: 8.
-		newForm copy: self bounds from: turtleTrailsForm
-					to: 0@0 rule: Form paint.
+		newForm _ Form extent: self extent depth: Display depth.
+		turtleTrailsForm ifNotNil: [
+			newForm copy: self bounds from: turtleTrailsForm
+					to: 0@0 rule: Form paint ].
 		turtleTrailsForm _ newForm.
 		turtlePen _ nil].
 
