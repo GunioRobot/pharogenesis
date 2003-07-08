5203333ab7fe218a5107da2bd7a63ef813c03160
ar <unknown>
2003-07-08 11:16:00 +0000

    no message

--- CCodeGenerator/instance/utilities/nilOrBooleanConstantReceiverOf-3A-.cs
+++ CCodeGenerator/instance/utilities/nilOrBooleanConstantReceiverOf-3A-.cs
@@ -2,6 +2,7 @@ nilOrBooleanConstantReceiverOf: sendNode
 	"Answer nil or the boolean constant that is the receiver of the given message send. Used to suppress conditional code when the condition is a translation-time constant."
 
 	| rcvr val |
+	generateDeadCode ifTrue:[^nil].
 	rcvr _ sendNode receiver.
 	rcvr isConstant ifTrue: [
 		val _ rcvr value.
