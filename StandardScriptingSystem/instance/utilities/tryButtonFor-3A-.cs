3b9858763ab64c0c212e04717d1cab2837c8221b
dgd <unknown>
2003-07-12 12:05:00 +0000

    no message

--- StandardScriptingSystem/instance/utilities/tryButtonFor-3A-.cs
+++ StandardScriptingSystem/instance/utilities/tryButtonFor-3A-.cs
@@ -1,7 +1,13 @@
-tryButtonFor: aPhraseTileMorph
+tryButtonFor: aPhraseTileMorph 
 	| aButton |
-	aButton _ SimpleButtonMorph new.
-	aButton target: aPhraseTileMorph; actionSelector: #try; label: '!' font: (StrikeFont familyName: #ComicBold size: 16); color: Color yellow; borderWidth: 0.
+	aButton := SimpleButtonMorph new.
+	aButton target: aPhraseTileMorph;
+		 actionSelector: #try;
+		
+		label: '!'
+		font: Preferences standardEToysFont;
+		 color: Color yellow;
+		 borderWidth: 0.
 	aButton actWhen: #whilePressed.
 	aButton balloonTextSelector: #try.
 	^ aButton
