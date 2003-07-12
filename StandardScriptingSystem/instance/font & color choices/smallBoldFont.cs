3d1449e298b83610b3a4904062269ffe3145d072
nk <unknown>
2003-07-12 08:59:00 +0000

    no message

--- StandardScriptingSystem/instance/font & color choices/smallBoldFont.cs
+++ StandardScriptingSystem/instance/font & color choices/smallBoldFont.cs
@@ -1,4 +1,4 @@
 smallBoldFont
 	"Answer a small bold font for use in some standard scripting-support structures"
 
-	^ StrikeFont familyName: #ComicBold size: 12
+	^ StrikeFont familyName: Preferences standardEToysFont familyName size: 12
