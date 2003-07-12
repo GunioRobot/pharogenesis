ed5513ec7e8db2d5ca226e0f2796d20dd6e7cd33
dgd <unknown>
2003-07-12 12:33:00 +0000

    no message

--- EToyGenericDialogMorph/instance/as yet unclassified/genericTextFieldNamed-3A-.cs
+++ EToyGenericDialogMorph/instance/as yet unclassified/genericTextFieldNamed-3A-.cs
@@ -1,10 +1,7 @@
-genericTextFieldNamed: aString
-
+genericTextFieldNamed: aString 
 	| newField |
-
-	newField _ TextMorph new
-		beAllFont: self myFont;
-		extent: 300@20;
-		contentsWrapped: ''.
+	newField := ShowEmptyTextMorph new beAllFont: self myFont;
+				 extent: 400 @ 20;
+				 contentsWrapped: ''.
 	namedFields at: aString put: newField.
-	^newField
+	^ newField
