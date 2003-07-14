b740c04c17b3372189d47f28e8f894579ca62d70
gk <unknown>
2003-07-14 15:04:00 +0000

    no message

---
+++ SMSqueakMap/class/discarding/discardSM.cs
@@ -0,0 +1,7 @@
+discardSM
+	"Discard SqueakMapBase. All the map state is kept in
+	the class var DefaultMap in SMSqueakMap and is thus also removed."
+
+	"SMSqueakMap discardSM"
+
+	SystemOrganization removeCategoriesMatching: 'SM-domain'.
