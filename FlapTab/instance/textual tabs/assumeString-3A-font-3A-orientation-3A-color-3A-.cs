59549e04f41fac99bb6e680a95789745ca7ce73d
yo <unknown>
2003-07-16 15:25:00 +0000

    no message

--- FlapTab/instance/textual tabs/assumeString-3A-font-3A-orientation-3A-color-3A-.cs
+++ FlapTab/instance/textual tabs/assumeString-3A-font-3A-orientation-3A-color-3A-.cs
@@ -1,27 +1,24 @@
-assumeString: aString font: aFont orientation: anOrientation color: aColor
-	| aTextMorph workString pad |
-	pad _ (anOrientation == #vertical) 
-		ifTrue:	[Character cr]
-		ifFalse:	[$ ].
-	workString _ pad asString.
-	aString do:
-		[:ch |
-			workString _ workString copyWith: ch.
-			workString _ workString copyWith: pad].
-	(anOrientation == #vertical)
-		ifTrue:
-			[workString _ workString copyFrom: 2 to: workString size - 1].
-
-	aTextMorph _ (TextMorph new beAllFont: aFont) width: 10; contents: workString; yourself.
+assumeString: aString font: aFont orientation: orientationSymbol color: aColor 
+	| aTextMorph workString tabStyle |
+	labelString := aString asString.
+	workString := orientationSymbol == #vertical 
+				ifTrue: 
+					[String streamContents: 
+							[:s | 
+							labelString do: [:c | s nextPut: c] separatedBy: [s nextPut: Character cr]]]
+				ifFalse: [labelString]. 
+	tabStyle := (TextStyle new)
+				leading: 0;
+				newFontArray: (Array with: aFont).
+	aTextMorph := (TextMorph new setTextStyle: tabStyle) 
+				contents: (workString asText addAttribute: (TextKern kern: 3)).
 	self removeAllMorphs.
-	self addMorph: aTextMorph centered.
-	aTextMorph lock.
-	anOrientation == #horizontal
-		ifTrue:
-			[self borderWidth: 0]
-		ifFalse:
-			[self borderWidth: 3; borderColor: #raised].
-	self fitContents.
+	self
+		borderWidth: 2;
+		borderColor: #raised.
 	aColor ifNotNil: [self color: aColor].
-	aTextMorph position: self position.
-	self layoutChanged
+	self addMorph: aTextMorph centered.
+	aTextMorph lock
+	"
+FlapTab allSubInstancesDo: [:ft | ft reformatTextualTab]
+"
