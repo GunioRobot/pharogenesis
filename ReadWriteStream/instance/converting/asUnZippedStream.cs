9a0d68fa64346edda97232a948272af42c785aba
yo <unknown>
2003-07-16 14:59:00 +0000

    no message

--- ReadWriteStream/instance/converting/asUnZippedStream.cs
+++ ReadWriteStream/instance/converting/asUnZippedStream.cs
@@ -1,14 +1,23 @@
 asUnZippedStream
-	| isGZip outputStream first |
-	"Decompress this file if needed, and return a stream.  No file is written.  File extension may be .gz or anything else."
+	| isGZip outputStream first strm archive which |
+	"Decompress this file if needed, and return a stream.  No file is written.  File extension may be .gz or anything else.  Also works on archives (.zip, .gZip)."
 
-	self binary.
-	first _ self next.
-	isGZip _ (self next * 256 + first) = (GZipConstants at: #GZipMagic).
-	self skip: -2.
+	strm _ self binary.
+	strm isZipArchive ifTrue: [
+		archive _ ZipArchive new readFrom: strm.
+		which _ archive members detect: [:any | any fileName asLowercase endsWith: '.ttf'] 
+								ifNone: [nil].
+		which ifNil: [archive close.
+					^ self error: 'Can''t find .ttf file in archive'].
+		strm _ which contentStream.
+		archive close].
+
+	first _ strm next.
+	isGZip _ (strm next * 256 + first) = (GZipConstants gzipMagic).
+	strm skip: -2.
 	isGZip 
-		ifTrue: [outputStream _ (RWBinaryOrTextStream with:
-					(GZipReadStream on: self) upToEnd) reset.
-				self close]
-		ifFalse: [outputStream _ self].
+		ifTrue: [outputStream _ (MultiByteBinaryOrTextStream with:
+									(GZipReadStream on: strm) upToEnd) reset.
+				strm close]
+		ifFalse: [outputStream _ strm].
 	^ outputStream
