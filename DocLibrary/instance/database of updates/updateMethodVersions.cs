f950524bde18f3df62e7132344391003d3205e80
yo <unknown>
2003-07-16 15:53:00 +0000

    no message

--- DocLibrary/instance/database of updates/updateMethodVersions.cs
+++ DocLibrary/instance/database of updates/updateMethodVersions.cs
@@ -2,12 +2,11 @@ updateMethodVersions
 	"See if any new updates have occurred, and put their methods into the database."
 
 	| indexFile list result |
-	self haveNetwork ifFalse: [^ self].
 	indexFile _ 'latest.ix'.
 	list _ OrderedCollection new.
 	[result _ self absorbAfter: lastUpdate from: indexFile.
 	"boolean if succeeded, or we are up to date, or server not available"
-	result class == String] whileTrue: [
+	 result isString] whileTrue: [
 		"result is the prev file name"
 		list addFirst: indexFile.
 		indexFile _ result].
