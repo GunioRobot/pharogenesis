bd1c55679dde67f452de47f7a3cf7fc4aba3f8e6
ajh <unknown>
2003-07-18 21:49:00 +0000

    no message

---
+++ BlockContext/instance/initialize-2D-release/privRefresh.cs
@@ -0,0 +1,9 @@
+privRefresh
+	"Reinitialize the receiver so that it is in the state it was at its creation."
+
+	pc _ startpc.
+	self stackp: 0.
+	nargs timesRepeat: [  "skip arg popping"
+		self nextInstruction selector = #popIntoTemporaryVariable:
+			ifFalse: [self halt: 'unexpected bytecode instruction']
+	].
