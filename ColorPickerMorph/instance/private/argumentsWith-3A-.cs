0fdbb928c6df2c0ce10f7b79864cdd30287360b1
ar <unknown>
2003-07-19 20:40:00 +0000

    no message

--- ColorPickerMorph/instance/private/argumentsWith-3A-.cs
+++ ColorPickerMorph/instance/private/argumentsWith-3A-.cs
@@ -2,7 +2,8 @@ argumentsWith: aColor
 	"Return an argument array appropriate to this action selector"
 
 	| nArgs |
-	nArgs _ selector numArgs.
+	nArgs _ selector ifNil:[0] ifNotNil:[selector numArgs].
+	nArgs = 0 ifTrue:[^#()].
 	nArgs = 1 ifTrue:[^ {aColor}].
 	nArgs = 2 ifTrue:[^ {aColor. sourceHand}].
 	nArgs = 3 ifTrue:[^ {aColor. argument. sourceHand}].
