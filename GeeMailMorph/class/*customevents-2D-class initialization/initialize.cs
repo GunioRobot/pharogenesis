6ecb3c3bdde46f97d7a9349ad2b1c9f39f8e7c41
nk <unknown>
2003-07-20 12:34:00 +0000

    no message

---
+++ GeeMailMorph/class/*customevents-2D-class initialization/initialize.cs
@@ -0,0 +1,4 @@
+initialize
+	"AlansTextPlusMorph initialize"
+	ScriptingSystem addCustomEventFor: self named: #scrolledIntoView help: 'when I am scrolled into view in a GeeMailMorph'.
+	ScriptingSystem addCustomEventFor: self named: #scrolledOutOfView help: 'when I am scrolled out of view in a GeeMailMorph'.
