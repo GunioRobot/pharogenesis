2c43a06766eb0ea4668774d8445c4d390e5e4ed0
ajh <unknown>
2003-07-20 23:55:00 +0000

    no message

--- Process/instance/debugging/debugWithTitle-3A-.cs
+++ Process/instance/debugging/debugWithTitle-3A-.cs
@@ -1,15 +1,6 @@
 debugWithTitle: title
-	"Open a debugger on me"
-	"Is this the UI process?"
-	self == Processor activeProcess
-		ifTrue: [Smalltalk isMorphic
-				ifTrue: [[Project interruptName: title] fork]
-				ifFalse: [[ScheduledControllers interruptName: title] fork].
-			^ self].
-	myList isNil
-		ifTrue: [^ self]
-		ifFalse: [myList
-				remove: self
-				ifAbsent: [].
-			self offList].
-	Debugger openInterrupt: title onProcess: self
+	"Open debugger on self"
+
+	| context |
+	context _ self isActiveProcess ifTrue: [thisContext] ifFalse: [self suspendedContext].
+	self debug: context title: title full: true.
