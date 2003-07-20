95250da5f9fe67c021390df0f1dceeb09fe2a4d9
ajh <unknown>
2003-07-20 22:51:00 +0000

    no message

---
+++ Process/instance/changing process state/primitiveSuspend.cs
@@ -0,0 +1,8 @@
+primitiveSuspend
+	"Primitive. Stop the process that self represents in such a way 
+	that it can be restarted at a later time (by sending #resume).
+	ASSUMES self is the active process.
+	Essential. See Object documentation whatIsAPrimitive."
+
+	<primitive: 88>
+	self primitiveFailed
