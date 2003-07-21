f897cbf5f368b37ee79ae65af732556ac470b370
ajh <unknown>
2003-07-21 00:29:00 +0000

    no message

--- CompiledMethod/instance/source code management/tempNames.cs
+++ CompiledMethod/instance/source code management/tempNames.cs
@@ -1,5 +1,9 @@
 tempNames
+
 	| byteCount bytes |
+	self holdsTempNames ifFalse: [
+		^ (1 to: self numTemps) collect: [:i | 't', i printString]
+	].
 	byteCount _ self at: self size.
 	byteCount = 0 ifTrue: [^ Array new].
 	bytes _ (ByteArray new: byteCount)
