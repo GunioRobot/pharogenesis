427cdd5178ced07a3bb8348e6309586aca1bddf0
ajh <unknown>
2003-07-21 10:08:00 +0000

    no message

--- Debugger/instance/private/resumeProcess-3A-.cs
+++ Debugger/instance/private/resumeProcess-3A-.cs
@@ -7,22 +7,18 @@ resumeProcess: aTopView
 		ifNotNil: [failedProject enterForEmergencyRecovery.
 			isolationHead invoke.
 			isolationHead _ nil].
-	interruptedProcess suspendedContext method
-			== (Process compiledMethodAt: #terminate)
-		ifFalse: [contextStackIndex > 1
-				ifTrue: [interruptedProcess popTo: self selectedContext]
-				ifFalse: [interruptedProcess install: self selectedContext].
-			Smalltalk isMorphic
-				ifTrue: [errorWasInUIProcess
-						ifTrue: [Project resumeProcess: interruptedProcess]
-						ifFalse: [interruptedProcess resume]]
-				ifFalse: [ScheduledControllers activeControllerNoTerminate: interruptedController andProcess: interruptedProcess]].
+	interruptedProcess isTerminated ifFalse: [
+		Smalltalk isMorphic
+			ifTrue: [errorWasInUIProcess
+					ifTrue: [Project resumeProcess: interruptedProcess]
+					ifFalse: [interruptedProcess resume]]
+			ifFalse: [ScheduledControllers activeControllerNoTerminate: interruptedController andProcess: interruptedProcess]].
 	"if old process was terminated, just terminate current one"
 	interruptedProcess _ nil.
 	"Before delete, so release doesn't terminate it"
 	Smalltalk isMorphic
 		ifTrue: [aTopView delete.
-			Display repaintMorphicDisplayNow]
+			World displayWorld]
 		ifFalse: [aTopView controller closeAndUnscheduleNoErase].
 	Smalltalk installLowSpaceWatcher.
 	"restart low space handler"
