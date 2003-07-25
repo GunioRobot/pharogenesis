73910fa0ebceffa564e9be3c1040d1bd9e5b7780
yo <unknown>
2003-07-25 16:56:00 +0000

    no message

--- HandMorph/instance/private events/generateKeyboardEvent-3A-.cs
+++ HandMorph/instance/private events/generateKeyboardEvent-3A-.cs
@@ -1,19 +1,20 @@
 generateKeyboardEvent: evtBuf
 	"Generate the appropriate mouse event for the given raw event buffer"
-	| buttons modifiers type keyValue pressType stamp |
-	stamp _ (evtBuf at: 2).
-	stamp = 0 ifTrue:[stamp _ Time millisecondClockValue].
-	keyValue _ evtBuf at: 3.
-	pressType _ evtBuf at: 4.
-	pressType = EventKeyDown ifTrue:[type _ #keyDown].
-	pressType = EventKeyUp ifTrue:[type _ #keyUp].
-	pressType = EventKeyChar ifTrue:[type _ #keystroke].
-	modifiers _ evtBuf at: 5.
-	buttons _ modifiers bitShift: 3.
-	^KeyboardEvent new
+
+	| buttons modifiers type pressType stamp char |
+	stamp := evtBuf second.
+	stamp = 0 ifTrue: [stamp := Time millisecondClockValue].
+	pressType := evtBuf fourth.
+	pressType = EventKeyDown ifTrue: [type := #keyDown].
+	pressType = EventKeyUp ifTrue: [type := #keyUp].
+	pressType = EventKeyChar ifTrue: [type := #keystroke].
+	modifiers := evtBuf fifth.
+	buttons := modifiers bitShift: 3.
+	char _ self keyboardInterpreter nextCharFrom: Sensor firstEvt: evtBuf.
+	^ KeyboardEvent new
 		setType: type
 		buttons: buttons
 		position: self position
-		keyValue: keyValue
+		keyValue: char asciiValue
 		hand: self
-		stamp: stamp
+		stamp: stamp.
