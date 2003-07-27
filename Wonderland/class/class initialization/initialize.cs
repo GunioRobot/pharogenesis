afde91a1423ef75fe30db33ffde61b1945f7e8c2
ar <unknown>
2003-07-27 23:11:00 +0000

    no message

--- Wonderland/class/class initialization/initialize.cs
+++ Wonderland/class/class initialization/initialize.cs
@@ -1,4 +1,7 @@
 initialize
 	"Initialize the WonderlandClass by creating the ActorPrototypeClasses collection"
-
+	self initializeWonderlandConstants.
 	ActorPrototypeClasses _ Dictionary new.
+	FileList registerFileReader: self.
+	 (TheWorldMenu respondsTo: #registerOpenCommand:)
+         ifTrue: [TheWorldMenu registerOpenCommand: {'Wonderland 3D'. {self. #new}}].
