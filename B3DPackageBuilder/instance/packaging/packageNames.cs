27987792e0ed94f3557cce1c02c9c0243bbaea69
ar <unknown>
2003-07-27 23:14:00 +0000

    no message

---
+++ B3DPackageBuilder/instance/packaging/packageNames.cs
@@ -0,0 +1,16 @@
+packageNames
+	"Answer the (ordered) DVS packages used to build the standard Balloon3D distribution"
+	^#(
+		'Graphics-Tools'
+		'Graphics-External'
+		'Balloon3D-Constants'
+		'Balloon3D-Kernel'
+		'Balloon3D-Plugins'
+		'Balloon3D-Import'
+		'Balloon3D-VRML'
+		'Balloon3D-Pooh'
+		'Balloon3D-Morphic'
+		'Balloon3D-Tutorial'
+		'Balloon3D-Wonderland'
+		'Balloon3D-Packaging'
+	)
