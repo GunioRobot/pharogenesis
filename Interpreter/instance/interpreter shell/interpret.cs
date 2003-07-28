996598fea8a479b7111dd2694db4aa23281d4c60
tpr <unknown>
2003-07-28 16:47:00 +0000

    no message

--- Interpreter/instance/interpreter shell/interpret.cs
+++ Interpreter/instance/interpreter shell/interpret.cs
@@ -2,8 +2,7 @@ interpret
 	"This is the main interpreter loop. It normally loops forever, fetching and executing bytecodes. When running in the context of a browser plugin VM, however, it must return control to the browser periodically. This should done only when the state of the currently running Squeak thread is safely stored in the object heap. Since this is the case at the moment that a check for interrupts is performed, that is when we return to the browser if it is time to do so. Interrupt checks happen quite frequently."
 
 	"record entry time when running as a browser plug-in"
-	GenerateBrowserPlugin ifTrue: [self plugInSetStartTime].
-
+	self browserPluginInitialiseIfNeeded.
 	self internalizeIPandSP.
 	self fetchNextBytecode.
 	[true] whileTrue: [self dispatchOn: currentBytecode in: BytecodeTable].
