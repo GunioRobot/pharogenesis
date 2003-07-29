695d271f0d80190bd91bb7f7ca52a1399b3a63d5
yo <unknown>
2003-07-29 22:58:00 +0000

    no message

---
+++ PositionableStream/instance/fileIn-2F-Out/decodeString-3A-andRuns-3A-.cs
@@ -0,0 +1,25 @@
+decodeString: string andRuns: runsRaw
+
+	| strm runLength runValues newString index |
+	strm _ ReadStream on: runsRaw from: 1 to: runsRaw size.
+	(strm peekFor: $( ) ifFalse: [^ nil].
+	runLength _ OrderedCollection new.
+	[strm skipSeparators.
+	 strm peekFor: $)] whileFalse: 
+		[runLength add: (Number readFrom: strm)].
+
+	runValues _ OrderedCollection new.
+	[strm atEnd not] whileTrue: 
+		[runValues add: (Number readFrom: strm).
+		strm next.].
+
+	newString _ MultiString new: string size.
+	index _ 1.
+	runLength with: runValues do: [:length :leadingChar |
+		index to: index + length - 1 do: [:pos |
+			newString at: pos put: (MultiCharacter leadingChar: leadingChar code: (string at: pos) charCode).
+		].
+		index _ index + length.
+	].
+
+	^ newString.
