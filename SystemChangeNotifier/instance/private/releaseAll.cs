94eebffcecd22526f4630bec1311b0aa5c8fb3aa
rw <unknown>
2003-07-29 17:05:00 +0000

    no message

---
+++ SystemChangeNotifier/instance/private/releaseAll.cs
@@ -0,0 +1,7 @@
+releaseAll
+	"Release all the dependents so that nobody receives notifications anymore."
+
+	"Done for cleaning up the system."
+	"self uniqueInstance releaseAll"
+
+	eventSource releaseActionMap
