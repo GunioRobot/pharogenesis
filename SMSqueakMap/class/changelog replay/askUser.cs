d2d6559acf17c13295dbbe06385f0c08c3392c34
gk <unknown>
2003-07-29 01:01:00 +0000

    no message

---
+++ SMSqueakMap/class/changelog replay/askUser.cs
@@ -0,0 +1,18 @@
+askUser
+	"Ask user about how to handle a replayed installation note
+	when there is no current SqueakMap in the image."
+
+	| choice |
+	[choice _ PopUpMenu withCaption:
+'There is no SqueakMap in this image,
+do you wish to create/recreate it? (typical answer is Yes)' chooseFrom: 'Yes\No\More info'.
+			choice = 3] whileTrue: [self inform:
+'When packages are installed using SqueakMap a little mark is made
+in the change log. When an image is reconstructed from the changelog
+these marks are intended to keep your map informed about what packages
+are installed. You probably already have a map on disk which will automatically be
+reloaded if you choose ''Yes'', otherwise an new empty map will be created.
+If you choose ''No'', it will only result in that SqueakMap will not know that this package
+is installed in your image.
+If you are still unsure - answer ''Yes'' since that is probably the best.'].
+	^choice = 1
