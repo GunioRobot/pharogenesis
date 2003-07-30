df301219b6616c5cd0b9e852856d866ba0d0b164
JF <unknown>
2003-07-30 16:05:00 +0000

    no message

--- TestResult/instance/running/runCase-3A-.cs
+++ TestResult/instance/running/runCase-3A-.cs
@@ -1,7 +1,17 @@
 runCase: aTestCase
-
-	self runCount: self runCount + 1.
-	[[aTestCase runCase]		on: self testFailureException do: [:ex |
-			self failures add: aTestCase]]
-		on: Error do: [:ex |
-			self errors add: aTestCase].
+	| testCasePassed |
+	testCasePassed := true.
+	[[aTestCase runCase] 
+			sunitOn: self class failure
+			do: 
+				[:signal | 
+				failures add: aTestCase.
+				testCasePassed := false.
+				signal sunitExitWith: false]]
+					sunitOn: self class error
+					do:
+						[:signal |
+						errors add: aTestCase.
+						testCasePassed := false.
+						signal sunitExitWith: false].
+	testCasePassed ifTrue: [passed add: aTestCase]
