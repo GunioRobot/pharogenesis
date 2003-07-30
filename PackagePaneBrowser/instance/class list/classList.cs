e0a98793fb5307286bff273f16c63da86b14b259
JF <unknown>
2003-07-30 12:26:00 +0000

    no message

--- PackagePaneBrowser/instance/class list/classList.cs
+++ PackagePaneBrowser/instance/class list/classList.cs
@@ -2,10 +2,8 @@ classList
 	"Answer an array of the class names of the selected category. Answer an 
 	empty array if no selection exists."
 
-	^systemCategoryListIndex = 0
-		ifTrue: [
-			self systemCategoryList isEmpty
-				ifTrue: [systemOrganizer listAtCategoryNumber: (systemOrganizer categories indexOf: self package asSymbol)]
-				ifFalse: [Array new]]
-		ifFalse: [systemOrganizer listAtCategoryNumber:
+	^ self hasSystemCategorySelected 
+		ifFalse:
+			[self packageClasses]
+		ifTrue: [systemOrganizer listAtCategoryNumber:
 			(systemOrganizer categories indexOf: self selectedSystemCategoryName asSymbol)]
