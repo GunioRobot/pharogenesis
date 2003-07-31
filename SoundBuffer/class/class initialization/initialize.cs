e36b4ed3a4605c522b99dd60886d0f98787c8211
ads <unknown>
2003-07-31 11:13:00 +0000

    no message

---
+++ SoundBuffer/class/class initialization/initialize.cs
@@ -0,0 +1,11 @@
+initialize
+	"Build a sine wave table."
+	"SoundBuffer initialize"
+
+	| tableSize radiansPerStep peak |
+	tableSize _ 4000.
+	SineTable _ self newMonoSampleCount: tableSize.
+	radiansPerStep _ (2.0 * Float pi) / tableSize asFloat.
+	peak _ ((1 bitShift: 15) - 1) asFloat.  "range is +/- (2^15 - 1)"
+	1 to: tableSize do: [:i |
+		SineTable at: i put: (peak * (radiansPerStep * (i - 1)) sin) rounded].
