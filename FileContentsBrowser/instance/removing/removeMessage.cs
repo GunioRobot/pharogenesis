f7aad53c51e34c7212a99217d4673b9bb9ba9687
ar <unknown>
2003-08-02 21:00:00 +0000

    no message

--- FileContentsBrowser/instance/removing/removeMessage.cs
+++ FileContentsBrowser/instance/removing/removeMessage.cs
@@ -1,10 +1,14 @@
 removeMessage
 	| messageName |
-	messageListIndex = 0 ifTrue: [^ self].
-	self okToChange ifFalse: [^ self].
+	messageListIndex = 0
+		ifTrue: [^ self].
+	self okToChange
+		ifFalse: [^ self].
 	messageName _ self selectedMessageName.
-	(self selectedClassOrMetaClass confirmRemovalOf: messageName) ifFalse:[^false].
+	(self selectedClass confirmRemovalOf: messageName)
+		ifFalse: [^ false].
 	self selectedClassOrMetaClass removeMethod: self selectedMessageName.
 	self messageListIndex: 0.
-	self setClassOrganizer.  "In case organization not cached"
-	self changed: #messageList.
+	self setClassOrganizer.
+	"In case organization not cached"
+	self changed: #messageList
