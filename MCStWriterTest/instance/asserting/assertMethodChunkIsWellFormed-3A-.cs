a2c68f545dd0b469ab883082e363aec8a4c50dbe
cwp <unknown>
2003-08-02 12:47:00 +0000

    no message

---
+++ MCStWriterTest/instance/asserting/assertMethodChunkIsWellFormed-3A-.cs
@@ -0,0 +1,8 @@
+assertMethodChunkIsWellFormed: chunk
+	Parser new
+		parse: chunk readStream 
+		class: UndefinedObject 
+		noPattern: false
+		context: nil
+		notifying: nil
+		ifFail: [self assert: false]
