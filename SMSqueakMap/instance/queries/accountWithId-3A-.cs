0acdaac5822a80f1fadf93feb3ac8016098e9fda
gk <unknown>
2003-08-04 16:30:00 +0000

    no message

---
+++ SMSqueakMap/instance/queries/accountWithId-3A-.cs
@@ -0,0 +1,9 @@
+accountWithId: anIdString 
+	"Look up an account. Return nil if missing.
+	Raise error if it is not an account."
+
+	| account |
+	account _ self objectWithId: anIdString.
+	account ifNil: [^nil].
+	account isAccount ifTrue:[^account].
+	self error: 'UUID did not map to a account.'
