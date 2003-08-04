303855be8f9d395f9cbab5c6e8741bab79dfa2c8
gk <unknown>
2003-08-04 14:50:00 +0000

    no message

---
+++ SMAccount/instance/passwords/correctPassword-3A-.cs
@@ -0,0 +1,8 @@
+correctPassword: aPassword
+	"We store the password as a SHA hash so that we can let the slave maps
+	have them too. Also check the optional new random password."
+
+	| try |
+	aPassword isEmptyOrNil ifTrue:[^false].
+	try _ SecureHashAlgorithm new hashMessage: aPassword.
+	^password = try or: [newPassword = try]
