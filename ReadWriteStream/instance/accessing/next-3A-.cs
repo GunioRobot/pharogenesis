26609ecdf17bd816078b5012503eced36457fe27
ar <unknown>
2003-08-05 02:23:00 +0000

    no message

--- ReadWriteStream/instance/accessing/next-3A-.cs
+++ ReadWriteStream/instance/accessing/next-3A-.cs
@@ -2,6 +2,7 @@ next: anInteger
 	"Answer the next anInteger elements of my collection.  overriden for efficiency"
 
 	| ans endPosition |
+	readLimit := readLimit max: position.
 
 	endPosition _ position + anInteger  min:  readLimit.
 	ans _ collection copyFrom: position+1 to: endPosition.
