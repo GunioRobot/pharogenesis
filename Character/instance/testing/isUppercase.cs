fb8234b74f5657831791577029b5ba3422f8ab4e
yo <unknown>
2003-08-05 16:43:00 +0000

    no message

--- Character/instance/testing/isUppercase.cs
+++ Character/instance/testing/isUppercase.cs
@@ -1,5 +1,3 @@
 isUppercase
-	"Answer whether the receiver is an uppercase letter.
-	(The old implementation answered whether the receiver is not a lowercase letter.)"
 
-	^8r101 <= value and: [value <= 8r132]
+	^ (EncodedCharSet charsetAt: self leadingChar) isUppercase: self.
