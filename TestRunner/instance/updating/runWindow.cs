ff9c5aed66fd5d381692f42d19dd4ce10743e8d9
nk <unknown>
2003-08-06 09:07:00 +0000

    no message

---
+++ TestRunner/instance/updating/runWindow.cs
@@ -0,0 +1,12 @@
+runWindow
+	| pc |
+	pc _ self defaultBackgroundColor.
+	passFailText isMorph
+		ifTrue: [passFailText color: pc.
+			detailsText color: pc]
+		ifFalse: [passFailText insideColor: pc.
+			detailsText insideColor: pc].
+	self updateErrors: TestResult new.
+	self updateFailures: TestResult new.
+	self displayPassFail: 'Running...'.
+	self displayDetails: '...'
