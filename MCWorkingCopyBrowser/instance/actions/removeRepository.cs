5e04a70c323c6390bba3549b0d6c3f2714afbdb7
ab <unknown>
2003-08-07 21:33:00 +0000

    no message

---
+++ MCWorkingCopyBrowser/instance/actions/removeRepository.cs
@@ -0,0 +1,6 @@
+removeRepository
+	self repository ifNotNilDo:
+		[:repos |
+		self repositoryGroup removeRepository: repos.
+		self repositorySelection: (1 min: self repositories size)].
+	self changed: #repositoryList
