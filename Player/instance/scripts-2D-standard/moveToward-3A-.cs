568c1d605d9fabd75c43fba8110306c9ac29a0dd
dgd <unknown>
2003-08-08 22:15:00 +0000

    no message

--- Player/instance/scripts-2D-standard/moveToward-3A-.cs
+++ Player/instance/scripts-2D-standard/moveToward-3A-.cs
@@ -1,9 +1,5 @@
 moveToward: aPlayer
 	"Move a standard amount in the direction of the given player.  If the object has an instance variable named 'speed', the speed of the motion will be governed by that value"
 
-	| angle aCostume |
-	(aPlayer == nil or: [aPlayer == self]) ifTrue: [^ self].
-	((aCostume _ self costume) bounds intersects: aPlayer costume bounds) ifTrue: [^ self].
-	angle _ aCostume referencePosition bearingToPoint: aPlayer costume referencePosition.
-	self setHeading: angle.
+	self turnToward: aPlayer.
 	self forward: self getSpeed
