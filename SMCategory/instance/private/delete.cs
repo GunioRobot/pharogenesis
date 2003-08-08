1db59d366936d2e3e05f0667a6e06177fd99ce30
gk <unknown>
2003-08-08 10:03:00 +0000

    no message

---
+++ SMCategory/instance/private/delete.cs
@@ -0,0 +1,9 @@
+delete
+	"Delete me. Disconnect me from my objects and my parent.
+	Then delete my subcategories."
+
+	super delete.
+	self removeFromObjects; removeFromParent.
+	self subCategories do: [:c | c delete ]
+	
+	
