e88923e40723614021dcf3f3ac0523ee878f121e
ar <unknown>
2003-08-10 18:31:00 +0000

    no message

--- Morph/instance/submorphs-2D-add-2F-remove/privateDelete.cs
+++ Morph/instance/submorphs-2D-add-2F-remove/privateDelete.cs
@@ -1,6 +1,3 @@
 privateDelete
-	"Remove the receiver as a submorph of its owner and make its new owner be nil, without informing anyone other than my owner"
-
-	owner ifNotNil:
-		[owner privateRemoveMorph: self.
-		owner _ nil].
+	"Remove the receiver as a submorph of its owner"
+	owner ifNotNil:[owner removeMorph: self].
