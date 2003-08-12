928f1ffa46970ece5574340276d3b54c82484ca6
yo <unknown>
2003-08-12 17:14:00 +0000

    no message

---
+++ MultiString/class/enumeration/allMultiStringMethods.cs
@@ -0,0 +1,28 @@
+allMultiStringMethods  
+	"Answer a SortedCollection of all the methods that implement the message 
+	aSelector."
+
+	| list adder num i |
+	list _ Set new.
+	adder _ [ :mrClass :mrSel |
+		list add: (
+			MethodReference new
+				setStandardClass: mrClass
+				methodSymbol: mrSel
+		)
+	].
+
+	num _ CompiledMethod allInstances size.
+	i _ 0.
+	'processing...' displayProgressAt: Sensor cursorPoint from: 0 to: num during: [:bar |
+		SystemNavigation new allBehaviorsDo: [ :class |
+			class selectors do: [:s |
+				bar value: (i _ i + 1).				
+				((class sourceCodeAt: s) asString isOctetString) ifFalse: [
+					adder value: class value: s.
+				]
+			]
+		]
+	].
+
+	^ list.
