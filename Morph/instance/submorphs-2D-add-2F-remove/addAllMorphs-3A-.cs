d2b6d7caa0844347cd1a31c9bcf76ac6320f7deb
ar <unknown>
2003-08-12 23:28:00 +0000

    no message

--- Morph/instance/submorphs-2D-add-2F-remove/addAllMorphs-3A-.cs
+++ Morph/instance/submorphs-2D-add-2F-remove/addAllMorphs-3A-.cs
@@ -1,20 +1,2 @@
 addAllMorphs: aCollection
-	| any box invalidate myWorld itsWorld |
-	myWorld _ self world.
-	invalidate _ myWorld notNil.
-	box _ nil.
-	aCollection do: [:m |
-		any _ m.
-		m owner ifNotNil: [
-			itsWorld _ m world.
-			itsWorld == myWorld ifFalse:[m outOfWorld: itsWorld].
-			m owner privateRemoveMorph: m].
-		invalidate ifTrue:[
-			box ifNil:[box _ m fullBounds copy] 
-				ifNotNil:[box _ box quickMerge: m fullBounds]].
-		m privateOwner: self.
-		itsWorld == myWorld ifFalse:[m intoWorld: myWorld].
-	].
-	submorphs _ submorphs, aCollection.
-	self layoutChanged.
-	box ifNotNil:[self invalidRect: box from: any].
+	^self privateAddAllMorphs: aCollection atIndex: submorphs size
