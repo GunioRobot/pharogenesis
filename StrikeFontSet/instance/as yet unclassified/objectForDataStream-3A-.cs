7c42fdf6a0d484687b30799d4c85016dff87fe11
yo <unknown>
2003-08-18 21:17:00 +0000

    no message

---
+++ StrikeFontSet/instance/as yet unclassified/objectForDataStream-3A-.cs
@@ -0,0 +1,13 @@
+objectForDataStream: refStrm
+	| dp |
+	"I am about to be written on an object file.  Write a reference to a known Font in the other system instead.  "
+
+	"A path to me"
+	(TextConstants at: #forceFontWriting ifAbsent: [false]) ifTrue: [^ self].
+		"special case for saving the default fonts on the disk.  See collectionFromFileNamed:"
+
+	dp _ DiskProxy global: #StrikeFontSet selector: #familyName:size:emphasized:
+			args: (Array with: self familyName with: self pointSize
+					with: self emphasis).
+	refStrm replace: self with: dp.
+	^ dp.
