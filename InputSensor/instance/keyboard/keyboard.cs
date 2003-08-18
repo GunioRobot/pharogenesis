b62cc00a2e9bb2dfae51188ae1f273401caa0629
yo <unknown>
2003-08-18 23:36:00 +0000

    no message

--- InputSensor/instance/keyboard/keyboard.cs
+++ InputSensor/instance/keyboard/keyboard.cs
@@ -1,4 +1,18 @@
 keyboard
 	"Answer the next character from the keyboard."
 
-	^ self characterForKeycode: self primKbdNext
+	| firstCharacter secondCharactor stream multiCharacter converter |
+	firstCharacter _ self characterForKeycode: self primKbdNext.
+	secondCharactor _ self characterForKeycode: self primKbdPeek.
+	secondCharactor isNil
+		ifTrue: [^ firstCharacter].
+	converter _ TextConverter defaultSystemConverter.
+	converter isNil
+		ifTrue: [^ firstCharacter].
+	stream _ ReadStream
+				on: (String with: firstCharacter with: secondCharactor).
+	multiCharacter _ converter nextFromStream: stream.
+	multiCharacter isOctetCharacter
+		ifTrue: [^ multiCharacter].
+	self primKbdNext.
+	^ multiCharacter
