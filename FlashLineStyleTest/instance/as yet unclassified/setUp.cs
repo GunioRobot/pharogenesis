83543beeb7c1c8c8d068080005f28bf96fa7639c
mjr <unknown>
2003-08-20 18:56:00 +0000

    no message

---
+++ FlashLineStyleTest/instance/as yet unclassified/setUp.cs
@@ -0,0 +1,10 @@
+setUp
+	super setUp.
+	prototypes
+		add: (FlashLineStyle color: 1 width: 1);
+		
+		add: (FlashLineStyle color: 1 width: 2);
+		
+		add: (FlashLineStyle color: 2 width: 1);
+		
+		add: (FlashLineStyle color: 2 width: 2) 
