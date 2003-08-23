2d753fbd330bd3a9ec9e7e5a14d93079c227ed69
dvf <unknown>
2003-08-23 11:51:00 +0000

    no message

--- Dictionary/instance/removing/unreferencedKeys.cs
+++ Dictionary/instance/removing/unreferencedKeys.cs
@@ -1,10 +1,15 @@
-unreferencedKeys		"TextConstants unreferencedKeys"
+unreferencedKeys
+	"TextConstants unreferencedKeys"
+
 	| n |
-	^ 'Scanning for references . . .'
+	^'Scanning for references . . .' 
 		displayProgressAt: Sensor cursorPoint
-		from: 0 to: self size
-		during:
-		[:bar | n _ 0.
-		self keys select:
-			[:key | bar value: (n _ n+1).
-			(Smalltalk allCallsOn: (self associationAt: key)) isEmpty]]
+		from: 0
+		to: self size
+		during: 
+			[:bar | 
+			n := 0.
+			self keys select: 
+					[:key | 
+					bar value: (n := n + 1).
+					(self systemNavigation allCallsOn: (self associationAt: key)) isEmpty]]
