a810dbe4d771a162c30d7165c4f363a4e6439e52
Bouraqadi <unknown>
2003-08-23 14:51:00 +0000

    no message

--- Behavior/instance/instance creation/new.cs
+++ Behavior/instance/instance creation/new.cs
@@ -1,5 +1,4 @@
 new
-	"Answer a new instance of the receiver (which is a class) with no indexable variables. Fail if the class is indexable."
+	"Answer a new initialized instance of the receiver (which is a class) with no indexable variables. Fail if the class is indexable."
 
-	<primitive: 70>  "This method runs primitively if successful"
-	^ self basicNew  "Exceptional conditions will be handled in basicNew"
+	^ self basicNew initialize
