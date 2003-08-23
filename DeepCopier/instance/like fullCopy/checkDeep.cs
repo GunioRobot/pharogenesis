3ea912e1c8bdeadce04db10bdc14dc5e29a3c7e1
dvf <unknown>
2003-08-23 11:52:00 +0000

    no message

--- DeepCopier/instance/like fullCopy/checkDeep.cs
+++ DeepCopier/instance/like fullCopy/checkDeep.cs
@@ -1,12 +1,23 @@
 checkDeep
-	"Write exceptions in the Transcript.  Every class that implements veryDeepInner: must copy all its inst vars.  Danger is that a user will add a new instance variable and forget to copy it.  This check is only run by hand once in a while to make sure nothing was forgotten.
+	"Write exceptions in the Transcript.  Every class that implements veryDeepInner: must copy all its inst vars.  Danger is that a user will add a new instance variable and forget to copy it.  This check is only run by hand once in a while to make sure nothing was forgotten.  
+(Please do not remove this method.)
 	DeepCopier new checkDeep 	"
 
-| mm |
-Transcript cr; show: 'Instance variables shared with the original object when it is copied'.
-(Smalltalk allClassesImplementing: #veryDeepInner:) do: [:aClass | 
-	(mm _ aClass instVarNames size) > 0 ifTrue: [
-		(aClass instSize - mm + 1) to: aClass instSize do: [:index |
-			((aClass compiledMethodAt: #veryDeepInner:) writesField: index) ifFalse: [
-				Transcript cr; show: aClass name; space; 
-					show: (aClass allInstVarNames at: index)]]]].
+	| mm |
+	Transcript
+		cr;
+		show: 'Instance variables shared with the original object when it is copied'.
+	(self systemNavigation allClassesImplementing: #veryDeepInner:) do: 
+			[:aClass | 
+			(mm := aClass instVarNames size) > 0 
+				ifTrue: 
+					[aClass instSize - mm + 1 to: aClass instSize
+						do: 
+							[:index | 
+							((aClass compiledMethodAt: #veryDeepInner:) writesField: index) 
+								ifFalse: 
+									[Transcript
+										cr;
+										show: aClass name;
+										space;
+										show: (aClass allInstVarNames at: index)]]]]
