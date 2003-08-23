44163f32e2594e2eab64bb6919891d1902574a2e
dvf <unknown>
2003-08-23 11:50:00 +0000

    no message

--- CompiledMethod/instance/printing/who.cs
+++ CompiledMethod/instance/printing/who.cs
@@ -1,10 +1,10 @@
-who 
+who
 	"Answer an Array of the class in which the receiver is defined and the 
 	selector to which it corresponds."
 
 	| sel |
-	Smalltalk allBehaviorsDo:
-		[:class |
-		(sel _ class methodDict keyAtIdentityValue: self ifAbsent: [nil])
-			ifNotNil: [^Array with: class with: sel]].
-	^ Array with: #unknown with: #unknown
+	self systemNavigation allBehaviorsDo: 
+			[:class | 
+			(sel := class methodDict keyAtIdentityValue: self ifAbsent: [nil]) 
+				ifNotNil: [^Array with: class with: sel]].
+	^Array with: #unknown with: #unknown
