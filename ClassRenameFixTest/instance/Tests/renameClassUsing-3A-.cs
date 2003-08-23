856bb5ff1dbd8c80fa862dbbae06c105302dddac
rw <unknown>
2003-08-23 16:46:00 +0000

    no message

---
+++ ClassRenameFixTest/instance/Tests/renameClassUsing-3A-.cs
@@ -0,0 +1,18 @@
+renameClassUsing: aBlock
+
+	| originalName createdClass newClassName foundClasses |
+	originalName := self newUniqueClassName.
+	createdClass := Object 
+		subclass: originalName
+		instanceVariableNames: ''
+		classVariableNames: ''
+		poolDictionaries: ''
+		category: 'ClassRenameFix-GeneradClass'.
+	newClassName := self newUniqueClassName.
+	aBlock value: createdClass value: newClassName.
+	self assert: (Smalltalk classNamed: originalName) isNil.
+	self assert: (Smalltalk classNamed: newClassName) notNil.
+	foundClasses := Smalltalk organization listAtCategoryNamed: 'ClassRenameFix-GeneradClass'.
+	self assert: (foundClasses notEmpty).
+	self assert: (foundClasses includes: newClassName).
+	self assert: (createdClass name = newClassName).
