af1364ff290932cea91aff0f29b788d9fb188875
dgd <unknown>
2003-08-26 21:10:00 +0000

    no message

--- PasteUpMorph/instance/world menu/putUpDesktopMenu-3A-.cs
+++ PasteUpMorph/instance/world menu/putUpDesktopMenu-3A-.cs
@@ -1,3 +1,3 @@
 putUpDesktopMenu: evt
 	"Put up the desktop menu"
-	^ ((self buildWorldMenu: evt) addTitle: Preferences desktopMenuTitle) popUpAt: evt position forHand: evt hand in: self
+	^ ((self buildWorldMenu: evt) addTitle: Preferences desktopMenuTitle translated) popUpAt: evt position forHand: evt hand in: self
