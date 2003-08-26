f31d769f4e7577f1daf8a732a74e14d97192d6dc
dgd <unknown>
2003-08-26 21:44:00 +0000

    no message

--- Morph/instance/dropping-2F-grabbing/undoGrabCommand.cs
+++ Morph/instance/dropping-2F-grabbing/undoGrabCommand.cs
@@ -1,9 +1,12 @@
 undoGrabCommand
-	"Return the undo command for grabbing the receiver"
+	"Return an undo command for grabbing the receiver"
+
 	| cmd |
-	owner ifNil:[^nil]. "no owner - no undo"
-	^(cmd _ Command new)
-		cmdWording: 'move ', (String streamContents: [:s | self printOn: s] limitedTo: 18);
+	owner ifNil:
+		[^ nil]. "no owner - no undo"
+	^ (cmd _ Command new)
+		cmdWording: 'move ' translated, self nameForUndoWording;
 		undoTarget: self
 		selector: #undoMove:redo:owner:bounds:predecessor:
-		arguments: {cmd. false. owner. self bounds. (owner morphPreceding: self)}
+		arguments: {cmd. false. owner. self bounds. (owner morphPreceding: self)};
+		yourself
