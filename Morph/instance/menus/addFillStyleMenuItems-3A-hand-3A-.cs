93a894e1096b873007007d431eaf0b289e34bc74
dgd <unknown>
2003-08-30 16:44:00 +0000

    no message

--- Morph/instance/menus/addFillStyleMenuItems-3A-hand-3A-.cs
+++ Morph/instance/menus/addFillStyleMenuItems-3A-hand-3A-.cs
@@ -1,13 +1,13 @@
 addFillStyleMenuItems: aMenu hand: aHand
 	"Add the items for changing the current fill style of the Morph"
 	| menu |
-	self canHaveFillStyles ifFalse:[^aMenu add: 'change color...' target: self action: #changeColor].
+	self canHaveFillStyles ifFalse:[^aMenu add: 'change color...' translated target: self action: #changeColor].
 	menu _ MenuMorph new defaultTarget: self.
 	self fillStyle addFillStyleMenuItems: menu hand: aHand from: self.
 	menu addLine.
-	menu add: 'solid fill' action: #useSolidFill.
-	menu add: 'gradient fill' action: #useGradientFill.
-	menu add: 'bitmap fill' action: #useBitmapFill.
-	menu add: 'default fill' action: #useDefaultFill.
-	aMenu add: 'fill style' subMenu: menu.
-	"aMenu add: 'change color...' action: #changeColor"
+	menu add: 'solid fill' translated action: #useSolidFill.
+	menu add: 'gradient fill' translated action: #useGradientFill.
+	menu add: 'bitmap fill' translated action: #useBitmapFill.
+	menu add: 'default fill' translated action: #useDefaultFill.
+	aMenu add: 'fill style' translated subMenu: menu.
+	"aMenu add: 'change color...' translated action: #changeColor"
