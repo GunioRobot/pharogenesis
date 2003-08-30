6104b0e366485105d6d189073d20591060e10295
dgd <unknown>
2003-08-30 21:41:00 +0000

    no message

--- FlashCharacterMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ FlashCharacterMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,4 +1,4 @@
 addCustomMenuItems: aMenu hand: aHand
 	super addCustomMenuItems: aMenu hand: aHand.
-	aMenu add:'add project target' action: #addProjectTarget.
-	aMenu add:'remove project target' action: #removeProjectTarget.
+	aMenu add:'add project target' translated action: #addProjectTarget.
+	aMenu add:'remove project target' translated action: #removeProjectTarget.
