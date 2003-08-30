0350aa11177aebe2a353bfb909262ddb2b147db9
dgd <unknown>
2003-08-30 16:55:00 +0000

    no message

--- Morph/instance/layout-2D-menu/hasProportionalLayoutString.cs
+++ Morph/instance/layout-2D-menu/hasProportionalLayoutString.cs
@@ -1,5 +1,7 @@
 hasProportionalLayoutString
 	| layout |
-	^((layout _ self layoutPolicy) notNil and:[layout isProportionalLayout])
-		ifTrue:['<on>proportional layout']
-		ifFalse:['<off>proportional layout'].
+	^ (((layout := self layoutPolicy) notNil
+			and: [layout isProportionalLayout])
+		ifTrue: ['<on>']
+		ifFalse: ['<off>'])
+		, 'proportional layout' translated
