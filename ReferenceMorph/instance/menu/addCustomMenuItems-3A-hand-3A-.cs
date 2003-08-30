0feea102cd3469d0a602f9e4fb4615d771d42ee6
dgd <unknown>
2003-08-30 21:59:00 +0000

    no message

--- ReferenceMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ ReferenceMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -6,10 +6,10 @@ addCustomMenuItems: aCustomMenu hand: aHandMorph
 	aCustomMenu addLine.
 	self isCurrentlyTextual
 		ifTrue:
-			[aCustomMenu add: 'change label wording...' action: #changeTabText.
-			aCustomMenu add: 'use graphical label' action: #useGraphicalTab]
+			[aCustomMenu add: 'change label wording...' translated action: #changeTabText.
+			aCustomMenu add: 'use graphical label' translated action: #useGraphicalTab]
 		ifFalse:
-			[aCustomMenu add: 'use textual label' action: #useTextualTab.
-			aCustomMenu add: 'choose graphic...' action: #changeTabGraphic.
+			[aCustomMenu add: 'use textual label' translated action: #useTextualTab.
+			aCustomMenu add: 'choose graphic...' translated action: #changeTabGraphic.
 			(sketch _ self findA: SketchMorph) ifNotNil:
-				[aCustomMenu add: 'repaint' target: sketch action: #editDrawing]]
+				[aCustomMenu add: 'repaint' translated target: sketch action: #editDrawing]]
