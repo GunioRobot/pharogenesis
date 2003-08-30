f08dfb73e27fe594d8abb9ce46a59656f9cfab75
dgd <unknown>
2003-08-30 21:56:00 +0000

    no message

--- PasteUpMorph/instance/menu & halo/addCustomMenuItems-3A-hand-3A-.cs
+++ PasteUpMorph/instance/menu & halo/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,11 +1,18 @@
 addCustomMenuItems: menu hand: aHandMorph
+	"Add morph-specific menu itemns to the menu for the hand"
 
 	super addCustomMenuItems: menu hand: aHandMorph.
 	self addStackMenuItems: menu hand: aHandMorph.
 	self addPenMenuItems: menu hand: aHandMorph.
 	self addPlayfieldMenuItems: menu hand: aHandMorph.
-	(self isWorldMorph and: [owner isKindOf: BOBTransformationMorph]) ifTrue: [
-		self addScalingMenuItems: menu hand: aHandMorph.
-	].
+
+	self isWorldMorph ifTrue:
+		[(owner isKindOf: BOBTransformationMorph) ifTrue:
+			[self addScalingMenuItems: menu hand: aHandMorph].
+		Flaps sharedFlapsAllowed ifTrue:
+			[menu addUpdating: #suppressFlapsString
+				target: CurrentProjectRefactoring 
+				action: #currentToggleFlapsSuppressed].
+		menu add: 'desktop menu...' translated target: self action: #putUpDesktopMenu:].
 
 	menu addLine
