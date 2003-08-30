6a76a0055ced681febf359a0415c7e55702deb59
dgd <unknown>
2003-08-30 21:31:00 +0000

    no message

--- FlapTab/instance/parts bin/partsBinString.cs
+++ FlapTab/instance/parts bin/partsBinString.cs
@@ -1,6 +1,6 @@
 partsBinString
-	"Answer the string to be shown in a menu to represent the parts-bin status"
-
-	^ referent isPartsBin
-		ifTrue:	['<yes>parts-bin']
-		ifFalse:	['<no>parts-bin']
+	"Answer the string to be shown in a menu to represent the 
+	parts-bin status"
+	^ (referent isPartsBin
+		ifTrue: ['<yes>']
+		ifFalse: ['<no>']), 'parts-bin' translated
