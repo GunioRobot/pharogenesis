b179d45da35232522b2ce3633b30f14f1d60a18c
dgd <unknown>
2003-08-30 21:42:00 +0000

    no message

--- FlashMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ FlashMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,4 +1,4 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
 	aCustomMenu addUpdating: #getSmoothingLevel action: #nextSmoothingLevel.
-	aCustomMenu add:'show compressed size' action: #showCompressedSize.
+	aCustomMenu add:'show compressed size' translated action: #showCompressedSize.
