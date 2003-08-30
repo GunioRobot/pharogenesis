87a5f0475ff603d58d5de871b414d826476c667c
dgd <unknown>
2003-08-30 21:14:00 +0000

    no message

--- TabbedPalette/instance/palette menu/addBookMenuItemsTo-3A-hand-3A-.cs
+++ TabbedPalette/instance/palette menu/addBookMenuItemsTo-3A-hand-3A-.cs
@@ -1,3 +1,3 @@
-addBookMenuItemsTo: aCustomMenu hand: aHandMorph
-	aCustomMenu add: 'add palette menu' action: #addMenuTab.
-	aCustomMenu add: 'become the Standard palette' action: #becomeStandardPalette
+addBookMenuItemsTo: aCustomMenu hand: aHandMorph 
+	aCustomMenu add: 'add palette menu' translated action: #addMenuTab.
+	aCustomMenu add: 'become the Standard palette' translated action: #becomeStandardPalette
