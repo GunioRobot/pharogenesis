dd86145344b2bbf1d2eeea979de55a0dc07f416c
dgd <unknown>
2003-08-30 21:54:00 +0000

    no message

--- MouseDownMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ MouseDownMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -2,17 +2,17 @@ addCustomMenuItems: aCustomMenu hand: aHandMorph
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
 "template..."
 	aCustomMenu addLine.
-	aCustomMenu add: 'set variable name...' action: #renameMe.
+	aCustomMenu add: 'set variable name...' translated action: #renameMe.
 	aCustomMenu addLine.
-	aCustomMenu add: 'plug mouseDown to model slot' action: #plugMouseDownToSlot.
-	aCustomMenu add: 'plug mouseMove to model slot' action: #plugMouseMoveToSlot.
-	aCustomMenu add: 'plug all to model slots' action: #plugAllToSlots.
+	aCustomMenu add: 'plug mouseDown to model slot' translated action: #plugMouseDownToSlot.
+	aCustomMenu add: 'plug mouseMove to model slot' translated action: #plugMouseMoveToSlot.
+	aCustomMenu add: 'plug all to model slots' translated action: #plugAllToSlots.
 	aCustomMenu addLine.
-	aCustomMenu add: 'plug mouseDown to model' action: #plugMouseDownToModel.
-	aCustomMenu add: 'plug mouseMove to model' action: #plugMouseMoveToModel.
-	aCustomMenu add: 'plug all to model' action: #plugAllToModel.
+	aCustomMenu add: 'plug mouseDown to model' translated action: #plugMouseDownToModel.
+	aCustomMenu add: 'plug mouseMove to model' translated action: #plugMouseMoveToModel.
+	aCustomMenu add: 'plug all to model' translated action: #plugAllToModel.
 	aCustomMenu addLine.
-	aCustomMenu add: 'set target...' action: #setTarget.
-	aCustomMenu add: 'set mouseDown selector...' action: #setMouseDownSelector.
-	aCustomMenu add: 'set mouseMove selector...' action: #setMouseMoveSelector.
-	aCustomMenu add: 'set mouseUp selector...' action: #setMouseUpSelector.
+	aCustomMenu add: 'set target...' translated action: #setTarget.
+	aCustomMenu add: 'set mouseDown selector...' translated action: #setMouseDownSelector.
+	aCustomMenu add: 'set mouseMove selector...' translated action: #setMouseMoveSelector.
+	aCustomMenu add: 'set mouseUp selector...' translated action: #setMouseUpSelector.
