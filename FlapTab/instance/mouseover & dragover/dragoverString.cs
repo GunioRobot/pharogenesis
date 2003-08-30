4e3694f24aaa0bbab13fe5d6216c25c379ca7aa3
dgd <unknown>
2003-08-30 21:32:00 +0000

    no message

--- FlapTab/instance/mouseover & dragover/dragoverString.cs
+++ FlapTab/instance/mouseover & dragover/dragoverString.cs
@@ -1,8 +1,6 @@
 dragoverString
-	"Answer the string to be shown in a menu to represent the dragover status"
-
-	^ popOutOnDragOver
-		ifTrue:
-			['<yes>pop out on dragover']
-		ifFalse:
-			['<no>pop out on dragover']
+	"Answer the string to be shown in a menu to represent the 
+	dragover status"
+	^ (popOutOnDragOver
+		ifTrue: ['<yes>']
+		ifFalse: ['<no>']), 'pop out on dragover' translated
