1fa239f91de2717e65c077c1bd71bd7c13e1894b
dgd <unknown>
2003-08-30 22:24:00 +0000

    no message

--- WatchMorph/instance/menus/addCustomMenuItems-3A-hand-3A-.cs
+++ WatchMorph/instance/menus/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,11 +1,14 @@
-addCustomMenuItems: aCustomMenu hand: aHandMorph
+addCustomMenuItems: aMenu hand: aHandMorph
 	"Add morph-specific items to the given menu which was invoked by the given hand."
 
-	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
-	aCustomMenu add: 'change font...' action: #changeFont.
-	romanNumerals
-		ifTrue: [aCustomMenu add: 'use latin numerals' action: #toggleRoman]
-		ifFalse: [aCustomMenu add: 'use roman numerals' action: #toggleRoman].
-	aCustomMenu add: 'toggle antialiasing' action: #toggleAntialias.
-	aCustomMenu add: 'change hands color...' action: #changeHandsColor.
-	aCustomMenu add: 'change center color...' action: #changeCenterColor
+	super addCustomMenuItems: aMenu hand: aHandMorph.
+	aMenu addLine.
+	aMenu addUpdating: #romanNumeralString action: #toggleRoman.
+	aMenu addUpdating: #antiAliasString action: #toggleAntialias.
+	aMenu addLine.
+	aMenu add: 'change font...' translated action: #changeFont.
+	aMenu balloonTextForLastItem: 'Allows you to change the font used to display the numbers.' translated.
+	aMenu add: 'change hands color...' translated action: #changeHandsColor.
+	aMenu balloonTextForLastItem: 'Allows you to specify a new color for the hands of the watch.  Note that actual *watch* color can be changed simply by using the halo''s recoloring handle.' translated.
+	aMenu add: 'change center color...' translated action: #changeCenterColor.
+	aMenu balloonTextForLastItem: 'Allows you to specify a new color to be used during PM hours for the center portion of the watch; during AM hours, a lighter shade of the same color will be used.' translated.
