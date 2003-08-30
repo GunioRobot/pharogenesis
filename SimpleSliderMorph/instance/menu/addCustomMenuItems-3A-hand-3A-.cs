423f1aebdbbd04f6126586203177f6e520575f18
dgd <unknown>
2003-08-30 22:13:00 +0000

    no message

--- SimpleSliderMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ SimpleSliderMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -2,13 +2,13 @@ addCustomMenuItems: aCustomMenu hand: aHandMorph
 
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
 	aCustomMenu addLine.
-	aCustomMenu add: 'set action selector' action: #setActionSelector.
-	aCustomMenu add: 'change arguments' action: #setArguments.
-	aCustomMenu add: 'set minimum value' action: #setMinVal.
-	aCustomMenu add: 'set maximum value' action: #setMaxVal.
+	aCustomMenu add: 'set action selector' translated action: #setActionSelector.
+	aCustomMenu add: 'change arguments' translated action: #setArguments.
+	aCustomMenu add: 'set minimum value' translated action: #setMinVal.
+	aCustomMenu add: 'set maximum value' translated action: #setMaxVal.
 	aCustomMenu addUpdating: #descendingString action: #toggleDescending.
 	aCustomMenu addUpdating: #truncateString action: #toggleTruncate.
 	((self world rootMorphsAt: aHandMorph targetOffset) size > 1) ifTrue: [
-		aCustomMenu add: 'set target' action: #setTarget:].
+		aCustomMenu add: 'set target' translated action: #setTarget:].
 	target ifNotNil: [
-		aCustomMenu add: 'clear target' action: #clearTarget].
+		aCustomMenu add: 'clear target' translated action: #clearTarget].
