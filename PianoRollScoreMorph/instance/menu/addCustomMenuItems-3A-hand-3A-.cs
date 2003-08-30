45d73b16166189e8b30d09d49bbb52cafd878a4a
dgd <unknown>
2003-08-30 21:56:00 +0000

    no message

--- PianoRollScoreMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ PianoRollScoreMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,10 +1,10 @@
 addCustomMenuItems: aMenu hand: aHandMorph
 
 	super addCustomMenuItems: aMenu hand: aHandMorph.
-	aMenu add: 'expand time' action: #expandTime.
-	aMenu add: 'contract time' action: #contractTime.
+	aMenu add: 'expand time' translated action: #expandTime.
+	aMenu add: 'contract time' translated action: #contractTime.
 	aMenu addLine.
-	aMenu add: 'add movie clip player' action: #addMovieClipPlayer.
+	aMenu add: 'add movie clip player' translated action: #addMovieClipPlayer.
 	(self valueOfProperty: #dragNDropEnabled) == true
-		ifTrue: [aMenu add: 'close drag and drop' action: #disableDragNDrop]
-		ifFalse: [aMenu add: 'open drag and drop' action: #enableDragNDrop].
+		ifTrue: [aMenu add: 'close drag and drop' translated action: #disableDragNDrop]
+		ifFalse: [aMenu add: 'open drag and drop' translated action: #enableDragNDrop].
