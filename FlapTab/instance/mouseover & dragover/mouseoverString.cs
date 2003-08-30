c14a3a23c858d748c9e7b6a97d40c23ff6e6a0bc
dgd <unknown>
2003-08-30 21:36:00 +0000

    no message

--- FlapTab/instance/mouseover & dragover/mouseoverString.cs
+++ FlapTab/instance/mouseover & dragover/mouseoverString.cs
@@ -1,8 +1,7 @@
 mouseoverString
-	"Answer the string to be shown in a menu to represent the mouseover status"
-
-	^ popOutOnMouseOver
-		ifTrue:
-			['<yes>pop out on mouseover']
-		ifFalse:
-			['<no>pop out on mouseover']
+	"Answer the string to be shown in a menu to represent the  
+	mouseover status"
+	^ (popOutOnMouseOver
+		ifTrue: ['<yes>']
+		ifFalse: ['<no>'])
+		, 'pop out on mouseover' translated 
