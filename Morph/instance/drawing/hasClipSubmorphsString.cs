de98e638d8c71eac3e1930859942cb08697588cb
dgd <unknown>
2003-08-30 20:20:00 +0000

    no message

--- Morph/instance/drawing/hasClipSubmorphsString.cs
+++ Morph/instance/drawing/hasClipSubmorphsString.cs
@@ -1,4 +1,6 @@
 hasClipSubmorphsString
-	^self clipSubmorphs
-		ifTrue:['<on>clip submorphs']
-		ifFalse:['<off>clip submorphs']
+	"Answer a string that represents the clip-submophs checkbox"
+	^ (self clipSubmorphs
+		ifTrue: ['<on>']
+		ifFalse: ['<off>'])
+		, 'provide clipping' translated
