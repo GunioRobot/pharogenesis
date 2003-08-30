21a90ee3060f8d71261f21f6655ce7e0856ea728
dgd <unknown>
2003-08-30 22:06:00 +0000

    no message

--- ScrollPane/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ ScrollPane/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,8 +1,8 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
 	retractableScrollBar
-		ifTrue: [aCustomMenu add: 'make scrollbar inboard' action: #retractableOrNot]
-		ifFalse: [aCustomMenu add: 'make scrollbar retractable' action: #retractableOrNot].
+		ifTrue: [aCustomMenu add: 'make scrollbar inboard' translated action: #retractableOrNot]
+		ifFalse: [aCustomMenu add: 'make scrollbar retractable' translated action: #retractableOrNot].
 	scrollBarOnLeft
-		ifTrue: [aCustomMenu add: 'scroll bar on right' action: #leftOrRight]
-		ifFalse: [aCustomMenu add: 'scroll bar on left' action: #leftOrRight]
+		ifTrue: [aCustomMenu add: 'scroll bar on right' translated action: #leftOrRight]
+		ifFalse: [aCustomMenu add: 'scroll bar on left' translated action: #leftOrRight]
