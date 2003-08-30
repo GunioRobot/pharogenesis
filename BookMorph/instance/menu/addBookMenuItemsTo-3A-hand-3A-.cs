7292d9298b454af75c87cd2a144da33d5d89db2e
dgd <unknown>
2003-08-30 21:13:00 +0000

    no message

--- BookMorph/instance/menu/addBookMenuItemsTo-3A-hand-3A-.cs
+++ BookMorph/instance/menu/addBookMenuItemsTo-3A-hand-3A-.cs
@@ -1,49 +1,49 @@
 addBookMenuItemsTo: aMenu hand: aHandMorph
 	| controlsShowing subMenu |
 	subMenu _ MenuMorph new defaultTarget: self.
-	subMenu add: 'previous page' action: #previousPage.
-	subMenu add: 'next page' action: #nextPage.
-	subMenu add: 'goto page' action: #goToPage.
-	subMenu add: 'insert a page' action: #insertPage.
-	subMenu add: 'delete this page' action: #deletePage.
+	subMenu add: 'previous page' translated action: #previousPage.
+	subMenu add: 'next page' translated action: #nextPage.
+	subMenu add: 'goto page' translated action: #goToPage.
+	subMenu add: 'insert a page' translated action: #insertPage.
+	subMenu add: 'delete this page' translated action: #deletePage.
 
 	controlsShowing _ self hasSubmorphWithProperty: #pageControl.
 	controlsShowing
 		ifTrue:
-			[subMenu add: 'hide page controls' action: #hidePageControls.
-			subMenu add: 'fewer page controls' action: #fewerPageControls]
+			[subMenu add: 'hide page controls' translated action: #hidePageControls.
+			subMenu add: 'fewer page controls' translated action: #fewerPageControls]
 		ifFalse:
-			[subMenu add: 'show page controls' action: #showPageControls].
+			[subMenu add: 'show page controls' translated action: #showPageControls].
 	self isInFullScreenMode ifTrue: [
-		subMenu add: 'exit full screen' action: #exitFullScreen.
+		subMenu add: 'exit full screen' translated action: #exitFullScreen.
 	] ifFalse: [
-		subMenu add: 'show full screen' action: #goFullScreen.
+		subMenu add: 'show full screen' translated action: #goFullScreen.
 	].
 	subMenu addLine.
-	subMenu add: 'sound effect for all pages' action: #menuPageSoundForAll:.
-	subMenu add: 'sound effect this page only' action: #menuPageSoundForThisPage:.
-	subMenu add: 'visual effect for all pages' action: #menuPageVisualForAll:.
-	subMenu add: 'visual effect this page only' action: #menuPageVisualForThisPage:.
+	subMenu add: 'sound effect for all pages' translated action: #menuPageSoundForAll:.
+	subMenu add: 'sound effect this page only' translated action: #menuPageSoundForThisPage:.
+	subMenu add: 'visual effect for all pages' translated action: #menuPageVisualForAll:.
+	subMenu add: 'visual effect this page only' translated action: #menuPageVisualForThisPage:.
 
 	subMenu addLine.
-	subMenu add: 'sort pages' action: #sortPages:.
-	subMenu add: 'uncache page sorter' action: #uncachePageSorter.
+	subMenu add: 'sort pages' translated action: #sortPages:.
+	subMenu add: 'uncache page sorter' translated action: #uncachePageSorter.
 	(self hasProperty: #dontWrapAtEnd)
-		ifTrue: [subMenu add: 'wrap after last page' selector: #setWrapPages: argument: true]
-		ifFalse: [subMenu add: 'stop at last page' selector: #setWrapPages: argument: false].
+		ifTrue: [subMenu add: 'wrap after last page' translated selector: #setWrapPages: argument: true]
+		ifFalse: [subMenu add: 'stop at last page' translated selector: #setWrapPages: argument: false].
 
 	subMenu addLine.
-	subMenu add: 'search for text' action: #textSearch.
+	subMenu add: 'search for text' translated action: #textSearch.
 	(aHandMorph pasteBuffer class isKindOf: PasteUpMorph class) ifTrue:
-		[subMenu add: 'paste book page'	action: #pasteBookPage].
+		[subMenu add: 'paste book page' translated	action: #pasteBookPage].
 
-	subMenu add: 'send all pages to server' action: #savePagesOnURL.
-	subMenu add: 'send this page to server' action: #saveOneOnURL.
-	subMenu add: 'reload all from server' action: #reload.
-	subMenu add: 'copy page url to clipboard' action: #copyUrl.
-	subMenu add: 'keep in one file' action: #keepTogether.
-	subMenu add: 'save as new-page prototype' action: #setNewPagePrototype.
+	subMenu add: 'send all pages to server' translated action: #savePagesOnURL.
+	subMenu add: 'send this page to server' translated action: #saveOneOnURL.
+	subMenu add: 'reload all from server' translated action: #reload.
+	subMenu add: 'copy page url to clipboard' translated action: #copyUrl.
+	subMenu add: 'keep in one file' translated action: #keepTogether.
+	subMenu add: 'save as new-page prototype' translated action: #setNewPagePrototype.
 	newPagePrototype ifNotNil:
-		[subMenu add: 'clear new-page prototype' action: #clearNewPagePrototype].
+		[subMenu add: 'clear new-page prototype' translated action: #clearNewPagePrototype].
 
-	aMenu add: 'book...' subMenu: subMenu
+	aMenu add: 'book...' translated subMenu: subMenu
