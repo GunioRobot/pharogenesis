d7efbce8595294b52cc4c252993a56faf985d314
dgd <unknown>
2003-08-30 21:53:00 +0000

    no message

--- MorphicModel/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ MorphicModel/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -2,5 +2,5 @@ addCustomMenuItems: aCustomMenu hand: aHandMorph
 
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
 	model ifNotNil: [model addModelMenuItemsTo: aCustomMenu forMorph: self hand: aHandMorph].
-	self isOpen ifTrue: [aCustomMenu add: 'close editing' action: #closeToEdits]
-			ifFalse: [aCustomMenu add: 'open editing' action: #openToEdits].
+	self isOpen ifTrue: [aCustomMenu add: 'close editing' translated action: #closeToEdits]
+			ifFalse: [aCustomMenu add: 'open editing' translated action: #openToEdits].
