a77e1c07dc91991325affc4c1a35ba47dcd85704
dgd <unknown>
2003-08-30 21:19:00 +0000

    no message

--- EnvelopeEditorMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ EnvelopeEditorMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -2,14 +2,14 @@ addCustomMenuItems: menu hand: aHandMorph
 	super addCustomMenuItems: menu hand: aHandMorph.
 	menu addLine.
 	envelope updateSelector = #ratio: ifTrue:
-		[menu add: 'choose denominator...' action: #chooseDenominator:].
-	menu add: 'adjust scale...' action: #adjustScale:.
+		[menu add: 'choose denominator...' translated action: #chooseDenominator:].
+	menu add: 'adjust scale...' translated action: #adjustScale:.
 	SoundPlayer isReverbOn
-		ifTrue: [menu add: 'turn reverb off' target: SoundPlayer selector: #stopReverb]
-		ifFalse: [menu add: 'turn reverb on' target: SoundPlayer selector: #startReverb].
+		ifTrue: [menu add: 'turn reverb off' translated target: SoundPlayer selector: #stopReverb]
+		ifFalse: [menu add: 'turn reverb on' translated target: SoundPlayer selector: #startReverb].
 	menu addLine.
-	menu add: 'get sound from lib' action: #chooseSound:.
-	menu add: 'put sound in lib' action: #saveSound:.
-	menu add: 'read sound from disk...' action: #readFromDisk:.
-	menu add: 'save sound on disk...' action: #saveToDisk:.
-	menu add: 'save library on disk...' action: #saveLibToDisk:.
+	menu add: 'get sound from lib' translated action: #chooseSound:.
+	menu add: 'put sound in lib' translated action: #saveSound:.
+	menu add: 'read sound from disk...' translated action: #readFromDisk:.
+	menu add: 'save sound on disk...' translated action: #saveToDisk:.
+	menu add: 'save library on disk...' translated action: #saveLibToDisk:.
