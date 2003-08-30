eb9ccd4af4b85473055c2de37f028eaf8ef2194a
dgd <unknown>
2003-08-30 20:19:00 +0000

    no message

--- Morph/instance/menus/stickinessString.cs
+++ Morph/instance/menus/stickinessString.cs
@@ -1,6 +1,7 @@
 stickinessString
-	"Answer the string to be shown in a menu to represent the stickiness status"
-
-	^ self isSticky
-		ifTrue: ['<yes>sticky']
-		ifFalse: ['<no>sticky']
+	"Answer the string to be shown in a menu to represent the  
+	stickiness status"
+	^ (self isSticky
+		ifTrue: ['<yes>']
+		ifFalse: ['<no>'])
+		, 'resist being picked up' translated
