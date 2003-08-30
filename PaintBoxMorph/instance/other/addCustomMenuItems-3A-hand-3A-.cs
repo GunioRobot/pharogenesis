aa36eca2c092b484e707cf527e899049260d1791
dgd <unknown>
2003-08-30 21:55:00 +0000

    no message

--- PaintBoxMorph/instance/other/addCustomMenuItems-3A-hand-3A-.cs
+++ PaintBoxMorph/instance/other/addCustomMenuItems-3A-hand-3A-.cs
@@ -2,5 +2,5 @@ addCustomMenuItems: aCustomMenu hand: aHandMorph
 
 	"super addCustomMenuItems: aCustomMenu hand: aHandMorph."
 		"don't want the ones from ImageMorph"
-	aCustomMenu add: 'grab stamp from screen' action: #grabFromScreen:.
+	aCustomMenu add: 'grab stamp from screen' translated action: #grabFromScreen:.
 
