11d46970b0241ae701f0790f6c7b70f73bc6cee0
dgd <unknown>
2003-08-30 21:15:00 +0000

    no message

--- BouncingAtomsMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ BouncingAtomsMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,6 +1,6 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph
 
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
-	aCustomMenu add: 'startInfection' action: #startInfection.
-	aCustomMenu add: 'set atom count' action: #setAtomCount.
-	aCustomMenu add: 'show infection history' action: #showInfectionHistory:.
+	aCustomMenu add: 'startInfection' translated action: #startInfection.
+	aCustomMenu add: 'set atom count' translated action: #setAtomCount.
+	aCustomMenu add: 'show infection history' translated action: #showInfectionHistory:.
