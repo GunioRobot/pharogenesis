1526536e14eb85ebe1a17b072cb74541d1deb551
dgd <unknown>
2003-08-31 18:03:00 +0000

    no message

---
+++ Preferences/class/get-2F-set/navigatorShowingString.cs
@@ -0,0 +1,6 @@
+navigatorShowingString
+	"Answer a string for the show-project-navigator menu item"
+	^ (self showProjectNavigator
+		ifTrue: ['<yes>']
+		ifFalse: ['<no>'])
+		, 'show navigator (N)' translated
