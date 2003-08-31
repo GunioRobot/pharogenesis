a793e60dbf2794cba7f57f08e8c3dd5a4865bd52
dgd <unknown>
2003-08-31 18:58:00 +0000

    no message

---
+++ FlapTab/instance/access/acquirePlausibleFlapID.cs
@@ -0,0 +1,8 @@
+acquirePlausibleFlapID
+	"Give the receiver a flapID that is globally unique; try to hit the mark vis a vis the standard system flap id's, for the case when this method is invoked as part of the one-time transition"
+
+	| wording |
+	wording _ self wording.
+	(wording isEmpty or: [wording = '---']) ifTrue: [wording _ 'Flap' translated].
+	
+	^ self provideDefaultFlapIDBasedOn: wording
