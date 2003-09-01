b03e25cf7179c05685a53b0381cc01a1a98960de
dgd <unknown>
2003-09-01 14:00:00 +0000

    no message

--- ScriptEditorMorph/instance/buttons/chooseFrequency.cs
+++ ScriptEditorMorph/instance/buttons/chooseFrequency.cs
@@ -6,6 +6,6 @@ chooseFrequency
 	#(1 2 5 10 25 50 100 1000 5000 10000) do:
 		[:i | aMenu add: i printString selector: #setFrequencyTo: argument: i].
 	
-	aMenu add: 'other...' action: #typeInFrequency.
-	aMenu addTitle: 'Choose frequency (current: ', currentFrequency printString, ')'.
+	aMenu add: 'other...' translated action: #typeInFrequency.
+	aMenu addTitle: ('Choose frequency (current: {1})' translated format: {currentFrequency}).
 	aMenu  popUpEvent: self currentEvent in: self world
