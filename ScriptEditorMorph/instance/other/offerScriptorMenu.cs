533c391fd8e3c114a272ab698dc576948a43a880
dgd <unknown>
2003-09-01 14:54:00 +0000

    no message

--- ScriptEditorMorph/instance/other/offerScriptorMenu.cs
+++ ScriptEditorMorph/instance/other/offerScriptorMenu.cs
@@ -4,37 +4,51 @@ offerScriptorMenu
 	| aMenu  count |
 
 	self modernize.
+	ActiveHand showTemporaryCursor: nil.
+
 	aMenu _ MenuMorph new defaultTarget: self.
 	aMenu addTitle: scriptName asString.
-	count _ self savedTileVersionsCount.
 
-	self showingMethodPane
-		ifFalse:				"currently showing tiles"
-			[aMenu add: 'show code textually' action: #showSourceInScriptor.
-			count > 0 ifTrue: 
-				[aMenu add: 'revert to tile version...' action:	 #revertScriptVersion].
-			aMenu add: 'save this version'	action: #saveScriptVersion]
+	Preferences universalTiles ifFalse:
+		[count _ self savedTileVersionsCount.
+		self showingMethodPane
+			ifFalse:				"currently showing tiles"
+				[aMenu add: 'show code textually' translated action: #showSourceInScriptor.
+				count > 0 ifTrue: 
+					[aMenu add: 'revert to tile version...' translated action:	 #revertScriptVersion].
+				aMenu add: 'save this version' translated	action: #saveScriptVersion]
 
-		ifTrue:				"current showing textual source"
-			[count >= 1 ifTrue:
-				[aMenu add: 'revert to tile version' action: #revertToTileVersion].
-			aMenu add: 'make tiles from this code' action: #recreateTileVersion].
-	Preferences noviceMode ifFalse: ["just for now, not for kids"
-		aMenu add: 'use new universal tiles' action: #useNewTiles].
-			
+			ifTrue:				"current showing textual source"
+				[count >= 1 ifTrue:
+					[aMenu add: 'revert to tile version' translated action: #revertToTileVersion]]].
 
-	aMenu addList: #(
-		-
-		('destroy this script'					destroyScript)
-		('rename this script'					renameScript)
-		-
-		('fires per tick...'					chooseFrequency)
-		('explain status alternatives' 		explainStatusAlternatives)
-		('hand me a tile for self'			tileForSelf)
-		('edit balloon help for this script'	editMethodDescription)
-		('button to fire this script'			tearOfButtonToFireScript)
-		 ).
+	aMenu addList: {
+		#-.
+		{'destroy this script' translated.					#destroyScript}.
+		{'rename this script' translated.					#renameScript}.
+		}.
+
+	self hasParameter ifFalse:
+		[aMenu addList: {{'button to fire this script' translated.			#tearOfButtonToFireScript}}].
+
+	aMenu addList: {
+		{'edit balloon help for this script' translated.		#editMethodDescription}.
+		#-.
+		{'explain status alternatives' translated. 			#explainStatusAlternatives}.
+		#-.
+		{'hand me a tile for self' translated.					#handUserTileForSelf}.
+		{'hand me a "random number" tile' translated.		#handUserRandomTile}.
+		{'hand me a "button down?" tile' translated.		#handUserButtonDownTile}.
+		{'hand me a "button up?" tile' translated.			#handUserButtonUpTile}.
+		}.
+
+	aMenu addList: (self hasParameter
+		ifTrue: [{
+			#-.
+			{'remove parameter' translated.					#ceaseHavingAParameter}}]
+		ifFalse: [{
+			{'fires per tick...' translated.						#chooseFrequency}.
+			#-.
+			{'add parameter' translated.						#addParameter}}]).
 
 	aMenu popUpInWorld: self currentWorld.
-
-	"		('add parameter to this script'	addParameter)"
