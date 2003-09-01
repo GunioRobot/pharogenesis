1556c3f9b293ade7ea7995e08b5847a0dac0e4cc
dgd <unknown>
2003-09-01 13:51:00 +0000

    no message

--- CategoryViewer/instance/get-2F-set slots/arrowSetterButtonFor-3A-type-3A-.cs
+++ CategoryViewer/instance/get-2F-set slots/arrowSetterButtonFor-3A-type-3A-.cs
@@ -6,8 +6,8 @@ arrowSetterButtonFor: partName type: partType
 		extent: 24@TileMorph defaultH;
 		borderWidth: 0.
 	m addMorphCentered: (ImageMorph new image: (ScriptingSystem formAtKey: 'Gets')).
-	m setBalloonText: 'drag from here to obtain an assignment phrase.'.
-	m on: #mouseDown send: #makeSetter:from:forPart:
+	m setBalloonText: 'drag from here to obtain an assignment phrase.' translated.
+	m on: #mouseDown send: #makeSetter:event:from:
 		to: self
 		withValue: (Array with: partName with: partType).
 	^ m
