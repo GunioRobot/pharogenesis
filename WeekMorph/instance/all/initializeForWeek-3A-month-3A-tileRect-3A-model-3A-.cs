c66ad8b4e5f5b1ff8c4db148c75211e140cd56ad
brp <unknown>
2003-09-02 15:16:00 +0000

    no message

--- WeekMorph/instance/all/initializeForWeek-3A-month-3A-tileRect-3A-model-3A-.cs
+++ WeekMorph/instance/all/initializeForWeek-3A-month-3A-tileRect-3A-model-3A-.cs
@@ -2,10 +2,12 @@ initializeForWeek: aWeek month: aMonth tileRect: rect model: aModel
 
 	super initialize.
 	tileRect _ rect.
-	self layoutInset: 0;
+	self 
+		layoutInset: 0;
 		color: Color transparent;
 		listDirection: #leftToRight;
 		hResizing: #shrinkWrap;
 		disableDragNDrop;
 		height: tileRect height.
+
 	self week: aWeek month: aMonth model: aModel
