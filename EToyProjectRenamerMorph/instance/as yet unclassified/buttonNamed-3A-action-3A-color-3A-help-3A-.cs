f4f341ff1c25b21caf6d3776c1d36c3e6b6b7a09
jm <unknown>
2003-09-02 19:39:00 +0000

    no message

--- EToyProjectRenamerMorph/instance/as yet unclassified/buttonNamed-3A-action-3A-color-3A-help-3A-.cs
+++ EToyProjectRenamerMorph/instance/as yet unclassified/buttonNamed-3A-action-3A-color-3A-help-3A-.cs
@@ -3,9 +3,9 @@ buttonNamed: aString action: aSymbol color: aColor help: helpString
 	| f col |
 	f _ SimpleButtonMorph new
 		target: self;
-		label: aString font: self myFont;
+		label: aString translated font: self myFont;
 		color: aColor;
 		actionSelector: aSymbol;
-		setBalloonText: helpString.
+		setBalloonText: helpString translated.
 	col _ (self inAColumn: {f}) hResizing: #spaceFill.
 	^col
