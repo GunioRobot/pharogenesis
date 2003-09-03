e8a98a78f01f4f40e4807b7a2cb7fcbc0d7d7f4e
brp <unknown>
2003-09-03 08:46:00 +0000

    no message

--- MonthMorph/instance/controls/chooseYear.cs
+++ MonthMorph/instance/controls/chooseYear.cs
@@ -1,14 +1,17 @@
 chooseYear
 
 	| newYear yearString |
-	newYear _ (SelectionMenu selections: {'today'} , (month year - 5 to: month year + 5) , {'other...'}) startUpWithCaption: 'Choose another year'.
+	newYear _ (SelectionMenu selections:
+					{'today'} , (month year - 5 to: month year + 5) , {'other...'})
+						startUpWithCaption: 'Choose another year'.
 	newYear ifNil: [^ self].
 	newYear isNumber ifTrue:
-		[^ self month: (Month newDay: 1 month: month monthName year: newYear)].
+		[^ self month: (Month month: month monthName year: newYear)].
 	newYear = 'today' ifTrue:
-		[^ self month: (Month fromDate: Date today)].
-	yearString _ FillInTheBlank request: 'Type in a year' initialAnswer: Date today year asString.
+		[^ self month: (Month starting: Date today)].
+	yearString _ FillInTheBlank 
+					request: 'Type in a year' initialAnswer: Date today year asString.
 	yearString ifNil: [^ self].
 	newYear _ yearString asNumber.
 	(newYear between: 0 and: 9999) ifTrue:
-		[^ self month: (Month newDay: 1 month: month monthName year: newYear)].
+		[^ self month: (Month month: month monthName year: newYear)].
