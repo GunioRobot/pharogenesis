8d784b904d8ee7d322bc2675fe61809bfb6a57c2
brp <unknown>
2003-09-03 08:45:00 +0000

    no message

--- PDA/instance/example/sampleScheduleList.cs
+++ PDA/instance/example/sampleScheduleList.cs
@@ -1,12 +1,12 @@
 sampleScheduleList
 
 	^ {
-	PDAEvent new key: 'home'; date: Date today; description: 'wake up'; time: (Time new hours: 6).
-	PDAEvent new key: 'home'; date: Date today; description: 'go for a run'; time: (Time new hours: 7).
-	PDAEvent new key: 'home'; date: Date today; description: 'take a shower'; time: (Time new hours: 8).
-	PDAEvent new key: 'home'; date: (Date today addDays: 2); description: 'dinner out'; time: (Time new hours: 18).
-	PDAEvent new key: 'work'; date: (Date today addDays: 1); description: 'conf call'; time: (Time new hours: 10).
-	PDAEvent new key: 'work'; date: (Date today addDays: 2); description: 'Leave for Conference'; time: (Time new hours: 8).
-	PDAEvent new key: 'work'; date: Date today; description: 'call Boss'; time: (Time new hours: 15).
-	PDAEvent new key: 'work'; date: Date today; description: 'Call about 401k'; time: (Time new hours: 10).
+	PDAEvent new key: 'home'; date: Date today; description: 'wake up'; time: (Time hour: 6 minute: 0 second: 0).
+	PDAEvent new key: 'home'; date: Date today; description: 'go for a run'; time: (Time hour: 7 minute: 0 second: 0).
+	PDAEvent new key: 'home'; date: Date today; description: 'take a shower'; time: (Time hour: 8 minute: 0 second: 0).
+	PDAEvent new key: 'home'; date: (Date today addDays: 2); description: 'dinner out'; time: (Time hour: 18 minute: 0 second: 0).
+	PDAEvent new key: 'work'; date: (Date today addDays: 1); description: 'conf call'; time: (Time hour: 10 minute: 0 second: 0).
+	PDAEvent new key: 'work'; date: (Date today addDays: 2); description: 'Leave for Conference'; time: (Time hour: 8 minute: 0 second: 0).
+	PDAEvent new key: 'work'; date: Date today; description: 'call Boss'; time: (Time hour: 15 minute: 0 second: 0).
+	PDAEvent new key: 'work'; date: Date today; description: 'Call about 401k'; time: (Time hour: 10 minute: 0 second: 0).
 	}
