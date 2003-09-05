1b4e6afbe7f915aff351fe1912943193f8277029
dgd <unknown>
2003-09-05 18:32:00 +0000

    no message

--- HaloMorph/instance/private/maybeDismiss-3A-with-3A-.cs
+++ HaloMorph/instance/private/maybeDismiss-3A-with-3A-.cs
@@ -1,10 +1,25 @@
 maybeDismiss: evt with: dismissHandle
 	"Ask hand to dismiss my target if mouse comes up in it."
+
 	evt hand obtainHalo: self.
 	(dismissHandle containsPoint: evt cursorPoint)
 		ifFalse:
 			[self delete.
 			target addHalo: evt]
 		ifTrue:
-			[self delete.
-			target dismissViaHalo]
+			[target resistsRemoval ifTrue:
+				[(PopUpMenu
+					confirm: 'Really throw this away' translated
+					trueChoice: 'Yes' translated
+					falseChoice: 'Um, no, let me reconsider' translated) ifFalse: [^ self]].
+
+			Preferences preserveTrash
+				ifTrue:
+					[Preferences soundsEnabled ifTrue:
+						[TrashCanMorph playDeleteSound].
+					self stopStepping.
+					super delete.
+					target slideToTrash: evt]
+				ifFalse:
+					[self delete.
+					target dismissViaHalo]]
