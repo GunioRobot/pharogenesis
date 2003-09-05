ec294c2e670accacca2c625262a6f6523d61ee0f
dgd <unknown>
2003-09-05 19:02:00 +0000

    no message

--- ParagraphEditor/class/class initialization/initializeTextEditorMenus.cs
+++ ParagraphEditor/class/class initialization/initializeTextEditorMenus.cs
@@ -1,23 +1,31 @@
-initializeTextEditorMenus   "ParagraphEditor initializeTextEditorMenus"
-		"Initialize the yellow button pop-up menu and corresponding messages."
+initializeTextEditorMenus
+	"Initialize the yellow button pop-up menu and corresponding messages."
+	"ParagraphEditor initializeTextEditorMenus"
 
 	TextEditorYellowButtonMenu _ SelectionMenu
-		labels: 
-'find...(f)
-find again (g)
-set search string (h)
-do again (j)
-undo (z)
-copy (c)
-cut (x)
-paste (v)
-paste...
-do it (d)
-print it (p)
-inspect it (i)
-accept (s)
-cancel (l)
-show bytecodes
-more...' 
-		lines: #(3 5 9 12 14 15)
-		selections: #(find findAgain setSearchString again undo copySelection cut paste pasteRecent doIt printIt inspectIt accept cancel showBytecodes shiftedYellowButtonActivity)
+		fromArray: {
+		{'find...(f)' translated.		#find}.			     
+		{'find again (g)' translated.		#findAgain}.		     
+		{'set search string (h)' translated.	#setSearchString}.
+		#-.	     
+		{'do again (j)' translated.		#again}.		     
+		{'undo (z)' translated.			#undo}.			     
+		#-.	     
+		{'copy (c)' translated.			#copySelection}.	     
+		{'cut (x)' translated.			#cut}.			     
+		{'paste (v)' translated.		#paste}.		     
+		{'paste...' translated.			#pasteRecent}.		     
+		#-.	     
+		{'do it (d)' translated.		#doIt}.			     
+		{'print it (p)' translated.		#printIt}.		     
+		{'inspect it (i)' translated.		#inspectIt}.		     
+		{'explore it (I)' translated.		#exploreIt}.		     
+		{'debug it' translated.			#debugIt}.		     
+		#-.	     
+		{'accept (s)' translated.		#accept}.		     
+		{'cancel (l)' translated.		#cancel}.		     
+		#-.	     
+		{'show bytecodes' translated.		#showBytecodes}.	     
+		#-.	     
+		{'more...' translated.			#shiftedTextPaneMenuRequest}.
+	}
