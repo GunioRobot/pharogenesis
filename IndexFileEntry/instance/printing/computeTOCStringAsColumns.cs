bbffa2241b6253425ce741ffb0ce20ad4a4b98f8
ads <unknown>
2003-09-06 11:55:00 +0000

    no message

--- IndexFileEntry/instance/printing/computeTOCStringAsColumns.cs
+++ IndexFileEntry/instance/printing/computeTOCStringAsColumns.cs
@@ -3,13 +3,15 @@ computeTOCStringAsColumns
 	"IndexFileEntry allInstancesDo: [: e | e flushTOCCache]"
 	| fromFieldSize array attachFlag |
 	fromFieldSize _ 18.
-	attachFlag _ self getMessage body isMultipart.
+	attachFlag _ Celeste showAttachmentsFlag
+		ifTrue: [self getMessage body isMultipart]
+		ifFalse: [false].
 	array _ Array new: 5.
 	array at: 1 put: self dateString.
 	array
 		at: 2
 		put: (self fromStringLimit: fromFieldSize).
-	array at: 3 put: subject decodeMimeHeader.
+	array at: 3 put: subject.
 	array at: 4 put: self textLength asStringWithCommas.
 	array at: 5 put: attachFlag.
 	^ array
