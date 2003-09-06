8c1cef79d5bb626524af577e11ed90153246bc68
dgd <unknown>
2003-09-06 17:59:00 +0000

    no message

--- PasteUpMorph/instance/menu & halo/autoExpansionString.cs
+++ PasteUpMorph/instance/menu & halo/autoExpansionString.cs
@@ -1,8 +1,7 @@
 autoExpansionString
-	"Answer the string to be shown in a menu to represent the auto-phrase-expansion status"
-
-	^ (self hasProperty: #automaticPhraseExpansion)
-		ifTrue:
-			['<on>auto-phrase-expansion']
-		ifFalse:
-			['<off>auto-phrase-expansion']
+	"Answer the string to be shown in a menu to represent the  
+	auto-phrase-expansion status"
+	^ ((self hasProperty: #automaticPhraseExpansion)
+		ifTrue: ['<on>']
+		ifFalse: ['<off>'])
+		, 'auto-phrase-expansion' translated
