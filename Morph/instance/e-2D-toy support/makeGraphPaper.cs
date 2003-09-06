23d84470c42b005674e7ba5e4923d15e3f371121
dgd <unknown>
2003-09-06 18:10:00 +0000

    no message

--- Morph/instance/e-2D-toy support/makeGraphPaper.cs
+++ Morph/instance/e-2D-toy support/makeGraphPaper.cs
@@ -1,7 +1,7 @@
 makeGraphPaper
 	| smallGrid backColor lineColor |
-	smallGrid _ Compiler evaluate: (FillInTheBlank request: 'Enter grid size' initialAnswer: '16').
+	smallGrid _ Compiler evaluate: (FillInTheBlank request: 'Enter grid size' translated initialAnswer: '16').
 	smallGrid ifNil: [^ self].
-	Utilities informUser: 'Choose a background color' during: [backColor _ Color fromUser].
-	Utilities informUser: 'Choose a line color' during: [lineColor _ Color fromUser].
+	Utilities informUser: 'Choose a background color' translated during: [backColor _ Color fromUser].
+	Utilities informUser: 'Choose a line color' translated during: [lineColor _ Color fromUser].
 	self makeGraphPaperGrid: smallGrid background: backColor line: lineColor.
