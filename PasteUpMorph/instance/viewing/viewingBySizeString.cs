73275e239d5309acffda029e6e28a574bf6e22be
dgd <unknown>
2003-09-06 17:50:00 +0000

    no message

--- PasteUpMorph/instance/viewing/viewingBySizeString.cs
+++ PasteUpMorph/instance/viewing/viewingBySizeString.cs
@@ -1,6 +1,10 @@
 viewingBySizeString
-	"Answer a string to show in a menu representing whether the receiver is currently viewing its subparts by size or not"
-
-	^ (self showingListView and: [(self valueOfProperty: #sortOrder ifAbsent: [nil]) == #reportableSize])
-		ifTrue:	['<yes>view by size']
-		ifFalse:	['<no>view by size']
+	"Answer a string to show in a menu representing whether the 
+	receiver is currently viewing its subparts by size or not"
+	^ ((self showingListView
+			and: [(self
+					valueOfProperty: #sortOrder
+					ifAbsent: [])
+					== #reportableSize])
+		ifTrue: ['<yes>']
+		ifFalse: ['<no>']), 'view by size' translated
