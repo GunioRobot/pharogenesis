68357a2b909029aa49108b9e56ee08f81800ea41
dgd <unknown>
2003-09-06 18:04:00 +0000

    no message

--- PasteUpMorph/instance/menu & halo/presentViewMenu.cs
+++ PasteUpMorph/instance/menu & halo/presentViewMenu.cs
@@ -42,30 +42,30 @@ presentViewMenu
 			[:triplet |
 				(isWorld and: [#(toggleAutoLineLayout toggleIndicateCursor toggleIsPartsBin toggleAlwaysShowThumbnail toggleAutomaticViewing ) includes: triplet second]) ifFalse:
 					[aMenu addUpdating: triplet first action: triplet second.
-					aMenu balloonTextForLastItem: triplet third]]. 
+					aMenu balloonTextForLastItem: triplet third translated]]. 
 
 	aMenu addLine.
-	aMenu add: 'round up strays' action: #roundUpStrays.
-	aMenu balloonTextForLastItem:  'Bring back all objects whose current coordinates keep them from being visible, so that at least a portion of each of my interior objects can be seen.'.
-	aMenu add: 'shuffle contents' action: #shuffleSubmorphs.
-	aMenu balloonTextForLastItem: 'Rearranges my contents in random order'.
-	aMenu add: 'set grid spacing...' action: #setGridSpec.
-	aMenu balloonTextForLastItem: 'Set the spacing to be used when gridding is on'.
+	aMenu add: 'round up strays' translated action: #roundUpStrays.
+	aMenu balloonTextForLastItem:  'Bring back all objects whose current coordinates keep them from being visible, so that at least a portion of each of my interior objects can be seen.' translated.
+	aMenu add: 'shuffle contents' translated action: #shuffleSubmorphs.
+	aMenu balloonTextForLastItem: 'Rearranges my contents in random order' translated.
+	aMenu add: 'set grid spacing...' translated action: #setGridSpec.
+	aMenu balloonTextForLastItem: 'Set the spacing to be used when gridding is on' translated.
 
 	isWorld ifFalse:
-		[aMenu add: 'set thumbnail height...' action: #setThumbnailHeight.
-		aMenu balloonTextForLastItem: 'if currently showing thumbnails governs the standard height for them'].
+		[aMenu add: 'set thumbnail height...' translated action: #setThumbnailHeight.
+		aMenu balloonTextForLastItem: 'if currently showing thumbnails governs the standard height for them' translated].
 
 	self backgroundSketch ifNotNil:
-		[aMenu add: 'delete background painting' action: #deleteBackgroundPainting.
-		aMenu balloonTextForLastItem: 'delete the graphic that forms the background for this me.'].
+		[aMenu add: 'delete background painting' translated action: #deleteBackgroundPainting.
+		aMenu balloonTextForLastItem: 'delete the graphic that forms the background for this me.' translated].
 	aMenu addLine.
 	self addPenTrailsMenuItemsTo: aMenu.
 	aMenu addLine.
-	aMenu add: 'use standard texture' action: #setStandardTexture.
-	aMenu balloonTextForLastItem: 'use a pale yellow-and-blue background texture here.'.
-	aMenu add: 'make graph paper...' action: #makeGraphPaper.
-	aMenu balloonTextForLastItem: 'Design your own graph paper and use it as the background texture here.'.
-	aMenu addTitle: 'viewing options for "', self externalName, '"'.
+	aMenu add: 'use standard texture' translated action: #setStandardTexture.
+	aMenu balloonTextForLastItem: 'use a pale yellow-and-blue background texture here.' translated.
+	aMenu add: 'make graph paper...' translated action: #makeGraphPaper.
+	aMenu balloonTextForLastItem: 'Design your own graph paper and use it as the background texture here.' translated.
+	aMenu addTitle: ('viewing options for "{1}"' translated format: {self externalName}).
 
 	aMenu popUpForHand: self activeHand in: self world
