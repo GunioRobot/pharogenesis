89902a394b700d75ff40e0f4335fbc01ac29462e
dgd <unknown>
2003-09-06 17:50:00 +0000

    no message

--- PasteUpMorph/instance/viewing/viewingByNameString.cs
+++ PasteUpMorph/instance/viewing/viewingByNameString.cs
@@ -1,6 +1,10 @@
 viewingByNameString
-	"Answer a string to show in a menu representing whether the receiver is currently viewing its subparts by name or not"
-
-	^ (self showingListView and: [(self valueOfProperty: #sortOrder ifAbsent: [nil]) == #downshiftedNameOfObjectRepresented])
-		ifTrue:	['<yes>view by name']
-		ifFalse:	['<no>view by name']
+	"Answer a string to show in a menu representing whether the 
+	receiver is currently viewing its subparts by name or not"
+	^ ((self showingListView
+			and: [(self
+					valueOfProperty: #sortOrder
+					ifAbsent: [])
+					== #downshiftedNameOfObjectRepresented])
+		ifTrue: ['<yes>']
+		ifFalse: ['<no>']), 'view by name' translated
