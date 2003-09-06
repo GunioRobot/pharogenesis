ad8574919d74147495fc2036f423aeb30427a46d
dgd <unknown>
2003-09-06 18:01:00 +0000

    no message

--- PasteUpMorph/instance/menu & halo/fenceEnabledString.cs
+++ PasteUpMorph/instance/menu & halo/fenceEnabledString.cs
@@ -1,8 +1,7 @@
 fenceEnabledString
-	"Answer the string to be shown in a menu to represent the fence enabled status"
-
-	^self fenceEnabled
-		ifTrue:
-			['<on>fence enabled']
-		ifFalse:
-			['<off>fence enabled']
+	"Answer the string to be shown in a menu to represent the  
+	fence enabled status"
+	^ (self fenceEnabled
+		ifTrue: ['<on>']
+		ifFalse: ['<off>'])
+		, 'fence enabled' translated
