65392cf1a198b8929c54b05479b5a8d2170c2e04
dgd <unknown>
2003-09-06 18:00:00 +0000

    no message

--- PasteUpMorph/instance/menu & halo/showThumbnailString.cs
+++ PasteUpMorph/instance/menu & halo/showThumbnailString.cs
@@ -1,8 +1,6 @@
 showThumbnailString
-	"Answer the string to be shown in a menu to represent the show-thumbnails status"
-
-	^( self hasProperty: #alwaysShowThumbnail)
-		ifTrue:
-			['<on>show thumbnails']
-		ifFalse:
-			['<off>show thumbnails']
+	"Answer the string to be shown in a menu to represent the 
+	show-thumbnails status"
+	^ ((self hasProperty: #alwaysShowThumbnail)
+		ifTrue: ['<on>']
+		ifFalse: ['<off>']), 'show thumbnails' translated
