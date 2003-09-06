dc8bf362763f31de1a3c9f0a81d2608416bbab78
dgd <unknown>
2003-09-06 18:01:00 +0000

    no message

--- PasteUpMorph/instance/menu & halo/batchPenTrailsString.cs
+++ PasteUpMorph/instance/menu & halo/batchPenTrailsString.cs
@@ -1,8 +1,6 @@
 batchPenTrailsString
-	"Answer the string to be shown in a menu to represent the batch-pen-trails enabled status"
-
-	^ self batchPenTrails
-		ifTrue:
-			['<on>batch pen trails']
-		ifFalse:
-			['<off>batch pen trails']
+	"Answer the string to be shown in a menu to represent the 
+	batch-pen-trails enabled status"
+	^ (self batchPenTrails
+		ifTrue: ['<on>']
+		ifFalse: ['<off>']), 'batch pen trails' translated
