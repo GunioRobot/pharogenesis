9c02a369827361e450e791ee7e1a7775090b40e1
dgd <unknown>
2003-09-06 18:05:00 +0000

    no message

--- PasteUpMorph/instance/options/setThumbnailHeight.cs
+++ PasteUpMorph/instance/options/setThumbnailHeight.cs
@@ -3,13 +3,13 @@ setThumbnailHeight
 	(self hasProperty: #alwaysShowThumbnail) ifFalse:
 		[^ self inform: 'setting the thumbnail height is only
 applicable when you are currently
-showing thumbnails.'].
+showing thumbnails.' translated].
 	reply _ FillInTheBlank
-		request: 'New height for thumbnails? '
+		request: 'New height for thumbnails? ' translated
 		initialAnswer: self heightForThumbnails printString.
 	reply isEmpty ifTrue: [^ self].
 	reply _ reply asNumber.
 	(reply > 0 and: [reply <= 150]) ifFalse:
-		[^ self inform: 'Please be reasonable!'].
+		[^ self inform: 'Please be reasonable!' translated].
 	self setProperty: #heightForThumbnails toValue: reply.
 	self updateSubmorphThumbnails
