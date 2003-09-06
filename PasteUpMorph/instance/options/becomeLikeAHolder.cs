8ea0168b884bd8a2b1ee65cd531999d878a4495d
dgd <unknown>
2003-09-06 17:55:00 +0000

    no message

--- PasteUpMorph/instance/options/becomeLikeAHolder.cs
+++ PasteUpMorph/instance/options/becomeLikeAHolder.cs
@@ -1,10 +1,8 @@
 becomeLikeAHolder
-	(self autoLineLayout and: [self indicateCursor])
-		ifTrue:
-			[^ self inform: 'This view is ALREADY
+	(self autoLineLayout
+			and: [self indicateCursor])
+		ifTrue: [^ self inform: 'This view is ALREADY
 behaving like a holder, which
 is to say, it is set to indicate the
-cursor and to have auto-line-layout.'].
- 
+cursor and to have auto-line-layout.' translated].
 	self behaveLikeHolder
-	
