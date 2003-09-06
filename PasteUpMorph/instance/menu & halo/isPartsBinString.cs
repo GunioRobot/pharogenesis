68b9323efb9d61d09b9e332de15bd07d0877da01
dgd <unknown>
2003-09-06 17:57:00 +0000

    no message

--- PasteUpMorph/instance/menu & halo/isPartsBinString.cs
+++ PasteUpMorph/instance/menu & halo/isPartsBinString.cs
@@ -1,8 +1,6 @@
 isPartsBinString
-	"Answer the string to be shown in a menu to represent the parts-bin status"
-
-	^ self isPartsBin
-		ifTrue:
-			['<on>parts bin']
-		ifFalse:
-			['<off>parts bin']
+	"Answer the string to be shown in a menu to represent the 
+	parts-bin status"
+	^ (self isPartsBin
+		ifTrue: ['<on>']
+		ifFalse: ['<off>']), 'parts bin' translated
