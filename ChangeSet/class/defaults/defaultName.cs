75fd63e5c8437cb2a3f1f31801cbc2aa5f1ce85d
dgd <unknown>
2003-09-06 19:56:00 +0000

    no message

--- ChangeSet/class/defaults/defaultName.cs
+++ ChangeSet/class/defaults/defaultName.cs
@@ -1,6 +1,2 @@
 defaultName
-	| namesInUse try |
-	namesInUse _ ChangeSorter gatherChangeSets collect: [:each | each name].
-	1 to: 999999 do:
-		[:i | try _ 'Unnamed' , i printString.
-		(namesInUse includes: try) ifFalse: [^ try]]
+	^ self uniqueNameLike: 'Unnamed' translated
