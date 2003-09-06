0fa8b46f076ad922c41b7f64369ff6044cac7b1a
dgd <unknown>
2003-09-06 17:50:00 +0000

    no message

--- PasteUpMorph/instance/viewing/viewingByIconString.cs
+++ PasteUpMorph/instance/viewing/viewingByIconString.cs
@@ -1,6 +1,7 @@
 viewingByIconString
-	"Answer a string to show in a menu representing whether the receiver is currently viewing its subparts by icon or not"
-
-	^ (self showingListView or: [self autoLineLayout == true])
-		ifFalse:	['<yes>view by icon']
-		ifTrue:	['<no>view by icon']
+	"Answer a string to show in a menu representing whether the 
+	receiver is currently viewing its subparts by icon or not"
+	^ ((self showingListView
+			or: [self autoLineLayout == true])
+		ifTrue: ['<no>']
+		ifFalse: ['<yes>']), 'view by icon' translated
