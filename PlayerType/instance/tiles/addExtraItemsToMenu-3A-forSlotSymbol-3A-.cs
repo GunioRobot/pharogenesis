4fb87b45fa3c372fa5d925079fd124b1c09ad906
dgd <unknown>
2003-09-06 20:30:00 +0000

    no message

---
+++ PlayerType/instance/tiles/addExtraItemsToMenu-3A-forSlotSymbol-3A-.cs
@@ -0,0 +1,4 @@
+addExtraItemsToMenu: aMenu forSlotSymbol: slotSym
+	"If the receiver has extra menu items to add to the slot menu, here is its chance to do it"
+
+	aMenu add: 'tiles to get...' translated selector: #offerGetterTiles: argument: slotSym
