13b0fd3c333b3aba362b243dde87dbebf2d9a28b
dgd <unknown>
2003-09-06 17:51:00 +0000

    no message

--- PasteUpMorph/instance/viewing/viewingNonOverlappingString.cs
+++ PasteUpMorph/instance/viewing/viewingNonOverlappingString.cs
@@ -1,6 +1,8 @@
 viewingNonOverlappingString
-	"Answer a string to show in a menu representing whether the receiver is currently viewing its subparts by non-overlapping-icon (aka auto-line-layout)"
-
-	^ (self showingListView or: [self autoLineLayout ~~ true])
-		ifFalse:	['<yes>view with line layout']
-		ifTrue:	['<no>view with line layout']
+	"Answer a string to show in a menu representing whether the 
+	receiver is currently viewing its subparts by 
+	non-overlapping-icon (aka auto-line-layout)"
+	^ ((self showingListView
+			or: [self autoLineLayout ~~ true])
+		ifTrue: ['<no>']
+		ifFalse: ['<yes>']), 'view with line layout' translated
