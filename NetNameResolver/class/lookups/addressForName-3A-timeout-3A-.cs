02b7cb152b29dcfba068f581a172747bb94b43ab
mu <unknown>
2003-09-07 22:53:00 +0000

    no message

--- NetNameResolver/class/lookups/addressForName-3A-timeout-3A-.cs
+++ NetNameResolver/class/lookups/addressForName-3A-timeout-3A-.cs
@@ -5,19 +5,22 @@ addressForName: hostName timeout: secs
 	"NetNameResolver addressForName: '1.7.6.4' timeout: 30"
 	"NetNameResolver addressForName: '' timeout: 30 (This seems to return nil?)"
 
-	| deadline ready success result |
-
+	| deadline result |
+	self initializeNetwork.
 	"check if this is a valid numeric host address (e.g. 1.2.3.4)"
 	result _ self addressFromString: hostName.
 	result isNil ifFalse: [^result].
 
 	"Look up a host name, including ones that start with a digit (e.g. 100000jobs.de or squeak.org)"
 	deadline _ Time millisecondClockValue + (secs * 1000).
-	ready _ self waitForResolverReadyUntil: deadline.
-	ready ifFalse: [^ nil].
-
-	self primStartLookupOfName: hostName.
-	success _ self waitForCompletionUntil: deadline.
-	success
-		ifTrue: [^ self primNameLookupResult]
-		ifFalse: [^ nil].
+	"Protect the execution of this block, as the ResolverSemaphore is used for both parts of the transaction."
+	self resolverMutex
+		critical: [
+			(self waitForResolverReadyUntil: deadline)
+				ifTrue: [
+					self primStartLookupOfName: hostName.
+					(self waitForCompletionUntil: deadline)
+						ifTrue: [result _ self primNameLookupResult]
+						ifFalse: [(NameLookupFailure hostName: hostName) signal: 'Could not resolve the server named: ', hostName]]
+				ifFalse: [(NameLookupFailure hostName: hostName) signal: 'Could not resolve the server named: ', hostName]].
+	^result
