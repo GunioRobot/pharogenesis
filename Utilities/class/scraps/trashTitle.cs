5df6a644e269eb9da57cefa70c424fb9026c6846
dgd <unknown>
2003-09-19 10:49:00 +0000

    no message

--- Utilities/class/scraps/trashTitle.cs
+++ Utilities/class/scraps/trashTitle.cs
@@ -1,8 +1,8 @@
 trashTitle
 	| label pgs |
-	label _ 'T R A S H'.
+	label _ 'T R A S H' translated.
 	^ (pgs _ ScrapsBook pages size) < 2
 		ifTrue:
 			[label]
 		ifFalse:
-			[label, '  (', pgs printString, ' pages)']
+			[label, ('  ({1} pages)' translated format:{pgs})]
