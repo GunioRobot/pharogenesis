9cd868e21cf749f4386bd91245df7c54fada73aa
dgd <unknown>
2003-09-19 12:51:00 +0000

    no message

--- ScorePlayerMorph/instance/initialization/openMIDIFile.cs
+++ ScorePlayerMorph/instance/initialization/openMIDIFile.cs
@@ -2,8 +2,9 @@ openMIDIFile
 	"Open a MIDI score and re-init controls..."
 	| score fileName f player |
 	fileName _ Utilities chooseFileWithSuffixFromList: #('.mid' '.midi')
-					withCaption: 'Choose a MIDI file to open'.
-	fileName ifNil: [^ self].
+					withCaption: 'Choose a MIDI file to open' translated.
+	(fileName isNil or: [ fileName == #none ])
+		ifTrue: [^ self inform: 'No .mid/.midi files found in the Squeak directory' translated].
 	f _ FileStream readOnlyFileNamed: fileName.
 	score _ (MIDIFileReader new readMIDIFrom: f binary) asScore.
 	f close.
