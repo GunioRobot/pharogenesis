9b4e60a7c8d91cf7ee1c2f7d6066d7a5a0225183
dgd <unknown>
2003-09-19 10:51:00 +0000

    no message

--- TrashCanMorph/instance/initialization/initialize.cs
+++ TrashCanMorph/instance/initialization/initialize.cs
@@ -5,6 +5,6 @@ initialize
 	self image: TrashPicOn;
 		offImage: TrashPic;
 		pressedImage: TrashPicOn.
-	self setNameTo: 'Trash'.
+	self setNameTo: 'Trash' translated.
 	self setBalloonText:
-'To remove an object, drop it on a trash can.'.
+'To remove an object, drop it on any trash can. To view, and maybe retrieve, items that have been thrown away, double-click on any trash-can.  Things are retained in the trash-can if the "preserveTrash" preference is set, otherwise they are purged immediately' translated.
