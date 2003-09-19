017ea9525e67f4d778d758dbfe3d1fca48eb49d0
dgd <unknown>
2003-09-19 13:33:00 +0000

    no message

--- MidiInputMorph/instance/as yet unclassified/invokeMenu.cs
+++ MidiInputMorph/instance/as yet unclassified/invokeMenu.cs
@@ -3,10 +3,10 @@ invokeMenu
 
 	| aMenu |
 	aMenu _ CustomMenu new.
-	aMenu add: 'add channel' action: #addChannel.
-	aMenu add: 'reload instruments' target: AbstractSound selector: #updateScorePlayers.
+	aMenu add: 'add channel' translated action: #addChannel.
+	aMenu add: 'reload instruments' translated target: AbstractSound selector: #updateScorePlayers.
 	midiSynth isOn ifFalse: [
-		aMenu add: 'set MIDI port' action: #setMIDIPort.
+		aMenu add: 'set MIDI port' translated action: #setMIDIPort.
 		midiSynth midiPort
-			ifNotNil: [aMenu add: 'close MIDI port' action: #closeMIDIPort]].	
+			ifNotNil: [aMenu add: 'close MIDI port' translated action: #closeMIDIPort]].	
 	aMenu invokeOn: self defaultSelection: nil.
