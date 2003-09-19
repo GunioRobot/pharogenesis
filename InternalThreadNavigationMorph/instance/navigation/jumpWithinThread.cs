a41377eed85653dabfc5297c7488a08fa1542e9b
dgd <unknown>
2003-09-19 15:33:00 +0000

    no message

---
+++ InternalThreadNavigationMorph/instance/navigation/jumpWithinThread.cs
@@ -0,0 +1,25 @@
+jumpWithinThread
+
+	| aMenu me weHaveOthers myIndex |
+
+	me _ CurrentProjectRefactoring currentProjectName.
+	aMenu _ MenuMorph new defaultTarget: self.
+	weHaveOthers _ false.
+	myIndex _ self currentIndex.
+	listOfPages withIndexDo: [ :each :index |
+		index = myIndex ifTrue: [
+			aMenu add: 'you are here' translated action: #yourself.
+			aMenu lastSubmorph color: Color red.
+		] ifFalse: [
+			weHaveOthers _ true.
+			aMenu add: ('jump to <{1}>' translated format:{each first}) selector: #jumpToIndex: argument: index.
+			myIndex = (index - 1) ifTrue: [
+				aMenu lastSubmorph color: Color blue
+			].
+			myIndex = (index + 1) ifTrue: [
+				aMenu lastSubmorph color: Color orange
+			].
+		].
+	].
+	weHaveOthers ifFalse: [^self inform: 'This is the only project in this thread' translated].
+	aMenu popUpEvent: self world primaryHand lastEvent in: self world
