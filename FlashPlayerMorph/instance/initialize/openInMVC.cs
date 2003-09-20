7a7b2721b44a9e2eda42839cec71305f71528caa
dgd <unknown>
2003-09-20 16:42:00 +0000

    no message

--- FlashPlayerMorph/instance/initialize/openInMVC.cs
+++ FlashPlayerMorph/instance/initialize/openInMVC.cs
@@ -2,7 +2,7 @@ openInMVC
 	| window extent |
 	self localBounds: localBounds.
 	extent _ bounds extent.
-	window _ FlashPlayerWindow labelled:'Flash Player'.
+	window _ FlashPlayerWindow labelled:'Flash Player' translated.
 	window model: (FlashPlayerModel player: self).
 	window addMorph: self frame:(0@0 corner: 1@1).
 	window openInMVCExtent: extent
