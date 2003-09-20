3c0050b1d31efdafef7d10faa4457474e8de40b9
dgd <unknown>
2003-09-20 19:11:00 +0000

    no message

--- RandomNumberTile/instance/initialization/initialize.cs
+++ RandomNumberTile/instance/initialization/initialize.cs
@@ -1,13 +1,17 @@
 initialize
+	"Initialize the receiver fully, including adding all its relevant submorphs"
+
 	| m1 m2 |
 	super initialize.
-	self typeColor: (ScriptingSystem colorForType: #number).
+	self vResizing: #shrinkWrap.
+	self typeColor: (ScriptingSystem colorForType: #Number).
 	self addArrows.
-	m1 _ StringMorph contents: 'random' font: ScriptingSystem fontForTiles.
+	m1 _ StringMorph contents: 'random' translated font: ScriptingSystem fontForTiles.
 	self addMorph: m1.
 	m2 _ UpdatingStringMorph contents: '180' font: ScriptingSystem fontForTiles.
 	m2 target: self; getSelector: #literal; putSelector: #literal:.
 	m2 position: m1 topRight.
 	self addMorphBack: m2.
 	literal _ 180.
-	self updateLiteralLabel
+	self updateLiteralLabel.
+	self makeAllTilesGreen
