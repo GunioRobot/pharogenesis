bdacf37fe45412902f78e08fcf366ab8e3265b8f
dgd <unknown>
2003-09-20 16:42:00 +0000

    no message

--- FlashPlayerMorph/instance/initialize/openInWorld.cs
+++ FlashPlayerMorph/instance/initialize/openInWorld.cs
@@ -2,7 +2,7 @@ openInWorld
 	| window extent |
 	self localBounds: localBounds.
 	extent _ bounds extent.
-	window _ FlashPlayerWindow labelled:'Flash Player'.
+	window _ FlashPlayerWindow labelled:'Flash Player' translated.
 	window model: (FlashPlayerModel player: self).
 	window addMorph: self frame:(0@0 corner: 1@1).
 	window openInWorldExtent: extent
