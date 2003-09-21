5d27fb40be0ccf3b2723e707a89d226b57391284
dgd <unknown>
2003-09-21 17:49:00 +0000

    no message

--- Project/instance/release/okToChange.cs
+++ Project/instance/release/okToChange.cs
@@ -3,35 +3,37 @@ okToChange
 
 	| ok is list |
 	self subProjects size  >0 ifTrue:
-		[PopUpMenu notify: 
-'The project ', self name printString, '
+		[self inform: 
+('The project {1}
 contains sub-projects.  You must remove these
-explicitly before removing their parent.'.
+explicitly before removing their parent.' translated format:{self name}).
 		^ false].
 	ok _ world isMorph not and: [world scheduledControllers size <= 1].
-	ok ifFalse: [self isMorphic ifTrue: [
-		self parent == CurrentProject 
-			ifFalse: [^true]]].  "view from elsewhere.  just delete it."
+	ok ifFalse: [self isMorphic ifTrue:
+		[self parent == CurrentProject 
+			ifFalse: [^ true]]].  "view from elsewhere.  just delete it."
 	ok _ (self confirm:
-'Really delete the project
-', self name printString, '
-and all its windows?').
+('Really delete the project
+{1}
+and all its windows?' translated format:{self name})).
 		
 	ok ifFalse: [^ false].
 
-	world isMorph 
-		ifTrue: [world submorphs do:   "special release for wonderlands"
-						[:m | (m isKindOf: WonderlandCameraMorph)
-								and: [m getWonderland release]].
+	world isMorph ifTrue:
+		[Smalltalk at: #WonderlandCameraMorph ifPresent:[:aClass |
+			world submorphs do:   "special release for wonderlands"
+						[:m | (m isKindOf: aClass)
+								and: [m getWonderland release]]].
 			"Remove Player classes and metaclasses owned by project"
 			is _ ImageSegment new arrayOfRoots: (Array with: self).
-			(list _ is rootsIncludingPlayers) ifNotNil: [
-				list do: [:playerCls | 
-					(playerCls respondsTo: #isMeta) ifTrue: [
-						playerCls isMeta ifFalse: [
-							playerCls removeFromSystemUnlogged]]]]].
+			(list _ is rootsIncludingPlayers) ifNotNil:
+				[list do: [:playerCls | 
+					(playerCls respondsTo: #isMeta) ifTrue:
+						[playerCls isMeta ifFalse:
+							[playerCls removeFromSystemUnlogged]]]]].
 
 	self removeChangeSetIfPossible.
 	"do this last since it will render project inaccessible to #allProjects and their ilk"
+	ProjectHistory forget: self.
 	Project deletingProject: self.
 	^ true
