40deddbbaf4b23904791486717ad4bd2081a6645
dgd <unknown>
2003-09-21 15:54:00 +0000

    no message

--- ScriptStatusControl/instance/initialization/assurePauseTickControlsShow.cs
+++ ScriptStatusControl/instance/initialization/assurePauseTickControlsShow.cs
@@ -1,33 +1,20 @@
 assurePauseTickControlsShow
 	"Add two little buttons that allow the user quickly to toggle between paused and ticking state"
 
-	| aButton |
-	tickPauseButtonsShowing ifTrue: [^ self].
+	| colorSelector status |
 	self beTransparent.
-	aButton _  UpdatingThreePhaseButtonMorph new.
-	aButton image:  (ScriptingSystem formAtKey: 'PausedPicOn');
-			offImage: (ScriptingSystem formAtKey: 'PausedPicOff');
-			pressedImage: (ScriptingSystem formAtKey: 'PausedPicPressed');
-			actionSelector: #pausedUp:with:; 
-			arguments: (Array with: nil with: aButton);
-			getSelector: #scriptIsPaused;
-			actWhen: #buttonUp;
-			target: self;
-			setBalloonText:
-'Pause this script for this object'.
-	submorphs first "the script button wrapper" addMorphBack: aButton;
-		addTransparentSpacerOfSize: (0 @ 3).
-	aButton _  UpdatingThreePhaseButtonMorph new.
-	aButton image:  (ScriptingSystem formAtKey: 'TickingPicOn');
-			offImage: (ScriptingSystem formAtKey: 'TickingPicOff');
-			pressedImage: (ScriptingSystem formAtKey: 'TickingPicPressed');
-			actionSelector: #tickingUp:with:; 
-			arguments: (Array with: nil with: aButton);
-			actWhen: #buttonUp;
-			getSelector: #scriptIsTicking;
-			target: self;
-			setBalloonText:
-'Set this script for this object to tick'.
-	submorphs first "the script button wrapper" addMorphBack: aButton.
-	self currentWorld startSteppingSubmorphsOf: self.
-	tickPauseButtonsShowing _ true
+	(tickPauseWrapper isKindOf: TickIndicatorMorph) ifFalse:[
+		"this was an old guy"
+		tickPauseWrapper ifNotNil:[tickPauseWrapper delete].
+		tickPauseWrapper _ TickIndicatorMorph new.
+		tickPauseWrapper on: #mouseDown send: #mouseDownTick:onItem: to: self.
+		tickPauseWrapper on: #mouseUp send: #mouseUpTick:onItem: to: self.
+		tickPauseWrapper setBalloonText:'Press to toggle ticking state. Hold down to set tick rate.' translated.
+		self addMorphFront: tickPauseWrapper.
+	].
+	status _ scriptInstantiation status.
+	colorSelector _ ScriptingSystem statusColorSymbolFor: status.
+	tickPauseWrapper color: (Color perform: colorSelector) muchLighter.
+	tickPauseWrapper stepTime: (1000 // scriptInstantiation tickingRate max: 0).
+	tickPauseWrapper isTicking: status == #ticking.
+	tickPauseButtonsShowing _ true.
