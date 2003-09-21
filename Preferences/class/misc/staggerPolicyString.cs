3cfe469e31ac721c095b7f9d0946e12f445d9a90
dgd <unknown>
2003-09-21 13:46:00 +0000

    no message

--- Preferences/class/misc/staggerPolicyString.cs
+++ Preferences/class/misc/staggerPolicyString.cs
@@ -1,6 +1,6 @@
 staggerPolicyString
-	"Answer the string to be shown in a menu to represent the stagger-policy status"
-
-	^ (self valueOfFlag: #reverseWindowStagger)
-		ifTrue: ['<yes>stagger windows']
-		ifFalse: ['<no>stagger windows']
+	"Answer the string to be shown in a menu to represent the 
+	stagger-policy status"
+	^ ((self valueOfFlag: #reverseWindowStagger)
+		ifTrue: ['<yes>']
+		ifFalse: ['<no>']), 'stagger windows' translated
