6a6ea3bab9767468174164c89a2c2d5e1936c01e
dgd <unknown>
2003-09-21 17:40:00 +0000

    no message

--- PluggableTextMorph/instance/unaccepted edits/promptForCancel.cs
+++ PluggableTextMorph/instance/unaccepted edits/promptForCancel.cs
@@ -2,5 +2,5 @@ promptForCancel
 	"Ask if it is OK to cancel changes to text"
 	(self confirm:
 'Changes have not been saved.
-Is it OK to cancel those changes?')
+Is it OK to cancel those changes?' translated)
 		ifTrue: [model clearUserEditFlag].
