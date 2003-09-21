cfa6e94e8816d4a0e8e8885d6fde562e1eb949d2
dgd <unknown>
2003-09-21 17:45:00 +0000

    no message

--- BookMorph/instance/insert and delete/deletePage.cs
+++ BookMorph/instance/insert and delete/deletePage.cs
@@ -4,7 +4,7 @@ deletePage
 	message _ 
 'Are you certain that you
 want to delete this page and
-everything that is on it? '.
+everything that is on it? ' translated.
 	(self confirm: message) ifTrue: 
 			[self deletePageBasic].
 	
