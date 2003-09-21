6d9190f3b71f4d03b499ade3d8b76c55f87a588e
dgd <unknown>
2003-09-21 17:55:00 +0000

    no message

--- FlapTab/instance/menu/changeColor.cs
+++ FlapTab/instance/menu/changeColor.cs
@@ -4,6 +4,6 @@ changeColor
 			[^ self inform: 'Color only pertains to a flap tab when the 
 tab is textual or "solid".  This tab is
 currently graphical, so color-choice
-does not apply.' ].
+does not apply.' translated].
 	super changeColor
 	
