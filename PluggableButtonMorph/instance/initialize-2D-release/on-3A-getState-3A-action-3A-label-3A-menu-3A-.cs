89f0195ad1531ac819b2bb6a40ab59978205f7cd
gk <unknown>
2003-09-22 09:10:00 +0000

    no message

--- PluggableButtonMorph/instance/initialize-2D-release/on-3A-getState-3A-action-3A-label-3A-menu-3A-.cs
+++ PluggableButtonMorph/instance/initialize-2D-release/on-3A-getState-3A-action-3A-label-3A-menu-3A-.cs
@@ -1,6 +1,5 @@
 on: anObject getState: getStateSel action: actionSel label: labelSel menu: menuSel
 
-	self initialize.
 	self model: anObject.
 	getStateSelector _ getStateSel.
 	actionSelector _ actionSel.
