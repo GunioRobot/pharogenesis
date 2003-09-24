b2dad6bda05fcee519c130c3b4478d46cdec7be2
sd <unknown>
2003-09-24 11:40:00 +0000

    no message

---
+++ SmalltalkImage/instance/system attribute/extractParameters.cs
@@ -0,0 +1,13 @@
+extractParameters
+
+	| pName value index globals |
+	globals := Dictionary new.
+	index := 3. "Muss bei 3 starten, da 2 documentName ist"
+	[pName := self  getSystemAttribute: index.
+	pName isEmptyOrNil] whileFalse:[
+		index := index + 1.
+		value := self getSystemAttribute: index.
+		value ifNil: [value _ ''].
+ 		globals at: pName asUppercase put: value.
+		index := index + 1].
+	^globals
