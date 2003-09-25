3e028079e54aff4fef5de8d01e68f46d9d64f114
nk <unknown>
2003-09-25 11:46:00 +0000

    no message

---
+++ Player/instance/scripts-2D-vector/-2D-.cs
@@ -0,0 +1,8 @@
+- aPlayer
+	"Treating Players like vectors, subtract aPlayer from me and return a new Player"
+
+	| new |
+	new _ costume usableSiblingInstance player.
+	new setX: self getX - aPlayer asPoint x.
+	new setY: self getY - aPlayer asPoint y.
+	^ new
