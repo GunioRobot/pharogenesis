f3d6087edee1f61dd1dbb5f6144da0cbe70698a1
nk <unknown>
2003-09-25 11:37:00 +0000

    no message

---
+++ Morph/instance/*customevents-2D-scripting/removeEventTrigger-3A-.cs
@@ -0,0 +1,9 @@
+removeEventTrigger: aSymbol
+	"Remove all the event registrations for my Player that are triggered by aSymbol.
+	User custom events are triggered at the World,
+	while system custom events are triggered on individual Morphs."
+
+	| player |
+	(player _ self player) ifNil: [ ^self ].
+	self removeEventTrigger: aSymbol for: player.
+	self currentWorld removeEventTrigger: aSymbol for: player.
