066f7a275bbca5ee9e1e04aa0173fc823b0955ef
yo <unknown>
2003-09-26 10:19:00 +0000

    no message

---
+++ String/class/m17n/multiClasses.cs
@@ -0,0 +1,17 @@
+multiClasses
+
+	Character subclass: #MultiCharacter
+		instanceVariableNames: ''
+		classVariableNames: ''
+		poolDictionaries: ''
+		category: 'Multilingual-BaseClasses'.
+	AbstractString variableWordSubclass: #MultiString
+		instanceVariableNames: ''
+		classVariableNames: ''
+		poolDictionaries: ''
+		category: 'Multilingual-BaseClasses'.
+	(Smalltalk at: #MultiString) variableWordSubclass: #MultiSymbol
+		instanceVariableNames: ''
+		classVariableNames: 'MultiSymbolTable NewMultiSymbols OneCharacterMultiSymbols '
+		poolDictionaries: ''
+		category: 'Multilingual-BaseClasses'.
