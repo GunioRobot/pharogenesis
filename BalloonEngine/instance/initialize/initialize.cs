6de289ee7cfbee4f103511ca21045c089c813c95
nk <unknown>
2003-09-26 10:52:00 +0000

    no message

--- BalloonEngine/instance/initialize/initialize.cs
+++ BalloonEngine/instance/initialize/initialize.cs
@@ -1,6 +1,8 @@
 initialize
+	| w |
+	w _ Display width > 2048 ifTrue: [ 4096 ] ifFalse: [ 2048 ].
 	externals _ OrderedCollection new: 100.
-	span _ Bitmap new: 2048.
+	span _ Bitmap new: w.
 	bitBlt _ nil.
 	self bitBlt: ((BitBlt toForm: Display) destRect: Display boundingBox; yourself).
 	forms _ #().
