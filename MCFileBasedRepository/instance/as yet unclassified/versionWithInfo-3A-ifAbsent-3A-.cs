aa95cd3a9db31bc8a55e38a2a0e497513ac0a21f
avi <unknown>
2003-09-26 16:27:00 +0000

    no message

---
+++ MCFileBasedRepository/instance/as yet unclassified/versionWithInfo-3A-ifAbsent-3A-.cs
@@ -0,0 +1,7 @@
+versionWithInfo: aVersionInfo ifAbsent: errorBlock
+	| version |
+	(self allFileNamesForVersionNamed: aVersionInfo name) do:
+		[:fileName |
+		version _ self versionFromFileNamed: fileName.
+		version info = aVersionInfo ifTrue: [^ version]].
+	^ errorBlock value
