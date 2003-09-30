b0f282cff996e3ce0448c370b8433a70cab1f5ec
sd <unknown>
2003-09-30 14:02:00 +0000

    no message

--- ImageSegment/class/fileIn-2F-Out/folder.cs
+++ ImageSegment/class/fileIn-2F-Out/folder.cs
@@ -2,5 +2,5 @@ folder
 	| im |
 	"Full path name of segments folder.  Be sure to duplicate and rename the folder when you duplicate and rename an image.  Is $_ legal in all file systems?"
 
-	im _ Smalltalk imageName.
+	im _ SmalltalkImage current imageName.
 	^ (im copyFrom: 1 to: im size - 6 "'.image' size"), '_segs'
