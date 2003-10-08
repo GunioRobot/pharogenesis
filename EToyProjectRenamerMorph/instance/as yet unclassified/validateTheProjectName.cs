c67192656df6de20d318ba2c461cf3c21e18acb8
dgd <unknown>
2003-10-08 18:53:00 +0000

    no message

--- EToyProjectRenamerMorph/instance/as yet unclassified/validateTheProjectName.cs
+++ EToyProjectRenamerMorph/instance/as yet unclassified/validateTheProjectName.cs
@@ -2,22 +2,22 @@ validateTheProjectName
 
 	| proposed |
 
-	proposed _ (namedFields at: 'projectname') contents string.
-	proposed size = 0 ifTrue: [
-		self inform: 'I do need a name for the project'.
+	proposed _ (namedFields at: 'projectname') contents string withBlanksTrimmed.
+	proposed isEmpty ifTrue: [
+		self inform: 'I do need a name for the project' translated.
 		^false
 	].
 	proposed size > 24 ifTrue: [
-		self inform: 'Please make the name 24 characters or less'.
+		self inform: 'Please make the name 24 characters or less' translated.
 		^false
 	].
 	(Project isBadNameForStoring: proposed) ifTrue: [
-		self inform: 'Please remove any funny characters from the name'.
+		self inform: 'Please remove any funny characters from the name' translated.
 		^false
 	].
 	proposed = theProject name ifTrue: [^true].
 	(ChangeSorter changeSetNamed: proposed) ifNotNil: [
-		Utilities inform: 'Sorry that name is already used'.
+		Utilities inform: 'Sorry that name is already used' translated.
 		^false
 	].
 	^true
