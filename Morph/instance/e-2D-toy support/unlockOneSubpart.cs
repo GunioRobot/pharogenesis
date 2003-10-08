f9f2cecd454f84dae67c94dca9e1a9fec06de4f2
dgd <unknown>
2003-10-08 19:30:00 +0000

    no message

--- Morph/instance/e-2D-toy support/unlockOneSubpart.cs
+++ Morph/instance/e-2D-toy support/unlockOneSubpart.cs
@@ -4,6 +4,6 @@ unlockOneSubpart
 		[:m | m isLocked].
 	unlockables size <= 1 ifTrue: [^ self unlockContents].
 	aMenu _ SelectionMenu labelList: (unlockables collect: [:m | m externalName]) selections: unlockables.
-	reply _ aMenu startUpWithCaption: 'Who should be be unlocked?'.
-	reply == nil ifTrue: [^ self].
+	reply _ aMenu startUpWithCaption: 'Who should be be unlocked?' translated.
+	reply isNil ifTrue: [^ self].
 	reply unlock
