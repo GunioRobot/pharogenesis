a68c3dd4bbc7ef9019da97f25cbf8c154c148263
dgd <unknown>
2003-10-08 20:12:00 +0000

    no message

--- WaveEditor/instance/menu/chooseLoopStart.cs
+++ WaveEditor/instance/menu/chooseLoopStart.cs
@@ -3,15 +3,15 @@ chooseLoopStart
 	| bestLoops menu secs choice start |
 	possibleLoopStarts ifNil: [
 		Utilities
-			informUser: 'Finding possible loop points...'
+			informUser: 'Finding possible loop points...' translated
 			during: [possibleLoopStarts _ self findPossibleLoopStartsFrom: graph cursor]].
 	bestLoops _ possibleLoopStarts copyFrom: 1 to: (100 min: possibleLoopStarts size).
 	menu _ CustomMenu new.
 	bestLoops do: [:entry |
 		secs _ ((loopEnd - entry first) asFloat / self samplingRate) roundTo: 0.01.
-		menu add: entry third printString, ' cycles; ', secs printString, ' secs' action: entry].
+		menu add: ('{1} cycles; {2} secs' translated format:{entry third. secs}) action: entry].
 	choice _ menu startUp.
 	choice ifNil: [^ self].
-	loopCycles _ choice at: 3.
+	loopCycles _ choice third.
 	start _ self fractionalLoopStartAt: choice first.
 	self loopLength: (loopEnd asFloat - start) + 1.0.
