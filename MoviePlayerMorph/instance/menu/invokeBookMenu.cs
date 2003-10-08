e9e75a9f4b5d9ad089c276202f16a3351db0f656
dgd <unknown>
2003-10-08 19:32:00 +0000

    no message

--- MoviePlayerMorph/instance/menu/invokeBookMenu.cs
+++ MoviePlayerMorph/instance/menu/invokeBookMenu.cs
@@ -2,18 +2,18 @@ invokeBookMenu
 	"Invoke the book's control panel menu."
 	| aMenu |
 	aMenu _ MVCMenuMorph new defaultTarget: self.
-	aMenu add:	'make a new movie' action: #makeAMovie.
-	aMenu add:	'open movie file' action: #openMovieFile.
-	aMenu add:	'add sound track' action: #addSoundTrack.
+	aMenu add:	'make a new movie' translated action: #makeAMovie.
+	aMenu add:	'open movie file' translated action: #openMovieFile.
+	aMenu add:	'add sound track' translated action: #addSoundTrack.
 	aMenu addLine.
 	scorePlayer ifNotNil:
-		[soundTrackForm == nil
-			ifTrue: [aMenu add:	'show sound track' action: #showHideSoundTrack]
-			ifFalse: [aMenu add:	'hide sound track' action: #showHideSoundTrack]].
-	aMenu add:	'make thumbnail' action: #thumbnailForThisPage.
+		[soundTrackForm isNil
+			ifTrue: [aMenu add:	'show sound track' translated action: #showHideSoundTrack]
+			ifFalse: [aMenu add:	'hide sound track' translated action: #showHideSoundTrack]].
+	aMenu add:	'make thumbnail' translated action: #thumbnailForThisPage.
 	cueMorph ifNotNil:
 		["Should check if piano roll and score already have a start event
 		prior to this time."
-		aMenu add:	'end clip here' action: #endClipHere].
+		aMenu add:	'end clip here' translated action: #endClipHere].
 
 	aMenu popUpEvent: self world activeHand lastEvent in: self world
