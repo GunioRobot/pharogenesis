2875be7c2025eb0d5f4080c89ab3414769e254f7
dgd <unknown>
2003-10-08 18:59:00 +0000

    no message

--- FatBitsPaint/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ FatBitsPaint/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -2,15 +2,14 @@ addCustomMenuItems: aCustomMenu hand: aHandMorph
 
         super addCustomMenuItems: aCustomMenu hand: aHandMorph.
         aCustomMenu 
-                add: 'background color' action: #setBackgroundColor:;
-                add: 'pen color' action: #setPenColor:;
-                add: 'pen size' action: #setPenSize:;
-                add: 'fill' action: #fill;
-                add: 'magnification' action: #setMagnification:;
-                add: 'accept' action: #accept;
-                add: 'revert' action: #revert;
-                add: 'inspect' action: #inspectForm;
-                add: 'file out' action: #fileOut;
-                add: 'selection...' action: #selectionMenu:;
-                add: 'tools...' action: #toolMenu:;
-                yourself
+                add: 'background color' translated action: #setBackgroundColor:;
+                add: 'pen color' translated action: #setPenColor:;
+                add: 'pen size' translated action: #setPenSize:;
+                add: 'fill' translated action: #fill;
+                add: 'magnification' translated action: #setMagnification:;
+                add: 'accept' translated action: #accept;
+                add: 'revert' translated action: #revert;
+                add: 'inspect' translated action: #inspectForm;
+                add: 'file out' translated action: #fileOut;
+                add: 'selection...' translated action: #selectionMenu:;
+                add: 'tools...' translated action: #toolMenu:
