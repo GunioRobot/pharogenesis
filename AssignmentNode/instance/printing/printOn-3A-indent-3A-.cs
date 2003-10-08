96ebe9a7c387603f036a3e7e802c820ff4be136b
brp <unknown>
2003-10-08 14:55:00 +0000

    no message

--- AssignmentNode/instance/printing/printOn-3A-indent-3A-.cs
+++ AssignmentNode/instance/printing/printOn-3A-indent-3A-.cs
@@ -1,10 +1,16 @@
-printOn: aStream indent: level
-
+printOn: aStream indent: level 
 	aStream dialect = #SQ00
-		ifTrue: [aStream withStyleFor: #setOrReturn do: [aStream nextPutAll: 'Set '].
-				variable printOn: aStream indent: level.
-				aStream withStyleFor: #setOrReturn do: [aStream nextPutAll: ' to '].
-				value printOn: aStream indent: level + 2]
+		ifTrue: [aStream
+				withStyleFor: #setOrReturn
+				do: [aStream nextPutAll: 'Set '].
+			variable printOn: aStream indent: level.
+			aStream
+				withStyleFor: #setOrReturn
+				do: [aStream nextPutAll: ' to '].
+			value printOn: aStream indent: level + 2]
 		ifFalse: [variable printOn: aStream indent: level.
-				aStream nextPutAll: ' _ '.
-				value printOn: aStream indent: level + 2]
+			aStream
+				nextPutAll: (Preferences ansiAssignmentOperatorWhenPrettyPrinting
+						ifTrue: [' := ']
+						ifFalse: [' _ ']).
+			value printOn: aStream indent: level + 2]
