b1e377c3832e523816d5c6c7a6a24a77abcb6272
ls <unknown>
2003-10-09 10:00:00 +0000

    no message

--- MailDB/instance/fetch-2D-import-2D-export/fetchMessageCount-3A-fromPOPConnection-3A-doFormatting-3A-.cs
+++ MailDB/instance/fetch-2D-import-2D-export/fetchMessageCount-3A-fromPOPConnection-3A-doFormatting-3A-.cs
@@ -1,40 +1,4 @@
 fetchMessageCount: msgCount fromPOPConnection: popConnection doFormatting: doFormatting
 	"Download the given number of messages from the given open POP3 connection. If doFormatting is true, messages will be formatted as they are received."
 
-	| nextID msgText msg location |
-	nextID _ self nextUnusedID.
-	messageFile beginAppend.
-	('Downloading ', msgCount printString, ' messages...')
-		displayProgressAt: Sensor mousePoint
-		from: 0
-		to: msgCount
-		during: [:progressBar |
-			1 to: msgCount do: [:messageNum |
-				progressBar value: messageNum.
-				popConnection isConnected ifFalse: [
-					popConnection destroy.  "network error"
-					messageFile endAppend.
-					LastID _ nextID.
-					self saveDB.
-					^ self inform: 'Server connection unexpectedly closed.'].
-
-				"get a message"
-				msgText _ popConnection retrieveMessage: messageNum.
-
-				"save that message"
-				msg _ MailMessage from: msgText.
-				doFormatting ifTrue: [msg format].
-				location _ messageFile basicAppend: msg text id: nextID.
-				indexFile
-					at: nextID
-					put: (IndexFileEntry
-						message: msg
-						location: location
-						messageFile: messageFile
-						msgID: nextID).
-				categoriesFile file: nextID inCategory: 'new'.
-				nextID _ nextID + 1]].
-
-	messageFile endAppend.
-	LastID _ nextID.
-	self saveDB.
+	^self fetchMessageCount: msgCount ofExpectedTotal: msgCount fromPOPConnection: popConnection doFormatting: doFormatting
