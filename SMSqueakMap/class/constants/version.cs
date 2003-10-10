104b42a8d38d64c4de98cc4a20b10cb7a63ca205
gk <unknown>
2003-10-10 11:40:00 +0000

    no message

---
+++ SMSqueakMap/class/constants/version.cs
@@ -0,0 +1,9 @@
+version
+	"This is the protocol version number used for clients to decide if
+	they need to update SMSqueakMap before synching with
+	the master. In short - only increase this if changes have made
+	the clients incompatible so that they need to be updated.
+
+	2.0: Removed Module stuff and added Package releases."
+
+	^'2.0'
