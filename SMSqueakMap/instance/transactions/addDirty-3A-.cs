5d2b9ce31c4d236051ed3729eb1083cb3515fcdd
gk <unknown>
2003-10-10 11:30:00 +0000

    no message

---
+++ SMSqueakMap/instance/transactions/addDirty-3A-.cs
@@ -0,0 +1,7 @@
+addDirty: anSMObject
+	"Add the SMObject to the dirty list making
+	sure it gets committed when transaction ends."
+
+"In first SM2 version we do nothing"
+
+"	dirtyList add: anSMObject"
