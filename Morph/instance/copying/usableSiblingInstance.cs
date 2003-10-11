db30fc1c32aa62feb2672434b8e1556f16900efa
nk <unknown>
2003-10-11 16:59:00 +0000

    no message

--- Morph/instance/copying/usableSiblingInstance.cs
+++ Morph/instance/copying/usableSiblingInstance.cs
@@ -1,29 +1,24 @@
 usableSiblingInstance
-	"Return another similar morph whose Player is of the same class as mine"
+	"Return another similar morph whose Player is of the same class as mine.
+	Do not open it in the world."
 
-	|  aName usedNames newPlayer newMorph topRenderer |
-	self flag: #noteToTed.  "I think this may have deviated majorly from your mainstream veryDeepCopy work, so there will probably be some problems entailed"
-	(topRenderer _ self topRendererOrSelf) == self ifFalse: [^ topRenderer usableSiblingInstance].
-
-	self player assureUniClass.
-	newMorph _ self veryDeepCopyWithoutPlayer.
-	newPlayer _ self player class new costume: newMorph.
-	newPlayer copyAddedStateFrom: self player.
+	| aName usedNames newPlayer newMorph topRenderer |
+	(topRenderer := self topRendererOrSelf) == self 
+		ifFalse: [^topRenderer usableSiblingInstance].
+	self assuredPlayer assureUniClass.
+	newMorph := self veryDeepCopySibling.
+	newPlayer := newMorph player.
 	newPlayer resetCostumeList.
-	self isFlexMorph ifTrue: [newMorph renderedMorph player: newPlayer]. "???"
-	newMorph actorState: (self player actorState shallowCopy initializeFor: newPlayer).
-
-	(aName _ self knownName) == nil ifTrue:
-		[self player ~~ nil ifTrue: [aName _ newMorph innocuousName]].
-			"Force a difference here"
-	aName ~~ nil ifTrue:
-		[usedNames _ (self world ifNil: [OrderedCollection new] ifNotNil: [self world allKnownNames]) copyWith: aName.
-		newMorph setNameTo: (Utilities keyLike: aName satisfying: [:f | (usedNames includes: f) not])].
+	(aName := self knownName) isNil 
+		ifTrue: [self player notNil ifTrue: [aName := newMorph innocuousName]].
+	"Force a difference here"
+	aName notNil 
+		ifTrue: 
+			[usedNames := (self world ifNil: [OrderedCollection new]
+						ifNotNil: [self world allKnownNames]) copyWith: aName.
+			newMorph setNameTo: (Utilities keyLike: aName
+						satisfying: [:f | (usedNames includes: f) not])].
 	newMorph privateOwner: nil.
-	(newMorph renderedMorph eventHandler ~~ nil) ifTrue:
-		[newPlayer assureEventHandlerRepresentsStatus].
-	self currentWorld addMorphBack: newMorph.
-
+	newPlayer assureEventHandlerRepresentsStatus.
 	self presenter flushPlayerListCache.
-
-	^ newMorph
+	^newMorph
