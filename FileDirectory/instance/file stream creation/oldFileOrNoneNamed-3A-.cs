fc34cb154630cc6f118f2a41f57a0cc52bcc3ae7
tpr <unknown>
2003-10-13 12:34:00 +0000

    no message

--- FileDirectory/instance/file stream creation/oldFileOrNoneNamed-3A-.cs
+++ FileDirectory/instance/file stream creation/oldFileOrNoneNamed-3A-.cs
@@ -1,8 +1,4 @@
 oldFileOrNoneNamed: fileName
 	"If the file exists, answer a read-only FileStream on it. If it doesn't, answer nil."
 
-	| fullName |
-	fullName _ FileStream fullName: fileName.
-	(FileStream concreteStream isAFileNamed: fullName)
-		ifTrue: [^ FileStream concreteStream readOnlyFileNamed: fullName]
-		ifFalse: [^ nil].
+	^ FileStream oldFileOrNoneNamed: fileName
