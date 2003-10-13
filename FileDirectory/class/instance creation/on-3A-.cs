82e744013645ac283044bf42fe6cd8bcaa32f035
tpr <unknown>
2003-10-13 10:49:00 +0000

    no message

--- FileDirectory/class/instance creation/on-3A-.cs
+++ FileDirectory/class/instance creation/on-3A-.cs
@@ -2,7 +2,7 @@ on: pathString
 	"Return a new file directory for the given path, of the appropriate FileDirectory subclass for the current OS platform."
 
 	| pathName |
-	DirectoryClass ifNil: [DirectoryClass _ self activeDirectoryClass].
+	DirectoryClass ifNil: [self setDefaultDirectoryClass].
 	"If path ends with a delimiter (: or /) then remove it"
 	((pathName _ pathString) endsWith: self pathNameDelimiter asString) ifTrue: [
 		pathName _ pathName copyFrom: 1 to: pathName size - 1].
