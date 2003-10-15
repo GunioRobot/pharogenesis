190450fab624e74eff6e618e3865566f5dc7bb63
jdr <unknown>
2003-10-15 15:48:00 +0000

    no message

---
+++ PNMReadWriter/instance/writing/writeHeader-3A-.cs
@@ -0,0 +1,21 @@
+writeHeader: myType
+	"this is ascii"
+	stream nextPut: ($P asciiValue).
+	stream nextPut: (myType asciiValue).
+	stream nextPut: 10.		"nl"
+	pragma ifNotNil:[
+		stream nextPutAll: (pragma asByteArray).
+	].
+	stream nextPutAll: (cols printString) asByteArray.
+	stream nextPut: 32.		" "
+	stream nextPutAll: (rows printString) asByteArray.
+	stream nextPut: 10.		"nl"
+	depth > 1 ifTrue: [| d c maxV |
+		d _ {1 . 2 . 4  . 8   . 16 . 32}.
+		c _ {1 . 3 . 15 . 255 . 31 . 255}. 
+		maxV _ nil.
+		1 to: d size do:[:i| ((d at: i) = depth and:[maxV = nil]) ifTrue:[maxV _ c at: i]].
+		stream nextPutAll: (maxV printString) asByteArray.
+		stream nextPut: 10.		"nl"
+	]
+	
