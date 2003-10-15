4880139b60d7d307bf1933fe5bcc9fcea2c113a1
yo <unknown>
2003-10-15 19:02:00 +0000

    no message

---
+++ PositionableStream/instance/fileIn-2F-Out/copyMethodChunkFrom-3A-at-3A-.cs
@@ -0,0 +1,8 @@
+copyMethodChunkFrom: aStream at: pos
+	"Copy the next chunk from aStream (must be different from the receiver)."
+	| chunk |
+	aStream position: pos.
+	chunk _ aStream nextChunkText.
+	chunk runs values size = 1 "Optimize for unembellished text"
+		ifTrue: [self nextChunkPut: chunk asString]
+		ifFalse: [self nextChunkPutWithStyle: chunk]
