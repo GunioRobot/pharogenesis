a443c66378dfab49a49e4f1163dc20fe0da3e0d9
jdr <unknown>
2003-10-16 16:03:00 +0000

    no message

---
+++ PNMReadWriter/instance/reading/readPlainBW.cs
@@ -0,0 +1,14 @@
+readPlainBW
+	"plain BW"
+	| val form poker |
+	form _ Form extent: cols@rows depth: depth.
+	poker _ BitBlt current bitPokerToForm: form.
+	0 to: rows-1 do: [:y |
+		0 to: cols-1 do: [:x |
+			[val _ stream next. (val = $0 or:[val = $1])] whileFalse:[
+				val ifNil:[self error:'End of file reading PBM'].
+			].
+			poker pixelAt: x@y put: (val asInteger).
+		]
+	].
+	^form
