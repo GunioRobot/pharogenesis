ebd16f0cfa7609f99b3f47e2bba4fa5d355c77a5
nk <unknown>
2003-10-16 14:02:00 +0000

    no message

--- Morph/instance/submorphs-2D-add-2F-remove/removeAllMorphsIn-3A-.cs
+++ Morph/instance/submorphs-2D-add-2F-remove/removeAllMorphsIn-3A-.cs
@@ -1,10 +1,11 @@
 removeAllMorphsIn: aCollection
 	"greatly speeds up the removal of *lots* of submorphs"
 	| set myWorld |
+	set _ IdentitySet new: aCollection size * 4 // 3.
+	aCollection do: [:each | each owner == self ifTrue: [ set add: each]].
 	myWorld _ self world.
 	(fullBounds notNil or:[myWorld notNil]) ifTrue:[self invalidRect: self fullBounds].
-	aCollection do: [:m | m outOfWorld: myWorld; privateOwner: nil].
-	set _ IdentitySet new: aCollection size * 4 // 3.
-	aCollection do: [:each | set add: each].
+	set do: [:m | myWorld ifNotNil: [ m outOfWorld: myWorld ]. m privateOwner: nil].
 	submorphs _ submorphs reject: [ :each | set includes: each].
+	set do: [ :m | self removedMorph: m ].
 	self layoutChanged.
