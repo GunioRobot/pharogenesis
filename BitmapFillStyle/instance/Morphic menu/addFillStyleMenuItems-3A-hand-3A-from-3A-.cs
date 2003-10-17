d9df56edd697a008f55d097410b41a379646430f
dgd <unknown>
2003-10-17 22:34:00 +0000

    no message

--- BitmapFillStyle/instance/Morphic menu/addFillStyleMenuItems-3A-hand-3A-from-3A-.cs
+++ BitmapFillStyle/instance/Morphic menu/addFillStyleMenuItems-3A-hand-3A-from-3A-.cs
@@ -1,5 +1,5 @@
 addFillStyleMenuItems: aMenu hand: aHand from: aMorph
 	"Add the items for changing the current fill style of the receiver"
-	aMenu add: 'choose new graphic' target: self selector: #chooseNewGraphicIn:event: argument: aMorph.
-	aMenu add: 'grab new graphic' target: self selector: #grabNewGraphicIn:event: argument: aMorph.
+	aMenu add: 'choose new graphic' translated target: self selector: #chooseNewGraphicIn:event: argument: aMorph.
+	aMenu add: 'grab new graphic' translated target: self selector: #grabNewGraphicIn:event: argument: aMorph.
 	super addFillStyleMenuItems: aMenu hand: aHand from: aMorph.
