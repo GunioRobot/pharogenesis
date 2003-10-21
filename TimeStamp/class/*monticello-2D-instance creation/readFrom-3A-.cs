1cc14e905facc2a4d00dd8de709f0dc83547965b
nk <unknown>
2003-10-21 23:04:00 +0000

    no message

--- TimeStamp/class/*monticello-2D-instance creation/readFrom-3A-.cs
+++ TimeStamp/class/*monticello-2D-instance creation/readFrom-3A-.cs
@@ -2,7 +2,6 @@ readFrom: stream
 	| date time |
 	stream skipSeparators.
 	date _ Date readFrom: stream.
-	stream peek = $, ifTrue: [stream next].
 	stream skipSeparators.
 	time _ Time readFrom: stream.
 	^self 
