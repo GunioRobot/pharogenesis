cf340feb13de4f5a11ffe1d21b1ff94f9a998ce3
md <unknown>
2003-10-22 15:51:00 +0000

    no message

--- ProjectViewMorph/instance/events/enter.cs
+++ ProjectViewMorph/instance/events/enter.cs
@@ -1,17 +1,22 @@
 enter
 	"Enter my project."
-	self world == self outermostWorldMorph ifFalse: [^1 beep]. "can't do this at the moment"
-	project class == DiskProxy ifFalse: [
-		(project world notNil and: [project world isMorph and:
-				[project world outermostWorldMorph == self outermostWorldMorph]]) ifTrue: [
-			^1 beep		"project is open in a window already"
-		].
-	].
 
-	project class == DiskProxy ifTrue: ["When target is not in yet"
-		self enterWhenNotPresent.	"will bring it in"
-		project class == DiskProxy ifTrue: [^self inform: 'Project not found']].
-	(owner isKindOf: SystemWindow)
-		ifTrue: [project setViewSize: self extent].
+	self world == self outermostWorldMorph ifFalse: [^Beeper beep].	"can't do this at the moment"
+	project class == DiskProxy 
+		ifFalse: 
+			[(project world notNil and: 
+					[project world isMorph 
+						and: [project world hasOwner: self outermostWorldMorph]]) 
+				ifTrue: [^Beeper beep	"project is open in a window already"]].
+	project class == DiskProxy 
+		ifTrue: 
+			["When target is not in yet"
+
+			self enterWhenNotPresent.	"will bring it in"
+			project class == DiskProxy ifTrue: [^self inform: 'Project not found']].
+	(owner isSystemWindow) ifTrue: [project setViewSize: self extent].
 	self showMouseState: 3.
-	project enter: false revert: false saveForRevert: false.
+	project 
+		enter: false
+		revert: false
+		saveForRevert: false
