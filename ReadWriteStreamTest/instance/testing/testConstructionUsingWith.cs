011d84ae24cf48d4b863ab4430b114acf27898b4
md <unknown>
2003-10-22 12:47:00 +0000

    no message

---
+++ ReadWriteStreamTest/instance/testing/testConstructionUsingWith.cs
@@ -0,0 +1,5 @@
+testConstructionUsingWith
+	"Use the with: constructor."
+	| aStream |
+	aStream _ ReadWriteStream with: #(1 2).
+	self assert: (aStream contents = #(1 2)) description: 'Ensure correct initialization.'
