52f9d468a763b2631e3124bc52dba7fc5b1071e8
md <unknown>
2003-10-22 16:16:00 +0000

    no message

--- UniclassScript/instance/versions/revertScriptVersionFrom-3A-.cs
+++ UniclassScript/instance/versions/revertScriptVersionFrom-3A-.cs
@@ -1,15 +1,15 @@
-revertScriptVersionFrom: anEditor
+revertScriptVersionFrom: anEditor 
 	"Let user choose which prior tile version to revert to, and revert to it"
 
 	| aMenu chosenStampAndTileList |
-	formerScriptingTiles isEmptyOrNil ifTrue: [^ self beep].
-	formerScriptingTiles size == 1
-		ifTrue:
-			[chosenStampAndTileList _ formerScriptingTiles first]
-		ifFalse:
-			[aMenu _ SelectionMenu labelList: (formerScriptingTiles collect: [:e | e first])
-				selections: formerScriptingTiles.
-			chosenStampAndTileList _ aMenu startUp].
-	chosenStampAndTileList ifNotNil:
-		[anEditor reinsertSavedTiles: chosenStampAndTileList second.
-		isTextuallyCoded _ false]
+	formerScriptingTiles isEmptyOrNil ifTrue: [^Beeper beep].
+	chosenStampAndTileList := formerScriptingTiles size == 1 
+		ifTrue: [ formerScriptingTiles first]
+		ifFalse: 
+			[aMenu := SelectionMenu 
+						labelList: (formerScriptingTiles collect: [:e | e first])
+						selections: formerScriptingTiles.
+			aMenu startUp].
+	chosenStampAndTileList ifNotNil: 
+			[anEditor reinsertSavedTiles: chosenStampAndTileList second.
+			isTextuallyCoded := false]
