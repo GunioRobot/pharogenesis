5e15f3aa88272d1db3b326935110cf5e9fffb54c
md <unknown>
2003-10-22 16:18:00 +0000

    no message

--- UpdatingStringMorph/instance/editing/setFontStyle.cs
+++ UpdatingStringMorph/instance/editing/setFontStyle.cs
@@ -1,8 +1,11 @@
 setFontStyle
 	| aList reply style |
-	aList _ (TextConstants select: [:anItem | anItem isKindOf: TextStyle])
-			keys asOrderedCollection.
-	reply _ (SelectionMenu labelList: aList selections: aList) startUp.
-	reply ~~ nil ifTrue:
-		[(style _ TextStyle named: reply) ifNil: [self beep. ^ true].
-		self font: (style defaultFont)]
+	aList := (TextConstants select: [:anItem | anItem isKindOf: TextStyle]) 
+				keys asOrderedCollection.
+	reply := (SelectionMenu labelList: aList selections: aList) startUp.
+	reply notNil 
+		ifTrue: 
+			[(style := TextStyle named: reply) ifNil: 
+					[Beeper beep.
+					^true].
+			self font: style defaultFont]
