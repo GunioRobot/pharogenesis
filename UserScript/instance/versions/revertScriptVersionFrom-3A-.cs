508916ac9542c13ddc39978854a303e73c687898
md <unknown>
2003-10-22 16:07:00 +0000

    no message

--- UserScript/instance/versions/revertScriptVersionFrom-3A-.cs
+++ UserScript/instance/versions/revertScriptVersionFrom-3A-.cs
@@ -1,14 +1,14 @@
-revertScriptVersionFrom: anEditor
+revertScriptVersionFrom: anEditor 
 	"Let user choose which prior tile version to revert to, and revert to it"
 
 	| aMenu result |
-	formerScriptEditors isEmptyOrNil ifTrue: [^ self beep].
-	formerScriptEditors size == 1
-		ifTrue:
-			[result _ formerScriptEditors first]
-		ifFalse:
-			[aMenu _ SelectionMenu labelList: (formerScriptEditors collect: [:e | e timeStamp])
-				selections: formerScriptEditors.
-			result _ aMenu startUp].
-	result ifNotNil:
-		[self revertScriptVersionFrom: anEditor installing: result]
+	formerScriptEditors isEmptyOrNil ifTrue: [^Beeper beep].
+	result := formerScriptEditors size == 1 
+		ifTrue: [formerScriptEditors first]
+		ifFalse: 
+			[aMenu := SelectionMenu 
+						labelList: (formerScriptEditors collect: [:e | e timeStamp])
+						selections: formerScriptEditors.
+			aMenu startUp].
+	result 
+		ifNotNil: [self revertScriptVersionFrom: anEditor installing: result]
