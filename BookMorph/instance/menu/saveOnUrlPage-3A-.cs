06120ce7a701ef80439c1efbea92337875db9568
dgd <unknown>
2003-10-26 13:18:00 +0000

    no message

--- BookMorph/instance/menu/saveOnUrlPage-3A-.cs
+++ BookMorph/instance/menu/saveOnUrlPage-3A-.cs
@@ -4,19 +4,20 @@ saveOnUrlPage: pageMorph
 	(self valueOfProperty: #keepTogether) ifNotNil: [
 		self inform: 'This book is marked ''keep in one file''. 
 Several pages use a common Player.
-Save the owner of the book instead.'.
+Save the owner of the book instead.' translated.
 		^ self].
 	"Don't give the chance to put in a different place.  Assume named by number"
-	((self valueOfProperty: #url) == nil and: [pages first url ~~ nil]) ifTrue: [
-		response _ (PopUpMenu labels: 'Old book\New book sharing old pages' 
-					withCRs)
-				startUpWithCaption: 'Modify the old book, or make a new\book sharing its pages?' withCRs.
+	((self valueOfProperty: #url) isNil and: [pages first url notNil]) ifTrue: [
+		response _ (PopUpMenu labels: 'Old book
+New book sharing old pages' translated)
+				startUpWithCaption: 'Modify the old book, or make a new
+book sharing its pages?' translated.
 		response = 2 ifTrue: [
 			"Make up new url for .bo file and confirm with user."  "Mark as shared"
 			[rand _ String new: 4.
 			1 to: rand size do: [:ii |
 				rand at: ii put: ('bdfghklmnpqrstvwz' at: 17 atRandom)].
-			(newPlace _ self getStemUrl) size = 0 ifTrue: [^ self].
+			(newPlace _ self getStemUrl) isEmpty ifTrue: [^ self].
 			newPlace _ (newPlace copyUpToLast: $/), '/BK', rand, '.bo'.
 			dir _ ServerFile new fullPath: newPlace.
 			(dir includesKey: dir fileName)] whileTrue.	"keep doing until a new file"
@@ -24,8 +25,8 @@ Save the owner of the book instead.'.
 		response = 0 ifTrue: [^ self]].
 
 	stem _ self getStemUrl.	"user must approve"
-	stem size = 0 ifTrue: [^ self].
-	ind _ pages identityIndexOf: pageMorph ifAbsent: [self error: 'where is the page?'].
+	stem isEmpty ifTrue: [^ self].
+	ind _ pages identityIndexOf: pageMorph ifAbsent: [self error: 'where is the page?' translated].
 	pageMorph isInMemory ifTrue: ["not out now"
 			pageMorph saveOnURL: stem,(ind printString),'.sp'].
 	self saveIndexOfOnly: pageMorph.
