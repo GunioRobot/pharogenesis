4ee26de00dc8538545d3cef6f626ba2b806f2300
dgd <unknown>
2003-10-26 13:21:00 +0000

    no message

--- BookPageSorterMorph/instance/as yet unclassified/addControls.cs
+++ BookPageSorterMorph/instance/as yet unclassified/addControls.cs
@@ -1,11 +1,13 @@
 addControls
 
-	| b r aButton str |
-	b _ SimpleButtonMorph new target: self; borderColor: Color black.
+	| bb r aButton str |
 	r _ AlignmentMorph newRow color: Color transparent; borderWidth: 0; layoutInset: 0.
-	r wrapCentering: #center; cellPositioning: #topCenter; hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
-	r addMorphBack: (self wrapperFor: (b fullCopy label: 'Okay';	actionSelector: #acceptSort)).
-	r addMorphBack: (self wrapperFor: (b fullCopy label: 'Cancel';	actionSelector: #delete)).
+	r wrapCentering: #center; cellPositioning: #topCenter; 
+			hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
+	bb _ SimpleButtonMorph new target: self; borderColor: Color black.
+	r addMorphBack: (self wrapperFor: (bb label: 'Okay' translated;	actionSelector: #acceptSort)).
+	bb _ SimpleButtonMorph new target: self; borderColor: Color black.
+	r addMorphBack: (self wrapperFor: (bb label: 'Cancel' translated;	actionSelector: #delete)).
 
 	r addTransparentSpacerOfSize: 8 @ 0.
 	r addMorphBack: (self wrapperFor: (aButton _ UpdatingThreePhaseButtonMorph checkBox)).
@@ -14,7 +16,7 @@ addControls
 		actionSelector: #togglePartsBinStatus;
 		arguments: #();
 		getSelector: #getPartsBinStatus.
-	str _ StringMorph contents: 'Parts bin'.
+	str _ StringMorph contents: 'Parts bin' translated.
 	r addMorphBack: (self wrapperFor: str lock).
 
 	self addMorphFront: r.
