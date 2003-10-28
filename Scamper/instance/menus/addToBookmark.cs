3497ba714113a5a907afec5f0b5033ced8b9e450
dgd <unknown>
2003-10-28 13:38:00 +0000

    no message

--- Scamper/instance/menus/addToBookmark.cs
+++ Scamper/instance/menus/addToBookmark.cs
@@ -1,11 +1,11 @@
 addToBookmark
 	| key value file filename |
-	key _ self document head title ifNil: ['Untitled'].
+	key _ self document head title ifNil: ['Untitled' translated].
 	value _ self currentUrl.
 	filename _ key,'.lin'.
 	bookDir deleteFileNamed: filename. 
 	file _ StandardFileStream fileNamed: (bookDir fullNameFor: filename).
-	file ifNil:[self error: 'could not save file'].
+	file ifNil:[self error: 'could not save file' translated].
 	file nextPutAll: value asString. 
 	file close.
 	bookmark add: (Association key: key value: value).
