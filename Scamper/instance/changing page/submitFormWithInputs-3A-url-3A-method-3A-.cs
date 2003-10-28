ce91924ef3252ced764c4cc48a83e2fbae5ebc37
dgd <unknown>
2003-10-28 13:35:00 +0000

    no message

--- Scamper/instance/changing page/submitFormWithInputs-3A-url-3A-method-3A-.cs
+++ Scamper/instance/changing page/submitFormWithInputs-3A-url-3A-method-3A-.cs
@@ -1,27 +1,28 @@
 submitFormWithInputs: inputs  url: url  method: method
+	"Submit the current form with the given arguments"
+
 	| newUrl newSource | 
 	self stopEverything.
 
-	(method asLowercase ~= 'get' and: [ method asLowercase ~= 'post' ]) ifTrue: [
-		self notify: 'unkown FORM method: ', method.
-		^false ].
+	(method asLowercase ~= 'get' and: [ method asLowercase ~= 'post' ]) ifTrue:
+		[self inform: ('unknown FORM method: {1}' translated format:{method}).
+		^ false ].
 
 	newUrl _ url asUrlRelativeTo: currentUrl.	
 
-	newUrl schemeName ~= 'http' ifTrue: [
-		self notify: 'I can only submit forms via HTTP'.
-		^false ].
+	newUrl schemeName ~= 'http' ifTrue:
+		[self inform: 'I can only submit forms via HTTP' translated.
+		^ false].
 
-	self status: 'submitting form...'.
+	self status: 'submitting form...' translated.
 
-	downloadingProcess _ [
-			method asLowercase = 'get' 
+	downloadingProcess _
+			[method asLowercase = 'get' 
 				ifTrue: [newSource _ newUrl retrieveContentsArgs: inputs] 
 				ifFalse: [newSource _ newUrl postFormArgs: inputs].
 			documentQueue nextPut:  newSource.
 
-			downloadingProcess _ nil.
-		] newProcess.
+			downloadingProcess _ nil] newProcess.
 
 		downloadingProcess resume.
-		^true
+	^ true
