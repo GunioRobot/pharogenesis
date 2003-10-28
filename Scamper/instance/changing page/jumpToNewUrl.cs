67b4e2fffbca9b4ad47e666b186da3bf5e0bb185
dgd <unknown>
2003-10-28 13:33:00 +0000

    no message

--- Scamper/instance/changing page/jumpToNewUrl.cs
+++ Scamper/instance/changing page/jumpToNewUrl.cs
@@ -1,6 +1,6 @@
 jumpToNewUrl
 	"change to a new, user-specified page"
 	| newUrl |
-	newUrl _ FillInTheBlank request: 'url to visit' initialAnswer: currentUrl toText.
+	newUrl _ FillInTheBlank request: 'url to visit' translated initialAnswer: currentUrl toText.
 	(newUrl isNil or: [ newUrl isEmpty ]) ifTrue: [ ^self ].
 	self jumpToAbsoluteUrl: newUrl
