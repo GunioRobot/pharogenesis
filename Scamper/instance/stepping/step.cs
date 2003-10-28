bba189dd51d9089c0d888051039319a089ed0084
dgd <unknown>
2003-10-28 13:25:00 +0000

    no message

--- Scamper/instance/stepping/step.cs
+++ Scamper/instance/stepping/step.cs
@@ -6,11 +6,11 @@ step
 
 		results == #stateDownloaded ifTrue: [ 
 			"images and such have been downloaded; update the page"
-			self status: 'reformatting page...'.
+			self status: 'reformatting page...' translated.
 			formattedPage _ document formattedTextForBrowser: self defaultBaseUrl: currentUrl.
 			backgroundColor _ Color fromString: document body bgcolor.
 			self changeAll: #(backgroundColor formattedPage).
-			self status: 'sittin'. ]
+			self status: 'sittin' translated. ]
 		 ifFalse: [		
 			self displayDocument: results 	
 		] ]
