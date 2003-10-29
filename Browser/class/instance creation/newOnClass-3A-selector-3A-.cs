0fd2fe031247df3e53d623242d063664c9874af0
jcg <unknown>
2003-10-29 23:12:00 +0000

    no message

--- Browser/class/instance creation/newOnClass-3A-selector-3A-.cs
+++ Browser/class/instance creation/newOnClass-3A-selector-3A-.cs
@@ -2,7 +2,8 @@ newOnClass: aClass selector: aSymbol
 	"Open a new class browser on this class."
 	| newBrowser |
 
-	newBrowser _ Browser new.
+	newBrowser _ self new.
 	newBrowser setClass: aClass selector: aSymbol.
-	Browser openBrowserView: (newBrowser openOnClassWithEditString: nil)
-			label: 'Class Browser: ', aClass name
+	^ self 
+		openBrowserView: (newBrowser openOnClassWithEditString: nil)
+		label: 'Class Browser: ', aClass name
