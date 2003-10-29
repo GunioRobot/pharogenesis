ab86f6da8f87d4018c2c68fcab812595c1dd7540
jcg <unknown>
2003-10-29 23:11:00 +0000

    no message

--- Browser/class/instance creation/openBrowser.cs
+++ Browser/class/instance creation/openBrowser.cs
@@ -1,8 +1,11 @@
 openBrowser
-	"Create and schedule a BrowserView with label 'System Browser'. The 
-	view consists of five subviews, starting with the list view of system 
+	"Create and schedule a BrowserView with default browser label. The
+	view consists of five subviews, starting with the list view of system
 	categories of SystemOrganization. The initial text view part is empty."
 
-	Browser openBrowserView: (Browser new openEditString: nil)
-			label: 'System Browser'
+	| br |
+	br := self new.
+	^ self
+		openBrowserView: (br openEditString: nil)
+		label: br defaultBrowserTitle.
 
