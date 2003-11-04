edc551a2628f0b58c34b859d3aec8b0816135f4f
lr <unknown>
2003-11-04 12:07:00 +0000

    no message

--- Collection/class/private/initialize.cs
+++ Collection/class/private/initialize.cs
@@ -2,4 +2,5 @@ initialize
 	"Set up a Random number generator to be used by atRandom when the 
 	user does not feel like creating his own Random generator."
 
-	RandomForPicking _ Random new
+	RandomForPicking _ Random new.
+	MutexForPicking _ Semaphore forMutualExclusion
