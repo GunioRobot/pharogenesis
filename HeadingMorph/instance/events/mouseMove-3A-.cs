2b0780e06707fcd5ad877c8e2b818d66c468280a
mk <unknown>
2003-11-07 11:36:00 +0000

    no message

--- HeadingMorph/instance/events/mouseMove-3A-.cs
+++ HeadingMorph/instance/events/mouseMove-3A-.cs
@@ -1,7 +1,3 @@
 mouseMove: evt
 
-	| v |
-	self changed.
-	v _ evt cursorPoint - bounds center.
-	degrees _ v theta radiansToDegrees.
-	magnitude _ (v r asFloat / (bounds width asFloat / 2.0)) min: 1.0.
+	self mouseDown: evt
