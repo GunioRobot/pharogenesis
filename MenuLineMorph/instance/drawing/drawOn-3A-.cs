b34da98b5b180017c48682d796a680b5375aeaa5
sd <unknown>
2003-11-08 16:00:00 +0000

    no message

--- MenuLineMorph/instance/drawing/drawOn-3A-.cs
+++ MenuLineMorph/instance/drawing/drawOn-3A-.cs
@@ -1,16 +1,20 @@
-drawOn: aCanvas
-	| pref |
-	aCanvas
-		fillRectangle: (bounds topLeft corner: bounds rightCenter)
-		color: ((pref _ Preferences menuColorFromWorld)
-					ifTrue:
-						[owner color darker]
-					ifFalse:
-						[Preferences menuLineUpperColor]).
-	aCanvas
-		fillRectangle: (bounds leftCenter corner: bounds bottomRight)
-		color: (pref
-					ifTrue:
-						[owner color lighter]
-					ifFalse:
-						[Preferences menuLineLowerColor])
+drawOn: aCanvas 
+	| baseColor |
+	baseColor := Preferences menuColorFromWorld
+				ifTrue: [owner color twiceDarker]
+				ifFalse: [Preferences menuAppearance3d
+						ifTrue: [owner color]
+						ifFalse: [Preferences menuLineColor]].
+	Preferences menuAppearance3d
+		ifTrue: [
+			aCanvas
+				fillRectangle: (bounds topLeft corner: bounds rightCenter)
+				color: baseColor twiceDarker.
+			
+			aCanvas
+				fillRectangle: (bounds leftCenter corner: bounds bottomRight)
+				color: baseColor twiceLighter]
+		ifFalse: [
+			aCanvas
+				fillRectangle: (bounds topLeft corner: bounds bottomRight)
+				color: baseColor]
