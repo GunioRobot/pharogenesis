2cdc240f9bcefe5fca78bbff3d946f33c6953440
nk <unknown>
2003-11-09 09:58:00 +0000

    no message

--- TextMorph/class/class initialization/initialize.cs
+++ TextMorph/class/class initialization/initialize.cs
@@ -1,7 +1,10 @@
 initialize	"TextMorph initialize"
+	
 	"Initialize constants shared by classes associated with text display."
 
 	CaretForm _ (ColorForm extent: 16@5
 					fromArray: #(2r001100e26 2r001100e26 2r011110e26 2r111111e26 2r110011e26)
 					offset: -2@0)
-					colors: (Array with: Color transparent with: self new selectionColor).
+					colors: (Array with: Color transparent with: Preferences textHighlightColor).
+
+	self registerInFlapsRegistry.
