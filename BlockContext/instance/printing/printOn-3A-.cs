2ca400379546dfa03e05d6155396a1c9fdbd6eb3
dew <unknown>
2003-11-11 01:15:00 +0000

    no message

--- BlockContext/instance/printing/printOn-3A-.cs
+++ BlockContext/instance/printing/printOn-3A-.cs
@@ -1,5 +1,14 @@
 printOn: aStream
+	| blockString truncatedBlockString |
 
 	home == nil ifTrue: [^aStream nextPutAll: 'a BlockContext with home=nil'].
 	aStream nextPutAll: '[] in '.
-	super printOn: aStream
+	super printOn: aStream.
+	aStream nextPutAll: ' '.
+	blockString _ ((self decompile ifNil: ['--source missing--']) printString
+						replaceAll: Character cr with: Character space)
+							replaceAll: Character tab with: Character space.
+	truncatedBlockString _ blockString truncateWithElipsisTo: 80.
+	truncatedBlockString size < blockString size ifTrue:
+		[truncatedBlockString _ truncatedBlockString, ']}'].
+	aStream nextPutAll: truncatedBlockString.
