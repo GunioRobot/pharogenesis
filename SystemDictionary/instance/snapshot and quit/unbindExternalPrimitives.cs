20859d20c608ea9b72c2dad082478a7ce364b6ec
nk <unknown>
2003-11-12 10:32:00 +0000

    no message

--- SystemDictionary/instance/snapshot and quit/unbindExternalPrimitives.cs
+++ SystemDictionary/instance/snapshot and quit/unbindExternalPrimitives.cs
@@ -1,4 +1,5 @@
 unbindExternalPrimitives
 	"Primitive. Force all external primitives to be looked up again afterwards. Since external primitives that have not found are bound for fast failure this method will force the lookup of all primitives again so that after adding some plugin the primitives may be found."
-	<primitive: 570>
+	^ self deprecated: 'Use SmalltalkImage unbindExternalPrimitives'
+		block: [SmalltalkImage unbindExternalPrimitives].
 	"Do nothing if the primitive fails for compatibility with older VMs"
