dd58d4f6ece6c385edff3d4b3631122ea1b72c47
sd <unknown>
2003-11-13 21:07:00 +0000

    no message

---
+++ FillInTheBlankMorph/instance/initialization/createAcceptButton.cs
@@ -0,0 +1,24 @@
+createAcceptButton
+	"create the [accept] button"
+	| result frame |
+	result := SimpleButtonMorph new target: self;
+				 color: Color lightGreen.
+	result
+		borderColor: (Preferences menuAppearance3d
+				ifTrue: [#raised]
+				ifFalse: [result color twiceDarker]).
+	result label: 'Accept(s)' translated;
+		 actionSelector: #accept.
+	result setNameTo: 'accept'.
+	frame := LayoutFrame new.
+	frame rightFraction: 0.5;
+		 rightOffset: -10;
+		 bottomFraction: 1.0;
+		 bottomOffset: -2.
+	result layoutFrame: frame.
+	self addMorph: result.
+	self
+		updateColor: result
+		color: result color
+		intensity: 2.
+	^ result
