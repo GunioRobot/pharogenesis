82a541d409e3f3d5e38044f3a573ec4870f55e72
sd <unknown>
2003-11-13 21:28:00 +0000

    no message

--- Morph/instance/meta-2D-actions/makeMultipleSiblings-3A-.cs
+++ Morph/instance/meta-2D-actions/makeMultipleSiblings-3A-.cs
@@ -2,7 +2,7 @@ makeMultipleSiblings: evt
 	"Make multiple siblings, first prompting the user for how many"
 
 	| result |
-	result _ FillInTheBlank request: 'how many siblings do you want?' initialAnswer: '2'.
+	result _ FillInTheBlank request: 'how many siblings do you want?' translated initialAnswer: '2'.
 	result isEmptyOrNil ifTrue: [^ self].
-	result first isDigit ifFalse: [^ self beep].
+	result first isDigit ifFalse: [^ Beeper beep].
 	self topRendererOrSelf makeSiblings: result asInteger.
