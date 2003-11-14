d8364cb569c4012aa5ea7cbd85b69adce036bab3
md <unknown>
2003-11-14 17:32:00 +0000

    no message

---
+++ TextFieldMorph/class/scripting/authoringPrototype.cs
@@ -0,0 +1,10 @@
+authoringPrototype 
+	"Answer an instance of the receiver that can serve as a prototype for authoring"
+
+	| proto |
+	proto _ super authoringPrototype.
+	proto setProperty: #shared toValue: true.
+	proto extent: 170 @ 30.
+	proto color: Color veryLightGray lighter.
+	proto contents: 'on a clear day you can...'.
+	^ proto
