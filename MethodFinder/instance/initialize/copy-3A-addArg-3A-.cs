38c8901db1cbf228c33589b7b9cecfc0acba6db7
md <unknown>
2003-11-14 16:47:00 +0000

    no message

--- MethodFinder/instance/initialize/copy-3A-addArg-3A-.cs
+++ MethodFinder/instance/initialize/copy-3A-addArg-3A-.cs
@@ -1,6 +1,6 @@
 copy: mthFinder addArg: aConstant
 	| more |
-	"Copy inputs and answers, add an additional data argument to the inputs.  The same constant for every example".
+	"Copy inputs and answers, add an additional data argument to the inputs.  The same constant for every example"
 
 	more _ Array with: aConstant.
 	data _ mthFinder data collect: [:argList | argList, more].
