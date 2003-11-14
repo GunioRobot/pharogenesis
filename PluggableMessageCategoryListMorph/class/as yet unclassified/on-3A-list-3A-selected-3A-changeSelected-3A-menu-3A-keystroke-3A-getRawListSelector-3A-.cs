9f6e4e89d474f825106eee04983fb3b1950f558a
md <unknown>
2003-11-14 16:59:00 +0000

    no message

--- PluggableMessageCategoryListMorph/class/as yet unclassified/on-3A-list-3A-selected-3A-changeSelected-3A-menu-3A-keystroke-3A-getRawListSelector-3A-.cs
+++ PluggableMessageCategoryListMorph/class/as yet unclassified/on-3A-list-3A-selected-3A-changeSelected-3A-menu-3A-keystroke-3A-getRawListSelector-3A-.cs
@@ -1,2 +1,2 @@
-on: anObject list: getListSel selected: getSelectionSel changeSelected: setSelectionSel menu: getMenuSel keystroke: keyActionSel getRawListSelector: getRawSel.
+on: anObject list: getListSel selected: getSelectionSel changeSelected: setSelectionSel menu: getMenuSel keystroke: keyActionSel getRawListSelector: getRawSel
 	^ self new on: anObject list: getListSel selected: getSelectionSel changeSelected: setSelectionSel menu: getMenuSel keystroke: keyActionSel getRawListSelector: getRawSel
