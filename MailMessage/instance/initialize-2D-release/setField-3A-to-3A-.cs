11aa8a47c2a413ffcb8f240598ab7fb6fc684b12
ads <unknown>
2003-11-14 22:41:00 +0000

    no message

--- MailMessage/instance/initialize-2D-release/setField-3A-to-3A-.cs
+++ MailMessage/instance/initialize-2D-release/setField-3A-to-3A-.cs
@@ -2,3 +2,4 @@ setField: fieldName to: aFieldValue
 	"set a field.  If any field of the specified name exists, it will be overwritten"
 	fields at: fieldName asLowercase put: (OrderedCollection with: aFieldValue).
 	text := nil.
+	tokens := nil.
