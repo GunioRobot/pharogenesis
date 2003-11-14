5d90b3c5a3a9f865af953aa557caf4592b542cb2
gk <unknown>
2003-11-14 13:00:00 +0000

    no message

---
+++ SMUtilities/class/utilities/sendMail-3A-.cs
@@ -0,0 +1,34 @@
+sendMail: aString
+	"Send the given mail message, but check for modern mail senders."
+
+	| server |
+
+	Smalltalk at: #MailSender ifPresent: [ :mailSender |
+		^mailSender sendMessage: ((Smalltalk at: #MailMessage) from: aString).
+	].
+
+	Smalltalk at: #MailComposition ifPresent: [ :mailComposition |
+		^mailComposition new
+			messageText:  aString;
+			open
+	].
+	
+	Smalltalk at: #Celeste ifPresent: [ :celeste |
+		celeste isSmtpServerSet ifTrue: [
+			Smalltalk at: #CelesteComposition ifPresent: [ :celesteComposition |
+				^celesteComposition
+					openForCeleste: celeste current 
+					initialText: aString
+			]
+		]
+	].
+
+	Smalltalk at: #AdHocComposition ifPresent: [ :adHocComposition |
+		server _  FillInTheBlank request: 'What is your mail server for outgoing mail?'.
+		^adHocComposition 
+			openForCeleste: server
+			initialText: aString
+	].
+
+	^self inform: 'Sorry, no known way to send the message'.
+	 	
