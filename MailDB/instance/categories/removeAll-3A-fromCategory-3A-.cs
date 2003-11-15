29806af59cc67697a7a3b5ffd7ac6447a58a202f
ads <unknown>
2003-11-15 13:57:00 +0000

    no message

--- MailDB/instance/categories/removeAll-3A-fromCategory-3A-.cs
+++ MailDB/instance/categories/removeAll-3A-fromCategory-3A-.cs
@@ -1,6 +1,2 @@
 removeAll: msgIDList fromCategory: categoryName
-	"Remove all the messages with ID's in the given list from the given category."
-
-	msgIDList do:
-		[: msgID |
-		 categoriesFile remove: msgID fromCategory: categoryName].
+	self categorizer removeAll: msgIDList fromCategory: categoryName.
