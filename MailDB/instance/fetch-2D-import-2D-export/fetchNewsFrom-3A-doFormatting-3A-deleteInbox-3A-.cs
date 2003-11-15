e664fbb0b909d50793a982e0bd5f149b025c2790
ads <unknown>
2003-11-15 13:51:00 +0000

    no message

--- MailDB/instance/fetch-2D-import-2D-export/fetchNewsFrom-3A-doFormatting-3A-deleteInbox-3A-.cs
+++ MailDB/instance/fetch-2D-import-2D-export/fetchNewsFrom-3A-doFormatting-3A-deleteInbox-3A-.cs
@@ -6,12 +6,12 @@ fetchNewsFrom: inboxPathName doFormatting: doFormatting deleteInbox: deleteInbox
 	((FileDirectory on: inboxPathName) includesKey: 'news') ifFalse: [^ 0].
 
 	inbox _ RNInboxFile openOn: inboxPathName, ':news'.
-	nextID _ self nextUnusedID.
 	count _ 0.
 	messageFile beginAppend.
 	inbox newsMessagesDo:
 		[: newsgroup : msgText |
 		 msg _ MailMessage from: msgText.
+		 nextID _ self nextUnusedID.
 		 doFormatting ifTrue: [msg format].
 		 location _ messageFile basicAppend: msg text id: nextID.
 		 indexFile
@@ -21,12 +21,10 @@ fetchNewsFrom: inboxPathName doFormatting: doFormatting deleteInbox: deleteInbox
 					location: location
 					messageFile: messageFile
 					msgID: nextID).
-		 categoriesFile file: nextID inCategory: newsgroup.
-		 categoriesFile file: nextID inCategory: 'new'.
-		 nextID _ nextID + 1.
+		 self categorizer file: nextID inCategory: newsgroup.
+		 self categorizer file: nextID inCategory: 'new'.
 		 count _ count + 1].
 	messageFile endAppend.
-	LastID _ nextID.
 
 	"snapshot the database and remove the inbox file"
 	self saveDB.
