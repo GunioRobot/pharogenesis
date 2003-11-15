f9c3c2f4d740088e4433501228db72907c85249f
ads <unknown>
2003-11-15 13:55:00 +0000

    no message

--- MailDB/instance/categories/removeCategory-3A-.cs
+++ MailDB/instance/categories/removeCategory-3A-.cs
@@ -1,4 +1,2 @@
 removeCategory: categoryName
-	"Remove the category with the given name. This does nothing if the category does not exist or if it is a pseudo-category."
-
-	categoriesFile removeCategory: categoryName.
+	self categorizer ensureCategoryDoesNotExist: categoryName.
