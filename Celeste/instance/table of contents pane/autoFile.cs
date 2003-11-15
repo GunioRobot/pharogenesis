19db815011dd8224a8d0932ae7ae90959820f2a8
ads <unknown>
2003-11-15 12:16:00 +0000

    no message

--- Celeste/instance/table of contents pane/autoFile.cs
+++ Celeste/instance/table of contents pane/autoFile.cs
@@ -1,6 +1,8 @@
 autoFile
 	"automatically pick a folder for the current message, and file the current message there"
 	| folder |
+	mailDB ifNil: [ ^self ].
 	folder := self chooseFilterForCurrentMessage.
 	folder ifNil: [ ^self].
+	self lastCategory: folder.
 	mailDB file: currentMsgID inCategory: folder.
