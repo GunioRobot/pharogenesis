ed560c5449183e70810b9fc35c90d22e6ab0043d
ads <unknown>
2003-11-15 12:16:00 +0000

    no message

--- Celeste/instance/table of contents pane/autoMove.cs
+++ Celeste/instance/table of contents pane/autoMove.cs
@@ -1,7 +1,9 @@
 autoMove
 	"automatically pick a folder for the current message, and move the message there"
 	| folder |
+	mailDB ifNil: [ ^self ].
 	folder := self chooseFilterForCurrentMessage.
 	folder ifNil: [ ^self].
+	self lastCategory: folder.
 	mailDB file: currentMsgID inCategory: folder.
 	self removeMessage.
