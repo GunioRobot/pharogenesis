bcc6b57806023bcbc762bdc266cb76e3634c762c
ads <unknown>
2003-11-15 13:55:00 +0000

    no message

--- MailDB/instance/categories/fileAll-3A-inCategory-3A-.cs
+++ MailDB/instance/categories/fileAll-3A-inCategory-3A-.cs
@@ -1,6 +1,2 @@
 fileAll: msgIDList inCategory: categoryName
-	"File all the messages with ID's in the given list in the given category."
-
-	msgIDList do:
-		[: msgID |
-		 categoriesFile file: msgID inCategory: categoryName].
+	self categorizer fileAll: msgIDList inCategory: categoryName.
