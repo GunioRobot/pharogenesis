c5612620e07abb7f6d8c6941427a4d0ed047e04b
ads <unknown>
2003-11-15 13:54:00 +0000

    no message

--- MailDB/instance/categories/renameCategory-3A-to-3A-.cs
+++ MailDB/instance/categories/renameCategory-3A-to-3A-.cs
@@ -1,4 +1,4 @@
 renameCategory: oldName to: newName
 	"Rename the given category. This does nothing if the category does not exist or if it is a special category ('.all.' or '.unclassified.')."
 
-	categoriesFile renameCategory: oldName to: newName.
+	self categorizer renameCategory: oldName to: newName.
