b01850070baa2f43eee730c3cc38bcb3eb6b357c
ads <unknown>
2003-11-15 13:55:00 +0000

    no message

--- MailDB/instance/categories/removeFromAllCategories-3A-.cs
+++ MailDB/instance/categories/removeFromAllCategories-3A-.cs
@@ -1,6 +1,4 @@
 removeFromAllCategories: msgID
 	"Remove the message with the given ID from all categories. The message will appear in 'unclassified'."
 
-	categoriesFile categories do:
-		[: categoryName |
-		 categoriesFile remove: msgID fromCategory: categoryName].
+	self categorizer removeFromAllCategories: msgID.
