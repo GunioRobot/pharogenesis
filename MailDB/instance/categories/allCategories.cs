ccb2a288595b23a2dc6ea6582c57cd95b8e6c7c9
ads <unknown>
2003-11-15 13:54:00 +0000

    no message

--- MailDB/instance/categories/allCategories.cs
+++ MailDB/instance/categories/allCategories.cs
@@ -1,4 +1,4 @@
 allCategories
 	"Answer a list of categories sorted in alphabetical order, including the special categories."
 
-	^categoriesFile categories asSortedCollection asOrderedCollection
+	^ self categorizer allCategories asSortedArray
