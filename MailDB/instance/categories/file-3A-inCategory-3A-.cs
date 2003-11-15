8644b8e1937626c26aced495943d9a432358a9b2
ads <unknown>
2003-11-15 13:58:00 +0000

    no message

--- MailDB/instance/categories/file-3A-inCategory-3A-.cs
+++ MailDB/instance/categories/file-3A-inCategory-3A-.cs
@@ -1,4 +1,2 @@
 file: msgID inCategory: categoryName
-	"File the message with the given ID in the given category."
-
-	categoriesFile file: msgID inCategory: categoryName.
+	self categorizer file: msgID inCategory: categoryName.
