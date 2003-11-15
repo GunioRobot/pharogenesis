808073afe8aaf4f9e486f38780ecf09563517ed9
ads <unknown>
2003-11-15 13:53:00 +0000

    no message

--- MailDB/instance/categories/messagesIn-3A-.cs
+++ MailDB/instance/categories/messagesIn-3A-.cs
@@ -3,13 +3,11 @@ messagesIn: categoryName
 	category, sorted in ascending time order.  If the category does not exist, 
 	answer an empty collection. The pseudo-categories '.all.' and 
 	'.unclassified.' are computed dynamically, which may take a little time."
-	| msgList category |
+	| msgList |
 	categoryName = '.unclassified.'
 		ifTrue: 
-			[Cursor execute showWhile: [msgList _ categoriesFile unclassifiedFrom: indexFile keys].
+			[Cursor execute showWhile: [msgList _ self categorizer unclassifiedFrom: indexFile keys].
 			^ msgList].
-	categoryName = '.all.' ifTrue: [^ indexFile keys].
+	categoryName = '.all.' ifTrue: [^ self allMessages ].
 	"otherwise, it is a real category"
-	category _ categoriesFile messagesIn: categoryName.
-	^self sortedKeysForMessages: category.
-	
+	^ self categorizer at: categoryName
