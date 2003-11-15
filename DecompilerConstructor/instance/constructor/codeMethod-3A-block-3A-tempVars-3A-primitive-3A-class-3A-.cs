1433392241b88fc4b6acccc3757a90f56c3815da
ajh <unknown>
2003-11-15 01:20:00 +0000

    no message

--- DecompilerConstructor/instance/constructor/codeMethod-3A-block-3A-tempVars-3A-primitive-3A-class-3A-.cs
+++ DecompilerConstructor/instance/constructor/codeMethod-3A-block-3A-tempVars-3A-primitive-3A-class-3A-.cs
@@ -1,16 +1,17 @@
 codeMethod: selector block: block tempVars: vars primitive: primitive class: class
 
-	| node |
+	| node methodTemps |
 	node _ self codeSelector: selector code: nil.
 	tempVars _ vars.
+	methodTemps _ tempVars select: [:t | t scope >= 0].
 	^MethodNode new
 		selector: node
-		arguments: (tempVars copyFrom: 1 to: nArgs)
+		arguments: (methodTemps copyFrom: 1 to: nArgs)
 		precedence: selector precedence
-		temporaries: (tempVars copyFrom: nArgs + 1 to: tempVars size)
+		temporaries: (methodTemps copyFrom: nArgs + 1 to: methodTemps size)
 		block: block
 		encoder: (Encoder new initScopeAndLiteralTables
-					nTemps: tempVars size
+					temps: tempVars
 					literals: literalValues
 					class: class)
 		primitive: primitive
