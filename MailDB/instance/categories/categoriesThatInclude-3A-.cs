8fe8400d9bef743eca326011f278442bbf29c36d
ads <unknown>
2003-11-15 13:57:00 +0000

    no message

--- MailDB/instance/categories/categoriesThatInclude-3A-.cs
+++ MailDB/instance/categories/categoriesThatInclude-3A-.cs
@@ -1,6 +1,2 @@
 categoriesThatInclude: msgID
-	"Answer a collection of names for real categories that include the message with the given ID. Pseudo-categories (such as '.unclassified.') are not considered real categories."
-
-	^categoriesFile categories select:
-		[: catName |
-		 (categoriesFile messagesIn: catName) includes: msgID]
+	^ self categorizer realCategoriesThatInclude: msgID
