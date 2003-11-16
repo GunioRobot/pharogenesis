1bc727d88092178f9d9b80d48ea081b26e96dab2
ads <unknown>
2003-11-16 20:50:00 +0000

    no message

--- MailDB/instance/open-2D-create-2D-save/openDB.cs
+++ MailDB/instance/open-2D-create-2D-save/openDB.cs
@@ -3,6 +3,7 @@ openDB
 
 	Transcript show: 'Opening mail database ''', rootFilename, '''...'.
 	messageFile _ MessageFile openOn: rootFilename, '.messages'.
-	indexFile _ IndexFile openOn: rootFilename, '.index' messageFile: messageFile.
+	indexFile _ IndexFile openOn: rootFilename, '.index' messageFile: messageFile readLogFlag: true.
 	categoriesFile _ CategoriesFile openOn: rootFilename, '.categories'.
+	self spamFilter. "Open spam filter file, if necessary."
 	Transcript show: 'Done.'; cr.
