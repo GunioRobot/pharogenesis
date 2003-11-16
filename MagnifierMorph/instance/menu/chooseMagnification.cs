f850bb4f9e2ca7817b5634716cc15bf5fdb7b769
md <unknown>
2003-11-16 15:14:00 +0000

    no message

--- MagnifierMorph/instance/menu/chooseMagnification.cs
+++ MagnifierMorph/instance/menu/chooseMagnification.cs
@@ -1,9 +1,9 @@
 chooseMagnification
 	| result |
 	result _ (SelectionMenu selections: #(1.5 2 4 8))
-		startUpWithCaption: 'Choose magnification
-(currently ', magnification printString, ')'.
-	(result == nil or: [result = magnification]) ifTrue: [^ self].
+		startUpWithCaption: ('Choose magnification
+(currently {1})' translated format:{magnification}).
+	(result isNil or: [result = magnification]) ifTrue: [^ self].
 	magnification _ result.
 	self extent: self extent. "round to new magnification"
-	self changed. "redraw even if extent wasn't changed".
+	self changed. "redraw even if extent wasn't changed"
