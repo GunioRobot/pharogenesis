dadd1a2345c2d2453893b516de26a08ca2696e13
sd <unknown>
2003-11-16 12:00:00 +0000

    no message

---
+++ SmalltalkImage/instance/sources, changes log/openSourceFiles.cs
@@ -0,0 +1,11 @@
+openSourceFiles
+
+	self imageName = LastImageName ifFalse:
+		["Reset the author initials to blank when the image gets moved"
+		LastImageName _ self imageName.
+		Utilities setAuthorInitials: ''].
+	FileDirectory
+		openSources: self sourcesName
+		andChanges: self changesName
+		forImage: LastImageName.
+	StandardSourceFileArray install
