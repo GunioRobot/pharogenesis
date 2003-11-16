ddf5a2b7f9595ad0df701fe0807d33c1430b36cd
ar <unknown>
2003-11-16 01:09:00 +0000

    no message

--- ObjectMemory/instance/header access/isWordsOrBytes-3A-.cs
+++ ObjectMemory/instance/header access/isWordsOrBytes-3A-.cs
@@ -1,7 +1,4 @@
 isWordsOrBytes: oop
 	"Answer true if the contains only indexable words or bytes (no oops). See comment in formatOf:"
 	"Note: Excludes CompiledMethods."
-
-	| fmt |
-	fmt _ self formatOf: oop.
-	^ fmt = 6 or: [(fmt >= 8) and: [fmt <= 11]]
+	^(self isNonIntegerObject: oop) and:[self isWordsOrBytesNonInt: oop]
