74a8fb8d0bebe3861261c0c2edde4739010178e5
ar <unknown>
2003-11-16 01:08:00 +0000

    no message

--- ObjectMemory/instance/header access/isPointers-3A-.cs
+++ ObjectMemory/instance/header access/isPointers-3A-.cs
@@ -1,4 +1,4 @@
 isPointers: oop
 	"Answer true if the argument has only fields that can hold oops. See comment in formatOf:"
 
-	^ (self formatOf: oop) <= 4
+	^(self isNonIntegerObject: oop) and:[self isPointersNonInt: oop]
