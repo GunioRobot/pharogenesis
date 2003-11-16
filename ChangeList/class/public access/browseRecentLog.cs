52fc5d41fb34b43e8ef32f987d9a3773bf5abf4f
sd <unknown>
2003-11-16 14:10:00 +0000

    no message

--- ChangeList/class/public access/browseRecentLog.cs
+++ ChangeList/class/public access/browseRecentLog.cs
@@ -1,21 +1,6 @@
-browseRecentLog    "ChangeList browseRecentLog"
-	"Prompt with a menu of how far back to go"
-	| end changesFile banners positions pos chunk i |
-	changesFile _ (SourceFiles at: 2) readOnlyCopy.
-	banners _ OrderedCollection new.
-	positions _ OrderedCollection new.
-	end _ changesFile size.
-	pos _ Smalltalk lastQuitLogPosition.
-	[pos = 0 or: [banners size > 20]] whileFalse:
-		[changesFile position: pos.
-		chunk _ changesFile nextChunk.
-		i _ chunk indexOfSubCollection: 'priorSource: ' startingAt: 1.
-		i > 0 ifTrue: [positions addLast: pos.
-					banners addLast: (chunk copyFrom: 5 to: i-2).
-					pos _ Number readFrom: (chunk copyFrom: i+13 to: chunk size)]
-			ifFalse: [pos _ 0]].
-	changesFile close.
-	pos _ (SelectionMenu labelList: banners selections: positions)
-				startUpWithCaption: 'Browse as far back as...'.
-	pos == nil ifTrue: [^ self].
-	self browseRecent: end-pos
+browseRecentLog
+	"ChangeList browseRecentLog"
+	"Prompt with a menu of how far back to go to browse the current image's changes log file"
+	^ self
+		browseRecentLogOn: (SourceFiles at: 2)
+		startingFrom: SmalltalkImage current lastQuitLogPosition
