44541e5926389fbe3f7f0e478762c70f583f11ce
ads <unknown>
2003-11-16 21:30:00 +0000

    no message

--- CategoriesFile/instance/read-2D-write/writeOn-3A-.cs
+++ CategoriesFile/instance/read-2D-write/writeOn-3A-.cs
@@ -1,13 +1,5 @@
-writeOn: aFileStream
-	"Write the categories to the given FileStream. The categories data is stored in binary (as opposed to a human-readable form) to save space."
+writeOn: aStream
+	"Write the categories to the given Stream. The categories data is stored in binary (as opposed to a human-readable form) to save space."
 
-	aFileStream binary; position: 0.
-	categories associationsDo:
-		[: category |
-		 "(category key) is the category name"
-		 "(category value) is the set of message ID's in that category"
-		 aFileStream nextStringPut: (category key).
-		 aFileStream nextWordPut: (category value) size.
-		 (category value) do:
-			[: messageID |
-			 aFileStream nextInt32Put: messageID]].
+	aStream binary; position: 0.
+	categorizer writeOn: aStream elementWriter: [:s :messageID | s nextInt32Put: messageID].
