c2341260b0c01a1731e5dd4a8b6c19fef3bdea21
gk <unknown>
2003-11-17 23:25:00 +0000

    no message

---
+++ SMSqueakMap/instance/queries/packageReleaseWithId-3A-.cs
@@ -0,0 +1,9 @@
+packageReleaseWithId: anIdString 
+	"Look up a package release. Return nil if missing.
+	Raise error if it is not a package release."
+
+	| r |
+	r _ self objectWithId: anIdString.
+	r ifNil: [^nil].
+	r isPackageRelease ifTrue:[^r].
+	self error: 'UUID did not map to a package release.'
