a1c6aebec4dbdfd720b47fcbac1162639707c71e
gk <unknown>
2003-11-17 23:15:00 +0000

    no message

---
+++ SMSqueakMap/instance/private/loadFullFrom-3A-.cs
@@ -0,0 +1,14 @@
+loadFullFrom: aServerName
+	"Contact the SqueakMap at the url <aSqueakMapUrl>
+	and load a full map from scratch."
+
+	| url  zipped |
+	url _ 'http://', aServerName, '/sm/loadgz?mapversion=', SMSqueakMap version, '&checkpoint=', checkpointNumber asString.
+	Transcript show: 'Fetch: ', (Time millisecondsToRun: [ zipped _ (HTTPSocket httpGet: url) contents]) asString, ' ms';cr.
+	Transcript show: 'Size: ', zipped size asString, ' bytes';cr.
+	((self checkVersion: zipped) and: [zipped ~= 'UPTODATE'])
+		ifTrue:[
+			Transcript show: 'Save checkpoint to disk: ', (Time millisecondsToRun: [
+			self saveCheckpoint: zipped]) asString, ' ms';cr.
+			Transcript show: 'Full reload from checkpoint: ', (Time millisecondsToRun: [
+			self reload]) asString, ' ms';cr.]
