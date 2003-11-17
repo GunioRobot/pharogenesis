3a949c66412b74c3a8c8b06fe6182e50eb69449d
gk <unknown>
2003-11-17 01:25:00 +0000

    no message

---
+++ SMPackageRelease/instance/services/noteInstalled.cs
@@ -0,0 +1,6 @@
+noteInstalled
+	"This package release was just successfully installed.
+	We tell the map so that it can keep track of what
+	package releases are installed."
+
+	map noteInstalled: self
