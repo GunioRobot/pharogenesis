2509f17ab59e9387cf710b99d1242b32539f4b1a
gk <unknown>
2003-11-17 23:35:00 +0000

    no message

---
+++ SMSqueakMap/instance/public-2D-installation/installPackageNamed-3A-autoVersion-3A-.cs
@@ -0,0 +1,12 @@
+installPackageNamed: aString autoVersion: version
+	"Install the release <version> of the package with a name
+	beginning with aString (see method comment
+	of #packageWithNameBeginning:). <version> is the
+	automatic version name."
+
+	| p r |
+	p _ self packageWithNameBeginning: aString.
+	p ifNil: [self error: 'No package found with name beginning with ', aString].
+	r _ p releaseWithAutomaticVersionString: version.
+	r ifNil: [self error: 'No package release found with automatic version ', version].
+	^self installPackageRelease: r
