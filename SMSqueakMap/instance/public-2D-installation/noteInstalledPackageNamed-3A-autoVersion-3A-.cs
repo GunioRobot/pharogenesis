36387a4f98a99d61c4d76a0b4646e659e168a09b
gk <unknown>
2003-11-18 17:51:00 +0000

    no message

---
+++ SMSqueakMap/instance/public-2D-installation/noteInstalledPackageNamed-3A-autoVersion-3A-.cs
@@ -0,0 +1,10 @@
+noteInstalledPackageNamed: aString autoVersion: aVersion
+	"Mark that the package release was just successfully installed.
+	<aVersion> is the automatic version as a String.
+	Can be used to inform SM of an installation not been done using SM."
+
+	| p |
+	p _ self packageWithNameBeginning: aString.
+	p ifNil: [self error: 'No package found with name beginning with ', aString].
+	
+^self noteInstalledPackage: p autoVersion: aVersion asVersion
