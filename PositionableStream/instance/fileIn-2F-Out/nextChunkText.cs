0e9a276e2ac44a5351af33222cb78fccb28676a8
sumim <unknown>
2003-11-20 18:13:00 +0000

    no message

--- PositionableStream/instance/fileIn-2F-Out/nextChunkText.cs
+++ PositionableStream/instance/fileIn-2F-Out/nextChunkText.cs
@@ -1,16 +1,17 @@
 nextChunkText
 	"Deliver the next chunk as a Text.  Decode the following ]style[ chunk if present.  Position at start of next real chunk."
-	| string runsRaw strm runs peek |
+	| string runsRaw strm runs peek pos |
 	"Read the plain text"
 	string _ self nextChunk.
 	
 	"Test for ]style[ tag"
+	pos _ self position.
 	peek _ self skipSeparatorsAndPeekNext.
-	peek = $] ifFalse: [^ string asText].  "no tag"
-	(self upTo: $[) = ']style' ifFalse: [^ string asText].  "different tag"
+	peek = $] ifFalse: [self position: pos. ^ string asText].  "no tag"
+	(self upTo: $[) = ']style' ifFalse: [self position: pos. ^ string asText].  "different tag"
 
 	"Read and decode the style chunk"
-	runsRaw _ self nextChunk.	"style encoding"
+	runsRaw _ self basicNextChunk.	"style encoding"
 	strm _ ReadStream on: runsRaw from: 1 to: runsRaw size.
 	runs _ RunArray scanFrom: strm.
 
