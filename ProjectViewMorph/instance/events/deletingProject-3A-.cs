e6d9db764073d018680658221128e78217246d4d
sumim <unknown>
2003-11-21 13:43:00 +0000

    no message

--- ProjectViewMorph/instance/events/deletingProject-3A-.cs
+++ ProjectViewMorph/instance/events/deletingProject-3A-.cs
@@ -4,4 +4,6 @@ deletingProject: aProject
 	self flag: #bob.		"zapping projects"
 
 
-	project == aProject ifTrue: [self delete].
+	project == aProject ifTrue: [
+		self owner isSystemWindow ifTrue: [self owner model: nil; delete].
+		self delete].
