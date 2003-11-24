07ab052d6afe2b706bfe9adba71c2891051c7660
lr <unknown>
2003-11-24 17:24:00 +0000

    no message

--- ClassDescription/instance/printing/sharedPoolsString.cs
+++ ClassDescription/instance/printing/sharedPoolsString.cs
@@ -1,7 +1,10 @@
 sharedPoolsString
 	"Answer a string of my shared pool names separated by spaces."
 
-	| aStream |
-	aStream _ WriteStream on: (String new: 100).
-	self sharedPools do: [:x | aStream nextPutAll: (self environment keyAtIdentityValue: x ifAbsent: ['private']); space].
-	^ aStream contents
+	^String streamContents: [ :stream |
+		self sharedPools 
+			do: [ :each |
+				stream nextPutAll: (self environment 
+					keyAtIdentityValue: each 
+					ifAbsent: [ 'private' ]) ]
+			separatedBy: [ stream space ] ]
