d4f1b1f5fc330d6efdcdfbd55331982c3f3963a5
asm <unknown>
2003-11-24 23:02:00 +0000

    no message

--- FreeCellStatistics/instance/printing/printSessionOn-3A-.cs
+++ FreeCellStatistics/instance/printing/printSessionOn-3A-.cs
@@ -1,10 +1,17 @@
 printSessionOn: aStream 
 	| total |
-	aStream nextPutAll: 'This session: ' , String tab.
-	self print: sessionWins type: #wins on: aStream.
+	aStream nextPutAll: 'This session: ' translated, String tab.
+	self
+		print: sessionWins
+		type: #wins
+		on: aStream.
 	aStream nextPutAll: ', '.
-	self print: sessionLosses type: #losses on: aStream.
-	total _ sessionWins + sessionLosses.
-	total ~~ 0 ifTrue: [aStream nextPutAll: ', ';
-		 print: (sessionWins / total * 100) asInteger;
-		 nextPut: $%]
+	self
+		print: sessionLosses
+		type: #losses
+		on: aStream.
+	total := sessionWins + sessionLosses.
+	total ~~ 0
+		ifTrue: [aStream nextPutAll: ', ';
+				 print: (sessionWins / total * 100) asInteger;
+				 nextPut: $%]
