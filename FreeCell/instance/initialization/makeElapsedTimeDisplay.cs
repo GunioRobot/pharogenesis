09084372c1c5076542284c2cb3135b1ccefa1038
asm <unknown>
2003-11-24 22:47:00 +0000

    no message

--- FreeCell/instance/initialization/makeElapsedTimeDisplay.cs
+++ FreeCell/instance/initialization/makeElapsedTimeDisplay.cs
@@ -1,6 +1,4 @@
 makeElapsedTimeDisplay
-
-	elapsedTimeDisplay _ LedTimerMorph new
-		digits: 3;
-		extent: (3*10@15).
-	^self wrapPanel: elapsedTimeDisplay label: 'Elapsed Time: '
+	elapsedTimeDisplay := LedTimerMorph new digits: 3;
+				 extent: 3 * 10 @ 15.
+	^ self wrapPanel: elapsedTimeDisplay label: 'Elapsed Time: ' translated
