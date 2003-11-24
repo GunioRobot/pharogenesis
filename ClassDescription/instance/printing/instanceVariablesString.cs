ff64b984e6754920c2e3b80b9f93b4fed996934d
lr <unknown>
2003-11-24 17:20:00 +0000

    no message

--- ClassDescription/instance/printing/instanceVariablesString.cs
+++ ClassDescription/instance/printing/instanceVariablesString.cs
@@ -1,5 +1,7 @@
 instanceVariablesString
 	"Answer a string of my instance variable names separated by spaces."
 
-	^ String streamContents:
-		[:strm | self instVarNames do: [:varName | strm nextPutAll: varName; space]]
+	^String streamContents: [ :stream |
+		self instVarNames 
+			do: [ :each | stream nextPutAll: each ]
+			separatedBy: [ stream space ] ]
