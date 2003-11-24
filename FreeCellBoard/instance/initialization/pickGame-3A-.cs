77f9607a164a18ac1625b1b9cb203e72bbef72f3
asm <unknown>
2003-11-24 23:13:00 +0000

    no message

--- FreeCellBoard/instance/initialization/pickGame-3A-.cs
+++ FreeCellBoard/instance/initialization/pickGame-3A-.cs
@@ -1,22 +1,22 @@
-pickGame: aSeedOrNil
+pickGame: aSeedOrNil 
 	| sorted msg |
-	cardDeck _ PlayingCardDeck newDeck.
+	cardDeck := PlayingCardDeck newDeck.
 	aSeedOrNil == 1
-	ifTrue:
-		["Special case of game 1 does a time profile playing the entire (trivial) game."
-		sorted _ cardDeck submorphs asSortedCollection: [ :a :b | a cardNumber >= b cardNumber].
-		cardDeck
-			removeAllMorphs;
-			addAllMorphs: sorted.
-		self resetBoard.
-		self world doOneCycle.
-		Utilities informUser: 'Game #1 is a special case
-for performance analysis' during:
-			[msg _ self world firstSubmorph.
-			msg align: msg topRight with: owner bottomRight.
-			MessageTally spyOn: [sorted last owner doubleClickOnCard: sorted last]]]
-	ifFalse:
-		[aSeedOrNil ifNotNil:[cardDeck seed: aSeedOrNil].
-		cardDeck shuffle.
-		self resetBoard].
-	
+		ifTrue: ["Special case of game 1 does a time profile playing the entire 
+			(trivial) game."
+			sorted := cardDeck submorphs
+						asSortedCollection: [:a :b | a cardNumber >= b cardNumber].
+			cardDeck removeAllMorphs; addAllMorphs: sorted.
+			self resetBoard.
+			self world doOneCycle.
+			Utilities
+				informUser: 'Game #1 is a special case
+for performance analysis' translated
+				during: [msg := self world firstSubmorph.
+					msg align: msg topRight with: owner bottomRight.
+					MessageTally
+						spyOn: [sorted last owner doubleClickOnCard: sorted last]]]
+		ifFalse: [aSeedOrNil
+				ifNotNil: [cardDeck seed: aSeedOrNil].
+			cardDeck shuffle.
+			self resetBoard]
