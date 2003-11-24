ab81529439145bdf4fe8ee917faac7ebb93d53a4
asm <unknown>
2003-11-24 22:57:00 +0000

    no message

--- FreeCellStatistics/instance/printing/print-3A-type-3A-on-3A-.cs
+++ FreeCellStatistics/instance/printing/print-3A-type-3A-on-3A-.cs
@@ -1,12 +1,13 @@
 print: aNumber type: type on: aStream 
-	"I moved the code from #printWins:on: and #printLosses:on: here because it is basically
+	"I moved the code from #printWins:on: and #printLosses:on: here because 
+	it is basically 
 	the same. I hope this increases the maintainability. - th 12/20/1999 20:37"
 	aStream print: aNumber.
 	type = #wins
 		ifTrue: [aNumber = 1
-				ifTrue: [aStream nextPutAll: ' win']
-				ifFalse: [aStream nextPutAll: ' wins']].
+				ifTrue: [aStream nextPutAll: ' win' translated]
+				ifFalse: [aStream nextPutAll: ' wins' translated]].
 	type = #losses
 		ifTrue: [aNumber = 1
-				ifTrue: [aStream nextPutAll: ' loss']
-				ifFalse: [aStream nextPutAll: ' losses']]
+				ifTrue: [aStream nextPutAll: ' loss' translated]
+				ifFalse: [aStream nextPutAll: ' losses' translated]]
