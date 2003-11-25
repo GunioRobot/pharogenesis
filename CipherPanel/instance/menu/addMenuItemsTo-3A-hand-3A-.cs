d99d02f694f721afbf412c774e042f87411e43b2
asm <unknown>
2003-11-25 22:21:00 +0000

    no message

--- CipherPanel/instance/menu/addMenuItemsTo-3A-hand-3A-.cs
+++ CipherPanel/instance/menu/addMenuItemsTo-3A-hand-3A-.cs
@@ -1,6 +1,21 @@
-addMenuItemsTo: aMenu hand: aHandMorph
-
-	aMenu add: 'show cipher help' target: self action: #showHelpWindow.
-	aMenu add: 'show cipher hints' target: self action: #showHintsWindow.
-	aMenu add: 'clear cipher typing' target: self action: #clearTyping.
-	aMenu add: 'enter a new cipher' target: self action: #enterANewCipher.
+addMenuItemsTo: aMenu hand: aHandMorph 
+	aMenu
+		add: 'show cipher help' translated
+		target: self
+		action: #showHelpWindow.
+	aMenu
+		add: 'show cipher hints' translated
+		target: self
+		action: #showHintsWindow.
+	aMenu
+		add: 'clear cipher typing' translated
+		target: self
+		action: #clearTyping.
+	aMenu
+		add: 'enter a new cipher' translated
+		target: self
+		action: #enterANewCipher.
+	aMenu
+		add: 'quote from Squeak' translated
+		target: self
+		action: #squeakCipher
