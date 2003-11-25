d74a2b202cdc7382fec308bcb7a1f8d6256e3010
asm <unknown>
2003-11-25 22:25:00 +0000

    no message

--- CrosticPanel/instance/menu/showHintsWindow.cs
+++ CrosticPanel/instance/menu/showHintsWindow.cs
@@ -1,19 +1,22 @@
 showHintsWindow
-
 	| hints |
 	(self confirm: 'As hints, you will be given the five longest answers.
-Do you really want to do this?') ifFalse: [^ self].
-	hints _ (answers asSortedCollection: [:x :y | x size > y size]) asArray
-		copyFrom: 1 to: 5.
-	((StringHolder new contents: 
-	'The five longest answers are...
-'	,
-	(String streamContents:
-		[:strm | hints do: 
-			[:hint | strm cr; nextPutAll: (hint collect: [:i | quote at: i])].
-		strm cr; cr])
-	,
-'Good luck!')
-		embeddedInMorphicWindowLabeled: 'Crostic Hints')
-			setWindowColor: (Color r: 1.0 g: 0.6 b: 0.0);
-			openInWorld: self world extent: 198@154
+Do you really want to do this?' translated)
+		ifFalse: [^ self].
+	hints := (answers
+				asSortedCollection: [:x :y | x size > y size]) asArray copyFrom: 1 to: 5.
+	((StringHolder new contents: 'The five longest answers are...
+' translated
+			, (String
+					streamContents: [:strm | 
+						hints
+							do: [:hint | strm cr;
+									nextPutAll: (hint
+											collect: [:i | quote at: i])].
+						strm cr; cr]) , 'Good luck!' translated)
+		embeddedInMorphicWindowLabeled: 'Crostic Hints' translated)
+		setWindowColor: (Color
+				r: 1.0
+				g: 0.6
+				b: 0.0);
+		 openInWorld: self world extent: 198 @ 154
