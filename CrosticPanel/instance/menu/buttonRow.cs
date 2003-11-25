9b8c2c5018ae25cd7e9c818ddfa946fd143ea8ad
asm <unknown>
2003-11-25 22:27:00 +0000

    no message

--- CrosticPanel/instance/menu/buttonRow.cs
+++ CrosticPanel/instance/menu/buttonRow.cs
@@ -1,21 +1,17 @@
 buttonRow
-
 	| row aButton |
-	row _ AlignmentMorph newRow
-		color: self color;
-		hResizing: #shrinkWrap;
-		vResizing: #shrinkWrap.
-	aButton _ SimpleButtonMorph new target: self.
-	aButton color: Color transparent; borderWidth: 1; borderColor: Color black.
-
-	#('show help' 'show errors' 'show hints' 'clear' 'open...')
-	with: 
-	#(showHelpWindow showErrors showHintsWindow clearTyping openFile)
-	do: [:label :selector |
-		aButton _ aButton fullCopy.
-		aButton actionSelector: selector.
-		aButton label: label.
-		row addMorphBack: aButton.
-		row addTransparentSpacerOfSize: (3 @ 0)].
-
+	row := AlignmentMorph newRow color: self color;
+				 hResizing: #shrinkWrap;
+				 vResizing: #shrinkWrap.
+	#('show help' 'show errors' 'show hints' 'clear' 'open...' )
+		with: #(#showHelpWindow #showErrors #showHintsWindow #clearTyping #openFile )
+		do: [:label :selector | 
+			aButton := SimpleButtonMorph new target: self.
+			aButton color: Color transparent;
+				 borderWidth: 1;
+				 borderColor: Color black.
+			aButton actionSelector: selector.
+			aButton label: label translated.
+			row addMorphBack: aButton.
+			row addTransparentSpacerOfSize: 3 @ 0].
 	^ row
