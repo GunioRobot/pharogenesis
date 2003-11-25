f2e0cc8a76ff4d05f4318c64ea3b0a76677fad12
asm <unknown>
2003-11-25 22:27:00 +0000

    no message

--- CrosticPanel/instance/menu/addMenuItemsTo-3A-hand-3A-.cs
+++ CrosticPanel/instance/menu/addMenuItemsTo-3A-hand-3A-.cs
@@ -1,7 +1,21 @@
-addMenuItemsTo: aMenu hand: aHandMorph
-
-	aMenu add: 'show crostic help' target: self action: #showHelpWindow.
-	aMenu add: 'show crostic hints' target: self action: #showHintsWindow.
-	aMenu add: 'show crostic errors' target: self action: #showErrors.
-	aMenu add: 'clear crostic typing' target: self action: #clearTyping.
-	aMenu add: 'open crostic file...' target: self action: #openFile.
+addMenuItemsTo: aMenu hand: aHandMorph 
+	aMenu
+		add: 'show crostic help' translated
+		target: self
+		action: #showHelpWindow.
+	aMenu
+		add: 'show crostic hints' translated
+		target: self
+		action: #showHintsWindow.
+	aMenu
+		add: 'show crostic errors' translated
+		target: self
+		action: #showErrors.
+	aMenu
+		add: 'clear crostic typing' translated
+		target: self
+		action: #clearTyping.
+	aMenu
+		add: 'open crostic file...' translated
+		target: self
+		action: #openFile
