ce8a86cb035059b17c2bd45e6e2c306ae16c4dfe
asm <unknown>
2003-11-25 22:23:00 +0000

    no message

--- CipherPanel/instance/menu/buttonRow.cs
+++ CipherPanel/instance/menu/buttonRow.cs
@@ -1,21 +1,17 @@
 buttonRow
-
 	| row aButton |
-	row _ AlignmentMorph newRow
-		color: self color;
-		hResizing: #shrinkWrap;
-		vResizing: #shrinkWrap.
-	aButton _ SimpleButtonMorph new target: self.
-	aButton color: Color transparent; borderWidth: 1; borderColor: Color black.
-
-	#('show help' 'show hints' 'clear typing' 'enter a new cipher')
-	with: 
-	#(showHelpWindow showHintsWindow clearTyping enterANewCipher)
-	do: [:label :selector |
-		aButton _ aButton fullCopy.
-		aButton actionSelector: selector.
-		aButton label: label.
-		row addMorphBack: aButton.
-		row addTransparentSpacerOfSize: (3 @ 0)].
-
+	row := AlignmentMorph newRow color: self color;
+				 hResizing: #shrinkWrap;
+				 vResizing: #shrinkWrap.
+	#('show help' 'show hints' 'clear typing' 'enter a new cipher' 'quote from Squeak' )
+		with: #(#showHelpWindow #showHintsWindow #clearTyping #enterANewCipher #squeakCipher )
+		do: [:label :selector | 
+			aButton := SimpleButtonMorph new target: self.
+			aButton color: Color transparent;
+				 borderWidth: 1;
+				 borderColor: Color black.
+			aButton actionSelector: selector.
+			aButton label: label translated.
+			row addMorphBack: aButton.
+			row addTransparentSpacerOfSize: 3 @ 0].
 	^ row
