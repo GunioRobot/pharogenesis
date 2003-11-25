7d55fc5215306b6e89572ff06ef1f9cabc470df1
asm <unknown>
2003-11-25 22:22:00 +0000

    no message

--- CipherPanel/instance/menu/showHintsWindow.cs
+++ CipherPanel/instance/menu/showHintsWindow.cs
@@ -1,15 +1,15 @@
 showHintsWindow
-
-	((StringHolder new contents: 
-'Most bodies of english text follow a general pattern of letter usage.  The following are the most common letters, in approximate order of frequency:
+	((StringHolder new contents: 'Most bodies of english text follow a general pattern of letter usage.  The following are the most common letters, in approximate order of frequency:
 	E  T  A  O  N  I  R  S  H
 The following are the most common digraphs:
 	EN  ER  RE  NT  TH  ON  IN
 
-The message you are trying to decode has the following specific statistics:'
-	, self cipherStats , '
+The message you are trying to decode has the following specific statistics:' translated , self cipherStats , '
 
-Good luck!')
-		embeddedInMorphicWindowLabeled: 'Some Useful Statistics')
-			setWindowColor: (Color r: 1.0 g: 0.6 b: 0.0);
-			openInWorld: self world extent: 318@326
+Good luck!' translated)
+		embeddedInMorphicWindowLabeled: 'Some Useful Statistics' translated)
+		setWindowColor: (Color
+				r: 1.0
+				g: 0.6
+				b: 0.0);
+		 openInWorld: self world extent: 318 @ 326
