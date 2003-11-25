ec00131184ad9111ca3d03a587a4c2f53eb0f5ac
asm <unknown>
2003-11-25 22:21:00 +0000

    no message

--- CipherPanel/instance/initialization/encodedQuote-3A-.cs
+++ CipherPanel/instance/initialization/encodedQuote-3A-.cs
@@ -1,25 +1,38 @@
-encodedQuote: aString
-	"World addMorph: CipherPanel test"
-
+encodedQuote: aString 
+	"World addMorph: CipherPanel new"
 	| morph prev |
-	haveTypedHere _ false.
-	quote _ aString asUppercase.
-	prev _ nil.
-	originalMorphs _ quote asArray collectWithIndex:
-		[:c :i | WordGameLetterMorph new plain indexInQuote: i id1: nil;
+	aString isEmpty
+		ifTrue: [^ self].
+	(letterMorphs isNil
+			or: [self isClean])
+		ifFalse: [(self confirm: 'Are you sure you want to discard all typing?' translated)
+				ifFalse: [^ self]].
+	haveTypedHere := false.
+	quote := aString asUppercase.
+	prev := nil.
+	originalMorphs := quote asArray
+				collectWithIndex: [:c :i | WordGameLetterMorph new plain indexInQuote: i id1: nil;
+						
 						setLetter: (quote at: i)].
-	letterMorphs _ OrderedCollection new.
-	decodingMorphs _ quote asArray collectWithIndex:
-		[:c :i | (quote at: i) isLetter
-		ifTrue:
-			[morph _ WordGameLetterMorph new underlined indexInQuote: i id1: nil.
-			morph on: #mouseDown send: #mouseDownEvent:letterMorph: to: self.
-			morph on: #keyStroke send: #keyStrokeEvent:letterMorph: to: self.
-			letterMorphs addLast: morph.
-			morph predecessor: prev.
-			prev ifNotNil: [prev successor: morph].
-			prev _ morph]
-		ifFalse:
-			[WordGameLetterMorph new plain indexInQuote: i id1: nil; setLetter: (quote at: i)]].
+	letterMorphs := OrderedCollection new.
+	decodingMorphs := quote asArray
+				collectWithIndex: [:c :i | (quote at: i) isLetter
+						ifTrue: [morph := WordGameLetterMorph new underlined indexInQuote: i id1: nil.
+							morph
+								on: #mouseDown
+								send: #mouseDownEvent:letterMorph:
+								to: self.
+							morph
+								on: #keyStroke
+								send: #keyStrokeEvent:letterMorph:
+								to: self.
+							letterMorphs addLast: morph.
+							morph predecessor: prev.
+							prev
+								ifNotNil: [prev successor: morph].
+							prev := morph]
+						ifFalse: [WordGameLetterMorph new plain indexInQuote: i id1: nil;
+								
+								setLetter: (quote at: i)]].
 	self color: originalMorphs first color.
-	self extent: 500@500
+	self extent: 500 @ 500
