1d1dca34dc6a346048c0eca16625e16f5708adb8
asm <unknown>
2003-11-25 22:26:00 +0000

    no message

--- CrosticPanel/instance/menu/openFile.cs
+++ CrosticPanel/instance/menu/openFile.cs
@@ -1,14 +1,15 @@
 openFile
-
 	| stdFileMenuResult crostic file |
-	stdFileMenuResult _ ((StandardFileMenu new) pattern: '*.crostic'; 
-		oldFileFrom: FileDirectory default ) 
-			startUpWithCaption: 'Select a Crostic File...'.
-	stdFileMenuResult ifNil: [^ nil].
-	file _ stdFileMenuResult directory readOnlyFileNamed: stdFileMenuResult name.
-	crostic _ CrosticPanel newFromFile: file.  file close.
-	(self isClean or: [self confirm: 'Is it OK to discard this crostic?'])
-		ifTrue: [self world addMorphFront: (crostic position: self position).
-				self delete]
-		ifFalse: [self world addMorphFront: crostic].
-
+	stdFileMenuResult := (StandardFileMenu new pattern: '*.crostic';
+				 oldFileFrom: FileDirectory default) startUpWithCaption: 'Select a Crostic File...' translated.
+	stdFileMenuResult
+		ifNil: [^ nil].
+	file := stdFileMenuResult directory readOnlyFileNamed: stdFileMenuResult name.
+	crostic := CrosticPanel newFromFile: file.
+	file close.
+	(self isClean
+			or: [self confirm: 'Is it OK to discard this crostic?' translated])
+		ifTrue: [self world
+				addMorphFront: (crostic position: self position).
+			self delete]
+		ifFalse: [self world addMorphFront: crostic]
