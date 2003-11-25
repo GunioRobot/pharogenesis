05beb504d1230cee9f12b792e9024890a2a17a25
asm <unknown>
2003-11-25 22:23:00 +0000

    no message

--- CipherPanel/instance/menu/showHelpWindow.cs
+++ CipherPanel/instance/menu/showHelpWindow.cs
@@ -1,11 +1,12 @@
 showHelpWindow
-
-	((StringHolder new contents: 
-'The Cipher Panel displays an encrypted message.  The encryption is a simple substitution code;  each letter of the alphabet has been changed to a different one.
+	((StringHolder new contents: 'The Cipher Panel displays an encrypted message.  The encryption is a simple substitution code;  each letter of the alphabet has been changed to a different one.
 
 You can solve the cipher by clicking above any letter in the message, and typing the letter you think it should be.  The Cipher Panel automatically makes the same substitution anywhere else that letter occurs in the encoded message.
 
-If you are having trouble, you can use the command menu to ''show cipher hints''.  That will display how many of each letter occurs, which is often a help in solving ciphers.')
-		embeddedInMorphicWindowLabeled: 'About the Cipher Panel')
-			setWindowColor: (Color r: 1.0 g: 0.6 b: 0.0);
-			openInWorld: self world extent: 389@209
+If you are having trouble, you can use the command menu to ''show cipher hints''.  That will display how many of each letter occurs, which is often a help in solving ciphers.' translated)
+		embeddedInMorphicWindowLabeled: 'About the Cipher Panel' translated)
+		setWindowColor: (Color
+				r: 1.0
+				g: 0.6
+				b: 0.0);
+		 openInWorld: self world extent: 389 @ 209
