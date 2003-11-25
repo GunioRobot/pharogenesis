31ee75813dc88dad8b07d98de5c1e7099cd9d031
asm <unknown>
2003-11-25 22:45:00 +0000

    no message

--- Tetris/instance/initialization/makeMovementControls.cs
+++ Tetris/instance/initialization/makeMovementControls.cs
@@ -1,33 +1,31 @@
 makeMovementControls
-
-	^self rowForButtons
-		addMorph:
-			(self
+	^ self rowForButtons
+		addMorph: (self
 				buildButtonTarget: board
 				label: '->'
 				selector: #moveRight
-				help: 'move to the right');
-		addMorph:
-			(self
+				help: 'move to the right' translated);
+		
+		addMorph: (self
 				buildButtonTarget: board
 				label: ' ) '
 				selector: #rotateClockWise
-				help: 'rotate clockwise');
-		addMorph:
-			(self
+				help: 'rotate clockwise' translated);
+		
+		addMorph: (self
 				buildButtonTarget: board
 				label: ' | '
 				selector: #dropAllTheWay
-				help: 'drop');
-		addMorph:
-			(self
+				help: 'drop' translated);
+		
+		addMorph: (self
 				buildButtonTarget: board
 				label: ' ( '
 				selector: #rotateAntiClockWise
-				help: 'rotate anticlockwise');
-		addMorph:
-			(self
+				help: 'rotate anticlockwise' translated);
+		
+		addMorph: (self
 				buildButtonTarget: board
 				label: '<-'
 				selector: #moveLeft
-				help: 'move to the left')
+				help: 'move to the left' translated)
