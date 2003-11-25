7b02cb25ea5d3e0d94dd50bdbde5614ebaeb4d44
asm <unknown>
2003-11-25 22:31:00 +0000

    no message

---
+++ Mines/instance/access/helpString.cs
@@ -0,0 +1,9 @@
+helpString
+	^ 'Mines is a quick and dirty knock-off of the Minesweeper game found on Windows. I used this to teach myself Squeak. I liberally borrowed from the <SameGame> example, so the code should look pretty familiar, though like any project it has rapidly ...morphed... to reflect my own idiosyncracies. Note especially the lack of any idiomatic structure to the code - I simply haven''t learned them yet.
+
+Mines is a very simple, yet extremely frustrating, game to play. The rules are just this: there are 99 mines laid down on the board. Find them without ""finding"" them. Your first tile is free - click anywhere. The tiles will tell you how many mines are right next to it, including the diagonals. If you uncover the number ''2'', you know that there are two mines hidden in the adjacent tiles. If you think you have found a mine, you can flag it by either ''shift'' clicking, or click with the ''yellow'' mouse button. Once you have flagged all of the mines adjacent to a numbered tile, you can click on the tile again to uncover the rest. Of course, you could be wrong about those too... 
+
+You win once you have uncovered all of the tiles that do not contain mines. Good luck...
+
+David A. Smith
+dastrs@bellsouth.net' translated
