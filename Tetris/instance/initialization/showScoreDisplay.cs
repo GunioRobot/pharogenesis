0d7dba84ba96f4d639ba120778b7f283adc8bbdd
asm <unknown>
2003-11-25 22:45:00 +0000

    no message

--- Tetris/instance/initialization/showScoreDisplay.cs
+++ Tetris/instance/initialization/showScoreDisplay.cs
@@ -1,6 +1,5 @@
 showScoreDisplay
-
-	^ self rowForButtons
-		hResizing: #shrinkWrap;
-		addMorph: (self wrapPanel: ((scoreDisplay _ LedMorph new) digits: 5; extent: (4*10@15)) 
-						label: 'Score:')
+	^ self rowForButtons hResizing: #shrinkWrap;
+		
+		addMorph: (self wrapPanel: ((scoreDisplay := LedMorph new) digits: 5;
+					 extent: 4 * 10 @ 15) label: 'Score:' translated)
