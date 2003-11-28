ec22cb725be87a83fcc683055d8e308450dabf74
tpr <unknown>
2003-11-28 11:44:00 +0000

    no message

--- FileList/instance/volume list and pattern/deleteDirectory.cs
+++ FileList/instance/volume list and pattern/deleteDirectory.cs
@@ -1,9 +1,9 @@
 deleteDirectory
 	"Remove the currently selected directory"
-	| localDir |
+	| localDirName |
 	directory entries size = 0 ifFalse:[^self inform:'Directory must be empty'].
-	localDir _ directory pathParts last.
-	(self confirm: 'Really delete ' , localDir printString , '?') ifFalse: [^ self].
+	localDirName _ directory localName.
+	(self confirm: 'Really delete ' , localDirName , '?') ifFalse: [^ self].
 	self volumeListIndex: self volumeListIndex-1.
-	directory deleteDirectory: localDir.
+	directory deleteDirectory: localDirName.
 	self updateFileList.
