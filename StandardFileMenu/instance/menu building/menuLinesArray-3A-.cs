8a5c56a42fbb5c86425c25f1aa5c4c8dcc3c038e
tpr <unknown>
2003-11-28 15:12:00 +0000

    no message

--- StandardFileMenu/instance/menu building/menuLinesArray-3A-.cs
+++ StandardFileMenu/instance/menu building/menuLinesArray-3A-.cs
@@ -1,13 +1,13 @@
 menuLinesArray: aDirectory
 "Answer a menu lines object corresponding to aDirectory"
 
-	| typeCount nameCnt |
+	| typeCount nameCnt dirDepth|
 	typeCount _ canTypeFileName 
 		ifTrue: [1] 
 		ifFalse: [0].
 	nameCnt _ aDirectory directoryNames size.
+	dirDepth _ aDirectory pathParts size.
 	^Array streamContents: [:s |
 		canTypeFileName ifTrue: [s nextPut: 1].
-		s nextPut: aDirectory pathParts size + typeCount + 1.
-		s nextPut: aDirectory pathParts size + 
-					nameCnt + typeCount + 1]
+		s nextPut: dirDepth + typeCount + 1.
+		s nextPut: dirDepth + nameCnt + typeCount + 1]
