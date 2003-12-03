ecb8ee56ec6d9fbb7419bd74d924c0a8fb8a5156
tk <unknown>
2003-12-03 21:04:00 +0000

    no message

--- RemoteFileStream/instance/as yet unclassified/close.cs
+++ RemoteFileStream/instance/as yet unclassified/close.cs
@@ -2,4 +2,4 @@ close
 	"Write if we have data to write.  FTP files are always binary to preserve the data exactly.  The binary/text (ascii) flag is just for tell how the bits are delivered from a read."
 
 	remoteFile writable ifTrue: [
-			remoteFile putFile: self named: remoteFile fileName]
+			remoteFile putFile: (self as: RWBinaryOrTextStream) reset named: remoteFile fileName]
