c63a322a898fcc52265b2e73556584b09134b864
JMM <unknown>
2003-12-03 23:54:00 +0000

    no message

--- Interpreter/instance/message sending/activateNewMethod.cs
+++ Interpreter/instance/message sending/activateNewMethod.cs
@@ -1,5 +1,6 @@
 activateNewMethod
-	| newContext methodHeader initialIP tempCount nilOop |
+	| newContext methodHeader initialIP tempCount nilOop where |
+
 	methodHeader _ self headerOf: newMethod.
 	newContext _ self allocateOrRecycleContext: (methodHeader bitAnd: LargeContextBit).
 
@@ -9,25 +10,20 @@ activateNewMethod
 	"Assume: newContext will be recorded as a root if necessary by the
 	 call to newActiveContext: below, so we can use unchecked stores."
 
-	self storePointerUnchecked: SenderIndex	ofObject: newContext
-		withValue: activeContext.
-	self storeWord: InstructionPointerIndex	ofObject: newContext
-		withValue: (self integerObjectOf: initialIP).
-	self storeWord: StackPointerIndex			ofObject: newContext
-		withValue: (self integerObjectOf: tempCount).
-	self storePointerUnchecked: MethodIndex ofObject: newContext
-		withValue: newMethod.
+	where _  newContext  + BaseHeaderSize.
+	self longAt: where + (SenderIndex << 2) put: activeContext.
+	self longAt: where + (InstructionPointerIndex << 2) put: (self integerObjectOf: initialIP).
+	self longAt: where + (StackPointerIndex << 2) put: (self integerObjectOf: tempCount).
+	self longAt: where + (MethodIndex << 2) put: newMethod.
 
 	"Copy the reciever and arguments..."
 	0 to: argumentCount do:
-		[:i | self storePointerUnchecked: ReceiverIndex+i ofObject: newContext
-			withValue: (self stackValue: argumentCount-i)].
+		[:i | self longAt: where + ((ReceiverIndex+i) << 2) put: (self stackValue: argumentCount-i)].
 
 	"clear remaining temps to nil in case it has been recycled"
 	nilOop _ nilObj.
-	argumentCount+1 to: tempCount do:
-		[:i | self storePointerUnchecked: ReceiverIndex+i ofObject: newContext
-			withValue: nilOop].
+	argumentCount+1+ReceiverIndex to: tempCount+ReceiverIndex do:
+		[:i | self longAt: where + (i << 2) put: nilOop].
 
 	self pop: argumentCount + 1.
 	reclaimableContextCount _ reclaimableContextCount + 1.
