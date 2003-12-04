b48d4654bc935f83ad80f4d193662fa1ebb5ae72
BG <unknown>
2003-12-04 14:22:00 +0000

    no message

--- FormMenuView/instance/private/makeButton-3A-.cs
+++ FormMenuView/instance/private/makeButton-3A-.cs
@@ -1,7 +1,8 @@
 makeButton: index
 
 	| buttonCache button |
-	buttonCache _ FormButtons at: index.
+	buttonCache _ (FormButtons at: index) shallowCopy.
+	buttonCache form: (FormButtons at: index) form copy.
 	button _ Button newOff.
 	button onAction: [model changeTool: buttonCache value].
 	self makeViews: buttonCache for: button.
