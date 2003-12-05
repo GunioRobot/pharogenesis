93487688aed31710e980b50e6f1cbdf02da83d3b
BG <unknown>
2003-12-05 22:31:00 +0000

    no message

--- Morph/instance/submorphs-2D-add-2F-remove/delete.cs
+++ Morph/instance/submorphs-2D-add-2F-remove/delete.cs
@@ -1,13 +1,19 @@
 delete
-	"Remove the receiver as a submorph of its owner and make its new owner be nil."
+	"Remove the receiver as a submorph of its owner and make its 
+	new owner be nil."
+
 	| aWorld |
-	owner ifNotNil:
-		[(extension == nil or: [self player == nil])
-		ifTrue: [owner privateRemoveMorph: self.
-				owner _ nil]
-		ifFalse: ["Player must be notified"
-				aWorld _ self world ifNil: [World].	"or some proper of getting the World"
-				owner privateRemoveMorph: self.
-				owner _ nil.
-				self player noteDeletionOf: self fromWorld: aWorld]
-		].
+	aWorld := self world ifNil: [World].
+	"Terminate genie recognition focus"
+	"I encountered a case where the hand was nil, so I put in a little 
+	protection - raa "
+	" This happens when we are in an MVC project and open
+	  a morphic window. - BG "
+	aWorld ifNotNil:
+	  [self disableSubmorphFocusForHand: self activeHand.
+	  self activeHand releaseKeyboardFocus: self;
+		  releaseMouseFocus: self.].
+	owner ifNotNil:[ self privateDelete.
+		self player ifNotNilDo: [ :player |
+			"Player must be notified"
+			player noteDeletionOf: self fromWorld: aWorld]].
