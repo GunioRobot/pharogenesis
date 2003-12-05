37137d8bfe5f18a0ebcaba8a6cbd7d1deef08421
BG <unknown>
2003-12-05 13:21:00 +0000

    no message

--- BitEditor/instance/menu messages/setColor-3A-.cs
+++ BitEditor/instance/menu messages/setColor-3A-.cs
@@ -1,7 +1,8 @@
-setColor: aColor
-	"Set the color that the next edited dots of the model to be the argument, 
-	aSymbol. aSymbol can be any color changing message understood by a 
+setColor: aColor 
+	"Set the color that the next edited dots of the model to be the argument,  
+	aSymbol. aSymbol can be any color changing message understood by a  
 	Form, such as white or black."
 
-	color _ aColor pixelValueForDepth: model depth.
+	color _ aColor pixelValueForDepth: Display depth.
 	squareForm fillColor: aColor.
+	self changed: #getCurrentColor
