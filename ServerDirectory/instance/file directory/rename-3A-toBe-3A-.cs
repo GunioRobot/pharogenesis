6bf459936a2e34e4e04a72a678cd0aa5729e73c4
mir <unknown>
2003-12-08 14:17:00 +0000

    no message

--- ServerDirectory/instance/file directory/rename-3A-toBe-3A-.cs
+++ ServerDirectory/instance/file directory/rename-3A-toBe-3A-.cs
@@ -1,6 +1,6 @@
 rename: fullName toBe: newName
 	"Rename a remote file.  fullName is just be a fileName, or can be directory path that includes name of the server.  newName is just a fileName"
-	| file so rr |
+	| file |
 
 	file _ self asServerFileNamed: fullName.
 	file isTypeFile ifTrue: [
@@ -8,13 +8,7 @@ rename: fullName toBe: newName
 			rename: file fileName toBe: newName
 	].
 	
-	so _ file openNoDataFTP.
-	so class == String ifTrue: ["error, was reported" ^ so].
-	so sendCommand: 'RNFR ', file fileName.
-	(rr _ so responseOK) == true ifFalse: [socket _ nil.  ^ rr].	""
-	so sendCommand: 'RNTO ', newName.
-	(rr _ so responseOK) == true ifFalse: [socket _ nil.  ^ rr].	""
-	socket ifNil: [
-		so sendCommand: 'QUIT'.
-		(rr _ so responseOK) == true ifFalse: [^ rr].	"221"
-		so destroy].	"Always OK to destroy"
+	client := self openFTPClient.
+	[client renameFileNamed: fullName to: newName]
+		ensure: [self quit].
+	
