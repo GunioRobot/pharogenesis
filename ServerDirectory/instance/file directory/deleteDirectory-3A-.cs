ad4a2bfb85f01e55f7033ec9b90905f5c6dc8599
mir <unknown>
2003-12-08 14:17:00 +0000

    no message

--- ServerDirectory/instance/file directory/deleteDirectory-3A-.cs
+++ ServerDirectory/instance/file directory/deleteDirectory-3A-.cs
@@ -1,17 +1,11 @@
 deleteDirectory: localName
 	"Delete the sub directory within the current one.  Call needs to ask user to confirm."
 
-	| so rr |
 	self isTypeFile ifTrue: [
 		^FileDirectory deleteFileNamed: localName
 	].
 		"Is this the right command???"
 
-	so _ self openNoDataFTP.
-	so class == String ifTrue: ["error, was reported" ^ so].
-	so sendCommand: 'RMD ', localName.
-	(rr _ so responseOK) == true ifFalse: [socket _ nil.  ^ rr].	""
-	socket ifNil: [
-		so sendCommand: 'QUIT'.
-		(rr _ so responseOK) == true ifFalse: [^ rr].	"221"
-		so destroy].	"Always OK to destroy"
+	client := self openFTPClient.
+	[client deleteDirectory: localName]
+		ensure: [self quit].
