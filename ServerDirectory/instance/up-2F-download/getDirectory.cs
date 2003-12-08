3ea73d66057d265c4bd9688c435687efb0959e07
mir <unknown>
2003-12-08 14:17:00 +0000

    no message

---
+++ ServerDirectory/instance/up-2F-download/getDirectory.cs
@@ -0,0 +1,8 @@
+getDirectory
+	"Return a stream with a listing of the current server directory.  (Later -- Use a proxy server if one has been registered.)"
+
+	| listing |
+	client := self openFTPClient.
+	[listing := client getDirectory]
+		ensure: [self quit].
+	^ReadStream on: listing
