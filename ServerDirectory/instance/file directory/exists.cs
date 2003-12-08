68a980cd1521ffee404c6d6eaa93200d21e62773
mir <unknown>
2003-12-08 14:17:00 +0000

    no message

--- ServerDirectory/instance/file directory/exists.cs
+++ ServerDirectory/instance/file directory/exists.cs
@@ -1,9 +1,19 @@
 exists
-	"It is difficult to tell if a directory exists.  This is ugly, but it works for writable directories.  (tested only for file://)"
+	"It is difficult to tell if a directory exists.  This is ugly, but it works for writable directories.  http: will fall back on ftp for this"
 
-	| probe |
-	self entries size > 0 ifTrue: [^ true].
-	(probe _ self newFileNamed: 'withNoName23') ifNil: [^ false].
-	probe close.
-	probe directory deleteFileNamed: probe localName.
-	^ true
+	| probe success |
+	success _ false.
+	self isTypeFile ifTrue: [
+		self entries size > 0 ifTrue: [^ true].
+		probe _ self newFileNamed: 'withNoName23'. 
+		probe ifNotNil: [
+			probe close.
+			probe directory deleteFileNamed: probe localName].
+		^success _ probe notNil].
+	[client := self openFTPClient.
+	[client pwd]
+		ensure: [self quit].
+		success := true]
+		on: Error
+		do: [:ex | ].
+	^success
