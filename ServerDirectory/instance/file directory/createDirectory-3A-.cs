96cc5c09b50d4122c461a3262a6ce68af586a2af
mir <unknown>
2003-12-08 14:16:00 +0000

    no message

--- ServerDirectory/instance/file directory/createDirectory-3A-.cs
+++ ServerDirectory/instance/file directory/createDirectory-3A-.cs
@@ -1,16 +1,10 @@
 createDirectory: localName
 	"Create a new sub directory within the current one"
 
-	| so rr |
 	self isTypeFile ifTrue: [
 		^FileDirectory createDirectory: localName
 	].
 
-	so _ self openNoDataFTP.
-	so class == String ifTrue: ["error, was reported" ^ so].
-	so sendCommand: 'MKD ', localName.
-	(rr _ so responseOK) == true ifFalse: [socket _ nil.  ^ rr].	""
-	socket ifNil: [
-		so sendCommand: 'QUIT'.
-		(rr _ so responseOK) == true ifFalse: [^ rr].	"221"
-		so destroy].	"Always OK to destroy"
+	client := self openFTPClient.
+	[client makeDirectory: localName]
+		ensure: [self quit].
