e229c386ceb626587a7b1ffa60229680941002ee
mir <unknown>
2003-12-08 14:17:00 +0000

    no message

--- ServerDirectory/instance/file directory/deleteFileNamed-3A-.cs
+++ ServerDirectory/instance/file directory/deleteFileNamed-3A-.cs
@@ -1,17 +1,12 @@
 deleteFileNamed: fullName
 	"Detete a remote file.  fullName is directory path, and does include name of the server.  Or it can just be a fileName."
-	| file so rr |
+	| file |
 	file _ self asServerFileNamed: fullName.
 	file isTypeFile ifTrue: [
 		^ (FileDirectory forFileName: (file fileNameRelativeTo: self)) 
 			deleteFileNamed: file fileName
 	].
 	
-	so _ file openNoDataFTP.
-	so class == String ifTrue: ["error, was reported" ^ so].
-	so sendCommand: 'DELE ', file fileName.
-	(rr _ so responseOK) == true ifFalse: [socket _ nil.  ^ rr].	""
-	socket ifNil: [
-		so sendCommand: 'QUIT'.
-		(rr _ so responseOK) == true ifFalse: [^ rr].	"221"
-		so destroy].	"Always OK to destroy"
+	client := self openFTPClient.
+	[client deleteFileNamed: fullName]
+		ensure: [self quit].
