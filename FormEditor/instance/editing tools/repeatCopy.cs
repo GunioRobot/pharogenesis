58688baeeba0427302698f5b7824d0f6350af952
BG <unknown>
2003-12-10 15:59:00 +0000

    no message

--- FormEditor/instance/editing tools/repeatCopy.cs
+++ FormEditor/instance/editing tools/repeatCopy.cs
@@ -1,13 +1,18 @@
 repeatCopy
 	"As long as the red button is pressed, copy the source form onto the 
 	display screen."
-
+  | drawingWasChanged |
+	drawingWasChanged := false.
 	[sensor redButtonPressed]
 		whileTrue: 
 		[(BitBlt current destForm: Display sourceForm: form halftoneForm: color
-			combinationRule: (Display depth > 1 ifTrue: [Form paint]
+			combinationRule: (Display depth > 1 ifTrue: [mode ~= Form erase ifTrue: [Form paint] ifFalse: [mode]]
 										ifFalse: [mode])
 			destOrigin: self cursorPoint sourceOrigin: 0@0 extent: form extent
 			clipRect: view insetDisplayBox)
 			colorMap: (Bitmap with: 0 with: 16rFFFFFFFF);
-			copyBits]
+			copyBits.
+		  drawingWasChanged := true.
+		].
+	drawingWasChanged
+	  ifTrue: [hasUnsavedChanges contents: true.]
