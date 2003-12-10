0073b89cf61c59c5ed06117b72876f48225f23a2
BG <unknown>
2003-12-10 16:00:00 +0000

    no message

--- FormEditor/instance/editing tools/singleCopy.cs
+++ FormEditor/instance/editing tools/singleCopy.cs
@@ -2,11 +2,13 @@ singleCopy
 	"If the red button is clicked, copy the source form onto the display 
 	screen."
 
-	form
-		displayOn: Display
-		at: self cursorPoint
-		clippingBox: view insetDisplayBox
-		rule: (Display depth > 1 ifTrue: [Form paint]
-										ifFalse: [mode])
-		fillColor: color.
-	sensor waitNoButton
+   (BitBlt destForm: Display
+           sourceForm: form halftoneForm: color
+           combinationRule: (Display depth > 1 ifTrue: [mode ~= Form erase ifTrue: [Form paint] ifFalse: [mode]]
+                                                     ifFalse: [mode])
+           destOrigin: self cursorPoint sourceOrigin: 0@0 extent: form extent
+           clipRect: view insetDisplayBox)
+           colorMap: (Bitmap with: 0 with: 16rFFFFFFFF);
+	copyBits.
+	sensor waitNoButton.
+	hasUnsavedChanges contents: true.
