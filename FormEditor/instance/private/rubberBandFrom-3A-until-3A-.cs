2abad43de2e9d2f249feeefb86889ccb6c34a122
BG <unknown>
2003-12-10 16:47:00 +0000

    no message

--- FormEditor/instance/private/rubberBandFrom-3A-until-3A-.cs
+++ FormEditor/instance/private/rubberBandFrom-3A-until-3A-.cs
@@ -5,23 +5,23 @@ rubberBandFrom: startPoint until: aBlock
 	[aBlock value] whileFalse:
 		[(endPoint _ self cursorPoint) = previousEndPoint 
 			ifFalse:
-			[(Line from: startPoint to: previousEndPoint withForm: form)
+			[(Line from: startPoint to: previousEndPoint withForm: form) 
 				displayOn: Display
 				at: 0 @ 0
 				clippingBox: view insetDisplayBox
 				rule: Form reverse
-				fillColor: Color black.
+				fillColor: Color gray.
 			(Line from: startPoint to: endPoint withForm: form)
 				displayOn: Display
 				at: 0 @ 0
 				clippingBox: view insetDisplayBox
 				rule: Form reverse
-				fillColor: Color black.
+				fillColor: Color gray.
 			previousEndPoint  _ endPoint]].
 	(Line from: startPoint to: previousEndPoint withForm: form)
 		displayOn: Display
 		at: 0 @ 0
 		clippingBox: view insetDisplayBox
 		rule: Form reverse
-		fillColor: Color black.
+		fillColor: (Display depth = 1 ifTrue: [Color gray] ifFalse: [Color black]).
 	^endPoint
