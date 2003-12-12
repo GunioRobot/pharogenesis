4f5b8d22f09fbd6e44ac4da52ce3037ddbd2637a
md <unknown>
2003-12-12 17:03:00 +0000

    no message

--- Utilities/class/flaps/globalFlapTabOrDummy-3A-.cs
+++ Utilities/class/flaps/globalFlapTabOrDummy-3A-.cs
@@ -1,7 +1,5 @@
 globalFlapTabOrDummy: aName
-	| gg |
-	"Find a global flap tab by name.  May be either 'flap: Tools' or 'Tools'.  Be sure to supply a "
+	"Find a global flap tab by name.  May be either 'flap: Tools' or 'Tools'.  Retained in Utilities for possible benefit of existing image segments"
 
-	(gg _ self globalFlapTab: aName) ifNil: [
-		^ StringMorph contents: aName, ' can''t be found'].
-	^ gg
+	self deprecated: 'Use Flaps globalFlapTabOrDummy: aName'.
+	^ Flaps globalFlapTabOrDummy: aName
