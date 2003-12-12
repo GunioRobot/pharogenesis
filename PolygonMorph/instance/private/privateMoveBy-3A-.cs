d0f8afa34920035927bbbcfd2354f6981d982cf2
md <unknown>
2003-12-12 16:22:00 +0000

    no message

--- PolygonMorph/instance/private/privateMoveBy-3A-.cs
+++ PolygonMorph/instance/private/privateMoveBy-3A-.cs
@@ -3,5 +3,5 @@ privateMoveBy: delta
 	vertices _ vertices collect: [:p | p + delta].
 	self arrowForms do: [:f | f offset: f offset + delta].
 	curveState _ nil.  "Force recomputation"
-	(self valueOfProperty: #referencePosition) doIfNotNil:
+	(self valueOfProperty: #referencePosition) ifNotNilDo:
 		[:oldPos | self setProperty: #referencePosition toValue: oldPos + delta]
