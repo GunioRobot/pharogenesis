b1b64bf30b968c0930477d2c908fab5ae734b1b1
md <unknown>
2003-12-12 16:21:00 +0000

    no message

--- HaloMorph/instance/private/endInteraction.cs
+++ HaloMorph/instance/private/endInteraction.cs
@@ -2,16 +2,20 @@ endInteraction
 	"Clean up after a user interaction with the a halo control"
 
 	| m |
-	(target isInWorld not or: [owner == nil]) ifTrue: [^ self].
-	[target isFlexMorph and: [target hasNoScaleOrRotation]]
-		whileTrue:
-			[m _ target firstSubmorph.
+	self isMagicHalo: false.	"no longer"
+	self magicAlpha: 1.0.
+	(target isInWorld not or: [owner isNil]) ifTrue: [^self].
+	[target isFlexMorph and: [target hasNoScaleOrRotation]] whileTrue: 
+			[m := target firstSubmorph.
 			target removeFlexShell.
-			target _ m].
-	self isInWorld ifTrue:
-		["make sure handles show in front, even if flex shell added"
-		self comeToFront.
-		self addHandles].
-	(self valueOfProperty: #commandInProgress) doIfNotNil:
-		[:cmd | self rememberCommand: cmd.
-		self removeProperty: #commandInProgress] 
+			target := m].
+	self isInWorld 
+		ifTrue: 
+			["make sure handles show in front, even if flex shell added"
+
+			self comeToFront.
+			self addHandles].
+	(self valueOfProperty: #commandInProgress) ifNotNilDo: 
+			[:cmd | 
+			self rememberCommand: cmd.
+			self removeProperty: #commandInProgress]
