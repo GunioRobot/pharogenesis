5176536d38b79cb51e753319201f06c50a708b7c
md <unknown>
2003-12-12 16:22:00 +0000

    no message

--- Presenter/instance/viewer/updateViewer-3A-forceToShow-3A-.cs
+++ Presenter/instance/viewer/updateViewer-3A-forceToShow-3A-.cs
@@ -1,9 +1,13 @@
-updateViewer: aViewer forceToShow: aCategory
+updateViewer: aViewer forceToShow: aCategorySymbol
 	"Update the given viewer to make sure it is in step with various possible changes in the outside world, and when reshowing it be sure it shows the given category"
 
-	| aPlayer aPosition newViewer oldOwner wasSticky barHeight cats |
-	cats _ aViewer categoriesCurrentlyShowing asOrderedCollection.
-	aCategory ifNotNil: [(cats includes: aCategory) ifFalse: [cats addFirst: aCategory]].
+	| aPlayer aPosition newViewer oldOwner wasSticky barHeight itsVocabulary aCategory categoryInfo |
+	aCategory _ aCategorySymbol ifNotNil: [aViewer currentVocabulary translatedWordingFor: aCategorySymbol].
+	categoryInfo _ aViewer categoryMorphs  asOrderedCollection collect:
+		[:aMorph | aMorph categoryRestorationInfo].
+
+	itsVocabulary _ aViewer currentVocabulary.
+	aCategory ifNotNil: [(categoryInfo includes: aCategorySymbol) ifFalse: [categoryInfo addFirst: aCategorySymbol]].
 	aPlayer _ aViewer scriptedPlayer.
 	aPosition _ aViewer position.
 	wasSticky _ aViewer isSticky.
@@ -13,7 +17,12 @@ updateViewer: aViewer forceToShow: aCategory
 			[aViewer submorphs first submorphs first height]
 		ifFalse:
 			[0].
-	newViewer initializeFor: aPlayer barHeight: barHeight includeDismissButton: aViewer hasDismissButton showCategories: cats.
+	Preferences viewersInFlaps ifTrue:
+		[newViewer setProperty: #noInteriorThumbnail toValue: true].
+
+	newViewer rawVocabulary: itsVocabulary.
+	newViewer limitClass: aViewer limitClass.
+	newViewer initializeFor: aPlayer barHeight: barHeight includeDismissButton: aViewer hasDismissButton showCategories: categoryInfo.
 	wasSticky ifTrue: [newViewer beSticky].
 	oldOwner _ aViewer owner.
 	oldOwner ifNotNil:
@@ -24,5 +33,5 @@ updateViewer: aViewer forceToShow: aCategory
 	newViewer position: aPosition.
 	newViewer enforceTileColorPolicy.
 	newViewer visible: true.
-	newViewer world doIfNotNil: [:aWorld | aWorld startSteppingSubmorphsOf: newViewer].
+	newViewer world ifNotNilDo: [:aWorld | aWorld startSteppingSubmorphsOf: newViewer].
 	newViewer layoutChanged
