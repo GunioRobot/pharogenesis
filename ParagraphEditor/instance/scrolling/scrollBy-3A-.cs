c378514b69d066aa22158f5f872761eade32a0d0
BG <unknown>
2003-12-12 15:31:00 +0000

    no message

--- ParagraphEditor/instance/scrolling/scrollBy-3A-.cs
+++ ParagraphEditor/instance/scrolling/scrollBy-3A-.cs
@@ -1,3 +1,3 @@
 scrollBy: heightToMove
 	"Move the paragraph by heightToMove, and reset the text selection."
-	^ paragraph scrollBy: heightToMove withSelectionFrom: startBlock to: stopBlock
+	^ paragraph scrollBy: heightToMove withSelectionFrom: self pointBlock to: self markBlock
