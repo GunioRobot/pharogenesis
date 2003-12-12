d09cd14e77d28891f9350c84ecfffc72c78aa0a5
BG <unknown>
2003-12-12 15:51:00 +0000

    no message

--- FormEditor/instance/editing tools/line.cs
+++ FormEditor/instance/editing tools/line.cs
@@ -3,13 +3,23 @@ line
 	button; second point--release red button. The resultant line is displayed 
 	according to the current form and mode."
 
-	| firstPoint endPoint |
+	| firstPoint endPoint drawForm |
+	drawForm := form asFormOfDepth: Display depth.
+	
+	 Display depth > 1 
+	  ifTrue:
+	    [drawForm mapColor: Color white to: Color transparent; 
+	                 mapColor: Color black to: color].
+	           
 	firstPoint _ self cursorPoint.
 	endPoint _ self rubberBandFrom: firstPoint until: [sensor noButtonPressed].
-	(Line from: firstPoint to: endPoint withForm: form)
+	endPoint isNil ifTrue: [^self].
+	Display depth > 1 ifTrue: [self deleteRubberBandFrom: firstPoint to: endPoint.].
+	(Line from: firstPoint to: endPoint withForm: drawForm)
 		displayOn: Display
 		at: 0 @ 0
 		clippingBox: view insetDisplayBox
-		rule: (Display depth > 1 ifTrue: [Form paint]
-										ifFalse: [mode])
-		fillColor: color
+		rule: (Display depth > 1 ifTrue: [mode ~= Form erase ifTrue: [Form paint] ifFalse: [mode]]
+								ifFalse: [mode])
+		fillColor: (Display depth = 1 ifTrue: [color] ifFalse: [nil]).  
+		hasUnsavedChanges contents: true.
