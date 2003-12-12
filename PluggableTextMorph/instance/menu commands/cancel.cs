c6dade2a479ef2cd8fa447a5bbf4aea9ac834bf5
md <unknown>
2003-12-12 16:21:00 +0000

    no message

--- PluggableTextMorph/instance/menu commands/cancel.cs
+++ PluggableTextMorph/instance/menu commands/cancel.cs
@@ -2,5 +2,5 @@ cancel
 	self setText: self getText.
 	self setSelection: self getSelection.
 	getTextSelector == #annotation ifFalse:
-		[(model dependents detect: [:dep | (dep isKindOf: PluggableTextMorph) and: [dep getTextSelector == #annotation]] ifNone: [nil]) doIfNotNil:
+		[(model dependents detect: [:dep | (dep isKindOf: PluggableTextMorph) and: [dep getTextSelector == #annotation]] ifNone: [nil]) ifNotNilDo:
 			[:aPane | model changed: #annotation]]
