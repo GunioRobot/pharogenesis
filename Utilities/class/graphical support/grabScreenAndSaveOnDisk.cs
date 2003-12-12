b739d1f72fd0155fcf2b1cb609e6f2d2aaaaed70
md <unknown>
2003-12-12 17:03:00 +0000

    no message

--- Utilities/class/graphical support/grabScreenAndSaveOnDisk.cs
+++ Utilities/class/graphical support/grabScreenAndSaveOnDisk.cs
@@ -1,7 +1,8 @@
 grabScreenAndSaveOnDisk  "Utilities grabScreenAndSaveOnDisk"
 	| form fileName |
+	self deprecated: 'Use GIFReaderWriter grabScreenAndSaveOnDisk'.
 	form _ Form fromUser.
-	form bits size = 0 ifTrue: [^ self beep].
+	form bits size = 0 ifTrue: [^ Beeper beep].
 	fileName _ FileDirectory default nextNameFor: 'Squeak' extension: 'gif'.
 	Utilities informUser: 'Writing ' , fileName
 		during: [GIFReadWriter putForm: form onFileNamed: fileName].
