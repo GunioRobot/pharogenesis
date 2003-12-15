99ecf70376e059f4650c7547749d42c5a787187b
dgd <unknown>
2003-12-15 10:36:00 +0000

    no message

--- SystemWindow/instance/resize-2F-collapse/collapseOrExpand.cs
+++ SystemWindow/instance/resize-2F-collapse/collapseOrExpand.cs
@@ -1,17 +1,21 @@
 collapseOrExpand
+	"Collapse or expand the window, depending on existing state"
+	| cf |
 	isCollapsed
 		ifTrue: 
 			["Expand -- restore panes to morphics structure"
 			isCollapsed _ false.
-			collapsedFrame _ self getBoundsWithFlex.
-			"First save latest collapsedFrame"
+			self activate.  "Bring to frint first"
+			Preferences collapseWindowsInPlace
+				ifTrue: 
+					[fullFrame := fullFrame align: fullFrame topLeft with: self getBoundsWithFlex topLeft]
+				ifFalse:
+					[collapsedFrame _ self getBoundsWithFlex].
+			collapseBox ifNotNil: [collapseBox setBalloonText: 'collapse this window' translated].
 			self setBoundsWithFlex: fullFrame.
-			paneMorphs
-				reverseDo: 
-					[:m | 
-					self addMorph: m.
-					self world startSteppingSubmorphsOf: m].
-			self activate "-- mainly for findWindow"]
+			paneMorphs reverseDo: 
+					[:m |  self addMorph: m unlock.
+					self world startSteppingSubmorphsOf: m]]
 		ifFalse: 
 			["Collapse -- remove panes from morphics structure"
 			isCollapsed _ true.
@@ -19,7 +23,10 @@ collapseOrExpand
 			"First save latest fullFrame"
 			paneMorphs do: [:m | m delete; releaseCachedState].
 			model modelSleep.
-			collapsedFrame ifNil:
-				[collapsedFrame _ RealEstateAgent assignCollapseFrameFor: self].
-			self setBoundsWithFlex: collapsedFrame].
+			cf := self getCollapsedFrame.
+			(collapsedFrame isNil and: [Preferences collapseWindowsInPlace not]) ifTrue:
+				[collapsedFrame _ cf].
+			self setBoundsWithFlex: cf.
+			collapseBox ifNotNil: [collapseBox setBalloonText: 'expand this window' translated].
+			expandBox ifNotNil: [expandBox setBalloonText: 'expand to full screen' translated]].
 	self layoutChanged
