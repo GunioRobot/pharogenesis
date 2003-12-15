b957077eea568dc7bb61bf2c71df8859cb02341e
tpr <unknown>
2003-12-15 12:02:00 +0000

    no message

--- FileDirectory/class/system start up/openSources-3A-andChanges-3A-forImage-3A-.cs
+++ FileDirectory/class/system start up/openSources-3A-andChanges-3A-forImage-3A-.cs
@@ -1,12 +1,8 @@
 openSources: sourcesName andChanges: changesName forImage: imageName 
-	"Initialize the default directory to the image directory and open the  
-	sources and changes files, if possible. Look for the changes file in  
-	image directory. Look for the system sources (or an alias to it) first in  
-	the VM directory, then in the image directory. Open the changes and  
-	sources files and install them in SourceFiles."
+	"Open the changes and sources files and install them in SourceFiles. Inform the user of problems regarding write permissions or CR/CRLF mixups."
 	"Note: SourcesName and imageName are full paths; changesName is a  
 	local name."
-	| sources changes sourceAlias msg wmsg localSourcesName |
+	| sources changes msg wmsg |
 	msg _ 'Squeak cannot locate &fileRef.
 
 Please check that the file is named properly and is in the
@@ -18,47 +14,30 @@ in the startup window, ''How Squeak Finds Source Code''.'.
 Please check that you have write permission for this file.
 
 You won''t be able to save this image correctly until you fix this.'.
-	self setDefaultDirectoryFrom: imageName.
-	sources _ changes _ nil.
-	"look for the sources file or an alias to it in the VM's directory"
-	(DefaultDirectory fileExists: sourcesName)
-		ifTrue: [sources _ DefaultDirectory readOnlyFileNamed: sourcesName]
-		ifFalse: 
-			["look for an un-renamed Macintosh alias to the sources file"
-			sourceAlias _ sourcesName , ' alias'.
-			(DefaultDirectory fileExists: sourceAlias)
-				ifTrue: [sources _ DefaultDirectory readOnlyFileNamed: sourceAlias]].
-	sources
-		ifNil: 
-			["look for the sources file or an alias to it in the image directory"
-			localSourcesName _ FileDirectory localNameFor: sourcesName.
-			(DefaultDirectory fileExists: localSourcesName)
-				ifTrue: [sources _ DefaultDirectory readOnlyFileNamed: localSourcesName]
-				ifFalse: 
-					["look for an un-renamed Macintosh alias to the sources  
-					file"
-					sourceAlias _ localSourcesName , ' alias'.
-					(DefaultDirectory fileExists: sourceAlias)
-						ifTrue: [sources _ DefaultDirectory readOnlyFileNamed: sourceAlias]]].
-	(DefaultDirectory fileExists: changesName)
-		ifTrue: 
-			[changes _ DefaultDirectory oldFileNamed: changesName.
-			changes isNil
-				ifTrue: 
-					[PopUpMenu notify: (wmsg copyReplaceAll: '&fileRef' with: 'the changes file named ' , changesName).
-					changes _ DefaultDirectory readOnlyFileNamed: changesName]].
+
+	sources _ self openSources: sourcesName forImage: imageName.
+	changes _ self openChanges: changesName forImage: imageName.
+
 	((sources == nil or: [sources atEnd])
-		and: [Preferences valueOfFlag: #warnIfNoSourcesFile])
-		ifTrue: 
-			[PopUpMenu notify: (msg copyReplaceAll: '&fileRef' with: 'the sources file named ' , sourcesName).
-			Smalltalk platformName = 'Mac OS'
-				ifTrue: [PopUpMenu notify: 'Make sure the sources file is not an Alias.']].
-	(changes == nil and: [Preferences valueOfFlag: #warnIfNoChangesFile])
-		ifTrue: [PopUpMenu notify: (msg copyReplaceAll: '&fileRef' with: 'the changes file named ' , changesName)].
-	(Preferences valueOfFlag: #warnIfNoChangesFile) ifTrue: [
-		((changes next: 200) includesSubString: String crlf) ifTrue: [
-			PopUpMenu notify: 'The changes file named ' , changesName, '
+			and: [Preferences valueOfFlag: #warnIfNoSourcesFile])
+		ifTrue: [SmalltalkImage current platformName = 'Mac OS'
+				ifTrue: [msg _ msg , '
+Make sure the sources file is not an Alias.'].
+self inform: (msg copyReplaceAll: '&fileRef' with: 'the sources file named ' , sourcesName)].
+
+	(changes == nil
+			and: [Preferences valueOfFlag: #warnIfNoChangesFile])
+		ifTrue: [self inform: (msg copyReplaceAll: '&fileRef' with: 'the changes file named ' , changesName)].
+
+	((Preferences valueOfFlag: #warnIfNoChangesFile) and: [changes notNil])
+		ifTrue: [changes isReadOnly
+				ifTrue: [self inform: (wmsg copyReplaceAll: '&fileRef' with: 'the changes file named ' , changesName)].
+
+			((changes next: 200)
+					includesSubString: String crlf)
+				ifTrue: [self inform: 'The changes file named ' , changesName , '
 has been injured by an unpacking utility.  Crs were changed to CrLfs.
 Please set the preferences in your decompressing program to 
 "do not convert text files" and unpack the system again.']].
+
 	SourceFiles _ Array with: sources with: changes
