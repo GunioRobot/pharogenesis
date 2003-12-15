15a27232dbb70462eb3caa3f00235ae7fddf3e05
tpr <unknown>
2003-12-15 16:01:00 +0000

    no message

---
+++ SmalltalkImage/instance/sources, changes log/saveChangesInFileNamed-3A-.cs
@@ -0,0 +1,8 @@
+saveChangesInFileNamed: aString
+	| fullChangesName |
+	fullChangesName _ (FileDirectory default fullNameFor: aString).
+	(FileDirectory default directoryNamed:(FileDirectory dirPathFor: fullChangesName )) assureExistence.
+	FileDirectory default 
+		copyFileWithoutOverwriteConfirmationNamed: SmalltalkImage current changesName 
+		toFileNamed: fullChangesName.
+	Smalltalk setMacFileInfoOn: fullChangesName.
