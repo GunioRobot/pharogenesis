7d5a7cd9b925f845096bef73938ee6b8c9713013
yo <unknown>
2003-12-18 23:30:00 +0000

    no message

--- StrikeFont/instance/emphasis/bonk-3A-with-3A-.cs
+++ StrikeFont/instance/emphasis/bonk-3A-with-3A-.cs
@@ -3,9 +3,14 @@ bonk: glyphForm with: bonkForm
 	between characters to prevent them from straying into an adjacent
 	character as a result of, eg, bolding or italicizing"
 	"Uses the bonkForm to erase at every character boundary in glyphs."
-	| bb offset |
+	| bb offset x |
 	offset _ bonkForm offset x.
 	bb _ BitBlt current toForm: glyphForm.
 	bb sourceForm: bonkForm; sourceRect: bonkForm boundingBox;
 		combinationRule: Form erase; destY: 0.
-	1 to: xTable size-1 do: [:i | bb destX: (xTable at: i) + offset; copyBits].
+	x _ self xTable.
+	(x isMemberOf: SparseLargeTable) ifTrue: [
+		x base to: x size-1 do: [:i | bb destX: (x at: i) + offset; copyBits].
+	] ifFalse: [
+		1 to: x size-1 do: [:i | bb destX: (x at: i) + offset; copyBits].
+	].
