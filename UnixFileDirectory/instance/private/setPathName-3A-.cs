23d3a367efadc7c514e868b8d7fec8e659bee977
yo <unknown>
2003-12-19 18:32:00 +0000

    no message

--- UnixFileDirectory/instance/private/setPathName-3A-.cs
+++ UnixFileDirectory/instance/private/setPathName-3A-.cs
@@ -2,5 +2,5 @@ setPathName: pathString
 	"Unix path names start with a leading delimiter character."
 
 	(pathString isEmpty or: [pathString first ~= self pathNameDelimiter])
-		ifTrue: [pathName _ self pathNameDelimiter asString, pathString]
-		ifFalse: [pathName _ pathString].
+		ifTrue: [pathName _ FilePath pathName: (self pathNameDelimiter asString, pathString)]
+		ifFalse: [pathName _ FilePath pathName: pathString].
