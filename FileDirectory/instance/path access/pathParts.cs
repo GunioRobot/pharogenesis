0aae0cdae934f8a218b7a4ba21e98db96b60de18
yo <unknown>
2003-12-19 21:15:00 +0000

    no message

--- FileDirectory/instance/path access/pathParts.cs
+++ FileDirectory/instance/path access/pathParts.cs
@@ -1,4 +1,4 @@
 pathParts
 	"Return the path from the root of the file system to this directory as an array of directory names."
 
-	^ pathName findTokens: self pathNameDelimiter asString
+	^ pathName asSqueakPathName findTokens: self pathNameDelimiter asString
