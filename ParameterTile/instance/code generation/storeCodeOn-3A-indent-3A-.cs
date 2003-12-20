ca964c58710ea21fb4b0ed77c3d1ed48127b611a
yo <unknown>
2003-12-20 02:49:00 +0000

    no message

---
+++ ParameterTile/instance/code generation/storeCodeOn-3A-indent-3A-.cs
@@ -0,0 +1,11 @@
+storeCodeOn: aStream indent: tabCount
+	"Store code on the stream"
+ 
+	| myTypeString |
+	myTypeString _ self resultType.
+	(self scriptEditor hasParameter and: [self scriptEditor typeForParameter = myTypeString])
+		ifTrue:
+			[aStream nextPutAll: 'parameter']
+		ifFalse:
+			["This script no longer bears a parameter, yet there's an orphaned Parameter tile in it"
+			aStream nextPutAll: '(self defaultValueOfType: #', myTypeString, ')']
