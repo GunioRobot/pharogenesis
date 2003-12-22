9f439d0859eb6f9d79b2cf179a65b484f07de9c6
dgd <unknown>
2003-12-22 07:58:00 +0000

    no message

---
+++ PRServerDirectory/instance/file directory/on-3A-.cs
@@ -0,0 +1,5 @@
+on: fullName 
+	"Answer another ServerDirectory on the partial path name.  
+	fullName is directory path, and does include the name of the  
+	server."
+	^ self class fullPath: fullName
