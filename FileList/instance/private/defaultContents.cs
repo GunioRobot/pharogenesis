8d91b520895553a0a9b272344b8389ca71265dcf
dgd <unknown>
2003-12-27 12:22:00 +0000

    no message

--- FileList/instance/private/defaultContents.cs
+++ FileList/instance/private/defaultContents.cs
@@ -2,8 +2,8 @@ defaultContents
 	contents _ list == nil
 		ifTrue: [String new]
 		ifFalse: [String streamContents:
-					[:s | s nextPutAll: 'NO FILE SELECTED'; cr.
-					s nextPutAll: '  -- Folder Summary --'; cr.
+					[:s | s nextPutAll: 'NO FILE SELECTED' translated; cr.
+					s nextPutAll: '  -- Folder Summary --' translated; cr.
 					list do: [:item | s nextPutAll: item; cr]]].
 	brevityState _ #FileList.
 	^ contents
