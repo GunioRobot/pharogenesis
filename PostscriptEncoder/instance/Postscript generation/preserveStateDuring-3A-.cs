a3a4936ae12280943a561cb68479494a4da6f735
nk <unknown>
2003-12-29 15:56:00 +0000

    no message

--- PostscriptEncoder/instance/Postscript generation/preserveStateDuring-3A-.cs
+++ PostscriptEncoder/instance/Postscript generation/preserveStateDuring-3A-.cs
@@ -1,7 +1,9 @@
-preserveStateDuring: aBlock
-	"Note that this method supplies self, and encoder, to the block"
+preserveStateDuring: aBlock 
+	"Note that this method supplies self, an encoder, to the block"
 	| retval |
-	self print: 'gsave'; cr.
-	retval _ aBlock value: self.
-	self print: 'grestore'; cr.
+	self print: 'gsave';
+		 cr.
+	retval := aBlock value: self.
+	self print: 'grestore';
+		 cr.
 	^ retval
