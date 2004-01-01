533db1cdd4747c02901605b55d6e9da2652be47a
nk <unknown>
2004-01-01 20:21:00 +0000

    no message

--- PostscriptCanvas/class/testing/morphAsPostscript-3A-rotated-3A-offsetBy-3A-.cs
+++ PostscriptCanvas/class/testing/morphAsPostscript-3A-rotated-3A-offsetBy-3A-.cs
@@ -2,7 +2,7 @@ morphAsPostscript:aMorph rotated:rotateFlag offsetBy:offset
  | psCanvas |
   psCanvas _ self new.
   psCanvas reset.
-  psCanvas bounds:(offset extent:aMorph bounds extent).
+  psCanvas bounds: (0@0 extent: (aMorph bounds extent + (2 * offset))).
   psCanvas topLevelMorph:aMorph.
   psCanvas resetContentRotated: rotateFlag.
   psCanvas fullDrawMorph: aMorph .
