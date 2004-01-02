02ce8a65b1ea61bf03995f10374e4c94bf3ec488
yo <unknown>
2004-01-02 06:40:00 +0000

    no message

--- Player/instance/scripts-2D-kernel/noteRenameOf-3A-to-3A-inPlayer-3A-.cs
+++ Player/instance/scripts-2D-kernel/noteRenameOf-3A-to-3A-inPlayer-3A-.cs
@@ -2,6 +2,5 @@ noteRenameOf: oldSlotName to: newSlotName inPlayer: aPlayer
 	"Note that aPlayer has renamed a slot formerly known as oldSlotName to be newSlotName"
 
 	self allScriptEditors do:
-		[:anEditor | (anEditor hasScriptReferencing: oldSlotName ofPlayer: aPlayer) ifTrue: 
-			[self flag: #deferred.   "Now what?  Just implement the following?!"
-			anEditor replaceReferencesToSlot: oldSlotName inPlayer: aPlayer with: newSlotName]]
+		[:anEditor | (anEditor showingMethodPane not and: [anEditor hasScriptReferencing: oldSlotName ofPlayer: aPlayer]) ifTrue: 
+			[anEditor replaceReferencesToSlot: oldSlotName inPlayer: aPlayer with: newSlotName]]
