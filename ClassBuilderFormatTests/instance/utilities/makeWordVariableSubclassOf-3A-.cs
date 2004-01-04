1b5853f27d6037013139bdd3dde2844a8f3ae7a8
ar <unknown>
2004-01-04 20:16:00 +0000

    no message

---
+++ ClassBuilderFormatTests/instance/utilities/makeWordVariableSubclassOf-3A-.cs
@@ -0,0 +1,6 @@
+makeWordVariableSubclassOf: aClass
+	subClass := aClass variableWordSubclass: self subClassName
+		instanceVariableNames: ''
+		classVariableNames: ''
+		poolDictionaries: ''
+		category: 'Kernel-Tests-ClassBuilder'
