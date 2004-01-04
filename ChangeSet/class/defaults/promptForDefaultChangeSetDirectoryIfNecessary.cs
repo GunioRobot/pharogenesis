086aaf730733bebbbd90b109e041e262aea3ed3b
nk <unknown>
2004-01-04 16:47:00 +0000

    no message

---
+++ ChangeSet/class/defaults/promptForDefaultChangeSetDirectoryIfNecessary.cs
@@ -0,0 +1,25 @@
+promptForDefaultChangeSetDirectoryIfNecessary
+	"Check the Preference (if any), and prompt the user to change it if necessary.
+	The default if the Preference is unset is the current directory.
+	Answer the directory."
+
+	"ChangeSet promptForDefaultChangeSetDirectoryIfNecessary"
+	| choice directoryName dir |
+	directoryName := Preferences
+				parameterAt: #defaultChangeSetDirectoryName
+				ifAbsentPut: [''].
+	[dir := FileDirectory default directoryNamed: directoryName.
+	dir exists]
+		whileFalse: [choice := PopUpMenu withCaption: 
+			('The preferred change set directory (''{1}'') does not exist.
+Create it or use the default directory ({2})?' translated format: { directoryName. FileDirectory default pathName })
+	chooseFrom: (#('Create directory' 'Use default directory and forget preference' 'Choose another directory' ) collect: [ :ea | ea translated ]).
+			choice = 1
+				ifTrue: [dir assureExistence ].
+			choice = 3
+				ifTrue: [dir := FileList2 modalFolderSelector.
+					directoryName := dir
+					ifNil: [ '' ]
+						ifNotNil: [dir pathName ]]].
+		self defaultChangeSetDirectory: directoryName.
+		^dir
