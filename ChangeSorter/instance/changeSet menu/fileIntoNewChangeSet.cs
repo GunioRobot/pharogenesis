c35aab653711a2e2c9049f11c57a51f7b533fd77
nk <unknown>
2004-01-04 17:07:00 +0000

    no message

--- ChangeSorter/instance/changeSet menu/fileIntoNewChangeSet.cs
+++ ChangeSorter/instance/changeSet menu/fileIntoNewChangeSet.cs
@@ -1,14 +1,16 @@
 fileIntoNewChangeSet
-	"Obtain a file designation from the user, and file its contents into a new change set whose name is a function of the filename.  Show the new set and leave the current changeSet unaltered."
-	| aFileName  aNewChangeSet |
-
-	self okToChange ifFalse: [^ self].
-	aFileName _ FillInTheBlank request: 'Name of file to be imported: '.
-	aFileName size == 0 ifTrue: [^ self].
-	(FileDirectory default fileExists: aFileName) ifFalse:
-		[^ self inform: 'Sorry -- cannot find that file'].
-
-	aNewChangeSet _ self class 
-			newChangesFromStream: (FileStream readOnlyFileNamed: aFileName) 
-			named: aFileName.
-	aNewChangeSet ifNotNil: [self showChangeSet: aNewChangeSet]
+	"Obtain a file designation from the user, and file its contents into a  
+	new change set whose name is a function of the filename. Show the  
+	new set and leave the current changeSet unaltered."
+	| aNewChangeSet stream |
+	self okToChange
+		ifFalse: [^ self].
+	ChangeSet promptForDefaultChangeSetDirectoryIfNecessary.
+	stream := StandardFileMenu oldFileStreamFrom: ChangeSet defaultChangeSetDirectory.
+	stream
+		ifNil: [^ self].
+	aNewChangeSet := self class
+				newChangesFromStream: stream
+				named: (FileDirectory localNameFor: stream name).
+	aNewChangeSet
+		ifNotNil: [self showChangeSet: aNewChangeSet]
