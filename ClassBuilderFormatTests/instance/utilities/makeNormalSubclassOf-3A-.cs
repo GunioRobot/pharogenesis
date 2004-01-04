231f2e4828bcada4ea071af55a1d49cb4132aae2
ar <unknown>
2004-01-04 20:15:00 +0000

    no message

---
+++ ClassBuilderFormatTests/instance/utilities/makeNormalSubclassOf-3A-.cs
@@ -0,0 +1,6 @@
+makeNormalSubclassOf: aClass
+	subClass := aClass subclass: self subClassName
+		instanceVariableNames: ''
+		classVariableNames: ''
+		poolDictionaries: ''
+		category: 'Kernel-Tests-ClassBuilder'
