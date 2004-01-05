98da3daf64774b119b68c8ea4bfc235e0a6300f0
md <unknown>
2004-01-05 14:59:00 +0000

    no message

---
+++ ClassTest/instance/setup/setUp.cs
@@ -0,0 +1,8 @@
+setUp
+
+	Smalltalk removeClassNamed: #TUTU.
+	Object subclass: #TUTU
+		instanceVariableNames: ''
+		classVariableNames: ''
+		poolDictionaries: ''
+		category: 'Tests-Kernel-Classes'
