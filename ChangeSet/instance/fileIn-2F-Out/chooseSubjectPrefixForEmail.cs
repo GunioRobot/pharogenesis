318613a8dade81017055a7ec908806c2f949fb17
FBS <unknown>
2004-01-06 16:59:00 +0000

    no message

--- ChangeSet/instance/fileIn-2F-Out/chooseSubjectPrefixForEmail.cs
+++ ChangeSet/instance/fileIn-2F-Out/chooseSubjectPrefixForEmail.cs
@@ -3,7 +3,7 @@ chooseSubjectPrefixForEmail
 	| subjectIndex |
 
 	subjectIndex _
-		(PopUpMenu labels: 'Bug fix [FIX]\Enhancement [ENH]\Goodie [GOODIE]\None of the above (will not be archived)' withCRs)
+		(PopUpMenu labels: 'Bug fix [FIX]\Enhancement [ENH]\Goodie [GOODIE]\Test suite [TEST]\None of the above (will not be archived)' withCRs)
 			startUpWithCaption: 'What type of change set\are you submitting to the list?' withCRs.
 
-	^ #('[CS] ' '[FIX] ' '[ENH] ' '[GOODIE] ' '[CS] ') at: subjectIndex + 1
+	^ #('[CS] ' '[FIX] ' '[ENH] ' '[GOODIE] ' '[TEST] ' '[CS] ') at: subjectIndex + 1
