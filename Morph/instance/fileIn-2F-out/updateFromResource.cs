f6f7ed3aba0168e6d441a7760ecc7a835ec1f556
nk <unknown>
2004-01-06 12:38:00 +0000

    no message

--- Morph/instance/fileIn-2F-out/updateFromResource.cs
+++ Morph/instance/fileIn-2F-out/updateFromResource.cs
@@ -1,14 +1,14 @@
 updateFromResource
-
 	| pathName newMorph f |
-	(pathName _ self valueOfProperty: #resourceFilePath) ifNil: [^ self].
-	(pathName asLowercase endsWith: '.morph')
-		ifTrue:
-			[newMorph _ (FileStream readOnlyFileNamed: pathName) fileInObjectAndCode.
-			(newMorph isKindOf: Morph) ifFalse: [^ self error: 'Resource not a single morph']]
-		ifFalse:
-			[f _ Form fromFileNamed: pathName.
-			f ifNil: [^ self error: 'unrecognized image file format'].
-			newMorph _ SketchMorph withForm: f].
+	(pathName := self valueOfProperty: #resourceFilePath) ifNil: [^self].
+	(pathName asLowercase endsWith: '.morph') 
+		ifTrue: 
+			[newMorph := (FileStream readOnlyFileNamed: pathName) fileInObjectAndCode.
+			(newMorph isMorph) 
+				ifFalse: [^self error: 'Resource not a single morph']]
+		ifFalse: 
+			[f := Form fromFileNamed: pathName.
+			f ifNil: [^self error: 'unrecognized image file format'].
+			newMorph := World drawingClass withForm: f].
 	newMorph setProperty: #resourceFilePath toValue: pathName.
 	self owner replaceSubmorph: self by: newMorph
