69b19e6ee79afa36c20e098a83eed0774b02caf7
nk <unknown>
2004-01-06 12:53:00 +0000

    no message

--- Morph/instance/menus/addPaintingItemsTo-3A-hand-3A-.cs
+++ Morph/instance/menus/addPaintingItemsTo-3A-hand-3A-.cs
@@ -1,17 +1,21 @@
-addPaintingItemsTo: aMenu hand: aHandMorph
+addPaintingItemsTo: aMenu hand: aHandMorph 
 	| subMenu movies |
-	subMenu _ MenuMorph new defaultTarget: self.
-
-	subMenu add: 'repaint' action: #editDrawing.
-	subMenu add: 'set rotation center' action: #setRotationCenter.
-	subMenu add: 'reset forward-direction' action: #resetForwardDirection.
-	subMenu add: 'set rotation style' action: #setRotationStyle.
-	subMenu add: 'erase pixels of color' action: #erasePixelsOfColor:.
-	subMenu add: 'recolor of pixels of color' action: #recolorPixelsOfColor:.
-	movies _
-		(self world rootMorphsAt: aHandMorph targetOffset)
-			select: [:m | (m isKindOf: MovieMorph) or:
-						[m isKindOf: SketchMorph]].
-	(movies size > 1) ifTrue:
-		[subMenu add: 'insert into movie' action: #insertIntoMovie:].
-	aMenu add: 'painting...' subMenu: subMenu
+	subMenu := MenuMorph new defaultTarget: self.
+	subMenu add: 'repaint' translated action: #editDrawing.
+	subMenu add: 'set rotation center' translated action: #setRotationCenter.
+	subMenu add: 'reset forward-direction' translated
+		action: #resetForwardDirection.
+	subMenu add: 'set rotation style' translated action: #setRotationStyle.
+	subMenu add: 'erase pixels of color' translated
+		action: #erasePixelsOfColor:.
+	subMenu add: 'recolor pixels of color' translated
+		action: #recolorPixelsOfColor:.
+	subMenu add: 'reduce color palette' translated action: #reduceColorPalette:.
+	subMenu add: 'add a border around this shape...' translated
+		action: #addBorderToShape:.
+	movies := (self world rootMorphsAt: aHandMorph targetOffset) 
+				select: [:m | (m isKindOf: MovieMorph) or: [m isSketchMorph]].
+	movies size > 1 
+		ifTrue: 
+			[subMenu add: 'insert into movie' translated action: #insertIntoMovie:].
+	aMenu add: 'painting...' translated subMenu: subMenu
