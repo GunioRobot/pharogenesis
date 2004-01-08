9a6dadc67b7ef974de11ad35b0a1f63123a07b65
al <unknown>
2004-01-08 12:21:00 +0000

    no message

--- HTTPSocket/class/class initialization/initialize.cs
+++ HTTPSocket/class/class initialization/initialize.cs
@@ -4,4 +4,7 @@ initialize
 	ParamDelimiters _ ' ', CrLf.
 	HTTPPort _ 80.
 	HTTPProxyServer _ nil.
-	HTTPBlabEmail _ ''.  "	'From: tedk@disney.com', CrLf	"
+	HTTPBlabEmail _ ''.  "	'From: somebody@no.where', CrLf	"
+	HTTPProxyCredentials _ ''.
+
+	ExternalSettings registerClient: self
