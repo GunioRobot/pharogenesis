7ee8d6446a48e846152dfa9b1f77ac32c5618791
BG <unknown>
2004-01-09 12:26:00 +0000

    no message

--- OrderedCollection/instance/private/find-3A-.cs
+++ OrderedCollection/instance/private/find-3A-.cs
@@ -1,4 +1,9 @@
 find: oldObject
+  "  This method answers an index in the range firstIndex .. lastIndex, which is meant for internal use only.
+     Never use this method in your code, the methods for public use are:
+        #indexOf:
+        #indexOf:ifAbsent: "
+
 	| index |
 	index _ firstIndex.
 	[index <= lastIndex]
