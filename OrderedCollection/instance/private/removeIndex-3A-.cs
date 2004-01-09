871cb113378ff8e16eac712188c702f284dc52f2
BG <unknown>
2004-01-09 12:28:00 +0000

    no message

--- OrderedCollection/instance/private/removeIndex-3A-.cs
+++ OrderedCollection/instance/private/removeIndex-3A-.cs
@@ -1,4 +1,9 @@
 removeIndex: removedIndex
+  "  removedIndex is an index in the range firstIndex .. lastIndex, such an index is not known from outside the collection.
+    Never use this method in your code, it is meant for private use by OrderedCollection only.
+     The method for public use is:
+        #removeAt: "
+
 	array 
 		replaceFrom: removedIndex 
 		to: lastIndex - 1 
