232bace00d26f08d95a17066a7d8a3ebea734647
nk <unknown>
2004-01-10 14:51:00 +0000

    no message

--- SketchMorph/instance/geometry/extent-3A-.cs
+++ SketchMorph/instance/geometry/extent-3A-.cs
@@ -1,5 +1,7 @@
 extent: newExtent
-	"Change my scale to fit myself into the given extent."
+	"Change my scale to fit myself into the given extent.
+	Avoid extents where X or Y is zero."
+	(newExtent y = 0 or: [ newExtent x = 0 ]) ifTrue: [ ^self ].
 	self extent = newExtent ifTrue:[^self].
-	scalePoint _ newExtent asFloatPoint / originalForm extent.
+	scalePoint _ newExtent asFloatPoint / (originalForm extent max: 1@1).
 	self layoutChanged.
