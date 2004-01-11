31f051a5b9f25830fa3fa2a1e792b1dc02c2d75d
nk <unknown>
2004-01-11 15:29:00 +0000

    no message

--- UpdatingStringMorph/instance/editing/doneWithEdits.cs
+++ UpdatingStringMorph/instance/editing/doneWithEdits.cs
@@ -1,5 +1,8 @@
 doneWithEdits
 	"If in a SyntaxMorph, shrink min width after editing"
 
+	| editor |
 	super doneWithEdits.
 	(owner respondsTo: #parseNode) ifTrue: [minimumWidth _ 8].
+	editor _ (submorphs detect: [ :sm | sm isKindOf: StringMorphEditor ] ifNone: [ ^self ]).
+	editor delete.
