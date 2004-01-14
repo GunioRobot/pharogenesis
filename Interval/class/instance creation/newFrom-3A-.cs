6c7b5d34c1677f659d27f14ca0651e4525f08ffe
md <unknown>
2004-01-14 11:42:00 +0000

    no message

--- Interval/class/instance creation/newFrom-3A-.cs
+++ Interval/class/instance creation/newFrom-3A-.cs
@@ -1,9 +1,13 @@
 newFrom: aCollection 
 	"Answer an instance of me containing the same elements as aCollection."
 
-	| newInterval |
-	newInterval _ self from: aCollection first to: aCollection last by:
-		(aCollection last - aCollection first)//(aCollection size - 1).
+    | newInterval n |
+
+    (n := aCollection size) <= 1 ifTrue: [
+		n = 0 ifTrue: [^self from: 1 to: 0].
+		^self from: aCollection first to: aCollection last].
+    	newInterval := self from: aCollection first to: aCollection last
+	by: (aCollection last - aCollection first) // (n - 1).
 	aCollection ~= newInterval
 		ifTrue: [self error: 'The argument is not an arithmetic progression'].
 	^newInterval
