9513d5465f87b2ef8ae9cf516c31ca41a7268e0c
yo <unknown>
2004-01-15 16:48:00 +0000

    no message

---
+++ StrikeFont/instance/file in-2F-out/readEFontBDFForKoreanFromFile-3A-name-3A-overrideWith-3A-.cs
@@ -0,0 +1,19 @@
+readEFontBDFForKoreanFromFile: fileName name: aString overrideWith: otherFileName
+
+	| fontReader stream |
+	fontReader _ EFontBDFFontReaderForRanges readOnlyFileNamed: fileName.
+	stream _ ReadStream on: (fontReader readRanges: fontReader rangesForKorean overrideWith: otherFileName otherRanges: {Array with: 8481 with: 12320} additionalOverrideRange: fontReader additionalRangesForKorean).
+	xTable _ stream next.
+	glyphs _ stream next.
+	minAscii _ stream next.
+	maxAscii _ stream next.
+	maxWidth _ stream next.
+	ascent _ stream next.
+	descent _ stream next.
+	pointSize _ stream next.
+	name _ aString.
+	type _ 0. "no one see this"
+	superscript _ ascent - descent // 3.	
+	subscript _ descent - ascent // 3.	
+	emphasis _ 0.
+	self reset.
