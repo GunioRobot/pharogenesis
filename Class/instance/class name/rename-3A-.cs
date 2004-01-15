797f382b34e9af531f37d19f35e8362c28e38eda
NS <unknown>
2004-01-15 15:41:00 +0000

    no message

--- Class/instance/class name/rename-3A-.cs
+++ Class/instance/class name/rename-3A-.cs
@@ -1,13 +1,13 @@
-rename: aString
+rename: aString 
 	"The new name of the receiver is the argument, aString."
 
 	| newName |
 	(newName _ aString asSymbol) ~= self name
-		ifTrue:
-			[(Smalltalk includesKey: newName)
-				ifTrue: [^self error: newName , ' already exists'].
-			(Undeclared includesKey: newName)
-				ifTrue: [SelectionMenu notify: 'There are references to, ' , aString printString , '
+		ifFalse: [^ self].
+	(self environment includesKey: newName)
+		ifTrue: [^ self error: newName , ' already exists'].
+	(Undeclared includesKey: newName)
+		ifTrue: [self inform: 'There are references to, ' , aString printString , '
 from Undeclared. Check them after this change.'].
-			Smalltalk renameClass: self as: newName.
-			name _ newName]
+	self environment renameClass: self as: newName.
+	name _ newName
