c71785bc388675e66e89c1bfc9dad5c6fdc7e606
sd <unknown>
2004-01-16 21:32:00 +0000

    no message

--- TheWorldMenu/instance/construction/changesMenu.cs
+++ TheWorldMenu/instance/construction/changesMenu.cs
@@ -4,11 +4,11 @@ changesMenu
         | menu |
         menu _ self menu: 'changes...'.
         self fillIn: menu from: {
-                { 'file out current change set' . { Utilities . #fileOutChanges}.
+                { 'file out current change set' . { ChangeSet current . #verboseFileOut}.
                                 'Write the current change set out to a file whose name reflects the change set name and the current date & time.'}.
                 { 'create new change set...' . { ChangeSorter . #newChangeSet}. 'Create a new change set and make it the current one.'}.
-                { 'browse changed methods' . { Smalltalk . #browseChangedMessages}.  'Open a message-list browser showing all methods in the current change set'}.
-                { 'check change set for slips' . { Smalltalk changes . #lookForSlips}.
+                { 'browse changed methods' . { ChangeSet  . #browseChangedMessages}.  'Open a message-list browser showing all methods in the current change set'}.
+                { 'check change set for slips' . { self  . #lookForSlips}.
                                 'Check the current change set for halts, references to the Transcript, etc., and if any such thing is found, open up a message-list browser detailing all possible slips.'}.
 
                 nil.
@@ -21,10 +21,10 @@ changesMenu
                                 'Open a new recent-submissions browser.  A recent-submissions browser is a message-list browser that shows the most recent methods that have been submitted.  If you submit changes within that browser, it will keep up-to-date, always showing the most recent submissions.'}.
 
                 { 'find recent submissions (R)' . { #myWorld . #openRecentSubmissionsBrowser:}.
-                                'Make an open recent-submissions browser be the the front-window, expanding a collapsed one or creating a new one if necessary.  A recent-submissions browser is a message-list browser that shows the most recent methods that have been submitted, latest first.  If you submit changes within that browser, it will keep up-to-date, always showing the most recent submissions at the top of the browser.'}.
+                                'Make an open recent-submissions browser be the front-window, expanding a collapsed one or creating a new one if necessary.  A recent-submissions browser is a message-list browser that shows the most recent methods that have been submitted, latest first.  If you submit changes within that browser, it will keep up-to-date, always showing the most recent submissions at the top of the browser.'}.
 
 			nil.
-                { 'recently logged changes...' . { ChangeList . #browseRecentLog}.'Open a change-list browser on the latter part of the changes log.'}.
+                { 'recently logged changes...' . { ChangeList . #browseRecentLog}.'Open a change-list browser on the latter part of the changes log.  You can use this browser to recover logged changes which were not saved in your image, in the event of a crash or other interruption.'}.
 
                 { 'recent log file...' . { Smalltalk . #writeRecentToFile}.
                                 'Create a file holding the logged changes (going as far back as you wish), and open a window on that file.'}.
