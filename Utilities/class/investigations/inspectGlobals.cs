04fddd6dc326a2c423ed7720c88657eee44714ff
sd <unknown>
2004-01-16 20:50:00 +0000

    no message

--- Utilities/class/investigations/inspectGlobals.cs
+++ Utilities/class/investigations/inspectGlobals.cs
@@ -1,8 +1,5 @@
 inspectGlobals
 	"Utilities  inspectGlobals"
-	| associations aDict |
-	associations _ ((Smalltalk keys select: [:aKey | ((Smalltalk at: aKey) isKindOf: Class) not]) asSortedArray collect:
-		[:aKey | Smalltalk associationAt: aKey]).
-	aDict _ IdentityDictionary new.
-	associations do: [:as | aDict add: as].
-	aDict inspectWithLabel: 'The Globals'
+	self deprecated: 'use Smalltalk inspectGlobals'.
+	
+	Smalltalk inspectGlobals. 
