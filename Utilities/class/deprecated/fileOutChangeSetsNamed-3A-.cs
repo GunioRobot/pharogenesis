35b5f674c3beca6debddb9a0af57610ef2e51c58
sd <unknown>
2004-01-16 21:37:00 +0000

    no message

---
+++ Utilities/class/deprecated/fileOutChangeSetsNamed-3A-.cs
@@ -0,0 +1,4 @@
+fileOutChangeSetsNamed: nameList
+	"File out the list of change sets whose names are provided"
+     self deprecated: 'Use ChangeSorter fileOutChangeSetsNamed: nameList'.
+	ChangeSorter fileOutChangeSetsNamed: nameList
