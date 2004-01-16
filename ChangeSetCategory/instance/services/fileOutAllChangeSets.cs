ae565bba3b054a5d097312156350f6bb40d29993
sd <unknown>
2004-01-16 21:37:00 +0000

    no message

---
+++ ChangeSetCategory/instance/services/fileOutAllChangeSets.cs
@@ -0,0 +1,12 @@
+fileOutAllChangeSets
+	"File out all the nonempty change sets in the current category, suppressing the checks for slips that might otherwise ensue.  Obtain user confirmation before undertaking this possibly prodigious task."
+
+	| aList |
+	aList _ self elementsInOrder select:
+		[:aChangeSet  | aChangeSet isEmpty not].
+	aList size == 0 ifTrue: [^ self inform: 'sorry, all the change sets in this category are empty'].
+	(self confirm: 'This will result in filing out ', aList size printString, ' change set(s)
+Are you certain you want to do this?') ifFalse: [^ self].
+
+	Preferences setFlag: #checkForSlips toValue: false during: 
+		[ChangeSorter fileOutChangeSetsNamed: (aList collect: [:m | m name]) asSortedArray]
