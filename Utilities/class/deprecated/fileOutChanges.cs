43931489754a4f506adda42999f66d6a119aaeda
sd <unknown>
2004-01-16 21:31:00 +0000

    no message

---
+++ Utilities/class/deprecated/fileOutChanges.cs
@@ -0,0 +1,5 @@
+fileOutChanges
+	"File out the current change set to a file whose name is a function of the current date and time."
+
+	self deprecated: 'Use ChangeSet current verboseFileOut'.
+	ChangeSet current verboseFileOut
