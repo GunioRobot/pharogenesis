accc15b7db17bf9acde3a5518e1cf9a7aa2a2dbf
sd <unknown>
2004-01-16 21:03:00 +0000

    no message

---
+++ Utilities/class/deprecated/hierarchyOfImplementorsOf-3A-forClass-3A-.cs
@@ -0,0 +1,5 @@
+hierarchyOfImplementorsOf: aSelector forClass: aClass
+	"Answer a list of classes in the hierarchy both above and below the given class which implement the given selector."
+
+	self deprecated: 'Use SystemNavigation default hierarchyOfImplementorsOf: aSelector forClass: aClass'.
+	SystemNavigation default hierarchyOfImplementorsOf: aSelector forClass: aClass
