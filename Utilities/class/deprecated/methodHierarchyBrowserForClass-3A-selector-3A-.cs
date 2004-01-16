a8ff954fd407cd3461116dc35066c4c0f4178eb8
sd <unknown>
2004-01-16 21:09:00 +0000

    no message

---
+++ Utilities/class/deprecated/methodHierarchyBrowserForClass-3A-selector-3A-.cs
@@ -0,0 +1,6 @@
+methodHierarchyBrowserForClass: aClass selector: sel
+	"Create and schedule a message set browser on all implementors of the 
+	currently selected message selector. Do nothing if no message is selected."
+	
+	self deprecated: 'Use SystemNavigation default methodHierarchyBrowserForClass: aClass selector: sel'. 
+	SystemNavigation default methodHierarchyBrowserForClass: aClass selector: sel
