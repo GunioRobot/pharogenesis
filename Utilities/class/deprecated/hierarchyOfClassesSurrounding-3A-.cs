c408b8c2773131de20ed2f0e47b8bec4a87ce804
sd <unknown>
2004-01-16 21:02:00 +0000

    no message

---
+++ Utilities/class/deprecated/hierarchyOfClassesSurrounding-3A-.cs
@@ -0,0 +1,7 @@
+hierarchyOfClassesSurrounding: aClass
+	"Answer a list of classes in the hierarchy both above and below the given class "
+	
+	self deprecated: 'Use SystemNavigation default hierarchyOfClassesSurrounding: aClass'.
+	SystemNavigation default hierarchyOfClassesSurrounding: aClass.
+	
+	
