42029ce6fd2e415cd4b2517aee2602569972d0c1
mir <unknown>
2004-01-17 12:27:00 +0000

    no message

---
+++ Parser/instance/expression types/temporariesIn-3A-.cs
@@ -0,0 +1,24 @@
+temporariesIn: methodSelector
+	" [ '|' (variable)* '|' ]"
+	| vars theActualText |
+	(self match: #verticalBar) ifFalse: 
+		["no temps"
+		doitFlag ifTrue: [requestor
+				ifNil: [tempsMark _ 1]
+				ifNotNil: [tempsMark _ requestor selectionInterval first].
+			^ #()].
+		tempsMark _ (prevEnd ifNil: [0]) + 1.
+		tempsMark _ hereMark	"formerly --> prevMark + prevToken".
+
+		tempsMark > 0 ifTrue:
+			[theActualText _ source contents.
+			[tempsMark < theActualText size and: [(theActualText at: tempsMark) isSeparator]]
+				whileTrue: [tempsMark _ tempsMark + 1]].
+			^ #()].
+	vars _ OrderedCollection new.
+	[hereType == #word]
+		whileTrue: [vars addLast: (encoder bindTemp: self advance in: methodSelector)].
+	(self match: #verticalBar) ifTrue: 
+		[tempsMark _ prevMark.
+		^ vars].
+	^ self expected: 'Vertical bar'
