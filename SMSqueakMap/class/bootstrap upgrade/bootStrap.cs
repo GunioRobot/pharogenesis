5611f8f4b1b445c1c1e0d3d937414b61035d1922
gk <unknown>
2004-01-17 13:03:00 +0000

    no message

---
+++ SMSqueakMap/class/bootstrap upgrade/bootStrap.cs
@@ -0,0 +1,13 @@
+bootStrap
+	"Bootstrap upgrade. Only used when SqueakMap itself is too old to
+	communicate with the server. This relies on the existence of a package
+	called SqueakMap that is a .st loadscript. The loadscript needs to do its
+	own changeset management."
+
+	| server url |
+	server _ self findServer.
+	server ifNotNil: ["Ok, found a SqueakMap server"
+		url _ (('http://', server, '/sm/packagebyname/squeakmap/downloadurl')
+				asUrl retrieveContents content) asUrl.
+		(url retrieveContents content unzipped readStream)
+				fileInAnnouncing: 'Upgrading SqueakMap...']
