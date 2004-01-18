e3956c87ca2a4188d0aa6fc53e8d15aa4260f773
mk <unknown>
2004-01-18 23:31:00 +0000

    no message

---
+++ Complex/instance/arithmetic/+.cs
@@ -0,0 +1,14 @@
++ anObject
+	"Answer the sum of the receiver and aNumber."
+	| a b c d newReal newImaginary |
+	anObject isComplex
+		ifTrue:
+			[a _ self real.
+			b _ self imaginary.
+			c _ anObject real.
+			d _ anObject imaginary.
+			newReal _ a + c.
+			newImaginary _ b + d.
+			^ Complex real: newReal imaginary: newImaginary]
+		ifFalse:
+			[^ anObject adaptToComplex: self andSend: #+]
