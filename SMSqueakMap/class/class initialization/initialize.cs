ebb247fa225b9b369aa94705bac8aa5297e7f3c0
gk <unknown>
2004-01-19 09:20:00 +0000

    no message

---
+++ SMSqueakMap/class/class initialization/initialize.cs
@@ -0,0 +1,9 @@
+initialize
+	"Initialize the list of master servers.
+	If the first three don't answer I just added the Bluefish
+	server so that I (Göran Krampe) could get it up and
+	running myself if needed."
+
+	"self initialize"
+
+	ServerList _ #('map1.squeakfoundation.org' 'map2.squeakfoundation.org' 'map.squeak.org' 'anakin.bluefish.se')
