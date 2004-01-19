b45d6bfe2aa0aec5a5d1f86cc5fb1e77ae677553
sd <unknown>
2004-01-19 20:58:00 +0000

    no message

--- Utilities/class/common requests/evaluate-3A-in-3A-to-3A-.cs
+++ Utilities/class/common requests/evaluate-3A-in-3A-to-3A-.cs
@@ -1,10 +1,5 @@
 evaluate: aString in: aContext to: aReceiver
 	"evaluate aString in the given context, and return the result.  2/2/96 sw"
-	| result |
-	result _ Compiler new
-				evaluate: aString
-				in: aContext
-				to: aReceiver
-				notifying: nil
-				ifFail: [^ #failedDoit].
-	^ result
+	
+	self deprecated: 'Use Compiler>>evaluate: aString in: aContext to: aReceiver'.
+	^ Compiler new evaluate: aString in: aContext to: aReceiver
