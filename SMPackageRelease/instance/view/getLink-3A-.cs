9ecadab3f8a21ebb4fcb30e4962a1792c738347c
gk <unknown>
2004-01-19 12:09:00 +0000

    no message

---
+++ SMPackageRelease/instance/view/getLink-3A-.cs
@@ -0,0 +1,6 @@
+getLink: aBuilder
+
+	^aBuilder
+		getLinkLocal: '/package/', self package id asString, '/autoversion/',
+					self automaticVersion versionString
+		text: self packageNameWithVersion
