fbb7fa1244df6e81a7714d05bfb2d5c23538ea1a
NS <unknown>
2004-01-21 09:20:00 +0000

    no message

--- ClassBuilder/instance/class definition/class-3A-instanceVariableNames-3A-unsafe-3A-.cs
+++ ClassBuilder/instance/class definition/class-3A-instanceVariableNames-3A-unsafe-3A-.cs
@@ -1,22 +1,27 @@
 class: oldClass instanceVariableNames: instVarString unsafe: unsafe
 	"This is the basic initialization message to change the definition of
 	an existing Metaclass"
-	| instVars newClass |
+	| instVars newClass needNew copyOfOldClass |
 	environ _ oldClass environment.
 	instVars _ Scanner new scanFieldNames: instVarString.
 	unsafe ifFalse:[
 		"Run validation checks so we know that we have a good chance for recompilation"
 		(self validateInstvars: instVars from: oldClass forSuper: oldClass superclass) ifFalse:[^nil].
 		(self validateSubclassFormat: oldClass typeOfClass from: oldClass forSuper: oldClass superclass extra: instVars size) ifFalse:[^nil]].
-	"Create a template for the new class (will return oldClass when there is no change)"
+	"See if we need a new subclass or not"
+	needNew _ self needsSubclassOf: oldClass superclass type: oldClass typeOfClass instanceVariables: instVars from: oldClass.
+	needNew ifNil:[^nil]. "some error"
+	needNew ifFalse:[^oldClass]. "no new class needed"
+
+	"Create the new class"
+	copyOfOldClass _ oldClass copy.
 	newClass _ self 
 		newSubclassOf: oldClass superclass 
 		type: oldClass typeOfClass
 		instanceVariables: instVars
-		from: oldClass
-		unsafe: unsafe.
-
-	newClass == nil ifTrue:[^nil]. "Some error"
+		from: oldClass.
+		
 	newClass _ self recompile: false from: oldClass to: newClass mutate: false.
 	self doneCompiling: newClass.
+	SystemChangeNotifier uniqueInstance classDefinitionChangedFrom: copyOfOldClass to: newClass.
 	^newClass
