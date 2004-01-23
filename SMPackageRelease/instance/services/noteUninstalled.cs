6f7ba6a5e9356ebcd419dfdd078d06b8ba9ee467
gk <unknown>
2004-01-23 10:01:00 +0000

    no message

---
+++ SMPackageRelease/instance/services/noteUninstalled.cs
@@ -0,0 +1,7 @@
+noteUninstalled
+	"This package release was just successfully uninstalled.
+	We tell the map so that it can keep track of what
+	package releases are installed."
+
+	self error: 'Uninstall is not working yet!'.
+	map noteUninstalled: self
