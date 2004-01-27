d8ee27070c8598dd46a1a7251b6423cb0734fdc7
NS <unknown>
2004-01-27 14:21:00 +0000

    no message

--- ClassBuilder/instance/private/recordClass-3A-replacedBy-3A-.cs
+++ ClassBuilder/instance/private/recordClass-3A-replacedBy-3A-.cs
@@ -1,14 +1,5 @@
 recordClass: oldClass replacedBy: newClass
-	"Record the replacement of oldClass by newClass so that we can
-	fix any references to oldClass later on."
-	classMap at: newClass put: oldClass.
-	(classMap includesKey: oldClass) ifTrue:[
-		"This will happen if we recompile from Class
-		in which case the metaclass gets recorded twice"
-		classMap at: newClass put: (classMap at: oldClass).
-		classMap removeKey: oldClass.
-	].
-	"And keep the changes up to date"
+	"Keep the changes up to date when we're moving instVars around"
 	(instVarMap includesKey: oldClass name) ifTrue:[
-		Smalltalk changes changeClass: newClass from: oldClass.
+		SystemChangeNotifier uniqueInstance classDefinitionChangedFrom: oldClass to: newClass.
 	].
