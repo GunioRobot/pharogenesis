4747a4e9862c144b752222fce0307910e34025d4
avi <unknown>
2004-01-27 00:29:00 +0000

    no message

---
+++ MCRepositoryGroup/instance/as yet unclassified/addRepository-3A-.cs
@@ -0,0 +1,5 @@
+addRepository: aRepository
+	((repositories includes: aRepository) or: [aRepository == MCCacheRepository default])
+		ifFalse: [repositories add: aRepository.
+				self class default addRepository: aRepository].
+	self changed: #repositories
