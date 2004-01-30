03b6a6be83b0102bd18d37daa3f2095e585c15e8
sd <unknown>
2004-01-30 15:16:00 +0000

    no message

--- CodeLoader/instance/installing/installSourceFile-3A-.cs
+++ CodeLoader/instance/installing/installSourceFile-3A-.cs
@@ -1,13 +1,11 @@
 installSourceFile: aStream
 	"Install the previously loaded source file"
-	| contents secured |
+	| contents trusted |
 	aStream ifNil:[^self error:'No content to install'].
-	secured _ self positionedToSecuredContentsOf: aStream.
-
-	secured
-		ifFalse: [
-			(aStream respondsTo: #close) ifTrue:[aStream close].
-			^self error:'Insecure content encountered'].
-	contents _ aStream upToEnd unzipped.
+	trusted _ SecurityManager default positionToSecureContentsOf: aStream.
+	trusted ifFalse:[(SecurityManager default enterRestrictedMode) 
+					ifFalse:[ aStream close.
+							^ self error:'Insecure content encountered']].
+	contents _ aStream ascii upToEnd unzipped.
 	(aStream respondsTo: #close) ifTrue:[aStream close].
 	^(RWBinaryOrTextStream with: contents) reset fileIn
