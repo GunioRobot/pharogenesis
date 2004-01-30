1ec51f060cd63e0e4f02827eeb37dc1b365ca40a
sd <unknown>
2004-01-30 15:16:00 +0000

    no message

--- CodeLoader/instance/installing/installSegment-3A-.cs
+++ CodeLoader/instance/installing/installSegment-3A-.cs
@@ -1,14 +1,12 @@
 installSegment: reqEntry
 	"Install the previously loaded segment"
-	| secured contentStream contents |
+	| contentStream contents trusted |
 	contentStream _ reqEntry value contentStream.
 	contentStream ifNil:[^self error:'No content to install: ', reqEntry key printString].
-	secured _ self positionedToSecuredContentsOf: contentStream.
-
-	secured
-		ifFalse: [
-			(contentStream respondsTo: #close) ifTrue:[contentStream close].
-			^self error:'Insecure content encountered: ', reqEntry key printString].
-	contents _ contentStream upToEnd unzipped.
+	trusted _ SecurityManager default positionToSecureContentsOf: contentStream.
+	trusted ifFalse:[(SecurityManager default enterRestrictedMode) ifFalse:[
+		contentStream close.
+		^self error:'Insecure content encountered: ', reqEntry key printString]].
+	contents _ contentStream ascii upToEnd unzipped.
 	(contentStream respondsTo: #close) ifTrue:[contentStream close].
 	^(RWBinaryOrTextStream with: contents) reset fileInObjectAndCode install.
