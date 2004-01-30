16f07628cfa883caa29ccf249dce5982b531f304
sd <unknown>
2004-01-30 15:18:00 +0000

    no message

--- GIFReadWriter/instance/accessing/nextImage.cs
+++ GIFReadWriter/instance/accessing/nextImage.cs
@@ -2,19 +2,25 @@ nextImage
 	"Read in the next GIF image from the stream. Read it all into
 memory first for speed."
 
-	| f |
+	| f thisImageColorTable |
 	stream class == ReadWriteStream ifFalse: [
-		(stream respondsTo: #binary) ifTrue: [stream binary].
+		stream binary.
 		self on: (ReadWriteStream with: (stream contentsOfEntireFile))].
 
+	localColorTable _ nil.
 	self readHeader.
 	f _ self readBody.
 	self close.
 	f == nil ifTrue: [^ self error: 'corrupt GIF file'].
 
+	thisImageColorTable _ localColorTable ifNil: [colorPalette].
 	transparentIndex ifNotNil: [
-		transparentIndex + 1 > colorPalette size ifTrue: [
-			colorPalette _ colorPalette forceTo: transparentIndex + 1 paddingWith: Color white].
-		colorPalette at: transparentIndex + 1 put: Color transparent].
-	f colors: colorPalette.
+		transparentIndex + 1 > thisImageColorTable size ifTrue: [
+			thisImageColorTable _ thisImageColorTable 
+				forceTo: transparentIndex + 1 
+				paddingWith: Color white
+		].
+		thisImageColorTable at: transparentIndex + 1 put: Color transparent
+	].
+	f colors: thisImageColorTable.
 	^ f
