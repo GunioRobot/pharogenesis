80ec49de4d8c57b7a6f5240ae5c025417b9042b7
sd <unknown>
2004-02-01 17:55:00 +0000

    no message

--- CodeHolder/instance/categories/changeCategory.cs
+++ CodeHolder/instance/categories/changeCategory.cs
@@ -1,11 +1,12 @@
 changeCategory
-	"Present a menu of the categories of messages for the current class, and let the user choose a new category for the current message"
+	"Present a menu of the categories of messages for the current class, 
+	and let the user choose a new category for the current message"
 
 	| aClass aSelector |
 	(aClass _ self selectedClassOrMetaClass) ifNotNil:
 		[(aSelector _ self selectedMessageName) ifNotNil:
-			[(aClass organization letUserReclassify: aSelector) ifTrue:
-				["Smalltalk changes reorganizeClass: aClass."
+			[(self letUserReclassify: aSelector in: aClass) ifTrue:
+				["ChangeSet current reorganizeClass: aClass."
 				"Decided on further review that the above, when present, could cause more
                     unexpected harm than good"
 				self methodCategoryChanged]]]
