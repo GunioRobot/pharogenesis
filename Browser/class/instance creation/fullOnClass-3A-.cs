a836916346b51c0f5be2bfbd34b6bb70b56e53a8
sd <unknown>
2004-02-02 13:50:00 +0000

    no message

--- Browser/class/instance creation/fullOnClass-3A-.cs
+++ Browser/class/instance creation/fullOnClass-3A-.cs
@@ -1,7 +1,8 @@
 fullOnClass: aClass 
 	"Open a new full browser set to class."
 	| brow |
-	brow _ Browser new.
+	brow _ self new.
 	brow setClass: aClass selector: nil.
-	Browser openBrowserView: (brow openEditString: nil)
+	^ self 
+		openBrowserView: (brow openEditString: nil)
 		label: 'System Browser'
