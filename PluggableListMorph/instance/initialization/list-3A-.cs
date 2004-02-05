d57456efe8e30127f3c2c799a05e4ffc3c72551c
ls <unknown>
2004-02-05 16:29:00 +0000

    no message

--- PluggableListMorph/instance/initialization/list-3A-.cs
+++ PluggableListMorph/instance/initialization/list-3A-.cs
@@ -1,26 +1,41 @@
-list: listOfStrings 
-	| morphList h loc index |
+list: listOfStrings  
+	"lex doesn't think this is used any longer, but is not yet brave enough to remove it.  It should be removed eventually"
+	
+	
+	"Set the receiver's list as specified"
+
+	| morphList h loc index converter item aSelector textColor font |
 	scroller removeAllMorphs.
 	list _ listOfStrings ifNil: [Array new].
 	list isEmpty ifTrue: [self setScrollDeltas.  ^ self selectedMorph: nil].
 	"NOTE: we will want a quick StringMorph init message, possibly even
 		combined with event install and positioning"
 	font ifNil: [font _ Preferences standardListFont].
-	morphList _ list collect:
-		[:item | item isText
+	converter _ self valueOfProperty: #itemConversionMethod.
+	converter ifNil: [converter _ #asStringOrText].
+	textColor _ self valueOfProperty: #textColor.
+	morphList _ list collect: [:each | | stringMorph |
+		item _ each.
+		item _ item perform: converter.
+		stringMorph _ item isText
 			ifTrue: [StringMorph contents: item font: font emphasis: (item emphasisAt: 1)]
-			ifFalse: [StringMorph contents: item font: font]].
-
-	self highlightSelector ifNotNil:[
-		model perform: self highlightSelector with: list with: morphList.
+			ifFalse: [StringMorph contents: item font: font].
+		textColor ifNotNil: [ stringMorph color: textColor ].
+		stringMorph
 	].
+	
+	(aSelector _ self valueOfProperty: #balloonTextSelectorForSubMorphs)
+		ifNotNil:
+			[morphList do: [:m | m balloonTextSelector: aSelector]].
+
+	self highlightSelector ifNotNil:
+		[model perform: self highlightSelector with: list with: morphList].
 
 	"Lay items out vertically and install them in the scroller"
 	h _ morphList first height "self listItemHeight".
 	loc _ 0@0.
 	morphList do: [:m | m bounds: (loc extent: 9999@h).  loc _ loc + (0@h)].
 	scroller addAllMorphs: morphList.
-	self installEventHandlerOn: morphList.
 
 	index _ self getCurrentSelectionIndex.
 	self selectedMorph: ((index = 0 or: [index > morphList size]) ifTrue: [nil] ifFalse: [morphList at: index]).
