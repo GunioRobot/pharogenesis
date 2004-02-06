0136af6cc5de6c6fe2f4bdd55943bab63826876a
ar <unknown>
2004-02-06 14:41:00 +0000

    no message

--- EventSensor/instance/private/primKbdPeek.cs
+++ EventSensor/instance/private/primKbdPeek.cs
@@ -1,2 +1,12 @@
 primKbdPeek
-	^keyboardBuffer peek
+	"Allows for use of old Sensor protocol to get at the keyboard,
+	as when running kbdTest or the InterpreterSimulator in Morphic"
+	| char |
+	self fetchMoreEvents.
+	keyboardBuffer isEmpty ifFalse: [^ keyboardBuffer peek].
+	char _ nil.
+	eventQueue ifNotNil:
+		[eventQueue nextOrNilSuchThat:  "NOTE: must not return out of this block, so loop to end"
+			[:buf | (self isKbdEvent: buf) ifTrue: [char ifNil: [char _ buf at: 3]].
+			false  "NOTE: block value must be false so Queue won't advance"]].
+	^ char
