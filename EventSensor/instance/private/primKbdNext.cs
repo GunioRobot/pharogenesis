939e8d22f0fd9b790b61fc8639f3757cfe4ccc67
ar <unknown>
2004-02-06 14:41:00 +0000

    no message

--- EventSensor/instance/private/primKbdNext.cs
+++ EventSensor/instance/private/primKbdNext.cs
@@ -1,4 +1,10 @@
 primKbdNext
-	keyboardBuffer isEmpty
-		ifTrue:[^nil]
-		ifFalse:[^keyboardBuffer next]
+	"Allows for use of old Sensor protocol to get at the keyboard,
+	as when running kbdTest or the InterpreterSimulator in Morphic"
+	| evtBuf |
+	self fetchMoreEvents.
+	keyboardBuffer isEmpty ifFalse:[^ keyboardBuffer next].
+	eventQueue ifNotNil:
+		[evtBuf _ eventQueue nextOrNilSuchThat: [:buf | self isKbdEvent: buf].
+		self flushNonKbdEvents].
+	^ evtBuf ifNotNil: [evtBuf at: 3]
