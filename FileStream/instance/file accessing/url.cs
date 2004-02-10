646c8d33743e3664ee41ee14873c08f52b157186
gk <unknown>
2004-02-10 13:21:00 +0000

    no message

--- FileStream/instance/file accessing/url.cs
+++ FileStream/instance/file accessing/url.cs
@@ -1,10 +1,4 @@
 url
-	"Convert my path into a file:// type url.  Use slash instead of the local delimiter (:), and convert odd characters to %32 notation."
-
-	"If / is not the file system delimiter, encode / before converting."
-	| list |
-	list _ self directory pathParts.
-	^ String streamContents: [:strm |
-		strm nextPutAll: 'file:/'.
-		list do: [:each | strm nextPut: $/; nextPutAll: each encodeForHTTP].
-		strm nextPut: $/; nextPutAll: self localName encodeForHTTP]
+	"Convert my path into a file:// type url String."
+	
+	^self asUrl toText
