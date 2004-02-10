ac9a30941e65256a409c4fc9fca74faf3bd64621
ar <unknown>
2004-02-10 23:55:00 +0000

    no message

--- PNGReadWriter/instance/chunks/processIHDRChunk.cs
+++ PNGReadWriter/instance/chunks/processIHDRChunk.cs
@@ -7,18 +7,13 @@ processIHDRChunk
 	"filterMethod _ chunk at: 12." "TODO - validate filterMethod"
 	interlaceMethod _ chunk at: 13. "TODO - validate interlace method"
 	(#(2 4 6) includes: colorType)
-		ifTrue: [
-			depth _ 32.
-			form _ Form extent: width@height depth: depth
-			].
-	(#(0 3) includes: colorType)
-		ifTrue: [
-			depth _ bitsPerChannel min: 8.
-			form _ ColorForm extent: width@height depth: depth.
-			colorType = 0 ifTrue: [ "grayscale"
-				form colors: (self grayColorsFor: depth).
-				]
-			].
+		ifTrue: [depth _ 32].
+	(#(0 3) includes: colorType) ifTrue: [
+		depth _ bitsPerChannel min: 8.
+		colorType = 0 ifTrue: [ "grayscale"
+			palette := self grayColorsFor: depth.
+		].
+	].
 	bitsPerPixel _ (BPP at: colorType+1) at: bitsPerChannel highBit.
 	bytesPerScanline _ width * bitsPerPixel + 7 // 8.
-	rowSize _ form width * form depth + 31 >> 5.
+	rowSize _ width * depth + 31 >> 5.
