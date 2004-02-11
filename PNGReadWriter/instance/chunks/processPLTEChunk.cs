0e266c403058e06d4b4f17703cd41a4013650617
ar <unknown>
2004-02-11 01:02:00 +0000

    no message

--- PNGReadWriter/instance/chunks/processPLTEChunk.cs
+++ PNGReadWriter/instance/chunks/processPLTEChunk.cs
@@ -7,10 +7,5 @@ processPLTEChunk
 	0 to: colorCount-1 do: [ :index |
 		i _ index * 3 + 1.
 		palette at: index+1 put:
-			(Color r: (chunk at: i)/255 g: (chunk at: i+1)/255 b: (chunk at: i+2)/255)
+			(Color r: (chunk at: i)/255.0 g: (chunk at: i+1)/255.0 b: (chunk at: i+2)/255.0)
 		].
-	(form isKindOf: ColorForm) ifTrue: [
-		form colors: palette
-	] ifFalse: [
-		self flag: #bob.		"palette for true color image!!"
-	].
