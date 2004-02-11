7639e45679884e373bc3392c60d0e620a316cd21
tpr <unknown>
2004-02-11 20:33:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimPointX.cs
+++ Interpreter/instance/common selector sends/bytecodePrimPointX.cs
@@ -1,10 +1,12 @@
 bytecodePrimPointX
 
+	| rcvr |
 	successFlag _ true.
-	self externalizeIPandSP.
-	self primitivePointX.
-	self internalizeIPandSP.
-	successFlag ifTrue: [^ self fetchNextBytecode "success"].
+	rcvr _ self internalStackTop.
+	self assertClassOf: rcvr is: (self splObj: ClassPoint).
+	successFlag
+		ifTrue: [self internalPop: 1 thenPush: (self fetchPointer: XIndex ofObject: rcvr).
+			^ self fetchNextBytecode "success"].
 
 	messageSelector _ self specialSelector: 30.
 	argumentCount _ 0.
