b6b0446861c7d02cf8bcab2c43affda5111f8fa8
nk <unknown>
2004-02-14 18:19:00 +0000

    no message

--- PluggableTextMorph/instance/initialization/initialize.cs
+++ PluggableTextMorph/instance/initialization/initialize.cs
@@ -1,5 +1,6 @@
 initialize
+	"initialize the state of the receiver"
 	super initialize.
 	hasUnacceptedEdits _ false.
 	hasEditingConflicts _ false.
-	askBeforeDiscardingEdits _ true
+	askBeforeDiscardingEdits _ true.
