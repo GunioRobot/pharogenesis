24a9478a1082032fbd74ef80fcdef9e34ad9139c
nk <unknown>
2004-02-14 15:09:00 +0000

    no message

--- Browser/instance/class functions/plusButtonHit.cs
+++ Browser/instance/class functions/plusButtonHit.cs
@@ -4,16 +4,10 @@ plusButtonHit
 	editSelection == #editComment
 		ifTrue: [self hierarchy. ^ self].
 	editSelection == #hierarchy
-		ifTrue: [editSelection := #editClass.
+		ifTrue: [self editSelection: #editClass.
 			classListIndex = 0 ifTrue: [^ self].
 			self okToChange ifFalse: [^ self].
 			self changed: #editComment.
 			self contentsChanged.
-			^self].
-	classListIndex = 0 ifTrue: [^ self].
-	self okToChange ifFalse: [^ self].
-	self messageCategoryListIndex: 0.
-	editSelection := #editComment.
-	self changed: #classSelectionChanged.
-	self decorateButtons.
-	self contentsChanged.
+			^ self].
+	self editComment
