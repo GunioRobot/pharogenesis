33c522d32440eb8aceaf53c0d15813e024d626f3
yo <unknown>
2004-02-14 01:17:00 +0000

    no message

---
+++ HostFont/class/instance creation/fontName-3A-size-3A-emphasis-3A-rangesArray-3A-.cs
@@ -0,0 +1,5 @@
+fontName: fontName size: ptSize emphasis: emphasisCode rangesArray: ranges
+	"
+		^HostFont fontName: (HostFont fontNameFromUser) size: 12 emphasis: 0.
+	"
+	^self new fontName: fontName size: ptSize emphasis: emphasisCode rangesArray: ranges
