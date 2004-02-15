db2d7457bc8475fa30dabdaf12a64027fd35769d
nk <unknown>
2004-02-15 08:16:00 +0000

    no message

--- EventHandler/instance/initialization/on-3A-send-3A-to-3A-.cs
+++ EventHandler/instance/initialization/on-3A-send-3A-to-3A-.cs
@@ -1,26 +1,30 @@
 on: eventName send: selector to: recipient
-	eventName = #mouseDown ifTrue:
+	eventName == #mouseDown ifTrue:
 		[mouseDownRecipient _ recipient.  mouseDownSelector _ selector. ^ self].
-	eventName = #mouseMove ifTrue:
+	eventName == #mouseMove ifTrue:
 		[mouseMoveRecipient _ recipient.  mouseMoveSelector _ selector. ^ self].
-	eventName = #mouseStillDown ifTrue:
+	eventName == #mouseStillDown ifTrue:
 		[mouseStillDownRecipient _ recipient.  mouseStillDownSelector _ selector. ^ self].
-	eventName = #mouseUp ifTrue:
+	eventName == #mouseUp ifTrue:
 		[mouseUpRecipient _ recipient.  mouseUpSelector _ selector. ^ self].
-	eventName = #mouseEnter ifTrue:
+	eventName == #mouseEnter ifTrue:
 		[mouseEnterRecipient _ recipient.  mouseEnterSelector _ selector. ^ self].
-	eventName = #mouseLeave ifTrue:
+	eventName == #mouseLeave ifTrue:
 		[mouseLeaveRecipient _ recipient.  mouseLeaveSelector _ selector. ^ self].
-	eventName = #mouseEnterDragging ifTrue:
+	eventName == #mouseEnterDragging ifTrue:
 		[mouseEnterDraggingRecipient _ recipient.  mouseEnterDraggingSelector _ selector. ^ self].
-	eventName = #mouseLeaveDragging ifTrue:
+	eventName == #mouseLeaveDragging ifTrue:
 		[mouseLeaveDraggingRecipient _ recipient.  mouseLeaveDraggingSelector _ selector. ^ self].
-	eventName = #click ifTrue:
+	eventName == #click ifTrue:
 		[clickRecipient _ recipient. clickSelector _ selector. ^ self].
-	eventName = #doubleClick ifTrue:
+	eventName == #doubleClick ifTrue:
 		[doubleClickRecipient _ recipient. doubleClickSelector _ selector. ^ self].
-	eventName = #startDrag ifTrue:
+	eventName == #doubleClickTimeout ifTrue:
+		[doubleClickTimeoutRecipient _ recipient. doubleClickTimeoutSelector _ selector. ^ self].
+	eventName == #startDrag ifTrue:
 		[startDragRecipient _ recipient. startDragSelector _ selector. ^ self].
-	eventName = #keyStroke ifTrue:
+	eventName == #keyStroke ifTrue:
 		[keyStrokeRecipient _ recipient.  keyStrokeSelector _ selector. ^ self].
+	eventName == #gesture ifTrue:
+		[ ^self onGestureSend: selector to: recipient ].
 	self error: 'Event name, ' , eventName , ' is not recognizable.'
