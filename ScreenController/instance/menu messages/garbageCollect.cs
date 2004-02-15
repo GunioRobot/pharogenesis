e6579898bcb103d62add7065ec933359d9f4b108
nk <unknown>
2004-02-15 09:37:00 +0000

    no message

--- ScreenController/instance/menu messages/garbageCollect.cs
+++ ScreenController/instance/menu messages/garbageCollect.cs
@@ -1,11 +1,4 @@
 garbageCollect
 	"Do a garbage collection, and report results to the user."
 
-	| cc |
-	Smalltalk at: #Command ifPresent:
-		[:cmdClass |
-		(cc _ cmdClass instanceCount) > 0 ifTrue:
-			[(self confirm: 'There are ' , cc printString , ' undo records in your system.
-would you like to purge them prior to measuring space left?')
-				ifTrue: [CommandHistory resetAllHistory]]].
-	self inform: (Smalltalk bytesLeft asStringWithCommas, ' bytes available').
+	Utilities garbageCollectAndReport
