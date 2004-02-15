5b0eda49dca2abeea1ddde8fc280bccb4d572782
nk <unknown>
2004-02-15 08:46:00 +0000

    no message

--- WorldState/instance/update cycle/doOneCycleNowFor-3A-.cs
+++ WorldState/instance/update cycle/doOneCycleNowFor-3A-.cs
@@ -1,14 +1,26 @@
 doOneCycleNowFor: aWorld
-	"Do one cycle of the interactive loop. This method is called repeatedly when the world is running."
+	"Immediately do one cycle of the interaction loop.
+	This should not be called directly, but only via doOneCycleFor:"
 
+	| capturingGesture |
+	DisplayScreen checkForNewScreenSize.
+	capturingGesture _ false.
 	self flag: #bob.		"need to consider remote hands in lower worlds"
+
 	"process user input events"
 	LastCycleTime _ Time millisecondClockValue.
 	self handsDo: [:h |
-		activeHand _ h.
+		ActiveHand _ h.
 		h processEvents.
-		activeHand _ nil
+		capturingGesture _ capturingGesture or: [ h isCapturingGesturePoints ].
+		ActiveHand _ nil
 	].
 
-	aWorld runStepMethods.		"there are currently some variations here"
-	self displayWorldSafely: aWorld.
+	"the default is the primary hand"
+	ActiveHand _ self hands first.
+
+	"The gesture recognizer needs enough points to be accurate.
+	Therefore morph stepping is disabled while capturing points for the recognizer"
+	capturingGesture ifFalse: 
+		[aWorld runStepMethods.		"there are currently some variations here"
+		self displayWorldSafely: aWorld].
