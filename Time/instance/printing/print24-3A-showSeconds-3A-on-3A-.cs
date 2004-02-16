b14a78450c7c8d8a94f2f43dfc71e73e65cd74a5
brp <unknown>
2004-02-16 09:10:00 +0000

    no message

--- Time/instance/printing/print24-3A-showSeconds-3A-on-3A-.cs
+++ Time/instance/printing/print24-3A-showSeconds-3A-on-3A-.cs
@@ -2,35 +2,34 @@ print24: hr24 showSeconds: showSeconds on: aStream
 	"Format is 'hh:mm:ss' or 'h:mm:ss am'  or, if showSeconds is false, 'hh:mm' or 'h:mm am'"
 
 	| h m s |
-	h _ self hours. m _ self minutes. s _ self seconds.
+	h _ self hour. m _ self minute. s _ self second.
 	hr24
-	ifTrue:
-	[
-		h < 10 ifTrue: [ aStream nextPutAll: '0' ].
-		h printOn: aStream
-	]
+	
+	ifTrue: 
+			[ h < 10 ifTrue: [ aStream nextPutAll: '0' ].
+	
+		h printOn: aStream ]
+	
 	ifFalse:
-	[
-		h > 12
-		ifTrue: [ h - 12 printOn: aStream]
+			[ h > 12
+		
+		ifTrue: [h - 12 printOn: aStream]
+		
 		ifFalse: 
-		[
-			h < 1
+			
+		[h < 1
+		
 				ifTrue: [ 12 printOn: aStream ]
-				ifFalse: [ h printOn: aStream ]
-		]
-	].
+						ifFalse: [ h printOn: aStream ]]].
 
 	aStream nextPutAll: (m < 10 ifTrue: [':0'] ifFalse: [':']).
 	m printOn: aStream.
 
 	showSeconds ifTrue:
-	[
-		aStream nextPutAll: (s < 10 ifTrue: [':0'] ifFalse: [':']).
-		s printOn: aStream
-	].
+	
+	[ aStream nextPutAll: (s < 10 ifTrue: [':0'] ifFalse: [':']).
+		s asInteger printOn: aStream ].
 
 	hr24 ifFalse:
-	[
-		aStream nextPutAll: (h < 12 ifTrue: [' am'] ifFalse: [' pm'])
-	].
+	
+	[ aStream nextPutAll: (h < 12 ifTrue: [' am'] ifFalse: [' pm']) ].
