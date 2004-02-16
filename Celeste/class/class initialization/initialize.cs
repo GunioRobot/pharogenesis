0523d2462c16e50d9a1ce9279374dcb4fea02e18
ads <unknown>
2004-02-16 11:44:00 +0000

    no message

--- Celeste/class/class initialization/initialize.cs
+++ Celeste/class/class initialization/initialize.cs
@@ -2,16 +2,30 @@ initialize
 	"Celeste initialize"
 
 	"user preferences"
-	CCList _ nil.
-	DeleteInboxAfterFetching _ false.
-	PopServer _ nil.
-	PopUserName _ nil.
-	SmtpServer _ nil.
-	SuppressWorthlessHeaderFields _ true.
-	UserName _ nil.
+	DeleteInboxAfterFetching ifNil: [ DeleteInboxAfterFetching _ false ].
+	SuppressWorthlessHeaderFields ifNil: [ SuppressWorthlessHeaderFields _ true ].
+	UseScaffoldingInterface ifNil: [ UseScaffoldingInterface _ true ].
 
 	"options with no UI; just set their values directly"
-	FormatWhenFetching _ false.
+	FormatWhenFetching ifNil: [ FormatWhenFetching _ false ].
+	self flag: #celeste.  "get rid of this preference, I think!"
 
-	"dictionary of custom filters"
-	CustomFilters _ Dictionary new.
+	"dictionary of custom filters (obsolete, but left here for transitioning)"
+	CustomFilters ifNil: [ CustomFilters _ Dictionary new ].
+
+	"dictionary of named filters"
+	NamedFilters ifNil: [ NamedFilters := Dictionary new ].
+	NamedFilters isEmpty ifTrue: [
+		self addExampleFilters.
+		self upgradeCustomFilters ].
+
+	"Add global preferences"
+	self addPreferenceForOptionalCelesteStatusPane.
+	self addPreferenceForCelesteShowingAttachmentsFlag.
+	self addPreferenceForCelesteDespam.
+
+	"add Celeste to the startup/shutdown regime"
+	Smalltalk addToStartUpList: self.
+
+	self registerInOpenMenu.
+	self registerInFlapsRegistry.
