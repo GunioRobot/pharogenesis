5b12093a47e0745f0e2bb9cdab1eca9b3918796f
ads <unknown>
2004-02-16 11:15:00 +0000

    no message

--- Celeste/instance/open-2D-close/close.cs
+++ Celeste/instance/open-2D-close/close.cs
@@ -1,7 +1,7 @@
 close
 	"Close the mail database."
 
-	userPassword _ nil.
-	mailDB ifNotNil: [
-		mailDB close; release.
-		mailDB _ nil].
+	self account clearPasswords.
+	mailDB ifNotNil: 
+			[mailDB close.
+			mailDB := nil]
