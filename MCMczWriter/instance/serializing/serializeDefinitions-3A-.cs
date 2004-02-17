0c1145f95b8e6fd8c514da71b76564582347b3c8
avi <unknown>
2004-02-17 02:18:00 +0000

    no message

---
+++ MCMczWriter/instance/serializing/serializeDefinitions-3A-.cs
@@ -0,0 +1,6 @@
+serializeDefinitions: aCollection
+	| writer s |
+	s _ RWBinaryOrTextStream on: String new.
+	writer _ self snapshotWriterClass on: s.
+	writer writeDefinitions: aCollection.
+	^ s contents
