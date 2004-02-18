2112ecf0a852e0830601bfab4092b53db220e1ca
ar <unknown>
2004-02-18 23:58:00 +0000

    no message

--- PNGReadWriter/instance/pixel copies/copyPixelsRGBA-3A-.cs
+++ PNGReadWriter/instance/pixel copies/copyPixelsRGBA-3A-.cs
@@ -1,30 +1,29 @@
 copyPixelsRGBA: y
 	"Handle non-interlaced RGBA color modes (colorType = 6)"
 
-	| i pixel tempForm tempBits |
-
+	| i pixel tempForm tempBits ff |
+	bitsPerChannel = 8 ifTrue: [
+		ff := Form extent: width@1 depth: 32 bits: thisScanline.
+		cachedDecoderMap 
+			ifNil:[cachedDecoderMap := self rgbaDecoderMapForDepth: depth].
+		(BitBlt toForm: form)
+			sourceForm: ff;
+			destOrigin: 0@y;
+			combinationRule: Form over;
+			colorMap: cachedDecoderMap;
+			copyBits.
+		^self.
+	].
 	tempForm _ Form extent: width@1 depth: 32.
 	tempBits _ tempForm bits.
 	pixel := LargePositiveInteger new: 4.
-	bitsPerChannel = 8 ifTrue: [
-		i := -3.
-		0 to: width-1 do: [ :x |
-			i := i + 4.
-			pixel at: 4 put: (thisScanline at: i+3);
-				at: 3 put: (thisScanline at: i);
-				at: 2 put: (thisScanline at: i+1);
-				at: 1 put: (thisScanline at: i+2).
-			tempBits at: x+1 put: pixel.
-		]
-	] ifFalse: [
-		i := -7.
-		0 to: width-1 do: [ :x |
+	i := -7.
+	0 to: width-1 do: [ :x |
 			i := i + 8.
 			pixel at: 4 put: (thisScanline at: i+6);
 				at: 3 put: (thisScanline at: i);
 				at: 2 put: (thisScanline at: i+2);
 				at: 1 put: (thisScanline at: i+4).
 			tempBits at: x+1 put: pixel.
-		]
 	].
-	tempForm displayOn: form at: 0@y rule: Form paint.
+	tempForm displayOn: form at: 0@y rule: Form over.
