473c2e7b608b8da01bd198862dac6f940da0df5b
fc <unknown>
2004-02-19 22:07:00 +0000

    no message

--- EmphasizedMenu/instance/emphasis/onlyBoldItem-3A-.cs
+++ EmphasizedMenu/instance/emphasis/onlyBoldItem-3A-.cs
@@ -1,5 +1,5 @@
 onlyBoldItem: itemNumber
 	"Set up emphasis such that all items are plain except for the given item number.  "
 
-	emphases _ (Array new: selections size) atAllPut: #plain.
+	emphases _ (Array new: selections size) atAllPut: #normal.
 	emphases at: itemNumber put: #bold
