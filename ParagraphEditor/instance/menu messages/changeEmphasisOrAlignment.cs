b305acf6f91b59d6e8f9ec0d623c5fe29e8e2976
fc <unknown>
2004-02-19 22:09:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/changeEmphasisOrAlignment.cs
+++ ParagraphEditor/instance/menu messages/changeEmphasisOrAlignment.cs
@@ -1,6 +1,6 @@
 changeEmphasisOrAlignment
 	| aList reply  |
-	aList _ #(plain bold italic narrow underlined struckOut leftFlush centered rightFlush justified).
+	aList _ #(normal bold italic narrow underlined struckOut leftFlush centered rightFlush justified).
 	reply _ (SelectionMenu labelList: aList lines: #(6) selections: aList) startUp.
 	reply ~~ nil ifTrue:
 		[(#(leftFlush centered rightFlush justified) includes: reply)
