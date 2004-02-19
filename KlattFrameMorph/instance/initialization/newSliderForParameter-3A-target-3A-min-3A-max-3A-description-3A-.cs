89e07c78f2e9d012f45743d0ebd84a008ee02c52
nk <unknown>
2004-02-19 16:58:00 +0000

    no message

--- KlattFrameMorph/instance/initialization/newSliderForParameter-3A-target-3A-min-3A-max-3A-description-3A-.cs
+++ KlattFrameMorph/instance/initialization/newSliderForParameter-3A-target-3A-min-3A-max-3A-description-3A-.cs
@@ -2,7 +2,7 @@ newSliderForParameter: parameter target: target min: min max: max description: d
 	| r slider m |
 	r _ AlignmentMorph newRow.
 	r color: self color; borderWidth: 0; layoutInset: 0.
-	r hResizing: #spaceFill; vResizing: #rigid; extent: 5@20; wrapCentering: #center; cellPositioning: #leftCenter.
+	r hResizing: #spaceFill; vResizing: #shrinkWrap; extent: 5@20; wrapCentering: #center; cellPositioning: #leftCenter; cellInset: 4@0.
 
 	slider _ SimpleSliderMorph new
 		color: (Color r: 0.065 g: 0.548 b: 0.645);
@@ -14,11 +14,11 @@ newSliderForParameter: parameter target: target min: min max: max description: d
 		adjustToValue: (target perform: parameter asSymbol).
 	r addMorphBack: slider.
 
-	m _ StringMorph new contents: parameter, ': '.
+	m _ StringMorph new contents: parameter, ': '; hResizing: #rigid.
 	r addMorphBack: m.
 	m _ UpdatingStringMorph new
 		target: target; getSelector: parameter asSymbol; putSelector: (parameter, ':') asSymbol;
-		width: 30; growable: false; floatPrecision: (max - min / 100.0 min: 1.0); step.
+		width: 60; growable: false; floatPrecision: (max - min / 100.0 min: 1.0); vResizing: #spaceFill; step.
 	r addMorphBack: m.
 	r setBalloonText: description.
 	^ r
