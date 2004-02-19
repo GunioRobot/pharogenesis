3edaa5dbf47ec87b4f7db8647bedae3f82013630
nk <unknown>
2004-02-19 16:50:00 +0000

    no message

--- Speaker/instance/playing/say-3A-.cs
+++ Speaker/instance/playing/say-3A-.cs
@@ -1,10 +1,30 @@
-say: aString
-	| events stream string |
-	events _ CompositeEvent new.
-	stream _ ReadStream on: (aString findTokens: '?' keep: '?').
+say: aString 
+	"aString may contain characters and punctuation.
+	You may also include the # symbol in aString;
+	for each one of these, a 200msec delay will be generated."
+
+	| events stream string token delay |
+
+	stream := ReadStream
+				on: ((aString
+						copyReplaceAll: '-'
+						with: ' '
+						asTokens: false)
+						findTokens: '?# '
+						keep: '?#').
+	string := ''.
+	delay := 0.
 	[stream atEnd]
-		whileFalse: [string _ stream next.
-					stream atEnd ifFalse: [string _ string, stream next].
-					events addAll: (self eventsFromString: string)].
-	events playOn: self voice delayed: events duration * 1000.
+		whileFalse: [token := stream next.
+			token = '#'
+				ifTrue: [ self voice playSilenceMSecs: self numberSignDelay.
+					delay := delay + self numberSignDelay ]
+				ifFalse: [string := string , ' ' , token.
+					(token = '?' or: [stream atEnd])
+						ifTrue: [
+							events := CompositeEvent new.
+							events addAll: (self eventsFromString: string).
+							events playOn: self voice delayed: delay.
+							delay := delay + (events duration * 1000).
+							string _ ''  ]]].
 	self voice flush
