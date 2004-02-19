3a92025ce2734de649053d03fb8db154971c7d09
fc <unknown>
2004-02-19 22:08:00 +0000

    no message

--- EmphasizedMenu/class/examples/example3.cs
+++ EmphasizedMenu/class/examples/example3.cs
@@ -2,5 +2,5 @@ example3
 	"EmphasizedMenu example3"
 
 	^ (self
-		selectionAndEmphasisPairs: #('how' #bold 'well' #plain 'does' #italic 'this' #struckOut 'work' #plain))
+		selectionAndEmphasisPairs: #('how' #bold 'well' #normal 'does' #italic 'this' #struckOut 'work' #normal))
 		startUpWithCaption: 'A Menu with Emphases'
