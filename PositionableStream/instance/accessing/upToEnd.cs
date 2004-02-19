62dd96c83def34a69b42607d36dccc54d5b44cf0
BG <unknown>
2004-02-19 14:06:00 +0000

    no message

--- PositionableStream/instance/accessing/upToEnd.cs
+++ PositionableStream/instance/accessing/upToEnd.cs
@@ -1,7 +1,7 @@
 upToEnd
 	"Answer a subcollection from the current access position through the last element of the receiver."
 
-	| newStream el |
+	| newStream |
 	newStream _ WriteStream on: (collection species new: 100).
-	[(el _ self next) == nil] whileFalse: [ newStream nextPut: el ].
+	[self atEnd] whileFalse: [ newStream nextPut: self next ].
 	^ newStream contents
