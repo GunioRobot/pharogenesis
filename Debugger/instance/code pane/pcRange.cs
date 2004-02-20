2e7b6211b9f95028a6599ad1ee486479f3947107
nk <unknown>
2004-02-20 15:35:00 +0000

    no message

--- Debugger/instance/code pane/pcRange.cs
+++ Debugger/instance/code pane/pcRange.cs
@@ -2,19 +2,26 @@ pcRange
 	"Answer the indices in the source code for the method corresponding to 
 	the selected context's program counter value."
 
-	| i methodNode pc end |
+	| i pc end |
 	(selectingPC and: [contextStackIndex ~= 0])
 		ifFalse: [^1 to: 0].
-	sourceMap == nil ifTrue:
-		[methodNode _ self selectedClass compilerClass new
-			parse: contents in: self selectedClass
-			notifying: nil dialect: true.
-		sourceMap _ methodNode sourceMap.
-		tempNames _ methodNode tempNames.
+	sourceMap ifNil:
+		[sourceMap _ theMethodNode sourceMap.
+		tempNames _ theMethodNode tempNames.
 		self selectedContext method cacheTempNames: tempNames].
-	sourceMap size = 0 ifTrue: [^1 to: 0].
+	(sourceMap size = 0 or: [ self selectedContext isDead ]) ifTrue: [^1 to: 0].
+	Smalltalk at: #RBProgramNode ifPresent:[:nodeClass|
+		(theMethodNode isKindOf: nodeClass) ifTrue: [
+			pc _ contextStackIndex = 1
+				ifTrue: [self selectedContext pc]
+				ifFalse: [self selectedContext previousPc].
+			i _ sourceMap findLast:[:pcRange | pcRange key <= pc].
+			i = 0 ifTrue:[^ 1 to: 0].
+			^ (sourceMap at: i) value
+		].
+	].
 	pc_ self selectedContext pc -
-		((externalInterrupt and: [contextStackIndex=1])
+		(("externalInterrupt" true and: [contextStackIndex=1])
 			ifTrue: [1]
 			ifFalse: [2]).
 	i _ sourceMap indexForInserting: (Association key: pc value: nil).
