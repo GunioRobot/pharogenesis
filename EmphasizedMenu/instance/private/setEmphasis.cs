3615067d716673801f76b70fb7fff8553b15a074
fc <unknown>
2004-02-20 11:01:00 +0000

    no message

--- EmphasizedMenu/instance/private/setEmphasis.cs
+++ EmphasizedMenu/instance/private/setEmphasis.cs
@@ -2,13 +2,14 @@ setEmphasis
 	"Set up the receiver to reflect the emphases in the emphases array.  "
 
 	| selStart selEnd currEmphasis |
+	
 	labelString _ labelString asText.
 	emphases isEmptyOrNil ifTrue: [^ self].
 	selStart _ 1.
 	1 to: selections size do:
 		[:line |
 			selEnd _ selStart + (selections at: line) size - 1.
-			((currEmphasis _ emphases at: line) size > 0 and: [currEmphasis ~~ #plain]) ifTrue:
+			((currEmphasis _ emphases at: line) size > 0 and: [currEmphasis ~~ #normal]) ifTrue:
 				[labelString addAttribute: (TextEmphasis perform: currEmphasis)
 					from: selStart to: selEnd].
 			selStart _ selEnd + 2]
