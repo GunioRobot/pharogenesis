5a7ce4d6281051fbf55b4d674933262f33c1f42c
tpr <unknown>
2004-02-21 19:27:00 +0000

    no message

--- CCodeGenerator/instance/C code generator/emitCCodeOn-3A-doInlining-3A-doAssertions-3A-.cs
+++ CCodeGenerator/instance/C code generator/emitCCodeOn-3A-doInlining-3A-doAssertions-3A-.cs
@@ -14,10 +14,17 @@ emitCCodeOn: aStream doInlining: inlineFlag doAssertions: assertionFlag
 	self doInlining: inlineFlag.
 
 	"code generation"
+
 	methodList _ methods asSortedCollection: [ :m1 :m2 | m1 selector < m2 selector ].
+	"clean out no longer valid variable names and then handle any global
+		variable usage in each method"
+	methodList do: [:m | self checkForGlobalUsage: m removeUnusedTemps in: m].
+	self localizeGlobalVariables.
+
 	self emitCHeaderOn: aStream.
-	self emitCVariablesOn: aStream.
+	self emitCConstantsOn: aStream.
 	self emitCFunctionPrototypes: methodList on: aStream.
+	self emitCVariablesOn: aStream.
 'Writing Translated Code...'
 displayProgressAt: Sensor cursorPoint
 from: 0 to: methods size
@@ -25,3 +32,4 @@ during: [:bar |
 	methodList doWithIndex: [ :m :i | bar value: i.
 		m emitCCodeOn: aStream generator: self.
 ]].
+	self emitExportsOn: aStream.
