b9f6422bf7b49112cdf15328ac66a50d79e22358
gk <unknown>
2004-02-23 20:51:00 +0000

    no message

--- SystemDictionary/instance/snapshot and quit/shutDownSound.cs
+++ SystemDictionary/instance/snapshot and quit/shutDownSound.cs
@@ -1,4 +1,4 @@
 shutDownSound
-	"No longer used in the release, but retained for backward compatibility"
+	"No longer used in the release, but retained for backward compatibility."
 
-	Smalltalk at: #SoundPlayer ifPresent: [:class | class shutDown].
+	SoundService default shutDown
