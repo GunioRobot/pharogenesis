08cf8f6cf2249287245ccefe18fbc13157167255
tpr <unknown>
2004-02-24 20:29:00 +0000

    no message

--- CCodeGenerator/instance/inlining/doInlining-3A-.cs
+++ CCodeGenerator/instance/inlining/doInlining-3A-.cs
@@ -1,33 +1,19 @@
 doInlining: inlineFlag
 	"Inline the bodies of all methods that are suitable for inlining."
-	"Modified slightly for the translator, since the first level of inlining for the interpret loop must be performed in order that the instruction implementations can easily discover their addresses."
+	"Modified slightly for the core VM translator, since the first level of inlining for the interpret loop must be performed in order that the instruction implementations can easily discover their addresses. Remember to inline the bytecode routines as well"
 
-	| pass progress |
 	inlineFlag ifFalse: [
 		self inlineDispatchesInMethodNamed: #interpret localizingVars: #().
 		^ self].
 
-	self collectInlineList.
-	pass _ 0.
-	progress _ true.
-	[progress] whileTrue: [
-		"repeatedly attempt to inline methods until no further progress is made"
-		progress _ false.
-		('Inlining pass ', (pass _ pass + 1) printString, '...')
-			displayProgressAt: Sensor cursorPoint
-			from: 0 to: methods size
-			during: [:bar |
-				methods doWithIndex: [:m :i |
-					bar value: i.
-					(m tryToInlineMethodsIn: self)
-						ifTrue: [progress _ true]]]].
-
+	self doBasicInlining: inlineFlag.
+	
 	'Inlining bytecodes'
 		displayProgressAt: Sensor cursorPoint
 		from: 1 to: 2
 		during: [:bar |
 			self inlineDispatchesInMethodNamed: #interpret
-				localizingVars: #(currentBytecode localIP localSP localHomeContext).
+				localizingVars: #(currentBytecode localIP localSP localHomeContext localReturnContext localReturnValue).
 			bar value: 1.
 			self removeMethodsReferingToGlobals: #(
 					currentBytecode localIP localSP localHomeContext)
