72462b50bcb70e4e0e731651141426bfe9aaff0e
gk <unknown>
2004-02-24 08:49:00 +0000

    no message

--- Object/instance/user interface/beep.cs
+++ Object/instance/user interface/beep.cs
@@ -1,4 +1,5 @@
 beep
-	"Make a beep sound.  Every object would like to be able to speak.  6/10/96 sw"
-
-	Smalltalk beep
+	"Deprecated."
+	
+	self deprecated: 'Use Beeper class>>beep instead.'.
+	Beeper beep
