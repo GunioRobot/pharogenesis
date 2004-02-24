324e53a42d045e4bc882d49b99f3da44cad402c1
gk <unknown>
2004-02-24 08:27:00 +0000

    no message

--- BooklikeMorph/instance/misc/playPageFlipSound-3A-.cs
+++ BooklikeMorph/instance/misc/playPageFlipSound-3A-.cs
@@ -1,5 +1,4 @@
 playPageFlipSound: soundName
 	self presenter ifNil: [^ self].  "Avoid failures when called too early"
-	(Preferences soundsEnabled "user-controllable" and:
-		[PageFlipSoundOn])  "mechanism to suppress sounds at init time"
+	PageFlipSoundOn  "mechanism to suppress sounds at init time"
 			ifTrue: [self playSoundNamed: soundName].
