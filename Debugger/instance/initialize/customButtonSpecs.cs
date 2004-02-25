43653ab5b5cecea6b44c69afddad6c2c042155fd
ab <unknown>
2004-02-25 18:59:00 +0000

    no message

---
+++ Debugger/instance/initialize/customButtonSpecs.cs
@@ -0,0 +1,17 @@
+customButtonSpecs
+	"Answer an array of elements of the form wording, selector, help-message, that characterize the custom button row of a debugger."
+
+	| list |
+	list _ #(('Proceed'	proceed				'close the debugger and proceed.')
+		('Restart'		restart				'reset this context to its start.')
+		('Into'			send				'step Into message sends')
+		('Over'			doStep				'step Over message sends')
+		('Through'		stepIntoBlock		'step into a block')
+		('Full Stack'		fullStack			'show full stack')
+		('Where'		where				'select current pc range')).
+	Preferences restartAlsoProceeds ifTrue:
+		[list _ list collect: [:each |
+			each second == #restart
+				ifTrue: [each copy at: 3 put: 'proceed from the beginning of this context.'; yourself]
+				ifFalse: [each]]].
+	^ list
