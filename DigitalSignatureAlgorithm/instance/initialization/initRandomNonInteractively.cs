7090a51093b3ff62f1b2c21a264f28c260df4f53
gk <unknown>
2004-02-26 09:52:00 +0000

    no message

---
+++ DigitalSignatureAlgorithm/instance/initialization/initRandomNonInteractively.cs
@@ -0,0 +1,6 @@
+initRandomNonInteractively
+	[self initRandom: (SoundService default randomBitsFromSoundInput: 512)]
+		ifError: [self initRandomFromString: 
+			Time millisecondClockValue printString, 
+			Date today printString, 
+			SmalltalkImage current platformName printString].
