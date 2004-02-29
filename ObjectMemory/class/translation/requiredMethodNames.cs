4cfb3c7c01ad4f5decb0d903ebb32b488c7b46ab
tpr <unknown>
2004-02-29 19:51:00 +0000

    no message

---
+++ ObjectMemory/class/translation/requiredMethodNames.cs
@@ -0,0 +1,3 @@
+requiredMethodNames
+	"return the list of method names that should be retained for export or other support reasons"
+	^ #(checkedLongAt: allocateChunk: firstAccessibleObject noteAsRoot:headerLoc: splObj:)
