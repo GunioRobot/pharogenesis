af4bc25888ed4a5f41a0f2860473763e3baf9c39
ar <unknown>
2004-02-29 03:31:00 +0000

    no message

--- ZLibReadStream/instance/initialize/on-3A-from-3A-to-3A-.cs
+++ ZLibReadStream/instance/initialize/on-3A-from-3A-to-3A-.cs
@@ -2,6 +2,7 @@ on: aCollection from: firstIndex to: lastIndex
 	"Check the header of the ZLib stream."
 	| method byte |
 	super on: aCollection from: firstIndex to: lastIndex.
+	crc _ 1.
 	method _ self nextBits: 8.
 	(method bitAnd: 15) = 8 ifFalse:[^self error:'Unknown compression method'].
 	(method bitShift: -4) + 8 > 15 ifTrue:[^self error:'Invalid window size'].
