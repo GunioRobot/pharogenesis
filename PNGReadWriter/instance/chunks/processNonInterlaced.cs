a81ceae0e62106ea9227959e89b2f1efec5aa842
ar <unknown>
2004-02-29 04:19:00 +0000

    no message

--- PNGReadWriter/instance/chunks/processNonInterlaced.cs
+++ PNGReadWriter/instance/chunks/processNonInterlaced.cs
@@ -1,17 +1,22 @@
 processNonInterlaced
-	| z filter temp |
+	| z filter temp copyMethod debug |
+	debug := self debugging.
+	copyMethod _ #(copyPixelsGray: nil copyPixelsRGB: copyPixelsIndexed:
+		  copyPixelsGrayAlpha: nil copyPixelsRGBA:) at: colorType+1.
+	debug ifTrue: [ Transcript cr; nextPutAll: 'NI chunk size='; print: chunk size ].
 	z _ ZLibReadStream on: chunk from: 1 to: chunk size.
 	prevScanline _ ByteArray new: bytesPerScanline.
 	thisScanline := ByteArray new: bytesPerScanline.
 	0 to: height-1 do: [ :y |
 		filter _ (z next: 1) first.
-		filtersSeen add: filter.
-		(filter isNil or: [(filter between: 0 and: 4) not])
-			ifTrue: [^ self].
+		debug ifTrue:[filtersSeen add: filter].
 		thisScanline _ z next: bytesPerScanline into: thisScanline startingAt: 1.
-		self filterScanline: filter count: bytesPerScanline.
-		self copyPixels: y.
+		(debug and: [ thisScanline size < bytesPerScanline ]) ifTrue: [ Transcript nextPutAll: ('wanted {1} but only got {2}' format: { bytesPerScanline. thisScanline size }); cr ].
+		filter = 0 ifFalse:[self filterScanline: filter count: bytesPerScanline].
+		self perform: copyMethod with: y.
 		temp := prevScanline.
 		prevScanline := thisScanline.
 		thisScanline := temp.
-		]
+		].
+	z atEnd ifFalse:[self error:'Unexpected data'].
+	debug ifTrue: [Transcript  nextPutAll: ' compressed size='; print: z position  ].
