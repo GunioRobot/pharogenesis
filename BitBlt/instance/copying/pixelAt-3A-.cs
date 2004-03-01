7cd014d0404397abb858e36d8c77e14b02394da3
ar <unknown>
2004-03-01 13:49:00 +0000

    no message

--- BitBlt/instance/copying/pixelAt-3A-.cs
+++ BitBlt/instance/copying/pixelAt-3A-.cs
@@ -3,6 +3,7 @@ pixelAt: aPoint
 	BitBlt bitPeekerFromForm:.  Returns the pixel at aPoint."
 	sourceX _ aPoint x.
 	sourceY _ aPoint y.
+	destForm unhibernate. "before poking"
 	destForm bits at: 1 put: 0.  "Just to be sure"
 	self copyBits.
 	^ destForm bits at: 1
