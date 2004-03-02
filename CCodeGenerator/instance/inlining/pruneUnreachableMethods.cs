e33e789755b425835248f88e3dfec0c51543b144
tpr <unknown>
2004-03-02 11:09:00 +0000

    no message

--- CCodeGenerator/instance/inlining/pruneUnreachableMethods.cs
+++ CCodeGenerator/instance/inlining/pruneUnreachableMethods.cs
@@ -1,27 +1,14 @@
 pruneUnreachableMethods
-	"Remove any methods that are not reachable. Retain methods needed by the BitBlt operation table, primitives, plug-ins, or interpreter support code."
- 
-	| retain |
-	"Build a set of selectors for methods that should be output even though they have no apparent callers. Some of these are stored in tables for indirect lookup, some are called by the C support code or by primitives."
-	retain _ BitBltSimulation opTable asSet.
-	#(checkedLongAt: fullDisplayUpdate interpret printCallStack
-	   readImageFromFile:HeapSize:StartingAt: success:
-		"Windows needs the following two for startup and debug"
-	   readableFormat: getCurrentBytecode
-		"Jitter reuses all of these"
-	   allocateChunk: characterForAscii:
-	   findClassOfMethod:forReceiver: findSelectorOfMethod:forReceiver:
-	   firstAccessibleObject loadInitialContext noteAsRoot:headerLoc:
-	   nullCompilerHook
-	   primitiveFloatAdd primitiveFloatDivide primitiveFloatMultiply
-	   primitiveFloatSubtract primitiveFlushExternalPrimitives
-	   setCompilerInitialized: splObj:)
-			do: [:sel | retain add: sel].
-	InterpreterProxy organization categories do: [:cat |
-		((cat ~= 'initialize') and: [cat ~= 'private']) ifTrue: [
-			retain addAll: (InterpreterProxy organization listAtCategoryNamed: cat)]].
+	"Remove any methods that are not reachable. Retain methods needed by the translated classes - see implementors of requiredMethodNames"
+ 	
+	| newMethods |
+	"add all the exported methods and all the called methods to the dNRML"
+	methods do: [ :m |
+		m export ifTrue:[doNotRemoveMethodList add: m selector].
+		doNotRemoveMethodList addAll: m allCalls].
 
-	"Remove all the unreachable methods that aren't retained for the reasons above."
-	self unreachableMethods do: [:sel |
-		(retain includes: sel) ifFalse: [
-			methods removeKey: sel ifAbsent: []]].
+	"build a new dictionary of methods from the collection of all the ones to keep"			
+	newMethods _ Dictionary new: doNotRemoveMethodList size.
+	doNotRemoveMethodList do:[:sel|
+		methods at: sel ifPresent:[:meth| newMethods at: sel put: meth]].
+	methods _ newMethods
