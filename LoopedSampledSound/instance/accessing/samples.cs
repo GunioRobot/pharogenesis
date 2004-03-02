f1c263c7516514aee143265faeb896abe917435f
zz <unknown>
2004-03-02 08:18:00 +0000

    no message

--- LoopedSampledSound/instance/accessing/samples.cs
+++ LoopedSampledSound/instance/accessing/samples.cs
@@ -1,4 +1,4 @@
 samples
-	"For compatability with SampledSound. Just return my left channel (which is the only channel if I am mono)."
+	"For compatibility with SampledSound. Just return my left channel (which is the only channel if I am mono)."
 
 	^ leftSamples
