a6f57f0c5e89930c298ed2a56a49b67472b7dab8
sw <unknown>
2004-03-02 23:53:00 +0000

    no message

--- CategoryViewer/instance/categories/chooseCategory.cs
+++ CategoryViewer/instance/categories/chooseCategory.cs
@@ -1,11 +1,16 @@
 chooseCategory
-	"The mouse went down on the receiver; pop up a list of category choices"
+	"The mouse went down on my category-list control; pop up a list of category choices"
 
 	| aList aMenu reply aLinePosition lineList |
 	aList _ scriptedPlayer categoriesForViewer: self.
+
 	aLinePosition _ aList indexOf: #miscellaneous ifAbsent: [nil].
+	aList _ aList collect:	
+		[:aCatSymbol | self currentVocabulary categoryWordingAt: aCatSymbol].
+
 	lineList _ aLinePosition ifNil: [#()] ifNotNil: [Array with: aLinePosition].
+	aList size == 0 ifTrue: [aList add: ScriptingSystem nameForInstanceVariablesCategory translated].
 	aMenu _ CustomMenu labels: aList lines: lineList selections: aList.
-	reply _ aMenu startUpWithCaption: 'category'.
+	reply _ aMenu startUpWithCaption: 'category' translated.
 	reply ifNil: [^ self].
-	self categoryChoice: reply asSymbol
+	self chooseCategoryWhoseTranslatedWordingIs: reply asSymbol
