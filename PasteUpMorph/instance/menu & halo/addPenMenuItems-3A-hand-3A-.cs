2767bbd0348707803cfd3ded2f2807ca4a8de67c
sw <unknown>
2004-03-03 01:14:00 +0000

    no message

--- PasteUpMorph/instance/menu & halo/addPenMenuItems-3A-hand-3A-.cs
+++ PasteUpMorph/instance/menu & halo/addPenMenuItems-3A-hand-3A-.cs
@@ -1,7 +1,4 @@
 addPenMenuItems: menu hand: aHandMorph
 	"Add a pen-trails-within submenu to the given menu"
 
-	| subMenu |
-	subMenu _ MenuMorph new defaultTarget: self.
-	self addPenTrailsMenuItemsTo: subMenu.
-	menu add: 'pens trails within...' subMenu: subMenu
+	menu add: 'penTrails within...' translated target: self action: #putUpPenTrailsSubmenu
