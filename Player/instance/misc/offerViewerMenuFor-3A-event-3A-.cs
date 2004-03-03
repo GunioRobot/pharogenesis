dd67c9a21317e445079137e12c62cd14449cbe2a
sw <unknown>
2004-03-03 00:21:00 +0000

    no message

--- Player/instance/misc/offerViewerMenuFor-3A-event-3A-.cs
+++ Player/instance/misc/offerViewerMenuFor-3A-event-3A-.cs
@@ -1,26 +1,29 @@
 offerViewerMenuFor: aViewer event: evt
-	"Put up the Viewer menu on behalf of the receiver."
+	"Put up the Viewer menu on behalf of the receiver.  If the shift key is held down, put up the alternate menu. The menu omits the 'add a new variable' item when in eToyFriendly mode, as per request from teachers using Squeakland in 2003 once the button for adding a new variable was added to the viewer"
 
 	| aMenu aWorld  |
+	(evt notNil and: [evt shiftPressed]) ifTrue:
+		[^ self offerAlternateViewerMenuFor: aViewer event: evt].
+
 	aWorld _ aViewer world.
 	aMenu _ MenuMorph new defaultTarget: self.
-	costumes ifNotNil:
-		[(costumes size > 1 or: [costumes size == 1 and: [costumes first ~~ costume renderedMorph]])
-			ifTrue:
-				[aMenu add: 'forget other costumes' target: self selector: #forgetOtherCostumes]].
+	Preferences eToyFriendly ifFalse: "exclude this from squeakland-like UI "
+		[aMenu add: 'add a new variable' translated target: self action: #addInstanceVariable.
+		aMenu balloonTextForLastItem: 'Add a new variable to this object and all of its siblings.  You will be asked to supply a name for it.' translated].
+
+	aMenu add: 'add a new script' translated target: aViewer action: #newPermanentScript.
+	aMenu balloonTextForLastItem: 'Add a new script that will work for this object and all of its siblings' translated.
 	aMenu addLine.
-	aMenu add: 'add a new instance variable' target: self action: #addInstanceVariable.
-	aMenu add: 'add a new script' target: aViewer action: #newPermanentScript.
-	aMenu add: 'expunge empty scripts' target: self action: #expungeEmptyScripts.
+	aMenu add: 'grab me' translated target: self selector: #grabPlayerIn: argument: aWorld.
+	aMenu balloonTextForLastItem: 'This will actually pick up the object this Viewer is looking at, and hand it to you.  Click the (left) button to drop it' translated.
+
+	aMenu add: 'reveal me' translated target: self selector: #revealPlayerIn: argument: aWorld.
+	aMenu balloonTextForLastItem: 'If you have misplaced the object that this Viewer is looking at, use this item to (try to) make it visible' translated.
+
 	aMenu addLine.
-	aMenu add: 'tile representing me' action: #tearOffTileForSelf.
-	aMenu add: 'reveal me' target: self selector: #revealPlayerIn: argument: aWorld.
-	aMenu add: 'grab me' target: self selector: #grabPlayerIn: argument: aWorld.
+	aMenu add: 'tile representing me' translated action: #tearOffTileForSelf.
+	aMenu add: 'add search pane' translated target: aViewer action: #addSearchPane.
 	aMenu addLine.
-	aMenu add: 'inspect morph' target: costume selector: #inspect.
-	aMenu add: 'inspect player' target: self selector: #inspect.
-	self belongsToUniClass ifTrue:
-		[aMenu add: 'browse class' target: self action: #browsePlayerClass.
-		aMenu add: 'inspect class' target: self class action: #inspect].
+	aMenu add: 'more...' translated target: self selector: #offerAlternateViewerMenuFor:event: argumentList: {aViewer. evt}.
 
 	aMenu popUpEvent: evt in: aWorld
