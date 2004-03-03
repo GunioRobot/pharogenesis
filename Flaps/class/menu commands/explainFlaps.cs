182620bd19f00c0c2d116635ef9e54d91b48d34e
sw <unknown>
2004-03-03 15:49:00 +0000

    no message

---
+++ Flaps/class/menu commands/explainFlaps.cs
@@ -0,0 +1,12 @@
+explainFlaps
+	"Open a window giving flap help."
+
+	(StringHolder new contents: self explainFlapsText translated)
+		openLabel: 'Flaps' translated
+
+"Flaps explainFlaps"
+
+
+
+
+	
