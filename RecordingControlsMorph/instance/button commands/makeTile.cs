c5e21bd9b2d5a5899b5f01ff49bdc8d7ede0cb86
sw <unknown>
2004-03-03 19:49:00 +0000

    no message

--- RecordingControlsMorph/instance/button commands/makeTile.cs
+++ RecordingControlsMorph/instance/button commands/makeTile.cs
@@ -1,13 +1,15 @@
 makeTile
+	"Make a tile representing my sound.  Get a sound-name from the user by which the sound is to be known."
 
 	| newStyleTile sndName tile |
+	recorder verifyExistenceOfRecordedSound ifFalse: [^ self].
 	recorder pause.
 	newStyleTile _ true.
 	newStyleTile
-		ifTrue: [
-			sndName _ FillInTheBlank
-				request: 'Please enter a name this sound'
-				initialAnswer: 'sound'.
+		ifTrue:
+			[sndName _ FillInTheBlank
+				request: 'Please name your new sound' translated
+				initialAnswer: 'sound' translated.
 			sndName isEmpty ifTrue: [^ self].
 
 			sndName _ SampledSound unusedSoundNameLike: sndName.
@@ -16,10 +18,11 @@ makeTile
 				samples: recorder condensedSamples
 				samplingRate: recorder samplingRate.
 			tile _ SoundTile new literal: sndName]
-		ifFalse: [
-			tile _ InterimSoundMorph new sound: 
+		ifFalse:
+			[tile _ InterimSoundMorph new sound: 
 				(SampledSound
 					samples: recorder condensedSamples
 					samplingRate: recorder samplingRate)].
 
-	self world firstHand attachMorph: tile.
+	tile bounds: tile fullBounds.
+	tile openInHand
