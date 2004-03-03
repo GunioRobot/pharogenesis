bcdf65a6dfa9a59197b3dc580fc72fc64439f32f
sw <unknown>
2004-03-03 00:00:00 +0000

    no message

---
+++ Player/instance/slots-2D-kernel/categoriesForVocabulary-3A-.cs
@@ -0,0 +1,16 @@
+categoriesForVocabulary: aVocabulary
+	"Answer a list of categories appropriate to the receiver and its costumes, in the given Vocabulary"
+
+	| aList |
+	self hasCostumeThatIsAWorld
+		ifTrue:
+			[aList _ self categoriesForWorld]
+		ifFalse:
+			[aList _ OrderedCollection new.
+			self slotNames ifNotEmpty:
+				[aList add: ScriptingSystem nameForInstanceVariablesCategory].
+			aList addAll: costume categoriesForViewer].
+	aVocabulary addCustomCategoriesTo: aList.
+	aList remove: ScriptingSystem nameForScriptsCategory ifAbsent: [].
+	aList add: ScriptingSystem nameForScriptsCategory after: aList first.
+	^ aList
