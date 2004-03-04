8611922ad99bb5052fb74fb344f5d868712c83d0
ajh <unknown>
2004-03-04 23:10:00 +0000

    no message

--- Debugger/instance/context stack menu/peelToFirst.cs
+++ Debugger/instance/context stack menu/peelToFirst.cs
@@ -1,8 +1,8 @@
 peelToFirst
 	"Peel the stack back to the second occurance of the currently selected message.  Very useful for an infinite recursion.  Gets back to the second call so you can see one complete recursion cycle, and how it was called at the beginning.  Also frees a lot of space!"
 
-	| upperGuy meth second |
-	contextStackIndex = 0 ifTrue: [^ self beep].
+	| upperGuy meth second ctxt |
+	contextStackIndex = 0 ifTrue: [^ Beeper beep].
 	"self okToChange ifFalse: [^ self]."
 	upperGuy _ contextStack at: contextStackIndex.
 	meth _ upperGuy method.
@@ -11,5 +11,7 @@ peelToFirst
 			second _ upperGuy.
 			upperGuy _ contextStack at: ind]].
 	second ifNil: [second _ upperGuy].
-	self resetContext: second.
-	interruptedProcess popTo: self selectedContext.
+	ctxt _ interruptedProcess popTo: self selectedContext.
+	ctxt == self selectedContext
+		ifTrue: [self resetContext: second]
+		ifFalse: [self resetContext: ctxt].  "unwind error"
