88ce2e50fe33bfc990fbb142e073ae0d1c81bf31
ajh <unknown>
2004-03-04 23:14:00 +0000

    no message

--- Debugger/instance/context stack menu/restart.cs
+++ Debugger/instance/context stack menu/restart.cs
@@ -1,5 +1,15 @@
 restart
-	"Proceed execution of the receiver's model, starting at the beginning of 
-	the currently selected method."
+	"Proceed from the initial state of the currently selected context. The 
+	argument is a controller on a view of the receiver. That view is closed."
+	"Closing now depends on a preference #restartAlsoProceeds - hmm 9/7/2001 16:46"
 
-	self restart: self topView.
+	| ctxt noUnwindError |
+	self okToChange ifFalse: [^ self].
+	self checkContextSelection.
+	ctxt _ interruptedProcess popTo: self selectedContext.
+	noUnwindError _ false.
+	ctxt == self selectedContext ifTrue: [
+		noUnwindError _ true.
+		interruptedProcess restartTop; stepToSendOrReturn].
+	self resetContext: ctxt.
+	(Preferences restartAlsoProceeds and: [noUnwindError]) ifTrue: [self proceed].
