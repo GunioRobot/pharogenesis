f6c621244d35b2235ca4ea6e6525918e7837f593
nk <unknown>
2004-03-06 10:47:00 +0000

    no message

--- MagnifierMorph/instance/initialization/initialize.cs
+++ MagnifierMorph/instance/initialization/initialize.cs
@@ -1,9 +1,9 @@
 initialize
+	"initialize the state of the receiver"
 	super initialize.
-	trackPointer _ true.
-	magnification _ 2.
-	color _ Color black.
-	borderWidth _ 1.
-	lastPos _ self sourcePoint.
-	self extent: 128@128.
 
+	trackPointer _ true.
+	showPointer _ false.
+	magnification _ 2.
+
+	self extent: 128 @ 128
