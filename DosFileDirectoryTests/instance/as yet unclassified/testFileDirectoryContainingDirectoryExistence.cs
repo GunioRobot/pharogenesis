baf2a8d4d63299940fe5cd1e865530867b7b5171
ar <unknown>
2004-03-06 04:05:00 +0000

    no message

---
+++ DosFileDirectoryTests/instance/as yet unclassified/testFileDirectoryContainingDirectoryExistence.cs
@@ -0,0 +1,6 @@
+testFileDirectoryContainingDirectoryExistence
+	"Hoping that you have 'C:' of course..."
+	| fd |
+	FileDirectory activeDirectoryClass == DosFileDirectory ifFalse:[^self].
+	fd := FileDirectory on: 'C:'.
+	self assert: (fd containingDirectory fileOrDirectoryExists: 'C:').
