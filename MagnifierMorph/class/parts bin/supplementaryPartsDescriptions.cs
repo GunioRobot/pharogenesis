4a063aecb1996eac990d9dfc2b2785012af06dc7
nk <unknown>
2004-03-06 10:27:00 +0000

    no message

---
+++ MagnifierMorph/class/parts bin/supplementaryPartsDescriptions.cs
@@ -0,0 +1,14 @@
+supplementaryPartsDescriptions
+	^ {DescriptionForPartsBin
+		formalName: 'RoundGlass'
+		categoryList: #(Useful)
+		documentation: 'A round magnifying glass'
+		globalReceiverSymbol: #MagnifierMorph
+		nativitySelector: #newRound.
+		
+	DescriptionForPartsBin
+		formalName: 'Hand Magnifier'
+		categoryList: #(Useful)
+		documentation: 'A magnifying glass that also shows Morphs in the Hand and displays the Hand position.'
+		globalReceiverSymbol: #MagnifierMorph
+		nativitySelector: #newShowingPointer }
