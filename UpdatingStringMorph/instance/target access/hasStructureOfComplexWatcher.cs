ac10187ad1109e30026f53fe1734725bb9873287
sw <unknown>
2004-03-07 15:49:00 +0000

    no message

---
+++ UpdatingStringMorph/instance/target access/hasStructureOfComplexWatcher.cs
@@ -0,0 +1,9 @@
+hasStructureOfComplexWatcher
+	"Answer whether the receiver has precisely the structure of a so-called complex watcher, as used in the etoy system."
+
+	| top |
+	top _ (self owner ifNil: [^ false]) owner.
+	^ ((((top isMemberOf: AlignmentMorph)
+		and: [top submorphs size = 4])
+			and: [top submorphs first isMemberOf: TileMorph])
+				and: [top submorphs third isMemberOf: AlignmentMorph])
