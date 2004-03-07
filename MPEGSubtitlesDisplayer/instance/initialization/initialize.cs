d052105db9ccdfc1fab01663e3adac9c4a2f36b4
dgd <unknown>
2004-03-07 21:16:00 +0000

    no message

---
+++ MPEGSubtitlesDisplayer/instance/initialization/initialize.cs
@@ -0,0 +1,11 @@
+initialize
+	"initialiaze the receiver"
+	super initialize.
+	""
+font := TextStyle defaultFont.""
+	self
+		backgroundColor: (Color black alpha: 0.4).
+	""
+	self margins: 4 @ 2.
+	self textColor: Color white.
+	self textStyle centered
