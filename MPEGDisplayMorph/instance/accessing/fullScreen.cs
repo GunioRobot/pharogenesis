fffd428461e8591bbbb944377cd53a9f1a5a920c
dgd <unknown>
2004-03-07 18:52:00 +0000

    no message

---
+++ MPEGDisplayMorph/instance/accessing/fullScreen.cs
@@ -0,0 +1,5 @@
+fullScreen
+	"answer whatever the receiver is fullScreen
+
+	Note: comparation with true to make it work with instances created before the introduccion of the variable"
+	^ fullScreen == true
