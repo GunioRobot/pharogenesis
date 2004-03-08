612201029af599c583bacf1dcc1ae838839a2447
nk <unknown>
2004-03-08 12:15:00 +0000

    no message

--- MessageTally/instance/printing/printOn-3A-total-3A-totalTime-3A-tallyExact-3A-.cs
+++ MessageTally/instance/printing/printOn-3A-total-3A-totalTime-3A-tallyExact-3A-.cs
@@ -1,25 +1,33 @@
-printOn: aStream total: total totalTime: totalTime tallyExact: isExact
+printOn: aStream total: total totalTime: totalTime tallyExact: isExact 
 	| aSelector className myTally aClass percentage |
-	isExact
-		ifTrue:
-			[myTally _ tally.
-			receivers == nil
-				ifFalse: [receivers do: [:r | myTally _ myTally - r tally]].
-			aStream print: myTally; space]
-		ifFalse:
-			[percentage _ tally asFloat / total * 100.0 roundTo: 0.1.
+	isExact 
+		ifTrue: 
+			[myTally := tally.
+			receivers == nil 
+				ifFalse: [receivers do: [:r | myTally := myTally - r tally]].
+			aStream
+				print: myTally;
+				space]
+		ifFalse: 
+			[percentage := tally asFloat / total * 100.0 roundTo: 0.1.
 			aStream
 				print: percentage;
 				nextPutAll: '% {';
 				print: (percentage * totalTime / 100) rounded;
 				nextPutAll: 'ms} '].
-	receivers == nil
-		ifTrue: [aStream nextPutAll: 'primitives'; cr]
+	receivers == nil 
+		ifTrue: 
+			[aStream
+				nextPutAll: 'primitives';
+				cr]
 		ifFalse: 
-			[aSelector _ class selectorAtMethod: method setClass: [:c | aClass _
-c].
-			className _ aClass name contractTo: 30.
-			aStream nextPutAll: class name;
-				nextPutAll: (aClass = class ifTrue: ['>>']
-								ifFalse: ['(' , aClass name , ')>>']);
-				nextPutAll: (aSelector contractTo: 60-className size); cr]
+			[aSelector := class selectorAtMethod: method setClass: [:c | aClass := c].
+			className := aClass name contractTo: self maxClassNameSize.
+			aStream
+				nextPutAll: class name;
+				nextPutAll: (aClass = class 
+							ifTrue: ['>>']
+							ifFalse: ['(' , aClass name , ')>>']);
+				nextPutAll: (aSelector 
+							contractTo: self maxClassPlusSelectorSize - className size);
+				cr]
