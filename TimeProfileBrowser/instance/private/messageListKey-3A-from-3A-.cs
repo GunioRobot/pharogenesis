6e030064f82ff5b2b48314a74f6f32ad26fbb44b
nk <unknown>
2004-03-08 13:22:00 +0000

    no message

--- TimeProfileBrowser/instance/private/messageListKey-3A-from-3A-.cs
+++ TimeProfileBrowser/instance/private/messageListKey-3A-from-3A-.cs
@@ -1,5 +1,5 @@
-messageListKey: aChar from: view
+messageListKey: aChar from: view 
 	"Respond to a Command key. Cmd-D means re-run block."
 
-	aChar == $d ifTrue: [^ Cursor execute showWhile: [self runBlock]].
-	^ self arrowKey: aChar from: view
+	aChar == $d ifTrue: [^Cursor execute showWhile: [ block value ]].
+	^super messageListKey: aChar from: view
