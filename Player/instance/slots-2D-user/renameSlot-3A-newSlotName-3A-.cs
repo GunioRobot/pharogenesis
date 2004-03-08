12f654874444dc6c1061b7bc8de67f4d717fa90c
sw <unknown>
2004-03-08 22:14:00 +0000

    no message

--- Player/instance/slots-2D-user/renameSlot-3A-newSlotName-3A-.cs
+++ Player/instance/slots-2D-user/renameSlot-3A-newSlotName-3A-.cs
@@ -1,18 +1,19 @@
 renameSlot: oldSlotName newSlotName: newSlotName
+	"Give an existing instance variable a new name"
+
 	self class renameSilentlyInstVar: oldSlotName to: newSlotName.
+	self renameSlotInWatchersOld: oldSlotName new: newSlotName.
+
 	self updateAllViewers.
-	self flag: #deferred.
-	"Any scripts that formerly sent oldSlotName should now send newSlotName"
 
-	self inform: 
-'Caution!  Any scripts that may reference this
-instance variable by its old name may now be
-broken -- you may need to fix them up manually.  
-In some future release, we''ll automatically
-fix those up, hopefully.'.
-
-	self costume world presenter allExtantPlayers do:
+	self presenter allExtantPlayers do:
 		[:aPlayer | (aPlayer hasScriptReferencing: oldSlotName ofPlayer: self)
 			ifTrue:
-				[^ aPlayer noteRenameOf: oldSlotName to: newSlotName inPlayer: self]].
+				[aPlayer noteRenameOf: oldSlotName to: newSlotName inPlayer: self]].
+
+	self presenter hasAnyTextuallyCodedScripts
+		ifTrue:
+			[self inform: 
+'Caution!  References in texutally coded scripts won''t be renamed.'].
+
 	^ true
