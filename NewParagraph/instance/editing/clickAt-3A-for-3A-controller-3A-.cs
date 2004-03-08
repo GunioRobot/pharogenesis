a8d9cd8455dfd31af2de37f849e06b1390b838c7
nk <unknown>
2004-03-08 14:56:00 +0000

    no message

--- NewParagraph/instance/editing/clickAt-3A-for-3A-controller-3A-.cs
+++ NewParagraph/instance/editing/clickAt-3A-for-3A-controller-3A-.cs
@@ -1,23 +1,21 @@
 clickAt: clickPoint for: model controller: editor
 	"Give sensitive text a chance to fire.  Display flash: (100@100 extent: 100@100)."
-	| startBlock action target |
+	| startBlock action target range boxes box |
 	action _ false.
 	startBlock _ self characterBlockAtPoint: clickPoint.
 	(text attributesAt: startBlock stringIndex forStyle: textStyle) 
 		do: [:att | att mayActOnClick ifTrue:
-				[
-	"			range _ text rangeOf: att startingAt: startBlock stringIndex.
+				[(target _ model) ifNil: [target _ editor morph].
+				range _ text rangeOf: att startingAt: startBlock stringIndex forStyle: textStyle.
 				boxes _ self selectionRectsFrom: (self characterBlockForIndex: range first) 
-							to: (self characterBlockForIndex: range last).
-				box _ boxes detect: [:each | each containsPoint: clickPoint].
-					*This doesn't work in morphic*
-				Need to replace by a highlighting morph that waits for moueUp.
-				Utilities awaitMouseUpIn: (editor transform invertRect: box)
+							to: (self characterBlockForIndex: range last+1).
+				box _ boxes detect: [:each | each containsPoint: clickPoint] ifNone: [nil].
+				box ifNotNil:
+					[ box _ (editor transformFrom: nil) invertBoundsRect: box.
+					editor morph allOwnersDo: [ :m | box _ box intersect: (m boundsInWorld) ].
+					Utilities awaitMouseUpIn: box
 						repeating: []
-						ifSucceed: [(att actOnClickFor: model) ifTrue: [action _ true]].
-	"
-				(target _ model) ifNil: [target _ editor morph].
-				(att actOnClickFor: target)
-					ifTrue: [Sensor waitNoButton.  "FIX THIS"
-							action _ true]]].
+						ifSucceed: [(att actOnClickFor: target in: self at: clickPoint editor: editor) ifTrue: [action _ true]].
+					Cursor currentCursor == Cursor webLink ifTrue:[Cursor normal show].
+				]]].
 	^ action
