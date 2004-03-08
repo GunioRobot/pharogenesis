2552c6bc1866376d0278f1bc11527bff3f891791
gk <unknown>
2004-03-08 20:08:00 +0000

    no message

---
+++ SMExternalResource/instance/accessing/contents.cs
@@ -0,0 +1,6 @@
+contents
+	"Return the contents of a stream from the downloaded resource.
+	Not yet tested, this resource returns the stream and not its contents."
+
+	map cache add: self.
+	^self cacheDirectory readOnlyFileNamed: self downloadFileName
