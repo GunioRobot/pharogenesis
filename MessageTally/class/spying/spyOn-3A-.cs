60fc85f4bbc59a76987817be7cd0af23e6d4ed04
nk <unknown>
2004-03-08 10:34:00 +0000

    no message

--- MessageTally/class/spying/spyOn-3A-.cs
+++ MessageTally/class/spying/spyOn-3A-.cs
@@ -1,7 +1,7 @@
 spyOn: aBlock    "MessageTally spyOn: [100 timesRepeat: [3.14159 printString]]"
 	| node result |
 	node _ self new.
-	result _ node spyEvery: 16 on: aBlock.
+	result _ node spyEvery: self defaultPollPeriod on: aBlock.
 	(StringHolder new contents: (String streamContents: [:s | node report: s; close]))
 		openLabel: 'Spy Results'.
 	^ result
