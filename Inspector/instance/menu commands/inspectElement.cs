19d487a9df9518c20585d4900c54f9a921b518cd
yo <unknown>
2004-03-09 11:04:00 +0000

    no message

--- Inspector/instance/menu commands/inspectElement.cs
+++ Inspector/instance/menu commands/inspectElement.cs
@@ -1,20 +1,22 @@
 inspectElement
-	| sel selSize countString count |
+	| sel selSize countString count nameStrs |
 	"Create and schedule an Inspector on an element of the receiver's model's currently selected collection."
 
 	self selectionIndex = 0 ifTrue: [^ self changed: #flash].
 	((sel _ self selection) isKindOf: SequenceableCollection) ifFalse:
-		[^ sel inspect].
+		[(sel isKindOf: MorphExtension) ifTrue: [^ sel inspectElement].
+		^ sel inspect].
 	(selSize _ sel size) == 1 ifTrue: [^ sel first inspect].
-	selSize <= 10 ifTrue:
-		[count _ (SelectionMenu selections: (1 to: selSize) asArray) startUpWithCaption: 'which element?'.
-		count ifNil: [^ self] ifNotNil: [^ (sel at: count) inspect]].
+	selSize <= 20 ifTrue:
+		[nameStrs _ (1 to: selSize) asArray collect: [:ii | 
+			ii printString, '   ', (((sel at: ii) printStringLimitedTo: 25) replaceAll: Character cr with: Character space)].
+		count _ PopUpMenu withCaption: 'which element?' chooseFrom: nameStrs.
+		count = 0 ifTrue: [^ self].
+		^ (sel at: count) inspect].
 
-	countString _ FillInTheBlank request: 'Which element? (1 - ', selSize printString, ')' initialAnswer: '1'.
+	countString _ FillInTheBlank request: 'Which element? (1 to ', selSize printString, ')' initialAnswer: '1'.
 	countString isEmptyOrNil ifTrue: [^ self].
 	count _ Integer readFrom: (ReadStream on: countString).
 	(count > 0 and: [count <= selSize])
-		ifTrue:
-			[(sel  at: count) inspect]
-		ifFalse:
-			[self beep]
+		ifTrue: [(sel at: count) inspect]
+		ifFalse: [Beeper beep]
