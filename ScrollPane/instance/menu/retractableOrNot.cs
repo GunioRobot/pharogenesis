769d218b7f64e71b10d111e6c3b4959a58703605
sps <unknown>
2004-03-09 17:47:00 +0000

    no message

--- ScrollPane/instance/menu/retractableOrNot.cs
+++ ScrollPane/instance/menu/retractableOrNot.cs
@@ -1,7 +1,11 @@
-retractableOrNot  "Change scroll bar operation"
+retractableOrNot
+	"Change scroll bar operation"
+
 	retractableScrollBar _ retractableScrollBar not.
 	retractableScrollBar
-		ifTrue: [self privateRemoveMorph: scrollBar]
+		ifTrue: [self removeMorph: scrollBar]
 		ifFalse: [(submorphs includes: scrollBar) 
-					ifFalse: [self privateAddMorph: scrollBar atIndex: 1]].
-	self extent: self extent
+					ifFalse: 
+						[self privateAddMorph: scrollBar atIndex: 1.
+						self privateAddMorph: hScrollBar atIndex: 1]].
+	self extent: self extent.
