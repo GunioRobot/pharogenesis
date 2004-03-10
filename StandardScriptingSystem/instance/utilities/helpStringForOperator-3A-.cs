1bc87acae561afbff1881ac58ecbd979f952294d
sw <unknown>
2004-03-10 23:24:00 +0000

    no message

--- StandardScriptingSystem/instance/utilities/helpStringForOperator-3A-.cs
+++ StandardScriptingSystem/instance/utilities/helpStringForOperator-3A-.cs
@@ -1,12 +1,5 @@
 helpStringForOperator: anOperator
-	"Answer the help string associated with the given operator"
+	"Answer the help string associated with the given operator. If none found, return a standard no-help-available reply"
 
-	| anIndex opsAndHelp |
-	(anIndex _ (opsAndHelp _ self arithmeticalOperatorsAndHelpStrings) first indexOf: anOperator) > 0
-		ifTrue:	[^ opsAndHelp second at: anIndex].
-
-	(anIndex _ (opsAndHelp _ self numericComparitorsAndHelpStrings) first indexOf: anOperator) > 0
-		ifTrue:	[^ opsAndHelp second at: anIndex].
-
-	^ 'And I called my cow, no help now' 
-	"This should never be seen, but if it is, we should hear the moo eventually"
+	^ (self helpStringOrNilForOperator: anOperator) ifNil:
+		['Sorry, no help available here' translated]  "This should never be seen, but is provided as a backstop"
