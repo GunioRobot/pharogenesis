14fe2db0a85b430f148352065789a6eec861e9e5
nk <unknown>
2004-03-10 14:30:00 +0000

    no message

--- Morph/instance/events-2D-processing/handleMouseDown-3A-.cs
+++ Morph/instance/events-2D-processing/handleMouseDown-3A-.cs
@@ -4,14 +4,24 @@ handleMouseDown: anEvent
 	anEvent hand removePendingBalloonFor: self.
 	anEvent hand removePendingHaloFor: self.
 	anEvent wasHandled: true.
-	anEvent controlKeyPressed ifTrue:[^self invokeMetaMenu: anEvent].
+
+	(anEvent controlKeyPressed
+			and: [Preferences cmdGesturesEnabled])
+		ifTrue: [^ self invokeMetaMenu: anEvent].
+
 	"Make me modal during mouse transitions"
 	anEvent hand newMouseFocus: self event: anEvent.
 	anEvent blueButtonChanged ifTrue:[^self blueButtonDown: anEvent].
+	
+	"this mouse down could be the start of a gesture, or the end of a gesture focus"
+	(self isGestureStart: anEvent)
+		ifTrue: [^ self gestureStart: anEvent].
+
 	self mouseDown: anEvent.
 	anEvent hand removeHaloFromClick: anEvent on: self.
+
 	(self handlesMouseStillDown: anEvent) ifTrue:[
 		self startStepping: #handleMouseStillDown: 
 			at: Time millisecondClockValue + self mouseStillDownThreshold
 			arguments: {anEvent copy resetHandlerFields}
-			stepTime: 1].
+			stepTime: self mouseStillDownStepRate ].
