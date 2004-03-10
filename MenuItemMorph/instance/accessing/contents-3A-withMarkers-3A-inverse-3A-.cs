e0d612d88be4cb977cfb22c02017ca5e06db056e
nk <unknown>
2004-03-10 15:55:00 +0000

    no message

--- MenuItemMorph/instance/accessing/contents-3A-withMarkers-3A-inverse-3A-.cs
+++ MenuItemMorph/instance/accessing/contents-3A-withMarkers-3A-inverse-3A-.cs
@@ -1,26 +1,24 @@
-contents: aString withMarkers: aBool inverse: inverse
+contents: aString withMarkers: aBool inverse: inverse 
 	"Set the menu item entry. If aBool is true, parse aString for embedded markers."
-	| markerIndex marker indent |
-	self contentString: nil. "get rid of old"
-	aBool ifFalse:[^super contents: aString].
-	self removeAllMorphs. "get rid of old markers if updating"
-	(aString at: 1) = $< ifFalse:[^super contents: aString].
-	markerIndex _ aString indexOf: $>.
-	markerIndex = 0 ifTrue:[^super contents: aString].
-	marker _ (aString copyFrom: 1 to: markerIndex) asLowercase.
-	(#('<on>' '<off>' '<yes>' '<no>') includes: marker) ifFalse:[^super contents: aString].
-	self contentString: aString. "remember actual string"
-	(marker = '<on>' or:[marker = '<yes>']) ~= inverse
-		ifTrue:[marker _ self onImage]
-		ifFalse:[marker _ self offImage].
-	"Indent the string using white spaces"
-	indent _ ' '.
-	font _ self fontToUse.
-	[ (font widthOfString: indent) < (marker width + 4) ] 
-		whileTrue:[indent _ indent copyWith: Character space].
-	"Set the string"
-	super contents: indent, (aString copyFrom: markerIndex+1 to: aString size).
+
+	| markerIndex marker |
+	self contentString: nil.	"get rid of old"
+	aBool ifFalse: [^super contents: aString].
+	self removeAllMorphs.	"get rid of old markers if updating"
+	self hasIcon ifTrue: [ self icon: nil ].
+	(aString notEmpty and: [aString first = $<]) 
+		ifFalse: [^super contents: aString].
+	markerIndex := aString indexOf: $>.
+	markerIndex = 0 ifTrue: [^super contents: aString].
+	marker := (aString copyFrom: 1 to: markerIndex) asLowercase.
+	(#('<on>' '<off>' '<yes>' '<no>') includes: marker) 
+		ifFalse: [^super contents: aString].
+	self contentString: aString.	"remember actual string"
+	marker := (marker = '<on>' or: [marker = '<yes>']) ~= inverse 
+				ifTrue: [self onImage]
+				ifFalse: [self offImage].
+	super contents:  (aString copyFrom: markerIndex + 1 to: aString size).
 	"And set the marker"
-	marker _ ImageMorph new image: marker.
-	marker position: (self left) @ (self top + 2).
-	self addMorphFront: marker.
+	marker := ImageMorph new image: marker.
+	marker position: self left @ (self top + 2).
+	self addMorphFront: marker
