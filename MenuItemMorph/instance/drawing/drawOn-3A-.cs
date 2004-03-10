c91e450a727e03d0258d698aac9917190fe79bbc
nk <unknown>
2004-03-10 15:46:00 +0000

    no message

--- MenuItemMorph/instance/drawing/drawOn-3A-.cs
+++ MenuItemMorph/instance/drawing/drawOn-3A-.cs
@@ -1,13 +1,27 @@
-drawOn: aCanvas
-	| selectionColor |
+drawOn: aCanvas 
+	| stringColor stringBounds leftEdge |
 	isSelected & isEnabled
-		ifTrue:
-			[selectionColor _ Display depth <= 2
-				ifTrue: [Color gray]
-				ifFalse: [owner color darker].
-			aCanvas fillRectangle: self bounds color: selectionColor].
-	super drawOn: aCanvas.
-	subMenu ifNotNil:
-		[aCanvas
-			paintImage: SubMenuMarker
-			at: self right - 8 @ (self top + self bottom - SubMenuMarker height // 2)]
+		ifTrue: [
+			aCanvas fillRectangle: self bounds fillStyle: self selectionFillStyle.
+			stringColor := color negated]
+		ifFalse: [stringColor := color].
+
+	leftEdge := 0.
+	self hasIcon
+		ifTrue: [| iconForm | 
+			iconForm := isEnabled ifTrue:[self icon] ifFalse:[self icon asGrayScale].
+			aCanvas paintImage: iconForm at: self left @ (self top + (self height - iconForm height // 2)).
+			leftEdge := iconForm width + 2].
+
+	self hasMarker
+		ifTrue: [ leftEdge := leftEdge + self submorphBounds width + 8 ].
+
+	stringBounds := bounds left: bounds left + leftEdge.
+
+	aCanvas
+		drawString: contents
+		in: stringBounds
+		font: self fontToUse
+		color: stringColor.
+	subMenu
+		ifNotNil: [aCanvas paintImage: SubMenuMarker at: self right - 8 @ (self top + self bottom - SubMenuMarker height // 2)]
