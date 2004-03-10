cef50e9b76983e64c808e1a1d6fd0f4700887beb
nk <unknown>
2004-03-10 12:15:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/getWidth.cs
+++ Player/instance/slot getters-2F-setters/getWidth.cs
@@ -1,4 +1,13 @@
 getWidth
 	"Answer the width of the object"
 
-	^ self costume width
+	| aWidth cost |
+	((cost _ self costume) isLineMorph) "annoying special case"
+		ifTrue:
+			[^ cost unrotatedWidth].
+	aWidth _ cost renderedMorph width.  "facing upward when unrotated"
+
+	cost isRenderer
+		ifTrue:
+			[aWidth _ aWidth * cost scaleFactor].
+	^ aWidth
