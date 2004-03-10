9c2ad84b67fdd3f956e9a6921d62e798b33019e3
nk <unknown>
2004-03-10 12:09:00 +0000

    no message

--- TextMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
+++ TextMorph/instance/menu/addCustomMenuItems-3A-hand-3A-.cs
@@ -1,23 +1,29 @@
 addCustomMenuItems: aCustomMenu hand: aHandMorph
 	| outer |
 	super addCustomMenuItems: aCustomMenu hand: aHandMorph.
-	aCustomMenu add: 'add predecessor' action: #addPredecessor:.
-	aCustomMenu add: 'add successor' action: #addSuccessor:.
-	(Preferences valueOfFlag: #noviceMode) not & 
-		(Preferences valueOfFlag: #simpleMenus) not ifTrue: [
-			aCustomMenu add: 'code pane menu...' action: #yellowButtonActivity.
-			aCustomMenu add: 'code pane shift menu....' action: #shiftedYellowButtonActivity].
+	aCustomMenu add: 'text color...' translated action: #changeTextColor.
+	aCustomMenu addUpdating: #autoFitString target: self action: #autoFitOnOff.
+	aCustomMenu addUpdating: #wrapString target: self action: #wrapOnOff.
+	aCustomMenu add: 'text margins...' translated action: #changeMargins:.
+	aCustomMenu add: 'add predecessor' translated action: #addPredecessor:.
+	aCustomMenu add: 'add successor' translated action: #addSuccessor:.
+	(Preferences noviceMode
+			or: [Preferences simpleMenus])
+		ifFalse: [aCustomMenu add: 'code pane menu...' translated action: #yellowButtonActivity.
+			aCustomMenu add: 'code pane shift menu...' translated action: #shiftedYellowButtonActivity].
 
 	outer _ self owner.
-	((outer isKindOf: PolygonMorph) and: [outer isOpen]) ifTrue:
-		[container == nil
-			ifTrue: [aCustomMenu add: 'follow owner''s curve' action: #followCurve]
-			ifFalse: [aCustomMenu add: 'reverse direction' action: #reverseCurveDirection.
-					aCustomMenu add: 'set baseline' action: #setCurveBaseline:]]
+	outer isLineMorph ifTrue:
+		[container isNil
+			ifTrue: [aCustomMenu add: 'follow owner''s curve' translated action: #followCurve]
+			ifFalse: [aCustomMenu add: 'reverse direction' translated action: #reverseCurveDirection.
+					aCustomMenu add: 'set baseline' translated action: #setCurveBaseline:]]
 		ifFalse:
-		[(container == nil or: [container fillsOwner not])
-			ifTrue: [aCustomMenu add: 'fill owner''s shape' action: #fillingOnOff]
-			ifFalse: [aCustomMenu add: 'rectangluar bounds' action: #fillingOnOff].
-		(container == nil or: [container avoidsOcclusions not])
-			ifTrue: [aCustomMenu add: 'avoid occlusions' action: #occlusionsOnOff]
-			ifFalse: [aCustomMenu add: 'ignore occlusions' action: #occlusionsOnOff]].
+		[(container isNil or: [container fillsOwner not])
+			ifTrue: [aCustomMenu add: 'fill owner''s shape' translated action: #fillingOnOff]
+			ifFalse: [aCustomMenu add: 'rectangular bounds' translated action: #fillingOnOff].
+		(container isNil or: [container avoidsOcclusions not])
+			ifTrue: [aCustomMenu add: 'avoid occlusions' translated action: #occlusionsOnOff]
+			ifFalse: [aCustomMenu add: 'ignore occlusions' translated action: #occlusionsOnOff]].
+	aCustomMenu addLine.
+	aCustomMenu add: 'holder for characters' translated action: #holderForCharacters
