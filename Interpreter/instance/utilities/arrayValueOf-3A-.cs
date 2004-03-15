72f10ebf2831b0da597b53401246b87cbd5e9734
tpr <unknown>
2004-03-15 19:38:00 +0000

    no message

--- Interpreter/instance/utilities/arrayValueOf-3A-.cs
+++ Interpreter/instance/utilities/arrayValueOf-3A-.cs
@@ -1,9 +1,8 @@
 arrayValueOf: arrayOop
-	"Return the address of first indexable field of resulting array object, or fail if the instance variable does not contain an indexable bytes or words object."
+	"Return the address of first indexable field of the word or byte arry-like object arrayOop. Fail if arrayOop is not an indexable bytes or words object."
 	"Note: May be called by translated primitive code."
 
 	self returnTypeC: 'void *'.
-	((self isIntegerObject: arrayOop) not and:
-	 [self isWordsOrBytes: arrayOop])
+	(self isWordsOrBytes: arrayOop)
 		ifTrue: [^ self cCode: '(void *) (arrayOop + 4)'].
 	self primitiveFail.
