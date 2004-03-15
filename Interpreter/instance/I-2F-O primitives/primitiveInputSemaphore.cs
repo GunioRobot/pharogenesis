060cbcad389a006f9fe1c4c39edd0716ee64a09d
tpr <unknown>
2004-03-15 20:12:00 +0000

    no message

--- Interpreter/instance/I-2F-O primitives/primitiveInputSemaphore.cs
+++ Interpreter/instance/I-2F-O primitives/primitiveInputSemaphore.cs
@@ -1,20 +1,25 @@
 primitiveInputSemaphore
-	"Register the input semaphore. If the argument is not a Semaphore, unregister the current input semaphore."
-
+	"Register the input semaphore. If the argument is not a 
+	Semaphore, unregister the current input semaphore."
 	| arg |
-	arg _ self stackValue: 0.
-	(self isIntegerObject: arg) ifTrue:[
-		"If arg is integer, then use it as an index
-		 into the external objects array and install it
-		 as the new event semaphore"
-		self ioSetInputSemaphore: (self integerValueOf: arg).
-		successFlag ifTrue:[self pop: 1].
-		^nil
-	].
+	arg _ self stackTop.
+	(self isIntegerObject: arg)
+		ifTrue: ["If arg is integer, then use it as an index 
+			into the external objects array and install it 
+			as the new event semaphore"
+			self ioSetInputSemaphore: (self integerValueOf: arg).
+			successFlag
+				ifTrue: [self pop: 1].
+			^ nil].
+
 	"old code for compatibility"
 	arg _ self popStack.
-	((self fetchClassOf: arg) = (self splObj: ClassSemaphore)) ifTrue: [
-		self storePointer: TheInputSemaphore ofObject: specialObjectsOop withValue: arg.
-	] ifFalse: [
-		self storePointer: TheInputSemaphore ofObject: specialObjectsOop withValue: nilObj.
-	].
+	(self fetchClassOf: arg) = (self splObj: ClassSemaphore)
+		ifTrue: [self
+				storePointer: TheInputSemaphore
+				ofObject: specialObjectsOop
+				withValue: arg]
+		ifFalse: [self
+				storePointer: TheInputSemaphore
+				ofObject: specialObjectsOop
+				withValue: nilObj]
