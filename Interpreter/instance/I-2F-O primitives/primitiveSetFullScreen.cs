8166cc53a61857ae527c9741b1882f9664ce3e30
tpr <unknown>
2004-03-15 14:23:00 +0000

    no message

--- Interpreter/instance/I-2F-O primitives/primitiveSetFullScreen.cs
+++ Interpreter/instance/I-2F-O primitives/primitiveSetFullScreen.cs
@@ -5,8 +5,7 @@ primitiveSetFullScreen
 	argOop _ self stackTop.
 	argOop = trueObj
 		ifTrue: [self ioSetFullScreen: true]
-		ifFalse: [
-			argOop = falseObj
+		ifFalse: [ argOop = falseObj
 				ifTrue: [self ioSetFullScreen: false]
 				ifFalse: [self primitiveFail]].
 	successFlag ifTrue: [self pop: 1].
