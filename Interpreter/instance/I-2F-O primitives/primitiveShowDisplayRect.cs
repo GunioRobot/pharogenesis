3daf261c90a2621b0376550f082eca55cb575a3d
tpr <unknown>
2004-03-15 14:24:00 +0000

    no message

--- Interpreter/instance/I-2F-O primitives/primitiveShowDisplayRect.cs
+++ Interpreter/instance/I-2F-O primitives/primitiveShowDisplayRect.cs
@@ -1,11 +1,12 @@
 primitiveShowDisplayRect
-	"Force the given rectangular section of the Display to be copied to the screen."
+	"Force the given rectangular section of the Display to be 
+	copied to the screen."
 	| bottom top right left |
-	bottom	_ self stackIntegerValue: 0.
-	top		_ self stackIntegerValue: 1.
-	right	_ self stackIntegerValue: 2.
-	left		_ self stackIntegerValue: 3.
+	bottom _ self stackIntegerValue: 0.
+	top _ self stackIntegerValue: 1.
+	right _ self stackIntegerValue: 2.
+	left _ self stackIntegerValue: 3.
 	self displayBitsOf: (self splObj: TheDisplay) Left: left Top: top Right: right Bottom: bottom.
-	successFlag ifTrue: [
-		self ioForceDisplayUpdate.
-		self pop: 4].  "pop left, right, top, bottom; leave rcvr on stack"
+	successFlag
+		ifTrue: [self ioForceDisplayUpdate.
+			self pop: 4]
