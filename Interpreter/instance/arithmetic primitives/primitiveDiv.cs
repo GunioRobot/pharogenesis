75fb8c59495e10b2c735bf770c8fbefb17971f3d
tpr <unknown>
2004-03-15 20:24:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveDiv.cs
+++ Interpreter/instance/arithmetic primitives/primitiveDiv.cs
@@ -1,4 +1,4 @@
 primitiveDiv
 	| quotient |
-	quotient _ self doPrimitiveDiv: (self stackValue: 1) by: (self stackValue: 0).
+	quotient _ self doPrimitiveDiv: (self stackValue: 1) by: (self stackTop).
 	self pop2AndPushIntegerIfOK: quotient
