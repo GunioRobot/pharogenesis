dea26b1df11133416470a846309f8da9dae9d2ae
tpr <unknown>
2004-03-15 20:27:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveMod.cs
+++ Interpreter/instance/arithmetic primitives/primitiveMod.cs
@@ -1,4 +1,4 @@
 primitiveMod
 	| mod |
-	mod _ self doPrimitiveMod: (self stackValue: 1) by: (self stackValue: 0).
+	mod _ self doPrimitiveMod: (self stackValue: 1) by: (self stackTop).
 	self pop2AndPushIntegerIfOK: mod
