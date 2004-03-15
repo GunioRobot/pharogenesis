1b57844f8c789c50085001b2a92fdfdb458bb139
tpr <unknown>
2004-03-15 14:27:00 +0000

    no message

--- Interpreter/instance/memory space primitives/primitiveFullGC.cs
+++ Interpreter/instance/memory space primitives/primitiveFullGC.cs
@@ -1,7 +1,7 @@
 primitiveFullGC
-	"Do a quick, incremental garbage collection and return the number of bytes available."
+	"Do a full garbage collection and return the number of bytes available (including swap space if dynamic memory management is supported)."
 
 	self pop: 1.
 	self incrementalGC.  "maximimize space for forwarding table"
 	self fullGC.
-	self pushInteger: (self sizeOfFree: freeBlock).
+	self pushInteger: (self bytesLeft: true).
