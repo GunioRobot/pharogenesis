3824722c8875b887b68df69c6469230e3730c99c
tpr <unknown>
2004-03-15 12:37:00 +0000

    no message

--- Interpreter/instance/I-2F-O primitives/primitiveInterruptSemaphore.cs
+++ Interpreter/instance/I-2F-O primitives/primitiveInterruptSemaphore.cs
@@ -1,10 +1,9 @@
 primitiveInterruptSemaphore
-	"Register the user interrupt semaphore. If the argument is not a Semaphore, unregister the current interrupt semaphore."
-
+	"Register the user interrupt semaphore. If the argument is 
+	not a Semaphore, unregister the current interrupt 
+	semaphore. "
 	| arg |
 	arg _ self popStack.
-	((self fetchClassOf: arg) = (self splObj: ClassSemaphore)) ifTrue: [
-		self storePointer: TheInterruptSemaphore ofObject: specialObjectsOop withValue: arg.
-	] ifFalse: [
-		self storePointer: TheInterruptSemaphore ofObject: specialObjectsOop withValue: nilObj.
-	].
+	(self fetchClassOf: arg) = (self splObj: ClassSemaphore)
+		ifTrue: [self storePointer: TheInterruptSemaphore ofObject: specialObjectsOop withValue: arg]
+		ifFalse: [self storePointer: TheInterruptSemaphore ofObject: specialObjectsOop withValue: nilObj]
