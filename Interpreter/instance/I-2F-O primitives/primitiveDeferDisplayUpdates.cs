a2245ade863c9dd55ca2221e5a5d04bd1250ff4b
tpr <unknown>
2004-03-15 12:16:00 +0000

    no message

--- Interpreter/instance/I-2F-O primitives/primitiveDeferDisplayUpdates.cs
+++ Interpreter/instance/I-2F-O primitives/primitiveDeferDisplayUpdates.cs
@@ -1,12 +1,13 @@
 primitiveDeferDisplayUpdates
-	"Set or clear the flag that controls whether modifications of the Display object are propagated to the underlying platform's screen."
-
+	"Set or clear the flag that controls whether modifications of 
+	the Display object are propagated to the underlying 
+	platform's screen."
 	| flag |
 	flag _ self stackTop.
 	flag = trueObj
 		ifTrue: [deferDisplayUpdates _ true]
-		ifFalse: [
-			flag = falseObj
+		ifFalse: [flag = falseObj
 				ifTrue: [deferDisplayUpdates _ false]
 				ifFalse: [self primitiveFail]].
-	successFlag ifTrue: [self pop: 1].  "pop flag; leave rcvr on stack"
+	successFlag
+		ifTrue: [self pop: 1]
