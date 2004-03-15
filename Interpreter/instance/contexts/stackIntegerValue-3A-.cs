5c08b4f926153bffa96ded0361926d438d479593
tpr <unknown>
2004-03-15 20:13:00 +0000

    no message

--- Interpreter/instance/contexts/stackIntegerValue-3A-.cs
+++ Interpreter/instance/contexts/stackIntegerValue-3A-.cs
@@ -1,6 +1,4 @@
 stackIntegerValue: offset
 	| integerPointer |
 	integerPointer _ self longAt: stackPointer - (offset*4).
-	(self isIntegerObject: integerPointer)
-		ifTrue: [ ^self integerValueOf: integerPointer ]
-		ifFalse: [ self primitiveFail. ^0 ]
+	^self checkedIntegerValueOf: integerPointer
