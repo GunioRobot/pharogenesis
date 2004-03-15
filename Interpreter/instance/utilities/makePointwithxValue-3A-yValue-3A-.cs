459284fcc6a33cf21338edb5425ab30413ca0f39
tpr <unknown>
2004-03-15 20:35:00 +0000

    no message

--- Interpreter/instance/utilities/makePointwithxValue-3A-yValue-3A-.cs
+++ Interpreter/instance/utilities/makePointwithxValue-3A-yValue-3A-.cs
@@ -1,9 +1,7 @@
 makePointwithxValue: xValue yValue: yValue
 
 	| pointResult |
-	pointResult _ self instantiateSmallClass: (self splObj: ClassPoint)
-							   sizeInBytes: 12
-									   fill: nilObj.
+	pointResult _ self instantiateSmallClass: (self splObj: ClassPoint) sizeInBytes: 12 fill: nilObj.
 	self storePointer: XIndex ofObject: pointResult withValue: (self integerObjectOf: xValue).
 	self storePointer: YIndex ofObject: pointResult withValue: (self integerObjectOf: yValue).
 	^ pointResult
