73d39525a6cc06ecd99d359286c42dd99186b236
tpr <unknown>
2004-03-15 12:39:00 +0000

    no message

--- Interpreter/instance/I-2F-O primitives/primitiveMouseButtons.cs
+++ Interpreter/instance/I-2F-O primitives/primitiveMouseButtons.cs
@@ -1,5 +1,6 @@
 primitiveMouseButtons
-	"Return the mouse button state. The low three bits encode the state of the <red><yellow><blue> mouse buttons. The next four bits encode the Smalltalk modifier bits <cmd><option><ctrl><shift>."
+	"Obsolete on virtually all platforms; old style input polling code.
+	Return the mouse button state. The low three bits encode the state of the <red><yellow><blue> mouse buttons. The next four bits encode the Smalltalk modifier bits <cmd><option><ctrl><shift>."
 
 	| buttonWord |
 	self pop: 1.
