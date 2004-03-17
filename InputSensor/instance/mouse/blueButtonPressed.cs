092bfc17ffdbd825b7373f618063e4934b23895b
nk <unknown>
2004-03-17 07:24:00 +0000

    no message

--- InputSensor/instance/mouse/blueButtonPressed.cs
+++ InputSensor/instance/mouse/blueButtonPressed.cs
@@ -1,4 +1,5 @@
 blueButtonPressed
-	"Answer whether only the blue mouse button is being pressed. This is the third mouse button or cmd+click on the Mac."
+	"Answer whether only the blue mouse button is being pressed. 
+	This is the third mouse button or cmd+click on the Mac."
 
-	^self buttons = 1
+	^ (self primMouseButtons bitAnd: 7) = 1
