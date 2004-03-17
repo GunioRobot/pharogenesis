cabd98caf930c337c18479f9e4fca953114939c5
nk <unknown>
2004-03-17 07:25:00 +0000

    no message

--- InputSensor/instance/mouse/waitNoButton.cs
+++ InputSensor/instance/mouse/waitNoButton.cs
@@ -1,7 +1,7 @@
 waitNoButton
-	"Wait for the user to release any mouse button and then answer with the 
-	current location of the cursor."
+	"Wait for the user to release any mouse button and then answer the current location of the cursor."
 
-	[self anyButtonPressed] whileTrue:
-		[(Delay forMilliseconds: 50) wait].
+	| delay |
+	delay _ Delay forMilliseconds: 50.
+	[self anyButtonPressed] whileTrue: [ delay wait].
 	^self cursorPoint
