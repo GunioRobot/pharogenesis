330ff3e6d529dfd3d74d980d930c299e942cd6d4
yo <unknown>
2004-03-22 15:01:00 +0000

    no message

--- FileDirectory/class/system start up/setDefaultDirectoryFrom-3A-.cs
+++ FileDirectory/class/system start up/setDefaultDirectoryFrom-3A-.cs
@@ -2,4 +2,4 @@ setDefaultDirectoryFrom: imageName
 	"Initialize the default directory to the directory containing the Squeak image file. This method is called when the image starts up."
 
 	DirectoryClass _ self activeDirectoryClass.
-	DefaultDirectory _ self on: (self dirPathFor: imageName).
+	DefaultDirectory _ self on: (FilePath pathName: (self dirPathFor: imageName) isEncoded: true) asSqueakPathName.
