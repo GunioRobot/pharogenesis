461e57bcbc87d569723b506c8234d7e8579e33f9
tpr <unknown>
2004-03-22 14:25:00 +0000

    no message

--- Interpreter/instance/processes/signalSemaphoreWithIndex-3A-.cs
+++ Interpreter/instance/processes/signalSemaphoreWithIndex-3A-.cs
@@ -1,14 +1,13 @@
 signalSemaphoreWithIndex: index
-	"Record the given semaphore index in the double buffer semaphores array to be signaled at the next convenient moment. Set the interruptCheckCounter to zero to force a real interrupt check as soon as possible."
+	"Record the given semaphore index in the double buffer semaphores array to be signaled at the next convenient moment. Force a real interrupt check as soon as possible."
 
 	index <= 0 ifTrue: [^ nil].  "bad index; ignore it"
 
-	semaphoresUseBufferA ifTrue: 
-		[semaphoresToSignalCountA < SemaphoresToSignalSize ifTrue: [
-			semaphoresToSignalCountA _ semaphoresToSignalCountA + 1.
-			semaphoresToSignalA at: semaphoresToSignalCountA put: index]]
-	ifFalse:
-		[semaphoresToSignalCountB < SemaphoresToSignalSize ifTrue: [
-			semaphoresToSignalCountB _ semaphoresToSignalCountB + 1.
-			semaphoresToSignalB at: semaphoresToSignalCountB put: index]].
-	interruptCheckCounter _ 0.
+	semaphoresUseBufferA
+		ifTrue: [semaphoresToSignalCountA < SemaphoresToSignalSize
+			ifTrue: [ semaphoresToSignalCountA _ semaphoresToSignalCountA + 1.
+				semaphoresToSignalA at: semaphoresToSignalCountA put: index]]
+		ifFalse: [semaphoresToSignalCountB < SemaphoresToSignalSize
+			ifTrue: [ semaphoresToSignalCountB _ semaphoresToSignalCountB + 1.
+				semaphoresToSignalB at: semaphoresToSignalCountB put: index]].
+	self forceInterruptCheck
