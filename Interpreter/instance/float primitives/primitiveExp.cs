f8632f55486b3801067112e5bd1ef3f78c225251
tpr <unknown>
2004-03-23 17:22:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveExp.cs
+++ Interpreter/instance/float primitives/primitiveExp.cs
@@ -5,6 +5,5 @@ primitiveExp
 	self var: #rcvr declareC: 'double rcvr'.
 	rcvr _ self popFloat.
 	successFlag
-		ifTrue: [self pushFloat: (self cCode: 'exp(rcvr)'
-									inSmalltalk: [rcvr exp])]
+		ifTrue: [self pushFloat: (self cCode: 'exp(rcvr)' inSmalltalk: [rcvr exp])]
 		ifFalse: [self unPop: 1]
