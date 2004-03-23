bc40b3d42d28c81546747430711b5ca1c12106dc
tpr <unknown>
2004-03-23 17:22:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveExponent.cs
+++ Interpreter/instance/float primitives/primitiveExponent.cs
@@ -4,10 +4,10 @@ primitiveExponent
 	| rcvr frac pwr |
 	self var: #rcvr declareC: 'double rcvr'.
 	self var: #frac declareC: 'double frac'.
+
 	rcvr _ self popFloat.
 	successFlag
 		ifTrue: [  "rcvr = frac * 2^pwr, where frac is in [0.5..1.0)"
-			self cCode: 'frac = frexp(rcvr, &pwr)'
-					inSmalltalk: [rcvr exponent].
+			self cCode: 'frac = frexp(rcvr, &pwr)' inSmalltalk: [pwr _ rcvr exponent].
 			self pushInteger: pwr - 1]
 		ifFalse: [self unPop: 1].
