e3192a2aea5a24a487a4f39e76dd68a4a2c1ebb5
tpr <unknown>
2004-03-23 17:24:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveLogN.cs
+++ Interpreter/instance/float primitives/primitiveLogN.cs
@@ -5,6 +5,5 @@ primitiveLogN
 	self var: #rcvr declareC: 'double rcvr'.
 	rcvr _ self popFloat.
 	successFlag
-		ifTrue: [self pushFloat: (self cCode: 'log(rcvr)'
-									inSmalltalk: [rcvr ln])]
+		ifTrue: [self pushFloat: (self cCode: 'log(rcvr)' inSmalltalk: [rcvr ln])]
 		ifFalse: [self unPop: 1]
