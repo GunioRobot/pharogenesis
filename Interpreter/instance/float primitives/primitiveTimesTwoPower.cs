0c44ad63396a58c5aae28361a06adbb92931015f
tpr <unknown>
2004-03-23 17:25:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveTimesTwoPower.cs
+++ Interpreter/instance/float primitives/primitiveTimesTwoPower.cs
@@ -4,6 +4,5 @@ primitiveTimesTwoPower
 	arg _ self popInteger.
 	rcvr _ self popFloat.
 	successFlag
-		ifTrue: [ self pushFloat: (self cCode: 'ldexp(rcvr, arg)'
-									inSmalltalk: [rcvr timesTwoPower: arg]) ]
+		ifTrue: [ self pushFloat: (self cCode: 'ldexp(rcvr, arg)' inSmalltalk: [rcvr timesTwoPower: arg]) ]
 		ifFalse: [ self unPop: 2 ].
