18a19f3693e7211ec9a2718a92ab0f31654edfd7
tpr <unknown>
2004-03-23 17:21:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveArctan.cs
+++ Interpreter/instance/float primitives/primitiveArctan.cs
@@ -4,6 +4,5 @@ primitiveArctan
 	self var: #rcvr declareC: 'double rcvr'.
 	rcvr _ self popFloat.
 	successFlag
-		ifTrue: [self pushFloat: (self cCode: 'atan(rcvr)'
-									inSmalltalk: [rcvr arcTan])]
+		ifTrue: [self pushFloat: (self cCode: 'atan(rcvr)' inSmalltalk: [rcvr arcTan])]
 		ifFalse: [self unPop: 1]
