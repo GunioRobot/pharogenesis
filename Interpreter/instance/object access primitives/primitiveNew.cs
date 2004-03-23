6600597081d63b585c82124d48a28c70719a83c3
tpr <unknown>
2004-03-23 17:43:00 +0000

    no message

--- Interpreter/instance/object access primitives/primitiveNew.cs
+++ Interpreter/instance/object access primitives/primitiveNew.cs
@@ -1,10 +1,9 @@
 primitiveNew
-	"Allocate a new fixed-size instance. Fail if the allocation would leave less than lowSpaceThreshold bytes free."
+	"Allocate a new fixed-size instance. Fail if the allocation would leave less than lowSpaceThreshold bytes free. May cause a GC"
 
 	| class spaceOkay |
-	class _ self stackValue: 0.
+	class _ self stackTop.
 	"The following may cause GC!"
 	spaceOkay _ self sufficientSpaceToInstantiate: class indexableSize: 0.
 	self success: spaceOkay.
-	successFlag
-		ifTrue: [ self push: (self instantiateClass: self popStack indexableSize: 0) ]
+	successFlag ifTrue: [ self push: (self instantiateClass: self popStack indexableSize: 0) ]
