f3916dd45f06f2ac4d330a7f85fa0ba27050e575
tpr <unknown>
2004-03-23 17:48:00 +0000

    no message

--- Interpreter/instance/object access primitives/primitiveStoreStackp.cs
+++ Interpreter/instance/object access primitives/primitiveStoreStackp.cs
@@ -1,17 +1,14 @@
 primitiveStoreStackp
-	"Atomic store into context stackPointer.
-	Also ensures that any newly accessible cells are initialized to nil"
+	"Atomic store into context stackPointer. 
+	Also ensures that any newly accessible cells are initialized to nil "
 	| ctxt newStackp stackp |
 	ctxt _ self stackValue: 1.
 	newStackp _ self stackIntegerValue: 0.
-	self success: (newStackp >= 0).
-	self success: (newStackp <= (LargeContextSize-BaseHeaderSize // 4 - CtxtTempFrameStart)).
+	self success: newStackp >= 0.
+	self success: newStackp <= (LargeContextSize - BaseHeaderSize // 4 - CtxtTempFrameStart).
 	successFlag ifFalse: [^ self primitiveFail].
-
 	stackp _ self fetchStackPointerOf: ctxt.
-	newStackp > stackp ifTrue:
-		["Nil any newly accessible cells"
-		stackp + 1 to: newStackp do:
-			[:i | self storePointer: i+CtxtTempFrameStart-1	ofObject: ctxt withValue: nilObj]].
+	newStackp > stackp ifTrue: ["Nil any newly accessible cells"
+			stackp + 1 to: newStackp do: [:i | self storePointer: i + CtxtTempFrameStart - 1 ofObject: ctxt withValue: nilObj]].
 	self storeStackPointerValue: newStackp inContext: ctxt.
 	self pop: 1
