824ecf4edacd399ddc94ff92356e315b676f4a98
tpr <unknown>
2004-03-23 17:25:00 +0000

    no message

--- Interpreter/instance/float primitives/primitiveSquareRoot.cs
+++ Interpreter/instance/float primitives/primitiveSquareRoot.cs
@@ -4,6 +4,5 @@ primitiveSquareRoot
 	rcvr _ self popFloat.
 	self success: rcvr >= 0.0.
 	successFlag
-		ifTrue: [self pushFloat: (self cCode: 'sqrt(rcvr)'
-									inSmalltalk: [rcvr sqrt])]
+		ifTrue: [self pushFloat: (self cCode: 'sqrt(rcvr)' inSmalltalk: [rcvr sqrt])]
 		ifFalse: [self unPop: 1]
