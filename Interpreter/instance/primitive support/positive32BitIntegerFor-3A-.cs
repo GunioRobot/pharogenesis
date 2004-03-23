9915045282441e7a089a424eea532185fa3893f7
tpr <unknown>
2004-03-23 18:10:00 +0000

    no message

--- Interpreter/instance/primitive support/positive32BitIntegerFor-3A-.cs
+++ Interpreter/instance/primitive support/positive32BitIntegerFor-3A-.cs
@@ -5,16 +5,9 @@ positive32BitIntegerFor: integerValue
 		Bitmap>at:, or integer>bitAnd:."
 	(integerValue >= 0 and: [self isIntegerValue: integerValue])
 		ifTrue: [^ self integerObjectOf: integerValue].
-	newLargeInteger _
-		self instantiateSmallClass: (self splObj: ClassLargePositiveInteger)
-				sizeInBytes: 8
-						 fill: 0.
-	self storeByte: 3 ofObject: newLargeInteger
-		withValue: ((integerValue >> 24) bitAnd: 16rFF).
-	self storeByte: 2 ofObject: newLargeInteger
-		withValue: ((integerValue >> 16) bitAnd: 16rFF).
-	self storeByte: 1 ofObject: newLargeInteger
-		withValue: ((integerValue >> 8) bitAnd: 16rFF).
-	self storeByte: 0 ofObject: newLargeInteger
-		withValue: (integerValue bitAnd: 16rFF).
+	newLargeInteger _ self instantiateSmallClass: (self splObj: ClassLargePositiveInteger) sizeInBytes: 8 fill: 0.
+	self storeByte: 3 ofObject: newLargeInteger withValue: ((integerValue >> 24) bitAnd: 16rFF).
+	self storeByte: 2 ofObject: newLargeInteger withValue: ((integerValue >> 16) bitAnd: 16rFF).
+	self storeByte: 1 ofObject: newLargeInteger withValue: ((integerValue >> 8) bitAnd: 16rFF).
+	self storeByte: 0 ofObject: newLargeInteger withValue: (integerValue bitAnd: 16rFF).
 	^ newLargeInteger
