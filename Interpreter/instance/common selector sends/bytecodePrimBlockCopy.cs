470cbdc12a4dc3a92d324b9acd68db5084da7e38
tpr <unknown>
2004-03-23 18:26:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimBlockCopy.cs
+++ Interpreter/instance/common selector sends/bytecodePrimBlockCopy.cs
@@ -5,12 +5,10 @@ bytecodePrimBlockCopy
 	successFlag _ true.
 	hdr _ self baseHeader: rcvr.
 	self success: (self isContextHeader: hdr).
-	successFlag ifTrue:
-		[self externalizeIPandSP.
+	successFlag ifTrue: [self externalizeIPandSP.
 		self primitiveBlockCopy.
 		self internalizeIPandSP].
-	successFlag ifFalse:
-		[messageSelector _ self specialSelector: 24.
+	successFlag ifFalse: [messageSelector _ self specialSelector: 24.
 		argumentCount _ 1.
 		^ self normalSend].
 	self fetchNextBytecode.
