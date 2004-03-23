512a7bf3d6ff527a7e84a909653800c9da272d1f
tpr <unknown>
2004-03-23 17:55:00 +0000

    no message

--- Interpreter/instance/control primitives/primitiveDoPrimitiveWithArgs.cs
+++ Interpreter/instance/control primitives/primitiveDoPrimitiveWithArgs.cs
@@ -3,10 +3,11 @@ primitiveDoPrimitiveWithArgs
 	argumentArray _ self stackTop.
 	arraySize _ self fetchWordLengthOf: argumentArray.
 	cntxSize _ self fetchWordLengthOf: activeContext.
-	self success: (self stackPointerIndex + arraySize) < cntxSize.
-	self assertClassOf: argumentArray is: (self splObj: ClassArray).
+	self success: self stackPointerIndex + arraySize < cntxSize.
+	(self isArray: argumentArray) ifFalse: [^ self primitiveFail].
+
 	primIdx _ self stackIntegerValue: 1.
-	successFlag ifFalse: [^ self primitiveFail].  "invalid args"
+	successFlag ifFalse: [^ self primitiveFail]. "invalid args"
 
 	"Pop primIndex and argArray, then push args in place..."
 	self pop: 2.
@@ -14,20 +15,17 @@ primitiveDoPrimitiveWithArgs
 	argumentCount _ arraySize.
 	index _ 1.
 	[index <= argumentCount]
-		whileTrue:
-		[self push: (self fetchPointer: index - 1 ofObject: argumentArray).
-		index _ index + 1].
+		whileTrue: [self push: (self fetchPointer: index - 1 ofObject: argumentArray).
+			index _ index + 1].
 
 	"Run the primitive (sets successFlag)"
-	self pushRemappableOop: argumentArray.	"prim might alloc/gc"
+	self pushRemappableOop: argumentArray. "prim might alloc/gc"
 	lkupClass _ nilObj.
 	self primitiveResponse.
 	argumentArray _ self popRemappableOop.
-
-	successFlag ifFalse:
-		["If primitive failed, then restore state for failure code"
-		self pop: arraySize.
-		self pushInteger: primIdx.
-		self push: argumentArray.
-		argumentCount _ 2.
-		"... caller (execNewMeth) will run failure code"]
+	successFlag
+		ifFalse: ["If primitive failed, then restore state for failure code"
+			self pop: arraySize.
+			self pushInteger: primIdx.
+			self push: argumentArray.
+			argumentCount _ 2]
