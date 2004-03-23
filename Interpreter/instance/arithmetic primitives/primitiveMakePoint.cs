62241f28f5dc7f979a86dc115f12de7445e64879
tpr <unknown>
2004-03-23 17:01:00 +0000

    no message

--- Interpreter/instance/arithmetic primitives/primitiveMakePoint.cs
+++ Interpreter/instance/arithmetic primitives/primitiveMakePoint.cs
@@ -1,21 +1,18 @@
 primitiveMakePoint
 	| rcvr argument pt |
-	argument _ self stackValue: 0.
+	argument _ self stackTop.
 	rcvr _ self stackValue: 1.
-	(self isIntegerObject: rcvr) ifTrue:[
-		(self isIntegerObject: argument)
-			ifTrue:[pt _ self makePointwithxValue: (self integerValueOf: rcvr)
-							yValue: (self integerValueOf: argument)]
-			ifFalse:[pt _ self makePointwithxValue: (self integerValueOf: rcvr)
-							yValue: 0.
+	(self isIntegerObject: rcvr)
+		ifTrue: [(self isIntegerObject: argument)
+				ifTrue: [pt _ self makePointwithxValue: (self integerValueOf: rcvr) yValue: (self integerValueOf: argument)]
+				ifFalse: [pt _ self makePointwithxValue: (self integerValueOf: rcvr) yValue: 0.
 					"Above may cause GC!"
-					self storePointer: 1 ofObject: pt withValue: (self stackValue: 0)].
-	] ifFalse:[
-		(self isFloatObject: rcvr) ifFalse:[^self success: false].
-		pt _ self makePointwithxValue: 0 yValue: 0.
-		"Above may cause GC!"
-		self storePointer: 0 ofObject: pt withValue: (self stackValue: 1).
-		self storePointer: 1 ofObject: pt withValue: (self stackValue: 0).
-	].
-	self pop: 2.
-	self push: pt.
+					self storePointer: 1 ofObject: pt withValue: (self stackValue: 0)]]
+		ifFalse: [(self isFloatObject: rcvr)
+				ifFalse: [^ self success: false].
+			pt _ self makePointwithxValue: 0 yValue: 0.
+			"Above may cause GC!"
+			self storePointer: 0 ofObject: pt withValue: (self stackValue: 1).
+			self storePointer: 1 ofObject: pt withValue: (self stackValue: 0)].
+
+	self pop: 2 thenPush: pt
