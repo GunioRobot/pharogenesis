0d0ee423a72b3b8f422779c761f12d8f92bfa356
tpr <unknown>
2004-03-24 22:02:00 +0000

    no message

--- ObjectMemory/instance/garbage collection/fullCompaction.cs
+++ ObjectMemory/instance/garbage collection/fullCompaction.cs
@@ -1,16 +1,18 @@
 fullCompaction
-	"Move all accessible objects down to leave one big free chunk at the end of memory."
-	"Assume: Incremental GC has just been done to maximimize forwarding table space."
-
+	"Move all accessible objects down to leave one big free chunk 
+	at the end of memory."
+	"Assume: Incremental GC has just been done to maximimize 
+	forwarding table space."
 	"need not move objects below the first free chunk"
-	compStart _ self lowestFreeAfter: (self startOfMemory).
-	compStart = freeBlock ifTrue: [
-		"memory is already compact; only free chunk is at the end"
-		^ self initializeMemoryFirstFree: freeBlock
-	].
-
+	| sz |
+	compStart _ self lowestFreeAfter: self startOfMemory.
+	compStart = freeBlock
+		ifTrue: ["memory is already compact; only free chunk is at the end "
+			^ self initializeMemoryFirstFree: freeBlock].
+	(sz _ self fwdTableSize: 8) < totalObjectCount
+		ifTrue: ["Try to grow OM to make a single pass full GC"
+			self growObjectMemory: totalObjectCount - sz + 10000 * 8].
 	"work up through memory until all free space is at the end"
-	[compStart < freeBlock] whileTrue: [
-		"free chunk returned by incCompBody becomes start of next compaction"
-		compStart _ self incCompBody.  "bubble of free space moves up each time"
-	].
+	[compStart < freeBlock]
+		whileTrue: ["free chunk returned by incCompBody becomes start of next compaction"
+			compStart _ self incCompBody]
