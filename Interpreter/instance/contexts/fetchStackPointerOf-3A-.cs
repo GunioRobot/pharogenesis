3cb8ccce836f081732c39844df6bc4a1e1150d1c
tpr <unknown>
2004-03-24 21:01:00 +0000

    no message

--- Interpreter/instance/contexts/fetchStackPointerOf-3A-.cs
+++ Interpreter/instance/contexts/fetchStackPointerOf-3A-.cs
@@ -3,5 +3,5 @@ fetchStackPointerOf: aContext
 	| sp |
 	self inline: true.
 	sp _ self fetchPointer: StackPointerIndex ofObject: aContext.
-	(self isIntegerObject: sp) ifFalse: [^ 0].
-	^ self integerValueOf: sp
+	(self isIntegerObject: sp) ifFalse: [^0].
+	^self integerValueOf: sp
