e6629121c9028a0192d1d92ad788db6071b504d0
tpr <unknown>
2004-03-24 18:29:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimGreaterOrEqual.cs
+++ Interpreter/instance/common selector sends/bytecodePrimGreaterOrEqual.cs
@@ -1,17 +1,14 @@
 bytecodePrimGreaterOrEqual
-	| rcvr arg bool |
+	| rcvr arg aBool |
 	rcvr _ self internalStackValue: 1.
 	arg _ self internalStackValue: 0.
 	(self areIntegers: rcvr and: arg) ifTrue:
-		[self cCode: '' inSmalltalk:
-			[^ self booleanCheat:
-					(self integerValueOf: rcvr) >= (self integerValueOf: arg)].
-		^ self booleanCheat: rcvr >= arg].
+		[self cCode: '' inSmalltalk: [^self booleanCheat: (self integerValueOf: rcvr) >= (self integerValueOf: arg)].
+		^self booleanCheat: rcvr >= arg].
 
 	successFlag _ true.
-	bool _ self primitiveFloatLess: rcvr thanArg: arg.
-	successFlag ifTrue:
-		[^ self booleanCheat: bool not].
+	aBool _ self primitiveFloatLess: rcvr thanArg: arg.
+	successFlag ifTrue: [^self booleanCheat: aBool not].
 
 	messageSelector _ self specialSelector: 5.
 	argumentCount _ 1.
