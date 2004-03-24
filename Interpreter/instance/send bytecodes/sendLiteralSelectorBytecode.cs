ed28bf0eca26b92d0928cd1527cf5d5662401546
tpr <unknown>
2004-03-24 18:36:00 +0000

    no message

--- Interpreter/instance/send bytecodes/sendLiteralSelectorBytecode.cs
+++ Interpreter/instance/send bytecodes/sendLiteralSelectorBytecode.cs
@@ -3,16 +3,4 @@ sendLiteralSelectorBytecode
 
 	messageSelector _ self literal: (currentBytecode bitAnd: 16rF).
 	argumentCount _ ((currentBytecode >> 4) bitAnd: 3) - 1.
-	self normalSend.
-"
-Note - if you ever want to try inlining these, the following will produce the code to paste into interp.c -- I found it actually slowed things down (cache size effect?).
-String streamContents:
-	[:s | 208 to: 255 do:
-		[:i | s tab; tab; nextPutAll: 'case ', i printString, ':'; cr.
-		s tab; tab; tab; nextPutAll: 'messageSelector = longAt(((char *) method) + 4 + ';
-			print: ((i bitAnd: 15) + 1) << 2; nextPutAll: ');'; cr.
-		s tab; tab; tab; nextPutAll: 'argumentCount = ';
-			print: (i >> 4 bitAnd: 3) - 1; nextPutAll: ';'; cr.
-		s tab; tab; tab; nextPutAll: 'goto commonSend;'; cr.
-		s tab; tab; tab; nextPutAll: 'break;'; cr; cr]]
-"
+	self normalSend
