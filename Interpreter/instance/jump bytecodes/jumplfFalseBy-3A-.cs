6dfb1e0b5804ee1feef4012f362e89bfa0ab6eef
tpr <unknown>
2004-03-24 18:36:00 +0000

    no message

--- Interpreter/instance/jump bytecodes/jumplfFalseBy-3A-.cs
+++ Interpreter/instance/jump bytecodes/jumplfFalseBy-3A-.cs
@@ -1,15 +1,11 @@
-jumplfFalseBy: offset
-
+jumplfFalseBy: offset 
 	| boolean |
 	boolean _ self internalStackTop.
-	boolean = falseObj ifTrue: [
-		self jump: offset.
-	] ifFalse: [
-		boolean = trueObj ifFalse: [
-			messageSelector _ self splObj: SelectorMustBeBoolean.
-			argumentCount _ 0.
-			^ self normalSend
-		].
-		self fetchNextBytecode.
-	].
-	self internalPop: 1.
+	boolean = falseObj
+		ifTrue: [self jump: offset]
+		ifFalse: [boolean = trueObj
+				ifFalse: [messageSelector _ self splObj: SelectorMustBeBoolean.
+					argumentCount _ 0.
+					^ self normalSend].
+			self fetchNextBytecode].
+	self internalPop: 1
