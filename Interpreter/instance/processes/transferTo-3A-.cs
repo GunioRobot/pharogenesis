a481e02c1ad8f21a9d7b97b416dd4cb4c7441036
tpr <unknown>
2004-03-24 20:58:00 +0000

    no message

--- Interpreter/instance/processes/transferTo-3A-.cs
+++ Interpreter/instance/processes/transferTo-3A-.cs
@@ -1,16 +1,15 @@
-transferTo: aProc
-	"Record a process to be awoken on the next interpreter cycle.
-	ikp 11/24/1999 06:07 -- added hook for external runtime compiler"
-
+transferTo: aProc 
+	"Record a process to be awoken on the next interpreter cycle. 
+	ikp 11/24/1999 06:07 -- added hook for external runtime 
+	compiler "
 	| sched oldProc newProc |
 	newProc _ aProc.
 	sched _ self schedulerPointer.
 	oldProc _ self fetchPointer: ActiveProcessIndex ofObject: sched.
 	self storePointer: ActiveProcessIndex ofObject: sched withValue: newProc.
-	compilerInitialized ifTrue: [
-		self compilerProcessChange: oldProc to: newProc.
-	] ifFalse: [
-		self storePointer: SuspendedContextIndex ofObject: oldProc withValue: activeContext.
-		self newActiveContext: (self fetchPointer: SuspendedContextIndex ofObject: newProc)
-	].
-	reclaimableContextCount _ 0.
+	compilerInitialized
+		ifTrue: [self compilerProcessChange: oldProc to: newProc]
+		ifFalse: [self storePointer: SuspendedContextIndex ofObject: oldProc withValue: activeContext.
+			self newActiveContext: (self fetchPointer: SuspendedContextIndex ofObject: newProc).
+			self storePointer: SuspendedContextIndex ofObject: newProc withValue: nilObj].
+	reclaimableContextCount _ 0
