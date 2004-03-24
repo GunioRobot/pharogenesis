224f34cf2b9aebe2c84fe6f77cf513224b91be4e
tpr <unknown>
2004-03-24 13:06:00 +0000

    no message

--- BitBltSimulation/instance/primitives/primitiveCopyBits.cs
+++ BitBltSimulation/instance/primitives/primitiveCopyBits.cs
@@ -3,7 +3,12 @@ primitiveCopyBits
 	| rcvr |
 	self export: true.
 	rcvr _ interpreterProxy stackValue: interpreterProxy methodArgumentCount.
-	(self loadBitBltFrom: rcvr) 
-		ifFalse:[^interpreterProxy primitiveFail].
+	(self loadBitBltFrom: rcvr)  ifFalse:[^interpreterProxy primitiveFail].
 	self copyBits.
+	interpreterProxy failed ifTrue:[^nil].
 	self showDisplayBits.
+	interpreterProxy failed ifTrue:[^nil].
+	interpreterProxy pop: interpreterProxy methodArgumentCount.
+	(combinationRule = 22) | (combinationRule = 32) ifTrue:[
+		interpreterProxy pop: 1.
+		^ interpreterProxy pushInteger: bitCount].
