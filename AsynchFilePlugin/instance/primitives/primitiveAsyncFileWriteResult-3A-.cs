09ae43b9b93cd501648367f425ea77d58e126c01
tpr <unknown>
2004-03-24 14:54:00 +0000

    no message

--- AsynchFilePlugin/instance/primitives/primitiveAsyncFileWriteResult-3A-.cs
+++ AsynchFilePlugin/instance/primitives/primitiveAsyncFileWriteResult-3A-.cs
@@ -2,8 +2,7 @@ primitiveAsyncFileWriteResult: fHandle
 
 	| f r |
 	self var: #f declareC: 'AsyncFile *f'.
-	self primitive: 'primitiveAsyncFileWriteResult'
-		parameters:#(Oop).
+	self primitive: 'primitiveAsyncFileWriteResult' parameters:#(Oop).
 
 	f _ self asyncFileValueOf: fHandle.
 	r _ self cCode:' asyncFileWriteResult(f)'.
