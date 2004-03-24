7a44d971f19bd0ba7c43d475bb644a957525e3a8
tpr <unknown>
2004-03-24 21:54:00 +0000

    no message

--- ObjectMemory/instance/object enumeration/firstAccessibleObject.cs
+++ ObjectMemory/instance/object enumeration/firstAccessibleObject.cs
@@ -1,10 +1,8 @@
 firstAccessibleObject
 	"Return the first accessible object in the heap."
-
 	| obj |
 	obj _ self firstObject.
-	[obj < endOfMemory] whileTrue: [
-		(self isFreeObject: obj) ifFalse: [ ^obj ].
-		obj _ self objectAfter: obj.
-	].
+	[obj < endOfMemory]
+		whileTrue: [(self isFreeObject: obj) ifFalse: [^ obj].
+			obj _ self objectAfter: obj].
 	self error: 'heap is empty'
