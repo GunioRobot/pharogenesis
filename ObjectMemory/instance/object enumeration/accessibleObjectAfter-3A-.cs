58e9bf1b3c5b717866695c3167e06a243bbd2031
tpr <unknown>
2004-03-24 21:31:00 +0000

    no message

--- ObjectMemory/instance/object enumeration/accessibleObjectAfter-3A-.cs
+++ ObjectMemory/instance/object enumeration/accessibleObjectAfter-3A-.cs
@@ -1,11 +1,10 @@
-accessibleObjectAfter: oop
-	"Return the accessible object following the given object or free chunk in the heap. Return nil when heap is exhausted."
-
+accessibleObjectAfter: oop 
+	"Return the accessible object following the given object or 
+	free chunk in the heap. Return nil when heap is exhausted."
 	| obj |
 	self inline: false.
 	obj _ self objectAfter: oop.
-	[obj < endOfMemory] whileTrue: [
-		(self isFreeObject: obj) ifFalse: [ ^obj ].
-		obj _ self objectAfter: obj.
-	].
+	[obj < endOfMemory]
+		whileTrue: [(self isFreeObject: obj) ifFalse: [^ obj].
+			obj _ self objectAfter: obj].
 	^ nil
