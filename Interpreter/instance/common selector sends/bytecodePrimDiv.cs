eb9c06ebeb68c94cdb0ac5a9e395f1cf167ed67d
tpr <unknown>
2004-03-24 18:26:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimDiv.cs
+++ Interpreter/instance/common selector sends/bytecodePrimDiv.cs
@@ -1,10 +1,8 @@
 bytecodePrimDiv
 	| quotient |
 	successFlag _ true.
-	quotient _ self doPrimitiveDiv: (self internalStackValue: 1)
-							by: (self internalStackValue: 0).
-	successFlag ifTrue:
-		[self internalPop: 2 thenPush: (self integerObjectOf: quotient).
+	quotient _ self doPrimitiveDiv: (self internalStackValue: 1) by: (self internalStackValue: 0).
+	successFlag ifTrue: [self internalPop: 2 thenPush: (self integerObjectOf: quotient).
 		^ self fetchNextBytecode "success"].
 
 	messageSelector _ self specialSelector: 13.
