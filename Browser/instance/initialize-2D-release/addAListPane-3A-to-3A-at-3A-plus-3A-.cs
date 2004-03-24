13b38d61ea90bd153aeffac7f03acf89d6ad1306
sps <unknown>
2004-03-24 11:50:00 +0000

    no message

--- Browser/instance/initialize-2D-release/addAListPane-3A-to-3A-at-3A-plus-3A-.cs
+++ Browser/instance/initialize-2D-release/addAListPane-3A-to-3A-at-3A-plus-3A-.cs
@@ -1,6 +1,6 @@
 addAListPane: aListPane to: window at: nominalFractions plus: verticalOffset
 
-	| row switchHeight |
+	| row switchHeight divider |
 
 	row _ AlignmentMorph newColumn
 		hResizing: #spaceFill;
@@ -9,22 +9,6 @@ addAListPane: aListPane to: window at: nominalFractions plus: verticalOffset
 		borderWidth: 1;
 		layoutPolicy: ProportionalLayout new.
 	switchHeight _ 25.
-	row 
-		addMorph: aListPane
-		fullFrame: (
-			LayoutFrame 
-				fractions: (0@0 corner: 1@1) 
-				offsets: (0@0 corner: 0@switchHeight negated)
-		).	
-
-	row 
-		addMorph: SubpaneDividerMorph forTopEdge
-		fullFrame: (
-			LayoutFrame 
-				fractions: (0@1 corner: 1@1) 
-				offsets: (0@switchHeight negated corner: 0@(1-switchHeight))
-		).	
-
 	self 
 		addMorphicSwitchesTo: row 
 		at: (
@@ -32,6 +16,26 @@ addAListPane: aListPane to: window at: nominalFractions plus: verticalOffset
 				fractions: (0@1 corner: 1@1) 
 				offsets: (0@(1-switchHeight)  corner: 0@0)
 		).
+
+	divider _ BorderedSubpaneDividerMorph forTopEdge.
+	Preferences alternativeWindowLook ifTrue:[
+		divider extent: 4@4; color: Color transparent; borderColor: #raised; borderWidth: 2.
+	].
+	row 
+		addMorph: divider
+		fullFrame: (
+			LayoutFrame 
+				fractions: (0@1 corner: 1@1) 
+				offsets: (0@switchHeight negated corner: 0@(1-switchHeight))
+		).	
+
+	row 
+		addMorph: aListPane
+		fullFrame: (
+			LayoutFrame 
+				fractions: (0@0 corner: 1@1) 
+				offsets: (0@0 corner: 0@(switchHeight negated))
+		).	
 
 	window 
 		addMorph: row
