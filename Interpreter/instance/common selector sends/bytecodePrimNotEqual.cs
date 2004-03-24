8cd49d74081647352523779887a14ea893f0b58b
tpr <unknown>
2004-03-24 18:32:00 +0000

    no message

--- Interpreter/instance/common selector sends/bytecodePrimNotEqual.cs
+++ Interpreter/instance/common selector sends/bytecodePrimNotEqual.cs
@@ -1,14 +1,12 @@
 bytecodePrimNotEqual
-	| rcvr arg bool |
+	| rcvr arg aBool |
 	rcvr _ self internalStackValue: 1.
 	arg _ self internalStackValue: 0.
-	(self areIntegers: rcvr and: arg) ifTrue:
-		[^ self booleanCheat: rcvr ~= arg].
+	(self areIntegers: rcvr and: arg) ifTrue: [^self booleanCheat: rcvr ~= arg].
 
 	successFlag _ true.
-	bool _ self primitiveFloatEqual: rcvr toArg: arg.
-	successFlag ifTrue:
-		[^ self booleanCheat: bool not].
+	aBool _ self primitiveFloatEqual: rcvr toArg: arg.
+	successFlag ifTrue: [^self booleanCheat: aBool not].
 
 	messageSelector _ self specialSelector: 7.
 	argumentCount _ 1.
