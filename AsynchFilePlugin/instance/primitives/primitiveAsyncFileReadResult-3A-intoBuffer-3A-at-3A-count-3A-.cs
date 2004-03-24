26e980093f1317379017d08d56a093057f374c3d
tpr <unknown>
2004-03-24 14:53:00 +0000

    no message

--- AsynchFilePlugin/instance/primitives/primitiveAsyncFileReadResult-3A-intoBuffer-3A-at-3A-count-3A-.cs
+++ AsynchFilePlugin/instance/primitives/primitiveAsyncFileReadResult-3A-intoBuffer-3A-at-3A-count-3A-.cs
@@ -1,10 +1,9 @@
 primitiveAsyncFileReadResult: fhandle intoBuffer: buffer at: start count: num 
 	| bufferSize bufferPtr r f count startIndex |
 	self var: #f declareC: 'AsyncFile *f'.
-	self primitive: 'primitiveAsyncFileReadResult'
-		parameters: #(Oop Oop SmallInteger SmallInteger ).
-	f _ self asyncFileValueOf: fhandle.
+	self primitive: 'primitiveAsyncFileReadResult' parameters: #(Oop Oop SmallInteger SmallInteger ).
 
+	f _ self asyncFileValueOf: fhandle.
 	count _ num.
 	startIndex _ start.
 	bufferSize _ interpreterProxy slotSizeOf: buffer. "in bytes or words"
@@ -15,8 +14,6 @@ primitiveAsyncFileReadResult: fhandle intoBuffer: buffer at: start count: num
 			bufferSize _ bufferSize * 4].
 	interpreterProxy success: (startIndex >= 1 and: [startIndex + count - 1 <= bufferSize]).
 
-	bufferPtr _ (self cCoerce: (interpreterProxy firstIndexableField: buffer)
-				to: 'int')
-				+ startIndex - 1. 	"adjust for zero-origin indexing"
+	bufferPtr _ (self cCoerce: (interpreterProxy firstIndexableField: buffer) to: 'int') + startIndex - 1. 	"adjust for zero-origin indexing"
 	interpreterProxy failed ifFalse: [r _ self cCode: 'asyncFileReadResult(f, bufferPtr, count)'].
 	^ r asOop: SmallInteger
