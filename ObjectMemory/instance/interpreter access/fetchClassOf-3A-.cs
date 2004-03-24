a3afe307a820a1b070f072b66a9e7453da8e31e1
tpr <unknown>
2004-03-24 12:35:00 +0000

    no message

--- ObjectMemory/instance/interpreter access/fetchClassOf-3A-.cs
+++ ObjectMemory/instance/interpreter access/fetchClassOf-3A-.cs
@@ -1,13 +1,11 @@
-fetchClassOf: oop
+fetchClassOf: oop 
 	| ccIndex |
 	self inline: true.
-	(self isIntegerObject: oop)
-		ifTrue: [ ^ self splObj: ClassInteger ].
+	(self isIntegerObject: oop) ifTrue: [^ self splObj: ClassInteger].
 
-	ccIndex _ ((self baseHeader: oop) >> 12) bitAnd: 16r1F.
+	ccIndex _ (self baseHeader: oop) >> 12 bitAnd: 31.
 	ccIndex = 0
-		ifTrue: [^ (self classHeader: oop) bitAnd: AllButTypeMask ]
+		ifTrue: [^ (self classHeader: oop)
+				bitAnd: AllButTypeMask]
 		ifFalse: ["look up compact class"
-				^ self fetchPointer: ccIndex - 1
-					ofObject: (self fetchPointer: CompactClasses ofObject: specialObjectsOop)
-		].
+			^ self fetchPointer: ccIndex - 1 ofObject: (self fetchPointer: CompactClasses ofObject: specialObjectsOop)]
