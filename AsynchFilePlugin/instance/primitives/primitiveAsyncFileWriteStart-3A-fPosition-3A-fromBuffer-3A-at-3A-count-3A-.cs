dea6c3dddde7f44fe5209659d066c35f3ec7239d
tpr <unknown>
2004-03-24 14:54:00 +0000

    no message

--- AsynchFilePlugin/instance/primitives/primitiveAsyncFileWriteStart-3A-fPosition-3A-fromBuffer-3A-at-3A-count-3A-.cs
+++ AsynchFilePlugin/instance/primitives/primitiveAsyncFileWriteStart-3A-fPosition-3A-fromBuffer-3A-at-3A-count-3A-.cs
@@ -1,8 +1,7 @@
 primitiveAsyncFileWriteStart: fHandle fPosition: fPosition fromBuffer: buffer at: start count: num 
 	| f bufferSize bufferPtr count startIndex |
 	self var: #f declareC: 'AsyncFile *f'.
-	self primitive: 'primitiveAsyncFileWriteStart'
-		parameters: #(Oop SmallInteger Oop SmallInteger SmallInteger ).
+	self primitive: 'primitiveAsyncFileWriteStart' parameters: #(Oop SmallInteger Oop SmallInteger SmallInteger ).
 	f _ self asyncFileValueOf: fHandle.
 	interpreterProxy failed ifTrue: [^ nil].
 
@@ -15,7 +14,5 @@ primitiveAsyncFileWriteStart: fHandle fPosition: fPosition fromBuffer: buffer at
 			startIndex _ startIndex - 1 * 4 + 1.
 			bufferSize _ bufferSize * 4].
 	interpreterProxy success: (startIndex >= 1 and: [startIndex + count - 1 <= bufferSize]).
-	bufferPtr _ (self cCoerce: (interpreterProxy firstIndexableField: buffer)
-				to: 'int')
-				+ startIndex - 1.	"adjust for zero-origin indexing"
+	bufferPtr _ (self cCoerce: (interpreterProxy firstIndexableField: buffer) to: 'int') + startIndex - 1.	"adjust for zero-origin indexing"
 	interpreterProxy failed ifFalse: [self cCode: 'asyncFileWriteStart(f, fPosition, bufferPtr, count)']
