a5420b8c07a635fefa28e6189145d77cdceaaa1d
tpr <unknown>
2004-03-24 12:58:00 +0000

    no message

--- ObjectMemory/instance/interpreter access/storePointer-3A-ofObject-3A-withValue-3A-.cs
+++ ObjectMemory/instance/interpreter access/storePointer-3A-ofObject-3A-withValue-3A-.cs
@@ -1,9 +1,5 @@
-storePointer: fieldIndex ofObject: oop withValue: valuePointer
-	"Note must check here for stores of young objects into old ones."
+storePointer: fieldIndex ofObject: oop withValue: valuePointer 
+	"Note must check here for stores of young objects into old ones. "
 
-	(oop < youngStart) ifTrue: [
-		self possibleRootStoreInto: oop value: valuePointer.
-	].
-
-	^ self longAt: (self cCoerce: oop to: 'char *') + BaseHeaderSize + (fieldIndex << 2)
-		put: valuePointer
+	oop < youngStart ifTrue: [self possibleRootStoreInto: oop value: valuePointer].
+	^ self longAt: (self cCoerce: oop to: 'char *') + BaseHeaderSize + (fieldIndex << 2) put: valuePointer
