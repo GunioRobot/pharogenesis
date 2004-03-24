43f8060e8d29aab5668f8486cc28d4231b577891
tpr <unknown>
2004-03-24 21:49:00 +0000

    no message

--- ObjectMemory/instance/memory access/checkAddress-3A-.cs
+++ ObjectMemory/instance/memory access/checkAddress-3A-.cs
@@ -1,9 +1,6 @@
-checkAddress: byteAddress
+checkAddress: byteAddress 
 	"Keep this method around for debugging the C code."
-
-	byteAddress < (self startOfMemory) ifTrue: [
-		self error: 'bad address: negative'.
-	].
-	byteAddress >= memoryLimit ifTrue: [
-		self error: 'bad address: past end of heap'.
-	].
+	byteAddress < self startOfMemory
+		ifTrue: [self error: 'bad address: negative'].
+	byteAddress >= memoryLimit
+		ifTrue: [self error: 'bad address: past end of heap']
