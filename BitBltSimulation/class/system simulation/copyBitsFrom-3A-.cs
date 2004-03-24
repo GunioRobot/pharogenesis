6108575fdc349a614ff84d3027e7c06e0d9d69ab
tpr <unknown>
2004-03-24 13:01:00 +0000

    no message

--- BitBltSimulation/class/system simulation/copyBitsFrom-3A-.cs
+++ BitBltSimulation/class/system simulation/copyBitsFrom-3A-.cs
@@ -2,13 +2,12 @@ copyBitsFrom: aBitBlt
 	"Simulate the copyBits primitive"
 	| proxy bb |
 	proxy _ InterpreterProxy new.
-	proxy loadStackFrom: thisContext sender.
+	proxy loadStackFrom: thisContext sender home.
 	bb _ self simulatorClass new.
+	bb initialiseModule.
 	bb setInterpreter: proxy.
 	proxy success: (bb loadBitBltFrom: aBitBlt).
 	bb copyBits.
 	proxy failed ifFalse:[
-		proxy showDisplayBits: aBitBlt destForm 
-				Left: bb affectedLeft Top: bb affectedTop 
-				Right: bb affectedRight Bottom: bb affectedBottom].
+		proxy showDisplayBits: aBitBlt destForm Left: bb affectedLeft Top: bb affectedTop Right: bb affectedRight Bottom: bb affectedBottom].
 	^proxy stackValue: 0
