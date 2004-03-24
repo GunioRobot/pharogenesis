3f57cdfc696a9eac2af9729c84786654568a29b7
tpr <unknown>
2004-03-24 12:58:00 +0000

    no message

--- ObjectMemory/instance/interpreter access/storePointerUnchecked-3A-ofObject-3A-withValue-3A-.cs
+++ ObjectMemory/instance/interpreter access/storePointerUnchecked-3A-ofObject-3A-withValue-3A-.cs
@@ -1,5 +1,4 @@
 storePointerUnchecked: fieldIndex ofObject: oop withValue: valuePointer
 	"Like storePointer:ofObject:withValue:, but the caller guarantees that the object being stored into is a young object or is already marked as a root."
 
-	^ self longAt: (self cCoerce: oop to: 'char *') + BaseHeaderSize + (fieldIndex << 2)
-			put: valuePointer
+	^ self longAt: (self cCoerce: oop to: 'char *') + BaseHeaderSize + (fieldIndex << 2) put: valuePointer
