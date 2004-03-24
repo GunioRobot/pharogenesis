77e124bbbcbcfe8b6f05a2fb2b6560b1537fbc8d
tpr <unknown>
2004-03-24 21:22:00 +0000

    no message

--- Interpreter/instance/initialization/moduleUnloaded-3A-.cs
+++ Interpreter/instance/initialization/moduleUnloaded-3A-.cs
@@ -1,9 +1,8 @@
-moduleUnloaded: aModuleName
-	"The module with the given name was just unloaded.
+moduleUnloaded: aModuleName 
+	"The module with the given name was just unloaded. 
 	Make sure we have no dangling references."
 	self export: true.
 	self var: #aModuleName type: 'char *'.
-	(aModuleName strcmp: 'SurfacePlugin') = 0 ifTrue:[
-		"Surface plugin went away. Should never happen. But then, who knows"
-		showSurfaceFn _ 0.
-	].
+	(aModuleName strcmp: 'SurfacePlugin') = 0
+		ifTrue: ["Surface plugin went away. Should never happen. But  then, who knows"
+			showSurfaceFn _ 0]
