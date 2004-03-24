ec553dcb1ff7fa853b48370f79889dc1343ee6ba
tpr <unknown>
2004-03-24 20:57:00 +0000

    no message

--- Interpreter/instance/processes/signalExternalSemaphores.cs
+++ Interpreter/instance/processes/signalExternalSemaphores.cs
@@ -4,21 +4,21 @@ signalExternalSemaphores
 	semaphoresUseBufferA _ semaphoresUseBufferA not.
 	xArray _ self splObj: ExternalObjectsArray.
 	xSize _ self stSizeOf: xArray.
-	semaphoresUseBufferA ifTrue: ["use opposite buffer during read"
-		1 to: semaphoresToSignalCountB do: [:i |
-			index _ semaphoresToSignalB at: i.
-			(index <= xSize) ifTrue:[
-				sema _ self fetchPointer: index - 1 ofObject: xArray.
-					"Note: semaphore indices are 1-based"
-				(self fetchClassOf: sema) = (self splObj: ClassSemaphore)
-					ifTrue: [self synchronousSignal: sema]]].
-		semaphoresToSignalCountB _ 0]
-	ifFalse: [
-		1 to: semaphoresToSignalCountA do: [:i |
-			index _ semaphoresToSignalA at: i.
-			(index <= xSize) ifTrue:[
-				sema _ self fetchPointer: index - 1 ofObject: xArray.
-					"Note: semaphore indices are 1-based"
-				(self fetchClassOf: sema) = (self splObj: ClassSemaphore)
-					ifTrue: [self synchronousSignal: sema]]].
-		semaphoresToSignalCountA _ 0]
+	semaphoresUseBufferA
+		ifTrue: ["use opposite buffer during read"
+			1 to: semaphoresToSignalCountB do: [:i | 
+					index _ semaphoresToSignalB at: i.
+					index <= xSize
+						ifTrue: [sema _ self fetchPointer: index - 1 ofObject: xArray.
+							"Note: semaphore indices are 1-based"
+							(self fetchClassOf: sema) = (self splObj: ClassSemaphore)
+								ifTrue: [self synchronousSignal: sema]]].
+			semaphoresToSignalCountB _ 0]
+		ifFalse: [1 to: semaphoresToSignalCountA do: [:i | 
+					index _ semaphoresToSignalA at: i.
+					index <= xSize
+						ifTrue: [sema _ self fetchPointer: index - 1 ofObject: xArray.
+							"Note: semaphore indices are 1-based"
+							(self fetchClassOf: sema) = (self splObj: ClassSemaphore)
+								ifTrue: [self synchronousSignal: sema]]].
+			semaphoresToSignalCountA _ 0]
