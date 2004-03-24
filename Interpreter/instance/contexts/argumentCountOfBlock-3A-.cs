e58f8a78983ed464ec4b1bcec694a716b55c1904
tpr <unknown>
2004-03-24 20:59:00 +0000

    no message

--- Interpreter/instance/contexts/argumentCountOfBlock-3A-.cs
+++ Interpreter/instance/contexts/argumentCountOfBlock-3A-.cs
@@ -1,8 +1,5 @@
 argumentCountOfBlock: blockPointer
 
 	| localArgCount |
-	localArgCount _ self fetchPointer: BlockArgumentCountIndex
-							ofObject: blockPointer.
-	(self isIntegerObject: localArgCount)
-		ifTrue: [ ^ self integerValueOf: localArgCount ]
-		ifFalse: [ self primitiveFail. ^0 ].
+	localArgCount _ self fetchPointer: BlockArgumentCountIndex ofObject: blockPointer.
+	^self checkedIntegerValueOf: localArgCount
