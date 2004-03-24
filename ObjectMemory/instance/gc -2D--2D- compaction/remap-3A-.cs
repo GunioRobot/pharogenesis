b9f5778b6ee0a090dd52fbaa28d29f549689a310
tpr <unknown>
2004-03-24 10:48:00 +0000

    no message

--- ObjectMemory/instance/gc -2D--2D- compaction/remap-3A-.cs
+++ ObjectMemory/instance/gc -2D--2D- compaction/remap-3A-.cs
@@ -1,12 +1,13 @@
-remap: oop
-	"Map the given oop to its new value during a compaction or become: operation. If it has no forwarding table entry, return the oop itself."
-
+remap: oop 
+	"Map the given oop to its new value during a compaction or 
+	become: operation. If it has no forwarding table entry, 
+	return the oop itself."
 	| fwdBlock |
 	self inline: false.
-	(self isObjectForwarded: oop) ifTrue: [
-		"get the new value for oop from its forwarding block"
-		fwdBlock _ ((self longAt: oop) bitAnd: AllButMarkBitAndTypeMask) << 1.
-		DoAssertionChecks ifTrue: [ self fwdBlockValidate: fwdBlock ].
-		^ self longAt: fwdBlock
-	].
+	(self isObjectForwarded: oop)
+		ifTrue: ["get the new value for oop from its forwarding block"
+			fwdBlock _ ((self longAt: oop) bitAnd: AllButMarkBitAndTypeMask) << 1.
+			DoAssertionChecks
+				ifTrue: [self fwdBlockValidate: fwdBlock].
+			^ self longAt: fwdBlock].
 	^ oop
