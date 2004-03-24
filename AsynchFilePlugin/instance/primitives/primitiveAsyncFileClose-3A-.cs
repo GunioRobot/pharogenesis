441393a5a1d290ebcadde1516771850e2198edc7
tpr <unknown>
2004-03-24 14:50:00 +0000

    no message

--- AsynchFilePlugin/instance/primitives/primitiveAsyncFileClose-3A-.cs
+++ AsynchFilePlugin/instance/primitives/primitiveAsyncFileClose-3A-.cs
@@ -1,7 +1,6 @@
 primitiveAsyncFileClose: fh 
 	| f |
 	self var: #f declareC: 'AsyncFile *f'.
-	self primitive: 'primitiveAsyncFileClose'
-		parameters: #(Oop ).
+	self primitive: 'primitiveAsyncFileClose' parameters: #(Oop ).
 	f _ self asyncFileValueOf: fh.
 	self asyncFileClose: f
