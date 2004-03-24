1fcf6bd2bf11b012a36285e55621d723784322f5
tpr <unknown>
2004-03-24 21:08:00 +0000

    no message

--- Interpreter/instance/compiled methods/methodClassOf-3A-.cs
+++ Interpreter/instance/compiled methods/methodClassOf-3A-.cs
@@ -1,5 +1,3 @@
 methodClassOf: methodPointer
 
-	^ self fetchPointer: ValueIndex ofObject:
-		(self literal: (self literalCountOf: methodPointer) - 1
-			ofMethod: methodPointer)
+	^ self fetchPointer: ValueIndex ofObject: (self literal: (self literalCountOf: methodPointer) - 1 ofMethod: methodPointer)
