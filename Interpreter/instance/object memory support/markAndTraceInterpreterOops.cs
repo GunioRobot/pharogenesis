889645654e6e4e8ddee7d481d80a2fb786812afc
tpr <unknown>
2004-03-24 21:20:00 +0000

    no message

--- Interpreter/instance/object memory support/markAndTraceInterpreterOops.cs
+++ Interpreter/instance/object memory support/markAndTraceInterpreterOops.cs
@@ -3,27 +3,16 @@ markAndTraceInterpreterOops
 	"Assume: All traced variables contain valid oops."
 	| oop |
 	self compilerMarkHook.
-
-	self markAndTrace: specialObjectsOop.
-		"also covers nilObj, trueObj, falseObj, and compact classes"
-
+	self markAndTrace: specialObjectsOop. "also covers nilObj, trueObj, falseObj, and compact classes"
 	compilerInitialized
-		ifTrue:
-			[self markAndTrace: receiver.
-			 self markAndTrace: method]
-		ifFalse:
-			[self markAndTrace: activeContext.  "traces entire stack"
-				"also covers theHomeContext, receiver, method"].
-
+		ifTrue: [self markAndTrace: receiver.
+			self markAndTrace: method]
+		ifFalse: [self markAndTrace: activeContext].
 	self markAndTrace: messageSelector.
 	self markAndTrace: newMethod.
 	self markAndTrace: methodClass.
 	self markAndTrace: lkupClass.
 	self markAndTrace: receiverClass.
-
-	1 to: remapBufferCount do: [ :i |
-		oop _ remapBuffer at: i.
-		(self isIntegerObject: oop) ifFalse: [
-			self markAndTrace: oop.
-		].
-	].
+	1 to: remapBufferCount do: [:i | 
+			oop _ remapBuffer at: i.
+			(self isIntegerObject: oop) ifFalse: [self markAndTrace: oop]]
