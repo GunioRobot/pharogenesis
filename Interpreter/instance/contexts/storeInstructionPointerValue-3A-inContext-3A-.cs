b5ec8591d11f8143233a8dcfd8888d949837bc9a
tpr <unknown>
2004-03-24 21:06:00 +0000

    no message

--- Interpreter/instance/contexts/storeInstructionPointerValue-3A-inContext-3A-.cs
+++ Interpreter/instance/contexts/storeInstructionPointerValue-3A-inContext-3A-.cs
@@ -1,6 +1,4 @@
 storeInstructionPointerValue: value inContext: contextPointer
 	"Assume: value is an integerValue"
 
-	self storeWord: InstructionPointerIndex
-		ofObject: contextPointer
-		withValue: (self integerObjectOf: value).
+	self storeWord: InstructionPointerIndex ofObject: contextPointer withValue: (self integerObjectOf: value).
