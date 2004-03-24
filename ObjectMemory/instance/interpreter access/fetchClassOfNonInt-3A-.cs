7ba450350cc27194e0ee1fc824b2bf6a3e474493
tpr <unknown>
2004-03-24 12:52:00 +0000

    no message

--- ObjectMemory/instance/interpreter access/fetchClassOfNonInt-3A-.cs
+++ ObjectMemory/instance/interpreter access/fetchClassOfNonInt-3A-.cs
@@ -1,10 +1,9 @@
-fetchClassOfNonInt: oop
+fetchClassOfNonInt: oop 
 	| ccIndex |
 	self inline: true.
-
-	ccIndex _ ((self baseHeader: oop) >> 12) bitAnd: 16r1F.
+	ccIndex _ (self baseHeader: oop) >> 12 bitAnd: 31.
 	ccIndex = 0
-		ifTrue: [^ (self classHeader: oop) bitAnd: AllButTypeMask ]
+		ifTrue: [^ (self classHeader: oop)
+				bitAnd: AllButTypeMask]
 		ifFalse: ["look up compact class"
-				^ self fetchPointer: ccIndex - 1
-					ofObject: (self fetchPointer: CompactClasses ofObject: specialObjectsOop)]
+			^ self fetchPointer: ccIndex - 1 ofObject: (self fetchPointer: CompactClasses ofObject: specialObjectsOop)]
