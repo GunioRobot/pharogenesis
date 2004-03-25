02219e13529a9b0f82eaaa92482005d847559fa6
laza <unknown>
2004-03-25 21:31:00 +0000

    no message

--- Morph/instance/geometry/extent-3A-.cs
+++ Morph/instance/geometry/extent-3A-.cs
@@ -2,6 +2,6 @@ extent: aPoint
 
 	bounds extent = aPoint ifTrue: [^ self].
 	self changed.
-	bounds _ bounds topLeft extent: aPoint.
+	bounds _ (bounds topLeft extent: aPoint) rounded.
 	self layoutChanged.
 	self changed.
