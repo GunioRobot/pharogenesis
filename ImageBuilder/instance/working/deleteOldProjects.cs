f5848bc83c8f555bbb8280c61499e837366aefa6
dgd <unknown>
2004-03-29 18:36:00 +0000

    no message

---
+++ ImageBuilder/instance/working/deleteOldProjects.cs
@@ -0,0 +1,9 @@
+deleteOldProjects
+	"delete the old projects"
+	self deleteProjectNamed: 'Building with Squeak'.
+self deleteProjectNamed: 'Fun with Morphic'.
+self deleteProjectNamed: 'More About Sound'.
+self deleteProjectNamed: 'Fun With Music'.
+self deleteProjectNamed: 'New Changes'.
+self deleteProjectNamed: 'Squeak and the Internet'.
+self deleteProjectNamed: 'The Worlds of Squeak'.
