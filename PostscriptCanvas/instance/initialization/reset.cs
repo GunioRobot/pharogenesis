4941f6b0736b596e03e6febbe895538ad68f7062
nk <unknown>
2004-04-01 19:09:00 +0000

    no message

--- PostscriptCanvas/instance/initialization/reset.cs
+++ PostscriptCanvas/instance/initialization/reset.cs
@@ -1,10 +1,12 @@
 reset
 	super reset.
-	origin _ 0@0.							"origin of the top-left corner of this cavas"
-	clipRect _ (0@0 corner: 10000@10000).		"default clipping rectangle"
-	currentTransformation _ nil.
-	morphLevel _ 0.
-	gstateStack _ OrderedCollection new.
-	self initializeFontMap.
-	usedFonts _ Set new.
-     initialScale _ 1.0
+	origin := 0 @ 0.				"origin of the top-left corner of this canvas"
+	clipRect := 0 @ 0 corner: 10000 @ 10000.		"default clipping rectangle"
+	currentTransformation := nil.
+	morphLevel := 0.
+	pages := 0.
+	gstateStack := OrderedCollection new.
+	usedFonts := Dictionary new.
+	initialScale := 1.0.
+	shadowColor := nil.
+	currentColor := nil
