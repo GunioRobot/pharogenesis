c8c61fd6f4c30264a536556dcb30df29dcdd859f
nk <unknown>
2004-04-01 20:36:00 +0000

    no message

---
+++ PostscriptCanvas/instance/private/drawGradient-3A-.cs
@@ -0,0 +1,5 @@
+drawGradient: fillColor 
+	self comment: 'not-solid fill ' with: fillColor.
+	self comment: ' origin ' with: fillColor origin.
+	self comment: ' direction ' with: fillColor direction.
+	self fill: fillColor asColor
