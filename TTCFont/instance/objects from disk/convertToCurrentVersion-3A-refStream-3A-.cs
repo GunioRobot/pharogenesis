311cbe46922e12e511bce1d5a50bb3404020257c
nk <unknown>
2004-04-02 11:29:00 +0000

    no message

---
+++ TTCFont/instance/objects from disk/convertToCurrentVersion-3A-refStream-3A-.cs
@@ -0,0 +1,8 @@
+convertToCurrentVersion: varDict refStream: smartRefStrm
+	"If we're reading in an old version with a pixelSize instance variable, convert it to a point size."
+
+	"Deal with the change from pixelSize to pointSize, assuming the current monitor dpi."
+	varDict at: 'pixelSize' ifPresent: [ :x | 
+		pointSize _ (TextStyle pixelsToPoints: x) rounded.
+	].
+	^super convertToCurrentVersion: varDict refStream: smartRefStrm.
