69b2272a1376619e8fe0d5141cd9cbc7b2f2f813
tpr <unknown>
2004-04-02 16:13:00 +0000

    no message

---
+++ InterpreterSimulator/instance/memory access/sqShrinkMemory-3A-By-3A-.cs
@@ -0,0 +1,4 @@
+sqShrinkMemory: oldLimit By: delta
+	Transcript show: 'shrink memory from ', oldLimit printString, ' by ', delta printString, ' remember it doesn''t actually shrink in simulation'; cr.
+
+	^ oldLimit
