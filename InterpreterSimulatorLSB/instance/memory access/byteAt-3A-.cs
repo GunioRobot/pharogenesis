e39cb91ce81386598a599ddcd69e74cd0089fee8
nk <unknown>
2004-04-03 06:58:00 +0000

    no message

--- InterpreterSimulatorLSB/instance/memory access/byteAt-3A-.cs
+++ InterpreterSimulatorLSB/instance/memory access/byteAt-3A-.cs
@@ -1,6 +1,10 @@
 byteAt: byteAddress
-	| lowBits |
+	| lowBits long |
 	lowBits _ byteAddress bitAnd: 3.
-	^((self longAt: byteAddress - lowBits)
-		bitShift: (0 - lowBits) * 8)
-		bitAnd: 16rFF
+	long _ self longAt: byteAddress - lowBits.
+	^(lowBits caseOf: {
+		[0] -> [ long ].
+		[1] -> [ long bitShift: -8  ].
+		[2] -> [ long bitShift: -16 ].
+		[3] -> [ long bitShift: -24 ]
+	}) bitAnd: 16rFF
