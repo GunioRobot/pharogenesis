a7264d94d0420f568c413500c0ab9c9ad77c4d3e
tpr <unknown>
2004-04-03 23:16:00 +0000

    no message

--- BitBltSimulator/instance/as yet unclassified/initBBOpTable.cs
+++ BitBltSimulator/instance/as yet unclassified/initBBOpTable.cs
@@ -1,6 +1,6 @@
 initBBOpTable
 	opTable _ OpTable.
 	maskTable _ Array new: 32.
-	#(1 2 4 8 16 32) do:[:i| maskTable at: i put: (1 << i)-1].
+	#(1 2 4 5 8 16 32) do:[:i| maskTable at: i put: (1 << i)-1].
 	self initializeDitherTables.
 	warpBitShiftTable _ CArrayAccessor on: (Array new: 32).
