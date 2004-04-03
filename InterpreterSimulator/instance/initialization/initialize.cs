319a2f7d25d434af25a36f1b685e1ebf836bcfe3
nk <unknown>
2004-04-03 11:49:00 +0000

    no message

--- InterpreterSimulator/instance/initialization/initialize.cs
+++ InterpreterSimulator/instance/initialization/initialize.cs
@@ -1,4 +1,5 @@
 initialize
+
 	"Initialize the InterpreterSimulator when running the interpreter inside
 	Smalltalk. The primary responsibility of this method is to allocate
 	Smalltalk Arrays for variables that will be declared as statically-allocated
@@ -10,15 +11,16 @@ initialize
 
 	methodCache _ Array new: MethodCacheSize.
 	atCache _ Array new: AtCacheTotalSize.
+	self flushMethodCache.
 	rootTable _ Array new: RootTableSize.
 	remapBuffer _ Array new: RemapBufferSize.
 	semaphoresUseBufferA _ true.
 	semaphoresToSignalA _ Array new: SemaphoresToSignalSize.
 	semaphoresToSignalB _ Array new: SemaphoresToSignalSize.
-	externalPrimitiveTable _ CArrayAccessor on: (Array new: MaxExternalPrimitiveTableSize size).
-
+	externalPrimitiveTable _ CArrayAccessor on: (Array new: MaxExternalPrimitiveTableSize).
+	primitiveTable _ self class primitiveTable.
 	obsoleteNamedPrimitiveTable _ 
-		CArrayAccessor on: self class obsoleteNamedPrimitiveTable.
+		CArrayAccessor on: (self class obsoleteNamedPrimitiveTable copyWith: (Array new: 3)).
 	obsoleteIndexedPrimitiveTable _ CArrayAccessor on: 
 		(self class obsoleteIndexedPrimitiveTable collect:[:spec| 
 			CArrayAccessor on:
@@ -30,7 +32,9 @@ initialize
 	"initialize InterpreterSimulator variables used for debugging"
 	byteCount _ 0.
 	sendCount _ 0.
+	quitBlock _ [^ self].
 	traceOn _ true.
 	myBitBlt _ BitBltSimulator new setInterpreter: self.
 	displayForm _ nil.  "displayForm is created in response to primitiveBeDisplay"
 	filesOpen _ OrderedCollection new.
+	headerTypeBytes _ CArrayAccessor on: (Array with:  8 with: 4 with: 0 with: 0).
