2b74ee09b29c41a513e5e7b8edccf9bb21a5d4d8
NS <unknown>
2004-04-06 15:31:00 +0000

    no message

--- ClassDescription/instance/copying/copyMethodDictionaryFrom-3A-.cs
+++ ClassDescription/instance/copying/copyMethodDictionaryFrom-3A-.cs
@@ -2,4 +2,4 @@ copyMethodDictionaryFrom: donorClass
 	"Copy the method dictionary of the donor class over to the receiver"
 
 	methodDict _ donorClass copyOfMethodDictionary.
-	organization _ donorClass organization deepCopy
+	self organization: donorClass organization deepCopy.
