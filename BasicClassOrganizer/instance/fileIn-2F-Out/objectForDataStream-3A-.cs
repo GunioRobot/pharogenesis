ecf01e7c133dedc2e996b66f3e340d9ae5911c5b
NS <unknown>
2004-04-07 16:04:00 +0000

    no message

---
+++ BasicClassOrganizer/instance/fileIn-2F-Out/objectForDataStream-3A-.cs
@@ -0,0 +1,12 @@
+objectForDataStream: refStrm
+	| dp |
+	"I am about to be written on an object file.  Write a path to me in the other system instead."
+
+	self hasSubject ifTrue: [
+		(refStrm insideASegment and: [self subject isSystemDefined not]) ifTrue: [
+			^ self].	"do trace me"
+		(self subject isKindOf: Class) ifTrue: [
+			dp _ DiskProxy global: self subject name selector: #organization args: #().
+			refStrm replace: self with: dp.
+			^ dp]].
+	^ self	"in desparation"
