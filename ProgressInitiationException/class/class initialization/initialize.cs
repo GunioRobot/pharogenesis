efea1e2a5adb8c1929aa7e9745de91377725a655
laza <unknown>
2004-04-07 14:44:00 +0000

    no message

---
+++ ProgressInitiationException/class/class initialization/initialize.cs
@@ -0,0 +1,2 @@
+initialize
+	Preferences addPreference: #morphicProgressStyle categories: #(#morphic #performance) default: true balloonHelp: 'This switches between morphic and plain style for progress display'
