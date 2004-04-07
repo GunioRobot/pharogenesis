c0774e432297fd83ee1af8482ed034993c1e47ce
NS <unknown>
2004-04-07 16:03:00 +0000

    no message

---
+++ BasicClassOrganizer/instance/accessing/classComment-3A-stamp-3A-.cs
@@ -0,0 +1,12 @@
+classComment: aString  stamp: aStamp
+	"Store the comment, aString, associated with the object that refers to the receiver."
+
+	self commentStamp: aStamp.
+	(aString isKindOf: RemoteString) 
+		ifTrue: [classComment _ aString]
+		ifFalse: [(aString == nil or: [aString size = 0])
+			ifTrue: [classComment _ nil]
+			ifFalse:
+				[self error: 'use aClass classComment:'.
+				classComment _ RemoteString newString: aString onFileNumber: 2]]
+				"Later add priorSource and date and initials?"
