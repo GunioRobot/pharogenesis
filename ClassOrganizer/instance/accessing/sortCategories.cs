a77621ac4b9747a6f5ec64e23b269a98a1fd7d62
NS <unknown>
2004-04-07 10:38:00 +0000

    no message

--- ClassOrganizer/instance/accessing/sortCategories.cs
+++ ClassOrganizer/instance/accessing/sortCategories.cs
@@ -1,10 +1,6 @@
 sortCategories
-	| privateCategories publicCategories newCategories |
-
-	privateCategories _ self categories select:
-		[:one | (one findString: 'private' startingAt: 1 caseSensitive: false) = 1].
-	publicCategories _ self categories copyWithoutAll: privateCategories.
-	newCategories _ publicCategories asSortedCollection asOrderedCollection
-		addAll: privateCategories asSortedCollection;
-		asArray.
-	self categories: newCategories
+	| oldCategories |
+	oldCategories _ self categories copy.
+	SystemChangeNotifier uniqueInstance doSilently: [
+		super sortCategories].
+	self notifyOfChangedCategoriesFrom: oldCategories to: self categories.
