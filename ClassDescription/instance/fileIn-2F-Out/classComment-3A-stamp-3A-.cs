22a20c615e85d32253e87b985ffa82e10fe717af
NS <unknown>
2004-04-08 11:35:00 +0000

    no message

--- ClassDescription/instance/fileIn-2F-Out/classComment-3A-stamp-3A-.cs
+++ ClassDescription/instance/fileIn-2F-Out/classComment-3A-stamp-3A-.cs
@@ -2,20 +2,22 @@ classComment: aString stamp: aStamp
 	"Store the comment, aString or Text or RemoteString, associated with the class we are organizing.  Empty string gets stored only if had a non-empty one before."
 
 	| ptr header file oldCommentRemoteStr |
-	(aString isKindOf: RemoteString) ifTrue: [^ self organization classComment: aString].
+	(aString isKindOf: RemoteString) ifTrue:
+		[SystemChangeNotifier uniqueInstance classCommented: self.
+		^ self organization classComment: aString stamp: aStamp].
+
 	oldCommentRemoteStr _ self organization commentRemoteStr.
-	(aString size = 0) & (oldCommentRemoteStr == nil) ifTrue: [^ organization classComment: nil].
+	(aString size = 0) & (oldCommentRemoteStr == nil) ifTrue: [^ self organization classComment: nil].
 		"never had a class comment, no need to write empty string out"
 
 	ptr _ oldCommentRemoteStr ifNil: [0] ifNotNil: [oldCommentRemoteStr sourcePointer].
-	SourceFiles ifNotNil: [(file _ SourceFiles at: 2) ifNotNil: [
-		file setToEnd; cr; nextPut: $!.	"directly"
+	SourceFiles ifNotNil: [(file _ SourceFiles at: 2) ifNotNil:
+		[file setToEnd; cr; nextPut: $!.	"directly"
 		"Should be saying (file command: 'H3') for HTML, but ignoring it here"
 		header _ String streamContents: [:strm | strm nextPutAll: self name;
 			nextPutAll: ' commentStamp: '.
 			aStamp storeOn: strm.
 			strm nextPutAll: ' prior: '; nextPutAll: ptr printString].
 		file nextChunkPut: header]].
-	Smalltalk changes commentClass: self.
-	aStamp size > 0 ifTrue: [self commentStamp: aStamp].
-	organization classComment: (RemoteString newString: aString onFileNumber: 2).
+	self organization classComment: (RemoteString newString: aString onFileNumber: 2) stamp: aStamp.
+	SystemChangeNotifier uniqueInstance classCommented: self.
