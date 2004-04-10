f93bdbc7ebce34db0c023d483dc8e2b4a1804582
sw <unknown>
2004-04-10 00:59:00 +0000

    no message

--- ScriptEditorMorph/instance/initialization/phrase-3A-.cs
+++ ScriptEditorMorph/instance/initialization/phrase-3A-.cs
@@ -1,5 +1,10 @@
 phrase: aPhraseTileMorph
-	"Make the receiver be an anonymous editor around aPhraseTileMorph"
+	"Make the receiver be a Scriptor for a new script whose initial contents is the given phrase."
+
+	| aHolder |
 	firstTileRow _ 2.
-	self addMorphBack: (AlignmentMorph newRow addMorphBack: aPhraseTileMorph).
+	aHolder _ AlignmentMorph newRow.
+	aHolder beTransparent; layoutInset: 0.
+	aHolder addMorphBack: aPhraseTileMorph.
+	self addMorphBack: aHolder.
 	self install
