3bf294c80a962002cb96b1c3c720a839d72f526b
sw <unknown>
2004-04-10 23:12:00 +0000

    no message

--- ScriptEditorMorph/instance/dropping-2F-grabbing/removeSpaces.cs
+++ ScriptEditorMorph/instance/dropping-2F-grabbing/removeSpaces.cs
@@ -1,4 +1,7 @@
 removeSpaces
+	"Remove vertical space"
+
 	self submorphsDo:
 		[:m | (m isMemberOf: Morph) ifTrue: [m delete]].
-	self removeEmptyRows
+	self removeEmptyRows.
+	submorphs isEmpty ifTrue: [self height: 14]
