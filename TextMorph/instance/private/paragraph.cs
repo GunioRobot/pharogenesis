99afea43198ffb2d95b06fd78ed5fe12d8941320
hpt <unknown>
2004-04-11 20:35:00 +0000

    no message

--- TextMorph/instance/private/paragraph.cs
+++ TextMorph/instance/private/paragraph.cs
@@ -1,14 +1,25 @@
 paragraph
 	"Paragraph instantiation is lazy -- create it only when needed"
 	paragraph ifNotNil: [^ paragraph].
+
+self setProperty: #CreatingParagraph toValue: true.
+
 	self setDefaultContentsIfNil.
 
 	"...Code here to recreate the paragraph..."
-	paragraph _ (self paragraphClass new textOwner: self owner)
-					compose: text style: textStyle copy
-					from: self startingIndex in: self container.
+	paragraph _ (self paragraphClass new textOwner: self owner).
+	paragraph wantsColumnBreaks: successor notNil.
+	paragraph
+		compose: text
+		style: textStyle copy
+		from: self startingIndex
+		in: self container.
 	wrapFlag ifFalse:
 		["Was given huge container at first... now adjust"
 		paragraph adjustRightX].
+	paragraph focused: (self currentHand keyboardFocus == self).
 	self fit.
+self removeProperty: #CreatingParagraph.
+
+
 	^ paragraph
