ac8c28eae54c50d727e2a1e017d00ba5b9fb8776
nk <unknown>
2004-04-12 19:21:00 +0000

    no message

--- EventSensor/instance/initialize/initialize.cs
+++ EventSensor/instance/initialize/initialize.cs
@@ -1,10 +1,10 @@
 initialize
 	"Initialize the receiver"
-	mouseButtons _ 0.
-	mousePosition _ 0@0.
-	keyboardBuffer _ SharedQueue new.
-	interruptKey _ interruptKey ifNil:[2094].  "cmd-."
-	interruptSemaphore _ (Smalltalk specialObjectsArray at: 31) ifNil:[Semaphore new].
-	eventQueue _ nil.
-	inputProcess _ nil.
-	inputSemaphore _ Semaphore new.
+	mouseButtons := 0.
+	mousePosition := 0 @ 0.
+	keyboardBuffer := SharedQueue new.
+	self setInterruptKey: (interruptKey ifNil: [$. asciiValue bitOr: 16r0800 ]). 	"cmd-."
+	interruptSemaphore := (Smalltalk specialObjectsArray at: 31) ifNil: [Semaphore new].
+	self flushAllButDandDEvents.
+	inputSemaphore := Semaphore new.
+	hasInputSemaphore := false.
