d6213bd37bf9b4c844a6f01b350887f5b2f9acfa
NS <unknown>
2004-04-13 16:26:00 +0000

    no message

---
+++ MonitorDelay/instance/private/setDelay-3A-forSemaphore-3A-monitor-3A-queue-3A-.cs
@@ -0,0 +1,4 @@
+setDelay: anInteger forSemaphore: aSemaphore monitor: aMonitor queue: anOrderedCollection
+	monitor _ aMonitor.
+	queue _ anOrderedCollection.
+	self setDelay: anInteger forSemaphore: aSemaphore.
