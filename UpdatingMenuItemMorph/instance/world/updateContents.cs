afde38f90b1ba3dec14470713344a6d61debec0f
nk <unknown>
2004-04-13 15:38:00 +0000

    no message

---
+++ UpdatingMenuItemMorph/instance/world/updateContents.cs
@@ -0,0 +1,22 @@
+updateContents
+	"Update the receiver's contents"
+
+	| newString enablement nArgs |
+	((wordingProvider isNil) or: [wordingSelector isNil]) ifTrue: [^ self].
+	nArgs _ wordingSelector numArgs.
+	newString _ nArgs == 0
+		ifTrue:
+			[wordingProvider perform: wordingSelector]
+		ifFalse:
+			[(nArgs == 1 and: [wordingArgument notNil])
+				ifTrue:
+					[wordingProvider perform: wordingSelector with: wordingArgument]
+				ifFalse:
+					[nArgs == arguments size ifTrue:
+						[wordingProvider perform: wordingSelector withArguments: arguments]]].
+	newString = (self contentString ifNil: [ contents ])
+		ifFalse: [self contents: newString.
+			MenuIcons decorateMenu: owner ].
+	enablementSelector ifNotNil:
+		[(enablement _ self enablement) == isEnabled 
+			ifFalse:	[self isEnabled: enablement]]
