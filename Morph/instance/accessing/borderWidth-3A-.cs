15ef18aa4913339a17e560353ebc817f4b5c1672
nk <unknown>
2004-04-14 17:48:00 +0000

    no message

--- Morph/instance/accessing/borderWidth-3A-.cs
+++ Morph/instance/accessing/borderWidth-3A-.cs
@@ -1,1 +1,8 @@
 borderWidth: aNumber
+	| style |
+	style _ self borderStyle.
+	style width = aNumber ifTrue: [ ^self ].
+
+	style style = #none
+		ifTrue: [ self borderStyle: (SimpleBorder width: aNumber color: Color transparent) ]
+		ifFalse: [ style width: aNumber. self changed ].
