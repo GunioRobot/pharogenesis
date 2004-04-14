fd1496e3e1292a4b6bdcc743d73c5dd1650f5b3e
nk <unknown>
2004-04-14 14:57:00 +0000

    no message

--- MenuItemMorph/instance/layout/minWidth.cs
+++ MenuItemMorph/instance/layout/minWidth.cs
@@ -1,5 +1,14 @@
 minWidth
-
-	| scanner |
-	scanner _ DisplayScanner quickPrintOn: Display box: Display boundingBox font: self fontToUse.
-	^ (scanner stringWidth: contents) + (subMenu == nil ifTrue: [0] ifFalse: [10])
+	| fontToUse iconWidth subMenuWidth markerWidth |
+	fontToUse := self fontToUse.
+	subMenuWidth := self hasSubMenu
+				ifFalse: [0]
+				ifTrue: [10].
+	iconWidth := self hasIcon
+				ifTrue: [self icon width + 2]
+				ifFalse: [0].
+	markerWidth := self hasMarker
+		ifTrue: [ self submorphBounds width + 8 ]
+		ifFalse: [ 0 ].
+	^ (fontToUse widthOfString: contents)
+		+ subMenuWidth + iconWidth + markerWidth.
