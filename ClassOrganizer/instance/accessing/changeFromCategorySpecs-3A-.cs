8be1253f3a7b651631ccbbbaddb763d9a1f0c448
NS <unknown>
2004-04-15 12:28:00 +0000

    no message

--- ClassOrganizer/instance/accessing/changeFromCategorySpecs-3A-.cs
+++ ClassOrganizer/instance/accessing/changeFromCategorySpecs-3A-.cs
@@ -1,36 +1,8 @@
-changeFromCategorySpecs: categorySpecs 
-	"Tokens is an array of categorySpecs as scanned from a browser 'reorganize' pane, or built up by some other process, such as a scan of an environment."
-
-	| oldElements newElements newCategories newStops currentStop temp ii cc catSpec |
-	oldElements _ elementArray asSet.
-	newCategories _ Array new: categorySpecs size.
-	newStops _ Array new: categorySpecs size.
-	currentStop _ 0.
-	newElements _ WriteStream on: (Array new: 16).
-	1 to: categorySpecs size do: 
-		[:i | 
-		catSpec _ categorySpecs at: i.
-		newCategories at: i put: catSpec first asSymbol.
-		catSpec allButFirst asSortedCollection do:
-			[:elem |
-			(oldElements remove: elem ifAbsent: [nil]) notNil ifTrue:
-				[newElements nextPut: elem.
-				currentStop _ currentStop+1]].
-		newStops at: i put: currentStop].
-
-	"Ignore extra elements but don't lose any existing elements!"
-	oldElements _ oldElements collect:
-		[:elem | Array with: (self categoryOfElement: elem) with: elem].
-	newElements _ newElements contents.
-	categoryArray _ newCategories.
-	(cc _ categoryArray asSet) size = categoryArray size ifFalse: ["has duplicate element"
-		temp _ categoryArray asOrderedCollection.
-		temp removeAll: categoryArray asSet asOrderedCollection.
-		temp do: [:dup | 
-			ii _ categoryArray indexOf: dup.
-			[dup _ (dup,' #2') asSymbol.  cc includes: dup] whileTrue.
-			cc add: dup.
-			categoryArray at: ii put: dup]].
-	categoryStops _ newStops.
-	elementArray _ newElements.
-	oldElements do: [:pair | self classify: pair last under: pair first].
+changeFromCategorySpecs: categorySpecs
+	| oldDict oldCategories |
+	oldDict _ self elementCategoryDict.
+	oldCategories _ self categories copy.
+	SystemChangeNotifier uniqueInstance doSilently: [
+		super changeFromCategorySpecs: categorySpecs].
+	self notifyOfChangedSelectorsOldDict: oldDict newDict: self elementCategoryDict.
+	self notifyOfChangedCategoriesFrom: oldCategories to: self categories.
