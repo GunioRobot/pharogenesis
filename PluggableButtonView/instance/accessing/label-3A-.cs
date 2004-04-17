0152f8ff36e7288b350743b3489b92cca33b3515
nk <unknown>
2004-04-17 19:49:00 +0000

    no message

--- PluggableButtonView/instance/accessing/label-3A-.cs
+++ PluggableButtonView/instance/accessing/label-3A-.cs
@@ -2,7 +2,7 @@ label: aStringOrDisplayObject
 	"Label this button with the given String or DisplayObject."
 
 	((aStringOrDisplayObject isKindOf: Paragraph)
-	or: [aStringOrDisplayObject isKindOf: Form])
+	or: [aStringOrDisplayObject isForm])
 		ifTrue: [label _ aStringOrDisplayObject]
 		ifFalse: [label _ aStringOrDisplayObject asParagraph].
 	self centerLabel.
