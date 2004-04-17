23a733636a53a294435f799f3b4326b9684c825f
nk <unknown>
2004-04-17 19:49:00 +0000

    no message

--- PluggableButtonView/instance/private/centerAlignLabelWith-3A-.cs
+++ PluggableButtonView/instance/private/centerAlignLabelWith-3A-.cs
@@ -5,4 +5,6 @@ centerAlignLabelWith: aPoint
 	alignPt _ label boundingBox center.
 	(label isKindOf: Paragraph) ifTrue: 
 		[alignPt _ alignPt + (0@(label textStyle leading))]. 
+	(label isForm)
+	  ifTrue: [label offset: 0 @ 0].
 	label align: alignPt with: aPoint
