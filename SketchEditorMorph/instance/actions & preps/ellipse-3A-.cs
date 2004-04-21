28c1e816cfd7c44e38915ccdc4215f94da302b2f
sw <unknown>
2004-04-21 13:23:00 +0000

    no message

--- SketchEditorMorph/instance/actions & preps/ellipse-3A-.cs
+++ SketchEditorMorph/instance/actions & preps/ellipse-3A-.cs
@@ -6,21 +6,20 @@ ellipse: evt
 	sOrigin _ self get: #strokeOrigin for: evt.
 	cColor _ self getColorFor: evt.
 	ext _ (sOrigin - evt cursorPoint) abs * 2.
-	evt shiftPressed ifTrue: [ext _ ext r].
+	evt shiftPressed ifTrue: [ext _ self shiftConstrainPoint: ext].
 	rect _ Rectangle center: sOrigin extent: ext.
 	ww _ (self getNibFor: evt) width.
 	(priorEvt _ self get: #lastEvent for: evt) ifNotNil: [
 		oldExt _ (sOrigin - priorEvt cursorPoint) abs + ww * 2.
-		priorEvt shiftPressed ifTrue: [oldExt _ oldExt r].
+		priorEvt shiftPressed ifTrue: [oldExt _ self shiftConstrainPoint: oldExt].
 		(oldExt < ext) ifFalse: ["Last draw sticks out, must erase the area"
 			oldRect _ Rectangle center: sOrigin extent: oldExt.
 			self restoreRect: oldRect]].
 	cColor == Color transparent
-	ifFalse:[
-	formCanvas fillOval: rect color: cColor 
-		borderWidth: 0 borderColor: Color transparent.]
-	ifTrue:[
-	formCanvas fillOval: rect color: cColor 
-		borderWidth: ww borderColor: Color black].
+		ifFalse:
+			[formCanvas fillOval: rect color: Color transparent borderWidth: ww borderColor: cColor]
+		ifTrue:
+			[formCanvas fillOval: rect color: cColor borderWidth: ww borderColor: Color black].
+
 	self invalidRect: rect.
 
