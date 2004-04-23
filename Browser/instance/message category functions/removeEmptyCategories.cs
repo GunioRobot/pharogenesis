5a1e6790d6afd0cf6dcdcf2008c8aa2eeaa62356
nk <unknown>
2004-04-23 09:18:00 +0000

    no message

--- Browser/instance/message category functions/removeEmptyCategories.cs
+++ Browser/instance/message category functions/removeEmptyCategories.cs
@@ -1,5 +1,4 @@
 removeEmptyCategories
-	messageCategoryListIndex = 0 ifTrue: [^ self].
 	self okToChange ifFalse: [^ self].
 	self selectedClassOrMetaClass organization removeEmptyCategories.
 	self changed: #messageCategoryList
