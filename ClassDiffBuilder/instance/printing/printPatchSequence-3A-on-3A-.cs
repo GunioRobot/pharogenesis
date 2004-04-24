59d1be0edcac12f4d57c9331a66ee0f079fb4af7
nk <unknown>
2004-04-24 08:49:00 +0000

    no message

--- ClassDiffBuilder/instance/printing/printPatchSequence-3A-on-3A-.cs
+++ ClassDiffBuilder/instance/printing/printPatchSequence-3A-on-3A-.cs
@@ -1,10 +1,8 @@
-printPatchSequence: ps on: aStream
-	| type line attr |
-	ps do:[:assoc|
-		type := assoc key.
-		line := assoc value.
-		attr := TextEmphasis normal.
-		type == #insert ifTrue:[attr := TextColor red].
-		type == #remove ifTrue:[attr := TextEmphasis struckOut].
-		aStream withAttribute: attr do:[aStream nextPutAll: line].
-	].
+printPatchSequence: ps on: aStream 
+	| type line |
+	ps do: [:assoc | 
+			type := assoc key.
+			line := assoc value.
+			aStream
+				withAttributes: (self attributesOf: type)
+				do: [aStream nextPutAll: line]]
