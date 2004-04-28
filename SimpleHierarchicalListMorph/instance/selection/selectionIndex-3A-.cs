75f7a78f2ac41fb086479d4ee45bc1748b9e4166
nk <unknown>
2004-04-28 10:26:00 +0000

    no message

--- SimpleHierarchicalListMorph/instance/selection/selectionIndex-3A-.cs
+++ SimpleHierarchicalListMorph/instance/selection/selectionIndex-3A-.cs
@@ -1,15 +1,16 @@
-selectionIndex: index
+selectionIndex: idx
 	"Called internally to select the index-th item."
-	| theMorph range |
-	index ifNil: [^ self].
+	| theMorph range index |
+	idx ifNil: [^ self].
+	index := idx min: scroller submorphs size max: 0.
 	(theMorph _ index = 0 ifTrue: [nil] ifFalse: [scroller submorphs at: index])
 		ifNotNil:
 		[((theMorph bounds top - scroller offset y) >= 0
 			and: [(theMorph bounds bottom - scroller offset y) <= bounds height]) ifFalse:
 			["Scroll into view -- should be elsewhere"
-			range _ self totalScrollRange.
+			range _ self vTotalScrollRange.
 			scrollBar value: (range > 0
-				ifTrue: [((index-1 * theMorph height) / self totalScrollRange)
+				ifTrue: [((index-1 * theMorph height) / self vTotalScrollRange)
 									truncateTo: scrollBar scrollDelta]
 				ifFalse: [0]).
 			scroller offset: -3 @ (range * scrollBar value)]].
