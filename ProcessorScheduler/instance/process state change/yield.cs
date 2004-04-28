5ecf514c22c7761c38983e15ea66351d417d44b1
tpr <unknown>
2004-04-28 17:53:00 +0000

    no message

--- ProcessorScheduler/instance/process state change/yield.cs
+++ ProcessorScheduler/instance/process state change/yield.cs
@@ -2,6 +2,8 @@ yield
 	"Give other Processes at the current priority a chance to run."
 
 	| semaphore |
+
+	<primitive: 167>
 	semaphore _ Semaphore new.
 	[semaphore signal] fork.
 	semaphore wait
