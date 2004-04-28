8ca2ba7c1f0d5b18ba1fe0d619ec65516f8b27d0
nk <unknown>
2004-04-28 10:24:00 +0000

    no message

---
+++ SystemDictionary/instance/shrinking/presumedSentMessages.cs
@@ -0,0 +1,23 @@
+presumedSentMessages   | sent |
+"Smalltalk presumedSentMessages"
+
+	"The following should be preserved for doIts, etc"
+	sent _ IdentitySet new.
+	#( rehashWithoutBecome compactSymbolTable rebuildAllProjects
+		browseAllSelect:  lastRemoval
+		scrollBarValue: vScrollBarValue: scrollBarMenuButtonPressed: 
+		withSelectionFrom:  to: removeClassNamed:
+		dragon: hilberts: mandala: web test3 factorial tinyBenchmarks benchFib
+		newDepth: restoreAfter: forgetDoIts zapAllMethods obsoleteClasses
+		removeAllUnSentMessages abandonSources removeUnreferencedKeys
+		reclaimDependents zapOrganization condenseChanges browseObsoleteReferences
+		subclass:instanceVariableNames:classVariableNames:poolDictionaries:category:
+		methodsFor:stamp: methodsFor:stamp:prior: instanceVariableNames:
+		startTimerInterruptWatcher unusedClasses) do:
+		[:sel | sent add: sel].
+	"The following may be sent by perform: in dispatchOnChar..."
+	(ParagraphEditor classPool at: #CmdActions) asSet do:
+		[:sel | sent add: sel].
+	(ParagraphEditor classPool at: #ShiftCmdActions) asSet do:
+		[:sel | sent add: sel].
+	^ sent
