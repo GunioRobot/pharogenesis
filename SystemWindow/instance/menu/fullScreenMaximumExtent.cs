bdcd56c3b53ab4e647de61a74bd63aabc69977c7
nk <unknown>
2004-04-28 10:25:00 +0000

    no message

---
+++ SystemWindow/instance/menu/fullScreenMaximumExtent.cs
@@ -0,0 +1,22 @@
+fullScreenMaximumExtent
+	"Zoom Window to Full World size with possible DeskMargins
+	obey the maximum extent rules"
+	
+	| left right possibleBounds |
+	left _ right _ 0.
+	self paneMorphs
+		do: [:pane | ((pane isKindOf: ScrollPane)
+					and: [pane retractableScrollBar])
+				ifTrue: [pane scrollBarOnLeft
+						ifTrue: [left _ left max: pane scrollBarThickness]
+						ifFalse: [right _ right max: pane scrollBarThickness]]].
+	possibleBounds _ self worldBounds
+				insetBy: (left @ 0 corner: right @ 0).
+
+	self maximumExtent ifNotNil:
+		[possibleBounds _ possibleBounds origin extent: ( self maximumExtent min: ( possibleBounds extent ))].
+	((Flaps sharedFlapsAllowed
+				and: [CurrentProjectRefactoring currentFlapsSuppressed not])
+			or: [Preferences fullScreenLeavesDeskMargins])
+		ifTrue: [possibleBounds _ possibleBounds insetBy: 22].
+	self bounds: possibleBounds
