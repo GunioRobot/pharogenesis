36cb8c344ef47fd6ae2a81c5ea287f15385c21a8
nk <unknown>
2004-04-29 10:46:00 +0000

    no message

---
+++ ArchiveViewer/instance/member operations/extractMember.cs
@@ -0,0 +1,16 @@
+extractMember
+	"Extract the member after prompting for a filename.
+	Answer the filename, or nil if error."
+
+	| result name |
+	self canExtractMember ifFalse: [ ^nil ].
+	result _ StandardFileMenu newFile.
+	result ifNil: [ ^nil ].
+	name _ (result directory fullNameFor: result name).
+	(archive canWriteToFileNamed: name)
+		ifFalse: [ self inform: name, ' is used by one or more members
+in your archive, and cannot be overwritten.
+Try extracting to another file name'.
+			^nil ].
+	self selectedMember extractToFileNamed: name.
+	^name
