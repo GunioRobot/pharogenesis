9592ef8eb801766e185f3715736283e9ae3f4268
nk <unknown>
2004-04-29 12:32:00 +0000

    no message

--- StringHolder/instance/message list menu/browseVersions.cs
+++ StringHolder/instance/message list menu/browseVersions.cs
@@ -1,12 +1,18 @@
 browseVersions
-	"Create and schedule a message set browser on all versions of the 
-	currently selected message selector."
-	| class selector |
-	(selector _ self selectedMessageName) ifNotNil:
-		[class _ self selectedClassOrMetaClass.
-		VersionsBrowser
-			browseVersionsOf: (class compiledMethodAt: selector)
-			class: self selectedClass
-			meta: class isMeta
-			category: self selectedMessageCategoryName
-			selector: selector]
+	"Create and schedule a Versions Browser, showing all versions of the 
+	currently selected message. Answer the browser or nil."
+	| selector class | 
+	self classCommentIndicated
+		ifTrue: [ ClassCommentVersionsBrowser browseCommentOf: self selectedClass.
+			^nil ].
+
+	(selector _ self selectedMessageName)
+		ifNil:[ self inform: 'Sorry, only actual methods have retrievable versions.'. ^nil ]
+		ifNotNil: [
+			class _ self selectedClassOrMetaClass.
+			^VersionsBrowser
+				browseVersionsOf: (class compiledMethodAt: selector)
+				class: self selectedClass
+				meta: class isMeta
+				category: (class organization categoryOfElement: selector)
+				selector: selector]
