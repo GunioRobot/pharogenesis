af8f81d0d33f33e4613dfdebcc7ca0526f53d9f1
nk <unknown>
2004-05-01 12:25:00 +0000

    no message

--- BalloonCanvas/instance/drawing/frameRectangle-3A-width-3A-color-3A-.cs
+++ BalloonCanvas/instance/drawing/frameRectangle-3A-width-3A-color-3A-.cs
@@ -1,6 +1,6 @@
 frameRectangle: r width: w color: c
 	"Draw a frame around the given rectangle"
 	^self frameAndFillRectangle: r
-			fillColor: nil
+			fillColor: Color transparent
 			borderWidth: w
 			borderColor: c
