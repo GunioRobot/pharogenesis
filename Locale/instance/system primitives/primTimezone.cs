1c628e22ee90dc71f6f69432c26fabe67d1d6de6
mir <unknown>
2004-05-13 18:56:00 +0000

    no message

---
+++ Locale/instance/system primitives/primTimezone.cs
@@ -0,0 +1,4 @@
+primTimezone
+	"The offset from UTC in seconds, with positive offsets being towards the east.
+	(San Francisco is in UTC -08*60 and Paris is in GMT +01*60 (daylight savings is not in effect)."
+	^0
