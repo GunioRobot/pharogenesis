de58b936b008b34c79728aa69465d196cc9b2baa
gm <unknown>
2004-05-15 02:59:00 +0000

    no message

--- Utilities/class/identification/setAuthorInitials.cs
+++ Utilities/class/identification/setAuthorInitials.cs
@@ -2,5 +2,5 @@ setAuthorInitials
 	"Put up a dialog allowing the user to specify the author's initials.  "
 
 	self setAuthorInitials:
-		(FillInTheBlank request: 'Please type your initals: '
+		(FillInTheBlank request: 'Please type your initials: '
 					initialAnswer: AuthorInitials)
