b620b240bef291d25285f481cb14d0429193cdd5
HEG <unknown>
2004-05-18 05:38:00 +0000

    no message

--- PDA/instance/notes/notesMenu-3A-.cs
+++ PDA/instance/notes/notesMenu-3A-.cs
@@ -1,6 +1,6 @@
 notesMenu: aMenu
 
 	aMenu add: 'add new note' target: self selector: #addNote.
-	toDoListIndex > 0 ifTrue:
+	notesListIndex > 0 ifTrue:
 		[aMenu add: 'remove note' target: self selector: #removeNote].
 	^ aMenu
