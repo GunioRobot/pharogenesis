593077aabbafec7624bd33b93b7aa20eb534363f
ajh <unknown>
2004-05-20 17:37:00 +0000

    no message

---
+++ BlockClosure/instance/exceptions/ensure-3A-.cs
@@ -0,0 +1,14 @@
+ensure: aBlock
+	"Evaluate a termination block after evaluating the receiver, regardless of whether the receiver's evaluation completes."
+
+	| returnValue b |
+	<primitive: 198>
+	returnValue := self value.
+	"aBlock wasn't nil when execution of this method began; it is nil'd out by the unwind machinery, and that's how we know it's already been evaluated ... otherwise, obviously, it needs to be evaluated"
+	aBlock == nil ifFalse: [
+		"nil out aBlock temp before evaluating aBlock so it is not executed again if aBlock remote returns"
+		b _ aBlock.
+		thisContext tempAt: 1 put: nil.  "aBlock _ nil"
+		b value.
+	].
+	^ returnValue
