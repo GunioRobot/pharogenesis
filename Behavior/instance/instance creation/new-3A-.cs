95d09979bff1658e2f026fc670caed3c41d2e27a
sd <unknown>
2004-05-20 11:20:00 +0000

    no message

--- Behavior/instance/instance creation/new-3A-.cs
+++ Behavior/instance/instance creation/new-3A-.cs
@@ -1,6 +1,5 @@
 new: sizeRequested 
-	"Answer an instance of this class with the number of indexable
+	"Answer an initialized instance of this class with the number of indexable
 	variables specified by the argument, sizeRequested."
 
-	<primitive: 71>  "This method runs primitively if successful"
-	^ self basicNew: sizeRequested  "Exceptional conditions will be handled in basicNew:"
+	^ (self basicNew: sizeRequested) initialize  
