35fe589f94e226103ca71f83a7f154aaca217d31
dtl <unknown>
2004-05-22 16:13:00 +0000

    no message

--- Project/class/utilities/interruptName-3A-.cs
+++ Project/class/utilities/interruptName-3A-.cs
@@ -1,16 +1,25 @@
 interruptName: labelString
 	"Create a Notifier on the active scheduling process with the given label."
-	| suspendingList projectProcess |
+	| preemptedProcess projectProcess suspendingList |
 	Smalltalk isMorphic ifFalse:
 		[^ ScheduledControllers interruptName: labelString].
-
-	World primaryHand ifNotNil: [World primaryHand releaseAllFoci].
+	ActiveHand ifNotNil:[ActiveHand interrupted].
+	ActiveWorld _ World. "reinstall active globals"
+	ActiveHand _ World primaryHand.
+	ActiveHand interrupted. "make sure this one's interrupted too"
+	ActiveEvent _ nil.
 
 	projectProcess _ self uiProcess.	"we still need the accessor for a while"
-	(suspendingList _ projectProcess suspendingList) == nil
-		ifTrue: [projectProcess == Processor activeProcess
-					ifTrue: [projectProcess suspend]]
-		ifFalse: [suspendingList remove: projectProcess.
-				projectProcess offList].
-
-	Debugger openInterrupt: labelString onProcess: projectProcess
+	preemptedProcess _ Processor preemptedProcess.
+	"Only debug preempted process if its priority is >= projectProcess' priority"
+	preemptedProcess priority < projectProcess priority ifTrue:[
+		(suspendingList _ projectProcess suspendingList) == nil
+			ifTrue: [projectProcess == Processor activeProcess
+						ifTrue: [projectProcess suspend]]
+			ifFalse: [suspendingList remove: projectProcess ifAbsent: [].
+					projectProcess offList].
+		preemptedProcess _ projectProcess.
+	] ifFalse:[
+		preemptedProcess _ projectProcess suspend offList.
+	].
+	Debugger openInterrupt: labelString onProcess: preemptedProcess
