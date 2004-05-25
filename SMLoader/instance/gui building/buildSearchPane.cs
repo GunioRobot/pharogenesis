2c9ce4e8ca3f2377fcd4f9a12fdd4e48321fe434
gk <unknown>
2004-05-25 12:21:00 +0000

    no message

---
+++ SMLoader/instance/gui building/buildSearchPane.cs
@@ -0,0 +1,11 @@
+buildSearchPane
+	| typeInView |
+	typeInView _ PluggableTextMorph on: self 
+		text: nil accept: #findPackage:notifying:
+		readSelection: nil menu: nil.
+	typeInView setBalloonText:'To find a package type in a fragment of its name and hit return'.
+	typeInView acceptOnCR: true.
+	(typeInView respondsTo: #hideScrollBarsIndefinitely) ifTrue: [
+		typeInView hideScrollBarsIndefinitely]
+	ifFalse: [typeInView hideScrollBarIndefinitely].
+	^typeInView
