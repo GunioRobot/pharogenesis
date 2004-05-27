a2ee8424e7afa58dc377798b1426fe98eda77162
rhi <unknown>
2004-05-27 17:09:00 +0000

    no message

--- Inspector/instance/menu commands/inspectorKey-3A-from-3A-.cs
+++ Inspector/instance/menu commands/inspectorKey-3A-from-3A-.cs
@@ -1,8 +1,14 @@
 inspectorKey: aChar from: view
-	"respond to a Command key.  Got here from a list of fields being inspected"
+	"Respond to a Command key issued while the cursor is over my field list"
 
-	aChar == $i ifTrue: [self selection inspect].
-	aChar == $I ifTrue: [self selection explore].
-	aChar == $b ifTrue:	[self browseMethodFull].
-	aChar == $c ifTrue: [self copyName].
+	aChar == $i ifTrue: [^ self selection inspect].
+	aChar == $I ifTrue: [^ self selection explore].
+	aChar == $b ifTrue:	[^ self browseMethodFull].
+	aChar == $h ifTrue:	[^ self classHierarchy].
+	aChar == $c ifTrue: [^ self copyName].
+	aChar == $p ifTrue: [^ self browseFullProtocol].
+	aChar == $N ifTrue: [^ self browseClassRefs].
+	aChar == $t ifTrue: [^ self tearOffTile].
+	aChar == $v ifTrue: [^ self viewerForValue].
+
 	^ self arrowKey: aChar from: view
