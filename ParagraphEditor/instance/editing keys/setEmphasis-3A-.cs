bc128da6e105895c9bdf4b1f8d2df6115ba45ac1
yo <unknown>
2004-05-27 13:56:00 +0000

    no message

--- ParagraphEditor/instance/editing keys/setEmphasis-3A-.cs
+++ ParagraphEditor/instance/editing keys/setEmphasis-3A-.cs
@@ -2,12 +2,10 @@ setEmphasis: emphasisSymbol
 	"Change the emphasis of the current selection."
 
 	| oldAttributes attribute |
-	oldAttributes _ paragraph text attributesAt: startBlock stringIndex forStyle: paragraph textStyle.
-	(emphasisSymbol == #plain) 
-		ifTrue:
-			[attribute _ TextEmphasis normal]
-		ifFalse:
-			[attribute _ TextEmphasis perform: emphasisSymbol.
-			oldAttributes do:
-				[:att | (att dominates: attribute) ifTrue: [attribute turnOff]]].
+	oldAttributes _ paragraph text attributesAt: self selectionInterval first forStyle: paragraph textStyle.
+
+	attribute _ TextEmphasis perform: emphasisSymbol.
+	(emphasisSymbol == #normal) 
+		ifFalse:	[oldAttributes do:	
+			[:att | (att dominates: attribute) ifTrue: [attribute turnOff]]].
 	self replaceSelectionWith: (self selection addAttribute: attribute)
