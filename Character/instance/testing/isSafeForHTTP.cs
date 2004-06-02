ec5c061a65a741136cc37510c7f9ef963a066efe
gg <unknown>
2004-06-02 15:18:00 +0000

    no message

--- Character/instance/testing/isSafeForHTTP.cs
+++ Character/instance/testing/isSafeForHTTP.cs
@@ -1,3 +1,6 @@
 isSafeForHTTP
 	"whether a character is 'safe', or needs to be escaped when used, eg, in a URL"
-	^self isAlphaNumeric or: [ '.~-_' includes: self ]
+	"[GG]  See http://www.faqs.org/rfcs/rfc1738.html. ~ is unsafe and has been removed"
+	^ value < 128
+		and: [self isAlphaNumeric
+				or: ['.-_' includes: self]]
