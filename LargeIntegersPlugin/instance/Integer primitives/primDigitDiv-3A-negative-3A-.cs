bf54a60c3d14666a482dc12bb7760862a22ba000
sr <unknown>
2004-06-08 04:51:00 +0000

    no message

--- LargeIntegersPlugin/instance/Integer primitives/primDigitDiv-3A-negative-3A-.cs
+++ LargeIntegersPlugin/instance/Integer primitives/primDigitDiv-3A-negative-3A-.cs
@@ -1,25 +1,41 @@
 primDigitDiv: secondInteger negative: neg 
-	"Answer the result of dividing firstInteger by secondInteger.  Fail if      
-	parameters are not integers or secondInteger is zero."
+	"Answer the result of dividing firstInteger by secondInteger. 
+	Fail if parameters are not integers, not normalized or secondInteger is 
+	zero. "
 	| firstAsLargeInteger secondAsLargeInteger firstInteger |
 	self debugCode: [self msg: 'primDigitDiv: secondInteger negative: neg'].
-	firstInteger _ self
+	firstInteger := self
 				primitive: 'primDigitDivNegative'
-				parameters: #(Integer Boolean )
+				parameters: #(#Integer #Boolean )
 				receiver: #Integer.
-	"Coerce SmallIntegers to corresponding (not normalized) large integers    
-	  and check for zerodivide."
+	"Avoid crashes in case of getting unnormalized args."
+	(self isNormalized: firstInteger)
+		ifFalse: [self
+				debugCode: [self msg: 'ERROR in primDigitDiv: secondInteger negative: neg'.
+					self msg: '------> receiver *not* normalized!'].
+			^ interpreterProxy primitiveFail].
+	(self isNormalized: secondInteger)
+		ifFalse: [self
+				debugCode: [self msg: 'ERROR in primDigitDiv: secondInteger negative: neg'.
+					self msg: '------> argument *not* normalized!'].
+			^ interpreterProxy primitiveFail].
+	"Coerce SmallIntegers to corresponding (not normalized) large integers  
+	and check for zerodivide."
 	(interpreterProxy isIntegerObject: firstInteger)
 		ifTrue: ["convert to LargeInteger"
-			self remapOop: secondInteger in: [firstAsLargeInteger _ self createLargeFromSmallInteger: firstInteger]]
-		ifFalse: [firstAsLargeInteger _ firstInteger].
+			self
+				remapOop: secondInteger
+				in: [firstAsLargeInteger := self createLargeFromSmallInteger: firstInteger]]
+		ifFalse: [firstAsLargeInteger := firstInteger].
 	(interpreterProxy isIntegerObject: secondInteger)
-		ifTrue: 
-			["check for zerodivide and convert to LargeInteger"
+		ifTrue: ["check for zerodivide and convert to LargeInteger"
 			(interpreterProxy integerValueOf: secondInteger)
-				= 0 ifTrue: [^ interpreterProxy primitiveFail].
-			self remapOop: firstAsLargeInteger in: [secondAsLargeInteger _ self createLargeFromSmallInteger: secondInteger]]
-		ifFalse: [secondAsLargeInteger _ secondInteger].
+					= 0
+				ifTrue: [^ interpreterProxy primitiveFail].
+			self
+				remapOop: firstAsLargeInteger
+				in: [secondAsLargeInteger := self createLargeFromSmallInteger: secondInteger]]
+		ifFalse: [secondAsLargeInteger := secondInteger].
 	^ self
 		digitDivLarge: firstAsLargeInteger
 		with: secondAsLargeInteger
