293cdf12928230d7315cda9700b9e78265127bb8
sr <unknown>
2004-06-08 05:09:00 +0000

    no message

--- LargeIntegersPlugin/instance/util/unsafeByteOf-3A-at-3A-.cs
+++ LargeIntegersPlugin/instance/util/unsafeByteOf-3A-at-3A-.cs
@@ -1,3 +1,3 @@
-unsafeByteOf: bytesOop at: ix 
-"Argument bytesOop must not be aSmallInteger!"
+unsafeByteOf: bytesOop at: ix
+	"Argument bytesOop must not be aSmallInteger!"
 	^ interpreterProxy integerValueOf: (interpreterProxy stObject: bytesOop at: ix)
