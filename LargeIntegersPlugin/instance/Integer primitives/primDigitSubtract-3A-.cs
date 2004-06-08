fe99855e08ec0d59ec932f40f7890823c1c3c617
sr <unknown>
2004-06-08 05:01:00 +0000

    no message

--- LargeIntegersPlugin/instance/Integer primitives/primDigitSubtract-3A-.cs
+++ LargeIntegersPlugin/instance/Integer primitives/primDigitSubtract-3A-.cs
@@ -1,17 +1,20 @@
 primDigitSubtract: secondInteger 
-	""
 	| firstLarge secondLarge firstInteger |
 	self debugCode: [self msg: 'primDigitSubtract: secondInteger'].
-	firstInteger _ self
-		primitive: 'primDigitSubtract'
-		parameters: #( Integer )
-		receiver: #Integer.
+	firstInteger := self
+				primitive: 'primDigitSubtract'
+				parameters: #(#Integer )
+				receiver: #Integer.
 	(interpreterProxy isIntegerObject: firstInteger)
 		ifTrue: ["convert it to a not normalized LargeInteger"
-			self remapOop: secondInteger in: [firstLarge _ self createLargeFromSmallInteger: firstInteger]]
-		ifFalse: [firstLarge _ firstInteger].
+			self
+				remapOop: secondInteger
+				in: [firstLarge := self createLargeFromSmallInteger: firstInteger]]
+		ifFalse: [firstLarge := firstInteger].
 	(interpreterProxy isIntegerObject: secondInteger)
 		ifTrue: ["convert it to a not normalized LargeInteger"
-			self remapOop: firstLarge in: [secondLarge _ self createLargeFromSmallInteger: secondInteger]]
-		ifFalse: [secondLarge _ secondInteger].
+			self
+				remapOop: firstLarge
+				in: [secondLarge := self createLargeFromSmallInteger: secondInteger]]
+		ifFalse: [secondLarge := secondInteger].
 	^ self digitSubLarge: firstLarge with: secondLarge
