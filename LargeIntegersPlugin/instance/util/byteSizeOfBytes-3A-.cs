c011c211ee7bcca21c0d6ef7dbb41b570f0ea70a
sr <unknown>
2004-06-08 05:10:00 +0000

    no message

--- LargeIntegersPlugin/instance/util/byteSizeOfBytes-3A-.cs
+++ LargeIntegersPlugin/instance/util/byteSizeOfBytes-3A-.cs
@@ -1,5 +1,5 @@
 byteSizeOfBytes: bytesOop 
 	"Precondition: bytesOop is not anInteger and a bytes object."
-	"Function #byteSizeOf: is used by the interpreter, be careful with name  
-	     clashes..."
+	"Function #byteSizeOf: is used by the interpreter, be careful with name
+	clashes..."
 	^ interpreterProxy slotSizeOf: bytesOop
