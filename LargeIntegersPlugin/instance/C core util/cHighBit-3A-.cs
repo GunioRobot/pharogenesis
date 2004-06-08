4762b8ed78ff4bf2f6ec64597ad2f471161651d6
sr <unknown>
2004-06-08 17:16:00 +0000

    no message

--- LargeIntegersPlugin/instance/C core util/cHighBit-3A-.cs
+++ LargeIntegersPlugin/instance/C core util/cHighBit-3A-.cs
@@ -1,16 +1,26 @@
 cHighBit: uint 
 	"Answer the index of the high order bit of the argument, or zero if the  
 	argument is zero."
+	"For 64 bit uints there could be added a 32-shift."
 	| shifted bitNo |
+	self flag: #is64bitClean.
 	self var: #shifted declareC: 'unsigned int  shifted'.
-	shifted _ uint.
-	bitNo _ 0.
-	[shifted < 16]
-		whileFalse: 
-			[shifted _ shifted bitShift: -4.
-			bitNo _ bitNo + 4].
-	[shifted = 0]
-		whileFalse: 
-			[shifted _ shifted bitShift: -1.
-			bitNo _ bitNo + 1].
-	^ bitNo
+	shifted := uint.
+	bitNo := 0.
+	shifted < (1 << 16)
+		ifFalse: [shifted := shifted bitShift: -16.
+			bitNo := bitNo + 16].
+	shifted < (1 << 8)
+		ifFalse: [shifted := shifted bitShift: -8.
+			bitNo := bitNo + 8].
+	shifted < (1 << 4)
+		ifFalse: [shifted := shifted bitShift: -4.
+			bitNo := bitNo + 4].
+	shifted < (1 << 2)
+		ifFalse: [shifted := shifted bitShift: -2.
+			bitNo := bitNo + 2].
+	shifted < (1 << 1)
+		ifFalse: [shifted := shifted bitShift: -1.
+			bitNo := bitNo + 1].
+	"shifted 0 or 1 now"
+	^ bitNo + shifted
