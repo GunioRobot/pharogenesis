6888b9eae77aedec5c0f150814c6de6fef6eec46
sr <unknown>
2004-06-08 04:53:00 +0000

    no message

--- LargeIntegersPlugin/instance/Integer primitives/primDigitMultiply-3A-negative-3A-.cs
+++ LargeIntegersPlugin/instance/Integer primitives/primDigitMultiply-3A-negative-3A-.cs
@@ -1,19 +1,22 @@
 primDigitMultiply: secondInteger negative: neg 
-	""
 	| firstLarge secondLarge firstInteger |
 	self debugCode: [self msg: 'primDigitMultiply: secondInteger negative: neg'].
-	firstInteger _ self
-		primitive: 'primDigitMultiplyNegative'
-		parameters: #( Integer Boolean )
-		receiver: #Integer.
+	firstInteger := self
+				primitive: 'primDigitMultiplyNegative'
+				parameters: #(#Integer #Boolean )
+				receiver: #Integer.
 	(interpreterProxy isIntegerObject: firstInteger)
 		ifTrue: ["convert it to a not normalized LargeInteger"
-			self remapOop: secondInteger in: [firstLarge _ self createLargeFromSmallInteger: firstInteger]]
-		ifFalse: [firstLarge _ firstInteger].
+			self
+				remapOop: secondInteger
+				in: [firstLarge := self createLargeFromSmallInteger: firstInteger]]
+		ifFalse: [firstLarge := firstInteger].
 	(interpreterProxy isIntegerObject: secondInteger)
 		ifTrue: ["convert it to a not normalized LargeInteger"
-			self remapOop: firstLarge in: [secondLarge _ self createLargeFromSmallInteger: secondInteger]]
-		ifFalse: [secondLarge _ secondInteger].
+			self
+				remapOop: firstLarge
+				in: [secondLarge := self createLargeFromSmallInteger: secondInteger]]
+		ifFalse: [secondLarge := secondInteger].
 	^ self
 		digitMultiplyLarge: firstLarge
 		with: secondLarge
