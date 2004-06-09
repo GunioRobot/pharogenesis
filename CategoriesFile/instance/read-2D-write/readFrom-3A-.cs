54ea962831ab0cfef06327bc29c5978f9f12e518
gg <unknown>
2004-06-09 20:10:00 +0000

    no message

--- CategoriesFile/instance/read-2D-write/readFrom-3A-.cs
+++ CategoriesFile/instance/read-2D-write/readFrom-3A-.cs
@@ -1,12 +1,8 @@
-readFrom: aFileStream 
-	"Read the categories from the given FileStream."
-	| name categorySize messageIDs |
-	categories _ Dictionary new: 64.
-	aFileStream binary; position: 0.
-	[aFileStream atEnd]
-		whileFalse: 
-			[name _ aFileStream ascii; nextString.
-			categorySize _ aFileStream binary; nextWord.
-			messageIDs _ PluggableSet integerSet.
-			categorySize timesRepeat: [messageIDs add: aFileStream nextInt32].
-			categories at: name put: messageIDs]
+readFrom: aStream 
+	"Read the categories from the given Stream."
+	aStream binary; position: 0.
+	categorizer _ CelesteCategorizer new
+		categoryPrototype: PluggableSet integerSet;
+		pseudoCategories: {'.all.'. '.unclassified.'};
+		readFrom: aStream elementReader: [:s | s nextInt32];
+		yourself.
