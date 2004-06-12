a88ff4e618bd6305a7b54b0fdcf9a3d13d71ae42
nk <unknown>
2004-06-12 10:00:00 +0000

    no message

--- Player/instance/pen/addPlayerMenuItemsTo-3A-hand-3A-.cs
+++ Player/instance/pen/addPlayerMenuItemsTo-3A-hand-3A-.cs
@@ -1,14 +1,18 @@
 addPlayerMenuItemsTo: aMenu hand: aHandMorph
+	"Note that these items are primarily available in another way in an object's Viewer"
+
 	| subMenu |
-	"Note that all these items are now available in another way in an object's Viewer"
-
-	(self slotInfo size > 0) ifTrue:
-		[aMenu add: 'change variable type...' target: self action: #changeVariableType].
-
 	subMenu _ MenuMorph new defaultTarget: self.
 	self getPenDown
 		ifTrue: [subMenu add: 'lift pen' action: #liftPen]
 		ifFalse: [subMenu add: 'lower pen' action: #lowerPen].
 	subMenu add: 'choose pen size...' action: #choosePenSize.
 	subMenu add: 'choose pen color...' action: #choosePenColor:.
-	aMenu add: 'pen...' subMenu: subMenu
+	aMenu add: 'pen...' subMenu: subMenu.
+
+	(costume renderedMorph isSketchMorph) ifTrue:
+		[self belongsToUniClass
+			ifFalse: 
+				[aMenu add: 'adopt scripts from...' target: self action: #adoptScriptsFrom]
+			ifTrue:
+				[aMenu add: 'impart scripts to...' target: self action: #impartSketchScripts]]
