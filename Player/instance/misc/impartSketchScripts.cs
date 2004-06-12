f4560cb59b2a6f51bf88b734652ef1a70390b967
nk <unknown>
2004-06-12 10:01:00 +0000

    no message

---
+++ Player/instance/misc/impartSketchScripts.cs
@@ -0,0 +1,9 @@
+impartSketchScripts
+	"Let the user designate another object to which my scripts and code should be imparted"
+
+	| aMorph |
+	Sensor waitNoButton.
+	aMorph _ ActiveWorld chooseClickTarget.
+	aMorph ifNil: [^ self].
+	(aMorph renderedMorph isSketchMorph) ifTrue:
+		[aMorph acquirePlayerSimilarTo: self]
