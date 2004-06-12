8bd5a1ad25a5b4b6d6e47645b30ab8f6b208ed8c
nk <unknown>
2004-06-12 09:59:00 +0000

    no message

--- BitmapFillStyle/instance/Morphic menu/chooseNewGraphicIn-3A-event-3A-.cs
+++ BitmapFillStyle/instance/Morphic menu/chooseNewGraphicIn-3A-event-3A-.cs
@@ -1,15 +1,10 @@
-chooseNewGraphicIn: aMorph event: evt
+chooseNewGraphicIn: aMorph event: evt 
 	"Used by any morph that can be represented by a graphic"
-	| reasonableForms aGraphicalMenu myGraphic |
-	reasonableForms _ (SketchMorph allSubInstances collect: [:m | m form]) asOrderedCollection.
-	reasonableForms addAll: (Smalltalk imageImports collect: [:f | f]).
-	reasonableForms addAll: (BitmapFillStyle allSubInstances collect:[:f| f form]).
-	reasonableForms _ reasonableForms asSet asOrderedCollection.
-	(reasonableForms includes: (myGraphic _ self form))
-		ifTrue:
-			[reasonableForms remove: myGraphic].
-	reasonableForms addFirst: myGraphic.
-
-	aGraphicalMenu _ GraphicalMenu new initializeFor: self withForms: reasonableForms coexist: true.
-	aGraphicalMenu selector: #newForm:forMorph:; argument: aMorph.
-	evt hand attachMorph: aGraphicalMenu.
+	| aGraphicalMenu |
+	aGraphicalMenu := GraphicalMenu new
+				initializeFor: self
+				withForms: aMorph reasonableBitmapFillForms
+				coexist: true.
+	aGraphicalMenu selector: #newForm:forMorph:;
+		 argument: aMorph.
+	evt hand attachMorph: aGraphicalMenu
