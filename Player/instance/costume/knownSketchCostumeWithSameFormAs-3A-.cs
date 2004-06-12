7b5f819dde1ff670f64195da53f0b197d6927b3c
nk <unknown>
2004-06-12 10:01:00 +0000

    no message

--- Player/instance/costume/knownSketchCostumeWithSameFormAs-3A-.cs
+++ Player/instance/costume/knownSketchCostumeWithSameFormAs-3A-.cs
@@ -1,5 +1,8 @@
-knownSketchCostumeWithSameFormAs: aSketchMorph
+knownSketchCostumeWithSameFormAs: aSketchMorph 
 	| itsForm |
-	itsForm _ aSketchMorph form.
-	^ costumes ifNotNil: [costumes detect: [:c | (c isKindOf: SketchMorph) and: [c form == itsForm]]
-					ifNone: [nil]]
+	itsForm := aSketchMorph form.
+	^ costumes
+		ifNotNil: [costumes
+				detect: [:c | c isSketchMorph
+						and: [c form == itsForm]]
+				ifNone: []]
