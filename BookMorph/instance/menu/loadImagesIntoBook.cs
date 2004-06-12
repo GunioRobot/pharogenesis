58781eff92e55b3b9d9b51f420be3439d10e33f8
nk <unknown>
2004-06-12 09:23:00 +0000

    no message

--- BookMorph/instance/menu/loadImagesIntoBook.cs
+++ BookMorph/instance/menu/loadImagesIntoBook.cs
@@ -3,29 +3,35 @@ loadImagesIntoBook
 	Load these into the book.  mjg 9/99"
 
 	| directory filenumber form newpage |
-	directory _ ((StandardFileMenu oldFileFrom: FileDirectory default) ifNil: [^ nil]) directory.
-	directory isNil ifTrue: [^ nil].
+	directory := ((StandardFileMenu oldFileFrom: FileDirectory default) 
+				ifNil: [^nil]) directory.
+	directory isNil ifTrue: [^nil].
 
 	"Start loading 'em up!"
-	filenumber _ 1.
-	[directory fileExists: 'Slide',(filenumber asString)] whileTrue:
-		[Transcript show: 'Slide',(filenumber asString); cr.
-		(Smalltalk bytesLeft < 1000000) ifTrue: ["Make some room"
-			(self valueOfProperty: #url) == nil ifTrue:
-				[self savePagesOnURL]
-			ifFalse: [self saveAsNumberedURLs].].
-		form _ Form fromFileNamed: 
-			(directory fullNameFor: 'Slide', (filenumber asString)).
-		newpage _ PasteUpMorph new extent: (form extent).
-		newpage addMorph: (SketchMorph withForm: form).
-		self pages addLast: newpage.
-		filenumber _ filenumber + 1.].
+	filenumber := 1.
+	[directory fileExists: 'Slide' , filenumber asString] whileTrue: 
+			[Transcript
+				show: 'Slide' , filenumber asString;
+				cr.
+			Smalltalk bytesLeft < 1000000 
+				ifTrue: 
+					["Make some room"
+
+					(self valueOfProperty: #url) isNil 
+						ifTrue: [self savePagesOnURL]
+						ifFalse: [self saveAsNumberedURLs]].
+			form := Form 
+						fromFileNamed: (directory fullNameFor: 'Slide' , filenumber asString).
+			newpage := PasteUpMorph new extent: form extent.
+			newpage addMorph: (World drawingClass withForm: form).
+			self pages addLast: newpage.
+			filenumber := filenumber + 1].
 
 	"After adding all, delete the first page."
 	self goToPage: 1.
 	self deletePageBasic.
 
 	"Save the book"
-	(self valueOfProperty: #url) == nil ifTrue:
-				[self savePagesOnURL]
-			ifFalse: [self saveAsNumberedURLs].
+	(self valueOfProperty: #url) isNil 
+		ifTrue: [self savePagesOnURL]
+		ifFalse: [self saveAsNumberedURLs]
