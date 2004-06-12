d65936a73aebc0aa64d0d8f8eebf0a39da3d53ef
nk <unknown>
2004-06-12 09:58:00 +0000

    no message

--- Morph/instance/menus/chooseNewGraphicCoexisting-3A-.cs
+++ Morph/instance/menus/chooseNewGraphicCoexisting-3A-.cs
@@ -1,24 +1,11 @@
-chooseNewGraphicCoexisting: aBoolean
+chooseNewGraphicCoexisting: aBoolean 
 	"Allow the user to choose a different form for her form-based morph"
-
-	| reasonableForms replacee aGraphicalMenu myGraphic |
-	reasonableForms _ (SketchMorph allInstances
-		select:
-			[:m | ((m owner isKindOf: SketchEditorMorph) or: [m owner isKindOf: IconicButton]) not]
-		thenCollect:
-			 [:m | m form]) asSet "eliminate duplicates" asOrderedCollection.
-
-	reasonableForms addAll: (Smalltalk imageImports collect: [:f | f]).
-	reasonableForms _ reasonableForms asSet asOrderedCollection.
-	(reasonableForms includes: (myGraphic _ self form))
-		ifTrue:
-			[reasonableForms remove: myGraphic].
-	reasonableForms addFirst: myGraphic.
-
-	aGraphicalMenu _ GraphicalMenu new initializeFor: self withForms: reasonableForms coexist: aBoolean.
+	| replacee aGraphicalMenu |
+	aGraphicalMenu := GraphicalMenu new
+				initializeFor: self
+				withForms: self reasonableForms
+				coexist: aBoolean.
 	aBoolean
-		ifFalse:
-			[replacee _ self topRendererOrSelf.
+		ifTrue: [self primaryHand attachMorph: aGraphicalMenu]
+		ifFalse: [replacee := self topRendererOrSelf.
 			replacee owner replaceSubmorph: replacee by: aGraphicalMenu]
-		ifTrue:
-			[self primaryHand attachMorph: aGraphicalMenu]
