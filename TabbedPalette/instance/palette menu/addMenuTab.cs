391916e1807dd5484008cc159d98c15820104be5
nk <unknown>
2004-06-12 10:05:00 +0000

    no message

--- TabbedPalette/instance/palette menu/addMenuTab.cs
+++ TabbedPalette/instance/palette menu/addMenuTab.cs
@@ -1,20 +1,22 @@
 addMenuTab
+	"Add the menu tab.  This is ancient code, not much in the spirit of anything current"
+
 	| aMenu aTab aGraphic sk |
 	aMenu _ MenuMorph new defaultTarget: self.
 	aMenu stayUp: true.
-	aMenu add:  'clear' action: #showNoPalette.
-	aMenu add:  'sort tabs' action: #sortTabs:.
-	aMenu add:  'choose new colors for tabs' action: #recolorTabs.
+	"aMenu add:  'clear' translated action: #showNoPalette."
+	aMenu add:  'sort tabs' translated action: #sortTabs:.
+	aMenu add:  'choose new colors for tabs' translated action: #recolorTabs.
 	aMenu setProperty: #paletteMenu toValue: true.
-	aMenu add:  'make me the Standard palette' action: #becomeStandardPalette.
-	aTab _ self addTabForBook: aMenu  withBalloonText: 'a menu of palette-related controls'.
+	"aMenu add:  'make me the Standard palette' translated action: #becomeStandardPalette."
+	aTab _ self addTabForBook: aMenu  withBalloonText: 'a menu of palette-related controls' translated.
 	aTab highlightColor: tabsMorph highlightColor; regularColor: tabsMorph regularColor.
 	tabsMorph laySubpartsOutInOneRow; layoutChanged.
 
-	aGraphic _ ScriptingSystem formAtKey: 'Menu'.
+	aGraphic _ ScriptingSystem formAtKey: 'TinyMenu'.
 	aGraphic ifNotNil:
 		[aTab removeAllMorphs.
-		aTab addMorph: (sk _ SketchMorph withForm: aGraphic).
+		aTab addMorph: (sk _ World drawingClass withForm: aGraphic).
 		sk position: aTab position.
 		sk lock.
 		aTab fitContents].
