141475ff5c4be9703b3bef0a73044ef839784fe8
nk <unknown>
2004-06-13 06:20:00 +0000

    no message

--- Browser/instance/message category list/selectMessageCategoryNamed-3A-.cs
+++ Browser/instance/message category list/selectMessageCategoryNamed-3A-.cs
@@ -1,6 +1,4 @@
 selectMessageCategoryNamed: aSymbol 
 	"Given aSymbol, select the category with that name.  Do nothing if 
 	aSymbol doesn't exist."
-	(self messageCategoryList includes: aSymbol)
-		ifFalse: [^ self].
-	self messageCategoryListIndex: (self messageCategoryList indexOf: aSymbol)
+	self messageCategoryListIndex: (self messageCategoryList indexOf: aSymbol ifAbsent: [ 1])
