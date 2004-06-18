546ce9206c98c9f7f3d8fdb259293507481aad23
edc <unknown>
2004-06-18 09:10:00 +0000

    no message

--- NewParagraph/instance/private/moveBy-3A-.cs
+++ NewParagraph/instance/private/moveBy-3A-.cs
@@ -1,4 +1,5 @@
 moveBy: delta
 	lines do: [:line | line moveBy: delta].
-	positionWhenComposed _ positionWhenComposed + delta.
+	positionWhenComposed ifNotNil:[
+	positionWhenComposed _ positionWhenComposed + delta].
 	container _ container translateBy: delta
