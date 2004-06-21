3272d3ddd252307bbcd5832c4352cd36f8a89b1d
nk <unknown>
2004-06-21 10:22:00 +0000

    no message

--- WeakArray/class/class initialization/initialize.cs
+++ WeakArray/class/class initialization/initialize.cs
@@ -1,18 +1,9 @@
 initialize
 	"WeakArray initialize"
+
 	"Do we need to initialize specialObjectsArray?"
 	Smalltalk specialObjectsArray size < 42 
 		ifTrue:[Smalltalk recreateSpecialObjectsArray].
-	"Check if Finalization is supported by this VM"
-	IsFinalizationSupported _ nil.
-	self isFinalizationSupported ifFalse:[^self].
 
-	FinalizationProcess notNil ifTrue:[FinalizationProcess terminate].
-	FinalizationSemaphore := Smalltalk specialObjectsArray at: 42.
-	FinalizationDependents isNil ifTrue:[
-		FinalizationDependents := WeakArray new: 10.
-	].
-	FinalizationLock := Semaphore forMutualExclusion.
-	FinalizationProcess := [self finalizationProcess] newProcess.
-	FinalizationProcess priority: Processor userInterruptPriority.
-	FinalizationProcess resume.
+	Smalltalk addToStartUpList: self.
+	self restartFinalizationProcess.
