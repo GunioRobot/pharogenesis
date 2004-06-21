de1d2c32ad5f2407f8590c5842d20789b98e5e6f
nk <unknown>
2004-06-21 09:59:00 +0000

    no message

--- ProcessBrowser/instance/process list/processNameList.cs
+++ ProcessBrowser/instance/process list/processNameList.cs
@@ -1,4 +1,14 @@
 processNameList
 	"since processList is a WeakArray, we have to strengthen the result"
-	^ processList asOrderedCollection
-		collect: [:each | self prettyNameForProcess: each] 
+	| pw tally |
+	pw _ Smalltalk at: #CPUWatcher ifAbsent: [ ].
+	tally _ pw ifNotNil: [ pw current ifNotNil: [ pw current tally ] ].
+	^ (processList asOrderedCollection
+		copyWithout: nil)
+		collect: [:each | | percent |
+			percent _ tally
+				ifNotNil: [ ((((tally occurrencesOf: each) * 100.0 / tally size) roundTo: 1)
+						asString padded: #left to: 2 with: $ ), '% '  ]
+				ifNil: [ '' ].
+			percent, (self prettyNameForProcess: each)
+		] 
