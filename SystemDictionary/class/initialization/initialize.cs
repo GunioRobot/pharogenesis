9c051fa81c9948331377c5260253bfb7ec3585ee
nk <unknown>
2004-06-21 10:18:00 +0000

    no message

--- SystemDictionary/class/initialization/initialize.cs
+++ SystemDictionary/class/initialization/initialize.cs
@@ -5,37 +5,52 @@ initialize
 	oldList _ StartUpList.
 	StartUpList _ OrderedCollection new.
 	"These get processed from the top down..."
-	Smalltalk addToStartUpList: DisplayScreen.
-	Smalltalk addToStartUpList: Cursor.
-	Smalltalk addToStartUpList: InputSensor.
-	Smalltalk addToStartUpList: ProcessorScheduler.  "Starts low space watcher and bkground."
-	Smalltalk addToStartUpList: Delay.
-	Smalltalk addToStartUpList: FileDirectory.  "Enables file stack dump and opens sources."
-	Smalltalk addToStartUpList: ShortIntegerArray.
-	Smalltalk addToStartUpList: ShortRunArray.
-	Smalltalk addToStartUpList: CrLfFileStream.
+	#(
+		DisplayScreen
+		Cursor
+		InputSensor
+		ProcessorScheduler  "Starts low space watcher and bkground."
+		Delay
+		FileDirectory  "Enables file stack dump and opens sources."
+		ShortIntegerArray
+		ShortRunArray
+		CrLfFileStream
+	) do:[:clsName|
+		Smalltalk at: clsName ifPresent:[:cls| Smalltalk addToStartUpList: cls].
+	].
 	oldList ifNotNil: [oldList do: [:className | Smalltalk at: className
 						ifPresent: [:theClass | Smalltalk addToStartUpList: theClass]]].
-	Smalltalk addToStartUpList: ImageSegment.
-	Smalltalk addToStartUpList: PasteUpMorph.
-	Smalltalk addToStartUpList: ControlManager.
+	#(
+		ImageSegment
+		PasteUpMorph
+		ControlManager
+	) do:[:clsName|
+		Smalltalk at: clsName ifPresent:[:cls| Smalltalk addToStartUpList: cls].
+	].
+		
 
 	oldList _ ShutDownList.
 	ShutDownList _ OrderedCollection new.
 	"These get processed from the bottom up..."
-	Smalltalk addToShutDownList: DisplayScreen.
-	Smalltalk addToShutDownList: Form.
-	Smalltalk addToShutDownList: ControlManager.
-	Smalltalk addToShutDownList: StrikeFont.
-	Smalltalk addToShutDownList: Color.
-	Smalltalk addToShutDownList: FileDirectory.
-	Smalltalk addToShutDownList: Delay.
-	Smalltalk addToShutDownList: SoundPlayer.
-	Smalltalk addToShutDownList: HttpUrl.
-	Smalltalk addToShutDownList: Password.
-	Smalltalk addToShutDownList: PWS.
-	Smalltalk addToShutDownList: MailDB.
-	Smalltalk addToShutDownList: ImageSegment.
+	#(
+		DisplayScreen
+		InputSensor
+		Form
+		ControlManager
+		PasteUpMorph
+		StrikeFont
+		Color
+		FileDirectory
+		Delay
+		SoundPlayer
+		HttpUrl
+		Password
+		PWS
+		MailDB
+		ImageSegment
+	) do:[:clsName|
+		Smalltalk at: clsName ifPresent:[:cls| Smalltalk addToShutDownList: cls].
+	].
 
 	oldList ifNotNil: [oldList reverseDo: [:className | Smalltalk at: className
 						ifPresent: [:theClass | Smalltalk addToShutDownList: theClass]]].
