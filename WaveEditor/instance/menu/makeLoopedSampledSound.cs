22ef25eb9ee248b41ac19d06359136c311694b13
nk <unknown>
2004-06-22 18:34:00 +0000

    no message

--- WaveEditor/instance/menu/makeLoopedSampledSound.cs
+++ WaveEditor/instance/menu/makeLoopedSampledSound.cs
@@ -1,19 +1,25 @@
 makeLoopedSampledSound
-
 	| data end snd basePitch |
-	data _ graph data.
-	((loopEnd = 0) or: [loopLength = 0])
-		ifTrue: [  "save as unlooped"
-			perceivedFrequency = 0
-				ifTrue: [basePitch _ 100.0]
-				ifFalse: [basePitch _ perceivedFrequency].
-			snd _ LoopedSampledSound new
-				unloopedSamples: data pitch: basePitch samplingRate: samplingRate]
-		ifFalse: [
-			end _ (loopEnd min: data size) max: 1.
-			basePitch _ (samplingRate * loopCycles) / loopLength.
-			snd _ LoopedSampledSound new
-				samples: data loopEnd: end loopLength: loopLength
-				pitch: basePitch samplingRate: samplingRate].
+	data := graph data.
+	snd := (loopEnd = 0 or: [loopLength = 0]) 
+				ifTrue: 
+					["save as unlooped"
+
+					basePitch := perceivedFrequency = 0 
+						ifTrue: [100.0]
+						ifFalse: [perceivedFrequency].
+					LoopedSampledSound new 
+						unloopedSamples: data
+						pitch: basePitch
+						samplingRate: samplingRate]
+				ifFalse: 
+					[end := (loopEnd min: data size) max: 1.
+					basePitch := samplingRate * loopCycles / loopLength.
+					LoopedSampledSound new 
+						samples: data
+						loopEnd: end
+						loopLength: end
+						pitch: basePitch
+						samplingRate: samplingRate].
 	snd addReleaseEnvelope.
-	^ snd
+	^snd
