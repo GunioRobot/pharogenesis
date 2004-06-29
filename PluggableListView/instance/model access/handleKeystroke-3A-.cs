cd360af5a7dd57060e8f6f255035a0d71554694a
nk <unknown>
2004-06-29 14:45:00 +0000

    no message

--- PluggableListView/instance/model access/handleKeystroke-3A-.cs
+++ PluggableListView/instance/model access/handleKeystroke-3A-.cs
@@ -1,13 +1,17 @@
-handleKeystroke: aCharacter
+handleKeystroke: aChar
 	"Answer the menu for this list view."
 
-	| args |
-	keystrokeActionSelector == nil ifTrue: [^ nil].
+	| args aSpecialKey |
+
+	aSpecialKey _ aChar asciiValue.
+	aSpecialKey < 32 ifTrue: [ self specialKeyPressed: aSpecialKey. ^nil ].
+	keystrokeActionSelector ifNil: [^ nil].
+
 	controller controlTerminate.
 	(args _ keystrokeActionSelector numArgs) = 1
-		ifTrue: [model perform: keystrokeActionSelector with: aCharacter.
+		ifTrue: [model perform: keystrokeActionSelector with: aChar.
 				^ controller controlInitialize].
 	args = 2
-		ifTrue: [model perform: keystrokeActionSelector with: aCharacter with: self.
+		ifTrue: [model perform: keystrokeActionSelector with: aChar with: self.
 				^ controller controlInitialize].
 	^ self error: 'The keystrokeActionSelector must be a 1- or 2-keyword symbol'
