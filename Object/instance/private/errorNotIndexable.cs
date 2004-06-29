93f56959acff2b20c3f61cb3082d27012f65271d
yo <unknown>
2004-06-29 11:37:00 +0000

    no message

--- Object/instance/private/errorNotIndexable.cs
+++ Object/instance/private/errorNotIndexable.cs
@@ -1,4 +1,4 @@
 errorNotIndexable
 	"Create an error notification that the receiver is not indexable."
 
-	self error: (self class name) , 's are not indexable'
+	self error: ('Instances of {1} are not indexable' translated format: {self class name})
