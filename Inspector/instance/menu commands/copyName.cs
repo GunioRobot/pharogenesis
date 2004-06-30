9e99b80ec09b0ea185087a271cd983223a2a76e6
PHK <unknown>
2004-06-30 11:47:00 +0000

    no message

--- Inspector/instance/menu commands/copyName.cs
+++ Inspector/instance/menu commands/copyName.cs
@@ -1,10 +1,14 @@
 copyName
-	"Copy the name of the current variable, so the user can paste it into the window below and work with is.  If collection, do (xxx at: 1). "
-
-	| sel aClass |
-	self selectionUnmodifiable ifTrue: [^ self changed: #flash].
-	(aClass _ self object class) isVariable ifTrue: [^ self changed: #flash].
-
-	sel _ aClass allInstVarNames at: selectionIndex - 2.
-	(self selection isKindOf: Collection) ifTrue: [sel _ '(',sel,' at: 1)'].
-	Clipboard clipboardText: sel asText.	"no undo allowed"
+	"Copy the name of the current variable, so the user can paste it into the 
+	window below and work with is. If collection, do (xxx at: 1)."
+	| sel aClass variableNames |
+	self selectionUnmodifiable
+		ifTrue: [^ self changed: #flash].
+	aClass _ self object class.
+	variableNames _ aClass allInstVarNames.
+	(aClass isVariable and: [selectionIndex > (variableNames size + 2)])
+		ifTrue: [sel _ '(self basicAt: ' , (selectionIndex - (variableNames size + 2)) asString , ')']
+		ifFalse: [sel _ variableNames at: selectionIndex - 2].
+	(self selection isKindOf: Collection)
+		ifTrue: [sel _ '(' , sel , ' at: 1)'].
+	Clipboard clipboardText: sel asText
