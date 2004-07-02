2ffe61757142f344555a0fb6236517a0d1893bba
yo <unknown>
2004-07-02 19:59:00 +0000

    no message

--- Project/instance/file in-2F-out/storeOnServerInnards.cs
+++ Project/instance/file in-2F-out/storeOnServerInnards.cs
@@ -1,58 +1,76 @@
 storeOnServerInnards
 	"Save to disk as an Export Segment.  Then put that file on the server I came from, as a new version.  Version is literal piece of file name.  Mime encoded and http encoded."
 
-	| servers resp newName primaryServerDirectory serverVersionPair localDirectory localVersionPair myVersionNumber warning maxNumber |
+	| resp newName primaryServerDirectory serverVersionPair localDirectory localVersionPair myVersionNumber warning maxNumber suppliedPassword oldResourceUrl |
 	self assureIntegerVersion.
 
 	"Find out what version"
-	(servers _ self serverList) ifNil: [
-		(primaryServerDirectory _ self findAFolderToStoreProjectIn) ifNotNil: [
-			servers _ Array with: primaryServerDirectory.
-			self storeNewPrimaryURL: primaryServerDirectory realUrl, '/'.
+	primaryServerDirectory _ self primaryServerIfNil: [
+		(primaryServerDirectory _ self findAFolderToStoreProjectIn) ifNil: [^self].
+		oldResourceUrl _ self resourceUrl.
+		primaryServerDirectory == #localOnly ifTrue: [
+			self storeNewPrimaryURL: FileDirectory default url.
+			nil
+		] ifFalse: [
+			self storeNewPrimaryURL: primaryServerDirectory downloadUrl.
+			primaryServerDirectory
 		].
-	] ifNotNil: [
-		primaryServerDirectory _ servers first.
 	].
+
 	localDirectory _ self squeakletDirectory.
 	serverVersionPair _ self class mostRecent: self name onServer: primaryServerDirectory.
 	localVersionPair _ self class mostRecent: self name onServer: localDirectory.
 	maxNumber _ myVersionNumber _ self currentVersionNumber.
+
 	ProgressNotification signal: '2:versionsDetected'.
 
 	warning _ ''.
 	myVersionNumber < serverVersionPair second ifTrue: [
-		warning _ warning,'\There are newer version(s) on the server'.
+		warning _ warning,'\There are newer version(s) on the server' translated.
 		maxNumber _ maxNumber max: serverVersionPair second.
 	].
 	myVersionNumber < localVersionPair second ifTrue: [
-		warning _ warning,'\There are newer version(s) in the local directory'.
+		warning _ warning,'\There are newer version(s) in the local directory' translated.
 		maxNumber _ maxNumber max: localVersionPair second.
 	].
 	"8 Nov 2000 - only check on the first attempt to publish"
 	myVersionNumber = 0 ifTrue: [
 		warning isEmpty ifFalse: [
 			myVersionNumber = 0 ifTrue: [
-				warning _ warning,'\THIS PROJECT HAS NEVER BEEN SAVED'
+				warning _ warning,'\THIS PROJECT HAS NEVER BEEN SAVED' translated.
 			].
-			warning _ 'WARNING', '\Project: ',self name,warning.
-			resp _ (PopUpMenu labels: 'Store anyway\Cancel' withCRs) startUpWithCaption: 
-				(warning, '\Please cancel, rename this project, and see what is there.') withCRs.
+			warning _ 'WARNING' translated, '\Project: ' translated, self name,warning.
+			resp _ (PopUpMenu labels: 'Store anyway\Cancel' translated withCRs) startUpWithCaption: 
+				(warning, '\Please cancel, rename this project, and see what is there.' translated) withCRs.
 				resp ~= 1 ifTrue: [^ nil]
 		].
 	].
 	version _ self bumpVersion: maxNumber.
 
+	oldResourceUrl
+		ifNotNil: [self resourceManager adjustToNewServer: self resourceUrl from: oldResourceUrl].
+
 	"write locally - now zipped automatically"
 	newName _ self versionedFileName.
 	lastSavedAtSeconds _ Time totalSeconds.
-	self exportSegmentFileName: newName directory: localDirectory.		
+	self exportSegmentFileName: newName directory: localDirectory.
+	(localDirectory readOnlyFileNamed: newName) setFileTypeToObject; close.
+
 	ProgressNotification signal: '4:localSaveComplete'.	"3 is deep in export logic"
 
 	primaryServerDirectory ifNotNil: [
-		self
-			writeFileNamed: newName 
-			fromDirectory: localDirectory 
-			toServer: primaryServerDirectory.
+		suppliedPassword _ ''.
+		Preferences passwordsOnPublish ifTrue: [
+			suppliedPassword _ FillInTheBlank requestPassword: 'Project password' translated
+		].
+		[
+		primaryServerDirectory
+			writeProject: self
+			inFileNamed: newName asFileName
+			fromDirectory: localDirectory.
+		] on: ProjectPasswordNotification do: [ :ex |
+			ex resume: (suppliedPassword ifNil: [''])
+		].
 	].
 	ProgressNotification signal: '9999 save complete'.
 
