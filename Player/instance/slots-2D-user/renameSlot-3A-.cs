5a6956c03459e685add44bf741430533a20342bd
yo <unknown>
2004-07-02 19:36:00 +0000

    no message

--- Player/instance/slots-2D-user/renameSlot-3A-.cs
+++ Player/instance/slots-2D-user/renameSlot-3A-.cs
@@ -1,6 +1,11 @@
-renameSlot: oldSlotName
+renameSlot: oldSlotName 
 	| reply newSlotName |
-	reply _   FillInTheBlank request: 'New name for "', oldSlotName, '":' initialAnswer: oldSlotName.
- 	reply size == 0 ifTrue: [^ self].
-	newSlotName _ ScriptingSystem acceptableSlotNameFrom: reply forSlotCurrentlyNamed:  oldSlotName asSlotNameIn: self world: self costume currentWorld.
+	reply := FillInTheBlank request: 'New name for "' translated , oldSlotName , '":'
+				initialAnswer: oldSlotName.
+	reply isEmpty ifTrue: [^self].
+	newSlotName := ScriptingSystem 
+				acceptableSlotNameFrom: reply
+				forSlotCurrentlyNamed: oldSlotName
+				asSlotNameIn: self
+				world: self costume currentWorld.
 	self renameSlot: oldSlotName newSlotName: newSlotName
