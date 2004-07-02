8ef98f49988f3d3d2d4d7bb9bf2b8b90c4361c5d
yo <unknown>
2004-07-02 13:14:00 +0000

    no message

--- Morph/instance/fileIn-2F-out/saveOnFile.cs
+++ Morph/instance/fileIn-2F-out/saveOnFile.cs
@@ -2,14 +2,14 @@ saveOnFile
 	"Ask the user for a filename and save myself on a SmartReferenceStream file.  Writes out the version and class structure.  The file is fileIn-able.  UniClasses will be filed out."
 
 	| aFileName fileStream ok |
-	aFileName _ ('my ', self class name) asFileName.	"do better?"
-	aFileName _ FillInTheBlank request: 'File name? (".morph" will be added to end)' 
+	aFileName _ ('my {1}' translated format: {self class name}) asFileName.	"do better?"
+	aFileName _ FillInTheBlank request: 'File name? (".morph" will be added to end)' translated 
 			initialAnswer: aFileName.
-	aFileName size == 0 ifTrue: [^ self beep].
+	aFileName isEmpty ifTrue: [^ Beeper beep].
 	self allMorphsDo: [:m | m prepareToBeSaved].
 
 	ok _ aFileName endsWith: '.morph'.	"don't double them"
 	ok _ ok | (aFileName endsWith: '.sp').
 	ok ifFalse: [aFileName _ aFileName,'.morph'].
-	fileStream _ FileStream newFileNamed: aFileName.
+	fileStream _ FileStream newFileNamed: aFileName asFileName.
 	fileStream fileOutClass: nil andObject: self.	"Puts UniClass definitions out anyway"
