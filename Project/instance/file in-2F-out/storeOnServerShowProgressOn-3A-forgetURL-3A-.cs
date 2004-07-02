1b30bc08963132d6ebf8914ef740663a28d02f50
yo <unknown>
2004-07-02 17:45:00 +0000

    no message

--- Project/instance/file in-2F-out/storeOnServerShowProgressOn-3A-forgetURL-3A-.cs
+++ Project/instance/file in-2F-out/storeOnServerShowProgressOn-3A-forgetURL-3A-.cs
@@ -2,17 +2,15 @@ storeOnServerShowProgressOn: aMorphOrNil forgetURL: forget
 
 	"Save to disk as an Export Segment.  Then put that file on the server I came from, as a new version.  Version is literal piece of file name.  Mime encoded and http encoded."
 
-	forget ifTrue: [
-		self forgetExistingURL.
-	] ifFalse: [
-		urlList isEmptyOrNil ifTrue: [urlList _ parentProject urlList copy].
-	].
 	world setProperty: #optimumExtentFromAuthor toValue: world extent.
 	self validateProjectNameIfOK: [
 		self isCurrentProject ifTrue: ["exit, then do the command"
+			forget
+				ifTrue: [self forgetExistingURL]
+				ifFalse: [urlList isEmptyOrNil ifTrue: [urlList _ parentProject urlList copy]].
 			^self
 				armsLengthCommand: #storeOnServerAssumingNameValid
-				withDescription: 'Publishing'
+				withDescription: 'Publishing' translated
 		].
 		self storeOnServerWithProgressInfoOn: aMorphOrNil.
 	] fixTemps.
