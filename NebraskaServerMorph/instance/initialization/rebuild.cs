3cb138853c376c5f59a5bda93a4b097388a56a17
yo <unknown>
2004-07-02 18:38:00 +0000

    no message

--- NebraskaServerMorph/instance/initialization/rebuild.cs
+++ NebraskaServerMorph/instance/initialization/rebuild.cs
@@ -1,33 +1,58 @@
 rebuild
 
-	| myServer |
+	| myServer toggle closeBox font |
 
+	font _ StrikeFont familyName: #Palatino size: 14.
 	self removeAllMorphs.
+	self setColorsAndBorder.
 	self updateCurrentStatusString.
+	toggle _ SimpleHierarchicalListMorph new perform: (
+		fullDisplay ifTrue: [#expandedForm] ifFalse: [#notExpandedForm]
+	).
+	closeBox _ SimpleButtonMorph new borderWidth: 0;
+			label: 'X' font: Preferences standardButtonFont; color: Color transparent;
+			actionSelector: #delete; target: self; extent: 14@14;
+			setBalloonText: 'End Nebrasks session'.
+
 	self addARow: {
-		self inAColumn: {
-			SimpleButtonMorph new target: self; actionSelector: #delete; label: 'X'.
-		}.
+		self inAColumn: {closeBox}.
 		self inAColumn: {
 			UpdatingStringMorph new
 				useStringFormat;
 				target:  self;
+				font: font;
 				getSelector: #currentStatusString;
 				contents: self currentStatusString;
 				stepTime: 2000;
 				lock.
 		}.
 		self inAColumn: {
-			SimpleButtonMorph new target: self; actionSelector: #toggleFull; label: 'O'.
+			toggle asMorph
+				on: #mouseUp send: #toggleFull to: self;
+				setBalloonText: 'Show more or less of Nebraska Status'
 		}.
 	}.
 	myServer _ self server.
 	(myServer isNil or: [fullDisplay not]) ifTrue: [
 		^World startSteppingSubmorphsOf: self
 	].
+	"--- the expanded display ---"
 	self addARow: {
 		self inAColumn: {
-			(StringMorph contents: '--clients--') lock.
+			UpdatingStringMorph new
+				useStringFormat;
+				target:  self;
+				font: font;
+				getSelector: #currentBacklogString;
+				contents: self currentBacklogString;
+				stepTime: 2000;
+				lock.
+		}.
+	}.
+
+	self addARow: {
+		self inAColumn: {
+			(StringMorph contents: '--clients--' translated) lock; font: font.
 		}.
 	}.
 
@@ -36,6 +61,7 @@ rebuild
 			UpdatingStringMorph new
 				useStringFormat;
 				target: each;
+				font: font;
 				getSelector: #currentStatusString;
 				contents: each currentStatusString;
 				stepTime: 2000;
