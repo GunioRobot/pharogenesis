3fa7b681f9b86a2e23d42020939819fc5a1b2197
yo <unknown>
2004-07-02 19:02:00 +0000

    no message

--- Player/instance/slots-2D-user/addInstanceVariable.cs
+++ Player/instance/slots-2D-user/addInstanceVariable.cs
@@ -1,13 +1,13 @@
 addInstanceVariable
-	"Offer the user the opportunity to add an instance variable, and if he goes through with it, actually add it"
+	"Offer the user the opportunity to add an instance variable, and if he goes through with it, actually add it."
 
-	| itsName initialValue typeChosen suggestedNames usedNames initialAnswer setterSelector originalString |
-	suggestedNames _ #('cargo' 'speed' 'weight' 'mzee' 'friml' 'verp' 'znak').
+	| itsName initialValue typeChosen usedNames initialAnswer setterSelector originalString |
 	usedNames _ self class instVarNames.
-	initialAnswer _ suggestedNames detect: [:aName |  (usedNames includes: aName) not] ifNone:
-		[Utilities keyLike: 'var1'  satisfying: [:aKey | (usedNames includes: aKey) not]].
 
-	originalString _ FillInTheBlank request: 'name for new inst var: ' initialAnswer: initialAnswer.
+	initialAnswer _ Utilities keyLike: ('var' translated, (usedNames size + 1) asString)  satisfying: [:aKey | (usedNames includes: aKey) not].
+
+	originalString _ FillInTheBlank request: 'name for new variable: ' translated initialAnswer: initialAnswer.
+	originalString isEmptyOrNil ifTrue: [^ self].
 	itsName _ ScriptingSystem acceptableSlotNameFrom: originalString forSlotCurrentlyNamed: nil asSlotNameIn: self world: self costume world.
 
  	itsName size == 0 ifTrue: [^ self].	
@@ -20,4 +20,4 @@ addInstanceVariable
 	setterSelector _ Utilities setterSelectorFor: itsName.
 	(self class allSubInstances copyWithout: self) do:
 		[:anInstance | anInstance perform: setterSelector with: initialValue].
-	self updateAllViewersAndForceToShow: #'instance variables'
+	self updateAllViewersAndForceToShow: ScriptingSystem nameForInstanceVariablesCategory
