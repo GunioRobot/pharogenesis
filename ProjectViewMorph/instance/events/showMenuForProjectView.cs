73f1fadedb86164439dbec82f5b28c8b2a98c6bd
yo <unknown>
2004-07-02 20:04:00 +0000

    no message

--- ProjectViewMorph/instance/events/showMenuForProjectView.cs
+++ ProjectViewMorph/instance/events/showMenuForProjectView.cs
@@ -1,24 +1,29 @@
 showMenuForProjectView
 	| menu selection |
 	(menu _ CustomMenu new)
-		add: 'enter this project'
+		add: 'enter this project' translated
 		action: [^ self enter];
 		
-		add: 'ENTER ACTIVE'
+		add: 'ENTER ACTIVE' translated
 		action: [self setProperty: #wasOpenedAsSubproject toValue: true.
 			^ self enterAsActiveSubproject];
 		
-		add: 'PUBLISH (also saves a local copy)'
+		add: 'PUBLISH (also saves a local copy)' translated
 		action: [^ project storeOnServerShowProgressOn: self forgetURL: false];
 		
-		add: 'PUBLISH to a different server'
+		add: 'PUBLISH to a different server' translated
 		action: [project forgetExistingURL.
 			^ project storeOnServerShowProgressOn: self forgetURL: true];
 		
-		add: 'see if server version is more recent'
-		action: [^ self checkForNewerVersionAndLoad].
-	selection _ menu build startUpCenteredWithCaption: 'Project Named
-' , '"' , project name , '"'.
+		add: 'see if server version is more recent' translated
+		action: [^ self checkForNewerVersionAndLoad];
+
+		addLine;
+		add: 'expunge this project' translated
+		action: [^ self expungeProject].
+
+	selection _ menu build startUpCenteredWithCaption: 
+'Project Named "\{1}"' translated withCRs.
 	selection
 		ifNil: [^ self].
 	selection value
