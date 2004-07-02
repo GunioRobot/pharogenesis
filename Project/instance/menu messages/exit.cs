315585790da6d9fbb2dcc500584698b1435e7b38
yo <unknown>
2004-07-02 19:46:00 +0000

    no message

--- Project/instance/menu messages/exit.cs
+++ Project/instance/menu messages/exit.cs
@@ -1,5 +1,5 @@
 exit
 	"Leave the current project and return to the project in which this one was created."
 
-	self isTopProject ifTrue: [^ PopUpMenu notify: 'Can''t exit the top project'].
+	self isTopProject ifTrue: [^ self inform: 'Can''t exit the top project' translated].
 	parentProject enter: false revert: false saveForRevert: false.
