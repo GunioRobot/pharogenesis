886cb5fd2ec8d5b7554980d8a1e716d773567111
yo <unknown>
2004-07-02 19:50:00 +0000

    no message

--- Project/instance/file in-2F-out/saveForRevert.cs
+++ Project/instance/file in-2F-out/saveForRevert.cs
@@ -1,8 +1,7 @@
 saveForRevert
-	| |
 	"Exit to the parent project.  Do a GC.  Save the project in a segment.  Record the ImageSegment object as the revertToMe in Project parameters"
 
-	self isTopProject ifTrue: [^ PopUpMenu notify: 'Can''t exit the top project'].
+	self isTopProject ifTrue: [^ self inform: 'Can''t exit the top project' translated].
 	parentProject enter: false revert: false saveForRevert: true.
 	"does not return!"
 
