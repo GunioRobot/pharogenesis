fb210f08a9dbd56d677f676821c35ab66b8a9c01
yo <unknown>
2004-07-02 21:27:00 +0000

    no message

--- TextMorph/class/scripting/authoringPrototype.cs
+++ TextMorph/class/scripting/authoringPrototype.cs
@@ -1,7 +1,9 @@
 authoringPrototype
 	| t |
 	t _ super authoringPrototype.
-"Strangeness here in order to avoid two offset copies of the default contents when operating in an mvc project before cursor enters the morphic window"
+	t contents: 'abc' translated asText.
+	t wrapFlag: true. 
 
+"Strangeness here in order to avoid two offset copies of the default contents when operating in an mvc project before cursor enters the morphic window"
 	t paragraph.
 	^ t
