af618d1e558246fb76b946e8bb36680419dfa799
dms <unknown>
2004-07-04 19:36:00 +0000

    no message

--- Celeste/instance/message text pane/messageText.cs
+++ Celeste/instance/message text pane/messageText.cs
@@ -1,12 +1,13 @@
 messageText
 	"Answer the text which makes up the complete message (header+body)"
 
+	mailDB ifNil: [ ^self messageTextIfNoDB ].
 	(currentMsgID isNil) ifTrue: [^''].
 
 	"Always show the full message header for messages in the category .tosend. so that all special header lines are preserved, shown and can be edited."
-	(currentCategory = '.tosend.')
-		ifTrue: [^ mailDB getText: currentMsgID].
+	(self currentCategory = '.tosend.')
+		ifTrue: [^ (mailDB getText: currentMsgID) isoToSqueak].
 
 	SuppressWorthlessHeaderFields
-		ifTrue: [^ self currentMessage formattedText]
-		ifFalse: [^ mailDB getText: currentMsgID].
+		ifTrue: [^ (self currentMessage formattedText)]
+		ifFalse: [^ (mailDB getText: currentMsgID) isoToSqueak].
