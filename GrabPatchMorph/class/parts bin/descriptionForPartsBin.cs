c6267410a85b2cbe672c86548a5f763e3967504b
sw <unknown>
2004-07-05 01:51:00 +0000

    no message

---
+++ GrabPatchMorph/class/parts bin/descriptionForPartsBin.cs
@@ -0,0 +1,6 @@
+descriptionForPartsBin
+	"Answer a description of the receiver's instances for a parts bin"
+
+	^ self partName:	'Grab Patch'
+		categories:		#('Graphics')
+		documentation:	'Use this to grab a rectangular patch from the screen'
