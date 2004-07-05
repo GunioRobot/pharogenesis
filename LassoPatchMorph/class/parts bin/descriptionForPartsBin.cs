493bb72ef536e28fc80c203fd8f74b1c61087669
sw <unknown>
2004-07-05 01:46:00 +0000

    no message

---
+++ LassoPatchMorph/class/parts bin/descriptionForPartsBin.cs
@@ -0,0 +1,6 @@
+descriptionForPartsBin
+	"Answer a description of the receiver to be used in a parts bin"
+
+	^ self partName:	'Lasso'
+		categories:		#('Graphics')
+		documentation:	'Drop this icon to grab a patch from the screen with a lasso.'
