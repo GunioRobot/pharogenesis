1cdba4eb68b32ca8ccc2eb0fae7c1325de4273b3
yo <unknown>
2004-07-05 20:16:00 +0000

    no message

--- ChangeList/instance/menu actions/fileOutSelections.cs
+++ ChangeList/instance/menu actions/fileOutSelections.cs
@@ -1,8 +1,9 @@
 fileOutSelections 
-	| f |
-	f _ FileStream newFileNamed: (FillInTheBlank request: 'Enter file name' initialAnswer: 'Filename.st').
-	f ifNil: [^ self].
-	f header; timeStamp.
+	| fileName internalStream |
+	fileName _ FillInTheBlank request: 'Enter the base of file name' initialAnswer: 'Filename'.
+	internalStream _ WriteStream on: (String new: 1000).
+	internalStream header; timeStamp.
 	listSelections with: changeList do: 
-		[:selected :item | selected ifTrue: [item fileOutOn: f]].
-	f close
+		[:selected :item | selected ifTrue: [item fileOutOn: internalStream]].
+
+	FileStream writeSourceCodeFrom: internalStream baseName: fileName isSt: true useHtml: false.
