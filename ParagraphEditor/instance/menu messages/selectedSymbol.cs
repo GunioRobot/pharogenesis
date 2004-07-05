d43e9220fd8d370ff8206d6c53ed007cc1554eff
yo <unknown>
2004-07-05 16:38:00 +0000

    no message

--- ParagraphEditor/instance/menu messages/selectedSymbol.cs
+++ ParagraphEditor/instance/menu messages/selectedSymbol.cs
@@ -2,8 +2,10 @@ selectedSymbol
 	"Return the currently selected symbol, or nil if none.  Spaces, tabs and returns are ignored"
 
 	| aString |
-	startBlock = stopBlock ifTrue: [^ nil].
-	aString _ self selection string copyWithoutAll:
+	self hasCaret ifTrue: [^ nil].
+	aString _ self selection string.
+	aString isOctetString ifTrue: [aString _ aString asOctetString].
+	aString _ aString copyWithoutAll:
 		{Character space.  Character cr.  Character tab}.
 	aString size == 0 ifTrue: [^ nil].
 	Symbol hasInterned: aString  ifTrue: [:sym | ^ sym].
