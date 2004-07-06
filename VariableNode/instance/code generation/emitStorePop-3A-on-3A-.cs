6196b7aadc82e41ffb45f4dc0eda44fbf0a98ff4
ab <unknown>
2004-07-06 17:37:00 +0000

    no message

--- VariableNode/instance/code generation/emitStorePop-3A-on-3A-.cs
+++ VariableNode/instance/code generation/emitStorePop-3A-on-3A-.cs
@@ -1,11 +1,11 @@
 emitStorePop: stack on: strm
-	(code between: 0 and: 7)
+	(self code between: 0 and: 7)
 		ifTrue: 
-			[strm nextPut: ShortStoP + code "short stopop inst"]
+			[strm nextPut: ShortStoP + self code "short stopop inst"]
 		ifFalse:
-			[(code between: 16 and: 23)
-				ifTrue: [strm nextPut: ShortStoP + 8 + code - 16 "short stopop temp"]
-				ifFalse: [(code >= 256 and: [code \\ 256 > 63 and: [code // 256 = 4]])
+			[(self code between: 16 and: 23)
+				ifTrue: [strm nextPut: ShortStoP + 8 + self code - 16 "short stopop temp"]
+				ifFalse: [(self code >= 256 and: [self code \\ 256 > 63 and: [self code // 256 = 4]])
 						ifTrue: [self emitLong: Store on: strm. strm nextPut: Pop]
 						ifFalse: [self emitLong: StorePop on: strm]]].
 	stack pop: 1
