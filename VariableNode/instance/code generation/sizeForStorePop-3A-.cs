368f25da1b97965ec91fe477bac12b0d6452f175
ab <unknown>
2004-07-06 17:43:00 +0000

    no message

--- VariableNode/instance/code generation/sizeForStorePop-3A-.cs
+++ VariableNode/instance/code generation/sizeForStorePop-3A-.cs
@@ -1,8 +1,8 @@
 sizeForStorePop: encoder
 	self reserve: encoder.
-	(code < 24 and: [code noMask: 8]) ifTrue: [^ 1].
-	code < 256 ifTrue: [^ 2].
-	code \\ 256 <= 63 ifTrue: [^ 2].  "extended StorePop"
-	code // 256 = 1 ifTrue: [^ 3].  "dbl extended StorePopInst"
-	code // 256 = 4 ifTrue: [^ 4].  "dbl extended StoreLitVar , Pop"
+	(self code < 24 and: [self code noMask: 8]) ifTrue: [^ 1].
+	self code < 256 ifTrue: [^ 2].
+	self code \\ 256 <= 63 ifTrue: [^ 2].  "extended StorePop"
+	self code // 256 = 1 ifTrue: [^ 3].  "dbl extended StorePopInst"
+	self code // 256 = 4 ifTrue: [^ 4].  "dbl extended StoreLitVar , Pop"
 	self halt.  "Shouldn't get here"
