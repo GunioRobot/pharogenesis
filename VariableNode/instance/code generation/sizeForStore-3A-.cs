0270198bb04c9f354ea899963e77a1435bc7fc26
ab <unknown>
2004-07-06 17:38:00 +0000

    no message

--- VariableNode/instance/code generation/sizeForStore-3A-.cs
+++ VariableNode/instance/code generation/sizeForStore-3A-.cs
@@ -1,5 +1,5 @@
 sizeForStore: encoder
 	self reserve: encoder.
-	code < 256 ifTrue: [^ 2].
-	(code \\ 256) <= 63 ifTrue: [^ 2].
+	self code < 256 ifTrue: [^ 2].
+	(self code \\ 256) <= 63 ifTrue: [^ 2].
 	^ 3
