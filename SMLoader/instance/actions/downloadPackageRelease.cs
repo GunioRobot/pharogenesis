746abdb4658f0f890c1a5b79260713cb5f57ad64
gk <unknown>
2004-07-10 18:50:00 +0000

    no message

---
+++ SMLoader/instance/actions/downloadPackageRelease.cs
@@ -0,0 +1,12 @@
+downloadPackageRelease
+	"Force a download of the selected package release into the cache."
+
+	| release |
+	release _ self selectedPackageOrRelease.
+	release isPackageRelease ifFalse: [ self error: 'Should be a package release!'].
+	[Cursor wait showWhile: [
+		(SMInstaller forPackageRelease: release) download]
+	] on: Error do: [:ex |
+		| msg | 
+		msg := ex messageText ifNil: [ex asString].
+		self informException: ex msg: ('Error occurred during download:\', msg, '\') withCRs]
