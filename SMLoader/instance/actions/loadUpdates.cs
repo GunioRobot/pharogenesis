6f883f16d0f14ab84d2b5fbdbe6833fb766240ae
gk <unknown>
2004-07-10 18:51:00 +0000

    no message

--- SMLoader/instance/actions/loadUpdates.cs
+++ SMLoader/instance/actions/loadUpdates.cs
@@ -1,6 +1,6 @@
 loadUpdates
 	[Cursor wait showWhile: [
-		model loadUpdates.
+		squeakMap loadUpdates.
 		self noteChanged ]
 	] on: Error do: [:ex |
 		self informException: ex msg: ('Error occurred when updating map:\', ex messageText, '\') withCRs]
