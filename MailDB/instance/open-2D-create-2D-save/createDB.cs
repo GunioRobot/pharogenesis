f55fd28525633f332f52624a9cab218689fc3878
gg <unknown>
2004-07-10 18:39:00 +0000

    no message

--- MailDB/instance/open-2D-create-2D-save/createDB.cs
+++ MailDB/instance/open-2D-create-2D-save/createDB.cs
@@ -1,12 +1,9 @@
 createDB
 	"Create a new mail database."
 
-	(self confirm:
-'Shall I create a new mail database
-named: ', rootFilename, '?')
-		ifFalse:	"abort create operation"
-			[rootFilename _ nil.
-			 self release.
-			^nil].
+
 	self openDB.							"creates new DB files"
+	"gg: adding base categories:"
+	self addCategory:'new'; addCategory:'.tosend.'.
+	
 	self saveDB.							"save the new mail database to disk"
