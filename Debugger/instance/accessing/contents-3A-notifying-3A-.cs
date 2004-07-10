d98a3ed1b0adfe7a0ff26d0c92771b3f0ebc5b4e
nk <unknown>
2004-07-10 14:17:00 +0000

    no message

--- Debugger/instance/accessing/contents-3A-notifying-3A-.cs
+++ Debugger/instance/accessing/contents-3A-notifying-3A-.cs
@@ -1,44 +1,51 @@
 contents: aText notifying: aController 
-	"The retrieved information has changed and its source must now be 
-	updated. In this case, the retrieved information is the method of the 
+	"The retrieved information has changed and its source must now be  
+	updated. In this case, the retrieved information is the method of the  
 	selected context."
-	| selector classOfMethod category method priorMethod parseNode |
-	contextStackIndex = 0 ifTrue: [^ false].
-	(self selectedContext isKindOf: MethodContext)
-		ifFalse:
-			[(self confirm:
-'I will have to revert to the method from
+	| selector classOfMethod category h ctxt newMethod |
+	contextStackIndex = 0
+		ifTrue: [^ false].
+	self selectedContext isExecutingBlock
+		ifTrue: [h := self selectedContext finalBlockHome.
+			h
+				ifNil: [self inform: 'Method not found for block, can''t edit'.
+					^ false].
+			(self confirm: 'I will have to revert to the method from
 which this block originated.  Is that OK?')
-				ifTrue: [self resetContext: self selectedContext home]
+				ifTrue: [self resetContext: h]
 				ifFalse: [^ false]].
-	classOfMethod _ self selectedClass.
-	category _ self selectedMessageCategoryName.
-	Cursor execute showWhile:
-		[method _ classOfMethod
-		compile: aText
-		notifying: aController
-		trailer: #(0 0 0 0)
-		ifFail: [^ false]
-		elseSetSelectorAndNode: 
-			[:sel :methodNode | selector _ sel.
-			selector == self selectedMessageName
-				ifFalse: [self notify: 'can''t change selector'. ^ false].
-			priorMethod _ (classOfMethod includesSelector: selector)
-				ifTrue: [classOfMethod compiledMethodAt: selector]
-				ifFalse: [nil].
-			sourceMap _ methodNode sourceMap.
-			tempNames _ methodNode tempNames.
-			parseNode _ methodNode].
-		method cacheTempNames: tempNames].
-	category isNil ifFalse: "Skip this for DoIts"
-		[method putSource: aText
-				fromParseNode: parseNode
-				class: classOfMethod
-				category: category
-				inFile: 2 priorMethod: priorMethod.
-		classOfMethod organization classify: selector under: category].
-	contents _ aText copy.
-	self selectedContext restartWith: method.
-	contextVariablesInspector object: nil.
-	self resetContext: self selectedContext.
-	^true
+
+	classOfMethod := self selectedClass.
+	category := self selectedMessageCategoryName.
+	selector := self selectedClass parserClass new parseSelector: aText.
+	selector == self selectedMessageName
+		ifFalse: [self inform: 'can''t change selector'.
+			^ false].
+	selector := classOfMethod
+				compile: aText
+				classified: category
+				notifying: aController.
+	selector ifNil: [^ false]. "compile cancelled"
+	contents := aText.
+	newMethod := classOfMethod compiledMethodAt: selector.
+	newMethod isQuick ifTrue: [ self down.
+		self selectedContext jump: (self selectedContext previousPc - self selectedContext pc) ].
+	ctxt := interruptedProcess popTo: self selectedContext.
+	ctxt == self selectedContext ifFalse: [
+		self inform: 'Method saved, but current context unchanged
+because of unwind error. Click OK to see error'.
+	] ifTrue: [ 
+		newMethod isQuick ifFalse: [
+			interruptedProcess
+				restartTopWith: newMethod;
+			 	stepToSendOrReturn ].
+		contextVariablesInspector object: nil.
+		theMethodNode := Preferences browseWithPrettyPrint
+			ifTrue: [ctxt methodNodeFormattedAndDecorated: Preferences colorWhenPrettyPrinting]
+			ifFalse: [ctxt methodNode].
+		sourceMap := theMethodNode sourceMap.
+		tempNames := theMethodNode tempNames.
+	].
+	self resetContext: ctxt.
+	Smalltalk isMorphic ifTrue: [ World addAlarm: #changed: withArguments: #(contentsSelection) for: self at: (Time millisecondClockValue + 200) ].
+	^ true
