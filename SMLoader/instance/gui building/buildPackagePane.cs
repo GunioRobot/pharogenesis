082f7cabc1cba3506002cbc20fde62aa336993eb
gk <unknown>
2004-07-11 03:03:00 +0000

    no message

---
+++ SMLoader/instance/gui building/buildPackagePane.cs
@@ -0,0 +1,13 @@
+buildPackagePane
+	"Create the text area to the right in the loader."
+
+	| ptm |
+	ptm _ PluggableTextMorph 
+		on: self 
+		text: #contents
+		accept: nil
+		readSelection: nil "#packageSelection "
+		menu: nil.
+	ptm setBalloonText: 'This is where the selected package or package release is displayed.'.
+	ptm lock.
+	^ptm
