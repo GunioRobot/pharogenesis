b52fce80e30c6ed75d65c1bb1763d9ab7191f140
mir <unknown>
2004-07-12 21:29:00 +0000

    no message

---
+++ ObjectWithDocumentation/instance/private/getterSetterHelpMessage-3A-.cs
@@ -0,0 +1,5 @@
+getterSetterHelpMessage: aString
+	"Sets a helpMessage that needs to be translated and then formatted with the elementSymbol.
+	'get value of {1}' translated format: {elSym}"
+
+	self propertyAt: #getterSetterHelpMessage put: aString
