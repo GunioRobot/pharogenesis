9abb7349fa386b2d2a5e3961b739b99645078302
mir <unknown>
2004-07-12 20:22:00 +0000

    no message

--- AssignmentTileMorph/instance/initialization/computeOperatorOrExpression.cs
+++ AssignmentTileMorph/instance/initialization/computeOperatorOrExpression.cs
@@ -1,7 +1,15 @@
 computeOperatorOrExpression
-	| aSuffix |
+	"Compute the operator or expression to use, and set the wording correectly on the tile face"
+
+	| aSuffix wording anInterface getter doc |
 	operatorOrExpression _ (assignmentRoot, assignmentSuffix) asSymbol.
-	aSuffix _ ScriptingSystem wordingForAssignmentSuffix: assignmentSuffix.
-	operatorReadoutString _ assignmentRoot, ' ', aSuffix.
+	aSuffix _ self currentVocabulary translatedWordingFor:  assignmentSuffix.
+	getter _ Utilities getterSelectorFor: assignmentRoot.
+	anInterface _ self currentVocabulary methodInterfaceAt: getter ifAbsent: [Vocabulary eToyVocabulary methodInterfaceAt: getter ifAbsent: [nil]].
+	wording _ anInterface ifNotNil: [anInterface wording] ifNil: [assignmentRoot copyWithout: $:].
+	(anInterface notNil and: [(doc _ anInterface documentation) notNil])
+		ifTrue:
+			[self setBalloonText: doc].
+	operatorReadoutString _ wording, ' ', aSuffix.
  	self line1: operatorReadoutString.
 	self addArrowsIfAppropriate
