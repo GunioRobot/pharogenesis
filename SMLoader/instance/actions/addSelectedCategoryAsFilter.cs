68a3de7ba31f223c04c08c6d3647eeb6bda6dba4
gk <unknown>
2004-07-12 14:42:00 +0000

    no message

---
+++ SMLoader/instance/actions/addSelectedCategoryAsFilter.cs
@@ -0,0 +1,5 @@
+addSelectedCategoryAsFilter
+	"Add a new filter that filters on the currently selected category.
+	Make it enabled as default."
+
+	categoriesToFilterIds add: self selectedCategory id
