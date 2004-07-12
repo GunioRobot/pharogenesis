32b9451b094b9e0da6ef29a69fc16f576c609238
gk <unknown>
2004-07-12 14:42:00 +0000

    no message

---
+++ SMLoader/instance/actions/saveFiltersAsDefault.cs
@@ -0,0 +1,6 @@
+saveFiltersAsDefault
+	"Save the current filters as default so that they
+	are selected the next time the loader is opened."
+
+	DefaultFilters := filters copy.
+	DefaultCategoriesToFilterIds := categoriesToFilterIds copy
