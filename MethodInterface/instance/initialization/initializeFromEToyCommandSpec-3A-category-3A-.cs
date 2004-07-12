4b16ecd9b5b46d16250d7bea0c59388af103ff52
mir <unknown>
2004-07-12 19:39:00 +0000

    no message

--- MethodInterface/instance/initialization/initializeFromEToyCommandSpec-3A-category-3A-.cs
+++ MethodInterface/instance/initialization/initializeFromEToyCommandSpec-3A-category-3A-.cs
@@ -2,9 +2,12 @@ initializeFromEToyCommandSpec: tuple category: aCategorySymbol
 	"tuple holds an old etoy command-item spec, of the form found in #additionsToViewerCategories methods.   Initialize the receiver to hold the same information"
 
 	selector _ tuple second.
-	self documentation: tuple third.
+	receiverType _ #Player.
+	selector numArgs == 1 ifTrue:
+		[argumentVariables _ OrderedCollection with:
+			(Variable new name: (Player formalHeaderPartsFor: selector) fourth type: tuple fourth)].
+
 	aCategorySymbol ifNotNil: [self flagAttribute: aCategorySymbol].
-	tuple size > 3 ifTrue:
-		[argumentSpecifications _ OrderedCollection with:
-			(Variable new variableType: tuple fourth)]
-	
+	self
+		wording: (ScriptingSystem wordingForOperator: selector);
+		helpMessage:  tuple third
