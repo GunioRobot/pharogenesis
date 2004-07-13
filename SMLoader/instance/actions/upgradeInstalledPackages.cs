e991a561269cecfec923f784564b0d4d6cca1c89
gk <unknown>
2004-07-13 15:25:00 +0000

    no message

--- SMLoader/instance/actions/upgradeInstalledPackages.cs
+++ SMLoader/instance/actions/upgradeInstalledPackages.cs
@@ -3,11 +3,11 @@ upgradeInstalledPackages
 	version of Squeak. So this is a conservative approach."
 
 	| installed old myRelease toUpgrade info |
-	installed := model installedPackages.
-	old := model oldPackages.
+	installed := squeakMap installedPackages.
+	old := squeakMap oldPackages.
 	old isEmpty ifTrue: [
 			^self inform: 'All ', installed size printString, ' installed packages are up to date.'].
-	toUpgrade := model upgradeableAndOldPackages.
+	toUpgrade := squeakMap upgradeableAndOldPackages.
 	toUpgrade isEmpty ifTrue: [
 			^self inform: 'None of the ', old size printString, ' old packages of the ', installed size printString, ' installed can be automatically upgraded. You need to upgrade them manually.'].
 	old size < toUpgrade size ifTrue: [
@@ -21,10 +21,9 @@ About to upgrade the following packages:
 ', (String streamContents: [:s | toUpgrade do: [:p | s nextPutAll: p nameWithVersionLabel; cr]]), 'Proceed?') ifTrue: [
 			myRelease := self installedReleaseOfMe.
 			[Cursor wait showWhile: [
-				model upgradeOldPackages.
+				squeakMap upgradeOldPackages.
 				self inform: toUpgrade size printString, ' packages successfully upgraded.'.
-				myRelease = self installedReleaseOfMe
-					ifFalse: [self reOpen]
-					ifTrue: [self noteChanged]]
+				myRelease = self installedReleaseOfMe ifFalse: [self reOpen].
+				self noteChanged]
 			] on: Error do: [:ex |
 				self informException: ex msg: ('Error occurred when upgrading old packages:\', ex messageText, '\') withCRs]]
