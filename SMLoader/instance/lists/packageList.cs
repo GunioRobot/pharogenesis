328c84bb12745e7af4d1c51cf525aeb0b0c5d278
gk <unknown>
2004-07-13 17:10:00 +0000

    no message

---
+++ SMLoader/instance/lists/packageList.cs
@@ -0,0 +1,19 @@
+packageList
+	"Return a list of the SMPackages that should be visible
+	by applying all the filters. Also filter based on the currently
+	selected category - if any."
+
+	| list selectedCategory |
+	list := packagesList ifNil: [
+			packagesList := self packages select: [:p | 
+				filters allSatisfy: [:currFilter |
+					currFilter isSymbol
+						ifTrue: [(self perform: currFilter) value: p]
+						ifFalse: [
+						self package: p
+							filteredByCategory: (squeakMap object: currFilter)]]]].
+	selectedCategoryWrapper ifNil:[self updateLabel: list. ^list].
+	selectedCategory := selectedCategoryWrapper category.
+	list := list select: [:each | self package: each filteredByCategory: selectedCategory].
+	self updateLabel: list.
+	^list
