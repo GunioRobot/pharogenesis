ec9af755d1d1eb387e34ef07c0aee8ea6539d470
apb <unknown>
2004-07-13 00:40:00 +0000

    no message

--- Behavior/instance/enumerating/allInstancesDo-3A-.cs
+++ Behavior/instance/enumerating/allInstancesDo-3A-.cs
@@ -1,10 +1,15 @@
 allInstancesDo: aBlock 
 	"Evaluate the argument, aBlock, for each of the current instances of the 
-	receiver."
+	receiver.
+	
+	Because aBlock might change the class of inst (for example, using become:),
+	it is essential to compute next before aBlock value: inst."
 	| inst next |
 	self ==  UndefinedObject ifTrue: [^ aBlock value: nil].
 	inst _ self someInstance.
 	[inst == nil]
 		whileFalse:
-		[aBlock value: inst.
-		inst _ inst nextInstance]
+		[
+		next _ inst nextInstance.
+		aBlock value: inst.
+		inst _ next]
