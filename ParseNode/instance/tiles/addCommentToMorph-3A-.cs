cceb776c08eb8a8dee076401c070726a06508f07
ab <unknown>
2004-07-13 13:47:00 +0000

    no message

--- ParseNode/instance/tiles/addCommentToMorph-3A-.cs
+++ ParseNode/instance/tiles/addCommentToMorph-3A-.cs
@@ -1,6 +1,6 @@
 addCommentToMorph: aMorph
 	| row |
-	(comment isNil or: [comment isEmpty]) ifTrue: [^ self].
+	(self comment isNil or: [self comment isEmpty]) ifTrue: [^ self].
 	row _ aMorph addTextRow:
 		(String streamContents: [:strm | self printCommentOn: strm indent: 1]).
 	row firstSubmorph color: (SyntaxMorph translateColor: #comment).
