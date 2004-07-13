704032c7f37e36d7e02823a53fe0436364369e77
ab <unknown>
2004-07-13 13:53:00 +0000

    no message

--- VariableNode/instance/tiles/explanation.cs
+++ VariableNode/instance/tiles/explanation.cs
@@ -3,7 +3,7 @@ explanation
 	self isSelfPseudoVariable ifTrue: [^'the pseudo variable <self> (refers to the receiver)'].
 	^(#('instance' 'temporary' 'LIT3' 'global') 
 			at: self type 
-			ifAbsent: ['UNK',self type printString]),' variable <',name,'>'
+			ifAbsent: ['UNK',self type printString]),' variable <',self name,'>'
 		
 
 	"LdInstType _ 1.
