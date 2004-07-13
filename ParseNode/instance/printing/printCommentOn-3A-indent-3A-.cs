7c791bd8ea8d740e7d038761a9b250266e23837d
ab <unknown>
2004-07-13 13:46:00 +0000

    no message

--- ParseNode/instance/printing/printCommentOn-3A-indent-3A-.cs
+++ ParseNode/instance/printing/printCommentOn-3A-indent-3A-.cs
@@ -1,14 +1,14 @@
 printCommentOn: aStream indent: indent 
 	| thisComment |
-	comment == nil ifTrue: [^ self].
+	self comment == nil ifTrue: [^ self].
 	aStream withStyleFor: #comment
-		do: [1 to: comment size do: 
+		do: [1 to: self comment size do: 
 				[:index | 
 				index > 1 ifTrue: [aStream crtab: indent].
 				aStream nextPut: $".
-				thisComment _ comment at: index.
+				thisComment _ self comment at: index.
 				self printSingleComment: thisComment
 					on: aStream
 					indent: indent.
 				aStream nextPut: $"]].
-	comment _ nil
+	self comment: nil
