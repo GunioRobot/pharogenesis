d6164981131d623fd8e6e41eb982a32295e34dd9
ab <unknown>
2004-07-13 13:53:00 +0000

    no message

--- VariableNode/instance/initialize-2D-release/name-3A-key-3A-index-3A-type-3A-.cs
+++ VariableNode/instance/initialize-2D-release/name-3A-key-3A-index-3A-type-3A-.cs
@@ -1,6 +1,6 @@
 name: varName key: objRef index: i type: type
 	"Only used for initting global (litInd) variables"
-	name _ varName.
+	self name: varName.
 	self key: objRef
 		index: i
 		type: type
