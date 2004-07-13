16279d06a141ec33d9fa6af2b10a20812281363f
ab <unknown>
2004-07-13 13:54:00 +0000

    no message

--- VariableNode/instance/tiles/asMorphicSyntaxIn-3A-.cs
+++ VariableNode/instance/tiles/asMorphicSyntaxIn-3A-.cs
@@ -1,5 +1,5 @@
 asMorphicSyntaxIn: parent
 
-	^ parent addToken: name
+	^ parent addToken: self name
 			type: #variable 
 			on: self clone	"don't hand out the prototype! See VariableNode>>initialize"
