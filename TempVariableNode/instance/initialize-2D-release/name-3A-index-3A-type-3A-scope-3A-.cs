b2f8a40eca3422bbfa979490c997e40e53fd5a80
ab <unknown>
2004-07-13 13:57:00 +0000

    no message

--- TempVariableNode/instance/initialize-2D-release/name-3A-index-3A-type-3A-scope-3A-.cs
+++ TempVariableNode/instance/initialize-2D-release/name-3A-index-3A-type-3A-scope-3A-.cs
@@ -1,8 +1,8 @@
 name: varName index: i type: type scope: level
 	"Only used for initting temporary variables"
-	name _ varName.
+	self name: varName.
 	self key: varName
 		index: i
 		type: type.
-	isAnArg _ hasDefs _ hasRefs _ false.
-	scope _ level
+	self isArg: (hasDefs _ hasRefs _ false).
+	self scope: level
