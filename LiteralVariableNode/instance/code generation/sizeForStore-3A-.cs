f779b36ee3823503cfb701289a5ea628fc1b7c39
ab <unknown>
2004-07-13 13:52:00 +0000

    no message

---
+++ LiteralVariableNode/instance/code generation/sizeForStore-3A-.cs
@@ -0,0 +1,9 @@
+sizeForStore: encoder
+	| index |
+	(self key isVariableBinding and:[self key isSpecialWriteBinding]) 
+		ifFalse:[^super sizeForStore: encoder].
+	self code < 0 ifTrue:[
+		index _ self index.
+		self code: (self code: index type: LdLitType)].
+	splNode _ encoder encodeSelector: #value:.
+	^ (splNode size: encoder args: 1 super: false) + (super sizeForValue: encoder)
