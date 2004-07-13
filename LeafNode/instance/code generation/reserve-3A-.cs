de161b11817789c89bda103ca2e541940e89aa1c
ab <unknown>
2004-07-13 13:52:00 +0000

    no message

--- LeafNode/instance/code generation/reserve-3A-.cs
+++ LeafNode/instance/code generation/reserve-3A-.cs
@@ -1,4 +1,4 @@
 reserve: encoder 
 	"If this is a yet unused literal of type -code, reserve it."
 
-	code < 0 ifTrue: [code _ self code: (encoder litIndex: key) type: 0 - code]
+	self code < 0 ifTrue: [self code: (self code: (encoder litIndex: self key) type: 0 - self code)]
