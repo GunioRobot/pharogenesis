010dda9fe2076bb6846220b6763f2cd0bc75c564
gk <unknown>
2004-07-13 17:07:00 +0000

    no message

--- SMLoader/instance/private/noteChanged.cs
+++ SMLoader/instance/private/noteChanged.cs
@@ -1,9 +1,8 @@
 noteChanged
+	packagesList := nil.
+	selectedCategoryWrapper := nil.
 	filters ifNil: [^self reOpen].
-	model ifNotNil: [
-		packagesList := nil.
-		selectedCategoryWrapper := nil.
-		self changed: #categoryWrapperList.
-		self changed: #packageWrapperList.
-		self changed: #packagesListIndex.	"update my selection"
-		self contentsChanged]
+	self changed: #categoryWrapperList.
+	self changed: #packageWrapperList.
+	self changed: #packagesListIndex.	"update my selection"
+	self contentsChanged
