bb64a24c68f2c6b714e6baf535d089477ef6f4ad
ab <unknown>
2004-07-13 13:53:00 +0000

    no message

--- LeafNode/instance/copying/veryDeepFixupWith-3A-.cs
+++ LeafNode/instance/copying/veryDeepFixupWith-3A-.cs
@@ -2,4 +2,4 @@ veryDeepFixupWith: deepCopier
 	"If fields were weakly copied, fix them here.  If they were in the tree being copied, fix them up, otherwise point to the originals!!"
 
 super veryDeepFixupWith: deepCopier.
-key _ deepCopier references at: key ifAbsent: [key].
+self key: (deepCopier references at: self key ifAbsent: [self key]).
