b52649725059f5abd306dbfe702721c4b7c1025d
ab <unknown>
2004-07-13 13:54:00 +0000

    no message

--- VariableNode/instance/tiles/variableGetterBlockIn-3A-.cs
+++ VariableNode/instance/tiles/variableGetterBlockIn-3A-.cs
@@ -2,19 +2,19 @@ variableGetterBlockIn: aContext
 
 	| temps index ivars |
 
-	(self type = 4 and: [key isKindOf: Association]) ifTrue: [
-		^[key value]
+	(self type = 4 and: [self key isVariableBinding]) ifTrue: [
+		^[self key value]
 	].
 	aContext ifNil: [^nil].
 	self isSelfPseudoVariable ifTrue: [^[aContext receiver]].
 	self type = 1 ifTrue: [
 		ivars _ aContext receiver class allInstVarNames.
-		index _ ivars indexOf: name ifAbsent: [^nil].
+		index _ ivars indexOf: self name ifAbsent: [^nil].
 		^[aContext receiver instVarAt: index]
 	].
 	self type = 2 ifTrue: [
 		temps _ aContext tempNames.
-		index _ temps indexOf: name ifAbsent: [^nil].
+		index _ temps indexOf: self name ifAbsent: [^nil].
 		^[aContext tempAt: index]
 	].
 	^nil
