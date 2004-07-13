2f0079eddcf7d4518f145bd27e5fb737b9c3e490
gk <unknown>
2004-07-13 17:10:00 +0000

    no message

--- SMLoader/instance/lists/updateLabel-3A-.cs
+++ SMLoader/instance/lists/updateLabel-3A-.cs
@@ -2,4 +2,4 @@ updateLabel: packagesShown
 	"Update the label of the window."
 
 	self setLabel: 'SqueakMap Package Loader (', packagesShown size printString,
-			'/', model packages size printString, ')'
+			'/', squeakMap packages size printString, ')'
