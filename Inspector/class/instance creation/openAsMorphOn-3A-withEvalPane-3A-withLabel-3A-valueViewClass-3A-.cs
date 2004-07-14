89fa6db22f69af792f664f56c9354060001a4744
apb <unknown>
2004-07-14 12:54:00 +0000

    no message

--- Inspector/class/instance creation/openAsMorphOn-3A-withEvalPane-3A-withLabel-3A-valueViewClass-3A-.cs
+++ Inspector/class/instance creation/openAsMorphOn-3A-withEvalPane-3A-withLabel-3A-valueViewClass-3A-.cs
@@ -1,4 +1,4 @@
 openAsMorphOn: anObject withEvalPane: withEval withLabel: label valueViewClass: valueViewClass
 	"Note: for now, this always adds an eval pane, and ignores the valueViewClass"
 
-	(self openAsMorphOn: anObject withLabel: label) openInWorld
+	^ (self openAsMorphOn: anObject withLabel: label) openInWorld
