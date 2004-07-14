fb5054dec4518f827a680cc458feebe0d5742048
gk <unknown>
2004-07-14 17:16:00 +0000

    no message

---
+++ SMPackage/instance/testing/isSafelyOld.cs
@@ -0,0 +1,8 @@
+isSafelyOld
+	"Answer if I am installed and there also is a
+	newer published version for this version of Squeak available."
+
+	| installed |
+	installed _ self installedRelease.
+	^installed ifNil: [false] ifNotNil: [
+		^(self lastPublishedReleaseForCurrentSystemVersionNewerThan: installed) notNil]
