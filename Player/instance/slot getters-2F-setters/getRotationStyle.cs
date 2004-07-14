8d3232db76fd3093623adf91ffc4c357a0a9827f
sw <unknown>
2004-07-14 21:37:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/getRotationStyle.cs
+++ Player/instance/slot getters-2F-setters/getRotationStyle.cs
@@ -1,2 +1,5 @@
 getRotationStyle
-	^ self getValueFromCostume: #rotationStyle
+	"Answer the symbol representing the rotation style"
+
+	^ (#(rotate #'do not rotate' #'flip left right' #'flip up down') at:
+		(#(normal none leftRight upDown ) indexOf: costume renderedMorph rotationStyle))
