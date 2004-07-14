2711f2229c873e63e9eb2edb17a6ab5db24c8c86
gk <unknown>
2004-07-14 17:31:00 +0000

    no message

---
+++ SMPackage/instance/installation/upgrade.cs
@@ -0,0 +1,8 @@
+upgrade
+	"Upgrade to the latest newer published version for this version of Squeak."
+
+	| installed |
+	installed _ self installedRelease.
+	installed
+		ifNil: [self error: 'No release installed, can not upgrade.']
+		ifNotNil: [^installed upgrade]
