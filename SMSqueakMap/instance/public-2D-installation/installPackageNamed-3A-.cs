ca41f4faaaa5688e6194beda943f14c0a0da8ba5
gk <unknown>
2004-07-14 15:54:00 +0000

    no message

---
+++ SMSqueakMap/instance/public-2D-installation/installPackageNamed-3A-.cs
@@ -0,0 +1,13 @@
+installPackageNamed: aString
+	"Install the last published release
+	for this Squeak version of the package with a name
+	beginning with aString (see method comment
+	of #packageWithNameBeginning:).
+
+	Note: This method should not be used anymore.
+	Better to specify a specific release."
+
+	| p |
+	p _ self packageWithNameBeginning: aString.
+	p ifNil: [self error: 'No package found with name beginning with ', aString].
+	^self installPackage: p
