43a69f82c468bd77d5808cf216ef257f445dc972
sw <unknown>
2004-07-14 21:35:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/setRotationStyle-3A-.cs
+++ Player/instance/slot getters-2F-setters/setRotationStyle-3A-.cs
@@ -1,2 +1,6 @@
-setRotationStyle: aStyle
-	self setCostumeSlot: #rotationStyle toValue: aStyle
+setRotationStyle: aStyleSymbol
+	"Set the rotation style to the indicated symbol; the external symbols seen are different, as you'll observe..."
+
+	costume renderedMorph rotationStyle: 
+		(#(normal none leftRight upDown ) at:
+		(#(rotate #'do not rotate' #'flip left right' #'flip up down') indexOf: aStyleSymbol))
