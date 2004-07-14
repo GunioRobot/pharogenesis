33e91555ceb699bd50c195b802b05ab528555484
gk <unknown>
2004-07-14 15:51:00 +0000

    no message

---
+++ SMSqueakMap/instance/public-2D-installation/installPackage-3A-.cs
@@ -0,0 +1,10 @@
+installPackage: aPackage
+	"Install the package.
+
+	Note: This method should not be used anymore, better
+	to specify a specific release."
+
+	| rel |
+	rel _ aPackage lastPublishedReleaseForCurrentSystemVersion
+			ifNil: [self error: 'No published release for this system version found to install.'].
+	^self installPackageRelease: rel
