b8db1fc12d73428dae7444c68eb684030833f3bd
gk <unknown>
2004-07-14 16:29:00 +0000

    no message

---
+++ SMLoader/instance/actions/upgradeInstalledPackagesConfirm.cs
@@ -0,0 +1,5 @@
+upgradeInstalledPackagesConfirm
+	"Tries to upgrade all installed packages to the latest published release for this
+	version of Squeak. Confirms on each upgrade."
+
+	^ self upgradeInstalledPackagesConfirm: true
