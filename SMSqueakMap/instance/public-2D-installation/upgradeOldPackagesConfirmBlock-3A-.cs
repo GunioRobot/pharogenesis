157128b91fb7960e7cb2f7ff7a7718a1ae5be2ce
gk <unknown>
2004-07-14 16:26:00 +0000

    no message

---
+++ SMSqueakMap/instance/public-2D-installation/upgradeOldPackagesConfirmBlock-3A-.cs
@@ -0,0 +1,14 @@
+upgradeOldPackagesConfirmBlock: aBlock
+	"First we find out which of the installed packages are upgradeable and old.
+	Then we upgrade them if confirmation block yields true.
+	The block will be called with each SMPackage to upgrade.
+	We return a Dictionary with the packages we tried to upgrade as keys
+	and the value being the result of the upgrade, true or false."
+
+	| result |
+	result _ Dictionary new.
+	self upgradeableAndOldPackages
+		do: [:package |
+			(aBlock value: package)
+				ifTrue:[ result at: package put: package upgrade]].
+	^result
