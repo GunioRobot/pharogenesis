6a133f76737f06925cda2070fd83dae1793f08dd
md <unknown>
2004-07-15 17:23:00 +0000

    no message

--- EventRecorderMorph/instance/fileIn-2F-Out/writeTape.cs
+++ EventRecorderMorph/instance/fileIn-2F-Out/writeTape.cs
@@ -1,18 +1,12 @@
 writeTape
 	| args b |
-	args _ (b _ self button: 'writeTape') isNil
+	args := (b := self button: 'writeTape') isNil 
 				ifTrue: [#()]
 				ifFalse: [b arguments].
-	(args size > 0 and: [args first size > 0])
-		ifTrue: [args first.
-				self writeTape: args first]
-		ifFalse: [(Smalltalk
-					at: #RequestBoxMorph
-					ifAbsent:
-						[^ self writeTape:
-							(FillInTheBlank
-								request: 'Tape to write'
-								initialAnswer: 'tapeName.tape')])
-						request: 'Tape to write'
-						respondTo: self
-						selector: #writeTape:]
+	(args notEmpty and: [args first notEmpty]) 
+		ifTrue: 
+			[args first.
+			self writeTape: args first]
+		ifFalse: 
+			[^self writeTape: (FillInTheBlank request: 'Tape to write'
+								initialAnswer: 'tapeName.tape')].
