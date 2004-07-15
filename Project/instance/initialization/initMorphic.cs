a9c6f718646a4b6f2cd2f93e95ebd131e8d2db63
mir <unknown>
2004-07-15 19:07:00 +0000

    no message

--- Project/instance/initialization/initMorphic.cs
+++ Project/instance/initialization/initMorphic.cs
@@ -1,7 +1,14 @@
 initMorphic
+	"Written so that Morphic can still be removed.  Note that #initialize is never actually called for a morphic project -- see the senders of this method."
 
-	"Written so that Morphic can still be removed."
 	Smalltalk verifyMorphicAvailability ifFalse: [^ nil].
-	self initialize.
-	world _ PasteUpMorph newWorldForProject: self.
+	changeSet := ChangeSet new.
+	transcript := TranscriptStream new.
+	displayDepth := Display depth.
+	parentProject := CurrentProject.
+	isolatedHead := false.
+	world := PasteUpMorph newWorldForProject: self.
+	Locale switchToID: CurrentProject localeID.
+	self initializeProjectPreferences "Do this *after* a world is installed so that the project will be recognized as a morphic one."
+
 
