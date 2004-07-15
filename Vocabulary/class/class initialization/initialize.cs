e559a44afb7cf81a841dbbf989e716d5030be479
mir <unknown>
2004-07-15 19:10:00 +0000

    no message

--- Vocabulary/class/class initialization/initialize.cs
+++ Vocabulary/class/class initialization/initialize.cs
@@ -1,7 +1,8 @@
 initialize
-	"Initialize a few standard vocabularies and place them in the AllVocabularies list."
+	"Initialize a few standard vocabularies and place them in the AllVocabularies list.  Call this to update all vocabularies."
 
 	self initializeStandardVocabularies.
+	self embraceAddedTypeVocabularies.
 
 	"Vocabulary initialize"
 
