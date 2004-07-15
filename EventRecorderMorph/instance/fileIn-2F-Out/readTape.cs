b65d5923ff71365b3ce314bba76f531cf0a9d758
md <unknown>
2004-07-15 17:22:00 +0000

    no message

--- EventRecorderMorph/instance/fileIn-2F-Out/readTape.cs
+++ EventRecorderMorph/instance/fileIn-2F-Out/readTape.cs
@@ -1,11 +1,4 @@
 readTape
-	(Smalltalk
-		at: #RequestBoxMorph
-		ifAbsent:
-			[^ self readTape:
-				(FillInTheBlank
-					request: 'Tape to read'
-					initialAnswer: 'tapeName.tape')])
-		request: 'Tape to read'
-		respondTo: self
-		selector: #readTape:
+	^ self readTape: (FillInTheBlank
+							request: 'Tape to read'
+							initialAnswer: 'tapeName.tape').
