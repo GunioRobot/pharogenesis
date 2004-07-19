56bd19811aeac7614a888014531eec8ff82316de
sw <unknown>
2004-07-19 10:37:00 +0000

    no message

---
+++ AllPlayersTool/class/parts bin/descriptionForPartsBin.cs
@@ -0,0 +1,6 @@
+descriptionForPartsBin
+	"Answer a description for use in parts bins"
+
+	^ self partName: 	'Players'
+		categories:		#('Scripting')
+		documentation:	'A tool showing all the players in your project'
