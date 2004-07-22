0bde77111facc5dcd2482c4b91a2b54e5b207553
md <unknown>
2004-07-22 11:48:00 +0000

    no message

---
+++ Complex/instance/arithmetic/divideSecureBy-3A-.cs
@@ -0,0 +1,20 @@
+divideSecureBy: anObject
+	"Answer the result of dividing receiver by aNumber"
+	" Both operands are scaled to avoid arithmetic overflow. This algorithm 
+	  works for a wide range of values, but it requires six divisions.  
+	  #divideFastAndSecureBy:  is also quite good, but it uses only 3 divisions.
+	   Note: #reciprocal uses #/ for devision"
+	 
+	| s ars ais brs bis newReal newImaginary |
+	anObject isComplex ifTrue:
+		[s := anObject real abs + anObject imaginary abs.
+		 ars := self real / s.
+		 ais := self imaginary / s.
+		 brs := anObject real / s.
+		 bis := anObject imaginary / s.
+		 s := brs squared + bis squared.
+		
+		newReal _ ars*brs + (ais*bis) /s.
+		newImaginary _ ais*brs - (ars*bis)/s.
+		^ Complex real: newReal imaginary: newImaginary].
+	^ anObject adaptToComplex: self andSend: #/.
