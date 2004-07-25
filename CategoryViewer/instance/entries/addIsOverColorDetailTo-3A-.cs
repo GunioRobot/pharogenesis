c3a0fecdb1a343e696fc9365af6f1f4aa6782678
sw <unknown>
2004-07-25 15:37:00 +0000

    no message

--- CategoryViewer/instance/entries/addIsOverColorDetailTo-3A-.cs
+++ CategoryViewer/instance/entries/addIsOverColorDetailTo-3A-.cs
@@ -1,8 +1,13 @@
 addIsOverColorDetailTo: aRow
-	| clrTile readout aTile |
-	aRow addMorphBack: (Morph new color: self color; extent: 2@10).  " spacer".
+	"Special-casee code for the boolean-valued phrase variously known as is-over-color or sees-color."
+
+	| clrTile |
+	aRow addMorphBack: (Morph new color: self color; extent: 2@10).  "spacer"
 	aRow addMorphBack: (clrTile _ Color blue newTileMorphRepresentative).
-	aRow addMorphBack: (AlignmentMorph new beTransparent).  "flexible spacer"
+
+"The following commented-out code put a readout up; the readout was very nice, but was very consumptive of cpu time, which is why the is-over-color tile got removed from the viewer long ago.  Now is-over-color is reinstated to the viewer, minus the expensive readout..."
+
+"	aRow addMorphBack: (AlignmentMorph new beTransparent).
 	readout _ UpdatingStringMorphWithArgument new
 			target: scriptedPlayer; getSelector: #seesColor:; growable: false; putSelector: nil;
 			argumentTarget: clrTile colorSwatch argumentGetSelector: #color.
@@ -10,4 +15,4 @@ addIsOverColorDetailTo: aRow
 	aTile _ StringReadoutTile new typeColor: Color lightGray lighter.
 	aTile addMorphBack: readout.
 	aRow addMorphBack: aTile.
-	aTile setLiteralTo: (scriptedPlayer seesColor: clrTile colorSwatch color) printString width: 30
+	aTile setLiteralTo: (scriptedPlayer seesColor: clrTile colorSwatch color) printString width: 30"
