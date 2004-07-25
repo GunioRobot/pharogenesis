ea39ddea010cf0e8ba16e6db0ec758da361eec7c
sw <unknown>
2004-07-25 17:26:00 +0000

    no message

--- Preferences/class/fonts/setCodeFontTo-3A-.cs
+++ Preferences/class/fonts/setCodeFontTo-3A-.cs
@@ -1,5 +1,4 @@
 setCodeFontTo: aFont
-	"Not currently sent, but once protocols are sorted out so that we can disriminate on whether a text object being launched is for code or not, will be reincorporated"
+	"Establish the code font."
 
-	Parameters at: #standardCodeFont put: aFont.
-	Utilities replaceToolsFlap
+	Parameters at: #standardCodeFont put: aFont
