917c66dd44d0676f37d9cc3cb23dd522eb56cfb5
sw <unknown>
2004-07-25 03:16:00 +0000

    no message

--- SketchEditorMorph/instance/actions & preps/rect-3A-.cs
+++ SketchEditorMorph/instance/actions & preps/rect-3A-.cs
@@ -1,7 +1,7 @@
 rect: evt 
-	"While moving corner, just write on the canvas. When done, write 
-	on the paintingForm"
-	| rect within oldRect now diff cor cColor sOrigin priorEvt |
+	"While moving corner, just write on the canvas. When done, write on the paintingForm"
+
+	| rect oldRect now diff cor cColor sOrigin priorEvt |
 	sOrigin _ self get: #strokeOrigin for: evt.
 	rect _ sOrigin rect: (now _ evt cursorPoint).
 	cColor _ self getColorFor: evt.
@@ -11,7 +11,7 @@ rect: evt
 						+ (Point r: (diff x abs min: diff y abs)
 									* 2 degrees: diff degrees // 90 * 90 + 45).
 			rect _ sOrigin rect: now].
-	(priorEvt _ self get: #lastEvent for: evt) == nil
+	(priorEvt _ self get: #lastEvent for: evt) isNil
 		ifFalse: [oldRect _ sOrigin rect: priorEvt cursorPoint.
 			priorEvt shiftPressed
 				ifTrue: [diff _ priorEvt cursorPoint - sOrigin.
@@ -19,19 +19,17 @@ rect: evt
 								+ (Point r: (diff x abs min: diff y abs)
 											* 2 degrees: diff degrees // 90 * 90 + 45).
 					oldRect _ sOrigin rect: cor].
-			within _ rect containsRect: oldRect.
-			within & cColor isTransparent not
-				ifFalse: ["Last draw will stick out, must erase the area"
-					self restoreRect: oldRect]].
+		self restoreRect: oldRect].  		"Last draw will stick out, must erase the area"
+
 	cColor == Color transparent
 		ifTrue: [formCanvas
 				frameAndFillRectangle: rect
-				fillColor: cColor
+				fillColor: Color transparent
 				borderWidth: (self getNibFor: evt) width
 				borderColor: Color black]
 		ifFalse: [formCanvas
 				frameAndFillRectangle: rect
-				fillColor: cColor
-				borderWidth: 0
-				borderColor: Color transparent].
+				fillColor: Color transparent
+				borderWidth: (self getNibFor: evt) width
+				borderColor: cColor].
 	self invalidRect: rect
