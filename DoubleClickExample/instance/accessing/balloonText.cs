9754d0da161e7f7ed6446b680e108aebc26bd413
nk <unknown>
2004-07-26 10:38:00 +0000

    no message

---
+++ DoubleClickExample/instance/accessing/balloonText.cs
@@ -0,0 +1,5 @@
+balloonText
+	^ 'Double-click on me to change my color; 
+single-click on me to change border color;
+hold mouse down within me and then move it to grow 
+(if I''m red) or shrink (if I''m blue).' translated
