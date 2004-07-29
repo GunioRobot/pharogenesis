728e807672a895de7bb2c5a5c4c011cce5600e46
nk <unknown>
2004-07-29 10:12:00 +0000

    no message

--- Preferences/class/personalization/personalizeUserMenu-3A-.cs
+++ Preferences/class/personalization/personalizeUserMenu-3A-.cs
@@ -2,8 +2,18 @@ personalizeUserMenu: aMenu
 	"The user has clicked on the morphic desktop with the yellow mouse button (option+click on the Mac); a menu is being constructed to present to the user in response; its default target is the current world.  In this method, you are invited to add items to the menu as per personal preferences.
 	The default implementation, for illustrative purposes, sets the menu title to 'personal', and adds items for go-to-previous-project, show/hide flaps, and load code updates"
 	
-	aMenu addTitle: 'personal'.  "Remove or modify this as per personal choice"
+	aMenu addTitle: 'personal' translated.  "Remove or modify this as per personal choice"
 
-	aMenu add: 'previous project' action: #goBack.
-	aMenu addUpdating: #suppressFlapsString target: Utilities action: #toggleFlapSuppressionInProject.
-	aMenu add: 'load latest code updates' target: Utilities action: #updateFromServer
+	aMenu addStayUpItem.
+	aMenu add: 'previous project' translated action: #goBack.
+	aMenu add: 'load latest code updates' translated target: Utilities action: #updateFromServer.
+	aMenu add: 'about this system...' translated target: SmalltalkImage current action: #aboutThisSystem.
+	Preferences isFlagship ifTrue:
+		"For benefit of Alan"
+		[aMenu addLine.
+		aMenu add: 'start using vectors' translated target: ActiveWorld action: #installVectorVocabulary.
+		aMenu add: 'stop using vectors' translated target: ActiveWorld action: #abandonVocabularyPreference].
+	aMenu addLine.
+				
+	aMenu addUpdating: #suppressFlapsString target: CurrentProjectRefactoring action: #currentToggleFlapsSuppressed.
+	aMenu balloonTextForLastItem: 'Whether prevailing flaps should be shown in the project right now or not.' translated
