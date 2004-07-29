2c4b9c96107cc45f903c96a02f7caca6bd64a6c8
nk <unknown>
2004-07-29 10:09:00 +0000

    no message

---
+++ SmalltalkImage/instance/sources, changes log/timeStamp-3A-.cs
@@ -0,0 +1,7 @@
+timeStamp: aStream 
+	"Writes system version and current time on stream aStream."
+
+	| dateTime |
+	dateTime _ Time dateAndTimeNow.
+	aStream nextPutAll: 'From ', SmalltalkImage current datedVersion, ' [', SmalltalkImage current lastUpdateString, '] on ', (dateTime at: 1) printString,
+						' at ', (dateTime at: 2) printString
