f6e7128430d48e499ffce29c0fbcddc19b14eede
nk <unknown>
2004-07-30 22:37:00 +0000

    no message

--- MorphWorldView/class/instance creation/openOn-3A-label-3A-model-3A-.cs
+++ MorphWorldView/class/instance creation/openOn-3A-label-3A-model-3A-.cs
@@ -1,13 +1,15 @@
-openOn: aWorldMorph label: aString model: aModel
+openOn: aWorldMorph label: aString model: aModel 
 	"Open a view with the given label on the given WorldMorph."
+
 	| topView |
-	topView _ self fullColorWhenInactive
-		ifTrue: [topView _ ColorSystemView new]
-		ifFalse: [topView _ StandardSystemView new].
-	topView model: aModel;
+	topView := self fullColorWhenInactive 
+				ifTrue: [topView := ColorSystemView new]
+				ifFalse: [topView := StandardSystemView new].
+	topView
+		model: aModel;
 		label: aString;
 		borderWidth: 1;
-		"minimumSize: aWorldMorph extent + (2@2); " "add border width"
-		addSubView: (self new initialize model: aWorldMorph);
+		addSubView: (self new model: aWorldMorph);
 		backgroundColor: aWorldMorph color.
-	topView controller open.
+	"minimumSize: aWorldMorph extent + (2@2); "	"add border width"
+	topView controller open
