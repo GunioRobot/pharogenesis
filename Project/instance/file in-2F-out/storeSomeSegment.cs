054897bff04c0a11611278367676fa704bbd868c
nk <unknown>
2004-07-30 17:52:00 +0000

    no message

--- Project/instance/file in-2F-out/storeSomeSegment.cs
+++ Project/instance/file in-2F-out/storeSomeSegment.cs
@@ -3,15 +3,20 @@ storeSomeSegment
 	Previous one has to wait for a garbage collection before it can go out."
 
 	| cnt pList start proj gain |
-	cnt _ 0.  gain _ 0.
-	pList _ Project allProjects.
-	start _ pList size atRandom.	"start in a random place"
-	start to: pList size + start do: [:ii | 
-		proj _ pList atWrap: ii.
-		proj storeSegment ifTrue: ["Yes, did send its morphs to the disk"
-			gain _ gain + (proj projectParameters at: #segmentSize 
-						ifAbsent: [0]).	"a guess"
-			self beep.
-			(cnt _ cnt + 1) >= 2 ifTrue: [^ gain]]].
-	self beep.
-	^ gain
+	cnt := 0.
+	gain := 0.
+	pList := Project allProjects.
+	start := pList size atRandom.	"start in a random place"
+	start to: pList size + start
+		do: 
+			[:ii | 
+			proj := pList atWrap: ii.
+			proj storeSegment 
+				ifTrue: 
+					["Yes, did send its morphs to the disk"
+
+					gain := gain + (proj projectParameters at: #segmentSize ifAbsent: [0]).	"a guess"
+					Beeper beep.
+					(cnt := cnt + 1) >= 2 ifTrue: [^gain]]].
+	Beeper  beep.
+	^gain
