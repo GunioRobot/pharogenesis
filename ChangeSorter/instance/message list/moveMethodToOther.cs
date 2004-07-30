51fc82f9d9ff8d1181577e4fae9ea97c2783e970
nk <unknown>
2004-07-30 17:58:00 +0000

    no message

--- ChangeSorter/instance/message list/moveMethodToOther.cs
+++ ChangeSorter/instance/message list/moveMethodToOther.cs
@@ -1,14 +1,17 @@
 moveMethodToOther
 	"Place this change in the other changeSet and remove it from this side"
+
 	| other cls sel |
-	self checkThatSidesDiffer: [^ self].
-	self okToChange ifFalse: [^ self beep].
-	currentSelector ifNotNil:
-		[other _ (parent other: self) changeSet.
-		other == myChangeSet ifTrue: [^ self beep].
-		cls _ self selectedClassOrMetaClass.
-		sel _ currentSelector asSymbol.
-		other absorbMethod: sel class: cls from: myChangeSet.
-	
-		(parent other: self) showChangeSet: other.
-		self forget "removes the method from this side"]
+	self checkThatSidesDiffer: [^self].
+	self okToChange ifFalse: [^Beeper beep].
+	currentSelector ifNotNil: 
+			[other := (parent other: self) changeSet.
+			other == myChangeSet ifTrue: [^Beeper  beep].
+			cls := self selectedClassOrMetaClass.
+			sel := currentSelector asSymbol.
+			other 
+				absorbMethod: sel
+				class: cls
+				from: myChangeSet.
+			(parent other: self) showChangeSet: other.
+			self forget	"removes the method from this side"]
