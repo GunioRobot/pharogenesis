6fb48ff528da2116b237f0486f7c7e7c8191cff2
nk <unknown>
2004-07-30 21:52:00 +0000

    no message

--- MorphWorldView/class/instance creation/convertToMVCWiWPasteUpMorph.cs
+++ MorphWorldView/class/instance creation/convertToMVCWiWPasteUpMorph.cs
@@ -1,30 +1,31 @@
 convertToMVCWiWPasteUpMorph
-"
+	"
 MorphWorldView convertToMVCWiWPasteUpMorph
 "
-	| current w newModel topView |
 
+	| current w newModel topView |
 	Smalltalk isMorphic ifTrue: [^self inform: 'do this in MVC'].
-	current _ self allInstances select: [ :each | each model class == PasteUpMorph].
-	current do: [ :oldWorldView |
-		w _ MVCWiWPasteUpMorph newWorldForProject: nil.
-		w 
-			color: oldWorldView model color;
-			addAllMorphs: oldWorldView model submorphs.
-		newModel _ CautiousModel new initialExtent: 300@300.
-		topView _ self fullColorWhenInactive
-			ifTrue: [ColorSystemView new]
-			ifFalse: [StandardSystemView new].
-		topView
-			model: newModel;
-			label: oldWorldView topView label;
-			borderWidth: 1;
-			addSubView: (self new initialize model: w);
-			backgroundColor: w color.
-		topView controller openNoTerminate.
-		topView reframeTo: (oldWorldView topView expandedFrame expandBy:
-				(0@0 extent: (0@topView labelHeight))).
-		oldWorldView topView controller closeAndUnscheduleNoTerminate.
-	].
+	current := self allInstances 
+				select: [:each | each model class == PasteUpMorph].
+	current do: 
+			[:oldWorldView | 
+			w := MVCWiWPasteUpMorph newWorldForProject: nil.
+			w
+				color: oldWorldView model color;
+				addAllMorphs: oldWorldView model submorphs.
+			newModel := CautiousModel new initialExtent: 300 @ 300.
+			topView := self fullColorWhenInactive 
+						ifTrue: [ColorSystemView new]
+						ifFalse: [StandardSystemView new].
+			topView
+				model: newModel;
+				label: oldWorldView topView label;
+				borderWidth: 1;
+				addSubView: (self new model: w);
+				backgroundColor: w color.
+			topView controller openNoTerminate.
+			topView reframeTo: (oldWorldView topView expandedFrame 
+						expandBy: (0 @ 0 extent: 0 @ topView labelHeight)).
+			oldWorldView topView controller closeAndUnscheduleNoTerminate].
 	ScheduledControllers restore.
-	Processor terminateActive.
+	Processor terminateActive
