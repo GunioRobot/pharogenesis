8aa92265d2343db75c4be1018569a44dc773adeb
nk <unknown>
2004-07-30 21:50:00 +0000

    no message

--- FillInTheBlank/class/instance creation/requestPassword-3A-.cs
+++ FillInTheBlank/class/instance creation/requestPassword-3A-.cs
@@ -1,4 +1,4 @@
-requestPassword: queryString
+requestPassword: queryString 
 	"Create an instance of me whose question is queryString. Invoke it centered
 	at the cursor, and answer the string the user accepts. Answer the empty 
 	string if the user cancels."
@@ -6,15 +6,13 @@ requestPassword: queryString
 	"FillInTheBlank requestPassword: 'POP password'"
 
 	| model fillInView |
-	Smalltalk isMorphic
-		ifTrue: [^ self fillInTheBlankMorphClass requestPassword: queryString].
-
-	model _ self new initialize.
+	Smalltalk isMorphic 
+		ifTrue: [^self fillInTheBlankMorphClass requestPassword: queryString].
+	model := self new.
 	model contents: ''.
-	fillInView _
-		self fillInTheBlankViewClass
-			requestPassword: model
-			message: queryString
-			centerAt: Sensor cursorPoint
-			answerHeight: 40.
-	^ model show: fillInView
+	fillInView := self fillInTheBlankViewClass 
+				requestPassword: model
+				message: queryString
+				centerAt: Sensor cursorPoint
+				answerHeight: 40.
+	^model show: fillInView
