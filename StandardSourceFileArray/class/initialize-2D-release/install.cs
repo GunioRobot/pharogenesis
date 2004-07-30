984870ca2404270f53862a54927187777bdc8ef1
nk <unknown>
2004-07-30 21:50:00 +0000

    no message

--- StandardSourceFileArray/class/initialize-2D-release/install.cs
+++ StandardSourceFileArray/class/initialize-2D-release/install.cs
@@ -1,5 +1,7 @@
 install
 	"Replace SourceFiles by an instance of me with the standard sources and changes files.
 	This only works if SourceFiles is either an Array or an instance of this class"
+
 	"StandardSourceFileArray install"
-	SourceFiles _ self new initialize
+
+	SourceFiles := self new
