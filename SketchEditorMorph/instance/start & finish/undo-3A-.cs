c3427af164886bd57b6c091f08eec36319f35499
nk <unknown>
2004-07-30 17:55:00 +0000

    no message

--- SketchEditorMorph/instance/start & finish/undo-3A-.cs
+++ SketchEditorMorph/instance/start & finish/undo-3A-.cs
@@ -1,20 +1,20 @@
-undo: evt
+undo: evt 
 	"revert to a previous state.  "
 
-	| temp poly |
-
-	self flag: #bob.		"what is undo in multihand environment?"
-	undoBuffer ifNil: [^ self beep].	"nothing to go back to"
-	(poly _ self valueOfProperty: #polygon) ifNotNil:
-		[poly delete.
-		self setProperty: #polygon toValue: nil.
-		^ self].
-	temp _ paintingForm.
-	paintingForm _ undoBuffer.
-	undoBuffer _ temp.		"can get back to what you had by undoing again"
-	(self get: #paintingFormPen for: evt) setDestForm: paintingForm.
-	formCanvas _ paintingForm getCanvas.	"used for lines, ovals, etc."
-	formCanvas _ formCanvas
-		copyOrigin: self topLeft negated
-		clipRect: (0@0 extent: bounds extent).
-	self render: bounds.
+	| temp poly pen |
+	self flag: #bob.	"what is undo in multihand environment?"
+	undoBuffer ifNil: [^Beeper beep].	"nothing to go back to"
+	(poly := self valueOfProperty: #polygon) ifNotNil: 
+			[poly delete.
+			self setProperty: #polygon toValue: nil.
+			^self].
+	temp := paintingForm.
+	paintingForm := undoBuffer.
+	undoBuffer := temp.	"can get back to what you had by undoing again"
+	pen := self get: #paintingFormPen for: evt.
+	pen ifNil: [^Beeper  beep].
+	pen setDestForm: paintingForm.
+	formCanvas := paintingForm getCanvas.	"used for lines, ovals, etc."
+	formCanvas := formCanvas copyOrigin: self topLeft negated
+				clipRect: (0 @ 0 extent: bounds extent).
+	self render: bounds
