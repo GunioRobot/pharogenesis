e61de579aa7d0cb13bfe188f738b8b71dab5c903
nk <unknown>
2004-07-30 21:50:00 +0000

    no message

--- PluggableTest/class/example/open.cs
+++ PluggableTest/class/example/open.cs
@@ -2,28 +2,27 @@ open
 	"PluggableTest open"
 
 	| model listView1 topView listView2 |
-	model _ self new initialize.
-	listView1 _
-		PluggableListView on: model
-			list: #musicTypeList
-			selected: #musicType
-			changeSelected: #musicType:
-			menu: #musicTypeMenu:
-			keystroke: #musicTypeKeystroke:.
-	listView1  menuTitleSelector: #musicTypeListTitle.
-	listView2 _
-		PluggableListView on: model
-			list: #artistList
-			selected: #artist
-			changeSelected: #artist:
-			menu: nil
-			keystroke: #artistKeystroke:.
-
-	topView _ StandardSystemView new
-		label: 'Pluggable Test';
-		minimumSize: 300@200;
-		borderWidth: 1;
-		addSubView: listView1;
-		addSubView: listView2 toRightOf: listView1.
+	model := self new.
+	listView1 := PluggableListView 
+				on: model
+				list: #musicTypeList
+				selected: #musicType
+				changeSelected: #musicType:
+				menu: #musicTypeMenu:
+				keystroke: #musicTypeKeystroke:.
+	listView1 menuTitleSelector: #musicTypeListTitle.
+	listView2 := PluggableListView 
+				on: model
+				list: #artistList
+				selected: #artist
+				changeSelected: #artist:
+				menu: nil
+				keystroke: #artistKeystroke:.
+	topView := (StandardSystemView new)
+				label: 'Pluggable Test';
+				minimumSize: 300 @ 200;
+				borderWidth: 1;
+				addSubView: listView1;
+				addSubView: listView2 toRightOf: listView1.
 	topView borderWidth: 1.
-	topView controller open.
+	topView controller open
