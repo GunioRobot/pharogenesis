89dd0defff7e34f6d1690928a4aa2a6458b07a5e
yo <unknown>
2004-08-01 19:46:00 +0000

    no message

--- Player/instance/slots-2D-user/setPrecisionFor-3A-.cs
+++ Player/instance/slots-2D-user/setPrecisionFor-3A-.cs
@@ -1,15 +1,32 @@
-setPrecisionFor: slotName
-	| aList aMenu reply precision val |
-	aList _ #('0' '1' '2' '3' '4' '5').
-	aMenu _ SelectionMenu labels: aList selections: aList.
-	reply _ aMenu startUpWithCaption: 'How many decimal places?'.
-	reply ifNotNil:
-		[(self slotInfoAt: slotName) floatPrecision:
-			(precision _ #(1 0.1 0.01 0.001 0.0001 0.00001 0.000001) at: (aList indexOf: reply)).
-		self class allInstancesDo:   "allSubInstancesDo:"
-			[:anInst |
-				precision ~= 1 ifTrue:
-					[((val _ anInst instVarNamed: slotName asString) isKindOf: Integer)
-						ifTrue:
-							[anInst instVarNamed: slotName asString put: val asFloat]].
-				anInst updateAllViewers]]
+setPrecisionFor: slotName 
+	"Set the precision for the given slot name"
+
+	| aList aMenu reply val aGetter places |
+	aGetter := Utilities getterSelectorFor: slotName.
+	places := Utilities 
+				decimalPlacesForFloatPrecision: (self defaultFloatPrecisionFor: aGetter).
+	aList := #('0' '1' '2' '3' '4' '5' '6').
+	aMenu := SelectionMenu labels: aList
+				selections: (aList collect: [:m | m asNumber]).
+	reply := aMenu 
+				startUpWithCaption: ('How many decimal places? (currently {1})' translated
+						format: {places}).
+	reply ifNotNil: 
+			[(self slotInfo includesKey: slotName) 
+				ifTrue: 
+					["it's a user slot"
+
+					(self slotInfoAt: slotName) 
+						floatPrecision: (Utilities floatPrecisionForDecimalPlaces: reply).
+					self class allInstancesDo: 
+							[:anInst | 
+							reply == 0 
+								ifFalse: 
+									[((val := anInst instVarNamed: slotName asString) isInteger) 
+										ifTrue: [anInst instVarNamed: slotName asString put: val asFloat]].
+							anInst updateAllViewers]]
+				ifFalse: 
+					["it's specifying a preference for precision on a system-defined numeric slot"
+
+					self noteDecimalPlaces: reply forGetter: aGetter.
+					self updateAllViewers]]
