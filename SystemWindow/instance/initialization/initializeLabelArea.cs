b28f49a18bd34afa2540bf688b69896b5b32c779
dew <unknown>
2004-08-03 01:21:00 +0000

    no message

---
+++ SystemWindow/instance/initialization/initializeLabelArea.cs
@@ -0,0 +1,37 @@
+initializeLabelArea
+	"Initialize the label area (titlebar) for the window."
+	label := StringMorph new
+				contents: labelString;
+				font: Preferences windowTitleFont
+				emphasis: (Preferences windowTitleFont isTTCFont ifTrue: [0] ifFalse: [1]).
+	"Add collapse box so #labelHeight will work"
+	collapseBox := self createCollapseBox.
+	stripes := Array
+				with: (RectangleMorph newBounds: bounds)
+				with: (RectangleMorph newBounds: bounds).
+	"see extent:"
+	self addLabelArea.
+	labelArea
+		addMorph: (stripes first borderWidth: 1).
+	labelArea
+		addMorph: (stripes second borderWidth: 2).
+	self setLabelWidgetAllowance.
+	self addCloseBox.
+	self addMenuControl.
+	labelArea addMorph: label.
+	self wantsExpandBox
+		ifTrue: [self addExpandBox].
+	labelArea addMorph: collapseBox.
+	self setFramesForLabelArea.
+	Preferences clickOnLabelToEdit
+		ifTrue: [label
+				on: #mouseDown
+				send: #relabel
+				to: self].
+	Preferences noviceMode
+		ifTrue: [closeBox
+				ifNotNil: [closeBox setBalloonText: 'close window' translated].
+			menuBox
+				ifNotNil: [menuBox setBalloonText: 'window menu' translated].
+			collapseBox
+				ifNotNil: [collapseBox setBalloonText: 'collapse/expand window' translated]].
