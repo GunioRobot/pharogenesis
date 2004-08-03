f7aa6b2e526e74e8d13ccbade8ad07a2fd40fdf7
dew <unknown>
2004-08-03 01:12:00 +0000

    no message

--- SystemWindow/instance/label/setLabel-3A-.cs
+++ SystemWindow/instance/label/setLabel-3A-.cs
@@ -11,5 +11,8 @@ setLabel: aString
 				collapsedFrame ifNotNil:
 					[collapsedFrame _ collapsedFrame withWidth: label width + labelWidgetAllowance]].
 	frame _ LayoutFrame new.
-	frame leftFraction: 0.5; topFraction: 0; leftOffset: label width negated // 2.
+	frame leftFraction: 0.5;
+		 topFraction: 0.5;
+		 leftOffset: label width negated // 2;
+		 topOffset: label height negated // 2.
 	label layoutFrame: frame.
