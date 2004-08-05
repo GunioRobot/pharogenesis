55a6afbe5d101c41a1c7b6f4d7366424a790e546
hpt <unknown>
2004-08-05 20:11:00 +0000

    no message

--- Browser/class/instance creation/fullOnClass-3A-selector-3A-.cs
+++ Browser/class/instance creation/fullOnClass-3A-selector-3A-.cs
@@ -2,8 +2,9 @@ fullOnClass: aClass selector: aSelector
 	"Open a new full browser set to class."
 
 	| brow classToUse |
-	classToUse _ Preferences browseToolClass.
+	classToUse _ SystemBrowser default.
 	brow _ classToUse new.
 	brow setClass: aClass selector: aSelector.
-	classToUse openBrowserView: (brow openEditString: nil)
-		label: brow defaultBrowserTitle
+	^ classToUse 
+		openBrowserView: (brow openEditString: nil)
+		label: brow labelString
