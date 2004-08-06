b89e6aae6759dba4a204f58f826776188e17be2f
hpt <unknown>
2004-08-06 23:22:00 +0000

    no message

--- Preferences/class/hard-2D-coded prefs/browseToolClass.cs
+++ Preferences/class/hard-2D-coded prefs/browseToolClass.cs
@@ -1,6 +1,3 @@
 browseToolClass
-	^ Preferences browserShowsPackagePane
-		ifTrue:
-			[PackagePaneBrowser]
-		ifFalse:
-			[Browser]
+	"This method is used for returning the appropiate class for the #browserShowsPackagePane preference. Now that preference modifies the registry so here we query directly to the registry"
+	^ SystemBrowser default.
