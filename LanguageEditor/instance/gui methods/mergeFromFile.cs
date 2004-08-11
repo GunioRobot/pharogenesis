6ce45493d41987513908b669a12c64c7073cb203
mir <unknown>
2004-08-11 09:56:00 +0000

    no message

---
+++ LanguageEditor/instance/gui methods/mergeFromFile.cs
@@ -0,0 +1,13 @@
+mergeFromFile
+	| fileName |
+	fileName := self selectTranslationFileName.
+	fileName isNil
+		ifTrue: [""
+			self beep.
+			^ self].
+	""
+	Cursor wait
+		showWhile: [
+			self translator loadFromFileNamed: fileName.
+			self changed: #translations.
+			self changed: #untranslated]
