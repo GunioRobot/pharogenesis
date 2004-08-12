56fb8ecb7d64939949efcd23770244b1be30ee5b
sw <unknown>
2004-08-12 02:27:00 +0000

    no message

--- Player/instance/slots-2D-user/tearOffWatcherFor-3A-.cs
+++ Player/instance/slots-2D-user/tearOffWatcherFor-3A-.cs
@@ -1,19 +1,37 @@
-tearOffWatcherFor: aSlotName
-	| aWatcher precision getSel |
+tearOffWatcherFor: aSlotGetter
+	"Tear off a simple textual watcher for the slot whose getter is provided"
+
+	| aWatcher anInterface info isNumeric |
+
+	info _ self slotInfoForGetter: aSlotGetter.
+	info
+		ifNotNil:
+			[isNumeric _ info type == #Number]
+		ifNil:
+			[anInterface _ Vocabulary eToyVocabulary methodInterfaceAt: aSlotGetter ifAbsent: [nil].
+			isNumeric _ anInterface notNil and: [anInterface resultType == #Number]].
 	aWatcher _ UpdatingStringMorph new.
-	getSel _ ScriptingSystem getterSelectorFor: aSlotName.
-	((self typeForSlot: aSlotName) = #number)
+	
+	aWatcher
+		growable: true;
+		getSelector: aSlotGetter;
+		putSelector: (info notNil
+			ifTrue:
+				[ScriptingSystem setterSelectorForGetter: aSlotGetter]
+			ifFalse:
+				[anInterface companionSetterSelector]);
+		setNameTo: (info notNil
+			ifTrue:
+				[Utilities inherentSelectorForGetter: aSlotGetter]
+			ifFalse:
+				[anInterface wording]);
+ 		target: self.
+	isNumeric
 		ifFalse:
 			[aWatcher useStringFormat]
 		ifTrue:
-			[precision _ self defaultFloatPrecisionFor: getSel.
-			precision ~= 1 ifTrue: [aWatcher floatPrecision: precision]].
-
-	aWatcher growable: true;
-		getSelector: getSel;
-		putSelector: (ScriptingSystem setterSelectorFor: aSlotName);
-		setNameTo: aSlotName.
- 	aWatcher target: self.
-	aWatcher step.
-	aWatcher fitContents.
-	self currentHand attachMorph: aWatcher
+			[self setFloatPrecisionFor: aWatcher].
+	aWatcher
+		step;
+		fitContents;
+		openInHand
