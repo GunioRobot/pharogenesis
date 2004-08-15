41ddc318d9a3254ddccfe14b87435be0a872bb34
kfr <unknown>
2004-08-15 15:27:00 +0000

    no message

--- SystemWindow/instance/menu/makeClosable.cs
+++ SystemWindow/instance/menu/makeClosable.cs
@@ -1,5 +1,12 @@
 makeClosable
-	mustNotClose _ false.
-	closeBox ifNil:
-		[self addCloseBox.
-		self extent: self extent]
+	| opaqueColor |
+	mustNotClose := false.
+	closeBox
+		ifNil: [self addCloseBox.
+			self isActive
+				ifTrue: [opaqueColor := self paneColor]
+				ifFalse: [opaqueColor := self paneColor muchDarker].
+			self
+				updateBox: closeBox
+				color: (opaqueColor alphaMixed: 0.5 with: Color red).
+			self extent: self extent]
