3861ea15174e083fbb65a5083eecfa59b133df6b
yo <unknown>
2004-08-16 11:34:00 +0000

    no message

--- Character/instance/converting/asUppercase.cs
+++ Character/instance/converting/asUppercase.cs
@@ -1,6 +1,9 @@
 asUppercase
 	"If the receiver is lowercase, answer its matching uppercase Character."
-	
-	(8r141 <= value and: [value <= 8r172])  "self isLowercase"
+	"A tentative implementation.  Eventually this should consult the Unicode table."	
+
+	| v |
+	v _ self charCode.
+	(((8r141 <= v and: [v <= 8r172]) or: [16rE0 <= v and: [v <= 16rF6]]) or: [16rF8 <= v and: [v <= 16rFE]])
 		ifTrue: [^ Character value: value - 8r40]
 		ifFalse: [^ self]
