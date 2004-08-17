b153c43fc180dce6676f1d93b823df092593472a
yo <unknown>
2004-08-17 09:59:00 +0000

    no message

--- ChangeList/class/public access/browseStream-3A-.cs
+++ ChangeList/class/public access/browseStream-3A-.cs
@@ -2,12 +2,14 @@ browseStream: changesFile
 	"Opens a changeList on a fileStream"
 	| changeList charCount |
 	changesFile readOnly.
+	changesFile setConverterForCode.
 	charCount _ changesFile size.
 	charCount > 1000000 ifTrue:
 		[(self confirm: 'The file ', changesFile name , '
 is really long (' , charCount printString , ' characters).
 Would you prefer to view only the last million characters?')
 			ifTrue: [charCount _ 1000000]].
+	"changesFile setEncoderForSourceCodeNamed: changesFile name."
 	Cursor read showWhile:
 		[changeList _ self new
 			scanFile: changesFile from: changesFile size-charCount to: changesFile size].
