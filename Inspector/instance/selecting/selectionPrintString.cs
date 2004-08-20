65d1ed2200ec4c40e38af291cb43bf228d9f2fb5
apb <unknown>
2004-08-20 22:03:00 +0000

    no message

--- Inspector/instance/selecting/selectionPrintString.cs
+++ Inspector/instance/selecting/selectionPrintString.cs
@@ -1,14 +1,11 @@
 selectionPrintString
-	| text nm |
-	selectionUpdateTime _ [text _ [self selection printStringLimitedTo: 5000]
-		on: Error do: 
-		[nm _ self selectionIndex < 3
-					ifTrue: ['self']
-					ifFalse: [self selectedSlotName].
-		text _ ('<error in printString: evaluate "' , nm , ' printString" to debug>') asText.
-		text
-			addAttribute: TextColor red
-			from: 1
-			to: text size.
-		text]] timeToRun.
+	| text |
+	selectionUpdateTime := [text := [self selection printStringLimitedTo: 5000]
+						on: Error
+						do: [text := self printStringErrorText.
+							text
+								addAttribute: TextColor red
+								from: 1
+								to: text size.
+							text]] timeToRun.
 	^ text
