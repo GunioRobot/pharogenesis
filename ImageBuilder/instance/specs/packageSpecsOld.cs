9c7585a4f347b795ea0ef76d42017e0695f546c4
sd <unknown>
2004-08-20 20:59:00 +0000

    no message

---
+++ ImageBuilder/instance/specs/packageSpecsOld.cs
@@ -0,0 +1,41 @@
+packageSpecsOld
+	"self new packageSpecs collect: [:arr |
+		(SMSqueakMap default packageWithId: arr first)
+releaseWithAutomaticVersionString: arr second]"
+
+	^{
+		{"MCInstaller"
+'af9d090d-2896-4a4e-82d0-c61cf2fdf40e'.'4'}.
+		{"Monticello"
+'66236497-7026-45f5-bcf6-ad00ba7a8a4e'. '14'}.
+		{"PackageInfo"
+'73fb0862-5f49-4b90-b899-c0101335b7c5'. '6'}.
+		{"SARInstaller"
+'75c970ab-dca7-48ee-af42-5a013912c880'. '2'}.
+		{"YAXO"
+'c6e0d836-d1e6-41ac-b1c2-0505bdd46663'. '1'}.
+		{"OpenOffice importer"
+'cf85bb75-1ff8-4da1-a82e-2e53aba05818'. '4'}.
+		{"NetworkHTML"
+'b8ef257d-ed61-46c9-8560-b3e2c1332dc6'. '2'}.
+		{"Scamper"
+'a55ea3ab-f8a0-40cd-8aa9-0077369d1db8'. '2'}.
+		{"Celeste"
+'97aadf92-12b0-4db0-8da3-65041ae18f13'. '2'}.
+		{"IRCe"
+'839bb65f-8e8e-4dbc-8fe1-dc52d822dbf9'. '5'}.
+		{"Skeleton"
+'cd5c7cce-5ee0-40f9-a9ed-86c54799b246' . '2'}.
+		{"Games"
+'643f4620-0fe6-493b-a129-7fd1224e0fe4' . '2'}.
+		{"DefaultExternalDrop"
+'d01f3dbe-bb33-4857-b645-07e81707187d' . '1'}.
+		{"LanguageEditor"
+'db2102cf-be1f-4cb1-ae4b-475c01f0920c'. '2'}.
+		{"VMMaker"
+'2e7f103e-22a6-470d-affe-54b1d04ef34a'. '3'}.
+		{"RB for 3.7"
+'1e73b9cc-a486-4ee1-822e-9352b059333a'. '3'}.
+		{"Balloon3D"
+'178bd01c-f698-41ab-9c73-d9278c65c241'. '1'}
+	}
