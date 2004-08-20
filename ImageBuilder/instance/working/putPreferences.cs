fd3181457bef3a290a33a3a418d8fd77f47c83e3
sd <unknown>
2004-08-20 20:22:00 +0000

    no message

---
+++ ImageBuilder/instance/working/putPreferences.cs
@@ -0,0 +1,34 @@
+putPreferences
+	" 
+	FullImageTools new putPreferences.  
+	"
+	Preferences enable: #abbreviatedBrowserButtons.
+	Preferences enable: #alternativeBrowseIt.
+	Preferences enable: #annotationPanes.
+	Preferences enable: #balloonHelpInMessageLists.
+	Preferences enable: #browseWithDragNDrop.
+	Preferences enable: #browseWithPrettyPrint.
+	Preferences enable: #colorWhenPrettyPrinting.
+	Preferences enable: #diffsWithPrettyPrint.
+	Preferences enable: #dragNDropWithAnimation.
+	Preferences enable: #autoAccessors.
+	Preferences enable: #duplicateControlAndAltKeys.
+	Preferences enable: #classicNewMorphMenu.
+	Preferences enable: #menuColorFromWorld.
+	Preferences enable: #menuKeyboardControl.
+	Preferences enable: #hiddenScrollBars.
+	Preferences enable: #collapseWindowsInPlace.
+	Preferences enable: #projectViewsInWindows.
+	Preferences enable: #scrollBarsOnRight.
+	Preferences disable: #fastDragWindowForMorphic.
+	Preferences enable: #scrollBarsNarrow.
+	Preferences disable: #menuColorFromWorld.
+	Preferences enable: #extraDebuggerButtons.
+	Preferences enable: #propertySheetFromHalo.
+	Preferences enable: #extractFlashInHighestQuality.
+	Preferences enable: #includeSoundControlInNavigator.
+	Preferences enable: #showDirectionForSketches.
+	Preferences enable: #selectiveHalos.
+	Preferences enable: #showDirectionHandles.
+	Preferences disable: #alwaysShowVScrollbar.
+	Preferences enable: #translationWithBabel.
