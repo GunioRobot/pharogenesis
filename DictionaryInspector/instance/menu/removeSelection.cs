9cb1428aca0c5b2379186b8af441973caafd0db5
apb <unknown>
2004-08-20 21:39:00 +0000

    no message

--- DictionaryInspector/instance/menu/removeSelection.cs
+++ DictionaryInspector/instance/menu/removeSelection.cs
@@ -1,10 +1,10 @@
 removeSelection
 	selectionIndex = 0 ifTrue: [^ self changed: #flash].
-	object removeKey: (keyArray at: selectionIndex).
+	object removeKey: (keyArray at: selectionIndex - self numberOfFixedFields).
 	selectionIndex _ 0.
 	contents _ ''.
 	self calculateKeyArray.
 	self changed: #inspectObject.
+	self changed: #selectionIndex.
 	self changed: #fieldList.
 	self changed: #selection.
-	self changed: #selectionIndex.
