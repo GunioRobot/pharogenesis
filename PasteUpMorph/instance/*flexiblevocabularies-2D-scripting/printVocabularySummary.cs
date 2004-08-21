e419be6b290cddab4b493c0cfd08fcba4fedbe89
nk <unknown>
2004-08-21 13:35:00 +0000

    no message

---
+++ PasteUpMorph/instance/*flexiblevocabularies-2D-scripting/printVocabularySummary.cs
@@ -0,0 +1,8 @@
+printVocabularySummary
+	"Put up a window with summaries of all Morph vocabularies."
+
+	
+	(StringHolder new contents: EToyVocabulary vocabularySummary) 
+	openLabel: 'EToy Vocabulary' 
+
+	"self currentWorld printVocabularySummary"
