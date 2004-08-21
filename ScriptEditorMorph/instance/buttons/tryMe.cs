d08c3d3208dcc63b6d2ef45be519e51cf5123a3a
nk <unknown>
2004-08-21 12:16:00 +0000

    no message

--- ScriptEditorMorph/instance/buttons/tryMe.cs
+++ ScriptEditorMorph/instance/buttons/tryMe.cs
@@ -1,5 +1,7 @@
 tryMe
 	"Evaluate the given script on behalf of the scripted object"
 
-	self playerScripted perform: scriptName asSymbol
+	scriptName numArgs = 0
+		ifTrue:
+			[self playerScripted performScriptIfCan: scriptName ]
 
