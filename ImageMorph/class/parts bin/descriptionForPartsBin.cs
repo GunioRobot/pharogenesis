e1673530067c4781d6fc2b14a7b645b06a88ced7
nk <unknown>
2004-08-23 18:11:00 +0000

    no message

---
+++ ImageMorph/class/parts bin/descriptionForPartsBin.cs
@@ -0,0 +1,4 @@
+descriptionForPartsBin
+	^ self partName:	'Image'
+		categories:		#('Graphics' 'Basic')
+		documentation:	'A non-editable picture.  If you use the Paint palette to make a picture, you can edit it afterwards.'
