f1904b73654f7bd87608c737b19279e0fb59feb3
nk <unknown>
2004-08-23 18:11:00 +0000

    no message

---
+++ CurveMorph/class/parts bin/supplementaryPartsDescriptions.cs
@@ -0,0 +1,7 @@
+supplementaryPartsDescriptions
+	^ {DescriptionForPartsBin
+		formalName: 'Curvy Arrow'
+		categoryList: #('Basic' 'Graphics')
+		documentation: 'A curved line with an arrowhead.  Shift-click to get handles and move the points.'
+		globalReceiverSymbol: #CurveMorph
+		nativitySelector: #arrowPrototype}
