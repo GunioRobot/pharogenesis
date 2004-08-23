14f93178ee3915ac63c27b2986982f5de0d3890b
nk <unknown>
2004-08-23 18:12:00 +0000

    no message

---
+++ ScriptableButton/class/name/descriptionForPartsBin.cs
@@ -0,0 +1,4 @@
+descriptionForPartsBin
+	^ self partName:	'Button'
+		categories:		#('Scripting' 'Basic')
+		documentation:	'A button to use with tile scripting; its script will be a method of its containing playfield'
