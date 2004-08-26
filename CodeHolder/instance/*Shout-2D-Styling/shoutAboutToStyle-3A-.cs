29b977118025b77f0f85d8f6609099c00076e088
tween <unknown>
2004-08-26 09:47:00 +0000

    no message

---
+++ CodeHolder/instance/*Shout-2D-Styling/shoutAboutToStyle-3A-.cs
@@ -0,0 +1,5 @@
+shoutAboutToStyle: aPluggableShoutMorphOrView
+	"This is a notification that aPluggableShoutMorphOrView is about to re-style its text.
+	The default is to answer false to veto the styling"
+	
+	^false
