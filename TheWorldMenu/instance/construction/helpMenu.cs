d0125cfeba8af227c557888563d86d34ea6b05b0
dgd <unknown>
2004-08-27 14:08:00 +0000

    no message

--- TheWorldMenu/instance/construction/helpMenu.cs
+++ TheWorldMenu/instance/construction/helpMenu.cs
@@ -1,20 +1,30 @@
 helpMenu
         "Build the help menu for the world."
-        | screenCtrl |
+        |  menu |
 
-        screenCtrl _ ScreenController new.
-        ^self fillIn: (self menu: 'help...') from:
+  	menu := self menu: 'help...'.
+
+        self fillIn: menu from:
         {
-                {'about this system...'. {Smalltalk. #aboutThisSystem}. 'current version information.'}.
+                {'about this system...'. {SmalltalkImage current. #aboutThisSystem}. 'current version information.'}.
                 {'update code from server'. {Utilities. #updateFromServer}. 'load latest code updates via the internet'}.
                 {'preferences...'. {Preferences. #openPreferencesInspector}. 'view and change various options.'}.
+			 {'set language...' . {Project. #chooseNaturalLanguage}. 'choose the language in which tiles should be displayed.'} .
                 nil.
-                {'command-key help'. { Utilities . #openCommandKeyHelp}. 'summary of keyboard shortcuts.'}.
+               {'command-key help'. { Utilities . #openCommandKeyHelp}. 'summary of keyboard shortcuts.'}
+	}.
+
+	self addGestureHelpItemsTo: menu.
+
+	self fillIn: menu from:
+	{
                 {'world menu help'. { self . #worldMenuHelp}. 'helps find menu items buried in submenus.'}.
                         "{'info about flaps' . { Utilities . #explainFlaps}. 'describes how to enable and use flaps.'}."
-                {'font size summary' . { Utilities . #fontSizeSummary}.  'summary of names and sizes of available fonts.'}.
+                {'font size summary' . { TextStyle . #fontSizeSummary}.  'summary of names and sizes of available fonts.'}.
                 {'useful expressions' . { Utilities . #openStandardWorkspace}. 'a window full of useful expressions.'}.
-                {'graphical imports' . { Smalltalk . #viewImageImports}.  'view the global repository called ImageImports; you can easily import external graphics into ImageImports via the FileList'}.
+			 {'annotation setup...' . { Preferences . #editAnnotations}. 'Click here to get a little window that will allow you to specify which types of annotations, in which order, you wish to see in the annotation panes of browsers and other tools'}.
+			nil.
+                {'graphical imports' . { Imports default . #viewImages}.  'view the global repository called ImageImports; you can easily import external graphics into ImageImports via the FileList'}.
                 {'standard graphics library' . { ScriptingSystem . #inspectFormDictionary}.  'lets you view and change the system''s standard library of graphics.'}.
                 nil.
                 {'telemorphic...' . {self. #remoteDo}.  'commands for doing multi-machine "telemorphic" experiments'}.
@@ -22,8 +32,12 @@ helpMenu
                 {'definition for...' . { Utilities . #lookUpDefinition}.  'if connected to the internet, use this to look up the definition of an English word.'}.
                 nil.
 
-                {'set author initials...' . { screenCtrl . #setAuthorInitials }. 'supply initials to be used to identify the author of code and other content.'}.
-                {'vm statistics' . { screenCtrl . #vmStatistics}.  'obtain some intriguing data about the vm.'}.
-                {'space left' . { screenCtrl . #garbageCollect}. 'perform a full garbage-collection and report how many bytes of space remain in the image.'}.
-        }
+                {'set author initials...' . { Utilities . #setAuthorInitials }. 'supply initials to be used to identify the author of code and other content.'}.
+                {'vm statistics' . { self . #vmStatistics}.  'obtain some intriguing data about the vm.'}.
+			  nil.
+			  {'purge undo records' . { CommandHistory . #resetAllHistory }. 'save space by removing all the undo information remembered in all projects.'}.
+                {'space left' . { self . #garbageCollect}. 'perform a full garbage-collection and report how many bytes of space remain in the image.'}.
+        }.
+
+	^menu
 
