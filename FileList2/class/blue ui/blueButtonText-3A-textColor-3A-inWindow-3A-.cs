2bcf0719c67865b2c89fc80a769e3422e897dac3
dgd <unknown>
2004-08-27 18:32:00 +0000

    no message

--- FileList2/class/blue ui/blueButtonText-3A-textColor-3A-inWindow-3A-.cs
+++ FileList2/class/blue ui/blueButtonText-3A-textColor-3A-inWindow-3A-.cs
@@ -1,6 +1,6 @@
 blueButtonText: aString textColor: textColor inWindow: window
 
-	^(window fancyText: aString ofSize: 15 color: textColor)
+	^(window fancyText: aString translated ofSize: 15 color: textColor)
 		setProperty: #buttonText toValue: aString;
 		hResizing: #rigid;
 		extent: 100@20;
