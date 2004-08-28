745fae3bef8dfeb10404516f59cd500bb922a8bc
jrp <unknown>
2004-08-28 14:53:00 +0000

    no message

--- HierarchicalUrl/instance/parsing/privateInitializeFromText-3A-.cs
+++ HierarchicalUrl/instance/parsing/privateInitializeFromText-3A-.cs
@@ -1,53 +1,64 @@
-privateInitializeFromText: aString
-
-	| remainder ind nextTok s specifiedSchemeName |
+privateInitializeFromText: aString 
+	| remainder ind specifiedSchemeName |
 	remainder _ aString.
+	schemeName ifNil: 
+			[specifiedSchemeName _ Url schemeNameForString: remainder.
+			specifiedSchemeName ifNotNil: 
+					[schemeName _ specifiedSchemeName.
+					remainder _ remainder copyFrom: schemeName size + 2 to: remainder size].
+			schemeName ifNil: 
+					["assume HTTP"
 
-	schemeName ifNil: [ 
-		specifiedSchemeName _ Url schemeNameForString: remainder.
-		specifiedSchemeName ifNotNil: [ 
-			schemeName _ specifiedSchemeName.
-			remainder _ remainder copyFrom: (schemeName size+2) to: remainder size ].
-		schemeName ifNil: [ "assume HTTP"  schemeName _ 'http' ] ].
+					schemeName _ 'http']].
 
 	"remove leading // if it's there"
-	(remainder beginsWith: '//') ifTrue: [
-		remainder _ remainder copyFrom: 3 to: remainder size ].
+	(remainder beginsWith: '//') 
+		ifTrue: [remainder _ remainder copyFrom: 3 to: remainder size].
 
 
 	"get the query"
 	ind _ remainder indexOf: $?.
-	ind > 0 ifTrue: [
-		query _ (remainder copyFrom: ind+1 to: remainder size).
-		remainder _ remainder copyFrom: 1 to: ind-1 ].
+	ind > 0 
+		ifTrue: 
+			[query _ remainder copyFrom: ind + 1 to: remainder size.
+			remainder _ remainder copyFrom: 1 to: ind - 1].
 
 	"get the authority"
 	ind _ remainder indexOf: $/.
-	ind > 0 ifTrue: [
-		ind = 1 ifTrue: [ authority _ '' ] ifFalse: [
-			authority _ remainder copyFrom: 1 to: ind-1.
-			remainder _ remainder copyFrom: ind+1 to: remainder size. ] ]
-	ifFalse: [
-		authority _ remainder.
-		remainder _ ''. ].
+	ind > 0 
+		ifTrue: 
+			[ind = 1 
+				ifTrue: [authority _ '']
+				ifFalse: 
+					[authority _ remainder copyFrom: 1 to: ind - 1.
+					remainder _ remainder copyFrom: ind + 1 to: remainder size]]
+		ifFalse: 
+			[authority _ remainder.
+			remainder _ ''].
+
+	"extract the username+password"
+	(authority includes: $@) 
+		ifTrue: 
+			[username _ authority copyUpTo: $@.
+			authority _ authority copyFrom: (authority indexOf: $@) + 1
+						to: authority size.
+			(username includes: $:) 
+				ifTrue: 
+					[password _ username copyFrom: (username indexOf: $:) + 1 to: username size.
+					username _ username copyUpTo: $:]].
 
 	"Extract the port"
-	(authority includes: $:)
-		ifTrue: [
-			port _ (authority copyFrom: (authority indexOf: $:) + 1 to: authority size) asNumber.
-			authority _ authority copyUpTo: $:].
+	(authority includes: $:) 
+		ifTrue: 
+			[| lastColonIndex portString |
+			lastColonIndex _ authority findLast: [:c | c = $:].
+			portString _ authority copyFrom: lastColonIndex + 1 to: authority size.
+			portString isAllDigits 
+				ifTrue: 
+					[port _ Integer readFromString: portString.
+					(port > 65535) ifTrue: [self error: 'Invalid port number'].
+					 authority _ authority copyFrom: 1 to: lastColonIndex - 1]
+				ifFalse:[self error: 'Invalid port number']].
 
 	"get the path"
-	path _ OrderedCollection new.
-	s _ ReadStream on: remainder.
-	[ 
-		s peek = $/ ifTrue: [ s next ].
-		nextTok _ WriteStream on: String new.
-		[ s atEnd or: [ s peek = $/ ] ] whileFalse: [ nextTok nextPut: s next ].
-		nextTok _ nextTok contents unescapePercents.
-		nextTok = '..' 
-			ifTrue: [ path size > 0 ifTrue: [ path removeLast ] ]
-			ifFalse: [ nextTok ~= '.' ifTrue: [ path add: nextTok ] ].
-		s atEnd 
-	] whileFalse.
-	path isEmpty ifTrue: [ path add: '' ].
+	path _ self privateParsePath: remainder relativeTo: #() .
