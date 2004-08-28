3edff57b6cbfc93fad4f9c67fb1dd93616b55ab5
dgd <unknown>
2004-08-28 18:44:00 +0000

    no message

--- PasteUpMorph/instance/event handling/handlesKeyboard-3A-.cs
+++ PasteUpMorph/instance/event handling/handlesKeyboard-3A-.cs
@@ -1,2 +1,2 @@
 handlesKeyboard: evt
-	^self isWorldMorph or:[evt keyCharacter == Character tab and:[self hasProperty: #tabAmongFields]]
+	^self isWorldMorph or:[evt keyCharacter == Character tab and:[self tabAmongFields]]
