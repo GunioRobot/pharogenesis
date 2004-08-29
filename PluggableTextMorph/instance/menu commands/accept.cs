b94ad87cfc5189798a3249d69dc8f8b569204b8d
tween <unknown>
2004-08-29 20:28:00 +0000

    no message

--- PluggableTextMorph/instance/menu commands/accept.cs
+++ PluggableTextMorph/instance/menu commands/accept.cs
@@ -1,20 +1,39 @@
 accept 
 	"Inform the model of text to be accepted, and return true if OK."
 
-	| textToAccept ok |
-	self canDiscardEdits ifTrue: [^ self flash].
+	| ok saveSelection saveScrollerOffset |
+"sps 8/13/2001 22:41: save selection and scroll info"
+	saveSelection _ self selectionInterval copy.
+	saveScrollerOffset _ scroller offset copy.
+
+	(self canDiscardEdits and: [(self hasProperty: #alwaysAccept) not])
+		ifTrue: [^ self flash].
+
 	self hasEditingConflicts ifTrue:
 		[(self confirm: 
 'Caution! This method may have been
 changed elsewhere since you started
-editing it here.  Accept anyway?') ifFalse: [^ self flash]].
-	textToAccept _ textMorph asText.
-	ok _ (setTextSelector == nil) or:
-		[setTextSelector numArgs = 2
-			ifTrue: [model perform: setTextSelector with: textToAccept with: self]
-			ifFalse: [model perform: setTextSelector with: textToAccept]].
-	ok ifTrue:
+editing it here.  Accept anyway?' translated) ifFalse: [^ self flash]].
+	ok _ self acceptTextInModel.
+	ok==true ifTrue:
 		[self setText: self getText.
 		self hasUnacceptedEdits: false.
-		(model dependents detect: [:dep | (dep isKindOf: PluggableTextMorph) and: [dep getTextSelector == #annotation]] ifNone: [nil]) doIfNotNil:
-			[:aPane | model changed: #annotation]]
+		(model dependents detect: [:dep | (dep isKindOf: PluggableTextMorph) and: [dep getTextSelector == #annotation]] ifNone: [nil]) ifNotNilDo:
+			[:aPane | model changed: #annotation]].
+
+	"sps 8/13/2001 22:41: restore selection and scroll info"
+	["During the step for the browser, updateCodePaneIfNeeded is called, and 
+		invariably resets the contents of the codeholding PluggableTextMorph
+		at that time, resetting the cursor position and scroller in the process.
+		The following line forces that update without waiting for the step, 		then restores the cursor and scrollbar"
+
+	ok ifTrue: "(don't bother if there was an error during compile)"
+		[(model isKindOf: CodeHolder) 
+			ifTrue: [model updateCodePaneIfNeeded].
+		WorldState addDeferredUIMessage:
+			[self currentHand newKeyboardFocus: textMorph.
+			scroller offset: saveScrollerOffset.
+			self setScrollDeltas.
+			self selectFrom: saveSelection first to: saveSelection last]]]
+
+			on: Error do: []
