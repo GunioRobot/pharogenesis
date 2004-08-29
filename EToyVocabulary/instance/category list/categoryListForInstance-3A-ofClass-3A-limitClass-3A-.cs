598e643175ec2124e661595738a56e02e16c42ce
nk <unknown>
2004-08-29 17:17:00 +0000

    no message

--- EToyVocabulary/instance/category list/categoryListForInstance-3A-ofClass-3A-limitClass-3A-.cs
+++ EToyVocabulary/instance/category list/categoryListForInstance-3A-ofClass-3A-limitClass-3A-.cs
@@ -1,12 +1,15 @@
 categoryListForInstance: anObject ofClass: aClass limitClass: mostGenericClass
 	"Answer the category list for the given object, considering only code implemented in aClass and lower"
 
-	^ (anObject isKindOf: Player)
+	^ (anObject isPlayerLike)
 		ifTrue:
-			[(mostGenericClass == aClass)
+			[self flag: #deferred.  "The bit commented out on next line is desirable but not yet workable, because it delivers categories that are not relevant to the costume in question"
+			"#(scripts #'instance variables'), (super categoryListForInstance: anObject ofClass: aClass limitClass: mostGenericClass)]"
+
+			self translatedWordingsFor: ((mostGenericClass == aClass)
 				ifFalse:
-					[anObject categories]
+					[anObject categoriesForVocabulary: self]
 				ifTrue:
-					[#(scripts #'instance variables')]]
+					[{ScriptingSystem nameForScriptsCategory.  ScriptingSystem nameForInstanceVariablesCategory}])]
 		ifFalse:
 			[super categoryListForInstance: anObject ofClass: aClass limitClass: mostGenericClass]
