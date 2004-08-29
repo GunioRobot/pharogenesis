b39e0f1967d1a42ea7c09f3a4cb6cb660d4f7160
nk <unknown>
2004-08-29 17:21:00 +0000

    no message

--- StandardViewer/instance/categories/likelyCategoryToShow.cs
+++ StandardViewer/instance/categories/likelyCategoryToShow.cs
@@ -1,21 +1,27 @@
 likelyCategoryToShow
 	"Choose a category to show based on what's already showing and on some predefined heuristics"
 
-	| possible |
-	possible _ (scriptedPlayer categoriesForViewer: self) asOrderedCollection.
-	self categoryMorphs do:
-		[:m | possible remove: m currentCategory ifAbsent: []].
+	| possible all aCat currVocab |
+	all := (scriptedPlayer categoriesForViewer: self) asOrderedCollection.
+	possible := all copy.
+	currVocab := self currentVocabulary.
+	self categoryMorphs do: 
+			[:m | 
+			aCat := currVocab categoryWhoseTranslatedWordingIs: m currentCategory.
+			aCat ifNotNil: [possible remove: aCat wording ifAbsent: []]].
+	(currVocab isEToyVocabulary) 
+		ifTrue: 
+			["hateful!"
 
-	((possible includes: #'instance variables') and: [scriptedPlayer hasUserDefinedSlots])
-		ifTrue:	[^ #'instance variables'].
-
-	((possible includes: #scripts) and: [scriptedPlayer hasUserDefinedScripts])
-		ifTrue:	[^ #'scripts'].
-
-	#(basic #'color & border') do:
-		[:preferred | (possible includes: preferred) ifTrue: [^ preferred]].
-	^ possible size > 0
-		ifTrue:
-			[possible first]
-		ifFalse:
-			[#basic]
+			((possible includes: ScriptingSystem nameForInstanceVariablesCategory translated) 
+				and: [scriptedPlayer hasUserDefinedSlots]) ifTrue: [^ ScriptingSystem nameForInstanceVariablesCategory].
+			((possible includes: ScriptingSystem nameForScriptsCategory translated) and: [scriptedPlayer hasUserDefinedScripts]) 
+				ifTrue: [^ ScriptingSystem nameForScriptsCategory]].
+	{#basic translated} 
+		do: [:preferred | (possible includes: preferred) ifTrue: [^preferred]].
+	((scriptedPlayer isPlayerLike) 
+		and: [scriptedPlayer hasOnlySketchCostumes]) 
+			ifTrue: [(possible includes: #tests translated) ifTrue: [^#tests translated]].
+	{#'color & border' translated. #tests translated. #color translated. #flagging translated. #comparing translated.} 
+		do: [:preferred | (possible includes: preferred) ifTrue: [^preferred]].
+	^possible isEmpty ifFalse: [possible first] ifTrue: [all first]
