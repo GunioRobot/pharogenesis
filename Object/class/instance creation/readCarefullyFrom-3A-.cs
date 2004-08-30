bf4d00c22dfda6128bee817612c5954cd23d4aca
nk <unknown>
2004-08-30 07:57:00 +0000

    no message

--- Object/class/instance creation/readCarefullyFrom-3A-.cs
+++ Object/class/instance creation/readCarefullyFrom-3A-.cs
@@ -1,10 +1,10 @@
-readCarefullyFrom: textOrString
-	"Create an object based on the contents of textOrString.  Return an error instead of putting up a SyntaxError window."
+readCarefullyFrom: textStringOrStream
+	"Create an object based on the contents of textStringOrStream.  Return an error instead of putting up a SyntaxError window."
 
-	| object ok |
-	ok _ (textOrString isKindOf: Stream) or: [textOrString isKindOf: String].
-	(ok or: [textOrString isKindOf: Text]) ifFalse: [^ self error: 'expected String or Text'].
-	object _ Compiler evaluate: textOrString for: nil 
+	| object |
+	(Compiler couldEvaluate: textStringOrStream)
+		ifFalse: [^ self error: 'expected String, Stream, or Text'].
+	object _ Compiler evaluate: textStringOrStream for: nil 
 				notifying: #error: "signal we want errors" logged: false.
 	(object isKindOf: self) ifFalse: [self error: self name, ' expected'].
 	^object
