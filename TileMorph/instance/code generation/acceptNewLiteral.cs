93903cf6fe4e32902618c3b91e449273ea82a2a6
sw <unknown>
2004-08-30 16:34:00 +0000

    no message

--- TileMorph/instance/code generation/acceptNewLiteral.cs
+++ TileMorph/instance/code generation/acceptNewLiteral.cs
@@ -1,6 +1,14 @@
 acceptNewLiteral
 	"Tell the scriptEditor who I belong to that I have a new literal value."
+
 	| topScript |
 	topScript _ self outermostMorphThat:
 		[:m | m isKindOf: ScriptEditorMorph].
-	topScript ifNotNil: [topScript installWithNewLiteral]
+	topScript ifNotNil: [topScript installWithNewLiteral].
+	(self ownerThatIsA: ViewerLine) ifNotNilDo:
+		[:aLine |
+			(self ownerThatIsA: PhraseTileMorph) ifNotNil:
+				[aLine removeHighlightFeedback.
+				self layoutChanged.
+				ActiveWorld doOneSubCycle.
+				aLine addCommandFeedback]]
