b01d70e6faa79694e5fd3abd286bb98af41e7e3b
nk <unknown>
2004-08-30 07:57:00 +0000

    no message

--- Object/class/instance creation/readFrom-3A-.cs
+++ Object/class/instance creation/readFrom-3A-.cs
@@ -1,9 +1,9 @@
-readFrom: aStream
-	"Create an object based on the contents of aStream."
+readFrom: textStringOrStream
+	"Create an object based on the contents of textStringOrStream."
 
-	| object ok |
-	ok _ (aStream isKindOf: Stream) or: [aStream isKindOf: String].
-	(ok or: [aStream isKindOf: Text]) ifFalse: [^ self error: 'expected String or Text'].
-	object _ Compiler evaluate: aStream.
+	| object |
+	(Compiler couldEvaluate: textStringOrStream)
+		ifFalse: [^ self error: 'expected String, Stream, or Text'].
+	object _ Compiler evaluate: textStringOrStream.
 	(object isKindOf: self) ifFalse: [self error: self name, ' expected'].
 	^object
