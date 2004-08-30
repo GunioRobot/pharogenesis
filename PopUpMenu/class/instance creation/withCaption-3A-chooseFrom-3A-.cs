5bc90c0f40bc439492d72f4cf5efd7fe5079823b
nk <unknown>
2004-08-30 07:59:00 +0000

    no message

--- PopUpMenu/class/instance creation/withCaption-3A-chooseFrom-3A-.cs
+++ PopUpMenu/class/instance creation/withCaption-3A-chooseFrom-3A-.cs
@@ -1,6 +1,9 @@
-withCaption: cap chooseFrom: labels
-	"Simply put up a menu.  Get the args in the right order with the caption first.  labels may be either an array of items or a string with CRs in it.  May use backslashes for returns."
+withCaption: cap chooseFrom: labels 
+	"Simply put up a menu. Get the args in the right order with the caption 
+	first. labels may be either an array of items or a string with CRs in it. 
+	May use backslashes for returns."
 
-	(labels isKindOf: String) 
-		ifTrue: [^ (self labels: labels withCRs lines: nil) startUpWithCaption: cap withCRs]
-		ifFalse: [^ (self labelArray: labels lines: nil) startUpWithCaption: cap withCRs]
+	^ (labels isString
+		ifTrue: [self labels: labels withCRs lines: nil]
+		ifFalse: [self labelArray: labels lines: nil])
+		startUpWithCaption: cap withCRs
