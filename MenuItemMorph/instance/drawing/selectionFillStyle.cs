dfda14b7796f1abc84c6804b6353964ae0567568
dgd <unknown>
2004-08-30 20:59:00 +0000

    no message

---
+++ MenuItemMorph/instance/drawing/selectionFillStyle.cs
@@ -0,0 +1,17 @@
+selectionFillStyle
+	"answer the fill style to use with the receiver is the selected  
+	element"
+	| fill baseColor preferenced |
+	Display depth <= 2
+		ifTrue: [^ Color gray].
+
+	preferenced := Preferences menuSelectionColor.
+	preferenced notNil ifTrue:[^ preferenced].
+
+	baseColor := owner color negated.
+	Preferences gradientMenu
+		ifFalse: [^ baseColor].
+	fill := GradientFillStyle ramp: {0.0 -> baseColor twiceLighter . 1 -> baseColor twiceDarker}.
+	fill origin: self topLeft.
+	fill direction: self width @ 0.
+	^ fill
