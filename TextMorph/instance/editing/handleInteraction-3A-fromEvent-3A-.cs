fd0c42956c8e80a703400736fd0ce3c148c57365
nk <unknown>
2004-08-31 15:31:00 +0000

    no message

--- TextMorph/instance/editing/handleInteraction-3A-fromEvent-3A-.cs
+++ TextMorph/instance/editing/handleInteraction-3A-fromEvent-3A-.cs
@@ -1,16 +1,22 @@
 handleInteraction: interactionBlock fromEvent: evt
-        | oldEditor oldParagraph |
-        "Perform the changes in interactionBlock, noting any change in selection"
+	"Perform the changes in interactionBlock, noting any change in selection
+	and possibly a change in the size of the paragraph (ar 9/22/2001 - added for TextPrintIts)"
+	"Also couple ParagraphEditor to Morphic keyboard events"
+	| oldEditor oldParagraph oldText |
+	self editor sensor: (KeyboardBuffer new startingEvent: evt).
+	oldEditor _ editor.
+	oldParagraph _ paragraph.
+	oldText _ oldParagraph text copy.
 
-        "Also couple ParagraphEditor to Morphic keyboard events"
-        self editor sensor: (KeyboardBuffer new startingEvent: evt).
-        oldEditor _ editor.
-        oldParagraph _ paragraph.
-        self selectionChanged.  "Note old selection"
+	self selectionChanged.  "Note old selection"
 
-                interactionBlock value.
+		interactionBlock value.
 
-        oldParagraph == paragraph ifTrue: [     "this will not work if the paragraph changed"
-                editor _ oldEditor.     "since it may have been changed while in block"
-        ].
-        self selectionChanged.  "Note new selection"
+	(oldParagraph == paragraph) ifTrue:[
+		"this will not work if the paragraph changed"
+		editor _ oldEditor.     "since it may have been changed while in block"
+	].
+	self selectionChanged.  "Note new selection"
+	(oldText = paragraph text and: [ oldText runs = paragraph text runs ])
+		ifFalse:[ self updateFromParagraph ].
+	self setCompositionWindow.
