10ab9ddadb1fb696d6c8da7ffe8eeafcd6111599
nk <unknown>
2004-08-31 09:23:00 +0000

    no message

--- FontSet/class/installing/size-3A-fromLiteral-3A-.cs
+++ FontSet/class/installing/size-3A-fromLiteral-3A-.cs
@@ -1,4 +1,4 @@
-size: pointSize fromLiteral: aString
+size: pointSize fromLiteral: aString 
 	"This method allows a font set to be captured as sourcecode in a subclass.
 	The string literals will presumably be created by printing, eg,
 		(FileStream readOnlyFileNamed: 'Palatino24.sf2') contentsOfEntireFile,
@@ -8,13 +8,16 @@ size24
 	'--unreadable binary data--'
 
 	See the method installAsTextStyle to see how this can be used."
+
 	"This method is old and for backward compatibility only.
 	please use fontNamed:fromLiteral: instead."
 
-	self flag: #bob.		"used in Alan's projects"
-
-	^ StrikeFont new 
-		name: self fontName , (pointSize < 10
-			ifTrue: ['0' , pointSize printString]
-			ifFalse: [pointSize printString]);
-		readFromStrike2Stream: ((RWBinaryOrTextStream with: aString) reset; binary)
+	self flag: #bob.	"used in Alan's projects"
+	^(StrikeFont new)
+		name: self fontName , (pointSize < 10 
+							ifTrue: ['0' , pointSize printString]
+							ifFalse: [pointSize printString]);
+		readFromStrike2Stream: ((RWBinaryOrTextStream with: aString)
+					reset;
+					binary);
+		yourself
