e59920f55457eb07c6891cbff3c913875482d3ba
sw <unknown>
2004-08-31 14:05:00 +0000

    no message

--- ScriptEditorMorph/instance/buttons/addDismissButtonTo-3A-.cs
+++ ScriptEditorMorph/instance/buttons/addDismissButtonTo-3A-.cs
@@ -1,12 +1,13 @@
 addDismissButtonTo: aRowMorph
-	aRowMorph addMorphBack:
-		((SimpleButtonMorph new label: 'O' font: Preferences standardButtonFont)
-			target: self;
-			color:  Color tan;
-			actionSelector: #dismiss;
+	"Add the brown dismiss button to the header"
+
+	| aButton |
+	aButton _ self tanOButton.
+	aRowMorph addMorphBack: aButton.
+	aButton actionSelector: #dismiss;
 			setBalloonText: 
 'Remove this script
 from the screen
 (you can open it
-again from a Viewer)').
+again from a Viewer)' translated.
 	^ aRowMorph
