b7c7f3a6d9156e79612ba47b231bef4de4c8dbe0
sw <unknown>
2004-08-31 14:01:00 +0000

    no message

--- CategoryViewer/instance/header pane/addHeaderMorph.cs
+++ CategoryViewer/instance/header pane/addHeaderMorph.cs
@@ -1,36 +1,15 @@
 addHeaderMorph
 	"Add the header at the top of the viewer, with a control for choosing the category, etc."
 
-	| header aFont aButton wrpr |
+	| header aButton |
 	header _ AlignmentMorph newRow color: self color; wrapCentering: #center; cellPositioning: #leftCenter.
-	aFont _ Preferences standardButtonFont.
-	header addMorph: (aButton _ SimpleButtonMorph new label: 'O' font: aFont).
-	aButton target: self;
-			color:  Color tan;
-			actionSelector: #delete;
-			setBalloonText: 'remove this pane from the screen
-don''t worry -- nothing will be lost!.'.
-	header addTransparentSpacerOfSize: 5@5.
-
-	header addUpDownArrowsFor: self.
-	(wrpr _ header submorphs last) submorphs second setBalloonText: 'previous category'.	
-	wrpr submorphs first  setBalloonText: 'next category'.
+	aButton _ self tanOButton.
+	header addMorph: aButton.
+	aButton actionSelector: #delete;
+		setBalloonText: 'remove this pane from the screen
+don''t worry -- nothing will be lost!.' translated.
+	self maybeAddArrowsTo: header.
 	header beSticky.
 	self addMorph: header.
-
-	namePane _ RectangleMorph newSticky color: Color brown veryMuchLighter.
-	namePane borderWidth: 0.
-	aButton _ (StringButtonMorph contents: '-----' font: (StrikeFont familyName: #NewYork size: 12)) color: Color black.
-	aButton target: self; arguments: Array new; actionSelector: #chooseCategory.
-	aButton actWhen: #buttonDown.
-	namePane addMorph: aButton.
-	aButton position: namePane position.
-	namePane align: namePane topLeft with: (bounds topLeft + (50 @ 0)).
-	namePane setBalloonText: 'category (click here to choose a different one)'.
-
-	header addMorphBack: namePane.
-	(namePane isKindOf: RectangleMorph) ifTrue:
-		[namePane addDropShadow.
-		namePane shadowColor: Color gray].
-
-	self categoryChoice: #basic
+	self addNamePaneTo: header.
+	chosenCategorySymbol _ #basic
