f36e7a65757bbe90c40bb0647e0ef8377407deb5
nk <unknown>
2004-08-31 08:59:00 +0000

    no message

---
+++ ChangeList/class/fileIn-2F-Out/browseCompressedChangesFile-3A-.cs
@@ -0,0 +1,11 @@
+browseCompressedChangesFile: fullName 
+	"Browse the selected file in fileIn format."
+
+	| zipped unzipped stream |
+	fullName ifNil: [^Beeper beep].
+	stream := FileStream readOnlyFileNamed: fullName.
+	stream converter: Latin1TextConverter new.
+	zipped := GZipReadStream on: stream.
+	unzipped := zipped contents asString.
+	stream := (MultiByteBinaryOrTextStream with: unzipped) reset.
+	ChangeList browseStream: stream
