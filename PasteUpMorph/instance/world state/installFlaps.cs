60f372acecf4086f4c6b3aefdf620ca3d8e9c630
dgd <unknown>
2004-08-31 16:25:00 +0000

    no message

--- PasteUpMorph/instance/world state/installFlaps.cs
+++ PasteUpMorph/instance/world state/installFlaps.cs
@@ -1,6 +1,9 @@
 installFlaps
+	"Get flaps installed within the bounds of the receiver"
+
+	Project current assureFlapIntegrity.
 	self addGlobalFlaps.
 	self localFlapTabs do:
 			[:aFlapTab | aFlapTab adaptToWorld].
 	self assureFlapTabsFitOnScreen.
-	self bringFlapTabsToFront
+	self bringTopmostsToFront
