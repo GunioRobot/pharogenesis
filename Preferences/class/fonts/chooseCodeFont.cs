7b37cf7a884e585594c4ecc569d77dd889151bf8
nk <unknown>
2004-09-01 10:48:00 +0000

    no message

--- Preferences/class/fonts/chooseCodeFont.cs
+++ Preferences/class/fonts/chooseCodeFont.cs
@@ -1,4 +1,4 @@
 chooseCodeFont
 	"Not currently sent, but once protocols are sorted out so that we can disriminate on whether a text object being launched is for code or not, will be reincorporated"
 
-	self chooseFontWithPrompt: 'standard code font' andSendTo: self withSelector: #setCodeFontTo:.
+	self chooseFontWithPrompt: 'Choose the font to be used for displaying code' translated andSendTo: self withSelector: #setCodeFontTo: highlight: self standardCodeFont.
