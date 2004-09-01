cb7bdd0831222f8baf42ac1176476c23311d5fb8
nk <unknown>
2004-09-01 11:37:00 +0000

    no message

---
+++ Preferences/class/fonts/printStandardSystemFonts.cs
@@ -0,0 +1,25 @@
+printStandardSystemFonts
+	"self printStandardSystemFonts"
+
+	| string |
+	string := String streamContents: [ :s |
+
+	#(standardDefaultTextFont standardListFont standardFlapFont 
+	standardEToysFont standardMenuFont windowTitleFont 
+	standardBalloonHelpFont standardCodeFont standardButtonFont) do: [:selector |
+		| font |
+		font _ Preferences perform: selector.
+		s
+			nextPutAll: selector; space;
+			nextPutAll: font familyName; space;
+			nextPutAll: (AbstractFont emphasisStringFor: font emphasis);
+			nextPutAll: ' points: ';
+			print: font pointSize;
+			nextPutAll: ' height: ';
+			print: font height;
+			cr
+		]].
+
+	(StringHolder new)
+		contents: string;
+		openLabel: 'Current system font settings' translated.
