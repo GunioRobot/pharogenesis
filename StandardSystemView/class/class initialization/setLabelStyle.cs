64cabe0ca53bb27ae9a9eb81050b5e11359795dc
nk <unknown>
2004-09-01 10:26:00 +0000

    no message

--- StandardSystemView/class/class initialization/setLabelStyle.cs
+++ StandardSystemView/class/class initialization/setLabelStyle.cs
@@ -2,5 +2,5 @@ setLabelStyle
 	| aFont |
 	"StandardSystemView setLabelStyle"
 	aFont _ Preferences windowTitleFont.
-	LabelStyle _ aFont textStyle copy consistOnlyOf: aFont.
+	LabelStyle _ TextStyle fontArray: { aFont }.
 	LabelStyle gridForFont: 1 withLead: 0
