dc397d92338ac6a7a064e653bc38f9dee52b7a66
nk <unknown>
2004-09-01 10:48:00 +0000

    no message

---
+++ PluggableListMorph/instance/menus/setListFont.cs
@@ -0,0 +1,4 @@
+setListFont
+	"set the font for the list"
+
+	Preferences chooseFontWithPrompt: 'Choose the font for this list' translated andSendTo: self withSelector: #font: highlight: self listMorph font
