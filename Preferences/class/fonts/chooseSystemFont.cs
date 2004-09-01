0b7b222ea384152703ad08bcca237a7d6c43546a
nk <unknown>
2004-09-01 10:49:00 +0000

    no message

--- Preferences/class/fonts/chooseSystemFont.cs
+++ Preferences/class/fonts/chooseSystemFont.cs
@@ -1,2 +1,2 @@
 chooseSystemFont
-	self chooseFontWithPrompt: 'Default text font' andSendTo: self withSelector: #setSystemFontTo:
+	self chooseFontWithPrompt: 'Choose the default text font' translated andSendTo: self withSelector: #setSystemFontTo: highlight: (TextConstants at: #DefaultTextStyle) defaultFont
