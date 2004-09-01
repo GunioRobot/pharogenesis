087452fb2b804ee4511a18f7fc76f4867ff93bf4
nk <unknown>
2004-09-01 10:27:00 +0000

    no message

--- PopUpMenu/class/class initialization/setMenuFontTo-3A-.cs
+++ PopUpMenu/class/class initialization/setMenuFontTo-3A-.cs
@@ -1,7 +1,8 @@
 setMenuFontTo: aFont
-	MenuStyle _ aFont textStyle copy consistOnlyOf: aFont.
+	"Set the menu font as indicated"
+
+	MenuStyle _ TextStyle fontArray: { aFont }.
 	MenuStyle 
 		gridForFont: 1 withLead: 0;
 		centered.
-	self allSubInstancesDo: [:m | m rescan].
-	Utilities replaceMenuFlap
+	self allSubInstancesDo: [:m | m rescan]
