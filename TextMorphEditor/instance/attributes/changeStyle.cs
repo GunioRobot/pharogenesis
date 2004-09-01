7201b63923d06890f36b6e5a9f849d19ab017961
nk <unknown>
2004-09-01 14:38:00 +0000

    no message

--- TextMorphEditor/instance/attributes/changeStyle.cs
+++ TextMorphEditor/instance/attributes/changeStyle.cs
@@ -1,8 +1,10 @@
 changeStyle
 	"Let user change styles for the current text pane."
-	| aList reply style theStyle menuList |
+	| aList reply style theStyle menuList startIndex stopIndex |
 	self flag: #arNote. "Move this up once we get rid of MVC"
-	aList _ StrikeFont familyNames remove: 'DefaultTextStyle' ifAbsent: []; asOrderedCollection.
+	startIndex _ self startIndex.
+	stopIndex := self stopIndex-1 min: paragraph text size.
+	aList _ StrikeFont actualFamilyNames.
 	theStyle _ paragraph textStyle.
 	menuList _ aList collect:[:styleName|
 		"Hack! use defaultFont for comparison - we have no name that we could use for compare and the style changes with alignment so they're no longer equal."
@@ -13,11 +15,12 @@ changeStyle
 		ifTrue:[menuList addFirst: '<on>DefaultTextStyle']
 		ifFalse:[menuList addFirst: '<off>DefaultTextStyle'].
 	aList addFirst: 'DefaultTextStyle'.
-	reply _ (SelectionMenu labelList: menuList lines: #(1) selections: aList) startUp.
-	reply ~~ nil ifTrue:
-		[(style _ TextStyle named: reply) ifNil: [self beep. ^ true].
+	reply _ (SelectionMenu labelList: menuList lines: #(1) selections: aList) startUpWithCaption: nil at: ActiveHand position allowKeyboard: false.
+	reply ifNotNil:
+		[(style _ TextStyle named: reply) ifNil: [Beeper beep. ^ true].
 		paragraph textStyle: style copy.
 		paragraph composeAll.
 		self recomputeSelection.
 		self mvcRedisplay].
+	morph selectFrom: startIndex to: stopIndex.
 	^ true
