5df68e65493b9f1464398581b743149eb268e971
kfr <unknown>
2004-09-03 19:27:00 +0000

    no message

--- SystemWindow/instance/panes/addMorph-3A-fullFrame-3A-.cs
+++ SystemWindow/instance/panes/addMorph-3A-fullFrame-3A-.cs
@@ -3,5 +3,13 @@ addMorph: aMorph fullFrame: aLayoutFrame
 	super addMorph: aMorph fullFrame: aLayoutFrame.
 
 	paneMorphs _ paneMorphs copyReplaceFrom: 1 to: 0 with: (Array with: aMorph).
-	aMorph borderWidth: 1.
-	aMorph color: self paneColor.
+	Preferences alternativeWindowLook
+		ifFalse:
+			[aMorph borderWidth: 1.
+			aMorph color: self paneColor]
+		ifTrue:
+		    [(aMorph isKindOf: ImageMorph) ifFalse:[
+			aMorph adoptPaneColor: self paneColor.
+			aMorph borderWidth: 2; borderColor: #inset; color: Color transparent]].
+	Preferences scrollBarsOnRight	"reorder panes so flop-out right-side scrollbar is visible"
+		ifTrue: [self addMorphBack: aMorph]
