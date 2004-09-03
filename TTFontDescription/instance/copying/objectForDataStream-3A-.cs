d7b28408c83433e9024e1d7fa95f582b7c9f8127
nk <unknown>
2004-09-03 14:48:00 +0000

    no message

---
+++ TTFontDescription/instance/copying/objectForDataStream-3A-.cs
@@ -0,0 +1,12 @@
+objectForDataStream: refStrm
+	| dp |
+	"I am about to be written on an object file.  Write a reference to a known Font in the other system instead.  "
+
+	"A path to me"
+	(TextConstants at: #forceFontWriting ifAbsent: [false]) ifTrue: [^ self].
+		"special case for saving the default fonts on the disk.  See collectionFromFileNamed:"
+
+	dp _ DiskProxy global: #TTFontDescription selector: #descriptionFullNamed:
+			args: {self fullName}.
+	refStrm replace: self with: dp.
+	^ dp.
