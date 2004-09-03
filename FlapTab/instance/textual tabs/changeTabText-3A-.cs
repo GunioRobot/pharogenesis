658cbab6e5a8741c726bab7828b818a8fafd7028
ar <unknown>
2004-09-03 14:58:00 +0000

    no message

---
+++ FlapTab/instance/textual tabs/changeTabText-3A-.cs
@@ -0,0 +1,12 @@
+changeTabText: aString 
+
+	| label |
+	aString isEmptyOrNil ifTrue: [^ self].
+	label _ Locale current languageEnvironment class flapTabTextFor: aString in: self.
+	label isEmptyOrNil ifTrue: [^ self].
+	self useStringTab: label.
+	submorphs first delete.
+	self assumeString: label
+		font: Preferences standardFlapFont
+		orientation: (Flaps orientationForEdge: self edgeToAdhereTo)
+		color: nil.
