23057f737bf82a0316295de6300d758b6970f149
nk <unknown>
2004-09-04 17:04:00 +0000

    no message

--- Player/instance/costume/hasOnlySketchCostumes.cs
+++ Player/instance/costume/hasOnlySketchCostumes.cs
@@ -1,7 +1,5 @@
 hasOnlySketchCostumes
 	"Answer true if the only costumes assocaited with this Player are SketchMorph costumes"
 
-	(costume renderedMorph isKindOf: SketchMorph) ifFalse: [^ false].
-	costumes ifNotNil: [costumes do:
-		[:cost | (cost isKindOf: SketchMorph) ifFalse: [^ false]]].
+	self costumesDo: [ :aCostume | aCostume isSketchMorph ifFalse: [^ false]].
 	^ true
