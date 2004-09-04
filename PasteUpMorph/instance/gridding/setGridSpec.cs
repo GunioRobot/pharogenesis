77ab3ca7500968d860d14beff4d167408e5cc365
kfr <unknown>
2004-09-04 15:44:00 +0000

    no message

--- PasteUpMorph/instance/gridding/setGridSpec.cs
+++ PasteUpMorph/instance/gridding/setGridSpec.cs
@@ -1,24 +1,21 @@
 setGridSpec
 	"Gridding rectangle provides origin and modulus"
-	| response result mx |
-	mx _ 36.  "... because it was that way before ..."
+	| response result |
 	response _ FillInTheBlank
-			request: 'New grid origin (usually 0@0):'
+			request: 'New grid origin (usually 0@0):' translated
 			initialAnswer: self gridOrigin printString.
 	response isEmpty ifTrue: [^ self].
 	result _ [Compiler evaluate: response] ifError: [^ self].
-	(result isPoint and: [(result >= (0@0)) & (result < (mx @ mx))])
+	(result isPoint and: [(result >= (0@0))])
 		ifTrue: [self gridOrigin: result]
-		ifFalse: [self inform: 'Must be a Point with coordinates between 0 & '
-								, (mx-1) printString].
+		ifFalse: [self inform: ('Must be a Point with coordinates (for example 10@10)' translated )].
 
 	response _ FillInTheBlank
-			request: 'New grid spacing:'
+			request: 'New grid spacing:' translated
 			initialAnswer: self gridModulus printString.
 	response isEmpty ifTrue: [^ self].
 	result _ [Compiler evaluate: response] ifError: [^ self].
-	(result isPoint and: [(result > (0@0)) & (result <= (mx @ mx))])
+	(result isPoint and: [(result > (0@0)) ])
 		ifTrue: [self gridModulus: result]
-		ifFalse: [self inform: 'Must be a Point with coordinates between 1 & '
-								, mx printString].
+		ifFalse: [self inform: ('Must be a Point with coordinates (for example 10@10)' translated )].
 
