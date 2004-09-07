1072a9160154c504926a5b04da1acfc888fd2348
nk <unknown>
2004-09-07 11:44:00 +0000

    no message

--- RectangleMorph/class/as yet unclassified/roundRectPrototype.cs
+++ RectangleMorph/class/as yet unclassified/roundRectPrototype.cs
@@ -1,2 +1,5 @@
 roundRectPrototype
-	^ self authoringPrototype useRoundedCorners color: Color green; setNameTo: 'RoundRect'
+	^ self authoringPrototype useRoundedCorners 
+		color: ((Color r: 1.0 g: 0.3 b: 0.6) alpha: 0.5); 
+		borderWidth: 1;
+		setNameTo: 'RoundRect'
