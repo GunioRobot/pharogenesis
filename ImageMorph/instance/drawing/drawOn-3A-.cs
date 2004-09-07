7d031347227dddbe97a30faf1b51834b5d3e2adc
dgd <unknown>
2004-09-07 17:24:00 +0000

    no message

--- ImageMorph/instance/drawing/drawOn-3A-.cs
+++ ImageMorph/instance/drawing/drawOn-3A-.cs
@@ -1,4 +1,8 @@
 drawOn: aCanvas
+	| style |
+	(style _ self borderStyle) ifNotNil:[
+		style frameRectangle: bounds on: aCanvas.
+	].
 	self isOpaque
-		ifTrue:[aCanvas drawImage: image at: bounds origin]
-		ifFalse:[aCanvas paintImage: image at: bounds origin]
+		ifTrue:[aCanvas drawImage: image at: self innerBounds origin]
+		ifFalse:[aCanvas translucentImage: image at: self innerBounds origin]
