76f6306b90acf0aa7b8e518fe9e939cf4c6266b2
dgd <unknown>
2004-09-11 20:56:00 +0000

    no message

--- PasteUpMorph/instance/event handling/mouseDown-3A-.cs
+++ PasteUpMorph/instance/event handling/mouseDown-3A-.cs
@@ -1,6 +1,7 @@
 mouseDown: evt
 	"Handle a mouse down event."
 	| grabbedMorph handHadHalos |
+
 	grabbedMorph _ self morphToGrab: evt.
 	grabbedMorph ifNotNil:[
 		grabbedMorph isSticky ifTrue:[^self].
@@ -10,23 +11,54 @@ mouseDown: evt
 		(grabbedMorph fullBounds containsPoint: evt position) 
 			ifFalse:[grabbedMorph position: evt position].
 		"Note: grabbedMorph is ownerless after duplicate so use #grabMorph:from: instead"
-		^evt hand grabMorph: grabbedMorph from: self].
+		^ evt hand grabMorph: grabbedMorph from: self].
 
 	(super handlesMouseDown: evt)
 		ifTrue:[^super mouseDown: evt].
+
 	handHadHalos _ evt hand halo notNil.
-	evt hand halo: nil. "shake off halos"
+
+	evt hand removeHalo. "shake off halos"
 	evt hand releaseKeyboardFocus. "shake of keyboard foci"
-	evt shiftPressed ifTrue:[
-		^evt hand 
-			waitForClicksOrDrag: self 
-			event: evt 
-			selectors: { #findWindow:. nil. #dragThroughOnDesktop:}
-			threshold: 5].
-	self isWorldMorph ifTrue: [
-		handHadHalos ifTrue: [^self addAlarm: #invokeWorldMenu: with: evt after: 200].
-		^self invokeWorldMenu: evt
+
+	(evt shiftPressed not
+			and:[ self isWorldMorph not ]
+			and:[ Preferences easySelection not ])
+	ifTrue:[
+		"explicitly ignore the event if we're not the world and we'll not select,
+		so that we could be picked up if need be"
+		evt wasHandled: false.
+		^ self.
 	].
-	"otherwise, explicitly ignore the event if we're not the world,
-	so that we could be picked up if need be"
-	self isWorldMorph ifFalse:[evt wasHandled: false].
+
+	( evt shiftPressed or: [ Preferences easySelection ] ) ifTrue:[
+		"We'll select on drag, let's decide what to do on click"
+		| clickSelector |
+
+		clickSelector := nil.
+
+		evt shiftPressed ifTrue:[
+			clickSelector := #findWindow:.
+		]
+		ifFalse:[
+			self isWorldMorph ifTrue:[
+				clickSelector := handHadHalos
+										ifTrue: [ #delayedInvokeWorldMenu: ]
+										ifFalse: [ #invokeWorldMenu: ]
+			]
+		].
+
+		evt hand 
+				waitForClicksOrDrag: self 
+				event: evt 
+				selectors: { clickSelector. nil. nil. #dragThroughOnDesktop: }
+				threshold: 5.
+	]
+	ifFalse:[
+		"We wont select, just bring world menu if I'm the world"
+		self isWorldMorph ifTrue:[
+			handHadHalos
+				ifTrue: [ self delayedInvokeWorldMenu: evt ]
+				ifFalse: [ self invokeWorldMenu: evt ]
+		]
+	].
