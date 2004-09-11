aca5751e62b416798c11f3dcd219c858a3c97874
avi <unknown>
2004-09-11 10:44:00 +0000

    no message

---
+++ MCVersionInfo/instance/initialize-2D-release/initializeWithName-3A-id-3A-message-3A-date-3A-time-3A-author-3A-ancestors-3A-stepChildren-3A-.cs
@@ -0,0 +1,9 @@
+initializeWithName: vName id: aUUID message: aString date: aDate time: aTime author: initials ancestors: aCollection stepChildren: stepCollection
+	name _ vName.
+	id _ aUUID.
+	message _ aString.
+	date _ aDate.
+	time _ aTime.
+	author _ initials.
+	ancestors _  aCollection.
+	stepChildren _ stepCollection
