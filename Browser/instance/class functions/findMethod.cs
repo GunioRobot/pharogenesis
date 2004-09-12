06ae176b67076caffad7ff579b656f9831f8f887
jon <unknown>
2004-09-12 15:29:00 +0000

    no message

--- Browser/instance/class functions/findMethod.cs
+++ Browser/instance/class functions/findMethod.cs
@@ -1,16 +1,36 @@
 findMethod
-	"Pop up a list of the current class's methods, and select the one chosen by the user.
-	5/21/96 sw, based on a suggestion of John Maloney's."
-	| aClass selectors reply cat messageCatIndex messageIndex |
+	"Pop up a list of the current class's methods, and select the one chosen by the user"
 
+	| aClass selectors reply cat messageCatIndex messageIndex |
 	self classListIndex = 0 ifTrue: [^ self].
 	self okToChange ifFalse: [^ self].
-	aClass _ self selectedClassOrMetaClass.
-	selectors _ aClass selectors asSortedArray.
-	reply _ (SelectionMenu labelList: selectors selections: selectors) startUp.
+	aClass := self selectedClassOrMetaClass.
+	selectors := aClass selectors asSortedArray.
+	selectors isEmpty ifTrue: [self inform: aClass name, ' has no methods.'. ^ self].
+	reply := (SelectionMenu
+		labelList: (Array with: 'Enter Wildcard'), selectors
+		lines: #(1)
+		selections: (Array with: 'EnterWildcard'), selectors) startUp.
 	reply == nil ifTrue: [^ self].
-	cat _ aClass whichCategoryIncludesSelector: reply.
-	messageCatIndex _ self messageCategoryList indexOf: cat.
+	reply = 'EnterWildcard'
+		ifTrue: [
+			reply := FillInTheBlank request: 'Enter partial method name:'.
+			(reply isNil or: [reply isEmpty])
+				ifTrue: [^self].
+			(reply includes: $*)
+				ifFalse: [reply := '*', reply, '*'].
+			selectors := selectors select: [:each | reply match: each].
+			selectors isEmpty ifTrue: [self inform: aClass name, ' has no matching methods.'. ^ self].
+			reply := selectors size = 1
+				ifTrue: [selectors first]
+				ifFalse: [
+					(SelectionMenu
+						labelList: selectors
+						selections: selectors) startUp].
+			reply == nil ifTrue: [^ self]].
+
+	cat := aClass whichCategoryIncludesSelector: reply.
+	messageCatIndex := self messageCategoryList indexOf: cat.
 	self messageCategoryListIndex: messageCatIndex.
-	messageIndex _ (self messageList indexOf: reply).
-	self messageListIndex: messageIndex.
+	messageIndex := (self messageList indexOf: reply).
+	self messageListIndex: messageIndex
