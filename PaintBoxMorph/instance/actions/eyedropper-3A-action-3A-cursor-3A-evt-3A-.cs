284fcbdc04b7839dc77f35ebbd54364d252011fd
JMM <unknown>
2004-09-13 09:47:00 +0000

    no message

--- PaintBoxMorph/instance/actions/eyedropper-3A-action-3A-cursor-3A-evt-3A-.cs
+++ PaintBoxMorph/instance/actions/eyedropper-3A-action-3A-cursor-3A-evt-3A-.cs
@@ -1,34 +1,49 @@
 eyedropper: aButton action: aSelector cursor: aCursor evt: evt 
 	"Take total control and pick up a color!!"
-	| pt feedbackColor |
+
+	| pt feedbackColor delay |
+	delay _ Delay forMilliseconds: 10.
 	aButton state: #on.
-	tool
-		ifNotNil: [tool state: #off].
-	currentCursor _ aCursor.
-	evt hand showTemporaryCursor: currentCursor hotSpotOffset: 6 negated @ 4 negated.
+	tool ifNotNil: [tool state: #off].
+	currentCursor := aCursor.
+	evt hand showTemporaryCursor: currentCursor
+		hotSpotOffset: 6 negated @ 4 negated.
 	"<<<< the form was changed a bit??"
-	feedbackColor _ Display colorAt: Sensor cursorPoint.
-	colorMemory align: colorMemory bounds topRight with: colorMemoryThin bounds topRight.
+	feedbackColor := Display colorAt: Sensor cursorPoint.
+	colorMemory align: colorMemory bounds topRight
+		with: colorMemoryThin bounds topRight.
 	self addMorphFront: colorMemory.
+
 	"Full color picker"
-	[Sensor anyButtonPressed]
-		whileFalse: [pt _ Sensor cursorPoint.
+	[Sensor anyButtonPressed] whileFalse: 
+			[pt := Sensor cursorPoint.
 			"deal with the fact that 32 bit displays may have garbage in the 
 			alpha bits"
-			feedbackColor _ Display depth = 32
-						ifTrue: [Color
-								colorFromPixelValue: ((Display pixelValueAt: pt)
-										bitOr: 4278190080)
+			feedbackColor := Display depth = 32 
+						ifTrue: 
+							[Color colorFromPixelValue: ((Display pixelValueAt: pt) bitOr: 4278190080)
 								depth: 32]
 						ifFalse: [Display colorAt: pt].
 			"the hand needs to be drawn"
 			evt hand position: pt.
-			self world displayWorldSafely].
-	Sensor waitNoButton.
+			currentColor ~= feedbackColor ifTrue: [
+				currentColor _ feedbackColor.
+				self showColor ].
+			self world displayWorldSafely.
+			delay wait].
+
+	"Now wait for the button to be released."
+	[Sensor anyButtonPressed] whileTrue:
+		[ pt := Sensor cursorPoint.
+		"the hand needs to be drawn"
+		evt hand position: pt.
+		self world displayWorldSafely.
+		delay wait].
+
 	evt hand showTemporaryCursor: nil hotSpotOffset: 0 @ 0.
 	self currentColor: feedbackColor evt: evt.
 	colorMemory delete.
-	tool
-		ifNotNil: [tool state: #on.
-			currentCursor _ tool arguments at: 3].
+	tool ifNotNil: 
+			[tool state: #on.
+			currentCursor := tool arguments third].
 	aButton state: #off
