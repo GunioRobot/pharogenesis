b8f9cc5f6c3b89bb228b58b81d965806c42708c3
JMM <unknown>
2004-09-13 09:08:00 +0000

    no message

--- PaintBoxColorPicker/instance/event handling/selectColor-3A-.cs
+++ PaintBoxColorPicker/instance/event handling/selectColor-3A-.cs
@@ -1,18 +1,18 @@
-selectColor: evt
+selectColor: evt 
 	"Update the receiver from the given event. Constrain locOfCurrent's center to lie within the color selection area. If it is partially in the transparent area, snap it entirely into it vertically."
 
 	| r |
-	locOfCurrent _ evt cursorPoint - self topLeft.
-	r _ Rectangle center: locOfCurrent extent: 9@9.
-	locOfCurrent _ locOfCurrent + (r amountToTranslateWithin: (5@11 corner: 140@136)).
-	locOfCurrent x > 128 ifTrue: [locOfCurrent _ 135@locOfCurrent y].  "snap into grayscale"
-	locOfCurrent y < 17
-		ifTrue: [
-			locOfCurrent _ locOfCurrent x@11.  "snap into transparent"
-			currentColor _ Color transparent]
-		ifFalse: [
-			currentColor _ image colorAt: locOfCurrent].
 
-	(owner isKindOf: PaintBoxMorph) ifTrue: [owner takeColorEvt: evt from: self].
-
-	self changed.
+	locOfCurrent := evt cursorPoint - self topLeft.
+	r := Rectangle center: locOfCurrent extent: 9 @ 9.
+	locOfCurrent := locOfCurrent 
+				+ (r amountToTranslateWithin: (8 @ 11 corner: (self image width-6) @ (self image height-6))).
+	locOfCurrent x > (self image width-(12+7))  ifTrue: [locOfCurrent := (self image width - 12) @ locOfCurrent y].	"snap into grayscale"
+	currentColor := locOfCurrent y < 19
+				ifTrue:  
+					[locOfCurrent := locOfCurrent x @ 11.	"snap into transparent"
+					Color transparent]
+				ifFalse: [image colorAt: locOfCurrent].
+	(owner isKindOf: PaintBoxMorph) 
+		ifTrue: [owner takeColorEvt: evt from: self].
+	self changed
