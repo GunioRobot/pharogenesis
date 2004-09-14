7ecd962f91c21a6fd4acbcfa0aeced4222a7164d
st <unknown>
2004-09-14 12:54:00 +0000

    no message

--- HaloMorph/instance/private/doGrow-3A-with-3A-.cs
+++ HaloMorph/instance/private/doGrow-3A-with-3A-.cs
@@ -1,14 +1,19 @@
 doGrow: evt with: growHandle
 	"Called while the mouse is down in the grow handle"
 
-	| newExtent extentToUse |
+	| newExtent extentToUse scale |
 	evt hand obtainHalo: self.
 	newExtent _ (target pointFromWorld: (target griddedPoint: evt cursorPoint - positionOffset))
 								- target topLeft.
-	evt shiftPressed ifTrue: [newExtent _ (newExtent x max: newExtent y) asPoint].
-	target renderedMorph extent: (extentToUse _ newExtent).
+	evt shiftPressed ifTrue: [
+		scale _ (newExtent x / (originalExtent x max: 1)) min:
+					(newExtent y / (originalExtent y max: 1)).
+		newExtent _ (originalExtent x * scale) asInteger @ (originalExtent y * scale) asInteger
+	].
+	(newExtent x < 1 or: [newExtent y < 1 ]) ifTrue: [^ self].
+	target renderedMorph setExtentFromHalo: (extentToUse _ newExtent).
 	growHandle position: evt cursorPoint - (growHandle extent // 2).
 	self layoutChanged.
-	(self valueOfProperty: #commandInProgress) doIfNotNil:  
+	(self valueOfProperty: #commandInProgress) ifNotNilDo:  
 		[:cmd | "Update the final extent"
-		cmd redoTarget: target selector: #extent: argument: extentToUse]
+		cmd redoTarget: target selector: #setExtentFromHalo: argument: extentToUse]
