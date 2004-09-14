ce785f1402eb6ab34d67e9d8ea838db34b114094
st <unknown>
2004-09-14 12:51:00 +0000

    no message

--- HaloMorph/instance/private/startGrow-3A-with-3A-.cs
+++ HaloMorph/instance/private/startGrow-3A-with-3A-.cs
@@ -1,14 +1,14 @@
 startGrow: evt with: growHandle
-	| botRt |
 	"Initialize resizing of my target.  Launch a command representing it, to support Undo"
-	evt hand obtainHalo: self.
-	growingOrRotating _ true.
-	self removeAllHandlesBut: growHandle.  "remove all other handles"
+
+	| botRt |
+	self obtainHaloForEvent: evt andRemoveAllHandlesBut: growHandle.
 	botRt _ target point: target bottomRight in: owner.
-	(self world viewBox containsPoint: botRt)
-		ifTrue: [positionOffset _ evt cursorPoint - botRt]
-		ifFalse: [positionOffset _ 0@0].
+	positionOffset _ (self world viewBox containsPoint: botRt)
+		ifTrue: [evt cursorPoint - botRt]
+		ifFalse: [0@0].
 	self setProperty: #commandInProgress toValue:
 		(Command new
-			cmdWording: 'resizing';
-			undoTarget: target selector: #extent: argument: target extent)
+			cmdWording: 'resizing' translated;
+			undoTarget: target selector: #setExtentFromHalo: argument: target extent).
+	originalExtent _ target extent
