fdb44396262efbfddd2001b95da67585c32544ec
avi <unknown>
2004-09-14 14:57:00 +0000

    no message

---
+++ MCWorkingCopyBrowser/instance/morphic ui/workingCopyTreeMenu-3A-.cs
@@ -0,0 +1,16 @@
+workingCopyTreeMenu: aMenu
+	workingCopy ifNil: [^ aMenu].
+	self fillMenu: aMenu fromSpecs:
+		#(('add required package' #addRequiredPackage)
+			('clear required packages' #clearRequiredPackages)
+			('browse package' #browseWorkingCopy)
+			('view changes' #viewChanges)
+			('view history' #viewHistory)
+			('recompile package' #recompilePackage)
+			('unload package' #unloadPackage)
+			('delete working copy' #deleteWorkingCopy)
+			('inspect working copy' #inspectWorkingCopy)).
+	(Smalltalk includesKey: #SARMCPackageDumper) ifTrue: [
+		aMenu add: 'make SAR' target: self selector: #fileOutAsSAR
+	].
+	^aMenu
