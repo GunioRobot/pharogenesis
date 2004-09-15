0d3434c336d258a6f0a4da33af39632a255c35d5
tpr <unknown>
2004-09-15 10:27:00 +0000

    no message

--- FormCanvas/instance/drawing-2D-images/stencil-3A-at-3A-sourceRect-3A-color-3A-.cs
+++ FormCanvas/instance/drawing-2D-images/stencil-3A-at-3A-sourceRect-3A-color-3A-.cs
@@ -1,9 +1,7 @@
 stencil: stencilForm at: aPoint sourceRect: sourceRect color: aColor
 	"Flood this canvas with aColor wherever stencilForm has non-zero pixels"
-	port isFXBlt "FXBlt has a very different setup"
-		ifTrue:[self setStencilColor: aColor form: stencilForm]
-		ifFalse:[self setPaintColor: aColor.
-				port colorMap: (Color maskingMap: stencilForm depth)].
+	self setPaintColor: aColor.
+	port colorMap: stencilForm maskingMap.
 	port stencil: stencilForm
 		at: aPoint + origin
 		sourceRect: sourceRect.
