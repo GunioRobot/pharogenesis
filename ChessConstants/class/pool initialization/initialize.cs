f258789fc1eb199e8549e28843e9133aa0fb4258
RM <unknown>
2004-09-16 14:34:00 +0000

    no message

---
+++ ChessConstants/class/pool initialization/initialize.cs
@@ -0,0 +1,10 @@
+initialize
+	"ChessConstants initialize"
+	self initializePieceConstants.
+	self initializeCastlingConstants.
+	self initializePieceValues.
+	self initializeMoves.
+	self initializeCenterScores.
+	self initializeBishopMovers.
+	self initializeRookMovers.
+	self initializeSquareConstants.
