b3592b1519491d10d07de4479b0f614157c92a75
kfr <unknown>
2004-09-22 21:30:00 +0000

    no message

--- SmartRefStream/instance/class changed shape/writeClassRenameMethod-3A-was-3A-fromInstVars-3A-.cs
+++ SmartRefStream/instance/class changed shape/writeClassRenameMethod-3A-was-3A-fromInstVars-3A-.cs
@@ -1,7 +1,7 @@
 writeClassRenameMethod: sel was: oldName fromInstVars: oldList
 	"The class coming is unknown.  Ask the user for the existing class it maps to.  If got one, write a method, and restart the obj fileIn.  If none, write a dummy method and get the user to complete it later.  "
 
-| tell choice  newName answ code oldVer newList newVer instSel |
+| tell choice  newName answ code |
 
 	self flag: #bobconv.	
 
@@ -13,7 +13,7 @@ Let me think about it
 Let me find a conversion file on the disk') startUpWithCaption: tell. 
 
 answ = 1 ifTrue: [
-	tell _ 'Name of the modern class that ', oldName, 's should it translate to:'.
+	tell := 'Name of the modern class {1} should translate to:' translated format: {oldName}.
 	choice _ FillInTheBlank request: tell.		"class name"
 	(choice size = 0) 
 		ifTrue: [answ _ 'conversion method needed']
@@ -28,26 +28,16 @@ answ = 'conversion method needed' ifTrue: [
 		self close.  
 		newName _ 'PutNewClassHere'].
 
-answ class == String ifFalse: [
-	oldVer _ self versionSymbol: (structures at: oldName).
-	newList _ (Array with: answ classVersion), (answ allInstVarNames).
-	newVer _ self versionSymbol: newList.
-	instSel _ 'convert',oldVer,':',newVer, ':'. 
-	].
-
 code _ WriteStream on: (String new: 500).
 code nextPutAll: sel; cr.
-answ class == String ifFalse: [
-	code cr; tab; nextPutAll: 'reshaped at: #', oldName, ' put: #', instSel, '.'.
-	code cr; tab; tab; nextPutAll: '"Be sure to define that conversion method in class ', answ name, '"'].
 code cr; tab; nextPutAll: '^ ', newName.	"Return new class"
 
 self class compile: code contents classified: 'conversion'.
 
 newName = 'PutNewClassHere' ifTrue: [
-	PopUpMenu notify: 'Please complete the following method and 
+	self inform: 'Please complete the following method and 
 then read-in the object file again.'.
-	Smalltalk browseAllImplementorsOf: sel asSymbol]. 
+	SystemNavigation default browseAllImplementorsOf: sel asSymbol]. 
 
 	"The class version number only needs to change under one specific circumstance.  That is when the first letters of the instance variables have stayed the same, but their meaning has changed.  A conversion method is needed, but this system does not know it.  
 	If this is true for class Foo, define classVersion in Foo class.  
