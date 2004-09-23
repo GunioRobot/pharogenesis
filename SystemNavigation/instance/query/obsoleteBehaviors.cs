b11e02eaf5ef8bff60db255ebc5d20a5adaa8802
sd <unknown>
2004-09-23 22:03:00 +0000

    no message

---
+++ SystemNavigation/instance/query/obsoleteBehaviors.cs
@@ -0,0 +1,12 @@
+obsoleteBehaviors
+	"SystemNavigation default obsoleteBehaviors inspect"
+	"Find all obsolete behaviors including meta classes"
+
+	| obs |
+	obs _ OrderedCollection new.
+	Smalltalk garbageCollect.
+	self 
+		allObjectsDo: [:cl | (cl isBehavior
+					and: [cl isObsolete])
+				ifTrue: [obs add: cl]].
+	^ obs asArray
