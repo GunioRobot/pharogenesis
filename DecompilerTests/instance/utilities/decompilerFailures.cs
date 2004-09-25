b318974697fe12160fcce3d4459045fbd3a0cb6c
sd <unknown>
2004-09-25 16:07:00 +0000

    no message

---
+++ DecompilerTests/instance/utilities/decompilerFailures.cs
@@ -0,0 +1,7 @@
+decompilerFailures
+	"here is the list of failures: DNU resulting in trying to decompile the following methods"
+
+	^ #((PNMReadWriter nextImage) (Collection #ifEmpty:ifNotEmpty:) (Collection #ifEmpty:) (Collection #ifNotEmpty:ifEmpty:) (Text #alignmentAt:ifAbsent:) (ObjectWithDocumentation propertyAt:ifAbsent:))
+
+
+
