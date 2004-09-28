dbf77895a5d310cf2a794be89ae1fa161915ef12
tpr <unknown>
2004-09-28 16:54:00 +0000

    no message

--- Form/instance/scaling, rotation/rotateBy-3A-magnify-3A-smoothing-3A-.cs
+++ Form/instance/scaling, rotation/rotateBy-3A-magnify-3A-smoothing-3A-.cs
@@ -1,14 +1,14 @@
 rotateBy: deg magnify: scale smoothing: cellSize
-	"Rotate the receiver by the indicated number of degrees and magnify.  "
+	"Rotate the receiver by the indicated number of degrees and magnify. scale can be a Point to make for interesting 3D effects "
 	"rot is the destination form, big enough for any angle."
 
 	| side rot warp r1 pts p bigSide |
-	side _ 1 + ((width*width) + (height*height)) asFloat sqrt asInteger.
-	bigSide _ (side * scale) rounded.
-	rot _ Form extent: bigSide@bigSide depth: self depth.
+	side _ 1 + self extent r asInteger.
+	bigSide _ (side asPoint * scale) rounded.
+	rot _ self class extent: bigSide depth: self depth.
 	warp _ (WarpBlt current toForm: rot)
 		sourceForm: self;
-		colorMap: (self colormapIfNeededForDepth: depth);
+		colorMap: (self colormapIfNeededFor: rot);
 		cellSize: cellSize;  "installs a new colormap if cellSize > 1"
 		combinationRule: Form paint.
 	r1 _ (0@0 extent: side@side) align: (side@side)//2 with: self boundingBox center.
@@ -24,6 +24,6 @@ rotateBy: deg magnify: scale smoothing: cellSize
  | a f |  f _ Form fromDisplay: (0@0 extent: 200@200).  a _ 0.
 [Sensor anyButtonPressed] whileFalse:
 	[((Form fromDisplay: (Sensor cursorPoint extent: 130@66))
-		rotateBy: (a _ a+5) magnify: 0.75 smoothing: 2) display].
+		rotateBy: (a _ a+5) magnify: 0.75@2 smoothing: 2) display].
 f display
 "
