034bbe32d2ac4de64aa7cd7b864456a0dfcf3ba1
tpr <unknown>
2004-09-28 16:55:00 +0000

    no message

--- Form/instance/scaling, rotation/rotateBy-3A-smoothing-3A-.cs
+++ Form/instance/scaling, rotation/rotateBy-3A-smoothing-3A-.cs
@@ -1,25 +1,6 @@
 rotateBy: deg smoothing: cellSize
 	"Rotate the receiver by the indicated number of degrees."
-	"rot is the destination form, bit enough for any angle."
-	| side rot warp r1 pts p center |
-	side _ 1 + ((width*width) + (height*height)) asFloat sqrt asInteger.
-	rot _ Form extent: side@side depth: self depth.
-	center _ rot extent // 2.
-
-	"Now compute the sin and cos constants for the rotation angle." 
-	warp _ (WarpBlt current toForm: rot)
-		sourceForm: self;
-		colorMap: (self colormapIfNeededForDepth: depth);
-		cellSize: cellSize;  "installs a new colormap if cellSize > 1"
-		combinationRule: Form over.
-	r1 _ rot boundingBox align: center with: self boundingBox center.
-
-	pts _ r1 innerCorners collect:
-		[:pt | p _ pt - r1 center.
-		(r1 center x asFloat + (p x asFloat*deg degreeCos) + (p y asFloat*deg degreeSin)) @
-		(r1 center y asFloat - (p x asFloat*deg degreeSin) + (p y asFloat*deg degreeCos))].
-	warp copyQuad: pts toRect: rot boundingBox.
-	^ rot
+	^self rotateBy: deg magnify: 1 smoothing: cellSize
 "
  | a f |  f _ Form fromDisplay: (0@0 extent: 200@200).  a _ 0.
 [Sensor anyButtonPressed] whileFalse:
