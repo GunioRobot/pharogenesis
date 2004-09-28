f94badc39a494aac393f23707bf34c4ec9f48813
asm <unknown>
2004-09-28 19:08:00 +0000

    no message

---
+++ MorphicGamesInfo/instance/introspection/changesText.cs
@@ -0,0 +1,8 @@
+changesText
+	^'
+version 6: included Rick McGeer''s Chess Castling Fix
+version 5: included Atomic (some changes were needed to make it work in 3.7) and some Babel stuff
+version 4: includes Andreas Raab''s rewrite of ChessConstants as declarative pool (update 5325)
+version 3: 	-added one change by the KCP team
+			-Chess its playing again
+version 2: applied changes by the MCP team'
