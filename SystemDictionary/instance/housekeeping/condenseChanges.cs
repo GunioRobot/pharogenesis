558c9a1d65bebf4d40c132afae4b1a09565f39db
sd <unknown>
2004-09-29 18:09:00 +0000

    no message

--- SystemDictionary/instance/housekeeping/condenseChanges.cs
+++ SystemDictionary/instance/housekeeping/condenseChanges.cs
@@ -1,25 +1,29 @@
-condenseChanges		"Smalltalk condenseChanges"
+condenseChanges
 	"Move all the changes onto a compacted sources file."
+	"Smalltalk condenseChanges"
 	| f oldChanges classCount |
-	f _ FileStream fileNamed: 'ST80.temp'.
+	f := FileStream fileNamed: 'ST80.temp'.
 	f header; timeStamp.
-'Condensing Changes File...'
-	displayProgressAt: Sensor cursorPoint
-	from: 0 to: Smalltalk classNames size
-	during:
-		[:bar | classCount _ 0.
-		Smalltalk allClassesDo:
-			[:class | bar value: (classCount _ classCount + 1).
-			class moveChangesTo: f.
-			class class moveChangesTo: f]].
-	LastQuitLogPosition _ f position.
+	'Condensing Changes File...'
+		displayProgressAt: Sensor cursorPoint
+		from: 0
+		to: self classNames size
+		during: [:bar | 
+			classCount := 0.
+			self
+				allClassesDo: [:class | 
+					bar value: (classCount := classCount + 1).
+					class moveChangesTo: f.
+					class putClassCommentToCondensedChangesFile: f.
+					class class moveChangesTo: f]].
+	SmalltalkImage current lastQuitLogPosition: f position.
 	f trailer; close.
-	oldChanges _ SourceFiles at: 2.
+	oldChanges := SourceFiles at: 2.
 	oldChanges close.
-	FileDirectory default 
-		deleteFileNamed: oldChanges name , '.old';
-		rename: oldChanges name toBe: oldChanges name , '.old';
-		rename: f name toBe: oldChanges name.
+	FileDirectory default deleteFileNamed: oldChanges name , '.old';
+		 rename: oldChanges name toBe: oldChanges name , '.old';
+		 rename: f name toBe: oldChanges name.
 	self setMacFileInfoOn: oldChanges name.
-	SourceFiles at: 2
-			put: (StandardFileStream oldFileNamed: oldChanges name).
+	SourceFiles
+		at: 2
+		put: (StandardFileStream oldFileNamed: oldChanges name)
