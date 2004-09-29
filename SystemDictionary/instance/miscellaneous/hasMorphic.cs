241cfd449b91e1508e419c2c44b4c13b2ff51a36
sd <unknown>
2004-09-29 18:17:00 +0000

    no message

--- SystemDictionary/instance/miscellaneous/hasMorphic.cs
+++ SystemDictionary/instance/miscellaneous/hasMorphic.cs
@@ -1,4 +1,8 @@
 hasMorphic
-	"Answer whether the Morphic classes are available in the system (they may have been stripped, such as by a call to Smalltalk removeMorphic"
-
-	^ ((Smalltalk at: #Morph ifAbsent: [nil]) isKindOf: Class)
+	"Answer whether the Morphic classes are available in the
+	system (they may have been stripped, such as by a call to
+	Smalltalk removeMorphic"
+	^ (self
+		at: #Morph
+		ifAbsent: [])
+		isKindOf: Class
