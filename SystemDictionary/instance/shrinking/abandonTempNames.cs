39bca653e233d3051c216462635a66d0b1730c97
sd <unknown>
2004-09-29 18:18:00 +0000

    no message

--- SystemDictionary/instance/shrinking/abandonTempNames.cs
+++ SystemDictionary/instance/shrinking/abandonTempNames.cs
@@ -1,31 +1,35 @@
 abandonTempNames
-	"Replaces every method by a copy with no source pointer or encoded temp names."
+	"Replaces every method by a copy with no source pointer or
+	encoded temp names."
 	"Smalltalk abandonTempNames"
-
-	 | continue oldMethods newMethods n m |
-	continue _ (self confirm:  '-- CAUTION --
+	| continue oldMethods newMethods n m |
+	continue := self confirm: '-- CAUTION --
 If you have backed up your system and
 are prepared to face the consequences of
 abandoning all source code, hit Yes.
 If you have any doubts, hit No,
-to back out with no harm done.').
-	continue ifFalse: [^ self inform: 'Okay - no harm done'].
-
-	Smalltalk forgetDoIts; garbageCollect.
-	oldMethods _ OrderedCollection new.
-	newMethods _ OrderedCollection new.
-	n _ 0.
+to back out with no harm done.'.
+	continue
+		ifFalse: [^ self inform: 'Okay - no harm done'].
+	self forgetDoIts; garbageCollect.
+	oldMethods := OrderedCollection new.
+	newMethods := OrderedCollection new.
+	n := 0.
 	'Removing temp names to save space...'
 		displayProgressAt: Sensor cursorPoint
-		from: 0 to: CompiledMethod instanceCount
-		during: [:bar |
-			Smalltalk allBehaviorsDo: [:cl |
-				cl selectors do: [:sel |
-					bar value: (n _ n + 1).
-					m _ cl compiledMethodAt: sel.
-					oldMethods addLast: m.
-					newMethods addLast: (m copyWithTrailerBytes: #(0))]]].
+		from: 0
+		to: CompiledMethod instanceCount
+		during: [:bar | self systemNavigation
+				allBehaviorsDo: [:cl | cl selectors
+						do: [:sel | 
+							bar value: (n := n + 1).
+							m := cl compiledMethodAt: sel.
+							oldMethods addLast: m.
+							newMethods
+								addLast: (m copyWithTrailerBytes: #(0 ))]]].
 	oldMethods asArray elementsExchangeIdentityWith: newMethods asArray.
-	Smalltalk closeSourceFiles.
+	SmalltalkImage current closeSourceFiles.
+	self flag: #shouldUseAEnsureBlockToBeSureThatTheFileIsClosed.
+	"sd: 17 April 2003"
 	Preferences disable: #warnIfNoChangesFile.
-	Preferences disable: #warnIfNoSourcesFile.
+	Preferences disable: #warnIfNoSourcesFile
