81562f978f904f2a80cb2212a2fcafc53f36e16d
sd <unknown>
2004-09-29 18:40:00 +0000

    no message

--- SystemDictionary/instance/shrinking/discard3D.cs
+++ SystemDictionary/instance/shrinking/discard3D.cs
@@ -1,11 +1,32 @@
 discard3D
-	"Discard 3D Support.
-Updated for 2.8 TPR"
-
-	Smalltalk removeKey: #WonderlandConstants ifAbsent: [].
-	Smalltalk removeKey: #AliceConstants ifAbsent: [].
-	Smalltalk removeKey: #B3DEngineConstants ifAbsent: [].
-	SystemOrganization removeCategoriesMatching: 'Morphic-Balloon3D'.
+	"Smalltalk discard3D"
+	"Discard 3D Support."
+	self discardWonderland.
+	self
+		removeKey: #B3DEngineConstants
+		ifAbsent: [].
+	self
+		at: #InterpolatingImageMorph
+		ifPresent: [:cls | cls removeFromSystem].
+	SystemOrganization removeCategoriesMatching: 'Graphics-FXBlt'.
+	SystemOrganization removeCategoriesMatching: 'Graphics-External'.
 	SystemOrganization removeCategoriesMatching: 'Balloon3D-*'.
-	SystemOrganization removeCategoriesMatching: 'Pooh-*'
-
+	SystemOrganization removeCategoriesMatching: 'Graphics-Tools-*'.
+	Color removeSelector: #asB3DColor.
+	Form removeSelector: #asTexture.
+	Morph removeSelector: #asTexture.
+	Morph removeSelector: #mapPrimitiveVertex:.
+	Morph removeSelector: #installAsWonderlandTextureOn:.
+	FileList removeSelector: #open3DSFile.
+	Point removeSelector: #@.
+	self
+		at: #BalloonCanvas
+		ifPresent: [:cc | cc removeSelector: #render:].
+	Stream removeSelector: #asVRMLStream.
+	SketchMorph removeSelector: #drawInterpolatedImage:on:.
+	SketchMorph removeSelector: #generateInterpolatedForm.
+	self
+		at: #B3DEnginePlugin
+		ifPresent: [:cls | cls
+				withAllSubclassesDo: [:each | each removeFromSystem]].
+	DataStream initialize
