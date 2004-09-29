33ecd6700f0fb5d26088c9418153f893448cc91f
sd <unknown>
2004-09-29 18:23:00 +0000

    no message

--- SystemDictionary/instance/shrinking/discardSUnit.cs
+++ SystemDictionary/instance/shrinking/discardSUnit.cs
@@ -1,9 +1,12 @@
-discardSUnit   "Smalltalk discardSUnit"
+discardSUnit
+	"Smalltalk discardSUnit"
 	| oc |
-	oc _ OrderedCollection new.
-	(Smalltalk at: #TestCase ifAbsent: [^ self])
+	oc := OrderedCollection new.
+	(self
+		at: #TestCase
+		ifAbsent: [^ self])
 		allSubclassesWithLevelDo: [:c :i | oc addFirst: c]
 		startingLevel: 0.
-	oc do: [:c | c removeFromSystem].
-
+	oc
+		do: [:c | c removeFromSystem].
 	SystemOrganization removeCategoriesMatching: 'SUnit-*'
