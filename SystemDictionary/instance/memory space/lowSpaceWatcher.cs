878d4548958a2b13d2a0643ead7de4bc4c901a9b
sd <unknown>
2004-09-29 18:16:00 +0000

    no message

--- SystemDictionary/instance/memory space/lowSpaceWatcher.cs
+++ SystemDictionary/instance/memory space/lowSpaceWatcher.cs
@@ -1,28 +1,30 @@
 lowSpaceWatcher
-	"Wait until the low space semaphore is signalled, then take appropriate actions."
-
+	"Wait until the low space semaphore is signalled, then take
+	appropriate actions."
 	| free |
-	self garbageCollectMost <= self lowSpaceThreshold ifTrue: [
-		self garbageCollect <= self lowSpaceThreshold ifTrue: [
-			"free space must be above threshold before starting low space watcher"
-			^ self beep]].
-
-	LowSpaceSemaphore _ Semaphore new.
+	self garbageCollectMost <= self lowSpaceThreshold
+		ifTrue: [self garbageCollect <= self lowSpaceThreshold
+				ifTrue: ["free space must be above threshold before
+					starting low space watcher"
+					^ Beeper beep]].
+	LowSpaceSemaphore := Semaphore new.
 	self primLowSpaceSemaphore: LowSpaceSemaphore.
-	self primSignalAtBytesLeft: self lowSpaceThreshold.  "enable low space interrupts"
-
-	LowSpaceSemaphore wait.  "wait for a low space condition..."
-
-	self primSignalAtBytesLeft: 0.  "disable low space interrupts"
+	self primSignalAtBytesLeft: self lowSpaceThreshold.
+	"enable low space interrupts"
+	LowSpaceSemaphore wait.
+	"wait for a low space condition..."
+	self primSignalAtBytesLeft: 0.
+	"disable low space interrupts"
 	self primLowSpaceSemaphore: nil.
-	LowSpaceProcess _ nil.
-	"Note: user now unprotected until the low space watcher is re-installed"
-
-	self memoryHogs isEmpty ifFalse: [
-		free := self bytesLeft.
-		self memoryHogs do: [ :hog | hog freeSomeSpace ].
-		self bytesLeft > free ifTrue: [ ^ self installLowSpaceWatcher ]].
-
-	Smalltalk isMorphic
-			ifTrue: [CurrentProjectRefactoring currentInterruptName: 'Space is low']
-			ifFalse: [ScheduledControllers interruptName: 'Space is low']
+	LowSpaceProcess := nil.
+	"Note: user now unprotected until the low space watcher is
+	re-installed "
+	self memoryHogs isEmpty
+		ifFalse: [free := self bytesLeft.
+			self memoryHogs
+				do: [:hog | hog freeSomeSpace].
+			self bytesLeft > free
+				ifTrue: [^ self installLowSpaceWatcher]].
+	self isMorphic
+		ifTrue: [CurrentProjectRefactoring currentInterruptName: 'Space is low']
+		ifFalse: [ScheduledControllers interruptName: 'Space is low']
