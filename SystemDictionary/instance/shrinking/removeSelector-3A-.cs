b17d073445f76c333e4775a0a100d66a3d9fd2ee
sd <unknown>
2004-09-29 18:26:00 +0000

    no message

--- SystemDictionary/instance/shrinking/removeSelector-3A-.cs
+++ SystemDictionary/instance/shrinking/removeSelector-3A-.cs
@@ -1,14 +1,16 @@
-removeSelector: descriptor
-	"Safely remove a selector from a class (or metaclass). If the class
-	or the method doesn't exist anymore, never mind and answer nil.
-	This method should be used instead of 'Class removeSelector: #method'
-	to omit global class references."
-
+removeSelector: descriptor 
+	"Safely remove a selector from a class (or metaclass). If the
+	class or the method doesn't exist anymore, never mind and
+	answer nil.
+	This method should be used instead of 'Class removeSelector:
+	#method' to omit global class references."
 	| class sel |
-	class _ Smalltalk at: descriptor first ifAbsent: [^ nil].
-	(descriptor size > 2 and: [descriptor second == #class])
-		ifTrue:
-			[class _ class class.
-			sel _ descriptor third]
-		ifFalse: [sel _ descriptor second].
+	class := self
+				at: descriptor first
+				ifAbsent: [^ nil].
+	(descriptor size > 2
+			and: [descriptor second == #class])
+		ifTrue: [class := class class.
+			sel := descriptor third]
+		ifFalse: [sel := descriptor second].
 	^ class removeSelector: sel
