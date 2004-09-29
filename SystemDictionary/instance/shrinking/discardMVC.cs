bcc0c85ccb8af589be2f7bbd25633cdf81398453
sd <unknown>
2004-09-29 18:21:00 +0000

    no message

--- SystemDictionary/instance/shrinking/discardMVC.cs
+++ SystemDictionary/instance/shrinking/discardMVC.cs
@@ -1,53 +1,56 @@
 discardMVC
-   "Smalltalk discardMVC"
-
+	"After suitable checks, strip out much of MVC from the system"
+	"Smalltalk discardMVC"
 	| keepers |
-
-	self flag: #bob.		"zapping projects"
-
-	Smalltalk isMorphic ifFalse:
-		[PopUpMenu notify: 'You must be in a Morphic project to discard MVC.'.
-		^ self].
+	self flag: #bob.
+	"zapping projects"
+	self isMorphic
+		ifFalse: [self inform: 'You must be in a Morphic project to discard MVC.'.
+			^ self].
 	"Check that there are no MVC Projects"
-	(Project allProjects allSatisfy: [ :proj | proj isMorphic]) ifFalse:
-		[(self confirm: 'Would you like a chance to remove your
+	(Project allProjects
+			allSatisfy: [:proj | proj isMorphic])
+		ifFalse: [(self confirm: 'Would you like a chance to remove your
 MVC projects in an orderly manner?')
-					ifTrue: [^ self].
-		(self confirm: 'If you wish, I can remove all MVC projects,
+				ifTrue: [^ self].
+			(self confirm: 'If you wish, I can remove all MVC projects,
 make this project be the top project, and place
 all orphaned sub-projects of MVC parents here.
 Would you like be to do this
 and proceed to discard all MVC classes?')
-					ifTrue: [self zapMVCprojects]
-					ifFalse: [^ self]].
-	Smalltalk reclaimDependents.
-
+				ifTrue: [self zapMVCprojects]
+				ifFalse: [^ self]].
+	self reclaimDependents.
 	"Remove old Paragraph classes and View classes."
-	(ChangeSet superclassOrder: Paragraph withAllSubclasses asArray) reverseDo: 
-		[:c | c removeFromSystem].
-	(ChangeSet superclassOrder: View withAllSubclasses asArray) reverseDo: 
-		[:c | c removeFromSystem].
-
+	self
+		at: #Paragraph
+		ifPresent: [:paraClass | (ChangeSet superclassOrder: paraClass withAllSubclasses asArray)
+				reverseDo: [:c | c removeFromSystem]].
+	self
+		at: #View
+		ifPresent: [:viewClass | (ChangeSet superclassOrder: viewClass withAllSubclasses asArray)
+				reverseDo: [:c | c removeFromSystem]].
 	"Get rid of ParagraphEditor's ScrollController dependence"
-	#(markerDelta viewDelta scrollAmount scrollBar computeMarkerRegion) do:
-			[:sel | ParagraphEditor removeSelector: sel].
+	#(#markerDelta #viewDelta #scrollAmount #scrollBar #computeMarkerRegion )
+		do: [:sel | ParagraphEditor removeSelector: sel].
 	ParagraphEditor compile: 'updateMarker'.
-	ParagraphEditor superclass: MouseMenuController .
-
-	"Get rid of all Controller classes not needed by ParagraphEditor and ScreenController"
-	keepers _ TextMorphEditor withAllSuperclasses copyWith: ScreenController.
-	(ChangeSet superclassOrder: Controller withAllSubclasses asArray) reverseDo: 
-		[:c | (keepers includes: c) ifFalse: [c removeFromSystem]].
-
+	"Reshape to MouseMenuController"
+	Compiler
+		evaluate: (ParagraphEditor definition copyReplaceAll: 'ScrollController' with: 'MouseMenuController').
+	"Get rid of all Controller classes not needed by
+	ParagraphEditor and ScreenController"
+	keepers := TextMorphEditor withAllSuperclasses copyWith: ScreenController.
+	(ChangeSet superclassOrder: Controller withAllSubclasses asArray)
+		reverseDo: [:c | (keepers includes: c)
+				ifFalse: [c removeFromSystem]].
 	SystemOrganization removeCategoriesMatching: 'ST80-Paths'.
+	SystemOrganization removeCategoriesMatching: 'ST80-Symbols'.
 	SystemOrganization removeCategoriesMatching: 'ST80-Pluggable Views'.
-
-	Smalltalk removeClassNamed: 'FormButtonCache'.
-	Smalltalk removeClassNamed: 'WindowingTransformation'.
-	Smalltalk removeClassNamed: 'ControlManager'.
-	Smalltalk removeClassNamed: 'DisplayTextView'.
-
-	ScheduledControllers _ nil.
+	self removeClassNamed: 'FormButtonCache'.
+	self removeClassNamed: 'WindowingTransformation'.
+	self removeClassNamed: 'ControlManager'.
+	self removeClassNamed: 'DisplayTextView'.
+	ScheduledControllers := nil.
 	Undeclared removeUnreferencedKeys.
 	SystemOrganization removeEmptyCategories.
-	Symbol rehash.
+	Symbol rehash
