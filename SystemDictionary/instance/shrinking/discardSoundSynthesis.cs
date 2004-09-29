d1f94c94090adf8fcada68b76014c3321625cb73
sd <unknown>
2004-09-29 18:23:00 +0000

    no message

--- SystemDictionary/instance/shrinking/discardSoundSynthesis.cs
+++ SystemDictionary/instance/shrinking/discardSoundSynthesis.cs
@@ -1,15 +1,18 @@
 discardSoundSynthesis
-	"Discard the sound synthesis facilities, and the methods and classes that use it. This also discards MIDI."
-
-	Smalltalk discardMIDI.
-	Smalltalk discardSpeech.
+	"Discard the sound synthesis facilities, and the methods and
+	classes that use it. This also discards MIDI."
+	self discardMIDI.
+	self discardSpeech.
 	SystemOrganization removeCategoriesMatching: 'Sound-Interface'.
-	Smalltalk at: #GraphMorph ifPresent: [:graphMorph |
-		#(playOnce readDataFromFile)
-			do: [:sel | graphMorph removeSelector: sel]].
-	Smalltalk at: #TrashCanMorph ifPresent: [:trashMorph |
-		trashMorph class removeSelector: #samplesForDelete.
-		trashMorph class removeSelector: #samplesForMouseEnter.
-		trashMorph class removeSelector: #samplesForMouseLeave].
+	self
+		at: #GraphMorph
+		ifPresent: [:graphMorph | #(#playOnce #readDataFromFile )
+				do: [:sel | graphMorph removeSelector: sel]].
+	self
+		at: #TrashCanMorph
+		ifPresent: [:trashMorph | 
+			trashMorph class removeSelector: #samplesForDelete.
+			trashMorph class removeSelector: #samplesForMouseEnter.
+			trashMorph class removeSelector: #samplesForMouseLeave].
 	SystemOrganization removeCategoriesMatching: 'Sound-Synthesis'.
-	SystemOrganization removeCategoriesMatching: 'Sound-Scores'.
+	SystemOrganization removeCategoriesMatching: 'Sound-Scores'
