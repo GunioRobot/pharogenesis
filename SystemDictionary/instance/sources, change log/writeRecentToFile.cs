c356c265b2ad47a0699a6ec6df988de7528768c1
sd <unknown>
2004-09-29 18:30:00 +0000

    no message

--- SystemDictionary/instance/sources, change log/writeRecentToFile.cs
+++ SystemDictionary/instance/sources, change log/writeRecentToFile.cs
@@ -1,9 +1,11 @@
 writeRecentToFile
-	| numChars aDirectory aFileName |
 	"Smalltalk writeRecentToFile"
-	aDirectory _ FileDirectory default.
-	aFileName _ Utilities keyLike: 'squeak-recent.01' withTrailing: '.log' satisfying:
-		[:aKey | (aDirectory includesKey: aKey) not].
-	numChars _ ChangeList getRecentLocatorWithPrompt: 'copy logged source as far back as...'.
-	numChars ifNotNil:
-		[Smalltalk writeRecentCharacters: numChars toFileNamed: aFileName]
+	| numChars aDirectory aFileName |
+	aDirectory := FileDirectory default.
+	aFileName := Utilities
+				keyLike: 'squeak-recent.01'
+				withTrailing: '.log'
+				satisfying: [:aKey | (aDirectory includesKey: aKey) not].
+	numChars := ChangeList getRecentLocatorWithPrompt: 'copy logged source as far back as...'.
+	numChars
+		ifNotNil: [self writeRecentCharacters: numChars toFileNamed: aFileName]
