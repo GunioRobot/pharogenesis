4a4e78bd4fec003d37939189c2a47faef7bc717a
sd <unknown>
2004-09-29 18:13:00 +0000

    no message

--- SystemDictionary/instance/housekeeping/makeExternalRelease.cs
+++ SystemDictionary/instance/housekeeping/makeExternalRelease.cs
@@ -1,23 +1,29 @@
-makeExternalRelease		"Smalltalk makeExternalRelease"
-	(self confirm: self version , '
+makeExternalRelease
+	"Smalltalk makeExternalRelease"
+	(self confirm: SystemVersion current version , '
 Is this the correct version designation?
-If not, choose no, and fix it.') ifFalse: [^ self].
+If not, choose no, and fix it.')
+		ifFalse: [^ self].
 	"Object classPool at: #DependentsFields"
-	Smalltalk reclaimDependents.
-	Preferences enable: #allowMVCprojects.
+	self reclaimDependents.
+	Preferences enable: #mvcProjectsAllowed.
 	Preferences enable: #fastDragWindowForMorphic.
 	Browser initialize.
-	Undeclared isEmpty ifFalse: [self halt].
+	Undeclared isEmpty
+		ifFalse: [self halt].
 	ScriptingSystem deletePrivateGraphics.
-	#(Helvetica Palatino Courier) do:
-		[:n | TextConstants removeKey: n ifAbsent: []].
-	(Utilities classPool at: #UpdateUrlLists) copy do:
-		[:pair | (pair first includesSubstring: 'Disney' caseSensitive: false) ifTrue: [
-			(Utilities classPool at: #UpdateUrlLists) remove: pair]].
-	(ServerDirectory serverNames copyWithoutAll: #('UCSBCreateArchive' 'UIUCArchive' 'UpdatesExtUIUC' 'UpdatesExtWebPage'))
+	#(#Helvetica #Palatino #Courier )
+		do: [:n | TextConstants
+				removeKey: n
+				ifAbsent: []].
+	(Utilities classPool at: #UpdateUrlLists) copy
+		do: [:pair | (pair first includesSubstring: 'Disney' caseSensitive: false)
+				ifTrue: [(Utilities classPool at: #UpdateUrlLists)
+						remove: pair]].
+	(ServerDirectory serverNames copyWithoutAll: #('UCSBCreateArchive' 'UIUCArchive' 'UpdatesExtUIUC' 'UpdatesExtWebPage' ))
 		do: [:sn | ServerDirectory removeServerNamed: sn].
-	Smalltalk garbageCollect.
-	Smalltalk obsoleteClasses isEmpty ifFalse: [self halt].
+	self  garbageCollect.
+	self obsoleteClasses isEmpty
+		ifFalse: [self halt].
 	Symbol rehash.
-	self halt: 'Ready to condense changes or sources'.
-	SystemDictionary removeSelector: #makeExternalRelease.
+	self halt: 'Ready to condense changes or sources'
