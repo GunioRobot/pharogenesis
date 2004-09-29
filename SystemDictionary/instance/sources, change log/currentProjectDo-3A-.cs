3b95427bf160edd5bcf545646ebe90fbe0b40c0d
sd <unknown>
2004-09-29 18:27:00 +0000

    no message

--- SystemDictionary/instance/sources, change log/currentProjectDo-3A-.cs
+++ SystemDictionary/instance/sources, change log/currentProjectDo-3A-.cs
@@ -1,4 +1,5 @@
-currentProjectDo: aBlock
+currentProjectDo: aBlock 
 	"So that code can work after removal of Projects"
-	Smalltalk at: #Project ifPresent:
-		[:projClass | aBlock value: projClass current].
+	self
+		at: #Project
+		ifPresent: [:projClass | aBlock value: projClass current]
