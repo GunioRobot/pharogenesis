ded389f6917ea3cd2fbd7f474c3fb2f30fd67fc4
sd <unknown>
2004-09-29 18:25:00 +0000

    no message

--- SystemDictionary/instance/shrinking/majorShrink.cs
+++ SystemDictionary/instance/shrinking/majorShrink.cs
@@ -1,117 +1,124 @@
-majorShrink    
-
+majorShrink
+	"Undertake a major shrinkage of the image.
+	This method throws out lots of the system that is not needed
+	for, eg, operation in a hand-held PC. majorShrink produces a
+	999k image in Squeak 2.8
+	Smalltalk majorShrink; abandonSources; lastRemoval"
 	| oldDicts newDicts |
-
-"Smalltalk majorShrink; abandonSources; lastRemoval"
-	"This method throws out lots of the system that is not needed for, eg, operation in a hand-held PC.  majorShrink produces a 999k image in Squeak 2.8"
-
-	Smalltalk isMorphic ifTrue: [^ self error: 'You can only run majorShrink in MVC'].
-	Project current isTopProject ifFalse: [^ self error: 'You can only run majorShrink in the top project'].
-	(Smalltalk confirm: 'All sub-projects will be deleted from this image.
+	self isMorphic
+		ifTrue: [^ self error: 'You can only run majorShrink in MVC'].
+	Project current isTopProject
+		ifFalse: [^ self error: 'You can only run majorShrink in the top project'].
+	(self confirm: 'All sub-projects will be deleted from this image.
 You should already have made a backup copy,
 or you must save with a different name after shrinking.
 Shall we proceed to discard most of the content in this image?')
-		ifFalse: [^ PopUpMenu notify: 'No changes have been made.'].
-
-	"Remove all projects but the current one.  - saves 522k"
-	ProjectView allInstancesDo: [:pv | pv controller closeAndUnscheduleNoTerminate].
+		ifFalse: [^ self inform: 'No changes have been made.'].
+	"Remove all projects but the current one. - saves 522k"
+	ProjectView
+		allInstancesDo: [:pv | pv controller closeAndUnscheduleNoTerminate].
 	Project current setParent: Project current.
-	MorphWorldView allInstancesDo: [:pv | pv topView controller closeAndUnscheduleNoTerminate].
-	Wonderland removeActorPrototypesFromSystem.
+	MorphWorldView
+		allInstancesDo: [:pv | pv topView controller closeAndUnscheduleNoTerminate].
+	self
+		at: #Wonderland
+		ifPresent: [:cls | cls removeActorPrototypesFromSystem].
 	Player freeUnreferencedSubclasses.
 	MorphicModel removeUninstantiatedModels.
 	Utilities classPool at: #ScrapsBook put: nil.
 	Utilities zapUpdateDownloader.
 	ProjectHistory currentHistory initialize.
 	Project rebuildAllProjects.
-
-	Smalltalk discardVMConstruction.  "755k"
-	Smalltalk discardSoundSynthesis.  "544k"
-	Smalltalk discardOddsAndEnds.  "227k"
-	Smalltalk discardNetworking.  "234k"
-	Smalltalk discard3D.  "407k"
-	Smalltalk discardFFI.  "33k"
-	Smalltalk discardMorphic.  "1372k"
-	Symbol rehash.  "40k"
+	"Smalltalk discardVMConstruction."
+	"755k"
+	self discardSoundSynthesis.
+	"544k"
+	self discardOddsAndEnds.
+	"227k"
+	self discardNetworking.
+	"234k"
+	"Smalltalk discard3D."
+	"407k"
+	self discardFFI.
+	"33k"
+	self discardMorphic.
+	"1372k"
+	Symbol rehash.
+	"40k"
 	"Above by itself saves about 4,238k"
-
-	"Remove references to a few classes to be deleted, so that they won't leave obsolete versions around."
-	FileList removeSelector: #fileIntoNewChangeSet.
+	"Remove references to a few classes to be deleted, so that they
+	won't leave obsolete versions around."
 	ChangeSet class compile: 'defaultName
 		^ ''Changes'' ' classified: 'initialization'.
 	ScreenController removeSelector: #openChangeManager.
 	ScreenController removeSelector: #exitProject.
 	ScreenController removeSelector: #openProject.
 	ScreenController removeSelector: #viewImageImports.
-
 	"Now delete various other classes.."
 	SystemOrganization removeSystemCategory: 'Graphics-Files'.
 	SystemOrganization removeSystemCategory: 'System-Object Storage'.
-	Smalltalk removeClassNamed: #ProjectController.
-	Smalltalk removeClassNamed: #ProjectView.
+	self removeClassNamed: #ProjectController.
+	self removeClassNamed: #ProjectView.
 	"Smalltalk removeClassNamed: #Project."
-	Smalltalk removeClassNamed: #Environment.
-	Smalltalk removeClassNamed: #Component1.
-
-	Smalltalk removeClassNamed: #FormSetFont.
-	Smalltalk removeClassNamed: #FontSet.
-	Smalltalk removeClassNamed: #InstructionPrinter.
-	Smalltalk removeClassNamed: #ChangeSorter.
-	Smalltalk removeClassNamed: #DualChangeSorter.
-	Smalltalk removeClassNamed: #EmphasizedMenu.
-	Smalltalk removeClassNamed: #MessageTally.
-
+	self removeClassNamed: #Environment.
+	self removeClassNamed: #Component1.
+	self removeClassNamed: #FormSetFont.
+	self removeClassNamed: #FontSet.
+	self removeClassNamed: #InstructionPrinter.
+	self removeClassNamed: #ChangeSorter.
+	self removeClassNamed: #DualChangeSorter.
+	self removeClassNamed: #EmphasizedMenu.
+	self removeClassNamed: #MessageTally.
 	StringHolder class removeSelector: #originalWorkspaceContents.
 	CompiledMethod removeSelector: #symbolic.
-
 	RemoteString removeSelector: #makeNewTextAttVersion.
 	Utilities class removeSelector: #absorbUpdatesFromServer.
-	Smalltalk removeClassNamed: #PenPointRecorder.
-	Smalltalk removeClassNamed: #Path.
-	Smalltalk removeClassNamed: #Base64MimeConverter.
-	"Smalltalk removeClassNamed: #EToySystem. Dont bother - its very small and used for timestamps etc"
-	Smalltalk removeClassNamed: #RWBinaryOrTextStream.
-	Smalltalk removeClassNamed: #AttributedTextStream.
-	Smalltalk removeClassNamed: #WordNet.
-	Smalltalk removeClassNamed: #SelectorBrowser.
-
-	TextStyle allSubInstancesDo:
-		[:ts | ts newFontArray: (ts fontArray copyFrom: 1 to: (2 min: ts fontArray size))].
+	self removeClassNamed: #PenPointRecorder.
+	self removeClassNamed: #Path.
+	self removeClassNamed: #Base64MimeConverter.
+	"Smalltalk removeClassNamed: #EToySystem. Dont bother - its
+	very small and used for timestamps etc"
+	self removeClassNamed: #RWBinaryOrTextStream.
+	self removeClassNamed: #AttributedTextStream.
+	self removeClassNamed: #WordNet.
+	self removeClassNamed: #SelectorBrowser.
+	TextStyle
+		allSubInstancesDo: [:ts | ts
+				newFontArray: (ts fontArray
+						copyFrom: 1
+						to: (2 min: ts fontArray size))].
 	ListParagraph initialize.
 	PopUpMenu initialize.
 	StandardSystemView initialize.
-
-	Smalltalk noChanges.
-	ChangeSorter classPool at: #AllChangeSets 
-		put: (OrderedCollection with: Smalltalk changes).
+	ChangeSet noChanges.
+	ChangeSorter classPool
+		at: #AllChangeSets
+		put: (OrderedCollection with: ChangeSet current).
 	SystemDictionary removeSelector: #majorShrink.
-
-	[Smalltalk removeAllUnSentMessages > 0]
-		whileTrue:
-		[Smalltalk unusedClasses do: [:c | (Smalltalk at: c) removeFromSystem]].
+	[self removeAllUnSentMessages > 0]
+		whileTrue: [Smalltalk unusedClasses
+				do: [:c | (Smalltalk at: c) removeFromSystem]].
 	SystemOrganization removeEmptyCategories.
-	Smalltalk allClassesDo: [:c | c zapOrganization].
-	Smalltalk garbageCollect.
-
+	self
+		allClassesDo: [:c | c zapOrganization].
+	self garbageCollect.
 	'Rehashing method dictionaries . . .'
 		displayProgressAt: Sensor cursorPoint
-		from: 0 to: MethodDictionary instanceCount
-		during: [:bar |
-			oldDicts _ MethodDictionary allInstances.
-			newDicts _ Array new: oldDicts size.
-			oldDicts withIndexDo: [:d :index | 
-				bar value: index.
-				newDicts at: index put: d rehashWithoutBecome.
-			].
-			oldDicts elementsExchangeIdentityWith: newDicts.
-		].
-	oldDicts _ newDicts _ nil.
+		from: 0
+		to: MethodDictionary instanceCount
+		during: [:bar | 
+			oldDicts := MethodDictionary allInstances.
+			newDicts := Array new: oldDicts size.
+			oldDicts
+				withIndexDo: [:d :index | 
+					bar value: index.
+					newDicts at: index put: d rehashWithoutBecome].
+			oldDicts elementsExchangeIdentityWith: newDicts].
+	oldDicts := newDicts := nil.
 	Project rebuildAllProjects.
-	Smalltalk changes initialize.
-
-	"seems to take more than one try to gc all the weak refs in SymbolTable"
-
-	3 timesRepeat: [
-		Smalltalk garbageCollect.
-		Symbol compactSymbolTable.
-	].
+	ChangeSet current initialize.
+	"seems to take more than one try to gc all the weak refs in
+	SymbolTable "
+	3
+		timesRepeat: [self garbageCollect.
+			Symbol compactSymbolTable]
