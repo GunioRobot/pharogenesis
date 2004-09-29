ecde6d3fd8aa25d2c9e910ae8c8e9ec8ccbd4172
sd <unknown>
2004-09-29 18:39:00 +0000

    no message

--- SystemDictionary/instance/class names/classNamed-3A-.cs
+++ SystemDictionary/instance/class names/classNamed-3A-.cs
@@ -1,14 +1,15 @@
 classNamed: className 
-	"className is either a class name or a class name followed by ' class'.
-	Answer the class or metaclass it names"
-
+	"className is either a class name or a class name followed by '
+	class'. Answer the class or metaclass it names"
 	| meta baseName baseClass |
 	(className endsWith: ' class')
-		ifTrue: [meta _ true.
-				baseName _ className copyFrom: 1 to: className size - 6]
-		ifFalse: [meta _ false.
-				baseName _ className].
-	baseClass _ Smalltalk at: baseName asSymbol ifAbsent: [^ nil].
+		ifTrue: [meta := true.
+			baseName := className copyFrom: 1 to: className size - 6]
+		ifFalse: [meta := false.
+			baseName := className].
+	baseClass := self
+				at: baseName asSymbol
+				ifAbsent: [^ nil].
 	meta
 		ifTrue: [^ baseClass class]
 		ifFalse: [^ baseClass]
