87667c65f374bcbfd1ba4c1194960bd62f4aadfb
md <unknown>
2004-09-30 16:42:00 +0000

    no message

--- Inspector/instance/menu commands/fieldListMenu-3A-.cs
+++ Inspector/instance/menu commands/fieldListMenu-3A-.cs
@@ -1,23 +1,27 @@
 fieldListMenu: aMenu
 	"Arm the supplied menu with items for the field-list of the receiver"
 
-	| sel |
-	((((sel _ self selection) isMemberOf: Array) or: [sel isMemberOf: OrderedCollection]) and: [sel size >= 1])
-		ifTrue:
-			[^ self fieldListMenuForCollection: aMenu].
+	Smalltalk isMorphic ifTrue:
+		[aMenu addStayUpItemSpecial].
 
 	aMenu addList: #(
-
 		('inspect (i)'						inspectSelection)
-		('explore (I)'						exploreSelection)
+		('explore (I)'						exploreSelection)).
+
+	self addCollectionItemsTo: aMenu.
+
+	aMenu addList: #(
 		-
 		('method refs to this inst var'		referencesToSelection)
 		('methods storing into this inst var'	defsOfSelection)
 		('objects pointing to this value'		objectReferencesToSelection)
+		('chase pointers'					chasePointers)
+		('explore pointers'				explorePointers)
 		-
 		('browse full (b)'					browseMethodFull)
 		('browse class'						browseClass)
-		('browse hierarchy'					classHierarchy)
+		('browse hierarchy (h)'					classHierarchy)
+		('browse protocol (p)'				browseFullProtocol)
 		-
 		('inst var refs...'					browseInstVarRefs)
 		('inst var defs...'					browseInstVarDefs)
@@ -25,16 +29,19 @@ fieldListMenu: aMenu
 		('class variables'					browseClassVariables)
 		('class refs (N)'						browseClassRefs)
 		-
-		('copy name'						copyName)		
+		('copy name (c)'					copyName)		
 		('basic inspect'						inspectBasic)).
 
 	Smalltalk isMorphic ifTrue:
 		[aMenu addList: #(
 			-
 			('tile for this value	(t)'			tearOffTile)
-"			('alias for this value'			aliasForValue)"
-"			('watcher for this slot'			watcherForSlot)"
 			('viewer for this value (v)'		viewerForValue))].
 
 	^ aMenu
+
+
+"			-
+			('alias for this value'			aliasForValue)
+			('watcher for this slot'			watcherForSlot)"
 
