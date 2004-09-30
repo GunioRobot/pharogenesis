969ad185fca0f5cd187aeb03e06b1a235dc3ffab
avi <unknown>
2004-09-30 21:56:00 +0000

    no message

---
+++ MCOrganizationDefinition/instance/as yet unclassified/commonPrefix.cs
@@ -0,0 +1,12 @@
+commonPrefix
+	| stream |
+	categories isEmpty ifTrue: [^ ''].
+	
+	stream _ String new writeStream.
+	categories first withIndexDo:
+		[:c :i|
+		categories do:
+			[:ea |
+			(ea at: i ifAbsent: []) = c ifFalse: [^ stream contents]].
+		stream nextPut: c].
+	^ stream contents
