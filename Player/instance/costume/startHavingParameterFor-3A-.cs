7a0b357626fc2ed39b7e822ad80abd78c22874c7
sw <unknown>
2004-09-30 04:49:00 +0000

    no message

---
+++ Player/instance/costume/startHavingParameterFor-3A-.cs
@@ -0,0 +1,6 @@
+startHavingParameterFor: aSelector
+	"Start having a parameter for the given selector.  After this change, the script name will change by the addition of a colon."
+
+	| newSelector |
+	self renameScript: aSelector newSelector: (newSelector _ (aSelector, ':') asSymbol).
+	(self scriptEditorFor: newSelector) install
