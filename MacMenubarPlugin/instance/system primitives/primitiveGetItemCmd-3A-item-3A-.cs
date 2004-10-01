58deee292b77c870e54fc6e1a12d6806aef4f7e1
JMM <unknown>
2004-10-01 13:56:00 +0000

    no message

---
+++ MacMenubarPlugin/instance/system primitives/primitiveGetItemCmd-3A-item-3A-.cs
@@ -0,0 +1,14 @@
+primitiveGetItemCmd: menuHandleOop item: anInteger
+	| menuHandle aCharacter |
+	self primitive: 'primitiveGetItemCmd'
+		parameters: #(Oop SmallInteger).
+	self var: 'menuHandle' type: 'MenuHandle'.
+	self var: #aCharacter declareC: 'CharParameter aCharacter'.
+	self var: #ptr type: 'char *'.
+	
+	menuHandle := self cCoerce: (interpreterProxy positive32BitValueOf: menuHandleOop) to: 'MenuHandle'.
+	(self ioCheckMenuHandle: menuHandle) ifFalse: [^interpreterProxy success: false].
+	aCharacter := 0.
+	self cCode: 'GetItemCmd(menuHandle,anInteger,&aCharacter)' inSmalltalk:[menuHandle].
+	^aCharacter asSmallIntegerObj
+
