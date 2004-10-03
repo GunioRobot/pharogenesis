1316776bfc973d2d1a768d6040e190ba8b35ef27
sw <unknown>
2004-10-03 01:27:00 +0000

    no message

---
+++ Player/instance/scripts-2D-standard/includeAtCursor-3A-.cs
@@ -0,0 +1,19 @@
+includeAtCursor: aPlayer 
+	"Add aPlayer to the list of objects logically 'within' me, at my current cursor position. ."
+
+	| aCostume |
+	(aPlayer isNil or: [aPlayer == self]) ifTrue: [^self].
+	(aPlayer class == Text or: [aPlayer class == String]) 
+		ifTrue: 
+			[^ self costume class == TextFieldMorph 
+				ifTrue: [self costume append: aPlayer]
+				ifFalse: [self]].
+	aCostume := self costume topRendererOrSelf.
+	aPlayer costume goHome.	"assure it's in view"
+	(aCostume isKindOf: PasteUpMorph) 
+		ifTrue:
+			[aCostume addMorph: aPlayer costume asElementNumber: self getCursor.
+			aCostume invalidRect: aCostume bounds]
+		ifFalse:
+			[aCostume addMorphBack: aPlayer.
+			self setCursor: aCostume submorphs size]
