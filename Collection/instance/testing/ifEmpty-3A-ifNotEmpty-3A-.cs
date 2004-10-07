1bd7ad2e500cd3de0376936f0460c630edd30aa2
md <unknown>
2004-10-07 14:49:00 +0000

    no message

---
+++ Collection/instance/testing/ifEmpty-3A-ifNotEmpty-3A-.cs
@@ -0,0 +1,5 @@
+ifEmpty: emptyBlock ifNotEmpty: notEmptyBlock
+	"Evaluate emptyBlock if I'm empty, notEmptyBlock otherwise"
+	" If the notEmptyBlock has an argument, eval with the receiver as its argument"
+
+	^ self isEmpty ifTrue: emptyBlock ifFalse: [notEmptyBlock valueWithPossibleArgument: self]
