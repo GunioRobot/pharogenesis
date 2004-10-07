4fc95030d81f69047f0d8662200b95fd04df3623
st <unknown>
2004-10-07 16:24:00 +0000

    no message

---
+++ CollectionTest/instance/testing/testIfNotEmptyifEmpty.cs
@@ -0,0 +1,4 @@
+testIfNotEmptyifEmpty
+	self assert: (empty ifEmpty: [true] ifNotEmpty: [false]).
+	self assert: (nonEmpty ifEmpty: [false] ifNotEmpty: [true]).
+	self assert: (nonEmpty ifEmpty: [false] ifNotEmpty: [:s | s first = #x])
