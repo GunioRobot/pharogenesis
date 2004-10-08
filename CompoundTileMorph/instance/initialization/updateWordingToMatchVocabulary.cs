d3fd4d1bb61b514affd6c042297b518834cd31af
nk <unknown>
2004-10-08 11:56:00 +0000

    no message

---
+++ CompoundTileMorph/instance/initialization/updateWordingToMatchVocabulary.cs
@@ -0,0 +1,7 @@
+updateWordingToMatchVocabulary
+	| labels |
+	labels _ OrderedCollection new.
+	self submorphs do: [:submorph |
+		submorph submorphs do: [:subsubmorph |
+			subsubmorph class == StringMorph ifTrue: [labels add: subsubmorph]]].
+	labels do: [:label | label knownName ifNotNilDo: [ :nm | label acceptValue: nm translated ]]
