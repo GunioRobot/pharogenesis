3ae6f4663d50faa8108691256516b6b474b00054
kfr <unknown>
2004-10-09 10:36:00 +0000

    no message

--- ProjectViewMorph/instance/events/showMouseState-3A-.cs
+++ ProjectViewMorph/instance/events/showMouseState-3A-.cs
@@ -1,17 +1,18 @@
-showMouseState: anInteger
-
-	anInteger = 1 ifTrue: [		"enter"
-		self addMouseActionIndicatorsWidth: 10 color: (Color blue alpha: 0.3).
-		"self showBorderAs: Color blue"
-	].
-
-	anInteger = 2 ifTrue: [		"down"
-		self addMouseActionIndicatorsWidth: 15 color: (Color blue alpha: 0.7).
-		"self showBorderAs: Color red."
-	].
-
-	anInteger = 3 ifTrue: [		"leave"
-		self deleteAnyMouseActionIndicators.
-		"self showBorderAs: Color gray"
-	].
-
+showMouseState: anInteger 
+	| aMorph |
+	(owner isSystemWindow)
+		ifTrue: [aMorph := owner]
+		ifFalse: [aMorph := self].
+	anInteger = 1
+		ifTrue: ["enter"
+			aMorph
+				addMouseActionIndicatorsWidth: 10
+				color: (Color blue alpha: 0.3)].
+	anInteger = 2
+		ifTrue: ["down"
+			aMorph
+				addMouseActionIndicatorsWidth: 15
+				color: (Color blue alpha: 0.7)].
+	anInteger = 3
+		ifTrue: ["leave"
+			aMorph deleteAnyMouseActionIndicators]
