a604c51ba3d97e5f4d2ccb5bfb90d42359297fe4
nk <unknown>
2004-10-13 10:42:00 +0000

    no message

--- KeyboardEvent/instance/printing/printOn-3A-.cs
+++ KeyboardEvent/instance/printing/printOn-3A-.cs
@@ -1,13 +1,8 @@
 printOn: aStream
+	"Print the receiver on a stream"
 
 	aStream nextPut: $[.
-	aStream nextPutAll: type.
-	self controlKeyPressed ifTrue: [
-		aStream nextPutAll: ' ''^'.
-		aStream nextPut: (keyValue + $a asciiValue - 1) asCharacter.
-	] ifFalse: [
-		aStream nextPutAll: ' '''.
-		aStream nextPut: self keyCharacter.
-	].
+	aStream nextPutAll: type; nextPutAll: ' '''.
+	self printKeyStringOn: aStream.
 	aStream nextPut: $'.
-	aStream nextPut: $].
+	aStream nextPut: $]
