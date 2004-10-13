85d90bf383b8f72cde8c3890ff67c5ab301ebb54
ar <unknown>
2004-10-13 17:54:00 +0000

    no message

--- FileUrl/instance/access/pathForFile.cs
+++ FileUrl/instance/access/pathForFile.cs
@@ -1,9 +1,3 @@
 pathForFile
 	"Path using local file system's delimiter.  $\ or $:"
-	| first |
-	^String streamContents: [ :s |
-		first _ true.
-		self path do: [ :p |
-			first ifFalse: [ s nextPut: FileDirectory default pathNameDelimiter ].
-			first _ false.
-			s nextPutAll: p ] ]
+	^FileDirectory default pathFromUrl: self
