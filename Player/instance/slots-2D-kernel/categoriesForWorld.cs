2ee3732bc3bc7168a56bc894db208979aed13fd3
nk <unknown>
2004-10-13 11:34:00 +0000

    no message

--- Player/instance/slots-2D-kernel/categoriesForWorld.cs
+++ Player/instance/slots-2D-kernel/categoriesForWorld.cs
@@ -2,10 +2,9 @@ categoriesForWorld
 	"Answer the list of categories given that the receiver is the Player representing a World"
 
 	| aList |
-	aList _ #(#'color & border' #'pen trails' playfield) asOrderedCollection.
-	self class scripts size > 0 ifTrue:
-		[aList addFirst: #scripts].
-	self slotNames size > 0 ifTrue:
-		[aList addFirst: #'instance variables'].
+	aList _ #(#'color & border' #'pen trails' playfield collections #'stack navigation') asOrderedCollection.
+	aList addFirst: ScriptingSystem nameForScriptsCategory.
+	aList addFirst: ScriptingSystem nameForInstanceVariablesCategory.
+	aList add: #input.
 
 	^ aList
