35f3c9e6ab98f48f2b0155b3b5870277a4cb23b4
nk <unknown>
2004-10-14 11:19:00 +0000

    no message

--- StandardScriptingSystem/instance/utilities/wordingForOperator-3A-.cs
+++ StandardScriptingSystem/instance/utilities/wordingForOperator-3A-.cs
@@ -2,31 +2,48 @@ wordingForOperator: aString
 	"Answer the wording to be seen by the user for the given operator symbol/string"
 
 	| toTest |
-	"StandardScriptingSystem initialize"
 	toTest _ aString asString.
-	#(	(append:				'append')
+	#(	(append:				'include at end')
+		(arrowheadsOnAllPens	'arrowheads on all pens')
 		(beep:					'make sound')
 		(bounce:				'bounce')
 		(clearTurtleTrails		'clear pen trails')
+		(clearOwnersPenTrails	'clear all pen trails')
+		(colorSees				'color  sees')
+		(color:sees:				'color sees')
 		(doMenuItem:			'do menu item')
+		(doScript:				'do')
 		(forward:				'forward by')
-		(moveToward:			'move toward')
 		(goToRightOf:			'align after')
+		(includeAtCursor:		'include at cursor')
 		(isDivisibleBy:			'is divisible by')
 		(liftAllPens				'lift all pens')
 		(lowerAllPens			'lower all pens')
-		(pauseScript:			'pause script')
+		(makeNewDrawingIn:	'start painting in')
 		(max:					'max')
 		(min:					'min')
+		(moveToward:			'move toward')
+		(noArrowheadsOnAllPens	'no arrowheads on pens')
+		(overlapsAny			'overlaps any')
+		(pauseAll:				'pause all')
+		(pauseScript:			'pause script')
+		(prepend:				'include at beginning')
 		(seesColor:				'is over color')
-		(makeNewDrawingIn:	'start painting in')
+		(startAll:				'start all')
 		(startScript:				'start script')
 		(stopProgramatically	'stop')
+		(stopAll:					'stop all')
 		(stopScript:				'stop script')
+		(tellAllSiblings:			'tell all siblings')
+		(tellSelfAndAllSiblings:	'send to all')
 		(turn:					'turn by')
+		(turnToward:				'turn toward')
 		(wearCostumeOf:		'look like'))
 
 	do:
 		[:pair | toTest = pair first ifTrue: [^ pair second]].
 
 	^ toTest
+
+	"StandardScriptingSystem initialize"
+
