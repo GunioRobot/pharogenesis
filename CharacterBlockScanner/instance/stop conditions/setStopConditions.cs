93387fae97997c883ea90c24dbcc5b37a5060c8d
ar <unknown>
2004-10-18 14:30:00 +0000

    no message

--- CharacterBlockScanner/instance/stop conditions/setStopConditions.cs
+++ CharacterBlockScanner/instance/stop conditions/setStopConditions.cs
@@ -2,8 +2,4 @@ setStopConditions
 	"Set the font and the stop conditions for the current run."
 	
 	self setFont.
-	textStyle alignment = Justified ifTrue:[
-		"Make a local copy of stop conditions so we don't modify the default"
-		stopConditions == DefaultStopConditions 
-			ifTrue:[stopConditions _ stopConditions copy].
-		stopConditions at: Space asciiValue + 1 put: #paddedSpace]
+	self setConditionArray: (alignment = Justified ifTrue: [#paddedSpace]).
