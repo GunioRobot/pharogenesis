17ac7f4034d6419ef9a42b90bb4c08359170f429
ar <unknown>
2004-10-18 14:31:00 +0000

    no message

--- CanvasCharacterScanner/instance/stop conditions/setStopConditions.cs
+++ CanvasCharacterScanner/instance/stop conditions/setStopConditions.cs
@@ -2,6 +2,4 @@ setStopConditions
 	"Set the font and the stop conditions for the current run."
 	
 	self setFont.
-	stopConditions 
-		at: Space asciiValue + 1 
-		put: (textStyle alignment = Justified ifTrue: [#paddedSpace])
+	self setConditionArray: (alignment = Justified ifTrue: [#paddedSpace]).
