fc34fe085143b25a60f23ce3153c8f75c4ff7205
brp <unknown>
2004-10-21 17:40:00 +0000

    no message

--- ExceptionTester/instance/testing/runTest-3A-.cs
+++ ExceptionTester/instance/testing/runTest-3A-.cs
@@ -1,15 +1,17 @@
 runTest: aSelector
 
-	| expectedResult |
-	[expectedResult := self perform: (aSelector, #Results) asSymbol.
-	self logTest: aSelector.
-	self clearLog.
-	self perform: aSelector.
-	] on: MyTestError
-	  do: 
-		[ :ex |
-		self log: 'Unhandled Exception'.
-		ex return: nil].
-	self log = expectedResult
+	| actualResult expectedResult |
+	[ self 
+		logTest: aSelector;
+		clearLog;
+		perform: aSelector ]
+			on: MyTestError do: 
+				[ :ex | self log: 'Unhandled Exception'.
+					ex return: nil ].
+
+	actualResult	:= self log.
+	expectedResult := self perform: (aSelector, #Results) asSymbol.
+
+	actualResult = expectedResult
 		ifTrue: [self logTestResult: 'succeeded']
-		ifFalse: [self logTestResult: 'failed']
+		ifFalse: [self logTestResult: 'failed' ].
