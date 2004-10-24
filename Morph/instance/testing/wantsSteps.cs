74dc4f754513ffc705e4cfc8915131d89abc05c5
sw <unknown>
2004-10-24 15:28:00 +0000

    no message

---
+++ Morph/instance/testing/wantsSteps.cs
@@ -0,0 +1,10 @@
+wantsSteps
+	"Return true if the receiver overrides the default Morph step method."
+	"Details: Find first class in superclass chain that implements #step and return true if it isn't class Morph."
+
+	| c |
+	self isPartsDonor ifTrue: [^ false].
+	(self == self topRendererOrSelf) ifTrue: [self player wantsSteps ifTrue: [^ true]].
+	c _ self class.
+	[c includesSelector: #step] whileFalse: [c _ c superclass].
+	^ c ~= Morph
