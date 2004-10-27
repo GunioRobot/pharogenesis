35e77704babb75af184de10fb4d3651b7707d2ee
md <unknown>
2004-10-27 14:24:00 +0000

    no message

--- InterpreterSimulator/instance/float primitives/storeFloatAt-3A-from-3A-.cs
+++ InterpreterSimulator/instance/float primitives/storeFloatAt-3A-from-3A-.cs
@@ -1,4 +1,4 @@
-storeFloatAt: floatBitsAddress from: aFloat.
+storeFloatAt: floatBitsAddress from: aFloat
 
 	self longAt: floatBitsAddress put: (aFloat at: 1).
 	self longAt: floatBitsAddress+4 put: (aFloat at: 2).
