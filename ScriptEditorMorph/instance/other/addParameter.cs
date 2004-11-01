a9e9abb8fe3e382f15cdc18c97332a664cdcd68d
sw <unknown>
2004-11-01 22:50:00 +0000

    no message

---
+++ ScriptEditorMorph/instance/other/addParameter.cs
@@ -0,0 +1,4 @@
+addParameter
+	"Transform the receiver so that it bears a parameter.  This will require a selector change, e.g. from #script3 to #script3:"
+
+	playerScripted startHavingParameterFor: scriptName asSymbol
