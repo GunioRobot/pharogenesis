f8ff215b30873fd0667bdab5043a4a600b2190d0
nk <unknown>
2004-11-03 09:26:00 +0000

    no message

--- WarpBlt/instance/system simulation/warpBitsSimulated.cs
+++ WarpBlt/instance/system simulation/warpBitsSimulated.cs
@@ -1,4 +1,7 @@
 warpBitsSimulated
 	"Simulate WarpBlt"
+
+	cellSize < 1 ifTrue: [ ^self error: 'cellSize must be >= 1' ].
+
 	self warpBitsSimulated: cellSize
 		sourceMap: (sourceForm colormapIfNeededForDepth: 32).
