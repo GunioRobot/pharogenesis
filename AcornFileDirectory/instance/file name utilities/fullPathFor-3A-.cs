3f841fb112d721aaaf15b2647efcd7a9cf3b5558
tpr <unknown>
2004-11-05 13:08:00 +0000

    no message

--- AcornFileDirectory/instance/file name utilities/fullPathFor-3A-.cs
+++ AcornFileDirectory/instance/file name utilities/fullPathFor-3A-.cs
@@ -1,4 +1,7 @@
 fullPathFor: path
-	path isEmpty ifTrue:[^pathName].
+	"if the arg is an empty string, just return my path name converted via the language stuff. 
+If the arg seems to be a  rooted path, return it raw, assuming it is already ok.
+Otherwise cons up a path"
+	path isEmpty ifTrue:[^pathName asSqueakPathName].
 	((path includes: $$ ) or:[path includes: $:]) ifTrue:[^path].
-	^pathName, self slash, path
+	^pathName asSqueakPathName, self slash, path
