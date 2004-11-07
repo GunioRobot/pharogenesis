37240aaec5110445a0b494a51837c2c4398a960f
cwp <unknown>
2004-11-07 14:47:00 +0000

    no message

---
+++ MCVersionTest/instance/asserting/assert-3A-orders-3A-as-3A-unresolved-3A-.cs
@@ -0,0 +1,11 @@
+assert: aSelector orders: sexpr as: expected unresolved: unresolved
+	| missing visited |
+	visited := OrderedCollection new.
+	missing := OrderedCollection new.
+	version := self versionFromTree: sexpr.
+	version 
+		perform: aSelector 
+		with: [:ea | visited add: ea info name]
+		with: [:ea | missing add: ea name].
+	self assert: visited asArray = expected.
+	self assert: missing asArray = unresolved.
