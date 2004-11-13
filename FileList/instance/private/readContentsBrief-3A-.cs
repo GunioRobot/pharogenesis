3ebf051c448d532ba10b3a98df0ac138f699a935
tlk <unknown>
2004-11-13 19:01:00 +0000

    no message

--- FileList/instance/private/readContentsBrief-3A-.cs
+++ FileList/instance/private/readContentsBrief-3A-.cs
@@ -3,24 +3,25 @@ readContentsBrief: brevityFlag
 	| f fileSize first5000 |
 
 	brevityFlag ifTrue: [
-		(directory isKindOf: ServerDirectory) ifTrue: [^ self readServerBrief]].
-	f _ directory oldFileOrNoneNamed: self fullName.
-	f ifNil: [^ 'For some reason, this file cannot be read'].
-	(brevityFlag not or: [(fileSize _ f size) <= 100000]) ifTrue:
-		[contents _ f contentsOfEntireFile.
-		brevityState _ #fullFile.   "don't change till actually read"
+		directory isRemoteDirectory ifTrue: [^ self readServerBrief]].
+	f := directory oldFileOrNoneNamed: self fullName.
+	f ifNil: [^ 'For some reason, this file cannot be read' translated].
+	f converter: (self defaultEncoderFor: self fullName).
+	(brevityFlag not or: [(fileSize := f size) <= 100000]) ifTrue:
+		[contents := f contentsOfEntireFile.
+		brevityState := #fullFile.   "don't change till actually read"
 		^ contents].
 
 	"if brevityFlag is true, don't display long files when first selected"
-	first5000 _ f next: 5000.
+	first5000 := f next: 5000.
 	f close.
-	contents _ 'File ''', fileName, ''' is ', fileSize printString, ' bytes long.
+	contents := 'File ''{1}'' is {2} bytes long.
 You may use the ''get'' command to read the entire file.
 
 Here are the first 5000 characters...
 ------------------------------------------
-', first5000 , '
+{3}
 ------------------------------------------
-... end of the first 5000 characters.'.
-	brevityState _ #briefFile.   "don't change till actually read"
+... end of the first 5000 characters.' translated format: {fileName. fileSize. first5000}.
+	brevityState := #briefFile.   "don't change till actually read"
 	^ contents.
