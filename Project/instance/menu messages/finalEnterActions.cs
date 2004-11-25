0c0c25f0fda3ec042935a7560808672f63838004
ar <unknown>
2004-11-25 15:36:00 +0000

    no message

--- Project/instance/menu messages/finalEnterActions.cs
+++ Project/instance/menu messages/finalEnterActions.cs
@@ -1,20 +1,45 @@
 finalEnterActions
-	| navigator armsLengthCmd navType |
+	"Perform the final actions necessary as the receiver project is entered"
+
+	| navigator armsLengthCmd navType thingsToUnhibernate fixBlock |
+
+	self projectParameters 
+		at: #projectsToBeDeleted 
+		ifPresent: [ :projectsToBeDeleted |
+			self removeParameter: #projectsToBeDeleted.
+			projectsToBeDeleted do: [ :each | 
+				Project deletingProject: each.
+				each removeChangeSetIfPossible]].
+
+	thingsToUnhibernate _ world valueOfProperty: #thingsToUnhibernate ifAbsent: [#()].
+	(thingsToUnhibernate anySatisfy:[:each| 
+		each isMorph and:[each hasProperty: #needsLayoutFixed]]) 
+			ifTrue:[fixBlock := self displayFontProgress].
+	thingsToUnhibernate do: [:each | each unhibernate].
+	world removeProperty: #thingsToUnhibernate.
+
+	fixBlock ifNotNil:[
+		fixBlock value.
+		world fullRepaintNeeded.
+	].
 
 	navType _ ProjectNavigationMorph preferredNavigator.
 	armsLengthCmd _ self parameterAt: #armsLengthCmd ifAbsent: [nil].
 	navigator _ world findA: navType.
-	Preferences showProjectNavigator & navigator isNil ifTrue: [
-		(navigator _ navType new)
+	(Preferences classicNavigatorEnabled and: [Preferences showProjectNavigator and: [navigator isNil]]) ifTrue:
+		[(navigator _ navType new)
 			bottomLeft: world bottomLeft;
-			openInWorld: world.
-	].
-	navigator notNil & armsLengthCmd notNil ifTrue: [
-		navigator color: Color lightBlue
-	].
-	armsLengthCmd notNil ifTrue: [
-		Preferences showFlapsWhenPublishing
-			ifFalse:[self flapsSuppressed: true.
-					navigator ifNotNil:[navigator visible: false]].
-		armsLengthCmd openInWorld: world
-	].
+			openInWorld: world].
+	navigator notNil & armsLengthCmd notNil ifTrue:
+		[navigator color: Color lightBlue].
+	armsLengthCmd notNil ifTrue:
+		[Preferences showFlapsWhenPublishing
+			ifFalse:
+				[self flapsSuppressed: true.
+				navigator ifNotNil:	[navigator visible: false]].
+		armsLengthCmd openInWorld: world].
+	Smalltalk isMorphic ifTrue:
+		[world reformulateUpdatingMenus.
+		world presenter positionStandardPlayer].
+
+	WorldState addDeferredUIMessage: [self startResourceLoading].
