f5fdcfde2d5e46554dac96132d69ad8452416c74
tk <unknown>
2004-11-26 05:56:00 +0000

    no message

--- ChangeSet/instance/converting/convertToCurrentVersion-3A-refStream-3A-.cs
+++ ChangeSet/instance/converting/convertToCurrentVersion-3A-refStream-3A-.cs
@@ -1,8 +1,10 @@
 convertToCurrentVersion: varDict refStream: smartRefStrm
-	
+
 	"major change - 4/4/2000"
-	varDict at: 'classChanges' ifPresent: [ :x | 
-		self convertApril2000: varDict using: smartRefStrm
-	].
+	| newish |
+	varDict at: 'classChanges' ifPresent: [ :x |
+		newish _ self convertApril2000: varDict using: smartRefStrm.
+		newish == self ifFalse: [^ newish].
+		].
 	^super convertToCurrentVersion: varDict refStream: smartRefStrm.
 
