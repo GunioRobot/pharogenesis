7e39c623f9559956df0d8bab99f216abc68d5252
tk <unknown>
2004-11-29 16:54:00 +0000

    no message

---
+++ TextMorph/instance/objects from disk/fixUponLoad-3A-seg-3A-.cs
@@ -0,0 +1,12 @@
+fixUponLoad: aProject seg: anImageSegment
+	"We are in an old project that is being loaded from disk.
+Fix up conventions that have changed."
+
+	| substituteFont |
+	substituteFont _ aProject projectParameters at:
+#substitutedFont ifAbsent: [#none].
+	(substituteFont ~~ #none and: [self textStyle fontArray
+includes: substituteFont])
+			ifTrue: [ self fit ].
+
+	^ super fixUponLoad: aProject seg: anImageSegment
