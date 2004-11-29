85fb2730220911b9d5d697109615f76019d5c827
tpr <unknown>
2004-11-29 14:57:00 +0000

    no message

--- ObjectMemory/instance/become/restoreHeadersAfterBecoming-3A-with-3A-.cs
+++ ObjectMemory/instance/become/restoreHeadersAfterBecoming-3A-with-3A-.cs
@@ -1,19 +1,25 @@
-restoreHeadersAfterBecoming: list1 with: list2
-	"Restore the headers of all oops in both lists. Exchange their hash bits so becoming objects in identity sets and dictionaries doesn't change their hash value."
-
+restoreHeadersAfterBecoming: list1 with: list2 
+	"Restore the headers of all oops in both lists. Exchange their hash bits so
+	becoming objects in identity sets and dictionaries doesn't change their
+	hash value."
+	"See also prepareForwardingTableForBecoming:with:woWay: for notes
+	regarding the case
+	of oop1 = oop2"
 	| fieldOffset oop1 oop2 hdr1 hdr2 |
-	fieldOffset _ self lastPointerOf: list1.
-	[fieldOffset >= BaseHeaderSize] whileTrue: [
-		oop1 _ self longAt: list1 + fieldOffset.
-		oop2 _ self longAt: list2 + fieldOffset.
-		self restoreHeaderOf: oop1.
-		self restoreHeaderOf: oop2.
-		"Exchange has bits of the two objects."
-		hdr1 _ self longAt: oop1.
-		hdr2 _ self longAt: oop2.
-		self longAt: oop1 put:
-			((hdr1 bitAnd: AllButHashBits) bitOr: (hdr2 bitAnd: HashBits)).
-		self longAt: oop2 put:
-			((hdr2 bitAnd: AllButHashBits) bitOr: (hdr1 bitAnd: HashBits)).
-		fieldOffset _ fieldOffset - 4.
-	].
+	fieldOffset := self lastPointerOf: list1.
+	[fieldOffset >= BaseHeaderSize]
+		whileTrue: [oop1 := self longAt: list1 + fieldOffset.
+			oop2 := self longAt: list2 + fieldOffset.
+			oop1 = oop2
+				ifFalse: [self restoreHeaderOf: oop1.
+					self restoreHeaderOf: oop2.
+					"Exchange hash bits of the two objects."
+					hdr1 := self longAt: oop1.
+					hdr2 := self longAt: oop2.
+					self
+						longAt: oop1
+						put: ((hdr1 bitAnd: AllButHashBits) bitOr: (hdr2 bitAnd: HashBits)).
+					self
+						longAt: oop2
+						put: ((hdr2 bitAnd: AllButHashBits) bitOr: (hdr1 bitAnd: HashBits))].
+			fieldOffset := fieldOffset - 4]
