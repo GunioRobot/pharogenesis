7703285baf7a30cc6f4858787c5ebcd2d71b32a8
tk <unknown>
2004-11-29 15:04:00 +0000

    no message

---
+++ Object/instance/objects from disk/fixUponLoad-3A-seg-3A-.cs
@@ -0,0 +1,4 @@
+fixUponLoad: aProject seg: anImageSegment
+	"change the object due to conventions that have changed on
+the project level.  (sent to all objects in the incoming project).
+Specific classes should reimplement this."
