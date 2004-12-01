a3d00e747a6877d5ce396c304d90ad7207e5716e
huma <unknown>
2004-12-01 18:53:00 +0000

    no message

--- Preferences/class/hard-2D-coded prefs/isFlagship.cs
+++ Preferences/class/hard-2D-coded prefs/isFlagship.cs
@@ -1,4 +1,4 @@
 isFlagship
-	"Manually change this to return true if you wish your system to be marked as a 'flagship'.  The intent here is to allow an update to query this flag before undertaking some radical do-it that might clobber important content is such an image."
+	"Manually change this to return true if you wish your system to be marked as a 'flagship'.  The intent here is to allow an update to query this flag before undertaking some radical do-it that might clobber important content in such an image."
 
 	^ false
