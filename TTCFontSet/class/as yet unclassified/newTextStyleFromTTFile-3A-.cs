96383036980325c457f9e79a906126f6b276155a
yo <unknown>
2004-12-02 12:58:00 +0000

    no message

---
+++ TTCFontSet/class/as yet unclassified/newTextStyleFromTTFile-3A-.cs
@@ -0,0 +1,12 @@
+newTextStyleFromTTFile: fileName
+"
+	TTCFontReader encodingTag: JapaneseEnvironment leadingChar.
+	self newTextStyleFromTTFile: 'C:\WINDOWS\Fonts\msmincho.TTC'
+
+	TTCFontReader encodingTag: 0.
+	self newTextStyleFromTTFile: 'C:\WINDOWS\Fonts\symbol.ttf'
+"
+
+	| description |
+	description _ TTCFontDescription addFromTTFile: fileName.
+	^ self newTextStyleFromTT: description.
