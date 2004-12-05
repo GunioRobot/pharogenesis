19e3097d997265c205a3f070cd8e6be7637a9f20
tak <unknown>
2004-12-05 14:36:00 +0000

    no message

--- TileMorph/instance/arrows/arrowAction-3A-.cs
+++ TileMorph/instance/arrows/arrowAction-3A-.cs
@@ -1,28 +1,15 @@
-arrowAction: delta
-	"Do what is appropriate when an arrow on the tile is pressed; delta will be +1 or -1"
-
-	| index aList |
-	owner ifNil: [^ self].
-	(type == #literal and: [literal isNumber])
-		ifTrue:
-			[self literal: literal + delta. ^ self layoutChanged.].
-
-	(type == #literal and: [literal isKindOf: Boolean])
-		ifTrue: [self literal: literal not.  ^ self layoutChanged].
-
-	operatorOrExpression ifNotNil:
-		[aList _ #(+ - * / // \\ min: max:).
-		index _ aList indexOf: operatorOrExpression.
-		index  > 0 ifTrue:
-			[self setOperatorAndUseArrows: (aList atWrap: index + delta)].
-
-		aList _ #(< <= = ~= > >= isDivisibleBy:).
-		index _ aList indexOf: operatorOrExpression.
-		index  > 0 ifTrue:
-			[owner firstSubmorph type = #number 
-				ifTrue: [self setOperator: (aList atWrap: index + delta)]
-				ifFalse: [self setOperator: (#(= ~=) atWrap: index - 2 + delta)]].
-						"Color does not understand <"
-			submorphs last setBalloonText: (ScriptingSystem helpStringForOperator: operatorOrExpression).
-			^ self acceptNewLiteral]
-	
+arrowAction: delta 
+	"Do what is appropriate when an arrow on the tile is pressed; delta will  
+	be +1 or -1"
+	| index options |
+	(type == #literal
+			and: [literal isNumber])
+		ifTrue: [self value: literal + delta]
+		ifFalse: [options _ self options
+						ifNil: [^ self].
+			index _ (options first indexOf: self value)
+						+ delta.
+			self
+				value: (options first atWrap: index).
+			submorphs last
+				setBalloonText: (options second atWrap: index)]
