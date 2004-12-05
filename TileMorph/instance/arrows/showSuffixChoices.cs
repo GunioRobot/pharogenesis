881fcd1c1a8c863f78fba990359fd4d7b5249d1e
tak <unknown>
2004-12-05 15:33:00 +0000

    no message

--- TileMorph/instance/arrows/showSuffixChoices.cs
+++ TileMorph/instance/arrows/showSuffixChoices.cs
@@ -7,21 +7,20 @@ showSuffixChoices
 	(type == #literal) & (literal isNumber) ifTrue: ["Tile is a constant number"
 		phrase lastSubmorph == owner "pad"
 			ifTrue: ["we are adding the first time (at end of our phrase)"
-				plusPhrase _ self presenter phraseForReceiver: literal 
-						op: #+ arg: 1 resultType: #number.
+				plusPhrase _ self phraseForOp: #+ arg: 1 resultType: #Number.
 				plusPhrase submorphs second submorphs last setBalloonText: (ScriptingSystem helpStringForOperator: #+).
 				owner acceptDroppingMorph: plusPhrase event: self primaryHand lastEvent.
 				num _ plusPhrase firstSubmorph firstSubmorph.
 				num deleteSuffixArrow]].
 
 	type == #operator ifTrue: ["Tile is accessor of an expression"
-		phrase resultType == #number ifTrue:
+		phrase resultType == #Number ifTrue:
 			[outer _ phrase ownerThatIsA: PhraseTileMorph.
 			pad _ self ownerThatIsA: TilePadMorph.
 			outer ifNotNil:
 				[outer lastSubmorph == pad ifTrue: [ "first time"
 					plusPhrase _ self presenter phraseForReceiver: 1 
-							op: #+ arg: 1 resultType: #number.
+							op: #+ arg: 1 resultType: #Number.
 					plusPhrase submorphs second submorphs last setBalloonText: (ScriptingSystem helpStringForOperator: #+).
 					pad acceptDroppingMorph: plusPhrase event: self primaryHand lastEvent.
 					plusPhrase firstSubmorph removeAllMorphs; addMorph: phrase.	"car's heading"
