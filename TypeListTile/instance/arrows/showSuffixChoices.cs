99004dfcdd414077b2403b7311a86939241c44b6
tak <unknown>
2004-12-07 14:28:00 +0000

    no message

---
+++ TypeListTile/instance/arrows/showSuffixChoices.cs
@@ -0,0 +1,4 @@
+showSuffixChoices
+	"When the user clicks on the suffix arrow, put up the type-choices menu"
+	"A bit dirty hack, avoided testing whether the down arrow includes mouse point"
+	 super showOptions
