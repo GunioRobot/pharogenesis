f61a1012d781d8503dd7ec1917dcbb4ab7842143
ar <unknown>
2004-12-14 12:20:00 +0000

    no message

--- WonderlandStyle/class/class initialization/initialize.cs
+++ WonderlandStyle/class/class initialization/initialize.cs
@@ -2,13 +2,13 @@ initialize
 	"Add the style constants gently, beginGently, endGently, and abruptly to the WonderlandConstants dictionary"
 
 	| dict |
-
 	dict _ Smalltalk at: #WonderlandConstants.
-	dict at: 'gently' put:
+	#(gently beginGently endGently abruptly) do:[:each| dict declare: each from: Undeclared].
+	dict at: #gently put:
 			[:elapsed :duration | WonderlandStyle gentlyAfter: elapsed outOf: duration ].
-	dict at: 'beginGently' put:
+	dict at: #beginGently put:
 			[:elapsed :duration | WonderlandStyle beginGentlyAfter: elapsed outOf: duration ].
-	dict at: 'endGently' put:
+	dict at: #endGently put:
 			[:elapsed :duration | WonderlandStyle endGentlyAfter: elapsed outOf: duration ].
-	dict at: 'abruptly' put:
+	dict at: #abruptly put:
 			[:elapsed :duration | WonderlandStyle abruptlyAfter: elapsed outOf: duration ].
