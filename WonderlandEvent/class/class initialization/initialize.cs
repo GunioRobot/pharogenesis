d8c63df049bab47d6d83892463ba767bf371019f
ar <unknown>
2004-12-14 12:22:00 +0000

    no message

--- WonderlandEvent/class/class initialization/initialize.cs
+++ WonderlandEvent/class/class initialization/initialize.cs
@@ -1,13 +1,9 @@
 initialize
 	"Add some new constants to the WonderlandConstants shared pool"
 
-	WonderlandConstants at: 'leftMouseDown' put: 'leftMouseDown'.
-	WonderlandConstants at: 'leftMouseUp' put: 'leftMouseUp'.
-	WonderlandConstants at: 'rightMouseDown' put: 'rightMouseDown'.
-	WonderlandConstants at: 'rightMouseUp' put: 'rightMouseUp'.
-	WonderlandConstants at: 'leftMouseClick' put: 'leftMouseUp'.
-	WonderlandConstants at: 'rightMouseClick' put: 'rightMouseUp'.
-	WonderlandConstants at: 'mouseMove' put: 'mouseMove'.
-	WonderlandConstants at: 'keyPress' put: 'keyPress'.
-	
- 
+	| dict |
+	dict _ Smalltalk at: #WonderlandConstants.
+	#(leftMouseDown leftMouseUp rightMouseDown rightMouseUp leftMouseClick rightMouseClick mouseMove keyPress) do:[:each| 
+		dict declare: each from: Undeclared.
+		dict at: each put: each.
+	].
