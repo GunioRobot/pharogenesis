0b3137787c6df9445c450a539726d81e902e9291
tak <unknown>
2004-12-20 10:23:00 +0000

    no message

--- StrikeFont/class/instance creation/passwordFontSize-3A-.cs
+++ StrikeFont/class/instance creation/passwordFontSize-3A-.cs
@@ -1,10 +1,2 @@
-passwordFontSize: aSize
-	| aFont newXTable newGlyphs |
-	aFont _ (StrikeFont familyName: #NewYork10 size: aSize) copy.
-	newXTable _ aFont xTable copy.
-	newGlyphs _ aFont glyphs copy.
-	aFont instVarNamed: 'xTable' put: newXTable.
-	aFont instVarNamed: 'glyphs' put: newGlyphs.
-	aFont minAscii to: aFont maxAscii do: [:ascii |
-		aFont characterFormAt: ascii asCharacter put: (aFont characterFormAt: $*)].
-	^aFont
+passwordFontSize: aSize 
+	^ FixedFaceFont new passwordFont fontSize: aSize
