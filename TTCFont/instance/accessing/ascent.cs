9d3daeaea162b25e252d94a15b67ae754ab03513
tak <unknown>
2004-12-22 02:28:00 +0000

    no message

---
+++ TTCFont/instance/accessing/ascent.cs
@@ -0,0 +1,7 @@
+ascent
+	| ascent |
+	ascent := ttcDescription ascender * self pixelSize // (ttcDescription ascender - ttcDescription descender) * Scale y.
+	^ (fallbackFont notNil
+			and: [fallbackFont ascent > ascent])
+		ifTrue: [fallbackFont ascent]
+		ifFalse: [ascent]
