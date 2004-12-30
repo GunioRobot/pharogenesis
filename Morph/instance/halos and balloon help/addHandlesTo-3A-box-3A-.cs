b19af815e56046225390fffebf9f9ec383acb254
sw <unknown>
2004-12-30 02:53:00 +0000

    no message

--- Morph/instance/halos and balloon help/addHandlesTo-3A-box-3A-.cs
+++ Morph/instance/halos and balloon help/addHandlesTo-3A-box-3A-.cs
@@ -1,6 +1,20 @@
 addHandlesTo: aHaloMorph box: box
+	"Add halo handles to the halo.  Apply the halo filter if appropriate"
+
+	| wantsIt aSelector |
 	aHaloMorph haloBox: box.
-	Preferences haloSpecifications do:
+	Preferences haloSpecifications  do:
 		[:aSpec | 
-			aHaloMorph perform: aSpec addHandleSelector with: aSpec].
+			aSelector _  aSpec addHandleSelector.
+			wantsIt _ Preferences selectiveHalos
+				ifTrue:
+					[self wantsHaloHandleWithSelector: aSelector inHalo: aHaloMorph]
+				ifFalse:
+					[true].
+			wantsIt ifTrue:
+				[(#(addMakeSiblingHandle: addDupHandle:) includes: aSelector) ifTrue:
+					[wantsIt _ self preferredDuplicationHandleSelector = aSelector].
+			wantsIt ifTrue:
+				[aHaloMorph perform: aSelector with: aSpec]]].
+
 	aHaloMorph innerTarget addOptionalHandlesTo: aHaloMorph box: box
