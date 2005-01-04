3580e65a9926be7f9551e8d3a7c1830b27bfff98
sw <unknown>
2005-01-04 02:20:00 +0000

    no message

--- StandardScriptingSystem/instance/universal slots & scripts/acceptableSlotNameFrom-3A-forSlotCurrentlyNamed-3A-asSlotNameIn-3A-world-3A-.cs
+++ StandardScriptingSystem/instance/universal slots & scripts/acceptableSlotNameFrom-3A-forSlotCurrentlyNamed-3A-asSlotNameIn-3A-world-3A-.cs
@@ -4,18 +4,15 @@ acceptableSlotNameFrom: originalString forSlotCurrentlyNamed: currentName asSlot
 	| aString stemAndSuffix proscribed stem suffix putative |
 	aString _ originalString asIdentifier: false.  "get an identifier not lowercase"
 	stemAndSuffix _ aString stemAndNumericSuffix.
-	proscribed _ #(self super thisContext costume costumes dependents true false size), aPlayer class allInstVarNames.
+	proscribed _ #(self super thisContext costume costumes dependents #true #false size), aPlayer class allInstVarNames.
 
 	stem _ stemAndSuffix first.
 	suffix _ stemAndSuffix last.
 	putative _ aString asSymbol.
 	
 	[(putative ~~ currentName) and: [(proscribed includes: putative)
-		or:	[(aPlayer class tileScriptNames includes: putative)
-		or: [(aPlayer respondsTo: putative)
-		or: [(SystemSlotDictionary includesKey: putative)
-		or: [(ReservedScriptNames includes: putative)
-		or:	[Smalltalk includesKey: putative]]]]]]]
+		or:	[(aPlayer respondsTo: putative)
+		or:	[Smalltalk includesKey: putative]]]]
 	whileTrue:
 		[suffix _ suffix + 1.
 		putative _ (stem, suffix printString) asSymbol].
