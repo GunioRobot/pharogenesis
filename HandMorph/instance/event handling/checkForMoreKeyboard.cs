bb1302161fa3ba6476c099db1f057554538112b6
tpr <unknown>
2005-01-05 17:34:00 +0000

    no message

--- HandMorph/instance/event handling/checkForMoreKeyboard.cs
+++ HandMorph/instance/event handling/checkForMoreKeyboard.cs
@@ -1,5 +1,9 @@
 checkForMoreKeyboard
 	"Quick check for more keyboard activity -- Allows, eg, many characters
 	to be accumulated into a single replacement during type-in."
-	self flag: #arNote. "Will not work if we don't examine event queue in Sensor"
-	^nil
+
+	| evtBuf |
+	self flag: #arNote.	"Will not work if we don't examine event queue in Sensor"
+	evtBuf := Sensor peekKeyboardEvent.
+	evtBuf ifNil: [^nil].
+	^self generateKeyboardEvent: evtBuf
