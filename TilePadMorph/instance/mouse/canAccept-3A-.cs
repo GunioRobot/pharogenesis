092d595ad3978554b3a7b520903de7db9e03c045
sw <unknown>
2005-01-06 02:41:00 +0000

    no message

--- TilePadMorph/instance/mouse/canAccept-3A-.cs
+++ TilePadMorph/instance/mouse/canAccept-3A-.cs
@@ -1,6 +1,7 @@
 canAccept: aMorph
+	"Answer whether this pad can accept the given morph"
 
-	((aMorph isKindOf: PhraseTileMorph) or:
-	 [aMorph isKindOf: TileMorph]) ifTrue: [
-		^ aMorph resultType == type].
+	((aMorph isKindOf: PhraseTileMorph) or: [aMorph isKindOf: TileMorph orOf: WatcherWrapper]) 		ifTrue:
+			[^ (aMorph resultType capitalized = self type capitalized "for bkwd compat") "or:
+				[(aMorph resultType == #unknown) and: [type == #Player]]"].
 	^ false
