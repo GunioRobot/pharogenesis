b71c26d7a0e5396fb0e01802aba0e1e6df4cde80
sw <unknown>
2005-01-06 04:34:00 +0000

    no message

--- PhraseTileMorph/instance/mouse/morphToDropInPasteUp-3A-.cs
+++ PhraseTileMorph/instance/mouse/morphToDropInPasteUp-3A-.cs
@@ -1,22 +1,33 @@
 morphToDropInPasteUp: aPasteUp
 	"Answer the morph to drop in aPasteUp, given that the receiver is the putative droppee"
 
-	| actualObject itsSelector aScriptor adjustment handy |
+	| actualObject itsSelector aScriptor pos aWatcher op |
 
-	self isCommand ifFalse: [^ self].
-	(actualObject _ self actualObject) ifNil: [^ self].
+	((actualObject _ self actualObject) isNil or: [actualObject costume isInWorld not]) ifTrue: [^ self].
+	self isCommand ifFalse:  "Can't expand to a scriptor, but maybe launch a watcher..."
+		[^ (Preferences dropProducesWatcher and: [(#(unknown command) includes: self resultType) not] and:
+			[(op _ self operatorTile operatorOrExpression) notNil] and: [op numArgs = 0] and: [(Vocabulary gettersForbiddenFromWatchers includes: op) not])
+			ifTrue:
+				[aWatcher _ self associatedPlayer fancyWatcherFor: op.
+				aWatcher position: self position]
+			ifFalse:
+				[self]].
+
 	self justGrabbedFromViewer ifFalse: [^ self].
 	actualObject assureUniClass.
 	itsSelector _ self userScriptSelector.
+	pos _ self position.
 	aScriptor _ itsSelector isEmptyOrNil
 		ifFalse:
-			[adjustment _ 0@0.
-			actualObject scriptEditorFor: itsSelector]
+			[actualObject scriptEditorFor: itsSelector]
 		ifTrue:
 			["It's a system-defined selector; construct an anonymous scriptor around it"
-			adjustment _ 60 @ 20.
 			actualObject newScriptorAround: self].
-
-	handy _ aPasteUp primaryHand.
-	aScriptor ifNotNil: [aScriptor position: handy position - adjustment].
-	^ aScriptor ifNil: [self]
+	aScriptor ifNil:[^self].
+	(self hasOwner: aScriptor) ifTrue:[
+		aScriptor fullBounds. "force layout"
+		aScriptor position: pos - self position.
+	] ifFalse:[
+		aScriptor position: self position.
+	].
+	^ aScriptor
