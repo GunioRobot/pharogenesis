67f2e70ed0998be0f2d667634c5bde6a99ddef54
yo <unknown>
2005-01-06 22:56:00 +0000

    no message

---
+++ MultiDisplayScanner/instance/scanning/placeEmbeddedObject-3A-.cs
@@ -0,0 +1,21 @@
+placeEmbeddedObject: anchoredMorph
+	anchoredMorph relativeTextAnchorPosition ifNotNil:[
+		anchoredMorph position: 
+			anchoredMorph relativeTextAnchorPosition +
+			(anchoredMorph owner textBounds origin x @ 0)
+			- (0@morphicOffset y) + (0@lineY).
+		^true
+	].
+	(super placeEmbeddedObject: anchoredMorph) ifFalse: [^ false].
+	anchoredMorph isMorph ifTrue: [
+		anchoredMorph position: ((destX - anchoredMorph width)@lineY) - morphicOffset
+	] ifFalse: [
+		destY _ lineY.
+		baselineY _ lineY + anchoredMorph height..
+		runX _ destX.
+		anchoredMorph 
+			displayOn: bitBlt destForm 
+			at: destX - anchoredMorph width @ destY
+			clippingBox: bitBlt clipRect
+	].
+	^ true
