c915ce3906e002f21d89559d9570477eb14e102f
yo <unknown>
2005-01-07 15:01:00 +0000

    no message

---
+++ StrikeFont/instance/displaying/displayString-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-baselineY-3A-.cs
@@ -0,0 +1,12 @@
+displayString: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint kern: kernDelta baselineY: baselineY
+	"Draw the given string from startIndex to stopIndex 
+	at aPoint on the (already prepared) BitBlt."
+	
+	(aString class == MultiString) ifTrue: [^ self displayMultiString: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint kern: kernDelta baselineY: baselineY.].
+
+	^ aBitBlt displayString: aString 
+			from: startIndex 
+			to: stopIndex 
+			at: aPoint 
+			strikeFont: self
+			kern: kernDelta.
