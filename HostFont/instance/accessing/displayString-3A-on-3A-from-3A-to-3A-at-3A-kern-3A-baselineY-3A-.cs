1e1cf2a12a8ae16c99865177b8f33197bf43428d
yo <unknown>
2005-01-07 15:14:00 +0000

    no message

---
+++ HostFont/instance/accessing/displayString-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-baselineY-3A-.cs
@@ -0,0 +1,3 @@
+displayString: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint kern: kernDelta baselineY: baselineY
+
+ 	^ super displayMultiString: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint kern: kernDelta baselineY: baselineY.
