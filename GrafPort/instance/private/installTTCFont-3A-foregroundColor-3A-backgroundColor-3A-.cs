c91e59dd1dc06a2dfbe773a2a069c1e6d568225c
yo <unknown>
2005-01-08 09:12:00 +0000

    no message

---
+++ GrafPort/instance/private/installTTCFont-3A-foregroundColor-3A-backgroundColor-3A-.cs
@@ -0,0 +1,6 @@
+installTTCFont: aTTCFont foregroundColor: foregroundColor backgroundColor: backgroundColor
+
+	super installTTCFont: aTTCFont foregroundColor: foregroundColor backgroundColor: backgroundColor.
+	lastFont _ aTTCFont.
+	lastFontForegroundColor _ foregroundColor.
+	lastFontBackgroundColor _ backgroundColor.
