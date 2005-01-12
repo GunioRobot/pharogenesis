545858d45b9e5a27476db8b16591558eba4fd6bd
yo <unknown>
2005-01-12 22:43:00 +0000

    no message

--- Preferences/class/fonts/restoreDefaultFonts.cs
+++ Preferences/class/fonts/restoreDefaultFonts.cs
@@ -1,18 +1,18 @@
 restoreDefaultFonts
-	"Preferences restoreDefaultFonts"
-	#(	(setButtonFontTo:		ComicBold		16)
-		"(setCodeFontTo:			NewYork		12)"  "Later"
-		(setFlapsFontTo:			ComicBold		16)
-		(setListFontTo:			NewYork		12)
-		(setMenuFontTo:			NewYork		12)
-		(setWindowTitleFontTo:	NewYork		15)
-		(setSystemFontTo:		NewYork		12)) do:
-			[:triplet |
-				self perform: triplet first with: (StrikeFont familyName: triplet second size: triplet third)].
+	"Since this is called from menus, we can take the opportunity to prompt for missing font styles."
+	"
+	Preferences restoreDefaultFonts
+	"
 
-	Smalltalk at: #BalloonMorph ifPresent:
-		[:thatClass | thatClass setBalloonFontTo: (StrikeFont familyName: #ComicPlain size: 12)].
-
-	"Note:  The standardCodeFont is not currently used -- the default font is instead; later hopefully we can split the code font out as  a separate choice, but only after we're able to have the protocols reorganized such that we can know whether it's code or not when we launch the text object.
-
-	Note:  The standard button font is reset by this code but is not otherwise settable by a public UI (too many things can go afoul) "
+	self setDefaultFonts: #(
+		(setSystemFontTo:		Accuny				10)
+		(setListFontTo:			Accuny				10)
+		(setFlapsFontTo:			Accushi				12)
+		(setEToysFontTo:			BitstreamVeraSansBold	9)
+		(setPaintBoxButtonFontTo:			BitstreamVeraSansBold	9)
+		(setMenuFontTo:			Accuny				10)
+		(setWindowTitleFontTo:	BitstreamVeraSansBold	12)
+		(setBalloonHelpFontTo:	Accujen				9)
+		(setCodeFontTo:			Accuny				10)
+		(setButtonFontTo:		BitstreamVeraSansMono				9)
+	)
