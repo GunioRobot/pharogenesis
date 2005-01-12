d47235f66b8144992a5ae584a05c287499079562
tak <unknown>
2005-01-12 14:57:00 +0000

    no message

--- Morph/instance/caching/releaseCachedState.cs
+++ Morph/instance/caching/releaseCachedState.cs
@@ -1,3 +1,7 @@
 releaseCachedState
 	"Release any state that can be recomputed on demand, such as the pixel values for a color gradient or the editor state for a TextMorph. This method may be called to save space when a morph becomes inaccessible. Implementations of this method should do 'super releaseCachedState'."
+	self formerOwner: nil.
+	self formerPosition: nil.
+	self removeProperty: #undoGrabCommand.
 	self wonderlandTexture: nil. "We can recreate it if needed"
+	self borderStyle releaseCachedState.
