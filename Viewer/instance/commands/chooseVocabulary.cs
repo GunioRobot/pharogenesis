1709c5fb87db7129a913f4fe49d428512cdb4e2a
yo <unknown>
2005-01-14 19:57:00 +0000

    no message

---
+++ Viewer/instance/commands/chooseVocabulary.cs
@@ -0,0 +1,16 @@
+chooseVocabulary
+	"Put up a menu allowing the user to specify which protocol to use in this viewer"
+
+	| aMenu |
+	aMenu _ MenuMorph new defaultTarget: self.
+	aMenu addTitle: 'Choose a vocabulary' translated.
+	"aMenu addStayUpItem."  "For debugging only"
+	Vocabulary allStandardVocabularies do:
+		[:aVocabulary |
+			(scriptedPlayer class implementsVocabulary: aVocabulary)
+				ifTrue:
+					[aMenu add: aVocabulary vocabularyName selector: #switchToVocabulary: argument: aVocabulary.
+					aVocabulary == self currentVocabulary ifTrue:
+						[aMenu lastItem color: Color blue]. 
+					aMenu balloonTextForLastItem: aVocabulary documentation]].
+	aMenu popUpInWorld: self currentWorld
