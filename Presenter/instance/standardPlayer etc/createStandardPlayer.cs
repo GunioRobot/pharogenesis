9172e61c80ea82b1d147e910c89f18e54f27f716
yo <unknown>
2005-01-14 19:37:00 +0000

    no message

--- Presenter/instance/standardPlayer etc/createStandardPlayer.cs
+++ Presenter/instance/standardPlayer etc/createStandardPlayer.cs
@@ -3,7 +3,7 @@ createStandardPlayer
 
 	aMorph _ ImageMorph new image: (ScriptingSystem formAtKey: 'standardPlayer').
 	associatedMorph addMorphFront: aMorph.
-	standardPlayer _ aMorph assuredPlayer renameTo: 'dot'.
+	standardPlayer _ aMorph assuredPlayer renameTo: 'dot' translated.
 	aMorph setBalloonText: '...'.
 	self positionStandardPlayer.
 	^ standardPlayer
