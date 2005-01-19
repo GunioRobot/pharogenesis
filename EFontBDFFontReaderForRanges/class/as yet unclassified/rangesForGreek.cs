0a21b6e9d124c738978e099205397d391d1ed34a
yo <unknown>
2005-01-19 11:24:00 +0000

    no message

---
+++ EFontBDFFontReaderForRanges/class/as yet unclassified/rangesForGreek.cs
@@ -0,0 +1,9 @@
+rangesForGreek
+
+	^ {
+		Array with: 16r1 with: 16rFF.
+		Array with: 16r370 with: 16r3FF.
+		Array with: 16r1F00 with: 16r1FFF.
+		Array with: 16r2000 with: 16r206F.
+		Array with: 16r20A0 with: 16r20AF
+	}.
