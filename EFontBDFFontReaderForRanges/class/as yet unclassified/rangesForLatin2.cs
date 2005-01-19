c03a6575a4748768a90db97750f6379bfeb51232
yo <unknown>
2005-01-19 11:24:00 +0000

    no message

---
+++ EFontBDFFontReaderForRanges/class/as yet unclassified/rangesForLatin2.cs
@@ -0,0 +1,9 @@
+rangesForLatin2
+
+	^ {
+		Array with: 0 with: 16r17F.
+		Array with: 16r2B0 with: 16r2FF.
+		Array with: 16r2000 with: 16r206F.
+		Array with: 16r2122 with: 16r2122.
+		Array with: 16rFFFF with: 16rFFFF. "sentinel"
+	}.
