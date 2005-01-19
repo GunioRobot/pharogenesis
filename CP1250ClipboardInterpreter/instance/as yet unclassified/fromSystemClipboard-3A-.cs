edcc459371b5d087a2ab24e757eabbb642613050
pk <unknown>
2005-01-19 20:26:00 +0000

    no message

---
+++ CP1250ClipboardInterpreter/instance/as yet unclassified/fromSystemClipboard-3A-.cs
@@ -0,0 +1,9 @@
+fromSystemClipboard: aString
+
+	| result converter |
+	result := WriteStream on: (String new: aString size).
+	converter := CP1250TextConverter new.
+	aString do: [:each |
+		result nextPut: (converter toSqueak: each squeakToIso) asCharacter.
+	].
+	^ result contents.
