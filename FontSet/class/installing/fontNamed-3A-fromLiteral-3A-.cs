4285e7515d77ffd07f380600671a853af6cd06b2
di <unknown>
2005-01-24 12:48:00 +0000

    no message

--- FontSet/class/installing/fontNamed-3A-fromLiteral-3A-.cs
+++ FontSet/class/installing/fontNamed-3A-fromLiteral-3A-.cs
@@ -1,13 +1,16 @@
 fontNamed: fontName fromLiteral: aString
-	"This method allows a font set to be captured as sourcecode in a subclass.
-	The string literals will presumably be created by printing, eg,
-		(FileStream readOnlyFileNamed: 'Palatino24.sf2') contentsOfEntireFile,
-		and then pasting into a browser after a heading like, eg,
-sizeNewYork10
-	^ self fontNamed: 'NewYork10' fromLiteral:
-	'--unreadable binary data--'
+	"NOTE -- THIS IS AN OBSOLETE METHOD THAT MAY CAUSE ERRORS.
 
-	See the method installAsTextStyle to see how this can be used."
+The old form of fileOut for FontSets produced binary literal strings which may not be accurately read in systems with support for international character sets.  If possible, file the FontSet out again from a system that produces the newer MIME encoding (current def of compileFont:), and uses the corresponding altered version of this method.  If this is not easy, then
+	file the fontSet into an older system (3.7 or earlier),
+	assume it is called FontSetZork...
+	execute FontSetZork installAsTextStyle.
+	copy the compileFont: method from this system into that older one.
+	remove the class FontSetZork.
+	Execute:  FontSet convertTextStyleNamed: 'Zork', and see that it creates a new FontSetZork.
+	FileOut the new class FontSetZork.
+	The resulting file should be able to be read into this system.
+"
 
 	^ StrikeFont new 
 		name: fontName;
