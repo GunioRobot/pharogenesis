58fe8c3045b752cacead4abb3ae6c2df2334003f
tak <unknown>
2005-01-26 14:58:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/getNewClone.cs
+++ Player/instance/slot getters-2F-setters/getNewClone.cs
@@ -1,4 +1,7 @@
 getNewClone
 	"Answer a new player of the same class as the receiver, with a costume much like mine"
 
-	^ costume usableSiblingInstance player
+	| clone |
+	clone _  costume usableSiblingInstance.
+	costume pasteUpMorph ifNotNilDo: [:parent | parent addMorph: clone].
+	^ clone player
