6f8eb60edc2498db7a845ad6091ed6cb9193be20
sw <unknown>
2005-01-28 00:54:00 +0000

    no message

---
+++ TileMorph/instance/misc/soundChoices.cs
@@ -0,0 +1,7 @@
+soundChoices
+	"Answer a list of sound choices.  This applies only to tiles that have sound-names as their literals, viz. SoundTiles and SoundReadoutTiles."
+
+	| aList |
+	aList _ SoundService default sampledSoundChoices asOrderedCollection.
+	aList removeAllFoundIn: (ScriptingSystem soundNamesToSuppress copyWithout: literal).
+	^ aList asSortedArray
