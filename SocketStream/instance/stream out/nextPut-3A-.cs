3b66837c423d8c99f0d9914606a9296c8a718dd0
gk <unknown>
2005-02-09 22:40:00 +0000

    no message

--- SocketStream/instance/stream out/nextPut-3A-.cs
+++ SocketStream/instance/stream out/nextPut-3A-.cs
@@ -1,3 +1,9 @@
 nextPut: char
-	self outStream nextPut: (self isBinary ifTrue: [char asInteger] ifFalse: [char asCharacter]).
+	"Put a single Character or byte onto the stream."
+
+	| toPut |
+	toPut _ binary ifTrue: [char asInteger] ifFalse: [char asCharacter].
+	self adjustOutBuffer: 1.
+	outBuffer at: outNextToWrite put: toPut.
+	outNextToWrite _ outNextToWrite + 1.
 	self checkFlush
