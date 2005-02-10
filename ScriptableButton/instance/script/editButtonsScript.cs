78075db89048c48ce67993c437f41b8d0c4074c5
yo <unknown>
2005-02-10 20:09:00 +0000

    no message

--- ScriptableButton/instance/script/editButtonsScript.cs
+++ ScriptableButton/instance/script/editButtonsScript.cs
@@ -2,18 +2,20 @@ editButtonsScript
 	"The user has touched my Scriptor halo-handle.  Bring up a Scriptor on the script of the button."
 
 	| cardsPasteUp cardsPlayer anEditor |
-	cardsPasteUp _ self pasteUpMorph.
-	(cardsPlayer _ cardsPasteUp assuredPlayer) assureUniClass.
-	anEditor _ scriptSelector
-			ifNil:
-				[scriptSelector _ cardsPasteUp scriptSelectorToTriggerFor: self.
-				cardsPlayer newScriptEditorFor: scriptSelector usingPhraseTile: nil]
-			ifNotNil:
-				[(cardsPlayer class selectors includes: scriptSelector)
-					ifTrue:
-						[cardsPlayer scriptEditorFor: scriptSelector]
-					ifFalse:
-						["Method somehow got removed; I guess we start aftresh"
-						scriptSelector _ nil.
-						^ self editButtonsScript]].
-	self currentHand attachMorph: anEditor.
+	cardsPasteUp := self pasteUpMorph.
+	(cardsPlayer := cardsPasteUp assuredPlayer) assureUniClass.
+	anEditor := scriptSelector ifNil: 
+					[scriptSelector := cardsPasteUp scriptSelectorToTriggerFor: self.
+					cardsPlayer newTextualScriptorFor: scriptSelector.
+					cardsPlayer scriptEditorFor: scriptSelector
+					]
+				ifNotNil: 
+					[(cardsPlayer class selectors includes: scriptSelector) 
+						ifTrue: [cardsPlayer scriptEditorFor: scriptSelector]
+						ifFalse: 
+							["Method somehow got removed; I guess we start afresh"
+
+							scriptSelector := nil.
+							^self editButtonsScript]].
+	anEditor showingMethodPane ifTrue: [anEditor toggleWhetherShowingTiles].
+	self currentHand attachMorph: anEditor
