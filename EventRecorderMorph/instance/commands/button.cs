cf9eb5d4cba3c77cd9425c836976f327a665eb88
yo <unknown>
2005-02-11 09:59:00 +0000

    no message

--- EventRecorderMorph/instance/commands/button.cs
+++ EventRecorderMorph/instance/commands/button.cs
@@ -1,7 +1,7 @@
 button
 	"Make a simple button interface for replay only"
 	| butnCaption erm |
-	butnCaption _ FillInTheBlank request: 'Caption for this butn?' initialAnswer: 'play'.
+	butnCaption _ FillInTheBlank request: 'Caption for this butn?' translated initialAnswer: 'play' translated.
 	butnCaption isEmpty ifTrue: [^ self].
 	erm _ (EventRecorderMorph basicNew
 				caption: butnCaption
