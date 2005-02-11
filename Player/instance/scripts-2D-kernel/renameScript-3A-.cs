f45e0c2dedea9023921c3d234def8ea42157788d
yo <unknown>
2005-02-11 15:37:00 +0000

    no message

--- Player/instance/scripts-2D-kernel/renameScript-3A-.cs
+++ Player/instance/scripts-2D-kernel/renameScript-3A-.cs
@@ -1,23 +1,28 @@
-renameScript: oldSelector
+renameScript: oldSelector 
 	"The user has asked to rename the script formerly known by oldSelector; obtain a new selector from the user, check it out, and if all is well, ascribe the new name as appropriate"
 
 	| reply newSelector aUserScript |
-	self flag: #deferred.  "Relax the below, yes?"
-	aUserScript _ self class userScriptForPlayer: self selector: oldSelector.
-	aUserScript isTextuallyCoded ifTrue:
-		[self inform: 'Sorry, for now you can only rename tiled scripts'.
-		^ self].
-
-	reply _   FillInTheBlank request: 'Script Name' initialAnswer: oldSelector.
- 	reply size == 0 ifTrue: [^ self].
-	reply = oldSelector ifTrue:[^ self beep].
-
-	newSelector _ ScriptingSystem acceptableScriptNameFrom: reply  forScriptCurrentlyNamed:  oldSelector asScriptNameIn: self world: costume world.
-	(self currentWorld hasProperty: #universalTiles) 
-		ifTrue:
+	self flag: #deferred.
+	"Relax the restriction below, before too long"
+	aUserScript := self class userScriptForPlayer: self selector: oldSelector.
+	aUserScript okayToRename 
+		ifFalse: 
+			[self 
+				inform: 'Sorry, we do not permit you to rename
+classic-tiled scripts that are currently
+textually coded.  Go back to tile scripts
+and try again.  Humble apologies.' translated.
+			^self].
+	reply := FillInTheBlank request: 'Script Name' translated initialAnswer: oldSelector.
+	reply isEmpty ifTrue: [^self].
+	reply = oldSelector ifTrue: [^Beeper beep].
+	newSelector := self acceptableScriptNameFrom: reply
+				forScriptCurrentlyNamed: oldSelector.
+	Preferences universalTiles 
+		ifTrue: 
 			["allow colons"
-			(reply copyWithout: $:) = newSelector 
-				ifTrue: [newSelector _ reply asSymbol]
-				ifFalse: [self inform: 'name will be modified']].	
 
+			(reply copyWithout: $:) = newSelector 
+				ifTrue: [newSelector := reply asSymbol]
+				ifFalse: [self inform: 'name will be modified']].
 	self renameScript: oldSelector newSelector: newSelector
