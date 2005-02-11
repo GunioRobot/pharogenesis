70958f93a1c1c7a55993eeea85f7df0cabc35216
yo <unknown>
2005-02-11 10:39:00 +0000

    no message

--- KeyboardMorphForInput/instance/initialization/addRecordingControls.cs
+++ KeyboardMorphForInput/instance/initialization/addRecordingControls.cs
@@ -1,69 +1,93 @@
 addRecordingControls
-
 	| button switch playRow durRow articRow modRow |
-	button _ SimpleButtonMorph new target: self;
-		borderColor: #raised; borderWidth: 2; color: color.
-	switch _ SimpleSwitchMorph new target: self;
-		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); borderWidth: 2;
-		setSwitchState: false.
 
 	"Add chord, rest and delete buttons"
 	playRow _ AlignmentMorph newRow.
 	playRow color: color; borderWidth: 0; layoutInset: 0.
 	playRow hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
-	playRow addMorphBack: (switch fullCopy label: 'chord'; actionSelector: #buildChord:).
-	playRow addMorphBack: (button fullCopy label: '          rest          '; actionSelector: #emitRest).
-	playRow addMorphBack: (button fullCopy label: 'del'; actionSelector: #deleteNotes).
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	playRow addMorphBack: (switch label: 'chord' translated; actionSelector: #buildChord:).
+	button _ SimpleButtonMorph new target: self;
+		borderColor: #raised; borderWidth: 2; color: color.
+	playRow addMorphBack: (button label: '          rest          ' translated; actionSelector: #emitRest).
+	button _ SimpleButtonMorph new target: self;
+		borderColor: #raised; borderWidth: 2; color: color.
+	playRow addMorphBack: (button label: 'del' translated; actionSelector: #deleteNotes).
 	self addMorph: playRow.
-	playRow align: playRow bounds topCenter
-			with: self bounds bottomCenter.
+	playRow align: playRow fullBounds topCenter
+			with: self fullBounds bottomCenter.
 
 	"Add note duration buttons"
 	durRow _ AlignmentMorph newRow.
 	durRow color: color; borderWidth: 0; layoutInset: 0.
 	durRow hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
-	durRow addMorphBack: (switch fullCopy label: 'whole';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	durRow addMorphBack: (switch label: 'whole' translated;
 				actionSelector: #duration:onOff:; arguments: #(1)).
-	durRow addMorphBack: (switch fullCopy label: 'half';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	durRow addMorphBack: (switch label: 'half' translated;
 				actionSelector: #duration:onOff:; arguments: #(2)).
-	durRow addMorphBack: (switch fullCopy label: 'quarter';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	durRow addMorphBack: (switch label: 'quarter' translated;
 				actionSelector: #duration:onOff:; arguments: #(4)).
-	durRow addMorphBack: (switch fullCopy label: 'eighth';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	durRow addMorphBack: (switch label: 'eighth' translated;
 				actionSelector: #duration:onOff:; arguments: #(8)).
-	durRow addMorphBack: (switch fullCopy label: 'sixteenth';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	durRow addMorphBack: (switch label: 'sixteenth' translated;
 				actionSelector: #duration:onOff:; arguments: #(16)).
 	self addMorph: durRow.
-	durRow align: durRow bounds topCenter
-			with: playRow bounds bottomCenter.
+	durRow align: durRow fullBounds topCenter
+			with: playRow fullBounds bottomCenter.
 
 	"Add note duration modifier buttons"
 	modRow _ AlignmentMorph newRow.
 	modRow color: color; borderWidth: 0; layoutInset: 0.
 	modRow hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
-	modRow addMorphBack: (switch fullCopy label: 'dotted';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	modRow addMorphBack: (switch label: 'dotted' translated;
 				actionSelector: #durMod:onOff:; arguments: #(dotted)).
-	modRow addMorphBack: (switch fullCopy label: 'normal';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	modRow addMorphBack: (switch label: 'normal' translated;
 				actionSelector: #durMod:onOff:; arguments: #(normal)).
-	modRow addMorphBack: (switch fullCopy label: 'triplets';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	modRow addMorphBack: (switch label: 'triplets' translated;
 				actionSelector: #durMod:onOff:; arguments: #(triplets)).
-	modRow addMorphBack: (switch fullCopy label: 'quints';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	modRow addMorphBack: (switch label: 'quints' translated;
 				actionSelector: #durMod:onOff:; arguments: #(quints)).
 	self addMorph: modRow.
-	modRow align: modRow bounds topCenter
-			with: durRow bounds bottomCenter.
+	modRow align: modRow fullBounds topCenter
+			with: durRow fullBounds bottomCenter.
 
 	"Add articulation buttons"
 	articRow _ AlignmentMorph newRow.
 	articRow color: color; borderWidth: 0; layoutInset: 0.
 	articRow hResizing: #shrinkWrap; vResizing: #shrinkWrap; extent: 5@5.
-	articRow addMorphBack: (switch fullCopy label: 'legato';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	articRow addMorphBack: (switch label: 'legato' translated;
 				actionSelector: #articulation:onOff:; arguments: #(legato)).
-	articRow addMorphBack: (switch fullCopy label: 'normal';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	articRow addMorphBack: (switch label: 'normal' translated;
 				actionSelector: #articulation:onOff:; arguments: #(normal)).
-	articRow addMorphBack: (switch fullCopy label: 'staccato';
+	switch _ SimpleSwitchMorph new target: self; borderWidth: 2;
+		offColor: color; onColor: (Color r: 1.0 g: 0.6 b: 0.6); setSwitchState: false.
+	articRow addMorphBack: (switch label: 'staccato' translated;
 				actionSelector: #articulation:onOff:; arguments: #(staccato)).
 	self addMorph: articRow.
-	articRow align: articRow bounds topCenter
-			with: modRow bounds bottomCenter.
+	articRow align: articRow fullBounds topCenter
+			with: modRow fullBounds bottomCenter.
 
 	self bounds: (self fullBounds expandBy: (0@0 extent: 0@borderWidth))
