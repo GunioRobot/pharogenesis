733bf490e682cb37c78cf3302cc8ce9bbfe55de1
yo <unknown>
2005-02-11 16:01:00 +0000

    no message

--- Player/instance/slots-2D-user/chooseSlotTypeFor-3A-.cs
+++ Player/instance/slots-2D-user/chooseSlotTypeFor-3A-.cs
@@ -1,15 +1,18 @@
-chooseSlotTypeFor: slotName
-	| typeChoices typeChosen |
-	self flag: #deferred.  "sound should be reinstated but too much difficulty at present"
+chooseSlotTypeFor: aGetter
+	"Let the user designate a type for the slot associated with the given getter"
 
-	typeChoices _ #(number player boolean color string "sound point costume").
-	typeChosen _ (SelectionMenu selections: typeChoices lines: #()) startUpWithCaption: 'Choose the TYPE
-for ', slotName.
+	| typeChoices typeChosen slotName |
+	slotName _ Utilities inherentSelectorForGetter: aGetter.
+	typeChoices _ Vocabulary typeChoices.
+	typeChosen _ (SelectionMenu labelList: (typeChoices collect: [:t | t translated]) lines: #() selections: typeChoices) startUpWithCaption: 
+		('Choose the TYPE
+for ' translated, slotName, '
+(currently ' translated, (self slotInfoAt: slotName) type translated, ')').
 	typeChosen isEmptyOrNil ifTrue: [^ self].
-	(self typeForSlot: slotName) = typeChosen ifTrue: [^ self].
+	(self typeForSlot: slotName) capitalized = typeChosen ifTrue: [^ self].
 
 	(self slotInfoAt: slotName) type: typeChosen.
 	self class allInstancesDo:   "allSubInstancesDo:"
 		[:anInst | anInst instVarNamed: slotName asString put: 
-			(anInst valueOfType: typeChosen from: (anInst instVarNamed: slotName)).
-		anInst updateAllViewers]
+			(anInst valueOfType: typeChosen from: (anInst instVarNamed: slotName))].
+	self updateAllViewers.	"does siblings too"
