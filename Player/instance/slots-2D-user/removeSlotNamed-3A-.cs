887f3cde4acede4ca35a6463677237ae46f038ba
yo <unknown>
2005-02-11 15:44:00 +0000

    no message

--- Player/instance/slots-2D-user/removeSlotNamed-3A-.cs
+++ Player/instance/slots-2D-user/removeSlotNamed-3A-.cs
@@ -1,13 +1,19 @@
 removeSlotNamed: aSlotName
+	"The user has requested that an instance variable be removed..."
+
+	| aSetter aGetter |
 	(self okayToRemoveSlotNamed: aSlotName) ifFalse:
-		[^ self inform: 'Sorry, ', aSlotName, ' is in
-use in a script.'].
-	self inform: 
-'Caution!  Any scripts that may reference
-this instance variable may now be broken.
-You may need to fix them up manually.  
-In some future release, we''ll automatically
-fix those up, hopefully.'.
+		[^ self inform: ('Sorry, {1} is in
+use in a script.' translated format: {aSlotName})].
+
+	aSetter _ Utilities setterSelectorFor: aSlotName.
+	aGetter _ Utilities getterSelectorFor: aSlotName.
+	((self systemNavigation allCallsOn: aSetter) size > 0 or: [(self systemNavigation allCallsOn: aGetter) size > 0]) ifTrue:
+		[self inform: 
+'Caution!  There may be scripts belonging to
+other objects that may rely on the presence of
+this variable.  If there are, they may now be broken.
+You may need to fix them up manually.' translated].
 
 	self class removeInstVarName: aSlotName asString.
 
