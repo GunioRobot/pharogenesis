5b584db3933a267bdcaf2453dcf3df6038c6aa8b
yo <unknown>
2005-02-12 19:25:00 +0000

    no message

--- Project/instance/SuperSwiki/tellAFriend-3A-.cs
+++ Project/instance/SuperSwiki/tellAFriend-3A-.cs
@@ -9,18 +9,6 @@ Project current tellAFriend
 	].
 	urlForLoading isEmptyOrNil ifTrue: [
 		^self inform: 'Since this project has not been saved yet,
-I cannot tell someone where it is.'
+I cannot tell someone where it is.' translated
 	].
-	FancyCelesteComposition new
-		celeste: nil 
-		to: (emailAddressOrNil ifNil: ['RECIPIENT.GOESHERE'])
-		subject: 'A Squeak project link you might like'
-		initialText: 'This is a link to a Squeak project: '
-		theLinkToInclude: 
-			'<A HREF="',
-			urlForLoading,
-			'">',
-			self name,
-			'</A>';
-
-		open.
+	HTTPClient tellAFriend: emailAddressOrNil url: urlForLoading name: self name
