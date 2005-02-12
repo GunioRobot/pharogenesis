01cf0185854a46a216916bce99a80478a4a6b8c2
yo <unknown>
2005-02-12 19:24:00 +0000

    no message

--- HaloMorph/instance/private/addDirectionHandles.cs
+++ HaloMorph/instance/private/addDirectionHandles.cs
@@ -3,7 +3,7 @@ addDirectionHandles
 	| centerHandle d w directionShaft patch patchColor crossHairColor |
 	self showingDirectionHandles ifFalse: [^ self].
 
-	directionArrowAnchor _ target referencePositionInWorld rounded.
+	directionArrowAnchor _ (target point: target referencePosition in: self world) rounded.
 	patch _ target imageFormForRectangle: (Rectangle center: directionArrowAnchor extent: 3@3).
 	patchColor _ patch colorAt: 1@1.
 
@@ -11,7 +11,7 @@ addDirectionHandles
 		borderWidth: 2; borderColor: (Color green orColorUnlike: patchColor).
 	self positionDirectionShaft: directionShaft.
 	self addMorphFront: directionShaft.
-	directionShaft setCenteredBalloonText: 'Set forward direction';
+	directionShaft setCenteredBalloonText: 'Set forward direction' translated;
 		on: #mouseDown send: #doDirection:with: to: self;
 		on: #mouseMove send: #trackDirectionArrow:with: to: self;
 		on: #mouseUp send: #setDirection:with: to: self.
@@ -20,11 +20,11 @@ addDirectionHandles
 	crossHairColor _ Color red orColorUnlike: patchColor.
 	(centerHandle _ EllipseMorph newBounds: (0@0 extent: d@d) color: Color transparent)
 			borderWidth: w; borderColor: (Color blue orColorUnlike: patchColor);
-			addMorph: (LineMorph from: (d//2)@w to: (d//2)@(d-w-1) color: crossHairColor width: 1);
-			addMorph: (LineMorph from: w@(d//2) to: (d-w-1)@(d//2) color: crossHairColor width: 1);
+			addMorph: (LineMorph from: (d//2)@w to: (d//2)@(d-w-1) color: crossHairColor width: 1) lock;
+			addMorph: (LineMorph from: w@(d//2) to: (d-w-1)@(d//2) color: crossHairColor width: 1) lock;
 			align: centerHandle bounds center with: directionArrowAnchor.
 	self addMorph: centerHandle.
-	centerHandle setCenteredBalloonText: 'Set rotation center';
+	centerHandle setCenteredBalloonText: 'Set rotation center' translated;
 			on: #mouseDown send: #prepareToTrackCenterOfRotation:with: to: self;
 			on: #mouseMove send: #trackCenterOfRotation:with: to: self;
 			on: #mouseUp send: #setCenterOfRotation:with: to: self
