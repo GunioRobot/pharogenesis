3ce3b7b1de1aa56c521c49757bd7359131dcb750
yo <unknown>
2005-02-17 14:55:00 +0000

    no message

--- ProjectNavigationMorph/instance/the actions/publishProject.cs
+++ ProjectNavigationMorph/instance/the actions/publishProject.cs
@@ -1,3 +1,10 @@
 publishProject
 
-	(self world ifNil: [^1 beep]) project storeOnServerShowProgressOn: self forgetURL: false.
+	self world paintBoxOrNil ifNotNil: [
+		(self confirm: 'You seem to be painting a sketch.
+Do you continue and publish the project with the paint tool?' translated) ifFalse: [^ self].
+	].
+	self 
+		publishStyle: #limitedSuperSwikiPublishDirectoryList 
+		forgetURL: false
+		withRename: false
