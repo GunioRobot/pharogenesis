f176fb652e5dbe71dfbad95e0fe9f6b389f8aef1
yo <unknown>
2005-02-17 18:03:00 +0000

    no message

--- Morph/instance/menus/printPSToFileNamed-3A-.cs
+++ Morph/instance/menus/printPSToFileNamed-3A-.cs
@@ -1,21 +1,20 @@
-printPSToFileNamed: aString
+printPSToFileNamed: aString 
 	"Ask the user for a filename and print this morph as postscript."
-
-	| fileName rotateFlag |
-	fileName _ aString asFileName.
-	fileName _ FillInTheBlank 
-		request: 'File name? (".eps" will be added to end)' 
-		initialAnswer: fileName.
-	fileName size == 0 ifTrue: [^ self beep].
-	(fileName endsWith: '.eps') ifFalse: [fileName _ fileName,'.eps'].
-
-	rotateFlag _ ((PopUpMenu labels:
-'portrait (tall)
-landscape (wide)')
-			startUpWithCaption: 'Choose orientation...') = 2.
-
-	(FileStream newFileNamed: fileName)
-		nextPutAll: (
-			PostscriptCanvas defaultCanvasType morphAsPostscript: self rotated: rotateFlag
-		); close.
-
+	| fileName rotateFlag psCanvasType psExtension |
+	fileName := aString asFileName.
+	psCanvasType _ PostscriptCanvas defaultCanvasType.
+	psExtension _ psCanvasType defaultExtension.
+	fileName := FillInTheBlank request: (String streamContents: [ :s |
+		s nextPutAll: ('File name? ("{1}" will be added to end)' translated format: {psExtension})])
+			initialAnswer: fileName.
+	fileName isEmpty
+		ifTrue: [^ Beeper beep].
+	(fileName endsWith: psExtension)
+		ifFalse: [fileName := fileName , psExtension].
+	rotateFlag := ((PopUpMenu labels: 'portrait (tall)
+landscape (wide)' translated)
+				startUpWithCaption: 'Choose orientation...' translated)
+				= 2.
+	((FileStream newFileNamed: fileName asFileName) converter: TextConverter defaultSystemConverter)
+		nextPutAll: (psCanvasType morphAsPostscript: self rotated: rotateFlag);
+		 close
