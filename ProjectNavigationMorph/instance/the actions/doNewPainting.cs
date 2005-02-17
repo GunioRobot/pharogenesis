229742e9df70f49b8c04bcb66f9e73a457d7d81d
yo <unknown>
2005-02-17 15:40:00 +0000

    no message

--- ProjectNavigationMorph/instance/the actions/doNewPainting.cs
+++ ProjectNavigationMorph/instance/the actions/doNewPainting.cs
@@ -1,7 +1,8 @@
 doNewPainting
 	
-	| w |
+	| w f |
 
 	w _ self world.
 	w assureNotPaintingElse: [^ self].
+	(f _ self owner flapTab) ifNotNil: [f hideFlap].
 	w makeNewDrawing: (self primaryHand lastEvent copy setPosition: w center)
