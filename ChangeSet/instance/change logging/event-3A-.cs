24ab9893c3227f15a6051ab55a87e1c9c4189638
NS <unknown>
2005-02-17 18:57:00 +0000

    no message

--- ChangeSet/instance/change logging/event-3A-.cs
+++ ChangeSet/instance/change logging/event-3A-.cs
@@ -1,23 +1,29 @@
 event: anEvent
 	"Hook for SystemChangeNotifier"
+	anEvent itemKind = SystemChangeNotifier classKind ifTrue: [
+		anEvent isRemoved 
+			ifTrue: [self noteRemovalOf: anEvent item].
+		anEvent isAdded 
+			ifTrue: [self addClass: anEvent item].
+		anEvent isModified 
+			ifTrue: [anEvent anyChanges ifTrue: [self changeClass: anEvent item from: anEvent oldItem]].
+		anEvent isCommented 
+			ifTrue: [self commentClass: anEvent item].
+		anEvent isRenamed 
+			ifTrue: [self renameClass: anEvent item as: anEvent newName].
+		anEvent isReorganized
+			ifTrue: [self reorganizeClass: anEvent item].
+		anEvent isRecategorized
+			ifTrue: [self changeClass: anEvent item from: anEvent item].
+	].
 
-	(anEvent isRemoved and: [anEvent itemKind = SystemChangeNotifier classKind]) 
-		ifTrue: [self noteRemovalOf: anEvent item].
-	(anEvent isAdded and: [anEvent itemKind = SystemChangeNotifier classKind]) 
-		ifTrue: [self addClass: anEvent item].
-	(anEvent isModified and: [anEvent itemKind = SystemChangeNotifier classKind])
-		ifTrue: [anEvent anyChanges ifTrue: [self changeClass: anEvent item from: anEvent oldItem]].
-	(anEvent isCommented and: [anEvent itemKind = SystemChangeNotifier classKind])
-		ifTrue: [self commentClass: anEvent item].
-	(anEvent isAdded and: [anEvent itemKind = SystemChangeNotifier methodKind])
-		ifTrue: [self noteNewMethod: anEvent item forClass: anEvent itemClass selector: anEvent itemSelector priorMethod: nil].
-	(anEvent isModified and: [anEvent itemKind = SystemChangeNotifier methodKind])
-		ifTrue: [self noteNewMethod: anEvent item forClass: anEvent itemClass selector: anEvent itemSelector priorMethod: anEvent oldItem].
-	(anEvent isRemoved and: [anEvent itemKind = SystemChangeNotifier methodKind])
-		ifTrue: [self removeSelector: anEvent itemSelector class: anEvent itemClass priorMethod: anEvent item lastMethodInfo: {anEvent item sourcePointer. anEvent itemProtocol}].
-	(anEvent isRenamed and: [anEvent itemKind = SystemChangeNotifier classKind])
-		ifTrue: [self renameClass: anEvent item as: anEvent newName].
-	(anEvent isReorganized and: [anEvent itemKind = SystemChangeNotifier classKind])
-		ifTrue: [self reorganizeClass: anEvent item].
-	(anEvent isRecategorized and: [anEvent itemKind = SystemChangeNotifier methodKind])
-		ifTrue: [self reorganizeClass: anEvent itemClass].
+	anEvent itemKind = SystemChangeNotifier methodKind ifTrue: [
+		anEvent isAdded
+			ifTrue: [self noteNewMethod: anEvent item forClass: anEvent itemClass selector: anEvent itemSelector priorMethod: nil].
+		anEvent isModified
+			ifTrue: [self noteNewMethod: anEvent item forClass: anEvent itemClass selector: anEvent itemSelector priorMethod: anEvent oldItem].
+		anEvent isRemoved
+			ifTrue: [self removeSelector: anEvent itemSelector class: anEvent itemClass priorMethod: anEvent item lastMethodInfo: {anEvent item sourcePointer. anEvent itemProtocol}].
+		anEvent isRecategorized
+			ifTrue: [self reorganizeClass: anEvent itemClass].
+	].
