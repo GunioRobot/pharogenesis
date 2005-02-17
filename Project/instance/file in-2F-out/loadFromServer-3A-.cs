a505ee565ecd3ad9c3f86d7587b4f89e5b43895b
yo <unknown>
2005-02-17 15:05:00 +0000

    no message

--- Project/instance/file in-2F-out/loadFromServer-3A-.cs
+++ Project/instance/file in-2F-out/loadFromServer-3A-.cs
@@ -1,24 +1,24 @@
 loadFromServer: newerAutomatically
 	"If a newer version of me is on the server, load it."
-	| servers pair resp |
+	| pair resp server |
 	self assureIntegerVersion.
 
 	self isCurrentProject ifTrue: ["exit, then do the command"
-		^ self armsLengthCommand: #loadFromServer withDescription: 'Loading'
+		^ self armsLengthCommand: #loadFromServer withDescription: 'Loading' translated
 	].
-	servers _ self tryToFindAServerWithMe ifNil: [^ nil].
-	pair _ self class mostRecent: self name onServer: servers first.
-	pair first ifNil: [^ self inform: 'can''t find file on server for ', self name].
+	server _ self tryToFindAServerWithMe ifNil: [^ nil].
+	pair _ self class mostRecent: self name onServer: server.
+	pair first ifNil: [^ self inform: ('can''t find file on server for {1}' translated format: {self name})].
 	self currentVersionNumber > pair second ifTrue: [
-		^ self inform: 'That server has an older version of the project.'].
+		^ self inform: ('That server has an older version of the project.' translated)].
 	version = (Project parseProjectFileName: pair first) second ifTrue: [
-		resp _ (PopUpMenu labels: 'Reload anyway\Cancel' withCRs) startUpWithCaption: 
-					'The only changes are the ones you made here.'.
+		resp _ (PopUpMenu labels: 'Reload anyway\Cancel' translated withCRs) startUpWithCaption: 
+					'The only changes are the ones you made here.' translated.
 		resp ~= 1 ifTrue: [^ nil]
 	] ifFalse: [
 		newerAutomatically ifFalse: [
-			resp _ (PopUpMenu labels: 'Load it\Cancel' withCRs) startUpWithCaption: 
-						'A newer version exists on the server.'.
+			resp _ (PopUpMenu labels: 'Load it\Cancel' translated withCRs) startUpWithCaption: 
+						'A newer version exists on the server.' translated.
 			resp ~= 1 ifTrue: [^ nil]
 		].
 	].
@@ -32,7 +32,7 @@ loadFromServer: newerAutomatically
 		withProgressDo: [
 			ProjectLoading
 				installRemoteNamed: pair first
-				from: servers first
+				from: server
 				named: self name
 				in: parentProject
 		]
