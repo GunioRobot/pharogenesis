9d04dc0fa4aaeeb91893553191289deb546a4912
yo <unknown>
2005-02-17 15:10:00 +0000

    no message

--- Project/instance/file in-2F-out/tryToFindAServerWithMe.cs
+++ Project/instance/file in-2F-out/tryToFindAServerWithMe.cs
@@ -1,13 +1,13 @@
 tryToFindAServerWithMe
 
-	| servers resp primaryServerDirectory |
+	| resp primaryServerDirectory |
 
 	urlList isEmptyOrNil ifTrue: [urlList _ parentProject urlList copy].
-	[(servers _ self serverList) isNil] whileTrue: [
-		resp _ (PopUpMenu labels: 'Try to find a server\Cancel' withCRs) startUpWithCaption: 
-					'This project thinks it has never been on a server'.
+	[self primaryServer isNil] whileTrue: [
+		resp _ (PopUpMenu labels: 'Try to find a server\Cancel' translated withCRs) startUpWithCaption: 
+					'This project thinks it has never been on a server' translated.
 		resp ~= 1 ifTrue: [^ nil].
 		(primaryServerDirectory _ self findAFolderToLoadProjectFrom) ifNil: [^nil].
-		self storeNewPrimaryURL: primaryServerDirectory realUrl, '/'.
+		self storeNewPrimaryURL: primaryServerDirectory downloadUrl.
 	].
-	^servers
+	^self primaryServer
