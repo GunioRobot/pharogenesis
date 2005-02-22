547e79418681d2e2d99bbf31b32efc54c5d57fb1
nk <unknown>
2005-02-22 15:31:00 +0000

    no message

---
+++ SystemNavigation/instance/query/obsoleteMethodReferences.cs
@@ -0,0 +1,29 @@
+obsoleteMethodReferences
+	"SystemNavigation default obsoleteMethodReferences"
+
+	"Open a browser on all referenced behaviors that are obsolete"
+
+	| obsClasses obsRefs references |
+	references := WriteStream on: Array new.
+	obsClasses := self obsoleteBehaviors.
+	'Scanning for methods referencing obsolete classes' 
+		displayProgressAt: Sensor cursorPoint
+		from: 1
+		to: obsClasses size
+		during: 
+			[:bar | 
+			obsClasses keysAndValuesDo: 
+					[:index :each | 
+					bar value: index.
+					obsRefs := PointerFinder pointersTo: each except: obsClasses.
+					obsRefs do: 
+							[:ref | 
+							"Figure out if it may be a global"
+
+							(ref isVariableBinding and: [ref key isString	"or Symbol"]) 
+								ifTrue: 
+									[(PointerFinder pointersTo: ref) do: 
+											[:meth | 
+											(meth isKindOf: CompiledMethod) 
+												ifTrue: [meth methodReference ifNotNilDo: [:mref | references nextPut: mref]]]]]]].
+	^references contents
