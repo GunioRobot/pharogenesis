292a55f029ad1711fbb5d73e4f604ca9fa5c750a
nk <unknown>
2005-02-22 15:26:00 +0000

    no message

--- ProcessBrowser/instance/process actions/inspectPointers.cs
+++ ProcessBrowser/instance/process actions/inspectPointers.cs
@@ -1,12 +1,14 @@
 inspectPointers
 	| tc pointers |
-	selectedProcess
-		ifNil: [^ self].
-	tc _ thisContext.
-			pointers _ Smalltalk pointersTo: selectedProcess except: {self processList. tc. self}.
-			pointers isEmpty
-				ifTrue: [^ self].
-			OrderedCollectionInspector
-				openOn: pointers
-				withEvalPane: false
-				withLabel: 'Objects pointing to ' , selectedProcess browserPrintString
+	selectedProcess ifNil: [^self].
+	tc := thisContext.
+	pointers := PointerFinder pointersTo: selectedProcess
+				except: { 
+						self processList.
+						tc.
+						self}.
+	pointers isEmpty ifTrue: [^self].
+	OrderedCollectionInspector 
+		openOn: pointers
+		withEvalPane: false
+		withLabel: 'Objects pointing to ' , selectedProcess browserPrintString
