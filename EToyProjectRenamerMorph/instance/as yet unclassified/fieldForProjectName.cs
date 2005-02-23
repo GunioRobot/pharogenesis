4be96f0bc2f1a536e0e4108f93d4bdd9c1e00120
yo <unknown>
2005-02-23 17:25:00 +0000

    no message

--- EToyProjectRenamerMorph/instance/as yet unclassified/fieldForProjectName.cs
+++ EToyProjectRenamerMorph/instance/as yet unclassified/fieldForProjectName.cs
@@ -3,8 +3,9 @@ fieldForProjectName
 	| tm |
 
 	tm _ self genericTextFieldNamed: 'projectname'.
+	tm crAction: (MessageSend receiver: self selector: #doOK).
 	tm setBalloonText: 'Pick a name 24 characters or less and avoid the following characters:
 
- : < > | / \ ? * " .'.
+ : < > | / \ ? * " .' translated.
 	^tm
 	
