3c77da10358e340634ad427f235fbe1b87da0b89
yo <unknown>
2005-02-24 18:34:00 +0000

    no message

--- FileDirectory/instance/private/directoryContentsFor-3A-.cs
+++ FileDirectory/instance/private/directoryContentsFor-3A-.cs
@@ -2,16 +2,18 @@ directoryContentsFor: fullPath
 	"Return a collection of directory entries for the files and directories in the directory with the given path. See primLookupEntryIn:index: for further details."
 	"FileDirectory default directoryContentsFor: ''"
 
-	| entries index done entryArray |
+	| entries index done entryArray f |
 	entries _ OrderedCollection new: 200.
 	index _ 1.
 	done _ false.
+	f _ fullPath asVmPathName.
 	[done] whileFalse: [
-		entryArray _ self primLookupEntryIn: fullPath index: index.
-		#badDirectoryPath = entryArray ifTrue: [^ OrderedCollection new].
+		entryArray _ self primLookupEntryIn: f index: index.
+		#badDirectoryPath = entryArray ifTrue: [
+			^(InvalidDirectoryError pathName: pathName asSqueakPathName) signal].
 		entryArray == nil
 			ifTrue: [done _ true]
 			ifFalse: [entries addLast: (DirectoryEntry fromArray: entryArray)].
 		index _ index + 1].
 
-	^ entries asArray
+	^ entries asArray collect: [:s | s convertFromSystemName].
