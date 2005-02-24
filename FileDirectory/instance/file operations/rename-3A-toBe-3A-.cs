81dd8d63d1c0f91d87d8a5e0cce5f958343e9fec
yo <unknown>
2005-02-24 18:34:00 +0000

    no message

--- FileDirectory/instance/file operations/rename-3A-toBe-3A-.cs
+++ FileDirectory/instance/file operations/rename-3A-toBe-3A-.cs
@@ -1,11 +1,13 @@
 rename: oldFileName toBe: newFileName
-	| selection |
+	| selection oldName newName |
 	"Rename the file of the given name to the new name. Fail if there is no file of the old name or if there is an existing file with the new name."
 	"Modified for retry after GC ar 3/21/98 18:09"
-
-	(self retryWithGC:[self primRename: (self fullNameFor: oldFileName)
-						to: (self fullNameFor: newFileName)]
-		until:[:result| result notNil]) ~~ nil ifTrue:[^self].
+	oldName _ self fullNameFor: oldFileName.
+	newName _ self fullNameFor: newFileName.
+	(StandardFileStream 
+		retryWithGC:[self primRename: oldName asVmPathName to: newName asVmPathName]
+		until:[:result| result notNil]
+		forFileNamed: oldName) ~~ nil ifTrue:[^self].
 	(self fileExists: oldFileName) ifFalse:[
 		^self error:'Attempt to rename a non-existent file'.
 	].
