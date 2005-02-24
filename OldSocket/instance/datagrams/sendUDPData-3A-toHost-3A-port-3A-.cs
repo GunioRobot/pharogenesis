571a221ca8d69034ba3fa11a72964bd72a779178
nk <unknown>
2005-02-24 14:37:00 +0000

    no message

---
+++ OldSocket/instance/datagrams/sendUDPData-3A-toHost-3A-port-3A-.cs
@@ -0,0 +1,18 @@
+sendUDPData: aStringOrByteArray toHost: hostAddress port: portNumber 
+	"Send a UDP packet containing the given data to the specified host/port."
+
+	| bytesToSend bytesSent count |
+	bytesToSend := aStringOrByteArray size.
+	bytesSent := 0.
+	[bytesSent < bytesToSend] whileTrue: 
+			[(self waitForSendDoneUntil: (self class deadlineSecs: 20)) 
+				ifFalse: [self error: 'send data timeout; data not sent'].
+			count := self 
+						primSocket: socketHandle
+						sendUDPData: aStringOrByteArray
+						toHost: hostAddress
+						port: portNumber
+						startIndex: bytesSent + 1
+						count: bytesToSend - bytesSent.
+			bytesSent := bytesSent + count].
+	^bytesSent
