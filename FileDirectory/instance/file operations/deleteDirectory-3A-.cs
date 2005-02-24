54b90654eb295246c4bfd08d46c5cceb2cff24eb
yo <unknown>
2005-02-24 18:33:00 +0000

    no message

--- FileDirectory/instance/file operations/deleteDirectory-3A-.cs
+++ FileDirectory/instance/file operations/deleteDirectory-3A-.cs
@@ -1,4 +1,4 @@
 deleteDirectory: localDirName
 	"Delete the directory with the given name in this directory. Fail if the path is bad or if a directory by that name does not exist."
 
- 	self primDeleteDirectory: (self fullNameFor: localDirName).
+ 	self primDeleteDirectory: (self fullNameFor: localDirName) asVmPathName.
