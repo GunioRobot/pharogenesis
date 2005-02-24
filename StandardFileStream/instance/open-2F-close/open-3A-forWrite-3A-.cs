e05e03048e69dc00836f82b080200218dfa4ce99
yo <unknown>
2005-02-24 18:34:00 +0000

    no message

--- StandardFileStream/instance/open-2F-close/open-3A-forWrite-3A-.cs
+++ StandardFileStream/instance/open-2F-close/open-3A-forWrite-3A-.cs
@@ -1,7 +1,12 @@
 open: fileName forWrite: writeMode 
 	"Open the file with the given name. If writeMode is true, allow writing, otherwise open the file in read-only mode."
 	"Changed to do a GC and retry before failing ar 3/21/98 17:25"
-	fileID _ self retryWithGC:[self primOpen: fileName writable: writeMode] until:[:id| id notNil].
+	| f |
+	f _ fileName asVmPathName.
+
+	fileID _ StandardFileStream retryWithGC:[self primOpen: f writable: writeMode] 
+					until:[:id| id notNil] 
+					forFileNamed: fileName.
 	fileID ifNil: [^ nil].  "allows sender to detect failure"
 	self register.
 	name _ fileName.
