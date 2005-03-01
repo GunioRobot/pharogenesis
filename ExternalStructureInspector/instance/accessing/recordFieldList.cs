e5e2adf502e77e1e3b478463a75595c091c3d7ee
gk <unknown>
2005-03-01 12:07:00 +0000

    no message

--- ExternalStructureInspector/instance/accessing/recordFieldList.cs
+++ ExternalStructureInspector/instance/accessing/recordFieldList.cs
@@ -2,4 +2,4 @@ recordFieldList
 	| fields |
 	fields _ object class fields.
 	(fields first isKindOf: Array) ifFalse: [fields _ Array with: fields].
-	^fields collect: [ :field | field first ] thenSelect: [:name | name ~~ #nil]
+	^fields collect: [ :field | field first ] thenSelect: [:name | name notNil]
