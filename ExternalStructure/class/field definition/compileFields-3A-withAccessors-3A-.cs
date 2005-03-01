d1ef4cb5f78dfb215c6a4c68e2deeb95afe4f709
gk <unknown>
2005-03-01 12:07:00 +0000

    no message

--- ExternalStructure/class/field definition/compileFields-3A-withAccessors-3A-.cs
+++ ExternalStructure/class/field definition/compileFields-3A-withAccessors-3A-.cs
@@ -1,37 +1,53 @@
-compileFields: specArray withAccessors: aBool
-	"Define all the fields in the receiver.
+compileFields: specArray withAccessors: aBool 
+	"Define all the fields in the receiver. 
 	Return the newly compiled spec."
-	| fieldName fieldType isPointerField externalType byteOffset typeSize typeSpec |
-	(specArray size > 0 and:[specArray first class ~~ Array])
-		ifTrue:[^self compileAlias: specArray withAccessors: aBool].
+	| fieldName fieldType isPointerField externalType byteOffset typeSize typeSpec selfRefering |
+	(specArray size > 0
+			and: [specArray first class ~~ Array])
+		ifTrue: [^ self compileAlias: specArray withAccessors: aBool].
 	byteOffset _ 1.
-	typeSpec _ WriteStream on: (WordArray new: 10).
-	typeSpec nextPut: (FFIFlagStructure). "dummy for size"
-	specArray do:[:spec|
-		fieldName _ spec first.
-		fieldType _ spec second.
-		isPointerField _ fieldType last = $*.
-		fieldType _ (fieldType findTokens: ' *') first.
-		externalType _ ExternalType atomicTypeNamed: fieldType.
-		externalType == nil ifTrue:["non-atomic"
-			Symbol hasInterned: fieldType ifTrue:[:sym|
-				externalType _ ExternalType structTypeNamed: sym]].
-		externalType == nil ifTrue:[
-			Transcript show:'(', fieldType,' is void)'.
-			externalType _ ExternalType void].
-		isPointerField ifTrue:[externalType _ externalType asPointerType].
-		typeSize _ externalType byteSize.
-		spec size > 2 ifTrue:["extra size"
-			spec third < typeSize ifTrue:[^self error:'Explicit type size is less than expected'].
-			typeSize _ spec third].
-		(fieldName ~~ #nil and:[aBool]) ifTrue:[
-			self defineFieldAccessorsFor: fieldName 
-				startingAt: byteOffset 
-				type: externalType].
-		typeSpec nextPutAll: (externalType embeddedSpecWithSize: typeSize).
-		byteOffset _ byteOffset + typeSize.
-	].
+	typeSpec _ WriteStream
+				on: (WordArray new: 10).
+	typeSpec nextPut: FFIFlagStructure.
+	"dummy for size"
+	specArray
+		do: [:spec | 
+			fieldName _ spec first.
+			fieldType _ spec second.
+			isPointerField _ fieldType last = $*.
+			fieldType _ (fieldType findTokens: ' *') first.
+			externalType _ ExternalType atomicTypeNamed: fieldType.
+			selfRefering _ externalType == nil and: fieldType = self asString and: isPointerField.
+			selfRefering
+				ifTrue: [externalType _ ExternalType void asPointerType]
+				ifFalse:
+			[externalType == nil
+				ifTrue: ["non-atomic"
+					Symbol
+						hasInterned: fieldType
+						ifTrue: [:sym | externalType _ ExternalType structTypeNamed: sym]].
+			externalType == nil
+				ifTrue: [Transcript show: '(' , fieldType , ' is void)'.
+					externalType _ ExternalType void].
+			isPointerField
+				ifTrue: [externalType _ externalType asPointerType]].
+			typeSize _ externalType byteSize.
+			spec size > 2
+				ifTrue: ["extra size"
+					spec third < typeSize
+						ifTrue: [^ self error: 'Explicit type size is less than expected'].
+					typeSize _ spec third].
+			(fieldName notNil and: [aBool])
+				ifTrue: [self
+						defineFieldAccessorsFor: fieldName
+						startingAt: byteOffset
+						type: externalType].
+			typeSpec
+				nextPutAll: (externalType embeddedSpecWithSize: typeSize).
+			byteOffset _ byteOffset + typeSize].
 	compiledSpec _ typeSpec contents.
-	compiledSpec at: 1 put: (byteOffset - 1 bitOr: FFIFlagStructure).
+	compiledSpec
+		at: 1
+		put: (byteOffset - 1 bitOr: FFIFlagStructure).
 	ExternalType noticeModificationOf: self.
-	^compiledSpec
+	^ compiledSpec
