eb5daf050c64f8bde0decd702ebe5399c06297af
gk <unknown>
2005-03-01 11:22:00 +0000

    no message

--- SocketStreamTest/instance/running/setUp.cs
+++ SocketStreamTest/instance/running/setUp.cs
@@ -1,6 +1,5 @@
 setUp
 	listener _ Socket createIfFail: [].
-	stream1 _ SocketStream on: listener.
-	"listener _ Socket newTCP listenOn: 7357 backlogSize: 5.
+	listener _ Socket newTCP listenOn: 7357.
 	stream1 _ SocketStream on: (Socket newTCP connectTo: NetNameResolver localHostAddress port: 7357).
-	stream2 _ SocketStream on: listener accept"
+	stream2 _ SocketStream on: listener accept
