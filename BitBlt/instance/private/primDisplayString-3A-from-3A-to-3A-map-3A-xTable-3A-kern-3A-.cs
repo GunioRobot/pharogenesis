076a413231d4cde51f402ce8f5f4e24b428fc0b8
yo <unknown>
2005-03-11 14:49:00 +0000

    no message

--- BitBlt/instance/private/primDisplayString-3A-from-3A-to-3A-map-3A-xTable-3A-kern-3A-.cs
+++ BitBlt/instance/private/primDisplayString-3A-from-3A-to-3A-map-3A-xTable-3A-kern-3A-.cs
@@ -1,11 +1,10 @@
 primDisplayString: aString from: startIndex to: stopIndex map: glyphMap xTable: xTable kern: kernDelta
-	| ascii glyph |
+	| ascii |
 	<primitive:'primitiveDisplayString' module:'BitBltPlugin'>
 	startIndex to: stopIndex do:[:charIndex|
 		ascii _ (aString at: charIndex) asciiValue.
-		glyph _ glyphMap at: ascii + 1.
-		sourceX _ xTable at: glyph + 1.
-		width _ (xTable at: glyph + 2) - sourceX.
+		sourceX _ xTable at: ascii + 1.
+		width _ (xTable at: ascii + 2) - sourceX.
 		self copyBits.
 		destX _ destX + width + kernDelta.
 	].
