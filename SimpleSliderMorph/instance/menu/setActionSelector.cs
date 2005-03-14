301462fae437d92fccc5fc47c77bb902630bbf22
yo <unknown>
2005-03-14 13:06:00 +0000

    no message

--- SimpleSliderMorph/instance/menu/setActionSelector.cs
+++ SimpleSliderMorph/instance/menu/setActionSelector.cs
@@ -1,13 +1,8 @@
 setActionSelector
-
 	| oldSel newSel |
-	setValueSelector == nil
-		ifTrue: [oldSel _ '']
-		ifFalse: [oldSel _ setValueSelector].
-	newSel _ FillInTheBlank
-		request:
-'Please type the selector to be sent to
-the target when this slider is changed'
-		initialAnswer: oldSel.
-	newSel isEmpty
-		ifFalse: [self actionSelector: newSel].
+	oldSel := setValueSelector isNil ifTrue: [''] ifFalse: [setValueSelector].
+	newSel := FillInTheBlank 
+				request: 'Please type the selector to be sent to
+the target when this slider is changed' translated
+				initialAnswer: oldSel.
+	newSel isEmpty ifFalse: [self actionSelector: newSel]
