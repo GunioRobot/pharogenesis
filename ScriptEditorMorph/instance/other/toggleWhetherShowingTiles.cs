fcae8356f1d611952a35fb3680073b767b5d4b7e
yo <unknown>
2005-03-14 12:43:00 +0000

    no message

--- ScriptEditorMorph/instance/other/toggleWhetherShowingTiles.cs
+++ ScriptEditorMorph/instance/other/toggleWhetherShowingTiles.cs
@@ -4,9 +4,20 @@ toggleWhetherShowingTiles
 	self showingMethodPane
 		ifFalse:				"currently showing tiles"
 			[self showSourceInScriptor]
+
 		ifTrue:				"current showing textual source"
-			[self savedTileVersionsCount >= 1
+			[Preferences universalTiles
+				ifTrue: [^ self revertToTileVersion].
+			self savedTileVersionsCount >= 1
 				ifTrue:
-					[self revertToTileVersion]
+					[(self userScriptObject lastSourceString = (playerScripted class compiledMethodAt: scriptName) decompileString)
+						ifFalse:
+							[(self confirm: 
+'Caution -- this script was changed
+textually; if you revert to tiles at this
+point you will lose all the changes you
+may have made textually.  Do you
+really want to do this?' translated) ifFalse: [^ self]].
+					self revertToTileVersion]
 				ifFalse:
-					[self beep]]
+					[Beeper beep]]
