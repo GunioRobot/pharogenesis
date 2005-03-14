2f0f4c52100339eaf5f0da5f5583d5cbb1385e34
yo <unknown>
2005-03-14 13:11:00 +0000

    no message

--- JoystickMorph/instance/menu/setYRange.cs
+++ JoystickMorph/instance/menu/setYRange.cs
@@ -3,7 +3,7 @@ setYRange
 	| range |
 	range _ FillInTheBlank
 		request:
-'Type the maximum value for the Y axis'
+'Type the maximum value for the Y axis'  translated
 		initialAnswer: ((yScale * (self width - handleMorph width) / 2.0) roundTo: 0.01) printString.
 	range isEmpty ifFalse: [
 		yScale _ (2.0 * range asNumber asFloat) / (self width - handleMorph width)].
