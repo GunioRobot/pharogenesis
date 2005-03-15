4f66ae373203773556f776532fc1e5aa90df6130
tak <unknown>
2005-03-15 11:40:00 +0000

    no message

--- PhraseTileMorph/instance/mouse/catchDivideByZero-3A-indent-3A-.cs
+++ PhraseTileMorph/instance/mouse/catchDivideByZero-3A-indent-3A-.cs
@@ -1,10 +1,12 @@
-catchDivideByZero: aStream indent: tabCount
+catchDivideByZero: aStream indent: tabCount 
 	"See if I am have divide as my operator. If so, insert a test in the argument to divide."
 
-	(submorphs at: 2) type = #operator ifFalse: [^ false].		"not me"
-	(submorphs at: 2) operatorOrExpression == #/ ifFalse: [^ false].	"not me"
+	| exp |
+	submorphs second type = #operator ifFalse: [^false].	"not me"
+	exp _ submorphs second operatorOrExpression.
+	(#(/ // \\) includes: exp) ifFalse: [^false].	"not me"
 	aStream space.
 	aStream nextPutAll: '(self beNotZero: '.
-	(submorphs at: 3) storeCodeOn: aStream indent: tabCount.
+	(submorphs third) storeCodeOn: aStream indent: tabCount.
 	aStream nextPut: $).
-	^ true
+	^true
