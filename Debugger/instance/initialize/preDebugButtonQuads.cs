ec28e6bc46e327d6924657c3df093099d964d40a
yo <unknown>
2005-03-15 13:18:00 +0000

    no message

--- Debugger/instance/initialize/preDebugButtonQuads.cs
+++ Debugger/instance/initialize/preDebugButtonQuads.cs
@@ -1,5 +1,13 @@
 preDebugButtonQuads
 
-	^ #(('Proceed'		proceed 	blue 	'continue execution' )
-		('Abandon'		abandon 	black 	'abandon this execution by closing this window')
-		('Debug'		debug 		red 		'bring up a debugger'))
+	^Preferences eToyFriendly
+		ifTrue: [
+	{
+	{'Store log' translated.	#storeLog. 	#blue. 	'write a log of the encountered problem' translated}.
+	{'Abandon' translated.	#abandon. 	#black.	'abandon this execution by closing this window' translated}.
+	{'Debug'	 translated.		#debug. 	#red. 	'bring up a debugger' translated}}]
+		ifFalse: [
+	{
+	{'Proceed' translated.	#proceed. 	#blue. 	'continue execution' translated}.
+	{'Abandon' translated.	#abandon. 	#black.	'abandon this execution by closing this window' translated}.
+	{'Debug'	 translated.		#debug.		#red. 	'bring up a debugger' translated}}]
