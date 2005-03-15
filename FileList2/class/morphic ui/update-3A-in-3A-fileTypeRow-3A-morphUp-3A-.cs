1eace24f7b4d82016a921f285b26283f2b18edf6
yo <unknown>
2005-03-15 12:36:00 +0000

    no message

--- FileList2/class/morphic ui/update-3A-in-3A-fileTypeRow-3A-morphUp-3A-.cs
+++ FileList2/class/morphic ui/update-3A-in-3A-fileTypeRow-3A-morphUp-3A-.cs
@@ -17,14 +17,11 @@ update: actionRow in: window fileTypeRow: fileTypeRow morphUp: morph
 	textColor1 _ Color r: 0.742 g: 0.839 b: 1.0.
 	actionRow removeAllMorphs.
 	fileTypeInfo _ self endingSpecs.
-	info2 _ fileTypeInfo detect: [ :each | each first = fileTypeString] ifNone: [self halt].
+	info2 _ fileTypeInfo detect: [ :each | each first = fileTypeString] ifNone: [self error: 'bad fileTypeString' ].
 	fileSuffixes _ info2 second.
 	fileActions _ info2 third.
-	buttons _ fileActions, #(('Cancel this search' cancelHit 'Cancel')) collect: [ :each |
-		(self blueButtonText: each third textColor: textColor1 inWindow: window)
-			setBalloonText: each first;
-			on: #mouseUp send: each second to: aFileList.
-	].
+	buttons _ fileActions collect: [ :each | aFileList blueButtonForService: each textColor: textColor1 inWindow: window ].
+	buttons addLast: (self blueButtonText: 'Cancel' textColor: textColor1 inWindow: window balloonText: 'Cancel this search' translated selector: #cancelHit recipient: aFileList).
 	buttons do: [ :each | actionRow addMorphBack: each].
 	window fullBounds.
 	buttons do: [ :each |
