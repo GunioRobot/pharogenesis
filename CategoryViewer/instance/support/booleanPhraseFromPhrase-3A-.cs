4e5e507ab2205cd078cba74ab0eb3307c99c98aa
sw <unknown>
2005-03-15 22:33:00 +0000

    no message

--- CategoryViewer/instance/support/booleanPhraseFromPhrase-3A-.cs
+++ CategoryViewer/instance/support/booleanPhraseFromPhrase-3A-.cs
@@ -1,12 +1,17 @@
 booleanPhraseFromPhrase: phrase
+	"Answer, if possible, a boolean-valued phrase derived from the phrase provided"
 
 	|  retrieverOp retrieverTile |
+	(phrase isKindOf: ParameterTile) ifTrue: [^ phrase booleanComparatorPhrase].
 	phrase isBoolean ifTrue: [^ phrase].
-	scriptedPlayer costume isInWorld ifFalse: [^ Array new].
+
+	((scriptedPlayer respondsTo: #costume) 
+		and:[scriptedPlayer costume isInWorld not]) ifTrue: [^ Array new].
 	((retrieverTile _ phrase submorphs last) isKindOf: TileMorph) ifFalse: [^ phrase].
 	retrieverOp _ retrieverTile operatorOrExpression.
 
-	(#(color number player) includes: phrase resultType) ifTrue:
-		[^ self booleanPhraseForRetrieverOfType: phrase resultType retrieverOp: retrieverOp player: phrase actualObject].
+	(Vocabulary vocabularyForType: phrase resultType)
+		affordsCoercionToBoolean ifTrue: 
+			[^ self booleanPhraseForRetrieverOfType: phrase resultType retrieverOp: retrieverOp player: phrase actualObject].
 
 	^ phrase
