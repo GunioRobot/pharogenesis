aba06aa30639e9ee9009ca76b8fc3dfc92c41f9a
yo <unknown>
2005-03-15 12:24:00 +0000

    no message

--- FileList2/class/blue ui/endingSpecs.cs
+++ FileList2/class/blue ui/endingSpecs.cs
@@ -1,44 +1,40 @@
 endingSpecs
+	"Answer a collection of specs to build the selective 'find anything' tool called by the Navigator. This version uses the services registry to do so."
+	"FileList2 morphicViewGeneralLoaderInWorld: World"
+	| categories services specs rejects |
+	rejects _ #(addFileToNewZip: compressFile: openInZipViewer: extractAllFrom: openOn:).
+	categories _ #(
+		('Art' ('bmp' 'gif' 'jpg' 'jpeg' 'form' 'png' 'pcx' 'xbm' 'xpm' 'ppm' 'pbm'))
+		('Morphs' ('morph' 'morphs' 'sp'))
+		('Projects' ('extseg' 'project' 'pr'))
+		('Books' ('bo'))
+		('Music' ('mid'))
+		('Movies' ('movie' 'mpg' 'mpeg' 'qt' 'mov'))
+		"('Code' ('st' 'cs'))"
+		('Flash' ('swf'))
+		('TrueType' ('ttf'))
+		('3ds' ('3ds'))
+		('Tape' ('tape'))
+		('Wonderland' ('wrl'))
+		('HTML' ('htm' 'html'))
+	).
+	categories first at: 2 put: ImageReadWriter allTypicalFileExtensions.
+	specs _ OrderedCollection new.
+	categories do: [ :cat | | catSpecs catServices okExtensions |
+		services _ Dictionary new.
+		catSpecs _ Array new: 3.
+		catServices _ OrderedCollection new.
+		okExtensions _ Set new.
 
-	^#(
-		('Art' ('bmp' 'gif' 'jpg' 'form' 'png')
-			(('open image in a window' openImageInWindow 'View')
-			('read image into ImageImports' importImage 'Import')
-			('open image as background' openAsBackground 'World'))
-		)
-
-		('Morphs' ('morph' 'morphs' 'sp')
-			(('load as morph' openMorphFromFile 'Morph')
-			('load as project' openProjectFromFile 'Project'))
-		)
-
-		('Projects' ('extseg' 'project' 'pr') (('load as project' openProjectFromFile 'Load')))
-
-		('Books' ('bo') (('load as book' openBookFromFile 'Open')))
-
-		('Music' ('mid') (('play midi file' playMidiFile 'Play')))
-
-		('Movies' ('movie') (('open as movie' openAsMovie 'Open')))
-
-		"('Code' ('st' 'cs')
-			(('fileIn' fileInSelection)
-			('file into new change set' fileIntoNewChangeSet)
-			('browse changes' browseChanges)
-			('browse code' browseFile)
-			('remove line feeds' removeLinefeeds)
-			('broadcast as update' putUpdate))
-		)"
-
-		('Flash' ('swf') (('open as Flash' openAsFlash 'Open')))
-
-		('TrueType' ('ttf') (('open true type font' openAsTTF 'Open')))
-
-		('3ds' ('3ds') (('Open 3DS file' open3DSFile' Open')))
-
-		('Tape' ('tape') (('open for playback' openTapeFromFile 'Open')))
-
-		('Wonderland' ('wrl') (('open in Wonderland' openVRMLFile 'Open')))
-
-		('HTML' ('htm' 'html') (('open in browser' openInBrowser 'Open')))
-
-	)
+		cat second do: [ :ext | (FileList itemsForFile: 'fred.',ext) do: [ :i |
+			(rejects includes: i selector) ifFalse: [
+				okExtensions add: ext.
+				services at: i label put: i ]]].
+		services do: [ :svc | catServices add: svc ].
+		services isEmpty ifFalse: [ 
+			catSpecs at: 1 put: cat first translated;
+				at: 2 put: okExtensions;
+				at: 3 put: catServices.
+			specs add: catSpecs ]
+	].
+	^specs
