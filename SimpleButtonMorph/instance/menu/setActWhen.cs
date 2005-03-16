45d9fbc3324b6f6a1b49156630c3ec348627e8b7
yo <unknown>
2005-03-16 21:00:00 +0000

    no message

--- SimpleButtonMorph/instance/menu/setActWhen.cs
+++ SimpleButtonMorph/instance/menu/setActWhen.cs
@@ -1,3 +1,7 @@
 setActWhen
-	actWhen _ (SelectionMenu selections: #(buttonDown buttonUp whilePressed))
-		startUpWithCaption: 'Choose one of the following conditions'
+
+	| selections |
+	selections _ #(buttonDown buttonUp whilePressed startDrag).
+	actWhen := (SelectionMenu 
+				labels: (selections collect: [:t | t translated]) selections: selections) 
+					startUpWithCaption: 'Choose one of the following conditions' translated
