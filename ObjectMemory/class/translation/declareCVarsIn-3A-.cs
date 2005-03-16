cc25c75a1b9297a31c2017f9adb199d7c65e7f94
nk <unknown>
2005-03-16 17:24:00 +0000

    no message

--- ObjectMemory/class/translation/declareCVarsIn-3A-.cs
+++ ObjectMemory/class/translation/declareCVarsIn-3A-.cs
@@ -5,4 +5,12 @@ declareCVarsIn: aCCodeGenerator
 		declareC: 'int remapBuffer[', (RemapBufferSize + 1) printString, ']'.
 	aCCodeGenerator
 		var: #rootTable
-		declareC: 'int rootTable[', (RootTableSize + 1) printString, ']'
+		declareC: 'int rootTable[', (RootTableSize + 1) printString, ']'.
+	aCCodeGenerator
+		var: #headerTypeBytes
+		declareC: 'int headerTypeBytes[4]'.
+	
+	aCCodeGenerator var: #youngStart type: 'unsigned'.
+	aCCodeGenerator var: #endOfMemory type: 'unsigned'.
+	aCCodeGenerator var: #memoryLimit type: 'unsigned'.
+	aCCodeGenerator var: #youngStartLocal type: 'unsigned'.
