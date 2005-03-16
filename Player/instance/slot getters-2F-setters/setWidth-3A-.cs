e9746d116e323e94b28dbd631229041bcda0fb55
yo <unknown>
2005-03-16 15:30:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/setWidth-3A-.cs
+++ Player/instance/slot getters-2F-setters/setWidth-3A-.cs
@@ -1,4 +1,15 @@
-setWidth: w
-	"Set the width to the given value"
+setWidth: aWidth
+	"Set the width"
 
-	^ self costume width: w
+	| cost widthToUse |
+	cost _ self costume.
+	cost isWorldMorph ifTrue: [^ self].
+	cost isLineMorph
+		ifTrue:
+			[^ cost unrotatedWidth: aWidth].
+	widthToUse _ cost isRenderer
+		ifTrue:
+			[aWidth / cost scaleFactor]
+		ifFalse:
+			[aWidth].
+	cost renderedMorph width: widthToUse
