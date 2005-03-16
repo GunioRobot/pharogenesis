ff4879d580176d8fc21ef1bfb73b790f4e59c0af
yo <unknown>
2005-03-16 21:02:00 +0000

    no message

--- StringButtonMorph/instance/menu/setActWhen.cs
+++ StringButtonMorph/instance/menu/setActWhen.cs
@@ -1,3 +1,6 @@
 setActWhen
-	actWhen _ (SelectionMenu selections: #(buttonDown buttonUp whilePressed))
-		startUpWithCaption: 'Choose one of the following conditions'
+
+	| selections |
+	selections _ #(buttonDown buttonUp whilePressed).
+	actWhen _ (SelectionMenu labelList: (selections collect: [:t | t translated]) selections: selections)
+		startUpWithCaption: 'Choose one of the following conditions' translated.
