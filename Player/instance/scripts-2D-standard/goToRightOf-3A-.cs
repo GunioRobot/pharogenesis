71f082e13d62a82f464cd6e9b8fa2bff51ec0474
yo <unknown>
2005-03-16 15:43:00 +0000

    no message

--- Player/instance/scripts-2D-standard/goToRightOf-3A-.cs
+++ Player/instance/scripts-2D-standard/goToRightOf-3A-.cs
@@ -1,10 +1,12 @@
-goToRightOf: aPlayer
+goToRightOf: aPlayer 
 	"Place the object so that it lies directly to the right of the given object"
 
 	| hisCostume aCostume |
-	(aPlayer == nil or: [aPlayer == self]) ifTrue: [^ self].
-	(hisCostume _ aPlayer costume) isInWorld ifFalse: [^ self].
-	((aCostume _ self costume) owner == hisCostume owner) ifFalse:
-		[hisCostume owner addMorphFront: aCostume].
-	aCostume position:
-		(hisCostume bounds rightCenter - (0 @ (aCostume height // 2)))
+	(aPlayer isNil or: [aPlayer == self]) ifTrue: [^self].
+	(hisCostume := aPlayer costume) isInWorld ifFalse: [^self].
+	aCostume := self costume.
+	aCostume isWorldMorph ifTrue: [^ self].
+	aCostume owner == hisCostume owner 
+		ifFalse: [hisCostume owner addMorphFront: aCostume].
+	aCostume 
+		position: hisCostume bounds rightCenter - (0 @ (aCostume height // 2))
