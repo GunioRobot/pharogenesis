48c4872db961076c8dd0bfa016cb005ebab7a9f7
nk <unknown>
2005-03-16 16:30:00 +0000

    no message

--- Interpreter/instance/utilities/externalizeIPandSP.cs
+++ Interpreter/instance/utilities/externalizeIPandSP.cs
@@ -1,6 +1,6 @@
 externalizeIPandSP
 	"Copy the local instruction and stack pointer to global variables for use in primitives and other functions outside the interpret loop."
 
-	instructionPointer _ self cCoerce: localIP to: 'int'.
-	stackPointer _ self cCoerce: localSP to: 'int'.
+	instructionPointer _ self cCoerce: localIP to: 'unsigned'.
+	stackPointer _ self cCoerce: localSP to: 'unsigned'.
 	theHomeContext _ localHomeContext.
