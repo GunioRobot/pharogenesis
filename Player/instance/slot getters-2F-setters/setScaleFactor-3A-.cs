da96195c85a7cdff355855bd9991911693b0a861
yo <unknown>
2005-03-16 15:33:00 +0000

    no message

--- Player/instance/slot getters-2F-setters/setScaleFactor-3A-.cs
+++ Player/instance/slot getters-2F-setters/setScaleFactor-3A-.cs
@@ -1,13 +1,7 @@
 setScaleFactor: aNumber
 	"Set the scale factor to be the given value"
 
-	| aCostume |
-	aCostume _ self costume.
-	1.0 = aNumber
-		ifTrue:
-			[0.0 = self getHeading ifTrue:
-				[aCostume isFlexMorph ifTrue: [aCostume removeFlexShell].
-				^ self]]
-		ifFalse:
-			[aCostume isFlexMorph ifFalse: [aCostume addFlexShell]].
-	costume scale: (aNumber asFloat max: 0.125)
+	| cost |
+	cost _ self costume.
+	cost isWorldMorph ifTrue: [^ self].
+	cost scaleFactor: ((aNumber asFloat max: 0.1) min: 10.0)
