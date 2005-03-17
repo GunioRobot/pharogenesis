08721e905a426b891e979b8831524fa7344f937c
sw <unknown>
2005-03-17 00:47:00 +0000

    no message

--- Player/instance/misc/beNotZero-3A-.cs
+++ Player/instance/misc/beNotZero-3A-.cs
@@ -1,5 +1,5 @@
 beNotZero: aNumber
-	"This is a runtime check if the arg to divide in a script is zero.  If it is, put up a message from M. Mouse.  Return 0.001 instead of 0.  Note the time.  If fails again within 1 min., don't tell the user again."
+	"This is a runtime check if the arg to divide in a script is zero.  If it is, put up a warning message.  Return 0.001 instead of 0.  Note the time.  If fails again within 1 min., don't tell the user again."
 
 	aNumber = 0 ifFalse: [^ aNumber].	"normal case"
 	"We have a problem"
@@ -7,10 +7,9 @@ beNotZero: aNumber
 		ifNil: [TimeOfError _ Time totalSeconds]
 		ifNotNil: [(Time totalSeconds - TimeOfError) > 45 ifTrue: [
 			TimeOfError _ Time totalSeconds.	"in case user interrupt and reenter"
-			PopUpMenu inform: 
-'Dividing by zero makes a number too 
-large for even a Sorcerer to handle.  
-Please change your script.  
-					-- M. Mouse'.
+			self inform: 
+'Dividing by zero makes a number too
+large for even a Sorcerer to handle.
+Please change your script.' translated.
 			TimeOfError _ Time totalSeconds]].
 	^ 0.001
