97e268011615624a26465029479f9855fdb6a78f
sw <unknown>
2005-03-24 01:55:00 +0000

    no message

--- ScriptEditorMorph/instance/buttons/editMethodDescription.cs
+++ ScriptEditorMorph/instance/buttons/editMethodDescription.cs
@@ -1,4 +1,5 @@
 editMethodDescription
 	"Edit the balloon help associated with the script"
 
-	self userScriptObject editDescription
+	self userScriptObject editDescription.
+	playerScripted updateAllViewers
