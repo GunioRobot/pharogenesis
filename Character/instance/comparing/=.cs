41984e1298fa4c711577702a0a12a63ef1105e04
ar <unknown>
2005-04-09 21:48:00 +0000

    no message

--- Character/instance/comparing/=.cs
+++ Character/instance/comparing/=.cs
@@ -3,7 +3,5 @@
 	object (have the same object pointer) and false otherwise. Optional. See
 	Object documentation whatIsAPrimitive."
 
-	"<primitive: 110>"
-	^ self == aCharacter
-		or: [aCharacter class == MultiCharacter
-				and: [aCharacter asciiValue = self asciiValue]].
+	^ self == aCharacter or:[
+		aCharacter isCharacter and: [self asciiValue = aCharacter asciiValue]]
