f73be55e732da9d68acca26e864f51bc99b46530
ar <unknown>
2005-04-09 22:19:00 +0000

    no message

--- Character/class/instance creation/value-3A-.cs
+++ Character/class/instance creation/value-3A-.cs
@@ -1,5 +1,5 @@
 value: anInteger 
 	"Answer the Character whose value is anInteger."
 
-	anInteger > 255 ifTrue: [^ MultiCharacter value: anInteger].
+	anInteger > 255 ifTrue: [^self basicNew setValue: anInteger].
 	^ CharacterTable at: anInteger + 1.
