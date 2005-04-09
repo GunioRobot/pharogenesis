41e8da8c6b5adc235bdeb41d6d5591a588ec957b
ar <unknown>
2005-04-09 22:37:00 +0000

    no message

--- CharacterSet/instance/collection ops/do-3A-.cs
+++ CharacterSet/instance/collection ops/do-3A-.cs
@@ -1,5 +1,5 @@
 do: aBlock
 	"evaluate aBlock with each character in the set"
 
-	Character allCharacters do: [ :c |
+	Character allByteCharacters do: [ :c |
 		(self includes: c) ifTrue: [ aBlock value: c ] ]
