3bf6a5ef8dea2bb75e801e54ffaca9f3f61c1586
ar <unknown>
2005-04-09 22:33:00 +0000

    no message

--- Unicode/class/instance creation/value-3A-.cs
+++ Unicode/class/instance creation/value-3A-.cs
@@ -4,4 +4,4 @@ value: code
 	code < 256 ifTrue: [^ Character value: code].
 	l _ Locale currentPlatform languageEnvironment leadingChar.
 	l = 0 ifTrue: [l _ 255].
-	^ MultiCharacter leadingChar: l code: code.
+	^ Character leadingChar: l code: code.
