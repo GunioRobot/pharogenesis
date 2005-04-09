87d90d673e4404b2068f80897b70ffa71561b5d2
ar <unknown>
2005-04-09 21:51:00 +0000

    no message

--- Character/instance/converting/asUnicode.cs
+++ Character/instance/converting/asUnicode.cs
@@ -1,3 +1,13 @@
 asUnicode
 
-	^ value
+	| table charset v |
+	self leadingChar = 0 ifTrue: [^ value].
+	charset _ EncodedCharSet charsetAt: self leadingChar.
+	charset isCharset ifFalse: [^ self charCode].
+	table _ charset ucsTable.
+	table isNil ifTrue: [^ 16rFFFD].
+
+	v _ table at: self charCode + 1.
+	v = -1 ifTrue: [^ 16rFFFD].
+
+	^ v.
