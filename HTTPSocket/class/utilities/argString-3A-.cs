c69aad41fed38c46f6cb5c6615a8cc6018fbe5ed
ar <unknown>
2005-04-10 18:47:00 +0000

    no message

--- HTTPSocket/class/utilities/argString-3A-.cs
+++ HTTPSocket/class/utilities/argString-3A-.cs
@@ -2,7 +2,7 @@ argString: args
 	"Return the args in a long string, as encoded in a url"
 
 	| argsString first |
-	args class == String ifTrue: ["sent in as a string, not a dictionary"
+	args isString ifTrue: ["sent in as a string, not a dictionary"
 		^ (args first = $? ifTrue: [''] ifFalse: ['?']), args].
 	argsString _ WriteStream on: String new.
 	argsString nextPut: $?.
