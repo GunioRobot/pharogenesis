7c9f620c9782ea29670465ac4f89d0f5f2206fd6
ar <unknown>
2005-04-10 18:45:00 +0000

    no message

--- BookPageThumbnailMorph/instance/fileIn-2F-Out/objectsInMemory.cs
+++ BookPageThumbnailMorph/instance/fileIn-2F-Out/objectsInMemory.cs
@@ -2,11 +2,11 @@ objectsInMemory
 	"See if page or bookMorph need to be brought in from a server."
 	| bookUrl bk wld try |
 	bookMorph ifNil: ["fetch the page"
-		page class == String ifFalse: [^ self].	"a morph"
+		page isString ifFalse: [^ self].	"a morph"
 		try _ (SqueakPageCache atURL: page) fetchContents.
 		try ifNotNil: [page _ try].
 		^ self].
-	bookMorph class == String ifTrue: [
+	bookMorph isString ifTrue: [
 		bookUrl _ bookMorph.
 		(wld _ self world) ifNil: [wld _ Smalltalk currentWorld].
 		bk _ BookMorph isInWorld: wld withUrl: bookUrl.
@@ -15,6 +15,6 @@ objectsInMemory
 		bk == #out ifTrue: [
 			(bk _ BookMorph new fromURL: bookUrl) ifNil: [^ self]].
 		bookMorph _ bk].
-	page class == String ifTrue: [
+	page isString ifTrue: [
 		page _ (bookMorph pages detect: [:pg | pg url = page] 
 					ifNone: [bookMorph pages first])].
