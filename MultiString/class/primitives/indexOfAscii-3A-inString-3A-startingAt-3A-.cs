674b2bc5d4a17abf53f9e6825f843aada99f5817
ar <unknown>
2005-04-10 16:32:00 +0000

    no message

--- MultiString/class/primitives/indexOfAscii-3A-inString-3A-startingAt-3A-.cs
+++ MultiString/class/primitives/indexOfAscii-3A-inString-3A-startingAt-3A-.cs
@@ -1,3 +1,12 @@
 indexOfAscii: anInteger inString: aString startingAt: start
 
-	^ self indexOfAscii: anInteger inMultiString: aString startingAt: start.
+	| stringSize |
+
+	self var: #aCharacter declareC: 'int anInteger'.
+	self var: #aString declareC: 'unsigned int *aString'.
+
+	stringSize _ aString size.
+	start to: stringSize do: [:pos |
+		(aString at: pos) asciiValue = anInteger ifTrue: [^ pos]].
+
+	^ 0
