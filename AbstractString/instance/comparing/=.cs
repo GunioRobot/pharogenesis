3dd83abfc54912eaae52fe4a2cff32223566f519
ar <unknown>
2005-04-10 16:48:00 +0000

    no message

--- AbstractString/instance/comparing/=.cs
+++ AbstractString/instance/comparing/=.cs
@@ -1,5 +1,5 @@
 = aString 
 	"Answer whether the receiver sorts equally as aString.
 	The collation order is simple ascii (with case differences)."
-
-	self subclassResponsibility.
+	aString isString ifFalse:[^false].
+	^(self compare: aString caseSensitive: true) = 2
