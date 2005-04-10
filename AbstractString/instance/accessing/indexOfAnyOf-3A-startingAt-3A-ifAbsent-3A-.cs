769f7f08239d5929d14c8c028a03874a7127d842
ar <unknown>
2005-04-10 16:22:00 +0000

    no message

--- AbstractString/instance/accessing/indexOfAnyOf-3A-startingAt-3A-ifAbsent-3A-.cs
+++ AbstractString/instance/accessing/indexOfAnyOf-3A-startingAt-3A-ifAbsent-3A-.cs
@@ -2,7 +2,7 @@ indexOfAnyOf: aCharacterSet  startingAt: start ifAbsent: aBlock
 	"returns the index of the first character in the given set, starting from start"
 
 	| ans |
-	ans _ String findFirstInString: self  inSet: aCharacterSet byteArrayMap startingAt: start.
+	ans _ self class findFirstInString: self  inSet: aCharacterSet byteArrayMap startingAt: start.
 
 	ans = 0 
 		ifTrue: [ ^aBlock value ]
