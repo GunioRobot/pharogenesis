991e4495c5af00b5d9cb2e859d7bfc748532d27f
ar <unknown>
2005-04-10 18:48:00 +0000

    no message

--- HTTPSocket/class/proxy settings/useProxyServerNamed-3A-port-3A-.cs
+++ HTTPSocket/class/proxy settings/useProxyServerNamed-3A-port-3A-.cs
@@ -6,10 +6,10 @@ useProxyServerNamed: proxyServerName port: portNum
 		HTTPProxyPort _ 80.
 		^ self].
 
-	proxyServerName class == String
+	proxyServerName isString
 		ifFalse: [self error: 'Server name must be a String or nil'].
 	HTTPProxyServer _ proxyServerName.
 
 	HTTPProxyPort _ portNum.
-	HTTPProxyPort class == String ifTrue: [HTTPProxyPort _ portNum asNumber].
+	HTTPProxyPort isString ifTrue: [HTTPProxyPort _ portNum asNumber].
 	HTTPProxyPort ifNil: [HTTPProxyPort _ self defaultPort].
