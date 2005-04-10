57387bd8336a10f8d8c2735c371d1749d72747d3
ar <unknown>
2005-04-10 19:26:00 +0000

    no message

--- RWBinaryOrTextStream/instance/as yet unclassified/next-3A-.cs
+++ RWBinaryOrTextStream/instance/as yet unclassified/next-3A-.cs
@@ -2,6 +2,5 @@ next: anInteger
 	"Answer the next anInteger elements of my collection. Must override to get class right."
 
 	| newArray |
-self flag: #ByteString.
-	newArray _ (isBinary ifTrue: [ByteArray] ifFalse: [String]) new: anInteger.
+	newArray _ (isBinary ifTrue: [ByteArray] ifFalse: [ByteString]) new: anInteger.
 	^ self nextInto: newArray
