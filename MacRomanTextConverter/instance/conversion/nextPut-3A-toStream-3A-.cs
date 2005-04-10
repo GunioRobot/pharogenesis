1ccddb554e0d0dd1ac166864767301639b0f8136
ar <unknown>
2005-04-10 16:05:00 +0000

    no message

--- MacRomanTextConverter/instance/conversion/nextPut-3A-toStream-3A-.cs
+++ MacRomanTextConverter/instance/conversion/nextPut-3A-toStream-3A-.cs
@@ -1,13 +1,4 @@
 nextPut: aCharacter toStream: aStream 
 
-	aStream isBinary ifTrue: [
-		aCharacter class == Character ifTrue: [
-			aStream basicNextPut: aCharacter.
-			^ aStream.
-		].
-		aCharacter class == MultiCharacter ifTrue: [
-			aStream nextInt32Put: aCharacter value.
-			^ aStream.
-		].
-	].
-	aStream basicNextPut: aCharacter isoToSqueak.
+	aStream isBinary ifTrue: [^aCharacter storeBinaryOn: aStream].
+	aStream basicNextPut: aCharacter squeakToMac.
