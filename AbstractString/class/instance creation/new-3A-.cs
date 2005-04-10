58fc5a60901555099a26836f00603b30b85e3172
ar <unknown>
2005-04-10 19:47:00 +0000

    no message

--- AbstractString/class/instance creation/new-3A-.cs
+++ AbstractString/class/instance creation/new-3A-.cs
@@ -1,4 +1,5 @@
 new: sizeRequested 
 	"Answer an instance of this class with the number of indexable
 	variables specified by the argument, sizeRequested."
+self flag: #ByteString.
 	^String new: sizeRequested
