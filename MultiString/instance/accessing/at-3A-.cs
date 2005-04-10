7de57cc520a29a1c3ae427831149b598995994d0
ar <unknown>
2005-04-10 17:14:00 +0000

    no message

--- MultiString/instance/accessing/at-3A-.cs
+++ MultiString/instance/accessing/at-3A-.cs
@@ -1,3 +1,3 @@
 at: index 
-
-	^ MultiCharacter value: (self basicAt: index).
+	"Answer the Character stored in the field of the receiver indexed by the argument."
+	^ Character value: (self wordAt: index).
