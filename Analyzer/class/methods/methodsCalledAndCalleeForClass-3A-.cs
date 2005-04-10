a0eb4df9cd9f0df00c55a4d95fe142ef41a2ba94
ar <unknown>
2005-04-10 22:15:00 +0000

    no message

--- Analyzer/class/methods/methodsCalledAndCalleeForClass-3A-.cs
+++ Analyzer/class/methods/methodsCalledAndCalleeForClass-3A-.cs
@@ -3,7 +3,7 @@ methodsCalledAndCalleeForClass: aClass
 	r := Set new.
 	aClass methodDict
 		associationsDo: [:assoc | (assoc value literals
-				select: [:l | l isKindOf: Symbol])
+				select: [:l | l isSymbol])
 				do: [:ll | r 
 						add: (Array with: assoc key with: ll)]].
 	^ r
