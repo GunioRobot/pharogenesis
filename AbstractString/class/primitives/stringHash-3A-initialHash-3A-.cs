42f8fa0f2717653cc87a29d7df7d8efe1fa7e031
ar <unknown>
2005-04-10 16:29:00 +0000

    no message

--- AbstractString/class/primitives/stringHash-3A-initialHash-3A-.cs
+++ AbstractString/class/primitives/stringHash-3A-initialHash-3A-.cs
@@ -1,3 +1,11 @@
 stringHash: aString initialHash: speciesHash
-
-	self subclassResponsibility.
+	| stringSize hash low |
+	stringSize _ aString size.
+	hash _ speciesHash bitAnd: 16rFFFFFFF.
+	1 to: stringSize do: [:pos |
+		hash _ hash + (aString at: pos) asInteger.
+		"Begin hashMultiply"
+		low _ hash bitAnd: 16383.
+		hash _ (16r260D * low + ((16r260D * (hash bitShift: -14) + (16r0065 * low) bitAnd: 16383) * 16384)) bitAnd: 16r0FFFFFFF.
+	].
+	^ hash.
