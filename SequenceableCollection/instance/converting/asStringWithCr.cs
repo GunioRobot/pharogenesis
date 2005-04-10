9a021565e7dfa150eb761aee45e74bb622ccd46f
ar <unknown>
2005-04-10 18:02:00 +0000

    no message

--- SequenceableCollection/instance/converting/asStringWithCr.cs
+++ SequenceableCollection/instance/converting/asStringWithCr.cs
@@ -5,7 +5,7 @@ usually strings.
 	| labelStream |
 	labelStream _ WriteStream on: (String new: 200).
 	self do: [:each |
-		(each isKindOf: AbstractString)
+		each isString
 			ifTrue: [labelStream nextPutAll: each; cr]
 			ifFalse: [each printOn: labelStream. labelStream cr]].
 	self size > 0 ifTrue: [labelStream skip: -1].
