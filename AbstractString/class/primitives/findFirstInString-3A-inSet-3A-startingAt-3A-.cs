149b29e4d0a9cb55f4f7185054d0844a41ef9014
ar <unknown>
2005-04-10 16:36:00 +0000

    no message

--- AbstractString/class/primitives/findFirstInString-3A-inSet-3A-startingAt-3A-.cs
+++ AbstractString/class/primitives/findFirstInString-3A-inSet-3A-startingAt-3A-.cs
@@ -1,3 +1,15 @@
-findFirstInString: aString  inSet: inclusionMap  startingAt: start
+findFirstInString: aString inSet: inclusionMap startingAt: start
+	"Trivial, non-primitive version"
+	| i stringSize ascii more |
+	inclusionMap size ~= 256 ifTrue: [^ 0].
+	stringSize _ aString size.
+	more _ true.
+	i _ start - 1.
+	[more and: [i + 1 <= stringSize]] whileTrue: [
+		i _ i + 1.
+		ascii _ (aString at: i) asciiValue.
+		more _ ascii < 256 ifTrue: [(inclusionMap at: ascii + 1) = 0] ifFalse: [true].
+	].
 
-	self subclassResponsibility.
+	i + 1 > stringSize ifTrue: [^ 0].
+	^ i
