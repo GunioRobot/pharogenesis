bfb29913d2c5104742df809a41d44b27c6853726
ar <unknown>
2005-04-10 19:27:00 +0000

    no message

--- SmartRefStream/instance/read write/initShapeDicts.cs
+++ SmartRefStream/instance/read write/initShapeDicts.cs
@@ -4,8 +4,7 @@ initShapeDicts
 	self flag: #bobconv.	
 
 	"These must stay constant.  When structures read in, then things can change."
-self flag: #ByteString.
-	steady _ {Array. Dictionary. Association. String. SmallInteger} asSet.
+	steady _ {Array. Dictionary. Association. ByteString. SmallInteger} asSet.
 
 	renamed ifNil: [
 		renamed _ Dictionary new.  "(old class name symbol -> new class name)"
