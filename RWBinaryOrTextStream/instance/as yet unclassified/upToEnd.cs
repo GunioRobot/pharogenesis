e643e1c283c86955975175e5ecfb3cc2b7d8f757
ar <unknown>
2005-04-10 19:27:00 +0000

    no message

--- RWBinaryOrTextStream/instance/as yet unclassified/upToEnd.cs
+++ RWBinaryOrTextStream/instance/as yet unclassified/upToEnd.cs
@@ -1,6 +1,5 @@
 upToEnd
 	"Must override to get class right."
 	| newArray |
-self flag: #ByteString.
-	newArray _ (isBinary ifTrue: [ByteArray] ifFalse: [String]) new: self size - self position.
+	newArray _ (isBinary ifTrue: [ByteArray] ifFalse: [ByteString]) new: self size - self position.
 	^ self nextInto: newArray
