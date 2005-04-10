983a9a9936c244fe3bc55933dd15c2acc7d24435
ar <unknown>
2005-04-10 16:10:00 +0000

    no message

--- SymbolInputInterpreter/instance/as yet unclassified/nextCharFrom-3A-firstEvt-3A-.cs
+++ SymbolInputInterpreter/instance/as yet unclassified/nextCharFrom-3A-firstEvt-3A-.cs
@@ -2,5 +2,5 @@ nextCharFrom: sensor firstEvt: evtBuf
 
 	| keyValue |
 	keyValue := evtBuf third.
-	evtBuf fifth > 1 ifTrue: [^ keyValue asCharacter squeakToIso].
+	evtBuf fifth > 1 ifTrue: [^ keyValue asCharacter macToSqueak].
 	^ (self symbolKeyValueToUnicode: keyValue) asCharacter.
