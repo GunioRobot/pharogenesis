b365737680840a9a5fecfc4284442537f120d0f7
ar <unknown>
2005-04-10 23:55:00 +0000

    no message

--- MultiSymbol/class/class initialization/initialize.cs
+++ MultiSymbol/class/class initialization/initialize.cs
@@ -1,8 +1,2 @@
 initialize
-
-	"MultiSymbol initialize"
-
-	MultiSymbol rehash.
-	OneCharacterMultiSymbols _ nil.
-	OneCharacterMultiSymbols _ (1 to: 256) collect: [ :i | (i - 1) asCharacter asSymbol].
-	Smalltalk addToShutDownList: self.
+	Smalltalk removeFromShutDownList: self. "@@@ Remove this later @@@"
