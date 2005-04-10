02af5e72672432787de2e073aadbde864a056492
ar <unknown>
2005-04-10 20:55:00 +0000

    no message

--- AbstractString/instance/converting/asOctetString.cs
+++ AbstractString/instance/converting/asOctetString.cs
@@ -1,3 +1,6 @@
 asOctetString
-
-	self subclassResponsibility.
+	"Convert the receiver into an octet string"
+	| string |
+	string _ String new: self size.
+	1 to: self size do: [:i | string at: i put: (self at: i)].
+	^string
