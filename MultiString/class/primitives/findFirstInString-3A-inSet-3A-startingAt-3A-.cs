8a2ae7be80297a11a7387c0eb897b9a437582b8b
ar <unknown>
2005-04-10 16:32:00 +0000

    no message

--- MultiString/class/primitives/findFirstInString-3A-inSet-3A-startingAt-3A-.cs
+++ MultiString/class/primitives/findFirstInString-3A-inSet-3A-startingAt-3A-.cs
@@ -1,3 +1,19 @@
 findFirstInString: aString  inSet: inclusionMap  startingAt: start
+	| i stringSize ascii more |
+	
+	self var: #aString declareC: 'unsigned int *aString'.
+	self var: #inclusionMap declareC: 'char *inclusionMap'.
 
-	^ self findFirstInMultiString: aString inSet: inclusionMap startingAt: start.
+	inclusionMap size ~= 256 ifTrue: [^ 0].
+
+	stringSize _ aString size.
+	more _ true.
+	i _ start - 1.
+	[more and: [i + 1 <= stringSize]] whileTrue: [
+		i _ i + 1.
+		ascii _ (aString at: i) asciiValue.
+		more _ ascii < 256 ifTrue: [(inclusionMap at: ascii + 1) = 0] ifFalse: [true].
+	].
+
+	i + 1 > stringSize ifTrue: [^ 0].
+	^ i.
