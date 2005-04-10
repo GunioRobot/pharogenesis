a44401a715f2ffe80779c8fe134de796f2c8da27
ar <unknown>
2005-04-10 16:10:00 +0000

    no message

--- WinKSX1001InputInterpreter/instance/as yet unclassified/nextCharFrom-3A-firstEvt-3A-.cs
+++ WinKSX1001InputInterpreter/instance/as yet unclassified/nextCharFrom-3A-firstEvt-3A-.cs
@@ -7,7 +7,7 @@ nextCharFrom: sensor firstEvt: evtBuf
 	pressType = EventKeyUp ifTrue: [type := #keyUp].
 	pressType = EventKeyChar ifTrue: [type := #keystroke].
 
-	char1Value _ (Character value: keyValue) squeakToIso asciiValue.
+	char1Value _ (Character value: keyValue) macToSqueak asciiValue.
 	((char1Value > 127 and: [char1Value < 160])
 		or: [char1Value > 223 and: [char1Value < 253]]) ifFalse: [
 			^ keyValue asCharacter.
@@ -26,7 +26,7 @@ nextCharFrom: sensor firstEvt: evtBuf
 									= EventKeyChar]]])
 		ifTrue: [
 			firstCharacter _ char1Value asCharacter.
-			secondCharacter _ (peekEvent at: 3) asCharacter squeakToIso.
+			secondCharacter _ (peekEvent at: 3) asCharacter macToSqueak.
 			stream _ ReadStream on: (String with: firstCharacter with: secondCharacter).
 			multiCharacter _ converter nextFromStream: stream.
 			multiCharacter isOctetCharacter ifFalse: [
