e7456c2c4791020c7e8344231510d69beb6b7e21
ar <unknown>
2005-04-10 16:10:00 +0000

    no message

--- MacUnicodeInputInterpreter/instance/as yet unclassified/nextCharFrom-3A-firstEvt-3A-.cs
+++ MacUnicodeInputInterpreter/instance/as yet unclassified/nextCharFrom-3A-firstEvt-3A-.cs
@@ -2,7 +2,7 @@ nextCharFrom: sensor firstEvt: evtBuf
 
 	| keyValue |
 	keyValue := evtBuf third.
-	keyValue < 256 ifTrue: [^ (Character value: keyValue) squeakToIso].
+	keyValue < 256 ifTrue: [^ (Character value: keyValue) macToSqueak].
 	"Smalltalk systemLanguage charsetClass charFromUnicode: keyValue."
 	^ Unicode value: keyValue.
 
