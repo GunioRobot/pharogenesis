3a4039d8be3f07627afb0d504e7cd23afdc63fd5
ar <unknown>
2005-04-10 16:05:00 +0000

    no message

--- MacRomanClipboardInterpreter/instance/as yet unclassified/toSystemClipboard-3A-.cs
+++ MacRomanClipboardInterpreter/instance/as yet unclassified/toSystemClipboard-3A-.cs
@@ -1,8 +1,8 @@
 toSystemClipboard: aString
 
 	| result |
-	aString isOctetString ifTrue: [^ aString asOctetString isoToSqueak].
+	aString isOctetString ifTrue: [^ aString asOctetString squeakToMac].
 
 	result _ WriteStream on: (String new: aString size).
-	aString do: [:each | each asciiValue < 256 ifTrue: [result nextPut: each isoToSqueak]].
+	aString do: [:each | each asciiValue < 256 ifTrue: [result nextPut: each squeakToMac]].
 	^ result contents.
