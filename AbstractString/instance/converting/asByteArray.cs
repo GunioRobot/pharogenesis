fd5ab290ceb3fa06e0f0f7be3482ce4678ea1cbe
ar <unknown>
2005-04-10 17:18:00 +0000

    no message

---
+++ AbstractString/instance/converting/asByteArray.cs
@@ -0,0 +1,8 @@
+asByteArray
+	"Convert to a ByteArray with the ascii values of the string."
+	| b |
+	b _ ByteArray new: self byteSize.
+	1 to: self size * 4 do: [:i |
+		b at: i put: (self byteAt: i).
+	].
+	^ b.
