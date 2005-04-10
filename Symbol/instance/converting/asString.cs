d6863b5e85ebc82b9b75ea132bcd0f1ea55b7470
ar <unknown>
2005-04-10 22:42:00 +0000

    no message

--- Symbol/instance/converting/asString.cs
+++ Symbol/instance/converting/asString.cs
@@ -1,7 +1,6 @@
 asString 
 	"Refer to the comment in String|asString."
-
 	| newString |
-	newString _ String new: self size.
-	1 to: self size do: [:index | newString at: index put: (self at: index)].
+	newString _ self species new: self size.
+	newString replaceFrom: 1 to: newString size with: self startingAt: 1.
 	^newString
