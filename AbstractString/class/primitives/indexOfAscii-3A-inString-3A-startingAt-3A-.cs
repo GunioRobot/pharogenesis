1b7aca27acd25d98fc0cf52c2b3e03d7f6e46dc3
ar <unknown>
2005-04-10 16:36:00 +0000

    no message

--- AbstractString/class/primitives/indexOfAscii-3A-inString-3A-startingAt-3A-.cs
+++ AbstractString/class/primitives/indexOfAscii-3A-inString-3A-startingAt-3A-.cs
@@ -1,3 +1,7 @@
 indexOfAscii: anInteger inString: aString startingAt: start
-
-	self subclassResponsibility.
+	"Trivial, non-primitive version"
+	| stringSize |
+	stringSize _ aString size.
+	start to: stringSize do: [:pos |
+		(aString at: pos) asInteger = anInteger ifTrue: [^ pos]].
+	^ 0
