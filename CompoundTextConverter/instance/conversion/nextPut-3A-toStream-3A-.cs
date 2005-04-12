f2ed284191e3d6c83143368286a2f9686ad25630
ar <unknown>
2005-04-12 14:10:00 +0000

    no message

--- CompoundTextConverter/instance/conversion/nextPut-3A-toStream-3A-.cs
+++ CompoundTextConverter/instance/conversion/nextPut-3A-toStream-3A-.cs
@@ -2,11 +2,11 @@ nextPut: aCharacter toStream: aStream
 
 	| ascii leadingChar class |
 	aStream isBinary ifTrue: [^aCharacter storeBinaryOn: aStream].
-	aCharacter isUnicode ifTrue: [
+	aCharacter isTraditionalDomestic ifFalse: [
 		class _ (EncodedCharSet charsetAt: aCharacter leadingChar) traditionalCharsetClass.
 		ascii _ (class charFromUnicode: aCharacter asUnicode) charCode.
 		leadingChar _ class leadingChar.
-	] ifFalse: [
+	] ifTrue: [
 		ascii _ aCharacter charCode.
 		leadingChar _ aCharacter leadingChar.
 	].
