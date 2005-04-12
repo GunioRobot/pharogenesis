7084793b92fa275c915f2a99e97f04f66d1f5fe8
ar <unknown>
2005-04-12 17:34:00 +0000

    no message

--- String/class/instance creation/with-3A-.cs
+++ String/class/instance creation/with-3A-.cs
@@ -2,6 +2,6 @@ with: aCharacter
 	| newCollection |
 	aCharacter asInteger < 256
 		ifTrue:[newCollection _ ByteString new: 1]
-		ifFalse:[newCollection _ MultiString new: 1].
+		ifFalse:[newCollection _ WideString new: 1].
 	newCollection at: 1 put: aCharacter.
 	^newCollection
