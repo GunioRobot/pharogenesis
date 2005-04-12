8391f60f67f085a2cae25659cdc7f9f296eb0cf8
ar <unknown>
2005-04-12 17:12:00 +0000

    no message

--- StrikeFont/instance/accessing/widthOf-3A-.cs
+++ StrikeFont/instance/accessing/widthOf-3A-.cs
@@ -1,13 +1,8 @@
 widthOf: aCharacter 
 	"Answer the width of the argument as a character in the receiver."
 	| code |
-	code := aCharacter class == Character
-				ifTrue: [aCharacter asciiValue]
-				ifFalse: [aCharacter charCode].
-	((code < minAscii
-				or: [maxAscii < code])
-			or: [(xTable at: code + 1)
-					< 0])
-		ifTrue: [^ self fallbackFont widthOf: aCharacter].
-	^ (xTable at: code + 2)
-		- (xTable at: code + 1)
+	code := aCharacter charCode.
+	((code < minAscii or: [maxAscii < code]) 
+		or: [(xTable at: code + 1) < 0])
+			ifTrue: [^ self fallbackFont widthOf: aCharacter].
+	^ (xTable at: code + 2) - (xTable at: code + 1)
