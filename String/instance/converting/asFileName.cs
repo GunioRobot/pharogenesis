04893fe4ca53dbb16134b8dd4e38a3b8f88986dc
ar <unknown>
2005-04-12 13:55:00 +0000

    no message

--- String/instance/converting/asFileName.cs
+++ String/instance/converting/asFileName.cs
@@ -2,4 +2,7 @@ asFileName
 	"Answer a String made up from the receiver that is an acceptable file 
 	name."
 
-	^FileDirectory checkName: self fixErrors: true
+	| string checkedString |
+	string _ FileDirectory checkName: self fixErrors: true.
+	checkedString _ (FilePath pathName: string) asVmPathName.
+	^ (FilePath pathName: checkedString isEncoded: true) asSqueakPathName.
