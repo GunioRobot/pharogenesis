e39c670ec50927443c67b50a6acaf61cda9fcdaa
ar <unknown>
2005-04-12 17:15:00 +0000

    no message

--- StrikeFontSet/instance/accessing/descentOf-3A-.cs
+++ StrikeFontSet/instance/accessing/descentOf-3A-.cs
@@ -1,10 +1,2 @@
 descentOf: aCharacter
-
-	| encoding f |
-	((aCharacter isMemberOf: Character) or: [aCharacter leadingChar = 0]) ifTrue: [
-		^ (fontArray at: 1) descent.
-	].
-	encoding _ aCharacter leadingChar + 1.
-	[f _ fontArray at: encoding] on: Exception do: [:ex | f _ fontArray at: 1].
-	f ifNil: [f _ fontArray at: 1].
-	^ f descent.
+	^(self fontOf: aCharacter) descent
