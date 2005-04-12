25eae523ab38fa93e669bee560a65157380304de
ar <unknown>
2005-04-12 17:15:00 +0000

    no message

--- StrikeFontSet/instance/accessing/widthOf-3A-.cs
+++ StrikeFontSet/instance/accessing/widthOf-3A-.cs
@@ -1,16 +1,3 @@
 widthOf: aCharacter 
 	"Answer the width of the argument as a character in the receiver."
-	"1: optimizing"
-	| encoding f |
-	aCharacter class == Character
-		ifTrue: [^ (fontArray at: 1)
-				widthOf: aCharacter].
-	"2: other case"
-	encoding := aCharacter leadingChar + 1.
-	f := ((((aCharacter isMemberOf: Character) not
-							and: [encoding > 1])
-						and: [encoding <= fontArray size])
-					and: [(fontArray at: encoding) notNil])
-				ifTrue: [fontArray at: encoding]
-				ifFalse: [self latin1].
-	^ f widthOf: aCharacter
+	^(self fontOf: aCharacter) widthOf: aCharacter
