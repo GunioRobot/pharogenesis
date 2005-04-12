1902dbac2fa1c76100c10414f27f98d524160caf
ar <unknown>
2005-04-12 17:15:00 +0000

    no message

--- StrikeFontSet/instance/accessing/heightOf-3A-.cs
+++ StrikeFontSet/instance/accessing/heightOf-3A-.cs
@@ -1,10 +1,2 @@
 heightOf: aCharacter
-
-	| encoding f |
-	((aCharacter isMemberOf: Character) or: [aCharacter leadingChar = 0]) ifTrue: [
-		^ (fontArray at: 1) height
-	].
-	encoding _ aCharacter leadingChar + 1.
-	[f _ fontArray at: encoding] on: Exception do: [:ex | f _ fontArray at: 1].
-	f ifNil: [f _ fontArray at: 1].
-	^ f height.
+	^(self fontOf: aCharacter) height
