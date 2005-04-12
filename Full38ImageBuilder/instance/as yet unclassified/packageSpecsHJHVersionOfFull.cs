09df74cca00152a4387cf887091ff7602d15a1ae
md <unknown>
2005-04-12 15:39:00 +0000

    no message

---
+++ Full38ImageBuilder/instance/as yet unclassified/packageSpecsHJHVersionOfFull.cs
@@ -0,0 +1,27 @@
+packageSpecsHJHVersionOfFull
+	"This is the list of my private version of full. I am testing various packages and I will add packages as I go along.
+
+HJH 12/16/2004 09:32"
+
+
+	^{
+		{"Monticello"
+'29185ffa-5d40-4d86-8e73-23e2db129237'. '23'}.     "adapted to 3.8 HJH 12/15/2004 16:56"
+
+          {"Balloon3D"
+           'c7d613bc-9f0d-4168-baa3-a573768e26c2', '2'}.  "HJH 12/16/2004 09:21"
+
+          {"Script Manager"
+           '7ea56945-3d86-4e45-a6d6-66b19e21b6dd', '1'}. 
+
+          {"Tracing Messages Browser"
+           '5903fd5f-36f5-4d85-9fce-f21c44a3632f', '2'}.
+
+		{"YAXO - still the first version, not registered for 3.8"
+'c6e0d836-d1e6-41ac-b1c2-0505bdd46663'. '1'}.
+
+	"add shout"
+
+       }
+
+
