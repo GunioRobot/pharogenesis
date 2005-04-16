43e2f1de9c3bfe55c8b4f73667ca511c80dd336f
dtl <unknown>
2005-04-16 01:06:00 +0000

    no message

--- SystemDictionary/instance/memory space/lowSpaceWatcher.cs
+++ SystemDictionary/instance/memory space/lowSpaceWatcher.cs
@@ -1,30 +1,37 @@
 lowSpaceWatcher
-	"Wait until the low space semaphore is signalled, then take
-	appropriate actions."
-	| free |
-	self garbageCollectMost <= self lowSpaceThreshold
-		ifTrue: [self garbageCollect <= self lowSpaceThreshold
-				ifTrue: ["free space must be above threshold before
-					starting low space watcher"
-					^ Beeper beep]].
-	LowSpaceSemaphore := Semaphore new.
+	"Wait until the low space semaphore is signalled, then take appropriate actions."
+
+	| free preemptedProcess |
+	self garbageCollectMost <= self lowSpaceThreshold ifTrue: [
+		self garbageCollect <= self lowSpaceThreshold ifTrue: [
+			"free space must be above threshold before starting low space watcher"
+			^ Beeper beep]].
+
+	Smalltalk specialObjectsArray at: 23 put: nil.  "process causing low space will be saved here"
+	LowSpaceSemaphore _ Semaphore new.
 	self primLowSpaceSemaphore: LowSpaceSemaphore.
-	self primSignalAtBytesLeft: self lowSpaceThreshold.
-	"enable low space interrupts"
-	LowSpaceSemaphore wait.
-	"wait for a low space condition..."
-	self primSignalAtBytesLeft: 0.
-	"disable low space interrupts"
+	self primSignalAtBytesLeft: self lowSpaceThreshold.  "enable low space interrupts"
+
+	LowSpaceSemaphore wait.  "wait for a low space condition..."
+
+	self primSignalAtBytesLeft: 0.  "disable low space interrupts"
 	self primLowSpaceSemaphore: nil.
-	LowSpaceProcess := nil.
-	"Note: user now unprotected until the low space watcher is
-	re-installed "
-	self memoryHogs isEmpty
-		ifFalse: [free := self bytesLeft.
-			self memoryHogs
-				do: [:hog | hog freeSomeSpace].
-			self bytesLeft > free
-				ifTrue: [^ self installLowSpaceWatcher]].
-	self isMorphic
-		ifTrue: [CurrentProjectRefactoring currentInterruptName: 'Space is low']
-		ifFalse: [ScheduledControllers interruptName: 'Space is low']
+	LowSpaceProcess _ nil.
+
+	"The process that was active at the time of the low space interrupt."
+	preemptedProcess _ Smalltalk specialObjectsArray at: 23.
+	Smalltalk specialObjectsArray at: 23 put: nil.
+
+	"Note: user now unprotected until the low space watcher is re-installed"
+
+	self memoryHogs isEmpty ifFalse: [
+		free := self bytesLeft.
+		self memoryHogs do: [ :hog | hog freeSomeSpace ].
+		self bytesLeft > free ifTrue: [ ^ self installLowSpaceWatcher ]].
+	Smalltalk isMorphic
+			ifTrue: [CurrentProjectRefactoring
+						currentInterruptName: 'Space is low'
+						preemptedProcess: preemptedProcess]
+			ifFalse: [ScheduledControllers
+						interruptName: 'Space is low'
+						preemptedProcess: preemptedProcess]
