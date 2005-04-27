70c15c143f4dff385ace6d6274df69beea573d3d
tpr <unknown>
2005-04-27 11:04:00 +0000

    no message

--- SystemDictionary/instance/deprecated/vmParameterAt-3A-.cs
+++ SystemDictionary/instance/deprecated/vmParameterAt-3A-.cs
@@ -1,26 +1,5 @@
 vmParameterAt: parameterIndex
-	"parameterIndex is a positive integer corresponding to one of the VM's internal
-	parameter/metric registers.  Answer with the current value of that register.
-	Fail if parameterIndex has no corresponding register.
-	VM parameters are numbered as follows:
-		1	end of old-space (0-based, read-only)
-		2	end of young-space (read-only)
-		3	end of memory (read-only)
-		4	allocationCount (read-only)
-		5	allocations between GCs (read-write)
-		6	survivor count tenuring threshold (read-write)
-		7	full GCs since startup (read-only)
-		8	total milliseconds in full GCs since startup (read-only)
-		9	incremental GCs since startup (read-only)
-		10	total milliseconds in incremental GCs since startup (read-only)
-		11	tenures of surving objects since startup (read-only)
-		12-20 specific to the translating VM
-		21	root table size (read-only)
-		22	root table overflows since startup (read-only)
-		23	bytes of extra memory to reserve for VM buffers, plugins, etc.
-
-		24	memory headroom when growing object memory (rw)
-		25	memory threshold above which shrinking object memory (rw)"
+	"See comment for SmalltalkImage>vmParameterAt:"
 
 	^ self deprecated: 'Use SmalltalkImage current vmParameterAt:'
 		block: [SmalltalkImage current vmParameterAt: parameterIndex]
