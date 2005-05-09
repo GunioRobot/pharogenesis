dc15f4c9aa6720cc15735272a0384909dcbeb424
md <unknown>
2005-05-09 13:39:00 +0000

    no message

---
+++ Full38ImageBuilder/instance/as yet unclassified/packageSpecsOfficialFor38.cs
@@ -0,0 +1,37 @@
+packageSpecsOfficialFor38
+	"WARNING: This is the future spec list for 3.8 full. Currently it is just a copy of the 3.7 speclist
+      with the first entry changed."
+
+	"self new packageSpecs collect: [:arr |
+		(SMSqueakMap default packageWithId: arr first)
+releaseWithAutomaticVersionString: arr second]
+
+	(SMSqueakMap default packageWithName: 'Celeste') id  
+	
+     
+	"
+
+	^{
+		{"Monticello"
+'66236497-7026-45f5-bcf6-ad00ba7a8a4e'. '25'}.     "adapted to 3.8 HJH 12/15/2004 16:56"
+		{"YAXO not registered for 3.7"
+'c6e0d836-d1e6-41ac-b1c2-0505bdd46663'. '1'}.
+		{"Games"
+		'643f4620-0fe6-493b-a129-7fd1224e0fe4' . '3'}. 
+		{"NetworkHTML"
+'b8ef257d-ed61-46c9-8560-b3e2c1332dc6'. '3'}.
+		{"Scamper"
+'a55ea3ab-f8a0-40cd-8aa9-0077369d1db8'. '3'}.
+		{"Celeste"
+'97aadf92-12b0-4db0-8da3-65041ae18f13'. '3'}.
+		{"IRCe"
+'839bb65f-8e8e-4dbc-8fe1-dc52d822dbf9'. '15'}.
+		{"VMMaker" 
+'2e7f103e-22a6-470d-affe-54b1d04ef34a'. '8'}.
+		{"Vassili's Regex"
+'c32158f2-ab2c-49c4-8451-d920dcf1023c'. '1'}.
+			{"Shout"
+'1314cee7-e1a3-4ca5-a167-b6517bf16367'. '5'}.
+			{"Balloon3D"
+'178bd01c-f698-41ab-9c73-d9278c65c241'. '2'}
+	}
