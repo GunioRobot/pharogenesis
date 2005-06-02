2d670d2a52d845a8f8be01f8e71d5e498bb92c6a
tpr <unknown>
2005-06-02 13:43:00 +0000

    no message

--- Locale/instance/system primitives/primTimezone.cs
+++ Locale/instance/system primitives/primTimezone.cs
@@ -1,4 +1,5 @@
 primTimezone
-	"The offset from UTC in seconds, with positive offsets being towards the east.
+	"The offset from UTC in minutes, with positive offsets being towards the east.
 	(San Francisco is in UTC -08*60 and Paris is in GMT +01*60 (daylight savings is not in effect)."
+	<primitive:'primitiveTimezoneOffset' module: 'LocalePlugin'>
 	^0
