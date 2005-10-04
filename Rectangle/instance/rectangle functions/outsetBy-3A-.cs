da7aadbf51bb93fff206e0cb1a2515f542de4457
md <unknown>
2005-10-04 14:41:00 +0000

    no message

--- Rectangle/instance/rectangle functions/outsetBy-3A-.cs
+++ Rectangle/instance/rectangle functions/outsetBy-3A-.cs
@@ -2,7 +2,7 @@ outsetBy: delta
 	"Answer a Rectangle that is outset from the receiver by delta. delta is a 
 	Rectangle, Point, or scalar."
 
-	(delta isKindOf: Rectangle)
+	(delta isRectangle)
 		ifTrue: [^Rectangle 
 					origin: origin - delta origin 
 					corner: corner + delta corner]
