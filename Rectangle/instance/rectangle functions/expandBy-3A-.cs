b44f28c5dd7f3e0ef8c889281c8c02feb5a3c7c1
md <unknown>
2005-10-04 14:42:00 +0000

    no message

--- Rectangle/instance/rectangle functions/expandBy-3A-.cs
+++ Rectangle/instance/rectangle functions/expandBy-3A-.cs
@@ -2,7 +2,7 @@ expandBy: delta
 	"Answer a Rectangle that is outset from the receiver by delta. delta is a 
 	Rectangle, Point, or scalar."
 
-	(delta isKindOf: Rectangle)
+	(delta isRectangle)
 		ifTrue: [^Rectangle 
 					origin: origin - delta origin 
 					corner: corner + delta corner]
