8c1c5a15912eef9153dafb12f13ee57506bfd5a6
gk <unknown>
2005-10-11 01:24:00 +0000

    no message

--- SocketStream/instance/stream in/upTo-3A-.cs
+++ SocketStream/instance/stream in/upTo-3A-.cs
@@ -1,11 +1,42 @@
-upTo: delim 
-	| resultStream nextChar |
-	resultStream _ WriteStream on: (self streamBuffer: 100).
+upTo: aCharacterOrByte
+	"Return data up to, but not including given character or byte.
+	
+	NOTE: Does not honour timeouts if shouldSignal is false!
+	
+	This method looks a bit complicated, and this is mainly because there is no fast search method
+	in String that takes a stoppingAt: argument. This means we need to ignore getting hits in the
+	dead buffer area above inNextToWrite.
+	Another measure is that this implementation is greedy and will load data into the buffer
+	until there is nothing more available, or it has loaded 100kb - and not until then we search the buffer.
 
-	[(nextChar _ self next) = delim]
-		whileFalse: [
-			nextChar
-				ifNil: [^resultStream contents]
-				ifNotNil: [resultStream nextPut: nextChar]].
+	A totally non greedy variant would search on every loop."
 
-	^resultStream contents
+	| index result lastRecentlyRead searchedSoFar |
+	searchedSoFar _ 0.
+	lastRecentlyRead _ 0.
+	index _ 0.
+	[self atEnd not and: [
+		((lastRecentlyRead = 0 and: [self isInBufferEmpty not]) or: [self inBufferSize > 100000]) ifTrue: [
+			"Data begins at lastRead + 1, we add searchedSoFar as offset."
+			index _ inBuffer indexOf: aCharacterOrByte startingAt: lastRead + searchedSoFar + 1.
+			searchedSoFar _ self inBufferSize.
+			(index > 0 and: [(index + 1) > inNextToWrite]) ifTrue: [
+				"Oops, hit in dead buffer area.
+				This is probably due to old data, so we ignore it.
+				No point in cleaning the dead area to avoid hits - it will still search it."
+				index _ 0]].
+		index = 0]]
+				whileTrue: [
+					recentlyRead = 0
+						ifTrue: ["blocking call for now, we don't want to poll"
+							self receiveData]
+						ifFalse: [
+							self receiveAvailableData].
+					lastRecentlyRead _ recentlyRead].
+	index > 0
+		ifTrue: ["found it"
+			result _ self nextInBuffer: index - lastRead - 1.
+			self skip: 1.
+			^ result]
+		ifFalse: ["atEnd"
+			^ self nextAllInBuffer]
