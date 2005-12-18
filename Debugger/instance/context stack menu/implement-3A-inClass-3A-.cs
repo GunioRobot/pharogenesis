814ed75e42c097bd2472d8f833216f07b20d1512
dgd <unknown>
2005-12-18 11:05:00 +0000

    no message

--- Debugger/instance/context stack menu/implement-3A-inClass-3A-.cs
+++ Debugger/instance/context stack menu/implement-3A-inClass-3A-.cs
@@ -3,7 +3,9 @@ implement: aMessage inClass: aClass
 	| category |
 	category := self askForCategoryIn: aClass default: 'as yet unclassified'.
 	aClass compile: aMessage createStubMethod classified: category.
+
 	self setContentsToForceRefetch.
 	self selectedContext privRefreshWith: (aClass lookupSelector: aMessage selector).
+	self selectedContext temps: aMessage arguments.
 	self resetContext: self selectedContext.
 	self debug.
