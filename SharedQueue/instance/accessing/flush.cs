7739649ca819c723ce65febe78ef7672d6a58df7
bf <unknown>
2006-02-11 15:17:00 +0000

    no message

--- SharedQueue/instance/accessing/flush.cs
+++ SharedQueue/instance/accessing/flush.cs
@@ -1,6 +1,8 @@
 flush
 	"Throw out all pending contents"
 	accessProtect critical: [
+		"nil out flushed slots --bf 02/11/2006"
+		contentsArray from: readPosition to: writePosition-1 put: nil.
 		readPosition _ 1.
 		writePosition _ 1.
 		"Reset the read synchronization semaphore"
