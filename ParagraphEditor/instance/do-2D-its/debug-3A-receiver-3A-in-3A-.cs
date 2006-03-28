720dbb25ff1198078ce408ca3cea1e4dec0debbf
mir <unknown>
2006-03-28 17:30:00 +0000

    no message

--- ParagraphEditor/instance/do-2D-its/debug-3A-receiver-3A-in-3A-.cs
+++ ParagraphEditor/instance/do-2D-its/debug-3A-receiver-3A-in-3A-.cs
@@ -1,11 +1,11 @@
 debug: aCompiledMethod receiver: anObject in: evalContext
 
 	| selector guineaPig debugger context |
-	selector _ evalContext isNil ifTrue: [#DoIt] ifFalse: [#DoItIn:].
+	selector _ evalContext isNil ifTrue: [#DebugIt] ifFalse: [#DebugItIn:].
 	anObject class addSelectorSilently: selector withMethod: aCompiledMethod.
 	guineaPig _ evalContext isNil
-		ifTrue: [[anObject DoIt] newProcess]
-		ifFalse: [[anObject DoItIn: evalContext] newProcess].
+		ifTrue: [[anObject DebugIt] newProcess]
+		ifFalse: [[anObject DebugItIn: evalContext] newProcess].
 	context _ guineaPig suspendedContext.
 	debugger _ Debugger new
 		process: guineaPig
