6fb8d76ed27f3fc55cd329ff2c58fe9373648d78
gk <unknown>
2006-05-05 02:05:00 +0000

    no message

--- SMLoader/instance/gui building/browseCacheDirectory.cs
+++ SMLoader/instance/gui building/browseCacheDirectory.cs
@@ -5,7 +5,7 @@ browseCacheDirectory
 	item := self selectedPackageOrRelease.
 	item ifNil: [^nil].
 	dir := item isPackage
-				ifTrue: [model cache directoryForPackage: item]
-				ifFalse: [model cache directoryForPackageRelease: item].
+				ifTrue: [squeakMap cache directoryForPackage: item]
+				ifFalse: [squeakMap cache directoryForPackageRelease: item].
 	win := FileList2 morphicViewOnDirectory: dir. " withLabel: item name, ' cache directory'."
 	win openInWorld
