5c85474e9709f7a8b52f0b28b81d5b47a6f2d186
lr <unknown>
2006-08-19 20:39:00 +0000

    no message

--- Parser/instance/pragmas/pragmaStatement.cs
+++ Parser/instance/pragmas/pragmaStatement.cs
@@ -1,22 +1,31 @@
 pragmaStatement
 	"Read a single pragma statement. Parse all generic pragmas in the form of: <key1: val1 key2: val2 ...> and remember them, including primitives."
 	
-	| arguments keyword |
-	(hereType = #keyword or: [ hereType = #word ])
+	| selector arguments words index keyword |
+	(hereType = #keyword or: [ hereType = #word or: [ hereType = #binary ] ])
 		ifFalse: [  ^ self expected: 'pragma declaration' ].
 
 	" This is a ugly hack into the compiler of the FFI package. FFI should be changed to use propre pragmas that can be parsed with the code here. "
 	(here = #apicall: or: [ here = #cdecl: ])
 		ifTrue: [ ^ self externalFunctionDeclaration ].
 
-	arguments := Array new.
-	keyword := self advance.
-	keyword last = $: ifTrue: [
-		[ arguments := arguments copyWith: self pragmaLiteral.
-		  hereType = #keyword ] whileTrue: [ keyword := keyword , self advance ] ].
-	keyword numArgs ~= arguments size
+	selector := String new.
+	arguments := OrderedCollection new.
+	words := OrderedCollection new.
+	[ hereType = #keyword or: [ (hereType = #word or: [ hereType = #binary ]) and: [ selector isEmpty ] ] ] whileTrue: [
+		index := self startOfNextToken + requestorOffset.
+		selector := selector , self advance.
+		words add: (index to: self endOfLastToken + requestorOffset).
+		(selector last = $: or: [ selector first isLetter not ])
+			ifTrue: [ arguments add: self pragmaLiteral ] ].
+	selector numArgs ~= arguments size
 		ifTrue: [ ^ self expected: 'pragma argument' ].
-	self addPragma: (Pragma 
-		keyword: keyword asSymbol 
-		arguments: arguments).
+	(Symbol hasInterned: selector 
+		ifTrue: [ :value | keyword := value]) 
+		ifFalse: [ 
+			keyword := self 
+				correctSelector: selector wordIntervals: words
+				exprInterval: (words first first to: words last last)
+				ifAbort: [ ^ self fail ] ].
+	self addPragma: (Pragma keyword: keyword arguments: arguments asArray).
 	^ true
