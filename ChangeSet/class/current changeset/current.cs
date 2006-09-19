2b37f0abcb89fcfb9acdcca892bba3cfc4c63f5c
wiz <unknown>
2006-09-19 03:21:00 +0000

    no message

--- ChangeSet/class/current changeset/current.cs
+++ ChangeSet/class/current changeset/current.cs
@@ -1,4 +1,7 @@
 current
-	"return the current changeset"
+	"return the current changeset
+	assure first that we have a named changeset. To cure mantis #4535. "
+	current isMoribund
+		ifTrue: [(ChangeSet newChanges: (ChangeSet assuredChangeSetNamed: 'Unnamed'))] .
 
 	^ current
