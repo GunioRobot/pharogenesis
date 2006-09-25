fd60958754fa51f498e96a5b06a17724dfe6fada
gk <unknown>
2006-09-25 23:12:00 +0000

    no message

--- SMLoader/instance/lists/categoryWrapperList.cs
+++ SMLoader/instance/lists/categoryWrapperList.cs
@@ -4,7 +4,7 @@ categoryWrapperList
 	is first if it exists."
 	 
 	| list first |
-	list := (((squeakMap categories select:[:each | each parent == nil]) asArray 
+	list := (((model categories select:[:each | each parent == nil]) asArray 
 		sort:[:c1 :c2 | c1 name <= c2 name])).
 	first := list detect:[:any | any name = 'Squeak versions'] ifNone:[nil].
 	first ifNotNil:[
