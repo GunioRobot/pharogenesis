e5d38815f8228a93197976b70d49ed696ea7ab5a
gk <unknown>
2006-09-26 00:25:00 +0000

    no message

--- SMLoader/instance/private/installedReleaseOfMe.cs
+++ SMLoader/instance/private/installedReleaseOfMe.cs
@@ -1,4 +1,4 @@
 installedReleaseOfMe
 	"Return the release of the installed package loader."
 
-	^squeakMap installedReleaseOf: (squeakMap packageWithId: '941c0108-4039-4071-9863-a8d7d2b3d4a3').
+	^SMSqueakMap default installedReleaseOf: (SMSqueakMap default packageWithId: '941c0108-4039-4071-9863-a8d7d2b3d4a3').
