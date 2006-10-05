e58ccde73e06fc7765d80d7a79348cf126405d12
lr <unknown>
2006-10-05 09:50:00 +0000

    no message

--- Parser/instance/primitives/primitive-3A-.cs
+++ Parser/instance/primitives/primitive-3A-.cs
@@ -1,9 +1,10 @@
 primitive: anIntegerOrString
 	"Create indexed primitive."
-		
+	
+	<primitive>
 	^ anIntegerOrString isInteger
 		ifTrue: [ anIntegerOrString ]
 		ifFalse: [ 
 			anIntegerOrString isString
 				ifTrue: [ self primitive: anIntegerOrString module: nil ]
-				ifFalse: [ self expected: 'Indexed primitive' ] ].
+				ifFalse: [ self expected: 'Indexed primitive' ] ]
