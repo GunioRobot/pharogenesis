b53ba9fe758b7202651345ff62509b0ac084b0d7
lr <unknown>
2006-10-05 10:04:00 +0000

    no message

--- Parser/instance/pragmas/pragmaPrimitives.cs
+++ Parser/instance/pragmas/pragmaPrimitives.cs
@@ -1,10 +1,13 @@
 pragmaPrimitives
-	| primitives |
-	primitives := self properties pragmas select: [ :each | 
-		#( primitive: primitive:module: ) 
-			includes: each keyword ].
+	| pragmas primitives |
+	self properties pragmas isEmpty
+		ifTrue: [ ^ 0 ].
+	pragmas := Pragma allNamed: #primitive from: self class to: Parser.
+	primitives := self properties pragmas select: [ :prim |
+		pragmas anySatisfy: [ :prag | 
+			prag selector = prim keyword ] ].
 	primitives isEmpty 
 		ifTrue: [ ^ 0 ].
 	primitives size = 1 
 		ifFalse: [ ^ self notify: 'Ambigous primitives' ].
-	^ primitives first message sendTo: self.
+	^ primitives first message sendTo: self
