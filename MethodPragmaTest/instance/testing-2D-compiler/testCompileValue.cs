fec2f71c372c1fcb8decb3bf97e4e168b498e7e4
lr <unknown>
2006-10-05 09:49:00 +0000

    no message

--- MethodPragmaTest/instance/testing-2D-compiler/testCompileValue.cs
+++ MethodPragmaTest/instance/testing-2D-compiler/testCompileValue.cs
@@ -1,7 +1,4 @@
 testCompileValue
 	self assertPragma: 'foo: true' givesKeyword: #foo: arguments: #( true ).
 	self assertPragma: 'foo: false' givesKeyword: #foo: arguments: #( false ).
-	self assertPragma: 'foo: nil' givesKeyword: #foo: arguments: #( nil ).
-	
-	self assertPragma: 'foo: String' givesKeyword: #foo: arguments: { String }.
-	self assertPragma: 'foo: Pragma' givesKeyword: #foo: arguments: { Pragma }.
+	self assertPragma: 'foo: nil' givesKeyword: #foo: arguments: #( nil )
