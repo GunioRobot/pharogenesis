8f09d1d64a00ac07c1b65b1d38af41615fd5f1f3
rw <unknown>
2006-10-07 08:30:00 +0000

    no message

--- Class/instance/class name/rename-3A-.cs
+++ Class/instance/class name/rename-3A-.cs
@@ -1,13 +1,13 @@
 rename: aString 
 	"The new name of the receiver is the argument, aString."
 
-	| newName |
-	(newName _ aString asSymbol) ~= self name
-		ifFalse: [^ self].
+	| oldName newName |
+	(newName _ aString asSymbol) = (oldName _ self name)
+		ifTrue: [^ self].
 	(self environment includesKey: newName)
 		ifTrue: [^ self error: newName , ' already exists'].
 	(Undeclared includesKey: newName)
 		ifTrue: [self inform: 'There are references to, ' , aString printString , '
 from Undeclared. Check them after this change.'].
-	self environment renameClass: self as: newName.
-	name := newName.
+	name _ newName.
+	self environment renameClass: self from: oldName
