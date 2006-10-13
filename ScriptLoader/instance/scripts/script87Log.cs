e689508d32a021dea648f6588b31994121757067
sd <unknown>
2006-10-13 19:11:00 +0000

    no message

---
+++ ScriptLoader/instance/scripts/script87Log.cs
@@ -0,0 +1,35 @@
+script87Log
+
+	"Name: KernelTests-lr.49
+Author: lr
+Time: 5 October 2006, 10:16:34 am
+UUID: 762d5c23-0364-4c26-ba13-129327ea7f39
+Ancestors: KernelTests-lr.48
+
+- merged code to fix unknown pragma selectors and binary selectors in pragmas
+- don't allow class names in pragmas (to be consistent)
+- use pragmas to define primitive-pragmas
+- added some tests
+
+Name: Compiler-lr.56
+Author: lr
+Time: 5 October 2006, 10:14:04 am
+UUID: f429f722-c39f-4acf-9d8c-c8a38320eaa8
+Ancestors: Compiler-lr.55
+
+- merged code to fix unknown pragma selectors
+- don't allow class names in pragmas (to be consistent)
+- use pragmas to define primitive-pragmas
+
+Morphic fix duplicate
+
+
+Change Set:		ToggleBreakFix39
+Date:			26 September 2006
+Author:			tim@rowledge.org
+
+Putative fix for Mantis http://bugs.impara.de/view.php?id=5123
+
+Fix Squeakmap
+
+"
