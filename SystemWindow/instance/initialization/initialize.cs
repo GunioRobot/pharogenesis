fc79204391389b8eb7c9dfd8b1d113e97e97eab9
dew <unknown>
2006-10-14 18:57:00 +0000

    no message

--- SystemWindow/instance/initialization/initialize.cs
+++ SystemWindow/instance/initialization/initialize.cs
@@ -11,36 +11,7 @@ initialize
 	self color: Color lightGray lighter lighter lighter.
 	self layoutPolicy: ProportionalLayout new.
 	
-	label _ StringMorph new contents: labelString;
-						 font: Preferences windowTitleFont emphasis: 1.
-			"Add collapse box so #labelHeight will work"
-			collapseBox _ self createCollapseBox.
-			stripes _ Array
-						with: (RectangleMorph newBounds: bounds)
-						with: (RectangleMorph newBounds: bounds).
-			"see extent:"
-			self addLabelArea.
-			self setLabelWidgetAllowance.
-			self addCloseBox.
-			self addMenuControl.
-			labelArea addMorphBack: (Morph new extent: self class borderWidth @ 0).
-			labelArea addMorphBack: label.
-			self wantsExpandBox
-				ifTrue: [self addExpandBox].
-			labelArea addMorphBack: collapseBox.
-			self setFramesForLabelArea.
-			Preferences clickOnLabelToEdit
-				ifTrue: [label
-						on: #mouseDown
-						send: #relabel
-						to: self].
-			Preferences noviceMode
-				ifTrue: [closeBox
-						ifNotNil: [closeBox setBalloonText: 'close window'].
-					menuBox
-						ifNotNil: [menuBox setBalloonText: 'window menu'].
-					collapseBox
-						ifNotNil: [collapseBox setBalloonText: 'collapse/expand window']].
+	self initializeLabelArea.
 				
 	self addCornerGrips.
 
