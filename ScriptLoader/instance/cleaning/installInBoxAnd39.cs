2c27299332553f03da5cb10cffc0c2e37433b8b4
sd <unknown>
2006-10-14 11:08:00 +0000

    no message

--- ScriptLoader/instance/cleaning/installInBoxAnd39.cs
+++ ScriptLoader/instance/cleaning/installInBoxAnd39.cs
@@ -3,7 +3,7 @@ installInBoxAnd39
 	
 	MCWorkingCopy allManagers do: [:each | 
 		each  repositoryGroup
-			addRepository: (MCHttpRepository new location: 'http://source.squeakfoundation.org/39a');
-			addRepository: (MCHttpRepository new location: 'http://source.squeakfoundation.org/inbox')].
+			addRepository: (MCHttpRepository new location: 'http://source.squeakfoundation.org/39a' ; user: ''; password: '');
+			addRepository: (MCHttpRepository new location: 'http://source.squeakfoundation.org/inbox' ; user: '' ; password: '')].
 	
 	
