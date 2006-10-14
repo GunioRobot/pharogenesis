4711902113a7d8a0c3fb2cf77ae2c9b65db5f2a0
dew <unknown>
2006-10-14 18:57:00 +0000

    no message

--- SystemWindow/instance/initialization/initializeLabelArea.cs
+++ SystemWindow/instance/initialization/initializeLabelArea.cs
@@ -1,38 +1,32 @@
 initializeLabelArea
 	"Initialize the label area (titlebar) for the window."
-	label := LabelStringMorph new
-				contents: labelString;
-				font: Preferences windowTitleFont
-				emphasis: (Preferences windowTitleFont isTTCFont ifTrue: [0] ifFalse: [1]);
-				mySystemWindow: self.
-	"Add collapse box so #labelHeight will work"
-	collapseBox := self createCollapseBox.
-	stripes := Array
-				with: (RectangleMorph newBounds: bounds)
-				with: (RectangleMorph newBounds: bounds).
-	"see extent:"
-	self addLabelArea.
-	labelArea
-		addMorph: (stripes first borderWidth: 1).
-	labelArea
-		addMorph: (stripes second borderWidth: 2).
-	self setLabelWidgetAllowance.
-	self addCloseBox.
-	self addMenuControl.
-	labelArea addMorph: label.
-	self wantsExpandBox
-		ifTrue: [self addExpandBox].
-	labelArea addMorph: collapseBox.
-	self setFramesForLabelArea.
-	Preferences clickOnLabelToEdit
-		ifTrue: [label
-				on: #mouseDown
-				send: #relabel
-				to: self].
-	Preferences noviceMode
-		ifTrue: [closeBox
-				ifNotNil: [closeBox setBalloonText: 'close window' translated].
-			menuBox
-				ifNotNil: [menuBox setBalloonText: 'window menu' translated].
-			collapseBox
-				ifNotNil: [collapseBox setBalloonText: 'collapse/expand window' translated]].
+	label _ StringMorph new contents: labelString;
+						 font: Preferences windowTitleFont emphasis: 0.
+			"Add collapse box so #labelHeight will work"
+			collapseBox _ self createCollapseBox.
+			stripes _ Array
+						with: (RectangleMorph newBounds: bounds)
+						with: (RectangleMorph newBounds: bounds).
+			"see extent:"
+			self addLabelArea.
+			self setLabelWidgetAllowance.
+			self addCloseBox.
+			self addMenuControl.
+			labelArea addMorphBack: (Morph new extent: self class borderWidth @ 0).
+			labelArea addMorphBack: label.
+			self wantsExpandBox
+				ifTrue: [self addExpandBox].
+			labelArea addMorphBack: collapseBox.
+			self setFramesForLabelArea.
+			Preferences clickOnLabelToEdit
+				ifTrue: [label
+						on: #mouseDown
+						send: #relabel
+						to: self].
+			Preferences noviceMode
+				ifTrue: [closeBox
+						ifNotNil: [closeBox setBalloonText: 'close window'].
+					menuBox
+						ifNotNil: [menuBox setBalloonText: 'window menu'].
+					collapseBox
+						ifNotNil: [collapseBox setBalloonText: 'collapse/expand window']].
