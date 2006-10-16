16c5a2f26dd2aef41f44db5edcfdfa0fc0aa2a97
md <unknown>
2006-10-16 09:53:00 +0000

    no message

--- Compiler/instance/public access/evaluate-3A-in-3A-to-3A-notifying-3A-ifFail-3A-logged-3A-.cs
+++ Compiler/instance/public access/evaluate-3A-in-3A-to-3A-notifying-3A-ifFail-3A-logged-3A-.cs
@@ -1,7 +1,7 @@
 evaluate: textOrStream in: aContext to: receiver notifying: aRequestor ifFail: failBlock logged: logFlag
 	"Compiles the sourceStream into a parse tree, then generates code into a method. In other words, if receiver is not nil, then the text can refer to instance variables of that receiver (the Inspector uses this). If aContext is not nil, the text can refer to temporaries in that context (the Debugger uses this). If aRequestor is not nil, then it will receive a notify:at: message before the attempt to evaluate is aborted. Finally, the compiled method is directly invoked without modifying the receiving-class."
 
-	| methodNode method value |
+	| methodNode method value toLog itsSelectionString itsSelection |
 	class := (aContext isNil 
 		ifTrue: [ receiver ] 
 		ifFalse: [ aContext receiver ])
@@ -20,6 +20,13 @@ evaluate: textOrStream in: aContext to: receiver notifying: aRequestor ifFail: f
 			ifTrue: [ #() ]
 			ifFalse: [ Array with: aContext ])
 		executeMethod: method.
-	logFlag 
-		ifTrue: [ SystemChangeNotifier uniqueInstance evaluated: sourceStream contents context: aContext ].
+	logFlag ifTrue:
+		[toLog := ((requestor respondsTo: #selection)  and:
+			[(itsSelection := requestor selection) notNil] and:
+			[(itsSelectionString := itsSelection asString) isEmptyOrNil not] )
+			ifTrue: 
+				[itsSelectionString]
+			ifFalse:
+				[sourceStream contents].
+		SystemChangeNotifier uniqueInstance evaluated: toLog context: aContext ].
 	^ value.
