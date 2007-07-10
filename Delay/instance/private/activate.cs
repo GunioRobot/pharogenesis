0872cb505f68470d87b7bcb02ef6cd53d8de559e
ar <unknown>
2007-07-10 21:24:00 +0000

    no message

--- Delay/instance/private/activate.cs
+++ Delay/instance/private/activate.cs
@@ -1,14 +1,13 @@
 activate
 	"Private! Make the receiver the Delay to be awoken when the next timer interrupt occurs. This method should only be called from a block protected by the AccessProtect semaphore."
-
-	ActiveDelay _ self.
-	ActiveDelayStartTime _ Time millisecondClockValue.
-	ActiveDelayStartTime >= resumptionTime ifTrue:[
-		"if my time is now or in the past, trigger me and get the next in line activated"
+	TimerEventLoop ifNotNil:[^nil].
+	ActiveDelay := self.
+	ActiveDelayStartTime := Time millisecondClockValue.
+	ActiveDelayStartTime > resumptionTime ifTrue:[
 		ActiveDelay signalWaitingProcess.
 		SuspendedDelays isEmpty ifTrue:[
-			ActiveDelay _ nil.
-			ActiveDelayStartTime _ nil.
+			ActiveDelay := nil.
+			ActiveDelayStartTime := nil.
 		] ifFalse:[SuspendedDelays removeFirst activate].
 	] ifFalse:[
 		TimingSemaphore initSignals.
