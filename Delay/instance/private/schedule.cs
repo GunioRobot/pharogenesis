3156833c9dc1e4d2dd1ccdde2502e745286501a5
ar <unknown>
2007-07-10 21:55:00 +0000

    no message

--- Delay/instance/private/schedule.cs
+++ Delay/instance/private/schedule.cs
@@ -2,9 +2,11 @@ schedule
 	"Private! Schedule this Delay, but return immediately rather than waiting. The receiver's semaphore will be signalled when its delay duration has elapsed."
 
 	beingWaitedOn ifTrue: [self error: 'This Delay has already been scheduled.'].
+
+	TimerEventLoop ifNotNil:[^self scheduleEvent].
 	AccessProtect critical: [
-		beingWaitedOn _ true.
-		resumptionTime _ Time millisecondClockValue + delayDuration.
+		beingWaitedOn := true.
+		resumptionTime := Time millisecondClockValue + delayDuration.
 		ActiveDelay == nil
 			ifTrue: [self activate]
 			ifFalse: [
