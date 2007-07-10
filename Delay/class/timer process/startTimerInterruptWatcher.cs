6285ea29eef55636de89aa35a84cf068fd322f08
ar <unknown>
2007-07-10 22:32:00 +0000

    no message

--- Delay/class/timer process/startTimerInterruptWatcher.cs
+++ Delay/class/timer process/startTimerInterruptWatcher.cs
@@ -1,17 +1,15 @@
 startTimerInterruptWatcher
 	"Reset the class variables that keep track of active Delays and re-start the timer interrupt watcher process. Any currently scheduled delays are forgotten."
 	"Delay startTimerInterruptWatcher"
-
 	| p |
-	self primSignal: nil atMilliseconds: 0.
-	TimingSemaphore == nil
-		ifFalse: [TimingSemaphore terminateProcess].
-	TimingSemaphore _ Semaphore new.
-	AccessProtect _ Semaphore forMutualExclusion.
-	SuspendedDelays _ 
+	self stopTimerEventLoop.
+	self stopTimerInterruptWatcher.
+	TimingSemaphore := Semaphore new.
+	AccessProtect := Semaphore forMutualExclusion.
+	SuspendedDelays := 
 		SortedCollection sortBlock: 
 			[:d1 :d2 | d1 resumptionTime <= d2 resumptionTime].
-	ActiveDelay _ nil.
-	p _ [self timerInterruptWatcher] newProcess.
+	ActiveDelay := nil.
+	p := [self timerInterruptWatcher] newProcess.
 	p priority: Processor timingPriority.
 	p resume.
