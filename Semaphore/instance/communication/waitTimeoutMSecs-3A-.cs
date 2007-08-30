f44e5b99a0b8e951e5a7bedc00da091060085882
ar <unknown>
2007-08-30 16:15:00 +0000

    no message

--- Semaphore/instance/communication/waitTimeoutMSecs-3A-.cs
+++ Semaphore/instance/communication/waitTimeoutMSecs-3A-.cs
@@ -1,11 +1,6 @@
 waitTimeoutMSecs: anInteger
 	"Wait on this semaphore for up to the given number of milliseconds, then timeout. It is up to the sender to determine the difference between the expected event and a timeout."
 
-	| waitingProcess wakeupProcess |
-	waitingProcess _ Processor activeProcess.
-	wakeupProcess _
-		[(Delay forMilliseconds: (anInteger max: 0)) wait.
-		self resumeProcess: waitingProcess] fork.
-
-	self wait.
-	wakeupProcess terminate.
+	| d |
+	d := Delay timeoutSemaphore: self afterMSecs: (anInteger max: 0).
+	[self wait] ensure:[d unschedule].
