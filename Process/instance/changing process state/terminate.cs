67a9e84dae36247ed755782182c8067252e9db14
ar <unknown>
2007-10-05 18:01:00 +0000

    no message

--- Process/instance/changing process state/terminate.cs
+++ Process/instance/changing process state/terminate.cs
@@ -1,27 +1,39 @@
 terminate 
 	"Stop the process that the receiver represents forever.  Unwind to execute pending ensure:/ifCurtailed: blocks before terminating."
 
-	| ctxt unwindBlock |
+	| ctxt unwindBlock inSema |
 	self isActiveProcess ifTrue: [
-		ctxt _ thisContext.
-		[	ctxt _ ctxt findNextUnwindContextUpTo: nil.
+		ctxt := thisContext.
+		[	ctxt := ctxt findNextUnwindContextUpTo: nil.
 			ctxt isNil
 		] whileFalse: [
-			unwindBlock _ ctxt tempAt: 1.
+			unwindBlock := ctxt tempAt: 1.
 			unwindBlock ifNotNil: [
 				ctxt tempAt: 1 put: nil.
 				thisContext terminateTo: ctxt.
 				unwindBlock value].
 		].
 		thisContext terminateTo: nil.
-		myList _ nil.
+		myList := nil.
 		self primitiveSuspend.
 	] ifFalse: [
+		"Since the receiver is not the active process, drop its priority to rock-bottom so that
+		it doesn't accidentally preempt the process that is trying to terminate it."
+		priority := 10.
 		myList ifNotNil: [
 			myList remove: self ifAbsent: [].
-			myList _ nil].
+			"Figure out if the receiver was terminated while waiting on a Semaphore"
+			inSema := myList class == Semaphore.
+			myList := nil].
 		suspendedContext ifNotNil: [
-			ctxt _ self popTo: suspendedContext bottomContext.
+			"Figure out if we are terminating the process while waiting in Semaphore>>critical:
+			In this case, pop the suspendedContext so that we leave the ensure: block inside
+			Semaphore>>critical: without signaling the semaphore."
+			(inSema == true and:[
+				suspendedContext method == (Semaphore compiledMethodAt: #critical:)]) ifTrue:[
+					suspendedContext := suspendedContext home.
+			].
+			ctxt := self popTo: suspendedContext bottomContext.
 			ctxt == suspendedContext bottomContext ifFalse: [
 				self debug: ctxt title: 'Unwind error during termination']].
 	].
