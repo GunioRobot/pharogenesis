678a332dca1ec76aa5529ccbb00598c100007405
ar <unknown>
2007-10-05 17:59:00 +0000

    no message

--- Semaphore/instance/mutual exclusion/critical-3A-.cs
+++ Semaphore/instance/mutual exclusion/critical-3A-.cs
@@ -1,10 +1,12 @@
-critical: mutuallyExcludedBlock 
-	"Evaluate mutuallyExcludedBlock only if the receiver is not currently in 
-	the process of running the critical: message. If the receiver is, evaluate 
+critical: mutuallyExcludedBlock			
+	"Evaluate mutuallyExcludedBlock only if the receiver is not currently in
+	the process of running the critical: message. If the receiver is, evaluate
 	mutuallyExcludedBlock after the other critical: message is finished."
-
-	| blockValue |
-	self wait.
-	[blockValue _ mutuallyExcludedBlock value]
-		ensure: [self signal].
+	| blockValue caught |
+	caught := false.
+	[
+		caught := true.
+		self wait.
+		blockValue := mutuallyExcludedBlock value
+	] ensure: [caught ifTrue: [self signal]].
 	^blockValue
