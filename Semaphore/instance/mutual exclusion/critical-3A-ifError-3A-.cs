97b363765ca2c5e7f7a1f38010912eb1dc1cddbe
ar <unknown>
2007-10-05 17:59:00 +0000

    no message

--- Semaphore/instance/mutual exclusion/critical-3A-ifError-3A-.cs
+++ Semaphore/instance/mutual exclusion/critical-3A-ifError-3A-.cs
@@ -2,16 +2,14 @@ critical: mutuallyExcludedBlock ifError: errorBlock
 	"Evaluate mutuallyExcludedBlock only if the receiver is not currently in 
 	the process of running the critical: message. If the receiver is, evaluate 
 	mutuallyExcludedBlock after the other critical: message is finished."
-
 	| blockValue hasError errMsg errRcvr |
-	self wait.
-	hasError _ false.
-	blockValue _ [mutuallyExcludedBlock value] ifError:[:msg :rcvr|
-		hasError _ true.
-		errMsg _ msg.
-		errRcvr _ rcvr].
-	hasError ifTrue:[
-		self signal.
-		^errorBlock value: errMsg value: errRcvr].
-	self signal.
+	hasError := false.
+	self critical:[
+		blockValue := [mutuallyExcludedBlock value] ifError:[:msg :rcvr|
+			hasError := true.
+			errMsg := msg.
+			errRcvr := rcvr
+		].
+	].
+	hasError ifTrue:[ ^errorBlock value: errMsg value: errRcvr].
 	^blockValue
