e2bcf6623c4ed744d40adb56f4d4610ef6e7bbde
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/class/as yet unclassified/createExternalFontFileForUnicodeJapanese-3A-.cs
+++ StrikeFontSet/class/as yet unclassified/createExternalFontFileForUnicodeJapanese-3A-.cs
@@ -4,18 +4,18 @@ createExternalFontFileForUnicodeJapanese: fileName
 "
 
 	| file array f installDirectory |
-	file _ FileStream newFileNamed: fileName.
-	installDirectory _ Smalltalk at: #M17nInstallDirectory ifAbsent: [].
-	installDirectory _ installDirectory
+	file := FileStream newFileNamed: fileName.
+	installDirectory := Smalltalk at: #M17nInstallDirectory ifAbsent: [].
+	installDirectory := installDirectory
 		ifNil: [String new]
 		ifNotNil: [installDirectory , FileDirectory pathNameDelimiter asString].
-	array _ Array
+	array := Array
 				with: (StrikeFont newForJapaneseFromEFontBDFFile: installDirectory , 'b12.bdf' name: 'Japanese10' overrideWith: 'shnmk12.bdf')
 				with: ((StrikeFont newForJapaneseFromEFontBDFFile: installDirectory , 'b14.bdf' name: 'Japanese12' overrideWith: 'shnmk14.bdf') "fixAscent: 14 andDescent: 1 head: 1")
 				with: ((StrikeFont newForJapaneseFromEFontBDFFile: 'b16.bdf' name: 'Japanese14' overrideWith: 'shnmk16.bdf') "fixAscent: 16 andDescent: 4 head: 4")
 				with: (StrikeFont newForJapaneseFromEFontBDFFile: installDirectory , 'b24.bdf' name: 'Japanese18' overrideWith: 'kanji24.bdf').
 	TextConstants at: #forceFontWriting put: true.
-	f _ ReferenceStream on: file.
+	f := ReferenceStream on: file.
 	f nextPut: array.
 	file close.
 	TextConstants removeKey: #forceFontWriting.
