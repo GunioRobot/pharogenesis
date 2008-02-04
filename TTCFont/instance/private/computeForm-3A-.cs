355c1fe922894069ae58065b59fcee53704d389f
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/instance/private/computeForm-3A-.cs
+++ TTCFont/instance/private/computeForm-3A-.cs
@@ -1,7 +1,7 @@
 computeForm: char
 
 	| ttGlyph scale |
-	scale _ self pixelSize asFloat / (ttcDescription ascender - ttcDescription descender).
-	Scale ifNotNil: [scale _ Scale * scale].
-	ttGlyph _ ttcDescription at: (char isCharacter ifTrue: [char charCode] ifFalse: [char]).
+	scale := self pixelSize asFloat / (ttcDescription ascender - ttcDescription descender).
+	Scale ifNotNil: [scale := Scale * scale].
+	ttGlyph := ttcDescription at: (char isCharacter ifTrue: [char charCode] ifFalse: [char]).
 	^ ttGlyph asFormWithScale: scale ascender: ttcDescription ascender descender: ttcDescription descender fgColor: foregroundColor bgColor: Color transparent depth: self depth.
