d3376f89ede678654379c27c638c0d2b735a7638
sd <unknown>
2008-02-04 21:21:00 +0000

    no message

--- LanguageEditor/instance/gui methods/selectNewerKeys.cs
+++ LanguageEditor/instance/gui methods/selectNewerKeys.cs
@@ -2,9 +2,9 @@ selectNewerKeys
 
 	| translations index |
 	self deselectAllTranslation.
-	translations _ self translations.
+	translations := self translations.
 	newerKeys do: [:k |
-		index _ translations indexOf: k ifAbsent: [0].
+		index := translations indexOf: k ifAbsent: [0].
 		index > 0 ifTrue: [
 			self selectedTranslationsAt: index put: true
 		].
