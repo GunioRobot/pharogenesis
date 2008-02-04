c09b7bbe17367bce6bcf6c92e0f3b9aa00967263
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontReader/instance/as yet unclassified/getTableDirEntry-3A-from-3A-offset-3A-.cs
+++ TTCFontReader/instance/as yet unclassified/getTableDirEntry-3A-from-3A-offset-3A-.cs
@@ -2,13 +2,13 @@ getTableDirEntry: tagString from: fontData offset: offset
 	"Find the table named tagString in fontData and return a table directory entry for it."
 
 	| nTables pos currentTag tag |
-	nTables _ fontData shortAt: 5 + offset bigEndian: true.
-	tag _ ByteArray new: 4.
+	nTables := fontData shortAt: 5 + offset bigEndian: true.
+	tag := ByteArray new: 4.
 	1 to: 4 do:[:i| tag byteAt: i put: (tagString at: i) asInteger].
-	tag _ tag longAt: 1 bigEndian: true.
-	pos _ 13 + offset.
+	tag := tag longAt: 1 bigEndian: true.
+	pos := 13 + offset.
 	1 to: nTables do:[:i|
-		currentTag _ fontData longAt: pos bigEndian: true.
+		currentTag := fontData longAt: pos bigEndian: true.
 		currentTag = tag ifTrue:[^TTFontTableDirEntry on: fontData at: pos].
-		pos _ pos+16].
+		pos := pos+16].
 	^nil
