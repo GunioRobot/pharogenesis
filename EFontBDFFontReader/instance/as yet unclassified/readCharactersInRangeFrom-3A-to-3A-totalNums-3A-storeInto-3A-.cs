454707d33b332bcb7e6ce0414f9fbb1c85b770e1
sd <unknown>
2008-02-04 20:28:00 +0000

    no message

--- EFontBDFFontReader/instance/as yet unclassified/readCharactersInRangeFrom-3A-to-3A-totalNums-3A-storeInto-3A-.cs
+++ EFontBDFFontReader/instance/as yet unclassified/readCharactersInRangeFrom-3A-to-3A-totalNums-3A-storeInto-3A-.cs
@@ -2,11 +2,11 @@ readCharactersInRangeFrom: start to: stop totalNums: upToNum storeInto: chars
 
 	| array form code |
 	1 to: upToNum do: [:i |
-		array _ self readOneCharacter.
-		code _ array at: 2.
+		array := self readOneCharacter.
+		code := array at: 2.
 		code > stop ifTrue: [^ self].
 		(code between: start and: stop) ifTrue: [
-			form _ array at: 1.
+			form := array at: 1.
 			form ifNotNil: [
 				chars add: array.
 			].
