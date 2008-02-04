c362a62c322fbd9129ac5667ab4d05462e47239d
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/class/as yet unclassified/decodedFromRemoteCanvas-3A-.cs
+++ StrikeFontSet/class/as yet unclassified/decodedFromRemoteCanvas-3A-.cs
@@ -1,5 +1,5 @@
 decodedFromRemoteCanvas: aString
 
 	| array |
-	array _ aString findTokens: #($ ).
+	array := aString findTokens: #($ ).
 	^ self familyName: (array at: 1) size: (array at: 2) asNumber emphasized: (array at: 3) asNumber.
