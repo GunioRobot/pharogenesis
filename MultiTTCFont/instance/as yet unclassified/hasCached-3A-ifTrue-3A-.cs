5be64861305314d192d0bd9ec0610a4b54d57bac
sd <unknown>
2008-02-04 21:12:00 +0000

    no message

--- MultiTTCFont/instance/as yet unclassified/hasCached-3A-ifTrue-3A-.cs
+++ MultiTTCFont/instance/as yet unclassified/hasCached-3A-ifTrue-3A-.cs
@@ -1,10 +1,10 @@
 hasCached: char ifTrue: twoArgBlock
 
 	| value elem |
-	value _ char asciiValue.
+	value := char asciiValue.
 
 	self cache size to: 1 by: -1 do: [:i |
-		elem _ self cache at: i.
+		elem := self cache at: i.
 		(elem first = value and: [elem second = foregroundColor]) ifTrue: [
 			^ twoArgBlock value: elem third value: i.
 		].
