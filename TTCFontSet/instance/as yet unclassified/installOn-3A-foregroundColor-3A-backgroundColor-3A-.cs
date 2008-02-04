304100ad5c6b946bc663bb9dc01a54367c282e8e
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontSet/instance/as yet unclassified/installOn-3A-foregroundColor-3A-backgroundColor-3A-.cs
+++ TTCFontSet/instance/as yet unclassified/installOn-3A-foregroundColor-3A-backgroundColor-3A-.cs
@@ -1,4 +1,4 @@
 installOn: aDisplayContext foregroundColor: fgColor backgroundColor: bgColor
 
-	foregroundColor _ fgColor.
+	foregroundColor := fgColor.
 	fontArray do: [:s | s ifNotNil: [s installOn: aDisplayContext foregroundColor: fgColor backgroundColor: bgColor]].
