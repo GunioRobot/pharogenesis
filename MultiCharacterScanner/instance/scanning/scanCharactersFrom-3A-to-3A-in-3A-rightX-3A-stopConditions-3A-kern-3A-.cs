dc3aade234e411bd502d157a20fabddc819e52fc
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiCharacterScanner/instance/scanning/scanCharactersFrom-3A-to-3A-in-3A-rightX-3A-stopConditions-3A-kern-3A-.cs
+++ MultiCharacterScanner/instance/scanning/scanCharactersFrom-3A-to-3A-in-3A-rightX-3A-stopConditions-3A-kern-3A-.cs
@@ -4,9 +4,9 @@ scanCharactersFrom: startIndex to: stopIndex in: sourceString rightX: rightX sto
 	(sourceString isByteString) ifTrue: [^ self basicScanCharactersFrom: startIndex to: stopIndex in: sourceString rightX: rightX stopConditions: stops kern: kernDelta.].
 
 	(sourceString isWideString) ifTrue: [
-		startIndex > stopIndex ifTrue: [lastIndex _ stopIndex. ^ stops at: EndOfRun].
-		startEncoding _  (sourceString at: startIndex) leadingChar.
-		selector _ (EncodedCharSet charsetAt: startEncoding) scanSelector.
+		startIndex > stopIndex ifTrue: [lastIndex := stopIndex. ^ stops at: EndOfRun].
+		startEncoding :=  (sourceString at: startIndex) leadingChar.
+		selector := (EncodedCharSet charsetAt: startEncoding) scanSelector.
 		^ self perform: selector withArguments: (Array with: startIndex with: stopIndex with: sourceString with: rightX with: stopConditions with: kernDelta).
 	].
 	
