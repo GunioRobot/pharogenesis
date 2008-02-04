5b4b46ca11bb0845ada66343967346f4a7001e12
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/instance/displaying/displayString-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-from-3A-baselineY-3A-.cs
+++ StrikeFontSet/instance/displaying/displayString-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-from-3A-baselineY-3A-.cs
@@ -1,29 +1,29 @@
 displayString: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint kern: kernDelta from: fromFont baselineY: baselineY
 
 	| destPoint leftX rightX glyphInfo g tag char destY |
-	destPoint _ aPoint.
-	rIndex _ startIndex.
-	tag _ (aString at: rIndex) leadingChar.
-	glyphInfo _ Array new: 5.
+	destPoint := aPoint.
+	rIndex := startIndex.
+	tag := (aString at: rIndex) leadingChar.
+	glyphInfo := Array new: 5.
 	[rIndex <= stopIndex] whileTrue: [
-		char _ aString at: rIndex.
+		char := aString at: rIndex.
 		((fromFont hasGlyphOf: char) or: [char leadingChar ~= tag]) ifTrue: [^ Array with: rIndex with: destPoint].
 		self glyphInfoOf: char into: glyphInfo.
-		g _ glyphInfo first.
-		leftX _ glyphInfo second.
-		rightX _ glyphInfo third.
+		g := glyphInfo first.
+		leftX := glyphInfo second.
+		rightX := glyphInfo third.
 		(glyphInfo fifth ~= aBitBlt lastFont) ifTrue: [
 			glyphInfo fifth installOn: aBitBlt.
 		].
 		aBitBlt sourceForm: g.
-		destY _ baselineY - glyphInfo fourth. 
+		destY := baselineY - glyphInfo fourth. 
 		aBitBlt destX: destPoint x.
 		aBitBlt destY: destY.
 		aBitBlt sourceOrigin: leftX @ 0.
 		aBitBlt width: rightX - leftX.
 		aBitBlt height: self height.
 		aBitBlt copyBits.
-		destPoint _ destPoint + (rightX - leftX + kernDelta @ 0).
-		rIndex _ rIndex + 1.
+		destPoint := destPoint + (rightX - leftX + kernDelta @ 0).
+		rIndex := rIndex + 1.
 	].
 	^ Array with: rIndex with: destPoint.
