1a62b44e292eac90091ec0ef052ab611bc8b45b4
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/instance/displaying/characters-3A-in-3A-displayAt-3A-clippedBy-3A-rule-3A-fillColor-3A-kernDelta-3A-on-3A-.cs
+++ StrikeFontSet/instance/displaying/characters-3A-in-3A-displayAt-3A-clippedBy-3A-rule-3A-fillColor-3A-kernDelta-3A-on-3A-.cs
@@ -3,24 +3,24 @@ characters: anInterval in: sourceString displayAt: aPoint clippedBy: clippingRec
 	No wrap-around is provided."
 
 	| ascii encoding destPoint leftX rightX sourceRect xTable noFont f |
-	destPoint _ aPoint.
+	destPoint := aPoint.
 	anInterval do: 
 		[:i |
-		encoding _ (sourceString at: i) leadingChar + 1.
-		noFont _ false.
-		[f _ fontArray at: encoding]
-			on: Exception do: [:ex | noFont _ true. f _ fontArray at: 1].
-		f ifNil: [noFont _ true. f _ fontArray at: 1].
-		ascii _ noFont ifTrue: [$?] ifFalse: [(sourceString at: i) charCode].
+		encoding := (sourceString at: i) leadingChar + 1.
+		noFont := false.
+		[f := fontArray at: encoding]
+			on: Exception do: [:ex | noFont := true. f := fontArray at: 1].
+		f ifNil: [noFont := true. f := fontArray at: 1].
+		ascii := noFont ifTrue: [$?] ifFalse: [(sourceString at: i) charCode].
 		(ascii < f minAscii
 			or: [ascii > f maxAscii])
-			ifTrue: [ascii _ f maxAscii].
-		xTable _ f xTable.
-		leftX _ xTable at: ascii + 1.
-		rightX _ xTable at: ascii + 2.
-		sourceRect _ leftX@0 extent: (rightX-leftX) @ self height.
+			ifTrue: [ascii := f maxAscii].
+		xTable := f xTable.
+		leftX := xTable at: ascii + 1.
+		rightX := xTable at: ascii + 2.
+		sourceRect := leftX@0 extent: (rightX-leftX) @ self height.
 		aBitBlt copyFrom: sourceRect in: f glyphs to: destPoint.
-		destPoint _ destPoint + ((rightX-leftX+kernDelta)@0).
+		destPoint := destPoint + ((rightX-leftX+kernDelta)@0).
 		"destPoint printString displayAt: 0@(i*20)."
 	].
 	^ destPoint.
