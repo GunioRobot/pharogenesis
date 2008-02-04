48101e7bc193646d68ba2994431ea4f099ecd4c3
sd <unknown>
2008-02-04 21:13:00 +0000

    no message

--- StrikeFontSet/instance/private/addNewFont-3A-at-3A-.cs
+++ StrikeFontSet/instance/private/addNewFont-3A-at-3A-.cs
@@ -2,10 +2,10 @@ addNewFont: aFont at: encodingIndex
 
 	| newArray |
 	encodingIndex > fontArray size ifTrue: [
-		newArray _ Array new: encodingIndex.
+		newArray := Array new: encodingIndex.
 		newArray replaceFrom: 1 to: fontArray size with: fontArray startingAt: 1.
 	] ifFalse: [
-		newArray _ fontArray.
+		newArray := fontArray.
 	].
 
 	newArray at: encodingIndex put: aFont.
