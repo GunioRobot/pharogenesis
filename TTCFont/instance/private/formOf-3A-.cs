a44bb92c1d281790e07975a472af42be39cbc596
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/instance/private/formOf-3A-.cs
+++ TTCFont/instance/private/formOf-3A-.cs
@@ -4,14 +4,14 @@ formOf: char
 	char charCode > 255
 		ifTrue: [^ self fallbackFont formOf: char].
 
-	code _ char charCode.
-	assoc _ self cache at: (code + 1).
+	code := char charCode.
+	assoc := self cache at: (code + 1).
 	assoc ifNotNil: [
 		(assoc key = foregroundColor) ifTrue: [
 			^ assoc value.
 		].
 	].
 
-	f _ self computeForm: code.
+	f := self computeForm: code.
 	self at: code put: f.
 	^ f.
