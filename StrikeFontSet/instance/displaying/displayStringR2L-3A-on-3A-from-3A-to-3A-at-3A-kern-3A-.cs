dbfe606e1f692e2719e5ccc5982de1ad40dc1608
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/instance/displaying/displayStringR2L-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-.cs
+++ StrikeFontSet/instance/displaying/displayStringR2L-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-.cs
@@ -1,17 +1,17 @@
 displayStringR2L: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint kern: kernDelta 
 
 	| destPoint font |
-	destPoint _ aPoint.
+	destPoint := aPoint.
 	startIndex to: stopIndex do: [:charIndex | 
 		| encoding ascii xTable leftX rightX | 
-		encoding _ (aString at: charIndex) leadingChar + 1.
-		ascii _ (aString at: charIndex) charCode.
-		font _ fontArray at: encoding.
+		encoding := (aString at: charIndex) leadingChar + 1.
+		ascii := (aString at: charIndex) charCode.
+		font := fontArray at: encoding.
 		((ascii between: font minAscii and: font maxAscii) not) ifTrue: [
-			ascii _ font maxAscii].
-		xTable _ font xTable.
-		leftX _ xTable at: ascii + 1.
-		rightX _ xTable at: ascii + 2.
+			ascii := font maxAscii].
+		xTable := font xTable.
+		leftX := xTable at: ascii + 1.
+		rightX := xTable at: ascii + 2.
 		aBitBlt sourceForm: font glyphs.
 		aBitBlt destX: destPoint x - (rightX - leftX).
 		aBitBlt destY: destPoint y.
@@ -19,5 +19,5 @@ displayStringR2L: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint 
 		aBitBlt width: rightX - leftX.
 		aBitBlt height: self height.
 		aBitBlt copyBits.
-		destPoint _ destPoint - (rightX - leftX + kernDelta @ 0).
+		destPoint := destPoint - (rightX - leftX + kernDelta @ 0).
 	].
