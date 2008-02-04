0e8fc110b5c67e45d3f9d6671fd5ccdd0e071804
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiCompositionScanner/instance/scanning/composeFrom-3A-inRectangle-3A-firstLine-3A-leftSide-3A-rightSide-3A-.cs
+++ MultiCompositionScanner/instance/scanning/composeFrom-3A-inRectangle-3A-firstLine-3A-leftSide-3A-rightSide-3A-.cs
@@ -3,37 +3,37 @@ composeFrom: startIndex inRectangle: lineRectangle
 	"Answer an instance of TextLineInterval that represents the next line in the paragraph."
 	| runLength done stopCondition |
 	"Set up margins"
-	leftMargin _ lineRectangle left.
-	leftSide ifTrue: [leftMargin _ leftMargin +
+	leftMargin := lineRectangle left.
+	leftSide ifTrue: [leftMargin := leftMargin +
 						(firstLine ifTrue: [textStyle firstIndent]
 								ifFalse: [textStyle restIndent])].
-	destX _ spaceX _ leftMargin.
-	firstDestX _ destX.
-	rightMargin _ lineRectangle right.
-	rightSide ifTrue: [rightMargin _ rightMargin - textStyle rightIndent].
-	lastIndex _ startIndex.	"scanning sets last index"
-	destY _ lineRectangle top.
-	lineHeight _ baseline _ 0.  "Will be increased by setFont"
+	destX := spaceX := leftMargin.
+	firstDestX := destX.
+	rightMargin := lineRectangle right.
+	rightSide ifTrue: [rightMargin := rightMargin - textStyle rightIndent].
+	lastIndex := startIndex.	"scanning sets last index"
+	destY := lineRectangle top.
+	lineHeight := baseline := 0.  "Will be increased by setFont"
 	self setStopConditions.	"also sets font"
-	runLength _ text runLengthFor: startIndex.
-	runStopIndex _ (lastIndex _ startIndex) + (runLength - 1).
-	line _ (TextLine start: lastIndex stop: 0 internalSpaces: 0 paddingWidth: 0)
+	runLength := text runLengthFor: startIndex.
+	runStopIndex := (lastIndex := startIndex) + (runLength - 1).
+	line := (TextLine start: lastIndex stop: 0 internalSpaces: 0 paddingWidth: 0)
 				rectangle: lineRectangle.
-	presentationLine _ (TextLine start: lastIndex stop: 0 internalSpaces: 0 paddingWidth: 0)
+	presentationLine := (TextLine start: lastIndex stop: 0 internalSpaces: 0 paddingWidth: 0)
 				rectangle: lineRectangle.
-	numOfComposition _ 0.
-	spaceCount _ 0.
+	numOfComposition := 0.
+	spaceCount := 0.
 	self handleIndentation.
-	leftMargin _ destX.
+	leftMargin := destX.
 	line leftMargin: leftMargin.
 	presentationLine leftMargin: leftMargin.
 
-	presentation _ TextStream on: (Text fromString: (WideString new: text size)).
+	presentation := TextStream on: (Text fromString: (WideString new: text size)).
 
-	done _ false.
+	done := false.
 	[done]
 		whileFalse: 
-			[stopCondition _ self scanCharactersFrom: lastIndex to: runStopIndex
+			[stopCondition := self scanCharactersFrom: lastIndex to: runStopIndex
 				in: text string rightX: rightMargin stopConditions: stopConditions
 				kern: kern.
 			"See setStopConditions for stopping conditions for composing."
