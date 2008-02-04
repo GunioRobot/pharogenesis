ca09435250313f7b968a57d357ff3628df59bae9
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontSet/class/as yet unclassified/newTextStyleFromTT-3A-.cs
+++ TTCFontSet/class/as yet unclassified/newTextStyleFromTT-3A-.cs
@@ -2,10 +2,10 @@ newTextStyleFromTT: descriptionArray
 
 	| array f textStyle styleName arrayOfArray |
 
-	arrayOfArray _ self pointSizes collect: [:pt |
+	arrayOfArray := self pointSizes collect: [:pt |
 		descriptionArray collect: [:ttc |
 			ttc ifNil: [nil] ifNotNil: [
-				f _ (ttc size > 256)
+				f := (ttc size > 256)
 					ifTrue: [MultiTTCFont new initialize]
 					ifFalse: [TTCFont new initialize].
 				f ttcDescription: ttc.
@@ -14,12 +14,12 @@ newTextStyleFromTT: descriptionArray
 		].
 	].
 
-	array _ arrayOfArray collect: [:fonts |
+	array := arrayOfArray collect: [:fonts |
 		self newFontArray: fonts.
 	].
 
-	styleName _ (array at: 1) familyName asSymbol.
-	textStyle _ TextStyle fontArray: array.
+	styleName := (array at: 1) familyName asSymbol.
+	textStyle := TextStyle fontArray: array.
 	TextConstants at: styleName put: textStyle.
 
 	self register: array at: styleName.
