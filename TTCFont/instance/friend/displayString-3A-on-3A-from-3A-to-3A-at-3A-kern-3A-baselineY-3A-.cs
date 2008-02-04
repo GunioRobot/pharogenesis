e0df0895b4cabd40df436b63285a95c24b7716eb
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/instance/friend/displayString-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-baselineY-3A-.cs
+++ TTCFont/instance/friend/displayString-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-baselineY-3A-.cs
@@ -1,15 +1,15 @@
 displayString: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint kern: kernDelta baselineY: baselineY
 
 	| destPoint form glyphInfo destY |
-	destPoint _ aPoint.
-	glyphInfo _ Array new: 5.
+	destPoint := aPoint.
+	glyphInfo := Array new: 5.
 	startIndex to: stopIndex do: [:charIndex |
 		self glyphInfoOf: (aString at: charIndex) into: glyphInfo.
-		form _ glyphInfo first.
+		form := glyphInfo first.
 		((glyphInfo at:5) ~= aBitBlt lastFont) ifTrue: [
 			(glyphInfo at:5) installOn: aBitBlt.
 		].
-		destY _ baselineY - (glyphInfo at:4). 
+		destY := baselineY - (glyphInfo at:4). 
 		aBitBlt sourceForm: form.
 		aBitBlt destX: destPoint x.
 		aBitBlt destY: destY.
@@ -17,6 +17,6 @@ displayString: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint ker
 		aBitBlt width: glyphInfo third - glyphInfo second.
 		aBitBlt height: form height.
 		aBitBlt copyBits.
-		destPoint _ destPoint + (((glyphInfo at:3) - (glyphInfo at:2)) + kernDelta @ 0).
+		destPoint := destPoint + (((glyphInfo at:3) - (glyphInfo at:2)) + kernDelta @ 0).
 	].
 	^ destPoint.
