bc1e51a2ed04a1bb7984c79d62c64f8a9ec779f7
sd <unknown>
2008-02-04 16:21:00 +0000

    no message

---
+++ ScriptLoader/instance/private helpers/WelcomeText.cs
@@ -0,0 +1,30 @@
+WelcomeText
+
+	^ 'Welcome to Squeak - an open Smalltalk system.
+
+What is this?
+Squeak is an open source implementation of Smalltalk with an expansive and rapidly developing world of objects included. 
+Squeak includes a fully integrated development environment, networking, sound synthesis and sampling, speech synthesis, 2 & 3D graphics, arithmetic and data structure libraries.
+The SqueakMap tool (click on -> "SMLoader open" to start it) tool provides easy access to a large range of packages and projects.
+Even the tools to produce the its own virtual machine (VM) are available so that you can build your own - and most of the VM is written in Smalltalk. Not only is all the Smalltalk source code included and changeable at will, it is also completely open and free. The Squeak system image runs bit-identically across all platforms, and VMs are available for just about every computer and operating system available. 
+
+To find out exactly which version of Squeak this is click on -> "SystemVersion current"
+ 
+Further Documentation
+The primary website for Squeak is 
+	http://www.squeak.org
+and a large amount of online information is linked to from there. There are several very active mailing lists that you might consider joining.
+The Squeak Swiki at Georgia Tech is currently the primary source of online documentation and community information regarding Squeak.  It contains a FAQ, tutorials, and much more.  Visit it at:
+	http://minnow.cc.gatech.edu/squeak
+Squeak depends on the involvement and contributions of people like you. Please take a look around the website, join the mail list, report bugs, offer suggestions and most of all have fun with it.
+
+The Squeak License
+Squeak is distributed for use and modification subject to a liberal open source license.  See the main Squeak website for details.  Unless stated to the contrary, works submitted for incorporation into or for distribution with Squeak shall be presumed subject to the same license.
+
+Portions of Squeak are:
+
+Copyright (c) 1996 Apple Computer, Inc.
+Copyright (c) 1997-2001 Walt Disney Company, and/or
+Copyrighted works of many other contributors.
+All rights reserved.
+'
