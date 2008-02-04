d7c418831be7957acf2dde0bb138710a4896747f
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontSet/class/as yet unclassified/familyName-3A-pointSize-3A-.cs
+++ TTCFontSet/class/as yet unclassified/familyName-3A-pointSize-3A-.cs
@@ -2,15 +2,15 @@ familyName: n pointSize: s
 
 	"(self familyName: 'MultiMSGothic' pointSize: 14) pointSize"
 	| t ret index |
-	t _ self registry at: n asSymbol ifAbsent: [#()].
+	t := self registry at: n asSymbol ifAbsent: [#()].
 	t isEmpty ifTrue: [
-		t _ (TextConstants at: #DefaultTextStyle) fontArray.
-		ret _ t first.
+		t := (TextConstants at: #DefaultTextStyle) fontArray.
+		ret := t first.
 		ret pointSize >= s ifTrue: [^ ret].
-		index _ 2.
+		index := 2.
 		[index <= t size and: [(t at: index) pointSize <= s]] whileTrue: [
-			ret _ t at: index.
-			index _ index + 1.
+			ret := t at: index.
+			index := index + 1.
 		].
 		^ ret.
 	].
