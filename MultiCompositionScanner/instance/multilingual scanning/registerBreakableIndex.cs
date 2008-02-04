47a9584965c1f8ad5c4c38a8ef2f81c291f50e56
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiCompositionScanner/instance/multilingual scanning/registerBreakableIndex.cs
+++ MultiCompositionScanner/instance/multilingual scanning/registerBreakableIndex.cs
@@ -5,17 +5,17 @@ registerBreakableIndex
 	right edge of the composition rectangle of the paragraph."
 
 	(text at: lastIndex) = Character space ifTrue: [
-		breakAtSpace _ true.
-		spaceX _ destX.
-		spaceCount _ spaceCount + 1.
-		lineHeightAtBreak _ lineHeight.
-		baselineAtBreak _ baseline.
-		breakableIndex _ lastIndex.
+		breakAtSpace := true.
+		spaceX := destX.
+		spaceCount := spaceCount + 1.
+		lineHeightAtBreak := lineHeight.
+		baselineAtBreak := baseline.
+		breakableIndex := lastIndex.
 		destX > rightMargin ifTrue: 	[^self crossedX].
 	] ifFalse: [
-		breakAtSpace _ false.
-		lineHeightAtBreak _ lineHeight.
-		baselineAtBreak _ baseline.
-		breakableIndex _ lastIndex - 1.
+		breakAtSpace := false.
+		lineHeightAtBreak := lineHeight.
+		baselineAtBreak := baseline.
+		breakableIndex := lastIndex - 1.
 	].
 	^ false.
