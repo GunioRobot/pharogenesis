bf7bf05013107e1c334899f31bf0f85871feeb35
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiDisplayScanner/instance/stop conditions/plainTab.cs
+++ MultiDisplayScanner/instance/stop conditions/plainTab.cs
@@ -1,6 +1,6 @@
 plainTab
 	| oldX |
-	oldX _ destX.
+	oldX := destX.
 	super plainTab.
 	fillBlt == nil ifFalse:
 		[fillBlt destX: oldX destY: destY width: destX - oldX height: font height; copyBits]
