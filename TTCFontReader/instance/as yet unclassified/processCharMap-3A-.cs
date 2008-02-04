25b6d3e3a9ec7d95606ec866a6b8c2f65ddfa372
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontReader/instance/as yet unclassified/processCharMap-3A-.cs
+++ TTCFontReader/instance/as yet unclassified/processCharMap-3A-.cs
@@ -2,23 +2,23 @@ processCharMap: assoc
 	"Process the given character map"
 
 	| glyph cmap encode0 encode1 char value null |
-	cmap _ assoc value.
-	null _ (glyphs at: (cmap at: Character space asUnicode + 1) + 1) copy.
+	cmap := assoc value.
+	null := (glyphs at: (cmap at: Character space asUnicode + 1) + 1) copy.
 	null contours: #().
 
-	encode0 _ Array new: 256 withAll: glyphs first.
-	encode1 _ Array new: 65536 withAll: glyphs first.
+	encode0 := Array new: 256 withAll: glyphs first.
+	encode1 := Array new: 65536 withAll: glyphs first.
 
 	0 to: 255 do: [:i |
-		char _ Character value: i.
-		glyph _ glyphs at: (cmap at: char asUnicode + 1) + 1.
+		char := Character value: i.
+		glyph := glyphs at: (cmap at: char asUnicode + 1) + 1.
 		encode0 at: i+1 put: glyph.
 	].
 	Character separators do: [:c |
 		encode0 at: (c asciiValue + 1) put: null.
 	].
 	0 to: 65536 - 1 do: [:i |
-		value _ cmap at: i+1.
+		value := cmap at: i+1.
 		value = 65535 ifFalse: [ "???"
 			encode1 at: i+1 put: (glyphs at: value+1).
 		]
