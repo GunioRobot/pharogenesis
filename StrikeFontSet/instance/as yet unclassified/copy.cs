7274b353f0d8e6d7671e30f144137996250d3e79
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/instance/as yet unclassified/copy.cs
+++ StrikeFontSet/instance/as yet unclassified/copy.cs
@@ -1,11 +1,11 @@
 copy
 
 	| s a |
-	s _ self class new.
+	s := self class new.
 	s name: self name.
 	s emphasis: self emphasis.
 	s reset.
-	a _ Array new: fontArray size.
+	a := Array new: fontArray size.
 	1 to: a size do: [:i |
 		a at: i put: (fontArray at: i) copy.
 	].
