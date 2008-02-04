1d62ed0940d9a57e22be283b1f1616d0b8ebe7ee
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/instance/private/addLined-3A-.cs
+++ TTCFont/instance/private/addLined-3A-.cs
@@ -1,11 +1,11 @@
 addLined: aTTCFont
 
 	| l |
-	l _ LinedTTCFont fromTTCFont: aTTCFont emphasis: 4.
+	l := LinedTTCFont fromTTCFont: aTTCFont emphasis: 4.
 	self derivativeFont: l at: l emphasis.
 
-	l _ LinedTTCFont fromTTCFont: aTTCFont emphasis: 16.
+	l := LinedTTCFont fromTTCFont: aTTCFont emphasis: 16.
 	self derivativeFont: l at: l emphasis.
 
-	l _ LinedTTCFont fromTTCFont: aTTCFont emphasis: 20.
+	l := LinedTTCFont fromTTCFont: aTTCFont emphasis: 20.
 	self derivativeFont: l at: l emphasis.
