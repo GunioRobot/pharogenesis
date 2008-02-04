76c83c87099a7f7d40d13eaa49ebd2f2e7de3885
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiDisplayScanner/instance/private/setFont.cs
+++ MultiDisplayScanner/instance/private/setFont.cs
@@ -1,7 +1,7 @@
 setFont 
-	foregroundColor _ paragraphColor.
+	foregroundColor := paragraphColor.
 	super setFont.  "Sets font and emphasis bits, and maybe foregroundColor"
 	font installOn: bitBlt foregroundColor: foregroundColor backgroundColor: Color transparent.
 	text ifNotNil:[
-		baselineY _ lineY + line baseline.
-		destY _ baselineY - font ascent].
+		baselineY := lineY + line baseline.
+		destY := baselineY - font ascent].
