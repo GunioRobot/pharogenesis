4ce76dfa8d6c912a41dc1d11159b71a3b1ced3bc
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontReader/instance/as yet unclassified/processCharacterMappingTable-3A-.cs
+++ TTCFontReader/instance/as yet unclassified/processCharacterMappingTable-3A-.cs
@@ -3,23 +3,23 @@ processCharacterMappingTable: entry
 	If an appropriate mapping can be found then return an association
 	with the format identifier and the contents of the table"
 	| copy initialOffset nSubTables pID sID offset cmap assoc |
-	initialOffset _ entry offset.
+	initialOffset := entry offset.
 	entry skip: 2. "Skip table version"
-	nSubTables _ entry nextUShort.
+	nSubTables := entry nextUShort.
 	1 to: nSubTables do:[:i|
-		pID _ entry nextUShort.
-		sID _ entry nextUShort.
-		offset _ entry nextULong.
+		pID := entry nextUShort.
+		sID := entry nextUShort.
+		offset := entry nextULong.
 		"Check if this is either a Macintosh encoded table
 		or a Windows encoded table"
 		(pID = 1 or:[pID = 3]) ifTrue:[
 			"Go to the beginning of the table"
-			copy _ entry copy.
+			copy := entry copy.
 			copy offset: initialOffset + offset.
-			cmap _ self decodeCmapFmtTable: copy.
+			cmap := self decodeCmapFmtTable: copy.
 			"(pID = 1 and: [cmap notNil])" "Prefer Macintosh encoding over everything else"
 				"ifTrue: [pID -> cmap]."
-			assoc _ pID -> cmap. "Keep it in case we don't find a Mac encoded table"
+			assoc := pID -> cmap. "Keep it in case we don't find a Mac encoded table"
 		].
 	].
 	^assoc
