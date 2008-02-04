74ebff9f7e48550d39b17f39a66a897884a1b268
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/instance/as yet unclassified/emphasis-3A-.cs
+++ StrikeFontSet/instance/as yet unclassified/emphasis-3A-.cs
@@ -2,4 +2,4 @@ emphasis: code
 	"Set the integer code for synthetic bold, itallic, underline, and strike-out, 
 	where bold=1, italic=2, underlined=4, and struck out=8."
 
-	emphasis _ code.
+	emphasis := code.
