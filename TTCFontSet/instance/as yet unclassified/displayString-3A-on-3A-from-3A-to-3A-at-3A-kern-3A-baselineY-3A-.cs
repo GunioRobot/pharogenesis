36ca05193df7462d02bd9c22933e1501e7655a57
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontSet/instance/as yet unclassified/displayString-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-baselineY-3A-.cs
+++ TTCFontSet/instance/as yet unclassified/displayString-3A-on-3A-from-3A-to-3A-at-3A-kern-3A-baselineY-3A-.cs
@@ -1,21 +1,21 @@
 displayString: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint kern: kernDelta baselineY: baselineY
 
 	| destPoint font form encoding glyphInfo char charCode destY |
-	destPoint _ aPoint.
-	glyphInfo _ Array new: 5.
+	destPoint := aPoint.
+	glyphInfo := Array new: 5.
 	startIndex to: stopIndex do: [:charIndex |
-		char _ aString at: charIndex.
-		encoding _ char leadingChar + 1.
-		charCode _ char charCode.
-		font _ fontArray at: encoding.
+		char := aString at: charIndex.
+		encoding := char leadingChar + 1.
+		charCode := char charCode.
+		font := fontArray at: encoding.
 		((charCode between: font minAscii and: font maxAscii) not) ifTrue: [
-			charCode _ font maxAscii].
+			charCode := font maxAscii].
 		self glyphInfoOf: char into: glyphInfo.
-		form _ glyphInfo first.
+		form := glyphInfo first.
 		(glyphInfo fifth ~= aBitBlt lastFont) ifTrue: [
 			glyphInfo fifth installOn: aBitBlt.
 		].
-		destY _ baselineY - glyphInfo fourth. 
+		destY := baselineY - glyphInfo fourth. 
 		aBitBlt sourceForm: form.
 		aBitBlt destX: destPoint x.
 		aBitBlt destY: destY.
@@ -23,6 +23,6 @@ displayString: aString on: aBitBlt from: startIndex to: stopIndex at: aPoint ker
 		aBitBlt width: form width.
 		aBitBlt height: form height.
 		aBitBlt copyBits.
-		destPoint _ destPoint + (form width + kernDelta @ 0).
+		destPoint := destPoint + (form width + kernDelta @ 0).
 	].
 	^ destPoint.
