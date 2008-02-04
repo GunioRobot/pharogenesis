2bacf2e60dc7eeeab028752357674658f2d2be3e
sd <unknown>
2008-02-04 21:18:00 +0000

    no message

--- StrikeFontSet/instance/emphasis/makeUnderlinedGlyphs.cs
+++ StrikeFontSet/instance/emphasis/makeUnderlinedGlyphs.cs
@@ -3,9 +3,9 @@ makeUnderlinedGlyphs
 
 	| g font |
 	1 to: fontArray size do: [:i |
-		font _ (fontArray at: i).
+		font := (fontArray at: i).
 		font ifNotNil: [
-			g _ font glyphs deepCopy.
+			g := font glyphs deepCopy.
 			g fillBlack: (0 @ (font ascent+1) extent: g width @ 1).
 			font setGlyphs: g
 		].
