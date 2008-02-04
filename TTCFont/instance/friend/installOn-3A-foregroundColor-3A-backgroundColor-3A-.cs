becc57d0eb0876cccc0baecf7f2cb427077655dc
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/instance/friend/installOn-3A-foregroundColor-3A-backgroundColor-3A-.cs
+++ TTCFont/instance/friend/installOn-3A-foregroundColor-3A-backgroundColor-3A-.cs
@@ -1,5 +1,5 @@
 installOn: aDisplayContext foregroundColor: fgColor backgroundColor: bgColor
 
-	foregroundColor _ fgColor.
+	foregroundColor := fgColor.
 
 	aDisplayContext installTTCFont: self foregroundColor: foregroundColor backgroundColor: bgColor
