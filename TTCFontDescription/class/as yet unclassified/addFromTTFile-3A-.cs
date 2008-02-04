49f4fa0eacabeec845bdb38fcbcb4a9eb107bbd6
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontDescription/class/as yet unclassified/addFromTTFile-3A-.cs
+++ TTCFontDescription/class/as yet unclassified/addFromTTFile-3A-.cs
@@ -6,12 +6,12 @@ addFromTTFile: fileName
 
 	| tt old |
 	(fileName asLowercase endsWith: 'ttf') ifTrue: [
-		tt _ TTCFontReader readTTFFrom: (FileStream readOnlyFileNamed: fileName).
+		tt := TTCFontReader readTTFFrom: (FileStream readOnlyFileNamed: fileName).
 	] ifFalse: [
-		tt _ TTCFontReader readFrom: (FileStream readOnlyFileNamed: fileName).
+		tt := TTCFontReader readFrom: (FileStream readOnlyFileNamed: fileName).
 	].
 		
-	old _ TTCDescriptions detect: [:f | f first name = tt first name] ifNone: [nil].
+	old := TTCDescriptions detect: [:f | f first name = tt first name] ifNone: [nil].
 	old ifNotNil: [TTCDescriptions remove: old].
 	TTCDescriptions add: tt.
 	^ tt.
