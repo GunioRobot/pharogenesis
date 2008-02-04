d1dc30cd0579e9ea41bbe64705e7b28c0ed86ea6
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiCharacterScanner/class/class initialization/initialize.cs
+++ MultiCharacterScanner/class/class initialization/initialize.cs
@@ -3,17 +3,17 @@ initialize
 	MultiCharacterScanner initialize
 "
 	| a |
-	a _ Array new: 258.
+	a := Array new: 258.
 	a at: 1 + 1 put: #embeddedObject.
 	a at: Tab asciiValue + 1 put: #tab.
 	a at: CR asciiValue + 1 put: #cr.
 	a at: EndOfRun put: #endOfRun.
 	a at: CrossedX put: #crossedX.
-	NilCondition _ a copy.
-	DefaultStopConditions _ a copy.
+	NilCondition := a copy.
+	DefaultStopConditions := a copy.
 
-	PaddedSpaceCondition _ a copy.
+	PaddedSpaceCondition := a copy.
 	PaddedSpaceCondition at: Space asciiValue + 1 put: #paddedSpace.
 	
-	SpaceCondition _ a copy.
+	SpaceCondition := a copy.
 	SpaceCondition at: Space asciiValue + 1 put: #space.
