a5f01e882b846e92474e9e9f40507dc7e14bb572
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontReader/instance/as yet unclassified/parseTTCHeaderFrom-3A-.cs
+++ TTCFontReader/instance/as yet unclassified/parseTTCHeaderFrom-3A-.cs
@@ -1,12 +1,12 @@
 parseTTCHeaderFrom: fontData
 
 	| pos nTables |
-	nTables _ fontData longAt: 9 bigEndian: true.
-	fonts _ Array new: nTables.
-	pos _ 13.
+	nTables := fontData longAt: 9 bigEndian: true.
+	fonts := Array new: nTables.
+	pos := 13.
 	1 to: nTables do: [:i |
 		fonts at: i put: (fontData longAt: pos bigEndian: true).
-		pos _ pos + 4.
+		pos := pos + 4.
 	].
 
 	^ fonts
