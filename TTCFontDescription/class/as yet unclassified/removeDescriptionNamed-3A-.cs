7ebaadfd61362f559eda6e3c7bed3c1ae528aa7b
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontDescription/class/as yet unclassified/removeDescriptionNamed-3A-.cs
+++ TTCFontDescription/class/as yet unclassified/removeDescriptionNamed-3A-.cs
@@ -2,6 +2,6 @@ removeDescriptionNamed: descriptionName
 
 	| tt |
 	TTCDescriptions ifNil: [^ self].
-	[(tt _ TTCDescriptions detect: [:f | ('Multi', f first name) = descriptionName] ifNone: [nil]) notNil] whileTrue:[
+	[(tt := TTCDescriptions detect: [:f | ('Multi', f first name) = descriptionName] ifNone: [nil]) notNil] whileTrue:[
 		 TTCDescriptions remove: tt
 	].
