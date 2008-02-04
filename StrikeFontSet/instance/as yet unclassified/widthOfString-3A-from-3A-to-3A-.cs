43b4cfad75b78522359bdd72de46512c6a3f0f6b
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/instance/as yet unclassified/widthOfString-3A-from-3A-to-3A-.cs
+++ StrikeFontSet/instance/as yet unclassified/widthOfString-3A-from-3A-to-3A-.cs
@@ -2,7 +2,7 @@ widthOfString: aString from: startIndex to: stopIndex
 	"Measure the length of the given string between start and stop index"
 
 	| resultX |
-	resultX _ 0.
+	resultX := 0.
 	startIndex to: stopIndex do:[:i | 
-		resultX _ resultX + (self widthOf: (aString at: i))].
+		resultX := resultX + (self widthOf: (aString at: i))].
 	^ resultX.
