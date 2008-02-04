a0c0a94a05c02f835af4a72f1623726d646b8a3b
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/instance/friend/derivativeFont-3A-.cs
+++ TTCFont/instance/friend/derivativeFont-3A-.cs
@@ -1,7 +1,7 @@
 derivativeFont: aTTCFont
 
 	| index |
-	index _ self indexOfSubfamilyName: (aTTCFont subfamilyName).
+	index := self indexOfSubfamilyName: (aTTCFont subfamilyName).
 	index < 1 ifTrue: [
 		^ self "inform: 'unknown sub family name.  This font will be skipped'".
 	].
