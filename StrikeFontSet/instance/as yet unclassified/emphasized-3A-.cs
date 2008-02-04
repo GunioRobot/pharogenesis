0d5d226356ac3ce6cb31148af7138bdad822bbd3
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/instance/as yet unclassified/emphasized-3A-.cs
+++ StrikeFontSet/instance/as yet unclassified/emphasized-3A-.cs
@@ -4,22 +4,22 @@ emphasized: code
 	| derivative addedEmphasis base safeCode |
 	code = 0 ifTrue: [^ self].
 	(derivativeFonts == nil or: [derivativeFonts size = 0]) ifTrue: [^ self].
-	derivative _ derivativeFonts at: (safeCode _ code min: derivativeFonts size).
+	derivative := derivativeFonts at: (safeCode := code min: derivativeFonts size).
 	derivative == nil ifFalse: [^ derivative].  "Already have this style"
 
 	"Dont have it -- derive from another with one with less emphasis"
-	addedEmphasis _ 1 bitShift: safeCode highBit - 1.
-	base _ self emphasized: safeCode - addedEmphasis.  "Order is Bold, Ital, Under, Narrow"
+	addedEmphasis := 1 bitShift: safeCode highBit - 1.
+	base := self emphasized: safeCode - addedEmphasis.  "Order is Bold, Ital, Under, Narrow"
 	addedEmphasis = 1 ifTrue:   "Compute synthetic bold version of the font"
-		[derivative _ (base copy name: base name , 'B') makeBoldGlyphs].
+		[derivative := (base copy name: base name , 'B') makeBoldGlyphs].
 	addedEmphasis = 2 ifTrue:   "Compute synthetic italic version of the font"
-		[ derivative _ (base copy name: base name , 'I') makeItalicGlyphs].
+		[ derivative := (base copy name: base name , 'I') makeItalicGlyphs].
 	addedEmphasis = 4 ifTrue:   "Compute underlined version of the font"
-		[derivative _ (base copy name: base name , 'U') makeUnderlinedGlyphs].
+		[derivative := (base copy name: base name , 'U') makeUnderlinedGlyphs].
 	addedEmphasis = 8 ifTrue:   "Compute narrow version of the font"
-		[derivative _ (base copy name: base name , 'N') makeCondensedGlyphs].
+		[derivative := (base copy name: base name , 'N') makeCondensedGlyphs].
 	addedEmphasis = 16 ifTrue:   "Compute struck-out version of the font"
-		[derivative _ (base copy name: base name , 'X') makeStruckOutGlyphs].
+		[derivative := (base copy name: base name , 'X') makeStruckOutGlyphs].
 	derivative emphasis: safeCode.
 	derivativeFonts at: safeCode put: derivative.
 	^ derivative
