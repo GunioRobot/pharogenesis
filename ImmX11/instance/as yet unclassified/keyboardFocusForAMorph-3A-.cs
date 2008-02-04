c4eae0d65bbf5911eda72f91d9b53a398e6585bf
sd <unknown>
2008-02-04 21:21:00 +0000

    no message

--- ImmX11/instance/as yet unclassified/keyboardFocusForAMorph-3A-.cs
+++ ImmX11/instance/as yet unclassified/keyboardFocusForAMorph-3A-.cs
@@ -3,9 +3,9 @@ keyboardFocusForAMorph: aMorph
 	| left bottom pos |
 	aMorph ifNil: [^ self].
 	[
-		pos _ aMorph preferredKeyboardPosition.
-		left _ (pos x min: Display width max: 0) asInteger.
-		bottom _ (pos y min: Display height max: 0) asInteger
+		pos := aMorph preferredKeyboardPosition.
+		left := (pos x min: Display width max: 0) asInteger.
+		bottom := (pos y min: Display height max: 0) asInteger
 			 + (aMorph paragraph
 				characterBlockForIndex: aMorph editor selectionInterval first) height.
 		self setCompositionWindowPositionX: left y: bottom
