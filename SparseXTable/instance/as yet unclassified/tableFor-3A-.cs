97bcf1c2013976c38a94fbe7faa405985a8fa2b3
sd <unknown>
2008-02-04 21:12:00 +0000

    no message

--- SparseXTable/instance/as yet unclassified/tableFor-3A-.cs
+++ SparseXTable/instance/as yet unclassified/tableFor-3A-.cs
@@ -1,6 +1,6 @@
 tableFor: code
 
 	| div t table |
-	div _ code // 65536.
-	t _ xTables at: div ifAbsent: [table _ Array new: 65536 withAll: 0. xTables at: div put: table. table].
+	div := code // 65536.
+	t := xTables at: div ifAbsent: [table := Array new: 65536 withAll: 0. xTables at: div put: table. table].
 	^ t.
