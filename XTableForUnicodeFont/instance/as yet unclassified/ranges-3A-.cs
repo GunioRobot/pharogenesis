157f5d48749d6745f81bf8e1317bc802d7032b85
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- XTableForUnicodeFont/instance/as yet unclassified/ranges-3A-.cs
+++ XTableForUnicodeFont/instance/as yet unclassified/ranges-3A-.cs
@@ -1,7 +1,7 @@
 ranges: pairArray
 
-	xTables _ Array new: 0.
+	xTables := Array new: 0.
 	pairArray do: [:range |
-		xTables _ xTables copyWith: (Array new: range last - range first + 1 withAll: 0).
+		xTables := xTables copyWith: (Array new: range last - range first + 1 withAll: 0).
 	].
-	ranges _ pairArray.
+	ranges := pairArray.
