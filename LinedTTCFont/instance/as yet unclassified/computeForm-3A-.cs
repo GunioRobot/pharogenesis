7863f0c05ad753a052be5a69f2271a03a684547a
sd <unknown>
2008-02-04 21:11:00 +0000

    no message

--- LinedTTCFont/instance/as yet unclassified/computeForm-3A-.cs
+++ LinedTTCFont/instance/as yet unclassified/computeForm-3A-.cs
@@ -5,6 +5,6 @@ computeForm: char
 	char = Character tab ifTrue: [^ super computeForm: char].
 
 	"char = $U ifTrue: [self doOnlyOnce: [self halt]]."
-	scale _ self pixelSize asFloat / (ttcDescription ascender - ttcDescription descender).
-	ttGlyph _ ttcDescription at: char.
+	scale := self pixelSize asFloat / (ttcDescription ascender - ttcDescription descender).
+	ttGlyph := ttcDescription at: char.
 	^ ttGlyph asFormWithScale: scale ascender: ttcDescription ascender descender: ttcDescription descender fgColor: foregroundColor bgColor: Color transparent depth: self depth replaceColor: false lineGlyph: lineGlyph lingGlyphWidth: contourWidth emphasis: emphasis
