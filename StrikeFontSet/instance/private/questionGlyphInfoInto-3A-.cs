47460ee4cf1c46259170a51d40081699ddacd692
sd <unknown>
2008-02-04 21:14:00 +0000

    no message

--- StrikeFontSet/instance/private/questionGlyphInfoInto-3A-.cs
+++ StrikeFontSet/instance/private/questionGlyphInfoInto-3A-.cs
@@ -1,8 +1,8 @@
 questionGlyphInfoInto: glyphInfoArray
 
 	| f ascii |
-	f _ fontArray at: 1.
-	ascii _ $? asciiValue.
+	f := fontArray at: 1.
+	ascii := $? asciiValue.
 	glyphInfoArray at: 1 put: f glyphs;
 		at: 2 put: (f xTable at: ascii + 1);
 		at: 3 put: (f xTable at: ascii + 2);
