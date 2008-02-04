f5ac878059f405d4e308a70e76374c400c1855d6
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/instance/as yet unclassified/setupDefaultFallbackFont.cs
+++ TTCFont/instance/as yet unclassified/setupDefaultFallbackFont.cs
@@ -1,10 +1,10 @@
 setupDefaultFallbackFont
 
 	| fonts f |
-	fonts _ TextStyle default fontArray.
-	f _ fonts first.
+	fonts := TextStyle default fontArray.
+	f := fonts first.
 	1 to: fonts size do: [:i |
-		self height > (fonts at: i) height ifTrue: [f _ fonts at: i].
+		self height > (fonts at: i) height ifTrue: [f := fonts at: i].
 	].
 	self fallbackFont: f.
 	self reset.
