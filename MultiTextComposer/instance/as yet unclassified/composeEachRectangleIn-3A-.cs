5ad6ef75a65e18878ce77bab2ed7b498b916ca06
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiTextComposer/instance/as yet unclassified/composeEachRectangleIn-3A-.cs
+++ MultiTextComposer/instance/as yet unclassified/composeEachRectangleIn-3A-.cs
@@ -4,7 +4,7 @@ composeEachRectangleIn: rectangles
 
 	1 to: rectangles size do: [:i | 
 		currCharIndex <= theText size ifFalse: [^false].
-		myLine _ scanner 
+		myLine := scanner 
 			composeFrom: currCharIndex 
 			inRectangle: (rectangles at: i)				
 			firstLine: isFirstLine 
@@ -12,11 +12,11 @@ composeEachRectangleIn: rectangles
 			rightSide: i=rectangles size.
 		lines addLast: myLine.
 		presentationLines addLast: scanner getPresentationLine.
-		presentation ifNil: [presentation _ scanner getPresentation]
-			ifNotNil: [presentation _ presentation, scanner getPresentation].
-		actualHeight _ actualHeight max: myLine lineHeight.  "includes font changes"
-		currCharIndex _ myLine last + 1.
-		lastChar _ theText at: myLine last.
+		presentation ifNil: [presentation := scanner getPresentation]
+			ifNotNil: [presentation := presentation, scanner getPresentation].
+		actualHeight := actualHeight max: myLine lineHeight.  "includes font changes"
+		currCharIndex := myLine last + 1.
+		lastChar := theText at: myLine last.
 		lastChar = Character cr ifTrue: [^#cr].
 		wantsColumnBreaks ifTrue: [
 			lastChar = TextComposer characterForColumnBreak ifTrue: [^#columnBreak].
