4b5bbf7138d58f08834b606bb29c15443736615e
sd <unknown>
2008-02-04 21:13:00 +0000

    no message

--- StrikeFontFixer/instance/as yet unclassified/displayOn-3A-at-3A-magnifyBy-3A-.cs
+++ StrikeFontFixer/instance/as yet unclassified/displayOn-3A-at-3A-magnifyBy-3A-.cs
@@ -1,15 +1,15 @@
 displayOn: aDisplayObject at: aPoint magnifyBy: aNumber
 
 	| form hStep vStep bb source nextPoint |
-	hStep _ (strikeFont maxWidth * aNumber * 1.2) asInteger.
-	vStep _ (strikeFont height * aNumber *  1.2) asInteger.
+	hStep := (strikeFont maxWidth * aNumber * 1.2) asInteger.
+	vStep := (strikeFont height * aNumber *  1.2) asInteger.
 	
-	form _ Form extent: (hStep * 16)@(vStep * 16).
-	bb _ BitBlt toForm: form.
+	form := Form extent: (hStep * 16)@(vStep * 16).
+	bb := BitBlt toForm: form.
 	0 to: 15 do: [:i |
 		1 to: 16 do: [:j |
-			source _ ((charForms at: (i * 16 + j)) magnifyBy: aNumber).
-			nextPoint _ (hStep * (j - 1)@(vStep * i)).
+			source := ((charForms at: (i * 16 + j)) magnifyBy: aNumber).
+			nextPoint := (hStep * (j - 1)@(vStep * i)).
 			bb copy: ((nextPoint+((hStep@vStep - source extent) // 2)) extent: source extent)
 				from: 0@0 in: source fillColor: Color black rule: Form over.
 		].
