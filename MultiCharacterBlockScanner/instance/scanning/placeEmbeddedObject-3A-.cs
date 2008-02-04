06c164135784a997a1f374efe25f6fdcc7660a4d
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiCharacterBlockScanner/instance/scanning/placeEmbeddedObject-3A-.cs
+++ MultiCharacterBlockScanner/instance/scanning/placeEmbeddedObject-3A-.cs
@@ -2,5 +2,5 @@ placeEmbeddedObject: anchoredMorph
 	"Workaround: The following should really use #textAnchorType"
 	anchoredMorph relativeTextAnchorPosition ifNotNil:[^true].
 	(super placeEmbeddedObject: anchoredMorph) ifFalse: [^ false].
-	specialWidth _ anchoredMorph width.
+	specialWidth := anchoredMorph width.
 	^ true
