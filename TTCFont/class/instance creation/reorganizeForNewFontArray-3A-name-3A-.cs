0991f33d59988bfdcfa9ebc4921b6b5871789404
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/class/instance creation/reorganizeForNewFontArray-3A-name-3A-.cs
+++ TTCFont/class/instance creation/reorganizeForNewFontArray-3A-name-3A-.cs
@@ -7,14 +7,14 @@ reorganizeForNewFontArray: array name: styleName
 	].
  
 	"There is a text style with the name I want to use.  See if it is a TTC font..."
-	style _ TextConstants at: styleName.
+	style := TextConstants at: styleName.
 	style isTTCStyle ifFalse: [
-		altName _ ((array at: 1) name, 'TT') asSymbol.
+		altName := ((array at: 1) name, 'TT') asSymbol.
 		^ self reorganizeForNewFontArray: array name: altName.
 	].
 
-	existings _ (self getExistings: style fontArray), (Array with: array).
-	regular _ existings detect: [:e | (e at: 1) isRegular] ifNone: [existings at: 1].
+	existings := (self getExistings: style fontArray), (Array with: array).
+	regular := existings detect: [:e | (e at: 1) isRegular] ifNone: [existings at: 1].
 
 	regular do: [:r |
 		r addLined: r.
@@ -24,7 +24,7 @@ reorganizeForNewFontArray: array name: styleName
 	existings do: [:e |
 		(e at: 1) isRegular ifFalse: [
 			regular do: [ :r | | f |
-				f _ e detect: [ :ea | ea pointSize = r pointSize ] ifNone: [ ].
+				f := e detect: [ :ea | ea pointSize = r pointSize ] ifNone: [ ].
 				f ifNotNil: [ r derivativeFont: f ].
 			].
 		].
