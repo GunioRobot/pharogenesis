a77f8699d15cffd20fa9a1ba3854848a1639a4e5
sd <unknown>
2008-02-04 21:09:00 +0000

    no message

--- EFontBDFFontReaderForRanges/instance/as yet unclassified/readRanges-3A-.cs
+++ EFontBDFFontReaderForRanges/instance/as yet unclassified/readRanges-3A-.cs
@@ -1,65 +1,65 @@
 readRanges: ranges
 
 	| xTable strikeWidth glyphs ascent descent minAscii maxAscii maxWidth chars charsNum height form encoding bbx width blt lastAscii pointSize ret lastValue start end |
-	form _ encoding _ bbx _ nil.
+	form := encoding := bbx := nil.
 	self initialize.
 	self readAttributes.
-	height _ Integer readFromString: ((properties at: #FONTBOUNDINGBOX) at: 2).
-	ascent _ Integer readFromString: (properties at: 'FONT_ASCENT' asSymbol) first.
-	descent _ Integer readFromString: (properties at: 'FONT_DESCENT' asSymbol) first.
+	height := Integer readFromString: ((properties at: #FONTBOUNDINGBOX) at: 2).
+	ascent := Integer readFromString: (properties at: 'FONT_ASCENT' asSymbol) first.
+	descent := Integer readFromString: (properties at: 'FONT_DESCENT' asSymbol) first.
 	(properties includesKey: 'POINT_SIZE' asSymbol) ifTrue: [
-		pointSize _ (Integer readFromString: (properties at: 'POINT_SIZE' asSymbol) first) // 10.
+		pointSize := (Integer readFromString: (properties at: 'POINT_SIZE' asSymbol) first) // 10.
 	] ifFalse: [
-		pointSize _ (ascent + descent) * 72 // 96.
+		pointSize := (ascent + descent) * 72 // 96.
 	].
 
-	maxWidth _ 0.
-	minAscii _ 16r200000.
-	strikeWidth _ 0.
-	maxAscii _ 0.
+	maxWidth := 0.
+	minAscii := 16r200000.
+	strikeWidth := 0.
+	maxAscii := 0.
 
-	charsNum _ Integer readFromString: (properties at: #CHARS) first.
-	chars _ Set new: charsNum.
+	charsNum := Integer readFromString: (properties at: #CHARS) first.
+	chars := Set new: charsNum.
 
 	self readCharactersInRanges: ranges storeInto: chars.
 
-	chars _ chars asSortedCollection: [:x :y | (x at: 2) <= (y at: 2)].
-	charsNum _ chars size. "undefined encodings make this different"
+	chars := chars asSortedCollection: [:x :y | (x at: 2) <= (y at: 2)].
+	charsNum := chars size. "undefined encodings make this different"
 
 	chars do: [:array |
-		encoding _ array at: 2.
-		bbx _ array at: 3..
-		width _ bbx at: 1.
-		maxWidth _ maxWidth max: width.
-		minAscii _ minAscii min: encoding.
-		maxAscii _ maxAscii max: encoding.
-		strikeWidth _ strikeWidth + width.
+		encoding := array at: 2.
+		bbx := array at: 3..
+		width := bbx at: 1.
+		maxWidth := maxWidth max: width.
+		minAscii := minAscii min: encoding.
+		maxAscii := maxAscii max: encoding.
+		strikeWidth := strikeWidth + width.
 	].
 
-	glyphs _ Form extent: strikeWidth@height.
-	blt _ BitBlt toForm: glyphs.
-	start _ (ranges collect: [:r | r first]) min.
-	end _ (ranges collect: [:r | r second]) max + 3.
+	glyphs := Form extent: strikeWidth@height.
+	blt := BitBlt toForm: glyphs.
+	start := (ranges collect: [:r | r first]) min.
+	end := (ranges collect: [:r | r second]) max + 3.
 
-	xTable _ SparseLargeTable new: end chunkSize: 64 arrayClass: Array base: start +1 defaultValue: -1.
-	lastAscii _ start.
+	xTable := SparseLargeTable new: end chunkSize: 64 arrayClass: Array base: start +1 defaultValue: -1.
+	lastAscii := start.
 	xTable at: lastAscii + 2 put: 0.
 	1 to: charsNum do: [:i |
-		form _ (chars at: i) first.
-		encoding _ (chars at: i) second.
-		bbx _ (chars at: i) third.
+		form := (chars at: i) first.
+		encoding := (chars at: i) second.
+		bbx := (chars at: i) third.
 		"lastAscii+1 to: encoding-1 do: [:a | xTable at: a+2 put: (xTable at: a+1)]."
-		lastValue _ xTable at: lastAscii + 1 + 1.
+		lastValue := xTable at: lastAscii + 1 + 1.
 		xTable at: encoding + 1 put: lastValue.
 		blt copy: (((xTable at: encoding+1)@(ascent - (bbx at: 2) - (bbx at: 4)))
 				extent: (bbx at: 1)@(bbx at: 2))
 			from: 0@0 in: form.
 		xTable at: encoding+2 put: (xTable at: encoding+1)+(bbx at: 1).
-		lastAscii _ encoding.
+		lastAscii := encoding.
 	].
 	xTable at: xTable size put: (xTable at: xTable size - 1).
 	xTable zapDefaultOnlyEntries.
-	ret _ Array new: 8.
+	ret := Array new: 8.
 	ret at: 1 put: xTable.
 	ret at: 2 put: glyphs.
 	ret at: 3 put: minAscii.
