819c322738d0d446bf5eed6be3ba587ebb4ffe7e
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiCharacterScanner/instance/scanner methods/scanMultiCharactersR2LFrom-3A-to-3A-in-3A-rightX-3A-stopConditions-3A-kern-3A-.cs
+++ MultiCharacterScanner/instance/scanner methods/scanMultiCharactersR2LFrom-3A-to-3A-in-3A-rightX-3A-stopConditions-3A-kern-3A-.cs
@@ -2,33 +2,33 @@ scanMultiCharactersR2LFrom: startIndex to: stopIndex in: sourceString rightX: ri
 
 	"Note that 'rightX' really means 'endX' in R2L context.  Ie.  rightX is usually smaller than destX."
 	| ascii encoding f nextDestX maxAscii startEncoding |
-	lastIndex _ startIndex.
-	lastIndex > stopIndex ifTrue: [lastIndex _ stopIndex. ^ stops at: EndOfRun].
-	startEncoding _ (sourceString at: startIndex) leadingChar.
-	font ifNil: [font _ (TextConstants at: #DefaultMultiStyle) fontArray at: 1].
+	lastIndex := startIndex.
+	lastIndex > stopIndex ifTrue: [lastIndex := stopIndex. ^ stops at: EndOfRun].
+	startEncoding := (sourceString at: startIndex) leadingChar.
+	font ifNil: [font := (TextConstants at: #DefaultMultiStyle) fontArray at: 1].
 	((font isMemberOf: StrikeFontSet) or: [font isKindOf: TTCFontSet]) ifTrue: [
-		[f _ font fontArray at: startEncoding + 1]
-			on: Exception do: [:ex | f _ font fontArray at: 1].
-		f ifNil: [ f _ font fontArray at: 1].
-		maxAscii _ f maxAscii.
-		spaceWidth _ f widthOf: Space.
+		[f := font fontArray at: startEncoding + 1]
+			on: Exception do: [:ex | f := font fontArray at: 1].
+		f ifNil: [ f := font fontArray at: 1].
+		maxAscii := f maxAscii.
+		spaceWidth := f widthOf: Space.
 	] ifFalse: [
-		maxAscii _ font maxAscii.
+		maxAscii := font maxAscii.
 	].
 
 	[lastIndex <= stopIndex] whileTrue: [
-		encoding _ (sourceString at: lastIndex) leadingChar.
-		encoding ~= startEncoding ifTrue: [lastIndex _ lastIndex - 1. ^ stops at: EndOfRun].
-		ascii _ (sourceString at: lastIndex) charCode.
-		ascii > maxAscii ifTrue: [ascii _ maxAscii].
+		encoding := (sourceString at: lastIndex) leadingChar.
+		encoding ~= startEncoding ifTrue: [lastIndex := lastIndex - 1. ^ stops at: EndOfRun].
+		ascii := (sourceString at: lastIndex) charCode.
+		ascii > maxAscii ifTrue: [ascii := maxAscii].
 		(encoding = 0 and: [(stopConditions at: ascii + 1) ~~ nil]) ifTrue: [^ stops at: ascii + 1].
 		(self isBreakableAt: lastIndex in: sourceString in: Latin1Environment) ifTrue: [
 			self registerBreakableIndex.
 		].
-		nextDestX _ destX - (font widthOf: (sourceString at: lastIndex)).
+		nextDestX := destX - (font widthOf: (sourceString at: lastIndex)).
 		nextDestX < rightX ifTrue: [^ stops at: CrossedX].
-		destX _ nextDestX - kernDelta.
-		lastIndex _ lastIndex + 1.
+		destX := nextDestX - kernDelta.
+		lastIndex := lastIndex + 1.
 	].
-	lastIndex _ stopIndex.
+	lastIndex := stopIndex.
 	^ stops at: EndOfRun
