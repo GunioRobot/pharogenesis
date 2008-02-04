61aa6205d8b8bb409578c153232b2999ac92d5ce
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- MultiCharacterScanner/instance/private/setFont.cs
+++ MultiCharacterScanner/instance/private/setFont.cs
@@ -1,26 +1,26 @@
 setFont
 	| priorFont |
 	"Set the font and other emphasis."
-	priorFont _ font.
+	priorFont := font.
 	text == nil ifFalse:[
-		emphasisCode _ 0.
-		kern _ 0.
-		indentationLevel _ 0.
-		alignment _ textStyle alignment.
-		font _ nil.
+		emphasisCode := 0.
+		kern := 0.
+		indentationLevel := 0.
+		alignment := textStyle alignment.
+		font := nil.
 		(text attributesAt: lastIndex forStyle: textStyle)
 			do: [:att | att emphasizeScanner: self]].
 	font == nil ifTrue:
 		[self setFont: textStyle defaultFontIndex].
-	font _ font emphasized: emphasisCode.
-	priorFont ifNotNil: [destX _ destX + priorFont descentKern].
-	destX _ destX - font descentKern.
+	font := font emphasized: emphasisCode.
+	priorFont ifNotNil: [destX := destX + priorFont descentKern].
+	destX := destX - font descentKern.
 	"NOTE: next statement should be removed when clipping works"
-	leftMargin ifNotNil: [destX _ destX max: leftMargin].
-	kern _ kern - font baseKern.
+	leftMargin ifNotNil: [destX := destX max: leftMargin].
+	kern := kern - font baseKern.
 
 	"Install various parameters from the font."
-	spaceWidth _ font widthOf: Space.
-	xTable _ font xTable.
-"	map _ font characterToGlyphMap."
-	stopConditions _ DefaultStopConditions.
+	spaceWidth := font widthOf: Space.
+	xTable := font xTable.
+"	map := font characterToGlyphMap."
+	stopConditions := DefaultStopConditions.
