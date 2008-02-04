2d83cf0d3f02f9a6c49ba3cb061a529ba99209ab
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/class/as yet unclassified/duplicateArrayElementsForLeadingCharShift.cs
+++ StrikeFontSet/class/as yet unclassified/duplicateArrayElementsForLeadingCharShift.cs
@@ -5,9 +5,9 @@ duplicateArrayElementsForLeadingCharShift
 	| array font |
 	self allInstances do: [:s |
 		s emphasis = 0 ifTrue: [
-			array _ s fontArray.
+			array := s fontArray.
 			2 to: (4 min: array size) do: [:i |
-				font _ array at: i.
+				font := array at: i.
 				s addNewFont: font at: ((i - 1) << 2) + 1.
 			].
 		] ifFalse: [
