bdf50c82c4bdaeef56f1bf86514608b95ab3e484
sd <unknown>
2008-02-04 21:22:00 +0000

    no message

--- CombinedChar/class/as yet unclassified/parseCompositionMappingFrom-3A-.cs
+++ CombinedChar/class/as yet unclassified/parseCompositionMappingFrom-3A-.cs
@@ -6,26 +6,26 @@ parseCompositionMappingFrom: stream
 
 	| line fieldEnd point fieldStart compositions toNumber diacritical result |
 
-	toNumber _ [:quad | ('16r', quad) asNumber].
+	toNumber := [:quad | ('16r', quad) asNumber].
 
-	Compositions _ IdentityDictionary new: 2048.
-	Decompositions _ IdentityDictionary new: 2048.
-	Diacriticals _ IdentitySet new: 2048.
+	Compositions := IdentityDictionary new: 2048.
+	Decompositions := IdentityDictionary new: 2048.
+	Diacriticals := IdentitySet new: 2048.
 
-	[(line _ stream upTo: Character cr) size > 0] whileTrue: [
-		fieldEnd _ line indexOf: $; startingAt: 1.
-		point _ ('16r', (line copyFrom: 1 to: fieldEnd - 1)) asNumber.
+	[(line := stream upTo: Character cr) size > 0] whileTrue: [
+		fieldEnd := line indexOf: $; startingAt: 1.
+		point := ('16r', (line copyFrom: 1 to: fieldEnd - 1)) asNumber.
 		2 to: 6 do: [:i |
-			fieldStart _ fieldEnd + 1.
-			fieldEnd _ line indexOf: $; startingAt: fieldStart.
+			fieldStart := fieldEnd + 1.
+			fieldEnd := line indexOf: $; startingAt: fieldStart.
 		].
-		compositions _ line copyFrom: fieldStart to: fieldEnd - 1.
+		compositions := line copyFrom: fieldStart to: fieldEnd - 1.
 		(compositions size > 0 and: [compositions first ~= $<]) ifTrue: [
-			compositions _ compositions substrings collect: toNumber.
+			compositions := compositions substrings collect: toNumber.
 			compositions size > 1 ifTrue: [
-				diacritical _ compositions first.
+				diacritical := compositions first.
 				Diacriticals add: diacritical.
-				result _ compositions second.
+				result := compositions second.
 				(Decompositions includesKey: point) ifTrue: [
 					self error: 'should not happen'.
 				] ifFalse: [
