fa270c3acd6074bd539a0f22bbf5d6697ac45344
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFontSet/class/as yet unclassified/removeStyleName-3A-.cs
+++ TTCFontSet/class/as yet unclassified/removeStyleName-3A-.cs
@@ -1,7 +1,7 @@
 removeStyleName: aString
 
 	| style symName |
-	symName _ aString asSymbol.
-	style _ TextConstants removeKey: symName ifAbsent: [].
+	symName := aString asSymbol.
+	style := TextConstants removeKey: symName ifAbsent: [].
 	style ifNotNil: [self unregister: symName].
 	TTCFontDescription removeDescriptionNamed: aString asString.
