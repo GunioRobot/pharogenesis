e3465e7374f58c3eb19c602c2228875a9aafe6cd
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/class/other/repairBadSizes.cs
+++ TTCFont/class/other/repairBadSizes.cs
@@ -8,16 +8,16 @@ repairBadSizes
 
 	"TTCFont repairBadSizes"
 	| description computedScale cached desiredScale newPointSize repaired |
-	repaired _ OrderedCollection new.
+	repaired := OrderedCollection new.
 	TTCFont allInstancesDo: [ :font |
 		cached := (font cache copyFrom: $A asciiValue + 1 to: $z asciiValue + 1)
 			detect: [ :f | f notNil ] ifNone: [].
 		cached := cached ifNil: [  font formOf: $A ] ifNotNil: [ cached value ].
-		description _ font ttcDescription.
-		desiredScale _ cached height asFloat / (description ascender - description descender).
-		computedScale _ font pixelSize asFloat / font ttcDescription unitsPerEm.
+		description := font ttcDescription.
+		desiredScale := cached height asFloat / (description ascender - description descender).
+		computedScale := font pixelSize asFloat / font ttcDescription unitsPerEm.
 		(((computedScale / desiredScale) - 1.0 * cached height) abs < 1.0) ifFalse: [
-			newPointSize _ (font pointSize * desiredScale / computedScale) rounded.
+			newPointSize := (font pointSize * desiredScale / computedScale) rounded.
 			font pointSize: newPointSize; flushCache.
 			repaired add: font.
 			font derivativeFonts do: [ :df | df ifNotNil: [
