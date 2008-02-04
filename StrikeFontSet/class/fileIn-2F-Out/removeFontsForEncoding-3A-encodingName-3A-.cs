6dc1972c4dd81f0196ae0c36e44a4becd1a106eb
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- StrikeFontSet/class/fileIn-2F-Out/removeFontsForEncoding-3A-encodingName-3A-.cs
+++ StrikeFontSet/class/fileIn-2F-Out/removeFontsForEncoding-3A-encodingName-3A-.cs
@@ -2,14 +2,14 @@ removeFontsForEncoding: leadingChar encodingName: encodingSymbol
 
 	| insts fonts newFonts index |
 	leadingChar = 0 ifTrue: [^ self error: 'you cannot delete the intrinsic fonts'].
-	insts _ self allInstances.
+	insts := self allInstances.
 	insts do: [:inst |
-		fonts _ inst fontArray.
+		fonts := inst fontArray.
 		fonts size >= (leadingChar + 1) ifTrue: [
 			leadingChar + 1 = fonts size ifTrue: [
-				newFonts _ fonts copyFrom: 1 to: fonts size - 1.
-				index _ newFonts indexOf: nil.
-				index > 0 ifTrue: [newFonts _ newFonts copyFrom: 1 to: index - 1].
+				newFonts := fonts copyFrom: 1 to: fonts size - 1.
+				index := newFonts indexOf: nil.
+				index > 0 ifTrue: [newFonts := newFonts copyFrom: 1 to: index - 1].
 				inst initializeWithFontArray: newFonts.
 			] ifFalse: [
 				fonts at: leadingChar + 1 put: nil.
