d78682a38604385c46d73a097f86ea3cc8425a0d
sd <unknown>
2008-02-04 21:20:00 +0000

    no message

--- TTCFont/class/instance creation/getExistings-3A-.cs
+++ TTCFont/class/instance creation/getExistings-3A-.cs
@@ -1,10 +1,10 @@
 getExistings: fontArray
 
 	| result em |
-	result _ OrderedCollection new.
+	result := OrderedCollection new.
 	result add: fontArray.
 	1 to: 3 do: [:i |
-		em _ (fontArray collect: [:f | f emphasized: i]).
+		em := (fontArray collect: [:f | f emphasized: i]).
 		(em at: 1) ~= (fontArray at: 1) ifTrue: [
 			result add: em.
 		].
